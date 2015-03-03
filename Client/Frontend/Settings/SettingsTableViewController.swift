mozilla-mobile/firefox-ios

Bug 1129161 - Re-implement connecting to Firefox Account in Settings sheet.
This implements a simpler view controller which displays an interstitial
spinner while loading.  On success; the underlying webview fades in.  On
error, a simple error text fades in.  There's no affordance to reload;
UX can paint that bikeshed as they see fit.

This patch also reflects the state of the Firefox Account in the
settings panel.  The state doesn't change yet, but it's there.

Client/Frontend/Settings/SettingsTableViewController.swift
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import Base32
 import UIKit
 
 class SettingsTableViewController: UITableViewController {
@@ -26,11 +27,11 @@ class SettingsTableViewController: UITableViewController {
 
         if indexPath.section == SECTION_ACCOUNT {
             cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: nil)
-            cell.editingAccessoryType = UITableViewCellAccessoryType.DisclosureIndicator
-            cell.textLabel?.text = "Sign in"
+            cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+            updateCell(cell, toReflectAccount: profile.getAccount())
         } else if indexPath.section == SECTION_SEARCH {
             cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
-            cell.editingAccessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+            cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
             cell.textLabel?.text = NSLocalizedString("Search", comment: "Settings")
         } else {
             cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
@@ -67,11 +68,92 @@ class SettingsTableViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
-        if indexPath.section == SECTION_SEARCH {
+        if indexPath.section == SECTION_ACCOUNT {
+            let viewController = FxAContentViewController()
+            viewController.delegate = self
+            if let account = profile.getAccount() {
+                switch account.getActionNeeded() {
+                case .None, .NeedsVerification:
+                    let cs = NSURLComponents(URL: profile.accountConfiguration.settingsURL, resolvingAgainstBaseURL: false)
+                    cs?.queryItems?.append(NSURLQueryItem(name: "email", value: account.email))
+                    viewController.url = cs?.URL
+                case .NeedsPassword:
+                    let cs = NSURLComponents(URL: profile.accountConfiguration.forceAuthURL, resolvingAgainstBaseURL: false)
+                    cs?.queryItems?.append(NSURLQueryItem(name: "email", value: account.email))
+                    viewController.url = cs?.URL
+                case .NeedsUpgrade:
+                    // In future, we'll want to link to an upgrade page.
+                    break
+                }
+            } else {
+                viewController.url = profile.accountConfiguration.signInURL
+            }
+            navigationController?.pushViewController(viewController, animated: true)
+        } else if indexPath.section == SECTION_SEARCH {
             let viewController = SearchSettingsTableViewController()
             viewController.model = profile.searchEngines
             navigationController?.pushViewController(viewController, animated: true)
         }
         return nil
+    }
+
+    func updateCell(cell: UITableViewCell, toReflectAccount account: FirefoxAccount?) {
+        if let account = account {
+            cell.textLabel?.text = account.email
+            cell.detailTextLabel?.text = nil
+
+            switch account.getActionNeeded() {
+            case .None:
+                break
+            case .NeedsVerification:
+                cell.detailTextLabel?.text = NSLocalizedString("Verify your email address.", comment: "Settings")
+            case .NeedsPassword:
+                // This assumes we never recycle cells.
+                cell.detailTextLabel?.textColor = UIColor(red: 255.0 / 255, green: 149.0 / 255, blue: 0.0 / 255, alpha: 1) // Firefox orange!
+                cell.detailTextLabel?.text = NSLocalizedString("Enter your password to connect.", comment: "Settings")
+            case .NeedsUpgrade:
+                cell.detailTextLabel?.textColor = UIColor(red: 255.0 / 255, green: 149.0 / 255, blue: 0.0 / 255, alpha: 1) // Firefox orange!
+                cell.detailTextLabel?.text = NSLocalizedString("Upgrade Firefox to connect.", comment: "Settings")
+                cell.accessoryType = UITableViewCellAccessoryType.None
+            }
+        } else {
+            cell.textLabel?.text = NSLocalizedString("Sign in", comment: "Settings")
+        }
+    }
+}
+
+extension SettingsTableViewController: FxAContentViewControllerDelegate {
+    func contentViewControllerDidSignIn(viewController: FxAContentViewController, data: JSON) {
+        if data["keyFetchToken"].asString? == nil || data["unwrapBKey"].asString? == nil {
+            // The /settings endpoint sends a partial "login"; ignore it entirely.
+            NSLog("Ignoring didSignIn with keyFetchToken or unwrapBKey missing.")
+            return
+        }
+
+        // TODO: Error handling.
+        let state = FirefoxAccountState.Engaged(
+            verified: data["verified"].asBool ?? false,
+            sessionToken: data["sessionToken"].asString!.hexDecodedData,
+            keyFetchToken: data["keyFetchToken"].asString!.hexDecodedData,
+            unwrapkB: data["unwrapBKey"].asString!.hexDecodedData
+        )
+
+        let account = FirefoxAccount(
+            email: data["email"].asString!,
+            uid: data["uid"].asString!,
+            authEndpoint: profile.accountConfiguration.authEndpointURL,
+            contentEndpoint: profile.accountConfiguration.profileEndpointURL,
+            oauthEndpoint: profile.accountConfiguration.oauthEndpointURL,
+            state: state
+        )
+
+        profile.setAccount(account)
+        tableView.reloadData()
+        navigationController?.popToRootViewControllerAnimated(true)
+    }
+
+    func contentViewControllerDidCancel(viewController: FxAContentViewController) {
+        NSLog("didCancel")
+        navigationController?.popToRootViewControllerAnimated(true)
     }
 }

brave/brave-core

Client/Frontend/Settings/SettingsTableViewController.swift
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import Base32
 import UIKit
 
 class SettingsTableViewController: UITableViewController {
@@ -26,11 +27,11 @@ class SettingsTableViewController: UITableViewController {
 
         if indexPath.section == SectionAccount {
             cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: nil)
-            cell.editingAccessoryType = UITableViewCellAccessoryType.DisclosureIndicator
-            cell.textLabel?.text = NSLocalizedString("Sign in", comment: "Settings")
-        } else if indexPath.section == SectionSearch {
+            cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+            updateCell(cell, toReflectAccount: profile.getAccount())
+        } else if indexPath.section == SECTION_SEARCH {
             cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
-            cell.editingAccessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+            cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
             cell.textLabel?.text = NSLocalizedString("Search", comment: "Settings")
         } else {
             cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
@@ -67,11 +68,92 @@ class SettingsTableViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
-        if indexPath.section == SectionSearch {
+        if indexPath.section == SectionAccount {
+            let viewController = FxAContentViewController()
+            viewController.delegate = self
+            if let account = profile.getAccount() {
+                switch account.getActionNeeded() {
+                case .None, .NeedsVerification:
+                    let components = NSURLComponents(URL: profile.accountConfiguration.settingsURL, resolvingAgainstBaseURL: false)
+                    components?.queryItems?.append(NSURLQueryItem(name: "email", value: account.email))
+                    viewController.url = components?.URL
+                case .NeedsPassword:
+                    let components = NSURLComponents(URL: profile.accountConfiguration.forceAuthURL, resolvingAgainstBaseURL: false)
+                    components?.queryItems?.append(NSURLQueryItem(name: "email", value: account.email))
+                    viewController.url = components?.URL
+                case .NeedsUpgrade:
+                    // In future, we'll want to link to an upgrade page.
+                    break
+                }
+            } else {
+                viewController.url = profile.accountConfiguration.signInURL
+            }
+            navigationController?.pushViewController(viewController, animated: true)
+        } else if indexPath.section == SectionSearch {
             let viewController = SearchSettingsTableViewController()
             viewController.model = profile.searchEngines
             navigationController?.pushViewController(viewController, animated: true)
         }
         return nil
+    }
+
+    func updateCell(cell: UITableViewCell, toReflectAccount account: FirefoxAccount?) {
+        if let account = account {
+            cell.textLabel?.text = account.email
+            cell.detailTextLabel?.text = nil
+
+            switch account.getActionNeeded() {
+            case .None:
+                break
+            case .NeedsVerification:
+                cell.detailTextLabel?.text = NSLocalizedString("Verify your email address.", comment: "Settings")
+            case .NeedsPassword:
+                // This assumes we never recycle cells.
+                cell.detailTextLabel?.textColor = UIColor(red: 255.0 / 255, green: 149.0 / 255, blue: 0.0 / 255, alpha: 1) // Firefox orange!
+                cell.detailTextLabel?.text = NSLocalizedString("Enter your password to connect.", comment: "Settings")
+            case .NeedsUpgrade:
+                cell.detailTextLabel?.textColor = UIColor(red: 255.0 / 255, green: 149.0 / 255, blue: 0.0 / 255, alpha: 1) // Firefox orange!
+                cell.detailTextLabel?.text = NSLocalizedString("Upgrade Firefox to connect.", comment: "Settings")
+                cell.accessoryType = UITableViewCellAccessoryType.None
+            }
+        } else {
+            cell.textLabel?.text = NSLocalizedString("Sign in", comment: "Settings")
+        }
+    }
+}
+
+extension SettingsTableViewController: FxAContentViewControllerDelegate {
+    func contentViewControllerDidSignIn(viewController: FxAContentViewController, data: JSON) {
+        if data["keyFetchToken"].asString? == nil || data["unwrapBKey"].asString? == nil {
+            // The /settings endpoint sends a partial "login"; ignore it entirely.
+            NSLog("Ignoring didSignIn with keyFetchToken or unwrapBKey missing.")
+            return
+        }
+
+        // TODO: Error handling.
+        let state = FirefoxAccountState.Engaged(
+            verified: data["verified"].asBool ?? false,
+            sessionToken: data["sessionToken"].asString!.hexDecodedData,
+            keyFetchToken: data["keyFetchToken"].asString!.hexDecodedData,
+            unwrapkB: data["unwrapBKey"].asString!.hexDecodedData
+        )
+
+        let account = FirefoxAccount(
+            email: data["email"].asString!,
+            uid: data["uid"].asString!,
+            authEndpoint: profile.accountConfiguration.authEndpointURL,
+            contentEndpoint: profile.accountConfiguration.profileEndpointURL,
+            oauthEndpoint: profile.accountConfiguration.oauthEndpointURL,
+            state: state
+        )
+
+        profile.setAccount(account)
+        tableView.reloadData()
+        navigationController?.popToRootViewControllerAnimated(true)
+    }
+
+    func contentViewControllerDidCancel(viewController: FxAContentViewController) {
+        NSLog("didCancel")
+        navigationController?.popToRootViewControllerAnimated(true)
     }
 }