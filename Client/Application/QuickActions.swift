roliusz/firefox-ios

Bug 1227108 - Add 'Open Last Bookmark' dynamic quick action * Ensure that Browser always returns a URL if available, grabbing from sessionData if none set on the webView * Add when new bookmark is added * Do not remove, only update when new bookmark added (To be confirmed by @tecgirl)

Client/Application/QuickActions.swift
@@ -12,6 +12,7 @@ import Foundation
 enum ShortcutType: String {
     case NewTab
     case NewPrivateTab
+    case OpenLastBookmark
 
     init?(fullType: String) {
         guard let last = fullType.componentsSeparatedByString(".").last else { return nil }
@@ -32,6 +33,13 @@ protocol QuickActionHandlerDelegate {
 @available(iOS 9, *)
 struct QuickActions {
 
+    static let QuickActionsVersion = "1.0"
+    static let QuickActionsVersionKey = "dynamicQuickActionsVersion"
+
+    private let lastBookmarkTitle = NSLocalizedString("Open Last Bookmark", comment: "String describing the action of opening the last added bookmark from the home screen Quick Actions via 3D Touch")
+
+    static var sharedInstance = QuickActions()
+
     var launchedShortcutItem: UIApplicationShortcutItem?
 
     func handleShortCutItem(shortcutItem: UIApplicationShortcutItem, completionBlock: (type: ShortcutType, userData: [String: NSSecureCoding]?)->Void ) -> Bool {
@@ -44,5 +52,43 @@ struct QuickActions {
         }
 
         return true
+    }
+
+    mutating func addDynamicApplicationShortcutItemOfType(type: ShortcutType, withUserData userData: [NSObject : AnyObject] = [NSObject : AnyObject](), toApplication application: UIApplication) -> Bool {
+        // add the quick actions version so that it is always in the user info
+        var userInfo = userData
+        userInfo[QuickActions.QuickActionsVersionKey] = QuickActions.QuickActionsVersion
+        var dynamicShortcutItems = application.shortcutItems ?? [UIApplicationShortcutItem]()
+        switch(type) {
+        case .OpenLastBookmark:
+            let openLastBookmarkShortcut = UIMutableApplicationShortcutItem(type: ShortcutType.OpenLastBookmark.type,
+                localizedTitle: lastBookmarkTitle,
+                localizedSubtitle: userData["bookmarkTitle"] as? String,
+                icon: UIApplicationShortcutIcon(templateImageName: "quick_action_last_bookmark"),
+                userInfo: userInfo
+            )
+            if dynamicShortcutItems.count == 0 {
+                dynamicShortcutItems.append(openLastBookmarkShortcut)
+            } else {
+                dynamicShortcutItems[0] = openLastBookmarkShortcut
+            }
+        default:
+            print("Cannot add static shortcut item of type \(type)")
+            return false
+        }
+        application.shortcutItems = dynamicShortcutItems
+        return true
+    }
+
+    mutating func removeDynamicApplicationShortcutItemOfType(type: ShortcutType, fromApplication application: UIApplication) {
+        guard var dynamicShortcutItems = application.shortcutItems else { return }
+
+        let index = dynamicShortcutItems.indexOf{
+            $0.type == type.type
+        }
+        guard let indexOfItemToBeRemoved = index else { return }
+
+        dynamicShortcutItems.removeAtIndex(indexOfItemToBeRemoved)
+        application.shortcutItems = dynamicShortcutItems
     }
 }

brave/brave-core

Client/Application/QuickActions.swift
@@ -8,10 +8,14 @@
 
 import Foundation
 
+import Shared
+import XCGLogger
+
 @available(iOS 9, *)
 enum ShortcutType: String {
     case NewTab
     case NewPrivateTab
+    case OpenLastBookmark
 
     init?(fullType: String) {
         guard let last = fullType.componentsSeparatedByString(".").last else { return nil }
@@ -32,6 +36,15 @@ protocol QuickActionHandlerDelegate {
 @available(iOS 9, *)
 struct QuickActions {
 
+    private let log = Logger.browserLogger
+
+    static let QuickActionsVersion = "1.0"
+    static let QuickActionsVersionKey = "dynamicQuickActionsVersion"
+
+    private let lastBookmarkTitle = NSLocalizedString("Open Last Bookmark", comment: "String describing the action of opening the last added bookmark from the home screen Quick Actions via 3D Touch")
+
+    static var sharedInstance = QuickActions()
+
     var launchedShortcutItem: UIApplicationShortcutItem?
 
     func handleShortCutItem(shortcutItem: UIApplicationShortcutItem, completionBlock: (type: ShortcutType, userData: [String: NSSecureCoding]?)->Void ) -> Bool {
@@ -44,5 +57,39 @@ struct QuickActions {
         }
 
         return true
+    }
+
+    mutating func addDynamicApplicationShortcutItemOfType(type: ShortcutType, var withUserData userData: [NSObject : AnyObject] = [NSObject : AnyObject](), toApplication application: UIApplication) -> Bool {
+        // add the quick actions version so that it is always in the user info
+        userData[QuickActions.QuickActionsVersionKey] = QuickActions.QuickActionsVersion
+        var dynamicShortcutItems = application.shortcutItems ?? [UIApplicationShortcutItem]()
+        switch(type) {
+        case .OpenLastBookmark:
+            let openLastBookmarkShortcut = UIMutableApplicationShortcutItem(type: ShortcutType.OpenLastBookmark.type,
+                localizedTitle: lastBookmarkTitle,
+                localizedSubtitle: userData["bookmarkTitle"] as? String,
+                icon: UIApplicationShortcutIcon(templateImageName: "quick_action_last_bookmark"),
+                userInfo: userData
+            )
+            // either replace the item if it already exists or add it if the array of items is empty
+            if dynamicShortcutItems.isEmpty {
+                dynamicShortcutItems.append(openLastBookmarkShortcut)
+            } else {
+                dynamicShortcutItems[0] = openLastBookmarkShortcut
+            }
+        default:
+            log.warning("Cannot add static shortcut item of type \(type)")
+            return false
+        }
+        application.shortcutItems = dynamicShortcutItems
+        return true
+    }
+
+    mutating func removeDynamicApplicationShortcutItemOfType(type: ShortcutType, fromApplication application: UIApplication) {
+        guard var dynamicShortcutItems = application.shortcutItems,
+            let index = (dynamicShortcutItems.indexOf{ $0.type == type.type }) else { return }
+
+        dynamicShortcutItems.removeAtIndex(index)
+        application.shortcutItems = dynamicShortcutItems
     }
 }