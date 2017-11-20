154fd800ecaded9d6aaf2a82c7ec48a36a356eff
Brian R. Bondy <netzen@gmail.com>
2017-11-20 09:25:53 -0500

    NoErrorsPlugin deprecated use NoEmitOnErrorsPlugin

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -31,7 +31,7 @@ const baseDevConfig = () => ({
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
     new webpack.IgnorePlugin(/[^/]+\/[\S]+.prod$/),
     new webpack.DefinePlugin({
       __HOST__: `'${host}'`,
