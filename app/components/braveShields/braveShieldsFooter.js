974d2264e2357dfb2b0a6ad5d6fd746b307bd89b
Cezar Augusto <cezaraugusto@outlook.it>
2017-11-29 00:19:28 -0200

    add static shields footer

--- app/components/braveShields/braveShieldsFooter.js
+++ app/components/braveShields/braveShieldsFooter.js
@@ -3,13 +3,24 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import React, { Component } from 'react'
+import { Grid, Column, ActionButton, Anchor } from 'brave-ui'
 
 export default class BraveShieldsFooter extends Component {
   render () {
     return (
-      <header>
-        <h1>Footer</h1>
-      </header>
+      <Grid gap='10px' padding='0 10px 10px' background='#eee'>
+        <Column align='flex-start' size={9}>
+          <Anchor
+            noStyle
+            href='chrome://settings'
+            target='_blank'
+            text='Edit default shield settings...'
+          />
+        </Column>
+        <Column align='flex-end' size={3}>
+          <ActionButton text='Reload' />
+        </Column>
+      </Grid>
     )
   }
 }
