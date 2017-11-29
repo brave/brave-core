b3931d5a856384beabdca94f8378d52c0376717b
Cezar Augusto <cezaraugusto@outlook.it>
2017-11-29 00:23:38 -0200

    add static shields header

--- app/components/braveShields/braveShieldsHeader.js
+++ app/components/braveShields/braveShieldsHeader.js
@@ -3,14 +3,55 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import React, { Component } from 'react'
+import { Grid, Column, Separator, SwitchButton, ActionButton, BrowserText } from 'brave-ui'
 
 export default class BraveShieldsHeader extends Component {
+  constructor () {
+    super()
+    this.onClosePopup = this.onClosePopup.bind(this)
+  }
+  onClosePopup () {
+    window.close()
+  }
   render () {
-    const { hostname } = this.props
+    const { shieldsToggled, hostname } = this.props
     return (
-      <header>
-        <h1>{hostname}</h1>
-      </header>
+      <div>
+        <Grid
+          id='shieldsHeader'
+          background='#808080'
+          padding='10px'
+          gap='0'
+          textColor='#fafafa'
+        >
+          <Column size={4} verticalAlign='center'>
+            <BrowserText noSelect fontSize='14px' text='Shields' />
+          </Column>
+          <Column size={6} verticalAlign='center'>
+            <SwitchButton
+              id='shieldsToggle'
+              leftText='Up'
+              rightText='Down'
+              checked={false}
+              onChange={shieldsToggled/* adBlockToggled */}
+            />
+          </Column>
+          <Column size={2} align='flex-end' verticalAlign='center'>
+            <ActionButton
+              fontSize='20px'
+              text='&times;'
+              onClick={this.onClosePopup}
+            />
+          </Column>
+          <Column>
+            <Separator />
+            <BrowserText noSelect text='Site shield settings for' />
+          </Column>
+          <Column verticalAlign='center'>
+            <BrowserText noSelect fontSize='20px' text={hostname} />
+          </Column>
+        </Grid>
+      </div>
     )
   }
 }
