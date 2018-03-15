5b1175566ad65c02cdd8db42ce015fa106c7353a
Garrett Robinson <grobinson@brave.com>
2018-03-15 12:09:07 -0700

    Use MPL

--- common/importer/chrome_importer_utils.cc
+++ common/importer/chrome_importer_utils.cc
@@ -1,6 +1,6 @@
-// Copyright (c) 2016 GitHub, Inc.
-// Use of this source code is governed by the MIT license that can be
-// found in the LICENSE file.
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/common/importer/chrome_importer_utils.h"
 
--- common/importer/chrome_importer_utils.h
+++ common/importer/chrome_importer_utils.h
@@ -1,6 +1,6 @@
-// Copyright (c) 2016 GitHub, Inc.
-// Use of this source code is governed by the MIT license that can be
-// found in the LICENSE file.
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #ifndef BRAVE_COMMON_IMPORTER_CHROME_IMPORTER_UTILS_H_
 #define BRAVE_COMMON_IMPORTER_CHROME_IMPORTER_UTILS_H_
