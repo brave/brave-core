712544f142cd12cd6011781a948aa926789fe1fd
Cezar Augusto <cezaraugusto@outlook.it>
2018-03-28 06:03:46 -0300

    fix missing closing bracket in test function

--- test/app/components/braveShields/braveShieldsControlsTest.tsx
+++ test/app/components/braveShields/braveShieldsControlsTest.tsx
@@ -46,6 +46,8 @@ function setup () {
     fingerprintingToggled: () => {
       return {
         type: actionTypes.FINGERPRINTING_TOGGLED
+      }
+    }
   }
 
   const renderer = renderIntoDocument(<BraveShieldsControls {...props} />) as React.Component<BraveShieldsControls>
