0fc55c53de7d21f9c18fa7e736bd5e7a4518ed1b
Brian R. Bondy <netzen@gmail.com>
2018-04-10 16:57:34 -0400

    Turn on 3rd party cookie blocking by default

--- browser/brave_profile_prefs.cc
+++ browser/brave_profile_prefs.cc
@@ -5,13 +5,21 @@
 #include "brave/browser/brave_profile_prefs.h"
 
 #include "brave/components/brave_shields/browser/brave_shields_web_contents_observer.h"
+#include "components/content_settings/core/common/pref_names.h"
 #include "components/pref_registry/pref_registry_syncable.h"
-
 
 namespace brave {
 
 void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   brave_shields::BraveShieldsWebContentsObserver::RegisterProfilePrefs(registry);
+
+  // This is registered from ProfileImpl::ProfileImpl in
+  // BrowserContextDependencyManager::GetInstance()->
+  //   RegisterProfilePrefsForServices(this, pref_registry_.get());
+  // We re-regsiter it here to override.
+  registry->RegisterBooleanPref(
+      prefs::kBlockThirdPartyCookies, true,
+      user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
 }
 
 }  // namespace brave
