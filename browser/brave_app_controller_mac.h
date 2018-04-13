393cd044575c3ff27276c8b04c0d5e416d26d8ff
Simon Hong <shong@igalia.com>
2018-04-14 00:00:08 +0900

    Introduce BraveAppController
    Move update stuff to BraveAppController from AppController.

---
+++ browser/brave_app_controller_mac.h
@@ -0,0 +1,14 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_BROWSER_BRAVE_APP_CONTROLLER_MAC_H_
+#define BRAVE_BROWSER_BRAVE_APP_CONTROLLER_MAC_H_
+
+#import "chrome/browser/app_controller_mac.h"
+
+@interface BraveAppController : AppController
+
+@end
+
+#endif  // BRAVE_BROWSER_BRAVE_APP_CONTROLLER_MAC_H_
