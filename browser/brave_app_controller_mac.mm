5c8fa3004efffdde51901b515528cc627e2476c2
Simon Hong <shong@igalia.com>
2018-04-17 16:36:40 +0900

    Cleanup - Make log more descriptive

--- browser/brave_app_controller_mac.mm
+++ browser/brave_app_controller_mac.mm
@@ -18,6 +18,12 @@ BOOL UpdateEnabled() {
       switches::kEnableBraveUpdateTest);
 }
 
+std::string GetDescriptionFromAppcastItem(SUAppcastItem* item) {
+  NSString* description = [NSString stringWithFormat:@"AppcastItem(Date: %@, Version: %@)",
+      [item dateString], [item versionString]];
+  return [description UTF8String];
+}
+
 }
 
 @implementation BraveAppController
@@ -45,85 +51,71 @@ BOOL UpdateEnabled() {
 }
 
 - (void)initializeBraveUpdater {
-  SUUpdater* updater = [SUUpdater sharedUpdater];
-
-  [updater setDelegate:(id<SUUpdaterDelegate>)self];
-
-  // Reset previous feed url set by setFeedURL().
-  // In the SUUpdater document, that value persists in the host bundle's user
-  // default.
-  // Tried to remove that value by removing Brave.app bundle in out/Release and
-  // ~/Library/Preferences/org.brave.Brave.plist.
-  // After these removing, previous value(https://example.org) is still remained
-  // and used when there is SUFeedURL is exsited in Info.plist.
-  // When nil is set, SUFeedURL value in Info.plist is used.
-  // TODO(shong): Remove this.
-  [updater setFeedURL:nil];
+  [[SUUpdater sharedUpdater] setDelegate:(id<SUUpdaterDelegate>)self];
 }
 
 #pragma mark - SUUpdaterDelegate
 
 - (void)updater:(SUUpdater *)updater
     didFinishLoadingAppcast:(SUAppcast *)appcast {
-  LOG(ERROR) << __FUNCTION__;
+  VLOG(0) << "brave update: did finish loading appcast";
 }
 
 - (void)updater:(SUUpdater *)updater didFindValidUpdate:(SUAppcastItem *)item {
-  LOG(ERROR) << __FUNCTION__;
+  VLOG(0) << "brave update: did finish valid update with " +
+             GetDescriptionFromAppcastItem(item);
 }
 
 - (void)updaterDidNotFindUpdate:(SUUpdater *)updater {
-  LOG(ERROR) << __FUNCTION__;
+  VLOG(0) << "brave update: did not find update";
 }
 
 - (void)updater:(SUUpdater *)updater
     willDownloadUpdate:(SUAppcastItem *)item
            withRequest:(NSMutableURLRequest *)request {
-  LOG(ERROR) << __FUNCTION__;
+  VLOG(0) << "brave update: willDownloadUpdate with " +
+             GetDescriptionFromAppcastItem(item);
 }
 
 - (void)updater:(SUUpdater *)updater
     failedToDownloadUpdate:(SUAppcastItem *)item error:(NSError *)error {
-  LOG(ERROR) << __FUNCTION__;
+  VLOG(0) << "brave update: failed to download update with " +
+             GetDescriptionFromAppcastItem(item) +
+             " with error - " + [[error description] UTF8String];
 }
 
 - (void)userDidCancelDownload:(SUUpdater *)updater {
-  LOG(ERROR) << __FUNCTION__;
+  VLOG(0) << "brave update: user did cancel download";
 }
 
 - (void)updater:(SUUpdater *)updater willInstallUpdate:(SUAppcastItem *)item {
-  LOG(ERROR) << __FUNCTION__;
+  VLOG(0) << "brave update: will install update with " +
+             GetDescriptionFromAppcastItem(item);
 }
 
 - (void)updaterWillRelaunchApplication:(SUUpdater *)updater {
-  LOG(ERROR) << __FUNCTION__;
+  VLOG(0) << "brave update: will relaunch application";
 }
 
 - (void)updaterDidRelaunchApplication:(SUUpdater *)updater {
-  LOG(ERROR) << __FUNCTION__;
-}
-
-- (void)updaterWillShowModalAlert:(SUUpdater *)updater {
-  LOG(ERROR) << __FUNCTION__;
-}
-
-- (void)updaterDidShowModalAlert:(SUUpdater *)updater {
-  LOG(ERROR) << __FUNCTION__;
+  VLOG(0) << "brave update: did relaunch application";
 }
 
 - (void)updater:(SUUpdater *)updater
     willInstallUpdateOnQuit:(SUAppcastItem *)item
     immediateInstallationInvocation:(NSInvocation *)invocation {
-  LOG(ERROR) << __FUNCTION__;
+  VLOG(0) << "brave update: will install update on quit with " +
+             GetDescriptionFromAppcastItem(item);
 }
 
 - (void)updater:(SUUpdater *)updater
     didCancelInstallUpdateOnQuit:(SUAppcastItem *)item {
-  LOG(ERROR) << __FUNCTION__;
+  VLOG(0) << "brave update: did cancel install update on quit with " +
+             GetDescriptionFromAppcastItem(item);
 }
 
 - (void)updater:(SUUpdater *)updater didAbortWithError:(NSError *)error {
-  LOG(ERROR) << [[error description] UTF8String];
+  VLOG(0) << [[error description] UTF8String];
 }
 
 @end  // @implementation BraveAppController
