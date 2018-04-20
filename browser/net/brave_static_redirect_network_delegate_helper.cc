be7c77761de7b03c61ecccc7585baed7f4a54ac4
Emerick Rogul <erogul@brave.com>
2018-04-20 16:49:03 -0400

    Redirect extension update requests to Brave's extension server

--- browser/net/brave_static_redirect_network_delegate_helper.cc
+++ browser/net/brave_static_redirect_network_delegate_helper.cc
@@ -5,11 +5,25 @@
 #include "brave/browser/net/brave_static_redirect_network_delegate_helper.h"
 
 #include "brave/common/network_constants.h"
+#include "components/component_updater/component_updater_url_constants.h"
 #include "extensions/common/url_pattern.h"
 #include "net/url_request/url_request.h"
 
 
 namespace brave {
+
+bool IsUpdaterURL(const GURL& gurl) {
+  static std::vector<URLPattern> updater_patterns({
+      URLPattern(URLPattern::SCHEME_HTTPS, std::string(component_updater::kUpdaterDefaultUrl) + "*"),
+      URLPattern(URLPattern::SCHEME_HTTPS, std::string(component_updater::kUpdaterFallbackUrl) + "*"),
+      URLPattern(URLPattern::SCHEME_HTTPS, std::string(component_updater::kUpdaterDefaultUrlAlt) + "*"),
+      URLPattern(URLPattern::SCHEME_HTTPS, std::string(component_updater::kUpdaterFallbackUrlAlt) + "*"),
+  });
+  return std::any_of(updater_patterns.begin(), updater_patterns.end(),
+      [&gurl](URLPattern pattern){
+        return pattern.MatchesURL(gurl);
+      });
+}
 
 int OnBeforeURLRequest_StaticRedirectWork(
     net::URLRequest* request,
@@ -19,6 +33,12 @@ int OnBeforeURLRequest_StaticRedirectWork(
   static URLPattern geo_pattern(URLPattern::SCHEME_HTTPS, kGeoLocationsPattern);
   if (geo_pattern.MatchesURL(request->url())) {
     *new_url = GURL(GOOGLEAPIS_ENDPOINT GOOGLEAPIS_API_KEY);
+    return net::OK;
+  }
+  if (IsUpdaterURL(request->url())) {
+    GURL::Replacements replacements;
+    replacements.SetQueryStr(request->url().query_piece());
+    *new_url = GURL(kBraveUpdatesExtensionsEndpoint).ReplaceComponents(replacements);
     return net::OK;
   }
   return net::OK;
--- common/network_constants.h
+++ common/network_constants.h
@@ -1,6 +1,8 @@
 #ifndef BRAVE_COMMON_NETWORK_CONSTANTS_H_
 #define BRAVE_COMMON_NETWORK_CONSTANTS_H_
 
+extern const char kBraveUpdatesExtensionsEndpoint[];
+
 extern const char kEmptyDataURI[];
 extern const char kJSDataURLPrefix[];
 extern const char kGeoLocationsPattern[];
