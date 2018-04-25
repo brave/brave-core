17b725ce54e97024c5b61e5e2c7de18d1ecd8548
Emerick Rogul <erogul@brave.com>
2018-04-25 09:16:39 -0400

    Specify correct scheme for fallback component updater URLs

--- browser/net/brave_static_redirect_network_delegate_helper.cc
+++ browser/net/brave_static_redirect_network_delegate_helper.cc
@@ -15,9 +15,9 @@ namespace brave {
 bool IsUpdaterURL(const GURL& gurl) {
   static std::vector<URLPattern> updater_patterns({
       URLPattern(URLPattern::SCHEME_HTTPS, std::string(component_updater::kUpdaterDefaultUrl) + "*"),
-      URLPattern(URLPattern::SCHEME_HTTPS, std::string(component_updater::kUpdaterFallbackUrl) + "*"),
+      URLPattern(URLPattern::SCHEME_HTTP, std::string(component_updater::kUpdaterFallbackUrl) + "*"),
       URLPattern(URLPattern::SCHEME_HTTPS, std::string(component_updater::kUpdaterDefaultUrlAlt) + "*"),
-      URLPattern(URLPattern::SCHEME_HTTPS, std::string(component_updater::kUpdaterFallbackUrlAlt) + "*"),
+      URLPattern(URLPattern::SCHEME_HTTP, std::string(component_updater::kUpdaterFallbackUrlAlt) + "*"),
   });
   return std::any_of(updater_patterns.begin(), updater_patterns.end(),
       [&gurl](URLPattern pattern){
