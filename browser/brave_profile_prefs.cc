68b54e3ed4948e0b9de653acded5d4b60062efe0
Brian R. Bondy <netzen@gmail.com>
2018-05-21 13:55:32 -0400

    Use default pref for setting preserve profile pref
    Fix: https://github.com/brave/brave-browser/issues/191

--- browser/brave_profile_prefs.cc
+++ browser/brave_profile_prefs.cc
@@ -5,6 +5,8 @@
 #include "brave/browser/brave_profile_prefs.h"
 
 #include "brave/components/brave_shields/browser/brave_shields_web_contents_observer.h"
+#include "chrome/browser/prefs/session_startup_pref.h"
+#include "chrome/common/pref_names.h"
 #include "components/content_settings/core/common/pref_names.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/signin/core/browser/signin_pref_names.h"
@@ -15,6 +17,8 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   brave_shields::BraveShieldsWebContentsObserver::RegisterProfilePrefs(registry);
   // Register any default pref overrides here
   registry->SetDefaultPrefValue(prefs::kSigninAllowed, base::Value(false));
+  registry->SetDefaultPrefValue(prefs::kRestoreOnStartup,
+      base::Value(SessionStartupPref::kPrefValueLast));
 }
 
 }  // namespace brave
