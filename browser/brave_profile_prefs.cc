1bc2989e7077eb79ddd16a29966f94ad6c139284
Brian R. Bondy <netzen@gmail.com>
2018-05-21 13:29:31 -0400

    Disable sign-in to Chrome
    Fix https://github.com/brave/brave-browser/issues/195

--- browser/brave_profile_prefs.cc
+++ browser/brave_profile_prefs.cc
@@ -7,12 +7,14 @@
 #include "brave/components/brave_shields/browser/brave_shields_web_contents_observer.h"
 #include "components/content_settings/core/common/pref_names.h"
 #include "components/pref_registry/pref_registry_syncable.h"
+#include "components/signin/core/browser/signin_pref_names.h"
 
 namespace brave {
 
 void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   brave_shields::BraveShieldsWebContentsObserver::RegisterProfilePrefs(registry);
   // Register any default pref overrides here
+  registry->SetDefaultPrefValue(prefs::kSigninAllowed, base::Value(false));
 }
 
 }  // namespace brave
