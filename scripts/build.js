df7d42d5985d0d70008d5474b10e2cce5e7d0927
Matt Bacchi <mbacchi@gmail.com>
2018-05-31 12:30:51 -0700

    fix webpack silently failing; update typescript and awesome-typescript-loader versions

--- scripts/build.js
+++ scripts/build.js
@@ -12,4 +12,7 @@ tasks.copyAssets('build')
 
 console.log('[Webpack Build]')
 console.log('-'.repeat(80))
-exec('webpack --config webpack/prod.config.js --progress --profile --colors')
+if (exec('webpack --config webpack/prod.config.js --progress --profile --colors').code !== 0) {
+  echo('Erorr: webpack failed')
+  exit(1)
+}
