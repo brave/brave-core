d67b21f665bc3eeb20a4f42d31de95fb1355adb1
Brian R. Bondy <netzen@gmail.com>
2018-06-12 14:41:32 -0400

    Handle and redirect brave:// to chrome://
    Fix https://github.com/brave/brave-browser/issues/213

---
+++ browser/autocomplete/brave_autocomplete_scheme_classifier.h
@@ -0,0 +1,23 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_BROWSER_AUTOCOMPLETE_BRAVE_AUTOCOMPLETE_SCHEME_CLASSIFIER_H_
+#define BRAVE_BROWSER_AUTOCOMPLETE_BRAVE_AUTOCOMPLETE_SCHEME_CLASSIFIER_H_
+
+#include "chrome/browser/autocomplete/chrome_autocomplete_scheme_classifier.h"
+
+class BraveAutocompleteSchemeClassifier : public ChromeAutocompleteSchemeClassifier {
+ public:
+  explicit BraveAutocompleteSchemeClassifier(Profile* profile);
+  ~BraveAutocompleteSchemeClassifier() override;
+
+  metrics::OmniboxInputType GetInputTypeForScheme(
+      const std::string& scheme) const override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(BraveAutocompleteSchemeClassifier);
+};
+
+#endif  // BRAVE_BROWSER_AUTOCOMPLETE_BRAVE_AUTOCOMPLETE_SCHEME_CLASSIFIER_H_
+
---
+++ browser/brave_content_browser_client_browsertest.cc
@@ -0,0 +1,26 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include <vector>
+
+#include "chrome/browser/ui/browser.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/ui_test_utils.h"
+#include "content/public/test/browser_test_utils.h"
+
+IN_PROC_BROWSER_TEST_F(InProcessBrowserTest, CanLoadChromeURL) {
+  GURL chrome_settings_url("chrome://settings/");
+  std::vector<GURL> urls {
+    chrome_settings_url,
+    GURL("about:settings/"),
+    GURL("brave://settings/")
+  };
+  std::for_each(urls.begin(), urls.end(), [this, &chrome_settings_url](const GURL& url) {
+    content::WebContents* contents = browser()->tab_strip_model()->GetActiveWebContents();
+    ui_test_utils::NavigateToURL(browser(), url);
+    ASSERT_TRUE(WaitForLoadStop(contents));
+    EXPECT_STREQ(contents->GetLastCommittedURL().spec().c_str(),
+        chrome_settings_url.spec().c_str());
+  });
+}
