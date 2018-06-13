74efa42ea72c265af8c9aa9269a59828cd64f100
bridiver <github@brianjohnson.cc>
2018-06-13 15:44:26 -0700

    disable account consistency fix https://github.com/brave/brave-browser/issues/338

---
+++ chromium_src/chrome/browser/signin/account_consistency_disabled_unittest.cc
@@ -0,0 +1,100 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/signin/account_consistency_mode_manager.h"
+
+#include <memory>
+#include <utility>
+
+#include "base/test/scoped_feature_list.h"
+#include "build/buildflag.h"
+#include "chrome/browser/prefs/browser_prefs.h"
+#include "chrome/browser/supervised_user/supervised_user_constants.h"
+#include "chrome/test/base/testing_profile.h"
+#include "components/prefs/pref_notifier_impl.h"
+#include "components/prefs/testing_pref_store.h"
+#include "components/signin/core/browser/profile_management_switches.h"
+#include "components/signin/core/browser/scoped_account_consistency.h"
+#include "components/signin/core/browser/signin_buildflags.h"
+#include "components/signin/core/browser/signin_pref_names.h"
+#include "components/sync_preferences/testing_pref_service_syncable.h"
+#include "content/public/common/content_features.h"
+#include "content/public/test/test_browser_thread_bundle.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
+// Checks that new profiles are migrated at creation.
+TEST(AccountConsistencyDisabledTest, NewProfile) {
+  content::TestBrowserThreadBundle test_thread_bundle;
+  base::test::ScopedFeatureList scoped_site_isolation;
+  scoped_site_isolation.InitAndEnableFeature(features::kSignInProcessIsolation);
+  TestingProfile::Builder profile_builder;
+  {
+    TestingPrefStore* user_prefs = new TestingPrefStore();
+
+    // Set the read error so that Profile::IsNewProfile() returns true.
+    user_prefs->set_read_error(PersistentPrefStore::PREF_READ_ERROR_NO_FILE);
+
+    std::unique_ptr<sync_preferences::TestingPrefServiceSyncable> pref_service =
+        std::make_unique<sync_preferences::TestingPrefServiceSyncable>(
+            new TestingPrefStore(), new TestingPrefStore(), user_prefs,
+            new TestingPrefStore(), new user_prefs::PrefRegistrySyncable(),
+            new PrefNotifierImpl());
+    RegisterUserProfilePrefs(pref_service->registry());
+    profile_builder.SetPrefService(std::move(pref_service));
+  }
+  std::unique_ptr<TestingProfile> profile = profile_builder.Build();
+  ASSERT_TRUE(profile->IsNewProfile());
+  EXPECT_FALSE(signin::IsDiceEnabledForProfile(profile->GetPrefs()));
+}
+
+TEST(AccountConsistencyDisabledTest, DiceFixAuthErrorsForAllProfiles) {
+  content::TestBrowserThreadBundle test_thread_bundle;
+
+  {
+    // Regular profile.
+    TestingProfile profile;
+    EXPECT_FALSE(
+        AccountConsistencyModeManager::IsDiceEnabledForProfile(&profile));
+    EXPECT_EQ(signin::AccountConsistencyMethod::kDiceFixAuthErrors,
+              AccountConsistencyModeManager::GetMethodForProfile(&profile));
+
+    // Incognito profile.
+    Profile* incognito_profile = profile.GetOffTheRecordProfile();
+    EXPECT_FALSE(AccountConsistencyModeManager::IsDiceEnabledForProfile(
+        incognito_profile));
+    EXPECT_FALSE(
+        AccountConsistencyModeManager::GetForProfile(incognito_profile));
+    EXPECT_EQ(
+        signin::AccountConsistencyMethod::kDiceFixAuthErrors,
+        AccountConsistencyModeManager::GetMethodForProfile(incognito_profile));
+  }
+
+  {
+    // Guest profile.
+    TestingProfile::Builder profile_builder;
+    profile_builder.SetGuestSession();
+    std::unique_ptr<Profile> profile = profile_builder.Build();
+    ASSERT_TRUE(profile->IsGuestSession());
+    EXPECT_FALSE(
+        AccountConsistencyModeManager::IsDiceEnabledForProfile(profile.get()));
+    EXPECT_EQ(
+        signin::AccountConsistencyMethod::kDiceFixAuthErrors,
+        AccountConsistencyModeManager::GetMethodForProfile(profile.get()));
+  }
+
+  {
+    // Legacy supervised profile.
+    TestingProfile::Builder profile_builder;
+    profile_builder.SetSupervisedUserId("supervised_id");
+    std::unique_ptr<Profile> profile = profile_builder.Build();
+    ASSERT_TRUE(profile->IsLegacySupervised());
+    EXPECT_FALSE(
+        AccountConsistencyModeManager::IsDiceEnabledForProfile(profile.get()));
+    EXPECT_EQ(
+        signin::AccountConsistencyMethod::kDiceFixAuthErrors,
+        AccountConsistencyModeManager::GetMethodForProfile(profile.get()));
+  }
+}
+#endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)
