6b436575d4a2b5dc79bd5d1fc8247f9bde2f374e
Jocelyn Liu <yrliou@gmail.com>
2018-06-22 13:29:13 -0700

    Add brave_page_visibility.js to provide our own pageVisibility setting

---
+++ app/resources/js/brave_page_visibility.js
@@ -0,0 +1,32 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+cr.define('settings', function() {
+  // use value defined in page_visibility.js in guest mode
+  if (loadTimeData.getBoolean('isGuest')) return;
+
+  // We need to specify values for every attribute in pageVisibility instead of
+  // only overriding specific attributes here because chromium does not
+  // explicitly define pageVisibility in page_visibility.js since polymer only
+  // notifies after a property is set.
+  // Use proxy objects here so we only need to write out the attributes we
+  // would like to hide.
+
+  const appearanceHandler = {
+    get: function(obj, prop) {
+      return prop === 'setTheme' ? false : true;
+    }
+  };
+
+  const handler = {
+    get: function(obj, prop) {
+      if (prop === 'appearance') return new Proxy({}, appearanceHandler);
+      return prop === 'a11y' ? false : true;
+    }
+  };
+
+  let proxy = new Proxy({}, handler);
+
+  return { pageVisibility: proxy };
+});
---
+++ chromium_src/chrome/grit/settings_resources.h
@@ -0,0 +1,2 @@
+#include "brave/grit/brave_generated_resources.h"
+#include "gen/chrome/grit/settings_resources.h"
