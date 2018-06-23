d0d487e1b9a92a9952972c327ec891d1c463b537
George Dregalo <37560650+gdregalo@users.noreply.github.com>
2018-06-22 22:01:45 -0400

    added bat_client_webrequest stub for non-chromium environment

---
+++ src/bat_client_webrequest.cc
@@ -0,0 +1,8 @@
+#include "bat_client_webrequest.h"
+
+
+namespace braveledger_bat_client_webrequest {
+
+  //TODO: implement
+
+} //namespace braveledger_bat_client_webreque
---
+++ src/bat_client_webrequest.h
@@ -0,0 +1,33 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVELEDGER_BAT_CLIENT_WEBREQUEST_H_
+#define BRAVELEDGER_BAT_CLIENT_WEBREQUEST_H_
+
+
+#include <vector>
+#include <string>
+#include "bat_helper.h"
+
+
+namespace braveledger_bat_client_webrequest {
+
+  class BatClientWebRequest
+  {
+  public:
+    BatClientWebRequest() = default;
+
+    void run(const std::string& url, braveledger_bat_helper::FetchCallback callback,
+      const std::vector<std::string>& headers, const std::string& content,
+      const std::string& contentType, const braveledger_bat_helper::FETCH_CALLBACK_EXTRA_DATA_ST& extraData,
+      const braveledger_bat_helper::URL_METHOD& method) {
+
+    }
+
+
+  };
+
+} //namespace braveledger_bat_client_webrequest
+
+#endif // BRAVELEDGER_BAT_CLIENT_WEBREQUEST_H_
