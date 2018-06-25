b1bbd8147bc3af06b8bfdcd2c2711c2c3666530a
Cezar Augusto <boss@cezaraugusto.net>
2018-06-25 13:23:30 -0300

    show browserAction as disabled when shield is off fix #240

---
+++ app/background/api/browserActionAPI.ts
@@ -0,0 +1,29 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * Sets the badge text
+ * @param {string} text - The text to put on the badge
+ */
+export const setBadgeText = (text: string) => {
+  if (chrome.browserAction) {
+    chrome.browserAction.setBadgeText({ text: String(text) })
+  }
+}
+
+/**
+ * Updates the shields icon based on shields state
+ */
+export const setIcon = (url: string, tabId: number, shieldsOn: boolean) => {
+  const shieldsEnabledIcon = '../../img/icon-16.png'
+  const shieldsDisabledIcon = '../../img/icon-16-disabled.png'
+  const isHttpOrHttps = url && /^http/.test(url)
+
+  if (chrome.browserAction) {
+    chrome.browserAction.setIcon({
+      path: shieldsOn && isHttpOrHttps ? shieldsEnabledIcon : shieldsDisabledIcon,
+      tabId
+    })
+  }
+}
