d2ba2bded7a45a329bfc53503d58a18f31e37316
Emerick Rogul <erogul@brave.com>
2018-06-26 10:48:53 -0400

    Support registering Brave local_state preferences

--- browser/brave_profile_prefs.cc
+++ browser/brave_profile_prefs.cc
@@ -4,7 +4,6 @@
 
 #include "brave/browser/brave_profile_prefs.h"
 
-#include "brave/browser/brave_stats_updater.h"
 #include "brave/components/brave_shields/browser/brave_shields_web_contents_observer.h"
 #include "chrome/browser/prefs/session_startup_pref.h"
 #include "chrome/common/pref_names.h"
@@ -16,7 +15,6 @@ namespace brave {
 
 void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   brave_shields::BraveShieldsWebContentsObserver::RegisterProfilePrefs(registry);
-  brave::RegisterPrefsForBraveStatsUpdater(registry);
 
   // No sign into Brave functionality
   registry->SetDefaultPrefValue(prefs::kSigninAllowed, base::Value(false));
