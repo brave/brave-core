11d35f09017605e4cbcf9a3c59551aa201db2b3c
Jocelyn Liu <yrliou@gmail.com>
2018-06-29 09:09:39 -0700

    Replace chrome cast support urls with brave community page

---
+++ chromium_src/chrome/browser/ui/singleton_tabs.cc
@@ -0,0 +1,14 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#define ShowSingletonTab ShowSingletonTab_ChromiumImpl
+#include "../../../../../chrome/browser/ui/singleton_tabs.cc"
+#undef ShowSingletonTab
+
+void ShowSingletonTab(Browser* browser, const GURL& url) {
+  GURL new_url = url.DomainIs("google.com") ?
+    GURL("https://community.brave.com/") : url;
+
+  ShowSingletonTab_ChromiumImpl(browser, new_url);
+}
