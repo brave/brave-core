f12b26ce57ffe4a5eb55a2922840384de476c108
Kyle Hickinson <kyle.hickinson@gmail.com>
2018-07-03 16:14:10 -0400

    Cleanup, add 4 spaces pref to project & added Xcode 10 compatibility

--- Client/Frontend/Settings/ClearPrivateDataTableViewController.swift
+++ Client/Frontend/Settings/ClearPrivateDataTableViewController.swift
@@ -17,199 +17,199 @@ private let log = Logger.browserLogger
 private let HistoryClearableIndex = 0
 
 class ClearPrivateDataTableViewController: UITableViewController {
-  fileprivate var clearButton: UITableViewCell?
-  
-  var profile: Profile!
-  var tabManager: TabManager!
-  
-  fileprivate var gotNotificationDeathOfAllWebViews = false
-  
-  fileprivate typealias DefaultCheckedState = Bool
-  
-  fileprivate lazy var clearables: [(clearable: Clearable, checked: DefaultCheckedState)] = {
-    return [
-      (HistoryClearable(), true),
-      (CacheClearable(), true),
-      (CookiesClearable(), true),
-      (PasswordsClearable(profile: self.profile), true),
-      ]
-  }()
-  
-  fileprivate lazy var toggles: [Bool] = {
-    if let savedToggles = self.profile.prefs.arrayForKey(TogglesPrefKey) as? [Bool] {
-      return savedToggles
-    }
+    fileprivate var clearButton: UITableViewCell?
     
-    return self.clearables.map { $0.checked }
-  }()
-  
-  fileprivate var clearButtonEnabled = true {
-    didSet {
-      clearButton?.textLabel?.textColor = clearButtonEnabled ? UIConstants.DestructiveRed : UIColor.lightGray
-    }
-  }
-  
-  init() {
-    super.init(style: .grouped)
-  }
-  
-  @available(*, unavailable)
-  required init?(coder aDecoder: NSCoder) {
-    fatalError()
-  }
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
+    var profile: Profile!
+    var tabManager: TabManager!
     
-    title = Strings.ClearPrivateData
+    fileprivate var gotNotificationDeathOfAllWebViews = false
     
-    tableView.register(SettingsTableSectionHeaderFooterView.self, forHeaderFooterViewReuseIdentifier: SectionHeaderFooterIdentifier)
+    fileprivate typealias DefaultCheckedState = Bool
     
-    tableView.separatorColor = UIConstants.TableViewSeparatorColor
-    tableView.backgroundColor = UIConstants.TableViewHeaderBackgroundColor
-  }
-  
-  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: nil)
+    fileprivate lazy var clearables: [(clearable: Clearable, checked: DefaultCheckedState)] = {
+        return [
+            (HistoryClearable(), true),
+            (CacheClearable(), true),
+            (CookiesClearable(), true),
+            (PasswordsClearable(profile: self.profile), true),
+            ]
+    }()
     
-    if indexPath.section == SectionToggles {
-      cell.textLabel?.text = clearables[indexPath.item].clearable.label
-      let control = UISwitch()
-      control.onTintColor = UIConstants.ControlTintColor
-      control.addTarget(self, action: #selector(ClearPrivateDataTableViewController.switchValueChanged(_:)), for: UIControlEvents.valueChanged)
-      control.isOn = toggles[indexPath.item]
-      cell.accessoryView = control
-      cell.selectionStyle = .none
-      control.tag = indexPath.item
-    } else {
-      assert(indexPath.section == SectionButton)
-      cell.textLabel?.text = Strings.ClearPrivateData
-      cell.textLabel?.textAlignment = NSTextAlignment.center
-      cell.textLabel?.textColor = UIConstants.DestructiveRed
-      cell.accessibilityTraits = UIAccessibilityTraitButton
-      cell.accessibilityIdentifier = "ClearPrivateData"
-      clearButton = cell
-    }
-    
-    return cell
-  }
-  
-  override func numberOfSections(in tableView: UITableView) -> Int {
-    return NumberOfSections
-  }
-  
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    if section == SectionToggles {
-      return clearables.count
-    }
-    
-    assert(section == SectionButton)
-    return 1
-  }
-  
-  override func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
-    guard indexPath.section == SectionButton else { return false }
-    
-    // Highlight the button only if it's enabled.
-    return clearButtonEnabled
-  }
-  
-  static func clearPrivateData(_ clearables: [Clearable], secondAttempt: Bool = false) -> Deferred<Void> {
-    let deferred = Deferred<Void>()
-    
-    clearables.enumerated().map { clearable in
-      print("Clearing \(clearable.element).")
-      
-      let res = Success()
-      succeed().upon() { _ in // move off main thread
-        clearable.element.clear().upon() { result in
-          res.fill(result)
-        }
-      }
-      return res
-      }
-      .allSucceed()
-      .upon { result in
-        if !result.isSuccess && !secondAttempt {
-          print("Private data NOT cleared successfully")
-          DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
-            // For some reason, a second attempt seems to always succeed
-            clearPrivateData(clearables, secondAttempt: true).upon() { _ in
-              deferred.fill(())
-            }
-          })
-          return
+    fileprivate lazy var toggles: [Bool] = {
+        if let savedToggles = self.profile.prefs.arrayForKey(TogglesPrefKey) as? [Bool] {
+            return savedToggles
         }
         
-        if !result.isSuccess {
-          print("Private data NOT cleared after 2 attempts")
-        }
-        deferred.fill(())
-    }
-    return deferred
-  }
-  
-  @objc fileprivate func allWebViewsKilled() {
-    gotNotificationDeathOfAllWebViews = true
+        return self.clearables.map { $0.checked }
+    }()
     
-    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: { // for some reason, even after all webviews killed, an big delay is needed before the filehandles are unlocked
-      var clear = [Clearable]()
-      for i in 0..<self.clearables.count {
-        if self.toggles[i] {
-          clear.append(self.clearables[i].clearable)
+    fileprivate var clearButtonEnabled = true {
+        didSet {
+            clearButton?.textLabel?.textColor = clearButtonEnabled ? UIConstants.DestructiveRed : UIColor.lightGray
         }
-      }
-      
-      // TODO: Bring back PrivateBrowsing
-      /*
-      if PrivateBrowsing.singleton.isOn {
-        PrivateBrowsing.singleton.exit().upon {
-          ClearPrivateDataTableViewController.clearPrivateData(clear).upon {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: {
-              PrivateBrowsing.singleton.enter()
-              getApp().tabManager.addTabAndSelect()
+    }
+    
+    init() {
+        super.init(style: .grouped)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = Strings.ClearPrivateData
+        
+        tableView.register(SettingsTableSectionHeaderFooterView.self, forHeaderFooterViewReuseIdentifier: SectionHeaderFooterIdentifier)
+        
+        tableView.separatorColor = UIConstants.TableViewSeparatorColor
+        tableView.backgroundColor = UIConstants.TableViewHeaderBackgroundColor
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: nil)
+        
+        if indexPath.section == SectionToggles {
+            cell.textLabel?.text = clearables[indexPath.item].clearable.label
+            let control = UISwitch()
+            control.onTintColor = UIConstants.ControlTintColor
+            control.addTarget(self, action: #selector(ClearPrivateDataTableViewController.switchValueChanged(_:)), for: UIControlEvents.valueChanged)
+            control.isOn = toggles[indexPath.item]
+            cell.accessoryView = control
+            cell.selectionStyle = .none
+            control.tag = indexPath.item
+        } else {
+            assert(indexPath.section == SectionButton)
+            cell.textLabel?.text = Strings.ClearPrivateData
+            cell.textLabel?.textAlignment = NSTextAlignment.center
+            cell.textLabel?.textColor = UIConstants.DestructiveRed
+            cell.accessibilityTraits = UIAccessibilityTraitButton
+            cell.accessibilityIdentifier = "ClearPrivateData"
+            clearButton = cell
+        }
+        
+        return cell
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return NumberOfSections
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == SectionToggles {
+            return clearables.count
+        }
+        
+        assert(section == SectionButton)
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
+        guard indexPath.section == SectionButton else { return false }
+        
+        // Highlight the button only if it's enabled.
+        return clearButtonEnabled
+    }
+    
+    static func clearPrivateData(_ clearables: [Clearable], secondAttempt: Bool = false) -> Deferred<Void> {
+        let deferred = Deferred<Void>()
+        
+        clearables.enumerated().map { clearable in
+            print("Clearing \(clearable.element).")
+            
+            let res = Success()
+            succeed().upon() { _ in // move off main thread
+                clearable.element.clear().upon() { result in
+                    res.fill(result)
+                }
+            }
+            return res
+            }
+            .allSucceed()
+            .upon { result in
+                if !result.isSuccess && !secondAttempt {
+                    print("Private data NOT cleared successfully")
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                        // For some reason, a second attempt seems to always succeed
+                        clearPrivateData(clearables, secondAttempt: true).upon() { _ in
+                            deferred.fill(())
+                        }
+                    })
+                    return
+                }
+                
+                if !result.isSuccess {
+                    print("Private data NOT cleared after 2 attempts")
+                }
+                deferred.fill(())
+        }
+        return deferred
+    }
+    
+    @objc fileprivate func allWebViewsKilled() {
+        gotNotificationDeathOfAllWebViews = true
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: { // for some reason, even after all webviews killed, an big delay is needed before the filehandles are unlocked
+            var clear = [Clearable]()
+            for i in 0..<self.clearables.count {
+                if self.toggles[i] {
+                    clear.append(self.clearables[i].clearable)
+                }
+            }
+            
+            // TODO: Bring back PrivateBrowsing
+            /*
+             if PrivateBrowsing.singleton.isOn {
+             PrivateBrowsing.singleton.exit().upon {
+             ClearPrivateDataTableViewController.clearPrivateData(clear).upon {
+             DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: {
+             PrivateBrowsing.singleton.enter()
+             getApp().tabManager.addTabAndSelect()
+             })
+             }
+             }
+             } else {*/
+            ClearPrivateDataTableViewController.clearPrivateData(clear).uponQueue(DispatchQueue.main) { _ in
+                // TODO: add API to avoid add/remove
+                self.tabManager.removeTab(self.tabManager.addTab())
+            }
+            //      }
+        })
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard indexPath.section == SectionButton else { return }
+        
+        tableView.deselectRow(at: indexPath, animated: false)
+        
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let clearAction = UIAlertAction(title: Strings.ClearPrivateData, style: .destructive) { (_) in
+            self.profile.prefs.setObject(self.toggles, forKey: TogglesPrefKey)
+            self.clearButtonEnabled = false
+            
+            //      NotificationCenter.default.removeObserver(self)
+            //      NotificationCenter.default.addObserver(self, selector: #selector(self.allWebViewsKilled), name: NSNotification.Name(rawValue: kNotificationAllWebViewsDeallocated), object: nil)
+            
+            self.tabManager.removeAll()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                if !self.gotNotificationDeathOfAllWebViews {
+                    self.tabManager.tabs.forEach { $0.deleteWebView() }
+                    self.allWebViewsKilled()
+                }
             })
-          }
         }
-      } else {*/
-        ClearPrivateDataTableViewController.clearPrivateData(clear).uponQueue(DispatchQueue.main) { _ in
-          // TODO: add API to avoid add/remove
-          self.tabManager.removeTab(self.tabManager.addTab())
-        }
-//      }
-    })
-  }
-  
-  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    guard indexPath.section == SectionButton else { return }
-    
-    tableView.deselectRow(at: indexPath, animated: false)
-    
-    let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-    let clearAction = UIAlertAction(title: Strings.ClearPrivateData, style: .destructive) { (_) in
-      self.profile.prefs.setObject(self.toggles, forKey: TogglesPrefKey)
-      self.clearButtonEnabled = false
-      
-//      NotificationCenter.default.removeObserver(self)
-//      NotificationCenter.default.addObserver(self, selector: #selector(self.allWebViewsKilled), name: NSNotification.Name(rawValue: kNotificationAllWebViewsDeallocated), object: nil)
-      
-      self.tabManager.removeAll()
-      DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
-        if !self.gotNotificationDeathOfAllWebViews {
-          self.tabManager.tabs.forEach { $0.deleteWebView() }
-          self.allWebViewsKilled()
-        }
-      })
+        actionSheet.addAction(clearAction)
+        actionSheet.addAction(.init(title: Strings.Cancel, style: .cancel, handler: nil))
+        present(actionSheet, animated: true, completion: nil)
     }
-    actionSheet.addAction(clearAction)
-    actionSheet.addAction(.init(title: Strings.Cancel, style: .cancel, handler: nil))
-    present(actionSheet, animated: true, completion: nil)
-  }
-  
-  @objc func switchValueChanged(_ toggle: UISwitch) {
-    toggles[toggle.tag] = toggle.isOn
     
-    // Dim the clear button if no clearables are selected.
-    clearButtonEnabled = toggles.contains(true)
-  }
+    @objc func switchValueChanged(_ toggle: UISwitch) {
+        toggles[toggle.tag] = toggle.isOn
+        
+        // Dim the clear button if no clearables are selected.
+        clearButtonEnabled = toggles.contains(true)
+    }
 }
--- Client/Frontend/Settings/Clearables.swift
+++ Client/Frontend/Settings/Clearables.swift
@@ -10,29 +10,29 @@ import BraveShared
 
 // A base protocol for something that can be cleared.
 protocol Clearable {
-  func clear() -> Success
-  var label: String { get }
+    func clear() -> Success
+    var label: String { get }
 }
 
 class ClearableError: MaybeErrorType {
-  fileprivate let msg: String
-  init(msg: String) {
-    self.msg = msg
-  }
-  
-  var description: String { return msg }
+    fileprivate let msg: String
+    init(msg: String) {
+        self.msg = msg
+    }
+    
+    var description: String { return msg }
 }
 
 struct ClearableErrorType: MaybeErrorType {
-  let err: Error
-  
-  init(err: Error) {
-    self.err = err
-  }
-  
-  var description: String {
-    return "Couldn't clear: \(err)."
-  }
+    let err: Error
+    
+    init(err: Error) {
+        self.err = err
+    }
+    
+    var description: String {
+        return "Couldn't clear: \(err)."
+    }
 }
 
 
@@ -41,156 +41,156 @@ struct ClearableErrorType: MaybeErrorType {
 // validateClearedWithNameContains can be nil, in which case the check is skipped or pass [] as a special case to verify that
 // the directory is empty.
 private func deleteLibraryFolderContents(_ folder: String, validateClearedExceptFor:[String]?) throws {
-  let manager = FileManager.default
-  let library = manager.urls(for: FileManager.SearchPathDirectory.libraryDirectory, in: .userDomainMask)[0]
-  let dir = library.appendingPathComponent(folder)
-  var contents = try manager.contentsOfDirectory(atPath: dir.path)
-  for content in contents {
-    do {
-      try manager.removeItem(at: dir.appendingPathComponent(content))
-    } catch where ((error as NSError).userInfo[NSUnderlyingErrorKey] as? NSError)?.code == Int(EPERM) {
-      // "Not permitted". We ignore this.
-      // Snapshots directory is an example of a Cache dir that is not permitted on device (but is permitted on simulator)
+    let manager = FileManager.default
+    let library = manager.urls(for: FileManager.SearchPathDirectory.libraryDirectory, in: .userDomainMask)[0]
+    let dir = library.appendingPathComponent(folder)
+    var contents = try manager.contentsOfDirectory(atPath: dir.path)
+    for content in contents {
+        do {
+            try manager.removeItem(at: dir.appendingPathComponent(content))
+        } catch where ((error as NSError).userInfo[NSUnderlyingErrorKey] as? NSError)?.code == Int(EPERM) {
+            // "Not permitted". We ignore this.
+            // Snapshots directory is an example of a Cache dir that is not permitted on device (but is permitted on simulator)
+        }
     }
-  }
-  
-  #if DEBUG
-  guard let allowedFileNames = validateClearedExceptFor else { return }
-  contents = try manager.contentsOfDirectoryAtPath(dir.path, withFilenamePrefix: "")
-  for content in contents {
-    for name in allowedFileNames {
-      if !content.contains(name) {
-        BraveApp.showErrorAlert(title: "Error clearing data", error: "Item not cleared: \(content)")
-      }
+    
+    #if DEBUG
+    guard let allowedFileNames = validateClearedExceptFor else { return }
+    contents = try manager.contentsOfDirectoryAtPath(dir.path, withFilenamePrefix: "")
+    for content in contents {
+        for name in allowedFileNames {
+            if !content.contains(name) {
+                BraveApp.showErrorAlert(title: "Error clearing data", error: "Item not cleared: \(content)")
+            }
+        }
     }
-  }
-  #endif
+    #endif
 }
 
 private func deleteLibraryFolder(_ folder: String) throws {
-  let manager = FileManager.default
-  let library = manager.urls(for: FileManager.SearchPathDirectory.libraryDirectory, in: .userDomainMask)[0]
-  let dir = library.appendingPathComponent(folder)
-  try manager.removeItem(at: dir)
+    let manager = FileManager.default
+    let library = manager.urls(for: FileManager.SearchPathDirectory.libraryDirectory, in: .userDomainMask)[0]
+    let dir = library.appendingPathComponent(folder)
+    try manager.removeItem(at: dir)
 }
 
 // Remove all cookies stored by the site. This includes localStorage, sessionStorage, and WebSQL/IndexedDB.
 class CookiesClearable: Clearable {
-  
-  var label: String {
-    return Strings.Cookies
-  }
-  
-  func clear() -> Success {
-    UserDefaults.standard.synchronize()
     
-    let result = Deferred<Maybe<()>>()
-    // need event loop to run to autorelease UIWebViews fully
-    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-      // Now we wipe the system cookie store (for our app).
-      let storage = HTTPCookieStorage.shared
-      if let cookies = storage.cookies {
-        for cookie in cookies {
-          storage.deleteCookie(cookie)
-        }
-      }
-      UserDefaults.standard.synchronize()
-      
-      // And just to be safe, we also wipe the Cookies directory.
-      do {
-        try deleteLibraryFolderContents("Cookies", validateClearedExceptFor: [])
-      } catch {
-        return result.fill(Maybe<()>(failure: ClearableErrorType(err: error)))
-      }
-      return result.fill(Maybe<()>(success: ()))
+    var label: String {
+        return Strings.Cookies
     }
-    return result
-  }
+    
+    func clear() -> Success {
+        UserDefaults.standard.synchronize()
+        
+        let result = Deferred<Maybe<()>>()
+        // need event loop to run to autorelease UIWebViews fully
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            // Now we wipe the system cookie store (for our app).
+            let storage = HTTPCookieStorage.shared
+            if let cookies = storage.cookies {
+                for cookie in cookies {
+                    storage.deleteCookie(cookie)
+                }
+            }
+            UserDefaults.standard.synchronize()
+            
+            // And just to be safe, we also wipe the Cookies directory.
+            do {
+                try deleteLibraryFolderContents("Cookies", validateClearedExceptFor: [])
+            } catch {
+                return result.fill(Maybe<()>(failure: ClearableErrorType(err: error)))
+            }
+            return result.fill(Maybe<()>(success: ()))
+        }
+        return result
+    }
 }
 
 // Clear the web cache. Note, this has to close all open tabs in order to ensure the data
 // cached in them isn't flushed to disk.
 class CacheClearable: Clearable {
-  
-  var label: String {
-    return Strings.Cache
-  }
-  
-  func clear() -> Success {
-    let result = Deferred<Maybe<()>>()
-    // need event loop to run to autorelease UIWebViews fully
-    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-      URLCache.shared.memoryCapacity = 0;
-      URLCache.shared.diskCapacity = 0;
-      // Remove the basic cache.
-      URLCache.shared.removeAllCachedResponses()
-      
-      var err: Error?
-      for item in ["Caches", "Preferences", "Cookies", "WebKit"] {
-        do {
-          try deleteLibraryFolderContents(item, validateClearedExceptFor: ["Snapshots"])
-        } catch {
-          err = error
-        }
-      }
-      if let err = err {
-        return result.fill(Maybe<()>(failure: ClearableErrorType(err: err)))
-      }
-      
-      // Clear image cache
-      ImageCache.shared.clear()
-      
-      // Leave the cache off in the error cases above
-      URLCache.shared.setupBraveDefaults()
-      result.fill(Maybe<()>(success: ()))
+    
+    var label: String {
+        return Strings.Cache
     }
     
-    return result
-  }
+    func clear() -> Success {
+        let result = Deferred<Maybe<()>>()
+        // need event loop to run to autorelease UIWebViews fully
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            URLCache.shared.memoryCapacity = 0;
+            URLCache.shared.diskCapacity = 0;
+            // Remove the basic cache.
+            URLCache.shared.removeAllCachedResponses()
+            
+            var err: Error?
+            for item in ["Caches", "Preferences", "Cookies", "WebKit"] {
+                do {
+                    try deleteLibraryFolderContents(item, validateClearedExceptFor: ["Snapshots"])
+                } catch {
+                    err = error
+                }
+            }
+            if let err = err {
+                return result.fill(Maybe<()>(failure: ClearableErrorType(err: err)))
+            }
+            
+            // Clear image cache
+            ImageCache.shared.clear()
+            
+            // Leave the cache off in the error cases above
+            URLCache.shared.setupBraveDefaults()
+            result.fill(Maybe<()>(success: ()))
+        }
+        
+        return result
+    }
 }
 
 
 // Clears our browsing history, including favicons and thumbnails.
 class HistoryClearable: Clearable {
-  init() {
-  }
-  
-  var label: String {
-    return Strings.Browsing_History
-  }
-  
-  func clear() -> Success {
-    let result = Success()
-    History.deleteAll {
-      NotificationCenter.default.post(name: .PrivateDataClearedHistory, object: nil)
-      result.fill(Maybe<()>(success: ()))
+    init() {
     }
-    return result
-  }
+    
+    var label: String {
+        return Strings.Browsing_History
+    }
+    
+    func clear() -> Success {
+        let result = Success()
+        History.deleteAll {
+            NotificationCenter.default.post(name: .PrivateDataClearedHistory, object: nil)
+            result.fill(Maybe<()>(success: ()))
+        }
+        return result
+    }
 }
 
 // Clear all stored passwords. This will clear SQLite storage and the system shared credential storage.
 class PasswordsClearable: Clearable {
-  let profile: Profile
-  init(profile: Profile) {
-    self.profile = profile
-  }
-  
-  var label: String {
-    return Strings.Saved_Logins
-  }
-  
-  func clear() -> Success {
-    // Clear our storage
-    return profile.logins.removeAll() >>== { res in
-      let storage = URLCredentialStorage.shared
-      let credentials = storage.allCredentials
-      for (space, credentials) in credentials {
-        for (_, credential) in credentials {
-          storage.remove(credential, for: space)
-        }
-      }
-      return succeed()
+    let profile: Profile
+    init(profile: Profile) {
+        self.profile = profile
     }
-  }
+    
+    var label: String {
+        return Strings.Saved_Logins
+    }
+    
+    func clear() -> Success {
+        // Clear our storage
+        return profile.logins.removeAll() >>== { res in
+            let storage = URLCredentialStorage.shared
+            let credentials = storage.allCredentials
+            for (space, credentials) in credentials {
+                for (_, credential) in credentials {
+                    storage.remove(credential, for: space)
+                }
+            }
+            return succeed()
+        }
+    }
 }
