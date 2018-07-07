5128710d97d25faca7728ae8c643b2a8c6de884f
Simon Hong <shong@igalia.com>
2018-07-07 13:59:53 +0900

    Use override method for ChromeBrowserMainExtraPartsView
    With this, we don't need to subclass ChromeBrowserMainExtraPartsView and
    touch ChromeContentBrowserClient.

--- browser/ui/views/brave_views_delegate_linux_browsertest.cc
+++ browser/ui/views/brave_views_delegate_linux_browsertest.cc
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "brave/browser/ui/views/brave_views_delegate_linux.h"
 #include "chrome/grit/chrome_unscaled_resources.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "ui/base/resource/resource_bundle.h"
