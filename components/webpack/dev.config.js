a489016a2b80faaa6e8b3e06b55fc59229cc3097
NejcZdovc <nejc.zdovc@3zsistemi.si>
2018-07-14 10:01:20 +0200

    Adds tslint, travis
    Adds ts-loader; Removes standard

--- components/webpack/dev.config.js
+++ components/webpack/dev.config.js
@@ -26,7 +26,7 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader',
+        loader: 'ts-loader'
       },
       {
         test: /\.js$/,
