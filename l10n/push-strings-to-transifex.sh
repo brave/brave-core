961335bf511b90fadb28c434e0e06ecfc8f5ff74
Terry Mancey <terrym@Terrys-MacBook-Pro-15.local>
2018-07-14 12:41:18 +0100

    Cleaned up scripts for pulling Transifex translations into Xcode project and pushing strings to Transifex

--- l10n/push-strings-to-transifex.sh
+++ l10n/push-strings-to-transifex.sh
@@ -21,23 +21,23 @@ cleanup()
 {
   echo "Cleaning up temporary files..."
 
-  if [ -e transifex.log ]; then
+  if [ -e transifex.log ] ; then
     cat transifex.log >> output.log
     rm transifex.log
   fi
 
-  if [ -e en.xliff ]; then
+  if [ -e en.xliff ] ; then
     rm en.xliff
   fi
 }
 
-if [ "${USERNAME}" = "" ]; then
+if [ "${USERNAME}" = "" ] ; then
   report_error 1 "USERNAME environment variable must be set to \"api\" or your Transifex username"
 fi
 
-if [ "${PASSWORD}" = "" ]; then
+if [ "${PASSWORD}" = "" ] ; then
   if [[ $(tr "[:upper:]" "[:lower:]" <<<"$USERNAME") = \
-  $(tr "[:upper:]" "[:lower:]" <<<"api") ]]; then
+  $(tr "[:upper:]" "[:lower:]" <<<"api") ]] ; then
     report_error 2 "PASSWORD environment variable must be set to your Transifex API token"
   else
     report_error 2 "PASSWORD environment variable must be set to your Transifex password"
@@ -48,23 +48,23 @@ cd $(dirname "$0")
 
 echo "Exporting strings from Xcode project..."
 (cd ../ && xcodebuild -exportLocalizations) &> output.log
-if [ $? != 0 ]; then
+if [ $? != 0 ] ; then
   report_error 3 "ERROR: Failed to export strings from Xcode project, please see output.log"
 fi
 
 mv -f ../Client/en.xliff . >>output.log 2>&1
-if [ $? != 0 ]; then
+if [ $? != 0 ] ; then
   report_error 3 "ERROR: Failed to export strings from Xcode project, please see output.log"
 fi
 
 echo "Cleaning up strings..."
 ./xliff-cleanup.py en.xliff >>output.log 2>&1
-if [ $? != 0 ]; then
+if [ $? != 0 ] ; then
   report_error 4 "ERROR: Failed to cleanup strings, please see output.log"
 fi
 
 sed -i '' 's/Shared\/Supporting Files/brave/' en.xliff >>output.log 2>&1
-if [ $? != 0 ]; then
+if [ $? != 0 ] ; then
   report_error 4 "ERROR: Failed to cleanup strings, please see output.log"
 fi
 
@@ -72,7 +72,7 @@ echo "Pushing string changes to Transifex..."
 curl -# -L -o transifex.log --user ${USERNAME}:${PASSWORD} -F file=@en.xliff -X PUT \
 https://www.transifex.com/api/2/project/brave-ios/resource/bravexliff/content/
 
-if ! grep -q "strings_" transifex.log; then
+if ! grep -q "strings_" transifex.log ; then
   report_error 5 "ERROR: Failed to push string changes to Transifex, please see output.log"
 fi
 
