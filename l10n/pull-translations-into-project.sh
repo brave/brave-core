79586382acdbbe876bb58f5a9bfa4bb3e3bcc53a
Terry Mancey <terrym@Terrys-MacBook-Pro-15.local>
2018-07-14 12:41:18 +0100

    Cleaned up scripts for pulling Transifex translations into Xcode project and pushing strings to Transifex

--- l10n/pull-translations-into-project.sh
+++ l10n/pull-translations-into-project.sh
@@ -21,18 +21,18 @@ cleanup()
 {
   echo "Cleaning up temporary files..."
 
-  if [ -d translated-xliffs ]; then
-	rm -Rf translated-xliffs
+  if [ -d translated-xliffs ] ; then
+    rm -Rf translated-xliffs
   fi
 }
 
-if [ "${USERNAME}" = "" ]; then
+if [ "${USERNAME}" = "" ] ; then
   report_error 1 "USERNAME environment variable must be set to \"api\" or your Transifex username"
 fi
 
-if [ "${PASSWORD}" = "" ]; then
+if [ "${PASSWORD}" = "" ] ; then
   if [[ $(tr "[:upper:]" "[:lower:]" <<<"$USERNAME") = \
-  $(tr "[:upper:]" "[:lower:]" <<<"api") ]]; then
+  $(tr "[:upper:]" "[:lower:]" <<<"api") ]] ; then
     report_error 2 "PASSWORD environment variable must be set to your Transifex API token"
   else
     report_error 2 "PASSWORD environment variable must be set to your Transifex password"
@@ -41,24 +41,24 @@ fi
 
 cd $(dirname "$0")
 
-if [ ! -d "translated-xliffs" ]; then
+if [ ! -d "translated-xliffs" ] ; then
   mkdir "translated-xliffs" &> output.log
-  if [ $? != 0 ]; then
-  	report_error 3 "ERROR: Failed to create \"translated-xliffs directory\", please see output.log"
+  if [ $? != 0 ] ; then
+    report_error 3 "ERROR: Failed to create \"translated-xliffs directory\", please see output.log"
   fi
 fi
 
 echo "Downloading translations from Transifex..."
 USERNAME="${USERNAME}" PASSWORD="${PASSWORD}" node downloadLanguages.js >>output.log 2>&1
-if [ $? != 0 ]; then
+if [ $? != 0 ] ; then
   report_error 4 "ERROR: Failed to download translations from Transifex, please see output.log"
 fi
 
 echo "Importing translations into Xcode project..."
-for path in translated-xliffs/*.xliff; do
+for path in translated-xliffs/*.xliff ; do
   (cd ../ && xcodebuild -importLocalizations -localizationPath "l10n/$path") >>output.log 2>&1
 done
-if [ $? != 0 ]; then
+if [ $? != 0 ] ; then
   report_error 5 "ERROR: Failed to import translations into Xcode project, please see output.log"
 fi
 
