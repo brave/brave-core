2fe9343fbdc1ea7a5c878255f1570b23eb398cf7
Brian R. Bondy <netzen@gmail.com>
2018-07-17 00:57:27 -0400

    Pass service_manifest_data_pack_ to BraveContentBrowserClient

--- app/brave_main_delegate.cc
+++ app/brave_main_delegate.cc
@@ -46,7 +46,13 @@ BraveMainDelegate::CreateContentBrowserClient() {
 #if defined(CHROME_MULTIPLE_DLL_CHILD)
   return NULL;
 #else
-  return g_brave_content_browser_client.Pointer();
+  if (chrome_content_browser_client_ == nullptr) {
+    DCHECK(service_manifest_data_pack_);
+    chrome_content_browser_client_ =
+        std::make_unique<BraveContentBrowserClient>(
+            std::move(service_manifest_data_pack_));
+  }
+  return chrome_content_browser_client_.get();
 #endif
 }
 
