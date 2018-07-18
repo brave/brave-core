2c5261214887042433901c6faca31eced7389228
Cezar Augusto <boss@cezaraugusto.net>
2018-07-18 18:01:24 -0300

    add rewards combine reducers test

---
+++ components/test/brave_rewards_ui/reducers/index_test.ts
@@ -0,0 +1,22 @@
+/* global describe, it */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import reducers from '../../../brave_rewards_ui/reducers/index'
+import { rewardsInitialState } from '../../testData'
+
+describe('rewards reducers test', () => {
+  it('reducers are a combined reducer function', () => {
+    expect(typeof reducers).toBe('function')
+  })
+
+  it('reducers passed with an unknown action returns the same input', () => {
+    const unknownAction = {
+      type: 'MR. BONDY IS THE REAL JON SNOW'
+    }
+
+    const state = { ...rewardsInitialState }
+    expect(reducers(state, unknownAction)).toBe(state)
+  })
+})
