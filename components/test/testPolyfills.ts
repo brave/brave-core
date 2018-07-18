5c8e45694a9e0246430ae6ac51a5ed60dd91949e
Cezar Augusto <boss@cezaraugusto.net>
2018-07-17 23:20:36 -0300

    make basic setup typescript compilant

--- components/test/testPolyfills.ts
+++ components/test/testPolyfills.ts
@@ -2,15 +2,19 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { getMockChrome } from './testData'
+declare namespace NodeJS {
+  interface Global {
+    requestAnimationFrame: any
+  }
+}
 
-global.window = {}
-window.localStorage = {
+(global as any).window = {} as any
+(window as any).localStorage = {
   getItem: jest.fn()
 }
 
 // This mocks rAF to avoid React console.error
 // while running Jest tests
-global.requestAnimationFrame = function (cb) {
+global.requestAnimationFrame = function (cb: () => void) {
   return setTimeout(cb, 0)
 }
--- components/test/testSetup.ts
+++ components/test/testSetup.ts
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const Enzyme = require('enzyme')
-const EnzymeAdapter = require('enzyme-adapter-react-16')
+import * as Enzyme from 'enzyme'
+import * as EnzymeAdapter from 'enzyme-adapter-react-16'
 
 // Setup enzyme's react adapter
 Enzyme.configure({ adapter: new EnzymeAdapter() })
