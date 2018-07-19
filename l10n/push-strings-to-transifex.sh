211b89c43d962f6754b6cb6e44eeee7008cc8f04
Terry Mancey <terrym@Terrys-MBP-15.home>
2018-07-19 12:03:55 +0100

    Improve error handling

--- l10n/push-strings-to-transifex.sh
+++ l10n/push-strings-to-transifex.sh
@@ -6,23 +6,24 @@
 #   0 = Success
 #   1 = USERNAME must be set
 #   2 = PASSWORD must be set
-#   3 = Failed to export strings from Xcode project
-#   4 = Failed to cleanup strings
-#   5 = Failed to push string changes to Transifex
+#   3 = Unauthorized access
+#   4 = Failed to export strings from Xcode project
+#   5 = Failed to cleanup strings
+#   6 = Failed to push string changes to Transifex
 
 report_error()
 {
   echo $2
+  echo $2 >>output.log 2>&1
   cleanup
   exit $1
 }
 
 cleanup()
 {
-  echo "Cleaning up temporary files..."
-
   if [ -e transifex.log ] ; then
     cat transifex.log >> output.log
+    echo >> output.log
     rm transifex.log
   fi
 
@@ -47,33 +48,39 @@ fi
 cd $(dirname "$0")
 
 echo "Exporting strings from Xcode project..."
-(cd ../ && xcodebuild -exportLocalizations) &> output.log
+(cd ../ && xcodebuild -exportLocalizations) >>output.log 2>&1
 if [ $? != 0 ] ; then
-  report_error 3 "ERROR: Failed to export strings from Xcode project, please see output.log"
+  report_error 4 "ERROR: Failed to export strings from Xcode project, please see output.log"
 fi
 
 mv -f ../Client/en.xliff . >>output.log 2>&1
 if [ $? != 0 ] ; then
-  report_error 3 "ERROR: Failed to export strings from Xcode project, please see output.log"
+  report_error 4 "ERROR: Failed to export strings from Xcode project, please see output.log"
 fi
 
 echo "Cleaning up strings..."
-./xliff-cleanup.py en.xliff >>output.log 2>&1
+./xliff-cleanup.py en.xliff
 if [ $? != 0 ] ; then
-  report_error 4 "ERROR: Failed to cleanup strings, please see output.log"
+  report_error 5 "ERROR: Failed to cleanup strings, please see output.log"
 fi
 
 sed -i '' 's/Shared\/Supporting Files/brave/' en.xliff >>output.log 2>&1
 if [ $? != 0 ] ; then
-  report_error 4 "ERROR: Failed to cleanup strings, please see output.log"
+  report_error 5 "ERROR: Failed to cleanup strings, please see output.log"
 fi
 
 echo "Pushing string changes to Transifex..."
-curl -# -L -o transifex.log --user ${USERNAME}:${PASSWORD} -F file=@en.xliff -X PUT \
-https://www.transifex.com/api/2/project/brave-ios/resource/bravexliff/content/
+http_status_code=$(curl --silent --write-out %{http_code} --list-only --output transifex.log --user ${USERNAME}:${PASSWORD} -F file=@en.xliff -X PUT \
+https://www.transifex.com/api/2/project/brave-ios/resource/bravexliff/content/)
 
-if ! grep -q "strings_" transifex.log ; then
-  report_error 5 "ERROR: Failed to push string changes to Transifex, please see output.log"
+if [ $http_status_code == 401 ] ; then
+  report_error 3 "ERROR: Unauthorized access, please see output.log"
+fi
+
+if [ $((http_status_code / 100)) != 2 ] ; then
+  report_error 6 "ERROR: Failed to push string changes to Transifex, please see output.log"
 fi
 
 cleanup
+
+echo "Successfully pushed string changes to Transifex"
