58fd0d554b28cbd36742b7a53318d7eba225f658
bridiver <github@brianjohnson.cc>
2018-07-21 18:34:53 -0700

    add publisher info persistence

--- browser/payments/payments_service_impl.h
+++ browser/payments/payments_service_impl.h
@@ -17,20 +17,26 @@
 
 namespace base {
 class SequencedTaskRunner;
-}
+}  // namespace base
 
 namespace ledger {
 class Ledger;
 class LedgerCallbackHandler;
-}
+}  // namespace ledger
+
+namespace leveldb {
+class DB;
+}  // namespace leveldb
 
 namespace net {
 class URLFetcher;
-}
+}  // namespace net
 
 class Profile;
 
 namespace payments {
+
+class PublisherInfoBackend;
 
 class PaymentsServiceImpl : public PaymentsService,
                             public ledger::LedgerClient,
@@ -44,6 +50,9 @@ class PaymentsServiceImpl : public PaymentsService,
   void Shutdown() override;
 
   void CreateWallet() override;
+  void GetContentSiteList(uint32_t start,
+                          uint32_t limit,
+     const GetContentSiteListCallback& callback) override;
 
  private:
   typedef base::Callback<void(int, const std::string&)> FetchCallback;
@@ -56,8 +65,16 @@ class PaymentsServiceImpl : public PaymentsService,
                              bool success);
   void OnPublisherStateLoaded(ledger::LedgerCallbackHandler* handler,
                               const std::string& data);
-
   void TriggerOnWalletCreated(int error_code);
+  void OnPublisherInfoSaved(ledger::PublisherInfoCallback callback,
+                            std::unique_ptr<ledger::PublisherInfo> info,
+                            bool success);
+  void OnPublisherInfoLoaded(ledger::PublisherInfoCallback callback,
+                             std::unique_ptr<ledger::PublisherInfo> info);
+  void OnPublisherInfoListLoaded(uint32_t start,
+                                 uint32_t limit,
+                                 ledger::GetPublisherInfoListCallback callback,
+                                 const ledger::PublisherInfoList& list);
 
   // ledger::LedgerClient
   std::string GenerateGUID() const override;
@@ -70,12 +87,24 @@ class PaymentsServiceImpl : public PaymentsService,
                        ledger::LedgerCallbackHandler* handler) override;
   void SavePublisherState(const std::string& publisher_state,
                           ledger::LedgerCallbackHandler* handler) override;
+
+  void SavePublisherInfo(std::unique_ptr<ledger::PublisherInfo> publisher_info,
+                         ledger::PublisherInfoCallback callback) override;
+  void LoadPublisherInfo(const ledger::PublisherInfo::id_type& publisher_id,
+                         ledger::PublisherInfoCallback callback) override;
+  void LoadPublisherInfoList(
+      uint32_t start,
+      uint32_t limit,
+      ledger::PublisherInfoFilter filter,
+      ledger::GetPublisherInfoListCallback callback) override;
+
   std::unique_ptr<ledger::LedgerURLLoader> LoadURL(const std::string& url,
       const std::vector<std::string>& headers,
       const std::string& content,
       const std::string& contentType,
       const ledger::URL_METHOD& method,
       ledger::LedgerCallbackHandler* handler) override;
+
   void RunIOTask(std::unique_ptr<ledger::LedgerTaskRunner> task) override;
   void RunTask(std::unique_ptr<ledger::LedgerTaskRunner> task) override;
 
@@ -87,6 +116,8 @@ class PaymentsServiceImpl : public PaymentsService,
   const scoped_refptr<base::SequencedTaskRunner> file_task_runner_;
   const base::FilePath ledger_state_path_;
   const base::FilePath publisher_state_path_;
+  const base::FilePath publisher_info_db_path_;
+  std::unique_ptr<PublisherInfoBackend> publisher_info_backend_;
 
   std::map<const net::URLFetcher*, FetchCallback> fetchers_;
 
