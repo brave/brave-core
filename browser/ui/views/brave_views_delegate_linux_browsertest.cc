4957da9c636e6c5e1c28204231a37cf87493beb9
Simon Hong <shong@igalia.com>
2018-07-23 12:15:57 +0900

    Enable GetDefaultWindowIconTest only in official(release) build
    Set views delegate to our BraveViewsDelegateLinux by explicitly initialize.
    This test is only added in official(release) build because setting views delegate
    twice isn't allowed in debug mode.

--- browser/ui/views/brave_views_delegate_linux_browsertest.cc
+++ browser/ui/views/brave_views_delegate_linux_browsertest.cc
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "brave/browser/ui/views/brave_views_delegate_linux.h"
 #include "chrome/grit/chrome_unscaled_resources.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -10,6 +11,14 @@
 using BraveViewsDelegateViewLinuxBrowserTest = InProcessBrowserTest;
 
 IN_PROC_BROWSER_TEST_F(BraveViewsDelegateViewLinuxBrowserTest, GetDefaultWindowIconTest) {
+  // In test, our BraveViewsDelegateLinux isn't set because ChromeViewsDelegate is set
+  // at the very early state by AccessibilityChecker(subclass of ChromeViewsDelegate)
+  // in ctor of InProcessBrowserTest. Introduced in v68.
+  // To have a test, explicitly set views delegate by initializing here.
+  // This is only effective in release mode because initializing views delegate twice isn't
+  // allowed by dcheck.
+  BraveViewsDelegateLinux brave_views_delegate;
+
   auto env = base::Environment::Create();
   views::ViewsDelegate* views_delegate = views::ViewsDelegate::GetInstance();
   auto& rb = ui::ResourceBundle::GetSharedInstance();
@@ -31,6 +40,7 @@ IN_PROC_BROWSER_TEST_F(BraveViewsDelegateViewLinuxBrowserTest, GetDefaultWindowI
   EXPECT_EQ(rb.GetImageSkiaNamed(IDR_PRODUCT_LOGO_128_NIGHTLY)->bitmap(),
             views_delegate->GetDefaultWindowIcon()->bitmap());
 #else  // OFFICIAL_BUILD
+  // TODO(simonhong): Enable this test in debug mode.
   EXPECT_EQ(rb.GetImageSkiaNamed(IDR_PRODUCT_LOGO_128_DEVELOPMENT)->bitmap(),
             views_delegate->GetDefaultWindowIcon()->bitmap());
 #endif
