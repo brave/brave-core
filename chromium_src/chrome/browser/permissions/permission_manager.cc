d6832e93082ee2047e5e58bef986923e409a9539
Anthony Tseng <darkdh@gmail.com>
2018-07-31 15:27:30 -0700

    overrides anonymous namespace function

--- chromium_src/chrome/browser/permissions/permission_manager.cc
+++ chromium_src/chrome/browser/permissions/permission_manager.cc
@@ -1,2 +1,18 @@
 #include "brave/browser/autoplay/autoplay_permission_context.h"
+#include "components/content_settings/core/common/content_settings.h"
+#include "content/public/browser/permission_type.h"
+
+using content::PermissionType;
+
+namespace {
+
+ContentSettingsType PermissionTypeToContentSetting_ChromiumImpl(PermissionType permission);
+
+ContentSettingsType PermissionTypeToContentSetting(PermissionType permission) {
+  if (permission == PermissionType::AUTOPLAY)
+    return CONTENT_SETTINGS_TYPE_AUTOPLAY;
+  return PermissionTypeToContentSetting_ChromiumImpl(permission);
+}
+}
+
 #include "../../../../../chrome/browser/permissions/permission_manager.cc"
