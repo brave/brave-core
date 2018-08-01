9569752e3d55d1f4cf6c298a82fcaf501fc7b29b
Simon Hong <shong@igalia.com>
2018-08-01 14:29:43 +0900

    Add simple test for changes in chrome_constants.cc

---
+++ chromium_src/chrome/common/chrome_constants_unittest_mac.cc
@@ -0,0 +1,15 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
++ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
++ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "chrome/common/chrome_constants.h"
+
+#include <string>
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+// Only checks prefix (Brave-Browser) because test build don't update branding.
+TEST(ChromeConstantsTest, ProductStringTest) {
+  EXPECT_EQ(std::string(chrome::kFrameworkName).substr(0, 13),
+            "Brave-Browser");
+}
