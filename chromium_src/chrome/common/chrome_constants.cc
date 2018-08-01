8dc0b858bfd6cec7d5d663c55710854cc5eee28b
Simon Hong <shong@igalia.com>
2018-08-01 16:28:30 +0900

    Add newly added constants to chrome_constants.cc

--- chromium_src/chrome/common/chrome_constants.cc
+++ chromium_src/chrome/common/chrome_constants.cc
@@ -155,6 +155,8 @@ const base::FilePath::CharType kOfflinePagePrefetchStoreDirname[] =
     FPL("Offline Pages/prefech_store");
 const base::FilePath::CharType kOfflinePageRequestQueueDirname[] =
     FPL("Offline Pages/request_queue");
+const base::FilePath::CharType kPageLoadCappingOptOutDBFilename[] =
+    FPL("page_load_capping_opt_out.db");
 const base::FilePath::CharType kPreferencesFilename[] = FPL("Preferences");
 const base::FilePath::CharType kPreviewsOptOutDBFilename[] =
     FPL("previews_opt_out.db");
