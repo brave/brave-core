07103de8c95c8fbc4e9e67633457dd2101038266
AlexeyBarabash <alexey@brave.com>
2018-08-07 18:51:57 +0300

    move js lib to extension background page; extension is packed

--- components/brave_sync/extension/background.js
+++ components/brave_sync/extension/background.js
@@ -2,11 +2,16 @@
 
 console.log("in chrome.runtime.onInstalled 001");
 
+var loaded = false;
 function dynamicallyLoadScript(url) {
   var script = document.createElement("script");
   script.src = url;
   document.head.appendChild(script);
 }
+
+chrome.runtime.onStartup.addListener(function() {
+  console.log("in chrome.runtime.onStartup");
+})
 
 chrome.runtime.onInstalled.addListener(function() {
   console.log("in chrome.runtime.onInstalled 002");
@@ -18,8 +23,12 @@ chrome.runtime.onInstalled.addListener(function() {
 
   chrome.braveSync.onBrowserToBackgroundPage.addListener(function(arg1) {
     console.log("in chrome.braveSync.onBrowserToBackgroundPage browser-native initiated call", arg1);
+    if (!loaded) {
+      var sync_bundle_url = chrome.runtime.getURL('bundle.js');
+      dynamicallyLoadScript(sync_bundle_url);
+      loaded = true;
+    }
   });
-
 });
 
 console.log("in chrome.runtime.onInstalled 003");
