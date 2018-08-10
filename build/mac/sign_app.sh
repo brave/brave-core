d533f45b37f8dbbf3a0e409c9f23a98ec65916d8
Ryan Gerstenkorn <ryan_gerstenkorn@fastmail.fm>
2018-08-10 12:29:32 -0500

    provisioning profile and entitlement support on mac
    This was added upstream which ended up breaking the the signing scripts
    and caused the osx build to not start.
    
    We might be ok reverting the changes if necessary like in
    https://github.com/brave/brave-core/commit/faed762d267041bf3b4a5b8b048a0c827509554a
    but i'd prefer to not worry about what that might break down the line.
    
    Upstream signing changes:
    https://chromium.googlesource.com/chromium/src.git/+/4d80b33e7c64069cb69041c4957ea77ecf6e14e8
    
    Original more descriptive but reverted commit:
    https://chromium.googlesource.com/chromium/src.git/+/b69d8c48da838196d9655402dd739e62227762a7

--- build/mac/sign_app.sh
+++ build/mac/sign_app.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 if [[ $# -lt "4" ]]; then
-  echo "usage: $0 <source_app> <dest_app> <packaging_dir> <mac_signing_keychain> <mac_signing_identifier>"
+  echo "usage: $0 <source_app> <dest_app> <packaging_dir> <mac_signing_keychain> <mac_signing_identifier> <mac_provisioning_profile>"
   exit 1
 fi
 
@@ -12,10 +12,9 @@ DEST="${2}"
 PKG_DIR="${3}"
 MAC_SIGNING_KEYCHAIN="${4}"
 MAC_SIGNING_IDENTIFIER="${5}"
+MAC_PROVISIONING_PROFILE="${6}"
 
 app_name="$(basename $SOURCE)"
-
-set -v
 
 function check_exit() {
     return=$?;
@@ -40,4 +39,4 @@ cp -a "$SOURCE" "$DEST"
 
 "${PKG_DIR}/sign_versioned_dir.sh" "$DEST" "$MAC_SIGNING_KEYCHAIN" "$MAC_SIGNING_IDENTIFIER"
 
-"${PKG_DIR}/sign_app.sh" "$DEST" "$MAC_SIGNING_KEYCHAIN" "$MAC_SIGNING_IDENTIFIER"
+"${PKG_DIR}/sign_app.sh" "$DEST" "$MAC_SIGNING_KEYCHAIN" "$MAC_SIGNING_IDENTIFIER" "$MAC_PROVISIONING_PROFILE" "${PKG_DIR}/entitlements.plist"
