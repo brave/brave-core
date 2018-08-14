dff75a1a7c234847594c21ce50233528163ae9e1
AlexeyBarabash <alexey@brave.com>
2018-08-14 19:09:55 +0300

    another forgotten class naming fix

--- components/brave_sync/client/client_ext_impl.cc
+++ components/brave_sync/client/client_ext_impl.cc
@@ -8,15 +8,15 @@
 
 namespace brave_sync {
 
-BraveSyncClientExtImpl::BraveSyncClientExtImpl() : handler_(nullptr) {
+ClientExtImpl::ClientExtImpl() : handler_(nullptr) {
   ;
 }
 
-BraveSyncClientExtImpl::~BraveSyncClientExtImpl() {
+ClientExtImpl::~ClientExtImpl() {
   ;
 }
 
-void BraveSyncClientExtImpl::SetProfile(Profile *profile) {
+void ClientExtImpl::SetProfile(Profile *profile) {
   DCHECK(profile);
   DCHECK(!brave_sync_event_router_);
 
@@ -25,17 +25,17 @@ void BraveSyncClientExtImpl::SetProfile(Profile *profile) {
   }
 }
 
-void BraveSyncClientExtImpl::SetSyncToBrowserHandler(SyncLibToBrowserHandler *handler) {
+void ClientExtImpl::SetSyncToBrowserHandler(SyncLibToBrowserHandler *handler) {
   DCHECK(handler);
   DCHECK(!handler_);
   handler_ = handler;
 }
 
-void BraveSyncClientExtImpl::LoadClient() {
+void ClientExtImpl::LoadClient() {
   ;
 }
 
-void BraveSyncClientExtImpl::SendBrowserToSync(
+void ClientExtImpl::SendBrowserToSync(
   const std::string &message,
   const base::Value &arg1,
   const base::Value &arg2,
@@ -46,43 +46,43 @@ void BraveSyncClientExtImpl::SendBrowserToSync(
   brave_sync_event_router_->BrowserToBackgroundPageRaw(message, arg1, arg2, arg3, arg4);
 }
 
-void BraveSyncClientExtImpl::SendGotInitDataStr(const std::string &seed, const std::string &device_id, const std::string & config) {
+void ClientExtImpl::SendGotInitDataStr(const std::string &seed, const std::string &device_id, const std::string & config) {
   ;
 }
 
-void BraveSyncClientExtImpl::SendFetchSyncRecords(
+void ClientExtImpl::SendFetchSyncRecords(
   const std::vector<std::string> &category_names, const base::Time &startAt,
   const int &max_records) {
   ;
 }
 
-void BraveSyncClientExtImpl::SendFetchSyncDevices() {
+void ClientExtImpl::SendFetchSyncDevices() {
   ;
 }
 
-void BraveSyncClientExtImpl::SendResolveSyncRecords(const std::string &category_name,
+void ClientExtImpl::SendResolveSyncRecords(const std::string &category_name,
   const SyncRecordAndExistingList &records_and_existing_objects) {
   ;
 }
 
-void BraveSyncClientExtImpl::SendSyncRecords(const std::string &category_name,
+void ClientExtImpl::SendSyncRecords(const std::string &category_name,
   const RecordsList &records) {
   ;
 }
 
-void BraveSyncClientExtImpl::SendDeleteSyncUser()  {
+void ClientExtImpl::SendDeleteSyncUser()  {
   ;
 }
 
-void BraveSyncClientExtImpl::SendDeleteSyncCategory(const std::string &category_name) {
+void ClientExtImpl::SendDeleteSyncCategory(const std::string &category_name) {
   ;
 }
 
-void BraveSyncClientExtImpl::SendGetBookmarksBaseOrder(const std::string &device_id, const std::string &platform) {
+void ClientExtImpl::SendGetBookmarksBaseOrder(const std::string &device_id, const std::string &platform) {
   ;
 }
 
-void BraveSyncClientExtImpl::SendGetBookmarkOrder(const std::string &prevOrder, const std::string &nextOrder) {
+void ClientExtImpl::SendGetBookmarkOrder(const std::string &prevOrder, const std::string &nextOrder) {
   ;
 }
 
