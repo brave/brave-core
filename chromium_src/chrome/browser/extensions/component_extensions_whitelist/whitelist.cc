a482b14a3fb4ffc60bd711ea1610e49ef1aaea49
Jocelyn Liu <yrliou@gmail.com>
2018-08-22 11:25:50 -0700

    reduce patching

---
+++ chromium_src/chrome/browser/extensions/component_extensions_whitelist/whitelist.cc
@@ -0,0 +1,40 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#define IsComponentExtensionWhitelisted IsComponentExtensionWhitelisted_ChromiumImpl
+#include "../../../../../../chrome/browser/extensions/component_extensions_whitelist/whitelist.cc"
+#undef IsComponentExtensionWhitelisted
+
+#include "brave/common/extensions/extension_constants.h"
+#include "components/grit/brave_components_resources.h"
+
+namespace extensions {
+
+  bool IsComponentExtensionWhitelisted(const std::string& extension_id) {
+    const char* const kAllowed[] = {
+      brave_extension_id,
+      brave_webtorrent_extension_id
+    };
+
+    for (size_t i = 0; i < arraysize(kAllowed); ++i) {
+      if (extension_id == kAllowed[i])
+        return true;
+    }
+
+    return IsComponentExtensionWhitelisted_ChromiumImpl(extension_id);
+  }
+
+  bool IsComponentExtensionWhitelisted(int manifest_resource_id) {
+    switch (manifest_resource_id) {
+      // Please keep the list in alphabetical order.
+      case IDR_BRAVE_EXTENSON:
+      case IDR_BRAVE_WEBTORRENT:
+        return true;
+    }
+
+    return IsComponentExtensionWhitelisted_ChromiumImpl(manifest_resource_id);
+  }
+
+}  // namespace extensions
+
---
+++ chromium_src/net/url_request/url_request_context_builder.cc
@@ -0,0 +1,6 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/net/url_request/magnet_protocol_handler.h"
+#include "../../../../net/url_request/url_request_context_builder.cc"
