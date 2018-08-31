46dde47b745886986a8245e31710cd6ed9596fd5
Brian R. Bondy <netzen@gmail.com>
2018-08-31 07:07:28 -0700

    Fix linking error for ToolbarActionsModelFactory
    Fix brave/brave-browser#895

--- chromium_src/chrome/browser/ui/views/toolbar/toolbar_view.cc
+++ chromium_src/chrome/browser/ui/views/toolbar/toolbar_view.cc
@@ -2,5 +2,5 @@
 #include "brave/browser/ui/views/location_bar/brave_location_bar_view.h"
 
 #define LocationBarView BraveLocationBarView
-
 #include "../../../../../../../chrome/browser/ui/views/toolbar/toolbar_view.cc"
+#undef LocationBarView
