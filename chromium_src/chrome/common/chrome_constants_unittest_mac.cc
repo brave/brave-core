ce8def778697ec17a5b3bfa7531931aa4ddc54c7
Anthony Tseng <darkdh@gmail.com>
2018-09-13 19:22:01 -0700

    Update brading unittest changed in https://github.com/brave/brave-core/pull/437
    Auditor: @bbondy, @rossmoody

--- chromium_src/chrome/common/chrome_constants_unittest_mac.cc
+++ chromium_src/chrome/common/chrome_constants_unittest_mac.cc
@@ -8,8 +8,8 @@
 
 #include "testing/gtest/include/gtest/gtest.h"
 
-// Only checks prefix (Brave-Browser) because test build don't update branding.
+// Only checks prefix (Brave Browser) because test build don't update branding.
 TEST(ChromeConstantsTest, ProductStringTest) {
   EXPECT_EQ(std::string(chrome::kFrameworkName).substr(0, 13),
-            "Brave-Browser");
+            "Brave Browser");
 }
