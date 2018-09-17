f51f295ffdc3d3fddeb84e361006a9118db4a16c
Ross Moody <fiftyfivehis@gmail.com>
2018-09-17 14:42:47 -0700

    Imported from brave-ui: Wording Tweaks
    - generally updated terminology to more consistently refer to windows instead of tabs
    - updated Tor communication on the private window to instruct users how to access a tor protected window through the file menu
    - updated to communication to omit speaking about DDG as if it is enabled by default (because it isn't right?)

--- components/brave_new_tab_ui/stories/private/fakeLocale.ts
+++ components/brave_new_tab_ui/stories/private/fakeLocale.ts
@@ -9,19 +9,19 @@ const locale = {
   estimatedTime: 'Estimated Time Saved',
   minutes: 'minutes',
   thisIsAPrivateWindow: 'This is a Private Window',
-  thisIsAPrivateWindowWithTor: 'This is a Private Window With Tor',
-  makeThisTabMuchMorePrivateWith: 'Make this tab much more private with',
+  thisIsAPrivateWindowWithTor: 'This is a Private Window with Tor',
+  makeThisTabMuchMorePrivateWith: 'Browse the web more privately with',
   tor: 'Tor',
   beta: 'beta',
-  defaultSearchEngineDisclaimer: 'By default, DuckDuckGo is the search engine for Private Tabs with Tor.',
-  searchPreferences: 'Search Preferences...',
-  torDisclaimer: 'Tor hides your IP address from the sites you visit, and hides the sites you visit from your ISP or your employer. Tor can slow down browsing. Some sites treat anonymous users differently, or might not work at all. Some sites might even ask you to prove you’re human.',
-  learnMore: 'Learn more...',
+  defaultSearchEngineDisclaimer: 'By default, DuckDuckGo is the search engine for Private Windows with Tor.',
+  searchPreferences: 'Search preferences',
+  torDisclaimer: 'To browse with even more privacy, enable Tor protection by opening a new window with Tor protection from the file menu. This functionality is in early Beta and is very volatile. It’s only advised for advanced users.',
+  learnMore: 'Learn more',
   privateSearchWith: 'Private search with',
   duckduckgo: 'DuckDuckGo',
-  duckduckgoDisclaimer: 'With private search, Brave will use DuckDuckGo to answer your searches while you are in a private tab. DuckDuckGo is a search engine that does not track your search history, enabling you to search privately.',
-  moreAboutPrivateTabs: 'More about private tabs',
-  privateTabsDisclaimer: 'Whether or not you use Tor, private tabs are not logged in page history or counted in Brave Payments calculations. Private tabs and their cookies always vanish when the browser is closed. If you use private tabs without Tor, the sites you visit will be visible to your ISP or your employer. You won’t be anonymous, and sites will learn your public IP address. With Tor, Brave works hard to ensure that you’re extremely difficult to track online while providing a delightful browsing experience. But if your personal safety depends on remaining anonymous you may wish to use the Tor Browser from https://torproject.org/ instead.'
+  duckduckgoDisclaimer: 'DuckDuckGo is a search engine that does not track your search history, enabling you to search more privately.',
+  moreAboutPrivateTabs: 'More about private windows',
+  privateTabsDisclaimer: 'Whether or not you use Tor, private windows are not logged in page history or counted in Brave Payments calculations. Private tabs and their cookies always vanish when the browser is closed. If you use private windows without Tor, the sites you visit will be visible to your ISP or your employer. You won’t be anonymous, and sites will learn your public IP address. With Tor, Brave works hard to ensure that you’re extremely difficult to track online while providing a delightful browsing experience. But if your personal safety depends on remaining anonymous you may wish to use the Tor Browser from https://torproject.org/ instead.'
 }
 
 export default locale
