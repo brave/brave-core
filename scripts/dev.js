26c1d1c0962ed98ced1f71150fd8af3ccb5f1355
Cezar Augusto <boss@cezaraugusto.net>
2018-09-17 18:37:35 -0300

    set TARGET_GEN_DIR for dev builds fix https://github.com/brave/brave-browser/issues/1102

--- scripts/dev.js
+++ scripts/dev.js
@@ -2,18 +2,21 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+const path = require('path')
 const tasks = require('./tasks')
 const createWebpackServer = require('webpack-httpolyglot-server')
-const devConfig = require('../webpack/dev.config')
+
+process.env.TARGET_GEN_DIR = path.join(__dirname, '../dev/')
+const buildDir = process.env.TARGET_GEN_DIR
 
 tasks.replaceWebpack()
 console.log('[Copy assets]')
 console.log('-'.repeat(80))
-tasks.copyAssets('dev')
+tasks.copyAssets('dev', buildDir)
 
 console.log('[Webpack Dev]')
 console.log('-'.repeat(80))
-createWebpackServer(devConfig, {
+createWebpackServer(require('../webpack/dev.config'), {
   host: 'localhost',
   port: 3000
 })
