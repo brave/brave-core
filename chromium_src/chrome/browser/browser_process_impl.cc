c53db9d42480bc4d6b9400bc7ca7701188fadfe6
Brian R. Bondy <netzen@gmail.com>
2018-09-18 00:08:22 -0400

    Remove patch for chrome/browser/browser_process_impl.cc

---
+++ chromium_src/chrome/browser/browser_process_impl.cc
@@ -0,0 +1,9 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/components/brave_shields/browser/brave_resource_dispatcher_host_delegate.h"
+
+#define ChromeResourceDispatcherHostDelegate BraveResourceDispatcherHostDelegate
+#include "../../../../chrome/browser/browser_process_impl.cc"
+#undef ChromeResourceDispatcherHostDelegate
