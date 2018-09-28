54b3e4aaaa7231c2b7e4cb7e8ceb106003be94da
Cezar Augusto <boss@cezaraugusto.net>
2018-09-28 06:29:09 -0300

    do not use arrow function for lifecycle methods

--- components/brave_sync/ui/components/app.tsx
+++ components/brave_sync/ui/components/app.tsx
@@ -54,7 +54,7 @@ export class SyncPage extends React.PureComponent<Props, State> {
     }
   }
 
-  componentDidMount = () => {
+  componentDidMount () {
     // Inform the back-end that Sync can be loaded
     syncActions.onPageLoaded()
   }
