d49727e436869560d9572b4dd31ccbf4f45f8238
Brian Clifton <brian@clifton.me>
2018-10-01 22:30:52 -0700

    Merge pull request #543 from brave/welcomeicon
    add welcome page favicon

--- components/webpack/prod.config.js
+++ components/webpack/prod.config.js
@@ -72,7 +72,7 @@ module.exports = {
         loader: 'url-loader?limit=13000&minetype=application/font-woff'
       },
       {
-        test: /\.(ttf|eot|svg|png|jpg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        test: /\.(ttf|eot|ico|svg|png|jpg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: 'file-loader'
       }]
   },
