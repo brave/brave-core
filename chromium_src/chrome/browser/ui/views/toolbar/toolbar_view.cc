008403004717ae0adb0597ecaa84f8db76285f31
petemill <miller.pete+commit@gmail.com>
2018-10-01 22:07:17 -0700

    Toolbar Tor profile icon
    Fix https://github.com/brave/brave-browser/issues/1384

--- chromium_src/chrome/browser/ui/views/toolbar/toolbar_view.cc
+++ chromium_src/chrome/browser/ui/views/toolbar/toolbar_view.cc
@@ -1,6 +1,10 @@
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "brave/browser/ui/views/location_bar/brave_location_bar_view.h"
+#include "chrome/browser/ui/views/profiles/avatar_toolbar_button.h"
+#include "brave/browser/ui/views/profiles/brave_avatar_toolbar_button.h"
 
 #define LocationBarView BraveLocationBarView
+#define AvatarToolbarButton BraveAvatarToolbarButton
 #include "../../../../../../../chrome/browser/ui/views/toolbar/toolbar_view.cc"
 #undef LocationBarView
+#undef AvatarToolbarButton
