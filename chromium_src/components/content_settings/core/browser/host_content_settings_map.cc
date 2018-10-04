600c7d84c9fc050d25739374cb3568a19409046b
Brian R. Bondy <bbondy@gmail.com>
2018-10-04 09:01:48 -0400

    Merge pull request #558 from brave/make_plugin_contents_type_persist
    Make shields settings persist

---
+++ chromium_src/components/content_settings/core/browser/host_content_settings_map.cc
@@ -0,0 +1,12 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/components/content_settings/core/browser/brave_content_settings_ephemeral_provider.h"
+#include "brave/components/content_settings/core/browser/brave_content_settings_pref_provider.h"
+
+#define EphemeralProvider BraveEphemeralProvider
+#define PrefProvider BravePrefProvider
+#include "../../../../../../components/content_settings/core/browser/host_content_settings_map.cc"
+#undef EphemeralProvider
+#undef PrefProvider
---
+++ components/content_settings/core/browser/brave_content_settings_ephemeral_provider.cc
@@ -0,0 +1,51 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/components/content_settings/core/browser/brave_content_settings_ephemeral_provider.h"
+
+#include "brave/components/brave_shields/common/brave_shield_constants.h"
+
+namespace {
+
+bool IsShieldsResourceID(
+    const content_settings::ResourceIdentifier& resource_identifier) {
+  if (resource_identifier == brave_shields::kAds ||
+      resource_identifier == brave_shields::kTrackers ||
+      resource_identifier == brave_shields::kHTTPUpgradableResources ||
+      resource_identifier == brave_shields::kJavaScript ||
+      resource_identifier == brave_shields::kFingerprinting ||
+      resource_identifier == brave_shields::kBraveShields ||
+      resource_identifier == brave_shields::kReferrers ||
+      resource_identifier == brave_shields::kCookies) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+}
+
+namespace content_settings {
+
+bool BraveEphemeralProvider::SetWebsiteSetting(
+    const ContentSettingsPattern& primary_pattern,
+    const ContentSettingsPattern& secondary_pattern,
+    ContentSettingsType content_type,
+    const ResourceIdentifier& resource_identifier,
+    base::Value* in_value) {
+  // Prevent this handle shields configuration.
+  if (content_type == CONTENT_SETTINGS_TYPE_PLUGINS &&
+      IsShieldsResourceID(resource_identifier)) {
+    return false;
+  }
+
+  // Only flash plugin setting can be reached here.
+  DCHECK(resource_identifier.empty());
+
+  return EphemeralProvider::SetWebsiteSetting(
+      primary_pattern, secondary_pattern,
+      content_type, resource_identifier, in_value);
+}
+
+}  // namespace content_settings
