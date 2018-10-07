046d86613c5df6bb39b6bb93f81c085239287743
Jason Sadler <jsadler@brave.com>
2018-10-07 18:16:52 -0400

    Fixes brave/brave-browser#1325
    Fixed linting errors

--- components/brave_rewards/ui/components/pageWallet.tsx
+++ components/brave_rewards/ui/components/pageWallet.tsx
@@ -85,8 +85,25 @@ class PageWallet extends React.Component<Props, State> {
 
   onModalBackupOnRestore = (key: string | MouseEvent) => {
     if (typeof key === 'string' && key.length > 0) {
+      key = this.pullRecoveryKeyFromFile(key)
       this.actions.recoverWallet(key)
     }
+  }
+
+  pullRecoveryKeyFromFile = (key: string) => {
+    let recoveryKey = null
+    if (key) {
+      let messageLines = key.match(/^.+$/gm)
+      if (messageLines) {
+        let passphraseLine = '' || messageLines[2]
+        if (passphraseLine) {
+          const passphrasePattern = new RegExp(['Recovery Key:', '(.+)$'].join(' '))
+          recoveryKey = (passphraseLine.match(passphrasePattern) || [])[1]
+          return recoveryKey
+        }
+      }
+    }
+    return key
   }
 
   onModalBackupOnImport = () => {
