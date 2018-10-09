86adb7753e0aeeebf28cad455ff496370ba2ac06
Anthony Tseng <darkdh@gmail.com>
2018-10-08 20:54:34 -0700

    Use different ports tor process in different channels

--- browser/tor/tor_profile_service.cc
+++ browser/tor/tor_profile_service.cc
@@ -6,8 +6,10 @@
 
 #include "brave/browser/tor/tor_launcher_service_observer.h"
 #include "brave/common/tor/pref_names.h"
+#include "chrome/common/channel_info.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/pref_registry/pref_registry_syncable.h"
+#include "components/version_info/channel.h"
 
 
 namespace tor {
@@ -25,8 +27,28 @@ void TorProfileService::RegisterProfilePrefs(
 }
 // static
 void TorProfileService::RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
-   registry->RegisterStringPref(tor::prefs::kTorProxyString,
-                                "socks5://127.0.0.1:9050");
+  switch (chrome::GetChannel()) {
+    case version_info::Channel::STABLE:
+      registry->RegisterStringPref(tor::prefs::kTorProxyString,
+                                   "socks5://127.0.0.1:9350");
+      break;
+    case version_info::Channel::BETA:
+      registry->RegisterStringPref(tor::prefs::kTorProxyString,
+                                   "socks5://127.0.0.1:9360");
+      break;
+    case version_info::Channel::DEV:
+      registry->RegisterStringPref(tor::prefs::kTorProxyString,
+                                   "socks5://127.0.0.1:9370");
+      break;
+    case version_info::Channel::CANARY:
+      registry->RegisterStringPref(tor::prefs::kTorProxyString,
+                                   "socks5://127.0.0.1:9380");
+      break;
+    case version_info::Channel::UNKNOWN:
+    default:
+      registry->RegisterStringPref(tor::prefs::kTorProxyString,
+                                   "socks5://127.0.0.1:9390");
+  }
 }
 
 void TorProfileService::AddObserver(TorLauncherServiceObserver* observer) {
