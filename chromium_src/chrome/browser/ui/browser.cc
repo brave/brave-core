4136ab906893e641e3deb3206e5edcf50937c0e6
petemill <miller.pete+commit@gmail.com>
2018-10-08 19:47:23 -0700

    Display chrome:// urls as brave:// in the LocationBar
    This only affects the DisplayURL in that, when the url is being edited or copied, the actual chrome://*** url will be present.
    This seems acceptable given:
    - These urls 99% of the time are for display purposes only, as a result of another UI action, and will not be typed manually
    - We intend to support brave://*** actual urls, redirecting to chrome://*** actual URLs automatically, which will get displayed as brave://*** URLs (https://github.com/brave/brave-browser/issues/810)
    
    Fix https://github.com/brave/brave-browser/issues/1458

--- chromium_src/chrome/browser/ui/browser.cc
+++ chromium_src/chrome/browser/ui/browser.cc
@@ -6,9 +6,12 @@
 #include "chrome/browser/ui/browser_content_setting_bubble_model_delegate.h"
 #include "brave/browser/ui/brave_browser_content_setting_bubble_model_delegate.h"
 #include "brave/browser/ui/brave_browser_command_controller.h"
+#include "brave/components/toolbar/brave_toolbar_model_impl.h"
 
+#define ToolbarModelImpl BraveToolbarModelImpl
 #define BrowserContentSettingBubbleModelDelegate BraveBrowserContentSettingBubbleModelDelegate
 #define BrowserCommandController BraveBrowserCommandController
 #include "../../../../../chrome/browser/ui/browser.cc"
 #undef BrowserContentSettingBubbleModelDelegate
 #undef BrowserCommandController
+#undef ToolbarModelImpl
