96149dde80c581ede92e590a9322acfe876693c5
Terry Mancey <terrym@Terrys-MacBook-Pro-15.local>
2018-10-09 04:03:19 +0100

    Added Ads Client

---
+++ include/mock_ads_client.h
@@ -0,0 +1,49 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#pragma once
+
+#include <string>
+#include <map>
+
+#include "../include/ads_client.h"
+#include "../include/ads_url_loader.h"
+#include "../include/ads_impl.h"
+
+namespace ads {
+class Ads;
+class CallbackHandler;
+}  // namespace ads
+
+namespace bat_ads {
+
+class MockAdsClient : public ads::AdsClient {
+ public:
+  MockAdsClient();
+  ~MockAdsClient() override;
+
+ protected:
+  // ads::AdsClient
+  void GenerateAdReportingEvent(const std::string& eventType,
+      const std::map<std::string, std::string>& action) override;
+  void Initialize() override;
+  void SetAdsEnabled(bool enabled) override;
+  void RemoveAllHistory() override;
+  void SaveCachedInfo() override;
+  void ConfirmAdUUIDIfAdEnabled(bool enabled) override;
+  void TestShoppingData(const std::string& url) override;
+  void TestSearchState(const std::string& url) override;
+  void RecordMediaPlaying(bool active, uint64_t tabId) override;
+  void ClassifyPage(uint64_t windowId) override;
+  void ChangeLocale(const std::string& locale) override;
+  void CollectActivity() override;
+  void RetrieveSSID(uint64_t error, const std::string& ssid) override;
+  void InitializeCatalog() override;
+  void CheckReadyAdServe(uint64_t windowId, bool forceP) override;
+  void ServeSampleAd(uint64_t windowId) override;
+
+  std::unique_ptr<ads::Ads> ads_;
+};
+
+}  // namespace bat_ads
---
+++ src/mock_ads_client.cc
@@ -0,0 +1,66 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "../include/mock_ads_client.h"
+#include "../include/ads_impl.h"
+#include "../include/ads.h"
+
+namespace bat_ads {
+
+MockAdsClient::MockAdsClient() : ads_(ads::Ads::CreateInstance(this)) {
+  // TODO(Terry Mancey): Mock AdsClient (#3)
+}
+
+MockAdsClient::~MockAdsClient() = default;
+
+void MockAdsClient::GenerateAdReportingEvent(const std::string& eventType,
+      const std::map<std::string, std::string>& action) {
+}
+
+void MockAdsClient::Initialize() {
+}
+
+void MockAdsClient::SetAdsEnabled(bool enabled) {
+}
+
+void MockAdsClient::RemoveAllHistory() {
+}
+
+void MockAdsClient::SaveCachedInfo() {
+}
+
+void MockAdsClient::ConfirmAdUUIDIfAdEnabled(bool enabled) {
+}
+
+void MockAdsClient::TestShoppingData(const std::string& url) {
+}
+
+void MockAdsClient::TestSearchState(const std::string& url) {
+}
+
+void MockAdsClient::RecordMediaPlaying(bool active, uint64_t tabId) {
+}
+
+void MockAdsClient::ClassifyPage(uint64_t windowId) {
+}
+
+void MockAdsClient::ChangeLocale(const std::string& locale) {
+}
+
+void MockAdsClient::CollectActivity() {
+}
+
+void MockAdsClient::InitializeCatalog() {
+}
+
+void MockAdsClient::RetrieveSSID(uint64_t error, const std::string& ssid) {
+}
+
+void MockAdsClient::CheckReadyAdServe(uint64_t windowId, bool forceP) {
+}
+
+void MockAdsClient::ServeSampleAd(uint64_t windowId) {
+}
+
+}  // namespace bat_ads
