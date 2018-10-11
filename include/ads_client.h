brave-intl/bat-native-ads

Added ability to save and load state

include/ads_client.h
@@ -30,6 +30,10 @@ class AdsClient {
  public:
   virtual ~AdsClient() = default;
 
+  virtual void LoadAdsState(CallbackHandler* handler) = 0;
+  virtual void SaveAdsState(const std::string& ads_state,
+      CallbackHandler* handler) = 0;
+
   virtual void SaveCampaignInfo(std::unique_ptr<catalog::CampaignInfo> info,
       CampaignInfoCallback callback) = 0;
   virtual void LoadCampaignInfo(catalog::CampaignInfoFilter filter,

include/ads_impl.h
@@ -26,6 +26,8 @@ class AdsImpl : public ads::Ads, public ads::CallbackHandler {
   AdsImpl(const AdsImpl&) = delete;
   AdsImpl& operator=(const AdsImpl&) = delete;
 
+  void SaveAdsState(const std::string& json);
+
   void GenerateAdReportingEvent(const std::string& eventType,
       const std::map<std::string, std::string>& action) override;
   void Initialize() override;

include/bat_client.h
@@ -8,6 +8,7 @@
 
 #include "../include/ads_impl.h"
 #include "../include/catalog.h"
+#include "../include/state_client.h"
 
 namespace bat_ads {
 class AdsImpl;
@@ -19,6 +20,8 @@ class BatClient {
  public:
   explicit BatClient(bat_ads::AdsImpl* ads);
   ~BatClient();
+
+  bool LoadState(const std::string& json);
 
   // Called whenever Brave Ads is enabled or disabled by
   // the user, or browser restart
@@ -33,7 +36,11 @@ class BatClient {
   void ApplyCatalog(const catalog::Catalog& catalog, bool bootP);
 
  private:
+  void SaveState();
+
   bat_ads::AdsImpl* ads_;  // NOT OWNED
+
+  std::unique_ptr<CLIENT_STATE_ST> state_;
 };
 
 }  // namespace ads_bat_client

brave/brave-core

include/ads_client.h
@@ -30,6 +30,10 @@ class AdsClient {
  public:
   virtual ~AdsClient() = default;
 
+  virtual void LoadState(CallbackHandler* handler) = 0;
+  virtual void SaveState(const std::string& ads_state,
+      CallbackHandler* handler) = 0;
+
   virtual void SaveCampaignInfo(std::unique_ptr<catalog::CampaignInfo> info,
       CampaignInfoCallback callback) = 0;
   virtual void LoadCampaignInfo(catalog::CampaignInfoFilter filter,
@@ -40,16 +44,9 @@ class AdsClient {
   virtual void LoadCreativeSetInfo(catalog::CreativeSetInfoFilter filter,
       CreativeSetInfoCallback callback) = 0;
 
-  // Called to generate ad report event
-  virtual void GenerateAdReportingEvent(const std::string& eventType,
-      const std::map<std::string, std::string>& action) = 0;
-
   // Initialize
   virtual void Initialize() = 0;
 
-  // Called whenever Brave Ads is enabled or disabled by
-  // the user, or browser restart
-  virtual void SetAdsEnabled(bool enabled) = 0;
 
   // Called to remove all cached history
   virtual void RemoveAllHistory() = 0;

include/ads_impl.h
@@ -26,10 +26,7 @@ class AdsImpl : public ads::Ads, public ads::CallbackHandler {
   AdsImpl(const AdsImpl&) = delete;
   AdsImpl& operator=(const AdsImpl&) = delete;
 
-  void GenerateAdReportingEvent(const std::string& eventType,
-      const std::map<std::string, std::string>& action) override;
   void Initialize() override;
-  void SetAdsEnabled(bool enabled) override;
   void AppFocused(bool focused) override;
   void TabUpdate() override;
   void RecordUnIdle() override;
@@ -48,6 +45,8 @@ class AdsImpl : public ads::Ads, public ads::CallbackHandler {
   void ServeSampleAd(uint64_t windowId) override;
 
   void OnTimer(uint32_t timer_id) override;
+
+  void SaveState(const std::string& json);
 
   void SetCampaignInfo(std::unique_ptr<catalog::CampaignInfo> info,
       ads::CampaignInfoCallback callback) override;

include/bat_client.h
@@ -8,6 +8,7 @@
 
 #include "../include/ads_impl.h"
 #include "../include/catalog.h"
+#include "../include/state_user_model.h"
 
 namespace bat_ads {
 class AdsImpl;
@@ -20,12 +21,7 @@ class BatClient {
   explicit BatClient(bat_ads::AdsImpl* ads);
   ~BatClient();
 
-  // Called whenever Brave Ads is enabled or disabled by
-  // the user, or browser restart
-  void SetAdsEnabled(bool enabled);
-
-  // Returns true if Brave Ads is enabled, otherwise false
-  bool IsAdsEnabled() const;
+  bool LoadState(const std::string& json);
 
   // Called when the catalog server has returned a result.
   // If the result is good, an upcall is made to save the catalog state
@@ -33,7 +29,11 @@ class BatClient {
   void ApplyCatalog(const catalog::Catalog& catalog, bool bootP);
 
  private:
+  void SaveState();
+
   bat_ads::AdsImpl* ads_;  // NOT OWNED
+
+  std::unique_ptr<USER_MODEL_STATE_ST> userModelState_;
 };
 
 }  // namespace ads_bat_client