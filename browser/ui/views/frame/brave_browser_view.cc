89b8cf5d98aa786b7b4f36a3afd06d780f274d11
petemill <miller.pete+commit@gmail.com>
2018-10-10 21:17:33 -0700

    Notify NativeTheme has changed when Theme changes
    Fix https://github.com/brave/brave-browser/issues/1273
    
    Chromium does this only for Private windows, we change that here to do it for all windows since the native theme can be changed via user preference.

--- browser/ui/views/frame/brave_browser_view.cc
+++ browser/ui/views/frame/brave_browser_view.cc
@@ -5,9 +5,25 @@
 #include "brave/browser/ui/views/frame/brave_browser_view.h"
 #include "brave/browser/ui/views/toolbar/brave_toolbar_view.h"
 #include "brave/browser/ui/views/toolbar/bookmark_button.h"
+#include "ui/native_theme/native_theme.h"
+#include "ui/native_theme/native_theme_dark_aura.h"
 
 void BraveBrowserView::SetStarredState(bool is_starred) {
   BookmarkButton* button = ((BraveToolbarView *)toolbar())->bookmark_button();
   if (button)
     button->SetToggled(is_starred);
+}
+
+void BraveBrowserView::OnThemeChanged() {
+  // When the theme changes, the native theme may also change (the usage
+  // of dark or normal hinges on the browser theme), so we have to
+  // propagate both kinds of change.
+  // First, make sure we do not propagate ThemeChanged to all children again in
+  // response to the native theme change.
+  base::AutoReset<bool> reset(&handling_theme_changed_, true);
+  // Notify dark (cross-platform) and light (platform-specific) variants
+  ui::NativeThemeDarkAura::instance()->NotifyObservers();
+  ui::NativeTheme::GetInstanceForNativeUi()->NotifyObservers();
+
+  views::View::OnThemeChanged();
 }
