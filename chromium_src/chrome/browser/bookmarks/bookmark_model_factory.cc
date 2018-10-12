c879d4298f5c4dddfdda7f3b5dd04b2f0af11d29
bridiver <github@brianjohnson.cc>
2018-10-11 18:07:20 -0700

    set sync_timestamp in resolve store deleted, but not synced bookmarks in a hidden folder
    support toolbar bookmarks
    
    send updates when the favicon changes
    
    add comment for noop BookmarkAllUserNodesRemoved
    
    BookmarkNodeChildrenReordered can be safely ignored

---
+++ chromium_src/chrome/browser/bookmarks/bookmark_model_factory.cc
@@ -0,0 +1,4 @@
+#include "chrome/browser/bookmarks/chrome_bookmark_client.h"
+#include "brave/browser/bookmarks/brave_bookmark_client.h"
+#define ChromeBookmarkCliet BraveBookmarkClient
+#include "../../../../../../chrome/browser/bookmarks/bookmark_model_factory.cc"
