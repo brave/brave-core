0047345daac98464150786e5c0d579ed6ff9dfc8
Brian R. Bondy <bbondy@gmail.com>
2018-10-13 22:41:05 -0400

    Merge pull request #625 from brave/brave-url-autocomplete
    Use BraveAutocompleteSchemeClassifier in Omnibox

---
+++ chromium_src/chrome/browser/ui/views/location_bar/location_bar_view.cc
@@ -0,0 +1,5 @@
+#include "brave/browser/ui/omnibox/brave_omnibox_client.h"
+
+#define ChromeOmniboxClient BraveOmniboxClient
+#include "../../../../../../../chrome/browser/ui/views/location_bar/location_bar_view.cc"
+#undef ChromeOmniboxClient
