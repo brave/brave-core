c85a92ae85cdaadada5cd88295670948a944f1f2
Max Karolinskiy <max@brave.com>
2018-10-15 14:32:58 -0400

    Patches bookmark_utils.cc's IsAppsShortcutEnabled and ShouldShowAppsShortcutInBookmarkBar functions to always return false and as such prevent bookmarks bar from showing Apps shortcut and prevents bookmarks bar context menu from offering it as an option.
    Fixes brave/brave-browser#878
    
    Adds unit test to verify that the bookmarks bar context menu doesn't
    have the menu item to show Apps in the bookmarks bar.

---
+++ chromium_src/chrome/browser/ui/bookmarks/bookmark_utils.cc
@@ -0,0 +1,21 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#define IsAppsShortcutEnabled IsAppsShortcutEnabled_Unused
+#define ShouldShowAppsShortcutInBookmarkBar ShouldShowAppsShortcutInBookmarkBar_Unused
+#include "../../../../../chrome/browser/ui/bookmarks/bookmark_utils.cc"
+#undef IsAppsShortcutEnabled
+#undef ShouldShowAppsShortcutInBookmarkBar
+
+namespace chrome {
+
+bool IsAppsShortcutEnabled(Profile* profile) {
+  return false;
+}
+
+bool ShouldShowAppsShortcutInBookmarkBar(Profile* profile) {
+  return false;
+}
+
+} // namespace chrome
---
+++ chromium_src/chrome/browser/ui/bookmarks/brave_bookmark_context_menu_controller_unittest.cc
@@ -0,0 +1,86 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "chrome/browser/ui/bookmarks/bookmark_context_menu_controller.h"
+
+#include <stddef.h>
+
+#include <memory>
+#include <string>
+
+#include "base/values.h"
+#include "chrome/app/chrome_command_ids.h"
+#include "chrome/browser/bookmarks/bookmark_model_factory.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/bookmarks/bookmark_utils.h"
+#include "chrome/browser/ui/bookmarks/bookmark_utils_desktop.h"
+#include "chrome/test/base/testing_profile.h"
+#include "components/bookmarks/browser/bookmark_model.h"
+#include "components/bookmarks/browser/bookmark_node.h"
+#include "components/bookmarks/common/bookmark_pref_names.h"
+#include "components/bookmarks/test/bookmark_test_helpers.h"
+#include "components/sync_preferences/testing_pref_service_syncable.h"
+#include "content/public/test/test_browser_thread.h"
+#include "content/public/test/test_browser_thread_bundle.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+using bookmarks::BookmarkModel;
+using bookmarks::BookmarkNode;
+using content::BrowserThread;
+
+class BraveBookmarkContextMenuControllerTest : public testing::Test {
+ public:
+  BraveBookmarkContextMenuControllerTest() : model_(nullptr) {}
+
+  void SetUp() override {
+    TestingProfile::Builder builder;
+    profile_ = builder.Build();
+    profile_->CreateBookmarkModel(true);
+    model_ = BookmarkModelFactory::GetForBrowserContext(profile_.get());
+    bookmarks::test::WaitForBookmarkModelToLoad(model_);
+  }
+
+ protected:
+  content::TestBrowserThreadBundle thread_bundle_;
+  std::unique_ptr<TestingProfile> profile_;
+  BookmarkModel* model_;
+};
+
+TEST_F(BraveBookmarkContextMenuControllerTest,
+       DontShowAppsShortcutContextMenuInBookmarksBar) {
+  BookmarkContextMenuController controller(NULL, NULL, NULL, profile_.get(),
+                                           NULL, model_->bookmark_bar_node(),
+                                           std::vector<const BookmarkNode*>());
+
+  // Show apps command is not present by default.
+  sync_preferences::TestingPrefServiceSyncable* prefs =
+      profile_->GetTestingPrefService();
+  EXPECT_FALSE(prefs->IsManagedPreference(
+      bookmarks::prefs::kShowAppsShortcutInBookmarkBar));
+  EXPECT_EQ(controller.menu_model()->GetIndexOfCommandId(
+                IDC_BOOKMARK_BAR_SHOW_APPS_SHORTCUT),
+            -1);
+
+  // Disabling the shorcut by policy doesn't cause the command to be added.
+  prefs->SetManagedPref(bookmarks::prefs::kShowAppsShortcutInBookmarkBar,
+                        std::make_unique<base::Value>(false));
+  EXPECT_EQ(controller.menu_model()->GetIndexOfCommandId(
+                IDC_BOOKMARK_BAR_SHOW_APPS_SHORTCUT),
+            -1);
+
+  // And enabling the shortcut by policy doesn't cause the command to be added.
+  prefs->SetManagedPref(bookmarks::prefs::kShowAppsShortcutInBookmarkBar,
+                        std::make_unique<base::Value>(true));
+  EXPECT_EQ(controller.menu_model()->GetIndexOfCommandId(
+                IDC_BOOKMARK_BAR_SHOW_APPS_SHORTCUT),
+            -1);
+
+  // And enabling the shortcut by user doesn't cause the command to be added.
+  prefs->RemoveManagedPref(bookmarks::prefs::kShowAppsShortcutInBookmarkBar);
+  prefs->SetUserPref(bookmarks::prefs::kShowAppsShortcutInBookmarkBar,
+                 std::make_unique<base::Value>(true));
+  EXPECT_EQ(controller.menu_model()->GetIndexOfCommandId(
+                IDC_BOOKMARK_BAR_SHOW_APPS_SHORTCUT),
+            -1);
+}
