eb2f16edbc728e4ac6206558cdcdf2566cf76cf9
Taylor `Riastradh' Campbell <taylor@brave.com>
2018-10-16 01:18:51 +0000

    Merge branch 'riastradh-newtormenu'
    https://github.com/brave/brave-core/pull/605

---
+++ chromium_src/chrome/browser/renderer_context_menu/render_view_context_menu.h
@@ -0,0 +1,26 @@
+// Copyright 2018 The Brave Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_RENDERER_CONTEXT_MENU_RENDER_VIEW_CONTEXT_MENU_H_OVERRIDE_
+#define CHROME_BROWSER_RENDERER_CONTEXT_MENU_RENDER_VIEW_CONTEXT_MENU_H_OVERRIDE_
+
+// Get the Chromium declaration.
+#define RenderViewContextMenu RenderViewContextMenu_Chromium
+#include "../../../../../chrome/browser/renderer_context_menu/render_view_context_menu.h"
+#undef RenderViewContextMenu
+
+// Declare our own subclass with overridden methods.
+class BraveRenderViewContextMenu : public RenderViewContextMenu_Chromium {
+ public:
+  BraveRenderViewContextMenu(content::RenderFrameHost* render_frame_host,
+                             const content::ContextMenuParams& params);
+  void AppendBraveLinkItems() override;
+  bool IsCommandIdEnabled(int command_id) const override;
+  void ExecuteCommand(int id, int event_flags) override;
+};
+
+// Use our own subclass as the real RenderViewContextMenu.
+#define RenderViewContextMenu BraveRenderViewContextMenu
+
+#endif  // CHROME_BROWSER_RENDERER_CONTEXT_MENU_RENDER_VIEW_CONTEXT_MENU_H_OVERRIDE_
