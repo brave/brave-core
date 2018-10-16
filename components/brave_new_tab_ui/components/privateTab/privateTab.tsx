2a80948a1fb8f462c9cd5cfc028e8321e47f3704
Cezar Augusto <boss@cezaraugusto.net>
2018-10-16 04:29:57 -0300

    private tabs v3

---
+++ components/brave_new_tab_ui/components/privateTab/privateTab.tsx
@@ -0,0 +1,91 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import * as React from 'react'
+
+// Feature-specific components
+import {
+  Grid,
+  HeaderGrid,
+  ButtonGroup,
+  Box,
+  Content,
+  HeaderBox,
+  Title,
+  SubTitle,
+  Text,
+  PrivateImage,
+  DuckDuckGoImage,
+  TorLockImage,
+  Separator,
+  FakeButton,
+  Link
+} from 'brave-ui/features/newTab'
+
+// Components
+import { Toggle } from 'brave-ui/features/shields'
+
+// Helpers
+import { getLocale } from '../../../common/locale'
+
+// Assets
+const privateWindowImg = require('../../../img/newtab/private-window.svg')
+
+interface Props {
+  useAlternativePrivateSearchEngine: boolean
+  onChangePrivateSearchEngine: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+export default class PrivateTab extends React.PureComponent<Props, {}> {
+  render () {
+    const { useAlternativePrivateSearchEngine, onChangePrivateSearchEngine } = this.props
+    return (
+      <Grid>
+        <HeaderBox>
+          <HeaderGrid>
+            <PrivateImage src={privateWindowImg} />
+            <div>
+              <SubTitle>{getLocale('headerLabel')}</SubTitle>
+              <Title>{getLocale('headerTitle')}</Title>
+              <Text>{getLocale('headerText')} <Link href='https://support.brave.com/hc/en-us/articles/360017840332' target='_blank'>{getLocale('headerButton')}</Link>
+              </Text>
+            </div>
+          </HeaderGrid>
+        </HeaderBox>
+        <Box style={{ minHeight: '475px' }}>
+          <Content>
+            <DuckDuckGoImage />
+            <SubTitle>{getLocale('boxDdgLabel')}</SubTitle>
+            <Title>{getLocale('boxDdgTitle')}</Title>
+            <Text>{getLocale('boxDdgText')}</Text>
+          </Content>
+          <Separator />
+          <ButtonGroup>
+            <FakeButton withToggle={true}>
+              <span style={{ color: '#fff' }}>{getLocale('boxDdgButton')}</span>
+              <Toggle
+                id='duckduckgo'
+                checked={useAlternativePrivateSearchEngine}
+                onChange={onChangePrivateSearchEngine}
+              />
+            </FakeButton>
+            <Link href='https://support.brave.com/hc/en-us/articles/360018266171' target='_blank'>{getLocale('learnMore')}</Link>
+          </ButtonGroup>
+        </Box>
+        <Box>
+          <Content>
+            <TorLockImage />
+            <SubTitle>{getLocale('boxTorLabel')}</SubTitle>
+            <Title>{getLocale('boxTorTitle')}</Title>
+            <Text>{getLocale('boxTorText2')}</Text>
+          </Content>
+          <Separator />
+          <FakeButton href='https://support.brave.com/hc/en-us/articles/360018121491' target='_blank'>
+            {getLocale('boxTorButton')}
+          </FakeButton>
+        </Box>
+      </Grid>
+    )
+  }
+}
