8796750ffa0a7841ba6b74a3c201a07618baeb0a
Brian Clifton <brian@clifton.me>
2018-10-16 00:40:16 -0700

    Merge pull request #642 from brave/fix/ntp-styles
    Bring New Tab Page styles more up to date with 0.24.x

--- components/brave_new_tab_ui/components/footerInfo.tsx
+++ components/brave_new_tab_ui/components/footerInfo.tsx
@@ -3,6 +3,7 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import * as React from 'react'
+import { getLocale } from '../../common/locale'
 
 interface Props {
   backgroundImage?: NewTab.Image
@@ -19,7 +20,7 @@ export default class FooterInfo extends React.Component<Props, {}> {
             bgImage && bgImage.name
             ? <div>
               <div className='copyrightCredits'>
-                <span className='photoBy' data-l10n-id='photoBy' />
+                <span className='photoBy'>{getLocale('photoBy')} </span>
                 <a className='copyrightOwner' href={bgImage.link} rel='noopener' target='_blank'>
                   {bgImage.author}
                 </a>
@@ -30,9 +31,9 @@ export default class FooterInfo extends React.Component<Props, {}> {
           }
         </div>
         <nav className='shortcutsContainer'>
-          <a href='chrome://settings'><span className='shortcutIcon settingsIcon' data-l10n-id='preferencesPage' /></a>
-          <a href='chrome://bookmarks'><span className='shortcutIcon bookmarksIcon' data-l10n-id='bookmarksPage' /></a>
-          <a href='chrome://history'><span className='shortcutIcon historyIcon' data-l10n-id='historyPage' /></a>
+          <a href='chrome://settings'><span className='shortcutIcon settingsIcon' title={getLocale('preferencesPageTitle')} /></a>
+          <a href='chrome://bookmarks'><span className='shortcutIcon bookmarksIcon' title={getLocale('bookmarksPageTitle')} /></a>
+          <a href='chrome://history'><span className='shortcutIcon historyIcon' title={getLocale('historyPageTitle')} /></a>
         </nav>
       </footer>
     )
