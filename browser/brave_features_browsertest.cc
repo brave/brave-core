3e53f0b079e281e61f2d21cfd24738a311ab7d60
Brian R. Bondy <bbondy@gmail.com>
2018-10-17 21:57:22 -0400

    Merge pull request #673 from brave/autofill-server
    Disable communication between autofill server.

--- browser/brave_features_browsertest.cc
+++ browser/brave_features_browsertest.cc
@@ -4,11 +4,19 @@
 
 #include "base/feature_list.h"
 #include "chrome/test/base/in_process_browser_test.h"
+#include "components/autofill/core/common/autofill_features.h"
 #include "components/password_manager/core/common/password_manager_features.h"
 
 using BraveFeaturesBrowserTest = InProcessBrowserTest;
 
 IN_PROC_BROWSER_TEST_F(BraveFeaturesBrowserTest, AutoFillPasswordDefault) {
   EXPECT_TRUE(
-    base::FeatureList::IsEnabled(password_manager::features::kFillOnAccountSelect));
+    base::FeatureList::IsEnabled(
+      password_manager::features::kFillOnAccountSelect));
+}
+
+IN_PROC_BROWSER_TEST_F(BraveFeaturesBrowserTest, AutoFillServerCommunication) {
+  EXPECT_FALSE(
+    base::FeatureList::IsEnabled(
+      autofill::features::kAutofillServerCommunication));
 }
