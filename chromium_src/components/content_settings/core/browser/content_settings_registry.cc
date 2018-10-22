d2573fc358f1b97c790f540874d3ab7af0c3848c
Brian R. Bondy <bbondy@gmail.com>
2018-10-22 14:10:20 -0400

    Merge pull request #700 from brave/mpilgrim_flash_2
    Move Flash default to ContentSettingsRegistry

--- chromium_src/components/content_settings/core/browser/content_settings_registry.cc
+++ chromium_src/components/content_settings/core/browser/content_settings_registry.cc
@@ -16,6 +16,22 @@ void ContentSettingsRegistry::BraveInit() {
            ContentSettingsInfo::INHERIT_IN_INCOGNITO,
            ContentSettingsInfo::PERSISTENT);
 
+  // Change plugins default to CONTENT_SETTING_BLOCK
+  content_settings_info_.erase(CONTENT_SETTINGS_TYPE_PLUGINS);
+  website_settings_registry_->UnRegister(CONTENT_SETTINGS_TYPE_PLUGINS);
+  Register(
+      CONTENT_SETTINGS_TYPE_PLUGINS, "plugins",
+      CONTENT_SETTING_BLOCK, WebsiteSettingsInfo::SYNCABLE,
+      WhitelistedSchemes(kChromeUIScheme, kChromeDevToolsScheme),
+      ValidSettings(CONTENT_SETTING_ALLOW, CONTENT_SETTING_BLOCK,
+                    CONTENT_SETTING_ASK,
+                    CONTENT_SETTING_DETECT_IMPORTANT_CONTENT),
+      WebsiteSettingsInfo::SINGLE_ORIGIN_WITH_EMBEDDED_EXCEPTIONS_SCOPE,
+      WebsiteSettingsRegistry::DESKTOP,
+      ContentSettingsInfo::INHERIT_IF_LESS_PERMISSIVE,
+      base::FeatureList::IsEnabled(features::kEnableEphemeralFlashPermission)
+      ? ContentSettingsInfo::EPHEMERAL
+      : ContentSettingsInfo::PERSISTENT);
 }
 
 } // namespace content_settings
