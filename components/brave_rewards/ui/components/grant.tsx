43fc1a051b0629596b1694efdceb150021a19779
Emerick Rogul <erogul@brave.com>
2018-10-22 13:59:17 -0400

    Pass isWindows property to GrantCaptcha
    Without that property, captchas don't work as expected when resolution
    is scaled.

--- components/brave_rewards/ui/components/grant.tsx
+++ components/brave_rewards/ui/components/grant.tsx
@@ -84,7 +84,7 @@ class Grant extends React.Component<Props, State> {
               title={grant.status === 'wrongPosition' ? getLocale('notQuite') : getLocale('almostThere')}
               text={getLocale('proveHuman')}
             >
-              <GrantCaptcha onSolution={this.onSolution} dropBgImage={grant.captcha} hint={grant.hint} />
+              <GrantCaptcha onSolution={this.onSolution} dropBgImage={grant.captcha} hint={grant.hint} isWindows={navigator.platform === 'Win32'} />
             </GrantWrapper>
             : null
         }
