c7fc679cf839b5851de8a07d6e099ad3cae89734
Brian R. Bondy <netzen@gmail.com>
2018-10-25 22:38:12 -0400

    Temporarily remove newtab handling
    TODO for Pete

--- chromium_src/chrome/browser/ui/views/tabs/tab_strip.cc
+++
@@ -1,7 +0,0 @@
-#include "brave/browser/ui/views/tabs/brave_new_tab_button.h"
-// include brave_tab header so that patch inside tab_strip works
-#include "brave/browser/ui/views/tabs/brave_tab.h"
-
-#define NewTabButton BraveNewTabButton
-#include "../../../../../../chrome/browser/ui/views/tabs/tab_strip.cc"
-#undef NewTabButton
