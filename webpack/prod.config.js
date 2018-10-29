3b75dd4f1735bcf1acec5a71b48c26a321a0f526
Cezar Augusto <boss@cezaraugusto.net>
2018-10-29 18:53:56 -0300

    make carat for select show on prod build

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -63,11 +63,11 @@ module.exports = {
         ]
       },
       {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        test: /\.(woff(2)|svg)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: 'url-loader?limit=13000&minetype=application/font-woff'
       },
       {
-        test: /\.(ttf|eot|svg|ico|png|jpg|jpeg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        test: /\.(ttf|eot|ico|png|jpg|jpeg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: 'file-loader'
       }]
   }
