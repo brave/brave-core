brave/brave-extension

fix failing tests

test/app/actions/shieldsPanelActionsTest.ts
@@ -8,7 +8,12 @@ import * as assert from 'assert'
 import * as types from '../../../app/constants/shieldsPanelTypes'
 import * as actions from '../../../app/actions/shieldsPanelActions'
 import { ShieldDetails, BlockDetails } from '../../../app/types/actions/shieldsPanelActions'
-import { BlockOptions, BlockFPOptions, BlockCookiesOptions } from '../../../app/types/other/blockTypes'
+import {
+  BlockOptions,
+  BlockFPOptions,
+  BlockJSOptions,
+  BlockCookiesOptions
+} from '../../../app/types/other/blockTypes'
 
 describe('shieldsPanelActions', () => {
   it('shieldsPanelDataUpdated', () => {
@@ -38,8 +43,10 @@ describe('shieldsPanelActions', () => {
   })
 
   it('blockJavaScript action', () => {
-    assert.deepEqual(actions.blockJavaScript(), {
-      type: types.JAVASCRIPT_BLOCKED
+    const setting: BlockJSOptions = 'allow'
+    assert.deepEqual(actions.blockJavaScript(setting), {
+      type: types.JAVASCRIPT_BLOCKED,
+      setting
     })
   })
 

test/app/background/reducers/shieldsPanelReducerTest.ts
@@ -377,7 +377,8 @@ describe('braveShieldsPanelReducer', () => {
     it('should call setAllowJavaScript', function () {
       assert.deepEqual(
         shieldsPanelReducer(state, {
-          type: types.JAVASCRIPT_BLOCKED
+          type: types.JAVASCRIPT_BLOCKED,
+          setting: 'allow'
         }), state)
       assert.equal(this.setAllowJavaScriptSpy.withArgs(origin, 'allow').calledOnce, true)
     })

brave/brave-core

test/app/actions/shieldsPanelActionsTest.ts
@@ -8,7 +8,11 @@ import * as assert from 'assert'
 import * as types from '../../../app/constants/shieldsPanelTypes'
 import * as actions from '../../../app/actions/shieldsPanelActions'
 import { ShieldDetails, BlockDetails } from '../../../app/types/actions/shieldsPanelActions'
-import { BlockOptions, BlockFPOptions, BlockCookiesOptions } from '../../../app/types/other/blockTypes'
+import {
+  BlockOptions,
+  BlockFPOptions,
+  BlockCookiesOptions
+} from '../../../app/types/other/blockTypes'
 
 describe('shieldsPanelActions', () => {
   it('shieldsPanelDataUpdated', () => {
@@ -38,14 +42,18 @@ describe('shieldsPanelActions', () => {
   })
 
   it('httpsEverywhereToggled action', () => {
-    assert.deepEqual(actions.httpsEverywhereToggled(), {
-      type: types.HTTPS_EVERYWHERE_TOGGLED
+    const setting: BlockOptions = 'allow'
+    assert.deepEqual(actions.httpsEverywhereToggled(setting), {
+      type: types.HTTPS_EVERYWHERE_TOGGLED,
+      setting
     })
   })
 
   it('javascriptToggled action', () => {
-    assert.deepEqual(actions.javascriptToggled(), {
-      type: types.JAVASCRIPT_TOGGLED
+    const setting: BlockOptions = 'allow'
+    assert.deepEqual(actions.blockJavaScript(setting), {
+      type: types.JAVASCRIPT_TOGGLED,
+      setting
     })
   })
 

test/app/background/reducers/shieldsPanelReducerTest.ts
@@ -378,7 +378,8 @@ describe('braveShieldsPanelReducer', () => {
     it('should call setAllowHTTPUpgradableResources', function () {
       assert.deepEqual(
         shieldsPanelReducer(state, {
-          type: types.HTTPS_EVERYWHERE_TOGGLED
+          type: types.HTTPS_EVERYWHERE_TOGGLED,
+          setting: 'block'
         }), state)
       assert.equal(this.setAllowHTTPUpgradableResourcesSpy.withArgs(origin, 'allow').calledOnce, true)
     })
@@ -396,7 +397,8 @@ describe('braveShieldsPanelReducer', () => {
     it('should call setAllowJavaScript', function () {
       assert.deepEqual(
         shieldsPanelReducer(state, {
-          type: types.JAVASCRIPT_TOGGLED
+          type: types.JAVASCRIPT_TOGGLED,
+          setting: 'allow'
         }), state)
       assert.equal(this.setAllowJavaScriptSpy.withArgs(origin, 'allow').calledOnce, true)
     })