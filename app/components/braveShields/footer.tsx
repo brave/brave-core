brave/brave-extension

add footer test

app/components/braveShields/footer.tsx
@@ -16,7 +16,7 @@ export default class ShieldsFooter extends React.PureComponent<{}, {}> {
 
   render () {
     return (
-      <Link href='#' onClick={this.openSettings}>
+      <Link id='braveShieldsFooter' href='#' onClick={this.openSettings}>
         <span>{getMessage('editDefaults')}</span>
         <LinkIcon />
       </Link>

brave/brave-core

app/components/braveShields/footer.tsx
@@ -19,7 +19,7 @@ export default class ShieldsFooter extends React.PureComponent<{}, {}> {
   }
   render () {
     return (
-      <MainFooterLinkFlex href='#' onClick={this.openSettings}>
+      <MainFooterLinkFlex id='braveShieldsFooter' href='#' onClick={this.openSettings}>
         <span>{getMessage('editDefaults')}</span>
         <LinkIcon />
       </MainFooterLinkFlex>