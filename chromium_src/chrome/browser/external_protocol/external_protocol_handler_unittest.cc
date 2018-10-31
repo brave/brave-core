ebbd576d1c5f725624aa9b418662d32dd1e58eca
Pranjal <pranjal@brave.com>
2018-10-31 07:28:22 -0700

    Issue 542: Request user confirmation before opening all external applications.
    auditor: @yrliou, @diracdeltas

---
+++ chromium_src/chrome/browser/external_protocol/external_protocol_handler_unittest.cc
@@ -0,0 +1,26 @@
+#include "chrome/browser/external_protocol/external_protocol_handler.h"
+
+#include "chrome/test/base/testing_browser_process.h"
+#include "chrome/test/base/testing_profile.h"
+#include "content/public/test/test_browser_thread_bundle.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+class ExternalProtocolHandlerTest : public testing::Test {
+ protected:
+  void SetUp() override {
+    profile_.reset(new TestingProfile());
+  }
+
+  void TearDown() override {
+    TestingBrowserProcess::GetGlobal()->SetLocalState(nullptr);
+  }
+
+  content::TestBrowserThreadBundle test_browser_thread_bundle_;
+  std::unique_ptr<TestingProfile> profile_;
+};
+
+TEST_F(ExternalProtocolHandlerTest, TestGetBlockStateMailto) {
+  ExternalProtocolHandler::BlockState block_state =
+      ExternalProtocolHandler::GetBlockState("mailto", profile_.get());
+  EXPECT_EQ(ExternalProtocolHandler::UNKNOWN, block_state);
+}
