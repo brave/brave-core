32219b18e60c28909f9d7495060432ab8d0eb827
Brian R. Bondy <bbondy@gmail.com>
2018-11-02 10:59:41 -0400

    Merge pull request #787 from brave/brave_behaviors
    Make brave's ${branding_path_component}_behaviors.cc file

---
+++ chromium_src/chrome/installer/setup/brave_behaviors.cc
@@ -0,0 +1,19 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "chrome/installer/setup/brand_behaviors.h"
+
+#define DoPostUninstallOperations DoPostUninstallOperations_UNUSED
+#include "../../../../../chrome/installer/setup/google_chrome_behaviors.cc"
+#undef DoPostUninstallOperations
+
+namespace installer {
+
+void DoPostUninstallOperations(const base::Version& version,
+                               const base::FilePath& local_data_path,
+                               const base::string16& distribution_data) {
+  // Brave browser doesn't launch uninstall survey page.
+}
+
+}
