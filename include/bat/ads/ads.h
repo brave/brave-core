3dffeb6eab77ae530865628a7c5a6550dc8fe613
Terry Mancey <terrym@Terrys-MacBook-Pro-15.local>
2018-11-05 14:13:08 +0000

    Improved public facing API documentation

--- include/bat/ads/ads.h
+++ include/bat/ads/ads.h
@@ -28,6 +28,7 @@ class ADS_EXPORT Ads {
 
   static Ads* CreateInstance(AdsClient* ads_client);
 
+  // Should be called when a notification has been shown
   virtual void GenerateAdReportingNotificationShownEvent(
       const NotificationShownInfo& info) = 0;
 
@@ -36,111 +37,114 @@ class ADS_EXPORT Ads {
   virtual void GenerateAdReportingNotificationResultEvent(
       const NotificationResultInfo& info) = 0;
 
+  // Should be called when a notification has been viewed for an extended period
+  // without interruption
   virtual void GenerateAdReportingSustainEvent(
       const SustainInfo& info) = 0;
 
-  // Initialize
+  // Should be called to initialize ads
   virtual void Initialize() = 0;
 
-  // Initialize user model
+  // Should be called to initialize the user model
   virtual void InitializeUserModel(const std::string& json) = 0;
 
-  // Called whenever the browser gains or loses focus (the active application)
+  // Should be called whenever the browser gains or loses focus
   virtual void AppFocused(const bool is_focused) = 0;
 
-  // Called to record user activity on a tab
+  // Should be called to record user activity on a tab
   virtual void TabUpdated(
       const std::string& tab_id,
       const std::string& url,
       const bool is_active,
       const bool is_incognito) = 0;
 
-  // Called to record when a user switches tab
+  // Should be called to record when a user switches tab
   virtual void TabSwitched(
       const std::string& tab_id,
       const std::string& url,
       const bool is_incognito) = 0;
 
-  // Called to record when a user closes a tab
+  // Should be called to record when a user closes a tab
   virtual void TabClosed(const std::string& tab_id) = 0;
 
-  // Called to record when a user is no longer idle
+  // Should be called to record when a user is no longer idle
   virtual void RecordUnIdle() = 0;
 
-  // Called to remove all cached history
+  // Should be called to remove all cached history
   virtual void RemoveAllHistory() = 0;
 
-  // Called when the browser is about to exit, if Brave Ads
-  // is not enabled, then removes all client state
+  // Should be called when the browser is about to exit, if ads is not enabled,
+  // then removes all client state
   virtual void SaveCachedInfo() = 0;
 
-  // Called to record whenever a tab is playing (or has
-  // stopped playing) media (A/V)
+  // Should be called to record whenever a tab is playing (or has stopped
+  // playing) media (A/V)
   virtual void RecordMediaPlaying(
       const std::string& tab_id,
       const bool is_playing) = 0;
 
-  // Called when a page is completely loaded and both the headers
-  // and body are available for analysis
+  // Should be called when a page is completely loaded and the body is available
+  // for analysis
   virtual void ClassifyPage(
       const std::string& url,
       const std::string& html) = 0;
 
-  // Called when the user changes their locale (e.g., "en", "fr", or "gb")
+  // Should be called when the user changes their locale (e.g., "en_US", "fr",
+  // or "gb")
   virtual void ChangeLocale(const std::string& locale) = 0;
 
-
-  // Frequently called to determine whether a notification should be
-  // displayed; if so, the notification is sent
+  // Frequently called to determine whether a notification should be displayed;
+  // if so, the notification is sent
   virtual void CheckReadyAdServe(const bool forced = false) = 0;
 
-  // Called when the user invokes "Show Sample Ad"
+  // Should be called when the user invokes "Show Sample Ad"
   virtual void ServeSampleAd() = 0;
 
-  // Called to flag whether notifications are available
+  // Should be called to flag whether notifications are available
   virtual void SetNotificationsAvailable(const bool available) = 0;
 
-  // Called to flag whether notifications are allowed
+  // Should be called to flag whether notifications are allowed
   virtual void SetNotificationsAllowed(const bool allowed) = 0;
 
-  // Called to flag whether notifications are configured
+  // Should be called to flag whether notifications are configured
   virtual void SetNotificationsConfigured(const bool configured) = 0;
 
-  // Called to flag whether notifications have expired
+  // Should be called to flag whether notifications have expired
   virtual void SetNotificationsExpired(const bool expired) = 0;
 
-  // Called when a timer is triggered
+  // Should be called when a timer is triggered
   virtual void OnTimer(const uint32_t timer_id) = 0;
 
-  // Called once the user model has loaded
+  // Should be called once the user model has loaded
   virtual void OnUserModelLoaded(const Result result) = 0;
 
-  // Called once settings have loaded
+  // Should be called once settings have loaded
   virtual void OnSettingsLoaded(
       const Result result,
       const std::string& json) = 0;
 
-  // Called once client has been saved
+  // Should be called once the client has been saved
   virtual void OnClientSaved(const Result result) = 0;
 
-  // Called once client has loaded
+  // Should be called once the client has loaded
   virtual void OnClientLoaded(
       const Result result,
       const std::string& json) = 0;
 
-  // Called once bundle has been saved
+  // Should be called once the bundle has been saved
   virtual void OnBundleSaved(const Result result) = 0;
 
-  // Called once bundle has loaded
+  // Should be called once the bundle has loaded
   virtual void OnBundleLoaded(
       const Result result,
       const std::string& json) = 0;
 
+  // Should be Called after getting a sample category
   virtual void OnGetSampleCategory(
       const Result result,
       const std::string& category) = 0;
 
-  // Called after getting ads
+  // Should be called after getting ads
   virtual void OnGetAds(
       const Result result,
       const std::string& category,
