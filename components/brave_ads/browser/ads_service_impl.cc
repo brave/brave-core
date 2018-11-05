9d93630694a037ecbefdcfcc8f20e1c4a787f894
bridiver <github@brianjohnson.cc>
2018-11-05 16:21:37 -0700

    ads service stub

---
+++ components/brave_ads/browser/ads_service_impl.cc
@@ -0,0 +1,137 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/components/brave_ads/browser/ads_service_impl.h"
+
+#include "base/files/file_util.h"
+#include "base/guid.h"
+#include "base/sequenced_task_runner.h"
+#include "base/task_runner_util.h"
+#include "base/task/post_task.h"
+#include "bat/ads/ads.h"
+#include "bat/ads/url_session.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/common/chrome_constants.h"
+#include "ui/base/l10n/l10n_util.h"
+
+
+namespace brave_ads {
+
+namespace {
+
+std::string LoadOnFileTaskRunner(
+    const base::FilePath& path) {
+  std::string data;
+  bool success = base::ReadFileToString(path, &data);
+
+  // Make sure the file isn't empty.
+  if (!success || data.empty()) {
+    LOG(ERROR) << "Failed to read file: " << path.MaybeAsASCII();
+    return std::string();
+  }
+  return data;
+}
+
+}
+
+AdsServiceImpl::AdsServiceImpl(Profile* profile) :
+    profile_(profile),
+    file_task_runner_(base::CreateSequencedTaskRunnerWithTraits(
+          {base::MayBlock(), base::TaskPriority::BEST_EFFORT,
+           base::TaskShutdownBehavior::BLOCK_SHUTDOWN})),
+    base_path_(profile_->GetPath().AppendASCII("ads_service")) {
+  DCHECK(!profile_->IsOffTheRecord());
+
+  if (is_enabled())
+    Init();
+}
+
+AdsServiceImpl::~AdsServiceImpl() {}
+
+void AdsServiceImpl::Init() {
+  DCHECK(is_enabled());
+
+  ads_.reset(ads::Ads::CreateInstance(this));
+}
+
+bool AdsServiceImpl::is_enabled() {
+  return true;
+}
+
+void AdsServiceImpl::Save(const std::string& name,
+                          const std::string& value,
+                          ads::OnSaveCallback callback) {
+
+}
+
+void AdsServiceImpl::Load(const std::string& name,
+                          ads::OnLoadCallback callback) {
+  base::PostTaskAndReplyWithResult(file_task_runner_.get(), FROM_HERE,
+      base::BindOnce(&LoadOnFileTaskRunner, base_path_.AppendASCII(name)),
+      base::BindOnce(&AdsServiceImpl::OnLoaded,
+                     AsWeakPtr(),
+                     std::move(callback)));
+}
+
+void AdsServiceImpl::OnLoaded(
+    const ads::OnLoadCallback& callback,
+    const std::string& value) {
+  if (value.empty())
+    callback(ads::Result::FAILED, value);
+  else
+    callback(ads::Result::SUCCESS, value);
+}
+
+void AdsServiceImpl::Reset(const std::string& name,
+            ads::OnResetCallback callback) {
+
+}
+
+const ads::ClientInfo AdsServiceImpl::GetClientInfo() const {
+  // TODO(bridiver) - these eventually get used in a catalog request
+  // and seem unecessary. Seems like it would be better to get the catalog from
+  // S3 and we should actually be filtering this stuff out from the headers when
+  // making requests as well
+  ads::ClientInfo client_info;
+  // this doesn't seem necessary
+  client_info.application_version = "";
+  // client_info.application_version = chrome::kChromeVersion;
+
+  // this doesn't seem necessary
+  client_info.platform = "";
+  // client_info.platform = base::OperatingSystemName();
+
+  // this is definitely a privacy issue
+  client_info.platform_version = "";
+
+  return client_info;
+}
+
+const std::string AdsServiceImpl::GenerateUUID() const {
+  return base::GenerateGUID();
+}
+
+const std::string AdsServiceImpl::GetSSID() const {
+  return "";
+}
+
+const std::vector<std::string>& AdsServiceImpl::GetLocales() const {
+  return l10n_util::GetAvailableLocales();
+}
+
+std::string AdsServiceImpl::SetLocale(const std::string& locale) {
+  return locale;
+}
+
+std::unique_ptr<ads::URLSession> AdsServiceImpl::URLSessionTask(
+      const std::string& url,
+      const std::vector<std::string>& headers,
+      const std::string& content,
+      const std::string& content_type,
+      const ads::URLSession::Method& method,
+      ads::URLSessionCallbackHandlerCallback callback) {
+  return nullptr;
+}
+
+}  // namespace brave_ads
---
+++ components/brave_ads/browser/ads_service_impl.h
@@ -0,0 +1,89 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_BRAVE_ADS_ADS_SERVICE_IMPL_
+#define BRAVE_COMPONENTS_BRAVE_ADS_ADS_SERVICE_IMPL_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "base/files/file_path.h"
+#include "base/memory/weak_ptr.h"
+#include "bat/ads/ads_client.h"
+#include "brave/components/brave_ads/browser/ads_service.h"
+
+class Profile;
+
+namespace base {
+class SequencedTaskRunner;
+}
+
+namespace ads {
+class Ads;
+}
+
+namespace brave_ads {
+
+class AdsServiceImpl : public AdsService,
+                       public ads::AdsClient,
+                       public base::SupportsWeakPtr<AdsServiceImpl> {
+ public:
+  explicit AdsServiceImpl(Profile* profile);
+  ~AdsServiceImpl() override;
+
+  bool is_enabled() override;
+
+ private:
+  void Init();
+
+  // AdsClient implementation
+  const ads::ClientInfo GetClientInfo() const override;
+  std::string SetLocale(const std::string& locale) override;
+  const std::vector<std::string>& GetLocales() const override;
+  const std::string GenerateUUID() const override;
+  const std::string GetSSID() const override;
+  void ShowAd(const std::unique_ptr<ads::AdInfo> info) override {}
+  void SetTimer(const uint64_t time_offset, uint32_t& timer_id) override {}
+  void KillTimer(uint32_t& timer_id) override {};
+  std::unique_ptr<ads::URLSession> URLSessionTask(
+      const std::string& url,
+      const std::vector<std::string>& headers,
+      const std::string& content,
+      const std::string& content_type,
+      const ads::URLSession::Method& method,
+      ads::URLSessionCallbackHandlerCallback callback) override;
+  void Save(const std::string& name,
+            const std::string& value,
+            ads::OnSaveCallback callback) override;
+  void Load(const std::string& name,
+            ads::OnLoadCallback callback) override;
+  void Reset(const std::string& name,
+             ads::OnResetCallback callback) override;
+
+  void OnLoaded(const ads::OnLoadCallback& callback,
+                const std::string& value);
+
+  void GetAds(
+      const std::string& winning_category,
+      ads::CallbackHandler* callback_handler) override {}
+  void GetSampleCategory(ads::CallbackHandler* callback_handler) override {}
+  void GetUrlComponents(
+      const std::string& url,
+      ads::UrlComponents& components) const override {}
+  void EventLog(const std::string& json) override {}
+  void DebugLog(const ads::LogLevel log_level, const char *fmt, ...) const override {}
+
+  Profile* profile_;  // NOT OWNED
+  const scoped_refptr<base::SequencedTaskRunner> file_task_runner_;
+  const base::FilePath base_path_;
+
+  std::unique_ptr<ads::Ads> ads_;
+
+  DISALLOW_COPY_AND_ASSIGN(AdsServiceImpl);
+};
+
+}  // namespace brave_ads
+
+#endif  // BRAVE_COMPONENTS_BRAVE_ADS_ADS_SERVICE_IMPL_
