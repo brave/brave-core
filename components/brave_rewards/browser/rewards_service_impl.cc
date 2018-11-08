b5035442edf397522a35eef76f1d837c5ca61240
Nejc Zdovc <NejcZdovc@users.noreply.github.com>
2018-11-08 10:57:29 +0100

    Merge pull request #806 from brave/refactor-contribution
    Splits contribution into separate file for better organisation

--- components/brave_rewards/browser/rewards_service_impl.cc
+++ components/brave_rewards/browser/rewards_service_impl.cc
@@ -748,7 +748,7 @@ void RewardsServiceImpl::LoadPublisherInfoList(
     uint32_t start,
     uint32_t limit,
     ledger::PublisherInfoFilter filter,
-    ledger::GetPublisherInfoListCallback callback) {
+    ledger::PublisherInfoListCallback callback) {
   auto now = base::Time::Now();
   filter.month = GetPublisherMonth(now);
   filter.year = GetPublisherYear(now);
@@ -769,7 +769,7 @@ void RewardsServiceImpl::LoadCurrentPublisherInfoList(
     uint32_t start,
     uint32_t limit,
     ledger::PublisherInfoFilter filter,
-    ledger::GetPublisherInfoListCallback callback) {
+    ledger::PublisherInfoListCallback callback) {
   base::PostTaskAndReplyWithResult(file_task_runner_.get(), FROM_HERE,
       base::Bind(&LoadPublisherInfoListOnFileTaskRunner,
                     start, limit, filter,
@@ -784,7 +784,7 @@ void RewardsServiceImpl::LoadCurrentPublisherInfoList(
 void RewardsServiceImpl::OnPublisherInfoListLoaded(
     uint32_t start,
     uint32_t limit,
-    ledger::GetPublisherInfoListCallback callback,
+    ledger::PublisherInfoListCallback callback,
     const ledger::PublisherInfoList& list) {
   uint32_t next_record = 0;
   if (list.size() == limit)
@@ -1420,12 +1420,12 @@ ledger::PublisherInfoList GetRecurringDonationsOnFileTaskRunner(PublisherInfoDat
   return list;
 }
 
-void RewardsServiceImpl::OnRecurringDonationsData(const ledger::RecurringDonationCallback callback,
+void RewardsServiceImpl::OnRecurringDonationsData(const ledger::PublisherInfoListCallback callback,
                                                   const ledger::PublisherInfoList list) {
-  callback(list);
+  callback(list, 0);
 }
 
-void RewardsServiceImpl::GetRecurringDonations(ledger::RecurringDonationCallback callback) {
+void RewardsServiceImpl::GetRecurringDonations(ledger::PublisherInfoListCallback callback) {
   base::PostTaskAndReplyWithResult(file_task_runner_.get(), FROM_HERE,
       base::Bind(&GetRecurringDonationsOnFileTaskRunner,
                     publisher_info_backend_.get()),
