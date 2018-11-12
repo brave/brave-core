3af046aeafd376d7e201db93d55fdb153f3f6a81
Kyle Hickinson <kylehickinson@users.noreply.github.com>
2018-11-12 14:41:16 -0500

    Fix build issues related to Carthage + CI (brave/brave-ios#392)

--- ClientTests/TestFavicons.swift
+++ ClientTests/TestFavicons.swift
@@ -21,31 +21,6 @@ class TestFavicons: ProfileTest {
         self.waitForExpectations(timeout: 100, handler: nil)
     }
 
-    func testFaviconFetcherParse() {
-        let expectation = self.expectation(description: "Wait for Favicons to be fetched")
-
-        let profile = MockProfile()
-        // I want a site that also has an iOS app so I can get "apple-touch-icon-precomposed" icons as well
-        let url = URL(string: "https://instagram.com")
-        FaviconFetcher.getForURL(url!, profile: profile).uponQueue(.main) { result in
-            guard let favicons = result.successValue, favicons.count > 0, let url = favicons.first?.url.asURL else {
-                XCTFail("Favicons were not found.")
-                return expectation.fulfill()
-            }
-            XCTAssertEqual(favicons.count, 1, "Instagram should have a Favicon.")
-
-            WebImageCacheWithNoPrivacyProtectionManager.shared.load(from: url) { image, _, _, _, _ in
-                guard let image = image else {
-                    XCTFail("Not a valid URL provided for a favicon.")
-                    return expectation.fulfill()
-                }
-                XCTAssertNotEqual(image.size, .zero)
-                expectation.fulfill()
-            }
-        }
-        self.waitForExpectations(timeout: 3000, handler: nil)
-    }
-
     func testDefaultFavicons() {
         let icon = FaviconFetcher.getDefaultIconForURL(url: URL(string: "http://www.google.de")!)
         XCTAssertNotNil(icon)
