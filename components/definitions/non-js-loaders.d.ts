8b9d1e7ef79956864220dea0e1fadb91e9661d97
petemill <miller.pete+commit@gmail.com>
2018-11-21 16:56:52 -0800

    Update brave-ui and required type definitions

---
+++ components/definitions/non-js-loaders.d.ts
@@ -0,0 +1,29 @@
+declare module '*.svg' {
+  const url: any
+  export default url
+}
+
+declare module '*.png' {
+  const url: any
+  export default url
+}
+
+declare module '*.jpg' {
+  const url: any
+  export default url
+}
+
+declare module '*.gif' {
+  const url: any
+  export default url
+}
+
+declare module '*.woff2' {
+  const url: any
+  export default url
+}
+
+declare module '*.css' {
+  const url: any
+  export default url
+}
