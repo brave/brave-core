0fd2f81fad14f44279a31bd9e898e146c397cdfa
Danish Jafri <danishjafri88@gmail.com>
2018-11-29 23:40:52 +0530

    Fixed #480: Toggle selection were ignored to clear data (#482)

--- Client/Frontend/Settings/ClearPrivateDataTableViewController.swift
+++ Client/Frontend/Settings/ClearPrivateDataTableViewController.swift
@@ -156,7 +156,7 @@ class ClearPrivateDataTableViewController: UITableViewController {
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: { // for some reason, even after all webviews killed, an big delay is needed before the filehandles are unlocked
             var clear = [Clearable]()
-            for i in 0..<self.clearables.count where i < self.toggles.count {
+            for i in 0..<self.clearables.count where i < self.toggles.count && self.toggles[i] {
                 clear.append(self.clearables[i].clearable)
             }
             
