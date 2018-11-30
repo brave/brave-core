4ebadda7daebd86d7bff06225e19be8fb6980a76
bridiver <github@brianjohnson.cc>
2018-11-30 08:28:44 -0700

    make AttachTabHelpers compatible with android

--- browser/brave_tab_helpers.cc
+++ browser/brave_tab_helpers.cc
@@ -6,21 +6,28 @@
 
 #include "brave/components/brave_ads/browser/ads_tab_helper.h"
 #include "brave/components/brave_rewards/browser/buildflags/buildflags.h"
-#include "brave/components/brave_shields/browser/brave_shields_web_contents_observer.h"
 #include "content/public/browser/web_contents.h"
 
+#if !defined(OS_ANDROID)
+#include "brave/components/brave_shields/browser/brave_shields_web_contents_observer.h"
 #if BUILDFLAG(BRAVE_REWARDS_ENABLED)
 #include "brave/components/brave_rewards/browser/rewards_helper.h"
+#endif
+// Add tab helpers here unless they are intended for android too
 #endif
 
 namespace brave {
 
 void AttachTabHelpers(content::WebContents* web_contents) {
+#if !defined(OS_ANDROID)
   brave_shields::BraveShieldsWebContentsObserver::CreateForWebContents(
       web_contents);
 #if BUILDFLAG(BRAVE_REWARDS_ENABLED)
   brave_rewards::RewardsHelper::CreateForWebContents(web_contents);
 #endif
+  // Add tab helpers here unless they are intended for android too
+#endif
+
   brave_ads::AdsTabHelper::CreateForWebContents(web_contents);
 }
 
