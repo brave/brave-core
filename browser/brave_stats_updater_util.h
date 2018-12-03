1a412ea61254c4781810d63680eff2933e215213
Emerick Rogul <erogul@brave.com>
2018-12-03 14:58:52 -0500

    Check every 5 minutes if we need to ping the update server for today
    We were previously pinging the update server once an hour. However,
    that approach will undercount in the following scenario: a user's
    laptop sends a ping, the user closes the laptop, and the next day the
    user runs Brave for less than one hour.

---
+++ browser/brave_stats_updater_util.h
@@ -0,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_BROWSER_BRAVE_STATS_UPDATER_UTIL_H_
+#define BRAVE_BROWSER_BRAVE_STATS_UPDATER_UTIL_H_
+
+#include <string>
+
+#include "base/time/time.h"
+
+namespace brave {
+
+std::string GetDateAsYMD(const base::Time& time);
+
+}  // namespace brave
+
+#endif  // BRAVE_BROWSER_BRAVE_STATS_UPDATER_UTIL_H_
