42933cf704daa82196fab484bf976b360b5191b9
Simon Hong <shong@igalia.com>
2018-12-12 04:19:53 +0900

    Merge pull request #1046 from brave/update_recommend_dialog_mac
    Use sparkle's relaunch api for update recommend dialog

---
+++ browser/ui/views/update_recommended_message_box_mac.h
@@ -0,0 +1,24 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_BROWSER_UI_VIEWS_UPDATE_RECOMMENDED_MESSAGE_BOX_MAC_H_
+#define BRAVE_BROWSER_UI_VIEWS_UPDATE_RECOMMENDED_MESSAGE_BOX_MAC_H_
+
+#include "chrome/browser/ui/views/update_recommended_message_box.h"
+
+class UpdateRecommendedMessageBoxMac : public UpdateRecommendedMessageBox {
+ public:
+  static void Show(gfx::NativeWindow parent_window);
+
+ private:
+  UpdateRecommendedMessageBoxMac() {}
+  ~UpdateRecommendedMessageBoxMac() override {}
+
+  // UpdateRecommendedMessageBox overrides:
+  bool Accept() override;
+
+  DISALLOW_COPY_AND_ASSIGN(UpdateRecommendedMessageBoxMac);
+};
+
+#endif  // BRAVE_BROWSER_UI_VIEWS_UPDATE_RECOMMENDED_MESSAGE_BOX_MAC_H_
