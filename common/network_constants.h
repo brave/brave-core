1a1b03d91c766c2cf5d045da22881536f3167638
Brian R. Bondy <bbondy@gmail.com>
2018-12-14 21:25:00 -0500

    Merge pull request #1104 from brave/revert-997-crlset_reintroduced
    Revert "CRLsets reintroduced"

--- common/network_constants.h
+++ common/network_constants.h
@@ -8,7 +8,6 @@ extern const char kBraveReferralsHeadersPath[];
 extern const char kBraveReferralsInitPath[];
 extern const char kBraveReferralsActivityPath[];
 
-extern const char kCRXDownloadPrefix[];
 extern const char kEmptyDataURI[];
 extern const char kEmptyImageDataURI[];
 extern const char kJSDataURLPrefix[];
@@ -18,8 +17,6 @@ extern const char kGoogleTagServicesPattern[];
 extern const char kForbesPattern[];
 extern const char kForbesExtraCookies[];
 extern const char kSafeBrowsingPrefix[];
-extern const char kCRLSetPrefix1[];
-extern const char kCRLSetPrefix2[];
 extern const char kTwitterPattern[];
 extern const char kTwitterReferrer[];
 extern const char kTwitterRedirectURL[];
