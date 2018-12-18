037b6c4e00bff0b65eb8f77d4eabe102de3e7127
bridiver <github@brianjohnson.cc>
2018-12-17 17:44:10 -0700

    fix android build

--- components/brave_ads/browser/ads_service_impl.cc
+++ components/brave_ads/browser/ads_service_impl.cc
@@ -1026,7 +1026,12 @@ void AdsServiceImpl::LoadSampleBundle(
 }
 
 bool AdsServiceImpl::IsNetworkConnectionAvailable() {
+#if defined(OS_ANDROID)
+  // TODO(bridiver) - fix for android
+  return true;
+#else
   return !content::GetNetworkConnectionTracker()->IsOffline();
+#endif
 }
 
 void AdsServiceImpl::OnShow(Profile* profile,
