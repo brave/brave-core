bef6294a73bb6d9f2a205d6fc5209d38d89c691d
Kyle Hickinson <git@kylehickinson.com>
2018-12-18 14:38:14 -0500

    Fix brave/brave-ios#645: Re-add `TabMO.isPrivate ` and remove private tabs on migration

--- Client/Application/Migration.swift
+++ Client/Application/Migration.swift
@@ -99,6 +99,14 @@ extension Preferences {
         
         // Core Data
         
+        // In 1.6.6 we included private tabs in CoreData (temporarely) until the user did one of the following:
+        //  - Cleared private data
+        //  - Exited Private Mode
+        //  - The app was terminated (bug)
+        // However due to a bug, some private tabs remain in the container. Since 1.7 removes `isPrivate` from TabMO,
+        // we must dismiss any records that are private tabs during migration from Model7
+        TabMO.deleteAll(predicate: NSPredicate(format: "isPrivate == true"), save: true)
+        
         // Migrate the shield overrides
         migrateShieldOverrides()
         
