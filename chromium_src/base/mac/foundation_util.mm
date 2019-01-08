7ea0dbff4beca01ac7c94ca845e3b60058139382
Brian Johnson <github@brianjohnson.cc>
2019-01-08 10:22:38 -0700

    Merge pull request #1253 from brave/issues/2794
    add support for development signing

--- chromium_src/base/mac/foundation_util.mm
+++ chromium_src/base/mac/foundation_util.mm
@@ -37,7 +37,7 @@ const char* BaseBundleID() {
   }
 
 #if !defined(OFFICIAL_BUILD)
-  return "com.brave.Browser";
+  return "com.brave.Browser.development";
 #else
   switch (chrome::GetChannel()) {
     case version_info::Channel::CANARY:
