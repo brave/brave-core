68b08522eb669ec6b780980169bd6fcdc497336d
Ivan Efremov <iefremov@igalia.com>
2019-01-18 01:24:49 +0700

    Issue 2554: Replace data urls with HTTP 200 OK in adblocker.
    To better maintain web compatibility we want to get rid of returning data
    urls with emtpy content, because they cause CORS errors and return 200 OK
    instead. This cannot be done using the current NetworkDelegate infra, so
    the URLRequestInterceptor is utilized, allowing us to easily block unwanted
    requests.
    
    Fix https://github.com/brave/a-browser/issues/2554

---
+++ content/browser/adblock_interceptor.cc
@@ -0,0 +1,95 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/content/browser/adblock_interceptor.h"
+
+#include "base/bind.h"
+#include "base/compiler_specific.h"
+#include "base/location.h"
+#include "base/single_thread_task_runner.h"
+#include "base/threading/thread_task_runner_handle.h"
+#include "base/memory/weak_ptr.h"
+#include "net/url_request/url_request_job.h"
+#include "net/http/http_response_info.h"
+#include "net/http/http_response_headers.h"
+#include "net/http/http_util.h"
+
+namespace brave {
+namespace content {
+
+class Http200OkJob : public net::URLRequestJob {
+ public:
+  Http200OkJob(net::URLRequest* request,
+               net::NetworkDelegate* network_delegate);
+
+  // URLRequestJob:
+  void Start() override;
+  void Kill() override;
+  bool GetMimeType(std::string* mime_type) const override;
+  void GetResponseInfo(net::HttpResponseInfo* info) override;
+
+ private:
+  ~Http200OkJob() override;
+  void StartAsync();
+
+  base::WeakPtrFactory<Http200OkJob> weak_factory_;
+};
+
+Http200OkJob::Http200OkJob(net::URLRequest* request,
+                           net::NetworkDelegate* network_delegate)
+    : net::URLRequestJob(request, network_delegate), weak_factory_(this) {}
+
+void Http200OkJob::Start()  {
+  // Start reading asynchronously so that all error reporting and data
+  // callbacks happen as they would for network requests.
+  base::ThreadTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE, base::BindOnce(&Http200OkJob::StartAsync,
+                                weak_factory_.GetWeakPtr()));
+}
+
+void Http200OkJob::Kill() {
+  weak_factory_.InvalidateWeakPtrs();
+  URLRequestJob::Kill();
+}
+
+bool Http200OkJob::GetMimeType(std::string* mime_type) const {
+  *mime_type = "text/html";
+  return true;
+}
+
+void Http200OkJob::GetResponseInfo(net::HttpResponseInfo* info) {
+  net::HttpResponseInfo new_info;
+  // TODO(iefremov): Allowing any origins still breaks some CORS requests.
+  // Maybe we can provide something smarter here.
+  std::string raw_headers =
+      "HTTP/1.1 200 OK\r\n"
+      "Access-Control-Allow-Origin: *\r\n";;
+  new_info.headers = new net::HttpResponseHeaders(
+      net::HttpUtil::AssembleRawHeaders(raw_headers.c_str(),
+                                        raw_headers.size()));
+  *info = new_info;
+}
+
+Http200OkJob::~Http200OkJob() {}
+
+void Http200OkJob::StartAsync() {
+  NotifyHeadersComplete();
+}
+
+AdBlockInterceptor::AdBlockInterceptor() {}
+AdBlockInterceptor::~AdBlockInterceptor() {}
+
+net::URLRequestJob* AdBlockInterceptor::MaybeInterceptRequest(
+    net::URLRequest* request,
+    net::NetworkDelegate* network_delegate) const {
+  std::string header;
+  if (request->extra_request_headers().GetHeader("X-Brave-Block", &header)) {
+    VLOG(1) << "Intercepting request: " << request->url().spec();
+    return new Http200OkJob(request, network_delegate);
+  }
+  return nullptr;
+}
+
+}  // namespace brave
+}  // namespace content
---
+++ content/browser/adblock_interceptor.h
@@ -0,0 +1,32 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CONTENT_BROWSER_ADBLOCK_INTERCEPTOR_H_
+#define BRAVE_CONTENT_BROWSER_ADBLOCK_INTERCEPTOR_H_
+
+#include "base/macros.h"
+#include "content/common/content_export.h"
+#include "net/url_request/url_request_interceptor.h"
+
+namespace brave {
+namespace content {
+
+// Intercepts certain requests and blocks them by silently returning 200 OK
+// and not allowing them to hit the network.
+class CONTENT_EXPORT AdBlockInterceptor : public net::URLRequestInterceptor {
+ public:
+  AdBlockInterceptor();
+  ~AdBlockInterceptor() override;
+
+protected:
+  // net::URLRequestInterceptor:
+  net::URLRequestJob* MaybeInterceptRequest(
+      net::URLRequest* request,
+      net::NetworkDelegate* network_delegate) const override;
+};
+
+}  // namespace brave
+}  // namespace content
+
+#endif  // BRAVE_CONTENT_BROWSER_ADBLOCK_INTERCEPTOR_H_
