46bb292ab8cfc42de5243020eb90071021cd56a9
Brian Clifton <brian@clifton.me>
2019-01-17 00:43:51 -0700

    Merge pull request #1249 from brave/brave-actions-size
    Brave Actions sized at 18px and highlighted correctly

---
+++ chromium_src/chrome/browser/ui/extensions/icon_with_badge_image_source.cc
@@ -0,0 +1,14 @@
+#include "../../../../../../chrome/browser/ui/extensions/icon_with_badge_image_source.cc"
+
+// Implement default virtual methods
+base::Optional<int> IconWithBadgeImageSource::GetCustomGraphicSize() {
+  return base::nullopt;
+}
+
+base::Optional<int> IconWithBadgeImageSource::GetCustomGraphicXOffset() {
+  return base::nullopt;
+}
+
+base::Optional<int> IconWithBadgeImageSource::GetCustomGraphicYOffset() {
+  return base::nullopt;
+}
