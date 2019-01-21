3b41631f96f42a775666f96833127d3b6f280eea
Ivan Efremov <iefremov@igalia.com>
2019-01-22 03:16:25 +0700

    Issue 2254: Avoid patching /content, move the code to /brave.

--- components/brave_shields/browser/adblock_interceptor.cc
+++ components/brave_shields/browser/adblock_interceptor.cc
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "brave/content/browser/adblock_interceptor.h"
+#include "brave/components/brave_shields/browser/adblock_interceptor.h"
 
 #include "base/bind.h"
 #include "base/compiler_specific.h"
