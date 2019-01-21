ad41b4867716b793aeddd636dfaf219c19f83e2d
Ross Moody <rossmoody@MacBook-Pro.local>
2019-01-21 11:26:48 -0800

    WebTorrent styling
    Test fixes
    
    Linter/Formatting Fixes

--- components/brave_webtorrent/extension/components/torrentViewerHeader.tsx
+++ components/brave_webtorrent/extension/components/torrentViewerHeader.tsx
@@ -3,14 +3,10 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import * as React from 'react'
-import { Button, Column, Grid } from 'brave-ui/components'
-import { TitleHeading } from 'brave-ui/old'
+import { Button, Heading } from 'brave-ui/components'
 
 // Constants
 import { TorrentObj } from '../constants/webtorrentState'
-
-// Themes
-import { theme } from '../constants/theme'
 
 let clipboardCopy = require('clipboard-copy')
 
@@ -23,7 +19,10 @@ interface Props {
   onStopDownload: (tabId: number) => void
 }
 
-export default class TorrentViewerHeader extends React.PureComponent<Props, {}> {
+export default class TorrentViewerHeader extends React.PureComponent<
+  Props,
+  {}
+> {
   onClick = () => {
     this.props.torrent
       ? this.props.onStopDownload(this.props.tabId)
@@ -43,42 +42,41 @@ export default class TorrentViewerHeader extends React.PureComponent<Props, {}> 
 
   render () {
     const { torrent } = this.props
-    const name = typeof(this.props.name) === 'object'
-      ? this.props.name[0]
-      : this.props.name
+    const name =
+      typeof this.props.name === 'object'
+        ? this.props.name[0]
+        : this.props.name
     const title = torrent
       ? name
       : name
-        ? `Start Torrenting ${name}?`
-        : 'Loading torrent information...'
-    const mainButtonText = torrent ? 'Stop Download' : 'Start Torrent'
+      ? `${name}`
+      : 'Loading torrent information...'
+    const mainButtonText = torrent ? 'Stop Torrent' : 'Start Torrent'
     const copyButtonText = this.props.torrentId.startsWith('magnet:')
       ? 'Copy Magnet Link'
-      : 'Save Torrent File'
+      : 'Save File'
 
     return (
-      <Grid>
-        <Column size={9} customStyle={theme.headerColumnLeft}>
-          <TitleHeading
-            text={title}
+      <div className='headerContainer'>
+        <div className='__column'>
+          <Heading children={title} className='__torrentTitle' />
+        </div>
+        <div className='__column'>
+          <Button
+            type='accent'
+            text={mainButtonText}
+            onClick={this.onClick}
+            className='__button'
           />
-        </Column>
-        <Column size={3} customStyle={theme.headerColumnRight}>
-          <div style={theme.buttonContainer}>
-            <Button
-              type='accent'
-              text={mainButtonText}
-              onClick={this.onClick}
-            />
-            <Button
-              type='accent'
-              level='secondary'
-              text={copyButtonText}
-              onClick={this.onCopyClick}
-            />
-          </div>
-        </Column>
-      </Grid>
+          <Button
+            type='accent'
+            level='secondary'
+            text={copyButtonText}
+            onClick={this.onCopyClick}
+            className='__button'
+          />
+        </div>
+      </div>
     )
   }
 }
