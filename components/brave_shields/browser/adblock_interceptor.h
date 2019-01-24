55cc6ef688cbd0836855099b4477e346a139a39d
Ivan Efremov <iefremov@igalia.com>
2019-01-24 17:26:30 +0700

    Issue 2254: Return 1x1 image stubs from adblock interceptor.

--- components/brave_shields/browser/adblock_interceptor.h
+++ components/brave_shields/browser/adblock_interceptor.h
@@ -2,18 +2,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef BRAVE_CONTENT_BROWSER_ADBLOCK_INTERCEPTOR_H_
-#define BRAVE_CONTENT_BROWSER_ADBLOCK_INTERCEPTOR_H_
+#ifndef COMPONENTS_BRAVE_SHIELDS_BROWSER_ADBLOCK_INTERCEPTOR_H_
+#define COMPONENTS_BRAVE_SHIELDS_BROWSER_ADBLOCK_INTERCEPTOR_H_
 
-#include "base/macros.h"
-#include "content/common/content_export.h"
 #include "net/url_request/url_request_interceptor.h"
 
 namespace brave_shields {
 
 // Intercepts certain requests and blocks them by silently returning 200 OK
 // and not allowing them to hit the network.
-class CONTENT_EXPORT AdBlockInterceptor : public net::URLRequestInterceptor {
+class AdBlockInterceptor : public net::URLRequestInterceptor {
  public:
   AdBlockInterceptor();
   ~AdBlockInterceptor() override;
@@ -23,8 +21,10 @@ class CONTENT_EXPORT AdBlockInterceptor : public net::URLRequestInterceptor {
   net::URLRequestJob* MaybeInterceptRequest(
       net::URLRequest* request,
       net::NetworkDelegate* network_delegate) const override;
+ private:
+  DISALLOW_COPY_AND_ASSIGN(AdBlockInterceptor);
 };
 
 }  // namespace brave_shields
 
-#endif  // BRAVE_CONTENT_BROWSER_ADBLOCK_INTERCEPTOR_H_
+#endif  // COMPONENTS_BRAVE_SHIELDS_BROWSER_ADBLOCK_INTERCEPTOR_H_
