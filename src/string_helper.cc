cb03634c2ae1f5fa9e7a5b8197dae59ca3efa463
Terry Mancey <terry.mancey@icloud.com>
2019-01-31 11:08:09 +0000

    Added string helper

---
+++ src/string_helper.cc
@@ -0,0 +1,21 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "string_helper.h"
+
+namespace helper {
+
+std::vector<uint8_t> String::decode_hex(const std::string& hexadecimal) {
+  std::vector<uint8_t> bytes;
+
+  for (size_t i = 0; i < hexadecimal.length(); i += 2) {
+      std::string hexidecimal_byte = hexadecimal.substr(i, 2);
+      uint8_t decimal = std::strtol(hexidecimal_byte.c_str(), 0, 16);
+      bytes.push_back(decimal);
+  }
+
+  return bytes;
+}
+
+}  // namespace helper
