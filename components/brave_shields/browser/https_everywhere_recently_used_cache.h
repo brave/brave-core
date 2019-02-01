c04de4dd26dda811347a012156a50d2f6a3e22ad
bridiver <github@brianjohnson.cc>
2019-02-01 12:59:42 -0700

    lint

--- components/brave_shields/browser/https_everywhere_recently_used_cache.h
+++ components/brave_shields/browser/https_everywhere_recently_used_cache.h
@@ -1,57 +1,63 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright 2016 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_BRAVE_SHIELDS_BROWSER_HTTPS_EVERYWHERE_RECENTLY_USED_CACHE_H_
+#define BRAVE_COMPONENTS_BRAVE_SHIELDS_BROWSER_HTTPS_EVERYWHERE_RECENTLY_USED_CACHE_H_
 
 #include <unordered_map>
 #include <string>
 #include <vector>
 
-template <class T> class RingBuffer
-{
-private:
-    int currentIdx = 0;
-    int count;
-    std::vector<T> data;
-public:
-    RingBuffer(int fixedSize) : count(fixedSize), data(count) {}
+template <class T> class RingBuffer {
+ public:
+  explicit RingBuffer(int fixedSize) : count(fixedSize), data(count) {}
 
-    const T& at(int i) {
-        return data[(currentIdx - (i % count) + count) % count];
-    }
+  const T& at(int i) {
+    return data[(currentIdx - (i % count) + count) % count];
+  }
 
-    void add(const T& newValue) {
-        currentIdx = (currentIdx + 1) % count;
-        data[currentIdx] = newValue;
-    }
+  void add(const T& newValue) {
+    currentIdx = (currentIdx + 1) % count;
+    data[currentIdx] = newValue;
+  }
 
-    T oldest() {
-        return data[(currentIdx + 1) % count];
-    }
+  T oldest() {
+    return data[(currentIdx + 1) % count];
+  }
 
-    void clear() {
-        data = std::vector<T>(count);
-    }
+  void clear() {
+    data = std::vector<T>(count);
+  }
+
+ private:
+  int currentIdx = 0;
+  int count;
+  std::vector<T> data;
 };
 
-template <class T> class HTTPSERecentlyUsedCache
-{
-private:
-    RingBuffer<T> keysByAge;
-public:
-    std::unordered_map<std::string, T> data;
+template <class T> class HTTPSERecentlyUsedCache {
+ public:
+  explicit HTTPSERecentlyUsedCache(unsigned int size = 100) : keysByAge(size) {}
 
-    HTTPSERecentlyUsedCache(unsigned int size = 100) : keysByAge(size) {}
-
-    void add(const std::string& key, const T& value) {
-        std::string old = keysByAge.oldest();
-        if (!old.empty()) {
-            keysByAge.data.erase(old);
-        }
-        keysByAge[key] = value;
+  void add(const std::string& key, const T& value) {
+    std::string old = keysByAge.oldest();
+    if (!old.empty()) {
+      keysByAge.data.erase(old);
     }
+    keysByAge[key] = value;
+  }
 
-    void clear() {
-        data.clear();
-        keysByAge.clear();
-    }
+  void clear() {
+    data.clear();
+    keysByAge.clear();
+  }
+
+  std::unordered_map<std::string, T> data;
+
+ private:
+  RingBuffer<T> keysByAge;
 };
+
+#endif  // BRAVE_COMPONENTS_BRAVE_SHIELDS_BROWSER_HTTPS_EVERYWHERE_RECENTLY_USED_CACHE_H_
