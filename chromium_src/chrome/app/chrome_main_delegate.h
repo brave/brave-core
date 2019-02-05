6f1f47bdb2ecd2b7e3daebe121b902a81e9b8468
bridiver <github@brianjohnson.cc>
2019-02-04 22:15:47 -0700

    register brave scheme fix https://github.com/brave/brave-browser/issues/3221

---
+++ chromium_src/chrome/app/chrome_main_delegate.h
@@ -0,0 +1,17 @@
+/* Copyright 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_APP_CHROME_MAIN_DELEGATE_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_APP_CHROME_MAIN_DELEGATE_H_
+
+#include "../../../chrome/common/chrome_content_client.h"  // NOLINT
+
+#include "brave/common/brave_content_client.h"
+
+#define ChromeContentClient BraveContentClient
+#include "../../../chrome/app/chrome_main_delegate.h"  // NOLINT
+#undef ChromeContentClient
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_APP_CHROME_MAIN_DELEGATE_H_
