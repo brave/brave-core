a62d48be5179ea04678084c842e55368c0a96dd8
Cezar Augusto <boss@cezaraugusto.net>
2019-02-07 22:19:28 -0200

    refactor components tests

--- test/app/components/braveShields/interfaceControlsTest.tsx
+++ test/app/components/braveShields/interfaceControlsTest.tsx
@@ -1,16 +1,12 @@
-/* global describe, it */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import 'mocha'
 import * as React from 'react'
-import * as assert from 'assert'
 import ShieldsInterfaceControls, { Props } from '../../../../app/components/braveShields/interfaceControls'
 import { BlockOptions } from '../../../../app/types/other/blockTypes'
 import * as actionTypes from '../../../../app/constants/shieldsPanelTypes'
 import { shallow } from 'enzyme'
-import * as sinon from 'sinon'
 
 const fakeProps: Props = {
   url: 'https://brave.com',
@@ -36,90 +32,90 @@ describe('InterfaceControls component', () => {
   it('renders the component', () => {
     const wrapper = shallow(baseComponent(fakeProps))
     const assertion = wrapper.find('#braveShieldsInterfaceControls').length === 1
-    assert.equal(assertion, true)
+    expect(assertion).toBe(true)
   })
 
   describe('ad control', () => {
     it('responds to the onChange event', () => {
       const value = { target: { value: true } }
-      const onChangeAdControlSelectOptions = sinon.spy()
+      const onChangeAdControlSelectOptions = jest.spyOn(fakeProps, 'blockAdsTrackers')
       const newProps = Object.assign(fakeProps, {
         blockAdsTrackers: onChangeAdControlSelectOptions
       })
 
       const wrapper = shallow(baseComponent(newProps))
       wrapper.find('#blockAds').simulate('change', value)
-      assert.equal(onChangeAdControlSelectOptions.calledOnce, true)
+      expect(onChangeAdControlSelectOptions).toBeCalled()
     })
 
     it('can toggle ad control on', () => {
       const newProps = Object.assign(fakeProps, { ads: 'block', trackers: 'block' })
       const wrapper = shallow(baseComponent(newProps))
       const assertion = wrapper.find('#blockAds').prop('checked')
-      assert.equal(assertion, true)
+      expect(assertion).toBe(true)
     })
 
     it('can toggle ad control off', () => {
       const newProps = Object.assign(fakeProps, { ads: 'allow', trackers: 'allow' })
       const wrapper = shallow(baseComponent(newProps))
       const assertion = wrapper.find('#blockAds').prop('checked')
-      assert.equal(assertion, false)
+      expect(assertion).toBe(false)
     })
 
     it('shows number of ads blocked', () => {
       const newProps = Object.assign(fakeProps, { adsBlocked: 13 })
       const wrapper = shallow(baseComponent(newProps))
       const assertion = wrapper.find('#blockAdsStat').props().children
-      assert.equal(assertion, 13)
+      expect(assertion).toBe('13')
     })
 
     it('trim ads blocked to 99+ if number is higher', () => {
       const newProps = Object.assign(fakeProps, { adsBlocked: 123123123123123 })
       const wrapper = shallow(baseComponent(newProps))
       const assertion = wrapper.find('#blockAdsStat').props().children
-      assert.equal(assertion, '99+')
+      expect(assertion).toBe('99+')
     })
   })
 
   describe('https everywhere control', () => {
     it('responds to the onChange event', () => {
       const value = { target: { value: true } }
-      const onChangeConnectionsEncryptedSwitch = sinon.spy()
+      const onChangeConnectionsEncryptedSwitch = jest.spyOn(fakeProps, 'httpsEverywhereToggled')
       const newProps = Object.assign(fakeProps, {
         httpsEverywhereToggled: onChangeConnectionsEncryptedSwitch
       })
 
       const wrapper = shallow(baseComponent(newProps))
       wrapper.find('#connectionsEncrypted').simulate('change', value)
-      assert.equal(onChangeConnectionsEncryptedSwitch.calledOnce, true)
+      expect(onChangeConnectionsEncryptedSwitch).toBeCalled()
     })
 
     it('can toggle ad control on', () => {
       const newProps = Object.assign(fakeProps, { httpUpgradableResources: 'block' })
       const wrapper = shallow(baseComponent(newProps))
       const assertion = wrapper.find('#connectionsEncrypted').prop('checked')
-      assert.equal(assertion, true)
+      expect(assertion).toBe(true)
     })
 
     it('can toggle ad control off', () => {
       const newProps = Object.assign(fakeProps, { httpUpgradableResources: 'allow' })
       const wrapper = shallow(baseComponent(newProps))
       const assertion = wrapper.find('#connectionsEncrypted').prop('checked')
-      assert.equal(assertion, false)
+      expect(assertion).toBe(false)
     })
 
     it('shows number of https redirected', () => {
       const newProps = Object.assign(fakeProps, { httpsRedirected: 13 })
       const wrapper = shallow(baseComponent(newProps))
       const assertion = wrapper.find('#connectionsEncryptedStat').props().children
-      assert.equal(assertion, 13)
+      expect(assertion).toBe('13')
     })
 
     it('trim https redirected to 99+ if number is higher', () => {
       const newProps = Object.assign(fakeProps, { httpsRedirected: 123123123 })
       const wrapper = shallow(baseComponent(newProps))
       const assertion = wrapper.find('#connectionsEncryptedStat').props().children
-      assert.equal(assertion, '99+')
+      expect(assertion).toBe('99+')
     })
   })
 })
