5f9fd778a171fa4585c85074fc42b118dcea975d
Brian R. Bondy <netzen@gmail.com>
2019-02-08 15:47:08 -0500

    Fix string handling for create_string_rc

---
+++ chromium_src/python_modules/chrome/installer/util/prebuild/create_installer_string_rc.py
@@ -0,0 +1,86 @@
+MODE_SPECIFIC_STRINGS = {
+  'IDS_APP_SHORTCUTS_SUBDIR_NAME': {
+    'google_chrome': [
+      'IDS_APP_SHORTCUTS_SUBDIR_NAME',
+      'IDS_APP_SHORTCUTS_SUBDIR_NAME_BETA',
+      'IDS_APP_SHORTCUTS_SUBDIR_NAME_DEV',
+      'IDS_APP_SHORTCUTS_SUBDIR_NAME_CANARY',
+    ],
+    'chromium': [
+      'IDS_APP_SHORTCUTS_SUBDIR_NAME',
+    ],
+    'brave': [
+      'IDS_APP_SHORTCUTS_SUBDIR_NAME',
+      'IDS_APP_SHORTCUTS_SUBDIR_NAME_BETA',
+      'IDS_APP_SHORTCUTS_SUBDIR_NAME_DEV',
+      'IDS_APP_SHORTCUTS_SUBDIR_NAME_CANARY',
+    ],
+    'brave-development': [
+      'IDS_APP_SHORTCUTS_SUBDIR_NAME',
+    ],
+  },
+  'IDS_INBOUND_MDNS_RULE_DESCRIPTION': {
+    'google_chrome': [
+      'IDS_INBOUND_MDNS_RULE_DESCRIPTION',
+      'IDS_INBOUND_MDNS_RULE_DESCRIPTION_BETA',
+      'IDS_INBOUND_MDNS_RULE_DESCRIPTION_DEV',
+      'IDS_INBOUND_MDNS_RULE_DESCRIPTION_CANARY',
+    ],
+    'chromium': [
+      'IDS_INBOUND_MDNS_RULE_DESCRIPTION',
+    ],
+    'brave': [
+      'IDS_INBOUND_MDNS_RULE_DESCRIPTION',
+      'IDS_INBOUND_MDNS_RULE_DESCRIPTION_BETA',
+      'IDS_INBOUND_MDNS_RULE_DESCRIPTION_DEV',
+      'IDS_INBOUND_MDNS_RULE_DESCRIPTION_CANARY',
+    ],
+    'brave-development': [
+      'IDS_INBOUND_MDNS_RULE_DESCRIPTION',
+    ],
+  },
+  'IDS_INBOUND_MDNS_RULE_NAME': {
+    'google_chrome': [
+      'IDS_INBOUND_MDNS_RULE_NAME',
+      'IDS_INBOUND_MDNS_RULE_NAME_BETA',
+      'IDS_INBOUND_MDNS_RULE_NAME_DEV',
+      'IDS_INBOUND_MDNS_RULE_NAME_CANARY',
+    ],
+    'chromium': [
+      'IDS_INBOUND_MDNS_RULE_NAME',
+    ],
+    'brave': [
+      'IDS_INBOUND_MDNS_RULE_NAME',
+      'IDS_INBOUND_MDNS_RULE_NAME_BETA',
+      'IDS_INBOUND_MDNS_RULE_NAME_DEV',
+      'IDS_INBOUND_MDNS_RULE_NAME_CANARY',
+    ],
+    'brave-development': [
+      'IDS_INBOUND_MDNS_RULE_NAME',
+    ],
+  },
+  # In contrast to the strings above, this one (IDS_PRODUCT_NAME) is used
+  # throughout Chrome in mode-independent contexts. Within the installer (the
+  # place where this mapping matters), it is only used for mode-specific strings
+  # such as the name of Chrome's shortcut.
+  'IDS_PRODUCT_NAME': {
+    'google_chrome': [
+      'IDS_PRODUCT_NAME',
+      'IDS_SHORTCUT_NAME_BETA',
+      'IDS_SHORTCUT_NAME_DEV',
+      'IDS_SXS_SHORTCUT_NAME',
+    ],
+    'chromium': [
+      'IDS_PRODUCT_NAME',
+    ],
+    'brave': [
+      'IDS_PRODUCT_NAME',
+      'IDS_SHORTCUT_NAME_BETA',
+      'IDS_SHORTCUT_NAME_DEV',
+      'IDS_SXS_SHORTCUT_NAME',
+    ],
+    'brave-development': [
+      'IDS_PRODUCT_NAME',
+    ],
+  },
+}
