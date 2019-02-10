085c4b20dfe7f3b823bd36d88db5dc34e7b55f56
Terry Mancey <terry.mancey@icloud.com>
2019-02-10 23:08:41 +0000

    Rename signing_key to public_key for confirmations wallet info

--- vendor/bat-native-ledger/src/ledger_impl.cc
+++ vendor/bat-native-ledger/src/ledger_impl.cc
@@ -1097,7 +1097,7 @@ const confirmations::WalletInfo LedgerImpl::GetConfirmationsWalletInfo(
   std::vector<uint8_t> secretKey = {};
   braveledger_bat_helper::getPublicKeyFromSeed(seed, publicKey, secretKey);
 
-  wallet_info.signing_key = braveledger_bat_helper::uint8ToHex(secretKey);
+  wallet_info.public_key = braveledger_bat_helper::uint8ToHex(secretKey);
 
   return wallet_info;
 }
