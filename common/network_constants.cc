80fc48b63ecd4d4e3d318687fd612dd1177f2098
yan <yan@brave.com>
2019-02-10 21:49:40 -0800

    Reject referral promo header names unless whitelisted
    Fix https://github.com/brave/brave-browser/issues/3301
    
    Currently the only whitelisted header is 'X-Brave-Partner'.

--- common/network_constants.cc
+++ common/network_constants.cc
@@ -26,6 +26,7 @@ const char kCookieHeader[] = "Cookie";
 // Intentional misspelling on referrer to match HTTP spec
 const char kRefererHeader[] = "Referer";
 const char kUserAgentHeader[] = "User-Agent";
+const char kBravePartnerHeader[] = "X-Brave-Partner";
 
 const char kBittorrentMimeType[] = "application/x-bittorrent";
 const char kOctetStreamMimeType[] = "application/octet-stream";
