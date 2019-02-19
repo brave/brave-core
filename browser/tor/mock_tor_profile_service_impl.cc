2ef1c25dbcb70f6b8734b7da4c9e31643c720caf
Taylor `Riastradh' Campbell <taylor@brave.com>
2019-02-19 23:38:22 +0000

    Appease lint and avoid C++ pitfalls.
    - Fix copyright headings to appease lint.
    - Fix header include guards to appease lint.
    - Fix missing includes.
    - Use explicit for unary constructors.
    - Fix whitespace to appease lint.

--- browser/tor/mock_tor_profile_service_impl.cc
+++ browser/tor/mock_tor_profile_service_impl.cc
@@ -1,8 +1,11 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/browser/tor/mock_tor_profile_service_impl.h"
+
+#include <string>
 
 #include "brave/browser/tor/tor_proxy_config_service.h"
 #include "brave/common/tor/tor_test_constants.h"
