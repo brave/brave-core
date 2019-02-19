0471a50fa8b5760bcf056cbafa9a549d80487ea2
Taylor `Riastradh' Campbell <taylor@brave.com>
2019-02-19 23:38:22 +0000

    Appease lint and avoid C++ pitfalls.
    - Fix copyright headings to appease lint.
    - Fix header include guards to appease lint.
    - Fix missing includes.
    - Use explicit for unary constructors.
    - Fix whitespace to appease lint.

--- browser/net/brave_tor_network_delegate_helper.cc
+++ browser/net/brave_tor_network_delegate_helper.cc
@@ -1,8 +1,11 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/browser/net/brave_tor_network_delegate_helper.h"
+
+#include <memory>
 
 #include "brave/browser/renderer_host/brave_navigation_ui_data.h"
 #include "brave/browser/tor/tor_profile_service.h"
--- browser/tor/mock_tor_profile_service_impl.h
+++ browser/tor/mock_tor_profile_service_impl.h
@@ -1,9 +1,10 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef BRAVE_BROWSER_TOR_MOCK_TOR_PROFILE_SERVICE_IMPL_
-#define BRAVE_BROWSER_TOR_MOCK_TOR_PROFILE_SERVICE_IMPL_
+#ifndef BRAVE_BROWSER_TOR_MOCK_TOR_PROFILE_SERVICE_IMPL_H_
+#define BRAVE_BROWSER_TOR_MOCK_TOR_PROFILE_SERVICE_IMPL_H_
 
 #include "brave/browser/tor/tor_profile_service.h"
 
@@ -13,7 +14,7 @@ namespace tor {
 
 class MockTorProfileServiceImpl : public TorProfileService {
  public:
-  MockTorProfileServiceImpl(Profile* profile);
+  explicit MockTorProfileServiceImpl(Profile* profile);
   ~MockTorProfileServiceImpl() override;
 
   // TorProfileService:
@@ -35,4 +36,4 @@ class MockTorProfileServiceImpl : public TorProfileService {
 
 }  // namespace tor
 
-#endif  // BRAVE_BROWSER_TOR_MOCK_TOR_PROFILE_SERVICE_IMPL_
+#endif  // BRAVE_BROWSER_TOR_MOCK_TOR_PROFILE_SERVICE_IMPL_H_
