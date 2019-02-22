03f008053ffabe3a821369b24f41807450e85d67
Brian R. Bondy <netzen@gmail.com>
2019-02-22 07:16:00 -0500

    Fix cpp lint

--- browser/extensions/brave_component_loader.cc
+++ browser/extensions/brave_component_loader.cc
@@ -1,8 +1,11 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/browser/extensions/brave_component_loader.h"
+
+#include <string>
 
 #include "base/command_line.h"
 #include "brave/browser/brave_browser_process_impl.h"
@@ -22,7 +25,7 @@
 
 namespace extensions {
 
-//static
+// static
 bool BraveComponentLoader::IsPdfjsDisabled() {
   const base::CommandLine& command_line =
     *base::CommandLine::ForCurrentProcess();
