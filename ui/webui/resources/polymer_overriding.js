9cb5bcc81721ad99e6b8c4af0320ccb221fff358
petemill <miller.pete+commit@gmail.com>
2019-03-01 10:49:53 -0800

    [wip] modify chromium html template

--- ui/webui/resources/polymer_overriding.js
+++ ui/webui/resources/polymer_overriding.js
@@ -5,13 +5,35 @@
 // This is in global scope,
 // so create a temporary scope.
 (function () {
+
+  // rewrite dom-module IDs so we can replace with ours
+  // const oldRegisterFn = window.Polymer.DomModule.prototype.register
+  // function newRegisterFn(id) {
+  //   if (id === 'br-toolbar')
+  //     id == 'br-toolbar-test'
+  // }
+
   window.BravePatching = window.BravePatching || {}
   const oldFn = window.Polymer._polymerFn
   function newPolymerFn(component){
     if (component && component.is) {
       AddBraveBehaviors(component)
     }
-    return oldFn(component)
+    const polymerComponent = oldFn(component)
+    if (component && component.is === 'cr-toolbar') {
+      const domModule = Polymer.DomModule.import('cr-toolbar')
+      const template = domModule.querySelector('template')
+      console.log(domModule, template)
+      const testToolbar = template.content.querySelector('div')
+      console.log('br-toolbar', testToolbar)
+      // const testToolbar = polymerComponent.template.content.querySelector('.br-toolbar')
+      // console.log('cr-toolbar register', testToolbar.outerHTML)
+      const testEl = document.createElement('h2')
+      testEl.innerText = '[[settingsTitle]]'
+      testToolbar.appendChild(testEl)
+      console.log('cr-toolbar modify', testToolbar.outerHTML)
+    }
+    return polymerComponent
   }
   window.Polymer._polymerFn = newPolymerFn
 
