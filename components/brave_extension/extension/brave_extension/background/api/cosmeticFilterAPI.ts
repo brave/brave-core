be4c79e0f0e0f05bdc7fdfafd00a535e6148c491
Cezar Augusto <boss@cezaraugusto.net>
2019-03-08 01:11:11 -0300

    remove unnecessary logging from shields tests

--- components/brave_extension/extension/brave_extension/background/api/cosmeticFilterAPI.ts
+++ components/brave_extension/extension/brave_extension/background/api/cosmeticFilterAPI.ts
@@ -22,13 +22,15 @@ export const applySiteFilters = (hostname: string) => {
   chrome.storage.local.get('cosmeticFilterList', (storeData = {}) => {
     if (!storeData.cosmeticFilterList) {
       if (process.env.NODE_ENV === 'shields_development') {
-        console.info('applySiteFilters: no cosmetic filter store yet')
+        console.log('applySiteFilters: no cosmetic filter store yet')
       }
       return
     }
     if (storeData.cosmeticFilterList[hostname] !== undefined) {
       storeData.cosmeticFilterList[hostname].map((rule: string) => {
-        console.log('applying rule', rule)
+        if (process.env.NODE_ENV === 'shields_development') {
+          console.log('applying rule', rule)
+        }
         chrome.tabs.insertCSS({
           code: `${rule} {display: none;}`,
           runAt: 'document_start'
