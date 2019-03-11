cba276e10332b1145ba0445ceddc6801cc3fd5fb
petemill <miller.pete+commit@gmail.com>
2019-03-11 12:43:35 -0700

    WebUI overriding in 4 ways:
    1. Add a behavior to an existing Polymer JS component. This is similar to injecting a superclass which will receive the same lifecycle method calls. It could also be used to add properties and methods to the component, usable from the component's html template.
    
    2. Modify an existing Polymer HTML template. When the component is registered, we provide the template DOM element which can be modified before it is cloned for use as content and attached to the document.
    
    3. Inject style to any existing Polymer HTML component. By naming a module 'brave-override-style-my-module', the <style> element from that module will be taken and injected in to the existing 'my-module' component.
    
    4. Replace icons in an existing iconset. This takes an icon from one existing iconset and replaces the content of an icon in another existing iconset. Can be used to replace a chromium icon with a brave icon.
    
    Whilst all this can be achieved via .patch files at sync / build time, this:
    - Keeps all the modifications functional. The advantage here is realized in the 100% inevitable and likely-common scenarios where the chromium webui implementations and underlying technology changes (e.g. html-modules vs es-modules, or simple module refactors). For some scenarios, we may be able to simply modify the central manipulation library (polymer_overrides.js). In more complicated scenarios, we will at least have all modifications functionally and individually labelled, making 'rebasing' potentially more straightforward than a .patch file with mixed intentions.
    - Maintains performance since these modifications are only performed once, whilst the component is being registered via the Polymer(component) function.
    
    Debugging:
    - Display (in verbose console log) how long it took to modify each Polymer template
    - Option to see timing of module and module-override registration (via hard-coded debug variable)

--- browser/resources/settings/brave_settings_overrides.js
+++ browser/resources/settings/brave_settings_overrides.js
@@ -1,8 +1,18 @@
+// Copyright (c) 2019 The Brave Authors. All rights reserved.
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // you can obtain one at http://mozilla.org/MPL/2.0/.
 
-window.BravePatching.registerPolymerBehaviors({
+if (!BravePatching) {
+  console.error('BravePatching was not available to brave_settings_overrides.js')
+}
+
+//
+// Override, extend or modify existing modules
+//
+
+// Polymer Component Behavior injection (like superclasses)
+BravePatching.RegisterPolymerComponentBehaviors({
   'settings-clear-browsing-data-dialog': [
     BraveClearBrowsingDataOnExitBehavior
   ]
