5db220863b6d4ae5e1f96c2d47fff06ed5ee9b44
Matt Bacchi <mbacchi@gmail.com>
2019-03-19 11:18:34 -0600

    Parse changelog for specific Brave version
    Output either markdown or html

---
+++ script/changelog.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+import argparse
+import logging
+import re
+import sys
+
+from argparse import RawTextHelpFormatter
+
+from lib.changelog import *
+
+
+def main():
+    """
+    Download the brave-browser/CHANGELOG.md file, parse it, and
+    return either markdown or html for a particular Brave tag.
+
+    Example:
+    python script/changelog.py -t refs/tags/v0.61.51 \
+        -u https://raw.githubusercontent.com/brave/brave-browser/master/CHANGELOG.md -o markdown
+
+    ## [0.61.51](https://github.com/brave/brave-browser/releases/tag/v0.61.51)
+
+    - Added new setting that allows Brave Rewards icon in the URL to be hidden if Rewards \
+        is inactive. ([#2975](https://github.com/brave/brave-browser/issues/2975))
+
+    """
+
+    args = parse_args()
+
+    if args.debug:
+        logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+    changelog_url = args.url
+
+    tag = args.tag
+
+    if not re.match(r'^refs/tags/', tag) and not re.match(r'^v', tag):
+        logging.error(" Tag prefix must contain {} or {}".format("\"refs/tags/\"", "\"v\""))
+        exit(1)
+
+    match = re.match(r'^refs/tags/(.*)$', tag)
+    if match:
+        tag = match.group(1)
+
+    match = re.match(r'^v(.*)$', tag)
+    if match:
+        version = match.group(1)
+
+    logging.debug("CHANGELOG_URL: {}".format(changelog_url))
+    logging.debug("TAG: {}".format(tag))
+    logging.debug("VERSION: {}".format(version))
+
+    changelog_txt = download_from_url(args, logging, changelog_url)
+
+    if args.output == 'markdown':
+        print(render_markdown(changelog_txt, version, logging))
+    elif args.output == 'html':
+        print(render_html(changelog_txt, version, logging))
+
+
+def parse_args():
+    desc = "Parse Brave Browser changelog and return changes for a tag"
+
+    parser = argparse.ArgumentParser(
+        description=desc, formatter_class=RawTextHelpFormatter)
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help='Print debug statements')
+    parser.add_argument('-o', '--output', help='Output format: markdown or html', required=True)
+    parser.add_argument('-t', '--tag',
+                        help='Brave version tag (allowed format: "v0.60.45" or "refs/tags/v0.60.45")', required=True)
+    parser.add_argument('-u', '--url', help='URL for Brave Browser raw markdown file (required)', required=True)
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ script/lib/changelog.py
@@ -0,0 +1,99 @@
+import requests
+import mistletoe
+
+from mistletoe import Document, ast_renderer
+
+
+def render_markdown(changelog_txt, version, logging):
+    """
+    Create an AST from the changelog document (which is in Markdown).
+    Search the AST for the version we're interested in, generate
+    and return markdown as a string for just that version.
+    """
+
+    d = Document(changelog_txt)
+    output = ast_renderer.get_ast(d)
+    s = str()
+
+    logging.debug("Locating the changelog AST for version: \'{}\'".format(version))
+
+    version_heading = ''
+    version_changes = ''
+    pos = 0
+    for item in output['children']:
+        if item['type'] == 'Heading' and item['level'] == 2:
+            if item['children'][0]['children'][0]['content'] == version:
+                version_heading = output['children'][pos]
+                version_changes = output['children'][pos+1]
+        pos = pos + 1
+    if version_heading and version_changes:
+        heading = reconstruct_heading(version_heading)
+        s = heading + '\n'
+        s = s + '\n'
+        changes = reconstruct_brave_changelog_list(version_changes)
+        for i in changes:
+            s = s + i + '\n'
+    else:
+        logging.error("Cannot Locate the changelog AST for version: \'{}\'".format(version))
+        exit(1)
+
+    return s
+
+
+def render_html(changelog_txt, version, logging):
+    """
+    Format an html rendered document from the render_markdown method above.
+    """
+
+    rendered = mistletoe.markdown(render_markdown(changelog_txt, version, logging))
+    return rendered
+
+
+def reconstruct_heading(heading):
+    """
+    heading is a dict
+    """
+
+    if heading['type'] is "Heading":
+        return ("{} [{}]({})".format('#'*heading['level'],
+                                     heading['children'][0]['children'][0]['content'],
+                                     heading['children'][0]['target']))
+
+
+def reconstruct_brave_changelog_list(li):
+    """
+    li is a list
+    """
+
+    changes = []
+
+    for item in li['children']:
+        for item2 in item['children']:
+            changes.append("{} {}[{}]({}){}".format(' -', item2['children'][0]['content'],
+                                                    item2['children'][1]['children'][0]['content'],
+                                                    item2['children'][1]['target'],
+                                                    item2['children'][2]['content']))
+    return changes
+
+
+def download_from_url(args, logging, changelog_url):
+    headers = {'Accept': 'application/octet-stream'}
+    if args.debug:
+        # disable urllib3 logging for this session to avoid showing
+        # access_token in logs
+        logging.getLogger("urllib3").setLevel(logging.WARNING)
+    logging.info("Downloading Changelog file: {}".format(changelog_url))
+    try:
+        r = requests.get(changelog_url, headers=headers)
+    except requests.exceptions.ConnectionError as e:
+        logging.error("Error: Received requests.exceptions.ConnectionError, Exiting...")
+        exit(1)
+    except Exception as e:
+        logging.error("Error: Received exception {},  Exiting...".format(type(e)))
+        exit(1)
+    logging.debug("r.status_code: {}".format(r.status_code))
+
+    if r.status_code == 200:
+        return r.text
+    else:
+        r.raise_for_status()
