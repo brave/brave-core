3b01cbfea3e6db53ddde7b00a2b7ded35b601001
Jocelyn Liu <yrliou@gmail.com>
2019-03-18 23:07:28 -0700

    Rewrite the text in the translate bubble to indicate that texts will be sent to the MS server.

---
+++ chromium_src/chrome/browser/ui/views/translate/translate_bubble_view.cc
@@ -0,0 +1,13 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/grit/brave_generated_resources.h"
+#include "chrome/grit/generated_resources.h"
+
+#undef IDS_TRANSLATE_BUBBLE_BEFORE_TRANSLATE_TITLE
+#define IDS_TRANSLATE_BUBBLE_BEFORE_TRANSLATE_TITLE \
+  IDS_BRAVE_TRANSLATE_BUBBLE_BEFORE_TRANSLATE_TITLE
+
+#include "../../../../../../chrome/browser/ui/views/translate/translate_bubble_view.cc"
