395bb766a877bc5e9e96979cbd97374102761a4c
samartnik <artem@brave.com>
2019-03-20 13:36:14 -0400

    Removed old code for CR72 and CR71

--- browser/version_info.h
+++ browser/version_info.h
@@ -7,22 +7,10 @@
 
 #include <string>
 
-#define CHROMIUM_CR71 strcmp(BRAVE_BROWSER_VERSION, "71.0.3578.99")
-#define CHROMIUM_CR72 strcmp(BRAVE_BROWSER_VERSION, "72.0.3626.96")
-
-#if $CHROMIUM_CR72 == 0
-  #define TRIM_PUBLISHERS_DB_MEMORY db_.TrimMemory(memory_pressure_level == base::MemoryPressureListener::MEMORY_PRESSURE_LEVEL_CRITICAL);
-  #define BASE_TIME_KSECONDSPERHOUR base::Time::kSecondsPerHour
-  #define URL_DECODE_URL_MODE
-#elif $CHROMIUM_CR71 == 0
-  #define MESSAGE_NOTIFIER_TYPE message_center::NotifierId::NotifierType::SYSTEM_COMPONENT
-  #define BASE_TIME_KSECONDSPERHOUR 60 * 60
-#else
-  #define MESSAGE_NOTIFIER_TYPE message_center::NotifierType::SYSTEM_COMPONENT
-  #define BASE_TIME_KSECONDSPERHOUR base::Time::kSecondsPerHour
-  #define TRIM_PUBLISHERS_DB_MEMORY db_.TrimMemory();
-  #define URL_DECODE_URL_MODE url::DecodeURLMode::kUTF8OrIsomorphic,
-#endif
+#define MESSAGE_NOTIFIER_TYPE message_center::NotifierType::SYSTEM_COMPONENT
+#define BASE_TIME_KSECONDSPERHOUR base::Time::kSecondsPerHour
+#define TRIM_PUBLISHERS_DB_MEMORY db_.TrimMemory();
+#define URL_DECODE_URL_MODE url::DecodeURLMode::kUTF8OrIsomorphic,
 
 namespace version_info {
 std::string GetBraveVersionWithoutChromiumMajorVersion();
