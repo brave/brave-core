ec12bf4e5e453fe716e8845685baa0503c8d7811
AlexeyBarabash <alexey@brave.com>
2019-03-20 12:51:38 +0200

    Lint fixes

--- components/brave_sync/brave_sync_prefs.cc
+++ components/brave_sync/brave_sync_prefs.cc
@@ -1,6 +1,7 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+/* Copyright 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/components/brave_sync/brave_sync_prefs.h"
 
@@ -91,7 +92,8 @@ bool Prefs::GetSyncSiteSettingsEnabled() const {
 }
 
 void Prefs::SetSyncSiteSettingsEnabled(const bool sync_site_settings_enabled) {
-  pref_service_->SetBoolean(kSyncSiteSettingsEnabled, sync_site_settings_enabled);
+  pref_service_->SetBoolean(
+      kSyncSiteSettingsEnabled, sync_site_settings_enabled);
 }
 
 bool Prefs::GetSyncHistoryEnabled() const {
@@ -170,5 +172,5 @@ void Prefs::Clear() {
   pref_service_->ClearPref(kSyncApiVersion);
 }
 
-} // namespace prefs
-} // namespace brave_sync
+}  // namespace prefs
+}  // namespace brave_sync
