afac91427eb6de4ae575b204f4f25965ba2339cf
Brian Johnson <github@brianjohnson.cc>
2019-03-21 19:51:28 -0700

    Merge pull request #2024 from brave/issues/3411
    only use brave:// url for display purposes because chrome does some s…

---
+++ browser/ui/toolbar/brave_location_bar_model_delegate.h
@@ -0,0 +1,28 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_BROWSER_UI_TOOLBAR_BRAVE_LOCATION_BAR_MODEL_DELEGATE_H_
+#define BRAVE_BROWSER_UI_TOOLBAR_BRAVE_LOCATION_BAR_MODEL_DELEGATE_H_
+
+#include "base/compiler_specific.h"
+#include "base/macros.h"
+#include "chrome/browser/ui/browser_location_bar_model_delegate.h"
+
+class Browser;
+
+class BraveLocationBarModelDelegate : public BrowserLocationBarModelDelegate {
+ public:
+  explicit BraveLocationBarModelDelegate(Browser* browser);
+  ~BraveLocationBarModelDelegate() override;
+
+ private:
+  base::string16 FormattedStringWithEquivalentMeaning(
+      const GURL& url,
+      const base::string16& formatted_url) const override;
+
+  DISALLOW_COPY_AND_ASSIGN(BraveLocationBarModelDelegate);
+};
+
+#endif  // BRAVE_BROWSER_UI_TOOLBAR_BRAVE_LOCATION_BAR_MODEL_DELEGATE_H_
