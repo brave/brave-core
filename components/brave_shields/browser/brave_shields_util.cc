8114d82b30522a8a7ad06131310a11359fc42cfb
Max Karolinskiy <max@brave.com>
2019-03-27 20:42:43 -0400

    Revert "Merge pull request #2088 from brave/revert-c74-with-deps"
    Reland C74 on master.
    
    This reverts commit f6cfa13c731da335b76844f44da4fe0558bf140d, reversing
    changes made to 6143676339cfdbeae209080bcae81a036a3565c6.

--- components/brave_shields/browser/brave_shields_util.cc
+++ components/brave_shields/browser/brave_shields_util.cc
@@ -1,8 +1,11 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/components/brave_shields/browser/brave_shields_util.h"
+
+#include <memory>
 
 #include "base/task/post_task.h"
 #include "brave/common/shield_exceptions.h"
@@ -27,7 +30,6 @@ using content::BrowserThread;
 using content::Referrer;
 using content::ResourceRequestInfo;
 using net::URLRequest;
-using namespace net::registry_controlled_domains;
 
 namespace brave_shields {
 
@@ -82,7 +84,7 @@ bool IsAllowContentSettingFromIO(const net::URLRequest* request,
     const std::string& resource_identifier) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
 
-  const content::ResourceRequestInfo* resource_info =
+  content::ResourceRequestInfo* resource_info =
       content::ResourceRequestInfo::ForRequest(request);
   if (!resource_info) {
     return GetDefaultFromResourceIdentifier(resource_identifier, primary_url,
@@ -140,7 +142,6 @@ void GetRenderFrameInfo(const URLRequest* request,
   }
   if (!content::ResourceRequestInfo::GetRenderFrameForRequest(
           request, render_process_id, render_frame_id)) {
-
     const content::WebSocketHandshakeRequestInfo* websocket_info =
       content::WebSocketHandshakeRequestInfo::ForRequest(request);
     if (websocket_info) {
@@ -169,8 +170,9 @@ bool ShouldSetReferrer(bool allow_referrers, bool shields_up,
       !shields_up ||
       original_referrer.is_empty() ||
       // Same TLD+1 whouldn't set the referrer
-      SameDomainOrHost(target_url, original_referrer,
-          INCLUDE_PRIVATE_REGISTRIES) ||
+      SameDomainOrHost(
+          target_url, original_referrer,
+          net::registry_controlled_domains::INCLUDE_PRIVATE_REGISTRIES) ||
       // Whitelisted referrers shoud never set the referrer
       brave::IsWhitelistedReferrer(tab_origin, target_url.GetOrigin())) {
     return false;
