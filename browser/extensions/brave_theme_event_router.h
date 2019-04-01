825b203307105f0a77131ae9676500f2c2e19b3b
Simon Hong <shong@igalia.com>
2019-04-02 08:16:46 +0900

    Make chrome.braveTheme.onBraveThemeTypeChanged emit by native theme change
    By observing default native theme and dark theme both, BraveThemeEvenRouter can
    emit its event from both native theme.

--- browser/extensions/brave_theme_event_router.h
+++ browser/extensions/brave_theme_event_router.h
@@ -1,23 +1,42 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #ifndef BRAVE_BROWSER_EXTENSIONS_BRAVE_THEME_EVENT_ROUTER_H_
 #define BRAVE_BROWSER_EXTENSIONS_BRAVE_THEME_EVENT_ROUTER_H_
 
-#include <memory>
+#include "base/gtest_prod_util.h"
+#include "base/scoped_observer.h"
+#include "ui/native_theme/native_theme_observer.h"
 
 class Profile;
 
 namespace extensions {
 
-class BraveThemeEventRouter {
+class BraveThemeEventRouter : public ui::NativeThemeObserver {
  public:
-  static std::unique_ptr<BraveThemeEventRouter> Create();
+  explicit BraveThemeEventRouter(Profile* profile);
+  ~BraveThemeEventRouter() override;
 
-  virtual ~BraveThemeEventRouter() {}
+ private:
+  friend class MockBraveThemeEventRouter;
 
-  virtual void OnBraveThemeTypeChanged(Profile* profile) = 0;
+  // ui::NativeThemeObserver overrides:
+  void OnNativeThemeUpdated(ui::NativeTheme* observed_theme) override;
+
+  bool IsDarkModeEnabled() const;
+  void ResetThemeObserver();
+
+  // Make virtual for testing.
+  virtual void Notify();
+
+  ui::NativeTheme* current_native_theme_for_testing_ = nullptr;
+  Profile* profile_;
+  bool using_dark_;
+  ScopedObserver<ui::NativeTheme, BraveThemeEventRouter> observer_;
+
+  DISALLOW_COPY_AND_ASSIGN(BraveThemeEventRouter);
 };
 
 }  // namespace extensions
