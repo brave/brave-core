53cdac97ca39ee5e4f38ae88164f17e4e2cafdfd
Terry Mancey <terry.mancey@icloud.com>
2019-04-09 03:53:28 +0100

    Change maximum Ads per day to 12 for Android and 20 for Desktop

--- components/brave_ads/common/pref_names.cc
+++ components/brave_ads/common/pref_names.cc
@@ -1,15 +1,22 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/components/brave_ads/common/pref_names.h"
 
 namespace brave_ads {
+
 namespace prefs {
 
 const char kBraveAdsEnabled[] = "brave.brave_ads.enabled";
 const char kBraveAdsPerHour[] = "brave.brave_ads.ads_per_hour";
 const char kBraveAdsPerDay[] = "brave.brave_ads.ads_per_day";
 const char kBraveAdsIdleThreshold[] = "brave.brave_ads.idle_threshold";
+const int kBraveAdsPrefsDefaultVersion = 1;
+const int kBraveAdsPrefsCurrentVersion = 2;
+const char kBraveAdsPrefsVersion[] = "brave.brave_ads.prefs.version";
+
 }  // namespace prefs
+
 }  // namespace brave_ads
