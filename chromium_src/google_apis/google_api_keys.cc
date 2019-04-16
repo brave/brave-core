70def0c486d9945c7be1946b346229156492be9e
Jocelyn Liu <yrliou@gmail.com>
2019-04-16 12:25:12 -0700

    Update translate manager unit tests to cover both with or without key cases

---
+++ chromium_src/google_apis/google_api_keys.cc
@@ -0,0 +1,15 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/chromium_src/google_apis/google_api_keys.h"
+#include "../../../google_apis/google_api_keys.cc" // NOLINT
+
+namespace google_apis {
+
+void SetAPIKeyForTesting(const std::string& api_key) {
+  g_api_key_cache.Get().set_api_key_for_testing(api_key);
+}
+
+}  // namespace google_apis
