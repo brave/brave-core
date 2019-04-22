7fa28a5968b5ca7fa18b30d59e891d8639477246
Anthony Tseng <darkdh@gmail.com>
2019-04-22 15:04:24 -0700

    Remove unused code

--- components/brave_sync/brave_sync_service.h
+++ components/brave_sync/brave_sync_service.h
@@ -20,8 +20,6 @@ class BraveSyncServiceObserver;
 class Settings;
 class SyncDevices;
 
-bookmarks::BookmarkPermanentNodeList
-LoadExtraNodes(bookmarks::LoadExtraCallback callback, int64_t* next_node_id);
 bool IsSyncManagedNode(const bookmarks::BookmarkPermanentNode* node);
 
 class BraveSyncService {
