7644e99241aa9ea5c72ce0bce961c64ba65064a7
Max Karolinskiy <max@brave.com>
2019-04-24 16:06:11 -0400

    Fix profile menu.
    * Incognito profile menu is now in a separate class, so need to add Tor
    menu to it separately.
    
    * ProfileChooseView is now instantiated in ProfileMenuViewBase so
    instantiate our subclasses there.
    
    * Menu is no longer built from constructor so we can use overridden
      method to fix the profile button instead of casting.
    
    * We only need to add the button to the menu_items now, don't need to
    worry about the layout.
    
    * Switch to 'using' base class constructors since we don't do anything
    in the subclassed ones.
    
    * ResetView was renamed to Reset.
    
    Chromium changes:
    
    commit 10f5093c58bc6785735a4d67498e24a6af81c5da
    Author: Ramin Halavati <rhalavati@chromium.org>
    Date:   Thu Mar 28 09:58:30 2019 +0000
    
        Separate content generation and formatting of ProfileChooserView.
    
        ProfileChooserView generates several menus and draws them. Drawing is
        done using different code paths causing difficulty in maintenance of
        similar output.
    
        This CL separates content generation code and menu formatting to make
        a unified code path for drawing all menus and prepare the class for
        separation of Incognito menu (which is used on all desktop platform)
        from Profile menu which is not used on Chrome OS.
    
        Bug: 934689
    
    commit ef75f6cddf70f78459048c6c0eb48919c2864b45
    Author: Ramin Halavati <rhalavati@chromium.org>
    Date:   Tue Apr 2 19:24:51 2019 +0000
    
        Separate Incognito menu from Profile menu.
    
        ProfileChooserView created all menus that are generated when user clicks
        on avatar menu. Except incognito menu, the other menus are not used in
        ChromeOS and have dependencies that are not supported on ChromeOS.
        To add the incognito menu to ChromeOS, this menu is separated into a
        different class.
    
        Bug: 934689

--- browser/ui/views/profiles/brave_profile_chooser_view_browsertest.cc
+++ browser/ui/views/profiles/brave_profile_chooser_view_browsertest.cc
@@ -1,44 +1,49 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
-* License, v. 2.0. If a copy of the MPL was not distributed with this file,
-* You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/browser/ui/views/profiles/brave_profile_chooser_view.h"
 
-#include "chrome/browser/profiles/avatar_menu.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/browser/ui/views/profiles/avatar_toolbar_button.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "ui/events/base_event_utils.h"
 #include "ui/events/event.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/views/controls/button/button.h"
-#include "ui/views/controls/button/label_button.h"
 
 class BraveProfileChooserViewTest : public InProcessBrowserTest {
  public:
   BraveProfileChooserViewTest() = default;
   ~BraveProfileChooserViewTest() override = default;
 
-protected:
+ protected:
   void OpenProfileChooserView(Browser* browser) {
     BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser);
-    views::View* button = browser_view->toolbar()->avatar_button();
+    AvatarToolbarButton* button =
+        browser_view->toolbar()->GetAvatarToolbarButton();
     DCHECK(button);
 
     ui::MouseEvent e(ui::ET_MOUSE_PRESSED, gfx::Point(), gfx::Point(),
-      ui::EventTimeForNow(), ui::EF_LEFT_MOUSE_BUTTON, 0);
+                     ui::EventTimeForNow(), ui::EF_LEFT_MOUSE_BUTTON, 0);
     button->OnMousePressed(e);
   }
 
   AvatarMenu* avatar_menu() {
-    return ProfileChooserView::profile_bubble_->avatar_menu_.get();
+    ProfileChooserView* bubble = static_cast<ProfileChooserView*>(
+        ProfileMenuViewBase::GetBubbleForTesting());
+    DCHECK(bubble);
+    return bubble->avatar_menu_.get();
   }
 
-  views::LabelButton* current_profile_card() {
-    return ProfileChooserView::profile_bubble_->current_profile_card_;
+  const base::string16& profile_button_name() const {
+    ProfileChooserView* bubble = static_cast<ProfileChooserView*>(
+        ProfileMenuViewBase::GetBubbleForTesting());
+    return bubble->current_profile_card_->GetAccessibleName();
   }
 
-private:
+ private:
   DISALLOW_COPY_AND_ASSIGN(BraveProfileChooserViewTest);
 };
 
@@ -46,6 +51,7 @@ IN_PROC_BROWSER_TEST_F(BraveProfileChooserViewTest, TestCurrentProfileView) {
   OpenProfileChooserView(browser());
 
   EXPECT_EQ(1u, avatar_menu()->GetNumberOfItems());
-  ASSERT_EQ(avatar_menu()->GetItemAt(0).name,
-            current_profile_card()->GetAccessibleName());
+  const AvatarMenu::Item& item = avatar_menu()->GetItemAt(0);
+  ASSERT_TRUE(!item.name.empty());
+  ASSERT_EQ(item.name, profile_button_name());
 }
