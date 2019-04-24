8e79fd913a1c62eda8a3315a048013e77f0eb7ce
brave-builds <devops@brave.com>
2019-04-24 17:49:49 +0000

    Uplift of #2328 (squashed) to beta

--- browser/extensions/brave_extension_management.cc
+++ browser/extensions/brave_extension_management.cc
@@ -1,22 +1,33 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/browser/extensions/brave_extension_management.h"
 
+#include <memory>
+
 #include "base/command_line.h"
 #include "brave/browser/brave_browser_process_impl.h"
 #include "brave/common/brave_switches.h"
 #include "brave/common/extensions/extension_constants.h"
+#include "brave/common/pref_names.h"
 #include "brave/browser/extensions/brave_extension_provider.h"
 #include "brave/browser/extensions/brave_tor_client_updater.h"
 #include "chrome/browser/extensions/external_policy_loader.h"
+#include "chrome/browser/profiles/profile.h"
+#include "components/prefs/pref_service.h"
+#include "extensions/browser/extension_registry.h"
+#include "extensions/common/extension.h"
 #include "extensions/common/extension_urls.h"
 
 namespace extensions {
 
 BraveExtensionManagement::BraveExtensionManagement(Profile* profile)
-    : ExtensionManagement(profile) {
+    : ExtensionManagement(profile),
+      extension_registry_observer_(this) {
+  extension_registry_observer_.Add(ExtensionRegistry::Get(
+        static_cast<content::BrowserContext*>(profile)));
   providers_.push_back(
       std::make_unique<BraveExtensionProvider>());
   RegisterBraveExtensions();
@@ -30,6 +41,21 @@ void BraveExtensionManagement::RegisterBraveExtensions() {
       *base::CommandLine::ForCurrentProcess();
   if (!command_line.HasSwitch(switches::kDisableTorClientUpdaterExtension))
     g_brave_browser_process->tor_client_updater()->Register();
+}
+
+void BraveExtensionManagement::OnExtensionLoaded(
+    content::BrowserContext* browser_context,
+    const Extension* extension) {
+  if (extension->id() == ipfs_companion_extension_id)
+    pref_service_->SetBoolean(kIPFSCompanionEnabled, true);
+}
+
+void BraveExtensionManagement::OnExtensionUnloaded(
+    content::BrowserContext* browser_context,
+    const Extension* extension,
+    UnloadedExtensionReason reason) {
+  if (extension->id() == ipfs_companion_extension_id)
+    pref_service_->SetBoolean(kIPFSCompanionEnabled, false);
 }
 
 }  // namespace extensions
