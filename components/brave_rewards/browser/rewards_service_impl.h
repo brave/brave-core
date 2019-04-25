0de48752811559a41813bb8ac8a23f07d0ca67d4
Terry Mancey <terry.mancey@icloud.com>
2019-04-25 15:11:25 +0100

    Fix the Ads rewards settings panel

--- components/brave_rewards/browser/rewards_service_impl.h
+++ components/brave_rewards/browser/rewards_service_impl.h
@@ -192,7 +192,6 @@ class RewardsServiceImpl : public RewardsService,
       const GetRewardsMainEnabledCallback& callback) const override;
 
   void GetAddressesForPaymentId(const GetAddressesCallback& callback) override;
-  std::pair<uint64_t, uint64_t> GetEarningsRange();
 
   const RewardsNotificationService::RewardsNotificationsMap&
   GetAllNotifications() override;
@@ -340,8 +339,8 @@ class RewardsServiceImpl : public RewardsService,
   void SetCatalogIssuers(const std::string& json) override;
   void ConfirmAd(const std::string& json) override;
   void SetConfirmationsIsReady(const bool is_ready) override;
-  void GetConfirmationsHistory(
-      brave_rewards::ConfirmationsHistoryCallback callback) override;
+  void GetTransactionHistoryForThisCycle(
+      GetTransactionHistoryForThisCycleCallback callback) override;
   void ConfirmationsTransactionHistoryDidChange() override;
 
   void OnExcludedSitesChanged(const std::string& publisher_id,
@@ -422,8 +421,8 @@ class RewardsServiceImpl : public RewardsService,
 
   // Mojo Proxy methods
   void OnPublisherBannerMojoProxy(const std::string& banner);
-  void OnGetConfirmationsHistory(
-      brave_rewards::ConfirmationsHistoryCallback callback,
+  void OnGetTransactionHistoryForThisCycle(
+      GetTransactionHistoryForThisCycleCallback callback,
       const std::string& transactions);
   void OnGetAllBalanceReports(
       const GetAllBalanceReportsCallback& callback,
