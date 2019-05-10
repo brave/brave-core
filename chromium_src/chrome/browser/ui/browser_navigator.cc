c7951e2f306061f81b07159b9dd2c2f459085351
Taylor `Riastradh' Campbell <taylor@brave.com>
2019-05-10 15:53:36 +0000

    WIP: Redirect .onion URLs to a private window with Tor, if possible.
    fix brave/brave-browser#806
    
    - If there is no private window with Tor open, fail.
    
      => TODO: This should fail noisily, beyond a console message.
    
      => One may be tempted to just create a private window with Tor, but:
         . This is not obviously easy to do here.
         . Users may not be happy if simply clicking a link can have the
           effect of using Tor when they didn't intend.
    
    - If there is a private window with Tor open, create a new tab to load
      the page.
    
      => User expectations around onion services are likely to be that
         they provide anonymity, so opening them in the non-anonymous
         window may be surprising.
    
      => It is not clear how easy it would be to load in a non-Tor window
         and ensure that everything is nevertheless loaded through Tor --
         and likewise links from it, &c.

--- chromium_src/chrome/browser/ui/browser_navigator.cc
+++ chromium_src/chrome/browser/ui/browser_navigator.cc
@@ -3,19 +3,39 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "brave/browser/profiles/brave_profile_manager.h"
 #include "brave/browser/renderer_host/brave_navigation_ui_data.h"
 #include "brave/common/webui_url_constants.h"
+#include "chrome/browser/browser_process.h"
+#include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
 #include "chrome/common/webui_url_constants.h"
 #include "url/gurl.h"
 
 namespace {
-void AdjustNavigateParamsForURLBraveImpl(NavigateParams* params) {
+bool AdjustNavigateParamsForURLBraveImpl(NavigateParams* params) {
   if (params->url.SchemeIs(content::kBraveUIScheme)) {
     GURL::Replacements replacements;
     replacements.SetSchemeStr(content::kChromeUIScheme);
     params->url = params->url.ReplaceComponents(replacements);
   }
+  if (params->url.SchemeIsHTTPOrHTTPS() &&
+      params->url.DomainIs("onion")) {
+    ProfileManager* profile_manager = g_browser_process->profile_manager();
+    Profile* profile = profile_manager->GetProfileByPath(
+        BraveProfileManager::GetTorProfilePath());
+    if (profile) {
+      Browser* browser = chrome::FindTabbedBrowser(profile, true);
+      DCHECK(browser);
+      params->disposition = WindowOpenDisposition::SINGLETON_TAB;
+      params->browser = browser;
+      params->window_action = NavigateParams::SHOW_WINDOW;
+    } else {
+      LOG(ERROR) << "Tried to load .onion without Tor ready";
+      return false;
+    }
+  }
+  return true;
 }
 
 bool IsHostAllowedInIncognitoBraveImpl(const base::StringPiece& host) {
