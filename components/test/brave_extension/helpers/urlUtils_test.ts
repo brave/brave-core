9877d6cde27e3f8bab653b355f439adbfa1bf010
Cezar Augusto <boss@cezaraugusto.net>
2019-05-10 18:17:19 -0300

    add tests for new urlutils methods

--- components/test/brave_extension/helpers/urlUtils_test.ts
+++ components/test/brave_extension/helpers/urlUtils_test.ts
@@ -2,7 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { isHttpOrHttps, hasPortNumber } from '../../../brave_extension/extension/brave_extension/helpers/urlUtils'
+import {
+  isHttpOrHttps,
+  hasPortNumber,
+  getOrigin,
+  getHostname,
+  stripProtocolFromUrl
+} from '../../../brave_extension/extension/brave_extension/helpers/urlUtils'
 
 describe('urlUtils test', () => {
   describe('isHttpOrHttps', () => {
@@ -48,6 +54,36 @@ describe('urlUtils test', () => {
       const url = 'http://brianbondy.com:8080'
       expect(hasPortNumber(url)).toBe(true)
       expect(isHttpOrHttps(url)).toBe(true)
+    })
+  })
+  describe('getOrigin', () => {
+    it('properly gets the origin from an URL', () => {
+      const url = 'https://pokemons-invading-tests-breaking-stuff.com/you-knew-that.js'
+      expect(getOrigin(url)).toBe('https://pokemons-invading-tests-breaking-stuff.com')
+    })
+  })
+  describe('getHostname', () => {
+    it('properly gets the hostname from an URL', () => {
+      const url = 'https://pokemons-invading-tests-breaking-stuff.com/you-knew-that.js'
+      expect(getHostname(url)).toBe('pokemons-invading-tests-breaking-stuff.com')
+    })
+  })
+  describe('stripProtocolFromUrl', () => {
+    it('properly strips out an HTTP protocol', () => {
+      const url = 'http://brave.com'
+      expect(stripProtocolFromUrl(url)).toBe('brave.com')
+    })
+    it('properly strips out an HTTPS protocol', () => {
+      const url = 'https://brave.com'
+      expect(stripProtocolFromUrl(url)).toBe('brave.com')
+    })
+    it('properly strips out an HTTP protocol when domain has HTTP as its name', () => {
+      const url = 'https://breakthis.http.com'
+      expect(stripProtocolFromUrl(url)).toBe('breakthis.http.com')
+    })
+    it('properly strips out an HTTPS protocol when domain has HTTPS as its name', () => {
+      const url = 'https://breakthis.https.com'
+      expect(stripProtocolFromUrl(url)).toBe('breakthis.https.com')
     })
   })
 })
