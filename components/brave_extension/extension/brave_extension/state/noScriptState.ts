4f7cd8442b89202b0d89fd6059f3529e1b5fc62c
Cezar Augusto <boss@cezaraugusto.net>
2019-05-10 22:57:04 -0300

    add noScriptState

---
+++ components/brave_extension/extension/brave_extension/state/noScriptState.ts
@@ -0,0 +1,178 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import { getActiveTabId, getActiveTabData } from './shieldsPanelState'
+import { filterNoScriptInfoByWillBlockState } from '../helpers/noScriptUtils'
+import { State, Tabs } from '../types/state/shieldsPannelState'
+import { getHostname } from '../helpers/urlUtils'
+import { NoScriptInfo } from '../types/other/noScriptInfo'
+
+/**
+ * Get NoScriptInfo initial state
+ * @param {State} state - The initial NoScriptState
+ * @returns {NoScriptInfo} The current NoScript data
+ */
+export const getNoScriptInfo = (state: State, tabId: number): NoScriptInfo | State => {
+  if ('noScriptInfo' in state.tabs[tabId] === false) {
+    state.tabs[tabId].noScriptInfo = {}
+  }
+  return state.tabs[tabId].noScriptInfo
+}
+
+ /**
+  * Set NoScriptInfo modified state
+  * @param {State} state - The Application state
+  * @param {tabId} number - The current tab ID
+  * @param {string} url - The current script URL
+  * @param {object} modifiedInfo - The current script URL object data to be modified
+  * @returns {State} state - The modified application state
+  */
+export const modifyNoScriptInfo = (state: State, tabId: number, url: string, modifiedInfo: {}): State => {
+  const tabs: Tabs = {
+    ...state.tabs,
+    [tabId]: {
+      ...state.tabs[tabId],
+      noScriptInfo: {
+        ...state.tabs[tabId].noScriptInfo,
+        [url]: {
+          ...state.tabs[tabId].noScriptInfo[url],
+          ...modifiedInfo
+        }
+      }
+    }
+  }
+
+  return { ...state, tabs }
+}
+
+/**
+ * Reset NoScriptInfo to its initial state
+ * @param {State} state - The Application state
+ * @param {number} tabId - The current tab ID
+ * @returns {State} The modified application state
+ */
+export const resetNoScriptInfo = (state: State, tabId: number, newOrigin: string): State => {
+  const tabs: Tabs = state.tabs
+
+  if (newOrigin !== tabs[tabId].origin) { // navigate away
+    tabs[tabId].noScriptInfo = {}
+  }
+
+  const noScriptInfo = getNoScriptInfo(state, tabId)
+  const allScripts = Object.entries(noScriptInfo)
+
+  for (const [url] of allScripts) {
+    // only keep entries which users want to allow
+    if (tabs[tabId].noScriptInfo[url].willBlock) {
+      delete tabs[tabId].noScriptInfo[url]
+    }
+    const groupedScriptsBlockedState = {
+      userInteracted: false,
+      actuallyBlocked: noScriptInfo[url].actuallyBlocked,
+      willBlock: noScriptInfo[url].willBlock
+    }
+    state = modifyNoScriptInfo(state, tabId, url, groupedScriptsBlockedState)
+  }
+  return state
+}
+
+export const setAllowNoScriptOriginsOnce = (state: State, origins?: Array<string>) => {
+  const tabData = getActiveTabData(state)
+  if (!tabData) {
+    console.error('Active tab not found')
+    return
+  }
+  const noScriptInfo = getNoScriptInfo(state, getActiveTabId(state))
+
+   // if no group of origins is defined, allow all origins
+  if (origins === undefined) {
+    const allOrigins = Object.keys(noScriptInfo)
+    origins = allOrigins.filter(key => noScriptInfo[key].willBlock === false)
+  }
+}
+
+ /**
+  * Reset NoScriptInfo to its initial state
+  * @param {State} state - The Application state
+  * @param {string} url - The current script URL
+  * @returns {State} The modified application state
+  */
+export const setScriptBlockedCurrentState = (state: State, url: string): State => {
+  const tabId: number = getActiveTabId(state)
+  const noScriptInfo = getNoScriptInfo(state, tabId)
+
+  const scriptBlockedState = {
+    userInteracted: true,
+    willBlock: !noScriptInfo[url].willBlock
+  }
+  state = modifyNoScriptInfo(state, tabId, url, scriptBlockedState)
+  return state
+}
+
+/**
+ * Set all scripts in a group to be either blocked or allowed
+ * @param {State} state - The Application state
+ * @param {string} hostname - The current script URL hostname
+ * @returns {State} The modified application state
+ */
+export const setGroupedScriptsBlockedCurrentState = (state: State, hostname: string, maybeBlock: boolean): State => {
+  const tabId: number = getActiveTabId(state)
+  const noScriptInfo = getNoScriptInfo(state, tabId)
+  const groupedScripts = Object.entries(noScriptInfo)
+
+  for (const [url] of filterNoScriptInfoByWillBlockState(groupedScripts, maybeBlock)) {
+    const groupedScriptsBlockedState = {
+      userInteracted: true,
+      willBlock: !noScriptInfo[url].willBlock
+    }
+
+    if (hostname === getHostname(url)) {
+      state = modifyNoScriptInfo(state, tabId, url, groupedScriptsBlockedState)
+    }
+  }
+  return state
+}
+
+/**
+ * Set all scripts to be either blocked or allowed
+ * @param {State} state - The Application state
+ * @returns {State} The modified application state
+ */
+export const setAllScriptsBlockedCurrentState = (state: State, maybeBlock: boolean): State => {
+  const tabId: number = getActiveTabId(state)
+  const noScriptInfo = getNoScriptInfo(state, tabId)
+  const allBlockedScripts = Object.entries(noScriptInfo)
+
+  for (const [url] of filterNoScriptInfoByWillBlockState(allBlockedScripts, !maybeBlock)) {
+    const groupedScriptsBlockedState = {
+      userInteracted: true,
+      willBlock: maybeBlock
+    }
+    state = modifyNoScriptInfo(state, tabId, url, groupedScriptsBlockedState)
+  }
+  return state
+}
+
+/**
+ * Set all scripts to be either blocked or allowed after user finish
+ * @param {State} state - The Application state
+ * @returns {State} The modified application state
+ */
+export const setFinalScriptsBlockedState = (state: State): State => {
+  const tabId: number = getActiveTabId(state)
+  const noScriptInfo = getNoScriptInfo(state, tabId)
+  const allScripts = Object.entries(noScriptInfo)
+
+  for (const [url] of allScripts) {
+    // `willBlock` set the state once user close Shields panel moving blocked/allowed resources
+    // to their respective lists. In this case we copy data from actuallyBlocked and apply
+    // the sate state to willBlock so users can see their scripts list updated.
+    const groupedScriptsBlockedState = {
+      userInteracted: false,
+      willBlock: noScriptInfo[url].actuallyBlocked
+    }
+    state = modifyNoScriptInfo(state, tabId, url, groupedScriptsBlockedState)
+  }
+  return state
+}
