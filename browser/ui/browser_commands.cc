b7889d94dd97407cc361110e0466cc43ff07db95
Taylor `Riastradh' Campbell <taylor@brave.com>
2019-05-11 13:43:48 +0000

    WIP: Toss cookies and other browsing data for New Tor Identity.
    fix brave/brave-browser#3872

--- browser/ui/browser_commands.cc
+++ browser/ui/browser_commands.cc
@@ -1,8 +1,11 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/browser/ui/browser_commands.h"
+
+#include <utility>
 
 #include "brave/browser/tor/tor_profile_service.h"
 #include "brave/browser/tor/tor_profile_service_factory.h"
@@ -13,6 +16,7 @@
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_controller.h"
+#include "content/public/browser/storage_partition.h"
 #include "content/public/browser/web_contents.h"
 
 using content::BrowserThread;
@@ -20,9 +24,29 @@ using content::NavigationController;
 using content::WebContents;
 
 namespace {
-void NewTorIdentityCallback(WebContents* current_tab) {
+void NewTorIdentityCallbackDone(WebContents* current_tab) {
   NavigationController& controller = current_tab->GetController();
   controller.Reload(content::ReloadType::BYPASSING_CACHE, true);
+}
+
+void NewTorIdentityCallback(WebContents* current_tab) {
+  auto* context = current_tab->GetBrowserContext();
+  auto* site = current_tab->GetSiteInstance();
+  auto* partition =
+    content::BrowserContext::GetStoragePartition(context, site);
+  auto cookie_delete_filter = network::mojom::CookieDeletionFilter::New();
+  // Delete all cookies, irrespective of persistence status.
+  cookie_delete_filter->session_control =
+    network::mojom::CookieDeletionSessionControl::IGNORE_CONTROL;
+  partition->ClearData(
+      content::StoragePartition::REMOVE_DATA_MASK_ALL,
+      content::StoragePartition::QUOTA_MANAGED_STORAGE_MASK_ALL,
+      content::StoragePartition::OriginMatcherFunction(),
+      std::move(cookie_delete_filter),
+      /*perform_cleanup*/ true,
+      /*begin*/ base::Time(),           // beginning of time
+      /*end*/ base::Time::Max(),        // end of time
+      base::Bind(&NewTorIdentityCallbackDone, current_tab));
 }
 }  // namespace
 
