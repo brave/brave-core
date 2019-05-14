122071cfb5db360ad9a67d75b264614eefe3bf52
Mihai Plesa <mplesa@brave.com>
2019-05-14 22:19:58 +0100

    multiple ci pipeline improvements

--- script/rust_deps_config.py
+++ script/rust_deps_config.py
@@ -4,5 +4,8 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 # Version number and URL for pre-configured rust dependency package, e.g. rust_deps_mac_0.1.0.gz
+# TODO: set to value below after migrating the script to Python 3
+# it now breaks because of urllib2 and SSL/TLS incompatibility when using Fastly
+# RUST_DEPS_PACKAGES_URL = "https://rust-pkg-brave-core.s3.brave.com"
 RUST_DEPS_PACKAGES_URL = "https://s3-us-west-2.amazonaws.com/rust-pkg-brave-core"
 RUST_DEPS_PACKAGE_VERSION = "0.1.1"
