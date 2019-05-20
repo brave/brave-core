bcb1a2caa978dac5229b9f1249dc64d2992d1b09
Albert Wang <amwang217@gmail.com>
2019-05-20 09:53:32 -0700

    Remove noise from ads_service.h

--- components/brave_ads/browser/ads_service.h
+++ components/brave_ads/browser/ads_service.h
@@ -22,10 +22,11 @@ class AdsService : public KeyedService {
   AdsService() = default;
 
   virtual bool IsSupportedRegion() const = 0;
-  virtual bool IsAdsEnabled() const = 0;
-  virtual uint64_t GetAdsPerHour() const = 0;
 
+  virtual bool IsAdsEnabled() const = 0;
   virtual void SetAdsEnabled(bool enabled) = 0;
+
+  virtual uint64_t GetAdsPerHour() const = 0;
   virtual void SetAdsPerHour(uint64_t ads_per_hour) = 0;
 
   // ads::Ads proxy
