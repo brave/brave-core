43d3a54b1fca3f0a3cef756ef46de48dd1bb4a8c
Michał Buczek <michal.buczek@protonmail.com>
2019-05-20 19:20:53 +0200

    Fix brave/brave-ios#1096: App crashes when opening websites. (brave/brave-ios#1097)

--- Client/WebFilters/ShieldStats/Adblock/AdBlockStats.swift
+++ Client/WebFilters/ShieldStats/Adblock/AdBlockStats.swift
@@ -84,10 +84,6 @@ class AdBlockStats: LocalAdblockResourceProtocol {
     }
     
     func shouldBlock(_ request: URLRequest) -> Bool {
-        // synchronize code from this point on.
-        objc_sync_enter(self)
-        defer { objc_sync_exit(self) }
-        
         guard let url = request.url else {
             return false
         }
