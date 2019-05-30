4a0aeddae9b48cffc0019c6874df0ae0b7a272ed
bridiver <github@brianjohnson.cc>
2019-05-30 16:40:39 -0700

    use xcode-select to find xcode

--- vendor/brave-ios/lib/compile-model.py
+++ vendor/brave-ios/lib/compile-model.py
@@ -3,13 +3,15 @@ import os
 import subprocess
 import shutil
 import sys
+import string
 
 def main():
     args = parse_args()
     compile_model(args.model[0], args.output[0])
 
 def compile_model(model, output):
-    subprocess.call("/Applications/Xcode.app/Contents/Developer/usr/bin/momc " + model + " " + output, shell=True)
+    xcode = subprocess.check_output(['xcode-select', '-print-path'])
+    subprocess.call(string.strip(xcode) + "/usr/bin/momc " + model + " " + output, shell=True)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Compile a CoreData model')
