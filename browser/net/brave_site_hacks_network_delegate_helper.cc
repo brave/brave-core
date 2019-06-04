42f559e6d9cf31f36a36b189618262b80205aafb
Brian Clifton <brian@clifton.me>
2019-06-04 00:54:06 -0700

    Lint fixes

--- browser/net/brave_site_hacks_network_delegate_helper.cc
+++ browser/net/brave_site_hacks_network_delegate_helper.cc
@@ -1,9 +1,11 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/browser/net/brave_site_hacks_network_delegate_helper.h"
 
+#include <memory>
 #include <string>
 
 #include "base/sequenced_task_runner.h"
@@ -101,7 +103,8 @@ int OnBeforeStartTransaction_SiteHacksWork(net::URLRequest* request,
   if (IsUAWhitelisted(request->url())) {
     std::string user_agent;
     if (headers->GetHeader(kUserAgentHeader, &user_agent)) {
-      base::ReplaceFirstSubstringAfterOffset(&user_agent, 0, "Chrome", "Brave Chrome");
+      base::ReplaceFirstSubstringAfterOffset(&user_agent, 0,
+        "Chrome", "Brave Chrome");
       headers->SetHeader(kUserAgentHeader, user_agent);
     }
   }
