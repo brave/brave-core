380352c0d94851f5127633ee286d92f77c7fed11
Simon Hong <shong@igalia.com>
2019-06-07 11:20:59 +0900

    Update extension install prompt title
    Improved the warning message so it doesn't send the wrong message to the user.

--- browser/extensions/brave_extension_install_prompt.cc
+++ browser/extensions/brave_extension_install_prompt.cc
@@ -1,28 +1,20 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/browser/extensions/brave_extension_install_prompt.h"
 
-#include "base/strings/utf_string_conversions.h"
 #include "brave/browser/extensions/brave_extension_provider.h"
 #include "brave/grit/brave_generated_resources.h"
-#include "extensions/common/extension_id.h"
 #include "ui/base/l10n/l10n_util.h"
-
-BravePrompt::BravePrompt(ExtensionInstallPrompt::PromptType type) :
-    ExtensionInstallPrompt::Prompt(type) {
-}
-
-BravePrompt::~BravePrompt() {
-}
 
 base::string16 BravePrompt::GetDialogTitle() const {
   if (!extensions::BraveExtensionProvider::IsVetted(extension())) {
     if (type_ == ExtensionInstallPrompt::INSTALL_PROMPT ||
         type_ == ExtensionInstallPrompt::WEBSTORE_WIDGET_PROMPT) {
-      return l10n_util::GetStringFUTF16(IDS_UNVETTED_EXTENSION_INSTALL_PROMPT_TITLE,
-          base::UTF8ToUTF16(extension_->name()));
+      return l10n_util::GetStringUTF16(
+          IDS_UNVETTED_EXTENSION_INSTALL_PROMPT_TITLE);
     }
   }
   return ExtensionInstallPrompt::Prompt::GetDialogTitle();
