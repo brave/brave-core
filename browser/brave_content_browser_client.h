4c05c325417dc925305be724658584005bfdeab2
Max Karolinskiy <max@brave.com>
2019-06-12 15:30:14 -0400

    Removed packaged services from BraveContentBrowserClient.
    - Packaged services have been removed and are now called builtin services.
    Our services are no longer added via an overlay, but instead in the
    GetExtraServiceManifests callback.
    
    - The builtin services are marked as out-of-process, added the same
    designation to our services in their manifests via the
    ExecutionMode::kOutOfProcessBuiltin option.
    
    -Removed service registration as it's no longer used.
    
    Chromium changes:
    
    commit 06fd283a53441cff85ce452e0de258a2a96d94aa
    Author: Ken Rockot <rockot@google.com>
    Date:   Fri May 24 17:24:44 2019 +0000
    
        Remove content_packaged_services
    
        This removes content_packaged_services entirely, switching instead to a
        direct delegate interface on Service Manager, allowing the Content
        layer to directly field service process launching requests.
    
        Follow-up CLs will further simplify in- and out-of-process service
        configuration, and this is ultimately a step towards migrating process
        launch out of Content and into Service Manager and removing other things
        like content::ServiceManagerConnection.
    
        Bug: 781334,895615
    
    commit fa5d57115b51a77be5d8e5757a3758e0db51cf05
    Author: Ken Rockot <rockot@google.com>
    Date:   Wed May 29 20:57:14 2019 +0000
    
        Simplify out-of-process service registration
    
        This eliminates the need to register out-of-process services at the
        Content layer, separate from their manifests. Display names for service
        processes are taken directly from the manifest now, including support
        for string resource IDs resolved to localized strings by the Service
        Manager at runtime.
    
        Bug: 915806, 895615

--- browser/brave_content_browser_client.h
+++ browser/brave_content_browser_client.h
@@ -58,11 +58,13 @@ class BraveContentBrowserClient : public ChromeContentBrowserClient {
       const url::Origin& requesting_origin,
       const url::Origin& embedding_origin) override;
 
-  void RegisterOutOfProcessServices(
-      OutOfProcessServiceMap* services) override;
-
+  void RunServiceInstance(
+      const service_manager::Identity& identity,
+      mojo::PendingReceiver<service_manager::mojom::Service>* receiver)
+      override;
   base::Optional<service_manager::Manifest> GetServiceManifestOverlay(
       base::StringPiece name) override;
+  std::vector<service_manager::Manifest> GetExtraServiceManifests() override;
 
   void AdjustUtilityServiceProcessCommandLine(
       const service_manager::Identity& identity,
