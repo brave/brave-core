0bff896aae756ce2047f8baaaa3e1b8721d245de
Jocelyn Liu <yrliou@gmail.com>
2019-06-17 14:51:47 -0700

    Destroy Tor profile if needed in ~Browser()

--- chromium_src/chrome/browser/ui/browser.cc
+++ chromium_src/chrome/browser/ui/browser.cc
@@ -9,6 +9,11 @@
 #include "brave/browser/ui/brave_browser_command_controller.h"
 #include "brave/browser/ui/toolbar/brave_location_bar_model_delegate.h"
 
+class Profile;
+namespace brave {
+void MaybeDestroyTorProfile(Profile* const profile);
+}  // namespace brave
+
 #define BrowserContentSettingBubbleModelDelegate \
   BraveBrowserContentSettingBubbleModelDelegate
 #define BrowserCommandController BraveBrowserCommandController
@@ -17,3 +22,27 @@
 #undef BrowserLocationBarModelDelegate
 #undef BrowserContentSettingBubbleModelDelegate
 #undef BrowserCommandController
+
+namespace brave {
+
+void MaybeDestroyTorProfile(Profile* const profile) {
+  if (!profile || !profile->IsTorProfile() ||
+      BrowserList::IsIncognitoSessionInUse(profile)) {
+    return;
+  }
+
+#if BUILDFLAG(ENABLE_PRINT_PREVIEW)
+  // The Printing Background Manager holds onto preview dialog WebContents
+  // whose corresponding print jobs have not yet fully spooled. Make sure
+  // these get destroyed before tearing down the incognito profile so that
+  // their render frame hosts can exit in time - see crbug.com/579155
+  g_browser_process->background_printing_manager()
+    ->DeletePreviewContentsForBrowserContext(profile);
+#endif
+  // A tor profile is no longer needed, this indirectly frees
+  // its cache and cookies once it gets destroyed at the appropriate time.
+  ProfileDestroyer::DestroyProfileWhenAppropriate(profile);
+  return;
+}
+
+}  // namespace brave
