b8087a36ad09aeba8924709314b6530cda7e6eea
Peter Snyder <snyderp@gmail.com>
2019-06-20 08:27:14 -0700

    fix for issue where resources fetched from different URLs are cached and handled with the same blink::request

--- third_party/blink/brave_page_graph/utilities/dispatchers.cc
+++ third_party/blink/brave_page_graph/utilities/dispatchers.cc
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "brave/third_party/blink/brave_page_graph/dispatchers.h"
+#include "brave/third_party/blink/brave_page_graph/utilities/dispatchers.h"
 #include "third_party/blink/renderer/core/dom/dom_node_ids.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
