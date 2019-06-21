c842e5e53432714a81b34df7ac3fca0948135637
Max Karolinskiy <max@brave.com>
2019-06-21 13:21:42 -0400

    kAutofillSaveCardSignInAfterLocalSave feature toggle has been removed.
    The feature is always on now, so we need to make sure not to show a
    propmt suggesting so sign in.
    
    Patched SaveCardBubbleControllerImpl::ShouldShowSignInPromo to always
    return false. Added browser test to verify that the bubble is not shown.
    
    Chromium change:
    
    commit ffdc2163c8e00148123a58f61f9f5e6bb28bad0a
    Author: Jared Saul <jsaul@google.com>
    Date:   Tue Apr 23 20:52:53 2019 +0000
    
        [Autofill] Clean up Sign in after local save flag
    
        Bug: 855182

---
+++ chromium_src/chrome/browser/ui/autofill/payments/brave_save_card_bubble_controller_impl_browsertest.cc
@@ -0,0 +1,45 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/autofill/payments/save_card_bubble_controller_impl.h"
+
+#include <memory>
+
+#include "base/macros.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/ui_test_utils.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+class BraveSaveCardBubbleControllerImplTest : public InProcessBrowserTest {
+ public:
+  BraveSaveCardBubbleControllerImplTest() {}
+
+  void ShowUi() {
+    content::WebContents* web_contents =
+        browser()->tab_strip_model()->GetActiveWebContents();
+
+    // Do lazy initialization of SaveCardBubbleControllerImpl.
+    autofill::SaveCardBubbleControllerImpl::CreateForWebContents(web_contents);
+    controller_ =
+        autofill::SaveCardBubbleControllerImpl::FromWebContents(web_contents);
+    DCHECK(controller_);
+    controller_->ShowBubbleForSignInPromo();
+  }
+
+  autofill::SaveCardBubbleControllerImpl* controller() { return controller_; }
+
+ private:
+  autofill::SaveCardBubbleControllerImpl* controller_ = nullptr;
+
+  DISALLOW_COPY_AND_ASSIGN(BraveSaveCardBubbleControllerImplTest);
+};
+
+// Tests that requesting to open signin promo bubble doesn't result in the
+// bubble being shown.
+IN_PROC_BROWSER_TEST_F(BraveSaveCardBubbleControllerImplTest, NoSignInPromo) {
+  ShowUi();
+  EXPECT_EQ(nullptr, controller()->save_card_bubble_view());
+}
