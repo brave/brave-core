2b424e0b62f92dc996c94f8c03f0dda611282264
Max Karolinskiy <max@brave.com>
2019-06-22 13:33:12 -0400

    ProxyResolutionService::ResetConfigService was removed.
    We still need it for TorProxyConfigService::TorSetProxy, so adding it
    back.
    
    Chromium change:
    
    commit d44ca309d337c37c778002c13b6ecf5ff6036fdd
    Author: Matt Menke <mmenke@chromium.org>
    Date:   Wed May 29 22:07:33 2019 +0000
    
        Remove ProxyResolutionService::ResetConfigService.
    
        This method isn't used in production code. It is used by
        chromeos/services/assistant/default_url_request_context_getter.h,
        which isn't used anywhere, since the network service is being used
        instead.  This CL also removes that class.
    
        Bug: 910366, 878582

---
+++ chromium_src/net/proxy_resolution/proxy_resolution_service.h
@@ -0,0 +1,20 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_NET_PROXY_RESOLUTION_PROXY_RESOLUTION_SERVICE_H_
+#define BRAVE_CHROMIUM_SRC_NET_PROXY_RESOLUTION_PROXY_RESOLUTION_SERVICE_H_
+
+// ResetConfigService was removed in c76 in commit
+// d44ca309d337c37c778002c13b6ecf5ff6036fdd, but we still need it.
+#define BRAVE_PROXY_RESOLUTION_SERVICE_METHODS \
+  void ResetConfigService(                     \
+      std::unique_ptr<ProxyConfigService> new_proxy_config_service);
+
+#include "../../../net/proxy_resolution/proxy_resolution_service.h"
+#undef BRAVE_PROXY_RESOLUTION_SERVICE_METHODS
+
+#endif  // BRAVE_CHROMIUM_SRC_NET_PROXY_RESOLUTION_PROXY_RESOLUTION_SERVICE_H_
+
+
