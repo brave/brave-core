41ec39e9ec74a85de15c4a49520e101d67458c1a
Max Karolinskiy <max@brave.com>
2019-06-22 13:33:12 -0400

    ProxyResolutionService::ResetConfigService was removed.
    We still need it for TorProxyConfigService::TorSetProxy, so adding it
    back.
    
    Chromium change:
    
    commit d44ca309d337c37c778002c13b6ecf5ff6036fdd
    Author: Matt Menke <mmenke@chromium.org>
    Date:   Wed May 29 22:07:33 2019 +0000
    
        Remove ProxyResolutionService::ResetConfigService.
    
        This method isn't used in production code. It is used by
        chromeos/services/assistant/default_url_request_context_getter.h,
        which isn't used anywhere, since the network service is being used
        instead.  This CL also removes that class.
    
        Bug: 910366, 878582

---
+++ chromium_src/net/proxy_resolution/proxy_resolution_service.cc
@@ -0,0 +1,22 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "../../../net/proxy_resolution/proxy_resolution_service.cc"  // NOLINT
+
+namespace net {
+void ProxyResolutionService::ResetConfigService(
+    std::unique_ptr<ProxyConfigService> new_proxy_config_service) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  State previous_state = ResetProxyConfig(true);
+  // Release the old configuration service.
+  if (config_service_.get())
+    config_service_->RemoveObserver(this);
+  // Set the new configuration service.
+  config_service_ = std::move(new_proxy_config_service);
+  config_service_->AddObserver(this);
+  if (previous_state != STATE_NONE)
+    ApplyProxyConfigIfAvailable();
+}
+}  // namespace net
