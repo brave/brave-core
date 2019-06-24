874b16eedcf0d35527b544c6b7bc666d86f61808
Francois Marier <francois@brave.com>
2019-06-24 13:52:19 -0700

    WIP: switch to an override

--- chromium_src/extensions/common/permissions/permissions_data.cc
+++ chromium_src/extensions/common/permissions/permissions_data.cc
@@ -3,40 +3,37 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#define CanRunOnPage CanRunOnPage_ChromiumImpl
-#include "../../../../../extensions/common/permissions/permissions_data.cc"
-#undef CanRunOnPage
-
 #include "brave/extensions/common/brave_extension_urls.h"
+#include "extensions/common/permissions/permissions_data.h"
+#include "url/origin.h"
 
-namespace extensions {
+namespace {
 
-PermissionsData::PageAccess PermissionsData::CanRunOnPage(
-    const GURL& document_url,
-    int tab_id,
-    const URLPatternSet& permitted_url_patterns,
-    const URLPatternSet& withheld_url_patterns,
-    const URLPatternSet* tab_url_patterns,
-    std::string* error) const {
-  return PageAccess::kDenied;  // TODO: remove this!
-  PermissionsData::PageAccess access = CanRunOnPage_ChromiumImpl(document_url,
-      tab_id, permitted_url_patterns, withheld_url_patterns, tab_url_patterns,
-      error);
-  if (access != PageAccess::kAllowed) {
-    return access;
-  }
+const char kCannotScriptWalletLinking[] =
+    "Pages part of the wallet linking flow cannot be scripted without user "
+    "interaction.";
 
-  if (CanExecuteScriptEverywhere(extension_id_, location_)) {
-    return PageAccess::kAllowed;
+bool IsBraveRestrictedUrl(const GURL& document_url,
+    const extensions::ExtensionId& extension_id,
+    extensions::Manifest::Location location,
+    std::string* error) {
+
+  if (extensions::PermissionsData::CanExecuteScriptEverywhere(extension_id,
+                                                              location)) {
+    return false;
   }
 
   if (extension_urls::IsBraveProtectedUrl(url::Origin::Create(document_url),
                                           document_url.path_piece())) {
-    // Disable content scripts until users click on the extension.
-    return PageAccess::kWithheld;
+    if (error) {
+      *error = kCannotScriptWalletLinking;
+    }
+    return true;
   }
 
-  return PageAccess::kAllowed;
+  return false;
 }
 
-}  // namespace extensions
+}  // namespace
+
+#include "../../../../../extensions/common/permissions/permissions_data.cc"
