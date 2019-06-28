056f47a71d1833f8dd8a332a272db6f03218c488
samartnik <artem@brave.com>
2019-06-28 16:28:35 -0400

    Disabled NTP suggestions and FTR experience

---
+++ android/java/org/chromium/chrome/browser/document/BraveLauncherActivity.java
@@ -0,0 +1,27 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.chromium.chrome.browser.document;
+
+import android.app.Activity;
+import android.os.Bundle;
+import org.chromium.base.CommandLine;
+import org.chromium.base.Log;
+import org.chromium.chrome.browser.ChromeSwitches;
+import org.chromium.chrome.browser.firstrun.FirstRunStatus;
+
+/**
+ * Base class for ChromeLauncherActivity
+ */
+public class BraveLauncherActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Disable FTR experience
+        CommandLine.getInstance().appendSwitch(ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE);
+        FirstRunStatus.setFirstRunFlowComplete(true);
+    }
+}
