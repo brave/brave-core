172dff94aff08dfabe15ff3c4d88a3f25c2c03e1
Francois Marier <francois@brave.com>
2019-07-05 17:23:09 -0700

    WIP: switch to subclassing ChromeExtensionsAPIClient

--- chromium_src/chrome/browser/extensions/chrome_extensions_browser_client.cc
+++ chromium_src/chrome/browser/extensions/chrome_extensions_browser_client.cc
@@ -1,9 +1,15 @@
-// Copyright (c) 2019 The Brave Authors. All rights reserved.
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+#include "brave/browser/extensions/api/brave_extensions_api_client.h"
 #include "brave/browser/extensions/updater/brave_update_client_config.h"
 #include "chrome/browser/extensions/chrome_extensions_browser_client.h"
 #include "chrome/browser/extensions/updater/chrome_update_client_config.h"
 
 #define ChromeUpdateClientConfig BraveUpdateClientConfig
+#define ChromeExtensionsAPIClient BraveExtensionsAPIClient
 #include "../../../../../chrome/browser/extensions/chrome_extensions_browser_client.cc"  // NOLINT
 #undef ChromeUpdateClientConfig
+#undef ChromeExtensionsAPIClient
