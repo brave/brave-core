6574eeefa07025c9523e7c7627ec0a867f097b60
Francois Marier <francois@brave.com>
2019-07-09 17:57:34 -0700

    WIP: switch to subclassing PermissionsData

---
+++ chromium_src/extensions/common/extension.cc
@@ -0,0 +1,10 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "../../../extensions/common/permissions/brave_permissions_data.h"
+
+#define PermissionsData BravePermissionsData
+#include "../../../../../extensions/common/extension.cc"
+#undef PermissionsData
