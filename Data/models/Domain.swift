beca4fa57764c36e3d3d393110a8e285ba43deb7
Joel Reis <joelhreis@gmail.com>
2019-07-16 17:00:16 -0400

    Fix #1192: HTTPSE shield setting is now scheme independent. (#1256)

--- Data/models/Domain.swift
+++ Data/models/Domain.swift
@@ -23,6 +23,10 @@ public final class Domain: NSManagedObject, CRUD {
     @NSManaged public var historyItems: NSSet?
     @NSManaged public var bookmarks: NSSet?
     
+    private var urlComponents: URLComponents? {
+        return URLComponents(string: url ?? "")
+    }
+    
     // MARK: - Public interface
     
     public class func getOrCreate(forUrl url: URL) -> Domain {
@@ -80,7 +84,7 @@ public final class Domain: NSManagedObject, CRUD {
             }
             
             for domain in httpDomains {
-                guard let urlString = domain.url, var urlComponents = URLComponents(string: urlString) else { continue }
+                guard var urlComponents = domain.urlComponents else { continue }
                 urlComponents.scheme = "https"
                 guard let httpsUrl = urlComponents.url?.absoluteString else { continue }
                 if let httpsDomain = Domain.first(where: NSPredicate(format: "url == %@", httpsUrl), context: context) {
@@ -199,7 +203,12 @@ extension Domain {
         switch shield {
         case .AllOff: shield_allOff = setting
         case .AdblockAndTp: shield_adblockAndTp = setting
-        case .HTTPSE: shield_httpse = setting
+        case .HTTPSE:
+          shield_httpse = setting
+            
+          // HTTPSE must be scheme indepedent or user may get stuck not being able to access the http version
+          //  of a website (turning off httpse for an upgraded-https domain does not allow access to http version)
+          self.domainForInverseHttpScheme(context: context)?.shield_httpse = setting
         case .SafeBrowsing: shield_safeBrowsing = setting
         case .FpProtection: shield_fpProtection = setting
         case .NoScript: shield_noScript = setting
@@ -255,5 +264,24 @@ extension Domain {
             return domain.getBraveShield(shield)
         }
         return shield.globalPreference
+    }
+    
+    /// Returns `url` but switches the scheme from `http` <-> `https`
+    private func domainForInverseHttpScheme(context: NSManagedObjectContext) -> Domain? {
+        
+        guard var urlComponents = self.urlComponents else { return nil }
+        
+        // Flip the scheme if valid
+        
+        switch urlComponents.scheme {
+        case "http": urlComponents.scheme = "https"
+        case "https": urlComponents.scheme = "http"
+        default: return nil
+        }
+        
+        guard let url = urlComponents.url else { return nil }
+        
+        // Return the flipped scheme version of `url`
+        return Domain.getOrCreateInternal(url, context: context, save: true)
     }
 }
--- DataTests/DomainTests.swift
+++ DataTests/DomainTests.swift
@@ -4,19 +4,24 @@
 
 import XCTest
 import CoreData
+import BraveShared
 @testable import Data
 
 class DomainTests: CoreDataTestCase {
     let fetchRequest = NSFetchRequest<Domain>(entityName: String(describing: Domain.self))
+    
+    // Should match but with different schemes
+    let url = URL(string: "http://example.com")!
+    let urlHTTPS = URL(string: "https://example.com")!
+    
+    let url2 = URL(string: "http://brave.com")!
+    let url2HTTPS = URL(string: "https://brave.com")!
     
     private func entity(for context: NSManagedObjectContext) -> NSEntityDescription {
         return NSEntityDescription.entity(forEntityName: String(describing: Domain.self), in: context)!
     }
     
     func testGetOrCreate() {
-        let url = URL(string: "http://example.com")!
-        let url2 = URL(string: "http://brave.com")!
-        
         XCTAssertNotNil(Domain.getOrCreate(forUrl: url))
         XCTAssertEqual(try! DataController.viewContext.count(for: fetchRequest), 1)
         
@@ -28,6 +33,103 @@ class DomainTests: CoreDataTestCase {
         XCTAssertNotNil(Domain.getOrCreate(forUrl: url2))
         XCTAssertEqual(try! DataController.viewContext.count(for: fetchRequest), 2)
     }
+    
+    func testGetOrCreateURLs() {
+        // This also validates that the schemes are being correctly saved
+        XCTAssertEqual(url.absoluteString, Domain.getOrCreate(forUrl: url).url)
+        XCTAssertEqual(url2.absoluteString, Domain.getOrCreate(forUrl: url2).url)
+        
+        let url3 = URL(string: "https://brave.com")!
+        let url4 = URL(string: "data://brave.com")!
+        XCTAssertEqual(url3.absoluteString, Domain.getOrCreate(forUrl: url3).url)
+        XCTAssertEqual(url4.absoluteString, Domain.getOrCreate(forUrl: url4).url)
+        XCTAssertEqual(try! DataController.viewContext.count(for: fetchRequest), 4)
+    }
+    
+    func testDefaultShieldSettings() {
+        
+        let domain = Domain.getOrCreate(forUrl: url)
+        XCTAssertTrue(domain.isShieldExpected(BraveShield.AdblockAndTp, isPrivateBrowsing: true))
+        XCTAssertTrue(domain.isShieldExpected(BraveShield.HTTPSE, isPrivateBrowsing: true))
+        XCTAssertTrue(domain.isShieldExpected(BraveShield.SafeBrowsing, isPrivateBrowsing: true))
+        XCTAssertFalse(domain.isShieldExpected(BraveShield.AllOff, isPrivateBrowsing: true))
+        XCTAssertFalse(domain.isShieldExpected(BraveShield.NoScript, isPrivateBrowsing: true))
+        XCTAssertFalse(domain.isShieldExpected(BraveShield.FpProtection, isPrivateBrowsing: true))
+        
+        XCTAssertEqual(domain.bookmarks?.count, 0)
+        XCTAssertEqual(domain.historyItems?.count, 0)
+        XCTAssertEqual(domain.url, url.domainURL.absoluteString)
+    }
+    
+    /// Tests non-HTTPSE shields
+    func testNormalShieldSettings() {
+        let domain = Domain.getOrCreate(forUrl: url2HTTPS)
+        backgroundSaveAndWaitForExpectation {
+            Domain.setBraveShield(forUrl: url2HTTPS, shield: .SafeBrowsing, isOn: true, isPrivateBrowsing: false)
+        }
+        
+        backgroundSaveAndWaitForExpectation {
+            Domain.setBraveShield(forUrl: url2HTTPS, shield: .AdblockAndTp, isOn: false, isPrivateBrowsing: false)
+        }
+        
+        XCTAssertTrue(domain.isShieldExpected(BraveShield.SafeBrowsing, isPrivateBrowsing: false))
+        // Not testing via isSheildExpected, since that adds default checks
+        XCTAssertFalse(domain.shield_adblockAndTp == 0)
+        
+        // These should be the same in this situation
+        XCTAssertTrue(domain.isShieldExpected(BraveShield.SafeBrowsing, isPrivateBrowsing: true))
+        // Not testing via isSheildExpected, since that adds default checks
+        XCTAssertFalse(domain.shield_adblockAndTp == 0)
+        
+        // Setting to "new" values
+        // Setting to same value
+        backgroundSaveAndWaitForExpectation {
+            Domain.setBraveShield(forUrl: url2HTTPS, shield: .SafeBrowsing, isOn: true, isPrivateBrowsing: false)
+        }
+        
+        backgroundSaveAndWaitForExpectation {
+            Domain.setBraveShield(forUrl: url2HTTPS, shield: .AdblockAndTp, isOn: true, isPrivateBrowsing: false)
+        }
+        
+        XCTAssertTrue(domain.isShieldExpected(BraveShield.SafeBrowsing, isPrivateBrowsing: false))
+        XCTAssertTrue(domain.isShieldExpected(BraveShield.AdblockAndTp, isPrivateBrowsing: false))
+        
+        // These should be the same in this situation
+        XCTAssertTrue(domain.isShieldExpected(BraveShield.SafeBrowsing, isPrivateBrowsing: true))
+        XCTAssertTrue(domain.isShieldExpected(BraveShield.AdblockAndTp, isPrivateBrowsing: true))
+    }
+    
+    /// Testing HTTPSE
+    /// if setting an HTTP scheme, that HTTPS is also set
+    func testHTTPSEforHTTPsetter() {
+        backgroundSaveAndWaitForExpectation {
+            Domain.setBraveShield(forUrl: url, shield: .HTTPSE, isOn: true, isPrivateBrowsing: false)
+        }
+        
+        // Should be one for HTTP and one for HTTPS schemes
+        XCTAssertEqual(try! DataController.viewContext.count(for: fetchRequest), 2)
+        
+        let domainRefetch1 = Domain.getOrCreate(forUrl: url)
+        XCTAssertEqual(domainRefetch1.isShieldExpected(.HTTPSE, isPrivateBrowsing: false), true)
+        
+        let domainRefetch2 = Domain.getOrCreate(forUrl: urlHTTPS)
+        XCTAssertEqual(domainRefetch2.isShieldExpected(.HTTPSE, isPrivateBrowsing: false), true)
+    }
+    
+    /// Testing HTTPSE
+    /// if setting an HTTPS scheme, that HTTP is also set
+    func testHTTPSEforHTTPSsetter() {
+        backgroundSaveAndWaitForExpectation {
+            Domain.setBraveShield(forUrl: url2HTTPS, shield: .HTTPSE, isOn: true, isPrivateBrowsing: false)
+        }
 
-    // BRAVE TODO: Add shields unit tests after they are finished.
+        // Should be one for HTTP and one for HTTPS schemes
+        XCTAssertEqual(try! DataController.viewContext.count(for: fetchRequest), 2)
+        
+        let domainRefetch1 = Domain.getOrCreate(forUrl: url2)
+        XCTAssertEqual(domainRefetch1.isShieldExpected(.HTTPSE, isPrivateBrowsing: false), true)
+        
+        let domainRefetch2 = Domain.getOrCreate(forUrl: url2HTTPS)
+        XCTAssertEqual(domainRefetch2.isShieldExpected(.HTTPSE, isPrivateBrowsing: false), true)
+    }
 }
