07018bfa1861093fcc9a6256de09213b7a61a31e
brave-builds <devops@brave.com>
2019-07-22 08:08:58 +0000

    Uplift of #2959 (squashed) to beta

--- components/brave_sync/brave_sync_service_impl.cc
+++ components/brave_sync/brave_sync_service_impl.cc
@@ -90,7 +90,7 @@ BraveSyncServiceImpl::BraveSyncServiceImpl(Profile* profile) :
         sync_client_.get(),
         sync_prefs_.get())),
     timer_(std::make_unique<base::RepeatingTimer>()),
-    unsynced_send_interval_(base::TimeDelta::FromMinutes(10)) {
+    unsynced_send_interval_(base::TimeDelta::FromMinutes(60)) {
   // Moniter syncs prefs required in GetSettingsAndDevices
   profile_pref_change_registrar_.Init(profile->GetPrefs());
   profile_pref_change_registrar_.Add(
