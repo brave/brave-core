9d4ff35cc2044b1ec97a160769253bd9992fc13b
Pete Miller <miller.pete@gmail.com>
2019-07-23 16:02:36 -0700

    Test: JS unit tests mock loadTimeData

--- components/test/testSetup.ts
+++ components/test/testSetup.ts
@@ -5,6 +5,8 @@
 import * as Enzyme from 'enzyme'
 import * as EnzymeAdapter from 'enzyme-adapter-react-16'
 
+import './testPolyfills'
+
 // not mocking this file make some tests to fail due to non clear reason.
 // other action creators didn't need mocking and tests are ok without it.
 // since we are not testing action creators ATM it seemed ok to have it mocked
