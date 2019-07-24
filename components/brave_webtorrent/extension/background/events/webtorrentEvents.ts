01bd099a15489dafbcbd7c7d91dff749914b6606
Feross Aboukhadijeh <feross@feross.org>
2019-07-24 16:42:39 -0700

    WebTorrent: Fix stats remain stuck after download finishes
    Fixes: https://github.com/brave/brave-browser/issues/5352
    
    - Always update stats at least once every 5 seconds, even when no torrent events fire
    - remove stray console logs
    - Listen for 'wire' (new peer) events
    - Listen for 'done' event

--- components/brave_webtorrent/extension/background/events/webtorrentEvents.ts
+++ components/brave_webtorrent/extension/background/events/webtorrentEvents.ts
@@ -3,9 +3,24 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import { Instance } from 'webtorrent'
+import webtorrentActions from '../actions/webtorrentActions'
+
+let interval: number
 
 export const addWebtorrentEvents = (webtorrent: Instance) => {
   webtorrent.on('error', (e: Error | string) => {
     console.log('WebTorrent error: ', e)
   })
+
+  // Always update stats at least once every 5 seconds, even when no torrent
+  // events fire
+  interval = setInterval(() => {
+    webtorrent.torrents.forEach(torrent => {
+      webtorrentActions.progressUpdated(torrent)
+    })
+  }, 5000)
+}
+
+export const removeWebTorrentEvents = (webtorrent: Instance) => {
+  clearInterval(interval)
 }
