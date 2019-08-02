2491124fb2e19d3be9d6b2e1d690eedfd3f96a32
Michael Smith <michael@spinda.net>
2019-08-02 15:48:36 -0700

    Allow for sibling-less inserts in PG

--- third_party/blink/brave_page_graph/page_graph.cc
+++ third_party/blink/brave_page_graph/page_graph.cc
@@ -403,7 +403,9 @@ void PageGraph::RegisterHTMLElementNodeInserted(const DOMNodeId node_id,
 
   LOG_ASSERT(element_nodes_.count(node_id) == 1);
   LOG_ASSERT(element_nodes_.count(parent_node_id) == 1);
-  LOG_ASSERT(element_nodes_.count(before_sibling_id) + text_nodes_.count(before_sibling_id) == 1);
+  LOG_ASSERT(before_sibling_id == 0
+                 || element_nodes_.count(before_sibling_id) +
+                        text_nodes_.count(before_sibling_id) == 1);
   NodeHTMLElement* const inserted_node = element_nodes_.at(node_id);
 
   AddEdge(new EdgeNodeInsert(this, acting_node, inserted_node,
@@ -421,7 +423,9 @@ void PageGraph::RegisterHTMLTextNodeInserted(const DOMNodeId node_id,
 
   LOG_ASSERT(text_nodes_.count(node_id) == 1);
   LOG_ASSERT(element_nodes_.count(parent_node_id) == 1);
-  LOG_ASSERT(element_nodes_.count(before_sibling_id) + text_nodes_.count(before_sibling_id) == 1);
+  LOG_ASSERT(before_sibling_id == 0
+                 || element_nodes_.count(before_sibling_id) +
+                        text_nodes_.count(before_sibling_id) == 1);
   NodeHTMLText* const inserted_node = text_nodes_.at(node_id);
 
   AddEdge(new EdgeNodeInsert(this, acting_node, inserted_node,
