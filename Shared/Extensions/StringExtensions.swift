d117b9555cd0bb0b5b8b8df49bca3cbbcd85449d
Michał Buczek <michal.buczek@protonmail.com>
2019-08-04 16:14:24 +0200

    Fix brave/brave-ios#1266: Truncate image title when it's too long on long press action. (brave/brave-ios#1270)

--- Shared/Extensions/StringExtensions.swift
+++ Shared/Extensions/StringExtensions.swift
@@ -109,4 +109,15 @@ extension String {
         addWordsDescriptionBolded.setAttributes(attributes, range: nsRangeOfBoldedText)
         return addWordsDescriptionBolded
     }
+    
+    /*
+     Truncates the string to the specified length number of characters and appends an optional trailing string if longer.
+     - Parameter length: Desired maximum lengths of a string
+     - Parameter trailing: A 'String' that will be appended after the truncation.
+     
+     - Returns: 'String' object.
+     */
+    public func truncate(length: Int, trailing: String = "…") -> String {
+        return (self.count > length) ? self.prefix(length) + trailing : self
+    }
 }
