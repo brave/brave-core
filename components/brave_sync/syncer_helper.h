00bff1e60938f2f19495bf5c268dc3b8a3ecacc3
AlexeyBarabash <alexey@brave.com>
2019-08-06 12:57:31 +0300

    In case of equal orders use object_id

--- components/brave_sync/syncer_helper.h
+++ components/brave_sync/syncer_helper.h
@@ -26,6 +26,16 @@ void RepositionOnApplyChangesFromSyncModel(
   bookmarks::BookmarkModel* bookmark_model,
   const std::multimap<int, const bookmarks::BookmarkNode*>& to_reposition);
 
+// Exported for test only
+void RepositionRespectOrder(
+    bookmarks::BookmarkModel* bookmark_model,
+    const bookmarks::BookmarkNode* node);
+
+uint64_t GetIndexByCompareOrderStartFrom(
+    const bookmarks::BookmarkNode* parent,
+    const bookmarks::BookmarkNode* src,
+    int index);
+
 }   // namespace brave_sync
 
 #endif  // BRAVE_COMPONENTS_BRAVE_SYNC_SYNCER_HELPER_H_
