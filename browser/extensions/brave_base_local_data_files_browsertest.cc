e9254771588c6a7835f9331ffa6e5bbffe60ee7f
Mark Pilgrim <mpilgrim@brave.com>
2019-08-06 19:00:15 -0400

    Refactor local data files browsertests

---
+++ browser/extensions/brave_base_local_data_files_browsertest.cc
@@ -0,0 +1,94 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/browser/extensions/brave_base_local_data_files_browsertest.h"
+
+#include "base/path_service.h"
+#include "base/run_loop.h"
+#include "base/task/post_task.h"
+#include "base/test/thread_test_helper.h"
+#include "brave/browser/brave_browser_process_impl.h"
+#include "brave/common/brave_paths.h"
+#include "brave/components/brave_component_updater/browser/local_data_files_observer.h"
+#include "brave/components/brave_component_updater/browser/local_data_files_service.h"
+#include "chrome/browser/extensions/extension_browsertest.h"
+#include "chrome/browser/net/url_request_mock_util.h"
+#include "content/public/browser/browser_task_traits.h"
+#include "content/public/browser/browser_thread.h"
+#include "content/public/test/browser_test_utils.h"
+#include "net/dns/mock_host_resolver.h"
+
+using brave_component_updater::LocalDataFilesObserver;
+using content::BrowserThread;
+using extensions::ExtensionBrowserTest;
+
+void BaseLocalDataFilesBrowserTest::SetUp() {
+  brave::RegisterPathProvider();
+  MaybeInitEmbeddedTestServer();
+  ExtensionBrowserTest::SetUp();
+}
+
+void BaseLocalDataFilesBrowserTest::SetUpOnMainThread() {
+  MaybeSetUpEmbeddedTestServerOnMainThread();
+  ExtensionBrowserTest::SetUpOnMainThread();
+}
+
+void BaseLocalDataFilesBrowserTest::PreRunTestOnMainThread() {
+  ExtensionBrowserTest::PreRunTestOnMainThread();
+  ASSERT_TRUE(
+      g_brave_browser_process->local_data_files_service()->IsInitialized());
+}
+
+void BaseLocalDataFilesBrowserTest::GetTestDataDir(
+    base::FilePath* test_data_dir) {
+  base::ScopedAllowBlockingForTesting allow_blocking;
+  base::PathService::Get(brave::DIR_TEST_DATA, test_data_dir);
+}
+
+void BaseLocalDataFilesBrowserTest::MaybeInitEmbeddedTestServer() {
+  if (embedded_test_server_directory() == NULL)
+    return;
+
+  base::FilePath test_data_dir;
+  GetTestDataDir(&test_data_dir);
+  test_data_dir = test_data_dir.AppendASCII(embedded_test_server_directory());
+  content::SetupCrossSiteRedirector(embedded_test_server());
+  embedded_test_server()->ServeFilesFromDirectory(test_data_dir);
+  ASSERT_TRUE(embedded_test_server()->Start());
+}
+
+void BaseLocalDataFilesBrowserTest::MaybeSetUpEmbeddedTestServerOnMainThread() {
+  if (!embedded_test_server_directory())
+    return;
+
+  base::PostTaskWithTraits(
+      FROM_HERE, {content::BrowserThread::IO},
+      base::BindOnce(&chrome_browser_net::SetUrlRequestMocksEnabled, true));
+  host_resolver()->AddRule("*", "127.0.0.1");
+}
+
+void BaseLocalDataFilesBrowserTest::WaitForService() {
+  scoped_refptr<base::ThreadTestHelper> tr_helper(new base::ThreadTestHelper(
+      g_brave_browser_process->local_data_files_service()->GetTaskRunner()));
+  ASSERT_TRUE(tr_helper->Run());
+  scoped_refptr<base::ThreadTestHelper> io_helper(new base::ThreadTestHelper(
+      base::CreateSingleThreadTaskRunnerWithTraits({BrowserThread::IO}).get()));
+  ASSERT_TRUE(io_helper->Run());
+  base::RunLoop().RunUntilIdle();
+}
+
+bool BaseLocalDataFilesBrowserTest::InstallMockExtension() {
+  base::FilePath test_data_dir;
+  GetTestDataDir(&test_data_dir);
+  const extensions::Extension* mock_extension =
+      InstallExtension(test_data_dir.AppendASCII(test_data_directory()), 1);
+  if (!mock_extension)
+    return false;
+
+  service()->OnComponentReady(mock_extension->id(), mock_extension->path(), "");
+  WaitForService();
+
+  return true;
+}
---
+++ browser/extensions/brave_base_local_data_files_browsertest.h
@@ -0,0 +1,39 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_BROWSER_EXTENSIONS_BASE_LOCAL_DATA_FILES_BROWSERTEST_H_
+#define BRAVE_BROWSER_EXTENSIONS_BASE_LOCAL_DATA_FILES_BROWSERTEST_H_
+
+#include "brave/components/brave_component_updater/browser/local_data_files_observer.h"
+#include "chrome/browser/extensions/extension_browsertest.h"
+
+using brave_component_updater::LocalDataFilesObserver;
+using extensions::ExtensionBrowserTest;
+
+// This is an abstract base class that centralizes functions common to all
+// LocalDataFiles-related extension browser tests.
+class BaseLocalDataFilesBrowserTest : public ExtensionBrowserTest {
+ public:
+  BaseLocalDataFilesBrowserTest() {}
+
+  // ExtensionBrowserTest overrides
+  void SetUp() override;
+  void SetUpOnMainThread() override;
+  void PreRunTestOnMainThread() override;
+
+ protected:
+  void WaitForService();
+  void GetTestDataDir(base::FilePath* test_data_dir);
+  void MaybeInitEmbeddedTestServer();
+  void MaybeSetUpEmbeddedTestServerOnMainThread();
+  bool InstallMockExtension();
+
+  // Descendant classes must override these
+  virtual const char* test_data_directory() = 0;
+  virtual const char* embedded_test_server_directory() = 0;
+  virtual LocalDataFilesObserver* service() = 0;
+};
+
+#endif  // BRAVE_BROWSER_EXTENSIONS_BASE_LOCAL_DATA_FILES_BROWSERTEST_H_
