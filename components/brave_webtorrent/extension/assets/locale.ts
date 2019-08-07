e534bf99d005112ad6f7d936a9d905ecf40d0fcc
Ross Moody <fiftyfivehis@gmail.com>
2019-08-07 09:49:54 -0700

    webtorrent storybook page
    move styles/assets into webtorrent directory
    
    - consolidate typefaces
    - move out of shared space
    - initial logo upload
    
    locale start
    
    fixes
    
    - revert font duplication
    - fix logo asset
    - fix font path
    - remove font import in story as it happens automatically

---
+++ components/brave_webtorrent/extension/assets/locale.ts
@@ -0,0 +1,12 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License. v. 2.0. If a copy of the MPL was not distributed with this file.
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const locale = {
+  startDownload: 'Start download',
+  stopDownload: 'Stop download',
+  saveTorrentFile: 'Save .torrent file',
+  startTorrentIntro: 'Click "Start Torrent" to begin your download.'
+}
+
+export default locale
