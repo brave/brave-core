a5f0aea635b65fc089990a36e9f52559bff1101f
Brian R. Bondy <netzen@gmail.com>
2019-08-09 14:43:11 -0400

    Only do braveShields test for chrome.braveWallet when buildflag applies

--- test/data/extensions/api_test/braveShieldsWithWallet/background.js
+++ test/data/extensions/api_test/braveShieldsWithWallet/background.js
@@ -3,11 +3,12 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 chrome.test.runTests([
-  function braveShieldsExtensionHasAccess() {
-    if (chrome.braveShields) {
+  function braveShieldsHasWalletAccessButNotSeed() {
+    if (chrome.braveWallet && !chrome.braveWallet.getWalletSeed) {
       chrome.test.succeed();
     } else {
       chrome.test.fail();
     }
   },
 ]);
+
