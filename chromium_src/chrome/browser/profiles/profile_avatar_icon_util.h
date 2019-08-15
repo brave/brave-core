988f9494fb3cf1d4524ca1103c1ada9c30276e2e
Pete Miller <miller.pete@gmail.com>
2019-08-15 16:17:13 -0700

    Test brave-specific profile avatars
    - Profiles created via the menu command get non-placeholder and unique avatars
    - Initial profile which does get the placeholder, has brave's placeholder icon and not chrome's
    - Profiles created with older versions of Brave and have user-selected chromium avatar still display that legacy avatar, not a Brave one
    - Only Brave's icons can be selected for avatars
    - Random icon choosing never selects the placeholder
    - User icon choice can select the placeholder

---
+++ chromium_src/chrome/browser/profiles/profile_avatar_icon_util.h
@@ -0,0 +1,16 @@
+// Copyright 2019 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_PROFILES_PROFILE_AVATAR_ICON_UTIL_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_PROFILES_PROFILE_AVATAR_ICON_UTIL_H_
+
+#include "../../../../../chrome/browser/profiles/profile_avatar_icon_util.h"
+
+namespace profiles {
+  // Access original value for tests
+  extern const size_t kBraveDefaultAvatarIconsCount;
+}  // namespace profiles
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_PROFILES_PROFILE_AVATAR_ICON_UTIL_H_
