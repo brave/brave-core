8d3ff46ada31fc3ac3fcbea4e17f8e1dfc22126e
Max Karolinskiy <max@brave.com>
2019-08-20 16:49:06 -0400

    Transition CSS from [dark] to @media.
    * Use `@media (prefers-color-scheme: dark)` instead of `[dark]`.
    * Turn on kWebUIDarkMode feature on all platforms - this feature now
      gates the above @media directive.
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/6e5a6179e616f62bb575b1cd5e1c078fd4a0bc47
    
    commit 6e5a6179e616f62bb575b1cd5e1c078fd4a0bc47
    Author: Dan Beam <dbeam@chromium.org>
    Date:   Fri Jun 7 21:44:46 2019 +0000
    
        WebUI Dark Mode: update ui/webui to use @media queries
    
        This changes the implementation around how dark mode is queried in CSS.
    
        Bug: 965811
    
    https://chromium.googlesource.com/chromium/src/+/f0a7e117061208634ebf1cff20c4253db53f1b7a
    
    commit f0a7e117061208634ebf1cff20c4253db53f1b7a
    Author: Dan Beam <dbeam@chromium.org>
    Date:   Fri Jun 7 18:40:58 2019 +0000
    
        Make @media (prefers-color-scheme:) respect features::kWebUIDarkMode
        while on chrome:// URLs
    
        This allows us to use this mechanism in CSS:
    
          @media (prefers-color-scheme: dark) {
          }
    
        to vary styles for Chrome web UI in dark mode while also disabling via
        feature on certain platforms.
    
        This unblocks us from migrating to the @media query (from the existing
        [dark] attribute syntax) and later removing a bit of JS/C++ custom code
        in favor of simply using the web platform.
    
        R=futhark@chromium.org
        BUG=965811
    
    https://bugs.chromium.org/p/chromium/issues/detail?id=965811

--- app/brave_main_delegate.cc
+++ app/brave_main_delegate.cc
@@ -149,10 +149,9 @@ bool BraveMainDelegate::BasicStartupComplete(int* exit_code) {
 #if BUILDFLAG(ENABLE_EXTENSIONS)
       extensions_features::kNewExtensionUpdaterService.name,
 #endif
-#if defined(OS_LINUX)
-      // Enable webui dark on linux.
+    // Enable webui dark theme: @media (prefers-color-scheme: dark) is gated on
+    // this feature.
       features::kWebUIDarkMode.name,
-#endif
       omnibox::kSimplifyHttpsIndicator.name,
   };
 
