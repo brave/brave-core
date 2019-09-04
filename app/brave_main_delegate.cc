6ad43e880dd16011b544cd762706acfccf91a89c
Max Karolinskiy <max@brave.com>
2019-09-04 16:07:41 -0400

    Disable features::kAudioServiceOutOfProcess.
    The feature used to be disabled by default but enabled in experiments.
    When we disabled experiments we removed the explicit disablement of the
    feature. Now the feaure has been flipped enabled by default on Linux and
    Mac/Windows are being enabled by default in upstream versions.
    Explicitly disabling it again, assuming we still want to. @jumde
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/65932d46ee07af807713aaca6f8f081412d26db8
    
    commit 65932d46ee07af807713aaca6f8f081412d26db8
    Author: Henrik Grunell <grunell@chromium.org>
    Date:   Wed Jun 12 09:05:03 2019 +0000
    
        Enable AudioServiceAudioStreams and AudioServiceOutOfProcess by default on Linux.
    
        Bug: 672469,832666
    
    Upstream changes:
    
    https://chromium.googlesource.com/chromium/src/+/f1623e97a8c7174f684e32267cc85872199a7a22
    
    commit f1623e97a8c7174f684e32267cc85872199a7a22
    Author: Henrik Grunell <grunell@chromium.org>
    Date:   Mon Aug 12 20:58:24 2019 +0000
    
        Enable audio process with kill at hang by default on Mac.
    
        Enables features AudioServiceOutOfProcess and
        AudioServiceOutOfProcessKillAtHang by default on Mac.
    
        Also
        * Fixes audio process study names for Win and Linux in field trial
          config.
        * Sets default audio service teardown to 15 minutes on all
          platforms since it has been launched on all platforms (corrects
          a miss in some of the previous launches).
    
        This change is similar to the previous launch for Linux:
        https://chromium-review.googlesource.com/c/chromium/src/+/1648499
    
        Bug: 974450
    
    https://chromium.googlesource.com/chromium/src/+/8a41113b970a3b6c892d97a0f54a76da674a216a
    
    commit 8a41113b970a3b6c892d97a0f54a76da674a216a
    Author: Henrik Grunell <grunell@chromium.org>
    Date:   Mon Aug 19 09:25:19 2019 +0000
    
        Enable audio process with kill at hang by default on Windows.
    
        Bug: 974448

--- app/brave_main_delegate.cc
+++ app/brave_main_delegate.cc
@@ -29,6 +29,7 @@
 #include "components/omnibox/common/omnibox_features.h"
 #include "components/password_manager/core/common/password_manager_features.h"
 #include "components/unified_consent/feature.h"
+#include "content/public/common/content_features.h"
 #include "extensions/common/extension_features.h"
 #include "services/network/public/cpp/features.h"
 #include "third_party/widevine/cdm/buildflags.h"
@@ -158,6 +159,7 @@ bool BraveMainDelegate::BasicStartupComplete(int* exit_code) {
   // Disabled features.
   const std::unordered_set<const char*> disabled_features = {
       autofill::features::kAutofillServerCommunication.name,
+      features::kAudioServiceOutOfProcess.name,
       features::kLookalikeUrlNavigationSuggestionsUI.name,
       unified_consent::kUnifiedConsent.name,
   };
