ea82c7cf4ae6de38b2589f4e805fba925b60a68d
Pete Miller <miller.pete@gmail.com>
2019-09-09 23:11:19 -0700

    Reader Mode: remove UI button
    The feature is not ready yet and whilst a flag exists, we default it to on (unlike Chromium) since we use the backend of the feature as a component for the Brave Ads feature

---
+++ chromium_src/chrome/browser/ui/views/page_action/omnibox_page_action_icon_container_view.cc
@@ -0,0 +1,10 @@
+// Copyright (c) 2019 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "brave/browser/ui/views/reader_mode/brave_reader_mode_icon_view.h"
+
+#define ReaderModeIconView BraveReaderModeIconView
+#include "../../../../../../../chrome/browser/ui/views/page_action/omnibox_page_action_icon_container_view.cc"
+#undef ReaderModeIconView
