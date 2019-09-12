18101cee6c1c7fed1a2f7b9fdf4bfa2c6c0ecbaf
Pranjal Jumde <pranjal.jumde@gmail.com>
2019-09-11 22:40:10 -0700

    Issue 5980: Proxying requests from SafeBrowsingNetworkContext

---
+++ chromium_src/components/safe_browsing/browser/safe_browsing_network_context.cc
@@ -0,0 +1,7 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/browser/net/brave_system_request_handler.h"
+#include "../../../../../components/safe_browsing/browser/safe_browsing_network_context.cc" 
