c372720a29dcd87c6327c89471da392babd53106
bridiver <github@brianjohnson.cc>
2019-09-12 17:03:16 -0700

    ChromeMainDelegateAndroid needs to subclass BraveMainDelegate fix https://github.com/brave/brave-browser/issues/6004

---
+++ chromium_src/chrome/app/android/chrome_main_delegate_android.cc
@@ -0,0 +1,10 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/app/brave_main_delegate.h"
+
+#define ChromeMainDelegate BraveMainDelegate
+#include "../../../../../chrome/app/android/chrome_main_delegate_android.cc"
+#undef ChromeMainDelegate
