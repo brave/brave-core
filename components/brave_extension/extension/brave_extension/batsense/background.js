40b2fe8105e1d95c575924372b2d98a809de8c8e
Pete Miller <miller.pete@gmail.com>
2019-09-20 19:18:33 -0700

    BATSense: real ad demo catalog

--- components/brave_extension/extension/brave_extension/batsense/background.js
+++ components/brave_extension/extension/brave_extension/batsense/background.js
@@ -1,25 +1,7 @@
-import creative1Url from './demo-creatives/creative-1.html'
+import RealAdDemoCatalog from './ad-catalogs/real-ads-demo'
 
-class DemoAdCatalog {
-  getAd (sizes, isResponsive) {
-    // TODO: More creatives
-    // TODO: destination Url for each creative
-    // TODO: min / max size for each creative
-    // TODO: return null if no match
-    console.log('asking for ad', sizes)
-    if (!sizes.some(size => size.length === 2 && Number(size[0]) > 200 && Number(size[1]) > 70)) {
-      console.log('too small')
-      return null
-    }
-    console.log('ok size', sizes[sizes.length - 1])
-    return {
-      creativeUrl: chrome.runtime.getURL(creative1Url),
-      size: sizes[sizes.length - 1]
-    }
-  }
-}
 
-const adCatalog = new DemoAdCatalog()
+const adCatalog = new RealAdDemoCatalog()
 
 // Respond to requests for creatives from tab content scripts
 chrome.runtime.onMessage.addListener(
