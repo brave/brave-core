c4c87992ba13d0fdd899aee7f640afd43678e40d
Pete Miller <miller.pete@gmail.com>
2019-09-21 17:48:50 -0700

    BATSense: leaderboard creative for 'real ad' demo

--- components/brave_extension/extension/brave_extension/batsense/ad-catalogs/real-ads-demo.js
+++ components/brave_extension/extension/brave_extension/batsense/ad-catalogs/real-ads-demo.js
@@ -1,9 +1,20 @@
 import audiDsky1Url from '../demo-creatives/audi-dsky-1.jpg'
+import audiLboard1Url from '../demo-creatives/audi-lboard-1.jpg'
+
+const categories = {
+  automotive: 'Automotive'
+}
 
 const realAds = [
   {
     size: [300, 600],
-    path: audiDsky1Url
+    path: audiDsky1Url,
+    category: categories.automotive
+  },
+  {
+    size: [728, 90],
+    path: audiLboard1Url,
+    category: categories.automotive
   }
 ]
 
@@ -15,9 +26,13 @@ export default class RealAdDemoCatalog {
       .map(sizeArray => [Number(sizeArray[0]), Number(sizeArray[1])])
     // Get the first ad that has a size within the list of slot sizes
     const chosenAd = realAds.find(ad => sizesTyped.some(size => size[0] === ad.size[0] && size[1] === ad.size[1]))
+    if (!chosenAd) {
+      return null
+    }
     return {
       size: chosenAd.size,
-      creativeUrl: chrome.runtime.getURL(chosenAd.path)
+      creativeUrl: chrome.runtime.getURL(chosenAd.path),
+      category: chosenAd.category
     }
   }
 }
