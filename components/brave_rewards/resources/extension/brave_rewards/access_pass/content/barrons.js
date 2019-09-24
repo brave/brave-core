608fcd2fb1ad7c53dcd744d7af0a4da31474e3cf
Pete Miller <miller.pete@gmail.com>
2019-09-24 13:32:13 -0700

    AccessPass: Reload active tab for bypassed publisher via passing bypass status between content-script and background script
    For some reason I'm still keeping around content-script-paywall-bypass possibility, maybe we will have to do that in the future. Otherwise the reload could be done in background script immediately after doing the background-script-paywall-bypass.

--- components/brave_rewards/resources/extension/brave_rewards/access_pass/content/barrons.js
+++ components/brave_rewards/resources/extension/brave_rewards/access_pass/content/barrons.js
@@ -1,4 +1,5 @@
 import WallbreakerController from './controller/wallbreaker-controller'
+import PerformBypassInBackgroundScript from './controller/perform-bypass-in-background'
 
 function detectBypassablePaywall() {
   console.log('Wallbreaker [barons]: detecting if page has a bypassable paywall')
@@ -6,13 +7,10 @@ function detectBypassablePaywall() {
 }
 
 function bypassPaywall() {
-  console.log('Wallbreaker [barrons]: bypassing paywall...')
-  chrome.runtime.sendMessage({
-    type: 'perform-bypass',
-    bypassArgs: {
-      cookiesToDelete: [ 'cookie1, cookie2' ],
-      cookiesToExtend: [ 'cookie3' ]
-    }
+  console.log('Wallbreaker [barrons]: bypassing paywall in background script...')
+  return PerformBypassInBackgroundScript({
+    cookiesToDelete: [ 'cookie1, cookie2' ],
+    cookiesToExtend: [ 'cookie3' ]
   })
 }
 
