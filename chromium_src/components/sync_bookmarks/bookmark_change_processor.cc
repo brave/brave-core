5be1261a0e234600c4351512b03e2a45c828a637
Anthony Tseng <darkdh@gmail.com>
2019-09-24 19:09:11 -0700

    Ignore favicon changes for sync update Previously, we tried to ignore favicon changes after applying CREATE sync record. https://github.com/brave/brave-core/pull/2010/commits/2b431d6e787b0ebbc212d0c7a3d4807095d4f0d5 Right now we ignore all the favicon changes because we don't send favicon image date in our sync record. favicon url is only used by muon.

--- chromium_src/components/sync_bookmarks/bookmark_change_processor.cc
+++ chromium_src/components/sync_bookmarks/bookmark_change_processor.cc
@@ -36,28 +36,6 @@ class ScopedPauseObserver {
   BookmarkModelObserver* observer_;  // Not owned
 };
 
-bool IsFirstLoadedFavicon(BookmarkChangeProcessor* bookmark_change_processor,
-                          BookmarkModel* bookmark_model,
-                          const BookmarkNode* node) {
-  // Avoid sending duplicate records right after applying CREATE records,
-  // BookmarkChangeProcessor::SetBookmarkFavicon, put favicon data into database
-  // BookmarkNode::favicon() and BookmarkNode::icon_url() are available only
-  // after first successfuly BookmarkModel::GetFavicon() which means
-  // BookmarkModel::OnFaviconDataAvailable has image result available.
-  // So we set metainfo to know if it is first time favicon load after create
-  // node from remote record
-  std::string FirstLoadedFavicon;
-  if (node->GetMetaInfo("FirstLoadedFavicon", &FirstLoadedFavicon)) {
-    if (!node->icon_url())
-      return true;
-    ScopedPauseObserver pause(bookmark_model, bookmark_change_processor);
-    BookmarkNode* mutable_node = const_cast<BookmarkNode*>(node);
-    mutable_node->DeleteMetaInfo("FirstLoadedFavicon");
-    return true;
-  }
-  return false;
-}
-
 }  // namespace
 
 namespace sync_bookmarks {
@@ -91,7 +69,6 @@ void BookmarkChangeProcessor::MoveSyncNode(
 }  // namespace sync_bookmarks
 
 #define BRAVE_BOOKMARK_CHANGE_PROCESSOR_BOOKMARK_NODE_FAVICON_CHANGED \
-  if (IsFirstLoadedFavicon(this, bookmark_model_, node))              \
     return;
 
 #define BRAVE_BOOKMARK_CHANGE_PROCESSOR_UPDATE_SYNC_NODE_PROPERTIES \
@@ -107,10 +84,7 @@ void BookmarkChangeProcessor::MoveSyncNode(
   brave_sync::AddBraveMetaInfo(child, model);              \
   SetSyncNodeMetaInfo(child, &sync_child);
 
-#define BRAVE_BOOKMARK_CHANGE_PROCESSOR_APPLY_CHANGES_FROM_SYNC_MODEL_1 \
-  bookmark_model_->SetNodeMetaInfo(dst, "FirstLoadedFavicon", "true");
-
-#define BRAVE_BOOKMARK_CHANGE_PROCESSOR_APPLY_CHANGES_FROM_SYNC_MODEL_2   \
+#define BRAVE_BOOKMARK_CHANGE_PROCESSOR_APPLY_CHANGES_FROM_SYNC_MODEL   \
   int new_index =                                                         \
       brave_sync::GetIndexByCompareOrderStartFrom(parent, it->second, 0); \
   if (it->first != new_index) {                                           \
@@ -123,5 +97,4 @@ void BookmarkChangeProcessor::MoveSyncNode(
 #undef BRAVE_BOOKMARK_CHANGE_PROCESSOR_UPDATE_SYNC_NODE_PROPERTIES
 #undef BRAVE_BOOKMARK_CHANGE_PROCESSOR_BOOKMARK_NODE_MOVED
 #undef BRAVE_BOOKMARK_CHANGE_PROCESSOR_CHILDREN_REORDERED
-#undef BRAVE_BOOKMARK_CHANGE_PROCESSOR_APPLY_CHANGES_FROM_SYNC_MODEL_1
-#undef BRAVE_BOOKMARK_CHANGE_PROCESSOR_APPLY_CHANGES_FROM_SYNC_MODEL_2
+#undef BRAVE_BOOKMARK_CHANGE_PROCESSOR_APPLY_CHANGES_FROM_SYNC_MODEL
