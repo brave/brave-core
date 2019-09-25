086c3eacb7a1ca73bd5deaa0f59333ebcbd94745
gpestana <g6pestana@gmail.com>
2019-09-25 15:33:40 +0100

    adds Barron's paywall logic

--- components/brave_rewards/resources/extension/brave_rewards/wallbreaker/content/barrons.js
+++ components/brave_rewards/resources/extension/brave_rewards/wallbreaker/content/barrons.js
@@ -2,19 +2,31 @@ import WallbreakerController from './controller/wallbreaker-controller'
 import PerformBypassInBackgroundScript from './controller/perform-bypass-in-background'
 
 function detectBypassablePaywall() {
-  console.log('Wallbreaker [barons]: detecting if page has a bypassable paywall')
-  return Promise.resolve(true)
+  console.log('Wallbreaker [barrons]: detecting if page has a bypassable paywall')
+  let where = document.location.href;
+  // skips landing page
+  if (where == "https://www.barrons.com/") {
+    return Promise.resolve({bypass: false})
+  }
+  // detects that paywall is up based on the page's DOM
+  let paywallDOM = document.getElementsByClassName("barrons-snippet-ad");
+  if (paywallDOM.length > 0) {
+	return Promise.resolve({bypass: true})
+  }
+  return Promise.resolve({bypass: false})
 }
 
 function bypassPaywall() {
   console.log('Wallbreaker [barrons]: bypassing paywall in background script...')
   return PerformBypassInBackgroundScript({
-    cookiesToDelete: [ 'cookie1, cookie2' ],
-    cookiesToExtend: [ 'cookie3' ]
+    cookiesToDelete: [ 'cX_P' ],
+    cookiesToExtend: [],
+    domain: ".barrons.com",
+    url: "https://barrons.com",
   })
 }
 
 new WallbreakerController({
-  fnBypassPaywall: detectBypassablePaywall,
+  fnDetectBypassablePaywall: detectBypassablePaywall,
   fnBypassPaywall: bypassPaywall
 }).runForCurrentPage()
