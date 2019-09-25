d6c93a6cefaddd2803f423914f44968bfc639c72
mkarolin <max@brave.com>
2019-09-25 16:48:20 -0400

    ProfileMenuView: button clicks now use action callbacks.
    ButtonPressed override in ProfileMenuViewBase has been marked `final`,
    we don't need to override it any more. Also, Reset was moved to the
    base, we don't need to override it either.
    
    Chromium changes:
    
    https://chromium.googlesource.com/chromium/src/+/7cbff633bb1691549909f3bc85a627a465de7a82
    
    commit 7cbff633bb1691549909f3bc85a627a465de7a82
    Author: Thomas Tangl <tangltom@chromium.org>
    Date:   Wed Aug 14 17:44:17 2019 +0000
    
        [profile-menu] Move button handling to ProfileMenuViewBase
    
        - The logic of the button actions remains in ProfileMenuView.
        - The button handling is done in ProfileMenuViewBase.
    
        Bug: 966388
    
    https://chromium.googlesource.com/chromium/src/+/5d41b7ef83207bf7c01eb53c5378760a7bbc7042
    
    commit 5d41b7ef83207bf7c01eb53c5378760a7bbc7042
    Author: Thomas Tangl <tangltom@chromium.org>
    Date:   Wed Aug 21 09:50:44 2019 +0000
    
        [profile-menu] Move bubble creation to ProfileMenuViewBase
    
        Bug: 966388

--- browser/ui/views/profiles/brave_profile_menu_view.cc
+++ browser/ui/views/profiles/brave_profile_menu_view.cc
@@ -18,27 +18,12 @@
 #include "chrome/grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
 
-void BraveProfileMenuView::ButtonPressed(views::Button* sender,
-                                            const ui::Event& event) {
-  if (sender == users_button_ &&
-             browser()->profile()->IsGuestSession()) {
-    if (brave::IsTorProfile(browser()->profile()))
-      profiles::CloseTorProfileWindows();
-    else
-      profiles::CloseGuestProfileWindows();
-  } else {
-    ProfileMenuView::ButtonPressed(sender, event);
-  }
-}
-
 void BraveProfileMenuView::AddAutofillHomeView() { }
 
 void BraveProfileMenuView::AddDiceSyncErrorView(
     const AvatarMenu::Item& avatar_item,
     sync_ui_util::AvatarSyncErrorType error,
     int button_string_id) {
-  ProfileMenuViewBase::MenuItems menu_items;
-
   auto current_profile_photo = std::make_unique<BadgedProfilePhoto>(
       BadgedProfilePhoto::BADGE_TYPE_NONE, avatar_item.icon);
   base::string16 profile_name = avatar_item.name;
@@ -49,8 +34,22 @@ void BraveProfileMenuView::AddDiceSyncErrorView(
 
   AddMenuGroup();
   current_profile_card_ = CreateAndAddTitleCard(
-      std::move(current_profile_photo), profile_name, base::string16());
+      std::move(current_profile_photo), profile_name, base::string16(),
+      base::BindRepeating(&BraveProfileMenuView::OnCurrentProfileCardClicked,
+                          base::Unretained(this)));
 
   current_profile_card_->SetAccessibleName(l10n_util::GetStringFUTF16(
       IDS_PROFILES_EDIT_PROFILE_ACCESSIBLE_NAME, profile_name));
+}
+
+void BraveProfileMenuView::OnExitProfileButtonClicked() {
+  if (browser()->profile()->IsGuestSession()) {
+    RecordClick(ActionableItem::kExitProfileButton);
+    if (brave::IsTorProfile(browser()->profile()))
+      profiles::CloseTorProfileWindows();
+    else
+      profiles::CloseGuestProfileWindows();
+  } else {
+    ProfileMenuView::OnExitProfileButtonClicked();
+  }
 }
