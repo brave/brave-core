db4146c8afd9fa1e689d5a242626abbd5b93c5eb
NejcZdovc <nejc.zdovc@3zsistemi.si>
2019-09-26 10:06:45 -0700

    AC verified change
    Resolves https://github.com/brave/brave-browser/issues/6228

--- components/brave_rewards/browser/rewards_service_impl.cc
+++ components/brave_rewards/browser/rewards_service_impl.cc
@@ -2559,7 +2559,18 @@ void RewardsServiceImpl::MaybeShowNotificationAddFunds() {
 
 void RewardsServiceImpl::MaybeShowNotificationAddFundsForTesting(
     base::OnceCallback<void(bool)> callback) {
-  bat_ledger_->HasSufficientBalanceToReconcile(std::move(callback));
+  bat_ledger_->HasSufficientBalanceToReconcile(
+      base::BindOnce(
+          &RewardsServiceImpl::OnMaybeShowNotificationAddFundsForTesting,
+          AsWeakPtr(),
+          std::move(callback)));
+}
+
+void RewardsServiceImpl::OnMaybeShowNotificationAddFundsForTesting(
+    base::OnceCallback<void(bool)> callback,
+    const bool sufficient) {
+  ShowNotificationAddFunds(sufficient);
+  std::move(callback).Run(sufficient);
 }
 
 bool RewardsServiceImpl::ShouldShowNotificationAddFunds() const {
@@ -2773,10 +2784,6 @@ void RewardsServiceImpl::SetLedgerEnvForTesting() {
 
 void RewardsServiceImpl::StartMonthlyContributionForTest() {
   bat_ledger_->StartMonthlyContribution();
-}
-
-void RewardsServiceImpl::CheckInsufficientFundsForTesting() {
-  MaybeShowNotificationAddFunds();
 }
 
 void RewardsServiceImpl::GetProduction(const GetProductionCallback& callback) {
