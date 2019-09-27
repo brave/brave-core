40159f7666fb261c8bdd01bf009be64e9f0c3461
samartnik <artem@brave.com>
2019-09-27 12:03:20 -0400

    Review fix (reverted unnecessary change)

--- components/services/brave_content_browser_overlay_manifest.cc
+++ components/services/brave_content_browser_overlay_manifest.cc
@@ -3,7 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#if !defined(OS_ANDROID)
 #include "brave/components/services/brave_content_browser_overlay_manifest.h"
 
 #include "base/no_destructor.h"
@@ -41,4 +40,3 @@ const service_manager::Manifest& GetBraveContentBrowserOverlayManifest() {
           .Build()};
   return *manifest;
 }
-#endif
