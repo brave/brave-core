56677f73ac2688969099b8492bf18a27682f9765
Jocelyn Liu <yrliou@gmail.com>
2019-10-01 15:29:52 -0700

    Don't open non-incognito windows in Tor

--- chromium_src/chrome/browser/ui/browser_navigator.cc
+++ chromium_src/chrome/browser/ui/browser_navigator.cc
@@ -3,18 +3,52 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "brave/browser/profiles/profile_util.h"
 #include "brave/common/webui_url_constants.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
 #include "chrome/common/webui_url_constants.h"
 #include "url/gurl.h"
 
 namespace {
 
-void AdjustNavigateParamsForURLBraveImpl(NavigateParams* params) {
+bool HandleURLInParent(NavigateParams* params, Profile* profile) {
+  if (brave::IsTorProfile(profile) &&
+      !params->browser->profile()->IsOffTheRecord()) {
+    return true;
+  }
+
+  return false;
+}
+
+// GetOrCreateBrowser is not accessible here
+Browser* BraveGetOrCreateBrowser(Profile* profile, bool user_gesture) {
+  Browser* browser = chrome::FindTabbedBrowser(profile, false);
+  return browser ? browser
+                 : new Browser(Browser::CreateParams(profile, user_gesture));
+}
+
+void UpdateBraveScheme(NavigateParams* params) {
   if (params->url.SchemeIs(content::kBraveUIScheme)) {
     GURL::Replacements replacements;
     replacements.SetSchemeStr(content::kChromeUIScheme);
     params->url = params->url.ReplaceComponents(replacements);
+  }
+}
+
+void MaybeHandleInParent(NavigateParams* params, bool allow_in_incognito) {
+  auto* profile = params->initiating_profile;
+  if (brave::IsSessionProfile(profile)) {
+    if (!allow_in_incognito) {
+      params->initiating_profile = profile->IsOffTheRecord()
+          ? brave::GetParentProfile(profile)->GetOffTheRecordProfile()
+          : brave::GetParentProfile(profile);
+    } else if (HandleURLInParent(params, profile)) {
+      params->browser = BraveGetOrCreateBrowser(
+          brave::GetParentProfile(profile),
+          params->user_gesture);
+    }
   }
 }
 
@@ -30,4 +64,10 @@ bool IsHostAllowedInIncognitoBraveImpl(const base::StringPiece& host) {
 
 }  // namespace
 
-#include "../../../../chrome/browser/ui/browser_navigator.cc"  // NOLINT
+#define BRAVE_ADJUST_NAVIGATE_PARAMS_FOR_URL \
+  UpdateBraveScheme(params); \
+  MaybeHandleInParent(params, \
+      IsURLAllowedInIncognito(params->url, params->initiating_profile));
+
+#include "../../../../../chrome/browser/ui/browser_navigator.cc"
+#undef BRAVE_ADJUST_NAVIGATE_PARAMS_FOR_URL
