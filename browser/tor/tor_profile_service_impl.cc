2a4daf4cbfcebd1c1394f51d33a966681bd9c08e
Jocelyn Liu <yrliou@gmail.com>
2019-10-02 11:35:28 -0700

    Fix BraveProfileManagerTest.ProxyConfigMonitorInTorProfile crash in linux.

--- browser/tor/tor_profile_service_impl.cc
+++ browser/tor/tor_profile_service_impl.cc
@@ -120,6 +120,7 @@ void OnNewTorCircuit(std::unique_ptr<NewTorCircuitTracker> tracker,
 
 TorProfileServiceImpl::TorProfileServiceImpl(Profile* profile)
     : profile_(profile),
+      tor_launcher_factory_(nullptr),
       weak_ptr_factory_(this) {
   // Return early since g_brave_browser_process and tor_client_updater are not
   // available in unit tests.
