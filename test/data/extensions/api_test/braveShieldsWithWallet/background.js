58a13db9809057b2df572c38a9aa520bc2aeb094
brave-builds <devops@brave.com>
2019-10-02 14:04:30 +0000

    Uplift of #3578 (squashed) to dev

--- test/data/extensions/api_test/braveShieldsWithWallet/background.js
+++ test/data/extensions/api_test/braveShieldsWithWallet/background.js
@@ -2,13 +2,43 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-chrome.test.runTests([
-  function braveShieldsHasWalletAccessButNotSeed() {
-    if (chrome.braveWallet && !chrome.braveWallet.getWalletSeed &&
-        !chrome.braveWallet.getProjectID) {
-      chrome.test.succeed();
-    } else {
-      chrome.test.fail();
-    }
-  },
-]);
+function testBasics() {
+  chrome.test.runTests([
+    function braveShieldsHasWalletAccessButNotSeed() {
+      if (chrome.braveWallet && !chrome.braveWallet.getWalletSeed &&
+          !chrome.braveWallet.getProjectID) {
+        chrome.test.succeed();
+      } else {
+        chrome.test.fail();
+      }
+    },
+  ]);
+}
+
+function testNotEnabled() {
+  chrome.test.runTests([
+    function testNotENabled() {
+      chrome.braveWallet.isEnabled((enabled) => {
+        if (enabled) {
+          chrome.test.fail();
+        } else {
+          chrome.test.succeed();
+        }
+      })
+    },
+  ]);
+}
+
+function testEnabled() {
+  chrome.test.runTests([
+    function testNotENabled() {
+      chrome.braveWallet.isEnabled((enabled) => {
+        if (enabled) {
+          chrome.test.succeed();
+        } else {
+          chrome.test.fail();
+        }
+      })
+    },
+  ]);
+}
