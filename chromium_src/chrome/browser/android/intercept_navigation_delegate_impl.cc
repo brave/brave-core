e0d8f6ce33b3c698e6d80cbe855c9e412c5777d3
Simon Hong <shong@igalia.com>
2019-10-02 10:04:42 +0900

    Override JNI_InterceptNavigationDelegateImpl_AssociateWithWebContents
    With this overrides, BraveInterceptNavigationDelegate is used and don't
    need to patch intercept_navigation_delegate_impl.cc

--- chromium_src/chrome/browser/android/intercept_navigation_delegate_impl.cc
+++ chromium_src/chrome/browser/android/intercept_navigation_delegate_impl.cc
@@ -6,12 +6,21 @@
 #include <string>
 
 #include "brave/common/pref_names.h"
+#include "chrome/android/chrome_jni_headers/InterceptNavigationDelegateImpl_jni.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiles/profile.h"
+#include "components/navigation_interception/intercept_navigation_delegate.h"
+#include "components/navigation_interception/navigation_params.h"
 #include "components/prefs/pref_service.h"
+#include "content/public/browser/browser_thread.h"
+#include "content/public/browser/web_contents.h"
+#include "net/base/escape.h"
 #include "url/gurl.h"
 
 namespace {
+
+using navigation_interception::InterceptNavigationDelegate;
+using navigation_interception::NavigationParams;
 
 Profile* GetOriginalProfile() {
   return ProfileManager::GetActiveUserProfile()->GetOriginalProfile();
@@ -31,10 +40,35 @@ bool ShouldPlayVideoInBrowser(const GURL& url) {
   return false;
 }
 
-}  // namespace
+class BraveInterceptNavigationDelegate : public InterceptNavigationDelegate {
+ public:
+  BraveInterceptNavigationDelegate(JNIEnv* env, jobject jdelegate)
+      : InterceptNavigationDelegate(env, jdelegate) {}
 
-#define IGNORE_IF_PLAY_VIDEO_IN_BROWSER_IS_AVAILABLE(url) \
-    if (ShouldPlayVideoInBrowser(url)) \
+  bool ShouldIgnoreNavigation(
+      const NavigationParams& navigation_params) override {
+    NavigationParams chrome_navigation_params(navigation_params);
+    chrome_navigation_params.url() =
+        GURL(net::EscapeExternalHandlerValue(navigation_params.url().spec()));
+
+    if (ShouldPlayVideoInBrowser(chrome_navigation_params.url()))
       return false;
 
-#include "../../../../../chrome/browser/android/intercept_navigation_delegate_impl.cc"  // NOLINT
+    return InterceptNavigationDelegate::ShouldIgnoreNavigation(
+        chrome_navigation_params);
+  }
+};
+
+}  // namespace
+
+static void JNI_InterceptNavigationDelegateImpl_AssociateWithWebContents(
+    JNIEnv* env,
+    const base::android::JavaParamRef<jobject>& jdelegate,
+    const base::android::JavaParamRef<jobject>& jweb_contents) {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  content::WebContents* web_contents =
+      content::WebContents::FromJavaWebContents(jweb_contents);
+  InterceptNavigationDelegate::Associate(
+      web_contents,
+      std::make_unique<BraveInterceptNavigationDelegate>(env, jdelegate));
+}
