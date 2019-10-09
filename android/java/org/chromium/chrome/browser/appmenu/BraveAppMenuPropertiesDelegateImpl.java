2b014a2bf8d56d26866505a3d321ae4d35b022c5
Simon Hong <shong@igalia.com>
2019-10-09 16:02:18 +0900

    Cleanup main menu and add brave specific menu items
    Instead of modifying menu.xml, subclassing
    AppMenuPropertiesDelegateImpl. This way seems more extensible.

---
+++ android/java/org/chromium/chrome/browser/appmenu/BraveAppMenuPropertiesDelegateImpl.java
@@ -0,0 +1,62 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.chromium.chrome.browser.appmenu;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import org.chromium.base.ObservableSupplier;
+import org.chromium.base.Log;
+import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ActivityTabProvider;
+import org.chromium.chrome.browser.compositor.layouts.OverviewModeBehavior;
+import org.chromium.chrome.browser.multiwindow.MultiWindowModeStateDispatcher;
+import org.chromium.chrome.browser.tabmodel.TabModelSelector;
+import org.chromium.chrome.browser.toolbar.ToolbarManager;
+
+public class BraveAppMenuPropertiesDelegateImpl extends AppMenuPropertiesDelegateImpl {
+    private Menu mMenu;
+
+    public BraveAppMenuPropertiesDelegateImpl(Context context, ActivityTabProvider activityTabProvider,
+            MultiWindowModeStateDispatcher multiWindowModeStateDispatcher,
+            TabModelSelector tabModelSelector, ToolbarManager toolbarManager, View decorView,
+            @Nullable ObservableSupplier<OverviewModeBehavior> overviewModeBehaviorSupplier) {
+        super(context, activityTabProvider, multiWindowModeStateDispatcher, tabModelSelector,
+              toolbarManager, decorView, overviewModeBehaviorSupplier);
+    }
+
+    @Override
+    public void prepareMenu(Menu menu) {
+        super.prepareMenu(menu);
+
+        mMenu = menu;
+
+        // Brave's items are only visible for page menu.
+        // To make logic simple, below three items are added whenever menu gets visible
+        // and removed when menu is dismissed.
+        if (!shouldShowPageMenu())
+            return;
+
+        // Brave donesn't show help menu item in app menu.
+        menu.findItem(R.id.help_id).setVisible(false).setEnabled(false);
+
+        menu.add(Menu.NONE, R.id.set_default_browser, 0, R.string.menu_set_default_browser);
+        menu.add(Menu.NONE, R.id.brave_rewards_id, 0, R.string.menu_brave_rewards);
+        menu.add(Menu.NONE, R.id.exit_id, 0, R.string.menu_exit);
+    }
+
+    @Override
+    public void onMenuDismissed() {
+        super.onMenuDismissed();
+
+        mMenu.removeItem(R.id.set_default_browser);
+        mMenu.removeItem(R.id.brave_rewards_id);
+        mMenu.removeItem(R.id.exit_id);
+    }
+}
