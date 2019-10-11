cda0b20513541c38e7fad20ae70853481bc2dce2
Kyle Hickinson <kylehickinson@users.noreply.github.com>
2019-10-11 10:44:25 -0400

    No Bug: Update BraveRewards framework & fix public API changes (#1629)
    Fixes brave/brave-rewards-ios#223

--- BraveRewards/BraveRewards.framework/Headers/Enums.h
+++ BraveRewards/BraveRewards.framework/Headers/Enums.h
@@ -4,22 +4,6 @@
 
 #import <Foundation/Foundation.h>
 
-typedef NS_ENUM(NSInteger, BATActivityMonth) {
-  BATActivityMonthAny = -1,
-  BATActivityMonthJanuary = 1,
-  BATActivityMonthFebruary = 2,
-  BATActivityMonthMarch = 3,
-  BATActivityMonthApril = 4,
-  BATActivityMonthMay = 5,
-  BATActivityMonthJune = 6,
-  BATActivityMonthJuly = 7,
-  BATActivityMonthAugust = 8,
-  BATActivityMonthSeptember = 9,
-  BATActivityMonthOctober = 10,
-  BATActivityMonthNovember = 11,
-  BATActivityMonthDecember = 12
-} NS_SWIFT_NAME(ActivityMonth);
-
 typedef NS_ENUM(NSInteger, BATLogLevel) {
   BATLogLevelLogError = 1,
   BATLogLevelLogWarning = 2,
@@ -28,25 +12,3 @@ typedef NS_ENUM(NSInteger, BATLogLevel) {
   BATLogLevelLogRequest = 5,
   BATLogLevelLogResponse = 6
 } NS_SWIFT_NAME(LogLevel);
-
-typedef NS_ENUM(NSInteger, BATPublisherExclude) {
-  BATPublisherExcludeAll = -1,
-  BATPublisherExcludeDefault = 0,
-  BATPublisherExcludeExcluded = 1,
-  BATPublisherExcludeIncluded = 2
-} NS_SWIFT_NAME(PublisherExclude);
-
-typedef NS_ENUM(NSInteger, BATReportType) {
-  BATReportTypeGrant = 0,
-  BATReportTypeAutoContribution = 1,
-  BATReportTypeDeposit = 2,
-  BATReportTypeAds = 3,
-  BATReportTypeTipRecurring = 4,
-  BATReportTypeTip = 5
-} NS_SWIFT_NAME(ReportType);
-
-typedef NS_ENUM(NSInteger, BATURLMethod) {
-  BATURLMethodGet = 0,
-  BATURLMethodPut = 1,
-  BATURLMethodPost = 2
-} NS_SWIFT_NAME(URLMethod);
