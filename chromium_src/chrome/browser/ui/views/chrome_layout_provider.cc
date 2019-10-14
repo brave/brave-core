990409758bdf85c97c6bf33b0799f3f2a4e12e86
Brian Clifton <brian@clifton.me>
2019-10-13 22:01:16 -0700

    Merge pull request #3668 from brave/fix-c78-radius
    Re-enable BraveLayoutProvider for c78

--- chromium_src/chrome/browser/ui/views/chrome_layout_provider.cc
+++ chromium_src/chrome/browser/ui/views/chrome_layout_provider.cc
@@ -1,4 +1,19 @@
+// Copyright (c) 2019 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+
 #include "brave/browser/ui/views/brave_layout_provider.h"
-#define MaterialRefreshLayoutProvider BraveLayoutProvider
-#include "../../../../../chrome/browser/ui/views/chrome_layout_provider.cc"
-#undef MaterialRefreshLayoutProvider
+
+// Replace LayoutProvider creation with our own function.
+// The definition is provided similarly by the override of
+// the header file.
+#define CreateLayoutProvider CreateLayoutProvider_ChromiumImpl
+#include "../../../../../../chrome/browser/ui/views/chrome_layout_provider.cc"
+#undef CreateLayoutProvider
+
+// static
+std::unique_ptr<views::LayoutProvider>
+ChromeLayoutProvider::CreateLayoutProvider() {
+  return std::make_unique<BraveLayoutProvider>();
+}
