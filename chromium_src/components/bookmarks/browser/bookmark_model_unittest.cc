f892c5d0bdce6169f6a4d135d49bd93e494a20bf
Anthony Tseng <darkdh@gmail.com>
2019-10-15 16:17:16 -0700

    Remove checking other_node visibility disregarding children count (ignore the temporary visibility by adding foster parent in chromium sync)

--- chromium_src/components/bookmarks/browser/bookmark_model_unittest.cc
+++ chromium_src/components/bookmarks/browser/bookmark_model_unittest.cc
@@ -3,12 +3,35 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "components/bookmarks/test/test_bookmark_client.h"
+
+namespace bookmarks {
+class BraveTestBookmarkClient : public TestBookmarkClient {
+ public:
+  BraveTestBookmarkClient() : TestBookmarkClient() {}
+  ~BraveTestBookmarkClient() override {}
+
+  static std::unique_ptr<BookmarkModel> CreateModel() {
+  return CreateModelWithClient(base::WrapUnique(new BraveTestBookmarkClient));
+  }
+
+ private:
+  // BookmarkClient:
+  bool IsPermanentNodeVisible(const BookmarkPermanentNode* node) override {
+    if (node->type() == bookmarks::BookmarkNode::OTHER_NODE)
+      return false;
+    return TestBookmarkClient::IsPermanentNodeVisible(node);
+  }
+  DISALLOW_COPY_AND_ASSIGN(BraveTestBookmarkClient);
+};
+}  // namespace bookmarks
+
+#define TestBookmarkClient BraveTestBookmarkClient
 #define NodeVisibility DISABLED_NodeVisibility
-#define ParentForNewNodes DISABLED_ParentForNewNodes
 #define MostRecentlyModifiedFolders DISABLED_MostRecentlyModifiedFolders
 #include "../../../../../components/bookmarks/browser/bookmark_model_unittest.cc"
+#undef TestBookmarkClient
 #undef MostRecentlyModifiedFolders
-#undef ParentForNewNodes
 #undef NodeVisibility
 namespace bookmarks {
 
@@ -23,8 +46,7 @@ TEST_F(BookmarkModelTest, BraveNodeVisibility) {
   // forced to be visible by the client.
   model_->SetPermanentNodeVisible(BookmarkNode::BOOKMARK_BAR, false);
   EXPECT_TRUE(model_->bookmark_bar_node()->IsVisible());
-  // Other node is persistently invisible
-  model_->SetPermanentNodeVisible(BookmarkNode::OTHER_NODE, true);
+  model_->SetPermanentNodeVisible(BookmarkNode::OTHER_NODE, false);
   EXPECT_FALSE(model_->other_node()->IsVisible());
   model_->SetPermanentNodeVisible(BookmarkNode::MOBILE, true);
   EXPECT_TRUE(model_->mobile_node()->IsVisible());
@@ -40,27 +62,6 @@ TEST_F(BookmarkModelTest, BraveNodeVisibility) {
 
   // Mobile folder should be visible now that it has a child.
   EXPECT_TRUE(model_->mobile_node()->IsVisible());
-
-  // Even other node has a child, it remains invisible
-  TestNode node_a;
-  PopulateNodeFromString("A", &node_a);
-  PopulateBookmarkNode(&node_a, model_.get(), model_->other_node());
-  EXPECT_TRUE(model_->other_node()->children().front()->IsVisible());
-  EXPECT_FALSE(model_->other_node()->IsVisible());
-}
-
-// Tests that adding a URL to a folder updates the last modified time.
-TEST_F(BookmarkModelTest, BraveParentForNewNodes) {
-  const base::string16 title(ASCIIToUTF16("foo"));
-  const GURL url("http://foo.com");
-
-  model_->AddURL(model_->other_node(), 0, title, url);
-  ASSERT_EQ(model_->bookmark_bar_node(), GetParentForNewNodes(model_.get()));
-
-  const BookmarkNode* folderA =
-      model_->AddFolder(model_->bookmark_bar_node(), 0, ASCIIToUTF16("A"));
-  model_->AddURL(folderA, 0, ASCIIToUTF16("B1"), GURL("https://B1.com"));
-  ASSERT_EQ(folderA, GetParentForNewNodes(model_.get()));
 }
 
 // Make sure recently modified stays in sync when adding a URL.
@@ -83,25 +84,6 @@ TEST_F(BookmarkModelTest, BraveMostRecentlyModifiedFolders) {
   most_recent_folders = GetMostRecentlyModifiedUserFolders(model_.get(), 1);
   ASSERT_EQ(1U, most_recent_folders.size());
   ASSERT_TRUE(most_recent_folders[0] != folder);
-
-  // Add a folder in other_node
-  const BookmarkNode* folder_other_node =
-      model_->AddFolder(model_->other_node(), 0, ASCIIToUTF16("foo"));
-  // Add a URL to it.
-  model_->AddURL(folder_other_node, 0, ASCIIToUTF16("blah"),
-                 GURL("http://foo.com"));
-
-  // Only bookmark bar in the most recently modified.
-  most_recent_folders = GetMostRecentlyModifiedUserFolders(model_.get(), 1);
-  ASSERT_EQ(1U, most_recent_folders.size());
-  ASSERT_EQ(model_->bookmark_bar_node(), most_recent_folders[0]);
-
-  // Nuke the folder and do another fetch, making sure folder isn't in the
-  // returned list and it is still bookmark bar.
-  model_->Remove(folder_other_node->parent()->children().front().get());
-  most_recent_folders = GetMostRecentlyModifiedUserFolders(model_.get(), 1);
-  ASSERT_EQ(1U, most_recent_folders.size());
-  ASSERT_EQ(model_->bookmark_bar_node(), most_recent_folders[0]);
 }
 
 TEST_F(BookmarkModelTest, MigrateOtherNode) {
