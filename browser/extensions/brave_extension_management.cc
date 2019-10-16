014d46f08a45d79985ddd08a3ddcb7560b893b2c
Simon Hong <shong@igalia.com>
2019-10-16 20:50:40 +0900

    Delete tor binaries if tor is disabled by gpo

--- browser/extensions/brave_extension_management.cc
+++ browser/extensions/brave_extension_management.cc
@@ -8,14 +8,18 @@
 #include <memory>
 
 #include "base/command_line.h"
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
 #include "brave/browser/brave_browser_process_impl.h"
 #include "brave/browser/tor/buildflags.h"
 #include "brave/common/brave_switches.h"
 #include "brave/common/extensions/extension_constants.h"
 #include "brave/common/pref_names.h"
 #include "brave/browser/extensions/brave_extension_provider.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/extensions/external_policy_loader.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "components/prefs/pref_service.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/common/extension.h"
@@ -24,6 +28,12 @@
 #if BUILDFLAG(ENABLE_TOR)
 #include "brave/browser/extensions/brave_tor_client_updater.h"
 #endif
+
+namespace {
+void DeleteDir(const base::FilePath& path) {
+  base::DeleteFile(path, true);
+}
+}  // namespace
 
 namespace extensions {
 
@@ -35,6 +45,7 @@ BraveExtensionManagement::BraveExtensionManagement(Profile* profile)
         static_cast<content::BrowserContext*>(profile)));
   providers_.push_back(
       std::make_unique<BraveExtensionProvider>());
+  CleanupBraveExtensions();
   RegisterBraveExtensions();
 }
 
@@ -50,6 +61,20 @@ void BraveExtensionManagement::RegisterBraveExtensions() {
       !command_line.HasSwitch(switches::kDisableTorClientUpdaterExtension) &&
       !profile_->AsTestingProfile()) {
     g_brave_browser_process->tor_client_updater()->Register();
+  }
+#endif
+}
+
+void BraveExtensionManagement::CleanupBraveExtensions() {
+#if BUILDFLAG(ENABLE_TOR)
+  // Delete tor binaries if tor is disabled by gpo.
+  if (profile_->GetPrefs()->GetBoolean(kTorDisabled)) {
+    ProfileManager* profile_manager = g_browser_process->profile_manager();
+    base::FilePath tor_component_dir =
+        profile_manager->user_data_dir().AppendASCII(
+            extensions::kTorClientComponentId);
+    g_brave_browser_process->tor_client_updater()->GetTaskRunner()->PostTask(
+        FROM_HERE, base::BindOnce(&DeleteDir, tor_component_dir));
   }
 #endif
 }
