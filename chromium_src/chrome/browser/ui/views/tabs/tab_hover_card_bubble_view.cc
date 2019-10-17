bd6e6d520426362acf2e1a26dceaae15610647b8
mkarolin <41635752+mkarolin@users.noreply.github.com>
2019-10-17 14:48:21 -0400

    Merge pull request #3687 from brave/maxk-fix-hover-card-bubble-2
    Fixes hover card bubble chrome:// urls.

---
+++ chromium_src/chrome/browser/ui/views/tabs/tab_hover_card_bubble_view.cc
@@ -0,0 +1,39 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * you can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include <string>
+
+#include "base/strings/string_util.h"
+#include "base/strings/utf_string_conversions.h"
+#include "chrome/browser/ui/views/tabs/tab.h"
+#include "chrome/browser/ui/views/tabs/tab_hover_card_bubble_view.h"
+#include "content/public/common/url_constants.h"
+#include "ui/views/controls/label.h"
+
+#define TabHoverCardBubbleView TabHoverCardBubbleView_ChromiumImpl
+#include "../../../../../../chrome/browser/ui/views/tabs/tab_hover_card_bubble_view.cc"
+#undef TabHoverCardBubbleView
+
+void TabHoverCardBubbleView_ChromiumImpl::BraveUpdateCardContent(
+    const Tab* tab) {
+  TabHoverCardBubbleView_ChromiumImpl::UpdateCardContent(tab);
+  const base::string16& domain = domain_label_->GetText();
+  const base::string16 kChromeUISchemeU16 =
+      base::ASCIIToUTF16(content::kChromeUIScheme);
+  // Replace chrome:// with brave://. Since this is purely in the UI we can
+  // just do a sub-string replacement instead of parsing into GURL.
+  if (base::StartsWith(domain, kChromeUISchemeU16,
+                       base::CompareCase::INSENSITIVE_ASCII)) {
+    base::string16 new_domain = domain;
+    base::ReplaceFirstSubstringAfterOffset(
+        &new_domain, 0ul, kChromeUISchemeU16,
+        base::ASCIIToUTF16(content::kBraveUIScheme));
+    domain_label_->SetText(new_domain);
+  }
+}
+
+void TabHoverCardBubbleView::UpdateCardContent(const Tab* tab) {
+  BraveUpdateCardContent(tab);
+}
