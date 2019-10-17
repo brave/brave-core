f07cdf2dda80af9addb7b685394a07e06f4fe1e2
Brian Clifton <brian@clifton.me>
2019-10-17 16:33:20 -0700

    lint fixes

--- browser/search_engines/tor_window_search_engine_provider_service.h
+++ browser/search_engines/tor_window_search_engine_provider_service.h
@@ -1,10 +1,12 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #ifndef BRAVE_BROWSER_SEARCH_ENGINES_TOR_WINDOW_SEARCH_ENGINE_PROVIDER_SERVICE_H_
 #define BRAVE_BROWSER_SEARCH_ENGINES_TOR_WINDOW_SEARCH_ENGINE_PROVIDER_SERVICE_H_
 
+#include <memory>
 #include "brave/browser/search_engines/search_engine_provider_service.h"
 #include "components/prefs/pref_member.h"
 #include "components/search_engines/template_url_service_observer.h"
