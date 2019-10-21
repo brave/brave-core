8ab0ef86e8ed1f2171debb169d2747a1f382288d
brave-builds <devops@brave.com>
2019-10-21 07:29:37 +0000

    Uplift of #3738 (squashed) to beta

--- browser/search_engines/tor_window_search_engine_provider_service.h
+++ browser/search_engines/tor_window_search_engine_provider_service.h
@@ -1,13 +1,18 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #ifndef BRAVE_BROWSER_SEARCH_ENGINES_TOR_WINDOW_SEARCH_ENGINE_PROVIDER_SERVICE_H_
 #define BRAVE_BROWSER_SEARCH_ENGINES_TOR_WINDOW_SEARCH_ENGINE_PROVIDER_SERVICE_H_
 
+#include <memory>
 #include "brave/browser/search_engines/search_engine_provider_service.h"
 #include "components/prefs/pref_member.h"
 #include "components/search_engines/template_url_service_observer.h"
+
+class PrefService;
+struct TemplateURLData;
 
 // The purpose of this service for tor is making user changed search engine
 // provider persist across the sessions.
@@ -23,7 +28,8 @@ class TorWindowSearchEngineProviderService
   // TemplateURLServiceObserver overrides:
   void OnTemplateURLServiceChanged() override;
 
-  int GetInitialSearchEngineProvider() const;
+  std::unique_ptr<TemplateURLData> GetInitialSearchEngineProvider(
+      PrefService* prefs) const;
 
   IntegerPrefMember alternative_search_engine_provider_in_tor_;
 
