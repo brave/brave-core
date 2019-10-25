81cdabd9f125cbfa9f3f097dca587a74255c0cd4
Mark Pilgrim <mpilgrim@brave.com>
2019-10-25 12:58:32 -0400

    HandleExternalProtocol now takes initiating_origin
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/13b66bdf6719bcfedf549ff8ce2b18d2279e0396
    
    commit    13b66bdf6719bcfedf549ff8ce2b18d2279e0396
    author    Emily Stark <estark@google.com>
    date      Mon Oct 14 11:11:33 2019
    
    Add origin to external protocol dialog
    
    This is a stopgap fix for the fact that it's unclear which frame
    triggered a navigation to an external protocol. Eventually, we want to
    implement some more robust fixes, like preventing a frame from
    navigating another cross-origin frame to an external protocol, but
    that'll involve a bit more investigation and metrics (to be added in a
    follow-up CL).
    
    Bug: 754304

--- browser/brave_content_browser_client.cc
+++ browser/brave_content_browser_client.cc
@@ -154,6 +154,7 @@ bool BraveContentBrowserClient::HandleExternalProtocol(
     bool is_main_frame,
     ui::PageTransition page_transition,
     bool has_user_gesture,
+    const base::Optional<url::Origin>& initiating_origin,
     network::mojom::URLLoaderFactoryPtr* out_factory) {
 #if BUILDFLAG(ENABLE_BRAVE_WEBTORRENT)
   if (webtorrent::HandleMagnetProtocol(url, web_contents_getter,
@@ -171,7 +172,7 @@ bool BraveContentBrowserClient::HandleExternalProtocol(
 
   return ChromeContentBrowserClient::HandleExternalProtocol(
       url, web_contents_getter, child_id, navigation_data, is_main_frame,
-      page_transition, has_user_gesture, out_factory);
+      page_transition, has_user_gesture, initiating_origin, out_factory);
 }
 
 base::Optional<service_manager::Manifest>
