8fc2a4e998bd7baa332f627b6a7c9f1a2b313aae
Kyle Hickinson <git@kylehickinson.com>
2019-10-28 11:11:20 -0400

    Update iOS wrapper to support new attestation APIs

---
+++ vendor/brave-ios/Ledger/Models/BATPromotionSolution.m
@@ -0,0 +1,24 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#import "BATPromotionSolution.h"
+
+@implementation BATPromotionSolution
+
+- (NSString *)JSONPayload
+{
+  NSDictionary *payload = @{
+    @"noonce": self.noonce,
+    @"blob": self.blob,
+    @"signature": self.signature
+  };
+  NSData *jsonData = [NSJSONSerialization dataWithJSONObject:payload options:0 error:nil];
+  if (!jsonData) {
+    NSLog(@"Missing JSON payload while attempting to attest promotion");
+    return @"";
+  }
+  return [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+}
+
+@end
