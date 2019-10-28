bfbe52e70b2f6e04fb0f6ad5bf33fc83d1e269b6
mkarolin <max@brave.com>
2019-10-28 11:58:43 -0400

    PageActionIcon interface changes.
    Also, Bookmark Star refactoring.
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/c7f0eff7fa72fdacba77cfc4cc57dc3044ae5332
    
    commit c7f0eff7fa72fdacba77cfc4cc57dc3044ae5332
    Author: Alan Cutter <alancutter@chromium.org>
    Date:   Mon Sep 16 02:17:32 2019 +0000
    
        Make PageActionIcon interface on BrowserWindow agnostic to containers
    
        This CL replaces
    
        BrowserWindow::GetOmniboxPageActionIconContainer()
        BrowserWindow::GetToolbarPageActionIconContainer()
    
        with
    
        BrowserWindow::UpdatePageActionIcon(type)
        BrowserWindow::ExecutePageActionIconForTesting(type)
    
        in order to hide the implementation detail of which container any
        given icon resides in.
    
        This is in preparation for adding more page-action-icons to PWA windows
        which only have one container.
    
        This is a pure refactor with no behavioural changes.
    
        Bug: 788051
    
    https://chromium.googlesource.com/chromium/src/+/35e8900d3d909a7797eb7173d2cd6140813e5057
    
    commit 35e8900d3d909a7797eb7173d2cd6140813e5057
    Author: Alan Cutter <alancutter@chromium.org>
    Date:   Wed Sep 18 04:40:32 2019 +0000
    
        Refactor the bookmark star icon to match all other PageActionIconViews
    
        This is a cleanup refactor, the bookmark star is the last PageActionIconView
        that isn't contained by OmniboxPageActionIconContainerView.
    
        This CL updates its implementation to conform with all the other PAIVs
        and remove specialised LocationBar methods now that it fits
        generic interface.
    
        Bug: 788051

---
+++ browser/ui/views/location_bar/brave_star_view.h
@@ -0,0 +1,22 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_BROWSER_UI_VIEWS_LOCATION_BAR_BRAVE_STAR_VIEW_H_
+#define BRAVE_BROWSER_UI_VIEWS_LOCATION_BAR_BRAVE_STAR_VIEW_H_
+
+#include "chrome/browser/ui/views/location_bar/star_view.h"
+
+class BraveStarView : public StarView {
+ public:
+  using StarView::StarView;
+
+ protected:
+  // views::View:
+  bool Update() override;
+
+  DISALLOW_COPY_AND_ASSIGN(BraveStarView);
+};
+
+#endif  // BRAVE_BROWSER_UI_VIEWS_LOCATION_BAR_BRAVE_STAR_VIEW_H_
