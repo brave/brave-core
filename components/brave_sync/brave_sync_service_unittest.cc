980be12588b1eabf5396c60ae986b7e30bbf04ab
mkarolin <max@brave.com>
2019-10-28 17:11:16 -0400

    TestBrowserThreadBundle was removed.
    Along with a few other test classes, see below.
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/2dfa2ba40a372f4a08ba0d051b5cbe5accf2c526
    
    commit 2dfa2ba40a372f4a08ba0d051b5cbe5accf2c526
    Author: Gabriel Charette <gab@chromium.org>
    Date:   Tue Sep 10 00:28:54 2019 +0000
    
        [TaskEnvironment] Remove old types
    
        Explicitly (to document for git log purposes), this removes:
          - base::test::ScopedTaskEnvironment
          - net::WithScopedTaskEnvironment
          - net::TestWithScopedTaskEnvironment
          - content::TestBrowserThreadBundle
          - web::TestWebThreadBundle
    
        Mass migrate stragglers which landed under old names after initial
        mass migration.
    
        TBR=dcheng@chromium.org
        (to bypass owners for mechanical changes outside //base)
    
        Bug: 992483

--- components/brave_sync/brave_sync_service_unittest.cc
+++ components/brave_sync/brave_sync_service_unittest.cc
@@ -228,7 +228,7 @@ class BraveSyncServiceTest : public testing::Test {
   // Need this as a very first member to run tests in UI thread
   // When this is set, class should not install any other MessageLoops, like
   // base::test::ScopedTaskEnvironment
-  content::TestBrowserThreadBundle thread_bundle_;
+  content::BrowserTaskEnvironment task_environment_;
 
   std::unique_ptr<Profile> profile_;
   BraveProfileSyncServiceImpl* sync_service_;
