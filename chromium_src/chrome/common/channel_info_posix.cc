7cbd40e9dd46801c41bfef425553745351885602
Max Karolinskiy <max@brave.com>
2019-11-01 17:05:00 -0400

    Linux: add GetDesktopName to override of channel_info_posix.cc
    The function used to be defined elesewhere but was moved to
    chrome/commin/channel_info_posix.cc which we completely override
    in chromium_src. Added the definition to our override minus
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/b8a8603ff3b2caf0106edec3d33093125e4a67cf
    
    commit b8a8603ff3b2caf0106edec3d33093125e4a67cf
    Author: Tom Anderson <thomasanderson@chromium.org>
    Date:   Wed Oct 9 20:12:45 2019 +0000
    
        Move unity_service out of libgtkui
    
        unity_service didn't have any dependencies on GTK, so it doesn't belong
        there.  We want libgtkui to be as small as possible because in the
        future we want to have alternate implementations of LinuxUi for eg. QT5.
    
        BUG=None

--- chromium_src/chrome/common/channel_info_posix.cc
+++ chromium_src/chrome/common/channel_info_posix.cc
@@ -4,6 +4,7 @@
 
 #include "chrome/common/channel_info.h"
 
+#include "base/environment.h"
 #include "base/strings/string_util.h"
 #include "brave/common/brave_channel_info_posix.h"
 #include "build/build_config.h"
@@ -17,9 +18,31 @@ std::string GetChannelName() {
   return modifier;
 }
 
+#if defined(OS_LINUX)
+std::string GetDesktopName(base::Environment* env) {
+#if defined(OFFICIAL_BUILD)
+  version_info::Channel product_channel(chrome::GetChannel());
+  switch (product_channel) {
+    case version_info::Channel::DEV:
+      return "brave-browser-dev.desktop";
+    case version_info::Channel::BETA:
+      return "brave-browser-beta.desktop";
+    default:
+      return "brave-browser.desktop";
+  }
+#endif  // defined(OFFICIAL_BUILD)
+  // Allow $CHROME_DESKTOP to override the built-in value, so that development
+  // versions can set themselves as the default without interfering with
+  // non-official, packaged versions using the built-in value.
+  std::string name;
+  if (env->GetVar("CHROME_DESKTOP", &name) && !name.empty())
+    return name;
+  return "brave-browser.desktop";
+}
+#endif  // defined(OS_LINUX)
+
 version_info::Channel GetChannel() {
   return brave::GetChannelImpl(nullptr, nullptr);
 }
 
 }  // namespace chrome
-
