2f7371e9b98ff37763ed9e97971142618d1184b6
Francois Marier <francois@brave.com>
2019-11-01 15:05:18 -0700

    Add licensing terms for background photos

--- components/brave_new_tab_ui/data/backgrounds.ts
+++ components/brave_new_tab_ui/data/backgrounds.ts
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// After editing this file, don't forget to update LICENSE as well.
+
 export const images: NewTab.Image[] = [
   {
     'name': '1',
