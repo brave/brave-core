769f76d5e7a83d7ee5ce6500411d377826bace9b
Jocelyn Liu <yrliou@gmail.com>
2019-11-08 10:37:10 -0800

    Move ads java files and its jni into browser/brave_ads instead.

--- browser/brave_ads/android/brave_ads_native_helper.cc
+++ browser/brave_ads/android/brave_ads_native_helper.cc
@@ -5,9 +5,9 @@
 
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
+#include "brave/browser/brave_ads/android/jni_headers/BraveAdsNativeHelper_jni.h"
 #include "brave/components/brave_ads/browser/ads_service.h"
 #include "brave/components/brave_ads/browser/ads_service_factory.h"
-#include "brave/components/brave_ads/browser/android/jni_headers/BraveAdsNativeHelper_jni.h"
 #include "brave/components/brave_ads/browser/locale_helper_android.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_android.h"
