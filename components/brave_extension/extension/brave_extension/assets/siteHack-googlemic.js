d74f9e8514a0540b46a97e540f2180d25e6dd50c
fanboynz <mp3geek@gmail.com>
2019-11-12 08:17:24 +0100

    Add variable check, before we hide it

--- components/brave_extension/extension/brave_extension/assets/siteHack-googlemic.js
+++ components/brave_extension/extension/brave_extension/assets/siteHack-googlemic.js
@@ -1,3 +1,10 @@
 // Stop the google mic from showing up.
 // https://github.com/brave/brave-browser/issues/2690
-document.querySelector('[aria-label="Search by voice"]').style.visibility = 'hidden';
+
+var selection = document.querySelector('[aria-label="Search by voice"]')!== null;
+if (selection) {
+  document.querySelector('[aria-label="Search by voice"]').style.visibility = 'hidden';
+} else {
+  console.warn('The element aria-label does not exist in the page.');
+}
+
