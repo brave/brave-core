44dd692c2ef370dc3f50aa8047b2155d47e61d0a
Terry Mancey <terry.mancey@icloud.com>
2019-11-21 05:03:10 +0000

    Add ledger state properties in preparation for transition to mojom

---
+++ vendor/bat-native-ledger/src/bat/ledger/internal/properties/publisher_settings_properties.cc
@@ -0,0 +1,55 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "bat/ledger/internal/properties/publisher_settings_properties.h"
+#include "bat/ledger/internal/static_values.h"
+
+namespace ledger {
+
+PublisherSettingsProperties::PublisherSettingsProperties()
+    : min_page_time_before_logging_a_visit(
+        braveledger_ledger::_default_min_page_time_before_logging_a_visit),
+      min_visits_for_publisher_relevancy(1),
+      allow_non_verified_sites_in_list(true),
+      allow_contribution_to_videos(true),
+      migrate_score_2(false) {}
+
+PublisherSettingsProperties::PublisherSettingsProperties(
+    const PublisherSettingsProperties& properties) {
+  min_page_time_before_logging_a_visit =
+      properties.min_page_time_before_logging_a_visit;
+  min_visits_for_publisher_relevancy =
+      properties.min_visits_for_publisher_relevancy;
+  allow_non_verified_sites_in_list =
+      properties.allow_non_verified_sites_in_list;
+  allow_contribution_to_videos = properties.allow_contribution_to_videos;
+  monthly_balances = properties.monthly_balances;
+  migrate_score_2 = properties.migrate_score_2;
+  processed_pending_publishers = properties.processed_pending_publishers;
+}
+
+PublisherSettingsProperties::~PublisherSettingsProperties() = default;
+
+bool PublisherSettingsProperties::operator==(
+    const PublisherSettingsProperties& rhs) const {
+  return
+      min_page_time_before_logging_a_visit ==
+          rhs.min_page_time_before_logging_a_visit &&
+      min_visits_for_publisher_relevancy ==
+          rhs.min_visits_for_publisher_relevancy &&
+      allow_non_verified_sites_in_list ==
+          rhs.allow_non_verified_sites_in_list &&
+      allow_contribution_to_videos == rhs.allow_contribution_to_videos &&
+      monthly_balances == rhs.monthly_balances &&
+      migrate_score_2 == rhs.migrate_score_2 &&
+      processed_pending_publishers == rhs.processed_pending_publishers;
+}
+
+bool PublisherSettingsProperties::operator!=(
+    const PublisherSettingsProperties& rhs) const {
+  return !(*this == rhs);
+}
+
+}  // namespace ledger
