639f152a99a477bc03d34518e5494b007135dd76
Brandon-T <bthomas@brave.com>
2019-11-22 15:36:07 -0500

    Fix brave/brave-ios#1739: AdsNotification dynamically update theme based on system dark mode (brave/brave-ios#1980)

--- BraveRewardsUI/Ads/AdsViewController.swift
+++ BraveRewardsUI/Ads/AdsViewController.swift
@@ -332,3 +332,15 @@ extension AdsViewController {
     })
   }
 }
+
+extension AdsViewController {
+  public override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+    super.traitCollectionDidChange(previousTraitCollection)
+    
+    if #available(iOS 13.0, *) {
+        if UITraitCollection.current.userInterfaceStyle != previousTraitCollection?.userInterfaceStyle {
+          visibleAdView?.adContentButton.applyTheme(for: traitCollection)
+        }
+    }
+  }
+}
