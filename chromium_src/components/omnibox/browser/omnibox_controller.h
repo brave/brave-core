07f8c7d9325c90b0ae5ee67fd238835f5d41805b
Simon Hong <shong@igalia.com>
2019-11-26 08:52:05 +0900

    Use override technique for omnibox_controller.h

---
+++ chromium_src/components/omnibox/browser/omnibox_controller.h
@@ -0,0 +1,13 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_OMNIBOX_BROWSER_OMNIBOX_CONTROLLER_H_
+#define BRAVE_CHROMIUM_SRC_COMPONENTS_OMNIBOX_BROWSER_OMNIBOX_CONTROLLER_H_
+
+#define StartAutocomplete virtual StartAutocomplete
+#include "../../../../../components/omnibox/browser/omnibox_controller.h"  // NOLINT
+#undef StartAutocomplete
+
+#endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_OMNIBOX_BROWSER_OMNIBOX_CONTROLLER_H_
