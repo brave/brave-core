6590e86ebe7e73d16951a5a2c2789a3d6bb4702a
Terry Mancey <terry.mancey@icloud.com>
2019-11-29 14:15:59 +0000

    Fixes wrong ads server url for ads/confirmations development environment

--- vendor/bat-native-ads/src/bat/ads/internal/static_values.h
+++ vendor/bat-native-ads/src/bat/ads/internal/static_values.h
@@ -16,7 +16,7 @@ namespace ads {
 
 #define PRODUCTION_SERVER "https://ads-serve.brave.com"
 #define STAGING_SERVER "https://ads-serve.bravesoftware.com"
-#define DEVELOPMENT_SERVER "https://ads-serve.brave.software.com"
+#define DEVELOPMENT_SERVER "https://ads-serve.brave.software"
 
 #define CATALOG_PATH "/v2/catalog"
 
