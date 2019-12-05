1eb46c1c55b5eef6698dca2ea501d50725ce6834
Cezar Augusto <boss@cezaraugusto.net>
2019-12-04 21:21:14 -0300

    move back front-end logic to chrome://adblock

--- components/brave_adblock_ui/components/app.tsx
+++ components/brave_adblock_ui/components/app.tsx
@@ -11,6 +11,9 @@ import { AdBlockItemList } from './adBlockItemList'
 import { CustomFilters } from './customFilters'
 import { NumBlockedStat } from './numBlockedStat'
 
+// Components for playlist
+import Table, { Cell, Row } from 'brave-ui/components/dataTables/table'
+
 // Utils
 import * as adblockActions from '../actions/adblock_actions'
 
@@ -19,19 +22,106 @@ interface Props {
   adblockData: AdBlock.State
 }
 
-export class AdblockPage extends React.Component<Props, {}> {
+interface State {
+  playlists: any
+}
+
+export class AdblockPage extends React.Component<Props, State> {
   constructor (props: Props) {
     super(props)
+    this.state = { playlists: [] }
+    this.getPlaylist()
   }
 
   get actions () {
     return this.props.actions
   }
 
+  getPlaylist = () => {
+    chrome.bravePlaylists.getAllPlaylists(playlists => {
+      this.setState({ playlists })
+    })
+  }
+
+  componentDidMount () {
+    chrome.bravePlaylists.onPlaylistsChanged.addListener((changeType, id) => {
+      this.getPlaylist()
+    })
+  }
+
+  componentDidUpdate (prevProps: any, prevState: any) {
+    if (JSON.stringify(prevState.playlists) !== JSON.stringify(this.state.playlists)) {
+      this.getPlaylist()
+    }
+  }
+
+  getPlaylistHeader = (): Cell[] => {
+    return [
+      { content: 'NAME' }
+    ]
+  }
+
+  get lazyButtonStyle () {
+    const lazyButtonStyle: any = {
+      alignItems: 'center',
+      WebkitAppearance: 'none',
+      width: '50px',
+      height: '50px',
+      display: 'flex',
+      borderRadius: '4px'
+    }
+    return lazyButtonStyle
+  }
+
+  getPlaylistRows = (playlist?: any): Row[] | undefined => {
+    if (playlist == null) {
+      return
+    }
+
+    return playlist.map((item: any, index: any): any => {
+      const cell: Row = {
+        content: [
+          { content: (
+            <div>
+              <h3>{item.playlistName}</h3>
+              <video
+                controls={true}
+                width={640}
+                poster={item.thumbnailUrl}
+                // super hack courtesy of cezar augusto
+                // TODO: this should show the poster at least
+                // or maybe just a unordered list with the video title
+                onClick={this.onClickPlayVideo.bind(this, item.id)}
+              >
+                <source src={item.videoMediaFilePath} type='video/mp4' />
+              </video>
+              {/* <video style={{ display: 'none' }}>
+                <source src={item.audioMediaFilePath} type='video/mp4' />
+              </video> */}
+            </div>
+          ) }
+        ]
+      }
+      return cell
+    })
+  }
+
+  onClickPlayVideo = (playlistId: string) => {
+    console.log('method called. playlist id:', playlistId)
+    chrome.bravePlaylists.play(playlistId)
+  }
+
   render () {
     const { actions, adblockData } = this.props
+    const { playlists } = this.state
     return (
       <div id='adblockPage'>
+        <div style={{ minHeight: '600px', width: '1200px' }}>
+          <Table header={this.getPlaylistHeader()} rows={this.getPlaylistRows(playlists)}>
+            YOUR PLAYLIST IS NOW EMPTY
+          </Table>
+        </div>
+        <hr />
         <NumBlockedStat adsBlockedStat={adblockData.stats.adsBlockedStat || 0} />
         <AdBlockItemList
           actions={actions}
--- components/brave_extension/extension/brave_extension/containers/braveShields.tsx
+++ components/brave_extension/extension/brave_extension/containers/braveShields.tsx
@@ -7,90 +7,55 @@ import * as React from 'react'
 // CSS normalizer
 import 'emptykit.css'
 
-import { Cell } from 'brave-ui/components/dataTables/table'
+// Types
+import { Tab, PersistentData } from '../types/state/shieldsPannelState'
+import {
+  ShieldsToggled,
+  ReportBrokenSite,
+  BlockAdsTrackers,
+  HttpsEverywhereToggled,
+  BlockJavaScript,
+  BlockFingerprinting,
+  BlockCookies,
+  AllowScriptOriginsOnce,
+  SetScriptBlockedCurrentState,
+  SetGroupedScriptsBlockedCurrentState,
+  SetAllScriptsBlockedCurrentState,
+  SetFinalScriptsBlockedState,
+  SetAdvancedViewFirstAccess,
+  ShieldsReady
+} from '../types/actions/shieldsPanelActions'
 
-const VideoFile = require('../video_file.mp4')
-const AudioFile = require('../audio_file.m4a')
+interface Props {
+  actions: {
+    shieldsToggled: ShieldsToggled
+    reportBrokenSite: ReportBrokenSite
+    blockAdsTrackers: BlockAdsTrackers
+    httpsEverywhereToggled: HttpsEverywhereToggled
+    blockJavaScript: BlockJavaScript
+    blockFingerprinting: BlockFingerprinting
+    blockCookies: BlockCookies
+    allowScriptOriginsOnce: AllowScriptOriginsOnce
+    setScriptBlockedCurrentState: SetScriptBlockedCurrentState
+    setGroupedScriptsBlockedCurrentState: SetGroupedScriptsBlockedCurrentState
+    setAllScriptsBlockedCurrentState: SetAllScriptsBlockedCurrentState
+    setFinalScriptsBlockedState: SetFinalScriptsBlockedState
+    setAdvancedViewFirstAccess: SetAdvancedViewFirstAccess
+    shieldsReady: ShieldsReady
+  }
+  shieldsPanelTabData: Tab
+  persistentData: PersistentData
+  settings: any
+}
 
 interface State {
   url: string
-  playlists: any
 }
 
-export default class Shields extends React.PureComponent<{}, State> {
-  videoRef: React.RefObject<any>
-  audioRef: React.RefObject<any>
-  constructor (props: {}) {
+export default class Shields extends React.PureComponent<Props, State> {
+  constructor (props: Props) {
     super(props)
-    this.state = {
-      url: '',
-      playlists: []
-    }
-    // fetch playlists right away
-    this.getPlaylist()
-    // create refs for audio/video
-    // TODO: cezaraugusto this won't work with multiple videos
-    // good way to solve this is to create a separate component
-    // that we can then map and so encapsulate state for each of them
-    this.videoRef = React.createRef()
-    this.audioRef = React.createRef()
-  }
-
-  getPlaylist = () => {
-    chrome.bravePlaylists.getAllPlaylists(playlists => {
-      this.setState({ playlists })
-    })
-  }
-
-  componentDidMount () {
-    this.getActiveTabUrl()
-    chrome.bravePlaylists.onPlaylistsChanged.addListener((changeType, id) => {
-      this.getPlaylist()
-    })
-  }
-
-
-  onPlayVideo = () => {
-    // play event is called whenever user change the video timeline
-    // ensure we're in sync with audio by adopting the same current time from video
-    this.audioRef.current.currentTime = this.videoRef.current.currentTime
-    // the play event will take care of playing the video,
-    // we just need to call the audio to play along
-    this.audioRef.current.play()
-  }
-
-  onPauseVideo = () => {
-    // video is paused. pause the audio too so they remain in sync
-    this.audioRef.current.pause()
-  }
-
-  get pageHasDownloadableVideo () {
-    return this.state.url.startsWith('https://www.youtube.com/watch')
-  }
-
-  getActiveTabUrl = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-      const activeTab = tabs[0]
-      if (activeTab.url) {
-        this.setState({ url: activeTab.url })
-      }
-    })
-  }
-
-  onClickDownloadVideo = (url: string) => {
-    chrome.bravePlaylists.requestDownload(url)
-  }
-
-  componentDidUpdate (prevProps: any, prevState: any) {
-    if (JSON.stringify(prevState.playlists) !== JSON.stringify(this.state.playlists)) {
-      this.getPlaylist()
-    }
-  }
-
-  getPlaylistHeader = (): Cell[] => {
-    return [
-      { content: 'NAME' }
-    ]
+    this.state = { url: '' }
   }
 
   get lazyButtonStyle () {
@@ -105,16 +70,21 @@ export default class Shields extends React.PureComponent<{}, State> {
     return lazyButtonStyle
   }
 
-  onClickRemoveALlVideos = () => {
-    console.log('nothing')
+  onClickDownloadVideo = (url: string) => {
+    chrome.bravePlaylists.requestDownload(url)
   }
 
-  onClickRemoveVideo = () => {
-    console.log('nothing')
+  get pageHasDownloadableVideo () {
+    return this.state.url.startsWith('https://www.youtube.com/watch')
   }
 
-  onClickPlayVideo = () => {
-    console.log('nothing')
+  getActiveTabUrl = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      const activeTab = tabs[0]
+      if (activeTab.url) {
+        this.setState({ url: activeTab.url })
+      }
+    })
   }
 
   render () {
@@ -133,23 +103,6 @@ export default class Shields extends React.PureComponent<{}, State> {
         >
           Click here to download
         </button>
-        <hr />
-        <div style={{ minHeight: '600px', width: '100%' }}>
-          <h1>Your playlist</h1>
-          <video
-            ref={this.videoRef}
-            controls={true}
-            width={370}
-            poster={''}
-            onPlay={this.onPlayVideo}
-            onPause={this.onPauseVideo}
-          >
-            <source src={VideoFile} type='video/mp4' />
-          </video>
-          <video ref={this.audioRef} style={{ display: 'none' }}>
-            <source src={AudioFile} type='audio/m4a' />
-          </video>
-        </div>
       </div>
     )
   }
