e31b66d3343193ab10040a895506b94552d1bbec
mkarolin <max@brave.com>
2019-12-06 18:56:04 -0500

    ContentSettingsType is now an enum class.
    Changed all CONTENT_SETTINGS_TYPE_ to ContentSettingsType::
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/42f5e7d92c0265d4fab93addb5cec0f252df6265
    
    commit 42f5e7d92c0265d4fab93addb5cec0f252df6265
    Author: Darin Fisher <darin@chromium.org>
    Date:   Wed Oct 30 07:15:45 2019 +0000
    
        Turn ContentSettingsType into an enum class type.
    
        This is a precursor to defining ContentSettingsType via mojom. That change
        will require more work as Clank depends on an Java definition of this type
        that is auto-generated from content_settings_types.h.

--- chromium_src/chrome/browser/ui/webui/site_settings_helper.cc
+++ chromium_src/chrome/browser/ui/webui/site_settings_helper.cc
@@ -1,3 +1,8 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 #define HasRegisteredGroupName HasRegisteredGroupName_ChromiumImpl
 #define ContentSettingsTypeFromGroupName ContentSettingsTypeFromGroupName_ChromiumImpl
 #define ContentSettingsTypeToGroupName ContentSettingsTypeToGroupName_ChromiumImpl
@@ -9,19 +14,19 @@
 namespace site_settings {
 
 bool HasRegisteredGroupName(ContentSettingsType type) {
-  if (type == CONTENT_SETTINGS_TYPE_AUTOPLAY)
+  if (type == ContentSettingsType::AUTOPLAY)
     return true;
   return HasRegisteredGroupName_ChromiumImpl(type);
 }
 
 ContentSettingsType ContentSettingsTypeFromGroupName(const std::string& name) {
   if (name == "autoplay")
-    return CONTENT_SETTINGS_TYPE_AUTOPLAY;
+    return ContentSettingsType::AUTOPLAY;
   return ContentSettingsTypeFromGroupName_ChromiumImpl(name);
 }
 
 std::string ContentSettingsTypeToGroupName(ContentSettingsType type) {
-  if (type == CONTENT_SETTINGS_TYPE_AUTOPLAY)
+  if (type == ContentSettingsType::AUTOPLAY)
     return "autoplay";
   return ContentSettingsTypeToGroupName_ChromiumImpl(type);
 }
--- components/content_settings/core/browser/brave_content_settings_pref_provider.h
+++ components/content_settings/core/browser/brave_content_settings_pref_provider.h
@@ -19,7 +19,7 @@
 namespace content_settings {
 
 // With this subclass, shields configuration is persisted across sessions.
-// Its content type is |CONTENT_SETTINGS_TYPE_PLUGIN| and its storage option is
+// Its content type is |ContentSettingsType::PLUGIN| and its storage option is
 // ephemeral because chromium want that flash configuration shouldn't be
 // persisted. (Maybe chromium assumes flash is the only one of this type).
 // Because of this reasion, shields configuration was also ephemeral.
