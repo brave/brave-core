0b95b05b822374e6dec1354715bf6fa4c613c674
Malcolm Sparrow <msparrow@maksw.com>
2019-12-06 16:59:25 +0000

    Follow up to #6684 - Alignment issue for `Token grants claimed` in BR panel
    Resolves https://github.com/brave/brave-browser/issues/7198

--- components/brave_rewards/resources/ui/components/listToken/style.ts
+++ components/brave_rewards/resources/ui/components/listToken/style.ts
@@ -19,11 +19,12 @@ export const StyledWrapper = styled<Partial<Props>, 'div'>('div')`
 
 export const StyledTitle = styled<{}, 'div'>('div')`
   font-size: 14px;
-  line-height: 2.79;
+  line-height: 1.3;
   color: #4b4c5c;
   flex-grow: 1;
   flex-shrink: 1;
   flex-basis: 60%;
+  padding: 10px 0px;
 `
 
 export const StyledContentWrapper = styled<{}, 'div'>('div')`
