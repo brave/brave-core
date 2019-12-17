c6e4feca6085aac5bbe61a2377fac9d221c55173
samartnik <artem@brave.com>
2019-12-17 07:19:25 -0500

    Python code clean up. Renamed variables.

--- build/android/sign_apk.py
+++ build/android/sign_apk.py
@@ -2,33 +2,35 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import argparse
 import sys
 import subprocess
 import tempfile
 
-if len(sys.argv) != 8:
-  print("Incorrect number of parameters")
-  print("Usage: " + sys.argv[0] + " <zipalign_path>, <apksigner_path>, <unsigned_apk_path>, <key_path>, <key_passwd>, <prvt_key_passwd>, <key_name>")
-  sys.exit(1)
+def main():
+  argument_parser = argparse.ArgumentParser()
+  argument_parser.add_argument('zipalign_path')
+  argument_parser.add_argument('apksigner_path')
+  argument_parser.add_argument('unsigned_apk_path')
+  argument_parser.add_argument('key_path')
+  argument_parser.add_argument('key_passwd')
+  argument_parser.add_argument('prvt_key_passwd')
+  argument_parser.add_argument('key_name')
+  args = argument_parser.parse_args()
 
-zipalign_path = sys.argv[1]
-apksigner_path = sys.argv[2]
-unsigned_apk_path = sys.argv[3]
-key_path = sys.argv[4]
-key_passwd = sys.argv[5]
-prvt_key_passwd = sys.argv[6]
-key_name = sys.argv[7]
+  with tempfile.NamedTemporaryFile() as staging_file:
+    subprocess.check_output([
+        args.zipalign_path, '-p', '-f', '4',
+        args.unsigned_apk_path, staging_file.name])
+    subprocess.check_output([
+        args.apksigner_path, 'sign',
+        '--in', staging_file.name,
+        '--out', args.unsigned_apk_path,
+        '--ks', args.key_path,
+        '--ks-key-alias', args.key_name,
+        '--ks-pass', 'pass:' + args.key_passwd,
+        '--key-pass', 'pass:' + args.prvt_key_passwd,
+    ])
 
-with tempfile.NamedTemporaryFile() as staging_file:
-  subprocess.check_output([
-      zipalign_path, '-p', '-f', '4',
-      unsigned_apk_path, staging_file.name])
-  subprocess.check_output([
-      apksigner_path, 'sign',
-      '--in', staging_file.name,
-      '--out', unsigned_apk_path,
-      '--ks', key_path,
-      '--ks-key-alias', key_name,
-      '--ks-pass', 'pass:' + key_passwd,
-      '--key-pass', 'pass:' + prvt_key_passwd,
-  ])
+if __name__ == '__main__':
+  sys.exit(main())
