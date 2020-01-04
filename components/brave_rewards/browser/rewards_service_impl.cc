dfffdaa0a6c75d89782ee7f30dd82247f5f83122
mkarolin <max@brave.com>
2020-01-03 20:39:07 -0500

    ConnectionManager Connector::BindInterface now takes a PendingReceiver.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/ab468693011a4447f8616d3cafaee6d82f688ed8
    
    commit ab468693011a4447f8616d3cafaee6d82f688ed8
    Author: Gyuyoung Kim <gyuyoung@igalia.com>
    Date:   Thu Dec 5 04:55:58 2019 +0000
    
        Convert InterfaceRequest to PendingReceiver in Connector::BindInterface
    
        This CL converts the argument type from mojo::InterfaceRequest<Interface>
        to mojo::PendingReceiver<Interface>. And, all callers start to call
        it with a pending receiver.
    
        Additionally. this CL renames ForwardRequest to ForwardReceiver.
    
        Bug: 955171

--- components/brave_rewards/browser/rewards_service_impl.cc
+++ components/brave_rewards/browser/rewards_service_impl.cc
@@ -503,8 +503,9 @@ void RewardsServiceImpl::StartLedger() {
   }
 
   connection->GetConnector()->BindInterface(
-      bat_ledger::mojom::kServiceName, &bat_ledger_service_);
-  bat_ledger_service_.set_connection_error_handler(
+      bat_ledger::mojom::kServiceName,
+      bat_ledger_service_.BindNewPipeAndPassReceiver());
+  bat_ledger_service_.set_disconnect_handler(
       base::Bind(&RewardsServiceImpl::ConnectionClosed, AsWeakPtr()));
 
   ledger::Environment environment = ledger::Environment::STAGING;
