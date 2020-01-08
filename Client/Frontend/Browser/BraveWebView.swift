7de5b36a5aae9044fad8ff381259c1bd68a74bb0
Brandon-T <bthomas@brave.com>
2020-01-08 14:21:20 -0500

    Fix brave/brave-ios#2162: Crash when javascript's `window.open` creates a new tab.(brave/brave-ios#2161)

--- Client/Frontend/Browser/BraveWebView.swift
+++ Client/Frontend/Browser/BraveWebView.swift
@@ -25,7 +25,6 @@ class BraveWebView: WKWebView {
         if isPrivate {
             configuration.websiteDataStore = BraveWebView.sharedNonPersistentStore()
         } else {
-            BraveWebView.nonPersistentDataStore = nil
             configuration.websiteDataStore = WKWebsiteDataStore.default()
         }
         
