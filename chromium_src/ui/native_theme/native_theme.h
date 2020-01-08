7a011de6bbd0888146ff19dbcff6cb86d7e06bdb
Emerick Rogul <erogul@brave.com>
2020-01-08 11:09:06 -0500

    Use a stubbed NativeTheme on Android to allow favicons to work

--- chromium_src/ui/native_theme/native_theme.h
+++ chromium_src/ui/native_theme/native_theme.h
@@ -6,9 +6,10 @@
 #ifndef BRAVE_CHROMIUM_SRC_UI_NATIVE_THEME_NATIVE_THEME_H_
 #define BRAVE_CHROMIUM_SRC_UI_NATIVE_THEME_NATIVE_THEME_H_
 
-#define BRAVE_UI_NATIVE_THEME_NATIVE_THEME_H_ \
-  friend void SetUseDarkColors(bool dark_mode); \
-  friend void ReCalcAndSetPreferredColorScheme();
+#define BRAVE_UI_NATIVE_THEME_NATIVE_THEME_H_     \
+  friend void SetUseDarkColors(bool dark_mode);   \
+  friend void ReCalcAndSetPreferredColorScheme(); \
+  static NativeTheme* BraveGetInstanceForNativeUi();
 
 #include "../../../../ui/native_theme/native_theme.h"
 #undef BRAVE_UI_NATIVE_THEME_NATIVE_THEME_H_
---
+++ chromium_src/ui/native_theme/native_theme_android.cc
@@ -0,0 +1,14 @@
+// Copyright (c) 2020 The Brave Authors
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "../../../../../ui/native_theme/native_theme_android.cc"
+
+ui::NativeTheme* ui::NativeTheme::BraveGetInstanceForNativeUi() {
+  struct StubNativeThemeAndroid : public ui::NativeThemeAndroid {
+    bool ShouldUseDarkColors() const override { return false; }
+  };
+  static base::NoDestructor<StubNativeThemeAndroid> s_native_theme;
+  return s_native_theme.get();
+}
