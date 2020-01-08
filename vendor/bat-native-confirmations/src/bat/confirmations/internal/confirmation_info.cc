0665ec8688107540883390e0fcbbbef72621d428
Terry Mancey <terry.mancey@icloud.com>
2020-01-08 09:01:07 +0000

    Fixes server error 500 can cause Confirmations to get stuck in the queue

--- vendor/bat-native-confirmations/src/bat/confirmations/internal/confirmation_info.cc
+++ vendor/bat-native-confirmations/src/bat/confirmations/internal/confirmation_info.cc
@@ -7,28 +7,16 @@
 
 namespace confirmations {
 
-ConfirmationInfo::ConfirmationInfo() :
-    id(""),
-    creative_instance_id(""),
-    type(ConfirmationType::UNKNOWN),
-    token_info(TokenInfo()),
-    payment_token(nullptr),
-    blinded_payment_token(nullptr),
-    credential(""),
-    timestamp_in_seconds(0),
-    created(false) {}
+ConfirmationInfo::ConfirmationInfo()
+    : type(ConfirmationType::UNKNOWN),
+      payment_token(nullptr),
+      blinded_payment_token(nullptr),
+      timestamp_in_seconds(0),
+      created(false) {}
 
-ConfirmationInfo::ConfirmationInfo(const ConfirmationInfo& info) :
-    id(info.id),
-    creative_instance_id(info.creative_instance_id),
-    type(info.type),
-    token_info(info.token_info),
-    payment_token(info.payment_token),
-    blinded_payment_token(info.blinded_payment_token),
-    credential(info.credential),
-    timestamp_in_seconds(info.timestamp_in_seconds),
-    created(info.created) {}
+ConfirmationInfo::ConfirmationInfo(
+    const ConfirmationInfo& info) = default;
 
-ConfirmationInfo::~ConfirmationInfo() {}
+ConfirmationInfo::~ConfirmationInfo() = default;
 
 }  // namespace confirmations
