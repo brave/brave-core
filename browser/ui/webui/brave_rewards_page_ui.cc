37b01261a8de90adb9272b38c61880b1cf4b8e9c
George Dregalo <37560650+gdregalo@users.noreply.github.com>
2020-01-09 10:14:12 -0500

    Calling Android specific ClaimPromotion function

--- browser/ui/webui/brave_rewards_page_ui.cc
+++ browser/ui/webui/brave_rewards_page_ui.cc
@@ -612,10 +612,16 @@ void RewardsDOMHandler::ClaimPromotion(const base::ListValue* args) {
   }
 
   const std::string promotion_id = args->GetList()[0].GetString();
+
+#if !defined(OS_ANDROID)
   rewards_service_->ClaimPromotion(
       base::Bind(&RewardsDOMHandler::OnClaimPromotion,
           weak_factory_.GetWeakPtr(),
           promotion_id));
+#else
+  // No need for a callback. The UI receives "brave_rewards.promotionFinish".
+  rewards_service_->ClaimPromotion(promotion_id, base::DoNothing());
+#endif
 }
 
 
