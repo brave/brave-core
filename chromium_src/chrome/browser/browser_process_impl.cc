a7909e456fe026c2c9a9c58e2c1b24b3ebf4659e
Max Karolinskiy <max@brave.com>
2020-01-09 11:52:01 -0500

    Added missing check for ENABLE_EXTENSIONS.

--- chromium_src/chrome/browser/browser_process_impl.cc
+++ chromium_src/chrome/browser/browser_process_impl.cc
@@ -3,7 +3,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "extensions/buildflags/buildflags.h"
+
+#if BUILDFLAG(ENABLE_EXTENSIONS)
 #include "brave/browser/extensions/brave_extensions_browser_client_impl.h"
+#endif
 
 #define ChromeExtensionsBrowserClient BraveExtensionsBrowserClientImpl
 #include "../../../../chrome/browser/browser_process_impl.cc"
