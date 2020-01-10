723ba93e55c945d646489193cf49c28a53591b5c
Max Karolinskiy <max@brave.com>
2020-01-10 18:21:26 -0500

    [Android] Use AndroidX's VisibleForTesting.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/ae72a5874fda2073eacd79d37f75f927d0a43cd9
    
    commit ae72a5874fda2073eacd79d37f75f927d0a43cd9
    Author: Henrique Nakashima <hnakashima@chromium.org>
    Date:   Wed Nov 13 16:56:16 2019 +0000
    
        Use AndroidX's VisibleForTesting instead of Chromium's.
    
        Chromium's only exists for historic reasons, currently serving
        documentation purposes only. AndroidX's version is supported by
        Android lint, though the check at build time is not currently
        working (crbug.com/1022939). This check is still useful for
        Android Studio.
    
        Bug: 889467

--- browser/brave_ads/android/java/org/chromium/chrome/browser/notifications/BraveAdsNotificationBuilder.java
+++ browser/brave_ads/android/java/org/chromium/chrome/browser/notifications/BraveAdsNotificationBuilder.java
@@ -26,9 +26,10 @@ import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.RemoteViews;
 
+import androidx.annotation.VisibleForTesting;
+
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.Log;
-import org.chromium.base.VisibleForTesting;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.notifications.channels.BraveChannelDefinitions;
