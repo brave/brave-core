dacb5e3918bb3e496fcde53a096b415a28b5eed5
Max Karolinskiy <max@brave.com>
2020-01-15 09:00:24 -0500

    [Linux] Added new channel info function in override.
    Not used in our overrides, so just added a dummy implementation.
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/7e526ba510cedc27ec8a3784b36412040d77fc21
    
    commit 7e526ba510cedc27ec8a3784b36412040d77fc21
    Author: Nico Weber <thakis@chromium.org>
    Date:   Tue Oct 29 18:40:57 2019 +0000
    
        Make channel data dir code less branding-dependent.
    
        Make GetChannelSuffixForDataDir() exist always on linux, to make
        it independent of GOOGLE_CHROME_BRANDING. On Linux, disentangle
        the logic in that function from GetChannelImpl() and instead
        make it call GetChannel(). This makes it automatically do the
        right thing in unbranded builds (returning an empty suffix).
    
        This should be behavior-preserving, but if we ever wanted to do
        release channels for unbranded builds, this is a tad easier
        to do now -- and we have less mode-dependent code.
    
        Bug: 961769

--- chromium_src/chrome/common/chrome_paths_linux.cc
+++ chromium_src/chrome/common/chrome_paths_linux.cc
@@ -2,6 +2,8 @@
 + * License, v. 2.0. If a copy of the MPL was not distributed with this file,
 + * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "chrome/common/chrome_paths_internal.h"
+
 #define GetDefaultUserDataDirectory GetDefaultUserDataDirectory_Disabled
 #include "../../../../chrome/common/chrome_paths_linux.cc"
 #undef GetDefaultUserDataDirectory
