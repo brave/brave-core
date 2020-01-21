a91f75518e50151e3300f216a740238e84037590
Cezar Augusto <boss@cezaraugusto.net>
2020-01-21 12:48:21 -0300

    Revert "Merge pull request #4357 from brave/bsc-revert-ntp-backgrounds"
    This change reverts the commit that reverted the addition of new NTP images.
    
    This reverts commit 721b4f05f9c33f874470779c76e63c2abcdc740f, reversing
    changes made to fd3b8cea067559c4d5bdcaca063c338b1c1b33cd.

--- components/brave_new_tab_ui/data/backgrounds.ts
+++ components/brave_new_tab_ui/data/backgrounds.ts
@@ -2,133 +2,132 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+//  'name': '1',
+//  'source': 'ntp-1.webp',
+//  'author': 'Pok Rie',
+//  'link': 'https://www.pexels.com/@pok-rie-33563',
+//  'originalUrl': 'https://www.pexels.com/photo/red-white-and-blue-abstract-painting-1045850/',
+//  'license': 'https://www.pexels.com/photo-license/'
+
 export const images: NewTab.Image[] = [
   {
-    'name': '1',
-    'source': 'ntp-1.webp',
-    'author': 'Pok Rie',
-    'link': 'https://www.pexels.com/@pok-rie-33563',
-    'originalUrl': 'https://www.pexels.com/photo/red-white-and-blue-abstract-painting-1045850/',
-    'license': 'https://www.pexels.com/photo-license/'
+    'name': 'ntp-fall-2019-1',
+    'source': 'anders-jilden.webp',
+    'author': 'Anders Jildén',
+    'link': 'https://unsplash.com/@andersjilden?utm_source=unsplash&utm_medium=referral&utm_content=credit',
+    'originalUrl': '',
+    'license': ''
   },
   {
-    'name': '2',
-    'source': 'ntp-2.webp',
-    'author': 'Jack Millard',
-    'link': 'https://unsplash.com/@millarjb',
-    'originalUrl': 'https://unsplash.com/photos/zjyP-UYI-ko',
-    'license': 'https://unsplash.com/license'
+    'name': 'ntp-fall-2019-2',
+    'source': 'andreas-gucklhorn.webp',
+    'author': 'Andreas Gücklhorn',
+    'link': 'https://unsplash.com/@draufsicht?utm_source=unsplash&utm_medium=referral&utm_content=credit',
+    'originalUrl': '',
+    'license': ''
   },
   {
-    'name': '3',
-    'source': 'ntp-3.webp',
-    'author': 'Buzz Andersen',
-    'link': 'https://unsplash.com/@ldandersen',
-    'originalUrl': 'https://unsplash.com/photos/E4944K_4SvI',
-    'license': 'https://unsplash.com/license'
+    'name': 'ntp-fall-2019-3',
+    'source': 'andy-mai.webp',
+    'author': 'Andy Mai',
+    'link': 'https://unsplash.com/@veroz?utm_source=unsplash&utm_medium=referral&utm_content=credit',
+    'originalUrl': '',
+    'license': ''
   },
   {
-    'name': '4',
-    'source': 'ntp-4.webp',
-    'author': 'Frank McKenna',
-    'link': 'https://unsplash.com/@frankiefoto',
-    'originalUrl': 'https://unsplash.com/photos/eXHeq48Z-Q4',
-    'license': 'https://unsplash.com/license'
+    'name': 'ntp-fall-2019-4',
+    'source': 'annie-spratt.webp',
+    'author': 'Annie Spratt',
+    'link': 'https://unsplash.com/@anniespratt?utm_source=unsplash&utm_medium=referral&utm_content=credit',
+    'originalUrl': '',
+    'license': ''
   },
   {
-    'name': '5',
-    'source': 'ntp-5.webp',
-    'author': 'SpaceX',
-    'link': 'https://www.flickr.com/photos/spacex/',
-    'originalUrl': 'https://www.flickr.com/photos/spacex/16510243060/',
-    'license': 'https://creativecommons.org/licenses/by-nc/2.0/'
+    'name': 'ntp-fall-2019-5',
+    'source': 'anton-repponen.webp',
+    'author': 'Anton Repponen',
+    'link': 'https://unsplash.com/@repponen?utm_source=unsplash&utm_medium=referral&utm_content=credit',
+    'originalUrl': '',
+    'license': ''
   },
   {
-    'name': '6',
-    'source': 'ntp-6.webp',
-    'author': 'SpaceX',
-    'link': 'https://www.flickr.com/photos/spacex/',
-    'originalUrl': 'https://www.flickr.com/photos/spacex/16236321533/',
-    'license': 'https://creativecommons.org/licenses/by-nc/2.0/'
+    'name': 'ntp-fall-2019-6',
+    'source': 'ben-karpinski.webp',
+    'author': 'Ben Karpinski',
+    'link': 'https://www.benkarpinski.com/landscapes',
+    'originalUrl': '',
+    'license': ''
   },
   {
-    'name': '7',
-    'source': 'ntp-7.webp',
-    'author': 'SpaceX',
-    'link': 'https://www.flickr.com/photos/spacex/',
-    'originalUrl': 'https://www.flickr.com/photos/spacex/40126461851/',
-    'license': 'https://creativecommons.org/licenses/by-nc/2.0/'
+    'name': 'ntp-fall-2019-7',
+    'source': 'dc-cavalleri.webp',
+    'author': 'D.C. Cavalleri',
+    'link': 'https://gradivis.com',
+    'originalUrl': '',
+    'license': ''
   },
   {
-    'name': '8',
-    'source': 'ntp-8.webp',
+    'name': 'ntp-fall-2019-8',
+    'source': 'joe-gardner.webp',
+    'author': 'Joe Gardner',
+    'link': 'https://unsplash.com/@josephgardnerphotography?utm_source=unsplash&utm_medium=referral&utm_content=credit',
+    'originalUrl': '',
+    'license': ''
+  },
+  {
+    'name': 'ntp-fall-2019-9',
+    'source': 'louis-kim.webp',
+    'author': 'Louis Kim',
+    'link': 'http://louiskimphotography.com',
+    'originalUrl': '',
+    'license': ''
+  },
+  {
+    'name': 'ntp-fall-2019-10',
+    'source': 'matt-palmer.webp',
+    'author': 'Matt Palmer',
+    'link': 'https://unsplash.com/@mattpalmer?utm_source=unsplash&utm_medium=referral&utm_content=credit',
+    'originalUrl': '',
+    'license': ''
+  },
+  {
+    'name': 'ntp-fall-2019-11',
+    'source': 'oliwier-gesla.webp',
+    'author': 'Oliwier Gesla',
+    'link': 'https://www.instagram.com/oliwiergesla/',
+    'originalUrl': '',
+    'license': ''
+  },
+  {
+    'name': 'ntp-fall-2019-12',
+    'source': 'svalbard-jerol-soibam.webp',
+    'author': 'Jerol Soibam',
+    'link': 'https://www.instagram.com/jerol_soibam',
+    'originalUrl': '',
+    'license': ''
+  },
+  {
+    'name': 'ntp-fall-2019-13',
+    'source': 'will-christiansen-glacier-peak.webp',
     'author': 'Will Christiansen',
-    'link': 'https://www.theskyfolk.com',
-    'originalUrl': 'https://www.instagram.com/p/BZRQxf9hRpw/',
-    'license': 'used with permission'
+    'link': 'www.theskyfolk.com',
+    'originalUrl': '',
+    'license': ''
   },
   {
-    'name': '9',
-    'source': 'ntp-9.webp',
+    'name': 'ntp-fall-2019-14',
+    'source': 'will-christiansen-ice.webp',
     'author': 'Will Christiansen',
-    'link': 'https://www.theskyfolk.com',
-    'originalUrl': 'https://www.instagram.com/p/BI7w_dFAlR0/',
-    'license': 'used with permission'
+    'link': 'www.theskyfolk.com',
+    'originalUrl': '',
+    'license': ''
   },
   {
-    'name': '10',
-    'source': 'ntp-10.webp',
-    'author': 'Will Christiansen',
-    'link': 'https://www.theskyfolk.com',
-    'originalUrl': 'https://www.instagram.com/p/Bo9ft6LBv6V/',
-    'license': 'used with permission'
-  },
-  {
-    'name': '11',
-    'source': 'ntp-11.webp',
-    'author': 'Nout Gons',
-    'link': 'https://www.instagram.com/ntgns/',
-    'originalUrl': 'https://www.instagram.com/p/BNyaAuFBiSt/',
-    'license': 'used with permission'
-  },
-  {
-    'name': '12',
-    'source': 'ntp-12.webp',
-    'author': 'Artem Bali',
-    'link': 'https://www.pexels.com/@belart84',
-    'originalUrl': 'https://www.pexels.com/photo/aerial-view-of-seashore-near-large-grey-rocks-853199/',
-    'license': 'https://www.pexels.com/photo-license/'
-  },
-  {
-    'name': '13',
-    'source': 'ntp-13.webp',
-    'author': 'Hristo Fidanov',
-    'link': 'https://www.pexels.com/@hristo-fidanov-520662',
-    'originalUrl': 'https://www.pexels.com/photo/scenic-view-of-forest-during-night-time-1252869/',
-    'license': 'https://www.pexels.com/photo-license/'
-  },
-  {
-    'name': '14',
-    'source': 'ntp-14.webp',
-    'author': 'Life of Pix',
-    'link': 'https://www.pexels.com/@life-of-pix',
-    'originalUrl': 'https://www.pexels.com/photo/bridge-california-cliff-golden-gate-bridge-7653/',
-    'license': 'https://www.pexels.com/photo-license/'
-  },
-  {
-    'name': '15',
-    'source': 'ntp-15.webp',
-    'author': 'Cassie Boca',
-    'link': 'https://unsplash.com/@cassieboca',
-    'originalUrl': 'https://unsplash.com/photos/gFyy2Po7T-k',
-    'license': 'https://unsplash.com/license'
-  },
-  {
-    'name': '16',
-    'source': 'ntp-16.webp',
-    'author': 'Braden Jarvis',
-    'link': 'https://unsplash.com/@jarvisphoto',
-    'originalUrl': 'https://unsplash.com/photos/prSogOoFmkw',
-    'license': 'https://unsplash.com/license'
+    'name': 'ntp-fall-2019-15',
+    'source': 'xavier-balderas-cejudo.webp',
+    'author': 'Xavier Balderas Cejudo',
+    'link': 'https://unsplash.com/@xavibalderas?utm_source=unsplash&utm_medium=referral&utm_content=credit',
+    'originalUrl': '',
+    'license': ''
   }
 ]
