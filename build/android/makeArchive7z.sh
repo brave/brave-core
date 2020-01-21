5aeed16c651e65622a67fef84d36ba7061302038
bridiver <github@brianjohnson.cc>
2020-01-21 10:13:29 -0700

    create symbol archive as part of create_dist

--- build/android/makeArchive7z.sh
+++ build/android/makeArchive7z.sh
@@ -1,2 +1,6 @@
+#!/usr/bin/env bash
 
- tar cf - $1 -P | pv -s $(du -sb $1  | awk '{print $1}') | 7z a -si -m0=lzma2 -mx=3 $2.tar.7z
+set -euo pipefail
+
+rm -f $2.tar.7z
+tar cf - $1 -P | pv -s $(du -sb $1  | awk '{print $1}') | 7z a -si -m0=lzma2 -mx=3 $2.tar.7z
