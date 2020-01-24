98847a2f7584ca6e9170ef03ea0c53ffbbdcf2b5
Mark Pilgrim <mpilgrim@brave.com>
2020-01-24 11:29:16 -0500

    convert components/content_settings/core/common/content_settings_mojom_traits.cc to chromium_src

---
+++ chromium_src/components/content_settings/core/common/content_settings_mojom_traits.cc
@@ -0,0 +1,18 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_CONTENT_SETTINGS_CORE_COMMON_CONTENT_SETTINGS_MOJOM_TRAITS_CC_
+#define BRAVE_CHROMIUM_SRC_COMPONENTS_CONTENT_SETTINGS_CORE_COMMON_CONTENT_SETTINGS_MOJOM_TRAITS_CC_
+
+#define BRAVE_CONTENT_SETTINGS_MOJOM_TRAITS_CC                    \
+  data.ReadAutoplayRules(&out->autoplay_rules) &&                 \
+      data.ReadFingerprintingRules(&out->fingerprinting_rules) && \
+      data.ReadBraveShieldsRules(&out->brave_shields_rules)&&
+
+#include "../../../../../components/content_settings/core/common/content_settings_mojom_traits.cc"  // NOLINT
+
+#undef BRAVE_CONTENT_SETTINGS_MOJOM_TRAITS_CC
+
+#endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_CONTENT_SETTINGS_CORE_COMMON_CONTENT_SETTINGS_MOJOM_TRAITS_CC_
