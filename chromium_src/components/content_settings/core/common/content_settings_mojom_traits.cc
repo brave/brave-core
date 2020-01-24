8856acbe191463e60deeb7bcf20bba85321c1075
Mark Pilgrim <mpilgrim@brave.com>
2020-01-24 11:50:26 -0500

    update label in content_settings/core/common/content_settings_mojom_traits.cc override, again

--- chromium_src/components/content_settings/core/common/content_settings_mojom_traits.cc
+++ chromium_src/components/content_settings/core/common/content_settings_mojom_traits.cc
@@ -6,13 +6,13 @@
 #ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_CONTENT_SETTINGS_CORE_COMMON_CONTENT_SETTINGS_MOJOM_TRAITS_CC_
 #define BRAVE_CHROMIUM_SRC_COMPONENTS_CONTENT_SETTINGS_CORE_COMMON_CONTENT_SETTINGS_MOJOM_TRAITS_CC_
 
-#define BRAVE_RENDERER_CONTENT_SETTING_RULES_DATA_VIEW_CC         \
+#define BRAVE_READ_RENDERER_CONTENT_SETTING_RULES_DATA_VIEW       \
   data.ReadAutoplayRules(&out->autoplay_rules) &&                 \
       data.ReadFingerprintingRules(&out->fingerprinting_rules) && \
       data.ReadBraveShieldsRules(&out->brave_shields_rules)&&
 
 #include "../../../../../components/content_settings/core/common/content_settings_mojom_traits.cc"  // NOLINT
 
-#undef BRAVE_RENDERER_CONTENT_SETTING_RULES_DATA_VIEW_CC
+#undef BRAVE_READ_RENDERER_CONTENT_SETTING_RULES_DATA_VIEW
 
 #endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_CONTENT_SETTINGS_CORE_COMMON_CONTENT_SETTINGS_MOJOM_TRAITS_CC_
