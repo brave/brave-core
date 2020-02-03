4e141ac6cf527bf4bd53f7e91a4170549e7ebf92
Francois Marier <francois@brave.com>
2020-02-03 07:29:33 -0800

    Auto-generate NTP backgrounds LICENSE file (fixes brave/brave-browser#7460)

--- components/brave_new_tab_ui/data/backgrounds.ts
+++ components/brave_new_tab_ui/data/backgrounds.ts
@@ -1,104 +1,134 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-// After editing this file, don't forget to update LICENSE as well.
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 export const images: NewTab.Image[] = [
   {
     'name': '1',
     'source': 'ntp-1.webp',
     'author': 'Pok Rie',
-    'link': 'https://www.pexels.com/@pok-rie-33563'
+    'link': 'https://www.pexels.com/@pok-rie-33563',
+    'originalUrl': 'https://www.pexels.com/photo/red-white-and-blue-abstract-painting-1045850/',
+    'license': 'https://www.pexels.com/photo-license/'
   },
   {
     'name': '2',
     'source': 'ntp-2.webp',
     'author': 'Jack Millard',
-    'link': 'https://unsplash.com/@millarjb'
+    'link': 'https://unsplash.com/@millarjb',
+    'originalUrl': 'https://unsplash.com/photos/zjyP-UYI-ko',
+    'license': 'https://unsplash.com/license'
   },
   {
     'name': '3',
     'source': 'ntp-3.webp',
     'author': 'Buzz Andersen',
-    'link': 'https://unsplash.com/@ldandersen'
+    'link': 'https://unsplash.com/@ldandersen',
+    'originalUrl': 'https://unsplash.com/photos/E4944K_4SvI',
+    'license': 'https://unsplash.com/license'
   },
   {
     'name': '4',
     'source': 'ntp-4.webp',
     'author': 'Frank McKenna',
-    'link': 'https://unsplash.com/@frankiefoto'
+    'link': 'https://unsplash.com/@frankiefoto',
+    'originalUrl': 'https://unsplash.com/photos/eXHeq48Z-Q4',
+    'license': 'https://unsplash.com/license'
   },
   {
     'name': '5',
     'source': 'ntp-5.webp',
     'author': 'SpaceX',
-    'link': 'https://www.flickr.com/photos/spacex/'
+    'link': 'https://www.flickr.com/photos/spacex/',
+    'originalUrl': 'https://www.flickr.com/photos/spacex/16510243060/',
+    'license': 'https://creativecommons.org/licenses/by-nc/2.0/'
   },
   {
     'name': '6',
     'source': 'ntp-6.webp',
     'author': 'SpaceX',
-    'link': 'https://www.flickr.com/photos/spacex/'
+    'link': 'https://www.flickr.com/photos/spacex/',
+    'originalUrl': 'https://www.flickr.com/photos/spacex/16236321533/',
+    'license': 'https://creativecommons.org/licenses/by-nc/2.0/'
   },
   {
     'name': '7',
     'source': 'ntp-7.webp',
     'author': 'SpaceX',
-    'link': 'https://www.flickr.com/photos/spacex/'
+    'link': 'https://www.flickr.com/photos/spacex/',
+    'originalUrl': 'https://www.flickr.com/photos/spacex/40126461851/',
+    'license': 'https://creativecommons.org/licenses/by-nc/2.0/'
   },
   {
     'name': '8',
     'source': 'ntp-8.webp',
     'author': 'Will Christiansen',
-    'link': 'https://www.theskyfolk.com'
+    'link': 'https://www.theskyfolk.com',
+    'originalUrl': 'https://www.instagram.com/p/BZRQxf9hRpw/',
+    'license': 'used with permission'
   },
   {
     'name': '9',
     'source': 'ntp-9.webp',
     'author': 'Will Christiansen',
-    'link': 'https://www.theskyfolk.com'
+    'link': 'https://www.theskyfolk.com',
+    'originalUrl': 'https://www.instagram.com/p/BI7w_dFAlR0/',
+    'license': 'used with permission'
   },
   {
     'name': '10',
     'source': 'ntp-10.webp',
     'author': 'Will Christiansen',
-    'link': 'https://www.theskyfolk.com'
+    'link': 'https://www.theskyfolk.com',
+    'originalUrl': 'https://www.instagram.com/p/Bo9ft6LBv6V/',
+    'license': 'used with permission'
   },
   {
     'name': '11',
     'source': 'ntp-11.webp',
     'author': 'Nout Gons',
-    'link': 'https://www.instagram.com/ntgns/'
+    'link': 'https://www.instagram.com/ntgns/',
+    'originalUrl': 'https://www.instagram.com/p/BNyaAuFBiSt/',
+    'license': 'used with permission'
   },
   {
     'name': '12',
     'source': 'ntp-12.webp',
     'author': 'Artem Bali',
-    'link': 'https://www.pexels.com/@belart84'
+    'link': 'https://www.pexels.com/@belart84',
+    'originalUrl': 'https://www.pexels.com/photo/aerial-view-of-seashore-near-large-grey-rocks-853199/',
+    'license': 'https://www.pexels.com/photo-license/'
   },
   {
     'name': '13',
     'source': 'ntp-13.webp',
     'author': 'Hristo Fidanov',
-    'link': 'https://www.pexels.com/@hristo-fidanov-520662'
+    'link': 'https://www.pexels.com/@hristo-fidanov-520662',
+    'originalUrl': 'https://www.pexels.com/photo/scenic-view-of-forest-during-night-time-1252869/',
+    'license': 'https://www.pexels.com/photo-license/'
   },
   {
     'name': '14',
     'source': 'ntp-14.webp',
     'author': 'Life of Pix',
-    'link': 'https://www.pexels.com/@life-of-pix'
+    'link': 'https://www.pexels.com/@life-of-pix',
+    'originalUrl': 'https://www.pexels.com/photo/bridge-california-cliff-golden-gate-bridge-7653/',
+    'license': 'https://www.pexels.com/photo-license/'
   },
   {
     'name': '15',
     'source': 'ntp-15.webp',
     'author': 'Cassie Boca',
-    'link': 'https://unsplash.com/@cassieboca'
+    'link': 'https://unsplash.com/@cassieboca',
+    'originalUrl': 'https://unsplash.com/photos/gFyy2Po7T-k',
+    'license': 'https://unsplash.com/license'
   },
   {
     'name': '16',
     'source': 'ntp-16.webp',
     'author': 'Braden Jarvis',
-    'link': 'https://unsplash.com/@jarvisphoto'
+    'link': 'https://unsplash.com/@jarvisphoto',
+    'originalUrl': 'https://unsplash.com/photos/prSogOoFmkw',
+    'license': 'https://unsplash.com/license'
   }
 ]
