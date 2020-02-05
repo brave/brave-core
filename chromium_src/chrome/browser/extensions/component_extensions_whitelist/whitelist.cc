085e1f8a6eb595bf96ecd497e05420fc261b9721
akshay <akshay24495@gmail.com>
2020-02-05 16:39:08 +0530

    Don't try to compile brave-rewards and brave-sync when disabled

--- chromium_src/chrome/browser/extensions/component_extensions_whitelist/whitelist.cc
+++ chromium_src/chrome/browser/extensions/component_extensions_whitelist/whitelist.cc
@@ -11,8 +11,14 @@
 #include "brave/common/extensions/extension_constants.h"
 #include "brave/components/brave_extension/grit/brave_extension.h"
 #include "components/grit/brave_components_resources.h"
+#include "brave/components/brave_rewards/browser/buildflags/buildflags.h"
+#include "brave/components/brave_sync/buildflags/buildflags.h"
+#if BUILDFLAG(BRAVE_REWARDS_ENABLED)
 #include "brave/components/brave_rewards/resources/extension/grit/brave_rewards_extension_resources.h"
+#endif
+#if BUILDFLAG(ENABLE_BRAVE_SYNC)
 #include "brave/components/brave_sync/grit/brave_sync_resources.h"
+#endif
 #include "brave/components/brave_webtorrent/grit/brave_webtorrent_resources.h"
 
 namespace extensions {
@@ -20,8 +26,12 @@ namespace extensions {
   bool IsComponentExtensionWhitelisted(const std::string& extension_id) {
     const char* const kAllowed[] = {
       brave_extension_id,
+      #if BUILDFLAG(BRAVE_REWARDS_ENABLED)
       brave_rewards_extension_id,
+      #endif
+      #if BUILDFLAG(ENABLE_BRAVE_SYNC)
       brave_sync_extension_id,
+      #endif
       ethereum_remote_client_extension_id,
       brave_webtorrent_extension_id
     };
@@ -38,8 +48,12 @@ namespace extensions {
     switch (manifest_resource_id) {
       // Please keep the list in alphabetical order.
       case IDR_BRAVE_EXTENSION:
+      #if BUILDFLAG(BRAVE_REWARDS_ENABLED)
       case IDR_BRAVE_REWARDS:
+      #endif
+      #if BUILDFLAG(ENABLE_BRAVE_SYNC)
       case IDR_BRAVE_SYNC_EXTENSION:
+      #endif
       case IDR_BRAVE_WEBTORRENT:
         return true;
     }
