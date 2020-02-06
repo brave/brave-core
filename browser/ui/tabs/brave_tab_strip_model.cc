30f2e37387eb3b8832758acbef34c12ce8440e1e
Guillaume Felley <guillaume@felley.io>
2020-02-06 19:39:21 +0100

    New extended class BraveTabStripModel and add patches to apply it

---
+++ browser/ui/tabs/brave_tab_strip_model.cc
@@ -0,0 +1,147 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include <memory>
+#include <algorithm>
+
+#include "brave/browser/ui/tabs/brave_tab_strip_model.h"
+
+#include "ui/events/event.h"
+#include "build/build_config.h"
+#include "ui/events/event_handler.h"
+#include "chrome/browser/profiles/profile.h"
+#include "components/prefs/pref_service.h"
+#include "content/public/browser/web_contents.h"
+#include "brave/common/pref_names.h"
+
+#if defined(USE_AURA)
+#include "ui/aura/window.h"
+#endif
+
+#if defined(OS_MACOSX)
+#include "ui/views/widget/widget.h"
+#endif
+
+BraveTabStripModel::BraveTabStripModel(TabStripModelDelegate* delegate,
+                                       Profile* profile)
+: TabStripModel(delegate, profile) {}
+
+BraveTabStripModel::~BraveTabStripModel() {}
+
+void BraveTabStripModel::SelectNextTab(UserGestureDetails detail) {
+  bool isMRUEnabled = Profile::FromBrowserContext(
+    GetActiveWebContents()->GetBrowserContext())
+      ->GetPrefs()
+      ->GetBoolean(kMRUCyclingEnabled);
+
+  if (isMRUEnabled) {
+     SelectTabMRU(false, detail);
+  } else {
+     SelectRelativeTab(true, detail);
+  }
+}
+
+void BraveTabStripModel::SelectPreviousTab(UserGestureDetails detail) {
+  bool isMRUEnabled = Profile::FromBrowserContext(
+    GetActiveWebContents()->GetBrowserContext())
+      ->GetPrefs()
+      ->GetBoolean(kMRUCyclingEnabled);
+
+  if (isMRUEnabled) {
+     SelectTabMRU(true, detail);
+  } else {
+     SelectRelativeTab(false, detail);
+  }
+}
+
+void BraveTabStripModel::SelectTabMRU(bool backward,
+                                      UserGestureDetails detail) {
+  if (current_mru_cycling_index == -1) {
+      // Start cycling
+
+      // Create a list of tab indexes sorted by time of last activation
+      for (int i = 0; i < count(); ++i) {
+        mru_cycle_list.push_back(i);
+      }
+
+      std::sort(mru_cycle_list.begin(),
+                mru_cycle_list.end(),
+                [this](int a, int b){
+        return GetWebContentsAt(a)->GetLastActiveTime() >
+          GetWebContentsAt(b)->GetLastActiveTime();
+        });
+
+      current_mru_cycling_index = 0;
+
+      // Create an event handler eating all keyboard events while tabing
+      ctrl_released_event_handler = std::make_unique<CtrlReleaseHandler>(this);
+
+      // Add the event handler
+      gfx::NativeWindow window = this->GetActiveWebContents()
+        ->GetTopLevelNativeWindow();
+#if defined(OS_MACOSX)
+      views::Widget::GetWidgetForNativeWindow(window)
+        ->GetRootView()
+        ->AddPreTargetHandler(ctrl_released_event_handler.get());
+#else
+      window->AddPreTargetHandler(ctrl_released_event_handler.get());
+#endif
+  }
+
+  int tabCount = mru_cycle_list.size();
+
+  if (backward) {
+    current_mru_cycling_index =
+      (current_mru_cycling_index - 1 % tabCount + tabCount) % tabCount;
+  } else {
+    current_mru_cycling_index =
+      (current_mru_cycling_index + 1 % tabCount + tabCount) % tabCount;
+  }
+
+  ActivateTabAt(mru_cycle_list[current_mru_cycling_index], detail);
+}
+
+void BraveTabStripModel::StopMRUCycling() {
+  current_mru_cycling_index = -1;
+  mru_cycle_list.clear();
+
+  if (ctrl_released_event_handler) {
+    // Remove the event handler
+    gfx::NativeWindow window = this->GetActiveWebContents()
+      ->GetTopLevelNativeWindow();
+#if defined(OS_MACOSX)
+    views::Widget::GetWidgetForNativeWindow(window)
+      ->GetRootView()
+      ->RemovePreTargetHandler(ctrl_released_event_handler.get());
+#else
+    window->RemovePreTargetHandler(ctrl_released_event_handler.get());
+#endif
+
+    ctrl_released_event_handler.reset();
+  }
+}
+
+
+BraveTabStripModel::CtrlReleaseHandler::CtrlReleaseHandler(
+  BraveTabStripModel* tab_strip): tab_strip(tab_strip) {}
+
+BraveTabStripModel::CtrlReleaseHandler::~CtrlReleaseHandler() {}
+
+void BraveTabStripModel::CtrlReleaseHandler::OnKeyEvent(ui::KeyEvent* event) {
+  if (event->key_code() == ui::VKEY_CONTROL &&
+      event->type() == ui::ET_KEY_RELEASED) {
+    // Ctrl key was released, stop the MRU cycling
+    tab_strip->StopMRUCycling();
+  } else if ((event->key_code() == ui::VKEY_TAB  &&
+             event->type() == ui::ET_KEY_PRESSED) ||
+             (event->key_code() == ui::VKEY_PRIOR  &&
+             event->type() == ui::ET_KEY_PRESSED) ||
+             (event->key_code() == ui::VKEY_NEXT  &&
+             event->type() == ui::ET_KEY_PRESSED)) {
+    // Block all keys while cycling except tab,pg previous, pg next keys
+  } else {
+    event->StopPropagation();
+  }
+}
---
+++ browser/ui/tabs/brave_tab_strip_model.h
@@ -0,0 +1,61 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_BROWSER_UI_TABS_BRAVE_TAB_STRIP_MODEL_H_
+#define BRAVE_BROWSER_UI_TABS_BRAVE_TAB_STRIP_MODEL_H_
+
+#include <memory>
+#include <vector>
+
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
+
+
+#include "ui/events/event_handler.h"
+
+
+class BraveTabStripModel : public TabStripModel {
+ public:
+  explicit BraveTabStripModel(TabStripModelDelegate* delegate,
+                               Profile* profile);
+
+  ~BraveTabStripModel() override;
+
+  void SelectNextTab(UserGestureDetails detail =
+    UserGestureDetails(GestureType::kOther)) override;
+
+  void SelectPreviousTab(UserGestureDetails detail =
+    UserGestureDetails(GestureType::kOther)) override;
+
+  // Set the next tab when doing a MRU cycling with Ctrl-tab
+  void SelectTabMRU(bool backward,
+  UserGestureDetails detail = UserGestureDetails(GestureType::kOther));
+
+  // Stop MRU cycling, called when releasing the Ctrl key
+  void StopMRUCycling();
+
+ private:
+  // To capture the release of the Ctrl key when doing a MRU cycling with
+  // Ctrl-tab
+  class CtrlReleaseHandler : public ui::EventHandler {
+   public:
+    explicit CtrlReleaseHandler(BraveTabStripModel* tab_strip);
+    ~CtrlReleaseHandler() override;
+
+   private:
+    void OnKeyEvent(ui::KeyEvent* event) override;
+    BraveTabStripModel* tab_strip;
+    DISALLOW_COPY_AND_ASSIGN(CtrlReleaseHandler);
+  };
+
+  std::unique_ptr<CtrlReleaseHandler> ctrl_released_event_handler;
+
+  // Current index when we are MRU cycling, set to -1 when not cycling
+  int current_mru_cycling_index = -1;
+
+  // List of tab indexes sorted by most recently used
+  std::vector<int> mru_cycle_list;
+};
+
+#endif  // BRAVE_BROWSER_UI_TABS_BRAVE_TAB_STRIP_MODEL_H_
