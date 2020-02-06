9836cd607fe17c56f4ef2d5d5ebce26c2340f64e
Pete Miller <miller.pete@gmail.com>
2020-02-05 16:16:36 -0800

    Merge pull request #4514 from brave/maxk-fix-rewards-image
    Fixes rewards-image handler post C80 bump.

--- components/ntp_sponsored_images/browser/ntp_sponsored_image_source.cc
+++ components/ntp_sponsored_images/browser/ntp_sponsored_image_source.cc
@@ -49,12 +49,7 @@ void NTPSponsoredImageSource::StartDataRequest(
     GotDataCallback callback) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
-  // GURL::path() returns path with the leading slash, which we don't want.
-  std::string path = url.path();
-  if (!path.empty()) {
-    path.erase(0, 1);
-  }
-
+  const std::string path = URLDataSource::URLToRequestPath(url);
   if (!IsValidPath(path)) {
     scoped_refptr<base::RefCountedMemory> bytes;
     std::move(callback).Run(std::move(bytes));
