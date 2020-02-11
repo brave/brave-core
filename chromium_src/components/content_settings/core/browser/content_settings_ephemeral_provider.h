158965129bc18a7b5c0d9eda7fb3cb018e021238
Simon Hong <shong@brave.com>
2020-02-11 18:22:10 +0900

    Fix lint error

--- chromium_src/components/content_settings/core/browser/content_settings_ephemeral_provider.h
+++ chromium_src/components/content_settings/core/browser/content_settings_ephemeral_provider.h
@@ -3,6 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_CONTENT_SETTINGS_CORE_BROWSER_CONTENT_SETTINGS_EPHEMERAL_PROVIDER_H_
+#define BRAVE_CHROMIUM_SRC_COMPONENTS_CONTENT_SETTINGS_CORE_BROWSER_CONTENT_SETTINGS_EPHEMERAL_PROVIDER_H_
+
 #define BRAVE_BROWSER_H \
  private: \
   friend class BraveEphemeralProvider;
@@ -10,3 +13,5 @@
 #include "../../../../../../components/content_settings/core/browser/content_settings_ephemeral_provider.h"
 
 #undef BRAVE_BROWSER_H
+
+#endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_CONTENT_SETTINGS_CORE_BROWSER_CONTENT_SETTINGS_EPHEMERAL_PROVIDER_H_
--- chromium_src/components/content_settings/core/browser/host_content_settings_map.h
+++ chromium_src/components/content_settings/core/browser/host_content_settings_map.h
@@ -3,6 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_CONTENT_SETTINGS_CORE_BROWSER_HOST_CONTENT_SETTINGS_MAP_H_
+#define BRAVE_CHROMIUM_SRC_COMPONENTS_CONTENT_SETTINGS_CORE_BROWSER_HOST_CONTENT_SETTINGS_MAP_H_
+
 #define BRAVE_BROWSER_H \
  private: \
   friend class BraveHostContentSettingsMap;
@@ -10,3 +13,5 @@
 #include "../../../../../../components/content_settings/core/browser/host_content_settings_map.h"
 
 #undef BRAVE_BROWSER_H
+
+#endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_CONTENT_SETTINGS_CORE_BROWSER_HOST_CONTENT_SETTINGS_MAP_H_
