dd067a360662b17ed89dd3d5e4634e54f33d72f4
Anthony Tseng <darkdh@gmail.com>
2020-02-12 13:00:13 -0800

    Merge pull request #4592 from brave/brave-sync-flag
    Add Brave Sync flag to control whether to append `--disable-sync`

---
+++ components/brave_sync/features.h
@@ -0,0 +1,21 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_BRAVE_SYNC_FEATURES_H_
+#define BRAVE_COMPONENTS_BRAVE_SYNC_FEATURES_H_
+
+namespace base {
+struct Feature;
+}  // namespace base
+
+namespace brave_sync {
+namespace features {
+
+extern const base::Feature kBraveSync;
+
+}  // namespace features
+}  // namespace brave_sync
+
+#endif  // BRAVE_COMPONENTS_BRAVE_SYNC_FEATURES_H_
