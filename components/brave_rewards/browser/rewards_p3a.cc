0ab98d8249268247fe51c4db85d375ee3db3c93b
brave-builds <devops@brave.com>
2020-02-13 16:15:06 +0000

    Uplift of #4414 (squashed) to dev

--- components/brave_rewards/browser/rewards_p3a.cc
+++ components/brave_rewards/browser/rewards_p3a.cc
@@ -14,18 +14,22 @@
 
 namespace brave_rewards {
 
-void RecordWalletBalanceP3A(bool wallet_created, size_t b) {
+void RecordWalletBalanceP3A(bool wallet_created, bool rewards_enabled,
+                            size_t b) {
   int answer = 0;
-  if (wallet_created) {
+  if (wallet_created && !rewards_enabled) {
+    answer = 1;
+  } else if (rewards_enabled) {
+    DCHECK(wallet_created);
     if (b < 10) {
-      answer = 1;
-    } else if (10 <= b && b < 50) {
       answer = 2;
-    } else if (50 <= b) {
+    } else if (10 <= b && b < 50) {
       answer = 3;
+    } else if (50 <= b) {
+      answer = 4;
     }
   }
-  UMA_HISTOGRAM_EXACT_LINEAR("Brave.Rewards.WalletBalance", answer, 3);
+  UMA_HISTOGRAM_EXACT_LINEAR("Brave.Rewards.WalletBalance.2", answer, 4);
 }
 
 void RecordAutoContributionsState(AutoContributionsP3AState state, int count) {
@@ -34,50 +38,58 @@ void RecordAutoContributionsState(AutoContributionsP3AState state, int count) {
   switch (state) {
     case AutoContributionsP3AState::kNoWallet:
       break;
-    case AutoContributionsP3AState::kWalletCreatedAutoContributeOff:
+    case AutoContributionsP3AState::kRewardsDisabled:
       answer = 1;
+      break;
+    case AutoContributionsP3AState::kWalletCreatedAutoContributeOff:
+      answer = 2;
       break;
     case AutoContributionsP3AState::kAutoContributeOn:
       switch (count) {
         case 0:
-          answer = 2;
-          break;
-        case 1:
           answer = 3;
           break;
-        default:
+        case 1:
           answer = 4;
+          break;
+        default:
+          answer = 5;
       }
       break;
     default:
       NOTREACHED();
   }
-  UMA_HISTOGRAM_EXACT_LINEAR("Brave.Rewards.AutoContributionsState", answer, 4);
+  UMA_HISTOGRAM_EXACT_LINEAR("Brave.Rewards.AutoContributionsState.2", answer,
+                             5);
 }
 
 void RecordTipsState(bool wallet_created,
+                     bool rewards_enabled,
                      int one_time_count,
                      int recurring_count) {
   DCHECK_GE(one_time_count, 0);
   DCHECK_GE(recurring_count, 0);
 
   int answer = 0;
-  if (wallet_created) {
+  if (wallet_created && !rewards_enabled) {
+    answer = 1;
+  } else if (rewards_enabled) {
+    DCHECK(wallet_created);
     if (one_time_count == 0 && recurring_count == 0) {
-      answer = 1;
-    } else if (one_time_count > 0 && recurring_count > 0) {
-      answer = 4;
-    } else if (one_time_count > 0) {
       answer = 2;
-    } else {
+    } else if (one_time_count > 0 && recurring_count > 0) {
+      answer = 5;
+    } else if (one_time_count > 0) {
       answer = 3;
+    } else {
+      answer = 4;
     }
   }
-  UMA_HISTOGRAM_EXACT_LINEAR("Brave.Rewards.TipsState", answer, 4);
+  UMA_HISTOGRAM_EXACT_LINEAR("Brave.Rewards.TipsState.2", answer, 5);
 }
 
 void RecordAdsState(AdsP3AState state) {
-  UMA_HISTOGRAM_ENUMERATION("Brave.Rewards.AdsState", state);
+  UMA_HISTOGRAM_ENUMERATION("Brave.Rewards.AdsState.2", state);
 }
 
 void UpdateAdsP3AOnPreferenceChange(PrefService *prefs,
@@ -85,25 +97,34 @@ void UpdateAdsP3AOnPreferenceChange(PrefService *prefs,
   using brave_rewards::AdsP3AState;
   const bool rewards_enabled =
       prefs->GetBoolean(brave_rewards::prefs::kBraveRewardsEnabled);
+  const bool ads_enabled = prefs->GetBoolean(brave_ads::prefs::kEnabled);
   if (pref == brave_ads::prefs::kEnabled) {
-    if (prefs->GetBoolean(brave_ads::prefs::kEnabled)) {
+    if (ads_enabled) {
       brave_rewards::RecordAdsState(AdsP3AState::kAdsEnabled);
       prefs->SetBoolean(brave_ads::prefs::kAdsWereDisabled, false);
     } else {
       // Apparently, the pref was disabled.
+      // TODO(ifremov): DCHECK(rewards_enabled)?
       brave_rewards::RecordAdsState(
           rewards_enabled ? AdsP3AState::kAdsEnabledThenDisabledRewardsOn :
                             AdsP3AState::kAdsEnabledThenDisabledRewardsOff);
       prefs->SetBoolean(brave_ads::prefs::kAdsWereDisabled, true);
     }
   } else if (pref == brave_rewards::prefs::kBraveRewardsEnabled) {
-    // Rewards pref was changed
+    // Rewards pref was changed.
     if (prefs->GetBoolean(brave_ads::prefs::kAdsWereDisabled)) {
+      DCHECK(!ads_enabled);
       brave_rewards::RecordAdsState(
           rewards_enabled ? AdsP3AState::kAdsEnabledThenDisabledRewardsOn :
                             AdsP3AState::kAdsEnabledThenDisabledRewardsOff);
+    } else if (!rewards_enabled) {
+      // Ads state had never been disabled, but the user has toggled rewards
+      // off.
+      brave_rewards::RecordAdsState(AdsP3AState::kRewardsDisabled);
+    } else {
+      RecordAdsState(ads_enabled ? AdsP3AState::kAdsEnabled :
+                                   AdsP3AState::kAdsDisabled);
     }
-    // Otherwise do nothing, the needed value should be already recorded.
   }
 }
 
@@ -117,11 +138,19 @@ void MaybeRecordInitialAdsP3AState(PrefService* prefs) {
 }
 
 void RecordNoWalletCreatedForAllMetrics() {
-  RecordWalletBalanceP3A(false, 0);
+  RecordWalletBalanceP3A(false, false, 0);
   RecordAutoContributionsState(AutoContributionsP3AState::kNoWallet, 0);
-  RecordTipsState(false, 0, 0);
+  RecordTipsState(false, false, 0, 0);
   RecordAdsState(AdsP3AState::kNoWallet);
 }
+
+void RecordRewardsDisabledForSomeMetrics() {
+  RecordWalletBalanceP3A(true, false, 1);
+  RecordAutoContributionsState(AutoContributionsP3AState::kRewardsDisabled, 0);
+  RecordTipsState(true, false, 0, 0);
+  // Ads state is handled separately.
+}
+
 
 double CalcWalletBalanceForP3A(base::flat_map<std::string, double> wallets,
                                std::string user_funds) {
@@ -186,7 +215,7 @@ void ExtractAndLogP3AStats(const base::DictionaryValue& dict) {
   }
   const uint64_t total =
       RoundProbiToUint64(probi_value->GetString()) - total_grants;
-  RecordWalletBalanceP3A(true, total);
+  RecordWalletBalanceP3A(true, true, total);
 }
 
 }  // namespace brave_rewards
