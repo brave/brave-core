d849fba1699011a9e2864348bb2cb0648ca40e17
NejcZdovc <nejc.zdovc@3zsistemi.si>
2020-02-19 10:45:24 +0100

    Removes all database references in rewards service

--- components/brave_rewards/browser/rewards_service_impl.h
+++ components/brave_rewards/browser/rewards_service_impl.h
@@ -69,7 +69,6 @@ class BraveRewardsBrowserTest;
 
 namespace brave_rewards {
 
-class PublisherInfoDatabase;
 class RewardsDatabase;
 class RewardsNotificationServiceImpl;
 class BraveRewardsBrowserTest;
@@ -695,10 +694,8 @@ class RewardsServiceImpl : public RewardsService,
   const base::FilePath ledger_state_path_;
   const base::FilePath publisher_state_path_;
   const base::FilePath publisher_info_db_path_;
-  const base::FilePath publisher_info_db_path2_;
   const base::FilePath publisher_list_path_;
   const base::FilePath rewards_base_path_;
-  std::unique_ptr<PublisherInfoDatabase> publisher_info_backend_;
   std::unique_ptr<RewardsDatabase> rewards_database_;
   std::unique_ptr<RewardsNotificationServiceImpl> notification_service_;
   base::ObserverList<RewardsServicePrivateObserver> private_observers_;
