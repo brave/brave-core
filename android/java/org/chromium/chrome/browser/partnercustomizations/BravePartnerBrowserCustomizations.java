65e6bd81b01d7c22134b81566f0e84d4bbcb826b
brave-builds <devops@brave.com>
2020-02-21 19:38:05 +0000

    Uplift of #4697 (squashed) to beta

---
+++ android/java/org/chromium/chrome/browser/partnercustomizations/BravePartnerBrowserCustomizations.java
@@ -0,0 +1,26 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.chromium.chrome.browser.partnercustomizations;
+
+public class BravePartnerBrowserCustomizations
+        extends PartnerBrowserCustomizations {
+    public static class ProviderPackage implements Provider {
+        @Override
+        public String getHomepage() {
+            return null;
+        }
+
+        @Override
+        public boolean isIncognitoModeDisabled() {
+            return false;
+        }
+
+        @Override
+        public boolean isBookmarksEditingDisabled() {
+            return false;
+        }
+    }
+}
