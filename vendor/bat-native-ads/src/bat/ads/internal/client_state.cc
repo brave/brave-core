21e9fc2ebaa9d5e83535a3e6e4dd9049f4f86f3a
Terry Mancey <terry.mancey@icloud.com>
2020-02-21 08:53:11 +0000

    Fixes Brave Ad notifications do not persist round-robin state after a browser restart introduced with #8294

--- vendor/bat-native-ads/src/bat/ads/internal/client_state.cc
+++ vendor/bat-native-ads/src/bat/ads/internal/client_state.cc
@@ -80,10 +80,10 @@ Result ClientState::FromJson(
   }
 
   if (client.HasMember("advertisersUUIDSeen")) {
-    for (const auto& advertiser_uuid_seen :
+    for (const auto& seen_advertiser :
         client["advertisersUUIDSeen"].GetObject()) {
-      seen_advertisers.insert({advertiser_uuid_seen.name.GetString(),
-          advertiser_uuid_seen.value.GetInt64()});
+      seen_advertisers.insert({seen_advertiser.name.GetString(),
+          seen_advertiser.value.GetInt64()});
     }
   }
 
@@ -234,7 +234,7 @@ void SaveToJson(JsonWriter* writer, const ClientState& state) {
 
   writer->String("adsUUIDSeen");
   writer->StartObject();
-  for (const auto& seen_ad_notification : state.seen_advertisers) {
+  for (const auto& seen_ad_notification : state.seen_ad_notifications) {
     writer->String(seen_ad_notification.first.c_str());
     writer->Uint64(seen_ad_notification.second);
   }
