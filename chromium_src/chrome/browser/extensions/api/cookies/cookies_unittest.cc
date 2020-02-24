9ec599bcfe4ff4ab43802e0d30a42b456238f3f7
Jonathan Kingston <jonathan@jooped.co.uk>
2020-02-24 22:17:32 +0000

    Fix Chromium unit_tests to compile
    Fixes: https://github.com/brave/brave-browser/issues/8376

---
+++ chromium_src/chrome/browser/extensions/api/cookies/cookies_unittest.cc
@@ -0,0 +1,9 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#define BRAVE_SETCOOKIE true /*is_from_http*/,
+
+#include "../../../../../../chrome/browser/extensions/api/cookies/cookies_unittest.cc"
+#undef BRAVE_SETCOOKIE
