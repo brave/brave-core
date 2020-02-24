f432813561432fb40b022aebf336287e6bb8764a
Deep <deeppandya91@gmail.com>
2020-02-24 17:55:58 +0530

    Add background images code for NTP
    Add resources for ntp
    
    Add QA pref for NTP
    
    Add string resources
    
    Update patches

---
+++ android/java/org/chromium/chrome/browser/widget/tile/BraveTileWithTextView.java
@@ -0,0 +1,47 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.chromium.chrome.browser.widget.tile;
+
+import android.os.Build;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+import android.content.SharedPreferences;
+
+import org.chromium.chrome.R;
+import org.chromium.base.ContextUtils;
+import org.chromium.chrome.browser.ntp.sponsored.SponsoredImageUtil;
+import org.chromium.chrome.browser.BraveRewardsHelper;
+import org.chromium.chrome.browser.ChromeTabbedActivity;
+import org.chromium.chrome.browser.tabmodel.TabModel;
+import org.chromium.chrome.browser.settings.BackgroundImagesPreferences;
+
+public class BraveTileWithTextView extends TileWithTextView {
+	public BraveTileWithTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void setTitle(String title, int titleLines) {
+
+    	super.setTitle(title, titleLines);
+
+        TextView mTitleView = findViewById(R.id.tile_view_title);
+
+    	SharedPreferences mSharedPreferences = ContextUtils.getAppSharedPreferences();
+        boolean isMoreTabs = false;
+        ChromeTabbedActivity chromeTabbedActivity = BraveRewardsHelper.getChromeTabbedActivity();
+        if(chromeTabbedActivity != null) {
+            TabModel tabModel = chromeTabbedActivity.getCurrentTabModel();
+            isMoreTabs = tabModel.getCount() >= SponsoredImageUtil.MAX_TABS ? true : false;
+        }
+
+        if(mSharedPreferences.getBoolean(BackgroundImagesPreferences.PREF_SHOW_BACKGROUND_IMAGES, true)
+            && (Build.VERSION.SDK_INT > Build.VERSION_CODES.M || (Build.VERSION.SDK_INT <= Build.VERSION_CODES.M && Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && !isMoreTabs))) {
+            mTitleView.setTextColor(getResources().getColor(android.R.color.white));
+        }
+    }
+}
