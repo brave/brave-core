70e5cbdb8399cea0c7e897affd384dcd140a05cd
Emerick Rogul <erogul@brave.com>
2020-02-26 07:10:19 -0500

    Merge pull request #4680 from brave/limit-ads-in-tray-android
    Fix handling of ad notification cap

--- vendor/bat-native-ads/src/bat/ads/internal/ads_impl.cc
+++ vendor/bat-native-ads/src/bat/ads/internal/ads_impl.cc
@@ -1407,11 +1407,10 @@ bool AdsImpl::ShowAd(
 
   notifications_->PushBack(*notification_info);
 
-#if defined(OS_ANDROID)
-  if (notifications_->Count() > kMaximumAdNotifications) {
+  if (kMaximumAdNotifications > 0 &&
+      notifications_->Count() > kMaximumAdNotifications) {
     notifications_->PopFront(true);
   }
-#endif
 
   return true;
 }
