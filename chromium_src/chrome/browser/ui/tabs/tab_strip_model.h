b92f1684a115cc831e5cf1c591978bfba1cb82ca
Guillaume Felley <guillaume@felley.io>
2020-02-27 11:56:44 +0100

    Proper tab_strip_model.h and browser.cc patching, requested by @yrliou.

---
+++ chromium_src/chrome/browser/ui/tabs/tab_strip_model.h
@@ -0,0 +1,18 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_TABS_TAB_STRIP_MODEL_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_TABS_TAB_STRIP_MODEL_H_
+
+#define SelectNextTab virtual SelectNextTab
+#define SelectPreviousTab virtual SelectPreviousTab
+#define TAB_STRIP_MODEL_H_ \
+  friend class BraveTabStripModel;
+  
+#include "../../../../../../chrome/browser/ui/tabs/tab_strip_model.h" // NOLINT
+#undef SelectNextTab
+#undef SelectPreviousTab
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_TABS_TAB_STRIP_MODEL_H_
