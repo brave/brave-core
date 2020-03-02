430ce5d1de075c36fd3036b0ab958de57b75b494
samartnik <artem@brave.com>
2020-03-02 14:54:08 -0500

    Code review changes

--- chromium_src/chrome/browser/android/browsing_data/browsing_data_bridge.cc
+++ chromium_src/chrome/browser/android/browsing_data/browsing_data_bridge.cc
@@ -3,9 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#define BRAVE_HANDLE_DATA_TYPE_DOWNLOADS                   \
+#define BRAVE_CLEAR_BROWSING_DATA                          \
   remove_mask |= BrowsingDataRemover::DATA_TYPE_DOWNLOADS; \
   break;                                                   \
 
 #include "../../../../../../chrome/browser/android/browsing_data/browsing_data_bridge.cc"
-#undef BRAVE_HANDLE_DATA_TYPE_DOWNLOADS
+#undef BRAVE_CLEAR_BROWSING_DATA
