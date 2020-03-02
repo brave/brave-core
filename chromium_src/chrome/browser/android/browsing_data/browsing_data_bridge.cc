7c3b686dd73e9b4a2f9b4e6d492dfa0c9b0b70b7
samartnik <artem@brave.com>
2020-03-02 14:02:17 -0500

    Added option to clear downloads

---
+++ chromium_src/chrome/browser/android/browsing_data/browsing_data_bridge.cc
@@ -0,0 +1,11 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#define BRAVE_HANDLE_DATA_TYPE_DOWNLOADS                   \
+  remove_mask |= BrowsingDataRemover::DATA_TYPE_DOWNLOADS; \
+  break;                                                   \
+
+#include "../../../../../../chrome/browser/android/browsing_data/browsing_data_bridge.cc"
+#undef BRAVE_HANDLE_DATA_TYPE_DOWNLOADS
