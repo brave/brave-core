5a7f916bce0bb09a65303eeed0de36a48bfe6ac0
Simon Hong <shong@brave.com>
2020-03-03 21:33:28 +0900

    Disable wayback machine for localhost request
    fix https://github.com/brave/brave-browser/issues/8420

---
+++ components/brave_wayback_machine/brave_wayback_machine_utils.cc
@@ -0,0 +1,22 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/components/brave_wayback_machine/brave_wayback_machine_utils.h"
+
+#include <string>
+
+#include "base/strings/string_util.h"
+#include "net/base/url_util.h"
+#include "url/gurl.h"
+
+bool IsWaybackMachineDisabledFor(const GURL& url) {
+  if (net::IsLocalhost(url))
+    return true;
+
+  if (base::EndsWith(url.host(), ".local", base::CompareCase::SENSITIVE))
+    return true;
+
+  return false;
+}
