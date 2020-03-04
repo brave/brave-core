8ab95b35ed164c1a844c28abc025f0dba9a06933
Anthony Tseng <darkdh@gmail.com>
2020-03-03 17:42:34 -0800

    Add sync controls to brave sync page
    using existing elements to avoid duplicate logic
    
    enforce custom passphrase
    
    Integrate sync code flow
    
    Add view code and fix sync status update

--- browser/resources/settings/brave_sync_page/brave_sync_page.js
+++ browser/resources/settings/brave_sync_page/brave_sync_page.js
@@ -2,28 +2,46 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
- (function() {
-  'use strict';
+/**
+ * 'settings-brave-sync-page' is the settings page containing brave's
+ * custom sync.
+ */
+Polymer({
+  is: 'settings-brave-sync-page',
 
-  /**
-   * 'settings-brave-sync-page' is the settings page containing brave's
-   * custom sync.
-   */
-  Polymer({
-    is: 'settings-brave-sync-page',
+  behaviors: [I18nBehavior, WebUIListenerBehavior],
 
-    properties: {},
-
-    /** @private {?settings.DefaultBraveSyncBrowserProxy} */
-    browserProxy_: null,
-
-    /** @override */
-    created: function() {
-      this.browserProxy_ = settings.DefaultBraveSyncBrowserProxyImpl.getInstance();
+  properties: {
+    /**
+     * Preferences state.
+     */
+    prefs: {
+      type: Object,
+      notify: true,
     },
 
-    /** @override */
-    ready: function() {
-    },
-  });
-  })();
+    /**
+     * The current sync status, supplied by SyncBrowserProxy.
+     * @type {?settings.SyncStatus}
+     */
+    syncStatus: Object,
+
+    /**
+     * Dictionary defining page visibility.
+     * @type {!PageVisibility}
+     */
+    pageVisibility: Object,
+  },
+
+  /** @override */
+  attached: function() {
+    this.addWebUIListener('sync-settings-saved', () => {
+    });
+  },
+  /** @private */
+  onSyncTap_: function() {
+    // Users can go to sync subpage regardless of sync status.
+    settings.navigateTo(settings.routes.BRAVE_SYNC_SETUP);
+  },
+
+});
