c2235650c0ca8582428982578ecd2b09f8743283
brave-builds <devops@brave.com>
2020-03-05 21:09:40 +0000

    Uplift of #4721 (squashed) to dev

---
+++ third_party/blink/renderer/modules/brave/navigator_brave.h
@@ -0,0 +1,45 @@
+// Copyright (c) 2020 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_THIRD_PARTY_BLINK_RENDERER_MODULES_BRAVE_NAVIGATOR_BRAVE_H_
+#define BRAVE_THIRD_PARTY_BLINK_RENDERER_MODULES_BRAVE_NAVIGATOR_BRAVE_H_
+
+#include "third_party/blink/renderer/core/frame/navigator.h"
+#include "third_party/blink/renderer/platform/bindings/name_client.h"
+#include "third_party/blink/renderer/platform/heap/handle.h"
+#include "third_party/blink/renderer/platform/supplementable.h"
+
+namespace blink {
+
+class Brave;
+class Navigator;
+
+class NavigatorBrave final
+    : public GarbageCollected<NavigatorBrave>,
+      public Supplement<Navigator>,
+      public NameClient {
+  USING_GARBAGE_COLLECTED_MIXIN(NavigatorBrave);
+
+ public:
+  static const char kSupplementName[];
+
+  static NavigatorBrave& From(Navigator&);
+  static Brave* brave(Navigator&);
+  Brave* brave();
+
+  explicit NavigatorBrave(Navigator&);
+
+  void Trace(blink::Visitor*) override;
+  const char* NameInHeapSnapshot() const override {
+    return "NavigatorBrave";
+  }
+
+ private:
+  Member<Brave> brave_;
+};
+
+}  // namespace blink
+
+#endif  // BRAVE_THIRD_PARTY_BLINK_RENDERER_MODULES_BRAVE_NAVIGATOR_BRAVE_H_
