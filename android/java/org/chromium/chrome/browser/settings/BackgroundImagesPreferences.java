dcd8c845e29669d92d3cd7cff018f60c5cc2392f
bridiver <github@brianjohnson.cc>
2020-03-10 18:53:54 -0700

    fix pref handling

--- android/java/org/chromium/chrome/browser/settings/BackgroundImagesPreferences.java
+++ android/java/org/chromium/chrome/browser/settings/BackgroundImagesPreferences.java
@@ -12,14 +12,11 @@ import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.chrome.R;
-import org.chromium.chrome.browser.ChromeFeatureList;
-import org.chromium.chrome.browser.BraveFeatureList;
+import org.chromium.chrome.browser.ntp_sponsored_images.NTPSponsoredImagesBridge;
 import org.chromium.chrome.browser.preferences.BravePrefServiceBridge;
 import org.chromium.chrome.browser.BraveRelaunchUtils;
 import org.chromium.chrome.browser.preferences.BravePref;
-import org.chromium.chrome.browser.preferences.PrefServiceBridge;
 import org.chromium.chrome.browser.profiles.Profile;
-import org.chromium.chrome.browser.BraveAdsNativeHelper;
 
 /**
  * Fragment to keep track of all the display related preferences.
@@ -42,9 +39,7 @@ public class BackgroundImagesPreferences extends BravePreferenceFragment
         super.onCreate(savedInstanceState);
         getActivity().setTitle(R.string.prefs_new_tab_page);
         SettingsUtils.addPreferencesFromResource(this, R.xml.background_images_preferences);
-        if (!BraveAdsNativeHelper.nativeIsLocaleValid(Profile.getLastUsedProfile())
-            || BravePrefServiceBridge.getInstance().getSafetynetCheckFailed()
-            || !ChromeFeatureList.isEnabled(BraveFeatureList.BRAVE_REWARDS)) {
+        if (!NTPSponsoredImagesBridge.enableSponsoredImages(Profile.getLastUsedProfile())) {
             removePreferenceIfPresent(PREF_SHOW_SPONSORED_IMAGES);
         }
     }
@@ -60,18 +55,13 @@ public class BackgroundImagesPreferences extends BravePreferenceFragment
         showBackgroundImagesPref = (ChromeSwitchPreference) findPreference(PREF_SHOW_BACKGROUND_IMAGES);
         if (showBackgroundImagesPref != null) {
             showBackgroundImagesPref.setEnabled(true);
-            showBackgroundImagesPref.setChecked(sharedPreferences.getBoolean(PREF_SHOW_BACKGROUND_IMAGES, true));
+            showBackgroundImagesPref.setChecked(BravePrefServiceBridge.getInstance().getBoolean(BravePref.NTP_SHOW_BACKGROUND_IMAGE));
             showBackgroundImagesPref.setOnPreferenceChangeListener(this);
         }
         showSponsoredImagesPref = (ChromeSwitchPreference) findPreference(PREF_SHOW_SPONSORED_IMAGES);
         if (showSponsoredImagesPref != null) {
-            if (sharedPreferences.getBoolean(PREF_SHOW_BACKGROUND_IMAGES, true)) {
-                showSponsoredImagesPref.setEnabled(true);
-            } else {
-                showSponsoredImagesPref.setEnabled(false);
-            }
-
-            showSponsoredImagesPref.setChecked(PrefServiceBridge.getInstance().getBoolean(BravePref.NTP_SHOW_BRANDED_BACKGROUND_IMAGE));
+            showSponsoredImagesPref.setEnabled(BravePrefServiceBridge.getInstance().getBoolean(BravePref.NTP_SHOW_BACKGROUND_IMAGE));
+            showSponsoredImagesPref.setChecked(BravePrefServiceBridge.getInstance().getBoolean(BravePref.NTP_SHOW_BRANDED_BACKGROUND_IMAGE));
             showSponsoredImagesPref.setOnPreferenceChangeListener(this);
         }
     }
@@ -79,27 +69,24 @@ public class BackgroundImagesPreferences extends BravePreferenceFragment
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (PREF_SHOW_BACKGROUND_IMAGES.equals(preference.getKey()) && showSponsoredImagesPref != null) {
-            if ((boolean)newValue) {
-                showSponsoredImagesPref.setEnabled(true);
-                showSponsoredImagesPref.setChecked(true);
-            } else {
-                showSponsoredImagesPref.setEnabled(false);
-            }
+            showSponsoredImagesPref.setEnabled((boolean)newValue);
         }
-
-        if (PREF_SHOW_SPONSORED_IMAGES.equals(preference.getKey())) {
-            PrefServiceBridge.getInstance().setBoolean(BravePref.NTP_SHOW_BRANDED_BACKGROUND_IMAGE, (boolean)newValue);
-        }
-        // setOnPreferenceValue(preference.getKey(), (boolean)newValue);
+        setOnPreferenceValue(preference.getKey(), (boolean)newValue);
         BraveRelaunchUtils.askForRelaunch(getActivity());
         return true;
     }
 
     public static void setOnPreferenceValue(String preferenceName, boolean newValue) {
-        SharedPreferences sharedPreferences = ContextUtils.getAppSharedPreferences();
-        SharedPreferences.Editor sharedPreferencesEditor = sharedPreferences.edit();
-        sharedPreferencesEditor.putBoolean(preferenceName, newValue);
-        sharedPreferencesEditor.apply();
+        if (PREF_SHOW_BACKGROUND_IMAGES.equals(preferenceName)) {
+            BravePrefServiceBridge.getInstance().setBoolean(BravePref.NTP_SHOW_BACKGROUND_IMAGE, newValue);
+        } else if (PREF_SHOW_SPONSORED_IMAGES.equals(preferenceName)) {
+            BravePrefServiceBridge.getInstance().setBoolean(BravePref.NTP_SHOW_BRANDED_BACKGROUND_IMAGE, (boolean)newValue);
+        } else {
+            SharedPreferences sharedPreferences = ContextUtils.getAppSharedPreferences();
+            SharedPreferences.Editor sharedPreferencesEditor = sharedPreferences.edit();
+            sharedPreferencesEditor.putBoolean(preferenceName, newValue);
+            sharedPreferencesEditor.apply();
+        }
     }
 
     public static void setOnPreferenceValue(String preferenceName, int newValue) {
