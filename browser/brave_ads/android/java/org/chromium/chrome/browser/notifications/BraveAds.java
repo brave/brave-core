guifel/brave-core-1

Silent background notifications. Issue# 2419

browser/brave_ads/android/java/org/chromium/chrome/browser/notifications/BraveAds.java
@@ -18,4 +18,9 @@ public abstract class BraveAds {
     public static String getBraveAdsChannelId() {
         return BraveChannelDefinitions.ChannelId.BRAVE_ADS;
     }
+
+    @CalledByNative
+    public static String getBraveAdsBackgroundChannelId() {
+        return ChannelDefinitions.ChannelId.BRAVE_ADS_BACKGROUND;
+    }
 }

brave/brave-core

browser/brave_ads/android/java/org/chromium/chrome/browser/notifications/BraveAds.java
@@ -18,4 +18,9 @@ public abstract class BraveAds {
     public static String getBraveAdsChannelId() {
         return BraveChannelDefinitions.ChannelId.BRAVE_ADS;
     }
+
+    @CalledByNative
+    public static String getBraveAdsBackgroundChannelId() {
+        return BraveChannelDefinitions.ChannelId.BRAVE_ADS_BACKGROUND;
+    }
 }