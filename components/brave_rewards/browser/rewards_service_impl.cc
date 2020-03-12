2d0a18252549167b5a91977578fae2f56ed5db96
brave-builds <devops@brave.com>
2020-03-12 19:34:55 +0000

    Uplift of #4907 (squashed) to release

--- components/brave_rewards/browser/rewards_service_impl.cc
+++ components/brave_rewards/browser/rewards_service_impl.cc
@@ -349,7 +349,7 @@ bool ResetOnFileTaskRunner(const base::FilePath& path) {
 bool ResetOnFilesTaskRunner(const std::vector<base::FilePath>& paths) {
   bool res = true;
   for (size_t i = 0; i < paths.size(); i++) {
-    if (!base::DeleteFile(paths[i], false)) {
+    if (!base::DeleteFileRecursively(paths[i])) {
       res = false;
     }
   }
