3bd1f134974a414b4d4ac917e0ad0019188ba668
Simon Hong <shong@brave.com>
2020-03-12 10:47:56 +0900

    Don't include import impl in android

--- chromium_src/chrome/utility/services.cc
+++ chromium_src/chrome/utility/services.cc
@@ -3,8 +3,15 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "brave/utility/importer/brave_profile_import_impl.h"
+#include "build/build_config.h"
 
+#if !defined(OS_ANDROID)
+#include "brave/utility/importer/brave_profile_import_impl.h"
 #define ProfileImportImpl BraveProfileImportImpl
+#endif
+
 #include "../../../../chrome/utility/services.cc"
+
+#if !defined(OS_ANDROID)
 #undef ProfileImportImpl
+#endif
