c43da4b30f2764b6f516f2d33e7dc5d416700bd2
Deep <deeppandya91@gmail.com>
2020-03-16 18:59:39 -0400

    Update image loading code
    Update image gradients

--- android/java/org/chromium/chrome/browser/util/ImageUtils.java
+++ android/java/org/chromium/chrome/browser/util/ImageUtils.java
@@ -16,6 +16,7 @@ import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Shader;
+import android.graphics.BitmapFactory;
 
 import org.chromium.chrome.R;
 import org.chromium.base.ContextUtils;
@@ -29,6 +30,29 @@ public class ImageUtils {
         Canvas canvas = new Canvas(outputimage);
         canvas.drawBitmap(src, 0, offsetY, null);
         return outputimage;
+    }
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
     }
 
     public static Bitmap addGradient(Bitmap src, int gradientHeight) {
