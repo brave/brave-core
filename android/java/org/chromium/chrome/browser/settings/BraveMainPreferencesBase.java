4050d8e737ac0829a503c0bb099179e47db334a3
brave-builds <devops@brave.com>
2020-03-17 13:28:26 +0000

    Uplift of #4959 (squashed) to release

--- android/java/org/chromium/chrome/browser/settings/BraveMainPreferencesBase.java
+++ android/java/org/chromium/chrome/browser/settings/BraveMainPreferencesBase.java
@@ -92,6 +92,7 @@ public class BraveMainPreferencesBase extends BravePreferenceFragment {
         if (!ChromeFeatureList.isEnabled(BraveFeatureList.BRAVE_REWARDS) ||
                 BravePrefServiceBridge.getInstance().getSafetynetCheckFailed()) {
             removePreferenceIfPresent(PREF_BRAVE_REWARDS);
+            removePreferenceIfPresent(PREF_WELCOME_TOUR);
         }
     }
 
