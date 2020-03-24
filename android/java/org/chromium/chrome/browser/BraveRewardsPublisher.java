1d972548bcf2c4e5dae1e72483efdd2af818d6a8
George Dregalo <37560650+gdregalo@users.noreply.github.com>
2020-03-24 11:45:47 -0400

    Android: Implement User wallets (Uphold). Issue# 8511.

---
+++ android/java/org/chromium/chrome/browser/BraveRewardsPublisher.java
@@ -0,0 +1,21 @@
+/** Copyright (c) 2020 The Brave Authors. All rights reserved.
+  * This Source Code Form is subject to the terms of the Mozilla Public
+  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+  * You can obtain one at http://mozilla.org/MPL/2.0/.
+  */
+package org.chromium.chrome.browser;
+
+import androidx.annotation.IntDef;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+class BraveRewardsPublisher {
+    //PublisherStatus @
+    //vendor/bat-native-ledger/include/bat/ledger/public/interfaces/ledger.mojom
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({NOT_VERIFIED, CONNECTED, VERIFIED})
+    public @interface PublisherStatus {}
+    public static final int NOT_VERIFIED = 0;
+    public static final int CONNECTED = 1;
+    public static final int VERIFIED = 2;
+}
--- components/brave_rewards/browser/external_wallet.cc
+++ components/brave_rewards/browser/external_wallet.cc
@@ -3,6 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "base/values.h"
+#include "base/json/json_writer.h"
 #include "brave/components/brave_rewards/browser/external_wallet.h"
 
 namespace brave_rewards {
@@ -25,4 +27,21 @@ namespace brave_rewards {
     account_url = properties.account_url;
   }
 
+  std::string ExternalWallet::toJson() {
+    std::string json_wallet;
+    base::Value dict(base::Value::Type::DICTIONARY);
+    dict.SetStringKey("token", token);
+    dict.SetStringKey("address", address);
+
+    // enum class WalletStatus : int32_t
+    dict.SetIntKey("status", static_cast<int32_t>(status));
+    dict.SetStringKey("type", type);
+    dict.SetStringKey("verify_url", verify_url);
+    dict.SetStringKey("add_url", add_url);
+    dict.SetStringKey("withdraw_url", withdraw_url);
+    dict.SetStringKey("user_name", user_name);
+    dict.SetStringKey("account_url", account_url);
+    base::JSONWriter::Write(dict, &json_wallet);
+    return json_wallet;
+  }
 }  // namespace brave_rewards
