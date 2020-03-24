55254c743c6d1fa6cad2a9056067d3b308cd5cd2
Emerick Rogul <erogul@brave.com>
2020-03-24 12:58:11 -0400

    Remove Brave Account autofill suggestion on Android

---
+++ chromium_src/chrome/browser/signin/signin_promo_util.cc
@@ -0,0 +1,16 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#define ShouldShowPromo ShouldShowPromo_ChromiumImpl
+#include "../../../../../chrome/browser/signin/signin_promo_util.cc"
+#undef ShouldShowPromo
+
+namespace signin {
+
+bool ShouldShowPromo(Profile* profile) {
+  return false;
+}
+
+}  // namespace signin
