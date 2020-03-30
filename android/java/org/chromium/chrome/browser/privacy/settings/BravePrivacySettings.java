3a47f979da77e42f32f00c6062782a5fce5b2517
mkarolin <max@brave.com>
2020-03-30 16:13:53 -0400

    [Android] Privacy Settings moved.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/a764638b06aaa69d60a43c3ceaecd0f4ce2792e8
    
    commit a764638b06aaa69d60a43c3ceaecd0f4ce2792e8
    Author: Natalie Chouinard <chouinard@chromium.org>
    Date:   Wed Mar 11 23:53:23 2020 +0000
    
        [Settings Refactor] Move Privacy settings
    
        Move remaining privacy settings screens classes to
        org.chromium.chrome.browser.privacy.settings package and update OWNERS.
    
        Bug: 1047357

--- android/java/org/chromium/chrome/browser/privacy/settings/BravePrivacySettings.java
+++ android/java/org/chromium/chrome/browser/privacy/settings/BravePrivacySettings.java
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.chromium.chrome.browser.settings.privacy;
+package org.chromium.chrome.browser.privacy.settings;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
