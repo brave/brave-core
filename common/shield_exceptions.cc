76c32d655b8eebe6a37f00540059510c87fb9f24
brave-builds <devops@brave.com>
2020-03-31 22:48:17 +0000

    Uplift of #5109 (squashed) to beta

--- common/shield_exceptions.cc
+++ common/shield_exceptions.cc
@@ -40,6 +40,18 @@ bool IsBlockedResource(const GURL& gurl) {
 
 bool IsWhitelistedFingerprintingException(const GURL& firstPartyOrigin,
     const GURL& subresourceUrl) {
+  // Always allow embeds from public.tableau.com while fingerprinting
+  // protections are being reworked to need less exceptions.
+  static const std::vector<URLPattern> embed_exceptions = {
+    URLPattern(URLPattern::SCHEME_ALL, "https://public.tableau.com/*"),
+    URLPattern(URLPattern::SCHEME_ALL, "https://www.arcgis.com/*"),
+  };
+  for (const auto exception : embed_exceptions) {
+    if (exception.MatchesURL(subresourceUrl)) {
+      return true;
+    }
+  }
+
   static std::map<URLPattern, std::vector<URLPattern> > whitelist_patterns = {
     {
       URLPattern(URLPattern::SCHEME_ALL, "https://uphold.com/"),
