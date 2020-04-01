0ad973542bb615ff9674a15bb4b2b13fb13a0763
mkarolin <max@brave.com>
2020-04-01 11:43:15 -0400

    ProxyResolutionService -> ConfiguredProxyResolutionService.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/d2013f9668d0587b1042572dadc6bd2e772d3248
    
    commit d2013f9668d0587b1042572dadc6bd2e772d3248
    Author: Nicolas Arciniega <niarci@microsoft.com>
    Date:   Fri Feb 7 23:00:56 2020 +0000
    
        Rename ProxyResolutionService to ConfiguredProxyResolutionService
    
        This change is the first step in creating a proxy resolver that
        completely relies on the underlying system to figure out a proxy. The
        ProxyResolutionService is currently deeply tied to the notion of first
        fetching proxy configurations (ex: a PAC URL) and then using those
        configurations to resolve a proxy. Since a proxy resolver that truly
        defers to the underlying operating system for all proxy logic does not
        follow this model, it is helpful if we separate the generic
        ProxyResolutionService from the notion of configurations.
    
        This change renames the existing ProxyResolutionService, which relies
        on fetching proxy configurations, to ConfiguredProxyResolutionService.
        The only other changes to this file are suggestions from presubmit and
        Tricium.
    
        A set of future CLs will break up the ConfiguredProxyResolutionService
        into the logical pieces needed to create a new, generic
        ProxyResolutionService that does not necessarily depend on configs.
    
        This change is intended to introduce zero functional changes. It is a
        pure refactor change.
    
        Bug: 1032820

--- chromium_src/net/proxy_resolution/configured_proxy_resolution_service.cc
+++ chromium_src/net/proxy_resolution/configured_proxy_resolution_service.cc
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "net/proxy_resolution/proxy_resolution_service.h"
+#include "net/proxy_resolution/configured_proxy_resolution_service.h"
 
 #include "brave/net/proxy_resolution/proxy_config_service_tor.h"
 
@@ -23,5 +23,5 @@ void SetTorCircuitIsolation(const ProxyConfigWithAnnotation& config,
 #define BRAVE_TRY_TO_COMPLETE_SYNCHRONOUSLY \
   SetTorCircuitIsolation(config_.value(), url, result, this);
 
-#include "../../../../net/proxy_resolution/proxy_resolution_service.cc"  // NOLINT
+#include "../../../../net/proxy_resolution/configured_proxy_resolution_service.cc"
 #undef BRAVE_TRY_TO_COMPLETE_SYNCHRONOUSLY
