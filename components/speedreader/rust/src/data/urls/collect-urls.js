eae930aa20a107600943c5cdbab08a77cc987d2b
AndriusA <aaucinas@brave.com>
2020-04-02 12:28:36 +0100

    moves all html data samples off to a separate repository

--- components/speedreader/rust/src/data/urls/collect-urls.js
+++
@@ -1,9 +0,0 @@
-let urls = [...document.querySelectorAll("a")]
-    .map(d => { 
-        if (d.href.startsWith("http")) {
-            return d.href;
-        }
-    });
-uniq_urls = [...new Set(urls)];
-uniq_urls.sort();
-JSON.stringify(uniq_urls, null, 2)
