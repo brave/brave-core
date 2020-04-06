1dda4968764e5f6088616f2b70546353edf2a67c
mkarolin <max@brave.com>
2020-04-06 15:17:58 -0400

    Override install_modes.cc.
    Chromium changes make is easier to override the whole file rather than
    patch each BUILDFLAG(USE_GOOGLE_UPDATE_INTEGRATION) location.
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/f2c32196752a7e8a7b32c358c8529784b3f33c53
    
    commit f2c32196752a7e8a7b32c358c8529784b3f33c53
    Author: Nico Weber <thakis@chromium.org>
    Date:   Thu Mar 26 15:06:15 2020 +0000
    
        win: Convert kUseGoogleUpdateIntegration to a buildflag.
    
        - Suppresses several -Wunreachable-code warnings
        - Makes it clearer this is a build-time constant
        - Removes half a use of GOOGLE_CHROME_BUILD in a .h file
    
        No intended behavior change.
    
        Bug: 346399,961769

---
+++ chromium_src/chrome/install_static/install_modes.cc
@@ -0,0 +1,18 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "chrome/install_static/buildflags.h"
+
+#if defined(OFFICIAL_BUILD)
+#define BUILDFLAG_INTERNAL_USE_GOOGLE_UPDATE_INTEGRATION() (1)
+#endif
+
+#define Google BraveSoftware
+#include "../../../../chrome/install_static/install_modes.cc"
+#undef Google
+
+#if defined(OFFICIAL_BUILD)
+#undef BUILDFLAG_INTERNAL_USE_GOOGLE_UPDATE_INTEGRATION
+#endif
