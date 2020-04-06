1e2ada7919f36c132708e91c530644fb23543906
Anthony Tseng <darkdh@gmail.com>
2020-04-06 13:41:57 -0700

    Add brave sync access token fetcher and consumer

---
+++ components/brave_sync/access_token_consumer.cc
@@ -0,0 +1,21 @@
+#include "brave/components/brave_sync/access_token_consumer.h"
+
+namespace brave_sync {
+
+AccessTokenConsumer::TokenResponse::TokenResponse(
+    const std::string& access_token,
+    const base::Time& expiration_time,
+    const std::string& id_token)
+    : access_token(access_token),
+      expiration_time(expiration_time),
+      id_token(id_token) {}
+
+AccessTokenConsumer::~AccessTokenConsumer() {}
+
+void AccessTokenConsumer::OnGetTokenSuccess(
+    const TokenResponse& token_response) {}
+
+void AccessTokenConsumer::OnGetTokenFailure(
+    const std::string& error) {}
+
+}   // namespace brave_sync
---
+++ components/brave_sync/access_token_fetcher_impl.h
@@ -0,0 +1,84 @@
+#ifndef BRAVE_COMPONENTS_BRAVE_SYNC_ACCESS_TOKEN_FETCHER_IMPL_H_
+#define BRAVE_COMPONENTS_BRAVE_SYNC_ACCESS_TOKEN_FETCHER_IMPL_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "base/macros.h"
+#include "base/memory/ref_counted.h"
+#include "brave/components/brave_sync/access_token_fetcher.h"
+#include "brave/components/brave_sync/access_token_consumer.h"
+#include "url/gurl.h"
+
+namespace network {
+class SimpleURLLoader;
+class SharedURLLoaderFactory;
+}
+
+namespace brave_sync {
+
+class AccessTokenFetcherImpl : public AccessTokenFetcher {
+ public:
+  AccessTokenFetcherImpl(
+      AccessTokenConsumer* consumer,
+      scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
+      const std::string& refresh_token);
+  ~AccessTokenFetcherImpl() override;
+
+  // Implementation of AccessTokenFetcher
+  void Start(const std::string& client_id,
+             const std::string& client_secret) override;
+
+  void CancelRequest() override;
+
+ private:
+  enum State {
+    INITIAL,
+    GET_ACCESS_TOKEN_STARTED,
+    GET_ACCESS_TOKEN_DONE,
+    ERROR_STATE,
+  };
+
+  void OnURLLoadComplete(std::unique_ptr<std::string> response_body);
+
+  // Helper methods for the flow.
+  void StartGetAccessToken();
+  void EndGetAccessToken(std::unique_ptr<std::string> response_body);
+
+  // Helper mehtods for reporting back results.
+  void OnGetTokenSuccess(
+      const AccessTokenConsumer::TokenResponse& token_response);
+  void OnGetTokenFailure(const std::string& error);
+
+  // Other helpers.
+  static GURL MakeGetAccessTokenUrl();
+  static std::string MakeGetAccessTokenBody(
+      const std::string& client_id,
+      const std::string& client_secret,
+      const std::string& refresh_token);
+
+  static bool ParseGetAccessTokenSuccessResponse(
+      std::unique_ptr<std::string> response_body,
+      std::string* access_token,
+      int* expires_in,
+      std::string* id_token);
+
+  static bool ParseGetAccessTokenFailureResponse(
+      std::unique_ptr<std::string> response_body,
+      std::string* error);
+
+  // State that is set during construction.
+  scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
+  const std::string refresh_token_;
+  State state_;
+
+  // While a fetch is in progress.
+  std::unique_ptr<network::SimpleURLLoader> url_loader_;
+  std::string client_id_;
+  std::string client_secret_;
+
+  DISALLOW_COPY_AND_ASSIGN(AccessTokenFetcherImpl);
+};
+}   // namespace brave_sync
+#endif  // BRAVE_COMPONENTS_BRAVE_SYNC_ACCESS_TOKEN_FETCHER_IMPL_H_
