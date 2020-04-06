4d8a38958b34de19cc6c839f74d7381b7250732b
mkarolin <max@brave.com>
2020-04-06 10:36:24 -0400

    views::FlexSpecification::ForXXX methods removed.
    Use constructors instead.
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/b0065070fe6ae7e9a1730b7552c8ea6a952bcdc0
    
    commit b0065070fe6ae7e9a1730b7552c8ea6a952bcdc0
    Author: Peter Kasting <pkasting@chromium.org>
    Date:   Tue Feb 4 20:19:40 2020 +0000
    
        Eliminate FlexSpecification private constructor.
    
        Having the various WithXXX() methods depending on this was fragile
        against future changes that add members to the class, unless the
        constructor takes them as args.  This bit me while fixing bug 1012136.
        They now do a copy-and-set (preserving all existing state).
    
        Having the ForXXX() methods depend on this failed to take advantage of
        member default values, and delegating constructors, which is also a bit
        of a maintenance hazard.  These are now constructors directly.
    
        Finally, having factory functions is unusual when construction cannot
        fail, needs no extra initialization, and doesn't return a pointer to a
        polymorphic base; plus, the factory methods are more verbose, but (IMO)
        do not really add significant/necessary clarity.
    
        Bug: none

--- browser/ui/views/infobars/brave_wayback_machine_infobar_contents_view.cc
+++ browser/ui/views/infobars/brave_wayback_machine_infobar_contents_view.cc
@@ -121,9 +121,8 @@ void BraveWaybackMachineInfoBarContentsView::InitializeChildren() {
   AddChildView(separator_);
 
   const views::FlexSpecification label_flex_rule =
-      views::FlexSpecification::ForSizeRule(
-          views::MinimumFlexSizeRule::kScaleToMinimum,
-          views::MaximumFlexSizeRule::kPreferred);
+      views::FlexSpecification(views::MinimumFlexSizeRule::kScaleToMinimum,
+                               views::MaximumFlexSizeRule::kPreferred);
   auto* label = CreateLabel(
       l10n_util::GetStringUTF16(
           IDS_BRAVE_WAYBACK_MACHINE_INFOBAR_PAGE_MISSING_TEXT));
@@ -156,9 +155,9 @@ void BraveWaybackMachineInfoBarContentsView::InitializeChildren() {
   place_holder_view->SetProperty(views::kMarginsKey, gfx::Insets(12, 0));
   place_holder_view->SetProperty(
       views::kFlexBehaviorKey,
-      views::FlexSpecification::ForSizeRule(
-          views::MinimumFlexSizeRule::kScaleToZero,
-          views::MaximumFlexSizeRule::kUnbounded).WithOrder(3));
+      views::FlexSpecification(views::MinimumFlexSizeRule::kScaleToZero,
+                               views::MaximumFlexSizeRule::kUnbounded)
+          .WithOrder(3));
   AddChildView(place_holder_view);
 
   label = CreateLabel(
