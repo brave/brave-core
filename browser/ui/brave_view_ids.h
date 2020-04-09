d2495b0c161511dfdd4c30549f90a0dbffd7b072
Ivan Efremov <iefremov@brave.com>
2020-04-10 02:22:58 +0700

    Get rid of the patch.

---
+++ browser/ui/brave_view_ids.h
@@ -0,0 +1,14 @@
+/* Copyright 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_BROWSER_UI_BRAVE_VIEW_IDS_H_
+#define BRAVE_BROWSER_UI_BRAVE_VIEW_IDS_H_
+
+// See src/chrome/browser/ui/view_ids.h
+enum BraveViewIds {
+  BRAVE_VIEW_ID_SPEEDREADER_BUTTON = 100000,
+};
+
+#endif  // BRAVE_BROWSER_UI_BRAVE_VIEW_IDS_H_
