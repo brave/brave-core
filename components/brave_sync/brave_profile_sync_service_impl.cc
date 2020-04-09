2498d9fdc856543059d2b027120662ce853f41c7
Brian Clifton <brian@clifton.me>
2020-04-09 09:05:56 -0700

    Put migration of sync data behind the overall feature flag
    - Prevents https://github.com/brave/brave-browser/issues/9110 (or similar) from happening
    - Added unit test to ensure migration not done if sync disabled

--- components/brave_sync/brave_profile_sync_service_impl.cc
+++ components/brave_sync/brave_profile_sync_service_impl.cc
@@ -824,9 +824,14 @@ void BraveProfileSyncServiceImpl::SetPermanentNodesOrder(
 }
 
 // static
-void BraveProfileSyncServiceImpl::MigrateDuplicatedBookmarksObjectIds(
+bool BraveProfileSyncServiceImpl::MigrateDuplicatedBookmarksObjectIds(
+    bool sync_enabled,
     Profile* profile,
     BookmarkModel* model) {
+  if (!sync_enabled) {
+    return false;
+  }
+
   DCHECK(model);
   DCHECK(model->loaded());
 
@@ -834,7 +839,7 @@ void BraveProfileSyncServiceImpl::MigrateDuplicatedBookmarksObjectIds(
       prefs::kDuplicatedBookmarksMigrateVersion);
 
   if (migrated_version >= 2) {
-    return;
+    return true;
   }
 
   // Copying bookmarks through brave://bookmarks page could duplicate brave sync
@@ -845,6 +850,7 @@ void BraveProfileSyncServiceImpl::MigrateDuplicatedBookmarksObjectIds(
   ClearDuplicatedNodes(&object_id_nodes, model);
 
   profile->GetPrefs()->SetInteger(prefs::kDuplicatedBookmarksMigrateVersion, 2);
+  return true;
 }
 
 std::unique_ptr<SyncRecord>
