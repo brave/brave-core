6bdce71ab0f2e6129b6b2d92063a9583a7bb4c1b
AlexeyBarabash <alexey@brave.com>
2020-04-13 15:24:49 +0300

    Include only necessary files from android_clang_ dir to packed symbols; fixes brave-browser#9168

--- build/android/makeArchive7z.sh
+++ build/android/makeArchive7z.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 mkdir -p dist
-tar -czvf $1 --ignore-failed-read *.so apks android_clang_* lib.unstripped android_chrome_versions.txt
+tar -czvf $1 --ignore-failed-read *.so apks android_clang_*/*.so android_clang_*/lib.unstripped lib.unstripped android_chrome_versions.txt
