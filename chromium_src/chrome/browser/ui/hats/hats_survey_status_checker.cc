78a8a51454cd0709b8d4963f7b84c2b7d76d15bf
mkarolin <max@brave.com>
2020-04-13 15:42:15 -0400

    Patched HatsSurveyStatusChecker's use of CanonicalCookie::Create.
    We added an extra parameter to CanonicalCookie::Create in the past.
    This change updates newly added code with the extra param.
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/dcf3e8ddbd1aaba4585859b713d00c9f8bc2c155
    
    commit dcf3e8ddbd1aaba4585859b713d00c9f8bc2c155
    Author: Wei Li <weili@chromium.org>
    Date:   Tue Mar 10 04:43:38 2020 +0000
    
        [HaTS] Stop when the survey reaches its capacity
    
        Added a check for the survey's status. When the survey is not
        reachable or over its capacity, we won't show the HaTS bubble any more.
        Also, we will record the over capacity information so we won't need to
        check it again.
    
        BUG=1048385

---
+++ chromium_src/chrome/browser/ui/hats/hats_survey_status_checker.cc
@@ -0,0 +1,9 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#define BRAVE_HATS_SURVEY_STATUS_CHECKER true /*is_from_http*/,
+
+#include "../../../../../../chrome/browser/ui/hats/hats_survey_status_checker.cc"
+#undef BRAVE_HATS_SURVEY_STATUS_CHECKER
