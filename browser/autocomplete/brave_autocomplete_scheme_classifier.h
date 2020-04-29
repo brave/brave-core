9908b06e0a4fcebaf63bb5994f6fc71823d5b591
Simon Hong <shong@brave.com>
2020-04-29 13:50:57 +0900

    Fix lint error

--- browser/autocomplete/brave_autocomplete_scheme_classifier.h
+++ browser/autocomplete/brave_autocomplete_scheme_classifier.h
@@ -1,14 +1,18 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #ifndef BRAVE_BROWSER_AUTOCOMPLETE_BRAVE_AUTOCOMPLETE_SCHEME_CLASSIFIER_H_
 #define BRAVE_BROWSER_AUTOCOMPLETE_BRAVE_AUTOCOMPLETE_SCHEME_CLASSIFIER_H_
 
+#include <string>
+
 #include "brave/components/brave_webtorrent/browser/buildflags/buildflags.h"
 #include "chrome/browser/autocomplete/chrome_autocomplete_scheme_classifier.h"
 
-class BraveAutocompleteSchemeClassifier : public ChromeAutocompleteSchemeClassifier {
+class BraveAutocompleteSchemeClassifier
+    : public ChromeAutocompleteSchemeClassifier {
  public:
   explicit BraveAutocompleteSchemeClassifier(Profile* profile);
   ~BraveAutocompleteSchemeClassifier() override;
--- browser/search_engines/search_engine_provider_service_factory.h
+++ browser/search_engines/search_engine_provider_service_factory.h
@@ -1,9 +1,10 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef BRAVE_BROWSER_SEARCH_ENGINE_SEARCH_ENGINE_PROVIDER_SERVICE_FACTORY_H_
-#define BRAVE_BROWSER_SEARCH_ENGINE_SEARCH_ENGINE_PROVIDER_SERVICE_FACTORY_H_
+#ifndef BRAVE_BROWSER_SEARCH_ENGINES_SEARCH_ENGINE_PROVIDER_SERVICE_FACTORY_H_
+#define BRAVE_BROWSER_SEARCH_ENGINES_SEARCH_ENGINE_PROVIDER_SERVICE_FACTORY_H_
 
 #include "base/memory/singleton.h"
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
@@ -44,4 +45,4 @@ class SearchEngineProviderServiceFactory
   DISALLOW_COPY_AND_ASSIGN(SearchEngineProviderServiceFactory);
 };
 
-#endif  // BRAVE_BROWSER_SEARCH_ENGINE_SEARCH_ENGINE_PROVIDER_SERVICE_FACTORY_H_
+#endif  // BRAVE_BROWSER_SEARCH_ENGINES_SEARCH_ENGINE_PROVIDER_SERVICE_FACTORY_H_
