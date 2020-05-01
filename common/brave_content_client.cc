d255080d3f3871fd0417c9ad904d35cb7e229482
Simon Hong <shong@brave.com>
2020-05-01 12:14:16 +0900

    Append brave's flags ui code via BraveContentClient::GetDataResourceBytes
    With this, we can add our code to upstream file w/o patching.

--- common/brave_content_client.cc
+++ common/brave_content_client.cc
@@ -5,11 +5,33 @@
 
 #include "brave/common/brave_content_client.h"
 
+#include <string>
+
+#include "base/memory/ref_counted_memory.h"
+#include "components/grit/brave_components_resources.h"
+#include "components/grit/components_resources.h"
 #include "content/public/common/url_constants.h"
 
 BraveContentClient::BraveContentClient() {}
 
 BraveContentClient::~BraveContentClient() {}
+
+base::RefCountedMemory* BraveContentClient::GetDataResourceBytes(
+    int resource_id) {
+  if (resource_id == IDR_FLAGS_UI_FLAGS_JS) {
+    auto* chromium_flags_ui_data =
+        ChromeContentClient::GetDataResourceBytes(resource_id);
+    auto* brave_flags_ui_data = ChromeContentClient::GetDataResourceBytes(
+        IDR_FLAGS_UI_BRAVE_FLAGS_OVERRIDES_JS);
+    std::string new_flags_js(chromium_flags_ui_data->front_as<const char>(),
+                             chromium_flags_ui_data->size());
+    new_flags_js.append(brave_flags_ui_data->front_as<const char>(),
+                        brave_flags_ui_data->size());
+    return new base::RefCountedStaticMemory(new_flags_js.c_str(),
+                                            new_flags_js.length());
+  }
+  return ChromeContentClient::GetDataResourceBytes(resource_id);
+}
 
 void BraveContentClient::AddAdditionalSchemes(Schemes* schemes) {
   ChromeContentClient::AddAdditionalSchemes(schemes);
