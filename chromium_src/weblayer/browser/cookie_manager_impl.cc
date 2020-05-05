de055d88d711219046c16244e3221704bbaaccda
samartnik <artem@brave.com>
2020-05-05 10:34:15 -0400

    [Android] Fixed build error related to CanonicalCookie additional param

---
+++ chromium_src/weblayer/browser/cookie_manager_impl.cc
@@ -0,0 +1,9 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#define BRAVE_SETCOOKIEINTERNAL true /*is_from_http*/,
+
+#include "../../../../weblayer/browser/cookie_manager_impl.cc"
+#undef BRAVE_SETCOOKIEINTERNAL
