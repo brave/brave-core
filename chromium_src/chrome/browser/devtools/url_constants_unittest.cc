4f9734aa97d6f32644d86b31087d291d84a91050
Pranjal Jumde <pranjal.jumde@gmail.com>
2020-05-09 21:08:32 -0700

    Fix 7645: Enable remote debugging by default and proxy devtools request

---
+++ chromium_src/chrome/browser/devtools/url_constants_unittest.cc
@@ -0,0 +1,13 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "chrome/browser/ui/webui/devtools_ui.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+const char kBraveDevtoolsURLHost[] = "devtools.brave.com";
+
+TEST(BraveDevtoolsURLConstants, TestDevtoolsURL) {
+  EXPECT_EQ(DevToolsUI::GetRemoteBaseURL().host(), kBraveDevtoolsURLHost);
+}
