7495fc6002a6796c8a416cfd112c42d5bb163d1f
Kyle Hickinson <git@kylehickinson.com>
2020-05-14 21:15:00 -0400

    [iOS] Fixes iOS build (gen_model_wrapper).
    Chromium changes:
    
    https://chromium.googlesource.com/chromium/src/+/90b95abb9b18974c90ddba4c638dd7cefd4feae7
    
    commit 90b95abb9b18974c90ddba4c638dd7cefd4feae7
    Author: Ken Rockot <rockot@google.com>
    Date:   Thu Apr 2 20:07:29 2020 +0000
    
        Extract mojom parser from bindings generator
    
        This extracts the mojom parsing logic from mojom_bindings_generator.py
        into a new mojom_parser.py script within the mojom tool directory.
    
        Apart from simply moving the parsing logic to a new script, this also
        changes how the parse result is persisted by an intermediate build step
        for subsequent build steps to consume.
    
        Prior to this change, the parsing step emitted a ".p" file for each
        processed .mojom file. This was nothing more than a serialized version
        of the parsed AST with [EnableIf] feature filtering applied. Every
        subsequent step for the mojom in question would load this AST, then
        recursively process its import dependencies while loading their ASTs in
        turn, before finally processing all of the aggregated data (resolving
        references, computing field packing data, etc) to produce a Module
        object suitable for bindings generation.
    
        This CL rolls most of that processing into the parsing step (with more
        to follow in future CLs), and the resulting computed Module object is
        persisted to a ".mojom-module" file.
    
        To generate bindings for a given .mojom file, the generator can simply
        call Module.Load() on the corresponding .mojom-module produced by the
        parser. The loaded Module is sufficient to inform code generation
        with no need to resolve dependencies, process other modules, or
        perform other complex computations on the model.
    
        Each mojom-module is thus entirely self-contained, replicating the full
        contents of all transitive import dependencies. This means the build
        consumes more disk space with intermediate build artifacts, but it
        also means generation steps are faster and simpler.
    
        This CL should result in no net changes to the final build outputs.
    
        Bug: 1060471
    
    https://chromium.googlesource.com/chromium/src/+/77cb7c1e2a6250342e3b9349fd0d1f02eb676032
    
    commit 77cb7c1e2a6250342e3b9349fd0d1f02eb676032
    Author: Ken Rockot <rockot@google.com>
    Date:   Fri Mar 13 03:19:49 2020 +0000
    
        Fix mojom parser tests
    
        This fixes all the existing mojom parser tests that are still meaningful
        and applicable and deletes the rest. This also fixes or (where
        appropriate) disables any outstanding pylint issues in these flies.
    
        The tests can be run locally with:
    
          python -m unittest discover \
              -s mojo/public/tools/mojom/ -p "*_unittest.py"
    
        A follow-up CL will introduce a general-purpose script for registering
        and running all Python-based tests related to Mojo tools so that they
        can run on the CQ as well.
    
        Bug: 1060473

--- vendor/brave-ios/scripts/mojo/gen_model_wrappers.py
+++ vendor/brave-ios/scripts/mojo/gen_model_wrappers.py
@@ -2,47 +2,20 @@
 
 import argparse
 
-try:
-  import cPickle as pickle
-except ImportError:
-  import pickle
-
 import importlib
 import os
 import sys
 
 _current_dir = os.path.dirname(os.path.realpath(__file__))
-sys.path.insert(1, os.path.join(_current_dir, *([os.pardir] * 5 + ['mojo/public/tools/bindings/pylib'])))
+sys.path.insert(1, os.path.join(_current_dir, *([os.pardir] * 5 + ['mojo/public/tools/mojom'])))
 
 import mojom.fileutil as fileutil
-import mojom.generate.generator as generator
 from mojom.generate import template_expander
-from mojom.generate import translate
-
-def _UnpickleAST(input_file):
-  try:
-    with open(input_file, "rb") as f:
-      return pickle.load(f)
-  except (IOError, pickle.UnpicklingError) as e:
-    print("%s: Error: %s" % (input_file, str(e)))
-    sys.exit(1)
-
-def _GenerateModule(tree, module_path):
-  imports = {}
-  for parsed_imp in tree.import_list:
-    filename = parsed_imp.import_filename
-    print("imported file " + filename)
-    import_tree = _UnpickleAST(os.path.join(ast_root_dir, os.path.splitext(os.path.basename(parsed_imp.import_filename))[0] + '.p'))
-    imports[filename] = _GenerateModule(import_tree, filename)
-  # Set the module path as relative to the source root.
-  # Normalize to unix-style path here to keep the generators simpler.
-  # module_path = rel_filename.relative_path().replace('\\', '/')
-  path = os.path.basename(module_path)
-  return translate.OrderedModule(tree, path, imports)
+from mojom.generate.module import Module
 
 def parse_args():
   parser = argparse.ArgumentParser(description='Generate Obj-C files from mojo definitions')
-  parser.add_argument('--pickled-ast', nargs=1)
+  parser.add_argument('--mojom-module', nargs=1)
   parser.add_argument('--module-include-path', nargs=1)
   parser.add_argument('--mojom-file', nargs=1)
   parser.add_argument('--output-dir', nargs=1)
@@ -52,14 +25,12 @@ def main():
   global ast_root_dir
 
   args = parse_args()
-  pickled_ast = args.pickled_ast[0]
+  mojom_module = args.mojom_module[0]
   module_include_path = args.module_include_path[0]
-  mojom_file = args.mojom_file[0]
   output_dir = args.output_dir[0]
 
-  ast_root_dir = os.path.dirname(pickled_ast)
+  ast_root_dir = os.path.dirname(mojom_module)
 
-  tree = _UnpickleAST(pickled_ast)
   generator_module = importlib.import_module('mojom_objc_generator')
   bytecode_path = os.path.join(output_dir, "objc_templates_bytecode")
   fileutil.EnsureDirectoryExists(bytecode_path)
@@ -68,7 +39,8 @@ def main():
   generator = generator_module.Generator(None)
   generator.bytecode_path = bytecode_path
   generator.module_include_path = module_include_path
-  generator.module = _GenerateModule(tree, mojom_file)
+  with open(mojom_module, 'rb') as f:
+    generator.module = Module.Load(f)
   generator.GenerateFiles(output_dir)
 
 if __name__ == "__main__":
