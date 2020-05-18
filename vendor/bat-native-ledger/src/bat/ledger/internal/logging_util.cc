c5378abd7560cb78e49e93f1adabfc0eb2379289
NejcZdovc <nejc.zdovc@3zsistemi.si>
2020-05-18 16:22:00 +0200

    Fixes request/response format

--- vendor/bat-native-ledger/src/bat/ledger/internal/logging_util.cc
+++ vendor/bat-native-ledger/src/bat/ledger/internal/logging_util.cc
@@ -19,44 +19,26 @@ std::string UrlRequestToString(
     const std::string& content,
     const std::string& content_type,
     const UrlMethod method) {
-  std::string log = "URL Request:\n";
-
-  log += base::StringPrintf("  URL: %s\n", url.c_str());
-
-  if (!headers.empty()) {
-    log += "  Headers:\n";
-
-    for (const auto& header : headers) {
-      log += base::StringPrintf("    %s\n", header.c_str());
-    }
-  }
-
-  if (!content.empty()) {
-    log += base::StringPrintf("  Content: %s\n", content.c_str());
-  }
-
-  if (!content_type.empty()) {
-    log += base::StringPrintf("  Content Type: %s\n", content_type.c_str());
-  }
+  std::string log = "\n[ REQUEST ]";
+  log += base::StringPrintf("\n> URL: %s", url.c_str());
 
   std::stringstream ss;
   ss << method;
+  log += base::StringPrintf("\n> Method: %s", ss.str().c_str());
 
-  log += base::StringPrintf("  Method: %s", ss.str().c_str());
+  if (!content.empty()) {
+    log += base::StringPrintf("\n> Content: %s", content.c_str());
+  }
+
+  if (!content_type.empty()) {
+    log += base::StringPrintf("\n> Content Type: %s", content_type.c_str());
+  }
+
+  for (const auto& header : headers) {
+    log += base::StringPrintf("\n> Header %s", header.c_str());
+  }
 
   return log;
-}
-
-std::string HeadersToString(
-    const std::map<std::string, std::string>& headers) {
-  std::string formatted_headers;
-  for (auto header = headers.begin(); header != headers.end(); ++header) {
-    formatted_headers += "> headers\n" + header->first + ": " + header->second;
-    if (header != headers.end()) {
-      formatted_headers += "\n";
-    }
-  }
-  return formatted_headers;
 }
 
 std::string UrlResponseToString(
@@ -68,14 +50,23 @@ std::string UrlResponseToString(
       response_status_code >= 200 && response_status_code < 300
           ? "Success" : "Failure";
 
-  const std::string timestamp = std::to_string(std::time(nullptr));
+  std::string formatted_headers;
+  for (const auto& header : headers) {
+    formatted_headers +=
+        "\n> Header " + header.first + ": " + header.second;
+  }
 
-  const std::string formatted_headers = HeadersToString(headers);
-
-  return base::StringPrintf("[ RESPONSE - %s ]\n> time: %s\n> result: %s\n"
-      "> http code: %d\n> response: %s\n%s[ END RESPONSE ]", func,
-          timestamp.c_str(), result.c_str(), response_status_code,
-              response.c_str(), formatted_headers.c_str());
+  return base::StringPrintf(
+      "\n[ RESPONSE - %s ]\n"
+      "> Result: %s\n"
+      "> HTTP Code: %d\n"
+      "> Body: %s"
+      "%s",
+      func,
+      result.c_str(),
+      response_status_code,
+      response.c_str(),
+      formatted_headers.c_str());
 }
 
 }  // namespace ledger
