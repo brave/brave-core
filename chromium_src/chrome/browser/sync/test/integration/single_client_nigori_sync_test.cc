36db396fa995873f4f9db20c63c408316346dad9
Anthony Tseng <darkdh@gmail.com>
2020-05-19 21:18:03 -0700

    More SingleClient Integration test and brave's SignOutPrimaryAccount

---
+++ chromium_src/chrome/browser/sync/test/integration/single_client_nigori_sync_test.cc
@@ -0,0 +1,5 @@
+#define ShouldExposeExperimentalAuthenticationKey DISABLED_ShouldExposeExperimentalAuthenticationKey
+#define SingleClientNigoriWithWebApiTest DISABLED_SingleClientNigoriWithWebApiTest
+#include "../../../../../../../chrome/browser/sync/test/integration/single_client_nigori_sync_test.cc"
+#undef ShouldExposeExperimentalAuthenticationKey
+#undef SingleClientNigoriWithWebApiTest
---
+++ chromium_src/chrome/browser/sync/test/integration/single_client_passwords_sync_test.cc
@@ -0,0 +1,11 @@
+#define StoresDataForNonSyncingPrimaryAccountInAccountDB DISABLED_StoresDataForNonSyncingPrimaryAccountInAccountDB
+#define StoresDataForSecondaryAccountInAccountDB DISABLED_StoresDataForSecondaryAccountInAccountDB
+#define ClearsAccountDBOnSignout DISABLED_ClearsAccountDBOnSignout
+#define SwitchesStoresOnEnablingSync DISABLED_SwitchesStoresOnEnablingSync
+#define SwitchesStoresOnMakingAccountPrimary DISABLED_SwitchesStoresOnMakingAccountPrimary
+#include "../../../../../../../chrome/browser/sync/test/integration/single_client_passwords_sync_test.cc"
+#undef StoresDataForNonSyncingPrimaryAccountInAccountDB
+#undef StoresDataForSecondaryAccountInAccountDB
+#undef ClearsAccountDBOnSignout
+#undef SwitchesStoresOnEnablingSync
+#undef SwitchesStoresOnMakingAccountPrimary
