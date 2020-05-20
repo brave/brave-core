433c9106a3452877ccef71ab8e6dfb22459d7c9b
bridiver <github@brianjohnson.cc>
2020-05-20 13:55:06 -0700

    simplify patches for max expiration date

--- chromium_src/net/cookies/canonical_cookie.cc
+++ chromium_src/net/cookies/canonical_cookie.cc
@@ -3,37 +3,27 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "net/cookies/canonical_cookie.h"
-#include "net/cookies/parsed_cookie.h"
+#include <algorithm>
+
+#include "base/time/time.h"
 
 namespace {
 
-const base::TimeDelta kMaxClientSideExpiration = base::TimeDelta::FromDays(7);
-const base::TimeDelta kMaxServerSideExpiration =
+// Javascript max expiration is handled by blink::CookieStore
+constexpr base::TimeDelta kMaxCookieExpiration =
     base::TimeDelta::FromDays(30*6);  // 6 months
 
-base::Time BraveCanonExpiration(const net::ParsedCookie& pc,
-                                const base::Time& current,
-                                const base::Time& server_time,
-                                const bool is_from_http) {
-  const base::Time max_expiration =
-      current +
-      (is_from_http ? kMaxServerSideExpiration : kMaxClientSideExpiration);
+base::Time BraveCanonExpiration(const base::Time& cookie_expires,
+                                const base::Time& creation_time) {
+  const base::Time max_expiration = creation_time + kMaxCookieExpiration;
 
-  return std::min(
-      net::CanonicalCookie::CanonExpiration(pc, current, server_time),
-      max_expiration);
+  return std::min(cookie_expires, max_expiration);
 }
 
 }  // namespace
 
-#define BRAVE_CANONICALCOOKIE_CREATE_EXTRA_PARAMS \
-  const bool is_from_http,
-
-#define BRAVE_CANONICALCOOKIE_CREATE_BRAVECANONEXPIRATION \
-  Time cookie_expires = BraveCanonExpiration(             \
-      parsed_cookie, creation_time, cookie_server_time, is_from_http);
+#define BRAVE_CREATE \
+  cookie_expires = BraveCanonExpiration(cookie_expires, creation_time);
 
 #include "../../../../net/cookies/canonical_cookie.cc"
-#undef BRAVE_CANONICALCOOKIE_CREATE_BRAVECANONEXPIRATION
-#undef BRAVE_CANONICALCOOKIE_CREATE_EXTRA_PARAMS
+#undef BRAVE_CREATE
