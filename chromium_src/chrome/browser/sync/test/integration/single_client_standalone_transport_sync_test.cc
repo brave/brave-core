a3bf6863b5251c0a31940f9d4426d9792da8dce5
Anthony Tseng <darkdh@gmail.com>
2020-05-21 16:23:38 -0700

    Auth integration test and other single client integration tests

---
+++ chromium_src/chrome/browser/sync/test/integration/single_client_standalone_transport_sync_test.cc
@@ -0,0 +1,3 @@
+#define HandlesResetFromDashboardWhenSyncActive DISABLED_HandlesResetFromDashboardWhenSyncActive
+#include "../../../../../../../chrome/browser/sync/test/integration/single_client_standalone_transport_sync_test.cc"
+#undef HandlesResetFromDashboardWhenSyncActive
