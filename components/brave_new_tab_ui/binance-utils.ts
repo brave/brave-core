4bb0042c8cf14bcc5de3142132d01785aaeff998
Pete Miller <miller.pete@gmail.com>
2020-05-22 15:06:22 -0700

    NTP WebUI: Split qr-image dependency into separate JS module
    Don't bundle in to main NTP JS

--- components/brave_new_tab_ui/binance-utils.ts
+++ components/brave_new_tab_ui/binance-utils.ts
@@ -1,10 +1,10 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
-const qr = require('qr-image')
 
-export const generateQRData = (url: string, asset: string, qrAction: any) => {
-  const image = qr.image(url)
+export async function generateQRData (url: string, asset: string, qrAction: any) {
+  const qr = await import('qr-image')
+  const image: any = qr.image(url)
   try {
     let chunks: Array<Uint8Array> = []
     image
