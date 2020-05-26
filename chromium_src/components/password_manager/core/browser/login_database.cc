ba2de6f60eeefdb72ddc0b1b248ea6e8cf87aebb
Simon Hong <shong@brave.com>
2020-05-26 11:14:55 +0900

    Fixed Saved Passwords section in settings is empty
    Some users reported that Saved Passwords sections is empty but passwords are autofilled
    to website's password form on linux and macOS.
    
    I think this can happen if user insert/update new password entries after login db encryption
    is corrupted.
    However, currently user can't anything even if current login db has decryptable entries.
    
    The reason is LoginDatabase::StatementToForms() gives empty password entry when any entry in
    login db has decryption failure on linux and macOS.
    
    To fix this, ENCRYPTION_RESULT_ITEM_FAILURE is use for individual decryption failure
    instead of ENCRYPTION_RESULT_SERVICE_FAILURE.
    
    If ENCRYPTION_RESULT_SERVICE_FAILURE is returned, LoginDatabase::StatementToForms() assumes
    all other entries will have decrypt failure. However, db could have decryptable entries
    even if current entry is failed to decrypt.

---
+++ chromium_src/components/password_manager/core/browser/login_database.cc
@@ -0,0 +1,12 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#define BRAVE_STATEMENT_TO_FORMS \
+    if (result == ENCRYPTION_RESULT_SERVICE_FAILURE) \
+      result = ENCRYPTION_RESULT_ITEM_FAILURE;
+
+#include "../../../../../../components/password_manager/core/browser/login_database.cc"
+
+#undef BRAVE_STATEMENT_TO_FORMS
