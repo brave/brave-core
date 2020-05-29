8bc623e8583580dec046838fe8257d0bd84312d2
mkarolin <max@brave.com>
2020-05-29 15:05:41 -0400

    Crash report url changes.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/c57eb4fda6e5bc5fee3aa77ce0f7ddf63f04eaeb
    
    commit c57eb4fda6e5bc5fee3aa77ce0f7ddf63f04eaeb
    Author: Jeremy Apthorp <jeremya@chromium.org>
    Date:   Wed May 13 19:06:23 2020 +0000
    
        [crash] allow embedder to set crash upload url
    
        Currently //components/crash hard-codes the crash report URL to be a
        Google-specific target. This CL adds a hook which allows embedders to override
        this URL. Without it, this module isn't usable for non-Google embedders.

---
+++ chromium_src/components/crash/core/app/crash_reporter_client.cc
@@ -0,0 +1,10 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#define BRAVE_CRASH_REPORTER_CLIENT_GET_UPLOAD_URL \
+  return "https://cr.brave.com";
+
+#include "../../../../../../components/crash/core/app/crash_reporter_client.cc"
+#undef BRAVE_CRASH_REPORTER_CLIENT_GET_UPLOAD_URL
