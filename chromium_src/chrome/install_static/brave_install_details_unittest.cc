6332ae167c4bdbbdbb8c1a2552c84397b4d8aef6
mkarolin <max@brave.com>
2020-05-29 12:23:06 -0400

    Multi-install has been removed.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/244fcc4dc4e35b9f544c8ea5d5a9341a46cbf36e
    
    commit 244fcc4dc4e35b9f544c8ea5d5a9341a46cbf36e
    Author: Greg Thompson <grt@chromium.org>
    Date:   Thu Apr 9 09:30:16 2020 +0000
    
        Reland: Remove multi-install remnants
    
        The multi-install support was deactivated in 2017 but there were still
        many remnants remaining in the Chromium code base.  These remnants have
        been a source of friction for changes being made in the install and
        update infrastructure.
    
        Some additional house-keeping is included by reviewer request.
    
        TBR=rsesek@chromium.org,jxiang@google.com
        NOTRY=true
    
        Bug: 1067489

--- chromium_src/chrome/install_static/brave_install_details_unittest.cc
+++ chromium_src/chrome/install_static/brave_install_details_unittest.cc
@@ -22,7 +22,6 @@ class FakeInstallDetails : public InstallDetails {
     constants.size = sizeof(constants);
     constants.install_suffix = L"";
     constants.default_channel_name = L"";
-    constants.supported_multi_install = true;
 #if defined(OFFICIAL_BUILD)
       constants.app_guid = L"testguid";
       constants.channel_strategy = ChannelStrategy::FIXED;
