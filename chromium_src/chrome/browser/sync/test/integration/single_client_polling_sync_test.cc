d39385b3bf5fd5340fc749743b023b841cdb9913
Anthony Tseng <darkdh@gmail.com>
2020-06-01 22:06:07 -0700

    Set SessionStartupPref to default value to prevent DCHECK browser->window()->IsVisible() failed in chrome/browser/sessions/session_restore.cc

--- chromium_src/chrome/browser/sync/test/integration/single_client_polling_sync_test.cc
+++ chromium_src/chrome/browser/sync/test/integration/single_client_polling_sync_test.cc
@@ -3,13 +3,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// TODO(darkdh): Find out why browser->window()->IsVisible() is false when
-// running `RestoreTabsToBrowser` and upstream test doesn't even run session
-// restore
-#define ShouldPollWhenIntervalExpiredAcrossRestarts \
-  DISABLED_ShouldPollWhenIntervalExpiredAcrossRestarts
-#define ShouldUsePollIntervalFromPrefs \
-  DISABLED_ShouldUsePollIntervalFromPrefs
+#include "chrome/browser/prefs/session_startup_pref.h"
+
+#define BRAVE_SET_DEFAULT_STARTUP_PREF \
+  SessionStartupPref::SetStartupPref(  \
+      GetProfile(0)->GetPrefs(),       \
+      SessionStartupPref(SessionStartupPref::GetDefaultStartupType()));
+
 #include "../../../../../../../chrome/browser/sync/test/integration/single_client_polling_sync_test.cc"
-#undef ShouldPollWhenIntervalExpiredAcrossRestarts
-#undef ShouldUsePollIntervalFromPrefs
+
+#undef BRAVE_SET_DEFAULT_STARTUP_PREF
