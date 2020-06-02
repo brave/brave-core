0124e7c9b0da86af900f0db7ff4376be6f085509
mkarolin <max@brave.com>
2020-06-02 11:35:16 -0400

    Do not show graphic in Save Password dialog.
    Fixes brave/brave-browser#10008

---
+++ chromium_src/chrome/browser/ui/views/passwords/password_save_update_view.cc
@@ -0,0 +1,8 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#define BRAVE_PASSWORD_SAVE_UPDATE_VIEW_ON_THEME_CHANGED return;
+#include "../../../../../../../chrome/browser/ui/views/passwords/password_save_update_view.cc"
+#undef BRAVE_PASSWORD_SAVE_UPDATE_VIEW_ON_THEME_CHANGED
