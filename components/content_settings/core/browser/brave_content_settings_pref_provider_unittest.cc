66baf009f37679a356ed92c87e0909bbcfefc723
Simon Hong <shong@brave.com>
2020-06-04 08:20:06 +0900

    Fix lint error

--- components/content_settings/core/browser/brave_content_settings_pref_provider_unittest.cc
+++ components/content_settings/core/browser/brave_content_settings_pref_provider_unittest.cc
@@ -92,10 +92,10 @@ class ShieldsCookieSetting : public ShieldsSetting {
 class ShieldsFingerprintingSetting : public ShieldsSetting {
  public:
   explicit ShieldsFingerprintingSetting(BravePrefProvider* provider)
-      : ShieldsSetting(
-            provider,
-            {{GURL(), brave_shields::kFingerprintingV2},
-             {GURL("https://firstParty/*"), brave_shields::kFingerprintingV2}}) {}
+      : ShieldsSetting(provider,
+                       {{GURL(), brave_shields::kFingerprintingV2},
+                       {GURL("https://firstParty/*"),
+                        brave_shields::kFingerprintingV2}}) {}
 };
 
 class ShieldsHTTPSESetting : public ShieldsSetting {
