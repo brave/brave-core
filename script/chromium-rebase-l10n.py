fcd88932ea813d04be333ef871922566545a1b09
serg <serg.zhukovsky@gmail.com>
2020-06-03 11:25:23 -0400

    automates grd files changes for Android extensions

--- script/chromium-rebase-l10n.py
+++ script/chromium-rebase-l10n.py
@@ -91,6 +91,16 @@ def main():
         elem1 = xml_tree.xpath(
             '//part[@file="settings_chromium_strings.grdp"]')[0]
         elem1.set('file', 'settings_brave_strings.grdp')
+        applyIsAndroidOr(xml_tree, '//part[@file="settings_brave_strings.grdp"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_ENTERPRISE_SIGNIN_TITLE"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_ABOUT_BROWSER_SWITCH_DESCRIPTION_UNKNOWN_BROWSER"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_LOGIN_POD_USER_REMOVE_WARNING_SYNC"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_CONTENT_CONTEXT_ACCESSIBILITY_LABELS_BUBBLE_TEXT"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_SYNC_UPGRADE_CLIENT"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_WELCOME_HEADER"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_RELAUNCH_RECOMMENDED_TITLE"]', 2)
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_ENTERPRISE_STARTUP_CLOUD_POLICY_ENROLLMENT_TOOLTIP"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_HATS_BUBBLE_TITLE"]')
 
     if filename == 'extensions_strings':
         for child in xml_tree.getroot():
@@ -157,7 +167,7 @@ def main():
         applyIsAndroidOr(xml_tree, '//include[@name="IDR_BROWSER_SWITCH_APP_JS"]')
         applyIsAndroidOr(xml_tree, '//include[@name="IDR_ABOUT_SYS_HTML"]')
         applyIsAndroidOr(xml_tree, '//include[@name="IDR_PAGE_NOT_AVAILABLE_FOR_GUEST_APP_HTML"]')
-        applyIsAndroidOr(xml_tree, '//include[@name="IDR_PLUGIN_DB_JSON"]')
+        applyIsAndroidOr(xml_tree, '//include[@file="resources\plugin_metadata\plugins_linux.json"]')
         applyIsAndroidOr(xml_tree, '//include[@name="IDR_MANAGEMENT_HTML"]')
         applyIsAndroidOr(xml_tree, '//include[@name="IDR_SYNC_DISABLED_CONFIRMATION_HTML"]')
         applyIsAndroidOr(xml_tree, '//include[@name="IDR_SIGNIN_EMAIL_CONFIRMATION_HTML"]')
@@ -172,6 +182,91 @@ def main():
 
     if filename == 'mojo_bindings_resources':
         applyIsAndroidOr(xml_tree, '//include[@name="IDR_MOJO_MOJO_BINDINGS_JS"]')
+
+    if filename == 'bookmarks_strings':
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_BOOKMARK_GROUP_FROM_FIREFOX"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_SHOW_BOOKMARK_BAR"]')
+
+    if filename == 'generated_resources':
+        applyIsAndroidOr(xml_tree, '//part[@file="settings_strings.grdp"]')
+        applyIsAndroidOr(xml_tree, '//part[@file="welcome_strings.grdp"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_SEARCH_CLEARED"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_CONTENT_CONTEXT_INSPECTELEMENT"]')
+        applyIsAndroidOr(xml_tree, '//message[@desc="In Title Case: The text label of a menu item for opening a new tab"]', 2)
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_DOWNLOAD_MENU_DEEP_SCAN"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_OMNIBOX_PWA_INSTALL_ICON_LABEL"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_CERT_SELECTOR_SUBJECT_COLUMN"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_TASK_MANAGER_MEM_CELL_TEXT"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_UTILITY_PROCESS_PROFILE_IMPORTER_NAME"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_PEPPER_BROKER_MESSAGE"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_ABOUT_SYS_TITLE"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_ABOUT_BROWSER_SWITCH_TITLE"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_TOOLTIP_INTENT_PICKER_ICON"]')
+        moveOnLevelHigher(xml_tree, '//message[@name="IDS_PASSWORD_MANAGER_BUBBLE_BLACKLIST_BUTTON"]')
+        moveOnLevelHigher(xml_tree, '//message[@name="IDS_PASSWORD_MANAGER_TOOLTIP_SAVE"]')
+        moveOnLevelHigher(xml_tree, '//message[@name="IDS_PASSWORD_MANAGER_TOOLTIP_MANAGE"]')
+        moveOnLevelHigher(xml_tree, '//message[@name="IDS_PASSWORD_MANAGER_IMPORT_BUTTON"]')
+        moveOnLevelHigher(xml_tree, '//message[@name="IDS_PASSWORD_MANAGER_IMPORT_DIALOG_TITLE"]')
+        moveOnLevelHigher(xml_tree, '//message[@name="IDS_PASSWORD_MANAGER_EXPORT_DIALOG_TITLE"]')
+        moveOnLevelHigher(xml_tree, '//message[@name="IDS_PASSWORD_MANAGER_USERNAME_LABEL"]')
+        moveOnLevelHigher(xml_tree, '//message[@name="IDS_PASSWORD_MANAGER_PASSWORD_LABEL"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_IMPORT_FROM_FIREFOX"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_TOOLBAR_INFORM_SET_HOME_PAGE"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_OMNIBOX_QRCODE_GENERATOR_ICON_LABEL"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_ACCNAME_INFOBAR_CONTAINER"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_MANAGED_WITH_HYPERLINK"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_APP_DEFAULT_PAGE_NAME"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_SYNC_START_SYNC_BUTTON_LABEL"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_TRANSLATE_BUBBLE_BEFORE_TRANSLATE_TITLE"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_MEDIA_GALLERIES_DIALOG_HEADER"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_BLUETOOTH_DEVICE_CHOOSER_PROMPT_ORIGIN"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_SERIAL_PORT_CHOOSER_PROMPT_ORIGIN"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_HID_CHOOSER_PROMPT_ORIGIN"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_REDIRECT_BLOCKED_MESSAGE"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_DOWNLOAD_OPEN_CONFIRMATION_DIALOG_TITLE"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_RELAUNCH_REQUIRED_CANCEL_BUTTON"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_WEBAUTHN_GENERIC_TITLE"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_HATS_BUBBLE_TEXT"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_ENTERPRISE_EXTENSION_REQUEST_APPROVED_TITLE"]')
+
+    if filename == 'profiles_strings':
+        moveOnLevelHigher(xml_tree, '//message[@name="IDS_SYNC_USER_NAME_IN_USE_BY_ERROR"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_PROFILES_CREATE_TITLE"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_USER_MANAGER_TUTORIAL_SLIDE_OUTRO_USER_NOT_FOUND"]')
+
+    if filename == 'autofill_payments_strings':
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_AUTOFILL_FIELD_LABEL_PHONE"]')
+        moveOnLevelHigher(xml_tree, '//message[@name="IDS_AUTOFILL_SAVE_CARD_BUBBLE_LOCAL_SAVE_ACCEPT"]', 2)
+        moveOnLevelHigher(xml_tree, '//message[@name="IDS_AUTOFILL_SAVE_CARD_BUBBLE_UPLOAD_SAVE_ACCEPT"]', 2)
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_AUTOFILL_GOOGLE_PAY_LOGO_ACCESSIBLE_NAME"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_AUTOFILL_WEBAUTHN_OPT_IN_DIALOG_TITLE"]', 2)
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_AUTOFILL_WEBAUTHN_VERIFY_PENDING_DIALOG_TITLE"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_AUTOFILL_CLOUD_TOKEN_DROPDOWN_OPTION_LABEL"]')
+
+    if filename == 'autofill_strings':
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_AUTOFILL_POPUP_ACCESSIBLE_NODE_DATA"]')
+
+    if filename == 'components_strings':
+        applyIsAndroidOr(xml_tree, '//part[@file="management_strings.grdp"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_PRINT"]', 2)
+
+    if filename == 'history_strings':
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_HISTORY_OTHER_SESSIONS_COLLAPSE_SESSION"]')
+
+    if filename == 'new_or_sad_tab_strings':
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_SAD_TAB_ERROR_CODE"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_NEW_TAB_OTR_HEADING"]')
+
+    if filename == 'page_info_strings':
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_PAGE_INFO_CERTIFICATE"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_PAGE_INFO_COOKIES"]')
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_PAGE_INFO_INFOBAR_TEXT"]')
+
+    if filename == 'payments_strings':
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_PAYMENT_REQUEST_PAYMENT_METHODS_PREVIEW"]')
+
+    if filename == 'sync_ui_strings':
+        applyIsAndroidOr(xml_tree, '//message[@name="IDS_SYNC_BASIC_ENCRYPTION_DATA"]')
 
     grit_root = xml_tree.xpath(
         '//grit' if extension == '.grd' else '//grit-part')[0]
@@ -199,11 +294,21 @@ def add_extensions_output(ifElem, lang):
     outputElem.set('lang', lang)
     ifElem.append(outputElem)
 
-def applyIsAndroidOr(xml_tree, toFind):
-    elem = xml_tree.xpath(toFind)[0]
-    parent = elem.find('..')
-    parent.set('expr', 'is_android or ' + parent.get('expr'))
+def applyIsAndroidOr(xml_tree, toFind, parentLevel=1):
+    elems = xml_tree.xpath(toFind)
+    for item in elems:
+        parent = item.find('..')
+        if (parentLevel == 2):
+            parent = parent.find('..')
+        parent.set('expr', 'is_android or ' + parent.get('expr'))
 
+def moveOnLevelHigher(xml_tree, toMove, parentLevel=1):
+    elem = xml_tree.xpath(toMove)[0]
+    parent = elem.find('..')
+    parentOfParent = parent.find('..')
+    if (parentLevel == 2):
+        parentOfParent = parentOfParent.find('..')
+    parentOfParent.append(elem)
 
 if __name__ == '__main__':
     sys.exit(main())
