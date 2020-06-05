1774c88eebea8f50426b7fa78cf7d95ee8a3506a
mkarolin <max@brave.com>
2020-06-05 15:40:10 -0400

    ContentSettings force_allowed_origins was deprecated.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/852366fe1b7905c397c153a85a8a220d5a67cf12
    
    commit 852366fe1b7905c397c153a85a8a220d5a67cf12
    Author: Jiewei Qian <qjw@chromium.org>
    Date:   Wed May 6 02:40:01 2020 +0000
    
        system-web-app: migrate Terminal permission to WebUIAllowlist
    
        Also deprecates ContentSettings force_allowed_origins.
    
        Bug: 1054261

---
+++ chromium_src/components/content_settings/core/browser/content_settings_registry.h
@@ -0,0 +1,32 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_CONTENT_SETTINGS_CORE_BROWSER_CONTENT_SETTINGS_REGISTRY_H_
+#define BRAVE_CHROMIUM_SRC_COMPONENTS_CONTENT_SETTINGS_CORE_BROWSER_CONTENT_SETTINGS_REGISTRY_H_
+
+// Pull in all includes from content_settings_registry.h because Init is too
+// common a name.
+#include <map>
+#include <memory>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "base/lazy_instance.h"
+#include "base/macros.h"
+#include "components/content_settings/core/browser/content_settings_info.h"
+#include "components/content_settings/core/browser/content_settings_utils.h"
+#include "components/content_settings/core/browser/website_settings_info.h"
+#include "components/content_settings/core/common/content_settings.h"
+#include "components/content_settings/core/common/content_settings_types.h"
+
+#define Init \
+  Init();    \
+  void BraveInit
+
+#include "../../../../../../components/content_settings/core/browser/content_settings_registry.h"
+#undef Init
+
+#endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_CONTENT_SETTINGS_CORE_BROWSER_CONTENT_SETTINGS_REGISTRY_H_
