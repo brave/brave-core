ff9b12e3f31ed9caa910e0a6588485c10e88aef2
Albert Wang <amwang217@gmail.com>
2020-06-09 13:50:43 -0700

    Disable wearable notifications. Ads won't look right on such a small screen, as our guidelines focus on mobile and desktop, not wearables.
    When we choose to implement or test this in the future,
    https://developer.android.com/training/wearables/apps/creating#emulator is a great guide
    for emulating Wear OS on the computer with a physical phone.
    
    Closes #9397

--- browser/brave_ads/android/java/org/chromium/chrome/browser/notifications/BraveAdsNotificationBuilder.java
+++ browser/brave_ads/android/java/org/chromium/chrome/browser/notifications/BraveAdsNotificationBuilder.java
@@ -156,6 +156,7 @@ public class BraveAdsNotificationBuilder extends NotificationBuilderBase {
         builder.setOnlyAlertOnce(!mRenotify);
         builder.setContent(compactView);
         builder.setAutoCancel(true);
+        builder.setLocalOnly(true); // Disables showing on other devices, e.g. Android Wear
 
         // Some things are duplicated in the builder to ensure the notification shows correctly on
         // Wear devices and custom lock screens.
