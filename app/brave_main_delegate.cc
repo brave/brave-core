88fc6aacb5213d8194d08fa3a56836f14aa31741
mkarolin <max@brave.com>
2020-06-09 23:21:50 -0400

    OriginTrialPolicy moved //components.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/3d4e89b95b4a77ea8f11256eb1d0e5aa1b213894
    
    commit 3d4e89b95b4a77ea8f11256eb1d0e5aa1b213894
    Author: Clark DuVall <cduvall@chromium.org>
    Date:   Fri Apr 3 23:07:32 2020 +0000
    
        Move OriginTrialPolicy implementation into //components
    
        This can now be shared between //chrome, //headless, and //weblayer.
    
        Bug: 1050722, 1049317

--- app/brave_main_delegate.cc
+++ app/brave_main_delegate.cc
@@ -28,6 +28,7 @@
 #include "chrome/common/chrome_switches.h"
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/autofill/core/common/autofill_payments_features.h"
+#include "components/embedder_support/switches.h"
 #include "components/feed/feed_feature_list.h"
 #include "components/offline_pages/core/offline_page_feature.h"
 #include "components/password_manager/core/common/password_manager_features.h"
@@ -165,8 +166,8 @@ bool BraveMainDelegate::BasicStartupComplete(int* exit_code) {
       "enforce");
 
   if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kOriginTrialPublicKey)) {
-    command_line.AppendSwitchASCII(switches::kOriginTrialPublicKey,
+          embedder_support::kOriginTrialPublicKey)) {
+    command_line.AppendSwitchASCII(embedder_support::kOriginTrialPublicKey,
                                    kBraveOriginTrialsPublicKey);
   }
 
