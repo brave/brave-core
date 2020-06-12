e6838d68375f732cdf083b41a3d98f615a53a194
mkarolin <max@brave.com>
2020-06-12 12:02:31 -0400

    Do not allow sending hints headers by default.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/8627cac2fdbba1d50116d6ca1bbc01ca790f98c5
    
    commit 8627cac2fdbba1d50116d6ca1bbc01ca790f98c5
    Author: Yoav Weiss <yoavweiss@chromium.org>
    Date:   Fri May 15 00:43:37 2020 +0000
    
        [ua-ch] Apply FP to UA and UA-Mobile
    
        Currently FeaturePolicy isn't applied to hints that are sent by default:
        Sec-CH-UA and Sec-CH-UA-Mobile.
        That's wrong, as it doesn't allow sites to e.g. block those headers on
        their own origin or on third-party origins.
        This CL fixes that.
    
        Bug: 1082252

---
+++ chromium_src/third_party/blink/common/client_hints/client_hints.cc
@@ -0,0 +1,9 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#define BRAVE_CLIENT_HINTS_IS_CLIENT_HINT_SENT_BY_DEFAULT return false;
+
+#include "../../../../../../third_party/blink/common/client_hints/client_hints.cc"
+#undef BRAVE_CLIENT_HINTS_IS_CLIENT_HINT_SENT_BY_DEFAULT
