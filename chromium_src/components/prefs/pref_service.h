e8873870d35e527836634a92c008df7962e5331f
samartnik <artem@brave.com>
2020-06-12 15:10:56 -0400

    [Android] Additional changes for InterceptNavigationDelegateImpl
    Fixes DCHECK error on browser initialization
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/204335845d2b7292bb99cb880203b7d132ddfa72
    
    commit 204335845d2b7292bb99cb880203b7d132ddfa72
    Author: Colin Blundell <blundell@chromium.org>
    Date:   Wed Apr 15 08:52:29 2020 +0000
    
        [Android] Componentize InterceptNavigationDelegateImpl
    
        After preceding CLs this class has no remaining //chrome dependencies
        other than resources, which this CL componentizes along with the class
        itself. As this implementation of InterceptNavigationDelegate is
        specific to the overall intent launching logic in
        //components/external_intents, this CL componentizes it into that
        component. Componentizing this class will enable it to be reused in
        WebLayer.
    
        Bug: 1031465

--- chromium_src/components/prefs/pref_service.h
+++ chromium_src/components/prefs/pref_service.h
@@ -7,32 +7,33 @@
 #define BRAVE_CHROMIUM_SRC_COMPONENTS_PREFS_PREF_SERVICE_H_
 
 #include "../../../../components/prefs/pref_service.h"
+#include "base/callback.h"
 #include "base/memory/singleton.h"
 
 namespace prefs {
+
 // Allows access to active user's profile PrefService in components
 class COMPONENTS_PREFS_EXPORT BravePrefService {
  public:
-  static BravePrefService* GetInstance() {
-    return base::Singleton<BravePrefService>::get();
-  }
+  typedef base::Callback<PrefService*()> GetPrefsCallback;
 
-  void RegisterPrefService(PrefService* pref_service) {
-    pref_service_ = pref_service;
-  }
+  static BravePrefService* GetInstance();
 
-  PrefService* GetPrefs() { return pref_service_; }
+  void RegisterGetPrefsCallback(const GetPrefsCallback& cb);
+
+  PrefService* GetPrefs();
 
  private:
   friend struct base::DefaultSingletonTraits<BravePrefService>;
 
-  BravePrefService() {}
-  ~BravePrefService() {}
+  BravePrefService();
+  ~BravePrefService();
 
-  PrefService* pref_service_;
+  GetPrefsCallback get_prefs_callback_;
 
   DISALLOW_COPY_AND_ASSIGN(BravePrefService);
 };
+
 }  // namespace prefs
 
 #endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_PREFS_PREF_SERVICE_H_
