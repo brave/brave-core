courage-browser/courage-core

Refactor Brave Ads unit tests, mocks, utils and structure

vendor/bat-native-ads/src/bat/ads/internal/ads_impl.h
@@ -21,13 +21,13 @@
 #include "bat/ads/ad_notification_info.h"
 #include "bat/ads/internal/ads_serve.h"
 #include "bat/ads/internal/bundle.h"
+#include "bat/ads/internal/classification/page_classifier/page_classifier.h"
 #include "bat/ads/internal/client.h"
 #include "bat/ads/internal/ad_conversions.h"
 #include "bat/ads/internal/ad_notification_result_type.h"
 #include "bat/ads/internal/ad_notifications.h"
 #include "bat/ads/internal/timer.h"
-#include "bat/ads/internal/page_classifier/page_classifier.h"
-#include "bat/ads/internal/purchase_intent/purchase_intent_classifier.h"
+#include "bat/ads/internal/classification/purchase_intent/purchase_intent_classifier.h"
 
 namespace ads {
 
@@ -50,9 +50,9 @@ class AdsImpl : public Ads {
 
   AdsClient* get_ads_client() const;
   Client* get_client() const;
-
   AdNotifications* get_ad_notifications() const;
-  PageClassifier* get_page_classifier() const;
+  AdConversions* get_ad_conversions() const;
+  classification::PageClassifier* get_page_classifier() const;
 
   InitializeCallback initialize_callback_;
   void Initialize(
@@ -182,12 +182,8 @@ class AdsImpl : public Ads {
       const Result result,
       const std::vector<std::string>& categories,
       const CreativeAdNotificationList& ads);
-  CategoryList GetCategoriesToServeAd();
-  void ServeAdNotification(
-      const CreativeAdNotificationList& ads);
-  void OnServeAdNotification(
-      const Result result,
-      const AdConversionList& ad_conversions,
+  classification::CategoryList GetCategoriesToServeAd();
+  void ServeAdNotificationWithPacing(
       const CreativeAdNotificationList& ads);
   void SuccessfullyServedAd();
   void FailedToServeAdNotification(
@@ -195,11 +191,6 @@ class AdsImpl : public Ads {
 
   CreativeAdNotificationList GetEligibleAds(
       const CreativeAdNotificationList& ads);
-  CreativeAdNotificationList GetEligibleAdsForConversions(
-      const CreativeAdNotificationList& ads,
-      const AdConversionList& ad_conversions);
-  CreativeAdNotificationList GetEligibleAdsForPriorities(
-      const CreativeAdNotificationList& ads) const;
   CreativeAdNotificationList GetUnseenAdsAndRoundRobinIfNeeded(
       const CreativeAdNotificationList& ads) const;
   CreativeAdNotificationList GetUnseenAds(
@@ -250,9 +241,8 @@ class AdsImpl : public Ads {
   std::unique_ptr<Client> client_;
   std::unique_ptr<Bundle> bundle_;
   std::unique_ptr<AdsServe> ads_serve_;
-  std::unique_ptr<FrequencyCapping> frequency_capping_;
   std::unique_ptr<AdConversions> ad_conversions_;
-  std::unique_ptr<PageClassifier> page_classifier_;
+  std::unique_ptr<classification::PageClassifier> page_classifier_;
   std::unique_ptr<PurchaseIntentClassifier> purchase_intent_classifier_;
 
  private:

vendor/bat-native-ads/src/bat/ads/internal/classification/classification_util.h
@@ -1,0 +1,21 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BAT_ADS_INTERNAL_CLASSIFICATION_CLASSIFICATION_UTIL_H_
+#define BAT_ADS_INTERNAL_CLASSIFICATION_CLASSIFICATION_UTIL_H_
+
+#include <string>
+#include <vector>
+
+namespace ads {
+namespace classification {
+
+std::vector<std::string> Split(
+    const std::string& classification);
+
+}  // namespace classification
+}  // namespace ads
+
+#endif  // BAT_ADS_INTERNAL_CLASSIFICATION_CLASSIFICATION_UTIL_H_

vendor/bat-native-ads/src/bat/ads/internal/classification/page_classifier/page_classifier.cc
@@ -1,0 +1,236 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "bat/ads/internal/classification/page_classifier/page_classifier.h"
+
+#include <algorithm>
+
+#include "base/logging.h"
+#include "brave/components/l10n/browser/locale_helper.h"
+#include "brave/components/l10n/common/locale_util.h"
+#include "bat/ads/internal/ads_impl.h"
+#include "bat/ads/internal/classification/classification_util.h"
+#include "bat/ads/internal/classification/page_classifier/page_classifier_util.h"  // NOLINT
+
+namespace ads {
+namespace classification {
+
+PageClassifier::PageClassifier(
+    const AdsImpl* const ads)
+    : ads_(ads) {
+  DCHECK(ads_);
+}
+
+PageClassifier::~PageClassifier() = default;
+
+bool PageClassifier::IsInitialized() {
+  return user_model_ && user_model_->IsInitialized();
+}
+
+bool PageClassifier::Initialize(
+    const std::string& json) {
+  user_model_.reset(usermodel::UserModel::CreateInstance());
+  return user_model_->InitializePageClassifier(json);
+}
+
+bool PageClassifier::ShouldClassifyPages() const {
+  const std::string locale =
+      brave_l10n::LocaleHelper::GetInstance()->GetLocale();
+
+  return ShouldClassifyPagesForLocale(locale);
+}
+
+std::string PageClassifier::ClassifyPage(
+    const std::string& url,
+    const std::string& content) {
+  DCHECK(!url.empty());
+  DCHECK(user_model_);
+
+  const std::string stripped_content =
+      StripHtmlTagsAndNonAlphaCharacters(content);
+
+  const PageProbabilitiesMap page_probabilities =
+      user_model_->ClassifyPage(stripped_content);
+
+  const std::string page_classification =
+      GetPageClassification(page_probabilities);
+
+  if (!page_classification.empty()) {
+    ads_->get_client()->AppendPageProbabilitiesToHistory(page_probabilities);
+    CachePageProbabilities(url, page_probabilities);
+  }
+
+  return page_classification;
+}
+
+CategoryList PageClassifier::GetWinningCategories() const {
+  CategoryList winning_categories;
+
+  if (!ShouldClassifyPages()) {
+    return winning_categories;
+  }
+
+  const PageProbabilitiesList page_probabilities =
+      ads_->get_client()->GetPageProbabilitiesHistory();
+  if (page_probabilities.empty()) {
+    return winning_categories;
+  }
+
+  const CategoryProbabilitiesMap category_probabilities =
+      GetCategoryProbabilities(page_probabilities);
+
+  const CategoryProbabilitiesList winning_category_probabilities =
+      GetWinningCategoryProbabilities(category_probabilities,
+          kTopWinningCategoryCountForServingAds);
+
+  winning_categories = ToCategoryList(winning_category_probabilities);
+
+  return winning_categories;
+}
+
+const PageProbabilitiesCacheMap&
+PageClassifier::get_page_probabilities_cache() const {
+  return page_probabilities_cache_;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+bool PageClassifier::ShouldClassifyPagesForLocale(
+    const std::string& locale) const {
+  const std::string language_code = brave_l10n::GetLanguageCode(locale);
+
+  const std::vector<std::string> user_model_languages =
+      ads_->get_ads_client()->GetUserModelLanguages();
+
+  const auto iter = std::find(user_model_languages.begin(),
+      user_model_languages.end(), language_code);
+
+  if (iter == user_model_languages.end()) {
+    return false;
+  }
+
+  return true;
+}
+
+std::string PageClassifier::GetPageClassification(
+    const PageProbabilitiesMap& page_probabilities) const {
+  if (page_probabilities.empty()) {
+    return "";
+  }
+
+  const auto iter = std::max_element(page_probabilities.begin(),
+      page_probabilities.end(), [](const CategoryProbabilityPair& a,
+          const CategoryProbabilityPair& b) -> bool {
+    return a.second < b.second;
+  });
+
+  return iter->first;
+}
+
+CategoryProbabilitiesMap PageClassifier::GetCategoryProbabilities(
+    const PageProbabilitiesList& page_probabilities) const {
+  CategoryProbabilitiesMap category_probabilities;
+
+  for (const auto& probabilities : page_probabilities) {
+    for (const auto& probability : probabilities) {
+      const std::string category = probability.first;
+      if (ShouldFilterCategory(category)) {
+        continue;
+      }
+
+      const double page_score = probability.second;
+
+      const auto iter = category_probabilities.find(category);
+      if (iter == category_probabilities.end()) {
+        CategoryProbabilityPair category_probability = {category, page_score};
+        category_probabilities.insert(category_probability);
+      } else {
+        iter->second += page_score;
+      }
+    }
+  }
+
+  return category_probabilities;
+}
+
+bool PageClassifier::ShouldFilterCategory(
+    const std::string& category) const {
+  // If passed in category has a subcategory and the current filter does not,
+  // check if it's a child of the filter. Conversely, if the passed in category
+  // has no subcategory but the current filter does, it can't be a match at all
+  // so move on to the next filter. Otherwise, perform an exact match to
+  // determine whether or not to filter the category
+
+  const std::vector<std::string> category_classifications = Split(category);
+
+  const FilteredCategoriesList filtered_categories =
+      ads_->get_client()->get_filtered_categories();
+
+  for (const auto& filtered_category : filtered_categories) {
+    const std::vector<std::string> filtered_category_classifications =
+        Split(filtered_category.name);
+
+    if (category_classifications.size() > 1 &&
+        filtered_category_classifications.size() == 1) {
+      if (category_classifications.front() ==
+          filtered_category_classifications.front()) {
+        return true;
+      }
+    } else if (category_classifications.size() == 1 &&
+        filtered_category_classifications.size() > 1) {
+      continue;
+    } else if (filtered_category.name == category) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+CategoryProbabilitiesList PageClassifier::GetWinningCategoryProbabilities(
+    const CategoryProbabilitiesMap& category_probabilities,
+    const int count) const {
+  CategoryProbabilitiesList winning_category_probabilities(count);
+
+  std::partial_sort_copy(category_probabilities.begin(),
+      category_probabilities.end(), winning_category_probabilities.begin(),
+          winning_category_probabilities.end(), [](
+              const CategoryProbabilityPair& lhs,
+                  const CategoryProbabilityPair& rhs) {
+    return lhs.second > rhs.second;
+  });
+
+  return winning_category_probabilities;
+}
+
+void PageClassifier::CachePageProbabilities(
+    const std::string& url,
+    const PageProbabilitiesMap& page_probabilities) {
+  if (page_probabilities.empty()) {
+    return;
+  }
+
+  const auto iter = page_probabilities_cache_.find(url);
+  if (iter == page_probabilities_cache_.end()) {
+    page_probabilities_cache_.insert({url, page_probabilities});
+  } else {
+    iter->second = page_probabilities;
+  }
+}
+
+CategoryList PageClassifier::ToCategoryList(
+    const CategoryProbabilitiesList category_probabilities) const {
+  CategoryList categories;
+
+  for (const auto& category_probability : category_probabilities) {
+    const std::string category = category_probability.first;
+    categories.push_back(category);
+  }
+
+  return categories;
+}
+
+}  // namespace classification
+}  // namespace ads

brave/brave-core

vendor/bat-native-ads/src/bat/ads/internal/ads_impl.h
@@ -21,13 +21,13 @@
 #include "bat/ads/ad_notification_info.h"
 #include "bat/ads/internal/ads_serve.h"
 #include "bat/ads/internal/bundle.h"
+#include "bat/ads/internal/classification/page_classifier/page_classifier.h"
 #include "bat/ads/internal/client.h"
 #include "bat/ads/internal/ad_conversions.h"
 #include "bat/ads/internal/ad_notification_result_type.h"
 #include "bat/ads/internal/ad_notifications.h"
 #include "bat/ads/internal/timer.h"
-#include "bat/ads/internal/page_classifier/page_classifier.h"
-#include "bat/ads/internal/purchase_intent/purchase_intent_classifier.h"
+#include "bat/ads/internal/classification/purchase_intent_classifier/purchase_intent_classifier.h"
 
 namespace ads {
 
@@ -37,7 +37,6 @@ class AdsServe;
 class SubdivisionTargeting;
 class AdNotifications;
 class AdConversions;
-class FrequencyCapping;
 class ExclusionRule;
 class PermissionRule;
 class PurchaseIntentClassifier;
@@ -51,9 +50,10 @@ class AdsImpl : public Ads {
 
   AdsClient* get_ads_client() const;
   Client* get_client() const;
-
   AdNotifications* get_ad_notifications() const;
-  PageClassifier* get_page_classifier() const;
+  SubdivisionTargeting* get_subdivision_targeting() const;
+  AdConversions* get_ad_conversions() const;
+  classification::PageClassifier* get_page_classifier() const;
 
   InitializeCallback initialize_callback_;
   void Initialize(
@@ -185,12 +185,8 @@ class AdsImpl : public Ads {
       const Result result,
       const std::vector<std::string>& categories,
       const CreativeAdNotificationList& ads);
-  CategoryList GetCategoriesToServeAd();
-  void ServeAdNotification(
-      const CreativeAdNotificationList& ads);
-  void OnServeAdNotification(
-      const Result result,
-      const AdConversionList& ad_conversions,
+  classification::CategoryList GetCategoriesToServeAd();
+  void ServeAdNotificationWithPacing(
       const CreativeAdNotificationList& ads);
   void SuccessfullyServedAd();
   void FailedToServeAdNotification(
@@ -198,11 +194,6 @@ class AdsImpl : public Ads {
 
   CreativeAdNotificationList GetEligibleAds(
       const CreativeAdNotificationList& ads);
-  CreativeAdNotificationList GetEligibleAdsForConversions(
-      const CreativeAdNotificationList& ads,
-      const AdConversionList& ad_conversions);
-  CreativeAdNotificationList GetEligibleAdsForPriorities(
-      const CreativeAdNotificationList& ads) const;
   CreativeAdNotificationList GetUnseenAdsAndRoundRobinIfNeeded(
       const CreativeAdNotificationList& ads) const;
   CreativeAdNotificationList GetUnseenAds(
@@ -254,9 +245,8 @@ class AdsImpl : public Ads {
   std::unique_ptr<Bundle> bundle_;
   std::unique_ptr<AdsServe> ads_serve_;
   std::unique_ptr<SubdivisionTargeting> subdivision_targeting_;
-  std::unique_ptr<FrequencyCapping> frequency_capping_;
   std::unique_ptr<AdConversions> ad_conversions_;
-  std::unique_ptr<PageClassifier> page_classifier_;
+  std::unique_ptr<classification::PageClassifier> page_classifier_;
   std::unique_ptr<PurchaseIntentClassifier> purchase_intent_classifier_;
 
  private:

vendor/bat-native-ads/src/bat/ads/internal/classification/classification_util.h
@@ -1,0 +1,21 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BAT_ADS_INTERNAL_CLASSIFICATION_CLASSIFICATION_UTIL_H_
+#define BAT_ADS_INTERNAL_CLASSIFICATION_CLASSIFICATION_UTIL_H_
+
+#include <string>
+#include <vector>
+
+namespace ads {
+namespace classification {
+
+std::vector<std::string> SplitCategory(
+    const std::string& category);
+
+}  // namespace classification
+}  // namespace ads
+
+#endif  // BAT_ADS_INTERNAL_CLASSIFICATION_CLASSIFICATION_UTIL_H_

vendor/bat-native-ads/src/bat/ads/internal/classification/page_classifier/page_classifier.cc
@@ -1,0 +1,237 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "bat/ads/internal/classification/page_classifier/page_classifier.h"
+
+#include <algorithm>
+
+#include "base/logging.h"
+#include "brave/components/l10n/browser/locale_helper.h"
+#include "brave/components/l10n/common/locale_util.h"
+#include "bat/ads/internal/ads_impl.h"
+#include "bat/ads/internal/classification/classification_util.h"
+#include "bat/ads/internal/classification/page_classifier/page_classifier_util.h"  // NOLINT
+
+namespace ads {
+namespace classification {
+
+PageClassifier::PageClassifier(
+    const AdsImpl* const ads)
+    : ads_(ads) {
+  DCHECK(ads_);
+}
+
+PageClassifier::~PageClassifier() = default;
+
+bool PageClassifier::IsInitialized() {
+  return user_model_ && user_model_->IsInitialized();
+}
+
+bool PageClassifier::Initialize(
+    const std::string& json) {
+  user_model_.reset(usermodel::UserModel::CreateInstance());
+  return user_model_->InitializePageClassifier(json);
+}
+
+bool PageClassifier::ShouldClassifyPages() const {
+  const std::string locale =
+      brave_l10n::LocaleHelper::GetInstance()->GetLocale();
+
+  return ShouldClassifyPagesForLocale(locale);
+}
+
+std::string PageClassifier::ClassifyPage(
+    const std::string& url,
+    const std::string& content) {
+  DCHECK(!url.empty());
+  DCHECK(user_model_);
+
+  const std::string stripped_content =
+      StripHtmlTagsAndNonAlphaCharacters(content);
+
+  const PageProbabilitiesMap page_probabilities =
+      user_model_->ClassifyPage(stripped_content);
+
+  const std::string page_classification =
+      GetPageClassification(page_probabilities);
+
+  if (!page_classification.empty()) {
+    ads_->get_client()->AppendPageProbabilitiesToHistory(page_probabilities);
+    CachePageProbabilities(url, page_probabilities);
+  }
+
+  return page_classification;
+}
+
+CategoryList PageClassifier::GetWinningCategories() const {
+  CategoryList winning_categories;
+
+  if (!ShouldClassifyPages()) {
+    return winning_categories;
+  }
+
+  const PageProbabilitiesList page_probabilities =
+      ads_->get_client()->GetPageProbabilitiesHistory();
+  if (page_probabilities.empty()) {
+    return winning_categories;
+  }
+
+  const CategoryProbabilitiesMap category_probabilities =
+      GetCategoryProbabilities(page_probabilities);
+
+  const CategoryProbabilitiesList winning_category_probabilities =
+      GetWinningCategoryProbabilities(category_probabilities,
+          kTopWinningCategoryCountForServingAds);
+
+  winning_categories = ToCategoryList(winning_category_probabilities);
+
+  return winning_categories;
+}
+
+const PageProbabilitiesCacheMap&
+PageClassifier::get_page_probabilities_cache() const {
+  return page_probabilities_cache_;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+bool PageClassifier::ShouldClassifyPagesForLocale(
+    const std::string& locale) const {
+  const std::string language_code = brave_l10n::GetLanguageCode(locale);
+
+  const std::vector<std::string> user_model_languages =
+      ads_->get_ads_client()->GetUserModelLanguages();
+
+  const auto iter = std::find(user_model_languages.begin(),
+      user_model_languages.end(), language_code);
+
+  if (iter == user_model_languages.end()) {
+    return false;
+  }
+
+  return true;
+}
+
+std::string PageClassifier::GetPageClassification(
+    const PageProbabilitiesMap& page_probabilities) const {
+  if (page_probabilities.empty()) {
+    return "";
+  }
+
+  const auto iter = std::max_element(page_probabilities.begin(),
+      page_probabilities.end(), [](const CategoryProbabilityPair& a,
+          const CategoryProbabilityPair& b) -> bool {
+    return a.second < b.second;
+  });
+
+  return iter->first;
+}
+
+CategoryProbabilitiesMap PageClassifier::GetCategoryProbabilities(
+    const PageProbabilitiesList& page_probabilities) const {
+  CategoryProbabilitiesMap category_probabilities;
+
+  for (const auto& probabilities : page_probabilities) {
+    for (const auto& probability : probabilities) {
+      const std::string category = probability.first;
+      if (ShouldFilterCategory(category)) {
+        continue;
+      }
+
+      const double page_score = probability.second;
+
+      const auto iter = category_probabilities.find(category);
+      if (iter == category_probabilities.end()) {
+        CategoryProbabilityPair category_probability = {category, page_score};
+        category_probabilities.insert(category_probability);
+      } else {
+        iter->second += page_score;
+      }
+    }
+  }
+
+  return category_probabilities;
+}
+
+bool PageClassifier::ShouldFilterCategory(
+    const std::string& category) const {
+  // If passed in category has a subcategory and the current filter does not,
+  // check if it's a child of the filter. Conversely, if the passed in category
+  // has no subcategory but the current filter does, it can't be a match at all
+  // so move on to the next filter. Otherwise, perform an exact match to
+  // determine whether or not to filter the category
+
+  const std::vector<std::string> category_classifications =
+      SplitCategory(category);
+
+  const FilteredCategoriesList filtered_categories =
+      ads_->get_client()->get_filtered_categories();
+
+  for (const auto& filtered_category : filtered_categories) {
+    const std::vector<std::string> filtered_category_classifications =
+        SplitCategory(filtered_category.name);
+
+    if (category_classifications.size() > 1 &&
+        filtered_category_classifications.size() == 1) {
+      if (category_classifications.front() ==
+          filtered_category_classifications.front()) {
+        return true;
+      }
+    } else if (category_classifications.size() == 1 &&
+        filtered_category_classifications.size() > 1) {
+      continue;
+    } else if (filtered_category.name == category) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+CategoryProbabilitiesList PageClassifier::GetWinningCategoryProbabilities(
+    const CategoryProbabilitiesMap& category_probabilities,
+    const int count) const {
+  CategoryProbabilitiesList winning_category_probabilities(count);
+
+  std::partial_sort_copy(category_probabilities.begin(),
+      category_probabilities.end(), winning_category_probabilities.begin(),
+          winning_category_probabilities.end(), [](
+              const CategoryProbabilityPair& lhs,
+                  const CategoryProbabilityPair& rhs) {
+    return lhs.second > rhs.second;
+  });
+
+  return winning_category_probabilities;
+}
+
+void PageClassifier::CachePageProbabilities(
+    const std::string& url,
+    const PageProbabilitiesMap& page_probabilities) {
+  if (page_probabilities.empty()) {
+    return;
+  }
+
+  const auto iter = page_probabilities_cache_.find(url);
+  if (iter == page_probabilities_cache_.end()) {
+    page_probabilities_cache_.insert({url, page_probabilities});
+  } else {
+    iter->second = page_probabilities;
+  }
+}
+
+CategoryList PageClassifier::ToCategoryList(
+    const CategoryProbabilitiesList category_probabilities) const {
+  CategoryList categories;
+
+  for (const auto& category_probability : category_probabilities) {
+    const std::string category = category_probability.first;
+    categories.push_back(category);
+  }
+
+  return categories;
+}
+
+}  // namespace classification
+}  // namespace ads