91a45539f117ed4b40fbb73026b636ec158c267b
Michał Buczek <michal.buczek@protonmail.com>
2020-06-16 06:29:09 +0200

    Fix brave/brave-ios#2630: Update adblock rust lib to 0.2.10 (brave/brave-ios#2631)
    Updated via adblock-rust-ffi based off this commit:
    da31bb0a986a42419f449833b7ae111864275d17

--- Client/WebFilters/ShieldStats/Adblock/Resources/ablock_rust_lib.h
+++ Client/WebFilters/ShieldStats/Adblock/Resources/ablock_rust_lib.h
@@ -9,15 +9,16 @@
 typedef struct C_Engine C_Engine;
 
 typedef struct {
-    const char *uuid;
-    const char *url;
-    const char *title;
-    const char *lang;
-    const char *lang2;
-    const char *lang3;
-    const char *support_url;
-    const char *component_id;
-    const char *base64_public_key;
+  const char *uuid;
+  const char *url;
+  const char *title;
+  const char *lang;
+  const char *lang2;
+  const char *lang3;
+  const char *support_url;
+  const char *component_id;
+  const char *base64_public_key;
+  const char *desc;
 } C_FList;
 
 /**
@@ -36,7 +37,7 @@ void engine_add_resource(C_Engine *engine,
                          const char *data);
 
 /**
- * Adds a list of resources in uBlock resources format
+ * Adds a list of `Resource`s from JSON format
  */
 void engine_add_resources(C_Engine *engine, const char *resources);
 
@@ -61,6 +62,18 @@ bool engine_deserialize(C_Engine *engine, const char *data, size_t data_size);
 void engine_destroy(C_Engine *engine);
 
 /**
+ * Returns a stylesheet containing all generic cosmetic rules that begin with any of the provided class and id selectors
+ * The leading '.' or '#' character should not be provided
+ */
+char *engine_hidden_class_id_selectors(C_Engine *engine,
+                                       const char *const *classes,
+                                       size_t classes_size,
+                                       const char *const *ids,
+                                       size_t ids_size,
+                                       const char *const *exceptions,
+                                       size_t exceptions_size);
+
+/**
  * Checks if a `url` matches for the specified `Engine` within the context.
  */
 bool engine_match(C_Engine *engine,
@@ -82,6 +95,11 @@ void engine_remove_tag(C_Engine *engine, const char *tag);
  * Checks if a tag exists in the engine
  */
 bool engine_tag_exists(C_Engine *engine, const char *tag);
+
+/**
+ * Returns a set of cosmetic filtering resources specific to the given url, in JSON format
+ */
+char *engine_url_cosmetic_resources(C_Engine *engine, const char *url);
 
 /**
  * Get the specific default list size
