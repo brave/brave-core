3361205828221056752731a2a0395edea5aeb3ce
Anthony Tseng <darkdh@gmail.com>
2020-06-16 14:41:35 -0700

    Subclass unused SyncStoppedReporter

---
+++ components/sync/driver/brave_sync_stopped_reporter.h
@@ -0,0 +1,34 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_SYNC_DRIVER_BRAVE_SYNC_STOPPED_REPORTER_H_
+#define BRAVE_COMPONENTS_SYNC_DRIVER_BRAVE_SYNC_STOPPED_REPORTER_H_
+
+#include <string>
+
+#include "components/sync/driver/sync_stopped_reporter.h"
+
+namespace syncer {
+
+class BraveSyncStoppedReporter : public SyncStoppedReporter {
+ public:
+  BraveSyncStoppedReporter(
+      const GURL& sync_service_url,
+      const std::string& user_agent,
+      scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
+      ResultCallback callback);
+  ~BraveSyncStoppedReporter() override;
+
+  void ReportSyncStopped(const std::string& access_token,
+                         const std::string& cache_guid,
+                         const std::string& birthday) override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(BraveSyncStoppedReporter);
+};
+
+}  // namespace syncer
+
+#endif  // BRAVE_COMPONENTS_SYNC_DRIVER_BRAVE_SYNC_STOPPED_REPORTER_H_
