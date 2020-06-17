3a6dbb761c6b5958fc2bf1382a48794971b488c2
samartnik <artem@brave.com>
2020-06-17 18:22:31 -0400

    [Android] NewTabPage modifications
    Fixes crash on opening new tab after closing all
    Fixes NTP image on changing phone layout
    
    Related to chromium change:
    https://chromium.googlesource.com/chromium/src/+/c0a930a52910bb56bf68925d9f233c29281c516b
    
    commit c0a930a52910bb56bf68925d9f233c29281c516b
    Author: Theresa Wellington <twellington@chromium.org>
    Date:   Fri May 1 20:33:45 2020 +0000
    
        Combine FeedNewTabPage with NewTabPage, delete dead code
    
        Fold FeedNewTabPage into NewTabPage and start cleaning-up Java code
        that is no longer needed. Obviously dead code has been removed;
        subsequent CLs will be needed to clean-up more Java code and C++
        classes.
    
        Bug: 1069183

--- build/android/bytecode/java/org/brave/bytecode/BraveClassAdapter.java
+++ build/android/bytecode/java/org/brave/bytecode/BraveClassAdapter.java
@@ -17,6 +17,7 @@ public class BraveClassAdapter {
         chain = new BraveManageSyncSettingsClassAdapter(chain);
         chain = new BraveNewTabPageLayoutClassAdapter(chain);
         chain = new BraveFeedSurfaceCoordinatorClassAdapter(chain);
+        chain = new BraveNewTabPageClassAdapter(chain);
         return chain;
     }
 }
