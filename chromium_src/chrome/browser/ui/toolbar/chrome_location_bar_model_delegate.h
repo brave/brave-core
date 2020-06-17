20ca22cc726e95acf0fd1a9869a18274711964c2
mkarolin <max@brave.com>
2020-06-17 11:16:58 -0400

    Updated chrome_location_bar_model_delegate.h patch with override.

---
+++ chromium_src/chrome/browser/ui/toolbar/chrome_location_bar_model_delegate.h
@@ -0,0 +1,17 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_TOOLBAR_CHROME_LOCATION_BAR_MODEL_DELEGATE_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_TOOLBAR_CHROME_LOCATION_BAR_MODEL_DELEGATE_H_
+
+#define BRAVE_CHROME_LOCATION_BAR_MODEL_DELEGATE_H_ \
+ private:                                           \
+  friend class BraveLocationBarModelDelegate;       \
+                                                    \
+ public:
+#include "../../../../../../chrome/browser/ui/toolbar/chrome_location_bar_model_delegate.h"
+#undef BRAVE_CHROME_LOCATION_BAR_MODEL_DELEGATE_H_
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_TOOLBAR_CHROME_LOCATION_BAR_MODEL_DELEGATE_H_
