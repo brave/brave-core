bfd9c5216741296d1dd9ae0018a05fa8190cf4fc
samartnik <artem@brave.com>
2020-06-17 08:21:26 -0400

    [Android] Additional fixes for new tap page Chromium change:
    https://chromium.googlesource.com/chromium/src/+/c0a930a52910bb56bf68925d9f233c29281c516b
    
    commit c0a930a52910bb56bf68925d9f233c29281c516b
    Author: Theresa Wellington <twellington@chromium.org>
    Date:   Fri May 1 20:33:45 2020 +0000
    
        Combine FeedNewTabPage with NewTabPage, delete dead code
    
        Fold FeedNewTabPage into NewTabPage and start cleaning-up Java code
        that is no longer needed. Obviously dead code has been removed;
        subsequent CLs will be needed to clean-up more Java code and C++
        classes.
    
        Bug: 1069183

---
+++ build/android/bytecode/java/org/brave/bytecode/BraveFeedSurfaceCoordinatorClassAdapter.java
@@ -0,0 +1,31 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.brave.bytecode;
+
+import org.objectweb.asm.ClassVisitor;
+
+public class BraveFeedSurfaceCoordinatorClassAdapter extends BraveClassVisitor {
+    static String sFeedSurfaceCoordinatorClassName =
+            "org/chromium/chrome/browser/feed/FeedSurfaceCoordinator";
+    static String sBraveFeedSurfaceCoordinatorClassName =
+            "org/chromium/chrome/browser/feed/BraveFeedSurfaceCoordinator";
+
+    public BraveFeedSurfaceCoordinatorClassAdapter(ClassVisitor visitor) {
+        super(visitor);
+
+        deleteField(sBraveFeedSurfaceCoordinatorClassName, "mActivity");
+        makeProtectedField(sFeedSurfaceCoordinatorClassName, "mActivity");
+
+        deleteField(sBraveFeedSurfaceCoordinatorClassName, "mScrollViewForPolicy");
+        makeProtectedField(sFeedSurfaceCoordinatorClassName, "mScrollViewForPolicy");
+
+        deleteField(sBraveFeedSurfaceCoordinatorClassName, "mNtpHeader");
+        makeProtectedField(sFeedSurfaceCoordinatorClassName, "mNtpHeader");
+
+        deleteField(sBraveFeedSurfaceCoordinatorClassName, "mRootView");
+        makeProtectedField(sFeedSurfaceCoordinatorClassName, "mRootView");
+    }
+}
