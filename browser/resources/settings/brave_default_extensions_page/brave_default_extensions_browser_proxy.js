060ead7ec92d28cfab97039be4ac87dbbc0221a0
Pete Miller <miller.pete@gmail.com>
2020-06-17 22:36:22 -0700

    Settings WebUI: compiles for es-module / polymer3 build
    * Privacy page modifications now happen via testing hook instead of global variable modification, since modules no longer use global variables.
    * Automatically convert "html import" modules to ES modules using js_modulizer and polymer_modulizer
    * Rewrite namespaces to account for loss of global-scoped settings.* namespace. Use ES imports instead, mostly automatically converted via [x]_modulizer.
    * Rollup cannot parse compressed output
    * Ensure generated JS modules get served (TODO: use `webui::SetupWebUIDataSource`)

--- browser/resources/settings/brave_default_extensions_page/brave_default_extensions_browser_proxy.js
+++ browser/resources/settings/brave_default_extensions_page/brave_default_extensions_browser_proxy.js
@@ -2,6 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// clang-format off
+// #import {addSingletonGetter, sendWithPromise} from 'chrome://resources/js/cr.m.js';
+// clang-format on
+
 cr.define('settings', function() {
   /** @interface */
   class BraveDefaultExtensionsBrowserProxy {
@@ -59,6 +63,7 @@ cr.define('settings', function() {
 
   cr.addSingletonGetter(BraveDefaultExtensionsBrowserProxyImpl);
 
+  // #cr_define_end
   return {
     BraveDefaultExtensionsBrowserProxy,
     BraveDefaultExtensionsBrowserProxyImpl
