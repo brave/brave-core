89878c2a9b45c01c82d995fb51d9ebd20fecb109
Simon Hong <shong@brave.com>
2020-06-24 15:39:46 +0900

    WIP - Use bookmarks webui overrides

---
+++ browser/resources/bookmarks/brave_bookmarks_overrides.js
@@ -0,0 +1,1 @@
+console.log('Brave bookmarks override engaged!')
--- chromium_src/chrome/browser/ui/webui/bookmarks/bookmarks_ui.cc
+++ chromium_src/chrome/browser/ui/webui/bookmarks/bookmarks_ui.cc
@@ -3,6 +3,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // you can obtain one at http://mozilla.org/MPL/2.0/.
 
+#include "brave/browser/resources/bookmarks/grit/brave_bookmarks_resources.h"
+#include "brave/browser/resources/bookmarks/grit/brave_bookmarks_resources_map.h"
 #include "brave/browser/ui/webui/navigation_bar_data_provider.h"
 #include "brave/grit/brave_generated_resources.h"
 #include "content/public/browser/web_ui_data_source.h"
@@ -10,7 +12,13 @@
 #define BRAVE_CREATE_BOOKMARKS_UI_HTML_SOURCE \
   NavigationBarDataProvider::Initialize(source); \
   source->AddLocalizedString( \
-      "emptyList", IDS_BRAVE_BOOKMARK_MANAGER_EMPTY_LIST);
+      "emptyList", IDS_BRAVE_BOOKMARK_MANAGER_EMPTY_LIST); \
+  const std::string prefix = "brave/"; \
+  for (size_t i = 0; i < kBraveBookmarksResourcesSize; ++i) { \
+    std::string path = prefix; \
+    path += kBraveBookmarksResources[i].name; \
+    source->AddResourcePath(path, kBraveBookmarksResources[i].value); \
+  }
 
 #include "../../../../../../../chrome/browser/ui/webui/bookmarks/bookmarks_ui.cc"
 
