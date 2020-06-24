873a09762832dc6350af56ef6d7ca5809ae38543
samartnik <artem@brave.com>
2020-06-24 17:00:14 -0400

    Added new strings, url and their translations

--- android/java/org/chromium/chrome/browser/upgrade/NotificationIntent.java
+++ android/java/org/chromium/chrome/browser/upgrade/NotificationIntent.java
@@ -34,8 +34,8 @@ public class NotificationIntent {
     private static final String TAG = "NotificationIntent";
     private static final String NOTIFICATION_TAG = "16c570a4-da7d-4c4e-8518-d2b7d6e41615";
     private static final String NOTIFICATION_CHANNEL_ID = "a79c3102-4183-4001-a553-ec3041bd0f49";
-    private static final String URL = "https://brave.com/new-brave-22-percent-faster/";
-    private static final List<String> mWhitelistedRegionalLocales = Arrays.asList("en", "ru", "uk", "be", "pt", "fr");
+    private static final String URL = "https://support.brave.com/hc/en-us/articles/360045401211/";
+    private static final List<String> mWhitelistedRegionalLocales = Arrays.asList("en", "ru", "uk", "de", "pt", "pl", "ja", "es");
     private static final int NOTIFICATION_ID = 732;
     //private static final String NOTIFICATION_TITLE = "Brave update";
     //private static final String NOTIFICATION_TEXT = "The new Brave browser is 22% faster";
@@ -70,6 +70,7 @@ public class NotificationIntent {
         mBuilder.setSmallIcon(R.drawable.ic_chrome);
         mBuilder.setContentTitle(context.getString(R.string.update_notification_title));
         mBuilder.setContentText(notification_text);
+        mBuilder.setStyle(new NotificationCompat.BigTextStyle().bigText(notification_text));
 
         mNotificationManager.notify(NOTIFICATION_TAG, NOTIFICATION_ID, mBuilder.build());
     }
