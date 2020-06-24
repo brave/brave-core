4e0553ddca28ab3350d3629a3559d0f382344ba2
Brian R. Bondy <netzen@gmail.com>
2020-06-24 13:13:47 -0400

    Generate override files for strings we override

--- script/chromium-rebase-l10n.py
+++ script/chromium-rebase-l10n.py
@@ -2,10 +2,13 @@ import argparse
 import io
 import os.path
 from os import walk
+import re
 import sys
 from lxml import etree
 from lib.config import get_env_var
-from lib.transifex import pull_source_files_from_transifex
+from lib.grd_string_replacements import (write_xml_file_from_tree, write_braveified_grd_override,
+                                         update_braveified_grd_tree_override, get_override_file_path)
+from lib.transifex import pull_source_files_from_transifex, textify
 
 SOURCE_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
@@ -16,10 +19,55 @@ def parse_args():
     return parser.parse_args()
 
 
+def generate_overrides_and_replace_strings(source_string_path):
+    original_xml_tree_with_branding_fixes = etree.parse(source_string_path)
+    update_braveified_grd_tree_override(original_xml_tree_with_branding_fixes, True)
+    write_braveified_grd_override(source_string_path)
+    modified_xml_tree = etree.parse(source_string_path)
+
+    original_messages = original_xml_tree_with_branding_fixes.xpath('//message')
+    modified_messages = modified_xml_tree.xpath('//message')
+    assert len(original_messages) == len(modified_messages)
+    for i in range(0, len(original_messages)):
+        if textify(original_messages[i]) == textify(modified_messages[i]):
+            modified_messages[i].getparent().remove(modified_messages[i])
+
+    # Remove uneeded things from the override grds
+    nodes_to_remove = [
+        '//file',
+        '//outputs',
+        '//comment()',
+    ]
+    for xpath_expr in nodes_to_remove:
+        nodes = modified_xml_tree.xpath(xpath_expr)
+        for n in nodes:
+            if n.getparent() is not None:
+                n.getparent().remove(n)
+    parts = modified_xml_tree.xpath('//part')
+    for part in parts:
+        override_file = get_override_file_path(part.attrib['file'])
+        if os.path.exists(os.path.join(os.path.dirname(source_string_path), override_file)):
+            part.attrib['file'] = override_file
+        else:
+            # No grdp override here, carry on
+            part.getparent().remove(part)
+
+    # Write out an override file that is a duplicate of the original file but with strings that
+    # are shared with Chrome stripped out.
+    filename = os.path.basename(source_string_path)
+    (basename, ext) = filename.split('.')
+    override_string_path = get_override_file_path(source_string_path)
+    modified_messages = modified_xml_tree.xpath('//message')
+    if len(modified_messages) > 0 or ext == 'grd':
+        write_xml_file_from_tree(override_string_path, modified_xml_tree)
+
+
 def main():
     args = parse_args()
+    # This file path is a string path inside brave/ but just recently copied
+    # in from chromium files which need replacements.
     source_string_path = os.path.join(SOURCE_ROOT, args.source_string_path[0])
-    filename = os.path.basename(source_string_path).split('.')[0]
+    filename = os.path.basename(source_string_path)
     extension = os.path.splitext(source_string_path)[1]
     if (extension != '.grd' and extension != '.grdp'):
         print 'returning early'
@@ -28,11 +76,13 @@ def main():
     print 'Rebasing source string file:', source_string_path
     print 'filename:', filename
 
-    content = ''
-    xml_tree = etree.parse(source_string_path)
+    generate_overrides_and_replace_strings(source_string_path)
+
     # If you modify the translateable attribute then also update
     # is_translateable_string function in brave/script/lib/transifex.py.
-    if filename == 'brave_strings':
+    xml_tree = etree.parse(source_string_path)
+    (basename, ext) = filename.split('.')
+    if basename == 'brave_strings':
         elem1 = xml_tree.xpath('//message[@name="IDS_SXS_SHORTCUT_NAME"]')[0]
         elem1.text = 'Brave Nightly'
         elem1.attrib.pop('desc')
