9b1ec9c847327eaa2e97d04c93db078ca33a41b1
AlexeyBarabash <alexey@brave.com>
2020-06-25 21:10:11 +0300

    Fixed issue when sync on Android didn't work after app restart. Modification to force act as AndroidSyncSettings.mChromeSyncEnabled is always true

---
+++ build/android/bytecode/java/org/brave/bytecode/BraveAndroidSyncSettingsClassAdapter.java
@@ -0,0 +1,27 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.brave.bytecode;
+
+import org.objectweb.asm.ClassVisitor;
+
+public class BraveAndroidSyncSettingsClassAdapter extends BraveClassVisitor {
+	static String sAndroidSyncSettingsClassName =
+            "org/chromium/components/sync/AndroidSyncSettings";
+    static String sBraveAndroidSyncSettingsClassName =
+            "org/chromium/components/sync/BraveAndroidSyncSettings";
+
+    public BraveAndroidSyncSettingsClassAdapter(ClassVisitor visitor) {
+        super(visitor);
+
+        addMethodAnnotation(sBraveAndroidSyncSettingsClassName,
+            "isSyncEnabled", "Ljava/lang/Override;");
+        addMethodAnnotation(sBraveAndroidSyncSettingsClassName,
+            "disableChromeSync", "Ljava/lang/Override;");
+
+        deleteField(sBraveAndroidSyncSettingsClassName, "mMasterSyncEnabled");
+        makeProtectedField(sAndroidSyncSettingsClassName, "mMasterSyncEnabled");
+    }
+}
---
+++ components/sync/android/java/src/org/chromium/components/sync/BraveAndroidSyncSettings.java
@@ -0,0 +1,28 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.chromium.components.sync;
+
+// see org.brave.bytecode.BraveAndroidSyncSettingsAdapter
+public class BraveAndroidSyncSettings extends AndroidSyncSettings {
+    private boolean mMasterSyncEnabled;
+
+    public BraveAndroidSyncSettings(SyncContentResolverDelegate syncContentResolverDelegate) {
+        super(syncContentResolverDelegate, null);
+    }
+
+    // Chromium's AndroidSyncSettings.mChromeSyncEnabled is never set to true
+    // after app restart when Brave sync v2 is enabled, and by this reason on
+    // next SyncController.updateSyncStateFromAndroid sync gets disabled, so
+    // we act here as mChromeSyncEnabled would be true.
+    // Following up issue https://github.com/brave/brave-browser/issues/10454
+    @Override
+    public boolean isSyncEnabled() {
+        return mMasterSyncEnabled;
+    }
+
+    @Override
+    public void disableChromeSync() { }
+}
