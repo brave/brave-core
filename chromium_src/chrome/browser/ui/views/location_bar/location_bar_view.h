620f8bdc25c829e13123897f8809f1cd4db88b8f
mkarolin <max@brave.com>
2020-06-26 17:14:33 -0400

    Updated location_bar_view.h patch and chromium_src override.

--- chromium_src/chrome/browser/ui/views/location_bar/location_bar_view.h
+++ chromium_src/chrome/browser/ui/views/location_bar/location_bar_view.h
@@ -6,8 +6,56 @@
 #ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_LOCATION_BAR_LOCATION_BAR_VIEW_H_
 #define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_LOCATION_BAR_LOCATION_BAR_VIEW_H_
 
+// Init and Update method names which we redefine below are too common, so
+// copying the entire include section from the original file.
+
+#include <stddef.h>
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "base/compiler_specific.h"
+#include "base/gtest_prod_util.h"
+#include "base/macros.h"
+#include "base/memory/weak_ptr.h"
+#include "chrome/browser/extensions/extension_context_menu_model.h"
+#include "chrome/browser/ui/location_bar/location_bar.h"
+#include "chrome/browser/ui/omnibox/chrome_omnibox_edit_controller.h"
+#include "chrome/browser/ui/page_action/page_action_icon_type.h"
+#include "chrome/browser/ui/views/dropdown_bar_host.h"
+#include "chrome/browser/ui/views/dropdown_bar_host_delegate.h"
+#include "chrome/browser/ui/views/extensions/extension_popup.h"
+#include "chrome/browser/ui/views/location_bar/content_setting_image_view.h"
+#include "chrome/browser/ui/views/location_bar/location_icon_view.h"
+#include "chrome/browser/ui/views/location_bar/permission_chip.h"
+#include "chrome/browser/ui/views/omnibox/omnibox_view_views.h"
+#include "chrome/browser/ui/views/page_action/page_action_icon_view.h"
+#include "components/security_state/core/security_state.h"
+#include "ui/base/pointer/touch_ui_controller.h"
+#include "ui/gfx/animation/slide_animation.h"
+#include "ui/gfx/font.h"
+#include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/image/image.h"
+#include "ui/views/animation/animation_delegate_views.h"
+#include "ui/views/controls/button/button.h"
+#include "ui/views/controls/focus_ring.h"
+#include "ui/views/drag_controller.h"
+
+#define BRAVE_LOCATION_BAR_VIEW_H_   \
+ private:                            \
+  friend class BraveLocationBarView; \
+                                     \
+ public:                             \
+  void Layout(views::View* trailing_view);
+
+#define Init virtual Init
+#define Update virtual Update
 #define GetBorderRadius virtual GetBorderRadius
 #include "../../../../../../../chrome/browser/ui/views/location_bar/location_bar_view.h"
+#undef Update
+#undef Init
 #undef GetBorderRadius
+#undef BRAVE_LOCATION_BAR_VIEW_H_
 
 #endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_LOCATION_BAR_LOCATION_BAR_VIEW_H_
