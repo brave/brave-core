edac247f141ccc84d3c40343164cebded2e20bc6
samartnik <artem@brave.com>
2020-06-29 20:21:29 -0400

    [Android] Additional changes for InterceptNavigationDelegateImpl Addresses code review suggestions
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/204335845d2b7292bb99cb880203b7d132ddfa72
    
    commit 204335845d2b7292bb99cb880203b7d132ddfa72
    Author: Colin Blundell <blundell@chromium.org>
    Date:   Wed Apr 15 08:52:29 2020 +0000
    
        [Android] Componentize InterceptNavigationDelegateImpl
    
        After preceding CLs this class has no remaining //chrome dependencies
        other than resources, which this CL componentizes along with the class
        itself. As this implementation of InterceptNavigationDelegate is
        specific to the overall intent launching logic in
        //components/external_intents, this CL componentizes it into that
        component. Componentizing this class will enable it to be reused in
        WebLayer.
    
        Bug: 1031465

--- chromium_src/components/external_intents/android/intercept_navigation_delegate_impl.cc
+++ chromium_src/components/external_intents/android/intercept_navigation_delegate_impl.cc
@@ -10,6 +10,8 @@
 #include "components/navigation_interception/intercept_navigation_delegate.h"
 #include "components/navigation_interception/navigation_params.h"
 #include "components/prefs/pref_service.h"
+#include "components/user_prefs/user_prefs.h"
+#include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/web_contents.h"
 #include "net/base/escape.h"
@@ -21,30 +23,14 @@ namespace {
 using navigation_interception::InterceptNavigationDelegate;
 using navigation_interception::NavigationParams;
 
-bool ShouldPlayVideoInBrowser(const GURL& url) {
-  PrefService* pref_service =
-      prefs::BravePrefService::GetInstance()->GetPrefs();
-  if (!pref_service) {
-    NOTREACHED();
-    return false;
-  }
-
-  if (!pref_service->GetBoolean(kPlayYTVideoInBrowserEnabled)) {
-    return false;
-  }
-
-  if (url.host().find("youtube.com") != std::string::npos ||
-      url.host().find("youtu.be") != std::string::npos) {
-    return true;
-  }
-
-  return false;
-}
-
 class BraveInterceptNavigationDelegate : public InterceptNavigationDelegate {
  public:
-  BraveInterceptNavigationDelegate(JNIEnv* env, jobject jdelegate)
-      : InterceptNavigationDelegate(env, jdelegate) {}
+  BraveInterceptNavigationDelegate(JNIEnv* env,
+                                   jobject jdelegate,
+                                   PrefService* pref_service)
+      : InterceptNavigationDelegate(env, jdelegate) {
+    pref_service_ = pref_service;
+  }
 
   bool ShouldIgnoreNavigation(
       const NavigationParams& navigation_params) override {
@@ -58,6 +44,27 @@ class BraveInterceptNavigationDelegate : public InterceptNavigationDelegate {
     return InterceptNavigationDelegate::ShouldIgnoreNavigation(
         chrome_navigation_params);
   }
+
+ private:
+  bool ShouldPlayVideoInBrowser(const GURL& url) {
+    if (!pref_service_) {
+      NOTREACHED();
+      return false;
+    }
+
+    if (!pref_service_->GetBoolean(kPlayYTVideoInBrowserEnabled)) {
+      return false;
+    }
+
+    if (url.host().find("youtube.com") != std::string::npos ||
+        url.host().find("youtu.be") != std::string::npos) {
+      return true;
+    }
+
+    return false;
+  }
+
+  PrefService* pref_service_;
 };
 
 }  // namespace
@@ -71,7 +78,9 @@ static void JNI_InterceptNavigationDelegateImpl_AssociateWithWebContents(
       content::WebContents::FromJavaWebContents(jweb_contents);
   InterceptNavigationDelegate::Associate(
       web_contents,
-      std::make_unique<BraveInterceptNavigationDelegate>(env, jdelegate));
+      std::make_unique<BraveInterceptNavigationDelegate>(
+          env, jdelegate,
+          user_prefs::UserPrefs::Get(web_contents->GetBrowserContext())));
 }
 
 }  // namespace external_intents
