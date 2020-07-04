f3f889d08cccccfd504ccd8d8ca5170409d4c39c
Deep <deeppandya91@gmail.com>
2020-07-04 18:53:42 -0400

    Add Brave stats UI
    Add datatable for saved bandwidth

---
+++ android/java/org/chromium/chrome/browser/local_database/SavedBandwidthTable.java
@@ -0,0 +1,38 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.chromium.chrome.browser.local_database;
+
+public class SavedBandwidthTable {
+    public static final String TABLE_NAME = "saved_bandwidth_table";
+
+    public static final String COLUMN_SAVED_BANDWIDTH = "saved_bandwidth";
+    public static final String COLUMN_TIMESTAMP = "timestamp";
+    // Create table SQL query
+    public static final String CREATE_TABLE =
+        "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + "("
+        + COLUMN_SAVED_BANDWIDTH + " INTEGER,"
+        + COLUMN_TIMESTAMP + " DATETIME"
+        + ")";
+
+    public SavedBandwidthTable() {
+    }
+
+    private long mSavedBandwidth;
+    private String mTimestamp;
+
+    public SavedBandwidthTable(long savedBandwidth, String timestamp) {
+        mSavedBandwidth = savedBandwidth;
+        mTimestamp = timestamp;
+    }
+
+    public long getSavedBandwidth() {
+        return mSavedBandwidth;
+    }
+
+    public String getTimestamp() {
+        return mTimestamp;
+    }
+}
