705fd008dbc3228e615e3a710ca4358726824db1
Kyle Hickinson <kylehickinson@users.noreply.github.com>
2020-07-06 11:45:07 -0400

    Fix #2697: Use correct icon size during edge loop for scaled image data (#2701)
    Also:
    - Adds a check for if the icon passed is non-zero in both axis
    - Dispatches completion blocks within the global thread on main queue because the function is now static and not private

--- ClientTests/TestFavicons.swift
+++ ClientTests/TestFavicons.swift
@@ -27,4 +27,53 @@ class TestFavicons: ProfileTest {
         }
         wait(for: [expectation], timeout: 5.0)
     }
+    
+    func testIconTransparentAroundEdges() {
+        let expectation = XCTestExpectation(description: "favicon.transparent.1")
+        var image: UIImage!
+        let size = CGSize(width: 48, height: 48)
+        UIGraphicsBeginImageContext(size)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setFillColor(UIColor.black.cgColor)
+        context.addEllipse(in: CGRect(size: size).insetBy(dx: 5, dy: 5))
+        context.fillPath()
+        image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        FaviconFetcher.isIconBackgroundTransparentAroundEdges(image) { isTransparent in
+            dispatchPrecondition(condition: .onQueue(.main))
+            XCTAssert(isTransparent)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 5.0)
+    }
+    
+    func testIconNotTransparentAroundEdges() {
+        let expectation = XCTestExpectation(description: "favicon.transparent.2")
+        var image: UIImage!
+        let size = CGSize(width: 48, height: 48)
+        UIGraphicsBeginImageContext(size)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setFillColor(UIColor.black.cgColor)
+        context.addRect(CGRect(size: size))
+        context.fillPath()
+        image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        FaviconFetcher.isIconBackgroundTransparentAroundEdges(image) { isTransparent in
+            dispatchPrecondition(condition: .onQueue(.main))
+            XCTAssertFalse(isTransparent)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 5.0)
+    }
+    
+    func testIconTransparencyDoesntCrashWithEmptyImage() {
+        let expectation = XCTestExpectation(description: "favicon.transparent.3")
+        let image = UIImage()
+        FaviconFetcher.isIconBackgroundTransparentAroundEdges(image) { isTransparent in
+            dispatchPrecondition(condition: .onQueue(.main))
+            XCTAssertFalse(isTransparent)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 5.0)
+    }
 }
