76bd8669c9bd6147a6deeb13e660a5c13c835907
Mark Pilgrim <mpilgrim@brave.com>
2020-07-09 11:57:03 -0400

    Implement navigator.userAgent farbling
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    fix unused function error on Linux
    
    break out build, always set UA, test farbling reset
    
    build things
    
    more deps
    
    less nesting
    
    no nesting
    
    don't run on DidRedirectNavigation, explicitly set ua override to false if we're not overriding
    
    max UA should be suffixed off default UA, not upstream UA

---
+++ browser/farbling/brave_navigator_useragent_farbling_browsertest.cc
@@ -0,0 +1,161 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "base/path_service.h"
+#include "base/strings/stringprintf.h"
+#include "base/task/post_task.h"
+#include "base/test/thread_test_helper.h"
+#include "brave/browser/brave_browser_process_impl.h"
+#include "brave/browser/brave_content_browser_client.h"
+#include "brave/browser/extensions/brave_base_local_data_files_browsertest.h"
+#include "brave/common/brave_paths.h"
+#include "brave/common/pref_names.h"
+#include "brave/components/brave_component_updater/browser/local_data_files_service.h"
+#include "brave/components/brave_shields/browser/brave_shields_util.h"
+#include "chrome/browser/content_settings/host_content_settings_map_factory.h"
+#include "chrome/browser/extensions/extension_browsertest.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/common/chrome_content_client.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/ui_test_utils.h"
+#include "components/permissions/permission_request.h"
+#include "components/prefs/pref_service.h"
+#include "content/public/browser/render_frame_host.h"
+#include "content/public/test/browser_test.h"
+#include "content/public/test/browser_test_utils.h"
+#include "net/dns/mock_host_resolver.h"
+
+using brave_shields::ControlType;
+
+const char kUserAgentScript[] =
+    "domAutomationController.send(navigator.userAgent);";
+
+class BraveNavigatorUserAgentFarblingBrowserTest : public InProcessBrowserTest {
+ public:
+  void SetUpOnMainThread() override {
+    InProcessBrowserTest::SetUpOnMainThread();
+
+    content_client_.reset(new ChromeContentClient);
+    content::SetContentClient(content_client_.get());
+    browser_content_client_.reset(new BraveContentBrowserClient());
+    content::SetBrowserClientForTesting(browser_content_client_.get());
+
+    host_resolver()->AddRule("*", "127.0.0.1");
+    content::SetupCrossSiteRedirector(embedded_test_server());
+
+    brave::RegisterPathProvider();
+    base::FilePath test_data_dir;
+    base::PathService::Get(brave::DIR_TEST_DATA, &test_data_dir);
+    embedded_test_server()->ServeFilesFromDirectory(test_data_dir);
+
+    ASSERT_TRUE(embedded_test_server()->Start());
+  }
+
+  void TearDown() override {
+    browser_content_client_.reset();
+    content_client_.reset();
+  }
+
+  HostContentSettingsMap* content_settings() {
+    return HostContentSettingsMapFactory::GetForProfile(browser()->profile());
+  }
+
+  void AllowFingerprinting(std::string domain) {
+    brave_shields::SetFingerprintingControlType(
+        content_settings(), ControlType::ALLOW,
+        embedded_test_server()->GetURL(domain, "/"));
+  }
+
+  void BlockFingerprinting(std::string domain) {
+    brave_shields::SetFingerprintingControlType(
+        content_settings(), ControlType::BLOCK,
+        embedded_test_server()->GetURL(domain, "/"));
+  }
+
+  void SetFingerprintingDefault(std::string domain) {
+    brave_shields::SetFingerprintingControlType(
+        content_settings(), ControlType::DEFAULT,
+        embedded_test_server()->GetURL(domain, "/"));
+  }
+
+  template <typename T>
+  int ExecScriptGetInt(const std::string& script, T* frame) {
+    int value;
+    EXPECT_TRUE(ExecuteScriptAndExtractInt(frame, script, &value));
+    return value;
+  }
+
+  template <typename T>
+  std::string ExecScriptGetStr(const std::string& script, T* frame) {
+    std::string value;
+    EXPECT_TRUE(ExecuteScriptAndExtractString(frame, script, &value));
+    return value;
+  }
+
+  content::WebContents* contents() {
+    return browser()->tab_strip_model()->GetActiveWebContents();
+  }
+
+  bool NavigateToURLUntilLoadStop(const GURL& url) {
+    ui_test_utils::NavigateToURL(browser(), url);
+    return WaitForLoadStop(contents());
+  }
+
+ private:
+  std::unique_ptr<ChromeContentClient> content_client_;
+  std::unique_ptr<BraveContentBrowserClient> browser_content_client_;
+};
+
+// Tests results of farbling user agent
+IN_PROC_BROWSER_TEST_F(BraveNavigatorUserAgentFarblingBrowserTest,
+                       FarbleNavigatorUserAgent) {
+  std::string domain_b = "b.com";
+  std::string domain_z = "z.com";
+  GURL url_b = embedded_test_server()->GetURL(domain_b, "/simple.html");
+  GURL url_z = embedded_test_server()->GetURL(domain_z, "/simple.html");
+
+  // Farbling level: off
+  // get real navigator.userAgent
+  AllowFingerprinting(domain_b);
+  NavigateToURLUntilLoadStop(url_b);
+  std::string off_ua_b = ExecScriptGetStr(kUserAgentScript, contents());
+  AllowFingerprinting(domain_z);
+  NavigateToURLUntilLoadStop(url_z);
+  std::string off_ua_z = ExecScriptGetStr(kUserAgentScript, contents());
+  // user agent should be the same on every domain if farbling is off
+  EXPECT_EQ(off_ua_b, off_ua_z);
+
+  // Farbling level: default
+  // navigator.userAgent may be farbled, but the farbling is not
+  // domain-specific
+  SetFingerprintingDefault(domain_b);
+  NavigateToURLUntilLoadStop(url_b);
+  std::string default_ua_b = ExecScriptGetStr(kUserAgentScript, contents());
+  SetFingerprintingDefault(domain_z);
+  NavigateToURLUntilLoadStop(url_z);
+  std::string default_ua_z = ExecScriptGetStr(kUserAgentScript, contents());
+  // user agent should be the same on every domain if farbling is default
+  EXPECT_EQ(default_ua_b, default_ua_z);
+
+  // Farbling level: maximum
+  // navigator.userAgent should be the possibly-farbled string from the default
+  // farbling level, further suffixed by a pseudo-random number of spaces based
+  // on domain and session key
+  BlockFingerprinting(domain_b);
+  NavigateToURLUntilLoadStop(url_b);
+  std::string max_ua_b = ExecScriptGetStr(kUserAgentScript, contents());
+  EXPECT_EQ(max_ua_b, default_ua_b + "   ");
+  BlockFingerprinting(domain_z);
+  NavigateToURLUntilLoadStop(url_z);
+  std::string max_ua_z = ExecScriptGetStr(kUserAgentScript, contents());
+  EXPECT_EQ(max_ua_z, default_ua_z + "  ");
+
+  // Farbling level: off
+  // verify that user agent is reset properly after having been farbled
+  AllowFingerprinting(domain_b);
+  NavigateToURLUntilLoadStop(url_b);
+  std::string off_ua_b2 = ExecScriptGetStr(kUserAgentScript, contents());
+  EXPECT_EQ(off_ua_b, off_ua_b2);
+}
