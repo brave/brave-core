f6b544c8d02c9bad9556c767a0141526cd1a89a6
Simon Hong <shong@brave.com>
2020-07-10 10:03:59 +0900

    Fix SR's Themes subpage is not loaded
    auto importing of chromium resources by brave module doesn't work properly.
    See https://github.com/brave/brave-core/pull/6029#issuecomment-655263184 for root cause.
    Instead, imports explicitely.

--- browser/resources/settings/brave_appearance_page/super_referral.js
+++ browser/resources/settings/brave_appearance_page/super_referral.js
@@ -3,6 +3,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // you can obtain one at http://mozilla.org/MPL/2.0/.
 
+import {AppearanceBrowserProxy, AppearanceBrowserProxyImpl} from '../appearance_page/appearance_browser_proxy.js';
+
 (function() {
 
 /**
