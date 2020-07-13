4f35d35bd6bed79913a4c1514f590e387a84078f
serg <serg.zhukovsky@gmail.com>
2020-07-13 14:00:11 -0400

    removes unneeded location service message on Android

---
+++ android/java/org/chromium/chrome/browser/search_engines/settings/BraveSearchEngineAdapter.java
@@ -0,0 +1,44 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.chromium.chrome.browser.search_engines.settings;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import androidx.annotation.StringRes;
+
+public class BraveSearchEngineAdapter extends BaseAdapter {
+    public BraveSearchEngineAdapter() {
+    }
+
+    // BaseAdapter:
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+    	return convertView;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public Object getItem(int pos) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @StringRes
+    protected int getPermissionsLinkMessage(String url) {
+    	return 0;
+    }
+}
