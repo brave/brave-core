6b9e0f77a1602f4e9db52c0dcd0f9051095d8ab9
Anthony Tseng <darkdh@gmail.com>
2020-07-14 19:22:31 -0700

    Show "Initializing Tor..." on omnibox and disable it until Tor process is launched

--- chromium_src/chrome/browser/ui/views/location_bar/location_bar_view.cc
+++ chromium_src/chrome/browser/ui/views/location_bar/location_bar_view.cc
@@ -4,12 +4,15 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/browser/ui/omnibox/brave_omnibox_client_impl.h"
+#include "brave/browser/ui/views/omnibox/brave_omnibox_view_views.h"
 
 #define BRAVE_LAYOUT_TRAILING_DECORATIONS     \
   if (right_most && right_most->GetVisible()) \
     trailing_decorations.AddDecoration(0, height(), false, 0, 0, right_most);
 
 #define ChromeOmniboxClient BraveOmniboxClientImpl
+#define OmniboxViewViews BraveOmniboxViewViews
 #include "../../../../../../../chrome/browser/ui/views/location_bar/location_bar_view.cc"
+#undef OmniboxViewViews
 #undef ChromeOmniboxClient
 #undef BRAVE_LAYOUT_TRAILING_DECORATIONS
