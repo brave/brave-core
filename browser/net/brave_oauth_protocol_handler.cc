67b96d7d67750a8fb783e3228af341107e889055
ryanml <ryanlanese@gmail.com>
2020-07-17 21:32:29 -0700

    Adding rewards protocol logic to brave_oauth_protocol_handler

--- browser/net/brave_oauth_protocol_handler.cc
+++ browser/net/brave_oauth_protocol_handler.cc
@@ -8,6 +8,7 @@
 #include <string>
 #include <map>
 #include <utility>
+#include <vector>
 
 #include "base/optional.h"
 #include "base/strings/strcat.h"
@@ -15,6 +16,7 @@
 #include "base/task/post_task.h"
 #include "brave/common/url_constants.h"
 #include "brave/components/binance/browser/buildflags/buildflags.h"
+#include "brave/components/brave_rewards/browser/buildflags/buildflags.h"
 #include "brave/components/gemini/browser/buildflags/buildflags.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/user_prefs/user_prefs.h"
@@ -31,6 +33,10 @@
 #include "brave/components/binance/browser/binance_service.h"
 #endif
 
+#if BUILDFLAG(BRAVE_REWARDS_ENABLED)
+#include "brave/components/brave_rewards/common/url_constants.h"
+#endif
+
 #if BUILDFLAG(GEMINI_ENABLED)
 #include "brave/browser/gemini/gemini_service_factory.h"
 #include "brave/components/gemini/browser/gemini_service.h"
@@ -41,12 +47,118 @@ namespace {
 const std::map<std::string, std::string> redirects = {
   {kBinanceScheme, "chrome://newtab?binanceAuth=1"},
   {kGeminiScheme, "chrome://newtab?geminiAuth=1"},
+  {kRewardsScheme, "chrome://rewards"},
 };
 
 const std::map<std::string, std::string> origins = {
   {kBinanceScheme, "https://accounts.binance.com"},
   {kGeminiScheme, "https://exchange.gemini.com"},
 };
+
+const std::vector<std::string> rewards_origins = {
+  "https://uphold.com",
+  "https://sandbox.uphold.com"
+};
+
+const std::string GetAuthToken(const GURL& url) {
+  std::map<std::string, std::string> parts;
+
+  for (net::QueryIterator it(url); !it.IsAtEnd(); it.Advance()) {
+    parts[it.GetKey()] = it.GetUnescapedValue();
+  }
+
+  if (parts.find("code") != parts.end()) {
+    return parts["code"];
+  } else {
+    return std::string();
+  }
+}
+
+GURL GetRedirectURL(const GURL& url,
+                    bool preserve_path = false) {
+  if (!url.is_valid()) {
+    return GURL();
+  }
+
+  const std::string scheme = url.scheme();
+
+  if (!preserve_path) {
+    return GURL(redirects.at(scheme));
+  }
+
+  std::string query;
+  std::string path = url.path();
+
+  if (url.has_query()) {
+    query = base::StrCat({
+      "?",
+      net::EscapeExternalHandlerValue(url.query())
+    });
+  }
+
+  base::ReplaceFirstSubstringAfterOffset(&path, 0, "/", "");
+
+  return GURL(
+      base::StrCat({
+        redirects.at(scheme),
+        path,
+        query
+      }));
+}
+
+void SetAuthToken(const std::string& auth_token,
+                  const std::string& oauth_scheme,
+                  content::WebContents* web_contents) {
+  if (auth_token.empty()) {
+    return;
+  }
+
+  if (oauth_scheme == kBinanceScheme) {
+#if BUILDFLAG(BINANCE_ENABLED)
+    Profile* profile =
+        Profile::FromBrowserContext(
+          web_contents->GetBrowserContext());
+    BinanceServiceFactory::GetInstance()
+      ->GetForProfile(profile)
+      ->SetAuthToken(auth_token);
+#endif
+    } else if (oauth_scheme == kGeminiScheme) {
+#if BUILDFLAG(GEMINI_ENABLED)
+    Profile* profile =
+        Profile::FromBrowserContext(
+          web_contents->GetBrowserContext());
+    GeminiServiceFactory::GetInstance()
+      ->GetForProfile(profile)
+      ->SetAuthToken(auth_token);
+#endif
+  }
+}
+
+bool IsAllowed(content::WebContents* web_contents,
+               const base::Optional<url::Origin>& initiating_origin,
+               const std::string& origin) {
+  GURL allowed_origin(origin);
+  bool unallowed_origin =
+    !initiating_origin.has_value() ||
+    initiating_origin.value().GetURL() != allowed_origin ||
+    web_contents->GetLastCommittedURL().GetOrigin() != allowed_origin;
+  return !unallowed_origin;
+}
+
+bool ShouldRedirect(content::WebContents* web_contents,
+                    const base::Optional<url::Origin>& initiating_origin,
+                    const std::string& scheme) {
+  if (scheme == kRewardsScheme) {
+    for (const auto& origin : rewards_origins) {
+      if (IsAllowed(web_contents, initiating_origin, origin)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  return IsAllowed(web_contents, initiating_origin, origins.at(scheme));
+}
 
 void LoadNewTabURL(
     const GURL& url,
@@ -64,46 +176,18 @@ void LoadNewTabURL(
     return;
   }
 
-  std::string oauth_scheme = url.scheme();
-  GURL allowed_origin(origins.at(oauth_scheme));
-
-  if (web_contents->GetLastCommittedURL().GetOrigin() != allowed_origin ||
-      !initiating_origin.has_value() ||
-      initiating_origin.value().GetURL() != allowed_origin) {
+  const std::string oauth_scheme = url.scheme();
+  if (!ShouldRedirect(web_contents,
+         initiating_origin, oauth_scheme)) {
     return;
   }
 
-  std::map<std::string, std::string> parts;
-  for (net::QueryIterator it(url); !it.IsAtEnd(); it.Advance()) {
-    parts[it.GetKey()] = it.GetUnescapedValue();
-  }
+  SetAuthToken(
+    GetAuthToken(url), oauth_scheme, web_contents);
 
-  if (parts.find("code") != parts.end()) {
-    std::string auth_token = parts["code"];
-
-    if (oauth_scheme == kBinanceScheme) {
-#if BUILDFLAG(BINANCE_ENABLED)
-    Profile* profile =
-        Profile::FromBrowserContext(
-          web_contents->GetBrowserContext());
-      BinanceServiceFactory::GetInstance()
-        ->GetForProfile(profile)
-        ->SetAuthToken(auth_token);
-#endif
-    } else if (oauth_scheme == kGeminiScheme) {
-#if BUILDFLAG(GEMINI_ENABLED)
-    Profile* profile =
-        Profile::FromBrowserContext(
-          web_contents->GetBrowserContext());
-      GeminiServiceFactory::GetInstance()
-        ->GetForProfile(profile)
-        ->SetAuthToken(auth_token);
-#endif
-    }
-  }
-
-  GURL redirect_url(redirects.at(oauth_scheme));
-  web_contents->GetController().LoadURL(GURL(redirect_url),
+  const auto redirect_url = GetRedirectURL(url,
+    (oauth_scheme == kRewardsScheme));
+  web_contents->GetController().LoadURL(redirect_url,
       content::Referrer(), page_transition, std::string());
 }
 
@@ -126,6 +210,12 @@ void HandleOauthProtocol(const GURL& url,
 bool IsOauthProtocol(const GURL& url) {
 #if BUILDFLAG(BINANCE_ENABLED)
   if (url.SchemeIs(kBinanceScheme)) {
+    return true;
+  }
+#endif
+
+#if BUILDFLAG(BRAVE_REWARDS_ENABLED)
+  if (url.SchemeIs(kRewardsScheme)) {
     return true;
   }
 #endif
