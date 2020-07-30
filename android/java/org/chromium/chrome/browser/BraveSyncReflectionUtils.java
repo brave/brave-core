7b44194ea03e39fe30d616929a4ff8fb03a67a7c
AlexeyBarabash <alexey@brave.com>
2020-07-30 19:11:41 +0300

    Removed forgotten debug leftovers

--- android/java/org/chromium/chrome/browser/BraveSyncReflectionUtils.java
+++ android/java/org/chromium/chrome/browser/BraveSyncReflectionUtils.java
@@ -38,12 +38,10 @@ public class BraveSyncReflectionUtils {
     }
 
     public static void showInformers() {
-Log.e(TAG, "showInformers 000");
         if (!BraveConfig.SYNC_ENABLED) {
-Log.e(TAG, "showInformers !BraveConfig.SYNC_ENABLED");
             return;
         }
-Log.e(TAG, "showInformers will show");
+
         try {
             Method method = Class.forName("org.chromium.chrome.browser.BraveSyncInformers").getDeclaredMethod("show");
             method.invoke(null);
@@ -51,71 +49,4 @@ Log.e(TAG, "showInformers will show");
             Log.e(TAG, "Cannot show sync informers with reflection ", e);
         }
     }
-
-    // public static boolean isSyncEnabledThroughConfig() {
-    //     if (BraveConfig.SYNC_ENABLED) {
-    //         return true;
-    //     } else {
-    //         return false;
-    //     }
-    // }
-
-//     public static boolean getSyncV1WasEnabled() {
-// Log.e(TAG, "BraveSyncWorkerHolder.getSyncV1WasEnabled 000++");
-//         Object braveSyncWorker = get();
-// Log.e(TAG, "BraveSyncWorkerHolder.getSyncV1WasEnabled braveSyncWorker="+braveSyncWorker);
-//         if (braveSyncWorker == null) return false;
-//
-// //         Class<?> c = braveSyncWorker.getClass();
-// // Log.e(TAG, "BraveSyncWorkerHolder.getSyncV1WasEnabled c.getName()="+c.getName());
-// //         Method[] allMethods = c.getDeclaredMethods();
-// //         for (Method m : allMethods) {
-// // Log.e(TAG, "BraveSyncWorkerHolder.getSyncV1WasEnabled m.getName()="+m.getName());
-// //         }
-// //
-// // Log.e(TAG, "BraveSyncWorkerHolder.getSyncV1WasEnabled---------------");
-// //         allMethods = c.getMethods();
-// //         for (Method m : allMethods) {
-// // Log.e(TAG, "BraveSyncWorkerHolder.getSyncV1WasEnabled getMethods m.getName()="+m.getName());
-// //         }
-//
-//         try {
-//             java.lang.reflect.Method method = braveSyncWorker.getClass().getDeclaredMethod("getSyncV1WasEnabled");
-//             Object ret = method.invoke(braveSyncWorker);
-//             return (boolean)ret;
-//         } catch (Exception e) {
-//             Log.e(TAG, "Cannot make call with reflection ", e);
-//         }
-//         return false;
-//     }
-//
-//     public static boolean getSyncV2MigrateNoticeDismissed() {
-// Log.e(TAG, "BraveSyncWorkerHolder.getSyncV2MigrateNoticeDismissed 000");
-//         Object braveSyncWorker = get();
-//         if (braveSyncWorker == null) return false;
-//
-//         java.lang.reflect.Method method;
-//         try {
-//             method = braveSyncWorker.getClass().getMethod("getSyncV2MigrateNoticeDismissed");
-//             Object ret = method.invoke(braveSyncWorker);
-//             return (boolean)ret;
-//         } catch (Exception e) {
-//             Log.e(TAG, "Cannot make call with reflection ", e);
-//         }
-//         return false;
-//     }
-//
-//     public static void setSyncV2MigrateNoticeDismissed(boolean isDismissed) {
-//     Log.e(TAG, "BraveSyncWorkerHolder.setSyncV2MigrateNoticeDismissed 000 isDismissed="+isDismissed);
-//         Object braveSyncWorker = get();
-//         if (braveSyncWorker == null) return;
-//
-//         java.lang.reflect.Method method;
-//         try {
-//             method = braveSyncWorker.getClass().getMethod("setSyncV2MigrateNoticeDismissed", boolean.class);
-//             method.invoke(braveSyncWorker, isDismissed);
-//         } catch (Exception e) {
-//             Log.e(TAG, "Cannot make call with reflection ", e);
-//         }
-//     }
 }
