b0561e7db0af1b6754868c2f18bdb1c72c39ef98
Anthony Tseng <darkdh@gmail.com>
2020-08-06 11:18:51 -0700

    Force to refresh sync cycle(poll/nudge) when transiting from CONFIGURATION to NORMAL mode.
    This is to mitigate the chance of bookmarks duplication when first time
    sync which is caused by first device send it local bookmarks only after
    a poll interval(60s). When second device wants to do
    BookmarkModelMerger::Merge(), it can't get anything from server because
    first device hasn't sent any. So it commit its own bookmarks which will
    cause duplication.
    
    Note: Chrome has FCM invalidation ready so its first device will receive
    notification within ~10s

---
+++ components/sync/engine_impl/brave_sync_manager_impl.h
@@ -0,0 +1,30 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_SYNC_ENGINE_IMPL_BRAVE_SYNC_MANAGER_IMPL_H_
+#define BRAVE_COMPONENTS_SYNC_ENGINE_IMPL_BRAVE_SYNC_MANAGER_IMPL_H_
+
+#include <string>
+
+#include "components/sync/engine_impl/sync_manager_impl.h"
+
+namespace syncer {
+
+class BraveSyncManagerImpl : public SyncManagerImpl {
+ public:
+  BraveSyncManagerImpl(
+      const std::string& name,
+      network::NetworkConnectionTracker* network_connection_tracker);
+  ~BraveSyncManagerImpl() override;
+
+  void StartSyncingNormally(base::Time last_poll_time) override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(BraveSyncManagerImpl);
+};
+
+}  // namespace syncer
+
+#endif  // BRAVE_COMPONENTS_SYNC_ENGINE_IMPL_BRAVE_SYNC_MANAGER_IMPL_H_
