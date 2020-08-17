133b94305fad55f721e07a1e2be5e3b17e1b4e4d
AlexeyBarabash <AlexeyBarabash@users.noreply.github.com>
2020-08-17 21:42:32 +0300

    Merge pull request #6417 from brave/android_sync_fix_11210
    isChromeSyncEnabled depends on profileSyncService.isSyncRequested()

--- components/sync/android/java/src/org/chromium/components/sync/BraveAndroidSyncSettings.java
+++ components/sync/android/java/src/org/chromium/components/sync/BraveAndroidSyncSettings.java
@@ -5,6 +5,13 @@
 
 package org.chromium.components.sync;
 
+import java.lang.reflect.Method;
+
+//import org.chromium.chrome.browser.sync.ProfileSyncService;
+//import org.chromium.components.sync.AndroidSyncSettings;
+import org.chromium.components.sync.SyncContentResolverDelegate;
+  import org.chromium.base.Log;
+
 // see org.brave.bytecode.BraveAndroidSyncSettingsAdapter
 public class BraveAndroidSyncSettings extends AndroidSyncSettings {
     private boolean mMasterSyncEnabled;
@@ -25,4 +32,33 @@ public class BraveAndroidSyncSettings extends AndroidSyncSettings {
 
     @Override
     public void disableChromeSync() { }
+
+    // We need to override this to make able
+    // DevicePickerBottomSheetContent.createContentView send the link
+    // For Brave we don't have an account in Android system account,
+    // so pretend sync for Brave "account" is always on when sync is requsted
+    @Override
+    public boolean isChromeSyncEnabled() {
+        // On Chrome 84 ProfileSyncService.java is in //chrome/android:chrome_java
+        // but AndroidSyncSettings.java is in //components/sync/android:sync_java
+        // //chrome/android:chrome_java depends on //components/sync/android:sync_java
+        // so I cannot add deps to //components/sync/android:sync_java
+        // Therefore doing with reflection
+        try {
+            Method methodGet = Class.forName("org.chromium.chrome.browser.sync.ProfileSyncService").getDeclaredMethod("get");
+            Object profileSyncService = methodGet.invoke(null);
+            if (profileSyncService == null) {
+                return false;
+            }
+
+            Method methodIsSyncRequested = Class.forName("org.chromium.chrome.browser.sync.ProfileSyncService").getDeclaredMethod("isSyncRequested");
+            Object isSyncRequested = methodIsSyncRequested.invoke(profileSyncService);
+            boolean bIsSyncRequested = (boolean)isSyncRequested;
+            return bIsSyncRequested;
+        } catch (Exception e) {
+            assert false;
+            // failsafe, but should never reach
+            return true;
+        }
+    }
 }
