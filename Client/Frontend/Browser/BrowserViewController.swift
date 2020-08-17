cc4a0571f9b7b50994ee53e6b6229b1a282f3fdc
Kyle Hickinson <git@kylehickinson.com>
2020-08-17 11:55:44 -0400

    Fix NTP ref cycle leak
    Also removes static definition on the feed data source in BVC

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -59,7 +59,7 @@ class BrowserViewController: UIViewController {
     
     // Single data source used for all favorites vcs
     let backgroundDataSource = NTPDataSource()
-    static let feedDataSource = FeedDataSource()
+    let feedDataSource = FeedDataSource()
     
     var loadQueue = Deferred<Void>()
 
@@ -1135,7 +1135,7 @@ class BrowserViewController: UIViewController {
             let ntpController = NewTabPageViewController(tab: selectedTab,
                                                          profile: profile,
                                                          dataSource: backgroundDataSource,
-                                                         feedDataSource: Self.feedDataSource,
+                                                         feedDataSource: feedDataSource,
                                                          rewards: rewards)
             ntpController.delegate = self
             selectedTab.newTabPageViewController = ntpController
