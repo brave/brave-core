0a0522fb5d8c753131ae401fe2ed9ae3f3d2c5bc
Anthony Tseng <darkdh@gmail.com>
2020-08-31 13:36:25 -0700

    Move TorControl under browser

--- browser/tor/tor_control_unittest.cc
+++ browser/tor/tor_control_unittest.cc
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "brave/common/tor/tor_control.h"
+#include "brave/browser/tor/tor_control.h"
 
 #include "testing/gtest/include/gtest/gtest.h"
 
