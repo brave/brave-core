2919270899e1120444b3e2eeba273f96e9de7490
Jacob Lamont <jlamont@brave.com>
2020-09-04 16:15:06 -0400

    adds all but chart to asset details view

--- components/brave_new_tab_ui/components/default/cryptoDotCom/index.tsx
+++ components/brave_new_tab_ui/components/default/cryptoDotCom/index.tsx
@@ -4,10 +4,23 @@
 
 // MOCK DATA
 const topMovers = [
-  { currency: "BTC", tickerPrice: 18484.10, tickerChange: 210.01 },
-  { currency: "ETH", tickerPrice: 334.54, tickerChange: 152.78 },
-  { currency: "CRO", tickerPrice: 1.20, tickerChange: 89.12 }
-];
+  { currency: 'BTC', tickerPrice: 18484.10, tickerChange: 210.01 },
+  { currency: 'ETH', tickerPrice: 334.54, tickerChange: 152.78 },
+  { currency: 'CRO', tickerPrice: 1.20, tickerChange: 89.12 }
+]
+
+const currencyData = {
+  btc: {
+    volume24hr: 1271112419,
+    chartData: [
+      { x: 1, y: 2 },
+      { x: 2, y: 3 },
+      { x: 3, y: 5 },
+      { x: 4, y: 4 },
+      { x: 5, y: 7 }
+    ]
+  }
+}
 
 import * as React from 'react'
 
@@ -17,6 +30,7 @@ import { StyledTitleTab } from '../widgetTitleTab'
 import currencyNames from './data'
 
 import {
+  ActionAnchor,
   ActionButton,
   AssetIcon,
   AssetIconWrapper,
@@ -31,7 +45,7 @@ import {
   StyledTitle,
   StyledTitleText,
   Text,
-  WidgetWrapper,
+  WidgetWrapper
 } from './style'
 // import {
 //   SearchIcon,
@@ -71,12 +85,6 @@ class CryptoDotCom extends React.PureComponent<Props, State> {
     }
   }
 
-  componentDidMount () {}
-
-  componentDidUpdate (prevProps: Props) {}
-
-  componentWillUnmount () {}
-
   setSelectedView = (view: string) => {
     this.setState({
       selectedView: view
@@ -93,7 +101,7 @@ class CryptoDotCom extends React.PureComponent<Props, State> {
     const iconBgColor = cryptoColors[key] || '#fff'
 
     return (
-      <AssetIconWrapper style={{ background: iconBgColor, color: "#000" }}>
+      <AssetIconWrapper style={{ background: iconBgColor, color: '#000' }}>
         <AssetIcon
           isDetail={isDetail}
           className={`crypto-icon icon-${key}`}
@@ -103,83 +111,140 @@ class CryptoDotCom extends React.PureComponent<Props, State> {
   }
 
   renderIndexView () {
-    const { optInBTCPrice, onBtcPriceOptIn } = this.props;
-    const currency = "BTC";
-    return <>
-      <Box isFlex>
-        <FlexItem style={{ paddingLeft: 5, paddingRight: 5 }}>
-          {this.renderIconAsset(currency.toLowerCase())}
-        </FlexItem>
-        <FlexItem>
-            <Text>{currency}</Text>
-            <Text small $color="light">{currencyNames[currency]}</Text>
-        </FlexItem>
-        <FlexItem textAlign="right" flex={1}>
-          {optInBTCPrice ? (
-            <>
-              <Text>$18,484.10</Text>
-              <Text $color="green">210.01%</Text>
-            </>
-          ) : (
-            <PlainButton onClick={onBtcPriceOptIn} textColor="green">Show Price</PlainButton>
-          )}
-        </FlexItem>
-        <FlexItem style={{ paddingLeft: 5 }}>
-          <ActionButton small light>BUY</ActionButton>
-        </FlexItem>
-      </Box>
-      <Text $hasSpacing>Connect to Crypto.com to view buy and trade crypto, view account balance and upcoming events.</Text>
-      <ActionButton onClick={() => this.setSelectedView('topMovers')}>View Top Movers</ActionButton>
-    </>
+    const { optInBTCPrice, onBtcPriceOptIn } = this.props
+    const currency = 'BTC'
+    return (
+      <>
+        <Box isFlex={true}>
+          <FlexItem style={{ paddingLeft: 5, paddingRight: 5 }}>
+            {this.renderIconAsset(currency.toLowerCase())}
+          </FlexItem>
+          <FlexItem>
+              <Text>{currency}</Text>
+              <Text small={true} $color='light'>{currencyNames[currency]}</Text>
+          </FlexItem>
+          <FlexItem textAlign='right' flex={1}>
+            {optInBTCPrice ? (
+              <>
+                <Text>{new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'USD', currencyDisplay: 'narrowSymbol' }).format(18484.10)}</Text>
+                <Text $color='green'>{210.01}%</Text>
+              </>
+            ) : (
+              <PlainButton onClick={onBtcPriceOptIn} textColor='green'>Show Price</PlainButton>
+            )}
+          </FlexItem>
+          <FlexItem style={{ paddingLeft: 5 }}>
+            <ActionButton small={true} light={true}>BUY</ActionButton>
+          </FlexItem>
+        </Box>
+        <Text center={true} style={{ marginTop: '1em' }}>Get 2% bonus on deposits</Text>
+        <Text center={true}>Stop paying trading fees</Text>
+        <ActionAnchor href='#' style={{ margin: '1em 0' }}>Buy Bitcoin Now</ActionAnchor>
+        <PlainButton textColor='light' onClick={this.setSelectedView.bind(this, 'topMovers')} style={{ margin: '0 auto' }}>View Crypto.com Markets</PlainButton>
+      </>
+    )
   }
 
   renderTopMoversView () {
     return (
       <List>
-        {topMovers.map(({currency, tickerPrice, tickerChange}) => (
-          <ListItem isFlex onClick={() => this.setSelectedAsset(currency)}>
+        {topMovers.map(({ currency, tickerPrice, tickerChange }) => (
+          <ListItem isFlex={true} onClick={this.setSelectedAsset.bind(this, currency)}>
             <FlexItem style={{ paddingLeft: 5, paddingRight: 5 }}>
               {this.renderIconAsset(currency.toLowerCase())}
             </FlexItem>
             <FlexItem>
               <Text>{currency}</Text>
-              <Text small $color="light">{currencyNames[currency]}</Text>
+              <Text small={true} $color='light'>{currencyNames[currency]}</Text>
             </FlexItem>
-            <FlexItem textAlign="right" flex={1}>
-              <Text>${tickerPrice.toFixed(2)}</Text>
-              <Text $color={tickerChange > 0 ? "green" : "red"}>{tickerChange}%</Text>
+            <FlexItem textAlign='right' flex={1}>
+              <Text>{new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'USD', currencyDisplay: 'narrowSymbol' }).format(tickerPrice)}</Text>
+              <Text $color={tickerChange > 0 ? 'green' : 'red'}>{tickerChange}%</Text>
             </FlexItem>
           </ListItem>
         ))}
       </List>
-    );
+    )
   }
 
   renderAssetDetailView () {
-    const { selectedAsset: currency } = this.state;
+    const { selectedAsset: currency } = this.state
+    const assetData = currencyData[currency.toLowerCase()]
     return (
-      <Box isFlex>
-        <FlexItem>
-          <BackArrow>
-            <CaratLeftIcon onClick={() => this.setSelectedAsset('')} />
-          </BackArrow>
-        </FlexItem>
-        <FlexItem style={{ paddingRight: 5 }}>
-          {this.renderIconAsset(currency.toLowerCase())}
-        </FlexItem>
-        <FlexItem flex={1}>
+      <Box noPadding={true}>
+        <FlexItem
+          hasPadding={true}
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: '100%',
+            borderBottom: '1px solid #979797'
+          }}
+        >
+          <FlexItem>
+            <BackArrow>
+              <CaratLeftIcon onClick={this.setSelectedAsset.bind(this, '')} />
+            </BackArrow>
+          </FlexItem>
+          <FlexItem style={{ paddingRight: 5 }}>
+            {this.renderIconAsset(currency.toLowerCase())}
+          </FlexItem>
+          <FlexItem flex={1}>
             <Text>{currency}</Text>
-            <Text small $color="light">{currencyNames[currency]}</Text>
+            <Text small={true} $color='light'>
+              {currencyNames[currency]}
+            </Text>
+          </FlexItem>
+          <FlexItem style={{ paddingLeft: 5 }}>
+            <ActionButton small={true} light={true}>
+              BUY
+            </ActionButton>
+          </FlexItem>
         </FlexItem>
-        <FlexItem style={{ paddingLeft: 5 }}>
-          <ActionButton small light>BUY</ActionButton>
+        <FlexItem
+          hasPadding={true}
+          style={{
+            width: '100%',
+            borderBottom: '1px solid #979797'
+          }}
+        >
+          {/* <VictoryLine
+            padding={0}
+            style={{
+              data: { stroke: "#44B0FF" }
+            }}
+            animate={{
+              duration: 2000,
+              onLoad: { duration: 1000 }
+            }}
+            data={assetData.chartData}
+          /> */}
+        </FlexItem>
+        <FlexItem
+          hasPadding={true}
+          style={{
+            width: '100%'
+          }}
+        >
+          <div style={{ marginTop: '0.2em' }}>
+            <Text small={true} $color='light' style={{ paddingBottom: '0.2rem' }}>
+              24HR VOLUME
+            </Text>
+            <Text weight={500}>{new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'USD', currencyDisplay: 'narrowSymbol' }).format(assetData.volume24hr)} USDT</Text>
+          </div>
+          <div style={{ marginTop: '1em' }}>
+            <Text small={true} $color='light' style={{ paddingBottom: '0.2rem' }}>SUPPORTED PAIRS</Text>
+            <ActionButton small={true} light={true} inline={true} style={{ marginRight: 5 }}>BTC/CRO</ActionButton>
+            <ActionButton small={true} light={true} inline={true}>BTC/USDT</ActionButton>
+          </div>
         </FlexItem>
       </Box>
     )
   }
 
   renderTitle () {
-    const { showContent } = this.props;
+    const { showContent } = this.props
     return (
       <Header showContent={showContent}>
         <StyledTitle>
@@ -205,14 +270,14 @@ class CryptoDotCom extends React.PureComponent<Props, State> {
   }
 
   renderSelectedView () {
-    const { selectedView } = this.state;
-    const { selectedAsset } = this.state;
-    
+    const { selectedView } = this.state
+    const { selectedAsset } = this.state
+
     if (selectedAsset) {
-      return this.renderAssetDetailView();
+      return this.renderAssetDetailView()
     }
 
-    switch(selectedView) {
+    switch (selectedView) {
       case 'topMovers':
         return this.renderTopMoversView()
       default:
--- components/brave_new_tab_ui/components/default/cryptoDotCom/style.ts
+++ components/brave_new_tab_ui/components/default/cryptoDotCom/style.ts
@@ -4,16 +4,21 @@
 
 import styled from 'styled-components'
 interface StyleProps {
-  as?: any
-  clickable?: boolean
   $color?: string
+  $hasSpacing?: boolean
+  as?: any
+  center?: boolean
+  clickable?: boolean
+  column?: boolean
   disabled?: boolean
   flex?: number
   hasBorder?: boolean
+  hasPadding?: boolean
   hide?: boolean
   hideBalance?: boolean
   hideOverflow?: boolean
-  light?: boolean
+  href?: string
+  inline?: boolean
   isActive?: boolean
   isAsset?: boolean
   isAuth?: boolean
@@ -24,87 +29,95 @@ interface StyleProps {
   isSmall?: boolean
   isSummary?: boolean
   itemsShowing?: boolean
-  $hasSpacing?: boolean
+  light?: boolean
   noPadding?: boolean
+  onClick?: Function
   position?: string
   showContent?: boolean
   small?: boolean
+  style?: object
   textAlign?: string
   textColor?: string
   userAuthed?: boolean
-  onClick?: Function
+  weight?: string | number
 }
 
 const colorNameToHex = {
-  green: "#58B2A9",
-  red: "#D986A2",
-  light: "rgba(255, 255, 255, 0.6)"
-};
+  green: '#58B2A9',
+  red: '#D986A2',
+  light: 'rgba(255, 255, 255, 0.6)'
+}
 
-function getColor(p: any) {
-  let colorName;
+function getColor (p: any) {
+  let colorName
   if (!/(string|undefined)/.test(typeof p)) {
-    const keys = Object.keys(p);
-    colorName = keys.find((key) => key in colorNameToHex);
+    const keys = Object.keys(p)
+    colorName = keys.find((key) => key in colorNameToHex)
   } else {
-    colorName = p;
+    colorName = p
   }
-  return colorNameToHex[colorName];
+  return colorNameToHex[colorName]
 }
 
 export const Text = styled<StyleProps, 'p'>('p')`
-  font-family: ${p => p.theme.fontFamily.heading};
-  font-weight: ${p => p.small ? "500" : "normal"};
-  color: ${p => getColor(p.$color) || "#ffffff"};
-  font-size: ${p => p.small ? "11px" : "14px"};
-  margin: ${p => p.$hasSpacing ? "20px 0px" : "0px"};
-`;
+  font-family: ${(p) => 'Poppins'};
+  font-weight: ${(p) => (p.weight || (p.small ? '500' : 'normal'))};
+  color: ${(p) => getColor(p.$color) || '#ffffff'};
+  font-size: ${(p) => (p.small ? '11px' : '14px')};
+  margin: ${(p) => (p.$hasSpacing ? '20px 0px' : '0px')};
+  text-align: ${(p) => (p.center ? 'center' : 'inherit')}
+ `
 
 export const Box = styled<StyleProps, 'div'>('div')`
   border: 1px solid #979797;
-  padding: ${p => p.noPadding ? "0" : "5px"};
+  padding: ${(p) => (p.noPadding ? '0' : '0.5em')};
   border-radius: 2px;
-  display: ${(p) => (p.isFlex ? "flex" : "block")};
+  display: ${(p) => (p.isFlex ? 'flex' : 'block')};
 
-  ${p => p.isFlex && `
-    justify-content: space-between;
-    align-items: center;
-  `}
-`;
+   ${(p) =>
+     p.isFlex &&
+     `
+      flex-direction: ${p.column ? 'column' : 'row'};
+      justify-content: space-between;
+      align-items: center;
+    `}
+ `
 
 export const FlexItem = styled<StyleProps, 'div'>('div')`
-  flex: ${p => p.flex || "inherit"};
-  text-align: ${p => p.textAlign || "left"}
-`;
+  flex: ${(p) => p.flex || 'inherit'};
+  text-align: ${(p) => p.textAlign || 'left'};
+  padding: ${(p) => (p.hasPadding ? '0.5em' : '0px')};
+ `
 
 export const PlainButton = styled<StyleProps, 'button'>('button')`
+  display: block;
   background: none;
   border: none;
   cursor: pointer;
-  color: ${(p) => getColor(p.textColor) || "#ffffff"};
+  color: ${(p) => getColor(p.textColor) || '#ffffff'};
 
-  &:focus {
-    outline: 0;
-  }
-`;
+   &:focus {
+     outline: 0;
+   }
+ `
 
 export const WidgetWrapper = styled<StyleProps, 'div'>('div')`
   color: white;
   padding: 6px 20px 30px 20px;
   border-radius: 6px;
   position: relative;
-  font-family: ${p => p.theme.fontFamily.body};
+  font-family: ${(p) => 'Poppins'};
   font-size: 14px;
   overflow: hidden;
   min-width: 284px;
   background: rgba(15, 28, 45, 0.7);
   backdrop-filter: blur(16px);
-`
+ `
 
 export const Header = styled<StyleProps, 'div'>('div')`
   text-align: left;
-  margin-bottom: ${p => p.showContent ? '15' : '0'}px;
-`
+  margin-bottom: ${(p) => (p.showContent ? '15' : '0')}px;
+ `
 
 export const StyledTitle = styled<{}, 'div'>('div')`
   margin-top: 6px;
@@ -114,68 +127,85 @@ export const StyledTitle = styled<{}, 'div'>('div')`
   font-size: 18px;
   font-weight: 600;
   color: #fff;
-  font-family: ${p => p.theme.fontFamily.heading};
-`
+  font-family: ${(p) => 'Poppins'};
+ `
 
 export const CryptoDotComIcon = styled<{}, 'div'>('div')`
   width: 27px;
   height: 27px;
   margin-right: 7px;
   margin-left: 2px;
-`
+ `
 
 export const StyledTitleText = styled<{}, 'div'>('div')`
   margin-top: 4px;
-`
+ `
 
 export const List = styled(Box)`
-  overflow-y: ${p => p.hideOverflow ? 'hidden' : 'scroll'};
+  overflow-y: ${(p) => (p.hideOverflow ? 'hidden' : 'scroll')};
   height: 260px;
   padding: 0;
   margin: 0;
-`
+ `
 
 export const ListItem = styled<StyleProps, 'li'>('li')`
   border-bottom: 1px solid #979797;
   padding: 5px;
   border-radius: 2px;
-  display: ${p => p.isFlex ? "flex" : "block"};
-  cursor: ${p => p.onClick ? "pointer" : "initial"}
-  
-  ${p => p.isFlex && `
+  display: ${(p) => (p.isFlex ? 'flex' : 'block')};
+  cursor: ${(p) => (p.onClick ? 'pointer' : 'initial')}
+  ${(p) =>
+    p.isFlex && `
     justify-content: space-between;
     align-items: center;
-  `}
-`;
+  `};
+`
 
 export const AssetIconWrapper = styled<StyleProps, 'div'>('div')`
   height: 18px;
   width: 18px;
   border-radius: 100px;
   display: inline-block;
-`
+ `
 
 export const AssetIcon = styled<StyleProps, 'span'>('span')`
   font-size: 0.9em;
   margin-top: 0.2em;
   margin-left: 0.2em;
-`
+ `
 
 export const BackArrow = styled<{}, 'div'>('div')`
   width: 20px;
   cursor: pointer;
-`
+ `
 
 export const ActionButton = styled<StyleProps, 'button'>('button')`
-  font-family: ${p => p.theme.fontFamily.heading};
-  font-size: ${p => p.small ? "13px" : "15px"};
-  font-weight: ${p => p.small ? "500" : "bold"};
+  font-family: ${(p) => 'Poppins'};
+  font-size: ${(p) => (p.small ? '13px' : '15px')};
+  font-weight: ${(p) => (p.small ? '500' : 'bold')};
   border-radius: 20px;
-  width: 100%;
-  background: ${p => p.light ? "rgba(255, 255, 255, 0.21)" : "#44B0FF"};
+  width: ${(p) => (p.inline ? 'auto' : '100%')};
+  background: ${(p) => (p.light ? 'rgba(255, 255, 255, 0.21)' : '#44B0FF')};
   border: 0;
-  padding: ${p => p.small ? "6px 10px" : "10px 0px"};
+  padding: ${(p) => (p.small ? '6px 10px' : '10px 0px')};
   cursor: pointer;
-  color: #FFFFFF;
+  color: #ffffff;
   line-height: 1;
-`
+ `
+
+export const ActionAnchor = styled<StyleProps, 'a'>('a')`
+  font-family: ${(p) => 'Poppins'};
+  font-size: ${(p) => (p.small ? '13px' : '15px')};
+  font-weight: ${(p) => (p.small ? '500' : 'bold')};
+  border-radius: 20px;
+  width: ${(p) => (p.inline ? 'auto' : '100%')};
+  background: ${(p) => (p.light ? 'rgba(255, 255, 255, 0.21)' : '#44B0FF')};
+  border: 0;
+  padding: ${(p) => (p.small ? '6px 10px' : '10px 0px')};
+  cursor: pointer;
+  color: #ffffff;
+  line-height: 1;
+  display: block;
+  text-align: center;
+  text-decoration: none;
+ `
