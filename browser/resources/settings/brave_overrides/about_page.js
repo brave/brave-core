b1380ee4f8972db5dcdf54dc97c4cec5d927076a
Brian Clifton <brian@clifton.me>
2020-09-16 13:39:35 -0700

    Update help link to be relevant for Windows users
    The <if> expression will get evaluated properly by grit
    
    Fixes https://github.com/brave/brave-browser/issues/11746

--- browser/resources/settings/brave_overrides/about_page.js
+++ browser/resources/settings/brave_overrides/about_page.js
@@ -16,5 +16,17 @@ RegisterPolymerTemplateModifications({
       }
       version.innerHTML = '<a id="release-notes" target="_blank" href="https://brave.com/latest/">' + version.innerHTML + '</a>'
     }
+
+    // Help link shown if update fails
+    const updateStatusMessageLink = section.querySelector('#updateStatusMessage a')
+    if (updateStatusMessageLink) {
+      // <if expr="is_win">
+      updateStatusMessageLink.href = 'https://support.brave.com/hc/en-us/articles/360042816611-Why-isn-t-Brave-updating-automatically-on-Windows-'
+      // </if>
+
+      // <if expr="not is_win">
+      updateStatusMessageLink.href = 'https://community.brave.com?p=update_error'
+      // </if>
+    }
   }
 })
