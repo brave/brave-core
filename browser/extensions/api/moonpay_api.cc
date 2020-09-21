b57dabd1c4e58a884e472f7a5c090db43e43691e
ryanml <rlanese@brave.com>
2020-09-21 00:57:15 -0700

    Adding Moonpay API

---
+++ browser/extensions/api/moonpay_api.cc
@@ -0,0 +1,35 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/browser/extensions/api/moonpay_api.h"
+
+#include <string>
+#include <memory>
+
+#include "base/environment.h"
+#include "brave/browser/profiles/profile_util.h"
+#include "brave/components/moonpay/browser/regions.h"
+#include "brave/components/ntp_widget_utils/browser/ntp_widget_utils_region.h"
+#include "chrome/browser/profiles/profile.h"
+
+namespace extensions {
+namespace api {
+
+ExtensionFunction::ResponseAction
+MoonpayIsBitcoinDotComSupportedFunction::Run() {
+  Profile* profile = Profile::FromBrowserContext(browser_context());
+
+  if (brave::IsTorProfile(profile)) {
+    return RespondNow(Error("Not available in Tor profile"));
+  }
+
+  bool is_supported = ntp_widget_utils::IsRegionSupported(
+      profile->GetPrefs(), moonpay::bitcoin_dot_com_supported_regions, true);
+  return RespondNow(OneArgument(
+      std::make_unique<base::Value>(is_supported)));
+}
+
+}  // namespace api
+}  // namespace extensions
---
+++ browser/extensions/api/moonpay_api.h
@@ -0,0 +1,32 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_BROWSER_EXTENSIONS_API_MOONPAY_API_H_
+#define BRAVE_BROWSER_EXTENSIONS_API_MOONPAY_API_H_
+
+#include <map>
+#include <string>
+
+#include "extensions/browser/extension_function.h"
+
+class Profile;
+
+namespace extensions {
+namespace api {
+
+class MoonpayIsBitcoinDotComSupportedFunction :
+    public ExtensionFunction {
+ public:
+  DECLARE_EXTENSION_FUNCTION("moonpay.isBitcoinDotComSupported", UNKNOWN)
+
+ protected:
+  ~MoonpayIsBitcoinDotComSupportedFunction() override {}
+  ResponseAction Run() override;
+};
+
+}  // namespace api
+}  // namespace extensions
+
+#endif  // BRAVE_BROWSER_EXTENSIONS_API_MOONPAY_API_H_
