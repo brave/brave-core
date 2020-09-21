3a4bca2cf5127dcfb5655b3c4a999601f89d3d49
bridiver <github@brianjohnson.cc>
2020-09-21 10:19:57 -0700

    add missing browser_resources values

---
+++ chromium_src/chrome/grit/browser_resources.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2020 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_GRIT_BROWSER_RESOURCES_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_GRIT_BROWSER_RESOURCES_H_
+
+#include "brave/grit/brave_browser_resources.h"
+#include "../gen/chrome/grit/browser_resources.h"
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_GRIT_BROWSER_RESOURCES_H_
