f89c0ca1eb33b3d05cd5b40e87b60b8ee7afe6e9
ryanml <rlanese@brave.com>
2020-09-21 23:40:18 -0700

    Adding widget logic, styled components

--- components/brave_new_tab_ui/components/default/bitcoinDotCom/index.tsx
+++ components/brave_new_tab_ui/components/default/bitcoinDotCom/index.tsx
@@ -6,9 +6,16 @@ import * as React from 'react'
 import createWidget from '../widget/index'
 import * as Styled from './style'
 import { StyledTitleTab } from '../widgetTitleTab'
+import assetIcons from './assets/icons'
 import BitcoinDotComLogo from './assets/logo.png'
+import { CaratDownIcon } from 'brave-ui/components/icons'
 
 interface State {
+  selectedAsset: string
+  selectedFiat: string
+  currentAmount: string
+  assetsShowing: boolean
+  fiatCurrenciesShowing: boolean
 }
 
 interface Props {
@@ -18,8 +25,33 @@ interface Props {
 }
 
 class BitcoinDotCom extends React.PureComponent<Props, State> {
+  private assets: Record<string, string>
+  private fiatCurrencies: string[]
+
   constructor (props: Props) {
     super(props)
+    this.state = {
+      selectedAsset: 'BCH',
+      selectedFiat: 'EUR',
+      currentAmount: '',
+      assetsShowing: false,
+      fiatCurrenciesShowing: false
+    }
+    this.assets = {
+      'BCH': 'Bitcoin Cash',
+      'BTC': 'Bitcoin',
+      'ETH': 'Ethereum',
+      'LTC': 'Litecoin',
+      'XLM': 'Stellar',
+      'XRP': 'Ripple'
+    }
+    this.fiatCurrencies = [
+      'EUR',
+      'AUD',
+      'CAD',
+      'GBP',
+      'USD'
+    ]
   }
 
   renderTitle () {
@@ -29,13 +61,196 @@ class BitcoinDotCom extends React.PureComponent<Props, State> {
       <Styled.Header>
         <Styled.Title contentShowing={showContent}>
           <Styled.BitcoinDotComIcon>
-            <Styled.BitcoinDotComIconImg src={BitcoinDotComLogo} />
+            <img src={BitcoinDotComLogo} />
           </Styled.BitcoinDotComIcon>
           <Styled.TitleText>
             {'Bitcoin.com'}
           </Styled.TitleText>
         </Styled.Title>
       </Styled.Header>
+    )
+  }
+
+  toggleAssetsShowing = () => {
+    this.setState({
+      assetsShowing: !this.state.assetsShowing
+    })
+  }
+
+  setSelectedAsset = (asset: string) => {
+    this.setState({
+      assetsShowing: false,
+      selectedAsset: asset
+    })
+  }
+
+  setCurrentAmount = ({ target }: any) => {
+    this.setState({
+      currentAmount: target.value
+    })
+  }
+
+  toggleFiatCurrenciesShowing = () => {
+    this.setState({
+      fiatCurrenciesShowing: !this.state.fiatCurrenciesShowing
+    })
+  }
+
+  setSelectedFiat = (fiat: string) => {
+    this.setState({
+      fiatCurrenciesShowing: false,
+      selectedFiat: fiat
+    })
+  }
+
+  openBuyURL = () => {
+    const {
+      currentAmount,
+      selectedAsset,
+      selectedFiat
+    } = this.state
+
+    const amount = currentAmount.trim()
+    const asset = selectedAsset.toLowerCase()
+    const fiat = selectedFiat.toLowerCase()
+
+    if (amount.length) {
+      window.open(`https://bitcoincom.moonpay.io/?currencyCode=${asset}&baseCurrencyCode=${fiat}&baseCurrencyAmount=${amount}`, '_blank', 'noopener')
+    }
+  }
+
+  openInfoURL = () => {
+    window.open('https://bitcoincom.moonpay.io/', '_blank', 'noopener')
+  }
+
+  renderCurrencyInput () {
+    const { assetsShowing, selectedAsset } = this.state
+
+    return (
+      <>
+        <Styled.InputHeader>
+          <Styled.InputLabel>
+            {'Currency'}
+          </Styled.InputLabel>
+        </Styled.InputHeader>
+        <Styled.Dropdown
+          dropdownShowing={assetsShowing}
+          onClick={this.toggleAssetsShowing}
+        >
+          <Styled.AssetItem>
+            <Styled.AssetImage src={assetIcons[selectedAsset]} />
+            <Styled.AssetTitle>
+              {this.assets[selectedAsset]}
+            </Styled.AssetTitle>
+            <Styled.AssetSymbol>
+              {selectedAsset}
+            </Styled.AssetSymbol>
+          </Styled.AssetItem>
+          <Styled.DropdownIcon>
+            <CaratDownIcon />
+          </Styled.DropdownIcon>
+        </Styled.Dropdown>
+        {assetsShowing && this.renderCurrencyItems()}
+      </>
+    )
+  }
+
+  renderCurrencyItems () {
+    const { selectedAsset } = this.state
+
+    return (
+      <Styled.CurrencyItems>
+        {Object.keys(this.assets).map((asset: string) => {
+          if (asset === selectedAsset) {
+            return null
+          }
+
+          return (
+            <Styled.AssetItem
+              key={`${asset}-item`}
+              onClick={this.setSelectedAsset.bind(this, asset)}
+            >
+              <Styled.AssetImage src={assetIcons[asset]} />
+              <Styled.AssetTitle>
+                {this.assets[asset]}
+              </Styled.AssetTitle>
+              <Styled.AssetSymbol>
+                {asset}
+              </Styled.AssetSymbol>
+            </Styled.AssetItem>
+          )
+        })}
+      </Styled.CurrencyItems>
+    )
+  }
+
+  renderAmountInput () {
+    const { fiatCurrenciesShowing, selectedFiat } = this.state
+
+    return (
+      <>
+        <Styled.InputHeader>
+          <Styled.InputLabel>
+            {'Amount'}
+          </Styled.InputLabel>
+        </Styled.InputHeader>
+        <Styled.AmountInputWrapper>
+          <Styled.AmountInput
+            type={'text'}
+            placeholder={'Enter amount'}
+            onChange={this.setCurrentAmount}
+            dropdownShowing={fiatCurrenciesShowing}
+            onClick={this.toggleFiatCurrenciesShowing}
+          />
+          <Styled.FiatDropdown
+            dropdownShowing={fiatCurrenciesShowing}
+            onClick={this.toggleFiatCurrenciesShowing}
+          >
+            <Styled.FiatLabel>
+              {selectedFiat}
+            </Styled.FiatLabel>
+            <Styled.DropdownIcon isFiat={true}>
+              <CaratDownIcon />
+            </Styled.DropdownIcon>
+          </Styled.FiatDropdown>
+          {fiatCurrenciesShowing && this.renderFiatItems()}
+        </Styled.AmountInputWrapper>
+      </>
+    )
+  }
+
+  renderFiatItems () {
+    return (
+      <Styled.FiatItems>
+        {this.fiatCurrencies.map((fiat: string) => {
+          return (
+            <Styled.FiatItem
+              key={`${fiat}-fiat`}
+              onClick={this.setSelectedFiat.bind(this, fiat)}
+            >
+              {fiat}
+            </Styled.FiatItem>
+          )
+        })}
+      </Styled.FiatItems>
+    )
+  }
+
+  renderFooter () {
+    return (
+      <Styled.FooterWrapper>
+        <Styled.BuyButton onClick={this.openBuyURL}>
+          {'Buy'}
+        </Styled.BuyButton>
+        <Styled.FooterInfo>
+          <Styled.LinkLabel>
+            {'New to crypto?'}
+          </Styled.LinkLabel>
+          <Styled.Link onClick={this.openInfoURL}>
+            {'Get the wallet here to get started'}
+          </Styled.Link>
+        </Styled.FooterInfo>
+      </Styled.FooterWrapper>
     )
   }
 
@@ -57,7 +272,9 @@ class BitcoinDotCom extends React.PureComponent<Props, State> {
     return (
       <Styled.WidgetWrapper tabIndex={0}>
         {this.renderTitle()}
-        {'Placeholder'}
+        {this.renderCurrencyInput()}
+        {this.renderAmountInput()}
+        {this.renderFooter()}
       </Styled.WidgetWrapper>
     )
   }
--- components/brave_new_tab_ui/components/default/bitcoinDotCom/style.ts
+++ components/brave_new_tab_ui/components/default/bitcoinDotCom/style.ts
@@ -2,10 +2,12 @@
  * License. v. 2.0. If a copy of the MPL was not distributed with this file.
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 interface StyleProps {
+  isFiat?: boolean
   contentShowing?: boolean
+  dropdownShowing?: boolean
 }
 
 export const WidgetWrapper = styled<StyleProps, 'div'>('div')`
@@ -44,5 +46,173 @@ export const TitleText = styled<{}, 'div'>('div')`
   margin-top: 4px;
 `
 
-export const BitcoinDotComIconImg = styled<{}, 'img'>('img')`
+export const InputHeader = styled<{}, 'div'>('div')`
+  margin-top: 15px;
+`
+
+export const InputLabel = styled<{}, 'span'>('span')`
+  color: #7E8496;
+  font-weight: bold;
+`
+
+export const Dropdown = styled<StyleProps, 'div'>('div')`
+  border: 1px solid lightgray;
+  border-radius: 6px;
+  margin-top: 5px;
+  cursor: pointer;
+
+  ${(p) => p.dropdownShowing
+    ? css`
+      border-bottom: none;
+      border-bottom-left-radius: 0px;
+      border-bottom-right-radius: 0px;
+    ` : ''
+  }
+`
+
+export const AssetItem = styled<{}, 'div'>('div')`
+  padding: 10px 7px;
+  cursor: pointer;
+`
+
+export const AssetImage = styled<{}, 'img'>('img')`
+  width: 24px;
+`
+
+export const AssetTitle = styled<{}, 'span'>('span')`
+  font-weight: bold;
+  font-size: 14px;
+  margin: 0px 5px;
+  vertical-align: super;
+`
+
+export const AssetSymbol = styled<{}, 'span'>('span')`
+  vertical-align: super;
+`
+
+export const DropdownIcon = styled<StyleProps, 'div'>('div')`
+  width: 15px;
+  float: right;
+  margin-top: ${p => p.isFiat ? 1 : -28}px;
+  margin-right: ${p => p.isFiat ? 3 : 10}px;
+`
+
+export const CurrencyItems = styled<{}, 'div'>('div')`
+  z-index: 1;
+  background: #fff;
+  overflow-y: scroll;
+  position: absolute;
+  min-width: 244px;
+  max-height: 135px;
+  border: 1px solid lightgray;
+  border-top: none;
+  left: auto;
+  border-bottom-left-radius: 6px;
+  border-bottom-right-radius: 6px;
+`
+
+export const AmountInputWrapper = styled<{}, 'div'>('div')`
+  margin-top: 5px;
+`
+
+export const AmountInput = styled<StyleProps, 'input'>('input')`
+  font-weight: bold;
+  height: 40px;
+  border: 1px solid lightgray;
+  border-radius: 6px;
+  font-size: 15px;
+  padding-left: 10px;
+  border-right: none;
+  border-top-right-radius: 0px;
+  border-bottom-right-radius: 0px;
+
+  ${(p) => p.dropdownShowing
+    ? css`
+      border-bottom-left-radius: 0px;
+    ` : ''
+  }
+`
+
+export const FiatLabel = styled<{}, 'span'>('span')`
+  color: #000;
+  font-weight: bold;
+  margin-left: 8px;
+  font-size: 15px;
+`
+
+export const FiatDropdown = styled<StyleProps, 'div'>('div')`
+  border-radius: 6px;
+  border: 1px solid lightgray;
+  display: inline-block;
+  height: 40px;
+  vertical-align: bottom;
+  width: 64px;
+  padding-top: 10px;
+  cursor: pointer;
+  border-top-left-radius: 0px;
+  border-bottom-left-radius: 0px;
+
+  ${(p) => p.dropdownShowing
+    ? css`
+      border-bottom: none;
+      border-bottom-right-radius: 0px;
+    ` : ''
+  }
+`
+
+export const FiatItems = styled<{}, 'div'>('div')`
+  z-index: 1;
+  background: #fff;
+  overflow-y: scroll;
+  position: absolute;
+  min-width: 244px;
+  max-height: 80px;
+  border: 1px solid lightgray;
+  border-top: none;
+  left: auto;
+  border-bottom-left-radius: 6px;
+  border-bottom-right-radius: 6px;
+`
+
+export const FiatItem = styled<{}, 'div'>('div')`
+  color: #000;
+  font-weight: bold;
+  font-size: 14px;
+  padding: 5px 10px;
+  cursor: pointer;
+`
+
+export const FooterWrapper = styled<{}, 'div'>('div')`
+  margin-top: 15px;
+`
+
+export const BuyButton = styled<{}, 'button'>('button')`
+  width: 100%;
+  border-radius: 15px;
+  border: none;
+  background: #00D68F;
+  color: white;
+  padding: 12px 0px;
+  font-weight: bold;
+  font-size: 13px;
+  cursor: pointer;
+`
+
+export const FooterInfo = styled<{}, 'div'>('div')`
+  font-size: 10px;
+  text-align: center;
+  margin-top: 15px;
+`
+
+export const LinkLabel = styled<{}, 'span'>('span')`
+  font-weight: bold;
+  color: #000;
+  margin-right: 5px;
+`
+
+export const Link = styled<{}, 'span'>('span')`
+  font-weight: bold;
+  color: #00C985;
+  text-decoration: underline;
+  cursor: pointer;
 `
