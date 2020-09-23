7a0602e79c0c7baa1d3997c0dfdf6a03cccd7946
mkarolin <max@brave.com>
2020-09-23 13:45:38 -0400

    Replaced prefs_utils.h patch with an override.
    The name of method we were patching changed from GetWhitelistedKeys to
    GetAllowlistedKeys.
    
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/a8ed6173f4b8897b3ff82a6f9d271b861e5bd3aa
    
    commit a8ed6173f4b8897b3ff82a6f9d271b861e5bd3aa
    Author: Alex Danilo <adanilo@chromium.org>
    Date:   Tue Sep 15 12:03:04 2020 +0000
    
        Rename 'whitelist' occurrences in chrome/browser/extensions
    
        Bug: 1097252

---
+++ chromium_src/chrome/browser/extensions/api/settings_private/prefs_util.h
@@ -0,0 +1,16 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_EXTENSIONS_API_SETTINGS_PRIVATE_PREFS_UTIL_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_EXTENSIONS_API_SETTINGS_PRIVATE_PREFS_UTIL_H_
+
+#define GetAllowlistedKeys \
+  Dummy();                 \
+  virtual const TypedPrefMap& GetAllowlistedKeys
+
+#include "../../../../../../../chrome/browser/extensions/api/settings_private/prefs_util.h"
+#undef GetAllowlistedKeys
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_EXTENSIONS_API_SETTINGS_PRIVATE_PREFS_UTIL_H_
