fd247c8f4ffdb591192901654a68750c354dd952
Francois Marier <francois@brave.com>
2020-09-29 17:34:51 -0700

    Filter personal data out of download protection pings (fixes brave/brave-browser#6267)

---
+++ chromium_src/chrome/browser/safe_browsing/download_protection/check_client_download_request_base.cc
@@ -0,0 +1,29 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "components/safe_browsing/core/proto/csd.pb.h"
+
+#define BRAVE_SEND_REQUEST_FILTER BraveFilterRequest(request.get());
+
+namespace safe_browsing {
+
+void BraveFilterRequest(ClientDownloadRequest* request) {
+  request->set_url("");  // URL must be present or we get a 400.
+  request->clear_file_basename();
+  request->clear_locale();
+  request->clear_resources();  // Contains URLs and referrers
+  request->clear_referrer_chain();
+
+  // Filter binaries within archives.
+  for (int i = 0; i < request->archived_binary_size(); i++) {
+    ClientDownloadRequest_ArchivedBinary* archived_binary =
+        request->mutable_archived_binary(i);
+    archived_binary->clear_file_basename();
+  }
+}
+
+}  // namespace safe_browsing
+
+#include "../../../../../../chrome/browser/safe_browsing/download_protection/check_client_download_request_base.cc"  // NOLINT
