82e8f27fdd1cf002e18dcca883ba138c6c6c2e30
samartnik <artem@brave.com>
2020-10-07 12:45:30 -0400

    [Android] TileView and TileWithTextView classes are collapsed
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/7561e2f936dbd8563840ee776a7dcb036515373c
    
    Create a basic TileView MVC component.
    
    This change introduces a set of classes and properties enabling the
    TileView to become a MVC component.
    
    The change collapses the TileView and TileWithTextView classes into
    one to avoid awkward naming. The TileView base class is never used as
    a stand-alone component and MVC allows the class to control the
    visibility of the TextView with ease.
    
    The MVC component features all UI specific features used by TileView,
    TileWithTextView, SuggestionsTileView and TopSitesTileView.
    
    Bug: 1106109

--- android/java/org/chromium/chrome/browser/suggestions/tile/BraveTileView.java
+++ android/java/org/chromium/chrome/browser/suggestions/tile/BraveTileView.java
@@ -3,25 +3,26 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.chromium.chrome.browser.widget.tile;
+package org.chromium.chrome.browser.suggestions.tile;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.BraveRewardsHelper;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
-import org.chromium.chrome.browser.preferences.BravePref;
-import org.chromium.components.user_prefs.UserPrefs;
-import org.chromium.chrome.browser.profiles.Profile;
-import org.chromium.chrome.browser.suggestions.tile.TileWithTextView;
-import org.chromium.chrome.browser.tabmodel.TabModel;
-import org.chromium.chrome.browser.ntp_background_images.util.SponsoredImageUtil;
 import org.chromium.chrome.browser.ntp_background_images.util.NTPUtil;
+import org.chromium.chrome.browser.ntp_background_images.util.SponsoredImageUtil;
+import org.chromium.chrome.browser.preferences.BravePref;
+import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.suggestions.tile.TileView;
+import org.chromium.chrome.browser.tabmodel.TabModel;
+import org.chromium.components.user_prefs.UserPrefs;
 
-public class BraveTileWithTextView extends TileWithTextView {
-    public BraveTileWithTextView(Context context, AttributeSet attrs) {
+public class BraveTileView extends TileView {
+    public BraveTileView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
