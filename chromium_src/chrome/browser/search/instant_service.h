884f9102c19b111c524c585ef91baeda1682924d
Brian Clifton <brian@clifton.me>
2020-10-08 16:12:53 -0700

    Lint fixes

--- chromium_src/chrome/browser/search/instant_service.h
+++ chromium_src/chrome/browser/search/instant_service.h
@@ -1,3 +1,8 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 #ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_SEARCH_INSTANT_SERVICE_H_
 #define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_SEARCH_INSTANT_SERVICE_H_
 
