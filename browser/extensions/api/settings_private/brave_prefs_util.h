8c9dc370242b522fea65b529024e8506163b0fa4
mkarolin <max@brave.com>
2020-10-13 18:34:43 -0400

    Lint fixes

--- browser/extensions/api/settings_private/brave_prefs_util.h
+++ browser/extensions/api/settings_private/brave_prefs_util.h
@@ -3,21 +3,22 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef BRAVE_BROWSER_EXTENSIONS_API_SETTINGS_PRIVATE_BRAVE_PREFS_UTIL_H
-#define BRAVE_BROWSER_EXTENSIONS_API_SETTINGS_PRIVATE_BRAVE_PREFS_UTIL_H
+#ifndef BRAVE_BROWSER_EXTENSIONS_API_SETTINGS_PRIVATE_BRAVE_PREFS_UTIL_H_
+#define BRAVE_BROWSER_EXTENSIONS_API_SETTINGS_PRIVATE_BRAVE_PREFS_UTIL_H_
 
 #include "chrome/browser/extensions/api/settings_private/prefs_util.h"
-namespace extensions{
+
+namespace extensions {
 
 class BravePrefsUtil : public PrefsUtil {
-  public:
-    using PrefsUtil::PrefsUtil;
-   // Gets the list of allowlisted pref keys -- that is, those which correspond
-   // to prefs that clients of the settingsPrivate API may retrieve and
-   // manipulate.
-    const PrefsUtil::TypedPrefMap& GetAllowlistedKeys() override;
+ public:
+  using PrefsUtil::PrefsUtil;
+  // Gets the list of allowlisted pref keys -- that is, those which correspond
+  // to prefs that clients of the settingsPrivate API may retrieve and
+  // manipulate.
+  const PrefsUtil::TypedPrefMap& GetAllowlistedKeys() override;
 };
 
-}
+}  // namespace extensions
 
-#endif
+#endif  // BRAVE_BROWSER_EXTENSIONS_API_SETTINGS_PRIVATE_BRAVE_PREFS_UTIL_H_
