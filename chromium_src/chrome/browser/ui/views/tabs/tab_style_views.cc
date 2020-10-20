9e6db01022d4c3b8bd01f3d63b3a171521a5be3d
mkarolin <max@brave.com>
2020-10-20 16:45:37 -0400

    Reduce number of patches in tab_style_views.cc

--- chromium_src/chrome/browser/ui/views/tabs/tab_style_views.cc
+++ chromium_src/chrome/browser/ui/views/tabs/tab_style_views.cc
@@ -3,30 +3,56 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#define BRAVE_GM2_TAB_STYLE_H                                                 \
- private:                                                                     \
-  gfx::FontList semibold_font_;
+#include "chrome/browser/ui/views/tabs/tab_style_views.h"
 
-#define BRAVE_GM2_TAB_STYLE                                                   \
-  semibold_font_ = normal_font_.DeriveWithWeight(gfx::Font::Weight::SEMIBOLD);
-
-#define BRAVE_CALCULATE_COLORS                                                \
-    const SkColor inactive_non_hovered_fg_color =                             \
-        SkColorSetA(foreground_color,                                         \
-                    gfx::Tween::IntValueBetween(0.7,                          \
-                                                SK_AlphaTRANSPARENT,          \
-                                                SK_AlphaOPAQUE));             \
-    const SkColor final_fg_color =                                            \
-        (tab_->IsActive() || tab_->mouse_hovered()) ?                         \
-            foreground_color : inactive_non_hovered_fg_color;                 \
-    return { final_fg_color, background_color};
-
-#define BRAVE_GET_FONT_LIST                                                   \
-  return tab_->IsActive() ? semibold_font_ : normal_font_;
-
+#define BRAVE_GM2_TAB_STYLE_H protected:
+#define CreateForTab CreateForTab_ChromiumImpl
 #include "../../../../../../../chrome/browser/ui/views/tabs/tab_style_views.cc"
-
+#undef CreateForTab
 #undef BRAVE_GM2_TAB_STYLE_H
-#undef BRAVE_GM2_TAB_STYLE
-#undef BRAVE_CALCULATE_COLORS
-#undef BRAVE_GET_FONT_LIST
+
+namespace {
+
+class BraveGM2TabStyle : public GM2TabStyle {
+ public:
+  explicit BraveGM2TabStyle(Tab* tab);
+  BraveGM2TabStyle(const GM2TabStyle&) = delete;
+  BraveGM2TabStyle& operator=(const BraveGM2TabStyle&) = delete;
+
+ protected:
+  TabStyle::TabColors CalculateColors() const override;
+  const gfx::FontList& GetFontList() const override;
+
+ private:
+  gfx::FontList semibold_font_;
+};
+
+BraveGM2TabStyle::BraveGM2TabStyle(Tab* tab)
+    : GM2TabStyle(tab),
+      semibold_font_(
+          normal_font_.DeriveWithWeight(gfx::Font::Weight::SEMIBOLD)) {}
+
+TabStyle::TabColors BraveGM2TabStyle::CalculateColors() const {
+  auto colors = GM2TabStyle::CalculateColors();
+  const SkColor inactive_non_hovered_fg_color = SkColorSetA(
+      colors.foreground_color,
+      gfx::Tween::IntValueBetween(0.7, SK_AlphaTRANSPARENT, SK_AlphaOPAQUE));
+  const SkColor final_fg_color = (tab_->IsActive() || tab_->mouse_hovered())
+                                     ? colors.foreground_color
+                                     : inactive_non_hovered_fg_color;
+  return {final_fg_color, colors.background_color};
+}
+
+const gfx::FontList& BraveGM2TabStyle::GetFontList() const {
+  const auto& font_list = GM2TabStyle::GetFontList();
+  if (&font_list == &normal_font_ && tab_->IsActive()) {
+    return semibold_font_;
+  }
+  return font_list;
+}
+
+}  // namespace
+
+std::unique_ptr<TabStyleViews> TabStyleViews::CreateForTab(Tab* tab) {
+  return std::make_unique<BraveGM2TabStyle>(tab);
+}
