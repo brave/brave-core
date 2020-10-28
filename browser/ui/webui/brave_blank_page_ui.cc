acd70ba8bda6b035cecfc45299e1ebc24da9a722
Simon Hong <shong@brave.com>
2020-10-28 09:49:44 +0900

    Use one blank tab html file by using media query

--- browser/ui/webui/brave_blank_page_ui.cc
+++ browser/ui/webui/brave_blank_page_ui.cc
@@ -5,7 +5,6 @@
 
 #include "brave/browser/ui/webui/brave_blank_page_ui.h"
 
-#include "brave/browser/themes/brave_dark_mode_utils.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/grit/brave_components_resources.h"
 #include "content/public/browser/web_ui_data_source.h"
@@ -13,15 +12,10 @@
 BraveBlankPageUI::BraveBlankPageUI(content::WebUI* web_ui,
                                    const std::string& host)
     : content::WebUIController(web_ui) {
-  const bool is_dark_mode =
-      dark_mode::GetActiveBraveDarkModeType() ==
-      dark_mode::BraveDarkModeType::BRAVE_DARK_MODE_TYPE_DARK;
-
   Profile* profile = Profile::FromWebUI(web_ui);
   content::WebUIDataSource* source =
       content::WebUIDataSource::Create(host);
-  source->SetDefaultResource(is_dark_mode ? IDR_BRAVE_DARK_BLANK_PAGE_HTML
-                                          : IDR_BRAVE_BLANK_PAGE_HTML);
+  source->SetDefaultResource(IDR_BRAVE_BLANK_PAGE_HTML);
   content::WebUIDataSource::Add(profile, source);
 }
 
