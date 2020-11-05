5722a08c4f47b43f3aa2b78bb124bf44ef3d6646
Simon Hong <shong@brave.com>
2020-11-05 10:41:45 +0900

    Generate delta file from final dmg file

--- build/mac/build_delta_installer.py
+++ build/mac/build_delta_installer.py
@@ -5,8 +5,31 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import optparse
+import os
+import re
 import subprocess
 import sys
+
+
+def unmount(volume_path):
+  print('-> unmount ' + volume_path)
+  command = ['hdiutil', 'detach', volume_path]
+  try:
+      subprocess.check_call(command)
+  except subprocess.CalledProcessError as e:
+      print(e.output)
+      raise e
+
+
+def mount_dmg(dmg_path, mount_point):
+  print('-> mounting ' + dmg_path + ' to ' + mount_point)
+
+  command = ['hdiutil', 'attach', dmg_path, '-mountpoint', mount_point]
+  try:
+      output = subprocess.check_output(command)
+  except subprocess.CalledProcessError as e:
+      print(e.output)
+      raise e
 
 
 def Main(argv):
@@ -17,10 +40,12 @@ def Main(argv):
       type='string', default=None, help='The path of sign_update binary')
   parser.add_option('--sign-key', dest='sign_key', action='store',
       type='string', default=None, help='The private key to sign patch file')
-  parser.add_option('--old-app', dest='old_app_path', action='store',
-      type='string', default=None, help='The path of old app bundle.')
-  parser.add_option('--new-app', dest='new_app_path', action='store',
-      type='string', default=None, help='The path of new app bundle.')
+  parser.add_option('--root-out-dir', dest='root_out_dir_path', action='store',
+      type='string', default=None, help='The path of root output dir.')
+  parser.add_option('--old-dmg', dest='old_dmg_path', action='store',
+      type='string', default=None, help='The path of old dmg.')
+  parser.add_option('--new-dmg', dest='new_dmg_path', action='store',
+      type='string', default=None, help='The path of new dmg.')
   parser.add_option('--patch-output', dest='patch_output_path', action='store',
       type='string', default=None, help='The path of generated delta file.')
   parser.add_option('--patch-eddsa-output', dest='patch_eddsa_output_path', action='store',
@@ -31,8 +56,17 @@ def Main(argv):
     print >> sys.stderr, parser.get_usage()
     return 1
 
+  old_dmg_mount_point = os.path.join(options.root_out_dir_path, 'old_dmg_mount_for_delta')
+  mount_dmg(options.old_dmg_path, old_dmg_mount_point)
+  old_app_path = os.path.join(old_dmg_mount_point, os.path.splitext(os.path.basename(options.old_dmg_path))[0] + '.app')
+
+  new_dmg_mount_point = os.path.join(options.root_out_dir_path, 'new_dmg_mount_for_delta')
+  mount_dmg(options.new_dmg_path, new_dmg_mount_point)
+  new_app_path = os.path.join(new_dmg_mount_point, os.path.splitext(os.path.basename(options.new_dmg_path))[0] + '.app')
+
   # generate patch file
-  command = [options.binary_delta_path, 'create', options.old_app_path, options.new_app_path, options.patch_output_path]
+  print('-> generate patch file with ' + old_app_path + ' and ' + new_app_path)
+  command = [options.binary_delta_path, 'create', old_app_path, new_app_path, options.patch_output_path]
   try:
       subprocess.check_call(command)
   except subprocess.CalledProcessError as e:
@@ -48,6 +82,9 @@ def Main(argv):
       print(e.output)
       raise e
   file.close()
+
+  unmount(old_dmg_mount_point)
+  unmount(new_dmg_mount_point)
 
   return 0
 
