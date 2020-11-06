c4b7fc28fd9cc6797ef2a1b2b21cbc827557fa44
Mario Sanchez Prada <mario@igalia.com>
2020-11-06 16:45:21 +0100

    Remove Brave override for SetupBundledWebUIDataSource
    This method has been removed upstream due to no longer being used
    so there's no need to override the CSP anymore.
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src.git/+/5691cab74720176938d40e321e65c1fef0b1a39b
    
    commit 5691cab74720176938d40e321e65c1fef0b1a39b
    Author: Rebekah Potter <rbpotter@chromium.org>
    Date:   Thu Oct 29 21:30:35 2020 +0000
    
        Reland "WebUI: Delete SetupBundledWebUIDataSource()"
    
        This reverts commit f15a4115c5d50df7bf1d1bc8dea9b0835b7afd9d.
    
        Reason for revert: Relanded the reverted CL that caused this
        CL to be reverted, because that CL was not the reason for build
        failure, and the failure has been fixed separately (see
        https://bugs.chromium.org/p/chromium/issues/detail?id=1143476)
    
        Original change's description:
        > Revert "WebUI: Delete SetupBundledWebUIDataSource()"
        >
        > This reverts commit f99198c064af6b05415da67cb9a2c61073a75718.
        >
        > Reason for revert: Compile errors due the removal of this function after having reverted https://chromium-review.googlesource.com/q/commit:85415a2c689f29c97626748a688b58c56cc22f16. See crbug.com/1143504.
        >
        > Original change's description:
        > > WebUI: Delete SetupBundledWebUIDataSource()
        > >
        > > With the update of OS settings to generate_grd, this method is no
        > > longer used.
        > >
        > > Bug: 1132403
        > > Change-Id: Id06f6249b05bebcc72eb22b417d0334bc4d5a708
        > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2505936
        > > Reviewed-by: dpapad <dpapad@chromium.org>
        > > Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
        > > Cr-Commit-Position: refs/heads/master@{#821828}
        >
        > TBR=dpapad@chromium.org,rbpotter@chromium.org
        >
        > Change-Id: I308bddd010b80241e99d731a0bfa0989ad655d73
        > No-Presubmit: true
        > No-Tree-Checks: true
        > No-Try: true
        > Bug: 1132403, 1143504
        > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2505359
        > Reviewed-by: Patti <patricialor@chromium.org>
        > Commit-Queue: Patti <patricialor@chromium.org>
        > Cr-Commit-Position: refs/heads/master@{#822006}
    
        TBR=dpapad@chromium.org,patricialor@chromium.org,rbpotter@chromium.org
    
        # Not skipping CQ checks because this is a reland.
    
        Bug: 1132403
        Bug: 1143504

--- chromium_src/chrome/browser/ui/webui/webui_util.cc
+++ chromium_src/chrome/browser/ui/webui/webui_util.cc
@@ -5,19 +5,9 @@
 
 #include "chrome/browser/ui/webui/webui_util.h"
 
-#include "chrome/common/buildflags.h"
-
 #define SetupWebUIDataSource SetupWebUIDataSource_ChromiumImpl
-#if BUILDFLAG(OPTIMIZE_WEBUI)
-#define SetupBundledWebUIDataSource SetupBundledWebUIDataSource_ChromiumImpl
-#endif
-
 #include "../../../../../../chrome/browser/ui/webui/webui_util.cc"
-
 #undef SetupWebUIDataSource
-#if BUILDFLAG(OPTIMIZE_WEBUI)
-#undef SetupBundledWebUIDataSource
-#endif
 
 namespace webui {
 
@@ -36,17 +26,5 @@ void SetupWebUIDataSource(content::WebUIDataSource* source,
   source->OverrideContentSecurityPolicy(
       network::mojom::CSPDirectiveName::ScriptSrc, kBraveCSP);
 }
-
-#if BUILDFLAG(OPTIMIZE_WEBUI)
-void SetupBundledWebUIDataSource(content::WebUIDataSource* source,
-                                 base::StringPiece bundled_path,
-                                 int bundle,
-                                 int default_resource) {
-  SetupBundledWebUIDataSource_ChromiumImpl(source, bundled_path, bundle,
-                                           default_resource);
-  source->OverrideContentSecurityPolicy(
-      network::mojom::CSPDirectiveName::ScriptSrc, kBraveCSP);
-}
-#endif
 
 }  // namespace webui
