58b12900741e206017920f8ad6216ffdad7cd208
mkarolin <max@brave.com>
2020-11-05 20:41:08 -0500

    UKM source id moved out of //base.
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/03a6990a12bc351d0b7cb22a1083a22ff8e68571
    
    commit 03a6990a12bc351d0b7cb22a1083a22ff8e68571
    Author: Yue Ru Sun <yrsun@chromium.org>
    Date:   Sat Oct 24 18:04:31 2020 +0000
    
        Move definition of UKM source id out of //base
    
        There are currently two definitions of "UKM source id".
        One is a feature class base::UkmSourceId (renaming to ukm::SourceIdObj)
        that contains source type information.
        The other is an alias for the underlying int64_t ukm::SourceId.
        Clarify that ukm::SourceId should be used if only an int is required
        to identify a source.
    
        Bug: 1046951

--- browser/brave_content_browser_client.cc
+++ browser/brave_content_browser_client.cc
@@ -286,7 +286,7 @@ bool BraveContentBrowserClient::WillCreateURLLoaderFactory(
     URLLoaderFactoryType type,
     const url::Origin& request_initiator,
     base::Optional<int64_t> navigation_id,
-    base::UkmSourceId ukm_source_id,
+    ukm::SourceIdObj ukm_source_id,
     mojo::PendingReceiver<network::mojom::URLLoaderFactory>* factory_receiver,
     mojo::PendingRemote<network::mojom::TrustedURLLoaderHeaderClient>*
         header_client,
