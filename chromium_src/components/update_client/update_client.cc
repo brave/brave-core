56acf2689d95349dce7952fd4650cbbf7323398f
Michael Herrmann <michael@herrmann.io>
2020-11-17 08:27:31 +0100

    Fix broken automatic Component updates

---
+++ chromium_src/components/update_client/update_client.cc
@@ -0,0 +1,24 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "components/update_client/update_client.h"
+
+#define UpdateClientFactory UpdateClientFactory_ChromiumImpl
+#include "../../../../components/update_client/update_client.cc"
+#undef UpdateClientFactory
+
+#include "base/logging.h"
+
+namespace update_client {
+
+scoped_refptr<UpdateClient> UpdateClientFactory(
+    scoped_refptr<Configurator> config) {
+  VLOG(3) << "Brave UpdateClientFactory called";
+  return base::MakeRefCounted<UpdateClientImpl>(
+      config, base::MakeRefCounted<PingManager>(config),
+      &SequentialUpdateChecker::Create);
+}
+
+}  // namespace update_client
