c3fdd98f1f7760dd28b869afed88fa49adedf9c0
Brandon <JustBrandonT@gmail.com>
2020-11-18 20:31:37 -0500

    Fixed unit tests linking and execution.

--- vendor/brave-ios/Ledger/Data/Model/ActivityInfo.h
+++ vendor/brave-ios/Ledger/Data/Model/ActivityInfo.h
@@ -9,6 +9,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+OBJC_EXPORT
 @interface ActivityInfo : NSManagedObject
 
 + (NSFetchRequest<ActivityInfo *> *)fetchRequest;
--- vendor/brave-ios/Ledger/Data/Model/PublisherInfo.h
+++ vendor/brave-ios/Ledger/Data/Model/PublisherInfo.h
@@ -9,6 +9,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+OBJC_EXPORT
 @interface PublisherInfo : NSManagedObject
 
 + (NSFetchRequest<PublisherInfo *> *)fetchRequest;
