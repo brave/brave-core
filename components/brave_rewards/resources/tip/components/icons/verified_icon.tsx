432d3eef2622f66fb5250ebacc2a5b8be5238de3
zenparsing <zenparsing@gmail.com>
2020-11-20 13:27:40 -0500

    Adjust publisher verified checkmark on the tip banner

--- components/brave_rewards/resources/tip/components/icons/verified_icon.tsx
+++ components/brave_rewards/resources/tip/components/icons/verified_icon.tsx
@@ -6,14 +6,13 @@ import * as React from 'react'
 
 export function VerifiedIcon () {
   return (
-    <svg className='icon' fill='none' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 33 34'>
+    <svg className='icon' viewBox='0 0 32 33'>
       <g filter='url(#filter0_d)'>
-        <path fillRule='evenodd' clipRule='evenodd' d='M16.661 27.177c6.627 0 12-5.457 12-12.188 0-6.732-5.373-12.189-12-12.189s-12 5.457-12 12.189c0 6.731 5.373 12.188 12 12.188z' fill='#4C54D2'/>
-        <path d='M28.161 14.989c0 6.463-5.156 11.688-11.5 11.688s-11.5-5.225-11.5-11.688c0-6.463 5.156-11.689 11.5-11.689s11.5 5.226 11.5 11.689z' stroke='#fff'/>
+        <path fillRule='evenodd' clipRule='evenodd' d='M16 26.46c6.627 0 12-5.458 12-12.19 0-6.73-5.373-12.188-12-12.188S4 7.539 4 14.271c0 6.731 5.373 12.188 12 12.188z' fill='#737ADE'/>
       </g>
-      <path d='M12.639 14.989l3.333 2.723 4.712-5.447' stroke='#fff' strokeWidth='1.813' strokeLinecap='round' strokeLinejoin='round'/>
+      <path d='M10.977 15.047l4.284 3.5 6.055-7' stroke='#fff' strokeWidth='2.8' strokeLinecap='round' strokeLinejoin='round' fill='#737ADE'/>
       <defs>
-        <filter id='filter0_d' x='.661' y='.8' width='32' height='32.377' filterUnits='userSpaceOnUse' colorInterpolationFilters='sRGB'>
+        <filter id='filter0_d' x='0' y='.082' width='32' height='32.377' filterUnits='userSpaceOnUse' colorInterpolationFilters='sRGB'>
           <feFlood floodOpacity='0' result='BackgroundImageFix'/>
           <feColorMatrix in='SourceAlpha' type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'/>
           <feOffset dy='2'/>
