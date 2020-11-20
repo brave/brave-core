f66c7ef39c3bef523a26b192c676332836788460
Brian Clifton <brian@clifton.me>
2020-11-19 23:21:51 -0700

    Removed unused includes (was causing a problem on Android)

--- browser/profiles/profile_util_unittest.cc
+++ browser/profiles/profile_util_unittest.cc
@@ -9,16 +9,12 @@
 #include "base/files/scoped_temp_dir.h"
 #include "brave/common/pref_names.h"
 #include "brave/components/search_engines/brave_prepopulated_engines.h"
-#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
-#include "chrome/test/base/scoped_testing_local_state.h"
-#include "chrome/test/base/test_browser_window.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile.h"
 #include "chrome/test/base/testing_profile_manager.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/test/browser_task_environment.h"
-#include "content/public/test/test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 class BraveProfileUtilTest : public testing::Test {
