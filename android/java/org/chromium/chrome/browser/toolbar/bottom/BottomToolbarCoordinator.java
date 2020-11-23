e98b75a2942873af801a8f135fdc97a207fa8dc6
samartnik <artem@brave.com>
2020-11-23 15:19:51 -0500

    [Android] Renamings and c-tor signature changes

--- android/java/org/chromium/chrome/browser/toolbar/bottom/BottomToolbarCoordinator.java
+++ android/java/org/chromium/chrome/browser/toolbar/bottom/BottomToolbarCoordinator.java
@@ -78,7 +78,6 @@ class BottomToolbarCoordinator implements View.OnLongClickListener {
     private final Callback<ShareDelegate> mShareDelegateSupplierCallback;
     private ObservableSupplierImpl<OnClickListener> mShareButtonListenerSupplier =
             new ObservableSupplierImpl<>();
-    private final Supplier<Boolean> mShowStartSurfaceCallable;
     private CallbackController mCallbackController = new CallbackController();
     ObservableSupplier<AppMenuButtonHelper> mMenuButtonHelperSupplier;
     private BottomControlsMediator mBottomControlsMediator;
@@ -98,8 +97,6 @@ class BottomToolbarCoordinator implements View.OnLongClickListener {
      * @param themeColorProvider The {@link ThemeColorProvider} for the bottom toolbar.
      * @param shareDelegateSupplier The supplier for the {@link ShareDelegate} the bottom controls
      *         should use to share content.
-     * @param showStartSurfaceCallable The action that opens the start surface, returning true if
-     * the start surface is shown.
      * @param openHomepageAction The action that opens the homepage.
      * @param setUrlBarFocusAction The function that sets Url bar focus. The first argument is
      * @param overviewModeBehaviorSupplier Supplier for the overview mode manager.
@@ -108,8 +105,7 @@ class BottomToolbarCoordinator implements View.OnLongClickListener {
     BottomToolbarCoordinator(ScrollingBottomViewResourceFrameLayout scrollingBottomView,
             ViewStub stub, ActivityTabProvider tabProvider,
             OnLongClickListener tabsSwitcherLongClickListner, ThemeColorProvider themeColorProvider,
-            ObservableSupplier<ShareDelegate> shareDelegateSupplier,
-            Supplier<Boolean> showStartSurfaceCallable, Runnable openHomepageAction,
+            ObservableSupplier<ShareDelegate> shareDelegateSupplier, Runnable openHomepageAction,
             Callback<Integer> setUrlBarFocusAction,
             OneshotSupplier<LayoutStateProvider> layoutStateProviderSupplier,
             ObservableSupplier<AppMenuButtonHelper> menuButtonHelperSupplier,
@@ -119,18 +115,11 @@ class BottomToolbarCoordinator implements View.OnLongClickListener {
         layoutStateProviderSupplier.onAvailable(
                 mCallbackController.makeCancelable(this::setLayoutStateProvider));
 
-        mShowStartSurfaceCallable = showStartSurfaceCallable;
         final OnClickListener homeButtonListener = v -> {
             openHomepageAction.run();
         };
 
         final OnClickListener searchAcceleratorListener = v -> {
-            // Only switch to HomePage when overview is showing.
-            if (mLayoutStateProvider != null
-                    && mLayoutStateProvider.isLayoutVisible(LayoutType.TAB_SWITCHER)
-                    && mShowStartSurfaceCallable != null) {
-                mShowStartSurfaceCallable.get();
-            }
             setUrlBarFocusAction.onResult(OmniboxFocusReason.ACCELERATOR_TAP);
         };
 
