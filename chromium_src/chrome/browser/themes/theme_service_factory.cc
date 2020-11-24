7a7c9be7a01ac6744c283c50effdcde535503c66
Simon Hong <shong@brave.com>
2020-11-24 11:17:22 +0900

    Avoid patching for theme_service_factory.cc

--- chromium_src/chrome/browser/themes/theme_service_factory.cc
+++ chromium_src/chrome/browser/themes/theme_service_factory.cc
@@ -3,9 +3,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "chrome/browser/themes/theme_service_factory.h"
-
 #include "brave/browser/themes/brave_theme_service.h"
+// Below files are included in advance to prevent overriding GetBrowserContextToUse.
+#include "chrome/browser/extensions/extension_system_factory.h"
+#include "chrome/browser/themes/theme_service_factory.h"
+#include "extensions/browser/extension_prefs_factory.h"
+#include "extensions/browser/extension_registry_factory.h"
 
 #if !defined(OS_LINUX)
 #define BRAVE_THEMESERVICEFACTORY_BUILDSERVICEINSTANCEFOR \
@@ -15,9 +18,15 @@
 #define BRAVE_THEMESERVICEFACTORY_BUILDSERVICEINSTANCEFOR
 #endif
 
-#define  BRAVE_THEMESERVICEFACTORY_GETBROWSERCONTEXTTOUSE \
-  if (context->IsTor()) return context;
+#define GetBrowserContextToUse GetBrowserContextToUse_ChromiumImpl
 
 #include "../../../../../chrome/browser/themes/theme_service_factory.cc"
 #undef BRAVE_THEMESERVICEFACTORY_BUILDSERVICEINSTANCEFOR
-#undef  BRAVE_THEMESERVICEFACTORY_GETBROWSERCONTEXTTOUSE
+#undef GetBrowserContextToUse
+
+content::BrowserContext* ThemeServiceFactory::GetBrowserContextToUse(
+    content::BrowserContext* context) const {
+  if (context->IsTor())
+    return context;
+  return ThemeServiceFactory::GetBrowserContextToUse_ChromiumImpl(context);
+}
---
+++ chromium_src/chrome/browser/themes/theme_service_factory.h
@@ -0,0 +1,18 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_THEMES_THEME_SERVICE_FACTORY_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_THEMES_THEME_SERVICE_FACTORY_H_
+
+// Below files are included in advance to prevent overriding GetBrowserContextToUse.
+#include "components/keyed_service/content/browser_context_keyed_service_factory.h"
+
+#define GetBrowserContextToUse GetBrowserContextToUse_ChromiumImpl(content::BrowserContext* context) const; content::BrowserContext* GetBrowserContextToUse
+
+#include "../../../../../chrome/browser/themes/theme_service_factory.h"
+
+#undef GetBrowserContextToUse
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_THEMES_THEME_SERVICE_FACTORY_H_
