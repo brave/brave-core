b9808d4f252f2c75fbd0fbc691778512b4494d7b
Michael Herrmann <michael@herrmann.io>
2020-12-03 19:21:40 +0100

    Remove unsupported components from brave://components
     * Legacy TLS Deprecation Config
     * Federated Learning of Cohorts
     * Autofill States Data
     * Subresource Filter Rules
     * Crowd Deny
     * Certificate Error Assistant
     * Safety Tips
     * Origin Trials
     * Zxcvbn Data Dictionaries

--- chromium_src/components/component_updater/component_installer.cc
+++ chromium_src/components/component_updater/component_installer.cc
@@ -2,7 +2,42 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "components/component_updater/component_installer.h"
+
+#include <algorithm>
+
 #include "components/crx_file/crx_verifier.h"
 #define CRX3_WITH_PUBLISHER_PROOF CRX3
+#define Register Register_ChromiumImpl
 #include "../../../../components/component_updater/component_installer.cc"
+#undef Register
 #undef CRX3_WITH_PUBLISHER_PROOF
+
+namespace component_updater {
+
+void ComponentInstaller::Register(ComponentUpdateService* cus,
+                                  base::OnceClosure callback) {
+  static std::vector<std::string> blacklisted_components({
+      "bklopemakmnopmghhmccadeonafabnal", // Legacy TLS Deprecation Config
+      "cmahhnpholdijhjokonmfdjbfmklppij", // Federated Learning of Cohorts
+      "eeigpngbgcognadeebkilcpcaedhellh", // Autofill States Data
+      "gcmjkmgdlgnkkcocmoeiminaijmmjnii", // Subresource Filter Rules
+      "ggkkehgbnfjpeggfpleeakpidbkibbmn", // Crowd Deny
+      "giekcmmlnklenlaomppkphknjmnnpneh", // Certificate Error Assistant
+      "jflookgnkcckhobaglndicnbbgbonegd", // Safety Tips
+      "llkgjffcdpffmhiakmfcdcblohccpfmo", // Origin Trials
+      "ojhpjlocmbogdgmfpkhlaaeamibhnphh"  // Zxcvbn Data Dictionaries
+  });
+  std::vector<uint8_t> hash;
+  installer_policy_->GetHash(&hash);
+  std::string id = update_client::GetCrxIdFromPublicKeyHash(hash);
+
+  if (std::find(blacklisted_components.begin(), blacklisted_components.end(),
+                id) != blacklisted_components.end())
+    VLOG(1) << "Skipping registration of Brave-unsupported component " << id
+            << ".";
+  else
+    Register_ChromiumImpl(cus, std::move(callback));
+}
+
+}  // namespace component_updater
