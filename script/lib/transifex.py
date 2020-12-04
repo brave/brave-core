a7e6e1d3adb9381c89a94a9c861e9916c589a9f8
Max <41635752+mkarolin@users.noreply.github.com>
2020-12-04 10:57:00 -0500

    Merge pull request #7338 from brave/maxk-l10n
    Update l10n (for 1.18.x release).

--- script/lib/transifex.py
+++ script/lib/transifex.py
@@ -214,7 +214,7 @@ def validate_tags_in_one_string(string_tag):
         string_xml = lxml.etree.fromstring('<string>' + string_text + '</string>')
     except lxml.etree.XMLSyntaxError as e:
         errors = ("\n--------------------\n"
-                  "{0}\nERROR: {1}\n").format(string_text, str(e))
+                  "{0}\nERROR: {1}\n").format(string_text.encode('utf-8'), str(e))
         print errors
         cont = raw_input('Enter C to ignore and continue. Enter anything else to exit : ')
         if cont == 'C' or cont == 'c':
@@ -258,7 +258,7 @@ def fix_links_with_target_blank_in_ph_text(text):
         return text[:target] + ' rel="noopener noreferrer"' + text[target:]
 
     rel += 5
-    rel_end = text[rel:].find('"')
+    rel_end = rel + text[rel:].find('"')
     rel_value = text[rel:rel_end]
     rel_add = ''
     if rel_value.find('noopener') == -1:
