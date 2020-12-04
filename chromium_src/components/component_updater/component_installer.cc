6dcff903998419c7670f4ab2c28e3baf666654a9
Michael Herrmann <michael@herrmann.io>
2020-12-04 06:47:14 +0100

    Correct copyright notice

--- chromium_src/components/component_updater/component_installer.cc
+++ chromium_src/components/component_updater/component_installer.cc
@@ -1,6 +1,7 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * you can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "components/component_updater/component_installer.h"
 
