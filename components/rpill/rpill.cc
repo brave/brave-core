032a22453e28a440395229a72fa0a1b52cd274e7
gpestana <g6pestana@gmail.com>
2020-12-10 17:01:36 +0000

    lint; format

--- components/rpill/rpill.cc
+++ components/rpill/rpill.cc
@@ -3,9 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "brave/components/rpill/rpill.h"
 #include "base/strings/string_util.h"
 #include "base/win/wmi.h"
-#include "brave/components/rpill/rpill.h"
 
 namespace rpill {
 
