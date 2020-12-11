e8671feb0670d834485db56ed47a257693df8800
Michael Herrmann <michael@herrmann.io>
2020-12-11 17:13:14 +0100

    Make code cleaner and more robust

--- chromium_src/components/component_updater/component_installer.cc
+++ chromium_src/components/component_updater/component_installer.cc
@@ -5,8 +5,6 @@
 
 #include "components/component_updater/component_installer.h"
 
-#include <algorithm>
-
 #include "components/crx_file/crx_verifier.h"
 #define CRX3_WITH_PUBLISHER_PROOF CRX3
 #define Register Register_ChromiumImpl
@@ -18,7 +16,7 @@ namespace component_updater {
 
 void ComponentInstaller::Register(ComponentUpdateService* cus,
                                   base::OnceClosure callback) {
-  static std::vector<std::string> blacklisted_components({
+  static const char* blacklisted_components[] = {
       "bklopemakmnopmghhmccadeonafabnal",  // Legacy TLS Deprecation Config
       "cmahhnpholdijhjokonmfdjbfmklppij",  // Federated Learning of Cohorts
       "eeigpngbgcognadeebkilcpcaedhellh",  // Autofill States Data
@@ -32,17 +30,19 @@ void ComponentInstaller::Register(ComponentUpdateService* cus,
       "lmelglejhemejginpboagddgdfbepgmp",  // Optimization Hints
       "obedbbhbpmojnkanicioggnmelmoomoc"   // OnDeviceHeadSuggest
 #endif
-  });
-  std::vector<uint8_t> hash;
-  installer_policy_->GetHash(&hash);
-  std::string id = update_client::GetCrxIdFromPublicKeyHash(hash);
-
-  if (std::find(blacklisted_components.begin(), blacklisted_components.end(),
-                id) != blacklisted_components.end())
-    VLOG(1) << "Skipping registration of Brave-unsupported component " << id
-            << ".";
-  else
-    Register_ChromiumImpl(cus, std::move(callback));
+  };
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  if (installer_policy_) {
+    std::vector<uint8_t> hash;
+    installer_policy_->GetHash(&hash);
+    const std::string id = update_client::GetCrxIdFromPublicKeyHash(hash);
+    if (base::Contains(blacklisted_components, id.c_str())) {
+      VLOG(1) << "Skipping registration of Brave-unsupported component "
+              << id << ".";
+      return;
+    }
+  }
+  Register_ChromiumImpl(cus, std::move(callback));
 }
 
 }  // namespace component_updater
