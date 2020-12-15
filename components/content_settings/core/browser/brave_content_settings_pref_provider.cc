8ba1f41a67cb366ac9a08af75035c677008691c8
Mario Sanchez Prada <mario@igalia.com>
2020-12-15 16:59:36 +0100

    Register ContentSettingsTypes as persistent and drop BraveEphemeralProvider
    Migrating away from ContentSettingsType::PLUGIN and ResourceIdentifier
    means that we no longer need to use ephemeral storage for Brave Shields
    supported modes, as we now register our own Brave-specific types for
    them and we can make them use persistent, right from the start.
    
    This means that we don't need to have a BraveEphemeralProvider subclass
    only to make sure that we don't handle Brave Shields in the same way
    the Flash plugin (now removed) used to be handled (i.e. ephemeral), so
    we can now remove that class along with the bits in BravePrefProvider's
    constructor that would register Brave Shields, since that will now be
    handled by upstream's PrefProvider constructor already.

--- components/content_settings/core/browser/brave_content_settings_pref_provider.cc
+++ components/content_settings/core/browser/brave_content_settings_pref_provider.cc
@@ -121,20 +121,6 @@ BravePrefProvider::BravePrefProvider(PrefService* prefs,
       base::BindRepeating(&BravePrefProvider::OnCookiePrefsChanged,
                           base::Unretained(this)));
 
-  WebsiteSettingsRegistry* website_settings =
-      WebsiteSettingsRegistry::GetInstance();
-  // Makes BravePrefProvider handle Brave-specific types.
-  for (const WebsiteSettingsInfo* info : *website_settings) {
-    if (content_settings::IsShieldsContentSettingsType(info->type())) {
-      content_settings_prefs_.insert(std::make_pair(
-          info->type(),
-          std::make_unique<ContentSettingsPref>(
-              info->type(), prefs_, &pref_change_registrar_,
-              info->pref_name(), off_the_record_, restore_session,
-              base::Bind(&PrefProvider::Notify, base::Unretained(this)))));
-    }
-  }
-
   MigrateShieldsSettings(off_the_record);
 
   OnCookieSettingsChanged(ContentSettingsType::BRAVE_COOKIES);
