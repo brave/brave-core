5bc6ba63288e339f250d5ff387b383d608e5afc1
mkarolin <max@brave.com>
2020-12-15 18:00:13 -0500

    Converted resources_util.cc patch to a chromium_src override.

---
+++ chromium_src/chrome/browser/resources_util.cc
@@ -0,0 +1,15 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/grit/brave_theme_resources_map.h"
+
+#define BRAVE_RESOURCES_UTIL                              \
+  for (size_t i = 0; i < kBraveThemeResourcesSize; ++i) { \
+    storage.emplace_back(kBraveThemeResources[i].name,    \
+                         kBraveThemeResources[i].value);  \
+  }
+
+#include "../../../../chrome/browser/resources_util.cc"
+#undef BRAVE_RESOURCES_UTIL
