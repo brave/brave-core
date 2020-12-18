071a4106b530f4fc68cb6c9c8289aa4cfb34f15c
samartnik <artem@brave.com>
2020-12-18 11:47:12 -0500

    [Android] Changes for SwipeGestureListener
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/8ab8cf08a01a3e3776bf63bbc26b7f8317b2a53f
    
    Android: Switch to using browser_ui/widget/gesture/SwipeGestureListener
    
    The newly added SwipeGestureListener replaces the old
    EdgeSwipeHandler/SwipeRecognizer.
    
    A bunch of swipe gesture methods in Stack/OverlappingStack were
    apparently left unused. Deleted them for clean up.
    
    Bug: 1142475

--- android/java/org/chromium/chrome/browser/toolbar/bottom/BraveScrollingBottomViewResourceFrameLayout.java
+++ android/java/org/chromium/chrome/browser/toolbar/bottom/BraveScrollingBottomViewResourceFrameLayout.java
@@ -9,13 +9,13 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 
-import org.chromium.chrome.browser.compositor.layouts.eventfilter.EdgeSwipeHandler;
-import org.chromium.chrome.browser.contextualsearch.SwipeRecognizer;
+import org.chromium.components.browser_ui.widget.gesture.SwipeGestureListener;
+import org.chromium.components.browser_ui.widget.gesture.SwipeGestureListener.SwipeHandler;
 
 public class BraveScrollingBottomViewResourceFrameLayout
         extends ScrollingBottomViewResourceFrameLayout {
     /** A swipe recognizer for handling swipe gestures. */
-    private SwipeRecognizer mSwipeRecognizer;
+    private SwipeGestureListener mSwipeGestureListener;
 
     public BraveScrollingBottomViewResourceFrameLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -26,9 +26,8 @@ public class BraveScrollingBottomViewResourceFrameLayout
      * events to be intercepted by the view itself.
      * @param handler A handler for swipe events on this view.
      */
-    public void setSwipeDetector(EdgeSwipeHandler handler) {
-        mSwipeRecognizer = new SwipeRecognizer(getContext());
-        mSwipeRecognizer.setSwipeHandler(handler);
+    public void setSwipeDetector(SwipeHandler handler) {
+        mSwipeGestureListener = new SwipeGestureListener(getContext(), handler);
 
         // TODO(mdjones): This line of code makes it impossible to scroll through the bottom
         // toolbar. If the user accidentally swipes up on this view, the scroll no longer goes
@@ -40,14 +39,14 @@ public class BraveScrollingBottomViewResourceFrameLayout
     @Override
     public boolean onInterceptTouchEvent(MotionEvent event) {
         boolean handledEvent = false;
-        if (mSwipeRecognizer != null) handledEvent = mSwipeRecognizer.onTouchEvent(event);
+        if (mSwipeGestureListener != null) handledEvent = mSwipeGestureListener.onTouchEvent(event);
         return handledEvent || super.onInterceptTouchEvent(event);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         boolean handledEvent = false;
-        if (mSwipeRecognizer != null) handledEvent = mSwipeRecognizer.onTouchEvent(event);
+        if (mSwipeGestureListener != null) handledEvent = mSwipeGestureListener.onTouchEvent(event);
         return handledEvent || super.onTouchEvent(event);
     }
 }
