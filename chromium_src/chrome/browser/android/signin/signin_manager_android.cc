9ddb963598cd495b886ed166831f2d7d0ef5f531
samartnik <artem@brave.com>
2020-12-18 10:20:38 -0500

    [Android] Interface for SigninManager was extracted
    Chromium change:
    https://github.com/chromium/chromium/commit/a81725278e7bcfc2e9a1e92ea1a428275a350a23
    
    [Android][Signin][Modularization] Extract interface for SigninManager
    
    This CL extracts an interface for SigninManager.
    
    Bug: 1150388

--- chromium_src/chrome/browser/android/signin/signin_manager_android.cc
+++ chromium_src/chrome/browser/android/signin/signin_manager_android.cc
@@ -5,18 +5,18 @@
 
 #include "brave/build/android/jni_headers/BraveSigninManager_jni.h"
 #include "chrome/browser/android/signin/signin_manager_android.h"
-#include "chrome/android/chrome_jni_headers/SigninManager_jni.h"
+#include "chrome/android/chrome_jni_headers/SigninManagerImpl_jni.h"
 
 namespace {
 // For preventing "unused Java_SigninManager_create method" compile error.
 class UnusedClass {
  private:
   void test() {
-    Java_SigninManager_create(nullptr, 0ll, nullptr, nullptr, nullptr);
+    Java_SigninManagerImpl_create(nullptr, 0ll, nullptr, nullptr, nullptr);
   }
 };
 }  // namespace
 
-#define Java_SigninManager_create Java_BraveSigninManager_create
+#define Java_SigninManagerImpl_create Java_BraveSigninManager_create
 #include "../../../../../../chrome/browser/android/signin/signin_manager_android.cc"  // NOLINT
-#undef Java_SigninManager_create
+#undef Java_SigninManagerImpl_create
