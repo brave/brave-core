6b90cb1df0d26cf7a46a60ef9a715ff82e8a42ee
Michael Herrmann <michael@herrmann.io>
2020-12-23 15:46:37 +0100

    Remove unsupported components from brave://components

--- chromium_src/components/component_updater/component_installer.cc
+++ chromium_src/components/component_updater/component_installer.cc
@@ -12,14 +12,13 @@
 #undef Register
 #undef CRX3_WITH_PUBLISHER_PROOF
 
-#include "base/stl_util.h"
+#include "base/containers/contains.h"
 
 namespace component_updater {
 
 void ComponentInstaller::Register(ComponentUpdateService* cus,
                                   base::OnceClosure callback) {
-  VLOG(1) << "Brave ComponentInstaller::Register override called.";
-  static const char* disallowed_components[] = {
+  static std::string disallowed_components[] = {
       "bklopemakmnopmghhmccadeonafabnal",  // Legacy TLS Deprecation Config
       "cmahhnpholdijhjokonmfdjbfmklppij",  // Federated Learning of Cohorts
       "eeigpngbgcognadeebkilcpcaedhellh",  // Autofill States Data
@@ -35,27 +34,17 @@ void ComponentInstaller::Register(ComponentUpdateService* cus,
 #endif
   };
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
-  VLOG(1) << "Brave ComponentInstaller::Register is on valid thread.";
   if (installer_policy_) {
-    VLOG(1) << "Brave ComponentInstaller::Register has installer_policy_.";
     std::vector<uint8_t> hash;
     installer_policy_->GetHash(&hash);
     const std::string id = update_client::GetCrxIdFromPublicKeyHash(hash);
-    VLOG(1) << "Brave ComponentInstaller::Register component id: " << id;
-    if (base::Contains(disallowed_components, id.c_str())) {
+    if (base::Contains(disallowed_components, id)) {
       VLOG(1) << "Skipping registration of Brave-unsupported component "
               << id << ".";
       return;
-    } else {
-      VLOG(1) << "Brave ComponentInstaller::Register " << id
-              << " is not blocked.";
     }
-  } else {
-    VLOG(1) << "Brave ComponentInstaller::Register has no installer_policy_.";
   }
-  VLOG(1) << "Brave ComponentInstaller::Register calling base implementation.";
   Register_ChromiumImpl(cus, std::move(callback));
-  VLOG(1) << "Brave ComponentInstaller::Register base implementation complete.";
 }
 
 }  // namespace component_updater
