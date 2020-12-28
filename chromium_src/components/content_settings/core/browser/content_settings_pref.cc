7c795caaa3a3d975d0750c3f16f3f37c23217a8a
mkarolin <max@brave.com>
2020-12-28 17:22:28 -0500

    IsShieldsContentSettingsType is not used on iOS.

--- chromium_src/components/content_settings/core/browser/content_settings_pref.cc
+++ chromium_src/components/content_settings/core/browser/content_settings_pref.cc
@@ -3,10 +3,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "build/build_config.h"
+
+#if !defined(OS_IOS)
 #include "brave/components/content_settings/core/browser/brave_content_settings_utils.h"
 
 #define BRAVE_SET_WEBSITE_SETTING \
   content_settings::IsShieldsContentSettingsType(content_type_) ||
+#else
+#define BRAVE_SET_WEBSITE_SETTING
+#endif
 
 #include "../../../../../../components/content_settings/core/browser/content_settings_pref.cc"
 
