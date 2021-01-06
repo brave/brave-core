ee1aebdcdb79749223f3f84f4e6bc109b12147db
mkarolin <max@brave.com>
2021-01-06 12:03:49 -0500

    Prevent crash on iOS when modifying AUTOPLAY pref.
    This prevents crash on iOS when AUTOPLAY isn't registered (unclear why
    ATM).
    
    Also, removes unneeded header include.

--- chromium_src/components/content_settings/core/browser/content_settings_default_provider.cc
+++ chromium_src/components/content_settings/core/browser/content_settings_default_provider.cc
@@ -3,8 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "brave/components/content_settings/core/browser/brave_content_settings_utils.h"
+#include "build/build_config.h"
 
+#if !defined(OS_IOS)
 #define BRAVE_DISCARD_OR_MIGRATE_OBSOLETE_PREFERENCES           \
   const std::string& autoplay_pref =                            \
       GetPrefName(ContentSettingsType::AUTOPLAY);               \
@@ -12,6 +13,9 @@
       ContentSetting::CONTENT_SETTING_ASK) {                    \
     prefs_->ClearPref(autoplay_pref);                           \
   }
+#else
+#define BRAVE_DISCARD_OR_MIGRATE_OBSOLETE_PREFERENCES
+#endif
 
 #include "../../../../../../components/content_settings/core/browser/content_settings_default_provider.cc"
 
