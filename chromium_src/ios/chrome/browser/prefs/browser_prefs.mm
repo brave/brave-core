f40268f674ec1a8867328b6ea938cd014d2eb617
mkarolin <max@brave.com>
2021-01-08 12:08:05 -0500

    cr88 -> 1.19.x: [iOS] Added a missing header include.

--- chromium_src/ios/chrome/browser/prefs/browser_prefs.mm
+++ chromium_src/ios/chrome/browser/prefs/browser_prefs.mm
@@ -4,6 +4,7 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/components/brave_sync/brave_sync_prefs.h"
+#include "components/pref_registry/pref_registry_syncable.h"
 
 void BraveRegisterBrowserStatePrefs(PrefRegistrySimple* registry) {
   brave_sync::Prefs::RegisterProfilePrefs(registry);
