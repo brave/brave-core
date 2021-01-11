a5261faef18917d8ef36a46593d8c8800c32bed9
brave-builds <devops@brave.com>
2021-01-11 16:08:40 +0000

    Uplift of #7542 (squashed) to beta

--- script/brave_license_helper.py
+++ script/brave_license_helper.py
@@ -123,6 +123,7 @@ def AddBraveCredits(prune_paths, special_cases, prune_dirs, additional_paths):
     prune_list += [
         'chromium_src',  # Brave's overrides, covered by main notice.
         'node_modules',  # See brave/third_party/npm-* instead.
+        '.vscode',       # Automatically added by Visual Studio.
     ]
     prune_dirs = tuple(prune_list)
 
