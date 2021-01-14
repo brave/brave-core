19eafff5373de42ce930eb24cbdb1b78dcba7ce8
Brian Clifton <brian@clifton.me>
2021-01-14 00:54:45 -0700

    Remove extra escapting from Ecosia search term
    Fixes https://github.com/brave/brave-browser/issues/13568

--- components/search_engines/brave_prepopulated_engines.cc
+++ components/search_engines/brave_prepopulated_engines.cc
@@ -12,7 +12,7 @@ namespace TemplateURLPrepopulateData {
 
 // IMPORTANT! Make sure to bump this value if you make changes to the
 // engines below or add/remove engines.
-const int kBraveCurrentDataVersion = 10;
+const int kBraveCurrentDataVersion = 11;
 // DO NOT CHANGE THIS ONE. Used for backfilling kBraveDefaultSearchVersion.
 const int kBraveFirstTrackedDataVersion = 6;
 
@@ -130,7 +130,7 @@ const PrepopulatedEngine brave_ecosia =
 #else
                        "e8eb07a6"
 #endif
-                       "&q={{searchTerms}}",
+                       "&q={searchTerms}",
                        "https://ac.ecosia.org/?q={searchTerms}",
                        PREPOPULATED_ENGINE_ID_ECOSIA);
 
