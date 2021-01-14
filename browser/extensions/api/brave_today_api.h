976e81322aab22de9bb3670f2cec23da76a1600f
Pete Miller <miller.pete@gmail.com>
2021-01-13 23:04:55 -0800

    Brave Today: cli flag to change hostname feed and sources are fetched from
    Flag is `--brave-today-host` and allows to use, for example, staging, dev or a local in-development feed.

---
+++ browser/extensions/api/brave_today_api.h
@@ -0,0 +1,27 @@
+// Copyright (c) 2020 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_BROWSER_EXTENSIONS_API_BRAVE_TODAY_API_H_
+#define BRAVE_BROWSER_EXTENSIONS_API_BRAVE_TODAY_API_H_
+
+#include "extensions/browser/extension_function.h"
+
+namespace extensions {
+namespace api {
+
+class BraveTodayGetHostnameFunction : public ExtensionFunction {
+ public:
+  DECLARE_EXTENSION_FUNCTION("braveToday.getHostname", UNKNOWN)
+
+ protected:
+  ~BraveTodayGetHostnameFunction() override {}
+
+  ResponseAction Run() override;
+};
+
+}  // namespace api
+}  // namespace extensions
+
+#endif  // BRAVE_BROWSER_EXTENSIONS_API_BRAVE_TODAY_API_H_
