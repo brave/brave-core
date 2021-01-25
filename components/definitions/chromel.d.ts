f1475043995efa1f26e357424b608d2584bbbaf8
Brian R. Bondy <netzen@gmail.com>
2021-01-25 11:27:33 -0500

    Make Brave extension translate IPFS URIs

--- components/definitions/chromel.d.ts
+++ components/definitions/chromel.d.ts
@@ -313,6 +313,10 @@ declare namespace chrome.braveWallet {
   const loadUI: (callback: () => void) => void
 }
 
+declare namespace chrome.ipfs {
+  const resolveIPFSURI: (uri: string, callback: (gateway_url: string) => void) => void
+}
+
 declare namespace chrome.test {
   const sendMessage: (message: string) => {}
 }
