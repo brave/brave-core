a47e1f010c1b22062c07da81de0fa2d6f1424317
mkarolin <max@brave.com>
2021-01-25 15:52:28 -0500

    //chrome/browser/browser_process.h moved to its own target.
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/d3ee8ec8af52972d0b0e0273b6d4e594e1c0e791
    
    commit d3ee8ec8af52972d0b0e0273b6d4e594e1c0e791
    Author: Alan Cutter <alancutter@chromium.org>
    Date:   Thu Nov 19 07:24:04 2020 +0000
    
        Reland "Reland "Extract browser_process.h/cc into own gn target""
    
        This is a reland of d4e5f661c140a678791119d7504cd17a47355b4a
    
        Re-fixed the Windows deps requirements. Also verified gn check locally
        on Linux cross compile target for Windows.
        https://chromium-review.googlesource.com/c/chromium/src/+/2546142/3/chrome/browser/win/conflicts/BUILD.gn
    
        Original change's description:
        > Reland "Extract browser_process.h/cc into own gn target"
        >
        > This is a reland of 86556051d6e906f2cb04e47609ab4a2dd667d698
        >
        > The original CL missed a dep requirement in the Chrome branded
        > Windows build. This has been fixed:
        > https://chromium-review.googlesource.com/c/chromium/src/+/2543873/2/chrome/browser/win/conflicts/BUILD.gn
        >
        > Original change's description:
        > > Extract browser_process.h/cc into own gn target
        > >
        > > This CL pulls out browser_process.h and browser_process.cc into their
        > > own gn target so that other gn targets can depend on it without creating
        > > cyclic dependencies with //chrome/browser:browser.
        > >
        > > This is required for CL:
        > > https://chromium-review.googlesource.com/c/chromium/src/+/2528519
        > >
        > > Bug: 1147410, 883570, 925153

--- browser/brave_stats/brave_stats_updater_unittest.cc
+++ browser/brave_stats/brave_stats_updater_unittest.cc
@@ -10,7 +10,6 @@
 #include "brave/common/pref_names.h"
 #include "brave/components/brave_referrals/browser/brave_referrals_service.h"
 #include "brave/components/brave_stats/browser/brave_stats_updater_util.h"
-#include "chrome/browser/browser_process.h"
 #include "components/prefs/testing_pref_service.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
