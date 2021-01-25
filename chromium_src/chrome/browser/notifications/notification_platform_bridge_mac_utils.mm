32a667fc72ada2cc9db8a4531f8a76cf638efadc
Mario Sanchez Prada <mario@igalia.com>
2021-01-25 17:58:52 +0100

    Adapt patch and override for notification_platform_bridge_mac.mm
    Some parts belong in notification_platform_bridge_mac_utils.mm now.
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src.git/+/4737f9a9625b17bbe7b8d3387823c8eef84f6d2b
    
    commit 4737f9a9625b17bbe7b8d3387823c8eef84f6d2b
    Author: Richard Knoll <knollr@chromium.org>
    Date:   Thu Jan 21 12:52:59 2021 +0000
    
        Remove NotificationCenter from AlertDispatcher
    
        This refactors the macOS AlertDispatcher interface to no longer require
        an instance of NSUserNotificationCenter to be passed in. We expect the
        caller to query their local NotificationCenter instead and merge the
        results. This allows us to use the same interface in a future CL for the
        UNNotification API.
    
        Bug: 1127306

---
+++ chromium_src/chrome/browser/notifications/notification_platform_bridge_mac_utils.mm
@@ -0,0 +1,10 @@
+/* Copyright 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#define BRAVE_SUPPORTSALERTSIMPL_ \
+  return true;
+
+#include "../../../../../chrome/browser/notifications/notification_platform_bridge_mac_utils.mm"  // NOLINT
+#undef BRAVE_SUPPORTSALERTSIMPL_
