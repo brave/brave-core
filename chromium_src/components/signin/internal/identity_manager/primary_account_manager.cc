44812e29461cbc6448b0e3805e13e043ae6c5519
AlexeyBarabash <alexey@brave.com>
2021-01-25 14:15:07 +0200

    Use empty implementation of PrimaryAccountMutatorImpl::RevokeSyncConsent
    to avoid DCHECK during leaving sync chain
    
    fixes brave-browser#13699
    
    Related Chromium change: https://source.chromium.org/chromium/chromium/src/+/3a5fd2389a1459ad2e60ca4e8ab5dfbb78f6252e

---
+++ chromium_src/components/signin/internal/identity_manager/primary_account_manager.cc
@@ -0,0 +1,14 @@
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "components/signin/internal/identity_manager/primary_account_manager.h"
+
+#define RevokeSyncConsent RevokeSyncConsent_ChromiumImpl
+#include "../../../../../../components/signin/internal/identity_manager/primary_account_manager.cc"
+#undef RevokeSyncConsent
+
+void PrimaryAccountManager::RevokeSyncConsent(
+    signin_metrics::ProfileSignout source_metric,
+    signin_metrics::SignoutDelete delete_metric) {}
