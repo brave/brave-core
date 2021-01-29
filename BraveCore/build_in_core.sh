dfe67d8a2e898def40b3a93be23aae9d7aa9a1d8
Kyle Hickinson <git@kylehickinson.com>
2021-01-29 14:11:40 -0500

    No Bug: Create xcframeworks instead of FAT frameworks for BraveCore

--- BraveCore/build_in_core.sh
+++ BraveCore/build_in_core.sh
@@ -12,7 +12,6 @@ build_device=0
 release_flag="Release"
 brave_browser_dir="${@: -1}"
 
-base_path=""
 sim_dir="out/ios_Release"
 device_dir="out/ios_Release_arm64"
 
@@ -20,7 +19,6 @@ function usage() {
   echo "Usage: ./build_in_core.sh [--clean] [--debug] {\$home/brave/brave-browser}"
   echo " --clean:           Cleans build directories before building"
   echo " --debug:           Builds a debug instead of release framework. (Should not be pushed with the repo)"
-  echo " --skip-update:     Skips cloning and rebasing"
   echo " --build-simulator: Build only for simulator"
   echo " --build-device:    Build only for device"
   exit 1
@@ -53,6 +51,12 @@ case $i in
 esac
 done
 
+# If neither argument is supplied, build both
+if [ "$build_simulator" = 0 ] && [ "$build_device" = 0 ]; then
+  build_simulator=1
+  build_device=1
+fi
+
 if [ ! -d "$brave_browser_dir/src/brave" ]; then
   echo "Did not pass in a directory pointing to brave-browser which has already been init"
   echo "(by running \`npm run init\` at its root)"
@@ -69,15 +73,9 @@ cd src
 
 git fetch --tags --quiet
 
-# [ -d brave/vendor/brave-rewards-ios ] && rm -r brave/vendor/brave-rewards-ios
-# rsync -a --delete "$current_dir/../" brave/vendor/brave-rewards-ios
-
-# TODO: Check if there are any changes made to any of the dependent vendors via git. If no files are changed,
-#       we can just skip building altogether
-
 if [ "$clean" = 1 ]; then
   # If this script has already been run, we'll clean out the build folders
-  [[ -d $sim_dir ]] && gn clean  $sim_dir
+  [[ -d $sim_dir ]] && gn clean $sim_dir
   [[ -d $device_dir ]] && gn clean $device_dir
 else
   # Force it to reassemble the products if they've been built already
@@ -86,48 +84,33 @@ else
   [[ -d $device_dir/BraveRewards.framework ]] && rm -rf $device_dir/BraveRewards.framework
 fi
 
-if { [ "$build_simulator" = 1 ] && [ "$build_device" = 1 ]; } || { [ "$build_simulator" = 0 ] && [ "$build_device" = 0 ]; } ; then
+bc_framework_args=""
+mc_framework_args=""
+
+if [ "$build_simulator" = 1 ]; then
   npm run build -- $release_flag --target_os=ios
+  bc_framework_args="-framework $sim_dir/BraveRewards.framework -debug-symbols $(pwd)/$sim_dir/BraveRewards.dSYM" 
+  mc_framework_args="-framework $sim_dir/MaterialComponents.framework"
+fi
+
+if [ "$build_device" = 1 ]; then
   npm run build -- $release_flag --target_os=ios --target_arch=arm64
-elif [ "$build_simulator" = 1 ]; then
-  npm run build -- $release_flag --target_os=ios
-elif [ "$build_device" = 1 ]; then
-  npm run build -- $release_flag --target_os=ios --target_arch=arm64
+  bc_framework_args="$bc_framework_args -framework $device_dir/BraveRewards.framework -debug-symbols $(pwd)/$device_dir/BraveRewards.dSYM" 
+  mc_framework_args="$mc_framework_args -framework $device_dir/MaterialComponents.framework"
 fi
 
-# Copy the framework structure (from iphoneos build) to the universal folder
-if { [ "$build_simulator" = 1 ] && [ "$build_device" = 1 ]; } || { [ "$build_simulator" = 0 ] && [ "$build_device" = 0 ]; } || { [ "$build_simulator" = 0 ] && [ "$build_device" = 1 ]; } ; then
-  base_path="$device_dir"
-elif [ "$build_simulator" = 1 ]; then
-  base_path="$sim_dir"
-fi
+[ -d "$framework_drop_point/BraveRewards.xcframework" ] && rm -rf "$framework_drop_point/BraveRewards.xcframework"
+xcodebuild -create-xcframework $bc_framework_args -output "$framework_drop_point/BraveRewards.xcframework"
+echo "Created XCFramework: $framework_drop_point/BraveRewards.xcframework"
 
-rsync -a --delete "$base_path/BraveRewards.framework" "$framework_drop_point/"
-rsync -a --delete "$base_path/MaterialComponents.framework" "$framework_drop_point/"
-
-if [ -d "$base_path/BraveRewards.dSYM" ]; then
-  # Copy the dSYM if available
-  pushd $base_path > /dev/null
-  # zip up the dSYM since its too big to upload
-  zip -FSr "BraveRewards.dSYM.zip" "BraveRewards.dSYM"
-  popd > /dev/null
-  rsync -a --delete "$base_path/BraveRewards.dSYM.zip" "$framework_drop_point/"
-fi
-
-# Create universal binary file using lipo and place the combined executable in the copied framework directory
-if { [ "$build_simulator" = 1 ] && [ "$build_device" = 1 ]; } || { [ "$build_simulator" = 0 ] && [ "$build_device" = 0 ]; } ; then
-  lipo -create -output "$framework_drop_point/BraveRewards.framework/BraveRewards" "$sim_dir/BraveRewards.framework/BraveRewards" "$device_dir/BraveRewards.framework/BraveRewards"
-  lipo -create -output "$framework_drop_point/MaterialComponents.framework/MaterialComponents" "$sim_dir/MaterialComponents.framework/MaterialComponents" "$device_dir/MaterialComponents.framework/MaterialComponents"
-fi
-
-echo "Created FAT framework: $framework_drop_point/BraveRewards.framework"
+[ -d "$framework_drop_point/MaterialComponents.xcframework" ] && rm -rf "$framework_drop_point/MaterialComponents.xcframework"
+xcodebuild -create-xcframework $mc_framework_args -output "$framework_drop_point/MaterialComponents.xcframework"
+echo "Created XCFramework: $framework_drop_point/MaterialComponents.xcframework"
 
 echo "Moving Frameworks to node_modules"
 mkdir -p "$node_modules_path"
-rsync -a --delete "$framework_drop_point/BraveRewards.framework" "$node_modules_path/"
-rsync -a --delete "$framework_drop_point/MaterialComponents.framework" "$node_modules_path/"
-rsync -a --delete "$framework_drop_point/BraveRewards.dSYM.zip" "$node_modules_path/"
-rsync -a --delete "$framework_drop_point/copy_rewards_dsym.sh" "$node_modules_path/"
+rsync -a --delete "$framework_drop_point/BraveRewards.xcframework" "$node_modules_path/"
+rsync -a --delete "$framework_drop_point/MaterialComponents.xcframework" "$node_modules_path/"
 echo "Moved Frameworks to node_modules"
 
 cd brave
@@ -138,16 +121,13 @@ brave_core_tag=`git describe --tags --abbrev=0`
 popd > /dev/null
 
 echo "Completed building BraveRewards from \`brave-core/$brave_core_build_hash\`"
-cat > "$framework_drop_point/BraveRewards.resolved" << EOL
+cat > "$framework_drop_point/Local.resolved" << EOL
+REMINDER: Your local brave-core-ios dependency has been overwritten in node_modules. Re-run bootstrap.sh when you are done testing.  
+
 build: $release_flag
 brave-browser: $brave_browser_branch ($brave_browser_build_hash)
 brave-core: $brave_core_branch ($brave_core_build_hash)
   latest tag: $brave_core_tag
-EOL
 
-# Check if any of the includes had changed.
-if `git diff --quiet "$node_modules_path/BraveRewards.framework/Headers/"`; then
-  echo "  → No updates to library includes were made"
-else
-  echo "  → Changes found in library includes"
-fi
+DO NOT COMMIT THIS FILE
+EOL
