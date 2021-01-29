80c7ed117ebd21cbea94942355b5809474b17dca
Simon Hong <shong@brave.com>
2021-01-29 21:10:05 +0900

    Subclass checkbox to show text multiline

--- chromium_src/chrome/browser/ui/views/first_run_dialog.cc
+++ chromium_src/chrome/browser/ui/views/first_run_dialog.cc
@@ -6,8 +6,22 @@
 #include "brave/grit/brave_generated_resources.h"
 #include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
+#include "ui/views/controls/button/checkbox.h"
 
-#define ShowFirstRunDialog ShowFirstRunDialog_UnUsed
+namespace views {
+// Override to call SetMultiLine().
+// The label of crash report checkbox should be formatted to 2 lines.
+// Otherwise, dialog width is too long.
+class MultiLineCheckBox : public views::Checkbox {
+ public:
+  explicit MultiLineCheckBox(const base::string16& label) : Checkbox(label) {
+    SetMultiLine(true);
+  }
+  ~MultiLineCheckBox() override = default;
+  MultiLineCheckBox(const MultiLineCheckBox&) = delete;
+  MultiLineCheckBox& operator=(const MultiLineCheckBox&) = delete;
+};
+}  // namespace views
 
 // Replaced string here instead of by running 'npm run chromium_rebase_l10n'
 // because same string is used from other IDS_XXX..
@@ -15,29 +29,10 @@
 #undef IDS_FR_CUSTOMIZE_DEFAULT_BROWSER
 #define IDS_FR_ENABLE_LOGGING IDS_FR_ENABLE_LOGGING_BRAVE
 #define IDS_FR_CUSTOMIZE_DEFAULT_BROWSER IDS_FR_CUSTOMIZE_DEFAULT_BROWSER_BRAVE
+#define Checkbox MultiLineCheckBox
+
 #include "../../../../../../chrome/browser/ui/views/first_run_dialog.cc"
+
 #undef IDS_FR_ENABLE_LOGGING
 #undef IDS_FR_CUSTOMIZE_DEFAULT_BROWSER
-#undef ShowFirstRunDialog
-
-// Override to call SetMultiLine(). Our above new IDS should be displayed in
-// two lines.
-void FirstRunDialog::ShowBrave(Profile* profile) {
-  FirstRunDialog* dialog = new FirstRunDialog(profile);
-  auto* report_crashes = static_cast<views::Checkbox*>(
-      dialog->children()[dialog->children().size() - 1]);
-  report_crashes->SetMultiLine(true);
-  views::DialogDelegate::CreateDialogWidget(dialog, NULL, NULL)->Show();
-
-  base::RunLoop run_loop(base::RunLoop::Type::kNestableTasksAllowed);
-  dialog->quit_runloop_ = run_loop.QuitClosure();
-  run_loop.Run();
-}
-
-namespace first_run {
-
-void ShowFirstRunDialog(Profile* profile) {
-  FirstRunDialog::ShowBrave(profile);
-}
-
-}  // namespace first_run
+#undef Checkbox
