48b52dd2e2e515b0a332cb44a55a251cf901ed4f
Mario Sanchez Prada <mario@igalia.com>
2021-02-03 17:55:48 +0100

    Fix build of upstream Chromium's unit_tests target
    Two build failures are currently preventing us from building the
    unit_tests binary from upstream:
    
    1. A compilation error due to FakeDeviceInfoTracker not implementing
       Brave-specific's GetAllBraveDeviceInfo() and DeleteDeviceInfo()
       virtual methods from DeviceInfoTracker (added via an override).
    
    2. A linking error due to push_messaging_service_unittest.cc requiring
       a dependency for BraveTestingProfile (also added via an override).
    
    This CL fixes both errors adding an override for FakeDeviceInfoTracker
    to implement those virtual methors + adding a missing dependency for
    the unit_tests target to have access to BraveTestingProfile.
    
    Fix https://github.com/brave/brave-browser/issues/14001

---
+++ chromium_src/components/sync_device_info/fake_device_info_tracker.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2021 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_SYNC_DEVICE_INFO_FAKE_DEVICE_INFO_TRACKER_H_
+#define BRAVE_CHROMIUM_SRC_COMPONENTS_SYNC_DEVICE_INFO_FAKE_DEVICE_INFO_TRACKER_H_
+
+#include "brave/components/sync_device_info/brave_device_info.h"
+#include "components/sync_device_info/device_info_tracker.h"
+
+#define ForcePulseForTest                                                    \
+  DeleteDeviceInfo(const std::string& client_id, base::OnceClosure callback) \
+      override;                                                              \
+  std::vector<std::unique_ptr<BraveDeviceInfo>> GetAllBraveDeviceInfo()      \
+      const override;                                                        \
+  void ForcePulseForTest
+
+#include "../../../../components/sync_device_info/fake_device_info_tracker.h"
+
+#undef ForcePulseForTest
+
+#endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_SYNC_DEVICE_INFO_FAKE_DEVICE_INFO_TRACKER_H_
