8f9e058fbd2f4c9e1e494c11c00c13817bb60bfb
Anthony Tseng <darkdh@gmail.com>
2021-02-10 12:17:30 -0800

    Make installation mode of Crypto Wallet extension to be INSTALLATION_RECOMMENDED so that IsInstallationExplicitlyAllowed can return true which is used by InstallVerifier::MustRemainDisabled

--- browser/extensions/brave_extension_management.cc
+++ browser/extensions/brave_extension_management.cc
@@ -9,11 +9,13 @@
 
 #include "brave/browser/brave_browser_process_impl.h"
 #include "brave/browser/extensions/brave_extension_provider.h"
-#include "brave/common/pref_names.h"
 #include "brave/browser/tor/tor_profile_service_factory.h"
+#include "brave/common/pref_names.h"
+#include "brave/components/brave_wallet/buildflags/buildflags.h"
 #include "brave/components/ipfs/buildflags/buildflags.h"
 #include "brave/components/tor/buildflags/buildflags.h"
 #include "brave/components/tor/pref_names.h"
+#include "chrome/browser/extensions/extension_management_internal.h"
 #include "chrome/browser/extensions/external_policy_loader.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/prefs/pref_service.h"
@@ -31,20 +33,27 @@
 #include "brave/components/ipfs/ipfs_utils.h"
 #endif
 
+#if BUILDFLAG(BRAVE_WALLET_ENABLED)
+#include "brave/components/brave_wallet/brave_wallet_constants.h"
+#endif
+
 namespace extensions {
 
 BraveExtensionManagement::BraveExtensionManagement(Profile* profile)
-    : ExtensionManagement(profile),
-      extension_registry_observer_(this) {
-  extension_registry_observer_.Add(ExtensionRegistry::Get(
-        static_cast<content::BrowserContext*>(profile)));
-  providers_.push_back(
-      std::make_unique<BraveExtensionProvider>());
+    : ExtensionManagement(profile), extension_registry_observer_(this) {
+  extension_registry_observer_.Add(
+      ExtensionRegistry::Get(static_cast<content::BrowserContext*>(profile)));
+  providers_.push_back(std::make_unique<BraveExtensionProvider>());
   local_state_pref_change_registrar_.Init(g_browser_process->local_state());
   local_state_pref_change_registrar_.Add(
       tor::prefs::kTorDisabled,
       base::BindRepeating(&BraveExtensionManagement::OnTorDisabledChanged,
                           base::Unretained(this)));
+  // Make IsInstallationExplicitlyAllowed to be true
+#if BUILDFLAG(BRAVE_WALLET_ENABLED)
+  AccessById(ethereum_remote_client_extension_id)->installation_mode =
+      INSTALLATION_RECOMMENDED;
+#endif
   Cleanup(profile);
 }
 
