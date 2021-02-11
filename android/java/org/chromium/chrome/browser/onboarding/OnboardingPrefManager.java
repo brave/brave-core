2b56d6f31d389da46203459c271d2d923d660bc5
Deep <deeppandya91@gmail.com>
2021-02-11 11:59:11 -0500

    Remove unused argument

--- android/java/org/chromium/chrome/browser/onboarding/OnboardingPrefManager.java
+++ android/java/org/chromium/chrome/browser/onboarding/OnboardingPrefManager.java
@@ -230,7 +230,7 @@ public class OnboardingPrefManager {
         context.startActivity(intent);
     }
 
-    public void onboardingNotification(Context context) {
+    public void onboardingNotification() {
         if (!isOnboardingNotificationShown()) {
             BraveOnboardingNotification.showOnboardingNotification();
             setOnboardingNotificationShown(true);
