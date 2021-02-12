5f7e8225b0c98a36c1c99af5ee4120f1d7a945f9
Brandon <JustBrandonT@gmail.com>
2021-02-12 16:19:02 -0500

    Removed unused headers.

--- chromium_src/ios/chrome/browser/memory/memory_debugger.mm
+++ chromium_src/ios/chrome/browser/memory/memory_debugger.mm
@@ -3,22 +3,12 @@
 // found in the LICENSE file.
 
 #import "ios/chrome/browser/memory/memory_debugger.h"
-
 #include <stdint.h>
-
 #include <memory>
-
-#import "build/branding_buildflags.h"
-#import "ios/chrome/browser/memory/memory_metrics.h"
-#include "ios/chrome/browser/ui/util/ui_util.h"
-#import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
-
-namespace {
-}  // namespace
 
 @implementation MemoryDebugger {
 }
