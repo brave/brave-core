49e7fce6000430b557e54d35b40053b24a3f1371
samartnik <artem@brave.com>
2021-02-16 14:51:36 -0500

    [Android] Removed check passwords preference

---
+++ build/android/bytecode/java/org/brave/bytecode/BravePasswordSettingsBaseClassAdapter.java
@@ -0,0 +1,25 @@
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.brave.bytecode;
+
+import org.objectweb.asm.ClassVisitor;
+
+public class BravePasswordSettingsBaseClassAdapter extends BraveClassVisitor {
+    static String sPasswordSettingsClassName =
+            "org/chromium/chrome/browser/password_manager/settings/PasswordSettings";
+    static String sBravePasswordSettingsBaseClassName =
+            "org/chromium/chrome/browser/password_manager/settings/BravePasswordSettingsBase";
+
+    public BravePasswordSettingsBaseClassAdapter(ClassVisitor visitor) {
+        super(visitor);
+
+        changeSuperName(sPasswordSettingsClassName, sBravePasswordSettingsBaseClassName);
+
+        changeMethodOwner(sPasswordSettingsClassName, "createCheckPasswords",
+                sBravePasswordSettingsBaseClassName);
+        deleteMethod(sPasswordSettingsClassName, "createCheckPasswords");
+    }
+}
