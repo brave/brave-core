efb3c2c571d0ba8435d340a934f2149a58608a8c
Mario Sanchez Prada <mario@igalia.com>
2021-02-22 18:16:50 +0100

    Avoid instantiating chrome_browser_net::NetErrorTabHelper
    This was only being used to determine whether to send DNS probes under
    certain scenarios, but then Brave disabled that behaviour via a patch,
    so we can avoid the instantiation in the first place instead.
    
    Fixes https://github.com/brave/brave-browser/issues/14219

--- chromium_src/chrome/browser/ui/tab_helpers.cc
+++ chromium_src/chrome/browser/ui/tab_helpers.cc
@@ -8,5 +8,25 @@
 #define BRAVE_TAB_HELPERS \
   brave::AttachTabHelpers(web_contents);
 
+// Dummy class for avoiding some TabHelpers from being added to the WebContents.
+class NoTabHelper {
+ public:
+  template <typename... Args>
+  static void CreateForWebContents(content::WebContents* contents,
+                                   Args&&... args) {}
+};
+
+// For each TabHelper we want to disable we need to do two things:
+//  1. Redefine below this point the namespaces of the TabHelpers we want to
+//     disable to empty, so that the NoTabHelper class from above can be found.
+//  2. Redefine the TabHelper we want to disable to NoTabHelper.
+//
+// Please keep all the redefinitions for disabling TabHelpers below this point.
+#define chrome_browser_net
+#define NetErrorTabHelper NoTabHelper
+
 #include "../../../../../chrome/browser/ui/tab_helpers.cc"
+
+#undef NetErrorTabHelper
+#undef chrome_browser_net
 #undef BRAVE_TAB_HELPERS
