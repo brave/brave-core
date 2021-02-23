97bffc2ec6eb2266e7b02760948effdd4980e927
Brian R. Bondy <netzen@gmail.com>
2021-02-23 15:37:56 -0500

    Add tests for KeccakHash

--- components/brave_wallet/brave_wallet_utils_unittest.cc
+++ components/brave_wallet/brave_wallet_utils_unittest.cc
@@ -12,4 +12,13 @@ TEST(BraveWalletUtilsUnitTest, ToHex) {
   ASSERT_EQ(ToHex("hello world"), "0x68656c6c6f20776f726c64");
 }
 
+TEST(BraveWalletUtilsUnitTest, KeccakHash) {
+  ASSERT_EQ(
+      KeccakHash(""),
+      "0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470");
+  ASSERT_EQ(
+      KeccakHash("hello world"),
+      "0x47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad");
+}
+
 }  // namespace brave_wallet
