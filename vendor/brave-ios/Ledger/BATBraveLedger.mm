f3e66ff1730991febc069cae6bb41eab9910086d
Kyle Hickinson <git@kylehickinson.com>
2021-02-23 15:44:10 -0500

    [iOS] Handle new Rewards boolean option to prevent runtime crash

--- vendor/brave-ios/Ledger/BATBraveLedger.mm
+++ vendor/brave-ios/Ledger/BATBraveLedger.mm
@@ -76,7 +76,8 @@ static const auto kOneDay = base::Time::kHoursPerDay * base::Time::kSecondsPerHo
 
 /// Ledger Prefs, keys will be defined in `bat/ledger/option_keys.h`
 const std::map<std::string, bool> kBoolOptions = {
-    {ledger::option::kClaimUGP, true}
+    {ledger::option::kClaimUGP, true},
+    {ledger::option::kContributionsDisabledForBAPMigration, false}
 };
 const std::map<std::string, int> kIntegerOptions = {};
 const std::map<std::string, double> kDoubleOptions = {};
