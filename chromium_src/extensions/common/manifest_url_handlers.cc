855d1e411b75767ec6f471f081044e95b002f36c
Mario Sanchez Prada <mario@igalia.com>
2021-02-24 17:02:45 +0100

    Remove extensions-common-manifest_url_handlers.cc.patch
    This can be removed in favour of a chromium_src override to redefine
    ManifestURL::GetWebStoreURL() into a method that unconditionally
    returns an empty GURL.
    
    Note that we need to also redefine GetHomepageURL() as it contains
    a call to GetWebStoreURL(), otherwise upstream's version of the
    GetWebStoreURL() function would be still be invoked via that way.
    This is not ideal, but at least the code in GetHomepageURL() that
    needs to be replicated in the chromium_src is very small and very
    stable (hasn't changed in 8 years), so risk should be minimal.
    
    Resolves https://github.com/brave/brave-browser/issues/14343

---
+++ chromium_src/extensions/common/manifest_url_handlers.cc
@@ -0,0 +1,34 @@
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "extensions/common/manifest_url_handlers.h"
+
+#define GetHomepageURL GetHomepageURL_Unused
+#define GetWebStoreURL GetWebStoreURL_Unused
+
+#include "../../../../extensions/common/manifest_url_handlers.cc"
+
+#undef GetHomepageURL
+#undef GetWebStoreURL
+
+namespace extensions {
+
+// We need to provide our own version of GetHomepageURL() as well to make sure
+// that we get our own version of GetWebStoreURL() called when invoked via
+// GetHomepageURL(). Otherwise the old version will still be called since the
+// renaming of GetWebStoreURL() above does also modify the call point.
+const GURL ManifestURL::GetHomepageURL(const Extension* extension) {
+  const GURL& homepage_url = Get(extension, keys::kHomepageURL);
+  if (homepage_url.is_valid())
+    return homepage_url;
+  return GetWebStoreURL(extension);
+}
+
+// static
+const GURL ManifestURL::GetWebStoreURL(const Extension* extension) {
+  return GURL::EmptyGURL();
+}
+
+}  // namespace extensions
