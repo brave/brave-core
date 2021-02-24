f2ea9a0feeacc5dd059974d7d9feb0ec7c2705d0
Mario Sanchez Prada <mario@igalia.com>
2021-02-24 12:46:01 +0100

    Remove chrome-common-chrome_content_client_constants.cc.patch
    This patch can be removed in favour of chromium_src overrides to
    redefine the values for the constants we're interested in.
    
    Resolves https://github.com/brave/brave-browser/issues/14338

---
+++ chromium_src/chrome/common/chrome_content_client_constants.cc
@@ -0,0 +1,17 @@
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "chrome/common/chrome_content_client.h"
+
+#define kPDFExtensionPluginName kPDFExtensionPluginName_Unused
+#define kPDFInternalPluginName kPDFInternalPluginName_Unused
+
+#include "../../../../chrome/common/chrome_content_client_constants.cc"
+
+#undef kPDFExtensionPluginName
+#undef kPDFInternalPluginName
+
+const char ChromeContentClient::kPDFExtensionPluginName[] = "Chrome PDF Viewer";
+const char ChromeContentClient::kPDFInternalPluginName[] = "Chrome PDF Plugin";
