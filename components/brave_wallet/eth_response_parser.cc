bd68075e161bf5595cbcfd6acf3d9b844ca8fa0c
Brian R. Bondy <netzen@gmail.com>
2021-02-25 15:41:53 -0500

    Add beginnings of ETH JSON RPC body parser

---
+++ components/brave_wallet/eth_response_parser.cc
@@ -0,0 +1,34 @@
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/components/brave_wallet/eth_requests.h"
+
+#include <utility>
+
+#include "base/json/json_reader.h"
+#include "base/logging.h"
+
+namespace brave_wallet {
+
+bool ParseEthGetBalance(const std::string& json, std::string* hex_balance) {
+  DCHECK(hex_balance);
+  base::JSONReader::ValueWithError value_with_error =
+      base::JSONReader::ReadAndReturnValueWithError(
+          json, base::JSONParserOptions::JSON_PARSE_RFC);
+  base::Optional<base::Value>& records_v = value_with_error.value;
+  if (!records_v) {
+    LOG(ERROR) << "Invalid response, could not parse JSON, JSON is: " << json;
+    return false;
+  }
+
+  const base::DictionaryValue* response_dict;
+  if (!records_v->GetAsDictionary(&response_dict)) {
+    return false;
+  }
+
+  return response_dict->GetString("result", hex_balance);
+}
+
+}  // namespace brave_wallet
