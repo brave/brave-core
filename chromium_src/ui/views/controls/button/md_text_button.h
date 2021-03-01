d34f2a4ce3329ac338e4fc1a41122b7566766cf8
Josh Soref <jsoref@users.noreply.github.com>
2021-02-28 20:31:34 -0500

    spelling: border
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- chromium_src/ui/views/controls/button/md_text_button.h
+++ chromium_src/ui/views/controls/button/md_text_button.h
@@ -23,7 +23,7 @@ namespace views {
 
 // Make visual changes to MdTextButton in line with Brave visual style:
 //  - More rounded rectangle (for regular border, focus ring and ink drop)
-//  - Different hover text and boder color for non-prominent button
+//  - Different hover text and border color for non-prominent button
 //  - Differenet hover bg color for prominent background
 //  - No shadow for prominent background
 class VIEWS_EXPORT MdTextButton : public MdTextButtonBase {
