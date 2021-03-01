c62b855cdab9798a515b7bed3d41568a7f578179
Josh Soref <jsoref@users.noreply.github.com>
2021-02-28 20:35:28 -0500

    spelling: logic
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- browser/widevine/widevine_permission_request.cc
+++ browser/widevine/widevine_permission_request.cc
@@ -37,7 +37,7 @@ void WidevinePermissionRequest::PermissionGranted(bool is_one_time) {
   // Prevent relaunch during the browser test.
   // This will cause abnormal termination during the test.
   if (for_restart_ && !is_test_) {
-    // Try relaunch after handling permission grant logics in this turn.
+    // Try relaunch after handling permission grant logic in this turn.
     base::SequencedTaskRunnerHandle::Get()->PostTask(
         FROM_HERE, base::BindOnce(&chrome::AttemptRelaunch));
   }
