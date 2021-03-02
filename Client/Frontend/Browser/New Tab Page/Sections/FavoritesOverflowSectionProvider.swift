8c9dd87ddffe53a64f0482d8d3ae577d465edd0f
James Matthew Mudgett <jmm.seasons@gmail.com>
2021-03-02 09:57:47 -0600

    Fix brave/brave-ios#3359 - remove 20px margin on show more button (brave/brave-ios#3360)
    Co-authored-by: Michał Buczek

--- Client/Frontend/Browser/New Tab Page/Sections/FavoritesOverflowSectionProvider.swift
+++ Client/Frontend/Browser/New Tab Page/Sections/FavoritesOverflowSectionProvider.swift
@@ -86,9 +86,9 @@ class FavoritesOverflowSectionProvider: NSObject, NTPObservableSectionProvider {
         return size
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
-    }
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets { .zero }
 }
 
 extension FavoritesOverflowSectionProvider: NSFetchedResultsControllerDelegate {
