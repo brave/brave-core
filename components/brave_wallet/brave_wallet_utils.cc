ca4d806d885cb83113b7163a9861ffdbfde28203
Anthony Tseng <darkdh@gmail.com>
2021-03-04 16:40:33 -0800

    Introduce GenerateMnemonic and MnemonicToSeed following bip39 in util of brave wallet

--- components/brave_wallet/brave_wallet_utils.cc
+++ components/brave_wallet/brave_wallet_utils.cc
@@ -7,14 +7,32 @@
 
 #include <algorithm>
 #include <sstream>
+#include <utility>
 
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "brave/third_party/ethash/src/include/ethash/keccak.h"
+#include "brave/vendor/bip39wally-core-native/include/wally_bip39.h"
+#include "crypto/random.h"
+#include "third_party/boringssl/src/include/openssl/evp.h"
 
 namespace brave_wallet {
+
+namespace {
+std::string GenerateMnemonicInternal(uint8_t* entropy, size_t size) {
+  char* words = nullptr;
+  std::string result;
+  if (bip39_mnemonic_from_bytes(nullptr, entropy, size, &words) != WALLY_OK) {
+    LOG(ERROR) << __func__ << ": bip39_mnemonic_from_bytes failed";
+    return result;
+  }
+  result = words;
+  wally_free_string(words);
+  return result;
+}
+}  // namespace
 
 std::string ToHex(const std::string& data) {
   if (data.empty()) {
@@ -131,6 +149,37 @@ std::string Uint256ValueToHex(uint256_t input) {
   std::string reversed_hex = ss.str();
   std::string out(reversed_hex.rbegin(), reversed_hex.rend());
   return out;
+}
+
+std::string GenerateMnemonic(size_t entropy_size) {
+  // entropy size should be 128, 160, 192, 224, 256 bits
+  if (entropy_size < 16 || entropy_size > 32 || entropy_size % 4 != 0) {
+    LOG(ERROR) << __func__ << ": Entropy should be 16, 20, 24, 28, 32 bytes";
+    return "";
+  }
+
+  std::vector<uint8_t> entropy(entropy_size);
+  crypto::RandBytes(&entropy[0], entropy.size());
+
+  return GenerateMnemonicInternal(entropy.data(), entropy.size());
+}
+
+std::string GenerateMnemonicForTest(const std::vector<uint8_t>& entropy) {
+  return GenerateMnemonicInternal(const_cast<uint8_t*>(entropy.data()),
+                                  entropy.size());
+}
+
+std::unique_ptr<std::vector<uint8_t>> MnemonicToSeed(
+    const std::string& mnemonic,
+    const std::string& passphrase) {
+  std::unique_ptr<std::vector<uint8_t>> seed =
+      std::make_unique<std::vector<uint8_t>>(64);
+  const std::string salt = "mnemonic" + passphrase;
+  int rv = PKCS5_PBKDF2_HMAC(mnemonic.data(), mnemonic.length(),
+                             reinterpret_cast<const uint8_t*>(salt.data()),
+                             salt.length(), 2048, EVP_sha512(), seed->size(),
+                             seed->data());
+  return rv == 1 ? std::move(seed) : nullptr;
 }
 
 }  // namespace brave_wallet
