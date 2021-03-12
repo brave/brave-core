3e34bac641eec9451017a3f9336d07a206626b45
samartnik <artem@brave.com>
2021-03-12 09:31:28 -0500

    [Android] Adjusts code for minor upstream code changes

--- android/java/org/chromium/chrome/browser/settings/BraveSearchEngineAdapter.java
+++ android/java/org/chromium/chrome/browser/settings/BraveSearchEngineAdapter.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import org.chromium.chrome.R;
+import org.chromium.chrome.browser.search_engines.R;
 import org.chromium.chrome.browser.search_engines.settings.SearchEngineAdapter;
 import org.chromium.components.search_engines.TemplateUrl;
 
