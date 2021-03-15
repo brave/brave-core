7f0bc31b89a997f19873b9f972516ff243a2e16f
Brian R. Bondy <netzen@gmail.com>
2021-03-15 14:47:56 -0400

    Add IPFS gateway support on Android

---
+++ chromium_src/chrome/browser/android/omnibox/autocomplete_controller_android.cc
@@ -0,0 +1,12 @@
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/browser/autocomplete/brave_autocomplete_scheme_classifier.h"
+#include "components/omnibox/browser/autocomplete_classifier.h"
+#include "components/omnibox/browser/autocomplete_controller.h"
+
+#define ChromeAutocompleteSchemeClassifier BraveAutocompleteSchemeClassifier
+#include "../../../../../../chrome/browser/android/omnibox/autocomplete_controller_android.cc"
+#undef ChromeAutocompleteSchemeClassifier
