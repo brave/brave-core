bf451ce57e1d16acb34d2a69f01ca380027432a2
Mario Sanchez Prada <mario@igalia.com>
2021-03-16 20:38:02 +0100

    Migrate imports of settings/*.m.js files to use settings/*.js instead
    This is needed because of the migration to Polymer3 upstream.
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src.git/+/1773114e63870db0381ba1f7c670a42b7214c734
    
    commit 1773114e63870db0381ba1f7c670a42b7214c734
    Author: dpapad <dpapad@chromium.org>
    Date:   Fri Mar 12 18:29:56 2021 +0000
    
        Settings: Check-in remaining Polymer3 files settings/
    
        Specifically addressing:
         - icons.js
         - settings_page_css.js
         - settings_shared_css.js
         - settings_vars_css.js
    
        Also removing unnecessary includes of js_moulizer.gni and
        polymer.gni from a few BUILD.gn files.
    
        Bug: 1184052
    
    fixup! Migrate imports of settings_*.m.js files to use settings_*.js instead

--- browser/resources/settings/brave_sync_page/brave_sync_page.js
+++ browser/resources/settings/brave_sync_page/brave_sync_page.js
@@ -11,8 +11,8 @@ import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.m.js';
 import 'chrome://resources/cr_elements/icons.m.js';
 import '../settings_page/settings_animated_pages.js';
 import '../settings_page/settings_subpage.js';
-import '../settings_shared_css.m.js';
-import '../settings_vars_css.m.js';
+import '../settings_shared_css.js';
+import '../settings_vars_css.js';
 import './brave_sync_subpage.js';
 
 import {I18nBehavior} from 'chrome://resources/js/i18n_behavior.m.js';
