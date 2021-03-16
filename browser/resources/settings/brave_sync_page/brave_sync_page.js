7985f0686d92cbe7225764127e774ce925f0a105
Mario Sanchez Prada <mario@igalia.com>
2021-03-16 19:01:56 +0100

    Adapt to more changes in autogenerated files from settings:modulize
    The settings_auto_imports GN variable does only exist from now on for
    ChromeOS builds, yet we still use its contents from different places
    inside Brave that take what's defined in //b/r/settings/settings.gni
    and then add to it depending on the case (e.g. brave_ipfs_page), so we
    need to keep it but set it instead of appending to it to avoid errors.
    
    Also, the dependency on //chrome/browser/resources/settings:router.m
    needs to be changed to //chrome/browser/resources/settings:router now,
    since the same upstream change also changed that target's name and,
    coincidentally, we need to import now router.js instead of router.m.js.
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src.git/+/1795a2f85eb10f422dfaffe50c5038acd73336a3
    
    commit 1795a2f85eb10f422dfaffe50c5038acd73336a3
    Author: rbpotter <rbpotter@chromium.org>
    Date:   Fri Mar 12 09:54:23 2021 +0000
    
        Settings: Check in autogenerated files from settings:modulize
    
        Check in files generated by js_modulizer target in the main
        c/b/r/settings folder.
    
        This means that browser settings no longer needs migrated imports or
        auto imports, so move these values to os_settings.gni.
    
        Note that settings_namespace_rewrites is still needed for tests.
    
        Bug: 1184052

--- browser/resources/settings/brave_sync_page/brave_sync_page.js
+++ browser/resources/settings/brave_sync_page/brave_sync_page.js
@@ -19,7 +19,7 @@ import {I18nBehavior} from 'chrome://resources/js/i18n_behavior.m.js';
 import {WebUIListenerBehavior} from 'chrome://resources/js/web_ui_listener_behavior.m.js';
 import {Polymer, html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
-import {Router} from '../router.m.js';
+import {Router} from '../router.js';
 import {SyncBrowserProxyImpl} from '../people_page/sync_browser_proxy.m.js';
 import {BraveSyncBrowserProxy} from './brave_sync_browser_proxy.js';
 
