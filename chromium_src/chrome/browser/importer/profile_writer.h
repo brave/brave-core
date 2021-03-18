edccf9cc8283aad73045841ae444bd0529599f76
Mario Sanchez Prada <mario@igalia.com>
2021-03-18 19:05:49 +0100

    Remove chrome-browser-importer-profile_writer.h.patch
    This patch could be removed in favour of a chromium_src override that
    would add the new AddCreditCard() method into place using the already
    existing AddAutofillFormDataEntries() method as leverage.
    
    Resolves https://github.com/brave/brave-browser/issues/14817

--- chromium_src/chrome/browser/importer/profile_writer.h
+++ chromium_src/chrome/browser/importer/profile_writer.h
@@ -10,15 +10,16 @@
 
 // Extends ProfileWriter by file overriding instead of subclassing because there
 // are many places that instantiate it.
-#define BRAVE_PROFILE_WRITER_H                                          \
-    void AddCreditCard(const base::string16& name_on_card,              \
-                       const base::string16& expiration_month,          \
-                       const base::string16& expiration_year,           \
-                       const base::string16& decrypted_card_number,     \
-                       const std::string& origin);
+#define AddAutofillFormDataEntries                           \
+  AddCreditCard(const base::string16& name_on_card,          \
+                const base::string16& expiration_month,      \
+                const base::string16& expiration_year,       \
+                const base::string16& decrypted_card_number, \
+                const std::string& origin);                  \
+  virtual void AddAutofillFormDataEntries
 
 #include "../../../../../chrome/browser/importer/profile_writer.h"
 
-#undef BRAVE_PROFILE_WRITER_H
+#undef AddAutofillFormDataEntries
 
 #endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_IMPORTER_PROFILE_WRITER_H_
