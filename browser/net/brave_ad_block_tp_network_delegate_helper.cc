f20e8ba35f798ee3a8206512705d7b708e67c090
Brian Clifton <brian@clifton.me>
2021-03-23 11:11:09 -0700

    Revert "Merge pull request #8288 from brave/pr8279_cname-adblock-doh_1.22.x"
    Fixes https://github.com/brave/brave-browser/issues/14721 for 1.22.x
    
    Problem still in `1.23.x` and `master` branches
    
    Auditor: @darkdh
    
    This reverts commit 27cf5ef5b8310c4903af963065732873b3ffc7ee, reversing
    changes made to e104d7cec5b00deb11a7b04137cb361efff1d587.

--- browser/net/brave_ad_block_tp_network_delegate_helper.cc
+++ browser/net/brave_ad_block_tp_network_delegate_helper.cc
@@ -21,8 +21,6 @@
 #include "brave/components/brave_shields/browser/brave_shields_web_contents_observer.h"
 #include "brave/components/brave_shields/common/brave_shield_constants.h"
 #include "brave/grit/brave_generated_resources.h"
-#include "chrome/browser/net/secure_dns_config.h"
-#include "chrome/browser/net/system_network_context_manager.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_frame_host.h"
@@ -144,15 +142,10 @@ class AdblockCnameResolveHostClient : public network::mojom::ResolveHostClient {
     network::mojom::ResolveHostParametersPtr optional_parameters =
         network::mojom::ResolveHostParameters::New();
     optional_parameters->include_canonical_name = true;
-
-    SecureDnsConfig secure_dns_config =
-        SystemNetworkContextManager::GetStubResolverConfigReader()
-            ->GetSecureDnsConfiguration(false);
-    // Explicitly specify source when DNS over HTTPS is enabled to avoid
-    // using `HostResolverProc` which will be handled by system resolver
+    // Explicitly specify source to avoid using `HostResolverProc`
+    // which will be handled by system resolver
     // See https://crbug.com/872665
-    if (secure_dns_config.mode() == net::SecureDnsMode::kSecure)
-      optional_parameters->source = net::HostResolverSource::DNS;
+    optional_parameters->source = net::HostResolverSource::DNS;
 
     network::mojom::NetworkContext* network_context =
         content::BrowserContext::GetDefaultStoragePartition(context)
