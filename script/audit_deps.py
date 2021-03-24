92d06ab7466af5f7bed77724329793b1c0678f64
Wojciech Knapik <wmknapik@gmail.com>
2021-03-24 22:03:54 +0100

    Fix.

--- script/audit_deps.py
+++ script/audit_deps.py
@@ -13,16 +13,15 @@ import sys
 
 from rust_deps_config import RUST_DEPS_PACKAGE_VERSION
 
-# Exclude these paths (and their subdirectories) from auditing (npm and cargo).
-EXCLUDE_PATHS = [
+# Use all (sub)paths except these for npm audit.
+NPM_EXCLUDE_PATHS = [
     'build',
     os.path.join('components', 'brave_sync', 'extension', 'brave-sync', 'node_modules'),
     os.path.join('node_modules'),
     os.path.join('vendor', 'brave-extension', 'node_modules'),
 ]
 
-# Only inspect these paths (and their subdirectories) in cargo_audit_deps().
-# The exclusions above take precedence over these inclusions.
+# Use only these (sub)paths for cargo audit.
 CARGO_INCLUDE_PATHS = [
     os.path.join('build', 'rust'),
 ]
@@ -46,30 +45,29 @@ def main():
     for dir_path, dirs, dummy in os.walk(args.source_root):
         for dir_name in dirs:
             full_path = os.path.join(dir_path, dir_name)
-            skip_dir = False
-            for exclusion in EXCLUDE_PATHS:
-                if full_path.startswith(os.path.join(args.source_root, exclusion)):
-                    skip_dir = True
-                    break
-
-            if not skip_dir:
-                errors += audit_path(full_path, args)
+            errors += audit_path(full_path, args)
 
     return errors > 0
 
 
 def audit_path(path, args):
-    return npm_audit_deps(path, args) and cargo_audit_deps(path, args)
+    full_path = os.path.join(os.path.abspath(path), "")
+    if os.path.isfile(os.path.join(path, 'package.json')) and \
+       os.path.isfile(os.path.join(path, 'package-lock.json')) and \
+       os.path.isdir(os.path.join(path, 'node_modules')) and \
+       not any(full_path.startswith(os.path.join(args.source_root, p, "")) for p in NPM_EXCLUDE_PATHS):
+        print('Auditing (npm) %s' % path)
+        return npm_audit_deps(path, args)
+    elif os.path.isfile(os.path.join(path, 'Cargo.toml')) and \
+         os.path.isfile(os.path.join(path, 'Cargo.lock')) and \
+         any(full_path.startswith(os.path.join(args.source_root, p, "")) for p in CARGO_INCLUDE_PATHS):
+        print('Auditing (cargo) %s' % path)
+        return cargo_audit_deps(path, args)
+
+    return 0
 
 
 def npm_audit_deps(path, args):
-    if os.path.isfile(os.path.join(path, 'package.json')) and \
-       os.path.isfile(os.path.join(path, 'package-lock.json')) and \
-       os.path.isdir(os.path.join(path, 'node_modules')):
-        print('Auditing (npm) %s' % path)
-    else:
-        return 0
-
     npm_cmd = 'npm'
     if sys.platform.startswith('win'):
         npm_cmd = 'npm.cmd'
@@ -105,14 +103,6 @@ def npm_audit_deps(path, args):
 
 
 def cargo_audit_deps(path, args):
-    full_path = os.path.join(args.source_root, path)
-    if os.path.isfile(os.path.join(path, 'Cargo.toml')) and \
-         os.path.isfile(os.path.join(path, 'Cargo.lock')) and \
-         any(full_path.startswith(p) for p in CARGO_INCLUDE_PATHS):
-        print('Auditing (cargo) %s' % path)
-    else:
-        return 0
-
     rustup_path = args.rustup_path
     cargo_path = args.cargo_path
 
