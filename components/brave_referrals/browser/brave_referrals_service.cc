0d38d1a4ff489ecd69aa46d572d969e22c248ccb
Kevin Kuehler <kevin@segfault.fun>
2021-03-30 17:15:04 -0700

    referrals: stats: Always RETRY_ON_NETWORK_CHANGE
    Resolves https://github.com/brave/brave-browser/issues/15034

--- components/brave_referrals/browser/brave_referrals_service.cc
+++ components/brave_referrals/browser/brave_referrals_service.cc
@@ -583,6 +583,8 @@ void BraveReferralsService::FetchReferralHeaders() {
   referral_headers_loader_ = network::SimpleURLLoader::Create(
       std::move(resource_request), traffic_annotation);
   referral_headers_loader_->SetAllowHttpErrorResults(true);
+  referral_headers_loader_->SetRetryOptions(
+      1, network::SimpleURLLoader::RetryMode::RETRY_ON_NETWORK_CHANGE);
   referral_headers_loader_->DownloadToString(
       loader_factory,
       base::BindOnce(&BraveReferralsService::OnReferralHeadersLoadComplete,
@@ -696,6 +698,8 @@ void BraveReferralsService::CheckForReferralFinalization() {
   referral_finalization_check_loader_->SetAllowHttpErrorResults(true);
   referral_finalization_check_loader_->AttachStringForUpload(
       BuildReferralFinalizationCheckPayload(), "application/json");
+  referral_finalization_check_loader_->SetRetryOptions(
+      1, network::SimpleURLLoader::RetryMode::RETRY_ON_NETWORK_CHANGE);
   referral_finalization_check_loader_->DownloadToString(
       loader_factory,
       base::BindOnce(
