WindevStudios/browse-core

Add anti-targeting frequency capping for Brave Ads

vendor/bat-native-ads/src/bat/ads/internal/ad_pacing/ad_notifications/ad_notification_pacing_unittest.cc
@@ -6,6 +6,7 @@
 #include "bat/ads/internal/ad_serving/ad_notifications/ad_notification_serving.h"
 #include "bat/ads/internal/ad_serving/ad_targeting/geographic/subdivision/subdivision_targeting.h"
 #include "bat/ads/internal/ad_targeting/ad_targeting.h"
+#include "bat/ads/internal/ad_targeting/resources/frequency_capping/anti_targeting_resource.h"
 #include "bat/ads/internal/unittest_base.h"
 #include "bat/ads/internal/unittest_util.h"
 
@@ -37,9 +38,11 @@ class BatAdsAdNotificationPacingTest : public UnitTestBase {
       : ad_targeting_(std::make_unique<AdTargeting>()),
         subdivision_targeting_(
             std::make_unique<ad_targeting::geographic::SubdivisionTargeting>()),
+        anti_targeting_(std::make_unique<resource::AntiTargeting>()),
         ad_serving_(std::make_unique<ad_notifications::AdServing>(
             ad_targeting_.get(),
-            subdivision_targeting_.get())) {}
+            subdivision_targeting_.get(),
+            anti_targeting_.get())) {}
 
   ~BatAdsAdNotificationPacingTest() override = default;
 
@@ -94,6 +97,7 @@ class BatAdsAdNotificationPacingTest : public UnitTestBase {
   std::unique_ptr<AdTargeting> ad_targeting_;
   std::unique_ptr<ad_targeting::geographic::SubdivisionTargeting>
       subdivision_targeting_;
+  std::unique_ptr<resource::AntiTargeting> anti_targeting_;
   std::unique_ptr<ad_notifications::AdServing> ad_serving_;
 
   std::vector<CreativeAdNotificationInfo> test_creative_notifications_;

vendor/bat-native-ads/src/bat/ads/internal/ad_targeting/resources/frequency_capping/anti_targeting_resource.cc
@@ -1,0 +1,121 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "bat/ads/internal/ad_targeting/resources/frequency_capping/anti_targeting_resource.h"
+
+#include <utility>
+#include <vector>
+
+#include "base/json/json_reader.h"
+#include "bat/ads/internal/ads_client_helper.h"
+#include "bat/ads/internal/logging.h"
+#include "bat/ads/result.h"
+#include "brave/components/l10n/common/locale_util.h"
+
+namespace ads {
+namespace resource {
+
+namespace {
+const int kCurrentVersion = 1;
+// TODO(Moritz Haller): generate proper ID instead of "antitargetinglists"
+const char kResourceId[] = "antitargetinglists";
+}  // namespace
+
+AntiTargeting::AntiTargeting() = default;
+
+AntiTargeting::~AntiTargeting() = default;
+
+bool AntiTargeting::IsInitialized() const {
+  return is_initialized_;
+}
+
+void AntiTargeting::Load() {
+  AdsClientHelper::Get()->LoadUserModelForId(
+      kResourceId, [=](const Result result, const std::string& json) {
+        if (result != SUCCESS) {
+          BLOG(1, "Failed to load resource " << kResourceId);
+          is_initialized_ = false;
+          return;
+        }
+
+        BLOG(1, "Successfully loaded resource " << kResourceId);
+
+        if (!FromJson(json)) {
+          BLOG(1, "Failed to initialize resource " << kResourceId);
+          is_initialized_ = false;
+          return;
+        }
+
+        is_initialized_ = true;
+
+        BLOG(1, "Successfully initialized resource " << kResourceId);
+      });
+}
+
+AntiTargetingInfo AntiTargeting::get() const {
+  return anti_targeting_;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+bool AntiTargeting::FromJson(const std::string& json) {
+  AntiTargetingInfo anti_targeting;
+
+  base::Optional<base::Value> root = base::JSONReader::Read(json);
+  if (!root) {
+    BLOG(1, "Failed to load from JSON, root missing");
+    return false;
+  }
+
+  if (base::Optional<int> version = root->FindIntPath("version")) {
+    if (kCurrentVersion != *version) {
+      BLOG(1, "Failed to load from JSON, version missing");
+      return false;
+    }
+
+    anti_targeting.version = *version;
+  }
+
+  base::Value* site_lists_value = root->FindDictPath("sites");
+  if (!site_lists_value) {
+    BLOG(1, "Failed to load from JSON, sites missing");
+    return false;
+  }
+
+  if (!site_lists_value->is_dict()) {
+    BLOG(1, "Failed to load from JSON, sites not of type dict");
+    return false;
+  }
+
+  base::DictionaryValue* dict;
+  if (!site_lists_value->GetAsDictionary(&dict)) {
+    BLOG(1, "Failed to load from JSON, get sites as dict");
+    return false;
+  }
+
+  for (base::DictionaryValue::Iterator iter(*dict); !iter.IsAtEnd();
+       iter.Advance()) {
+    if (!iter.value().is_list()) {
+      BLOG(1, "Failed to load from JSON, sites not of type list")
+      return false;
+    }
+
+    std::vector<std::string> sites;
+    for (const auto& site : iter.value().GetList()) {
+      sites.push_back(site.GetString());
+    }
+
+    anti_targeting.sites.insert({iter.key(), sites});
+  }
+
+  anti_targeting_ = anti_targeting;
+
+  BLOG(1, "Parsed anti targeting resource version " << anti_targeting_.version);
+
+  return true;
+}
+
+}  // namespace resource
+}  // namespace ads

brave/brave-core

vendor/bat-native-ads/src/bat/ads/internal/ad_pacing/ad_notifications/ad_notification_pacing_unittest.cc
@@ -6,6 +6,7 @@
 #include "bat/ads/internal/ad_serving/ad_notifications/ad_notification_serving.h"
 #include "bat/ads/internal/ad_serving/ad_targeting/geographic/subdivision/subdivision_targeting.h"
 #include "bat/ads/internal/ad_targeting/ad_targeting.h"
+#include "bat/ads/internal/ad_targeting/resources/frequency_capping/anti_targeting_resource.h"
 #include "bat/ads/internal/unittest_base.h"
 #include "bat/ads/internal/unittest_util.h"
 
@@ -37,9 +38,11 @@ class BatAdsAdNotificationPacingTest : public UnitTestBase {
       : ad_targeting_(std::make_unique<AdTargeting>()),
         subdivision_targeting_(
             std::make_unique<ad_targeting::geographic::SubdivisionTargeting>()),
+        anti_targeting_resource_(std::make_unique<resource::AntiTargeting>()),
         ad_serving_(std::make_unique<ad_notifications::AdServing>(
             ad_targeting_.get(),
-            subdivision_targeting_.get())) {}
+            subdivision_targeting_.get(),
+            anti_targeting_resource_.get())) {}
 
   ~BatAdsAdNotificationPacingTest() override = default;
 
@@ -94,6 +97,7 @@ class BatAdsAdNotificationPacingTest : public UnitTestBase {
   std::unique_ptr<AdTargeting> ad_targeting_;
   std::unique_ptr<ad_targeting::geographic::SubdivisionTargeting>
       subdivision_targeting_;
+  std::unique_ptr<resource::AntiTargeting> anti_targeting_resource_;
   std::unique_ptr<ad_notifications::AdServing> ad_serving_;
 
   std::vector<CreativeAdNotificationInfo> test_creative_notifications_;

vendor/bat-native-ads/src/bat/ads/internal/ad_targeting/resources/frequency_capping/anti_targeting_resource.cc
@@ -1,0 +1,120 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "bat/ads/internal/ad_targeting/resources/frequency_capping/anti_targeting_resource.h"
+
+#include <utility>
+#include <vector>
+
+#include "base/json/json_reader.h"
+#include "bat/ads/internal/ads_client_helper.h"
+#include "bat/ads/internal/logging.h"
+#include "bat/ads/result.h"
+#include "brave/components/l10n/common/locale_util.h"
+
+namespace ads {
+namespace resource {
+
+namespace {
+const int kCurrentVersion = 1;
+const char kResourceId[] = "mkdhnfmjhklfnamlheoliekgeohamoig";
+}  // namespace
+
+AntiTargeting::AntiTargeting() = default;
+
+AntiTargeting::~AntiTargeting() = default;
+
+bool AntiTargeting::IsInitialized() const {
+  return is_initialized_;
+}
+
+void AntiTargeting::Load() {
+  AdsClientHelper::Get()->LoadUserModelForId(
+      kResourceId, [=](const Result result, const std::string& json) {
+        if (result != SUCCESS) {
+          BLOG(1, "Failed to load resource " << kResourceId);
+          is_initialized_ = false;
+          return;
+        }
+
+        BLOG(1, "Successfully loaded resource " << kResourceId);
+
+        if (!FromJson(json)) {
+          BLOG(1, "Failed to initialize resource " << kResourceId);
+          is_initialized_ = false;
+          return;
+        }
+
+        is_initialized_ = true;
+
+        BLOG(1, "Successfully initialized resource " << kResourceId);
+      });
+}
+
+AntiTargetingInfo AntiTargeting::get() const {
+  return anti_targeting_;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+bool AntiTargeting::FromJson(const std::string& json) {
+  AntiTargetingInfo anti_targeting;
+
+  base::Optional<base::Value> root = base::JSONReader::Read(json);
+  if (!root) {
+    BLOG(1, "Failed to load from JSON, root missing");
+    return false;
+  }
+
+  if (base::Optional<int> version = root->FindIntPath("version")) {
+    if (kCurrentVersion != *version) {
+      BLOG(1, "Failed to load from JSON, version missing");
+      return false;
+    }
+
+    anti_targeting.version = *version;
+  }
+
+  base::Value* site_lists_value = root->FindDictPath("sites");
+  if (!site_lists_value) {
+    BLOG(1, "Failed to load from JSON, sites missing");
+    return false;
+  }
+
+  if (!site_lists_value->is_dict()) {
+    BLOG(1, "Failed to load from JSON, sites not of type dict");
+    return false;
+  }
+
+  base::DictionaryValue* dict;
+  if (!site_lists_value->GetAsDictionary(&dict)) {
+    BLOG(1, "Failed to load from JSON, get sites as dict");
+    return false;
+  }
+
+  for (base::DictionaryValue::Iterator iter(*dict); !iter.IsAtEnd();
+       iter.Advance()) {
+    if (!iter.value().is_list()) {
+      BLOG(1, "Failed to load from JSON, sites not of type list")
+      return false;
+    }
+
+    std::vector<std::string> sites;
+    for (const auto& site : iter.value().GetList()) {
+      sites.push_back(site.GetString());
+    }
+
+    anti_targeting.sites.insert({iter.key(), sites});
+  }
+
+  anti_targeting_ = anti_targeting;
+
+  BLOG(1, "Parsed anti targeting resource version " << anti_targeting_.version);
+
+  return true;
+}
+
+}  // namespace resource
+}  // namespace ads