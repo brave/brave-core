710ac00a77a70215ebf79282a14f261686ea6a77
Emerick Rogul <erogul@brave.com>
2021-04-05 13:48:27 -0400

    Fix statistical voting for case where publisher receives no votes

--- vendor/bat-native-ledger/src/bat/ledger/internal/contribution/contribution_unblinded_unittest.cc
+++ vendor/bat-native-ledger/src/bat/ledger/internal/contribution/contribution_unblinded_unittest.cc
@@ -92,5 +92,51 @@ TEST_F(UnblindedTest, NotEnoughFunds) {
       });
 }
 
+TEST_F(UnblindedTest, GetStatisticalVotingWinner) {
+  ledger::type::ContributionPublisherList publisher_list;
+
+  auto publisher1 = type::ContributionPublisher::New();
+  publisher1->publisher_key = "publisher1";
+  publisher1->total_amount = 2.0;
+  publisher_list.push_back(std::move(publisher1));
+
+  auto publisher2 = type::ContributionPublisher::New();
+  publisher2->publisher_key = "publisher2";
+  publisher2->total_amount = 13.0;
+  publisher_list.push_back(std::move(publisher2));
+
+  auto publisher3 = type::ContributionPublisher::New();
+  publisher3->publisher_key = "publisher3";
+  publisher3->total_amount = 14.0;
+  publisher_list.push_back(std::move(publisher3));
+
+  auto publisher4 = type::ContributionPublisher::New();
+  publisher4->publisher_key = "publisher4";
+  publisher4->total_amount = 23.0;
+  publisher_list.push_back(std::move(publisher4));
+
+  auto publisher5 = type::ContributionPublisher::New();
+  publisher5->publisher_key = "publisher5";
+  publisher5->total_amount = 38.0;
+  publisher_list.push_back(std::move(publisher5));
+
+  struct {
+    double dart;
+    const char* publisher_key;
+  } cases[] = {
+      {0.01, "publisher1"}, {0.05, "publisher2"}, {0.10, "publisher2"},
+      {0.20, "publisher3"}, {0.30, "publisher4"}, {0.40, "publisher4"},
+      {0.50, "publisher4"}, {0.60, "publisher5"}, {0.70, "publisher5"},
+      {0.80, "publisher5"}, {0.90, "publisher5"},
+  };
+
+  for (size_t i = 0; i < base::size(cases); i++) {
+    const std::string publisher_key =
+        unblinded_->GetStatisticalVotingWinnerForTesting(cases[i].dart, 100.0,
+                                                         publisher_list);
+    EXPECT_STREQ(publisher_key.c_str(), cases[i].publisher_key);
+  }
+}
+
 }  // namespace contribution
 }  // namespace ledger
