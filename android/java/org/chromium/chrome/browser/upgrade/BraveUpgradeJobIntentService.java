358b417c4bb3a378a1eb61364de896aa10b9546b
wchen342 <feiyu2817@gmail.com>
2021-04-07 16:38:35 +0300

    Move BraveUpgradeJobIntentService to base module

--- android/java/org/chromium/chrome/browser/upgrade/BraveUpgradeJobIntentService.java
+++ android/java/org/chromium/chrome/browser/upgrade/BraveUpgradeJobIntentService.java
@@ -5,66 +5,12 @@
 
 package org.chromium.chrome.browser.upgrade;
 
-import android.content.Context;
-import android.content.Intent;
+import org.chromium.chrome.browser.base.SplitCompatJobIntentService;
+import org.chromium.chrome.browser.base.SplitCompatUtils;
 
-import androidx.annotation.NonNull;
-import androidx.core.app.JobIntentService;
-
-import org.chromium.base.Callback;
-import org.chromium.base.Log;
-import org.chromium.base.task.PostTask;
-import org.chromium.chrome.browser.BraveFeatureList;
-import org.chromium.chrome.browser.flags.ChromeFeatureList;
-import org.chromium.chrome.browser.preferences.BravePrefServiceBridge;
-import org.chromium.chrome.browser.util.SafetyNetCheck;
-import org.chromium.chrome.browser.util.TabUtils;
-import org.chromium.content_public.browser.BrowserStartupController;
-import org.chromium.content_public.browser.UiThreadTaskTraits;
-
-public class BraveUpgradeJobIntentService extends JobIntentService {
-    private static final String TAG = "BraveUpgradeJobIntentService";
-
-    private static final int JOB_ID = 1;
-
-    public static void maybePerformUpgradeTasks(Context context) {
-        BraveUpgradeJobIntentService.enqueueWork(context, new Intent());
-    }
-
-    private static void enqueueWork(Context context, Intent work) {
-        enqueueWork(context, BraveUpgradeJobIntentService.class, JOB_ID, work);
-    }
-
-    @Override
-    protected void onHandleWork(@NonNull Intent intent) {
-        PostTask.runOrPostTask(UiThreadTaskTraits.DEFAULT, () -> {
-            BrowserStartupController.getInstance().addStartupCompletedObserver(
-                    new BrowserStartupController.StartupCallback() {
-                        @Override
-                        public void onSuccess() {
-                            if (ChromeFeatureList.isEnabled(BraveFeatureList.BRAVE_REWARDS)
-                                    && BravePrefServiceBridge.getInstance()
-                                               .getSafetynetCheckFailed()) {
-                                Callback<Boolean> callback = value -> {
-                                    if (value == null || !value.booleanValue()) {
-                                        return;
-                                    }
-                                    // Reset flag and update UI
-                                    BravePrefServiceBridge.getInstance().setSafetynetCheckFailed(
-                                            false);
-                                    TabUtils.enableRewardsButton();
-                                };
-                                // Re-perform safetynet check
-                                SafetyNetCheck.updateSafetynetStatus(callback);
-                            }
-                        }
-
-                        @Override
-                        public void onFailure() {
-                            Log.e(TAG,
-                                    "Failed to perform upgrade tasks: BrowserStartupController.StartupCallback failed");
-                        }
-                    });
-        });
+public class BraveUpgradeJobIntentService extends SplitCompatJobIntentService {
+    public BraveUpgradeJobIntentService() {
+        super(SplitCompatUtils.getIdentifierName(
+                "org.chromium.chrome.browser.upgrade.BraveUpgradeJobIntentServiceImpl"));
     }
 }
