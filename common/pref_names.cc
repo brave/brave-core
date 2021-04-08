6ca583c2dc1a1879787137df7fff9c3d57f38d66
brave-builds <devops@brave.com>
2021-04-08 06:32:32 +0000

    Uplift of #8474 (squashed) to beta

--- common/pref_names.cc
+++ common/pref_names.cc
@@ -88,7 +88,7 @@ const char kBraveDarkMode[] = "brave.dark_mode";
 const char kOtherBookmarksMigrated[] = "brave.other_bookmarks_migrated";
 const char kBraveShieldsSettingsVersion[] = "brave.shields_settings_version";
 const char kDefaultBrowserPromptEnabled[] =
-    "brave.default_browser_prompot_enabled";
+    "brave.default_browser_prompt_enabled";
 #if !BUILDFLAG(USE_GCM_FROM_PLATFORM)
 const char kBraveGCMChannelStatus[] = "brave.gcm.channel_status";
 #endif
