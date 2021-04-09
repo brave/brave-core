31544911c6e8e4a816ada27742dd94794f163ead
Wojciech Knapik <wmknapik@gmail.com>
2021-04-10 00:48:40 +0200

    Small improvements in pylint.sh.

--- script/pylint.sh
+++ script/pylint.sh
@@ -6,7 +6,7 @@
 shopt -s inherit_errexit
 set -eEo pipefail
 
-# Defaults.
+# Defaults. Overwritten in parse_cmdline() and set readonly.
 check_folders=(build components installer script tools)
 only_new=0
 pylint_options=(-j0 -rn --rcfile=.pylintrc)
@@ -21,11 +21,11 @@ help() {
 
 	Run pylint on python files in: ${check_folders[*]}
 
-	--compare-with <base_branch>: only analyse files changed compared to base_branch
+	--compare-with <base_branch>: only analyse files changed relative to base_branch
 	--only-new: exclude old findings from <base_branch> (NOT IMPLEMENTED YET)
 	--report: produce a parseable report file
 	
-	Calling via \`npm run', e.g.:
+	This script should be called via \`npm run', e.g.:
 	npm run pylint
 	npm run pylint -- --compare-with master
 	EOF
@@ -37,17 +37,21 @@ help() {
 error() { printf "%s\n\n" "ERROR: $*" >&2; help; }
 
 # Parse command line arguments.
-for ((i=1; i <= $#; ++i)); do
-    case "${@:$i:1}" in
-        --compare-with)
-            ((++i <= $#)) || error "The --compare-with option requires an argument";
-            base_branch="${*:$i:1}";;
-        --only-new) only_new=1;;
-        --report) report=1;;
-        --help) help;;
-        *) error "Extraneous arguments passed: ${*:$i}";;
-    esac
-done
+# $@ := script_arguments
+parse_cmdline() {
+    for ((i=1; i <= $#; ++i)); do
+        case "${@:$i:1}" in
+            --compare-with)
+                ((++i <= $#)) || error "The --compare-with option requires an argument";
+                base_branch="${*:$i:1}";;
+            --only-new) only_new=1;;
+            --report) report=1;;
+            --help) help;;
+            *) error "Extraneous arguments passed: ${*:$i}";;
+        esac
+    done
+    readonly check_folders base_branch only_new report report_file
+}
 
 # Print a tab-delimited list of python scripts changed since $base_branch.
 # $@ := ""
@@ -72,14 +76,26 @@ check() {
     fi
 }
 
-# Validate options and decide which files to check.
-case "$only_new,$base_branch" in
-    1,)  error "ERROR: The --only-new option is only valid with --compare-with";;
-    1,*) #read -ra check_files <<<"$(get_changed_files)"
-         #check "new pylint findings compared with $base_branch in: ${check_folders[*]}"
-         error "The --only-new option is not implemented yet";;
-    0,)  check_files=("${check_folders[@]}")
-         check "pylint findings in: ${check_folders[*]}";;
-    0,*) read -ra check_files <<<"$(get_changed_files)"
-         check "pylint findings compared with $base_branch in: ${check_folders[*]}";;
-esac
+# $@ := script_arguments
+main() {
+    parse_cmdline "$@"
+
+    # Decide which files to check and call check() to execute pylint.
+    case "$only_new,$base_branch" in
+        1,)  error "ERROR: The --only-new option is only valid with --compare-with";;
+
+        1,*) #read -ra check_files <<<"$(get_changed_files)"
+             #check "new pylint findings in python scripts changed relative to"\
+             #    "$base_branch in: ${check_folders[*]}"
+             error "The --only-new option is not implemented yet";;
+
+        0,)  check_files=("${check_folders[@]}")
+             check "pylint findings in: ${check_folders[*]}";;
+
+        0,*) read -ra check_files <<<"$(get_changed_files)"
+             check "pylint findings in python scripts changed relative to"\
+                 "$base_branch in: ${check_folders[*]}";;
+    esac
+}
+
+main "$@"
