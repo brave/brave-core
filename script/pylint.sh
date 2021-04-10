88dafcf53f42c846088b6913c95c2da91478fdb6
Wojciech Knapik <wmknapik@gmail.com>
2021-04-10 14:53:21 +0200

    Removed unnecessary code.

--- script/pylint.sh
+++ script/pylint.sh
@@ -8,7 +8,6 @@ set -eEo pipefail
 
 # Defaults. Overwritten in parse_cmdline() and set readonly.
 check_folders=(build components installer script tools)
-only_new=0
 pylint_options=(-j0 -rn --rcfile=.pylintrc)
 report=0
 report_file=pylint-report.txt
@@ -18,13 +17,12 @@ report_file=pylint-report.txt
 help() {
 	cat <<-EOF
 	USAGE: pylint.sh --help
-	       pylint.sh [--compare-with <base_branch> [--only-new]] [--report]
+	       pylint.sh [--compare-with <base_branch>] [--report]
 
 	Run pylint on versioned python scripts in:
 	${check_folders[*]/%/\/}
 
 	--compare-with <base_branch>: only analyse files changed relative to base_branch
-	--only-new: exclude old findings from <base_branch> (NOT IMPLEMENTED YET)
 	--report: produce a parseable report file
 	
 	The .pylintrc file at the top of this repo defines pylint options.
@@ -48,13 +46,12 @@ parse_cmdline() {
             --compare-with)
                 ((++i <= $#)) || error "The --compare-with option requires an argument";
                 base_branch="${*:$i:1}";;
-            --only-new) only_new=1;;
             --report) report=1;;
             --help) help 0;;
-            *) error "Extraneous arguments passed: ${*:$i}";;
+            *) error "Invalid arguments passed: ${*:$i}";;
         esac
     done
-    readonly base_branch check_folders only_new report report_file
+    readonly base_branch check_folders report report_file
 }
 
 # Print a tab-delimited list of python scripts changed relative to $base_branch.
@@ -89,20 +86,11 @@ main() {
 
     parse_cmdline "$@"
 
-    # Decide which files to check and call check() to execute pylint.
-    case "$only_new,$base_branch" in
-        1,)  error "ERROR: The --only-new option is only valid with --compare-with";;
-
-        1,*) #read -ra check_files <<<"$(get_changed_files)"
-             #check "new pylint findings in scripts changed relative to $base_branch" \
-             #      "${check_files[@]}"
-             error "The --only-new option is not implemented yet";;
-
-        0,)  check "pylint findings" "${check_folders[@]}";;
-
-        0,*) read -ra check_files <<<"$(get_changed_files)"
-             check "pylint findings in scripts changed relative to $base_branch" \
-                   "${check_files[@]}";;
+    case "$base_branch" in
+        "") check "pylint findings" "${check_folders[@]}";;
+        *)  read -ra check_files <<<"$(get_changed_files)"
+            check "pylint findings in scripts changed relative to $base_branch" \
+                  "${check_files[@]}";;
     esac
 }
 
