667f0d504304ac6fe6d2377825f9ea19cf0aab9e
Mihai PLESA <mihai.plesa@gmail.com>
2021-04-13 20:30:13 -0500

    fix

--- script/redirect-cc.py
+++ script/redirect-cc.py
@@ -4,28 +4,20 @@ import subprocess
 import os.path
 import os
 
-IS_WIN32 = sys.platform == 'win32'
-
 
 def main():
     args = sys.argv[1:]
-    brave_path = replace_cc_arg(args)
+    replace_cc_arg(args)
     if 'CC_WRAPPER' in os.environ:
         args = [os.environ['CC_WRAPPER']] + args
     cc_retcode = subprocess.call(args)
-    # To check the redirected file timestamp, it should be marked as dependency for ninja.
-    # Linux/MacOS gcc deps format includes this file properly.
-    # Windows msvc deps format does not include it, so we do it manually here.
-    if IS_WIN32 and cc_retcode == 0 and brave_path:
-        # This is a specially crafted string that ninja will look for to create deps.
-        sys.stderr.write('Note: including file: %s\n' % brave_path)
     return cc_retcode
 
 
 def replace_cc_arg(args):
     # Interested in -c <path>.cc
     try:
-        if IS_WIN32:
+        if sys.platform == 'win32':
             index_c = args.index('/c')
         else:
             index_c = args.index('-c')
@@ -83,7 +75,6 @@ def replace_cc_arg(args):
     if os.path.isfile(brave_path):
         # Okay, we can replace
         args[index_path] = os.path.relpath(brave_path, os.path.abspath('.'))
-        return brave_path
 
 
 if __name__ == '__main__':
