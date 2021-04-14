a1f855315a4dccd350ed0711377ec365e9a1cf35
mkarolin <max@brave.com>
2021-04-14 01:23:07 -0400

    PageInfoBubbleView was refactored.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src.git/+/3ed46b480719847f8745ddf43c2d39edeead6156
    
    commit 3ed46b480719847f8745ddf43c2d39edeead6156
    Author: Olesia Marukhno <olesiamarukhno@google.com>
    Date:   Tue Mar 23 15:26:39 2021 +0000
    
        Refactor PageInfoBubbleView
    
        Removed references to web_contents in bubble and mostly removed
        references to profile. This is needed for future redesign, to keep new
        views independent from web_contents and profile and keep all logic in
        presenter and delegates.
    
        Bug: 1188101

--- chromium_src/chrome/browser/ui/page_info/chrome_page_info_delegate.cc
+++ chromium_src/chrome/browser/ui/page_info/chrome_page_info_delegate.cc
@@ -3,10 +3,32 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "chrome/browser/ui/page_info/chrome_page_info_delegate.h"
+
+#include "brave/components/content_settings/core/browser/brave_content_settings_utils.h"
+#include "brave/components/ipfs/buildflags/buildflags.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/common/url_constants.h"
+
+#if BUILDFLAG(IPFS_ENABLED)
+#include "brave/components/ipfs/ipfs_constants.h"
+#include "brave/components/ipfs/ipfs_utils.h"
+
+namespace chrome {
+std::string GetIPFSLearnMoreURL(const GURL& url) {
+  if (ipfs::IsIPFSScheme(url))
+    return ipfs::kIPFSLearnMoreURL;
+  return std::string(chrome::kPageInfoHelpCenterURL);
+}
+}  // namespace chrome
+
+#define kPageInfoHelpCenterURL GetIPFSLearnMoreURL(web_contents_->GetURL())
+#endif  // BUILDFLAG(IPFS_ENABLED)
 
 #include "../../../../../../chrome/browser/ui/page_info/chrome_page_info_delegate.cc"
-#include "brave/components/content_settings/core/browser/brave_content_settings_utils.h"
+#if BUILDFLAG(IPFS_ENABLED)
+#undef kPageInfoHelpCenterURL
+#endif  // BUILDFLAG(IPFS_ENABLED)
 
 bool ChromePageInfoDelegate::BraveShouldShowPermission(
     ContentSettingsType type) {
