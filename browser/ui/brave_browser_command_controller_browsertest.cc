feb4a38d3f760c80f6f4cc8d1c00ad974f9a31f0
Mario Sanchez Prada <mario@igalia.com>
2021-04-15 19:16:43 +0200

    Replace use of NOTIFICATION_BROWSER_OPENED with BrowserListObserver
    These kind of notifications are gone now upstream so we need to adapt
    Brave-specific code in tests to use BrowserListObserver and rely in
    its OnBrowserAdded() method instead to wait for the right moment in
    those different browser tests where waiting is required.
    
    commit 4e16ce6eeb534f1bc7d703050e75af243fcd94b0
    Author: Evan Stade <estade@chromium.org>
    Date:   Thu Apr 8 20:06:26 2021 +0000
    
        [Code health] Remove NOTIFICATION_BROWSER_OPENED
    
        ... and some other notifications that were used in
        AppShimTerminationManagerImpl.
    
        Bug: 1174784,1174777,1174776

--- browser/ui/brave_browser_command_controller_browsertest.cc
+++ browser/ui/brave_browser_command_controller_browsertest.cc
@@ -18,6 +18,7 @@
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/ui_test_utils.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/test_utils.h"
@@ -111,22 +112,13 @@ IN_PROC_BROWSER_TEST_F(BraveBrowserCommandControllerTest,
                   IDC_SHOW_BRAVE_WEBCOMPAT_REPORTER));
 
   // Create guest browser and test its brave commands status.
-  content::WindowedNotificationObserver browser_creation_observer(
-      chrome::NOTIFICATION_BROWSER_OPENED,
-      content::NotificationService::AllSources());
+  ui_test_utils::BrowserChangeObserver browser_creation_observer(
+      nullptr, ui_test_utils::BrowserChangeObserver::ChangeType::kAdded);
   profiles::SwitchToGuestProfile(ProfileManager::CreateCallback());
 
-  browser_creation_observer.Wait();
-
-  auto* browser_list = BrowserList::GetInstance();
-  Browser* guest_browser = nullptr;
-  for (Browser* browser : *browser_list) {
-    if (browser->profile()->IsGuestSession()) {
-      guest_browser = browser;
-      break;
-    }
-  }
+  Browser* guest_browser = browser_creation_observer.Wait();
   DCHECK(guest_browser);
+  EXPECT_TRUE(guest_browser->profile()->IsGuestSession());
   command_controller = guest_browser->command_controller();
 #if BUILDFLAG(BRAVE_REWARDS_ENABLED)
   EXPECT_FALSE(command_controller->IsCommandEnabled(IDC_SHOW_BRAVE_REWARDS));
@@ -156,19 +148,12 @@ IN_PROC_BROWSER_TEST_F(BraveBrowserCommandControllerTest,
 
 #if BUILDFLAG(ENABLE_TOR)
   // Launch tor window and check its command status.
-  content::WindowedNotificationObserver tor_browser_creation_observer(
-      chrome::NOTIFICATION_BROWSER_OPENED,
-      content::NotificationService::AllSources());
+  ui_test_utils::BrowserChangeObserver tor_browser_creation_observer(
+      nullptr, ui_test_utils::BrowserChangeObserver::ChangeType::kAdded);
   brave::NewOffTheRecordWindowTor(browser());
-  tor_browser_creation_observer.Wait();
-  Browser* tor_browser = nullptr;
-  for (Browser* browser : *browser_list) {
-    if (browser->profile()->IsTor()) {
-      tor_browser = browser;
-      break;
-    }
-  }
+  Browser* tor_browser = tor_browser_creation_observer.Wait();
   DCHECK(tor_browser);
+  EXPECT_TRUE(tor_browser->profile()->IsTor());
   command_controller = tor_browser->command_controller();
 #if BUILDFLAG(BRAVE_REWARDS_ENABLED)
   EXPECT_TRUE(command_controller->IsCommandEnabled(IDC_SHOW_BRAVE_REWARDS));
