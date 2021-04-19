567a96b0dcbbf4cce5be95d3df5b7c9d6e45691c
Aleksey Khoroshilov <akhoroshilov@brave.com>
2021-04-19 23:01:16 +0700

    Always use valid ExecutionContext in HTMLCanvasElement::toDataURL.

--- chromium_src/third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
+++ chromium_src/third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
@@ -3,11 +3,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "base/auto_reset.h"
 #include "third_party/blink/public/platform/web_content_settings_client.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 
 #define BRAVE_TO_DATA_URL_INTERNAL                                          \
-  if (ExecutionContext* context = GetExecutionContext()) {                  \
+  if (ExecutionContext* context =                                           \
+          (scoped_execution_context_ ? scoped_execution_context_.Get()      \
+                                     : GetExecutionContext())) {            \
     if (WebContentSettingsClient* settings =                                \
             brave::GetContentSettingsClientFor(context)) {                  \
       brave::BraveSessionCache::From(*context).PerturbPixels(               \
@@ -16,5 +19,21 @@
   }
 
 #include "../../../../../../../../third_party/blink/renderer/core/html/canvas/html_canvas_element.cc"
+
+namespace blink {
+
+String HTMLCanvasElement::toDataURL(ScriptState* script_state,
+                                    const String& mime_type,
+                                    const ScriptValue& quality_argument,
+                                    ExceptionState& exception_state) const {
+  base::AutoReset<UntracedMember<ExecutionContext>>
+      execution_context_auto_reset(&scoped_execution_context_,
+                                   GetExecutionContext()
+                                       ? GetExecutionContext()
+                                       : ExecutionContext::From(script_state));
+  return toDataURL(mime_type, quality_argument, exception_state);
+}
+
+}  // namespace blink
 
 #undef BRAVE_TO_DATA_URL_INTERNAL
