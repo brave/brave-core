5540de29f06912a5ab163a26b56a6e8f9219443a
mkarolin <max@brave.com>
2021-04-19 17:04:14 -0400

    Revert "Adapts several py scripts to python3."
    Looks like we are back to python2 for now.
    
    This reverts commit 0f58435c0dfd2fe9279718b8708dceb8bfb00812.

--- vendor/brave-ios/scripts/compile-model.py
+++ vendor/brave-ios/scripts/compile-model.py
@@ -1,14 +1,17 @@
 import argparse
+import os
 import subprocess
+import shutil
 import sys
+import string
 
 def main():
     args = parse_args()
     compile_model(args.model[0], args.output[0])
 
 def compile_model(model, output):
-    xcode = subprocess.check_output(['xcode-select', '-print-path']).decode('utf-8')
-    subprocess.call(xcode.strip() + "/usr/bin/momc " + model + " " + output, shell=True)
+    xcode = subprocess.check_output(['xcode-select', '-print-path'])
+    subprocess.call(string.strip(xcode) + "/usr/bin/momc " + model + " " + output, shell=True)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Compile a CoreData model')
