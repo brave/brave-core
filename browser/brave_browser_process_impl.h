b878e2323866b42cadf974c584aa4b0a51d02d34
Anton Lazarev <antonok35@gmail.com>
2021-04-28 13:11:23 -0700

    remove TrackingProtectionService

--- browser/brave_browser_process_impl.h
+++ browser/brave_browser_process_impl.h
@@ -37,7 +37,6 @@ class AdBlockService;
 class AdBlockCustomFiltersService;
 class AdBlockRegionalServiceManager;
 class HTTPSEverywhereService;
-class TrackingProtectionService;
 }  // namespace brave_shields
 
 namespace brave_stats {
@@ -96,8 +95,6 @@ class BraveBrowserProcessImpl : public BraveBrowserProcess,
 #if BUILDFLAG(ENABLE_GREASELION)
   greaselion::GreaselionDownloadService* greaselion_download_service() override;
 #endif
-  brave_shields::TrackingProtectionService* tracking_protection_service()
-      override;
   brave_shields::HTTPSEverywhereService* https_everywhere_service() override;
   brave_component_updater::LocalDataFilesService* local_data_files_service()
       override;
@@ -152,8 +149,6 @@ class BraveBrowserProcessImpl : public BraveBrowserProcess,
   std::unique_ptr<greaselion::GreaselionDownloadService>
       greaselion_download_service_;
 #endif
-  std::unique_ptr<brave_shields::TrackingProtectionService>
-      tracking_protection_service_;
   std::unique_ptr<brave_shields::HTTPSEverywhereService>
       https_everywhere_service_;
   std::unique_ptr<brave_stats::BraveStatsUpdater> brave_stats_updater_;
