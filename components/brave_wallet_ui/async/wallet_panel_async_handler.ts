5536d82328460c342560de9d7c23b7b7a7d7fb4d
Pete Miller <miller.pete@gmail.com>
2021-04-28 22:30:03 -0700

    Wallet Panel API: put mojo API in a separate webpack chunk

--- components/brave_wallet_ui/async/wallet_panel_async_handler.ts
+++ components/brave_wallet_ui/async/wallet_panel_async_handler.ts
@@ -8,9 +8,14 @@ import AsyncActionHandler from '../../common/AsyncActionHandler'
 import * as Actions from '../actions/wallet_panel_actions'
 import { State, WalletPanelReducerState } from '../constants/types'
 import { AccountPayloadType } from '../constants/action_types'
-import walletPanelApiProxy from '../wallet_panel_api_proxy.js'
 
 const handler = new AsyncActionHandler()
+
+async function getAPIProxy() {
+  // TODO(petemill): don't lazy import() if this actually makes the time-to-first-data slower!
+  const api = await import('../wallet_panel_api_proxy.js')
+  return api.default.getInstance()
+}
 
 function getState (store: MiddlewareAPI<Dispatch<AnyAction>, any>): WalletPanelReducerState {
   return (store.getState() as State).walletPanelReducer
@@ -26,7 +31,7 @@ handler.on(Actions.initialize.getType(), async (store) => {
   document.addEventListener('visibilitychange', () => {
     store.dispatch(Actions.visibilityChanged(document.visibilityState === 'visible'))
   })
-  const apiProxy = walletPanelApiProxy.getInstance()
+  const apiProxy = await getAPIProxy()
   apiProxy.showUI()
   // TODO: Fetch any data we need for initial display, instead of fake wait.
   await new Promise(resolve => setTimeout(resolve, 400))
@@ -35,22 +40,22 @@ handler.on(Actions.initialize.getType(), async (store) => {
 })
 
 handler.on(Actions.cancelConnectToSite.getType(), async (store) => {
-  const apiProxy = walletPanelApiProxy.getInstance()
+  const apiProxy = await getAPIProxy()
   console.log('cancel connect to site!')
   apiProxy.closeUI()
 })
 
 handler.on(Actions.connectToSite.getType(), async (store, payload: AccountPayloadType) => {
-  const apiProxy = walletPanelApiProxy.getInstance()
+  const apiProxy = await getAPIProxy()
   console.log('Got connect to site for accounts: ', payload.selectedAccounts, 'for site:', payload.siteToConnectTo)
   apiProxy.closeUI()
 })
 
-handler.on(Actions.visibilityChanged.getType(), (store, isVisible) => {
+handler.on(Actions.visibilityChanged.getType(), async (store, isVisible) => {
   if (!isVisible) {
     return
   }
-  const apiProxy = walletPanelApiProxy.getInstance()
+  const apiProxy = await getAPIProxy()
   apiProxy.showUI()
 })
 
