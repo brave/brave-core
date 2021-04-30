275f5e198997f03c870ac93b2c8781bbb0ca365f
Mario Sanchez Prada <mario@igalia.com>
2021-04-30 13:25:23 +0200

    Move chromium_src for IsURLHandledByNetworkService to the right path
    It should be for //services/network/public/cpp/url_util.h now.
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src.git/+/a277f0bd38143a3e76cd8004a7cd6bd0484a9ae9
    
    commit a277f0bd38143a3e76cd8004a7cd6bd0484a9ae9
    Author: Lukasz Anforowicz <lukasza@chromium.org>
    Date:   Thu Apr 22 16:22:11 2021 +0000
    
        Move IsURLHandledByNetworkService from Blink to //services/network.
    
        This CL moves the IsURLHandledByNetworkService helper function from
        third_party/blink/public/common/loader/network_utils.h to a new
        //services/network/public/cpp/url_util.h.  This move also requires
        moving features::kFtpProtocol.
    
        Bug: 973885

--- chromium_src/services/network/public/cpp/url_util.cc
+++ chromium_src/services/network/public/cpp/url_util.cc
@@ -1,18 +1,23 @@
-/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "third_party/blink/public/common/loader/network_utils.h"
+#include "services/network/public/cpp/url_util.h"
 
+#include "base/feature_list.h"
+#include "build/build_config.h"
+#include "net/net_buildflags.h"
+#include "services/network/public/cpp/features.h"
+#include "services/network/public/cpp/is_potentially_trustworthy.h"
 #include "url/gurl.h"
+#include "url/url_constants.h"
 
 #define IsURLHandledByNetworkService IsURLHandledByNetworkService_ChromiumImpl
-#include "../../../../../../third_party/blink/common/loader/network_utils.cc"
+#include "../../../../../../services/network/public/cpp/url_util.cc"
 #undef IsURLHandledByNetworkService
 
-namespace blink {
-namespace network_utils {
+namespace network {
 
 bool IsURLHandledByNetworkService(const GURL& url) {
   if (url.SchemeIs("ipns") || url.SchemeIs("ipfs")) {
@@ -21,5 +26,4 @@ bool IsURLHandledByNetworkService(const GURL& url) {
   return IsURLHandledByNetworkService_ChromiumImpl(url);
 }
 
-}  // namespace network_utils
-}  // namespace blink
+}  // namespace network
