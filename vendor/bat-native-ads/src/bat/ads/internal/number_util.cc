f3fc08a8861fadbdee16227a04a4801abbccd4d5
Terry Mancey <terry.mancey@icloud.com>
2021-05-12 15:45:59 -0500

    Fixes ad rewards not updated at brave://rewards due to loss of precision

--- vendor/bat-native-ads/src/bat/ads/internal/number_util.cc
+++ vendor/bat-native-ads/src/bat/ads/internal/number_util.cc
@@ -10,13 +10,41 @@
 namespace ads {
 
 namespace {
-const double kEpsilon = .0001;
+
+const double kEpsilon = .00001;
+const int kDecimalPlaces = 4;
+
+double RoundDoubleNPlaces(const double value, const int n_places) {
+  const double pow_10 = pow(10.0f, static_cast<double>(n_places));
+  return round(value * pow_10) / pow_10;
+}
+
 }  // namespace
 
-bool DoubleEquals(const double a, const double b) {
+bool DoubleEquals(const double lhs, const double rhs) {
   // Choice of epsilon for double comparison allows for proper comparison, we
   // want it to be relatively high to avoid false negative comparison results
-  return std::abs(a - b) < kEpsilon;
+  return std::abs(lhs - rhs) <= kEpsilon;
+}
+
+bool DoubleIsGreaterEqual(const double lhs, const double rhs) {
+  return RoundDoubleNPlaces(lhs, kDecimalPlaces) >=
+         RoundDoubleNPlaces(rhs, kDecimalPlaces);
+}
+
+bool DoubleIsGreater(const double lhs, const double rhs) {
+  return RoundDoubleNPlaces(lhs, kDecimalPlaces) >
+         RoundDoubleNPlaces(rhs, kDecimalPlaces);
+}
+
+bool DoubleIsLessEqual(const double lhs, const double rhs) {
+  return RoundDoubleNPlaces(lhs, kDecimalPlaces) <=
+         RoundDoubleNPlaces(rhs, kDecimalPlaces);
+}
+
+bool DoubleIsLess(const double lhs, const double rhs) {
+  return RoundDoubleNPlaces(lhs, kDecimalPlaces) <
+         RoundDoubleNPlaces(rhs, kDecimalPlaces);
 }
 
 }  // namespace ads
