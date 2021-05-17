d640c184f536fa093aed4089e5f829cab869532d
Brian Clifton <brian@clifton.me>
2021-05-17 09:50:27 -0700

    Re-enable QR code sharing for a URL
    Fixes https://github.com/brave/brave-browser/issues/15900
    
    Also includes cleanup; removes obsolete flags added with
    https://github.com/brave/brave-core/pull/5435 (this experiment was
    removed and enabled by default with Chromium 91)

--- app/brave_main_delegate_browsertest.cc
+++ app/brave_main_delegate_browsertest.cc
@@ -4,7 +4,6 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "chrome/browser/domain_reliability/service_factory.h"
-#include "chrome/browser/sharing/features.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/profile_picker.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -77,12 +76,10 @@ IN_PROC_BROWSER_TEST_F(BraveMainDelegateBrowserTest, DisabledFeatures) {
       &features::kSignedExchangePrefetchCacheForNavigations,
       &features::kSignedExchangeSubresourcePrefetch,
       &features::kSubresourceWebBundles,
-      &features::kTabHoverCards,
       &features::kWebOTP,
       &federated_learning::kFederatedLearningOfCohorts,
       &federated_learning::kFlocIdComputedEventLogging,
       &kEnableProfilePickerOnStartupFeature,
-      &kSharingQRCodeGenerator,
       &net::features::kFirstPartySets,
       &network_time::kNetworkTimeServiceQuerying,
   };
