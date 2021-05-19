785a62a5c7d646870062552e798e249f6bef2f2f
Mario Sanchez Prada <mario@igalia.com>
2021-05-19 20:45:14 +0200

    Include all the original headers for download_item_view.h override
    Starting with Chromium 92, not doing this will cause a problem because
    the redefined method SetMode() has a very common name, meaning that it
    will get redefined in more places than intended resulting in the test
    SavePackageFinishedObserverBrowserTest.Success failing with this:
    
      BrowserTestBase received signal: Segmentation fault. Backtrace:
      #0 0x7fd03595cb89 base::debug::CollectStackTrace()
      #1 0x7fd03585a813 base::debug::StackTrace::StackTrace()
      #2 0x560e4f228946 content::(anonymous namespace)::DumpStackTraceSignalHandler()
      #3 0x7fd025a42210 (/usr/lib/x86_64-linux-gnu/libc-2.31.so+0x4620f)
      [1/1] SavePackageFinishedObserverBrowserTest.Success (CRASHED)

--- chromium_src/chrome/browser/ui/views/download/download_item_view.h
+++ chromium_src/chrome/browser/ui/views/download/download_item_view.h
@@ -6,6 +6,33 @@
 #ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_DOWNLOAD_DOWNLOAD_ITEM_VIEW_H_
 #define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_DOWNLOAD_DOWNLOAD_ITEM_VIEW_H_
 
+// Note: SetMode method name is quite common. To re-define only SetMode in
+// download_item_view.h, all the original headers need to be added.
+#include "base/files/file_path.h"
+#include "base/memory/weak_ptr.h"
+#include "base/optional.h"
+#include "base/scoped_observation.h"
+#include "base/task/cancelable_task_tracker.h"
+#include "base/time/time.h"
+#include "base/timer/timer.h"
+#include "chrome/browser/download/download_commands.h"
+#include "chrome/browser/download/download_ui_model.h"
+#include "chrome/browser/icon_loader.h"
+#include "chrome/browser/ui/views/download/download_shelf_context_menu_view.h"
+#include "ui/base/metadata/metadata_header_macros.h"
+#include "ui/base/models/image_model.h"
+#include "ui/base/resource/resource_bundle.h"
+#include "ui/base/ui_base_types.h"
+#include "ui/gfx/animation/slide_animation.h"
+#include "ui/gfx/animation/throb_animation.h"
+#include "ui/gfx/geometry/size.h"
+#include "ui/gfx/image/image.h"
+#include "ui/gfx/image/image_skia.h"
+#include "ui/views/animation/animation_delegate_views.h"
+#include "ui/views/context_menu_controller.h"
+#include "ui/views/controls/button/button.h"
+#include "ui/views/view.h"
+
 #define BRAVE_DOWNLOAD_DOWNLOAD_ITEM_VIEW_H_ \
  private:                                    \
   friend class BraveDownloadItemView;        \
