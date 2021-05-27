f956b0f980513ad972da4f9c31b3c1cb82617d3d
Alex C <alexcapitaneanu@gmail.com>
2021-05-27 10:36:37 -0400

    [Android] fix for share stats crash and showing on NTP

--- android/java/org/chromium/base/BraveReflectionUtil.java
+++ android/java/org/chromium/base/BraveReflectionUtil.java
@@ -43,7 +43,9 @@ public class BraveReflectionUtil {
                 Log.e(TAG, "Illegal access for method: " + e);
                 assert (false);
             } catch (InvocationTargetException e) {
-                Log.e(TAG, "Method invocation error: " + e);
+                Log.e(TAG, "Method invocation error e: " + e);
+                Log.e(TAG, "Method invocation error e.getMessage: " + e.getMessage());
+                Log.e(TAG, "Method invocation error  e.getCause(): " + e.getCause());
                 assert (false);
             }
         } catch (NoSuchMethodException e) {
