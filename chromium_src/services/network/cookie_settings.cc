81aee54c9d96d5a56abf2ae3e1666b8d31c846d9
Aleksey Khoroshilov <akhoroshilov@brave.com>
2021-05-28 22:06:50 +0700

    Block third-party cookies when SESSION_ONLY mode is selected.

---
+++ chromium_src/services/network/cookie_settings.cc
@@ -0,0 +1,13 @@
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#define BRAVE_COOKIE_SETTINGS_GET_COOKIE_SETTINGS_INTERNAL \
+  if (cookie_setting == CONTENT_SETTING_SESSION_ONLY) {    \
+    /* Do nothing */                                       \
+  } else
+
+#include "../../../../services/network/cookie_settings.cc"
+
+#undef BRAVE_COOKIE_SETTINGS_GET_COOKIE_SETTINGS_INTERNAL
