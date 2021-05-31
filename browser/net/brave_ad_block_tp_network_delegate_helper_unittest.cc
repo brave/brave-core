6942f81db35fe68ca895432ddeb0f304636347d6
Ivan Efremov <iefremov@brave.com>
2021-05-31 19:25:10 +0700

    Minor cleanup.

--- browser/net/brave_ad_block_tp_network_delegate_helper_unittest.cc
+++ browser/net/brave_ad_block_tp_network_delegate_helper_unittest.cc
@@ -8,15 +8,13 @@
 #include <memory>
 #include <string>
 #include <utility>
-#include <vector>
 
-#include "base/task/thread_pool.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "brave/browser/brave_browser_process.h"
 #include "brave/browser/net/url_context.h"
 #include "brave/common/network_constants.h"
 #include "brave/components/brave_shields/browser/ad_block_service.h"
 #include "brave/test/base/testing_brave_browser_process.h"
-#include "components/prefs/pref_service.h"
 #include "content/public/test/browser_task_environment.h"
 #include "net/base/net_errors.h"
 #include "net/dns/mock_host_resolver.h"
@@ -26,13 +24,25 @@
 
 using brave::ResponseCallback;
 
+namespace {
+
+// Note: extract a common impl if needed for other tests, do not copy.
+
+// TODO(iefremov): This is only needed to provide a task runner to the adblock
+// service. We can drop this stub once the service doesn't need an
+// "external" runner.
 class TestingBraveComponentUpdaterDelegate : public BraveComponent::Delegate {
  public:
-  TestingBraveComponentUpdaterDelegate()
-      : task_runner_(base::ThreadPool::CreateSequencedTaskRunner({})) {}
-  ~TestingBraveComponentUpdaterDelegate() override {}
+  TestingBraveComponentUpdaterDelegate() = default;
+  ~TestingBraveComponentUpdaterDelegate() override = default;
+
+  TestingBraveComponentUpdaterDelegate(TestingBraveComponentUpdaterDelegate&) =
+      delete;
+  TestingBraveComponentUpdaterDelegate& operator=(
+      TestingBraveComponentUpdaterDelegate&) = delete;
 
   using ComponentObserver = update_client::UpdateClient::Observer;
+
   // brave_component_updater::BraveComponent::Delegate implementation
   void Register(const std::string& component_name,
                 const std::string& component_base64_public_key,
@@ -45,19 +55,17 @@ class TestingBraveComponentUpdaterDelegate : public BraveComponent::Delegate {
   void RemoveObserver(ComponentObserver* observer) override {}
 
   scoped_refptr<base::SequencedTaskRunner> GetTaskRunner() override {
-    return task_runner_;
+    return base::ThreadTaskRunnerHandle::Get();
   }
 
   const std::string locale() const override { return "en"; }
-  PrefService* local_state() override { return pref_service_.get(); }
-
- private:
-  scoped_refptr<base::SequencedTaskRunner> task_runner_;
-
-  std::unique_ptr<PrefService> pref_service_;
-
-  DISALLOW_COPY_AND_ASSIGN(TestingBraveComponentUpdaterDelegate);
+  PrefService* local_state() override {
+    NOTREACHED();
+    return nullptr;
+  }
 };
+
+}  // namespace
 
 class BraveAdBlockTPNetworkDelegateHelperTest : public testing::Test {
  protected:
@@ -91,21 +99,15 @@ class BraveAdBlockTPNetworkDelegateHelperTest : public testing::Test {
     service->ResetForTest(rules, resources);
   }
 
-  // Serves as `next_callback` for the request handler
-  void OnCompletion() {}
-
   // Returns true if the request handler deferred control back to the calling
   // thread before completion, or true if it completed instantly.
   bool CheckRequest(std::shared_ptr<brave::BraveRequestInfo> request_info) {
-    // request_identifier must be nonzero, or else nothing will be tested
+    // `request_identifier` must be nonzero, or else nothing will be tested.
     request_info->request_identifier = 1;
 
-    auto cb = base::Bind(&BraveAdBlockTPNetworkDelegateHelperTest::OnCompletion,
-                         base::Unretained(this));
-
-    int rc = OnBeforeURLRequest_AdBlockTPPreWork(cb, request_info);
+    int rc =
+        OnBeforeURLRequest_AdBlockTPPreWork(base::DoNothing(), request_info);
     EXPECT_TRUE(rc == net::OK || rc == net::ERR_IO_PENDING);
-
     task_environment_.RunUntilIdle();
 
     return rc == net::ERR_IO_PENDING;
@@ -127,9 +129,7 @@ TEST_F(BraveAdBlockTPNetworkDelegateHelperTest, NoInitiatorURL) {
   auto request_info = std::make_shared<brave::BraveRequestInfo>(url);
   request_info->resource_type = blink::mojom::ResourceType::kScript;
 
-  bool handled = CheckRequest(request_info);
-
-  EXPECT_FALSE(handled);
+  EXPECT_FALSE(CheckRequest(request_info));
   EXPECT_EQ(request_info->blocked_by, brave::kNotBlocked);
   EXPECT_TRUE(request_info->new_url_spec.empty());
 }
@@ -139,9 +139,7 @@ TEST_F(BraveAdBlockTPNetworkDelegateHelperTest, EmptyRequestURL) {
   request_info->initiator_url = GURL("https://example.com");
   request_info->resource_type = blink::mojom::ResourceType::kScript;
 
-  bool handled = CheckRequest(request_info);
-
-  EXPECT_FALSE(handled);
+  EXPECT_FALSE(CheckRequest(request_info));
   EXPECT_EQ(request_info->blocked_by, brave::kNotBlocked);
   EXPECT_TRUE(request_info->new_url_spec.empty());
 }
@@ -153,9 +151,7 @@ TEST_F(BraveAdBlockTPNetworkDelegateHelperTest, DevToolURL) {
       GURL("devtools://devtools/bundled/root/root.js");
   request_info->resource_type = blink::mojom::ResourceType::kScript;
 
-  bool handled = CheckRequest(request_info);
-
-  EXPECT_FALSE(handled);
+  EXPECT_FALSE(CheckRequest(request_info));
   EXPECT_EQ(request_info->blocked_by, brave::kNotBlocked);
   EXPECT_TRUE(request_info->new_url_spec.empty());
 }
@@ -170,9 +166,7 @@ TEST_F(BraveAdBlockTPNetworkDelegateHelperTest, SimpleBlocking) {
   request_info->resource_type = blink::mojom::ResourceType::kScript;
   request_info->initiator_url = GURL("https://brave.com");
 
-  bool handled = CheckRequest(request_info);
-
-  EXPECT_TRUE(handled);
+  EXPECT_TRUE(CheckRequest(request_info));
   EXPECT_EQ(request_info->blocked_by, brave::kAdBlocked);
   EXPECT_TRUE(request_info->new_url_spec.empty());
   // It's unclear whether or not this is a Tor request, so no DNS queries are
