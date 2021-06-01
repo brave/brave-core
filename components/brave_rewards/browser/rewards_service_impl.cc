adrielsand/adrbrowsiel-core

Fix #15237 - Improve disconnected brave rewards wallet state

components/brave_rewards/browser/rewards_service_impl.cc
@@ -114,6 +114,8 @@ std::string URLMethodToRequestType(ledger::type::UrlMethod method) {
       return "PUT";
     case ledger::type::UrlMethod::PATCH:
       return "PATCH";
+    case ledger::type::UrlMethod::DELETE:
+      return "DELETE";
     default:
       NOTREACHED();
       return "GET";

brave/brave-core

components/brave_rewards/browser/rewards_service_impl.cc
@@ -114,6 +114,8 @@ std::string URLMethodToRequestType(ledger::type::UrlMethod method) {
       return "PUT";
     case ledger::type::UrlMethod::PATCH:
       return "PATCH";
+    case ledger::type::UrlMethod::DEL:
+      return "DELETE";
     default:
       NOTREACHED();
       return "GET";