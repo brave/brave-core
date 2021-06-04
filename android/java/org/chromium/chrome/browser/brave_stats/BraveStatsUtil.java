825840f7c9e29c7962152b09e62d8aafcf52eec8
Alex C <alexcapitaneanu@gmail.com>
2021-06-04 14:15:20 -0400

    [Android] deletes share stats image after sharing

--- android/java/org/chromium/chrome/browser/brave_stats/BraveStatsUtil.java
+++ android/java/org/chromium/chrome/browser/brave_stats/BraveStatsUtil.java
@@ -50,10 +50,14 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import android.content.ContentResolver;
+
 public class BraveStatsUtil {
     public static final short MILLISECONDS_PER_ITEM = 50;
     public static final int SHARE_STATS_WRITE_EXTERNAL_STORAGE_PERM = 3867;
+    public static final int SHARE_STATS_REQUEST_CODE = 4367;
     public static final String TAG = "BraveStatsUtil";
+    private static String shareStatsFile = "";
     /*
      * Gets string view of specific time in seconds for Brave stats
      */
@@ -168,20 +172,21 @@ public class BraveStatsUtil {
         shareStatsAction(view);
     }
 
+
     public static void shareStatsAction(View view) {
         try {
             Context context = ContextUtils.getApplicationContext();
             Bitmap bmp = convertToBitmap(view);
-            String path = "";
+            
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                path = MediaStore.Images.Media.insertImage(
+                shareStatsFile = MediaStore.Images.Media.insertImage(
                         context.getContentResolver(), bmp, "tempimage", null);
             } else {
                 storeImage(bmp);
-                path = getOutputMediaFile().getAbsolutePath();
+                shareStatsFile = getOutputMediaFile().getAbsolutePath();
             }
 
-            Uri uri = Uri.parse(path);
+            Uri uri = Uri.parse(shareStatsFile);
 
             Intent sendIntent = new Intent();
             sendIntent.setAction(Intent.ACTION_SEND);
@@ -193,9 +198,25 @@ public class BraveStatsUtil {
 
             Intent shareIntent = Intent.createChooser(sendIntent, " ");
             shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(shareIntent);
+
+            if (BraveActivity.getBraveActivity() != null){
+                BraveActivity.getBraveActivity().startActivityForResult(shareIntent, SHARE_STATS_REQUEST_CODE);
+            }
         } catch (Exception e) {
-            Log.e(TAG, "exception :" + e);
+            e.printStackTrace();
+        }
+    }
+
+    public static void removeShareStatsFile() {
+        Context context = ContextUtils.getApplicationContext();
+        if (shareStatsFile.startsWith("content://")) {
+            ContentResolver contentResolver = context.getContentResolver();
+            contentResolver.delete(Uri.parse(shareStatsFile), null, null);
+        } else {
+            File file = new File(shareStatsFile);
+            if (file.exists()) {
+                file.delete();
+            }
         }
     }
 
