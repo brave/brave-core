67b22edabb8e44ca59a74d0967177a347e335dec
Mario Sanchez Prada <mario@igalia.com>
2021-06-07 11:53:38 +0200

    Remove references to kEnableProfilePickerOnStartupFeature
    The new profile menu/picker design is now the only one available, so
    there's no longer any need to deal with this parameter.
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/62060fb114a2f54030c2c5200982eed77fa52d2c
    
    commit 62060fb114a2f54030c2c5200982eed77fa52d2c
    Author: David Roger <droger@chromium.org>
    Date:   Tue May 25 23:22:41 2021 +0000
    
        [profiles] Delete flag for new profile picker
    
        The new profile picker is now enabled by default, remove the flag to
        disable it.
    
        Bug: 1063856

--- app/brave_main_delegate.cc
+++ app/brave_main_delegate.cc
@@ -234,8 +234,6 @@ bool BraveMainDelegate::BasicStartupComplete(int* exit_code) {
     offline_pages::kPrefetchingOfflinePagesFeature.name,
     signin::kMobileIdentityConsistency.name,
     translate::kTranslate.name,
-#else
-    kEnableProfilePickerOnStartupFeature.name,
 #endif
   };
 
