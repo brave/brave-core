63b23efb43741287dcebbfa5e3613132460299ff
Simon Hong <shong@brave.com>
2021-06-09 13:22:08 +0900

    Use different icon for some file types
    fix https://github.com/brave/brave-browser/issues/12761
    
    When brave browser is set as a default application,
    different icon will be used for that file type.
    Currently, .pdf and .svg files will have different icon.
    
    So far, we set BraveBHTML (beta) prog id for supported file types in
    registry.
    and that BraveBHTML entry has some attributes such as default icon or
    commands. That default icon is used for the file type when browser
    is set as its default application. default icon of BraveBHTML reg entry
    is brave browser's default icon.
    Because of this, all file types uses same icon.
    
    This PR introduces new BraveFile prog entry and it uses different icon.
    And, new BraveFile id is set for some file types when brave is its
    default application.

--- chromium_src/chrome/installer/util/shell_util.cc
+++ chromium_src/chrome/installer/util/shell_util.cc
@@ -3,6 +3,36 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "base/notreached.h"
+#include "brave/installer/util/brave_shell_util.h"
+#include "chrome/common/chrome_icon_resources_win.h"
+#include "chrome/install_static/install_util.h"
+#include "components/version_info/channel.h"
+
+namespace {
+
+#if defined(OFFICIAL_BUILD)
+
+int GetIconIndexForFileType() {
+  switch (install_static::GetChromeChannel()) {
+    case version_info::Channel::STABLE:
+      return icon_resources::kFileTypeIndex;
+    case version_info::Channel::BETA:
+      return icon_resources::kBetaFileTypeIndex;
+    case version_info::Channel::DEV:
+      return icon_resources::kDevFileTypeIndex;
+    case version_info::Channel::CANARY:
+      return icon_resources::kSxSFileTypeIndex;
+    default:
+      NOTREACHED();
+      return icon_resources::kFileTypeIndex;
+  }
+}
+
+#endif  // OFFICIAL_BUILD
+
+}  // namespace
+
 #define BRAVE_IPFS L"ipfs"
 #define BRAVE_IPNS L"ipns"
 
@@ -12,7 +42,43 @@
   if (base::EqualsCaseInsensitiveASCII(protocol, BRAVE_IPNS))             \
     return base::StringPrintf(kSystemSettingsDefaultAppsFormat, L"IPNS");
 
+#if defined(OFFICIAL_BUILD)
+// Add BraveFile prog id in registry with proper icon.
+// This prog id will be referenced from serveral file association reg entry.
+#define BRAVE_GET_CHROME_PROG_ID_ENTRIES                     \
+  app_info.prog_id = installer::GetProgIdForFileType();      \
+  app_info.file_type_icon_index = GetIconIndexForFileType(); \
+  GetProgIdEntries(app_info, entries);
+
+// Give BraveXXFile prog id for some file type.(ex, .pdf or .svg) instead of
+// BraveHTML.
+#define BRAVE_GET_APP_EXT_REGISTRATION_ENTRIES                         \
+  if (installer::ShouldUseFileTypeProgId(ext)) {                       \
+    entries->push_back(std::make_unique<RegistryEntry>(                \
+        key_name, installer::GetProgIdForFileType(), std::wstring())); \
+    return;                                                            \
+  }
+
+// Give BraveXXFile prog id for some file type.(ex, .pdf or .svg) instead of
+// BraveHTML.
+#define BRAVE_GET_SHELL_INTEGRATION_ENTRIES                               \
+  const std::wstring file_ext = ShellUtil::kPotentialFileAssociations[i]; \
+  if (installer::ShouldUseFileTypeProgId(file_ext)) {                     \
+    entries->push_back(std::make_unique<RegistryEntry>(                   \
+        capabilities + L"\\FileAssociations", file_ext,                   \
+        installer::GetProgIdForFileType()));                              \
+    continue;                                                             \
+  }
+#else
+#define BRAVE_GET_CHROME_PROG_ID_ENTRIES
+#define BRAVE_GET_APP_EXT_REGISTRATION_ENTRIES
+#define BRAVE_GET_SHELL_INTEGRATION_ENTRIES
+#endif
+
 #include "../../../../../chrome/installer/util/shell_util.cc"
+#undef BRAVE_GET_SHELL_INTEGRATION_ENTRIES
+#undef BRAVE_GET_APP_EXT_REGISTRATION_ENTRIES
+#undef BRAVE_GET_CHROME_PROG_ID_ENTRIES
 #undef BRAVE_GET_TARGET_FOR_DEFAULT_APP_SETTINGS
 #undef BRAVE_IPFS
 #undef BRAVE_IPNS
