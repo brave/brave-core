64579a42f47fa185d9b3abb658bfcf35cd79c380
mkarolin <max@brave.com>
2021-06-09 20:50:20 -0400

    Update headers of scripts transitioned to python3.

--- script/sign_binaries.py
+++ script/sign_binaries.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2021 The Brave Authors. All rights reserved.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import optparse
 import os
 import subprocess
