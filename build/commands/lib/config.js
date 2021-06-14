9be4979a752058cb9dad03f3eb75c9de6e4a2c3a
Mihai PLESA <mihai.plesa@gmail.com>
2021-06-14 11:28:04 -0500

    added missing line

--- build/commands/lib/config.js
+++ build/commands/lib/config.js
@@ -401,6 +401,8 @@ Config.prototype.buildArgs = function () {
     args.ios_enable_credential_provider_extension = false
     args.ios_enable_widget_kit_extension = false
 
+    args.use_system_xcode = false
+
     delete args.safebrowsing_api_endpoint
     delete args.updater_prod_endpoint
     delete args.updater_dev_endpoint
