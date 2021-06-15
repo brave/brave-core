7ec54e02d7cd51160d822e4c9bb62ccb0ca1ccfa
Sergey P <spylogsster@gmail.com>
2021-06-15 14:27:21 +0300

    Fix subscription for ipfs component updates

--- browser/ipfs/test/brave_ipfs_client_updater_browsertest.cc
+++ browser/ipfs/test/brave_ipfs_client_updater_browsertest.cc
@@ -101,6 +101,10 @@ class BraveIpfsClientUpdaterTest : public ExtensionBrowserTest {
     base::RunLoop loop;
     loop.RunUntilIdle();
   }
+
+  void SetIpfsExecutablePath(const base::FilePath& path) {
+    g_brave_browser_process->ipfs_client_updater()->SetExecutablePath(path);
+  }
 };
 
 // Load the Ipfs client updater extension and verify that it correctly
@@ -130,5 +134,10 @@ IN_PROC_BROWSER_TEST_F(BraveIpfsClientUpdaterTest, IpfsExecutableReady) {
   ASSERT_TRUE(PathExists(executable_path));
 
   EXPECT_EQ(ipfs_service->GetIpfsExecutablePath(), executable_path);
+  ASSERT_TRUE(ipfs_service->IsIPFSExecutableAvailable());
+
+  base::FilePath new_path(FILE_PATH_LITERAL("newpath"));
+  SetIpfsExecutablePath(new_path);
+  EXPECT_EQ(ipfs_service->GetIpfsExecutablePath(), new_path);
   ASSERT_TRUE(ipfs_service->IsIPFSExecutableAvailable());
 }
