cefa618bad2ef2fd4f60ffcee82c803abde638e9
Mario Sanchez Prada <mario@igalia.com>
2021-06-18 18:39:44 +0200

    Remove Brave-specific code related to the Media Router extension
    This extention is no longer used upstream as Media Router capabilities
    are now unconditionally enabled, meaning that there's no need to select
    any setting to have access to enable ChromeCast in the browser.
    
    Thus, drop the code from https://github.com/brave/brave-core/pull/2836,
    plus every other reference to this extension in Brave-specific code.
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/e287bab0c15dc688d9604f9e43993ccd3b217adf
    
    commit e287bab0c15dc688d9604f9e43993ccd3b217adf
    Author: Takumi Fujimoto <takumif@chromium.org>
    Date:   Thu Jun 10 06:19:57 2021 +0000
    
        Stop loading the Media Router component extension
    
        We no longer use the component extension so it can now be removed.
        Although this CL does not explicitly uninstall the extension, if the
        extension is installed, its files get deleted on startup via
        ExtensionService::CheckExternalUninstall().
    
        Bug: 1179785, b/184392261
    
    fixup! [WIP] Remove Brave-specific code related to the Media Router extension

--- chromium_src/chrome/browser/about_flags.cc
+++ chromium_src/chrome/browser/about_flags.cc
@@ -3,38 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include <string>
 
 #include "brave/browser/about_flags.cc"
-#include "brave/common/pref_names.h"
-#include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/profiles/profile_manager.h"
-#include "components/prefs/pref_service.h"
 
-#define SetFeatureEntryEnabled SetFeatureEntryEnabled_ChromiumImpl
-#include "../../../../chrome/browser/about_flags.cc"  // NOLINT
-#undef SetFeatureEntryEnabled
-
-namespace about_flags {
-
-void UpdateBraveMediaRouterPref(const std::string& internal_name,
-                                Profile* profile) {
-  bool enable = false;
-  if (base::EndsWith(internal_name, "@1", base::CompareCase::SENSITIVE)) {
-    enable = true;
-  }
-  if (base::StartsWith(internal_name, "load-media-router-component-extension",
-                       base::CompareCase::SENSITIVE)) {
-    profile->GetPrefs()->SetBoolean(kBraveEnabledMediaRouter, enable);
-  }
-}
-
-void SetFeatureEntryEnabled(flags_ui::FlagsStorage* flags_storage,
-                            const std::string& internal_name,
-                            bool enable) {
-  UpdateBraveMediaRouterPref(internal_name,
-                             ProfileManager::GetActiveUserProfile());
-  SetFeatureEntryEnabled_ChromiumImpl(flags_storage, internal_name, enable);
-}
-
-}  // namespace about_flags
+#include "../../../../chrome/browser/about_flags.cc"
