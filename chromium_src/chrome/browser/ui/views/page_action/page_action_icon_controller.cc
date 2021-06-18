fb490ef975274f832cc4d3e26d31e6d04dd27c25
Kevin Kuehler <kevin@segfault.fun>
2021-06-18 15:55:36 -0700

    Remove unnecessary patch. Thanks Simon!

--- chromium_src/chrome/browser/ui/views/page_action/page_action_icon_controller.cc
+++ chromium_src/chrome/browser/ui/views/page_action/page_action_icon_controller.cc
@@ -8,6 +8,7 @@
 #include "brave/browser/ui/views/speedreader/speedreader_icon_view.h"
 #include "brave/browser/ui/views/translate/brave_translate_icon_view.h"
 #include "chrome/browser/ui/page_action/page_action_icon_type.h"
+#include "chrome/browser/ui/views/reader_mode/reader_mode_icon_view.h"
 
 namespace {
 constexpr PageActionIconType kUndefinedPageActionIconType =
