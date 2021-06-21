921e9c92dd771daadac8f0879a74807c7459969f
Mario Sanchez Prada <mario@igalia.com>
2021-06-21 13:35:32 +0200

    Use RenderFrame::GetMainRenderFrame() instead of RenderView's
    The method has been removed from RenderView in favour of the
    newly added one in RenderFrame.
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/a3f2d52bc842c320df2d8d648f7510225e43992a
    
    commit a3f2d52bc842c320df2d8d648f7510225e43992a
    Author: Dave Tapuska <dtapuska@chromium.org>
    Date:   Fri Jun 11 15:30:18 2021 +0000
    
        Remove GetMainRenderFrame from RenderView.
    
        The MainFrame can be accessed off of WebView's API so there is no
        need to store it in RenderViewImpl. Provide a convenience method on
        RenderFrame to get the MainRenderFrame from the WebView.
    
        BUG=1155202

--- components/content_settings/renderer/brave_content_settings_agent_impl_autoplay_browsertest.cc
+++ components/content_settings/renderer/brave_content_settings_agent_impl_autoplay_browsertest.cc
@@ -105,9 +105,8 @@ class BraveContentSettingsAgentImplAutoplayBrowserTest
 
     // Unbind the ContentSettingsAgent interface that would be registered by
     // the ContentSettingsAgentImpl created when the render frame is created.
-    view_->GetMainRenderFrame()
-        ->GetAssociatedInterfaceRegistry()
-        ->RemoveInterface(mojom::ContentSettingsAgent::Name_);
+    GetMainRenderFrame()->GetAssociatedInterfaceRegistry()->RemoveInterface(
+        mojom::ContentSettingsAgent::Name_);
   }
 };
 
@@ -125,7 +124,7 @@ TEST_F(BraveContentSettingsAgentImplAutoplayBrowserTest,
           content_settings::ContentSettingToValue(CONTENT_SETTING_BLOCK)),
       std::string(), false));
 
-  MockContentSettingsAgentImpl agent(view_->GetMainRenderFrame());
+  MockContentSettingsAgentImpl agent(GetMainRenderFrame());
   agent.SetContentSettingRules(&content_setting_rules);
   EXPECT_FALSE(agent.AllowAutoplay(true));
   base::RunLoop().RunUntilIdle();
@@ -158,7 +157,7 @@ TEST_F(BraveContentSettingsAgentImplAutoplayBrowserTest,
           content_settings::ContentSettingToValue(CONTENT_SETTING_ALLOW)),
       std::string(), false));
 
-  MockContentSettingsAgentImpl agent(view_->GetMainRenderFrame());
+  MockContentSettingsAgentImpl agent(GetMainRenderFrame());
   agent.SetContentSettingRules(&content_setting_rules);
   EXPECT_TRUE(agent.AllowAutoplay(true));
 
