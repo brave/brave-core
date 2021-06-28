fe3f2ca6b2eab0cb3b778708770082cf5c8d6f3c
Szilard Szaloki <szilardszaloki@gmail.com>
2021-06-28 17:41:38 +0200

    Adding notification_keys.h for common notifications.

--- vendor/bat-native-ledger/src/bat/ledger/internal/bitflyer/bitflyer.cc
+++ vendor/bat-native-ledger/src/bat/ledger/internal/bitflyer/bitflyer.cc
@@ -18,6 +18,7 @@
 #include "bat/ledger/internal/endpoint/bitflyer/bitflyer_server.h"
 #include "bat/ledger/internal/ledger_impl.h"
 #include "bat/ledger/internal/logging/event_log_keys.h"
+#include "bat/ledger/internal/notifications/notification_keys.h"
 #include "bat/ledger/internal/state/state_keys.h"
 #include "bat/ledger/internal/wallet/wallet_util.h"
 #include "brave_base/random.h"
@@ -179,8 +180,8 @@ void Bitflyer::DisconnectWallet(const bool manual) {
   const bool shutting_down = ledger_->IsShuttingDown();
 
   if (!manual && !shutting_down) {
-    ledger_->ledger_client()->ShowNotification("wallet_disconnected", {},
-                                               [](type::Result _) {});
+    ledger_->ledger_client()->ShowNotification(
+        ledger::notifications::kWalletDisconnected, {}, [](type::Result _) {});
   }
 
   SetWallet(wallet->Clone());
--- vendor/bat-native-ledger/src/bat/ledger/internal/uphold/uphold_transfer.cc
+++ vendor/bat-native-ledger/src/bat/ledger/internal/uphold/uphold_transfer.cc
@@ -9,6 +9,7 @@
 #include "bat/ledger/global_constants.h"
 #include "bat/ledger/internal/endpoint/uphold/uphold_server.h"
 #include "bat/ledger/internal/ledger_impl.h"
+#include "bat/ledger/internal/notifications/notification_keys.h"
 #include "bat/ledger/internal/uphold/uphold_transfer.h"
 #include "bat/ledger/internal/uphold/uphold_util.h"
 #include "bat/ledger/internal/wallet/wallet_util.h"
@@ -21,16 +22,14 @@ using std::placeholders::_3;
 namespace ledger {
 namespace uphold {
 
-UpholdTransfer::UpholdTransfer(LedgerImpl* ledger) :
-    ledger_(ledger),
-    uphold_server_(std::make_unique<endpoint::UpholdServer>(ledger)) {
-}
+UpholdTransfer::UpholdTransfer(LedgerImpl* ledger)
+    : ledger_(ledger),
+      uphold_server_(std::make_unique<endpoint::UpholdServer>(ledger)) {}
 
 UpholdTransfer::~UpholdTransfer() = default;
 
-void UpholdTransfer::Start(
-    const Transaction& transaction,
-    client::TransactionCallback callback) {
+void UpholdTransfer::Start(const Transaction& transaction,
+                           client::TransactionCallback callback) {
   auto wallet = ledger_->uphold()->GetWallet();
   if (!wallet) {
     BLOG(0, "Wallet is null");
@@ -38,25 +37,19 @@ void UpholdTransfer::Start(
     return;
   }
 
-  auto url_callback = std::bind(&UpholdTransfer::OnCreateTransaction,
-      this,
-      _1,
-      _2,
-      callback);
-  uphold_server_->post_transaction()->Request(
-      wallet->token,
-      wallet->address,
-      transaction,
-      url_callback);
+  auto url_callback =
+      std::bind(&UpholdTransfer::OnCreateTransaction, this, _1, _2, callback);
+  uphold_server_->post_transaction()->Request(wallet->token, wallet->address,
+                                              transaction, url_callback);
 }
 
-void UpholdTransfer::OnCreateTransaction(
-    const type::Result result,
-    const std::string& id,
-    client::TransactionCallback callback) {
+void UpholdTransfer::OnCreateTransaction(const type::Result result,
+                                         const std::string& id,
+                                         client::TransactionCallback callback) {
   if (result == type::Result::EXPIRED_TOKEN) {
     callback(type::Result::EXPIRED_TOKEN, "");
-    ledger_->uphold()->DisconnectWallet(notifications::kWalletDisconnected);
+    ledger_->uphold()->DisconnectWallet(
+        ledger::notifications::kWalletDisconnected);
     return;
   }
 
@@ -69,9 +62,8 @@ void UpholdTransfer::OnCreateTransaction(
   CommitTransaction(id, callback);
 }
 
-void UpholdTransfer::CommitTransaction(
-    const std::string& transaction_id,
-    client::TransactionCallback callback) {
+void UpholdTransfer::CommitTransaction(const std::string& transaction_id,
+                                       client::TransactionCallback callback) {
   auto wallet = ledger_->uphold()->GetWallet();
   if (!wallet) {
     BLOG(0, "Wallet is null");
@@ -85,25 +77,19 @@ void UpholdTransfer::CommitTransaction(
     return;
   }
 
-  auto url_callback = std::bind(&UpholdTransfer::OnCommitTransaction,
-      this,
-      _1,
-      transaction_id,
-      callback);
+  auto url_callback = std::bind(&UpholdTransfer::OnCommitTransaction, this, _1,
+                                transaction_id, callback);
   uphold_server_->post_transaction_commit()->Request(
-      wallet->token,
-      wallet->address,
-      transaction_id,
-      url_callback);
+      wallet->token, wallet->address, transaction_id, url_callback);
 }
 
-void UpholdTransfer::OnCommitTransaction(
-    const type::Result result,
-    const std::string& transaction_id,
-    client::TransactionCallback callback) {
+void UpholdTransfer::OnCommitTransaction(const type::Result result,
+                                         const std::string& transaction_id,
+                                         client::TransactionCallback callback) {
   if (result == type::Result::EXPIRED_TOKEN) {
     callback(type::Result::EXPIRED_TOKEN, "");
-    ledger_->uphold()->DisconnectWallet(notifications::kWalletDisconnected);
+    ledger_->uphold()->DisconnectWallet(
+        ledger::notifications::kWalletDisconnected);
     return;
   }
 
