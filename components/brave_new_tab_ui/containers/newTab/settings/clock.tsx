fc1023a851d80bba04f927e96e3ee0f411cba04e
Taher <taher.dasten@gmail.com>
2021-06-30 10:12:18 -0700

    fix contrast on clock settings dropdown for NTP

--- components/brave_new_tab_ui/containers/newTab/settings/clock.tsx
+++ components/brave_new_tab_ui/containers/newTab/settings/clock.tsx
@@ -26,47 +26,61 @@ interface Props {
   clockFormat: string
 }
 
-class ClockSettings extends React.PureComponent<Props, {}> {
-  onClockFormatChanged = (selectedValue: any) => {
-    this.props.actions.clockWidgetUpdated(
-      this.props.showClock,
-      selectedValue)
+function useDarkPreference (): boolean {
+  const mql = window.matchMedia('(prefers-color-scheme: dark)')
+  const [isDark, setDark] = React.useState(mql.matches)
+
+  React.useEffect(() => {
+    const handleChange = () => setDark(mql.matches)
+    mql.addEventListener('change', handleChange)
+    return () => mql.removeEventListener('change', handleChange)
+  }, [])
+
+  return isDark
+}
+
+function ClockSettings ({
+  actions,
+  toggleShowClock,
+  showClock,
+  clockFormat
+}: Props) {
+  const isDarkMode = useDarkPreference()
+  const onClockFormatChanged = (selectedValue: any) => {
+    actions.clockWidgetUpdated(showClock, selectedValue)
   }
 
-  render () {
-    const { toggleShowClock, showClock, clockFormat } = this.props
-
-    let localeInfo = ''
-    const dateFormat = new Intl.DateTimeFormat()
-    const dateFormatOptions = dateFormat && dateFormat.resolvedOptions()
-    if (dateFormatOptions && dateFormatOptions.locale) {
-      localeInfo = ` (${dateFormatOptions.locale})`
-    }
-
-    return (
-      <div>
-        <SettingsRow>
-          <SettingsText>{getLocale('showClock')}</SettingsText>
-          <Toggle
-            onChange={toggleShowClock}
-            checked={showClock}
-            size='large'
-          />
-        </SettingsRow>
-        <SettingsRow>
-          <SettingsText>{getLocale('clockFormat')}</SettingsText>
-          <Select
-            value={clockFormat}
-            onChange={this.onClockFormatChanged}
-          >
-            <div key='clock-default' data-value=''>{getLocale('clockFormatDefault')}{localeInfo}</div>
-            <div key='clock-12' data-value='12'>{getLocale('clockFormat12')}</div>
-            <div key='clock-24' data-value='24'>{getLocale('clockFormat24')}</div>
-          </Select>
-        </SettingsRow>
-      </div>
-    )
+  let localeInfo = ''
+  const dateFormat = new Intl.DateTimeFormat()
+  const dateFormatOptions = dateFormat && dateFormat.resolvedOptions()
+  if (dateFormatOptions && dateFormatOptions.locale) {
+    localeInfo = ` (${dateFormatOptions.locale})`
   }
+
+  return (
+    <div>
+      <SettingsRow>
+        <SettingsText>{getLocale('showClock')}</SettingsText>
+        <Toggle
+          onChange={toggleShowClock}
+          checked={showClock}
+          size='large'
+        />
+      </SettingsRow>
+      <SettingsRow>
+        <SettingsText>{getLocale('clockFormat')}</SettingsText>
+        <Select
+          value={clockFormat}
+          onChange={onClockFormatChanged}
+          type={isDarkMode ? 'dark' : 'light'}
+        >
+          <div key='clock-default' data-value=''>{getLocale('clockFormatDefault')}{localeInfo}</div>
+          <div key='clock-12' data-value='12'>{getLocale('clockFormat12')}</div>
+          <div key='clock-24' data-value='24'>{getLocale('clockFormat24')}</div>
+        </Select>
+      </SettingsRow>
+    </div>
+  )
 }
 
 export default ClockSettings
