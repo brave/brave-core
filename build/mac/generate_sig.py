dfab9945d09cd5b3319bb588059ee9f6bb82ef1f
Max <41635752+mkarolin@users.noreply.github.com>
2021-07-01 01:50:03 -0400

    Merge pull request #9290 from brave/maxk-cr92-followup-fix-mac-signing
    [cr92 followup] Fixes MacOS signing.

--- build/mac/generate_sig.py
+++ build/mac/generate_sig.py
@@ -14,23 +14,31 @@ def Main():
     parser = argparse.ArgumentParser(usage='%(prog)s [options]')
     parser.add_argument('--sign-update', dest='sign_update_path', action='store',
         help='The path of sign_update binary', required=True)
-    parser.add_argument('--sign-key-file', dest='sign_key_file', action='store',
-        help='The private key to sign patch file', required=True)
+    group_key = parser.add_mutually_exclusive_group(required=True)
+    group_key.add_argument('--sign-key-file', dest='sign_key_file', action='store',
+        help='The private key file to sign dmg file', required=True)
+    group_key.add_argument('--sign-key', dest='sign_key', action='store',
+        help='The private key to sign dmg or patch file', required=True)
     parser.add_argument('--target', dest='target', action='store',
         help='Target file path for signing.', required=True)
     parser.add_argument('--output', dest='output', action='store',
         help='The path of the output.', required=True)
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("--dsa", action="store_true", help='Use DSA')
-    group.add_argument("--eddsa", action="store_true", help='Use EdDSA')
+    group_algorithm = parser.add_mutually_exclusive_group(required=True)
+    group_algorithm.add_argument("--dsa", action="store_true", help='Use DSA')
+    group_algorithm.add_argument("--eddsa", action="store_true", help='Use EdDSA')
     args = parser.parse_args()
 
     # sign file with the specified algorithm
     with open(args.output, 'w') as file:
         if args.dsa:
+            if not args.sign_key_file:
+                print('--sign-key-file argument is required with --dsa.', file=sys.stderr)
+                return 1
             command = [args.sign_update_path, args.target, args.sign_key_file]
-        else:
-            # EdDSA
+        if args.eddsa:
+            if not args.sign_key:
+                print('--sign-key argument is required with --eddsa.', file=sys.stderr)
+                return 1
             command = [args.sign_update_path, '-s', args.sign_key, args.target]
         try:
             subprocess.check_call(command, stdout=file)
