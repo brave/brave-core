69e28dfc3623d3573fe755e211e1f26468c9e08a
samartnik <artem@brave.com>
2021-07-09 16:53:52 -0400

    [Android] Changes for MenuButton implementation
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/4a6f627495111794c56c46873349521bcc9b5b22
    
    Toolbar: Remove deps on chrome/browser/omaha
    
    This CL applies some refactoring around toolbar/omaha in preparation
    for toolbar modularization:
    
    - Moves menu button/ui/item state definitions from
       c.b.omaha.UpdateMenuItemHelper to toolbar/menu_button
    - Inject MenuButtonState supplier to MenuButton MVC instead of
       the components calling UpdateMenuItemHelper directly
    
    Bug: 1127732

--- android/java/org/chromium/chrome/browser/toolbar/menu_button/BraveMenuButtonCoordinator.java
+++ android/java/org/chromium/chrome/browser/toolbar/menu_button/BraveMenuButtonCoordinator.java
@@ -21,6 +21,8 @@ import org.chromium.chrome.browser.theme.ThemeColorProvider;
 import org.chromium.chrome.browser.toolbar.top.BraveToolbarLayout;
 import org.chromium.chrome.browser.ui.appmenu.AppMenuCoordinator;
 import org.chromium.ui.base.WindowAndroid;
+import org.chromium.ui.modelutil.PropertyModel;
+import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
 
 public class BraveMenuButtonCoordinator extends MenuButtonCoordinator {
     private static final String BRAVE_IS_MENU_FROM_BOTTOM = "brave_is_menu_from_bottom";
@@ -77,5 +79,15 @@ public class BraveMenuButtonCoordinator extends MenuButtonCoordinator {
     public static boolean isMenuFromBottom() {
         SharedPreferences sharedPreferences = ContextUtils.getAppSharedPreferences();
         return sharedPreferences.getBoolean(BRAVE_IS_MENU_FROM_BOTTOM, true);
+    }
+
+    public static void setupPropertyModel(
+            MenuButton menuButton, Supplier<MenuButtonState> menuButtonStateSupplier) {
+        PropertyModel menuButtonPropertyModel =
+                new PropertyModel.Builder(MenuButtonProperties.ALL_KEYS)
+                        .with(MenuButtonProperties.STATE_SUPPLIER, menuButtonStateSupplier)
+                        .build();
+        PropertyModelChangeProcessor.create(
+                menuButtonPropertyModel, menuButton, new MenuButtonViewBinder());
     }
 }
