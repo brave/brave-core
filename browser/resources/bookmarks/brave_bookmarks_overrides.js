b0682a5f8d5abbb7750b110b02bc407c1ed65bab
Simon Hong <shong@brave.com>
2021-07-13 13:41:06 +0900

    Replaced innerHTML with html()

--- browser/resources/bookmarks/brave_bookmarks_overrides.js
+++ browser/resources/bookmarks/brave_bookmarks_overrides.js
@@ -3,8 +3,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at http://mozilla.org/MPL/2.0/.
 
-const template = document.createElement('template');
-template.innerHTML = `
+import {html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js'
+const template =  html`
 <dom-module id="brave-bookmarks-shared-style">{__html_template__}</dom-module>
 `;
 document.body.appendChild(template.content.cloneNode(true));
