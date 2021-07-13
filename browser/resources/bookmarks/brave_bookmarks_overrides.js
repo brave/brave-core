e193b2a1ec04e36a906c8161c04157a9ecc13727
Pete Miller <miller.pete@gmail.com>
2021-07-12 23:42:04 -0700

    Bookmarks WebUI: remove individual patch, include general patch. Plus, optimized builds will now included our modifications in the chromium bookmarks JS bundle

--- browser/resources/bookmarks/brave_bookmarks_overrides.js
+++ browser/resources/bookmarks/brave_bookmarks_overrides.js
@@ -1,10 +1,21 @@
 // Copyright (c) 2021 The Brave Authors. All rights reserved.
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
-// You can obtain one at http://mozilla.org/MPL/2.0/.
+// you can obtain one at http://mozilla.org/MPL/2.0/.
 
 import {html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js'
-const template =  html`
-<dom-module id="brave-bookmarks-shared-style">{__html_template__}</dom-module>
-`;
-document.body.appendChild(template.content.cloneNode(true));
+import {RegisterStyleOverride} from 'chrome://brave-resources/polymer_overriding.js'
+
+RegisterStyleOverride(
+  'bookmarks-folder-node',
+  html`
+    <style>
+<if expr="not is_macosx">
+      @media (prefers-color-scheme: dark) {
+        .folder-icon[open] { content: url(chrome://theme/IDR_BRAVE_BOOKMARK_FOLDER_OPEN_WHITE); }
+        .folder-icon { content: url(chrome://theme/IDR_BRAVE_BOOKMARK_FOLDER_CLOSED_WHITE); }
+      }
+</if>
+    </style>
+  `
+)
