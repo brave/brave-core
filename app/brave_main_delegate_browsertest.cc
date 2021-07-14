db7fa9d6090ff25341cd73802a1fa6a7d364dbf9
Max <41635752+mkarolin@users.noreply.github.com>
2021-07-14 09:21:48 -0400

    Merge pull request #9420 from brave/maxk-disable-account-data-storage-for-passwords
    Disables kEnablePasswordsAccountStorage feature.

--- app/brave_main_delegate_browsertest.cc
+++ app/brave_main_delegate_browsertest.cc
@@ -90,6 +90,7 @@ IN_PROC_BROWSER_TEST_F(BraveMainDelegateBrowserTest, DisabledFeatures) {
       &net::features::kFirstPartySets,
       &network::features::kTrustTokens,
       &network_time::kNetworkTimeServiceQuerying,
+      &password_manager::features::kEnablePasswordsAccountStorage,
   };
 
   for (const auto* feature : disabled_features)
