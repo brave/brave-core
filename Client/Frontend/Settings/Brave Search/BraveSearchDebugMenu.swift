006162927463ecb14fa0036c9a80819914d072f8
Michał Buczek <michal.buczek@protonmail.com>
2021-07-15 14:53:11 +0200

    Fix brave/brave-ios#3857: Add Brave Search debug menu. (brave/brave-ios#3919)

---
+++ Client/Frontend/Settings/Brave Search/BraveSearchDebugMenu.swift
@@ -0,0 +1,51 @@
+// Copyright 2021 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import SwiftUI
+
+struct BraveSearchDebugMenu: View {
+    
+    @ObservedObject var logging: BraveSearchLogEntry
+    
+    var body: some View {
+        List {
+            Section {
+                let toggle = Toggle("Enable callback logging", isOn: $logging.isEnabled)
+                if #available(iOS 14.0, *) {
+                    toggle
+                        .toggleStyle(SwitchToggleStyle(tint: .accentColor))
+                } else {
+                    toggle
+                }
+            }
+            
+            Section(header: Text("Logs")) {
+                ForEach(logging.logs) { logEntry in
+                    NavigationLink(destination: BraveSearchDebugMenuDetail(logEntry: logEntry)) {
+                        VStack(alignment: .leading) {
+                            Text(formattedDate(logEntry.date))
+                                .font(.caption)
+                            Text(logEntry.query)
+                                .font(.body)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    private func formattedDate(_ date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .short
+        return dateFormatter.string(from: date)
+    }
+}
+
+struct BraveSearchDebugMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        BraveSearchDebugMenu(logging: BraveSearchDebugMenuFixture.loggingSample)
+    }
+}
---
+++ Client/Frontend/Settings/Brave Search/BraveSearchDebugMenuDetail.swift
@@ -0,0 +1,81 @@
+// Copyright 2021 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import SwiftUI
+import Shared
+import BraveShared
+import BraveUI
+
+private let log = Logger.browserLogger
+
+struct BraveSearchDebugMenuDetail: View {
+  let logEntry: BraveSearchLogEntry.FallbackLogEntry
+  
+  @State private var showingSheet = false
+  
+  var body: some View {
+    Form {
+      ValueRow(title: "URL", value: logEntry.url.absoluteString)
+      
+      Section(header: Text("/can/answer")) {
+        ValueRow(title: "Cookies", value: cookieNames)
+        ValueRow(title: "Time taken(s)", value: logEntry.canAnswerTime ?? "-")
+        ValueRow(title: "Response", value: logEntry.backupQuery ?? "-")
+      }
+      
+      Section(header: Text("Search Fallback")) {
+        ValueRow(title: "Time taken(s)", value: logEntry.fallbackTime ?? "-")
+        
+        Button("Export response") {
+          showingSheet.toggle()
+        }
+        .disabled(logEntry.fallbackData == nil)
+        .background(
+          ActivityView(isPresented: $showingSheet, activityItems: [dataAsUrl].compactMap { $0 })
+        )
+      }
+    }
+  }
+  
+  private var cookieNames: String {
+    logEntry.cookies.map {
+      "\($0.name): \($0.value)"
+    }
+    .joined(separator: ", ")
+  }
+  
+  private var dataAsUrl: URL? {
+    guard let data = logEntry.fallbackData else { return nil }
+    let tempUrl = FileManager.default.temporaryDirectory.appendingPathComponent("output.html")
+    
+    do {
+      try data.write(to: tempUrl)
+      return tempUrl
+    } catch {
+      log.error("data write-to error")
+      return nil
+    }
+  }
+  
+  private struct ValueRow: View {
+    var title: LocalizedStringKey
+    var value: String
+    
+    var body: some View {
+      HStack {
+        Text(title)
+        Spacer()
+        Text(value)
+      }
+    }
+  }
+}
+
+struct BraveSearchDebugMenuDetail_Previews: PreviewProvider {
+  static var previews: some View {
+    BraveSearchDebugMenuDetail(
+      logEntry: BraveSearchDebugMenuFixture.sample)
+  }
+}
