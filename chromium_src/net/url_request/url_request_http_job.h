df534e0193f20a1428265bea00c906dd21d0a8aa
Mario Sanchez Prada <mario@igalia.com>
2021-07-15 16:23:15 +0200

    CookieOptions::SamePartyCookieContextType -> SamePartyContext
    Adapt to this change upstream, and add the new include required.
    
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/362c4a00017b8ed537014db3896c98552368a6b0
    
    commit 362c4a00017b8ed537014db3896c98552368a6b0
    Author: cfredric <cfredric@chromium.org>
    Date:   Fri Jul 9 22:40:40 2021 +0000
    
        Add UseCounters and UMA to measure incidence of SameSite=None cookies in
        certain contexts.
    
        Specifically, SameSite=None cookies that could be replaced by:
        * Nothing (have to remain a SameSite=None cookie)
        * a SameParty cookie, using the top frame and resource URL to determine
        sameparty-ness
        * a SameParty cookie, using the whole frame tree to determine
        sameparty-ness
        * a SameSite=Lax cookie
        * a SameSite=Strict cookie
    
        These metrics fire for both reads and writes.

--- chromium_src/net/url_request/url_request_http_job.h
+++ chromium_src/net/url_request/url_request_http_job.h
@@ -9,14 +9,15 @@
 #include "net/base/isolation_info.h"
 #include "net/base/request_priority.h"
 #include "net/cookies/cookie_options.h"
+#include "net/cookies/same_party_context.h"
 
-#define NotifyBeforeStartTransactionCallback                        \
-  NotUsed() const {}                                                \
-  CookieOptions CreateCookieOptions(                                \
-      CookieOptions::SameSiteCookieContext same_site_context,       \
-      CookieOptions::SamePartyCookieContextType same_party_context, \
-      const IsolationInfo& isolation_info,                          \
-      bool is_in_nontrivial_first_party_set) const;                 \
+#define NotifyBeforeStartTransactionCallback                  \
+  NotUsed() const {}                                          \
+  CookieOptions CreateCookieOptions(                          \
+      CookieOptions::SameSiteCookieContext same_site_context, \
+      SamePartyContext same_party_context,                    \
+      const IsolationInfo& isolation_info,                    \
+      bool is_in_nontrivial_first_party_set) const;           \
   void NotifyBeforeStartTransactionCallback
 
 #include "../../../../net/url_request/url_request_http_job.h"
