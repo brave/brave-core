795faa0c44ee0a2749dccc48aa6bcb172e7c86cc
Simon Hong <shong@brave.com>
2021-07-22 21:21:31 +0900

    Introduce brave vpn feature flag
    fix https://github.com/brave/brave-browser/issues/17108
    
    We will implement brave vpn behind this feature flag.

---
+++ components/brave_vpn/brave_vpn_unittest.cc
@@ -0,0 +1,17 @@
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "base/feature_list.h"
+#include "brave/components/brave_vpn/features.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace brave_vpn {
+
+TEST(BraveVPNTest, Basic) {
+  // Disable by default till we implement all brave vpn features.
+  EXPECT_FALSE(base::FeatureList::IsEnabled(kBraveVPNFeature));
+}
+
+}  // namespace brave_vpn
