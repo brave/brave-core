27679e281aa683618e92060bf3ff9b3cea2e18c5
mkarolin <max@brave.com>
2021-07-22 17:00:30 -0400

    IWYU: url_response_head.mojom.h

--- components/ipfs/ipfs_service.cc
+++ components/ipfs/ipfs_service.cc
@@ -39,6 +39,7 @@
 #include "net/http/http_status_code.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/cpp/simple_url_loader.h"
+#include "services/network/public/mojom/url_response_head.mojom.h"
 #include "url/gurl.h"
 
 #if BUILDFLAG(IPFS_LOCAL_NODE_ENABLED)
