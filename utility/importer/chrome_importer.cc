31dc285a8b4b0202f8530833e72557adb0536410
mkarolin <max@brave.com>
2021-07-22 18:01:19 -0400

    sql::Statement::ColumnBlob() now returns base::span.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src.git/+/1268a99
    
    commit 1268a999c4a953d7e2b2c26bf9fa019e38ee0504
    Author: Victor Costan <pwnall@chromium.org>
    Date:   Fri Jul 16 17:16:39 2021 +0000
    
        sql: Change the Statement::ColumnBlob() return type to base::span.
    
        ColumnBlob() currently returns a pointer to a data buffer, which must be
        paired with the return result of ColumnByteLength(). This made sense
        when the code was written, but we have safer and more ergonomic
        alternatives now.
    
        This CL switches the return type to base::span<const uint8_t>, which is
        the closest reflection of the fact that BLOBs are arrays of bytes.
        sql::Statement still has helpers for retrieving BLOB data as std::string
        and std::vector<char> / std::vector<uint8_t>.
    
        Bug: 1229451

--- utility/importer/chrome_importer.cc
+++ utility/importer/chrome_importer.cc
@@ -139,12 +139,11 @@ bool SetEncryptionKey(const base::FilePath& source_path) {
 
 std::u16string DecryptedCardFromColumn(sql::Statement* s, int column_index) {
   std::u16string credit_card_number;
-  int encrypted_number_len = s->ColumnByteLength(column_index);
-  if (encrypted_number_len) {
-    std::string encrypted_number;
-    encrypted_number.resize(encrypted_number_len);
-    memcpy(&encrypted_number[0], s->ColumnBlob(column_index),
-           encrypted_number_len);
+  base::span<const uint8_t> encrypted_number_blob = s->ColumnBlob(column_index);
+  if (!encrypted_number_blob.empty()) {
+    std::string encrypted_number(
+        reinterpret_cast<const char*>(encrypted_number_blob.data()),
+        encrypted_number_blob.size());
     OSCrypt::DecryptString16(encrypted_number, &credit_card_number);
   }
   return credit_card_number;
