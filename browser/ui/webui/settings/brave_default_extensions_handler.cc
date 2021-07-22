f5e6423583ee2d171c0d23459f1c17fbb0be3cb4
mkarolin <max@brave.com>
2021-07-22 19:25:44 -0400

    ListValue::GetInteger was removed.
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/09a28ea4051160db33c621c202600d086dce4840
    
    commit 09a28ea4051160db33c621c202600d086dce4840
    Author: Alex Cooper <alcooper@chromium.org>
    Date:   Mon Jul 19 23:10:30 2021 +0000
    
        [CodeHealth] Remove ListValue::GetInteger
    
        Now that all usages of ListValue::GetInteger have been removed from the
        code base, this deprecated method (and it's usage in the UnitTests), can
        be removed.
    
        Fixed: 1187087

--- browser/ui/webui/settings/brave_default_extensions_handler.cc
+++ browser/ui/webui/settings/brave_default_extensions_handler.cc
@@ -483,9 +483,9 @@ void BraveDefaultExtensionsHandler::LaunchIPFSService(
 void BraveDefaultExtensionsHandler::SetIPFSStorageMax(
     const base::ListValue* args) {
   CHECK_EQ(args->GetSize(), 1U);
+  CHECK(args[0].is_int());
   CHECK(profile_);
-  int storage_max_gb = 0;
-  args->GetInteger(0, &storage_max_gb);
+  int storage_max_gb = args[0].GetInt();
   PrefService* prefs = Profile::FromWebUI(web_ui())->GetPrefs();
   prefs->SetInteger(kIpfsStorageMax, storage_max_gb);
   ipfs::IpfsService* service =
