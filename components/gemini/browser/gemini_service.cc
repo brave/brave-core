4921cbf254a0d448fb38d996733edf070458ad8f
mkarolin <max@brave.com>
2021-07-22 17:00:30 -0400

    IWYU: url_response_head.mojom.h

--- components/gemini/browser/gemini_service.cc
+++ components/gemini/browser/gemini_service.cc
@@ -36,6 +36,7 @@
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/cpp/simple_url_loader.h"
 #include "services/network/public/mojom/fetch_api.mojom-shared.h"
+#include "services/network/public/mojom/url_response_head.mojom.h"
 
 namespace {
   const char oauth_host[] = "exchange.gemini.com";
