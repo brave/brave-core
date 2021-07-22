137c2f8d3c516364714b4e6027ff52b90e6430d1
bridiver <github@brianjohnson.cc>
2021-07-22 10:30:32 -0700

    ios build workarounds

--- ios/browser/api/wallet/brave_wallet_api.mm
+++ ios/browser/api/wallet/brave_wallet_api.mm
@@ -4,10 +4,10 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #import "brave/ios/browser/api/wallet/brave_wallet_api.h"
-#import "brave/ios/browser/api/wallet/keyring_controller_ios+private.h"
+// #import "brave/ios/browser/api/wallet/keyring_controller_ios+private.h"
 
 #include "base/strings/sys_string_conversions.h"
-#include "brave/components/brave_wallet/browser/keyring_controller.h"
+// #include "brave/components/brave_wallet/browser/keyring_controller.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
