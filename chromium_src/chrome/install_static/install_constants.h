92eb6dd249214d7549879f0f4e45cf2502ad7a69
mkarolin <max@brave.com>
2021-07-23 16:37:30 -0400

    install_static::ChannelStrategy values are now conditional on branding.
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/a5400731f7ac9251e6e383c0c176aa559db7ec68
    
    commit a5400731f7ac9251e6e383c0c176aa559db7ec68
    Author: Greg Thompson <grt@chromium.org>
    Date:   Tue Jul 20 20:40:50 2021 +0000
    
        install_static::ChannelStrategy branding cleanup.
    
        Make the values in the enum install_static::ChannelStrategy conditional
        on the branding. The meaning of the values are branding-specific, so it
        makes sense that the values themselves are, too.
    
        This is pure cleanup with no change in behavior.
    
        Bug: 1229222

--- chromium_src/chrome/install_static/install_constants.h
+++ chromium_src/chrome/install_static/install_constants.h
@@ -1,17 +1,23 @@
-/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+/* Copyright 2021 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// Brand-specific types and constants for Google Chrome.
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_INSTALL_STATIC_INSTALL_CONSTANTS_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_INSTALL_STATIC_INSTALL_CONSTANTS_H_
+
 #if defined(OFFICIAL_BUILD)
 #include "chrome/install_static/buildflags.h"
-#include "chrome/install_static/install_constants.h"
 #undef BUILDFLAG_INTERNAL_USE_GOOGLE_UPDATE_INTEGRATION
 #define BUILDFLAG_INTERNAL_USE_GOOGLE_UPDATE_INTEGRATION() (1)
 #endif
 
-#include "../../../../../chrome/installer/setup/install_worker.cc"
+#include "../../../../chrome/install_static/install_constants.h"
 
 #if defined(OFFICIAL_BUILD)
 #undef BUILDFLAG_INTERNAL_USE_GOOGLE_UPDATE_INTEGRATION
 #endif
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_INSTALL_STATIC_INSTALL_CONSTANTS_H_
