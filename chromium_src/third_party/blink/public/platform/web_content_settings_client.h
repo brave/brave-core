bd7ddb2fcee60767caa7242a0ec0ddb16b090727
Aleksey Khoroshilov <akhoroshilov@brave.com>
2021-07-28 20:34:50 +0700

    Remove unused StorageType parameter from EphemeralOrigin getter.

--- chromium_src/third_party/blink/public/platform/web_content_settings_client.h
+++ chromium_src/third_party/blink/public/platform/web_content_settings_client.h
@@ -9,18 +9,17 @@
 #include "brave/third_party/blink/renderer/brave_farbling_constants.h"
 #include "third_party/blink/public/platform/web_security_origin.h"
 
-#define AllowStorageAccessSync                                    \
-  AllowAutoplay(bool play_requested) { return true; }             \
-  virtual bool AllowFingerprinting(bool enabled_per_settings) {   \
-    return enabled_per_settings;                                  \
-  }                                                               \
-  virtual BraveFarblingLevel GetBraveFarblingLevel() {            \
-    return BraveFarblingLevel::OFF;                               \
-  }                                                               \
-  virtual blink::WebSecurityOrigin GetEphemeralStorageOriginSync( \
-      StorageType storageType) {                                  \
-    return blink::WebSecurityOrigin();                            \
-  }                                                               \
+#define AllowStorageAccessSync                                       \
+  AllowAutoplay(bool play_requested) { return true; }                \
+  virtual bool AllowFingerprinting(bool enabled_per_settings) {      \
+    return enabled_per_settings;                                     \
+  }                                                                  \
+  virtual BraveFarblingLevel GetBraveFarblingLevel() {               \
+    return BraveFarblingLevel::OFF;                                  \
+  }                                                                  \
+  virtual blink::WebSecurityOrigin GetEphemeralStorageOriginSync() { \
+    return blink::WebSecurityOrigin();                               \
+  }                                                                  \
   virtual bool AllowStorageAccessSync
 
 #include "../../../../../../third_party/blink/public/platform/web_content_settings_client.h"
