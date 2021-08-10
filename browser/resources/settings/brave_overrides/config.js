870086c75a97f827b39dbe27be41f70c26cdcd4e
Pete Miller <miller.pete@gmail.com>
2021-08-09 22:45:16 -0700

    Settings: override a classes "static get properties" via subclassing the actual element.
    We need to:
    - Make sure chromium's component does not get registered, so we add a dep to a JS module which makes sure the element name is on an ignore list
    - Define our own element which subclasses chromium's one and accesses and modifies the original static property

---
+++ browser/resources/settings/brave_overrides/config.js
@@ -0,0 +1,17 @@
+// Copyright (c) 2021 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+
+import {RegisterPolymerComponentToIgnore} from 'chrome://brave-resources/polymer_overriding.js'
+
+// Replacing chromium polymer components with subclasses of them is a
+// 2-step process:
+// 1. Make sure we ignore the chromium components when they are defined
+// 2. Override the chromium components with a subclass and define the
+//    components with their original chromium name.
+// (This is because the chomium components define themselves via customElements.define
+// in their module, so we want to register to ignore the component before the module
+// is imported).
+
+RegisterPolymerComponentToIgnore('settings-site-settings-page')
