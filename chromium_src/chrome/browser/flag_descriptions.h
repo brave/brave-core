6f26f8bc20485d2a64e7cbec15932d053ea271cd
Mario Sanchez Prada <mario@igalia.com>
2021-08-16 11:25:31 +0200

    Move Brave-specific flag descriptions out of brave/chromium_src
    Having all of Brave-specific flag descriptions inside a chromium_src
    override via chromium_src/chrome/browser/flag_descriptions.h and
    chromium_src/chrome/browser/flag_descriptions.cc is non-optimal for
    reviewing purposes as it requires chromium_src reviews each time
    something is changed there, as pointed out by @bridiver on Slack.
    
    Instead, we can just have a brave/browser/flag_descriptions.h file
    with that content and simply use the chromium_src override to
    include it when compiling upstream's flag_descriptions.h.
    
    Last, this commit re-orders the list of flags in alphabetically-sorted
    Name & Description pairs, just like chrome/browser/flag_descriptions.h.
    
    Resolves https://github.com/brave/brave-browser/issues/17456

--- chromium_src/chrome/browser/flag_descriptions.h
+++ chromium_src/chrome/browser/flag_descriptions.h
@@ -6,72 +6,7 @@
 #ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_FLAG_DESCRIPTIONS_H_
 #define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_FLAG_DESCRIPTIONS_H_
 
-#include "brave/components/brave_rewards/common/buildflags/buildflags.h"
-
 #include "../../../../chrome/browser/flag_descriptions.h"
-
-namespace flag_descriptions {
-extern const char kUseDevUpdaterUrlName[];
-extern const char kUseDevUpdaterUrlDescription[];
-extern const char kBraveNTPBrandedWallpaperName[];
-extern const char kBraveNTPBrandedWallpaperDescription[];
-extern const char kBraveNTPBrandedWallpaperDemoName[];
-extern const char kBraveNTPBrandedWallpaperDemoDescription[];
-extern const char kBraveAdblockDefault1pBlockingName[];
-extern const char kBraveAdblockDefault1pBlockingDescription[];
-extern const char kBraveAdblockCnameUncloakingName[];
-extern const char kBraveAdblockCnameUncloakingDescription[];
-extern const char kBraveAdblockCollapseBlockedElementsName[];
-extern const char kBraveAdblockCollapseBlockedElementsDescription[];
-extern const char kBraveAdblockCosmeticFilteringName[];
-extern const char kBraveAdblockCosmeticFilteringNativeName[];
-extern const char kBraveAdblockCosmeticFilteringDescription[];
-extern const char kBraveAdblockCosmeticFilteringNativeDescription[];
-extern const char kBraveAdblockCspRulesDescription[];
-extern const char kBraveAdblockCspRulesName[];
-extern const char kBraveDomainBlockName[];
-extern const char kBraveDomainBlockDescription[];
-extern const char kBraveExtensionNetworkBlockingName[];
-extern const char kBraveExtensionNetworkBlockingDescription[];
-extern const char kBraveDarkModeBlockName[];
-extern const char kBraveDarkModeBlockDescription[];
-extern const char kBraveSidebarName[];
-extern const char kBraveSidebarDescription[];
-extern const char kBraveVPNName[];
-extern const char kBraveVPNDescription[];
-extern const char kBraveSpeedreaderName[];
-extern const char kBraveSpeedreaderDescription[];
-extern const char kBraveSpeedreaderLegacyName[];
-extern const char kBraveSpeedreaderLegacyDescription[];
-extern const char kBraveSyncName[];
-extern const char kBraveSyncDescription[];
-extern const char kBraveIpfsName[];
-extern const char kBraveIpfsDescription[];
-
-#if BUILDFLAG(ENABLE_GEMINI_WALLET)
-extern const char kBraveRewardsGeminiName[];
-extern const char kBraveRewardsGeminiDescription[];
-#endif
-
-extern const char kBraveRewardsVerboseLoggingName[];
-extern const char kBraveRewardsVerboseLoggingDescription[];
-extern const char kBraveAdsCustomNotificationsName[];
-extern const char kBraveAdsCustomNotificationsDescription[];
-extern const char kNativeBraveWalletName[];
-extern const char kNativeBraveWalletDescription[];
-extern const char kBraveSearchDefaultAPIName[];
-extern const char kBraveSearchDefaultAPIDescription[];
-extern const char kBraveDecentralizedDnsName[];
-extern const char kBraveDecentralizedDnsDescription[];
-extern const char kBraveSuperReferralName[];
-extern const char kBraveSuperReferralDescription[];
-extern const char kBraveEphemeralStorageName[];
-extern const char kBraveEphemeralStorageDescription[];
-extern const char kBraveEphemeralStorageKeepAliveName[];
-extern const char kBraveEphemeralStorageKeepAliveDescription[];
-
-extern const char kBraveTalkName[];
-extern const char kBraveTalkDescription[];
-}  // namespace flag_descriptions
+#include "brave/browser/flag_descriptions.h"
 
 #endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_FLAG_DESCRIPTIONS_H_
