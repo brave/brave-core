c517f6be7b21034e195cea58cf6d8acefaf1d73f
Brian R. Bondy <netzen@gmail.com>
2021-08-26 14:26:13 -0400

    Rename eth_call_data_builder to eth_data_builder
    This is because we use it for transactions as well which are not related to the eth_call method

--- components/brave_wallet/browser/eth_data_builder.h
+++ components/brave_wallet/browser/eth_data_builder.h
@@ -3,8 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef BRAVE_COMPONENTS_BRAVE_WALLET_BROWSER_ETH_CALL_DATA_BUILDER_H_
-#define BRAVE_COMPONENTS_BRAVE_WALLET_BROWSER_ETH_CALL_DATA_BUILDER_H_
+#ifndef BRAVE_COMPONENTS_BRAVE_WALLET_BROWSER_ETH_DATA_BUILDER_H_
+#define BRAVE_COMPONENTS_BRAVE_WALLET_BROWSER_ETH_DATA_BUILDER_H_
 
 #include <string>
 #include <vector>
@@ -42,4 +42,4 @@ bool GetContentHashAddress(const std::string& domain, std::string* data);
 
 }  // namespace brave_wallet
 
-#endif  // BRAVE_COMPONENTS_BRAVE_WALLET_BROWSER_ETH_CALL_DATA_BUILDER_H_
+#endif  // BRAVE_COMPONENTS_BRAVE_WALLET_BROWSER_ETH_DATA_BUILDER_H_
