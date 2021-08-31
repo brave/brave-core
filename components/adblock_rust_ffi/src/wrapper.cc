0d9feb949e5d5a5ef4440385987c1a8154b9ec15
bridiver <github@brianjohnson.cc>
2021-08-31 13:13:25 -0700

    need to keep relative path for now for wrapper.h

--- components/adblock_rust_ffi/src/wrapper.cc
+++ components/adblock_rust_ffi/src/wrapper.cc
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "brave/components/adblock_rust_ffi/src/wrapper.h"
+#include "wrapper.h"  // NOLINT https://github.com/brave/brave-browser/issues/14821
 #include <iostream>
 
 extern "C" {
