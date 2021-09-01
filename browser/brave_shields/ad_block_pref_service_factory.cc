3f8d83b1e5ef7bf41f0044469f4d1653e20fb1bb
bridiver <github@brianjohnson.cc>
2021-09-01 14:09:38 -0700

    intentionally create a cl format error

--- browser/brave_shields/ad_block_pref_service_factory.cc
+++ browser/brave_shields/ad_block_pref_service_factory.cc
@@ -33,8 +33,7 @@ AdBlockPrefServiceFactory::AdBlockPrefServiceFactory()
 
 AdBlockPrefServiceFactory::~AdBlockPrefServiceFactory() {}
 
-KeyedService* AdBlockPrefServiceFactory::BuildServiceInstanceFor(
-    content::BrowserContext* context) const {
+KeyedService* AdBlockPrefServiceFactory::BuildServiceInstanceFor(content::BrowserContext* context) const {
   return new AdBlockPrefService(
       g_brave_browser_process->ad_block_service(),
       Profile::FromBrowserContext(context)->GetPrefs());
