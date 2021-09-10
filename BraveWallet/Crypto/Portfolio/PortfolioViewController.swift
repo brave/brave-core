2f24e7b62b22878727275a10d9a647d2a46b4b41
Kyle Hickinson <git@kylehickinson.com>
2021-09-10 16:29:58 -0400

    Cleanup in portfolio & backup banner

--- BraveWallet/Crypto/Portfolio/PortfolioViewController.swift
+++ BraveWallet/Crypto/Portfolio/PortfolioViewController.swift
@@ -24,17 +24,45 @@ struct PortfolioView: View {
   @ObservedObject var keyringStore: KeyringStore
   @ObservedObject var networkStore: EthNetworkStore
   
+  @State private var dismissedBackupBannerThisSession: Bool = false
+  @State private var isPresentingBackup: Bool = false
+  
+  private var isShowingBackupBanner: Bool {
+    !keyringStore.keyring.isBackedUp && !dismissedBackupBannerThisSession
+  }
+  
+  private var listHeader: some View {
+    VStack(spacing: 0) {
+      if isShowingBackupBanner {
+        BackupNotifyView(action: {
+          isPresentingBackup = true
+        }, onDismiss: {
+          // Animating this doesn't seem to work in SwiftUI.. will keep an eye out for iOS 15
+          dismissedBackupBannerThisSession = true
+        })
+        .buttonStyle(PlainButtonStyle())
+        .padding([.top, .leading, .trailing], 12)
+        .sheet(isPresented: $isPresentingBackup) {
+          NavigationView {
+            BackupRecoveryPhraseView(keyringStore: keyringStore)
+          }
+          .environment(\.modalPresentationMode, $isPresentingBackup)
+        }
+      }
+      BalanceHeaderView(
+        balance: "$12,453.17",
+        networkStore: networkStore
+      )
+    }
+  }
+  
   var body: some View {
     List {
       Section(
-        header: BalanceHeaderView(
-          balance: "$12,453.17",
-          keyringStore: keyringStore,
-          networkStore: networkStore
-        )
-        .padding(.horizontal, -16) // inset grouped layout margins workaround
-        .listRowInsets(.zero)
-        .textCase(.none)
+        header: listHeader
+          .padding(.horizontal, -16) // inset grouped layout margins workaround
+          .listRowInsets(.zero)
+          .textCase(.none)
       ) {
       }
       Section(
@@ -59,15 +87,12 @@ struct PortfolioView: View {
 @available(iOS 14.0, *)
 struct BalanceHeaderView: View {
   var balance: String
-  @ObservedObject var keyringStore: KeyringStore
   @ObservedObject var networkStore: EthNetworkStore
   
   @Environment(\.sizeCategory) private var sizeCategory
   @Environment(\.horizontalSizeClass) private var horizontalSizeClass
   @State private var selectedDateRange: BraveWallet.AssetPriceTimeframe = .oneDay
   @State private var selectedCandle: Candle?
-  @State private var dismissedBackupThisSession: Bool = false
-  @State private var isPresentingBackup: Bool = false
   
   var data: [Candle] {
     switch selectedDateRange {
@@ -130,46 +155,30 @@ struct BalanceHeaderView: View {
   }
   
   var body: some View {
-    VStack {
-      if !keyringStore.keyring.isBackedUp && !dismissedBackupThisSession {
-        BackupNotifyView(action: {
-          isPresentingBackup = true
-        }, onDismiss: {
-          dismissedBackupThisSession = true
-        })
-        .padding([.top, .leading, .trailing], 12)
-        .sheet(isPresented: $isPresentingBackup) {
-          NavigationView {
-            BackupRecoveryPhraseView(keyringStore: keyringStore)
-          }
-          .environment(\.modalPresentationMode, $isPresentingBackup)
-        }
+    VStack(alignment: .leading, spacing: 4) {
+      balanceOrDataPointView
+      HStack(spacing: 4) {
+        Image(systemName: "triangle.fill")
+          .font(.system(size: 8))
+          .foregroundColor(.green)
+        Text(verbatim: "1.3%")
+          .foregroundColor(.green)
+        Text(verbatim: "Today") // NSLocalizedString
+          .foregroundColor(.secondary)
       }
-      VStack(alignment: .leading, spacing: 4) {
-        balanceOrDataPointView
-        HStack(spacing: 4) {
-          Image(systemName: "triangle.fill")
-            .font(.system(size: 8))
-            .foregroundColor(.green)
-          Text(verbatim: "1.3%")
-            .foregroundColor(.green)
-          Text(verbatim: "Today") // NSLocalizedString
-            .foregroundColor(.secondary)
-        }
-        .font(.subheadline)
-        .frame(maxWidth: .infinity, alignment: .leading)
-        LineChartView(data: data, numberOfColumns: 12, selectedDataPoint: $selectedCandle) {
-          LinearGradient(braveGradient: .lightGradient02)
-        }
-        .frame(height: 148)
-        .padding(.horizontal, -12)
-        .animation(.default, value: data)
-        if horizontalSizeClass == .compact {
-          DateRangeView(selectedRange: $selectedDateRange)
-        }
+      .font(.subheadline)
+      .frame(maxWidth: .infinity, alignment: .leading)
+      LineChartView(data: data, numberOfColumns: 12, selectedDataPoint: $selectedCandle) {
+        LinearGradient(braveGradient: .lightGradient02)
       }
-      .padding(12)
+      .frame(height: 148)
+      .padding(.horizontal, -12)
+      .animation(.default, value: data)
+      if horizontalSizeClass == .compact {
+        DateRangeView(selectedRange: $selectedDateRange)
+      }
     }
+    .padding(12)
   }
 }
 
@@ -177,7 +186,10 @@ struct BalanceHeaderView: View {
 @available(iOS 14.0, *)
 struct PortfolioViewController_Previews: PreviewProvider {
   static var previews: some View {
-    PortfolioView(keyringStore: .previewStore, networkStore: .previewStore)
+    NavigationView {
+      PortfolioView(keyringStore: .previewStore, networkStore: .previewStore)
+        .navigationBarTitleDisplayMode(.inline)
+    }
       .previewColorSchemes()
   }
 }
