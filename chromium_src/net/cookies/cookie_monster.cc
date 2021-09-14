b3c21881544a192137e09405dc35646333377278
Mario Sanchez Prada <mario@igalia.com>
2021-09-14 19:06:19 +0200

    Add missing parameter to CookieMonster::GetCookieListWithOptionsAsync()
    We need to adapt the override to include the newly added parameter.
    
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/0b9a4e96a91a48872e1edda8611e49189f229328
    
    commit 0b9a4e96a91a48872e1edda8611e49189f229328
    Author: Dylan Cutler <dylancutler@google.com>
    Date:   Mon Sep 13 17:34:25 2021 +0000
    
        Add support for partitioned cookies to CookieMonster.
    
        This change is large, so here is a breakdown of the changes to help
        reviewers understand what is going on here:
    
        In cookie_monster.h, I define a new map, PartitionedCookieMap, which
        stores cookies set with the Partitioned attribute separately from
        unpartitioned cookies. The reason for doing so is:
    
        1. Since each request will need to request both partitioned and
           unpartitioned cookies, it is more efficient to query two smaller
           maps once than one larger map twice.
    
        2. Partitioned cookies will be subject to different garbage collection
           rules than normal cookies in order to prevent leaking information
           across different cookies partitions*. Storing partitioned cookies
           in a separate map makes implementing these garbage collection rules
           simpler.
    
        PartitionedCookieMap is a map of CookieMaps keyed on CookiePartitionKey.
        Each cookie partition is stored in a separate CookieMap, and some
        code which originally only operated on cookies_ was extended to operate
        on CookieMap instances supplied as arguments.
    
        Partitioned cookies are added/deleted from the map in the two new
        methods, CookieMonster::InternalInsertPartitionedCookie and
        CookieMonster::InternalDeletePartitionedCookie.
    
        PartitionedCookies are read from CookieMonster by adding a cookie
        partition key to the CookieOptions when calling
        CookieMonster::GetCookieListWithOptions. Partitioned cookies are
        also included in CookieMonster::GetAllCookies calls.
    
        *: See https://github.com/WICG/CHIPS#applying-the-180-cookies-per-domain-limit
    
        Bug: 1225444

--- chromium_src/net/cookies/cookie_monster.cc
+++ chromium_src/net/cookies/cookie_monster.cc
@@ -124,6 +124,7 @@ void CookieMonster::SetCanonicalCookieAsync(
 void CookieMonster::GetCookieListWithOptionsAsync(
     const GURL& url,
     const CookieOptions& options,
+    const absl::optional<net::CookiePartitionKey>& cookie_partition_key,
     GetCookieListCallback callback) {
   if (options.should_use_ephemeral_storage()) {
     if (!options.top_frame_origin()) {
@@ -136,13 +137,13 @@ void CookieMonster::GetCookieListWithOptionsAsync(
     ChromiumCookieMonster* ephemeral_monster =
         GetOrCreateEphemeralCookieStoreForTopFrameURL(
             options.top_frame_origin()->GetURL());
-    ephemeral_monster->GetCookieListWithOptionsAsync(url, options,
-                                                     std::move(callback));
+    ephemeral_monster->GetCookieListWithOptionsAsync(
+        url, options, cookie_partition_key, std::move(callback));
     return;
   }
 
-  ChromiumCookieMonster::GetCookieListWithOptionsAsync(url, options,
-                                                       std::move(callback));
+  ChromiumCookieMonster::GetCookieListWithOptionsAsync(
+      url, options, cookie_partition_key, std::move(callback));
 }
 
 }  // namespace net
