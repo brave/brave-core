147cc70d81c72ad4f2814ea31fc256edc9e7d566
Kyle Hickinson <git@kylehickinson.com>
2021-09-17 11:29:33 -0400

    SwiftUI extensions for BraveWallet mojo structs

---
+++ BraveWallet/Extensions/BraveWalletSwiftUIExtensions.swift
@@ -0,0 +1,30 @@
+// Copyright 2021 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import Foundation
+import SwiftUI
+import BraveCore
+
+extension BraveWallet.AccountInfo: Identifiable {
+  public var id: String {
+    address
+  }
+}
+
+extension BraveWallet.TransactionInfo: Identifiable {
+  // Already has `id` property
+}
+
+extension BraveWallet.EthereumChain: Identifiable {
+  public var id: String {
+    chainId
+  }
+}
+
+extension BraveWallet.ERCToken: Identifiable {
+  public var id: String {
+    contractAddress
+  }
+}
