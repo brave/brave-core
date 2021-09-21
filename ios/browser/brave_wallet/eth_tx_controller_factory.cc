5bd967410934041d1d64cecd267992f1c8f7e5ef
Kyle Hickinson <git@kylehickinson.com>
2021-09-21 12:26:06 -0400

    Add helper method to build EthTxController

--- ios/browser/brave_wallet/eth_tx_controller_factory.cc
+++ ios/browser/brave_wallet/eth_tx_controller_factory.cc
@@ -7,10 +7,9 @@
 
 #include <utility>
 
-#include "brave/components/brave_wallet/browser/eth_nonce_tracker.h"
 #include "brave/components/brave_wallet/browser/eth_tx_controller.h"
-#include "brave/components/brave_wallet/browser/eth_tx_state_manager.h"
 #include "brave/components/brave_wallet/browser/pref_names.h"
+#include "brave/components/brave_wallet/factory/eth_tx_controller_factory_helper.h"
 #include "brave/ios/browser/brave_wallet/eth_json_rpc_controller_factory.h"
 #include "brave/ios/browser/brave_wallet/keyring_controller_factory.h"
 #include "components/keyed_service/ios/browser_state_dependency_manager.h"
@@ -44,23 +43,12 @@ EthTxControllerFactory::~EthTxControllerFactory() = default;
 std::unique_ptr<KeyedService> EthTxControllerFactory::BuildServiceInstanceFor(
     web::BrowserState* context) const {
   auto* browser_state = ChromeBrowserState::FromBrowserState(context);
-  auto tx_state_manager = std::make_unique<EthTxStateManager>(
-      browser_state->GetPrefs(),
-      EthJsonRpcControllerFactory::GetControllerForBrowserState(browser_state)
-          ->MakeRemote());
-  auto eth_nonce_tracker = std::make_unique<EthNonceTracker>(
-      tx_state_manager.get(),
-      EthJsonRpcControllerFactory::GetControllerForBrowserState(browser_state));
-  auto eth_pending_tx_tracker = std::make_unique<EthPendingTxTracker>(
-      tx_state_manager.get(),
-      EthJsonRpcControllerFactory::GetControllerForBrowserState(browser_state),
-      eth_nonce_tracker.get());
-  std::unique_ptr<EthTxController> controller(new EthTxController(
-      EthJsonRpcControllerFactory::GetControllerForBrowserState(browser_state),
-      KeyringControllerFactory::GetControllerForBrowserState(browser_state),
-      std::move(tx_state_manager), std::move(eth_nonce_tracker),
-      std::move(eth_pending_tx_tracker), browser_state->GetPrefs()));
-  return controller;
+  auto* rpc_controller =
+      EthJsonRpcControllerFactory::GetControllerForBrowserState(browser_state);
+  auto* keyring_controller =
+      KeyringControllerFactory::GetControllerForBrowserState(browser_state);
+  return brave_wallet::BuildEthTxController(rpc_controller, keyring_controller,
+                                            browser_state->GetPrefs());
 }
 
 bool EthTxControllerFactory::ServiceIsNULLWhileTesting() const {
