69e5d0dc61e857b361129ccf0370588b2d604fa1
Kevin Kuehler <kevin@segfault.fun>
2021-09-21 10:38:41 -0700

    Dynamically inject WDP content script
    This way users that don't have the feature enabled won't incur the
    performance cost of loading the script.
    
    Includes webRequest optimizations in the web-discovery codebase.

--- components/brave_extension/extension/brave_extension/background/webDiscoveryProject.ts
+++ components/brave_extension/extension/brave_extension/background/webDiscoveryProject.ts
@@ -15,7 +15,27 @@ if (App !== undefined) {
 
   const WEB_DISCOVERY_PREF_KEY = 'brave.web_discovery_enabled'
 
-  const toggleWebDiscovery = (pref: chrome.settingsPrivate.PrefObject) => {
+  // Dynamically inject WDP content script so that users with the pref disabled
+  // don't need to pay the loading cost on each page.
+  chrome.tabs.onUpdated.addListener((tabId: number, changeInfo: chrome.tabs.TabChangeInfo, tab: chrome.tabs.Tab) => {
+    if (tab.id) {
+      const isEnabled = APP.modules['web-discovery-project'].isEnabled
+      const urlMatches = (): boolean => {
+        const url = tab.url || tab.pendingUrl || ''
+        return url ? !/chrome:\/\//.test(url) : false
+      }
+      // We inject on `complete` since multiple `loading` events can fire.
+      if (isEnabled && changeInfo.status === 'complete' && urlMatches()) {
+        chrome.tabs.executeScript(tab.id, {
+          file: 'bundles/web-discovery-content-script.bundle.js',
+          matchAboutBlank: false,
+          runAt: 'document_start'
+        }, () => { /* Deliberately left blank */ })
+      }
+    }
+  })
+
+  const toggleWebDiscovery = (pref?: chrome.settingsPrivate.PrefObject) => {
     if (pref && pref.type === chrome.settingsPrivate.PrefType.BOOLEAN) {
       const enable = pref.value
       if (enable) {
@@ -36,9 +56,7 @@ if (App !== undefined) {
 
   chrome.settingsPrivate.onPrefsChanged.addListener((prefs: chrome.settingsPrivate.PrefObject[]) => {
     const pref = prefs.find(p => p.key === WEB_DISCOVERY_PREF_KEY)
-    if (pref) {
-      toggleWebDiscovery(pref)
-    }
+    toggleWebDiscovery(pref)
   })
 
   chrome.settingsPrivate.getPref(WEB_DISCOVERY_PREF_KEY, (pref: chrome.settingsPrivate.PrefObject) => {
