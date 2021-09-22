31c18f20832a3623e4bc603e0fe75ac2ba331362
Mikhail Atuchin <matuchin@brave.com>
2021-09-22 17:45:29 +0700

    Lazily register onCommitted. Fix injection in devtool

--- components/brave_extension/extension/brave_extension/background/webDiscoveryProject.ts
+++ components/brave_extension/extension/brave_extension/background/webDiscoveryProject.ts
@@ -7,6 +7,28 @@ import { App } from '../../../../../vendor/web-discovery-project/build'
 
 declare var window: any
 
+
+function onCommitted (details: chrome.webNavigation.WebNavigationTransitionCallbackDetails) {
+  // Only inject if page is acceptable protocol (to skip internal pages like
+  // devtools://, brave://, etc.). We also make sure to only inject if we
+  // receive the onCommitted for the main_frame (frameId === 0).
+  if (
+    details.frameId === 0 &&
+    (details.url.startsWith('https://') || details.url.startsWith('http://'))
+  ) {
+    chrome.tabs.executeScript(details.tabId, {
+      file: 'bundles/web-discovery-content-script.bundle.js',
+      matchAboutBlank: false,
+      runAt: 'document_end'
+    }, () => {
+      if (chrome.runtime.lastError) {
+        console.warn('[web-discovery-project] Could not inject script:', chrome.runtime.lastError)
+      }
+    })
+  }
+}
+
+
 if (App !== undefined) {
   const APP = new App({
     version: chrome.runtime.getManifest().version
@@ -15,25 +37,6 @@ if (App !== undefined) {
 
   const WEB_DISCOVERY_PREF_KEY = 'brave.web_discovery_enabled'
 
-  // Dynamically inject WDP content script so that users with the pref disabled
-  // don't need to pay the loading cost on each page.
-  chrome.webNavigation.onCommitted.addListener((details: chrome.webNavigation.WebNavigationTransitionCallbackDetails) => {
-    // We skip injecting on bfcache load since it will cause us to double
-    // inject.
-    const isBfCache = details.transitionQualifiers.includes('forward_back')
-    if (APP.isRunning && !isBfCache && !/chrome:\/\//.test(details.url)) {
-      chrome.tabs.executeScript(details.tabId, {
-        file: 'bundles/web-discovery-content-script.bundle.js',
-        matchAboutBlank: false,
-        runAt: 'document_end'
-      }, () => {
-        if (chrome.runtime.lastError) {
-          console.warn('[web-discovery-project] Could not inject script:', chrome.runtime.lastError)
-        }
-      })
-    }
-  })
-
   const toggleWebDiscovery = (pref?: chrome.settingsPrivate.PrefObject) => {
     if (pref && pref.type === chrome.settingsPrivate.PrefType.BOOLEAN) {
       const enable = pref.value
@@ -41,10 +44,19 @@ if (App !== undefined) {
         // enable
         APP.start()
           .then(
-            () => { /* Deliberately left blank */ },
+            () => {
+              // Dynamically inject WDP content script so that users with the pref disabled
+              // don't need to pay the loading cost on each page.
+              chrome.webNavigation.onCommitted.addListener(onCommitted)
+            },
             (err) => { console.error('[web-discovery]', err) }
           )
       } else {
+        // Stop injecting dynamic content scripts
+        if (chrome.webNavigation.onCommitted.hasListener(onCommitted)) {
+          chrome.webNavigation.onCommitted.removeListener(onCommitted)
+        }
+
         // disable
         if (APP.isRunning) {
           APP.stop()
