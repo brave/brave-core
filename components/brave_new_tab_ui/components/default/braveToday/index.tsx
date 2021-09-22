5990ca2740013f067f11d6d403a87ea45c34dc79
Pete Miller <miller.pete@gmail.com>
2021-09-22 16:46:08 -0700

    Brave News: each promoted item rendered in the feed should generate a unique ID for the purposes of ad service view and visit reporting

--- components/brave_new_tab_ui/components/default/braveToday/index.tsx
+++ components/brave_new_tab_ui/components/default/braveToday/index.tsx
@@ -7,12 +7,12 @@ import * as React from 'react'
 import * as BraveTodayElement from './default'
 import CardOptIn from './cards/cardOptIn'
 import CardLoading from './cards/cardLoading'
-import { ReadFeedItemPayload, DisplayAdViewedPayload, VisitDisplayAdPayload } from '../../../actions/today_actions'
+import { PromotedItemViewedPayload, ReadFeedItemPayload, DisplayAdViewedPayload, VisitDisplayAdPayload } from '../../../actions/today_actions'
 const Content = React.lazy(() => import('./content'))
 
 export type OnReadFeedItem = (args: ReadFeedItemPayload) => any
 export type OnSetPublisherPref = (publisherId: string, enabled: boolean) => any
-export type OnPromotedItemViewed = (item: BraveToday.FeedItem) => any
+export type OnPromotedItemViewed = (args: PromotedItemViewedPayload) => any
 export type OnVisitDisplayAd = (args: VisitDisplayAdPayload) => any
 export type OnViewedDisplayAd = (args: DisplayAdViewedPayload) => any
 export type GetDisplayAdContent = () => Promise<BraveToday.DisplayAd | null>
