5eb1ef27e9a8193620d7188af0eb9b344a703643
Sergey P <spylogsster@gmail.com>
2021-09-24 00:19:17 +0300

    update

--- components/webpack/webpack.config.js
+++ components/webpack/webpack.config.js
@@ -89,7 +89,7 @@ module.exports = async function (env, argv) {
       alias: pathMap,
       // For explanation of "chromeapp", see:
       // https://github.com/brave/brave-browser/issues/5587
-      aliasFields: ['chromeapp']
+      aliasFields: ['chromeapp', 'browser']
     },
     optimization: {
       // Define NO_CONCATENATE for analyzing module size.
