45fe0f06420642dcce31b36a1e059f6a91ef2518
Mario Sanchez Prada <mario@igalia.com>
2021-09-24 18:21:48 +0200

    Fix EphemeralStorage-related browser tests again
    The upstream CL ntroduced a new call to LocalDOMWindow::GetStorageKey()
    inside of StorageNamespace::BindStorageArea(), making our overrides not
    to work as expected since we shouldn't consider redefining the call to
    that method with GetEphemeralStorageKeyOrStorageKey() when session
    storage is in use, so we need to update the override to reflect that.
    
    This fixes the following 20 browser tests:
    
    - EphemeralStorageNoSiteIsolationAndKeepAliveDisabledBrowserTest.RenderInitiatedNavigationClearsEphemeralStorage
    - EphemeralStorageTest.CrossSiteCookiesBlockedInitial
    - EphemeralStorageTest.CrossSiteCookiesBlockedNewPageResetSession
    - EphemeralStorageTest.CrossSiteCookiesBlockedRemotePageNewSession
    - EphemeralStorageTest.CrossSiteCookiesBlockedRemotePageSameSession
    - EphemeralStorageTest.CrossSiteCookiesBlockedThisPageDifferentSession
    - EphemeralStorageTest.CrossSiteCookiesBlockedThisPageSameSession
    - EphemeralStorageBrowserTest.BroadcastChannelIsPartitioned
    - EphemeralStorageBrowserTest.ClosingTabClearsEphemeralStorage
    - EphemeralStorageBrowserTest.EphemeralStorageDoesNotLeakBetweenProfiles
    - EphemeralStorageBrowserTest.FirstPartyNestedInThirdParty
    - EphemeralStorageBrowserTest.LocalStorageIsShared
    - EphemeralStorageBrowserTest.NavigatingClearsEphemeralStorageAfterKeepAlive
    - EphemeralStorageBrowserTest.NetworkCookiesArePartitioned
    - EphemeralStorageBrowserTest.NetworkCookiesAreSentIn3p
    - EphemeralStorageBrowserTest.NetworkCookiesAreSetIn3p
    - EphemeralStorageBrowserTest.ReloadDoesNotClearEphemeralStorage
    - EphemeralStorageBrowserTest.StorageIsPartitioned
    - EphemeralStorageKeepAliveDisabledBrowserTest.NavigatingClearsEphemeralStorageWhenKeepAliveDisabled
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/b54d90420260245961929f43e8fb19f1a2e62e6a
    
    commit b54d90420260245961929f43e8fb19f1a2e62e6a
    Author: Ari Chivukula <arichiv@chromium.org>
    Date:   Tue Sep 21 00:27:14 2021 +0000
    
        [DOM Storage + StorageKey] (2) Add StorageKey to IPC
    
        This CL takes the first of two steps in IPC changes from the StorageKey
        migration. The step taken here is the lower risk one as (for the moment)
        StorageKey is a wrapper for Origin. The following step (adding
        LocalFrameToken and verifying it) was the source of the errors last
        time.
    
        The prior attempt at this change (https://crrev.com/c/3122751). Took a
        quick and dirty approach which required a revert. This code is on a
        critical path, has a lot of quick fixes layered, and is under-tested.
        Let's take a slower approach as follows:
        (1) Refactor session and local storage checks
        (2) Add StorageKey to IPC
        (3) Add LocalFrameToken to IPC and verify
    
        Bug: 1212808

--- chromium_src/third_party/blink/renderer/modules/storage/storage_namespace.cc
+++ chromium_src/third_party/blink/renderer/modules/storage/storage_namespace.cc
@@ -9,14 +9,29 @@
 #include "third_party/blink/renderer/modules/storage/storage_controller.h"
 
 #define GetStorageKey GetEphemeralStorageKeyOrStorageKey
-
-#define OpenLocalStorage                                                  \
-  OpenLocalStorage(local_dom_window.GetEphemeralStorageKeyOrStorageKey(), \
-                   std::move(receiver));                                  \
-  if (false)                                                              \
-  controller_->dom_storage()->OpenLocalStorage
+#define BindStorageArea BindStorageArea_Unused
 
 #include "../../../../../../../third_party/blink/renderer/modules/storage/storage_namespace.cc"
 
-#undef OpenLocalStorage
+#undef BindStorageArea
 #undef GetStorageKey
+
+namespace blink {
+
+// We only want to call GetEphemeralStorageKeyOrStorageKey() instead of
+// GetStorageKey() when not using session storage, but that call will also get
+// overriden by the define above, so we need to provide the entire method.
+void StorageNamespace::BindStorageArea(
+    const LocalDOMWindow& local_dom_window,
+    mojo::PendingReceiver<mojom::blink::StorageArea> receiver) {
+  if (IsSessionStorage()) {
+    controller_->dom_storage()->BindSessionStorageArea(
+        local_dom_window.GetStorageKey(), namespace_id_, std::move(receiver));
+  } else {
+    controller_->dom_storage()->OpenLocalStorage(
+        local_dom_window.GetEphemeralStorageKeyOrStorageKey(),
+        std::move(receiver));
+  }
+}
+
+}  // namespace blink
