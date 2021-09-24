b5dabe59d7718687f95eae828e9a389dbf104d0d
Mario Sanchez Prada <mario@igalia.com>
2021-09-24 12:19:46 +0200

    Adapt override of GetProductNameAndVersionForUserAgent()
    Needs to retun a const std::string reference, not a new string.
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/6295d7860b41b5201e4136341ef6be107b485334
    
    commit 6295d7860b41b5201e4136341ef6be107b485334
    Author: Ali Beyad <abeyad@chromium.org>
    Date:   Tue Sep 21 15:26:32 2021 +0000
    
        Use a static variable to store the Chrome Product Version
    
        The Chrome Product Version that is returned by
        version_info::GetProductNameAndVersionForUserAgent is a static string
        that can never change during the course of a running binary, since the
        PRODUCT_VERSION it is based off of is a macro generated at build time.
    
        Instead of recreating this string on every invocation, we store it in a
        static constant.
    
        This CL also cleans up some unnecessary namespace qualifications.

--- chromium_src/components/version_info/version_info.cc
+++ chromium_src/components/version_info/version_info.cc
@@ -1,17 +1,22 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #define GetChannelString GetChannelString_ChromiumImpl
-#define GetProductNameAndVersionForUserAgent GetProductNameAndVersionForUserAgent_Unused
+#define GetProductNameAndVersionForUserAgent \
+  GetProductNameAndVersionForUserAgent_Unused
+
 #include "../../../../components/version_info/version_info.cc"
 #undef GetChannelString
 #undef GetProductNameAndVersionForUserAgent
 
 namespace version_info {
 
-std::string GetProductNameAndVersionForUserAgent() {
-  return std::string("Chrome/") + BRAVE_CHROMIUM_VERSION;
+const std::string& GetProductNameAndVersionForUserAgent() {
+  static const base::NoDestructor<std::string> product_and_version(
+      "Chrome/" + std::string(BRAVE_CHROMIUM_VERSION));
+  return *product_and_version;
 }
 
 // We use |nightly| instead of |canary|.
