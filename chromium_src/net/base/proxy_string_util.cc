c455bc564cd969d1a9f072643ea0ef9f6f654f3b
Mario Sanchez Prada <mario@igalia.com>
2021-09-29 13:01:33 +0200

    Adapt to changes in ProxyServer and its related utilities
    ProxyServer does no longer construct a HostPortPair manually from the
    ProxyServer::FromSchemeHostAndPort() method, but rely in a new utility
    functions instead, which explicitly used the host and ports and ignore
    the authentication information, if present.
    
    This change, along with some additional refactoring done around these
    classes, means we need to rework our override/patches to work in a
    slightly different way. This patch implementes the idea of (1) making
    sure that we don't bail out early when auth information is available,
    that (2) we get that information inserted in the HostPortPair that is
    used to created a ProxyServer, and (3) that we adapt the new utility
    function ProxyServerToProxyUri() to make sure it inserts the auth
    information, if present, in the string returned.
    
    Additionally, this also removes our own ParseAuthHostAndPort() function
    since we can just use url::ParseAuthority() instead.
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/e71c7f43d205a4a28f799ac4487c5942f496c7cb
    
    commit 5ccc3f0d4f3bb7d9b9caa93c266f3dbdfbf83205
    Author: Eric Orth <ericorth@chromium.org>
    Date:   Thu Sep 23 00:01:57 2021 +0000
    
        Split ProxyServer PAC/URI conversion to a separate file
    
        No functional change.
    
        Helps simplify the ProxyServer class around its core purpose of
        representing proxy servers for net stack connections, which will
        make it easier to make upcoming refactors to both that and the
        separated-off conversion functionality.
    
        Bug: 1243398
    
    commit 5dbd4599a981da1fac5dc5211772fe843a06a9b0
    Author: Eric Orth <ericorth@chromium.org>
    Date:   Fri Sep 24 21:21:24 2021 +0000
    
        Stop dealing with HostPortPair in proxy_string_util.(cc/h)
    
        String->ProxyServer now done using ProxyServer::FromSchemeHostAndPort,
        and ProxyServer->String now done using ProxyServer::GetHost() and
        ProxyServer::GetPort(). With less ProxyServer-external dealing with
        HostPortPair, will aide the future-CL transition to not storing
        HostPortPair for all proxies.
    
        Also has the effect that construction-from-string now results in
        canonicalization of the ProxyServer hostname. Slight impact on tests
        that were parsing stuff with now-recognized-as-invalid hostnames
        (especially the v8 tracing tests that were hacking information into
        wildly invalid hostnames), but don't expect any real-world impact
        because these are clearly invalid names that shouldn't ever be able to
        resolve to real servers.
    
        Also moved the hostname:port parsing directly into proxy_string_util.cc
        (from url_util::ParseHostAndPort()) to avoid silliness like stripping
        IPv6 brackets just for them to be added back for canonicalization.
    
        Bug: 1243398

---
+++ chromium_src/net/base/proxy_string_util.cc
@@ -0,0 +1,97 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "net/base/proxy_string_util.h"
+
+#include "base/strings/strcat.h"
+#include "url/third_party/mozilla/url_parse.h"
+
+namespace net {
+namespace {
+
+// Based on FromSchemeHostAndPort() from proxy_server.cc, but to consider auth
+// information when creating a ProxyServer, instead of bailing out.
+ProxyServer CreateProxyServerWithAuthInfo(
+    const ProxyServer::Scheme& scheme,
+    const base::StringPiece& host_and_port) {
+  url::Component username_component;
+  url::Component password_component;
+  url::Component hostname_component;
+  url::Component port_component;
+  url::ParseAuthority(host_and_port.data(),
+                      url::Component(0, host_and_port.size()),
+                      &username_component, &password_component,
+                      &hostname_component, &port_component);
+
+  base::StringPiece hostname =
+      host_and_port.substr(hostname_component.begin, hostname_component.len);
+  if (port_component.is_valid() && !port_component.is_nonempty())
+    return ProxyServer();
+  base::StringPiece port =
+      port_component.is_nonempty()
+          ? host_and_port.substr(port_component.begin, port_component.len)
+          : "";
+
+  // Prepend AUTH info to hostname if needed before creating the ProxyServer.
+  std::string auth_hostname(hostname);
+  std::string auth_str;
+  if (username_component.is_valid()) {
+    auth_str = std::string(
+        host_and_port.substr(username_component.begin, username_component.len));
+    if (password_component.is_valid()) {
+      auth_str = base::StrCat({auth_str, ":",
+                               host_and_port.substr(password_component.begin,
+                                                    password_component.len)});
+    }
+    auth_hostname = auth_str + "@" + auth_hostname;
+  }
+
+  return ProxyServer::FromSchemeHostAndPort(scheme, auth_hostname, port);
+}
+
+}  // namespace
+
+std::string ProxyServerToProxyUri(const ProxyServer& proxy_server) {
+  std::string scheme_prefix;
+  std::string proxy_uri = ProxyServerToProxyUri_ChromiumImpl(proxy_server);
+  size_t colon = proxy_uri.find(':');
+  if (colon != std::string::npos && proxy_uri.size() - colon >= 3 &&
+      proxy_uri[colon + 1] == '/' && proxy_uri[colon + 2] == '/') {
+    scheme_prefix = proxy_uri.substr(0, colon + 3);
+    proxy_uri = proxy_uri.substr(colon + 3);  // Skip past the "://"
+  }
+
+  std::string auth_string;
+  const HostPortPair& host_port_pair = proxy_server.host_port_pair();
+  if (!host_port_pair.username().empty()) {
+    auth_string += host_port_pair.username();
+    if (!host_port_pair.password().empty()) {
+      auth_string += ':' + host_port_pair.password();
+    }
+    auth_string += '@';
+  }
+
+  std::string result = scheme_prefix + auth_string + proxy_uri;
+  return result;
+}
+
+}  // namespace net
+
+#define ProxyServerToProxyUri ProxyServerToProxyUri_ChromiumImpl
+
+#define ParseAuthority(HOST_AND_PORT, AUTH, USER, PASS, HOST, PORT)            \
+  ParseAuthority(host_and_port.data(),                                         \
+                 url::Component(0, host_and_port.size()), &username_component, \
+                 &password_component, &hostname_component, &port_component);   \
+  url::ParseAuthority(HOST_AND_PORT, AUTH, USER, PASS, HOST, PORT);            \
+  if (!hostname_component.is_nonempty())                                       \
+    return ProxyServer();                                                      \
+  else                                                                         \
+    return CreateProxyServerWithAuthInfo(scheme, host_and_port);
+
+#include "../../../../net/base/proxy_string_util.cc"
+
+#undef ParseAuthority
+#undef ProxyServerToProxyUri
