130216567d88147a9a41e272df79ba83ccfc8ecb
bridiver <github@brianjohnson.cc>
2021-09-29 12:49:21 -0700

    lint fix

--- browser/brave_wallet/brave_wallet_service_delegate_helper.cc
+++ browser/brave_wallet/brave_wallet_service_delegate_helper.cc
@@ -3,8 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "brave/components/brave_wallet/browser/brave_wallet_service_delegate.h"
-
 #include <memory>
 
 #include "brave/browser/brave_wallet/brave_wallet_service_delegate_impl.h"
