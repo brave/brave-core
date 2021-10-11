98108bdb3f733c4796fac4b183702f6be67e6f48
Jocelyn Liu <yrliou@gmail.com>
2021-10-10 17:29:42 -0700

    Tests

--- components/brave_wallet/browser/eth_response_parser_unittest.cc
+++ components/brave_wallet/browser/eth_response_parser_unittest.cc
@@ -8,6 +8,7 @@
 #include <vector>
 
 #include "brave/components/brave_wallet/browser/eth_response_parser.h"
+#include "brave/components/ipfs/ipfs_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace brave_wallet {
@@ -135,6 +136,126 @@ TEST(EthResponseParserUnitTest, ParseEthGetTransactionReceiptNullContractAddr) {
   EXPECT_EQ(receipt.contract_address, "");
   EXPECT_EQ(receipt.logs_bloom, "0x00...0");
   EXPECT_TRUE(receipt.status);
+}
+
+TEST(EthResponseParserUnitTest, ParseEnsAddress) {
+  std::string json =
+      "{\"jsonrpc\":\"2.0\",\"id\":1,\"result\":"
+      "\"0x0000000000000000000000004976fb03c32e5b8cfe2b6ccb31c09ba78ebaba41\"}";
+  std::string addr;
+  EXPECT_TRUE(ParseEnsAddress(json, &addr));
+  EXPECT_EQ(addr, "0x4976fb03c32e5b8cfe2b6ccb31c09ba78ebaba41");
+
+  // Non-expected address size.
+  addr = "";
+  json =
+      "{\"jsonrpc\":\"2.0\",\"id\":1,\"result\":"
+      "\"0x0000000000000000000000004976fb03c32e5b8cfe2b6ccb31c09ba78eba\"}";
+  EXPECT_FALSE(ParseEnsAddress(json, &addr));
+  EXPECT_TRUE(addr.empty());
+}
+
+TEST(EthResponseParserUnitTest, ParseEnsResolverContentHash) {
+  std::string json =
+      "{\"jsonrpc\":\"2.0\",\"id\":1,\"result\":"
+      "\"0x00000000000000000000000000000000000000000000000000000000000000200000"
+      "00"
+      "0000000000000000000000000000000000000000000000000000000026e3010170122023"
+      "e0160eec32d7875c19c5ac7c03bc1f306dc260080d621454bc5f631e7310a70000000000"
+      "000000000000000000000000000000000000000000\"}";
+  std::string content_hash;
+  EXPECT_TRUE(ParseEnsResolverContentHash(json, &content_hash));
+  EXPECT_EQ(
+      ipfs::ContentHashToCIDv1URL(content_hash).spec(),
+      "ipfs://bafybeibd4ala53bs26dvygofvr6ahpa7gbw4eyaibvrbivf4l5rr44yqu4");
+
+  content_hash = "";
+  json =
+      "{\"jsonrpc\":\"2.0\",\"id\":1,\"result\":"
+      "\"0x000000000000000000000000000000000000000000000000000000000000002000";
+
+  EXPECT_FALSE(ParseEnsResolverContentHash(json, &content_hash));
+  EXPECT_TRUE(content_hash.empty());
+}
+
+TEST(EthResponseParserUnitTest, ParseUnstoppableDomainsProxyReaderGetMany) {
+  std::string json =
+      "{\"jsonrpc\":\"2.0\",\"id\":1,\"result\":"
+      // offset for array
+      "\"0x0000000000000000000000000000000000000000000000000000000000000020"
+      // count for array
+      "0000000000000000000000000000000000000000000000000000000000000006"
+      // offsets for array elements
+      "00000000000000000000000000000000000000000000000000000000000000c0"
+      "0000000000000000000000000000000000000000000000000000000000000120"
+      "0000000000000000000000000000000000000000000000000000000000000180"
+      "00000000000000000000000000000000000000000000000000000000000001a0"
+      "00000000000000000000000000000000000000000000000000000000000001c0"
+      "0000000000000000000000000000000000000000000000000000000000000200"
+      // count for "QmWrdNJWMbvRxxzLhojVKaBDswS4KNVM7LvjsN7QbDrvka"
+      "000000000000000000000000000000000000000000000000000000000000002e"
+      // encoding for "QmWrdNJWMbvRxxzLhojVKaBDswS4KNVM7LvjsN7QbDrvka"
+      "516d5772644e4a574d62765278787a4c686f6a564b614244737753344b4e564d"
+      "374c766a734e3751624472766b61000000000000000000000000000000000000"
+      // count for "QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR"
+      "000000000000000000000000000000000000000000000000000000000000002e"
+      // encoding for "QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR"
+      "516d6257717842454b433350387471734b633938786d574e7a727a4474524c4d"
+      "694d504c387742755447734d6e52000000000000000000000000000000000000"
+      // count for empty dns.A
+      "0000000000000000000000000000000000000000000000000000000000000000"
+      // count for empty dns.AAAA
+      "0000000000000000000000000000000000000000000000000000000000000000"
+      // count for "https://fallback1.test.com"
+      "000000000000000000000000000000000000000000000000000000000000001a"
+      // encoding for "https://fallback1.test.com"
+      "68747470733a2f2f66616c6c6261636b312e746573742e636f6d000000000000"
+      // count for "https://fallback2.test.com"
+      "000000000000000000000000000000000000000000000000000000000000001a"
+      // encoding for "https://fallback2.test.com"
+      "68747470733a2f2f66616c6c6261636b322e746573742e636f6d000000000000\"}";
+
+  std::vector<std::string> expected_values = {
+      "QmWrdNJWMbvRxxzLhojVKaBDswS4KNVM7LvjsN7QbDrvka",  // dweb.ipfs.hash
+      "QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR",  // ipfs.html.value
+      "",                                                // dns.A
+      "",                                                // dns.AAAA
+      "https://fallback1.test.com",                      // browser.redirect_url
+      "https://fallback2.test.com",  // ipfs.redirect_domain.value
+  };
+
+  std::vector<std::string> values;
+  EXPECT_TRUE(ParseUnstoppableDomainsProxyReaderGetMany(json, &values));
+  EXPECT_EQ(values, expected_values);
+
+  values.clear();
+  json =
+      "{\"jsonrpc\":\"2.0\",\"id\":1,\"result\":"
+      "\"0x000000000000000000000000000000000000000000000000000000000000002000";
+  EXPECT_FALSE(ParseUnstoppableDomainsProxyReaderGetMany(json, &values));
+  EXPECT_TRUE(values.empty());
+}
+
+TEST(EthResponseParserUnitTest, ParseUnstoppableDomainsProxyReaderGet) {
+  std::string json =
+      "{\"jsonrpc\":\"2.0\",\"id\":1,\"result\":"
+      // offset to string
+      "\"0x0000000000000000000000000000000000000000000000000000000000000020"
+      // string len
+      "000000000000000000000000000000000000000000000000000000000000002a"
+      // Encoded string of 0x8aaD44321A86b170879d7A244c1e8d360c99DdA8
+      "3078386161443434333231413836623137303837396437413234346331653864"
+      "3336306339394464413800000000000000000000000000000000000000000000\"}";
+  std::string value;
+  EXPECT_TRUE(ParseUnstoppableDomainsProxyReaderGet(json, &value));
+  EXPECT_EQ(value, "0x8aaD44321A86b170879d7A244c1e8d360c99DdA8");
+
+  value = "";
+  json =
+      "{\"jsonrpc\":\"2.0\",\"id\":1,\"result\":"
+      "\"0x000000000000000000000000000000000000000000000000000000000000002000";
+  EXPECT_FALSE(ParseUnstoppableDomainsProxyReaderGet(json, &value));
+  EXPECT_TRUE(value.empty());
 }
 
 }  // namespace brave_wallet
