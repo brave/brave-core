bf721684b01696e1fd70fb3159fa5fffa1f2d224
Brian Clifton <brian@clifton.me>
2021-10-10 23:23:23 -0700

    Fixes for linker errors and workaround for CxxString crash on Windows
    This commit squashes a few commits together:
    - patch from @jumde https://github.com/brave/brave-core/pull/10844/commits/6f9dd94d7e875b5aff7628a72055eb7a37034262
    - patch from @antonok-edm https://github.com/brave/brave-core/pull/10844/commits/e98549d43f442bc6ccc8c18f9c60db727ebc9e47
    - patch from @bsclifton https://github.com/brave/brave-core/pull/10844/commits/6a532974326da6d5b21f55b1e082feab11cb006e
    - uses `::rust::String` (String) instead of `std::string` (CxxString) for KV retrieval
    - lint / gn_check / misc cleanup

--- components/skus/browser/brave-rewards-cxx/src/shim.h
+++ components/skus/browser/brave-rewards-cxx/src/shim.h
@@ -10,11 +10,14 @@ enum class RewardsResult : uint8_t;
 struct HttpRequest;
 struct HttpResponse;
 struct HttpRoundtripContext;
+class SkusSdkImpl;
+class SkusSdkFetcher;
 
 class RefreshOrderCallbackState {
  public:
   skus::mojom::SkusSdk::RefreshOrderCallback cb;
-  // TODO: possibly store Prefs object here
+  SkusSdkImpl* instance;
+  SkusSdkFetcher* fetcher;
 };
 
 using RefreshOrderCallback = void (*)(RefreshOrderCallbackState* callback_state,
@@ -22,7 +25,7 @@ using RefreshOrderCallback = void (*)(RefreshOrderCallbackState* callback_state,
                                       rust::cxxbridge1::Str order);
 void shim_purge();
 void shim_set(rust::cxxbridge1::Str key, rust::cxxbridge1::Str value);
-const std::string& shim_get(rust::cxxbridge1::Str key);
+::rust::String shim_get(rust::cxxbridge1::Str key);
 
 void shim_scheduleWakeup(::std::uint64_t delay_ms,
                          rust::cxxbridge1::Fn<void()> done);
