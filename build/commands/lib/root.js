69346486196937f819e3e0ebbd90e5cd2bf65925
Mikhail Atuchin <matuchin@brave.com>
2021-10-13 20:33:47 +0700

    Use upper case drives letter in build for win

--- build/commands/lib/root.js
+++ build/commands/lib/root.js
@@ -1,3 +1,10 @@
 const path = require('path')
 
-module.exports = path.resolve(__dirname, '..', '..', '..', '..', '..')
+let normalizedSrcDir = __dirname
+if (process.platform === 'win32' &&
+    normalizedSrcDir.length >= 2 &&
+    normalizedSrcDir[1] == ':') {
+  normalizedSrcDir = normalizedSrcDir[0].toUpperCase() + normalizedSrcDir.substr(1)
+}
+
+module.exports = path.resolve(normalizedSrcDir, '..', '..', '..', '..', '..')
