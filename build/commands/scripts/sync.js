8d213b95ac0d6d81a01a8ce7e293c03314658cfc
bridiver <github@brianjohnson.cc>
2021-10-12 17:12:22 -0700

    only allow braveCoreRef with `init`

--- build/commands/scripts/sync.js
+++ build/commands/scripts/sync.js
@@ -23,7 +23,6 @@ program
   .option('--init', 'initialize all dependencies')
   .option('--all', 'This flag is deprecated and no longer has any effect')
   .option('--force', 'force reset all projects to origin/ref')
-  .option('--create', 'create a new branch if needed for [ref]')
 
 const installDepotTools = (options = config.defaultOptions) => {
   options.cwd = config.braveCoreDir
@@ -62,6 +61,11 @@ async function RunCommand () {
     Log.warn('--all, --run_hooks and --run_sync are deprecated. Will behave as if flag was not passed. Please update your command to `npm run sync` in the future.')
   }
 
+  if (braveCoreRef && !program.init) {
+    Log.error('[ref] option requies --init to work correctly')
+    process.exit(1)
+  }
+
   if (program.init || !fs.existsSync(config.depotToolsDir)) {
     installDepotTools()
   }
@@ -79,23 +83,6 @@ async function RunCommand () {
     // we're doing a reset of brave-core so try to stash any changes
     Log.progress('Stashing any local changes')
     util.runGit(config.braveCoreDir, ['stash'], true)
-  }
-
-  if (braveCoreRef) {
-    Log.progress(`Resetting brave core to "${braveCoreRef}"...`)
-    // try to checkout to the right ref if possible
-    util.runGit(config.braveCoreDir, ['reset', '--hard', 'HEAD'], true)
-    let checkoutResult = util.runGit(config.braveCoreDir, ['checkout', braveCoreRef], true)
-    if (checkoutResult === null && program.create) {
-      checkoutResult = util.runGit(config.braveCoreDir, ['checkout', '-b', braveCoreRef], true)
-    }
-    // Handle checkout failure
-    if (checkoutResult === null) {
-      Log.error('Could not checkout: ' + braveCoreRef)
-    }
-    // Checkout was successful
-    const braveCoreSha = util.runGit(config.braveCoreDir, ['rev-parse', 'HEAD'])
-    Log.progress(`...brave core is now at commit ID ${braveCoreSha}`)
   }
 
   Log.progress('Running gclient sync...')
