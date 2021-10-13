5d791cf36a4ed99a6f62f141122517c3b3717a3a
Mikhail Atuchin <matuchin@brave.com>
2021-10-13 22:06:26 +0700

    clang format

--- build/commands/lib/root.js
+++ build/commands/lib/root.js
@@ -1,9 +1,7 @@
 const path = require('path')
 
 let dirName = __dirname
-if (process.platform === 'win32' &&
-  dirName.length >= 2 &&
-  dirName[1] == ':') {
+if (process.platform === 'win32' && dirName.length >= 2 && dirName[1] == ':') {
   dirName = dirName[0].toUpperCase() + dirName.substr(1)
 }
 
