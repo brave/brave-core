26cf3013960964cac44ed80551454c31e2c15536
Mikhail Atuchin <matuchin@brave.com>
2021-10-18 18:45:39 +0700

    Fix footers

--- browser/translate/brave_translate_utils.cc
+++ browser/translate/brave_translate_utils.cc
@@ -1,6 +1,7 @@
-// Copyright 2021 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/browser/translate/brave_translate_utils.h"
 
