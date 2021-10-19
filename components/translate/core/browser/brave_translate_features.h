2c6a5e8c26bb07599c621b87ed29b296fe545165
Mikhail Atuchin <matuchin@brave.com>
2021-10-19 21:33:53 +0700

    Fix comments

--- components/translate/core/browser/brave_translate_features.h
+++ components/translate/core/browser/brave_translate_features.h
@@ -22,15 +22,15 @@ extern const base::FeatureParam<bool>
 }  // namespace features
 
 // The translate engine can work the folowing ways:
-// 1. IsBraveTranslateGoAvailable() == true: Show a bubble to suggest a user
+// 1. IsTranslateExtensionAvailable() == true: Show a bubble to suggest a user
 // installation of Google translate extension.
-// 2. IsTranslateExtensionAvailable() == true: The internal translation engine
+// 2. IsBraveTranslateGoAvailable() == true: The internal translation engine
 // is used instead of the old bubble.
 // 3. The other: no translation is available.
 bool IsBraveTranslateGoAvailable();
 bool IsTranslateExtensionAvailable();
 
-// True if the translate urls in native code is redirected to
+// True if the translate urls in native code are redirected to
 // translate-relay.brave.com. True by default, use false only for local testing.
 bool UseBraveTranslateRelay();
 
