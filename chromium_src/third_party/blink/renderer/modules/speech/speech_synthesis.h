8a6e84d0491bd8713f483042eb1b9c23c5a0fa1f
Mark Pilgrim <mpilgrim@brave.com>
2021-10-20 12:39:40 -0400

    Add a farbling-determined voice name to getVoices
    call upstream function if farbling is off
    add DEPS
    exit early if no voices
    extend test timeout
    move exit condition earlier
    feedback

---
+++ chromium_src/third_party/blink/renderer/modules/speech/speech_synthesis.h
@@ -0,0 +1,21 @@
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * you can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_MODULES_SPEECH_SPEECH_SYNTHESIS_H_
+#define BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_MODULES_SPEECH_SPEECH_SYNTHESIS_H_
+
+// We need to include this here before redefining OnSetVoiceList to avoid a
+// name collision with a method in the mojom class.
+#include "third_party/blink/public/mojom/speech/speech_synthesis.mojom-blink.h"
+
+#define OnSetVoiceList                                       \
+  OnSetVoiceList_ChromiumImpl(                               \
+      Vector<mojom::blink::SpeechSynthesisVoicePtr> voices); \
+  void OnSetVoiceList
+
+#include "../../../../../../../third_party/blink/renderer/modules/speech/speech_synthesis.h"
+#undef OnSetVoiceList
+
+#endif  // BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_MODULES_SPEECH_SPEECH_SYNTHESIS_H_
