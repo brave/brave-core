f79b8d3189364e8de06f54717d05b15c12369758
Pete Miller <miller.pete@gmail.com>
2021-10-20 10:52:23 -0700

    Settings: Elements which used the now-removed-PrefsBehavior need to use PrefsMixin and be converted to a class
    The prefs/prefs_behavior.js file has moved to ChromeOS-specific files,
    so we need to do this to keep the same functionality as before.
    
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/79004eab4da931e20858e7de531db8c4cb5c82dd
    
    commit 79004eab4da931e20858e7de531db8c4cb5c82dd
    Author: dpapad <dpapad@chromium.org>
    Date:   Fri Oct 1 07:19:22 2021 +0000
    
        Settings: Move prefs_behavior.js to CrOS Settings, only used there.
    
        Browser Settings uses prefs_mixin.js instead. CrOS Settings can't
        leverage Mixins yet, as it is using the legacy Polymer syntax.
    
        Bug: 1234307

--- browser/resources/settings/brave_wallet_page/brave_wallet_page.js
+++ browser/resources/settings/brave_wallet_page/brave_wallet_page.js
@@ -1,88 +1,95 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
-import {Router, RouteObserverBehavior} from '../router.js';
-import {PrefsBehavior} from '../prefs/prefs_behavior.js';
+
 import 'chrome://resources/cr_elements/cr_input/cr_input.m.js';
 import './wallet_networks_subpage.js';
 
-(function() {
-'use strict';
+import {BraveWalletBrowserProxy,  BraveWalletBrowserProxyImpl} from './brave_wallet_browser_proxy.m.js';
+import {I18nMixin} from 'chrome://resources/js/i18n_mixin.js';
+import {PrefsMixin} from '../prefs/prefs_mixin.js';
+import {PolymerElement, html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {Router, RouteObserverMixin} from '../router.js';
+import {WebUIListenerMixin} from 'chrome://resources/js/web_ui_listener_mixin.js';
+
+const SettingsBraveWalletPageBase = WebUIListenerMixin(PrefsMixin(I18nMixin(RouteObserverMixin(PolymerElement))))
 
 /**
  * 'settings-brave-default-extensions-page' is the settings page containing
  * brave's default extensions.
  */
-Polymer({
-  is: 'settings-brave-wallet-page',
+class SettingsBraveWalletPage extends SettingsBraveWalletPageBase {
+  static get is() {
+    return 'settings-brave-wallet-page'
+  }
 
-  behaviors: [
-    WebUIListenerBehavior,
-    PrefsBehavior,
-    I18nBehavior,
-    RouteObserverBehavior
-  ],
+  static get template() {
+    return html`{__html_template__}`
+  }
 
-  properties: {
-    isNativeWalletEnabled_: Boolean,
-    isNetworkEditor_: {
-      type: Boolean,
-      value: false,
+  static get properties() {
+    return {
+      isNativeWalletEnabled_: {
+        type: Boolean
+      },
+
+      isNetworkEditor_: {
+        type: Boolean,
+        value: false,
+      },
     }
-  },
+  }
 
-  /** @private {?settings.BraveWalletBrowserProxy} */
-  browserProxy_: null,
+  browserProxy_ = BraveWalletBrowserProxyImpl.getInstance()
 
-  /** @override */
-  created: function() {
-    this.browserProxy_ = settings.BraveWalletBrowserProxyImpl.getInstance();
-  },
-
-  /** @override */
-  ready: function() {
-    this.onBraveWalletEnabledChange_ = this.onBraveWalletEnabledChange_.bind(this)
-    this.onInputAutoLockMinutes_ = this.onInputAutoLockMinutes_.bind(this)
-    this.onResetWallet_ = this.onResetWallet_.bind(this)
+  ready() {
+    super.ready()
     this.browserProxy_.getWeb3ProviderList().then(list => {
+      // TODO(petemill): provide wallets type
       this.wallets_ = JSON.parse(list)
     });
     this.browserProxy_.isNativeWalletEnabled().then(val => {
-      this.isNativeWalletEnabled_ = val;
+      this.isNativeWalletEnabled_ = val
     });
     this.browserProxy_.getAutoLockMinutes().then(val => {
       this.$.walletAutoLockMinutes.value = val
     })
-  },
+  }
 
-  onBraveWalletEnabledChange_: function() {
+  onBraveWalletEnabledChange_() {
     this.browserProxy_.setBraveWalletEnabled(this.$.braveWalletEnabled.checked);
-  },
-  isNetworkEditorRoute: function () {
+  }
+
+  isNetworkEditorRoute() {
     const router = Router.getInstance();
     return (router.getCurrentRoute() == router.getRoutes().BRAVE_WALLET_NETWORKS);
-  },
+  }
 
   /** @protected */
-  currentRouteChanged: function() {
+  currentRouteChanged() {
     this.isNetworkEditor_ = this.isNetworkEditorRoute()
-  },
-  onInputAutoLockMinutes_: function() {
+  }
+
+  onInputAutoLockMinutes_() {
     let value = Number(this.$.walletAutoLockMinutes.value)
     if (Number.isNaN(value) || value < 1 || value > 10080) {
       return
     }
     this.setPrefValue('brave.wallet.auto_lock_minutes', value)
-  },
-  onWalletNetworksEditorClick_: function() {
+  }
+
+  onWalletNetworksEditorClick_() {
     const router = Router.getInstance();
     router.navigateTo(router.getRoutes().BRAVE_WALLET_NETWORKS);
-  },
-  onResetWallet_: function() {
+  }
+
+  onResetWallet_() {
     var message = this.i18n('walletResetConfirmation')
     if (window.prompt(message) !== this.i18n('walletResetConfirmationPhrase'))
       return
-    this.browserProxy_.resetWallet();
+    this.browserProxy_.resetWallet()
   }
-});
-})();
+}
+
+customElements.define(
+  SettingsBraveWalletPage.is, SettingsBraveWalletPage)
