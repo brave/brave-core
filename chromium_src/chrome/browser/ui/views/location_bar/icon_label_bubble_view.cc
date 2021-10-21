ab9d14435608cb2ea0d6ddd6c9279ed95284fdbf
Pranjal Jumde <pranjal@brave.com>
2021-10-21 14:12:29 -0500

    Fix #18918 - Null deref in BRAVE_ICON_LABEL_BUBBLE_VIEW_GET_HIGHLIGHT_PATH

--- chromium_src/chrome/browser/ui/views/location_bar/icon_label_bubble_view.cc
+++ chromium_src/chrome/browser/ui/views/location_bar/icon_label_bubble_view.cc
@@ -3,12 +3,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#define BRAVE_ICON_LABEL_BUBBLE_VIEW_GET_HIGHLIGHT_PATH                      \
-  if (!ShouldShowSeparator())                                                \
-    highlight_bounds.Inset(0, 0, 1, 0);                                      \
-  auto layout_radius =                                                       \
-      GetLayoutProvider()->GetCornerRadiusMetric(views::Emphasis::kMaximum); \
-  const SkRect new_rect = RectToSkRect(highlight_bounds);                    \
+#define BRAVE_ICON_LABEL_BUBBLE_VIEW_GET_HIGHLIGHT_PATH                    \
+  if (!ShouldShowSeparator())                                              \
+    highlight_bounds.Inset(0, 0, 1, 0);                                    \
+  const auto* layout_provider = GetLayoutProvider();                       \
+  auto layout_radius = corner_radius;                                      \
+  if (layout_provider) {                                                   \
+    layout_radius =                                                        \
+        layout_provider->GetCornerRadiusMetric(views::Emphasis::kMaximum); \
+  }                                                                        \
+  const SkRect new_rect = RectToSkRect(highlight_bounds);                  \
   return SkPath().addRoundRect(new_rect, layout_radius, layout_radius);
 
 #include "../../../../../../../chrome/browser/ui/views/location_bar/icon_label_bubble_view.cc"
