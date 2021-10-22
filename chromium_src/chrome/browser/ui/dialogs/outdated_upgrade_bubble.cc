86f58a3a85e5fde00e82f215ead1b097de7b8f4f
mkarolin <max@brave.com>
2021-10-22 15:41:57 -0400

    OutdatedUpgradeBubbleView moved to chrome/browser/ui/dialogs/
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/9a697f9b7b1b6a51118ae420adb4ee96fa647243
    
    commit 9a697f9b7b1b6a51118ae420adb4ee96fa647243
    Author: Peter Boström <pbos@chromium.org>
    Date:   Fri Oct 15 23:27:56 2021 +0000
    
        Add chrome::ShowBubble() for anchored DialogModels
    
        This migrates OutdatedUpgradeBubbleView to chrome/browser/ui/dialogs/ as
        ShowOutdatedUpgradeBubble(). Follow-up actions should move other dialogs
        (such as window_name_prompt) into this directory.
    
        This also adds chrome/browser/ui/browser_element_identifiers.h to be
        able to refer to the app menu from outside of views code to end up
        anchoring to it. This also replaces BrowserAppMenuButton::kIdentifier with
        kAppMenuElementId for this purpose.
    
        Bug: 1259210

---
+++ chromium_src/chrome/browser/ui/dialogs/outdated_upgrade_bubble.cc
@@ -0,0 +1,8 @@
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/common/url_constants.h"  // for kDownloadBraveUrl
+
+#include "../../../../../../chrome/browser/ui/dialogs/outdated_upgrade_bubble.cc"
