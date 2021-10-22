364549bc9603e80b8e96e3aa0f5a74fd07166990
mkarolin <max@brave.com>
2021-10-22 18:31:46 -0400

    Task-related files moved from base/ to base/task/
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/39810e70da06baca176c0cdbc93164be1f980f59
    
    commit 39810e70da06baca176c0cdbc93164be1f980f59
    Author: Patrick Monette <pmonette@chromium.org>
    Date:   Thu Oct 14 18:06:59 2021 +0000
    
        Reland "Move task-related files from base/ to base/task/"
    
        This is a reland of 092c30c5fd4def5a0c63d6f3a8953bd07768dd44
    
        The 2 problematic subrepos have been migrated and a couple late
        addition of the old headers were migrated.
    
        Original change's description:
        > Move task-related files from base/ to base/task/
        >
        > The forward headers are updated to point to the new location, and the
        > header guards were fixed.
        >
        > Bug: 1255932

--- components/tor/tor_file_watcher.cc
+++ components/tor/tor_file_watcher.cc
@@ -10,10 +10,10 @@
 
 #include "base/files/file.h"
 #include "base/logging.h"
-#include "base/sequenced_task_runner.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/task/post_task.h"
+#include "base/task/sequenced_task_runner.h"
 #include "base/task/thread_pool.h"
 #include "base/time/time.h"
 
