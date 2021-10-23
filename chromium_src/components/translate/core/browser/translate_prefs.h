faf9ce4971ba285713102d1df30dc1517b0f453b
mkarolin <max@brave.com>
2021-10-22 22:24:15 -0400

    Fixes browser_prefs.cc override.
    We are redefining MigrateObsoleteProfilePrefs, but a new call was added
    to browser_prefs.cc that uses the same method name:
    
    translate::TranslatePrefs::MigrateObsoleteProfilePrefs
    
    Added overrides to translate_prefs.h/cc to rename the method the same
    way it gets renamed in browser_prefs.cc
    
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/0c61cb910bb9da119599b9c680655bc16403e88c
    
    commit 0c61cb910bb9da119599b9c680655bc16403e88c
    Author: mlcui <mlcui@google.com>
    Date:   Wed Oct 20 01:56:11 2021 +0000
    
        inclusive: Migrate always translate pref to "translate_allowlists"
    
        This pref name was already used in TranslatePrefs::MapPreferenceName,
        and this CL follows chrome/browser/prefs/README.md to migrate the pref
        over to use "translate_allowlists" in the pref store.
    
        As a result, this removes the usage of less-inclusive language in a few
        JavaScript files.
    
        Fixes: chromium:1233176

--- chromium_src/components/translate/core/browser/translate_prefs.h
+++ chromium_src/components/translate/core/browser/translate_prefs.h
@@ -3,21 +3,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_TRANSLATE_CORE_BROWSER_TRANSLATE_PREFS_H_
+#define BRAVE_CHROMIUM_SRC_COMPONENTS_TRANSLATE_CORE_BROWSER_TRANSLATE_PREFS_H_
+
 // This is done to allow the same renaming in
 // chromium_src/chrome/browser/prefs/browser_prefs.cc
-#include "base/feature_override.h"
-#include "components/translate/core/browser/translate_prefs.h"
-
 #define MigrateObsoleteProfilePrefs MigrateObsoleteProfilePrefs_ChromiumImpl
-#include "../../../../../../components/translate/core/browser/translate_prefs.cc"
+#include "../../../../../../components/translate/core/browser/translate_prefs.h"
 #undef MigrateObsoleteProfilePrefs
 
-namespace translate {
-
-OVERRIDE_FEATURE_DEFAULT_STATES({{
-#if defined(OS_ANDROID)
-    {kTranslate, base::FEATURE_DISABLED_BY_DEFAULT},
-#endif
-}});
-
-}  // namespace translate
+#endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_TRANSLATE_CORE_BROWSER_TRANSLATE_PREFS_H_
