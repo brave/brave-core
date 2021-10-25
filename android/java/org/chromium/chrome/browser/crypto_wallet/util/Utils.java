31cd70d73fe1f35f1cc555a722809db88ebeef56
samartnik <artem@brave.com>
2021-10-25 17:23:55 -0400

    [Android] Lint fixes

--- android/java/org/chromium/chrome/browser/crypto_wallet/util/Utils.java
+++ android/java/org/chromium/chrome/browser/crypto_wallet/util/Utils.java
@@ -31,6 +31,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 import org.chromium.brave_wallet.mojom.BraveWalletConstants;
@@ -570,7 +571,9 @@ public class Utils {
                     } else if (textView != null) {
                         textView.setCompoundDrawablesRelativeWithIntrinsicBounds(
                                 new BitmapDrawable(context.getResources(), bitmap), null,
-                                context.getResources().getDrawable(R.drawable.ic_carat_down), null);
+                                ApiCompatibilityUtils.getDrawable(
+                                        context.getResources(), R.drawable.ic_carat_down),
+                                null);
                     }
                 });
             } catch (IOException exc) {
