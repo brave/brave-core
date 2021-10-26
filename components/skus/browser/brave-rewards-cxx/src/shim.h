b662a3cc07c0c9b2472cba8881f4438267520ff8
Brian Clifton <brian@clifton.me>
2021-10-26 16:11:58 -0700

    Update `shim_scheduleWakeup` to provide `WakeupContext`
    Implementation for work cherry-picked from https://github.com/brave/brave-core/pull/10711

--- components/skus/browser/brave-rewards-cxx/src/shim.h
+++ components/skus/browser/brave-rewards-cxx/src/shim.h
@@ -28,9 +28,11 @@ void shim_purge();
 void shim_set(rust::cxxbridge1::Str key, rust::cxxbridge1::Str value);
 ::rust::String shim_get(rust::cxxbridge1::Str key);
 
-void shim_scheduleWakeup(::std::uint64_t delay_ms,
-      rust::cxxbridge1::Fn<void(rust::cxxbridge1::Box<brave_rewards::WakeupContext>)> done,
-      rust::cxxbridge1::Box<brave_rewards::WakeupContext> ctx);
+void shim_scheduleWakeup(
+    ::std::uint64_t delay_ms,
+    rust::cxxbridge1::Fn<
+        void(rust::cxxbridge1::Box<brave_rewards::WakeupContext>)> done,
+    rust::cxxbridge1::Box<brave_rewards::WakeupContext> ctx);
 
 void shim_executeRequest(
     const brave_rewards::HttpRequest& req,
