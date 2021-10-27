a7d2378a7cf2b779b4a09695211fb430581f497a
Mario Sanchez Prada <mario@igalia.com>
2021-10-27 13:04:29 +0200

    IWYU: base/containers/contains.h

--- chromium_src/chrome/browser/ui/webui/about_ui.cc
+++ chromium_src/chrome/browser/ui/webui/about_ui.cc
@@ -3,6 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "base/containers/contains.h"
 #include "base/strings/string_split.h"
 #include "third_party/re2/src/re2/re2.h"
 
