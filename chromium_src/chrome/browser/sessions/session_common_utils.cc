d7ea6534dcdc8c322f4a6e2034009eb95d66ca1c
Brian R. Bondy <netzen@gmail.com>
2021-11-02 16:32:27 -0400

    Allow session restore for brave://wallet

--- chromium_src/chrome/browser/sessions/session_common_utils.cc
+++ chromium_src/chrome/browser/sessions/session_common_utils.cc
@@ -7,7 +7,6 @@
 
 #if BUILDFLAG(ETHEREUM_REMOTE_CLIENT_ENABLED)
 #include "brave/browser/ethereum_remote_client/ethereum_remote_client_constants.h"
-#include "brave/components/brave_wallet/browser/brave_wallet_constants.h"
 #include "content/public/common/url_constants.h"
 #include "extensions/common/constants.h"
 #include "url/gurl.h"
@@ -21,10 +20,8 @@
 
 bool ShouldTrackURLForRestore(const GURL& url) {
 #if BUILDFLAG(ETHEREUM_REMOTE_CLIENT_ENABLED)
-  if (url.SchemeIs(content::kChromeUIScheme) && url.host() == "wallet") {
-    return false;
-  } else if (url.SchemeIs("chrome-extension") &&
-             url.host() == ethereum_remote_client_extension_id) {
+  if (url.SchemeIs("chrome-extension") &&
+      url.host() == ethereum_remote_client_extension_id) {
     return false;
   }
 #endif
