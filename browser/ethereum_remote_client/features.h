9e7a25353f8d2fdeb32e128d78552d60de4c4ba7
brave-builds <devops@brave.com>
2021-11-04 00:47:00 +0000

    Uplift of #10855 (squashed) to beta

---
+++ browser/ethereum_remote_client/features.h
@@ -0,0 +1,21 @@
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_BROWSER_ETHEREUM_REMOTE_CLIENT_FEATURES_H_
+#define BRAVE_BROWSER_ETHEREUM_REMOTE_CLIENT_FEATURES_H_
+
+namespace base {
+struct Feature;
+}  // namespace base
+
+namespace ethereum_remote_client {
+namespace features {
+
+extern const base::Feature kCryptoWalletsForNewInstallsFeature;
+
+}  // namespace features
+}  // namespace ethereum_remote_client
+
+#endif  // BRAVE_BROWSER_ETHEREUM_REMOTE_CLIENT_FEATURES_H_
