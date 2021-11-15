cb8e5356e73c6220430357a708decb9846b0908d
sergey p <spylogsster@gmail.com>
2021-11-15 15:54:59 +0300

    Fixed crash if transactions notified twice

--- components/brave_wallet/browser/brave_wallet_service.cc
+++ components/brave_wallet/browser/brave_wallet_service.cc
@@ -589,7 +589,8 @@ void BraveWalletService::GetPendingSignMessageRequests(
 
 void BraveWalletService::NotifySignMessageRequestProcessed(bool approved,
                                                            int id) {
-  if (sign_message_requests_.front()->id != id) {
+  if (sign_message_requests_.empty() ||
+      sign_message_requests_.front()->id != id) {
     VLOG(1) << "id: " << id << " is not expected, should be "
             << sign_message_requests_.front()->id;
     return;
@@ -606,7 +607,8 @@ void BraveWalletService::NotifySignMessageHardwareRequestProcessed(
     int id,
     const std::string& signature,
     const std::string& error) {
-  if (sign_message_requests_.front()->id != id) {
+  if (sign_message_requests_.empty() ||
+      sign_message_requests_.front()->id != id) {
     VLOG(1) << "id: " << id << " is not expected, should be "
             << sign_message_requests_.front()->id;
     return;
