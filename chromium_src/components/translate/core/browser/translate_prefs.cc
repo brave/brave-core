8df62f160ea47c0414990a9d82352d67b0ea5c81
mkarolin <max@brave.com>
2021-11-15 16:09:12 -0500

    Lint fix.

--- chromium_src/components/translate/core/browser/translate_prefs.cc
+++ chromium_src/components/translate/core/browser/translate_prefs.cc
@@ -3,9 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "base/feature_override.h"
 // This is done to allow the same renaming in
 // chromium_src/chrome/browser/prefs/browser_prefs.cc
-#include "base/feature_override.h"
 #include "components/translate/core/browser/translate_prefs.h"
 
 #define MigrateObsoleteProfilePrefs MigrateObsoleteProfilePrefs_ChromiumImpl
