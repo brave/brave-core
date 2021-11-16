daae4b50c579c90923c4ff2c5b2ebbbac870928e
Douglas Daniel <douglashdaniel@gmail.com>
2021-11-16 16:53:06 -0700

    Fixed Wallet UI bugs due to Translations

--- components/brave_wallet_ui/components/extension/confirm-transaction-panel/style.ts
+++ components/brave_wallet_ui/components/extension/confirm-transaction-panel/style.ts
@@ -172,6 +172,23 @@ export const SectionRow = styled.div`
   height: inherit;
 `
 
+export const SectionColumn = styled.div`
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  height: inherit;
+`
+
+export const SingleRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+  width: 100%;
+`
+
 export const SectionRightColumn = styled.div`
   display: flex;
   align-items: flex-end;
