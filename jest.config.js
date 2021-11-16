589688bfa6bca4fd438292137a5a5a3f0b7deff9
Pete Miller <miller.pete@gmail.com>
2021-11-16 09:35:12 -0800

    Jest includes most possible build output dir paths
    This is a short term fix for CI builds on various architectures. The better fix is to get this parameter from the configuration

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,18 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 
+const buildConfigs = [ 'Component', 'Static', 'Debug', 'Release' ]
+const extraArchitectures = [ 'arm64', 'x86' ]
+
+function getBuildOuptutPathList (buildOutputRelativePath) {
+  return buildConfigs.flatMap(config => [
+    `<rootDir>/../out/${config}/${buildOutputRelativePath}`,
+    ...extraArchitectures.map(arch =>
+      `<rootDir>/../out/${config}_${arch}/${buildOutputRelativePath}`,
+    )
+  ])
+}
+
 module.exports = {
   preset: "ts-jest/presets/default",
   testEnvironment: "<rootDir>/components/test/testEnvironment.js",
@@ -57,24 +69,14 @@ module.exports = {
     "\\.(css|less)$": "identity-obj-proxy",
     "^brave-ui$": "<rootDir>/node_modules/brave-ui/src",
     "^brave-ui\\/(.*)": "<rootDir>/node_modules/brave-ui/src/$1",
-    // "^chrome:\/\/resources\\/(.*)": "<rootDir>/.storybook/chrome-resources-mock/$1",
-    // "^gen\\/(.*)": "<rootDir>/.storybook/gen-mock/$1",
-    "^gen\\/(.*)": [
-      // TODO(petemill): The ordering here can get problematic for devs
-      // who have more than 1 build type at a time, since if the file exists
-      // at the first path, it will be used for Type analysis instead of the second
-      // path, even if it's more recent. We should investigate a test-time
-      // command / environment variable which would use the correct path here.
-      "<rootDir>/../out/Component/gen/$1",
-      "<rootDir>/../out/Static/gen/$1",
-      "<rootDir>/../out/Release/gen/$1",
-      "<rootDir>/../out/Debug/gen/$1",
-    ],
-    "chrome:\/\/resources\\/(.*)": [
-      "<rootDir>/../out/Component/gen/ui/webui/resources/preprocessed/$1",
-      "<rootDir>/../out/Static/gen/ui/webui/resources/preprocessed/$1",
-      "<rootDir>/../out/Release/gen/ui/webui/resources/preprocessed/$1",
-      "<rootDir>/../out/Debug/gen/ui/webui/resources/preprocessed/$1",
-    ]
+    // TODO(petemill): The ordering here can get problematic for devs
+    // who have more than 1 build type at a time, since if the file exists
+    // at the first path, it will be used for Type analysis instead of the second
+    // path, even if it's more recent.
+    // It can also break if CI or devs perform a build in a directory not known
+    // by this list.
+    // Instead, we should get the directory from config.js:outputDir.
+    "^gen\\/(.*)": getBuildOuptutPathList('gen/$1'),
+    "chrome:\/\/resources\\/(.*)": getBuildOuptutPathList('gen/ui/webui/resources/preprocessed/$1')
   }
 }
