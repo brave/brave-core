c5deb620ac4679ba9761a339a45682df4c667383
eV <8796196+evq@users.noreply.github.com>
2021-11-17 17:25:02 +0000

    move sku sdk rust code into tree (#11068)

---
+++ components/skus/browser/rust/lib/src/sdk/credentials/fetch.rs
@@ -0,0 +1,254 @@
+use core::iter;
+use std::collections::HashMap;
+
+use challenge_bypass_ristretto::voprf::*;
+
+use chrono::NaiveDate;
+use futures_retry::FutureRetry;
+use rand::rngs::OsRng;
+use serde::{Deserialize, Serialize};
+use sha2::Sha512;
+use tracing::instrument;
+
+use crate::errors::{InternalError, RewardsError};
+use crate::http::HttpHandler;
+use crate::models::*;
+use crate::sdk::SDK;
+use crate::{HTTPClient, StorageClient};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct ItemCredentialsRequest {
+    item_id: String,
+    blinded_creds: Vec<BlindedToken>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+enum ItemCredentialsResponse {
+    #[serde(rename_all = "camelCase")]
+    SingleUse {
+        id: String,
+        order_id: String,
+        blinded_creds: Vec<BlindedToken>,
+        signed_creds: Vec<SignedToken>,
+        batch_proof: BatchDLEQProof,
+        public_key: PublicKey,
+        issuer_id: String,
+    },
+    // FIXME should we just use full date format over wire?
+    #[serde(rename_all = "camelCase")]
+    TimeLimited {
+        expires_at: String,
+        id: String,
+        issued_at: String,
+        order_id: String,
+        token: String,
+    },
+}
+
+impl<U> SDK<U>
+where
+    U: HTTPClient + StorageClient,
+{
+    #[instrument]
+    pub async fn submit_order_credentials_to_sign(
+        &self,
+        order_id: &str,
+    ) -> Result<(), RewardsError> {
+        let order = match self.client.get_order(order_id).await {
+            Ok(Some(order)) => order,
+            _ => self.refresh_order(order_id).await?,
+        };
+
+        if !order.is_paid() {
+            return Err(InternalError::OrderUnpaid.into());
+        }
+
+        let mut csprng = OsRng;
+
+        for item in order.items {
+            match item.credential_type {
+                CredentialType::SingleUse => {
+                    let blinded_creds: Vec<BlindedToken> =
+                        match self.client.get_single_use_item_creds(&item.id).await? {
+                            Some(item_creds) => {
+                                item_creds.creds.iter().map(|t| t.blind()).collect()
+                            }
+                            None => {
+                                let creds: Vec<Token> =
+                                    iter::repeat_with(|| Token::random::<Sha512, _>(&mut csprng))
+                                        .take(item.quantity as usize)
+                                        .collect();
+
+                                let blinded_creds: Vec<BlindedToken> =
+                                    creds.iter().map(|t| t.blind()).collect();
+
+                                self.client
+                                    .init_single_use_item_creds(&item.id, creds)
+                                    .await?;
+                                blinded_creds
+                            }
+                        };
+
+                    let claim_req = ItemCredentialsRequest {
+                        item_id: item.id,
+                        blinded_creds,
+                    };
+
+                    let request_with_retries = FutureRetry::new(
+                        || async {
+                            let body = serde_json::to_vec(&claim_req)
+                                .or(Err(InternalError::SerializationFailed))?;
+
+                            let req = http::Request::builder()
+                                .method("POST")
+                                .uri(format!(
+                                    "{}/v1/orders/{}/credentials",
+                                    self.base_url, order_id
+                                ))
+                                .body(body)
+                                .unwrap();
+                            let resp = self.fetch(req).await?;
+
+                            match resp.status() {
+                                http::StatusCode::OK => Ok(()),
+                                http::StatusCode::CONFLICT => {
+                                    Err(InternalError::BadRequest(http::StatusCode::CONFLICT))
+                                }
+                                http::StatusCode::NOT_FOUND => Err(InternalError::NotFound),
+                                _ => Err(resp.into()),
+                            }
+                        },
+                        HttpHandler::new(3, "Sign order item credentials request", &self.client),
+                    );
+                    request_with_retries.await?;
+                }
+                CredentialType::TimeLimited => (), // Time limited credentials do not require a submission step
+            }
+        }
+        Ok(())
+    }
+
+    #[instrument]
+    pub async fn refresh_order_credentials(&self, order_id: &str) -> Result<(), RewardsError> {
+        let order = self.fetch_order(order_id).await?;
+        if let Some(local_order) = self.client.get_order(order_id).await? {
+            if order.last_paid_at != local_order.last_paid_at {
+                self.fetch_order_credentials(order_id).await?;
+                // store the latest retrieved order information after we've successfully fetched
+                self.client.upsert_order(&order).await?;
+            }
+            Ok(())
+        } else {
+            Err(InternalError::NotFound.into())
+        }
+    }
+
+    #[instrument]
+    pub async fn fetch_order_credentials(&self, order_id: &str) -> Result<(), RewardsError> {
+        self.submit_order_credentials_to_sign(order_id).await?;
+
+        let request_with_retries = FutureRetry::new(
+            || async move {
+                let mut builder = http::Request::builder();
+                builder.method("GET");
+                builder.uri(format!(
+                    "{}/v1/orders/{}/credentials",
+                    self.base_url, order_id
+                ));
+
+                let req = builder.body(vec![]).unwrap();
+
+                let resp = self.fetch(req).await?;
+
+                match resp.status() {
+                    http::StatusCode::OK => Ok(resp),
+                    http::StatusCode::ACCEPTED => Err(InternalError::RetryLater(None)),
+                    http::StatusCode::NOT_FOUND => Err(InternalError::NotFound),
+                    _ => Err(resp.into()),
+                }
+            },
+            HttpHandler::new(3, "Fetch order credentials request", &self.client),
+        );
+
+        let (resp, _) = request_with_retries.await?;
+
+        let resp: Vec<ItemCredentialsResponse> = serde_json::from_slice(resp.body())?;
+
+        let mut time_limited_creds: HashMap<String, Vec<TimeLimitedCredential>> = HashMap::new();
+
+        for item_cred in resp {
+            match item_cred {
+                ItemCredentialsResponse::SingleUse {
+                    id: item_id,
+                    signed_creds,
+                    batch_proof,
+                    public_key,
+                    issuer_id,
+                    ..
+                } => {
+                    if let Some(item_creds) =
+                        self.client.get_single_use_item_creds(&item_id).await?
+                    {
+                        // Rederive blinded creds so that the proof will fail if different creds were signed
+                        let blinded_creds: Vec<BlindedToken> =
+                            item_creds.creds.iter().map(|t| t.blind()).collect();
+
+                        let unblinded_creds = batch_proof
+                            .verify_and_unblind::<Sha512, _>(
+                                &item_creds.creds,
+                                &blinded_creds,
+                                &signed_creds,
+                                &public_key,
+                            )
+                            .or(Err(InternalError::InvalidProof))?;
+
+                        self.client
+                            .complete_single_use_item_creds(&item_id, &issuer_id, unblinded_creds)
+                            .await?;
+                    }
+                }
+                ItemCredentialsResponse::TimeLimited {
+                    id: item_id,
+                    order_id: _,
+                    issued_at,
+                    expires_at,
+                    token,
+                } => {
+                    let cred = TimeLimitedCredential {
+                        item_id: item_id.clone(),
+                        issued_at: NaiveDate::parse_from_str(&issued_at, "%Y-%m-%d")
+                            .map_err(|_| {
+                                InternalError::InvalidResponse(
+                                    "Could not parse issued at".to_string(),
+                                )
+                            })?
+                            .and_hms(0, 0, 0),
+                        expires_at: NaiveDate::parse_from_str(&expires_at, "%Y-%m-%d")
+                            .map_err(|_| {
+                                InternalError::InvalidResponse(
+                                    "Could not parse expires at".to_string(),
+                                )
+                            })?
+                            .and_hms(0, 0, 0),
+                        token,
+                    };
+                    if let Some(item_creds) = time_limited_creds.get_mut(&item_id) {
+                        item_creds.push(cred);
+                    } else {
+                        time_limited_creds.insert(item_id, vec![cred]);
+                    }
+                }
+            }
+        }
+
+        for (item_id, item_creds) in time_limited_creds.into_iter() {
+            self.client
+                .store_time_limited_creds(&item_id, item_creds)
+                .await?;
+        }
+
+        Ok(())
+    }
+}
---
+++ components/skus/browser/rust/lib/src/sdk/orders.rs
@@ -0,0 +1,214 @@
+use core::convert::{TryFrom, TryInto};
+
+use chrono::{DateTime, Utc};
+use futures_retry::FutureRetry;
+use serde::{Deserialize, Serialize};
+use tracing::instrument;
+
+#[cfg(feature = "e2e_test")]
+use serde_json::{json, to_vec};
+
+use crate::errors::{InternalError, RewardsError};
+use crate::http::HttpHandler;
+use crate::models::*;
+use crate::sdk::SDK;
+use crate::{HTTPClient, StorageClient};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct OrderMetadataResponse {
+    stripe_checkout_session_id: Option<String>,
+}
+
+impl From<OrderMetadataResponse> for OrderMetadata {
+    fn from(order_metadata: OrderMetadataResponse) -> Self {
+        OrderMetadata {
+            stripe_checkout_session_id: order_metadata.stripe_checkout_session_id,
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct OrderItemResponse {
+    id: String,
+    order_id: String,
+    sku: String,
+    created_at: DateTime<Utc>,
+    updated_at: DateTime<Utc>,
+    currency: String,
+    quantity: i32,
+    price: String,
+    subtotal: String,
+    location: String,
+    description: String,
+    credential_type: CredentialType,
+    // TODO add credential_version
+    // TODO add accepted payment types
+}
+
+impl TryFrom<OrderItemResponse> for OrderItem {
+    type Error = RewardsError;
+
+    fn try_from(order_item: OrderItemResponse) -> Result<Self, Self::Error> {
+        let price = order_item.price.parse::<f64>().map_err(|_| {
+            InternalError::InvalidResponse("Could not parse unit price".to_string())
+        })?;
+        let subtotal = order_item
+            .subtotal
+            .parse::<f64>()
+            .map_err(|_| InternalError::InvalidResponse("Could not parse subtotal".to_string()))?;
+        Ok(OrderItem {
+            id: order_item.id,
+            order_id: order_item.order_id,
+            sku: order_item.sku,
+            created_at: order_item.created_at.naive_utc(),
+            updated_at: order_item.updated_at.naive_utc(),
+            currency: order_item.currency,
+            quantity: order_item.quantity,
+            price,
+            subtotal,
+            location: order_item.location,
+            description: order_item.description,
+            credential_type: order_item.credential_type,
+            //valid_for: order_item.valid_for,
+        })
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct OrderResponse {
+    id: String,
+    created_at: DateTime<Utc>,
+    currency: String,
+    updated_at: DateTime<Utc>,
+    total_price: String,
+    location: String,
+    merchant_id: String,
+    status: String,
+    items: Vec<OrderItemResponse>,
+    metadata: Option<OrderMetadataResponse>,
+    expires_at: Option<DateTime<Utc>>,
+    last_paid_at: Option<DateTime<Utc>>,
+}
+
+impl TryFrom<OrderResponse> for Order {
+    type Error = RewardsError;
+
+    fn try_from(order: OrderResponse) -> Result<Self, Self::Error> {
+        let total_price = order.total_price.parse::<f64>().map_err(|_| {
+            InternalError::InvalidResponse("Could not parse total price".to_string())
+        })?;
+        let items: Result<Vec<OrderItem>, _> = order
+            .items
+            .into_iter()
+            .map(|item| item.try_into())
+            .collect();
+        Ok(Order {
+            id: order.id,
+            created_at: order.created_at.naive_utc(),
+            currency: order.currency,
+            updated_at: order.updated_at.naive_utc(),
+            total_price,
+            location: order.location,
+            merchant_id: order.merchant_id,
+            status: order.status,
+            items: items?,
+            metadata: order.metadata.map(|m| m.into()),
+            expires_at: order.expires_at.map(|ex| ex.naive_utc()),
+            last_paid_at: order.last_paid_at.map(|lp| lp.naive_utc()),
+            //valid_for: order.valid_for,
+        })
+    }
+}
+
+impl<U> SDK<U>
+where
+    U: HTTPClient + StorageClient,
+{
+    #[cfg(feature = "e2e_test")]
+    pub async fn create_order(&self, kind: &str) -> Result<Order, RewardsError> {
+        let sku = match kind {
+            "trial" => "AgEVc2VhcmNoLmJyYXZlLnNvZnR3YXJlAh9zZWFyY2ggY2xvc2VkIGJldGEgcHJvZ3JhbSBkZW1vAAIWc2t1PXNlYXJjaC1iZXRhLWFjY2VzcwACB3ByaWNlPTAAAgxjdXJyZW5jeT1CQVQAAi1kZXNjcmlwdGlvbj1TZWFyY2ggY2xvc2VkIGJldGEgcHJvZ3JhbSBhY2Nlc3MAAhpjcmVkZW50aWFsX3R5cGU9c2luZ2xlLXVzZQAABiB3uXfAAkNSRQd24jSauRny3VM0BYZ8yOclPTEgPa0xrA==",
+            "paid" => "MDAyOWxvY2F0aW9uIHRvZ2V0aGVyLmJzZy5icmF2ZS5zb2Z0d2FyZQowMDMwaWRlbnRpZmllciBicmF2ZS10b2dldGhlci1wYWlkIHNrdSB0b2tlbiB2MQowMDIwY2lkIHNrdT1icmF2ZS10b2dldGhlci1wYWlkCjAwMTBjaWQgcHJpY2U9NQowMDE1Y2lkIGN1cnJlbmN5PVVTRAowMDQzY2lkIGRlc2NyaXB0aW9uPU9uZSBtb250aCBwYWlkIHN1YnNjcmlwdGlvbiBmb3IgQnJhdmUgVG9nZXRoZXIKMDAyNWNpZCBjcmVkZW50aWFsX3R5cGU9dGltZS1saW1pdGVkCjAwMjZjaWQgY3JlZGVudGlhbF92YWxpZF9kdXJhdGlvbj1QMU0KMDAyZnNpZ25hdHVyZSDKLJ7NuuzP3KdmTdVnn0dI3JmIfNblQKmY+WBJOqnQJAo=",
+            "beta" => "AgEVc2VhcmNoLmJyYXZlLnNvZnR3YXJlAh9zZWFyY2ggY2xvc2VkIGJldGEgcHJvZ3JhbSBkZW1vAAIWc2t1PXNlYXJjaC1iZXRhLWFjY2VzcwACB3ByaWNlPTAAAgxjdXJyZW5jeT1CQVQAAi1kZXNjcmlwdGlvbj1TZWFyY2ggY2xvc2VkIGJldGEgcHJvZ3JhbSBhY2Nlc3MAAhpjcmVkZW50aWFsX3R5cGU9c2luZ2xlLXVzZQAABiB3uXfAAkNSRQd24jSauRny3VM0BYZ8yOclPTEgPa0xrA==",
+            _ => "",
+        };
+
+        let quantity = match kind {
+            "beta" => 10,
+            _ => 1,
+        };
+
+        let order = json!({
+            "items": [
+                {
+                    "sku": sku,
+                    "quantity": quantity
+                }
+           ]
+        });
+
+        let request_with_retries = FutureRetry::new(
+            || async {
+                let mut builder = http::Request::builder();
+                builder.method("POST");
+                builder.uri(format!("{}/v1/orders", self.base_url));
+
+                let body = to_vec(&order).or(Err(InternalError::SerializationFailed))?;
+
+                let req = builder.body(body).unwrap();
+                let resp = self.fetch(req).await?;
+
+                match resp.status() {
+                    http::StatusCode::CREATED => Ok(resp),
+                    _ => Err(resp.into()),
+                }
+            },
+            HttpHandler::new(3, "Create order request", &self.client),
+        );
+        let (resp, _) = request_with_retries.await?;
+
+        let order: OrderResponse = serde_json::from_slice(resp.body())?;
+        let order: Order = order.try_into()?;
+
+        self.client.upsert_order(&order).await?;
+
+        Ok(order)
+    }
+
+    #[instrument]
+    pub async fn fetch_order(&self, order_id: &str) -> Result<Order, RewardsError> {
+        let request_with_retries = FutureRetry::new(
+            || async {
+                let mut builder = http::Request::builder();
+                builder.method("GET");
+                builder.uri(format!("{}/v1/orders/{}", self.base_url, order_id));
+
+                let req = builder.body(vec![]).unwrap();
+                let resp = self.fetch(req).await?;
+
+                match resp.status() {
+                    http::StatusCode::OK => Ok(resp),
+                    http::StatusCode::NOT_FOUND => Err(InternalError::NotFound),
+                    _ => Err(resp.into()),
+                }
+            },
+            HttpHandler::new(3, "Fetch order request", &self.client),
+        );
+        let (resp, _) = request_with_retries.await?;
+
+        let order: OrderResponse = serde_json::from_slice(resp.body())?;
+        let order: Order = order.try_into()?;
+
+        Ok(order)
+    }
+
+    #[instrument]
+    pub async fn refresh_order(&self, order_id: &str) -> Result<Order, RewardsError> {
+        let order = self.fetch_order(order_id).await?;
+        self.client.upsert_order(&order).await?;
+        Ok(order)
+    }
+}
