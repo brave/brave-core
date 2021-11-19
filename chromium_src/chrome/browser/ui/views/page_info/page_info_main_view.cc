db49d234105db919bd98a58bb54d735d720589c6
Sergey P <spylogsster@gmail.com>
2021-11-19 15:37:43 +0300

    Updated logo for item

--- chromium_src/chrome/browser/ui/views/page_info/page_info_main_view.cc
+++ chromium_src/chrome/browser/ui/views/page_info/page_info_main_view.cc
@@ -4,9 +4,32 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/components/ipfs/ipfs_utils.h"
+#include "brave/grit/brave_theme_resources.h"
+#include "chrome/browser/ui/views/page_info/page_info_view_factory.h"
+#include "components/page_info/page_info.h"
+#include "ui/base/models/image_model.h"
+#include "ui/base/resource/resource_bundle.h"
 
+namespace {
+
+bool IsIPFSPage(PageInfo* presenter) {
+  return presenter && ipfs::IsIPFSScheme(presenter->site_url());
+}
+
+const ui::ImageModel GetIpfsGetConnectionSecureIcon() {
+  auto& bundle = ui::ResourceBundle::GetSharedInstance();
+  const auto& ipfs_logo = *bundle.GetImageSkiaNamed(IDR_BRAVE_IPFS_LOGO);
+  return ui::ImageModel::FromImageSkia(ipfs_logo);
+}
+
+}  // namespace
+
+#define GetConnectionSecureIcon                                           \
+  GetConnectionSecureIcon() != ui::ImageModel() && IsIPFSPage(presenter_) \
+      ? GetIpfsGetConnectionSecureIcon()                                  \
+      : PageInfoViewFactory::GetConnectionSecureIcon
 #define BRAVE_PAGE_INFO_MAIN_VIEW_CALCULATE_PREFERRED_SIZE                     \
-  if (presenter_ && ipfs::IsIPFSScheme(presenter_->site_url())) {              \
+  if (IsIPFSPage(presenter_)) {                                                \
     width =                                                                    \
         std::max(security_container_view_->GetPreferredSize().width(), width); \
   }
@@ -14,3 +37,4 @@
 #include "../../../../../../../chrome/browser/ui/views/page_info/page_info_main_view.cc"
 
 #undef BRAVE_PAGE_INFO_MAIN_VIEW_CALCULATE_PREFERRED_SIZE
+#undef GetConnectionSecureIcon
