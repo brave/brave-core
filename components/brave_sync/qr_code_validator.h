55a973c61997c7ba4c36e2a90cd608400018b5f7
AlexeyBarabash <alexey@brave.com>
2021-11-23 15:46:52 +0200

    class QrCodeDataValidator and the tests

--- components/brave_sync/qr_code_validator.h
+++ components/brave_sync/qr_code_validator.h
@@ -8,22 +8,29 @@
 
 #include <string>
 
+#include "base/time/time.h"
+
 namespace brave_sync {
 
 enum class QrCodeDataValidationResult {
   kValid = 0,
-  kInvalidUnknowReason = 1,
-  kNotWellFormed = 2,
-  kVersionNotRecognized = 3,
-  kVersionDeprecated = 4,
-  kExpired = 5,
-  kValidForTooLong = 6,
+  kNotWellFormed = 1,
+  kVersionNotRecognized = 2,
+  kVersionDeprecated = 3,
+  kExpired = 4,
+  kValidForTooLong = 5,
 };
 
 class QrCodeDataValidator {
  public:
-  static QrCodeDataValidationResult ValidateQrData(
+  static QrCodeDataValidationResult ValidateQrDataJson(
       const std::string& qr_data_string);
+
+  static base::Time GetQRv1SunsetDay();
+
+ private:
+  static bool IsValidSeedHex(const std::string& seed_hex);
+  static base::Time qr_v1_sunset_day_;
 };
 
 }  // namespace brave_sync
