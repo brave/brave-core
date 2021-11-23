c0caab59b985ae2c32b5597b466bcacf4387c5f7
Mikhail Atuchin <matuchin@brave.com>
2021-11-23 18:48:55 +0700

    Reformat repack-archive.py

--- script/repack-archive.py
+++ script/repack-archive.py
@@ -8,33 +8,33 @@ from lib.util import scoped_cwd, make_zip, tempdir
 
 
 def GetLZMAExec(build_dir):
-  if sys.platform == 'win32':
-    lzma_exec = os.path.join(build_dir, "..", "..", "third_party",
-                             "lzma_sdk", "Executable", "7za.exe")
-  else:
-    lzma_exec = '7zr'  # Use system 7zr.
-  return lzma_exec
+    if sys.platform == 'win32':
+        lzma_exec = os.path.join(build_dir, "..", "..", "third_party",
+                                 "lzma_sdk", "Executable", "7za.exe")
+    else:
+        lzma_exec = '7zr'  # Use system 7zr.
+    return lzma_exec
 
 
 def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--output', help='Path to output archive.')
-  parser.add_argument('--input', help='Path to input archive.')
-  parser.add_argument('--build_dir',
-                      help=('Full path to build directory',
-                            'i.e. <something>/out/Release)'))
-  parser.add_argument('--target_dir',
-                      help='If provided, the paths in the archive will be '
-                      'relative to this directory', default='.')
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output', help='Path to output archive.')
+    parser.add_argument('--input', help='Path to input archive.')
+    parser.add_argument('--build_dir',
+                        help=('Full path to build directory',
+                              'i.e. <something>/out/Release)'))
+    parser.add_argument('--target_dir',
+                        help='If provided, the paths in the archive will be '
+                        'relative to this directory', default='.')
+    args = parser.parse_args()
 
-  temp_dir = tempdir('brave_archive')
-  lzma_exec = GetLZMAExec(args.build_dir)
-  cmd = [lzma_exec, 'x', args.input, '-y', '-o' + temp_dir]
-  subprocess.check_call(cmd, stdout=subprocess.PIPE, shell=False)
-  with scoped_cwd(os.path.join(temp_dir, args.target_dir)):
-    make_zip(args.output, [], ['.'])
+    temp_dir = tempdir('brave_archive')
+    lzma_exec = GetLZMAExec(args.build_dir)
+    cmd = [lzma_exec, 'x', args.input, '-y', '-o' + temp_dir]
+    subprocess.check_call(cmd, stdout=subprocess.PIPE, shell=False)
+    with scoped_cwd(os.path.join(temp_dir, args.target_dir)):
+        make_zip(args.output, [], ['.'])
 
 
 if __name__ == '__main__':
-  sys.exit(main())
+    sys.exit(main())
