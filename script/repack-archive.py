dcb214b3b15fa7949509e6e0898da73557f39fb0
Mikhail Atuchin <matuchin@brave.com>
2021-11-24 14:59:48 +0700

    Remove root_out_dir

--- script/repack-archive.py
+++ script/repack-archive.py
@@ -7,7 +7,7 @@ import subprocess
 from lib.util import scoped_cwd, make_zip, tempdir
 
 
-def GetLZMAExec(build_dir):
+def GetLZMAExec():
     if sys.platform == 'win32':
         root_src_dir = os.path.abspath(os.path.join(
             os.path.dirname(__file__), *[os.pardir] * 2))
@@ -22,16 +22,13 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--output', help='Path to output archive.')
     parser.add_argument('--input', help='Path to input archive.')
-    parser.add_argument('--build_dir',
-                        help=('Full path to build directory',
-                              'i.e. <something>/out/Release)'))
     parser.add_argument('--target_dir',
                         help='If provided, the paths in the archive will be '
                         'relative to this directory', default='.')
     args = parser.parse_args()
 
     temp_dir = tempdir('brave_archive')
-    lzma_exec = GetLZMAExec(args.build_dir)
+    lzma_exec = GetLZMAExec()
     cmd = [lzma_exec, 'x', args.input, '-y', '-o' + temp_dir]
     subprocess.check_call(cmd, stdout=subprocess.PIPE, shell=False)
     with scoped_cwd(os.path.join(temp_dir, args.target_dir)):
