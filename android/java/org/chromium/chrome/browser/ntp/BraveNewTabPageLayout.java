c102342c38f2f944a44c83c736619131ca48fb72
samartnik <artem@brave.com>
2021-11-30 18:23:20 -0500

    [Android] Adjustments to UI changes in the upstream
    Chromium change:
    https://chromium-review.googlesource.com/c/chromium/src/+/3283625
    
    Clean up legacy non-ThemeRefactorAndroid code now that the feature has
    launched.
    
    Bug: 1251194

--- android/java/org/chromium/chrome/browser/ntp/BraveNewTabPageLayout.java
+++ android/java/org/chromium/chrome/browser/ntp/BraveNewTabPageLayout.java
@@ -1345,8 +1345,8 @@ public class BraveNewTabPageLayout
                     GlobalNightModeStateProviderHolder.getInstance().isInNightMode()
                     ? android.R.color.white
                     : android.R.color.black;
-            ImageViewCompat.setImageTintList(
-                    mSuperReferralLogo, ColorStateList.valueOf(floatingButtonIconColor));
+            ImageViewCompat.setImageTintList(mSuperReferralLogo,
+                    ColorStateList.valueOf(getResources().getColor(floatingButtonIconColor)));
             mSuperReferralLogo.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
