e2761d6cf51037b8ea670ca958565dc075572531
mkarolin <max@brave.com>
2021-11-30 17:21:34 -0500

    heap.h -> garbage_collected.h
    Chromium changes:
    
    https://chromium.googlesource.com/chromium/src/+/4cfe86482b000e848009077783ba35f83f3c3cfe
    
    commit 4cfe86482b000e848009077783ba35f83f3c3cfe
    Author: Michael Lippautz <mlippautz@chromium.org>
    Date:   Tue Nov 23 09:58:42 2021 +0000
    
        heap: Replace heap.h include with garbage_collected.h in **/modules/**
    
        `heap.h` was only referring to `garbage_collected.h`. Blink code
        should use `garbage_collected.h` to declare a GCed class. This allows
        the header to be reused in future for actual heap internals.
    
        This CL does not generally address useless includes other than
        running `git cl format`.
    
        Bug: chromium:1269227
    
    https://chromium.googlesource.com/chromium/src/+/f97e904fbce401da83a9f1c4c73f1ffe83c83b1b
    
    commit f97e904fbce401da83a9f1c4c73f1ffe83c83b1b
    Author: Michael Lippautz <mlippautz@chromium.org>
    Date:   Tue Nov 23 10:37:36 2021 +0000
    
        heap: Replace platform/heap/heap.h with garbage_collected.h
    
        `heap.h` was only referring to `garbage_collected.h`. Blink code should use
        `garbage_collected.h` to declare a GCed class. This allows the header to
        be reused in future for actual heap internals.
    
        Bug: chromium:1269227

--- chromium_src/third_party/blink/renderer/modules/plugins/dom_plugin_array.cc
+++ chromium_src/third_party/blink/renderer/modules/plugins/dom_plugin_array.cc
@@ -12,7 +12,7 @@
 #include "third_party/blink/renderer/core/page/plugin_data.h"
 #include "third_party/blink/renderer/modules/plugins/dom_plugin.h"
 #include "third_party/blink/renderer/modules/plugins/dom_plugin_array.h"
-#include "third_party/blink/renderer/platform/heap/heap.h"
+#include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
