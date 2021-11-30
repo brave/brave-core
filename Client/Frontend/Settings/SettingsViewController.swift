6162ff0e03038dc20fb799f815847fc261175e23
Michał Buczek <michal.buczek@protonmail.com>
2021-11-30 21:07:45 +0100

    No Bug: Hide saved logins until we are ready to deliver it to users.

--- Client/Frontend/Settings/SettingsViewController.swift
+++ Client/Frontend/Settings/SettingsViewController.swift
@@ -431,7 +431,9 @@ class SettingsViewController: TableViewController {
         return Section(
             header: .title(Strings.security),
             rows: [
-                .boolRow(title: Strings.browserLock, detailText: Strings.browserLockDescription, option: Preferences.Privacy.lockWithPasscode, image: #imageLiteral(resourceName: "settings-passcode").template),
+                .boolRow(title: Strings.browserLock, detailText: Strings.browserLockDescription, option: Preferences.Privacy.lockWithPasscode, image: #imageLiteral(resourceName: "settings-passcode").template)
+                // TODO: Uncomment once we restore saved logins, see #4583
+                /*,
                 Row(text: Strings.Login.loginListNavigationTitle, selection: { [unowned self] in
                     let loginsPasswordsViewController = LoginListViewController(
                         profile: self.profile,
@@ -439,6 +441,7 @@ class SettingsViewController: TableViewController {
                     loginsPasswordsViewController.settingsDelegate = self.settingsDelegate
                     self.navigationController?.pushViewController(loginsPasswordsViewController, animated: true)
                 }, image: #imageLiteral(resourceName: "settings-save-logins").template, accessory: .disclosureIndicator)
+                */
             ]
         )
     }()
