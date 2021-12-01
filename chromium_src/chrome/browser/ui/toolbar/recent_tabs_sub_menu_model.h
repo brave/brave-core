b27cd8bf5063b3bdd6638f5a3ba5a35fede1eee7
mkarolin <max@brave.com>
2021-11-30 19:11:41 -0500

    Replace auto_ptr with unique_ptr.
    Upstream is testing C++17 which removes std::auto_ptr. While they will
    likely revert this back to C++14 there's no reason for us to keep using
    std::auto_ptr.
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/dd0a664ba1f7dcec51b16c48625f9b63e88eaa66
    
    commit dd0a664ba1f7dcec51b16c48625f9b63e88eaa66
    Author: Nico Weber <thakis@chromium.org>
    Date:   Tue Nov 30 13:23:56 2021 +0000
    
        Reland^2: Build with -std=c++17 on some platforms
    
        This is a reland of 41972c9047186a913d95378eb96879514adaafa1
        The reland changes WEB_CONTENTS_USER_DATA_KEY_DECL()
        to `static const int kUserDataKey = 0`. In C++17, class-level
        constexpr statics are implicitly inline variables (see bug),
        which means that they don't have unique addresses across shared
        libraries. WEB_CONTENTS_USER_DATA_KEY_DECL requires that, so
        just make the variable `const` instead of `constexpr` to make things
        work with C++17.
    
        Original change's description:
        > Reland: Build with -std=c++17 on some platforms
        >
        > In the reland, we keep using -std=c++14 for CFI builds for a short
        > time, see crbug.com/1273966.
        >
        > This is to find unknown things that break with -std=c++17.
        >
        > Do NOT use C++17 features anywhere yet. We'll likely have to revert
        > this again in a bit.
        >
        > This does NOT enable C++17 on iOS and macOS -- there are
        > a few (likely small) kinks we have to work out first on those
        > platforms.
        >
        > It does enable C++17 on Android, Cast, Chrome OS, Fuchsia,
        > (non-CFI) Linux.
        > C++17 was already enabled on Windows in an earlier change.
        >
        > This relands https://chromium-review.googlesource.com/c/chromium/src/+/3297210/
        > (but the change is much smaller now since much of that change landed
        > when we turned on C++17 on Windows, in
        > https://chromium-review.googlesource.com/c/chromium/src/+/3301748 )
        >
        > Bug: 752720,1273966
        > Cq-Include-Trybots: luci.chromium.try:linux_chromium_cfi_rel_ng
        > Change-Id: I4e4979f0af2052803154fb154edbe9797da5aa40
        > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3304484
        > Auto-Submit: Nico Weber <thakis@chromium.org>
        > Commit-Queue: Hans Wennborg <hans@chromium.org>
        > Reviewed-by: Hans Wennborg <hans@chromium.org>
        > Cr-Commit-Position: refs/heads/main@{#945796}
    
        Bug: 752720,1273966,1274431

--- chromium_src/chrome/browser/ui/toolbar/recent_tabs_sub_menu_model.h
+++ chromium_src/chrome/browser/ui/toolbar/recent_tabs_sub_menu_model.h
@@ -6,9 +6,11 @@
 #ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_TOOLBAR_RECENT_TABS_SUB_MENU_MODEL_H_
 #define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_TOOLBAR_RECENT_TABS_SUB_MENU_MODEL_H_
 
+#include <memory>
+
 #define BRAVE_RECENT_TABS_SUB_MENU_MODEL_H_ \
   private: \
-    std::auto_ptr<sessions::SessionTab> stub_tab_; \
+    std::unique_ptr<sessions::SessionTab> stub_tab_; \
     friend class BraveRecentTabsSubMenuModel; \
   public:
 #include "src/chrome/browser/ui/toolbar/recent_tabs_sub_menu_model.h"
