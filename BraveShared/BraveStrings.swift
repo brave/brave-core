ab6cff38649548dcb45f8c38d149399e96130b24
Michał Buczek <michal.buczek@protonmail.com>
2021-12-03 20:11:18 +0100

    Fix #4627: Update copy for X shields blocked popup. (#4628)

--- BraveShared/BraveStrings.swift
+++ BraveShared/BraveStrings.swift
@@ -1181,7 +1181,7 @@ extension Strings {
         public static let trackerCountShareTitle =
             NSLocalizedString("shieldEducation.trackerCountShareTitle",
                               bundle: .braveShared,
-                              value: "%ld trackers & ads blocked",
+                              value: "%ld trackers & ads blocked!",
                               comment: "Title for Shield Education Tracker Count Share. The parameter substituted for \"%ld\" is the count of the trackers and ads blocked in total until present. E.g.: 5000 trackers & ads blocked")
         
         public static let videoAdBlockTitle =
@@ -1213,6 +1213,12 @@ extension Strings {
                               bundle: .braveShared,
                               value: "Share the news",
                               comment: "Action title for actionable Share warnings")
+        
+        public static let benchmarkAnyTierTitle =
+            NSLocalizedString("shieldEducation.benchmarkAnyTierTitle",
+                              bundle: .braveShared,
+                              value: "Congrats on reaching this privacy milestone.",
+                              comment: "Subtitle for tracker benchmark Share")
         
         public static let benchmarkSpecialTierTitle =
             NSLocalizedString("shieldEducation.benchmarkSpecialTierTitle",
