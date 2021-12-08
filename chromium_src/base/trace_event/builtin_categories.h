9b1660c908d01d448c06a11cea94a8e3ea38fb54
Mikhail Atuchin <matuchin@brave.com>
2021-12-08 18:02:50 +0700

    Rewrite a comment

--- chromium_src/base/trace_event/builtin_categories.h
+++ chromium_src/base/trace_event/builtin_categories.h
@@ -6,11 +6,14 @@
 #ifndef BRAVE_CHROMIUM_SRC_BASE_TRACE_EVENT_BUILTIN_CATEGORIES_H_
 #define BRAVE_CHROMIUM_SRC_BASE_TRACE_EVENT_BUILTIN_CATEGORIES_H_
 
-// Please add all brave categories here. For small features use the existing
-// 'brave' category. For new big subsystems with a lot of traces create an
-// separated category named 'brave.<feature_name>`.
+// Please add all brave categories here. Take a look at the original
+// builtin_categories.h for more details. Please:
+// * For small features use the existing 'brave' category.
+// * For new big subsystems with a lot of traces create a separate category
+// named 'brave.<feature_name>`.
+// * Keep the list sorted.
 //
-// Note: The macros shouldn't be undefined because it can be used in exported
+// Note: THere is no #undef for this macros because it is used in exported
 // macros of the chromium builtin_categories.h.
 #define BRAVE_INTERNAL_TRACE_LIST_BUILTIN_CATEGORIES(X) \
   X("brave")                                            \
