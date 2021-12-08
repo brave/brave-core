a4060d9db920bb586e56fca18757f9e190594a8c
brave-builds <devops@brave.com>
2021-12-08 10:04:28 +0000

    Uplift of #11469 (squashed) to release

--- android/java/org/chromium/chrome/browser/crypto_wallet/activities/BuySendSwapActivity.java
+++ android/java/org/chromium/chrome/browser/crypto_wallet/activities/BuySendSwapActivity.java
@@ -107,7 +107,7 @@ public class BuySendSwapActivity extends AsyncInitializationActivity
 
     private CameraSource mCameraSource;
     private CameraSourcePreview mCameraSourcePreview;
-    private boolean mInitialLayoutInflationComplete = false;
+    private boolean mInitialLayoutInflationComplete;
 
     public enum ActivityType {
         BUY(0),
