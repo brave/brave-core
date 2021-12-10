e3fe2ef7cab54b0fe2ebd6166a5cd5ff28f7ded5
Aleksey Khoroshilov <akhoroshilov@brave.com>
2021-12-10 13:40:53 +0700

    Remove ifdef guards to restore net log events, added NOLINT for guards.

--- chromium_src/net/log/net_log_event_type_list.h
+++ chromium_src/net/log/net_log_event_type_list.h
@@ -2,8 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef BRAVE_CHROMIUM_SRC_NET_LOG_NET_LOG_EVENT_TYPE_LIST_H_
-#define BRAVE_CHROMIUM_SRC_NET_LOG_NET_LOG_EVENT_TYPE_LIST_H_
+// NOTE: No header guards are used, since this file is intended to be expanded
+// directly into net_log.h. DO NOT include this file anywhere else.
+// The following line silences a presubmit warning that would otherwise be
+// triggered by this:
+// no-include-guard-because-multiply-included
+// NOLINT(build/header_guard)
 
 #include "src/net/log/net_log_event_type_list.h"
 
@@ -12,5 +16,3 @@ EVENT_TYPE(SOCKS5_AUTH_WRITE)
 
 // The time spent waiting for the authentication response from the SOCKS server
 EVENT_TYPE(SOCKS5_AUTH_READ)
-
-#endif  // BRAVE_CHROMIUM_SRC_NET_LOG_NET_LOG_EVENT_TYPE_LIST_H_
