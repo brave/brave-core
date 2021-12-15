f268a0dadf5e448497d587e9c833c9970a2777d4
mkarolin <max@brave.com>
2021-12-15 09:41:28 -0500

    Some startup infobars functionality was moved to a different file.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/c6243e519e6cdf1653650d264c9efa41dece921d
    
    commit c6243e519e6cdf1653650d264c9efa41dece921d
    Author: Christian Flach <cmfcmf@chromium.org>
    Date:   Wed Dec 1 10:39:34 2021 +0000
    
        Show infobars in web apps
    
        Infobars were never shown when the users started a web app (i.e. by
        passing the --app-id or --app) parameters. Most importantly, infobars
        warning about kEnableAutomation and kBadFlags were not shown.
    
        We now also call AddInfoBarsIfNecessary when a web app is started.
        We introduce a new `is_web_app` parameter so that infobars that are not
        applicable to web apps are not created for web apps. This currently
        applies to the session restore bubble that is shown after a crash and
        the default browser prompt (it might be confusing to users when a web
        app asks them to set their default browser, since web apps are supposed
        to feel more like native apps).
    
        Bug: 1273509

--- chromium_src/chrome/browser/ui/startup/startup_browser_creator_impl.cc
+++ chromium_src/chrome/browser/ui/startup/startup_browser_creator_impl.cc
@@ -3,30 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "brave/browser/ui/startup/default_brave_browser_prompt.h"
-#include "chrome/browser/ui/session_crashed_bubble.h"
-#include "chrome/browser/ui/startup/google_api_keys_infobar_delegate.h"
-#include "components/infobars/content/content_infobar_manager.h"
-#include "components/infobars/core/confirm_infobar_delegate.h"
-
-#define ShowIfNotOffTheRecordProfile ShowIfNotOffTheRecordProfileBrave
-#define GoogleApiKeysInfoBarDelegate BraveGoogleKeysInfoBarDelegate
-
-class BraveGoogleKeysInfoBarDelegate {
- public:
-  static void Create(infobars::ContentInfoBarManager* infobar_manager) {
-    // lulz
-  }
-};
-
 #define BRAVE_STARTUPBROWSERCREATORIMPL_DETERMINEURLSANDLAUNCH \
   welcome_enabled = true;
 
-#define ShowDefaultBrowserPrompt ShowDefaultBraveBrowserPrompt
-
 #include "src/chrome/browser/ui/startup/startup_browser_creator_impl.cc"
-
 #undef BRAVE_STARTUPBROWSERCREATORIMPL_DETERMINEURLSANDLAUNCH
-#undef GoogleApiKeysInfoBarDelegate
-#undef ShowDefaultBrowserPrompt
-#undef ShowIfNotOffTheRecordProfile
