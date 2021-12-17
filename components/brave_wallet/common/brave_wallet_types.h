805367ffaa9b888fffa9a563310430807e3e2509
Mario Sanchez Prada <mario@igalia.com>
2021-12-17 13:05:58 +0100

    Fix build errors due to deprecated _ExtInt type
    Chromium 98 has updated clang to a recent version (see [1]) including
    a change deprecating _ExtInt in favour of _BigInt (see [2]).
    
    [1] https://reviews.llvm.org/D115051
    [2] https://reviews.llvm.org/D108643
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/d76c5d369a73d7c1f77aa2d56ef08a2fbabb9c8f
    
    commit d76c5d369a73d7c1f77aa2d56ef08a2fbabb9c8f
    Author: Nico Weber <thakis@chromium.org>
    Date:   Tue Dec 7 17:52:09 2021 +0000
    
        Roll clang llvmorg-14-init-11416-gc22b1106-2 : llvmorg-14-init-11564-g37fbf238-1
    
        Ran `./tools/clang/scripts/upload_revision.py 37fbf238f4427b651a16956eca1cb0e2ab5cdbfd`.
    
        Another small 150-rev follow-up roll to pick up
        https://reviews.llvm.org/rGe4eb6216c2ebeac5203ff9cab8fe96f092d08c0f
    
        Bug: 1269407

--- components/brave_wallet/common/brave_wallet_types.h
+++ components/brave_wallet/common/brave_wallet_types.h
@@ -11,8 +11,8 @@
 
 namespace brave_wallet {
 
-typedef unsigned _ExtInt(256) uint256_t;
-typedef _ExtInt(256) int256_t;
+typedef unsigned _BitInt(256) uint256_t;
+typedef _BitInt(256) int256_t;
 
 struct TransactionReceipt {
   TransactionReceipt();
