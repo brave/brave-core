7ada08ed27fe77e479d17d05e3b4632ca93ae7a0
Terry Mancey <terry.mancey@icloud.com>
2021-12-22 14:10:44 +0000

    Use Update instead of Insert for creative_ads_conversion table

--- vendor/bat-native-ads/src/bat/ads/internal/database/database_version.cc
+++ vendor/bat-native-ads/src/bat/ads/internal/database/database_version.cc
@@ -9,11 +9,11 @@ namespace ads {
 namespace database {
 
 int32_t version() {
-  return 19;
+  return 20;
 }
 
 int32_t compatible_version() {
-  return 19;
+  return 20;
 }
 
 }  // namespace database
