9c9ae68f552ac3b85b1ab40951f96f4c145a1964
Mikhail <45488748+atuchin-m@users.noreply.github.com>
2022-01-06 15:41:08 +0700

    Get chrome_pgo_phase from npm config (#11549)
    * Add chrome_pgo_phase npm param
    * Make chrome_pgo_phase always int
    * Use a channel based is_brave_release_build
    * Fix buildGClientConfig()
    * Disabled channel based logic with TODO
    * Remove is_nightly() check
    * Enable isBraveReleaseBuild by default
    * add channel !== `development`
    * Fix using a bug with stuck args.gn
    * Update build/commands/lib/config.js
    * Update build/commands/lib/util.js

--- build/commands/lib/config.js
+++ build/commands/lib/config.js
@@ -134,7 +134,6 @@ const Config = function () {
   this.rewardsGrantDevEndpoint = getNPMConfig(['rewards_grant_dev_endpoint']) || ''
   this.rewardsGrantStagingEndpoint = getNPMConfig(['rewards_grant_staging_endpoint']) || ''
   this.rewardsGrantProdEndpoint = getNPMConfig(['rewards_grant_prod_endpoint']) || ''
-  this.chromePgoPhase = 0
   // this.buildProjects()
   this.braveVersion = getNPMConfig(['version']) || '0.0.0'
   this.androidOverrideVersionName = this.braveVersion
@@ -175,6 +174,17 @@ const Config = function () {
 
 Config.prototype.isOfficialBuild = function () {
   return this.buildConfig === 'Release'
+}
+
+Config.prototype.isBraveReleaseBuild = function () {
+  const npm_brave_relese_build = getNPMConfig(['is_brave_release_build'])
+  if (npm_brave_relese_build !== undefined) {
+    assert(npm_brave_relese_build === '0' || npm_brave_relese_build === '1',
+      'Bad is_brave_release_build npm value (should be 0 or 1)')
+    return npm_brave_relese_build === '1'
+  }
+
+  return false
 }
 
 Config.prototype.isComponentBuild = function () {
@@ -285,11 +295,14 @@ Config.prototype.buildArgs = function () {
     enable_cdm_host_verification: this.enableCDMHostVerification(),
     enable_pseudolocales: this.enable_pseudolocales,
     skip_signing: !this.shouldSign(),
-    chrome_pgo_phase: this.chromePgoPhase,
     sparkle_dsa_private_key_file: this.sparkleDSAPrivateKeyFile,
     sparkle_eddsa_private_key: this.sparkleEdDSAPrivateKey,
     sparkle_eddsa_public_key: this.sparkleEdDSAPublicKey,
     ...this.extraGnArgs,
+  }
+
+  if (!this.isBraveReleaseBuild()) {
+    args.chrome_pgo_phase = 0
   }
 
   if (this.shouldSign()) {
