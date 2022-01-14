8ccf80a9584e9c7f68fa334353956611a25b7748
samartnik <artem@brave.com>
2022-01-14 11:28:30 -0500

    [Android] Removed obsolete param for TabModel setIndex

--- android/java/org/chromium/chrome/browser/app/BraveActivity.java
+++ android/java/org/chromium/chrome/browser/app/BraveActivity.java
@@ -1003,7 +1003,7 @@ public abstract class BraveActivity<C extends ChromeActivityComponent>
         if (tabIndex != TabModel.INVALID_TAB_INDEX) {
             tab = tabModel.getTabAt(tabIndex);
             // Set active tab
-            tabModel.setIndex(tabIndex, TabSelectionType.FROM_USER, false);
+            tabModel.setIndex(tabIndex, TabSelectionType.FROM_USER);
             return tab;
         } else {
             return null;
