0aa10bdcee0cc83781736b8f84a09dec812900c2
Szilard Szaloki <szilardszaloki@gmail.com>
2022-01-18 10:12:47 +0100

    Brave Sync-related - `RewardsChromeSyncClient` - `RewardsSyncServiceFactory` - add `CreateScopedPrefService` to `PrefServiceSyncable`

---
+++ chromium_src/chrome/browser/sync/chrome_sync_client.h
@@ -0,0 +1,18 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_SYNC_CHROME_SYNC_CLIENT_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_SYNC_CHROME_SYNC_CLIENT_H_
+
+#include "components/sync/driver/sync_client.h"
+
+#define OnLocalSyncTransportDataCleared                             \
+SetDefaultEnabledTypes(syncer::SyncService* sync_service) override; \
+void OnLocalSyncTransportDataCleared
+
+#include "src/chrome/browser/sync/chrome_sync_client.h"
+#undef OnLocalSyncTransportDataCleared
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_SYNC_CHROME_SYNC_CLIENT_H_
