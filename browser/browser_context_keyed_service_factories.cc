08cc604000f1daee6e92d5a2d242ba84a2f0033b
Szilard Szaloki <szilardszaloki@gmail.com>
2022-01-18 10:12:47 +0100

    Brave Sync-related changes - `RewardsChromeSyncClient` - `RewardsSyncServiceFactory` - add `CreateScopedPrefService` to `PrefServiceSyncable`

--- browser/browser_context_keyed_service_factories.cc
+++ browser/browser_context_keyed_service_factories.cc
@@ -9,6 +9,7 @@
 #include "brave/browser/brave_federated/brave_federated_service_factory.h"
 #include "brave/browser/brave_news/brave_news_controller_factory.h"
 #include "brave/browser/brave_rewards/rewards_service_factory.h"
+#include "brave/browser/brave_rewards/rewards_sync_service_factory.h"
 #include "brave/browser/brave_shields/ad_block_pref_service_factory.h"
 #include "brave/browser/brave_shields/cookie_pref_service_factory.h"
 #include "brave/browser/brave_wallet/asset_ratio_service_factory.h"
@@ -62,6 +63,7 @@ void EnsureBrowserContextKeyedServiceFactoriesBuilt() {
   brave_ads::AdsServiceFactory::GetInstance();
   brave_federated::BraveFederatedServiceFactory::GetInstance();
   brave_rewards::RewardsServiceFactory::GetInstance();
+  RewardsSyncServiceFactory::GetInstance();
   brave_shields::AdBlockPrefServiceFactory::GetInstance();
   brave_shields::CookiePrefServiceFactory::GetInstance();
   debounce::DebounceServiceFactory::GetInstance();
