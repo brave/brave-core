4632aeb0596e2fe33dc20c4801ab774d72cdfda3
Szilard Szaloki <szilardszaloki@gmail.com>
2022-01-18 10:12:47 +0100

    Brave Sync-related - `RewardsChromeSyncClient` - `RewardsSyncServiceFactory` - add `CreateScopedPrefService` to `PrefServiceSyncable`

---
+++ browser/brave_rewards/rewards_sync_service_factory.cc
@@ -0,0 +1,20 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/browser/brave_rewards/rewards_sync_service_factory.h"
+
+#include "brave/browser/brave_rewards/rewards_chrome_sync_client.h"
+#include "brave/browser/brave_rewards/vg_sync_service_factory.h"
+#include "chrome/browser/sync/chrome_sync_client.h"
+#include "chrome/browser/sync/sync_service_factory.h"
+#include "chrome/browser/undo/bookmark_undo_service_factory.h"
+
+#define BookmarkUndoServiceFactory BookmarkUndoServiceFactory::GetInstance()); DependsOn(brave_rewards::VgSyncServiceFactory
+#define ChromeSyncClient RewardsChromeSyncClient
+#define SyncServiceFactory RewardsSyncServiceFactory
+#include "chrome/browser/sync/sync_service_factory.cc"
+#undef SyncServiceFactory
+#undef ChromeSyncClient
+#undef BookmarkUndoServiceFactory
