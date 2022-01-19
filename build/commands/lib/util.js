84d99f42346a43b3c9ee929289cc6bcd583ef45d
Mikhail <45488748+atuchin-m@users.noreply.github.com>
2022-01-19 20:19:03 +0700

    Return with_tags

--- build/commands/lib/util.js
+++ build/commands/lib/util.js
@@ -646,7 +646,7 @@ const util = {
     // base args
     const initialArgs = ['sync', '--reset', '--nohooks']
     const chromiumArgs = ['--revision', 'src@' + config.getProjectRef('chrome')]
-    const resetArgs = ['--upstream']
+    const resetArgs = ['--with_tags', '--upstream']
 
     let args = [...initialArgs]
     let didUpdateChromium = false
