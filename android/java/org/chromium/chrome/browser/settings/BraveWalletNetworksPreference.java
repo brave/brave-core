b75f54472cde48351a4d27fdde8c72be9ff3640a
samartnik <artem@brave.com>
2022-01-24 13:03:34 -0500

    [Android] Fix default_control_color for wallet network preferences
    Fixes https://github.com/brave/brave-browser/issues/20648

--- android/java/org/chromium/chrome/browser/settings/BraveWalletNetworksPreference.java
+++ android/java/org/chromium/chrome/browser/settings/BraveWalletNetworksPreference.java
@@ -19,7 +19,6 @@ import org.chromium.brave_wallet.mojom.JsonRpcService;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.crypto_wallet.JsonRpcServiceFactory;
 import org.chromium.chrome.browser.crypto_wallet.util.Utils;
-import org.chromium.components.browser_ui.styles.SemanticColorUtils;
 import org.chromium.components.browser_ui.widget.TintedDrawable;
 import org.chromium.mojo.bindings.ConnectionErrorHandler;
 import org.chromium.mojo.system.MojoException;
@@ -32,11 +31,9 @@ public class BraveWalletNetworksPreference extends Preference
     private NetworkListBaseAdapter mAdapter;
     private BraveWalletAddNetworksFragment.Launcher mLauncher;
     private JsonRpcService mJsonRpcService;
-    private Context mContext;
 
     public BraveWalletNetworksPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mContext = context;
         mAdapter = new NetworkListBaseAdapter(context, this);
     }
 
@@ -49,7 +46,7 @@ public class BraveWalletNetworksPreference extends Preference
         mbtAddNetwork = (TextView) holder.findViewById(R.id.add_network);
         mbtAddNetwork.setCompoundDrawablesRelativeWithIntrinsicBounds(
                 TintedDrawable.constructTintedDrawable(getContext(), R.drawable.plus,
-                        SemanticColorUtils.getDefaultControlColorActive(mContext)),
+                        R.color.default_control_color_active_baseline),
                 null, null, null);
         mbtAddNetwork.setOnClickListener(view -> { mLauncher.launchAddNetwork(""); });
         mLauncher.setRefresher(this);
