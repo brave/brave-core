262d68294239986bba77d9296342d1416011d9ac
Mario Sanchez Prada <mario@igalia.com>
2022-01-24 19:56:31 +0100

    Add EXTENSIONS_EXPORT macro to Brave's extensions constants
    This is now needed or Brave will fail to link.
    
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/bc0972bdb213c227e0fa22631e078cc76e172014
    
    commit bc0972bdb213c227e0fa22631e078cc76e172014
    Author: Devlin Cronin <rdevlin.cronin@chromium.org>
    Date:   Tue Jan 18 23:01:59 2022 +0000
    
        [Extensions] Make extensions/common:common_constants component-friendly
    
        The //extensions layer is currently not component-friendly - it is a
        bunch of targets with source_sets, which means that if components depend
        on these targets, they are pulled in multiple times. Among other things,
        this is very bad for singletons (which are no longer singletons).
    
        A number of //components depend on //extensions; however, all but
        //components/sessions only appear to depend on the
        //extensions/common:common_constants target.
    
        Make //extensions/common:common_constants a component-friendly target
        and introduce a top-level //extensions component target (which currently
        only includes //extensions/common:common_constants). This allows
        //components to depend on this target, and gives us a component that we
        can slowly add items to to make the //extensions layer itself a full
        component.
    
        Bug: 1286881

--- chromium_src/extensions/common/constants.h
+++ chromium_src/extensions/common/constants.h
@@ -8,25 +8,25 @@
 
 #include "src/extensions/common/constants.h"
 
-extern const int kBraveActionGraphicSize;
-extern const int kBraveActionRightMargin;
+EXTENSIONS_EXPORT extern const int kBraveActionGraphicSize;
+EXTENSIONS_EXPORT extern const int kBraveActionRightMargin;
 
-extern const char brave_extension_id[];
-extern const char brave_rewards_extension_id[];
-extern const char hangouts_extension_id[];
-extern const char widevine_extension_id[];
-extern const char crl_set_extension_id[];
+EXTENSIONS_EXPORT extern const char brave_extension_id[];
+EXTENSIONS_EXPORT extern const char brave_rewards_extension_id[];
+EXTENSIONS_EXPORT extern const char hangouts_extension_id[];
+EXTENSIONS_EXPORT extern const char widevine_extension_id[];
+EXTENSIONS_EXPORT extern const char crl_set_extension_id[];
 
-extern const char brave_webtorrent_extension_id[];
-extern const char brave_webtorrent_extension_filename[];
-extern const char brave_webtorrent_extension_filename2[];
+EXTENSIONS_EXPORT extern const char brave_webtorrent_extension_id[];
+EXTENSIONS_EXPORT extern const char brave_webtorrent_extension_filename[];
+EXTENSIONS_EXPORT extern const char brave_webtorrent_extension_filename2[];
 
-extern const char ipfs_companion_extension_id[];
-extern const char ipfs_companion_extension_name[];
-extern const char ipfs_companion_extension_public_key[];
-extern const char ipfs_companion_beta_extension_id[];
-extern const char ipfs_persmission_extension_id[];
+EXTENSIONS_EXPORT extern const char ipfs_companion_extension_id[];
+EXTENSIONS_EXPORT extern const char ipfs_companion_extension_name[];
+EXTENSIONS_EXPORT extern const char ipfs_companion_extension_public_key[];
+EXTENSIONS_EXPORT extern const char ipfs_companion_beta_extension_id[];
+EXTENSIONS_EXPORT extern const char ipfs_persmission_extension_id[];
 
-extern const char google_translate_extension_id[];
+EXTENSIONS_EXPORT extern const char google_translate_extension_id[];
 
 #endif  // BRAVE_CHROMIUM_SRC_EXTENSIONS_COMMON_CONSTANTS_H_
