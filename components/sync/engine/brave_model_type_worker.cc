d91d8b0ac70337839dd99793b2d7b674cfe2958b
Mario Sanchez Prada <mario@igalia.com>
2022-01-24 16:50:16 +0100

    Adapt to new name of ModelType[Set]FromDebugString
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/1b7f3efc2ec96bc400f0722ff729ca481a734fbe
    
    commit 1b7f3efc2ec96bc400f0722ff729ca481a734fbe
    Author: Victor Hugo Vianna Silva <victorvianna@google.com>
    Date:   Wed Jan 19 16:47:33 2022 +0000
    
        Delete ModelType[Set]FromDebugString
    
        No behavior is changed, this method is unused in production code
        since crrev.com/c/3399838.
    
        Fixed: 1173546

--- components/sync/engine/brave_model_type_worker.cc
+++ components/sync/engine/brave_model_type_worker.cc
@@ -110,7 +110,7 @@ bool BraveModelTypeWorker::IsResetProgressMarkerRequired(
 }
 
 void BraveModelTypeWorker::ResetProgressMarker() {
-  VLOG(1) << "Reset progress marker for type " << ModelTypeToString(type_);
+  VLOG(1) << "Reset progress marker for type " << ModelTypeToDebugString(type_);
   // Normal reset of progress marker due to 7th failure
   // P3A sample is 0
   base::UmaHistogramExactLinear("Brave.Sync.ProgressTokenEverReset", 0, 1);
