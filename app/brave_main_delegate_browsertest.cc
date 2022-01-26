9deea9e6454f58db43b5f3df3dc90cc75301ce2c
Mario Sanchez Prada <mario@igalia.com>
2022-01-26 18:04:36 +0100

    Stop disabling the apps::kAppDiscoveryRemoteUrlSearch feature
    This is a ChromeOS-specific feature now that we don't need to
    disable (besides, app_discovery_features.h is no longer built),
    so we can drop the override and the line from the browser test.
    
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/c79366cf5bd167967e8504d2910d80038c01113f
    
    commit c79366cf5bd167967e8504d2910d80038c01113f
    Author: Melissa Zhang <melzhang@google.com>
    Date:   Wed Jan 12 03:11:47 2022 +0000
    
        [App Discovery] Query new Play endpoint behind flag & parse response.
    
        This CL adds querying to the new Play endpoint when the
        AppDiscoveryForOobe flag is enabled. The new response is passed
        to the recommended_arc_app_fetcher in app discovery to be parsed
        into Results.
    
        This is a simple working implementation that doesn't heavily
        impact the existing call to the previous endpoint, which is
        still needed as we gradually make the transition between the two.
    
        Bug:1223321

--- app/brave_main_delegate_browsertest.cc
+++ app/brave_main_delegate_browsertest.cc
@@ -33,7 +33,6 @@
 #if defined(OS_ANDROID)
 #include "chrome/test/base/android/android_browser_test.h"
 #else
-#include "chrome/browser/apps/app_discovery_service/app_discovery_features.h"
 #include "chrome/browser/browser_features.h"
 #include "chrome/browser/ui/profile_picker.h"
 #include "chrome/browser/ui/ui_features.h"
@@ -85,7 +84,6 @@ IN_PROC_BROWSER_TEST_F(BraveMainDelegateBrowserTest, OriginTrialsTest) {
 IN_PROC_BROWSER_TEST_F(BraveMainDelegateBrowserTest, DisabledFeatures) {
   const base::Feature* disabled_features[] = {
 #if !defined(OS_ANDROID)
-    &apps::kAppDiscoveryRemoteUrlSearch,
     &translate::kTFLiteLanguageDetectionEnabled,
 #endif
     &autofill::features::kAutofillEnableAccountWalletStorage,
