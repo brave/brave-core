1097687edbe14b0989f80a3b0310b1b71e0e0e82
Serg <serg.zhukovsky@gmail.com>
2022-01-28 12:16:12 -0500

    Replace upstream permission dialog to a wallet permission dialog for wallet requests on Android

--- chromium_src/components/permissions/permission_request.cc
+++ chromium_src/components/permissions/permission_request.cc
@@ -16,7 +16,16 @@
 #define IsDuplicateOf IsDuplicateOf_ChromiumImpl
 
 // `kWidevine` handled by an override in `WidevinePermissionRequest` and the
-// Brave Ethereum permission has its own permission request prompt.
+// Brave Ethereum permission has its own permission request prompt on desktop.
+// We hardcode it with Ethereum on Android to prevent assert for empty String.
+#if defined(OS_ANDROID)
+#define BRAVE_ENUM_ITEMS_FOR_SWITCH     \
+  case RequestType::kBraveEthereum:     \
+    return std::u16string(u"Ethereum"); \
+  case RequestType::kWidevine:          \
+    NOTREACHED();                       \
+    return std::u16string();
+#else
 #define BRAVE_ENUM_ITEMS_FOR_SWITCH \
   case RequestType::kBraveEthereum: \
     NOTREACHED();                   \
@@ -24,6 +33,7 @@
   case RequestType::kWidevine:      \
     NOTREACHED();                   \
     return std::u16string();
+#endif
 
 namespace {
 #if BUILDFLAG(IS_ANDROID)
