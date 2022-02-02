c08789ad0de7e7d3fc0c89954269e9f37ea90bd3
tapanmodh <modhtapan@gmail.com>
2022-02-02 19:39:16 +0530

    add test to make sure prepareLaunchPromoIfNeeded is in DefaultBrowserPromoUtils

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -263,6 +263,9 @@ public class BytecodeTest {
                 "org/chromium/chrome/browser/ui/appmenu/AppMenu", "getPopupPosition", false, null));
         Assert.assertTrue(methodExists("org/chromium/chrome/browser/ui/appmenu/AppMenu",
                 "runMenuItemEnterAnimations", false, null));
+        Assert.assertTrue(
+                methodExists("org/chromium/chrome/browser/ui/default_browser_promo/DefaultBrowserPromoUtils", 
+                        "prepareLaunchPromoIfNeeded", false, null));
         Assert.assertTrue(methodExists("org/chromium/chrome/browser/toolbar/ToolbarManager",
                 "onOrientationChange", false, null));
         Assert.assertTrue(methodExists("org/chromium/chrome/browser/toolbar/ToolbarManager",
