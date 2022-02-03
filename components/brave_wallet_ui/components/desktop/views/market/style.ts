d07e20037ad2834cb08c4cbe598d29543cad5e1b
William Muli <48117473+muliswilliam@users.noreply.github.com>
2022-02-03 14:54:26 +0300

    Refactored market datatable

--- components/brave_wallet_ui/components/desktop/views/market/style.ts
+++ components/brave_wallet_ui/components/desktop/views/market/style.ts
@@ -1,9 +1,5 @@
 import styled from 'styled-components'
 
-export interface StyleProps {
-  alignment: 'right' | 'left'
-}
-
 export const StyledWrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -21,35 +17,4 @@ export const TopRow = styled.div`
   width: 100%;
   margin-bottom: 24px;
   gap: 10px;
-`
-
-export const AssetsColumnWrapper = styled.div`
-  display: inline-flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: left;
-`
-
-export const AssetsColumnItemSpacer = styled.div`
-  display: inline-flex;
-  align-items: 'center';
-  justify-content: 'center';
-  margin-right: 19px;
-`
-export const TextWrapper = styled.div<StyleProps>`
-  display: flex;
-  justify-content: ${p => p.alignment === 'right' ? 'flex-end' : 'flex-start'};
-  width: 100%;
-  font-family: Poppins;
-  font-size: 14px;
-  letter-spacing: 0.01em;
-`
-
-export const LineChartWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  height: 30px;
-  max-width: 120px;
-  margin: 0 auto;
 `
--- components/brave_wallet_ui/components/market-datatable/style.ts
+++ components/brave_wallet_ui/components/market-datatable/style.ts
@@ -1,7 +1,42 @@
 import styled from 'styled-components'
 
+export interface StyleProps {
+  alignment: 'right' | 'left'
+}
+
 export const StyledWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
+`
+
+export const AssetsColumnWrapper = styled.div`
+  display: inline-flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: left;
+`
+
+export const AssetsColumnItemSpacer = styled.div`
+  display: inline-flex;
+  align-items: 'center';
+  justify-content: 'center';
+  margin-right: 19px;
+`
+export const TextWrapper = styled.div<StyleProps>`
+  display: flex;
+  justify-content: ${p => p.alignment === 'right' ? 'flex-end' : 'flex-start'};
+  width: 100%;
+  font-family: Poppins;
+  font-size: 14px;
+  letter-spacing: 0.01em;
+`
+
+export const LineChartWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  height: 30px;
+  max-width: 120px;
+  margin: 0 auto;
 `
