88cda37d1755e0bdcc8e11ec74949e745cc41d59
William Muli <48117473+muliswilliam@users.noreply.github.com>
2022-02-03 14:54:26 +0300

    Refactored market datatable

--- components/brave_wallet_ui/components/desktop/views/market/index.tsx
+++ components/brave_wallet_ui/components/desktop/views/market/index.tsx
@@ -1,27 +1,14 @@
 import * as React from 'react'
 
 import { SearchBar } from '../../../shared'
-import { AssetsFilterDropdown, LineChart } from '../..'
+import { AssetsFilterDropdown } from '../..'
 import { AssetFilterOptions } from '../../../../options/market-data-filter-options'
 import { CoinMarketMetadata, MarketDataTableColumnTypes, SortOrder } from '../../../../constants/types'
 import MarketDataTable, { MarketDataHeader } from '../../../../components/market-datatable'
-import { Cell, Row } from '../../../../components/shared/datatable'
-import AssetWishlistStar from '../../../../components/asset-wishlist-star'
-import AssetPriceChange from '../../../../components/asset-price-change'
-import AssetNameAndIcon from '../../../../components/asset-name-and-icon'
-import {
-  formatFiatAmountWithCommasAndDecimals,
-  formatPricePercentageChange,
-  formatPriceWithAbbreviation
-} from '../../../../utils/format-prices'
 
 import {
-  AssetsColumnWrapper,
-  AssetsColumnItemSpacer,
   StyledWrapper,
-  TopRow,
-  TextWrapper,
-  LineChartWrapper
+  TopRow
 } from './style'
 import { useMarketDataManagement } from '../../../../common/hooks/market-data-management'
 
@@ -74,95 +61,6 @@ const MarketView = (props: Props) => {
     setSortedMarketData(updatedMarketData)
   }, [sortOrder, sortByColumnId])
 
-  const renderCells = (coinMarkDataItem: CoinMarketMetadata) => {
-    const {
-      name,
-      symbol,
-      imageUrl,
-      currentPrice,
-      priceChange24h,
-      priceChangePercentage24h,
-      marketCap,
-      marketCapRank,
-      totalVolume,
-      priceHistory
-    } = coinMarkDataItem
-
-    const formattedPrice = formatFiatAmountWithCommasAndDecimals(currentPrice.toString(), 'USD')
-    const formattedPercentageChange = formatPricePercentageChange(priceChangePercentage24h, true)
-    const formattedMarketCap = formatPriceWithAbbreviation(marketCap.toString(), 'USD', 1)
-    const formattedVolume = formatPriceWithAbbreviation(totalVolume.toString(), 'USD', 1)
-    const isDown = priceChange24h < 0
-
-    const cellsContent: React.ReactNode[] = [
-      // Assets Column
-      <AssetsColumnWrapper>
-        <AssetsColumnItemSpacer>
-          <AssetWishlistStar active={true} />
-        </AssetsColumnItemSpacer>
-        <AssetsColumnItemSpacer>
-          <TextWrapper alignment="right">{marketCapRank}</TextWrapper>
-        </AssetsColumnItemSpacer>
-        <AssetNameAndIcon
-          assetName={name}
-          symbol={symbol}
-          assetLogo={imageUrl}
-        />
-      </AssetsColumnWrapper>,
-
-      // Price Column
-      <TextWrapper alignment="right">{formattedPrice}</TextWrapper>,
-
-      // Price Change Column
-      <TextWrapper alignment="right">
-        <AssetPriceChange
-          isDown={isDown}
-          priceChangePercentage={formattedPercentageChange}
-        />
-      </TextWrapper>,
-
-      // Market Cap Column
-      <TextWrapper alignment="right">{formattedMarketCap}</TextWrapper>,
-
-      // Volume Column
-      <TextWrapper alignment="right">{formattedVolume}</TextWrapper>,
-
-      // Line Chart Column
-      <LineChartWrapper>
-        <LineChart
-          priceData={priceHistory}
-          isLoading={false}
-          isDisabled={false}
-          isDown={isDown}
-          isAsset={true}
-          onUpdateBalance={() => {}}
-          showPulsatingDot={false}
-          customStyle={{
-            height: '20px',
-            width: '100%',
-            marginBottom: '0px'
-          }}
-        />
-      </LineChartWrapper>
-    ]
-
-    const cells: Cell[] = cellsContent.map(cellContent => {
-      return {
-        content: cellContent
-      }
-    })
-
-    return cells
-  }
-
-  const rows: Row[] = React.useMemo(() => {
-    const cells = sortedMarketData.map((coinMarketItem: CoinMarketMetadata) => {
-      return renderCells(coinMarketItem)
-    })
-
-    return cells.map(cell => { return { content: cell } })
-  }, [sortedMarketData, marketDataTableHeaders])
-
   return (
     <StyledWrapper>
       <TopRow>
@@ -179,7 +77,7 @@ const MarketView = (props: Props) => {
       </TopRow>
       <MarketDataTable
         headers={tableHeaders}
-        rows={rows}
+        coinMarketData={sortedMarketData}
         onSort={onSort}
       />
     </StyledWrapper>
--- components/brave_wallet_ui/components/market-datatable/index.tsx
+++ components/brave_wallet_ui/components/market-datatable/index.tsx
@@ -1,7 +1,22 @@
 import * as React from 'react'
-import { MarketDataTableColumnTypes, SortOrder } from '../../constants/types'
-import { StyledWrapper } from './style'
-import Table, { Header, Row } from '../shared/datatable'
+import { CoinMarketMetadata, MarketDataTableColumnTypes, SortOrder } from '../../constants/types'
+import Table, { Cell, Header, Row } from '../shared/datatable'
+import {
+  AssetsColumnWrapper,
+  AssetsColumnItemSpacer,
+  StyledWrapper,
+  TextWrapper,
+  LineChartWrapper
+} from './style'
+import LineChart from '../desktop/line-chart'
+import {
+  formatFiatAmountWithCommasAndDecimals,
+  formatPricePercentageChange,
+  formatPriceWithAbbreviation
+} from '../../utils/format-prices'
+import AssetNameAndIcon from '../asset-name-and-icon'
+import AssetPriceChange from '../asset-price-change'
+import AssetWishlistStar from '../asset-wishlist-star'
 
 export interface MarketDataHeader extends Header {
   id: MarketDataTableColumnTypes
@@ -9,12 +24,101 @@ export interface MarketDataHeader extends Header {
 
 export interface Props {
   headers: MarketDataHeader[]
-  rows: Row[]
+  coinMarketData: CoinMarketMetadata[]
   onSort?: (column: MarketDataTableColumnTypes, newSortOrder: SortOrder) => void
 }
 
 const MarketDataTable = (props: Props) => {
-  const { headers, rows, onSort } = props
+  const { headers, coinMarketData, onSort } = props
+
+  const renderCells = (coinMarkDataItem: CoinMarketMetadata) => {
+    const {
+      name,
+      symbol,
+      imageUrl,
+      currentPrice,
+      priceChange24h,
+      priceChangePercentage24h,
+      marketCap,
+      marketCapRank,
+      totalVolume,
+      priceHistory
+    } = coinMarkDataItem
+
+    const formattedPrice = formatFiatAmountWithCommasAndDecimals(currentPrice.toString(), 'USD')
+    const formattedPercentageChange = formatPricePercentageChange(priceChangePercentage24h, true)
+    const formattedMarketCap = formatPriceWithAbbreviation(marketCap.toString(), 'USD', 1)
+    const formattedVolume = formatPriceWithAbbreviation(totalVolume.toString(), 'USD', 1)
+    const isDown = priceChange24h < 0
+
+    const cellsContent: React.ReactNode[] = [
+      // Assets Column
+      <AssetsColumnWrapper>
+        <AssetsColumnItemSpacer>
+          <AssetWishlistStar active={true} />
+        </AssetsColumnItemSpacer>
+        <AssetsColumnItemSpacer>
+          <TextWrapper alignment="right">{marketCapRank}</TextWrapper>
+        </AssetsColumnItemSpacer>
+        <AssetNameAndIcon
+          assetName={name}
+          symbol={symbol}
+          assetLogo={imageUrl}
+        />
+      </AssetsColumnWrapper>,
+
+      // Price Column
+      <TextWrapper alignment="right">{formattedPrice}</TextWrapper>,
+
+      // Price Change Column
+      <TextWrapper alignment="right">
+        <AssetPriceChange
+          isDown={isDown}
+          priceChangePercentage={formattedPercentageChange}
+        />
+      </TextWrapper>,
+
+      // Market Cap Column
+      <TextWrapper alignment="right">{formattedMarketCap}</TextWrapper>,
+
+      // Volume Column
+      <TextWrapper alignment="right">{formattedVolume}</TextWrapper>,
+
+      // Line Chart Column
+      <LineChartWrapper>
+        <LineChart
+          priceData={priceHistory}
+          isLoading={false}
+          isDisabled={false}
+          isDown={isDown}
+          isAsset={true}
+          onUpdateBalance={() => {}}
+          showPulsatingDot={false}
+          customStyle={{
+            height: '20px',
+            width: '100%',
+            marginBottom: '0px'
+          }}
+        />
+      </LineChartWrapper>
+    ]
+
+    const cells: Cell[] = cellsContent.map(cellContent => {
+      return {
+        content: cellContent
+      }
+    })
+
+    return cells
+  }
+
+  const rows: Row[] = React.useMemo(() => {
+    const cells = coinMarketData.map((coinMarketItem: CoinMarketMetadata) => {
+      return renderCells(coinMarketItem)
+    })
+
+    return cells.map(cell => { return { content: cell } })
+  }, [coinMarketData, headers])
 
   return (
     <StyledWrapper>
