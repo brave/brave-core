897852d89090407bc7fe309c3defdc68bf511f50
Terry Mancey <terry.mancey@icloud.com>
2022-02-04 13:03:31 -0500

    Fixes ad confirmation is not sent when ad is not in the catalog

---
+++ vendor/bat-native-ads/src/bat/ads/internal/account/deposits/cash_deposit.h
@@ -0,0 +1,26 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_VENDOR_BAT_NATIVE_ADS_SRC_BAT_ADS_INTERNAL_ACCOUNT_DEPOSITS_CASH_DEPOSIT_H_
+#define BRAVE_VENDOR_BAT_NATIVE_ADS_SRC_BAT_ADS_INTERNAL_ACCOUNT_DEPOSITS_CASH_DEPOSIT_H_
+
+#include <string>
+
+#include "bat/ads/internal/account/deposits/deposit_base.h"
+
+namespace ads {
+
+class CashDeposit final : public DepositBase {
+ public:
+  CashDeposit();
+  ~CashDeposit() override;
+
+  void GetValue(const std::string& creative_instance_id,
+                GetDepositCallback callback) override;
+};
+
+}  // namespace ads
+
+#endif  // BRAVE_VENDOR_BAT_NATIVE_ADS_SRC_BAT_ADS_INTERNAL_ACCOUNT_DEPOSITS_CASH_DEPOSIT_H_
