5504dff588cb3ca45c4f78e4983e1bb7615d8a8b
brave-builds <devops@brave.com>
2022-02-09 20:54:55 +0000

    Uplift of #12149 (squashed) to beta

---
+++ android/javatests/org/chromium/chrome/browser/contextmenu/BraveContextMenuPopulatorTest.java
@@ -0,0 +1,30 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.chromium.chrome.browser.contextmenu;
+
+import static org.junit.Assert.assertFalse;
+
+import androidx.test.filters.SmallTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.chrome.browser.flags.ChromeSwitches;
+import org.chromium.chrome.browser.tasks.tab_management.BraveTabUiFeatureUtilities;
+import org.chromium.chrome.browser.tasks.tab_management.TabUiFeatureUtilities;
+
+@RunWith(BaseJUnit4ClassRunner.class)
+@CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
+public class BraveContextMenuPopulatorTest {
+    @Test
+    @SmallTest
+    public void testTabGroupAutoCreationPreference() {
+        BraveTabUiFeatureUtilities.maybeOverrideEnableTabGroupAutoCreationPreference();
+        assertFalse(TabUiFeatureUtilities.ENABLE_TAB_GROUP_AUTO_CREATION.getValue());
+    }
+}
