5044f92586214751953fd805373a539630c7258e
Emerick Rogul <erogul@brave.com>
2022-02-10 09:37:54 -0500

    Adapt to tab muting changes
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/f27f7ff938a4858ad57289f11c39c34bbf1f4ee1
    
    commit f27f7ff938a4858ad57289f11c39c34bbf1f4ee1
    Author: evliu <evliu@google.com>
    Date:   Tue Jan 25 21:15:24 2022 +0000
    
        Enable tab muting behind a flag
    
        This CL reimplements the tab muting feature that was previously removed
        in 2018. This implementation follows the original UX proposal of making
        the existing tab alert indicator into a clickable button but removes the
        hover and double click behavior.
    
        Bug: 1250848

--- chromium_src/chrome/browser/ui/views/tabs/tab.cc
+++ chromium_src/chrome/browser/ui/views/tabs/tab.cc
@@ -3,24 +3,24 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "brave/browser/ui/views/tabs/brave_alert_indicator.h"
+#include "brave/browser/ui/views/tabs/brave_alert_indicator_button.h"
 
 // Set alert indicator's pos to start of the title and
 // move title after the alert indicator.
 // Title right should respect close btn's space
-#define BRAVE_UI_VIEWS_TABS_TAB_ALERT_INDICATOR_POSITION    \
-  alert_indicator_->SetX(title_left - after_title_padding); \
-  title_right = close_x - after_title_padding;              \
-  if (showing_close_button_)                                \
-    title_right = close_x - after_title_padding;            \
-  title_left =                                              \
-      alert_indicator_->x() + alert_indicator_->width() + after_title_padding;
+#define BRAVE_UI_VIEWS_TABS_TAB_ALERT_INDICATOR_POSITION           \
+  alert_indicator_button_->SetX(title_left - after_title_padding); \
+  title_right = close_x - after_title_padding;                     \
+  if (showing_close_button_)                                       \
+    title_right = close_x - after_title_padding;                   \
+  title_left = alert_indicator_button_->x() +                      \
+               alert_indicator_button_->width() + after_title_padding;
 
 #define BRAVE_UI_VIEWS_TABS_TAB_UPDATE_ICON_VISIBILITY \
   showing_close_button_ &= mouse_hovered();
 
-#define AlertIndicator BraveAlertIndicator
+#define AlertIndicatorButton BraveAlertIndicatorButton
 #include "src/chrome/browser/ui/views/tabs/tab.cc"
-#undef AlertIndicator
+#undef AlertIndicatorButton
 #undef BRAVE_UI_VIEWS_TABS_TAB_UPDATE_ICON_VISIBILITY
 #undef BRAVE_UI_VIEWS_TABS_TAB_ALERT_INDICATOR_POSITION
