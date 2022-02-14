e0113c8031d34469beb3874a80d11fd572cf333a
Pavneet-Sing <psingh@brave.com>
2022-02-15 03:17:20 +0530

    show DApp wallet panel

---
+++ android/java/org/chromium/chrome/browser/crypto_wallet/modal/BraveWalletPanel.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (c) 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.chromium.chrome.browser.crypto_wallet.modal;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
+
+import org.chromium.base.SysUtils;
+import org.chromium.chrome.R;
+import org.chromium.chrome.browser.BraveRewardsHelper;
+import org.chromium.chrome.browser.ChromeTabbedActivity;
+import org.chromium.chrome.browser.util.ConfigurationUtils;
+import org.chromium.ui.base.DeviceFormFactor;
+
+public class BraveWalletPanel {
+    private static final String TAG = "BraveWalletPanel";
+    private final View mAnchorViewHost;
+    private final PopupWindow mPopupWindow;
+    private ViewGroup mPopupView;
+    private final ChromeTabbedActivity mActivity;
+
+    public BraveWalletPanel(View mAnchorViewHost) {
+        this.mAnchorViewHost = mAnchorViewHost;
+        mPopupWindow = new PopupWindow(mAnchorViewHost.getContext());
+        mPopupWindow.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+        mPopupWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        mPopupWindow.setElevation(20);
+
+        mPopupWindow.setTouchInterceptor(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_OUTSIDE) {
+                    dismiss();
+                    return true;
+                }
+                return false;
+            }
+        });
+        mPopupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {}
+        });
+        mActivity = BraveRewardsHelper.getChromeTabbedActivity();
+        setUpViews();
+    }
+
+    public void showLikePopDownMenu() {
+        mPopupWindow.setTouchable(true);
+        mPopupWindow.setFocusable(true);
+        mPopupWindow.setOutsideTouchable(true);
+
+        mPopupWindow.setAnimationStyle(R.style.OverflowMenuAnim);
+
+        if (SysUtils.isLowEndDevice()) {
+            mPopupWindow.setAnimationStyle(0);
+        }
+
+        mPopupWindow.showAsDropDown(mAnchorViewHost, 0, 0);
+    }
+
+    public void dismiss() {
+        mPopupWindow.dismiss();
+    }
+
+    private void setUpViews() {
+        LayoutInflater inflater = LayoutInflater.from(mAnchorViewHost.getContext());
+        mPopupView = (ViewGroup) inflater.inflate(R.layout.brave_wallet_panel_layout, null);
+
+        int deviceWidth = ConfigurationUtils.getDisplayMetrics(mActivity).get("width");
+        boolean isTablet = DeviceFormFactor.isNonMultiDisplayContextOnTablet(mActivity);
+
+        mPopupWindow.setWidth((int) (isTablet ? (deviceWidth * 0.6) : (deviceWidth * 0.95)));
+
+        mPopupWindow.setContentView(mPopupView);
+    }
+}
