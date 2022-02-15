db57bfc2660f8620aa113ff3ac1c27ecbe1bbb81
Emerick Rogul <erogul@brave.com>
2022-02-15 14:45:24 -0500

    Include field_trial_params.h as necessary
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/c290fc89c4b4953caaaace648fc419953ae440bf
    
    commit c290fc89c4b4953caaaace648fc419953ae440bf
    Author: David Sanders <dsanders11@ucsbalum.com>
    Date:   Fri Feb 4 18:04:55 2022 +0000
    
        Reland "Remove unused include of //base/metrics/field_trial_params.h"
    
        This is a reland of ff34cabdb95a8e887f253f140abcb66a79455b93
    
        Original change's description:
        > Remove unused include of //base/metrics/field_trial_params.h
        >
        > Bug: 242216
        > Change-Id: Ifaff25245e573b377f21e89e901be961909fcc63
        > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3425470
        > Reviewed-by: Lei Zhang <thestig@chromium.org>
        > Commit-Queue: Lei Zhang <thestig@chromium.org>
        > Owners-Override: Lei Zhang <thestig@chromium.org>
        > Cr-Commit-Position: refs/heads/main@{#966975}
    
        Bug: 242216

--- vendor/bat-native-ads/src/bat/ads/internal/user_activity/user_activity_features.cc
+++ vendor/bat-native-ads/src/bat/ads/internal/user_activity/user_activity_features.cc
@@ -5,6 +5,7 @@
 
 #include "bat/ads/internal/user_activity/user_activity_features.h"
 
+#include "base/metrics/field_trial_params.h"
 #include "base/time/time.h"
 #include "bat/ads/internal/features/features_util.h"
 
