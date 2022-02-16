68c076b676b004a810fcaf038814d27bf5c61d05
Emerick Rogul <erogul@brave.com>
2022-02-16 08:42:48 -0500

    Add missing include for net/http/http_util.h

--- net/dns/dns_transaction_unittest.cc
+++ net/dns/dns_transaction_unittest.cc
@@ -43,6 +43,7 @@
 #include "net/dns/public/dns_over_https_server_config.h"
 #include "net/dns/public/dns_protocol.h"
 #include "net/dns/resolve_context.h"
+#include "net/http/http_util.h"
 #include "net/log/net_log.h"
 #include "net/log/net_log_capture_mode.h"
 #include "net/log/net_log_with_source.h"
