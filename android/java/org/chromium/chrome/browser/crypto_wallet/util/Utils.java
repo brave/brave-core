094b251971b145af60bb1100cb391d225d86b348
Serg <serg.zhukovsky@gmail.com>
2022-02-17 13:13:01 -0500

    Adds a check canAuthenticate before invoking BiometricPrompt dialog on Android on wallet unlock

--- android/java/org/chromium/chrome/browser/crypto_wallet/util/Utils.java
+++ android/java/org/chromium/chrome/browser/crypto_wallet/util/Utils.java
@@ -22,14 +22,18 @@ import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
+import android.hardware.biometrics.BiometricManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Handler;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.annotation.VisibleForTesting;
+import androidx.core.hardware.fingerprint.FingerprintManagerCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -1182,6 +1186,41 @@ public class Utils {
             String tag, String apiName, Integer error, String errorMessage) {
         if (error != ProviderError.SUCCESS) {
             Log.d(tag, apiName + ": " + error + " - " + errorMessage);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    private static boolean canAuthenticate(BiometricManager biometricManager) {
+        assert biometricManager != null;
+
+        return biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_WEAK)
+                == BiometricManager.BIOMETRIC_SUCCESS;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    public static boolean isBiometricAvailable(Context context) {
+        // Only Android versions 9 and above are supported.
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
+            return false;
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            BiometricManager biometricManager = context.getSystemService(BiometricManager.class);
+            if (biometricManager == null) {
+                return false;
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                return Utils.canAuthenticate(biometricManager);
+            }
+
+            return biometricManager.canAuthenticate() == BiometricManager.BIOMETRIC_SUCCESS;
+        } else {
+            // For API level < Q, we will use FingerprintManagerCompat to check enrolled
+            // fingerprints. Note that for API level lower than 23, FingerprintManagerCompat behaves
+            // like no fingerprint hardware and no enrolled fingerprints.
+            FingerprintManagerCompat fingerprintManager = FingerprintManagerCompat.from(context);
+            return fingerprintManager != null && fingerprintManager.isHardwareDetected()
+                    && fingerprintManager.hasEnrolledFingerprints();
         }
     }
 }
