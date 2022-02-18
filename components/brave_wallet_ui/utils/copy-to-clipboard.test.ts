36efc32797f3902e4eb75fe7eca79c4c455f6390
William Muli <48117473+muliswilliam@users.noreply.github.com>
2022-02-18 11:17:36 +0300

    Added tests for copy-to-clipboard util

---
+++ components/brave_wallet_ui/utils/copy-to-clipboard.test.ts
@@ -0,0 +1,28 @@
+import { copyToClipboard } from './copy-to-clipboard'
+
+Object.assign(navigator, {
+  clipboard: {
+    writeText: (text: string) => {}
+  }
+})
+
+describe('copyToClipboard', () => {
+  it('should call nativator.clipboard.writeText', async (done) => {
+    const mockData = 'someText'
+    jest.spyOn(navigator.clipboard, 'writeText')
+    await copyToClipboard(mockData)
+    expect(navigator.clipboard.writeText).toHaveBeenCalledWith(mockData)
+
+    done()
+  })
+
+  it('should throw error if navigator.clipboard.writeText throws an error', async (done) => {
+    jest.spyOn(navigator.clipboard, 'writeText').mockImplementation(() => {
+      throw new Error()
+    })
+    await copyToClipboard('data')
+    expect(navigator.clipboard.writeText).toThrowError()
+
+    done()
+  })
+})
