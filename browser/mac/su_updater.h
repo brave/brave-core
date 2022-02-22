a29f563282fa09905b3c7cf7c90a30db894e3a53
Michael Herrmann <michael@herrmann.io>
2022-02-22 15:28:42 +0100

    Prevent the "Update available" Sparkle popup on Mac
    It was shown when Brave was run as non-admin user and tried to
    update a system-wide installation.

--- browser/mac/su_updater.h
+++ browser/mac/su_updater.h
@@ -1,4 +1,5 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
@@ -16,16 +17,19 @@
 
 @interface SUUpdater : NSObject
 
-@property (strong) SUUpdateDriver *driver;
+@property(strong) SUUpdateDriver* driver;  // NOLINT
 
-+ (SUUpdater *)sharedUpdater;
++ (SUUpdater*)sharedUpdater;  // NOLINT
 
 - (void)checkForUpdates:(id)sender;
 - (void)setDelegate:(id)delegate;
 - (void)setAutomaticallyChecksForUpdates:(BOOL)enable;
 - (void)setAutomaticallyDownloadsUpdates:(BOOL)enable;
 - (void)checkForUpdatesInBackground;
+- (void)checkForUpdatesInBackgroundWithoutUi;
 - (void)setUpdateCheckInterval:(NSTimeInterval)interval;
+
+@property BOOL automaticallyDownloadsUpdates;
 
 @end
 
