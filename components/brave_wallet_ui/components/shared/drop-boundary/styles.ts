b9068ae28fc7b415c9915c24a5d73dcf0216405c
Josh Leonard <jleonard@brave.com>
2022-02-23 08:32:41 -0700

    feat: phrase word sorting during verification

---
+++ components/brave_wallet_ui/components/shared/drop-boundary/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex: 1;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+`
---
+++ components/brave_wallet_ui/utils/random-utils.ts
@@ -0,0 +1,12 @@
+export function unbiasedRandom (min: number, max: number) {
+    const range = max - min + 1
+    const bytesNeeded = Math.ceil(Math.log2(range) / 8)
+    const cutoff = Math.floor((256 ** bytesNeeded) / range) * range
+    const bytes = new Uint8Array(bytesNeeded)
+    let value
+    do {
+        window.crypto.getRandomValues(bytes)
+        value = bytes.reduce((acc, x, n) => acc + x * 256 ** n, 0)
+    } while (value >= cutoff)
+    return min + value % range
+}
