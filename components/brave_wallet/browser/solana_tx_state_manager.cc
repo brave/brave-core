d5944d08d3e85031dbc7d7c77551a44b95568ae3
Jocelyn Liu <yrliou@gmail.com>
2022-03-03 22:01:03 -0800

    Implement SolanaTxStateManager

--- components/brave_wallet/browser/solana_tx_state_manager.cc
+++ components/brave_wallet/browser/solana_tx_state_manager.cc
@@ -5,7 +5,12 @@
 
 #include "brave/components/brave_wallet/browser/solana_tx_state_manager.h"
 
-#include "base/notreached.h"
+#include "base/strings/strcat.h"
+#include "base/values.h"
+#include "brave/components/brave_wallet/browser/brave_wallet_constants.h"
+#include "brave/components/brave_wallet/browser/brave_wallet_utils.h"
+#include "brave/components/brave_wallet/browser/json_rpc_service.h"
+#include "brave/components/brave_wallet/browser/solana_tx_meta.h"
 
 namespace brave_wallet {
 
@@ -15,15 +20,51 @@ SolanaTxStateManager::SolanaTxStateManager(PrefService* prefs,
 
 SolanaTxStateManager::~SolanaTxStateManager() = default;
 
+std::unique_ptr<SolanaTxMeta> SolanaTxStateManager::ValueToSolanaTxMeta(
+    const base::Value& value) {
+  return std::unique_ptr<SolanaTxMeta>{
+      static_cast<SolanaTxMeta*>(ValueToTxMeta(value).release())};
+}
+
 std::unique_ptr<TxMeta> SolanaTxStateManager::ValueToTxMeta(
     const base::Value& value) {
-  NOTIMPLEMENTED();
-  return nullptr;
+  std::unique_ptr<SolanaTxMeta> meta = std::make_unique<SolanaTxMeta>();
+
+  if (!TxStateManager::ValueToTxMeta(value, meta.get()))
+    return nullptr;
+
+  const base::Value* tx_value = value.FindKey("tx");
+  if (!tx_value)
+    return nullptr;
+  absl::optional<SolanaTransaction> tx =
+      SolanaTransaction::FromValue(*tx_value);
+  if (!tx)
+    return nullptr;
+  meta->set_tx(std::make_unique<SolanaTransaction>(*tx));
+
+  const base::Value* signature_status_value = value.FindKey("signature_status");
+  if (!signature_status_value)
+    return nullptr;
+  absl::optional<SolanaSignatureStatus> signature_status =
+      SolanaSignatureStatus::FromValue(*signature_status_value);
+  if (!signature_status)
+    return nullptr;
+  meta->set_signature_status(*signature_status);
+
+  return meta;
 }
 
 std::string SolanaTxStateManager::GetTxPrefPathPrefix() {
-  NOTIMPLEMENTED();
-  return "";
+  return base::StrCat(
+      {kSolanaPrefKey, ".",
+       GetNetworkId(prefs_, mojom::CoinType::SOL,
+                    json_rpc_service_->GetChainId(mojom::CoinType::SOL))});
+}
+
+std::unique_ptr<SolanaTxMeta> SolanaTxStateManager::GetSolanaTx(
+    const std::string& id) {
+  return std::unique_ptr<SolanaTxMeta>{
+      static_cast<SolanaTxMeta*>(TxStateManager::GetTx(id).release())};
 }
 
 }  // namespace brave_wallet
