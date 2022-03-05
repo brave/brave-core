dd006e76599edad635b6c21a72985bfc2d1e4a25
Josh Leonard <jleonard@brave.com>
2022-03-04 22:55:18 -0700

    fix: swap-sweepstakes banner redesign

---
+++ components/brave_wallet_ui/components/desktop/sweepstakes-banner/style.ts
@@ -0,0 +1,86 @@
+import styled from 'styled-components'
+export { CloseButton } from '../popup-modals/style'
+
+export const Card = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    background: #D8CBB2;
+    border-radius: 8px;
+    border: 2px solid #5B4F40;
+    padding-left: 16px;
+    padding-right: 8px;
+    margin-top: 16px;
+`
+
+const Row = styled.div`
+    display: flex;
+    flex: 1;
+    flex-direction: row;
+`
+
+export const TitleAndCloseRow = styled(Row)`
+    width: 100%;
+    justify-content: space-between;
+    padding-top: 8px;
+`
+
+export const Title = styled.h2`
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 26px;
+    margin-top: 4px;
+    margin-bottom: 4px;
+    word-wrap: no-wrap;
+    color: #212529;
+`
+
+export const Footer = Row
+
+const Column = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const BodyText = styled.span`
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 18px;
+    letter-spacing: 0.01em;
+    color: #212529;
+`
+
+export const FooterLeftColumn = styled(Column)`
+    flex: 1;
+`
+
+export const FooterRightColumn = styled(Column)`
+    align-items: flex-end;
+    justify-content: flex-end;
+    padding-bottom: 8px;
+`
+
+export const LearnMoreLink = styled.a`
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 20px;
+    text-decoration: none;
+    color: #5B4F40;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 40px;
+`
+
+export const SweepStakesBannerIllustration = styled.img`
+    width: 81px;
+    height: 74px;
+    margin-top: -8px;
+    align-self: flex-end;
+`
