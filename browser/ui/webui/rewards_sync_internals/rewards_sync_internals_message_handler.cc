c627cdcceaed05614ebb64ac7b4f14e6130eeef0
Szilard Szaloki <szilardszaloki@gmail.com>
2022-03-08 14:39:02 +0100

    brave://rewards-sync-internals

---
+++ browser/ui/webui/rewards_sync_internals/rewards_sync_internals_message_handler.cc
@@ -0,0 +1,16 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/browser/ui/webui/rewards_sync_internals/rewards_sync_internals_message_handler.h"
+
+#include "brave/browser/brave_rewards/rewards_sync_service_factory.h"
+#include "chrome/browser/sync/sync_service_factory.h"
+#include "chrome/browser/ui/webui/sync_internals/sync_internals_message_handler.h"
+
+#define SyncInternalsMessageHandler RewardsSyncInternalsMessageHandler
+#define SyncServiceFactory RewardsSyncServiceFactory
+#include "chrome/browser/ui/webui/sync_internals/sync_internals_message_handler.cc"
+#undef SyncServiceFactory
+#undef SyncInternalsMessageHandler
