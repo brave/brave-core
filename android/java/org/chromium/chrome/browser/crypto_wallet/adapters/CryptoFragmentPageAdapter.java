2daa7032a3e3f57cb62a33b27348dd14664c0ac2
wchen342 <feiyu2817@gmail.com>
2022-03-08 18:47:12 +0200

    Add pending transaction notification button

--- android/java/org/chromium/chrome/browser/crypto_wallet/adapters/CryptoFragmentPageAdapter.java
+++ android/java/org/chromium/chrome/browser/crypto_wallet/adapters/CryptoFragmentPageAdapter.java
@@ -27,6 +27,8 @@ public class CryptoFragmentPageAdapter extends FragmentStatePagerAdapter {
     private final List<String> mTitles =
             new ArrayList<>(Arrays.asList(mContext.getResources().getString(R.string.portfolio),
                     mContext.getResources().getString(R.string.accounts)));
+    private PortfolioFragment mCurrentPortfolioFragment;
+
     public CryptoFragmentPageAdapter(FragmentManager fm) {
         super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
     }
@@ -37,7 +39,8 @@ public class CryptoFragmentPageAdapter extends FragmentStatePagerAdapter {
         if (position == 1) {
             return AccountsFragment.newInstance();
         } else {
-            return PortfolioFragment.newInstance();
+            mCurrentPortfolioFragment = PortfolioFragment.newInstance();
+            return mCurrentPortfolioFragment;
         }
     }
 
@@ -50,5 +53,9 @@ public class CryptoFragmentPageAdapter extends FragmentStatePagerAdapter {
     @Override
     public CharSequence getPageTitle(int position) {
         return mTitles.get(position);
+    }
+
+    public PortfolioFragment getCurrentPortfolioFragment() {
+        return mCurrentPortfolioFragment;
     }
 }
