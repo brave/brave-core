92c133d8c915a1963075d5495bf71c982176683b
Simon Hong <shong@brave.com>
2022-03-11 16:44:41 +0900

    Adjust sidebar colors
    fix https://github.com/brave/brave-browser/issues/21590
    
    So far, sidebar can't aligned with custom theme
    because sidebar only knows brave's dark/light theme color.
    
    With below changes, sidebar colors are aligned well with custom theme also.
    
    Used toolbar color as a sidebar background color.
    Used toolbar button's hover color for sidebar item/arrow button hover color.
    Picked proper color between light/dark color for builtin item icon color based on toolbar color.
    Used toolbar area separator color(separator between toolbar and web contents) for sidebar border.

--- browser/themes/brave_theme_helper.cc
+++ browser/themes/brave_theme_helper.cc
@@ -8,9 +8,11 @@
 #include "base/numerics/safe_conversions.h"
 #include "brave/browser/themes/brave_dark_mode_utils.h"
 #include "brave/browser/themes/theme_properties.h"
+#include "brave/components/sidebar/buildflags/buildflags.h"
 #include "chrome/browser/themes/theme_properties.h"
 #include "chrome/browser/ui/omnibox/omnibox_theme.h"
 #include "ui/gfx/color_palette.h"
+#include "ui/gfx/color_utils.h"
 #include "ui/native_theme/native_theme.h"
 
 #if defined(OS_LINUX)
@@ -104,6 +106,43 @@ SkColor BraveThemeHelper::GetDefaultColor(
   if (!incognito && (is_tor_ || is_guest_)) {
     incognito = true;
   }
+
+#if BUILDFLAG(ENABLE_SIDEBAR)
+  switch (id) {
+    // Pick most contrast color between our light and dark colors based on
+    // current toolbar color.
+    case BraveThemeProperties::COLOR_SIDEBAR_ITEM_DRAG_INDICATOR_COLOR:
+    case BraveThemeProperties::COLOR_SIDEBAR_ADD_BUTTON_DISABLED:
+    case BraveThemeProperties::COLOR_SIDEBAR_BUTTON_BASE:
+    case BraveThemeProperties::COLOR_SIDEBAR_ARROW_NORMAL:
+    case BraveThemeProperties::COLOR_SIDEBAR_ARROW_DISABLED: {
+      const auto toolbar_color =
+          GetColor(ThemeProperties::COLOR_TOOLBAR, incognito, theme_supplier);
+      const auto base_button_color_light = MaybeGetDefaultColorForBraveUi(
+          id, incognito, is_tor_,
+          dark_mode::BraveDarkModeType::BRAVE_DARK_MODE_TYPE_LIGHT);
+      const auto base_button_color_dark = MaybeGetDefaultColorForBraveUi(
+          id, incognito, is_tor_,
+          dark_mode::BraveDarkModeType::BRAVE_DARK_MODE_TYPE_DARK);
+      DCHECK(base_button_color_light && base_button_color_dark);
+      return color_utils::PickContrastingColor(base_button_color_light.value(),
+                                               base_button_color_dark.value(),
+                                               toolbar_color);
+    }
+    case BraveThemeProperties::COLOR_SIDEBAR_ARROW_BACKGROUND_HOVERED:
+    case BraveThemeProperties::COLOR_SIDEBAR_ITEM_BACKGROUND_HOVERED: {
+      // Copied from chrome/browser/ui/views/toolbar/toolbar_ink_drop_util.h
+      // to use same hover background with toolbar button.
+      constexpr float kToolbarInkDropHighlightVisibleOpacity = 0.08f;
+      return SkColorSetA(GetColor(ThemeProperties::COLOR_TOOLBAR_INK_DROP,
+                                  incognito, theme_supplier),
+                         0xFF * kToolbarInkDropHighlightVisibleOpacity);
+    }
+    default:
+      break;
+  }
+#endif
+
   const dark_mode::BraveDarkModeType type =
       dark_mode::GetActiveBraveDarkModeType();
   const absl::optional<SkColor> braveColor =
