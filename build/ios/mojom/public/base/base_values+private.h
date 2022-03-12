020e2c04f8749fb6afa6e1e4058eb1b9622a8d49
Kyle Hickinson <git@kylehickinson.com>
2022-03-11 19:18:43 -0500

    [iOS] Add support for mojo base values

--- build/ios/mojom/public/base/base_values+private.h
+++ build/ios/mojom/public/base/base_values+private.h
@@ -3,10 +3,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#import <Foundation/Foundation.h>
+#ifndef BRAVE_BUILD_IOS_MOJOM_PUBLIC_BASE_BASE_VALUES_PRIVATE_H_
+#define BRAVE_BUILD_IOS_MOJOM_PUBLIC_BASE_BASE_VALUES_PRIVATE_H_
 
-#ifndef BRAVE_BUILD_IOS_MOJOM_UTIL_BASE_VALUE_UTIL_H_
-#define BRAVE_BUILD_IOS_MOJOM_UTIL_BASE_VALUE_UTIL_H_
+#include "brave/build/ios/mojom/public/base/base_values.h"
+
+@class MojoBaseValue;
 
 namespace base {
 class Value;
@@ -17,21 +19,27 @@ namespace brave {
 // Clone the contents of a `base::Value` whos type is `base::Value::Type::LIST`
 // into an Obj-C NSArray container. Any types found within the `base::Value`
 // that are unsupported or `NONE` will become `NSNull`
-NSArray* NSArrayFromBaseValue(const base::Value);
+NSArray<MojoBaseValue*>* NSArrayFromBaseValue(const base::Value);
 
 // Clone the contents of a `base::Value` whos type is
 // `base::Value::Type::DICTIONARY`.  Any types found within the `base::Value`
 // that are unsupported or `NONE` will become `NSNull`
-NSDictionary<NSString*, id>* NSDictionaryFromBaseValue(const base::Value);
+NSDictionary<NSString*, MojoBaseValue*>* NSDictionaryFromBaseValue(
+    const base::Value);
 
 // Clone the contents of an NSArray into a `base::Value` with the type
 // `base::Value::Type::LIST`
-base::Value BaseValueFromNSArray(NSArray*);
+base::Value BaseValueFromNSArray(NSArray<MojoBaseValue*>*);
 
 // Clone the contents of an NSDictionary into a `base::Value` with the type
 // `base::Value::Type::DICTIONARY`
-base::Value BaseValueFromNSDictionary(NSDictionary<NSString*, id>*);
+base::Value BaseValueFromNSDictionary(NSDictionary<NSString*, MojoBaseValue*>*);
 
 }  // namespace brave
 
-#endif  // BRAVE_BUILD_IOS_MOJOM_UTIL_BASE_VALUE_UTIL_H_
+@interface MojoBaseValue (Private)
+- (instancetype)initWithValue:(const base::Value)value;
+- (base::Value)cppObjPtr;
+@end
+
+#endif  // BRAVE_BUILD_IOS_MOJOM_PUBLIC_BASE_BASE_VALUES_PRIVATE_H_
