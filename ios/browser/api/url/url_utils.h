1b99be7926104df08c08675baa4808c062015ef5
Brandon T <JustBrandonT@gmail.com>
2022-03-16 15:24:07 -0400

    Addressed feedback

--- ios/browser/api/url/url_utils.h
+++ ios/browser/api/url/url_utils.h
@@ -1,4 +1,3 @@
-
 /* Copyright (c) 2021 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
@@ -20,8 +19,8 @@ OBJC_EXPORT
 
 /// Returns the eTLD+1 not including private registries.
 @property(readonly, nullable)
-    NSString* brave_baseDomainExcludingPrivateRegistries NS_SWIFT_NAME(
-        baseDomainExcludingPrivateRegistries);
+    NSString* brave_domainAndRegistryExcludingPrivateRegistries NS_SWIFT_NAME(
+        domainAndRegistryExcludingPrivateRegistries);
 
 /// Returns `true` when the `host` is an IP address.
 @property(readonly) bool brave_isHostIPAddress NS_SWIFT_NAME(isHostIPAddress);
@@ -31,16 +30,21 @@ OBJC_EXPORT
 @property(readonly) NSString* brave_spec NS_SWIFT_NAME(spec);
 
 /// Appends the key=value query parameter, escaping if needed.
-- (NSURL*)addingQueryParameter:(NSString*)key value:(NSString*)value;
+- (NSURL*)brave_addingQueryParameter:(NSString*)key
+                               value:(NSString*)value
+    NS_SWIFT_NAME(addingQueryParameter(key:value:));
 
 /// Appends OR Replaces the key=value query parameter, escaping if needed.
-- (NSURL*)replacingQueryParameter:(NSString*)key value:(NSString*)value;
+- (NSURL*)brave_replacingQueryParameter:(NSString*)key
+                                  value:(NSString*)value
+    NS_SWIFT_NAME(replacingQueryParameter(key:value:));
 
 /// Returns the value for the query parameter. NULL if no parameter exists.
-- (nullable NSString*)valueForQueryParameter:(NSString*)key;
+- (nullable NSString*)brave_valueForQueryParameter:(NSString*)key
+    NS_SWIFT_NAME(valueForQueryParameter(key:));
 
 /// Returns true if the URL has a scheme.
-- (bool)hasScheme:(NSString*)scheme;
+- (bool)brave_hasScheme:(NSString*)scheme NS_SWIFT_NAME(hasScheme(scheme:));
 @end
 
 NS_ASSUME_NONNULL_END
