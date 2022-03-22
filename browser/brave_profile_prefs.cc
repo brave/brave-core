5da8720666fc417164a3c4f8febb5965ed1af521
samartnik <artem@brave.com>
2022-03-22 15:47:55 -0400

    [Android] Added registering feed prefs
    Registering of those prefs was moved under ENABLE_FEED_V2 guards and
    causes pref not registered issues.
    
    Chromium change:
    https://github.com/chromium/chromium/commit/c9a227cf13e3e32338dc0dfe901f64aac094bf9b
    
    Add menu item to follow a site for desktop feed
    Also move some files out of android directory so that they can be
    used for desktop version.
    
    Bug: 1292559

--- browser/brave_profile_prefs.cc
+++ browser/brave_profile_prefs.cc
@@ -117,6 +117,7 @@
 #endif
 
 #if BUILDFLAG(IS_ANDROID)
+#include "components/feed/core/common/pref_names.h"
 #include "components/feed/core/shared_prefs/pref_names.h"
 #include "components/ntp_tiles/pref_names.h"
 #include "components/translate/core/browser/translate_pref_names.h"
@@ -256,10 +257,9 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   registry->SetDefaultPrefValue(ntp_tiles::prefs::kPopularSitesJsonPref,
                                 base::Value(base::Value::Type::LIST));
   // Disable NTP suggestions
-  registry->SetDefaultPrefValue(feed::prefs::kEnableSnippets,
-                                base::Value(false));
-  registry->SetDefaultPrefValue(feed::prefs::kArticlesListVisible,
-                                base::Value(false));
+  feed::RegisterProfilePrefs(registry);
+  registry->RegisterBooleanPref(feed::prefs::kEnableSnippets, false);
+  registry->RegisterBooleanPref(feed::prefs::kArticlesListVisible, false);
   // Translate is not available on Android
   registry->SetDefaultPrefValue(translate::prefs::kOfferTranslateEnabled,
                                 base::Value(false));
