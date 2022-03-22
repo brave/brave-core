ffb4a13308e6ebe0c8af573edab9f32f69406a71
Mario Sanchez Prada <mario@igalia.com>
2022-03-22 13:53:52 +0100

    Add missing include

--- components/brave_rewards/browser/service_sandbox_type.h
+++ components/brave_rewards/browser/service_sandbox_type.h
@@ -6,6 +6,7 @@
 #ifndef BRAVE_COMPONENTS_BRAVE_REWARDS_BROWSER_SERVICE_SANDBOX_TYPE_H_
 #define BRAVE_COMPONENTS_BRAVE_REWARDS_BROWSER_SERVICE_SANDBOX_TYPE_H_
 
+#include "build/build_config.h"
 #include "content/public/browser/service_process_host.h"
 
 namespace bat_ledger {
