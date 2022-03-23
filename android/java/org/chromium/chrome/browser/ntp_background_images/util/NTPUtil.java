c2337d5b9c0ab8150df22b0a9009273bf0d336b2
tapanmodh <modhtapan@gmail.com>
2022-03-23 10:39:22 +0530

    fixed ntp image credit and si logo cut off in small screens

--- android/java/org/chromium/chrome/browser/ntp_background_images/util/NTPUtil.java
+++ android/java/org/chromium/chrome/browser/ntp_background_images/util/NTPUtil.java
@@ -113,9 +113,7 @@ public class NTPUtil {
                 if (!isTablet) {
                     imageCreditCorrection = isLandscape
                             ? (int) (pxHeight * (isCompensate ? 0.12 : 0.88))
-                            : (int) (pxHeight
-                                    - dpToPx(activity,
-                                            355));
+                            : (int) (pxHeight - dpToPx(activity, 355));
                 }
                 if (!ChromeFeatureList.isEnabled(BraveFeatureList.BRAVE_NEWS)) {
                     imageCreditCorrection = (int) imageCreditCorrection - (int) (pxHeight * 0.04);
@@ -124,9 +122,7 @@ public class NTPUtil {
                 if (!isTablet) {
                     imageCreditCorrection = isLandscape
                             ? (int) (pxHeight * (isCompensate ? 0.02 : 0.98))
-                            : (int) (pxHeight
-                                    - dpToPx(activity,
-                                            480));
+                            : (int) (pxHeight - dpToPx(activity, 480));
                 } else {
                     imageCreditCorrection = isLandscape
                             ? (int) (pxHeight * (isCompensate ? 0.28 : 0.72))
