7f1ab6c4bb7818d6f94368b2590d06a7f082b2c6
Sergey P <spylogsster@gmail.com>
2022-03-23 10:07:52 +0300

    Renamings and small nits according to review comments

--- components/json/json_parser_unittest.cc
+++ components/json/json_parser_unittest.cc
@@ -10,266 +10,262 @@ namespace brave_wallet {
 
 TEST(JsonParser, ConvertUint64ToString) {
   std::string json = "{\"a\": " + std::to_string(UINT64_MAX) + "}";
-  EXPECT_EQ(std::string(json::convert_uint64_to_unicode_string("/a", json)),
+  EXPECT_EQ(std::string(json::convert_uint64_value_to_string("/a", json)),
             R"({"a":"18446744073709551615"})");
 
   // UINT64_MAX + 1
   json = "{\"a\": 18446744073709551616 }";
   EXPECT_TRUE(
-      std::string(json::convert_uint64_to_unicode_string("/a", json)).empty());
+      std::string(json::convert_uint64_value_to_string("/a", json)).empty());
 
   // INT64_MIN
   json = "{\"a\": " + std::to_string(INT64_MIN) + "}";
   EXPECT_TRUE(
-      std::string(json::convert_uint64_to_unicode_string("/a", json)).empty());
+      std::string(json::convert_uint64_value_to_string("/a", json)).empty());
 
   json = R"({"a": { "b": 1, "c": 2 }, "d": "string"})";
-  EXPECT_EQ(std::string(json::convert_uint64_to_unicode_string("/a/b", json)),
+  EXPECT_EQ(std::string(json::convert_uint64_value_to_string("/a/b", json)),
             R"({"a":{"b":"1","c":2},"d":"string"})");
 
   json = R"({"a": { "b": [{"e":1}], "c": 2 }, "d": "string"})";
-  EXPECT_EQ(
-      std::string(json::convert_uint64_to_unicode_string("/a/b/0/e", json)),
-      R"({"a":{"b":[{"e":"1"}],"c":2},"d":"string"})");
+  EXPECT_EQ(std::string(json::convert_uint64_value_to_string("/a/b/0/e", json)),
+            R"({"a":{"b":[{"e":"1"}],"c":2},"d":"string"})");
 
   json = R"({"a":[{"b":1}]})";
-  EXPECT_EQ(std::string(json::convert_uint64_to_unicode_string("/a/0/b", json)),
+  EXPECT_EQ(std::string(json::convert_uint64_value_to_string("/a/0/b", json)),
             R"({"a":[{"b":"1"}]})");
 
   json = R"({"a":[1]})";
-  EXPECT_EQ(std::string(json::convert_uint64_to_unicode_string("/a/0", json)),
+  EXPECT_EQ(std::string(json::convert_uint64_value_to_string("/a/0", json)),
             R"({"a":["1"]})");
 
   json = R"({"a": 0 })";
-  EXPECT_EQ(std::string(json::convert_uint64_to_unicode_string("/a", json)),
+  EXPECT_EQ(std::string(json::convert_uint64_value_to_string("/a", json)),
             R"({"a":"0"})");
 
   json = R"({"a": 0.1 })";
   EXPECT_TRUE(
-      std::string(json::convert_uint64_to_unicode_string("/a", json)).empty());
+      std::string(json::convert_uint64_value_to_string("/a", json)).empty());
   json = R"({"a": "1" })";
   EXPECT_TRUE(
-      std::string(json::convert_uint64_to_unicode_string("/a", json)).empty());
+      std::string(json::convert_uint64_value_to_string("/a", json)).empty());
   json = R"({"a": "" })";
   EXPECT_TRUE(
-      std::string(json::convert_uint64_to_unicode_string("/a", json)).empty());
+      std::string(json::convert_uint64_value_to_string("/a", json)).empty());
   json = R"({"a": -1.0 })";
   EXPECT_TRUE(
-      std::string(json::convert_uint64_to_unicode_string("/a", json)).empty());
+      std::string(json::convert_uint64_value_to_string("/a", json)).empty());
   json = R"({"a": "a" })";
   EXPECT_TRUE(
-      std::string(json::convert_uint64_to_unicode_string("/a", json)).empty());
+      std::string(json::convert_uint64_value_to_string("/a", json)).empty());
   json = R"({"b": 1 })";
   EXPECT_TRUE(
-      std::string(json::convert_uint64_to_unicode_string("/a", json)).empty());
+      std::string(json::convert_uint64_value_to_string("/a", json)).empty());
   json = R"({"a": [] })";
   EXPECT_TRUE(
-      std::string(json::convert_uint64_to_unicode_string("/a", json)).empty());
+      std::string(json::convert_uint64_value_to_string("/a", json)).empty());
   json = R"({"a": {} })";
   EXPECT_TRUE(
-      std::string(json::convert_uint64_to_unicode_string("/a", json)).empty());
+      std::string(json::convert_uint64_value_to_string("/a", json)).empty());
 }
 
 TEST(JsonParser, ConvertInt64ToString) {
   std::string json = "{\"a\": 18446744073709551615 }";
   EXPECT_TRUE(
-      std::string(json::convert_int64_to_unicode_string("/a", json)).empty());
+      std::string(json::convert_int64_value_to_string("/a", json)).empty());
 
   // INT64_MIN
   json = "{\"a\": " + std::to_string(INT64_MIN) + "}";
-  EXPECT_EQ(std::string(json::convert_int64_to_unicode_string("/a", json)),
+  EXPECT_EQ(std::string(json::convert_int64_value_to_string("/a", json)),
             R"({"a":"-9223372036854775808"})");
 
   // INT64_MIN - 1
   json = "{\"a\": -9223372036854775809 }";
   EXPECT_TRUE(
-      std::string(json::convert_int64_to_unicode_string("/a", json)).empty());
+      std::string(json::convert_int64_value_to_string("/a", json)).empty());
 
   // INT64_MAX
   json = "{\"a\": " + std::to_string(INT64_MAX) + "}";
-  EXPECT_EQ(std::string(json::convert_int64_to_unicode_string("/a", json)),
+  EXPECT_EQ(std::string(json::convert_int64_value_to_string("/a", json)),
             R"({"a":"9223372036854775807"})");
 
   json = R"({"a": { "b": 1, "c": 2 }, "d": "string"})";
-  EXPECT_EQ(std::string(json::convert_int64_to_unicode_string("/a/b", json)),
+  EXPECT_EQ(std::string(json::convert_int64_value_to_string("/a/b", json)),
             R"({"a":{"b":"1","c":2},"d":"string"})");
 
   json = R"({"a": { "b": [{"e":1}], "c": 2 }, "d": "string"})";
-  EXPECT_EQ(
-      std::string(json::convert_int64_to_unicode_string("/a/b/0/e", json)),
-      R"({"a":{"b":[{"e":"1"}],"c":2},"d":"string"})");
+  EXPECT_EQ(std::string(json::convert_int64_value_to_string("/a/b/0/e", json)),
+            R"({"a":{"b":[{"e":"1"}],"c":2},"d":"string"})");
 
   json = R"({"a":[{"b":1}]})";
-  EXPECT_EQ(std::string(json::convert_int64_to_unicode_string("/a/0/b", json)),
+  EXPECT_EQ(std::string(json::convert_int64_value_to_string("/a/0/b", json)),
             R"({"a":[{"b":"1"}]})");
 
   json = R"({"a":[1]})";
-  EXPECT_EQ(std::string(json::convert_int64_to_unicode_string("/a/0", json)),
+  EXPECT_EQ(std::string(json::convert_int64_value_to_string("/a/0", json)),
             R"({"a":["1"]})");
 
   json = R"({"a": 0 })";
-  EXPECT_EQ(std::string(json::convert_int64_to_unicode_string("/a", json)),
+  EXPECT_EQ(std::string(json::convert_int64_value_to_string("/a", json)),
             R"({"a":"0"})");
 
   json = R"({"a": 0.1 })";
   EXPECT_TRUE(
-      std::string(json::convert_int64_to_unicode_string("/a", json)).empty());
+      std::string(json::convert_int64_value_to_string("/a", json)).empty());
   json = R"({"a": "1" })";
   EXPECT_TRUE(
-      std::string(json::convert_int64_to_unicode_string("/a", json)).empty());
+      std::string(json::convert_int64_value_to_string("/a", json)).empty());
   json = R"({"a": "" })";
   EXPECT_TRUE(
-      std::string(json::convert_int64_to_unicode_string("/a", json)).empty());
+      std::string(json::convert_int64_value_to_string("/a", json)).empty());
   json = R"({"a": -1.0 })";
   EXPECT_TRUE(
-      std::string(json::convert_int64_to_unicode_string("/a", json)).empty());
+      std::string(json::convert_int64_value_to_string("/a", json)).empty());
   json = R"({"a": "a" })";
   EXPECT_TRUE(
-      std::string(json::convert_int64_to_unicode_string("/a", json)).empty());
+      std::string(json::convert_int64_value_to_string("/a", json)).empty());
   json = R"({"b": 1 })";
   EXPECT_TRUE(
-      std::string(json::convert_int64_to_unicode_string("/a", json)).empty());
+      std::string(json::convert_int64_value_to_string("/a", json)).empty());
   json = R"({"a": [] })";
   EXPECT_TRUE(
-      std::string(json::convert_int64_to_unicode_string("/a", json)).empty());
+      std::string(json::convert_int64_value_to_string("/a", json)).empty());
   json = R"({"a": {} })";
   EXPECT_TRUE(
-      std::string(json::convert_int64_to_unicode_string("/a", json)).empty());
+      std::string(json::convert_int64_value_to_string("/a", json)).empty());
 }
 
 TEST(JsonParser, ConvertStringToUint64) {
   // UINT64_MAX
   std::string json = R"({"a":"18446744073709551615"})";
-  EXPECT_EQ(std::string(json::convert_unicode_string_to_uint64("/a", json)),
+  EXPECT_EQ(std::string(json::convert_string_value_to_uint64("/a", json)),
             R"({"a":18446744073709551615})");
 
   json = R"({"a":"-1"})";
   EXPECT_TRUE(
-      std::string(json::convert_unicode_string_to_uint64("/a", json)).empty());
+      std::string(json::convert_string_value_to_uint64("/a", json)).empty());
   // UINT64_MAX + 1
   json = R"({\"a\":18446744073709551616})";
   EXPECT_TRUE(
-      std::string(json::convert_unicode_string_to_uint64("/a", json)).empty());
+      std::string(json::convert_string_value_to_uint64("/a", json)).empty());
 
   json = R"({"a": { "b": "1", "c": 2 }, "d": "string"})";
-  EXPECT_EQ(std::string(json::convert_unicode_string_to_uint64("/a/b", json)),
+  EXPECT_EQ(std::string(json::convert_string_value_to_uint64("/a/b", json)),
             R"({"a":{"b":1,"c":2},"d":"string"})");
 
   json = R"({"a": { "b": [{"e":"1"}], "c": 2 }, "d": "string"})";
-  EXPECT_EQ(
-      std::string(json::convert_unicode_string_to_uint64("/a/b/0/e", json)),
-      R"({"a":{"b":[{"e":1}],"c":2},"d":"string"})");
+  EXPECT_EQ(std::string(json::convert_string_value_to_uint64("/a/b/0/e", json)),
+            R"({"a":{"b":[{"e":1}],"c":2},"d":"string"})");
 
   json = R"({"a": { "b": "1" }})";
-  EXPECT_EQ(std::string(json::convert_unicode_string_to_uint64("/a/b", json)),
+  EXPECT_EQ(std::string(json::convert_string_value_to_uint64("/a/b", json)),
             R"({"a":{"b":1}})");
 
   json = R"({"a":[{"b":"1"}]})";
-  EXPECT_EQ(std::string(json::convert_unicode_string_to_uint64("/a/0/b", json)),
+  EXPECT_EQ(std::string(json::convert_string_value_to_uint64("/a/0/b", json)),
             R"({"a":[{"b":1}]})");
 
   json = R"({"a":["1"]})";
-  EXPECT_EQ(std::string(json::convert_unicode_string_to_uint64("/a/0", json)),
+  EXPECT_EQ(std::string(json::convert_string_value_to_uint64("/a/0", json)),
             R"({"a":[1]})");
 
   json = R"({"a": "0" })";
-  EXPECT_EQ(std::string(json::convert_unicode_string_to_uint64("/a", json)),
+  EXPECT_EQ(std::string(json::convert_string_value_to_uint64("/a", json)),
             R"({"a":0})");
 
   json = R"({"a": 1 })";
   EXPECT_TRUE(
-      std::string(json::convert_unicode_string_to_uint64("/a", json)).empty());
+      std::string(json::convert_string_value_to_uint64("/a", json)).empty());
   json = R"({"a": 0.1 })";
   EXPECT_TRUE(
-      std::string(json::convert_unicode_string_to_uint64("/a", json)).empty());
+      std::string(json::convert_string_value_to_uint64("/a", json)).empty());
   json = R"({"a": "" })";
   EXPECT_TRUE(
-      std::string(json::convert_unicode_string_to_uint64("/a", json)).empty());
+      std::string(json::convert_string_value_to_uint64("/a", json)).empty());
   json = R"({"a": -1.0 })";
   EXPECT_TRUE(
-      std::string(json::convert_unicode_string_to_uint64("/a", json)).empty());
+      std::string(json::convert_string_value_to_uint64("/a", json)).empty());
   json = R"({"a": "a" })";
   EXPECT_TRUE(
-      std::string(json::convert_unicode_string_to_uint64("/a", json)).empty());
+      std::string(json::convert_string_value_to_uint64("/a", json)).empty());
   json = R"({"b": 1 })";
   EXPECT_TRUE(
-      std::string(json::convert_unicode_string_to_uint64("/a", json)).empty());
+      std::string(json::convert_string_value_to_uint64("/a", json)).empty());
   json = R"({"a": [] })";
   EXPECT_TRUE(
-      std::string(json::convert_unicode_string_to_uint64("/a", json)).empty());
+      std::string(json::convert_string_value_to_uint64("/a", json)).empty());
   json = R"({"a": {} })";
   EXPECT_TRUE(
-      std::string(json::convert_unicode_string_to_uint64("/a", json)).empty());
+      std::string(json::convert_string_value_to_uint64("/a", json)).empty());
 }
 
 TEST(JsonParser, ConvertStringToInt64) {
   // INT64_MIN
   std::string json = R"({"a":"-9223372036854775808"})";
-  EXPECT_EQ(std::string(json::convert_unicode_string_to_int64("/a", json)),
+  EXPECT_EQ(std::string(json::convert_string_value_to_int64("/a", json)),
             R"({"a":-9223372036854775808})");
   // INT64_MIN - 1
   json = R"({"a":"-9223372036854775809"})";
   EXPECT_TRUE(
-      std::string(json::convert_unicode_string_to_int64("/a", json)).empty());
+      std::string(json::convert_string_value_to_int64("/a", json)).empty());
 
   // INT64_MAX
   json = "{\"a\": \"" + std::to_string(INT64_MAX) + "\"}";
-  EXPECT_EQ(std::string(json::convert_unicode_string_to_int64("/a", json)),
+  EXPECT_EQ(std::string(json::convert_string_value_to_int64("/a", json)),
             R"({"a":9223372036854775807})");
   // INT64_MAX + 1
   json = "{\"a\": \"9223372036854775808\"}";
   EXPECT_TRUE(
-      std::string(json::convert_unicode_string_to_int64("/a", json)).empty());
+      std::string(json::convert_string_value_to_int64("/a", json)).empty());
 
   json = R"({"a": { "b": "1", "c": 2 }, "d": "string"})";
-  EXPECT_EQ(std::string(json::convert_unicode_string_to_int64("/a/b", json)),
+  EXPECT_EQ(std::string(json::convert_string_value_to_int64("/a/b", json)),
             R"({"a":{"b":1,"c":2},"d":"string"})");
 
   json = R"({"a": { "b": [{"e":"1"}], "c": 2 }, "d": "string"})";
-  EXPECT_EQ(
-      std::string(json::convert_unicode_string_to_int64("/a/b/0/e", json)),
-      R"({"a":{"b":[{"e":1}],"c":2},"d":"string"})");
+  EXPECT_EQ(std::string(json::convert_string_value_to_int64("/a/b/0/e", json)),
+            R"({"a":{"b":[{"e":1}],"c":2},"d":"string"})");
 
   json = R"({"a": { "b": "1" }})";
-  EXPECT_EQ(std::string(json::convert_unicode_string_to_int64("/a/b", json)),
+  EXPECT_EQ(std::string(json::convert_string_value_to_int64("/a/b", json)),
             R"({"a":{"b":1}})");
 
   json = R"({"a":[{"b":"1"}]})";
-  EXPECT_EQ(std::string(json::convert_unicode_string_to_int64("/a/0/b", json)),
+  EXPECT_EQ(std::string(json::convert_string_value_to_int64("/a/0/b", json)),
             R"({"a":[{"b":1}]})");
 
   json = R"({"a":["1"]})";
-  EXPECT_EQ(std::string(json::convert_unicode_string_to_int64("/a/0", json)),
+  EXPECT_EQ(std::string(json::convert_string_value_to_int64("/a/0", json)),
             R"({"a":[1]})");
 
   json = R"({"a": "0" })";
-  EXPECT_EQ(std::string(json::convert_unicode_string_to_int64("/a", json)),
+  EXPECT_EQ(std::string(json::convert_string_value_to_int64("/a", json)),
             R"({"a":0})");
 
   json = R"({"a": 1 })";
   EXPECT_TRUE(
-      std::string(json::convert_unicode_string_to_int64("/a", json)).empty());
+      std::string(json::convert_string_value_to_int64("/a", json)).empty());
   json = R"({"a": 0.1 })";
   EXPECT_TRUE(
-      std::string(json::convert_unicode_string_to_int64("/a", json)).empty());
+      std::string(json::convert_string_value_to_int64("/a", json)).empty());
   json = R"({"a": "" })";
   EXPECT_TRUE(
-      std::string(json::convert_unicode_string_to_int64("/a", json)).empty());
+      std::string(json::convert_string_value_to_int64("/a", json)).empty());
   json = R"({"a": -1.0 })";
   EXPECT_TRUE(
-      std::string(json::convert_unicode_string_to_int64("/a", json)).empty());
+      std::string(json::convert_string_value_to_int64("/a", json)).empty());
   json = R"({"a": "a" })";
   EXPECT_TRUE(
-      std::string(json::convert_unicode_string_to_int64("/a", json)).empty());
+      std::string(json::convert_string_value_to_int64("/a", json)).empty());
   json = R"({"b": 1 })";
   EXPECT_TRUE(
-      std::string(json::convert_unicode_string_to_int64("/a", json)).empty());
+      std::string(json::convert_string_value_to_int64("/a", json)).empty());
   json = R"({"a": [] })";
   EXPECT_TRUE(
-      std::string(json::convert_unicode_string_to_int64("/a", json)).empty());
+      std::string(json::convert_string_value_to_int64("/a", json)).empty());
   json = R"({"a": {} })";
   EXPECT_TRUE(
-      std::string(json::convert_unicode_string_to_int64("/a", json)).empty());
+      std::string(json::convert_string_value_to_int64("/a", json)).empty());
 }
 
 }  // namespace brave_wallet
--- components/json/rs/src/lib.rs
+++ components/json/rs/src/lib.rs
@@ -1,128 +1,121 @@
 #[cxx::bridge(namespace = json)]
 mod ffi {
     extern "Rust" {
-      fn convert_uint64_to_unicode_string(path: &str, json: &str) -> String;
-      fn convert_int64_to_unicode_string(path: &str, json: &str) -> String;
-      fn convert_unicode_string_to_uint64(path: &str, json: &str) -> String;
-      fn convert_unicode_string_to_int64(path: &str, json: &str) -> String;
+        fn convert_uint64_value_to_string(path: &str, json: &str) -> String;
+        fn convert_int64_value_to_string(path: &str, json: &str) -> String;
+        fn convert_string_value_to_uint64(path: &str, json: &str) -> String;
+        fn convert_string_value_to_int64(path: &str, json: &str) -> String;
     }
 }
 
-// Converts uint64 value to string and serializes json with new values.
-// A path is a Unicode string with the reference tokens separated by /. Inside tokens / is replaced by ~1 and ~ is replaced by ~0.
-// Examples: convert_uint64_to_unicode_string("/a/b", json) for { a : { b : 1 }}
-//           convert_uint64_to_unicode_string("/a/0", json) for { a : [ 1 ]}
-pub fn convert_uint64_to_unicode_string(path: &str, json: &str) -> String {
-  let mut unwrapped_value: serde_json::Value = serde_json::from_str(&json).unwrap_or_else(|e| {
-    eprintln!("Failed to parse JSON: {}", e);
-    return serde_json::Value::Null;
-  });
-  if unwrapped_value.is_null() {
-    return String::new();
-  }
-  let mutable_pointer = unwrapped_value.pointer_mut(path);
-  if mutable_pointer.is_none() {
-    return String::new();
-  }
-  let value = mutable_pointer.unwrap();
-  if value.is_null() {
-    return String::new();
-  }
-  if !value.is_u64() {
-    return String::new();
-  }
-  *value = serde_json::Value::String(value.to_string());
-  return serde_json::to_string(&unwrapped_value).unwrap_or_else(|_| "".into())
+// Parses and re-serializes json with the value at path converted from a uint64
+// to a string representation of the same number.
+// Returns an empty String if such conversion is not possible.
+// A path is a Unicode string with the reference tokens separated by /.
+// Inside tokens / is replaced by ~1 and ~ is replaced by ~0.
+// Examples: convert_uint64_value_to_string("/a/b", json) for { a : { b : 1 }}
+//           convert_uint64_value_to_string("/a/0", json) for { a : [ 1 ]}
+pub fn convert_uint64_value_to_string(path: &str, json: &str) -> String {
+    let result_value = serde_json::from_str(&json);
+    if let Err(result_value) = result_value {
+        return String::new();
+    }
+    let mut unwrapped_value: serde_json::Value = result_value.unwrap();
+    let mutable_pointer = unwrapped_value.pointer_mut(path);
+    if mutable_pointer.is_none() {
+        return String::new();
+    }
+    let value = mutable_pointer.unwrap();
+    if !value.is_u64() {
+        return String::new();
+    }
+    *value = serde_json::Value::String(value.to_string());
+    serde_json::to_string(&unwrapped_value).unwrap_or_else(|_| "".into())
 }
 
-// Converts int64 value to string and serializes json with new values.
-// A path is a Unicode string with the reference tokens separated by /. Inside tokens / is replaced by ~1 and ~ is replaced by ~0.
+// Parses and re-serializes json with the value at path converted from a int64
+// to a string representation of the same number.
+// Returns an empty String if such conversion is not possible.
+// A path is a Unicode string with the reference tokens separated by /.
+// Inside tokens / is replaced by ~1 and ~ is replaced by ~0.
 // Examples: convert_int64_to_unicode_string("/a/b", json) for { a : { b : 1 }}
 //           convert_int64_to_unicode_string("/a/0", json) for { a : [ 1 ]}
-pub fn convert_int64_to_unicode_string(path: &str, json: &str) -> String {
-  let mut unwrapped_value: serde_json::Value = serde_json::from_str(&json).unwrap_or_else(|e| {
-    eprintln!("Failed to parse JSON: {}", e);
-    return serde_json::Value::Null;
-  });
-  if unwrapped_value.is_null() {
-    return String::new();
-  }
-  let mutable_pointer = unwrapped_value.pointer_mut(path);
-  if mutable_pointer.is_none() {
-    return String::new();
-  }
-  let value = mutable_pointer.unwrap();
-  if value.is_null() {
-    return String::new();
-  }
-  if !value.is_i64() {
-    return String::new();
-  }
-  *value = serde_json::Value::String(value.to_string());
-  return serde_json::to_string(&unwrapped_value).unwrap_or_else(|_| "".into())
+pub fn convert_int64_value_to_string(path: &str, json: &str) -> String {
+    let result_value = serde_json::from_str(&json);
+    if let Err(result_value) = result_value {
+        return String::new();
+    }
+    let mut unwrapped_value: serde_json::Value = result_value.unwrap();
+
+    let mutable_pointer = unwrapped_value.pointer_mut(path);
+    if mutable_pointer.is_none() {
+        return String::new();
+    }
+    let value = mutable_pointer.unwrap();
+    if !value.is_i64() {
+        return String::new();
+    }
+    *value = serde_json::Value::String(value.to_string());
+    serde_json::to_string(&unwrapped_value).unwrap_or_else(|_| "".into())
 }
 
-// Converts string value to uint64 and serializes json with new values.
-// A path is a Unicode string with the reference tokens separated by /. Inside tokens / is replaced by ~1 and ~ is replaced by ~0.
-// Examples: convert_unicode_string_to_uint64("/a/b", json) for { a : { b : '1' }} -> { a : { b : 1 }}
-//           convert_unicode_string_to_uint64("/a/0", json) for { a : [ '1' ]} -> { a : [ 1 ]}
-pub fn convert_unicode_string_to_uint64(path: &str, json: &str) -> String {
-  let mut unwrapped_value: serde_json::Value = serde_json::from_str(&json).unwrap_or_else(|e| {
-    eprintln!("Failed to parse JSON: {}", e);
-    return serde_json::Value::Null;
-  });
-  if unwrapped_value.is_null() {
-    return String::new();
-  }
-  let mutable_pointer = unwrapped_value.pointer_mut(path);
-  if mutable_pointer.is_none() {
-    return String::new();
-  }
-  let value = mutable_pointer.unwrap();
-  if value.is_null() {
-    return String::new();
-  }
-  if !value.is_string() {
-    return String::new();
-  }
-  let unwrapped_number = value.as_str().unwrap();
-  let uint64 = unwrapped_number.parse::<u64>();
-  if let Ok(uint64) = uint64 {
-    *value = serde_json::Value::from(uint64);
-    return serde_json::to_string(&unwrapped_value).unwrap_or_else(|_| "".into())
-  }
-  return String::new();
+// Parses and re-serializes json with the value at path converted from a string
+// to a uint64 representation of the same number.
+// Returns an empty String if such conversion is not possible.
+// A path is a Unicode string with the reference tokens separated by /.
+// Inside tokens / is replaced by ~1 and ~ is replaced by ~0.
+// Examples: convert_string_value_to_uint64("/a/b", json) for { a : { b : '1' }} -> { a : { b : 1 }}
+//           convert_string_value_to_uint64("/a/0", json) for { a : [ '1' ]} -> { a : [ 1 ]}
+pub fn convert_string_value_to_uint64(path: &str, json: &str) -> String {
+    let result_value = serde_json::from_str(&json);
+    if let Err(result_value) = result_value {
+        return String::new();
+    }
+    let mut unwrapped_value: serde_json::Value = result_value.unwrap();
+    let mutable_pointer = unwrapped_value.pointer_mut(path);
+    if mutable_pointer.is_none() {
+        return String::new();
+    }
+    let value = mutable_pointer.unwrap();
+    if !value.is_string() {
+        return String::new();
+    }
+    let unwrapped_number = value.as_str().unwrap();
+    let uint64 = unwrapped_number.parse::<u64>();
+    if let Ok(uint64) = uint64 {
+        *value = serde_json::Value::from(uint64);
+        return serde_json::to_string(&unwrapped_value).unwrap_or_else(|_| "".into());
+    }
+    String::new()
 }
 
-// Converts string value to int64 and serializes json with new values.
-// A path is a Unicode string with the reference tokens separated by /. Inside tokens / is replaced by ~1 and ~ is replaced by ~0.
-// Examples: convert_unicode_string_to_int64("/a/b", json) for { a : { b : '1' }} -> { a : { b : 1 }}
-//           convert_unicode_string_to_int64("/a/0", json) for { a : [ '1' ]} -> { a : [ 1 ]}
-pub fn convert_unicode_string_to_int64(path: &str, json: &str) -> String {
-  let mut unwrapped_value: serde_json::Value = serde_json::from_str(&json).unwrap_or_else(|e| {
-    eprintln!("Failed to parse JSON: {}", e);
-    return serde_json::Value::Null;
-  });
-  if unwrapped_value.is_null() {
-    return String::new();
-  }
-  let mutable_pointer = unwrapped_value.pointer_mut(path);
-  if mutable_pointer.is_none() {
-    return String::new();
-  }
-  let value = mutable_pointer.unwrap();
-  if value.is_null() {
-    return String::new();
-  }
-  if !value.is_string() {
-    return String::new();
-  }
-  let unwrapped_number = value.as_str().unwrap();
-  let int64 = unwrapped_number.parse::<i64>();
-  if let Ok(int64) = int64 {
-    *value = serde_json::Value::from(int64);
-    return serde_json::to_string(&unwrapped_value).unwrap_or_else(|_| "".into())
-  }
-  
-  return String::new();
+// Parses and re-serializes json with the value at path converted from a string
+// to a int64 representation of the same number.
+// Returns an empty String if such conversion is not possible.
+// A path is a Unicode string with the reference tokens separated by /.
+// Inside tokens / is replaced by ~1 and ~ is replaced by ~0.
+// Examples: convert_string_value_to_int64("/a/b", json) for { a : { b : '1' }} -> { a : { b : 1 }}
+//           convert_string_value_to_int64("/a/0", json) for { a : [ '1' ]} -> { a : [ 1 ]}
+pub fn convert_string_value_to_int64(path: &str, json: &str) -> String {
+    let result_value = serde_json::from_str(&json);
+    if let Err(result_value) = result_value {
+        return String::new();
+    }
+    let mut unwrapped_value: serde_json::Value = result_value.unwrap();
+    let mutable_pointer = unwrapped_value.pointer_mut(path);
+    if mutable_pointer.is_none() {
+        return String::new();
+    }
+    let value = mutable_pointer.unwrap();
+    if !value.is_string() {
+        return String::new();
+    }
+    let unwrapped_number = value.as_str().unwrap();
+    let int64 = unwrapped_number.parse::<i64>();
+    if let Ok(int64) = int64 {
+        *value = serde_json::Value::from(int64);
+        return serde_json::to_string(&unwrapped_value).unwrap_or_else(|_| "".into());
+    }
+
+    String::new()
 }
