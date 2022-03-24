e8845b052ba06c6ec79bfda18be6d2bf70867514
William Muli <willi.wambu@gmail.com>
2022-03-24 13:06:34 +0300

    Update brave_wallet_constants.h

--- components/brave_wallet/browser/brave_wallet_constants.h
+++ components/brave_wallet/browser/brave_wallet_constants.h
@@ -643,14 +643,18 @@ constexpr webui::LocalizedString kLocalizedStrings[] = {
      IDS_BRAVE_WALLET_MARKET_DATA_VOLUME_COLUMN},
     {"braveWalletMarketDataNoAssetsFound",
      IDS_BRAVE_WALLET_MARKET_DATA_NO_ASSETS_FOUND},
-    {"braveWalletMarketDataDetailRank", IDS_BRAVE_WALLET_MARKET_DATA_DETAIL_RANK},
-    {"braveWalletMarketDataDetailVolume", IDS_BRAVE_WALLET_MARKET_DATA_DETAIL_VOLUME},
-    {"braveWalletMarketDataDetailMarketCap", IDS_BRAVE_WALLET_MARKET_DATA_DETAIL_MARKET_CAP},
-    {"braveWalletMarketDataDetailWebsite", IDS_BRAVE_WALLET_MARKET_DATA_DETAIL_WEBSITE},
-    {"braveWalletMarketDataDetailInformation", IDS_BRAVE_WALLET_MARKET_DATA_DETAIL_INFORMATION},
+    {"braveWalletMarketDataDetailRank",
+     IDS_BRAVE_WALLET_MARKET_DATA_DETAIL_RANK},
+    {"braveWalletMarketDataDetailVolume",
+     IDS_BRAVE_WALLET_MARKET_DATA_DETAIL_VOLUME},
+    {"braveWalletMarketDataDetailMarketCap",
+     IDS_BRAVE_WALLET_MARKET_DATA_DETAIL_MARKET_CAP},
+    {"braveWalletMarketDataDetailWebsite",
+     IDS_BRAVE_WALLET_MARKET_DATA_DETAIL_WEBSITE},
+    {"braveWalletMarketDataDetailInformation",
+     IDS_BRAVE_WALLET_MARKET_DATA_DETAIL_INFORMATION},
     {"braveWalletAssetNotSupported", IDS_BRAVE_WALLET_ASSET_NOT_SUPPORTED},
-    {"braveWalletNotValidFilAddress",
-     IDS_BRAVE_WALLET_NOT_VALID_FIL_ADDRESS}};
+    {"braveWalletNotValidFilAddress", IDS_BRAVE_WALLET_NOT_VALID_FIL_ADDRESS}};
 
 // Swap constants
 const char kRopstenSwapBaseAPIURL[] = "https://ropsten.api.0x.org/";
