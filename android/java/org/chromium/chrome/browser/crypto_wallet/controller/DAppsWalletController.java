e37bb2409a527ec04ebcb48bf28ed284ca0036d7
Serg <serg.zhukovsky@gmail.com>
2022-04-06 13:57:53 -0400

    Adds account picture, account info, balance on DApps panel

--- android/java/org/chromium/chrome/browser/crypto_wallet/controller/DAppsWalletController.java
+++ android/java/org/chromium/chrome/browser/crypto_wallet/controller/DAppsWalletController.java
@@ -15,10 +15,14 @@ import androidx.lifecycle.DefaultLifecycleObserver;
 import androidx.lifecycle.LifecycleOwner;
 
 import org.chromium.brave_wallet.mojom.AccountInfo;
+import org.chromium.brave_wallet.mojom.AssetRatioService;
 import org.chromium.brave_wallet.mojom.BraveWalletConstants;
+import org.chromium.brave_wallet.mojom.BraveWalletService;
 import org.chromium.brave_wallet.mojom.JsonRpcService;
 import org.chromium.brave_wallet.mojom.KeyringService;
 import org.chromium.chrome.browser.app.BraveActivity;
+import org.chromium.chrome.browser.crypto_wallet.AssetRatioServiceFactory;
+import org.chromium.chrome.browser.crypto_wallet.BraveWalletServiceFactory;
 import org.chromium.chrome.browser.crypto_wallet.JsonRpcServiceFactory;
 import org.chromium.chrome.browser.crypto_wallet.KeyringServiceFactory;
 import org.chromium.chrome.browser.crypto_wallet.modal.BraveWalletPanel;
@@ -29,10 +33,13 @@ import org.chromium.chrome.browser.crypto_wallet.util.Utils;
 import org.chromium.mojo.bindings.ConnectionErrorHandler;
 import org.chromium.mojo.system.MojoException;
 
-public class DAppsWalletController implements ConnectionErrorHandler, KeyringServiceObserver {
+public class DAppsWalletController implements ConnectionErrorHandler, KeyringServiceObserver,
+                                              BraveWalletPanel.BraveWalletPanelServices {
     private Context mContext;
     private View mAnchorViewHost;
+    private AssetRatioService mAssetRatioService;
     private KeyringService mKeyringService;
+    private BraveWalletService mBraveWalletService;
     protected JsonRpcService mJsonRpcService;
     private boolean mHasStateInitialise;
     private DAppsDialog mDAppsDialog;
@@ -60,8 +67,10 @@ public class DAppsWalletController implements ConnectionErrorHandler, KeyringSer
     }
 
     public void showWalletPanel() {
+        InitAssetRatioService();
         InitKeyringService();
         InitJsonRpcService();
+        InitBraveWalletService();
         if (Utils.shouldShowCryptoOnboarding()) {
             showOnBoardingOrUnlock();
         } else {
@@ -87,7 +96,7 @@ public class DAppsWalletController implements ConnectionErrorHandler, KeyringSer
                 accountInfos = keyringInfo.accountInfos;
             }
             mBraveWalletPanel = new BraveWalletPanel(
-                    mAnchorViewHost, mDialogOrPanelDismissListener, mJsonRpcService, accountInfos);
+                    mAnchorViewHost, mDialogOrPanelDismissListener, this, accountInfos);
             mBraveWalletPanel.showLikePopDownMenu();
             setupLifeCycleUpdater();
         });
@@ -103,13 +112,51 @@ public class DAppsWalletController implements ConnectionErrorHandler, KeyringSer
     }
 
     @Override
+    public AssetRatioService getAssetRatioService() {
+        assert mAssetRatioService != null;
+        return mAssetRatioService;
+    }
+
+    @Override
+    public BraveWalletService getBraveWalletService() {
+        assert mBraveWalletService != null;
+        return mBraveWalletService;
+    }
+
+    @Override
+    public KeyringService getKeyringService() {
+        assert mKeyringService != null;
+        return mKeyringService;
+    }
+
+    @Override
+    public JsonRpcService getJsonRpcService() {
+        assert mJsonRpcService != null;
+        return mJsonRpcService;
+    }
+
+    @Override
     public void onConnectionError(MojoException e) {
-        mKeyringService.close();
-        mJsonRpcService.close();
-        mKeyringService = null;
-        mJsonRpcService = null;
+        if (mKeyringService != null) {
+            mKeyringService.close();
+            mKeyringService = null;
+        }
+        if (mJsonRpcService != null) {
+            mJsonRpcService.close();
+            mJsonRpcService = null;
+        }
+        if (mBraveWalletService != null) {
+            mBraveWalletService.close();
+            mBraveWalletService = null;
+        }
+        if (mAssetRatioService != null) {
+            mAssetRatioService.close();
+            mAssetRatioService = null;
+        }
+        InitAssetRatioService();
         InitKeyringService();
         InitJsonRpcService();
+        InitBraveWalletService();
         updateState();
     }
 
@@ -153,12 +200,38 @@ public class DAppsWalletController implements ConnectionErrorHandler, KeyringSer
         mJsonRpcService = JsonRpcServiceFactory.getInstance().getJsonRpcService(this);
     }
 
+    private void InitBraveWalletService() {
+        if (mBraveWalletService != null) {
+            return;
+        }
+
+        mBraveWalletService = BraveWalletServiceFactory.getInstance().getBraveWalletService(this);
+    }
+
+    private void InitAssetRatioService() {
+        if (mAssetRatioService != null) {
+            return;
+        }
+
+        mAssetRatioService = AssetRatioServiceFactory.getInstance().getAssetRatioService(this);
+    }
+
     private void cleanUp() {
         if (mKeyringService != null) {
             mKeyringService.close();
+            mKeyringService = null;
         }
         if (mJsonRpcService != null) {
             mJsonRpcService.close();
+            mJsonRpcService = null;
+        }
+        if (mBraveWalletService != null) {
+            mBraveWalletService.close();
+            mBraveWalletService = null;
+        }
+        if (mAssetRatioService != null) {
+            mAssetRatioService.close();
+            mAssetRatioService = null;
         }
 
         if (mActivity != null) {
--- android/java/org/chromium/chrome/browser/crypto_wallet/modal/BraveWalletPanel.java
+++ android/java/org/chromium/chrome/browser/crypto_wallet/modal/BraveWalletPanel.java
@@ -12,55 +12,81 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.chromium.base.SysUtils;
 import org.chromium.brave_wallet.mojom.AccountInfo;
+import org.chromium.brave_wallet.mojom.AssetRatioService;
 import org.chromium.brave_wallet.mojom.BraveWalletService;
 import org.chromium.brave_wallet.mojom.CoinType;
 import org.chromium.brave_wallet.mojom.JsonRpcService;
+import org.chromium.brave_wallet.mojom.KeyringService;
 import org.chromium.brave_wallet.mojom.NetworkInfo;
+import org.chromium.brave_wallet.mojom.ProviderError;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.app.BraveActivity;
-import org.chromium.chrome.browser.crypto_wallet.BraveWalletServiceFactory;
 import org.chromium.chrome.browser.crypto_wallet.activities.NetworkSelectorActivity;
 import org.chromium.chrome.browser.crypto_wallet.util.AccountsPermissionsHelper;
+import org.chromium.chrome.browser.crypto_wallet.util.SingleTokenBalanceHelper;
 import org.chromium.chrome.browser.crypto_wallet.util.Utils;
 import org.chromium.chrome.browser.util.ConfigurationUtils;
-import org.chromium.mojo.bindings.ConnectionErrorHandler;
-import org.chromium.mojo.system.MojoException;
 import org.chromium.ui.base.DeviceFormFactor;
 
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class BraveWalletPanel implements ConnectionErrorHandler, DialogInterface {
+public class BraveWalletPanel implements DialogInterface {
     private static final String TAG = "BraveWalletPanel";
     private final View mAnchorViewHost;
     private final PopupWindow mPopupWindow;
-    private ViewGroup mPopupView;
     private final AppCompatActivity mActivity;
+    private ViewGroup mPopupView;
     private OnDismissListener mOnDismissListener;
     private Button mBtnConnectedStatus;
     private Button mBtnSelectedNetwork;
+    private ImageView mAccountImage;
+    private TextView mAccountName;
+    private TextView mAccountAddress;
+    private TextView mAmountAsset;
+    private TextView mAmountFiat;
     private AccountInfo[] mAccountInfos;
-    private BraveWalletService mBraveWalletService;
-    protected JsonRpcService mJsonRpcService;
+    private HashSet<AccountInfo> mAccountsWithPermissions;
+    private ExecutorService mExecutor;
+    private Handler mHandler;
+    private BraveWalletPanelServices mBraveWalletPanelServices;
+
+    public interface BraveWalletPanelServices {
+        AssetRatioService getAssetRatioService();
+        BraveWalletService getBraveWalletService();
+        KeyringService getKeyringService();
+        JsonRpcService getJsonRpcService();
+    }
 
     public BraveWalletPanel(View anchorViewHost, OnDismissListener onDismissListener,
-            JsonRpcService jsonRpcService, AccountInfo[] accountInfos) {
+            BraveWalletPanelServices braveWalletPanelServices, AccountInfo[] accountInfos) {
+        mAccountsWithPermissions = new HashSet<AccountInfo>();
+        mExecutor = Executors.newSingleThreadExecutor();
+        mHandler = new Handler(Looper.getMainLooper());
         mAnchorViewHost = anchorViewHost;
         mOnDismissListener = onDismissListener;
         mActivity = BraveActivity.getChromeTabbedActivity();
-        mJsonRpcService = jsonRpcService;
         mAccountInfos = accountInfos;
+        mBraveWalletPanelServices = braveWalletPanelServices;
 
         mPopupWindow = new PopupWindow(mAnchorViewHost.getContext());
         mPopupWindow.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
@@ -112,10 +138,6 @@ public class BraveWalletPanel implements ConnectionErrorHandler, DialogInterface
         if (mOnDismissListener != null) {
             mOnDismissListener.onDismiss(this);
         }
-        if (mBraveWalletService != null) {
-            mBraveWalletService.close();
-            mBraveWalletService = null;
-        }
     }
 
     public void resume() {
@@ -129,9 +151,8 @@ public class BraveWalletPanel implements ConnectionErrorHandler, DialogInterface
     }
 
     private void updateState() {
-        assert mJsonRpcService != null;
-        mJsonRpcService.getChainId(CoinType.ETH, chainId -> {
-            mJsonRpcService.getAllNetworks(CoinType.ETH, chains -> {
+        mBraveWalletPanelServices.getJsonRpcService().getChainId(CoinType.ETH, chainId -> {
+            mBraveWalletPanelServices.getJsonRpcService().getAllNetworks(CoinType.ETH, chains -> {
                 NetworkInfo[] customNetworks = Utils.getCustomNetworks(chains);
                 String strNetwork = Utils.getNetworkShortText(mActivity, chainId).toString();
                 mBtnSelectedNetwork.setText(strNetwork);
@@ -140,23 +161,98 @@ public class BraveWalletPanel implements ConnectionErrorHandler, DialogInterface
     }
 
     private void updateStatus() {
-        assert mBraveWalletService != null;
-        AccountsPermissionsHelper accountsPermissionsHelper = new AccountsPermissionsHelper(
-                mBraveWalletService, mAccountInfos, getCurrentHostHttpAddress());
+        AccountsPermissionsHelper accountsPermissionsHelper =
+                new AccountsPermissionsHelper(mBraveWalletPanelServices.getBraveWalletService(),
+                        mAccountInfos, getCurrentHostHttpAddress());
         accountsPermissionsHelper.checkAccounts(() -> {
-            HashSet<AccountInfo> mAccountsWithPermissions =
-                    accountsPermissionsHelper.getAccountsWithPermissions();
-            if (mAccountsWithPermissions.size() != 0) {
-                mBtnConnectedStatus.setText(mPopupView.getResources().getString(
-                        R.string.dapp_wallet_panel_connectivity_status_connected));
-                mBtnConnectedStatus.setCompoundDrawablesRelativeWithIntrinsicBounds(
-                        R.drawable.ic_check_white, 0, 0, 0);
+            mAccountsWithPermissions = accountsPermissionsHelper.getAccountsWithPermissions();
+            updateAccount();
+        });
+    }
+
+    private void updateAccount() {
+        mBraveWalletPanelServices.getKeyringService().getSelectedAccount(CoinType.ETH, address -> {
+            String selectedAccount = "";
+            if (address != null && !address.isEmpty()) {
+                selectedAccount = address;
+                updateAccountInfo(selectedAccount);
+            } else {
+                if (!mAccountsWithPermissions.isEmpty()) {
+                    selectedAccount = mAccountsWithPermissions.iterator().next().address;
+                } else if (mAccountInfos.length > 0) {
+                    selectedAccount = mAccountInfos[0].address;
+                }
+                setSelectedAccount(selectedAccount);
             }
         });
     }
 
+    private void setSelectedAccount(String selectedAccount) {
+        mBraveWalletPanelServices.getKeyringService().setSelectedAccount(
+                selectedAccount, CoinType.ETH, success -> {
+                    if (success) {
+                        updateAccountInfo(selectedAccount);
+                    }
+                });
+    }
+
+    private void updateAccountInfo(String selectedAccount) {
+        Utils.setBlockiesBitmapResource(mExecutor, mHandler, mAccountImage, selectedAccount, true);
+        for (AccountInfo accountInfo : mAccountInfos) {
+            if (accountInfo.address.equals(selectedAccount)) {
+                mAccountName.setText(accountInfo.name);
+                mAccountAddress.setText(Utils.stripAccountAddress(accountInfo.address));
+                getBalance(new AccountInfo[] {accountInfo});
+                updateAccountConnected(selectedAccount);
+                break;
+            }
+        }
+    }
+
+    private void updateAccountConnected(String selectedAccount) {
+        Iterator<AccountInfo> it = mAccountsWithPermissions.iterator();
+        boolean found = false;
+        while (it.hasNext()) {
+            if (it.next().address.equals(selectedAccount)) {
+                found = true;
+                break;
+            }
+        }
+        mBtnConnectedStatus.setText(mPopupView.getResources().getString(found
+                        ? R.string.dapp_wallet_panel_connectivity_status_connected
+                        : R.string.dapp_wallet_panel_connectivity_status));
+        mBtnConnectedStatus.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                found ? R.drawable.ic_check_white : 0, 0, 0, 0);
+    }
+
+    private void getBalance(AccountInfo[] selectedAccount) {
+        if (selectedAccount.length == 0) {
+            return;
+        }
+        mBraveWalletPanelServices.getJsonRpcService().getChainId(CoinType.ETH, chainId -> {
+            SingleTokenBalanceHelper singleTokenBalanceHelper =
+                    new SingleTokenBalanceHelper(mBraveWalletPanelServices.getAssetRatioService(),
+                            mBraveWalletPanelServices.getJsonRpcService());
+            singleTokenBalanceHelper.getPerAccountBalances(
+                    chainId, "", "eth", 18, selectedAccount, () -> {
+                        Double thisAccountFiatBalance = Utils.getOrDefault(
+                                singleTokenBalanceHelper.getPerAccountFiatBalance(),
+                                selectedAccount[0].address, 0.0d);
+                        String fiatBalanceString = String.format(
+                                Locale.getDefault(), "$%,.2f", thisAccountFiatBalance);
+
+                        Double thisAccountCryptoBalance = Utils.getOrDefault(
+                                singleTokenBalanceHelper.getPerAccountCryptoBalance(),
+                                selectedAccount[0].address, 0.0d);
+                        String cryptoBalanceString = String.format(
+                                Locale.getDefault(), "%.4f %s", thisAccountCryptoBalance, "ETH");
+                        mAmountAsset.setText(cryptoBalanceString);
+                        mAmountFiat.setText(fiatBalanceString);
+                    });
+        });
+    }
+
     private void setUpViews() {
-        InitBraveWalletService();
         LayoutInflater inflater = LayoutInflater.from(mAnchorViewHost.getContext());
         mPopupView = (ViewGroup) inflater.inflate(R.layout.brave_wallet_panel_layout, null);
 
@@ -172,6 +268,11 @@ public class BraveWalletPanel implements ConnectionErrorHandler, DialogInterface
         });
         mBtnConnectedStatus = mPopupView.findViewById(R.id.sp_dapps_panel_state);
         mPopupWindow.setContentView(mPopupView);
+        mAccountImage = mPopupView.findViewById(R.id.iv_dapps_panel_account_image);
+        mAccountName = mPopupView.findViewById(R.id.tv_dapps_panel_from_to);
+        mAccountAddress = mPopupView.findViewById(R.id.tv_dapps_panel_account_address);
+        mAmountAsset = mPopupView.findViewById(R.id.tv_dapps_panel_amount_asset);
+        mAmountFiat = mPopupView.findViewById(R.id.tv_dapps_panel_amount_fiat);
         updateState();
         updateStatus();
         // TODO: show connected or disconnected account page
@@ -183,20 +284,5 @@ public class BraveWalletPanel implements ConnectionErrorHandler, DialogInterface
             return activity.getActivityTab().getUrl().getOrigin().getSpec();
         }
         return "";
-    }
-
-    @Override
-    public void onConnectionError(MojoException e) {
-        mBraveWalletService.close();
-        mBraveWalletService = null;
-        InitBraveWalletService();
-    }
-
-    private void InitBraveWalletService() {
-        if (mBraveWalletService != null) {
-            return;
-        }
-
-        mBraveWalletService = BraveWalletServiceFactory.getInstance().getBraveWalletService(this);
     }
 }
