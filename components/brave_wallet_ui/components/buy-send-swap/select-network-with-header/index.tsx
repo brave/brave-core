ca9af1a5e404ac212d67dd947050e58768758bd3
Josh Leonard <jleonard@brave.com>
2022-04-07 17:46:55 -0600

    fix: dismiss network list after selecting network

--- components/brave_wallet_ui/components/buy-send-swap/select-network-with-header/index.tsx
+++ components/brave_wallet_ui/components/buy-send-swap/select-network-with-header/index.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react'
+import { useDispatch } from 'react-redux'
+
 import { SelectNetwork } from '../../shared'
 import Header from '../select-header'
 import { getLocale } from '../../../../common/locale'
@@ -8,24 +10,42 @@ import {
   SelectScrollContainer
 } from '../shared-styles'
 
+import { WalletActions } from '../../../common/actions'
+import { PanelActions } from '../../../panel/actions'
+import { BraveWallet } from '../../../constants/types'
+
 export interface Props {
   hasAddButton?: boolean
   onBack: () => void
   onAddNetwork?: () => void
 }
 
-function SelectNetworkWithHeader (props: Props) {
-  const { onBack, hasAddButton, onAddNetwork } = props
+function SelectNetworkWithHeader ({
+  onBack,
+  hasAddButton,
+  onAddNetwork
+}: Props) {
+  // redux
+  const dispatch = useDispatch()
 
+  // methods
+  const onSelectCustomNetwork = React.useCallback((network: BraveWallet.NetworkInfo): void => {
+    dispatch(WalletActions.selectNetwork(network))
+    dispatch(PanelActions.navigateTo('main'))
+    onBack()
+  }, [onBack])
+
+  // render
   return (
     <SelectWrapper>
       <Header
         title={getLocale('braveWalletSelectNetwork')}
         onBack={onBack}
         hasAddButton={hasAddButton}
-        onClickAdd={onAddNetwork} />
+        onClickAdd={onAddNetwork}
+      />
       <SelectScrollContainer>
-        <SelectNetwork />
+        <SelectNetwork onSelectCustomNetwork={onSelectCustomNetwork} />
       </SelectScrollContainer>
     </SelectWrapper>
   )
