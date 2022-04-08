b914ff92df58cb892f2bca724be54f1840a3840a
Serg <serg.zhukovsky@gmail.com>
2022-04-08 15:46:55 -0400

    Fixes add network feature on Android, adds Java unit tests to prevent the breakage in the feature

--- android/java/org/chromium/chrome/browser/settings/BraveWalletAddNetworksFragment.java
+++ android/java/org/chromium/chrome/browser/settings/BraveWalletAddNetworksFragment.java
@@ -271,6 +271,7 @@ public class BraveWalletAddNetworksFragment extends Fragment implements Connecti
         } else {
             chain.blockExplorerUrls = new String[0];
         }
+        chain.coin = CoinType.ETH;
 
         assert mJsonRpcService != null;
         if (!mChainId.isEmpty()) {
