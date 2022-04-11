160683d8e441f83527c28b0169280d2a0e3994ad
Kyle Hickinson <git@kylehickinson.com>
2022-04-11 10:22:28 -0400

    Fix pylint errors

--- build/rust/generate_unexport_list.py
+++ build/rust/generate_unexport_list.py
@@ -4,7 +4,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import argparse
 import os
+import subprocess
 import sys
 
 # Set up path to load build_utils.py which enables us to do
@@ -12,32 +14,29 @@ import sys
 sys.path.append(
   os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir,
                os.pardir, os.pardir, 'build', 'android', 'gyp'))
-from util import build_utils
-
-import argparse
-import subprocess
+from util import build_utils # pylint: disable=no-name-in-module, wrong-import-position
 
 
-def run(exe, input, output):
+def run(exe, input_file, output_file):
   cmdargs = [exe]
   cmdargs += ["--extern-only"]
   cmdargs += ["--no-sort"]
   cmdargs += ["--just-symbol-name"]
-  cmdargs += [input]
-  job = subprocess.run(cmdargs, capture_output=True)
+  cmdargs += [input_file]
+  job = subprocess.run(cmdargs, capture_output=True, check=False)
   if job.returncode != 0:
     messages = job.stderr.decode('utf-8')
     if messages.rstrip():
       print(messages, file=sys.stderr)
     return job.returncode
-  with build_utils.AtomicOutput(output) as output:
+  with build_utils.AtomicOutput(output_file) as output:
     output.write(job.stdout)
   return 0
 
 
 def main():
   parser = argparse.ArgumentParser("generate_unexport_list.py")
-  parser.add_argument("--bin_path", required=True),
+  parser.add_argument("--bin_path", required=True)
   parser.add_argument("--input", required=True)
   parser.add_argument("--output", required=True)
   args = parser.parse_args()
