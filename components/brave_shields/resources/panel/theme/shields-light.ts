6974cdaebb3621a4a15dd72524dbe0b279a6bea1
taher <taher@tahers-MacBook-Pro-2.local>
2022-04-12 12:56:50 -0700

    Favicon and color fixes for Shields v2

--- components/brave_shields/resources/panel/theme/shields-light.ts
+++ components/brave_shields/resources/panel/theme/shields-light.ts
@@ -5,15 +5,20 @@
 
  import ITheme from 'brave-ui/theme/theme-interface'
  import defaultTheme from 'brave-ui/theme/brave-default'
+ import IThemeShields from './shields-theme'
 
- const shieldsLightTheme: ITheme = {
+ const shieldsLightTheme: ITheme & IThemeShields = {
    ...defaultTheme,
    name: 'Shields light',
    color: {
      ...defaultTheme.color,
      background03: '#F8F9FA',
      text01: '#212529',
-     warningIcon: '#EA3A0D'
+     warningIcon: '#EA3A0D',
+     interactive05: '#4C54D2',
+     interactive06: '#737ADE',
+     interactive07: '#4C54D2',
+     interactive08: '#353DAB'
    }
  }
 
