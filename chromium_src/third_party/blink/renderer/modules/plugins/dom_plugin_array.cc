f8a8900c2bb7882b310335a5e12ce6b138c9893e
Mark Pilgrim <43750833+pilgrim-brave@users.noreply.github.com>
2022-04-13 10:28:21 -0400

    Allow callers to call DOMPluginArray::IsPdfViewerAvailable unconditionally even if should_return_fixed_plugin_data_ is false (#12989)
    * Allow callers to call DOMPluginArray::IsPdfViewerAvailable unconditionally even if should_return_fixed_plugin_data_ is false
    
    * just remove the DCHECK

--- chromium_src/third_party/blink/renderer/modules/plugins/dom_plugin_array.cc
+++ chromium_src/third_party/blink/renderer/modules/plugins/dom_plugin_array.cc
@@ -154,4 +154,5 @@ void FarblePlugins(DOMPluginArray* owner,
 
 #include "src/third_party/blink/renderer/modules/plugins/dom_plugin_array.cc"
 
-#undef BRAVE_DOM_PLUGIN_ARRAY_GET_PLUGIN_DATA
+#undef BRAVE_DOM_PLUGINS_UPDATE_PLUGIN_DATA__FARBLE_PLUGIN_DATA
+#undef BRAVE_DOM_PLUGINS_UPDATE_PLUGIN_DATA__RESET_PLUGIN_DATA
