7761c201d2404c77f80ecd9cf7c94398cf946a05
Mark Pilgrim <mpilgrim@brave.com>
2022-04-21 18:45:03 -0400

    lint

--- chromium_src/third_party/abseil-cpp/absl/random/random.h
+++ chromium_src/third_party/abseil-cpp/absl/random/random.h
@@ -3,8 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef BRAVE_CHROMIUM_SRC_THIRD_PARTY_ABSEIL_ABSL_RANDOM_RANDOM_H_
-#define BRAVE_CHROMIUM_SRC_THIRD_PARTY_ABSEIL_ABSL_RANDOM_RANDOM_H_
+#ifndef BRAVE_CHROMIUM_SRC_THIRD_PARTY_ABSEIL_CPP_ABSL_RANDOM_RANDOM_H_
+#define BRAVE_CHROMIUM_SRC_THIRD_PARTY_ABSEIL_CPP_ABSL_RANDOM_RANDOM_H_
 
 #include "src/third_party/abseil-cpp/absl/random/random.h"
 
@@ -12,4 +12,4 @@ namespace brave {
 typedef absl::random_internal::randen_engine<uint64_t> FarblingPRNG;
 }
 
-#endif
+#endif  // BRAVE_CHROMIUM_SRC_THIRD_PARTY_ABSEIL_CPP_ABSL_RANDOM_RANDOM_H_
