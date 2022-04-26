e6317629885064ae09ad9919be82d3fdd57df994
Brian Clifton <brian@clifton.me>
2022-04-26 13:54:54 -0700

    Revert peeking card in 1.38 only
    We'll need to re-enable before maintenance release and properly handle
    orientation.
    
    Fixes https://github.com/brave/brave-browser/issues/22569 in 1.38.x only
    by reverting the peeking card.
    
    -----------------------
    
    Revert "Merge pull request #13050 from brave/pr13017_bravenews-android-optin-back_1.38.x"
    
    This reverts commit 923a78ed7aea118b310d80341183fce63ae46fad, reversing
    changes made to dccc46726a84fc991cf66d4aa00f293b38ebff67.

--- android/java/org/chromium/chrome/browser/ntp/BraveNewTabPageLayout.java
+++ android/java/org/chromium/chrome/browser/ntp/BraveNewTabPageLayout.java
@@ -461,9 +461,14 @@ public class BraveNewTabPageLayout extends NewTabPageLayout implements Connectio
                 mIsShowOptin =
                         sharedPreferences.getBoolean(BraveNewsPreferences.PREF_SHOW_OPTIN, false);
                 mIsShowNewsOn = BravePrefServiceBridge.getInstance().getShowNews();
-                mOptinLayout.setVisibility(View.GONE);
-                mRecyclerView.setVisibility(View.VISIBLE);
-                initNews();
+                if ((!mIsNewsOn && mIsShowOptin) || (mIsNewsOn && !mIsShowOptin)) {
+                    SharedPreferences.Editor sharedPreferencesEditor = sharedPreferences.edit();
+                    sharedPreferencesEditor.putBoolean(BraveNewsPreferences.PREF_SHOW_OPTIN, true);
+                    sharedPreferencesEditor.apply();
+                    mOptinLayout.setVisibility(View.VISIBLE);
+                    mRecyclerView.setVisibility(View.GONE);
+                    initNews();
+                }
             }
         };
     }
@@ -839,7 +844,7 @@ public class BraveNewTabPageLayout extends NewTabPageLayout implements Connectio
         SharedPreferences sharedPreferences = ContextUtils.getAppSharedPreferences();
 
         mIsNewsOn = BravePrefServiceBridge.getInstance().getNewsOptIn();
-        mIsShowOptin = sharedPreferences.getBoolean(BraveNewsPreferences.PREF_SHOW_OPTIN, true);
+        mIsShowOptin = sharedPreferences.getBoolean(BraveNewsPreferences.PREF_SHOW_OPTIN, false);
         mIsShowNewsOn = BravePrefServiceBridge.getInstance().getShowNews();
 
         if ((!mIsNewsOn && mIsShowOptin) || (mIsNewsOn && mIsShowOptin)) {
@@ -1237,8 +1242,6 @@ public class BraveNewTabPageLayout extends NewTabPageLayout implements Connectio
                     SharedPreferences.Editor sharedPreferencesEditor = sharedPreferences.edit();
                     sharedPreferencesEditor.putBoolean(BraveNewsPreferences.PREF_SHOW_OPTIN, false);
                     sharedPreferencesEditor.apply();
-                    BravePrefServiceBridge.getInstance().setNewsOptIn(true);
-                    BravePrefServiceBridge.getInstance().setShowNews(false);
                     correctPosition(false);
                     mParentScrollView.fullScroll(NestedScrollView.FOCUS_UP);
                     mImageCreditLayout.setAlpha(1.0f);
