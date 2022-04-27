ed1e0138a2416ef0d14d8e042e1f944d0e77dfc6
brave-builds <devops@brave.com>
2022-04-27 14:47:55 +0000

    Uplift of #13179 (squashed) to beta

--- chromium_src/content/renderer/render_frame_impl.cc
+++ chromium_src/content/renderer/render_frame_impl.cc
@@ -3,11 +3,17 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+#include "build/build_config.h"
+
+#if BUILDFLAG(IS_ANDROID)
+#define BRAVE_RENDER_FRAME_IMPL_SHOULD_USE_USER_AGENT_OVERRIDE
+#else
 // clang-format off
 #define BRAVE_RENDER_FRAME_IMPL_SHOULD_USE_USER_AGENT_OVERRIDE \
     return true;                                               \
   if (false)                                                   \
 // clang-format on
+#endif
 
 #include "src/content/renderer/render_frame_impl.cc"
 #undef BRAVE_RENDER_FRAME_IMPL_SHOULD_USE_USER_AGENT_OVERRIDE
