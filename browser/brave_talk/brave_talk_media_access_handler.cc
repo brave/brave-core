07860fe200680da5d2ae117ae2f87be28eb6b55b
Jay Harris <jay.harris@outlook.co.nz>
2022-05-10 09:01:30 +1200

    Stub handlerequest

--- browser/brave_talk/brave_talk_media_access_handler.cc
+++ browser/brave_talk/brave_talk_media_access_handler.cc
@@ -5,4 +5,12 @@ namespace brave_talk {
 BraveTalkMediaAccessHandler::BraveTalkMediaAccessHandler() {}
 BraveTalkMediaAccessHandler::~BraveTalkMediaAccessHandler() = default;
 
+void BraveTalkMediaAccessHandler::HandleRequest(
+    content::WebContents* web_contents,
+    const content::MediaStreamRequest& request,
+    content::MediaResponseCallback callback,
+    const extensions::Extension* extension) {
+  LOG(ERROR) << "WOW. Tried to handle a request :O";
+}
+
 }  // namespace brave_talk
--- browser/brave_talk/brave_talk_media_access_handler.h
+++ browser/brave_talk/brave_talk_media_access_handler.h
@@ -2,7 +2,15 @@
 #define BRAVE_BROWSER_BRAVE_TALK_BRAVE_TALK_MEDIA_ACCESS_HANDLER_H_
 
 #include "chrome\browser\media\capture_access_handler_base.h"
+#include "content\public\browser\media_stream_request.F"
 
+namespace contents {
+class WebContents;
+}
+
+namespace extensions {
+class Extensions;
+}
 namespace brave_talk {
 
 class BraveTalkMediaAccessHandler : public CaptureAccessHandlerBase {
@@ -12,8 +20,12 @@ class BraveTalkMediaAccessHandler : public CaptureAccessHandlerBase {
   BraveTalkMediaAccessHandler(const BraveTalkMediaAccessHandler&) = delete;
   BraveTalkMediaAccessHandler& operator=(const BraveTalkMediaAccessHandler&) =
       delete;
-
   ~BraveTalkMediaAccessHandler() override;
+
+  void HandleRequest(content::WebContents* web_contents,
+                     const content::MediaStreamRequest& request,
+                     content::MediaResponseCallback callback,
+                     const extensions::Extension* extension) override;
 };
 
 }  // namespace brave_talk
