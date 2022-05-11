bda1d812159f3f51a3e16f8144a8812462f716a3
tapanmodh <modhtapan@gmail.com>
2022-05-11 16:44:13 +0530

    mentioned usage

--- android/java/org/chromium/chrome/browser/util/BraveConstants.java
+++ android/java/org/chromium/chrome/browser/util/BraveConstants.java
@@ -8,9 +8,12 @@
 package org.chromium.chrome.browser.util;
 
 public final class BraveConstants {
+    // used in BraveRewardsPanel
     public static final int SITE_BANNER_REQUEST_CODE = 33;
     public static final int VERIFY_WALLET_ACTIVITY_REQUEST_CODE = 34;
     public static final int USER_WALLET_ACTIVITY_REQUEST_CODE = 35;
     public static final int MONTHLY_CONTRIBUTION_REQUEST_CODE = 36;
+
+    // used in BravseSetDefaultBrowserUtils
     public static final int DEFAULT_BROWSER_ROLE_REQUEST_CODE = 37;
 }
