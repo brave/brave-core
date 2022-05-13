fe55c2861debbc98979f3eba8c72e4136e164d5c
Claudio DeSouza <cdesouza@igalia.com>
2022-05-13 12:46:29 +0100

    Fixing transient inclusions for url/gurl.h

--- components/brave_shields/browser/brave_shields_p3a_unittest.cc
+++ components/brave_shields/browser/brave_shields_p3a_unittest.cc
@@ -12,6 +12,7 @@
 #include "chrome/test/base/testing_profile.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
 
 namespace brave_shields {
 
