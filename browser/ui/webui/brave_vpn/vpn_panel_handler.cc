472af24e3c3c951086079caa02ff78571e8aa640
cepera <spylogsster@gmail.com>
2022-05-16 23:26:09 +0300

    Added handler to take actions from page and replaced extensions api calls in vpn panel popup

--- browser/ui/webui/brave_vpn/vpn_panel_handler.cc
+++ browser/ui/webui/brave_vpn/vpn_panel_handler.cc
@@ -9,7 +9,32 @@
 #include <utility>
 
 #include "brave/browser/brave_vpn/brave_vpn_service_factory.h"
+#include "brave/components/brave_vpn/brave_vpn_constants.h"
 #include "brave/components/brave_vpn/brave_vpn_service.h"
+#include "brave/components/brave_vpn/brave_vpn_utils.h"
+#include "chrome/browser/ui/browser_finder.h"
+#include "chrome/browser/ui/browser_tabstrip.h"
+
+namespace {
+
+GURL GetURLForUIType(const std::string& type) {
+  if (type == "recover" || type == "checkout") {
+    GURL manage_url = GURL(brave_vpn::GetManageUrl());
+    DCHECK(manage_url.is_valid());
+    std::string query = "intent=" + type + "&product=vpn";
+    GURL::Replacements replacements;
+    replacements.SetQueryStr(query);
+    return manage_url.ReplaceComponents(replacements);
+  } else if (type == "privacy") {
+    return GURL("https://brave.com/privacy/browser/#vpn");
+  } else if (type == "about") {
+    return GURL(brave_vpn::kAboutUrl);
+  }
+  DCHECK_EQ(type, "manage");
+  return GURL(brave_vpn::GetManageUrl());
+}
+
+}  // namespace
 
 VPNPanelHandler::VPNPanelHandler(
     mojo::PendingReceiver<brave_vpn::mojom::PanelHandler> receiver,
@@ -37,4 +62,9 @@ void VPNPanelHandler::CloseUI() {
   if (embedder) {
     embedder->CloseUI();
   }
+}
+
+void VPNPanelHandler::OpenVpnUI(const std::string& type) {
+  auto* browser = chrome::FindLastActiveWithProfile(profile_);
+  chrome::AddTabAt(browser, GetURLForUIType(type), -1, true);
 }
