0ff77a1e18cf989a0cd7da403700e5fec090c828
Jay Harris <jay.harris@outlook.co.nz>
2022-05-18 10:27:51 +1200

    Begin laying out approach for  patch

--- chromium_src/chrome/browser/media/webrtc/media_capture_devices_dispatcher.cc
+++ chromium_src/chrome/browser/media/webrtc/media_capture_devices_dispatcher.cc
@@ -1,5 +1,7 @@
-#define FOO
+#include "brave/browser/brave_talk/brave_talk_media_access_handler.h"
+
+#define BRAVE_MEDIA_CAPTURE_DEVICES_DISPATCHER media_access_handlers_.push_back(std::make_unique<brave_talk::BraveTalkMediaAccessHandler>());
 
 #include "src/chrome/browser/media/webrtc/media_capture_devices_dispatcher.cc"
 
-#undef FOO
+#undef BRAVE_MEDIA_CAPTURE_DEVICES_DISPATCHER
