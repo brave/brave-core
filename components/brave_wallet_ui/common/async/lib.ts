419d86aa8c41d22fa1b60ffb6c005d12a289d933
Douglas Daniel <douglashdaniel@gmail.com>
2022-05-17 16:22:20 -0600

    Merge pull request #13349 from brave/wired-up-solana-address-validation-in-ui
    feat(wallet): Solana Address Validation in UI

--- components/brave_wallet_ui/common/async/lib.ts
+++ components/brave_wallet_ui/common/async/lib.ts
@@ -100,6 +100,11 @@ export async function getChecksumEthAddress (value: string) {
   return (await keyringService.getChecksumEthAddress(value))
 }
 
+export async function isBase58EncodedSolanaPubkey (value: string) {
+  const { braveWalletService } = getAPIProxy()
+  return braveWalletService.isBase58EncodedSolanaPubkey(value)
+}
+
 export async function isStrongPassword (value: string) {
   const apiProxy = getAPIProxy()
   return (await apiProxy.keyringService.isStrongPassword(value)).result
