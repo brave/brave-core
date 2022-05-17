02f4ca17d18756433af3a75c8d7644fb41d0a23c
Jay Harris <jay.harris@outlook.co.nz>
2022-05-18 10:55:06 +1200

    Make it possible to override sharing hub model

--- chromium_src/chrome/browser/sharing_hub/sharing_hub_model.h
+++ chromium_src/chrome/browser/sharing_hub/sharing_hub_model.h
@@ -1,11 +1,15 @@
 #ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_SHARING_HUB_SHARING_HUB_MODEL_H_
 #define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_SHARING_HUB_SHARING_HUB_MODEL_H_
 
-#define PopulateFirstPartyActions PopulateFirstPartyActions(); \
-void PopulateFirstPartyActions_Chromium \
+#define BRAVE_SHARING_HUB_MODEL_H \
+private: \
+    friend class BraveSharingHubModel;
+
+#define PopulateFirstPartyActions virtual PopulateFirstPartyActions
 
 #include "src/chrome/browser/sharing_hub/sharing_hub_model.h"
 
 #undef PopulateFirstPartyActions
+#undef BRAVE_SHARING_HUB_MODEL_H
 
 #endif // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_SHARING_HUB_SHARING_HUB_MODEL_H_
