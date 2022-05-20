edec4cdf96d5662cb03f17acd6fea3a54bb16584
Anirudha Bose <anirudha.bose@alumni.cern>
2022-05-20 15:40:37 +0200

    feat(wallet): build out post transaction confirmation UI

---
+++ components/brave_wallet_ui/components/extension/post-confirmation/common/common.style.ts
@@ -0,0 +1,70 @@
+// Copyright (c) 2022 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+import styled from 'styled-components'
+
+import LinkSvg from '../../../../assets/svg-icons/link-icon.svg'
+import { WalletButton } from '../../../shared/style'
+
+export const TransactionStatusIcon = styled.div`
+  width: 112px;
+  height: 112px;
+  margin: 24px 0;
+`
+
+export const TransactionStatusText = styled.div`
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 27px;
+
+  text-align: center;
+  letter-spacing: 0.02em;
+`
+
+export const TransactionStatusDescription = styled.div`
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 20px;
+  text-align: center;
+  color: ${(p) => p.theme.color.text02};
+
+  padding: 8px 16px;
+  flex-grow: 1;
+`
+
+export const ButtonRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  width: 100%;
+  padding-bottom: 32px;
+`
+
+export const LinkIcon = styled.div`
+  width: 12px;
+  height: 12px;
+  background: url(${LinkSvg});
+  margin-left: 8px;
+`
+
+export const DetailButton = styled(WalletButton)`
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 20px;
+  text-align: center;
+  color: ${p => p.theme.color.interactive05};
+  background: none;
+  cursor: pointer;
+  outline: none;
+  border: none;
+  margin: 0;
+  padding: 0;
+`
---
+++ components/brave_wallet_ui/components/extension/post-confirmation/confirming/confirming.tsx
@@ -0,0 +1,84 @@
+// Copyright (c) 2022 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+import * as React from 'react'
+import { useSelector } from 'react-redux'
+
+// Constants
+import { WalletState } from '../../../../constants/types'
+
+// Hooks
+import { useExplorer } from '../../../../common/hooks'
+
+// Utils
+import { getLocale } from '$web-common/locale'
+
+// Styled components
+import { Panel } from '../..'
+import {
+  ConfirmationsNumber,
+  ConfirmingIcon,
+  ConfirmingText,
+  Title
+} from './confirming.style'
+import {
+  ButtonRow,
+  DetailButton,
+  LinkIcon,
+  TransactionStatusDescription
+} from '../common/common.style'
+
+interface Props {
+  headerTitle: string
+  confirmations: number
+  confirmationsNeeded: number
+  onClose: () => void
+}
+
+export const TransactionConfirming = (props: Props) => {
+  const {
+    headerTitle,
+    confirmations,
+    confirmationsNeeded,
+    onClose
+  } = props
+
+  // redux
+  const selectedNetwork = useSelector((state: { wallet: WalletState }) =>
+    state.wallet.selectedNetwork)
+  const onClickViewOnBlockExplorer = useExplorer(selectedNetwork)
+
+  return (
+    <Panel
+      navAction={onClose}
+      title={headerTitle}
+      headerStyle='slim'
+    >
+      <ConfirmingIcon>
+        <ConfirmingText>
+          {getLocale('braveWalletTransactionConfirmingText')}
+        </ConfirmingText>
+        <ConfirmationsNumber>
+          {confirmations} / {confirmationsNeeded}
+        </ConfirmationsNumber>
+      </ConfirmingIcon>
+
+      <Title>
+        {getLocale('braveWalletTransactionConfirmingTitle')}
+      </Title>
+
+      <TransactionStatusDescription>
+        {getLocale('braveWalletTransactionConfirmingDescription')}
+      </TransactionStatusDescription>
+      <ButtonRow>
+        <DetailButton
+          onSubmit={onClickViewOnBlockExplorer('tx', '0x1')}
+        >
+          {getLocale('braveWalletTransactionExplorer')}
+        </DetailButton>
+        <LinkIcon />
+      </ButtonRow>
+    </Panel>
+  )
+}
