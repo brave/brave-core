0ae229cc14e33cb78d2e9ff9ebe4d0ec0261700e
Sergey P <spylogsster@gmail.com>
2022-05-21 16:29:41 +0300

    Use rust::Slice in bls_private_key_to_public_key

--- browser/brave_wallet/filecoin_keyring_unittest.cc
+++ browser/brave_wallet/filecoin_keyring_unittest.cc
@@ -17,9 +17,8 @@ namespace {
 std::vector<uint8_t> GetPublicKey(const std::string& private_key_hex) {
   std::vector<uint8_t> private_key;
   base::HexStringToBytes(private_key_hex, &private_key);
-  std::array<uint8_t, 32> payload;
-  std::copy_n(private_key.begin(), 32, payload.begin());
-  auto result = filecoin::bls_private_key_to_public_key(payload);
+  auto result = filecoin::bls_private_key_to_public_key(
+      rust::Slice<const uint8_t>{private_key.data(), private_key.size()});
   std::vector<uint8_t> public_key(result.begin(), result.end());
   return public_key;
 }
--- components/filecoin/rs/src/lib.rs
+++ components/filecoin/rs/src/lib.rs
@@ -6,7 +6,7 @@ use crate::signature::transaction_sign;
 #[cxx::bridge(namespace = filecoin)]
 mod ffi {
     extern "Rust" {
-      fn bls_private_key_to_public_key(private_key: &[u8; 32]) -> [u8; 48];
+      fn bls_private_key_to_public_key(private_key: &[u8]) -> [u8; 48];
       fn transaction_sign(transaction: &str, private_key: &[u8]) -> String;
     }
 }
@@ -14,7 +14,7 @@ mod ffi {
 /// Generates a public key from the private key
 /// Original implementation in Filecoin xFFI project:
 /// <https://github.com/filecoin-project/filecoin-ffi/blob/ac631c8fc34ec957e3aa71eeddf12d33f6382d8f/rust/src/bls/api.rs#L324>
-fn bls_private_key_to_public_key(private_key: &[u8; 32]) -> [u8; 48] {
+fn bls_private_key_to_public_key(private_key: &[u8]) -> [u8; 48] {
     let mut public_key: [u8; 48] = [0; 48];
     let wrapped_private_key = bls_signatures::PrivateKey::from_bytes(private_key);
     if let Ok(wrapped_private_key) = wrapped_private_key {
