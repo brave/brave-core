0133575089a8adc8e1a760e6e69d7a2eb2cce8df
Taher <nullhook@users.noreply.github.com>
2022-05-23 15:34:42 -0700

    Added country flag icons to vpn panel

---
+++ components/brave_vpn/resources/panel/components/flag/index.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+interface Props {
+  countryCode?: string
+}
+
+export const IconBox = styled.span`
+  width: 24px;
+  height: 24px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+`
+
+const COUNTRIES = ['GB', 'US', 'CH', 'ES', 'SG', 'NL', 'JP', 'DE', 'FR', 'CA', 'AU']
+
+function Flag (props: Props) {
+  let [url, setUrl] = React.useState(undefined)
+
+  React.useEffect(() => {
+    let canceled = false
+    const updateSvgUrl = async () => {
+      const svgUrl = await import(`../../assets/country-flags/${props.countryCode}.svg`)
+      setUrl(svgUrl.default)
+    }
+
+    if (COUNTRIES.includes(props?.countryCode ?? '') && !canceled) {
+      updateSvgUrl()
+    }
+
+    return () => {
+      canceled = true
+    }
+  }, [props?.countryCode])
+
+  if (url) {
+    return (
+      <IconBox>
+        <img src={url} />
+      </IconBox>
+    )
+  }
+
+  return null
+}
+
+export default Flag
--- components/brave_vpn/resources/panel/stories/mock-data/region-list.ts
+++ components/brave_vpn/resources/panel/stories/mock-data/region-list.ts
@@ -2,66 +2,79 @@ export const mockRegionList = [
   {
     'continent': 'oceania',
     'name': 'au-au',
-    'namePretty': 'Australia'
-  },
-  {
-    'continent': 'south-america',
-    'name': 'sa-br',
-    'namePretty': 'Brazil'
+    'namePretty': 'Australia',
+    'countryIsoCode': 'AU'
   },
   {
     'continent': 'north-america',
     'name': 'ca-east',
-    'namePretty': 'Canada'
+    'namePretty': 'Canada',
+    'countryIsoCode': 'CA'
+  },
+  {
+    'continent': 'europe',
+    'name': 'eu-fr',
+    'namePretty': 'France',
+    'countryIsoCode': 'FR'
   },
   {
     'continent': 'europe',
     'name': 'eu-de',
-    'namePretty': 'Germany'
+    'namePretty': 'Germany',
+    'countryIsoCode': 'DE'
   },
   {
     'continent': 'asia',
     'name': 'asia-jp',
-    'namePretty': 'Japan'
+    'namePretty': 'Japan',
+    'countryIsoCode': 'JP'
   },
   {
     'continent': 'europe',
     'name': 'eu-nl',
-    'namePretty': 'Netherlands'
+    'namePretty': 'Netherlands',
+    'countryIsoCode': 'NL'
   },
   {
     'continent': 'asia',
     'name': 'asia-sg',
-    'namePretty': 'Singapore'
+    'namePretty': 'Singapore',
+    'countryIsoCode': 'SG'
   },
   {
     'continent': 'europe',
     'name': 'eu-es',
-    'namePretty': 'Spain'
+    'namePretty': 'Spain',
+    'countryIsoCode': 'ES'
   },
   {
     'continent': 'europe',
     'name': 'eu-ch',
-    'namePretty': 'Switzerland'
+    'namePretty': 'Switzerland',
+    'countryIsoCode': 'CH'
   },
   {
     'continent': 'north-america',
     'name': 'us-central',
-    'namePretty': 'USA (Central)'
+    'namePretty': 'USA (Central)',
+    'countryIsoCode': 'US'
   },
   {
     'continent': 'north-america',
     'name': 'us-east',
-    'namePretty': 'USA (East)'
+    'namePretty': 'USA (East)',
+    'countryIsoCode': 'US'
   },
   {
     'continent': 'north-america',
     'name': 'us-west',
-    'namePretty': 'USA (West)'
+    'namePretty': 'USA (West)',
+    'countryIsoCode': 'US'
   },
   {
     'continent': 'europe',
     'name': 'eu-en',
-    'namePretty': 'United Kingdom'
+    'namePretty': 'United Kingdom',
+    'countryIsoCode': 'GB'
   }
 ]
