e014dadfdc8a64d7cd1a8c5e37e4db6dc406899a
Jay Harris <jay.harris@outlook.co.nz>
2022-05-24 12:11:47 +1200

    Add new test that you can't consume tokens in the wrong frame

--- browser/brave_talk/brave_talk_api_browsertest.cc
+++ browser/brave_talk/brave_talk_api_browsertest.cc
@@ -210,7 +210,6 @@ IN_PROC_BROWSER_TEST_F(BraveTalkAPIBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(BraveTalkAPIBrowserTest,
                        SubFrameOriginChangeInvalidatesRequest) {
-
   auto device_id = GetDeviceID("frame");
 
   // We should have a share request for the |target_contents()| now.
@@ -219,6 +218,21 @@ IN_PROC_BROWSER_TEST_F(BraveTalkAPIBrowserTest,
       target_contents()->GetMainFrame()->GetRoutingID()));
 
   SetRequesterFrameOrigins("talk.brave.com", "example.com");
+
+  EXPECT_TRUE(content::ExecJs(requester_contents(),
+                              "delegateCaptureToFrame('" + device_id + "');"));
+  EXPECT_EQ(false,
+            content::EvalJs(requester_sub_frame(), "startCapturePromise"));
+}
+
+IN_PROC_BROWSER_TEST_F(BraveTalkAPIBrowserTest,
+                       CantConsumeMainFrameTokenInSubFrame) {
+  auto device_id = GetDeviceID("");
+
+  // We should have a share request for the |target_contents()| now.
+  EXPECT_TRUE(registry()->VerifyRequest(
+      target_contents()->GetMainFrame()->GetProcess()->GetID(),
+      target_contents()->GetMainFrame()->GetRoutingID()));
 
   EXPECT_TRUE(content::ExecJs(requester_contents(),
                               "delegateCaptureToFrame('" + device_id + "');"));
