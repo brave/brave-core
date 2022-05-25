e90d4e6f44ffaf3d19d96254a77758513f6ac798
Terry Mancey <terry.mancey@icloud.com>
2022-05-25 13:46:36 +0100

    Brave Search Result ads should use search.anonymous.ads.*.com host when spending confirmation tokens

---
+++ vendor/bat-native-ads/src/bat/ads/internal/account/utility/redeem_unblinded_token/redeem_unblinded_token_util.cc
@@ -0,0 +1,20 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "bat/ads/internal/account/utility/redeem_unblinded_token/redeem_unblinded_token_util.h"
+
+#include "base/strings/stringprintf.h"
+#include "bat/ads/ad_type.h"
+#include "bat/ads/internal/server/url/hosts/server_host_util.h"
+
+namespace ads {
+
+std::string GetAnonymousHost(const AdType& ad_type) {
+  return base::StringPrintf("%s%s",
+                            ad_type == AdType::kSearchResultAd ? "search." : "",
+                            server::GetAnonymousHost().c_str());
+}
+
+}  // namespace ads
