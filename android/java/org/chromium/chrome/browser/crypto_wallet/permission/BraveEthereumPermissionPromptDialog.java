a055fa38f3233ebfdd180781a9aad49ea085d7e6
Pavneet-Sing <psingh@brave.com>
2022-05-25 21:12:14 +0530

    fix(wallet): enable cancel if account selected - fix multiple account select/deselect crash

--- android/java/org/chromium/chrome/browser/crypto_wallet/permission/BraveEthereumPermissionPromptDialog.java
+++ android/java/org/chromium/chrome/browser/crypto_wallet/permission/BraveEthereumPermissionPromptDialog.java
@@ -9,6 +9,8 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Rect;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,10 +22,14 @@ import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.NativeMethods;
 import org.chromium.brave_wallet.mojom.AccountInfo;
 import org.chromium.brave_wallet.mojom.BraveWalletConstants;
+import org.chromium.brave_wallet.mojom.BraveWalletService;
 import org.chromium.brave_wallet.mojom.KeyringService;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.app.BraveActivity;
 import org.chromium.chrome.browser.crypto_wallet.KeyringServiceFactory;
+import org.chromium.chrome.browser.crypto_wallet.util.Utils;
+import org.chromium.chrome.browser.crypto_wallet.util.WalletConstants;
+import org.chromium.components.browser_ui.modaldialog.ModalDialogView;
 import org.chromium.content_public.browser.ImageDownloadCallback;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.mojo.bindings.ConnectionErrorHandler;
@@ -57,6 +63,8 @@ public class BraveEthereumPermissionPromptDialog
     private int mRequestId; // Used for favicon downloader
     private KeyringService mKeyringService;
     private boolean mMojoServicesClosed;
+    private BraveWalletService mBraveWalletService;
+    private View mPermissionDialogPositiveButton;
 
     @CalledByNative
     private static BraveEthereumPermissionPromptDialog create(long nativeDialogController,
@@ -109,6 +117,26 @@ public class BraveEthereumPermissionPromptDialog
         if (activity != null) {
             activity.dismissWalletPanelOrDialog();
         }
+        ViewGroup container = getPermissionModalViewContainer(customView);
+        mPermissionDialogPositiveButton =
+                container.findViewById(activity.getResources().getIdentifier(
+                        WalletConstants.PERMISSION_DIALOG_POSITIVE_BUTTON_ID,
+                        WalletConstants.RESOURCE_ID, activity.getPackageName()));
+        if (mPermissionDialogPositiveButton != null) {
+            mPermissionDialogPositiveButton.setEnabled(false);
+        }
+    }
+
+    @NonNull
+    private ViewGroup getPermissionModalViewContainer(View customView) {
+        ViewParent viewParent = customView.getParent();
+        while (viewParent.getParent() != null) {
+            viewParent = viewParent.getParent();
+            if (viewParent instanceof ModalDialogView) {
+                break;
+            }
+        }
+        return (ViewGroup) viewParent;
     }
 
     private void initAccounts() {
@@ -117,8 +145,19 @@ public class BraveEthereumPermissionPromptDialog
             if (keyringInfo == null) {
                 return;
             }
-            mAccountsListAdapter = new BraveEthereumPermissionAccountsListAdapter(
-                    keyringInfo.accountInfos, true, null);
+            mAccountsListAdapter =
+                    new BraveEthereumPermissionAccountsListAdapter(keyringInfo.accountInfos, true,
+                            new BraveEthereumPermissionAccountsListAdapter
+                                    .BraveEthereumPermissionDelegate() {
+                                        @Override
+                                        public void onAccountCheckChanged(
+                                                AccountInfo account, boolean isChecked) {
+                                            if (mPermissionDialogPositiveButton != null) {
+                                                mPermissionDialogPositiveButton.setEnabled(
+                                                        getSelectedAccounts().length > 0);
+                                            }
+                                        }
+                                    });
             mRecyclerView.setAdapter(mAccountsListAdapter);
             LinearLayoutManager layoutManager = new LinearLayoutManager(mContext);
             mRecyclerView.setLayoutManager(layoutManager);
