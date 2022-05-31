ad49b42dfa06e052be019178df15d04f6575fabc
mkarolin <max@brave.com>
2022-05-31 19:09:36 -0400

    kDisableClientSidePhishingDetection switch was removed.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/96308cdad6d81c4227ed61eecda71495bca374ae
    
    commit 96308cdad6d81c4227ed61eecda71495bca374ae
    Author: Daniel Rubery <drubery@chromium.org>
    Date:   Tue May 17 19:33:01 2022 +0000
    
        Remove --disable-client-side-phishing-detection
    
        This flag is not used anymore, and can be replaced entirely with the
        check for Safe Browsing preference level.
    
        Fixed: 360220

---
+++ chromium_src/chrome/browser/safe_browsing/client_side_detection_service_factory.h
@@ -0,0 +1,23 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_SAFE_BROWSING_CLIENT_SIDE_DETECTION_SERVICE_FACTORY_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_SAFE_BROWSING_CLIENT_SIDE_DETECTION_SERVICE_FACTORY_H_
+
+#define ClientSideDetectionServiceFactory \
+  ClientSideDetectionServiceFactory_ChromiumImpl
+#include "src/chrome/browser/safe_browsing/client_side_detection_service_factory.h"
+#undef ClientSideDetectionServiceFactory
+
+namespace safe_browsing {
+
+class ClientSideDetectionServiceFactory {
+ public:
+  static ClientSideDetectionService* GetForProfile(Profile* profile);
+};
+
+}  // namespace safe_browsing
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_SAFE_BROWSING_CLIENT_SIDE_DETECTION_SERVICE_FACTORY_H_
