a3e39634699e91737c4ca6f8c9f442fff0904201
Andreea Zaharia <andreea.zaharia10@gmail.com>
2022-06-03 15:15:03 +0100

    Synthetic data generation for experiments

--- components/brave_federated/synthetic_dataset/synthetic_dataset.cc
+++ components/brave_federated/synthetic_dataset/synthetic_dataset.cc
@@ -4,6 +4,10 @@
 #include <cmath>
 #include <iostream>
 #include <random>
+#include <fstream>
+#include <chrono>
+
+#include "brave/components/brave_federated/linear_algebra_util/linear_algebra_util.h"
 
 namespace brave_federated {
 
@@ -53,6 +57,161 @@ SyntheticDataset::SyntheticDataset(std::vector<float> ms,
   this->data_points_ = data_points;
 }
 
+SyntheticDataset::SyntheticDataset(float alpha,
+                                   float beta,
+                                   int num_features,
+                                   size_t size) {
+  
+  // Generate time of day and day of week uniformly.
+  std::uniform_int_distribution<> distrday(0, 7);
+  std::uniform_int_distribution<> distrtime(0, 144);
+
+
+  std::default_random_engine generator;  
+  generator.seed(std::chrono::system_clock::now().time_since_epoch().count());
+  std::cout <<"SEED" << std::chrono::system_clock::now().time_since_epoch().count() << "\n";
+
+  std::normal_distribution<float> normal_zero_alpha(0.0, alpha);
+  std::normal_distribution<float> normal_zero_beta(0.0, beta);
+
+  std::vector<float> cov_x(num_features, 0.0);
+  for (int j = 0; j < num_features; j++) {
+    cov_x[j] = pow((j + 1), -1.2);
+  }
+
+  std::vector<float> mean_x(num_features, 1.0);
+  std::normal_distribution<float> normal_aux_1(normal_zero_beta(generator),
+                                               1.0);
+  for (int i = 0; i < num_features; i++) {
+    mean_x[i] = normal_aux_1(generator);
+  }
+
+  float mean_W = normal_zero_alpha(generator);
+  std::vector<std::vector<float>> W(2, std::vector<float>(num_features));
+  std::vector<float> b(2);
+  std::normal_distribution<float> normal_W(mean_W, 1.0);
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < num_features; j++) {
+      W[i][j] = normal_W(generator);
+    }
+    b[i] = normal_W(generator);
+  }
+
+  std::vector<std::vector<float>> xs(size,
+                                     std::vector<float>(num_features, 0.0));
+  
+  for (int j = 0; j < (int) size; j++) {
+    int day_index = distrday(generator);
+    int time_index = distrtime(generator);
+
+    xs[j][0] = sin(day_index * 2.0 * M_PI / 7.0);
+    xs[j][1] = cos(day_index * 2.0 * M_PI / 7.0);
+
+    xs[j][2] = sin(time_index * 2.0 * M_PI / 144.0);
+    xs[j][3] = cos(time_index * 2.0 * M_PI / 144.0);
+  }
+
+  for (int i = 4; i < (int)num_features; i++) {
+    std::normal_distribution<float> normal_i(mean_x[i], cov_x[i]);
+    for (int j = 0; j < (int) size; j++) {
+      xs[j][i] = normal_i(generator);
+    }
+  }
+
+  std::vector<std::vector<float>> data_points;
+  for (int i = 0; i < (int) size; i++) {
+    std::vector<float> tmp = LinearAlgebraUtil::AddVectors(
+        LinearAlgebraUtil::MultiplyMatrixVector(W, xs[i]), b);
+
+    float ymax = 0.0;
+    // float val_ymax = -100.0;
+    
+    if (Softmax(tmp[0]) >= Softmax(tmp[1])) {
+      ymax = 1.0;
+    }
+    else {
+      ymax = 0.0;
+    }
+
+    std::vector<float> data_point;
+    data_point.insert(data_point.end(), xs[i].begin(), xs[i].end());
+    data_point.push_back(ymax);
+
+    data_points.push_back(data_point);
+  }
+
+  this->data_points_ = data_points;
+}
+
+SyntheticDataset::SyntheticDataset(std::vector<std::vector<float>> W,
+                                   std::vector<float> b,
+                                   int num_features,
+                                   size_t size) {
+  
+  // Generate time of day and day of week uniformly.
+  std::uniform_int_distribution<> distrday(0, 7);
+  std::uniform_int_distribution<> distrtime(0, 144);
+
+  std::default_random_engine generator;  
+  generator.seed(std::chrono::system_clock::now().time_since_epoch().count());
+  std::cout <<"SEED" << std::chrono::system_clock::now().time_since_epoch().count() << "\n";
+
+  std::vector<float> cov_x(num_features, 0.0);
+  for (int j = 0; j < num_features; j++) {
+    cov_x[j] = pow((j + 1), -1.2);
+  }
+
+  std::vector<float> mean_x(num_features, 0.0);
+
+  std::vector<std::vector<float>> xs(size,
+                                     std::vector<float>(num_features, 0.0));
+  
+  for (int j = 0; j < (int) size; j++) {
+    int day_index = distrday(generator);
+    int time_index = distrtime(generator);
+
+    xs[j][0] = sin(day_index * 2.0 * M_PI / 7.0);
+    xs[j][1] = cos(day_index * 2.0 * M_PI / 7.0);
+
+    xs[j][2] = sin(time_index * 2.0 * M_PI / 144.0);
+    xs[j][3] = cos(time_index * 2.0 * M_PI / 144.0);
+  }
+
+  for (int i = 4; i < (int)num_features; i++) {
+    std::normal_distribution<float> normal_i(mean_x[i], cov_x[i]);
+    for (int j = 0; j < (int) size; j++) {
+      xs[j][i] = normal_i(generator);
+    }
+  }
+
+  std::vector<std::vector<float>> data_points;
+  for (int i = 0; i < (int) size; i++) {
+    std::vector<float> tmp = LinearAlgebraUtil::AddVectors(
+        LinearAlgebraUtil::MultiplyMatrixVector(W, xs[i]), b);
+
+    float ymax = 0.0;
+    // float val_ymax = -100.0;
+    
+    if (Softmax(tmp[0]) >= Softmax(tmp[1])) {
+      ymax = 1.0;
+    }
+    else {
+      ymax = 0.0;
+    }
+
+    std::vector<float> data_point;
+    data_point.insert(data_point.end(), xs[i].begin(), xs[i].end());
+    data_point.push_back(ymax);
+
+    data_points.push_back(data_point);
+  }
+
+  this->data_points_ = data_points;
+}
+
+
+SyntheticDataset::SyntheticDataset(std::vector<std::vector<float>> data_points) : data_points_(data_points) {}
+
 SyntheticDataset::~SyntheticDataset() {}
 
 size_t SyntheticDataset::size() {
@@ -65,6 +224,33 @@ int SyntheticDataset::get_features_count() {
 
 std::vector<std::vector<float>> SyntheticDataset::DataPoints() {
   return this->data_points_;
+}
+
+SyntheticDataset SyntheticDataset::SeparateTestData(int num_training) {
+  std::vector<std::vector<float>> split_lo(this->data_points_.begin(), this->data_points_.begin() + num_training);
+  std::vector<std::vector<float>> split_hi(this->data_points_.begin() + num_training, this->data_points_.end());
+
+  this->data_points_ = split_lo;
+  return SyntheticDataset(split_hi);
+}
+
+float SyntheticDataset::Softmax(float z) {
+  return 1.0 / (1 + exp(-1.0 * z));
+}
+
+void SyntheticDataset::DumpToCSV(std::string prefix) {
+  std::fstream csv_data;
+
+  std::string csv_name = prefix + std::to_string(std::chrono::system_clock::now().time_since_epoch().count()) + ".csv";
+  csv_data.open(csv_name, std::ios::out);
+
+  for (int i = 0; i < (int) data_points_.size(); i++) {
+    for (int j = 0; j < (int) data_points_[i].size(); j++) {
+      csv_data << data_points_[i][j] << ",";
+    }
+    csv_data << "\n";
+  }
+  csv_data.close();
 }
 
 }  // namespace brave_federated
--- components/brave_federated/synthetic_dataset/synthetic_dataset.h
+++ components/brave_federated/synthetic_dataset/synthetic_dataset.h
@@ -10,6 +10,12 @@ class SyntheticDataset {
   // Generates the synthetic dataset of size size around given vector m of size
   // ms_size and given bias b.
   SyntheticDataset(std::vector<float> ms, float b, size_t size);
+  SyntheticDataset(float alpha, float beta, int num_features, size_t size);
+  SyntheticDataset(std::vector<std::vector<float>> data_points);
+  SyntheticDataset(std::vector<std::vector<float>> W,
+                                     std::vector<float> b,
+                                     int num_features,
+                                     size_t size);
 
   ~SyntheticDataset();
 
@@ -19,10 +25,16 @@ class SyntheticDataset {
   // Returns the dataset.
   std::vector<std::vector<float>> DataPoints();
 
+  SyntheticDataset SeparateTestData(int num_training);
+
   int get_features_count();
+
+  void DumpToCSV(std::string prefix);
 
  private:
   std::vector<std::vector<float>> data_points_;
+  
+  float Softmax(float z);
 };
 
 }  // namespace brave_federated
