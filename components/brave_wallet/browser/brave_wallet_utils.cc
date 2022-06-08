08421cbdcf6da237352db97d44c9f6a8082a9782
Anton Paymyshev <anton.paymyshev@gmail.com>
2022-06-08 13:33:01 +0700

    Uplift of #13680 (squashed) to beta

--- components/brave_wallet/browser/brave_wallet_utils.cc
+++ components/brave_wallet/browser/brave_wallet_utils.cc
@@ -90,7 +90,7 @@ const brave_wallet::mojom::NetworkInfo kKnownEthNetworks[] = {
      {},
      {},
      "ETH",
-     "Ether",
+     "Ethereum",
      18,
      brave_wallet::mojom::CoinType::ETH,
      mojom::NetworkInfoData::NewEthData(mojom::NetworkInfoDataETH::New(true))},
