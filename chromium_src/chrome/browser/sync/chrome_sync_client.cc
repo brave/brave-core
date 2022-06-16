44258cd6bbe05ef2296bdf7f3a799099f3217206
Szilard Szaloki <szilardszaloki@gmail.com>
2022-06-15 22:10:34 -0500

    Android support

--- chromium_src/chrome/browser/sync/chrome_sync_client.cc
+++ chromium_src/chrome/browser/sync/chrome_sync_client.cc
@@ -6,9 +6,46 @@
 #include "chrome/browser/sync/chrome_sync_client.h"
 
 #include "build/buildflag.h"
+#if BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/sync/trusted_vault_client_android.h"
+#endif
 #include "components/sync/driver/sync_service.h"
 #include "components/sync/driver/sync_user_settings.h"
+#include "components/sync/driver/trusted_vault_client.h"
 #include "extensions/buildflags/buildflags.h"
+
+namespace {
+
+struct NoOpTrustedVaultClient : syncer::TrustedVaultClient {
+  void AddObserver(Observer* observer) override {}
+
+  void RemoveObserver(Observer* observer) override {}
+
+  void FetchKeys(
+      const CoreAccountInfo& account_info,
+      base::OnceCallback<void(const std::vector<std::vector<uint8_t>>&)> cb)
+      override {}
+
+  void MarkLocalKeysAsStale(const CoreAccountInfo& account_info,
+                            base::OnceCallback<void(bool)> cb) override {}
+
+  void StoreKeys(const std::string& gaia_id,
+                 const std::vector<std::vector<uint8_t>>& keys,
+                 int last_key_version) override {}
+
+  void GetIsRecoverabilityDegraded(const CoreAccountInfo& account_info,
+                                   base::OnceCallback<void(bool)> cb) override {
+  }
+
+  void AddTrustedRecoveryMethod(const std::string& gaia_id,
+                                const std::vector<uint8_t>& public_key,
+                                int method_type_hint,
+                                base::OnceClosure cb) override {}
+
+  void ClearDataForAccount(const CoreAccountInfo& account_info) override {}
+};
+
+}  // namespace
 
 #if !BUILDFLAG(ENABLE_EXTENSIONS)
 
@@ -16,6 +53,8 @@
 #define CHROME_BROWSER_WEB_APPLICATIONS_WEB_APP_SYNC_BRIDGE_H_
 
 #endif  // !BUILDFLAG(ENABLE_EXTENSIONS)
+
+#define TrustedVaultClientAndroid NoOpTrustedVaultClient
 
 // Default enabled type is Bookmarks.
 #define OnLocalSyncTransportDataCleared                                       \
@@ -31,3 +70,4 @@
 
 #include "src/chrome/browser/sync/chrome_sync_client.cc"
 #undef OnLocalSyncTransportDataCleared
+#undef TrustedVaultClientAndroid
