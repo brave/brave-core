f1dd849dcf4e1c3326baeddfc0ff2161305c1512
Sergey P <spylogsster@gmail.com>
2022-06-17 14:14:50 +0300

    Enable skus feature for tests

--- renderer/skus/js_skus_browsertest.cc
+++ renderer/skus/js_skus_browsertest.cc
@@ -4,6 +4,9 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/components/skus/renderer/skus_render_frame_observer.h"
+
+#include "base/test/scoped_feature_list.h"
+#include "brave/components/skus/common/features.h"
 #include "chrome/common/chrome_isolated_world_ids.h"
 #include "content/public/test/content_mock_cert_verifier.h"
 #include "content/public/test/render_view_test.h"
@@ -13,7 +16,9 @@ namespace skus {
 
 class JsSkusBrowserTest : public content::RenderViewTest {
  public:
-  JsSkusBrowserTest() = default;
+  JsSkusBrowserTest() {
+    scoped_feature_list_.InitWithFeatures({skus::features::kSkusFeature}, {});
+  }
   ~JsSkusBrowserTest() override = default;
 
   bool ExecuteJavascript(const std::u16string& script) {
@@ -21,6 +26,9 @@ class JsSkusBrowserTest : public content::RenderViewTest {
     EXPECT_TRUE(ExecuteJavaScriptAndReturnIntValue(script, &result));
     return result == 1;
   }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 TEST_F(JsSkusBrowserTest, AttachSkus) {
