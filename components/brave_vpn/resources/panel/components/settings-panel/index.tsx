ba9df34151fa056b94fd1fd9d6e5ff7072a69f39
Sergey P <spylogsster@gmail.com>
2022-06-21 15:36:35 +0300

    Added space handlers for settings panel view

--- components/brave_vpn/resources/panel/components/settings-panel/index.tsx
+++ components/brave_vpn/resources/panel/components/settings-panel/index.tsx
@@ -7,14 +7,21 @@ import getPanelBrowserAPI from '../../api/panel_browser_api'
 
 interface Props {
   closeSettingsPanel: React.MouseEventHandler<HTMLButtonElement>
-  showContactSupport: React.MouseEventHandler<HTMLAnchorElement>
+  showContactSupport: () => void
 }
 
 function SettingsPanel (props: Props) {
   const handleClick = (entry: string) => {
     getPanelBrowserAPI().panelHandler.openVpnUI(entry)
   }
-
+  const handleKeyDown = (entry: string, event: React.KeyboardEvent<HTMLAnchorElement>) => {
+    if (event.keyCode !== 32) { return }
+    if (entry === 'support') {
+      props.showContactSupport()
+      return
+    }
+    handleClick(entry)
+  }
   return (
     <S.Box>
       <S.PanelContent>
@@ -30,17 +37,17 @@ function SettingsPanel (props: Props) {
         </S.PanelHeader>
         <S.List>
           <li>
-            <a href="#" onClick={handleClick.bind(this, 'manage')}>
+            <a href="#" onClick={handleClick.bind(this, 'manage')} onKeyDown={handleKeyDown.bind(this, 'manage')}>
               {getLocale('braveVpnManageSubscription')}
             </a>
           </li>
           <li>
-            <a href="#" onClick={props.showContactSupport}>
+            <a href="#" onClick={props.showContactSupport} onKeyDown={handleKeyDown.bind(this, 'support')}>
               {getLocale('braveVpnContactSupport')}
             </a>
           </li>
           <li>
-            <a href="#" onClick={handleClick.bind(this, 'about')}>
+            <a href="#" onClick={handleClick.bind(this, 'about')} onKeyDown={handleKeyDown.bind(this, 'about')}>
               {getLocale('braveVpnAbout')}
               {' '}
               {getLocale('braveVpn')}
