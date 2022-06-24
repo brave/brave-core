fe6af5765246e37d5b9be2025e657b5d0726d3b4
brave-builds <devops@brave.com>
2022-06-24 14:30:53 +0000

    Uplift of #13930 (squashed) to beta

--- android/java/org/chromium/chrome/browser/crypto_wallet/util/AndroidUtils.java
+++ android/java/org/chromium/chrome/browser/crypto_wallet/util/AndroidUtils.java
@@ -1,6 +1,9 @@
 package org.chromium.chrome.browser.crypto_wallet.util;
 
 import android.content.Context;
+import android.os.Build;
+import android.text.Html;
+import android.text.Spanned;
 import android.util.TypedValue;
 import android.view.View;
 
@@ -30,6 +33,14 @@ public class AndroidUtils {
         if (view != null) {
             view.setEnabled(false);
             view.setClickable(false);
+        }
+    }
+
+    public static Spanned formateHTML(String html) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY);
+        } else {
+            return Html.fromHtml(html);
         }
     }
 }
