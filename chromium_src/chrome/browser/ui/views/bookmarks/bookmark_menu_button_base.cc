e56910458c8d826d0998448a8b2d732043dec4d0
Simon Hong <shong@brave.com>
2022-07-01 21:05:06 +0900

    Merge pull request #14003 from brave/fix_bookmark_bar_button_highlight_path
    Fixed bookmark bar button highlight path regression

---
+++ chromium_src/chrome/browser/ui/views/bookmarks/bookmark_menu_button_base.cc
@@ -0,0 +1,18 @@
+/* Copyright 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+namespace views {
+
+class View;
+
+void DontInstallHighlightPathGenerator(View* view) {
+  // Do nothing: the default highlight path is what we want.
+}
+
+}  // namespace views
+
+#define InstallPillHighlightPathGenerator DontInstallHighlightPathGenerator
+#include "src/chrome/browser/ui/views/bookmarks/bookmark_menu_button_base.cc"
+#undef InstallPillHighlightPathGenerator
