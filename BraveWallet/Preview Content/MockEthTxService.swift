e48e1d25b3d7d6d5156fd8aa2ecf6783c10baca4
Michał Buczek <michal.buczek@protonmail.com>
2022-07-14 20:08:02 +0200

    Fix #5643: Update brave-core to 1.41.97. (#5691)

--- BraveWallet/Preview Content/MockEthTxService.swift
+++ BraveWallet/Preview Content/MockEthTxService.swift
@@ -54,6 +54,10 @@ class MockTxService: BraveWalletTxService {
   func approveTransaction(_ coinType: BraveWallet.CoinType, txMetaId: String, completion: @escaping (Bool, BraveWallet.ProviderErrorUnion, String) -> Void) {
     completion(false, .init(providerError: .internalError), "Error Message")
   }
+  
+  func transactionMessage(toSign coinType: BraveWallet.CoinType, txMetaId: String, completion: @escaping (BraveWallet.MessageToSignUnion?) -> Void) {
+    completion(.none)
+  }
 }
 
 #endif
