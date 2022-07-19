b4d77e49bb61d13f41be1cf82b8cde00c7c2eefe
Claudio DeSouza <cdesouza@igalia.com>
2022-07-19 11:14:39 +0100

    Lint error fixes
    This has been triggered by the CI for some reason, so it is being
    submitted in a separate change for correction.

--- vendor/bat-native-ledger/src/bat/ledger/internal/legacy/bat_helper.h
+++ vendor/bat-native-ledger/src/bat/ledger/internal/legacy/bat_helper.h
@@ -3,8 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef BRAVELEDGER_BAT_HELPER_H_
-#define BRAVELEDGER_BAT_HELPER_H_
+#ifndef BRAVE_VENDOR_BAT_NATIVE_LEDGER_SRC_BAT_LEDGER_INTERNAL_LEGACY_BAT_HELPER_H_
+#define BRAVE_VENDOR_BAT_NATIVE_LEDGER_SRC_BAT_LEDGER_INTERNAL_LEGACY_BAT_HELPER_H_
 
 #include <string>
 #include <vector>
@@ -41,4 +41,4 @@ bool HasSameDomainAndPath(
 
 }  // namespace braveledger_bat_helper
 
-#endif  // BRAVELEDGER_BAT_HELPER_H_
+#endif  // BRAVE_VENDOR_BAT_NATIVE_LEDGER_SRC_BAT_LEDGER_INTERNAL_LEGACY_BAT_HELPER_H_
