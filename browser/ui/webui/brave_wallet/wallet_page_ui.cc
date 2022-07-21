0a2cddf808ac79d5a67e07f52d240258db7be6a4
William Muli <willi.wambu@gmail.com>
2022-07-21 15:00:24 +0300

    Add string to access via loadTimeData object

--- browser/ui/webui/brave_wallet/wallet_page_ui.cc
+++ browser/ui/webui/brave_wallet/wallet_page_ui.cc
@@ -65,6 +65,8 @@ WalletPageUI::WalletPageUI(content::WebUI* web_ui)
       network::mojom::CSPDirectiveName::FrameSrc,
       std::string("frame-src ") + kUntrustedNftURL + ";");
   source->AddString("braveWalletTrezorBridgeUrl", kUntrustedTrezorURL);
+  source->AddString("braveWalletNftBridgeUrl", kUntrustedNftURL);
+  source->AddString("braveWalletBridgeUrl", kBraveUIWalletURL);
   auto* profile = Profile::FromWebUI(web_ui);
   content::WebUIDataSource::Add(profile, source);
   content::URLDataSource::Add(profile,
