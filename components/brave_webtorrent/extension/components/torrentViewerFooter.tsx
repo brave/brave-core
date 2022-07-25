e1661f3933bc0e3a8ec29256bbeaf43071d61cde
yan <yan@brave.com>
2022-07-25 11:32:44 -0700

    Add proxy to webtorrent warning
    Fix https://github.com/brave/brave-browser/issues/23854

--- components/brave_webtorrent/extension/components/torrentViewerFooter.tsx
+++ components/brave_webtorrent/extension/components/torrentViewerFooter.tsx
@@ -34,11 +34,11 @@ export default class TorrentViewerFooter extends React.PureComponent<
     } else {
       return (
         <div className='footerNotice'>
-          Privacy Warning: When you click "Start Torrent" Brave will begin
+          <b>Privacy Warning:</b> When you click "Start Torrent" Brave will begin
           downloading pieces of the torrent file from other users and uploading to
           them in turn. This action will share that you're downloading this file.
           Others may be able to see what you're downloading and/or determine your
-          public IP address.
+          public IP address. The download may bypass your proxy settings.
           <br /><br />
           The WebTorrent extension can be disabled from Brave settings.
         </div>
