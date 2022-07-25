cbf4b072165f9ddaf0e8bce50bdd34eaed94ff86
Jay Harris <7678024+fallaciousreasoning@users.noreply.github.com>
2022-07-26 09:05:31 +1200

    Disable Save Site button when no text is entered in the URL box. (#14264)

--- components/brave_new_tab_ui/components/default/editTopSite/index.tsx
+++ components/brave_new_tab_ui/components/default/editTopSite/index.tsx
@@ -18,11 +18,7 @@ import {
   StyledButton
 } from './style'
 import CloseIcon from './assets/close-icon'
-
-interface State {
-  title: string
-  url: string
-}
+import { useCallback, useEffect, useState, useRef } from 'react'
 
 interface Props {
   targetTopSiteForEditing?: NewTab.Site
@@ -31,128 +27,80 @@ interface Props {
   onSave: (title: string, url: string, newUrl: string) => void
 }
 
-export default class EditTopSite extends React.PureComponent<Props, State> {
-  editTopSiteDialogRef: React.RefObject<any>
-  constructor (props: Props) {
-    super(props)
-    this.editTopSiteDialogRef = React.createRef()
+export default function EditTopSite ({ targetTopSiteForEditing, textDirection, onClose, onSave }: Props) {
+  const dialogRef = useRef<HTMLDivElement>(null)
+  const [title, setTitle] = useState(targetTopSiteForEditing?.title ?? '')
+  const [url, setUrl] = useState(targetTopSiteForEditing?.url ?? '')
 
-    this.state = {
-      title: (props.targetTopSiteForEditing && props.targetTopSiteForEditing.title) ? props.targetTopSiteForEditing.title : '',
-      url: (props.targetTopSiteForEditing && props.targetTopSiteForEditing.url) ? props.targetTopSiteForEditing.url : ''
+  const saveTopSite = useCallback(() => {
+    if (!url) return
+    onSave(title, targetTopSiteForEditing?.url ?? '', url)
+  }, [url, title, targetTopSiteForEditing, onSave])
+
+  useEffect(() => {
+    const handleKeyPress = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') onClose()
+      if (e.key === 'Enter') saveTopSite()
     }
-  }
 
-  componentDidMount () {
-    document.addEventListener('mousedown', this.handleClickOutside)
-    document.addEventListener('keydown', this.onKeyPressSettings)
-  }
-
-  componentWillUnmount () {
-    document.removeEventListener('mousedown', this.handleClickOutside)
-    document.removeEventListener('keydown', this.onKeyPressSettings)
-  }
-
-  onKeyPressSettings = (event: KeyboardEvent) => {
-    if (event.key === 'Escape') {
-      this.props.onClose()
-    } else if (event.key === 'Enter') {
-      if (this.state.url) {
-        this.saveNewTopSite()
+    const handleClickOutside = (e: Event) => {
+      if (dialogRef.current && !dialogRef.current.contains(e.target as Node)) {
+        onClose()
       }
     }
-  }
 
-  handleClickOutside = (event: Event) => {
-    if (
-      this.editTopSiteDialogRef &&
-      this.editTopSiteDialogRef.current &&
-      !this.editTopSiteDialogRef.current.contains(event.target)
-    ) {
-      this.props.onClose()
+    document.addEventListener('keydown', handleKeyPress)
+    document.addEventListener('mousedown', handleClickOutside)
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress)
+      document.removeEventListener('mousedown', handleClickOutside)
     }
-  }
+  }, [saveTopSite, onClose])
 
-  saveNewTopSite = () => {
-    this.props.onSave(this.state.title,
-                      this.props.targetTopSiteForEditing ? this.props.targetTopSiteForEditing.url
-                                                         : '',
-                      this.state.url)
-  }
-
-  onClickSave = () => {
-    if (this.state.url) {
-      this.saveNewTopSite()
-    } else {
-      // Just close if user don't type anything to url.
-      this.props.onClose()
-    }
-  }
-
-  onTitleInputChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ title: e.target.value })
-  }
-
-  onURLInputChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ url: e.target.value })
-  }
-
-  render () {
-    const {
-      targetTopSiteForEditing,
-      textDirection,
-      onClose
-    } = this.props
-
-    return (
-      <StyledDialogWrapper textDirection={textDirection}>
-        <StyledDialog
-          ref={this.editTopSiteDialogRef}
-          textDirection={textDirection}
-        >
-          <DialogTitle>
-            {targetTopSiteForEditing ? getLocale('editTopSiteDialogTitle')
-                                     : getLocale('addTopSiteDialogTitle')}
-          </DialogTitle>
-          <CloseButton onClick={onClose}>
-            <CloseIcon/>
-          </CloseButton>
-          <StyledInputLabel>
-            {getLocale('addTopSiteDialogNameLabel')}
-          </StyledInputLabel>
-          <StyledInput
-            autoFocus={true}
-            type='text'
-            value={this.state.title}
-            onChange={this.onTitleInputChanged}
-            placeholder={getLocale('addTopSiteDialogNameInputPlaceHolder')}
-          />
-          <StyledInputLabel>
-            {getLocale('addTopSiteDialogURLLabel')}
-          </StyledInputLabel>
-          <StyledInput
-            type='url'
-            value={this.state.url}
-            onChange={this.onURLInputChanged}
-            placeholder={getLocale('addTopSiteDialogURLInputPlaceHolder')}
-          />
-          <StyledButtonsContainer>
-            <StyledButton
-              text={getLocale('addTopSiteDialogCancelButtonLabel')}
-              level={'secondary'}
-              size={'small'}
-              onClick={onClose}
-            />
-            <StyledButton
-              text={getLocale('addTopSiteDialogSaveButtonLabel')}
-              level={'primary'}
-              type={'accent'}
-              size={'small'}
-              onClick={this.onClickSave}
-            />
-          </StyledButtonsContainer>
-        </StyledDialog>
-      </StyledDialogWrapper>
-    )
-  }
+  return <StyledDialogWrapper textDirection={textDirection}>
+    <StyledDialog
+      ref={dialogRef}
+      textDirection={textDirection}>
+      <DialogTitle>
+        {targetTopSiteForEditing
+          ? getLocale('editTopSiteDialogTitle')
+          : getLocale('addTopSiteDialogTitle')}
+      </DialogTitle>
+      <CloseButton onClick={onClose}>
+        <CloseIcon />
+      </CloseButton>
+      <StyledInputLabel>
+        {getLocale('addTopSiteDialogNameLabel')}
+      </StyledInputLabel>
+      <StyledInput
+        autoFocus={true}
+        type='text'
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+        placeholder={getLocale('addTopSiteDialogNameInputPlaceHolder')} />
+      <StyledInputLabel>
+        {getLocale('addTopSiteDialogURLLabel')}
+      </StyledInputLabel>
+      <StyledInput
+        type='url'
+        value={url}
+        onChange={e => setUrl(e.target.value)}
+        placeholder={getLocale('addTopSiteDialogURLInputPlaceHolder')} />
+      <StyledButtonsContainer>
+        <StyledButton
+          text={getLocale('addTopSiteDialogCancelButtonLabel')}
+          level={'secondary'}
+          size={'small'}
+          onClick={onClose} />
+        <StyledButton
+          text={getLocale('addTopSiteDialogSaveButtonLabel')}
+          level={'primary'}
+          type={'accent'}
+          size={'small'}
+          disabled={!url}
+          onClick={saveTopSite} />
+      </StyledButtonsContainer>
+    </StyledDialog>
+  </StyledDialogWrapper>
 }
