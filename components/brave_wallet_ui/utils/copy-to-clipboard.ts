cc371f175fee15cf812287d1cd8f83b6f802c885
William Muli <willi.wambu@gmail.com>
2022-07-25 14:46:19 +0300

    chore: fix unstable test behaviour

--- components/brave_wallet_ui/utils/copy-to-clipboard.ts
+++ components/brave_wallet_ui/utils/copy-to-clipboard.ts
@@ -1,6 +1,6 @@
 export const copyToClipboard = async (data: string) => {
   try {
-    await navigator.clipboard.writeText(data)
+    await navigator.clipboard?.writeText(data)
   } catch (e) {
     console.log(`Could not copy address ${e.toString()}`)
   }
