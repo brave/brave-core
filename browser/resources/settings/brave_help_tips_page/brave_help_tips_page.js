8da3ac52a955bd777247d261f58f9bc315bcd429
Kamil Jozwiak <kamiljoz@gmail.com>
2022-07-26 14:51:51 -0400

    Revert "The content on brave://settings/braveHelpTips disappears upon reload and direct access via the address bar (uplift to 1.42.x)"

--- browser/resources/settings/brave_help_tips_page/brave_help_tips_page.js
+++ browser/resources/settings/brave_help_tips_page/brave_help_tips_page.js
@@ -2,23 +2,15 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
- 
- import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
- import {WebUIListenerMixin} from 'chrome://resources/js/web_ui_listener_mixin.js';
- import {PrefsMixin} from '../prefs/prefs_mixin.js';
- 
- const SettingBraveHelpTipsPageElementBase = WebUIListenerMixin(PrefsMixin(PolymerElement))
 
+(function() {
+  'use strict';
 
-class SettingsBraveHelpTipsPageElement extends SettingBraveHelpTipsPageElementBase {
-  static get is() {
-    return 'settings-brave-help-tips-page'
-  }
-
-  static get template() {
-    return html`{__html_template__}`
-  }
-}
-
-customElements.define(
-  SettingsBraveHelpTipsPageElement.is, SettingsBraveHelpTipsPageElement)
+  /**
+   * 'settings-brave-help-tips-page' is the settings page containing
+   * brave's help tips features.
+   */
+  Polymer({
+    is: 'settings-brave-help-tips-page'
+  });
+})();
