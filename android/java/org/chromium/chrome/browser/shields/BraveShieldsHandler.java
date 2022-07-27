0c418988d589387d1da087e127e12e56d5fbcb53
Deep <deeppandya91@gmail.com>
2022-07-27 04:33:11 -0400

    Set fp BLOCK_THIRD_PARTY to DEFAULT

--- android/java/org/chromium/chrome/browser/shields/BraveShieldsHandler.java
+++ android/java/org/chromium/chrome/browser/shields/BraveShieldsHandler.java
@@ -670,8 +670,11 @@ public class BraveShieldsHandler implements BraveRewardsHelper.LargeIconReadyCal
                     BraveShieldsContentSettings.getShieldsValue(mProfile, mHost, layout);
             if (settingOption.equals(BraveShieldsContentSettings.BLOCK_RESOURCE)) {
                 mBlockShieldsOption1.setChecked(true);
-            } else if (settingOption.equals(
-                               BraveShieldsContentSettings.BLOCK_THIRDPARTY_RESOURCE)) {
+            } else if (settingOption.equals(layout
+                                               == BraveShieldsContentSettings
+                                                          .RESOURCE_IDENTIFIER_FINGERPRINTING
+                                       ? BraveShieldsContentSettings.DEFAULT
+                                       : BraveShieldsContentSettings.BLOCK_THIRDPARTY_RESOURCE)) {
                 mBlockShieldsOption2.setChecked(true);
             } else if (settingOption.equals(BraveShieldsContentSettings.ALLOW_RESOURCE)) {
                 mBlockShieldsOption3.setChecked(true);
@@ -694,7 +697,12 @@ public class BraveShieldsHandler implements BraveRewardsHelper.LargeIconReadyCal
                                 } else if (checkedId == R.id.option2) {
                                     BraveShieldsContentSettings.setShieldsValue(mProfile, mHost,
                                             layout,
-                                            BraveShieldsContentSettings.BLOCK_THIRDPARTY_RESOURCE,
+                                            layout
+                                                            == BraveShieldsContentSettings
+                                                                       .RESOURCE_IDENTIFIER_FINGERPRINTING
+                                                    ? BraveShieldsContentSettings.DEFAULT
+                                                    : BraveShieldsContentSettings
+                                                              .BLOCK_THIRDPARTY_RESOURCE,
                                             false);
                                 } else if (checkedId == R.id.option3) {
                                     BraveShieldsContentSettings.setShieldsValue(mProfile, mHost,
