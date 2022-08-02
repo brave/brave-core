ad9fa968d5841bfac71c260b23334b2180be9cb4
Brian Clifton <brian@clifton.me>
2022-08-01 23:06:13 -0700

    Remove `window.phantom` alias
    Reverts https://github.com/brave/brave-core/pull/14282
    
    Fixes https://github.com/brave/brave-browser/issues/24289
    Unfixes https://github.com/brave/brave-browser/issues/24192

--- components/brave_wallet/resources/solana_provider.js
+++ components/brave_wallet/resources/solana_provider.js
@@ -4,12 +4,34 @@
 // you can obtain one at http://mozilla.org/MPL/2.0/.
 
 (function () {
-  if (!window.braveSolana) {
+  if (!window.braveSolana || !window.solana) {
     return
   }
   const EventEmitter = require('events')
   var SolanaEventEmitter = new EventEmitter()
   Object.defineProperties(window.braveSolana, {
+    on: {
+      value: SolanaEventEmitter.on,
+      writable: false
+    },
+    off: {
+      value: SolanaEventEmitter.off,
+      writable: false
+    },
+    emit: {
+      value: SolanaEventEmitter.emit,
+      writable: false
+    },
+    removeListener: {
+      value: SolanaEventEmitter.removeListener,
+      writable: false
+    },
+    removeAllListeners: {
+      value: SolanaEventEmitter.removeAllListeners,
+      writable: false
+    }
+  })
+  Object.defineProperties(window.solana, {
     on: {
       value: SolanaEventEmitter.on,
       writable: false
