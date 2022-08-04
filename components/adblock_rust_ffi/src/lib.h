458969cfab950ef6cd4dbfd3ce44801d617d8082
Anton Lazarev <antonok35@gmail.com>
2022-08-04 11:56:07 -0700

    add content blocking conversion method

--- components/adblock_rust_ffi/src/lib.h
+++ components/adblock_rust_ffi/src/lib.h
@@ -184,4 +184,8 @@ char* engine_hidden_class_id_selectors(struct C_Engine* engine,
                                        const char* const* exceptions,
                                        size_t exceptions_size);
 
+#if defined(IS_IOS)
+char* convert_rules_to_content_blocking(const char* rules);
+#endif
+
 #endif /* BRAVE_COMPONENTS_ADBLOCK_RUST_FFI_SRC_LIB_H_ */
