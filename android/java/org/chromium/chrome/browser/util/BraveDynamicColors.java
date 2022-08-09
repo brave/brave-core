ddf4cb106c7f1cffd20a07ef5e1fe130271e5bfa
samartnik <artem@brave.com>
2022-08-09 14:01:43 -0400

    [Android] DynamicColors features removed
    Chromium change:
    chromium/chromium@74c6794
    
    Remove launched DynamicColors features.
    Bug: 1345778

---
+++ android/java/org/chromium/chrome/browser/util/BraveDynamicColors.java
@@ -0,0 +1,21 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.chromium.chrome.browser.util;
+
+import android.app.Activity;
+
+import com.google.android.material.color.DynamicColorsOptions;
+
+public class BraveDynamicColors {
+    public static void applyToActivityIfAvailable(Activity activity) {
+        // We disable dynamic colors as it causes styling issues with Brave theme.
+    }
+
+    public static void applyToActivityIfAvailable(
+            Activity activity, DynamicColorsOptions dynamicColorsOptions) {
+        // We disable dynamic colors as it causes styling issues with Brave theme.
+    }
+}
