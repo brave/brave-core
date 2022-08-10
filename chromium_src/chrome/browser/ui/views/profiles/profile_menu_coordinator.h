ef433162523b5936fbd9183d0ef6cd7c3a2ba93c
Claudio DeSouza <cdesouza@igalia.com>
2022-08-10 15:22:22 +0100

    ProfileMenuViewBase methods moved to a coordinator
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/1e5a2425d2dcd372151cb550f1547a56fa26aaf5
    
    commit 1e5a2425d2dcd372151cb550f1547a56fa26aaf5
    Author: Thomas Lukaszewicz <tluk@chromium.org>
    Date:   Mon Jul 25 17:41:21 2022 +0000
    
        Update ProfileMenuViewBase to use the Coordinator pattern
    
        This CL eliminates the use of a globally tracked dialog bubble view
        from the profile menu view base - and updates the code to leverage
        the views Coordinator pattern (see below).
    
        https://docs.google.com/document/d/1_S2XfH870E4jwNH3nkKXJXuu9rG2Cu1sU38d5V_3bqs/edit?usp=sharing
    
        The coordinator has been implemented as browser user data and who's
        lifetime is 1-1 with the associated browser. The existing
        browsertests have also been updated to match the changes.
    
        Bug: 1342246

---
+++ chromium_src/chrome/browser/ui/views/profiles/profile_menu_coordinator.h
@@ -0,0 +1,17 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_PROFILES_PROFILE_MENU_COORDINATOR_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_PROFILES_PROFILE_MENU_COORDINATOR_H_
+
+#define GetProfileMenuViewBaseForTesting() \
+  GetProfileMenuViewBaseForTesting();      \
+  friend class BraveProfileMenuViewTest
+
+#include "src/chrome/browser/ui/views/profiles/profile_menu_coordinator.h"
+
+#undef GetProfileMenuViewBaseForTesting
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_PROFILES_PROFILE_MENU_COORDINATOR_H_
