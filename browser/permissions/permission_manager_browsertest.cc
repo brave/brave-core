7941f3b90ed75635d00bc8300bb83c26637cf287
Claudio DeSouza <cdesouza@igalia.com>
2022-08-10 15:19:36 +0100

    Add TabCloseTypes enum
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/df79d1e8d39025472d5305abd9a235b7e52da1ca
    
    commit df79d1e8d39025472d5305abd9a235b7e52da1ca
    Author: David Pennington <dpenning@chromium.org>
    Date:   Wed Jul 27 16:11:28 2022 +0000
    
        Move CloseTypes into enums
    
        Change-Id: I122f84df457404b7a80ff7de36d67c99d741178c

--- browser/permissions/permission_manager_browsertest.cc
+++ browser/permissions/permission_manager_browsertest.cc
@@ -17,6 +17,7 @@
 #include "chrome/browser/permissions/permission_manager_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/tabs/tab_enums.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
@@ -323,7 +324,7 @@ IN_PROC_BROWSER_TEST_F(PermissionManagerBrowserTest,
     // Close tab with active request pending.
     content::WebContentsDestroyedWatcher tab_destroyed_watcher(web_contents());
     browser()->tab_strip_model()->CloseWebContentsAt(0,
-                                                     TabStripModel::CLOSE_NONE);
+                                                     TabCloseTypes::CLOSE_NONE);
     tab_destroyed_watcher.Wait();
     EXPECT_TRUE(IsPendingGroupedRequestsEmpty(cases[i].type));
   }
