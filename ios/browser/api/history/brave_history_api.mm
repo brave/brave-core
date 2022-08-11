0d20dcc99429b30bc4686fa48f8cbbcfd05dbf85
Claudio DeSouza <cdesouza@igalia.com>
2022-08-11 15:56:09 +0100

    Changes to HistoryAddPageArgs constructor
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/a59e38d5a2f90376cba5932ace524f3d44792cab
    https://chromium.googlesource.com/chromium/src/+/054b433e420ef82acab7371cb5282df59417215e
    https://chromium.googlesource.com/chromium/src/+/18e77c1ead5c3ce63f8bed29ad29973d2dfcdb11
    
    commit a59e38d5a2f90376cba5932ace524f3d44792cab
    Author: Brandon Wylie <wylieb@chromium.org>
    Date:   Thu Jun 23 21:21:06 2022 +0000
    
        [bookmarks] Add BookmarkId as an optional navigation parameter
    
        Bookmark navigations will now set the bookmark_id parameter to track
        direct bookmark opens in the chrome UI.
        * Adds parameter to ChromeUINavigationData.
        * Observes incoming BookmarkIds in HistoryTabHelper.
        * Plumb BookmarkId through to the HistoryBackend/Client.
        * Scaffolding for BookmarkModel method to track opens.
    
        Bug: 1320950
    
    commit 054b433e420ef82acab7371cb5282df59417215e
    Author: Marc Treib <treib@chromium.org>
    Date:   Thu Jul 28 10:04:33 2022 +0000
    
        HistoryService::AddPage: Remove floc_allowed param
    
        ...as a parameter from AddPage() itself as well as from
        HistoryAddPageArgs.
        It wasn't used anymore - always false in practice (with the exception
        of a single unit test), but anyway the value in HistoryAddPageArgs was never used.
    
        Bug: none
    
    commit 18e77c1ead5c3ce63f8bed29ad29973d2dfcdb11
    Author: Sophie Chang <sophiechang@chromium.org>
    Date:   Wed Sep 1 01:16:10 2021 +0000
    
        Reland "Persist the visit ID that opened the visit to History"
    
        This is a reland of 857f5bc42c1768c2f2d9d5c8ec141c7e106ebf90
    
        Diff: Set floc_allowed in HistoryAddPageArgs constructor in history_types.cc
    
        Original change's description:
        > Persist the visit ID that opened the visit to History
        >
        > The opener visit ID provides the last missing link that the referring
        > visit ID doesn't provide as the referring visit is only calculated for
        > visits that were shown in the same WebContents.
        >
        > This requires a DB migration as it adds an additional field to the
        > Visits table (and cleans up publicly_routable field per comments in
        > visit_database.cc)
        >
        > Bug: 1241633
        > Change-Id: If5f71cac7c865e2dab6482f710955b98a19a7171
        > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3115313
        > Reviewed-by: Scott Violet <sky@chromium.org>
        > Reviewed-by: Yao Xiao <yaoxia@chromium.org>
        > Reviewed-by: Michael Crouse <mcrouse@chromium.org>
        > Reviewed-by: Tommy Li <tommycli@chromium.org>
        > Commit-Queue: Sophie Chang <sophiechang@chromium.org>
        > Cr-Commit-Position: refs/heads/main@{#916638}
    
        Bug: 1241633

--- ios/browser/api/history/brave_history_api.mm
+++ ios/browser/api/history/brave_history_api.mm
@@ -130,7 +130,6 @@
   DCHECK(history_service_->backend_loaded());
 
   // Important! Only Typed URL is being synced in core side
-  // Disable Floc - Not allow tracking!! 
   history::HistoryAddPageArgs args = history::HistoryAddPageArgs(
       /*url*/ net::GURLWithNSURL(history.url),
       /*time*/ base::Time::FromNSDate(history.dateAdded),
@@ -141,8 +140,9 @@
       isURLTyped ? ui::PAGE_TRANSITION_TYPED : ui::PAGE_TRANSITION_LINK,
       /*hidden=*/false, /*visit_source*/ history::VisitSource::SOURCE_BROWSED,
       /*did_replace_entry=*/false, /*consider_for_ntp_most_visited=*/true,
-      /*floc_allowed=*/false,
-      /*title*/ base::SysNSStringToUTF16(history.title));
+      /*title*/ base::SysNSStringToUTF16(history.title),
+      /*opener*/ absl::nullopt,
+      /*bookmark_id*/ absl::nullopt);
 
   history_service_->AddPage(args);
 }
