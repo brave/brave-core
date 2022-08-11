624b8164ac4ca0d5a2d39bd0b7876f3b231ef7ad
Jay Harris <jay.harris@outlook.co.nz>
2022-08-11 12:13:06 +1200

    Finish converting header

--- components/brave_webtorrent/extension/components/torrentViewerHeader.tsx
+++ components/brave_webtorrent/extension/components/torrentViewerHeader.tsx
@@ -38,56 +38,52 @@ export default function TorrentHeaderViewer ({ torrent, torrentId, name, tabId, 
         console.log('Copy failed')
       }
     } else {
-      const removeDownloadListener = () => chrome.downloads.onDeterminingFilename.removeListener(downloadListener)
-      const onTorrentDownloadError = () => {
-        removeDownloadListener()
-        const msg = 'Error: file is not a .torrent.'
-        // By default, alert() shows up on every tab that has loaded webtorrent.
-        // We only want it to show on the current tab.
-        chrome.tabs.query({
-          active: true,
-          lastFocusedWindow: true
-        }, (tabs) => {
-          const tab = tabs[0]
-          if (tab && typeof tab.id === 'number') {
-            chrome.tabs.executeScript(tab.id, {
-              code: `alert('${msg}')`
-            }, () => {
-              if (chrome.runtime.lastError) {
-                console.error('tabs.executeScript failed: ' + chrome.runtime.lastError.message)
-              }
-            })
-            return
-          }
-          alert(msg)
-        })
-      }
       const downloadListener = (item: chrome.downloads.DownloadItem) => {
-        if (!item || !item.url || item.url !== this.props.torrentId) {
+        chrome.downloads.onDeterminingFilename.removeListener(downloadListener)
+
+        if (!item || !item.url || item.url !== torrentId) {
           // Only listen for downloads initiated by Webtorrent.
-          removeDownloadListener()
           return
         }
         const url = new URL(item.finalUrl || item.url)
         if (!url || !remoteProtocols.includes(url.protocol) ||
             url.hostname === '127.0.0.1') {
           // Non-remote files are trusted.
-          removeDownloadListener()
           return
         }
         if (!item.filename || !item.filename.endsWith('.torrent')) {
           try {
-            chrome.downloads.cancel(item.id, onTorrentDownloadError)
+            chrome.downloads.cancel(item.id, () => {
+              const msg = 'Error: file is not a .torrent.'
+              // By default, alert() shows up on every tab that has loaded webtorrent.
+              // We only want it to show on the current tab.
+              chrome.tabs.query({
+                active: true,
+                lastFocusedWindow: true
+              }, (tabs) => {
+                const tab = tabs[0]
+                if (tab && typeof tab.id === 'number') {
+                  chrome.tabs.executeScript(tab.id, {
+                    code: `alert('${msg}')`
+                  }, () => {
+                    if (chrome.runtime.lastError) {
+                      console.error('tabs.executeScript failed: ' + chrome.runtime.lastError.message)
+                    }
+                  })
+                  return
+                }
+                alert(msg)
+              })
+            })
           } catch (e) {
-            removeDownloadListener()
             console.error(e)
           }
         }
       }
 
-      // Listen for malicious files pretending to be .torrents (#11488)
+      // Listen for malicious files pretending to be .torrents
       chrome.downloads.onDeterminingFilename.addListener(downloadListener)
-      let a = document.createElement('a')
+      const a = document.createElement('a')
       a.download = ''
       a.href = torrentId
       a.click()
