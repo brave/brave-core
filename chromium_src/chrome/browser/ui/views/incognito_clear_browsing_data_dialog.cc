d19c7832944f52ece864a8fae3f877b813f70a8c
AlexNguyen1612 <alexnguyen1612@gmail.com>
2022-08-13 12:20:21 +0930

    Refactor code to a simpler solution
    Although the original solution worked, it added some complexity only to use SetShowCloseButton(false). This is a much effecient way to do it
    
    Fix https://github.com/brave/brave-browser/issues/21771

--- chromium_src/chrome/browser/ui/views/incognito_clear_browsing_data_dialog.cc
+++ chromium_src/chrome/browser/ui/views/incognito_clear_browsing_data_dialog.cc
@@ -3,23 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "ui/views/bubble/bubble_dialog_delegate_view.h"
+
+#define SetShowCloseButton(SHOW) SetShowCloseButton(false)
+
 #include "src/chrome/browser/ui/views/incognito_clear_browsing_data_dialog.cc"
-
-BraveIncognitoClearBrowsingDataDialog::BraveIncognitoClearBrowsingDataDialog(
-    views::View* anchor_view,
-    Profile* incognito_profile,
-    Type type)
-    : IncognitoClearBrowsingDataDialog(anchor_view, incognito_profile, type) {
-  SetShowCloseButton(false);
-}
-
-// static
-void BraveIncognitoClearBrowsingDataDialog::Show(views::View* anchor_view,
-                                                 Profile* incognito_profile,
-                                                 Type type) {
-  g_incognito_cbd_dialog = new BraveIncognitoClearBrowsingDataDialog(
-      anchor_view, incognito_profile, type);
-  views::Widget* const widget =
-      BubbleDialogDelegateView::CreateBubble(g_incognito_cbd_dialog);
-  widget->Show();
-}
