86dc115a5504d2842462c76bfd35c40c4120dce3
Jay Harris <7678024+fallaciousreasoning@users.noreply.github.com>
2022-08-17 09:27:15 +1200

    Fix for stats display (#14519)

--- components/brave_new_tab_ui/api/stats.ts
+++ components/brave_new_tab_ui/api/stats.ts
@@ -20,12 +20,36 @@ export type Stats = {
   bandwidthSavedStat: number
 }
 
+// This is a 'good enough' solution to a problem which occurred in the
+// base::Value migration. These are uint64s in C++ land, and previously these
+// were being serialized as JS numbers (which have a max value of 2^53 - 1). The
+// serialization now converts them (correctly) to strings. Unfortunately, this
+// breaks our localization logic, which just `.toLocaleString()`s the numbers.
+// These are used as part of estimates anyway, so even if we do lose some
+// precision for bigger values, it's okay.
+const convertToNumber: Array<keyof Stats> = [
+  'adsBlockedStat',
+  'javascriptBlockedStat',
+  'fingerprintingBlockedStat',
+  'httpsUpgradesStat',
+  'bandwidthSavedStat'
+]
+
 type StatsUpdatedHandler = (statsData: Stats) => void
 
-export function getStats (): Promise<Stats> {
-  return Cr.sendWithPromise('getNewTabPageStats')
+const rawToStats = (data: any): Stats => {
+  for (const key of convertToNumber) {
+    if (!data[key]) continue
+    data[key] = Number(data[key])
+  }
+  return data
+}
+
+export async function getStats (): Promise<Stats> {
+  const result = await Cr.sendWithPromise('getNewTabPageStats')
+  return rawToStats(result)
 }
 
 export function addChangeListener (listener: StatsUpdatedHandler): void {
-  Cr.addWebUIListener('stats-updated', listener)
+  Cr.addWebUIListener('stats-updated', (raw: any) => listener(rawToStats(raw)))
 }
