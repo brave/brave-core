70e86b5ffcc1f289ed8dead19ebb1712cc1af364
Pavel Beloborodov <73575789+boocmp@users.noreply.github.com>
2022-08-17 18:03:01 +0700

    Do not share SpeedreaderJsHandler betweeen gin handles. (#14642)

--- components/speedreader/renderer/speedreader_render_frame_observer.cc
+++ components/speedreader/renderer/speedreader_render_frame_observer.cc
@@ -5,7 +5,6 @@
 
 #include "brave/components/speedreader/renderer/speedreader_render_frame_observer.h"
 
-#include "brave/components/speedreader/common/constants.h"
 #include "brave/components/speedreader/common/url_readable_hints.h"
 #include "brave/components/speedreader/renderer/speedreader_js_handler.h"
 #include "content/public/renderer/render_frame.h"
@@ -24,21 +23,10 @@ void SpeedreaderRenderFrameObserver::DidStartNavigation(
   is_speedreadable_url_ = IsURLLooksReadable(url);
 }
 
-void SpeedreaderRenderFrameObserver::DidCreateScriptContext(
-    v8::Local<v8::Context> context,
-    int32_t world_id) {
-  if (!is_speedreadable_url_ || !render_frame()->IsMainFrame() ||
-      world_id != kIsolatedWorldId) {
+void SpeedreaderRenderFrameObserver::DidClearWindowObject() {
+  if (!is_speedreadable_url_ || !render_frame()->IsMainFrame())
     return;
-  }
-
-  if (!native_javascript_handle_) {
-    native_javascript_handle_.reset(new SpeedreaderJSHandler(render_frame()));
-  } else {
-    native_javascript_handle_->ResetRemote(render_frame());
-  }
-
-  native_javascript_handle_->AddJavaScriptObjectToFrame(context);
+  SpeedreaderJSHandler::Install(render_frame());
 }
 
 void SpeedreaderRenderFrameObserver::OnDestruct() {
