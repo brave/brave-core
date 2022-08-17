bcfcde448dd636b340b28405c5ef47f920529c80
samartnik <artem@brave.com>
2022-08-17 14:34:57 -0400

    kIsolatePrerenders was moved
    Chromium change:
    https://github.com/chromium/chromium/commit/4ae1ab38595ebe2d7fa5dbd3e698af316413d866
    
    Move chrome/prefetch/prefetch_proxy to //preloading/prefetch
    With our Precog effort, we want to bring together all Preloading code
    together at one place to bring more structuring and reusing the common
    logic between various preloading types and predictors to decrease the
    code complexity.
    
    In this CL, we move all files in the
    chrome/browser/prefetch/prefetch_proxy directory to
    chrome/browser/preloading/prefetch_proxy.
    
    This is a no-op move.
    
    For more details on directory structuring please see:
    https://docs.google.com/document/d/13SnVgi16aD6X0rb-b4_F5h-7Reb9iGY_cKfzHZmXGDg/edit?usp=sharing
    
    BUG=1337129

--- chromium_src/chrome/browser/preloading/prefetch/prefetch_proxy/prefetch_proxy_features.cc
+++ chromium_src/chrome/browser/preloading/prefetch/prefetch_proxy/prefetch_proxy_features.cc
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "src/chrome/browser/prefetch/prefetch_proxy/prefetch_proxy_features.cc"
+#include "src/chrome/browser/preloading/prefetch/prefetch_proxy/prefetch_proxy_features.cc"
 
 #include "base/feature_override.h"
 
