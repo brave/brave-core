8f50a31fdf18f7e3810a3a46e882395d46ba776e
Aleksey Khoroshilov <akhoroshilov@brave.com>
2022-08-19 15:22:53 +0700

    Add v8 script tracking.

---
+++ chromium_src/v8/src/codegen/compiler.cc
@@ -0,0 +1,24 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/components/brave_page_graph/common/buildflags.h"
+
+#define BRAVE_COMPILER_GET_FUNCTION_FROM_EVAL                   \
+  IF_BUILDFLAG(ENABLE_BRAVE_PAGE_GRAPH, {                       \
+    auto* page_graph_delegate = isolate->page_graph_delegate(); \
+    if (V8_UNLIKELY(page_graph_delegate)) {                     \
+      Object maybe_script = result->shared().script();          \
+      if (maybe_script.IsScript()) {                            \
+        const int script_id = Script::cast(maybe_script).id();  \
+        page_graph_delegate->OnEvalScriptCompiled(              \
+            reinterpret_cast<v8::Isolate*>(isolate), script_id, \
+            v8::Utils::ToLocal(source));                        \
+      }                                                         \
+    }                                                           \
+  })
+
+#include "src/v8/src/codegen/compiler.cc"
+
+#undef BRAVE_COMPILER_GET_FUNCTION_FROM_EVAL
---
+++ chromium_src/v8/src/execution/isolate.cc
@@ -0,0 +1,70 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "src/v8/src/execution/isolate.cc"
+
+namespace v8 {
+namespace internal {
+
+#if BUILDFLAG(ENABLE_BRAVE_PAGE_GRAPH)
+namespace {
+
+std::vector<v8::page_graph::ExecutingScript>
+GetExecutingScriptsImpl(Isolate* isolate, bool all, bool include_position) {
+  std::vector<v8::page_graph::ExecutingScript> result;
+  JavaScriptFrameIterator it(isolate);
+  while (!it.done()) {
+    JavaScriptFrame* frame = it.frame();
+    std::vector<SharedFunctionInfo> functions;
+    frame->GetFunctions(&functions);
+    for (const auto& shared : functions) {
+      Object maybe_script = shared.script();
+      if (!maybe_script.IsScript()) {
+        continue;
+      }
+
+      const int script_id = Script::cast(maybe_script).id();
+      if (script_id <= 0) {
+        continue;
+      }
+
+      int script_position = 0;
+      if (include_position && !isolate->has_pending_exception()) {
+        Handle<SharedFunctionInfo> shared_handle(shared, isolate);
+        SharedFunctionInfo::EnsureSourcePositionsAvailable(isolate,
+                                                           shared_handle);
+        script_position = frame->position();
+      }
+
+      result.emplace_back(
+          v8::page_graph::ExecutingScript{script_id, script_position});
+      if (!all) {
+        return result;
+      }
+    }
+    it.Advance();
+  }
+  return result;
+}
+
+}  // namespace
+
+v8::page_graph::ExecutingScript Isolate::GetExecutingScript(
+    bool include_position) {
+  auto executing_scripts =
+      GetExecutingScriptsImpl(this, false, include_position);
+  if (!executing_scripts.empty()) {
+    return std::move(executing_scripts[0]);
+  }
+  return v8::page_graph::ExecutingScript();
+}
+
+std::vector<v8::page_graph::ExecutingScript> Isolate::GetAllExecutingScripts() {
+  return GetExecutingScriptsImpl(this, true, true);
+}
+#endif  // BUILDFLAG(ENABLE_BRAVE_PAGE_GRAPH)
+
+}  // namespace internal
+}  // namespace v8
