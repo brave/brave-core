e440166fe663752e76abfd59e09262dae80308ed
Jay Harris <jay.harris@outlook.co.nz>
2022-08-25 12:52:59 +1200

    Add en_US

--- components/brave_webtorrent/extension/background/api/locale_api.ts
+++ components/brave_webtorrent/extension/background/api/locale_api.ts
@@ -1,12 +1,13 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
+import type Messages from '../../_locales/en_GB/messages.json'
 
 /**
  * Gets the locale message specified in messages.json
  * @param {string} message - The locale string
  */
- export const getMessage = (message: string, substitutions?: string[]): string => {
+ export const getMessage = (message: keyof typeof Messages, substitutions?: string[]): string => {
     if (chrome.i18n) {
       let translated = chrome.i18n.getMessage(message, substitutions)
 
