59cd3cc3f0bf06a0984e493650908fcb4a47e0a1
Deep <deeppandya91@gmail.com>
2022-08-29 14:11:01 -0400

    Remove activity alias and add new activity

---
+++ android/java/org/chromium/chrome/browser/searchwidget/SearchWidgetProviderActivity.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.chromium.chrome.browser.searchwidget;
+
+import android.content.Intent;
+
+import org.chromium.chrome.browser.searchwidget.SearchActivity;
+
+public class SearchWidgetProviderActivity extends SearchActivity {
+    @Override
+    public void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+    }
+}
