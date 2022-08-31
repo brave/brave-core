ae459f69f49abb6efb8f333808550194146d0b5e
brave-builds <devops@brave.com>
2022-08-31 23:31:52 +0000

    Uplift of #14930 (squashed) to release

--- chromium_src/chrome/browser/flags/android/chrome_feature_list.cc
+++ chromium_src/chrome/browser/flags/android/chrome_feature_list.cc
@@ -3,6 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "base/feature_override.h"
 #include "brave/components/brave_rewards/common/features.h"
 #include "brave/components/brave_search_conversion/features.h"
 #include "brave/components/brave_today/common/features.h"
@@ -20,3 +21,13 @@
 
 #include "src/chrome/browser/flags/android/chrome_feature_list.cc"
 #undef kForceWebContentsDarkMode
+
+namespace chrome {
+namespace android {
+
+OVERRIDE_FEATURE_DEFAULT_STATES({{
+    {kAddToHomescreenIPH, base::FEATURE_DISABLED_BY_DEFAULT},
+}});
+
+}  // namespace android
+}  // namespace chrome
