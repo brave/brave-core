f6315b655cb002ca7b923de2d7bac82d083aae77
Pavel Beloborodov <73575789+boocmp@users.noreply.github.com>
2022-08-31 21:15:14 +0700

    Added command line flag to override speedreader style sheet. (#14880)

--- components/speedreader/speedreader_component.cc
+++ components/speedreader/speedreader_component.cc
@@ -7,8 +7,12 @@
 
 #include <utility>
 
-#include "base/bind.h"
-#include "base/logging.h"
+#include "base/command_line.h"
+#include "base/files/file_path_watcher.h"
+#include "base/task/bind_post_task.h"
+#include "base/task/task_runner_util.h"
+#include "base/task/thread_pool.h"
+#include "base/threading/sequenced_task_runner_handle.h"
 
 namespace speedreader {
 
@@ -18,6 +22,8 @@ constexpr base::FilePath::CharType kDatFileVersion[] = FILE_PATH_LITERAL("1");
 
 constexpr base::FilePath::CharType kStylesheetFileName[] =
     FILE_PATH_LITERAL("content-stylesheet.css");
+
+constexpr const char kSpeedreaderStylesheet[] = "speedreader-stylesheet";
 
 constexpr char kComponentName[] = "Brave SpeedReader Updater";
 constexpr char kComponentId[] = "jicbkmdloagakknpihibphagfckhjdih";
@@ -29,6 +35,18 @@ constexpr char kComponentPublicKey[] =
     "2Ts/TDEddNv8eV7XbwQdL1HUEoFj+RXDq1CuQJjvQdc7YRmy0WGV0GIXu0lAFOQ6D/Z/rjtOe"
     "//2uc4zIkviMcUlrvHaJwIDAQAB";
 
+base::FilePathWatcher* CreateAndStartFilePathWatcher(
+    const base::FilePath& watch_path,
+    const base::FilePathWatcher::Callback& callback) {
+  auto watcher = std::make_unique<base::FilePathWatcher>();
+  if (!watcher->Watch(watch_path, base::FilePathWatcher::Type::kNonRecursive,
+                      callback)) {
+    return nullptr;
+  }
+
+  return watcher.release();
+}
+
 }  // namespace
 
 SpeedreaderComponent::SpeedreaderComponent(Delegate* delegate)
@@ -37,7 +55,11 @@ SpeedreaderComponent::SpeedreaderComponent(Delegate* delegate)
   Register(kComponentName, kComponentId, kComponentPublicKey);
 }
 
-SpeedreaderComponent::~SpeedreaderComponent() = default;
+SpeedreaderComponent::~SpeedreaderComponent() {
+  if (file_watcher_) {
+    watch_task_runner_->DeleteSoon(FROM_HERE, file_watcher_);
+  }
+}
 
 void SpeedreaderComponent::AddObserver(Observer* observer) {
   observers_.AddObserver(observer);
@@ -50,12 +72,45 @@ void SpeedreaderComponent::RemoveObserver(Observer* observer) {
 void SpeedreaderComponent::OnComponentReady(const std::string& component_id,
                                             const base::FilePath& install_dir,
                                             const std::string& manifest) {
-  stylesheet_path_ =
-      install_dir.Append(kDatFileVersion).Append(kStylesheetFileName);
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          kSpeedreaderStylesheet)) {
+    stylesheet_path_ =
+        base::CommandLine::ForCurrentProcess()->GetSwitchValuePath(
+            kSpeedreaderStylesheet);
+    watch_task_runner_ =
+        base::ThreadPool::CreateSequencedTaskRunner({base::MayBlock()});
+
+    base::PostTaskAndReplyWithResult(
+        watch_task_runner_.get(), FROM_HERE,
+        base::BindOnce(
+            &CreateAndStartFilePathWatcher, stylesheet_path_,
+            base::BindPostTask(
+                base::SequencedTaskRunnerHandle::Get(),
+                base::BindRepeating(&SpeedreaderComponent::OnFileChanged,
+                                    weak_factory_.GetWeakPtr()))),
+        base::BindOnce(&SpeedreaderComponent::OnWatcherStarted,
+                       weak_factory_.GetWeakPtr()));
+  } else {
+    stylesheet_path_ =
+        install_dir.Append(kDatFileVersion).Append(kStylesheetFileName);
+  }
 
   for (Observer& observer : observers_) {
     observer.OnStylesheetReady(stylesheet_path_);
   }
+}
+
+void SpeedreaderComponent::OnFileChanged(const base::FilePath& path,
+                                         bool error) {
+  DCHECK_EQ(path, stylesheet_path_);
+  for (Observer& observer : observers_) {
+    observer.OnStylesheetReady(stylesheet_path_);
+  }
+}
+
+void SpeedreaderComponent::OnWatcherStarted(
+    base::FilePathWatcher* file_watcher) {
+  file_watcher_ = file_watcher;
 }
 
 }  // namespace speedreader
