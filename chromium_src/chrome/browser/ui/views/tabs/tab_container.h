cf5a374ee28bc8ef52941d5d8d128423ca9151c8
sangwoo.ko <sangwoo108@gmail.com>
2022-09-02 10:14:06 +0900

    Cleanup

--- chromium_src/chrome/browser/ui/views/tabs/tab_container.h
+++ chromium_src/chrome/browser/ui/views/tabs/tab_container.h
@@ -1,7 +1,7 @@
-t /* Copyright (c) 2022 The Brave Authors. All rights reserved.
-   * This Source Code Form is subject to the terms of the Mozilla Public
-   * License, v. 2.0. If a copy of the MPL was not distributed with this file,
-   * You can obtain one at http://mozilla.org/MPL/2.0/. */
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_TABS_TAB_CONTAINER_H_
 #define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_TABS_TAB_CONTAINER_H_
--- chromium_src/chrome/browser/ui/views/tabs/tab_strip.cc
+++ chromium_src/chrome/browser/ui/views/tabs/tab_strip.cc
@@ -19,7 +19,7 @@
   AddTab(std::make_unique<BraveTab>(this), MODEL_INDEX, PINNED)
 #define TabHoverCardController BraveTabHoverCardController
 #define TabContainer BraveTabContainer
-#define TAB_DRAG_CONTEXT_CALCULATE_INSERTION_INDEX_FOR_VERTICAL_TABS           \
+#define BRAVE_CALCULATE_INSERTION_INDEX                                        \
   if (tabs::features::ShouldShowVerticalTabs()) {                              \
     const int ideal_y =                                                        \
         candidate_index == 0 ? 0 : ideal_bounds(candidate_index - 1).bottom(); \
@@ -31,22 +31,22 @@
     continue;                                                                  \
   }
 
-#define TAB_DRAG_CONTEXT_CALCULATE_BOUNDS_FOR_DRAGGED_VIEWS_VERTICAL_TABS \
-  if (tabs::features::ShouldShowVerticalTabs()) {                         \
-    std::vector<gfx::Rect> bounds;                                        \
-    int y = 0;                                                            \
-    for (const TabSlotView* view : views) {                               \
-      const int height = view->height();                                  \
-      bounds.push_back(gfx::Rect(0, y, view->width(), view->height()));   \
-      y += height;                                                        \
-    }                                                                     \
-    return bounds;                                                        \
+#define BRAVE_CALCULATE_BOUNDS_FOR_DRAGGED_VIEWS        \
+  if (tabs::features::ShouldShowVerticalTabs()) {       \
+    std::vector<gfx::Rect> bounds;                      \
+    int y = 0;                                          \
+    for (const TabSlotView* view : views) {             \
+      const int height = view->height();                \
+      bounds.emplace_back(0, y, view->width(), height); \
+      y += height;                                      \
+    }                                                   \
+    return bounds;                                      \
   }
 
 #include "src/chrome/browser/ui/views/tabs/tab_strip.cc"
 
-#undef BRAVE_TAB_STRIP_CALCULATE_DRAGGED_VIEW_BOUNDS_FOR_VERTICAL_TABS
-#undef BRAVE_TAB_STRIP_CALCULATE_INDEX_FOR_VERTICAL_TABS
+#undef BRAVE_CALCULATE_BOUNDS_FOR_DRAGGED_VIEWS
+#undef BRAVE_CALCULATE_INSERTION_INDEX
 #undef TabContainer
 #undef TabHoverCardController
 #undef AddTab
