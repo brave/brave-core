4fa57f2a4a4974bc0360203154770fc0b7d89536
Claudio DeSouza <cdesouza@igalia.com>
2022-09-02 12:57:46 +0100

    Changes to WebContentsDelegate::AddNewContents
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/767c27a182cf3813733b7111d28c71ff0ea7130d
    
    commit 767c27a182cf3813733b7111d28c71ff0ea7130d
    Author: Brad Triebwasser <btriebw@chromium.org>
    Date:   Thu Aug 25 22:56:05 2022 +0000
    
        Change gfx::Rect to blink::mojom::WindowFeatures in AddNewContents and some related functions.
    
        The intention for this change is to allow the following
        when creating popup windows:
        1) Differentiate between 0 values and unset/undefined values
           in the window bounds(x,y,width,height).
        2) Pass additional properties that affect the behavior of the
           resulting popup window.
    
        This is simply a refactor with no behavior change. It changes some function signatures accepting gfx::Rect window bounds to blink::mojom::WindowFeatures which includes additional data along with the bounds. The following functions are changed including all overrides and call sites:
        content::WebContentsDelegate::AddNewContents
        chrome::AddWebContents
        content::RenderFrameHostDelegate::ShowCreatedWindow
        extensions::AppDelegate::AddNewContents
        blink::ChromeClient::Show
        ui::WebContentsHandler::AddNewContents
    
        I also refactored blink::mojom::WindowFeatures  to substitute the x/y/width/height with a nested gfx::mojom::Rect to make things easier and be better compatible with the gfx::Rect struct.
    
        Bug: 1142516

--- chromium_src/chrome/browser/ui/views/bubble/bubble_contents_wrapper.h
+++ chromium_src/chrome/browser/ui/views/bubble/bubble_contents_wrapper.h
@@ -15,16 +15,16 @@
 #include "ui/base/window_open_disposition.h"
 #include "url/gurl.h"
 
-#define RenderViewHostChanged                                             \
-  SetWebContentsAddNewContentsDelegate(                                   \
-      base::WeakPtr<content::WebContentsDelegate> browser_delegate);      \
-  void AddNewContents(content::WebContents* source,                       \
-                      std::unique_ptr<content::WebContents> new_contents, \
-                      const GURL& target_url,                             \
-                      WindowOpenDisposition disposition,                  \
-                      const gfx::Rect& initial_rect, bool user_gesture,   \
-                      bool* was_blocked) override;                        \
-  std::vector<int32_t>& GetPopupIds() { return popup_ids_; }              \
+#define RenderViewHostChanged                                              \
+  SetWebContentsAddNewContentsDelegate(                                    \
+      base::WeakPtr<content::WebContentsDelegate> browser_delegate);       \
+  void AddNewContents(content::WebContents* source,                        \
+                      std::unique_ptr<content::WebContents> new_contents,  \
+                      const GURL& target_url,                              \
+                      WindowOpenDisposition disposition,                   \
+                      const blink::mojom::WindowFeatures& window_features, \
+                      bool user_gesture, bool* was_blocked) override;      \
+  std::vector<int32_t>& GetPopupIds() { return popup_ids_; }               \
   void RenderViewHostChanged
 
 #define webui_resizes_host_        \
