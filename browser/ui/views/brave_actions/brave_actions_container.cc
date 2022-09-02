17126911207be6a58ea18292a735b381a86a2f15
Claudio DeSouza <cdesouza@igalia.com>
2022-09-02 17:02:11 +0100

    BraveActionsContainer requires UpdateToolbarActionHoverCard
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/c313724030f1fa5dcf1e2ca1ce127b00e1497aa2
    
    commit c313724030f1fa5dcf1e2ca1ce127b00e1497aa2
    Author: Emilia Paz <emiliapaz@chromium.org>
    Date:   Fri Aug 19 20:44:32 2022 +0000
    
        [Extensions c2s] Display hover card for toolbar action views
    
        Displays a hover card when mouse hovers a toolbar action view. If the
        mouse moves between toolbar actions, the hover card transitions between
        them. Hides the hover card when the mouse is moved outside of the toolbar
        action views and on any other event.
    
        Toolbar action hover card controller is based on tab hover card
        controller. For now we are copying functionality, with a future goal of
        having a base hover card.
    
        Toolbar action hover cards are under a feature flag.
    
        Screencast: https://drive.google.com/file/d/1oe_g0V0BDSyT7VP5i8Zkuf2GEoMEZNQt/view?usp=sharing
    
        Bug: 1351778

--- browser/ui/views/brave_actions/brave_actions_container.cc
+++ browser/ui/views/brave_actions/brave_actions_container.cc
@@ -534,3 +534,7 @@ void BraveActionsContainer::ToggleExtensionsMenu() {}
 bool BraveActionsContainer::HasAnyExtensions() const {
   return false;
 }
+
+void BraveActionsContainer::UpdateToolbarActionHoverCard(
+    ToolbarActionView* action_view,
+    ToolbarActionHoverCardUpdateType update_type) {}
