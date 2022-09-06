784d9832898e0cfeaefe53f0a6733b7ba317adca
Simon Hong <shong@brave.com>
2022-09-06 11:22:02 +0900

    fixup! Fixed item is empty in sidebar add bubble for non-standard url

--- browser/ui/views/sidebar/sidebar_items_contents_view.cc
+++ browser/ui/views/sidebar/sidebar_items_contents_view.cc
@@ -10,6 +10,7 @@
 #include "base/bind.h"
 #include "base/i18n/case_conversion.h"
 #include "base/notreached.h"
+#include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "brave/app/vector_icons/vector_icons.h"
 #include "brave/browser/ui/brave_browser.h"
@@ -54,8 +55,7 @@ std::string GetFirstCharFromURL(const GURL& url) {
   std::string target = url.host();
   if (target.empty())
     target = url.spec();
-  size_t pos = target.find("www.");
-  if (pos != std::string::npos && pos == 0) {
+  if (base::StartsWith(target, "www.")) {
     target = target.substr(4, 1);
   } else {
     target = target.substr(0, 1);
