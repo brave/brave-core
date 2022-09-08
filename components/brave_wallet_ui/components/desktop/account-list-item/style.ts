6c6707dff71c9972ec9f827ae3cf4757d8a93ebb
Douglas Daniel <douglashdaniel@gmail.com>
2022-09-08 16:58:23 -0500

    feat(wallet): Added Buttons to Account List Item

--- components/brave_wallet_ui/components/desktop/account-list-item/style.ts
+++ components/brave_wallet_ui/components/desktop/account-list-item/style.ts
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-import TrashIcon from '../../../assets/svg-icons/trash-icon.svg'
 import FlashdriveIcon from '../../../assets/svg-icons/flashdrive-icon.svg'
 import { WalletButton } from '../../shared/style'
 
@@ -13,7 +12,14 @@ export const StyledWrapper = styled.div`
   justify-content: space-between;
   flex-direction: row;
   width: 100%;
-  margin-bottom: 15px;
+  padding: 7px;
+  background-color: none;
+  border-radius: 10px;
+  --show-buttons: none;
+  &:hover {
+    --show-buttons: flex;
+    background-color: ${(p) => p.theme.color.interactive08}15;
+  }
 `
 
 export const NameAndIcon = styled.div`
@@ -63,7 +69,7 @@ export const AccountAddress = styled(WalletButton)`
 `
 
 export const RightSide = styled.div`
-  display: flex;
+  display: var(--show-buttons);
   align-items: flex-end;
   justify-content: center;
   flex-direction: row;
@@ -78,22 +84,39 @@ export const AccountCircle = styled.div<StyleProps>`
   margin-right: 12px;
 `
 
-export const DeleteButton = styled(WalletButton)`
-  display: flex;;
+export const Icon = styled.div<{
+  icon: string
+}>`
+  width: 14px;
+  height: 14px;
+  margin-left: 4px;
+  margin-right: 8px;
+  background-color: ${(p) => p.theme.color.interactive07};
+  -webkit-mask-image: url(${(p) => p.icon});
+  mask-image: url(${(p) => p.icon});
+  mask-size: 100%;
+`
+
+export const OvalButton = styled(WalletButton)`
+  display: flex;
   align-items: center;
   justify-content: center;
   cursor: pointer;
   outline: none;
   background: none;
-  border: none;
+  border-radius: 48px;
+  padding: 3px 10px;
+  border: 1px solid ${(p) => p.theme.color.interactive08};
+  margin-right: 6px;
 `
 
-export const DeleteIcon = styled.div`
-  width: 18px;
-  height: 18px;
-  background-color: ${(p) => p.theme.color.text02};
-  -webkit-mask-image: url(${TrashIcon});
-  mask-image: url(${TrashIcon});
+export const OvalButtonText = styled.span`
+  font-family: Poppins;
+  font-size: 12px;
+  line-height: 18px;
+  letter-spacing: 0.01em;
+  color: ${(p) => p.theme.color.text02};
+  font-weight: 600;
 `
 
 export const HardwareIcon = styled.div`
