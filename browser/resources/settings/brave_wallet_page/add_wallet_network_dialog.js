d75ac8d1b6ad6d1b9933c38db8b024618e9b6190
Claudio DeSouza <cdesouza@igalia.com>
2022-09-13 14:48:08 +0100

    Fixing inclusion of drop down js file for Wallet webui
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/91ee380978edabe9dee5774818afbd2b73cef232
    
    commit 91ee380978edabe9dee5774818afbd2b73cef232
    Author: dpapad <dpapad@chromium.org>
    Date:   Fri Jul 22 21:42:47 2022 +0000
    
        WebUI: Refactor ui/webui/resources/cr_elements/BUILD.gn.
    
        So that the same HTML/TS files don't need to be listed multiple times.
        This is in prepration of turning on HTML/CSS minification for
        cr_elements.
    
        Bug: 1325169

--- browser/resources/settings/brave_wallet_page/add_wallet_network_dialog.js
+++ browser/resources/settings/brave_wallet_page/add_wallet_network_dialog.js
@@ -6,6 +6,7 @@
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
 import 'chrome://resources/cr_elements/cr_input/cr_input.js';
+import 'chrome://resources/cr_elements/cr_searchable_drop_down/cr_searchable_drop_down.js';
 import { Polymer, html } from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import { I18nBehavior } from 'chrome://resources/js/i18n_behavior.m.js';
 import { BraveWalletBrowserProxyImpl } from './brave_wallet_browser_proxy.m.js';
