93684799249c2e17e466dfe42de9523865978123
Claudio DeSouza <cdesouza@igalia.com>
2022-09-14 15:56:21 +0100

    FieldTrial::group() renamed
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/bf95934655c3a12bb4bfb1968b5507027899e7f4
    
    commit bf95934655c3a12bb4bfb1968b5507027899e7f4
    Author: Steven Holte <holte@chromium.org>
    Date:   Fri Aug 26 04:31:56 2022 +0000
    
        Simplify FactoryGetFieldTrial
    
        * Make GetEntropyProviderForOneTimeRandomization CHECK fail instead of
          returning nullptr. (All callers did previously) and return a reference.
        * Add GetEntropyProviderForSessionRandomization.
        * Unify the randomization_type and overide_entropy_provider arguments to
          just a required entropy_provider.
        * Remove the default_group output parameter, since it's really only used
          by tests and it's complicated.
        * Merge "withRandomizationSeed" to just be a default parameter.
    
        Bug: b/183955043

--- vendor/bat-native-ads/src/bat/ads/internal/account/user_data/studies_user_data_unittest.cc
+++ vendor/bat-native-ads/src/bat/ads/internal/account/user_data/studies_user_data_unittest.cc
@@ -35,19 +35,19 @@ TEST(BatAdsStudiesUserDataTest, GetStudies) {
   const std::string group_name_1 = "GroupA";
   const scoped_refptr<base::FieldTrial> trial_1 =
       base::FieldTrialList::CreateFieldTrial(trial_name_1, group_name_1);
-  trial_1->group();
+  trial_1->group_name();
 
   const std::string trial_name_2 = "BarStudyForBraveAds";
   const std::string group_name_2 = "GroupB";
   const scoped_refptr<base::FieldTrial> trial_2 =
       base::FieldTrialList::CreateFieldTrial(trial_name_2, group_name_2);
-  trial_2->group();
+  trial_2->group_name();
 
   const std::string trial_name_3 = "FooBarStudy";
   const std::string group_name_3 = "GroupC";
   const scoped_refptr<base::FieldTrial> trial_3 =
       base::FieldTrialList::CreateFieldTrial(trial_name_3, group_name_3);
-  trial_3->group();
+  trial_3->group_name();
 
   ASSERT_EQ(3U, base::FieldTrialList::GetFieldTrialCount());
 
