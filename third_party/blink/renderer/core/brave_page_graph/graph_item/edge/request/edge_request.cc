c12ddbf86fc005885c97d031166d338528995f9c
Aleksey Khoroshilov <akhoroshilov@brave.com>
2022-09-15 13:00:55 +0700

    Replace copyrights with the actual version.

--- third_party/blink/renderer/core/brave_page_graph/graph_item/edge/request/edge_request.cc
+++ third_party/blink/renderer/core/brave_page_graph/graph_item/edge/request/edge_request.cc
@@ -1,7 +1,7 @@
-/* Copyright (c) 2019 The Brave Software Team. Distributed under the MPL2
- * license. This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/third_party/blink/renderer/core/brave_page_graph/graph_item/edge/request/edge_request.h"
 
--- third_party/blink/renderer/core/brave_page_graph/graph_item/edge/request/edge_request_start.h
+++ third_party/blink/renderer/core/brave_page_graph/graph_item/edge/request/edge_request_start.h
@@ -1,7 +1,7 @@
-/* Copyright (c) 2019 The Brave Software Team. Distributed under the MPL2
- * license. This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #ifndef BRAVE_THIRD_PARTY_BLINK_RENDERER_CORE_BRAVE_PAGE_GRAPH_GRAPH_ITEM_EDGE_REQUEST_EDGE_REQUEST_START_H_
 #define BRAVE_THIRD_PARTY_BLINK_RENDERER_CORE_BRAVE_PAGE_GRAPH_GRAPH_ITEM_EDGE_REQUEST_EDGE_REQUEST_START_H_
