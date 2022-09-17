192e5e65f89c2bf750b852c2e712fe206f2a1198
Arthur Edelstein <arthuredelstein@gmail.com>
2022-09-16 17:33:01 -0700

    lint fixes

--- third_party/blink/renderer/core/dom/dom_high_res_time_stamp_feature.h
+++ third_party/blink/renderer/core/dom/dom_high_res_time_stamp_feature.h
@@ -3,8 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef BRAVE_THIRD_PARTY_BLINK_RENDERER_CORE_DOM_HIGH_RES_TIME_STAMP_FEATURE_H_
-#define BRAVE_THIRD_PARTY_BLINK_RENDERER_CORE_DOM_HIGH_RES_TIME_STAMP_FEATURE_H_
+#ifndef BRAVE_THIRD_PARTY_BLINK_RENDERER_CORE_DOM_DOM_HIGH_RES_TIME_STAMP_FEATURE_H_
+#define BRAVE_THIRD_PARTY_BLINK_RENDERER_CORE_DOM_DOM_HIGH_RES_TIME_STAMP_FEATURE_H_
 
 #include "third_party/blink/renderer/core/core_export.h"
 
@@ -14,4 +14,4 @@ CORE_EXPORT bool IsTimeStampRoundingEnabled();
 
 }  // namespace brave
 
-#endif  // BRAVE_THIRD_PARTY_BLINK_RENDERER_CORE_DOM_HIGH_RES_TIME_STAMP_FEATURE_H_
+#endif  // BRAVE_THIRD_PARTY_BLINK_RENDERER_CORE_DOM_DOM_HIGH_RES_TIME_STAMP_FEATURE_H_
