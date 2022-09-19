122bec011a1ff9c0429763c45331ae5d762d7bba
Jay Harris <7678024+fallaciousreasoning@users.noreply.github.com>
2022-09-20 09:22:28 +1200

    Add setting to toggle the side panel button (#15062)

---
+++ chromium_src/chrome/browser/ui/views/toolbar/side_panel_toolbar_button.cc
@@ -0,0 +1,65 @@
+// Copyright (c) 2022 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <memory>
+
+#include "base/memory/raw_ptr.h"
+#include "brave/components/constants/pref_names.h"
+#include "brave/grit/brave_generated_resources.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/views/toolbar/side_panel_toolbar_button.h"
+#include "chrome/browser/ui/views/toolbar/toolbar_button.h"
+#include "components/prefs/pref_service.h"
+#include "ui/base/models/simple_menu_model.h"
+#include "ui/views/context_menu_controller.h"
+
+#define SidePanelToolbarButton SidePanelToolbarButton_ChromiumImpl
+
+#include "src/chrome/browser/ui/views/toolbar/side_panel_toolbar_button.cc"
+
+#undef SidePanelToolbarButton
+
+namespace {
+class SidePanelMenuModel : public ui::SimpleMenuModel,
+                           public ui::SimpleMenuModel::Delegate {
+ public:
+  explicit SidePanelMenuModel(PrefService* prefs)
+      : ui::SimpleMenuModel(this), prefs_(prefs) {
+    Build();
+  }
+
+  SidePanelMenuModel(const SidePanelMenuModel&) = delete;
+  SidePanelMenuModel& operator=(const SidePanelMenuModel&) = delete;
+  ~SidePanelMenuModel() override = default;
+
+ private:
+  enum ContextMenuCommand { kHideSideBarButton };
+
+  void Build() {
+    AddItemWithStringId(ContextMenuCommand::kHideSideBarButton,
+                        IDS_HIDE_SIDE_PANEL_TOOLBAR_BUTTON);
+  }
+
+  // ui::SimpleMenuModel::Delegate:
+  void ExecuteCommand(int command_id, int event_flags) override {
+    if (command_id == ContextMenuCommand::kHideSideBarButton)
+      prefs_->SetBoolean(kShowSidePanelButton, false);
+  }
+
+  raw_ptr<PrefService> prefs_ = nullptr;
+};
+}  // namespace
+
+SidePanelToolbarButton::SidePanelToolbarButton(Browser* browser)
+    : SidePanelToolbarButton_ChromiumImpl(browser) {
+  SetMenuModel(
+      std::make_unique<SidePanelMenuModel>(browser->profile()->GetPrefs()));
+
+  // Visibility is managed by |SideBarContainerView|.
+  SetVisible(false);
+}
+
+SidePanelToolbarButton::~SidePanelToolbarButton() = default;
--- components/constants/pref_names.h
+++ components/constants/pref_names.h
@@ -26,6 +26,7 @@ extern const char kAskWidevineInstall[];
 extern const char kBraveThemeType[];
 extern const char kUseOverriddenBraveThemeType[];
 extern const char kShowBookmarksButton[];
+extern const char kShowSidePanelButton[];
 extern const char kLocationBarIsWide[];
 extern const char kReferralDownloadID[];
 extern const char kReferralTimestamp[];
