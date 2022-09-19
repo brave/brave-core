b097f5cad4a9cb21c60818cd0ee1ed9eb1fed033
samartnik <artem@brave.com>
2022-09-19 17:27:52 -0400

    [Android] Added check for Tab switcher on return flag

---
+++ android/java/org/chromium/chrome/browser/tasks/BraveReturnToChromeUtil.java
@@ -0,0 +1,19 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.chromium.chrome.browser.tasks;
+
+import org.chromium.chrome.browser.BraveFeatureUtil;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
+
+public final class BraveReturnToChromeUtil {
+    public static boolean shouldShowTabSwitcher(final long lastBackgroundedTimeMillis) {
+        if (!BraveFeatureUtil.sTabSwitcherOnReturn.isEnabled()) {
+            return false;
+        }
+
+        return ReturnToChromeUtil.shouldShowTabSwitcher(lastBackgroundedTimeMillis);
+    }
+}
---
+++ build/android/bytecode/java/org/brave/bytecode/BraveReturnToChromeUtilClassAdapter.java
@@ -0,0 +1,22 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.brave.bytecode;
+
+import org.objectweb.asm.ClassVisitor;
+
+public class BraveReturnToChromeUtilClassAdapter extends BraveClassVisitor {
+    static String sReturnToChromeUtilClassName =
+            "org/chromium/chrome/browser/tasks/ReturnToChromeUtil";
+    static String sBraveReturnToChromeUtilClassName =
+            "org/chromium/chrome/browser/tasks/BraveReturnToChromeUtil";
+
+    public BraveReturnToChromeUtilClassAdapter(ClassVisitor visitor) {
+        super(visitor);
+
+        changeMethodOwner(sReturnToChromeUtilClassName, "shouldShowTabSwitcher",
+                sBraveReturnToChromeUtilClassName);
+    }
+}
