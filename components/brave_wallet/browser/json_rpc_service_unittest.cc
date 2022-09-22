3b0bd7233127abe34c129a38848e157fa802e4ef
Anton Paymyshev <anton.paymyshev@gmail.com>
2022-09-22 14:06:14 +0700

    Update json_rpc_service_unittest.cc

--- components/brave_wallet/browser/json_rpc_service_unittest.cc
+++ components/brave_wallet/browser/json_rpc_service_unittest.cc
@@ -1299,7 +1299,7 @@ TEST_F(JsonRpcServiceUnitTest, EnsGetEthAddr_ZeroAddress) {
   EXPECT_CALL(callback,
               Run("", mojom::ProviderError::kInvalidParams,
                   l10n_util::GetStringUTF8(IDS_WALLET_INVALID_PARAMETERS)));
-  json_rpc_service_->EnsGetEthAddr("brantly-test.eth", nullptr, callback.Get());
+  json_rpc_service_->EnsGetEthAddr("brantly-test.eth", callback.Get());
   base::RunLoop().RunUntilIdle();
 }
 
