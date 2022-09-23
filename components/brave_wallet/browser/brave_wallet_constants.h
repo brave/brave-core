8614329530fd45656e53f3e1c8bad96f589831b3
Anirudha Bose <anirudha.bose@alumni.cern>
2022-09-23 21:22:52 +0530

    feat(wallet): only use direct routes for Jupiter quotes

--- components/brave_wallet/browser/brave_wallet_constants.h
+++ components/brave_wallet/browser/brave_wallet_constants.h
@@ -956,7 +956,7 @@ constexpr char kAffiliateAddress[] =
 constexpr char kSolanaSwapBaseAPIURL[] = "https://quote-api.jup.ag/";
 constexpr char kSolanaBuyTokenFeeBps[] = "85";
 constexpr char kSolanaFeeRecipient[] =
-    "8eekKfUAGSJbq3CdA2TmHb8tKuyzd5gtEas3MYAtXzrT";
+    "3NUW8hWoCnLgJwWCVnwdFo2Dsz8bKwLac9A3VgS2jLUQ";
 
 constexpr int64_t kBlockTrackerDefaultTimeInSeconds = 20;
 
