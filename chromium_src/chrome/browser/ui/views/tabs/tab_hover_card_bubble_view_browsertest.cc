1d7cb587d17b445e61e3a0ad3e220183128d52f6
mkarolin <max@brave.com>
2022-09-30 18:01:36 -0400

    Updates TabHoverCardBubbleViewBrowserTest with upstream changes.
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/4021762e0a866da98e41e9813c628cd8793aa84c
    
    commit 4021762e0a866da98e41e9813c628cd8793aa84c
    Author: Dana Fried <dfried@chromium.org>
    Date:   Thu Sep 22 01:03:26 2022 +0000
    
        [Tab Hover Cards] Rewrite tests to (hopefully) be more reliable.
    
        This CL does not remove any tests, but rather moves and restructures
        existing tests that have historically been flaky.
    
        It moves a number of tests from browser_tests to interactive_ui_tests
        (though it's actually a cost savings since tests were running in
        pixel_browser_tests that did not need to); running tests heavily
        dependent on mouse position in an environment where tests were not
        guaranteed to run in a single-process environment was a recipe for
        flakes.
    
        It makes the actual pixel tests explicit, moving them to
        tab_hover_card_bubble_view_dialog_browsertest.cc
    
        It adds a common library of methods and helpers for writing tab hover
        card controller tests.
    
        It removes a number of unnecessary waits on widget state and moves the cursor to the upper left of the screen before starting the test to avoid potentially triggering a hover card by hovering a tab (these were
        also potentially a source of flakiness).
    
        LOW_COVERAGE_REASON=Low coverage on test code itself; don't know why we're getting this error.
    
        Bug: 1363394, 1050765

--- chromium_src/chrome/browser/ui/views/tabs/tab_hover_card_bubble_view_browsertest.cc
+++ chromium_src/chrome/browser/ui/views/tabs/tab_hover_card_bubble_view_browsertest.cc
@@ -11,102 +11,68 @@
 #include "chrome/browser/ui/browser_command_controller.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/tabs/tab_renderer_data.h"
 #include "chrome/browser/ui/test/test_browser_dialog.h"
-#include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/tabs/tab.h"
-#include "chrome/browser/ui/views/tabs/tab_close_button.h"
-#include "chrome/browser/ui/views/tabs/tab_hover_card_bubble_view.h"
-#include "chrome/browser/ui/views/tabs/tab_hover_card_controller.h"
-#include "chrome/browser/ui/views/tabs/tab_hover_card_metrics.h"
+#include "chrome/browser/ui/views/tabs/tab_hover_card_test_util.h"
 #include "chrome/browser/ui/views/tabs/tab_strip.h"
-#include "chrome/grit/generated_resources.h"
-#include "chrome/test/base/ui_test_utils.h"
 #include "content/public/test/browser_test.h"
-#include "ui/gfx/animation/animation_test_api.h"
-#include "ui/views/controls/label.h"
-#include "ui/views/test/widget_test.h"
-#include "ui/views/widget/widget.h"
-#include "ui/views/widget/widget_observer.h"
+#include "url/gurl.h"
 
-using views::Widget;
+namespace {
 
-class TabHoverCardBubbleViewBrowserTest : public DialogBrowserTest {
+constexpr char16_t kTabDomain[] = u"brave://settings";
+constexpr char16_t kTabTitle[] = u"Settings - Addresses and more";
+constexpr char kTabUrl[] = "chrome://settings/addresses";
+
+}  // namespace
+
+class TabHoverCardBubbleViewBrowserTest : public DialogBrowserTest,
+                                          public test::TabHoverCardTestUtil {
  public:
-  TabHoverCardBubbleViewBrowserTest()
-      : animation_mode_reset_(gfx::AnimationTestApi::SetRichAnimationRenderMode(
-            gfx::Animation::RichAnimationRenderMode::FORCE_DISABLED)) {
-    TabHoverCardController::disable_animations_for_testing_ = true;
-  }
+  TabHoverCardBubbleViewBrowserTest() = default;
+
   TabHoverCardBubbleViewBrowserTest(const TabHoverCardBubbleViewBrowserTest&) =
       delete;
   TabHoverCardBubbleViewBrowserTest& operator=(
       const TabHoverCardBubbleViewBrowserTest&) = delete;
+
   ~TabHoverCardBubbleViewBrowserTest() override = default;
-
-  void SetUpOnMainThread() override {
-    DialogBrowserTest::SetUpOnMainThread();
-    tab_strip_ = BrowserView::GetBrowserViewForBrowser(browser())->tabstrip();
-  }
-
-  TabStrip* tab_strip() { return tab_strip_; }
-
-  TabHoverCardBubbleView* hover_card() {
-    return tab_strip()->hover_card_controller_->hover_card_;
-  }
-
-  std::u16string GetHoverCardTitle() {
-    return hover_card()->GetTitleTextForTesting();
-  }
-
-  std::u16string GetHoverCardDomain() {
-    return hover_card()->GetDomainTextForTesting();
-  }
-
-  int GetHoverCardsSeenCount() {
-    return tab_strip()
-        ->hover_card_controller_->metrics_for_testing()
-        ->cards_seen_count();
-  }
-
-  void MouseExitTabStrip() {
-    ui::MouseEvent stop_hover_event(ui::ET_MOUSE_EXITED, gfx::Point(),
-                                    gfx::Point(), base::TimeTicks(),
-                                    ui::EF_NONE, 0);
-    tab_strip()->OnMouseExited(stop_hover_event);
-  }
-
-  void ClickMouseOnTab(int index) {
-    Tab* const tab = tab_strip()->tab_at(index);
-    ui::MouseEvent click_event(ui::ET_MOUSE_PRESSED, gfx::Point(), gfx::Point(),
-                               base::TimeTicks(), ui::EF_NONE, 0);
-    tab->OnMousePressed(click_event);
-  }
-
-  void HoverMouseOverTabAt(int index) {
-    // We don't use Tab::OnMouseEntered here to invoke the hover card because
-    // that path is disabled in browser tests. If we enabled it, the real mouse
-    // might interfere with the test.
-    tab_strip()->UpdateHoverCard(
-        tab_strip()->tab_at(index),
-        TabSlotController::HoverCardUpdateType::kHover);
-  }
 
   // DialogBrowserTest:
   void ShowUi(const std::string& name) override {
-    HoverMouseOverTabAt(0);
-    views::test::WidgetVisibleWaiter(hover_card()->GetWidget()).Wait();
+    TabRendererData new_tab_data = TabRendererData();
+    new_tab_data.title = kTabTitle;
+    new_tab_data.last_committed_url = GURL(kTabUrl);
+    GetTabStrip(browser())->AddTabAt(1, new_tab_data);
+
+    SimulateHoverTab(browser(), 1);
   }
 
- private:
-  std::unique_ptr<base::AutoReset<gfx::Animation::RichAnimationRenderMode>>
-      animation_mode_reset_;
+  bool VerifyUi() override {
+    if (!DialogBrowserTest::VerifyUi())
+      return false;
 
-  TabStrip* tab_strip_ = nullptr;
+    TabStrip* const tab_strip = GetTabStrip(browser());
+    Tab* const tab = tab_strip->tab_at(1);
+    if (!tab) {
+      NOTREACHED();
+      return false;
+    }
+
+    TabHoverCardBubbleView* const hover_card = GetHoverCard(tab_strip);
+    if (!hover_card) {
+      NOTREACHED();
+      return false;
+    }
+
+    EXPECT_EQ(kTabTitle, hover_card->GetTitleTextForTesting());
+    EXPECT_EQ(kTabDomain, hover_card->GetDomainTextForTesting());
+    return true;
+  }
 };
 
-// This test appears to be flaky on Windows. Upstream tests were also found to
-// be flaky, so for now we'll follow the upstream lead and disable this one on
-// Windows. See crbug.com/1050765.
+// This test times out on Windows CI, but works fine locally.
 #if BUILDFLAG(IS_WIN)
 #define MAYBE_ChromeSchemeUrl DISABLED_ChromeSchemeUrl
 #else
@@ -114,16 +80,5 @@ class TabHoverCardBubbleViewBrowserTest : public DialogBrowserTest {
 #endif
 IN_PROC_BROWSER_TEST_F(TabHoverCardBubbleViewBrowserTest,
                        MAYBE_ChromeSchemeUrl) {
-  TabRendererData new_tab_data = TabRendererData();
-  new_tab_data.title = u"Settings - Addresses and more";
-  new_tab_data.last_committed_url = GURL("chrome://settings/addresses");
-  tab_strip()->AddTabAt(1, new_tab_data);
-
-  ShowUi("default");
-  Widget* widget = hover_card()->GetWidget();
-  EXPECT_TRUE(widget != nullptr);
-  EXPECT_TRUE(widget->IsVisible());
-  HoverMouseOverTabAt(1);
-  EXPECT_EQ(GetHoverCardTitle(), u"Settings - Addresses and more");
-  EXPECT_EQ(GetHoverCardDomain(), u"brave://settings");
+  ShowAndVerifyUi();
 }
