1ae8c9f624b0ddefdfddfe668521c3e5185a67fc
deeppandya <deeppandya91@gmail.com>
2022-10-04 16:38:07 -0400

    Add playlist mojom implementation for android

---
+++ android/java/org/chromium/chrome/browser/PlaylistPageHandlerFactory.java
@@ -0,0 +1,55 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.chromium.chrome.browser;
+
+import org.chromium.base.annotations.JNINamespace;
+import org.chromium.base.annotations.NativeMethods;
+import org.chromium.chrome.browser.crypto_wallet.util.Utils;
+import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.mojo.bindings.ConnectionErrorHandler;
+import org.chromium.mojo.bindings.Interface;
+import org.chromium.mojo.bindings.Interface.Proxy.Handler;
+import org.chromium.mojo.system.MessagePipeHandle;
+import org.chromium.mojo.system.impl.CoreImpl;
+import org.chromium.playlist.mojom.PageHandler;
+
+@JNINamespace("chrome::android")
+public class PlaylistPageHandlerFactory {
+    private static final Object lock = new Object();
+    private static PlaylistPageHandlerFactory instance;
+
+    public static PlaylistPageHandlerFactory getInstance() {
+        synchronized (lock) {
+            if (instance == null) {
+                instance = new PlaylistPageHandlerFactory();
+            }
+        }
+        return instance;
+    }
+
+    private PlaylistPageHandlerFactory() {}
+
+    public PageHandler getPlaylistPageHandler(ConnectionErrorHandler connectionErrorHandler) {
+        Profile profile = Utils.getProfile(false); // Always use regular profile
+        int nativeHandle =
+                PlaylistPageHandlerFactoryJni.get().getInterfaceToPlaylistPageHandler(profile);
+        MessagePipeHandle handle = wrapNativeHandle(nativeHandle);
+        PageHandler pageHandler = PageHandler.MANAGER.attachProxy(handle, 0);
+        Handler handler = ((Interface.Proxy) pageHandler).getProxyHandler();
+        handler.setErrorHandler(connectionErrorHandler);
+
+        return pageHandler;
+    }
+
+    private MessagePipeHandle wrapNativeHandle(int nativeHandle) {
+        return CoreImpl.getInstance().acquireNativeHandle(nativeHandle).toMessagePipeHandle();
+    }
+
+    @NativeMethods
+    interface Natives {
+        int getInterfaceToPlaylistPageHandler(Profile profile);
+    }
+}
---
+++ browser/playlist/playlist_page_handler_factory.h
@@ -0,0 +1,50 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_BROWSER_PLAYLIST_PLAYLIST_PAGE_HANDLER_FACTORY_H_
+#define BRAVE_BROWSER_PLAYLIST_PLAYLIST_PAGE_HANDLER_FACTORY_H_
+
+#include "base/memory/singleton.h"
+#include "brave/browser/ui/webui/playlist_page_handler.h"
+#include "brave/components/playlist/mojom/playlist.mojom.h"
+#include "components/keyed_service/content/browser_context_keyed_service_factory.h"
+#include "components/keyed_service/core/keyed_service.h"
+#include "content/public/browser/browser_context.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
+
+class PlaylistPageHandler;
+
+namespace playlist {
+
+class PlaylistPageHandlerFactory : public BrowserContextKeyedServiceFactory {
+ public:
+  PlaylistPageHandlerFactory(const PlaylistPageHandlerFactory&) = delete;
+  PlaylistPageHandlerFactory& operator=(const PlaylistPageHandlerFactory&) =
+      delete;
+
+  static mojo::PendingRemote<mojom::PageHandler> GetForContext(
+      content::BrowserContext* context);
+  static PlaylistPageHandler* GetServiceForContext(
+      content::BrowserContext* context);
+  static PlaylistPageHandlerFactory* GetInstance();
+  static void BindForContext(
+      content::BrowserContext* context,
+      mojo::PendingReceiver<mojom::PageHandler> receiver);
+
+ private:
+  friend struct base::DefaultSingletonTraits<PlaylistPageHandlerFactory>;
+
+  PlaylistPageHandlerFactory();
+  ~PlaylistPageHandlerFactory() override;
+
+  KeyedService* BuildServiceInstanceFor(
+      content::BrowserContext* context) const override;
+  content::BrowserContext* GetBrowserContextToUse(
+      content::BrowserContext* context) const override;
+};
+
+}  // namespace playlist
+
+#endif  // BRAVE_BROWSER_PLAYLIST_PLAYLIST_PAGE_HANDLER_FACTORY_H_
