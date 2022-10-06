54f294d2b108a20640605c929669da65e18b9765
Mihai PLESA <mihai.plesa@gmail.com>
2022-10-06 09:52:10 +0300

    bumped rust deps to 1.1.0

--- script/deps_config.py
+++ script/deps_config.py
@@ -8,7 +8,7 @@ import os
 # Version number and URL for pre-configured rust dependency package
 # e.g. rust_deps_mac_0.1.0.gz
 DEPS_PACKAGES_URL = "https://brave-build-deps-public.s3.brave.com"
-RUST_DEPS_PACKAGE_VERSION = "1.0.0"
+RUST_DEPS_PACKAGE_VERSION = "1.1.0"
 MAC_TOOLCHAIN_ROOT = os.path.join(os.path.dirname(os.path.dirname(
                                   os.path.dirname(__file__))),
                                   'build', 'mac_files')
