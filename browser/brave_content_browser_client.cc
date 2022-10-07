48c984e45892f38a7d5862117376144efe4c218f
Shivan <shivankaulsahib@gmail.com>
2022-10-07 00:04:43 -0700

    Add dark mode check to OverrideWebkitPrefs

--- browser/brave_content_browser_client.cc
+++ browser/brave_content_browser_client.cc
@@ -1033,12 +1033,8 @@ BraveContentBrowserClient::CreateThrottlesForNavigation(
   return throttles;
 }
 
-bool BraveContentBrowserClient::OverrideWebPreferencesAfterNavigation(
-    WebContents* web_contents,
-    WebPreferences* prefs) {
-  bool changed =
-      ChromeContentBrowserClient::OverrideWebPreferencesAfterNavigation(
-          web_contents, prefs);
+bool PreventDarkModeFingerprinting(WebContents* web_contents,
+                                   WebPreferences* prefs) {
   Profile* profile =
       Profile::FromBrowserContext(web_contents->GetBrowserContext());
   const GURL url = web_contents->GetLastCommittedURL();
@@ -1052,14 +1048,24 @@ bool BraveContentBrowserClient::OverrideWebPreferencesAfterNavigation(
           brave_shields::features::kBraveDarkModeBlock) &&
       shields_up && fingerprinting_type == ControlType::BLOCK) {
     prefs->preferred_color_scheme = blink::mojom::PreferredColorScheme::kLight;
-    changed = true;
+    return true;
   }
-  return changed;
+  return false;
+}
+
+bool BraveContentBrowserClient::OverrideWebPreferencesAfterNavigation(
+    WebContents* web_contents,
+    WebPreferences* prefs) {
+  bool changed =
+      ChromeContentBrowserClient::OverrideWebPreferencesAfterNavigation(
+          web_contents, prefs);
+  return PreventDarkModeFingerprinting(web_contents, prefs) || changed;
 }
 
 void BraveContentBrowserClient::OverrideWebkitPrefs(WebContents* web_contents,
                                                     WebPreferences* web_prefs) {
   ChromeContentBrowserClient::OverrideWebkitPrefs(web_contents, web_prefs);
+  PreventDarkModeFingerprinting(web_contents, web_prefs);
   // This will stop NavigatorPlugins from returning fixed plugins data and will
   // allow us to return our farbled data
   web_prefs->allow_non_empty_navigator_plugins = true;
