a2ad8daa352e113b3a18d6813c5708c48b340323
Michael Herrmann <michael@herrmann.io>
2022-10-07 10:48:59 +0200

    Improve comment

--- build/commands/lib/config.js
+++ build/commands/lib/config.js
@@ -398,7 +398,7 @@ Config.prototype.buildArgs = function () {
     // On Linux x86, ELF32 cannot be > 4GiB.
     // For x86 and Arm64 Windows, chrome.dll.pdb sometimes becomes > 4 GiB and
     // llvm-pdbutil on that file errors out with "The data is in an unexpected
-    // format. Too many directory blocks." Associated llvm issue:
+    // format. Too many directory blocks". Associated llvm issue:
     // https://github.com/llvm/llvm-project/issues/54445
     args.symbol_level = 1
   }
