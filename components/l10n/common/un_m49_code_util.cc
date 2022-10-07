1a2bb73a2db76222caceaa085dea754e6a4d73f1
Terry Mancey <terry.mancey@icloud.com>
2022-10-07 15:32:03 -0500

    Add l10n UN M.49 area code util

---
+++ components/l10n/common/un_m49_code_util.cc
@@ -0,0 +1,18 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/components/l10n/common/un_m49_code_util.h"
+
+#include "base/containers/contains.h"
+#include "base/strings/string_piece.h"
+#include "brave/components/l10n/common/un_m49_code_constants.h"
+
+namespace brave_l10n {
+
+bool IsUNM49Code(const base::StringPiece code) {
+  return base::Contains(kUnM49Codes, code);
+}
+
+}  // namespace brave_l10n
