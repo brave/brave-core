a1bddb33708139fb52b4d191154558aeab6ffba1
Sujit Acharya <sujitacharya2005@gmail.com>
2022-10-11 18:05:42 +0530

    update format
    update

--- android/java/org/chromium/chrome/browser/app/BraveActivity.java
+++ android/java/org/chromium/chrome/browser/app/BraveActivity.java
@@ -1230,7 +1230,8 @@ public abstract class BraveActivity<C extends ChromeActivityComponent> extends C
         BraveNotificationWarningDialog notificationWarningDialog =
                 new BraveNotificationWarningDialog();
         notificationWarningDialog.setCancelable(false);
-        notificationWarningDialog.show(getSupportFragmentManager(), "NotificationWarningDialog");
+        notificationWarningDialog.show(getSupportFragmentManager(),
+                BraveNotificationWarningDialog.NOTIFICATION_WARNING_DIALOG_TAG);
     }
 
     /**
