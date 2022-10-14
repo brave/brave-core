976c1cd75387aea5e4b8c53ac64d3634e3f5455f
Claudio DeSouza <cdesouza@igalia.com>
2022-10-14 14:13:37 +0100

    Updateing CookieMonster constructor
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/badf72dc6e24b0e7abb016aa49bedc9d361c06cd
    
    commit badf72dc6e24b0e7abb016aa49bedc9d361c06cd
    Author: Chris Fredrickson <cfredric@chromium.org>
    Date:   Wed Oct 5 23:42:42 2022 +0000
    
        Remove FPS-enabled plumbing in the network stack.
    
        Since we already have the FPS-enabled bit in
        FirstPartySetsAccessDelegate, and those instances are already the way
        that NetworkContexts access FPS info, we don't need to duplicate this
        bool elsewhere in the network service. Furthermore, since this bool
        is going to become mutable, updating it in only one place is much
        simpler than updating all of the copies of it (and hoping that there
        aren't subtle inconsistencies as a result).
    
        Bug: 1286070, 1366918

--- chromium_src/net/cookies/cookie_monster.h
+++ chromium_src/net/cookies/cookie_monster.h
@@ -16,13 +16,10 @@ class NET_EXPORT CookieMonster : public ChromiumCookieMonster {
  public:
   // These constructors and destructors must be kept in sync with those in
   // Chromium's CookieMonster.
-  CookieMonster(scoped_refptr<PersistentCookieStore> store,
-                NetLog* net_log,
-                bool first_party_sets_enabled);
+  CookieMonster(scoped_refptr<PersistentCookieStore> store, NetLog* net_log);
   CookieMonster(scoped_refptr<PersistentCookieStore> store,
                 base::TimeDelta last_access_threshold,
-                NetLog* net_log,
-                bool first_party_sets_enabled);
+                NetLog* net_log);
   ~CookieMonster() override;
 
   // CookieStore implementation.
