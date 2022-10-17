e74f23774e0f7040c041a538e5718200f1de1b66
Mikhail Atuchin <matuchin@brave.com>
2022-10-17 19:41:33 +0600

    [translate] Clear migration preferences

--- browser/translate/brave_translate_prefs_migration.cc
+++ browser/translate/brave_translate_prefs_migration.cc
@@ -5,7 +5,6 @@
 
 #include "brave/browser/translate/brave_translate_prefs_migration.h"
 
-#include "brave/components/translate/core/common/brave_translate_features.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
 #include "components/translate/core/browser/translate_pref_names.h"
@@ -22,17 +21,8 @@ void RegisterBraveProfilePrefsForMigration(PrefRegistrySimple* registry) {
   registry->RegisterBooleanPref(prefs::kMigratedToInternalTranslation, false);
 }
 
-void MigrateBraveProfilePrefs(PrefService* prefs) {
-  if (!IsBraveTranslateGoAvailable())
-    return;
-  if (prefs->GetBoolean(prefs::kMigratedToInternalTranslation))
-    return;  // Already migrated
-
-  prefs->SetBoolean(prefs::kMigratedToInternalTranslation, true);
-
-  // TODO(matuchin): make kOfferTranslateEnabled syncable again when the
-  // migration is finished.
-  prefs->ClearPref(prefs::kOfferTranslateEnabled);
+void ClearMigrationBraveProfilePrefs(PrefService* prefs) {
+  prefs->ClearBoolean(prefs::kMigratedToInternalTranslation);
 }
 
 }  // namespace translate
