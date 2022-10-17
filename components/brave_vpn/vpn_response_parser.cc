d7e049444000846e77907845dacfdba079f20bde
Cepera <spylogsster@gmail.com>
2022-10-17 09:34:39 +0300

    Handle expired state for subscriptions (#15463)

---
+++ components/brave_vpn/vpn_response_parser.cc
@@ -0,0 +1,34 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/components/brave_vpn/vpn_response_parser.h"
+
+#include "base/json/json_reader.h"
+#include "base/logging.h"
+#include "base/values.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
+namespace brave_vpn {
+
+std::string ParseSubscriberCredentialFromJson(const std::string& json,
+                                              std::string* error) {
+  absl::optional<base::Value> records_v =
+      base::JSONReader::Read(json, base::JSON_PARSE_CHROMIUM_EXTENSIONS |
+                                       base::JSONParserOptions::JSON_PARSE_RFC);
+  if (!records_v || !records_v->is_dict()) {
+    VLOG(1) << __func__ << "Invalid response, could not parse JSON.";
+    return "";
+  }
+
+  const auto& dict = records_v->GetDict();
+  const auto* subscriber_credential = dict.FindString("subscriber-credential");
+  const auto* error_title = dict.FindString("error-title");
+  if (error_title && error) {
+    *error = *error_title;
+  }
+  return subscriber_credential == nullptr ? "" : *subscriber_credential;
+}
+
+}  // namespace brave_vpn
