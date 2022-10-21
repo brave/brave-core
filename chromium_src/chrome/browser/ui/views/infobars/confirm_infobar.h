56b57445febf3d131ad60d6abbe7a8424c53bb70
AlexeyBarabash <alexey@brave.com>
2022-10-21 10:16:06 +0300

    UI fix: changed informer on Desktop
    This Brave Sync account was deleted from other device. If you didn’t mean to remove the Sync, _re-create the account_  [OK]
    
    introduced BraveSyncAccountDeletedInfoBar class for ConfirmInfoBar customization

---
+++ chromium_src/chrome/browser/ui/views/infobars/confirm_infobar.h
@@ -0,0 +1,18 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_INFOBARS_CONFIRM_INFOBAR_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_INFOBARS_CONFIRM_INFOBAR_H_
+
+#define GetDelegate                            \
+  Unused() { return nullptr; }                 \
+  friend class BraveSyncAccountDeletedInfoBar; \
+  ConfirmInfoBarDelegate* GetDelegate
+
+#include "src/chrome/browser/ui/views/infobars/confirm_infobar.h"
+
+#undef GetDelegate
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_INFOBARS_CONFIRM_INFOBAR_H_
