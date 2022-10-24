853171fe2d5bf2e633d5e7475a59b6bdc3c5f0c7
sangwoo.ko <sangwoo108@gmail.com>
2022-10-24 21:19:32 +0900

    Fix include bug

--- chromium_src/chrome/browser/ui/views/frame/browser_non_client_frame_view_factory_views.cc
+++ chromium_src/chrome/browser/ui/views/frame/browser_non_client_frame_view_factory_views.cc
@@ -3,6 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include <functional>
+#include <type_traits>
+
 #include "build/build_config.h"
 
 #if BUILDFLAG(IS_WIN)
@@ -10,12 +13,24 @@
 #define GlassBrowserFrameView BraveGlassBrowserFrameView
 #endif
 
+// This file is included for all platform by upstream source code and we need
+// to include this first to avoid #define conflicts.
+#include "chrome/browser/ui/views/frame/browser_frame_view_linux.h"
+
 #include "brave/browser/ui/views/frame/brave_opaque_browser_frame_view.h"
 #define OpaqueBrowserFrameView BraveOpaqueBrowserFrameView
 
 #include "src/chrome/browser/ui/views/frame/browser_non_client_frame_view_factory_views.cc"
 
 #undef OpaqueBrowserFrameView
+
+// A sanity check for our macro
+static_assert(
+    std::is_same_v<std::unique_ptr<BraveOpaqueBrowserFrameView>,
+                   decltype(std::function{
+                       chrome::CreateOpaqueBrowserFrameView})::result_type>,
+    "CreateOpaqueBrowserFrameView is not returning "
+    "BraveOpaqueBrowserFrameView");
 
 #if BUILDFLAG(IS_WIN)
 #undef GlassBrowserFrameView
