a040d09998a75af6f1963f1c21ed1b26e0da4d90
Jay Harris <jay.harris@outlook.co.nz>
2022-10-24 13:21:53 +1300

    Add popular sources view

---
+++ components/brave_new_tab_ui/components/default/braveToday/customize/Popular.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react'
+import styled from 'styled-components'
+import Button from '$web-components/button'
+import { useBraveNews } from './Context'
+import DiscoverSection from './DiscoverSection'
+import FeedCard from './FeedCard'
+import { getLocale } from '$web-common/locale'
+
+const LoadMoreButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  grid-column: 2;
+`
+
+const DEFAULT_SUGGESTIONS_COUNT = 3
+
+export default function Suggestions () {
+  const { filteredPublisherIds, publishers } = useBraveNews()
+  const [showAll, setShowAll] = React.useState(false)
+  const popularPublisherIds = React.useMemo(() => filteredPublisherIds.map(id => publishers[id])
+    .filter(p => p.rank !== 0) // Rank of zero is unranked
+    .sort((a, b) => a.rank - b.rank)
+    .map(p => p.publisherId), [filteredPublisherIds, publishers])
+
+  const popularPublishersTruncated = React.useMemo(() => showAll
+    ? popularPublisherIds
+    : popularPublisherIds.slice(0, DEFAULT_SUGGESTIONS_COUNT), [popularPublisherIds, showAll])
+
+  if (!popularPublisherIds.length) {
+    return null
+  }
+
+  return (
+    <DiscoverSection name={'Popular Sources'}>
+      {popularPublishersTruncated.map(s => <FeedCard key={s} publisherId={s} />)}
+      {!showAll && popularPublisherIds.length > DEFAULT_SUGGESTIONS_COUNT &&
+      <LoadMoreButtonContainer>
+        <Button onClick={() => setShowAll(true)}>
+          {getLocale('braveNewsShowMoreButton')}
+        </Button>
+      </LoadMoreButtonContainer>}
+    </DiscoverSection>
+  )
+}
