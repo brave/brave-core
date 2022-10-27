3263578c67e6ba9752c7ff2014458351be579c43
Michael Herrmann <michael@herrmann.io>
2022-10-27 12:24:52 +0200

    Fix .adm Windows Group Policy Template

--- chromium_src/python_modules/components/policy/tools/template_writers/brave_writer_configuration.py
+++ chromium_src/python_modules/components/policy/tools/template_writers/brave_writer_configuration.py
@@ -1,6 +1,8 @@
-import writer_configuration
+import json
+import unittest
 
 def GetConfigurationForBuild(defines):
+    import writer_configuration
     base = writer_configuration.GetConfigurationForBuild({'_chromium'})
     return _merge_dicts(_BRAVE_VALUES, base)
 
@@ -17,8 +19,8 @@ _BRAVE_VALUES = {
                 'Software\\Policies\\BraveSoftware\\Brave',
             'reg_recommended_key_name':
                 'Software\\Policies\\BraveSoftware\\Brave\\Recommended',
-            'mandatory_category_path': ['brave'],
-            'recommended_category_path': ['brave_recommended'],
+            'mandatory_category_path': ['Brave:Cat_Brave', 'brave'],
+            'recommended_category_path': ['Brave:Cat_Brave', 'brave_recommended'],
             'category_path_strings': {
                 'brave': 'Brave',
                 'brave_recommended':
@@ -27,14 +29,32 @@ _BRAVE_VALUES = {
             'namespace': 'BraveSoftware.Policies.Brave',
         },
     },
+    # The string 'Brave' is defined in bravesoftware.adml for ADMX, but ADM
+    # doesn't support external references, so we define this map here.
+    'adm_category_path_strings': {
+        'Brave:Cat_Brave': 'Brave'
+    },
     'admx_prefix': 'brave',
+    'admx_using_namespaces': {
+        'Brave': 'BraveSoftware.Policies'  # prefix: namespace
+    },
     'linux_policy_path': '/etc/brave/policies/',
     'bundle_id': 'com.brave.ios.core',
 }
 
 def _merge_dicts(src, dst):
-    return {
-        k: _merge_dicts(src.get(k, {}), v) if isinstance(v, dict) else
-           src.get(k, v)
-        for k, v in dst.items()
-    }
+    result = dict(dst)
+    for k, v in src.items():
+        result[k] = \
+            _merge_dicts(v, dst.get(k, {})) if isinstance(v, dict) else v
+    return result
+
+class MergeDictsTest(unittest.TestCase):
+    def test_merge(self):
+        self.assertEqual(
+            {'brave': 'Brave', "googlechrome": "Google Chrome"},
+            _merge_dicts({'brave': 'Brave'}, {"googlechrome": "Google Chrome"})
+        )
+
+if __name__ == '__main__':
+    unittest.main()
