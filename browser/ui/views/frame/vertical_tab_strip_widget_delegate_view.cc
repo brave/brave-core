0ccd2b5ec25871a67cd202043126bf1ca07aabb8
sangwoo.ko <sangwoo108@gmail.com>
2022-10-27 19:13:19 +0900

    Try fixing test

--- browser/ui/views/frame/vertical_tab_strip_widget_delegate_view.cc
+++ browser/ui/views/frame/vertical_tab_strip_widget_delegate_view.cc
@@ -112,7 +112,8 @@ void VerticalTabStripWidgetDelegateView::UpdateWidgetBounds() {
     return;
 
   auto* widget = GetWidget();
-  DCHECK(widget);
+  if (!widget)
+    return;
 
   // Convert coordinate system based on Browser's widget.
   gfx::Rect widget_bounds = host_->ConvertRectToWidget(host_->GetLocalBounds());
