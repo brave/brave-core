aee5da75a87b99c77d63404b3fd8692e859d19a8
Sangwoo Ko <sko@brave.com>
2022-10-27 16:24:34 +0900

    Use BraveTabHoverCardController instead of upstream implementation (#15671)
    * Use BraveTabHoverCardController instead of upstream's implementation.
    
    This was accidentally missed during rebase.

--- chromium_src/chrome/browser/ui/views/tabs/tab_strip.cc
+++ chromium_src/chrome/browser/ui/views/tabs/tab_strip.cc
@@ -23,6 +23,7 @@
   AddTab(std::make_unique<BraveTab>(this), MODEL_INDEX, PINNED)
 #define CompoundTabContainer BraveCompoundTabContainer
 #define TabContainerImpl BraveTabContainer
+#define TabHoverCardController BraveTabHoverCardController
 #define BRAVE_CALCULATE_INSERTION_INDEX                                       \
   if (tabs::features::ShouldShowVerticalTabs()) {                             \
     const int ideal_y =                                                       \
@@ -59,6 +60,7 @@
 
 #undef BRAVE_CALCULATE_BOUNDS_FOR_DRAGGED_VIEWS
 #undef BRAVE_CALCULATE_INSERTION_INDEX
+#undef TabHoverCardController
 #undef CompoundTabContainer
 #undef TabContainerImpl
 #undef AddTab
