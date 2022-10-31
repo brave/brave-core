2455397f3fd552dcfed85169e59d1abc5724229f
Claudio DeSouza <cdesouza@igalia.com>
2022-10-31 18:05:56 +0000

    HostContentSettingsMap and DefaultProvider metric support
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/882516cc2f5d61f5f2c1c297662d5165c10119f0
    
    commit 882516cc2f5d61f5f2c1c297662d5165c10119f0
    Author: Ryan Sultanem <rsult@google.com>
    Date:   Thu Oct 20 12:39:12 2022 +0000
    
        ContentSettings Histogram not to record metrics for irregular Profiles
    
        ContentSettings.* histograms used to record metrics for all profiles,
        including irregular ones (Incognito, Guest, System Profile and other
        ChromeOS specific irregular profiles).
    
        Now those histograms only record metrics for Regular user profiles.
    
        Renamed the histograms to follow the following logic:
    
        ContentSettings.{RegularProfileFiltered}Default*
        ContentSettings.{RegularProfileFiltered}Exceptions.*
    
        Where RegularProfileFiltered={"","RegularProfile."}
        ""                 -> old obsolete histograms (all profile types).
        "RegularProfile."  -> new histograms (regular profile only).
    
        Bug: 1259891,1284659

--- chromium_src/components/privacy_sandbox/privacy_sandbox_settings_unittest.cc
+++ chromium_src/components/privacy_sandbox/privacy_sandbox_settings_unittest.cc
@@ -43,7 +43,7 @@ class PrivacySandboxSettingsTest : public testing::Test {
 
     host_content_settings_map_ = new HostContentSettingsMap(
         &prefs_, false /* is_off_the_record */, false /* store_last_modified */,
-        false /* restore_session */);
+        false /* restore_session */, false /* should_record_metrics */);
     cookie_settings_ = new content_settings::CookieSettings(
         host_content_settings_map_.get(), &prefs_, false, "chrome-extension");
   }
