4895cea9702ac550444625c0e63a2ef156b2cda7
Michael Herrmann <michael@herrmann.io>
2022-10-31 09:44:21 +0100

    Add comment

--- components/policy/pack_policy_templates.py
+++ components/policy/pack_policy_templates.py
@@ -3,6 +3,10 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+"""
+Create a Zip file of Windows Group Policy templates similar to Chrome's.
+"""
+
 import argparse
 import os
 
