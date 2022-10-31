b5b62268d0be0b66bb17fbc956ed3bd2d62e75e4
Michael Herrmann <michael@herrmann.io>
2022-10-31 09:31:17 +0100

    Add copyright notice to new file

--- chromium_src/python_modules/components/policy/tools/template_writers/brave_writer_configuration.py
+++ chromium_src/python_modules/components/policy/tools/template_writers/brave_writer_configuration.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+# Copyright 2022 The Brave Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 import unittest
 import writer_configuration
 
