d81983c6500d67d1a266b36888928dc109230b6e
Josh Leonard <jleonard@brave.com>
2022-10-31 18:05:44 -0600

    chore: update createWalletReducer to use RTK

--- components/brave_wallet_ui/common/reducers/wallet_reducer.ts
+++ components/brave_wallet_ui/common/reducers/wallet_reducer.ts
@@ -3,7 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { createReducer } from 'redux-act'
 import {
   AccountInfo,
   AccountTransactions,
@@ -530,9 +529,7 @@ export const createWalletSlice = (initialState: WalletState = defaultState) => {
 }
 
 export const createWalletReducer = (initialState: WalletState) => {
-  const reducer = createReducer<WalletState>({}, initialState)
-
-  return reducer
+  return createWalletSlice(initialState).reducer
 }
 
 const reducer = createWalletReducer(defaultState)
