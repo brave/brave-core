45904ffc9c5b5397f7707d2afaf6c0db6ace4282
Cepera <spylogsster@gmail.com>
2022-11-01 13:52:26 +0300

    Added parallel import (#15674)
    * Added opportunity to make multiple import requests in parallel

---
+++ browser/ui/webui/settings/brave_importer_observer.cc
@@ -0,0 +1,83 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/browser/ui/webui/settings/brave_importer_observer.h"
+
+#include <utility>
+
+#include "base/logging.h"
+#include "chrome/browser/importer/external_process_importer_host.h"
+
+BraveImporterObserver::BraveImporterObserver(
+    ExternalProcessImporterHost* importer_host,
+    const importer::SourceProfile& source_profile,
+    uint16_t imported_items,
+    ReportProgressCallback callback)
+    : source_profile_(source_profile),
+      imported_items_(imported_items),
+      callback_(std::move(callback)),
+      importer_host_(importer_host) {
+  DCHECK(importer_host);
+  importer_host->set_observer(this);
+}
+
+BraveImporterObserver::~BraveImporterObserver() {
+  if (importer_host_)
+    importer_host_->set_observer(nullptr);
+}
+
+// importer::ImporterProgressObserver:
+void BraveImporterObserver::ImportStarted() {
+  if (import_started_called_)
+    return;
+  import_started_called_ = true;
+  base::Value::Dict data;
+  data.Set("importer_name", source_profile_.importer_name);
+  data.Set("importer_type", source_profile_.importer_type);
+  data.Set("items_to_import", imported_items_);
+  data.Set("event", "ImportStarted");
+  callback_.Run(source_profile_, base::Value(std::move(data)));
+}
+
+void BraveImporterObserver::ImportItemStarted(importer::ImportItem item) {
+  base::Value::Dict data;
+  data.Set("importer_name", source_profile_.importer_name);
+  data.Set("importer_type", source_profile_.importer_type);
+  data.Set("items_to_import", imported_items_);
+  data.Set("event", "ImportItemStarted");
+  data.Set("item", item);
+  callback_.Run(source_profile_, base::Value(std::move(data)));
+}
+
+void BraveImporterObserver::ImportItemEnded(importer::ImportItem item) {
+  base::Value::Dict data;
+  data.Set("importer_name", source_profile_.importer_name);
+  data.Set("importer_type", source_profile_.importer_type);
+  data.Set("items_to_import", imported_items_);
+  data.Set("event", "ImportItemEnded");
+  data.Set("item", item);
+  callback_.Run(source_profile_, base::Value(std::move(data)));
+}
+
+void BraveImporterObserver::ImportEnded() {
+  base::Value::Dict data;
+  data.Set("importer_name", source_profile_.importer_name);
+  data.Set("importer_type", source_profile_.importer_type);
+  data.Set("items_to_import", imported_items_);
+  data.Set("event", "ImportEnded");
+
+  DCHECK(importer_host_);
+  if (importer_host_)
+    importer_host_->set_observer(nullptr);
+
+  importer_host_ = nullptr;
+
+  callback_.Run(source_profile_, base::Value(std::move(data)));
+}
+
+ExternalProcessImporterHost*
+BraveImporterObserver::GetImporterHostForTesting() {
+  return importer_host_.get();
+}
---
+++ browser/ui/webui/settings/import_feature.h
@@ -0,0 +1,15 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_BROWSER_UI_WEBUI_SETTINGS_IMPORT_FEATURE_H_
+#define BRAVE_BROWSER_UI_WEBUI_SETTINGS_IMPORT_FEATURE_H_
+
+#include "base/feature_list.h"
+
+namespace settings {
+bool IsParallelImportEnabled();
+}  // namespace settings
+
+#endif  // BRAVE_BROWSER_UI_WEBUI_SETTINGS_IMPORT_FEATURE_H_
