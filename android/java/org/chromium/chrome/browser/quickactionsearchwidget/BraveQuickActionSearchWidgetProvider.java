a81a9b3432958a09c75bfe190325f198a7f169fe
deeppandya <deeppandya91@gmail.com>
2022-11-02 12:42:36 -0400

    Set public to new owner method

--- android/java/org/chromium/chrome/browser/quickactionsearchwidget/BraveQuickActionSearchWidgetProvider.java
+++ android/java/org/chromium/chrome/browser/quickactionsearchwidget/BraveQuickActionSearchWidgetProvider.java
@@ -10,7 +10,7 @@ package org.chromium.chrome.browser.quickactionsearchwidget;
 import org.chromium.chrome.browser.quickactionsearchwidget.QuickActionSearchWidgetProvider;
 
 public abstract class BraveQuickActionSearchWidgetProvider extends QuickActionSearchWidgetProvider {
-    private static void setWidgetEnabled(
+    public static void setWidgetEnabled(
             boolean shouldEnableQuickActionSearchWidget, boolean shouldEnableDinoVariant) {
         // We don't need to do anything here as in QuickActionSearchWidgetProvider we only set dino
         // and quick search widgets.
