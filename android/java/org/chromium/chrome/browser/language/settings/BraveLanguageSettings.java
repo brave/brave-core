a25e1facd31c85bdfd96865dff89ccfc9da038a3
deeppandya <deeppandya91@gmail.com>
2022-11-04 14:36:09 -0400

    Add settings option to enable/disable brave translate

--- android/java/org/chromium/chrome/browser/language/settings/BraveLanguageSettings.java
+++ android/java/org/chromium/chrome/browser/language/settings/BraveLanguageSettings.java
@@ -9,8 +9,16 @@ import android.os.Bundle;
 
 import androidx.preference.PreferenceCategory;
 
+import org.chromium.chrome.R;
+import org.chromium.chrome.browser.BraveRelaunchUtils;
+import org.chromium.chrome.browser.preferences.BravePref;
+import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.components.browser_ui.settings.ChromeSwitchPreference;
+import org.chromium.components.user_prefs.UserPrefs;
+
 public class BraveLanguageSettings extends LanguageSettings {
     static final String TRANSLATION_SETTINGS_SECTION = "translation_settings_section";
+    static final String APP_LANGUAGE_SECTION = "app_language_section";
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -20,6 +28,29 @@ public class BraveLanguageSettings extends LanguageSettings {
                 (PreferenceCategory) findPreference(TRANSLATION_SETTINGS_SECTION);
         if (translateSwitch != null) {
             getPreferenceScreen().removePreference(translateSwitch);
+        }
+
+        PreferenceCategory appLanguageSection =
+                (PreferenceCategory) findPreference(APP_LANGUAGE_SECTION);
+        if (appLanguageSection != null) {
+            boolean isBraveTranslateEnabled =
+                    UserPrefs.get(Profile.getLastUsedRegularProfile())
+                            .getBoolean(BravePref.OFFER_TRANSLATE_ENABLED);
+            ChromeSwitchPreference braveTranslateFeaturePreference =
+                    new ChromeSwitchPreference(getContext());
+            braveTranslateFeaturePreference.setTitle(
+                    getResources().getString(R.string.use_brave_translate));
+            braveTranslateFeaturePreference.setChecked(isBraveTranslateEnabled);
+            braveTranslateFeaturePreference.setOnPreferenceChangeListener(
+                    (preference, newValue) -> {
+                        UserPrefs.get(Profile.getLastUsedRegularProfile())
+                                .setBoolean(BravePref.OFFER_TRANSLATE_ENABLED, (boolean) newValue);
+                        if (getActivity() != null) {
+                            BraveRelaunchUtils.askForRelaunch(getActivity());
+                        }
+                        return true;
+                    });
+            appLanguageSection.addPreference(braveTranslateFeaturePreference);
         }
     }
 }
