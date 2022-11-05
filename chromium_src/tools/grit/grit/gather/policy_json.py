0e69eaba55bcbba34efc6f76f1d79fa02dc5bcba
Michael Herrmann <michael@herrmann.io>
2022-11-05 19:58:40 +0100

    Use override_utils to redefine method

---
+++ chromium_src/tools/grit/grit/gather/policy_json.py
@@ -0,0 +1,17 @@
+# Copyright 2022 The Brave Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import override_utils
+
+@override_utils.override_method(PolicyJson)
+def SetDefines(self, original_method, defines):
+    if '_google_chrome' in defines:
+        self._config = {
+            'build': 'brave',
+            'app_name': 'Brave',
+            'frame_name': 'Brave Frame',
+            'os_name': 'Google Chrome OS'
+        }
+    else:
+        original_method(self, defines)
