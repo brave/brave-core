077e87894b7433883ccbc57f6cc90b8c14f60552
Michael Herrmann <michael@herrmann.io>
2022-11-05 20:00:12 +0100

    Fix indent

--- components/policy/pack_policy_templates.py
+++ components/policy/pack_policy_templates.py
@@ -41,11 +41,11 @@ def _pack_policy_templates(chrome_policy_zip, dest_zip):
     assert exists(join(tmp_dir, 'windows/admx/en-US/chrome.adml'))
 
     with ZipFile(dest_zip, 'w') as dest_zipfile:
-       for dirpath, _, filenames in os.walk(tmp_dir):
-         for filename in filenames:
-           filepath = join(dirpath, filename)
-           arcname = relpath(filepath, tmp_dir).replace('chrome', 'brave')
-           dest_zipfile.write(filepath, arcname=arcname)
+      for dirpath, _, filenames in os.walk(tmp_dir):
+        for filename in filenames:
+          filepath = join(dirpath, filename)
+          arcname = relpath(filepath, tmp_dir).replace('chrome', 'brave')
+          dest_zipfile.write(filepath, arcname=arcname)
 
 if __name__ == '__main__':
   main()
