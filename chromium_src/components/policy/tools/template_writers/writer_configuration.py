1027d1320baa99b5cbfd09f9c0203c68a862ce6e
Michael Herrmann <michael@herrmann.io>
2022-11-05 19:57:38 +0100

    Remove unnecessary file header

--- chromium_src/components/policy/tools/template_writers/writer_configuration.py
+++ chromium_src/components/policy/tools/template_writers/writer_configuration.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # Copyright 2022 The Brave Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
