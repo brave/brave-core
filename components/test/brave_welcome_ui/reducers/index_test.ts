4647e72c984c5b1296480948bd44a7d644b29a60
Jay Harris <jay.harris@outlook.co.nz>
2022-11-07 10:18:11 +1300

    Fix bad license (not first)

--- components/test/brave_welcome_ui/reducers/index_test.ts
+++ components/test/brave_welcome_ui/reducers/index_test.ts
@@ -1,7 +1,7 @@
-/* global describe, it */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
+/* global describe, it */
 
 import reducers from '../../../brave_welcome_ui/reducers/index'
 import { welcomeInitialState } from '../../testData'
