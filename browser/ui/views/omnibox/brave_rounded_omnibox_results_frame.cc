982fc38940c8a8fce8750ff10d3a9f926019f274
Sangwoo Ko <sko@brave.com>
2022-11-07 18:23:26 +0900

    Remove top shadow for omnibox popup when using vertical tabs and hiding window title (#15819)
    * Remove top shadow for omnibox popup when using vertical tabs and hiding window title
    
    In this case, popup's bounds shouldn't be out of browser. Especially on
    Mac, Widgets can't be out of workspace bounds. If we set a coordinate
    that out of the workspace, it'll be adjusted by the system. This leads
    to UI glitch. So we should remove top shadow of the popup so that it
    can stay inside browser.

---
+++ browser/ui/views/omnibox/brave_rounded_omnibox_results_frame.cc
@@ -0,0 +1,61 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/browser/ui/views/omnibox/brave_rounded_omnibox_results_frame.h"
+
+#include <memory>
+#include <utility>
+
+#include "brave/browser/ui/tabs/brave_tab_prefs.h"
+#include "brave/browser/ui/views/tabs/features.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/views/location_bar/location_bar_view.h"
+#include "ui/base/metadata/metadata_impl_macros.h"
+
+BraveRoundedOmniboxResultsFrame::BraveRoundedOmniboxResultsFrame(
+    views::View* contents,
+    LocationBarView* location_bar)
+    : RoundedOmniboxResultsFrame(contents, location_bar),
+      browser_(location_bar->browser()) {
+  UpdateShadowBorder();
+
+  show_vertical_tabs_.Init(
+      brave_tabs::kVerticalTabsEnabled,
+      browser_->profile()->GetOriginalProfile()->GetPrefs(),
+      base::BindRepeating(&BraveRoundedOmniboxResultsFrame::UpdateShadowBorder,
+                          base::Unretained(this)));
+  show_window_title_for_vertical_tabs_.Init(
+      brave_tabs::kVerticalTabsShowTitleOnWindow,
+      browser_->profile()->GetOriginalProfile()->GetPrefs(),
+      base::BindRepeating(&BraveRoundedOmniboxResultsFrame::UpdateShadowBorder,
+                          base::Unretained(this)));
+}
+
+BraveRoundedOmniboxResultsFrame::~BraveRoundedOmniboxResultsFrame() = default;
+
+void BraveRoundedOmniboxResultsFrame::UpdateShadowBorder() {
+  int corner_radius = views::LayoutProvider::Get()->GetCornerRadiusMetric(
+      views::Emphasis::kHigh);
+
+  auto border = std::make_unique<views::BubbleBorder>(
+      views::BubbleBorder::Arrow::NONE,
+      views::BubbleBorder::Shadow::STANDARD_SHADOW);
+  border->SetCornerRadius(corner_radius);
+  border->set_md_shadow_elevation(GetShadowElevation());
+  if (tabs::features::ShouldShowVerticalTabs(browser_) &&
+      !tabs::features::ShouldShowWindowTitleForVerticalTabs(browser_)) {
+    // Remove top shadow inset so that omnibox popup stays inside browser
+    // widget. Especially on Mac, Widgets can't be out of screen so we need to
+    // adjust popup position.
+    // https://github.com/brave/brave-browser/issues/26573
+    border->set_insets(
+        RoundedOmniboxResultsFrame::GetShadowInsets().set_top(0));
+  }
+  SetBorder(std::move(border));
+}
+
+BEGIN_METADATA(BraveRoundedOmniboxResultsFrame, RoundedOmniboxResultsFrame)
+END_METADATA
---
+++ chromium_src/chrome/browser/ui/views/omnibox/rounded_omnibox_results_frame.h
@@ -0,0 +1,17 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_OMNIBOX_ROUNDED_OMNIBOX_RESULTS_FRAME_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_OMNIBOX_ROUNDED_OMNIBOX_RESULTS_FRAME_H_
+
+#define GetShadowInsets \
+  GetShadowInsets();    \
+  static int GetShadowElevation
+
+#include "src/chrome/browser/ui/views/omnibox/rounded_omnibox_results_frame.h"
+
+#undef GetShadowInsets
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_OMNIBOX_ROUNDED_OMNIBOX_RESULTS_FRAME_H_
