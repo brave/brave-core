39471e38559d6b23cc521d771c8e3e00fbc90b36
Kyle Hickinson <git@kylehickinson.com>
2022-11-14 18:08:33 -0500

    [Q49] Add answer for `Brave.Today.DirectFeedsTotal`

--- Sources/Growth/P3A.swift
+++ Sources/Growth/P3A.swift
@@ -5,6 +5,7 @@
 
 import Foundation
 import BraveCore
+import os.log
 
 /// For adding a sample to an enumerated histogram
 public func UmaHistogramEnumeration<E: RawRepresentable & CaseIterable>(
@@ -12,4 +13,42 @@ public func UmaHistogramEnumeration<E: RawRepresentable & CaseIterable>(
   sample: E
 ) where E.RawValue == Int {
   UmaHistogramExactLinear(name, sample.rawValue, E.allCases.count + 1)
+}
+
+/// A bucket that may span a single value or a range of values
+///
+/// Essentially a type eraser around `RangeExpression`
+public struct Bucket {
+  var contains: (Int) -> Bool
+  public static func equals(_ value: Int) -> Self {
+    .init(contains: { value == $0 })
+  }
+  public static func r(_ value: some RangeExpression<Int>) -> Self {
+    .init(contains: { value.contains($0) })
+  }
+}
+
+extension Bucket: ExpressibleByIntegerLiteral {
+  public init(integerLiteral value: IntegerLiteralType) {
+    self = .equals(value)
+  }
+}
+
+/// Adds a sample to a specific bucket. The answer will be the index of the bucket the value falls into.
+///
+/// Examples:
+///   `UmaHistogramRecordValueToBucket("", buckets: [0, .r(1..<10), 10, .r(11...)], value: 0)` would answer 0
+///   `UmaHistogramRecordValueToBucket("", buckets: [0, .r(1..<10), 10, .r(11...)], value: 4)` would answer 1
+///   `UmaHistogramRecordValueToBucket("", buckets: [0, .r(1..<10), 10, .r(11...)], value: 10)` would answer 2
+///   `UmaHistogramRecordValueToBucket("", buckets: [0, .r(1..<10), 10, .r(11...)], value: 21)` would answer 3
+public func UmaHistogramRecordValueToBucket(
+  _ name: String,
+  buckets: [Bucket],
+  value: Int
+) {
+  guard let answer = buckets.firstIndex(where: { $0.contains(value) }) else {
+    Logger.module.warning("Value (\(value)) not found in any bucket for histogram \(name)")
+    return
+  }
+  UmaHistogramExactLinear(name, answer, buckets.count + 1)
 }
