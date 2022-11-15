006bdb37f906251cee287698f6886bdf49b44b30
Taher <8665427+nullhook@users.noreply.github.com>
2022-11-15 08:47:19 -0800

    Enable speedreader v2 panel flag

--- components/speedreader/common/features.cc
+++ components/speedreader/common/features.cc
@@ -20,7 +20,7 @@ BASE_FEATURE(kSpeedreaderFeature,
 
 BASE_FEATURE(kSpeedreaderPanelV2,
              "SpeedreaderPanelV2",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 const base::FeatureParam<int> kSpeedreaderMinOutLengthParam{
     &kSpeedreaderFeature, "min_out_length", 1000};
