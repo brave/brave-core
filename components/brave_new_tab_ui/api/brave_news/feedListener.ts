11fff49b6ad99e8633e5226d46bf8f1de52f1fc8
Jay Harris <jay.harris@outlook.co.nz>
2022-11-22 12:41:45 +1300

    linting

--- components/brave_new_tab_ui/api/brave_news/feedListener.ts
+++ components/brave_new_tab_ui/api/brave_news/feedListener.ts
@@ -1,26 +1,30 @@
-import getBraveNewsController from ".";
+// Copyright (c) 2022 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at https://mozilla.org/MPL/2.0/.
+import getBraveNewsController from '.'
 import {
   BraveNewsControllerRemote,
   FeedListenerInterface,
   FeedListenerReceiver
-} from "../../../../../out/Component/gen/brave/components/brave_today/common/brave_news.mojom.m";
+} from '../../../../../out/Component/gen/brave/components/brave_today/common/brave_news.mojom.m'
 
 export const addFeedListener = (listener: (feedHash: string) => void) =>
   new (class implements FeedListenerInterface {
-    #receiver = new FeedListenerReceiver(this);
-    #controller: BraveNewsControllerRemote;
+    #receiver = new FeedListenerReceiver(this)
+    #controller: BraveNewsControllerRemote
 
-    constructor() {
-      this.#controller = getBraveNewsController();
+    constructor () {
+      this.#controller = getBraveNewsController()
 
-      if (process.env.NODE_ENV !== "test") {
+      if (process.env.NODE_ENV !== 'test') {
         this.#controller.addFeedListener(
           this.#receiver.$.bindNewPipeAndPassRemote()
-        );
+        )
       }
     }
 
-    onUpdateAvailable(feedHash: string): void {
-      listener(feedHash);
+    onUpdateAvailable (feedHash: string): void {
+      listener(feedHash)
     }
-  })();
+  })()
--- components/brave_new_tab_ui/async/today.ts
+++ components/brave_new_tab_ui/async/today.ts
@@ -3,106 +3,106 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // you can obtain one at https://mozilla.org/MPL/2.0/.
 
-import AsyncActionHandler from "../../common/AsyncActionHandler";
-import * as Actions from "../actions/today_actions";
-import { ApplicationState } from "../reducers";
-import { saveIsBraveTodayOptedIn } from "../api/preferences";
-import getBraveNewsController, * as BraveNews from "../api/brave_news";
-import store from "../store";
-import { addFeedListener } from "../api/brave_news/feedListener";
+import AsyncActionHandler from '../../common/AsyncActionHandler'
+import * as Actions from '../actions/today_actions'
+import { ApplicationState } from '../reducers'
+import { saveIsBraveTodayOptedIn } from '../api/preferences'
+import getBraveNewsController, * as BraveNews from '../api/brave_news'
+import store from '../store'
+import { addFeedListener } from '../api/brave_news/feedListener'
 
 addFeedListener((hash) => {
   const current = store.getState().today.feed?.hash
   store.dispatch(Actions.isUpdateAvailable({ isUpdateAvailable: current !== hash }))
-});
+})
 
-function storeInHistoryState(data: Object) {
+function storeInHistoryState (data: Object) {
   const oldHistoryState =
-    typeof history.state === "object" ? history.state : {};
-  const newHistoryState = { ...oldHistoryState, ...data };
-  history.pushState(newHistoryState, document.title);
+    typeof history.state === 'object' ? history.state : {}
+  const newHistoryState = { ...oldHistoryState, ...data }
+  history.pushState(newHistoryState, document.title)
 }
 
-const handler = new AsyncActionHandler();
+const handler = new AsyncActionHandler()
 
 handler.on(Actions.interactionBegin.getType(), async () => {
-  console.debug("Brave News: Marking actual interaction begin");
-  getBraveNewsController().onInteractionSessionStarted();
-});
+  console.debug('Brave News: Marking actual interaction begin')
+  getBraveNewsController().onInteractionSessionStarted()
+})
 
 handler.on<Actions.RefreshPayload>(
   Actions.refresh.getType(),
   async (store, payload) => {
     if (payload && payload.isFirstInteraction) {
-      console.debug("Brave News: Marking actual interaction begin");
-      getBraveNewsController().onInteractionSessionStarted();
+      console.debug('Brave News: Marking actual interaction begin')
+      getBraveNewsController().onInteractionSessionStarted()
     }
     try {
-      console.debug("Brave News: Getting data...");
+      console.debug('Brave News: Getting data...')
       const [{ feed }, { publishers }] = await Promise.all([
         getBraveNewsController().getFeed(),
         getBraveNewsController().getPublishers()
-      ]);
-      console.debug("Brave News: ...data received.");
-      store.dispatch(Actions.dataReceived({ feed, publishers }));
+      ])
+      console.debug('Brave News: ...data received.')
+      store.dispatch(Actions.dataReceived({ feed, publishers }))
     } catch (e) {
-      console.error("error receiving feed", e);
-      store.dispatch(Actions.errorGettingDataFromBackground(e));
+      console.error('error receiving feed', e)
+      store.dispatch(Actions.errorGettingDataFromBackground(e))
     }
   }
-);
+)
 
 handler.on(Actions.optIn.getType(), async () => {
-  saveIsBraveTodayOptedIn(true);
-});
+  saveIsBraveTodayOptedIn(true)
+})
 
 handler.on(Actions.ensureSettingsData.getType(), async (store) => {
-  const state = store.getState() as ApplicationState;
+  const state = store.getState() as ApplicationState
   if (state.today.publishers && Object.keys(state.today.publishers).length) {
-    return;
+    return
   }
-  const { publishers } = await getBraveNewsController().getPublishers();
-  store.dispatch(Actions.dataReceived({ publishers }));
-});
+  const { publishers } = await getBraveNewsController().getPublishers()
+  store.dispatch(Actions.dataReceived({ publishers }))
+})
 
 handler.on<Actions.ReadFeedItemPayload>(
   Actions.readFeedItem.getType(),
   async (store, payload) => {
-    const state = store.getState() as ApplicationState;
+    const state = store.getState() as ApplicationState
     getBraveNewsController().onSessionCardVisitsCountChanged(
       state.today.cardsVisited
-    );
+    )
     if (payload.isPromoted) {
-      const promotedArticle = payload.item.promotedArticle;
+      const promotedArticle = payload.item.promotedArticle
       if (!promotedArticle) {
         console.error(
-          "Brave News: readFeedItem payload with invalid promoted article",
+          'Brave News: readFeedItem payload with invalid promoted article',
           payload
-        );
-        return;
+        )
+        return
       }
       if (!payload.promotedUUID) {
         console.error(
-          "Brave News: invalid promotedUUID for readFeedItem",
+          'Brave News: invalid promotedUUID for readFeedItem',
           payload
-        );
-        return;
+        )
+        return
       }
       getBraveNewsController().onPromotedItemVisit(
         payload.promotedUUID,
         promotedArticle.creativeInstanceId
-      );
+      )
     }
     const data =
       payload.item.article?.data ||
       payload.item.promotedArticle?.data ||
-      payload.item.deal?.data;
+      payload.item.deal?.data
     if (!data) {
       console.error(
-        "Brave News: readFeedItem payload item not present",
+        'Brave News: readFeedItem payload item not present',
         payload
-      );
-      return;
+      )
+      return
     }
     if (!payload.openInNewTab) {
       // remember article so we can scroll to it on "back" navigation
@@ -112,108 +112,108 @@ handler.on<Actions.ReadFeedItemPayload>(
         todayArticle: data,
         todayPageIndex: state.today.currentPageIndex,
         todayCardsVisited: state.today.cardsVisited
-      });
+      })
       // visit article url
-      window.location.href = data.url.url;
+      window.location.href = data.url.url
     } else {
-      window.open(data.url.url, "_blank");
+      window.open(data.url.url, '_blank')
     }
   }
-);
+)
 
 handler.on<Actions.PromotedItemViewedPayload>(
   Actions.promotedItemViewed.getType(),
   async (store, payload) => {
     if (!payload.item.promotedArticle) {
       console.error(
-        "Brave News: promotedItemViewed invalid promoted article",
+        'Brave News: promotedItemViewed invalid promoted article',
         payload
-      );
-      return;
+      )
+      return
     }
     getBraveNewsController().onPromotedItemView(
       payload.uuid,
       payload.item.promotedArticle.creativeInstanceId
-    );
+    )
   }
-);
+)
 
 handler.on<number>(
   Actions.feedItemViewedCountChanged.getType(),
   async (store, payload) => {
-    const state = store.getState() as ApplicationState;
+    const state = store.getState() as ApplicationState
     getBraveNewsController().onSessionCardViewsCountChanged(
       state.today.cardsViewed
-    );
+    )
   }
-);
+)
 
 handler.on<Actions.RemoveDirectFeedPayload>(
   Actions.removeDirectFeed.getType(),
   async (store, payload) => {
-    getBraveNewsController().removeDirectFeed(payload.directFeed.publisherId);
+    getBraveNewsController().removeDirectFeed(payload.directFeed.publisherId)
     window.setTimeout(() => {
-      store.dispatch(Actions.checkForUpdate());
-    }, 3000);
+      store.dispatch(Actions.checkForUpdate())
+    }, 3000)
   }
-);
+)
 
 handler.on<Actions.SetPublisherPrefPayload>(
   Actions.setPublisherPref.getType(),
   async (store, payload) => {
-    const { publisherId, enabled } = payload;
+    const { publisherId, enabled } = payload
     let userStatus =
       enabled === null
         ? BraveNews.UserEnabled.NOT_MODIFIED
         : enabled
         ? BraveNews.UserEnabled.ENABLED
-        : BraveNews.UserEnabled.DISABLED;
-    getBraveNewsController().setPublisherPref(publisherId, userStatus);
+        : BraveNews.UserEnabled.DISABLED
+    getBraveNewsController().setPublisherPref(publisherId, userStatus)
     // Refreshing of content after prefs changed is throttled, so wait
     // a while before seeing if we have new content yet.
     // This doesn't have to be exact since we often check for update when
     // opening or scrolling through the feed.
     window.setTimeout(() => {
-      store.dispatch(Actions.checkForUpdate());
-    }, 3000);
+      store.dispatch(Actions.checkForUpdate())
+    }, 3000)
   }
-);
+)
 
 handler.on(Actions.checkForUpdate.getType(), async function (store) {
-  const state = store.getState() as ApplicationState;
+  const state = store.getState() as ApplicationState
   // Detect if we did not get any data successfully and therefore we can try again
   // to fetch data
   if (!state.today.feed) {
-    store.dispatch(Actions.isUpdateAvailable({ isUpdateAvailable: true }));
-    return;
+    store.dispatch(Actions.isUpdateAvailable({ isUpdateAvailable: true }))
+    return
   }
-  const hash = state.today.feed.hash;
+  const hash = state.today.feed.hash
   const isUpdateAvailable: { isUpdateAvailable: boolean } =
-    await getBraveNewsController().isFeedUpdateAvailable(hash);
-  store.dispatch(Actions.isUpdateAvailable(isUpdateAvailable));
-});
+    await getBraveNewsController().isFeedUpdateAvailable(hash)
+  store.dispatch(Actions.isUpdateAvailable(isUpdateAvailable))
+})
 
 handler.on(Actions.resetTodayPrefsToDefault.getType(), async function (store) {
-  getBraveNewsController().clearPrefs();
-  const { publishers } = await getBraveNewsController().getPublishers();
-  store.dispatch(Actions.dataReceived({ publishers }));
-  store.dispatch(Actions.checkForUpdate());
-});
+  getBraveNewsController().clearPrefs()
+  const { publishers } = await getBraveNewsController().getPublishers()
+  store.dispatch(Actions.dataReceived({ publishers }))
+  store.dispatch(Actions.checkForUpdate())
+})
 
 handler.on(Actions.anotherPageNeeded.getType(), async function (store) {
-  store.dispatch(Actions.checkForUpdate());
-});
+  store.dispatch(Actions.checkForUpdate())
+})
 
 handler.on<Actions.VisitDisplayAdPayload>(
   Actions.visitDisplayAd.getType(),
   async function (store, payload) {
-    const state = store.getState() as ApplicationState;
-    const todayPageIndex = state.today.currentPageIndex;
+    const state = store.getState() as ApplicationState
+    const todayPageIndex = state.today.currentPageIndex
     getBraveNewsController().onDisplayAdVisit(
       payload.ad.uuid,
       payload.ad.creativeInstanceId
-    );
-    const destinationUrl = payload.ad.targetUrl.url;
+    )
+    const destinationUrl = payload.ad.targetUrl.url
     if (!payload.openInNewTab) {
       // Remember display ad location so we can scroll to it on "back" navigation
       // We remember position and not ad ID since it can be a different ad on
@@ -224,14 +224,14 @@ handler.on<Actions.VisitDisplayAdPayload>(
         todayAdPosition: todayPageIndex,
         todayPageIndex,
         todayCardsVisited: state.today.cardsVisited
-      });
+      })
       // visit article url
-      window.location.href = destinationUrl;
+      window.location.href = destinationUrl
     } else {
-      window.open(destinationUrl, "_blank");
+      window.open(destinationUrl, '_blank')
     }
   }
-);
+)
 
 handler.on<Actions.DisplayAdViewedPayload>(
   Actions.displayAdViewed.getType(),
@@ -239,8 +239,8 @@ handler.on<Actions.DisplayAdViewedPayload>(
     getBraveNewsController().onDisplayAdView(
       item.ad.uuid,
       item.ad.creativeInstanceId
-    );
+    )
   }
-);
+)
 
-export default handler.middleware;
+export default handler.middleware
