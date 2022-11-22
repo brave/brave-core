34a51949d15ca5a25b39d4dc9e26ddfb40f111ca
brave-builds <devops@brave.com>
2022-11-22 16:14:23 +0000

    Uplift of #16052 (squashed) to beta

--- components/brave_vpn/resources/panel/components/error-panel/index.tsx
+++ components/brave_vpn/resources/panel/components/error-panel/index.tsx
@@ -17,7 +17,7 @@ function ErrorPanel (props: Props) {
   const currentRegion = useSelector(state => state.currentRegion)
 
   const handleTryAgain = () => {
-    dispatch(Actions.retryConnect())
+    dispatch(Actions.connect())
   }
 
   const handleChooseServer = () => {
