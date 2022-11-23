d97c30e669e6d3f6a474c440425754802b0851cf
sergei <spylogsster@gmail.com>
2022-11-23 16:01:39 +0300

    Change placeholder for shields in add site dialog

---
+++ browser/resources/settings/brave_add_site_dialog/brave_add_site_dialog_behavior.ts
@@ -0,0 +1,20 @@
+// @ts-nocheck
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * you can obtain one at https://mozilla.org/MPL/2.0/. */
+
+
+import {loadTimeData} from "../i18n_setup.js"
+
+const BraveAddSiteDialogBehaviorImpl = {
+  ready: function() {
+    const is_brave_shields = this.category === 'braveShields'
+    const resource_id = is_brave_shields ? 'braveShieldsExampleTemplate' : 'addSiteExceptionPlaceholder'
+    this.$.site.placeholder = loadTimeData.getString(resource_id)
+  },
+}
+
+export const BraveAddSiteDialogBehavior = [
+  BraveAddSiteDialogBehaviorImpl
+]
---
+++ browser/resources/settings/brave_overrides/add_site_dialog.ts
@@ -0,0 +1,15 @@
+// Copyright (c) 2022 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at https://mozilla.org/MPL/2.0/.
+
+
+import {RegisterPolymerComponentBehaviors} from 'chrome://resources/polymer_overriding.js'
+import {BraveAddSiteDialogBehavior} from '../brave_add_site_dialog/brave_add_site_dialog_behavior.js'
+
+RegisterPolymerComponentBehaviors({
+  'add-site-dialog': [
+    BraveAddSiteDialogBehavior
+  ]}
+)
+
