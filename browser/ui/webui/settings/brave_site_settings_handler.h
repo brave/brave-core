b5967ea926d9c0887f5a8a3ea25ed5ef8c3f1e60
sergei <spylogsster@gmail.com>
2022-11-24 13:07:45 +0300

    Subclass webui handler

---
+++ browser/ui/webui/settings/brave_site_settings_handler.h
@@ -0,0 +1,34 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_BROWSER_UI_WEBUI_SETTINGS_BRAVE_SITE_SETTINGS_HANDLER_H_
+#define BRAVE_BROWSER_UI_WEBUI_SETTINGS_BRAVE_SITE_SETTINGS_HANDLER_H_
+
+#include "chrome/browser/ui/webui/settings/site_settings_handler.h"
+
+namespace settings {
+
+class BraveSiteSettingsHandler : public SiteSettingsHandler {
+ public:
+  explicit BraveSiteSettingsHandler(Profile* profile);
+
+  BraveSiteSettingsHandler(const BraveSiteSettingsHandler&) = delete;
+  BraveSiteSettingsHandler& operator=(const BraveSiteSettingsHandler&) = delete;
+
+  ~BraveSiteSettingsHandler() override;
+
+  // SettingsPageUIHandler:
+  void RegisterMessages() override;
+
+  // Returns whether the pattern is valid given the type.
+  void HandleIsPatternValidForType(const base::Value::List& args);
+
+ private:
+  friend class TestBraveSiteSettingsHandlerUnittest;
+};
+
+}  // namespace settings
+
+#endif  // BRAVE_BROWSER_UI_WEBUI_SETTINGS_BRAVE_SITE_SETTINGS_HANDLER_H_
