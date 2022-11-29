ceea2098295e2d5964568c418fbf81c0ca11c308
nvonpentz <12549658+nvonpentz@users.noreply.github.com>
2022-11-29 12:37:50 -0500

    Fix header import for solana_data_decoder_utils.cc

--- components/brave_wallet/browser/solana_data_decoder_utils.cc
+++ components/brave_wallet/browser/solana_data_decoder_utils.cc
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "brave/components/brave_wallet/browser/solana_instruction_data_decoder.h"
+#include "brave/components/brave_wallet/browser/solana_data_decoder_utils.h"
 
 #include <tuple>
 #include <utility>
