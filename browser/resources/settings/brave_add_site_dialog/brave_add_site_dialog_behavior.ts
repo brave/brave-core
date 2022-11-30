d24af248f771f01bda37b5e1aac1c84df9880df2
Sergey P <spylogsster@gmail.com>
2022-11-30 17:24:36 +0300

    Fixed review comments

--- browser/resources/settings/brave_add_site_dialog/brave_add_site_dialog_behavior.ts
+++ browser/resources/settings/brave_add_site_dialog/brave_add_site_dialog_behavior.ts
@@ -1,9 +1,8 @@
 // @ts-nocheck
-/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * you can obtain one at https://mozilla.org/MPL/2.0/. */
-
 
 import {loadTimeData} from "../i18n_setup.js"
 
@@ -11,6 +10,10 @@ const BraveAddSiteDialogBehaviorImpl = {
   ready: function() {
     const is_brave_shields = this.category === 'braveShields'
     const resource_id = is_brave_shields ? 'braveShieldsExampleTemplate' : 'addSiteExceptionPlaceholder'
+    if (!this.$.site) {
+      console.error('[Brave Settings Overrides] site input field not found')
+      return
+    }
     this.$.site.placeholder = loadTimeData.getString(resource_id)
   },
 }
--- browser/resources/settings/brave_overrides/add_site_dialog.ts
+++ browser/resources/settings/brave_overrides/add_site_dialog.ts
@@ -3,7 +3,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // you can obtain one at https://mozilla.org/MPL/2.0/.
 
-
 import {RegisterPolymerComponentBehaviors} from 'chrome://resources/polymer_overriding.js'
 import {BraveAddSiteDialogBehavior} from '../brave_add_site_dialog/brave_add_site_dialog_behavior.js'
 
