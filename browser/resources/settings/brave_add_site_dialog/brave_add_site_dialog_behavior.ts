23b6133e1d8e6535ccd194be0f230c52ae934e64
sergei <spylogsster@gmail.com>
2022-12-01 16:34:17 +0300

    Rework webui injection

--- browser/resources/settings/brave_add_site_dialog/brave_add_site_dialog_behavior.ts
+++ browser/resources/settings/brave_add_site_dialog/brave_add_site_dialog_behavior.ts
@@ -1,4 +1,3 @@
-// @ts-nocheck
 /* Copyright (c) 2022 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
@@ -6,8 +5,12 @@
 
 import {loadTimeData} from "../i18n_setup.js"
 
-const BraveAddSiteDialogBehaviorImpl = {
-  ready: function() {
+import {WebUIListenerMixin} from 'chrome://resources/cr_elements/web_ui_listener_mixin.js';
+import {AddSiteDialogElement} from '../site_settings/add_site_dialog.js'
+
+const BaseElement = WebUIListenerMixin(AddSiteDialogElement)
+export class BraveAddSiteDialogElement extends BaseElement {
+  override ready() {
     const is_brave_shields = this.category === 'braveShields'
     const resource_id = is_brave_shields ? 'braveShieldsExampleTemplate' : 'addSiteExceptionPlaceholder'
     if (!this.$.site) {
@@ -15,9 +18,5 @@ const BraveAddSiteDialogBehaviorImpl = {
       return
     }
     this.$.site.placeholder = loadTimeData.getString(resource_id)
-  },
+  }
 }
-
-export const BraveAddSiteDialogBehavior = [
-  BraveAddSiteDialogBehaviorImpl
-]
--- browser/resources/settings/brave_overrides/add_site_dialog.ts
+++ browser/resources/settings/brave_overrides/add_site_dialog.ts
@@ -3,12 +3,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // you can obtain one at https://mozilla.org/MPL/2.0/.
 
-import {RegisterPolymerComponentBehaviors} from 'chrome://resources/polymer_overriding.js'
-import {BraveAddSiteDialogBehavior} from '../brave_add_site_dialog/brave_add_site_dialog_behavior.js'
+import {BraveAddSiteDialogElement} from '../brave_add_site_dialog/brave_add_site_dialog_behavior.js'
+import {RegisterPolymerComponentReplacement} from 'chrome://resources/polymer_overriding.js'
 
-RegisterPolymerComponentBehaviors({
-  'add-site-dialog': [
-    BraveAddSiteDialogBehavior
-  ]}
+RegisterPolymerComponentReplacement(
+  'add-site-dialog',
+  BraveAddSiteDialogElement
 )
-
