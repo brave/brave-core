c3f1cce69a16fe698135757ba41ed5804c4a7a34
sergei <spylogsster@gmail.com>
2022-12-06 11:06:59 +0300

    Rename brave_add_site_dialog_behavior.ts -> brave_add_site_dialog.ts

--- browser/resources/settings/brave_overrides/add_site_dialog.ts
+++ browser/resources/settings/brave_overrides/add_site_dialog.ts
@@ -3,7 +3,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import {BraveAddSiteDialogElement} from '../brave_add_site_dialog/brave_add_site_dialog_behavior.js'
+import {BraveAddSiteDialogElement} from '../brave_add_site_dialog/brave_add_site_dialog.js'
 import {RegisterPolymerComponentReplacement, RegisterPolymerTemplateModifications} from 'chrome://resources/polymer_overriding.js'
 
 RegisterPolymerComponentReplacement(
