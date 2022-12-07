09f066ce279f7c9c8ab756b68f49e6bbe56e18d4
Jay Harris <jay.harris@outlook.co.nz>
2022-12-06 18:01:54 -0800

    Hide page action icons when editing the Omnibox (#16243)

--- browser/ui/views/location_bar/brave_location_bar_view.cc
+++ browser/ui/views/location_bar/brave_location_bar_view.cc
@@ -127,13 +127,15 @@ void BraveLocationBarView::Update(content::WebContents* contents) {
   if (brave_actions_) {
     brave_actions_->Update();
   }
+
+  auto show_page_actions = !ShouldHidePageActionIcons();
 #if BUILDFLAG(ENABLE_TOR)
   if (onion_location_view_)
-    onion_location_view_->Update(contents);
+    onion_location_view_->Update(contents, show_page_actions);
 #endif
 #if BUILDFLAG(ENABLE_IPFS)
   if (ipfs_location_view_)
-    ipfs_location_view_->Update(contents);
+    ipfs_location_view_->Update(contents, show_page_actions);
 #endif
 
   LocationBarView::Update(contents);
@@ -161,21 +163,21 @@ ui::ImageModel BraveLocationBarView::GetLocationIcon(
 }
 
 void BraveLocationBarView::OnChanged() {
+  auto hide_page_actions = ShouldHidePageActionIcons();
   if (brave_actions_) {
-    // Do not show actions whilst omnibar is open or url is being edited
-    const bool should_hide =
-        ShouldHidePageActionIcons() && !omnibox_view_->GetText().empty();
-    brave_actions_->SetShouldHide(should_hide);
+    brave_actions_->SetShouldHide(hide_page_actions);
   }
 #if BUILDFLAG(ENABLE_TOR)
   if (onion_location_view_)
     onion_location_view_->Update(
-        browser_->tab_strip_model()->GetActiveWebContents());
+        browser_->tab_strip_model()->GetActiveWebContents(),
+        !hide_page_actions);
 #endif
 #if BUILDFLAG(ENABLE_IPFS)
   if (ipfs_location_view_)
     ipfs_location_view_->Update(
-        browser_->tab_strip_model()->GetActiveWebContents());
+        browser_->tab_strip_model()->GetActiveWebContents(),
+        !hide_page_actions);
 #endif
 
   // OnChanged calls Layout
