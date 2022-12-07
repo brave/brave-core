938628e5ad2f7a5e5bdfbc5aa01a7198da199925
Darnell Andries <dandries@brave.com>
2022-12-07 13:26:05 -0800

    Update copyright notices

--- components/misc_metrics/menu_metrics.h
+++ components/misc_metrics/menu_metrics.h
@@ -1,13 +1,10 @@
 /* Copyright (c) 2022 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #ifndef BRAVE_COMPONENTS_MISC_METRICS_MENU_METRICS_H_
 #define BRAVE_COMPONENTS_MISC_METRICS_MENU_METRICS_H_
-
-#include <memory>
-#include <vector>
 
 #include "base/containers/flat_map.h"
 #include "base/task/cancelable_task_tracker.h"
