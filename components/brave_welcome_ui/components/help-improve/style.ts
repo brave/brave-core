5071645d826f2e2dc0a89944e0410d43b28fb310
Taher <8665427+nullhook@users.noreply.github.com>
2022-12-08 22:45:26 -0800

    Optimized the new Welcome UI and style fixes (#16164)
    * Converted assets to webp format
    * Lazy loading
    * Style fixes
    * Added background image loading handle

--- components/brave_welcome_ui/components/help-improve/style.ts
+++ components/brave_welcome_ui/components/help-improve/style.ts
@@ -64,10 +64,10 @@ export const Grid = styled.div`
     width: 22px;
     height: 22px;
     border-radius: 4px;
-    border: 1px solid ${(p) => p.theme.color.interactive08};
+    border: 1px solid #AEB1C2;
 
     &:checked {
-      accent-color: ${(p) => p.theme.color.interactive05};
+      accent-color: #4C54D2;
     }
   }
 `
