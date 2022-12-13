98d716603516674929f36f7d947a40db161cf659
Pete Miller <miller.pete@gmail.com>
2022-12-12 17:29:40 -0800

    Perform same crypto.createHash node v18 workaround that we have in the build webpack v4 config, in the Storybook webpack v4 config

--- components/webpack/webpack.config.js
+++ components/webpack/webpack.config.js
@@ -11,10 +11,11 @@ const pathMap = require('./path-map')
 
 const tsConfigPath = path.join(process.env.ROOT_GEN_DIR, 'tsconfig-webpack.json')
 
-// OpenSSL 3 no longer supports the insecure md4 hash, but webpack < 6
+// OpenSSL 3 no longer supports the insecure md4 hash, but webpack < 5.54.0
 // hardcodes it. Work around by substituting a supported algorithm.
 // https://github.com/webpack/webpack/issues/13572
 // https://github.com/webpack/webpack/issues/14532
+// TODO(petemill): Remove this patching when webpack > 5.54.0 is being used.
 const crypto = require("crypto");
 const crypto_orig_createHash = crypto.createHash;
 crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
