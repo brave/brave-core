570b21bb268c94129c947a7409ef0d7f3cd3b4d1
Sergey P <spylogsster@gmail.com>
2022-12-13 13:00:00 +0300

    Move kLocalizedStrings -> brave_vpn_localized_string_provider

---
+++ browser/ui/webui/brave_vpn/brave_vpn_localized_string_provider.h
@@ -0,0 +1,20 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_BROWSER_UI_WEBUI_BRAVE_VPN_BRAVE_VPN_LOCALIZED_STRING_PROVIDER_H_
+#define BRAVE_BROWSER_UI_WEBUI_BRAVE_VPN_BRAVE_VPN_LOCALIZED_STRING_PROVIDER_H_
+
+#include "base/no_destructor.h"
+
+namespace content {
+class WebUIDataSource;
+}
+namespace brave_vpn {
+
+void AddLocalizedStrings(content::WebUIDataSource* html_source);
+
+}  // namespace brave_vpn
+
+#endif  // BRAVE_BROWSER_UI_WEBUI_BRAVE_VPN_BRAVE_VPN_LOCALIZED_STRING_PROVIDER_H_
