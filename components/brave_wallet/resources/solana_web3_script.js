afe39b712f0a8a50f4789a108788fad1c5e4da72
Anthony Tseng <darkdh@gmail.com>
2022-12-13 09:57:18 -0800

    Move auto generated script version into comment

--- components/brave_wallet/resources/solana_web3_script.js
+++ components/brave_wallet/resources/solana_web3_script.js
@@ -3,6 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+/* Auto generated from @solana/web3.js 1.47.1 index.iife.min.js */
+
 var solanaWeb3 = $(function (exports) {
   "use strict";
 
