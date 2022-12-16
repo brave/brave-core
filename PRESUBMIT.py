def552f0291f4ac4a63e5040c017c05c37571470
Aleksey Khoroshilov <akhoroshilov@brave.com>
2022-12-16 23:26:48 +0700

    Integrate prettier with presubmit format checks.

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -7,15 +7,31 @@ import copy
 import os
 import sys
 
-import import_inline
+import brave_node
 import chromium_presubmit_overrides
 import git_cl
+import import_inline
 import override_utils
 
 USE_PYTHON3 = True
 PRESUBMIT_VERSION = '2.0.0'
 
 # pylint: disable=line-too-long,protected-access
+
+
+# Use a custom git diff call instead of input_api.AffectedSourceFiles(). This
+# way we get non-staged files similar to how "git cl format" works.
+def _GetDiffFiles(input_api):
+    upstream_branch = input_api.change.UpstreamBranch()
+    if not upstream_branch:
+        cl = git_cl.Changelist()
+        upstream_branch = cl.GetUpstreamBranch()
+    upstream_commit = git_cl.RunGit(['merge-base', 'HEAD', upstream_branch])
+    upstream_commit = upstream_commit.strip()
+    changed_files_cmd = git_cl.BuildGitDiffCmd('--name-only', upstream_commit,
+                                               [])
+    diff_output = git_cl.RunGit(changed_files_cmd)
+    return diff_output.splitlines()
 
 
 # Adds support for chromium_presubmit_config.json5 and some helpers.
@@ -53,6 +69,36 @@ def CheckPatchFormatted(input_api, output_api):
                                                   suppress_stderr=True)
 
     is_format_required = git_cl_format_code == 2
+
+    if not is_format_required or input_api.PRESUBMIT_FIX:
+        # Use Prettier to format other file types.
+        prettier_types = (
+            '.js',
+            '.ts',
+            '.tsx',
+        )
+
+        files_to_format = [
+            f for f in _GetDiffFiles(input_api)
+            if os.path.isfile(f) and f.endswith(prettier_types)
+        ]
+
+        node_args = [
+            brave_node.PathInNodeModules('prettier', 'bin-prettier'),
+            '--write' if input_api.PRESUBMIT_FIX else '--check',
+        ]
+
+        files_per_command = 10 if input_api.is_windows else 1000
+        for i in range(0, len(files_to_format), files_per_command):
+            args = node_args + files_to_format[i:i + files_per_command]
+            try:
+                brave_node.RunNode(args)
+            except RuntimeError as err:
+                if 'Forgot to run Prettier?' in str(err):
+                    is_format_required = True
+                    break
+                # Raise on unexpected output. Could be node or prettier issues.
+                raise
 
     if is_format_required:
         if input_api.PRESUBMIT_FIX:
