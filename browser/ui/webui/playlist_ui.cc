4fca1c93c120e46b5650a9fd9659dc87d4b1f9d2
deeppandya <deeppandya91@gmail.com>
2022-12-20 01:13:19 -0500

    Resolve license error
    Add missing import

--- browser/ui/webui/playlist_ui.cc
+++ browser/ui/webui/playlist_ui.cc
@@ -1,7 +1,6 @@
 /* Copyright (c) 2021 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
-#include "chrome/browser/profiles/profile.h"
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/browser/ui/webui/playlist_ui.h"
