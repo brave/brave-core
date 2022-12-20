d32847d242de74eef5dc2b52cb4a1bfeddf07774
zenparsing <zenparsing@gmail.com>
2022-12-20 11:51:43 -0500

    Add box shadow to Rewards page promo cards

--- components/brave_rewards/resources/page/components/sidebar_promotion_panel.style.ts
+++ components/brave_rewards/resources/page/components/sidebar_promotion_panel.style.ts
@@ -29,6 +29,9 @@ export const rewardsTourPromo = styled.div``
 export const promotion = styled.div`
   color: #000;
   background: #fff;
+  box-shadow:
+    0px 0px 1px rgba(0, 0, 0, 0.11),
+    0px 0.5px 1.5px rgba(0, 0, 0, 0.1);
   border-radius: 8px;
   min-height: 157px;
   cursor: pointer;
