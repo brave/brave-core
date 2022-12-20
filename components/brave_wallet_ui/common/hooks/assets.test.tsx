e94033f19c652373407174c1b94eb508b55fcaa9
William Muli <48117473+muliswilliam@users.noreply.github.com>
2022-12-20 11:22:28 +0300

    feat(wallet): Add Transak on-ramp provider (#16406)
    * Add Transak on-ramp provider
    
    * Add new buy networks
    
    * fix aurora token
    
    * chore: format

--- components/brave_wallet_ui/common/hooks/assets.test.tsx
+++ components/brave_wallet_ui/common/hooks/assets.test.tsx
@@ -8,7 +8,7 @@ import { TextEncoder, TextDecoder } from 'util'
 // @ts-expect-error
 global.TextDecoder = TextDecoder
 global.TextEncoder = TextEncoder
-import { renderHook, act } from '@testing-library/react-hooks'
+import { renderHook } from '@testing-library/react-hooks'
 import {
   mockAccount,
   mockAssetPrices,
@@ -16,7 +16,6 @@ import {
 } from '../constants/mocks'
 import useAssets from './assets'
 import { WalletAccountType } from '../../constants/types'
-import { setMockedBuyAssets } from '../async/__mocks__/lib'
 import * as MockedLib from '../async/__mocks__/lib'
 import { LibContext } from '../context/lib.context'
 import { mockWalletState } from '../../stories/mock-data/mock-wallet-state'
@@ -56,9 +55,8 @@ const renderHookOptionsWithCustomStore = (store: any) => ({
 })
 
 describe('useAssets hook', () => {
-  it('Selected account has balances, should return expectedResult', async () => {
-    setMockedBuyAssets(mockVisibleList)
-    const { result, waitForNextUpdate } = renderHook(
+  it('Selected account has balances, should return expectedResult', () => {
+    const { result } = renderHook(
       () => useAssets(),
       renderHookOptionsWithCustomStore(
         createMockStore({
@@ -74,14 +72,11 @@ describe('useAssets hook', () => {
         })
       )
     )
-    await act(async () => {
-      await waitForNextUpdate()
-    })
     expect(result.current.panelUserAssetList).toEqual(mockVisibleList)
   })
 
-  it('should return empty array for panelUserAssetList if visible assets is empty', async () => {
-    const { result, waitForNextUpdate } = renderHook(
+  it('should return empty array for panelUserAssetList if visible assets is empty', () => {
+    const { result } = renderHook(
       () => useAssets(),
       renderHookOptionsWithCustomStore(
         createMockStore({
@@ -97,9 +92,6 @@ describe('useAssets hook', () => {
         })
       )
     )
-    await act(async () => {
-      await waitForNextUpdate()
-    })
     expect(result.current.panelUserAssetList).toEqual([])
   })
 })
--- components/brave_wallet_ui/stories/wallet-extension-panels.tsx
+++ components/brave_wallet_ui/stories/wallet-extension-panels.tsx
@@ -82,6 +82,7 @@ import { mockPageState } from './mock-data/mock-page-state'
 import { mockWalletState } from './mock-data/mock-wallet-state'
 import { mockSendCryptoState } from './mock-data/send-crypto-state'
 import { mockUserAccounts } from './mock-data/user-accounts'
+import { BuyOptions } from '../options/buy-with-options'
 
 export default {
   title: 'Wallet/Extension/Panels',
@@ -600,6 +601,7 @@ export const _ConnectedPanel = (args: { locked: boolean }) => {
   const [, setSelectedAsset] = React.useState<BraveWallet.BlockchainToken>(mockBasicAttentionToken)
   const [showSelectAsset, setShowSelectAsset] = React.useState<boolean>(false)
   const [selectedTransaction, setSelectedTransaction] = React.useState<SerializableTransactionInfo | undefined>(transactionList[1][0])
+  const [buyAmount, setBuyAmount] = React.useState<string>('')
 
   const onChangeSendView = (view: BuySendSwapViewTypes) => {
     if (view === 'assets') {
@@ -711,6 +713,14 @@ export const _ConnectedPanel = (args: { locked: boolean }) => {
     navigateTo('currencies')
   }, [])
 
+  const onChangeBuyAmount = React.useCallback((amount: string) => {
+    setBuyAmount(amount)
+  }, [])
+
+  const onOpenBuyAssetLink = React.useCallback(() => {
+    console.log('Open buy asset link')
+  }, [])
+
   return (
     <WalletPanelStory>
       <StyledExtensionWrapper>
@@ -800,6 +810,11 @@ export const _ConnectedPanel = (args: { locked: boolean }) => {
                           onChangeBuyView={onChangeSendView}
                           selectedAsset={selectedWyreAsset}
                           onShowCurrencySelection={onShowCurrencySelection}
+                          isSelectedNetworkSupported={true}
+                          buyAmount={buyAmount}
+                          buyOptions={BuyOptions}
+                          onChangeBuyAmount={onChangeBuyAmount}
+                          openBuyAssetLink={onOpenBuyAssetLink}
                         />
                       }
                       {selectedPanel === 'sitePermissions' &&
