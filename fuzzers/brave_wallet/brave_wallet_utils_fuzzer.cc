e22eb6691d71495b641de0a9524eb57231e48688
boocmp <boocmp@yandex.ru>
2022-12-26 20:32:49 +0700

    Added brave_wallet_utils_fuzzer.

---
+++ fuzzers/brave_wallet/brave_wallet_utils_fuzzer.cc
@@ -0,0 +1,38 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "brave/components/brave_wallet/browser/brave_wallet_utils.h"
+
+#include <string>
+
+#include "base/logging.h"
+#include "fuzzer/FuzzedDataProvider.h"
+
+struct Environment {
+  Environment() { logging::SetMinLogLevel(logging::LOG_FATAL); }
+};
+
+// To run: ../out/Fuzzer/brave_wallet_utils_fuzzer
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  static Environment env;
+
+  FuzzedDataProvider data_provider(data, size);
+
+  const size_t array_size =
+      data_provider.ConsumeIntegralInRange<size_t>(0, 128);
+  const size_t offset = data_provider.ConsumeIntegralInRange<size_t>(0, 1024);
+  const std::string input = data_provider.ConsumeRemainingBytesAsString();
+
+  std::string output;
+  brave_wallet::EncodeString(input, &output);
+  brave_wallet::EncodeStringArray(std::vector<std::string>(array_size, input),
+                                  &output);
+
+  brave_wallet::DecodeString(offset, input, &output);
+  std::vector<std::string> array_output;
+  brave_wallet::DecodeStringArray(input, &array_output);
+
+  return 0;
+}
