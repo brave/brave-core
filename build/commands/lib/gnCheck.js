e73b5fdc00015642fb9a29d8c596c8b0046ab861
Michael Herrmann <michael@herrmann.io>
2023-01-04 09:03:47 +0100

    Let CI distinguish test compilation and test run failures (#15600)
    Previously, `npm run test --output` exited with a non-zero code on both
    compilation and test errors. This made it impossible to determine which
    of the two happened - and thus hard to decide whether to generate test
    reports. Now, the command exits with 0 on test failures. (Note that the
    behavior is unchanged when --output is not given. That is, in this case
    you still get non-zero exit codes for test failures.)

--- build/commands/lib/gnCheck.js
+++ build/commands/lib/gnCheck.js
@@ -1,7 +1,7 @@
 const config = require('../lib/config')
 const util = require('../lib/util')
 
-const gnCheck = (buildConfig = config.defaultBuildConfig, options) => {
+const gnCheck = (buildConfig = config.defaultBuildConfig, options = {}) => {
   config.buildConfig = buildConfig
   config.update(options)
   util.run('gn', ['check', config.outputDir, '//brave/*'], config.defaultOptions)
