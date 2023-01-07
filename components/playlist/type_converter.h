f0d4d56fd8ec195d2ed54b4c0d8540cdd5892d80
sangwoo.ko <sangwoo108@gmail.com>
2023-01-07 22:03:38 +0900

    Fix presubmit - copyright

--- components/playlist/type_converter.h
+++ components/playlist/type_converter.h
@@ -1,7 +1,7 @@
-/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #ifndef BRAVE_COMPONENTS_PLAYLIST_TYPE_CONVERTER_H_
 #define BRAVE_COMPONENTS_PLAYLIST_TYPE_CONVERTER_H_
@@ -12,8 +12,6 @@
 
 namespace playlist {
 
-// This class encapsulates how we convert data between mojo types and
-// base::Value. As base::Value's APIs are not that stable and quite confusing to
 bool IsItemValueMalformed(const base::Value::Dict& dict);
 
 // Converters between mojom::PlaylistItem and base::Value --------------------
