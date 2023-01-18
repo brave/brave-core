575df5cd1cc06674883ed83d456734197f12314b
Artem Samoilenko <artem@brave.com>
2023-01-18 12:25:31 -0500

    [Android] Fix for 'Automatically open when possible' option

--- browser/download/android/java/src/org/chromium/chrome/browser/download/BraveMimeUtils.java
+++ browser/download/android/java/src/org/chromium/chrome/browser/download/BraveMimeUtils.java
@@ -1,16 +1,22 @@
 /* Copyright (c) 2020 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 package org.chromium.chrome.browser.download;
 
 import org.chromium.base.BravePreferenceKeys;
 import org.chromium.base.ContextUtils;
+import org.chromium.base.annotations.CalledByNative;
 
 public class BraveMimeUtils {
+    @CalledByNative
     public static boolean canAutoOpenMimeType(String mimeType) {
-        return ContextUtils.getAppSharedPreferences().getBoolean(
-                BravePreferenceKeys.BRAVE_DOWNLOADS_AUTOMATICALLY_OPEN_WHEN_POSSIBLE, true);
+        if (ContextUtils.getAppSharedPreferences().getBoolean(
+                    BravePreferenceKeys.BRAVE_DOWNLOADS_AUTOMATICALLY_OPEN_WHEN_POSSIBLE, true)) {
+            return MimeUtils.canAutoOpenMimeType(mimeType);
+        }
+
+        return false;
     }
 }
