9fa665bad75012b064268c4bd2ea7cee6b31f9b4
Arthur Edelstein <aedelstein@brave.com>
2023-01-19 12:13:36 -0600

    Fix 27492 (broken facebook popup when screen farbling enabled) (#16571)

--- chromium_src/third_party/blink/renderer/core/frame/local_dom_window.h
+++ chromium_src/third_party/blink/renderer/core/frame/local_dom_window.h
@@ -36,6 +36,14 @@
   screenY_ChromiumImpl() const; \
   int screenTop
 
+#define resizeTo                                      \
+  resizeTo_ChromiumImpl(int width, int height) const; \
+  void resizeTo
+
+#define moveTo                             \
+  moveTo_ChromiumImpl(int x, int y) const; \
+  void moveTo
+
 #include "src/third_party/blink/renderer/core/frame/local_dom_window.h"
 
 #undef SetStorageKey
@@ -43,5 +51,7 @@
 #undef outerWidth
 #undef screenLeft
 #undef screenTop
+#undef resizeTo
+#undef moveTo
 
 #endif  // BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_CORE_FRAME_LOCAL_DOM_WINDOW_H_
