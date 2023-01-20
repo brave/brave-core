c923227138504ecf7087d4a0b8c57769640dc4c9
William Muli <willi.wambu@gmail.com>
2023-01-20 16:53:12 +0300

    Format

--- components/brave_wallet/browser/asset_ratio_service.cc
+++ components/brave_wallet/browser/asset_ratio_service.cc
@@ -238,7 +238,8 @@ void AssetRatioService::GetBuyUrlV1(mojom::OnRampProvider provider,
         net::AppendQueryParameter(transak_url, "fiatCurrency", currency_code);
     transak_url =
         net::AppendQueryParameter(transak_url, "defaultCryptoCurrency", symbol);
-    transak_url = net::AppendQueryParameter(transak_url, "defaultFiatAmount", amount);
+    transak_url =
+        net::AppendQueryParameter(transak_url, "defaultFiatAmount", amount);
     transak_url =
         net::AppendQueryParameter(transak_url, "walletAddress", address);
     transak_url = net::AppendQueryParameter(
