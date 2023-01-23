a7c0d5f4e3642e1ecf18c69ae18b8179edfa73d3
deeppandya <deeppandya91@gmail.com>
2023-01-23 13:36:59 -0500

    Update exoplayer deps to resolve deug builds

--- android/preprocess/xml/menu_main_menu_xml.py
+++ android/preprocess/xml/menu_main_menu_xml.py
@@ -2,7 +2,10 @@ import xml.etree.ElementTree as ET
 
 def _ProcessXML(root):
     # Register namespaces
-    ns = { 'android' : 'http://schemas.android.com/apk/res/android', 'app' : 'http://schemas.android.com/apk/res-auto' }    #NOSONAR
+    ns = {
+        'android': 'http://schemas.android.com/apk/res/android',
+        'app': 'http://schemas.android.com/apk/res-auto'
+    }  #NOSONAR
     for prefix, uri in ns.items():
         ET.register_namespace(prefix, uri)
 
@@ -13,7 +16,8 @@ def _ProcessXML(root):
     node = ET.fromstring(node_str, parser=ET.XMLParser(encoding="utf-8"))
 
     parent = root.find('group/[@android:id="@+id/PAGE_MENU"]', namespaces=ns)
-    child = parent.find('item/[@android:id="@+id/all_bookmarks_menu_id"]', namespaces=ns)
+    child = parent.find('item/[@android:id="@+id/all_bookmarks_menu_id"]',
+                        namespaces=ns)
     idx = list(parent).index(child)
     parent.insert(idx + 1, node)
 
@@ -32,8 +36,10 @@ def _ProcessXML(root):
     set_as_default_node_str = '<item xmlns:android="http://schemas.android.com/apk/res/android" '\
       'android:id="@+id/set_default_browser" ' \
       'android:title="@string/menu_set_default_browser" />'
-    set_as_default_node = ET.fromstring(set_as_default_node_str, parser=ET.XMLParser(encoding="utf-8"))
-    set_as_default_child = parent.find('item/[@android:id="@+id/preferences_id"]', namespaces=ns)
+    set_as_default_node = ET.fromstring(set_as_default_node_str,
+                                        parser=ET.XMLParser(encoding="utf-8"))
+    set_as_default_child = parent.find(
+        'item/[@android:id="@+id/preferences_id"]', namespaces=ns)
     set_as_default_idx = list(parent).index(set_as_default_child)
     parent.insert(set_as_default_idx + 1, set_as_default_node)
 
@@ -49,8 +55,10 @@ def _ProcessXML(root):
                   'android:checkable="true" /> '\
             '</menu> '\
             '</item>'
-    vpn_node = ET.fromstring(vpn_node_str, parser=ET.XMLParser(encoding="utf-8"))
-    vpn_child = parent.find('item/[@android:id="@+id/set_default_browser"]', namespaces=ns)
+    vpn_node = ET.fromstring(vpn_node_str,
+                             parser=ET.XMLParser(encoding="utf-8"))
+    vpn_child = parent.find('item/[@android:id="@+id/set_default_browser"]',
+                            namespaces=ns)
     vpn_idx = list(parent).index(vpn_child)
     parent.insert(vpn_idx + 1, vpn_node)
 
