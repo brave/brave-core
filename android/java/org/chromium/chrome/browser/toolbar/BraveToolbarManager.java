8fdcab185d392afbc75f3329c60a26a8d95a8e68
Artem Samoilenko <artem@brave.com>
2023-01-27 17:45:04 -0500

    [Android] Fix for crash or no action on page bookmark

--- android/java/org/chromium/chrome/browser/toolbar/BraveToolbarManager.java
+++ android/java/org/chromium/chrome/browser/toolbar/BraveToolbarManager.java
@@ -325,6 +325,7 @@ public class BraveToolbarManager extends ToolbarManager {
     }
 
     protected void updateBookmarkButtonStatus() {
+        if (mBookmarkModelSupplier == null) return;
         Tab currentTab = mLocationBarModel.getTab();
         BookmarkModel bridge = mBookmarkModelSupplier.get();
         boolean isBookmarked =
