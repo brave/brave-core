934c520220fc0829fc5f47f8b46a3282b0f0324a
Cepera <spylogsster@gmail.com>
2023-01-31 20:10:51 +0300

    Added Microsoft IE (#16800) uplift to 1.48.x (#16832)
    Added Microsoft IE (#16800)

--- components/brave_welcome_ui/state/component_types.ts
+++ components/brave_welcome_ui/state/component_types.ts
@@ -24,7 +24,8 @@ export enum BrowserType {
   Mozilla_Firefox = 'Mozilla Firefox',
   Microsoft_Edge = 'Microsoft Edge',
   Vivaldi = 'Vivaldi',
-  Opera = 'Opera'
+  Opera = 'Opera',
+  Microsoft_IE = 'Microsoft Internet Explorer'
 }
 
 export interface Scenes {
