2ca179f2bec10207790d3d69b0b35691bd7ee9d8
mkarolin <max@brave.com>
2023-02-04 18:15:05 -0500

    TabGroupUnderline::GetPath has been removed.
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/083810a10df234642c08c8223849371e6104b5de
    
    commit 083810a10df234642c08c8223849371e6104b5de
    Author: Shibalik Mohapatra <shibalik@chromium.org>
    Date:   Thu Jan 19 06:57:45 2023 +0000
    
        [Chrome Refresh 2023] Update the shape of the tab group underline to be a rounded rect
    
        Add TabGroupStyle to seperate out logic for ChromeRefresh23
    
        Bug: 1399944

--- browser/ui/views/tabs/brave_tab_group_underline.cc
+++ browser/ui/views/tabs/brave_tab_group_underline.cc
@@ -11,13 +11,10 @@
 #include "brave/browser/ui/views/tabs/brave_tab_group_header.h"
 #include "brave/browser/ui/views/tabs/vertical_tab_utils.h"
 #include "cc/paint/paint_flags.h"
+#include "chrome/browser/ui/views/tabs/tab_group_style.h"
 #include "chrome/browser/ui/views/tabs/tab_group_views.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/geometry/skia_conversions.h"
-
-namespace {
-constexpr int kStrokeThicknessForVerticalTabs = 4;
-}  // namespace
 
 BraveTabGroupUnderline::BraveTabGroupUnderline(
     TabGroupViews* tab_group_views,
@@ -33,28 +30,17 @@ void BraveTabGroupUnderline::UpdateBounds(const views::View* leading_view,
   if (!ShouldShowVerticalTabs() || !GetVisible())
     return;
 
-  // override bounds for vertical tabs mode.
-  gfx::RectF leading_bounds = gfx::RectF(leading_view->bounds());
-  ConvertRectToTarget(leading_view->parent(), parent(), &leading_bounds);
-  leading_bounds.Inset(gfx::InsetsF(GetInsetsForUnderline(leading_view)));
+  const gfx::Rect tab_group_underline_bounds =
+      CalculateTabGroupUnderlineBounds(this, leading_view, trailing_view);
 
-  gfx::RectF trailing_bounds = gfx::RectF(trailing_view->bounds());
-  ConvertRectToTarget(trailing_view->parent(), parent(), &trailing_bounds);
-  trailing_bounds.Inset(gfx::InsetsF(GetInsetsForUnderline(trailing_view)));
-  if (trailing_bounds.height()) {
-    trailing_bounds.set_height(trailing_bounds.height() +
-                               BraveTabGroupHeader::kPaddingForGroup);
-  }
-
-  gfx::Rect group_bounds = ToEnclosingRect(leading_bounds);
-  group_bounds.Union(ToEnclosingRect(trailing_bounds));
-  if (group_bounds.height() == 0) {
+  if (tab_group_underline_bounds.height() == 0) {
     SetVisible(false);
     return;
   }
 
-  SetBounds(0, group_bounds.y(), kStrokeThicknessForVerticalTabs,
-            group_bounds.height());
+  SetBounds(0, tab_group_underline_bounds.y(),
+            TabGroupStyle::kStrokeThicknessForVerticalTabs,
+            tab_group_underline_bounds.height());
 }
 
 gfx::Insets BraveTabGroupUnderline::GetInsetsForUnderline(
@@ -65,47 +51,33 @@ gfx::Insets BraveTabGroupUnderline::GetInsetsForUnderline(
   return {};
 }
 
-SkPath BraveTabGroupUnderline::GetPath() const {
-  if (!ShouldShowVerticalTabs())
-    return TabGroupUnderline::GetPath();
-
-  // In vertical tabs, underline is not actually "underline'. It's vertical line
-  // at the left side of the tab group. And it has half rounded corners.
-  //
-  // +   group header   | '+' is the underline.
-  // ++  tab 1          | Drawing starts from top-right and goes
-  // ++  tab 2          | counter-clockwise
-  // +   tab 3          |
-  //
-  SkPath path;
-  path.arcTo(/* rx = */ kStrokeThicknessForVerticalTabs,
-             /* ry = */ kStrokeThicknessForVerticalTabs,
-             /* angle = */ 180.f, SkPath::kSmall_ArcSize, SkPathDirection::kCW,
-             /* x = */ kStrokeThicknessForVerticalTabs,
-             /* y = */ kStrokeThicknessForVerticalTabs);
-  path.lineTo(kStrokeThicknessForVerticalTabs,
-              height() - kStrokeThicknessForVerticalTabs);
-  path.arcTo(/* rx = */ kStrokeThicknessForVerticalTabs,
-             /* ry = */ kStrokeThicknessForVerticalTabs,
-             /* angle = */ 180.f, SkPath::kSmall_ArcSize, SkPathDirection::kCW,
-             /* x = */ 0,
-             /* y = */ height());
-  path.close();
-
-  return path;
-}
-
-void BraveTabGroupUnderline::OnPaint(gfx::Canvas* canvas) {
+gfx::Rect BraveTabGroupUnderline::CalculateTabGroupUnderlineBounds(
+    const views::View* const underline_view,
+    const views::View* const leading_view,
+    const views::View* const trailing_view) const {
   if (!ShouldShowVerticalTabs()) {
-    TabGroupUnderline::OnPaint(canvas);
-    return;
+    return TabGroupUnderline::CalculateTabGroupUnderlineBounds(
+        underline_view, leading_view, trailing_view);
   }
 
-  cc::PaintFlags flags;
-  flags.setAntiAlias(true);
-  flags.setColor(tab_group_views_->GetGroupColor());
-  flags.setStyle(cc::PaintFlags::kFill_Style);
-  canvas->DrawPath(GetPath(), flags);
+  // override bounds for vertical tabs mode.
+  gfx::RectF leading_bounds = gfx::RectF(leading_view->bounds());
+  ConvertRectToTarget(leading_view->parent(), underline_view->parent(),
+                      &leading_bounds);
+  leading_bounds.Inset(gfx::InsetsF(GetInsetsForUnderline(leading_view)));
+
+  gfx::RectF trailing_bounds = gfx::RectF(trailing_view->bounds());
+  ConvertRectToTarget(trailing_view->parent(), underline_view->parent(),
+                      &trailing_bounds);
+  trailing_bounds.Inset(gfx::InsetsF(GetInsetsForUnderline(trailing_view)));
+  if (trailing_bounds.height()) {
+    trailing_bounds.set_height(trailing_bounds.height() +
+                               BraveTabGroupHeader::kPaddingForGroup);
+  }
+
+  gfx::Rect group_bounds = ToEnclosingRect(leading_bounds);
+  group_bounds.Union(ToEnclosingRect(trailing_bounds));
+  return group_bounds;
 }
 
 bool BraveTabGroupUnderline::ShouldShowVerticalTabs() const {
---
+++ chromium_src/chrome/browser/ui/views/tabs/tab_group_style.cc
@@ -0,0 +1,58 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "chrome/browser/ui/views/tabs/tab_group_style.h"
+
+#include "brave/browser/ui/tabs/features.h"
+#include "brave/browser/ui/views/tabs/vertical_tab_utils.h"
+
+#define TabGroupStyle TabGroupStyle_ChromiumImpl
+#define ChromeRefresh2023TabGroupStyle \
+  ChromeRefresh2023TabGroupStyle_ChromiumImpl
+
+#include "src/chrome/browser/ui/views/tabs/tab_group_style.cc"
+#undef ChromeRefresh2023TabGroupStyle
+#undef TabGroupStyle
+
+const int TabGroupStyle::kStrokeThicknessForVerticalTabs = 4;
+
+SkPath TabGroupStyle::GetUnderlinePath(gfx::Rect local_bounds) const {
+  if (!ShouldShowVerticalTabs()) {
+    return TabGroupStyle_ChromiumImpl::GetUnderlinePath(local_bounds);
+  }
+
+  // In vertical tabs, underline is not actually "underline'. It's vertical line
+  // at the left side of the tab group. And it has half rounded corners.
+  //
+  // +   group header   | '+' is the underline.
+  // ++  tab 1          | Drawing starts from top-right and goes
+  // ++  tab 2          | counter-clockwise
+  // +   tab 3          |
+  //
+  SkPath path;
+  path.arcTo(/* rx = */ kStrokeThicknessForVerticalTabs,
+             /* ry = */ kStrokeThicknessForVerticalTabs,
+             /* angle = */ 180.f, SkPath::kSmall_ArcSize, SkPathDirection::kCW,
+             /* x = */ kStrokeThicknessForVerticalTabs,
+             /* y = */ kStrokeThicknessForVerticalTabs);
+  path.lineTo(kStrokeThicknessForVerticalTabs,
+              local_bounds.height() - kStrokeThicknessForVerticalTabs);
+  path.arcTo(/* rx = */ kStrokeThicknessForVerticalTabs,
+             /* ry = */ kStrokeThicknessForVerticalTabs,
+             /* angle = */ 180.f, SkPath::kSmall_ArcSize, SkPathDirection::kCW,
+             /* x = */ 0,
+             /* y = */ local_bounds.height());
+  path.close();
+
+  return path;
+}
+
+bool TabGroupStyle::ShouldShowVerticalTabs() const {
+  if (!base::FeatureList::IsEnabled(tabs::features::kBraveVerticalTabs)) {
+    return false;
+  }
+
+  return tabs::utils::ShouldShowVerticalTabs(tab_group_views_->GetBrowser());
+}
