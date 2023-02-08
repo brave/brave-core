86447c8d086754917836208ff1cfa9f24e2eabe5
Brandon-T <bthomas@brave.com>
2023-02-08 17:06:25 -0500

    Fix #6888: Fixes __firefox__ overrides to not override themselves (#6910)
    * Fix security

--- Sources/Brave/Frontend/UserContent/UserScripts/__firefox__.js
+++ Sources/Brave/Frontend/UserContent/UserScripts/__firefox__.js
@@ -136,7 +136,11 @@ if (!window.__firefox__) {
             // on custom objects we don't own. We secure it,
             // but not freeze it.
             // The object may want to change it or override it, etc.
-            secureToString(value[name]);
+            // Do not secure our already secured toString override,
+            // if the custom override happens to be the same
+            if (value[name] !== toString) {
+              secureToString(value[name]);
+            }
             continue;
           }
           
@@ -149,7 +153,11 @@ if (!window.__firefox__) {
             // on custom objects we don't own. We secure it,
             // but not freeze it.
             // The object may want to change it or override it, etc.
-            secureToString(value[name]);
+            // Do not secure our already secured toString override,
+            // if the custom override happens to be the same
+            if (descriptor.value !== toString) {
+              secureToString(descriptor.value);
+            }
             continue;
           }
         }
