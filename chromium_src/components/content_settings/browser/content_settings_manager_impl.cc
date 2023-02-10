5ce9f8f4a8fc0a6cd47d34da05ee5a05c92aff75
Claudio DeSouza <cdesouza@igalia.com>
2023-02-10 16:59:43 +0000

    net::CookieSettingOverride::kNone removed
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/bfaa7208ef6e0a2255af3f66ce86a969ec6d5192
    
    commit bfaa7208ef6e0a2255af3f66ce86a969ec6d5192
    Author: Chris Fredrickson <cfredric@chromium.org>
    Date:   Tue Jan 31 17:13:24 2023 +0000
    
        Remove unused kNone variant
    
        This variant is never used explicitly, and there are no
        default-initialized CookieSettingOverride variables, so it is never
        used implicitly either.
    
        Thus far, the only use of the CookieSettingOverride enum is in an
        EnumSet, and in that use case, it's unnecessary to have an explicit
        variant for "no override", since the set could just be empty instead.
    
        (If, in the future, we need a way to represent "a single override or
        maybe nothing", we should use a absl::optional<CookieSettingOverride>
        for that.)
    
        Change-Id: If0c6b37812818c581ccdcf5a1e2271e03a9ac57a

--- chromium_src/components/content_settings/browser/content_settings_manager_impl.cc
+++ chromium_src/components/content_settings/browser/content_settings_manager_impl.cc
@@ -17,8 +17,8 @@ void ContentSettingsManagerImpl::AllowEphemeralStorageAccess(
     AllowEphemeralStorageAccessCallback callback) {
   url::Origin storage_origin;
   const bool should_use = cookie_settings_->ShouldUseEphemeralStorage(
-      origin, site_for_cookies, top_frame_origin,
-      net::CookieSettingOverride::kNone, storage_origin);
+      origin, site_for_cookies, top_frame_origin, net::CookieSettingOverride(),
+      storage_origin);
   std::move(callback).Run(should_use
                               ? absl::make_optional<url::Origin>(storage_origin)
                               : absl::nullopt);
