b9490b7a0a660811e417dc15dea75013fc41c24d
Claudio DeSouza <cdesouza@igalia.com>
2023-02-10 13:49:12 +0000

    Removing 3p desktop sharing overrides
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/6d2d822ce8f464252e6da8788ca6fcc89c8fdee1
    
    commit 6d2d822ce8f464252e6da8788ca6fcc89c8fdee1
    Author: Elly Fong-Jones <ellyjones@chromium.org>
    Date:   Mon Jan 30 18:21:48 2023 +0000
    
        desktop-sharing: remove 3p targets from desktop share hub
    
        After some experimentation, we concluded that having these targets here
        is not a good fit for what users actually want: these targets are less
        convenient and less predictable than copying URLs from the omnibox
        directly into other apps, and don't offer any added value.
    
        To remove the 3p section, this change:
        1. Removes the Views UI code for 3p share targets in the sharing hub
        2. Removes the code to maintain the 3p target list in the sharing model
        3. Removes the protobuf definition and supporting code for the 3p target
           list component
        4. Has the desktop browser use the existing desktop sharing hub cleanup
           mechanism, already used on Android, to ensure that the component
           doesn't linger on disk
    
        Once this change reaches stable, the component will be unpublished and
        deleted server-side.
    
        Bug: 1410275

--- browser/sharing_hub/brave_sharing_hub_browsertest.cc
+++ browser/sharing_hub/brave_sharing_hub_browsertest.cc
@@ -3,39 +3,18 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/sharing_hub/sharing_hub_model.h"
-#include "chrome/browser/sharing_hub/sharing_hub_service.h"
-#include "chrome/browser/sharing_hub/sharing_hub_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/page_action/page_action_icon_type.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/toolbar_button_provider.h"
 #include "chrome/browser/ui/views/page_action/page_action_icon_view.h"
-#include "chrome/common/pref_names.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
-#include "components/prefs/pref_service.h"
 #include "content/public/test/browser_test.h"
 
 using BraveSharingHubTest = InProcessBrowserTest;
 
 namespace sharing_hub {
-
-IN_PROC_BROWSER_TEST_F(BraveSharingHubTest,
-                       SharingHubThirdPartyActionsEmptyTest) {
-  auto* profile = browser()->profile();
-
-  EXPECT_TRUE(
-      profile->GetPrefs()->GetBoolean(prefs::kDesktopSharingHubEnabled));
-
-  auto* sharing_hub_service = SharingHubServiceFactory::GetForProfile(profile);
-  auto* model = sharing_hub_service->GetSharingHubModel();
-  std::vector<SharingHubAction> list;
-  model->GetThirdPartyActionList(
-      browser()->tab_strip_model()->GetActiveWebContents(), &list);
-  EXPECT_TRUE(list.empty());
-}
 
 IN_PROC_BROWSER_TEST_F(BraveSharingHubTest, SharingHubIconVisibility) {
   views::View* sharing_hub_icon =
