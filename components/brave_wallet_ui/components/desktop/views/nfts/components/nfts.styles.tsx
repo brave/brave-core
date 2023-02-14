4ee15d0ee392518f035b776b17d9e6e924860d22
William Muli <willi.wambu@gmail.com>
2023-02-14 10:24:58 +0300

    Update nfts.styles.tsx

--- components/brave_wallet_ui/components/desktop/views/nfts/components/nfts.styles.tsx
+++ components/brave_wallet_ui/components/desktop/views/nfts/components/nfts.styles.tsx
@@ -8,6 +8,13 @@ import { WalletButton } from '../../../../shared/style'
 import Ipfs from '../../../../../assets/svg-icons/nft-ipfs/ipfs.svg'
 import PlusIcon from '../../../../../assets/svg-icons/plus-icon.svg'
 
+export const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  justify-content: center;
+`
+
 export const FilterTokenRow = styled.div`
   display: flex;
   align-items: center;
