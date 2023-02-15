06f2191eab6ec1584dec2865dffc268e8f7abd21
mkarolin <max@brave.com>
2023-02-15 17:24:31 -0500

    Fixes navigator.connection for workers.
    Adds the same override we use for V8Navigator to V8WorkerNavigator.

--- chromium_src/third_party/blink/renderer/platform/bindings/idl_member_installer.h
+++ chromium_src/third_party/blink/renderer/platform/bindings/idl_member_installer.h
@@ -1,10 +1,12 @@
 /* Copyright (c) 2021 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #ifndef BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_PLATFORM_BINDINGS_IDL_MEMBER_INSTALLER_H_
 #define BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_PLATFORM_BINDINGS_IDL_MEMBER_INSTALLER_H_
+
+struct BraveNavigatorAttributeInstallerTrait;
 
 #define BRAVE_IDL_MEMBER_INSTALLER_H_                     \
   template <typename T>                                   \
