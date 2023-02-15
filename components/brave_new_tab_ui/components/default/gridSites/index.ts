c2c879eace8ce3008b34bece19bb5d39ab2d1c25
Jay Harris <jay.harris@outlook.co.nz>
2023-02-16 12:29:12 +1300

    [Uplift] Top Sites Alignment 1.48 (#17172)
    Top Sites Alignment (#17044)

--- components/brave_new_tab_ui/components/default/gridSites/index.ts
+++ components/brave_new_tab_ui/components/default/gridSites/index.ts
@@ -111,6 +111,7 @@ export const GridPagesContainer = styled('div')<{ customLinksEnabled: boolean }>
   display: flex;
   flex-direction: row;
 
+  margin-left: 24px;
   padding: 24px 24px 0 24px;
   max-width: calc((var(--grid-columns) + 1) * var(--grid-column-width));
   overflow-x: ${p => p.customLinksEnabled ? 'auto' : 'hidden'};
