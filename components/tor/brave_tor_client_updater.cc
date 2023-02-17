ac803529318dfbfb9dbc1c45639cbfdb5fabd4f0
Linh Kikuchi <xuongrongl84@gmail.com>
2023-02-17 17:06:35 +0900

    Fix tor component regex

--- components/tor/brave_tor_client_updater.cc
+++ components/tor/brave_tor_client_updater.cc
@@ -40,7 +40,7 @@ std::pair<base::FilePath, base::FilePath> InitTorPath(
     base::FileEnumerator::FileInfo file_info = traversal.GetInfo();
     if (RE2::FullMatch(
             file_info.GetName().MaybeAsASCII(),
-            "tor-\\d+\\.\\d+\\.\\d+\\.\\d+-\\w+|\\w+-\\w+-brave-\\d+")) {
+            "tor-\\d+\\.\\d+\\.\\d+\\.\\d+-\\w+(-\\w+)?-brave-\\d+")) {
       executable_path = current;
     } else if (file_info.GetName().MaybeAsASCII() == "tor-torrc") {
       torrc_path = current;
