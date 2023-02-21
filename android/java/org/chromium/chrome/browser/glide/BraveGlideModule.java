7a969cb8e25b4e8c82a1b4c7ba8aadba3d274277
Simone Arpe <simon.arpe@gmail.com>
2023-02-21 18:58:12 +0100

    Implement Glide module using meta data in manifest

--- android/java/org/chromium/chrome/browser/glide/BraveGlideModule.java
+++ android/java/org/chromium/chrome/browser/glide/BraveGlideModule.java
@@ -11,9 +11,10 @@ import android.graphics.drawable.PictureDrawable;
 import androidx.annotation.NonNull;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.Registry;
-import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.module.GlideModule;
 import com.caverock.androidsvg.SVG;
 
 import java.io.InputStream;
@@ -21,10 +22,8 @@ import java.nio.ByteBuffer;
 
 /**
  * Module for all the Glide components used by Brave app.
- * There can be at most one {@link AppGlideModule} in an application.
  */
-@GlideModule
-public class BraveGlideModule extends AppGlideModule {
+public class BraveGlideModule implements GlideModule {
     @Override
     public void registerComponents(
             @NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
@@ -35,8 +34,7 @@ public class BraveGlideModule extends AppGlideModule {
     }
 
     @Override
-    public boolean isManifestParsingEnabled() {
-        // Disable manifest parsing to avoid adding similar modules twice.
-        return false;
+    public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
+        // Default empty impl.
     }
 }
