8dc31d2f5073b9d98c357eede30d27e4140824fa
Josh Leonard <30185185+josheleonard@users.noreply.github.com>
2023-02-23 18:44:49 -0500

    chore: remove unused buy-send-swap widget code

--- components/brave_wallet_ui/components/buy-send-swap/shared-styles.ts
+++ components/brave_wallet_ui/components/buy-send-swap/shared-styles.ts
@@ -3,8 +3,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // you can obtain one at https://mozilla.org/MPL/2.0/.
 import styled from 'styled-components'
-import { WalletButton } from '../shared/style'
-
 export const BubbleContainer = styled.div`
   display: flex;
   width: 100%;
@@ -51,52 +49,4 @@ export const SelectScrollContainer = styled.div`
   bottom: 18px;
   left: 18px;
   right: 18px;
-`
-
-export const ErrorText = styled.span`
-  font-family: Poppins;
-  letter-spacing: 0.01em;
-  font-size: 12px;
-  color: ${(p) => p.theme.color.errorText};
-  word-break: break-word;
-  margin-bottom: 12px;
-`
-
-export const DivderTextWrapper = styled.div`
-  display: flex;
-  width: 100%;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
-  border-bottom: 2px solid ${(p) => p.theme.color.divider01};
-  padding-bottom: 6px;
-  margin-bottom: 10px;
-`
-
-export const DividerText = styled.span`
-  font-family: Poppins;
-  font-size: 13px;
-  line-height: 20px;
-  letter-spacing: 0.01em;
-  font-weight: 600;
-  color: ${(p) => p.theme.color.text01};
-`
-
-export const ResetButton = styled(WalletButton)`
-  cursor: pointer;
-  outline: none;
-  border: none;
-  font-weight: 600;
-  font-family: Poppins;
-  font-size: 12px;
-  line-height: 18px;
-  letter-spacing: 0.01em;
-  padding: 2px 0px;
-  width: 48px;
-  margin: 12px 0px;
-  background: none;
-  color: ${(p) => p.theme.color.interactive05};
-  @media (prefers-color-scheme: dark) {
-    color: ${(p) => p.theme.palette.blurple300};
-  }
 `
