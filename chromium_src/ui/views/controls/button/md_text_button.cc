463407810a41d403e63de9458c767bbd28fad959
Claudio DeSouza <cdesouza@igalia.com>
2023-02-27 12:32:51 +0000

    MdTextButton::GetCornerRadius returning float
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/c0009b0683aff2cf01b8696ec118998f87d719f2
    
    commit c0009b0683aff2cf01b8696ec118998f87d719f2
    Author: David Yeung <dayeung@chromium.org>
    Date:   Wed Feb 15 23:13:45 2023 +0000
    
        [views]: Adding radius tokens for LayoutProvider
    
        Similar to ColorProvider, LayoutProvider will have tokens that are aligned between UX/UI and Engineering. These token are used to replace hard coded values and will be standardized across all UI development. This is for Chrome2023Refresh.
    
        There are 2 tokens in LayoutProvider.
    
        `ContextTokens`: These tokens are specific to the component.
                - kButtonRadius, kComponentRadius, kTextFieldRadius
        `SysShapeTokens`: These tokens will map to a value
                - kXsmall, kSmall, kMedium, kLarge, kFull
    
        Because kFull requires shaping and is dependent on the component size (which is set after the component is initialized), the values will be fetched from a switch statement instead of a static map.
    
        Change-Id: I03a444102f54fc6fde4e3e95d9b3e2476b985607
        Bug: 1412134

--- chromium_src/ui/views/controls/button/md_text_button.cc
+++ chromium_src/ui/views/controls/button/md_text_button.cc
@@ -181,7 +181,8 @@ MdTextButton::MdTextButton(PressedCallback callback,
         const SkColor fill_color = SK_ColorTRANSPARENT;
         gfx::RectF boundsF(host->GetLocalBounds());
         return std::make_unique<InkDropHighlight>(
-            boundsF.size(), static_cast<MdTextButton*>(host)->GetCornerRadius(),
+            boundsF.size(),
+            static_cast<MdTextButton*>(host)->GetCornerRadiusValue(),
             boundsF.CenterPoint(), fill_color);
       },
       this));
@@ -191,7 +192,7 @@ MdTextButton::~MdTextButton() = default;
 
 SkPath MdTextButton::GetHighlightPath() const {
   SkPath path;
-  int radius = GetCornerRadius();
+  float radius = GetCornerRadiusValue();
   path.addRRect(
       SkRRect::MakeRectXY(RectToSkRect(GetLocalBounds()), radius, radius));
   return path;
@@ -297,7 +298,7 @@ void MdTextButton::UpdateBackgroundColor() {
 
   SetBackground(
       CreateBackgroundFromPainter(Painter::CreateRoundRectWith1PxBorderPainter(
-          colors.background_color, colors.stroke_color, GetCornerRadius())));
+          colors.background_color, colors.stroke_color, GetCornerRadiusValue())));
 }
 
 void MdTextButton::UpdateColors() {
@@ -326,7 +327,7 @@ void MdTextButton::OnPaintBackground(gfx::Canvas* canvas) {
     flags.setColor(current_color);
     flags.setStyle(cc::PaintFlags::kFill_Style);
     flags.setAntiAlias(true);
-    canvas->DrawRoundRect(gfx::RectF(GetLocalBounds()), GetCornerRadius(),
+    canvas->DrawRoundRect(gfx::RectF(GetLocalBounds()), GetCornerRadiusValue(),
                           flags);
   }
 }
