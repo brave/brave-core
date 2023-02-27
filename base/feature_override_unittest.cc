16eb492fb87a6f4e16bba81ae427a712c27e49e5
Aleksey Khoroshilov <5928869+goodov@users.noreply.github.com>
2023-02-27 14:14:11 +0700

    Optimize feature overrides on startup. (#17272)
    Optimize feature overrides init on startup.

--- base/feature_override_unittest.cc
+++ base/feature_override_unittest.cc
@@ -1,7 +1,7 @@
 /* Copyright (c) 2021 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "base/feature_override.h"
 #include "base/feature_list.h"
@@ -15,29 +15,27 @@ using testing::_;
 namespace base {
 namespace {
 
-const Feature kTestControlEnabledFeature{"TestControlEnabledFeature",
-                                         FEATURE_ENABLED_BY_DEFAULT};
-const Feature kTestControlDisabledFeature{"TestControlDisabledFeature",
-                                          FEATURE_DISABLED_BY_DEFAULT};
+BASE_FEATURE(kTestControlEnabledFeature,
+             "TestControlEnabledFeature",
+             FEATURE_ENABLED_BY_DEFAULT);
+BASE_FEATURE(kTestControlDisabledFeature,
+             "TestControlDisabledFeature",
+             FEATURE_DISABLED_BY_DEFAULT);
 
-const Feature kTestEnabledButOverridenFeature{"TestEnabledButOverridenFeature",
-                                              FEATURE_ENABLED_BY_DEFAULT};
-const Feature kTestDisabledButOverridenFeature{
-    "TestDisabledButOverridenFeature", FEATURE_DISABLED_BY_DEFAULT};
+BASE_FEATURE(kTestEnabledButOverridenFeature,
+             "TestEnabledButOverridenFeature",
+             FEATURE_ENABLED_BY_DEFAULT);
+BASE_FEATURE(kTestDisabledButOverridenFeature,
+             "TestDisabledButOverridenFeature",
+             FEATURE_DISABLED_BY_DEFAULT);
 
-constexpr Feature kTestConstexprEnabledButOverridenFeature{
-    "TestConstexprEnabledButOverridenFeature", FEATURE_ENABLED_BY_DEFAULT};
-constexpr Feature kTestConstexprDisabledButOverridenFeature{
-    "TestConstexprDisabledButOverridenFeature", FEATURE_DISABLED_BY_DEFAULT};
-
-const Feature kTestEnabledButOverridenFeatureWithSameState{
-    "TestEnabledButOverridenFeatureWithSameState", FEATURE_ENABLED_BY_DEFAULT};
+BASE_FEATURE(kTestEnabledButOverridenFeatureWithSameState,
+             "TestEnabledButOverridenFeatureWithSameState",
+             FEATURE_ENABLED_BY_DEFAULT);
 
 OVERRIDE_FEATURE_DEFAULT_STATES({{
     {kTestEnabledButOverridenFeature, FEATURE_DISABLED_BY_DEFAULT},
     {kTestDisabledButOverridenFeature, FEATURE_ENABLED_BY_DEFAULT},
-    {kTestConstexprEnabledButOverridenFeature, FEATURE_DISABLED_BY_DEFAULT},
-    {kTestConstexprDisabledButOverridenFeature, FEATURE_ENABLED_BY_DEFAULT},
 
     // Override, but keep the same state as `default_state`. We should properly
     // return false from IsFeatureOverridden in this case.
@@ -52,7 +50,7 @@ TEST(FeatureOverrideTest, OverridesTest) {
     const bool is_enabled;
     const bool is_overridden;
   };
-  const TestCase kTestCases[] = {
+  constexpr TestCase kTestCases[] = {
       // Untouched features.
       {kTestControlEnabledFeature, true, false},
       {kTestControlDisabledFeature, false, false},
@@ -60,10 +58,6 @@ TEST(FeatureOverrideTest, OverridesTest) {
       // Overridden features.
       {kTestEnabledButOverridenFeature, false, true},
       {kTestDisabledButOverridenFeature, true, true},
-
-      // Overridden constexpr features.
-      {kTestConstexprEnabledButOverridenFeature, false, true},
-      {kTestConstexprDisabledButOverridenFeature, true, true},
 
       // Overridden but with the same state.
       {kTestEnabledButOverridenFeatureWithSameState, true, false},
@@ -79,6 +73,10 @@ TEST(FeatureOverrideTest, OverridesTest) {
 
 #if DCHECK_IS_ON()
 TEST(FeatureOverrideTest, FeatureDuplicateDChecks) {
+  // Check any feature to make sure overridden features are finalized (moved
+  // from an unsorted vector to a sorted flat_map).
+  ASSERT_FALSE(base::FeatureList::IsEnabled(kTestEnabledButOverridenFeature));
+
   base::MockCallback<logging::LogAssertHandlerFunction> mock_log_handler;
   logging::ScopedLogAssertHandler scoped_log_handler(mock_log_handler.Get());
   EXPECT_CALL(
@@ -94,6 +92,12 @@ TEST(FeatureOverrideTest, FeatureDuplicateDChecks) {
           _))
       .Times(2);
 
+  // This will add a feature to an unsorted vector of overrides.
+  internal::FeatureDefaultStateOverrider init_overrides{{
+      {kTestEnabledButOverridenFeature, FEATURE_DISABLED_BY_DEFAULT},
+  }};
+
+  // This should trigger DCHECKs.
   internal::FeatureDefaultStateOverrider test_overrider{{
       {kTestEnabledButOverridenFeature, FEATURE_DISABLED_BY_DEFAULT},
       {kTestEnabledButOverridenFeature, FEATURE_DISABLED_BY_DEFAULT},
