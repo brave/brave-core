dbe3003cd94d68c4206d8838353be47023a50c62
AlexeyBarabash <alexey@brave.com>
2023-03-01 20:13:06 +0200

    Disable ChromePreferenceKeyChecker to avoid crashes on Nightly
    Fixes brave/brave-browser#28831

--- build/android/bytecode/java/org/brave/bytecode/BraveClassAdapter.java
+++ build/android/bytecode/java/org/brave/bytecode/BraveClassAdapter.java
@@ -58,6 +58,7 @@ public class BraveClassAdapter {
         chain = new BravePermissionDialogDelegateClassAdapter(chain);
         chain = new BravePermissionDialogModelClassAdapter(chain);
         chain = new BravePreferenceFragmentClassAdapter(chain);
+        chain = new BravePreferenceKeyCheckerClassAdapter(chain);
         chain = new BravePureJavaExceptionReporterClassAdapter(chain);
         chain = new BraveQueryTileSectionClassAdapter(chain);
         chain = new BraveReaderModeManagerClassAdapter(chain);
