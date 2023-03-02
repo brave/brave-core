74ab6332a321a3b87a3f034c5a07566b0d1d3dae
Michał Buczek <michal.buczek@protonmail.com>
2023-03-02 19:10:54 +0100

    Ref #6957: Update brave-core to 1.49.117 (#7045)

--- Sources/BraveWallet/Preview Content/MockAssetRatioService.swift
+++ Sources/BraveWallet/Preview Content/MockAssetRatioService.swift
@@ -39,6 +39,10 @@ class MockAssetRatioService: BraveWalletAssetRatioService {
   func coinMarkets(_ vsAsset: String, limit: UInt8, completion: @escaping (Bool, [BraveWallet.CoinMarket]) -> Void) {
     completion(false, [])
   }
+  
+  func sellUrl(_ provider: BraveWallet.OffRampProvider, chainId: String, address: String, symbol: String, amount: String, currencyCode: String, completion: @escaping (String, String?) -> Void) {
+    completion("", nil)
+  }
 }
 
 #endif
