4bd170cff3049d541b524264f7b2fd648e806572
Jay Harris <jay.harris@outlook.co.nz>
2023-03-02 18:43:33 +1300

    [Uplift] [1.49] Fix bug where unsubscribing wouldn't update sources (#17372) (#17374)
    Fix bug where unsubscribing wouldn't update sources (#17372)

--- components/brave_today/browser/brave_news_controller.cc
+++ components/brave_today/browser/brave_news_controller.cc
@@ -430,8 +430,8 @@ void BraveNewsController::SetPublisherPref(const std::string& publisher_id,
           } else {
             update->Set(publisher_id,
                         (new_status == mojom::UserEnabled::ENABLED));
-            controller->publishers_controller_.EnsurePublishersIsUpdating();
           }
+          controller->publishers_controller_.EnsurePublishersIsUpdating();
         }
       },
       publisher_id, new_status, base::Unretained(this)));
