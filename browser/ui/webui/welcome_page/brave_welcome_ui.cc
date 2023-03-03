81515b72287e43a437066097cecece4f5458e28a
sergei <spylogsster@gmail.com>
2023-03-03 11:09:31 +0300

    Move welcome page files to separate folder

--- browser/ui/webui/welcome_page/brave_welcome_ui.cc
+++ browser/ui/webui/welcome_page/brave_welcome_ui.cc
@@ -1,7 +1,7 @@
 /* Copyright (c) 2019 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "brave/browser/ui/webui/welcome_page/brave_welcome_ui.h"
 
@@ -11,46 +11,30 @@
 
 #include "base/feature_list.h"
 #include "base/memory/raw_ptr.h"
-#include "base/metrics/histogram_macros.h"
-#include "base/strings/utf_string_conversions.h"
 #include "base/task/single_thread_task_runner.h"
 #include "brave/browser/ui/webui/brave_webui_source.h"
 #include "brave/browser/ui/webui/settings/brave_import_bulk_data_handler.h"
 #include "brave/browser/ui/webui/settings/brave_search_engines_handler.h"
-#include "brave/common/importer/importer_constants.h"
+#include "brave/browser/ui/webui/welcome_page/welcome_dom_handler.h"
 #include "brave/components/brave_welcome/common/features.h"
 #include "brave/components/brave_welcome/resources/grit/brave_welcome_generated_map.h"
 #include "brave/components/constants/pref_names.h"
 #include "brave/components/constants/webui_url_constants.h"
 #include "brave/components/l10n/common/localization_util.h"
-#include "brave/components/p3a/buildflags.h"
-#include "chrome/browser/browser_process.h"
-#include "chrome/browser/metrics/metrics_reporting_state.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
-#include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/webui/settings/privacy_sandbox_handler.h"
 #include "chrome/browser/ui/webui/settings/settings_default_browser_handler.h"
 #include "chrome/common/pref_names.h"
-#include "chrome/common/webui_url_constants.h"
-#include "chrome/grit/chromium_strings.h"
 #include "components/country_codes/country_codes.h"
 #include "components/grit/brave_components_resources.h"
 #include "components/grit/brave_components_strings.h"
-#include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/gpu_data_manager.h"
 #include "content/public/browser/page_navigator.h"
 #include "content/public/browser/web_ui_data_source.h"
 #include "content/public/browser/web_ui_message_handler.h"
-#include "ui/base/l10n/l10n_util.h"
-
-#if BUILDFLAG(BRAVE_P3A_ENABLED)
-#include "brave/components/p3a/pref_names.h"
-#endif
-
-using content::WebUIMessageHandler;
 
 namespace {
 
@@ -101,55 +85,12 @@ void OpenJapanWelcomePage(Profile* profile) {
   }
 }
 
-void RecordP3AHistogram(int screen_number, bool finished) {
-  int kCurrentScreen = 0;
-  int kMaxScreens = 6;
-  if (finished) {
-    kCurrentScreen = kMaxScreens + 1;
-  } else {
-    kCurrentScreen = std::min(screen_number, kMaxScreens);
-  }
-  UMA_HISTOGRAM_EXACT_LINEAR("Brave.Welcome.InteractionStatus", kCurrentScreen,
-                             kMaxScreens + 1);
-}
-
-// The handler for Javascript messages for the chrome://welcome page
-class WelcomeDOMHandler : public WebUIMessageHandler {
- public:
-  explicit WelcomeDOMHandler(Profile* profile);
-  WelcomeDOMHandler(const WelcomeDOMHandler&) = delete;
-  WelcomeDOMHandler& operator=(const WelcomeDOMHandler&) = delete;
-  ~WelcomeDOMHandler() override;
-
-  // WebUIMessageHandler implementation.
-  void RegisterMessages() override;
-
- private:
-  void HandleImportNowRequested(const base::Value::List& args);
-  void HandleRecordP3A(const base::Value::List& args);
-  void HandleGetDefaultBrowser(const base::Value::List& args);
-  void SetLocalStateBooleanEnabled(const std::string& path,
-                                   const base::Value::List& args);
-  void OnGetDefaultBrowser(shell_integration::DefaultWebClientState state,
-                           const std::u16string& name);
-  void SetP3AEnabled(const base::Value::List& args);
-  void HandleOpenSettingsPage(const base::Value::List& args);
-  void HandleSetMetricsReportingEnabled(const base::Value::List& args);
-  Browser* GetBrowser();
-
-  int screen_number_ = 0;
-  bool finished_ = false;
-  bool skipped_ = false;
-  std::u16string default_browser_name_;
-  raw_ptr<Profile> profile_ = nullptr;
-  base::WeakPtrFactory<WelcomeDOMHandler> weak_ptr_factory_{this};
-};
-
 // Converts Chromium country ID to 2 digit country string
 // For more info see src/components/country_codes/country_codes.h
 std::string CountryIDToCountryString(int country_id) {
-  if (country_id == country_codes::kCountryIDUnknown)
+  if (country_id == country_codes::kCountryIDUnknown) {
     return std::string();
+  }
 
   char chars[3] = {static_cast<char>(country_id >> 8),
                    static_cast<char>(country_id), 0};
