a4dc1f2081d2cbb6eff53141444cf5c4417cc6d7
Aleksey Khoroshilov <akhoroshilov@brave.com>
2023-03-05 23:51:48 +0700

    Expose navigator.globalPrivacyControl to workers.

--- browser/net/global_privacy_control_network_delegate_helper_browsertest.cc
+++ browser/net/global_privacy_control_network_delegate_helper_browsertest.cc
@@ -3,20 +3,21 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+#include "base/path_service.h"
+#include "base/thread_annotations.h"
+#include "brave/components/constants/brave_paths.h"
 #include "brave/components/constants/network_constants.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
-#include "components/network_session_configurator/common/network_switches.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
-#include "content/public/test/test_utils.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/http_request.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 enum class GPCHeaderResult {
   kOk,
-  kNoRequest,
   kNoHeader,
   kWrongValue,
 };
@@ -30,8 +31,6 @@ class GlobalPrivacyControlNetworkDelegateBrowserTest
   void SetUpOnMainThread() override {
     InProcessBrowserTest::SetUpOnMainThread();
 
-    header_result_ = GPCHeaderResult::kNoRequest;
-
     host_resolver()->AddRule("*", "127.0.0.1");
 
     https_server_.SetSSLConfig(net::EmbeddedTestServer::CERT_TEST_NAMES);
@@ -39,11 +38,19 @@ class GlobalPrivacyControlNetworkDelegateBrowserTest
         &GlobalPrivacyControlNetworkDelegateBrowserTest::HandleRequest,
         base::Unretained(this)));
 
+    brave::RegisterPathProvider();
+    base::FilePath test_data_dir;
+    base::PathService::Get(brave::DIR_TEST_DATA, &test_data_dir);
+    https_server_.ServeFilesFromDirectory(test_data_dir);
+
     ASSERT_TRUE(https_server_.Start());
   }
 
   void HandleRequest(const net::test_server::HttpRequest& request) {
     base::AutoLock auto_lock(header_result_lock_);
+    if (!start_tracking_) {
+      return;
+    }
 
     auto it = request.headers.find(kSecGpcHeader);
     if (it == request.headers.end()) {
@@ -57,22 +64,53 @@ class GlobalPrivacyControlNetworkDelegateBrowserTest
 
   const net::EmbeddedTestServer& https_server() { return https_server_; }
 
+  void StartTracking() {
+    base::AutoLock auto_lock(header_result_lock_);
+    ASSERT_FALSE(header_result_);
+    start_tracking_ = true;
+  }
+
   GPCHeaderResult header_result() {
     base::AutoLock auto_lock(header_result_lock_);
-    return header_result_;
+    EXPECT_TRUE(header_result_);
+    return *header_result_;
+  }
+
+  content::EvalJsResult MessageServiceWorker(
+      const content::ToRenderFrameHost& execution_target,
+      std::string message) {
+    constexpr char kScript[] = R"(
+      new Promise((resolve) => {
+        navigator.serviceWorker.addEventListener(
+          'message',
+          (msg) => {
+            if (msg.data['cmd'] === $1) {
+              resolve(msg.data['result'])
+            }
+          },
+          { once: true }
+        )
+        navigator.serviceWorker.controller.postMessage({cmd: $1})
+      })
+    )";
+    return content::EvalJs(execution_target,
+                           content::JsReplace(kScript, message));
   }
 
  private:
   net::test_server::EmbeddedTestServer https_server_;
   mutable base::Lock header_result_lock_;
-  GPCHeaderResult header_result_;
+  bool start_tracking_ GUARDED_BY(header_result_lock_) = false;
+  absl::optional<GPCHeaderResult> header_result_
+      GUARDED_BY(header_result_lock_);
 };
 
 // When kGlobalPrivacyControl is enabled, the Sec-GPC flag should appear on
 // request headers.
 IN_PROC_BROWSER_TEST_F(GlobalPrivacyControlNetworkDelegateBrowserTest,
                        IncludesSecGPCHeader) {
-  const GURL target = https_server().GetURL("a.test", "/index.html");
+  const GURL target = https_server().GetURL("a.test", "/simple.html");
+  StartTracking();
   ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), target));
   EXPECT_EQ(header_result(), GPCHeaderResult::kOk);
 }
@@ -82,7 +120,7 @@ IN_PROC_BROWSER_TEST_F(GlobalPrivacyControlNetworkDelegateBrowserTest,
 // it will always return `true`.
 IN_PROC_BROWSER_TEST_F(GlobalPrivacyControlNetworkDelegateBrowserTest,
                        NavigatorGlobalPrivacyAPI) {
-  const GURL target = https_server().GetURL("a.test", "/index.html");
+  const GURL target = https_server().GetURL("a.test", "/simple.html");
   ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), target));
 
   auto* rfh = browser()
@@ -96,4 +134,43 @@ IN_PROC_BROWSER_TEST_F(GlobalPrivacyControlNetworkDelegateBrowserTest,
                                   "  navigator.globalPrivacyControl = false;"
                                   "  return navigator.globalPrivacyControl;"
                                   "})()"));
+}
+
+// The Global Privacy Control spec also defines the
+// `navigator.globalPrivacyControl` JS property, which is read-only. In Brave
+// it will always return `true`.
+IN_PROC_BROWSER_TEST_F(GlobalPrivacyControlNetworkDelegateBrowserTest,
+                       ServiceWorkerGPCAvailable) {
+  const GURL target = https_server().GetURL("a.test", "/navigator/simple.html");
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), target));
+
+  auto* rfh = browser()
+                  ->tab_strip_model()
+                  ->GetActiveWebContents()
+                  ->GetPrimaryMainFrame();
+
+  EXPECT_TRUE(content::ExecJs(rfh, R"(
+    new Promise((resolve) => {
+      navigator.serviceWorker
+        .register('./service-workers-gpc.js')
+        .then((registration) => {
+          if (registration.active) {
+            resolve(true)
+          } else if (registration.installing) {
+            registration.installing.addEventListener('statechange', () => {
+              if (registration.active) {
+                resolve(true)
+              }
+            })
+          }
+        })
+    })
+  )"));
+
+  StartTracking();
+  EXPECT_EQ(MessageServiceWorker(rfh, "fetch"), "LOADED");
+  EXPECT_EQ(header_result(), GPCHeaderResult::kOk);
+
+  EXPECT_EQ(MessageServiceWorker(rfh, "hasGpc"), true);
+  EXPECT_EQ(MessageServiceWorker(rfh, "checkGpc"), true);
 }
