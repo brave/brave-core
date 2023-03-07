132465f3147e289bbea5607b8198b0fec91045dc
brave-builds <brave-builds+devops@brave.com>
2023-03-07 18:42:51 +0000

    Uplift of #17508 (squashed) to release

--- android/java/org/chromium/chrome/browser/rewards/BraveRewardsPanel.java
+++ android/java/org/chromium/chrome/browser/rewards/BraveRewardsPanel.java
@@ -1661,10 +1661,21 @@ public class BraveRewardsPanel
         mRewardsVbatExpireNoticeModal.setVisibility(View.VISIBLE);
         TextView vBatModalTitle = mRewardsVbatExpireNoticeModal.findViewById(R.id.vbat_modal_title);
         TextView vBatModalText = mRewardsVbatExpireNoticeModal.findViewById(R.id.vbat_modal_text);
-        FrameLayout vBatConnectButton =
-                mRewardsVbatExpireNoticeModal.findViewById(R.id.btn_vbat_connect_account);
-        vBatConnectButton.setVisibility(
+        FrameLayout vBatConnectButtonLayout =
+                mRewardsVbatExpireNoticeModal.findViewById(R.id.layout_vbat_connect_account);
+        vBatConnectButtonLayout.setVisibility(
                 mBraveRewardsNativeWorker.canConnectAccount() ? View.VISIBLE : View.GONE);
+        vBatConnectButtonLayout.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TabUtils.openUrlInNewTab(
+                        false, BraveActivity.BRAVE_REWARDS_SETTINGS_WALLET_VERIFICATION_URL);
+                dismiss();
+            }
+        }));
+
+        Button vBatConnectButton =
+                mRewardsVbatExpireNoticeModal.findViewById(R.id.btn_vbat_connect_account);
         vBatConnectButton.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -1673,6 +1684,7 @@ public class BraveRewardsPanel
                 dismiss();
             }
         }));
+
         Button vBatLearnMoreButton =
                 mRewardsVbatExpireNoticeModal.findViewById(R.id.btn_vbat_learn_more);
         vBatLearnMoreButton.setOnClickListener((new View.OnClickListener() {
