cae81fa69853e8a1a730f976ee7b204049443250
Nuo Xu <nuoxu.nx@gmail.com>
2023-03-08 17:14:55 -0500

    expose the getter and setter of default ipfs public gateway. fix ipfs api to resolve url using correct public gateway.

--- ios/browser/api/ipfs/ipfs_api+private.h
+++ ios/browser/api/ipfs/ipfs_api+private.h
@@ -16,8 +16,10 @@ class ChromeBrowserState;
 
 @interface IpfsAPIImpl : NSObject <IpfsAPI>
 @property(nonatomic, nullable) NSURL* nftIpfsGateway;
+@property(nonatomic, nullable) NSURL* ipfsGateway;
 
-- (nullable NSURL*)resolveGatewayUrlFor:(NSURL*)input;
+- (nullable NSURL*)resolveGatewayUrlFor:(NSURL*)input
+                               isForNFT:(BOOL)isForNFT;
 - (instancetype)initWithBrowserState:(ChromeBrowserState*)mainBrowserState;
 - (instancetype)init NS_UNAVAILABLE;
 @end
--- ios/browser/api/ipfs/ipfs_api.h
+++ ios/browser/api/ipfs/ipfs_api.h
@@ -16,8 +16,10 @@ OBJC_EXPORT
 @required
 
 @property(nonatomic, nullable) NSURL* nftIpfsGateway;
+@property(nonatomic, nullable) NSURL* ipfsGateway;
 
-- (nullable NSURL*)resolveGatewayUrlFor:(NSURL*)input;
+- (nullable NSURL*)resolveGatewayUrlFor:(NSURL*)input
+                                isForNFT:(BOOL)isForNFT;
 
 - (nullable NSURL*)contentHashToCIDv1URLFor:(NSArray<NSNumber*>*)contentHash;
 
