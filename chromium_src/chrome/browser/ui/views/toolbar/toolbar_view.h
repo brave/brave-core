3132d11cc2c2b491568d17c3a8f7e62fbad3f270
Claudio DeSouza <cdesouza@igalia.com>
2023-03-08 17:54:33 +0000

    Adding overrides for toolbar_view.h
    This change removes the use of patches in favour of overrides for
    toolbar_view.h.

---
+++ chromium_src/chrome/browser/ui/views/toolbar/toolbar_view.h
@@ -0,0 +1,28 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_TOOLBAR_TOOLBAR_VIEW_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_TOOLBAR_TOOLBAR_VIEW_H_
+
+#include "chrome/browser/ui/omnibox/chrome_omnibox_edit_model_delegate.h"
+#include "chrome/browser/ui/views/frame/browser_root_view.h"
+#include "chrome/browser/ui/views/location_bar/custom_tab_bar_view.h"
+
+#define Init virtual Init
+#define Update virtual Update
+#define ShowBookmarkBubble virtual ShowBookmarkBubble
+#define LoadImages               \
+  LoadImagesUnused();            \
+  friend class BraveToolbarView; \
+  virtual void LoadImages
+
+#include "src/chrome/browser/ui/views/toolbar/toolbar_view.h"  // IWYU pragma: export
+
+#undef Init
+#undef Update
+#undef ShowBookmarkBubble
+#undef LoadImages
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_TOOLBAR_TOOLBAR_VIEW_H_
