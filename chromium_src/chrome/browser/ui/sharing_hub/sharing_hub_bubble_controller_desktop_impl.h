b6b8aafb1c47fccd9aad9763c5d6a0e9d2ee27e3
Simon Hong <shong@brave.com>
2023-03-14 10:46:02 +0900

    Fixed presubmit errors

--- chromium_src/chrome/browser/ui/sharing_hub/sharing_hub_bubble_controller_desktop_impl.h
+++ chromium_src/chrome/browser/ui/sharing_hub/sharing_hub_bubble_controller_desktop_impl.h
@@ -1,7 +1,7 @@
 /* Copyright (c) 2023 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_SHARING_HUB_SHARING_HUB_BUBBLE_CONTROLLER_DESKTOP_IMPL_H_
 #define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_SHARING_HUB_SHARING_HUB_BUBBLE_CONTROLLER_DESKTOP_IMPL_H_
@@ -15,7 +15,7 @@
   ShouldOfferOmniboxIcon_ChromiumImpl(); \
   bool ShouldOfferOmniboxIcon
 
-#include "src/chrome/browser/ui/sharing_hub/sharing_hub_bubble_controller_desktop_impl.h"
+#include "src/chrome/browser/ui/sharing_hub/sharing_hub_bubble_controller_desktop_impl.h"  // IWYU pragma: export
 
 #undef ShouldOfferOmniboxIcon
 
