d0c6a77773eb8533176a784f7d1dcff85b6acd86
Aleksey Khoroshilov <akhoroshilov@brave.com>
2023-03-16 13:01:50 +0700

    Disable thread profilers.

---
+++ common/profiler/thread_profile_configuration_unittest.cc
@@ -0,0 +1,21 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "chrome/common/profiler/thread_profiler_configuration.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+TEST(ThreadProfilerConfigurationTest, DisabledForAllThreads) {
+  const auto* config = ThreadProfilerConfiguration::Get();
+  for (int t =
+           static_cast<int>(metrics::CallStackProfileParams::Thread::kUnknown);
+       t <= static_cast<int>(metrics::CallStackProfileParams::Thread::kMax);
+       ++t) {
+    // This should be disabled for all threads.
+    EXPECT_FALSE(config->IsProfilerEnabledForCurrentProcessAndThread(
+        static_cast<metrics::CallStackProfileParams::Thread>(t)))
+        << t;
+  }
+}
