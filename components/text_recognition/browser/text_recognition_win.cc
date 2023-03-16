e1fff9de3b76c10f937bb6c462df4971453d4efb
Simon Hong <shong@brave.com>
2023-03-16 14:40:52 +0900

    Enabled text_recognition and added stub impls on Windows

---
+++ components/text_recognition/browser/text_recognition_win.cc
@@ -0,0 +1,14 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "brave/components/text_recognition/browser/text_recognition.h"
+
+namespace text_recognition {
+
+std::vector<std::string> GetTextFromImage(const SkBitmap& image) {
+  return {};
+}
+
+}  // namespace text_recognition
