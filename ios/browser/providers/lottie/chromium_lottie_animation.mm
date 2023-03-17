4b4f36c407fd88516c2f5df901817b033074c216
Kyle Hickinson <git@kylehickinson.com>
2023-03-17 14:40:38 -0400

    [iOS] Replace Lottie Chromium provider to avoid linking/bundling Lottie

---
+++ ios/browser/providers/lottie/chromium_lottie_animation.mm
@@ -0,0 +1,19 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#import "ios/public/provider/chrome/browser/lottie/lottie_animation_api.h"
+#import "ios/public/provider/chrome/browser/lottie/lottie_animation_configuration.h"
+
+namespace ios {
+namespace provider {
+
+// Generate an instance of LottieAnimation.
+id<LottieAnimation> GenerateLottieAnimation(
+    LottieAnimationConfiguration* config) {
+  return nil;
+}
+
+}  // namespace provider
+}  // namespace ios
