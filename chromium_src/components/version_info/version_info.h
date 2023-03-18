1bc19a9a7ce9897762ee20d3c8774440a8bb78c8
Artem Samoilenko <artem@brave.com>
2023-03-18 17:10:21 -0400

    Make several version_info functions constexpr
    Chromium change:
    https://github.com/chromium/chromium/commit/f630755c514e612256c787f5a73a14775c9bccd7
    
    Make several version_info functions constexpr
    This CL makes most functions in version_info return constexpr
    values.
    
    This allows them to be assigned to constexpr values elsewhere
    in the code.
    
    BUG=None
    
    Change-Id: I45c96a22d22f499d422b7313d6756fbc43610390

---
+++ chromium_src/components/version_info/version_info.h
@@ -0,0 +1,35 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_VERSION_INFO_VERSION_INFO_H_
+#define BRAVE_CHROMIUM_SRC_COMPONENTS_VERSION_INFO_VERSION_INFO_H_
+
+#include "brave/components/version_info/version_info_values.h"
+
+#define GetChannelString GetChannelString_ChromiumImpl
+#define GetVersionNumber GetVersionNumber_Unused
+
+#include "src/components/version_info/version_info.h"
+#undef GetChannelString
+#undef GetVersionNumber
+
+namespace version_info {
+
+constexpr std::string GetVersionNumber() {
+  return constants::kBraveChromiumVersion;
+}
+
+// We use |nightly| instead of |canary|.
+constexpr std::string GetChannelString(Channel channel) {
+  if (channel == Channel::CANARY) {
+    return "nightly";
+  }
+
+  return GetChannelString_ChromiumImpl(channel);
+}
+
+}  // namespace version_info
+
+#endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_VERSION_INFO_VERSION_INFO_H_
