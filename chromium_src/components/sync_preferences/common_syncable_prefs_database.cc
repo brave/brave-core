962bb856289dc2b8b1c32cee68bbe69c990e5a3b
AlexeyBarabash <AlexeyBarabash@users.noreply.github.com>
2023-03-21 16:10:38 +0200

    [CR113] Fix for failed DCHECK at pref_model_associator.cc(457) (#17693)
    Fix for failed DCHECK at pref_model_associator.cc(457)
    
    Add Brave's syncable preferences to alowlist.
    
    Related Chromium's changes are
    
    https://source.chromium.org/chromium/chromium/src/+/776b9ab57cc0c1ee278c42988c09b6738a5ccc40
    [sync] Implement interface for allowlists of syncable prefs
    
    Currently, a pref can be made syncable just by setting the appropriate
    flag. The Sync team would like more control over which prefs get synced.
    To tackle this, this CL comes up with an base interface for an allowlist
    for syncable prefs.
    
    Note:
    1. The associated DCHECK is behind a feature flag, disabled by default.
    2. The allowlists have not been implemented yet and are part of separate
    CLs.
    
    Bug: 1401271
    Change-Id: Icbd1b91f1a733f20e7ab6900f592785a30bc7763
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4224757
    
    and
    
    https://source.chromium.org/chromium/chromium/src/+/da762d4ecf4a2b17d4a77dc18c7f335337aa27ce
    [sync] Create metadata for common syncable prefs
    
    This assigns an ID to each syncable pref. This should allow introducing
    new metrics for prefs sync.
    
    More metadata fields can be added later.
    
    Bug: 1422534
    Change-Id: I2e0aa7f5e33400b42b7ddc08eccba9da23eb74e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4323387

---
+++ chromium_src/components/sync_preferences/common_syncable_prefs_database.cc
@@ -0,0 +1,27 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "components/search_engines/search_engines_pref_names.h"
+// "//components/sync_preferences:common_syncable_prefs_database" already
+// depends on "//components/search_engines"
+
+#define BRAVE_SYNCABLE_PREFS_IDS                    \
+  , kSyncedDefaultPrivateSearchProviderGUID = 1000, \
+    kSyncedDefaultPrivateSearchProviderData = 1001
+
+#define BRAVE_COMMON_SYNCABLE_PREFS_ALLOW_LIST                         \
+  ,                                                                    \
+      {prefs::kSyncedDefaultPrivateSearchProviderGUID,                 \
+       {syncable_prefs_ids::kSyncedDefaultPrivateSearchProviderGUID}}, \
+  {                                                                    \
+    prefs::kSyncedDefaultPrivateSearchProviderData, {                  \
+      syncable_prefs_ids::kSyncedDefaultPrivateSearchProviderData      \
+    }                                                                  \
+  }
+
+#include "src/components/sync_preferences/common_syncable_prefs_database.cc"
+
+#undef BRAVE_COMMON_SYNCABLE_PREFS_ALLOW_LIST
+#undef BRAVE_SYNCABLE_PREFS_IDS
