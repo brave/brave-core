586f864537eea856c04a9731ff6846f7aa1d81bf
AlexeyBarabash <AlexeyBarabash@users.noreply.github.com>
2023-03-21 16:10:38 +0200

    Syncable preferences must be listed in the allowlists.
    Fix for failed DCHECK at pref_model_associator.cc(457)
    
    Add Brave's syncable preferences to alowlist.
    
    Chromium changes:
    
    https://source.chromium.org/chromium/chromium/src/+/776b9ab57cc0c1ee278c42988c09b6738a5ccc40
    
    commit 776b9ab57cc0c1ee278c42988c09b6738a5ccc40
    Author: Ankush Singh <ankushkush@google.com>
    Date:   Wed Feb 8 19:13:40 2023 +0000
    
        [sync] Implement interface for allowlists of syncable prefs
    
        Currently, a pref can be made syncable just by setting the appropriate
        flag. The Sync team would like more control over which prefs get synced.
        To tackle this, this CL comes up with an base interface for an allowlist
        for syncable prefs.
    
        Note:
        1. The associated DCHECK is behind a feature flag, disabled by default.
        2. The allowlists have not been implemented yet and are part of separate
        CLs.
    
        Bug: 1401271
    
    https://source.chromium.org/chromium/chromium/src/+/da762d4ecf4a2b17d4a77dc18c7f335337aa27ce
    commit da762d4ecf4a2b17d4a77dc18c7f335337aa27ce
    Author: Ankush Singh <ankushkush@google.com>
    Date:   Fri Mar 10 12:23:51 2023 +0000
    
        [sync] Create metadata for common syncable prefs
    
        This assigns an ID to each syncable pref. This should allow introducing
        new metrics for prefs sync.
    
        More metadata fields can be added later.
    
        Bug: 1422534
    
    https://chromium.googlesource.com/chromium/src/+/2aa0cf94dfd32e40286ea67fc97802d8436df073
    
    commit 2aa0cf94dfd32e40286ea67fc97802d8436df073
    Author: Marc Treib <treib@chromium.org>
    Date:   Thu Mar 16 11:14:57 2023 +0000
    
        Sync ModelTypeState: Use new initial_sync_state enum
    
        Previous CL https://crrev.com/c/4329031 introduced a new enum
        `initial_sync_state` intended to replace the previous
        `initial_sync_done` flag.
        This CL updates all call sites to consume the new enum instead of the
        old flag, and also updates unit tests to *set* the new enum. (Non-test
        code already sets both of them.)
    
        By itself, this CL changes little, but it unlocks several followups:
        * Recording Sync.NonReflectionUpdateFreshnessPossiblySkewed2 for
          ApplyUpdatesImmediatelyTypes (made possible by properly tracking
          when the initial download is done for these types).
        * Implementing metrics to track the initial download time for
          ApplyUpdatesImmediatelyTypes.
        * Exposing the "download complete?" state to other features.
    
        Bug: 1423338
    
    https://source.chromium.org/chromium/chromium/src/+/fa3e80d6cdabe39beae15590373d51cbcd00afb9
    
    commit fa3e80d6cdabe39beae15590373d51cbcd00afb9
    Author: Ankush Singh <ankushkush@google.com>
    Date:   Thu Mar 9 15:09:33 2023 +0000
    
        [sync] Enforce syncable prefs allowlist
    
        Currently, a pref can be made syncable just by setting the appropriate
        flag. The Sync team would like more control over which prefs get synced.
        To tackle this, sync team has introduced an allowlist for syncable
        preferences.
    
        This CL enables a DCHECK to test the registered preferences against the
        allowlist of syncable preferences.
    
        Bug: 1401271

---
+++ chromium_src/chrome/browser/sync/prefs/chrome_syncable_prefs_database.h
@@ -0,0 +1,19 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_SYNC_PREFS_CHROME_SYNCABLE_PREFS_DATABASE_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_SYNC_PREFS_CHROME_SYNCABLE_PREFS_DATABASE_H_
+
+#include "components/sync_preferences/common_syncable_prefs_database.h"
+#include "components/sync_preferences/syncable_prefs_database.h"
+
+#define GetSyncablePrefMetadata                                             \
+  GetSyncablePrefMetadata_ChromiumImpl(const std::string& pref_name) const; \
+  absl::optional<sync_preferences::SyncablePrefMetadata> GetSyncablePrefMetadata
+
+#include "src/chrome/browser/sync/prefs/chrome_syncable_prefs_database.h"
+#undef GetSyncablePrefMetadata
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_SYNC_PREFS_CHROME_SYNCABLE_PREFS_DATABASE_H_
---
+++ chromium_src/components/sync_preferences/common_syncable_prefs_database.h
@@ -0,0 +1,26 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_SYNC_PREFERENCES_COMMON_SYNCABLE_PREFS_DATABASE_H_
+#define BRAVE_CHROMIUM_SRC_COMPONENTS_SYNC_PREFERENCES_COMMON_SYNCABLE_PREFS_DATABASE_H_
+
+#include "components/sync_preferences/syncable_prefs_database.h"
+
+// To accommodate ChromeSyncablePrefsDatabase::GetSyncablePrefMetadata having
+// GetSyncablePrefMetadata renamed to GetSyncablePrefMetadata_ChromiumImpl, we
+// add two additional methods here. GetSyncablePrefMetadata_ChromiumImpl will
+// just call GetSyncablePrefMetadata and GetSyncablePrefMetadata of this class
+// will become GetSyncablePrefMetadata_ChromiumOriginalImpl.
+#define GetSyncablePrefMetadata                                              \
+  GetSyncablePrefMetadata_ChromiumImpl(const std::string& pref_name) const;  \
+  absl::optional<sync_preferences::SyncablePrefMetadata>                     \
+  GetSyncablePrefMetadata_ChromiumOriginalImpl(const std::string& pref_name) \
+      const;                                                                 \
+  absl::optional<sync_preferences::SyncablePrefMetadata> GetSyncablePrefMetadata
+
+#include "src/components/sync_preferences/common_syncable_prefs_database.h"
+#undef GetSyncablePrefMetadata
+
+#endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_SYNC_PREFERENCES_COMMON_SYNCABLE_PREFS_DATABASE_H_
