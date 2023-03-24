43a250d094284a6ee0113988f1abec6614032ac0
Emerick Rogul <erogul@brave.com>
2023-03-24 16:35:24 -0400

    Upgrade from Chromium 112.0.5615.29 to Chromium 112.0.5615.39 (#17741)
    * Upgrade from Chromium 112.0.5615.29 to Chromium 112.0.5615.39
    
    * Upgrade patches from Chromium 112.0.5615.29 to Chromium 112.0.5615.39
    
    * Update pins list timestamp
    
    ---------
    
    Co-authored-by: brave-builds <brave-builds+devops@brave.com>

--- chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
+++ chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
@@ -535,9 +535,9 @@ bool ParseCertificatesFile(base::StringPiece certs_input,
                            Pinsets* pinsets,
                            base::Time* timestamp) {
   constexpr base::StringPiece brave_certs = R"brave_certs(
-# Last updated: Wed Mar 22 11:43:59 UTC 2023
+# Last updated: Thu Mar 23 19:17:56 UTC 2023
 PinsListTimestamp
-1679485439
+1679599076
 
 # =====BEGIN BRAVE ROOTS ASC=====
 #From https://www.amazontrust.com/repository/
