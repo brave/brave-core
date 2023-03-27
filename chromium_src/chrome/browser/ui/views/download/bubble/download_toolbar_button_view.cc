02f38f78929142421cde40f0cd2acfaf94dc2de3
Simon Hong <shong@brave.com>
2023-03-27 15:10:29 +0900

    Updated toolbar download button style
    fix https://github.com/brave/brave-browser/issues/29324

---
+++ chromium_src/chrome/browser/ui/views/download/bubble/download_toolbar_button_view.cc
@@ -0,0 +1,28 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "brave/browser/ui/color/brave_color_id.h"
+
+#include "chrome/browser/ui/views/download/bubble/download_toolbar_button_view.h"
+
+#define DownloadToolbarButtonView DownloadToolbarButtonViewChromium
+
+#include "src/chrome/browser/ui/views/download/bubble/download_toolbar_button_view.cc"
+
+#undef DownloadToolbarButtonView
+
+SkColor DownloadToolbarButtonView::GetIconColor() const {
+  const DownloadDisplayController::IconInfo icon_info = GetIconInfo();
+
+  // Apply active color only when download is completed and user doesn't
+  // interact with this button.
+  if (icon_info.icon_state == download::DownloadIconState::kComplete &&
+      icon_info.is_active) {
+    return GetColorProvider()->GetColor(kColorBraveDownloadToolbarButtonActive);
+  }
+
+  // Otherwise, always use inactive color.
+  return GetColorProvider()->GetColor(kColorDownloadToolbarButtonInactive);
+}
