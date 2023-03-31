ef5e9736748657db2a9447c12c68471d8aaf1602
brave-builds <brave-builds+devops@brave.com>
2023-03-31 14:38:25 +0000

    Uplift of #17758 (squashed) to beta

--- components/brave_wallet/common/features.cc
+++ components/brave_wallet/common/features.cc
@@ -50,7 +50,12 @@ BASE_FEATURE(kBraveWalletSnsFeature,
 
 BASE_FEATURE(kBraveWalletNftPinningFeature,
              "BraveWalletNftPinning",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+#if BUILDFLAG(IS_ANDROID)
+             base::FEATURE_DISABLED_BY_DEFAULT
+#else
+             base::FEATURE_ENABLED_BY_DEFAULT
+#endif
+);
 
 BASE_FEATURE(kBraveWalletPanelV2Feature,
              "BraveWalletPanelV2",
