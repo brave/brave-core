eae3eaa4f19048e07aa2a5fe7428c297a50cf434
Claudio DeSouza <cdesouza@igalia.com>
2023-03-31 10:09:29 +0100

    Reanchoring ChromeAutocompleteProviderClient overrides
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/6803e1b4a0811753b2dd79a237050223f8fc38d4
    
    commit 6803e1b4a0811753b2dd79a237050223f8fc38d4
    Author: Boris Sazonov <bsazonov@google.com>
    Date:   Fri Mar 17 17:02:05 2023 +0000
    
        Rename AutocompleteProviderClient::GetBookmarkModel
    
        Renames AutocompleteProviderClient::GetBookmarkModel to
        GetLocalOrSyncableBookmarkModel. In subsequent CLs,
        GetAccountBookmarkModel will be added. No behavior change.
    
        Bug: 1404250

--- chromium_src/chrome/browser/autocomplete/chrome_autocomplete_provider_client.h
+++ chromium_src/chrome/browser/autocomplete/chrome_autocomplete_provider_client.h
@@ -10,13 +10,13 @@
 
 #if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
 #include "brave/components/commander/browser/commander_frontend_delegate.h"
-#define GetBookmarkModel       \
-  GetBookmarkModel() override; \
+#define GetLocalOrSyncableBookmarkModel       \
+  GetLocalOrSyncableBookmarkModel() override; \
   commander::CommanderFrontendDelegate* GetCommanderDelegate
 #endif
 
 #include "src/chrome/browser/autocomplete/chrome_autocomplete_provider_client.h"  // IWYU pragma: export
 
-#undef GetBookmarkModel
+#undef GetLocalOrSyncableBookmarkModel
 
 #endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_AUTOCOMPLETE_CHROME_AUTOCOMPLETE_PROVIDER_CLIENT_H_
