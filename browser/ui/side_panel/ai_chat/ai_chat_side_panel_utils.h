7983a2c2924e498495b469f4ddf46e102414a688
Taher <8665427+nullhook@users.noreply.github.com>
2023-04-04 11:45:41 -0700

    Added AIChat sidepanel helper

---
+++ browser/ui/side_panel/ai_chat/ai_chat_side_panel_utils.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2023 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_BROWSER_UI_SIDE_PANEL_AI_CHAT_AI_CHAT_SIDE_PANEL_UTILS_H_
+#define BRAVE_BROWSER_UI_SIDE_PANEL_AI_CHAT_AI_CHAT_SIDE_PANEL_UTILS_H_
+
+#include "content/public/browser/web_contents.h"
+
+class Browser;
+
+namespace ai_chat {
+
+// Move this function to browser_finder.
+// https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/side_panel/companion/companion_side_panel_controller_utils.h;l=19;drc=e87fd2634a1140a87d59c7aa72668d16e4c102c2
+Browser* GetBrowserForWebContents(content::WebContents* web_contents);
+
+}  // namespace ai_chat
+
+#endif  // BRAVE_BROWSER_UI_SIDE_PANEL_AI_CHAT_AI_CHAT_SIDE_PANEL_UTILS_H_
--- browser/ui/webui/ai_chat/ai_chat_ui.cc
+++ browser/ui/webui/ai_chat/ai_chat_ui.cc
@@ -9,6 +9,7 @@
 
 #include "brave/browser/ai_chat/constants.h"
 #include "brave/browser/ai_chat/pref_names.h"
+#include "brave/browser/ui/side_panel/ai_chat/ai_chat_side_panel_utils.h"
 #include "brave/browser/ui/webui/ai_chat/ai_chat_ui_page_handler.h"
 #include "brave/browser/ui/webui/brave_webui_source.h"
 #include "brave/components/ai_chat_ui/grit/ai_chat_ui_generated_map.h"
@@ -16,7 +17,6 @@
 #include "brave/components/l10n/common/localization_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/webui/webui_util.h"
 #include "components/grit/brave_components_resources.h"
 #include "components/prefs/pref_service.h"
@@ -29,7 +29,6 @@ AIChatUI::AIChatUI(content::WebUI* web_ui)
     : ui::UntrustedWebUIController(web_ui),
       profile_(Profile::FromWebUI(web_ui)) {
   DCHECK(profile_);
-  browser_ = chrome::FindBrowserWithProfile(profile_);
 
   // Create a URLDataSource and add resources.
   content::WebUIDataSource* untrusted_source =
@@ -71,12 +70,18 @@ AIChatUI::~AIChatUI() = default;
 
 void AIChatUI::BindInterface(
     mojo::PendingReceiver<ai_chat::mojom::PageHandler> receiver) {
-  page_handler_ = std::make_unique<AIChatUIPageHandler>(
-      browser_->tab_strip_model(), profile_, std::move(receiver));
-
+  // We call ShowUI() before creating the PageHandler object so that
+  // the WebContents is added to a Browser which we can get a reference
+  // to and provide to the PageHandler.
   if (embedder_) {
     embedder_->ShowUI();
   }
+
+  browser_ = ai_chat::GetBrowserForWebContents(web_ui()->GetWebContents());
+  DCHECK(browser_);
+
+  page_handler_ = std::make_unique<AIChatUIPageHandler>(
+      browser_->tab_strip_model(), profile_, std::move(receiver));
 }
 
 std::unique_ptr<content::WebUIController>
