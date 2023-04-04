d70c9278cd558ae27327bd02769f7dc503029a32
sergei <spylogsster@gmail.com>
2023-04-04 11:29:43 +0300

    Move kBraveSimplePolicyMap to a separate file

--- chromium_src/chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chromium_src/chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -4,59 +4,8 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "chrome/browser/policy/configuration_policy_handler_list_factory.h"
-
-#include "brave/components/brave_vpn/common/buildflags/buildflags.h"
-#include "brave/components/constants/pref_names.h"
-#include "brave/components/ipfs/buildflags/buildflags.h"
-#include "brave/components/tor/buildflags/buildflags.h"
-#include "build/build_config.h"
+#include "brave/browser/policy/brave_simple_policy_map.h"
 #include "components/policy/core/browser/configuration_policy_handler.h"
-#include "components/policy/policy_constants.h"
-
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
-#include "brave/components/brave_rewards/common/pref_names.h"
-#include "brave/components/brave_wallet/common/pref_names.h"
-#endif
-
-#if BUILDFLAG(ENABLE_TOR)
-#include "brave/components/tor/pref_names.h"
-#endif
-
-#if BUILDFLAG(ENABLE_IPFS)
-#include "brave/components/ipfs/pref_names.h"
-#endif
-
-#if BUILDFLAG(ENABLE_BRAVE_VPN)
-#include "brave/components/brave_vpn/common/pref_names.h"
-#endif
-
-namespace {
-
-const policy::PolicyToPreferenceMapEntry kBraveSimplePolicyMap[] = {
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
-    {policy::key::kBraveRewardsDisabled,
-     brave_rewards::prefs::kDisabledByPolicy, base::Value::Type::BOOLEAN},
-    {policy::key::kBraveWalletDisabled, brave_wallet::prefs::kDisabledByPolicy,
-     base::Value::Type::BOOLEAN},
-    {policy::key::kBraveShieldsDisabledForUrls,
-     kManagedBraveShieldsDisabledForUrls, base::Value::Type::LIST},
-    {policy::key::kBraveShieldsEnabledForUrls,
-     kManagedBraveShieldsEnabledForUrls, base::Value::Type::LIST},
-#endif
-#if BUILDFLAG(ENABLE_TOR)
-    {policy::key::kTorDisabled, tor::prefs::kTorDisabled,
-     base::Value::Type::BOOLEAN},
-#endif
-#if BUILDFLAG(ENABLE_IPFS)
-    {policy::key::kIPFSEnabled, kIPFSEnabled, base::Value::Type::BOOLEAN},
-#endif
-#if BUILDFLAG(ENABLE_BRAVE_VPN)
-    {policy::key::kBraveVPNDisabled, brave_vpn::prefs::kManagedBraveVPNDisabled,
-     base::Value::Type::BOOLEAN},
-#endif
-};
-
-}  // namespace
 
 #define BuildHandlerList BuildHandlerList_ChromiumImpl
 #include "src/chrome/browser/policy/configuration_policy_handler_list_factory.cc"
