09b823a3dee62c092571b60600c47010e5ad0202
Kyle Hickinson <git@kylehickinson.com>
2023-04-05 16:36:14 -0400

    [iOS] Add a factory to get a BraveWalletIpfsService

--- ios/browser/brave_wallet/brave_wallet_factory_wrappers.mm
+++ ios/browser/brave_wallet/brave_wallet_factory_wrappers.mm
@@ -7,6 +7,7 @@
 
 #include "brave/ios/browser/api/brave_wallet/brave_wallet.mojom.objc+private.h"
 #include "brave/ios/browser/brave_wallet/asset_ratio_service_factory.h"
+#include "brave/ios/browser/brave_wallet/brave_wallet_ipfs_service_factory.h"
 #include "brave/ios/browser/brave_wallet/brave_wallet_service_factory.h"
 #include "brave/ios/browser/brave_wallet/json_rpc_service_factory.h"
 #include "brave/ios/browser/brave_wallet/keyring_service_factory.h"
@@ -114,5 +115,18 @@
   }
   return [[BraveWalletSwapServiceMojoImpl alloc]
       initWithSwapService:std::move(service)];
+}
+@end
+
+@implementation BraveWalletIpfsServiceFactory
++ (nullable id)serviceForBrowserState:(ChromeBrowserState*)browserState {
+  auto service =
+      brave_wallet::BraveWalletIpfsServiceFactory::GetForBrowserState(
+          browserState);
+  if (!service) {
+    return nil;
+  }
+  return [[BraveWalletIpfsServiceMojoImpl alloc]
+      initWithIpfsService:std::move(service)];
 }
 @end
