edfcc46024c63955a40f467d6b2da68237f0ff65
Pete Miller <miller.pete@gmail.com>
2023-04-05 01:22:31 -0700

    Fetch some video transcripts for AI summarization

--- components/ai_chat/ai_chat_tab_helper.cc
+++ components/ai_chat/ai_chat_tab_helper.cc
@@ -6,11 +6,13 @@
 #include "brave/components/ai_chat/ai_chat_tab_helper.h"
 
 #include <queue>
+#include <memory>
 #include <string>
 #include <utility>
 
 #include "base/containers/contains.h"
 #include "base/strings/strcat.h"
+#include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "brave/components/ai_chat/ai_chat.mojom-shared.h"
 #include "brave/components/ai_chat/constants.h"
@@ -20,12 +22,49 @@
 #include "ui/accessibility/ax_node.h"
 #include "ui/accessibility/ax_tree.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "net/base/load_flags.h"
+#include "net/http/http_request_headers.h"
+#include "services/network/public/cpp/resource_request.h"
+#include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "services/network/public/cpp/simple_url_loader.h"
+#include "services/network/public/mojom/url_response_head.mojom.h"
 
 using ai_chat::mojom::CharacterType;
 using ai_chat::mojom::ConversationTurn;
 using ai_chat::mojom::ConversationTurnVisibility;
 
 namespace {
+
+const char16_t kYoutubeTranscriptExtractionScript[] =
+    uR"JS(
+        console.log('Starting youtube transcript extraction')
+        const url = ytplayer.config?.args.raw_player_response.captions.playerCaptionsTracklistRenderer.captionTracks[0].baseUrl
+        console.log('got url', url)
+        url
+    )JS";
+
+net::NetworkTrafficAnnotationTag GetNetworkTrafficAnnotationTag() {
+  return net::DefineNetworkTrafficAnnotation("ai_chat", R"(
+      semantics {
+        sender: "AI Chat"
+        description:
+          "This is used to fetch video transcript"
+          "on behalf of the user interacting with the ChatUI."
+        trigger:
+          "Triggered by user asking for a summary."
+        data:
+          "Will generate a text that attempts to match the user gave it"
+        destination: WEBSITE
+      }
+      policy {
+        cookies_allowed: NO
+        policy_exception_justification:
+          "Not implemented."
+      }
+    )");
+}
+
+
 static const ax::mojom::Role kContentRoles[] = {
     ax::mojom::Role::kHeading,
     ax::mojom::Role::kParagraph,
@@ -211,6 +250,95 @@ void AIChatTabHelper::RequestSummary() {
   ui::AXTreeID tree_id = web_contents()->GetPrimaryMainFrame()->GetAXTreeID();
   content::RenderFrameHost* rfh =
       content::RenderFrameHost::FromAXTreeID(tree_id);
+
+  // Special case for YouTube videos transcripts
+  // TODO(petemill): better hostname compare
+  if (base::CompareCaseInsensitiveASCII(web_contents()->GetLastCommittedURL().host(), "www.youtube.com") == 0) {
+    // TODO(petemill): Refactor to VideoTranscriptFetcher
+    DVLOG(2) << __func__ << " Is YouTube, attempting to extract transcript...";
+    rfh->ExecuteJavaScriptForTests(kYoutubeTranscriptExtractionScript, base::BindOnce([](AIChatTabHelper* instance, base::Value result){
+      DVLOG(2) << "result is: " << base::Value::GetTypeName(result.type());
+      auto* transcript_url_ref = result.GetIfString();
+      if (!transcript_url_ref || transcript_url_ref->empty()) {
+        // TODO(petemill): centralise handle no summary available
+        DVLOG(2) << "result was empty";
+        for (auto& obs : instance->observers_) {
+          obs.OnRequestSummaryFailed();
+        }
+        return;
+      }
+      GURL transcript_url = GURL(*transcript_url_ref);
+      DVLOG(2) << "result was url: " << transcript_url.spec();
+      if (!transcript_url.is_valid()) {
+        // TODO(petemill): centralise handle no summary available
+        DVLOG(2) << "result was invalid.";
+        for (auto& obs : instance->observers_) {
+          obs.OnRequestSummaryFailed();
+        }
+        return;
+      }
+      auto request = std::make_unique<network::ResourceRequest>();
+      request->url = GURL(transcript_url);
+      request->load_flags = net::LOAD_DO_NOT_SAVE_COOKIES;
+      request->credentials_mode = network::mojom::CredentialsMode::kOmit;
+      request->method = net::HttpRequestHeaders::kGetMethod;
+      auto url_loader = network::SimpleURLLoader::Create(
+          std::move(request), GetNetworkTrafficAnnotationTag());
+      url_loader->SetRetryOptions(
+          1, network::SimpleURLLoader::RetryMode::RETRY_ON_5XX |
+                network::SimpleURLLoader::RetryMode::RETRY_ON_NETWORK_CHANGE);
+      url_loader->SetAllowHttpErrorResults(true);
+      auto iter = instance->url_loaders_.insert(instance->url_loaders_.begin(), std::move(url_loader));
+      iter->get()->DownloadToString(
+          instance->web_contents()->GetBrowserContext()
+                                      ->GetDefaultStoragePartition()
+                                      ->GetURLLoaderFactoryForBrowserProcess().get(),
+          // Handle response
+          base::BindOnce([](AIChatTabHelper* instance, SimpleURLLoaderList::iterator iter,
+              std::unique_ptr<std::string> response_body) {
+            auto* loader = iter->get();
+            auto response_code = -1;
+            base::flat_map<std::string, std::string> headers;
+            if (loader->ResponseInfo()) {
+              auto headers_list = loader->ResponseInfo()->headers;
+              if (headers_list) {
+                response_code = headers_list->response_code();
+              }
+            }
+            instance->url_loaders_.erase(iter);
+            // Validate if we get a feed
+            std::string transcript_xml = response_body ? *response_body : "";
+            if (response_code < 200 || response_code >= 300 || transcript_xml.empty()) {
+              DVLOG(1) << __func__ << " invalid video transcript response from url: " << iter->get()->GetFinalURL().spec() << " status: " << response_code;
+              return;
+            }
+            DVLOG(2) << "Got video text: " << transcript_xml;
+            // Prevent indirect prompt injections being sent to the AI model.
+            // TODO(petemill): Abstract prompt injection cleanups to a central place
+            base::ReplaceSubstringsAfterOffset(&transcript_xml, 0, ai_chat::kHumanPrompt,
+                                              "");
+            base::ReplaceSubstringsAfterOffset(&transcript_xml, 0, ai_chat::kAIPrompt, "");
+
+            VLOG(1) << __func__
+                    << " Number of chars in video transcript xml = " << transcript_xml.length()
+                    << "\n";
+
+            std::string summarize_prompt = base::ReplaceStringPlaceholders(
+                l10n_util::GetStringUTF8(IDS_AI_CHAT_SUMMARIZE_VIDEO_PROMPT), {transcript_xml},
+                nullptr);
+
+            // Get summary from API
+            instance->MakeAPIRequestWithConversationHistoryUpdate(
+                {CharacterType::HUMAN, ConversationTurnVisibility::HIDDEN,
+                summarize_prompt});
+          }, base::Unretained(instance),
+          iter),
+          2 * 1024 * 1024);
+    }, this));
+    return;
+  }
+
+  DVLOG(3) << __func__ << " URL host to get summary for is: " << web_contents()->GetLastCommittedURL().host();
 
   if (!rfh) {
     // TODO(petemill): Don't allow the UI to submit requests at this state
