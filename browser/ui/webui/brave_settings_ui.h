d04bbeabd7df12786eeaf0c0ed00d970bd9ac396
Jay Harris <jay.harris@outlook.co.nz>
2023-04-06 12:41:12 +1200

    Update copyright

--- browser/ui/webui/brave_settings_ui.h
+++ browser/ui/webui/brave_settings_ui.h
@@ -1,7 +1,7 @@
-/* Copyright 2019 The Brave Authors. All rights reserved.
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #ifndef BRAVE_BROWSER_UI_WEBUI_BRAVE_SETTINGS_UI_H_
 #define BRAVE_BROWSER_UI_WEBUI_BRAVE_SETTINGS_UI_H_
