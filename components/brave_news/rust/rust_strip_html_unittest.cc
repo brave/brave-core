ad79b20108c8d1922827336e7039674a36dd722a
Jay Harris <jay.harris@outlook.co.nz>
2023-04-12 14:09:15 +1200

    Formatting

--- components/brave_news/rust/rust_strip_html_unittest.cc
+++ components/brave_news/rust/rust_strip_html_unittest.cc
@@ -38,8 +38,8 @@ TEST(RustStripHtml, TagsCanCoverNewLines) {
 }
 
 TEST(RustStripHtml, NonsenseIsIgnored) {
-  auto result = Strip(R"(<this is 
-    a bunch of text>|<pfg somran>Foo</ fwe span>|< fawefaewf>)");
+  auto result = Strip(
+      R"(<this is a bunch of text>|<pfg somran>Foo</ fwe span>|< fawefaewf>)");
   EXPECT_EQ("|Foo|", result);
 }
 
@@ -62,7 +62,8 @@ TEST(RustStripHtml, CantMakeATagWithComments) {
 }
 
 TEST(RustStripHtml, CantCloseATagFromInsideAComment) {
-  EXPECT_EQ("Content", Strip("<dialog <!-- sneaky close tag > --> open=false>Content"));
+  EXPECT_EQ("Content",
+            Strip("<dialog <!-- sneaky close tag > --> open=false>Content"));
 }
 
 TEST(RustStripHtml, CDataIsStripped) {
@@ -97,7 +98,7 @@ TEST(RustStripHtml, NonsenseWithComments) {
 // Some test data from the voca_rs project
 // https://github.com/a-merezhanyi/voca_rs/blob/master/tests/unit/strip.rs
 //
-//MIT License
+// MIT License
 //
 // Copyright (c) 2018-2022 A. Merezhanyi
 //
@@ -108,11 +109,11 @@ TEST(RustStripHtml, NonsenseWithComments) {
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
 //
-// 1. The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+// 1. The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 //
-// 2. Redistributions of source code must retain the above copyright notice, this
-// list of conditions and the following disclaimer.
+// 2. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
 //
 // 3. Redistributions in binary form must reproduce the above copyright notice,
 // this list of conditions and the following disclaimer in the documentation
@@ -126,11 +127,16 @@ TEST(RustStripHtml, NonsenseWithComments) {
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 // ## Acknowledgements:
-// - Voca.js https://github.com/panzerdp/voca `Copyright (c) 2017 Dmitri Pavlutin`
-// - Unidecode https://github.com/chowdhurya/rust-unidecode `Copyright (c) 2015, Amit Chowdhury`
-// - heck https://github.com/withoutboats/heck `Copyright (c) 2018, Saoirse Shipwreckt`
-// - Inflector https://github.com/whatisinternet/inflector `Copyright (c) 2019, Josh Teeter`
-// - Graphite Helpers https://github.com/GrafiteInc/Helpers `Copyright (c) 2020, Matt Lantz`
+// - Voca.js https://github.com/panzerdp/voca `Copyright (c) 2017 Dmitri
+// Pavlutin`
+// - Unidecode https://github.com/chowdhurya/rust-unidecode `Copyright (c) 2015,
+// Amit Chowdhury`
+// - heck https://github.com/withoutboats/heck `Copyright (c) 2018, Saoirse
+// Shipwreckt`
+// - Inflector https://github.com/whatisinternet/inflector `Copyright (c) 2019,
+// Josh Teeter`
+// - Graphite Helpers https://github.com/GrafiteInc/Helpers `Copyright (c) 2020,
+// Matt Lantz`
 // ---------------------------
 
 TEST(RustStripHtml, VocaGeneral) {
