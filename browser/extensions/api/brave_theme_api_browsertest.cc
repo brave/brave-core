61f5e8a74db4fa0548aaba5b0c10b50e4b2f5af3
Claudio DeSouza <cdesouza@igalia.com>
2023-04-12 11:50:36 +0100

    extension_function_test_utils.h deleted upstream
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/97a2b21a6bafbef2f53b79f93f5d88fcb67f1d3c
    
    commit 97a2b21a6bafbef2f53b79f93f5d88fcb67f1d3c
    Author: Claudio DeSouza <cdesouza@igalia.com>
    Date:   Wed Apr 5 18:33:54 2023 +0000
    
        Delete extension_api_unittest.h/cc
    
        All uses of this header have been migrated away. This CL deletes
        extension_api_unittest.h/cc, and removes the polyfill in place to allow
        the migration to an enum class in api_test_utils.h, replacing it with an
        actual enum class.
    
        Additionally, RunFunctionAndReturnSingleValue is getting renamed back to
        RunFunctionAndReturnValue, for better readability.
    
        Bug: 394840

--- browser/extensions/api/brave_theme_api_browsertest.cc
+++ browser/extensions/api/brave_theme_api_browsertest.cc
@@ -6,14 +6,16 @@
 #include "brave/browser/extensions/api/brave_theme_api.h"
 #include "brave/browser/themes/brave_dark_mode_utils.h"
 #include "brave/components/constants/pref_names.h"
-#include "chrome/browser/extensions/extension_function_test_utils.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/test/browser_test.h"
+#include "extensions/browser/api_test_utils.h"
 #include "extensions/common/extension_builder.h"
 
 using extensions::api::BraveThemeGetBraveThemeTypeFunction;
-using extension_function_test_utils::RunFunctionAndReturnSingleResult;
+using extensions::api_test_utils::RunFunctionAndReturnSingleResult;
 
 class BraveThemeAPIBrowserTest : public InProcessBrowserTest {
  public:
@@ -41,8 +43,8 @@ IN_PROC_BROWSER_TEST_F(BraveThemeAPIBrowserTest,
   scoped_refptr<BraveThemeGetBraveThemeTypeFunction> get_function(
       new BraveThemeGetBraveThemeTypeFunction());
   get_function->set_extension(extension().get());
-  std::unique_ptr<base::Value> value;
-  value = RunFunctionAndReturnSingleResult(get_function.get(),
-                                           std::string("[]"), browser());
+  auto value = RunFunctionAndReturnSingleResult(
+      get_function.get(), std::string("[]"), browser()->profile());
+  ASSERT_TRUE(value);
   EXPECT_EQ(value->GetString(), "Light");
 }
