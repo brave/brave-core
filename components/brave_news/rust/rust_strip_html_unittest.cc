2bbb51c382a6aaa8f85926df5372ce7da1adabb8
Jay Harris <jay.harris@outlook.co.nz>
2023-04-12 12:30:19 +1200

    Add license header

--- components/brave_news/rust/rust_strip_html_unittest.cc
+++ components/brave_news/rust/rust_strip_html_unittest.cc
@@ -86,14 +86,43 @@ TEST(RustStripHtml, NonsenseWithComments) {
 }
 
 // ---------------------------
-// voca_rs test cases
-// we include a subset of the voca_rs tests for stripping tags.
-// Note: The EXPECTED/ACTUAL result is backwards in these tests to make
-// copy/pasting from voca_rs easier.
-// Note: We have a much less forgiving approach to HTML sanitation than Voca, so
-// in a few cases our output is different to theirs (for example, we never
-// output a '>' or a '<' character).
+// Some test data from the voca_rs project
 // https://github.com/a-merezhanyi/voca_rs/blob/master/tests/unit/strip.rs
+//
+//MIT License
+//
+// Copyright (c) 2018-2022 A. Merezhanyi
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// 1. The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// 2. Redistributions of source code must retain the above copyright notice, this
+// list of conditions and the following disclaimer.
+//
+// 3. Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// ## Acknowledgements:
+// - Voca.js https://github.com/panzerdp/voca `Copyright (c) 2017 Dmitri Pavlutin`
+// - Unidecode https://github.com/chowdhurya/rust-unidecode `Copyright (c) 2015, Amit Chowdhury`
+// - heck https://github.com/withoutboats/heck `Copyright (c) 2018, Saoirse Shipwreckt`
+// - Inflector https://github.com/whatisinternet/inflector `Copyright (c) 2019, Josh Teeter`
+// - Graphite Helpers https://github.com/GrafiteInc/Helpers `Copyright (c) 2020, Matt Lantz`
 // ---------------------------
 
 TEST(RustStripHtml, VocaGeneral) {
