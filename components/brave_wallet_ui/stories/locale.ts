723f3cb461f511489cf1d83e589bd575419ccbbb
William Muli <willi.wambu@gmail.com>
2023-04-17 21:54:31 +0300

    Update NFT auto-discovery modal text

--- components/brave_wallet_ui/stories/locale.ts
+++ components/brave_wallet_ui/stories/locale.ts
@@ -888,7 +888,7 @@ provideStrings({
   
   // NFT auto discovery modal
   braveWalletEnableNftAutoDiscoveryModalHeader: 'Want your NFTs displayed automatically?',
-  braveWalletEnableNftAutoDiscoveryModalDescription: 'Brave Wallet can uses a third-party service to automatically display your NFTs. Brave will share your public addresses with $1SimpleHash$2 to provide this service. $3Learn more.$4',
+  braveWalletEnableNftAutoDiscoveryModalDescription: 'Brave Wallet can use a third-party service to automatically display your NFTs. Brave will share your wallet addresses with $1SimpleHash$2 to provide this service. $3Learn more.$4',
   braveWalletEnableNftAutoDiscoveryModalConfirm: 'Yes, proceed',
   braveWalletEnableNftAutoDiscoveryModalCancel: 'No thanks, I\'ll do it manually'
 })
