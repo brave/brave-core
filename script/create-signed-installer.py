443401569aeb5b1bdb3826b5f234f66043dce97a
Michael Herrmann <michael@herrmann.io>
2023-04-18 09:03:08 +0200

    Remove brave_installer_{w}_{x}_{y}_{z}.exe
    It is the same as brave_installer.exe. CI has already been updated to only use
    the latter.

--- script/create-signed-installer.py
+++ script/create-signed-installer.py
@@ -1,5 +1,4 @@
 import argparse
-import os
 import shutil
 import sys
 from sign_binaries import sign_binary
@@ -7,32 +6,23 @@ from sign_binaries import sign_binary
 
 def main():
     args = parse_args()
-    root_out_dir = os.path.join(
-        os.path.dirname(os.path.dirname(os.path.dirname(
-            os.path.realpath(__file__)))),
-        args.root_out_dir[0])
-    create_signed_installer(
-        root_out_dir, args.brave_installer_exe[0], args.skip_signing)
+    create_signed_installer(args.mini_installer_exe, args.brave_installer_exe,
+                            args.skip_signing)
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Create signed installer')
-    parser.add_argument('--root_out_dir', nargs=1)
-    parser.add_argument('--brave_installer_exe', nargs=1)
+    parser.add_argument('mini_installer_exe')
+    parser.add_argument('brave_installer_exe')
     parser.add_argument('--skip_signing', action='store_true')
     return parser.parse_args()
 
 
-def create_signed_installer(root_out_dir, brave_installer_exe, skip_signing, env=None):
-    installer_file = os.path.join(root_out_dir, 'brave_installer.exe')
-    shutil.copyfile(os.path.join(root_out_dir, 'mini_installer.exe'),
-                    installer_file)
+def create_signed_installer(mini_installer_exe, brave_installer_exe,
+                            skip_signing):
+    shutil.copyfile(mini_installer_exe, brave_installer_exe)
     if not skip_signing:
-        sign_binary(installer_file)
-    # Copy signed installer to version appended name
-    installer_file_with_version = os.path.join(root_out_dir,
-                                               brave_installer_exe)
-    shutil.copyfile(installer_file, installer_file_with_version)
+        sign_binary(brave_installer_exe)
 
 
 if __name__ == '__main__':
