c62b8657f63a0ed709cbc3eb5b0fcfde14bcc38f
Michał Buczek <michal.buczek@protonmail.com>
2023-04-19 18:58:42 +0200

    No Bug: Use correct table name for two Wallet localized strings. (#7290)

--- Sources/BraveWallet/WalletStrings.swift
+++ Sources/BraveWallet/WalletStrings.swift
@@ -3906,14 +3906,14 @@ extension Strings {
     // Unstoppable Domains
     public static let udDomainInterstitialPageTitle = NSLocalizedString(
       "wallet.udDomainInterstitialPageTitle",
-      tableName: "BraveShared",
+      tableName: "BraveWallet",
       bundle: .module,
       value: "Enable support of Unstoppable Domains in Brave?",
       comment: "Title displayed when users chose Brave to ask them if they want the Unstoppable Domains domain to be resolved every time they enter one."
     )
     public static let udDomainInterstitialPageDescription = NSLocalizedString(
       "wallet.udDomainInterstitialPageDescription",
-      tableName: "BraveShared",
+      tableName: "BraveWallet",
       bundle: .module,
       value: "Brave will be using Infura to resolve .crypto (and also %@) domain names that are on Unstoppable Domains. Brave hides your IP address. If you enable this, Infura will see that someone is trying to visit these domains but nothing else. See Infura's <a href=%@>%@</a> and <a href=%@>%@</a>.",
       comment: "Description displayed when users chose Brave to ask them if they want the Unstoppable Domains to be resolved every time they enter one. The first '%@' will be replaced with a list of supported TLDs like '.x' or '.bitcoin'. The second '%@' be replaced with a link to Infura's terms of use page. The third '%@' will be replaced with the value of 'Web3DomainInterstitialPageTAndU'. The fourth '%@' will be replaced with a link to Infura's privacy policy page. The last '%@' will be replaced with the value of 'Web3DomainInterstitialPagePrivacyPolicy'."
