506a5b25dc8c80ecd8f777d9abd985d6e0169d11
Terry Mancey <terry.mancey@icloud.com>
2023-04-22 08:21:43 +0100

    Fix Brave Ads iOS initialization

--- ios/browser/api/ads/brave_ads.mm
+++ ios/browser/api/ads/brave_ads.mm
@@ -189,6 +189,15 @@ brave_ads::mojom::DBCommandResponseInfoPtr RunDBTransactionOnTaskRunner(
            selector:@selector(applicationDidBackground)
                name:UIApplicationDidEnterBackgroundNotification
              object:nil];
+
+    const auto dbPath = base::SysNSStringToUTF8([self adsDatabasePath]);
+    adsDatabase = new brave_ads::Database(base::FilePath(dbPath));
+
+    adEventHistory = new brave_ads::AdEventHistory();
+
+    adsClient = new AdsClientIOS(self);
+    adsClientNotifier = new brave_ads::AdsClientNotifier();
+    ads = brave_ads::Ads::CreateInstance(adsClient);
   }
   return self;
 }
@@ -237,14 +246,6 @@ brave_ads::mojom::DBCommandResponseInfoPtr RunDBTransactionOnTaskRunner(
     return;
   }
 
-  const auto dbPath = base::SysNSStringToUTF8([self adsDatabasePath]);
-  adsDatabase = new brave_ads::Database(base::FilePath(dbPath));
-
-  adEventHistory = new brave_ads::AdEventHistory();
-
-  adsClient = new AdsClientIOS(self);
-  adsClientNotifier = new brave_ads::AdsClientNotifier();
-  ads = brave_ads::Ads::CreateInstance(adsClient);
   auto cppSysInfo =
       sysInfo ? sysInfo.cppObjPtr : brave_ads::mojom::SysInfo::New();
   ads->SetSysInfo(std::move(cppSysInfo));
