a127921404964b1457d65afbf9af111b16aa11d9
Claudio DeSouza <cdesouza@igalia.com>
2023-04-24 16:40:04 +0100

    sync_browser_proxy.js given absolute path
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/c5a83191c11bbee94e387f0a90415af26ccc0859
    
    commit c5a83191c11bbee94e387f0a90415af26ccc0859
    Author: dpapad <dpapad@chromium.org>
    Date:   Fri Apr 14 18:50:08 2023 +0000
    
        Settings: Move shared proxies to the "limited sharing" mechanism.
    
        Bug: 1431477

--- browser/resources/settings/brave_appearance_page/tabs.ts
+++ browser/resources/settings/brave_appearance_page/tabs.ts
@@ -3,13 +3,16 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js'
-import {I18nMixin, I18nMixinInterface} from 'chrome://resources/cr_elements/i18n_mixin.js'
-import {PrefsMixin, PrefsMixinInterface} from 'chrome://resources/cr_components/settings_prefs/prefs_mixin.js';
 import '../settings_shared.css.js'
 import '../settings_vars.css.js'
+
+import {PrefsMixin, PrefsMixinInterface} from 'chrome://resources/cr_components/settings_prefs/prefs_mixin.js';
+import {I18nMixin, I18nMixinInterface} from 'chrome://resources/cr_elements/i18n_mixin.js'
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js'
+
+import {loadTimeData} from '../i18n_setup.js'
+
 import {getTemplate} from './tabs.html.js'
-import { loadTimeData } from '../i18n_setup.js'
 
 const SettingsBraveAppearanceTabsElementBase = PrefsMixin(I18nMixin(PolymerElement)) as {
   new (): PolymerElement & I18nMixinInterface & PrefsMixinInterface
