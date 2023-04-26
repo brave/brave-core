cf2e0423113982f2485201161d283ba90c73cc61
Claudio DeSouza <cdesouza@igalia.com>
2023-04-26 17:13:41 +0100

    settings_dropdown_menu.js given absolute path
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/c0936f3e627453c2aea06a747de40f275f2d1d86
    
    commit c0936f3e627453c2aea06a747de40f275f2d1d86
    Author: dpapad <dpapad@chromium.org>
    Date:   Tue Apr 25 17:31:02 2023 +0000
    
        Settings: Move settings-dropdown-menu to "limited sharing".
    
        Bug: 1431477

--- browser/resources/settings/brave_web3_domains_page/brave_web3_domains_page.ts
+++ browser/resources/settings/brave_web3_domains_page/brave_web3_domains_page.ts
@@ -3,10 +3,9 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
+import {DropdownMenuOptionList} from '/shared/settings/controls/settings_dropdown_menu.js';
 import {PrefsMixin, PrefsMixinInterface} from 'chrome://resources/cr_components/settings_prefs/prefs_mixin.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js'
-
-import {DropdownMenuOptionList} from '../controls/settings_dropdown_menu.js';
 
 import {BraveWeb3DomainsBrowserProxyImpl} from './brave_web3_domains_browser_proxy.js'
 import {getTemplate} from './brave_web3_domains_page.html.js'
