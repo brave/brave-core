32d3ad83aeb7bbe01bea830278d161bcc914339f
Claudio DeSouza <cdesouza@igalia.com>
2023-04-26 17:11:48 +0100

    kOmniboxTriggerForNoStatePrefetch moved to browser header
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/9dbc62d1288090bbaf228d94880fa1f4c1f2f04d
    
    commit 9dbc62d1288090bbaf228d94880fa1f4c1f2f04d
    Author: lingqi <lingqi@chromium.org>
    Date:   Thu Apr 20 02:38:51 2023 +0000
    
        Prerender: move feature flags from chrome/common to chrome/browser
    
        Thanks to I7565b5db89758450ab618609518e0f778d9139e2, prerender
        feature flags in the chrome layer are no longer used by renderer
        processes, and we can move them to the browser dir to make things
        simpler.
    
        Bug: n/a

--- chromium_src/chrome/common/chrome_features.cc
+++ chromium_src/chrome/common/chrome_features.cc
@@ -16,7 +16,6 @@ namespace features {
 OVERRIDE_FEATURE_DEFAULT_STATES({{
     {kHttpsFirstModeV2, base::FEATURE_ENABLED_BY_DEFAULT},
     {kKAnonymityService, base::FEATURE_DISABLED_BY_DEFAULT},
-    {kOmniboxTriggerForNoStatePrefetch, base::FEATURE_DISABLED_BY_DEFAULT},
     {kSCTAuditing, base::FEATURE_DISABLED_BY_DEFAULT},
 #if !BUILDFLAG(IS_ANDROID)
     {kTrustSafetySentimentSurvey, base::FEATURE_DISABLED_BY_DEFAULT},
