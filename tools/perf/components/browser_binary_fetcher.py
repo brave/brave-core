1cbfe00013f0d854b217e5ab5438108bbe7d066f
Mikhail <matuchin@brave.com>
2023-04-26 18:16:58 +0600

    Support Brave in android perf tests (#18097)
    * Support Brave in android perf tests
    * Fix review issues
    * Fix formatting
    * Add some android configs
    * Fix win32 => windows

--- tools/perf/components/browser_binary_fetcher.py
+++ tools/perf/components/browser_binary_fetcher.py
@@ -8,7 +8,43 @@ import os
 import re
 from typing import Tuple, Optional
 
+import components.path_util as path_util
 from components.browser_type import BrowserType, BraveVersion
+from components.perf_test_utils import GetProcessOutput
+
+with path_util.SysPath(path_util.GetTelemetryDir()):
+  from telemetry.internal.backends import android_browser_backend_settings  # pylint: disable=import-error
+
+
+class BrowserBinary:
+  binary_path: Optional[str] = None
+  android_package: Optional[str] = None
+  telemetry_browser_type: Optional[str] = None
+
+  @classmethod
+  def from_binary(cls, binary_path: str) -> 'BrowserBinary':
+    binary = BrowserBinary()
+    binary.binary_path = binary_path
+    return binary
+
+  @classmethod
+  def from_android_package(cls, android_package: str) -> 'BrowserBinary':
+    binary = BrowserBinary()
+    binary.android_package = android_package
+    for b in android_browser_backend_settings.ANDROID_BACKEND_SETTINGS:
+      if b.package == android_package:
+        binary.telemetry_browser_type = b.browser_type
+    if binary.telemetry_browser_type is None:
+      raise RuntimeError('No matching browser-type found')
+
+    return binary
+
+  def __str__(self) -> str:
+    if self.binary_path:
+      return self.binary_path
+    if self.android_package:
+      return 'package:' + self.android_package
+    return '<empty>'
 
 
 def ParseTarget(target: str) -> Tuple[Optional[BraveVersion], str]:
@@ -21,11 +57,83 @@ def ParseTarget(target: str) -> Tuple[Optional[BraveVersion], str]:
   return tag, location
 
 
+def _GetPackageName(apk_path: str) -> str:
+  aapt2 = os.path.join(path_util.GetSrcDir(), 'third_party',
+                       'android_build_tools', 'aapt2', 'aapt2')
+  assert (apk_path.endswith('.apk'))
+  _, aapt2_info = GetProcessOutput([aapt2, 'dump', 'badging', apk_path],
+                                   check=True)
+  package_match = re.search(r'package: name=\'((?:\w|\.)+)\'', aapt2_info)
+  assert (package_match is not None)
+  return package_match.group(1)
+
+
+def _GetPackageVersion(package: str) -> str:
+  _, dump_info = GetProcessOutput(
+      [path_util.GetAdbPath(), 'shell', 'dumpsys', 'package', package],
+      check=True)
+  version_match = re.search(r'versionName=((?:\w|\.)+)', dump_info)
+  assert (version_match is not None)
+  return version_match.group(1)
+
+
+def _InstallApk(apk_path: str, browser_type: BrowserType,
+                tag: Optional[BraveVersion]) -> str:
+  assert (apk_path.endswith('.apk'))
+  package = _GetPackageName(apk_path)
+
+  adb = path_util.GetAdbPath()
+
+  GetProcessOutput([adb, 'uninstall', package])
+  GetProcessOutput([adb, 'install', apk_path], check=True)
+
+  GetProcessOutput([
+      adb, 'shell', 'pm', 'grant', package,
+      'android.permission.POST_NOTIFICATIONS'
+  ],
+                   check=True)
+
+  if browser_type.GetName().startswith('brave'):
+    version = 'v' + _GetPackageVersion(package)
+    if version != tag.__str__():
+      raise RuntimeError(f'Version mismatch: tag {tag}, installed {version}')
+
+  return package
+
+
+def _PostInstallAndroidSetup(package: str):
+  GetProcessOutput([
+      path_util.GetAdbPath(), 'shell', 'pm', 'grant', package,
+      'android.permission.POST_NOTIFICATIONS'
+  ],
+                   check=True)
+
+
 def PrepareBinary(out_dir: str, tag: Optional[BraveVersion],
-                  location: Optional[str], browser_type: BrowserType) -> str:
+                  location: Optional[str], browser_type: BrowserType,
+                  target_os: str) -> BrowserBinary:
+  binary_location = None
+  package = None
   if location:  # local binary
     if os.path.exists(location):
-      return location
-    raise RuntimeError(f'{location} doesn\'t exist')
-  assert (tag is not None)
-  return browser_type.DownloadBrowserBinary(tag, out_dir)
+      binary_location = location
+    elif location.startswith('package:') and target_os == 'android':
+      package = location[len('package:'):]
+    else:
+      raise RuntimeError(f'{location} doesn\'t exist')
+  else:
+    assert (tag is not None)
+    binary_location = browser_type.DownloadBrowserBinary(
+        tag, out_dir, target_os)
+
+  if target_os != 'android':
+    assert (binary_location)
+    return BrowserBinary.from_binary(binary_location)
+
+  if package is None:
+    assert (binary_location)
+    assert (binary_location.endswith('.apk'))
+    package = _InstallApk(binary_location, browser_type, tag)
+
+  _PostInstallAndroidSetup(package)
+  return BrowserBinary.from_android_package(package)
