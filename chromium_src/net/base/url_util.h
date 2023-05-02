7729fbb9682090bd4398a44859b7d7a252410f7e
Arthur Edelstein <arthuredelstein@gmail.com>
2023-05-01 22:41:01 -0700

    license year

--- chromium_src/net/base/url_util.h
+++ chromium_src/net/base/url_util.h
@@ -1,8 +1,7 @@
-// Copyright (c) 2013 The Brave Authors. All rights reserved.
+// Copyright (c) 2023 The Brave Authors. All rights reserved.
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
-
 
 #ifndef BRAVE_CHROMIUM_SRC_NET_BASE_URL_UTIL_H_
 #define BRAVE_CHROMIUM_SRC_NET_BASE_URL_UTIL_H_
