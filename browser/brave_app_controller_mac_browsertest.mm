41a982dae984295c779ef69377711a7641397be8
Simon Hong <shong@brave.com>
2023-05-03 12:39:49 +0900

    Added browser test to check normal window has valid bookmark items
    after private window is closed.

--- browser/brave_app_controller_mac_browsertest.mm
+++ browser/brave_app_controller_mac_browsertest.mm
@@ -19,14 +19,23 @@
 #include "brave/browser/brave_browser_features.h"
 #include "brave/browser/ui/views/frame/brave_browser_view.h"
 #include "chrome/app/chrome_command_ids.h"
+#include "chrome/browser/bookmarks/bookmark_model_factory.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window.h"
+#include "chrome/browser/ui/cocoa/bookmarks/bookmark_menu_bridge.h"
 #include "chrome/browser/ui/location_bar/location_bar.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
+#include "components/bookmarks/browser/bookmark_model.h"
+#include "components/bookmarks/browser/bookmark_utils.h"
+#include "components/bookmarks/test/bookmark_test_helpers.h"
 #include "components/omnibox/browser/omnibox_view.h"
 #include "content/public/test/browser_test.h"
+
+using bookmarks::BookmarkModel;
+using bookmarks::BookmarkNode;
 
 namespace {
 
@@ -37,6 +46,13 @@ class BraveAppControllerBrowserTest : public InProcessBrowserTest {
   BraveAppControllerBrowserTest() {
     features_.InitWithFeatureState(features::kBraveCopyCleanLinkByDefault,
                                    true);
+  }
+
+  BookmarkModel* WaitForBookmarkModel(Profile* profile) {
+    BookmarkModel* bookmark_model =
+        BookmarkModelFactory::GetForBrowserContext(profile);
+    bookmarks::test::WaitForBookmarkModelToLoad(bookmark_model);
+    return bookmark_model;
   }
 
  private:
@@ -196,6 +212,54 @@ IN_PROC_BROWSER_TEST_F(BraveAppControllerBrowserTest,
   [ac menuNeedsUpdate:[clean_link_menu_item menu]];
   base::RunLoop().RunUntilIdle();
   EXPECT_TRUE([clean_link_menu_item isHidden]);
+}
+
+IN_PROC_BROWSER_TEST_F(BraveAppControllerBrowserTest,
+                       BookmarkItemsFromMenuBarTest) {
+  AppController* ac =
+      base::mac::ObjCCastStrict<AppController>([NSApp delegate]);
+  [ac mainMenuCreated];
+  [ac setLastProfile:browser()->profile()];
+
+  // Added one bookmark item.
+  constexpr char kPersistBookmarkURL[] = "http://www.cnn.com/";
+  constexpr char16_t kPersistBookmarkTitle[] = u"CNN";
+  BookmarkModel* bookmark_model = WaitForBookmarkModel(browser()->profile());
+  bookmarks::AddIfNotBookmarked(bookmark_model, GURL(kPersistBookmarkURL),
+                                kPersistBookmarkTitle);
+
+  // Update menubar's bookmark menu to make it includes above item.
+  NSMenu* normal_window_submenu = [ac bookmarkMenuBridge]->BookmarkMenu();
+  [[normal_window_submenu delegate] menuNeedsUpdate:normal_window_submenu];
+
+  // Total 5 items - basic 3 items(Bookmark Manager, Bookmark This Tab... and
+  // Bookmark All Tabs..), separator and bookmark item. and check last item is
+  // bookmark item.
+  EXPECT_EQ(5, [normal_window_submenu numberOfItems]);
+  EXPECT_EQ(
+      std::u16string(kPersistBookmarkTitle),
+      base::SysNSStringToUTF16([[normal_window_submenu itemAtIndex:4] title]));
+
+  // Create private browser and check bookmark menubar has same items.
+  auto* private_browser = CreateIncognitoBrowser(browser()->profile());
+  [ac setLastProfile:private_browser->profile()];
+  NSMenu* private_browser_submenu = [ac bookmarkMenuBridge]->BookmarkMenu();
+  [[private_browser_submenu delegate] menuNeedsUpdate:private_browser_submenu];
+  EXPECT_EQ(5, [private_browser_submenu numberOfItems]);
+  EXPECT_EQ(std::u16string(kPersistBookmarkTitle),
+            base::SysNSStringToUTF16(
+                [[private_browser_submenu itemAtIndex:4] title]));
+
+  // Close private browser and check bookmark menubar still has same items.
+  chrome::CloseWindow(private_browser);
+  ui_test_utils::WaitForBrowserToClose();
+
+  [ac setLastProfile:browser()->profile()];
+  [[normal_window_submenu delegate] menuNeedsUpdate:normal_window_submenu];
+  EXPECT_EQ(5, [normal_window_submenu numberOfItems]);
+  EXPECT_EQ(
+      std::u16string(kPersistBookmarkTitle),
+      base::SysNSStringToUTF16([[normal_window_submenu itemAtIndex:4] title]));
 }
 
 }  // namespace
