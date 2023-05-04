97896010c2e3566b667411cf372625e2885b8839
Pavneet-Sing <psingh@brave.com>
2023-05-04 13:44:32 +0530

    Uplift of #18343 (squashed) to beta

--- android/java/org/chromium/chrome/browser/crypto_wallet/fragments/SwapBottomSheetDialogFragment.java
+++ android/java/org/chromium/chrome/browser/crypto_wallet/fragments/SwapBottomSheetDialogFragment.java
@@ -22,38 +22,33 @@ import androidx.fragment.app.FragmentTransaction;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 import org.chromium.base.Log;
+import org.chromium.brave_wallet.mojom.BraveWalletConstants;
 import org.chromium.brave_wallet.mojom.CoinType;
 import org.chromium.brave_wallet.mojom.NetworkInfo;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.app.BraveActivity;
+import org.chromium.chrome.browser.app.domain.WalletModel;
 import org.chromium.chrome.browser.crypto_wallet.activities.BuySendSwapActivity;
+import org.chromium.chrome.browser.crypto_wallet.util.JavaUtils;
 import org.chromium.chrome.browser.crypto_wallet.util.Utils;
 import org.chromium.chrome.browser.crypto_wallet.util.WalletConstants;
 
 public class SwapBottomSheetDialogFragment
         extends BottomSheetDialogFragment implements View.OnClickListener {
     public static final String TAG_FRAGMENT = SwapBottomSheetDialogFragment.class.getName();
+    private static final String TAG = "BSS-bottom-dialog";
     private LinearLayout mBuyLayout;
     private LinearLayout mSendLayout;
     private LinearLayout mSwapLayout;
     private NetworkInfo mNetworkInfo;
-    private boolean mIsSwapSupported;
-    private boolean mIsCustomNetwork;
+    private WalletModel mWalletModel;
 
-    public SwapBottomSheetDialogFragment(boolean isSwapSupported) {
-        mIsSwapSupported = isSwapSupported;
-    }
-
-    public static SwapBottomSheetDialogFragment newInstance(boolean isSwapSupported) {
-        return new SwapBottomSheetDialogFragment(isSwapSupported);
-    }
-
-    public void setIsCustomNetwork(boolean isCustomNetwork) {
-        mIsCustomNetwork = isCustomNetwork;
+    public static SwapBottomSheetDialogFragment newInstance() {
+        return new SwapBottomSheetDialogFragment();
     }
 
     public void setNetwork(NetworkInfo networkInfo) {
         mNetworkInfo = networkInfo;
-        updateViewState();
     }
 
     @Override
@@ -70,6 +65,17 @@ public class SwapBottomSheetDialogFragment
             transaction.commitAllowingStateLoss();
         } catch (IllegalStateException e) {
             Log.e("SwapBottomSheetDialogFragment", e.getMessage());
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            mWalletModel = BraveActivity.getBraveActivity().getWalletModel();
+        } catch (BraveActivity.BraveActivityNotFoundException e) {
+            Log.e(TAG, "onCreate ", e);
+            e.printStackTrace();
         }
     }
 
@@ -90,27 +96,6 @@ public class SwapBottomSheetDialogFragment
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-        updateViewState();
-    }
-
-    private void updateViewState() {
-        if (getView() == null) return;
-        if (!mIsCustomNetwork
-                && (mNetworkInfo.coin == CoinType.ETH || mNetworkInfo.coin == CoinType.SOL)
-                && Utils.allowSwap(mNetworkInfo.chainId)) {
-            mSwapLayout.setVisibility(View.VISIBLE);
-        } else {
-            mSwapLayout.setVisibility(View.GONE);
-        }
-
-        if (!Utils.allowBuy(mNetworkInfo.chainId)) {
-            mBuyLayout.setVisibility(View.GONE);
-        }
-    }
-
-    @Override
     public void onClick(View view) {
         BuySendSwapActivity.ActivityType activityType = BuySendSwapActivity.ActivityType.BUY;
         if (view == mSendLayout) {
@@ -118,6 +103,18 @@ public class SwapBottomSheetDialogFragment
         } else if (view == mSwapLayout) {
             activityType = BuySendSwapActivity.ActivityType.SWAP;
         }
+
+        if (!JavaUtils.anyNull(mWalletModel, mNetworkInfo)) {
+            BuySendSwapActivity.ActivityType finalActivityType = activityType;
+            mWalletModel.getNetworkModel().setNetwork(
+                    mNetworkInfo, isSelected -> { openBssAndDismiss(finalActivityType); });
+            return;
+        }
+        openBssAndDismiss(activityType);
+    }
+
+    // Open buy send swap (BSS)
+    private void openBssAndDismiss(BuySendSwapActivity.ActivityType activityType) {
         Utils.openBuySendSwapActivity(getActivity(), activityType);
         dismiss();
     }
