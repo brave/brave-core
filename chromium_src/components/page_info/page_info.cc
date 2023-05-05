6ea98434f0cf5da8f59de4fc41bb407972af3bbe
Claudio DeSouza <cdesouza@igalia.com>
2023-05-05 12:51:06 +0100

    Reanchoring PageInfo overrides
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/7d6587dbf1411cea1359c1c3c2a106caa0256070
    
    commit 7d6587dbf1411cea1359c1c3c2a106caa0256070
    Author: Christian Dullweber <dullweber@chromium.org>
    Date:   Wed Apr 26 09:40:40 2023 +0000
    
        SAA: Add StorageAccess permissions to page_info
    
        Add StorageAccess permissions to pageinfo permission list. Pass
        information about embedded URLs to allow changing these permissions
        later on. This does not properly display the permission yet. The UI
        will be updated in a separate CL.
    
        Bug: 1433644

--- chromium_src/components/page_info/page_info.cc
+++ chromium_src/components/page_info/page_info.cc
@@ -3,9 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#define BRAVE_PAGE_INFO_SHOULD_SHOW_PERMISSION                     \
-  if (!delegate_->BraveShouldShowPermission(permission_info.type)) \
-    continue;
+#define BRAVE_PAGE_INFO_SHOULD_SHOW_PERMISSION          \
+  if (!delegate_->BraveShouldShowPermission(info.type)) \
+    return false;
 
 #include "src/components/page_info/page_info.cc"
 #undef BRAVE_PAGE_INFO_SHOULD_SHOW_PERMISSION
