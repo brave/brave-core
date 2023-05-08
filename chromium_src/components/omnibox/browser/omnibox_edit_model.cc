22b9116d2fd2f5815b59f4acb2d579ec64bb2db6
Jay Harris <jay.harris@outlook.co.nz>
2023-05-08 12:08:06 +1200

    Fix obsoleted OmniboxView overrides
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/7ddbf0f4f03ac2e58ffac0c4ba590461e978ee4d
    
    commit 7ddbf0f4f03ac2e58ffac0c4ba590461e978ee4d
    Author: Moe Ahmadi <mahmadi@chromium.org>
    Date:   Wed Apr 26 15:11:16 2023 +0000
    
        [omnibox] Makes OmniboxController own OmniboxEditModel
    
        Currently OmniboxEditModel - owned by OmniboxView - is at the center of
        the omnibox UI machinery. It owns an instance of OmniboxController
        and OmniboxClient, while OmniboxController owns AutocompleteController.
    
        A better long-term direction is for the OmniboxController to be at the
        center of both the Autocomplete and the omnibox UI machinery and own
        both OmniboxClient and OmniboxEditModel in addition to the
        AutocompleteController.
    
        This change does the following:
        - transfers ownership of OmniboxClient to OmniboxController.
        - reverses ownership model of OmniboxController and OmniboxEditModel.
        - makes OmniboxView own OmniboxController instead of OmniboxEditModel.
    
        Given that the lifetime of all these objects is ultimately bound to
        that of the OmniboxView, this change in ownership should be safe.
    
        Bug: 1404748

--- chromium_src/components/omnibox/browser/omnibox_edit_model.cc
+++ chromium_src/components/omnibox/browser/omnibox_edit_model.cc
@@ -3,11 +3,18 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "components/omnibox/browser/omnibox_edit_model.h"
+
 #include "base/memory/raw_ptr.h"
 #include "brave/components/omnibox/browser/brave_omnibox_client.h"
 #include "components/omnibox/browser/omnibox_client.h"
 #include "components/omnibox/browser/omnibox_controller.h"
 #include "url/gurl.h"
+
+#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
+#include "brave/components/commander/common/constants.h"
+#include "brave/components/commander/common/features.h"
+#endif
 
 #if !BUILDFLAG(IS_IOS)
 #include "content/public/common/url_constants.h"
@@ -54,10 +61,21 @@ void BraveAdjustTextForCopy(GURL* url) {
 
 }  // namespace
 
-#define BRAVE_ADJUST_TEXT_FOR_COPY \
-  BraveAdjustTextForCopy(url_from_text);
+#define BRAVE_ADJUST_TEXT_FOR_COPY BraveAdjustTextForCopy(url_from_text);
 
+#define CanPasteAndGo CanPasteAndGo_Chromium
 #define OmniboxController BraveOmniboxController
 #include "src/components/omnibox/browser/omnibox_edit_model.cc"
 #undef OmniboxController
+#undef CanPasteAndGo
 #undef BRAVE_ADJUST_TEXT_FOR_COPY
+
+bool OmniboxEditModel::CanPasteAndGo(const std::u16string& text) const {
+#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
+  if (commander::CommanderEnabled() &&
+      base::StartsWith(text, commander::kCommandPrefix)) {
+    return false;
+  }
+#endif
+  return CanPasteAndGo_Chromium(text);
+}
