e6dff97af10c44809577dcb51d6c293670ff82a3
Claudio DeSouza <cdesouza@igalia.com>
2023-05-08 13:42:40 +0100

    [ios] WebStateList move to shared path
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/02358d040c4b526db13ac1b76eb76270c4258aaf
    
    commit 02358d040c4b526db13ac1b76eb76270c4258aaf
    Author: Gauthier Ambard <gambard@chromium.org>
    Date:   Fri May 5 11:24:46 2023 +0000
    
        [iOS] Move WebStateList to a shared location
    
        This CL moves the files that are supposed to be shared among several
        classes in the shared folder.
        It is leaving the ones that are supposed to be shared.
    
        Bug: 1319852

--- ios/browser/api/web/web_state/web_state_native.mm
+++ ios/browser/api/web/web_state/web_state_native.mm
@@ -8,9 +8,9 @@
 #include "ios/chrome/browser/main/browser.h"
 #include "ios/chrome/browser/sessions/ios_chrome_session_tab_helper.h"
 #include "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
+#include "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
+#include "ios/chrome/browser/shared/model/web_state_list/web_state_opener.h"
 #include "ios/chrome/browser/tabs/synced_window_delegate_browser_agent.h"
-#include "ios/chrome/browser/web_state_list/web_state_list.h"
-#include "ios/chrome/browser/web_state_list/web_state_opener.h"
 
 #include "ios/web/public/session/crw_navigation_item_storage.h"
 #include "ios/web/public/session/crw_session_storage.h"
