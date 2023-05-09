b40d6e8630d01f5e607b8de9ef8296b03f9ff3b9
Douglas Daniel <douglashdaniel@gmail.com>
2023-05-08 20:14:29 -0500

    feat(wallet): Reusable Sticky Card Header

---
+++ components/brave_wallet_ui/components/desktop/card-headers/card-headers.style.ts
@@ -0,0 +1,16 @@
+// Copyright (c) 2023 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import styled from 'styled-components'
+import * as leo from '@brave/leo/tokens/css'
+
+export const HeaderTitle = styled.span`
+  font-family: Poppins;
+  font-style: normal;
+  font-size: 28px;
+  font-weight: 500;
+  line-height: 40px;
+  color: ${leo.color.text.primary};
+`
