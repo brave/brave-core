7af87b4f06c0145cd861d6b07e3ad05b4079bfaa
Jay Harris <jay.harris@outlook.co.nz>
2023-05-10 09:44:50 +1200

    Make it possible to disable autocomplete again

--- chromium_src/components/omnibox/browser/omnibox_edit_model.cc
+++ chromium_src/components/omnibox/browser/omnibox_edit_model.cc
@@ -6,8 +6,6 @@
 #include "components/omnibox/browser/omnibox_edit_model.h"
 
 #include "base/memory/raw_ptr.h"
-#include "brave/components/omnibox/browser/brave_omnibox_client.h"
-#include "components/omnibox/browser/omnibox_client.h"
 #include "components/omnibox/browser/omnibox_controller.h"
 #include "url/gurl.h"
 
@@ -19,34 +17,6 @@
 #if !BUILDFLAG(IS_IOS)
 #include "content/public/common/url_constants.h"
 #endif
-
-class BraveOmniboxController : public OmniboxController {
- public:
-  BraveOmniboxController(OmniboxView* view,
-                         OmniboxEditModelDelegate* edit_model_delegate,
-                         std::unique_ptr<OmniboxClient> client)
-      : OmniboxController(view, edit_model_delegate, std::move(client)) {}
-  BraveOmniboxController(
-      OmniboxEditModelDelegate* edit_model_delegate,
-      std::unique_ptr<AutocompleteController> autocomplete_controller,
-      std::unique_ptr<OmniboxClient> client)
-      : OmniboxController(edit_model_delegate,
-                          std::move(autocomplete_controller),
-                          std::move(client)) {}
-  BraveOmniboxController(const BraveOmniboxController&) = delete;
-  BraveOmniboxController& operator=(const BraveOmniboxController&) = delete;
-  ~BraveOmniboxController() override = default;
-
-  // OmniboxController overrides:
-  void StartAutocomplete(const AutocompleteInput& input) const override {
-    auto* client = static_cast<BraveOmniboxClient*>(client_.get());
-    if (!client->IsAutocompleteEnabled()) {
-      return;
-    }
-
-    OmniboxController::StartAutocomplete(input);
-  }
-};
 
 namespace {
 void BraveAdjustTextForCopy(GURL* url) {
@@ -64,9 +34,7 @@ void BraveAdjustTextForCopy(GURL* url) {
 #define BRAVE_ADJUST_TEXT_FOR_COPY BraveAdjustTextForCopy(url_from_text);
 
 #define CanPasteAndGo CanPasteAndGo_Chromium
-#define OmniboxController BraveOmniboxController
 #include "src/components/omnibox/browser/omnibox_edit_model.cc"
-#undef OmniboxController
 #undef CanPasteAndGo
 #undef BRAVE_ADJUST_TEXT_FOR_COPY
 
