3c9de7f34e3cb9bd4ec58222788f776a355f2967
Claudio DeSouza <cdesouza@igalia.com>
2023-05-12 03:59:47 +0100

    Reanchoring overrides for MixedContentChecker
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/974a4bdf9247c35d13a73e33ebbe06640762cad9
    
    commit 974a4bdf9247c35d13a73e33ebbe06640762cad9
    Author: Ming-Ying Chung <mych@chromium.org>
    Date:   Wed May 10 06:04:32 2023 +0000
    
        Extract `ShouldBlockNavigation()` into a utility `MixedContentChecker`
    
        This CL does not change any production logic. Instead it does the
        followings:
    
        1. Extract `MixedContentNavigationThrottle::ShouldBlockNavigation()`
           and its dependent methods into a standalone utility class,
           `content::MixedContentChecker`
        2. Add more unit tests coverage for `content::MixedContentChecker`.
        3. Update c++ comments to match the current codebase.
    
        Subsequent CL will implement an in-browser version of
        `blink::MixedContentChecker::ShouldBlockFetch()` to use in
        `content::KeepAliveURLLoader`.
        See [this section] of the keepalive infra migration design doc.
    
        [this section]: https://docs.google.com/document/d/1ZzxMMBvpqn8VZBZKnb7Go8TWjnrGcXuLS_USwVVRUvY/edit#heading=h.xcx0lc6fz9g8
    
        Bug: 1356128

--- chromium_src/content/browser/renderer_host/mixed_content_checker.h
+++ chromium_src/content/browser/renderer_host/mixed_content_checker.h
@@ -1,10 +1,10 @@
 /* Copyright (c) 2022 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#ifndef BRAVE_CHROMIUM_SRC_CONTENT_BROWSER_RENDERER_HOST_MIXED_CONTENT_NAVIGATION_THROTTLE_H_
-#define BRAVE_CHROMIUM_SRC_CONTENT_BROWSER_RENDERER_HOST_MIXED_CONTENT_NAVIGATION_THROTTLE_H_
+#ifndef BRAVE_CHROMIUM_SRC_CONTENT_BROWSER_RENDERER_HOST_MIXED_CONTENT_CHECKER_H_
+#define BRAVE_CHROMIUM_SRC_CONTENT_BROWSER_RENDERER_HOST_MIXED_CONTENT_CHECKER_H_
 
 #define MaybeSendBlinkFeatureUsageReport                                       \
   NotUsed();                                                                   \
@@ -13,8 +13,8 @@
                                                                                \
   void MaybeSendBlinkFeatureUsageReport
 
-#include "src/content/browser/renderer_host/mixed_content_navigation_throttle.h"  // IWYU pragma: export
+#include "src/content/browser/renderer_host/mixed_content_checker.h"  // IWYU pragma: export
 
 #undef MaybeSendBlinkFeatureUsageReport
 
-#endif  // BRAVE_CHROMIUM_SRC_CONTENT_BROWSER_RENDERER_HOST_MIXED_CONTENT_NAVIGATION_THROTTLE_H_
+#endif  // BRAVE_CHROMIUM_SRC_CONTENT_BROWSER_RENDERER_HOST_MIXED_CONTENT_CHECKER_H_
