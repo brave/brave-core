f8b0d898d6a9671a35e3f3f669c0b1e8e220b6e4
boocmp <boocmp@yandex.ru>
2023-05-15 15:04:35 +0700

    Fixed android build.

--- browser/brave_content_browser_client.cc
+++ browser/brave_content_browser_client.cc
@@ -173,11 +173,12 @@ using extensions::ChromeContentBrowserClientExtensionsPart;
 #if BUILDFLAG(ENABLE_SPEEDREADER)
 #include "brave/browser/speedreader/speedreader_service_factory.h"
 #include "brave/browser/speedreader/speedreader_tab_helper.h"
-#include "brave/browser/ui/webui/speedreader/speedreader_toolbar_ui.h"
-#include "brave/components/speedreader/common/speedreader_toolbar.mojom.h"
 #include "brave/components/speedreader/speedreader_throttle.h"
 #include "brave/components/speedreader/speedreader_util.h"
-#include "third_party/blink/public/mojom/loader/resource_load_info.mojom-shared.h"
+#if !BUILDFLAG(IS_ANDROID)
+#include "brave/browser/ui/webui/speedreader/speedreader_toolbar_ui.h"
+#include "brave/components/speedreader/common/speedreader_toolbar.mojom.h"
+#endif
 #endif
 
 #if BUILDFLAG(ENABLE_WIDEVINE)
