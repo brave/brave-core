x01zeroOne/Brave_Browser___brave-core

Add P3A metric for News opt-in

components/brave_news/browser/brave_news_p3a.cc
@@ -21,6 +21,27 @@
 namespace brave_news {
 namespace p3a {
 
+constexpr char kDaysInMonthUsedCountHistogramName[] =
+    "Brave.Today.DaysInMonthUsedCount";
+constexpr char kWeeklySessionCountHistogramName[] =
+    "Brave.Today.WeeklySessionCount";
+constexpr char kWeeklyMaxCardVisitsHistogramName[] =
+    "Brave.Today.WeeklyMaxCardVisitsCount";
+constexpr char kWeeklyMaxCardViewsHistogramName[] =
+    "Brave.Today.WeeklyMaxCardViewsCount";
+constexpr char kTotalCardViewsHistogramName[] =
+    "Brave.Today.WeeklyTotalCardViews";
+constexpr char kWeeklyDisplayAdsViewedHistogramName[] =
+    "Brave.Today.WeeklyDisplayAdsViewedCount";
+constexpr char kDirectFeedsTotalHistogramName[] =
+    "Brave.Today.DirectFeedsTotal";
+constexpr char kWeeklyAddedDirectFeedsHistogramName[] =
+    "Brave.Today.WeeklyAddedDirectFeedsCount";
+constexpr char kLastUsageTimeHistogramName[] = "Brave.Today.LastUsageTime";
+constexpr char kNewUserReturningHistogramName[] =
+    "Brave.Today.NewUserReturning";
+constexpr char kOptInHistogramName[] = "Brave.Today.OptIn";
+
 namespace {
 
 uint16_t UpdateWeeklyStorageWithValueAndGetMax(PrefService* prefs,
@@ -165,6 +186,12 @@ void RecordTotalCardViews(PrefService* prefs,
           << " curr session = " << cards_viewed_session_total_count;
   p3a_utils::RecordToHistogramBucket(kTotalCardViewsHistogramName, buckets,
                                      total);
+}
+
+void RecordOptInChange(PrefService* prefs) {
+  bool enabled = prefs->GetBoolean(prefs::kBraveNewsOptedIn) &&
+                 prefs->GetBoolean(prefs::kNewTabPageShowToday);
+  UMA_HISTOGRAM_BOOLEAN(kOptInHistogramName, enabled);
 }
 
 void RecordAtInit(PrefService* prefs) {

components/brave_news/browser/brave_news_p3a.h
@@ -14,25 +14,17 @@ class PrefService;
 namespace brave_news {
 namespace p3a {
 
-constexpr char kDaysInMonthUsedCountHistogramName[] =
-    "Brave.Today.DaysInMonthUsedCount";
-constexpr char kWeeklySessionCountHistogramName[] =
-    "Brave.Today.WeeklySessionCount";
-constexpr char kWeeklyMaxCardVisitsHistogramName[] =
-    "Brave.Today.WeeklyMaxCardVisitsCount";
-constexpr char kWeeklyMaxCardViewsHistogramName[] =
-    "Brave.Today.WeeklyMaxCardViewsCount";
-constexpr char kTotalCardViewsHistogramName[] =
-    "Brave.Today.WeeklyTotalCardViews";
-constexpr char kWeeklyDisplayAdsViewedHistogramName[] =
-    "Brave.Today.WeeklyDisplayAdsViewedCount";
-constexpr char kDirectFeedsTotalHistogramName[] =
-    "Brave.Today.DirectFeedsTotal";
-constexpr char kWeeklyAddedDirectFeedsHistogramName[] =
-    "Brave.Today.WeeklyAddedDirectFeedsCount";
-constexpr char kLastUsageTimeHistogramName[] = "Brave.Today.LastUsageTime";
-constexpr char kNewUserReturningHistogramName[] =
-    "Brave.Today.NewUserReturning";
+extern const char kDaysInMonthUsedCountHistogramName[];
+extern const char kWeeklySessionCountHistogramName[];
+extern const char kWeeklyMaxCardVisitsHistogramName[];
+extern const char kWeeklyMaxCardViewsHistogramName[];
+extern const char kTotalCardViewsHistogramName[];
+extern const char kWeeklyDisplayAdsViewedHistogramName[];
+extern const char kDirectFeedsTotalHistogramName[];
+extern const char kWeeklyAddedDirectFeedsHistogramName[];
+extern const char kLastUsageTimeHistogramName[];
+extern const char kNewUserReturningHistogramName[];
+extern const char kOptInHistogramName[];
 
 void RecordAtInit(PrefService* prefs);
 void RecordAtSessionStart(PrefService* prefs);
@@ -46,6 +38,7 @@ void RecordWeeklyAddedDirectFeedsCount(PrefService* prefs, int change);
 void RecordDirectFeedsTotal(PrefService* prefs);
 void RecordTotalCardViews(PrefService* prefs,
                           uint64_t cards_viewed_session_total_count);
+void RecordOptInChange(PrefService* prefs);
 void RegisterProfilePrefs(PrefRegistrySimple* registry);
 
 }  // namespace p3a

components/brave_news/browser/brave_news_p3a_unittest.cc
@@ -395,5 +395,17 @@ TEST_F(BraveNewsP3ATest, TestNewUserReturningNotFollowingDay) {
   histogram_tester_.ExpectBucketCount(kNewUserReturningHistogramName, 1, 1);
 }
 
+TEST_F(BraveNewsP3ATest, TestOptIn) {
+  PrefService* prefs = GetPrefs();
+
+  prefs->SetBoolean(prefs::kBraveNewsOptedIn, true);
+  RecordOptInChange(prefs);
+  histogram_tester_.ExpectUniqueSample(kOptInHistogramName, 1, 1);
+
+  prefs->SetBoolean(prefs::kBraveNewsOptedIn, false);
+  RecordOptInChange(prefs);
+  histogram_tester_.ExpectBucketCount(kOptInHistogramName, 0, 1);
+}
+
 }  // namespace p3a
 }  // namespace brave_news

components/p3a/metric_names.h
@@ -193,6 +193,7 @@ constexpr inline auto kCollectedSlowHistograms =
 constexpr inline auto kCollectedExpressHistograms =
   base::MakeFixedFlatSet<base::StringPiece>({
     "Brave.Core.UsageDaily",
+    "Brave.Today.OptIn",
     "Brave.Rewards.EnabledInstallationTime",
     "Brave.Wallet.UsageDaily"
 });
@@ -201,6 +202,7 @@ constexpr inline auto kCollectedExpressHistograms =
 // Once the metric value has been sent, the value will be removed from the log store.
 constexpr inline auto kEphemeralHistograms =
   base::MakeFixedFlatSet<base::StringPiece>({
+    "Brave.Today.OptIn",
     "Brave.Playlist.UsageDaysInWeek",
     "Brave.Playlist.FirstTimeOffset",
     "Brave.Rewards.EnabledInstallationTime",

brave/brave-core

components/brave_news/browser/brave_news_p3a.cc
@@ -21,6 +21,27 @@
 namespace brave_news {
 namespace p3a {
 
+constexpr char kDaysInMonthUsedCountHistogramName[] =
+    "Brave.Today.DaysInMonthUsedCount";
+constexpr char kWeeklySessionCountHistogramName[] =
+    "Brave.Today.WeeklySessionCount";
+constexpr char kWeeklyMaxCardVisitsHistogramName[] =
+    "Brave.Today.WeeklyMaxCardVisitsCount";
+constexpr char kWeeklyMaxCardViewsHistogramName[] =
+    "Brave.Today.WeeklyMaxCardViewsCount";
+constexpr char kTotalCardViewsHistogramName[] =
+    "Brave.Today.WeeklyTotalCardViews";
+constexpr char kWeeklyDisplayAdsViewedHistogramName[] =
+    "Brave.Today.WeeklyDisplayAdsViewedCount";
+constexpr char kDirectFeedsTotalHistogramName[] =
+    "Brave.Today.DirectFeedsTotal";
+constexpr char kWeeklyAddedDirectFeedsHistogramName[] =
+    "Brave.Today.WeeklyAddedDirectFeedsCount";
+constexpr char kLastUsageTimeHistogramName[] = "Brave.Today.LastUsageTime";
+constexpr char kNewUserReturningHistogramName[] =
+    "Brave.Today.NewUserReturning";
+constexpr char kIsEnabledHistogramName[] = "Brave.Today.IsEnabled";
+
 namespace {
 
 uint16_t UpdateWeeklyStorageWithValueAndGetMax(PrefService* prefs,
@@ -165,6 +186,12 @@ void RecordTotalCardViews(PrefService* prefs,
           << " curr session = " << cards_viewed_session_total_count;
   p3a_utils::RecordToHistogramBucket(kTotalCardViewsHistogramName, buckets,
                                      total);
+}
+
+void RecordOptInChange(PrefService* prefs) {
+  bool enabled = prefs->GetBoolean(prefs::kBraveNewsOptedIn) &&
+                 prefs->GetBoolean(prefs::kNewTabPageShowToday);
+  UMA_HISTOGRAM_BOOLEAN(kIsEnabledHistogramName, enabled);
 }
 
 void RecordAtInit(PrefService* prefs) {

components/brave_news/browser/brave_news_p3a.h
@@ -14,25 +14,17 @@ class PrefService;
 namespace brave_news {
 namespace p3a {
 
-constexpr char kDaysInMonthUsedCountHistogramName[] =
-    "Brave.Today.DaysInMonthUsedCount";
-constexpr char kWeeklySessionCountHistogramName[] =
-    "Brave.Today.WeeklySessionCount";
-constexpr char kWeeklyMaxCardVisitsHistogramName[] =
-    "Brave.Today.WeeklyMaxCardVisitsCount";
-constexpr char kWeeklyMaxCardViewsHistogramName[] =
-    "Brave.Today.WeeklyMaxCardViewsCount";
-constexpr char kTotalCardViewsHistogramName[] =
-    "Brave.Today.WeeklyTotalCardViews";
-constexpr char kWeeklyDisplayAdsViewedHistogramName[] =
-    "Brave.Today.WeeklyDisplayAdsViewedCount";
-constexpr char kDirectFeedsTotalHistogramName[] =
-    "Brave.Today.DirectFeedsTotal";
-constexpr char kWeeklyAddedDirectFeedsHistogramName[] =
-    "Brave.Today.WeeklyAddedDirectFeedsCount";
-constexpr char kLastUsageTimeHistogramName[] = "Brave.Today.LastUsageTime";
-constexpr char kNewUserReturningHistogramName[] =
-    "Brave.Today.NewUserReturning";
+extern const char kDaysInMonthUsedCountHistogramName[];
+extern const char kWeeklySessionCountHistogramName[];
+extern const char kWeeklyMaxCardVisitsHistogramName[];
+extern const char kWeeklyMaxCardViewsHistogramName[];
+extern const char kTotalCardViewsHistogramName[];
+extern const char kWeeklyDisplayAdsViewedHistogramName[];
+extern const char kDirectFeedsTotalHistogramName[];
+extern const char kWeeklyAddedDirectFeedsHistogramName[];
+extern const char kLastUsageTimeHistogramName[];
+extern const char kNewUserReturningHistogramName[];
+extern const char kIsEnabledHistogramName[];
 
 void RecordAtInit(PrefService* prefs);
 void RecordAtSessionStart(PrefService* prefs);
@@ -46,6 +38,7 @@ void RecordWeeklyAddedDirectFeedsCount(PrefService* prefs, int change);
 void RecordDirectFeedsTotal(PrefService* prefs);
 void RecordTotalCardViews(PrefService* prefs,
                           uint64_t cards_viewed_session_total_count);
+void RecordOptInChange(PrefService* prefs);
 void RegisterProfilePrefs(PrefRegistrySimple* registry);
 
 }  // namespace p3a

components/brave_news/browser/brave_news_p3a_unittest.cc
@@ -395,5 +395,18 @@ TEST_F(BraveNewsP3ATest, TestNewUserReturningNotFollowingDay) {
   histogram_tester_.ExpectBucketCount(kNewUserReturningHistogramName, 1, 1);
 }
 
+TEST_F(BraveNewsP3ATest, TestIsEnabled) {
+  PrefService* prefs = GetPrefs();
+
+  prefs->SetBoolean(prefs::kBraveNewsOptedIn, true);
+  prefs->SetBoolean(prefs::kNewTabPageShowToday, true);
+  RecordOptInChange(prefs);
+  histogram_tester_.ExpectUniqueSample(kIsEnabledHistogramName, 1, 1);
+
+  prefs->SetBoolean(prefs::kNewTabPageShowToday, false);
+  RecordOptInChange(prefs);
+  histogram_tester_.ExpectBucketCount(kIsEnabledHistogramName, 0, 1);
+}
+
 }  // namespace p3a
 }  // namespace brave_news

components/p3a/metric_names.h
@@ -193,6 +193,7 @@ constexpr inline auto kCollectedExpressHistograms =
   base::MakeFixedFlatSet<base::StringPiece>({
     "Brave.Core.UsageDaily",
     "Brave.Rewards.EnabledInstallationTime",
+    "Brave.Today.IsEnabled",
     "Brave.Wallet.UsageDaily"
 });
 
@@ -207,6 +208,7 @@ constexpr inline auto kEphemeralHistograms =
     "Brave.Rewards.InlineTipTrigger",
     "Brave.Rewards.TipsSent",
     "Brave.Rewards.ToolbarButtonTrigger",
+    "Brave.Today.IsEnabled",
     "Brave.Wallet.UsageDaily",
     "Brave.Wallet.UsageMonthly",
     "Brave.Wallet.UsageWeekly"