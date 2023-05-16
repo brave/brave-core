d5d0f817e21b264ce7406ba4901f2e9e136f83da
Nuo Xu <nuoxu.nx@gmail.com>
2023-05-16 07:23:34 -0700

    Fix #7455: Tx links does not respect tx's chain id (#7456)

--- Sources/BraveWallet/Crypto/Accounts/AccountTransactionListView.swift
+++ Sources/BraveWallet/Crypto/Accounts/AccountTransactionListView.swift
@@ -43,7 +43,8 @@ struct AccountTransactionListView: View {
               .contextMenu {
                 if !txSummary.txHash.isEmpty {
                   Button(action: {
-                    if let baseURL = self.networkStore.selectedChain.blockExplorerUrls.first.map(URL.init(string:)),
+                    if let txNetwork = self.networkStore.allChains.first(where: { $0.chainId == txSummary.txInfo.chainId }),
+                       let baseURL = txNetwork.blockExplorerUrls.first.map(URL.init(string:)),
                        let url = baseURL?.appendingPathComponent("tx/\(txSummary.txHash)") {
                       openWalletURL(url)
                     }
