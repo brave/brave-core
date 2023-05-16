27ac3556da5b2fa45a1b6878a22004ddbb78d710
AlexeyBarabash <alexey@brave.com>
2023-05-16 16:48:48 +0300

    Re-organized patch for SplitMonochromeApplication.java

--- android/java/org/chromium/android_webview/nonembedded/WebViewApkApplication.java
+++ android/java/org/chromium/android_webview/nonembedded/WebViewApkApplication.java
@@ -5,12 +5,40 @@
 
 package org.chromium.android_webview.nonembedded;
 
+import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 
 public class WebViewApkApplication {
     private static final String TAG = "BraveWebViewApkApplication";
 
     public static void postDeveloperUiLauncherIconTask() {
+        wtf();
+    }
+
+    // Copied from upstreams's android_webview/nonembedded/WebViewApkApplication.java
+    // to ensure we don't create any webview process
+    // Returns true if running in the "webview_apk" or "webview_service" process.
+    public static boolean isWebViewProcess() {
+        // Either "webview_service", or "webview_apk".
+        // "webview_service" is meant to be very light-weight and never load the native library.
+        boolean hasWebViewPrefix = ContextUtils.getProcessName().contains(":webview_");
+        if (hasWebViewPrefix) {
+            wtf();
+        }
+        return hasWebViewPrefix;
+    }
+
+    public static void maybeInitProcessGlobals() {
+        wtf();
+    }
+
+    public static void checkForAppRecovery() {
+        wtf();
+    }
+
+    // WTF is for `What a Terrible Failure`
+    private static void wtf() {
         Log.wtf(TAG, "Android Webview API must not be invoked");
+        assert false;
     }
 }
