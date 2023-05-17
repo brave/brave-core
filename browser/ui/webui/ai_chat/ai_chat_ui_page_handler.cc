93ef5d4ab7e72d6c318389feaaf502b8a304679e
Lorenzo <lminto@brave.com>
2023-05-17 12:28:32 +0200

    Improve input sanitisation

--- browser/ui/webui/ai_chat/ai_chat_ui_page_handler.cc
+++ browser/ui/webui/ai_chat/ai_chat_ui_page_handler.cc
@@ -55,6 +55,11 @@ void AIChatUIPageHandler::SubmitHumanConversationEntry(
   base::ReplaceSubstringsAfterOffset(&input_copy, 0, ai_chat::kAIPrompt, "");
   base::ReplaceSubstringsAfterOffset(&input_copy, 0, "<article>", "");
   base::ReplaceSubstringsAfterOffset(&input_copy, 0, "</article>", "");
+  base::ReplaceSubstringsAfterOffset(&input_copy, 0, "<history>", "");
+  base::ReplaceSubstringsAfterOffset(&input_copy, 0, "</history>", "");
+  base::ReplaceSubstringsAfterOffset(&input_copy, 0, "<question>", "");
+  base::ReplaceSubstringsAfterOffset(&input_copy, 0, "</question>", "");
+
 
   active_chat_tab_helper_->MakeAPIRequestWithConversationHistoryUpdate(
       {CharacterType::HUMAN, ConversationTurnVisibility::VISIBLE, input_copy});
