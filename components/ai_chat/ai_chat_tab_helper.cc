x01zeroOne/Brave_Browser___brave-core

Added text streams to AIChat UI

components/ai_chat/ai_chat_tab_helper.cc
@@ -149,6 +149,25 @@ void AIChatTabHelper::AddToConversationHistory(const ConversationTurn& turn) {
   }
 }
 
+void AIChatTabHelper::UpdateLastEntryInConversationHistory(
+    const std::string& updated_text) {
+  ConversationTurn& prev_turn = chat_history_.back();
+
+  if (prev_turn.character_type != CharacterType::ASSISTANT) {
+    AddToConversationHistory({CharacterType::ASSISTANT,
+                              ConversationTurnVisibility::VISIBLE,
+                              updated_text});
+    return;
+  }
+
+  prev_turn.text = updated_text;
+
+  // Trigger an observer update to refresh the UI.
+  for (auto& obs : observers_) {
+    obs.OnHistoryUpdate();
+  }
+}
+
 void AIChatTabHelper::SetArticleSummaryString(const std::string& text) {
   article_summary_ = text;
 }
@@ -279,8 +298,8 @@ void AIChatTabHelper::DistillViaAlgorithm(const ui::AXTree& tree) {
 void AIChatTabHelper::CleanUp() {
   chat_history_.clear();
   article_summary_.clear();
-  SetRequestInProgress(false);
 
+  // Trigger an observer update to refresh the UI.
   for (auto& obs : observers_) {
     obs.OnHistoryUpdate();
   }
@@ -301,49 +320,51 @@ void AIChatTabHelper::MakeAPIRequestWithConversationHistoryUpdate(
   bool contains_summary =
       turn.visibility == ConversationTurnVisibility::HIDDEN ? true : false;
 
-  SetRequestInProgress(true);
-
   ai_chat_api_->QueryPrompt(
-      base::BindOnce(&AIChatTabHelper::OnAPIResponse, base::Unretained(this),
-                     contains_summary),
+      base::BindRepeating(&AIChatTabHelper::OnAPIStreamDataReceived,
+                          base::Unretained(this)),
+      base::BindOnce(&AIChatTabHelper::OnAPIStreamDataComplete,
+                     base::Unretained(this), contains_summary),
       std::move(prompt_with_history));
 }
 
-void AIChatTabHelper::OnAPIResponse(bool contains_summary,
-                                    const std::string& assistant_input,
-                                    bool success) {
-  SetRequestInProgress(false);
+bool AIChatTabHelper::IsRequestInProgress() {
+  DCHECK(ai_chat_api_);
 
-  if (!success) {
+  return ai_chat_api_->IsRequestInProgress();
+}
+
+void AIChatTabHelper::OnAPIStreamDataReceived(const std::string& text) {
+  UpdateLastEntryInConversationHistory(text);
+
+  // Trigger an observer update to refresh the UI.
+  for (auto& obs : observers_) {
+    obs.OnAPIRequestInProgress(IsRequestInProgress());
+  }
+}
+
+void AIChatTabHelper::OnAPIStreamDataComplete(bool contains_summary,
+                                              bool success,
+                                              int response_code) {
+  if (contains_summary && success) {
+    SetArticleSummaryString(chat_history_.back().text);
+  }
+
+  if (!success || response_code != 200) {
     // TODO(petemill): show error state separate from assistant message
     AddToConversationHistory(ConversationTurn{
         CharacterType::ASSISTANT, ConversationTurnVisibility::VISIBLE,
         l10n_util::GetStringUTF8(IDS_CHAT_UI_API_ERROR)});
-
-    return;
   }
 
-  ConversationTurn turn = {CharacterType::ASSISTANT,
-                           ConversationTurnVisibility::VISIBLE,
-                           assistant_input};
-
-  if (contains_summary && !assistant_input.empty()) {
-    SetArticleSummaryString(assistant_input);
-  }
-
-  AddToConversationHistory(turn);
-}
-
-void AIChatTabHelper::SetRequestInProgress(bool in_progress) {
-  is_request_in_progress_ = in_progress;
-
+  // Trigger an observer update to refresh the UI.
   for (auto& obs : observers_) {
     obs.OnAPIRequestInProgress(IsRequestInProgress());
   }
 }
 
 void AIChatTabHelper::PrimaryPageChanged(content::Page& page) {
-  // TODO(nullhook): Cancel inflight API requests
+  // TODO(nullhook): Cancel inflight API request
   CleanUp();
 }
 

components/ai_chat/ai_chat_tab_helper.h
@@ -38,9 +38,10 @@ class AIChatTabHelper : public content::WebContentsObserver,
 
   const std::vector<ai_chat::mojom::ConversationTurn>& GetConversationHistory();
   void AddToConversationHistory(const ai_chat::mojom::ConversationTurn& turn);
+  void UpdateLastEntryInConversationHistory(const std::string& text);
   void MakeAPIRequestWithConversationHistoryUpdate(
       const ai_chat::mojom::ConversationTurn& turn);
-  bool IsRequestInProgress() { return is_request_in_progress_; }
+  bool IsRequestInProgress();
   void AddObserver(Observer* observer);
   void RemoveObserver(Observer* observer);
 
@@ -62,10 +63,10 @@ class AIChatTabHelper : public content::WebContentsObserver,
   void DistillViaAlgorithm(const ui::AXTree& tree);
   void SetArticleSummaryString(const std::string& text);
   void CleanUp();
-  void OnAPIResponse(bool contains_summary,
-                     const std::string& assistant_input,
-                     bool success);
-  void SetRequestInProgress(bool in_progress);
+  void OnAPIStreamDataReceived(const std::string& text);
+  void OnAPIStreamDataComplete(bool contains_summary,
+                               bool success,
+                               int response_code);
 
   // content::WebContentsObserver:
   void PrimaryPageChanged(content::Page& page) override;
@@ -79,7 +80,6 @@ class AIChatTabHelper : public content::WebContentsObserver,
   std::vector<ai_chat::mojom::ConversationTurn> chat_history_;
   std::string history_text_;
   std::string article_summary_;
-  bool is_request_in_progress_;
 
   WEB_CONTENTS_USER_DATA_KEY_DECL();
 };

brave/brave-core

components/ai_chat/ai_chat_tab_helper.cc
@@ -150,6 +150,25 @@ void AIChatTabHelper::AddToConversationHistory(const ConversationTurn& turn) {
   }
 }
 
+void AIChatTabHelper::UpdateLastEntryInConversationHistory(
+    const std::string& updated_text) {
+  ConversationTurn& prev_turn = chat_history_.back();
+
+  if (prev_turn.character_type != CharacterType::ASSISTANT) {
+    AddToConversationHistory({CharacterType::ASSISTANT,
+                              ConversationTurnVisibility::VISIBLE,
+                              updated_text});
+    return;
+  }
+
+  prev_turn.text = updated_text;
+
+  // Trigger an observer update to refresh the UI.
+  for (auto& obs : observers_) {
+    obs.OnHistoryUpdate();
+  }
+}
+
 void AIChatTabHelper::SetArticleSummaryString(const std::string& text) {
   article_summary_ = text;
 }
@@ -286,8 +305,8 @@ void AIChatTabHelper::CleanUp() {
   chat_history_.clear();
   article_summary_.clear();
   article_text_.clear();
-  SetRequestInProgress(false);
 
+  // Trigger an observer update to refresh the UI.
   for (auto& obs : observers_) {
     obs.OnHistoryUpdate();
   }
@@ -308,55 +327,57 @@ void AIChatTabHelper::MakeAPIRequestWithConversationHistoryUpdate(
 
   DCHECK(ai_chat_api_);
 
-  SetRequestInProgress(true);
-
   // Assuming a hidden conversation has a summary prompt,
   // the incoming response is expected to include the AI-generated summary.
   // TODO(nullhook): Improve this heuristic, as it may or may not be true.
   bool is_summarize_prompt =
-      turn.visibility == ConversationTurnVisibility::INTERNAL ? true : false;
+      turn.visibility == ConversationTurnVisibility::HIDDEN ? true : false;
 
   ai_chat_api_->QueryPrompt(
-      base::BindOnce(&AIChatTabHelper::OnAPIResponse, base::Unretained(this),
-                     is_summarize_prompt),
+      base::BindRepeating(&AIChatTabHelper::OnAPIStreamDataReceived,
+                          base::Unretained(this)),
+      base::BindOnce(&AIChatTabHelper::OnAPIStreamDataComplete,
+                     base::Unretained(this), is_summarize_prompt),
       std::move(prompt_with_history));
 }
 
-void AIChatTabHelper::OnAPIResponse(bool contains_summary,
-                                    const std::string& assistant_input,
-                                    bool success) {
-  SetRequestInProgress(false);
+bool AIChatTabHelper::IsRequestInProgress() {
+  DCHECK(ai_chat_api_);
 
-  if (!success) {
+  return ai_chat_api_->IsRequestInProgress();
+}
+
+void AIChatTabHelper::OnAPIStreamDataReceived(const std::string& text) {
+  UpdateLastEntryInConversationHistory(text);
+
+  // Trigger an observer update to refresh the UI.
+  for (auto& obs : observers_) {
+    obs.OnAPIRequestInProgress(IsRequestInProgress());
+  }
+}
+
+void AIChatTabHelper::OnAPIStreamDataComplete(bool is_summarize_prompt,
+                                              bool success,
+                                              int response_code) {
+  if (is_summarize_prompt && success) {
+    SetArticleSummaryString(chat_history_.back().text);
+  }
+
+  if (!success || response_code != 200) {
     // TODO(petemill): show error state separate from assistant message
     AddToConversationHistory(ConversationTurn{
         CharacterType::ASSISTANT, ConversationTurnVisibility::VISIBLE,
         l10n_util::GetStringUTF8(IDS_CHAT_UI_API_ERROR)});
-
-    return;
   }
 
-  ConversationTurn turn = {CharacterType::ASSISTANT,
-                           ConversationTurnVisibility::VISIBLE,
-                           assistant_input};
-
-  if (contains_summary && !assistant_input.empty()) {
-    SetArticleSummaryString(assistant_input);
-  }
-
-  AddToConversationHistory(turn);
-}
-
-void AIChatTabHelper::SetRequestInProgress(bool in_progress) {
-  is_request_in_progress_ = in_progress;
-
+  // Trigger an observer update to refresh the UI.
   for (auto& obs : observers_) {
     obs.OnAPIRequestInProgress(IsRequestInProgress());
   }
 }
 
 void AIChatTabHelper::PrimaryPageChanged(content::Page& page) {
-  // TODO(nullhook): Cancel inflight API requests
+  // TODO(nullhook): Cancel inflight API request
   CleanUp();
 }
 

components/ai_chat/ai_chat_tab_helper.h
@@ -38,9 +38,10 @@ class AIChatTabHelper : public content::WebContentsObserver,
 
   const std::vector<ai_chat::mojom::ConversationTurn>& GetConversationHistory();
   void AddToConversationHistory(const ai_chat::mojom::ConversationTurn& turn);
+  void UpdateLastEntryInConversationHistory(const std::string& text);
   void MakeAPIRequestWithConversationHistoryUpdate(
       const ai_chat::mojom::ConversationTurn& turn);
-  bool IsRequestInProgress() { return is_request_in_progress_; }
+  bool IsRequestInProgress();
   void AddObserver(Observer* observer);
   void RemoveObserver(Observer* observer);
 
@@ -62,10 +63,10 @@ class AIChatTabHelper : public content::WebContentsObserver,
   void DistillViaAlgorithm(const ui::AXTree& tree);
   void SetArticleSummaryString(const std::string& text);
   void CleanUp();
-  void OnAPIResponse(bool contains_summary,
-                     const std::string& assistant_input,
-                     bool success);
-  void SetRequestInProgress(bool in_progress);
+  void OnAPIStreamDataReceived(const std::string& text);
+  void OnAPIStreamDataComplete(bool is_summarize_prompt,
+                               bool success,
+                               int response_code);
 
   // content::WebContentsObserver:
   void PrimaryPageChanged(content::Page& page) override;
@@ -80,7 +81,6 @@ class AIChatTabHelper : public content::WebContentsObserver,
   std::string article_text_;
   std::string history_text_;
   std::string article_summary_;
-  bool is_request_in_progress_;
 
   WEB_CONTENTS_USER_DATA_KEY_DECL();
 };