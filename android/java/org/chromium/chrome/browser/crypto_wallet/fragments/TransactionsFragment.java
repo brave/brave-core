2765f578a032c45ff2f66a2e66ad82e1d10b1921
Pavneet-Sing <psingh@brave.com>
2023-05-25 19:35:49 +0530

    - add progress bar - optimize flow

--- android/java/org/chromium/chrome/browser/crypto_wallet/fragments/TransactionsFragment.java
+++ android/java/org/chromium/chrome/browser/crypto_wallet/fragments/TransactionsFragment.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
