7a9f84ded78bb073e7ed1e155e48c0d7b82062f4
Emerick Rogul <erogul@brave.com>
2023-05-25 22:34:20 -0400

    Upgrade from Chromium 114.0.5735.35 to Chromium 114.0.5735.53 (1.52.x) (#18613)
    * Upgrade from Chromium 114.0.5735.35 to Chromium 114.0.5735.53
    
    * Upgrade patches from Chromium 114.0.5735.35 to Chromium 114.0.5735.53
    
    * Update pins list timestamp
    
    * [Android] Clean up code for BraveTabSwitcherModeTopToolbar.mToggleTabStackButton
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/d422158bc1109201e9ba59ef20b1d2285b58cc44
    
    [A11y] Hide tab switcher button in a11y list tab switcher
    
    Hide tab switcher button in a11y list tab switcher.
    -Tab switcher toolbar no longer shows this button, so it's been removed.
    
    (cherry picked from commit 650061ccbc4dd02957c60aafa24f15a1c223cd72)
    
    Bug: 1445874
    
    ---------
    
    Co-authored-by: brave-builds <brave-builds+devops@brave.com>
    Co-authored-by: Artem Samoilenko <artem@brave.com>

--- chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
+++ chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
@@ -535,9 +535,9 @@ bool ParseCertificatesFile(base::StringPiece certs_input,
                            Pinsets* pinsets,
                            base::Time* timestamp) {
   constexpr base::StringPiece brave_certs = R"brave_certs(
-# Last updated: Tue May 23 21:01:04 UTC 2023
+# Last updated: Thu May 25 14:23:39 UTC 2023
 PinsListTimestamp
-1684875664
+1685024619
 
 # =====BEGIN BRAVE ROOTS ASC=====
 #From https://www.amazontrust.com/repository/
