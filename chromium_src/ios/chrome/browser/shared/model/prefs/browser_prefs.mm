c5a20dcba7e255f832ddc0bae7ba87e235eb55cf
Claudio DeSouza <cdesouza@igalia.com>
2023-05-26 09:49:13 +0100

    [ios] Browser prefs moved to shared path
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/fd73b8a1809ae2278c9a7df653dbcebf4c9afaeb
    
    commit fd73b8a1809ae2278c9a7df653dbcebf4c9afaeb
    Author: Gauthier Ambard <gambard@chromium.org>
    Date:   Wed May 17 09:54:43 2023 +0000
    
        [iOS] Move Prefs to shared folder
    
        This CL moves the pref names and browser prefs to the shared folder
        and leaves the factory and the associator in the folder.
    
        Bug: 1319852

--- chromium_src/ios/chrome/browser/shared/model/prefs/browser_prefs.mm
+++ chromium_src/ios/chrome/browser/shared/model/prefs/browser_prefs.mm
@@ -1,7 +1,7 @@
 /* Copyright (c) 2020 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "brave/components/brave_sync/brave_sync_prefs.h"
 #include "brave/components/brave_wallet/browser/brave_wallet_prefs.h"
@@ -45,8 +45,9 @@ void BraveMigrateObsoleteBrowserStatePrefs(PrefService* prefs) {
   brave_wallet::MigrateObsoleteProfilePrefs(prefs);
 }
 
-#define BRAVE_REGISTER_BROWSER_STATE_PREFS BraveRegisterBrowserStatePrefs(registry);
+#define BRAVE_REGISTER_BROWSER_STATE_PREFS \
+  BraveRegisterBrowserStatePrefs(registry);
 #define BRAVE_REGISTER_LOCAL_STATE_PREFS BraveRegisterLocalStatePrefs(registry);
 #define BRAVE_MIGRATE_OBSOLETE_BROWSER_STATE_PREFS \
   BraveMigrateObsoleteBrowserStatePrefs(prefs);
-#include "src/ios/chrome/browser/prefs/browser_prefs.mm"
+#include "src/ios/chrome/browser/shared/model/prefs/browser_prefs.mm"
