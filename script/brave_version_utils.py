2bc61b2ed98671021c11db9b9305cd1b0d680710
Artem Samoilenko <artem@brave.com>
2023-05-29 11:21:51 -0400

    [Android] Fixes for python pre-submit issues

--- script/brave_version_utils.py
+++ script/brave_version_utils.py
@@ -7,9 +7,11 @@
 
 
 def remap_build_number(build_number):
-    # Starting from Chromium's build number 5750 there is a change how we generate version
-    # codes in order to provide proper app version to the intel devices that support arm as
-    # well. Since we patch build number with our version, we need to remap it.
+    # Starting from Chromium's build number 5750 there is a change how
+    # we generate version codes in order to provide proper app version
+    # to the intel devices that support arm as well. Since we patch
+    # build number with our version, we need to remap it.
     # We want to introduce this change starting from 1.53.x.
-    if build_number >= 53: build_number = 5750
+    if build_number >= 53:
+        build_number = 5750
     return build_number
