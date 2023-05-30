8176b76ab398d192097db01a8435800ca79d3a6b
Simone Arpe <simon.arpe@gmail.com>
2023-05-30 18:48:15 +0200

    Implement shimmer effect while loading coin list

--- android/java/org/chromium/chrome/browser/crypto_wallet/fragments/MarketFragment.java
+++ android/java/org/chromium/chrome/browser/crypto_wallet/fragments/MarketFragment.java
@@ -12,11 +12,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatImageView;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -37,6 +39,7 @@ import org.chromium.chrome.browser.app.domain.MarketModel;
 import org.chromium.chrome.browser.app.domain.MarketModel.CoinMarketsCallback;
 import org.chromium.chrome.browser.app.domain.WalletModel;
 import org.chromium.chrome.browser.app.helpers.Api33AndPlusBackPressHelper;
+import org.chromium.chrome.browser.app.shimmer.ShimmerFrameLayout;
 import org.chromium.chrome.browser.crypto_wallet.activities.AccountDetailActivity;
 import org.chromium.chrome.browser.crypto_wallet.activities.AddAccountActivity;
 import org.chromium.chrome.browser.crypto_wallet.activities.BraveWalletActivity;
@@ -44,7 +47,9 @@ import org.chromium.chrome.browser.crypto_wallet.adapters.MarketCoinAdapter;
 import org.chromium.chrome.browser.crypto_wallet.adapters.WalletCoinAdapter;
 import org.chromium.chrome.browser.crypto_wallet.listeners.OnWalletListItemClick;
 import org.chromium.chrome.browser.crypto_wallet.model.WalletListItemModel;
+import org.chromium.chrome.browser.crypto_wallet.util.AndroidUtils;
 import org.chromium.chrome.browser.crypto_wallet.util.Utils;
+import org.chromium.chrome.browser.custom_layout.LockableNestedScrollView;
 import org.chromium.chrome.browser.settings.BraveWalletPreferences;
 import org.chromium.chrome.browser.settings.SettingsLauncherImpl;
 import org.chromium.components.browser_ui.settings.SettingsLauncher;
@@ -56,11 +61,20 @@ import java.util.List;
 public class MarketFragment extends Fragment implements CoinMarketsCallback {
     private static final String TAG = "MarketFragment";
 
-    private View rootView;
+    private LockableNestedScrollView mRootView;
     private RecyclerView mCoinMarkets;
+    private ShimmerFrameLayout mShimmerViewContainer;
+    private LinearLayout mSingleItemShimmer;
+    private CardView mShimmerCardView;
+    private LinearLayout mShimmerListContainer;
+    private CardView mMarketListCardView;
+
     private MarketCoinAdapter mMarketCoinAdapter;
+
     private WalletModel mWalletModel;
     private MarketModel mMarketModel;
+
+    private boolean mShimmerActive = true;
 
     public static MarketFragment newInstance() {
         return new MarketFragment();
@@ -88,23 +102,82 @@ public class MarketFragment extends Fragment implements CoinMarketsCallback {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
             @Nullable Bundle savedInstanceState) {
-        rootView = inflater.inflate(R.layout.fragment_market, container, false);
-        mCoinMarkets = rootView.findViewById(R.id.coin_markets_recycler_view);
+        mRootView = (LockableNestedScrollView) inflater.inflate(
+                R.layout.fragment_market, container, false);
+        mRootView.setScrollingEnabled(false);
+        mShimmerViewContainer = mRootView.findViewById(R.id.shimmer_view_container);
+        mSingleItemShimmer = mRootView.findViewById(R.id.market_coin_item);
+        mShimmerCardView = mRootView.findViewById(R.id.card_view_market_placeholder);
+        mShimmerListContainer = mRootView.findViewById(R.id.shimmer_list);
+        mMarketListCardView = mRootView.findViewById(R.id.market_list_card_view_view_container);
+
+        mCoinMarkets = mRootView.findViewById(R.id.coin_markets_recycler_view);
         mCoinMarkets.addItemDecoration(
                 new DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL));
         mCoinMarkets.setAdapter(mMarketCoinAdapter);
         mCoinMarkets.setLayoutManager(new LinearLayoutManager(getActivity()));
         mMarketModel.getCoinMarkets(this);
-        return rootView;
+
+        int totalRow = getTotalRow(mSingleItemShimmer, mShimmerCardView);
+        for (int i = 1; i < totalRow; i++) {
+            LayoutInflater.from(requireContext())
+                    .inflate(R.layout.brave_wallet_divider, mShimmerListContainer, true);
+            LayoutInflater.from(requireContext())
+                    .inflate(R.layout.market_coin_shimmer_item, mShimmerListContainer, true);
+        }
+
+        return mRootView;
+    }
+
+    private int getTotalRow(final ViewGroup mSingleItemShimmer, final CardView shimmerCardView) {
+        int widthMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        int heightMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+
+        mSingleItemShimmer.measure(widthMeasureSpec, heightMeasureSpec);
+        int itemHeight = mSingleItemShimmer.getMeasuredHeight();
+        int totalHeight = AndroidUtils.getScreenHeight();
+
+        int totalHeightNoPadding =
+                totalHeight - shimmerCardView.getPaddingTop() - shimmerCardView.getPaddingBottom();
+
+        return Math.max(1, (int) Math.ceil((float) totalHeightNoPadding / itemHeight));
     }
 
     @Override
     public void onCoinMarketsSuccess(final CoinMarket[] coinMarkets) {
         mMarketCoinAdapter.setCoinMarkets(Arrays.asList(coinMarkets));
+        disableShimmerEffect();
     }
 
     @Override
     public void onCoinMarketsFail() {
-        // No-op.
+        Log.e(TAG, "Failed to load coin market list.");
+        disableShimmerEffect();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mShimmerActive) {
+            mShimmerViewContainer.startShimmer();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mShimmerActive) {
+            mShimmerViewContainer.stopShimmer();
+        }
+    }
+
+    private void disableShimmerEffect() {
+        if (mShimmerActive) {
+            mShimmerViewContainer.stopShimmer();
+            mShimmerViewContainer.setVisibility(View.GONE);
+            mRootView.setScrollingEnabled(true);
+            mMarketListCardView.setVisibility(View.VISIBLE);
+            mShimmerActive = false;
+        }
     }
 }
