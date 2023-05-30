fd122900d57b87191b7be84f32d5fedfd1751aad
Claudio DeSouza <cdesouza@igalia.com>
2023-05-30 16:19:37 +0100

    DNS host records just a vector type
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/c60143b3b7b6b1824fb733a90b279d959e241333
    
    commit c60143b3b7b6b1824fb733a90b279d959e241333
    Author: Eric Orth <ericorth@chromium.org>
    Date:   Fri May 19 14:02:05 2023 +0000
    
        Remove optionality from result lists in HostCache::Entry
    
        The ability to track nullopt/nullptr vs empty is soon going away as a
        simplification with the switch to HostResolverInternalResult, so soon
        anything going through HostResolverInternalResult will just return
        empty lists for what would have previously been nullopt or nullptr.
        Make this change now with HostCache::Entry too, just to separate off
        all of the test/plumbing churn that would occur when the switch is
        made to start converting some internal results to
        HostResolverInternalResult.
    
        This is technically a behavior change for the overall HostResolver
        API (and mojo API), because while I'm leaving things nullable/optional
        in those APIs (to be cleaned up in a future CL), they may now sometimes
        now return empty instead of null, depending on whether or not results
        went through HostCache::Entry. But the behavior change doesn't actually
        matter because nothing outside of tests was ever actually calling the
        Get*Results() methods in situations where it would have been
        nullopt/nullptr (e.g. nobody was calling GetTextResults() after address
        requests).
    
        Bug: 1381506, 1290920

--- chromium_src/services/network/resolve_host_request.cc
+++ chromium_src/services/network/resolve_host_request.cc
@@ -3,10 +3,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#define BRAVE_START                                   \
-  if (internal_request_->GetTextResults()) {          \
-    response_client->OnTextResults(                   \
-        internal_request_->GetTextResults().value()); \
+#define BRAVE_START                                                       \
+  if (internal_request_->GetTextResults() &&                              \
+      !internal_request_->GetTextResults()->empty()) {                    \
+    response_client->OnTextResults(*internal_request_->GetTextResults()); \
   }
 
 #include "src/services/network/resolve_host_request.cc"
