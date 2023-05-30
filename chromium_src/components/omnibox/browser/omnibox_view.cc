c8bb7d9b88fa18d3fbd939ca557667032df4f3da
Claudio DeSouza <cdesouza@igalia.com>
2023-05-30 16:09:47 +0100

    OmniboxController overriding to single contructor
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/8a0c9919bab5e3af6a42966ae902ab8d71e80c37
    
    commit 8a0c9919bab5e3af6a42966ae902ab8d71e80c37
    Author: Moe Ahmadi <mahmadi@chromium.org>
    Date:   Mon May 15 23:05:06 2023 +0000
    
        [omnibox] Single way to construct OmniboxController
    
        `OmniboxController` provides two constructor, one for the realbox which
        does not use `OmniboxView` and passes an instance of
        `AutocompleteController`, and another for the omnibox which does use
        `OmniboxView` and whose `AutocompleteController` is created inside the
        `OmniboxController`.
    
        This change introduces a single constructor for `OmniboxController`.
        making realbox explicitly pass a nullptr for `OmniboxView` and use
        the `AutocompleteController` instance created by `OmniboxController`.
    
        Bug: 1404748

--- chromium_src/components/omnibox/browser/omnibox_view.cc
+++ chromium_src/components/omnibox/browser/omnibox_view.cc
@@ -13,13 +13,6 @@ class BraveOmniboxController : public OmniboxController {
                          OmniboxEditModelDelegate* edit_model_delegate,
                          std::unique_ptr<OmniboxClient> client)
       : OmniboxController(view, edit_model_delegate, std::move(client)) {}
-  BraveOmniboxController(
-      OmniboxEditModelDelegate* edit_model_delegate,
-      std::unique_ptr<AutocompleteController> autocomplete_controller,
-      std::unique_ptr<OmniboxClient> client)
-      : OmniboxController(edit_model_delegate,
-                          std::move(autocomplete_controller),
-                          std::move(client)) {}
   BraveOmniboxController(const BraveOmniboxController&) = delete;
   BraveOmniboxController& operator=(const BraveOmniboxController&) = delete;
   ~BraveOmniboxController() override = default;
