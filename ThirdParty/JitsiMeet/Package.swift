e77b1ede4fcfd301e9dde60b1fdd77740baff76a
Kyle Hickinson <kylehickinson@users.noreply.github.com>
2023-06-02 14:31:49 -0400

    Fix #7524: Use SPM to fetch Jitsi WebRTC (#7562)

--- ThirdParty/JitsiMeet/Package.swift
+++ ThirdParty/JitsiMeet/Package.swift
@@ -5,18 +5,18 @@ import PackageDescription
 
 let package = Package(
   name: "JitsiMeet",
+  platforms: [.iOS(.v15), .macOS(.v11)],
   products: [
-    .library(name: "JitsiMeet", targets: ["JitsiMeetSDK", "WebRTC"]),
+    .library(name: "JitsiMeet", targets: ["JitsiMeet", "JitsiMeetSDK"]),
   ],
-  dependencies: [],
+  dependencies: [
+    .package(url: "https://github.com/jitsi/webrtc", revision: "83690c8ecfd2d745eb0a41c77de6c4e740c18b80")
+  ],
   targets: [
+    .target(name: "JitsiMeet", dependencies: [.product(name: "WebRTC", package: "webrtc")]),
     .binaryTarget(
       name: "JitsiMeetSDK",
       path: "JitsiMeetSDK.xcframework"
-    ),
-    .binaryTarget(
-      name: "WebRTC",
-      path: "WebRTC.xcframework"
     ),
   ]
 )
