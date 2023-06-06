3dd8412837fef353e75f18ae938ef140ae0ee4cb
Anton Paymyshev <anton.paymyshev@gmail.com>
2023-06-06 18:45:57 +0700

    cleanup

--- components/brave_wallet/browser/brave_wallet_utils.cc
+++ components/brave_wallet/browser/brave_wallet_utils.cc
@@ -23,7 +23,6 @@
 #include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
-#include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/time/time.h"
 #include "base/values.h"
