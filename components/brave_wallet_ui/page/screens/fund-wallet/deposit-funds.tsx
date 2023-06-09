e9b64b4e7aac2c32486c4b7713513d63f247c147
Douglas Daniel <douglashdaniel@gmail.com>
2023-06-09 13:06:36 -0500

    fix(wallet): Auto Selected Account on Fund Screens

--- components/brave_wallet_ui/page/screens/fund-wallet/deposit-funds.tsx
+++ components/brave_wallet_ui/page/screens/fund-wallet/deposit-funds.tsx
@@ -244,11 +244,12 @@ export const DepositFundsScreen = (props: Props) => {
   // sync default selected account with selected asset
   React.useEffect(() => {
     if (
-      selectedAccount &&
       selectedAsset &&
       selectedAssetNetwork &&
-      accountsForSelectedAssetNetwork.length && // asset is selected & account is available
-      selectedAccount.accountId.coin !== selectedAsset.coin // needs to change accounts to one with correct network
+      // asset is selected & account is available
+      accountsForSelectedAssetNetwork.length &&
+      // needs to change accounts to one with correct network
+      selectedAccount?.accountId?.coin !== selectedAsset.coin
     ) {
       setSelectedAccount(accountsForSelectedAssetNetwork[0])
     }
@@ -256,7 +257,7 @@ export const DepositFundsScreen = (props: Props) => {
     selectedAsset,
     selectedAssetNetwork,
     accountsForSelectedAssetNetwork,
-    selectedAccount
+    selectedAccount?.accountId?.coin
   ])
 
   React.useEffect(() => {
