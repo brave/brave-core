15d0f3810242fb1fe4b50d99fb6891d4c5a7a10f
Lorenzo <lminto@brave.com>
2023-06-12 17:15:55 +0100

    Add tests for train and evaluate model

--- components/brave_federated/task/model_unittest.cc
+++ components/brave_federated/task/model_unittest.cc
@@ -57,4 +57,56 @@ TEST(BraveFederatedLearningModelTest, GetBatchSize) {
   EXPECT_EQ(static_cast<int>(model.GetBatchSize()), g_k_model_spec.batch_size);
 }
 
+TEST(BraveFederatedLearningModelTest, Train) {
+  // Arrange
+  DataSet test_data = {
+      {1.1, 2.2, 3.3, 4.4, 5.5},
+      {1.2, 3.2, 3.4, 5.4, 6.5},
+      {5.0, 4.0, 3.0, 2.0, 1.0},
+      {1.0, 1.0, 1.0, 1.0, 1.0},
+  };
+  ModelSpec model_spec = {
+      static_cast<int>(test_data[0].size()),  // num_params
+      2,                                      // batch_size
+      0.1,                                    // learning_rate
+      1,                                      // num_iterations
+      0.7                                     // threshold
+  };
+  Model model(model_spec);
+
+  // Act
+  PerformanceReport train_report = model.Train(test_data);
+
+  // Assert
+  EXPECT_EQ(train_report.dataset_size, test_data.size());
+  EXPECT_EQ(static_cast<int>(train_report.parameters[0].size()),
+            model_spec.num_params);
+  EXPECT_EQ(train_report.parameters[1].size(), 1U);
+}
+
+TEST(BraveFederatedLearningModelTest, Evaluate) {
+  // Arrange
+  DataSet test_data = {
+      {1.1, 2.2, 3.3, 4.4, 5.5},
+      {1.2, 3.2, 3.4, 5.4, 6.5},
+      {5.0, 4.0, 3.0, 2.0, 1.0},
+      {1.0, 1.0, 1.0, 1.0, 1.0},
+  };
+  ModelSpec model_spec = {
+      static_cast<int>(test_data[0].size()),  // num_params
+      2,                                      // batch_size
+      0.1,                                    // learning_rate
+      1,                                      // num_iterations
+      0.7                                     // threshold
+  };
+  Model model(model_spec);
+
+  // Act
+  PerformanceReport evaluate_report = model.Evaluate(test_data);
+
+  // Assert
+  EXPECT_EQ(evaluate_report.dataset_size, test_data.size());
+  EXPECT_EQ(evaluate_report.parameters.size(), 0U);
+}
+
 }  // namespace brave_federated
