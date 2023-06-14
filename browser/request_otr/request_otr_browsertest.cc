5c22c83e0082a0c5ba874f07b92bb490331b3056
Mark Pilgrim <43750833+pilgrim-brave@users.noreply.github.com>
2023-06-14 17:13:51 -0400

    Add test for HTTP header that triggers OTR tab (#18713)

--- browser/request_otr/request_otr_browsertest.cc
+++ browser/request_otr/request_otr_browsertest.cc
@@ -50,6 +50,7 @@ using ::testing::_;
 namespace {
 
 const char kTestDataDirectory[] = "request-otr-data";
+const char kRequestOTRResponseHeader[] = "Request-OTR";
 
 class TestObserver : public infobars::InfoBarManager::Observer {
  public:
@@ -57,6 +58,22 @@ class TestObserver : public infobars::InfoBarManager::Observer {
   ~TestObserver() override = default;
   MOCK_METHOD(void, OnInfoBarAdded, (infobars::InfoBar * infobar), (override));
 };
+
+std::unique_ptr<net::test_server::HttpResponse> RespondWithCustomHeader(
+    const net::test_server::HttpRequest& request) {
+  auto http_response = std::make_unique<net::test_server::BasicHttpResponse>();
+  http_response->set_code(net::HTTP_OK);
+  http_response->set_content_type("text/plain");
+  http_response->set_content("Well OK I guess");
+  if (request.relative_url.find("include-response-header-with-1") !=
+      std::string::npos) {
+    http_response->AddCustomHeader(kRequestOTRResponseHeader, "1");
+  } else if (request.relative_url.find("include-response-header-with-0") !=
+             std::string::npos) {
+    http_response->AddCustomHeader(kRequestOTRResponseHeader, "0");
+  }
+  return http_response;
+}
 
 }  // namespace
 
@@ -506,6 +523,41 @@ IN_PROC_BROWSER_TEST_F(RequestOTRServiceWorkerBrowserTest,
   NavigateTo(https_server_.GetURL("sensitive.a.com",
                                   "/workers/service_worker_setup.html"));
   ASSERT_TRUE(content::ExecJs(web_contents(), "setup();"));
+}
+
+// Define a subclass that sets up a special HTTP server that responds with
+// a custom header to trigger an OTR tab.
+class RequestOTRCustomHeaderBrowserTest : public RequestOTRBrowserTest {
+ public:
+  void SetUp() override {
+    content::SetupCrossSiteRedirector(embedded_test_server());
+    embedded_test_server()->RegisterRequestHandler(
+        base::BindRepeating(&RespondWithCustomHeader));
+    ASSERT_TRUE(embedded_test_server()->Start());
+    ExtensionBrowserTest::SetUp();
+  }
+};
+
+IN_PROC_BROWSER_TEST_F(RequestOTRCustomHeaderBrowserTest,
+                       CustomHeaderShowsInterstitial) {
+  SetRequestOTRPref(RequestOTRService::RequestOTRActionOption::kAsk);
+
+  // No Request-OTR header -> do not show interstitial
+  GURL url = embedded_test_server()->GetURL("z.com", "/simple.html");
+  NavigateTo(url);
+  ASSERT_FALSE(IsShowingInterstitial());
+
+  // 'Request-OTR: 1' header -> show interstitial
+  url = embedded_test_server()->GetURL(
+      "z.com", "/simple.html?test=include-response-header-with-1");
+  NavigateTo(url);
+  ASSERT_TRUE(IsShowingInterstitial());
+
+  // 'Request-OTR: 0' header -> do not show interstitial
+  url = embedded_test_server()->GetURL(
+      "z.com", "/simple.html?test=include-response-header-with-0");
+  NavigateTo(url);
+  ASSERT_FALSE(IsShowingInterstitial());
 }
 
 }  // namespace request_otr
