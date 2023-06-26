fcb1b64940b5edb8b7089ffa508a2f268b47aee2
sergei <spylogsster@gmail.com>
2023-06-26 16:56:40 +0300

    Move service_constants.h/cc to brave/components/brave_vpn/common/wireguard
    Move DEPS to brave/components/brave_vpn/common/wireguard

--- components/brave_vpn/browser/connection/wireguard/win/brave_vpn_wireguard_service/service/wireguard_tunnel_service.cc
+++ components/brave_vpn/browser/connection/wireguard/win/brave_vpn_wireguard_service/service/wireguard_tunnel_service.cc
@@ -23,8 +23,8 @@
 #include "base/win/windows_types.h"
 #include "brave/components/brave_vpn/browser/connection/common/win/scoped_sc_handle.h"
 #include "brave/components/brave_vpn/browser/connection/common/win/utils.h"
-#include "brave/components/brave_vpn/browser/connection/wireguard/win/brave_vpn_wireguard_service/common/service_constants.h"
 #include "brave/components/brave_vpn/browser/connection/wireguard/win/brave_vpn_wireguard_service/common/wireguard_utils.h"
+#include "brave/components/brave_vpn/common/wireguard/win/service_constants.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace brave_vpn {
