ab830859d4c017c4306c286e6faedd1b0dbeaf38
Emerick Rogul <erogul@brave.com>
2023-06-29 14:20:10 -0400

    Upgrade from Chromium 115.0.5790.40 to Chromium 115.0.5790.56 (1.53.x) (#19090)
    * Upgrade from Chromium 115.0.5790.40 to Chromium 115.0.5790.56
    
    * Upgrade patches from Chromium 115.0.5790.40 to Chromium 115.0.5790.56
    
    * Update pins list timestamp
    
    * Updated strings for Chromium 115.0.5790.56
    
    * Use new internal namespace for kSidePanelCompanion and disable related feature
    
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/8168e68f8e290ab3d73da121d2db3248c9b5c41c
    
    commit 8168e68f8e290ab3d73da121d2db3248c9b5c41c
    Author: Tarun Bansal <tbansal@google.com>
    Date:   Mon Jun 26 19:02:28 2023 +0000
    
        [M115 Merge] [CSC] Enable companion when exps is enabled
    
        This CL dynamically enables Chrome companion if Chrome detects that user
        is enabled in exps. This happens if the
        CompanionEnabledByObservingExpsNavigations feature is enabled.
    
        The CL achieves this by changing some of the existing checks to be
        enabled by either of the 2 field trial experiments: (i) CSC itself is
        enabled as a feature (ii) Detection of exps enrollment is enabled
    
        If detection of exps enrollment is enabled and exps enrollment is
        detected, then companion is dynamically enabled and an entry is pinned
        to the toolbar.
    
        Design discussion: http://shortn/_nrSG5apNyX
    
        (cherry picked from commit 083f2748173dcfc19fca70cc40390b1058d54e11)
    
        Bug: b:286111571, 1449021
    
    ---------
    
    Co-authored-by: brave-builds <brave-builds+devops@brave.com>

--- chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
+++ chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
@@ -541,9 +541,9 @@ bool ParseCertificatesFile(base::StringPiece certs_input,
                            Pinsets* pinsets,
                            base::Time* timestamp) {
   constexpr base::StringPiece brave_certs = R"brave_certs(
-# Last updated: Fri Jun 23 10:11:24 UTC 2023
+# Last updated: Wed Jun 28 16:55:46 UTC 2023
 PinsListTimestamp
-1687515084
+1687971346
 
 # =====BEGIN BRAVE ROOTS ASC=====
 #From https://www.amazontrust.com/repository/
