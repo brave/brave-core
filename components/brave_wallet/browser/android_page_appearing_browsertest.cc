4033cce198035d5e1249361d18e239b7640ec5ea
Simone Arpe <simon.arpe@gmail.com>
2023-06-30 14:14:39 +0200

    Apply code formatting

--- components/brave_wallet/browser/android_page_appearing_browsertest.cc
+++ components/brave_wallet/browser/android_page_appearing_browsertest.cc
@@ -432,8 +432,7 @@ IN_PROC_BROWSER_TEST_F(AndroidPageAppearingBrowserTest, TestSendPageAppearing) {
       << "Could not execute script: " << result.error;
 
   EXPECT_TRUE(console_observer.Wait());
-  VerifyConsoleOutputNoErrors(
-      console_observer, blink::mojom::ConsoleMessageLevel::kWarning,
-      {});
+  VerifyConsoleOutputNoErrors(console_observer,
+                              blink::mojom::ConsoleMessageLevel::kWarning, {});
 }
 }  // namespace brave_wallet
