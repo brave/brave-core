345150dae46e84c097441662bea239a278ada1e2
Claudio DeSouza <cdesouza@brave.com>
2023-07-22 06:35:25 +0100

    Add missing `GetViewBoundsInScreen` for wallet permission
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/583f07e3957db9bc6eaa936d46a30cc38446ebef
    
    commit 583f07e3957db9bc6eaa936d46a30cc38446ebef
    Author: Thomas Nguyen <tungnh@chromium.org>
    Date:   Wed Jul 19 17:45:37 2023 +0000
    
        Dont show popup if its bounds intersect with permission prompt
    
        Bug: 1441228

--- browser/permissions/mock_permission_lifetime_prompt.h
+++ browser/permissions/mock_permission_lifetime_prompt.h
@@ -22,6 +22,7 @@ class MockPermissionLifetimePrompt : public PermissionPrompt {
   bool UpdateAnchor() override;
   TabSwitchingBehavior GetTabSwitchingBehavior() override;
   PermissionPromptDisposition GetPromptDisposition() const override;
+  absl::optional<gfx::Rect> GetViewBoundsInScreen() const override;
 
   Delegate* delegate() { return delegate_; }
   void ResetFactory();
