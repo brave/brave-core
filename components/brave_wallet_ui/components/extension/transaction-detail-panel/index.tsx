3a4e873954ab64a58a9af41525c955e7a7e7d2ae
Josh Leonard <30185185+josheleonard@users.noreply.github.com>
2023-07-26 16:46:35 -0400

    fix: incorrect pending transaction fee fiat price

--- components/brave_wallet_ui/components/extension/transaction-detail-panel/index.tsx
+++ components/brave_wallet_ui/components/extension/transaction-detail-panel/index.tsx
@@ -39,7 +39,8 @@ import {
   isSolanaTransaction,
   isSwapTransaction,
   parseTransactionWithPrices,
-  findTransactionToken
+  findTransactionToken,
+  getETHSwapTransactionBuyAndSellTokens
 } from '../../../utils/tx-utils'
 import { getPriceIdForToken } from '../../../utils/api-utils'
 import { toProperCase } from '../../../utils/string-utils'
@@ -151,12 +152,18 @@ const TransactionDetailPanel = (props: Props) => {
   }, [transactionsNetwork])
 
   const txToken = findTransactionToken(transaction, combinedTokensList)
+  const { buyToken, sellToken } = getETHSwapTransactionBuyAndSellTokens({
+    tx: transaction,
+    tokensList: combinedTokensList,
+    nativeAsset: networkAsset
+  })
 
-  const tokenPriceIds = React.useMemo(() =>
-    txToken && networkAsset
-      ? [getPriceIdForToken(txToken), getPriceIdForToken(networkAsset)]
-      : [],
-    [txToken, networkAsset]
+  const tokenPriceIds = React.useMemo(
+    () =>
+      [txToken, networkAsset, buyToken, sellToken]
+        .filter((t): t is BraveWallet.BlockchainToken => Boolean(t))
+        .map(getPriceIdForToken),
+    [txToken, networkAsset, buyToken, sellToken]
   )
 
   const {
