01caa873d925b94f3503cedee99ce1758588b1ee
Simone Arpe <simon.arpe@gmail.com>
2023-07-27 12:10:15 +0200

    Fix pylint errors for undefined variable

--- chromium_src/build/android/gradle/generate_gradle.py
+++ chromium_src/build/android/gradle/generate_gradle.py
@@ -2,7 +2,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at https://mozilla.org/MPL/2.0/.
-
+"""A inline part of generate_gradle.py"""
 import os
 
 import override_utils
@@ -12,8 +12,9 @@ _INPUT_SRC_JARS = 'input_srcjars'
 _BRAVE_GEN_COMPONENTS = os.path.join('gen', 'brave', 'components')
 
 
+# pylint: disable=undefined-variable
 @override_utils.override_method(_ProjectEntry)
-def GeneratedJavaSubdir(orig_func, self):
+def GeneratedJavaSubdir(self, _):
     gen_path = os.path.join('gen', self.GradleSubdir(), _GENERATED_JAVA_SUBDIR)
     if gen_path.startswith(_BRAVE_GEN_COMPONENTS):
         gen_path_src_jars = os.path.join(gen_path, _INPUT_SRC_JARS)
