3db8fa0bbf1711ef787849dad1f52370db8e92d9
Claudio DeSouza <cdesouza@brave.com>
2023-07-31 15:50:09 +0100

    `QRCodeGenerator` moved into its own namespace
    This class was sitting on the global namespace before, but recent
    changes upstream have placed it under `qr_code_generator`.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/b51d821ff0923aed5c072ec861b10641df930046
    
    commit b51d821ff0923aed5c072ec861b10641df930046
    Author: Lukasz Anforowicz <lukasza@chromium.org>
    Date:   Wed Jul 26 16:32:57 2023 +0000
    
        Put `//components/qr_code_generator` into `qr_code_generator` namespace.
    
        This CL puts the C++ code under `//components/qr_code_generator` into
        the `qr_code_generator`.  This is motivated by the desire to:
    
        1. Follow the C++ style guide:
           https://google.github.io/styleguide/cppguide.html#Namespaces:
           "With few exceptions, place code in a namespace."
    
        2. Ensure that the C++/Rust FFI of QR code generator is a good role
           model for replicating in future C++/Rust projects.  In particular,
           this CL add the namespace to
           `#[cxx::bridge(namespace = "qr_code_generator")]`.
    
        To keep the scope of this CL small, we avoid changing:
    
        A. The `dino_image` namespace used by
           `//components/qr_code_generator/dino_image.h`.
    
        B. The `qrcode_generator` namespace used by
           `//chrome/services/qrcode_generator`.
    
        Bug: 1431991

--- ios/browser/qr_code_generator/qrcode_generator_service.cc
+++ ios/browser/qr_code_generator/qrcode_generator_service.cc
@@ -270,15 +270,16 @@ std::unique_ptr<GenerateQRCodeResponse> QRCodeGeneratorService::generateQRCode(
   }
   input[data_size - 1] = 0;
 
-  QRCodeGenerator qr;
+  qr_code_generator::QRCodeGenerator qr;
   // The QR version (i.e. size) must be >= 5 because otherwise the dino painted
   // over the middle covers too much of the code to be decodable.
   constexpr int kMinimumQRVersion = 5;
-  absl::optional<QRCodeGenerator::GeneratedCode> qr_data = qr.Generate(
-      base::span<const std::uint8_t>(
-          reinterpret_cast<const std::uint8_t*>(request->data.data()),
-          request->data.size()),
-      kMinimumQRVersion);
+  absl::optional<qr_code_generator::QRCodeGenerator::GeneratedCode> qr_data =
+      qr.Generate(
+          base::span<const std::uint8_t>(
+              reinterpret_cast<const std::uint8_t*>(request->data.data()),
+              request->data.size()),
+          kMinimumQRVersion);
   if (!qr_data || qr_data->data.data() == nullptr ||
       qr_data->data.size() == 0) {
     // The above check should have caught the too-long-URL case.
