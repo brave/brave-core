01fdfb9aaab7b886d84d0c277803469a82b02344
Claudio DeSouza <cdesouza@brave.com>
2023-08-01 21:22:41 +0100

    Straggler format changes for skus
    For some reason codes in skus keeps coming up whenever format is run
    against the branch. This change is purely formatting, to satisfy that.

--- components/speedreader/rust/ffi/speedreader.rs
+++ components/speedreader/rust/ffi/speedreader.rs
@@ -1,3 +1,8 @@
+// Copyright (c) 2020 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use super::*;
 use libc::{c_void, strlen};
 use std::panic::{self, AssertUnwindSafe};
