c15129b91539d0dd03db03b2668f6204fe495fe4
Aleksey Seren <aseren@brave.com>
2023-08-06 17:55:10 -0500

    Enable brave news ads prefs on iOS if ads are enabled.

--- components/brave_ads/core/internal/ads/inline_content_ad_handler.cc
+++ components/brave_ads/core/internal/ads/inline_content_ad_handler.cc
@@ -63,6 +63,9 @@ InlineContentAdHandler::~InlineContentAdHandler() = default;
 void InlineContentAdHandler::MaybeServe(
     const std::string& dimensions,
     MaybeServeInlineContentAdCallback callback) {
+  CHECK(UserHasOptedInToBraveNewsAds())
+      << "Should only be called if the user has opted-in to Brave News Ads";
+
   serving_.MaybeServeAd(
       dimensions,
       base::BindOnce(&InlineContentAdHandler::MaybeServeCallback,
@@ -78,6 +81,8 @@ void InlineContentAdHandler::TriggerEvent(
   CHECK_NE(mojom::InlineContentAdEventType::kServed, event_type)
       << "Should not be called with kServed as this event is handled when "
          "calling MaybeServe";
+  CHECK(UserHasOptedInToBraveNewsAds())
+      << "Should only be called if the user has opted-in to Brave News Ads";
 
   event_handler_.FireEvent(
       placement_id, creative_instance_id, event_type,
