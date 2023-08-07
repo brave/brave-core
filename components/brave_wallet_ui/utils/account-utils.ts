9e1e80e578321bf27dc50c5178998b06d6030488
brave-builds <brave-builds+devops@brave.com>
2023-08-07 20:17:00 +0000

    Uplift of #19589 (squashed) to beta

--- components/brave_wallet_ui/utils/account-utils.ts
+++ components/brave_wallet_ui/utils/account-utils.ts
@@ -58,6 +58,9 @@ export const findAccountName = <
   accounts: T[],
   address: string
 ) => {
+  if (!address) {
+    return undefined
+  }
   return accounts.find(
     (account) => account.address.toLowerCase() === address.toLowerCase()
   )?.name
