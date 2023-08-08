1b68aadd115e4bc4017a3d7aa4cffb6b4ec54293
Aleksey Seren <aseren@brave.com>
2023-08-08 12:15:55 -0500

    Make walletInfo parameter optional for initializeWithSysInfo.

--- ios/browser/api/ads/brave_ads.mm
+++ ios/browser/api/ads/brave_ads.mm
@@ -255,7 +255,7 @@ brave_ads::mojom::DBCommandResponseInfoPtr RunDBTransactionOnTaskRunner(
 
 - (void)initializeWithSysInfo:(BraveAdsSysInfo*)sysInfo
              buildChannelInfo:(BraveAdsBuildChannelInfo*)buildChannelInfo
-                   walletInfo:(BraveAdsWalletInfo*)walletInfo
+                   walletInfo:(nullable BraveAdsWalletInfo*)walletInfo
                    completion:(void (^)(bool))completion {
   if ([self isAdsServiceRunning]) {
     completion(false);
@@ -273,7 +273,11 @@ brave_ads::mojom::DBCommandResponseInfoPtr RunDBTransactionOnTaskRunner(
                                  : brave_ads::mojom::BuildChannelInfo::New();
   ads->SetBuildChannel(std::move(cppBuildChannelInfo));
   ads->SetFlags(brave_ads::BuildFlags());
-  ads->Initialize(walletInfo.cppObjPtr, base::BindOnce(^(const bool success) {
+
+  auto cppWalletInfo =
+      walletInfo ? walletInfo.cppObjPtr : brave_ads::mojom::WalletInfoPtr();
+  ads->Initialize(std::move(cppWalletInfo),
+                  base::BindOnce(^(const bool success) {
                     [self periodicallyCheckForAdsResourceUpdates];
                     [self registerAdsResources];
                     if (success && self->adsClientNotifier != nil) {
