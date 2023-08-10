1affd5b70181b91a1b14ff89f4fb0437ad32f2f0
Taher <8665427+nullhook@users.noreply.github.com>
2023-08-09 19:14:35 -0700

    Added custom tooltip

--- components/ai_chat/resources/page/components/site_title/index.tsx
+++ components/ai_chat/resources/page/components/site_title/index.tsx
@@ -4,6 +4,7 @@
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 import * as React from 'react'
+import classnames from '$web-common/classnames'
 import Icon from '@brave/leo/react/icon'
 import Button from '@brave/leo/react/button'
 
@@ -16,9 +17,51 @@ interface SiteTitleProps {
   onDisconnectClick?: () => void
 }
 
+type Timer = ReturnType<typeof setTimeout>
+
+const HOVER_TRANSITION_DELAY = 500
+
+interface ToolTipProps {
+  isVisible: boolean
+}
+
+function Tooltip(props: ToolTipProps) {
+  const tooltipStyles = classnames({
+    [styles.tooltip]: true,
+    [styles.tooltipVisible]: props.isVisible
+  })
+
+  return (
+    <div className={tooltipStyles}>
+      Page content will be sent to Brave Leo along with your messages. Click the disconnect button on the right to start a new conversation about a different topic
+    </div>
+  )
+}
+
 function SiteTitle (props: SiteTitleProps) {
+  const [isTooltipVisible, setIsTooltipVisible] = React.useState(false)
+  const timerId = React.useRef<Timer | undefined>();
+
+  const showTooltipWithDelay = () => {
+    timerId.current = setTimeout(() => {
+      setIsTooltipVisible(true)
+    }, HOVER_TRANSITION_DELAY)
+  }
+
+  const hideTooltip = () => {
+    timerId.current && clearTimeout(timerId.current)
+    setIsTooltipVisible(false)
+  }
+
    return (
-    <div className={styles.box} title="Page content will be sent to Brave Leo along with your messages. Click the disconnect button on the right to start a new conversation about a different topic.">
+    <div
+      className={styles.box}
+      onMouseOver={showTooltipWithDelay}
+      onMouseOut={hideTooltip}
+      onTouchStart={() => setIsTooltipVisible(true)}
+      onTouchEnd={() => setIsTooltipVisible(false)}
+    >
+      <Tooltip isVisible={isTooltipVisible} />
       <div className={styles.favIconBox}>
         { props.favIconUrl && <img src={props.favIconUrl} /> }
       </div>
