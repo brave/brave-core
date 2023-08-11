b22bbb9ff4ffef900806079e69d7bb597c93fe7a
sangwoo.ko <sangwoo108@gmail.com>
2023-08-11 10:18:48 +0900

    Address code review

--- components/playlist/browser/resources/utils/l10n.ts
+++ components/playlist/browser/resources/utils/l10n.ts
@@ -3,7 +3,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import { getLocale } from '../../../../common/locale'
+import { getLocale } from '$web-common/locale'
 
 export type Message =
   | 'braveDefaultPlaylistName'
