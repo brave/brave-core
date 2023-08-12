a5779f0c8922467345e11bc4522cd22eb8915d65
sergei <spylogsster@gmail.com>
2023-08-12 19:19:43 +0300

    wrapped gn files to enable_brave_vpn && is_win

--- browser/resources/settings/brave_overrides/system_page.ts
+++ browser/resources/settings/brave_overrides/system_page.ts
@@ -10,7 +10,7 @@ import {
 } from 'chrome://resources/brave/polymer_overriding.js'
 import { loadTimeData } from '../i18n_setup.js'
 import '../brave_system_page/brave_performance_page.js'
-// <if expr="is_win">
+// <if expr="enable_brave_vpn and is_win">
 import '../brave_system_page/brave_vpn_page.js'
 // </if>
 import '../shortcuts_page/shortcuts_page.js'
@@ -82,7 +82,7 @@ RegisterPolymerTemplateModifications({
       `
     )
 
-    // <if expr="is_win">
+    // <if expr="enable_brave_vpn and is_win">
     templateContent.appendChild(
       html`
         <settings-brave-vpn-page prefs="{{prefs}}">
