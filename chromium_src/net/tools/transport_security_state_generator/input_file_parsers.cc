2ef6f02151a6929af4e0aae999715426536c575c
Max Karolinskiy <41635752+mkarolin@users.noreply.github.com>
2023-08-14 23:40:59 -0400

    Upgrade from Chromium 116.0.5845.82 to Chromium 116.0.5845.96 (1.57.x). (#19677)
    * Upgrade from Chromium 116.0.5845.82 to Chromium 116.0.5845.96
    
    * Upgrade patches from Chromium 116.0.5845.82 to Chromium 116.0.5845.96
    
    * Update pins list timestamp
    
    * Disable failing upstream browser test.
    
    ---------
    
    Co-authored-by: brave-builds <brave-builds+devops@brave.com>

--- chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
+++ chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
@@ -541,9 +541,9 @@ bool ParseCertificatesFile(base::StringPiece certs_input,
                            Pinsets* pinsets,
                            base::Time* timestamp) {
   constexpr base::StringPiece brave_certs = R"brave_certs(
-# Last updated: Wed Aug  9 21:51:58 UTC 2023
+# Last updated: Mon Aug 14 20:16:28 UTC 2023
 PinsListTimestamp
-1691617918
+1692044188
 
 # =====BEGIN BRAVE ROOTS ASC=====
 #From https://www.amazontrust.com/repository/
