4ea7dbae68dc938ac35506a63e718d22c1ef75b4
AlexeyBarabash <alexey@brave.com>
2023-08-15 19:32:58 +0300

    Inline patches => Java asm patch

---
+++ android/java/org/chromium/base/library_loader/BraveLibraryLoader.java
@@ -0,0 +1,61 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+package org.chromium.base.library_loader;
+
+import android.annotation.SuppressLint;
+import android.content.pm.ApplicationInfo;
+import android.os.Build;
+
+import org.chromium.base.BraveReflectionUtil;
+import org.chromium.base.BundleUtils;
+import org.chromium.build.NativeLibraries;
+
+/**
+ * Class to override upstream's LibraryLoader.loadWithSystemLinkerAlreadyLocked
+ * See also BraveLibraryLoaderClassAdapter
+ */
+public class BraveLibraryLoader {
+    public void loadWithSystemLinkerAlreadyLocked(ApplicationInfo appInfo, boolean inZygote) {
+        try {
+            callChromiumLoadWithSystemLinkerAlreadyLocked(appInfo, inZygote);
+        } catch (UnsatisfiedLinkError ex) {
+            loadLibsfromSplits();
+        }
+    }
+
+    private void callChromiumLoadWithSystemLinkerAlreadyLocked(
+            ApplicationInfo appInfo, boolean inZygote) {
+        BraveReflectionUtil.InvokeMethod(LibraryLoader.class, this,
+                "loadWithSystemLinkerAlreadyLocked", ApplicationInfo.class, appInfo, Boolean.class,
+                inZygote);
+    }
+
+    @SuppressLint({"UnsafeDynamicallyLoadedCode"})
+    private void loadLibsfromSplits() {
+        String splitName = getSplitNameByCurrentAbi();
+        for (String library : NativeLibraries.LIBRARIES) {
+            System.load(BundleUtils.getNativeLibraryPath(library, splitName));
+        }
+    }
+
+    private String getSplitNameByCurrentAbi() {
+        assert Build.SUPPORTED_ABIS.length > 0;
+        String currentAbi = Build.SUPPORTED_ABIS[0];
+        switch (currentAbi) {
+            case "arm64-v8a":
+                return "config.arm64_v8a";
+            case "armeabi-v7a":
+                return "config.armeabi_v7a";
+            case "x86_64":
+                return "config.x86_64";
+            case "x86":
+                return "config.x86";
+            default:
+                assert false;
+                return "config.arm64_v8a";
+        }
+    }
+}
---
+++ build/android/bytecode/java/org/brave/bytecode/BraveLibraryLoaderClassAdapter.java
@@ -0,0 +1,26 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+package org.brave.bytecode;
+
+import org.objectweb.asm.ClassVisitor;
+
+/**
+ * Class for patch work on overriding upstream's
+ * LibraryLoader.loadWithSystemLinkerAlreadyLocked.
+ * See also BraveLibraryLoader
+ */
+public class BraveLibraryLoaderClassAdapter extends BraveClassVisitor {
+    static String sLibraryLoaderClassName = "org/chromium/base/library_loader/LibraryLoader";
+    static String sBraveLibraryLoaderClassName =
+            "org/chromium/base/library_loader/BraveLibraryLoader";
+
+    public BraveLibraryLoaderClassAdapter(ClassVisitor visitor) {
+        super(visitor);
+        changeSuperName(sLibraryLoaderClassName, sBraveLibraryLoaderClassName);
+        changeMethodOwner(sLibraryLoaderClassName, "loadWithSystemLinkerAlreadyLocked",
+                sBraveLibraryLoaderClassName);
+    }
+}
