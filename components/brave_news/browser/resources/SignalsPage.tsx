cd1ff57a8650adf3acde1036725aae5f0a9dba5e
Jay Harris <jay.harris@outlook.co.nz>
2023-08-17 12:16:51 +1200

    Display signals nicely

--- components/brave_news/browser/resources/SignalsPage.tsx
+++ components/brave_news/browser/resources/SignalsPage.tsx
@@ -9,8 +9,9 @@ import Card from './feed/Card';
 import Radio from '@brave/leo/react/radioButton'
 import Dropdown from '@brave/leo/react/dropdown';
 import Flex from '../../../brave_new_tab_ui/components/Flex'
-import { Channel, Publisher, Signal } from '../../../../../out/Component/gen/brave/components/brave_news/common/brave_news.mojom.m';
+import { Channel, Publisher } from '../../../../../out/Component/gen/brave/components/brave_news/common/brave_news.mojom.m';
 import styled from 'styled-components';
+import { color, effect, radius, spacing } from '@brave/leo/tokens/css';
 
 interface Props {
 }
@@ -19,38 +20,70 @@ const Container = styled(Flex)`
   width: 800px;
 `
 
-function SignalCard({ item, signals }: { item: Channel | Publisher, signals: { [key: string]: Signal } }) {
-  const key = 'channelName' in item ? item.channelName : item.publisherId
-  const name = 'channelName' in item ? item.channelName : item.publisherName
-  return <Card>
-    <b>{name}</b>
-    <br />
-    <b>Subscribed:</b> {signals[key]?.subscribed}
-    <br />
-    <b>Visit Weighting:</b> {signals[key]?.visitWeight}
-  </Card>
-}
+const SearchBox = styled.input`
+  all: unset;
+  flex: 1;
+  border: 1px solid ${color.divider.subtle};
 
-export default function SignalsPage(props: Props) {
-  const { publishers, channels, signals } = useInspectContext();
-  const [show, setShow] = React.useState<'all' | 'publishers' | 'channels'>('all')
-  const [sort, setSort] = React.useState<'name' | 'subscribed' | 'visitWeight'>('visitWeight')
-
-  const compareSignals = (a: Signal, b: Signal) => {
-    if (sort === 'subscribed') return +b.subscribed - +a.subscribed
-    if (sort === 'visitWeight') return b.visitWeight - a.visitWeight
-    return 0
+  &:hover {
+    border-color: ${color.divider.strong};
   }
 
-  return <Container>
+  &:focus {
+    border-color: ${color.divider.interactive};
+  }
+
+  border-radius: ${radius[8]};
+  box-shadow: ${effect.elevation[3]};
+  padding: ${spacing[8]};
+  height: 32px;
+`
+
+function SignalCards<T>({ items, sort, filter, getName, getKey }: { items: T[], sort: 'name' | 'subscribed' | 'visitWeight', filter: string, getName: (item: T) => string, getKey: (item: T) => string }) {
+  const { signals } = useInspectContext()
+
+  const filteredAndSorted = items
+    .filter(item => getName(item).toLowerCase().includes(filter))
+    .sort((a, b) => {
+      if (sort === 'name') return getName(a).localeCompare(getName(b))
+
+      const aSignal = signals[getKey(a)]
+      const bSignal = signals[getKey(b)]
+
+      if (sort === 'subscribed') return +bSignal.subscribed - +aSignal.subscribed
+      return bSignal.visitWeight - aSignal.visitWeight
+    })
+  return <>
+    {filteredAndSorted.map(a => <Card>
+      <b>{getName(a)}</b>
+      <br />
+      <b>Subscribed:</b> {signals[getKey(a)]?.subscribed.toString()}
+      <br />
+      <b>Visit Weighting:</b> {signals[getKey(a)]?.visitWeight}
+    </Card>)}
+  </>
+}
+
+const getChannelKey = (channel: Channel) => channel.channelName
+const getPublisherKey = (p: Publisher) => p.publisherId
+const getPublisherName = (p: Publisher) => p.publisherName
+
+export default function SignalsPage(props: Props) {
+  const { publishers, channels } = useInspectContext();
+  const [show, setShow] = React.useState<'all' | 'publishers' | 'channels'>('all')
+  const [sort, setSort] = React.useState<'name' | 'subscribed' | 'visitWeight'>('visitWeight')
+  const [filter, setFilter] = React.useState('')
+
+  return <Container direction='column'>
     <h2>Signals Page</h2>
     <Flex direction='row' gap={8}>
-      <Flex direction='column' justify='space-between' gap={8}>
+      <Flex direction='column' gap={8}>
         Show only:
         <Radio name='show' value="all" currentValue={show} onChange={e => setShow(e.detail.value)} />
         <Radio name='show' value="publishers" currentValue={show} onChange={e => setShow(e.detail.value)} />
         <Radio name='show' value="channels" currentValue={show} onChange={e => setShow(e.detail.value)} />
       </Flex>
+      <SearchBox placeholder='filter...' value={filter} onChange={e => setFilter(e.target.value)} />
       <Dropdown value={sort} onChange={e => setSort(e.detail.value)}>
         <div slot="label">Sort by</div>
         <leo-option>name</leo-option>
@@ -62,17 +95,11 @@ export default function SignalsPage(props: Props) {
     <Flex direction='column' gap={8}>
       {show !== 'channels' && <>
         <b>Publishers</b>
-        {Object.values(publishers).sort((a, b) => {
-          if (sort === 'name') return a.publisherName.localeCompare(b.publisherName)
-          return compareSignals(signals[a.publisherId], signals[b.publisherId])
-        }).map(p => <SignalCard item={p} signals={signals} />)}
+        <SignalCards filter={filter} getKey={getPublisherKey} getName={getPublisherName} items={Object.values(publishers)} sort={sort} />
       </>}
       {show !== 'publishers' && <>
         <b>Channels</b>
-        {Object.values(channels).sort((a, b) => {
-          if (sort === 'name') return a.channelName.localeCompare(b.channelName)
-          return compareSignals(signals[a.channelName], signals[b.channelName])
-        }).map(c => <SignalCard item={c} signals={signals} />)}
+        <SignalCards filter={filter} getKey={getChannelKey} getName={getChannelKey} items={Object.values(channels)} sort={sort} />
       </>}
     </Flex>
   </Container>
