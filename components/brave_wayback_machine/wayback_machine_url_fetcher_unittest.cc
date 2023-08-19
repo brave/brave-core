c87ec20b7e6845aaa1e85fec4bb9b01f3d78316e
Simon Hong <shong@brave.com>
2023-08-19 12:13:15 +0900

    Cleard sensitive info from url before sending to wayback machine svc
    fix https://github.com/brave/brave-browser/issues/32385
    
    Cleared user/pwd info from url before sending to wayback machine service.

--- components/brave_wayback_machine/wayback_machine_url_fetcher_unittest.cc
+++ components/brave_wayback_machine/wayback_machine_url_fetcher_unittest.cc
@@ -8,8 +8,10 @@
 #include <utility>
 
 #include "base/run_loop.h"
+#include "base/strings/strcat.h"
 #include "base/test/bind.h"
 #include "base/test/task_environment.h"
+#include "brave/components/brave_wayback_machine/url_constants.h"
 #include "services/data_decoder/public/cpp/test_support/in_process_data_decoder.h"
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
 #include "services/network/test/test_url_loader_factory.h"
@@ -60,10 +62,17 @@ class WaybackMachineURLFetcherUnitTest : public testing::Test {
   void Interceptor(const network::ResourceRequest& request) {
     url_loader_factory_.ClearResponses();
     url_loader_factory_.AddResponse(request.url.spec(), response_text_);
+
+    if (!expected_fetch_url_.is_empty()) {
+      EXPECT_EQ(expected_fetch_url_, request.url);
+      std::move(callback_).Run();
+    }
   }
+
   void SetResponseText(const std::string& response) {
     response_text_ = response;
   }
+
   void Fetch(const GURL& expected_url) {
     base::RunLoop loop;
     client_->SetCallback(loop.QuitClosure());
@@ -72,7 +81,16 @@ class WaybackMachineURLFetcherUnitTest : public testing::Test {
     loop.Run();
   }
 
- private:
+  void TestFetchURL(const GURL& url, const GURL& expected_fetch_url) {
+    base::RunLoop loop;
+    callback_ = loop.QuitClosure();
+    expected_fetch_url_ = expected_fetch_url;
+    wayback_url_loader_->Fetch(url);
+    loop.Run();
+  }
+
+  base::OnceClosure callback_;
+  GURL expected_fetch_url_;
   std::string response_text_;
   base::test::TaskEnvironment task_environment_;
   std::unique_ptr<WaybackMachineURLFetcher> wayback_url_loader_;
@@ -92,4 +110,15 @@ TEST_F(WaybackMachineURLFetcherUnitTest, SanitizedResponse) {
   SetResponseText(
       R"(,{"archived_snapshots":{"closest":{"url":"https://example.com/favicon.ico"}}})");
   Fetch(GURL());
+}
+
+TEST_F(WaybackMachineURLFetcherUnitTest, InputURLSanitizeTest) {
+  constexpr char kInputURL[] = "http://myid:mypwd@test.com/";
+  constexpr char kSanitizedURL[] = "http://test.com/";
+  EXPECT_EQ(GURL(kSanitizedURL),
+            wayback_url_loader_->GetSanitizedURL(GURL(kInputURL)));
+
+  // Test sanitized url is passed to url loader.
+  TestFetchURL(GURL(kInputURL),
+               GURL(base::StrCat({kWaybackQueryURL, kSanitizedURL})));
 }
