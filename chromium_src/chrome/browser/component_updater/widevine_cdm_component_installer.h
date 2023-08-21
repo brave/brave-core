96734c597cb86a95322f0b1226978fea4abf44c8
Michael Herrmann <michael@herrmann.io>
2023-08-21 13:35:01 +0200

    Simplify header file

--- chromium_src/chrome/browser/component_updater/widevine_cdm_component_installer.h
+++ chromium_src/chrome/browser/component_updater/widevine_cdm_component_installer.h
@@ -9,42 +9,28 @@
 #include "brave/components/widevine/static_buildflags.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
-#define RegisterWidevineCdmComponent_Prefix \
-  RegisterWidevineCdmComponent(                                          \
-      ComponentUpdateService* cus,
-
-#if BUILDFLAG(WIDEVINE_ARM64_DLL_FIX)
-#define RegisterWidevineCdmComponent_Body \
-  scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
-#else
-#define RegisterWidevineCdmComponent_Body
-#endif
-
-#define RegisterWidevineCdmComponent_Suffix                     \
-      base::OnceCallback<void()> callback = base::DoNothing()); \
-  void RegisterWidevineCdmComponent_Unused
-
 #ifdef RegisterWidevineCdmComponent
 #pragma push_macro("RegisterWidevineCdmComponent")
 #undef RegisterWidevineCdmComponent
 #define RegisterWidevineCdmComponent_WasDefined
 #endif
 
-#define RegisterWidevineCdmComponent                                    \
-  RegisterWidevineCdmComponent_Prefix RegisterWidevineCdmComponent_Body \
-      RegisterWidevineCdmComponent_Suffix
+namespace component_updater {
 
-#include "src/chrome/browser/component_updater/widevine_cdm_component_installer.h"  // IWYU pragma: export
+class ComponentUpdateService;
+
+void RegisterWidevineCdmComponent(
+    ComponentUpdateService* cus,
+#if BUILDFLAG(WIDEVINE_ARM64_DLL_FIX)
+    scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
+#endif
+    base::OnceCallback<void()> callback = base::DoNothing());
+
+}  // namespace component_updater
 
 #ifdef RegisterWidevineCdmComponent_WasDefined
 #pragma pop_macro("RegisterWidevineCdmComponent")
 #undef RegisterWidevineCdmComponent_WasDefined
-#else
-#undef RegisterWidevineCdmComponent
 #endif
-
-#undef RegisterWidevineCdmComponent_Prefix
-#undef RegisterWidevineCdmComponent_Body
-#undef RegisterWidevineCdmComponent_Suffix
 
 #endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_COMPONENT_UPDATER_WIDEVINE_CDM_COMPONENT_INSTALLER_H_
