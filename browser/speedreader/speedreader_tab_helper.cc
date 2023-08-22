da3ecbcbce650304488b3f4d36adae17a371d357
boocmp <boocmp@yandex.ru>
2023-08-22 09:54:40 +0700

    Removed unused string.

--- browser/speedreader/speedreader_tab_helper.cc
+++ browser/speedreader/speedreader_tab_helper.cc
@@ -313,7 +313,7 @@ void SpeedreaderTabHelper::DOMContentLoaded(
   }
 
   static base::NoDestructor<std::u16string> kSpeedreaderData(GetSpeedreaderData(
-      {{"showOriginalLinkText", IDS_SPEEDREADER_SHOW_ORIGINAL_PAGE_LINK},
+      {{"showOriginalLinkText", IDS_READER_MODE_SHOW_ORIGINAL_PAGE_LINK},
        {"minutesText", IDS_READER_MODE_MINUTES_TEXT}}));
 
   static base::NoDestructor<std::u16string> kJsScript(base::UTF8ToUTF16(
