4b75bc8d5b76e8611732f4e72646de8eb7839be6
mkarolin <max@brave.com>
2023-08-27 11:44:37 -0400

    Hide items in Page Info -> Cookies view.
    Hides description with the link to Settings page and 3P Cookies toggle.
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/d5e718183549b4adb0791d25fc9984caca8a6c2a
    
    commit d5e718183549b4adb0791d25fc9984caca8a6c2a
    Author: Olesia Marukhno <olesiamarukhno@google.com>
    Date:   Wed Jul 5 21:18:50 2023 +0000
    
        [page info][site data] Clean up after site data dialog launch
    
        Bug: 1344787, 1346305

---
+++ chromium_src/chrome/browser/ui/views/page_info/page_info_cookies_content_view.cc
@@ -0,0 +1,25 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "chrome/browser/ui/views/page_info/page_info_cookies_content_view.h"
+
+#define SetCookieInfo SetCookieInfo_ChromiumImpl
+#include "src/chrome/browser/ui/views/page_info/page_info_cookies_content_view.cc"
+#undef SetCookieInfo
+
+void PageInfoCookiesContentView::SetCookieInfo(
+    const CookiesNewInfo& cookie_info) {
+  SetCookieInfo_ChromiumImpl(cookie_info);
+
+  // Remove cookies text and link to settings.
+  RemoveChildView(children()[0]);
+
+  // Hide 3P cookies toggle if shown.
+  if (blocking_third_party_cookies_row_) {
+    blocking_third_party_cookies_row_->SetVisible(false);
+  }
+
+  PreferredSizeChanged();
+}
