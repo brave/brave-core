1f8e3bfc3b80a41fdb401232644b79c37dba742b
brave-builds <45370463+brave-builds@users.noreply.github.com>
2023-08-29 10:09:13 +0900

    Show vpn state at brave://skus-internals (uplift to 1.57.x) (#19887)
    Uplift of #19857 (squashed) to release

--- browser/ui/webui/skus_internals_ui.cc
+++ browser/ui/webui/skus_internals_ui.cc
@@ -10,7 +10,7 @@
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
 #include "base/notreached.h"
-#include "base/values.h"
+#include "brave/browser/brave_browser_process.h"
 #include "brave/browser/ui/webui/brave_webui_source.h"
 #include "brave/components/brave_vpn/common/buildflags/buildflags.h"
 #include "brave/components/skus/browser/pref_names.h"
@@ -22,6 +22,7 @@
 
 #if BUILDFLAG(ENABLE_BRAVE_VPN)
 #include "brave/components/brave_vpn/browser/brave_vpn_service_helper.h"
+#include "brave/components/brave_vpn/browser/connection/brave_vpn_os_connection_api.h"
 #include "brave/components/brave_vpn/common/brave_vpn_utils.h"
 #include "brave/components/brave_vpn/common/pref_names.h"
 #endif
@@ -52,7 +53,6 @@ void SkusInternalsUI::GetEventLog(GetEventLogCallback callback) {
 }
 
 void SkusInternalsUI::GetSkusState(GetSkusStateCallback callback) {
-  // TODO(simonhong): Determine which value should be displayed.
   const auto& skus_state = local_state_->GetDict(skus::prefs::kSkusState);
   base::Value::Dict dict;
 
@@ -80,6 +80,79 @@ void SkusInternalsUI::GetSkusState(GetSkusStateCallback callback) {
   std::move(callback).Run(result);
 }
 
+void SkusInternalsUI::GetVpnState(GetVpnStateCallback callback) {
+  base::Value::Dict dict;
+#if BUILDFLAG(ENABLE_BRAVE_VPN)
+#if !BUILDFLAG(IS_ANDROID)
+  dict.Set("Last connection error", GetLastVPNConnectionError());
+#endif
+  dict.Set("Order", GetVPNOrderInfo());
+#endif
+  std::string result;
+  base::JSONWriter::Write(dict, &result);
+  std::move(callback).Run(result);
+}
+
+base::Value::Dict SkusInternalsUI::GetVPNOrderInfo() const {
+  base::Value::Dict dict;
+#if BUILDFLAG(ENABLE_BRAVE_VPN)
+  auto* profile = Profile::FromWebUI(web_ui());
+  if (!brave_vpn::IsBraveVPNEnabled(profile->GetPrefs())) {
+    return dict;
+  }
+
+  dict.Set("env",
+           local_state_->GetString(brave_vpn::prefs::kBraveVPNEnvironment));
+
+  const auto& skus_state = local_state_->GetDict(skus::prefs::kSkusState);
+  for (const auto kv : skus_state) {
+    if (!base::StartsWith(kv.first, "skus:")) {
+      continue;
+    }
+
+    // Convert to Value as it's stored as string in local state.
+    auto json_value = base::JSONReader::Read(kv.second.GetString());
+    if (!json_value) {
+      continue;
+    }
+
+    const auto* skus = json_value->GetIfDict();
+    if (!skus) {
+      continue;
+    }
+
+    const auto* orders = skus->FindDict("orders");
+    if (!orders) {
+      continue;
+    }
+    base::Value::Dict order_dict_output;
+    for (const auto order : *orders) {
+      const auto* order_dict = order.second.GetIfDict();
+      if (!order_dict) {
+        continue;
+      }
+
+      if (auto* location = order_dict->FindString("location")) {
+        if (!base::StartsWith(*location, "vpn.")) {
+          continue;
+        }
+        order_dict_output.Set("location", *location);
+      }
+
+      if (auto* id = order_dict->FindString("id")) {
+        order_dict_output.Set("id", *id);
+      }
+      if (auto* expires_at = order_dict->FindString("expires_at")) {
+        order_dict_output.Set("expires_at", *expires_at);
+      }
+    }
+    // Set output with env like {skus:production: {...}}.
+    dict.Set(kv.first, std::move(order_dict_output));
+  }
+#endif
+  return dict;
+}
+
 void SkusInternalsUI::ResetSkusState() {
 #if BUILDFLAG(ENABLE_BRAVE_VPN)
   // VPN service caches credentials. It should be cleared also
@@ -94,4 +167,13 @@ void SkusInternalsUI::ResetSkusState() {
   local_state_->ClearPref(skus::prefs::kSkusState);
 }
 
+std::string SkusInternalsUI::GetLastVPNConnectionError() const {
+  std::string error;
+#if BUILDFLAG(ENABLE_BRAVE_VPN)
+  auto* api = g_brave_browser_process->brave_vpn_os_connection_api();
+  CHECK(api);
+  error = api->GetLastConnectionError();
+#endif
+  return error;
+}
 WEB_UI_CONTROLLER_TYPE_IMPL(SkusInternalsUI)
