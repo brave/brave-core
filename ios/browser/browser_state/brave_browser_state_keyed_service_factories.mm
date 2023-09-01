7b75f735997c976516f8b98534b318948c4aa373
bridiver <34129+bridiver@users.noreply.github.com>
2023-08-31 19:36:11 -0700

    fix license header

--- ios/browser/browser_state/brave_browser_state_keyed_service_factories.mm
+++ ios/browser/browser_state/brave_browser_state_keyed_service_factories.mm
@@ -1,7 +1,7 @@
 /* Copyright (c) 2020 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "brave/ios/browser/browser_state/brave_browser_state_keyed_service_factories.h"
 
