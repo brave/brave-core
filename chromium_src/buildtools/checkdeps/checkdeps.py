b57b9dba3ef7cee47ddae44b1a13a4e1dd3552a9
Aleksey Khoroshilov <akhoroshilov@brave.com>
2023-09-04 18:02:30 +0700

    Reformat checkdeps.py.

--- chromium_src/buildtools/checkdeps/checkdeps.py
+++ chromium_src/buildtools/checkdeps/checkdeps.py
@@ -10,35 +10,35 @@ from builddeps import DepsBuilder, NormalizePath
 
 
 class BraveDepsBuilder(DepsBuilder):
-  def _ApplyDirectoryRules(self, existing_rules, dir_path_local_abs):
-    directory_rules, excluded_subdirs = super(DepsBuilder,
-                                              self)._ApplyDirectoryRules(
-                                                  existing_rules,
-                                                  dir_path_local_abs)
+    def _ApplyDirectoryRules(self, existing_rules, dir_path_local_abs):
+        directory_rules, excluded_subdirs = super(DepsBuilder,
+                                                  self)._ApplyDirectoryRules(
+                                                      existing_rules,
+                                                      dir_path_local_abs)
 
-    dir_path_norm = NormalizePath(dir_path_local_abs)
-    if '/brave/chromium_src' in dir_path_norm:
-      # Append rules from the original `src/...` dir.
-      root_src_dir_path_norm = dir_path_norm.replace('/brave/chromium_src', '',
-                                                     1)
-      directory_rules, _ = super(DepsBuilder, self)._ApplyDirectoryRules(
-          directory_rules, root_src_dir_path_norm)
+        dir_path_norm = NormalizePath(dir_path_local_abs)
+        if '/brave/chromium_src' in dir_path_norm:
+            # Append rules from the original `src/...` dir.
+            root_src_dir_path_norm = dir_path_norm.replace(
+                '/brave/chromium_src', '', 1)
+            directory_rules, _ = super(DepsBuilder, self)._ApplyDirectoryRules(
+                directory_rules, root_src_dir_path_norm)
 
-      # Add `+src/...` rule.
-      root_src_relative_dir = 'src/' + posixpath.relpath(
-          root_src_dir_path_norm, NormalizePath(self.base_directory))
-      directory_rules.AddRule('+' + root_src_relative_dir,
-                              root_src_relative_dir,
-                              'Src rule for ' + root_src_relative_dir)
+            # Add `+src/...` rule.
+            root_src_relative_dir = 'src/' + posixpath.relpath(
+                root_src_dir_path_norm, NormalizePath(self.base_directory))
+            directory_rules.AddRule('+' + root_src_relative_dir,
+                                    root_src_relative_dir,
+                                    'Src rule for ' + root_src_relative_dir)
 
-      # Add `+../gen/...` rule.
-      root_src_relative_dir = '../gen/' + posixpath.relpath(
-          root_src_dir_path_norm, NormalizePath(self.base_directory))
-      directory_rules.AddRule('+' + root_src_relative_dir,
-                              root_src_relative_dir,
-                              'Gen rule for ' + root_src_relative_dir)
+            # Add `+../gen/...` rule.
+            root_src_relative_dir = '../gen/' + posixpath.relpath(
+                root_src_dir_path_norm, NormalizePath(self.base_directory))
+            directory_rules.AddRule('+' + root_src_relative_dir,
+                                    root_src_relative_dir,
+                                    'Gen rule for ' + root_src_relative_dir)
 
-    return directory_rules, excluded_subdirs
+        return directory_rules, excluded_subdirs
 
 
 DepsBuilder = BraveDepsBuilder
