f208073f1480493236c47a580d8746c5aed2bb7c
Josh Leonard <30185185+josheleonard@users.noreply.github.com>
2023-09-05 18:56:51 -0400

    chore: add render options to tx fee component (#20000)
    fix: add render options to tx fee component

--- components/brave_wallet_ui/components/extension/pending-transaction-network-fee/pending-transaction-network-fee.tsx
+++ components/brave_wallet_ui/components/extension/pending-transaction-network-fee/pending-transaction-network-fee.tsx
@@ -4,10 +4,22 @@
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
 import * as React from 'react'
+import { skipToken } from '@reduxjs/toolkit/query'
+
+// types
+import { BraveWallet } from '../../../constants/types'
 
 // utils
 import Amount from '../../../utils/amount'
 import { getLocale } from '../../../../common/locale'
+import { getCoinFromTxDataUnion } from '../../../utils/network-utils'
+import {
+  getGasFeeFiatValue,
+  getTransactionGasFee,
+  isSolanaTransaction
+} from '../../../utils/tx-utils'
+import { getPriceIdForToken } from '../../../utils/api-utils'
+import { makeNetworkAsset } from '../../../options/asset-options'
 
 // components
 import { CreateNetworkIcon } from '../../shared/create-network-icon/index'
@@ -16,10 +28,13 @@ import { EditButton } from '../confirm-transaction-panel/style'
 
 // hooks
 import {
-  usePendingTransactions //
+  useSelectedPendingTransaction //
 } from '../../../common/hooks/use-pending-transaction'
 import {
-  useGetDefaultFiatCurrencyQuery //
+  useGetDefaultFiatCurrencyQuery,
+  useGetNetworkQuery,
+  useGetSolanaEstimatedFeeQuery,
+  useGetTokenSpotPricesQuery
 } from '../../../common/slices/api.slice'
 
 // style
@@ -35,28 +50,90 @@ import {
 interface Props {
   onToggleEditGas?: () => void
   onToggleAdvancedTransactionSettings?: () => void
+  feeDisplayMode?: 'fiat' | 'crypto'
+  showNetworkLogo?: boolean
 }
 
 export const PendingTransactionNetworkFeeAndSettings: React.FC<Props> = ({
   onToggleAdvancedTransactionSettings,
-  onToggleEditGas
+  onToggleEditGas,
+  feeDisplayMode = 'fiat',
+  showNetworkLogo
 }) => {
   // custom hooks
-  const { transactionDetails, transactionsNetwork } = usePendingTransactions()
+  const selectedPendingTransaction = useSelectedPendingTransaction()
 
   // queries
-  const { data: defaultFiatCurrency } = useGetDefaultFiatCurrencyQuery()
+  const { data: txNetwork } = useGetNetworkQuery(
+    selectedPendingTransaction
+      ? {
+          chainId: selectedPendingTransaction.chainId,
+          coin: getCoinFromTxDataUnion(selectedPendingTransaction.txDataUnion)
+        }
+      : skipToken
+  )
 
+  const { data: defaultFiatCurrency } = useGetDefaultFiatCurrencyQuery(
+    feeDisplayMode === 'fiat' ? undefined : skipToken
+  )
+
+  const networkTokenPriceId = txNetwork
+    ? getPriceIdForToken(makeNetworkAsset(txNetwork))
+    : ''
+
+  const { data: pricesRegistry } = useGetTokenSpotPricesQuery(
+    txNetwork && defaultFiatCurrency
+      ? {
+          ids: [networkTokenPriceId],
+          toCurrency: defaultFiatCurrency,
+          timeframe: BraveWallet.AssetPriceTimeframe.Live
+        }
+      : skipToken
+  )
+
+  const isSolTx = isSolanaTransaction(selectedPendingTransaction)
+
+  const { data: solFeeEstimate = '' } = useGetSolanaEstimatedFeeQuery(
+    selectedPendingTransaction && isSolTx
+      ? {
+          chainId: selectedPendingTransaction?.chainId || '',
+          txId: selectedPendingTransaction?.id || ''
+        }
+      : skipToken
+  )
+
+  // computed
+  const networkFee = selectedPendingTransaction
+    ? isSolTx
+      ? solFeeEstimate
+      : getTransactionGasFee(selectedPendingTransaction)
+    : ''
+
+  const networkFeeFiat = getGasFeeFiatValue({
+    gasFee: networkFee,
+    networkSpotPrice: pricesRegistry?.[networkTokenPriceId]?.price || '',
+    txNetwork
+  })
+
+  // render
   return (
     <NetworkFeeAndSettingsContainer>
       <NetworkFeeContainer>
         <NetworkFeeTitle>{getLocale('braveWalletNetworkFees')}</NetworkFeeTitle>
         <NetworkFeeValue>
-          <CreateNetworkIcon network={transactionsNetwork} marginRight={0} />
-          {transactionDetails?.gasFeeFiat ? (
-            new Amount(transactionDetails.gasFeeFiat).formatAsFiat(
-              defaultFiatCurrency
+          {showNetworkLogo ? (
+            <CreateNetworkIcon network={txNetwork} marginRight={0} />
+          ) : null}
+          {feeDisplayMode === 'fiat' ? (
+            networkFeeFiat ? (
+              new Amount(networkFeeFiat).formatAsFiat(defaultFiatCurrency)
+            ) : (
+              <LoadingSkeleton width={38} />
             )
+          ) : txNetwork ? (
+            new Amount(networkFee)
+              .divideByDecimals(txNetwork.decimals)
+              .formatAsAsset(6, txNetwork?.symbol)
           ) : (
             <LoadingSkeleton width={38} />
           )}
