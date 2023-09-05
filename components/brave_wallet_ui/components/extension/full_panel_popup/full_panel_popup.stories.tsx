02ca061691c28676117aff00ddba20f47c3038db
Josh Leonard <30185185+josheleonard@users.noreply.github.com>
2023-09-05 16:41:24 -0400

    chore: add full screen panel popup component

---
+++ components/brave_wallet_ui/components/extension/full_panel_popup/full_panel_popup.stories.tsx
@@ -0,0 +1,22 @@
+// Copyright (c) 2023 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import * as React from 'react'
+
+import { FullPanelPopup } from './full_panel_popup'
+
+export const _FullPanelPopup = () => {
+  return (
+    <FullPanelPopup onClose={() => alert('closed')}>
+      CONTENT HERE
+    </FullPanelPopup>
+  )
+}
+
+_FullPanelPopup.story = {
+  name: 'Full Panel Popup'
+}
+
+export default _FullPanelPopup
---
+++ components/brave_wallet_ui/components/extension/full_panel_popup/full_panel_popup.tsx
@@ -0,0 +1,40 @@
+// Copyright (c) 2023 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import * as React from 'react'
+
+import { CloseIcon, Column, Row } from '../../shared/style'
+import {
+  FullScreenPanelPopupWrapper,
+  IconButton
+} from './full_panel_popup.style'
+
+interface Props {
+  kind?: 'danger'
+  onClose: () => void
+}
+
+export const FullPanelPopup: React.FC<Props> = ({
+  children,
+  kind,
+  onClose
+}) => {
+  return (
+    <FullScreenPanelPopupWrapper kind={kind}>
+      <Column fullHeight fullWidth justifyContent='flex-start'>
+        <Row justifyContent='flex-end' alignItems='center' padding={'16px'}>
+          <Column width='20px'>
+            <IconButton kind='plain' onClick={onClose}>
+              <CloseIcon />
+            </IconButton>
+          </Column>
+        </Row>
+        {children}
+      </Column>
+    </FullScreenPanelPopupWrapper>
+  )
+}
+
+export default FullPanelPopup
