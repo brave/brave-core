68d32fcb28d75d170ea9fa77da3a5b6425a74d26
Claudio DeSouza <cdesouza@brave.com>
2023-09-13 18:05:12 +0200

    [v8] Object instances must be tagged
    This change makes sure our overrides use `Tagged<>` objects, when
    interacting with v8 objects, in line with recent upstream changes.
    
    Chromium change:
    https://chromium.googlesource.com/v8/v8/+/a5001584c36018ce64114a6711df733483079930
    
    commit a5001584c36018ce64114a6711df733483079930
    Author: Leszek Swirski <leszeks@chromium.org>
    Date:   Tue Sep 5 17:17:36 2023 +0200
    
        [tagged-ptr] Remove Smi & Object constructors
    
        Remove the last remaining way of creating raw Smi and Object, instead of
        using Tagged<Smi>/Tagged<Object>.
    
        This reverts the default constructor of Tagged<Object> to initialise
        with kNullAddress instead of kZapValue, since there were too many places
        relying on this initialisation for subsequent checks.
    
        Bug: v8:12710

--- chromium_src/v8/src/codegen/compiler.cc
+++ chromium_src/v8/src/codegen/compiler.cc
@@ -9,7 +9,7 @@
   IF_BUILDFLAG(ENABLE_BRAVE_PAGE_GRAPH, {                       \
     auto* page_graph_delegate = isolate->page_graph_delegate(); \
     if (V8_UNLIKELY(page_graph_delegate)) {                     \
-      Object maybe_script = result->shared()->script();         \
+      Tagged<Object> maybe_script = result->shared()->script(); \
       if (IsScript(maybe_script)) {                             \
         const int script_id = Script::cast(maybe_script)->id(); \
         page_graph_delegate->OnEvalScriptCompiled(              \
