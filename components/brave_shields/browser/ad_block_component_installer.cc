25faf993563ca66e51a58d017a52016857a5a756
Jacob Sikorski <jacob.sikorski@gmail.com>
2023-09-12 19:03:07 -0600

    Fix #32799: Use correct components for ad-blocking

--- components/brave_shields/browser/ad_block_component_installer.cc
+++ components/brave_shields/browser/ad_block_component_installer.cc
@@ -1,7 +1,7 @@
-/* Copyright (c) 2021 The Brave Authors. All rights reserved.
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+// Copyright (c) 2023 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
 
 #include "brave/components/brave_shields/browser/ad_block_component_installer.h"
 
@@ -48,17 +48,16 @@ const char kAdBlockFilterListCatalogComponentBase64PublicKey[] =
     "1H8y9SR970LqsUMozu3ioSHtFh/IVgq7Nqy4TljaKsTE+3AdtjiOyHpW9ZaOkA7j"
     "2QIDAQAB";
 
-const char kAdBlockIosDefaultDatComponentName[] = "Brave Ad Block Updater";
-const char kAdBlockIosDefaultDatComponentId[] =
-    "cffkpbalmllkdoenhmdmpbkajipdjfam";
-const char kAdBlockIosDefaultDatComponentBase64PublicKey[] =
-    "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAs0qzJmHSgIiw7IGFCxij"
-    "1NnB5hJ5ZQ1LKW9htL4EBOaMJvmqaDs/wfq0nw/goBHWsqqkMBynRTu2Hxxirvdb"
-    "cugn1Goys5QKPgAvKwDHJp9jlnADWm5xQvPQ4GE1mK1/I3ka9cEOCzPW6GI+wGLi"
-    "VPx9VZrxHHsSBIJRaEB5Tyi5bj0CZ+kcfMnRTsXIBw3C6xJgCVKISQUkd8mawVvG"
-    "vqOhBOogCdb9qza5eJ1Cgx8RWKucFfaWWxKLOelCiBMT1Hm1znAoVBHG/blhJJOD"
-    "5HcH/heRrB4MvrE1J76WF3fvZ03aHVcnlLtQeiNNOZ7VbBDXdie8Nomf/QswbBGa"
-    "VwIDAQAB";
+const char kAdBlockDefaultComponentName[] = "Brave Ad Block Updater";
+const char kAdBlockDefaultComponentId[] = "iodkpdagapdfkphljnddpjlldadblomo";
+const char kAdBlockDefaultComponentBase64PublicKey[] =
+    "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsD/B/MGdz0gh7WkcFARn"
+    "ZTBX9KAw2fuGeogijoI+fET38IK0L+P/trCT2NshqhRNmrDpLzV2+Dmes6PvkA+O"
+    "dQkUV6VbChJG+baTfr3Oo5PdE0WxmP9Xh8XD7p85DQrk0jJilKuElxpK7Yq0JhcT"
+    "Sc3XNHeTwBVqCnHwWZZ+XysYQfjuDQ0MgQpS/s7U04OZ63NIPe/iCQm32stvS/pE"
+    "ya7KdBZXgRBQ59U6M1n1Ikkp3vfECShbBld6VrrmNrl59yKWlEPepJ9oqUc2Wf2M"
+    "q+SDNXROG554RnU4BnDJaNETTkDTZ0Pn+rmLmp1qY5Si0yGsfHkrv3FS3vdxVozO"
+    "PQIDAQAB";
 
 class AdBlockComponentInstallerPolicy
     : public component_updater::ComponentInstallerPolicy {
@@ -170,7 +169,7 @@ void OnRegistered(const std::string& component_id) {
 
 }  // namespace
 
-void RegisterAdBlockIosDefaultDatComponent(
+void RegisterAdBlockDefaultComponent(
     component_updater::ComponentUpdateService* cus,
     OnComponentReadyCallback callback) {
   // In test, |cus| could be nullptr.
@@ -179,11 +178,10 @@ void RegisterAdBlockIosDefaultDatComponent(
 
   auto installer = base::MakeRefCounted<component_updater::ComponentInstaller>(
       std::make_unique<AdBlockComponentInstallerPolicy>(
-          kAdBlockIosDefaultDatComponentBase64PublicKey,
-          kAdBlockIosDefaultDatComponentId, kAdBlockIosDefaultDatComponentName,
-          callback));
+          kAdBlockDefaultComponentBase64PublicKey, kAdBlockDefaultComponentId,
+          kAdBlockDefaultComponentName, callback));
   installer->Register(
-      cus, base::BindOnce(&OnRegistered, kAdBlockIosDefaultDatComponentId));
+      cus, base::BindOnce(&OnRegistered, kAdBlockDefaultComponentId));
 }
 
 void RegisterAdBlockDefaultResourceComponent(
--- components/brave_shields/browser/ad_block_component_installer.h
+++ components/brave_shields/browser/ad_block_component_installer.h
@@ -1,7 +1,7 @@
-/* Copyright (c) 2021 The Brave Authors. All rights reserved.
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+// Copyright (c) 2023 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
 
 #ifndef BRAVE_COMPONENTS_BRAVE_SHIELDS_BROWSER_AD_BLOCK_COMPONENT_INSTALLER_H_
 #define BRAVE_COMPONENTS_BRAVE_SHIELDS_BROWSER_AD_BLOCK_COMPONENT_INSTALLER_H_
@@ -20,7 +20,7 @@ namespace brave_shields {
 using OnComponentReadyCallback =
     base::RepeatingCallback<void(const base::FilePath& install_path)>;
 
-void RegisterAdBlockIosDefaultDatComponent(
+void RegisterAdBlockDefaultComponent(
     component_updater::ComponentUpdateService* cus,
     OnComponentReadyCallback callback);
 
