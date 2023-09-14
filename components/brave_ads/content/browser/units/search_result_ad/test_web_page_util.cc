03d608f282a06b189396069e88bad6b0b841f4b3
Terry Mancey <terry.mancey@icloud.com>
2023-09-14 15:57:01 -0500

    Remove Brave Ads deprecated search result ad properties

--- components/brave_ads/content/browser/units/search_result_ad/test_web_page_util.cc
+++ components/brave_ads/content/browser/units/search_result_ad/test_web_page_util.cc
@@ -97,8 +97,6 @@ class TestWebPageEntitiesConstructor final {
     AddProperty<std::string>(&entity->properties, "data-rewards-value", "0.5");
     AddProperty<int64_t>(&entity->properties,
                          "data-conversion-observation-window-value", 1);
-    AddProperty<bool>(&entity->properties,
-                      "data-conversion-extract-external-id-value", true);
 
     // Generate values for simple string properties.
     int index = 0;
