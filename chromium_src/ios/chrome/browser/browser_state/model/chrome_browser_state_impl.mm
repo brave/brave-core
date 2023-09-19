936a4c6f1875c530e422cb36785b66d1ea780154
Claudio DeSouza <cdesouza@brave.com>
2023-09-19 15:40:43 +0300

    [ios] `browser_state` with `model` subdirectory
    This target has been split into, with the presence of a `model`
    subfolder with the contents of certain files. This change corrects the
    inclusion paths in cxx, and gn.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/cb0cb69b27b34c3ceb6350f029ff4212b0d25cfe
    
    commit cb0cb69b27b34c3ceb6350f029ff4212b0d25cfe
    Author: Aman Verma <amanvr@google.com>
    Date:   Fri Sep 15 14:55:15 2023 +0000
    
        [iOS] Move ios/c/b/browser_state to model subfolder
    
        To simplify the folder hierarchy on iOS and have a clearer layer
        separation, all the files in ios/c/b/<foo>/ will be moved to
        ios/c/b/<foo>/model/ to showcase that they are model files.
    
        Bug: 1319852, 1480281

--- chromium_src/ios/chrome/browser/browser_state/model/chrome_browser_state_impl.mm
+++ chromium_src/ios/chrome/browser/browser_state/model/chrome_browser_state_impl.mm
@@ -3,9 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "ios/chrome/browser/browser_state/chrome_browser_state_impl.h"
+#include "ios/chrome/browser/browser_state/model/chrome_browser_state_impl.h"
 
-#include "ios/chrome/browser/browser_state/off_the_record_chrome_browser_state_impl.h"
+#include "ios/chrome/browser/browser_state/model/off_the_record_chrome_browser_state_impl.h"
 #include "ios/chrome/browser/policy/browser_policy_connector_ios.h"
 #include "ios/chrome/browser/policy/browser_state_policy_connector.h"
 #include "ios/chrome/browser/policy/browser_state_policy_connector_factory.h"
@@ -22,7 +22,7 @@
 
 #define GetPolicyConnector GetPolicyConnector_ChromiumImpl
 
-#include "src/ios/chrome/browser/browser_state/chrome_browser_state_impl.mm"
+#include "src/ios/chrome/browser/browser_state/model/chrome_browser_state_impl.mm"
 
 #undef GetPolicyConnector
 #undef BuildBrowserStatePolicyConnector
