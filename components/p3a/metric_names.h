7c2527eab88cdd45042da53f9bcab095bca0f78d
Soner YUKSEL <grkmyksl@gmail.com>
2023-09-18 21:41:55 -0400

    [iOS] Adding New Keys for iOS Forward Navigation (uplift to 1.58.x) (#20189)
    Merge pull request #20134 from brave/ios/p3a/new-navigation-key

--- components/p3a/metric_names.h
+++ components/p3a/metric_names.h
@@ -197,6 +197,7 @@ constexpr inline auto kCollectedSlowHistograms =
     "Brave.Sync.EnabledTypes",
     "Brave.Sync.SyncedObjectsCount",
     "Brave.Today.UsageMonthly",
+    "Brave.Toolbar.ForwardNavigationAction",
     "Brave.Wallet.UsageMonthly"
 });
 
