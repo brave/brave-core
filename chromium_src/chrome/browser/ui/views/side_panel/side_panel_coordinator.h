c10a41d3b4752bf9b7136dbfe315e6eee68be1ac
Simon Hong <shong@brave.com>
2023-09-20 16:21:13 +0900

    Deleted chrome-browser-ui-views-side_panel-side_panel_coordinator.h.patch
    We also can re-define final keyword!

--- chromium_src/chrome/browser/ui/views/side_panel/side_panel_coordinator.h
+++ chromium_src/chrome/browser/ui/views/side_panel/side_panel_coordinator.h
@@ -6,15 +6,36 @@
 #ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_SIDE_PANEL_SIDE_PANEL_COORDINATOR_H_
 #define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_SIDE_PANEL_SIDE_PANEL_COORDINATOR_H_
 
+// Moved all header includes of upstream side_panel_coordinator.h to apply
+// final define only to side_panel_coordinator.h as the final keyword is very
+// commonly used from many places.
+#include "base/gtest_prod_util.h"
+#include "base/memory/raw_ptr.h"
+#include "base/observer_list.h"
+#include "base/scoped_multi_source_observation.h"
+#include "base/scoped_observation_traits.h"
+#include "base/time/time.h"
+#include "chrome/browser/ui/side_panel/side_panel_ui.h"
+#include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
+#include "chrome/browser/ui/views/side_panel/side_panel_entry.h"
+#include "chrome/browser/ui/views/side_panel/side_panel_registry.h"
+#include "chrome/browser/ui/views/side_panel/side_panel_registry_observer.h"
+#include "chrome/browser/ui/views/side_panel/side_panel_util.h"
+#include "chrome/browser/ui/views/side_panel/side_panel_view_state_observer.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+#include "ui/views/view_observer.h"
+
 #define CreateHeader                      \
   CreateHeader_UnUsed() {                 \
     return nullptr;                       \
   }                                       \
   friend class BraveSidePanelCoordinator; \
   virtual std::unique_ptr<views::View> CreateHeader
+#define final
 
 #include "src/chrome/browser/ui/views/side_panel/side_panel_coordinator.h"  // IWYU pragma: export
 
+#undef final
 #undef CreateHeader
 
 #endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_SIDE_PANEL_SIDE_PANEL_COORDINATOR_H_
