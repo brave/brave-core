b7b35175943c192d6a4cb2b89ea1967ce43876b1
Ralph Giles <rgiles@brave.com>
2023-09-20 08:21:08 -0700

    speedreader: Add license headers
    Document each file as MPL 2.0-licensed, per code policy.
    Addresses an `npm run presubmit` warning.

--- components/speedreader/rust/lib/src/readability/src/scorer.rs
+++ components/speedreader/rust/lib/src/readability/src/scorer.rs
@@ -1,3 +1,8 @@
+// Copyright (c) 2021 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use crate::{dom, util};
 use html5ever::tendril::StrTendril;
 use html5ever::tree_builder::TreeSink;
