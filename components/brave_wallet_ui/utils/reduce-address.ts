c30c5bb004f57da1b86e10752f0bc11642d962a9
Josh Leonard <30185185+josheleonard@users.noreply.github.com>
2023-09-21 17:48:44 -0400

    chore: fix arg type for reduce address util

--- components/brave_wallet_ui/utils/reduce-address.ts
+++ components/brave_wallet_ui/utils/reduce-address.ts
@@ -3,7 +3,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // you can obtain one at https://mozilla.org/MPL/2.0/.
 
-export const reduceAddress = (address: string, fillerChars?: string) => {
+export const reduceAddress = (address?: string, fillerChars?: string) => {
   if (!address) {
     return ''
   }
