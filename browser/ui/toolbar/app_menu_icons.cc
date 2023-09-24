c5a185afa72f10dc9a0b50e5b1bdc4f8d5c50fd3
Jay Harris <jay.harris@outlook.co.nz>
2023-09-25 11:21:52 +1300

    Add icon for new tor connection

--- browser/ui/toolbar/app_menu_icons.cc
+++ browser/ui/toolbar/app_menu_icons.cc
@@ -26,6 +26,7 @@ const std::map<int, const gfx::VectorIcon&>& GetCommandIcons() {
           {IDC_NEW_TAB, kLeoBrowserAddIcon},
           {IDC_NEW_WINDOW, kLeoWindowTabNewIcon},
           {IDC_NEW_INCOGNITO_WINDOW, kLeoWindowTabPrivateIcon},
+          {IDC_NEW_TOR_CONNECTION_FOR_SITE, kLeoWindowTabTorIcon},
           {IDC_NEW_OFFTHERECORD_WINDOW_TOR, kLeoWindowTabTorIcon},
           {IDC_SHOW_BRAVE_REWARDS, kLeoProductBatOutlineIcon},
           {IDC_SHOW_BRAVE_WALLET, kLeoProductBraveWalletIcon},
