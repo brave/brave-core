2edcebc69f91e6ffd62f4a94a1d34308659cf2fe
Jay Harris <jay.harris@outlook.co.nz>
2023-09-25 11:12:05 +1300

    Remove new badge from Performance

---
+++ chromium_src/components/feature_engagement/public/feature_constants.cc
@@ -0,0 +1,15 @@
+// Copyright (c) 2023 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "base/feature_override.h"
+
+#include "src/components/feature_engagement/public/feature_constants.cc"
+
+namespace feature_engagement {
+
+OVERRIDE_FEATURE_DEFAULT_STATES({{kIPHPerformanceNewBadgeFeature,
+                                  base::FEATURE_DISABLED_BY_DEFAULT}});
+
+}
