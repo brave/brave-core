4672085ae5d6a756d5397b9fc2e18b85c00eb848
Jacob Sikorski <jacob.sikorski@gmail.com>
2023-09-27 10:56:15 -0600

    Use the brave-core debounce service

--- ios/brave-ios/Sources/Brave/Frontend/Browser/BrowserViewController/BVC+WKNavigationDelegate.swift
+++ ios/brave-ios/Sources/Brave/Frontend/Browser/BrowserViewController/BVC+WKNavigationDelegate.swift
@@ -1669,22 +1669,9 @@ extension BrowserViewController: WKUIDelegate {
     // Then we simply get all elements up until the user allows us to redirect
     // (i.e. appropriate settings are enabled for that redirect rule)
     if Preferences.Shields.autoRedirectTrackingURLs.value,
-      let currentURL = tab.webView?.url,
-      currentURL.baseDomain != requestURL.baseDomain
-    {
-      let redirectChain = DebouncingService.shared
-        .redirectChain(for: requestURL)
-        .contiguousUntil { _, rule in
-          return rule.preferences.allSatisfy { pref in
-            switch pref {
-            case .deAmpEnabled:
-              return Preferences.Shields.autoRedirectAMPPages.value
-            }
-          }
-        }
-
-      // Once we check the redirect chain only need the last (final) url from our redirect chain
-      if let redirectURL = redirectChain.last?.url {
+       let currentURL = tab.webView?.url,
+       currentURL.baseDomain != requestURL.baseDomain {
+      if let redirectURL = DebounceServiceFactory.get(privateMode: tab.isPrivate)?.debounce(requestURL) {
         // For now we only allow the `Referer`. The browser will add other headers during navigation.
         var modifiedRequest = URLRequest(url: redirectURL)
 
