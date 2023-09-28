c763e665a2c316c78832d125a07edd4f68ccc09f
Michael Herrmann <michael@herrmann.io>
2023-09-28 16:05:57 +0200

    Revert "Reorder some code fragments"
    This reverts commit edfe91f9122a05626ec506113963fe68b3d01902.
    
    Revert "Add sanity check"
    
    This reverts commit 710323c72a83b1b205d8fcab2edd111a77847e31.
    
    Revert "Small code improvements"
    
    This reverts commit 9a58a0b0316848604d21377499aa324508b32efa.
    
    Revert "Lint"
    
    This reverts commit 7c736a200cf3d45350fb3c9e1b91da3f7032ff76.
    
    Revert "Fix formatting"
    
    This reverts commit 07e8c81755603ba105987dd1a9c649b269eb024a.
    
    Revert "Fix Release build"
    
    This reverts commit 890accc2de90cfae93eb23236649fa7894cc9b44.

--- chromium_src/chrome/install_static/install_constants.h
+++ chromium_src/chrome/install_static/install_constants.h
@@ -1,7 +1,7 @@
-/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+/* Copyright 2021 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at https://mozilla.org/MPL/2.0/. */
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 // Brand-specific types and constants for Google Chrome.
 
@@ -9,18 +9,15 @@
 #define BRAVE_CHROMIUM_SRC_CHROME_INSTALL_STATIC_INSTALL_CONSTANTS_H_
 
 #if defined(OFFICIAL_BUILD)
-// clang-format off
-// NOLINTBEGIN(sort-order)
 #include "chrome/install_static/buildflags.h"
-#include "chrome/install_static/brave_enable_google_update_integration.h"
-// NOLINTEND(sort-order)
-// clang-format on
-#endif  // defined(OFFICIAL_BUILD)
+#undef BUILDFLAG_INTERNAL_USE_GOOGLE_UPDATE_INTEGRATION
+#define BUILDFLAG_INTERNAL_USE_GOOGLE_UPDATE_INTEGRATION() (1)
+#endif
 
 #include "src/chrome/install_static/install_constants.h"  // IWYU pragma: export
 
 #if defined(OFFICIAL_BUILD)
-#include "chrome/install_static/brave_restore_google_update_integration.h"
-#endif  // defined(OFFICIAL_BUILD)
+#undef BUILDFLAG_INTERNAL_USE_GOOGLE_UPDATE_INTEGRATION
+#endif
 
 #endif  // BRAVE_CHROMIUM_SRC_CHROME_INSTALL_STATIC_INSTALL_CONSTANTS_H_
