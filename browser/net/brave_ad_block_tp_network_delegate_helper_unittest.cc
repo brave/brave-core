8162a69dd40eb4f2db6f35faa7cd2d06204e44c3
Mikhail Atuchin <matuchin@brave.com>
2023-10-07 22:37:49 +0200

    Fix presubmit issues

--- browser/net/brave_ad_block_tp_network_delegate_helper_unittest.cc
+++ browser/net/brave_ad_block_tp_network_delegate_helper_unittest.cc
@@ -1,7 +1,7 @@
 /* Copyright (c) 2019 The Brave Software Team. Distributed under the MPL2
  * license. This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "brave/browser/net/brave_ad_block_tp_network_delegate_helper.h"
 
