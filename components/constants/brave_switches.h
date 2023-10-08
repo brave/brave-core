56d758ba953d88bf2dc88c31268d1264be938c15
Anton Bershanskiy <45960703+bershanskiy@users.noreply.github.com>
2023-10-08 16:56:13 +0300

    Cleanup unused flags

--- components/constants/brave_switches.h
+++ components/constants/brave_switches.h
@@ -24,10 +24,6 @@ inline constexpr char kDarkMode[] = "dark-mode";
 // in debug mode with auto-reloading.
 inline constexpr char kDisableBraveExtension[] = "disable-brave-extension";
 
-// Allows disabling the Brave Rewards extension.
-inline constexpr char kDisableBraveRewardsExtension[] =
-    "disable-brave-rewards-extension";
-
 // This switch disables update module(Sparkle).
 inline constexpr char kDisableBraveUpdate[] = "disable-brave-update";
 
