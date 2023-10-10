26070196ba1c17e3858ad5f2ca2c282f7fc34e65
Claudio DeSouza <cdesouza@brave.com>
2023-10-10 11:09:02 +0100

    `kOmniboxMostVisitedTilesOnSrp` feature flag deleted
    This flag was being disabled, but the functinality is now completely
    removed upstream.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/99c9119fce7942e50ba23629c135904181eba39e
    
    commit 99c9119fce7942e50ba23629c135904181eba39e
    Author: Tomasz Wiszkowski <ender@google.com>
    Date:   Fri Oct 6 23:15:41 2023 +0000
    
        Clean up Most Visited Tiles on Search Results Page
    
        Feature is evaluated and is not launching on any platforms.
    
        Change-Id: I7f467230af565ebb1ebb48e1ef657de71dad3f4f
        Fixed: 1490101

--- app/brave_main_delegate_browsertest.cc
+++ app/brave_main_delegate_browsertest.cc
@@ -275,7 +275,6 @@ IN_PROC_BROWSER_TEST_F(BraveMainDelegateBrowserTest, DisabledFeatures) {
     &omnibox::kDocumentProviderNoSyncRequirement,
     &omnibox::kExpandedStateHeight,
     &omnibox::kExpandedStateShape,
-    &omnibox::kOmniboxMostVisitedTilesOnSrp,
     &omnibox::kOmniboxSteadyStateHeight,
     &omnibox::kRichAutocompletion,
     &optimization_guide::features::kOptimizationGuideFetchingForSRP,
