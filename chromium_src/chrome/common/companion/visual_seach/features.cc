e3211bc8dfe600e3e861d704f5d7edd28f8c35ef
Emerick Rogul <erogul@brave.com>
2023-10-10 18:38:39 -0400

    Use new directory for visual_search companion
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/1f6592bfba42222917781c2017281a17cfcff544
    
    commit 1f6592bfba42222917781c2017281a17cfcff544
    Author: Pierre St Juste <pstjuste@google.com>
    Date:   Thu Oct 5 00:35:21 2023 +0000
    
        [M118] Enabling page classification from renderer agent.
    
        This is a reland of commit 05d5244b10f1b20826b6dd9b9a8880f4784a8e61
    
        Original change's description:
        > Enabling page classification from renderer agent.
        >
        > This CL allows the VisualSearchClassifierAgent to request the model from the VisualSearchSuggestionsService. Currently, the model is >
        >
        > Bug: b/290953353
        > Change-Id: If3b78575e00e4cd8fc53b1b941b9903e95e4f71b
        > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4808509
        > Commit-Queue: Pierre St Juste <pstjuste@google.com>
        > Reviewed-by: Alex Gough <ajgo@chromium.org>
        > Reviewed-by: Michael Crouse <mcrouse@chromium.org>
        > Cr-Commit-Position: refs/heads/main@{#1188748}
    
        (cherry picked from commit 472355cd0c2616806d5292d715cb97bebee31c6c)
    
        Bug: 1449021, b/290953353

--- chromium_src/chrome/common/companion/visual_seach/features.cc
+++ chromium_src/chrome/common/companion/visual_seach/features.cc
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "src/chrome/browser/companion/visual_search/features.cc"
+#include "src/chrome/common/companion/visual_search/features.cc"
 
 #include "base/feature_override.h"
 
