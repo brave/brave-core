13c8b0d62e245da505d67f41727166ee09b8831c
Szilard Szaloki <szilardszaloki@gmail.com>
2023-10-10 15:08:25 -0500

    chromium_src 4

--- chromium_src/components/autofill/core/browser/single_field_form_fill_router.h
+++ chromium_src/components/autofill/core/browser/single_field_form_fill_router.h
@@ -6,6 +6,8 @@
 #ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_AUTOFILL_CORE_BROWSER_SINGLE_FIELD_FORM_FILL_ROUTER_H_
 #define BRAVE_CHROMIUM_SRC_COMPONENTS_AUTOFILL_CORE_BROWSER_SINGLE_FIELD_FORM_FILL_ROUTER_H_
 
+#include "components/autofill/core/browser/iban_manager.h"
+#include "components/autofill/core/browser/merchant_promo_code_manager.h"
 #include "components/autofill/core/browser/single_field_form_filler.h"
 
 #define OnWillSubmitForm(form, form_structure, is_autocomplete_enabled) \
