e283dd049420573bc4f38a7516c98cfc0730ec90
AlexeyBarabash <alexey@brave.com>
2023-10-11 01:31:27 +0300

    Added unit test for LowPriorityUserTypes

--- chromium_src/components/sync/base/model_type_unittest.cc
+++ chromium_src/components/sync/base/model_type_unittest.cc
@@ -12,6 +12,12 @@ TEST_F(ModelTypeTest, EncryptableUserTypes) {
   EXPECT_TRUE(EncryptableUserTypes().Has(DEVICE_INFO));
 }
 
+TEST_F(ModelTypeTest, LowPriorityUserTypes) {
+  EXPECT_TRUE(LowPriorityUserTypes().Has(HISTORY_DELETE_DIRECTIVES));
+  EXPECT_FALSE(LowPriorityUserTypes().Has(HISTORY));
+  EXPECT_TRUE(LowPriorityUserTypes().Has(USER_EVENTS));
+}
+
 // This test is supposed to fail when sync types are increased/decreased
 TEST_F(ModelTypeTest, ModelTypeCounts) {
   EXPECT_EQ(static_cast<int>(ModelTypeForHistograms::kMaxValue), 61);
