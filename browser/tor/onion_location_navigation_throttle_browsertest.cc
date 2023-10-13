5ada6448176f56b18238347e04ece1a87796385d
Claudio DeSouza <cdesouza@brave.com>
2023-10-13 14:58:20 +0100

    `chrome_notification_types.h` header renamed
    This is merely a change of inclusion name for the file.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/b8ac52738cbd3a5a44967e57ffee393b61d863ef
    
    commit b8ac52738cbd3a5a44967e57ffee393b61d863ef
    Author: Lei Zhang <thestig@chromium.org>
    Date:   Thu Oct 12 21:33:34 2023 +0000
    
        Rename chrome_notification_types.h to auth_notification_types.h
    
        Since the only notification types left in chrome_notification_types.h
        are for authentication purposes, rename the file to reflect this.
    
        Bug: 1174785

--- browser/tor/onion_location_navigation_throttle_browsertest.cc
+++ browser/tor/onion_location_navigation_throttle_browsertest.cc
@@ -18,7 +18,7 @@
 #include "brave/components/tor/tor_navigation_throttle.h"
 #include "brave/grit/brave_generated_resources.h"
 #include "brave/net/proxy_resolution/proxy_config_service_tor.h"
-#include "chrome/browser/chrome_notification_types.h"
+#include "chrome/browser/auth_notification_types.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_list_observer.h"
