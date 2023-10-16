cb3dcb014a53375f845e50e7f1fc46617deb5679
Jay Harris <jay.harris@outlook.co.nz>
2023-10-17 09:41:45 +1300

    Add brave://news-internals to about page

--- chromium_src/chrome/common/webui_url_constants.cc
+++ chromium_src/chrome/common/webui_url_constants.cc
@@ -10,7 +10,7 @@
 #define kChromeUIAttributionInternalsHost                                     \
   kChromeUIAttributionInternalsHost, kAdblockHost, kAdblockInternalsHost,     \
       kRewardsPageHost, kRewardsInternalsHost, kWelcomeHost, kWalletPageHost, \
-      kTorInternalsHost, kSkusInternalsHost
+      kTorInternalsHost, kSkusInternalsHost, kBraveNewsInternalsHost
 #define kChromeUIPerformanceSettingsURL kChromeUIPerformanceSettingsURL_UnUsed
 #define kPerformanceSubPage kPerformanceSubPage_UnUsed
 
