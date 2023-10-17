6b6c60408121bb0e1b08f964bfb22622174f408c
brave-builds <brave-builds+devops@brave.com>
2023-10-17 14:01:32 +0000

    Uplift of #20556 (squashed) to beta

--- components/brave_wallet_ui/panel/container.tsx
+++ components/brave_wallet_ui/panel/container.tsx
@@ -383,16 +383,18 @@ function Container () {
   if (selectedAccount && (selectedPendingTransaction || signMessageData.length) &&
     selectedPanel === 'connectHardwareWallet') {
     return (
-      <PanelWrapper isLonger={false}>
-        <StyledExtensionWrapper>
-          <ConnectHardwareWalletPanel
-            onCancel={onCancelConnectHardwareWallet}
-            account={selectedAccount}
-            hardwareWalletCode={hardwareWalletCode}
-            onClickInstructions={onClickInstructions}
-          />
-        </StyledExtensionWrapper>
-      </PanelWrapper>
+      <BrowserRouter>
+        <PanelWrapper isLonger={false}>
+          <StyledExtensionWrapper>
+            <ConnectHardwareWalletPanel
+              onCancel={onCancelConnectHardwareWallet}
+              account={selectedAccount}
+              hardwareWalletCode={hardwareWalletCode}
+              onClickInstructions={onClickInstructions}
+            />
+          </StyledExtensionWrapper>
+        </PanelWrapper>
+      </BrowserRouter>
     )
   }
 
