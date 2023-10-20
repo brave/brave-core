a4d44284fc19bb8e1c6c4f860786d2b105f1e0ff
Brian R. Bondy <netzen@gmail.com>
2023-10-20 12:26:32 -0400

    Rebrand from Brave Leo to Leo (#20601)

--- components/ai_chat/resources/page/components/main/index.tsx
+++ components/ai_chat/resources/page/components/main/index.tsx
@@ -85,7 +85,7 @@ function Main() {
         <div className={styles.logo}>
           <Icon name='product-brave-leo' />
           <div className={styles.logoTitle}>
-            Brave <span>Leo</span>
+            <span>Leo</span>
           </div>
           {context.isPremiumUser && <div className={styles.badgePremium}>PREMIUM</div>}
         </div>
