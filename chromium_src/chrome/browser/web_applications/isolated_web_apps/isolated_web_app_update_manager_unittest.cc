69c6b67c92f4f95b09890468ee4b15582fb0ebd1
Claudio DeSouza <cdesouza@brave.com>
2023-10-23 18:30:34 +0100

    Fixing `ChromeBrowsingDataRemoverDelegate` new occurrence
    Due to the way how substitutions for this class is taking place,
    upstream uses of certain functions of this class will result in linking
    errors that require shadowing of the files using it, replicating the
    substitution again.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/e9dc9a59bc3f341099d5ae83686c384bc62f73bd
    
    commit e9dc9a59bc3f341099d5ae83686c384bc62f73bd
    Author: Christian Flach <cmfcmf@chromium.org>
    Date:   Thu Oct 19 11:22:14 2023 +0000
    
        iwa: Add buttons to chrome://web-app-internals to update dev proxy IWAs
    
        This CL adds a list of all installed dev mode proxy IWAs to
        chrome://web-app-internals. Each list item has a button to trigger an
        update, which uses the same update system used for normal IWA updates,
        except for the update discovery part.
        I will look into adding similar functionality for dev-mode bundle apps
        later.
    
        Screenshot: https://imgur.com/a/5E3lZUt
    
        Low-Coverage-Reason: OTHER Debug-only code with no existing coverage.
        Bug: 1444407

---
+++ chromium_src/chrome/browser/web_applications/isolated_web_apps/isolated_web_app_update_manager_unittest.cc
@@ -0,0 +1,8 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#define ChromeBrowsingDataRemoverDelegate BraveBrowsingDataRemoverDelegate
+#include "src/chrome/browser/web_applications/isolated_web_apps/isolated_web_app_update_manager_unittest.cc"
+#undef ChromeBrowsingDataRemoverDelegate
