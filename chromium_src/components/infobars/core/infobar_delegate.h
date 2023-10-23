80d4215fa0ee06771fb32bf9baf3d2b4d272de28
Simon Hong <shong@brave.com>
2023-10-23 16:52:38 +0900

    Launch dev channel deprecation infobar at startup
    fix https://github.com/brave/brave-browser/issues/33822

--- chromium_src/components/infobars/core/infobar_delegate.h
+++ chromium_src/components/infobars/core/infobar_delegate.h
@@ -12,14 +12,15 @@
 // When there will be too many items, redo java_cpp_enum.py to generate it
 // automatically
 
-#define BRAVE_INFOBAR_DELEGATE_IDENTIFIERS                                   \
-  BRAVE_CONFIRM_P3A_INFOBAR_DELEGATE = 500,                                  \
-  WAYBACK_MACHINE_INFOBAR_DELEGATE = 502,                                    \
-  SYNC_V2_MIGRATE_INFOBAR_DELEGATE = 503,                                    \
-  ANDROID_SYSTEM_SYNC_DISABLED_INFOBAR = 504, SYNC_CANNOT_RUN_INFOBAR = 505, \
-  WEB_DISCOVERY_INFOBAR_DELEGATE = 506,                                      \
-  BRAVE_SYNC_ACCOUNT_DELETED_INFOBAR = 507,                                  \
-  BRAVE_REQUEST_OTR_INFOBAR_DELEGATE = 508, BRAVE_IPFS_INFOBAR_DELEGATE = 509,
+#define BRAVE_INFOBAR_DELEGATE_IDENTIFIERS                                     \
+  BRAVE_CONFIRM_P3A_INFOBAR_DELEGATE = 500,                                    \
+  WAYBACK_MACHINE_INFOBAR_DELEGATE = 502,                                      \
+  SYNC_V2_MIGRATE_INFOBAR_DELEGATE = 503,                                      \
+  ANDROID_SYSTEM_SYNC_DISABLED_INFOBAR = 504, SYNC_CANNOT_RUN_INFOBAR = 505,   \
+  WEB_DISCOVERY_INFOBAR_DELEGATE = 506,                                        \
+  BRAVE_SYNC_ACCOUNT_DELETED_INFOBAR = 507,                                    \
+  BRAVE_REQUEST_OTR_INFOBAR_DELEGATE = 508, BRAVE_IPFS_INFOBAR_DELEGATE = 509, \
+  DEV_CHANNEL_DEPRECATION_INFOBAR_DELEGATE = 510,
 
 #include "src/components/infobars/core/infobar_delegate.h"  // IWYU pragma: export
 
