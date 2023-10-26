3bd6d2477993596db5d0f287defa182b1c6650f5
Anirudha Bose <anirudha.bose@alumni.cern>
2023-10-26 14:31:57 +0300

    fix storybook error

--- components/brave_wallet_ui/stories/mock-data/mock-wallet-state.ts
+++ components/brave_wallet_ui/stories/mock-data/mock-wallet-state.ts
@@ -91,7 +91,7 @@ export const mockWalletState: WalletState = {
   isSolanaEnabled: false,
   isBitcoinEnabled: false,
   isZCashEnabled: false,
-  isAnkrBalancesEnabled: false,
+  isAnkrBalancesFeatureEnabled: false,
   solFeeEstimates: {
     fee: 0.000005 * LAMPORTS_PER_SOL as unknown as bigint
   },
