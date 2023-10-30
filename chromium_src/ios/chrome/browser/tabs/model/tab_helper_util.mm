4630bd0cbff5962aab023665160484759d94bd33
Claudio DeSouza <cdesouza@brave.com>
2023-10-30 16:36:48 +0000

    Browser `tabs` moved into `model/` subfolder
    This affects the inclusion paths for the files moved, and one of the
    file shadowings we had for this component, which has been corrected with
    this change.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/596fe298e01648f418c87ce26b32eebb1249295f
    
    commit 596fe298e01648f418c87ce26b32eebb1249295f
    Author: Robbie Gibson <rkgibson@google.com>
    Date:   Fri Oct 27 01:00:36 2023 +0000
    
        [Code Health][iOS] Move ios/c/b/tabs to model subfolder
    
        To simplify the folder hierarchy on iOS and have a clearer layer
        separation, all the files in ios/c/b/<foo>/ will be moved to
        ios/c/b/<foo>/model/ to showcase that they are model files.
    
        This is part of a multi-step CL:
        1. crrev.com/c/4956595
        2. crrev.com/i/6582794
        3. This CL
    
        Bug: 1480363

--- chromium_src/ios/chrome/browser/tabs/model/tab_helper_util.mm
+++ chromium_src/ios/chrome/browser/tabs/model/tab_helper_util.mm
@@ -3,12 +3,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#import "ios/chrome/browser/tabs/tab_helper_util.h"
+#import "ios/chrome/browser/tabs/model/tab_helper_util.h"
 
 #include "ios/chrome/browser/complex_tasks/model/ios_task_tab_helper.h"
 #include "ios/chrome/browser/sessions/ios_chrome_session_tab_helper.h"
 #include "ios/chrome/browser/sync/model/ios_chrome_synced_tab_delegate.h"
-#include "ios/chrome/browser/tabs/tab_helper_util.h"
 #include "ios/chrome/browser/web/session_state/web_session_state_tab_helper.h"
 
 void AttachTabHelpers(web::WebState* web_state, bool for_prerender) {
