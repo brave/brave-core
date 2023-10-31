71a0824b89f4c5cffd8a4424fb61eb9280311603
Pete Miller <miller.pete@gmail.com>
2023-10-30 21:00:42 -0700

    Merge pull request #20746 from brave/ai-chat-mini-2
    AI Chat mini fixes

--- components/ai_chat/resources/page/components/feature_button_menu/index.tsx
+++ components/ai_chat/resources/page/components/feature_button_menu/index.tsx
@@ -95,12 +95,23 @@ export default function FeatureMenu() {
         </div>
       </leo-menu-item>
 
+      {!context.isPremiumUser &&
       <leo-menu-item onClick={context.goPremium}>
         <div className={classnames(styles.menuItemWithIcon, styles.menuItemMainItem)}>
           <Icon name='lock-open' />
           <span className={styles.menuText}>{getLocale('menuGoPremium')}</span>
         </div>
       </leo-menu-item>
+      }
+
+      {context.isPremiumUser &&
+      <leo-menu-item onClick={context.managePremium}>
+        <div className={classnames(styles.menuItemWithIcon, styles.menuItemMainItem)}>
+          <Icon name='lock-open' />
+          <span className={styles.menuText}>{getLocale('menuManageSubscription')}</span>
+        </div>
+      </leo-menu-item>
+      }
 
       <leo-menu-item onClick={handleSettingsClick}>
         <div className={classnames(styles.menuItemWithIcon, styles.menuItemMainItem)}>
