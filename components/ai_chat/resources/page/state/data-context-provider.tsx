6784ecf8cecbe863038df25dcf142185f7c9d8ee
brave-builds <brave-builds+devops@brave.com>
2023-11-01 17:24:09 +0000

    Uplift of #20780 (squashed) to release

--- components/ai_chat/resources/page/state/data-context-provider.tsx
+++ components/ai_chat/resources/page/state/data-context-provider.tsx
@@ -292,7 +292,7 @@ function DataContextProvider (props: DataContextProviderProps) {
     suggestedQuestions,
     canGenerateQuestions,
     userAutoGeneratePref,
-    siteInfo: siteInfo || null,
+    siteInfo: siteInfo,
     favIconUrl,
     currentError,
     hasAcceptedAgreement,
