ec596b8e15f0ae26c1d07261bbbcd7b96bf0b136
Claudio DeSouza <cdesouza@brave.com>
2023-11-03 17:32:20 +0000

    `HttpsUpgradesNavigationThrottle` site engagement check fixes
    This change removes the use of `MaybeEnableHttpsFirstModeForUrl` from
    this throttle, mirroring the behaviour from upstream, as this particular
    function is now private, to avoid modifying HFM during navigation.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/b6d546c6fd282db44dae9e0a474d9755fb1172e9
    
    commit b6d546c6fd282db44dae9e0a474d9755fb1172e9
    Author: Mustafa Emre Acer <meacer@chromium.org>
    Date:   Fri Nov 3 00:37:13 2023 +0000
    
        HTTPS-First Mode: Move Site Engagement checks to startup
    
        crrev.com/c/4986308 moved the checks for the Typically Secure User
        heuristic to startup. This CL does the same for the Site Engagement
        heuristic. With this change, we'll no longer modify the HFM pref in the
        middle of a navigation.
    
        This CL does not properly handle sites falling off of the Site
        Engagement list. There's a TODO to fix this in a follow up CL.
    
        Bug: 1442679

--- chromium_src/chrome/browser/ssl/https_upgrades_navigation_throttle.cc
+++ chromium_src/chrome/browser/ssl/https_upgrades_navigation_throttle.cc
@@ -80,7 +80,8 @@ HttpsUpgradesNavigationThrottle::MaybeCreateThrottleFor(
       HttpsFirstModeServiceFactory::GetForProfile(profile);
   if (hfm_service) {
     // Can be null in some cases, e.g. when using Ash sign-in profile.
-    hfm_service->MaybeEnableHttpsFirstModeForUrl(handle->GetURL());
+    interstitial_state.enabled_by_typically_secure_browsing =
+        hfm_service->IsInterstitialEnabledByTypicallySecureUserHeuristic();
   }
   // StatefulSSLHostStateDelegate can be null during tests.
   if (state && state->IsHttpsEnforcedForHost(handle->GetURL().host(),
