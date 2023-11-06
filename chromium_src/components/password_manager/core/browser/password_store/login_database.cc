0b7c5cf6881d681fb7a528a2be341e081999190d
Claudio DeSouza <cdesouza@brave.com>
2023-11-06 17:34:24 +0000

    `password_manager` core files moved to `password_store` subfolder
    This is merely a file path restructuring that doesn't affect brave's
    codebase beyond fixing patches, inclusion paths, and shadowing files.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/fa7338ad8aa34f8c7ff1d143725417604ff7eeb2
    
    commit fa7338ad8aa34f8c7ff1d143725417604ff7eeb2
    Author: Viktor Semeniuk <vsemeniuk@google.com>
    Date:   Fri Nov 3 14:55:46 2023 +0000
    
        Adding password_store subfolder to c/password_manager/core/browser/
    
        This CL moves some of the files from core/browser/ to
        core/browser/password_store. More files will be moved in a follow up.
    
        Bug: 1479425

--- chromium_src/components/password_manager/core/browser/password_store/login_database.cc
+++ chromium_src/components/password_manager/core/browser/password_store/login_database.cc
@@ -3,10 +3,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#define BRAVE_STATEMENT_TO_FORMS \
-    if (result == ENCRYPTION_RESULT_SERVICE_FAILURE) \
-      result = ENCRYPTION_RESULT_ITEM_FAILURE;
+#define BRAVE_STATEMENT_TO_FORMS                   \
+  if (result == ENCRYPTION_RESULT_SERVICE_FAILURE) \
+    result = ENCRYPTION_RESULT_ITEM_FAILURE;
 
-#include "src/components/password_manager/core/browser/login_database.cc"
+#include "src/components/password_manager/core/browser/password_store/login_database.cc"
 
 #undef BRAVE_STATEMENT_TO_FORMS
