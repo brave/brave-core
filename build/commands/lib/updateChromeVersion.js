b7bd9199bc4a011a73469bff181146f1bb538af9
Aleksey Khoroshilov <akhoroshilov@brave.com>
2023-11-13 14:28:36 +0700

    Force PATCH=0 in non-release builds.

--- build/commands/lib/updateChromeVersion.js
+++ build/commands/lib/updateChromeVersion.js
@@ -45,11 +45,14 @@ function updateChromeVersion() {
     .join('.')
     .replace(/[A-Z=]/g, '')
   const newVersionFileContent = versionLines.join('\n')
+  const useFullVersionInfo = config.useFullVersion
+    ? ''
+    : ' (PATCH=0 in dev builds)'
   if (newVersionFileContent !== versionFileContent) {
     fs.writeFileSync(versionFilePath, newVersionFileContent)
-    Log.status(`Set chrome/VERSION: ${versionToLog}`)
+    Log.status(`Set chrome/VERSION: ${versionToLog}${useFullVersionInfo}`)
   } else {
-    console.log(`chrome/VERSION: ${versionToLog}`)
+    console.log(`chrome/VERSION: ${versionToLog}${useFullVersionInfo}`)
   }
 }
 
