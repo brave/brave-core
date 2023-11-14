15ca4aababf5446cf23f7cdfa80495238c894129
Aleksey Khoroshilov <akhoroshilov@brave.com>
2023-11-14 13:10:11 +0700

    Enable ThirdPartyStoragePartitioning feature.

--- app/brave_main_delegate_browsertest.cc
+++ app/brave_main_delegate_browsertest.cc
@@ -262,8 +262,6 @@ IN_PROC_BROWSER_TEST_F(BraveMainDelegateBrowserTest, DisabledFeatures) {
       &metrics::structured::kPhoneHubStructuredMetrics,
       &net::features::kEnableWebTransportDraft07,
       &net::features::kPartitionedCookies,
-      &net::features::kThirdPartyPartitionedStorageAllowedByDefault,
-      &net::features::kThirdPartyStoragePartitioning,
       &net::features::kTpcdMetadataGrants,
       &net::features::kWaitForFirstPartySetsInit,
       &network::features::kFledgePst,
