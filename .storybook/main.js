61521c5e741236c82620ac6f1159a48103eccf23
Josh Leonard <30185185+josheleonard@users.noreply.github.com>
2023-11-17 03:49:46 +0000

    chore: force type checks before building storybook (#21016)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,20 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../components/**/stories/*.tsx', '../components/**/*.stories.tsx'],
+  typescript: {
+    check: true,
+    checkOptions: {
+      tsconfig: path.resolve(__dirname, '..', 'tsconfig-storybook.json'),
+      compilerOptions: {
+        allowJs: true,
+        skipLibCheck: true,
+        noImplicitAny: true,
+        outDir: './storybook_dist'
+      },
+      async: false
+    }
+  },
   addons: ['@storybook/addon-knobs', '@storybook/addon-essentials'],
   staticDirs: [
     { from: '../node_modules/@brave/leo/icons', to: 'icons/' },
