a118755e40d5abb68897ef4535f9df1bc5e08ff6
mkarolin <max@brave.com>
2023-11-20 14:23:53 -0500

    Fixes upstream shared storage browser tests.

---
+++ chromium_src/chrome/browser/storage/shared_storage_browsertest.cc
@@ -0,0 +1,10 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "brave/browser/brave_content_browser_client.h"
+
+#define ChromeContentBrowserClient BraveContentBrowserClient
+#include "src/chrome/browser/storage/shared_storage_browsertest.cc"
+#undef ChromeContentBrowserClient
