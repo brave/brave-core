4e30dd91fb965d9482e5d37e75a333dd62291be7
Kyle Hickinson <kylehickinson@users.noreply.github.com>
2023-11-22 22:37:46 -0500

    [iOS] Use correct pref service for SkusService (uplift to 1.61.x) (#21089)
    Uplift of #21071 (squashed) to beta
    
    Co-authored-by: Michał Buczek <michal.buczek@protonmail.com>

--- ios/browser/skus/skus_service_factory.mm
+++ ios/browser/skus/skus_service_factory.mm
@@ -1,7 +1,7 @@
-/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "brave/ios/browser/skus/skus_service_factory.h"
 
@@ -13,6 +13,7 @@
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/keyed_service/ios/browser_state_dependency_manager.h"
 #include "components/pref_registry/pref_registry_syncable.h"
+#include "ios/chrome/browser/shared/model/application_context/application_context.h"
 #include "ios/chrome/browser/shared/model/browser_state/browser_state_otr_helper.h"
 #include "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
 #include "ios/web/public/browser_state.h"
@@ -54,8 +55,11 @@ std::unique_ptr<KeyedService> SkusServiceFactory::BuildServiceInstanceFor(
   if (browser_state->IsOffTheRecord()) {
     return nullptr;
   }
-  std::unique_ptr<skus::SkusServiceImpl> sku_service(new skus::SkusServiceImpl(
-      browser_state->GetPrefs(), browser_state->GetSharedURLLoaderFactory()));
+  skus::MigrateSkusSettings(browser_state->GetPrefs(),
+                            GetApplicationContext()->GetLocalState());
+  std::unique_ptr<skus::SkusServiceImpl> sku_service(
+      new skus::SkusServiceImpl(GetApplicationContext()->GetLocalState(),
+                                browser_state->GetSharedURLLoaderFactory()));
   return sku_service;
 }
 
