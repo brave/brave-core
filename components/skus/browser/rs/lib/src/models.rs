brave/brave-core

commit 3c4d4d698d16291a273fbb0724638d58d17db4fa

SKUs: fixes to refresh_order_credentials (#20667)
fixes to refresh_order_credentials

fixes for cherry-pick to master

components/skus/browser/rs/lib/src/models.rs
@@ -127,7 +127,7 @@ pub struct UnredeemedToken {
     pub promotion_id: String,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 #[serde(rename_all = "kebab-case")]
 pub enum CredentialType {
     SingleUse,
@@ -205,7 +205,14 @@ impl Order {
     }
 
     pub fn is_paid(&self) -> bool {
-        self.status == "paid" || (self.status == "canceled" && self.expires_at.is_some())
+        self.status == "paid"
+    }
+
+    pub fn has_expired(&self, now: NaiveDateTime) -> bool {
+        if let Some(expires_at) = self.expires_at {
+            return expires_at <= now;
+        }
+        false
     }
 }
 

components/skus/browser/rs/lib/src/sdk/credentials/fetch.rs
@@ -77,12 +77,15 @@ where
     ) -> Result<Vec<BlindedToken>, SkusError> {
         let mut csprng = OsRng;
 
-        let creds: Vec<Token> =
-            iter::repeat_with(|| Token::random::<Sha512, _>(&mut csprng)).take(num_creds).collect();
+        let creds: Vec<Token> = iter::repeat_with(|| Token::random::<Sha512, _>(&mut csprng))
+            .take(num_creds)
+            .collect();
 
         let blinded_creds: Vec<BlindedToken> = creds.iter().map(|t| t.blind()).collect();
 
-        self.client.upsert_time_limited_v2_item_creds(item_id, creds).await?;
+        self.client
+            .upsert_time_limited_v2_item_creds(item_id, creds)
+            .await?;
 
         Ok(blinded_creds)
     }
@@ -95,7 +98,7 @@ where
             _ => self.refresh_order(order_id).await?,
         };
 
-        if !order.is_paid() {
+        if !order.is_paid() || order.has_expired(Utc::now().naive_utc()) {
             return Err(InternalError::OrderUnpaid.into());
         }
 
@@ -110,8 +113,9 @@ where
 
                     let mut num_creds: usize = 0;
                     if let Some(ref metadata) = order.metadata {
-                        let num_intervals =
-                            metadata.num_intervals.ok_or(InternalError::OrderMisconfiguration)?;
+                        let num_intervals = metadata
+                            .num_intervals
+                            .ok_or(InternalError::OrderMisconfiguration)?;
                         let num_per_interval = metadata
                             .num_per_interval
                             .ok_or(InternalError::OrderMisconfiguration)?;
@@ -168,7 +172,10 @@ where
                     // request.  this happens if we currently are full with credentials
 
                     if !blinded_creds.is_empty() {
-                        let claim_req = ItemCredentialsRequest { item_id: item.id, blinded_creds };
+                        let claim_req = ItemCredentialsRequest {
+                            item_id: item.id,
+                            blinded_creds,
+                        };
 
                         let request_with_retries = FutureRetry::new(
                             || async {
@@ -218,12 +225,17 @@ where
                                 let blinded_creds: Vec<BlindedToken> =
                                     creds.iter().map(|t| t.blind()).collect();
 
-                                self.client.init_single_use_item_creds(&item.id, creds).await?;
+                                self.client
+                                    .init_single_use_item_creds(&item.id, creds)
+                                    .await?;
                                 blinded_creds
                             }
                         };
 
-                    let claim_req = ItemCredentialsRequest { item_id: item.id, blinded_creds };
+                    let claim_req = ItemCredentialsRequest {
+                        item_id: item.id,
+                        blinded_creds,
+                    };
 
                     let request_with_retries = FutureRetry::new(
                         || async {
@@ -270,17 +282,28 @@ where
 
     #[instrument]
     pub async fn refresh_order_credentials(&self, order_id: &str) -> Result<(), SkusError> {
-        let order = self.fetch_order(order_id).await?;
-
-        if let Some(local_order) = self.client.get_order(order_id).await? {
-            // if we have no credentials at all for the order (prior to generated state)
-            // or the last_paid_at is different from the fetched order (resubscribe)
-            if !self.client.has_credentials(order_id).await?
-                || order.last_paid_at != local_order.last_paid_at
-            {
-                self.fetch_order_credentials(order_id).await?;
-                // store the latest retrieved order information after we've successfully fetched
-                self.client.upsert_order(&order).await?;
+        if let Some(mut order) = self.client.get_order(order_id).await? {
+            if order.is_paid() && order.has_expired(Utc::now().naive_utc()) {
+                // if our order has expired, one of two things has happened:
+                //   1. our subscription was renewed, resulting in a future expiry and paid status
+                //   2. our subscription was cancelled, resulting in a past expiry and cancelled status
+                // therefore we can be reasonably sure this refresh will only happen once
+                order = self.refresh_order(order_id).await?
+            }
+            if !order.has_expired(Utc::now().naive_utc()) {
+                for item in &order.items {
+                    if item.credential_type == CredentialType::TimeLimitedV2 {
+                        if let Some(credential_expires_at) = self
+                            .last_matching_time_limited_v2_credential(&item.id)
+                            .await?
+                            .map(|cred| cred.valid_to)
+                        {
+                            if Utc::now().naive_utc() > credential_expires_at {
+                                return self.fetch_order_credentials(order_id).await;
+                            }
+                        }
+                    }
+                }
             }
             Ok(())
         } else {
@@ -296,7 +319,10 @@ where
             || async move {
                 let mut builder = http::Request::builder();
                 builder.method("GET");
-                builder.uri(format!("{}/v1/orders/{}/credentials", self.base_url, order_id));
+                builder.uri(format!(
+                    "{}/v1/orders/{}/credentials",
+                    self.base_url, order_id
+                ));
 
                 let req = builder.body(vec![]).unwrap();
 
@@ -472,7 +498,9 @@ where
         }
 
         for (item_id, item_creds) in time_limited_creds.into_iter() {
-            self.client.store_time_limited_creds(&item_id, item_creds).await?;
+            self.client
+                .store_time_limited_creds(&item_id, item_creds)
+                .await?;
         }
 
         Ok(())

components/skus/browser/rs/lib/src/sdk/credentials/mod.rs
@@ -31,6 +31,9 @@ where
         order_id: &str,
         domain: &str,
     ) -> Result<Option<CredentialSummary>, SkusError> {
+        // refresh order credentials if necessary
+        self.refresh_order_credentials(order_id).await?;
+
         let wrapped_order = self.client.get_order(order_id).await?;
         let order = wrapped_order.ok_or(InternalError::NotFound)?;
         if !order.location_matches(&self.environment, domain) {
@@ -46,17 +49,6 @@ where
                         .await?
                         .map(|cred| cred.valid_to)
                         .or(expires_at);
-                    if let Some(expires_at) = expires_at {
-                        // attempt to refresh credentials if we're within 5 days of expiry
-                        if Utc::now().naive_utc() > (expires_at - chrono::Duration::days(5)) {
-                            error!("Within 5 days of expiry; refreshing order credentials.");
-                            let refreshed = self.refresh_order_credentials(order_id).await;
-                            if refreshed.is_err() {
-                                error!("Error refreshing order credentials.");
-                            }
-                        }
-                    }
-
                     if let Some(creds) = self.matching_time_limited_v2_credential(&item.id).await? {
                         let unblinded_creds =
                             creds.unblinded_creds.ok_or(InternalError::NotFound)?;
@@ -101,16 +93,6 @@ where
                         .await?
                         .map(|cred| cred.expires_at)
                         .or(expires_at);
-                    if let Some(expires_at) = expires_at {
-                        // attempt to refresh credentials if we're within 5 days of expiry
-                        if Utc::now().naive_utc() > (expires_at - chrono::Duration::days(5)) {
-                            error!("Within 5 days of expiry; refreshing order credentials.");
-                            let refreshed = self.refresh_order_credentials(order_id).await;
-                            if refreshed.is_err() {
-                                error!("Error refreshing order credentials.");
-                            }
-                        }
-                    }
 
                     if let Ok(Some(_)) = self.matching_time_limited_credential(&item.id).await {
                         return Ok(Some(CredentialSummary {

commit bef83f165d63f80c95b586160c0fb47566a154c5

fixes a bug preventing cancelled state from getting credentials
introduced in #20667

components/skus/browser/rs/lib/src/models.rs
@@ -208,11 +208,19 @@ impl Order {
         self.status == "paid"
     }
 
+    pub fn is_cancelled(&self) -> bool {
+        self.status == "canceled" || self.status == "cancelled"
+    }
+
     pub fn has_expired(&self, now: NaiveDateTime) -> bool {
         if let Some(expires_at) = self.expires_at {
             return expires_at <= now;
         }
         false
+    }
+
+    pub fn can_submit_credentials(&self, now: NaiveDateTime) -> bool {
+        return self.is_paid() || (self.is_cancelled() && !self.has_expired(now));
     }
 }
 

components/skus/browser/rs/lib/src/sdk/credentials/fetch.rs
@@ -95,7 +95,7 @@ where
             _ => self.refresh_order(order_id).await?,
         };
 
-        if !order.is_paid() || order.has_expired(Utc::now().naive_utc()) {
+        if !order.can_submit_credentials(Utc::now().naive_utc()) {
             return Err(InternalError::OrderUnpaid.into());
         }
 