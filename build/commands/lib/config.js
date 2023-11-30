d27de0a590115c2e290e061137ac8564d4c6c09f
Claudio DeSouza <cdesouza@brave.com>
2023-11-30 08:47:53 +0000

    Revert "Deactivate dangling pointer checks"
    This reverts commit 4001e777a4c879ab525e2cd93cae05cfc913d7f8.

--- build/commands/lib/config.js
+++ build/commands/lib/config.js
@@ -268,7 +268,6 @@ const Config = function () {
   this.offline = getNPMConfig(['offline']) || false
   this.use_libfuzzer = false
   this.androidAabToApk = false
-  this.enable_dangling_raw_ptr_checks = false
   this.useBraveHermeticToolchain = this.rbeService.includes('.brave.com:')
   this.brave_services_key_id = getNPMConfig(['brave_services_key_id']) || ''
   this.service_key_aichat = getNPMConfig(['service_key_aichat']) || ''
@@ -445,7 +444,6 @@ Config.prototype.buildArgs = function () {
     brave_services_production_domain: this.braveServicesProductionDomain,
     brave_services_staging_domain: this.braveServicesStagingDomain,
     brave_services_dev_domain: this.braveServicesDevDomain,
-    enable_dangling_raw_ptr_checks: this.enable_dangling_raw_ptr_checks,
     brave_services_key_id: this.brave_services_key_id,
     service_key_aichat: this.service_key_aichat,
     ...this.extraGnArgs,
