fe0900cc9ea96ebe943a08d3f61e8b40cfc65d7f
AlexeyBarabash <alexey@brave.com>
2023-12-01 13:27:51 +0200

    Uplift of #21168 (squashed) to beta

--- browser/share/android/java/src/org/chromium/chrome/browser/share/send_tab_to_self/BraveManageAccountDevicesLinkView.java
+++ browser/share/android/java/src/org/chromium/chrome/browser/share/send_tab_to_self/BraveManageAccountDevicesLinkView.java
@@ -10,10 +10,25 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import org.chromium.components.signin.base.AccountCapabilities;
+import org.chromium.components.signin.base.AccountInfo;
+import org.chromium.components.signin.base.CoreAccountId;
+
 public class BraveManageAccountDevicesLinkView extends LinearLayout {
     public BraveManageAccountDevicesLinkView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         setVisibility(View.GONE);
+    }
+
+    public static AccountInfo getSharingAccountInfo() {
+        return new AccountInfo(
+                new CoreAccountId(""),
+                "",
+                "",
+                null,
+                null,
+                null,
+                new AccountCapabilities(new String[0], new boolean[0]));
     }
 }
