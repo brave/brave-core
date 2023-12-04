d14c7e26ffe33b69f9de2eb511611d5982af8ffc
brave-builds <brave-builds+devops@brave.com>
2023-12-04 22:21:37 +0000

    Uplift of #21175 (squashed) to beta

--- components/query_filter/utils.cc
+++ components/query_filter/utils.cc
@@ -102,9 +102,6 @@ static constexpr auto kSimpleQueryStringTrackers =
         "wickedid",
         // https://github.com/brave/brave-browser/issues/11578
         "yclid",
-        // https://github.com/brave/brave-browser/issues/33216
-        "ymclid",
-        "ysclid",
     });
 
 static constexpr auto kConditionalQueryStringTrackers =
