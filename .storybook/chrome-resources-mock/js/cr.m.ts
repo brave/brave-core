70688c6e1d086d73106cf5682652f63d21e7106b
Jay Harris <jay.harris@outlook.co.nz>
2023-12-06 09:17:41 +1300

    [Webpack 5]: Storybook Fixes (#21229)

--- .storybook/chrome-resources-mock/js/cr.m.ts
+++
@@ -1,4 +0,0 @@
-export function addWebUIListener(eventName: string, handler: Function) {}
-export function sendWithPromise(message: string, ...args: any[]): Promise<any> {
-  return new Promise((resolve) => console.log('sendWithPromise', message, args))
-}
