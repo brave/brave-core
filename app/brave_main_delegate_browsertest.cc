7fb687f5a4b756f9bb3a15c37d9eae7b8a2387c0
Claudio DeSouza <cdesouza@brave.com>
2023-12-05 08:38:08 +0000

    `kDownloadBubble` deleted upstream
    This feature flag was being enbled by default in brave, but it is now
    the default behaviour in upstream with no guarding flag.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/c91f56c5d92c0f9625dfc8088fcf185c4569990d
    
    commit c91f56c5d92c0f9625dfc8088fcf185c4569990d
    Author: Lily Chen <chlily@chromium.org>
    Date:   Fri Dec 1 20:04:30 2023 +0000
    
        [DownloadBubble] Remove feature flag
    
        This removes the base::Feature for kDownloadBubble as part of
        post-launch cleanup. This hardcodes IsDownloadBubbleEnabled() to return
        true unless the platform is ChromeOS Ash.
    
        Cleanup of the shelf code will be done once ChromeOS Ash no longer uses
        the download shelf (or no longer exists).
    
        Bug: 1504207

--- app/brave_main_delegate_browsertest.cc
+++ app/brave_main_delegate_browsertest.cc
@@ -324,7 +324,6 @@ IN_PROC_BROWSER_TEST_F(BraveMainDelegateBrowserTest, EnabledFeatures) {
     &features::kWinrtGeolocationImplementation,
 #endif
 #if !BUILDFLAG(IS_ANDROID)
-    &safe_browsing::kDownloadBubble,
     &sharing_hub::kDesktopScreenshots,
 #endif
     &media::kEnableTabMuting,
