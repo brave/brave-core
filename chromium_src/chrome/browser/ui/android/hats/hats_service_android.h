f5f954df0d21fea6e7cd9095885f41d0a243fb9f
mkarolin <max@brave.com>
2023-12-05 15:59:36 -0500

    Presubmit errors and warnings: Updated license headers.

--- chromium_src/chrome/browser/ui/android/hats/hats_service_android.h
+++ chromium_src/chrome/browser/ui/android/hats/hats_service_android.h
@@ -1,7 +1,7 @@
 /* Copyright (c) 2021 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_ANDROID_HATS_HATS_SERVICE_ANDROID_H_
 #define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_ANDROID_HATS_HATS_SERVICE_ANDROID_H_
