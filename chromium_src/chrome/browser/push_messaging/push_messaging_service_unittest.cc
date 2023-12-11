ed257b660ec5666296a04124a688d77ed2356a63
mkarolin <max@brave.com>
2023-12-11 10:57:40 -0500

    Disables failing upstream unit tests.
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/58cbedfd2f2bf6d3a7f2f7861bf0f945477f4390
    
    commit 58cbedfd2f2bf6d3a7f2f7861bf0f945477f4390
    Author: William Xu <willxu@google.com>
    Date:   Thu Nov 30 17:20:21 2023 +0000
    
        Reliably record notification permission revocations.
    
        First, record if there is a ScopedRevocationReporter on the stack in the onContentSettingsChanged path. If there is one, record which PermissionSourceUI it came from. If there isn't one, manually call PermissionRevoked and record an 'UNIDENTIFIED' PermissionSourceUI.
    
        We suspect that on Android, UNIDENTIFIED actually maps to ANDROID_SETTINGS, since permission toggles in Android are not handled in Chrome directly (i.e. in the OS Settings), and differences are detected and observed via the onContentSettingsChanged method. In these cases there is no explicit ScopedRevocationReporter created which means our current logging misses these events.
    
        Bug: 1473118

--- chromium_src/chrome/browser/push_messaging/push_messaging_service_unittest.cc
+++ chromium_src/chrome/browser/push_messaging/push_messaging_service_unittest.cc
@@ -1,10 +1,20 @@
 /* Copyright (c) 2019 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "brave/test/base/brave_testing_profile.h"
+
+// These tests fail because we do not record permissions UKM, see
+// brave/chromium_src/components/permissions/permission_uma_util.cc
+// GetUkmSourceId override.
+#define RecordsRevocationAndSourceUiNoReporterTest \
+  DISABLED_RecordsRevocationAndSourceUiNoReporterTest
+#define RecordsRevocationAndSourceUiWithReporterTest \
+  DISABLED_RecordsRevocationAndSourceUiWithReporterTest
 
 #define TestingProfile BraveTestingProfile
 #include "src/chrome/browser/push_messaging/push_messaging_service_unittest.cc"
 #undef TestingProfile
+#undef RecordsRevocationAndSourceUiWithReporterTest
+#undef RecordsRevocationAndSourceUiNoReporterTest
