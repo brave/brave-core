fb7e92e63dd2c6c25e86d6650a38d3a2655a7b03
Claudio DeSouza <cdesouza@brave.com>
2023-12-21 12:59:55 +0000

    [ios] Browser `tab` moved into `model/` subfolder.
    This change merely affects inclusion paths.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/9baed1f5d6064a8aa83525afe3f2a87c355418b4
    
    commit 9baed1f5d6064a8aa83525afe3f2a87c355418b4
    Author: Sylvain Defresne <sdefresne@chromium.org>
    Date:   Tue Dec 19 16:25:59 2023 +0000
    
        [ios] Move tabs related model files to //ios/chrome/browser/tabs/model
    
        As discussed in https://crrev.com/c/5134814, move a couple of files from
        //ios/chrome/browser/sync/model to //ios/chrome/browser/tabs/model.
    
        Bug: none

--- chromium_src/ios/chrome/browser/tabs/model/tab_helper_util.mm
+++ chromium_src/ios/chrome/browser/tabs/model/tab_helper_util.mm
@@ -7,7 +7,7 @@
 
 #include "ios/chrome/browser/complex_tasks/model/ios_task_tab_helper.h"
 #include "ios/chrome/browser/sessions/ios_chrome_session_tab_helper.h"
-#include "ios/chrome/browser/sync/model/ios_chrome_synced_tab_delegate.h"
+#include "ios/chrome/browser/tabs/model/ios_chrome_synced_tab_delegate.h"
 #include "ios/chrome/browser/web/session_state/web_session_state_tab_helper.h"
 
 void AttachTabHelpers(web::WebState* web_state, bool for_prerender) {
