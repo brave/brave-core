14e67a39a122e57faaadfdcdfe3f99ce3f60c39d
Claudio DeSouza <cdesouza@brave.com>
2023-12-22 14:29:21 +0000

    [ios] Browser `web` moved into `model/` subfolder
    This change affects inclusion paths, deps, and some of the shadowing
    files in our codebase. There are no functional changes however.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/dca21fb37574f4076e051aa229bba6cc122a1e4f
    
    commit dca21fb37574f4076e051aa229bba6cc122a1e4f
    Author: Weizhong Xia <weizhong@google.com>
    Date:   Wed Dec 20 20:10:11 2023 +0000
    
        [Code Health][iOS] Move ios/c/b/web to model subfolder
    
        To simplify the folder hierarchy on iOS and have a clearer layer
        separation, all the files in ios/c/b/<foo>/ will be moved to
        ios/c/b/<foo>/model/ to showcase that they are model files.
    
        Bug: 1480376

--- chromium_src/ios/chrome/browser/web/model/chrome_main_parts.h
+++ chromium_src/ios/chrome/browser/web/model/chrome_main_parts.h
@@ -3,8 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#ifndef BRAVE_CHROMIUM_SRC_IOS_CHROME_BROWSER_WEB_CHROME_MAIN_PARTS_H_
-#define BRAVE_CHROMIUM_SRC_IOS_CHROME_BROWSER_WEB_CHROME_MAIN_PARTS_H_
+#ifndef BRAVE_CHROMIUM_SRC_IOS_CHROME_BROWSER_WEB_MODEL_CHROME_MAIN_PARTS_H_
+#define BRAVE_CHROMIUM_SRC_IOS_CHROME_BROWSER_WEB_MODEL_CHROME_MAIN_PARTS_H_
 
 #include "ios/web/public/init/web_main_parts.h"
 
@@ -17,7 +17,7 @@
  protected:                                \
   void PreCreateMainMessageLoop
 
-#include "src/ios/chrome/browser/web/chrome_main_parts.h"  // IWYU pragma: export
+#include "src/ios/chrome/browser/web/model/chrome_main_parts.h"  // IWYU pragma: export
 #undef PreCreateMainMessageLoop
 
-#endif  // BRAVE_CHROMIUM_SRC_IOS_CHROME_BROWSER_WEB_CHROME_MAIN_PARTS_H_
+#endif  // BRAVE_CHROMIUM_SRC_IOS_CHROME_BROWSER_WEB_MODEL_CHROME_MAIN_PARTS_H_
--- chromium_src/ios/chrome/browser/web/model/chrome_main_parts.mm
+++ chromium_src/ios/chrome/browser/web/model/chrome_main_parts.mm
@@ -3,12 +3,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "ios/chrome/browser/web/chrome_main_parts.h"
+#include "ios/chrome/browser/web/model/chrome_main_parts.h"
 #include "brave/ios/browser/application_context/brave_application_context_impl.h"
 
 #define PreCreateMainMessageLoop PreCreateMainMessageLoop_ChromiumImpl
 #define ApplicationContextImpl BraveApplicationContextImpl
-#include "src/ios/chrome/browser/web/chrome_main_parts.mm"
+#include "src/ios/chrome/browser/web/model/chrome_main_parts.mm"
 #undef ApplicationContextImpl
 #undef PreCreateMainMessageLoop
 
