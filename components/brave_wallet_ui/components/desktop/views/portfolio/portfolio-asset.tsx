4d025ee4a25f7b297b04542827cb55d12dd00a51
Josh Leonard <30185185+josheleonard@users.noreply.github.com>
2023-12-28 18:25:45 +0000

    perf: search only user tokens for portfolio asset details lookup

--- components/brave_wallet_ui/components/desktop/views/portfolio/portfolio-asset.tsx
+++ components/brave_wallet_ui/components/desktop/views/portfolio/portfolio-asset.tsx
@@ -74,12 +74,10 @@ import {
   useGetTokenSpotPricesQuery,
   useGetPriceHistoryQuery,
   useGetDefaultFiatCurrencyQuery,
-  useGetExternalRewardsWalletQuery
+  useGetExternalRewardsWalletQuery,
+  useGetUserTokensRegistryQuery
 } from '../../../../common/slices/api.slice'
-import {
-  useAccountsQuery,
-  useGetCombinedTokensRegistryQuery
-} from '../../../../common/slices/api.slice.extra'
+import { useAccountsQuery } from '../../../../common/slices/api.slice.extra'
 import {
   querySubscriptionOptions60s //
 } from '../../../../common/slices/constants'
@@ -132,8 +130,8 @@ export const PortfolioAsset = () => {
   )
 
   // Queries
-  const { data: combinedTokensRegistry, isLoading: isLoadingTokens } =
-    useGetCombinedTokensRegistryQuery()
+  const { data: userTokensRegistry, isLoading: isLoadingTokens } =
+    useGetUserTokensRegistryQuery()
   const { data: defaultFiat = 'USD' } = useGetDefaultFiatCurrencyQuery()
   const { data: externalRewardsInfo } = useGetExternalRewardsWalletQuery()
 
@@ -143,12 +141,12 @@ export const PortfolioAsset = () => {
     if (isRewardsToken) {
       return getRewardsBATToken(externalRewardsInfo?.provider ?? undefined)
     }
-    return assetId ? combinedTokensRegistry?.entities[assetId] : undefined
+    return assetId ? userTokensRegistry?.entities[assetId] : undefined
   }, [
     isRewardsToken,
     externalRewardsInfo?.provider,
     assetId,
-    combinedTokensRegistry
+    userTokensRegistry
   ])
 
   // queries
