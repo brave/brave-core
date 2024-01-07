f20d88b0f40e466b04bca79561c8303a9a43f19f
Jay Harris <jay.harris@outlook.co.nz>
2024-01-08 12:49:58 +1300

    Fix loading spinner background

--- components/brave_news/browser/resources/feed/LoadingCard.tsx
+++ components/brave_news/browser/resources/feed/LoadingCard.tsx
@@ -15,7 +15,6 @@ const Container = styled(Card)`
 
 const Spinner = styled(ProgressRing)`
   --leo-progressring-color: var(--bn-glass-25);
-  backdrop-filter: blur(64px);
 `
 
 export default function LoadingCard() {
