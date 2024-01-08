b55fab9ab63c5729057aadef5ea50f3871ad1c36
Mikhail Atuchin <matuchin@brave.com>
2024-01-08 17:47:19 +0700

    Support versions for perf profiles

--- tools/perf/components/perf_profile.py
+++ tools/perf/components/perf_profile.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2022 The Brave Authors. All rights reserved.
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
-# you can obtain one at http://mozilla.org/MPL/2.0/.
+# You can obtain one at https://mozilla.org/MPL/2.0/.
 
 import logging
 import os
@@ -9,6 +9,9 @@ import uuid
 import shutil
 
 import components.path_util as path_util
+
+from components.perf_test_utils import GetFileAtRevision
+from components.version import BraveVersion
 
 with path_util.SysPath(path_util.GetBraveScriptDir(), 0):
   from lib.util import extract_zip
@@ -32,8 +35,9 @@ def DownloadFromGoogleStorage(sha1: str, output_path: str) -> None:
     raise RuntimeError(f'Failed to download: {gs_path}')
 
 
-def GetProfilePath(profile: str, work_directory: str) -> str:
-  assert (profile != 'clean')
+def GetProfilePath(profile: str, work_directory: str,
+                   version: BraveVersion) -> str:
+  assert profile != 'clean'
 
   profile_dir = None
   if os.path.isdir(profile):  # local profile
@@ -42,30 +46,37 @@ def GetProfilePath(profile: str, work_directory: str) -> str:
     logging.debug('Copy %s to %s ', profile, profile_dir)
     shutil.copytree(profile, profile_dir)
   else:
-    zip_path = os.path.join(path_util.GetBravePerfProfileDir(),
-                            profile + '.zip')
     zip_path_sha1 = os.path.join(path_util.GetBravePerfProfileDir(),
-                                 profile + '.zip.sha1')
+                                 f'{profile}.zip.sha1')
+    zip_path_sha1_fallback = zip_path_sha1 + '.fallback'
 
-    if not os.path.isfile(zip_path_sha1):
-      raise RuntimeError(f'Unknown profile, file {zip_path_sha1} not found')
+    if not os.path.isfile(zip_path_sha1) and not os.path.isfile(
+        zip_path_sha1_fallback):
+      raise RuntimeError(
+          f'Unknown profile, file {zip_path_sha1}[.fallback] not found')
 
-    sha1 = None
-    with open(zip_path_sha1, 'r', encoding='utf8') as sha1_file:
-      sha1 = sha1_file.read().rstrip()
-    logging.debug('Expected hash %s for profile %s', sha1, profile)
+    sha1 = GetFileAtRevision(zip_path_sha1, version.git_hash)
+    if sha1 is None:
+      zip_path_sha1 = zip_path_sha1_fallback
+      logging.info('Using the fallback profile %s', zip_path_sha1)
+      if not os.path.isfile(zip_path_sha1):
+        raise RuntimeError(f'No fallback profile for {zip_path_sha1} not found')
+
+      with open(zip_path_sha1, 'r', encoding='utf8') as sha1_file:
+        sha1 = sha1_file.read().rstrip()
+
+    assert sha1 is not None
+    sha1 = sha1.rstrip()
+    logging.debug('Use gs hash %s for profile %s', sha1, profile)
+
+    zip_path = os.path.join(path_util.GetBravePerfProfileDir(),
+                            f'{profile}_{sha1}.zip')
     if not sha1:
       raise RuntimeError(f'Bad sha1 in {zip_path_sha1}')
 
     if not os.path.isfile(zip_path):
       DownloadFromGoogleStorage(sha1, zip_path)
-    else:
-      current_sha1 = download_from_google_storage.get_sha1(zip_path)
-      if current_sha1 != sha1:
-        logging.info(
-            'Profile %s needs to be updated. Current hash %s, expected %s',
-            zip_path, current_sha1, sha1)
-        DownloadFromGoogleStorage(sha1, zip_path)
+
     profile_dir = os.path.join(work_directory, 'profiles', sha1)
 
     if not os.path.isdir(profile_dir):
