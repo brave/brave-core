194a9d623e20784f9e6339ef48c6211aa0997230
Kyle Hickinson <git@kylehickinson.com>
2024-01-08 16:01:52 -0500

    Build frameworks to src/out directory instead of inside brave-ios folder

--- ios/brave-ios/BraveCore/build_in_core.sh
+++ ios/brave-ios/BraveCore/build_in_core.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-# Back to brave-core root since currently brave-ios has its own npm commands
-# TODO(@brave/ios): Remove iOS-specific npm commands if possible or shift them into brave-core root
-cd "${PROJECT_DIR}/../../.."
+cd $PROJECT_DIR
+src_dir="$PROJECT_DIR/../../../.."
+echo "Current Dir: $(pwd)"
+
 target_environment=""
 target_environment_dir=""
 if [[ $PLATFORM_NAME == "iphonesimulator" ]]; then
@@ -18,22 +19,24 @@ if [[ $HOST_ARCH == "x86_64" ]]; then
   target_arch_dir=""
 fi
 
-ios_dir="$(pwd)/ios/brave-ios/BraveCore/build"
-cp -fR "$ios_dir/../placeholders/BraveCore.xcframework" "$ios_dir"
-cp -fR "$ios_dir/../placeholders/MaterialComponents.xcframework" "$ios_dir"
+output_dir="$src_dir/out/ios_Build"
+mkdir -p $output_dir
+
+cp -fR "$(pwd)/../BraveCore/build/placeholders/BraveCore.xcframework" "$output_dir"
+cp -fR "$(pwd)/../BraveCore/build/placeholders/MaterialComponents.xcframework" "$output_dir"
 
 npm run build -- $CONFIGURATION --target_os=ios --target_arch=$target_arch $target_environment
 
 # Create xcframeworks
-pushd "../out/iOS_${CONFIGURATION}_${target_arch_dir}${target_environment_dir}"
-rm -rf "$ios_dir/BraveCore.xcframework"
-xcodebuild -create-xcframework -framework "BraveCore.framework" -debug-symbols "$(pwd)/BraveCore.dSYM" -output "$ios_dir/BraveCore.xcframework"
-rm -rf "$ios_dir/MaterialComponents.xcframework"
-xcodebuild -create-xcframework -framework "MaterialComponents.framework" -debug-symbols "$(pwd)/MaterialComponents.dSYM" -output "$ios_dir/MaterialComponents.xcframework"
+pushd "$src_dir/out/iOS_${CONFIGURATION}_${target_arch_dir}${target_environment_dir}"
+rm -rf "$output_dir/BraveCore.xcframework"
+xcodebuild -create-xcframework -framework "BraveCore.framework" -debug-symbols "$(pwd)/BraveCore.dSYM" -output "$output_dir/BraveCore.xcframework"
+rm -rf "$output_dir/MaterialComponents.xcframework"
+xcodebuild -create-xcframework -framework "MaterialComponents.framework" -debug-symbols "$(pwd)/MaterialComponents.dSYM" -output "$output_dir/MaterialComponents.xcframework"
 
 # Delete Chromium Assets from BraveCore.framework since they aren't used.
 # TODO(@brave/ios): Get this removed in the brave-core builds if possible
-find "$ios_dir/BraveCore.xcframework" -name 'BraveCore.framework' -print0 | while read -d $'\0' framework
+find "$output_dir/BraveCore.xcframework" -name 'BraveCore.framework' -print0 | while read -d $'\0' framework
 do
   if [[ -f "$framework/Assets.car" ]]; then
     rm "$framework/Assets.car"
@@ -49,7 +52,7 @@ copy_args=(
   "brave_services_key"
   "brave_stats_api_key"
 )
-args_file="$ios_dir/args.xcconfig"
+args_file="$output_dir/args.xcconfig"
 if [ -f $args_file ]; then
   rm $args_file
 fi
--- ios/brave-ios/Package.swift
+++ ios/brave-ios/Package.swift
@@ -118,8 +118,8 @@ var package = Package(
     ),
     .target(name: "BraveShields", dependencies: ["Strings", "Preferences"], plugins: ["LoggerPlugin"]),
     .target(name: "DesignSystem", plugins: ["LeoAssetsPlugin"]),
-    .binaryTarget(name: "BraveCore", path: "BraveCore/build/BraveCore.xcframework"),
-    .binaryTarget(name: "MaterialComponents", path: "BraveCore/build/MaterialComponents.xcframework"),
+    .binaryTarget(name: "BraveCore", path: "../../../out/ios_Build/BraveCore.xcframework"),
+    .binaryTarget(name: "MaterialComponents", path: "../../../out/ios_Build/MaterialComponents.xcframework"),
     .binaryTarget(name: "GRDWireGuardKit", path: "ThirdParty/GRDWireGuardKit/GRDWireGuardKit.xcframework"),
     .target(
       name: "Storage",
