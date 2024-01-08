eb64d09a5db4592267968454e4f36da739a046f6
Artem Samoilenko <artem@brave.com>
2024-01-08 12:31:43 -0500

    [Android] Minor unit tests fixes

--- android/javatests/org/chromium/chrome/browser/BraveSwipeRefreshHandlerTest.java
+++ android/javatests/org/chromium/chrome/browser/BraveSwipeRefreshHandlerTest.java
@@ -74,7 +74,8 @@ public class BraveSwipeRefreshHandlerTest {
                 new PlaceholderJankTracker(),
                 rootUiCoordinator.getToolbarManager()::getToolbar,
                 null,
-                null);
+                null,
+                rootUiCoordinator.getToolbarManager().getTabStripHeightSupplier());
     }
 
     @Test
