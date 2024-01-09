0b9ce0568fe3301bcc58d584a2501a9ae6080e40
Terry Mancey <terry.mancey@icloud.com>
2024-01-09 07:42:45 -0500

    [ads] Cleanup text embedding based prediction sampler

--- components/brave_ads/core/internal/serving/prediction/embedding_based/sampling/creative_ad_embedding_based_predictor_sampling.h
+++ components/brave_ads/core/internal/serving/prediction/embedding_based/sampling/creative_ad_embedding_based_predictor_sampling.h
@@ -16,6 +16,14 @@
 
 namespace brave_ads {
 
+// Performs a random sampling operation based on the provided probabilities. It
+// generates a random number and iterates through the creative ad probabilities,
+// accumulating the probabilities. Once the accumulated sum is greater than or
+// approximately equal to the random number, the function returns the
+// corresponding creative ad. If no ad is sampled during the iteration (which
+// can happen if the probabilities do not sum up to 1), the function returns
+// `std::nullopt`.
+
 template <typename T>
 std::optional<T> MaybeSampleCreativeAd(
     const std::vector<T>& creative_ads,
@@ -27,7 +35,7 @@ std::optional<T> MaybeSampleCreativeAd(
   for (size_t i = 0; i < creative_ads.size(); ++i) {
     sum += creative_ad_probabilities.at(i);
 
-    if (rand <= sum || base::IsApproximatelyEqual(
+    if (sum >= rand || base::IsApproximatelyEqual(
                            rand, sum, std::numeric_limits<double>::epsilon())) {
       return creative_ads.at(i);
     }
