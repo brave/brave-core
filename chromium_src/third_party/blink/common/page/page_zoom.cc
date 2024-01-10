4a57b373270c223b93ea43237ca82efa665678e7
Claudio DeSouza <cdesouza@brave.com>
2024-01-10 13:23:05 +0000

    Move `kPresetZoomFactors` overrides to the new blink path
    This change corrects the naming and location for the override of this
    values to the new location.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/fb71620081ef26d4fce675edf57919840d3bf807
    
    commit fb71620081ef26d4fce675edf57919840d3bf807
    Author: Tove Petersson <tovep@chromium.org>
    Date:   Fri Jan 5 11:43:10 2024 +0000
    
        Move kPresetZoomFactors to blink
    
        kPresetZoomFactors is moved to blink so that it can be exposed to
        JavaScript without interprocess communication.
    
        Bug: 1466247

---
+++ chromium_src/third_party/blink/common/page/page_zoom.cc
@@ -0,0 +1,21 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "third_party/blink/public/common/page/page_zoom.h"
+
+#define kPresetZoomFactorsArray kPresetZoomFactorsArray_ChromiumImpl
+#define kPresetZoomFactors kPresetZoomFactors_ChromiumImpl
+#include "src/third_party/blink/common/page/page_zoom.cc"
+#undef kPresetZoomFactorsArray
+#undef kPresetZoomFactors
+
+namespace blink {
+
+static constexpr double kPresetZoomFactorsArray[] = {
+    0.25,    1 / 3.0, 0.5, 2 / 3.0, 0.75, 0.8, 0.9, 1.0, 1.1, 1.25,
+    4 / 3.0, 7 / 5.0, 1.5, 1.75,    2.0,  2.5, 3.0, 4.0, 5.0};
+const base::span<const double> kPresetZoomFactors(kPresetZoomFactorsArray);
+
+}  // namespace blink
