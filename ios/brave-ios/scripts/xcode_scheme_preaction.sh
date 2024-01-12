d7cde9c9c93eae84fb8e290ddcae8461389fb696
Kyle Hickinson <git@kylehickinson.com>
2024-01-12 16:17:40 -0500

    Make fastlane channel builds also release to testers
    Also fixes an issue where Xcode injected environment variables were available during GN builds which would cause build failures in Release configurations

---
+++ ios/brave-ios/scripts/xcode_scheme_preaction.sh
@@ -0,0 +1,17 @@
+# Copyright (c) 2024 The Brave Authors. All rights reserved.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at https://mozilla.org/MPL/2.0/.
+
+# This script is run by Xcode and assumes Xcode build settings are injected
+# into the environment.
+
+# $ACTION is empty for Xcode builds unfortunately
+# $RUN_CLANG_STATIC_ANALYZER is NO for builds, YES for cleans 
+if [[ $RUN_CLANG_STATIC_ANALYZER = "NO" ]]; then
+  # Do not inject Xcode build configs into the GN build
+  env -i PATH="$PATH" python3 \
+    "${PROJECT_DIR}/../scripts/scheme_preaction.py" \
+    --configuration $CONFIGURATION \
+    --platform_name $PLATFORM_NAME
+fi
