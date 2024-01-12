8d3743e2a6580ddca03922eb384cb0618cf6ce43
Jacob Sikorski <jacob.sikorski@gmail.com>
2024-01-12 16:17:36 -0700

    Remove imports

--- components/debounce/content/browser/debounce_navigation_throttle.cc
+++ components/debounce/content/browser/debounce_navigation_throttle.cc
@@ -15,7 +15,6 @@
 #include "base/memory/weak_ptr.h"
 #include "base/task/sequenced_task_runner.h"
 #include "brave/components/debounce/core/browser/debounce_service.h"
-#include "brave/components/debounce/core/common/pref_names.h"
 #include "components/user_prefs/user_prefs.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/navigation_entry.h"
