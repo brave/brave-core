2581b2c46cd197607975c115b68e4cc3b32df278
nvonpentz <12549658+nvonpentz@users.noreply.github.com>
2024-01-16 17:24:24 -0500

    Add SERVICE_KEY_AI_CHAT and KEY_ID to config.js
    This way they can be sourced from .env.

--- build/commands/lib/config.js
+++ build/commands/lib/config.js
@@ -241,6 +241,8 @@ const Config = function () {
   this.androidAabToApk = false
   this.enable_dangling_raw_ptr_checks = false
   this.useBraveHermeticToolchain = this.rbeService.includes('.brave.com:')
+  this.key_id = getNPMConfig(['key_id']) || ''
+  this.service_key_ai_chat = getNPMConfig(['service_key_ai_chat']) || ''
 }
 
 Config.prototype.isReleaseBuild = function () {
@@ -414,6 +416,8 @@ Config.prototype.buildArgs = function () {
     brave_services_staging_domain: this.braveServicesStagingDomain,
     brave_services_dev_domain: this.braveServicesDevDomain,
     enable_dangling_raw_ptr_checks: this.enable_dangling_raw_ptr_checks,
+    key_id: this.key_id,
+    service_key_ai_chat: this.service_key_ai_chat,
     ...this.extraGnArgs,
   }
 
