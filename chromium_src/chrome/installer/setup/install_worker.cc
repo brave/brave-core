fulldiveVR/e-browser-core

Persist value after Windows service removal; don't run more than once.
Includes unit tests.

chromium_src/chrome/installer/setup/install_worker.cc
@@ -14,6 +14,7 @@
 #include "base/process/launch.h"
 #include "base/process/process.h"
 #include "base/version.h"
+#include "base/win/registry.h"
 #include "brave/components/brave_vpn/common/buildflags/buildflags.h"
 #include "build/buildflag.h"
 #include "chrome/install_static/install_util.h"
@@ -41,12 +42,6 @@
 
 namespace {
 
-base::FilePath GetBraveVPNHelperPath(const base::FilePath& target_path,
-                                     const base::Version& version) {
-  return target_path.AppendASCII(version.GetString())
-      .Append(brave_vpn::kBraveVPNHelperExecutable);
-}
-
 // delete `BraveVpnWireguardService` from services and remove the tray icon.
 bool UninstallBraveVPNWireguardService(const base::FilePath& exe_path,
                                        const CallbackWorkItem&) {
@@ -67,8 +62,7 @@ bool UninstallBraveVPNWireguardService(const base::FilePath& exe_path,
 // purchased Brave VPN and has credentials or when VPN is used.
 //
 // See https://github.com/brave/brave-browser/issues/33726 for more info
-void AddBraveVPNHelperServiceWorkItems(const base::FilePath& vpn_service_path,
-                                       WorkItemList* list) {
+void AddBraveVPNHelperServiceWorkItems() {
   DCHECK(::IsUserAnAdmin());
   // delete `BraveVpnService` from services
   if (!installer::InstallServiceWorkItem::DeleteService(
@@ -98,19 +92,79 @@ void AddBraveVPNWireguardServiceWorkItems(
 
 }  // namespace
 
-#define GetElevationServicePath GetElevationServicePath(                   \
-  target_path, new_version), install_list);                                \
-  AddBraveVPNWireguardServiceWorkItems(                                    \
-      brave_vpn::GetBraveVPNWireguardServiceInstallationPath(target_path,  \
-                                                             new_version), \
-      install_list);                                                       \
-  AddBraveVPNHelperServiceWorkItems(GetBraveVPNHelperPath
+namespace installer {
+
+// We can consider removing this code and the registry key sometime in the
+// future once enough time has gone by and we're confident folks have ran this
+// cleanup. This same service cleanup happens on uninstall.
+//
+// For more info see https://github.com/brave/brave-browser/issues/33726
+bool OneTimeServiceCleanup(const base::FilePath& target_path,
+                           const base::Version& new_version,
+                           WorkItemList* install_list,
+                           bool is_test) {
+  // Check registry for `ran` value.
+  // Only run the clean up logic if this hasn't ran yet.
+  base::win::RegKey key;
+  LONG rv = key.Create(HKEY_LOCAL_MACHINE,
+                       brave_vpn::kBraveVpnOneTimeServiceCleanupStoragePath,
+                       KEY_ALL_ACCESS);
+  if (rv != ERROR_SUCCESS) {
+    VLOG(1) << "Failed to open registry key:"
+            << brave_vpn::kBraveVpnOneTimeServiceCleanupStoragePath << "\n"
+            << logging::SystemErrorCodeToString(rv);
+    return false;
+  }
+
+  if (!key.Valid()) {
+    VLOG(1) << "Registry key not valid:"
+            << brave_vpn::kBraveVpnOneTimeServiceCleanupStoragePath;
+    return false;
+  }
+
+  DWORD cleanup_ran = 0;
+  if (key.ReadValueDW(brave_vpn::kBraveVpnOneTimeServiceCleanupValue,
+                      &cleanup_ran) == ERROR_SUCCESS) {
+    if (cleanup_ran == 1) {
+      VLOG(1) << "OneTimeServiceCleanup has already ran; skipping";
+      return false;
+    }
+  }
+
+  // If is_test=true, the removal won't happen. Default is false.
+  // See chromium_src/chrome/installer/setup/install_worker_unittest.cc
+  if (!is_test) {
+    AddBraveVPNWireguardServiceWorkItems(
+        brave_vpn::GetBraveVPNWireguardServiceInstallationPath(target_path,
+                                                               new_version),
+        install_list);
+    AddBraveVPNHelperServiceWorkItems();
+  }
+
+  // Write registry value
+  cleanup_ran = 1;
+  rv = key.WriteValue(brave_vpn::kBraveVpnOneTimeServiceCleanupValue,
+                      &cleanup_ran, REG_DWORD, sizeof(DWORD));
+  if (rv != ERROR_SUCCESS) {
+    VLOG(1) << "Failed to write registry key value: "
+            << brave_vpn::kBraveVpnOneTimeServiceCleanupStoragePath << ":"
+            << brave_vpn::kBraveVpnOneTimeServiceCleanupValue << "\n"
+            << logging::SystemErrorCodeToString(rv);
+  }
+  return true;
+}
+
+}  // namespace installer
+
+#define AddUpdateDowngradeVersionItem                            \
+  OneTimeServiceCleanup(target_path, new_version, install_list); \
+  AddUpdateDowngradeVersionItem
 
 #endif  // BUILDFLAG(ENABLE_BRAVE_VPN)
 
 #include "src/chrome/installer/setup/install_worker.cc"
 #if BUILDFLAG(ENABLE_BRAVE_VPN)
-#undef GetElevationServicePath
+#undef AddUpdateDowngradeVersionItem
 #endif  // BUILDFLAG(ENABLE_BRAVE_VPN)
 #if defined(OFFICIAL_BUILD)
 #include "chrome/install_static/brave_restore_google_update_integration.h"

chromium_src/chrome/installer/setup/install_worker.h
@@ -1,0 +1,22 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_INSTALLER_SETUP_INSTALL_WORKER_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_INSTALLER_SETUP_INSTALL_WORKER_H_
+
+#include "src/chrome/installer/setup/install_worker.h"  // IWYU pragma: export
+
+namespace installer {
+
+// Method specifically used to do a one-time removal of VPN services from
+// a person's machine. The service will then only be installed if they have
+// purchased the Brave VPN product.
+bool OneTimeServiceCleanup(const base::FilePath& target_path,
+                           const base::Version& new_version,
+                           WorkItemList* install_list,
+                           bool is_test = false);
+}  // namespace installer
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_INSTALLER_SETUP_INSTALL_WORKER_H_

brave/brave-core

chromium_src/chrome/installer/setup/install_worker.cc
@@ -14,6 +14,7 @@
 #include "base/process/launch.h"
 #include "base/process/process.h"
 #include "base/version.h"
+#include "base/win/registry.h"
 #include "brave/components/brave_vpn/common/buildflags/buildflags.h"
 #include "build/buildflag.h"
 #include "chrome/install_static/install_util.h"
@@ -41,12 +42,6 @@
 
 namespace {
 
-base::FilePath GetBraveVPNHelperPath(const base::FilePath& target_path,
-                                     const base::Version& version) {
-  return target_path.AppendASCII(version.GetString())
-      .Append(brave_vpn::kBraveVPNHelperExecutable);
-}
-
 // delete `BraveVpnWireguardService` from services and remove the tray icon.
 bool UninstallBraveVPNWireguardService(const base::FilePath& exe_path,
                                        const CallbackWorkItem&) {
@@ -67,8 +62,7 @@ bool UninstallBraveVPNWireguardService(const base::FilePath& exe_path,
 // purchased Brave VPN and has credentials or when VPN is used.
 //
 // See https://github.com/brave/brave-browser/issues/33726 for more info
-void AddUninstallVpnServiceWorkItems(const base::FilePath& vpn_service_path,
-                                     WorkItemList* list) {
+void AddUninstallVpnServiceWorkItems() {
   DCHECK(::IsUserAnAdmin());
   // delete `BraveVpnService` from services
   if (!installer::InstallServiceWorkItem::DeleteService(
@@ -98,19 +92,79 @@ void AddUninstallWireguardServiceWorkItems(
 
 }  // namespace
 
-#define GetElevationServicePath GetElevationServicePath(                   \
-  target_path, new_version), install_list);                                \
-  AddUninstallWireguardServiceWorkItems(                                   \
-      brave_vpn::GetBraveVPNWireguardServiceInstallationPath(target_path,  \
-                                                             new_version), \
-      install_list);                                                       \
-  AddUninstallVpnServiceWorkItems(GetBraveVPNHelperPath
+namespace installer {
+
+// We can consider removing this code and the registry key sometime in the
+// future once enough time has gone by and we're confident folks have ran this
+// cleanup. This same service cleanup happens on uninstall.
+//
+// For more info see https://github.com/brave/brave-browser/issues/33726
+bool OneTimeVpnServiceCleanup(const base::FilePath& target_path,
+                              const base::Version& new_version,
+                              WorkItemList* install_list,
+                              bool is_test) {
+  // Check registry for `ran` value.
+  // Only run the clean up logic if this hasn't ran yet.
+  base::win::RegKey key;
+  LONG rv = key.Create(HKEY_LOCAL_MACHINE,
+                       brave_vpn::kBraveVpnOneTimeServiceCleanupStoragePath,
+                       KEY_ALL_ACCESS);
+  if (rv != ERROR_SUCCESS) {
+    VLOG(1) << "Failed to open registry key:"
+            << brave_vpn::kBraveVpnOneTimeServiceCleanupStoragePath << "\n"
+            << logging::SystemErrorCodeToString(rv);
+    return false;
+  }
+
+  if (!key.Valid()) {
+    VLOG(1) << "Registry key not valid:"
+            << brave_vpn::kBraveVpnOneTimeServiceCleanupStoragePath;
+    return false;
+  }
+
+  DWORD cleanup_ran = 0;
+  if (key.ReadValueDW(brave_vpn::kBraveVpnOneTimeServiceCleanupValue,
+                      &cleanup_ran) == ERROR_SUCCESS) {
+    if (cleanup_ran == 1) {
+      VLOG(1) << "OneTimeVpnServiceCleanup has already ran; skipping";
+      return false;
+    }
+  }
+
+  // If is_test=true, the removal won't happen. Default is false.
+  // See chromium_src/chrome/installer/setup/install_worker_unittest.cc
+  if (!is_test) {
+    AddUninstallWireguardServiceWorkItems(
+        brave_vpn::GetBraveVPNWireguardServiceInstallationPath(target_path,
+                                                               new_version),
+        install_list);
+    AddUninstallVpnServiceWorkItems();
+  }
+
+  // Write registry value
+  cleanup_ran = 1;
+  rv = key.WriteValue(brave_vpn::kBraveVpnOneTimeServiceCleanupValue,
+                      &cleanup_ran, REG_DWORD, sizeof(DWORD));
+  if (rv != ERROR_SUCCESS) {
+    VLOG(1) << "Failed to write registry key value: "
+            << brave_vpn::kBraveVpnOneTimeServiceCleanupStoragePath << ":"
+            << brave_vpn::kBraveVpnOneTimeServiceCleanupValue << "\n"
+            << logging::SystemErrorCodeToString(rv);
+  }
+  return true;
+}
+
+}  // namespace installer
+
+#define AddUpdateDowngradeVersionItem                               \
+  OneTimeVpnServiceCleanup(target_path, new_version, install_list); \
+  AddUpdateDowngradeVersionItem
 
 #endif  // BUILDFLAG(ENABLE_BRAVE_VPN)
 
 #include "src/chrome/installer/setup/install_worker.cc"
 #if BUILDFLAG(ENABLE_BRAVE_VPN)
-#undef GetElevationServicePath
+#undef AddUpdateDowngradeVersionItem
 #endif  // BUILDFLAG(ENABLE_BRAVE_VPN)
 #if defined(OFFICIAL_BUILD)
 #include "chrome/install_static/brave_restore_google_update_integration.h"

chromium_src/chrome/installer/setup/install_worker.h
@@ -1,0 +1,22 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_INSTALLER_SETUP_INSTALL_WORKER_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_INSTALLER_SETUP_INSTALL_WORKER_H_
+
+#include "src/chrome/installer/setup/install_worker.h"  // IWYU pragma: export
+
+namespace installer {
+
+// Method specifically used to do a one-time removal of VPN services from
+// a person's machine. The service will then only be installed if they have
+// purchased the Brave VPN product.
+bool OneTimeVpnServiceCleanup(const base::FilePath& target_path,
+                              const base::Version& new_version,
+                              WorkItemList* install_list,
+                              bool is_test = false);
+}  // namespace installer
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_INSTALLER_SETUP_INSTALL_WORKER_H_