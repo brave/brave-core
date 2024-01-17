870c077f7f739eec439fc4b2afab727265338920
nvonpentz <12549658+nvonpentz@users.noreply.github.com>
2024-01-16 19:02:34 -0500

    Add comments explaining functions in service_key_utils

--- components/brave_service_keys/service_key_utils.h
+++ components/brave_service_keys/service_key_utils.h
@@ -16,14 +16,22 @@
 
 namespace brave_service_keys {
 
+// Calculates the SHA-256 hash of the supplied payload and returns a pair
+// comprising of the digest header field, and header value in the format
+// "SHA-256=<base64_encoded_hash>".
 std::pair<std::string, std::string> GetDigestHeader(const std::string& payload);
 
+// Generates the the string to be signed over and included in the authorization
+// header. See
+// https://datatracker.ietf.org/doc/html/draft-cavage-http-signatures-08#section-2.3:w
 std::pair<std::string, std::string> CreateSignatureString(
     base::flat_map<std::string, std::string> headers,
     const GURL& url,
     const std::string& method,
     const std::vector<std::string>& headers_to_sign);
 
+// Generates an authorization header field and value pair using the provided
+// service key to sign over specified headers.
 std::optional<std::pair<std::string, std::string>> GetAuthorizationHeader(
     const std::string& service_key,
     const base::flat_map<std::string, std::string>& headers,
