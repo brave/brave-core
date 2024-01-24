449f27b53276e0b216c01f2bff486fcb751167ac
brave-builds <brave-builds+devops@brave.com>
2024-01-24 01:49:09 +0000

    Uplift of #21361 (squashed) to beta

---
+++ chromium_src/chrome/browser/ui/webui/side_panel/bookmarks/bookmarks_side_panel_ui.h
@@ -0,0 +1,21 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_WEBUI_SIDE_PANEL_BOOKMARKS_BOOKMARKS_SIDE_PANEL_UI_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_WEBUI_SIDE_PANEL_BOOKMARKS_BOOKMARKS_SIDE_PANEL_UI_H_
+
+#define BookmarksSidePanelUI BookmarksSidePanelUI_ChromiumImpl
+
+#include "src/chrome/browser/ui/webui/side_panel/bookmarks/bookmarks_side_panel_ui.h"  // IWYU pragma: export
+
+#undef BookmarksSidePanelUI
+
+class BookmarksSidePanelUI : public BookmarksSidePanelUI_ChromiumImpl {
+ public:
+  explicit BookmarksSidePanelUI(content::WebUI* web_ui);
+  ~BookmarksSidePanelUI() override;
+};
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_WEBUI_SIDE_PANEL_BOOKMARKS_BOOKMARKS_SIDE_PANEL_UI_H_
