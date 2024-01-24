cb3fc150260592ba30c50c27587d5691c51f4447
Josh Leonard <30185185+josheleonard@users.noreply.github.com>
2024-01-24 15:08:06 +0000

    chore: add ETH Sign Typed Data story to storybook

---
+++ components/brave_wallet_ui/components/extension/sign-panel/common/eth_sign_typed_data.stories.tsx
@@ -0,0 +1,26 @@
+// Copyright (c) 2024 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import * as React from 'react'
+
+// components
+import { EthSignTypedData } from './eth_sign_typed_data'
+
+// mocks
+import {
+  mockSignMessageRequest //
+} from '../../../../stories/mock-data/mock-eth-requests'
+
+export function _EthSignTypedData() {
+  return (
+    <EthSignTypedData
+      data={mockSignMessageRequest.signData.ethSignTypedData}
+      width={'100%'}
+      height={'100%'}
+    />
+  )
+}
+
+export default _EthSignTypedData
