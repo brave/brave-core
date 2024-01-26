6169510f0bb49c66134e7428a53c85b1be4f7823
Brandon-T <bthomas@brave.com>
2024-01-26 12:49:46 -0500

    Fix #35593: Remove browser policy connector override for iOS (#21758)
    - Remove browser policy connector override on iOS. 
    - Desktop doesn't have it, and having it null will prevent variations and other services from working.

--- ios/browser/application_context/brave_application_context_impl.h
+++ ios/browser/application_context/brave_application_context_impl.h
@@ -36,7 +36,6 @@ class BraveApplicationContextImpl : public ApplicationContextImpl {
 
   // ApplicationContextImpl
   ukm::UkmRecorder* GetUkmRecorder() override;
-  BrowserPolicyConnectorIOS* GetBrowserPolicyConnector() override;
   gcm::GCMDriver* GetGCMDriver() override;
 
   // BraveApplicationContextImpl
