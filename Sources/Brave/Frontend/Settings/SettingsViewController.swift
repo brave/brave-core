1e116072330c3d3633650e2e5d093f4d6e82aa62
Brandon-T <bthomas@brave.com>
2024-01-26 12:49:00 -0500

    Fix brave/brave-ios#8705: Add display for version info (brave/brave-ios#8706)
    - Add display for version info

--- Sources/Brave/Frontend/Settings/SettingsViewController.swift
+++ Sources/Brave/Frontend/Settings/SettingsViewController.swift
@@ -744,9 +744,20 @@ class SettingsViewController: TableViewController {
               UIPasteboard.general.setSecureString(AppStorageDebugComposer.compose(),
                                                    expirationDate: Date().addingTimeInterval(2.minutes))
             }
+            
+            let viewMoreDetails = UIAlertAction(title: Strings.viewAllVersionInfo, style: .default) { [unowned self, weak actionSheet] _ in
+              let versionController = ChromeWebViewController(privateBrowsing: false).then {
+                $0.loadURL("brave://version/?show-variations-cmd")
+              }
+              
+              actionSheet?.dismiss(animated: true, completion: {
+                self.present(versionController, animated: true)
+              })
+            }
 
             actionSheet.addAction(copyDebugInfoAction)
             actionSheet.addAction(copyAppInfoAction)
+            actionSheet.addAction(viewMoreDetails)
             actionSheet.addAction(UIAlertAction(title: Strings.cancelButtonTitle, style: .cancel, handler: nil))
             self.navigationController?.present(actionSheet, animated: true, completion: nil)
           }, cellClass: MultilineValue1Cell.self),
