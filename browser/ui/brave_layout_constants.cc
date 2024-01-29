bcdd114f548197347afb5fd2e43c909360929116
Simon Hong <shong@brave.com>
2024-01-30 06:12:12 +0900

    Omnibox changes (location bar height, inkdrop color, icon changes) (uplift to 1.62.x) (#21761)
    * Merge pull request #21722 from brave/update_http_icon
    
    Updated http icon
    
    * Updated omnibox chip button's background color in light mode (#21637)
    
    fix https://github.com/brave/brave-browser/issues/35358
    
    * Merge pull request #21721 from brave/adjust_location_bar_height
    
    Use 32px for location bar height
    
    * Merge pull request #21729 from brave/brave_actions_highlight
    
    Fixed to have proper inkdrop size for browser actions

--- browser/ui/brave_layout_constants.cc
+++ browser/ui/brave_layout_constants.cc
@@ -37,9 +37,10 @@ absl::optional<int> GetBraveLayoutConstant(LayoutConstant constant) {
       return 24;
     }
     case LOCATION_BAR_HEIGHT:
-      return touch ? 36 : 34;
+      // Consider adjust below element padding also when this height is changed.
+      return 32;
     case LOCATION_BAR_ELEMENT_PADDING:
-      return 4;
+      return 2;
     default:
       break;
   }
