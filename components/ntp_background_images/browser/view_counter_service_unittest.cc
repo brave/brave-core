d08cef0d4b6a8ab7d0331af5ae05b564f604e0f1
brave-builds <brave-builds+devops@brave.com>
2024-01-29 17:57:43 +0000

    Uplift of #21751 (squashed) to beta

--- components/ntp_background_images/browser/view_counter_service_unittest.cc
+++ components/ntp_background_images/browser/view_counter_service_unittest.cc
@@ -232,7 +232,7 @@ class NTPBackgroundImagesViewCounterTest : public testing::Test {
   }
 
   int GetInitialCountToBrandedWallpaper() const {
-    return features::kInitialCountToBrandedWallpaper.Get();
+    return features::kInitialCountToBrandedWallpaper.Get() - 1;
   }
 
   std::optional<base::Value::Dict> TryGetFirstSponsoredImageWallpaper() {
@@ -360,7 +360,7 @@ TEST_F(NTPBackgroundImagesViewCounterTest, IsActiveOptedIn) {
 TEST_F(NTPBackgroundImagesViewCounterTest, PrefsWithModelTest) {
   auto& model = view_counter_->model_;
 
-  EXPECT_EQ(features::kInitialCountToBrandedWallpaper.Get(),
+  EXPECT_EQ(features::kInitialCountToBrandedWallpaper.Get() - 1,
             model.show_branded_wallpaper_);
   EXPECT_TRUE(model.show_wallpaper_);
   EXPECT_TRUE(model.show_branded_wallpaper_);
