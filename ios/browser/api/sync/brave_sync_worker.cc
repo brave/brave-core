121ba4f7de1a6b7f8a78d152087886fc930f0af6
AlexeyBarabash <alexey@brave.com>
2024-01-31 23:07:33 +0200

    Uplift of #21767 (squashed) to release

--- ios/browser/api/sync/brave_sync_worker.cc
+++ ios/browser/api/sync/brave_sync_worker.cc
@@ -313,6 +313,8 @@ void BraveSyncWorker::ResetSync() {
     return;
   }
 
+  sync_service->prefs().AddLeaveChainDetail(__FILE__, __LINE__, __func__);
+
   auto* device_info_service =
       DeviceInfoSyncServiceFactory::GetForBrowserState(browser_state_);
   DCHECK(device_info_service);
@@ -357,6 +359,8 @@ void BraveSyncWorker::PermanentlyDeleteAccount(
   if (!sync_service) {
     return;
   }
+
+  sync_service->prefs().AddLeaveChainDetail(__FILE__, __LINE__, __func__);
 
   sync_service->PermanentlyDeleteAccount(std::move(callback));
 }
