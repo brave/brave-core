1363474d0442212ec418b922ac293d679e0dc6df
Emerick Rogul <erogul@brave.com>
2024-01-30 22:53:57 -0500

    Upgrade from Chromium 121.0.6167.85 to Chromium 121.0.6167.139 (1.63.x) (#21825)
    * Upgrade from Chromium 121.0.6167.85 to Chromium 121.0.6167.139
    
    * Update pins list timestamp
    
    * Disable upstream test that's failing due to DIPS being disabled
    
    ---------
    
    Co-authored-by: brave-builds <brave-builds+devops@brave.com>

--- chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
+++ chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
@@ -549,9 +549,9 @@ bool ParseCertificatesFile(std::string_view certs_input,
                            Pinsets* pinsets,
                            base::Time* timestamp) {
   constexpr std::string_view brave_certs = R"brave_certs(
-# Last updated: Mon Jan 22 18:34:52 UTC 2024
+# Last updated: Tue Jan 30 21:01:29 UTC 2024
 PinsListTimestamp
-1705948492
+1706648489
 
 # =====BEGIN BRAVE ROOTS ASC=====
 #From https://www.amazontrust.com/repository/
