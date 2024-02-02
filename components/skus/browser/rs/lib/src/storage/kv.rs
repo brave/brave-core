e466cad0f1defd379beb43c0ecb79e07962d02d7
brave-builds <brave-builds+devops@brave.com>
2024-02-02 03:54:49 +0000

    Uplift of #21847 (squashed) to release

--- components/skus/browser/rs/lib/src/storage/kv.rs
+++ components/skus/browser/rs/lib/src/storage/kv.rs
@@ -302,6 +302,7 @@ where
                 // don't overwrite the existing unblinded_creds, we just want to add the new blinded
                 // tokens to the item_credentials.creds for signing request
                 if let Credentials::TimeLimitedV2(item_credentials) = item_credentials {
+                    item_credentials.request_id = Some(request_id.to_string());
                     item_credentials.creds = creds;
                     // update state to generated credentials
                     item_credentials.state = CredentialState::GeneratedCredentials;
