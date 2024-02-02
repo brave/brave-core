c8649304e2dc5f1671a488d7b0624bd568f86eb7
Artem Samoilenko <artem@brave.com>
2024-02-02 13:54:58 -0500

    [Android] Minor code adjustments to the C123 upstream changes

--- android/java/org/chromium/chrome/browser/signin/BraveSigninManager.java
+++ android/java/org/chromium/chrome/browser/signin/BraveSigninManager.java
@@ -5,14 +5,15 @@
 
 package org.chromium.chrome.browser.signin;
 
-import android.accounts.Account;
 
 import androidx.annotation.MainThread;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import org.jni_zero.CalledByNative;
 
 import org.chromium.base.Callback;
+import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.signin.services.SigninManager;
 import org.chromium.components.signin.base.CoreAccountId;
 import org.chromium.components.signin.base.CoreAccountInfo;
@@ -63,17 +64,9 @@ public class BraveSigninManager implements SigninManager {
     public void runAfterOperationInProgress(Runnable runnable) {}
 
     @Override
-    public void signinAndEnableSync(Account account, @SigninAccessPoint int accessPoint,
-            @Nullable SignInCallback callback) {}
-
-    @Override
     public void signinAndEnableSync(
             CoreAccountInfo coreAccountInfo,
             @SigninAccessPoint int accessPoint,
-            @Nullable SignInCallback callback) {}
-
-    @Override
-    public void signin(Account account, @SigninAccessPoint int accessPoint,
             @Nullable SignInCallback callback) {}
 
     @Override
@@ -106,9 +99,13 @@ public class BraveSigninManager implements SigninManager {
     public void reloadAllAccountsFromSystem(@Nullable CoreAccountId primaryAccountId) {}
 
     @CalledByNative
-    static SigninManager create(long nativeSigninManagerAndroid,
-            AccountTrackerService accountTrackerService, IdentityManager identityManager,
-            IdentityMutator identityMutator, SyncService syncService) {
+    static SigninManager create(
+            long nativeSigninManagerAndroid,
+            Profile profile,
+            AccountTrackerService accountTrackerService,
+            IdentityManager identityManager,
+            IdentityMutator identityMutator,
+            SyncService syncService) {
         AccountInfoServiceProvider.init(identityManager, accountTrackerService);
         return new BraveSigninManager(identityManager);
     }
@@ -135,4 +132,16 @@ public class BraveSigninManager implements SigninManager {
             CoreAccountInfo coreAccountInfo,
             @SigninAccessPoint int accessPoint,
             @Nullable SignInCallback callback) {}
+
+    @Override
+    public boolean getUserAcceptedAccountManagement() {
+        return false;
+    }
+
+    @Override
+    public void setUserAcceptedAccountManagement(boolean acceptedAccountManagement) {}
+
+    @Override
+    public void isAccountManaged(
+            @NonNull CoreAccountInfo account, final Callback<Boolean> callback) {}
 }
