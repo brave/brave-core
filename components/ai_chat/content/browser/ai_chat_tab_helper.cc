90df2add63a548c5b0f51084258eaa9335e05e87
Anthony Tseng <darkdh@gmail.com>
2024-02-05 21:18:09 -0800

    Extract PDF content from browser process (uplift to 1.63.x) (#21912)
    Extract PDF content from browser process (#21570)
    
    * Extract PDF content from browser process
    
    Since a11y tree info is guaranteed to be synced between renderer and
    browser, we toggle on basic a11y mode for PDF process when embedded
    pdf webcontents is attached to main one. And then we wait for "Finished
    loading PDF" event so we can fetch PDF content from browser a11y tree.
    
    * Enforce enable_pdf guard for IDS_PDF_LOADED_TO_A11Y_TREE
    
    * Rebase onto PR #21408
    
    * Wait for OnPDFA11yInfoLoaded before proceeding FetchPageContent for PDF content

--- components/ai_chat/content/browser/ai_chat_tab_helper.cc
+++ components/ai_chat/content/browser/ai_chat_tab_helper.cc
@@ -23,13 +23,38 @@
 #include "components/favicon/content/content_favicon_driver.h"
 #include "components/grit/brave_components_strings.h"
 #include "components/prefs/pref_service.h"
+#include "components/strings/grit/components_strings.h"
 #include "components/user_prefs/user_prefs.h"
+#include "content/public/browser/ax_event_notification_details.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/storage_partition.h"
+#include "pdf/buildflags.h"
 #include "ui/base/l10n/l10n_util.h"
 
 namespace ai_chat {
+
+AIChatTabHelper::PDFA11yInfoLoadObserver::PDFA11yInfoLoadObserver(
+    content::WebContents* web_contents,
+    AIChatTabHelper* helper)
+    : content::WebContentsObserver(web_contents), helper_(helper) {}
+
+void AIChatTabHelper::PDFA11yInfoLoadObserver::AccessibilityEventReceived(
+    const content::AXEventNotificationDetails& details) {
+#if BUILDFLAG(ENABLE_PDF)
+  for (const auto& update : details.updates) {
+    for (const auto& node : update.nodes) {
+      if (node.GetStringAttribute(ax::mojom::StringAttribute::kName) ==
+          l10n_util::GetStringUTF8(IDS_PDF_LOADED_TO_A11Y_TREE)) {
+        helper_->OnPDFA11yInfoLoaded();
+        break;
+      }
+    }
+  }
+#endif
+}
+
+AIChatTabHelper::PDFA11yInfoLoadObserver::~PDFA11yInfoLoadObserver() = default;
 
 AIChatTabHelper::AIChatTabHelper(
     content::WebContents* web_contents,
@@ -48,12 +73,32 @@ AIChatTabHelper::AIChatTabHelper(
           web_contents->GetBrowserContext()
               ->GetDefaultStoragePartition()
               ->GetURLLoaderFactoryForBrowserProcess(),
-          channel_name) {
+          channel_name),
+      pdf_load_observer_(
+          std::make_unique<PDFA11yInfoLoadObserver>(nullptr, this)) {
   favicon::ContentFaviconDriver::FromWebContents(web_contents)
       ->AddObserver(this);
 }
 
 AIChatTabHelper::~AIChatTabHelper() = default;
+
+void AIChatTabHelper::SetOnPDFA11yInfoLoadedCallbackForTesting(
+    base::OnceClosure cb) {
+  on_pdf_a11y_info_loaded_cb_ = std::move(cb);
+}
+
+void AIChatTabHelper::OnPDFA11yInfoLoaded() {
+  DVLOG(3) << "PDF Loaded";
+  is_pdf_a11y_info_loaded_ = true;
+  if (pending_get_page_content_callback_) {
+    FetchPageContent(web_contents(), "",
+                     std::move(pending_get_page_content_callback_));
+  }
+  pdf_load_observer_.reset();
+  if (on_pdf_a11y_info_loaded_cb_) {
+    std::move(on_pdf_a11y_info_loaded_cb_).Run();
+  }
+}
 
 // content::WebContentsObserver
 
@@ -99,6 +144,24 @@ void AIChatTabHelper::TitleWasSet(content::NavigationEntry* entry) {
   }
 }
 
+void AIChatTabHelper::InnerWebContentsAttached(
+    content::WebContents* inner_web_contents,
+    content::RenderFrameHost* render_frame_host,
+    bool is_full_page) {
+  // Setting a11y mode for PDF process which is dedicated for each
+  // PDF so we don't have to unset it.
+  if (content::WebContents::FromRenderFrameHost(render_frame_host)
+              ->GetContentsMimeType() == "application/pdf" &&
+      HasUserOptedIn()) {
+    // We need `AXMode::kNativeAPIs` for accessing pdf a11y info and
+    // `AXMode::kWebContents` for observing a11y events from WebContents.
+    inner_web_contents->SetAccessibilityMode(ui::AXMode(ui::kAXModeBasic));
+    pdf_load_observer_ =
+        std::make_unique<PDFA11yInfoLoadObserver>(inner_web_contents, this);
+    is_pdf_a11y_info_loaded_ = false;
+  }
+}
+
 // favicon::FaviconDriverObserver
 
 void AIChatTabHelper::OnFaviconUpdated(
@@ -116,10 +179,18 @@ GURL AIChatTabHelper::GetPageURL() const {
   return web_contents()->GetLastCommittedURL();
 }
 
-void AIChatTabHelper::GetPageContent(
-    GetPageContentCallback callback,
-    std::string_view invalidation_token) const {
-  FetchPageContent(web_contents(), invalidation_token, std::move(callback));
+void AIChatTabHelper::GetPageContent(GetPageContentCallback callback,
+                                     std::string_view invalidation_token) {
+  if (web_contents()->GetContentsMimeType() == "application/pdf" &&
+      !is_pdf_a11y_info_loaded_) {
+    if (pending_get_page_content_callback_) {
+      std::move(pending_get_page_content_callback_).Run("", false, "");
+    }
+    // invalidation_token doesn't matter for PDF extraction.
+    pending_get_page_content_callback_ = std::move(callback);
+  } else {
+    FetchPageContent(web_contents(), invalidation_token, std::move(callback));
+  }
 }
 
 std::u16string AIChatTabHelper::GetPageTitle() const {
