2ecad00696dc2fb7126fee2a94b5f7f3aa0a1b03
Emerick Rogul <erogul@brave.com>
2024-02-06 23:33:35 -0500

    Upgrade from Chromium 121.0.6167.139 to Chromium 121.0.6167.164 (1.62.x) (#21928)
    * Upgrade from Chromium 121.0.6167.139 to Chromium 121.0.6167.164
    
    * Update pins list timestamp
    
    * Updated strings for Chromium 121.0.6167.164
    
    * Disable flakey upstream tests
    
    ---------
    
    Co-authored-by: brave-builds <brave-builds+devops@brave.com>

--- chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
+++ chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
@@ -549,9 +549,9 @@ bool ParseCertificatesFile(std::string_view certs_input,
                            Pinsets* pinsets,
                            base::Time* timestamp) {
   constexpr std::string_view brave_certs = R"brave_certs(
-# Last updated: Tue Jan 30 20:48:50 UTC 2024
+# Last updated: Tue Feb  6 22:49:48 UTC 2024
 PinsListTimestamp
-1706647730
+1707259788
 
 # =====BEGIN BRAVE ROOTS ASC=====
 #From https://www.amazontrust.com/repository/
