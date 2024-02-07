5715f29b10601ab7f0eab03ca769f2e6af2e4e5f
Darnell Andries <dandries@brave.com>
2024-02-07 11:44:42 -0800

    Disable flaky P3A test on macOS

--- components/p3a/p3a_service_unittest.cc
+++ components/p3a/p3a_service_unittest.cc
@@ -275,7 +275,13 @@ TEST_F(P3AServiceTest, UpdateLogsAndSendExpress) {
   EXPECT_EQ(p3a_creative_sent_metrics_.size(), 0U);
 }
 
-TEST_F(P3AServiceTest, UpdateLogsAndSendSlow) {
+#if !BUILDFLAG(IS_MAC)
+#define MAYBE_UpdateLogsAndSendSlow UpdateLogsAndSendSlow
+#else
+#define MAYBE_UpdateLogsAndSendSlow DISABLED_UpdateLogsAndSendSlow
+#endif
+
+TEST_F(P3AServiceTest, MAYBE_UpdateLogsAndSendSlow) {
   // Increase upload interval to reduce test time (less tasks to execute)
   config_.average_upload_interval = base::Seconds(6000);
   SetUpP3AService();
