44a4bec0992a7a9f102594ac625027da3ea14041
Claudio DeSouza <cdesouza@brave.com>
2024-02-09 04:44:09 +0000

    `AvatarToolbarButton` overrides moved into the delegate
    Some decision making function that used to be in `AvatarToolbarButton`
    are now part of `AvatarToolbarButtonDelegate`. This change transposes
    the overrides to the new location, while making sure the previous
    existing operations are kept as they were.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/ea3e09ceb613dc335da634903cfb74f8a9ae9080
    
    commit ea3e09ceb613dc335da634903cfb74f8a9ae9080
    Author: Ryan Sultanem <rsult@google.com>
    Date:   Tue Feb 6 11:42:25 2024 +0000
    
        [AvatarButton] Encapuslate decision making into the delegate
    
        The goal is to reduce the back and forth between the button and the
        delegate, which creates a complex stack.
        The button primarily takes care of the UI modifications, but gets
        it's dynamic information from the delegate which is aware of Chrome
        and Profile modifications.
        This CL encapsulates those decisions more within the delegate to get a
        clearer flow, it only reorders some calls, no behavior change is
        expected
    
        Potential future changes:
        - Further decrease the coupling by having the delegate notify
        the exact changes to be applied, instead of requesting the info
        after calling `UpdateText()` again.
        - Try to simplify the `Identity animation` flow.
        - Cleanup the intercept text showing; make it more generic as a way
        to enfornce a specific text controlled by some external
        structure/logic. (next CL)
    
        Bug: b:324018028

--- browser/ui/views/profiles/brave_avatar_toolbar_button_delegate.cc
+++ browser/ui/views/profiles/brave_avatar_toolbar_button_delegate.cc
@@ -5,11 +5,56 @@
 
 #include "brave/browser/ui/views/profiles/brave_avatar_toolbar_button_delegate.h"
 
+#include "brave/app/vector_icons/vector_icons.h"
+#include "brave/browser/ui/color/color_palette.h"
+#include "brave/components/l10n/common/localization_util.h"
+#include "brave/components/vector_icons/vector_icons.h"
+#include "brave/grit/brave_generated_resources.h"
+#include "chrome/app/vector_icons/vector_icons.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/layout_constants.h"
+#include "ui/base/models/image_model.h"
+#include "ui/color/color_provider.h"
+
 BraveAvatarToolbarButtonDelegate::BraveAvatarToolbarButtonDelegate(
     AvatarToolbarButton* button,
     Browser* browser)
-    : AvatarToolbarButtonDelegate(button, browser) {}
+    : AvatarToolbarButtonDelegate(button, browser), browser_(browser) {}
 
 gfx::Image BraveAvatarToolbarButtonDelegate::GetGaiaAccountImage() const {
   return gfx::Image();
+}
+
+ui::ImageModel BraveAvatarToolbarButtonDelegate::GetAvatarIcon(
+    int icon_size,
+    SkColor icon_color) const {
+  const auto location_bar_icon_size = GetLayoutConstant(LOCATION_BAR_ICON_SIZE);
+  if (browser_->profile()->IsTor()) {
+    return ui::ImageModel::FromVectorIcon(kLeoProductTorIcon,
+                                          SkColorSetRGB(0x3C, 0x82, 0x3C),
+                                          location_bar_icon_size);
+  }
+
+  if (browser_->profile()->IsIncognitoProfile()) {
+    return ui::ImageModel::FromVectorIcon(
+        kIncognitoIcon, SkColorSetRGB(0xFF, 0xFF, 0xFF), icon_size);
+  }
+
+  if (browser_->profile()->IsGuestSession()) {
+    return ui::ImageModel::FromVectorIcon(kUserMenuGuestIcon, icon_color,
+                                          location_bar_icon_size);
+  }
+
+  return AvatarToolbarButtonDelegate::GetAvatarIcon(location_bar_icon_size,
+                                                    icon_color);
+}
+
+std::u16string BraveAvatarToolbarButtonDelegate::GetAvatarTooltipText() const {
+  if (browser_->profile()->IsTor()) {
+    return brave_l10n::GetLocalizedResourceUTF16String(
+        IDS_TOR_AVATAR_BUTTON_TOOLTIP_TEXT);
+  }
+
+  return AvatarToolbarButtonDelegate::GetAvatarTooltipText();
 }
---
+++ chromium_src/chrome/browser/ui/views/profiles/avatar_toolbar_button_delegate.h
@@ -0,0 +1,25 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_PROFILES_AVATAR_TOOLBAR_BUTTON_DELEGATE_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_PROFILES_AVATAR_TOOLBAR_BUTTON_DELEGATE_H_
+
+#include "chrome/browser/profiles/profile_attributes_entry.h"
+#include "chrome/browser/profiles/profile_attributes_storage.h"
+
+#define GetAvatarIcon virtual GetAvatarIcon
+#define GetAvatarTooltipText virtual GetAvatarTooltipText
+// Adding friend class to `BraveAvatarToolbarButton` to still allow its instance
+// to call `GetWindowCount`.
+#define OnUserIdentityChanged            \
+  OnUserIdentityChangedUnused() {}       \
+  friend class BraveAvatarToolbarButton; \
+  void OnUserIdentityChanged
+#include "src/chrome/browser/ui/views/profiles/avatar_toolbar_button_delegate.h"  // IWYU pragma: export
+#undef GetAvatarIcon
+#undef GetAvatarTooltipText
+#undef OnUserIdentityChanged
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_PROFILES_AVATAR_TOOLBAR_BUTTON_DELEGATE_H_
