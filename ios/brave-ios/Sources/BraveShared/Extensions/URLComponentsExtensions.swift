717982b3f3da1ca334274ebfabf63a01e473a56a
Mihai PLESA <mplesa@brave.com>
2024-02-12 18:17:42 +0000

    Revert "Add `brave-ios` repo & git history" (#22003)
    Revert "Add `brave-ios` repo & git history (#21961)"
    
    This reverts commit 507e227b00806bde52ffb6c0750674721bb20844.

--- ios/brave-ios/Sources/BraveShared/Extensions/URLComponentsExtensions.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-extension URLComponents {
-  // Return the first query parameter that matches
-  public func valueForQuery(_ param: String) -> String? {
-    return self.queryItems?.first { $0.name == param }?.value
-  }
-}
