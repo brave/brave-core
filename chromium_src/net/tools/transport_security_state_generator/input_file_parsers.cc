0640cb8200a05f4464835fd26ccfc38bd8ba2d7b
brave-builds <brave-builds+devops@brave.com>
2024-02-13 20:17:06 +0000

    Update pins list timestamp

--- chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
+++ chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
@@ -549,9 +549,9 @@ bool ParseCertificatesFile(std::string_view certs_input,
                            Pinsets* pinsets,
                            base::Time* timestamp) {
   constexpr std::string_view brave_certs = R"brave_certs(
-# Last updated: Tue Feb  6 23:03:33 UTC 2024
+# Last updated: Tue Feb 13 20:17:04 UTC 2024
 PinsListTimestamp
-1707260613
+1707855424
 
 # =====BEGIN BRAVE ROOTS ASC=====
 #From https://www.amazontrust.com/repository/
