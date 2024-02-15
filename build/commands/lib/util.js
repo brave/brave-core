89209bf2ada50a0476c01d31e1cf95c48fd29f79
Kyle Hickinson <git@kylehickinson.com>
2024-02-15 12:33:05 -0500

    [iOS] Enable swift-format in presubmit/format commands

--- build/commands/lib/util.js
+++ build/commands/lib/util.js
@@ -735,8 +735,6 @@ const util = {
       args.push('--no-rust-fmt')
     if (options.swift)
       args.push('--swift-format')
-    else
-      args.push('--no-swift-format')
     util.run(cmd, args, cmd_options)
   },
 
