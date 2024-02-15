86b1ecf84e5799d4b0b67f3132c02f455ad0852d
Kyle Hickinson <git@kylehickinson.com>
2024-02-15 15:02:45 -0500

    [iOS] Bring Static dep back into SPM
    The repo now has its own `Package.swift` manifest so it no longer needs to be vendored in

--- ios/brave-ios/Package.swift
+++ ios/brave-ios/Package.swift
@@ -61,7 +61,7 @@ var package = Package(
     .package(url: "https://github.com/mkrd/Swift-BigInt", from: "2.0.0"),
     .package(url: "https://github.com/GuardianFirewall/GuardianConnect", exact: "1.8.5"),
     .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "0.6.0"),
-    .package(name: "Static", path: "../../third_party/ios_deps/Static"),
+    .package(url: "https://github.com/venmo/Static", revision: "622a6804d39515600ead16e6259cb5d5e50f40df"),
   ],
   targets: [
     .target(
