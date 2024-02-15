5094ff954c61094b622963bfc7550a95c94b3fb8
Emerick Rogul <erogul@brave.com>
2024-02-15 17:58:35 -0500

    base::MD5Sum takes a base::span argument now
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/8b0e8c0c1bbeb66ca9e7924fa686520a23974d72
    
    commit 8b0e8c0c1bbeb66ca9e7924fa686520a23974d72
    Author: Lukasz Anforowicz <lukasza@chromium.org>
    Date:   Fri Feb 9 15:26:36 2024 +0000
    
        Delete `base::MD5Sum` overload taking separate `data` and `size` params.
    
        Going forward the overload taking `base::span` should be used instead.
    
        Bug: 40284755

--- browser/default_protocol_handler_utils_win.cc
+++ browser/default_protocol_handler_utils_win.cc
@@ -52,7 +52,7 @@ inline DWORD WordSwap(DWORD v) {
 std::wstring HashString(std::wstring_view input_string) {
   auto* input_bytes =
       reinterpret_cast<const unsigned char*>(input_string.data());
-  const int input_byte_count = (input_string.length() + 1) * sizeof(wchar_t);
+  const size_t input_byte_count = (input_string.length() + 1) * sizeof(wchar_t);
 
   constexpr size_t kDWordsPerBlock = 2;
   constexpr size_t kBlockSize = sizeof(DWORD) * kDWordsPerBlock;
@@ -66,7 +66,7 @@ std::wstring HashString(std::wstring_view input_string) {
   // Compute an MD5 hash. md5[0] and md5[1] will be used as constant multipliers
   // in the scramble below.
   base::MD5Digest digest;
-  base::MD5Sum(input_bytes, input_byte_count, &digest);
+  base::MD5Sum(base::span(input_bytes, input_byte_count), &digest);
   auto* md5 = reinterpret_cast<DWORD*>(digest.a);
   // The following loop effectively computes two checksums, scrambled like a
   // hash after every DWORD is added.
