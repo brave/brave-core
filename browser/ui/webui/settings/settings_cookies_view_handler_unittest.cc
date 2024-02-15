58370df69b323de7f1d7ced300c70b11fd209fd7
Claudio DeSouza <cdesouza@brave.com>
2024-02-15 03:19:59 +0000

    `LocalDataContainer` not taking a indexed db helper
    This argument has been removed from the constructor list, however none
    was being passed on the single use of it in a test.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/80ef91cb4c22932bf0045ad34cb12466502fe720
    
    commit 80ef91cb4c22932bf0045ad34cb12466502fe720
    Author: Mariam Ali <alimariam@google.com>
    Date:   Wed Feb 7 12:10:38 2024 +0000
    
        [Deprecate CookiesTreeModel] Remove unused IndexedDBHelper
    
        Bug: b:40205603, b:277057324

--- browser/ui/webui/settings/settings_cookies_view_handler_unittest.cc
+++ browser/ui/webui/settings/settings_cookies_view_handler_unittest.cc
@@ -69,7 +69,6 @@ class CookiesViewHandlerTest : public ChromeRenderViewHostTestHarness {
         mock_browsing_data_cookie_helper_,
         /*database_helper=*/nullptr, mock_browsing_data_local_storage_helper_,
         /*session_storage_helper=*/nullptr,
-        /*indexed_db_helper=*/nullptr,
         /*file_system_helper=*/nullptr,
         /*quota_helper=*/nullptr,
         /*service_worker_helper=*/nullptr,
