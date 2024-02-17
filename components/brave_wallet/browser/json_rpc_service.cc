db713170b866234226d49408bb0ec5e9acb38e4c
brave-builds <45370463+brave-builds@users.noreply.github.com>
2024-02-17 06:17:39 +0100

    Add more UD domains (uplift to 1.63.x) (#22125)
    Uplift of #22102 (squashed) to release

--- components/brave_wallet/browser/json_rpc_service.cc
+++ components/brave_wallet/browser/json_rpc_service.cc
@@ -80,7 +80,8 @@ constexpr char kSnsDomainPattern[] = R"(^(?:[a-z0-9-]+\.)+sol$)";
 // Then one of fixed suffixes(should match `supportedUDExtensions` array from
 // domain-extensions.ts).
 constexpr char kUDPattern[] =
-    "(?:[a-z0-9-]+)\\.(?:crypto|x|nft|dao|wallet|blockchain|bitcoin|zil)";
+    "(?:[a-z0-9-]+)\\.(?:crypto|x|nft|dao|wallet|blockchain|bitcoin|zil|"
+    "altimist|anime|klever|manga|polygon|unstoppable|pudgy)";
 
 net::NetworkTrafficAnnotationTag GetNetworkTrafficAnnotationTag() {
   return net::DefineNetworkTrafficAnnotation("json_rpc_service", R"(
