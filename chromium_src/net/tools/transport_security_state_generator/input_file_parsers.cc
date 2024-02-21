c426a9da4d4113ceade852b6993947a0ce817289
Emerick Rogul <erogul@brave.com>
2024-02-21 01:04:34 -0500

    Upgrade from Chromium 122.0.6261.43 to Chromium 122.0.6261.57 (1.64.x) (#22193)
    * Upgrade from Chromium 122.0.6261.43 to Chromium 122.0.6261.57
    
    * Update pins list timestamp
    
    ---------
    
    Co-authored-by: brave-builds <brave-builds+devops@brave.com>

--- chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
+++ chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
@@ -549,9 +549,9 @@ bool ParseCertificatesFile(std::string_view certs_input,
                            Pinsets* pinsets,
                            base::Time* timestamp) {
   constexpr std::string_view brave_certs = R"brave_certs(
-# Last updated: Wed Feb 14 20:16:02 UTC 2024
+# Last updated: Wed Feb 21 01:25:37 UTC 2024
 PinsListTimestamp
-1707941762
+1708478737
 
 # =====BEGIN BRAVE ROOTS ASC=====
 #From https://www.amazontrust.com/repository/
