ab6c5e9a25460bc1fd333291b323adbe8d7f4b91
Emerick Rogul <erogul@brave.com>
2024-02-21 15:37:53 -0500

    Affiliations was moved into its own component
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/085fcf5f9b49f64700c0651ba7ca61b68a03c39b
    
    commit 085fcf5f9b49f64700c0651ba7ca61b68a03c39b
    Author: Jan Keitel <jkeitel@google.com>
    Date:   Thu Feb 15 09:12:36 2024 +0000
    
        Move affiliations into its own component.
    
        The motivation for this CL is as follows:
        Currently, password manager is the only component using affiliations.
        Soon, this will change - plus_addresses will do the same. Since there
        is no natural reason for plus_addresses to depend on password_manager,
        this CL creates its own separate component for the affiliation service.
    
        Despite its size, this CL does very little:
        - It moves //components/password_manager/core/browser/affiliation
          into its own components with the exception of AffiliationPrefetcher
          and AffiliatedMatcherHelper which contain PWM-specific logic. It also
          moves the SQLTableBuilder helper there.
        - It adjusts build files, includes, and namespaces accordingly.
        - It adds DEPS, metadata and OWNERs information to the new component.
        - It adds a README.
    
        Further mechanical follow-up CLs will:
        - Also move the factories in the embedder into their own /affiliations
          subdirectory.
        - Move affiliations-related constants (AFAICT there is only one) into
          their own constants-file.
        - Potentially move affiliations-related histograms into their own
          histograms file.
    
        There will be further work required to actually make affiliations
        usable for plus addresses (e.g., adjust affiliations prefetching,
        introduce affiliations matching logic specific to plus_addresses, etc.)
    
        Note: Skipping presubmits because the DEPS check does not understand
        earlier +1s that got invalidated due to the addition of a single file.
    
        Bug: b/324553078

--- chromium_src/components/affiliations/core/browser/affiliation_fetcher_factory_impl.cc
+++ chromium_src/components/affiliations/core/browser/affiliation_fetcher_factory_impl.cc
@@ -3,21 +3,21 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "components/password_manager/core/browser/affiliation/hash_affiliation_fetcher.h"
+#include "components/affiliations/core/browser/hash_affiliation_fetcher.h"
 
 namespace {
 
 class BraveHashAffiliationFetcher
-    : public password_manager::HashAffiliationFetcher {
+    : public affiliations::HashAffiliationFetcher {
  public:
   using HashAffiliationFetcher::HashAffiliationFetcher;
 
-  void StartRequest(const std::vector<password_manager::FacetURI>& facet_uris,
+  void StartRequest(const std::vector<affiliations::FacetURI>& facet_uris,
                     RequestInfo request_info) override {}
 };
 
 }  // namespace
 
 #define HashAffiliationFetcher BraveHashAffiliationFetcher
-#include "src/components/password_manager/core/browser/affiliation/affiliation_fetcher_factory_impl.cc"
+#include "src/components/affiliations/core/browser/affiliation_fetcher_factory_impl.cc"
 #undef HashAffiliationFetcher
