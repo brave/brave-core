5eb18e9b3b73f3ceabbbfd19d20146ee60ba4c1e
mschfh <37435502+mschfh@users.noreply.github.com>
2024-02-22 04:43:46 -0500

    Migrate hermetic toolchains

--- build/commands/lib/config.js
+++ build/commands/lib/config.js
@@ -1243,7 +1243,7 @@ Object.defineProperty(Config.prototype, 'defaultOptions', {
       env.USE_BRAVE_HERMETIC_TOOLCHAIN = '1'
       env.DEPOT_TOOLS_WIN_TOOLCHAIN = '1'
       env.GYP_MSVS_HASH_27370823e7 = '01b3b59461'
-      env.DEPOT_TOOLS_WIN_TOOLCHAIN_BASE_URL = 'https://brave-build-deps-public.s3.brave.com/windows-hermetic-toolchain/'
+      env.DEPOT_TOOLS_WIN_TOOLCHAIN_BASE_URL = 'https://vhemnu34de4lf5cj6bx2wwshyy0egdxk.lambda-url.us-west-2.on.aws/windows-hermetic-toolchain/'
     }
 
     if (this.getCachePath()) {
