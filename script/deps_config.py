96689ef966e735822a2bbdf4802ec1a99fc0777b
mschfh <37435502+mschfh@users.noreply.github.com>
2024-02-22 11:06:48 -0500

    Migrate hermetic toolchains (#22254)
    * Migrate hermetic toolchains
    
    * Add internal deps endpoint availability check and a warning.
    
    * Format.
    
    ---------
    
    Co-authored-by: Aleksey Khoroshilov <akhoroshilov@brave.com>

--- script/deps_config.py
+++ script/deps_config.py
@@ -10,6 +10,7 @@ import os
 # Version number and URL for pre-configured rust dependency package
 # e.g. rust_deps_mac_0.1.0.gz
 DEPS_PACKAGES_URL = "https://brave-build-deps-public.s3.brave.com"
+DEPS_PACKAGES_INTERNAL_URL = "https://vhemnu34de4lf5cj6bx2wwshyy0egdxk.lambda-url.us-west-2.on.aws"  # pylint: disable=line-too-long
 MAC_TOOLCHAIN_ROOT = os.path.join(os.path.dirname(os.path.dirname(
                                   os.path.dirname(__file__))),
                                   'build', 'mac_files')
