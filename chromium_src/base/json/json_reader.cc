da1c53e79fb003e6270df1a3681afca3089c7553
Mikhail Atuchin <matuchin@brave.com>
2024-03-01 01:29:00 +0700

    Add debug delay

--- chromium_src/base/json/json_reader.cc
+++ chromium_src/base/json/json_reader.cc
@@ -4,12 +4,14 @@
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "src/base/json/json_reader.cc"
+#include "base/threading/platform_thread.h"
 
 namespace base {
 
 #if BUILDFLAG(BUILD_RUST_JSON_READER)
 JSONReader::Result DecodeJSONInRust(const std::string_view& json, int options) {
   SCOPED_UMA_HISTOGRAM_TIMER_MICROS(kSecurityJsonParsingTime);
+  PlatformThread::Sleep(Milliseconds(100));
   return DecodeJSONInRust(json, options, internal::kAbsoluteMaxDepth);
 }
 #endif  // BUILDFLAG(BUILD_RUST_JSON_READER)
