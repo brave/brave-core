96e10b124bb8156ee1a0477fbc75d3ae458f72e3
sangwoo.ko <sangwoo108@gmail.com>
2024-03-01 13:43:01 +0900

    Try removing mime=>ext pair[]

--- components/playlist/browser/mime_util.cc
+++ components/playlist/browser/mime_util.cc
@@ -8,7 +8,6 @@
 #include <optional>
 #include <utility>
 
-#include "base/containers/contains.h"
 #include "base/containers/fixed_flat_map.h"
 #include "base/containers/flat_map.h"
 #include "base/files/file_path.h"
@@ -28,75 +27,44 @@ namespace {
 // * Mimetype to extension
 //   * https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
 //
-constexpr inline std::pair<std::string_view, base::FilePath::StringPieceType>
-    kMimeToExtensionData[] = {
-        /*m3u8*/
-        {"application/x-mpegurl", FILE_PATH_LITERAL("m3u8")},
-        {"application/vnd.apple.mpegurl", FILE_PATH_LITERAL("m3u8")},
-        {"audio/x-mpegurl", FILE_PATH_LITERAL("m3u8")},
-        {"audio/mpegurl", FILE_PATH_LITERAL("m3u8")},
-        /*aac*/
-        {"audio/aac", FILE_PATH_LITERAL("aac")},
-        /*flac*/
-        {"audio/flac", FILE_PATH_LITERAL("flac")},
-        /*mp3*/
-        {"audio/mp3", FILE_PATH_LITERAL("mp3")},
-        {"audio/x-mp3", FILE_PATH_LITERAL("mp3")},
-        {"audio/mpeg", FILE_PATH_LITERAL("mp3")},
-        /*wav*/
-        {"audio/wav", FILE_PATH_LITERAL("wav")},
-        {"audio/x-wav", FILE_PATH_LITERAL("wav")},
-        /*webm*/
-        {"audio/webm", FILE_PATH_LITERAL("weba")},
-        {"video/webm", FILE_PATH_LITERAL("webm")},
-        /*m4a*/
-        {"audio/x-m4a", FILE_PATH_LITERAL("m4a")},
-        /*3gp*/
-        {"video/3gpp", FILE_PATH_LITERAL("3gp")},
-        /*mp2t*/
-        {"video/mp2t", FILE_PATH_LITERAL("ts")},
-        /*mp4*/
-        {"video/mp4", FILE_PATH_LITERAL("mp4")},
-        {"audio/mp4", FILE_PATH_LITERAL("mp4")},
-        /*mpeg*/
-        {"video/mpeg", FILE_PATH_LITERAL("mpeg")},
-        /*ogg*/
-        {"application/ogg", FILE_PATH_LITERAL("ogx")},
-        {"audio/ogg", FILE_PATH_LITERAL("oga")},
-        {"video/ogg", FILE_PATH_LITERAL("ogv")},
-        /*m4v*/
-        {"video/x-m4v", FILE_PATH_LITERAL("m4v")}};
-
-// Helper templates to build flat_map from array of pairs.
-template <typename T, size_t N, size_t... I>
-auto MakeMimeToExtensionMap(const T (&pairs)[N], std::index_sequence<I...>) {
-  return base::flat_map<std::string_view, base::FilePath::StringPieceType>(
-      {pairs[I]...});
-}
-
-template <typename T, size_t N>
-auto MakeMimeToExtensionMap(const T (&pairs)[N]) {
-  return MakeMimeToExtensionMap(pairs, std::make_index_sequence<N>());
-}
-
-// For extension to mime map, reverse the pair.
-template <typename T>
-auto ReversePair(const T& pair) {
-  return std::pair{pair.second, pair.first};
-}
-
-template <typename T, size_t N, size_t... I>
-auto MakeExtensionToMimeMap(const T (&pairs)[N], std::index_sequence<I...>) {
-  // base::flat_map discards duplicated key, so we can pass pairs without
-  // filtering. Only the first will be picked.
-  return base::flat_map<base::FilePath::StringPieceType, std::string_view>(
-      {ReversePair(pairs[I])...});
-}
-
-template <typename T, size_t N>
-auto MakeExtensionToMimeMap(const T (&pairs)[N]) {
-  return MakeExtensionToMimeMap(pairs, std::make_index_sequence<N>());
-}
+constexpr auto kMimeToExtensionMap =
+    base::MakeFixedFlatMap<std::string_view, base::FilePath::StringPieceType>(
+        {/*m3u8*/
+         {"application/x-mpegurl", FILE_PATH_LITERAL("m3u8")},
+         {"application/vnd.apple.mpegurl", FILE_PATH_LITERAL("m3u8")},
+         {"audio/x-mpegurl", FILE_PATH_LITERAL("m3u8")},
+         {"audio/mpegurl", FILE_PATH_LITERAL("m3u8")},
+         /*aac*/
+         {"audio/aac", FILE_PATH_LITERAL("aac")},
+         /*flac*/
+         {"audio/flac", FILE_PATH_LITERAL("flac")},
+         /*mp3*/
+         {"audio/mp3", FILE_PATH_LITERAL("mp3")},
+         {"audio/x-mp3", FILE_PATH_LITERAL("mp3")},
+         {"audio/mpeg", FILE_PATH_LITERAL("mp3")},
+         /*wav*/
+         {"audio/wav", FILE_PATH_LITERAL("wav")},
+         {"audio/x-wav", FILE_PATH_LITERAL("wav")},
+         /*webm*/
+         {"audio/webm", FILE_PATH_LITERAL("weba")},
+         {"video/webm", FILE_PATH_LITERAL("webm")},
+         /*m4a*/
+         {"audio/x-m4a", FILE_PATH_LITERAL("m4a")},
+         /*3gp*/
+         {"video/3gpp", FILE_PATH_LITERAL("3gp")},
+         /*mp2t*/
+         {"video/mp2t", FILE_PATH_LITERAL("ts")},
+         /*mp4*/
+         {"video/mp4", FILE_PATH_LITERAL("mp4")},
+         {"audio/mp4", FILE_PATH_LITERAL("mp4")},
+         /*mpeg*/
+         {"video/mpeg", FILE_PATH_LITERAL("mpeg")},
+         /*ogg*/
+         {"application/ogg", FILE_PATH_LITERAL("ogx")},
+         {"audio/ogg", FILE_PATH_LITERAL("oga")},
+         {"video/ogg", FILE_PATH_LITERAL("ogv")},
+         /*m4v*/
+         {"video/x-m4v", FILE_PATH_LITERAL("m4v")}});
 
 }  // namespace
 
@@ -104,11 +72,9 @@ namespace mime_util {
 
 std::optional<base::FilePath::StringType> GetFileExtensionForMimetype(
     std::string_view mime_type) {
-  static const base::NoDestructor<
-      base::flat_map<std::string_view, base::FilePath::StringPieceType>>
-      kMimeToExtensionMap(MakeMimeToExtensionMap(kMimeToExtensionData));
-  if (auto iter = kMimeToExtensionMap->find(mime_type);
-      iter != kMimeToExtensionMap->end()) {
+  if (decltype(kMimeToExtensionMap)::const_iterator iter =
+          kMimeToExtensionMap.find(mime_type);
+      iter != kMimeToExtensionMap.end()) {
     return base::FilePath::StringType(iter->second);
   }
 
@@ -119,7 +85,13 @@ std::optional<std::string> GetMimeTypeForFileExtension(
     base::FilePath::StringPieceType file_extension) {
   static const base::NoDestructor<
       base::flat_map<base::FilePath::StringPieceType, std::string_view>>
-      kExtensionToMimeMap(MakeExtensionToMimeMap(kMimeToExtensionData));
+      kExtensionToMimeMap(([]() {
+        base::flat_map<base::FilePath::StringPieceType, std::string_view> map;
+        for (const auto& [mime, ext] : kMimeToExtensionMap) {
+          map[ext] = mime;
+        }
+        return map;
+      })());
 
   if (auto iter = kExtensionToMimeMap->find(file_extension);
       iter != kExtensionToMimeMap->end()) {
