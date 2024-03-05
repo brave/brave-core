6e88a58d865f093f4a5b267fd3ad77a3d2a40664
Simone Arpe <simon.arpe@gmail.com>
2024-03-05 10:15:26 +0100

    Include new component into password creation screen

--- android/java/org/chromium/chrome/browser/crypto_wallet/fragments/onboarding/OnboardingSecurePasswordFragment.java
+++ android/java/org/chromium/chrome/browser/crypto_wallet/fragments/onboarding/OnboardingSecurePasswordFragment.java
@@ -6,24 +6,27 @@
 package org.chromium.chrome.browser.crypto_wallet.fragments.onboarding;
 
 import android.annotation.SuppressLint;
+import android.app.Application;
 import android.hardware.biometrics.BiometricPrompt;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.CancellationSignal;
+import android.text.Editable;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelStoreOwner;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.chromium.base.ContextUtils;
 import org.chromium.brave_wallet.mojom.BraveWalletP3a;
 import org.chromium.brave_wallet.mojom.KeyringService;
 import org.chromium.brave_wallet.mojom.OnboardingAction;
@@ -31,6 +34,8 @@ import org.chromium.chrome.R;
 import org.chromium.chrome.browser.crypto_wallet.model.OnboardingViewModel;
 import org.chromium.chrome.browser.crypto_wallet.util.KeystoreHelper;
 import org.chromium.chrome.browser.crypto_wallet.util.Utils;
+import org.chromium.chrome.browser.custom_layout.PasswordStrengthMeterView;
+import org.chromium.ui.UiUtils;
 import org.chromium.ui.widget.Toast;
 
 import java.util.concurrent.Executor;
@@ -38,25 +43,16 @@ import java.util.concurrent.Executor;
 public class OnboardingSecurePasswordFragment extends BaseOnboardingWalletFragment {
     private boolean mCreateWalletClicked;
     private OnboardingViewModel mOnboardingViewModel;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    private PasswordStrengthMeterView mPasswordStrengthMeterView;
+    private AppCompatButton mContinueButton;
+    private TextInputEditText mRetypePasswordEditText;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mCreateWalletClicked = false;
-        View view = inflater.inflate(R.layout.fragment_secure_password, container, false);
-        view.setOnTouchListener((v, event) -> {
-            if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                Utils.hideKeyboard(requireActivity());
-            }
-            return true;
-        });
-        return view;
+        return inflater.inflate(R.layout.fragment_secure_password, container, false);
     }
 
     @Override
@@ -64,93 +60,73 @@ public class OnboardingSecurePasswordFragment extends BaseOnboardingWalletFragme
         super.onViewCreated(view, savedInstanceState);
         mOnboardingViewModel = new ViewModelProvider((ViewModelStoreOwner) requireActivity())
                                        .get(OnboardingViewModel.class);
-        Button secureCryptoButton = view.findViewById(R.id.btn_secure_crypto_continue);
-        secureCryptoButton.setOnClickListener(v -> {
-            if (mCreateWalletClicked) {
+
+        mRetypePasswordEditText = view.findViewById(R.id.text_input_retype_edit_text);
+        mContinueButton = view.findViewById(R.id.btn_secure_crypto_continue);
+        mContinueButton.setOnClickListener(v -> {
+            Editable text = mRetypePasswordEditText.getText();
+            if (mCreateWalletClicked || text == null) {
                 return;
             }
             mCreateWalletClicked = true;
-            EditText passwordEdittext = view.findViewById(R.id.secure_crypto_password);
-            String passwordInput = passwordEdittext.getText().toString();
-
-            KeyringService keyringService = getKeyringService();
-            assert keyringService != null;
-            keyringService.isStrongPassword(passwordInput, result -> {
-                if (!result) {
-                    passwordEdittext.setError(getResources().getString(R.string.password_text));
-                    mCreateWalletClicked = false;
-
-                    return;
-                }
-                proceedWithStrongPassword(passwordInput, view);
-            });
+            proceedWithStrongPassword(text.toString());
         });
+
+        mPasswordStrengthMeterView = view.findViewById(R.id.password_strength_meter);
+        mPasswordStrengthMeterView.setListener(match -> enable(mContinueButton, match));
     }
 
-    private void proceedWithStrongPassword(@NonNull final String password, @NonNull final View view) {
-        EditText retypePasswordEdittext = view.findViewById(R.id.secure_crypto_retype_password);
-        String retypePasswordInput = retypePasswordEdittext.getText().toString();
-        if (!password.equals(retypePasswordInput)) {
-            retypePasswordEdittext.setError(
-                    getResources().getString(R.string.retype_password_error));
-            mCreateWalletClicked = false;
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mCreateWalletClicked = false;
+    }
+
+    private void proceedWithStrongPassword(@NonNull final String password) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P
+                && Utils.isBiometricAvailable(requireContext())) {
+            setUpBiometric(password);
         } else {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P
-                    && Utils.isBiometricAvailable(getContext())) {
-                final BiometricPrompt.AuthenticationCallback authenticationCallback =
-                        new BiometricPrompt.AuthenticationCallback() {
-                            private void onNextPage() {
-                                // Go to next Page
-                                goToTheNextPage(password);
-                            }
-
-                            @Override
-                            public void onAuthenticationSucceeded(
-                                    BiometricPrompt.AuthenticationResult result) {
-                                super.onAuthenticationSucceeded(result);
-                                KeystoreHelper.useBiometricOnUnlock(password);
-                                onNextPage();
-                            }
-
-                            @Override
-                            public void onAuthenticationError(
-                                    int errorCode, CharSequence errString) {
-                                super.onAuthenticationError(errorCode, errString);
-
-                                // Even though we have an error, we still let to proceed
-                                Toast.makeText(getActivity(), errString, Toast.LENGTH_SHORT).show();
-                                onNextPage();
-                            }
-                        };
-                showFingerprintDialog(authenticationCallback);
-            } else {
-                goToTheNextPage(password);
-            }
-        }
-    }
-
-    private void goToTheNextPage(String passwordInput) {
-            showCreatingWalletPage();
+            goToTheNextPage(password);
         }
     }
 
     @SuppressLint("MissingPermission")
     @RequiresApi(api = Build.VERSION_CODES.P)
-    private void showFingerprintDialog(
-            @NonNull final BiometricPrompt.AuthenticationCallback authenticationCallback) {
-        Executor executor = ContextCompat.getMainExecutor(requireActivity());
-        new BiometricPrompt.Builder(requireActivity())
+    private void setUpBiometric(@NonNull final String password) {
+        final BiometricPrompt.AuthenticationCallback authenticationCallback =
+                new BiometricPrompt.AuthenticationCallback() {
+                    @Override
+                    public void onAuthenticationSucceeded(
+                            BiometricPrompt.AuthenticationResult result) {
+                        super.onAuthenticationSucceeded(result);
+                        KeystoreHelper.useBiometricOnUnlock(password);
+                        goToTheNextPage(password);
+                    }
+
+                    @Override
+                    public void onAuthenticationError(int errorCode, CharSequence errString) {
+                        super.onAuthenticationError(errorCode, errString);
+                        // Even though we have an error, we still let to proceed
+                        Toast.makeText(ContextUtils.getApplicationContext(), errString, Toast.LENGTH_SHORT).show();
+                        goToTheNextPage(password);
+                    }
+                };
+        Executor executor = ContextCompat.getMainExecutor(requireContext());
+        new BiometricPrompt.Builder(requireContext())
                 .setTitle(getResources().getString(R.string.enable_fingerprint_unlock))
                 .setDescription(getResources().getString(R.string.enable_fingerprint_text))
                 .setNegativeButton(getResources().getString(android.R.string.cancel), executor,
                         (dialog, which)
                                 -> authenticationCallback.onAuthenticationError(
-                                        BiometricPrompt.BIOMETRIC_ERROR_USER_CANCELED, ""))
+                                BiometricPrompt.BIOMETRIC_ERROR_USER_CANCELED, ""))
                 .build()
                 .authenticate(new CancellationSignal(), executor, authenticationCallback);
     }
 
-    private void showCreatingWalletPage() {
+    private void goToTheNextPage(@NonNull final String passwordInput) {
+        mOnboardingViewModel.setPassword(passwordInput);
         if (mOnNextPage != null) {
             mOnNextPage.gotoNextPage();
         }
