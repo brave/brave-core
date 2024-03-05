f10e43c0b488986ecc46afefea5a336f8fc13331
Simone Arpe <simon.arpe@gmail.com>
2024-03-05 10:23:05 +0100

    Remove unused import

--- android/java/org/chromium/chrome/browser/crypto_wallet/adapters/CryptoWalletOnboardingPagerAdapter.java
+++ android/java/org/chromium/chrome/browser/crypto_wallet/adapters/CryptoWalletOnboardingPagerAdapter.java
@@ -6,7 +6,6 @@
 package org.chromium.chrome.browser.crypto_wallet.adapters;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
