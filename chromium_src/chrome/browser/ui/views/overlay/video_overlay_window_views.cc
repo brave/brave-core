382b41f0a3a78c92bb2bfeecb10b56367b9a588c
brave-builds <brave-builds+devops@brave.com>
2024-03-06 05:58:16 +0000

    Uplift of #22468 (squashed) to beta

--- chromium_src/chrome/browser/ui/views/overlay/video_overlay_window_views.cc
+++ chromium_src/chrome/browser/ui/views/overlay/video_overlay_window_views.cc
@@ -7,14 +7,9 @@
 #include "brave/browser/ui/views/overlay/brave_video_overlay_window_views.h"
 
 #define BRAVE_UPDATE_MAX_SIZE max_size_ = work_area.size();
-#define BRAVE_CREATE        \
-  overlay_window->Close();  \
-  overlay_window.release(); \
-  overlay_window = std::make_unique<BraveVideoOverlayWindowViews>(controller);
 #define BackToTabLabelButton BraveBackToTabLabelButton
 
 #include "src/chrome/browser/ui/views/overlay/video_overlay_window_views.cc"
 
 #undef BackToTabLabelButton
-#undef BRAVE_CREATE
 #undef BRAVE_UPDATE_MAX_SIZE
