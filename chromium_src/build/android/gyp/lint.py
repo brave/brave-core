f7e2cabeb4ed1c402ef74ebf1453eaeaf66fb1b7
AlexeyBarabash <alexey@brave.com>
2024-03-07 14:33:40 +0200

    Limit JavaCmd python patch to lint.py, codereview notice

--- chromium_src/build/android/gyp/lint.py
+++ chromium_src/build/android/gyp/lint.py
@@ -2,11 +2,12 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at https://mozilla.org/MPL/2.0/.
-"""A inline part of build_utils.py"""
+
 import override_utils
 
-@override_utils.override_function(globals())
-def JavaCmd(original_function, xmx='1G'):
+
+@override_utils.override_function(build_utils)
+def JavaCmd(original_function, xmx='1G'):  # pylint: disable=unused-argument
     # Override to pass xmx='4G', to fix error
     # java.lang.OutOfMemoryError: Java heap space
     # for Android incremental builds
