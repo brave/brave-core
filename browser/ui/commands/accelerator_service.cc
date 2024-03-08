3fde0336ae6f77971950e1f13b044edf0127353e
Claudio DeSouza <cdesouza@brave.com>
2024-03-08 18:29:18 +0000

    `std::erase` now available
    This change removes the unused inclusion of stragling headers for the
    old `base::Erase/EraseIf`
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/fcb1dcc7fc7b7de16a27399f8ef7373dda9463cf
    
    commit fcb1dcc7fc7b7de16a27399f8ef7373dda9463cf
    Author: Andrew Rayskiy <greengrape@google.com>
    Date:   Wed Mar 6 13:24:21 2024 +0000
    
        [base] Remove Erase()/EraseIf() overloads for std::vector<>
    
        All call sites have been updated to std::erase()/std::erase_if().
    
        Bug: 40256229

--- browser/ui/commands/accelerator_service.cc
+++ browser/ui/commands/accelerator_service.cc
@@ -12,7 +12,6 @@
 #include <vector>
 
 #include "base/containers/contains.h"
-#include "base/containers/cxx20_erase_vector.h"
 #include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
 #include "base/ranges/algorithm.h"
