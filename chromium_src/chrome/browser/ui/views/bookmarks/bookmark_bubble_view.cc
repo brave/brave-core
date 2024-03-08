f402fbf4f66c887b94551e8e362bb2d48c982f48
brave-builds <brave-builds+devops@brave.com>
2024-03-08 07:23:44 +0000

    Uplift of #22489 (squashed) to release

--- chromium_src/chrome/browser/ui/views/bookmarks/bookmark_bubble_view.cc
+++ chromium_src/chrome/browser/ui/views/bookmarks/bookmark_bubble_view.cc
@@ -3,8 +3,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#define BRAVE_BOOKMARK_BUBBLE_VIEW \
+#define BRAVE_BOOKMARK_BUBBLE_VIEW_SHOW_BUBBLE_SET_ARROW \
   bubble->SetArrow(views::BubbleBorder::TOP_LEFT);
 
+#define BRAVE_BOOKMARK_BUBBLE_VIEW_SHOW_BUBBLE_SHOW_SIMPLIFIED \
+  show_simplified_flow = false;
+
 #include "src/chrome/browser/ui/views/bookmarks/bookmark_bubble_view.cc"
-#undef BRAVE_BOOKMARK_BUBBLE_VIEW
+#undef BRAVE_BOOKMARK_BUBBLE_VIEW_SHOW_BUBBLE_SHOW_SIMPLIFIED
+#undef BRAVE_BOOKMARK_BUBBLE_VIEW_SHOW_BUBBLE_SET_ARROW
