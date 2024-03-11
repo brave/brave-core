60a31188671c455b45ca40b40458bee8cd6c8b32
Anthony Tseng <darkdh@gmail.com>
2024-03-11 11:44:41 -0700

    Always reset PrintPreviewUI remote before binding to avoid double bind

--- browser/ui/webui/ai_chat/ai_chat_ui_page_handler.cc
+++ browser/ui/webui/ai_chat/ai_chat_ui_page_handler.cc
@@ -535,6 +535,9 @@ void AIChatUIPageHandler::MaybeCreatePrintPreview() {
     if (!print_render_frame_.is_bound()) {
       rfh->GetRemoteAssociatedInterfaces()->GetInterface(&print_render_frame_);
     }
+
+    print_render_frame_->InitiatePrintPreview(false);
+
     if (!owner_->IsBound()) {
       print_render_frame_->SetPrintPreviewUI(owner_->BindPrintPreviewUI());
     }
@@ -544,8 +547,6 @@ void AIChatUIPageHandler::MaybeCreatePrintPreview() {
       preview_ui_id = owner_->GetPreviewUIId();
     }
     CHECK(preview_ui_id);
-
-    print_render_frame_->InitiatePrintPreview(false);
 
     // A mininum print setting to avoid PrinterSettingsInvalid
     auto settings = base::JSONReader::Read(R"({
