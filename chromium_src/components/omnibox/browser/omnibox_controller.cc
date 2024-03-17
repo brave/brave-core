c74fbc5af66cdb4d4e121cb8b3be5bf205bab52a
Jay Harris <jay.harris@outlook.co.nz>
2024-03-18 08:59:48 +1300

    [Autocomplete]: More robust IsAutocompleteEnabled implementation (#22614)

--- chromium_src/components/omnibox/browser/omnibox_controller.cc
+++ chromium_src/components/omnibox/browser/omnibox_controller.cc
@@ -5,13 +5,24 @@
 
 #include "components/omnibox/browser/omnibox_controller.h"
 
+#include "brave/components/omnibox/browser/brave_omnibox_prefs.h"
+#include "components/prefs/pref_service.h"
+
+namespace {
+
+bool IsAutocompleteEnabled(const PrefService* prefs) {
+  return prefs->GetBoolean(omnibox::kAutocompleteEnabled);
+}
+
+}  // namespace
+
 #define StartAutocomplete StartAutocomplete_ChromiumImpl
 #include "src/components/omnibox/browser/omnibox_controller.cc"
 #undef StartAutocomplete
 
 void OmniboxController::StartAutocomplete(
     const AutocompleteInput& input) const {
-  if (!client_->IsAutocompleteEnabled()) {
+  if (!IsAutocompleteEnabled(client_->GetPrefs())) {
     ClearPopupKeywordMode();
     return;
   }
