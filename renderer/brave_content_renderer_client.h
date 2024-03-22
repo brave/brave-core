aba538d8e0480820bca5c60485200269b94ae032
Claudio DeSouza <cdesouza@brave.com>
2024-03-22 17:06:24 +0000

    Return type for `GetSupportedKeySystems` changed
    The return type for this function is merely passed along in one override
    used in the codebase, and the declaration has been correct to match
    what is on upstream.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/64a9bed52fde5a0efec87c39f68a3901dde3dfda
    
    commit 64a9bed52fde5a0efec87c39f68a3901dde3dfda
    Author: Feras Aldahlawi <feras@chromium.org>
    Date:   Thu Mar 21 01:46:11 2024 +0000
    
        Cleanup media::KeySystemSupportObserver
    
        - Rename KeySystemSupportObserver to KeySystemSupportRegistration
        - Remove unnecessary virtual function
        - Add clarifying comments.
    
        TESTED:
        * Add logs to see the size observer remote set in browser process.
          (after this line
          https://source.chromium.org/chromium/chromium/src/+/main:content/browser/media/key_system_support_impl.cc;drc=eb3c1d87f1c717de44fdaadb8b83041584f6a6b3;l=59)
    
        The following is observed only when moving key systems to a per-frame
        model. See
        https://chromium-review.googlesource.com/c/chromium/src/+/5168850
    
        * Visit integration.widevine.com/player
        * Click play observer logs (observer_remotes_.size() = 1)
        * Visit another site (e.g. shaka-player-demo.appspot.com) and play
          encrypted media
        * Check logs find (observer_remotes_.size() = 2)
        * Close integration.widevine.com/player tab and ensure render process
          for that tab is destroyed (e.g. in Task Manager)
        * Visit integration.staging.widevine.com/player and click play
        * Observer logs and find (observer_remotes_.size() = 2) whereas before
          https://chromium-review.googlesource.com/c/chromium/src/+/5168850 the
          logs would say (observer_remotes_.size() = 3)
    
        Bug: b/321307544

--- renderer/brave_content_renderer_client.h
+++ renderer/brave_content_renderer_client.h
@@ -30,7 +30,7 @@ class BraveContentRendererClient : public ChromeContentRendererClient {
   void RenderThreadStarted() override;
   void SetRuntimeFeaturesDefaultsBeforeBlinkInitialization() override;
   void RenderFrameCreated(content::RenderFrame* render_frame) override;
-  std::unique_ptr<media::KeySystemSupportObserver> GetSupportedKeySystems(
+  std::unique_ptr<media::KeySystemSupportRegistration> GetSupportedKeySystems(
       media::GetSupportedKeySystemsCB cb) override;
   void RunScriptsAtDocumentStart(content::RenderFrame* render_frame) override;
   void RunScriptsAtDocumentEnd(content::RenderFrame* render_frame) override;
