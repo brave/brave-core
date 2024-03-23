10255d545204285f8b36489ec473acf63249dfaf
Claudio DeSouza <cdesouza@brave.com>
2024-03-23 18:36:26 +0000

    `IDS_DOWNLOAD_TITLE` renamed
    This constant now refers to "Download History"
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/63dd0097aa5b5c453d1c05b72c9ce33e916301a3
    
    commit 63dd0097aa5b5c453d1c05b72c9ce33e916301a3
    Author: Lily Chen <chlily@chromium.org>
    Date:   Thu Mar 21 20:30:20 2024 +0000
    
        [Downloads] Rename chrome://downloads to "Download history"
    
        This renames the "Downloads" WebUI page (at chrome://downloads) to
        "Download history" to better reflect its contents. The search bar
        placeholder string is also updated.
    
        The toolbar CSS is adjusted to prevent the title from wrapping.
    
        The ChroMenu label ("Downloads - Ctrl+J") is kept as-is, as instructed
        by UXD.
    
        Screenshots:
        https://drive.google.com/drive/folders/1-yEHHrnMc7U2VOobahYaSMopOlelzpH8
    
        Bug: 40067647, 41492311

--- browser/ui/webui/navigation_bar_data_provider.cc
+++ browser/ui/webui/navigation_bar_data_provider.cc
@@ -1,7 +1,7 @@
-// Copyright (c) 2019 The Brave Authors
+// Copyright (c) 2019 The Brave Authors. All rights reserved.
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
-// you can obtain one at http://mozilla.org/MPL/2.0/.
+// You can obtain one at https://mozilla.org/MPL/2.0/.
 
 #include "brave/browser/ui/webui/navigation_bar_data_provider.h"
 
@@ -20,7 +20,8 @@ void NavigationBarDataProvider::Initialize(content::WebUIDataSource* source,
   source->AddLocalizedString("brToolbarSettingsTitle", IDS_SETTINGS_SETTINGS);
   source->AddLocalizedString("brToolbarBookmarksTitle",
                              IDS_BOOKMARK_MANAGER_TITLE);
-  source->AddLocalizedString("brToolbarDownloadsTitle", IDS_DOWNLOAD_TITLE);
+  source->AddLocalizedString("brToolbarDownloadsTitle",
+                             IDS_DOWNLOAD_HISTORY_TITLE);
   source->AddLocalizedString("brToolbarHistoryTitle", IDS_HISTORY_TITLE);
   source->AddLocalizedString("brToolbarRewardsTitle",
                              IDS_BRAVE_UI_BRAVE_REWARDS);
