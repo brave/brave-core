311b269652100fde5d0e7859520859bba5c9fcac
brave-builds <brave-builds+devops@brave.com>
2024-03-26 04:12:53 +0000

    Uplift of #22195 and #22892 (squashed) to beta

--- components/misc_metrics/general_browser_usage.cc
+++ components/misc_metrics/general_browser_usage.cc
@@ -6,6 +6,9 @@
 #include "brave/components/misc_metrics/general_browser_usage.h"
 
 #include "base/metrics/histogram_macros.h"
+#include "base/strings/strcat.h"
+#include "base/strings/string_util.h"
+#include "base/time/time.h"
 #include "brave/components/misc_metrics/pref_names.h"
 #include "brave/components/p3a_utils/bucket.h"
 #include "brave/components/time_period_storage/iso_weekly_storage.h"
@@ -24,9 +27,25 @@ constexpr int kProfileCountBuckets[] = {0, 1, 2, 3, 5};
 
 }  // namespace
 
-GeneralBrowserUsage::GeneralBrowserUsage(PrefService* local_state) {
+GeneralBrowserUsage::GeneralBrowserUsage(
+    PrefService* local_state,
+    std::optional<std::string> day_zero_experiment_variant,
+    bool is_first_run,
+    base::Time first_run_time)
+    : local_state_(local_state), first_run_time_(first_run_time) {
   usage_storage_ = std::make_unique<ISOWeeklyStorage>(
       local_state, kMiscMetricsBrowserUsageList);
+
+  if (is_first_run) {
+    if (day_zero_experiment_variant) {
+      local_state->SetString(kMiscMetricsDayZeroVariantAtInstall,
+                             *day_zero_experiment_variant);
+    }
+    if (first_run_time.is_null()) {
+      first_run_time_ = base::Time::Now();
+    }
+  }
+
   Update();
 }
 
@@ -34,6 +53,7 @@ GeneralBrowserUsage::~GeneralBrowserUsage() = default;
 
 void GeneralBrowserUsage::RegisterPrefs(PrefRegistrySimple* registry) {
   registry->RegisterListPref(kMiscMetricsBrowserUsageList);
+  registry->RegisterStringPref(kMiscMetricsDayZeroVariantAtInstall, {});
 }
 
 void GeneralBrowserUsage::ReportWeeklyUse() {
@@ -41,6 +61,24 @@ void GeneralBrowserUsage::ReportWeeklyUse() {
   UMA_HISTOGRAM_EXACT_LINEAR(kWeeklyUseHistogramName,
                              usage_storage_->GetLastISOWeekSum(), 8);
 }
+
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_WIN)
+void GeneralBrowserUsage::ReportInstallTime() {
+  int days_since_install = (base::Time::Now() - first_run_time_).InDays();
+  if (days_since_install < 0 || days_since_install > 30) {
+    return;
+  }
+  std::string day_zero_variant =
+      local_state_->GetString(kMiscMetricsDayZeroVariantAtInstall);
+  if (day_zero_variant.empty()) {
+    return;
+  }
+  std::string histogram_name = base::StrCat(
+      {kDayZeroInstallTimePrefix, base::ToUpperASCII(day_zero_variant),
+       kDayZeroInstallTimeSuffix});
+  base::UmaHistogramExactLinear(histogram_name, days_since_install, 31);
+}
+#endif  // BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_WIN)
 
 void GeneralBrowserUsage::ReportProfileCount(size_t count) {
 #if !BUILDFLAG(IS_ANDROID)
@@ -56,6 +94,9 @@ void GeneralBrowserUsage::SetUpUpdateTimer() {
 
 void GeneralBrowserUsage::Update() {
   ReportWeeklyUse();
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_WIN)
+  ReportInstallTime();
+#endif  // BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_WIN)
 
   SetUpUpdateTimer();
 }
