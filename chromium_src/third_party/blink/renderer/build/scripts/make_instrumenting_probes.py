RinkuDas7857/Brave

Serialize Page Graph args as json.

chromium_src/third_party/blink/renderer/build/scripts/make_instrumenting_probes.py
@@ -79,7 +79,7 @@ def _add_page_graph_events_to_pidl_source(source):
     if _IS_PG_WEBAPI_PROBES_ENABLED:
         ext += """
             void RegisterPageGraphBindingEvent([Keep] ExecutionContext*, const char* name, PageGraphBindingType type, PageGraphBindingEvent event);
-            void RegisterPageGraphWebAPICallWithResult([Keep] ExecutionContext*, const char* name, const PageGraphBlinkReceiverData& receiver_data, const PageGraphBlinkArgs& args, const ExceptionState* exception_state, const std::optional<String>& result);
+            void RegisterPageGraphWebAPICallWithResult([Keep] ExecutionContext*, const char* name, const PageGraphBlinkReceiverData& receiver_data, const PageGraphBlinkArgs& args, const ExceptionState* exception_state, const std::optional<PageGraphBlinkArg>& result);
         """
 
     return source[:idx] + ext + source[idx:]

third_party/blink/renderer/core/brave_page_graph/graph_item/edge/js/edge_js_result.h
@@ -6,9 +6,11 @@
 #ifndef BRAVE_THIRD_PARTY_BLINK_RENDERER_CORE_BRAVE_PAGE_GRAPH_GRAPH_ITEM_EDGE_JS_EDGE_JS_RESULT_H_
 #define BRAVE_THIRD_PARTY_BLINK_RENDERER_CORE_BRAVE_PAGE_GRAPH_GRAPH_ITEM_EDGE_JS_EDGE_JS_RESULT_H_
 
+#include <string>
+
+#include "brave/third_party/blink/renderer/core/brave_page_graph/blink_probe_types.h"
 #include "brave/third_party/blink/renderer/core/brave_page_graph/graph_item/edge/js/edge_js.h"
 #include "third_party/blink/renderer/platform/wtf/casting.h"
-#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace brave_page_graph {
 
@@ -20,7 +22,7 @@ class EdgeJSResult final : public EdgeJS {
   EdgeJSResult(GraphItemContext* context,
                NodeJS* out_node,
                NodeScript* in_node,
-               const String& result);
+               const blink::PageGraphBlinkArg& result);
 
   ~EdgeJSResult() override;
 
@@ -30,12 +32,12 @@ class EdgeJSResult final : public EdgeJS {
   void AddGraphMLAttributes(xmlDocPtr doc,
                             xmlNodePtr parent_node) const override;
 
-  const String& GetResult() const;
+  const std::string& GetResult() const;
   const MethodName& GetMethodName() const override;
   bool IsEdgeJSResult() const override;
 
  private:
-  const String result_;
+  const std::string result_;
 };
 
 }  // namespace brave_page_graph

third_party/blink/renderer/core/brave_page_graph/graph_item/edge/storage/edge_storage_set.cc
@@ -16,8 +16,9 @@ EdgeStorageSet::EdgeStorageSet(GraphItemContext* context,
                                NodeScript* out_node,
                                NodeStorage* in_node,
                                const String& key,
-                               const String& value)
-    : EdgeStorage(context, out_node, in_node, key), value_(value) {}
+                               const blink::PageGraphBlinkArg& value)
+    : EdgeStorage(context, out_node, in_node, key),
+      value_(blink::PageGraphBaseValueToString(value)) {}
 
 EdgeStorageSet::~EdgeStorageSet() = default;
 

brave/brave-core

chromium_src/third_party/blink/renderer/build/scripts/make_instrumenting_probes.py
@@ -79,7 +79,7 @@ def _add_page_graph_events_to_pidl_source(source):
     if _IS_PG_WEBAPI_PROBES_ENABLED:
         ext += """
             void RegisterPageGraphBindingEvent([Keep] ExecutionContext*, const char* name, PageGraphBindingType type, PageGraphBindingEvent event);
-            void RegisterPageGraphWebAPICallWithResult([Keep] ExecutionContext*, const char* name, const PageGraphBlinkReceiverData& receiver_data, const PageGraphBlinkArgs& args, const ExceptionState* exception_state, const std::optional<String>& result);
+            void RegisterPageGraphWebAPICallWithResult([Keep] ExecutionContext*, const char* name, const PageGraphObject& receiver_data, const PageGraphValues& args, const ExceptionState* exception_state, const std::optional<PageGraphValue>& result);
         """
 
     return source[:idx] + ext + source[idx:]

third_party/blink/renderer/core/brave_page_graph/graph_item/edge/js/edge_js_result.h
@@ -6,9 +6,11 @@
 #ifndef BRAVE_THIRD_PARTY_BLINK_RENDERER_CORE_BRAVE_PAGE_GRAPH_GRAPH_ITEM_EDGE_JS_EDGE_JS_RESULT_H_
 #define BRAVE_THIRD_PARTY_BLINK_RENDERER_CORE_BRAVE_PAGE_GRAPH_GRAPH_ITEM_EDGE_JS_EDGE_JS_RESULT_H_
 
+#include <string>
+
+#include "brave/third_party/blink/renderer/core/brave_page_graph/blink_probe_types.h"
 #include "brave/third_party/blink/renderer/core/brave_page_graph/graph_item/edge/js/edge_js.h"
 #include "third_party/blink/renderer/platform/wtf/casting.h"
-#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace brave_page_graph {
 
@@ -20,7 +22,7 @@ class EdgeJSResult final : public EdgeJS {
   EdgeJSResult(GraphItemContext* context,
                NodeJS* out_node,
                NodeScript* in_node,
-               const String& result);
+               const blink::PageGraphValue& result);
 
   ~EdgeJSResult() override;
 
@@ -30,12 +32,12 @@ class EdgeJSResult final : public EdgeJS {
   void AddGraphMLAttributes(xmlDocPtr doc,
                             xmlNodePtr parent_node) const override;
 
-  const String& GetResult() const;
+  const std::string& GetResult() const;
   const MethodName& GetMethodName() const override;
   bool IsEdgeJSResult() const override;
 
  private:
-  const String result_;
+  const std::string result_;
 };
 
 }  // namespace brave_page_graph

third_party/blink/renderer/core/brave_page_graph/graph_item/edge/storage/edge_storage_set.cc
@@ -16,8 +16,9 @@ EdgeStorageSet::EdgeStorageSet(GraphItemContext* context,
                                NodeScript* out_node,
                                NodeStorage* in_node,
                                const String& key,
-                               const String& value)
-    : EdgeStorage(context, out_node, in_node, key), value_(value) {}
+                               const blink::PageGraphValue& value)
+    : EdgeStorage(context, out_node, in_node, key),
+      value_(blink::PageGraphValueToString(value)) {}
 
 EdgeStorageSet::~EdgeStorageSet() = default;
 