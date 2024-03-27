2cb502df1163477f3b8784f10459f1afbf8a63fa
Claudio DeSouza <cdesouza@brave.com>
2024-03-27 00:12:29 -0300

    `SyncSetupService` has been deleted\
    This seems to be of no consequence for the only use in which the service
    was being initialised.

--- ios/browser/api/sync/brave_sync_worker.cc
+++ ios/browser/api/sync/brave_sync_worker.cc
@@ -33,8 +33,6 @@
 #include "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/sync/model/device_info_sync_service_factory.h"
 #include "ios/chrome/browser/sync/model/sync_service_factory.h"
-#include "ios/chrome/browser/sync/model/sync_setup_service.h"
-#include "ios/chrome/browser/sync/model/sync_setup_service_factory.h"
 #include "ios/web/public/thread/web_thread.h"
 
 namespace {
