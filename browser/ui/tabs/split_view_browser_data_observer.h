d34bd8ae60cf0456adf1615e73b7bae8235f1b5e
sangwoo.ko <sangwoo108@gmail.com>
2024-04-02 10:10:03 +0900

    Remove unnecessary access control

--- browser/ui/tabs/split_view_browser_data_observer.h
+++ browser/ui/tabs/split_view_browser_data_observer.h
@@ -13,10 +13,6 @@ class SplitViewBrowserDataObserver : public base::CheckedObserver {
  public:
   virtual void OnTileTabs(const SplitViewBrowserData::Tile& tile) = 0;
   virtual void OnWillBreakTile(const SplitViewBrowserData::Tile& tile) = 0;
-
- protected:
-  SplitViewBrowserDataObserver() = default;
-  ~SplitViewBrowserDataObserver() override = default;
 };
 
 #endif  // BRAVE_BROWSER_UI_TABS_SPLIT_VIEW_BROWSER_DATA_OBSERVER_H_
