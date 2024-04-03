db024c198a914c5be595c68ed4fb850e976c6ebc
Claudio DeSouza <cdesouza@brave.com>
2024-04-03 14:32:37 +0100

    Leo icon overrides moved to `SearchboxHandler`
    `RealboxHandler::PedalVectorIconToResourceName` has been deleted and
    moved into `SearchboxHandler::ActionVectorIconToResourceName`. This
    change moves the overrides to the new location.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/6bf8d813e830c2205db706ed455ce6d5d8a23555
    
    commit 6bf8d813e830c2205db706ed455ce6d5d8a23555
    Author: Nihar Majmudar <niharm@google.com>
    Date:   Thu Mar 28 18:52:59 2024 +0000
    
        Refactor RealboxHandler functions into SearchboxHandler.
    
        This CL moves the onResultChanged code (and all dependent code) to the
        SearchboxHandler. Essentially the only thing remaining in the
        RealboxHandler namespace now is the RealboxOmniboxClient code.
    
        Bug: 331452566

--- chromium_src/chrome/browser/ui/webui/searchbox/realbox_handler.cc
+++ chromium_src/chrome/browser/ui/webui/searchbox/realbox_handler.cc
@@ -5,24 +5,7 @@
 
 #include "chrome/browser/ui/webui/searchbox/realbox_handler.h"
 
-#include "brave/components/vector_icons/vector_icons.h"
 #include "components/omnibox/browser/autocomplete_input.h"
-#include "components/omnibox/browser/vector_icons.h"
-
-namespace {
-constexpr char kLeoWindowTabNewIconResourceName[] =
-    "//resources/brave-icons/window-tab-new.svg";
-}  // namespace
-
-// This patches |PedalVectorIconToResourceName| to include the additional
-// omnibox icons we add in Brave. Unfortunately, the method is used inside the
-// file, so we can't just override the method. There is only one usage of
-// kSwitchIcon in the file, so we add additional statements from it.
-#define kSwitchIcon kSwitchIcon.name) {}        \
-  if (icon.name == kLeoWindowTabNewIcon.name) { \
-    return kLeoWindowTabNewIconResourceName;    \
-  }                                             \
-  if (icon.name == omnibox::kSwitchIcon
 
 // We tweak a few AutocompleteInput settings because unlike Chromium we only
 // want keyword search results.
@@ -36,4 +19,3 @@ constexpr char kLeoWindowTabNewIconResourceName[] =
 
 #undef set_prefer_keyword
 #undef set_allow_exact_keyword_match
-#undef kSwitchIcon
