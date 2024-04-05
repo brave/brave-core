173786c39c55f5347778021050e151792ceadadc
mkarolin <max@brave.com>
2024-04-05 15:22:24 -0400

    fixup! Recent tabs menu code moved `ui/tabs`

--- chromium_src/chrome/browser/ui/tabs/recent_tabs_sub_menu_model.h
+++ chromium_src/chrome/browser/ui/tabs/recent_tabs_sub_menu_model.h
@@ -1,10 +1,10 @@
 /* Copyright (c) 2020 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_TOOLBAR_RECENT_TABS_SUB_MENU_MODEL_H_
-#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_TOOLBAR_RECENT_TABS_SUB_MENU_MODEL_H_
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_TABS_RECENT_TABS_SUB_MENU_MODEL_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_TABS_RECENT_TABS_SUB_MENU_MODEL_H_
 
 #include <memory>
 
@@ -17,4 +17,4 @@
 
 #undef local_tab_cancelable_task_tracker_
 
-#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_TOOLBAR_RECENT_TABS_SUB_MENU_MODEL_H_
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_TABS_RECENT_TABS_SUB_MENU_MODEL_H_
