36d90174b5553d62ac25460450c8c2469501b141
Josh Leonard <30185185+josheleonard@users.noreply.github.com>
2024-04-10 20:42:14 +0100

    fix(wallet): delay core api function calls on ios

--- components/brave_wallet_ui/common/async/bridge.ts
+++ components/brave_wallet_ui/common/async/bridge.ts
@@ -9,6 +9,10 @@ import getWalletPageApiProxy from '../../page/wallet_page_api_proxy'
 
 import { LOCAL_STORAGE_KEYS } from '../../common/constants/local-storage-keys'
 import { WalletApiDataOverrides } from '../../constants/testing_types'
+import { unbiasedRandom } from '../../utils/random-utils'
+
+// TODO: get from loadTimeData
+const isIos = true
 
 const debugProxyHandler = (path?: string) => ({
   get(target: any, propertyKey: PropertyKey, receiver?: unknown): any {
@@ -33,11 +37,57 @@ const debugProxyHandler = (path?: string) => ({
   }
 })
 
+const MIN_DELAY_MILLISECONDS = 16 // ~ 1 frame on a 60fps screen
+const MAX_DELAY_MILLISECONDS = 33 // ~ 1 frame on a 30fps screen
+
+const delayedProxyHandler = (path?: string) => ({
+  get(target: any, propertyKey: PropertyKey, receiver?: unknown): any {
+    const v = Reflect.get(target, propertyKey, receiver)
+
+    const propertyPath = path
+      ? `${path}.${propertyKey.toString()}`
+      : propertyKey.toString()
+
+    if (typeof v === 'function') {
+      return (...args: any[]) => {
+        const result = v.apply(target, args)
+        if (result instanceof Promise) {
+          return result.then(
+            (result) =>
+              new Promise((res) => {
+                setTimeout(
+                  () => {
+                    res(result)
+                  },
+                  // wait time
+                  unbiasedRandom(MIN_DELAY_MILLISECONDS, MAX_DELAY_MILLISECONDS)
+                )
+              })
+          )
+        }
+
+        return result
+      }
+    }
+
+    if (typeof v === 'object') {
+      return new Proxy(v, delayedProxyHandler(propertyPath))
+    }
+
+    return v
+  }
+})
+
 export function getAPIProxy(): WalletApiProxy {
   const nativeProxy =
     window.location.hostname === 'wallet-panel.top-chrome'
       ? getWalletPanelApiProxy()
       : getWalletPageApiProxy()
+
+  const shouldDelay = isIos
+  if (shouldDelay) {
+    return new Proxy(nativeProxy, delayedProxyHandler())
+  }
 
   const debug = window.localStorage.getItem(LOCAL_STORAGE_KEYS.DEBUG)
   if (!debug || debug !== 'true') {
