95c4911de11c8162d273b73f3fab9e3d9c84aa5c
Pete Miller <miller.pete@gmail.com>
2024-04-11 18:15:33 -0700

    fix(wallet) incorrect react-virtualized-auto-sizer reference and type (#23043)

--- components/brave_wallet_ui/components/desktop/views/portfolio/components/token-lists/virtualized-tokens-list.tsx
+++ components/brave_wallet_ui/components/desktop/views/portfolio/components/token-lists/virtualized-tokens-list.tsx
@@ -5,7 +5,7 @@
 
 import * as React from 'react'
 import { VariableSizeList as List } from 'react-window'
-import AutoSizer from '@brave/react-virtualized-auto-sizer'
+import AutoSizer from 'react-virtualized-auto-sizer'
 
 // Types
 import { BraveWallet } from '../../../../../../constants/types'
@@ -139,7 +139,7 @@ export const VirtualizedTokensList = React.memo(function <T extends any[]>({
   // render
   return (
     <AutoSizer style={AutoSizerStyle}>
-      {function ({ height, width }) {
+      {function ({ height, width }: { height: number; width: number }) {
         return (
           <List
             ref={onListMount}
