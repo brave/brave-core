fea9e9180c89f1c2747bbb4120838c742d4366d8
Terry Mancey <terry.mancey@icloud.com>
2024-04-16 11:21:35 -0500

    [ads] Deprecate v1 ad serving pipelines

--- components/brave_ads/core/internal/serving/eligible_ads/pipelines/notification_ads/eligible_notification_ads_factory.cc
+++ components/brave_ads/core/internal/serving/eligible_ads/pipelines/notification_ads/eligible_notification_ads_factory.cc
@@ -6,7 +6,6 @@
 #include "brave/components/brave_ads/core/internal/serving/eligible_ads/pipelines/notification_ads/eligible_notification_ads_factory.h"
 
 #include "brave/components/brave_ads/core/internal/serving/eligible_ads/pipelines/notification_ads/eligible_notification_ads_base.h"
-#include "brave/components/brave_ads/core/internal/serving/eligible_ads/pipelines/notification_ads/eligible_notification_ads_v1.h"
 #include "brave/components/brave_ads/core/internal/serving/eligible_ads/pipelines/notification_ads/eligible_notification_ads_v2.h"
 #include "brave/components/brave_ads/core/internal/targeting/behavioral/anti_targeting/resource/anti_targeting_resource.h"
 #include "brave/components/brave_ads/core/internal/targeting/geographical/subdivision/subdivision_targeting.h"
@@ -19,11 +18,6 @@ EligibleNotificationAdsFactory::Build(
     const SubdivisionTargeting& subdivision_targeting,
     const AntiTargetingResource& anti_targeting_resource) {
   switch (version) {
-    case 1: {
-      return std::make_unique<EligibleNotificationAdsV1>(
-          subdivision_targeting, anti_targeting_resource);
-    }
-
     case 2: {
       return std::make_unique<EligibleNotificationAdsV2>(
           subdivision_targeting, anti_targeting_resource);
