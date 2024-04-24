81cc257929ff0accd0fe538a55cb3ec13e6d49da
sangwoo.ko <sangwoo108@gmail.com>
2024-04-24 10:11:18 +0900

    Fix "New Split View" command with pinned tabs
    This Patch fixes wrong index calculation. When opening new tabs,
    tab index is lower bounded to pinned tab count, as unpinned tabs can't
    be in the middle of pinned tabs.

--- browser/ui/browser_commands.cc
+++ browser/ui/browser_commands.cc
@@ -915,11 +915,15 @@ void NewSplitViewForTab(Browser* browser, std::optional<tabs::TabHandle> tab) {
   }
 
   auto* model = browser->tab_strip_model();
-  int tab_index = model->GetIndexOfTab(*tab);
-  chrome::AddTabAt(browser, GURL("chrome://newtab"), tab_index + 1,
+  const int tab_index = model->GetIndexOfTab(*tab);
+  const int new_tab_index = model->IsTabPinned(tab_index)
+                                ? model->IndexOfFirstNonPinnedTab()
+                                : tab_index + 1;
+
+  chrome::AddTabAt(browser, GURL("chrome://newtab"), new_tab_index,
                    /*foreground*/ true);
   split_view_data->TileTabs(std::make_pair(
-      model->GetTabHandleAt(tab_index), model->GetTabHandleAt(tab_index + 1)));
+      model->GetTabHandleAt(tab_index), model->GetTabHandleAt(new_tab_index)));
 }
 
 void CloseSplitViewForTab(Browser* browser,
