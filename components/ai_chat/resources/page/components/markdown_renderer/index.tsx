6a75606a6be7384eae18d8ac3b7871112c30421d
Taher <8665427+nullhook@users.noreply.github.com>
2024-04-25 12:20:05 -0700

    simplified hast parsing

--- components/ai_chat/resources/page/components/markdown_renderer/index.tsx
+++ components/ai_chat/resources/page/components/markdown_renderer/index.tsx
@@ -5,7 +5,8 @@
 
 import * as React from 'react'
 import Markdown from 'react-markdown'
-import { Root, Element, Properties } from 'hast'
+import type { Root, Element as HastElement } from 'hast'
+
 const visit = require('unist-util-visit/index')
 
 import styles from './style.module.scss'
@@ -17,15 +18,6 @@ const CodeBlock = React.lazy(async () => ({
 const CodeInline = React.lazy(async () => ({
   default: (await import('../code_block')).default.Inline
 }))
-
-// Any custom prop on DOM element must be a lowercase otherwise React will utilize it. The following gives hint:
-// Warning: React does not recognize the `%s` prop on a DOM element. If you intentionally want it to appear in the DOM as a custom attribute, spell it as lowercase `%s` instead.
-
-interface ElementWithData extends Element {
-  properties: Properties & {
-    'is-cursor-visible': string
-  }
-}
 
 const allowedElements = [
   // Headings
@@ -59,26 +51,42 @@ const allowedElements = [
   'hr'
 ]
 
+interface CursorDecoratorProps {
+  as: React.ElementType
+  children: React.ReactNode
+  isCursorVisible: boolean
+}
+
+function CursorDecorator(props: CursorDecoratorProps) {
+  const Tag = props.as
+
+  return (
+    <Tag>
+      {props.children}
+      {props.isCursorVisible && (
+        <span className={styles.textCursor}>
+          <CaretSVG />
+        </span>
+      )}
+    </Tag>
+  )
+}
+
 interface MarkdownRendererProps {
   text: string
   shouldShowTextCursor: boolean
 }
 
 export default function MarkdownRenderer(mainProps: MarkdownRendererProps) {
+  const lastElementRef = React.useRef<HastElement | undefined>()
+
   const plugin = React.useCallback(() => {
     const transformer = (tree: Root) => {
       const totalLines = tree.position?.end.line
 
-      visit(tree, 'element', (el: Element) => {
-        if (!('properties' in el)) {
-          el.properties = {}
-        }
-
-        const newEl = el as ElementWithData
-
-        newEl.properties = {
-          ...el.properties,
-          'is-cursor-visible': (totalLines === el.position?.end.line).toString()
+      visit(tree, 'element', (el: HastElement) => {
+        if (totalLines === el.position?.end.line) {
+          lastElementRef.current = el
         }
       })
     }
@@ -96,22 +104,24 @@ export default function MarkdownRenderer(mainProps: MarkdownRendererProps) {
         unwrapDisallowed={true}
         children={mainProps.text}
         components={{
-          p: (props) => {
-            const el = props.node as ElementWithData
-            const isCursorVisible =
-              el.properties['is-cursor-visible'] === 'true'
-
-            return (
-              <p>
-                {props.children}
-                {isCursorVisible && (
-                  <span className={styles.textCursor}>
-                    <CaretSVG />
-                  </span>
-                )}
-              </p>
-            )
-          },
+          p: (props) => (
+            <CursorDecorator
+              as='p'
+              children={props.children}
+              isCursorVisible={
+                (props.node as HastElement) === lastElementRef.current
+              }
+            />
+          ),
+          li: (props) => (
+            <CursorDecorator
+              as='li'
+              children={props.children}
+              isCursorVisible={
+                (props.node as HastElement) === lastElementRef.current
+              }
+            />
+          ),
           code: (props) => {
             const { children, className } = props
             const match = /language-([^ ]+)/.exec(className || '')
--- components/ai_chat/resources/page/components/svg/caret.tsx
+++ components/ai_chat/resources/page/components/svg/caret.tsx
@@ -5,13 +5,12 @@
 
 import * as React from "react"
 
-const SvgComponent = (props: any) => (
+const SvgComponent = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={3}
     height={12}
     fill="none"
-    {...props}
   >
     <rect width={3} height={12} rx={1} />
   </svg>
