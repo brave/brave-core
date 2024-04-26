3588802b9df317b28b0918680e89e9e440654af7
Darnell Andries <dandries@brave.com>
2024-04-26 13:09:58 -0700

    Add WDP keyed service

--- browser/browser_context_keyed_service_factories.cc
+++ browser/browser_context_keyed_service_factories.cc
@@ -30,6 +30,7 @@
 #include "brave/browser/search_engines/search_engine_tracker.h"
 #include "brave/browser/sync/brave_sync_alerts_service_factory.h"
 #include "brave/browser/url_sanitizer/url_sanitizer_service_factory.h"
+#include "brave/browser/web_discovery/wdp_service_factory.h"
 #include "brave/components/ai_chat/core/common/buildflags/buildflags.h"
 #include "brave/components/brave_perf_predictor/browser/named_third_party_registry_factory.h"
 #include "brave/components/brave_vpn/common/buildflags/buildflags.h"
@@ -216,6 +217,8 @@ void EnsureBrowserContextKeyedServiceFactoriesBuilt() {
 #if BUILDFLAG(ENABLE_AI_CHAT)
   ai_chat::ModelServiceFactory::GetInstance();
 #endif
+
+  web_discovery::WDPServiceFactory::GetInstance();
 }
 
 }  // namespace brave
---
+++ browser/web_discovery/wdp_service_factory.h
@@ -0,0 +1,36 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_BROWSER_WEB_DISCOVERY_WDP_SERVICE_FACTORY_H_
+#define BRAVE_BROWSER_WEB_DISCOVERY_WDP_SERVICE_FACTORY_H_
+
+#include "base/no_destructor.h"
+#include "components/keyed_service/content/browser_context_keyed_service_factory.h"
+
+namespace web_discovery {
+
+class WDPServiceFactory : public BrowserContextKeyedServiceFactory {
+ public:
+  static WDPServiceFactory* GetInstance();
+
+ private:
+  friend base::NoDestructor<WDPServiceFactory>;
+
+  WDPServiceFactory();
+  ~WDPServiceFactory() override;
+
+  WDPServiceFactory(const WDPServiceFactory&) = delete;
+  WDPServiceFactory& operator=(const WDPServiceFactory&) = delete;
+
+  KeyedService* BuildServiceInstanceFor(
+      content::BrowserContext* context) const override;
+  content::BrowserContext* GetBrowserContextToUse(
+      content::BrowserContext* context) const override;
+  bool ServiceIsCreatedWithBrowserContext() const override;
+};
+
+}  // namespace web_discovery
+
+#endif  // BRAVE_BROWSER_WEB_DISCOVERY_WDP_SERVICE_FACTORY_H_
