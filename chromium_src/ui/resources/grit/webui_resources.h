f09aa157a35da254841d438fcd30de68e6cb9530
Jay Harris <jay.harris@outlook.co.nz>
2024-04-29 09:57:29 +1200

    Don't override on IOS/ANDROID

--- chromium_src/ui/resources/grit/webui_resources.h
+++ chromium_src/ui/resources/grit/webui_resources.h
@@ -6,8 +6,11 @@
 #ifndef BRAVE_CHROMIUM_SRC_UI_RESOURCES_GRIT_WEBUI_RESOURCES_H_
 #define BRAVE_CHROMIUM_SRC_UI_RESOURCES_GRIT_WEBUI_RESOURCES_H_
 
+#include "build/build_config.h"
+
 #include "../gen/ui/resources/grit/webui_resources.h"  // IWYU pragma: export
 
+#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
 // Override cr-button HTML
 #ifdef IDR_WEBUI_CR_ELEMENTS_CR_BUTTON_CR_BUTTON_HTML_JS
 #undef IDR_WEBUI_CR_ELEMENTS_CR_BUTTON_CR_BUTTON_HTML_JS
@@ -42,6 +45,7 @@
   IDR_BRAVE_CR_ELEMENTS_CR_TOGGLE_CR_TOGGLE_JS
 #else
 #error IDR_WEBUI_CR_ELEMENTS_CR_TOGGLE_CR_TOGGLE_JS is not defined upstream
+#endif
 #endif
 
 #endif  // BRAVE_CHROMIUM_SRC_UI_RESOURCES_GRIT_WEBUI_RESOURCES_H_
