eb00102bea620e0fcc4e0853b6539fa6c3932084
Taher <8665427+nullhook@users.noreply.github.com>
2024-04-29 10:51:00 -0700

    aichat: simplify model intro

--- components/ai_chat/resources/page/components/model_intro/index.tsx
+++ components/ai_chat/resources/page/components/model_intro/index.tsx
@@ -5,13 +5,16 @@
 
 import * as React from 'react'
 import Icon from '@brave/leo/react/icon'
+import Tooltip from '@brave/leo/react/tooltip'
+import Button from '@brave/leo/react/button'
+import Link from '@brave/leo/react/link'
 import formatMessage from '$web-common/formatMessage'
 import { getLocale } from '$web-common/locale'
 import * as mojom from '../../api/page_handler'
 import DataContext from '../../state/context'
 import styles from './style.module.scss'
 
-function getCategoryName (category: mojom.ModelCategory) {
+function getCategoryName(category: mojom.ModelCategory) {
   // To avoid problems when order of enum values change, we base the key
   // on the enum name rather than the number value, e.g. "CHAT" vs 0
   const categoryKey = Object.keys(mojom.ModelCategory)[category]
@@ -19,18 +22,29 @@ function getCategoryName (category: mojom.ModelCategory) {
   return getLocale(key)
 }
 
-function getIntroMessage (model: mojom.Model) {
+function getIntroMessage(model: mojom.Model) {
   const key = `introMessage-${model.key}`
   return getLocale(key)
 }
 
-export default function ModelIntro () {
+export default function ModelIntro() {
+  const [isTooltipVisible, setIsTooltipVisible] = React.useState(false)
   const context = React.useContext(DataContext)
 
   const model = context.currentModel
   if (!model) {
     console.error('Rendered ModelIntro when currentModel does not exist!')
     return <></>
+  }
+
+  const handleInfoIconTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    setIsTooltipVisible(true)
+  }
+
+  const handleInfoIconTouchEnd = (e: React.TouchEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    setIsTooltipVisible(false)
   }
 
   return (
@@ -47,8 +61,30 @@ export default function ModelIntro () {
               $2: model.displayMaker
             }
           })}
+          <Tooltip visible={isTooltipVisible}>
+            <div
+              slot='content'
+              className={styles.tooltipContainer}
+            >
+              {getIntroMessage(model)}
+              <div>
+                <Link href='/'>Learn more</Link>
+              </div>
+            </div>
+            <Button
+              kind='plain-faint'
+              className={styles.tooltipButton}
+              onMouseOver={() => setIsTooltipVisible(true)}
+              onMouseOut={() => setIsTooltipVisible(false)}
+              onTouchStart={handleInfoIconTouchStart}
+              onTouchEnd={handleInfoIconTouchEnd}
+              onFocus={() => setIsTooltipVisible(true)}
+              onBlur={() => setIsTooltipVisible(false)}
+            >
+              <Icon name='info-outline' />
+            </Button>
+          </Tooltip>
         </h3>
-        <p className={styles.modelIntro}>{getIntroMessage(model)}</p>
       </div>
     </div>
   )
