d7758873768554e26c94fc65021bd18622fdf213
Claudio DeSouza <cdesouza@brave.com>
2024-04-29 22:43:10 +0100

    `BraveHasAPIKeyConfigured` to be rellocated
    This function was being overriden into a file that has been deleted and
    replaced by `google_apis/google_api_keys-inc.cc`. This change corrects
    the override file accordingly.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/6d65d3f71df312ebf047c2b720e5875c3a6f3c6e
    
    commit 6d65d3f71df312ebf047c2b720e5875c3a6f3c6e
    Author: Mihai Sardarescu <msarda@chromium.org>
    Date:   Fri Apr 5 14:06:34 2024 +0000
    
        [google_apis] Move google_api_keys.cc to google_api_keys-inc.cc
    
        File google/google_api_keys.cc is included directly from
        google/google_api_keys_unittest.cc which is now forbidden by
        PRESUMIT and adding any new test of this new file leads to a
        presubmit error.
    
        This CL simply moves the file to google_api_keys-inc.cc to respect
        the new PRESUBMIT rules.
    
        Note: Even if file google_api_keys-inc.cc looks like a new added file,
        its contents are identical to the previous version of
        google_api_keys.cc (it is a pure move of the previous file, without
        any edits).
    
        Bug: None

--- chromium_src/google_apis/google_api_keys-inc.cc
+++ chromium_src/google_apis/google_api_keys-inc.cc
@@ -1,10 +1,9 @@
-/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "brave/chromium_src/google_apis/google_api_keys.h"
-#include "src/google_apis/google_api_keys.cc"
+#include "src/google_apis/google_api_keys-inc.cc"
 
 namespace google_apis {
 
