e12147b04390d39f756eb76b7662f864bcdc0a0c
Claudio DeSouza <cdesouza@brave.com>
2024-05-01 20:41:35 +0100

    Revert "Fixes debug build."
    This reverts commit 2aa6342c39fb3949df6a7eb1f3c67bc5ab5a6189.

--- components/brave_wallet/browser/zcash/rust/extended_spending_key.h
+++ components/brave_wallet/browser/zcash/rust/extended_spending_key.h
@@ -26,12 +26,12 @@ class ExtendedSpendingKey {
 
   // Derives hardened key using index and the current key
   virtual std::unique_ptr<ExtendedSpendingKey> DeriveHardenedChild(
-      uint32_t index) = 0;
+      uint32_t index);
 
   // Returns public or internal address that may be used as a recipient address
   // in transactions
   virtual std::optional<std::array<uint8_t, kOrchardRawBytesSize>>
-  GetDiversifiedAddress(uint32_t div_index, OrchardAddressKind kind) = 0;
+  GetDiversifiedAddress(uint32_t div_index, OrchardAddressKind kind);
 };
 
 }  // namespace brave_wallet::orchard
