ede5c0881e9367ad043a8965ded995d9a0804db0
taher <8665427+nullhook@users.noreply.github.com>
2024-05-02 11:39:01 -0700

    aichat: replace NOTREACHED with DumpWithoutCrashing in conversation driver (#23208)

--- components/ai_chat/core/browser/conversation_driver.cc
+++ components/ai_chat/core/browser/conversation_driver.cc
@@ -13,6 +13,7 @@
 
 #include "base/containers/contains.h"
 #include "base/containers/fixed_flat_set.h"
+#include "base/debug/crash_logging.h"
 #include "base/functional/bind.h"
 #include "base/memory/weak_ptr.h"
 #include "base/no_destructor.h"
@@ -269,7 +270,10 @@ void ConversationDriver::InitEngine() {
   auto* model = GetModel(model_key_);
   // Make sure we get a valid model, defaulting to static default or first.
   if (!model) {
-    NOTREACHED() << "Model was not part of static model list";
+    // It is unexpected that we get here. Dump a call stack
+    // to help figure out why it happens.
+    SCOPED_CRASH_KEY_STRING1024("BraveAIChatModel", "key", model_key_);
+    base::debug::DumpWithoutCrashing();
     // Use default
     model = GetModel(features::kAIModelsDefaultKey.Get());
     DCHECK(model);
