f6a125fa4bfa285b355444a0bd8e3ffb44346691
Pavel Beloborodov <73575789+boocmp@users.noreply.github.com>
2024-05-03 19:18:19 +0700

    The built-in bridges have been updated. Added a request for built-in bridges. (#23316)
    * Built-in bridges updated. Added request for built-in bridges.

--- components/tor/tor_profile_service_impl.cc
+++ components/tor/tor_profile_service_impl.cc
@@ -34,6 +34,10 @@
 #include "net/base/schemeful_site.h"
 #include "net/proxy_resolution/proxy_resolution_service.h"
 #include "net/url_request/url_request_context.h"
+#include "services/data_decoder/public/cpp/data_decoder.h"
+#include "services/network/public/cpp/resource_request.h"
+#include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "services/network/public/cpp/simple_url_loader.h"
 #include "services/network/public/mojom/network_context.mojom.h"
 #include "services/network/public/mojom/proxy_lookup_client.mojom.h"
 
@@ -114,9 +118,95 @@ void OnNewTorCircuit(std::unique_ptr<NewTorCircuitTracker> tracker,
                              !proxy_info->is_direct());
 }
 
+constexpr const char kTorBuiltinBridgesFetchUrl[] =
+    "https://bridges.torproject.org/moat/circumvention/builtin";
+
+constexpr net::NetworkTrafficAnnotationTag kTorBuiltinBridgesMoatAnnotation =
+    net::DefineNetworkTrafficAnnotation("brave_tor_bridges", R"(
+    semantics {
+      sender:
+        "Built-in Bridges Request"
+      description:
+        "This service sends requests to the Tor bridges server."
+      trigger:
+        "When user opens new Tor window, but no more than once a day."
+      destination: WEBSITE
+    }
+    policy {
+      cookies_allowed: NO
+    })");
+
+constexpr const size_t kMaxBodySize = 4 * 1024;
+
 }  // namespace
 
+class BuiltinBridgesRequest {
+ public:
+  using ResultCallback = base::OnceCallback<void(const base::Value::Dict&)>;
+
+  static std::unique_ptr<BuiltinBridgesRequest> MaybeUpdateBuiltinBridges(
+      content::BrowserContext* browser_context,
+      PrefService* local_state,
+      ResultCallback callback) {
+    const base::Time last_request_time =
+        local_state->GetTime(prefs::kBuiltinBridgesRequestTime);
+    if (base::Time::Now() > last_request_time + base::Days(1)) {
+      local_state->SetTime(prefs::kBuiltinBridgesRequestTime,
+                           base::Time::Now());
+      return base::WrapUnique(
+          new BuiltinBridgesRequest(browser_context, std::move(callback)));
+    }
+    return nullptr;
+  }
+
+ private:
+  BuiltinBridgesRequest(content::BrowserContext* browser_context,
+                        ResultCallback callback)
+      : url_loader_factory_(browser_context->GetDefaultStoragePartition()
+                                ->GetURLLoaderFactoryForBrowserProcess()),
+        result_callback_(std::move(callback)) {
+    auto request = std::make_unique<network::ResourceRequest>();
+    request->url = GURL(kTorBuiltinBridgesFetchUrl);
+    request->method = net::HttpRequestHeaders::kGetMethod;
+    request->credentials_mode = network::mojom::CredentialsMode::kOmit;
+    request->load_flags = net::LOAD_DO_NOT_SAVE_COOKIES |
+                          net::LOAD_BYPASS_CACHE | net::LOAD_DISABLE_CACHE;
+    request->headers.SetHeader("Content-Type", "application/json");
+    simple_url_loader_ = network::SimpleURLLoader::Create(
+        std::move(request), kTorBuiltinBridgesMoatAnnotation);
+    simple_url_loader_->DownloadToString(
+        url_loader_factory_.get(),
+        base::BindOnce(&BuiltinBridgesRequest::OnResponse,
+                       weak_factory_.GetWeakPtr()),
+        kMaxBodySize);
+  }
+
+  void OnResponse(std::optional<std::string> response_body) {
+    simple_url_loader_.reset();
+    if (!response_body) {
+      OnDataParsed(base::unexpected("Request has failed."));
+    } else {
+      data_decoder::DataDecoder::ParseJsonIsolated(
+          *response_body, base::BindOnce(&BuiltinBridgesRequest::OnDataParsed,
+                                         weak_factory_.GetWeakPtr()));
+    }
+  }
+
+  void OnDataParsed(data_decoder::DataDecoder::ValueOrError value) {
+    if (!value.has_value() || !value->is_dict()) {
+      return std::move(result_callback_).Run(base::Value::Dict());
+    }
+    std::move(result_callback_).Run(value->GetDict());
+  }
+
+  scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
+  std::unique_ptr<network::SimpleURLLoader> simple_url_loader_;
+  ResultCallback result_callback_;
+  base::WeakPtrFactory<BuiltinBridgesRequest> weak_factory_{this};
+};
+
 TorProfileServiceImpl::TorProfileServiceImpl(
+    content::BrowserContext* original_context,
     content::BrowserContext* context,
     PrefService* local_state,
     BraveTorClientUpdater* tor_client_updater,
@@ -140,6 +230,11 @@ TorProfileServiceImpl::TorProfileServiceImpl(
   }
 
   pref_change_registrar_.Init(local_state_.get());
+
+  builtin_bridges_request_ = BuiltinBridgesRequest::MaybeUpdateBuiltinBridges(
+      original_context, local_state,
+      base::BindOnce(&TorProfileServiceImpl::OnBuiltinBridgesResponse,
+                     weak_ptr_factory_.GetWeakPtr()));
 }
 
 TorProfileServiceImpl::~TorProfileServiceImpl() {
@@ -157,8 +252,9 @@ TorProfileServiceImpl::~TorProfileServiceImpl() {
 }
 
 void TorProfileServiceImpl::OnExecutableReady(const base::FilePath& path) {
-  if (path.empty())
+  if (path.empty()) {
     return;
+  }
 
   if (tor_launcher_factory_->GetTorPid() < 0) {
     LaunchTor();
@@ -166,8 +262,9 @@ void TorProfileServiceImpl::OnExecutableReady(const base::FilePath& path) {
 }
 
 void TorProfileServiceImpl::OnPluggableTransportReady(bool success) {
-  if (!success || !tor_launcher_factory_)
+  if (!success || !tor_launcher_factory_) {
     return;
+  }
 
 #if DCHECK_IS_ON()
   // Check we can touch pluggable transport executables from the tor's working
@@ -181,8 +278,9 @@ void TorProfileServiceImpl::OnPluggableTransportReady(bool success) {
       base::BindOnce(
           [](const base::FilePath& tor, const base::FilePath& snowflake_path,
              const base::FilePath& obfs4_path) {
-            if (tor.empty())
+            if (tor.empty()) {
               return;
+            }
             const auto tor_path = tor.DirName();
             DCHECK(base::PathExists(tor_path.Append(snowflake_path)));
             DCHECK(base::PathExists(tor_path.Append(obfs4_path)));
@@ -197,8 +295,9 @@ void TorProfileServiceImpl::OnBridgesConfigChanged() {
   auto config = tor::BridgesConfig::FromDict(
                     local_state_->GetDict(tor::prefs::kBridgesConfig))
                     .value_or(tor::BridgesConfig());
-  if (!tor_pluggable_transport_updater_)
+  if (!tor_pluggable_transport_updater_) {
     return;
+  }
 
   if (!tor_pluggable_transport_updater_->IsReady()) {
     if (config.use_bridges != tor::BridgesConfig::Usage::kNotUsed) {
@@ -216,6 +315,19 @@ void TorProfileServiceImpl::OnBridgesConfigChanged() {
       tor_pluggable_transport_updater_->GetSnowflakeExecutable(),
       tor_pluggable_transport_updater_->GetObfs4Executable());
   tor_launcher_factory_->SetupBridges(std::move(config));
+}
+
+void TorProfileServiceImpl::OnBuiltinBridgesResponse(
+    const base::Value::Dict& bridges) {
+  if (!bridges.empty()) {
+    auto config = tor::BridgesConfig::FromDict(
+                      local_state_->GetDict(tor::prefs::kBridgesConfig))
+                      .value_or(tor::BridgesConfig());
+    config.UpdateBuiltinBridges(bridges);
+    local_state_->SetDict(tor::prefs::kBridgesConfig, config.ToDict());
+  }
+
+  builtin_bridges_request_.reset();
 }
 
 void TorProfileServiceImpl::LaunchTor() {
@@ -293,8 +405,9 @@ void TorProfileServiceImpl::SetNewTorCircuit(WebContents* tab) {
 }
 
 void TorProfileServiceImpl::KillTor() {
-  if (tor_launcher_factory_)
+  if (tor_launcher_factory_) {
     tor_launcher_factory_->KillTorProcess();
+  }
   UnregisterTorClientUpdater();
 }
 
@@ -324,15 +437,17 @@ TorProfileServiceImpl::CreateProxyConfigService() {
 }
 
 bool TorProfileServiceImpl::IsTorConnected() {
-  if (!tor_launcher_factory_)
+  if (!tor_launcher_factory_) {
     return false;
+  }
   return tor_launcher_factory_->IsTorConnected();
 }
 
 void TorProfileServiceImpl::SetTorLauncherFactoryForTest(
     TorLauncherFactory* factory) {
-  if (!factory)
+  if (!factory) {
     return;
+  }
   tor_launcher_factory_ = factory;
 }
 
