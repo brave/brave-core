6aeba28c3ad36748e34a4d24d3238eee35deddaf
ajayseeker <ajay257223@gmail.com>
2024-05-03 12:37:13 +0530

    made changes in brave specific code to ignore ctrl+w when active tab is pinned

--- browser/ui/brave_browser_command_controller.cc
+++ browser/ui/brave_browser_command_controller.cc
@@ -13,6 +13,7 @@
 #include "base/containers/contains.h"
 #include "base/debug/stack_trace.h"
 #include "base/feature_list.h"
+#include "base/metrics/user_metrics.h"
 #include "base/notreached.h"
 #include "base/types/to_address.h"
 #include "brave/app/brave_command_ids.h"
@@ -76,6 +77,7 @@ bool IsBraveOverrideCommands(int id) {
   static std::vector<int> override_commands({
       IDC_NEW_WINDOW,
       IDC_NEW_INCOGNITO_WINDOW,
+      IDC_CLOSE_TAB,
   });
   return base::Contains(override_commands, id);
 }
@@ -657,6 +659,15 @@ bool BraveBrowserCommandController::ExecuteBraveCommandWithDisposition(
       break;
     case IDC_BREAK_TILE:
       brave::BreakTiles(&*browser_);
+      break;
+    case IDC_CLOSE_TAB:
+      {
+        int activeTabIndex = browser_->tab_strip_model()->active_index();
+        if( !browser_->tab_strip_model()->IsTabPinned(activeTabIndex) ) {
+          base::RecordAction(base::UserMetricsAction("CloseTabByKey"));
+          CloseTab(&*browser_);
+        }
+      }
       break;
     default:
       LOG(WARNING) << "Received Unimplemented Command: " << id;
