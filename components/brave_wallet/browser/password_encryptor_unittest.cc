60e97e3e460a5f1190a490f352adbe6d95a7b846
Anton Paymyshev <anton.paymyshev@gmail.com>
2024-05-06 14:30:19 +0700

    fix review

--- components/brave_wallet/browser/password_encryptor_unittest.cc
+++ components/brave_wallet/browser/password_encryptor_unittest.cc
@@ -3,11 +3,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+#include "brave/components/brave_wallet/browser/password_encryptor.h"
+
 #include <string_view>
 
 #include "base/base64.h"
 #include "base/test/values_test_util.h"
-#include "brave/components/brave_wallet/browser/password_encryptor.h"
 #include "crypto/aead.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
