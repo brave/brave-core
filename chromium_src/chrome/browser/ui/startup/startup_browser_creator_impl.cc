fa43d0a2b14620847f36955c0f916e6512e0ab29
Emerick Rogul <erogul@brave.com>
2024-05-08 09:59:53 -0400

    Continue to show updater PromotionInfoBar immediately at startup on Mac
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/153730cf46e506f9306cee4896f66a8314661188
    
    commit 153730cf46e506f9306cee4896f66a8314661188
    Author: Joshua Pawlicki <waffles@chromium.org>
    Date:   Tue Apr 16 15:14:57 2024 +0000
    
        Reland "Chrome/Updater: Use the updater scheduler to ensure updater presence."
    
        This is a reland of commit 6c8d9f199eb403213d463b85787040c0f4736580
        The original CL is patchset 1.
    
        Original change's description:
        > Chrome/Updater: Use the updater scheduler to ensure updater presence.
        >
        > The goal of this CL is to simplify and consolidate the number of flows
        > that lead to installation of the updater. This reduces contention on
        > the updater locks, etc, and may improve the startup performance of
        > Chrome and the reliability of the promotion infobar. These changes have
        > the following effects:
        >
        > 1. Installation of the updater and registration of Chrome will happen
        >    19 seconds after browser startup, rather than twice during message
        >    loop and UI pre-initialization.
        > 2. The promotion infobar will be shown at that time, if necessary,
        >    rather than right away. This is beneficial in some cases since
        >    previously the infobar wouldn't be shown in cases where the profile
        >    selection dialog is shown.
        > 3. The infobar will be shown on the first run of Chrome if needed.
        > 4. The infobar will shown several hours into the browsing session if
        >    it did not previously show, or if things have changed and promotion
        >    is now needed.
        >
        > There is still one other path to EnsureUpdater that I did not alter
        > here - if Chrome is restarted with a chrome://help tab open, that tab
        > will initiate an on-demand update check, installing the updater if
        > necessary. Multiple such tabs might cause contention on the locks, but
        > do not cause any errors. Performance issues associated with that seem
        > infrequent and don't justify additional complexity in my opinion.
        >
        > Fixed: b/332911252
        > Change-Id: Ibdef62b1807f25e7cddaf8f41ab37ca2499cf969
        > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5425369
        > Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
        > Reviewed-by: Mark Mentovai <mark@chromium.org>
        > Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
        > Reviewed-by: Greg Thompson <grt@chromium.org>
        > Cr-Commit-Position: refs/heads/main@{#1287969}
    
        Change-Id: If70f187f22e2ea3b557d6430e5af5afcfe3d88fe

--- chromium_src/chrome/browser/ui/startup/startup_browser_creator_impl.cc
+++ chromium_src/chrome/browser/ui/startup/startup_browser_creator_impl.cc
@@ -3,8 +3,29 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "build/build_config.h"
+#include "chrome/browser/buildflags.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/signin/public/base/signin_buildflags.h"
+
+#if BUILDFLAG(ENABLE_UPDATER)
+#include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/cocoa/keystone_infobar_delegate.h"
+#include "chrome/browser/ui/startup/startup_types.h"
+#endif
+
+#if BUILDFLAG(IS_MAC) && BUILDFLAG(ENABLE_UPDATER)
+namespace {
+void MaybeShowPromotionInfoBar(
+    chrome::startup::IsProcessStartup process_startup) {
+  if (process_startup == chrome::startup::IsProcessStartup::kYes) {
+    // Check whether the auto-update system needs to be promoted from user
+    // to system.
+    ShowUpdaterPromotionInfoBar();
+  }
+}
+}  // namespace
+#endif
 
 // This override is in place to make sure the first run page from Chromium is
 // not shown, as Brave has its own first run page
@@ -16,5 +37,17 @@
     has_first_run_experience = !onboarding_tabs.empty();                \
   }
 
+#if BUILDFLAG(IS_MAC) && BUILDFLAG(ENABLE_UPDATER)
+#define GetLastActive                         \
+  GetLastActive();                            \
+  MaybeShowPromotionInfoBar(process_startup); \
+  BrowserList::GetInstance()->GetLastActive
+#endif
+
 #include "src/chrome/browser/ui/startup/startup_browser_creator_impl.cc"
+
+#if BUILDFLAG(IS_MAC) && BUILDFLAG(ENABLE_UPDATER)
+#undef GetLastActive
+#endif
+
 #undef BRAVE_STARTUPBROWSERCREATORIMPL_DETERMINESTARTUPTABS
