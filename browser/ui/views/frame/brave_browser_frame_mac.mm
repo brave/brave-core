fbf12017cac328d720f8cce767901211da6b5ced
ajayseeker <ajay257223@gmail.com>
2024-05-09 10:35:59 +0530

    formatted a comment

--- browser/ui/views/frame/brave_browser_frame_mac.mm
+++ browser/ui/views/frame/brave_browser_frame_mac.mm
@@ -62,10 +62,10 @@ bool BraveBrowserFrameMac::ExecuteCommand(
     int32_t command,
     WindowOpenDisposition window_open_disposition,
     bool is_before_first_responder) {
-    /* is_before_first_responder tells whether or not the app/window was in focus
-     * while the keyboard command was fired. In current method, it helps in
-     * distinguishing the 'file -> close tab' (false, as toolbar was in focus)
-     * command from 'ctrl + w' (true, as tab was in focus) command. */
+  // is_before_first_responder tells whether or not the app/window was in focus
+  // while the keyboard command was fired. In current method, it helps in
+  // distinguishing the 'file -> close tab' (false, as toolbar was in focus)
+  // command from 'ctrl + w' (true, as tab was in focus) command.
   Browser* browser = browser_view_->browser();
   int active_tab_index = browser->tab_strip_model()->active_index();
   if (command == IDC_CLOSE_TAB && is_before_first_responder &&
