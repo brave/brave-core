8854b9b719ad7776dc45829abc29155f9f0d4138
Josh Leonard <30185185+josheleonard@users.noreply.github.com>
2024-05-09 13:05:12 +0100

    fix: update design system variable imports

--- components/brave_wallet_ui/components/extension/pending-transaction-network-fee/pending-transaction-network-fee.style.tsx
+++ components/brave_wallet_ui/components/extension/pending-transaction-network-fee/pending-transaction-network-fee.style.tsx
@@ -4,7 +4,7 @@
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
 import styled from 'styled-components'
-import * as leo from '@brave/leo/tokens/css'
+import * as leo from '@brave/leo/tokens/css/variables'
 import { SettingsAdvancedIcon } from 'brave-ui/components/icons'
 
 import { WalletButton } from '../../shared/style'
