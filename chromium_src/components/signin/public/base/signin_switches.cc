4c99b7c06fea8cf93671ed88072af9b8af0c1b96
Emerick Rogul <erogul@brave.com>
2024-05-10 10:10:42 -0400

    Signin features were merged into signin_switches.cc
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/028530eff08eb34bb29bcda1a0d3b77348467ffe
    
    commit 028530eff08eb34bb29bcda1a0d3b77348467ffe
    Author: Boris Sazonov <bsazonov@chromium.org>
    Date:   Thu May 2 15:10:00 2024 +0000
    
        Merge signin_features.{h,cc} into signin_switches.{h,cc}
    
        No behavior changes.
    
        Bug: None

--- chromium_src/components/signin/public/base/signin_switches.cc
+++ chromium_src/components/signin/public/base/signin_switches.cc
@@ -3,15 +3,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "chrome/browser/signin/signin_features.h"
-
-#include "src/chrome/browser/signin/signin_features.cc"
+#include "src/components/signin/public/base/signin_switches.cc"
 
 #include "base/feature_override.h"
 
 OVERRIDE_FEATURE_DEFAULT_STATES({{
-#if BUILDFLAG(ENABLE_MIRROR)
+#if BUILDFLAG(ENABLE_MIRROR) && !BUILDFLAG(IS_IOS)
     {kVerifyRequestInitiatorForMirrorHeaders,
      base::FEATURE_DISABLED_BY_DEFAULT},
-#endif  // BUILDFLAG(ENABLE_MIRROR)
+#endif  // BUILDFLAG(ENABLE_MIRROR) && !BUILDFLAG(IS_IOS)
 }});
