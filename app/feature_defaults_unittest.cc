0ec262f514854d97c2983c711e418abcabca86c2
Claudio DeSouza <cdesouza@brave.com>
2024-05-13 20:37:44 +0100

    `kOmniboxTriggerForNoStatePrefetch` deleted upstream
    The flag has been deleted with the feature, as prerender2 is the new
    favoured way.
    
    Chromium chnange:
    https://chromium.googlesource.com/chromium/src/+/00502dc6a135995ad944d9f85c611c8445cabf3c
    
    commit 00502dc6a135995ad944d9f85c611c8445cabf3c
    Author: Hiroki Nakagawa <nhiroki@chromium.org>
    Date:   Fri May 10 02:18:13 2024 +0000
    
        NoStatePrefetch: Remove unused Omnibox trigger
    
        Omnibox no longer uses NoStatePrefetch in favor of Prerender2.
    
        OBSOLETE_HISTOGRAMS=NoStatePrefetch on Omnibox is superseded by Prerender2
    
        Change-Id: If6fc14baf4accefdd2fa0c640993a20821354e33
        Bug: 335028973

--- app/feature_defaults_unittest.cc
+++ app/feature_defaults_unittest.cc
@@ -160,7 +160,6 @@ TEST(FeatureDefaultsTest, DisabledFeatures) {
       &features::kKAnonymityServiceOHTTPRequests,
       &features::kNewTabPageTriggerForPrerender2,
       &features::kNotificationTriggers,
-      &features::kOmniboxTriggerForNoStatePrefetch,
       &features::kOmniboxTriggerForPrerender2,
 #if BUILDFLAG(IS_ANDROID)
       &features::kPrivacyGuidePreloadAndroid,
