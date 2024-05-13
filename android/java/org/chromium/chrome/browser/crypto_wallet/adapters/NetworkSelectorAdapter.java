c9940ec1d9591a562a0159dce2bd58d3ecb509f5
Simone Arpe <simon.arpe@gmail.com>
2024-05-13 11:53:59 +0200

    Remove unused import

--- android/java/org/chromium/chrome/browser/crypto_wallet/adapters/NetworkSelectorAdapter.java
+++ android/java/org/chromium/chrome/browser/crypto_wallet/adapters/NetworkSelectorAdapter.java
@@ -11,7 +11,6 @@ import android.graphics.ColorMatrixColorFilter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
