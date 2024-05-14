8afe153872d5b4d43f1f1ff9bde73d78349a715f
Taher <8665427+nullhook@users.noreply.github.com>
2024-05-14 08:20:23 -0700

    aichat: dont lose suggested questions when toggling

--- components/ai_chat/resources/page/state/data-context-provider.tsx
+++ components/ai_chat/resources/page/state/data-context-provider.tsx
@@ -217,7 +217,6 @@ function DataContextProvider (props: DataContextProviderProps) {
 
   const updateShouldSendPageContents = (shouldSend: boolean) => {
     setShouldSendPageContents(shouldSend)
-    getPageHandlerInstance().pageHandler.setShouldSendPageContents(shouldSend)
   }
 
   const getShouldSendPageContents = () => {
@@ -309,6 +308,10 @@ function DataContextProvider (props: DataContextProviderProps) {
     if (isCharLimitExceeded) return
     if (shouldDisableUserInput) return
     if (handleFilterActivation()) return
+
+    if (conversationHistory.length === 0) {
+      getPageHandlerInstance().pageHandler.setShouldSendPageContents(shouldSendPageContents)
+    }
 
     if (selectedActionType) {
       getPageHandlerInstance().pageHandler.submitHumanConversationEntryWithAction(inputText, selectedActionType)
