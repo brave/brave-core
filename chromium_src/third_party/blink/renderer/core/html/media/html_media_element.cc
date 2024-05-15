497b79418b72bef9ac3833e3e3f4bea55aed0cf2
Szilard Szaloki <szilardszaloki@gmail.com>
2024-05-15 16:22:17 -0500

    patches ==> chromium_src

--- chromium_src/third_party/blink/renderer/core/html/media/html_media_element.cc
+++ chromium_src/third_party/blink/renderer/core/html/media/html_media_element.cc
@@ -4,6 +4,7 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "third_party/blink/renderer/core/html/media/autoplay_policy.h"
+#include "third_party/blink/renderer/core/html/media/html_media_element.h"
 
 // Do not pause video if it was blocked by autoplay policy on volume change.
 bool SkipPauseIfAutoplayIsBlockedByPolicy(bool is_gesture_needed) {
@@ -17,5 +18,16 @@ bool SkipPauseIfAutoplayIsBlockedByPolicy(bool is_gesture_needed) {
       SkipPauseIfAutoplayIsBlockedByPolicy(                  \
           autoplay_policy_->IsGestureNeededForPlayback()) && \
       EffectiveMediaVolume
+
+#define OnLoadFinished()                                                   \
+  OnLoadFinished();                                                        \
+  for (auto& observer : media_player_observer_remote_set_->Value()) {      \
+    observer->OnMediaLoaded(                                               \
+        current_src_.GetSource(),                                          \
+        GetLoadType() == WebMediaPlayer::kLoadTypeMediaSource, duration_); \
+  }
+
 #include "src/third_party/blink/renderer/core/html/media/html_media_element.cc"
+
+#undef OnLoadFinished
 #undef RequestAutoplayUnmute
