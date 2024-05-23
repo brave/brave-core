34800f85573700a734a5423028f9559708108a2a
Emerick Rogul <erogul@brave.com>
2024-05-23 15:46:47 -0400

    Set use_bubble_border = true in menu_config to re-enable rounded menus

--- chromium_src/ui/views/controls/menu/menu_config.cc
+++ chromium_src/ui/views/controls/menu/menu_config.cc
@@ -28,6 +28,7 @@ const MenuConfig& MenuConfig::instance() {
       mutable_config.item_horizontal_padding =
           16 - config.item_horizontal_border_padding;
       mutable_config.corner_radius = 8;
+      mutable_config.use_bubble_border = true;
     }
   } const run_once(config);
 
