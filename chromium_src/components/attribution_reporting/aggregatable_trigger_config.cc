3ab24b9a94dcbc394a3fd55baefa5da4c461ed87
Claudio DeSouza <cdesouza@brave.com>
2024-05-23 18:10:52 +0100

    `kAttributionReportingTriggerContextId` deleted upstream
    Since `kAttributionReportingTriggerContextId` is now deleted upstream, stub out
    AggregatableTriggerConfig to achieve the same effect (approach verified with
    Brave security team).
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/b2a7eed342dce867df33aece233e838210a5f3b0
    
    commit b2a7eed342dce867df33aece233e838210a5f3b0
    Author: Nan Lin <linnan@chromium.org>
    Date:   Mon May 20 12:45:08 2024 +0000
    
        Remove feature kAttributionReportingTriggerContextId
    
        The feature was default enabled in M121.
    
        Change-Id: I5bd76749ac97eb1e5e9eebfc77c190f7104f1afa
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5548744
        Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
        Commit-Queue: Nan Lin <linnan@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1303274}

---
+++ chromium_src/components/attribution_reporting/aggregatable_trigger_config.cc
@@ -0,0 +1,22 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "components/attribution_reporting/aggregatable_trigger_config.h"
+
+#include <optional>
+
+#define Create Create_Unused
+#include "src/components/attribution_reporting/aggregatable_trigger_config.cc"
+#undef Create
+
+namespace attribution_reporting {
+
+std::optional<AggregatableTriggerConfig> AggregatableTriggerConfig::Create(
+    mojom::SourceRegistrationTimeConfig,
+    std::optional<std::string> trigger_context_id) {
+  return std::nullopt;
+}
+
+}  // namespace attribution_reporting
---
+++ chromium_src/components/attribution_reporting/aggregatable_trigger_config.h
@@ -0,0 +1,21 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_ATTRIBUTION_REPORTING_AGGREGATABLE_TRIGGER_CONFIG_H_
+#define BRAVE_CHROMIUM_SRC_COMPONENTS_ATTRIBUTION_REPORTING_AGGREGATABLE_TRIGGER_CONFIG_H_
+
+#include "components/attribution_reporting/source_registration_time_config.mojom.h"
+#include "components/attribution_reporting/trigger_registration_error.mojom-forward.h"
+
+#define Create                                                  \
+  Create_Unused(mojom::SourceRegistrationTimeConfig,            \
+                std::optional<std::string> trigger_context_id); \
+  static std::optional<AggregatableTriggerConfig> Create
+
+#include "src/components/attribution_reporting/aggregatable_trigger_config.h"  // IWYU pragma: export
+
+#undef Create
+
+#endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_ATTRIBUTION_REPORTING_AGGREGATABLE_TRIGGER_CONFIG_H_
