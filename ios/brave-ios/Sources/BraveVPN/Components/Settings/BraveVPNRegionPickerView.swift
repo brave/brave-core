7425eb2ce14aee91ddb1fb1a9dfc29186075d7a9
Soner Yuksel <grkmyksl@gmail.com>
2024-05-27 17:06:00 -0400

    Adding actions to list region automation

--- ios/brave-ios/Sources/BraveVPN/Components/Settings/BraveVPNRegionPickerView.swift
+++ ios/brave-ios/Sources/BraveVPN/Components/Settings/BraveVPNRegionPickerView.swift
@@ -7,6 +7,7 @@ import SwiftUI
 
 public struct BraveVPNRegionPickerView: View {
   @State public var isAutomatic: Bool
+  @State private var isRegionDetailsPresented = false
 
   public var serverRegions: [ServerRegion] = [
     ServerRegion(name: "Australia", servers: 5),
@@ -33,60 +34,75 @@ public struct BraveVPNRegionPickerView: View {
   }
 
   public var body: some View {
-    List {
-      Section(
-        footer: Text(
-          "A server region most proximate to you will be automatically selected, based on your system timezone. This is recommended in order to ensure fast internet speeds."
-        )
-        .foregroundColor(Color(.black))
-      ) {
-        Toggle(
-          "Automatic",
-          isOn: Binding(
-            get: { isAutomatic },
-            set: { toggledBiometricsUnlock($0) }
-          )
-        )
-        .foregroundColor(Color(.black))
-        .toggleStyle(SwitchToggleStyle(tint: Color(.blue)))
-        .listRowBackground(Color(.white))
-      }
-
-      if !isAutomatic {
-        Section {
-          ForEach(serverRegions) { region in
-            HStack {
-              Image(systemName: "flag.2.crossed")
-
-              VStack(alignment: .leading) {
-                Text("\(region.name)")
-
-                Text("\(region.servers) servers")
-                  .foregroundColor(.gray)
-              }
-              Spacer()
-
-              if region.isConnected {
-                Text("Connected")
-                  .foregroundColor(.green)
-              }
-
-              Button(
-                action: {
-                },
-                label: {
-                  Image(systemName: "info.circle")
-                }
+    NavigationStack {
+      VStack {
+        List {
+          Section(
+            footer: Text("A server region most proximate to you will be automatically selected, based on your system timezone. This is recommended in order to ensure fast internet speeds.")
+              .foregroundColor(Color(.black))
+          ) {
+            Toggle(
+              "Automatic",
+              isOn: Binding(
+                get: { isAutomatic },
+                set: { enableAutomaticServer($0) }
               )
+            )
+            .foregroundColor(Color(.black))
+            .toggleStyle(SwitchToggleStyle(tint: Color(.blue)))
+            .listRowBackground(Color(.white))
+          }
+          
+          if !isAutomatic {
+            Section {
+              ForEach(Array(serverRegions.enumerated()), id: \.offset) { index, region in
+                Button {
+                  print("Index Outside \(index)")
+                } label: {
+                  HStack {
+                    Image(systemName: "flag.2.crossed")
+                    VStack(alignment: .leading) {
+                      Text("\(region.name)")
+                      Text("\(region.servers) servers servers serverss servers servers servers")
+                        .foregroundColor(.gray)
+                    }
+                    Spacer()
+                    if region.isConnected {
+                      Text("Connected")
+                        .foregroundColor(.green)
+                    }
+                    infoButtonView(index: index)
+                      .hidden()
+                  }
+                }
+                .overlay(alignment: .trailing) {
+                  infoButtonView(index: index)
+                }
+              }
             }
+            .listRowBackground(Color(.white))
           }
         }
-        .listRowBackground(Color(.white))
+        .navigationDestination(isPresented: $isRegionDetailsPresented) {
+          BraveRegionDetailsView()
+        }
       }
     }
   }
+  
+  private func infoButtonView(index: Int) -> some View {
+    Button(
+      action: {
+        isRegionDetailsPresented = true
+      },
+      label: {
+        Image(systemName: "info.circle")
+      }
+    )
+    .buttonStyle(.plain)
+  }
 
-  private func toggledBiometricsUnlock(_ enabled: Bool) {
+  private func enableAutomaticServer(_ enabled: Bool) {
     isAutomatic = enabled
   }
 }
