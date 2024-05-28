d7ed5b401247757182ce16d9128618833a1df0e8
bridiver <34129+bridiver@users.noreply.github.com>
2024-05-28 15:20:57 -0700

    re-add missing BraveContentClient override

---
+++ chromium_src/chrome/app/chrome_main_delegate.h
@@ -0,0 +1,16 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_APP_CHROME_MAIN_DELEGATE_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_APP_CHROME_MAIN_DELEGATE_H_
+
+#include "brave/common/brave_content_client.h"
+#include "chrome/common/chrome_content_client.h"
+
+#define ChromeContentClient BraveContentClient
+#include "src/chrome/app/chrome_main_delegate.h"  // IWYU pragma: export
+#undef ChromeContentClient
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_APP_CHROME_MAIN_DELEGATE_H_
