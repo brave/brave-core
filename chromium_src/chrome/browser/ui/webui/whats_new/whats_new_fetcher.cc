a29d9ecd970f08f955e051c14b571ed360e8466a
Claudio DeSouza <cdesouza@brave.com>
2024-05-29 23:08:21 +0100

    Reanchor `StartWhatsNewFetch` overrides
    This function has been moved into a new file, and a corresponding
    overriding file must be created to re-apply the substitution.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/99defa39a5dee2988c8f0ba87fef142b41a625ed
    
    commit 99defa39a5dee2988c8f0ba87fef142b41a625ed
    Author: rbpotter <rbpotter@chromium.org>
    Date:   Tue May 28 20:04:07 2024 +0000
    
        What's New: Remove page on ChromeOS
    
        Due to differences in the ChromeOS update process (specifically,
        browser and OS updating together) vs other platforms, What's New will
        not be launching on ChromeOS any time soon. Instead of including all
        the page code on ChromeOS but having the page disabled there, remove
        most of the code on ChromeOS platforms. Leave limited functions in
        whats_new_util to determine e.g. whether the page is enabled, to avoid
        excessive #if statements in code outside of c/b/ui/webui/whats_new.
    
        This CL refactors code from whats_new_util to whats_new_fetcher, so
        that the latter can hold network code that is not needed on CrOS
        platforms. Code making direct network requests is stubbed out in tests
        as this causes tests to flake (i.e. hard to test).
    
        Low-Coverage-Reason: HARD_TO_TEST
        Bug: 339711041

--- chromium_src/chrome/browser/ui/webui/whats_new/whats_new_fetcher.cc
+++ chromium_src/chrome/browser/ui/webui/whats_new/whats_new_fetcher.cc
@@ -3,19 +3,18 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "chrome/browser/ui/webui/whats_new/whats_new_util.h"
+#include "chrome/browser/ui/webui/whats_new/whats_new_fetcher.h"
 
 #include "brave/browser/ui/whats_new/whats_new_util.h"
 
-#define ShouldShowForState ShouldShowForState_UnUsed
-#include "src/chrome/browser/ui/webui/whats_new/whats_new_util.cc"
-#undef ShouldShowForState
+#define StartWhatsNewFetch StartWhatsNewFetch_UnUsed
+#include "src/chrome/browser/ui/webui/whats_new/whats_new_fetcher.cc"
+#undef StartWhatsNewFetch
 
 namespace whats_new {
 
-bool ShouldShowForState(PrefService* local_state,
-                        bool promotional_tabs_enabled) {
-  return ShouldShowBraveWhatsNewForState(local_state);
+void StartWhatsNewFetch(Browser* browser) {
+  StartBraveWhatsNew(browser);
 }
 
 }  // namespace whats_new
