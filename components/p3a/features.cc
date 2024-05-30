69d54d72efd7b364d71c630801a13a578ffbff96
Ralph Giles <rgiles@brave.com>
2024-05-29 17:18:46 -0700

    Add documentation comments for Nebula constants
    Describe where the new constants come from and link to the original
    issue which describes the protocol design.

--- components/p3a/features.cc
+++ components/p3a/features.cc
@@ -8,18 +8,27 @@
 
 namespace p3a::features {
 
+// Report answers to P3A questions encrypted to the STAR/Constellation
+// threshold aggregation scheme.
 BASE_FEATURE(kConstellation,
              "BraveP3AConstellation",
              base::FEATURE_ENABLED_BY_DEFAULT);
+// Verify Constellation randomness server secure enclave certificate.
 BASE_FEATURE(kConstellationEnclaveAttestation,
              "BraveP3AConstellationEnclaveAttestation",
              base::FEATURE_DISABLED_BY_DEFAULT);
+// Disable reporting answers over direct https+json
+// for typical (weekly) cadence P3A questions.
 BASE_FEATURE(kTypicalJSONDeprecation,
              "BraveP3ATypicalJSONDeprecation",
              base::FEATURE_DISABLED_BY_DEFAULT);
+// Disable reporting answers over direct https+json
+// for other (daily or monthly) cadence P3A questions.
 BASE_FEATURE(kOtherJSONDeprecation,
              "BraveP3AOtherJSONDeprecation",
              base::FEATURE_DISABLED_BY_DEFAULT);
+// Report P3A responses with "Nebula" differential privacy
+// sampling enabled. See https://github.com/brave/brave-browser/issues/35841
 BASE_FEATURE(kNebula,
              "BraveP3ADifferentialSampling",
              base::FEATURE_DISABLED_BY_DEFAULT);
