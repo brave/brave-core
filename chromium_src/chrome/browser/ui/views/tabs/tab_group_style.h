2202a444fb93eb22eee35f223a8168f6060054d0
Claudio DeSouza <cdesouza@brave.com>
2024-06-03 18:04:27 +0100

    `ChromeRefresh2023TabGroupStyle` merged into `TabGroupStyle`
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/d97c5a73e614939c32a7fd4c93ea6f37033bd7d6
    
    commit 1a3a63173de17e612af8c0db7367cf39b9a0be18
    Author: Alison Gale <agale@chromium.org>
    Date:   Fri May 31 16:35:27 2024 +0000
    
        Delete kChromeRefresh2023 and kChromeRefreshSecondary2023 flags
    
        #chrome-refresh-2023-cleanup
    
        Fixed: 335903780

--- chromium_src/chrome/browser/ui/views/tabs/tab_group_style.h
+++ chromium_src/chrome/browser/ui/views/tabs/tab_group_style.h
@@ -7,11 +7,7 @@
 #define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_TABS_TAB_GROUP_STYLE_H_
 
 #define TabGroupStyle TabGroupStyle_ChromiumImpl
-#define ChromeRefresh2023TabGroupStyle \
-  ChromeRefresh2023TabGroupStyle_ChromiumImpl
-
 #include "src/chrome/browser/ui/views/tabs/tab_group_style.h"  // IWYU pragma: export
-#undef ChromeRefresh2023TabGroupStyle
 #undef TabGroupStyle
 
 class TabGroupStyle : public TabGroupStyle_ChromiumImpl {
@@ -32,23 +28,6 @@ class TabGroupStyle : public TabGroupStyle_ChromiumImpl {
 
  private:
   bool ShouldShowVerticalTabs() const;
-};
-
-// Clobber ChromeRefresh2023TabGroupStyle for now because of how
-// chrome/browser/ui/views/tabs/tab_group_views.cc wants to instantiate the
-// style:
-// style_ = features::IsChromeRefresh2023()
-//   ? std::make_unique<const ChromeRefresh2023TabGroupStyle>(*this)
-//   : std::make_unique<const TabGroupStyle>(*this);
-// and ChromeRefresh2023TabGroupStyle is no longer inheriting from TabGroupStyle
-// due to the above redefine.
-class ChromeRefresh2023TabGroupStyle : public TabGroupStyle {
- public:
-  using TabGroupStyle::TabGroupStyle;
-
-  // A necessary stub to go back to the original class, as this static gets used
-  // by `TabGroupUnderline::GetStrokeInset`.
-  static int GetTabGroupOverlapAdjustment();
 };
 
 #endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_TABS_TAB_GROUP_STYLE_H_
