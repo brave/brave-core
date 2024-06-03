b635262de3afada7d22449f551aea2af97de5976
Artem Samoilenko <artem@brave.com>
2024-06-03 10:24:59 -0400

    [Android] Require @JniType conversions functions to appear in .h files
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/f46caa514f421ef199f1370042147b309728fc8f
    
    JNI Zero: Require @JniType conversions functions to appear in .h files
    
    This has the following advantages:
     * Missing conversions will be a compile-time error rather than
       link-time.
     * Allows us to define a default implementations that maps a pointer
       type to its dereferenced type.
     * Allows us to define a default implementations for std::optional<T>.
    
    Bug: 334905258

--- chromium_src/chrome/browser/android/preferences/about_settings_bridge.cc
+++ chromium_src/chrome/browser/android/preferences/about_settings_bridge.cc
@@ -3,6 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "base/android/jni_string.h"
 #include "brave/components/version_info/version_info.h"
 #include "chrome/android/chrome_jni_headers/AboutSettingsBridge_jni.h"
 
