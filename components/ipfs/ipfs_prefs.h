190f149d62a326cd55b20c618008ed9e6f12c3c8
Vadym Struts <vstruts@brave.com>
2024-06-03 21:06:40 +0200

    format
    Signed-off-by: Vadym Struts <vstruts@brave.com>

--- components/ipfs/ipfs_prefs.h
+++ components/ipfs/ipfs_prefs.h
@@ -1,7 +1,7 @@
-/* Copyright (c) 2024 The Brave Authors. All rights reserved.
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+// Copyright (c) 2024 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
 
 #ifndef BRAVE_COMPONENTS_IPFS_IPFS_PREFS_H_
 #define BRAVE_COMPONENTS_IPFS_IPFS_PREFS_H_
