8ca7bfb65c5fa9b513307f4f07f08dbe098ebc88
Josh Leonard <30185185+josheleonard@users.noreply.github.com>
2024-06-05 11:25:58 -0600

    test: fix flakey jest-transform failures

--- jest.config.js
+++ jest.config.js
@@ -76,7 +76,14 @@ module.exports = {
   ],
   testTimeout: 30000,
   transformIgnorePatterns: [
-    '<rootDir>/node_modules/(?!(@brave/brave-ui|@brave/leo)/)'
+    '<rootDir>/node_modules/(?!(@brave/brave-ui|@brave/leo)/)',
+    // prevent jest from transforming itself
+    // https://github.com/jestjs/jest/issues/9503#issuecomment-709041807
+    '<rootDir>/node_modules/@babel',
+    '<rootDir>/node_modules/@jest',
+    '<rootDir>/node_modules/lodash',
+    'signal-exit',
+    'is-typedarray'
   ],
   setupFilesAfterEnv: ['<rootDir>/components/test/testSetup.ts'],
   moduleNameMapper: {
