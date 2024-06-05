382ec5e214b9516ca5426980105805ee67918004
brave-builds <45370463+brave-builds@users.noreply.github.com>
2024-06-05 07:41:04 +0200

    Switch zcash endpoint url (uplift to 1.66.x) (#23994)
    Uplift of #23981 (squashed) to release

--- components/brave_wallet/browser/brave_wallet_utils_unittest.cc
+++ components/brave_wallet/browser/brave_wallet_utils_unittest.cc
@@ -837,7 +837,7 @@ TEST(BraveWalletUtilsUnitTest, GetNetworkURLTest) {
       GURL("https://test-btc.com"),
       GetNetworkURL(&prefs, mojom::kBitcoinMainnet, mojom::CoinType::BTC));
 
-  EXPECT_EQ(GURL("https://mainnet.lightwalletd.com:9067/"),
+  EXPECT_EQ(GURL("https://zec.rocks:443/"),
             GetNetworkURL(&prefs, mojom::kZCashMainnet, mojom::CoinType::ZEC));
   auto custom_zec_network =
       GetKnownChain(&prefs, mojom::kZCashMainnet, mojom::CoinType::ZEC);
@@ -1013,11 +1013,11 @@ TEST(BraveWalletUtilsUnitTest, GetChain) {
             btc_mainnet.Clone());
 
   // Zcash
-  mojom::NetworkInfo zec_mainnet(
-      mojom::kZCashMainnet, "Zcash Mainnet",
-      {"https://zcashblockexplorer.com/transactions"}, {}, 0,
-      {GURL("https://mainnet.lightwalletd.com:9067/")}, "ZEC", "Zcash", 8,
-      mojom::CoinType::ZEC, {mojom::KeyringId::kZCashMainnet}, false);
+  mojom::NetworkInfo zec_mainnet(mojom::kZCashMainnet, "Zcash Mainnet",
+                                 {"https://3xpl.com/zcash/transaction"}, {}, 0,
+                                 {GURL("https://zec.rocks:443/")}, "ZEC",
+                                 "Zcash", 8, mojom::CoinType::ZEC,
+                                 {mojom::KeyringId::kZCashMainnet}, false);
   EXPECT_FALSE(GetChain(&prefs, "0x123", mojom::CoinType::ZEC));
   EXPECT_EQ(GetChain(&prefs, "zcash_mainnet", mojom::CoinType::ZEC),
             zec_mainnet.Clone());
