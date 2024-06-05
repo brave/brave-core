d2b80eeeae48ce473350428e13c014c97ed7a250
Stephen Heaps <steaps.4@gmail.com>
2024-06-05 16:29:22 -0400

    review(supermassive): restore supported coins logic in case of disabling flag via Griffin.

--- ios/brave-ios/Sources/BraveWallet/WalletConstants.swift
+++ ios/brave-ios/Sources/BraveWallet/WalletConstants.swift
@@ -103,7 +103,23 @@ public struct WalletConstants {
   ) -> OrderedSet<BraveWallet.CoinType> {
     switch mode {
     case .general:
-      return [.eth, .sol, .fil, .btc]
+      #if DEBUG
+      // Only enable .btc for unit tests.
+      // Local Debug build need to
+      // 1. Remove this check
+      // 2. Enable bitcoin feature via build argument
+      if isUnitTesting {
+        return [.eth, .sol, .fil, .btc]
+      }
+      #endif
+      // Any non-debug build will check bitcoin feature flag from core
+      // TF public build can use BraveCore Switches in Browser Settings,
+      // Debug section in order to enable Bitcoin.
+      if FeatureList.kBraveWalletBitcoinFeature.enabled {
+        return [.eth, .sol, .fil, .btc]
+      } else {
+        return [.eth, .sol, .fil]
+      }
     case .dapps:
       return [.eth, .sol]
     }
