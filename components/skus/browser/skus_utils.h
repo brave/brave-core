cc05d92fa585f38f4395bd364781b626fcbbd740
Brian Clifton <brian@clifton.me>
2024-06-06 01:51:03 -0700

    Exit logic in BraveVpnService if called using a non-VPN products.
    Fixes https://github.com/brave/brave-browser/issues/36321
    
    This change adds product check into `LoadPurchasedState` and `OnCredentialSummary`.
    Comments around credential usage were updated to be more clear.
    
    The call to BraveVpnService::LoadPurchasedState was originally added
    to `components/skus/renderer/skus_js_handler.cc` so the VPN Service
    could update its environment (dev, staging, prod) if the person using
    Brave was switching between brave.software / bravesoftware.com / etc.
    
    More info about that and a proposal to create a VPN-specific CLI arg
    can be found here: https://github.com/brave/brave-browser/issues/38883

--- components/skus/browser/skus_utils.h
+++ components/skus/browser/skus_utils.h
@@ -20,6 +20,7 @@ inline constexpr char kEnvDevelopment[] = "development";
 std::string GetDefaultEnvironment();
 std::string GetDomain(const std::string& prefix, const std::string& domain);
 std::string GetEnvironmentForDomain(const std::string& domain);
+bool DomainIsForProduct(const std::string& domain, const std::string& product);
 void RegisterLocalStatePrefs(PrefRegistrySimple* registry);
 void RegisterProfilePrefsForMigration(PrefRegistrySimple* registry);
 void MigrateSkusSettings(PrefService* profile_prefs, PrefService* local_prefs);
