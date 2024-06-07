fd27cb102bf607c93e445e0a9e1afb11b6a1eb8d
Brian Clifton <brian@clifton.me>
2024-06-07 00:35:40 -0700

    Remove unused `Learn more` link from WireGuard fallback
    Fixes https://github.com/brave/brave-browser/issues/38888
    Closes https://github.com/brave/brave-browser/issues/31652

--- browser/ui/views/brave_vpn/brave_vpn_fallback_dialog_view.h
+++ browser/ui/views/brave_vpn/brave_vpn_fallback_dialog_view.h
@@ -33,7 +33,6 @@ class BraveVpnFallbackDialogView : public views::DialogDelegateView {
 
   void OnAccept();
   void OnClosing();
-  void OnLearnMoreLinkClicked();
 
   // views::DialogDelegate overrides:
   ui::ModalType GetModalType() const override;
