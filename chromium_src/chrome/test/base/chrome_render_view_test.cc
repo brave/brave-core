0ea09bf41399ac0497d32221e831c94e2537c6bb
bridiver <34129+bridiver@users.noreply.github.com>
2024-06-09 07:43:04 -0700

    use brave delegates

--- chromium_src/chrome/test/base/chrome_render_view_test.cc
+++ chromium_src/chrome/test/base/chrome_render_view_test.cc
@@ -3,9 +3,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "brave/browser/brave_content_browser_client.h"
 #include "brave/common/brave_content_client.h"
-#include "brave/test/base/testing_brave_browser_process.h"
+#include "brave/renderer/brave_content_renderer_client.h"
 
 #define ChromeContentClient BraveContentClient
-#include "src/chrome/test/base/chrome_unit_test_suite.cc"
+#define ChromeContentBrowserClient BraveContentBrowserClient
+#define ChromeContentRendererClient BraveContentRendererClient
+#include "src/chrome/test/base/chrome_render_view_test.cc"
 #undef ChromeContentClient
+#undef ChromeContentBrowserClient
+#undef ChromeContentRendererClient
