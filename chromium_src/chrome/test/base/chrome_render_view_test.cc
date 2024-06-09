3b552d1d725ba77bff54d4356b240bc58ccf5b46
bridiver <34129+bridiver@users.noreply.github.com>
2024-06-09 09:28:26 -0700

    presubmit fixes

--- chromium_src/chrome/test/base/chrome_render_view_test.cc
+++ chromium_src/chrome/test/base/chrome_render_view_test.cc
@@ -1,7 +1,7 @@
-/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "brave/browser/brave_content_browser_client.h"
 #include "brave/common/brave_content_client.h"
