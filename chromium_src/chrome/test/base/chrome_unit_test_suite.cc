95390f01447b6061c125d6b9edfede4b8b33c3f5
Brian Johnson <34129+bridiver@users.noreply.github.com>
2024-06-11 09:04:00 -0700

    use brave delegates for all tests (#24080)
    Use brave delegates for all tests
    Fixes https://github.com/brave/brave-browser/issues/38964

--- chromium_src/chrome/test/base/chrome_unit_test_suite.cc
+++ chromium_src/chrome/test/base/chrome_unit_test_suite.cc
@@ -1,8 +1,11 @@
 /* Copyright (c) 2021 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+#include "brave/common/brave_content_client.h"
 #include "brave/test/base/testing_brave_browser_process.h"
 
+#define ChromeContentClient BraveContentClient
 #include "src/chrome/test/base/chrome_unit_test_suite.cc"
+#undef ChromeContentClient
