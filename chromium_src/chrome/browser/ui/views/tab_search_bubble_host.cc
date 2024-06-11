6a19145378f74632f8475ae4476331fa9aa68120
Claudio DeSouza <cdesouza@brave.com>
2024-06-11 13:41:53 +0100

    Removing broken `TabSearchBubbleHost` override
    `ShouldTabSearchRenderBeforeTabStrip` has an override that relies on a
    substitution of `IsChromeRefresh2023`, however this call is no more.
    This change introduces a patch, as this function should always be
    returning false for brave.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/b43d6eac96efad39df9ea265fdd566325ad34deb
    
    commit b43d6eac96efad39df9ea265fdd566325ad34deb
    Author: Dana Fried <dfried@chromium.org>
    Date:   Fri Jun 7 00:40:40 2024 +0000
    
        [GM2 Cleanup] tab_search_bubble_host.cc
    
        Moves tab search button to the right in all cases except on Mac.
        This may be temporary as the button may be moving back.
    
        #chrome-refresh-2023-cleanup
    
        Change-Id: I35c5fb7f8d2d48b65726b7dfaa646af96842939a

--- chromium_src/chrome/browser/ui/views/tab_search_bubble_host.cc
+++ chromium_src/chrome/browser/ui/views/tab_search_bubble_host.cc
@@ -6,19 +6,7 @@
 #include "build/build_config.h"
 #include "ui/base/ui_base_features.h"
 
-#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_WIN)
-namespace {
-bool ShouldTabSearchAppearBeforeTabStrip() {
+#define BRAVE_TAB_SEARCH_BUBBLE_HOST_SHOULD_TAB_SEARCH_RENDER_BEFORE_TAB_STRIP \
   return false;
-}
-}  // namespace
-
-#define IsChromeRefresh2023 \
-  IsChromeRefresh2023() && ShouldTabSearchAppearBeforeTabStrip
-#endif
-
 #include "src/chrome/browser/ui/views/tab_search_bubble_host.cc"
-
-#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_WIN)
-#undef IsChromeRefresh2023
-#endif
+#undef BRAVE_TAB_SEARCH_BUBBLE_HOST_SHOULD_TAB_SEARCH_RENDER_BEFORE_TAB_STRIP
