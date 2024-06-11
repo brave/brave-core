3dd1932d1463ac3aa2cd5fb30e41f1b46e886387
Jay Harris <jay.harris@outlook.co.nz>
2024-06-12 11:04:46 +1200

    [NTP Search]: Support direct navigation to URLs (#24117)

--- components/brave_new_tab_ui/components/search/SearchResult.tsx
+++ components/brave_new_tab_ui/components/search/SearchResult.tsx
@@ -7,15 +7,14 @@ import { getLocale } from '$web-common/locale';
 import Icon from '@brave/leo/react/icon';
 import { color, font, gradient, icon, radius, spacing } from '@brave/leo/tokens/css/variables';
 import { mojoString16ToString } from 'chrome://resources/js/mojo_type_util.js';
-import { useUnpaddedImageUrl } from '../../../brave_news/browser/resources/shared/useUnpaddedImageUrl';
 import { AutocompleteMatch } from 'gen/ui/webui/resources/cr_components/searchbox/searchbox.mojom.m';
 import * as React from 'react';
 import styled from 'styled-components';
-import { omniboxController } from './SearchContext';
+import { useUnpaddedImageUrl } from '../../../brave_news/browser/resources/shared/useUnpaddedImageUrl';
 
 interface Props {
   match: AutocompleteMatch
-  line: number
+  onClick: (e: React.MouseEvent) => void
   selected: boolean
 }
 
@@ -52,10 +51,15 @@ const IconContainer = styled.div`
   }
 `
 
-const FavIcon = styled.span<{ url: string }>`
+const SearchIcon = styled.span<{ url: string }>`
   background: rgba(255, 255, 255, 0.5);
   mask-image: url(${p => p.url});
   mask-size: contain;
+`
+
+const FavIcon = styled.span<{ url: string }>`
+  background-image: url(${p => p.url});
+  background-size: contain;
 `
 
 const Content = styled.span`
@@ -94,15 +98,25 @@ function RichImage({ url }: { url: string }) {
   return <img src={iconUrl} style={loaded ? show : hide} />
 }
 function Image({ match, isAskLeo }: { match: AutocompleteMatch, isAskLeo: boolean }) {
+  // AskLeo is a case we treat specially. It's included on most queries.
   if (isAskLeo) return <LeoIcon name='product-brave-leo' />
 
+  // We have three separate cases here:
+  // 1. A chromium generic search result icon:
+  //    We display the icon as a mask-image, so we can change the color
+  // 2. An `imageUrl` with the chrome:// scheme, which we can load on the NTP:
+  //    We display the image as a background, as its safe on the NTP
+  // 3. A web resource, which we need to load from the WebUI via the unpadded
+  //    url machinery.
   const isGeneric = !match.imageUrl
   return isGeneric
-    ? <FavIcon url={match.iconUrl} />
-    : <RichImage url={match.imageUrl} />
+    ? <SearchIcon url={match.iconUrl} />
+    : match.imageUrl.startsWith('chrome')
+      ? <FavIcon url={match.imageUrl} />
+      : <RichImage url={match.imageUrl} />
 }
 
-export default function SearchResult({ match, line, selected }: Props) {
+export default function SearchResult({ match, selected, onClick }: Props) {
   const contents = mojoString16ToString(match.swapContentsAndDescription ? match.description : match.contents)
   const description = mojoString16ToString(match.swapContentsAndDescription ? match.contents : match.description)
   const isAskLeo = description === getLocale('searchAskLeo')
@@ -114,7 +128,7 @@ export default function SearchResult({ match, line, selected }: Props) {
   const subtitle = match.destinationUrl.url || description
   const result = <Container href={match.destinationUrl.url} aria-selected={selected} onClick={e => {
     e.preventDefault()
-    omniboxController.openAutocompleteMatch(line, match.destinationUrl, true, e.button, e.altKey, e.ctrlKey, e.metaKey, e.shiftKey)
+    onClick(e)
   }}>
     <IconContainer>
       <Image key={match.imageUrl ?? match.iconUrl} match={match} isAskLeo={isAskLeo} />
