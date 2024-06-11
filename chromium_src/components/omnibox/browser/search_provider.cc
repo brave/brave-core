4e364e25a14169a308ac647c7023b6891108edb4
brave-builds <brave-builds+devops@brave.com>
2024-06-11 21:09:39 +0000

    Uplift of #24058 (squashed) to beta

--- chromium_src/components/omnibox/browser/search_provider.cc
+++ chromium_src/components/omnibox/browser/search_provider.cc
@@ -4,19 +4,24 @@
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "components/omnibox/browser/search_suggestion_parser.h"
+#include "components/search_engines/search_engine_type.h"
 
 // Pass additional argument to check if it's Brave Search's rich suggestion
-#define ParseSuggestResults(root_list, input, scheme_classifier,         \
-                            default_result_relevance, is_keyword_result, \
-                            results)                                     \
-  ParseSuggestResults(                                                   \
-      root_list, input, scheme_classifier, default_result_relevance,     \
-      is_keyword_result, results,                                        \
-      /*is_brave_rich_suggestion*/ providers_.GetDefaultProviderURL() && \
-          providers_.GetDefaultProviderURL()->GetEngineType(             \
-              client()->GetTemplateURLService()->search_terms_data()) == \
-              SEARCH_ENGINE_BRAVE)
+#define ParseSuggestResults(root_list, input, scheme_classifier,            \
+                            default_result_relevance, is_keyword_result,    \
+                            results)                                        \
+  ParseSuggestResults(root_list, input, scheme_classifier,                  \
+                      default_result_relevance, is_keyword_result, results, \
+                      IsBraveRichSuggestion(is_keyword_result))
 
 #include "src/components/omnibox/browser/search_provider.cc"
 
 #undef ParseSuggestResults
+
+bool SearchProvider::IsBraveRichSuggestion(bool is_keyword) {
+  auto* url = is_keyword ? providers_.GetKeywordProviderURL()
+                         : providers_.GetDefaultProviderURL();
+  return url && url->GetEngineType(
+                    client()->GetTemplateURLService()->search_terms_data()) ==
+                    SEARCH_ENGINE_BRAVE;
+}
