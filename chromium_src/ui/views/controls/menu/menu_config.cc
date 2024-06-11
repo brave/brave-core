b75db3dae615ab8a0f15ee3938272f48c187823b
Simon Hong <shong@brave.com>
2024-06-11 15:12:21 +0900

    Updated upgrade menu item's bg radius, border padding

--- chromium_src/ui/views/controls/menu/menu_config.cc
+++ chromium_src/ui/views/controls/menu/menu_config.cc
@@ -25,6 +25,7 @@ const MenuConfig& MenuConfig::instance() {
       // Each platform sets its own config in its Init().
       // Apply our config globally at once after Init() is done.
       mutable_config.item_vertical_margin = 4;
+      mutable_config.item_horizontal_border_padding = 4;
       mutable_config.item_horizontal_padding =
           16 - config.item_horizontal_border_padding;
       mutable_config.corner_radius = 8;
