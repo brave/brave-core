9eb372cd3d3befe26b15569baa829d9ad403c743
Simon Hong <shong@brave.com>
2024-06-12 11:41:30 +0900

    Fixed crash when opening playlist/chat side panel
    As upstream started to use browser action for each side panel,
    our panel items also should have their panel action items.

---
+++ browser/ui/brave_browser_actions.cc
@@ -0,0 +1,81 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "brave/browser/ui/brave_browser_actions.h"
+
+#include "brave/components/ai_chat/core/common/buildflags/buildflags.h"
+#include "brave/components/playlist/common/buildflags/buildflags.h"
+#include "brave/components/vector_icons/vector_icons.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/views/side_panel/side_panel_action_callback.h"
+#include "chrome/browser/ui/views/side_panel/side_panel_entry_id.h"
+#include "chrome/browser/ui/views/side_panel/side_panel_entry_key.h"
+#include "components/grit/brave_components_strings.h"
+#include "ui/actions/actions.h"
+#include "ui/base/l10n/l10n_util.h"
+#include "ui/gfx/vector_icon_types.h"
+
+#if BUILDFLAG(ENABLE_AI_CHAT)
+#include "brave/components/ai_chat/core/browser/utils.h"
+#endif  // BUILDFLAG(ENABLE_AI_CHAT)
+
+#if BUILDFLAG(ENABLE_PLAYLIST)
+#include "brave/components/playlist/common/features.h"
+#endif  // BUILDFLAG(ENABLE_PLAYLIST)
+
+namespace {
+
+actions::ActionItem::ActionItemBuilder SidePanelAction(
+    SidePanelEntryId id,
+    int title_id,
+    int tooltip_id,
+    const gfx::VectorIcon& icon,
+    actions::ActionId action_id,
+    Browser* browser,
+    bool is_pinnable) {
+  return actions::ActionItem::Builder(CreateToggleSidePanelActionCallback(
+                                          SidePanelEntryKey(id), browser))
+      .SetActionId(action_id)
+      .SetText(l10n_util::GetStringUTF16(title_id))
+      .SetTooltipText(l10n_util::GetStringUTF16(tooltip_id))
+      .SetImage(ui::ImageModel::FromVectorIcon(icon, ui::kColorIcon))
+      .SetProperty(actions::kActionItemPinnableKey, is_pinnable);
+}
+
+}  // namespace
+
+BraveBrowserActions::BraveBrowserActions(Browser& browser)
+    : BrowserActions(browser) {
+  InitializeBraveBrowserActions();
+}
+
+BraveBrowserActions::~BraveBrowserActions() = default;
+
+void BraveBrowserActions::InitializeBraveBrowserActions() {
+  Browser* browser = &(browser_.get());
+
+#if BUILDFLAG(ENABLE_PLAYLIST)
+  if (base::FeatureList::IsEnabled(playlist::features::kPlaylist)) {
+    root_action_item_->AddChild(
+        SidePanelAction(
+            SidePanelEntryId::kPlaylist, IDS_SIDEBAR_PLAYLIST_ITEM_TITLE,
+            IDS_SIDEBAR_PLAYLIST_ITEM_TITLE, kLeoProductPlaylistIcon,
+            kPlaylistSidePanelShowAssistant, browser, true)
+            .Build());
+  }
+#endif
+
+#if BUILDFLAG(ENABLE_AI_CHAT)
+  Profile* profile = browser_->profile();
+  if (ai_chat::IsAIChatEnabled(profile->GetPrefs())) {
+    root_action_item_->AddChild(
+        SidePanelAction(SidePanelEntryId::kChatUI, IDS_CHAT_UI_TITLE,
+                        IDS_CHAT_UI_TITLE, kLeoProductBraveLeoIcon,
+                        kChatUISidePanelShowAssistant, browser, false)
+            .Build());
+  }
+#endif
+}
