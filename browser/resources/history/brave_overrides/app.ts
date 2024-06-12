886c6bf0f56611e2534f39a7f66d78c388891796
brave-builds <brave-builds+devops@brave.com>
2024-06-12 15:45:50 +0000

    Uplift of #24147 (squashed) to beta

---
+++ browser/resources/history/brave_overrides/app.ts
@@ -0,0 +1,20 @@
+// Copyright (c) 2024 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+// @ts-nocheck TODO(petemill): Define types and remove ts-nocheck
+
+import {RegisterStyleOverride} from 'chrome://resources/brave/polymer_overriding.js'
+import {html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js'
+
+RegisterStyleOverride(
+  'history-app',
+  html`
+    <style>
+        #tabs-content, #tabs-content>* {
+            height: 100%; 
+        }
+    </style>
+  `
+)
