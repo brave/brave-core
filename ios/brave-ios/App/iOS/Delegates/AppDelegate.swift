84829431cd6cc3784fa331e28a6849750527a424
Brandon-T <bthomas@brave.com>
2024-06-12 13:54:56 -0400

    [iOS] - Fix tab-loss bug on iPhone (uplift to 1.66.x) (#24156)
    Fix tab-loss bug on iPhone.

--- ios/brave-ios/App/iOS/Delegates/AppDelegate.swift
+++ ios/brave-ios/App/iOS/Delegates/AppDelegate.swift
@@ -424,6 +424,14 @@ extension AppDelegate {
     _ application: UIApplication,
     didDiscardSceneSessions sceneSessions: Set<UISceneSession>
   ) {
+    // Do not discard sessions on iPhones!
+    // There is a bug in the OS where this will be called randomly
+    // especially after launching and closing the app hundreds of times
+    // while also locking the device.
+    if !UIApplication.shared.supportsMultipleScenes {
+      return
+    }
+
     // Called when the user discards a scene session.
     // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
     // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
