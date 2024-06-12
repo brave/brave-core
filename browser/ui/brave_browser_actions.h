ac7a187a3a5dd81ece762cad088ba634fad36fb7
Simon Hong <shong@brave.com>
2024-06-12 11:41:30 +0900

    Fixed crash when opening playlist/chat side panel
    As upstream started to use browser action for each side panel,
    our panel items also should have their panel action items.

---
+++ browser/ui/brave_browser_actions.h
@@ -0,0 +1,21 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_BROWSER_UI_BRAVE_BROWSER_ACTIONS_H_
+#define BRAVE_BROWSER_UI_BRAVE_BROWSER_ACTIONS_H_
+
+#include "chrome/browser/ui/browser_actions.h"
+
+// Add more side panel actions for our playlist/chat panels.
+class BraveBrowserActions : public BrowserActions {
+ public:
+  explicit BraveBrowserActions(Browser& browser);
+  ~BraveBrowserActions() override;
+
+ private:
+  void InitializeBraveBrowserActions();
+};
+
+#endif  // BRAVE_BROWSER_UI_BRAVE_BROWSER_ACTIONS_H_
---
+++ chromium_src/chrome/browser/ui/browser_actions.h
@@ -0,0 +1,18 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_BROWSER_ACTIONS_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_BROWSER_ACTIONS_H_
+
+#define InitializeBrowserActions    \
+  UnUsed() {}                       \
+  friend class BraveBrowserActions; \
+  void InitializeBrowserActions
+
+#include "src/chrome/browser/ui/browser_actions.h"  // IWYU pragma: export
+
+#undef InitializeBrowserActions
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_BROWSER_ACTIONS_H_
