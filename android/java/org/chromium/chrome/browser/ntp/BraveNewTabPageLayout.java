62ae3adfc06e0cdafcb8b423364e234b4bf0d3b4
Artem Samoilenko <artem@brave.com>
2024-06-17 17:58:08 -0400

    [Android] Fix for fixed MVT view
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/d864e62dddc14e305f0a274f6058ab082ad2b99a
    
    [SurfacePolish][CleanUp] Clean up surface polish code for MVT (Part 5)
    
    This CL represents part 5 of the cleanup for the surface polish code in
    the MVT, focusing on refining the code associated most visited tiles
    layout. The update removes the grid layout from most visited tiles and
    renames the most visited tiles carousel layout to simply most visited
    tiles layout.
    
    Bug: 331667743

--- android/java/org/chromium/chrome/browser/ntp/BraveNewTabPageLayout.java
+++ android/java/org/chromium/chrome/browser/ntp/BraveNewTabPageLayout.java
@@ -72,7 +72,6 @@ import org.chromium.chrome.browser.brave_news.models.FeedItemCard;
 import org.chromium.chrome.browser.brave_news.models.FeedItemsCard;
 import org.chromium.chrome.browser.brave_stats.BraveStatsUtil;
 import org.chromium.chrome.browser.feed.FeedSurfaceScrollDelegate;
-import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
 import org.chromium.chrome.browser.local_database.DatabaseHelper;
 import org.chromium.chrome.browser.local_database.TopSiteTable;
@@ -98,7 +97,7 @@ import org.chromium.chrome.browser.rate.RateUtils;
 import org.chromium.chrome.browser.settings.BackgroundImagesPreferences;
 import org.chromium.chrome.browser.settings.BraveNewsPreferencesV2;
 import org.chromium.chrome.browser.settings.SettingsLauncherImpl;
-import org.chromium.chrome.browser.suggestions.tile.MostVisitedTilesLayout;
+import org.chromium.chrome.browser.suggestions.tile.BraveMostVisitedTilesLayoutBase;
 import org.chromium.chrome.browser.suggestions.tile.TileGroup.Delegate;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabAttributes;
@@ -109,7 +108,6 @@ import org.chromium.components.browser_ui.widget.displaystyle.UiConfig;
 import org.chromium.components.user_prefs.UserPrefs;
 import org.chromium.mojo.bindings.ConnectionErrorHandler;
 import org.chromium.mojo.system.MojoException;
-import org.chromium.ui.base.DeviceFormFactor;
 import org.chromium.ui.base.WindowAndroid;
 
 import java.util.ArrayList;
@@ -241,30 +239,45 @@ public class BraveNewTabPageLayout
     protected void insertSiteSectionView() {
         mMainLayout = findViewById(R.id.ntp_content);
 
-        mMvTilesContainerLayout = (ViewGroup) LayoutInflater.from(mMainLayout.getContext())
-                                          .inflate(R.layout.mv_tiles_container, mMainLayout, false);
+        mMvTilesContainerLayout =
+                (ViewGroup)
+                        LayoutInflater.from(mMainLayout.getContext())
+                                .inflate(R.layout.mv_tiles_container, mMainLayout, false);
         mMvTilesContainerLayout.setPadding(0, 0, 0, 0);
         mMvTilesContainerLayout.setVisibility(View.VISIBLE);
 
-        mMvTilesContainerLayout.post(new Runnable() {
-            @Override
-            public void run() {
-                mMvTilesContainerLayout.addOnLayoutChangeListener(
-                        (View view, int left, int top, int right, int bottom, int oldLeft,
-                                int oldTop, int oldRight, int oldBottom) -> {
-                            int oldHeight = oldBottom - oldTop;
-                            int newHeight = bottom - top;
+        mMvTilesContainerLayout.post(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        mMvTilesContainerLayout.addOnLayoutChangeListener(
+                                (View view,
+                                        int left,
+                                        int top,
+                                        int right,
+                                        int bottom,
+                                        int oldLeft,
+                                        int oldTop,
+                                        int oldRight,
+                                        int oldBottom) -> {
+                                    int oldHeight = oldBottom - oldTop;
+                                    int newHeight = bottom - top;
 
-                            if (oldHeight != newHeight && mIsTopSitesEnabled
-                                    && mNtpAdapter != null) {
-                                new Handler(Looper.getMainLooper()).post(() -> {
-                                    mNtpAdapter.notifyItemRangeChanged(mNtpAdapter.getStatsCount(),
-                                            mNtpAdapter.getNewContentCount() + 2);
+                                    if (oldHeight != newHeight
+                                            && mIsTopSitesEnabled
+                                            && mNtpAdapter != null) {
+                                        new Handler(Looper.getMainLooper())
+                                                .post(
+                                                        () -> {
+                                                            mNtpAdapter.notifyItemRangeChanged(
+                                                                    mNtpAdapter.getStatsCount(),
+                                                                    mNtpAdapter.getNewContentCount()
+                                                                            + 2);
+                                                        });
+                                    }
                                 });
-                            }
-                        });
-            }
-        });
+                    }
+                });
 
         // The page contents are initially hidden; otherwise they'll be drawn centered on the
         // page before the tiles are available and then jump upwards to make space once the
@@ -1190,10 +1203,28 @@ public class BraveNewTabPageLayout
                 isTablet,
                 tabStripHeightSupplier);
 
+        assert mMvTilesContainerLayout != null : "Something has changed in the upstream!";
+
+        if (mMvTilesContainerLayout != null && useFixedMVTLayout()) {
+            ViewGroup tilesLayout = mMvTilesContainerLayout.findViewById(R.id.mv_tiles_layout);
+
+            assert tilesLayout instanceof BraveMostVisitedTilesLayoutBase
+                    : "Something has changed in the upstream!";
+
+            if (tilesLayout instanceof BraveMostVisitedTilesLayoutBase) {
+                ((BraveMostVisitedTilesLayoutBase) tilesLayout).setUseFixedLayout(true);
+            }
+        }
+
         assert (activity instanceof BraveActivity);
         mActivity = activity;
         ((BraveActivity) mActivity).dismissShieldsTooltip();
         ((BraveActivity) mActivity).setNewTabPageManager(manager);
+    }
+
+    protected boolean useFixedMVTLayout() {
+        return !UserPrefs.get(ProfileManager.getLastUsedRegularProfile())
+                .getBoolean(BravePref.NEW_TAB_PAGE_SHOW_BACKGROUND_IMAGE);
     }
 
     public void setTabProvider(Supplier<Tab> tabProvider) {
