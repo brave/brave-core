f4a1f8cdbffa96828c8295e590d9cc3ce3261229
deeppandya <deeppandya91@gmail.com>
2024-06-19 05:25:20 -0400

    Add quick search NTP view
    Update changes in BravActivity to render the view
    
    Add helper class for checking keyboard height

--- android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
+++ android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
@@ -1252,6 +1252,13 @@ public abstract class BraveToolbarLayoutImpl extends ToolbarLayout
                 searchActivityIntent.setAction(Intent.ACTION_VIEW);
                 context.startActivity(searchActivityIntent);
             }
+            // if (hasFocus) {
+            //     Log.e("quick_search", "showQuickActionSearchEnginesView");
+            //     BraveActivity.getBraveActivity().showQuickActionSearchEnginesView();
+            // } else {
+            //     Log.e("quick_search", "removeQuickActionSearchEnginesView");
+            //     BraveActivity.getBraveActivity().removeQuickActionSearchEnginesView();
+            // }
 
         } catch (BraveActivity.BraveActivityNotFoundException e) {
             Log.e(TAG, "onUrlFocusChange " + e);
