2e70c52afdc9a5b80b21cd28f2e78cb22da6a8ff
AlexeyBarabash <alexey@brave.com>
2024-06-21 23:31:34 +0300

    Added tabStripTransitionDelegateSupplier arg to TopToolbarCoordinator.ctor
    Related Chromium change
    https://source.chromium.org/chromium/chromium/src/+/c08e6b5dd8c9fcac93e6376f7548ff060797397e
    
    	[TSLO] Create TabStripTransitionDelegate
    
    	Move TabStripHeightObserver methods to the new delegate interface:
    	- onHeightChanged
    	- onTransitionFinished
    
    	Bug: 345849359
    	Change-Id: I106a15f0e93e6eb48ae80731708d1d6e72f303c4
    	Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5636752

--- android/java/org/chromium/chrome/browser/toolbar/top/BraveTopToolbarCoordinator.java
+++ android/java/org/chromium/chrome/browser/toolbar/top/BraveTopToolbarCoordinator.java
@@ -35,6 +35,7 @@ import org.chromium.chrome.browser.toolbar.menu_button.MenuButton;
 import org.chromium.chrome.browser.toolbar.menu_button.MenuButtonCoordinator;
 import org.chromium.chrome.browser.toolbar.top.NavigationPopup.HistoryDelegate;
 import org.chromium.chrome.browser.toolbar.top.ToolbarTablet.OfflineDownloader;
+import org.chromium.chrome.browser.toolbar.top.tab_strip.TabStripTransitionCoordinator.TabStripTransitionDelegate;
 import org.chromium.chrome.browser.ui.appmenu.AppMenuButtonHelper;
 import org.chromium.chrome.browser.ui.appmenu.AppMenuDelegate;
 import org.chromium.chrome.browser.ui.desktop_windowing.DesktopWindowStateProvider;
@@ -93,7 +94,8 @@ public class BraveTopToolbarCoordinator extends TopToolbarCoordinator {
                     browserStateBrowserControlsVisibilityDelegate,
             FullscreenManager fullscreenManager,
             TabObscuringHandler tabObscuringHandler,
-            @Nullable DesktopWindowStateProvider desktopWindowStateProvider) {
+            @Nullable DesktopWindowStateProvider desktopWindowStateProvider,
+            OneshotSupplier<TabStripTransitionDelegate> tabStripTransitionDelegateSupplier) {
         super(
                 controlContainer,
                 toolbarStub,
@@ -126,7 +128,8 @@ public class BraveTopToolbarCoordinator extends TopToolbarCoordinator {
                 browserStateBrowserControlsVisibilityDelegate,
                 fullscreenManager,
                 tabObscuringHandler,
-                desktopWindowStateProvider);
+                desktopWindowStateProvider,
+                tabStripTransitionDelegateSupplier);
 
         mBraveToolbarLayout = toolbarLayout;
         mBraveMenuButtonCoordinator = browsingModeMenuButtonCoordinator;
