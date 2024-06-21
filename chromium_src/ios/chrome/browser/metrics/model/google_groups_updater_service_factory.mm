108aa100dce7dd064c7485b6596fd2f9e94c6d10
Emerick Rogul <erogul@brave.com>
2024-06-20 21:03:10 -0400

    fixup! [iOS] Disable GoogleGroupsUpdaterService via factory

--- chromium_src/ios/chrome/browser/metrics/model/google_groups_updater_service_factory.mm
+++ chromium_src/ios/chrome/browser/metrics/model/google_groups_updater_service_factory.mm
@@ -5,12 +5,42 @@
 
 #include "ios/chrome/browser/metrics/model/google_groups_updater_service_factory.h"
 
-#define BuildServiceInstanceFor BuildServiceInstanceFor_ChromiumImpl
-#include "src/ios/chrome/browser/metrics/model/google_groups_updater_service_factory.mm"
-#undef BuildServiceInstanceFor
+#import "components/keyed_service/ios/browser_state_dependency_manager.h"
+#import "components/variations/service/google_groups_updater_service.h"
+#import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
+
+// static
+GoogleGroupsUpdaterService*
+GoogleGroupsUpdaterServiceFactory::GetForBrowserState(
+    ChromeBrowserState* browser_state) {
+  return nullptr;
+}
+
+// static
+GoogleGroupsUpdaterServiceFactory*
+GoogleGroupsUpdaterServiceFactory::GetInstance() {
+  return nullptr;
+}
+
+GoogleGroupsUpdaterServiceFactory::GoogleGroupsUpdaterServiceFactory()
+    : BrowserStateKeyedServiceFactory(
+          "GoogleGroupsUpdaterService",
+          BrowserStateDependencyManager::GetInstance()) {}
 
 std::unique_ptr<KeyedService>
 GoogleGroupsUpdaterServiceFactory::BuildServiceInstanceFor(
     web::BrowserState* context) const {
   return nullptr;
 }
+
+bool GoogleGroupsUpdaterServiceFactory::ServiceIsCreatedWithBrowserState()
+    const {
+  return true;
+}
+
+bool GoogleGroupsUpdaterServiceFactory::ServiceIsNULLWhileTesting() const {
+  return true;
+}
+
+void GoogleGroupsUpdaterServiceFactory::RegisterBrowserStatePrefs(
+    user_prefs::PrefRegistrySyncable* registry) {}
