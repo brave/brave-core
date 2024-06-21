ba7f1f80c84d296c35bdb020338238317e209e0e
Emerick Rogul <erogul@brave.com>
2024-06-21 07:00:38 -0400

    fixup! Removing broken `TabSearchBubbleHost` override

--- chromium_src/chrome/browser/ui/views/tab_search_bubble_host.cc
+++ chromium_src/chrome/browser/ui/views/tab_search_bubble_host.cc
@@ -3,6 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+// We always want the tab search button to render after the tab strip (i.e., on
+// the right side of the frame), regardless of platform.
 #define BRAVE_TAB_SEARCH_BUBBLE_HOST_SHOULD_TAB_SEARCH_RENDER_BEFORE_TAB_STRIP \
   return false;
 #include "src/chrome/browser/ui/views/tab_search_bubble_host.cc"
