4b9aa78f2106938a386772ec5f71104d85012b1d
Emerick Rogul <erogul@brave.com>
2024-06-21 09:34:29 -0400

    fixup! Continue to use profile_background_container_ to set profile menu background

--- chromium_src/chrome/browser/ui/views/profiles/profile_menu_view_base.cc
+++ chromium_src/chrome/browser/ui/views/profiles/profile_menu_view_base.cc
@@ -3,8 +3,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-// Continue to use profile_background_container_ to set the background color in
-// the profile menu, rather than identiy_info_color_callback_
+// Now that ChromeRefresh2023 is active, ProfileMenuViewBase uses
+// identity_info_color_callback_ to draw its background which results in
+// unwanted changes to the menu UI. This switches us back to continue using the
+// profile_background_container_ to set the background as was done pre-refresh.
 #define BRAVE_PROFILE_MENU_VIEW_BASE_SET_BACKGROUND    \
   identity_info_color_callback_ = base::DoNothing();   \
   profile_background_container_->SetBackground(        \
