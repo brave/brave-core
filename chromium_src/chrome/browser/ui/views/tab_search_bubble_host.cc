e22f77b475a96dee1eb41e6e4aeca2ddfdeebc65
Emerick Rogul <erogul@brave.com>
2024-06-20 20:05:55 -0400

    fixup! Removing broken `TabSearchBubbleHost` override

--- chromium_src/chrome/browser/ui/views/tab_search_bubble_host.cc
+++ chromium_src/chrome/browser/ui/views/tab_search_bubble_host.cc
@@ -3,9 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "build/build_config.h"
-#include "ui/base/ui_base_features.h"
-
 #define BRAVE_TAB_SEARCH_BUBBLE_HOST_SHOULD_TAB_SEARCH_RENDER_BEFORE_TAB_STRIP \
   return false;
 #include "src/chrome/browser/ui/views/tab_search_bubble_host.cc"
