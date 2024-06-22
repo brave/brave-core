b33f828ac491d2878786bc0a3c57c248f7a7de8d
Jagadesh P <jagadeshjai1999@gmail.com>
2024-06-22 18:08:34 +0530

    [BraveShields] Rename BraveShieldsDataController -> BraveShieldsTabHelper to maintain standard naming convention.
    Also move this file to 'browser/brave_shields/' to maintain directory structure.

--- browser/ui/browser_commands.cc
+++ browser/ui/browser_commands.cc
@@ -17,9 +17,9 @@
 #include "base/ranges/algorithm.h"
 #include "base/strings/utf_string_conversions.h"
 #include "brave/app/brave_command_ids.h"
+#include "brave/browser/brave_shields/brave_shields_tab_helper.h"
 #include "brave/browser/debounce/debounce_service_factory.h"
 #include "brave/browser/ui/bookmark/brave_bookmark_prefs.h"
-#include "brave/browser/ui/brave_shields_data_controller.h"
 #include "brave/browser/ui/sidebar/sidebar_service_factory.h"
 #include "brave/browser/ui/tabs/brave_tab_prefs.h"
 #include "brave/browser/ui/tabs/brave_tab_strip_model.h"
@@ -387,7 +387,7 @@ void ToggleShieldsEnabled(Browser* browser) {
     return;
   }
   auto* shields =
-      brave_shields::BraveShieldsDataController::FromWebContents(contents);
+      brave_shields::BraveShieldsTabHelper::FromWebContents(contents);
   if (!shields) {
     return;
   }
@@ -405,7 +405,7 @@ void ToggleJavascriptEnabled(Browser* browser) {
     return;
   }
   auto* shields =
-      brave_shields::BraveShieldsDataController::FromWebContents(contents);
+      brave_shields::BraveShieldsTabHelper::FromWebContents(contents);
   if (!shields) {
     return;
   }
