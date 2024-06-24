4542cc74d03dff498e6459622048eb3d3e97e1c0
Pete Miller <miller.pete@gmail.com>
2024-06-24 14:16:26 -0700

    Disable conversation API on iOS (#24313)

--- components/ai_chat/core/common/features.cc
+++ components/ai_chat/core/common/features.cc
@@ -29,8 +29,16 @@ const base::FeatureParam<bool> kFreemiumAvailable(&kAIChat,
                                                   "is_freemium_available",
                                                   true);
 const base::FeatureParam<bool> kAIChatSSE{&kAIChat, "ai_chat_sse", true};
-const base::FeatureParam<bool> kConversationAPIEnabled{
-    &kAIChat, "conversation_api", true};
+
+const base::FeatureParam<bool> kConversationAPIEnabled{&kAIChat,
+                                                       "conversation_api",
+#if BUILDFLAG(IS_IOS)
+                                                       false
+#else
+                                                       true
+#endif
+};
+
 const base::FeatureParam<double> kAITemperature{&kAIChat, "temperature", 0.2};
 
 bool IsAIChatEnabled() {
