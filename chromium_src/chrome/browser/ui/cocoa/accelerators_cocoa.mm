ea91b1b314551c5f9a685c224b0772a0c17ab42d
Jay Harris <jay.harris@outlook.co.nz>
2024-06-24 14:39:37 +1200

    [Commander]: Change default macOS shortcut (#24336)

--- chromium_src/chrome/browser/ui/cocoa/accelerators_cocoa.mm
+++ chromium_src/chrome/browser/ui/cocoa/accelerators_cocoa.mm
@@ -10,12 +10,12 @@ constexpr int kNewWindowCmdID = IDC_NEW_WINDOW;
 }  // namespace
 
 #undef IDC_NEW_WINDOW
-#define IDC_NEW_WINDOW                                                    \
-  IDC_NEW_OFFTHERECORD_WINDOW_TOR, ui::EF_COMMAND_DOWN | ui::EF_ALT_DOWN, \
-      ui::VKEY_N                                                          \
-  }                                                                       \
-  , {IDC_TOGGLE_TAB_MUTE, ui::EF_CONTROL_DOWN, ui::VKEY_M},               \
-      {IDC_COMMANDER, ui::EF_CONTROL_DOWN, ui::VKEY_SPACE}, {             \
+#define IDC_NEW_WINDOW                                                        \
+  IDC_NEW_OFFTHERECORD_WINDOW_TOR, ui::EF_COMMAND_DOWN | ui::EF_ALT_DOWN,     \
+      ui::VKEY_N                                                              \
+  }                                                                           \
+  , {IDC_TOGGLE_TAB_MUTE, ui::EF_CONTROL_DOWN, ui::VKEY_M},                   \
+      {IDC_COMMANDER, ui::EF_COMMAND_DOWN | ui::EF_SHIFT_DOWN, ui::VKEY_P}, { \
     kNewWindowCmdID
 
 #include "src/chrome/browser/ui/cocoa/accelerators_cocoa.mm"
