2b20bd7f2ce6e8640732af4390099e4bbbc3311e
Soner Yuksel <grkmyksl@gmail.com>
2024-07-04 13:15:41 -0400

    Adding Progress Style for Loading

--- ios/brave-ios/Sources/BraveVPN/Components/Region/BraveVPNRegionLoadingIndicator.swift
+++ ios/brave-ios/Sources/BraveVPN/Components/Region/BraveVPNRegionLoadingIndicator.swift
@@ -6,28 +6,63 @@
 import SwiftUI
 
 struct BraveVPNRegionLoadingIndicatorView: View {
-
   @State
   private var isAnimating = false
 
   var body: some View {
-    ZStack {
-      Circle()
-        .stroke(lineWidth: 7)
-        .opacity(0.3)
-        .foregroundStyle(Color(braveSystemName: .iconInteractive))
-        .frame(width: 48, height: 48)
-      Circle()
-        .trim(from: 0.75, to: 1.0)
-        .stroke(style: StrokeStyle(lineWidth: 7, lineCap: .round))
-        .foregroundColor(Color(braveSystemName: .iconInteractive))
-        .frame(width: 48, height: 48)
-        .rotationEffect(Angle(degrees: isAnimating ? 360 : 0))
-        .onAppear {
-          withAnimation(Animation.linear(duration: 1.0).repeatForever(autoreverses: false)) {
-            isAnimating = true
-          }
-        }
+    ProgressView()
+      .progressViewStyle(BraveVPNCircularProgressViewStyle(thickness: 3))
+  }
+}
+
+struct BraveVPNCircularProgressViewStyle: ProgressViewStyle {
+  private let thickness: Double
+
+  init(thickness: Double) {
+    self.thickness = thickness
+  }
+
+  func makeBody(configuration: Configuration) -> some View {
+    TimelineView(.animation(minimumInterval: 0.1, paused: false)) { context in
+      BraveVPNCircularProgressView(
+        endDate: context.date,
+        thickness: thickness
+      )
+    }
+  }
+
+  private struct BraveVPNCircularProgressView: View {
+    @State
+    private var startDate: Date = .now
+
+    private var thickness: Double
+
+    private let endDate: Date
+
+    init(endDate: Date, thickness: Double) {
+      self.endDate = endDate
+      self.thickness = thickness
+    }
+
+    private var progress: Double {
+      return (endDate.timeIntervalSince1970 - startDate.timeIntervalSince1970) * 3
+    }
+
+    var body: some View {
+      ZStack {
+        Circle()
+          .stroke(lineWidth: 7)
+          .opacity(0.3)
+          .foregroundStyle(Color(braveSystemName: .iconInteractive))
+          .frame(width: 48, height: 48)
+        Circle()
+          .trim(from: 0.75, to: 1.0)
+          .stroke(style: StrokeStyle(lineWidth: 7, lineCap: .round))
+          .foregroundColor(Color(braveSystemName: .iconInteractive))
+          .frame(width: 48, height: 48)
+          .rotationEffect(.degrees(90.0 * progress))
+          .animation(.linear, value: endDate)
+      }
     }
   }
 }
