b3f82fd16a6ddea6292ba2366b60b7df7a4e3cb5
brave-builds <45370463+brave-builds@users.noreply.github.com>
2024-07-05 21:04:35 +0200

    [Android] Fix for copied URL for typed URLs (uplift to 1.68.x) (#24480)
    Uplift of #24252 (squashed) to beta

---
+++ browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/editurl/BraveEditUrlSuggestionProcessorBase.java
@@ -0,0 +1,44 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+package org.chromium.chrome.browser.omnibox.suggestions.editurl;
+
+import android.content.Context;
+
+import org.chromium.base.BraveReflectionUtil;
+import org.chromium.chrome.browser.omnibox.styles.OmniboxImageSupplier;
+import org.chromium.chrome.browser.omnibox.suggestions.SuggestionHost;
+import org.chromium.chrome.browser.omnibox.suggestions.base.BaseSuggestionViewProcessor;
+import org.chromium.components.omnibox.AutocompleteMatch;
+
+import java.util.Optional;
+
+public abstract class BraveEditUrlSuggestionProcessorBase extends BaseSuggestionViewProcessor {
+    public BraveEditUrlSuggestionProcessorBase(
+            Context context,
+            SuggestionHost suggestionHost,
+            Optional<OmniboxImageSupplier> imageSupplier) {
+        super(context, suggestionHost, imageSupplier);
+    }
+
+    /*
+     * Calls to the upstream's `EditUrlSuggestionProcessor.onCopyLink` will be redirected here via bytecode.
+     * Here we call the upstream's `EditUrlSuggestionProcessor.onCopyLink` with possibly changed suggestion.
+     * See comment to `BraveEditUrlSuggestionProcessor.maybeUpdateSuggestionForCopyLink` for more details on why it may be changed.
+     */
+    public void onCopyLink(AutocompleteMatch suggestion) {
+        BraveReflectionUtil.InvokeMethod(
+                EditUrlSuggestionProcessor.class,
+                this,
+                "onCopyLink",
+                AutocompleteMatch.class,
+                maybeUpdateSuggestionForCopyLink(suggestion));
+    }
+
+    public AutocompleteMatch maybeUpdateSuggestionForCopyLink(AutocompleteMatch suggestion) {
+        assert false : "This method should be overridden by BraveEditUrlSuggestionProcessor";
+        return suggestion;
+    }
+}
