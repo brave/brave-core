63b641f578ac7e2d72d995303543d742256cef84
Simon Hong <shong@brave.com>
2024-07-05 12:04:17 +0900

    Fixed wrong infobar background color
    fix https://github.com/brave/brave-browser/issues/39580
    
    Upstream doesn't use toolbar color for it anymore since cr127.
    But, we want to have same bg color with toolbar.

--- browser/ui/color/brave_color_mixer.cc
+++ browser/ui/color/brave_color_mixer.cc
@@ -443,6 +443,8 @@ void AddBravifiedChromeThemeColorMixer(ui::ColorProvider* provider,
   AddMaterialSidePanelColorMixer(provider, key);
 
   // TODO(simonhong): Use leo color when it's ready.
+  // TODO(simonhong): Move these overriding to
+  // AddChromeColorMixerForAllThemes().
   ui::ColorMixer& mixer = provider->AddMixer();
   mixer[kColorAppMenuHighlightSeverityLow] = {
       PickColorContrastingToToolbar(key, mixer, SkColorSetRGB(0x00, 0x46, 0x07),
@@ -453,6 +455,7 @@ void AddBravifiedChromeThemeColorMixer(ui::ColorProvider* provider,
   mixer[kColorAppMenuHighlightSeverityHigh] = {
       PickColorContrastingToToolbar(key, mixer, SkColorSetRGB(0x7D, 0x00, 0x1A),
                                     SkColorSetRGB(0xFF, 0xB3, 0xB2))};
+  mixer[kColorInfoBarBackground] = {kColorToolbar};
 
   if (key.custom_theme) {
     return;
