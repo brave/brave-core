8633acee19f8d57039bf4a79b3095468978bc1f1
William Muli <willi.wambu@gmail.com>
2024-07-10 12:00:30 +0300

    Added quote details and action button

--- components/brave_wallet_ui/page/screens/fund-wallet/components/buy_quote/buy_quote.tsx
+++ components/brave_wallet_ui/page/screens/fund-wallet/components/buy_quote/buy_quote.tsx
@@ -13,44 +13,95 @@ import {
   Estimate,
   PaymentMethodsWrapper,
   PaymentMethodIcon,
-  CaratIcon
+  CaratIcon,
+  QuoteDetailsWrapper,
+  QuoteDetailsRow,
+  QuoteDetailsLabel,
+  QuoteDetailsValue,
+  Divider,
+  QuoteTotal,
+  BuyButton,
+  BestOptionLabel
 } from './buy_quote.style'
 import { Column, Row } from '../../../../../components/shared/style'
+import Icon from '@brave/leo/react/icon'
 
 interface BuyQuoteProps {}
 
 export const BuyQuote = (props: BuyQuoteProps) => {
-  const [isOpen, setIsOpen] = React.useState(false)
+  const [isOpen, setIsOpen] = React.useState(true)
 
   return (
-    <StyledWrapper>
+    <StyledWrapper isOpen={isOpen}>
       <Row
         justifyContent='space-between'
         alignItems='flex-start'
         width='100%'
         onClick={() => setIsOpen(!isOpen)}
       >
-        <Row 
-        justifyContent='flex-start'
-        alignItems='center'>
+        <Row
+          justifyContent='flex-start'
+          alignItems='center'
+        >
           <ProviderImage src='https://via.placeholder.com/40' />
 
-          <Column>
+          <Column alignItems='flex-start'>
             <ProviderName>Transak</ProviderName>
             <Estimate>€100.00 = ~0.0609 ETH</Estimate>
           </Column>
         </Row>
-        <Row gap='8px'>
+        <Row
+          gap='8px'
+          justifyContent='flex-end'
+        >
+          <BestOptionLabel>
+            <div slot='icon-before'>
+              <Icon name='thumb-up' />
+            </div>
+            BEST OPEN
+          </BestOptionLabel>
           <PaymentMethodsWrapper>
             <PaymentMethodIcon name='bank' />
             <PaymentMethodIcon name='credit-card' />
           </PaymentMethodsWrapper>
-            <CaratIcon
-              isOpen={isOpen}
-              name='carat-down'
-            />
+          <CaratIcon
+            isOpen={isOpen}
+            name='carat-down'
+          />
         </Row>
       </Row>
+      {isOpen ? (
+        <Column
+          width='100%'
+          gap='8px'
+        >
+          <QuoteDetailsWrapper>
+            <QuoteDetailsRow>
+              <QuoteDetailsLabel>Exchange rate with fees</QuoteDetailsLabel>
+              <QuoteDetailsValue>≈ 1,601.78 EUR / ETH</QuoteDetailsValue>
+            </QuoteDetailsRow>
+            <QuoteDetailsRow>
+              <QuoteDetailsLabel>Price EUR</QuoteDetailsLabel>
+              <QuoteDetailsValue>≈ 95.00 EUR</QuoteDetailsValue>
+            </QuoteDetailsRow>
+            <QuoteDetailsRow>
+              <QuoteDetailsLabel>Fees</QuoteDetailsLabel>
+              <QuoteDetailsValue>≈ 5.00 EUR</QuoteDetailsValue>
+            </QuoteDetailsRow>
+            <Divider />
+            <QuoteDetailsRow>
+              <QuoteTotal>Total</QuoteTotal>
+              <QuoteTotal>100.00EUR</QuoteTotal>
+            </QuoteDetailsRow>
+          </QuoteDetailsWrapper>
+          <BuyButton>
+            Buy with Transak
+            <div slot='icon-after'>
+              <Icon name='launch' />
+            </div>
+          </BuyButton>
+        </Column>
+      ) : null}
     </StyledWrapper>
   )
 }
