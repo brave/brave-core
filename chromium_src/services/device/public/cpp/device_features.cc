b84d50032d092b15a4d47fc18bea43b935d01c2d
Emerick Rogul <erogul@brave.com>
2024-07-15 11:19:30 -0400

    Fix inaccurate geolocation
    In cr127, features::kWinrtGeolocationImplementation (and a related Mac feature
    flag) were replaced with kLocationProviderManager. Enable that flag and set it
    to kPlatformOnly so that we use the platform's geolocation provider.
    
    Chromiuum change:
    https://source.chromium.org/chromium/chromium/src/+/cc04eb8474feb1d62287c0866af29351aa762186
    
    commit cc04eb8474feb1d62287c0866af29351aa762186
    Author: alvinji <alvinji@chromium.org>
    Date:   Wed May 15 23:06:21 2024 +0000
    
        geolocation: Add kLocationProviderManager feature flag
    
        This CL adds `kLocationProviderManager` feature flag and associated
        parameters to enable flexible mode configuration of the
        LocationProviderManager (implementation to follow). The configuration
        will determine which location provider is used as the source for
        Geolocation API.
    
        Change-Id: I98a2a25f7b05002fb6a1244fde5091a96bad2756
        Bug: 333294295

---
+++ chromium_src/services/device/public/cpp/device_features.cc
@@ -0,0 +1,26 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "services/device/public/cpp/device_features.h"
+
+#define kLocationProviderManagerParam kLocationProviderManagerParamUnused
+#include "src/services/device/public/cpp/device_features.cc"
+#undef kLocationProviderManagerParam
+
+#include "base/feature_override.h"
+
+namespace features {
+
+OVERRIDE_FEATURE_DEFAULT_STATES({{
+    {kLocationProviderManager, base::FEATURE_ENABLED_BY_DEFAULT},
+}});
+
+const base::FeatureParam<device::mojom::LocationProviderManagerMode>
+    kLocationProviderManagerParam{
+        &kLocationProviderManager, "LocationProviderManagerMode",
+        device::mojom::LocationProviderManagerMode::kPlatformOnly,
+        &location_provider_manager_mode_options};
+
+}  // namespace features
