baa0dd4626e096569fc1ebe43c67ce75ae04dc18
Mikhail Atuchin <matuchin@brave.com>
2024-07-15 15:35:45 +0400

    A new way to opt-out of chromium build telemetry

--- build/commands/lib/depotTools.js
+++ build/commands/lib/depotTools.js
@@ -133,20 +133,6 @@ function installDepotTools(options = config.defaultOptions) {
       }
     }
 
-    const ninjaLogCfgPath = path.join(config.depotToolsDir, 'ninjalog.cfg')
-    if (!fs.existsSync(ninjaLogCfgPath)) {
-      // Create a ninja config to prevent autoninja from calling "cipd
-      // auth-info" each time. See for details:
-      // https://chromium.googlesource.com/chromium/tools/depot_tools/+/main/ninjalog.README.md
-      const ninjaLogCfgConfig = {
-        'is-googler': false,
-        'version': 3,
-        'countdown': 10,
-        'opt-in': false
-      }
-      fs.writeFileSync(ninjaLogCfgPath, JSON.stringify(ninjaLogCfgConfig))
-    }
-
     if (process.platform === 'win32' && config.isCI) {
       // Bootstrap gsutil on Windows manually to fix random LockFile issues.
       util.run(path.join(config.depotToolsDir, 'gsutil.py.bat'), [], {
@@ -154,6 +140,12 @@ function installDepotTools(options = config.defaultOptions) {
         stdio: 'pipe'
       })
     }
+
+    // Opt-out of chromium build telemetry. See for details:
+    // https://chromium.googlesource.com/chromium/tools/depot_tools/+/main/ninjalog.README.md
+    util.run('vpython3',
+             [path.join(config.depotToolsDir, 'build_telemetry.py'), 'opt-out'],
+             options)
   })
 }
 
