59eb41ec2ea1a37dd262f2636fc95ef118f74b3f
Simone Arpe <simon.arpe@gmail.com>
2024-07-15 14:52:58 +0200

    Apply code formatting

--- android/java/org/chromium/chrome/browser/crypto_wallet/fragments/onboarding/OnboardingVerifyRecoveryPhraseFragment.java
+++ android/java/org/chromium/chrome/browser/crypto_wallet/fragments/onboarding/OnboardingVerifyRecoveryPhraseFragment.java
@@ -5,7 +5,6 @@
 
 package org.chromium.chrome.browser.crypto_wallet.fragments.onboarding;
 
-import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.Editable;
@@ -20,7 +19,6 @@ import android.widget.TextView;
 import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.content.res.AppCompatResources;
 import androidx.appcompat.widget.AppCompatButton;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -59,8 +57,7 @@ public class OnboardingVerifyRecoveryPhraseFragment extends BaseOnboardingWallet
     private ImageView mStep1;
     private ImageView mStep2;
     private ImageView mStep3;
-    @DrawableRes
-    private int mCurrentStepRes;
+    @DrawableRes private int mCurrentStepRes;
     private boolean mIsOnboarding;
     private VerificationStep mVerificationStep;
     private Pair<Integer, String> mWordToMatch;
