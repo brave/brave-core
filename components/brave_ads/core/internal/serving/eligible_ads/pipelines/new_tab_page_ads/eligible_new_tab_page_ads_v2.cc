a4fb06f335e46fa662e72bbdc6555ea58e792340
Terry Mancey <terry.mancey@icloud.com>
2024-07-16 10:53:59 -0500

    [ads] Removing `Failed to get ads` DumpWithoutCrashing because working as expected
    Caused by db error: 5/database is locked

--- components/brave_ads/core/internal/serving/eligible_ads/pipelines/new_tab_page_ads/eligible_new_tab_page_ads_v2.cc
+++ components/brave_ads/core/internal/serving/eligible_ads/pipelines/new_tab_page_ads/eligible_new_tab_page_ads_v2.cc
@@ -8,7 +8,6 @@
 #include <optional>
 #include <utility>
 
-#include "base/debug/dump_without_crashing.h"
 #include "base/functional/bind.h"
 #include "brave/components/brave_ads/core/internal/client/ads_client_util.h"
 #include "brave/components/brave_ads/core/internal/common/logging_util.h"
@@ -85,12 +84,6 @@ void EligibleNewTabPageAdsV2::GetEligibleAdsCallback(
     const SegmentList& /*segments*/,
     const CreativeNewTabPageAdList& creative_ads) {
   if (!success) {
-    // TODO(https://github.com/brave/brave-browser/issues/32066):
-    // Detect potential defects using `DumpWithoutCrashing`.
-    SCOPED_CRASH_KEY_STRING64("Issue32066", "failure_reason",
-                              "Failed to get ads");
-    base::debug::DumpWithoutCrashing();
-
     BLOG(0, "Failed to get ads");
 
     return std::move(callback).Run(/*eligible_ads=*/{});
