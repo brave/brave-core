5dd66196b5215c7d9c963e84e8875b37d0f43681
Jacob Sikorski <jacob.sikorski@gmail.com>
2024-07-18 11:43:52 -0600

    Fix issue with subframe engine scripts not working

--- ios/brave-ios/Sources/Brave/Frontend/UserContent/UserScripts/Scripts_Dynamic/Scripts/DomainSpecific/Paged/FrameCheckWrapper.js
+++ ios/brave-ios/Sources/Brave/Frontend/UserContent/UserScripts/Scripts_Dynamic/Scripts/DomainSpecific/Paged/FrameCheckWrapper.js
@@ -3,8 +3,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-'use strict'
-
 window.__firefox__.execute(function ($) {
   (_ => {
     // Because we can't inject scripts to specific frames (except main frame),
