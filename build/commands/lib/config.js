a2b5974638e37a82c2cdf7e7b2e105b04165e2ef
Claudio DeSouza <cdesouza@brave.com>
2024-07-23 16:41:41 +0100

    `feed::FetchRssLinks` not linkable with feed enabled
    Upstream has hidden a couple of files behind `enable_feed_v2` to reduce
    binary size. However this functions are required for brave news
    `feed::FetchRssLinks`.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/d3500b942cde04737bc13021173b6ffa11aaf1b9
    
    commit d3500b942cde04737bc13021173b6ffa11aaf1b9
    Author: Chris Davis <chrdavis@microsoft.com>
    Date:   Mon Apr 29 19:44:45 2024 +0000
    
        Ensure feeds code is excluded if it is not enabled
    
        The feeds (rss) code was getting included even when enable_feed_v2 was
        not enabled.  This also removes dead code left behind from previous
        recent feed-related code removal. This saves ~700k of disk space for
        chrome.dll when feeds are disabled.
    
        Bug: 335003908

--- build/commands/lib/config.js
+++ build/commands/lib/config.js
@@ -366,6 +366,7 @@ Config.prototype.buildArgs = function () {
     branding_path_product: "brave",
     enable_nacl: false,
     enable_widevine: true,
+    enable_feed_v2: true,
     // Our copy of signature_generator.py doesn't support --ignore_missing_cert:
     ignore_missing_widevine_signing_cert: false,
     target_cpu: this.targetArch,
