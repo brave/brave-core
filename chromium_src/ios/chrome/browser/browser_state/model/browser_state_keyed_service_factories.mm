39bf2e30f92c0a00edce1e758a08d62c1e6c82d4
Claudio DeSouza <cdesouza@brave.com>
2024-07-23 16:53:09 +0100

    [ios] `browser/sessions` moved under `model/`
    This just affects the inclusion paths, and shadowing for a given
    override.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/7dafde4ef30597bdc30c506085c4a6aaadecc1b6
    
    commit 7dafde4ef30597bdc30c506085c4a6aaadecc1b6
    Author: Sylvain Defresne <sdefresne@chromium.org>
    Date:   Thu Jul 18 14:17:20 2024 +0000
    
        [ios] Move //ios/chrome/browser/{sessions => sessions/model} files
    
        Move all the files and targets now that the internal code has been
        updated to use the new paths.
    
        This is part of a multi-CLs change:
         -  https://crrev.com/c/5717949
         -  https://crrev.com/i/7498390
         => https://crrev.com/c/5717871
    
        Fixed: 40281350

--- chromium_src/ios/chrome/browser/browser_state/model/browser_state_keyed_service_factories.mm
+++ chromium_src/ios/chrome/browser/browser_state/model/browser_state_keyed_service_factories.mm
@@ -24,7 +24,7 @@
 #include "ios/chrome/browser/passwords/model/ios_chrome_profile_password_store_factory.h"
 #include "ios/chrome/browser/reading_list/model/reading_list_model_factory.h"
 #include "ios/chrome/browser/search_engines/model/template_url_service_factory.h"
-#include "ios/chrome/browser/sessions/session_restoration_service_factory.h"
+#include "ios/chrome/browser/sessions/model/session_restoration_service_factory.h"
 #include "ios/chrome/browser/signin/model/account_consistency_service_factory.h"
 #include "ios/chrome/browser/signin/model/identity_manager_factory.h"
 #include "ios/chrome/browser/sync/model/ios_user_event_service_factory.h"
--- chromium_src/ios/chrome/browser/tabs/model/tab_helper_util.mm
+++ chromium_src/ios/chrome/browser/tabs/model/tab_helper_util.mm
@@ -6,7 +6,7 @@
 #import "ios/chrome/browser/tabs/model/tab_helper_util.h"
 
 #include "ios/chrome/browser/complex_tasks/model/ios_task_tab_helper.h"
-#include "ios/chrome/browser/sessions/ios_chrome_session_tab_helper.h"
+#include "ios/chrome/browser/sessions/model/ios_chrome_session_tab_helper.h"
 #include "ios/chrome/browser/tabs/model/ios_chrome_synced_tab_delegate.h"
 #import "ios/chrome/browser/web/model/session_state/web_session_state_tab_helper.h"
 
