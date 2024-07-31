e556e05d2026aeb49c25eba788278c6d76530b66
Kyle Hickinson <git@kylehickinson.com>
2024-07-31 11:44:28 -0400

    [ios] re-enforce early registration of KeyedServiceFactories
    https://source.chromium.org/chromium/chromium/src/+/344b25db37e9c8d52316db5830a4a5dc1571f4c3
    
    commit 344b25db37e9c8d52316db5830a4a5dc1571f4c3
    Author: Sylvain Defresne <sdefresne@chromium.org>
    Date:   Tue Jul 16 17:40:28 2024 +0000
    
        Reland "Preparation for decoupling creation/initialization of context"
    
            This is a reland of https://crrev.com/c/5678966. The
            original version of the CL forgot to initialize an
            member variable of MappingInfo{} causing undefined
            behavior (found via failures of some tests).
    
            This version of the CL fixes the MappingInfo{} struct
            to correctly initialize all member variables.

--- chromium_src/ios/chrome/browser/browser_state/model/browser_state_keyed_service_factories.mm
+++ chromium_src/ios/chrome/browser/browser_state/model/browser_state_keyed_service_factories.mm
@@ -7,12 +7,17 @@
 
 #include "brave/ios/browser/browser_state/brave_browser_state_keyed_service_factories.h"
 #include "ios/chrome/browser/autocomplete/model/autocomplete_classifier_factory.h"
+#include "ios/chrome/browser/autocomplete/model/zero_suggest_cache_service_factory.h"
 #include "ios/chrome/browser/autofill/model/personal_data_manager_factory.h"
+#include "ios/chrome/browser/bookmarks/model/account_bookmark_sync_service_factory.h"
 #include "ios/chrome/browser/bookmarks/model/bookmark_model_factory.h"
 #include "ios/chrome/browser/bookmarks/model/bookmark_undo_service_factory.h"
+#include "ios/chrome/browser/bookmarks/model/local_or_syncable_bookmark_sync_service_factory.h"
 #include "ios/chrome/browser/consent_auditor/model/consent_auditor_factory.h"
 #include "ios/chrome/browser/content_settings/model/host_content_settings_map_factory.h"
 #include "ios/chrome/browser/credential_provider/model/credential_provider_buildflags.h"
+#include "ios/chrome/browser/data_sharing/model/data_sharing_service_factory.h"
+#include "ios/chrome/browser/download/model/background_service/background_download_service_factory.h"
 #include "ios/chrome/browser/favicon/model/favicon_service_factory.h"
 #include "ios/chrome/browser/favicon/model/ios_chrome_favicon_loader_factory.h"
 #include "ios/chrome/browser/favicon/model/ios_chrome_large_icon_cache_factory.h"
@@ -21,16 +26,41 @@
 #include "ios/chrome/browser/history/model/top_sites_factory.h"
 #include "ios/chrome/browser/history/model/web_history_service_factory.h"
 #include "ios/chrome/browser/invalidation/model/ios_chrome_profile_invalidation_provider_factory.h"
+#include "ios/chrome/browser/metrics/model/google_groups_manager_factory.h"
+#include "ios/chrome/browser/optimization_guide/model/optimization_guide_service_factory.h"
+#include "ios/chrome/browser/page_info/about_this_site_service_factory.h"
+#include "ios/chrome/browser/passwords/model/ios_chrome_account_password_store_factory.h"
+#include "ios/chrome/browser/passwords/model/ios_chrome_password_receiver_service_factory.h"
+#include "ios/chrome/browser/passwords/model/ios_chrome_password_reuse_manager_factory.h"
+#include "ios/chrome/browser/passwords/model/ios_chrome_password_sender_service_factory.h"
 #include "ios/chrome/browser/passwords/model/ios_chrome_profile_password_store_factory.h"
+#include "ios/chrome/browser/plus_addresses/model/plus_address_service_factory.h"
+#include "ios/chrome/browser/plus_addresses/model/plus_address_setting_service_factory.h"
+#include "ios/chrome/browser/power_bookmarks/model/power_bookmark_service_factory.h"
+#include "ios/chrome/browser/push_notification/model/push_notification_browser_state_service_factory.h"
 #include "ios/chrome/browser/reading_list/model/reading_list_model_factory.h"
+#include "ios/chrome/browser/safe_browsing/model/safe_browsing_metrics_collector_factory.h"
+#include "ios/chrome/browser/saved_tab_groups/model/tab_group_sync_service_factory.h"
 #include "ios/chrome/browser/search_engines/model/template_url_service_factory.h"
+#include "ios/chrome/browser/segmentation_platform/model/segmentation_platform_service_factory.h"
 #include "ios/chrome/browser/sessions/model/session_restoration_service_factory.h"
+#include "ios/chrome/browser/shared/model/browser/browser_list_factory.h"
 #include "ios/chrome/browser/signin/model/account_consistency_service_factory.h"
+#include "ios/chrome/browser/signin/model/account_reconcilor_factory.h"
 #include "ios/chrome/browser/signin/model/identity_manager_factory.h"
+#include "ios/chrome/browser/supervised_user/model/child_account_service_factory.h"
+#include "ios/chrome/browser/supervised_user/model/list_family_members_service_factory.h"
+#include "ios/chrome/browser/supervised_user/model/supervised_user_service_factory.h"
+#include "ios/chrome/browser/supervised_user/model/supervised_user_settings_service_factory.h"
+#include "ios/chrome/browser/sync/model/device_info_sync_service_factory.h"
 #include "ios/chrome/browser/sync/model/ios_user_event_service_factory.h"
 #include "ios/chrome/browser/sync/model/model_type_store_service_factory.h"
+#include "ios/chrome/browser/sync/model/send_tab_to_self_sync_service_factory.h"
 #include "ios/chrome/browser/sync/model/session_sync_service_factory.h"
 #include "ios/chrome/browser/sync/model/sync_service_factory.h"
+#include "ios/chrome/browser/translate/model/translate_ranker_factory.h"
+#include "ios/chrome/browser/unified_consent/model/unified_consent_service_factory.h"
+#include "ios/chrome/browser/webauthn/model/ios_passkey_model_factory.h"
 #include "ios/chrome/browser/webdata_services/model/web_data_service_factory.h"
 
 #if BUILDFLAG(IOS_CREDENTIAL_PROVIDER_ENABLED)
@@ -39,30 +69,60 @@
 
 void EnsureBrowserStateKeyedServiceFactoriesBuilt() {
   autofill::PersonalDataManagerFactory::GetInstance();
-  ConsentAuditorFactory::GetInstance();
+  data_sharing::DataSharingServiceFactory::GetInstance();
+  ios::AccountBookmarkSyncServiceFactory::GetInstance();
   ios::AccountConsistencyServiceFactory::GetInstance();
+  ios::AccountReconcilorFactory::GetInstance();
   ios::BookmarkModelFactory::GetInstance();
   ios::BookmarkUndoServiceFactory::GetInstance();
   ios::FaviconServiceFactory::GetInstance();
   ios::HistoryServiceFactory::GetInstance();
+  ios::LocalOrSyncableBookmarkSyncServiceFactory::GetInstance();
   ios::TopSitesFactory::GetInstance();
   ios::AutocompleteClassifierFactory::GetInstance();
   ios::HostContentSettingsMapFactory::GetInstance();
   ios::TemplateURLServiceFactory::GetInstance();
   ios::WebDataServiceFactory::GetInstance();
   ios::WebHistoryServiceFactory::GetInstance();
+  ios::ZeroSuggestCacheServiceFactory::GetInstance();
+  segmentation_platform::SegmentationPlatformServiceFactory::GetInstance();
+  tab_groups::TabGroupSyncServiceFactory::GetInstance();
+  translate::TranslateRankerFactory::GetInstance();
+  AboutThisSiteServiceFactory::GetInstance();
+  BackgroundDownloadServiceFactory::GetInstance();
+  BrowserListFactory::GetInstance();
+  ChildAccountServiceFactory::GetInstance();
+  ConsentAuditorFactory::GetInstance();
+  DeviceInfoSyncServiceFactory::GetInstance();
+  GoogleGroupsManagerFactory::GetInstance();
   IdentityManagerFactory::GetInstance();
+  IOSChromeAccountPasswordStoreFactory::GetInstance();
   IOSChromeFaviconLoaderFactory::GetInstance();
   IOSChromeLargeIconCacheFactory::GetInstance();
   IOSChromeLargeIconServiceFactory::GetInstance();
+  IOSChromePasswordReceiverServiceFactory::GetInstance();
+  IOSChromePasswordReuseManagerFactory::GetInstance();
+  IOSChromePasswordSenderServiceFactory::GetInstance();
   IOSChromeProfilePasswordStoreFactory::GetInstance();
   IOSChromeProfileInvalidationProviderFactory::GetInstance();
+  IOSPasskeyModelFactory::GetInstance();
   IOSUserEventServiceFactory::GetInstance();
+  ListFamilyMembersServiceFactory::GetInstance();
+  OptimizationGuideServiceFactory::GetInstance();
   ModelTypeStoreServiceFactory::GetInstance();
+  PlusAddressServiceFactory::GetInstance();
+  PlusAddressSettingServiceFactory::GetInstance();
+  PowerBookmarkServiceFactory::GetInstance();
+  PushNotificationBrowserStateServiceFactory::GetInstance();
+  SupervisedUserServiceFactory::GetInstance();
   SyncServiceFactory::GetInstance();
+  UnifiedConsentServiceFactory::GetInstance();
   ReadingListModelFactory::GetInstance();
+  SafeBrowsingMetricsCollectorFactory::GetInstance();
+  SendTabToSelfSyncServiceFactory::GetInstance();
   SessionRestorationServiceFactory::GetInstance();
   SessionSyncServiceFactory::GetInstance();
+  SupervisedUserSettingsServiceFactory::GetInstance();
 
 #if BUILDFLAG(IOS_CREDENTIAL_PROVIDER_ENABLED)
   CredentialProviderServiceFactory::GetInstance();
