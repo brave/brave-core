d1cea724b18cfbeab7481c8085409d5e696fbeec
sangwoo.ko <sangwoo108@gmail.com>
2024-08-01 15:06:39 +0900

    Recover playlist item even if it's already cached.
    Cached item could be a invalid file, so we should recover it.

--- components/playlist/browser/playlist_service.cc
+++ components/playlist/browser/playlist_service.cc
@@ -895,13 +895,20 @@ void PlaylistService::RecoverLocalDataForItem(
         mojom::PlaylistItemPtr new_item =
             service->GetPlaylistItem(old_item->id);
         DCHECK(new_item);
-        DCHECK(!new_item->cached);
         DCHECK_EQ(new_item->media_source, old_item->media_source);
         DCHECK_EQ(new_item->media_path, old_item->media_path);
+        const bool was_cached = new_item->cached;
+        if (was_cached)
+          new_item->cached = false;
         new_item->media_source = found_items.front()->media_source;
-        new_item->media_path = new_item->media_path;
+        new_item->media_path = new_item->media_source;
         service->UpdatePlaylistItemValue(
             new_item->id, base::Value(ConvertPlaylistItemToValue(new_item)));
+
+        if (was_cached) {
+          service->NotifyPlaylistChanged(
+              mojom::PlaylistEvent::kItemLocalDataRemoved, new_item->id);
+        }
 
         service->RecoverLocalDataForItemImpl(
             std::move(new_item),
