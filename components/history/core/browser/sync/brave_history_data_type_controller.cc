b1e09f17608e38bbf4790e6220cb597cb7bb3687
Claudio DeSouza <cdesouza@brave.com>
2024-08-06 13:20:54 +0100

    [sync] Renaming types/files away from `ModelType`
    This change affects the name of certain types and derived types, as well
    as some shadow files we have to override the classes affected.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/d6859549f6684a0e3317979dc75b6d543328a8f7
    
    commit d6859549f6684a0e3317979dc75b6d543328a8f7
    Author: Mikel Astiz <mastiz@chromium.org>
    Date:   Mon Aug 5 10:34:29 2024 +0000
    
        [sync] Rename internal types to avoid ModelType in the name
    
        See linked bug for rationale.
    
        Pure refactoring to rename classes, generated using the following
        commands:
        git grep -l ModelType \
          -- components/sync/engine components/sync/test | \
          xargs rename 's/model_type_/data_type_/'
        git add .
        tools/git/mass-rename.py
        git grep -l ModelType | \
          xargs sed -i -E 's/ModelType(Registry|Connector|Configurer|Worker|Debug)/DataType\1/g'
        git grep -l model_type_ | \
          xargs sed -i -E 's/model_type_(registry|connector|configurer|worker|debug)/data_type_\1/g'
        git cl format
    
        Change-Id: Ied81019c80822909645fc5fc30527fe541316b15
        Bug: 356649891

--- components/history/core/browser/sync/brave_history_data_type_controller.cc
+++ components/history/core/browser/sync/brave_history_data_type_controller.cc
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "brave/components/history/core/browser/sync/brave_history_model_type_controller.h"
+#include "brave/components/history/core/browser/sync/brave_history_data_type_controller.h"
 
 namespace history {
 
