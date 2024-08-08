18373a9e324f905ba7eae90ac70be2ea9e04e839
mkarolin <max@brave.com>
2024-08-07 21:26:31 -0400

    Temporarily disable AllCommandsShouldBeExecutableWithoutCrash.
    This test is flaky.
    
    On Windows it occasionally crashes in
    
    safe_browsing::IncidentReportingService::Receiver::AddIncidentForProfile [0x00007FF7331FCD50+336]
    (C:\ws\src\chrome\browser\safe_browsing\incident_reporting\incident_reporting_service.cc:227)
    
    safe_browsing::PreferenceValidationDelegate::OnSplitPreferenceValidation [0x00007FF735421392+274]
    (C:\ws\src\chrome\browser\safe_browsing\incident_reporting\preference_validation_delegate.cc:125)
    
    prefs::mojom::TrackedPreferenceValidationDelegateStubDispatch::Accept
    
    On MacOS it times out and gets killed.
    
    In both cases the test seems to run to the end and encounters problems
    on shutting down.

--- app/command_utils_browsertest.cc
+++ app/command_utils_browsertest.cc
@@ -55,11 +55,21 @@ class CommandUtilsBrowserTest : public InProcessBrowserTest {
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX)
 };
 
+// This test is currently flaky on Windows and MacOS. On Windows it occasionally
+// crashes and on MacOS it times out. Disabling on these platforms until further
+// investigation can be done.
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
+#define MAYBE_AllCommandsShouldBeExecutableWithoutCrash \
+  DISABLED_AllCommandsShouldBeExecutableWithoutCrash
+#else
+#define MAYBE_AllCommandsShouldBeExecutableWithoutCrash \
+  AllCommandsShouldBeExecutableWithoutCrash
+#endif
 // This test is a sanity check - if commands fail here but work when testing
 // things manually there's probably a conflict with some of the other commands,
 // in which case we can just add it to the ignored commands list.
 IN_PROC_BROWSER_TEST_F(CommandUtilsBrowserTest,
-                       AllCommandsShouldBeExecutableWithoutCrash) {
+                       MAYBE_AllCommandsShouldBeExecutableWithoutCrash) {
   // Some commands, particularly those that create dialogs introduce some test
   // flakes, so we disable them.
   constexpr int kKnownGoodCommandsThatSometimesBreakTest[] = {
