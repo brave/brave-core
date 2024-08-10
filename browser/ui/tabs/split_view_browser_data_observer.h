1e9a2a320ec3ffc22d003cce0761c902530d9e48
sangwoo.ko <sangwoo108@gmail.com>
2024-08-10 19:16:50 +0900

    Fix use-after-free in BraveBrowserCommandController
    SplitViewBrowserData is cleared before the BraveBrowserController from
    `Browser::~Browser()` - it's calling `ClearAllUserData()` at the start
    of the destructor. This means that the `SplitViewBrowserData` is deleted
    before its member variables so we need to make it sure that we don't
    unsubscribe the observation after user data is freed.

--- browser/ui/tabs/split_view_browser_data_observer.h
+++ browser/ui/tabs/split_view_browser_data_observer.h
@@ -15,6 +15,7 @@ class SplitViewBrowserDataObserver : public base::CheckedObserver {
   virtual void OnWillBreakTile(const SplitViewBrowserData::Tile& tile) {}
   virtual void OnDidBreakTile(const SplitViewBrowserData::Tile& tile) {}
   virtual void OnSwapTabsInTile(const SplitViewBrowserData::Tile& tile) {}
+  virtual void OnWillDeleteBrowserData() {}
 };
 
 #endif  // BRAVE_BROWSER_UI_TABS_SPLIT_VIEW_BROWSER_DATA_OBSERVER_H_
