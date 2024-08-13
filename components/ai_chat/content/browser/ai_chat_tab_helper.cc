7c342782d48fe2d9116260be1c56173ff7acd6ed
brave-builds <brave-builds+devops@brave.com>
2024-08-13 21:14:05 +0000

    Uplift of #25105 (squashed) to release

--- components/ai_chat/content/browser/ai_chat_tab_helper.cc
+++ components/ai_chat/content/browser/ai_chat_tab_helper.cc
@@ -228,7 +228,7 @@ void AIChatTabHelper::InnerWebContentsAttached(
 void AIChatTabHelper::OnWebContentsFocused(
     content::RenderWidgetHost* render_widget_host) {
   if (IsPdf(web_contents())) {
-    CheckPDFA11yTree(web_contents()->GetPrimaryMainFrame());
+    CheckPDFA11yTree();
   }
 }
 
@@ -311,8 +311,9 @@ uint32_t AIChatTabHelper::GetMaxPageContentLength() {
   return GetCurrentModel().max_page_content_length;
 }
 
-void AIChatTabHelper::CheckPDFA11yTree(content::RenderFrameHost* primary_rfh) {
+void AIChatTabHelper::CheckPDFA11yTree() {
   DVLOG(3) << __func__ << ": " << GetPageURL();
+  auto* primary_rfh = web_contents()->GetPrimaryMainFrame();
   if (!primary_rfh || is_pdf_a11y_info_loaded_) {
     return;
   }
@@ -329,7 +330,7 @@ void AIChatTabHelper::CheckPDFA11yTree(content::RenderFrameHost* primary_rfh) {
     base::SequencedTaskRunner::GetCurrentDefault()->PostDelayedTask(
         FROM_HERE,
         base::BindOnce(&AIChatTabHelper::CheckPDFA11yTree,
-                       weak_ptr_factory_.GetWeakPtr(), primary_rfh),
+                       weak_ptr_factory_.GetWeakPtr()),
         base::Seconds(3));
     return;
   }
