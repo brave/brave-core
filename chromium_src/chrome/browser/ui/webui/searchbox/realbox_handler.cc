3311322f5a29e714553a9134f35cd47dd176ec39
Jay Harris <jay.harris@outlook.co.nz>
2024-08-13 12:03:47 +1200

    Changes for PR

--- chromium_src/chrome/browser/ui/webui/searchbox/realbox_handler.cc
+++ chromium_src/chrome/browser/ui/webui/searchbox/realbox_handler.cc
@@ -5,9 +5,26 @@
 
 #include "chrome/browser/ui/webui/searchbox/realbox_handler.h"
 
+#include "brave/components/search_engines/brave_prepopulated_engines.h"
 #include "components/omnibox/browser/autocomplete_input.h"
 #include "content/public/browser/web_contents.h"
-#include "brave/components/search_engines/brave_prepopulated_engines.h"
+#include "net/base/url_util.h"
+
+namespace {
+
+content::OpenURLParams MaybeOverrideURLParams(content::OpenURLParams params,
+                                              TemplateURL* template_url) {
+  if (template_url &&
+      template_url->prepopulate_id() ==
+          TemplateURLPrepopulateData::PREPOPULATED_ENGINE_ID_BRAVE) {
+    params.url =
+        net::AppendOrReplaceQueryParameter(params.url, "source", "new_tab");
+  }
+
+  return params;
+}
+
+}  // namespace
 
 // We tweak a few AutocompleteInput settings because unlike Chromium we only
 // want keyword search results.
@@ -24,25 +41,14 @@
 // the behavior there too. This seems like the patch of least changes:
 // 1. If this is a keyword search with Brave Search
 // 2. Then replace &source=desktop with &source=new_tab
-#define OpenURL(params, callback)                                             \
-  GetURL();                                                                   \
-  GURL url = destination_url;                                                 \
-  auto* turl =                                                                \
-      GetTemplateURLService()->GetTemplateURLForKeyword(match.keyword);       \
-  if (turl && turl->prepopulate_id() ==                                       \
-                  TemplateURLPrepopulateData::PREPOPULATED_ENGINE_ID_BRAVE) { \
-    auto raw_gurl = url.spec();                                               \
-    base::ReplaceFirstSubstringAfterOffset(&raw_gurl, 0, "&source=desktop",   \
-                                           "&source=new_tab");                \
-    url = GURL(raw_gurl);                                                     \
-  }                                                                           \
-  web_contents_->OpenURL(                                                     \
-      content::OpenURLParams(url, content::Referrer(), disposition,           \
-                             transition, false),                              \
-      callback)
+#define OpenURL(PARAMS, CALLBACK)                                        \
+  OpenURL(MaybeOverrideURLParams(                                        \
+              PARAMS, GetTemplateURLService()->GetTemplateURLForKeyword( \
+                          match.keyword)),                               \
+          CALLBACK)
 
 #include "src/chrome/browser/ui/webui/searchbox/realbox_handler.cc"
 
+#undef OpenURL
 #undef set_prefer_keyword
 #undef set_allow_exact_keyword_match
-#undef OpenURL
