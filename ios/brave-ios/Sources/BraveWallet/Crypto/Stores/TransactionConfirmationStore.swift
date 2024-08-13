6c04de27a5166f78bf6de91e174db671663fe7f1
Nuo Xu <nuoxu.nx@gmail.com>
2024-08-13 17:31:24 -0400

    address review comments.

--- ios/brave-ios/Sources/BraveWallet/Crypto/Stores/TransactionConfirmationStore.swift
+++ ios/brave-ios/Sources/BraveWallet/Crypto/Stores/TransactionConfirmationStore.swift
@@ -573,6 +573,7 @@ public class TransactionConfirmationStore: ObservableObject, WalletObserverStore
       value = details.fromAmount
       fiat = details.fromFiat ?? ""
 
+      // TODO: Some UI updates for solana compressed NFT transfer brave-browser#40433
       if activeParsedTransaction.transaction.txType == .solanaCompressedNftTransfer {
         symbol = "SOL"
       }
