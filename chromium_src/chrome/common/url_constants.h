dacaab55fc5961cf8392b40c52b57ee2a6386bc9
Claudio DeSouza <cdesouza@brave.com>
2024-08-15 13:45:58 +0100

    `kFirstPartySetsLearnMoreURL` renamed
    `FirstPartySets` has been migrated to `RelatedWebsiteSets` in upstream.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/4f4f9b1b8084f91c086be9f45ecde481677f9e69
    
    commit 4f4f9b1b8084f91c086be9f45ecde481677f9e69
    Author: Kene Okoye <kenok@google.com>
    Date:   Wed Aug 14 18:36:37 2024 +0000
    
        [PS] Cleanup - Migrate FirstPartySets to RelatedWebsiteSets in C++ UI
    
        This changes variable and class names with  "FirstPartySets", "fps", or a similar variant in them to "RelatedWebsiteSets", "rws", etc. respectively. Strings are mainly untouched
    
        Bug: b:331453627

--- chromium_src/chrome/common/url_constants.h
+++ chromium_src/chrome/common/url_constants.h
@@ -86,7 +86,7 @@ inline constexpr char kChromeHelpViaMenuURL[] = "https://support.brave.com/";
 
 inline constexpr char kChromeHelpViaWebUIURL[] = "https://support.brave.com/";
 
-inline constexpr char kFirstPartySetsLearnMoreURL[] =
+inline constexpr char kRelatedWebsiteSetsLearnMoreURL[] =
     "https://support.brave.com/";
 
 // The isolated-app: scheme is used for Isolated Web Apps. A public explainer
