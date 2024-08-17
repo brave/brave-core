834228ea001ce1551530b37af815304abe520b9c
ajayseeker <ajay257223@gmail.com>
2024-08-17 16:26:50 +0530

    moved style changes to RegisterStyleOverride method

--- browser/resources/settings/brave_overrides/search_engines_page.ts
+++ browser/resources/settings/brave_overrides/search_engines_page.ts
@@ -4,10 +4,12 @@
 // you can obtain one at https://mozilla.org/MPL/2.0/.
 
 import {RegisterPolymerTemplateModifications} from 'chrome://resources/brave/polymer_overriding.js'
+import {RegisterStyleOverride} from 'chrome://resources/brave/polymer_overriding.js'
+import {html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js'
 
 RegisterPolymerTemplateModifications({
     'settings-search-engines-page': (templateContent) => {
-        var addSearchEngineButton = templateContent.getElementById("addSearchEngine");
+        const addSearchEngineButton = templateContent.getElementById("addSearchEngine");
         if(!addSearchEngineButton) {
             console.error("[Brave Settings Overrides] Couldn't find addSearchEngine button");
             return;
@@ -19,10 +21,18 @@ RegisterPolymerTemplateModifications({
             return;
         }
 
-        //moving addSearchEngine button below Active Search Engines list and 
-        //aligning it towards the right end
-        addSearchEngineButton.style.float = 'right';
-        addSearchEngineButton.style.margin = '0% 5% 0% 0%';
+        //moving addSearchEngine button below Active Search Engines list
         activeEnginesList.after(addSearchEngineButton);
     }
 })
+
+RegisterStyleOverride(
+    'settings-search-engines-page',
+    html`
+      <style>
+        #addSearchEngine {
+          float: right;
+          margin: 0 5% 0 0;
+        }
+      </style>
+    ` )
