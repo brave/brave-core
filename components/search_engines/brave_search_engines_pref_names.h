5283f06f7eccd03017b22e5227275034c5e30e60
brave-builds <brave-builds+devops@brave.com>
2024-08-18 18:38:50 +0000

    Uplift of #23659 (squashed) to beta

---
+++ components/search_engines/brave_search_engines_pref_names.h
@@ -0,0 +1,18 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_SEARCH_ENGINES_BRAVE_SEARCH_ENGINES_PREF_NAMES_H_
+#define BRAVE_COMPONENTS_SEARCH_ENGINES_BRAVE_SEARCH_ENGINES_PREF_NAMES_H_
+
+#include "build/build_config.h"
+
+namespace prefs {
+
+inline constexpr char kAddOpenSearchEngines[] =
+    "brave.other_search_engines_enabled";
+
+}  // namespace prefs
+
+#endif  // BRAVE_COMPONENTS_SEARCH_ENGINES_BRAVE_SEARCH_ENGINES_PREF_NAMES_H_
