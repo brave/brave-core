cd5a21f236a4eed6a0d9518ebab79b901d072f23
Brandon T <JustBrandonT@gmail.com>
2024-08-19 16:16:39 -0400

    Fix crash in translation + add UI to translate via Menu

--- ios/brave-ios/Sources/Brave/Frontend/UserContent/UserScripts/Scripts_Dynamic/ScriptHandlers/Sandboxed/BraveTranslateScriptHandler.swift
+++ ios/brave-ios/Sources/Brave/Frontend/UserContent/UserScripts/Scripts_Dynamic/ScriptHandlers/Sandboxed/BraveTranslateScriptHandler.swift
@@ -6,33 +6,24 @@
 import Foundation
 import NaturalLanguage
 import Shared
+import SwiftUI
 import Translation
 import WebKit
 import os.log
-
-enum BraveTranslateError: Error {
-  case invalidURL
-  case invalidLanguage
-  case sameLanguage
-  case invalidPageSource
-  case invalidTranslationResponseCode
-  case invalidTranslationResponse
-  case otherError
-}
-
-struct BraveTranslateLanguageInfo {
-  let currentLanguage: Locale.Language
-  let pageLanguage: Locale.Language
-}
 
 class BraveTranslateScriptHandler: NSObject, TabContentScript {
   private weak var tab: Tab?
   private let recognizer = NLLanguageRecognizer()
   private static let namespace = "translate_\(uniqueID)"
 
-  private var translationSession: Any?
+  private var url: URL?
+  private var isTranslationReady = false
+  private var translationController: UIHostingController<BraveTranslateContainerView>!
+  private var translationSession: BraveTranslateSession?
+  private var urlObserver: NSObjectProtocol?
+  private var currentLanguageInfo = BraveTranslateLanguageInfo()
 
-  private struct RequestMessage: Codable {
+  struct RequestMessage: Codable {
     let method: String
     let url: URL
     let headers: [String: String]
@@ -41,7 +32,37 @@ class BraveTranslateScriptHandler: NSObject, TabContentScript {
 
   init(tab: Tab) {
     self.tab = tab
+    self.url = tab.url
     super.init()
+
+    translationController = UIHostingController(
+      rootView: BraveTranslateContainerView(
+        onTranslationSessionUpdated: { [weak self] session in
+          self?.translationSession = session
+        },
+        languageInfo: currentLanguageInfo
+      )
+    )
+
+    urlObserver = tab.webView?.observe(
+      \.url,
+      options: [.new],
+      changeHandler: { [weak self] _, change in
+        guard let self = self, let url = change.newValue else { return }
+        if self.url != url {
+          self.url = url
+          self.isTranslationReady = false
+        }
+      }
+    )
+  }
+
+  deinit {
+    print("DEALLOCATED TRANSLATION!")
+
+    translationController?.willMove(toParent: nil)
+    translationController?.removeFromParent()
+    translationController = nil
   }
 
   static let scriptName = "BraveTranslateScript"
@@ -142,13 +163,7 @@ class BraveTranslateScriptHandler: NSObject, TabContentScript {
     return .init(currentLanguage: Locale.current.language, pageLanguage: pageLanguage)
   }
 
-  @available(iOS 18.0, *)
-  func activateScript(using session: TranslationSession, languageInfo: BraveTranslateLanguageInfo) {
-    self.translationSession = session
-    activateScript(languageInfo: languageInfo)
-  }
-
-  func activateScript(languageInfo: BraveTranslateLanguageInfo) {
+  func startTranslation(languageInfo: BraveTranslateLanguageInfo) {
     Task { @MainActor in
       guard let currentLanguage = languageInfo.currentLanguage.languageCode?.identifier,
         let pageLanguage = languageInfo.pageLanguage.languageCode?.identifier,
@@ -164,13 +179,43 @@ class BraveTranslateScriptHandler: NSObject, TabContentScript {
     }
   }
 
+  func presentUI(on controller: UIViewController) {
+    guard isTranslationReady else {
+      // TODO: Show Error to the user!
+      return
+    }
+
+    if translationController?.parent != controller {
+      controller.addChild(translationController)
+      tab?.webView?.addSubview(translationController.view)
+      translationController.didMove(toParent: controller)
+      tab?.webView?.sendSubviewToBack(translationController.view)
+    }
+
+    let picker = UIHostingController(
+      rootView: BraveTranslateView(
+        startTranslation: { [weak self] view in
+          guard let self = self else { return }
+          self.startTranslation(languageInfo: self.currentLanguageInfo)
+        },
+        languageInfo: currentLanguageInfo
+      )
+    )
+    controller.present(picker, animated: true)
+  }
+
   func userContentController(
     _ userContentController: WKUserContentController,
     didReceiveScriptMessage message: WKScriptMessage,
     replyHandler: @escaping (Any?, String?) -> Void
   ) {
+    guard translationController != nil else {
+      Logger.module.error("Invalid Translation Controller")
+      return
+    }
+
     guard let body = message.body as? [String: Any] else {
-      Logger.module.error("INVALID BRAVE TRANSLATE MESSAGE")
+      Logger.module.error("Invalid Brave Translate Message")
       return
     }
 
@@ -182,45 +227,19 @@ class BraveTranslateScriptHandler: NSObject, TabContentScript {
             from: JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
           )
 
-          if #available(iOS 18.0, *),
-            let translationSession = translationSession as? TranslationSession,
-            message.url.path.starts(with: "/translate_a/")
+          let translationSession = self.translationSession ?? BraveTranslateSession()
+
+          // The message is for HTML or CSS request
+          if self.translationSession == nil
+            && BraveTranslateSession.isPhraseTranslationRequest(message)
           {
-            let components = URLComponents(string: "https://translate.brave.com?\(message.body)")
-            let phrases = components!.queryItems!.map({ "\($0.value ?? "")" })
-            let results = try await translationSession.translations(
-              from: phrases.map({ .init(sourceText: $0) })
-            ).map({ $0.targetText })
-
-            let data = try JSONSerialization.data(withJSONObject: results, options: [])
-
-            replyHandler(
-              [
-                "value": [
-                  "statusCode": 200,
-                  "responseType": "",
-                  "response": String(data: data, encoding: String.Encoding.utf8) ?? "",
-                  "headers": "",
-                ]
-              ],
-              nil
-            )
-            return
+            throw BraveTranslateError.otherError
           }
 
-          var request = URLRequest(url: message.url)
-          request.httpMethod = message.method
-          request.httpBody = message.body.data(using: .utf8)
-          message.headers.forEach { (key, value) in
-            request.setValue(value, forHTTPHeaderField: key)
-          }
-
-          let session = URLSession(configuration: .ephemeral)
-          defer { session.finishTasksAndInvalidate() }
-          let (data, response) = try await session.data(for: request)
+          let (data, response) = try await translationSession.translate(message)
 
           guard let response = response as? HTTPURLResponse else {
-            return
+            throw BraveTranslateError.invalidTranslationResponse
           }
 
           replyHandler(
@@ -237,7 +256,8 @@ class BraveTranslateScriptHandler: NSObject, TabContentScript {
             nil
           )
         } catch {
-          print("ERROR: \(error)")
+          Logger.module.error("Brave Translate Error: \(error)")
+          replyHandler(nil, "Translation Error")
         }
       }
 
@@ -245,13 +265,198 @@ class BraveTranslateScriptHandler: NSObject, TabContentScript {
     }
 
     if body["command"] as? String == "ready" {
-      print("TRANSLATE IS READY!")
+      isTranslationReady = true
 
       Task { @MainActor in
-        self.activateScript(languageInfo: await getLanguageInfo())
+        let languageInfo = await getLanguageInfo()
+        self.currentLanguageInfo.pageLanguage = languageInfo.pageLanguage
+        self.currentLanguageInfo.currentLanguage = languageInfo.currentLanguage
       }
     }
 
     replyHandler(nil, nil)
+  }
+}
+
+enum BraveTranslateError: Error {
+  case invalidURL
+  case invalidLanguage
+  case sameLanguage
+  case invalidPageSource
+  case invalidTranslationResponseCode
+  case invalidTranslationResponse
+  case otherError
+}
+
+class BraveTranslateLanguageInfo: NSObject, ObservableObject {
+  @Published
+  var currentLanguage: Locale.Language
+
+  @Published
+  var pageLanguage: Locale.Language
+
+  override init() {
+    currentLanguage = .init(identifier: Locale.current.identifier)
+    pageLanguage = .init(identifier: Locale.current.identifier)
+    super.init()
+  }
+
+  init(currentLanguage: String, pageLanguage: String) {
+    self.currentLanguage = .init(identifier: currentLanguage)
+    self.pageLanguage = .init(identifier: pageLanguage)
+    super.init()
+  }
+
+  init(currentLanguage: Locale.Language, pageLanguage: Locale.Language) {
+    self.currentLanguage = currentLanguage
+    self.pageLanguage = pageLanguage
+    super.init()
+  }
+}
+
+class BraveTranslateSession {
+  static func isPhraseTranslationRequest(
+    _ request: BraveTranslateScriptHandler.RequestMessage
+  ) -> Bool {
+    return request.url.path.starts(with: "/translate_a/")
+  }
+
+  func translate(
+    _ request: BraveTranslateScriptHandler.RequestMessage
+  ) async throws -> (data: Data, response: URLResponse) {
+    var urlRequest = URLRequest(url: request.url)
+    urlRequest.httpMethod = request.method
+    urlRequest.httpBody = request.body.data(using: .utf8)
+    request.headers.forEach { (key, value) in
+      urlRequest.setValue(value, forHTTPHeaderField: key)
+    }
+
+    let session = URLSession(configuration: .ephemeral)
+    defer { session.finishTasksAndInvalidate() }
+    return try await session.data(for: urlRequest)
+  }
+}
+
+@available(iOS 18.0, *)
+class BraveTranslateSessionApple: BraveTranslateSession {
+  private weak var session: TranslationSession?
+
+  init(session: TranslationSession) {
+    self.session = session
+  }
+
+  override func translate(
+    _ request: BraveTranslateScriptHandler.RequestMessage
+  ) async throws -> (data: Data, response: URLResponse) {
+    // Do not attempt to translate requests to html and css from Brave-Translate script
+    guard Self.isPhraseTranslationRequest(request) else {
+      return try await super.translate(request)
+    }
+
+    guard let session = session else {
+      throw BraveTranslateError.otherError
+    }
+
+    let components = URLComponents(string: "https://translate.brave.com?\(request.body)")
+    let phrases = components!.queryItems!.map({ "\($0.value ?? "")" })
+    let results = try await session.translations(
+      from: phrases.map({ .init(sourceText: $0) })
+    ).map({ $0.targetText })
+
+    let data = try JSONSerialization.data(withJSONObject: results, options: [])
+    let response =
+      HTTPURLResponse(
+        url: request.url,
+        statusCode: 200,
+        httpVersion: "HTTP/1.1",
+        headerFields: ["Content-Type": "text/html"]
+      )
+      ?? URLResponse(
+        url: request.url,
+        mimeType: "text/html",
+        expectedContentLength: data.count,
+        textEncodingName: "UTF-8"
+      )
+    return (data, response)
+  }
+}
+
+struct BraveTranslateContainerView: View {
+  var onTranslationSessionUpdated: ((BraveTranslateSession) -> Void)?
+
+  @ObservedObject
+  var languageInfo: BraveTranslateLanguageInfo
+
+  var body: some View {
+    Color.clear
+      .osAvailabilityModifiers({ view in
+        if #available(iOS 18.0, *) {
+          view
+            .translationTask(
+              .init(source: languageInfo.pageLanguage, target: languageInfo.currentLanguage),
+              action: { session in
+                onTranslationSessionUpdated?(BraveTranslateSessionApple(session: session))
+              }
+            )
+        } else {
+          view.task {
+            onTranslationSessionUpdated?(BraveTranslateSession())
+          }
+        }
+      })
+  }
+}
+
+struct BraveTranslateView: View {
+  var startTranslation: ((BraveTranslateView) -> Void)?
+
+  @ObservedObject
+  var languageInfo: BraveTranslateLanguageInfo
+
+  @State
+  var supportedLanguages = [Locale.Language]()
+
+  var body: some View {
+    VStack {
+      //      Picker("Translate From: ", selection: $languageInfo.pageLanguage) {
+      //        ForEach(supportedLanguages, id: \.languageCode) {
+      //          if let languageCode = $0.languageCode?.identifier {
+      //            Text(Locale.current.localizedString(forIdentifier: languageCode) ?? languageCode)
+      //          }
+      //        }
+      //      }
+      //
+      //      Picker("Translate To: ", selection: $languageInfo.currentLanguage) {
+      //        ForEach(supportedLanguages, id: \.languageCode) {
+      //          if let languageCode = $0.languageCode?.identifier {
+      //            Text(Locale.current.localizedString(forIdentifier: languageCode) ?? languageCode)
+      //          }
+      //        }
+      //      }
+
+      if let pageLanguage = languageInfo.pageLanguage.languageCode?.identifier,
+        let currentLanguage = languageInfo.currentLanguage.languageCode?.identifier
+      {
+        Text(
+          "Translate From: \(Locale.current.localizedString(forIdentifier: pageLanguage) ?? pageLanguage) To: \(Locale.current.localizedString(forIdentifier: currentLanguage) ?? currentLanguage)"
+        )
+      }
+
+      Text("Translate From: \(languageInfo.pageLanguage) To: \(languageInfo.currentLanguage)")
+
+      Button {
+        startTranslation?(self)
+      } label: {
+        Text("Translate")
+      }
+    }
+    .frame(maxHeight: 100.0)
+    .task {
+      if #available(iOS 18.0, *) {
+        supportedLanguages = await LanguageAvailability().supportedLanguages
+      } else {
+        supportedLanguages = Locale.Language.systemLanguages
+      }
+    }
   }
 }
