f67719cfb677ee5ccbe3ae54542d65d8861b55a5
Artem Samoilenko <artem@brave.com>
2024-08-19 11:59:55 -0400

    [Android] Added actions to tab switcher context menu
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/5c504189e14455bc3386ef55b13149379be02098
    
    [IncognitoMigration] Added actions to tab switcher context menu.
    
    Screenshot:
    https://screenshot.googleplex.com/NBQfLB4wwdkk3F4
    https://screenshot.googleplex.com/4BjZogUzAgX3KaP
    
    Bug: 356925907

--- browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/BottomTabSwitcherActionMenuCoordinator.java
+++ browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/BottomTabSwitcherActionMenuCoordinator.java
@@ -8,7 +8,9 @@ package org.chromium.chrome.browser.toolbar.top;
 import android.view.View.OnLongClickListener;
 
 import org.chromium.base.Callback;
+import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.ui.modelutil.MVCListAdapter.ModelList;
 
 /**
@@ -16,14 +18,18 @@ import org.chromium.ui.modelutil.MVCListAdapter.ModelList;
  * creating the popup menu and building a list of menu items.
  */
 public class BottomTabSwitcherActionMenuCoordinator extends TabSwitcherActionMenuCoordinator {
-    public BottomTabSwitcherActionMenuCoordinator(Profile profile) {
-        super(profile);
+    public BottomTabSwitcherActionMenuCoordinator(
+            Profile profile, ObservableSupplier<TabModelSelector> tabModelSelectorSupplier) {
+        super(profile, tabModelSelectorSupplier);
     }
 
     public static OnLongClickListener createOnLongClickListener(
-            Callback<Integer> onItemClicked, Profile profile) {
+            Callback<Integer> onItemClicked,
+            Profile profile,
+            ObservableSupplier<TabModelSelector> tabModelSelectorSupplier) {
         return createOnLongClickListener(
-                new BottomTabSwitcherActionMenuCoordinator(profile), onItemClicked);
+                new BottomTabSwitcherActionMenuCoordinator(profile, tabModelSelectorSupplier),
+                onItemClicked);
     }
 
     @Override
