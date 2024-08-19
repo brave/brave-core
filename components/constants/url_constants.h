be913deec8ee97d79c4db825d7ddc9c2faa8001b
mkarolin <max@brave.com>
2024-08-19 14:59:02 -0400

    Fixes re-install link when browser is out of date for too long.

--- components/constants/url_constants.h
+++ components/constants/url_constants.h
@@ -37,11 +37,6 @@ inline constexpr char kDevChannelDeprecationLearnMoreUrl[] =
     "17924707453581-How-do-I-migrate-my-Brave-Dev-data-to-another-channel-"
     "Nightly-Beta-Release-";
 
-// This is introduced to replace |kDownloadChromeUrl| in
-// outdated_upgrade_bubble_view.cc"
-// |kDownloadChromeUrl| couldn't be replaced with char array because array
-// should be initialized with initialize list or string literal.
-// So, this macro is used.
-#define kDownloadBraveUrl "https://www.brave.com/download"
+inline constexpr char kDownloadBraveUrl[] = "https://www.brave.com/download";
 
 #endif  // BRAVE_COMPONENTS_CONSTANTS_URL_CONSTANTS_H_
