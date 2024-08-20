73ca8a0ebdbd71e88105c283d9cc11a71525ecea
brave-builds <brave-builds+devops@brave.com>
2024-08-20 05:05:52 +0000

    Uplift of #25202 (squashed) to beta

--- chromium_src/chrome/browser/ui/dialogs/outdated_upgrade_bubble.cc
+++ chromium_src/chrome/browser/ui/dialogs/outdated_upgrade_bubble.cc
@@ -3,6 +3,36 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "base/version_info/channel.h"
 #include "brave/components/constants/url_constants.h"  // for kDownloadBraveUrl
+#include "chrome/common/channel_info.h"
+#include "content/public/browser/page_navigator.h"
+#include "url/gurl.h"
+
+namespace {
+
+const char* BraveGetUpdateUrlSuffix(version_info::Channel channel) {
+  switch (channel) {
+    case version_info::Channel::CANARY:
+      return "-nightly";
+    case version_info::Channel::DEV:
+      return "-dev";
+    case version_info::Channel::BETA:
+      return "-beta";
+    case version_info::Channel::UNKNOWN:
+    case version_info::Channel::STABLE:
+      return "";
+  }
+}
+
+GURL BraveGetUpdateUrl() {
+  return GURL(std::string(kDownloadBraveUrl) +
+              BraveGetUpdateUrlSuffix(chrome::GetChannel()));
+}
+
+}  // namespace
+
+#define OpenURLParams(URL, ...) OpenURLParams(BraveGetUpdateUrl(), __VA_ARGS__)
 
 #include "src/chrome/browser/ui/dialogs/outdated_upgrade_bubble.cc"
+#undef OpenURLParams
