5e59cb1bc1408b238b9793a19fea129594ec686f
mkarolin <max@brave.com>
2024-08-20 14:45:42 -0400

    Adds missing copyright headers.

--- build/commands/lib/test.js
+++ build/commands/lib/test.js
@@ -1,3 +1,8 @@
+ // Copyright (c) 2018 The Brave Authors. All rights reserved.
+ // This Source Code Form is subject to the terms of the Mozilla Public
+ // License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ // You can obtain one at https://mozilla.org/MPL/2.0/.
+
 const fs = require('fs-extra')
 const path = require('path')
 
