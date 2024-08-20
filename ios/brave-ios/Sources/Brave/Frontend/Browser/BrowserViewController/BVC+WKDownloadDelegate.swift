bd7733a31301fa75a779bd30a23f3618c19a3177
brave-builds <brave-builds+devops@brave.com>
2024-08-20 16:13:54 +0000

    Uplift of #25102 (squashed) to beta

--- ios/brave-ios/Sources/Brave/Frontend/Browser/BrowserViewController/BVC+WKDownloadDelegate.swift
+++ ios/brave-ios/Sources/Brave/Frontend/Browser/BrowserViewController/BVC+WKDownloadDelegate.swift
@@ -11,25 +11,38 @@ import WebKit
 
 extension BrowserViewController: WKDownloadDelegate {
 
-  struct PendingDownload: Hashable, Equatable {
-    let fileURL: URL
-    let response: URLResponse
-    let suggestedFileName: String
-  }
-
   public func download(
     _ download: WKDownload,
     decideDestinationUsing response: URLResponse,
     suggestedFilename: String
   ) async -> URL? {
+
+    if let httpResponse = response as? HTTPURLResponse {
+      if httpResponse.mimeType != MIMEType.passbook {
+        let shouldDownload = await downloadAlert(
+          download,
+          response: response,
+          suggestedFileName: suggestedFilename
+        )
+        if !shouldDownload {
+          return nil
+        }
+      }
+    }
+
     let temporaryDir = NSTemporaryDirectory()
     let fileName = temporaryDir + "/" + suggestedFilename
     let url = URL(fileURLWithPath: fileName)
-    pendingDownloads[download] = PendingDownload(
+    let pendingDownload = WebKitDownload(
       fileURL: url,
       response: response,
-      suggestedFileName: suggestedFilename
+      suggestedFileName: suggestedFilename,
+      download: download,
+      downloadQueue: downloadQueue
     )
+
+    downloadQueue.enqueue(pendingDownload)
+
     return url
   }
 
@@ -50,11 +63,14 @@ extension BrowserViewController: WKDownloadDelegate {
 
   @MainActor
   public func downloadDidFinish(_ download: WKDownload) {
-    guard let downloadInfo = pendingDownloads[download] else {
+    guard
+      let downloadInfo = downloadQueue.downloads.compactMap({ $0 as? WebKitDownload }).first(
+        where: { $0.download == download })
+    else {
       return
     }
 
-    pendingDownloads.removeValue(forKey: download)
+    downloadQueue.download(downloadInfo, didFinishDownloadingTo: downloadInfo.fileURL)
 
     let response = URLResponse(
       url: downloadInfo.fileURL,
@@ -63,15 +79,17 @@ extension BrowserViewController: WKDownloadDelegate {
       textEncodingName: downloadInfo.response.textEncodingName
     )
 
-    if let passbookHelper = OpenPassBookHelper(
-      request: nil,
-      response: response,
-      canShowInWebView: false,
-      forceDownload: false,
-      browserViewController: self
-    ) {
-      Task {
-        await passbookHelper.open()
+    if downloadInfo.response.mimeType == MIMEType.passbook {
+      if let passbookHelper = OpenPassBookHelper(
+        request: nil,
+        response: response,
+        canShowInWebView: false,
+        forceDownload: false,
+        browserViewController: self
+      ) {
+        Task {
+          await passbookHelper.open()
+        }
       }
     }
 
@@ -82,6 +100,15 @@ extension BrowserViewController: WKDownloadDelegate {
 
   @MainActor
   public func download(_ download: WKDownload, didFailWithError error: Error, resumeData: Data?) {
+    guard
+      let downloadInfo = downloadQueue.downloads.compactMap({ $0 as? WebKitDownload }).first(
+        where: { $0.download == download })
+    else {
+      return
+    }
+
+    downloadQueue.download(downloadInfo, didCompleteWithError: error)
+
     // display an error
     let alertController = UIAlertController(
       title: Strings.unableToAddPassErrorTitle,
@@ -94,5 +121,68 @@ extension BrowserViewController: WKDownloadDelegate {
       }
     )
     present(alertController, animated: true, completion: nil)
+  }
+
+  @MainActor
+  private func downloadAlert(
+    _ download: WKDownload,
+    response: URLResponse,
+    suggestedFileName: String
+  ) async -> Bool {
+    // Only download if there is a valid host
+    guard let host = download.originalRequest?.url?.host() else {
+      return false
+    }
+
+    // Never present the download alert on a tab that isn't visible
+    guard let webView = download.webView, let tab = tabManager.tabForWebView(webView),
+      tab === tabManager.selectedTab
+    else {
+      return false
+    }
+
+    let filename = HTTPDownload.stripUnicode(fromFilename: suggestedFileName)
+    let totalBytesExpected =
+      response.expectedContentLength > 0 ? response.expectedContentLength : nil
+
+    let expectedSize =
+      totalBytesExpected != nil
+      ? ByteCountFormatter.string(fromByteCount: totalBytesExpected!, countStyle: .file)
+      : nil
+
+    let title = "\(filename) - \(host)"
+
+    var downloadActionText = Strings.download
+    if let expectedSize = expectedSize {
+      downloadActionText += " (\(expectedSize))"
+    }
+
+    return await withCheckedContinuation { continuation in
+      let downloadAlert = UIAlertController(
+        title: title,
+        message: nil,
+        preferredStyle: .actionSheet
+      )
+
+      downloadAlert.addAction(
+        UIAlertAction(title: downloadActionText, style: .default) { _ in
+          continuation.resume(returning: true)
+        }
+      )
+
+      downloadAlert.addAction(
+        UIAlertAction(title: Strings.cancelButtonTitle, style: .cancel) { _ in
+          continuation.resume(returning: false)
+        }
+      )
+
+      downloadAlert.popoverPresentationController?.do {
+        $0.sourceView = view
+        $0.sourceRect = CGRect(x: view.bounds.midX, y: view.bounds.maxY - 16, width: 0, height: 0)
+        $0.permittedArrowDirections = []
+      }
+
+      present(downloadAlert, animated: true, completion: nil)
+    }
   }
 }
