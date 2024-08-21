f25a0d9e9aa0519f52750b1588f0f8ad59147b9a
AlexeyBarabash <AlexeyBarabash@users.noreply.github.com>
2024-08-21 21:09:35 +0300

    [Android] 2nd follow-up for cr128 GP top crash (#25254)
    * Revert "[Android] Follow-up for cr128 top GP crash (#25191)"
    
    This reverts commit a7407de42a300a2e83c8edb3017642ae2108f859.
    
    * Revert "[Android] Fix crash after cr128 upgrade at TabSwitcherPaneCoordinator (#25130)"
    
    This reverts commit 68bc172deeba826035da08cac67cd572e821b718.
    
    * [Android] Follow-up with incognito tabs for Fix for crash after cr128 upgrade when pressing back at tab switcher
    
    Fixes brave/brave-browser#40581

---
+++ android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/BraveTabSwitcherPaneBase.java
@@ -0,0 +1,40 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+package org.chromium.chrome.browser.tasks.tab_management;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import org.chromium.chrome.browser.hub.HubContainerView;
+import org.chromium.chrome.browser.hub.HubLayoutAnimatorProvider;
+import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.browser.tasks.tab_management.TabListCoordinator.TabListMode;
+
+import java.util.function.DoubleConsumer;
+
+public abstract class BraveTabSwitcherPaneBase extends TabSwitcherPaneBase {
+    BraveTabSwitcherPaneBase(
+            @NonNull Context context,
+            @NonNull TabSwitcherPaneCoordinatorFactory factory,
+            boolean isIncognito,
+            @NonNull DoubleConsumer onToolbarAlphaChange) {
+        super(context, factory, isIncognito, onToolbarAlphaChange);
+    }
+
+    @Override
+    public @NonNull HubLayoutAnimatorProvider createHideHubLayoutAnimatorProvider(
+            @NonNull HubContainerView hubContainerView) {
+        int tabId = getCurrentTabId();
+        if (getTabListMode() != TabListMode.LIST && tabId != Tab.INVALID_TAB_ID) {
+            // Force call TabSwitcherPaneBase.createTabSwitcherPaneCoordinator
+            // to ensure TabSwitcherPaneBase.mTabSwitcherPaneCoordinatorSupplier is set
+            super.createTabSwitcherPaneCoordinator();
+        }
+
+        return super.createHideHubLayoutAnimatorProvider(hubContainerView);
+    }
+}
