f46605dd64fb1689887d59fbfae436f1204e54c5
mkarolin <max@brave.com>
2024-08-21 13:12:34 -0400

    Update chromium_src override of DownloadShelfView.
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/6df73618ddb19683e8e6954141487fc7f415e379
    
    commit 6df73618ddb19683e8e6954141487fc7f415e379
    Author: Ashish Kumar <ashishkum@microsoft.com>
    Date:   Wed Aug 14 17:27:53 2024 +0000
    
        [views-ax] Migrates role to view constructor in chrome/browser/ui/views
    
        This CL migrates setting of the accessible role from
        View::GetAccessibleNodeData to the view constructor. This ensures
        that the role is updated in the accessibility cache as soon as the
        view is constructed.
    
        The migration is applied to following files:
        1. chrome/browser/ui/views/critical_notification_bubble_view.cc
        2. chrome/browser/ui/views/desktop_capture/*
        3. chrome/browser/ui/views/download/*
        4. chrome/browser/ui/views/editor_menu/*
        5. chrome/browser/ui/views/extensions/*
    
        This CL is part of the ViewsAX project:
        https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
    
        Bug: 325137417

--- chromium_src/chrome/browser/ui/views/download/download_item_view_unittest.cc
+++ chromium_src/chrome/browser/ui/views/download/download_item_view_unittest.cc
@@ -1,12 +1,10 @@
-/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "chrome/browser/ui/views/download/download_shelf_view.h"
-
 #include "brave/browser/ui/views/download/brave_download_item_view.h"
 
 #define DownloadItemView BraveDownloadItemView
-#include "src/chrome/browser/ui/views/download/download_shelf_view.cc"
+#include "src/chrome/browser/ui/views/download/download_item_view_unittest.cc"
 #undef DownloadItemView
---
+++ chromium_src/chrome/browser/ui/views/download/download_shelf_view.h
@@ -0,0 +1,15 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_DOWNLOAD_DOWNLOAD_SHELF_VIEW_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_DOWNLOAD_DOWNLOAD_SHELF_VIEW_H_
+
+#include "chrome/browser/ui/views/download/download_item_view.h"
+
+#define DownloadItemView BraveDownloadItemView
+#include "src/chrome/browser/ui/views/download/download_shelf_view.h"  // IWYU pragma: export
+#undef DownloadItemView
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_DOWNLOAD_DOWNLOAD_SHELF_VIEW_H_
