36b5be43d13e89a8af2711a2254ecac01d3896ef
Pete Miller <miller.pete@gmail.com>
2024-08-22 16:32:23 -0700

    News: re-encode images downloaded from third parties

--- components/brave_news/browser/urls.cc
+++ components/brave_news/browser/urls.cc
@@ -7,12 +7,34 @@
 
 #include <string>
 
+#include "base/logging.h"
+#include "base/strings/strcat.h"
 #include "brave/brave_domains/service_domains.h"
 
 namespace brave_news {
 
+namespace {
+const char kBraveNewsHostnamePrefix[] = "brave-today-cdn";
+const char kPCDNHostnamePrefix[] = "pcdn";
+}  // namespace
+
 std::string GetHostname() {
-  return brave_domains::GetServicesDomain("brave-today-cdn");
+  return brave_domains::GetServicesDomain(kBraveNewsHostnamePrefix);
+}
+
+std::string GetMatchingPCDNHostname() {
+  // Check for expected PCDN hostname in the feed returned from the server
+  // that the feed files are fetched from (brave-today-cdn.xyz)
+  std::string feed_hostname = GetHostname();
+  if (!feed_hostname.starts_with(kBraveNewsHostnamePrefix)) {
+    // Format has changed, return something that otherwise makes sense
+    DLOG(ERROR) << "Feed hostname \"" << feed_hostname
+                << "\" unexpectedly did not start with prefix \""
+                << kBraveNewsHostnamePrefix << "\"";
+    return brave_domains::GetServicesDomain(kPCDNHostnamePrefix);
+  }
+  auto base = feed_hostname.substr(strlen(kBraveNewsHostnamePrefix));
+  return base::StrCat({kPCDNHostnamePrefix, base});
 }
 
 }  // namespace brave_news
