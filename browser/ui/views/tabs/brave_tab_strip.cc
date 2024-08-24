b1b392372171cb7302e0cb7f2947a3ad274f433d
Claudio DeSouza <cdesouza@brave.com>
2024-08-24 02:40:02 +0100

    Add `discarded` arg to `TabStrip` and related types
    Upstream is plumbling WebContents discard through some of types that we
    override. This is immaterial to our code, however it has to be matched
    through our overrides to be plummed through them. This change adds the
    argument to the missing interfaces.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/00d3927706c48be0896aac45e9341db2470c16bd
    
    commit 00d3927706c48be0896aac45e9341db2470c16bd
    Author: Thomas Lukaszewicz <tluk@chromium.org>
    Date:   Wed Aug 21 23:54:10 2024 +0000
    
        Reland "Implement WebContents discard by clearing the primary frame document"
    
        This is a reland of commit ce4ea2692091b299b26cc7967f9e1b980c583e09
    
        See diff against PS1.
    
        Addressed timeout issue in DiscardClearsDedicatedWorkers test.
        Destruction of blink::DedicatedWorker post Discard() relies on
        async GC sweep in V8, which can result in timeouts on bots.
    
        Updated extension tab loading status to report unloaded when the
        tab is discarded. The tab will retain its render process when
        discarded with the new mechanism (consistent with previous behavior).
    
        Original change's description:
        > Implement WebContents discard by clearing the primary frame document
        >
        > See go/discard-frame-tree for additional details and to contribute
        > to discussion.
        >
        > This carries on from fdoray's prototype (crrev.com/c/4060064) and
        > erik's follow up (crrev.com/c/5454457).
        >
        > This CL introduces a new discard mechanism that avoids needing
        > to replace the tab's WebContents.
        >
        > Discard works by clearing the primary frame tree, done via replacing
        > Blink's primary document. This frees resources associated with the
        > primary frame and clears away any child frames attached to the
        > frame tree.
        >
        > Discard does not result in creation of any session entries and does
        > not emit navigation-related events
        >
        > Key changes:
        >  - Update TabLifecycleUnit::FinishDiscard to no longer create
        >    a empty WebContents for the discard operation. Instead discard
        >    is implemented via replacing the primary blink document
        >
        >  - The tab thumbnail implementation has been updated to handle the
        >    new discard mechanism
        >
        >  - OneTimePermissionsTrackerHelper was updated to leverage the
        >    existing WebContents observation instead of relying on
        >    TabLifecycleObserver
        >
        >  - Multiple test updates / fixes to reflect discard API changes
        >    and address incorrect assumptions (active tabs cannot be
        >    discarded)
        >
        > Notifications for discard events with both the old and new
        > WebContents from the original API remain. These can be removed or
        > refactored in the follow up CLs.
        >
        > PERFETTO_TESTS=`autoninja -C out/Default perfetto_diff_tests &&
        > out/Default/bin/run_perfetto_diff_tests`
        >
        > Bug: 347770670
        > Change-Id: I32e87b2b992914a7ae0e0bbdddf78f1b27c5773b
        > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5637909
        > Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
        > Reviewed-by: Nicolás Peña <npm@chromium.org>
        > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
        > Reviewed-by: Victor Vianna <victorvianna@google.com>
        > Reviewed-by: Florian Jacky <fjacky@chromium.org>
        > Reviewed-by: David Pennington <dpenning@chromium.org>
        > Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
        > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
        > Cr-Commit-Position: refs/heads/main@{#1343920}
    
        Bug: 347770670

--- browser/ui/views/tabs/brave_tab_strip.cc
+++ browser/ui/views/tabs/brave_tab_strip.cc
@@ -45,8 +45,9 @@
 #include "ui/gfx/canvas.h"
 #include "ui/views/layout/flex_layout.h"
 
-BraveTabStrip::BraveTabStrip(std::unique_ptr<TabStripController> controller)
-    : TabStrip(std::move(controller)) {}
+BraveTabStrip::BraveTabStrip(std::unique_ptr<TabStripController> controller,
+                             TabStripModel* model)
+    : TabStrip(std::move(controller), model) {}
 
 BraveTabStrip::~BraveTabStrip() = default;
 
