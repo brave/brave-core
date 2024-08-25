740ffcd7b2efec48f3a2ba03aa7517ee220386d6
AlexeyBarabash <AlexeyBarabash@users.noreply.github.com>
2024-08-25 14:09:27 +0300

    [Android] Fix incognito switcher icon (#25291)
    * [Android] Fixed incognito icon at tab switcher; fixes brave/brave-browser#40584

--- android/java/org/chromium/base/BraveReflectionUtil.java
+++ android/java/org/chromium/base/BraveReflectionUtil.java
@@ -10,12 +10,12 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class BraveReflectionUtil {
-    private static String TAG = "BraveReflectionUtil";
+    private static final String TAG = "BraveReflectionUtil";
 
     // NOTE: For each method for invocation add appropriate test to `testMethodsForInvocationExist`
     // method in 'brave/android/javatests/org/chromium/chrome/browser/BytecodeTest.java' file with
     // checking parameter types.
-    public static Object InvokeMethod(
+    public static Object invokeMethod(
             Class methodOwner, Object obj, String method, Object... typesAndArgs) {
         try {
             Class<?>[] parameterTypes = null;
@@ -67,11 +67,11 @@ public class BraveReflectionUtil {
         return null;
     }
 
-    public static void setIntField(Class ownerClass, String fieldName, Object obj, int newValue) {
+    public static void setField(Class ownerClass, String fieldName, Object obj, Object newValue) {
         try {
             Field field = ownerClass.getDeclaredField(fieldName);
-            if (!field.isAccessible()) field.setAccessible(true);
-            field.setInt(obj, newValue);
+            field.setAccessible(true);
+            field.set(obj, newValue);
         } catch (NoSuchFieldException e) {
             Log.e(TAG, "Field not found: " + e);
             assert (false);
@@ -83,7 +83,7 @@ public class BraveReflectionUtil {
 
     // Types should be compatible after bytecode patching
     @SuppressWarnings("EqualsIncompatibleType")
-    public static Boolean EqualTypes(Class type1, Class type2) {
+    public static Boolean equalTypes(Class type1, Class type2) {
         return type1.equals(type2);
     }
 }
