32b3dc693046fb037484281f1286fe70ad842219
Mikhail Atuchin <matuchin@brave.com>
2024-08-29 17:28:21 +0700

    Disable loading the translate language list

---
+++ chromium_src/components/translate/core/browser/translate_language_list.cc
@@ -0,0 +1,22 @@
+// Copyright (c) 2024 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "components/translate/core/browser/translate_language_list.h"
+
+#include "brave/components/translate/core/common/brave_translate_features.h"
+
+#define TranslateLanguageList TranslateLanguageList_ChromiumImpl
+#include "src/components/translate/core/browser/translate_language_list.cc"
+#undef TranslateLanguageList
+
+namespace translate {
+
+void TranslateLanguageList::SetResourceRequestsAllowed(bool allowed) {
+  if (!ShouldUpdateLanguagesList())
+    allowed = false;
+  TranslateLanguageList_ChromiumImpl::SetResourceRequestsAllowed(allowed);
+}
+
+}  // namespace translate
---
+++ chromium_src/components/translate/core/browser/translate_language_list.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2024 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_TRANSLATE_CORE_BROWSER_TRANSLATE_LANGUAGE_LIST_H_
+#define BRAVE_CHROMIUM_SRC_COMPONENTS_TRANSLATE_CORE_BROWSER_TRANSLATE_LANGUAGE_LIST_H_
+
+#define TranslateLanguageList TranslateLanguageList_ChromiumImpl
+#define SetResourceRequestsAllowed virtual SetResourceRequestsAllowed
+#include "src/components/translate/core/browser/translate_language_list.h"
+#undef SetResourceRequestsAllowed
+#undef TranslateLanguageList
+
+namespace translate {
+
+class TranslateLanguageList : public TranslateLanguageList_ChromiumImpl {
+ public:
+  void SetResourceRequestsAllowed(bool allowed) override;
+};
+
+}  // namespace translate
+
+#endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_TRANSLATE_CORE_BROWSER_TRANSLATE_LANGUAGE_LIST_H_
