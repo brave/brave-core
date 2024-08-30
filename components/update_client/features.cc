2e82f1fb053c578bff4010b1fd18349e947d9c16
Michael Herrmann <michael@herrmann.io>
2024-08-30 18:33:48 +0200

    Add comment

--- components/update_client/features.cc
+++ components/update_client/features.cc
@@ -11,6 +11,13 @@ static_assert(BUILDFLAG(IS_MAC), "Currently for macOS only");
 
 namespace brave {
 
+// DO NOT TURN THIS FEATURE ON IN PRODUCTION. As of this writing, it only
+// implements the happy path of switching from Sparkle to Omaha 4 for per-user
+// installations. It does not handle system-wide implementations, nor switching
+// from Omaha 4 back to Sparkle. When you do enable the feature in the future,
+// make sure that there are no old clients that suffer from the above
+// limitations. In practice, this will very likely mean that you will need to
+// rename the feature.
 BASE_FEATURE(kBraveUseOmaha4,
              "BraveUseOmaha4",
              base::FEATURE_DISABLED_BY_DEFAULT);
