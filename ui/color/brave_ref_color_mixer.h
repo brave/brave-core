363865fa89a20f912d50e1a7a092ead2569f1d09
Jay Harris <jay.harris@outlook.co.nz>
2024-08-30 14:44:31 +1200

    [Nala]: Change baseline temperature (#25373)
    * [Nala]: Change baseline temperature
    
    * Sort variables properly and override Chromium variables Nala doesn't use
    
    * Add vertical whitespace

--- ui/color/brave_ref_color_mixer.h
+++ ui/color/brave_ref_color_mixer.h
@@ -3,8 +3,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#ifndef BRAVE_UI_COLOR_MISSING_COLOR_REF_MIXER_H_
-#define BRAVE_UI_COLOR_MISSING_COLOR_REF_MIXER_H_
+#ifndef BRAVE_UI_COLOR_BRAVE_REF_COLOR_MIXER_H_
+#define BRAVE_UI_COLOR_BRAVE_REF_COLOR_MIXER_H_
 
 #include "ui/color/color_provider.h"
 #include "ui/color/color_provider_key.h"
@@ -12,11 +12,11 @@
 namespace ui {
 
 // This adds colors which are part of the Material Design Palette that Chromium
-// doesn't define (as it doesn't need them). Nala does requirethem, so we inject
-// them here.
-void AddMissingRefColorMixerForNala(ColorProvider* provider,
-                                    const ColorProviderKey& key);
+// doesn't define (as it doesn't need them) and redefines the baseline color
+// palette to be slightly cooler.
+void AddBraveRefColorMixer(ColorProvider* provider,
+                           const ColorProviderKey& key);
 
 }  // namespace ui
 
-#endif  // BRAVE_UI_COLOR_MISSING_COLOR_REF_MIXER_H_
+#endif  // BRAVE_UI_COLOR_BRAVE_REF_COLOR_MIXER_H_
