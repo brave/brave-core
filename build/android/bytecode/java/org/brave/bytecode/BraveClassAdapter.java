5db8590332892b5c0e80430af905f1cf4a7df87b
Jagadesh P <jagadeshjai1999@gmail.com>
2024-09-02 23:54:04 +0530

    [Android] Use reflection to call |showUndoBar| in |UndoBarController| by having |BraveUndoBarControllerBase| as super to |UndoBarController|. Add byte code test for |showUndoBar| method existence and remove the unrequired patch.

--- build/android/bytecode/java/org/brave/bytecode/BraveClassAdapter.java
+++ build/android/bytecode/java/org/brave/bytecode/BraveClassAdapter.java
@@ -118,10 +118,10 @@ public class BraveClassAdapter {
         chain = new BraveToolbarManagerClassAdapter(chain);
         chain = new BraveToolbarSwipeLayoutClassAdapter(chain);
         chain = new BraveTopToolbarCoordinatorClassAdapter(chain);
+        chain = new BraveUndoBarControllerBaseClassAdapter(chain);
         chain = new BraveVariationsSeedFetcherClassAdapter(chain);
         chain = new BraveWebsiteClassAdapter(chain);
         chain = new BraveWebsitePermissionsFetcherClassAdapter(chain);
-        chain = new BraveUndoBarControllerClassAdapter(chain);
         return chain;
     }
 }
