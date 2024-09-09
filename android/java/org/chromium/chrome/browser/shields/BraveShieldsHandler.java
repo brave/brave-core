ed8444422d94bc77c8c28c4121fa3878e9bb6592
Vadym Struts <vstruts@brave.com>
2024-09-09 23:36:16 +0200

    formatting
    Signed-off-by: Vadym Struts <vstruts@brave.com>

--- android/java/org/chromium/chrome/browser/shields/BraveShieldsHandler.java
+++ android/java/org/chromium/chrome/browser/shields/BraveShieldsHandler.java
@@ -395,7 +395,8 @@ public class BraveShieldsHandler implements BraveRewardsHelper.LargeIconReadyCal
         mSiteBrokenWarningText = mPopupView.findViewById(R.id.site_broken_warning_text);
 
         mReportBrokenSiteLayout = mPopupView.findViewById(R.id.brave_shields_report_site_layout_id);
-        mReportErrorPageLayout = mPopupView.findViewById(R.id.brave_shields_report_error_page_layout_id);
+        mReportErrorPageLayout =
+                mPopupView.findViewById(R.id.brave_shields_report_error_page_layout_id);
         mThankYouLayout = mPopupView.findViewById(R.id.brave_shields_thank_you_layout_id);
         mPrivacyReportLayout = mPopupView.findViewById(R.id.brave_shields_privacy_report_layout_id);
 
@@ -769,12 +770,13 @@ public class BraveShieldsHandler implements BraveRewardsHelper.LargeIconReadyCal
 
     private void setupErrorPageLayout() {
         Button closeButton = mReportErrorPageLayout.findViewById(R.id.btn_close);
-        closeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                hideBraveShieldsMenu();
-            }
-        });
+        closeButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        hideBraveShieldsMenu();
+                    }
+                });
     }
 
     private void setUpMainSwitchLayout(boolean isChecked) {
@@ -782,20 +784,25 @@ public class BraveShieldsHandler implements BraveRewardsHelper.LargeIconReadyCal
 
         TextView mShieldDownText = mMainLayout.findViewById(R.id.shield_down_text);
         Button mReportBrokenSiteButton = mMainLayout.findViewById(R.id.btn_report_broken_site);
-        mReportBrokenSiteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Tab currentActiveTab = mIconFetcher.getTab();                
-                mMainLayout.setVisibility(View.GONE);
-                if(!currentActiveTab.isShowingErrorPage()) {
-                    mReportBrokenSiteLayout.setVisibility(View.VISIBLE);
-                    setUpReportBrokenSiteLayout();
-                } else {
-                    mReportErrorPageLayout.setVisibility(View.VISIBLE);
-                    setupErrorPageLayout();
-                }
-            }
-        });
+        mReportBrokenSiteButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Tab currentActiveTab = mIconFetcher.getTab();
+                        if(!currentActiveTab) {
+                            return;
+                        }
+                        
+                        mMainLayout.setVisibility(View.GONE);
+                        if (!currentActiveTab.isShowingErrorPage()) {
+                            mReportBrokenSiteLayout.setVisibility(View.VISIBLE);
+                            setUpReportBrokenSiteLayout();
+                        } else {
+                            mReportErrorPageLayout.setVisibility(View.VISIBLE);
+                            setupErrorPageLayout();
+                        }
+                    }
+                });
 
         LinearLayout mSiteBlockLayout = mMainLayout.findViewById(R.id.site_block_layout);
         TextView mSiteBrokenWarningText = mMainLayout.findViewById(R.id.site_broken_warning_text);
