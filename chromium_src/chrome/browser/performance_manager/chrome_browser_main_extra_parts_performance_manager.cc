b72b1fbb47c742a75eb394e959b33218ebef0e69
Claudio DeSouza <cdesouza@brave.com>
2024-09-11 01:48:14 +0100

    `page_resource_monitor.h` moved under `public/` folder
    This is merely an inclusion path correction.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/359886ccf2092449ff291788a67ffcde3cc2b8ee
    
    commit 359886ccf2092449ff291788a67ffcde3cc2b8ee
    Author: Youngsoo Choi <youngsoo.choi@lge.com>
    Date:   Fri Sep 6 03:57:59 2024 +0000
    
        [PM] Move PageResourceMonitor metric to components/.
    
        This CL moves the PageResourceMonitor metric to //components, since the
        class doesn't have any dependencies on //chrome.
    
        Bug: 40623222

--- chromium_src/chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
+++ chromium_src/chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "chrome/browser/performance_manager/metrics/page_resource_monitor.h"
+#include "components/performance_manager/public/metrics/page_resource_monitor.h"
 
 namespace performance_manager::metrics {
 namespace {
