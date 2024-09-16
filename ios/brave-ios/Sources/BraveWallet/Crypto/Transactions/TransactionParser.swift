4d436e00edf62ae821ffe98066a77243643d7f34
Nuo Xu <nuoxu.nx@gmail.com>
2024-09-16 09:59:44 -0700

    chore(wallet): Reading compressed NFT info from txInfo. (#25561)

--- ios/brave-ios/Sources/BraveWallet/Crypto/Transactions/TransactionParser.swift
+++ ios/brave-ios/Sources/BraveWallet/Crypto/Transactions/TransactionParser.swift
@@ -678,15 +678,12 @@ enum TransactionParser {
       else {
         return nil
       }
-      let fromToken: BraveWallet.BlockchainToken? =
-        transaction.txType == .solanaCompressedNftTransfer
-        ? nil
-        : token(
-          for: splTokenMintAddress,
-          network: txNetwork,
-          userAssets: userAssets,
-          allTokens: allTokens
-        )  // solana compressed NFT transfer might get it's NFT info after issue #40353
+      let fromToken: BraveWallet.BlockchainToken? = token(
+        for: splTokenMintAddress,
+        network: txNetwork,
+        userAssets: userAssets,
+        allTokens: allTokens
+      )
       let tokenNFTMetadata: BraveWallet.NftMetadata?
       if let fromToken {
         tokenNFTMetadata = nftMetadata[fromToken.id]
@@ -696,24 +693,22 @@ enum TransactionParser {
       let fromValue = "\(amount)"
       var fromValueFormatted = ""
       var fromFiat = "$0.00"
-      if transaction.txType == .solanaCompressedNftTransfer {
+      if let token = fromToken {
+        fromValueFormatted =
+          formatter.decimalString(for: fromValue, radix: .decimal, decimals: Int(token.decimals))?
+          .trimmingTrailingZeros ?? ""
+        if token.isNft {
+          fromFiat = ""  // don't show fiat for NFTs
+        } else {
+          fromFiat =
+            currencyFormatter.formatAsFiat(
+              assetRatios[token.assetRatioId.lowercased(), default: 0]
+                * (Double(fromValueFormatted) ?? 0)
+            ) ?? "$0.00"
+        }
+      } else {
         fromValueFormatted = "0"
         fromFiat = ""
-      } else {
-        if let token = fromToken {
-          fromValueFormatted =
-            formatter.decimalString(for: fromValue, radix: .decimal, decimals: Int(token.decimals))?
-            .trimmingTrailingZeros ?? ""
-          if token.isNft {
-            fromFiat = ""  // don't show fiat for NFTs
-          } else {
-            fromFiat =
-              currencyFormatter.formatAsFiat(
-                assetRatios[token.assetRatioId.lowercased(), default: 0]
-                  * (Double(fromValueFormatted) ?? 0)
-              ) ?? "$0.00"
-          }
-        }
       }
       // Example:
       // Send 0.1234 SMB
