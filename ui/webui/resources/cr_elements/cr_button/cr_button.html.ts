d10bd876756d19d02048dbcf8e99f7362bb84ecb
Jay Harris <jay.harris@outlook.co.nz>
2024-09-17 13:57:29 +0200

    [CrOverrides]: Migrate existing overrides to Lit (#25600)

---
+++ ui/webui/resources/cr_elements/cr_button/cr_button.html.ts
@@ -0,0 +1,18 @@
+// Copyright (c) 2024 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import { html } from '//resources/lit/v3_0/lit.rollup.js';
+import type { CrButtonElement } from './cr_button.js';
+
+export function getHtml(this: CrButtonElement) {
+  return html`
+<leo-button id="button" kind="outline" size="small">
+  <slot slot="icon-before" name="prefix-icon"></slot>
+  <slot></slot>
+  <slot slot="icon-after" name="suffix-icon"></slot>
+</leo-button>`;
+}
+
+
