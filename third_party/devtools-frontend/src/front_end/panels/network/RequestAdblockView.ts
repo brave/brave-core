c6f631caa87a715768b99c730a6ae8704e61e2bd
Emerick Rogul <erogul@brave.com>
2024-09-17 10:16:34 -0400

    fixup! FinishedLoading -> FINISHED_LOADING in devtools-frontend

--- third_party/devtools-frontend/src/front_end/panels/network/RequestAdblockView.ts
+++ third_party/devtools-frontend/src/front_end/panels/network/RequestAdblockView.ts
@@ -87,7 +87,7 @@ export class RequestAdblockView extends LegacyWrapper.LegacyWrapper
 
   override willHide(): void {
     this.#request.removeEventListener(
-      SDK.NetworkRequest.Events.FinishedLoading,
+      SDK.NetworkRequest.Events.FINISHED_LOADING,
       this.#refresh,
       this
     )
