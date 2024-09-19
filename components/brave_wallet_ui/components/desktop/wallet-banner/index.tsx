b65c189f6ccd68adac6ac2a7f4bc32f47f0eeae0
Douglas Daniel <douglashdaniel@gmail.com>
2024-09-19 18:54:28 -0500

    style(wallet): Use Nala Alert for Wallet Banners

--- components/brave_wallet_ui/components/desktop/wallet-banner/index.tsx
+++ components/brave_wallet_ui/components/desktop/wallet-banner/index.tsx
@@ -3,13 +3,19 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // you can obtain one at https://mozilla.org/MPL/2.0/.
 import * as React from 'react'
-// Styled Components
-import { StyledWrapper, WarningText, BannerButton, ButtonRow } from './style'
+import Button from '@brave/leo/react/button'
+
+// Utils
 import { getLocale } from '../../../../common/locale'
+
+// Styled Components
+import { StyledWrapper, Alert, Icon } from './style'
+import { Row } from '../../shared/style'
+
 export interface Props {
   onClick: () => void
   onDismiss: () => void
-  bannerType: 'warning' | 'danger'
+  bannerType: 'warning' | 'error'
   description: string
   buttonText: string
 }
@@ -18,22 +24,41 @@ export const WalletBanner = (props: Props) => {
   const { onDismiss, onClick, bannerType, description, buttonText } = props
 
   return (
-    <StyledWrapper bannerType={bannerType}>
-      <WarningText>{description}</WarningText>
-      <ButtonRow>
-        <BannerButton
-          onClick={onClick}
-          buttonType='primary'
+    <StyledWrapper>
+      <Alert
+        type={bannerType}
+        mode='full'
+      >
+        <Icon
+          slot='icon'
+          name={
+            bannerType === 'warning'
+              ? 'warning-triangle-filled'
+              : 'warning-circle-filled'
+          }
+        />
+        {description}
+        <Row
+          alignItems='flex-start'
+          slot='actions'
+          width='unset'
         >
-          {buttonText}
-        </BannerButton>
-        <BannerButton
-          onClick={onDismiss}
-          buttonType='secondary'
-        >
-          {getLocale('braveWalletDismissButton')}
-        </BannerButton>
-      </ButtonRow>
+          <Button
+            kind='plain'
+            onClick={onClick}
+            size='tiny'
+          >
+            {buttonText}
+          </Button>
+          <Button
+            kind='plain-faint'
+            onClick={onDismiss}
+            size='tiny'
+          >
+            {getLocale('braveWalletDismissButton')}
+          </Button>
+        </Row>
+      </Alert>
     </StyledWrapper>
   )
 }
