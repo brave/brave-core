f35ee757311e0144b1a76b697ed9709e288abff6
Brian Clifton <brian@clifton.me>
2024-09-23 12:43:52 -0700

    Rework our existing group policies and match the style that Chromium uses.
    The assets generated now include all of the Brave group policies in a group
    called "BraveSoftware".
    
    Fixes https://github.com/brave/brave-browser/issues/26502

--- components/policy/pack_policy_templates.py
+++ components/policy/pack_policy_templates.py
@@ -4,6 +4,25 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at https://mozilla.org/MPL/2.0/.
 
+# Script that prepares a Brave-specific version of the `policy_templates.zip`
+# file that folks expect for administering Brave via group policy.
+#
+# For more info, see:
+# https://support.brave.com/hc/en-us/articles/360039248271-Group-Policy
+# and
+# https://github.com/brave/brave-browser/issues/26502
+#
+# NOTE: There are assets on other platforms but we don't currently use them.
+#
+# - macOS should have two assets:
+#     - $root_out_dir/mac/app-Manifest.plist
+#     - $root_out_dir/mac/jamf.json
+#
+# - Linux has one asset:
+#     - $root_out_dir/linux/examples/chrome.json
+#
+# For more info, see:
+# https://source.chromium.org/chromium/chromium/src/+/main:components/policy/resources/policy_templates.gni # pylint: disable=line-too-long
 """
 Create a Zip file of Windows Group Policy templates similar to Chrome's.
 """
@@ -16,37 +35,38 @@ from tempfile import TemporaryDirectory
 from zipfile import ZipFile, ZIP_DEFLATED
 
 def main():
-  chrome_policy_zip, dest_zip = _get_args()
-  _pack_policy_templates(chrome_policy_zip, dest_zip)
+    chrome_policy_zip, dest_zip = _get_args()
+    _pack_policy_templates(chrome_policy_zip, dest_zip)
 
 def _get_args():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('chrome_policy_zip',
-                      help="Path to Chrome's policy_templates.zip")
-  parser.add_argument('dest_zip',
-                      help="Path to the Zip file to be created")
-  args = parser.parse_args()
-  return args.chrome_policy_zip, args.dest_zip
+    parser = argparse.ArgumentParser()
+    parser.add_argument('chrome_policy_zip',
+                        help="Path to Chrome's policy_templates.zip")
+    parser.add_argument('dest_zip', help="Path to the Zip file to be created")
+    args = parser.parse_args()
+    return args.chrome_policy_zip, args.dest_zip
 
 def _pack_policy_templates(chrome_policy_zip, dest_zip):
-  with TemporaryDirectory() as tmp_dir:
-    with ZipFile(chrome_policy_zip) as src_zip:
-      src_zip.extract('VERSION', tmp_dir)
-      namelist = src_zip.namelist()
-      for dir_ in ('windows/adm/', 'windows/admx/', 'windows/examples/'):
-        src_zip.extractall(tmp_dir, (n for n in namelist if n.startswith(dir_)))
+    with TemporaryDirectory() as tmp_dir:
+        with ZipFile(chrome_policy_zip) as src_zip:
+            src_zip.extract('VERSION', tmp_dir)
+            namelist = src_zip.namelist()
+            for dir_ in ('windows/adm/', 'windows/admx/', 'windows/examples/'):
+                src_zip.extractall(tmp_dir,
+                                   (n for n in namelist if n.startswith(dir_)))
 
-    # Some sanity checks:
-    assert exists(join(tmp_dir, 'windows/adm/en-US/chrome.adm'))
-    assert exists(join(tmp_dir, 'windows/admx/chrome.admx'))
-    assert exists(join(tmp_dir, 'windows/admx/en-US/chrome.adml'))
+        # Some sanity checks:
+        assert exists(join(tmp_dir, 'windows/adm/en-US/chrome.adm'))
+        assert exists(join(tmp_dir, 'windows/admx/chrome.admx'))
+        assert exists(join(tmp_dir, 'windows/admx/en-US/chrome.adml'))
 
-    with ZipFile(dest_zip, 'w', ZIP_DEFLATED) as dest_zipfile:
-      for dirpath, _, filenames in os.walk(tmp_dir):
-        for filename in filenames:
-          filepath = join(dirpath, filename)
-          arcname = relpath(filepath, tmp_dir).replace('chrome', 'brave')
-          dest_zipfile.write(filepath, arcname=arcname)
+        with ZipFile(dest_zip, 'w', ZIP_DEFLATED) as dest_zipfile:
+            for dirpath, _, filenames in os.walk(tmp_dir):
+                for filename in filenames:
+                    filepath = join(dirpath, filename)
+                    arcname = relpath(filepath,
+                                      tmp_dir).replace('chrome', 'brave')
+                    dest_zipfile.write(filepath, arcname=arcname)
 
 if __name__ == '__main__':
-  main()
+    main()
---
+++ components/policy/resources/templates/policy_definitions/README.md
@@ -0,0 +1,20 @@
+## Policy Settings in Brave
+
+These are Brave specific group policies.
+
+Our official documentation is [available here](https://support.brave.com/hc/en-us/articles/360039248271-Group-Policy).
+
+For information about adding a new policy, please see [this page](https://source.chromium.org/chromium/chromium/src/+/main:docs/enterprise/add_new_policy.md).
+
+In a nutshell, the steps for adding a new policy in Brave look like this:
+
+1. Create a new .yaml file under `//brave/components/policy/resources/templates/policy_definitions/BraveSoftware/`. The name of the file itself will be the policy name. Chromium uses capital casing. For example, we have a policy for disabling Brave Rewards called `BraveRewardsDisable.yaml`. The name used for matching is `BraveRewardsDisable`.
+2. Update the properties in that file accordingly. Look at some of the existing ones as an example.
+3. Go into `//brave/browser/policy/brave_simple_policy_map.h` and add your entry here. It'll be auto-generated as `policy::key::k` and then the policy name. With the above example, that would be `policy::key::kBraveRewardsDisable`. You must map this to a profile preference (you must create a new one). That new preference is what you'll check in the code.
+4. Go into `//brave/chromium_src/components/policy/resources/policy_templates.py` and add your policy name to the end of the existing string array there.
+5. In the code where you want to check the profile preference, you can tell if it's set via policy by checking `prefs->IsManagedPreference(<profile_preference_key_here>)`. If this is set to true, you might want to have the UI display something like "This setting is managed by your organization" and have it be read-only.
+6. Build the project like regular (`npm run build`).
+7. You can build the policy templates using `npm run build -- --target=brave/components/policy:pack_policy_templates`.
+8. The group policy assets get output to `//out/<build_type_here>/brave_policy_templates.zip`. You can open this up and inspect the registry and adm/admx files.
+
+If rebuilding isn't picking up your policy, you can try to delete `//out/<build_type_here>/gen/components/policy/policy_constants.cc`. Rebuilding will trigger the rebuild for this file and your changes should be there.
