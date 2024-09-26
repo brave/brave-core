036a44f83d80cf42d9370a3eb9b2b80a647af639
Claudio DeSouza <cdesouza@brave.com>
2024-09-26 15:44:13 +0100

    `KURL::Host` returning `StringView`
    This change has run into build errors because `StringView` doesn't offer
    a `StartsWith` method. Additionaly, in a generic context where `T::Host`
    is called, for types like `SecurityOrigin `, `Host` still return
    `String`, which has to be reconciled with the `KURL` case.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/55f1e9efbe0c4c1dd9d53da37f030c0041c34591
    
    commit 55f1e9efbe0c4c1dd9d53da37f030c0041c34591
    Author: Dave Tapuska <dtapuska@chromium.org>
    Date:   Mon Sep 23 18:46:26 2024 +0000
    
        [kURL] Change GetHost to return a StringView
    
        Return StringView as returning a String can be a footgun for allocations.
        Eventually I'll move all properties on KURL to StringViews (this matches
        what Webkit did for their KURL fork). Collapse GetHost and GetHostView
        into one function.
    
        Bug: 366411876

--- chromium_src/third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
+++ chromium_src/third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
@@ -13,7 +13,7 @@
 namespace {
 
 bool IsGoogleMaps(const blink::KURL& url) {
-  const auto host = url.Host();
+  const auto host = url.Host().ToString();
   if (!host.StartsWith("google.") && !host.Contains(".google.")) {
     return false;
   }
