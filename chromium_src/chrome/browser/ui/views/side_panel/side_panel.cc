ef1a87906c760344158796696bc99a851b83d487
Claudio DeSouza <cdesouza@brave.com>
2024-09-27 17:20:22 +0100

    [CodeHealth] Exclude `SidePanel` from the build
    `BraveSidePanel` acts as a complete replacement to chromium's
    `SidePanel` type, however the upstream type has never been properly
    excluded from the build. This leads occasional breaks where new
    references to `SidePanel` appear in different places, and new overrides
    have to be introduced to correct those occurrences.
    
    This change entirely removes side panel from being built by exluding the
    particular headers/UT from being imported through their respective
    shadow files. This will no only reduce the number of places where we
    have to honour the token replacements, which means less build breakage
    going foward, but it will also prevent ODR violation bugs from
    occurring, which can be a particularly nasty type of UB.

--- chromium_src/chrome/browser/ui/views/side_panel/side_panel.cc
+++ chromium_src/chrome/browser/ui/views/side_panel/side_panel.cc
@@ -3,10 +3,5 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // you can obtain one at http://mozilla.org/MPL/2.0/.
 
-#include "chrome/browser/ui/views/side_panel/side_panel.h"
-
-#define kAlignRight kHorizontalAlignRight
-
-#include "src/chrome/browser/ui/views/side_panel/side_panel.cc"
-
-#undef kAlignRight
+// This shadow file is left empty, to entirely exclude
+// `chrome/browser/ui/views/side_panel/side_panel.cc` from the build.
