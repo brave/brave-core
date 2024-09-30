07e185ec85991b93a0327c99ccbaa78db59161d8
Claudio DeSouza <cdesouza@brave.com>
2024-09-30 15:30:17 +0100

    Fix `BookmarkSubMenuModel` substitution failure
    A substitution failure has popped up on `app_menu_model.cc`, due to the
    `.cc` shadow failing to premptively include `app_menu_model.h`, which
    undefs the substitution.
    
    This override must have been working incidently due transient header
    inclusions.

--- chromium_src/chrome/browser/ui/toolbar/app_menu_model.cc
+++ chromium_src/chrome/browser/ui/toolbar/app_menu_model.cc
@@ -3,6 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "chrome/browser/ui/toolbar/app_menu_model.h"
+
 #include "brave/browser/ui/toolbar/brave_bookmark_sub_menu_model.h"
 #include "brave/browser/ui/toolbar/brave_recent_tabs_sub_menu_model.h"
 #include "brave/grit/brave_generated_resources.h"
