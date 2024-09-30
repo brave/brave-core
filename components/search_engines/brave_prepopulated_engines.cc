d70b593d7fe51ef8c400a7a2fccfa195d844dec4
Brian Clifton <brian@clifton.me>
2024-09-30 13:36:11 -0700

    Merge pull request #25731 from brave/bsc-increment-search-version
    Increment search version in brave_prepopulated_engines.cc

--- components/search_engines/brave_prepopulated_engines.cc
+++ components/search_engines/brave_prepopulated_engines.cc
@@ -13,7 +13,7 @@ namespace TemplateURLPrepopulateData {
 
 // IMPORTANT! Make sure to bump this value if you make changes to the
 // engines below or add/remove engines.
-const int kBraveCurrentDataVersion = 28;
+const int kBraveCurrentDataVersion = 29;
 // DO NOT CHANGE THIS ONE. Used for backfilling kBraveDefaultSearchVersion.
 const int kBraveFirstTrackedDataVersion = 6;
 
