b40e3c38144d261e909d99c059cacc4135d17d06
brave-builds <brave-builds+devops@brave.com>
2024-10-02 21:12:23 +0000

    Uplift of #25776 (squashed) to beta

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -651,11 +651,13 @@ public class BytecodeTest {
         Assert.assertTrue(
                 methodExists(
                         "org/chromium/chrome/browser/tasks/ReturnToChromeUtil",
-                        "shouldShowTabSwitcher",
+                        "shouldShowNtpAsHomeSurfaceAtStartup",
                         MethodModifier.STATIC,
                         true,
                         boolean.class,
-                        long.class));
+                        Intent.class,
+                        Bundle.class,
+                        ChromeInactivityTracker.class));
         Assert.assertTrue(
                 methodExists(
                         "org/chromium/chrome/browser/IntentHandler",
