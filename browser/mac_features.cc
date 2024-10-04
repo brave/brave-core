f2c0f824cabb66700afec4848ec1b3c1a5a29a42
Michael Herrmann <michael@herrmann.io>
2024-10-04 16:07:51 +0200

    Support feature UseOmaha4Alpha in system-wide installs (#25796)
    Fix feature UseOmaha4Alpha in system-wide installs

--- browser/mac_features.cc
+++ browser/mac_features.cc
@@ -12,11 +12,10 @@ static_assert(BUILDFLAG(IS_MAC), "Currently for macOS only");
 namespace brave {
 
 // DO NOT TURN THIS FEATURE ON IN PRODUCTION. As of this writing, it only
-// implements the happy path of switching from Sparkle to Omaha 4 for per-user
-// installations. It does not handle system-wide implementations, nor switching
-// from Omaha 4 back to Sparkle. When you do enable the feature in the future,
-// make sure that it is not enabled for any clients that suffer from the above
-// limitations.
+// implements the happy path of switching from Sparkle to Omaha 4. It does not
+// handle switching from Omaha 4 back to Sparkle. When you do enable the feature
+// in the future, make sure that it is not enabled for any clients that suffer
+// from the above limitations.
 BASE_FEATURE(kBraveUseOmaha4Alpha,
              "BraveUseOmaha4Alpha",
              base::FEATURE_DISABLED_BY_DEFAULT);
--- chromium_src/chrome/browser/ui/cocoa/keystone_infobar_delegate.cc
+++ chromium_src/chrome/browser/ui/cocoa/keystone_infobar_delegate.cc
@@ -3,5 +3,18 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-// This override is intentionally empty as we already implement all of the
-// required functionality in //brave/browser/mac/keystone_infobar_delegate.mm
+#include "chrome/browser/ui/cocoa/keystone_infobar_delegate.h"
+
+#include "brave/browser/mac_features.h"
+
+#define ShowUpdaterPromotionInfoBar ShowUpdaterPromotionInfoBar_ChromiumImpl
+
+#include "src/chrome/browser/ui/cocoa/keystone_infobar_delegate.cc"
+
+#undef ShowUpdaterPromotionInfoBar
+
+void ShowUpdaterPromotionInfoBar() {
+  if (brave::ShouldUseOmaha4()) {
+    ShowUpdaterPromotionInfoBar_ChromiumImpl();
+  }
+}
