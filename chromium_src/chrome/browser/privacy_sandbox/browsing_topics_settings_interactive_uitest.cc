08efb40a25ddcb2a87c5c7cbd06271eed6d417fe
tech-zilla <chromiumreactcoder@gmail.com>
2024-10-06 23:28:02 +0300

    Fix interactive ui tests build

---
+++ chromium_src/chrome/browser/privacy_sandbox/browsing_topics_settings_interactive_uitest.cc
@@ -0,0 +1,19 @@
+#include <stddef.h>
+
+namespace chrome {
+inline constexpr char kPrivacySandboxManageTopicsURL[] =
+    "chrome://settings/adPrivacy/interests/manage";
+inline constexpr char kPrivacySandboxAdTopicsURL[] =
+    "chrome://settings/adPrivacy/interests";
+}  // namespace chrome
+
+#define StartWithTwoBlockedTopics DISABLED_StartWithTwoBlockedTopics
+#define BlockFirstTopicOnManageTopicsPage \
+  DISABLED_BlockFirstTopicOnManageTopicsPage
+#define UnblockOneTopicOnAdTopicsPage DISABLED_UnblockOneTopicOnAdTopicsPage
+#define ConfirmDefaultIconIsNotUsed DISABLED_ConfirmDefaultIconIsNotUsed
+#include "src/chrome/browser/privacy_sandbox/browsing_topics_settings_interactive_uitest.cc"
+#undef StartWithTwoBlockedTopics
+#undef BlockFirstTopicOnManageTopicsPage
+#undef UnblockOneTopicOnAdTopicsPage
+#undef ConfirmDefaultIconIsNotUsed
