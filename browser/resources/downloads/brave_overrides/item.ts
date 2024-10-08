80cb6b06f7595fe23efef818e8791bdd7ca709be
Jay Harris <jay.harris@outlook.co.nz>
2024-10-08 14:36:23 +1300

    [Downloads]: Fix progress bar color (#25865)

---
+++ browser/resources/downloads/brave_overrides/item.ts
@@ -0,0 +1,14 @@
+// Copyright (c) 2023 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import { injectStyle } from '//resources/brave/lit_overriding.js'
+import { css } from '//resources/lit/v3_0/lit.rollup.js'
+import { DownloadsItemElement } from '../item.js'
+
+injectStyle(DownloadsItemElement, css`
+    #progress {
+        --cr-progress-active-color: var(--leo-color-icon-interactive) !important;
+    }
+`)
