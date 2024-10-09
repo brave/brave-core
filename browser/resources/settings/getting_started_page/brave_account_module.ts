6b862e7022e5594d7eb37a1e12b6b97e20da788f
Szilard Szaloki <szilardszaloki@gmail.com>
2024-10-09 13:58:13 -0600

    gfdsa

---
+++ browser/resources/settings/getting_started_page/brave_account_module.ts
@@ -0,0 +1,46 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * you can obtain one at https://mozilla.org/MPL/2.0/. */
+
+// @ts-nocheck TODO(petemill): Define types and remove ts-nocheck
+
+import '../settings_shared.css.js';
+import '../settings_vars.css.js';
+
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {WebUiListenerMixin} from 'chrome://resources/cr_elements/web_ui_listener_mixin.js';
+import {getTemplate} from './brave_account_module.html.js'
+
+const SettingsBraveAccountModuleBase = WebUiListenerMixin(PolymerElement)
+
+class SettingsBraveAccountModule extends SettingsBraveAccountModuleBase {
+  static get is() {
+    return 'settings-brave-account-module'
+  }
+
+  static get template() {
+    return getTemplate()
+  }
+
+  static get properties() {
+    return {
+      showBraveAccountDialog_: {
+        type: Boolean,
+        value: false,
+      },
+    }
+  }
+
+  private showBraveAccountDialog_: boolean;
+
+  onGetStartedButtonClicked_() {
+    this.showBraveAccountDialog_ = true
+  }
+
+  private onBraveAccountDialogClosed_() {
+    this.showBraveAccountDialog_ = false
+  }
+}
+
+customElements.define(SettingsBraveAccountModule.is, SettingsBraveAccountModule);
