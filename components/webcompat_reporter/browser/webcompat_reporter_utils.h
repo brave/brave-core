a772fff200c183d568121aca8e07433f9d03ad95
Vadym Struts <vstruts@brave.com>
2024-10-11 10:53:06 +0200

    fix format
    Signed-off-by: Vadym Struts <vstruts@brave.com>

--- components/webcompat_reporter/browser/webcompat_reporter_utils.h
+++ components/webcompat_reporter/browser/webcompat_reporter_utils.h
@@ -3,8 +3,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#ifndef BRAVE_COMPONENTS_WEBCOMPAT_REPORTER_COMMON_WEBCOMPAT_REPORTER_UTILS_H_
-#define BRAVE_COMPONENTS_WEBCOMPAT_REPORTER_COMMON_WEBCOMPAT_REPORTER_UTILS_H_
+#ifndef BRAVE_COMPONENTS_WEBCOMPAT_REPORTER_BROWSER_WEBCOMPAT_REPORTER_UTILS_H_
+#define BRAVE_COMPONENTS_WEBCOMPAT_REPORTER_BROWSER_WEBCOMPAT_REPORTER_UTILS_H_
 
 #include <string>
 
@@ -12,4 +12,4 @@ namespace webcompat_reporter {
 bool NeedsToGetComponentInfo(const std::string& component_id);
 }  // namespace webcompat_reporter
 
-#endif  // BRAVE_COMPONENTS_WEBCOMPAT_REPORTER_COMMON_WEBCOMPAT_REPORTER_UTILS_H_
+#endif  // BRAVE_COMPONENTS_WEBCOMPAT_REPORTER_BROWSER_WEBCOMPAT_REPORTER_UTILS_H_
