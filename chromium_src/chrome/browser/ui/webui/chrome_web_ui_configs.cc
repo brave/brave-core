be8e0ea65867eaeb46916d3cfe22748011a8dca6
mkarolin <max@brave.com>
2024-10-14 14:46:19 -0400

    SettingsUI was migrated to WebUIConfig.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src.git/+/beb0649ca837e
    
    commit beb0649ca837e64e0448b618dd46de480183d65b
    Author: Teresa Mao <temao@chromium.org>
    Date:   Tue Oct 8 20:35:15 2024 +0000
    
        WebUI: Migrate SettingsUI to WebUIConfig.
    
        Bug: 40222519
        Fixed: 361535277

--- chromium_src/chrome/browser/ui/webui/chrome_web_ui_configs.cc
+++ chromium_src/chrome/browser/ui/webui/chrome_web_ui_configs.cc
@@ -16,6 +16,7 @@
 #include "brave/browser/ui/webui/brave_rewards/rewards_page_top_ui.h"
 #include "brave/browser/ui/webui/brave_rewards/rewards_panel_ui.h"
 #include "brave/browser/ui/webui/brave_rewards/tip_panel_ui.h"
+#include "brave/browser/ui/webui/brave_settings_ui.h"
 #include "brave/browser/ui/webui/brave_shields/cookie_list_opt_in_ui.h"
 #include "brave/browser/ui/webui/brave_shields/shields_panel_ui.h"
 #include "brave/browser/ui/webui/brave_wallet/wallet_panel_ui.h"
@@ -39,6 +40,9 @@ void RemoveOverridenWebUIs(content::WebUIConfigMap& map) {
   // NewTabUIConfig (overriden with BravePrivateNewTabUIConfig)
   map.RemoveConfig(
       GetWebUIConfigURL(content::kChromeUIScheme, chrome::kChromeUINewTabHost));
+  // SettingsUIConfig (overriden with BraveSettingsUIConfig)
+  map.RemoveConfig(GetWebUIConfigURL(content::kChromeUIScheme,
+                                     chrome::kChromeUISettingsHost));
 #endif  // !BUILDFLAG(IS_ANDROID)
 }
 
@@ -54,6 +58,7 @@ void RegisterChromeWebUIConfigs() {
   map.AddWebUIConfig(std::make_unique<brave_rewards::RewardsPanelUIConfig>());
   map.AddWebUIConfig(std::make_unique<brave_rewards::TipPanelUIConfig>());
   map.AddWebUIConfig(std::make_unique<BravePrivateNewTabUIConfig>());
+  map.AddWebUIConfig(std::make_unique<BraveSettingsUIConfig>());
   map.AddWebUIConfig(std::make_unique<CookieListOptInUIConfig>());
   map.AddWebUIConfig(std::make_unique<ShieldsPanelUIConfig>());
   map.AddWebUIConfig(std::make_unique<SpeedreaderToolbarUIConfig>());
