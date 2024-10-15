af75cf18463a8797b728f0bf04a68290c527cb94
Terry Mancey <terry.mancey@icloud.com>
2024-10-15 17:34:13 -0500

    [ads] Add additional SmartNTT unit tests

--- components/brave_ads/core/internal/serving/new_tab_page_ad_serving_condition_matcher_util_unittest.cc
+++ components/brave_ads/core/internal/serving/new_tab_page_ad_serving_condition_matcher_util_unittest.cc
@@ -5,8 +5,10 @@
 
 #include "brave/components/brave_ads/core/public/serving/new_tab_page_ad_serving_condition_matcher_util.h"
 
+#include "base/time/time.h"
 #include "brave/components/brave_ads/core/internal/ads_client/ads_client_pref_provider.h"
 #include "brave/components/brave_ads/core/internal/common/test/test_base.h"
+#include "brave/components/brave_ads/core/internal/common/test/time_test_util.h"
 #include "brave/components/brave_ads/core/public/prefs/pref_names.h"
 
 // npm run test -- brave_unit_tests --filter=BraveAds*
@@ -15,6 +17,14 @@ namespace brave_ads {
 
 class BraveAdsNewTabPageAdServingConditionMatcherUtilTest
     : public test::TestBase {
+ public:
+  BraveAdsNewTabPageAdServingConditionMatcherUtilTest() : pref_provider_() {
+    // We need to set the clock to 00:00:00 UTC here to ensure the pref registry
+    // in `common/test/pref_registry_test_util.cc` is initialized correctly with
+    // deterministic default values.
+    AdvanceClockTo(test::TimeFromUTCString("14 October 2024 00:00:00"));
+  }
+
  protected:
   const AdsClientPrefProvider pref_provider_;
 };
@@ -25,7 +35,8 @@ TEST_F(BraveAdsNewTabPageAdServingConditionMatcherUtilTest,
   EXPECT_TRUE(MatchConditions(&pref_provider_, /*condition_matchers=*/{}));
 }
 
-TEST_F(BraveAdsNewTabPageAdServingConditionMatcherUtilTest, MatchConditions) {
+TEST_F(BraveAdsNewTabPageAdServingConditionMatcherUtilTest,
+       MatchConditionsIfAllConditionsAreTrue) {
   // Arrange
   const NewTabPageAdConditionMatchers condition_matchers = {
       {prefs::kSubdivisionTargetingUserSelectedSubdivision, "AUTO"},
@@ -36,22 +47,90 @@ TEST_F(BraveAdsNewTabPageAdServingConditionMatcherUtilTest, MatchConditions) {
 }
 
 TEST_F(BraveAdsNewTabPageAdServingConditionMatcherUtilTest,
-       DoNotMatchCondition) {
+       MatchEqualOperatorCondition) {
   // Arrange
   const NewTabPageAdConditionMatchers condition_matchers = {
-      {prefs::kSubdivisionTargetingUserSelectedSubdivision, "US"},
-      {prefs::kOptedInToNotificationAds, "1"}};
+      {prefs::kServeAdAt, "[=]:7"}};
+
+  AdvanceClockBy(base::Days(7));
+
+  // Act & Assert
+  EXPECT_TRUE(MatchConditions(&pref_provider_, condition_matchers));
+}
+
+TEST_F(BraveAdsNewTabPageAdServingConditionMatcherUtilTest,
+       DoNotMatchEqualOperatorCondition) {
+  // Arrange
+  const NewTabPageAdConditionMatchers condition_matchers = {
+      {prefs::kServeAdAt, "[=]:7"}};
+
+  AdvanceClockBy(base::Days(7) - base::Milliseconds(1));
 
   // Act & Assert
   EXPECT_FALSE(MatchConditions(&pref_provider_, condition_matchers));
 }
 
 TEST_F(BraveAdsNewTabPageAdServingConditionMatcherUtilTest,
-       DoNotMatchConditions) {
+       MatchPatternCondition) {
   // Arrange
   const NewTabPageAdConditionMatchers condition_matchers = {
-      {prefs::kSubdivisionTargetingUserSelectedSubdivision, "US"},
-      {prefs::kOptedInToNotificationAds, "0"}};
+      {prefs::kSubdivisionTargetingUserSelectedSubdivision,
+       "?UT*"}};  // Value is "AUTO" in the pref.
+
+  // Act & Assert
+  EXPECT_TRUE(MatchConditions(&pref_provider_, condition_matchers));
+}
+
+TEST_F(BraveAdsNewTabPageAdServingConditionMatcherUtilTest,
+       DoNotMatchPatternCondition) {
+  // Arrange
+  const NewTabPageAdConditionMatchers condition_matchers = {
+      {prefs::kSubdivisionTargetingUserSelectedSubdivision,
+       "?FOO*"}};  // Value is "AUTO" in the pref.
+
+  // Act & Assert
+  EXPECT_FALSE(MatchConditions(&pref_provider_, condition_matchers));
+}
+
+TEST_F(BraveAdsNewTabPageAdServingConditionMatcherUtilTest,
+       MatchRegexCondition) {
+  // Arrange
+  const NewTabPageAdConditionMatchers condition_matchers = {
+      {prefs::kSubdivisionTargetingUserSelectedSubdivision,
+       "^AU"}};  // Value is "AUTO" in the pref.
+
+  // Act & Assert
+  EXPECT_TRUE(MatchConditions(&pref_provider_, condition_matchers));
+}
+
+TEST_F(BraveAdsNewTabPageAdServingConditionMatcherUtilTest,
+       DoNotMatchRegexCondition) {
+  // Arrange
+  const NewTabPageAdConditionMatchers condition_matchers = {
+      {prefs::kSubdivisionTargetingUserSelectedSubdivision,
+       "^FOO"}};  // Value is "AUTO" in the pref.
+
+  // Act & Assert
+  EXPECT_FALSE(MatchConditions(&pref_provider_, condition_matchers));
+}
+
+TEST_F(BraveAdsNewTabPageAdServingConditionMatcherUtilTest,
+       DoNotMatchConditionsIfPrefPathWasNotFound) {
+  // Arrange
+  const NewTabPageAdConditionMatchers condition_matchers = {{"foo.bar", "baz"}};
+
+  // Act & Assert
+  EXPECT_FALSE(MatchConditions(&pref_provider_, condition_matchers));
+}
+
+TEST_F(BraveAdsNewTabPageAdServingConditionMatcherUtilTest,
+       DoNotMatchConditionsIfAllConditionsAreFalse) {
+  // Arrange
+  const NewTabPageAdConditionMatchers condition_matchers = {
+      {prefs::kOptedInToNotificationAds, "0"},  // Value is "1" in the pref.
+      {prefs::kServeAdAt, "[>]:7"}};            // 5 days ago in the pref.
+
+  AdvanceClockBy(base::Days(5));
 
   // Act & Assert
   EXPECT_FALSE(MatchConditions(&pref_provider_, condition_matchers));
