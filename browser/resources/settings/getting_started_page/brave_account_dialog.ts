7e75c832bcbe31da32c60451f993a0e4b5564bff
Szilard Szaloki <szilardszaloki@gmail.com>
2024-10-15 17:10:35 -0600

    Entry and sign-in dialogs in terms of the base.

--- browser/resources/settings/getting_started_page/brave_account_dialog.ts
+++ browser/resources/settings/getting_started_page/brave_account_dialog.ts
@@ -3,11 +3,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * you can obtain one at https://mozilla.org/MPL/2.0/. */
 
-import './brave_account_sign_in_dialog.js';
+
 import '../settings_shared.css.js';
 import 'chrome://resources/cr_elements/cr_shared_style.css.js';
 import 'chrome://resources/polymer/v3_0/iron-flex-layout/iron-flex-layout-classes.js';
 
+import type {CrDialogElement} from 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {getTemplate} from './brave_account_dialog.html.js'
 
@@ -15,6 +16,12 @@ import {getTemplate} from './brave_account_dialog.html.js'
  * @fileoverview
  * 'settings-brave-account-dialog'...
  */
+
+interface SettingsBraveAccountDialogElement {
+  $: {
+    dialog: CrDialogElement,
+  };
+}
 
 class SettingsBraveAccountDialogElement extends PolymerElement {
   static get is() {
@@ -25,8 +32,44 @@ class SettingsBraveAccountDialogElement extends PolymerElement {
     return getTemplate()
   }
 
-  private onSignInButtonClicked_() {
-    this.dispatchEvent(new CustomEvent('sign-in-button-clicked'))
+  static get properties() {
+    return {
+      isTermsAccepted_: {
+        type: Boolean,
+        value: false,
+      },
+      isEmailAddressInvalid_: {
+        type: Boolean,
+        value: true,
+      },
+      showBackButton: {
+        type: Boolean
+      }
+    };
+  }
+
+  showBackButton: boolean = false;
+
+  private onBackButtonClicked_() {
+    this.dispatchEvent(new CustomEvent('back-button-clicked'))
+  }
+
+  private cancel() {
+    this.$.dialog.cancel();
+  }
+
+  protected onConditionsChanged_() {
+    this.canCreateAccount_ = this.isTermsAccepted_ && !this.isEmailAddressInvalid_;
+  }
+
+  private isTermsAccepted_: boolean = false;
+  private isEmailAddressInvalid_: boolean = true;
+  private canCreateAccount_: boolean = false;
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'settings-brave-account-dialog': SettingsBraveAccountDialogElement;
   }
 }
 
