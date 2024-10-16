cc2e2884afad18ecc60a7a9c4260742b30f9d758
mkarolin <max@brave.com>
2024-10-15 20:42:39 -0400

    Add KDF implementations without CHECKs.
    Chromium implementation of DeriveKeyScrypt has a CHECK in it that would
    crash the browser if the underlying BoringSSL function failed. We use
    this code from HDKey::GenerateFromV3UTC where the data is retrieved from
    JSON and can't be guaranteed to always be valid. In order not to crash,
    added DeriveKeyScrypt implementation without the CHECK and also changed
    our DeriveKeyPbkdf2HmacSha256 implementation to no use CHECK either.
    
    Additionally, moved HDKey::GenerateFromV3UTC code to use DeriveKeyScrypt
    and DeriveKeyPbkdf2HmacSha256 directly instead of using SymmetricKey
    wrappers that are being deprecated.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/4046edfc95050796d4a1bc54cdba6942257b6f38
    
    commit 4046edfc95050796d4a1bc54cdba6942257b6f38
    Author: Elly <ellyjones@chromium.org>
    Date:   Thu Oct 10 20:51:36 2024 +0000
    
        crypto: add kdf interface
    
        Right now, //crypto supports KDFs via two interfaces:
        * PBKDF2-HMAC-SHA1 and Scrypt on crypto::SymmetricKey
        * HKDF-SHA256 via free functions
    
        This change:
        * Adds crypto::SubtlePassKey for access to subtle crypto primitives
        * Creates a new KDF interface
        * Moves the PBKDF2-HMAC-SHA1 and Scrypt implementations to that
          interface, and guards them with SubtlePassKeys
        * Adds new unit tests covering those KDFs
        * Deprecates SymmetricKey
        * Deletes many of the unit tests for SymmetricKey, which either covered
          logic which is now moved to the KDF implementations (and is covered by
          their tests) or tested key lengths we don't allow in production.
    
        Bug: 369653192, 369649346

--- chromium_src/crypto/kdf_unittest.cc
+++ chromium_src/crypto/kdf_unittest.cc
@@ -9,7 +9,7 @@ TEST(KDFTest, Pbkdf2HmacSha256KnownAnswers) {
   struct TestCase {
     std::string password;
     std::string salt;
-    crypto::kdf::Pbkdf2HmacSha1Params params;
+    crypto::kdf::Pbkdf2HmacSha256Params params;
     size_t len;
     const char* result;
   };
@@ -34,12 +34,69 @@ TEST(KDFTest, Pbkdf2HmacSha256KnownAnswers) {
 
   for (const auto& c : cases) {
     std::vector<uint8_t> key(c.len);
-    crypto::kdf::DeriveKeyPbkdf2HmacSha256(
+    ASSERT_TRUE(crypto::kdf::DeriveKeyPbkdf2HmacSha256(
         c.params, base::as_byte_span(c.password), base::as_byte_span(c.salt),
-        key, crypto::SubtlePassKey::ForTesting());
+        key));
 
     std::vector<uint8_t> result_bytes(c.len);
     ASSERT_TRUE(base::HexStringToSpan(c.result, result_bytes));
     EXPECT_EQ(key, result_bytes);
+  }
+}
+
+// Test that our implementation in DeriveKeyScryptNoCheck produces results that
+// match the upstream's implementation in DeriveKeyScrypt.
+TEST(KDFTest, ScryptNoCheckKnownAnswers) {
+  struct TestCase {
+    std::string password;
+    std::string salt;
+    crypto::kdf::ScryptParams params;
+    size_t len;
+    const char* result;
+  };
+
+  // RFC 7914 test vectors - note that RFC 7914 does not specify
+  // max_memory_bytes so we just pass 0 here and let BoringSSL figure it out for
+  // us.
+  constexpr auto cases = std::to_array<TestCase>({
+      {"password",
+       "NaCl",
+       {.cost = 1024, .block_size = 8, .parallelization = 16},
+       64,
+       "fdbabe1c9d3472007856e7190d01e9fe7c6ad7cbc8237830e77376634b373162"
+       "2eaf30d92e22a3886ff109279d9830dac727afb94a83ee6d8360cbdfa2cc0640"},
+  });
+
+  for (const auto& c : cases) {
+    std::vector<uint8_t> key(c.len);
+    EXPECT_TRUE(crypto::kdf::DeriveKeyScryptNoCheck(
+        c.params, base::as_byte_span(c.password), base::as_byte_span(c.salt),
+        key));
+
+    std::vector<uint8_t> key_with_check(c.len);
+    crypto::kdf::DeriveKeyScrypt(c.params, base::as_byte_span(c.password),
+                                 base::as_byte_span(c.salt), key_with_check,
+                                 crypto::SubtlePassKey::ForTesting());
+    EXPECT_EQ(key, key_with_check);
+
+    std::vector<uint8_t> result_bytes(c.len);
+    ASSERT_TRUE(base::HexStringToSpan(c.result, result_bytes));
+    EXPECT_EQ(key, result_bytes);
+  }
+}
+
+// Test that our implementation in DeriveKeyScryptNoCheck doesn't cause a crash
+// when invalid parameter is passed in.
+TEST(KDFTest, InvalidScryptNoCheckParameters) {
+  constexpr auto cases = std::to_array<crypto::kdf::ScryptParams>({
+      // cost parameter is not a power of 2
+      {.cost = 1023, .block_size = 8, .parallelization = 16},
+      // TODO: others, after we document the exact constraints
+  });
+
+  for (const auto& c : cases) {
+    std::vector<uint8_t> key(64);
+    EXPECT_FALSE(crypto::kdf::DeriveKeyScryptNoCheck(
+        c, base::as_byte_span("password"), base::as_byte_span("NaCl"), key));
   }
 }
