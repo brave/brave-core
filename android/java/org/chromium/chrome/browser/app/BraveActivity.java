3dffa75e8c53794248708f1a8a75dff943f27b8c
AlexeyBarabash <alexey@brave.com>
2024-10-17 00:40:56 +0300

    [Android] OTR to [oO]tr in java code
    Related Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/c0be837dd32c9c92ec22d6b3d43ec414d0c0bc2c
    
    	OTR to [oO]tr in java code.
    
    	Bug: 372670589
    	Change-Id: I6d5aaa3b5b8af401e09c1990e12bfc889e93815c
    	Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5924233

--- android/java/org/chromium/chrome/browser/app/BraveActivity.java
+++ android/java/org/chromium/chrome/browser/app/BraveActivity.java
@@ -1678,7 +1678,7 @@ public abstract class BraveActivity extends ChromeActivity
                 BraveSearchEngineUtils.setDSEPrefs(yandexTemplateUrl, lastUsedRegularProfile);
                 BraveSearchEngineUtils.setDSEPrefs(
                         yandexTemplateUrl,
-                        lastUsedRegularProfile.getPrimaryOTRProfile(/* createIfNeeded= */ true));
+                        lastUsedRegularProfile.getPrimaryOtrProfile(/* createIfNeeded= */ true));
             }
         }
     }
