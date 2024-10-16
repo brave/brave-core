45fda6c438b16b7822eb4fcdccf5576f8ddf6a72
mkarolin <max@brave.com>
2024-10-15 20:42:39 -0400

    Add KDF implementations without CHECKs.
    Chromium implementation of DeriveKeyScrypt has a CHECK in it that would
    crash the browser if the underlying BoringSSL function failed. We use
    this code from HDKey::GenerateFromV3UTC where the data is retrieved from
    JSON and can't be guaranteed to always be valid. In order not to crash,
    added DeriveKeyScrypt implementation without the CHECK and also changed
    our DeriveKeyPbkdf2HmacSha256 implementation to no use CHECK either.
    
    Additionally, moved HDKey::GenerateFromV3UTC code to use DeriveKeyScrypt
    and DeriveKeyPbkdf2HmacSha256 directly instead of using SymmetricKey
    wrappers that are being deprecated.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/4046edfc95050796d4a1bc54cdba6942257b6f38
    
    commit 4046edfc95050796d4a1bc54cdba6942257b6f38
    Author: Elly <ellyjones@chromium.org>
    Date:   Thu Oct 10 20:51:36 2024 +0000
    
        crypto: add kdf interface
    
        Right now, //crypto supports KDFs via two interfaces:
        * PBKDF2-HMAC-SHA1 and Scrypt on crypto::SymmetricKey
        * HKDF-SHA256 via free functions
    
        This change:
        * Adds crypto::SubtlePassKey for access to subtle crypto primitives
        * Creates a new KDF interface
        * Moves the PBKDF2-HMAC-SHA1 and Scrypt implementations to that
          interface, and guards them with SubtlePassKeys
        * Adds new unit tests covering those KDFs
        * Deprecates SymmetricKey
        * Deletes many of the unit tests for SymmetricKey, which either covered
          logic which is now moved to the KDF implementations (and is covered by
          their tests) or tested key lengths we don't allow in production.
    
        Bug: 369653192, 369649346

--- chromium_src/crypto/kdf.cc
+++ chromium_src/crypto/kdf.cc
@@ -7,17 +7,28 @@
 
 namespace crypto::kdf {
 
-void DeriveKeyPbkdf2HmacSha256(const kdf::Pbkdf2HmacSha1Params& params,
+bool DeriveKeyScryptNoCheck(const ScryptParams& params,
+                            base::span<const uint8_t> password,
+                            base::span<const uint8_t> salt,
+                            base::span<uint8_t> result) {
+  OpenSSLErrStackTracer err_tracer(FROM_HERE);
+  int rv = EVP_PBE_scrypt(
+      base::as_chars(password).data(), password.size(), salt.data(),
+      salt.size(), params.cost, params.block_size, params.parallelization,
+      params.max_memory_bytes, result.data(), result.size());
+
+  return rv == 1;
+}
+
+bool DeriveKeyPbkdf2HmacSha256(const kdf::Pbkdf2HmacSha256Params& params,
                                base::span<const uint8_t> password,
                                base::span<const uint8_t> salt,
-                               base::span<uint8_t> result,
-                               crypto::SubtlePassKey) {
+                               base::span<uint8_t> result) {
   OpenSSLErrStackTracer err_tracer(FROM_HERE);
   int rv = PKCS5_PBKDF2_HMAC(base::as_chars(password).data(), password.size(),
                              salt.data(), salt.size(), params.iterations,
                              EVP_sha256(), result.size(), result.data());
-
-  CHECK_EQ(rv, 1);
+  return rv == 1;
 }
 
 }  // namespace crypto::kdf
--- chromium_src/crypto/kdf.h
+++ chromium_src/crypto/kdf.h
@@ -10,12 +10,26 @@
 
 namespace crypto::kdf {
 
-CRYPTO_EXPORT void DeriveKeyPbkdf2HmacSha256(
-    const kdf::Pbkdf2HmacSha1Params& params,
+// Upstream function DeriveKeyScrypt has a CHECK_EQ(rv, 1) which we don't
+// want as we shouldn't crash the browser because of bad input data, so this
+// is our own version of the same function but returning a bool instead of
+// CHECKing.
+CRYPTO_EXPORT bool DeriveKeyScryptNoCheck(const ScryptParams& params,
+                                          base::span<const uint8_t> password,
+                                          base::span<const uint8_t> salt,
+                                          base::span<uint8_t> result);
+
+struct Pbkdf2HmacSha256Params {
+  // BoringSSL uses a uint32_t for the iteration count for PBKDF2, so we match
+  // that.
+  uint32_t iterations;
+};
+
+CRYPTO_EXPORT bool DeriveKeyPbkdf2HmacSha256(
+    const kdf::Pbkdf2HmacSha256Params& params,
     base::span<const uint8_t> password,
     base::span<const uint8_t> salt,
-    base::span<uint8_t> result,
-    crypto::SubtlePassKey);
+    base::span<uint8_t> result);
 
 }  // namespace crypto::kdf
 
