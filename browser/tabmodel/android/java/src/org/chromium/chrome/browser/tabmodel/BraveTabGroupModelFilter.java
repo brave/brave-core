a45719da83a7557b7bb53f54b8eb6c8f1071c8d2
Artem Samoilenko <artem@brave.com>
2024-10-16 07:26:50 -0400

    [Android] Deprecate tab_groups package
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/65eee63c0e75ebd343c409a7c14addb00ca4b6dd
    
    [Tab Model] Deprecate tab_groups package
    
    Merge the tab_groups/ package into tabmodel/. The two packages have
    significant overlap and should really have a lot of their logic merged
    anyway.
    
    This allows us to package protect TabGroupModelFilterInternal and make
    destroy method package private as well.
    
    Fixed: 372068933

--- browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/BraveTabGroupModelFilter.java
+++ browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/BraveTabGroupModelFilter.java
@@ -2,32 +2,23 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
-package org.chromium.chrome.browser.tasks.tab_groups;
-
-import androidx.annotation.NonNull;
+package org.chromium.chrome.browser.tabmodel;
 
 import org.chromium.base.BravePreferenceKeys;
 import org.chromium.base.BraveReflectionUtil;
-import org.chromium.base.Token;
 import org.chromium.chrome.browser.preferences.ChromeSharedPreferences;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabLaunchType;
-import org.chromium.chrome.browser.tabmodel.TabModel;
-import org.chromium.chrome.browser.tabmodel.TabModelFilterBase;
 
 /** Brave's super class for {@link TabGroupModelFilter} */
-public abstract class BraveTabGroupModelFilter extends TabModelFilterBase {
+public class BraveTabGroupModelFilter {
     /**
-     * This variable will be used instead of {@link TabGroupModelFilter}'s variable, that will be
-     * deleted in bytecode.
+     * This variable will be used instead of {@link TabGroupModelFilterImpl}'s variable, that will
+     * be deleted in bytecode.
      */
     protected boolean mIsResetting;
 
-    public BraveTabGroupModelFilter(TabModel tabModel) {
-        super(tabModel);
-    }
-
-    /** Call from {@link TabGroupModelFilter} will be redirected here via bytrcode. */
+    /** Call from {@link TabGroupModelFilterImpl} will be redirected here via bytecode. */
     public boolean shouldUseParentIds(Tab tab) {
         if (linkClicked(tab.getLaunchType())
                 && ChromeSharedPreferences.getInstance()
@@ -39,7 +30,7 @@ public abstract class BraveTabGroupModelFilter extends TabModelFilterBase {
         // Otherwise just call parent.
         return (boolean)
                 BraveReflectionUtil.invokeMethod(
-                        TabGroupModelFilter.class, this, "shouldUseParentIds", Tab.class, tab);
+                        TabGroupModelFilterImpl.class, this, "shouldUseParentIds", Tab.class, tab);
     }
 
     /** Determine if a launch type is the result of linked being clicked. */
@@ -47,9 +38,9 @@ public abstract class BraveTabGroupModelFilter extends TabModelFilterBase {
         return type == TabLaunchType.FROM_LINK || type == TabLaunchType.FROM_LONGPRESS_FOREGROUND;
     }
 
-    private static Token getOrCreateTabGroupId(@NonNull Tab tab) {
-        return (Token)
+    private boolean isTabModelRestored() {
+        return (boolean)
                 BraveReflectionUtil.invokeMethod(
-                        TabGroupModelFilter.class, null, "getOrCreateTabGroupId", Tab.class, tab);
+                        TabGroupModelFilterImpl.class, this, "isTabModelRestored");
     }
 }
