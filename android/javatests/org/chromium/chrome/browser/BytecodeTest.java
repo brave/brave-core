8c1c3219ff10d1b08a7bf461819e8c837106ca90
Artem Samoilenko <artem@brave.com>
2024-10-16 07:26:50 -0400

    [Android] Deprecate tab_groups package
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/65eee63c0e75ebd343c409a7c14addb00ca4b6dd
    
    [Tab Model] Deprecate tab_groups package
    
    Merge the tab_groups/ package into tabmodel/. The two packages have
    significant overlap and should really have a lot of their logic merged
    anyway.
    
    This allows us to package protect TabGroupModelFilterInternal and make
    destroy method package private as well.
    
    Fixed: 372068933

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -35,7 +35,6 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.Callback;
 import org.chromium.base.FeatureMap;
-import org.chromium.base.Token;
 import org.chromium.base.jank_tracker.JankTracker;
 import org.chromium.base.shared_preferences.PreferenceKeyRegistry;
 import org.chromium.base.shared_preferences.SharedPreferencesManager;
@@ -1020,7 +1019,7 @@ public class BytecodeTest {
                         Tab.class));
         Assert.assertTrue(
                 methodExists(
-                        "org/chromium/chrome/browser/tasks/tab_groups/TabGroupModelFilter",
+                        "org/chromium/chrome/browser/tasks/tab_groups/TabGroupModelFilterImpl",
                         "shouldUseParentIds",
                         MethodModifier.REGULAR,
                         true,
@@ -1028,12 +1027,11 @@ public class BytecodeTest {
                         Tab.class));
         Assert.assertTrue(
                 methodExists(
-                        "org/chromium/chrome/browser/tasks/tab_groups/TabGroupModelFilter",
-                        "getOrCreateTabGroupId",
-                        MethodModifier.STATIC,
+                        "org/chromium/chrome/browser/tasks/tab_groups/TabGroupModelFilterImpl",
+                        "isTabModelRestored",
+                        MethodModifier.REGULAR,
                         true,
-                        Token.class,
-                        Tab.class));
+                        boolean.class));
 
         Assert.assertTrue(
                 methodExists(
@@ -2073,7 +2071,7 @@ public class BytecodeTest {
                 fieldExists("org/chromium/components/cached_flags/CachedFlag", "mDefaultValue"));
         Assert.assertFalse(
                 fieldExists(
-                        "org/chromium/chrome/browser/tasks/tab_groups/TabGroupModelFilter",
+                        "org/chromium/chrome/browser/tabmodel/TabGroupModelFilterImpl",
                         "mIsResetting"));
         Assert.assertTrue(
                 fieldExists("org/chromium/chrome/browser/SwipeRefreshHandler", "mSwipeType"));
@@ -2232,8 +2230,8 @@ public class BytecodeTest {
                         "org/chromium/chrome/browser/bookmarks/BraveBookmarkBridge"));
         Assert.assertTrue(
                 checkSuperName(
-                        "org/chromium/chrome/browser/tasks/tab_groups/TabGroupModelFilter",
-                        "org/chromium/chrome/browser/tasks/tab_groups/BraveTabGroupModelFilter"));
+                        "org/chromium/chrome/browser/tabmodel/TabGroupModelFilterImpl",
+                        "org/chromium/chrome/browser/tabmodel/BraveTabGroupModelFilter"));
         Assert.assertTrue(
                 checkSuperName(
                         "org/chromium/chrome/browser/media/PictureInPictureActivity",
