1c3a9748c5438d2ff6e0e07b0eeda12d96bb89b8
mkarolin <max@brave.com>
2024-10-19 10:09:30 -0400

    Disables features flagged by security/privacy teams.

--- chromium_src/extensions/common/extension_features.cc
+++ chromium_src/extensions/common/extension_features.cc
@@ -11,6 +11,7 @@ namespace extensions_features {
 
 OVERRIDE_FEATURE_DEFAULT_STATES({{
     {kExtensionManifestV2DeprecationWarning, base::FEATURE_DISABLED_BY_DEFAULT},
+    {kExtensionManifestV2Unsupported, base::FEATURE_DISABLED_BY_DEFAULT},
     {kExtensionsManifestV3Only, base::FEATURE_DISABLED_BY_DEFAULT},
 }});
 
