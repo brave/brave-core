6279ae638bb35b05b662f052b90d11606c966ac2
Jagadesh P <jagadeshjai1999@gmail.com>
2024-10-20 00:08:29 +0530

    [VerticalTabs] Add a option for toggling vertical tabs in tabstrip/toolbar

---
+++ chromium_src/chrome/browser/ui/views/frame/system_menu_model_delegate.h
@@ -0,0 +1,19 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_FRAME_SYSTEM_MENU_MODEL_DELEGATE_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_FRAME_SYSTEM_MENU_MODEL_DELEGATE_H_
+
+#include "ui/base/models/simple_menu_model.h"
+
+#define IsCommandIdChecked                               \
+  IsCommandIdChecked_ChromiumImpl(int command_id) const; \
+  bool IsCommandIdChecked
+
+#include "src/chrome/browser/ui/views/frame/system_menu_model_delegate.h"  // IWYU pragma: export
+
+#undef IsCommandIdChecked
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_FRAME_SYSTEM_MENU_MODEL_DELEGATE_H_
