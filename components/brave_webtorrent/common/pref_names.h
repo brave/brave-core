b4180ac2c1f9eee52bdb3f6b70f8f54fda105c6f
Wanzhong Wu <u7642453@anu.edu.au>
2024-10-20 19:31:53 +1100

    Create a "common" directory under component/brave_webtorrent, then add the pref_names.h for webtorrent policy.

---
+++ components/brave_webtorrent/common/pref_names.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2021 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_COMPONENTS_WEBTORRENT_COMMON_PREF_NAMES_H_
+#define BRAVE_COMPONENTS_WEBTORRENT_COMMON_PREF_NAMES_H_
+
+namespace webtorrent {
+namespace prefs {
+
+inline constexpr char kWebTorrentDisabledByPolicy[] =
+    "webtorrent.disabled_by_policy";
+
+}  // namespace prefs
+}  // namespace webtorrent
+
+#endif  // BRAVE_COMPONENTS_WEBTORRENT_COMMON_PREF_NAMES_H_
