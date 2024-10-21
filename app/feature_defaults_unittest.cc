bb080cad0c17e815095ea4c7402c1bd0c88423a0
Claudio DeSouza <cdesouza@brave.com>
2024-10-21 17:04:34 +0100

    [WIP][cr132] `kChromeLabs` feature flag deleted
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/7233f923cec850de2131352b9de16394afa31625
    
    commit 7233f923cec850de2131352b9de16394afa31625
    Author: dpapad <dpapad@chromium.org>
    Date:   Fri Oct 18 17:54:11 2024 +0000
    
        Remove kChromeLabs feature flag.
    
        Fixed: 356235522

--- app/feature_defaults_unittest.cc
+++ app/feature_defaults_unittest.cc
@@ -136,7 +136,6 @@ TEST(FeatureDefaultsTest, DisabledFeatures) {
       &features::kToolbarPinning,
 #endif
       &features::kBookmarkTriggerForPrerender2,
-      &features::kChromeLabs,
       &features::kChromeStructuredMetrics,
       &features::kCookieDeprecationFacilitatedTesting,
 #if !BUILDFLAG(IS_ANDROID)
