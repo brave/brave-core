dc8a84d7f220c8c08ddf456741d575abd3a0fe55
Aleksei Khoroshilov <5928869+goodov@users.noreply.github.com>
2024-10-22 20:02:42 +0700

    Remove override to not misuse ProfileKeyedServiceFactory. (#26089)
    Remove src override to not misuse ProfileKeyedServiceFactory.

--- chromium_src/chrome/browser/themes/theme_service_factory.cc
+++ chromium_src/chrome/browser/themes/theme_service_factory.cc
@@ -3,13 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "brave/browser/themes/brave_theme_service.h"
-// Below files are included in advance to prevent overriding
-// GetBrowserContextToUse.
-#include "chrome/browser/extensions/extension_system_factory.h"
 #include "chrome/browser/themes/theme_service_factory.h"
-#include "extensions/browser/extension_prefs_factory.h"
-#include "extensions/browser/extension_registry_factory.h"
+
+#include "brave/browser/themes/brave_theme_service.h"
 
 #if !BUILDFLAG(IS_LINUX)
 #define BRAVE_THEMESERVICEFACTORY_BUILDSERVICEINSTANCEFOR \
@@ -20,11 +16,5 @@
 #endif
 
 #include "src/chrome/browser/themes/theme_service_factory.cc"
-#undef BRAVE_THEMESERVICEFACTORY_BUILDSERVICEINSTANCEFOR
 
-content::BrowserContext* ThemeServiceFactory::GetBrowserContextToUse(
-    content::BrowserContext* context) const {
-  if (context->IsTor())
-    return context;
-  return ProfileKeyedServiceFactory::GetBrowserContextToUse(context);
-}
+#undef BRAVE_THEMESERVICEFACTORY_BUILDSERVICEINSTANCEFOR
