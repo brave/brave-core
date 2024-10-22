34ce6d1b0aa7bf643afec36a4d0b0475fefc4dec
Simon Hong <shong@brave.com>
2024-10-22 16:06:16 +0900

    Fixed contextual panel item is not activated after attaching to window
    fix https://github.com/brave/brave-browser/issues/41782
    
    We called SidebarContainerView::WillShowSidePanel() to observer
    tab's contextual entry/global entry when panel starts to show
    from some places. But there was corner-case that WillShowSidePanel()
    is not called when panel is shown. It happened when tab is attached
    to another window. And realized that PopulateSidePanel() is always
    called whenever panel is shown in whatever situation.
    Fixed by calling WillShowSidePanel() from PopulateSidePanel().

--- browser/ui/views/side_panel/brave_side_panel_coordinator.cc
+++ browser/ui/views/side_panel/brave_side_panel_coordinator.cc
@@ -42,11 +42,6 @@ void BraveSidePanelCoordinator::Show(
   sidebar::SetLastUsedSidePanel(browser_view_->GetProfile()->GetPrefs(),
                                 entry_key.id());
 
-  // Notify to give opportunity to observe another panel entries from
-  // global or active tab's contextual registry.
-  auto* brave_browser_view = static_cast<BraveBrowserView*>(browser_view_);
-  brave_browser_view->WillShowSidePanel();
-
   SidePanelCoordinator::Show(entry_key, open_trigger);
 }
 
@@ -91,13 +86,6 @@ void BraveSidePanelCoordinator::Toggle() {
 void BraveSidePanelCoordinator::Toggle(
     SidePanelEntryKey key,
     SidePanelUtil::SidePanelOpenTrigger open_trigger) {
-  // Notify to give opportunity to observe another panel entries from
-  // global or active tab's contextual registry.
-  if (!IsSidePanelShowing()) {
-    auto* brave_browser_view = static_cast<BraveBrowserView*>(browser_view_);
-    brave_browser_view->WillShowSidePanel();
-  }
-
   SidePanelCoordinator::Toggle(key, open_trigger);
 }
 
@@ -160,6 +148,11 @@ void BraveSidePanelCoordinator::PopulateSidePanel(
     return;
   }
 
+  // Notify to give opportunity to observe another panel entries from
+  // global or active tab's contextual registry.
+  auto* brave_browser_view = static_cast<BraveBrowserView*>(browser_view_);
+  brave_browser_view->WillShowSidePanel();
+
   SidePanelCoordinator::PopulateSidePanel(supress_animations, unique_key, entry,
                                           std::move(content_view));
 }
@@ -167,11 +160,6 @@ void BraveSidePanelCoordinator::PopulateSidePanel(
 void BraveSidePanelCoordinator::NotifyPinnedContainerOfActiveStateChange(
     SidePanelEntryKey key,
     bool is_active) {
-  // // Notify to give opportunity to observe another panel entries from
-  // // global or active tab's contextual registry.
-  // auto* brave_browser_view = static_cast<BraveBrowserView*>(browser_view_);
-  // brave_browser_view->WillShowSidePanel();
-
   if (!browser_view_->toolbar()->pinned_toolbar_actions_container()) {
     return;
   }
