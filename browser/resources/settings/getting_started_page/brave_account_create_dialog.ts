a1c51ace4f69e5e163736bc9c7469ea9db783e5d
Szilard Szaloki <szilardszaloki@gmail.com>
2024-10-23 10:15:21 -0600

    `Polymer` `==>` `Lit`

--- browser/resources/settings/getting_started_page/brave_account_create_dialog.ts
+++ browser/resources/settings/getting_started_page/brave_account_create_dialog.ts
@@ -3,18 +3,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * you can obtain one at https://mozilla.org/MPL/2.0/. */
 
-import './brave_account_dialog.js';
-import 'chrome://resources/brave/leo.bundle.js'
-
-import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-import {getTemplate} from './brave_account_create_dialog.html.js'
+import { CrLitElement } from '//resources/lit/v3_0/lit.rollup.js'
+import { getCss } from './brave_account_create_dialog.css.js'
+import { getHtml } from './brave_account_create_dialog.html.js'
 
 /**
  * @fileoverview
  * 'settings-brave-account-create-dialog'...
  */
 
-interface SettingsBraveAccountCreateDialogElement {
+export interface SettingsBraveAccountCreateDialogElement {
   $: {
     email_address: HTMLInputElement,
     account_name: HTMLInputElement,
@@ -23,41 +21,51 @@ interface SettingsBraveAccountCreateDialogElement {
   }
 }
 
-class SettingsBraveAccountCreateDialogElement extends PolymerElement {
+export class SettingsBraveAccountCreateDialogElement extends CrLitElement {
   static get is() {
     return 'settings-brave-account-create-dialog'
   }
 
-  static get template() {
-    return getTemplate()
+  static override get styles() {
+    return getCss()
   }
 
-  private onEmailAddressInput() {
+  override render() {
+    return getHtml.bind(this)()
+  }
+
+  static override get properties() {
+    return {
+      isEmailAddressValid: { type: Boolean },
+      isAccountNameValid: { type: Boolean },
+      isCreatePasswordValid: { type: Boolean },
+      isConfirmPasswordValid: { type: Boolean },
+      isChecked: { type: Boolean },
+    }
+  }
+
+  protected onEmailAddressInput() {
     // https://www.regular-expressions.info
     this.isEmailAddressValid = Boolean(this.$.email_address.value.match('^[a-zA-Z0-9._%+\\-]+@[a-zA-Z0-9.\\-]+\\.[a-zA-Z]{2,4}$'))
   }
 
-  private onAccountNameInput() {
+  protected onAccountNameInput() {
     this.isAccountNameValid = this.$.account_name.value.length !== 0
   }
 
-  private onCreatePasswordInput() {
+  protected onCreatePasswordInput() {
     this.isCreatePasswordValid = this.$.create_password.value.length !== 0
   }
 
-  private onConfirmPasswordInput() {
+  protected onConfirmPasswordInput() {
     this.isConfirmPasswordValid = this.$.confirm_password.value.length !== 0
   }
 
-  private onChange(e: { checked: boolean }) {
+  protected onChange(e: { checked: boolean }) {
     this.isChecked = e.checked
   }
 
-  private canCreateAccount() {
-    return this.isEmailAddressValid && this.isAccountNameValid && this.isCreatePasswordValid && this.isConfirmPasswordValid && this.isChecked
-  }
-
-  private show() {
+  protected show() {
     // event.preventDefault()
     // const icon = this.$.password.querySelector('#icon')
     // if (!icon) {
@@ -68,20 +76,16 @@ class SettingsBraveAccountCreateDialogElement extends PolymerElement {
     // this.$.password.setAttribute('type', isShowing ? 'password' : 'text')
   }
 
-  private onForgotPasswordButtonClicked() {
-    this.dispatchEvent(new CustomEvent('forgot-password-button-clicked'))
-  }
-
-  private isEmailAddressValid: boolean = false
-  private isAccountNameValid: boolean = false
-  private isCreatePasswordValid: boolean = false
-  private isConfirmPasswordValid: boolean = false
-  private isChecked: boolean = false
+  protected isEmailAddressValid: boolean = false
+  protected isAccountNameValid: boolean = false
+  protected isCreatePasswordValid: boolean = false
+  protected isConfirmPasswordValid: boolean = false
+  protected isChecked: boolean = false
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'settings-brave-account-create-dialog': SettingsBraveAccountCreateDialogElement;
+    'settings-brave-account-create-dialog': SettingsBraveAccountCreateDialogElement
   }
 }
 
