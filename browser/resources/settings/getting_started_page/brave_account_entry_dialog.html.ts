2d898e0715feadd5128cbfbe7f9728e3a73fff70
Szilard Szaloki <szilardszaloki@gmail.com>
2024-10-23 10:15:21 -0600

    `Polymer` `==>` `Lit`

---
+++ browser/resources/settings/getting_started_page/brave_account_entry_dialog.html.ts
@@ -0,0 +1,35 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * you can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import './brave_account_dialog.js'
+import { html } from '//resources/lit/v3_0/lit.rollup.js'
+import { SettingsBraveAccountEntryDialogElement } from './brave_account_entry_dialog.js'
+
+export function getHtml(this: SettingsBraveAccountEntryDialogElement) {
+  return html`<!--_html_template_start_-->
+    <settings-brave-account-dialog
+      header-text-top="Get started with your Brave account"
+      header-text-bottom="$i18n{braveSyncBraveAccountDesc}">
+      <div slot="buttons">
+        <leo-button size="medium" @click=${() => this.fire('create-button-clicked')}>
+          Create a Brave account
+        </leo-button>
+        <leo-button kind="outline" size="medium" @click=${() => this.fire('sign-in-button-clicked')}>
+          Already have an account? Sign in
+        </leo-button>
+      </div>
+      <div slot="footer" class="footer">
+        <div class="footer-text">
+          For advanced users, we also support self-custody of your Brave account keys for an extra layer of privacy. <a href="#"><u>Learn more</u></a>
+        </div>
+        <div class="button-wrapper">
+          <leo-button class="button" kind="plain" size="medium" @click=${() => this.fire('self-custody-button-clicked')}>
+            Use self-custody
+          </leo-button>
+        </div>
+      </div>
+    </settings-brave-account-dialog>
+  <!--_html_template_end_-->`
+}
--- browser/resources/settings/getting_started_page/brave_account_sign_in_dialog.ts
+++ browser/resources/settings/getting_started_page/brave_account_sign_in_dialog.ts
@@ -3,68 +3,66 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * you can obtain one at https://mozilla.org/MPL/2.0/. */
 
-import './brave_account_dialog.js';
-import 'chrome://resources/brave/leo.bundle.js'
-
-import { PolymerElement } from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-import { getTemplate } from './brave_account_sign_in_dialog.html.js'
+import { CrLitElement } from '//resources/lit/v3_0/lit.rollup.js'
+import { getCss } from './brave_account_sign_in_dialog.css.js'
+import { getHtml } from './brave_account_sign_in_dialog.html.js'
 
 /**
  * @fileoverview
  * 'settings-brave-account-sign-in-dialog'...
  */
 
-interface SettingsBraveAccountSignInDialogElement {
+export interface SettingsBraveAccountSignInDialogElement {
   $: {
     email: HTMLInputElement,
     password: HTMLInputElement,
   }
 }
 
-class SettingsBraveAccountSignInDialogElement extends PolymerElement {
+export class SettingsBraveAccountSignInDialogElement extends CrLitElement {
   static get is() {
     return 'settings-brave-account-sign-in-dialog'
   }
 
-  static get template() {
-    return getTemplate()
+  static override get styles() {
+    return getCss()
   }
 
-  private onEmailInput() {
+  override render() {
+    return getHtml.bind(this)()
+  }
+
+  static override get properties() {
+    return {
+      isEmailValid: { type: Boolean },
+      isPasswordValid: { type: Boolean },
+    }
+  }
+
+  protected onEmailInput() {
     // https://www.regular-expressions.info
     this.isEmailValid = Boolean(this.$.email.value.match('^[a-zA-Z0-9._%+\\-]+@[a-zA-Z0-9.\\-]+\\.[a-zA-Z]{2,4}$'))
   }
 
-  private onPasswordInput() {
+  protected onPasswordInput() {
     this.isPasswordValid = this.$.password.value.length !== 0
   }
 
-  private canSignIn() {
-    return this.isEmailValid && this.isPasswordValid
-  }
-
-  private show(event: Event) {
+  protected show(event: Event) {
     event.preventDefault()
     const icon = this.$.password.querySelector('#icon')
-    if (!icon) {
-      return
-    }
-    const isShowing = icon.getAttribute('name') === 'eye-on'
-    icon.setAttribute('name', isShowing ? 'eye-off' : 'eye-on')
+    const isShowing = icon!.getAttribute('name') === 'eye-on'
+    icon!.setAttribute('name', isShowing ? 'eye-off' : 'eye-on')
     this.$.password.setAttribute('type', isShowing ? 'password' : 'text')
   }
 
-  private onForgotPasswordButtonClicked() {
-    this.dispatchEvent(new CustomEvent('forgot-password-button-clicked'))
-  }
-
-  private isEmailValid: boolean = false
-  private isPasswordValid: boolean = false
+  protected isEmailValid: boolean = false
+  protected isPasswordValid: boolean = false
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'settings-brave-account-sign-in-dialog': SettingsBraveAccountSignInDialogElement;
+    'settings-brave-account-sign-in-dialog': SettingsBraveAccountSignInDialogElement
   }
 }
 
