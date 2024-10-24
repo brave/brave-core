bd2a83b4f766b4054daa15c4514ad5cc9c17f1fd
deeppandya <deeppandya91@gmail.com>
2024-10-23 20:40:42 -0400

    Remove unnecessary feature flag

--- chromium_src/chrome/browser/flags/android/chrome_feature_list.cc
+++ chromium_src/chrome/browser/flags/android/chrome_feature_list.cc
@@ -6,7 +6,6 @@
 #include "base/feature_override.h"
 #include "brave/browser/android/background_video/features.h"
 #include "brave/browser/android/safe_browsing/features.h"
-#include "brave/browser/brave_browser_features.h"
 #include "brave/components/ai_chat/core/common/buildflags/buildflags.h"
 #include "brave/components/brave_news/common/features.h"
 #include "brave/components/brave_rewards/common/features.h"
@@ -50,8 +49,7 @@
     &google_sign_in_permission::features::kBraveGoogleSignInPermission, \
     &net::features::kBraveForgetFirstPartyStorage,                      \
     &brave_shields::features::kBraveShowStrictFingerprintingMode,       \
-    &brave_shields::features::kBraveLocalhostAccessPermission,          \
-    &features::kBraveDayZeroExperiment
+    &brave_shields::features::kBraveLocalhostAccessPermission
 
 // clang-format on
 
