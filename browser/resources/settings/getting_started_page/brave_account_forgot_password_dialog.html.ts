1f866b145b7416c61ec271e73f0518dbc3aea50b
Szilard Szaloki <szilardszaloki@gmail.com>
2024-10-24 09:50:07 -0600

    "Forgot password" dialog — `Lit`

---
+++ browser/resources/settings/getting_started_page/brave_account_forgot_password_dialog.html.ts
@@ -0,0 +1,30 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * you can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import './brave_account_dialog.js'
+import { html } from '//resources/lit/v3_0/lit.rollup.js'
+import type { SettingsBraveAccountForgotPasswordDialogElement } from './brave_account_forgot_password_dialog.js'
+
+export function getHtml(this: SettingsBraveAccountForgotPasswordDialogElement) {
+  return html`
+    <settings-brave-account-dialog show-back-button
+      header-text-top="Forgot your password?"
+      header-text-bottom="Confirm your Brave account email and we'll email you a link to reset your password. The link will only be valid for [X] hours.">
+      <div slot="inputs">
+        <leo-input id="email" placeholder="Enter your email address" @input=${this.onEmailInput}>
+          <div class="label">Email address</div>
+        </leo-input>
+      </div>
+      <div slot="buttons">
+        <leo-button size="medium" ?isDisabled=${!this.isEmailValid}>
+          Reset your password
+        </leo-button>
+        <leo-button size="medium" kind="plain-faint" @click=${() => this.fire('cancel-button-clicked')}>
+          Cancel
+        </leo-button>
+      </div>
+    </settings-brave-account-dialog>
+  `
+}
