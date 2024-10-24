910f99e2141769e1cd84ec574e820bcc04ab805e
Nuo Xu <nuoxu.nx@gmail.com>
2024-10-24 11:33:15 -0700

    sec(wallet): adding a warning message for Solana System Program assign instruction (#26198)

--- ios/brave-ios/Sources/BraveWallet/Crypto/Transactions/TransactionParser.swift
+++ ios/brave-ios/Sources/BraveWallet/Crypto/Transactions/TransactionParser.swift
@@ -1027,6 +1027,23 @@ struct ParsedTransaction: Equatable {
     }
   }
 
+  var hasSystemProgramAssignInstruction: Bool {
+    if case .solDappTransaction(let details) = details {
+      for parsedInstruction in details.instructions {
+        let isAssign =
+          parsedInstruction.instruction.instructionTypeName
+          == WalletConstants.solanaTxInstructionTypeNameAssign
+        let isAssignWithSeed =
+          parsedInstruction.instruction.instructionTypeName
+          == WalletConstants.solanaTxInstructionTypeNameAssignWithSeed
+        if isAssign || isAssignWithSeed {
+          return true
+        }
+      }
+    }
+    return false
+  }
+
   init() {
     self.transaction = .init()
     self.namedFromAddress = ""
