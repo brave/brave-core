d7f2abe2688f95d1bac38825154062299dc6c92a
Agustín Ruiz <aguscr182@gmail.com>
2024-10-24 17:08:10 -0400

    Desktop settings - Fixes (#26177)
    * Fix unnecessary scroll
    
    Fixes https://github.com/brave/brave-browser/issues/41796#issuecomment-2433314568
    
    * Add Manage Profile page to customize padding
    
    * Update to Nala tokens
    
    * Update browser/resources/settings/brave_overrides/settings_manage_profile.ts
    
    Co-authored-by: Jay Harris <jay.harris@outlook.co.nz>
    
    * imports: order alphabetically
    
    ---------
    
    Co-authored-by: Jay Harris <jay.harris@outlook.co.nz>

--- browser/resources/settings/brave_overrides/settings_ui.ts
+++ browser/resources/settings/brave_overrides/settings_ui.ts
@@ -65,7 +65,7 @@ RegisterStyleOverride(
         background: var(--leo-color-page-background);
         padding-bottom: var(--leo-spacing-2xl);
         border-radius: var(--leo-radius-xl) var(--leo-radius-m) var(--leo-radius-m) var(--leo-radius-xl);
-        overflow: scroll;
+        overflow: auto;
       }
       #right {
         /* this element is only a space filler in chromium */
