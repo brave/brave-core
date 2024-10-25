506d9377b26ebb2aded879671f8b3a10d5bd694b
Jagadesh P <jagadeshjai1999@gmail.com>
2024-10-25 05:31:15 +0530

    [VerticalTabs] Add browser tests to verify the toggle_vertical_tabs command in tab_strip/toolbar.

--- browser/ui/views/frame/brave_system_menu_model_builder.cc
+++ browser/ui/views/frame/brave_system_menu_model_builder.cc
@@ -6,6 +6,7 @@
 #include "brave/browser/ui/views/frame/brave_system_menu_model_builder.h"
 
 #include "brave/app/brave_command_ids.h"
+#include "brave/browser/ui/views/tabs/vertical_tab_utils.h"
 #include "brave/grit/brave_generated_resources.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "ui/base/models/simple_menu_model.h"
@@ -19,13 +20,15 @@ BraveSystemMenuModelBuilder::~BraveSystemMenuModelBuilder() = default;
 
 void BraveSystemMenuModelBuilder::InsertBraveSystemMenuForBrowserWindow(
     ui::SimpleMenuModel* model) {
-  std::optional<size_t> task_manager_index =
-      model->GetIndexOfCommandId(IDC_TASK_MANAGER);
+  if (tabs::utils::SupportsVerticalTabs(browser())) {
+    std::optional<size_t> task_manager_index =
+        model->GetIndexOfCommandId(IDC_TASK_MANAGER);
 
-  if (task_manager_index.has_value()) {
-    model->InsertCheckItemWithStringIdAt(task_manager_index.value(),
-                                         IDC_TOGGLE_VERTICAL_TABS,
-                                         IDS_TAB_CXMENU_SHOW_VERTICAL_TABS);
+    if (task_manager_index.has_value()) {
+      model->InsertCheckItemWithStringIdAt(task_manager_index.value(),
+                                           IDC_TOGGLE_VERTICAL_TABS,
+                                           IDS_TAB_CXMENU_SHOW_VERTICAL_TABS);
+    }
   }
 }
 
