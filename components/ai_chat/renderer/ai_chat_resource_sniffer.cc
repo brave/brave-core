8ee53fd6e71778f8b43642f140673b78f014848d
Pavel Beloborodov <73575789+boocmp@users.noreply.github.com>
2024-10-26 14:57:40 +0700

    Replaced NOTREACHED_IN_MIGRATION with NOTREACHED. (#26229)

--- components/ai_chat/renderer/ai_chat_resource_sniffer.cc
+++ components/ai_chat/renderer/ai_chat_resource_sniffer.cc
@@ -82,7 +82,7 @@ bool AIChatResourceSniffer::IsTransformer() const {
 void AIChatResourceSniffer::Transform(
     std::string body,
     base::OnceCallback<void(std::string)> on_complete) {
-  NOTREACHED_IN_MIGRATION();
+  NOTREACHED();
 }
 
 void AIChatResourceSniffer::UpdateResponseHead(
