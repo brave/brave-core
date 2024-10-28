57cc98bebe28061cf1dbff774f031055ce4bb93d
Szilard Szaloki <szilardszaloki@gmail.com>
2024-10-28 11:36:16 -0600

    Styles links precisely.

--- browser/resources/settings/getting_started_page/brave_account_entry_dialog.html.ts
+++ browser/resources/settings/getting_started_page/brave_account_entry_dialog.html.ts
@@ -22,7 +22,7 @@ export function getHtml(this: SettingsBraveAccountEntryDialogElement) {
       </div>
       <div slot="footer" class="footer">
         <div class="footer-text">
-          For advanced users, we also support self-custody of your Brave account keys for an extra layer of privacy. <a href="#"><u>Learn more</u></a>
+          For advanced users, we also support self-custody of your Brave account keys for an extra layer of privacy. <a href="#">Learn more</a>
         </div>
         <div class="button-wrapper">
           <leo-button class="button" kind="plain" size="medium" @click=${() => this.fire('self-custody-button-clicked')}>
