48025be04a5687d8def02bacb5b19a7e20c6c2a4
Emerick Rogul <erogul@brave.com>
2024-10-28 10:36:14 -0400

    Migrate extensions sidebar override to use Lit
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/b926b290454f361d1ca67ee185a41a32aed62f0d
    
    commit b926b290454f361d1ca67ee185a41a32aed62f0d
    Author: rbpotter <rbpotter@chromium.org>
    Date:   Wed Oct 2 20:53:24 2024 +0000
    
        Extensions: Migrate sidebar and toolbar to Lit
    
        Bug: 40943652

--- browser/resources/extensions/brave_overrides/side_bar.ts
+++ browser/resources/extensions/brave_overrides/side_bar.ts
@@ -3,39 +3,34 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-// @ts-nocheck TODO(petemill): Define types and remove ts-nocheck
+import { injectStyle } from '//resources/brave/lit_overriding.js'
+import { css } from '//resources/lit/v3_0/lit.rollup.js'
+import { ExtensionsSidebarElement } from '../sidebar.js'
 
-import {RegisterStyleOverride} from 'chrome://resources/brave/polymer_overriding.js'
-import {html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js'
+injectStyle(ExtensionsSidebarElement, css`
+  .cr-nav-menu-item {
+    min-height: 20px !important;
+    border-end-end-radius: 0px !important;
+    border-start-end-radius: 0px !important;
+    box-sizing: content-box !important;
+  }
 
-RegisterStyleOverride(
-  'extensions-sidebar',
-  html`
-    <style>
-      .cr-nav-menu-item {
-        min-height: 20px !important;
-        border-end-end-radius: 0px !important;
-        border-start-end-radius: 0px !important;
-        box-sizing: content-box !important;
-      }
+  .cr-nav-menu-item:hover {
+    background: transparent !important;
+  }
 
-      .cr-nav-menu-item:hover {
-        background: transparent !important;
-      }
+  .cr-nav-menu-item[selected] {
+    --iron-icon-fill-color: var(--cr-link-color) !important;
+    color: var(--cr-link-color) !important;
+    background: transparent !important;
+  }
 
-      .cr-nav-menu-item[selected] {
-        --iron-icon-fill-color: var(--cr-link-color) !important;
-        color: var(--cr-link-color) !important;
-        background: transparent !important;
-      }
+  .cr-nav-menu-item iron-icon {
+    display: none !important;
+  }
 
-      .cr-nav-menu-item iron-icon {
-        display: none !important;
-      }
-
-      .cr-nav-menu-item cr-ripple {
-        display: none !important;
-      }
-    </style>
+  .cr-nav-menu-item cr-ripple {
+    display: none !important;
+  }
   `
 )
