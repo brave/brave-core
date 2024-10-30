f69aa89bbd26d40952db28f87ef8de09a572d6ef
Artem Samoilenko <artem@brave.com>
2024-10-30 13:08:27 -0400

    [Android] Fix for crash in Password settings
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/df77cbacb9476b0b6d3e75cc34b5d127985e79c3
    
    [Settings] Enable the single-activity mode by default
    
    Bug: b/356743945

--- android/java/org/chromium/chrome/browser/settings/BraveSettingsIntentUtil.java
+++ android/java/org/chromium/chrome/browser/settings/BraveSettingsIntentUtil.java
@@ -12,7 +12,13 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
+
 public class BraveSettingsIntentUtil {
+    private static String PASSWORD_SETTINGS_FRAGMENT =
+            "org.chromium.chrome.browser.password_manager.settings.PasswordSettings";
+    private static String CREDENTIAL_EDIT_FRAGMENT =
+            "org.chromium.chrome.browser.password_entry_edit.CredentialEditFragmentView";
 
     public static Intent createIntent(
             @NonNull Context context,
@@ -20,6 +26,19 @@ public class BraveSettingsIntentUtil {
             @Nullable Bundle fragmentArgs) {
         Intent intent = SettingsIntentUtil.createIntent(context, fragmentName, fragmentArgs);
         intent.setClass(context, BraveSettingsActivity.class);
+        /*
+         * Password settings and credential edit fragments are not used in the upstream anymore and
+         * thus not adjusted to be used within single Settings activity. For now we just open them
+         * with a separate activity as it used to be.
+         * Going forward we should adjust them to be used within the single Settings activity
+         * https://github.com/brave/brave-browser/issues/41977
+         */
+        if (ChromeFeatureList.sSettingsSingleActivity.isEnabled()
+                && fragmentName != null
+                && (fragmentName.equals(PASSWORD_SETTINGS_FRAGMENT)
+                        || fragmentName.equals(CREDENTIAL_EDIT_FRAGMENT))) {
+            intent.removeFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        }
         return intent;
     }
 }
