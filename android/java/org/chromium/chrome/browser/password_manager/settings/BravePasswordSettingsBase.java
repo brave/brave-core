c22f2c501201c3ce67698493325c6881f8334a36
Artem Samoilenko <artem@brave.com>
2024-10-30 13:08:27 -0400

    [Android] Fix for crash in Password settings
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/df77cbacb9476b0b6d3e75cc34b5d127985e79c3
    
    [Settings] Enable the single-activity mode by default
    
    Bug: b/356743945

--- android/java/org/chromium/chrome/browser/password_manager/settings/BravePasswordSettingsBase.java
+++ android/java/org/chromium/chrome/browser/password_manager/settings/BravePasswordSettingsBase.java
@@ -5,32 +5,10 @@
 
 package org.chromium.chrome.browser.password_manager.settings;
 
-import android.view.Menu;
-
 import org.chromium.chrome.browser.settings.BravePreferenceFragment;
 
 public abstract class BravePasswordSettingsBase extends BravePreferenceFragment {
-    /*
-     * This variable will be used instead of upstream's `PasswordSettings#mMenu`.
-     */
-    protected Menu mMenu;
-
     public void createCheckPasswords() {
         // Do nothing here as we don't have check passwords option in Brave.
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-
-        /*
-         * In `PasswordSettings` class `passwordListAvailable` method gets called before `onCreateOptionsMenu`.
-         * Now on pressing `back`, Seetings activity is not finished, but just returns 1 fragment back.
-         * This causes null pointer crash on returning to the password settings page, since `mMenu` is already not null,
-         * but not inflated yet by the time we call `passwordListAvailable` and we try to access `export_passwords`.
-         * This issue is not happening in the upstream since they use different password settings page that utilizes OS level password manager.
-         * So here we make sure `mMenu` is null every time we leave password settings page.
-         */
-        mMenu = null;
     }
 }
