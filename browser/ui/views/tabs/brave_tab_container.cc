183be4ebb68faf1b93dbc407ff1061e0c16e2149
Claudio DeSouza <cdesouza@brave.com>
2024-11-01 14:49:47 +0000

    [cr132] Revert "[cr131] `Tab::set_group` => `Tab::SetGroup`"
    This reverts commit d9a3fa1250b65923c9ef671dfb276d1a5dd7191d.
    
    This got reverted in upstream
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/1eadd223cce221615fa18db6647451a26a0315a2
    
    commit bf601391652cebeaae149fab39eda062a5f28732
    Author: Nasko Oskov <nasko@chromium.org>
    Date:   Thu Oct 31 16:57:50 2024 +0000
    
        Revert "[views-ax] Migration of kName attribute in Tab"
    
        This reverts commit 1eadd223cce221615fa18db6647451a26a0315a2.
    
        Reason for revert: This CL has regressed window name availability to a11y APIs - https://crbug.com/374947846.
    
        Original change's description:
        > [views-ax] Migration of kName attribute in Tab
        >
        > This CL migrates the kName attribute in views to be updated
        > whenever its value should change, rather than querying the value
        > and computing it only when needed.
        >
        > As part of this change, the following views are updated:
        > Tab, BrowserView, PermissionChipView, and TabSlotView. The Tab
        > class extends TabSlotView. BrowserView::GetAccessibleTabLabel
        >  constructs the accessible name for a tab by gathering various
        > properties such as the title, group, memory usage, and possibly
        > other elements like permission chips, to create a comprehensive
        > and accurate label.
        >
        > This CL is part of the ViewsAX project:
        > https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
        >
        > Bug: 325137417
        > Change-Id: Id1ca1da2e970c82575dff12bd48434f716d4548f
        > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5836477
        > Reviewed-by: Jacques Newman <janewman@microsoft.com>
        > Commit-Queue: Gaurav Kumar <gauravkum@microsoft.com>
        > Reviewed-by: Elly FJ <ellyjones@chromium.org>
        > Cr-Commit-Position: refs/heads/main@{#1367340}
    
        Bug: 325137417

--- browser/ui/views/tabs/brave_tab_container.cc
+++ browser/ui/views/tabs/brave_tab_container.cc
@@ -405,7 +405,7 @@ void BraveTabContainer::SetTabSlotVisibility() {
   for (Tab* tab : layout_helper_->GetTabs()) {
     if (std::optional<tab_groups::TabGroupId> group = tab->group();
         group && !group_views_.contains(*group)) {
-      tab->SetGroup(std::nullopt);
+      tab->set_group(std::nullopt);
     }
   }
 
