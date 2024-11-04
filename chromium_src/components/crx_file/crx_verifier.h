3b81f266bda5bda081937f3bf731c98370b26e3d
cdesouza-chromium <cdesouza@brave.com>
2024-11-04 18:53:15 +0000

    [CodeHealth] Avoid `NoDestructor` where a static array suffices (#26354)
    This PR removes some of the uses of `NoDestructor` for cases where
    `std::vector` was being used to store static data. This is safe, as the
    storage of the data is static.

--- chromium_src/components/crx_file/crx_verifier.h
+++ chromium_src/components/crx_file/crx_verifier.h
@@ -6,11 +6,13 @@
 #ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_CRX_FILE_CRX_VERIFIER_H_
 #define BRAVE_CHROMIUM_SRC_COMPONENTS_CRX_FILE_CRX_VERIFIER_H_
 
+#include "base/containers/span.h"
+
 #include "src/components/crx_file/crx_verifier.h"  // IWYU pragma: export
 
 namespace crx_file {
 
-void SetBravePublisherKeyHashForTesting(const std::vector<uint8_t>& test_key);
+void SetBravePublisherKeyHashForTesting(base::span<const uint8_t> test_key);
 
 }  // namespace crx_file
 
