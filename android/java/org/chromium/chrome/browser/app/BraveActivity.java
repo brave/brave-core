9d25c58e002db34abf66c4fcc721857f53690005
Simone Arpe <simon.arpe@gmail.com>
2024-11-04 17:25:56 +0100

    Improve logic upon rotations

--- android/java/org/chromium/chrome/browser/app/BraveActivity.java
+++ android/java/org/chromium/chrome/browser/app/BraveActivity.java
@@ -529,7 +529,7 @@ public abstract class BraveActivity extends ChromeActivity
                 && isYTVideoUrl(currentTab.getUrl())
                 && !isInPictureInPictureMode()
                 && BackgroundVideoPlaybackTabHelper.isPlayingMedia(currentTab.getWebContents())) {
-            BackgroundVideoPlaybackTabHelper.toggleFullscreen(currentTab.getWebContents(), true);
+            BackgroundVideoPlaybackTabHelper.setFullscreen(currentTab.getWebContents());
             try {
                 enterPictureInPictureMode(new PictureInPictureParams.Builder().build());
             } catch (IllegalStateException | IllegalArgumentException e) {
@@ -546,10 +546,10 @@ public abstract class BraveActivity extends ChromeActivity
         if (currentTab != null
                 && currentTab.getUrl() != null
                 && isYTVideoUrl(currentTab.getUrl())
-                && !isInPictureInPictureMode()) {
-            BackgroundVideoPlaybackTabHelper.toggleFullscreen(
-                    currentTab.getWebContents(),
-                    newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE);
+                && !isInPictureInPictureMode()
+                && newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            BackgroundVideoPlaybackTabHelper.setFullscreen(
+                    currentTab.getWebContents());
         }
     }
 
