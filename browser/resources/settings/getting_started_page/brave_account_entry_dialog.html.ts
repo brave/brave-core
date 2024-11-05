924c688d72be092b396bfb5c7898871b558d5c22
Szilard Szaloki <szilardszaloki@gmail.com>
2024-11-05 10:51:37 -0700

    default button size

--- browser/resources/settings/getting_started_page/brave_account_entry_dialog.html.ts
+++ browser/resources/settings/getting_started_page/brave_account_entry_dialog.html.ts
@@ -12,10 +12,10 @@ export function getHtml(this: SettingsBraveAccountEntryDialogElement) {
     <settings-brave-account-dialog text-bottom="$i18n{braveSyncBraveAccountDesc}"
                                    text-top="Get started with your Brave account">
       <div slot="buttons">
-        <leo-button size="medium" @click=${() => this.fire('create-button-clicked')}>
+        <leo-button @click=${() => this.fire('create-button-clicked')}>
           Create a Brave account
         </leo-button>
-        <leo-button kind="outline" size="medium" @click=${() => this.fire('sign-in-button-clicked')}>
+        <leo-button kind="outline" @click=${() => this.fire('sign-in-button-clicked')}>
           Already have an account? Sign in
         </leo-button>
       </div>
@@ -23,7 +23,7 @@ export function getHtml(this: SettingsBraveAccountEntryDialogElement) {
         <div class="footer-text">
           For advanced users, we also support self-custody of your Brave account keys for an extra layer of privacy. <a href="#">Learn more</a>
         </div>
-        <leo-button class="button" kind="plain-faint" size="medium" @click=${() => this.fire('self-custody-button-clicked')}>
+        <leo-button class="button" kind="plain-faint" @click=${() => this.fire('self-custody-button-clicked')}>
           Use self-custody
         </leo-button>
       </div>
