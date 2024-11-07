5cb05fa04f2a82e89bbdc71f3de7b6a36be99a0a
Aleksei Seren <aseren@brave.com>
2024-11-07 18:45:15 -0500

    [ads] Decouple AdsService functionality from iOS BraveAds implementation

---
+++ ios/browser/ads/ads_service_factory_ios.mm
@@ -0,0 +1,49 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "brave/ios/browser/ads/ads_service_factory_ios.h"
+
+#include "base/no_destructor.h"
+#include "brave/ios/browser/ads/ads_service_impl_ios.h"
+#include "components/keyed_service/core/keyed_service.h"
+#include "components/keyed_service/ios/browser_state_dependency_manager.h"
+#include "components/pref_registry/pref_registry_syncable.h"
+#include "ios/chrome/browser/shared/model/profile/profile_ios.h"
+#include "ios/web/public/browser_state.h"
+
+namespace brave_ads {
+
+// static
+AdsServiceImplIOS* AdsServiceFactoryIOS::GetForBrowserState(
+    ProfileIOS* profile) {
+  return static_cast<AdsServiceImplIOS*>(
+      GetInstance()->GetServiceForBrowserState(profile, true));
+}
+
+// static
+AdsServiceFactoryIOS* AdsServiceFactoryIOS::GetInstance() {
+  static base::NoDestructor<AdsServiceFactoryIOS> instance;
+  return instance.get();
+}
+
+AdsServiceFactoryIOS::AdsServiceFactoryIOS()
+    : BrowserStateKeyedServiceFactory(
+          "AdsService",
+          BrowserStateDependencyManager::GetInstance()) {}
+
+AdsServiceFactoryIOS::~AdsServiceFactoryIOS() = default;
+
+std::unique_ptr<KeyedService> AdsServiceFactoryIOS::BuildServiceInstanceFor(
+    web::BrowserState* context) const {
+  ProfileIOS* profile = ProfileIOS::FromBrowserState(context);
+  CHECK(profile);
+  if (profile->IsOffTheRecord()) {
+    return {};
+  }
+
+  return std::make_unique<AdsServiceImplIOS>(profile->GetPrefs());
+}
+
+}  // namespace brave_ads
---
+++ ios/browser/ads/ads_service_impl_ios.h
@@ -0,0 +1,127 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_IOS_BROWSER_ADS_ADS_SERVICE_IMPL_IOS_H_
+#define BRAVE_IOS_BROWSER_ADS_ADS_SERVICE_IMPL_IOS_H_
+
+#include <memory>
+#include <string>
+
+#include "base/functional/callback_forward.h"
+#include "base/memory/weak_ptr.h"
+#include "base/threading/sequence_bound.h"
+#include "brave/components/brave_ads/core/mojom/brave_ads.mojom-forward.h"
+#include "brave/components/brave_ads/core/public/ads_callback.h"
+#include "brave/components/brave_ads/core/public/ads_client/ads_client_callback.h"
+#include "components/keyed_service/core/keyed_service.h"
+
+class PrefService;
+
+namespace base {
+class SequencedTaskRunner;
+}  // namespace base
+
+namespace brave_ads {
+
+class Ads;
+class AdsClient;
+class Database;
+
+class AdsServiceImplIOS : public KeyedService {
+ public:
+  explicit AdsServiceImplIOS(PrefService* prefs);
+
+  AdsServiceImplIOS(const AdsServiceImplIOS&) = delete;
+  AdsServiceImplIOS& operator=(const AdsServiceImplIOS&) = delete;
+
+  AdsServiceImplIOS(AdsServiceImplIOS&&) noexcept = delete;
+  AdsServiceImplIOS& operator=(AdsServiceImplIOS&&) noexcept = delete;
+
+  ~AdsServiceImplIOS() override;
+
+  bool IsRunning() const;
+
+  void InitializeAds(const std::string& storage_path,
+                     AdsClient& ads_client,
+                     mojom::SysInfoPtr mojom_sys_info,
+                     mojom::BuildChannelInfoPtr mojom_build_channel,
+                     mojom::WalletInfoPtr mojom_wallet,
+                     InitializeCallback callback);
+  void ShutdownAds(ShutdownCallback callback);
+
+  void ClearData(base::OnceClosure callback);
+
+  void RunDBTransaction(mojom::DBTransactionInfoPtr mojom_db_transaction,
+                        RunDBTransactionCallback callback);
+
+  void GetStatementOfAccounts(GetStatementOfAccountsCallback callback);
+
+  void MaybeServeInlineContentAd(const std::string& dimensions,
+                                 MaybeServeInlineContentAdCallback callback);
+
+  void TriggerInlineContentAdEvent(
+      const std::string& placement_id,
+      const std::string& creative_instance_id,
+      mojom::InlineContentAdEventType mojom_ad_event_type,
+      TriggerAdEventCallback callback);
+
+  void TriggerNewTabPageAdEvent(
+      const std::string& placement_id,
+      const std::string& creative_instance_id,
+      mojom::NewTabPageAdEventType mojom_ad_event_type,
+      TriggerAdEventCallback callback);
+
+  void MaybeGetNotificationAd(const std::string& placement_id,
+                              MaybeGetNotificationAdCallback callback);
+
+  void TriggerNotificationAdEvent(
+      const std::string& placement_id,
+      mojom::NotificationAdEventType mojom_ad_event_type,
+      TriggerAdEventCallback callback);
+
+  void TriggerPromotedContentAdEvent(
+      const std::string& placement_id,
+      const std::string& creative_instance_id,
+      mojom::PromotedContentAdEventType mojom_ad_event_type,
+      TriggerAdEventCallback callback);
+
+  void MaybeGetSearchResultAd(const std::string& placement_id,
+                              MaybeGetSearchResultAdCallback callback);
+
+  void TriggerSearchResultAdEvent(
+      mojom::CreativeSearchResultAdInfoPtr mojom_creative_ad,
+      mojom::SearchResultAdEventType mojom_ad_event_type,
+      TriggerAdEventCallback callback);
+
+  void PurgeOrphanedAdEventsForType(
+      mojom::AdType mojom_ad_type,
+      PurgeOrphanedAdEventsForTypeCallback callback);
+
+ private:
+  // KeyedService:
+  void Shutdown() override;
+
+  void InitializeDatabase(const std::string& storage_path);
+  void Cleanup();
+
+  void OnInitialize(InitializeCallback callback, bool success);
+  void OnShutdown(ShutdownCallback callback, bool success);
+
+  const raw_ptr<PrefService> prefs_ = nullptr;  // Not owned.
+
+  const scoped_refptr<base::SequencedTaskRunner> database_queue_;
+
+  base::FilePath storage_path_;
+
+  base::SequenceBound<brave_ads::Database> database_;
+
+  std::unique_ptr<brave_ads::Ads> ads_;
+
+  base::WeakPtrFactory<AdsServiceImplIOS> weak_ptr_factory_{this};
+};
+
+}  // namespace brave_ads
+
+#endif  // BRAVE_IOS_BROWSER_ADS_ADS_SERVICE_IMPL_IOS_H_
