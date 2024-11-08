a1c1b000b653edf55a71af9e4b41fd029351776d
Agustín Ruiz <aruiz@brave.com>
2024-11-07 22:12:06 -0300

    Css tweaks and icon change for sidebar

--- components/ai_chat/resources/page/components/full_page/index.tsx
+++ components/ai_chat/resources/page/components/full_page/index.tsx
@@ -86,6 +86,7 @@ export default function FullScreen() {
 
   }, [isSmall])
 
+  // change to <Icon name={isNavigationCollapsed ? 'sidenav-expand' : 'sidenav-collapse'} />
   return (
     <div className={styles.fullscreen}>
       <div className={styles.left}>
