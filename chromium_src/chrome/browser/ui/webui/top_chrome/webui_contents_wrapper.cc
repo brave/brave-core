e60a9d44745156e4c278ac3391346a16038aa953
Emerick Rogul <erogul@brave.com>
2024-11-10 12:38:43 -0500

    [cr132] Fix `WebUIContentsWrapper::AddNewContents`
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/a8901e53d93008146ba5b5655e43107d8fe41172
    
    commit a8901e53d93008146ba5b5655e43107d8fe41172
    Author: Pengchao Cai <pengchaocai@chromium.org>
    Date:   Thu Nov 7 19:17:58 2024 +0000
    
        [data sharing] Open WebUI learn more links to browser
    
        This CL added the override of WebContentsDelegate::AddNewContents in
        WebUIContentsWrapper which will be invoked (by WebContentsImpl
        ::CreateNewWindow) when left clicking a href link from the data sharing
        webui webcontents. The call is then forwarded to WebUIContentsWrapper
        ::Host.
    
        Change-Id: Ib7366380bef6bb8f5447052334a7f7106578d2a1
        Bug: 373898207

--- chromium_src/chrome/browser/ui/webui/top_chrome/webui_contents_wrapper.cc
+++ chromium_src/chrome/browser/ui/webui/top_chrome/webui_contents_wrapper.cc
@@ -8,44 +8,62 @@
 #include <utility>
 
 #include "components/sessions/content/session_tab_helper.h"
-#include "components/site_engagement/content/site_engagement_helper.h"
-#include "content/public/browser/web_contents.h"
-#include "content/public/browser/web_contents_delegate.h"
-#include "content/public/browser/web_contents_observer.h"
 
-// The buble delegate doesnt allow to open popups and we use
+// The bubble delegate doesn't allow to open popups and we use
 // the Browser window delegate to redirect opening new popup content
 // to the Browser delegate instead of default one.
 // In order to close all popups we also save tab ids of each opened popup window
 // and close all with the bubble together.
-#define PrimaryPageChanged                                                    \
-  SetWebContentsAddNewContentsDelegate(                                       \
-      base::WeakPtr<content::WebContentsDelegate> browser_delegate) {         \
-    browser_delegate_ = std::move(browser_delegate);                          \
-  }                                                                           \
-  content::WebContents* WebUIContentsWrapper::AddNewContents(                 \
-      content::WebContents* source,                                           \
-      std::unique_ptr<content::WebContents> new_contents,                     \
-      const GURL& target_url, WindowOpenDisposition disposition,              \
-      const blink::mojom::WindowFeatures& window_features, bool user_gesture, \
-      bool* was_blocked) {                                                    \
-    if (!browser_delegate_) {                                                 \
-      return nullptr;                                                         \
-    }                                                                         \
-    auto* raw_popup_contents = new_contents.get();                            \
-    auto* contents = browser_delegate_->AddNewContents(                       \
-        source, std::move(new_contents), target_url,                          \
-        WindowOpenDisposition::NEW_POPUP, window_features, user_gesture,      \
-        was_blocked);                                                         \
-    auto tab_id =                                                             \
-        sessions::SessionTabHelper::IdForTab(raw_popup_contents).id();        \
-    popup_ids_.push_back(tab_id);                                             \
-    return contents;                                                          \
-  }                                                                           \
-  void WebUIContentsWrapper::ClearPopupIds() {                                \
-    popup_ids_.clear();                                                       \
-  }                                                                           \
-  void WebUIContentsWrapper::PrimaryPageChanged
 
+#define AddNewContents AddNewContents_ChromiumImpl
 #include "src/chrome/browser/ui/webui/top_chrome/webui_contents_wrapper.cc"
-#undef PrimaryPageChanged
+#undef AddNewContents
+
+void WebUIContentsWrapper::SetWebContentsAddNewContentsDelegate(
+    base::WeakPtr<content::WebContentsDelegate> browser_delegate) {
+  browser_delegate_ = std::move(browser_delegate);
+}
+
+void WebUIContentsWrapper::ClearPopupIds() {
+  popup_ids_.clear();
+}
+
+content::WebContents* WebUIContentsWrapper::Host::AddNewContents(
+    content::WebContents* source,
+    std::unique_ptr<content::WebContents> new_contents,
+    const GURL& target_url,
+    WindowOpenDisposition disposition,
+    const blink::mojom::WindowFeatures& window_features,
+    bool user_gesture,
+    bool* was_blocked) {
+  return WebUIContentsWrapper::Host::AddNewContents_ChromiumImpl(
+      source, std::move(new_contents), target_url, disposition, window_features,
+      user_gesture, was_blocked);
+}
+
+content::WebContents* WebUIContentsWrapper::AddNewContents(
+    content::WebContents* source,
+    std::unique_ptr<content::WebContents> new_contents,
+    const GURL& target_url,
+    WindowOpenDisposition disposition,
+    const blink::mojom::WindowFeatures& window_features,
+    bool user_gesture,
+    bool* was_blocked) {
+  if (!browser_delegate_) {
+    return host_ ? host_->AddNewContents(
+                       source, std::move(new_contents), target_url, disposition,
+                       window_features, user_gesture, was_blocked)
+                 : nullptr;
+  }
+  auto* raw_popup_contents = new_contents.get();
+  auto* contents = browser_delegate_->AddNewContents(
+      source, std::move(new_contents), target_url,
+      WindowOpenDisposition::NEW_POPUP, window_features, user_gesture,
+      was_blocked);
+  if (!contents) {
+    return nullptr;
+  }
+  auto tab_id = sessions::SessionTabHelper::IdForTab(raw_popup_contents).id();
+  popup_ids_.push_back(tab_id);
+  return contents;
+}
