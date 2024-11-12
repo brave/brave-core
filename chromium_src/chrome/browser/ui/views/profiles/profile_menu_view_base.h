62f9c4b0a1b30ad6c9db047babd44741825e4aeb
cdesouza-chromium <cdesouza@brave.com>
2024-11-12 23:46:03 +0000

    [DanglingPtr] Remove `ProfileMenuViewBase` patching (#26499)
    This patch was introduced as a temporary measure to allow switching on
    the dangling pointer detector.
    
    This PR fixes the two dangling pointers in `ProfileMenuViewBase` by
    avoiding calling `SetProfileIdentityInfo` more than once, and actually
    overriding it, so the first call to the `SetProfileIdentityInfo` can be
    given the correct arguments.
    
    One test had to be disabled, as it was not actually testing the way the
    profile menu viewer was being instantiated. The way brave does it
    requires to consult theme data, which is not available for this
    particular test.
    
    Resolves https://github.com/brave/brave-browser/issues/42231

--- chromium_src/chrome/browser/ui/views/profiles/profile_menu_view_base.h
+++ chromium_src/chrome/browser/ui/views/profiles/profile_menu_view_base.h
@@ -11,7 +11,13 @@
   friend class BraveProfileMenuViewTest; \
                                          \
  public:
+
+// Overriding this function to avoid a second call to it.
+#define SetProfileIdentityInfo(...) virtual SetProfileIdentityInfo(__VA_ARGS__)
+
 #include "src/chrome/browser/ui/views/profiles/profile_menu_view_base.h"  // IWYU pragma: export
+
 #undef BRAVE_PROFILE_MENU_VIEW_BASE_H_
+#undef SetProfileIdentityInfo
 
 #endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_PROFILES_PROFILE_MENU_VIEW_BASE_H_
