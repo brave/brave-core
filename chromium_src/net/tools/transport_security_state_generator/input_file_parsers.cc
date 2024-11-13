2b63c75e21aa2ef02d1248cce4e93bee4f355fae
Max Karolinskiy <41635752+mkarolin@users.noreply.github.com>
2024-11-13 02:09:54 -0500

    Upgrade from Chromium 131.0.6778.39 to Chromium 131.0.6778.69 (#26518)
    * Upgrade from Chromium 131.0.6778.39 to Chromium 131.0.6778.69
    
    * Update pins list timestamp
    
    * Recent tabs menu model has been updated.
    
    The upstream code has been updated to only show history clusters menu
    item when history clusters are enabled. We disable history clusters and
    don't need to remove that menu item manually any more.
    
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/088cf43bda717ed6db36ae23388f58b2cac5ac0c
    
    commit 088cf43bda717ed6db36ae23388f58b2cac5ac0c
    Author: Toni Barzic <tbarzic@chromium.org>
    Date:   Fri Nov 8 16:53:49 2024 +0000
    
        [Merge to 131] Don't show history clusters menu item if history clusters is disabled.
    
        Prior to this CL, history clusters could be disabled but Chrome would
        continue to show the history clusters menu item.
    
        Bug: 374013418, 376751203
    
    * Disables failing upstream unit tests.
    
    ---------
    
    Co-authored-by: brave-builds <brave-builds+devops@brave.com>

--- chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
+++ chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
@@ -552,9 +552,9 @@ bool ParseCertificatesFile(std::string_view certs_input,
                            Pinsets* pinsets,
                            base::Time* timestamp) {
   constexpr std::string_view brave_certs = R"brave_certs(
-# Last updated: Wed Nov  6 23:53:38 UTC 2024
+# Last updated: Tue Nov 12 21:42:18 UTC 2024
 PinsListTimestamp
-1730937218
+1731447738
 
 # =====BEGIN BRAVE ROOTS ASC=====
 #From https://www.amazontrust.com/repository/
