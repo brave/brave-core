75c6b3ddb93585b86593599c314a20ed917d866e
mkarolin <max@brave.com>
2024-11-14 15:51:23 -0500

    Update certificate manager menu styling.

---
+++ ui/webui/resources/cr_components/certificate_manager/brave_overrides/index.ts
@@ -0,0 +1,6 @@
+// Copyright (c) 2023 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import './certificate_manager_v2.js'
