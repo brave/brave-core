44f5db930f1fe9856d95e40cb6d2d4f7cb4d7c73
Sampson <jjdsampson@gmail.com>
2024-11-14 10:57:08 -0600

    adds informative error message
    We aim to provide an instructive error message to the user when they have provided an endpoint value that would only be valid with the enabling of optional private IPs.

--- components/ai_chat/resources/page/stories/locale.ts
+++ components/ai_chat/resources/page/stories/locale.ts
@@ -17,6 +17,7 @@ provideStrings({
   errorNetworkLabel: 'There was a network issue connecting to Leo, check your connection and try again.',
   errorRateLimit: 'You\'ve reached the premium rate limit. Please try again in a few hours.',
   braveLeoAssistantEndpointInvalidError: 'The endpoint URL is invalid. Please check the URL and try again.',
+  braveLeoAssistantEndpointValidAsPrivateIp: 'If you would like to use a private IP address, you must first enable "Private IP Addresses for Custom Model Enpoints" via brave://flags/#brave-ai-chat-allow-private-ips',
   retryButtonLabel: 'Retry',
   learnMore: 'Learn more',
   dismissButtonLabel: 'Dismiss',
