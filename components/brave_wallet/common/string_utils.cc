100dca45495185d4735298cd4556660dd1f719eb
cdesouza-chromium <cdesouza@brave.com>
2024-11-14 16:42:05 +0000

    [CodeHealth] Use `//base` backend for number conversions (#26553)
    Recently we managed to get a fix upstream to allow safe numerics with
    `int256_t` and similar types. This was done to correct some undefined
    behaviour on implemention doing back and forth convertion between
    256-bit numbers and string. Nevertheless, as safe numerics can now be
    used with these large ints, it also allows us to just use the `//base`
    backend for things like base10 conversions.
    
    This PR removes our own implementations for these conversions, and moves
    to use the chromium ones.
    
    Resolves https://github.com/brave/brave-browser/issues/42300

--- components/brave_wallet/common/string_utils.cc
+++ components/brave_wallet/common/string_utils.cc
@@ -10,80 +10,25 @@
 #include <string_view>
 
 #include "base/numerics/safe_math.h"
+#include "base/strings/string_number_conversions_internal.h"
 #include "base/strings/string_util.h"
 
 namespace brave_wallet {
 
-// Determines if the passed in base-10 string is valid
-bool IsValidBase10String(std::string_view input) {
-  if (input.empty()) {
-    return false;
-  }
-  auto check_input =
-      !input.empty() && input.front() == '-' ? input.substr(1) : input;
-  for (const auto& c : check_input) {
-    if (!base::IsAsciiDigit(c)) {
-      return false;
-    }
-  }
-  return true;
-}
-
 std::optional<uint256_t> Base10ValueToUint256(std::string_view input) {
-  if (!IsValidBase10String(input)) {
-    return std::nullopt;
-  }
-
-  base::CheckedNumeric<uint256_t> out = 0;
-  for (char c : input) {
-    out *= 10u;
-    out += static_cast<uint8_t>(base::HexDigitToInt(c));
-  }
-
-  if (!out.IsValid()) {
-    return std::nullopt;
-  }
-  return out.ValueOrDie();
+  uint256_t output;
+  return base::internal::StringToIntImpl(input, output) ? std::optional(output)
+                                                        : std::nullopt;
 }
 
 std::optional<int256_t> Base10ValueToInt256(std::string_view input) {
-  if (!IsValidBase10String(input)) {
-    return std::nullopt;
-  }
-
-  base::CheckedNumeric<int256_t> out = 0;
-  bool negative = (!input.empty() && input.front() == '-');
-  auto check_input = negative ? input.substr(1) : input;
-
-  for (char c : check_input) {
-    out *= 10;
-    if (negative) {
-      out -= base::HexDigitToInt(c);
-    } else {
-      out += base::HexDigitToInt(c);
-    }
-  }
-
-  if (!out.IsValid()) {
-    return std::nullopt;
-  }
-  return out.ValueOrDie();
+  int256_t output;
+  return base::internal::StringToIntImpl(input, output) ? std::optional(output)
+                                                        : std::nullopt;
 }
 
-std::string Uint256ValueToBase10(uint256_t input) {
-  if (input == 0) {
-    return "0";
-  }
-
-  std::string result;
-  result.reserve(78);  // 78 is the max length of a uint256_t in base 10
-
-  while (input > 0) {
-    result += '0' + static_cast<uint8_t>(input % 10);
-    input /= 10;
-  }
-  std::reverse(result.begin(), result.end());
-  return result;
+std::string Uint256ValueToBase10(uint256_t value) {
+  return base::internal::IntToStringT<std::string>(value);
 }
 
 }  // namespace brave_wallet
