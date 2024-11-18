b503a00d496a04cd40cd6806d6898ff91d277506
Claudio DeSouza <cdesouza@brave.com>
2024-11-18 11:42:01 +0000

    [DanglingPtr] Remove patching for `AutocompleteMatch`
    This PR removes the patching for `AutocompleteMatch`, introduced to
    allow switching on the dangling pointer detector, as a temporary
    measure.
    
    The main issue was that `BraveRenderViewContextMenu` was instantiating
    `AutocompleteClassifier` discretely, rather than using the factory for
    it, and this was causing dangling checks to fire with
    `AutocompleteClassifier`, which is supposed to be retrieved with
    `AutocompleteClassifierFactory`.
    
    Resolves https://github.com/brave/brave-browser/issues/42339

--- components/omnibox/browser/brave_history_quick_provider_unittest.cc
+++ components/omnibox/browser/brave_history_quick_provider_unittest.cc
@@ -115,6 +115,7 @@ class BraveHistoryQuickProviderTest : public testing::Test {
   }
 
   void TearDown() override {
+    ac_matches_.clear();
     provider_ = nullptr;
     client_.reset();
   }
@@ -242,9 +243,8 @@ class BraveHistoryQuickProviderTest : public testing::Test {
   base::ScopedTempDir history_dir_;
   std::unique_ptr<FakeAutocompleteProviderClient> client_;
 
-  ACMatches ac_matches_;  // The resulting matches after running RunTest.
-
   scoped_refptr<BraveHistoryQuickProvider> provider_;
+  ACMatches ac_matches_;  // The resulting matches after running RunTest.
 };
 
 BraveHistoryQuickProviderTest::SetShouldContain::SetShouldContain(
