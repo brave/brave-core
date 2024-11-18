ab25c42a644159ea2a73eeceb2cf494467cc9415
AlexeyBarabash <alexey@brave.com>
2024-11-19 00:03:59 +0200

    Addressed codereview notice - used shopt -s nullglob

--- build/android/pack_symbols.sh
+++ build/android/pack_symbols.sh
@@ -9,14 +9,8 @@ set -euo pipefail
 
 mkdir -p "$1"
 
-objects_to_compress="*.so apks lib.unstripped android_chrome_versions.txt"
+shopt -s nullglob
 
-if ls android_clang_*/*.so 1> /dev/null 2>&1; then
-    objects_to_compress="${objects_to_compress} android_clang_*/*.so"
-fi
+tar -czvf "$2" ./*.so apks android_clang_*/*.so android_clang_*/lib.unstripped lib.unstripped android_chrome_versions.txt
 
-if ls android_clang_*/lib.unstripped 1> /dev/null 2>&1; then
-    objects_to_compress="${objects_to_compress} android_clang_*/lib.unstripped"
-fi
-
-tar -czvf "$2" --ignore-failed-read "${objects_to_compress}"
+shopt -u nullglob
