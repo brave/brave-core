c9d4489726e7953238764a7c86e0da597370bf07
Artem Samoilenko <artem@brave.com>
2024-11-18 14:29:12 -0500

    [Android] Short circuit focus animation when controls are bottom-anchored
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/20d5954df30f7c8db5731ae2161d75fdf4630282
    
    [Anchor] Short circuit focus animation when controls are bottom-anchored
    
    This is one piece of a larger set of changes implementing the omnibox
    focus animation for bottom-anchored toolbar. Note that it also:
    
    * Removes old logic that inadvertently canceled the defocus animation
      (woops)
    * Wires a function to autocomplete allowing it to trigger its own logic
      for this scenario
    
    Bug: 369134015

--- android/java/org/chromium/chrome/browser/omnibox/BraveLocationBarCoordinator.java
+++ android/java/org/chromium/chrome/browser/omnibox/BraveLocationBarCoordinator.java
@@ -16,6 +16,7 @@ import org.chromium.base.Callback;
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.supplier.Supplier;
 import org.chromium.chrome.browser.back_press.BackPressManager;
+import org.chromium.chrome.browser.browser_controls.BrowserControlsStateProvider;
 import org.chromium.chrome.browser.browser_controls.BrowserStateBrowserControlsVisibilityDelegate;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
 import org.chromium.chrome.browser.merchant_viewer.MerchantTrustSignalsCoordinator;
@@ -89,7 +90,8 @@ public class BraveLocationBarCoordinator extends LocationBarCoordinator {
             LocationBarEmbedderUiOverrides uiOverrides,
             @Nullable View baseChromeLayout,
             Supplier<Integer> bottomWindowPaddingSupplier,
-            @Nullable OnLongClickListener onLongClickListener) {
+            @Nullable OnLongClickListener onLongClickListener,
+            @Nullable BrowserControlsStateProvider browserControlsStateProvider) {
         super(
                 locationBarLayout,
                 autocompleteAnchorView,
@@ -122,7 +124,8 @@ public class BraveLocationBarCoordinator extends LocationBarCoordinator {
                 () ->
                         bottomWindowPaddingSupplier.get()
                                 + (isBottomToolbarVisible() ? locationBarLayout.getHeight() : 0),
-                onLongClickListener);
+                onLongClickListener,
+                browserControlsStateProvider);
 
         if (mUrlBar != null) {
             ((UrlBar) mUrlBar).setSelectAllOnFocus(true);
