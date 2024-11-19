45f1a52ae54e79c2bfaee51139225d627e9e8bda
Max Karolinskiy <max@brave.com>
2024-11-19 16:06:31 -0500

    [cr133][Android] Fixes importer build error due to missing strings.
    ../../chrome/common/importer/firefox_importer_utils.cc:335:38: error: use of undeclared identifier 'IDS_IMPORT_FROM_ICEWEASEL'
      335 |     return l10n_util::GetStringUTF16(IDS_IMPORT_FROM_ICEWEASEL);
          |                                      ^
    ../../chrome/common/importer/firefox_importer_utils.cc:336:36: error: use of undeclared identifier 'IDS_IMPORT_FROM_FIREFOX'
      336 |   return l10n_util::GetStringUTF16(IDS_IMPORT_FROM_FIREFOX);
          |                                    ^
    2 errors generated.
    
    Upstream has these strings only for non-Android platforms, but we are
    using the importer code on Android as well, so the string need to be
    re-added for Android.

---
+++ chromium_src/chrome/common/importer/firefox_importer_utils.cc
@@ -0,0 +1,11 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+// Clang format wants to place this header after the .cc file for some reason.
+// clang-format off
+#include "brave/grit/brave_generated_resources.h"
+
+#include "src/chrome/common/importer/firefox_importer_utils.cc"
+// clang-format on
