a697fb22aafeb1ecb50171e6ea6e0e267b09ab71
Artem Samoilenko <artem@brave.com>
2024-11-20 17:16:01 -0500

    [Android] Fixes importer linker error
    Error fixed:
    ld.lld: error: undefined symbol: GetProfilesINI()
    >>> referenced by firefox_importer_utils.cc:62 (../../../src/chrome/common/importer/firefox_importer_utils.cc:62)
    >>>               obj/chrome/common/importer/importer/firefox_importer_utils.o:(GetFirefoxDetails(std::__Cr::basic_string<char, std::__Cr::char_traits<char>, std::__Cr::allocator<char>> const&))

--- chromium_src/chrome/common/importer/firefox_importer_utils.cc
+++ chromium_src/chrome/common/importer/firefox_importer_utils.cc
@@ -3,9 +3,18 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+#include "base/files/file_path.h"
 // Clang format wants to place this header after the .cc file for some reason.
 // clang-format off
 #include "brave/grit/brave_generated_resources.h"
+#include "build/build_config.h"
+
+#if BUILDFLAG(IS_ANDROID)
+// We don't use firefox importer on Android, so just return an empty path to avoid linker error. 
+base::FilePath GetProfilesINI() {
+  return base::FilePath();
+}
+#endif // !BUILDFLAG(IS_ANDROID)
 
 #include "src/chrome/common/importer/firefox_importer_utils.cc"
 // clang-format on
