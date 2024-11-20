6c830925a5239e1b1e3428e6eb358d4996d6ce2d
mkarolin <max@brave.com>
2024-11-20 11:50:28 -0500

    [cr133] IsOnDeviceModelAdaptationEnabled was removed.
    Chormium change:
    
    https://source.chromium.org/chromium/chromium/src/+/c7c2131f0802a47d1ca20033fc4f68c33912fc9f
    
    commit c7c2131f0802a47d1ca20033fc4f68c33912fc9f
    Author: Steven Holte <holte@google.com>
    Date:   Tue Nov 5 21:07:37 2024 +0000
    
        Remove IsOnDeviceModelAdaptationEnabled.
    
        * Removes support for kModelAdaptationCompose = Disabled
        * Removes support for kOnDeviceModelTestFeature with
          "enable_adaptation" = false.
        * Eliminates IsOnDeviceModelAdaptationEnabled as redundant with
          IsOnDeviceModelEnabled.
        * Consolidates IsOnDeviceModelEnabled and GetOptimizationTargetFor...,
          since we don't need the target if it's not enabled.
    
        Bug: 376754799

--- chromium_src/components/optimization_guide/core/model_execution/model_execution_features.cc
+++ chromium_src/components/optimization_guide/core/model_execution/model_execution_features.cc
@@ -3,16 +3,17 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#define IsOnDeviceModelAdaptationEnabled \
-  IsOnDeviceModelAdaptationEnabled_ChromiumImpl
+#define GetOptimizationTargetForCapability \
+  GetOptimizationTargetForCapability_ChromiumImpl
 
 #include "src/components/optimization_guide/core/model_execution/model_execution_features.cc"
-#undef IsOnDeviceModelAdaptationEnabled
+#undef GetOptimizationTargetForCapability
 
 namespace optimization_guide::features::internal {
 
-bool IsOnDeviceModelAdaptationEnabled(ModelBasedCapabilityKey feature) {
-  return false;
+std::optional<proto::OptimizationTarget> GetOptimizationTargetForCapability(
+    ModelBasedCapabilityKey feature_key) {
+  return std::nullopt;
 }
 
 }  // namespace optimization_guide::features::internal
