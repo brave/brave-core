949ce52bc4811d53b307805f3aa7e5845fc620ed
AlexeyBarabash <AlexeyBarabash@users.noreply.github.com>
2024-11-20 10:48:03 +0200

    [Android] Fix warning from pack symbols of android_clang_* dir (#26587)
    * [Android] Fix warning from pack symbols of android_clang_* dir
    
    Fixes brave/brave-browser/issues/42317

--- build/android/pack_symbols.sh
+++ build/android/pack_symbols.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
+# Copyright (c) 2024 The Brave Authors. All rights reserved.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at https://mozilla.org/MPL/2.0/.
+
 set -euo pipefail
 
-mkdir -p dist
-tar -czvf $1 --ignore-failed-read *.so apks android_clang_*/*.so android_clang_*/lib.unstripped lib.unstripped android_chrome_versions.txt
+mkdir -p "${1:?}"
+
+shopt -s nullglob
+
+tar -czvf "${2:?}" ./*.so apks android_clang_*/*.so android_clang_*/lib.unstripped lib.unstripped android_chrome_versions.txt
