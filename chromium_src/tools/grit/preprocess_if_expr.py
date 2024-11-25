cfe9fa2726303d5f116faaeb95270e2c5dc2ba92
Jay Harris <jay.harris@outlook.co.nz>
2024-11-26 11:06:14 +1300

    [Web UI]: Improve overriding approach to replace args

--- chromium_src/tools/grit/preprocess_if_expr.py
+++ chromium_src/tools/grit/preprocess_if_expr.py
@@ -3,25 +3,39 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at https://mozilla.org/MPL/2.0/.
 
+import argparse
+import glob
 import override_utils
 import os
 import shutil
-import glob
 
 from brave_chromium_utils import get_chromium_src_override
 
+def get_overridden_file_name(file_name):
+    """Gets the name of an upstream file which is being overridden (but still
+       referenced)
+       for example `foo.ts` ==> `foo-chromium.ts`
+                   `bar.css` ==> `bar-chromium.css`
+    """ 
+    name_bits = os.path.splitext(file_name)
+    return "".join([name_bits[0], "-chromium", name_bits[1]])
 
-def purge_overrides(out_folder):
+
+def purge_overrides(out_folder, in_files):
     """Deletes all overridden upstream files from the preprocess directory - we
        need this so removing an override doesn't break the build."""
-    for f in glob.glob(os.path.join(out_folder, '**/*-chromium*')):
-        if os.path.exists(f):
-            os.remove(f)
+    for file in in_files:
+        overridden_name = get_overridden_file_name(file)
+        overridden_path = os.path.join(out_folder, overridden_name)
+        if os.path.exists(overridden_path):
+            os.remove(overridden_path)
 
 
 def maybe_keep_upstream_version(override_in_folder, out_folder, override_file):
+    """Decides whether we should keep the upstream version of a file by looking
+       at the override and see if it references the upstream version"""
     name_bits = os.path.splitext(override_file)
-    overridden_name = "".join([name_bits[0], "-chromium", name_bits[1]])
+    overridden_name = get_overridden_file_name(override_file)
 
     # The path to the brave-core override file
     override_in_path = os.path.join(override_in_folder, override_file)
@@ -47,28 +61,45 @@ def get_brave_overrides(in_folder, in_files):
         if os.path.exists(in_file):
             override_files.append(file)
 
-    return (get_chromium_src_override(in_folder), override_files)
+    return override_files
 
+# Used to indicate that there are no overrides, so we don't need to reprocess
+class NoOverridesException(Exception): pass
 
 @override_utils.override_function(globals())
 def main(original_function, argv):
-    frame, result = override_utils.call_function_get_frame(
-        original_function, argv)
+    # Run the preprocessor as normal, to preprocess the upstream files.
+    original_function(argv)
 
-    globals().update(frame.f_locals)
-    purge_overrides(out_folder)
+    # When we rerun the processor, we detect files which should be overridden.
+    @override_utils.override_method(argparse.ArgumentParser)
+    def parse_args(self, original_method, argv):
+        args = original_method(self, argv)
 
-    for input_file in args.in_files:
-        in_path = os.path.join(in_folder, input_file)
-        process_file(in_path, input_file)
+        cwd = os.getcwd()
+        out_folder = os.path.normpath(os.path.join(cwd, args.out_folder))
+        in_folder = os.path.normpath(os.path.join(cwd, args.in_folder))
+        override_root_folder = get_chromium_src_override(in_folder)
 
-    (override_root_folder,
-     overrides) = get_brave_overrides(in_folder, args.in_files)
-    if len(overrides) != 0:
+        purge_overrides(out_folder, args.in_files)
+        overrides = get_brave_overrides(in_folder, args.in_files)
+
+        if len(overrides) == 0:
+            raise NoOverridesException
+
         for override_file in overrides:
             maybe_keep_upstream_version(override_root_folder, out_folder,
                                         override_file)
 
-            in_path = os.path.join(override_root_folder, override_file)
-            process_file(in_path, override_file)
-    return result
+        args.in_folder = get_chromium_src_override(args.in_folder)
+        args.in_files = overrides
+        return args
+
+    try:
+        # Try and run the function again - if we have no overrides this will
+        # raise an exception so we don't reprocess the files.
+        original_function(argv)
+    except NoOverridesException:
+        # we throw an exception when there are no overrides, so we don't reparse
+        pass
+
--- script/override_utils.py
+++ script/override_utils.py
@@ -6,7 +6,6 @@
 import contextlib
 import inspect
 import types
-import sys
 
 from typing import Any
 
@@ -127,31 +126,3 @@ def override_scope_variable(scope,
             setattr(scope, name, original_value)
         else:
             delattr(scope, name)
-
-
-def call_function_get_frame(func, *args, **kwargs):
-    """
-    Calls the function *func* with the specified arguments and keyword
-    arguments and snatches its local frame before it actually executes.
-    Taken from: https://stackoverflow.com/a/52358426
-
-    This is useful if you need to override a function but use some of its
-    internal state.
-    """
-
-    frame = None
-    trace = sys.gettrace()
-
-    def snatch_locals(_frame, name, _):
-        nonlocal frame
-        if frame is None and name == 'call':
-            frame = _frame
-            sys.settrace(trace)
-        return trace
-
-    sys.settrace(snatch_locals)
-    try:
-        result = func(*args, **kwargs)
-    finally:
-        sys.settrace(trace)
-    return frame, result
