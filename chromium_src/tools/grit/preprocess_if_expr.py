65c4734150b88b2d56fdf005566b3a315917b7ce
Jay Harris <jay.harris@outlook.co.nz>
2024-11-26 11:17:52 +1300

    [Web UI]: Add manifest to preprocess output

--- chromium_src/tools/grit/preprocess_if_expr.py
+++ chromium_src/tools/grit/preprocess_if_expr.py
@@ -8,15 +8,17 @@ import glob
 import override_utils
 import os
 import shutil
+import json
 
 from brave_chromium_utils import get_chromium_src_override
+
 
 def get_overridden_file_name(file_name):
     """Gets the name of an upstream file which is being overridden (but still
        referenced)
        for example `foo.ts` ==> `foo-chromium.ts`
                    `bar.css` ==> `bar-chromium.css`
-    """ 
+    """
     name_bits = os.path.splitext(file_name)
     return "".join([name_bits[0], "-chromium", name_bits[1]])
 
@@ -63,17 +65,24 @@ def get_brave_overrides(in_folder, in_files):
 
     return override_files
 
+
 # Used to indicate that there are no overrides, so we don't need to reprocess
-class NoOverridesException(Exception): pass
+class NoOverridesException(Exception):
+    pass
+
 
 @override_utils.override_function(globals())
 def main(original_function, argv):
     # Run the preprocessor as normal, to preprocess the upstream files.
     original_function(argv)
 
+    manifest_data = {}
+    manifest_path = None
+
     # When we rerun the processor, we detect files which should be overridden.
     @override_utils.override_method(argparse.ArgumentParser)
     def parse_args(self, original_method, argv):
+        nonlocal manifest_path, manifest_data
         args = original_method(self, argv)
 
         cwd = os.getcwd()
@@ -87,6 +96,12 @@ def main(original_function, argv):
         if len(overrides) == 0:
             raise NoOverridesException
 
+        if args.out_manifest:
+            manifest_path = os.path.join(cwd, args.out_manifest)
+            manifest_data = json.load(open(manifest_path))
+            manifest_data['files'].extend(
+                [get_overridden_file_name(f) for f in overrides])
+
         for override_file in overrides:
             maybe_keep_upstream_version(override_root_folder, out_folder,
                                         override_file)
@@ -99,7 +114,13 @@ def main(original_function, argv):
         # Try and run the function again - if we have no overrides this will
         # raise an exception so we don't reprocess the files.
         original_function(argv)
+
+        # We need to update the manifest to include all the -chromium files.
+        if manifest_path:
+            print("Manifest path", manifest_path)
+            with open(manifest_path, 'w', encoding='utf-8', newline='\n') as f:
+                json.dump(manifest_data, f)
+
     except NoOverridesException:
         # we throw an exception when there are no overrides, so we don't reparse
         pass
-
