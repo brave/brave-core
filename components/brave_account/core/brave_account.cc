fb86300d8c561cd2eba6f7e96b83a7b706d9bd4c
Szilard Szaloki <szilardszaloki@gmail.com>
2024-11-26 14:17:49 -0700

    `brave_account` component + builds `wasm-pack`

---
+++ components/brave_account/core/brave_account.cc
@@ -0,0 +1,14 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "brave/components/brave_account/core/brave_account.h"
+
+namespace brave_account {
+
+int Return42() {
+  return 42;
+}
+
+}  // namespace brave_account
---
+++ components/brave_account/core/brave_account.h
@@ -0,0 +1,17 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_BRAVE_ACCOUNT_CORE_BRAVE_ACCOUNT_H_
+#define BRAVE_COMPONENTS_BRAVE_ACCOUNT_CORE_BRAVE_ACCOUNT_H_
+
+#include "base/component_export.h"
+
+namespace brave_account {
+
+COMPONENT_EXPORT(BRAVE_ACCOUNT) int Return42();
+
+}  // namespace brave_account
+
+#endif  // BRAVE_COMPONENTS_BRAVE_ACCOUNT_CORE_BRAVE_ACCOUNT_H_
