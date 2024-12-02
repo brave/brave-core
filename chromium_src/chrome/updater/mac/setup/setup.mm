a9b559ce6ac78922a506da7ded153cb16448b255
Michael Herrmann <michael@herrmann.io>
2024-12-02 12:21:17 +0100

    Improve comment

--- chromium_src/chrome/updater/mac/setup/setup.mm
+++ chromium_src/chrome/updater/mac/setup/setup.mm
@@ -5,10 +5,14 @@
 
 #include "chrome/updater/util/mac_util.h"
 
-// We must prevent Omaha 4 from running at the same time as Sparkle. We can
-// control this in the browser, where we invoke either Omaha 4 or Sparkle. But
-// we have no control over when Omaha 4's background updates run. We therefore
-// disable them by not registering the wake job:
+// We are in the process of migrating from Sparkle to Omaha 4. The two operate
+// differently: Sparkle checks for updates in the browser, while Omaha 4
+// registers a wake job that runs periodically in the background, also when the
+// browser isn't running. We must prevent Omaha 4 and Sparkle from running at
+// the same time. We do this by not registering Omaha 4's wake job, and instead
+// invoking its on-demand API from inside the browser instead of Sparkle. Once
+// the migration to Sparkle is complete, we will re-enable the wake job by
+// releasing a new version of Omaha 4 itself.
 #define GetWakeTaskPlistPath(scope) \
   std::nullopt;                     \
   return true;
