100ae8d2977f528592c09f4382ec72e326fb4ea7
Michael Herrmann <michael@herrmann.io>
2024-12-02 13:58:42 +0100

    Lint

--- chromium_src/chrome/updater/test/integration_tests_mac.mm
+++ chromium_src/chrome/updater/test/integration_tests_mac.mm
@@ -11,8 +11,8 @@
 
 #include <optional>
 
-#include "base/files/file_path.h"
 #include "base/auto_reset.h"
+#include "base/files/file_path.h"
 #include "chrome/updater/updater_scope.h"
 #include "chrome/updater/util/mac_util.h"
 
