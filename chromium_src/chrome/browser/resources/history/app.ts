c77e3e1e82d9e3802e70671e60f0690beed8a7ba
Jay Harris <jay.harris@outlook.co.nz>
2024-12-05 20:08:21 +1300

    [Web UI]: Convert History to new overrides (#26879)

--- chromium_src/chrome/browser/resources/history/app.ts
+++ chromium_src/chrome/browser/resources/history/app.ts
@@ -3,8 +3,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-// @ts-nocheck TODO(petemill): Define types and remove ts-nocheck
-
 import {RegisterStyleOverride} from 'chrome://resources/brave/polymer_overriding.js'
 import {html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js'
 
@@ -13,8 +11,10 @@ RegisterStyleOverride(
   html`
     <style>
         #tabs-content, #tabs-content>* {
-            height: 100%; 
+            height: 100%;
         }
     </style>
   `
 )
+
+export * from './app-chromium.js'
