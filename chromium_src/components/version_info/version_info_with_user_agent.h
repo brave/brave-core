26c6d991bad31ec29b5dba698a2469e15a9d61bc
mkarolin <max@brave.com>
2024-12-05 16:00:35 -0500

    [cr133] GetProductNameAndVersionForUserAgent moved to own header.
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/94ba97dc616a73046b47a0ea053791e344706842
    
    commit 94ba97dc616a73046b47a0ea053791e344706842
    Author: Henrique Nakashima <hnakashima@chromium.org>
    Date:   Tue Mar 5 00:09:17 2024 +0000
    
        Create version_info_with_user_agent.cc|h in //components
    
        These methods in version_info are Chrome-specific should not be in
        //base. Move them back to components while keeping most of version_info
        in //base.
    
        Bug: 327638952

--- chromium_src/components/version_info/version_info_with_user_agent.h
+++ chromium_src/components/version_info/version_info_with_user_agent.h
@@ -1,19 +1,19 @@
-/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_VERSION_INFO_VERSION_INFO_H_
-#define BRAVE_CHROMIUM_SRC_COMPONENTS_VERSION_INFO_VERSION_INFO_H_
+#ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_VERSION_INFO_VERSION_INFO_WITH_USER_AGENT_H_
+#define BRAVE_CHROMIUM_SRC_COMPONENTS_VERSION_INFO_VERSION_INFO_WITH_USER_AGENT_H_
 
-#include <string_view>
+#include <string>
 
 #include "brave/components/version_info/version_info_values.h"
 
 #define GetProductNameAndVersionForUserAgent \
   GetProductNameAndVersionForUserAgent_Unused
 
-#include "src/components/version_info/version_info.h"  // IWYU pragma: export
+#include "src/components/version_info/version_info_with_user_agent.h"  // IWYU pragma: export
 #undef GetProductNameAndVersionForUserAgent
 
 namespace version_info {
@@ -24,4 +24,4 @@ constexpr std::string GetProductNameAndVersionForUserAgent() {
 
 }  // namespace version_info
 
-#endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_VERSION_INFO_VERSION_INFO_H_
+#endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_VERSION_INFO_VERSION_INFO_WITH_USER_AGENT_H_
