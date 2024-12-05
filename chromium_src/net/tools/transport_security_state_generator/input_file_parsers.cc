4b3bb5d167a60b7ba0bedf8d01d22e1ff1108a5a
brave-builds <brave-builds+devops@brave.com>
2024-12-05 14:41:33 +0000

    Upgrade from Chromium 132.0.6834.15 to Chromium 132.0.6834.33
    Update pins list timestamp
    
    Updated strings for Chromium 132.0.6834.33

--- chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
+++ chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
@@ -554,9 +554,9 @@ bool ParseCertificatesFile(std::string_view certs_input,
                            Pinsets* pinsets,
                            base::Time* timestamp) {
   constexpr std::string_view brave_certs = R"brave_certs(
-# Last updated: Tue Dec  3 19:46:08 UTC 2024
+# Last updated: Thu Dec  5 14:43:43 UTC 2024
 PinsListTimestamp
-1733255168
+1733409823
 
 # =====BEGIN BRAVE ROOTS ASC=====
 #From https://www.amazontrust.com/repository/
