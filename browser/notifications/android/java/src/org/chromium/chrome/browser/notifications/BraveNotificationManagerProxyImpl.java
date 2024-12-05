5c172465d590c05bc0499c29ddf372dc50ebad96
Artem Samoilenko <artem@brave.com>
2024-12-05 15:50:21 -0500

    [Android] Switch from TargetApi to RequiresApi
    That's new lint requirement from the upstream.

--- browser/notifications/android/java/src/org/chromium/chrome/browser/notifications/BraveNotificationManagerProxyImpl.java
+++ browser/notifications/android/java/src/org/chromium/chrome/browser/notifications/BraveNotificationManagerProxyImpl.java
@@ -1,15 +1,14 @@
 /**
- * Copyright (c) 2020 The Brave Authors. All rights reserved.
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright (c) 2020 The Brave Authors. All rights reserved. This Source Code Form is subject to
+ * the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with
+ * this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-
 package org.chromium.chrome.browser.notifications;
 
-import android.annotation.TargetApi;
 import android.app.NotificationChannel;
 import android.os.Build;
+
+import androidx.annotation.RequiresApi;
 
 import org.chromium.chrome.browser.notifications.channels.BraveChannelDefinitions;
 import org.chromium.components.browser_ui.notifications.NotificationManagerProxyImpl;
@@ -19,7 +18,7 @@ public class BraveNotificationManagerProxyImpl extends NotificationManagerProxyI
         super();
     }
 
-    @TargetApi(Build.VERSION_CODES.O)
+    @RequiresApi(Build.VERSION_CODES.O)
     @Override
     public void createNotificationChannel(NotificationChannel channel) {
         if (channel.getId().equals(BraveChannelDefinitions.ChannelId.BRAVE_ADS)) {
