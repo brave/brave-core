036b87d00ac28f3adaa2f41d184665ca10988b10
Jay Harris <jay.harris@outlook.co.nz>
2024-12-05 20:08:21 +1300

    [Web UI]: Convert History to new overrides (#26879)

--- chromium_src/chrome/browser/resources/history/history_item.ts
+++ chromium_src/chrome/browser/resources/history/history_item.ts
@@ -3,8 +3,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import {RegisterStyleOverride} from 'chrome://resources/brave/polymer_overriding.js'
-import {html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js'
+import { RegisterStyleOverride } from 'chrome://resources/brave/polymer_overriding.js'
+import { html } from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js'
 
 RegisterStyleOverride(
   'history-item',
@@ -14,6 +14,12 @@ RegisterStyleOverride(
         font-size: 13px;
         font-weight: 400;
       }
+
+      #menu-button {
+        transform: rotate(90deg) !important;
+      }
     </style>
   `
 )
+
+export * from './history_item-chromium.js'
