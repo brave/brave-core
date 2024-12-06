4695ca3a762e216a22c86d51c5bb476fce913b9e
mkarolin <max@brave.com>
2024-12-06 15:04:22 -0500

    [cr133] BookmarkContextMenuController requires non-empty selection.
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/1728842ca698772352670c2dc9c25e8a32daf6e8
    
    commit 1728842ca698772352670c2dc9c25e8a32daf6e8
    Author: Monica Basta <msalama@chromium.org>
    Date:   Thu Dec 5 13:03:26 2024 +0000
    
        [B4B-D] Enforce selection in context menu is not empty
    
        It is meaningless to creat context menu with empty nodes. This CL
        enforces selection to be not empty, nor null or have repeated nodes.
    
        Bug: 369304373

--- browser/ui/toolbar/brave_bookmark_context_menu_controller_unittest.cc
+++ browser/ui/toolbar/brave_bookmark_context_menu_controller_unittest.cc
@@ -68,8 +68,7 @@ TEST_F(BraveBookmarkContextMenuControllerTest,
   BookmarkContextMenuController controller(
       nullptr, nullptr, nullptr, profile_.get(),
       BookmarkLaunchLocation::kSidePanelContextMenu,
-      std::vector<
-          raw_ptr<const bookmarks::BookmarkNode, VectorExperimental>>());
+      {model_->bookmark_bar_node()});
 
   // Show apps command is not present by default.
   sync_preferences::TestingPrefServiceSyncable* prefs =
@@ -106,9 +105,7 @@ TEST_F(BraveBookmarkContextMenuControllerTest,
 TEST_F(BraveBookmarkContextMenuControllerTest, AddBraveBookmarksSubmenu) {
   BraveBookmarkContextMenuController controller(
       nullptr, nullptr, nullptr, profile_.get(),
-      BookmarkLaunchLocation::kSidePanelFolder,
-      std::vector<
-          raw_ptr<const bookmarks::BookmarkNode, VectorExperimental>>());
+      BookmarkLaunchLocation::kSidePanelFolder, {model_->bookmark_bar_node()});
   EXPECT_FALSE(controller.menu_model()
                    ->GetIndexOfCommandId(IDC_BOOKMARK_BAR_ALWAYS_SHOW)
                    .has_value());
