9f805d19ee6a25c01623fd14977a7c2b0b56d81a
Jay Harris <jay.harris@outlook.co.nz>
2024-12-06 13:29:37 +1300

    [Web UI]: Convert extensions to new overrides (#26881)

--- browser/resources/extensions/brave_overrides/index.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2020 The Brave Authors. All rights reserved.
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this file,
-// you can obtain one at https://mozilla.org/MPL/2.0/.
-
-import './extensions_detail_view.js'
-import './extensions_toolbar.js'
-import './side_bar.js'
