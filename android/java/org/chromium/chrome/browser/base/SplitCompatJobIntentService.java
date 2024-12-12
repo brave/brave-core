e613859d912eb5b4f7e698532f71a97aba8ddc5b
Artem Samoilenko <artem@brave.com>
2024-12-12 09:34:29 -0500

    [Android] Deleting now unused BundleUtils.newInstance method
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/27efce8f930f6db5030890a2ba0d110af6f08d5a
    
    Deleting now unused BundleUtils.newInstance method
    
    Bug: 378723187

--- android/java/org/chromium/chrome/browser/base/SplitCompatJobIntentService.java
+++ android/java/org/chromium/chrome/browser/base/SplitCompatJobIntentService.java
@@ -33,14 +33,11 @@ public class SplitCompatJobIntentService extends JobIntentService {
     @Override
     protected void attachBaseContext(Context context) {
         // Make sure specified split is installed, otherwise fall back to chrome split.
+        String splitName = SplitCompatApplication.CHROME_SPLIT_NAME;
         if (mSplitName != null && BundleUtils.isIsolatedSplitInstalled(mSplitName)) {
-            context = BundleUtils.createIsolatedSplitContext(mSplitName);
-        } else {
-            context =
-                    BundleUtils.createIsolatedSplitContext(
-                            SplitCompatApplication.CHROME_SPLIT_NAME);
+            splitName = mSplitName;
         }
-        mImpl = (Impl) BundleUtils.newInstance(context, mServiceClassName);
+        mImpl = (Impl) BundleUtils.newInstance(mServiceClassName, splitName);
         mImpl.setService(this);
         super.attachBaseContext(context);
     }
