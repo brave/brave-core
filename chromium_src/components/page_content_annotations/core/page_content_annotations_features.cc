05fa00ac69e647167510d4d419fbb1dab499345d
Claudio DeSouza <cdesouza@brave.com>
2024-12-16 10:35:54 +0000

    [cr133] `kPageVisibilityBatchAnnotations` deleted
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/b976995ec8850c69e81f7ffe454412d09eff3523
    
    commit b976995ec8850c69e81f7ffe454412d09eff3523
    Author: Nathan Memmott <memmott@chromium.org>
    Date:   Thu Dec 12 10:06:36 2024 -0800
    
        Code Health: Clean up stale PageVisibilityBatchAnnotations
    
        Fixed: 356236726

--- chromium_src/components/page_content_annotations/core/page_content_annotations_features.cc
+++ chromium_src/components/page_content_annotations/core/page_content_annotations_features.cc
@@ -3,17 +3,18 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "src/components/page_content_annotations/core/page_content_annotations_features.cc"
+#include "components/page_content_annotations/core/page_content_annotations_features.h"
 
-#include "base/feature_override.h"
+#define ShouldEnablePageContentAnnotations \
+  ShouldEnablePageContentAnnotations_ChromiumImpl
+
+#include "src/components/page_content_annotations/core/page_content_annotations_features.cc"
+#undef ShouldEnablePageContentAnnotations
 
 namespace page_content_annotations::features {
 
-OVERRIDE_FEATURE_DEFAULT_STATES({{
-    {kPageContentAnnotations, base::FEATURE_DISABLED_BY_DEFAULT},
-    {kPageVisibilityBatchAnnotations, base::FEATURE_DISABLED_BY_DEFAULT},
-    {kPageVisibilityPageContentAnnotations, base::FEATURE_DISABLED_BY_DEFAULT},
-    {kRemotePageMetadata, base::FEATURE_DISABLED_BY_DEFAULT},
-}});
+bool ShouldEnablePageContentAnnotations() {
+  return false;
+}
 
 }  // namespace page_content_annotations::features
