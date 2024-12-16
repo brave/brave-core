04da65627954297e7d52d7979e3eb26b751e8854
AlexeyBarabash <AlexeyBarabash@users.noreply.github.com>
2024-12-16 15:43:04 +0200

    Replaced lint-suppressions.xml.patch with python chromium_src override (#27020)
    * Replaced lint-suppressions.xml.patch with python chromium_src override

--- chromium_src/build/android/gyp/lint.py
+++ chromium_src/build/android/gyp/lint.py
@@ -3,8 +3,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at https://mozilla.org/MPL/2.0/.
 
+import brave_chromium_utils
 import override_utils
-
 
 @override_utils.override_function(build_utils)
 # pylint: disable=unused-argument
@@ -15,3 +15,20 @@ def JavaCmd(original_function, xmx='1G'):  #NOSONAR
     # for Android incremental builds
 
     return original_function('4G')
+
+
+@override_utils.override_function(globals())
+def _GenerateConfigXmlTree(original_function, orig_config_path,
+                           backported_methods):
+    # Expand upstream's lint-suppressions.xml with python chromium_src override
+    original_root_node = original_function(orig_config_path,
+                                           backported_methods)
+
+    if orig_config_path:
+        brave_config_path = brave_chromium_utils.get_chromium_src_override(
+            orig_config_path)
+        brave_root_node = ElementTree.parse(brave_config_path).getroot()
+        for item in brave_root_node.findall('issue'):
+            original_root_node.append(item)
+
+    return original_root_node
