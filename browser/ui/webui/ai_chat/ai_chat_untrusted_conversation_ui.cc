c6ed69e050a4e5388f3559a5ed1765e762796794
yan <yan@brave.com>
2024-12-17 15:36:11 -0800

    fix presubmit

--- browser/ui/webui/ai_chat/ai_chat_untrusted_conversation_ui.cc
+++ browser/ui/webui/ai_chat/ai_chat_untrusted_conversation_ui.cc
@@ -37,7 +37,8 @@
 
 namespace {
 constexpr char kURLLearnMoreBraveSearchLeo[] =
-    "https://support.brave.com/hc/en-us/articles/27586048343309-How-does-Leo-get-current-information";
+    "https://support.brave.com/hc/en-us/articles/"
+    "27586048343309-How-does-Leo-get-current-information";
 
 // Implments the interface to calls from the UI to the browser
 class UIHandler : public ai_chat::mojom::UntrustedUIHandler {
