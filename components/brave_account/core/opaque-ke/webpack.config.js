2f4e7dfbb4ef91285b5618e1da93d8660843c133
Szilard Szaloki <szilardszaloki@gmail.com>
2024-12-20 22:05:37 -0700

    `webpack` WASM loader plugin

--- components/brave_account/core/opaque-ke/webpack.config.js
+++ components/brave_account/core/opaque-ke/webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+const { XMLHttpRequestWasmLoaderPlugin } = require('./xml_http_request_wasm_loader_plugin.js');
 
 module.exports = (env) => {
   // WasmPackPlugin expects wasm-pack to be in $PATH,
-  // so prepend ours to process.env.PATH (so that it's found first)
+  // so prepend ours to process.env.PATH (so that it's found first).
   process.env.PATH = env.wasm_pack_path + path.delimiter + process.env.PATH
 
   return {
@@ -23,7 +24,9 @@ module.exports = (env) => {
       module: true,
       path: path.resolve(env.output_path, 'dist'),
       publicPath: 'chrome://resources/brave/opaque-ke/bundle/',
-      webassemblyModuleFilename: 'opaque_ke.module.wasm'
+      webassemblyModuleFilename: 'opaque_ke.module.wasm',
+      enabledWasmLoadingTypes: [ "xml-http-request" ],
+      wasmLoading: "xml-http-request",
       // trustedTypes: true,
     },
     plugins: [
@@ -33,6 +36,7 @@ module.exports = (env) => {
         outDir: path.resolve(env.output_path, 'pkg'),
         extraArgs: `--target-dir ${path.resolve(env.output_path, 'target')}`
       }),
+      new XMLHttpRequestWasmLoaderPlugin(),
     ],
     devtool: 'source-map',  // suppressing generating eval() calls
     mode: 'development',
