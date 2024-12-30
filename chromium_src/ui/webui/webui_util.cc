a7fa574972424817ed8ef8eceab5c334b6f2af55
mkarolin <max@brave.com>
2024-12-30 18:22:00 -0500

    [cr133] chrome/browser/ui/webui/webui_util.cc moved.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/268fce2af040718df7aa7d7fb1da6b93f71f278c
    
    commit 268fce2af040718df7aa7d7fb1da6b93f71f278c
    Author: Teresa Mao <temao@chromium.org>
    Date:   Wed Dec 18 01:20:40 2024 -0800
    
        WebUI: Move chrome/browser/ui/webui/webui_util.* to ui/webui.
    
        - Introduce //ui/webui/webui_util.* and move all functions to the new
          location. Note that the existing
          chrome/browser/ui/webui/webui_util.* will be removed once all the uses
          have been migrated to the new files.
        - Delegate the implementation of chrome/browser/ui/webui/webui_util.h
          to ui/webui/webui_util.h.
        - Update CommerceInternalsUIConfig and HistoryClustersInternalsUIConfig
          to use SetupWebUIDataSource() from ui/webui/webui_util.h and eliminate
          the wrapper callback in these two WebUI configs.
        - Replace outdated IS_CHROMEOS_LACROS with IS_CHROMEOS.
    
        The motivation for this migration is to allow WebUIs residing outside of
        chrome/ to leverage functionalities in webui_util.cc.
    
        Bug: 40745251

--- chromium_src/ui/webui/webui_util.cc
+++ chromium_src/ui/webui/webui_util.cc
@@ -1,7 +1,7 @@
 /* Copyright (c) 2020 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "ui/webui/webui_util.h"
 
@@ -10,7 +10,7 @@
 #include "content/public/common/url_constants.h"
 
 #define SetupWebUIDataSource SetupWebUIDataSource_ChromiumImpl
-#include "src/chrome/browser/ui/webui/webui_util.cc"
+#include "src/ui/webui/webui_util.cc"
 #undef SetupWebUIDataSource
 
 namespace webui {
