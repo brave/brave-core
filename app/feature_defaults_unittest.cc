db4106a3a665fc5d017f669a122c8e5b85ca5a15
Claudio DeSouza <cdesouza@brave.com>
2024-12-30 19:35:25 +0000

    [cr133] `kJourneysNamedNewTabGroups` deleted
    We disable Journeys.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/be14af36864e654cbaa3a8ec8c6c23a01bd8d112
    
    commit be14af36864e654cbaa3a8ec8c6c23a01bd8d112
    Author: Nan Lin <linnan@chromium.org>
    Date:   Fri Dec 20 13:16:13 2024 -0800
    
        [Code Health] Remove stale feature JourneysNamedNewTabGroups
    
        Bug: 356236250

--- app/feature_defaults_unittest.cc
+++ app/feature_defaults_unittest.cc
@@ -174,7 +174,6 @@ TEST(FeatureDefaultsTest, DisabledFeatures) {
       &history_clusters::internal::kHistoryClustersNavigationContextClustering,
       &history_clusters::internal::kJourneys,
       &history_clusters::internal::kJourneysImages,
-      &history_clusters::internal::kJourneysNamedNewTabGroups,
       &history_clusters::internal::kOmniboxAction,
       &history_clusters::internal::kOmniboxHistoryClusterProvider,
 #if BUILDFLAG(ENABLE_MIRROR)
