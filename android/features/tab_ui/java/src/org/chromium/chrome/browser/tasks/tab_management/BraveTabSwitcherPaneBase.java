b9c17a3b25bf5fd6636a4a3f5b2d87859568daee
Artem Samoilenko <artem@brave.com>
2025-01-02 12:42:55 -0500

    [Android] Empty Tab List Animation
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/20b3b308ad1929de3048a600721e45faeadee89b
    
    [Tab Groups] Empty Tab List Animation
    
    - Visible only for phone users
    - New class for building animations
    - New animation factory for translations. Only really useful in niche situations.
    
    Bug: 362329064

--- android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/BraveTabSwitcherPaneBase.java
+++ android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/BraveTabSwitcherPaneBase.java
@@ -16,7 +16,6 @@ import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tasks.tab_management.TabListCoordinator.TabListMode;
 import org.chromium.chrome.browser.ui.edge_to_edge.EdgeToEdgeController;
 import org.chromium.chrome.browser.user_education.UserEducationHelper;
-import org.chromium.chrome.tab_ui.R;
 
 import java.util.function.DoubleConsumer;
 
@@ -35,16 +34,6 @@ public abstract class BraveTabSwitcherPaneBase extends TabSwitcherPaneBase {
                 onToolbarAlphaChange,
                 userEducationHelper,
                 edgeToEdgeSupplier);
-        // These checks are to avoid resource not used warning. We use own own UI for the empty
-        // state.
-        assert R.drawable.phone_tab_switcher_empty_state_illustration_cloud_left > 0
-                : "Something has changed in the upstream!";
-        assert R.drawable.phone_tab_switcher_empty_state_illustration_cloud_right > 0
-                : "Something has changed in the upstream!";
-        assert R.drawable.phone_tab_switcher_empty_state_illustration_bottom_window > 0
-                : "Something has changed in the upstream!";
-        assert R.drawable.phone_tab_switcher_empty_state_illustration_top_window > 0
-                : "Something has changed in the upstream!";
     }
 
     @Override
