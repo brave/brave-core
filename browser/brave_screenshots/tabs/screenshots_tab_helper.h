47051e6b6b38529e25f00d6b58b63aca83638c72
Sampson <jjdsampson@gmail.com>
2025-01-02 16:56:41 -0600

    minor presubmit fixes

--- browser/brave_screenshots/tabs/screenshots_tab_helper.h
+++ browser/brave_screenshots/tabs/screenshots_tab_helper.h
@@ -1,10 +1,12 @@
-// Copyright (c) 2024 The Brave Authors. All rights reserved.
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
 #ifndef BRAVE_BROWSER_BRAVE_SCREENSHOTS_TABS_SCREENSHOTS_TAB_HELPER_H_
 #define BRAVE_BROWSER_BRAVE_SCREENSHOTS_TABS_SCREENSHOTS_TAB_HELPER_H_
+
+#include <memory>
 
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/image_editor/screenshot_flow.h"
