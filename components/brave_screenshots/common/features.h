6ad78d1e35fda681cc3a50d08104505fe3e53fe9
Sampson <jjdsampson@gmail.com>
2025-01-02 16:56:41 -0600

    minor presubmit fixes

--- components/brave_screenshots/common/features.h
+++ components/brave_screenshots/common/features.h
@@ -1,10 +1,10 @@
-// Copyright (c) 2024 The Brave Authors. All rights reserved.
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
-// you can obtain one at http://mozilla.org/MPL/2.0/.
+// You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#ifndef BRAVE_BROWSER_COMPONENTS_BRAVE_SCREENSHOTS_COMMON_FEATURES_H
-#define BRAVE_BROWSER_COMPONENTS_BRAVE_SCREENSHOTS_COMMON_FEATURES_H
+#ifndef BRAVE_COMPONENTS_BRAVE_SCREENSHOTS_COMMON_FEATURES_H_
+#define BRAVE_COMPONENTS_BRAVE_SCREENSHOTS_COMMON_FEATURES_H_
 
 #include "base/feature_list.h"
 
@@ -14,4 +14,4 @@ BASE_DECLARE_FEATURE(kBraveScreenshots);
 
 }  // namespace brave_screenshots::features
 
-#endif  // BRAVE_BROWSER_COMPONENTS_BRAVE_SCREENSHOTS_COMMON_FEATURES_H
+#endif  // BRAVE_COMPONENTS_BRAVE_SCREENSHOTS_COMMON_FEATURES_H_
