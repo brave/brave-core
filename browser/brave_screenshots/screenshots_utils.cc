a9e236418074f1d959149e179617229a6e8e487d
Sampson <jjdsampson@gmail.com>
2025-01-04 09:55:01 -0600

    removes unnecessary /ui/views/ subdirectory

--- browser/brave_screenshots/screenshots_utils.cc
+++ browser/brave_screenshots/screenshots_utils.cc
@@ -3,7 +3,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#include "brave/browser/ui/views/brave_screenshots/screenshots_utils.h"
+#include "brave/browser/brave_screenshots/screenshots_utils.h"
 
 #include "chrome/browser/image_editor/screenshot_flow.h"
 #include "chrome/browser/ui/browser.h"
