e23f79337e3d5b88fc7d1c4fa4658417cd570238
Sampson <jjdsampson@gmail.com>
2025-01-04 09:47:59 -0600

    removes unnecessary /tabs/ subdirectory

--- browser/brave_screenshots/screenshots_tab_helper.cc
+++ browser/brave_screenshots/screenshots_tab_helper.cc
@@ -3,7 +3,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#include "brave/browser/brave_screenshots/tabs/screenshots_tab_helper.h"
+#include "brave/browser/brave_screenshots/screenshots_tab_helper.h"
 
 #include <memory>
 #include <string>
