f0a08424781aa60eb8a687b4f8f0f37f81a6ccc3
Sampson <jjdsampson@gmail.com>
2025-01-06 17:57:21 -0600

    refactors devtools code into distinct helper

---
+++ browser/brave_screenshots/devtools_helper.h
@@ -0,0 +1,36 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_BROWSER_BRAVE_SCREENSHOTS_DEVTOOLS_HELPER_H_
+#define BRAVE_BROWSER_BRAVE_SCREENSHOTS_DEVTOOLS_HELPER_H_
+
+#include "base/memory/weak_ptr.h"
+#include "chrome/browser/image_editor/screenshot_flow.h"
+#include "content/public/browser/devtools_agent_host.h"
+
+namespace content {
+class WebContents;
+} // namespace content
+
+namespace brave_screenshots {
+
+class DevToolsHelper {
+ public:
+  DevToolsHelper(base::WeakPtr<content::WebContents> web_contents,
+                 image_editor::ScreenshotCaptureCallback callback);
+  ~DevToolsHelper();
+
+  bool Attach();
+  void SendCaptureFullscreenCommand();
+
+ private:
+  class DevToolsClientImpl;
+  scoped_refptr<content::DevToolsAgentHost> devtools_host_ = nullptr;
+  std::unique_ptr<content::DevToolsAgentHostClient> devtools_client_ = nullptr;
+};
+
+} // namespace brave_screenshots
+
+#endif  // BRAVE_BROWSER_BRAVE_SCREENSHOTS_DEVTOOLS_HELPER_H_
