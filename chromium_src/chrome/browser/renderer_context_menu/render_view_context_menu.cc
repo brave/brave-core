843c25ebe2a94c67825585ea8af14e509cc17d40
Sampson <jjdsampson@gmail.com>
2025-01-06 07:45:01 -0600

    more accurate conditional method name

--- chromium_src/chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chromium_src/chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -689,7 +689,7 @@ void BraveRenderViewContextMenu::BuildAIChatMenu() {
       IDS_AI_CHAT_CONTEXT_LEO_TOOLS, &ai_chat_submenu_model_);
 }
 
-void BraveRenderViewContextMenu::BuildBraveScreenshotsMenu() {
+void BraveRenderViewContextMenu::MaybeBuildBraveScreenshotsMenu() {
   if (!brave_screenshots::features::IsBraveScreenshotsEnabled()) {
     return;
   }
@@ -859,7 +859,7 @@ void BraveRenderViewContextMenu::InitMenu() {
         IDS_CONTENT_CONTEXT_SPLIT_VIEW);
   }
 
-  BuildBraveScreenshotsMenu();
+  MaybeBuildBraveScreenshotsMenu();
 }
 
 void BraveRenderViewContextMenu::NotifyMenuShown() {
