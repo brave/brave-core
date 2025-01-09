99a834d04f168e40c8e4ccb6becb3f1bf5de3bff
Emerick Rogul <erogul@brave.com>
2025-01-08 21:01:10 -0500

    Upgrade from Chromium 132.0.6834.57 to Chromium 132.0.6834.83 (1.74.x) (#27160)
    * Upgrade from Chromium 132.0.6834.57 to Chromium 132.0.6834.83 (requires manual update)
    
    * Update pins list timestamp
    
    * Updated strings for Chromium 132.0.6834.83
    
    * Disable upstream tests related to LensOverlayController
    
    ---------
    
    Co-authored-by: brave-builds <brave-builds+devops@brave.com>

--- chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
+++ chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
@@ -554,9 +554,9 @@ bool ParseCertificatesFile(std::string_view certs_input,
                            Pinsets* pinsets,
                            base::Time* timestamp) {
   constexpr std::string_view brave_certs = R"brave_certs(
-# Last updated: Thu Dec 19 19:36:10 UTC 2024
+# Last updated: Wed Jan  8 20:28:14 UTC 2025
 PinsListTimestamp
-1734636970
+1736368094
 
 # =====BEGIN BRAVE ROOTS ASC=====
 #From https://www.amazontrust.com/repository/
