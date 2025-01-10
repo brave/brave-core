9b5102d647586e937f7c5b4d00e1f7971809af2b
Szilard Szaloki <szilardszaloki@gmail.com>
2025-01-10 10:09:52 -0700

    Rust-to-WASM example — to be reverted

---
+++ ui/webui/resources/some_resource/index.js
@@ -0,0 +1,8 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import { return42 } from 'gen/brave/web-ui-some_resource/rust_crate_1/pkg'
+import { return24 } from 'gen/brave/web-ui-some_resource/rust_crate_2/pkg'
+export { return42, return24 }
