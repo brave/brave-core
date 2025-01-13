39e94d48e927183916d58ef55c94de50ae8e9a7c
deeppandya <deeppandya91@gmail.com>
2025-01-13 17:15:43 -0500

    Add notification permission when user tries to turn on the VPN

--- android/java/org/chromium/chrome/browser/vpn/utils/BraveVpnProfileUtils.java
+++ android/java/org/chromium/chrome/browser/vpn/utils/BraveVpnProfileUtils.java
@@ -1,19 +1,21 @@
-/**
- * Copyright (c) 2021 The Brave Authors. All rights reserved.
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/.
- */
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 package org.chromium.chrome.browser.vpn.utils;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
 import android.os.Build;
 
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import org.chromium.chrome.browser.vpn.timer.TimerUtils;
@@ -23,6 +25,8 @@ import org.chromium.chrome.browser.vpn.wireguard.WireguardUtils;
 public class BraveVpnProfileUtils {
     private static volatile BraveVpnProfileUtils sBraveVpnProfileUtils;
     private static Object sMutex = new Object();
+
+    private static final int NOTIFICATION_PERMISSION_CODE = 123;
 
     private BraveVpnProfileUtils() {}
 
@@ -63,6 +67,21 @@ public class BraveVpnProfileUtils {
     public void startVpn(Context context) {
         ContextCompat.startForegroundService(context, new Intent(context, WireguardService.class));
         TimerUtils.cancelScheduledVpnAction(context);
+        // For Android 13 (Tiramisu) and above, we need to explicitly request notification
+        // permission
+        if (context != null
+                && context instanceof Activity
+                && Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
+                && ContextCompat.checkSelfPermission(
+                                context, Manifest.permission.POST_NOTIFICATIONS)
+                        != PackageManager.PERMISSION_GRANTED) {
+            // Permission not granted yet, request it from the user via system dialog
+            // This is required for VPN service notifications to show stats
+            ActivityCompat.requestPermissions(
+                    (Activity) context,
+                    new String[] {Manifest.permission.POST_NOTIFICATIONS},
+                    NOTIFICATION_PERMISSION_CODE);
+        }
     }
 
     public void stopVpn(Context context) {
