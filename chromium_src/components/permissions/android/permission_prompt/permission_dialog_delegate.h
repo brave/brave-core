f256c72f6c20d9e2ddaf938926c273889f797271
AlexeyBarabash <alexey@brave.com>
2025-01-16 20:20:03 +0200

    [cr134] [Android] changes at PermissionPromptAndroid class - ShouldUseRequestingOriginFavicon is now virtual - introduced protected delegate() method
    Related Chromium change
    https://source.chromium.org/chromium/chromium/src/+/b03957663284904033846c9dc667fac4e052d0bc
    
    	[PEPC] Add  embedded prompt class for Android
    
    	As outlined in go/pepc-on-clank-dd, this CL introduces a new PEPC
    	prompt class extending from the PermissionPromptAndroid base.
    
    	Change-Id: I32e7a2a85e5febf3df34080b17a72054abc3a86b
    	Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6162756

---
+++ chromium_src/components/permissions/android/permission_prompt/permission_dialog_delegate.h
@@ -0,0 +1,34 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_PERMISSIONS_ANDROID_PERMISSION_PROMPT_PERMISSION_DIALOG_DELEGATE_H_
+#define BRAVE_CHROMIUM_SRC_COMPONENTS_PERMISSIONS_ANDROID_PERMISSION_PROMPT_PERMISSION_DIALOG_DELEGATE_H_
+
+// Forward includes to avoid redefine of Create term
+#include "base/task/cancelable_task_tracker.h"
+#include "content/public/browser/web_contents_observer.h"
+
+namespace permissions {
+class PermissionPromptAndroid_ChromiumImpl;
+}  // namespace permissions
+
+// Additional PermissionDialogDelegate::Create stub method to be in accordance
+// with the overridden method at
+//   void CreatePermissionDialogDelegate() {
+//     PermissionDialogDelegate::Create(web_contents_, this);
+//   }
+// which after PermissionPromptAndroid_ChromiumImpl override wants us to pass
+// PermissionPromptAndroid_ChromiumImpl* as the second arg.
+
+#define Create                                                     \
+  Create(content::WebContents* web_contents,                       \
+         PermissionPromptAndroid_ChromiumImpl* permission_prompt); \
+  static void Create
+
+#include "src/components/permissions/android/permission_prompt/permission_dialog_delegate.h"  // IWYU pragma: export
+
+#undef Create
+
+#endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_PERMISSIONS_ANDROID_PERMISSION_PROMPT_PERMISSION_DIALOG_DELEGATE_H_
