fa76fd85bc87fb868f772d731577648d36d7b78a
Max Karolinskiy <max@brave.com>
2025-01-16 13:31:36 -0500

    [cr133] Workaround for BraveTabStripModelTest flakiness on MacOS.

--- browser/ui/tabs/test/brave_tab_strip_model_browsertest.cc
+++ browser/ui/tabs/test/brave_tab_strip_model_browsertest.cc
@@ -3,6 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "base/run_loop.h"
 #include "brave/components/constants/pref_names.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/profiles/profile.h"
@@ -10,7 +11,40 @@
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "components/prefs/pref_service.h"
+#include "content/public/browser/web_contents_observer.h"
 #include "content/public/test/browser_test.h"
+
+namespace {
+
+class TabVisibilityWaiter : public content::WebContentsObserver {
+ public:
+  explicit TabVisibilityWaiter(content::WebContents* web_contents)
+      : content::WebContentsObserver(web_contents) {}
+
+  TabVisibilityWaiter(const TabVisibilityWaiter&) = delete;
+  TabVisibilityWaiter& operator=(const TabVisibilityWaiter&) = delete;
+
+  ~TabVisibilityWaiter() override = default;
+
+  void WaitForTabToBecomeVisible() {
+    if (web_contents()->GetVisibility() == content::Visibility::VISIBLE) {
+      return;
+    }
+    visibility_changed_run_loop_.Run();
+  }
+
+  // content::WebContentsObserver:
+  void OnVisibilityChanged(content::Visibility visibility) override {
+    if (visibility == content::Visibility::VISIBLE) {
+      visibility_changed_run_loop_.Quit();
+    }
+  }
+
+ private:
+  base::RunLoop visibility_changed_run_loop_;
+};
+
+}  // namespace
 
 using BraveTabStripModelTest = InProcessBrowserTest;
 
@@ -20,12 +54,18 @@ IN_PROC_BROWSER_TEST_F(BraveTabStripModelTest, MRUCyclingBasic) {
   // Open 3 tabs
   chrome::NewTab(browser());
   chrome::NewTab(browser());
-  EXPECT_EQ(browser()->tab_strip_model()->count(), 3);
-  EXPECT_EQ(browser()->tab_strip_model()->active_index(), 2);
+  EXPECT_EQ(tab_strip_model->count(), 3);
+  EXPECT_EQ(tab_strip_model->active_index(), 2);
 
+  // Before enabling MRU set up tab visibility waiter to make sure the activated
+  // tab's WebContents becomes visible which will update the last active time on
+  // which MRU relies to sort the tabs in MR order.
+  TabVisibilityWaiter tab_visibility_waiter(
+      tab_strip_model->GetWebContentsAt(0));
   // Normal next tab expected by default, 2 -> 0
   chrome::ExecuteCommand(browser(), IDC_SELECT_NEXT_TAB);
   EXPECT_EQ(tab_strip_model->active_index(), 0);
+  tab_visibility_waiter.WaitForTabToBecomeVisible();
 
   // Activate MRU cycling
   browser()->profile()->GetPrefs()->SetBoolean(kMRUCyclingEnabled, true);
@@ -51,26 +91,42 @@ IN_PROC_BROWSER_TEST_F(BraveTabStripModelTest, TabClosingWhileMRUCycling) {
 
   TabStripModel* tab_strip_model = browser()->tab_strip_model();
 
-  // Open 3 tabs
+  // Open 3 tabs. There's some kind of timing issue or flakiness on MacOS that
+  // causes the tabs visibility not to be updated and the last active timestamp
+  // doesn't get changed. So we force the visibility here.
   chrome::NewTab(browser());
+  tab_strip_model->GetWebContentsAt(1)->WasShown();
   chrome::NewTab(browser());
+  tab_strip_model->GetWebContentsAt(2)->WasShown();
   chrome::NewTab(browser());
-  EXPECT_EQ(browser()->tab_strip_model()->count(), 4);
-  EXPECT_EQ(browser()->tab_strip_model()->active_index(), 3);
+  tab_strip_model->GetWebContentsAt(3)->WasShown();
+  EXPECT_EQ(tab_strip_model->count(), 4);
+  EXPECT_EQ(tab_strip_model->active_index(), 3);
+  // MRU should be 3 > 2 > 1 > 0
 
   // MRU cycling, 3 -> 2
   chrome::ExecuteCommand(browser(), IDC_SELECT_NEXT_TAB);
   EXPECT_EQ(tab_strip_model->active_index(), 2);
+  // Again, force visibility to update its last active timestamp.
+  tab_strip_model->GetWebContentsAt(2)->WasShown();
+  // MRU should be 2 > 1 > 0 > 3
 
   // MRU cycling, 2 -> 1
   chrome::ExecuteCommand(browser(), IDC_SELECT_NEXT_TAB);
   EXPECT_EQ(tab_strip_model->active_index(), 1);
+  // MRU should be 1 > 0 > 3 > 2. No point in updating visibility since we are
+  // going to close the tab.
 
   // Close current tab (index 1).
   chrome::ExecuteCommand(browser(), IDC_CLOSE_TAB);
   EXPECT_EQ(tab_strip_model->active_index(), 1);
+  // Again, force visibility to update its last active timestamp.
+  tab_strip_model->GetWebContentsAt(1)->WasShown();
+  // MRU restarts and resorts tabs by last active timestamp.
+  // MRU should be 1 (former 2) > 2 (former 3) > 0
 
-  // New MRU cycling is started
+  // New MRU cycling is started, 1 -> 2
   chrome::ExecuteCommand(browser(), IDC_SELECT_NEXT_TAB);
   EXPECT_EQ(tab_strip_model->active_index(), 2);
+  // MRU should be: 2 > 0 > 1
 }
