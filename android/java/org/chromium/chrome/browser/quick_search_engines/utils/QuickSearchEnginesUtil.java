43bdeffe58654aed36979bd8c173e17375a5f46b
deeppandya <deeppandya91@gmail.com>
2025-01-22 16:06:00 -0500

    Add search engine function

--- android/java/org/chromium/chrome/browser/quick_search_engines/utils/QuickSearchEnginesUtil.java
+++ android/java/org/chromium/chrome/browser/quick_search_engines/utils/QuickSearchEnginesUtil.java
@@ -377,4 +377,8 @@ public class QuickSearchEnginesUtil {
                 .getDefaultSearchEngineTemplateUrl()
                 .getKeyword();
     }
+
+    public static void addSearchEngine(Profile profile) {
+        TemplateUrlServiceFactory.getForProfile(profile).addSearchEngine();
+    }
 }
