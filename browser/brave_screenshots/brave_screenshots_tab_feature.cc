e376178553b56c74dea8f39d6d2537aef95b1b67
Sampson <jjdsampson@gmail.com>
2025-01-22 14:42:31 -0600

    filenames and classnames should match

--- browser/brave_screenshots/brave_screenshots_tab_feature.cc
+++ browser/brave_screenshots/brave_screenshots_tab_feature.cc
@@ -3,7 +3,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#include "brave/browser/brave_screenshots/screenshots_tab_feature.h"
+#include "brave/browser/brave_screenshots/brave_screenshots_tab_feature.h"
 
 #include "base/notreached.h"
 #include "brave/browser/brave_screenshots/screenshots_utils.h"
--- chromium_src/chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chromium_src/chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -13,7 +13,7 @@
 #include "base/strings/string_util.h"
 #include "brave/browser/autocomplete/brave_autocomplete_scheme_classifier.h"
 #include "brave/browser/brave_screenshots/features.h"
-#include "brave/browser/brave_screenshots/screenshots_tab_feature.h"
+#include "brave/browser/brave_screenshots/brave_screenshots_tab_feature.h"
 #include "brave/browser/brave_shields/brave_shields_tab_helper.h"
 #include "brave/browser/cosmetic_filters/cosmetic_filters_tab_helper.h"
 #include "brave/browser/misc_metrics/profile_misc_metrics_service.h"
