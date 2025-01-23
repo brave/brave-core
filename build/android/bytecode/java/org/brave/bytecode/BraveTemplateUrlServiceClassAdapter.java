f15aab9ad9e6767d32ca4ae846cdefe4620ad3da
deeppandya <deeppandya91@gmail.com>
2025-01-23 17:57:28 -0500

    Add BraveTemplateUrlService
    Add bytecode changes
    
    Add new function AddSearchEngine in chromium_src

---
+++ build/android/bytecode/java/org/brave/bytecode/BraveTemplateUrlServiceClassAdapter.java
@@ -0,0 +1,23 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+package org.brave.bytecode;
+
+import org.objectweb.asm.ClassVisitor;
+
+public class BraveTemplateUrlServiceClassAdapter extends BraveClassVisitor {
+    static String sTemplateUrlServiceClassName =
+            "org/chromium/components/search_engines/TemplateUrlService";
+    static String sBraveTemplateUrlServiceClassName =
+            "org/chromium/components/search_engines/BraveTemplateUrlService";
+
+    public BraveTemplateUrlServiceClassAdapter(ClassVisitor visitor) {
+        super(visitor);
+
+        redirectConstructor(sTemplateUrlServiceClassName, sBraveTemplateUrlServiceClassName);
+        deleteField(sBraveTemplateUrlServiceClassName, "mNativeTemplateUrlServiceAndroid");
+        makeProtectedField(sTemplateUrlServiceClassName, "mNativeTemplateUrlServiceAndroid");
+    }
+}
