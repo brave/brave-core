a302b53b438f6a8c375f94f6713f04c54ed527ef
Max Karolinskiy <max@brave.com>
2025-01-23 11:15:57 -0500

    fixup! [cr133] Use `sql::Database::Tag`

--- chromium_src/sql/sql_name_variants.h
+++ chromium_src/sql/sql_name_variants.h
@@ -3,8 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#ifndef SQL_SQL_NAME_VARIANTS_H_
-#define SQL_SQL_NAME_VARIANTS_H_
+#ifndef BRAVE_CHROMIUM_SRC_SQL_SQL_NAME_VARIANTS_H_
+#define BRAVE_CHROMIUM_SRC_SQL_SQL_NAME_VARIANTS_H_
 
 #define IsValidDatabaseTag IsValidDatabaseTag_ChomiumImpl
 #include "../gen/sql/sql_name_variants.h"  // IWYU pragma: export
@@ -20,4 +20,4 @@ constexpr bool IsValidDatabaseTag(std::string_view s) {
 
 }  // namespace sql_metrics
 
-#endif  // SQL_SQL_NAME_VARIANTS_H_
+#endif  // BRAVE_CHROMIUM_SRC_SQL_SQL_NAME_VARIANTS_H_
