416916c847bf008dd33737f6b6351acce7de68e3
Simone Arpe <simon.arpe@gmail.com>
2025-01-24 17:30:36 +0100

    Implement `youtube_injector_host`

---
+++ components/youtube_script_injector/browser/content/youtube_injector_host.h
@@ -0,0 +1,34 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_COMPONENTS_YOUTUBE_SCRIPT_INJECTOR_BROWSER_CONTENT_YOUTUBE_INJECTOR_HOST_H_
+#define BRAVE_COMPONENTS_YOUTUBE_SCRIPT_INJECTOR_BROWSER_CONTENT_YOUTUBE_INJECTOR_HOST_H_
+
+#include <string>
+
+#include "brave/components/youtube_script_injector/common/youtube_injector.mojom.h"
+#include "url/gurl.h"
+
+namespace youtube_script_injector {
+
+class COMPONENT_EXPORT(YOUTUBE_SCRIPT_INJECTOR_BROWSER_CONTENT) YouTubeInjectorHost final
+    : public youtube_script_injector::mojom::YouTubeInjector {
+ public:
+ YouTubeInjectorHost(const YouTubeInjectorHost&) = delete;
+ YouTubeInjectorHost& operator=(const YouTubeInjectorHost&) = delete;
+
+ YouTubeInjectorHost(const GURL& url);
+  ~YouTubeInjectorHost() override;
+
+ // youtube_script_injector::mojom::YouTubeInjector:
+ void NativePipMode() override;
+
+ private:
+ const GURL url_;
+};
+
+}  // namespace youtube_script_injector
+
+#endif  // BRAVE_COMPONENTS_YOUTUBE_SCRIPT_INJECTOR_BROWSER_CONTENT_YOUTUBE_INJECTOR_HOST_H_
