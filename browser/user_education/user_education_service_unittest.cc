1c8be1525c378384be65e228d9a84dc1db9002ea
Max Karolinskiy <max@brave.com>
2025-01-24 17:43:41 -0500

    Adds a feature controlling Chromium's user education service.

--- browser/user_education/user_education_service_unittest.cc
+++ browser/user_education/user_education_service_unittest.cc
@@ -6,23 +6,31 @@
 #include "chrome/browser/user_education/user_education_service.h"
 
 #include "base/files/scoped_temp_dir.h"
+#include "base/test/scoped_feature_list.h"
+#include "brave/browser/user_education/features.h"
 #include "chrome/browser/user_education/user_education_service_factory.h"
 #include "chrome/test/base/testing_profile.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-class UserEducationServiceTest : public testing::Test {
+class UserEducationServiceTest : public testing::Test,
+                                 public ::testing::WithParamInterface<bool> {
  public:
   UserEducationServiceTest() = default;
   UserEducationServiceTest(const UserEducationServiceTest&) = delete;
   UserEducationServiceTest& operator=(const UserEducationServiceTest&) = delete;
 
   void SetUp() override {
+    if (!IsChromiumUserEductionEnabled()) {
+      features_.InitWithFeatures({}, {features::kChromiumUserEducation});
+    }
     ASSERT_TRUE(profile_dir_.CreateUniqueTempDir());
     TestingProfile::Builder profile_builder;
     profile_builder.SetPath(profile_dir_.GetPath());
     profile_ = profile_builder.Build();
   }
+
+  bool IsChromiumUserEductionEnabled() { return GetParam(); }
 
   Profile* profile() { return profile_.get(); }
 
@@ -30,9 +38,16 @@ class UserEducationServiceTest : public testing::Test {
   base::ScopedTempDir profile_dir_;
   content::BrowserTaskEnvironment task_environment_;
   std::unique_ptr<TestingProfile> profile_;
+  base::test::ScopedFeatureList features_;
 };
 
-TEST_F(UserEducationServiceTest, NoUserEducationService) {
+TEST_P(UserEducationServiceTest, UserEducationServiceFeature) {
   auto* service = UserEducationServiceFactory::GetForBrowserContext(profile());
-  EXPECT_EQ(nullptr, service);
+  if (IsChromiumUserEductionEnabled()) {
+    EXPECT_NE(nullptr, service);
+  } else {
+    EXPECT_EQ(nullptr, service);
+  }
 }
+
+INSTANTIATE_TEST_SUITE_P(, UserEducationServiceTest, ::testing::Bool());
