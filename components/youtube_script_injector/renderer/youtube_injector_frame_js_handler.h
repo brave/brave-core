19a8ed9e4c48774216adfee6de013c703975f08a
Simone Arpe <simon.arpe@gmail.com>
2025-01-28 12:59:02 +0100

    Fix license header

--- components/youtube_script_injector/renderer/youtube_injector_frame_js_handler.h
+++ components/youtube_script_injector/renderer/youtube_injector_frame_js_handler.h
@@ -1,7 +1,7 @@
-// Copyright (c) 2025 The Brave Authors. All rights reserved.
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this file,
-// you can obtain one at http://mozilla.org/MPL/2.0/.
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #ifndef BRAVE_COMPONENTS_YOUTUBE_SCRIPT_INJECTOR_RENDERER_YOUTUBE_INJECTOR_FRAME_JS_HANDLER_H_
 #define BRAVE_COMPONENTS_YOUTUBE_SCRIPT_INJECTOR_RENDERER_YOUTUBE_INJECTOR_FRAME_JS_HANDLER_H_
