3cce559e1e8a2ccedde95cd66d8aaa6b90605e0c
Kyle Hickinson <git@kylehickinson.com>
2025-01-31 17:30:52 -0500

    move CWVWebView subclass into ios/browser, fix gn/presubmit

---
+++ ios/web_view/public/cwv_web_view_extras.h
@@ -0,0 +1,77 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_IOS_WEB_VIEW_PUBLIC_CWV_WEB_VIEW_EXTRAS_H_
+#define BRAVE_IOS_WEB_VIEW_PUBLIC_CWV_WEB_VIEW_EXTRAS_H_
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
+
+#import "cwv_export.h"
+#import "cwv_web_view.h"
+
+typedef NSInteger CWVUserAgentType NS_TYPED_ENUM;
+OBJC_EXPORT const CWVUserAgentType CWVUserAgentTypeNone;
+OBJC_EXPORT const CWVUserAgentType CWVUserAgentTypeAutomatic;
+OBJC_EXPORT const CWVUserAgentType CWVUserAgentTypeMobile;
+OBJC_EXPORT const CWVUserAgentType CWVUserAgentTypeDesktop;
+
+NS_ASSUME_NONNULL_BEGIN
+
+CWV_EXPORT
+@interface CWVWebView (Extras)
+
+/// The user agent type currently used on the page (e.g. mobile/desktop)
+- (CWVUserAgentType)currentItemUserAgentType;
+
+/// Reloads the page with a specific user agent type
+- (void)reloadWithUserAgentType:(CWVUserAgentType)userAgentType;
+
+#pragma mark -
+
+/// Creates a PDF of the current page
+///
+/// Equivalent of -[WKWebView createPDFWithConfiguration:completionHandler:]
+- (void)createPDF:(void (^)(NSData* _Nullable))completionHandler;
+
+/// Creates an image from the current rendered page for a given CGRect
+///
+/// Equivalent of -[WKWebView takeSnapshotWithConfiguration:completionHandler:]
+- (void)takeSnapshotWithRect:(CGRect)rect
+           completionHandler:(void (^)(UIImage* _Nullable))completionHandler;
+
+/// Evaluates JavaScript on the page on a given WebKit content world
+///
+/// Equivalent of -[WKWebView
+/// evaluateJavaScript:contentWorld:completionHandler:]
+- (void)evaluateJavaScript:(NSString*)javaScriptString
+              contentWorld:(WKContentWorld*)contentWorld
+         completionHandler:
+             (nullable void (^)(id _Nullable result,
+                                NSError* _Nullable error))completion;
+
+#pragma mark -
+
+/// Determines if the data used to restore a CWVWebView is a WebState cache and
+/// is valid.
+///
+/// Used for migration purposes, can be removed in the future
++ (BOOL)isRestoreDataValid:(NSData*)data;
+
+/// Resets & reinjects all JavaScript features
+- (void)updateScripts;
+
+/// The underlying WKWebView if one has been created already.
+@property(readonly, nullable) WKWebView* underlyingWebView;
+
+/// The underlying WKWebViewConfiguration for this CWVWebView
+@property(readonly) WKWebViewConfiguration* WKConfiguration;
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#endif  // BRAVE_IOS_WEB_VIEW_PUBLIC_CWV_WEB_VIEW_EXTRAS_H_
