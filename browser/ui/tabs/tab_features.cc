010ae00410dbac32b01572235454041737acefe3
Sampson <jjdsampson@gmail.com>
2025-01-31 12:25:31 -0600

    moves to new TabFeatures impl

--- browser/ui/tabs/tab_features.cc
+++ browser/ui/tabs/tab_features.cc
@@ -12,6 +12,8 @@
 #include "base/no_destructor.h"
 #include "brave/browser/ai_chat/ai_chat_utils.h"
 #include "brave/browser/ai_chat/tab_data_web_contents_observer.h"
+#include "brave/browser/brave_screenshots/brave_screenshots_tab_feature.h"
+#include "brave/browser/brave_screenshots/features.h"
 #include "brave/browser/ui/side_panel/brave_side_panel_utils.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/tabs/public/tab_features.h"
@@ -45,6 +47,12 @@ TabFeatures::~TabFeatures() = default;
 
 void TabFeatures::Init(TabInterface& tab, Profile* profile) {
   TabFeatures_Chromium::Init(tab, profile);
+
+  // Brave Screenshots (via Context Menu and Commander)
+  if (brave_screenshots::features::IsBraveScreenshotsEnabled()) {
+    brave_screenshots_tab_feature_ =
+        std::make_unique<brave_screenshots::BraveScreenshotsTabFeature>();
+  }
 
   // Expect upstream's Init to create the registry.
   CHECK(side_panel_registry());
