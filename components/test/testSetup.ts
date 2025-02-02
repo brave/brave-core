7a2b906752b9b70021238730e338a4636894c0a6
Pete Miller <miller.pete@gmail.com>
2025-02-02 13:48:43 -0800

    update jest and ts-jest, use es modules

--- components/test/testSetup.ts
+++ components/test/testSetup.ts
@@ -3,5 +3,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
+import { jest } from '@jest/globals';
 import '@testing-library/jest-dom'
 import './testPolyfills'
+
+global.jest = jest;
