9017aa87650c129e9330864682039790730f96d4
Max Karolinskiy <max@brave.com>
2025-02-03 12:49:17 -0500

    Disables "control media" and "manage passwords" help bubbles.

---
+++ chromium_src/components/feature_engagement/public/feature_constants.cc
@@ -0,0 +1,18 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "src/components/feature_engagement/public/feature_constants.cc"
+
+#include "base/feature_override.h"
+
+namespace feature_engagement {
+
+OVERRIDE_FEATURE_DEFAULT_STATES({{
+    {kIPHGMCCastStartStopFeature, base::FEATURE_DISABLED_BY_DEFAULT},
+    {kIPHPasswordsManagementBubbleAfterSaveFeature,
+     base::FEATURE_DISABLED_BY_DEFAULT},
+}});
+
+}  // namespace feature_engagement
