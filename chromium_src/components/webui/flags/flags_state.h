00a2ee6447cbde88a703b4f5264a5f14f3e49408
Claudio DeSouza <cdesouza@brave.com>
2025-02-06 14:17:51 +0000

    [cr135] `flags_ui` moved under `//components/webui/`
    This affects several gn inclusion paths, header files, and shadow files
    to this component.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/422d5eb80c1ee199aaf98f07707e71449fb42d86
    
    commit 422d5eb80c1ee199aaf98f07707e71449fb42d86
    Author: rbpotter <rbpotter@chromium.org>
    Date:   Wed Feb 5 08:42:48 2025 -0800
    
        WebUI: Refactor components/flags_ui into components/webui/flags
    
        Followup from refactoring recommended by components/ OWNER in
        https://chromium-review.googlesource.com/c/chromium/src/+/6178599
    
        no other changes
    
        Bug: 379889249

--- chromium_src/components/webui/flags/flags_state.h
+++ chromium_src/components/webui/flags/flags_state.h
@@ -1,10 +1,10 @@
 /* Copyright (c) 2020 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_FLAGS_UI_FLAGS_STATE_H_
-#define BRAVE_CHROMIUM_SRC_COMPONENTS_FLAGS_UI_FLAGS_STATE_H_
+#ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_WEBUI_FLAGS_FLAGS_STATE_H_
+#define BRAVE_CHROMIUM_SRC_COMPONENTS_WEBUI_FLAGS_FLAGS_STATE_H_
 
 #define GetFlagFeatureEntries(...)                                             \
   GetFlagFeatureEntries(__VA_ARGS__);                                          \
@@ -12,8 +12,8 @@
       const FeatureEntry& entry, const std::set<std::string>& enabled_entries) \
       const
 
-#include "src/components/flags_ui/flags_state.h"  // IWYU pragma: export
+#include "src/components/webui/flags/flags_state.h"  // IWYU pragma: export
 
 #undef GetFlagFeatureEntries
 
-#endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_FLAGS_UI_FLAGS_STATE_H_
+#endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_WEBUI_FLAGS_FLAGS_STATE_H_
