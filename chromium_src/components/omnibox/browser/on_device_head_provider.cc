ae21be5c5a6feae42f79d9a2409ef5b4b0d4ea4e
boocmp <boocmp@yandex.ru>
2025-02-06 19:36:51 +0700

    Enable on-device suggestion model for all search providers.

--- chromium_src/components/omnibox/browser/on_device_head_provider.cc
+++ chromium_src/components/omnibox/browser/on_device_head_provider.cc
@@ -3,26 +3,24 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
+#include "src/components/omnibox/browser/on_device_head_provider.h"
+
 #include "components/search/search.h"
 #include "components/search_engines/template_url_service.h"
 
 namespace search {
-bool IsOffTheRecordClient(bool is_off_the_record) {
-  return is_off_the_record;
-}
-}  // namespace search
-
-namespace {
 bool BraveEnableOnHeadDeviceForAnyProvider(
     const TemplateURLService* template_url_service) {
   return true;
 }
-}  // namespace
+}  // namespace search
 
-#define DefaultSearchProviderIsGoogle(...)            \
-  IsOffTheRecordClient(client()->IsOffTheRecord()) && \
-      BraveEnableOnHeadDeviceForAnyProvider(__VA_ARGS__)
+#define SearchSuggestEnabled(...) SearchSuggestEnabled() && false
+
+#define DefaultSearchProviderIsGoogle(...) \
+  BraveEnableOnHeadDeviceForAnyProvider(__VA_ARGS__)
 
 #include "src/components/omnibox/browser/on_device_head_provider.cc"
 
 #undef DefaultSearchProviderIsGoogle
+#undef SearchSuggestEnabled
