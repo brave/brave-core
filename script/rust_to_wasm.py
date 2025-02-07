57282490fb81337e41e325b7e59583e86dd98bad
Szilard Szaloki <szilardszaloki@gmail.com>
2025-02-07 13:15:00 -0700

    `brave-core` changes

--- script/rust_to_wasm.py
+++ script/rust_to_wasm.py
@@ -5,22 +5,22 @@
 
 # Description:
 # rust_to_wasm.py serves as a proxy script that allows you to
-# specify wasm-bindgen's path for wasm-pack (via --wasm_bindgen_path),
+# specify wasm-bindgen-cli's path for wasm-pack (via --wasm_bindgen_cli_path),
 # while forwarding the rest of the command line arguments to wasm-pack.
 # See more on that in components/common/rust_to_wasm.gni.
-# This is required since wasm-bindgen is in root_build_dir
+# This is required since wasm-bindgen-cli is in root_build_dir
 # (normally, we would add it to the $PATH in build/commands/lib/config.js).
 
 # Example usage:
-# python3 rust_to_wasm.py --wasm_bindgen_path /path/to/wasm-bindgen /path/to/wasm-pack [wasm-pack command line arguments...]
+# python3 rust_to_wasm.py --wasm_bindgen_cli_path /path/to/wasm-bindgen-cli /path/to/wasm-pack [wasm-pack command line arguments...]
 
 import argparse
 import os
 import subprocess
 
 parser = argparse.ArgumentParser(description='Compile Rust packages to WASM')
-parser.add_argument('--wasm_bindgen_path', required=True)
+parser.add_argument('--wasm_bindgen_cli_path', required=True)
 args, wasm_pack_args = parser.parse_known_args()
 env = os.environ.copy()
-env['PATH'] = args.wasm_bindgen_path + os.pathsep + env['PATH']
+env['PATH'] = args.wasm_bindgen_cli_path + os.pathsep + env['PATH']
 subprocess.check_call(wasm_pack_args, env=env)
