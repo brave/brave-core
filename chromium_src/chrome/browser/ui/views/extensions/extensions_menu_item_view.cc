baedf0b0b7c056a495a5959c7c20e8d1ea9da317
Jay Harris <jay.harris@outlook.co.nz>
2025-02-12 06:49:38 +1300

    [Extensions]: Use kebab icon (#27608)

---
+++ chromium_src/chrome/browser/ui/views/extensions/extensions_menu_item_view.cc
@@ -0,0 +1,15 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "chrome/browser/ui/views/extensions/extensions_menu_item_view.h"
+
+#include "brave/components/vector_icons/vector_icons.h"
+#include "chrome/app/vector_icons/vector_icons.h"
+
+#define kBrowserToolsChromeRefreshIcon kLeoMoreVerticalIcon
+
+#include "src/chrome/browser/ui/views/extensions/extensions_menu_item_view.cc"
+
+#undef kBrowserToolsChromeRefreshIcon
