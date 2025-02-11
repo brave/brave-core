54845e2d0d863d609a9db87356bf994647c4ae61
Claudio DeSouza <cdesouza@brave.com>
2025-02-11 09:11:39 +0000

    [cr135] Lifting deleted `UsageClock` into Brave
    This class has been deleted from upstream for being unused, so it is
    being lifted directly into brave as it was still in use in our codebase.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/1bfee585c32eb3fa81838e7d64e82e7510da517b
    
    commit 1bfee585c32eb3fa81838e7d64e82e7510da517b
    Author: Joe Mason <joenotcharles@google.com>
    Date:   Fri Feb 7 08:40:39 2025 -0800
    
        Remove unused LifecycleUnit methods (2/N)
    
        Removes GetChromeUsageTimeWhenHidden.
    
        This is the only user of the UsageClock class so removes that as well.
    
        R=fdoray
    
        Bug: 383374205

---
+++ browser/misc_metrics/usage_clock_unittest.cc
@@ -0,0 +1,68 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "brave/browser/misc_metrics/usage_clock.h"
+
+#include "base/test/simple_test_clock.h"
+#include "base/test/simple_test_tick_clock.h"
+#include "base/test/task_environment.h"
+#include "chrome/browser/metrics/desktop_session_duration/desktop_session_duration_tracker.h"
+#include "chrome/browser/resource_coordinator/time.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace misc_metrics {
+
+TEST(ResourceCoordinatorUsageClock, UsageClock) {
+  // Required to use DesktopSessionDurationTracker.
+  base::test::TaskEnvironment task_environment;
+
+  {
+    base::SimpleTestClock clock;
+    base::SimpleTestTickClock tick_clock;
+    tick_clock.Advance(base::Minutes(42));
+    resource_coordinator::ScopedSetClocksForTesting
+        scoped_set_clocks_for_testing(&clock, &tick_clock);
+
+    metrics::DesktopSessionDurationTracker::Initialize();
+    auto* tracker = metrics::DesktopSessionDurationTracker::Get();
+    tracker->OnVisibilityChanged(true, base::TimeDelta());
+    tracker->OnUserEvent();
+    EXPECT_TRUE(tracker->in_session());
+
+    UsageClock usage_clock;
+    EXPECT_EQ(usage_clock.GetTotalUsageTime(), base::TimeDelta());
+    EXPECT_TRUE(tracker->in_session());
+    EXPECT_TRUE(usage_clock.IsInUse());
+
+    // Verify that time advances when Chrome is in use.
+    tick_clock.Advance(base::Minutes(1));
+    EXPECT_EQ(usage_clock.GetTotalUsageTime(), base::Minutes(1));
+    tick_clock.Advance(base::Minutes(1));
+    EXPECT_EQ(usage_clock.GetTotalUsageTime(), base::Minutes(2));
+
+    // Verify that time is updated when Chrome stops being used.
+    tick_clock.Advance(base::Minutes(1));
+    tracker->OnVisibilityChanged(false, base::TimeDelta());
+    EXPECT_FALSE(tracker->in_session());
+    EXPECT_FALSE(usage_clock.IsInUse());
+    EXPECT_EQ(usage_clock.GetTotalUsageTime(), base::Minutes(3));
+
+    // Verify that time stays still when Chrome is not in use.
+    tick_clock.Advance(base::Minutes(1));
+    EXPECT_EQ(usage_clock.GetTotalUsageTime(), base::Minutes(3));
+
+    // Verify that time advances again when Chrome is in use.
+    tracker->OnVisibilityChanged(true, base::TimeDelta());
+    EXPECT_TRUE(tracker->in_session());
+    EXPECT_TRUE(usage_clock.IsInUse());
+    tick_clock.Advance(base::Minutes(1));
+    EXPECT_EQ(usage_clock.GetTotalUsageTime(), base::Minutes(4));
+  }
+
+  // Must be after UsageClock destruction.
+  metrics::DesktopSessionDurationTracker::CleanupForTesting();
+}
+
+}  // namespace misc_metrics
