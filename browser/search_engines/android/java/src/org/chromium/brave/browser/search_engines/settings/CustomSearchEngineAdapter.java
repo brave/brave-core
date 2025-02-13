7260dc12587f4e4d83cbc689f3fff08ff32090af
deeppandya <deeppandya91@gmail.com>
2025-02-13 09:29:30 -0500

    Add custom preference for search engines list with recyclerview

---
+++ browser/search_engines/android/java/src/org/chromium/brave/browser/search_engines/settings/CustomSearchEngineAdapter.java
@@ -0,0 +1,67 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+package org.chromium.brave.browser.search_engines.settings;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.chromium.base.Log;
+import org.chromium.chrome.browser.search_engines.R;
+
+import java.util.List;
+
+public class CustomSearchEngineAdapter extends RecyclerView.Adapter<CustomSearchEngineViewHolder> {
+    private List<String> mSearchEngines;
+    private CustomSearchEnginesCallback mCustomSearchEnginesCallback;
+
+    public CustomSearchEngineAdapter(
+            List<String> searchEngines, CustomSearchEnginesCallback customSearchEnginesCallback) {
+        mSearchEngines = searchEngines;
+        mCustomSearchEnginesCallback = customSearchEnginesCallback;
+    }
+
+    @Override
+    public void onBindViewHolder(
+            @NonNull CustomSearchEngineViewHolder customSearchEngineViewHolder, int position) {
+        final String searchEngineKeyword = mSearchEngines.get(position);
+        Log.e("brave_search", "CustomSearchEngineViewHolder : " + searchEngineKeyword);
+        customSearchEngineViewHolder.mSearchEngineText.setText(searchEngineKeyword);
+        customSearchEngineViewHolder.mView.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mCustomSearchEnginesCallback.onSearchEngineClick(searchEngineKeyword);
+                    }
+                });
+        customSearchEngineViewHolder.mDeleteIcon.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mCustomSearchEnginesCallback.removeSearchEngine(searchEngineKeyword);
+                    }
+                });
+        mCustomSearchEnginesCallback.loadSearchEngineLogo(
+                customSearchEngineViewHolder.mSearchEngineLogo, searchEngineKeyword);
+    }
+
+    @NonNull
+    @Override
+    public CustomSearchEngineViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view =
+                LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.custom_search_engine_item, parent, false);
+        return new CustomSearchEngineViewHolder(view);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSearchEngines.size();
+    }
+}
