54c27f424306653c273967a0521c6e76c3e349c7
brave-builds <brave-builds+devops@brave.com>
2025-02-13 22:20:57 +0000

    Uplift of #27650 (squashed) to release

--- chromium_src/components/omnibox/browser/omnibox_edit_model.h
+++ chromium_src/components/omnibox/browser/omnibox_edit_model.h
@@ -11,11 +11,18 @@
 #define CanPasteAndGo                                       \
   CanPasteAndGo_Chromium(const std::u16string& text) const; \
   bool CanPasteAndGo
+
 #define PasteAndGo                                                \
   PasteAndGo_Chromium(const std::u16string& text,                 \
                       base::TimeTicks match_selection_timestamp); \
   void PasteAndGo
+
+#define GetSuperGIcon(...)                 \
+  GetSuperGIcon_Unused(__VA_ARGS__) const; \
+  ui::ImageModel GetSuperGIcon(__VA_ARGS__)
+
 #include "src/components/omnibox/browser/omnibox_edit_model.h"  // IWYU pragma: export
+#undef GetSuperGIcon
 #undef CanPasteAndGo
 #undef PasteAndGo
 
