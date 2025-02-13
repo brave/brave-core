9947392852b34a06ffe5281acc0ef926117a9893
Szilard Szaloki <szilardszaloki@gmail.com>
2025-02-12 17:25:34 -0700

    Removes `cargo_audit`, `wasm_bindgen`, `wasm_pack`

--- tools/crates/cargo_audit/v0_21/crate/src/prelude.rs
+++
@@ -1,8 +0,0 @@
-//! Application-local prelude: conveniently import types/functions/macros
-//! which are generally useful and should be available everywhere.
-
-/// Abscissa core prelude
-pub use abscissa_core::prelude::*;
-
-/// Application state
-pub use crate::application::APP;
