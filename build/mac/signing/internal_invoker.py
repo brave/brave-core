f53e55dd33ca6acfa6e3f98b8667ac5c4360d1f0
Michael Herrmann <michael@herrmann.io>
2025-02-14 10:43:16 +0100

    Fix a problem with --skip_signing

--- build/mac/signing/internal_invoker.py
+++ build/mac/signing/internal_invoker.py
@@ -3,7 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at https://mozilla.org/MPL/2.0/.
 
-from signing import standard_invoker, signing, commands
+from signing import standard_invoker, commands
 
 
 class Invoker(standard_invoker.Invoker):
@@ -16,28 +16,20 @@ class Invoker(standard_invoker.Invoker):
     def __init__(self, args, config):
         super().__init__(args, config)
         if args.skip_signing:
-            self._signer = StubSigner()
             stub_out_signing_in_upstream()
         # The config can use this to access the args:
         self.args = args
 
 
-class StubSigner:
-
-    def codesign(self, config, product, path):
-        # Do nothing.
-        pass
-
-
 def stub_out_signing_in_upstream():
-    # When we skip signing, then upstream's verify_part function fails at
-    # various points of the pipeline. Upstream doesn't give us a way to
-    # customize verify_part. This function therefore monkey-patches it.
-    signing.verify_part = lambda *args, **kwargs: None
-
     run_command_orig = commands.run_command
 
     def run_command(args, **kwargs):
+        if args[0] == 'codesign':
+            # Even non-signing commands such as `codesign --verify` or
+            # `codesign --display` fail when signing is skipped. So don't invoke
+            # codesign at all:
+            return
         if args[0] == 'productbuild':
             try:
                 sign_index = args.index('--sign')
@@ -46,6 +38,6 @@ def stub_out_signing_in_upstream():
             else:
                 for _ in range(2):
                     args.pop(sign_index)
-        return run_command_orig(args, **kwargs)
+        run_command_orig(args, **kwargs)
 
     commands.run_command = run_command
