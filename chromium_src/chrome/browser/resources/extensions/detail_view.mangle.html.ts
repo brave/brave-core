d2fb48d9fe237ebadffce9c3e26cbeb85bdeb98d
Jay Harris <jay.harris@outlook.co.nz>
2025-02-18 12:42:14 +1300

    Update lit_mangler to run via a script

--- chromium_src/chrome/browser/resources/extensions/detail_view.mangle.html.ts
+++ chromium_src/chrome/browser/resources/extensions/detail_view.mangle.html.ts
@@ -3,23 +3,14 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import { mangle, load, write, findTemplate } from 'lit_mangler'
+import { mangle } from 'lit_mangler'
 
-const upstream = load('../chrome/browser/resources/extensions/detail_view.html.ts')
-
-const incognitoTemplate = findTemplate(upstream, (template) => template.text.includes('id="allow-incognito"'))
-if (!incognitoTemplate) {
-    throw new Error('[Brave Extensions Overrides] Could not find incognitoSetting. Has the ID changed?')
-}
-
-mangle(incognitoTemplate, (element) => {
+mangle((element) => {
     const section = element.querySelector('.section-content')
     if (!section) {
         throw new Error('[Brave Extensions Overrides] Could not find .section-content. Has the ID changed?')
     }
     section.textContent = '$i18n{privateInfoWarning}'
-    section.append('<span ?hidden=${(this.data as any).incognitoAccess.isActive}>$i18n{spanningInfoWarning}</span>')
-    section.append('<span>$i18n{privateAndTorInfoWarning}</span>')
-})
-
-write('./detail_view.html.ts', upstream)
+    section.append('<span ?hidden=${!this.data.incognitoAccess.isActive}> $i18n{spanningInfoWarning}</span>')
+    section.append('<span> $i18n{privateAndTorInfoWarning}</span>')
+}, t => t.text.includes('id="allow-incognito"'))
