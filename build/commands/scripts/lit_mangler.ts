5fab2a95bb201c7980f79e8392adae6abf5f25e3
Jay Harris <jay.harris@outlook.co.nz>
2025-02-18 11:10:17 +1300

    Typescript AST mangling

--- build/commands/scripts/lit_mangler.ts
+++ build/commands/scripts/lit_mangler.ts
@@ -7,165 +7,116 @@
 // old Polymer overrides used to. It generates a new .html.ts file with our
 // modifications to upstream's template.
 
-import { JSDOM } from 'jsdom'
-
+import ts from 'typescript'
 import { readFileSync, writeFileSync } from 'fs'
+import path from 'path'
+import {JSDOM} from 'jsdom'
 
-// A specific instance of the html tagged template literal.
-interface Template {
-    tag?: string,
-    rawText: string,
+interface HTMLTemplateTags {
     id: number,
-    startIndex: number,
-    endIndex: number,
-
-    subtemplates: Template[]
-}
-
-// The result of parsing a .html.ts file.
-interface Result {
     text: string,
-    templates: Template[]
+    children: HTMLTemplateTags[]
 }
 
-// Gets the line and column of the given index in the text for printing errors.
-const getLineColumn = (text: string, index: number) => {
-    const lines = text.substring(0, index).split('\n')
-    return [
-        lines.length,
-        lines.at(-1)!.length
-    ]
-}
-
-// We need a unique ID for each template in the file, so we can put things back
-// together after we've mangled them.
 let nextId = 1
 
-// Note: This method is super niaive and doesn't attempt to handle nesting non-html
-// tagged template literals. For the .html.ts file in Chromium this seems like
-// its going to be good enough for most cases.
-function* readTags(filepath: string, text: string): Iterable<Template> {
-    let i = 0
-    const stack: Pick<Template, 'id' | 'startIndex' | 'tag' | 'subtemplates'>[] = []
+const loadRaw = (filepath: string) => {
+    const text = readFileSync(filepath, 'utf-8')
+    const tsFile = ts.createSourceFile(path.basename(filepath), text, ts.ScriptTarget.Latest)
 
-    const consumeUntilMatch = () => {
-        i = text.indexOf(text[i], i + 1)
-    }
-
-    for (; i < text.length; ++i) {
-        const isInTemplate = stack.length !== 0
-        const char = text.at(i)
-
-        // If we're not in a template and we did a string, read till the end
-        if (!isInTemplate && (char == `"` || char === `'`)) {
-            consumeUntilMatch()
-            continue
-        }
-
-        // We're only really interested in backticks
-        if (char !== '`') continue
-
-        // This backtick is a literal, so ignore it
-        if (text[i - 1] === '\\') continue
-
-        // This is the start of an HTML template literal
-        if (text.substring(i - 4, i) === 'html') {
-            stack.push({
-                tag: 'html',
-                id: nextId++,
-                startIndex: i,
-                subtemplates: []
-            })
-            continue
-        }
-
-        // This must be an end tag!
-        const templateStart = stack.pop()!
-        if (!templateStart) {
-            throw new Error(`Encountered close tag without open at ${filepath}:${getLineColumn(text, i).join(':')}`)
-        }
-
-        const template: Template = {
-            ...templateStart,
-            endIndex: i,
-            rawText: text.substring(templateStart.startIndex + 1, i)
-        }
-        if (stack.length) {
-            stack.at(-1)?.subtemplates.push(template)
-            continue;
-        }
-        yield template
-    }
+    return tsFile
 }
 
-const setTemplatePlaceholders = (text: string, templates: Template[]) => {
-    for (const template of templates) {
-        const original = template.rawText
-        template.rawText = setTemplatePlaceholders(template.rawText, template.subtemplates)
-        text = text.replace(original, `$$$$lit_mangler_${template.id}$$$$`)
+const getTemplateLiterals = (source: ts.SourceFile, node: ts.Node, templateTags: HTMLTemplateTags) => {
+    if (ts.isTaggedTemplateExpression(node) && node.tag.getText(source) === 'html') {
+        let text = node.template.getText(source)
+        text = text.substring(1, text.length - 1)
+        const tag: HTMLTemplateTags = {
+            children: [],
+            text,
+            id: nextId++
+        }
+
+        getTemplateLiterals(source, node.template, tag)
+
+        templateTags.children.push(tag)
+        return
     }
-    return text
+
+    node.forEachChild(n => {
+        getTemplateLiterals(source, n, templateTags)
+    })
 }
 
-const replacePlaceholders = (text: string, templates: Template[]) => {
-    for (const template of templates) {
-        template.rawText = replacePlaceholders(template.rawText, template.subtemplates)
-        text = text.replaceAll(`$$lit_mangler_${template.id}$$`, template.rawText)
-    }
-
-    return text
+const getLiteralsFromFile = (filepath: string) => {
+    const file = loadRaw(filepath)
+    const root: HTMLTemplateTags = { text: file.getText(), children: [], id: nextId++ }
+    getTemplateLiterals(file, file, root)
+    return root
 }
 
-export const loadRaw = (path: string): Result => {
-    const text = readFileSync(path, 'utf-8')
-        .replaceAll(/(\w+)(\s+)?=(\s+)?(\$\{.*?\})(\s|>)/gi, "$1='$4'$5")
-
-    const templates = [...readTags(path, text)]
-    const modifiedText = setTemplatePlaceholders(text, templates)
-
-    return {
-        text: modifiedText,
-        templates: templates
+const injectPlaceholders = (template: HTMLTemplateTags) => {
+    for (const child of template.children) {
+        const original = child.text
+        child.text = injectPlaceholders(child)
+        template.text = template.text.replace(original, `$$$$lit_mangler_${child.id}$$$$`)
     }
+
+    return template.text
 }
 
-export const write = (file: string, result: Result) => {
-    const text = replacePlaceholders(result.text, result.templates)
+const replacePlaceholders = (template: HTMLTemplateTags) => {
+    for (const child of template.children) {
+        child.text = replacePlaceholders(child)
+        template.text = template.text.replaceAll(`$$lit_mangler_${child.id}$$`, child.text)
+    }
+
+    return template.text
+}
+
+export const load = (file: string) => {
+    const input = getLiteralsFromFile(file)
+    injectPlaceholders(input)
+    return input
+}
+
+export const write = (file: string, result: HTMLTemplateTags) => {
+    const text = replacePlaceholders(result)
     writeFileSync(file, text)
 }
 
-export const findTemplate = (from: Template[] | Result, predicate: (template: Template) => boolean) => {
-    const templates = 'templates' in from ? from.templates : from
-
-    // Search this level first
-    for (const template of templates) {
-        if (predicate(template)) {
-            return template
-        }
-    }
-
-    // Then search the subtemplates
-    for (const template of templates) {
-        const result = findTemplate(template.subtemplates, predicate)
-        if (result) {
-            return result
-        }
-    }
-}
-
-export const mangle = (template: Template, mangler: (element: DocumentFragment) => void) => {
+export const mangle = (template: HTMLTemplateTags, mangler: (element: DocumentFragment) => void) => {
     const world = new JSDOM()
     const document = world.window.document
     const element = document.createElement('template')
-    element.innerHTML = template.rawText
+    element.innerHTML = template.text
+        // Note: we ensure that all attributes are quoted so the template can be
+        // parsed as HTML.
+        // This won't work for attributes with braces in them, but it'll
+        // drastically break the HTML output from upstrean, so it should be
+        // pretty obvious that something went wrong.
+        .replaceAll(/(\w+)(\s+)?=(\s+)?(\$\{.*?\})(\s|>)/gi, (...args) => {
+            return `${args[1]}="${args[4].replaceAll('"', '&quot;')}"${args[5]}`
+        })
 
     mangler(element.content)
 
-    // We need to unescape the HTML - Lit will escape it for us when it creates
-    // its templates.
-    template.rawText = element.innerHTML
+    template.text = element.innerHTML
         .replaceAll('&gt;', '>')
         .replaceAll('&lt;', '<')
         .replaceAll('&amp;', '&')
         .replaceAll('&quot;', '"')
+}
+
+export const findTemplate = (upstream: HTMLTemplateTags, predicate: (template: HTMLTemplateTags) => boolean) => {
+    if (predicate(upstream)) {
+        return upstream
+    }
+
+    for (const child of upstream.children) {
+        const result = findTemplate(child, predicate)
+        if (result) {
+            return result
+        }
+    }
 }
