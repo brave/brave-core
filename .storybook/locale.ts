ca880cdcbb8227020cb7fde311314f5c0dc7aeff
Agustín Ruiz <aruiz@brave.com>
2025-02-17 15:21:14 -0300

    Update placeholder text for Leo a(

--- .storybook/locale.ts
+++ .storybook/locale.ts
@@ -268,7 +268,7 @@ let locale: Record<string, string> = {
     'By turning on {{title}}, you agree to the $1Terms of Service$2 and $3Privacy Policy$4.',
   editCardsTitle: 'Edit Cards',
   placeholderLabel: 'Ask follow up',
-  initialPlaceholderLabel: 'What shall we accomplish today?'
+  initialPlaceholderLabel: 'How can I help you today?y?'
 }
 
 export function provideStrings(strings: Record<string, string>) {
