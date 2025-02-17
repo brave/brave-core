f842d684501bd7c6c20c1bb0d94c3303d6493853
Jay Harris <jay.harris@outlook.co.nz>
2025-02-18 11:10:17 +1300

    Typescript AST mangling

--- chromium_src/chrome/browser/resources/extensions/detail_view.mangle.html.ts
+++ chromium_src/chrome/browser/resources/extensions/detail_view.mangle.html.ts
@@ -3,11 +3,11 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import { mangle, loadRaw, write, findTemplate } from '../../../../../build/commands/scripts/lit_mangler.js'
+import { mangle, load, write, findTemplate } from 'lit_mangler'
 
-const upstream = loadRaw('../chrome/browser/resources/extensions/detail_view.html.ts')
+const upstream = load('../chrome/browser/resources/extensions/detail_view.html.ts')
 
-const incognitoTemplate = findTemplate(upstream, (template) => template.rawText.includes('id="allow-incognito"'))
+const incognitoTemplate = findTemplate(upstream, (template) => template.text.includes('id="allow-incognito"'))
 if (!incognitoTemplate) {
     throw new Error('[Brave Extensions Overrides] Could not find incognitoSetting. Has the ID changed?')
 }
