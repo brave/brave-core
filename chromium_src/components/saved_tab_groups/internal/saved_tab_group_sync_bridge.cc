e7e6dc50c07ad62af4767b2447f06988a9a2deb5
AlexeyBarabash <alexey@brave.com>
2025-02-18 14:08:30 +0200

    Moved patch to #define

--- chromium_src/components/saved_tab_groups/internal/saved_tab_group_sync_bridge.cc
+++ chromium_src/components/saved_tab_groups/internal/saved_tab_group_sync_bridge.cc
@@ -3,8 +3,19 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+#include "components/saved_tab_groups/proto/local_tab_group_data.pb.h"
+#include "components/saved_tab_groups/public/saved_tab_group.h"
+
+namespace {
+static bool get_true() {
+  return true;
+}
+}  // namespace
+
+#define created_before_syncing_tab_groups \
+  created_before_syncing_tab_groups() || get_true
 #define BRAVE_APPLY_DISABLE_SYNC_CHANGES_CLEAR_GROUPS \
-  groups_to_close_locally.clear();                    \
   DCHECK(groups_to_close_locally.empty());
 #include "src/components/saved_tab_groups/internal/saved_tab_group_sync_bridge.cc"
 #undef BRAVE_APPLY_DISABLE_SYNC_CHANGES_CLEAR_GROUPS
+#undef created_before_syncing_tab_groups
