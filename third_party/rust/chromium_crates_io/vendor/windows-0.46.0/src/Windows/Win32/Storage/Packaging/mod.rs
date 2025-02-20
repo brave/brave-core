bc5a016a40ff265f03574bd9ebe50ff431ebd454
Brian Johnson <34129+bridiver@users.noreply.github.com>
2025-02-20 12:43:21 -0700

    [CodeHealth][rust] Remove unused chrono features and associated deps (#27739)

--- third_party/rust/chromium_crates_io/vendor/windows-0.46.0/src/Windows/Win32/Storage/Packaging/mod.rs
+++
@@ -1,6 +0,0 @@
-#[cfg(feature = "Win32_Storage_Packaging_Appx")]
-pub mod Appx;
-#[cfg(feature = "Win32_Storage_Packaging_Opc")]
-pub mod Opc;
-#[cfg(feature = "implement")]
-::core::include!("impl.rs");
