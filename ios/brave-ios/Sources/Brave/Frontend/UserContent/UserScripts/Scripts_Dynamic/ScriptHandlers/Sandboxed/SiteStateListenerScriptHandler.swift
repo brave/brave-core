AsilHQ/kahf-browser-core-experimental

Add unit tests for :has() & :has-text() filters.

ios/brave-ios/Sources/Brave/Frontend/UserContent/UserScripts/Scripts_Dynamic/ScriptHandlers/Sandboxed/SiteStateListenerScriptHandler.swift
@@ -85,7 +85,7 @@ class SiteStateListenerScriptHandler: TabContentScript {
             cachedStandardSelectors = standard
             cachedAggressiveSelectors = aggressive
           }
-          let setup = try self.makeSetup(
+          let setup = UserScriptType.SelectorsPollerSetup.makeSetup(
             from: models,
             isAggressive: domain.globalBlockAdsAndTrackingLevel.isAggressive,
             cachedStandardSelectors: cachedStandardSelectors,
@@ -116,13 +116,15 @@ class SiteStateListenerScriptHandler: TabContentScript {
       Logger.module.error("\(error.localizedDescription)")
     }
   }
+}
 
-  @MainActor private func makeSetup(
+extension UserScriptType.SelectorsPollerSetup {
+  public static func makeSetup(
     from modelTuples: [AdBlockGroupsManager.CosmeticFilterModelTuple],
     isAggressive: Bool,
     cachedStandardSelectors: Set<String>,
     cachedAggressiveSelectors: Set<String>
-  ) throws -> UserScriptType.SelectorsPollerSetup {
+  ) -> UserScriptType.SelectorsPollerSetup {
     var standardSelectors = cachedStandardSelectors
     var aggressiveSelectors = cachedAggressiveSelectors
 

ios/brave-ios/Tests/ClientTests/User Scripts/ScriptExecutionTests.swift
@@ -34,6 +34,8 @@ final class ScriptExecutionTests: XCTestCase {
     let upwardInt: Bool
     let upwardSelector: Bool
     let localFrameElement: Bool
+    let hasTextDisplayIsNone: Bool
+    let hasDisplayIsNone: Bool
   }
 
   override class func setUp() {
@@ -247,15 +249,6 @@ final class ScriptExecutionTests: XCTestCase {
       "test-ad-primary-standard-1st-party", "test-ad-primary-standard-3rd-party",
       "test-ad-primary-aggressive-1st-party", "test-ad-primary-aggressive-3rd-party",
     ]
-    let setup = UserScriptType.SelectorsPollerSetup(
-      hideFirstPartyContent: false,
-      genericHide: false,
-      firstSelectorsPollingDelayMs: nil,
-      switchToSelectorsPollingThreshold: 1000,
-      fetchNewClassIdRulesThrottlingMs: 100,
-      aggressiveSelectors: initialAggressiveSelectors,
-      standardSelectors: initialStandardSelectors.union(invalidSelectors)
-    )
     AdblockEngine.setDomainResolver()
     let engine = try AdblockEngine(
       rules: [
@@ -265,11 +258,22 @@ final class ScriptExecutionTests: XCTestCase {
         "brave.com###test-style-element:style(background-color: red !important)",
         "brave.com###test-upward-int-target:upward(2)",
         "brave.com###test-upward-selector-target:upward(#test-upward-selector)",
+        "brave.com###test-has-text:has-text(hide me)",
+        "brave.com###test-has:has(a.banner-link)",
       ].joined(separator: "\n")
     )
-    let proceduralFilters = try engine.cosmeticFilterModel(
+    let cosmeticFilterModel = try engine.cosmeticFilterModel(
       forFrameURL: URL(string: "https://brave.com")!
-    )?.proceduralActions
+    )!
+    // We only care about our AdblockEngine for this test
+    let models: [AdBlockGroupsManager.CosmeticFilterModelTuple] = [(false, cosmeticFilterModel)]
+    let setup = UserScriptType.SelectorsPollerSetup.makeSetup(
+      from: models,
+      isAggressive: false,
+      cachedStandardSelectors: initialStandardSelectors.union(invalidSelectors),
+      cachedAggressiveSelectors: initialAggressiveSelectors
+    )
+    let proceduralFilters = cosmeticFilterModel.proceduralActions
     XCTAssertNotNil(proceduralFilters)
 
     // Add mock SiteStateListenerScriptHandler and inject the SiteStateListener script so we get a message from each frame with the `WKFrameInfo`.
@@ -369,7 +373,7 @@ final class ScriptExecutionTests: XCTestCase {
     for try await message in siteStateListenerMockMessageHandler {
       frameInfos.insert(message.frameInfo)
       let script = try ScriptFactory.shared.makeScript(
-        for: .selectorsPoller(setup, proceduralActions: Set(proceduralFilters ?? []))
+        for: .selectorsPoller(setup, proceduralActions: Set(proceduralFilters))
       )
       try await viewController.webView.evaluateSafeJavaScriptThrowing(
         functionName: script.source,
@@ -451,6 +455,8 @@ final class ScriptExecutionTests: XCTestCase {
     XCTAssertTrue(resultsAfterPump?.styledElement ?? false)
     XCTAssertTrue(resultsAfterPump?.upwardInt ?? false)
     XCTAssertTrue(resultsAfterPump?.upwardSelector ?? false)
+    XCTAssertTrue(resultsAfterPump?.hasTextDisplayIsNone ?? false)
+    XCTAssertTrue(resultsAfterPump?.hasDisplayIsNone ?? false)
     // Test for local frames
     XCTAssertTrue(resultsAfterPump?.localFrameElement ?? false)
   }

brave/brave-core

ios/brave-ios/Sources/Brave/Frontend/UserContent/UserScripts/Scripts_Dynamic/ScriptHandlers/Sandboxed/SiteStateListenerScriptHandler.swift
@@ -85,7 +85,7 @@ class SiteStateListenerScriptHandler: TabContentScript {
             cachedStandardSelectors = standard
             cachedAggressiveSelectors = aggressive
           }
-          let setup = try self.makeSetup(
+          let setup = UserScriptType.ContentCosmeticSetup.makeSetup(
             from: models,
             isAggressive: domain.globalBlockAdsAndTrackingLevel.isAggressive,
             cachedStandardSelectors: cachedStandardSelectors,
@@ -116,13 +116,15 @@ class SiteStateListenerScriptHandler: TabContentScript {
       Logger.module.error("\(error.localizedDescription)")
     }
   }
+}
 
-  @MainActor private func makeSetup(
+extension UserScriptType.ContentCosmeticSetup {
+  public static func makeSetup(
     from modelTuples: [AdBlockGroupsManager.CosmeticFilterModelTuple],
     isAggressive: Bool,
     cachedStandardSelectors: Set<String>,
     cachedAggressiveSelectors: Set<String>
-  ) throws -> UserScriptType.ContentCosmeticSetup {
+  ) -> UserScriptType.ContentCosmeticSetup {
     var standardSelectors = cachedStandardSelectors
     var aggressiveSelectors = cachedAggressiveSelectors
 

ios/brave-ios/Tests/ClientTests/User Scripts/ScriptExecutionTests.swift
@@ -34,6 +34,8 @@ final class ScriptExecutionTests: XCTestCase {
     let upwardInt: Bool
     let upwardSelector: Bool
     let localFrameElement: Bool
+    let hasTextDisplayIsNone: Bool
+    let hasDisplayIsNone: Bool
   }
 
   override class func setUp() {
@@ -247,15 +249,6 @@ final class ScriptExecutionTests: XCTestCase {
       "test-ad-primary-standard-1st-party", "test-ad-primary-standard-3rd-party",
       "test-ad-primary-aggressive-1st-party", "test-ad-primary-aggressive-3rd-party",
     ]
-    let setup = UserScriptType.ContentCosmeticSetup(
-      hideFirstPartyContent: false,
-      genericHide: false,
-      firstSelectorsPollingDelayMs: nil,
-      switchToSelectorsPollingThreshold: 1000,
-      fetchNewClassIdRulesThrottlingMs: 100,
-      aggressiveSelectors: initialAggressiveSelectors,
-      standardSelectors: initialStandardSelectors.union(invalidSelectors)
-    )
     AdblockEngine.setDomainResolver()
     let engine = try AdblockEngine(
       rules: [
@@ -265,11 +258,22 @@ final class ScriptExecutionTests: XCTestCase {
         "brave.com###test-style-element:style(background-color: red !important)",
         "brave.com###test-upward-int-target:upward(2)",
         "brave.com###test-upward-selector-target:upward(#test-upward-selector)",
+        "brave.com###test-has-text:has-text(hide me)",
+        "brave.com###test-has:has(a.banner-link)",
       ].joined(separator: "\n")
     )
-    let proceduralFilters = try engine.cosmeticFilterModel(
+    let cosmeticFilterModel = try engine.cosmeticFilterModel(
       forFrameURL: URL(string: "https://brave.com")!
-    )?.proceduralActions
+    )!
+    // We only care about our AdblockEngine for this test
+    let models: [AdBlockGroupsManager.CosmeticFilterModelTuple] = [(false, cosmeticFilterModel)]
+    let setup = UserScriptType.ContentCosmeticSetup.makeSetup(
+      from: models,
+      isAggressive: false,
+      cachedStandardSelectors: initialStandardSelectors.union(invalidSelectors),
+      cachedAggressiveSelectors: initialAggressiveSelectors
+    )
+    let proceduralFilters = cosmeticFilterModel.proceduralActions
     XCTAssertNotNil(proceduralFilters)
 
     // Add mock SiteStateListenerScriptHandler and inject the SiteStateListener script so we get a message from each frame with the `WKFrameInfo`.
@@ -369,7 +373,7 @@ final class ScriptExecutionTests: XCTestCase {
     for try await message in siteStateListenerMockMessageHandler {
       frameInfos.insert(message.frameInfo)
       let script = try ScriptFactory.shared.makeScript(
-        for: .contentCosmetic(setup, proceduralActions: Set(proceduralFilters ?? []))
+        for: .contentCosmetic(setup, proceduralActions: Set(proceduralFilters))
       )
       try await viewController.webView.evaluateSafeJavaScriptThrowing(
         functionName: script.source,
@@ -451,6 +455,8 @@ final class ScriptExecutionTests: XCTestCase {
     XCTAssertTrue(resultsAfterPump?.styledElement ?? false)
     XCTAssertTrue(resultsAfterPump?.upwardInt ?? false)
     XCTAssertTrue(resultsAfterPump?.upwardSelector ?? false)
+    XCTAssertTrue(resultsAfterPump?.hasTextDisplayIsNone ?? false)
+    XCTAssertTrue(resultsAfterPump?.hasDisplayIsNone ?? false)
     // Test for local frames
     XCTAssertTrue(resultsAfterPump?.localFrameElement ?? false)
   }