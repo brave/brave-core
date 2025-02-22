cd3639001245aba8825780cedd98ae024d9bd110
Arthur Edelstein <aedelstein@brave.com>
2025-02-21 17:00:13 -0800

    fix crash when switching tabs

--- browser/ui/views/email_aliases_bubble_view.cc
+++ browser/ui/views/email_aliases_bubble_view.cc
@@ -5,7 +5,6 @@
 
 #include "brave/browser/ui/views/email_aliases_bubble_view.h"
 
-#include <iostream>
 #include <memory>
 #include <string>
 #include <utility>
@@ -33,10 +32,10 @@ std::unique_ptr<views::Widget> widget_ptr_;
 void EmailAliasesBubbleView::Show(content::WebContents* web_contents,
                                   views::View* anchor_view,
                                   uint64_t field_renderer_id) {
+  auto bubble_view = std::make_unique<EmailAliasesBubbleView>(anchor_view, web_contents, field_renderer_id);
   std::unique_ptr<views::Widget> widget(
       views::BubbleDialogDelegateView::CreateBubble(
-          std::make_unique<EmailAliasesBubbleView>(anchor_view, web_contents,
-                                                   field_renderer_id),
+          std::move(bubble_view),
           views::Widget::InitParams::CLIENT_OWNS_WIDGET));
   widget->Show();
   widget_ptr_ = std::move(widget);
@@ -60,7 +59,6 @@ EmailAliasesBubbleView::EmailAliasesBubbleView(
   auto web_view =
       std::make_unique<views::WebView>(web_contents->GetBrowserContext());
   SetButtons(static_cast<int>(ui::mojom::DialogButton::kNone));
-  web_view->SetPreferredSize(gfx::Size(500, 350));
   web_view_ = AddChildView(std::move(web_view));
 
   // Load URL after adding to view hierarchy
@@ -107,9 +105,19 @@ void EmailAliasesBubbleView::FillFieldWithNewAlias(
 
 gfx::Size EmailAliasesBubbleView::CalculatePreferredSize(
     const views::SizeBounds& available_size) const {
-  std::cout << "EmailAliasesBubbleView::CalculatePreferredSize" << std::endl;
   return gfx::Size(500, 350);
+}
+
+void EmailAliasesBubbleView::OnWidgetDestroying(views::Widget* widget) {
+  widget_is_being_destroyed_ = true;
+}
+
+void EmailAliasesBubbleView::OnAnchorBoundsChanged() {
+  if (!widget_is_being_destroyed_) {
+    BubbleDialogDelegate::OnAnchorBoundsChanged();
+  }
 }
 
 BEGIN_METADATA(EmailAliasesBubbleView)
 END_METADATA
+
