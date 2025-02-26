af43a01423f53062ccf956985b56b74502742c58
Douglas Daniel <douglashdaniel@gmail.com>
2025-02-25 18:20:58 -0600

    fix(wallet): Dont Show Sync Warning for Zero Blocks Behind

--- components/brave_wallet_ui/components/desktop/views/accounts/account.tsx
+++ components/brave_wallet_ui/components/desktop/views/accounts/account.tsx
@@ -227,7 +227,10 @@ export const Account = () => {
     ? chainTipStatus.chainTip - chainTipStatus.latestScannedBlock
     : 0
 
-  const showSyncWarning = isShieldedAccount && !syncWarningDismissed
+  const showSyncWarning =
+    isShieldedAccount &&
+    (blocksBehind > 0 || chainTipStatus === null) &&
+    !syncWarningDismissed
 
   // custom hooks & memos
   const scrollIntoView = useScrollIntoView()
