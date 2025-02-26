ce9743edde38660e741c8427bfb0ef60f3ed53e6
Claudio DeSouza <cdesouza@brave.com>
2025-02-26 12:50:30 +0000

    [cr135] `kBiddingAndScoringDebugReportingAPI` enabled default
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/9bafc6332a47845a03f184ccc27bc8f4eeca2d68
    
    commit 9bafc6332a47845a03f184ccc27bc8f4eeca2d68
    Author: Qingxin Wu <qingxinwu@google.com>
    Date:   Tue Feb 25 07:33:46 2025 -0800
    
        Remove flag BiddingAndScoringDebugReportingAPI.
    
        It has been enabled since https://crrev.com/c/5041672 (Nov 2023). And
        it's no longer needed since there's no plan of disabling it anymore.
    
        Change-Id: I3058f39cdd3cd3f2a8aa7090629c30a19efa59c0

--- chromium_src/third_party/blink/common/features.cc
+++ chromium_src/third_party/blink/common/features.cc
@@ -21,7 +21,6 @@ OVERRIDE_FEATURE_DEFAULT_STATES({{
     {kAttributionReportingInBrowserMigration,
      base::FEATURE_DISABLED_BY_DEFAULT},
     {kBackgroundResourceFetch, base::FEATURE_DISABLED_BY_DEFAULT},
-    {kBiddingAndScoringDebugReportingAPI, base::FEATURE_DISABLED_BY_DEFAULT},
     {kBrowsingTopics, base::FEATURE_DISABLED_BY_DEFAULT},
     {kControlledFrame, base::FEATURE_DISABLED_BY_DEFAULT},
     {kCssSelectorFragmentAnchor, base::FEATURE_DISABLED_BY_DEFAULT},
