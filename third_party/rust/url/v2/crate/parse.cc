055b755c3ca9ae29a1b4196b54bd54ff4927017c
bridiver <34129+bridiver@users.noreply.github.com>
2025-02-28 05:36:37 -0700

    use chromium code for url crate

---
+++ third_party/rust/url/v2/crate/parse.cc
@@ -0,0 +1,46 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "brave/third_party/rust/url/v2/crate/parse.h"
+
+#include <string>
+
+#include "brave/third_party/rust/url/v2/crate/src/lib.rs.h"
+#include "url/gurl.h"
+#include "base/i18n/icu_util.h"
+
+namespace parse {
+
+void InitializeICUForTesting() {
+  CHECK(base::i18n::InitializeICU());
+}
+
+ParseResult ParseURL(rust::Str url_string) {
+  GURL url((std::string(url_string)));
+  ParseResult res;
+  res.serialization = url.possibly_invalid_spec();
+  res.has_host = url.has_host();
+  res.host = url.host();
+  res.has_path = url.has_path();
+  res.path = url.path();
+  res.has_fragment = url.has_ref();
+  res.fragment = url.ref();
+  res.has_scheme = url.has_scheme();
+  res.scheme = url.scheme();
+  res.has_query = url.has_query();
+  res.query = url.query();
+  res.has_port = url.has_port();
+  // Max port value is 65535 and this has already been parsed and validated
+  // by GURL
+  res.port = base::saturated_cast<uint16_t>(url.IntPort());
+  res.valid = url.is_valid();
+  return res;
+}
+
+ParseResult Resolve(const ParseResult& base, rust::Str path) {
+  return ParseURL(GURL((std::string(base.serialization))).Resolve((std::string(path))).possibly_invalid_spec());
+}
+
+}  // namespace parse
