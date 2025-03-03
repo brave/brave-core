87ab867535c1de735e083006be9cec16a7f4181f
Jay Harris <jay.harris@outlook.co.nz>
2025-03-04 08:20:56 +1300

    [AI Chat]: Floating attachments panel on small screens (#27807)

---
+++ components/ai_chat/resources/page/hooks/useIsElementSmall.ts
@@ -0,0 +1,25 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import * as React from "react"
+
+export const useIsElementSmall = (small = 768) => {
+    const [isSmall, setIsSmall] = React.useState(false)
+    const [element, setElement] = React.useState<HTMLElement | null>(null)
+
+    React.useEffect(() => {
+        if (!element) return
+        const resizeObserver = new ResizeObserver((entries) => {
+            setIsSmall(entries[0].contentRect.width < small)
+        })
+        resizeObserver.observe(element)
+        return () => resizeObserver.disconnect()
+    }, [element, small])
+
+    return {
+        isSmall,
+        setElement
+    }
+}
