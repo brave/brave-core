AsilHQ/kahf-browser-core-experimental

[cr135] Lifting deleted `UsageClock` into Brave
This class has been deleted from upstream for being unused, so it is
being lifted directly into brave as it was still in use in our codebase.

Chromium change:
https://chromium.googlesource.com/chromium/src/+/1bfee585c32eb3fa81838e7d64e82e7510da517b

commit 1bfee585c32eb3fa81838e7d64e82e7510da517b
Author: Joe Mason <joenotcharles@google.com>
Date:   Fri Feb 7 08:40:39 2025 -0800

    Remove unused LifecycleUnit methods (2/N)

    Removes GetChromeUsageTimeWhenHidden.

    This is the only user of the UsageClock class so removes that as well.

    R=fdoray

    Bug: 383374205

browser/misc_metrics/usage_clock.h
@@ -1,0 +1,53 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef CHROME_BROWSER_RESOURCE_COORDINATOR_USAGE_CLOCK_H_
+#define CHROME_BROWSER_RESOURCE_COORDINATOR_USAGE_CLOCK_H_
+
+#include "base/time/time.h"
+#include "chrome/browser/metrics/desktop_session_duration/desktop_session_duration_tracker.h"
+
+namespace misc_metrics {
+
+// A clock that advances when Chrome is in use.
+//
+// See metrics::DesktopSessionDurationTracker for how Chrome usage is tracked.
+// If metrics::DesktopSessionDurationTracker isn't initialized before this, the
+// clock will advance continuously, regardless of Chrome usage. This avoids
+// forcing all tests that indirectly depend on this to initialize
+// metrics::DesktopSessionDurationTracker.
+class UsageClock : public metrics::DesktopSessionDurationTracker::Observer {
+ public:
+  UsageClock();
+
+  UsageClock(const UsageClock&) = delete;
+  UsageClock& operator=(const UsageClock&) = delete;
+
+  ~UsageClock() override;
+
+  // Returns the amount of Chrome usage time since this was instantiated.
+  base::TimeDelta GetTotalUsageTime() const;
+
+  // Returns true if Chrome is currently considered to be in use.
+  bool IsInUse() const;
+
+ private:
+  void OnSessionStarted(base::TimeTicks session_start) override;
+  void OnSessionEnded(base::TimeDelta session_length,
+                      base::TimeTicks session_end) override;
+
+  // The total time elapsed in completed usage sessions. The duration of the
+  // current usage session, if any, must be added to this to get the total usage
+  // time of Chrome.
+  base::TimeDelta usage_time_in_completed_sessions_;
+
+  // The time at which the current session started, or a null TimeTicks if not
+  // currently in a session.
+  base::TimeTicks current_usage_session_start_time_;
+};
+
+}  // namespace misc_metrics
+
+#endif  // CHROME_BROWSER_RESOURCE_COORDINATOR_USAGE_CLOCK_H_

brave/brave-core

browser/misc_metrics/usage_clock.h
@@ -1,0 +1,53 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_BROWSER_MISC_METRICS_USAGE_CLOCK_H_
+#define BRAVE_BROWSER_MISC_METRICS_USAGE_CLOCK_H_
+
+#include "base/time/time.h"
+#include "chrome/browser/metrics/desktop_session_duration/desktop_session_duration_tracker.h"
+
+namespace misc_metrics {
+
+// A clock that advances when Chrome is in use.
+//
+// See metrics::DesktopSessionDurationTracker for how Chrome usage is tracked.
+// If metrics::DesktopSessionDurationTracker isn't initialized before this, the
+// clock will advance continuously, regardless of Chrome usage. This avoids
+// forcing all tests that indirectly depend on this to initialize
+// metrics::DesktopSessionDurationTracker.
+class UsageClock : public metrics::DesktopSessionDurationTracker::Observer {
+ public:
+  UsageClock();
+
+  UsageClock(const UsageClock&) = delete;
+  UsageClock& operator=(const UsageClock&) = delete;
+
+  ~UsageClock() override;
+
+  // Returns the amount of Chrome usage time since this was instantiated.
+  base::TimeDelta GetTotalUsageTime() const;
+
+  // Returns true if Chrome is currently considered to be in use.
+  bool IsInUse() const;
+
+ private:
+  void OnSessionStarted(base::TimeTicks session_start) override;
+  void OnSessionEnded(base::TimeDelta session_length,
+                      base::TimeTicks session_end) override;
+
+  // The total time elapsed in completed usage sessions. The duration of the
+  // current usage session, if any, must be added to this to get the total usage
+  // time of Chrome.
+  base::TimeDelta usage_time_in_completed_sessions_;
+
+  // The time at which the current session started, or a null TimeTicks if not
+  // currently in a session.
+  base::TimeTicks current_usage_session_start_time_;
+};
+
+}  // namespace misc_metrics
+
+#endif  // BRAVE_BROWSER_MISC_METRICS_USAGE_CLOCK_H_