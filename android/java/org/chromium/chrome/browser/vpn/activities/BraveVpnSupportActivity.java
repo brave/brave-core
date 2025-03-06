000176805d60098db253b777915d3c40dabf3a59
AlexeyBarabash <alexey@brave.com>
2025-03-06 13:51:45 +0200

    [cr136][Android] SyncConsent fragments and activity were removed
    FirstRunFlowSequencer.onFlowIsKnown() argument is changed from bundle to boolean
    
    down_arrow drawable was used at
    //chrome/browser/ui/android/signin/java/res/layout/signin_view.xml
    and now is also removed
    
    Related Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/aae9d74edf5532ed12952d4961a7ee20c3c6b092
    
    	[Android] Remove SyncConsent fragments and activity
    
    	This cl removes the following classes:
    	- SyncConsentFragmentBase
    	- SyncConsentFragment
    	- SyncConsentFirstRunFragment
    	- SyncConsentActivity
    	- All xml resources related to these classes
    
    	FirstRunFlowSequencer.onFlowIsKnown() argument is changed from bundle
    	to boolean.
    
    	Bug: 350461111
    	Change-Id: If1ad68f40e98f843d42f1b3903878c22af6ed2cb
    	Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6169618

--- android/java/org/chromium/chrome/browser/vpn/activities/BraveVpnSupportActivity.java
+++ android/java/org/chromium/chrome/browser/vpn/activities/BraveVpnSupportActivity.java
@@ -12,7 +12,6 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
-import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.view.MenuItem;
 import android.view.View;
@@ -148,7 +147,7 @@ public class BraveVpnSupportActivity extends AsyncInitializationActivity {
         mFirstRunFlowSequencer =
                 new BraveFirstRunFlowSequencer(mProfileSupplier) {
                     @Override
-                    public void onFlowIsKnown(Bundle freProperties) {
+                    public void onFlowIsKnown(boolean isChild) {
                         initializeViews();
                     }
                 };
