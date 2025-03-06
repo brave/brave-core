5163f7a719974c88c690337213808ba46191330c
Szilard Szaloki <szilardszaloki@gmail.com>
2025-03-06 11:37:47 -0700

    `opaque-ke` skeleton

---
+++ ui/webui/resources/opaque_ke/src/lib.rs
@@ -0,0 +1,11 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn return42() -> i32 {
+    return 42;
+}
