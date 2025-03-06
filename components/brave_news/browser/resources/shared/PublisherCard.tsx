c303c1c5725e255a96188b4faeadb5ac7c9b6b4e
Jay Harris <jay.harris@outlook.co.nz>
2025-03-06 13:17:45 +1300

    Use encodeURIComponent

--- components/brave_news/browser/resources/shared/PublisherCard.tsx
+++ components/brave_news/browser/resources/shared/PublisherCard.tsx
@@ -73,7 +73,7 @@ export default function PublisherCard(props: {
 
   return <Flex direction="column" gap={8}>
     <Card backgroundColor={backgroundColor} data-feed-card-is-followed={followed}>
-      {coverUrl && <CoverImage backgroundImage={`chrome://maybe-padded-image?url=${coverUrl}`} />}
+      {coverUrl && <CoverImage backgroundImage={`chrome://maybe-padded-image?url=${encodeURIComponent(coverUrl)}`} />}
       <StyledFollowButton fab size='tiny' following={followed} onClick={() => setFollowed(!followed)} />
     </Card>
     <Name>
