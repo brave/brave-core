771a02cd4e6296afef3fa3afecbc952a529539b9
Mikhail <matuchin@brave.com>
2025-03-06 16:50:08 +0400

    Disable chromium metrics providers (#26294)
    * Disable chromium metrics providers
    
    * Add unit tests
    
    * Fix format

---
+++ chromium_src/chrome/browser/metrics/chrome_metrics_service_client_unittest.cc
@@ -0,0 +1,49 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "base/gtest_prod_util.h"
+FORWARD_DECLARE_TEST(ChromeMetricsServiceClientTest,
+                     BraveTestRegisterUKMProviders);
+
+#define cloned_install_subscription_                         \
+  cloned_install_subscription_;                              \
+  FRIEND_TEST_ALL_PREFIXES(::ChromeMetricsServiceClientTest, \
+                           BraveTestRegisterUKMProviders)
+#include "components/ukm/ukm_service.h"
+#undef cloned_install_subscription_
+
+// Disable the original tests we're going to override.
+#define TestRegisterUKMProviders DISABLED_TestRegisterUKMProviders
+#define TestRegisterMetricsServiceProviders \
+  DISABLED_TestRegisterMetricsServiceProviders
+
+#include "src/chrome/browser/metrics/chrome_metrics_service_client_unittest.cc"
+
+#undef TestRegisterMetricsServiceProviders
+#undef TestRegisterUKMProviders
+
+TEST_F(ChromeMetricsServiceClientTest, BraveTestRegisterUKMProviders) {
+  std::unique_ptr<ChromeMetricsServiceClient> chrome_metrics_service_client =
+      TestChromeMetricsServiceClient::Create(metrics_state_manager_.get(),
+                                             synthetic_trial_registry_.get());
+  size_t observed_count = chrome_metrics_service_client->GetUkmService()
+                              ->metrics_providers_.GetProviders()
+                              .size();
+  // In Brave, we expect 0 UKM providers regardless of feature flag
+  EXPECT_EQ(0ul, observed_count);
+}
+
+TEST_F(ChromeMetricsServiceClientTest, BraveRegisterMetricsServiceProviders) {
+  std::unique_ptr<TestChromeMetricsServiceClient>
+      chrome_metrics_service_client = TestChromeMetricsServiceClient::Create(
+          metrics_state_manager_.get(), synthetic_trial_registry_.get());
+
+  // In Brave, we expect only 2 metrics providers (the ones added in the
+  // MetricsService constructor)
+  EXPECT_EQ(2ul, chrome_metrics_service_client->GetMetricsService()
+                     ->GetDelegatingProviderForTesting()
+                     ->GetProviders()
+                     .size());
+}
--- chromium_src/chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
+++ chromium_src/chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
@@ -3,21 +3,45 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+// This file is designed to disable certain components of the
+// performance_manager to optimize CPU usage. Since Brave does not send UMA/UKM
+// data, collecting these metrics is unnecessary, especially if it requires
+// significant resources.
+// Specifically, we:
+// 1. disable PageResourceMonitor.
+// 2. disable MetricsProviderDesktop::Initialize() to support overrides in
+//    ChromeMetricsServiceClient.
+
 #include "components/performance_manager/public/metrics/page_resource_monitor.h"
 
 #if !BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/performance_manager/metrics/metrics_provider_desktop.h"
 namespace performance_manager::metrics {
 namespace {
 class StubGraphOwnedDefaultImpl : public GraphOwnedDefaultImpl {};
 }  // namespace
 }  // namespace performance_manager::metrics
 
-// PageResourceMonitor collects data only to send UMA/UKM. It means it does
-// nothing in Brave, but the collection is CPU-intensive. Disable it to save
-// CPU during startup.
+namespace performance_manager {
+
+class FakeMetricsProviderDesktop {
+ public:
+  static FakeMetricsProviderDesktop* GetInstance() {
+    static FakeMetricsProviderDesktop instance;
+    return &instance;
+  }
+
+  void Initialize() {
+    // Do nothing
+  }
+};
+}  // namespace performance_manager
+
 #define PageResourceMonitor StubGraphOwnedDefaultImpl
+#define MetricsProviderDesktop FakeMetricsProviderDesktop
 #endif  // !BUILDFLAG(IS_ANDROID)
 #include "src/chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc"
 #if !BUILDFLAG(IS_ANDROID)
+#undef MetricsProviderDesktop
 #undef PageResourceMonitor
 #endif  // !BUILDFLAG(IS_ANDROID)
