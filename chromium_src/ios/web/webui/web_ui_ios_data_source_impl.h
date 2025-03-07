5f8e95f31a902356e43d1204e5aea1e5bf2ecc36
Brandon <JustBrandonT@gmail.com>
2025-03-07 17:09:44 -0500

    Use inheritance to avoid duplicating code

---
+++ chromium_src/ios/web/webui/web_ui_ios_data_source_impl.h
@@ -0,0 +1,32 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_IOS_WEB_WEBUI_WEB_UI_IOS_DATA_SOURCE_IMPL_H_
+#define BRAVE_CHROMIUM_SRC_IOS_WEB_WEBUI_WEB_UI_IOS_DATA_SOURCE_IMPL_H_
+
+#include "ios/web/public/webui/url_data_source_ios.h"
+#include "ios/web/public/webui/web_ui_ios_data_source.h"
+#include "ios/web/webui/url_data_manager_ios.h"
+#include "ios/web/webui/url_data_source_ios_impl.h"
+
+/// Override that allows BraveWebUIIOSDataSource to access
+/// WebUIIOSDataSourceImpl members. Also, allows it to construct a
+/// WebUIIOSDataSourceImpl with its own source. Avoids duplicating a lot of
+/// code.
+
+class URLDataSourceIOS;
+class BraveWebUIIOSDataSource;
+
+#define WebUIIOSDataSourceTest                                    \
+  WebUIIOSDataSourceTest;                                         \
+  friend class ::BraveWebUIIOSDataSource;                         \
+  explicit WebUIIOSDataSourceImpl(const std::string& source_name, \
+                                  URLDataSourceIOS* source)
+
+#include "src/ios/web/webui/web_ui_ios_data_source_impl.h"
+
+#undef WebUIIOSDataSourceTest
+
+#endif  // BRAVE_CHROMIUM_SRC_IOS_WEB_WEBUI_WEB_UI_IOS_DATA_SOURCE_IMPL_H_
