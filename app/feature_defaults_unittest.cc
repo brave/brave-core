27abd3eb92450124fa8eb97d4030b19439002f9f
Claudio DeSouza <cdesouza@brave.com>
2025-03-07 13:29:44 +0000

    [cr136] `kPermissionPredictionsV3` deleted
    Now that permissions::features::kPermissionPredictionsV3 is deleted upstream,
    our security team has asked us to stub out the PredictionService instead.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/f9617c511f3f1ec9c50ecc99e9d6052485e4728a
    
    commit f9617c511f3f1ec9c50ecc99e9d6052485e4728a
    Author: Judith Hemp <hempjudith@google.com>
    Date:   Thu Mar 6 04:37:10 2025 -0800
    
        Clean up CPSSv3 feature flag
    
        CPSSv3 is launched and can be entirely removed from Chrome. CPSSv2 gates
        the most critical infrastructure already.
    
        Bug: 399593722

--- app/feature_defaults_unittest.cc
+++ app/feature_defaults_unittest.cc
@@ -228,7 +228,6 @@ TEST(FeatureDefaultsTest, DisabledFeatures) {
 #endif
       &permissions::features::kPermissionOnDeviceNotificationPredictions,
       &permissions::features::kPermissionPredictionsV2,
-      &permissions::features::kPermissionPredictionsV3,
       &permissions::features::kShowRelatedWebsiteSetsPermissionGrants,
       &plus_addresses::features::kPlusAddressesEnabled,
       &privacy_sandbox::kEnforcePrivacySandboxAttestations,
