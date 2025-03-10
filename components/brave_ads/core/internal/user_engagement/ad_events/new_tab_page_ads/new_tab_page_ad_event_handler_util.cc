85f94185d6193877b6eb0f7289dd492f951efbf9
Terry Mancey <terry.mancey@icloud.com>
2025-03-10 13:47:22 -0500

    [ads] Transition to v4 confirmation endpoint

--- components/brave_ads/core/internal/user_engagement/ad_events/new_tab_page_ads/new_tab_page_ad_event_handler_util.cc
+++ components/brave_ads/core/internal/user_engagement/ad_events/new_tab_page_ads/new_tab_page_ad_event_handler_util.cc
@@ -24,14 +24,14 @@ bool ShouldFireAdEvent(const NewTabPageAdInfo& ad,
   if (UserHasJoinedBraveRewards() &&
       !WasAdServed(ad, ad_events, mojom_ad_event_type)) {
     BLOG(1,
-         "Search result ad: Not allowed because an ad was not served for "
+         "New tab page ad: Not allowed because an ad was not served for "
          "placement id "
              << ad.placement_id);
     return false;
   }
 
   if (ShouldDeduplicateAdEvent(ad, ad_events, mojom_ad_event_type)) {
-    BLOG(1, "Search result ad: Not allowed as deduplicated "
+    BLOG(1, "New tab page ad: Not allowed as deduplicated "
                 << mojom_ad_event_type << " event for placement id "
                 << ad.placement_id);
     return false;
