71d825cbbd8ca8e95cc256702513ff023a264bdc
Simon Hong <shong@brave.com>
2025-03-12 21:04:11 +0900

    Handle tab-fullscreen from split view (#28104)
    fix https://github.com/brave/brave-browser/issues/43111
    
    When active tab initiates tab-fullscreen from split view,
    it should occupy whole screen.

--- browser/ui/views/frame/brave_browser_view.cc
+++ browser/ui/views/frame/brave_browser_view.cc
@@ -917,6 +917,12 @@ views::View* BraveBrowserView::GetContentsContainerForLayoutManager() {
                      : BrowserView::GetContentsContainerForLayoutManager();
 }
 
+void BraveBrowserView::ReadyToListenFullscreenChanges() {
+  if (split_view_) {
+    split_view_->ListenFullscreenChanges();
+  }
+}
+
 bool BraveBrowserView::IsSidebarVisible() const {
   return sidebar_container_view_ && sidebar_container_view_->IsSidebarVisible();
 }
