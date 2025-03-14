b5c66bfb09fc5b3032c27b56ff70101fb52984c6
Claudio DeSouza <cdesouza@brave.com>
2025-03-14 14:15:15 +0000

    fixup! ContentsLayoutManager takes a new lens_overlay_view parameter

--- browser/ui/views/frame/brave_browser_view.cc
+++ browser/ui/views/frame/brave_browser_view.cc
@@ -236,7 +236,7 @@ BraveBrowserView::BraveBrowserView(std::unique_ptr<Browser> browser)
   contents_container_->SetLayoutManager(
       std::make_unique<BraveContentsLayoutManager>(
           devtools_web_view(), contents_web_view(), contents_scrim_view(),
-          secondary_lens_overlay_view_, nullptr, watermark_view_.get(),
+          lens_overlay_view_, nullptr, watermark_view_.get(),
           reader_mode_toolbar_));
 #endif
 
