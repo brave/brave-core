a9183fe42cafe36406debc5f0251a75703c11866
boocmp <boocmp@yandex.ru>
2025-03-18 21:39:02 +0700

    Importing settings for extension from indexed db source.

--- browser/importer/extensions_import_helpers.cc
+++ browser/importer/extensions_import_helpers.cc
@@ -5,92 +5,330 @@
 
 #include "brave/browser/importer/extensions_import_helpers.h"
 
+#include <algorithm>
 #include <memory>
+#include <optional>
+#include <utility>
 
-#include "base/containers/flat_map.h"
+#include "base/files/file_enumerator.h"
 #include "base/files/file_util.h"
-#include "base/logging.h"
-#include "base/task/thread_pool.h"
-#include "brave/common/importer/importer_constants.h"
+#include "base/no_destructor.h"
+#include "base/task/sequenced_task_runner.h"
+#include "brave/common/importer/chrome_importer_utils.h"
+#include "chrome/browser/extensions/extension_service.h"
+#include "chrome/browser/extensions/webstore_install_with_prompt.h"
+#include "chrome/browser/profiles/profile.h"
 #include "components/value_store/value_store.h"
 #include "components/value_store/value_store_factory.h"
 #include "components/value_store/value_store_factory_impl.h"
 #include "extensions/browser/api/storage/value_store_util.h"
 #include "extensions/browser/extension_file_task_runner.h"
+#include "extensions/browser/extension_registry.h"
+#include "extensions/browser/extension_system.h"
 #include "extensions/common/constants.h"
 
-namespace brave {
-namespace {
-using ExtensionStorageMap =
-    base::flat_map<std::string, std::unique_ptr<value_store::ValueStore>>;
+namespace extensions_import {
 
-std::vector<std::string> GetChromeExtensionsSettingsList(
+// Silent installer via websotre w/o any prompt or bubble.
+class WebstoreInstallerForImporting
+    : public extensions::WebstoreInstallWithPrompt {
+ public:
+  using WebstoreInstallWithPrompt::AbortInstall;
+  using WebstoreInstallWithPrompt::WebstoreInstallWithPrompt;
+
+ private:
+  ~WebstoreInstallerForImporting() override = default;
+
+  std::unique_ptr<ExtensionInstallPrompt::Prompt> CreateInstallPrompt()
+      const override {
+    return nullptr;
+  }
+  bool ShouldShowPostInstallUI() const override { return false; }
+};
+
+base::expected<std::vector<ImportingExtension>, bool> GetExtensionsList(
     const base::FilePath& source_profile,
-    std::vector<std::string> extensions_ids) {
-  auto store_factory_source =
+    const base::FilePath& target_profile) {
+  std::vector<ImportingExtension> result;
+  const auto extensions = GetImportableChromeExtensionsList(source_profile);
+  if (!extensions || extensions->empty()) {
+    return base::ok(std::move(result));
+  }
+
+  auto source_store_factory =
       base::MakeRefCounted<value_store::ValueStoreFactoryImpl>(source_profile);
-  std::vector<std::string> result;
-  for (const auto& id : extensions_ids) {
-    if (!store_factory_source->HasValueStore(
+
+  for (const auto& extension_id : *extensions) {
+    const auto value_store =
+        base::FilePath(extensions::kLocalExtensionSettingsDirectoryName)
+            .AppendASCII(extension_id);
+
+    bool has_local_settings = false;
+
+    if (source_store_factory->HasValueStore(
             base::FilePath(extensions::kLocalExtensionSettingsDirectoryName)
-                .AppendASCII(id)))
-      continue;
-    result.push_back(id);
-  }
-  return result;
-}
-
-ExtensionStorageMap CreateStorages(
-    std::vector<std::string> ids,
-    bool skip_if_exists,
-    scoped_refptr<value_store::ValueStoreFactory> factory) {
-  ExtensionStorageMap storages;
-  for (const auto& id : ids) {
-    if (skip_if_exists &&
-        extensions::value_store_util::HasValueStore(
-            extensions::settings_namespace::LOCAL,
-            extensions::value_store_util::ModelType::EXTENSION, id, factory)) {
-      continue;
+                .AppendASCII(extension_id))) {
+      auto store = extensions::value_store_util::CreateSettingsStore(
+          extensions::settings_namespace::LOCAL,
+          extensions::value_store_util::ModelType::EXTENSION, extension_id,
+          source_store_factory);
+      const auto settings = store->Get();
+      if (!settings.status().ok()) {
+        if (!settings.status().IsCorrupted()) {
+          return base::unexpected(false);
+        }
+      } else {
+        has_local_settings = true;
+      }
     }
-    storages[id] = extensions::value_store_util::CreateSettingsStore(
-        extensions::settings_namespace::LOCAL,
-        extensions::value_store_util::ModelType::EXTENSION, id, factory);
+    ImportingExtension e;
+    e.id = extension_id;
+    e.has_local_settings = has_local_settings;
+    result.push_back(std::move(e));
   }
-  return storages;
+
+  return base::ok(std::move(result));
 }
 
-}  // namespace
+bool ImportLocalExtensionSettings(const std::string& extension_id,
+                                  const base::FilePath& source_profile,
+                                  const base::FilePath& target_profile) {
+  auto source_store_factory =
+      base::MakeRefCounted<value_store::ValueStoreFactoryImpl>(source_profile);
+  auto source_store = extensions::value_store_util::CreateSettingsStore(
+      extensions::settings_namespace::LOCAL,
+      extensions::value_store_util::ModelType::EXTENSION, extension_id,
+      source_store_factory);
 
-void ImportStorages(base::FilePath source_profile,
-                    base::FilePath target_profile,
-                    std::vector<std::string> extensions_ids) {
-  DCHECK(
-      extensions::GetExtensionFileTaskRunner()->RunsTasksInCurrentSequence());
-  const auto ids_with_settings =
-      GetChromeExtensionsSettingsList(source_profile, extensions_ids);
-  const auto source_storages = CreateStorages(
-      ids_with_settings, false,
-      base::MakeRefCounted<value_store::ValueStoreFactoryImpl>(source_profile));
-  auto target_storages = CreateStorages(
-      ids_with_settings, true,
-      base::MakeRefCounted<value_store::ValueStoreFactoryImpl>(target_profile));
-  for (const auto& source_store : source_storages) {
-    auto content = source_store.second->Get();
-    if (!content.status().ok() || !target_storages.count(source_store.first))
-      continue;
-    target_storages[source_store.first]->Set(value_store::ValueStore::DEFAULTS,
-                                             content.PassSettings());
+  auto settings = source_store->Get();
+  if (!settings.status().ok()) {
+    return false;
   }
-}
 
-void RemoveExtensionsSettings(base::FilePath target_profile,
-                              const std::string& extension_id) {
-  DCHECK(
-      extensions::GetExtensionFileTaskRunner()->RunsTasksInCurrentSequence());
+  auto target_store_factory =
+      base::MakeRefCounted<value_store::ValueStoreFactoryImpl>(target_profile);
+
   extensions::value_store_util::DeleteValueStore(
       extensions::settings_namespace::LOCAL,
       extensions::value_store_util::ModelType::EXTENSION, extension_id,
-      base::MakeRefCounted<value_store::ValueStoreFactoryImpl>(target_profile));
+      target_store_factory);
+
+  auto target_store = extensions::value_store_util::CreateSettingsStore(
+      extensions::settings_namespace::LOCAL,
+      extensions::value_store_util::ModelType::EXTENSION, extension_id,
+      target_store_factory);
+
+  const auto result = target_store->Set(value_store::ValueStore::DEFAULTS,
+                                        settings.PassSettings());
+  return result.status().ok();
 }
 
-}  // namespace brave
+bool ImportIndexedDBExtensionSettings(const std::string& extension_id,
+                                      const base::FilePath& source_profile,
+                                      const base::FilePath& target_profile) {
+  constexpr char kIndexedDBDir[] = "IndexedDB";
+
+  const base::FilePath pattern = base::FilePath::FromASCII(
+      base::StrCat({"chrome-extension_", extension_id, "_*indexeddb*"}));
+
+  base::FileEnumerator target_enumerator(
+      target_profile.AppendASCII(kIndexedDBDir), false,
+      base::FileEnumerator::DIRECTORIES, pattern.value());
+  // Clear the target profile.
+  target_enumerator.ForEach(
+      [](const base::FilePath& path) { base::DeletePathRecursively(path); });
+
+  base::FileEnumerator source_enumerator(
+      source_profile.AppendASCII(kIndexedDBDir), false,
+      base::FileEnumerator::DIRECTORIES, pattern.value());
+  // Copy settings.
+  source_enumerator.ForEach([&target_profile,
+                             &kIndexedDBDir](const base::FilePath& path) {
+    base::CopyDirectory(
+        path, target_profile.AppendASCII(kIndexedDBDir).Append(path.BaseName()),
+        true);
+  });
+
+  return true;
+}
+
+bool ImportExtensionSettings(const std::string& extension_id,
+                             const base::FilePath& source_profile,
+                             const base::FilePath& target_profile) {
+  if (!ImportLocalExtensionSettings(extension_id, source_profile,
+                                    target_profile)) {
+    return false;
+  }
+  if (!ImportIndexedDBExtensionSettings(extension_id, source_profile,
+                                        target_profile)) {
+    return false;
+  }
+  return true;
+}
+
+ImportingExtension::ImportingExtension() = default;
+ImportingExtension::ImportingExtension(ImportingExtension&&) = default;
+ImportingExtension& ImportingExtension::operator=(ImportingExtension&&) =
+    default;
+
+ImportingExtension::~ImportingExtension() {
+  if (installer) {
+    installer->AbortInstall();
+  }
+}
+
+ExtensionsImporter::ExtensionsImporter(const base::FilePath& source_profile,
+                                       Profile* target_profile)
+    : source_profile_(source_profile), target_profile_(target_profile) {
+  CHECK(target_profile_);
+  CHECK_NE(source_profile_, target_profile_->GetPath());
+}
+
+ExtensionsImporter::~ExtensionsImporter() = default;
+
+// static
+base::RepeatingCallback<ExtensionImportStatus(const std::string& extension_id)>&
+ExtensionsImporter::GetExtensionInstallerForTesting() {
+  static base::NoDestructor<base::RepeatingCallback<ExtensionImportStatus(
+      const std::string& extension_id)>>
+      g_intsaller;
+  return *g_intsaller;
+}
+
+void ExtensionsImporter::Prepare(OnReady on_ready) {
+  CHECK(extensions_.empty());
+  extensions::GetExtensionFileTaskRunner()->PostTaskAndReplyWithResult(
+      FROM_HERE,
+      base::BindOnce(&GetExtensionsList, source_profile_,
+                     target_profile_->GetPath()),
+      base::BindOnce(&ExtensionsImporter::OnGetExtensionsForImport,
+                     weak_factory_.GetWeakPtr(), std::move(on_ready)));
+}
+
+bool ExtensionsImporter::Import(OnExtensionImported on_extension) {
+  CHECK(!IsImportInProgress());
+
+  for (auto& extension : extensions_) {
+    if (extension.is_installed) {
+      on_extension.Run(extension.id, ExtensionImportStatus::kOk);
+      continue;
+    }
+
+    auto wrapper = base::BindOnce(
+        [](base::WeakPtr<ExtensionsImporter> self, OnExtensionImported callback,
+           const std::string& id, ExtensionImportStatus status) {
+          if (self) {
+            --self->in_progress_count_;
+            callback.Run(id, status);
+          }
+        },
+        weak_factory_.GetWeakPtr(), on_extension);
+
+    ++in_progress_count_;
+    if (auto installer = GetExtensionInstallerForTesting()) {
+      const auto status = installer.Run(extension.id);
+      base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
+          FROM_HERE,
+          base::BindOnce(&ExtensionsImporter::OnExtensionInstalled,
+                         weak_factory_.GetWeakPtr(), &extension,
+                         std::move(wrapper),
+                         status == ExtensionImportStatus::kOk, "",
+                         extensions::webstore_install::Result::SUCCESS));
+    } else {
+      extension.installer = base::MakeRefCounted<WebstoreInstallerForImporting>(
+          extension.id, target_profile_, nullptr,
+          base::BindOnce(&ExtensionsImporter::OnExtensionInstalled,
+                         weak_factory_.GetWeakPtr(), &extension,
+                         std::move(wrapper)));
+      extension.installer->BeginInstall();
+    }
+  }
+
+  return IsImportInProgress();
+}
+
+const ImportingExtension* ExtensionsImporter::GetExtension(
+    const std::string& id) const {
+  auto fnd = std::ranges::find(extensions_, id, &ImportingExtension::id);
+  if (fnd == extensions_.end()) {
+    return nullptr;
+  }
+  return &(*fnd);
+}
+
+bool ExtensionsImporter::IsImportInProgress() const {
+  return in_progress_count_ > 0;
+}
+
+void ExtensionsImporter::OnGetExtensionsForImport(OnReady on_ready,
+                                                  ExtensionsListResult result) {
+  if (!result.has_value()) {
+    return std::move(on_ready).Run(false);
+  }
+  extensions_ = std::move(result).value();
+
+  auto* registry = extensions::ExtensionRegistry::Get(target_profile_);
+  for (auto& extension : extensions_) {
+    extension.is_installed =
+        registry->GetInstalledExtension(extension.id) != nullptr;
+  }
+
+  std::move(on_ready).Run(true);
+}
+
+void ExtensionsImporter::OnExtensionInstalled(
+    ImportingExtension* extension,
+    OnOneExtensionImported on_extension,
+    bool success,
+    const std::string& error,
+    extensions::webstore_install::Result result) {
+  extension->is_installed = success;
+  extension->installer.reset();
+
+  if (!success) {
+    return std::move(on_extension)
+        .Run(extension->id, ExtensionImportStatus::kFailedToInstall);
+  } else if (!extension->has_local_settings) {
+    return std::move(on_extension)
+        .Run(extension->id, ExtensionImportStatus::kOk);
+  }
+
+  CHECK(extension->has_local_settings);
+
+  auto* service =
+      extensions::ExtensionSystem::Get(target_profile_)->extension_service();
+  service->DisableExtension(extension->id,
+                            extensions::disable_reason::DISABLE_RELOAD);
+  ImportExtensionSettings(extension, std::move(on_extension));
+}
+
+void ExtensionsImporter::ImportExtensionSettings(
+    ImportingExtension* extension,
+    OnOneExtensionImported on_extension) {
+  CHECK(extension->has_local_settings);
+  extensions::GetExtensionFileTaskRunner()->PostTaskAndReplyWithResult(
+      FROM_HERE,
+      base::BindOnce(&extensions_import::ImportExtensionSettings, extension->id,
+                     source_profile_, target_profile_->GetPath()),
+      base::BindOnce(&ExtensionsImporter::OnExtensionSettingsImported,
+                     weak_factory_.GetWeakPtr(), extension,
+                     std::move(on_extension)));
+}
+
+void ExtensionsImporter::OnExtensionSettingsImported(
+    ImportingExtension* extension,
+    OnOneExtensionImported on_extension,
+    bool success) {
+  auto* service =
+      extensions::ExtensionSystem::Get(target_profile_)->extension_service();
+  service->EnableExtension(extension->id);
+
+  if (!success) {
+    return std::move(on_extension)
+        .Run(extension->id, ExtensionImportStatus::kFailedToImportSettings);
+  }
+
+  std::move(on_extension).Run(extension->id, ExtensionImportStatus::kOk);
+}
+
+}  // namespace extensions_import
--- browser/importer/extensions_import_helpers.h
+++ browser/importer/extensions_import_helpers.h
@@ -9,16 +9,85 @@
 #include <string>
 #include <vector>
 
-#include "base/files/file_util.h"
-#include "base/values.h"
+#include "base/files/file_path.h"
+#include "base/functional/callback_forward.h"
+#include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
+#include "base/types/expected.h"
+#include "chrome/common/extensions/webstore_install_result.h"
 
-namespace brave {
-void ImportStorages(base::FilePath source_profile,
-                    base::FilePath target_profile,
-                    std::vector<std::string> extensions_ids);
+class Profile;
 
-void RemoveExtensionsSettings(base::FilePath target_profile,
-                              const std::string& extension_id);
-}  // namespace brave
+namespace extensions_import {
+
+struct ImportingExtension {
+  ImportingExtension();
+  ImportingExtension(ImportingExtension&&);
+  ImportingExtension& operator=(ImportingExtension&&);
+  ~ImportingExtension();
+
+  std::string id;
+  bool is_installed = false;
+  bool has_local_settings = false;
+  scoped_refptr<class WebstoreInstallerForImporting> installer;
+};
+
+enum class ExtensionImportStatus : int32_t {
+  kOk = 0,
+  kFailedToInstall,
+  kFailedToImportSettings,
+};
+
+class ExtensionsImporter {
+ public:
+  using OnReady = base::OnceCallback<void(bool ready)>;
+  using OnExtensionImported =
+      base::RepeatingCallback<void(const std::string& id,
+                                   ExtensionImportStatus status)>;
+
+  ExtensionsImporter(const base::FilePath& source_profile,
+                     Profile* target_profile);
+  ~ExtensionsImporter();
+
+  void Prepare(OnReady on_ready);
+  bool Import(OnExtensionImported on_extension);
+
+  const ImportingExtension* GetExtension(const std::string& id) const;
+  bool IsImportInProgress() const;
+
+  static base::RepeatingCallback<
+      ExtensionImportStatus(const std::string& extension_id)>&
+  GetExtensionInstallerForTesting();
+
+ private:
+  using ExtensionsListResult =
+      base::expected<std::vector<ImportingExtension>, bool>;
+  using OnOneExtensionImported =
+      base::OnceCallback<void(const std::string& id,
+                              ExtensionImportStatus status)>;
+
+  void OnGetExtensionsForImport(OnReady on_ready, ExtensionsListResult result);
+  void OnExtensionInstalled(ImportingExtension* extension,
+                            OnOneExtensionImported on_extension,
+                            bool success,
+                            const std::string& error,
+                            extensions::webstore_install::Result result);
+
+  void ImportExtensionSettings(ImportingExtension* extension,
+                               OnOneExtensionImported on_extension);
+  void OnExtensionSettingsImported(ImportingExtension* extension,
+                                   OnOneExtensionImported on_extension,
+                                   bool success);
+
+  base::FilePath source_profile_;
+  raw_ptr<Profile> target_profile_ = nullptr;
+
+  std::vector<ImportingExtension> extensions_;
+  size_t in_progress_count_ = 0;
+
+  base::WeakPtrFactory<ExtensionsImporter> weak_factory_{this};
+};
+
+}  // namespace extensions_import
 
 #endif  // BRAVE_BROWSER_IMPORTER_EXTENSIONS_IMPORT_HELPERS_H_
