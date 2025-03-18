5b0d95a517ba33ded929b6e6958918f3c295fc40
deeppandya <deeppandya91@gmail.com>
2025-03-18 12:18:02 -0400

    Add delete search engine confirmation dialog

--- browser/search_engines/android/java/src/org/chromium/brave/browser/search_engines/settings/CustomSearchEnginesPreference.java
+++ browser/search_engines/android/java/src/org/chromium/brave/browser/search_engines/settings/CustomSearchEnginesPreference.java
@@ -5,9 +5,16 @@
 
 package org.chromium.brave.browser.search_engines.settings;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.preference.Preference;
@@ -84,14 +91,64 @@ public class CustomSearchEnginesPreference extends Preference
 
     @Override
     public void removeSearchEngine(String searchEngineKeyword) {
-        Runnable templateUrlServiceReady =
-                () -> {
-                    Log.e("brave_search", "removeSearchEngine");
-                    ((BraveTemplateUrlService) TemplateUrlServiceFactory.getForProfile(mProfile))
-                            .removeSearchEngine(searchEngineKeyword);
-                    CustomSearchEnginesUtil.removeCustomSearchEngine(searchEngineKeyword);
-                    updateCustomSearchEngines();
-                };
-        TemplateUrlServiceFactory.getForProfile(mProfile).runWhenLoaded(templateUrlServiceReady);
+        showConfirmDeleteSearchEngineDialog(searchEngineKeyword);
+    }
+
+    private void showConfirmDeleteSearchEngineDialog(String searchEngineKeyword) {
+        final Dialog dialog = createDialog();
+        setupDialogViews(dialog, searchEngineKeyword);
+        setupDialogButtons(dialog, searchEngineKeyword);
+        showDialog(dialog);
+    }
+
+    private Dialog createDialog() {
+        final Dialog dialog = new Dialog(getContext());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setContentView(R.layout.custom_search_engine_alert_dialog_layout);
+        return dialog;
+    }
+
+    private void setupDialogViews(Dialog dialog, String searchEngineKeyword) {
+        TextView titleTextView = dialog.findViewById(R.id.dialogTitle);
+        TextView messageTextView = dialog.findViewById(R.id.dialogMessage);
+
+        titleTextView.setText(getContext().getString(R.string.delete_custom_search_engine_title));
+        messageTextView.setText(
+                getContext()
+                        .getString(R.string.delete_custom_search_engine_text, searchEngineKeyword));
+    }
+
+    private void setupDialogButtons(Dialog dialog, String searchEngineKeyword) {
+        Button positiveButton = dialog.findViewById(R.id.positiveButton);
+        Button negativeButton = dialog.findViewById(R.id.negativeButton);
+
+        positiveButton.setText(getContext().getString(R.string.delete_action_text));
+        negativeButton.setText(getContext().getString(R.string.cancel_action_text));
+
+        positiveButton.setOnClickListener(
+                v -> {
+                    Runnable templateUrlServiceReady =
+                            () -> {
+                                Log.e("brave_search", "removeSearchEngine");
+                                ((BraveTemplateUrlService)
+                                                TemplateUrlServiceFactory.getForProfile(mProfile))
+                                        .removeSearchEngine(searchEngineKeyword);
+                                CustomSearchEnginesUtil.removeCustomSearchEngine(
+                                        searchEngineKeyword);
+                                updateCustomSearchEngines();
+                            };
+                    TemplateUrlServiceFactory.getForProfile(mProfile)
+                            .runWhenLoaded(templateUrlServiceReady);
+                    dialog.dismiss();
+                });
+
+        negativeButton.setOnClickListener(v -> dialog.dismiss());
+    }
+
+    private void showDialog(Dialog dialog) {
+        dialog.show();
+        Window window = dialog.getWindow();
+        window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
     }
 }
