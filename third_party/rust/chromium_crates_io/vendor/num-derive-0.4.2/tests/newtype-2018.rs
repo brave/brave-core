9155e6fa79ad24490991e2eca18ce3103c434419
Anton Paymyshev <anton.paymyshev@gmail.com>
2025-03-20 18:55:14 +0700

    remove rust deps

--- third_party/rust/chromium_crates_io/vendor/num-derive-0.4.2/tests/newtype-2018.rs
+++
@@ -1,2 +0,0 @@
-// Same source, just compiled for 2018 edition
-include!("newtype.rs");
--- third_party/rust/chromium_crates_io/vendor/toml_edit-0.22.22/tests/testsuite/main.rs
+++
@@ -1,9 +0,0 @@
-#![recursion_limit = "256"]
-
-mod convert;
-mod datetime;
-mod edit;
-mod float;
-mod invalid;
-mod parse;
-mod stackoverflow;
