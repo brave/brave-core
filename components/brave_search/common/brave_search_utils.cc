a63a2051ffe2d72c9c7766d8bdeb88cde3fba07c
bridiver <34129+bridiver@users.noreply.github.com>
2025-03-21 16:48:45 -0700

    make origins with privilidged js APIs use dedicated process

--- components/brave_search/common/brave_search_utils.cc
+++ components/brave_search/common/brave_search_utils.cc
@@ -5,32 +5,11 @@
 
 #include "brave/components/brave_search/common/brave_search_utils.h"
 
-#include <string>
-#include <string_view>
-#include <vector>
-
 #include "base/containers/contains.h"
-#include "base/containers/fixed_flat_set.h"
 #include "base/feature_list.h"
 #include "brave/components/brave_search/common/features.h"
 #include "url/gurl.h"
 #include "url/url_constants.h"
-
-namespace {
-
-constexpr auto kVettedHosts =
-    base::MakeFixedFlatSet<std::string_view>(base::sorted_unique,
-                                             {
-                                                 "safesearch.brave.com",
-                                                 "safesearch.brave.software",
-                                                 "safesearch.bravesoftware.com",
-                                                 "search-dev-local.brave.com",
-                                                 "search.brave.com",
-                                                 "search.brave.software",
-                                                 "search.bravesoftware.com",
-                                             });
-
-}  // namespace
 
 namespace brave_search {
 
--- components/brave_search/common/brave_search_utils.h
+++ components/brave_search/common/brave_search_utils.h
@@ -6,9 +6,25 @@
 #ifndef BRAVE_COMPONENTS_BRAVE_SEARCH_COMMON_BRAVE_SEARCH_UTILS_H_
 #define BRAVE_COMPONENTS_BRAVE_SEARCH_COMMON_BRAVE_SEARCH_UTILS_H_
 
+#include <string_view>
+
+#include "base/containers/fixed_flat_set.h"
+
 class GURL;
 
 namespace brave_search {
+
+inline constexpr auto kVettedHosts =
+    base::MakeFixedFlatSet<std::string_view>(base::sorted_unique,
+                                             {
+                                                 "safesearch.brave.com",
+                                                 "safesearch.brave.software",
+                                                 "safesearch.bravesoftware.com",
+                                                 "search-dev-local.brave.com",
+                                                 "search.brave.com",
+                                                 "search.brave.software",
+                                                 "search.bravesoftware.com",
+                                             });
 
 bool IsAllowedHost(const GURL& url);
 bool IsDefaultAPIEnabled();
