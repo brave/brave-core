c8f4175825463f74bf2c82b87bf98ba82dd3ffc1
Emerick Rogul <erogul@brave.com>
2025-03-24 16:18:03 -0400

    Fix chromium_src override for SavedTabGroupButton
    Due to upstream refactoring changes for SavedTabGroupButton, we need to override
    the header file and unit test module.
    
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/6c693fa66eb3c9e9756faa89cf5ab34e8034987f
    
    commit 6c693fa66eb3c9e9756faa89cf5ab34e8034987f
    Author: David Pennington <dpenning@chromium.org>
    Date:   Wed Mar 19 22:54:48 2025 -0700
    
        Refactor SavedTabGroupBar to remove OverflowMenu
    
        This CL is prework for Fixing some browser hangs. The saved tab group
        bar is bit rotting, and in order to fix overquerying we need to clean
        it up. Before doing that we will remove dead code and hacks put in place
        when there were multiple overflow buttons.
    
        - Fixed an issue with the TGSSProxy not sending the same observer
          commands as the TGSService OnInitialized. There was special case
          logic to preemptively populate since there was no notification that
          it could load data.
        - Remove Hide/Show overflow menu, they are no longer used
        - Remove OverflowMenu, it's no longer used
        - Remove special case'd everything menu sizing, and use margins for
          button sizing
    
        Bug: 398856721

---
+++ chromium_src/chrome/browser/ui/views/bookmarks/saved_tab_groups/saved_tab_group_bar.h
@@ -0,0 +1,15 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_BOOKMARKS_SAVED_TAB_GROUPS_SAVED_TAB_GROUP_BAR_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_BOOKMARKS_SAVED_TAB_GROUPS_SAVED_TAB_GROUP_BAR_H_
+
+#define SavedTabGroupButton BraveSavedTabGroupButton
+
+#include "src/chrome/browser/ui/views/bookmarks/saved_tab_groups/saved_tab_group_bar.h"  // IWYU pragma: export
+
+#undef SavedTabGroupButton
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_BOOKMARKS_SAVED_TAB_GROUPS_SAVED_TAB_GROUP_BAR_H_
