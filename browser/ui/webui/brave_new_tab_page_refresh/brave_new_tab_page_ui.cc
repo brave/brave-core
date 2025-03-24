7bcf0d8b5b6cc4bcf7669f72d0b1daeac8d18368
Claudio DeSouza <cdesouza@brave.com>
2025-03-24 19:08:25 +0000

    [cr136] `tab_interface.h` moved under `tab_collections/public/`
    This only affects path inclusions.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/5353fe504b1d43b8f4772621e4d87cabccf929aa
    
    commit 5353fe504b1d43b8f4772621e4d87cabccf929aa
    Author: Calder Kitagawa <ckitagawa@chromium.org>
    Date:   Fri Mar 21 18:58:17 2025 -0700
    
        [Tab Collections] Move TabInterface to tab_collections/
    
        Move tab_interface.h to tab_collections/. TabInterface will soon be
        shared with Android to facilitate sharing TabCollections. Some
        functionality of TabInterface is not applicable on Android so it is
        disabled via buildflags.
    
        Bug: 404296389

--- browser/ui/webui/brave_new_tab_page_refresh/brave_new_tab_page_ui.cc
+++ browser/ui/webui/brave_new_tab_page_refresh/brave_new_tab_page_ui.cc
@@ -28,8 +28,8 @@
 #include "chrome/browser/ntp_tiles/chrome_most_visited_sites_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
-#include "chrome/browser/ui/tabs/public/tab_interface.h"
 #include "chrome/browser/ui/webui/searchbox/realbox_handler.h"
+#include "components/tab_collections/public/tab_interface.h"
 
 #if BUILDFLAG(ENABLE_BRAVE_VPN)
 #include "brave/browser/brave_vpn/brave_vpn_service_factory.h"
