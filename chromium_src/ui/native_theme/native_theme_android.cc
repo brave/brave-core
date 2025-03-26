5375ca7c0c6065935c3ad7240311be4357333b35
brave-builds <brave-builds+devops@brave.com>
2025-03-26 03:47:05 +0000

    Uplift of #28313 (squashed) to beta

--- chromium_src/ui/native_theme/native_theme_android.cc
+++ chromium_src/ui/native_theme/native_theme_android.cc
@@ -3,7 +3,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+#include "ui/native_theme/native_theme_android.h"
+
+#define GetInstanceForNativeUi GetInstanceForNativeUi_UnUsed
 #include "src/ui/native_theme/native_theme_android.cc"
+#undef GetInstanceForNativeUi
 
 ui::NativeTheme* ui::NativeTheme::BraveGetInstanceForNativeUi() {
   struct StubNativeThemeAndroid : public ui::NativeThemeAndroid {
@@ -11,4 +15,8 @@ ui::NativeTheme* ui::NativeTheme::BraveGetInstanceForNativeUi() {
   };
   static base::NoDestructor<StubNativeThemeAndroid> s_native_theme;
   return s_native_theme.get();
+}
+
+ui::NativeTheme* ui::NativeTheme::GetInstanceForNativeUi() {
+  return BraveGetInstanceForNativeUi();
 }
