cfb5126aa44d9895127339a78baf0cf1104d3bb4
boocmp <boocmp@yandex.ru>
2025-03-26 20:00:52 +0700

    Added description about shields cookies.

--- browser/resources/settings/brave_overrides/cookies_page.ts
+++ browser/resources/settings/brave_overrides/cookies_page.ts
@@ -3,35 +3,48 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // you can obtain one at https://mozilla.org/MPL/2.0/.
 
-import {RegisterPolymerTemplateModifications} from 'chrome://resources/brave/polymer_overriding.js'
+import {
+  html,
+  RegisterPolymerComponentBehaviors,
+  RegisterPolymerTemplateModifications
+} from 'chrome://resources/brave/polymer_overriding.js'
+
+import { loadTimeData } from '../i18n_setup.js'
+
+import { SettingsCookiesPageElement } from '../privacy_page/cookies_page.js'
 
 RegisterPolymerTemplateModifications({
   'settings-cookies-page': (templateContent) => {
-    const isNot3pcdRedesignEnabledTemplate = templateContent.
-      querySelector(
-        'template[if*="!is3pcdRedesignEnabled_"]'
-      )
+    const isNot3pcdRedesignEnabledTemplate = templateContent.querySelector(
+      'template[if*="!is3pcdRedesignEnabled_"]'
+    )
     if (!isNot3pcdRedesignEnabledTemplate) {
       console.error(
         '[Brave Settings Overrides] Could not find template with ' +
-        'if*=!is3pcdRedesignEnabledTemplate on cookies page.')
+          'if*=!is3pcdRedesignEnabledTemplate on cookies page.'
+      )
     } else {
       const blockThirdPartyIncognitoRadioButton =
-        isNot3pcdRedesignEnabledTemplate.content.
-          getElementById('blockThirdPartyIncognito')
+        isNot3pcdRedesignEnabledTemplate.content.getElementById(
+          'blockThirdPartyIncognito'
+        )
       if (!blockThirdPartyIncognitoRadioButton) {
         console.error(
           '[Brave Settings Overrides] Could not find ' +
-          'blockThirdPartyIncognito id on cookies page.')
+            'blockThirdPartyIncognito id on cookies page.'
+        )
       } else {
         blockThirdPartyIncognitoRadioButton.setAttribute('hidden', 'true')
       }
-      const generalControls = isNot3pcdRedesignEnabledTemplate.content.
-          getElementById('generalControls')
+      const generalControls =
+        isNot3pcdRedesignEnabledTemplate.content.getElementById(
+          'generalControls'
+        )
       if (!generalControls) {
         console.error(
           '[Brave Settings Overrides] Could not find generalControls id ' +
-          'on cookies page.')
+            'on cookies page.'
+        )
       } else {
         generalControls.setAttribute('hidden', 'true')
       }
@@ -40,16 +53,47 @@ RegisterPolymerTemplateModifications({
     if (!siteDataTrigger) {
       console.error(
         '[Brave Settings Overrides] Could not find site-data-trigger id ' +
-        'on cookies page')
+          'on cookies page'
+      )
     } else {
       siteDataTrigger.setAttribute('hidden', 'true')
     }
     const doNotTrackToggle = templateContent.getElementById('doNotTrack')
     if (!doNotTrackToggle) {
       console.error(
-        '[Brave Settings Overrides] Could not find toggle id on cookies page')
+        '[Brave Settings Overrides] Could not find toggle id on cookies page'
+      )
     } else {
       doNotTrackToggle.setAttribute('hidden', 'true')
     }
   }
+})
+
+const BraveSettingsCookiePageBehavior = {
+  ready: function (this: SettingsCookiesPageElement) {
+    const siteList = this.shadowRoot!.getElementById('allow3pcExceptionsList')
+    if (!siteList) {
+      console.error(
+        '[Brave Settings Overrides] Could not find allow3pcExceptionsList'
+      )
+      return
+    }
+    const listHeader = siteList.shadowRoot!.getElementById('listHeader')
+    if (!listHeader) {
+      console.error(
+        '[Brave Settings Overrides] Could not find allow3pcExceptionsList'
+      )
+      return
+    }
+    const wrapper = document.createElement('div')
+    listHeader.parentNode!.insertBefore(wrapper, listHeader)
+    wrapper.appendChild(listHeader)
+    wrapper.appendChild(
+      html`<b>${loadTimeData.getString('cookieControlledByShieldsHeader')}</b>`
+    )
+  }
+}
+
+RegisterPolymerComponentBehaviors({
+  'settings-cookies-page': [BraveSettingsCookiePageBehavior]
 })
