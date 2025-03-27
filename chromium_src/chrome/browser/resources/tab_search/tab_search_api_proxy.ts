a144575178e49ce8114544e66032f425fdad0e14
Douglas Daniel <douglashdaniel@gmail.com>
2025-03-27 16:07:42 -0500

    Fix Illustration Resource

--- chromium_src/chrome/browser/resources/tab_search/tab_search_api_proxy.ts
+++ chromium_src/chrome/browser/resources/tab_search/tab_search_api_proxy.ts
@@ -12,6 +12,7 @@ export interface BraveTabSearchApiProxy extends TabSearchApiProxy {
   getFocusTabs: (topic: string) => Promise<{ windowCreated: boolean, error: Error | null }>
   undoFocusTabs: () => Promise<void>
   openLeoGoPremiumPage: () => void
+  enableTabFocus: (enable: boolean) => void
 }
 
 export class BraveTabSearchApiProxyImpl extends TabSearchApiProxyImpl implements BraveTabSearchApiProxy {
@@ -29,6 +30,10 @@ export class BraveTabSearchApiProxyImpl extends TabSearchApiProxyImpl implements
 
   openLeoGoPremiumPage() {
     this.handler.openLeoGoPremiumPage()
+  }
+
+  enableTabFocus(enable: boolean) {
+    this.handler.enableTabFocus(enable)
   }
 }
 
