9ad3d1102a613b59c1bfab5afdb4ee7d7a3209dd
Szilard Szaloki <szilardszaloki@gmail.com>
2025-03-27 17:55:27 -0600

    Makes `BraveAccountHandler` a Mojom page handler.

--- browser/resources/settings/getting_started_page/brave_account_browser_proxy.ts
+++ browser/resources/settings/getting_started_page/brave_account_browser_proxy.ts
@@ -3,18 +3,31 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-import { sendWithPromise } from '//resources/js/cr.js'
+ import {
+  BraveAccountClientCallbackRouter,
+  BraveAccountHandlerFactory,
+  BraveAccountHandlerRemote
+} from '../brave_account.mojom-webui.js'
+import type { BraveAccountHandlerInterface } from '../brave_account.mojom-webui.js'
 
 export interface BraveAccountBrowserProxy {
-  getPasswordStrength(password: string): Promise<number>
+  callbackRouter: BraveAccountClientCallbackRouter;
+  handler: BraveAccountHandlerInterface;
 }
 
 export class BraveAccountBrowserProxyImpl implements BraveAccountBrowserProxy {
-  getPasswordStrength (password: string) {
-    return sendWithPromise('getPasswordStrength', password)
+  callbackRouter: BraveAccountClientCallbackRouter;
+  handler: BraveAccountHandlerInterface;
+
+  private constructor() {
+    this.callbackRouter = new BraveAccountClientCallbackRouter();
+    this.handler = new BraveAccountHandlerRemote();
+    BraveAccountHandlerFactory.getRemote().createBraveAccountHandler(
+        (this.handler as BraveAccountHandlerRemote).$.bindNewPipeAndPassReceiver(),
+        this.callbackRouter.$.bindNewPipeAndPassRemote());
   }
 
-  static getInstance() {
+  static getInstance(): BraveAccountBrowserProxy {
     return instance || (instance = new BraveAccountBrowserProxyImpl())
   }
 }
--- browser/ui/webui/settings/brave_account_handler.cc
+++ browser/ui/webui/settings/brave_account_handler.cc
@@ -5,29 +5,21 @@
 
 #include "brave/browser/ui/webui/settings/brave_account_handler.h"
 
-#include <string>
+#include <utility>
 
-#include "base/check_op.h"
-#include "base/functional/bind.h"
 #include "components/password_manager/core/browser/ui/weak_check_utility.h"
 
-BraveAccountHandler::BraveAccountHandler() = default;
+namespace brave_account {
+BraveAccountHandler::BraveAccountHandler(
+    mojo::PendingReceiver<mojom::BraveAccountHandler> handler,
+    mojo::PendingRemote<mojom::BraveAccountClient> client)
+    : handler_(this, std::move(handler)), client_(std::move(client)) {}
 
 BraveAccountHandler::~BraveAccountHandler() = default;
 
-void BraveAccountHandler::RegisterMessages() {
-  web_ui()->RegisterMessageCallback(
-      "getPasswordStrength",
-      base::BindRepeating(&BraveAccountHandler::GetPasswordStrength,
-                          base::Unretained(this)));
+void BraveAccountHandler::GetPasswordStrength(
+    const std::string& password,
+    mojom::BraveAccountHandler::GetPasswordStrengthCallback callback) {
+  std::move(callback).Run(password_manager::GetPasswordStrength(password));
 }
-
-void BraveAccountHandler::GetPasswordStrength(const base::Value::List& args) {
-  CHECK_EQ(args.size(), 2U);
-  auto* password = args[1].GetIfString();
-  CHECK(password);
-
-  AllowJavascript();
-  ResolveJavascriptCallback(
-      args[0], base::Value(password_manager::GetPasswordStrength(*password)));
-}
+}  // namespace brave_account
