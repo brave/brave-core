77693d46056d99a5f52b85044cee3d3247569c81
Jay Harris <jay.harris@outlook.co.nz>
2025-04-01 14:39:04 +1300

    [PWA]: Change the default button to install (#28419)

---
+++ chromium_src/chrome/browser/ui/views/web_apps/web_app_diy_install_dialog.cc
@@ -0,0 +1,14 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "ui/base/models/dialog_model.h"
+
+// Change the default button from kCancel to kOk
+#define OverrideDefaultButton(__VA_ARGS__) \
+  OverrideDefaultButton(ui::mojom::DialogButton::kOk)
+
+#include "src/chrome/browser/ui/views/web_apps/web_app_diy_install_dialog.cc"
+
+#undef OverrideDefaultButton
