c864d4ba2f739f5bf1c8391ab4c5101bd98d3b51
Claudio DeSouza <cdesouza@brave.com>
2025-04-01 21:23:00 +0100

    [canary] Add PR creation to B🚀
    This PR adds support for PR creation in Brockit. This will be useful in
    the CI job doing minor bumps, as PR creation is always desirable. To
    have an upstream branch is a requirement with this, as one needs a base
    branch to create a PR with.
    
    Resolves https://github.com/brave/brave-browser/issues/45126

--- tools/cr/brockit.py
+++ tools/cr/brockit.py
@@ -111,6 +111,11 @@ However it is also possible to run this task on standalone as well.
 tools/cr/brockit.py update-version-issue
 ````
 
+Different from a lift using `--with-github`, the command above will
+also attempt to create a PR, if none exists, however it is necessary that the
+current branch has an upstream branch set, as creating a PR involves having an
+upstream branch.
+
 ### Infra mode
 When running on infra, the `--infra-mode` flag should be provided. This will
 suppress all status updates, and rather provide a keep-alive type of feedback
@@ -986,8 +991,7 @@ class PatchFailureResolver:
         """
         for _, patches in self.patch_files.items():
             for patch in patches:
-                if (ignore_deleted_files is True
-                        and not Path(patch.path).exists()):
+                if (ignore_deleted_files and not Path(patch.path).exists()):
                     # Skip deleted files.
                     continue
 
@@ -1140,12 +1144,15 @@ class GitHubIssue(Versioned):
     def status_message(self):
         return "Creating/Updating GitHub issue for upgrade..."
 
-    def create_or_updade_version_issue(self):
-        """Creates a github issue for the upgrade.
+    def compose_issue_title(self):
+        """Generates the title for the upgrade issue.
 
-        This function creates/updates the upgrade github issue.
+        This function generates the title for the upgrade issue, based on the
+        base and target version. The title is generated in a way that it can be
+        used for both major and minor upgrades.
+
+        This title is the same used for the push request.
         """
-
         title = 'Upgrade from Chromium {previous} to Chromium {to}'
         if self.target_version.major > self.base_version.major:
             # For major updates, the issue description doesn't have a precise
@@ -1155,18 +1162,104 @@ class GitHubIssue(Versioned):
         else:
             title = title.format(previous=str(self.base_version),
                                  to=str(self.target_version))
+        return title
 
-        link = self.target_version.get_googlesource_diff_link(
-            from_version=str(self.base_version))
+    def lookup_issue(self, title: str) -> Optional[List]:
+        """Looks up the issue for the upgrade.
 
+        This function checks if there's already an issue with the title
+        provided, and returns its details.
+        """
         results = json.loads(
             terminal.run([
                 'gh', 'issue', 'list', '--repo', 'brave/brave-browser',
                 '--search', title, '--state', 'open', '--json',
                 'number,title,url,body'
             ]).stdout.strip())
-        issue = next((entry for entry in results if entry['title'] == title),
-                     None)
+        return next((entry for entry in results if entry['title'] == title),
+                    None)
+
+    def create_push_request(self, issue_url: str) -> bool:
+        """Creates a push request for the current branch.
+
+        This function creates a push request for the upgrade.
+        """
+        current_branch = Repository.brave().current_branch()
+        if current_branch == 'HEAD':
+            logging.error('Cannot create a push request: Not in a branch')
+            return False
+
+        # It is assumed that the upstream branch is the base branch for the PR.
+        upstream_branch = _get_current_branch_upstream_name()
+        if upstream_branch is None:
+            logging.error(
+                'Cannot create a push request: Not in a branch with an upstream'
+            )
+            return False
+        if '/' in upstream_branch:
+            # removing the remote portion.
+            upstream_branch = upstream_branch.split("/", 1)[-1]
+
+        results = json.loads(
+            terminal.run([
+                'gh', 'pr', 'list', '--head', current_branch,
+                '--json=number,url'
+            ]).stdout.strip())
+        if results:
+            console.log(
+                f'The push request for {current_branch} is: {results[0]["url"]}'
+            )
+            return True
+
+        tag = f'[{upstream_branch}] ' if upstream_branch != 'master' else ''
+        cmd = [
+            'gh', 'pr', 'create', '--base', upstream_branch, '--head',
+            current_branch, '--title', f'{tag}{self.compose_issue_title()}',
+            '--body', f'Resolves {issue_url}', '--label',
+            '"CI/run-audit-deps"', '--label', '"CI/run-network-audit"',
+            '--assignee', 'cdesouza-chromium'
+        ]
+        # Emerick and Alexey take care of even-numbered major versions, while
+        # Max and Sam do the odd ones.
+        if self.target_version.major % 2 == 0:
+            cmd += ['--assignee', 'emerick', '--assignee', 'AlexeyBarabash']
+        else:
+            cmd += ['--assignee', 'mkarolin', '--assignee', 'samartnik']
+
+        is_major = self.target_version.major > self.base_version.major
+
+        if is_major or upstream_branch == 'master':
+            # It is not common for upstream test to be run on uplifts of minor
+            # upgrades.
+            cmd += ['--label', '"CI/run-upstream-tests"']
+
+        if is_major:
+            cmd += ['--label', '"CI/storybook-url"']
+
+            # Always create PR for major version upgrades as draft
+            cmd.append('--draft')
+
+        try:
+            pr_url = terminal.run(cmd).stdout.strip()
+            terminal.log_task(f'GitHub PR created for this bump: {pr_url}')
+        except subprocess.CalledProcessError as e:
+            console.log(
+                f'Failed to create PR for {current_branch}: {e.stderr.strip()}'
+            )
+            return False
+
+        return True
+
+    def create_or_update_version_issue(self, with_pr: bool) -> bool:
+        """Creates a github issue for the upgrade.
+
+        This function creates/updates the upgrade github issue.
+        """
+        link = self.target_version.get_googlesource_diff_link(
+            from_version=str(self.base_version))
+
+        title = self.compose_issue_title()
+        issue = self.lookup_issue(title)
         if issue is not None:
             pattern = r"https://chromium\.googlesource\.com/chromium/src/\+log/[^\s]+"
             body = re.sub(pattern, link, issue['body'])
@@ -1180,8 +1273,10 @@ class GitHubIssue(Versioned):
                     str(issue['number']), '--repo', 'brave/brave-browser',
                     '--body', f'{body}'
                 ])
-                terminal.log_task(f'GitHub issue udpated {str(issue["url"])}.')
-            return
+                terminal.log_task(f'GitHub issue updated {str(issue["url"])}.')
+            if with_pr:
+                return self.create_push_request(issue["url"])
+            return True
 
         body = MINOR_VERSION_BUMP_ISSUE_TEMPLATE.format(
             googlesource_log_link=link)
@@ -1196,14 +1291,16 @@ class GitHubIssue(Versioned):
         ]).stdout.strip()
         terminal.log_task(f'GitHub Issue created for this bump: {issue_url}')
 
+        if with_pr:
+            return self.create_push_request(issue_url)
+        return True
+
     def execute(self) -> bool:
         if not _is_gh_cli_logged_in():
             logging.error('GitHub CLI is not logged in.')
             return False
 
-        self.create_or_updade_version_issue()
-        return True
-
+        return self.create_or_update_version_issue(with_pr=True)
 
 class ReUpgrade(Task):
     """Restarts the upgrade process.
@@ -1358,12 +1455,12 @@ class Upgrade(Versioned):
         terminal.run_npm_command('update_patches')
 
         status = GitStatus()
-        if status.has_deleted_patch_files() is True:
+        if status.has_deleted_patch_files():
             logging.error(
                 'Deleted patches detected. These should be committed as their '
                 'own changes:\n%s' % '\n'.join(status.deleted))
             return False
-        if status.has_untracked_patch_files() is True:
+        if status.has_untracked_patch_files():
             logging.error(
                 'Untracked patch files detected. These should be committed as '
                 'their own changes:\n%s' % '\n'.join(status.untracked))
@@ -1631,19 +1728,19 @@ class Upgrade(Versioned):
             Indicates that a continuation does not produce a conflict-resolved
             change.
         """
-        if no_conflict_continuation is not True:
+        if not no_conflict_continuation:
             # There's no need to try to create a `conflict-resolved` commit if
             # all changes have already been committed during the run's break.
             resolver = PatchFailureResolver(
                 ContinuationFile.load(self.target_version))
 
-            if resolver.requires_conflict_resolution() is True:
-                if self._run_update_patches_with_no_deletions() is not True:
+            if resolver.requires_conflict_resolution():
+                if not self._run_update_patches_with_no_deletions():
                     return False
 
             resolver.stage_all_patches(ignore_deleted_files=True)
 
-            if Repository.brave().has_staged_changed() is False:
+            if not Repository.brave().has_staged_changed():
                 logging.error(
                     'Nothing has been staged to commit conflict-resolved '
                     'patches.')
@@ -1716,7 +1813,7 @@ class Upgrade(Versioned):
 
             # When no conflicts come back, we can proceed with the
             # update_patches.
-            if self._run_update_patches_with_no_deletions() is not True:
+            if not self._run_update_patches_with_no_deletions():
                 return False
         except subprocess.CalledProcessError as e:
             if ('There were some failures during git reset of specific '
@@ -1732,7 +1829,7 @@ class Upgrade(Versioned):
                 resolver = PatchFailureResolver()
                 resolver.apply_patches_3way(target_version=self.target_version,
                                             launch_vscode=launch_vscode)
-                if resolver.requires_conflict_resolution() is True:
+                if resolver.requires_conflict_resolution():
                     # Manual resolution required.
                     console.log(
                         '👋 (Address all sections with '
@@ -1741,7 +1838,7 @@ class Upgrade(Versioned):
                         '--continue[/])')
                     return False
 
-                if self._run_update_patches_with_no_deletions() is not True:
+                if not self._run_update_patches_with_no_deletions():
                     return False
 
                 resolver.stage_all_patches()
@@ -1818,7 +1915,7 @@ class Upgrade(Versioned):
                              working_version=self.working_version,
                              base_version=self.base_version).save()
 
-        if with_github is True and _is_gh_cli_logged_in() is False:
+        if with_github and not _is_gh_cli_logged_in():
             # Fail early if gh cli is not logged in.
             logging.error('GitHub CLI is not logged in.')
             return False
@@ -1838,10 +1935,10 @@ class Upgrade(Versioned):
             result = self._start(launch_vscode=launch_vscode,
                                  ack_advisory=ack_advisory)
 
-        if result is True and with_github is True:
+        if result and with_github:
             GitHubIssue(base_version=self.base_version,
                         target_version=self.target_version
-                        ).create_or_updade_version_issue()
+                        ).create_or_update_version_issue(with_pr=False)
 
         return result
 
