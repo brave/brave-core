19661cb293e2977c4051c721516f401b5ce508da
Francois Marier <francois@brave.com>
2025-04-01 17:03:28 -0700

    Add more test cases

--- chromium_src/components/autofill/core/browser/foundations/browser_autofill_manager_unittest.cc
+++ chromium_src/components/autofill/core/browser/foundations/browser_autofill_manager_unittest.cc
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "components/autofill/core/browser/foundations/browser_autofill_manager_unittest.cc"
+#include "src/components/autofill/core/browser/foundations/browser_autofill_manager_unittest.cc"
 
 namespace autofill {
 namespace {
@@ -29,13 +29,30 @@ TEST_F(BrowserAutofillManagerTest, Onion_MixedForm1) {
                   SuggestionType::kMixedFormMessage)});
 }
 
-// Test that if a form is mixed content we show a warning instead of any
-// suggestions. A .onion hostname is considered secure.
 TEST_F(BrowserAutofillManagerTest, Onion_MixedForm2) {
   // Set up our form data.
   FormData form;
   form.set_name(u"MyForm");
   form.set_url(GURL("http://myform.onion/form.html"));
+  form.set_action(GURL("http://myform.com/submit.html"));
+  form.set_fields({CreateTestFormField("Name on Card", "nameoncard", "",
+                                       FormControlType::kInputText)});
+
+  OnAskForValuesToFill(form, form.fields()[0]);
+
+  // Test that we sent the right values to the external delegate.
+  external_delegate()->CheckSuggestions(
+      form.fields().back().global_id(),
+      {Suggestion(l10n_util::GetStringUTF8(IDS_AUTOFILL_WARNING_MIXED_FORM), "",
+                  Suggestion::Icon::kNoIcon,
+                  SuggestionType::kMixedFormMessage)});
+}
+
+TEST_F(BrowserAutofillManagerTest, Onion_MixedForm3) {
+  // Set up our form data.
+  FormData form;
+  form.set_name(u"MyForm");
+  form.set_url(GURL("http://a.myform.onion/form.html"));
   form.set_action(GURL("http://myform.com/submit.html"));
   form.set_fields({CreateTestFormField("Name on Card", "nameoncard", "",
                                        FormControlType::kInputText)});
--- chromium_src/components/security_interstitials/core/insecure_form_util_unittest.cc
+++ chromium_src/components/security_interstitials/core/insecure_form_util_unittest.cc
@@ -3,14 +3,32 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "components/security_interstitials/core/insecure_form_util_unittest.cc"
+#include "src/components/security_interstitials/core/insecure_form_util_unittest.cc"
 
 TEST_F(InsecureFormUtilTest, IsInsecureFormActionOnOnionSource) {
+  // Should work even without special-casing .onion
+  EXPECT_TRUE(IsInsecureFormActionOnSecureSource(
+      url::Origin::Create(GURL("https://foo.onion")),
+      GURL("http://example.com")));
+  EXPECT_FALSE(IsInsecureFormActionOnSecureSource(
+      url::Origin::Create(GURL("http://foo.onion")),
+      GURL("https://example.com")));
+
+  // Basic case
   EXPECT_TRUE(IsInsecureFormActionOnSecureSource(
       url::Origin::Create(GURL("http://foo.onion")),
       GURL("http://example.com")));
 
+  // Subdomains
   EXPECT_TRUE(IsInsecureFormActionOnSecureSource(
-      url::Origin::Create(GURL("https://foo.onion")),
+      url::Origin::Create(GURL("http://foo.bar.onion")),
+      GURL("http://example.com")));
+
+  // Non-onion URLs
+  EXPECT_FALSE(IsInsecureFormActionOnSecureSource(
+      url::Origin::Create(GURL("http://foo.onion.com")),
+      GURL("http://example.com")));
+  EXPECT_FALSE(IsInsecureFormActionOnSecureSource(
+      url::Origin::Create(GURL("http://foo.baronion")),
       GURL("http://example.com")));
 }
