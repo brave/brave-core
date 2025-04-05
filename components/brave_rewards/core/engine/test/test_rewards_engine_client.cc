5f959942fc73a8df4086a526e82d3f34edca9946
Claudio DeSouza <cdesouza@brave.com>
2025-04-05 02:15:32 +0100

    Revert "Reapply "[cr136] `GetVlogLevelHelper` takes a `string_view`""
    This reverts commit f642c21caa97ab0c9f5a0ce9c12e07bd81a5e63d.

--- components/brave_rewards/core/engine/test/test_rewards_engine_client.cc
+++ components/brave_rewards/core/engine/test/test_rewards_engine_client.cc
@@ -208,7 +208,8 @@ void TestRewardsEngineClient::Log(const std::string& file,
                                   int32_t line,
                                   int32_t verbose_level,
                                   const std::string& message) {
-  int vlog_level = logging::GetVlogLevelHelper(file);
+  int vlog_level =
+      logging::GetVlogLevelHelper(file.c_str(), strlen(file.c_str()));
 
   if (verbose_level <= vlog_level) {
     logging::LogMessage(file.c_str(), line, -verbose_level).stream() << message;
