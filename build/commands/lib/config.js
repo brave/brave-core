602a5e8b6a373e8a55c1b482fa6d3c332ba40ae6
Claudio DeSouza <cdesouza@brave.com>
2025-04-07 20:07:55 +0100

    [cr137] Repackaging WinSDK `10.0.26100.3323` with `d3dcompiler_47.dll`
    This is an update to the Windows SDK with a repackaged SDK, that
    includes a version of `d3dcompiler_47.dll` from `10.0.22621.2428`.
    
    Resolves https://github.com/brave/brave-browser/issues/45263
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/fca8e8720feecf9faa988b92806b574cd530b23e
    
    commit fca8e8720feecf9faa988b92806b574cd530b23e
    Author: Devon Loehr <dloehr@google.com>
    Date:   Thu Mar 20 09:54:01 2025 -0700
    
        Update Supported Windows SDK to 10.0.26100.3323
    
        The floor bug (https://crbug.com/374162585) which blocked the previous roll has been addressed, so we're trying to update the SDK again.
    
        Code changes are essentially the same as the previous update attempt
        (https://crrev.com/c/5898835). Some minor changes to gate new SDK features behind a #define while we transition, and some regenerated image baselines to support the new 3D emojis. Note that updating mc.exe and midl.exe outputs produced no changes.
    
        Pleasantly, the linking change to d3dcompiler_47.dll has been undone, so we no longer need to patch in an older version. This means the update is about as straightforward as it gets.
    
        -----------------------------------------------------------------
        Instructions to reproduce:
    
        These instructions are best done on a fresh VM. Make sure to uninstall any existing installations of Visual Studio and any existing SDK installations before starting.
    
        1. Install git for windows from their website, with the default settings (https://git-scm.com/downloads/win)
        2. Install depot_tools following the instructions on their webpage (https://commondatastorage.googleapis.com/chrome-infra-docs/flat/depot_tools/docs/html/depot_tools_tutorial.html#_setting_up)
        3. Run:
        $ git config --global depot-tools.allowGlobalGitConfig true
        4. Download Visual Studio Professional (Free Trial) from the official site (https://visualstudio.microsoft.com/downloads/)
        5. Run the VS installer with the following arguments:
        ```
        $ VisualStudioSetup.exe --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Component.VC.ATLMFC --add Microsoft.VisualStudio.Component.VC.Tools.ARM64 --add Microsoft.VisualStudio.Component.VC.MFC.ARM64 --includeRecommended --passive
        ```
        6. Open the VS installer (from the start menu), click “Modify”, and uninstall any existing SDK versions (search for “windows sdk”).
        7. Open “Add or Remove Programs” and make sure there are no SDK versions installed; remove them if so (search for “windows software development kit”).
        8. Download SDK Version 10.0.26100.3323 from the official site (https://developer.microsoft.com/en-us/windows/downloads/windows-sdk/)
        9. Run the SDK installer with default settings, except disable “insights”.
        10. Run the packaging script from depot_tools, e.g
        $ python3 depot_tools\win_toolchain\package_from_installed.py 2022 -w 10.0.26100.0
    
        Link to packaging script version used for packaging: https://source.chromium.org/chromium/chromium/tools/depot_tools/+/main:win_toolchain/package_from_installed.py;drc=371aa997c04791d21e222ed43a1a0d55b450dd53
    
        This resulted in the final package with hash 076960eda6.
    
        Bug: 362650376
        Change-Id: Ib6cb299ab20cfca1e72f34728e6ea165ab057d05
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6252774
        Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
        Reviewed-by: Dominic Farolino <dom@chromium.org>
        Reviewed-by: Zequan Wu <zequanwu@google.com>
        Commit-Queue: Devon Loehr <dloehr@google.com>
        Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
        Reviewed-by: Dominik Röttsches <drott@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1435503}

--- build/commands/lib/config.js
+++ build/commands/lib/config.js
@@ -1089,7 +1089,7 @@ Object.defineProperty(Config.prototype, 'defaultOptions', {
       // Use hermetic toolchain only internally.
       env.USE_BRAVE_HERMETIC_TOOLCHAIN = '1'
       env.DEPOT_TOOLS_WIN_TOOLCHAIN = '1'
-      env.GYP_MSVS_HASH_076960eda6 = '970c977bc2'
+      env.GYP_MSVS_HASH_68a20d6dee = '6c25999c85'
       env.DEPOT_TOOLS_WIN_TOOLCHAIN_BASE_URL = `${this.internalDepsUrl}/windows-hermetic-toolchain/`
     }
 
