2e6c2753b96fc5ebee94a3207d73bcc581355942
Kyle Hickinson <git@kylehickinson.com>
2025-04-08 16:21:23 -0400

    [cr136][iOS] Add TabGroupServiceFactory to factory build list
    Chromium change: https://chromium-review.googlesource.com/c/chromium/src/+/6304464
    
    commit 6e2d4797aa27bbfcd13e05895cd8a519eb78d206
    Author: Aliona DANGLA <alionadangla@google.com>
    Date:   Fri Mar 21 09:45:02 2025 -0700
    
        [iOS] Add TabGroupService
    
        Create a tab group service to store some helper functions needed for the
        web state list.
    
        Fixed: 378881981
        Change-Id: Iaf49b35e36c1fee7f6a14e2bbedc0babf46d4ff7

--- chromium_src/ios/chrome/browser/profile/model/keyed_service_factories.mm
+++ chromium_src/ios/chrome/browser/profile/model/keyed_service_factories.mm
@@ -53,10 +53,12 @@
 #include "ios/chrome/browser/reading_list/model/reading_list_model_factory.h"
 #include "ios/chrome/browser/safe_browsing/model/safe_browsing_client_factory.h"
 #include "ios/chrome/browser/safe_browsing/model/safe_browsing_metrics_collector_factory.h"
+#include "ios/chrome/browser/saved_tab_groups/model/tab_group_service_factory.h"
 #include "ios/chrome/browser/saved_tab_groups/model/tab_group_sync_service_factory.h"
 #include "ios/chrome/browser/search_engines/model/template_url_service_factory.h"
 #include "ios/chrome/browser/segmentation_platform/model/segmentation_platform_service_factory.h"
 #include "ios/chrome/browser/sessions/model/session_restoration_service_factory.h"
+#include "ios/chrome/browser/share_kit/model/share_kit_service_factory.h"
 #include "ios/chrome/browser/shared/model/browser/browser_list_factory.h"
 #include "ios/chrome/browser/signin/model/account_consistency_service_factory.h"
 #include "ios/chrome/browser/signin/model/account_reconcilor_factory.h"
@@ -140,7 +142,9 @@ void EnsureProfileKeyedServiceFactoriesBuilt() {
   PowerBookmarkServiceFactory::GetInstance();
   PushNotificationProfileServiceFactory::GetInstance();
   SupervisedUserServiceFactory::GetInstance();
+  ShareKitServiceFactory::GetInstance();
   SyncServiceFactory::GetInstance();
+  TabGroupServiceFactory::GetInstance();
   UnifiedConsentServiceFactory::GetInstance();
   ReadingListModelFactory::GetInstance();
   SafeBrowsingClientFactory::GetInstance();
