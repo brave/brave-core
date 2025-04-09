61beca038618abf257d060a0531418018f47f789
Simone Arpe <simon.arpe@gmail.com>
2025-04-09 11:54:21 +0200

    Add missing headers

--- test/data/youtube-script-injector-component-data/scripts/extra-controls/extra-controls-pip.js
+++ test/data/youtube-script-injector-component-data/scripts/extra-controls/extra-controls-pip.js
@@ -1,2 +1,7 @@
-  // Change document title to "EXTRA CONTROLS TESTED".
-  document.title = 'EXTRA CONTROLS TESTED';
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+// Change document title to "EXTRA CONTROLS TESTED".
+document.title = 'EXTRA CONTROLS TESTED';
