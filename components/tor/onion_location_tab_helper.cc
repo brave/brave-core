5d1dcae8a7e39590f0deb16977e113cd2c468963
Pavel Beloborodov <73575789+boocmp@users.noreply.github.com>
2025-04-10 13:15:12 +0700

    Fixed .onion location. (#28576)

--- components/tor/onion_location_tab_helper.cc
+++ components/tor/onion_location_tab_helper.cc
@@ -5,6 +5,46 @@
 
 #include "brave/components/tor/onion_location_tab_helper.h"
 
+#include <memory>
+
+#include "base/memory/ptr_util.h"
+#include "content/public/browser/navigation_entry.h"
+#include "content/public/browser/navigation_handle.h"
+#include "content/public/browser/web_contents_delegate.h"
+
+namespace {
+
+struct OnionLocation final : public base::SupportsUserData::Data {
+  ~OnionLocation() final = default;
+
+  static const GURL& Get(content::NavigationEntry* entry) {
+    if (auto* data =
+            static_cast<OnionLocation*>(entry->GetUserData(kUserDataKey))) {
+      return data->onion_location_;
+    }
+    return GURL::EmptyGURL();
+  }
+
+  static void Set(content::NavigationEntry* entry, const GURL& url) {
+    if (!entry) {
+      return;
+    }
+    if (url.is_empty()) {
+      entry->RemoveUserData(kUserDataKey);
+    } else {
+      entry->SetUserData(kUserDataKey,
+                         base::WrapUnique(new OnionLocation(url)));
+    }
+  }
+
+ private:
+  static constexpr char kUserDataKey[] = "tor_onion_location";
+  explicit OnionLocation(const GURL& url) : onion_location_(url) {}
+
+  GURL onion_location_;
+};
+}  // namespace
+
 namespace tor {
 
 OnionLocationTabHelper::~OnionLocationTabHelper() = default;
@@ -13,16 +53,42 @@ OnionLocationTabHelper::~OnionLocationTabHelper() = default;
 void OnionLocationTabHelper::SetOnionLocation(
     content::WebContents* web_contents,
     const GURL& onion_location) {
-  OnionLocationTabHelper* tab_helper = FromWebContents(web_contents);
-  if (!tab_helper)
+  auto* tab_helper = OnionLocationTabHelper::FromWebContents(web_contents);
+  if (!tab_helper) {
     return;
+  }
   tab_helper->onion_location_ = onion_location;
 }
 
 OnionLocationTabHelper::OnionLocationTabHelper(
     content::WebContents* web_contents)
-    : content::WebContentsUserData<OnionLocationTabHelper>(*web_contents) {}
+    : content::WebContentsUserData<OnionLocationTabHelper>(*web_contents),
+      content::WebContentsObserver(web_contents) {}
 
 WEB_CONTENTS_USER_DATA_KEY_IMPL(OnionLocationTabHelper);
+
+void OnionLocationTabHelper::DidFinishNavigation(
+    content::NavigationHandle* navigation_handle) {
+  if (!navigation_handle->IsInPrimaryMainFrame() ||
+      !navigation_handle->HasCommitted() ||
+      navigation_handle->IsSameDocument()) {
+    return;
+  }
+  auto* entry = navigation_handle->GetNavigationEntry();
+  if (!entry) {
+    return;
+  }
+
+  if (navigation_handle->IsServedFromBackForwardCache()) {
+    onion_location_ = OnionLocation::Get(entry);
+    if (auto* delegate = web_contents()->GetDelegate()) {
+      // This forces the page action to update the ui state.
+      delegate->NavigationStateChanged(
+          web_contents(), content::InvalidateTypes::INVALIDATE_TYPE_URL);
+    }
+  } else {
+    OnionLocation::Set(entry, onion_location_);
+  }
+}
 
 }  // namespace tor
