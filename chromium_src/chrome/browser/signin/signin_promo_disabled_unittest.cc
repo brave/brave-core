2b8f00d335a80b8bce8871c603e9a4ed6f15f1b2
Claudio DeSouza <cdesouza@brave.com>
2025-04-11 13:02:01 +0100

    [bugfix] Add `chromium_src` check heuristics to detect partial matches
    It was recently uncovered that an override `ShouldShowPromo` silently
    slipped away, as the presubmit check was partially matching for
    `ShouldShowPromoBasedOnImpressionOrDismissalCount`. This has been hiding
    a live bug that users have reported where they were being shown the sign
    in promo.
    
    This change does a few things. First, there's an improvement to how the
    presubmit works, by making sure overrides do occur in the target files
    by matching them as a token.
    
    Additionally, to determine if it is an error or a warning the absecence
    of a token in the target file, the presubmit relies on a check for the
    token occcurring in the override file, and when it does, it concludes
    just warns, as there is a chance the macro is being used in the
    override. This check is being tweaked a bit in this change, to be
    constrained only to `SHOUTY_CASE` tokens, as common word tokens are
    most certainly not common macros and should occur in the target file, so
    their presence in the override file is immaterial to the rules.
    
    Lastly, this change corrects a few places where overrides had slipped
    away, and it changes a few overrides to their respective files, rather
    than relying on replacement of transient inclusions.
    
    In particular, the `ShouldShowPromo` override is being fixed, and these
    functions are being blocked again.
    
    Resolves https://github.com/brave/brave-browser/issues/45354
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/5692313c36387f63418877d922669cc3134a18cc
    https://chromium.googlesource.com/chromium/src/+/d59dbad55ace20c22aefe84aed328f83065827ad
    
    commit 5692313c36387f63418877d922669cc3134a18cc
    Author: Amelie Schneider <amelies@google.com>
    Date:   Mon May 13 09:22:14 2024 +0000
    
        [UNO] Don't show Sign-in promo after 5 impressions/2 dismissals
    
        Only show the autofill sign in promo bubble 5 times per account per
        profile with 1 week delay between impressions. Do not show it again if
        it was dismissed twice.
        Also moves SyncPromoUI::ShouldShowSyncPromo to
        chrome/browser/signin/signin_promo_util.h for consistency.
    
        Bug: b:319411476
    
        Change-Id: I8de968cb5d08dcc0561f46471eab5b68af47ec02
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5446906
        Commit-Queue: Amelie Schneider <amelies@google.com>
        Reviewed-by: Alex Ilin <alexilin@chromium.org>
        Reviewed-by: Ryan Sultanem <rsult@google.com>
        Cr-Commit-Position: refs/heads/main@{#1299945}
    
    commit d59dbad55ace20c22aefe84aed328f83065827ad
    Author: Caroline Rising <corising@chromium.org>
    Date:   Mon Apr 8 17:23:43 2024 +0000
    
        Clean up chrome refresh 2023 from RecentTabsSubMenuModelTests
    
        Change-Id: I766820c477e06f5c156cb2b4e85f3efdbb1f9ce9
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5435914
        Commit-Queue: Caroline Rising <corising@chromium.org>
        Reviewed-by: Darryl James <dljames@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1283927}

---
+++ chromium_src/chrome/browser/signin/signin_promo_disabled_unittest.cc
@@ -0,0 +1,130 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "chrome/browser/signin/chrome_signin_pref_names.h"
+#include "chrome/browser/signin/identity_test_environment_profile_adaptor.h"
+#include "chrome/browser/signin/signin_promo.h"
+#include "chrome/browser/signin/signin_promo_util.h"
+#include "chrome/browser/sync/sync_service_factory.h"
+#include "components/autofill/core/browser/test_utils/test_profiles.h"
+#include "components/signin/public/base/signin_switches.h"
+#include "components/sync/test/mock_sync_service.h"
+#include "content/public/test/browser_task_environment.h"
+#include "extensions/common/extension_builder.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace signin {
+
+class ShowPromoTest : public testing::Test {
+ public:
+  ShowPromoTest() {
+    TestingProfile::Builder profile_builder;
+    profile_builder.AddTestingFactory(
+        SyncServiceFactory::GetInstance(),
+        base::BindRepeating([](content::BrowserContext* context) {
+          return static_cast<std::unique_ptr<KeyedService>>(
+              std::make_unique<syncer::MockSyncService>());
+        }));
+    profile_ = IdentityTestEnvironmentProfileAdaptor::
+        CreateProfileForIdentityTestEnvironment(profile_builder);
+
+    identity_test_env_adaptor_ =
+        std::make_unique<IdentityTestEnvironmentProfileAdaptor>(profile_.get());
+  }
+
+  syncer::MockSyncService* sync_service() {
+    return static_cast<syncer::MockSyncService*>(
+        SyncServiceFactory::GetForProfile(profile()));
+  }
+
+  IdentityManager* identity_manager() {
+    return identity_test_env_adaptor_->identity_test_env()->identity_manager();
+  }
+
+  TestingProfile* profile() { return profile_.get(); }
+
+  const extensions::Extension* CreateExtension(
+      extensions::mojom::ManifestLocation location =
+          extensions::mojom::ManifestLocation::kInternal) {
+    extension_ = extensions::ExtensionBuilder()
+                     .SetManifest(base::Value::Dict()
+                                      .Set("name", "test")
+                                      .Set("manifest_version", 2)
+                                      .Set("version", "1.0.0"))
+                     .SetLocation(location)
+                     .Build();
+
+    return extension_.get();
+  }
+
+ private:
+  content::BrowserTaskEnvironment task_environment_;
+  std::unique_ptr<TestingProfile> profile_;
+  std::unique_ptr<IdentityTestEnvironmentProfileAdaptor>
+      identity_test_env_adaptor_;
+  scoped_refptr<const extensions::Extension> extension_;
+};
+
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
+
+class ShowSigninPromoTestWithFeatureFlagsIsDisabled : public ShowPromoTest {
+ public:
+  void SetUp() override {
+    ShowPromoTest::SetUp();
+    feature_list_.InitWithFeatures(
+        /*enabled_features=*/
+        {switches::kImprovedSigninUIOnDesktop,
+         switches::kSyncEnableBookmarksInTransportMode,
+         switches::kEnableExtensionsExplicitBrowserSignin},
+        /*disabled_features=*/{});
+    ON_CALL(*sync_service(), GetDataTypesForTransportOnlyMode())
+        .WillByDefault(testing::Return(syncer::DataTypeSet::All()));
+  }
+
+  GaiaId gaia_id() {
+    return identity_manager()
+        ->GetPrimaryAccountInfo(ConsentLevel::kSignin)
+        .gaia;
+  }
+
+  autofill::AutofillProfile CreateAddress(
+      const std::string& country_code = "US") {
+    return autofill::test::StandardProfile(AddressCountryCode(country_code));
+  }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
+};
+
+TEST_F(ShowSigninPromoTestWithFeatureFlagsIsDisabled, ShowPromoWithNoAccount) {
+  EXPECT_FALSE(ShouldShowPasswordSignInPromo(*profile()));
+}
+
+TEST_F(ShowSigninPromoTestWithFeatureFlagsIsDisabled,
+       ShowPromoWithWebSignedInAccount) {
+  MakeAccountAvailable(identity_manager(), "test@email.com");
+  EXPECT_FALSE(ShouldShowPasswordSignInPromo(*profile()));
+}
+
+TEST_F(ShowSigninPromoTestWithFeatureFlagsIsDisabled,
+       ShowPromoWithSignInPendingAccount) {
+  AccountInfo info = MakePrimaryAccountAvailable(
+      identity_manager(), "test@email.com", ConsentLevel::kSignin);
+  signin::SetInvalidRefreshTokenForPrimaryAccount(identity_manager());
+  EXPECT_FALSE(ShouldShowPasswordSignInPromo(*profile()));
+}
+
+TEST_F(ShowSigninPromoTestWithFeatureFlagsIsDisabled, DoNotShowAddressPromo) {
+  ASSERT_FALSE(ShouldShowAddressSignInPromo(*profile(), CreateAddress()));
+}
+
+TEST_F(ShowSigninPromoTestWithFeatureFlagsIsDisabled, DoNotShowBookmarkPromo) {
+  ASSERT_FALSE(ShouldShowBookmarkSignInPromo(*profile()));
+}
+
+#endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)
+
+}  // namespace signin
