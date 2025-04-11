84e8b0138e86b2c45f3a30003c2ede8791c64ce0
Max Karolinskiy <max@brave.com>
2025-04-11 17:02:07 -0400

    [cr137] Fixes CertificateTransparencyBrowserTest.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/d8eea086288a64ac09c6454422e2733e42a78868
    
    commit d8eea086288a64ac09c6454422e2733e42a78868
    Author: Matt Mueller <mattm@chromium.org>
    Date:   Thu Apr 3 13:54:01 2025 -0700
    
        refactor RequireCTDelegate to be run by CertVerifyProc
    
        Also changes component updater CT kill switch behavior so that CT will
        be re-enabled if a new component update is received which turns the
        kill switch off. Previously CT would not be re-enabled until the user
        restarted the browser.
    
        Also changes Chrome Root Store SCT constraints to fail open if the
        component updater CT kill switch is used. (This was the intended
        behavior but wasn't implemented for the component kill switch yet.)
    
        This still leaves one use of RequireCTDelegate in the network service
        for SpdySession::CanPool, which should be removed in a followup.
    
        The fuchsia binary size increase seems to largely be caused by
        duplicating the SetCTPolicy mojom method in both the network service
        and cert verifier service. Once the refactoring is completed to remove
        it from the network service that increase should be mitigated.
        Bloaty comparison: https://paste.googleplex.com/5216979231965184
    
        Bug: 41392053,40941039

--- browser/ssl/certificate_transparency_browsertest.cc
+++ browser/ssl/certificate_transparency_browsertest.cc
@@ -11,51 +11,24 @@
 //   Use of this source code is governed by a BSD-style license that can be
 //   found in the LICENSE file.
 
-#include <string_view>
-
-#include "base/run_loop.h"
 #include "chrome/browser/net/system_network_context_manager.h"
-#include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ssl/cert_verifier_browser_test.h"
 #include "chrome/browser/ssl/ssl_browsertest_util.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
-#include "components/policy/core/common/mock_configuration_policy_provider.h"
 #include "content/public/browser/storage_partition.h"
 #include "content/public/test/browser_test.h"
-#include "crypto/sha2.h"
-#include "net/base/hash_value.h"
-#include "net/cert/asn1_util.h"
-#include "net/cert/x509_util.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/cert_test_util.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
-#include "net/test/test_data_directory.h"
-#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-namespace {
-
-// Returns the Sha256 hash of the SPKI of |cert|.
-net::HashValue GetSPKIHash(const CRYPTO_BUFFER* cert) {
-  std::string_view spki_bytes;
-  EXPECT_TRUE(net::asn1::ExtractSPKIFromDERCert(
-      net::x509_util::CryptoBufferAsStringPiece(cert), &spki_bytes));
-  net::HashValue sha256(net::HASH_VALUE_SHA256);
-  sha256.span().copy_from(crypto::SHA256Hash(base::as_byte_span(spki_bytes)));
-  return sha256;
-}
-
-}  // namespace
-
 // Class used to run browser tests that verify SSL UI triggered due to
-// Certificate Transparency verification failures/successes.
-class CertificateTransparencyBrowserTest : public CertVerifierBrowserTest {
+// Certificate Transparency verification failures.
+class CertificateTransparencyBrowserTest : public InProcessBrowserTest {
  public:
-  CertificateTransparencyBrowserTest()
-      : https_server_(net::EmbeddedTestServer::TYPE_HTTPS) {
+  CertificateTransparencyBrowserTest() {
     SystemNetworkContextManager::SetEnableCertificateTransparencyForTesting(
         true);
   }
@@ -68,49 +41,27 @@ class CertificateTransparencyBrowserTest : public CertVerifierBrowserTest {
   ~CertificateTransparencyBrowserTest() override {}
 
   void SetUpOnMainThread() override {
-    CertVerifierBrowserTest::SetUpOnMainThread();
     host_resolver()->AddRule("*", "127.0.0.1");
-    https_server_.AddDefaultHandlers(GetChromeTestDataDir());
+    embedded_https_test_server().SetCertHostnames(
+        {"sct-exempted.bravesoftware.com"});
+    ASSERT_TRUE(embedded_https_test_server().Start());
   }
-
-  void SetUp() override {
-    policy_provider_.SetDefaultReturns(
-        /*is_initialization_complete_return=*/true,
-        /*is_first_policy_load_complete_return=*/true);
-    CertVerifierBrowserTest::SetUp();
-  }
-
-  void SetUpCertVerifier() {
-    SystemNetworkContextManager::GetInstance()->SetCTLogListTimelyForTesting();
-
-    ASSERT_TRUE(https_server()->Start());
-
-    net::CertVerifyResult verify_result;
-    verify_result.verified_cert =
-        net::ImportCertFromFile(net::GetTestCertsDirectory(), "may_2018.pem");
-    ASSERT_TRUE(verify_result.verified_cert);
-    verify_result.is_issued_by_known_root = true;
-    verify_result.public_key_hashes.push_back(
-        GetSPKIHash(verify_result.verified_cert->cert_buffer()));
-
-    mock_cert_verifier()->AddResultForCert(
-        https_server()->GetCertificate().get(), verify_result, net::OK);
-  }
-
-  net::EmbeddedTestServer* https_server() { return &https_server_; }
-
- private:
-  net::EmbeddedTestServer https_server_;
-
-  testing::NiceMock<policy::MockConfigurationPolicyProvider> policy_provider_;
 };
 
 IN_PROC_BROWSER_TEST_F(CertificateTransparencyBrowserTest, EnforcedByDefault) {
-  SetUpCertVerifier();
+  SystemNetworkContextManager::GetInstance()->SetCTLogListTimelyForTesting();
+
+  // Make the test root be interpreted as a known root so that CT will be
+  // required.
+  scoped_refptr<net::X509Certificate> root_cert =
+      net::ImportCertFromFile(net::EmbeddedTestServer::GetRootCertPemPath());
+  ASSERT_TRUE(root_cert);
+  net::ScopedTestKnownRoot scoped_known_root(root_cert.get());
 
   // Normal non-exempt URL
   ASSERT_TRUE(ui_test_utils::NavigateToURL(
-      browser(), https_server()->GetURL("/ssl/google.html")));
+      browser(),
+      embedded_https_test_server().GetURL("example.com", "/ssl/google.html")));
 
   ssl_test_util::CheckSecurityState(
       browser()->tab_strip_model()->GetActiveWebContents(),
@@ -120,12 +71,19 @@ IN_PROC_BROWSER_TEST_F(CertificateTransparencyBrowserTest, EnforcedByDefault) {
 }
 
 IN_PROC_BROWSER_TEST_F(CertificateTransparencyBrowserTest, ExemptedHost) {
-  SetUpCertVerifier();
+  SystemNetworkContextManager::GetInstance()->SetCTLogListTimelyForTesting();
+
+  // Make the test root be interpreted as a known root so that CT will be
+  // required.
+  scoped_refptr<net::X509Certificate> root_cert =
+      net::ImportCertFromFile(net::EmbeddedTestServer::GetRootCertPemPath());
+  ASSERT_TRUE(root_cert);
+  net::ScopedTestKnownRoot scoped_known_root(root_cert.get());
 
   // URL exempted from SCT requirements
   ASSERT_TRUE(ui_test_utils::NavigateToURL(
-      browser(), https_server()->GetURL("sct-exempted.bravesoftware.com",
-                                        "/ssl/google.html")));
+      browser(), embedded_https_test_server().GetURL(
+                     "sct-exempted.bravesoftware.com", "/ssl/google.html")));
 
   ssl_test_util::CheckSecurityState(
       browser()->tab_strip_model()->GetActiveWebContents(),
