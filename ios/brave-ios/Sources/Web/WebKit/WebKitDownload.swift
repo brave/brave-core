eb2537dc1eb1ef3cb79d19a22056ae30f1525dd0
Brandon T <JustBrandonT@gmail.com>
2025-04-14 15:41:06 -0400

    Fix downloads not working for data URIs

--- ios/brave-ios/Sources/Web/WebKit/WebKitDownload.swift
+++ ios/brave-ios/Sources/Web/WebKit/WebKitDownload.swift
@@ -24,13 +24,20 @@ class WebKitDownload: Download {
     self.webView = download.webView
     self.downloadDecisionHandler = downloadDecisionHandler
 
+    // The original url is the download's url
+    var originalURL = download.originalRequest?.url
+
+    // For downloads that are data URI (base64), the original url is the page/frame's url
+    // IE: It's the main document url of that frame.
+    if originalURL?.scheme == "data" {
+      originalURL = download.originalRequest?.mainDocumentURL
+    }
+
     super.init(
       suggestedFilename: suggestedFileName,
-      originalURL: download.originalRequest?.url,
+      originalURL: originalURL,
       mimeType: response.mimeType
     )
-
-    self.originalURL = download.originalRequest?.url
 
     self.totalBytesExpected =
       response.expectedContentLength > 0 ? response.expectedContentLength : nil
