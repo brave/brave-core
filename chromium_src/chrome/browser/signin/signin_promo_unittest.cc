d25318c9b019c83a41a206713f92a9a1d19f5596
Claudio DeSouza <cdesouza@brave.com>
2025-04-14 18:20:11 +0100

    [CodeHealth] Promo disable tests shadowing upstream
    This is a better approach than copying the test classes in brave-core,
    when we can just make use of the existing tests provided by upstream and
    derive from them, to check if the promo is truly disabled.

---
+++ chromium_src/chrome/browser/signin/signin_promo_unittest.cc
@@ -0,0 +1,49 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "src/chrome/browser/signin/signin_promo_unittest.cc"
+
+namespace signin {
+
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
+
+// Creating a derived class, so disabling
+// `ShowSigninPromoTestExplicitBrowserSignin` in filter files won't affect these
+// disable tests.
+class ShowSigninPromoTestExplicitBrowserSigninIsDisabled
+    : public ShowSigninPromoTestExplicitBrowserSignin {};
+
+TEST_F(ShowSigninPromoTestExplicitBrowserSigninIsDisabled,
+       ShowPromoWithNoAccount) {
+  EXPECT_FALSE(ShouldShowPasswordSignInPromo(*profile()));
+}
+
+TEST_F(ShowSigninPromoTestExplicitBrowserSigninIsDisabled,
+       ShowPromoWithWebSignedInAccount) {
+  MakeAccountAvailable(identity_manager(), "test@email.com");
+  EXPECT_FALSE(ShouldShowPasswordSignInPromo(*profile()));
+}
+
+TEST_F(ShowSigninPromoTestExplicitBrowserSigninIsDisabled,
+       ShowPromoWithSignInPendingAccount) {
+  AccountInfo info = MakePrimaryAccountAvailable(
+      identity_manager(), "test@email.com", ConsentLevel::kSignin);
+  signin::SetInvalidRefreshTokenForPrimaryAccount(identity_manager());
+  EXPECT_FALSE(ShouldShowPasswordSignInPromo(*profile()));
+}
+
+TEST_F(ShowSigninPromoTestExplicitBrowserSigninIsDisabled,
+       DoNotShowAddressPromo) {
+  ASSERT_FALSE(ShouldShowAddressSignInPromo(*profile(), CreateAddress()));
+}
+
+TEST_F(ShowSigninPromoTestExplicitBrowserSigninIsDisabled,
+       DoNotShowBookmarkPromo) {
+  ASSERT_FALSE(ShouldShowBookmarkSignInPromo(*profile()));
+}
+
+#endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)
+
+}  // namespace signin
