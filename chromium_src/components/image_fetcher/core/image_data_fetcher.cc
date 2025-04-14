911bbff8ac586115222112877a0edfd45e46c537
deeppandya <deeppandya91@gmail.com>
2025-04-14 11:55:32 -0400

    Add DEPS for brave/components/constants
    Update patch to add macro
    
    Add new line to DEPS

--- chromium_src/components/image_fetcher/core/image_data_fetcher.cc
+++ chromium_src/components/image_fetcher/core/image_data_fetcher.cc
@@ -4,12 +4,12 @@
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "components/image_fetcher/core/image_data_fetcher.h"
+
 #include "brave/components/constants/brave_services_key.h"
 
 #define BRAVE_ADJUST_HEADERS(request_ptr)                                  \
   if ((request_ptr)->url.DomainIs("favicons.proxy.brave.com") &&           \
       (request_ptr)->url.path_piece().starts_with("/faviconV2")) {         \
-    LOG(ERROR) << "BRAVE_ADJUST_HEADERS" << request_ptr->url.spec();       \
     (request_ptr)                                                          \
         ->headers.SetHeader("x-brave-key", BUILDFLAG(BRAVE_SERVICES_KEY)); \
   }
