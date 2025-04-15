cc1eecfe32a4fe55dfd2b4d352a1e012813239d5
Claudio DeSouza <cdesouza@brave.com>
2025-04-15 18:34:09 +0100

    [plaster] Initial prototype
    This is the initial prototype for 🩹 Plaster. This work is based on a
    recenlty worked out proposal:
    
    https://docs.google.com/document/d/1aPf7cMB5Pc_ssKFC_I4-mLHr-7D4lTqsvItSXY-WzQM/
    
    Run this tool with:
    
    ```
    tools/cr/plaster.py
    ```
    
    The tool at the moment is in a very early prototyping stage, and there
    are many tickets opened for it, to get it into a mature state. The
    intent is to manage to deliver this in gradual steps, while acquiring
    experience alongside the way.
    
    - Overlay files.
    
    These are TOML files, even though they look pretty similar to ini files.
    
    Each `[[plaster]]` entry represents an individual regex operation to be applied
    to a file. The format for plasters is something like this
    
    ```toml
    [[plaster]]
    description = ''
    pattern = ''
    replace = ''
    flags = ''
    count = 0
    ```
    
    A user can list as many of these entries as necessary for each regex
    that one desires to apply to a given source.
    
    Resolves https://github.com/brave/brave-browser/issues/45051

--- chromium_src/chrome/browser/signin/signin_promo_util.cc
+++
@@ -1,8 +0,0 @@
-/* Copyright (c) 2020 The Brave Authors. All rights reserved.
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-#define SHOULD_SHOW_SIGNIN_PROMO_COMMON return false;
-#include "src/chrome/browser/signin/signin_promo_util.cc"
-#undef SHOULD_SHOW_SIGNIN_PROMO_COMMON
