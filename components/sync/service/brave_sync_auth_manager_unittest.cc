2283c9571a0a83736896e8a3de714875fc7fcfae
Claudio DeSouza <cdesouza@brave.com>
2025-04-16 11:29:01 +0100

    [cr137] Fixing `SyncAuthManager::Delegate` mock
    `SyncAuthGetLastSyncingGaiaId` has been added to this delegate and it
    has to be mocked as well.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/453baf307ae868828a35ac5a917e858f3c6ee401
    
    commit 453baf307ae868828a35ac5a917e858f3c6ee401
    Author: Mikel Astiz <mastiz@chromium.org>
    Date:   Tue Apr 15 01:42:35 2025 -0700
    
        [sync] Introduce PreviouslySyncingGaiaIdInfoForMetrics
    
        The enum is computed by SyncServiceImpl when a new configuration is
        triggered, based on the information available about the current user ID
        and the last gaia ID known to have enabled sync.
    
        The enum gets passed to datatype controllers but not actually consumed
        for now. In follow-up patches, histograms will be introduced leveraging
        this notion.
    
        Change-Id: Idec0bd3deed5ec06b3d193d9b1bc6a27d2411a2a
        Bug: 407729650
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6439644
        Commit-Queue: Mikel Astiz <mastiz@chromium.org>
        Reviewed-by: Marc Treib <treib@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1446970}

--- components/sync/service/brave_sync_auth_manager_unittest.cc
+++ components/sync/service/brave_sync_auth_manager_unittest.cc
@@ -49,6 +49,7 @@ class MockDelegate : public SyncAuthManager::Delegate {
 
   MOCK_METHOD(void, SyncAuthAccountStateChanged, (), (override));
   MOCK_METHOD(void, SyncAuthCredentialsChanged, (), (override));
+  MOCK_METHOD(GaiaId, SyncAuthGetLastSyncingGaiaId, (), (override));
 };
 
 class BraveSyncAuthManagerTest : public testing::Test {
