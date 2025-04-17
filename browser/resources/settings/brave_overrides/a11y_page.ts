3b60f2b2af20078955415971509c68ace3d10d46
Emerick Rogul <erogul@brave.com>
2025-04-17 12:29:25 -0400

    Hide the image labels toggle button in brave://settings/accessibility

--- browser/resources/settings/brave_overrides/a11y_page.ts
+++ browser/resources/settings/brave_overrides/a11y_page.ts
@@ -3,19 +3,35 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import { RegisterPolymerTemplateModifications } from 'chrome://resources/brave/polymer_overriding.js'
+import {
+  RegisterPolymerTemplateModifications
+} from 'chrome://resources/brave/polymer_overriding.js'
 
 RegisterPolymerTemplateModifications({
   'settings-a11y-page': (templateContent) => {
+
     // We remove all the templates related to captions - there are two modes
     // depending on whether `captionSettingsOpensExternally_` is set. We remove
     // both.
-    const captionsLinkTemplates = templateContent.querySelectorAll('template[if*="captionSettingsOpensExternally_"]')
+    const captionsLinkTemplates = templateContent.
+      querySelectorAll('template[if*="captionSettingsOpensExternally_"]')
     if (captionsLinkTemplates.length !== 3) {
-      console.error(`Expected 3 captionsLinkTemplates, got ${captionsLinkTemplates.length} - this indicates something upstream has changed. The override may need to be updated.`)
+      console.error(
+        `Expected 3 captionsLinkTemplates, got ${captionsLinkTemplates.length}`
+        + ' - this indicates something upstream has changed.'
+        + ' The override may need to be updated.')
     }
     for (const link of captionsLinkTemplates) {
       link.remove()
+    }
+
+    // We hide the image labels toggle button as we don't support this service
+    const imageLabelsToggle = templateContent.querySelector(
+      'settings-toggle-button[on-change="onA11yImageLabelsChange_"]')
+    if (imageLabelsToggle) {
+      imageLabelsToggle.setAttribute('hidden', 'true')
+    } else {
+      console.error('[Settings] missing image labels toggle button')
     }
   }
 })
