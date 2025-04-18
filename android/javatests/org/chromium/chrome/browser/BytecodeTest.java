dc322c416f288940689c7a384ffbc846bdfa03e6
AlexeyBarabash <AlexeyBarabash@users.noreply.github.com>
2025-04-18 13:50:22 +0300

    [Android] Wait for native initialization when creating bottom strip (#28728)
    * [cr137] [Android] Wait for native initialization when creating bottom strip
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/ea006a988d2a36db0eb25801e1c02e9c6e9f5b86
    
    [Tab Group UI] Wait for native initialization when creating bottom strip
    
    Wait for native initialization to complete before initializing the
    bottom tab strip. Otherwise it will try to initialize pre-native and
    crash. This appears to have regressed in M134/M135 likely due to some
    lifecycle change for the BottomControlsCoordinator.
    
    Fixed: 407305088
    
    * Suppressed warning around ContextUtils.getAppSharedPreferences()
    
    ---------
    
    Co-authored-by: Artem Samoilenko <artem@brave.com>

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -1973,6 +1973,14 @@ public class BytecodeTest {
                         "mCurrentOrientation"));
         Assert.assertTrue(
                 fieldExists(
+                        "org/chromium/chrome/browser/toolbar/ToolbarManager",
+                        "mInitializedWithNative"));
+        Assert.assertTrue(
+                fieldExists(
+                        "org/chromium/chrome/browser/toolbar/ToolbarManager",
+                        "mTabGroupUiOneshotSupplier"));
+        Assert.assertTrue(
+                fieldExists(
                         "org/chromium/chrome/browser/toolbar/top/TopToolbarCoordinator",
                         "mOptionalButtonController"));
         Assert.assertTrue(
