a344b60836c42ae9e48041c0d160591bc47f4bd5
Douglas Daniel <douglashdaniel@gmail.com>
2025-04-21 09:34:58 -0500

    feat(wallet): Shield Address Support on Deposit Screen

--- components/brave_wallet_ui/page/screens/fund-wallet/deposit-funds.style.ts
+++ components/brave_wallet_ui/page/screens/fund-wallet/deposit-funds.style.ts
@@ -3,8 +3,28 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 import styled from 'styled-components'
+import LeoSegmentedControl, {
+  SegmentedControlProps
+} from '@brave/leo/react/segmentedControl'
+
+// Shared Styles
 import { Title } from '../onboarding/onboarding.style'
+import { Row, Text } from '../../../components/shared/style'
 
 export const DepositTitle = styled(Title)`
   margin-top: 0px;
+`
+
+export const ControlsWrapper = styled(Row)`
+  margin-bottom: 24px;
+  --leo-segmented-control-width: 100%;
+`
+
+export const SegmentedControl = styled(LeoSegmentedControl).attrs({
+  size: 'default'
+})<SegmentedControlProps>``
+
+export const AddressText = styled(Text)`
+  word-break: break-all;
+  max-width: 500px;
 `
