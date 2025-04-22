03d8d73756b32bdb96503475f1f1a9cb9355b6c2
Max Karolinskiy <41635752+mkarolin@users.noreply.github.com>
2025-04-22 13:01:06 -0400

    [cr136 follow up] Fixes avatar menu guest icon. (#28751)
    Chormium change:
    
    https://source.chromium.org/chromium/chromium/src/+/83106df76363b6a0f301dd60745e711751cf88cd
    
    commit 83106df76363b6a0f301dd60745e711751cf88cd
    Author: David Roger <droger@chromium.org>
    Date:   Thu Mar 13 14:09:39 2025 -0700
    
        Reland "[Uno-D] Enable Phase 0 fast-follow flags by default"
    
        This is a reland of commit cefe36665901c8a494126cbf669708aaefd3d955
    
        Original change's description:
        > [Uno-D] Enable Phase 0 fast-follow flags by default
        >
        > Enables by default:
        > * kOutlineSilhouetteIcon
        > * kImprovedSigninUIOnDesktop
        > * kImprovedSettingsUIOnDesktop
        > * kAutofillRemovePaymentsButterDropdown
        >
        > Bug: 356606258

--- chromium_src/chrome/browser/ui/views/profiles/profile_menu_view.cc
+++ chromium_src/chrome/browser/ui/views/profiles/profile_menu_view.cc
@@ -3,9 +3,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "brave/components/vector_icons/vector_icons.h"
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "ui/gfx/vector_icon_types.h"
 
 #define kGuestMenuArtIcon gfx::VectorIcon::EmptyIcon()
+#define kAccountBoxIcon kLeoUserPictureIcon
 #include "src/chrome/browser/ui/views/profiles/profile_menu_view.cc"
+#undef kAccountBoxIcon
 #undef kGuestMenuArtIcon
