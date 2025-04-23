6e9bf550b9b60704b3c445596ebe27280f86d848
Max Karolinskiy <max@brave.com>
2025-04-23 16:40:51 -0400

    [cr137][iOS] CSPDirectiveName::RequireSRIFor was removed.
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/b3eedf5802876c5142074ee8768c1345a435f987
    
    commit b3eedf5802876c5142074ee8768c1345a435f987
    Author: Yoav Weiss <yoavweiss@chromium.org>
    Date:   Tue Apr 22 08:59:36 2025 -0700
    
        Remove `require-sri-for` CSP directive
    
        This spins off the `require-sri-for` removal from [1], as it is
        being replaced by an `Integrity-Policy` header.
    
        [1] https://chromium-review.googlesource.com/c/chromium/src/+/6408111
    
        Bug: 412588111

--- ios/browser/ui/webui/brave_url_data_source_ios.mm
+++ ios/browser/ui/webui/brave_url_data_source_ios.mm
@@ -104,7 +104,6 @@ std::string BraveURLDataSourceIOS::GetContentSecurityPolicy(
     case network::mojom::CSPDirectiveName::ManifestSrc:
     case network::mojom::CSPDirectiveName::MediaSrc:
     case network::mojom::CSPDirectiveName::ReportURI:
-    case network::mojom::CSPDirectiveName::RequireSRIFor:
     case network::mojom::CSPDirectiveName::Sandbox:
     case network::mojom::CSPDirectiveName::ScriptSrcAttr:
     case network::mojom::CSPDirectiveName::ScriptSrcElem:
