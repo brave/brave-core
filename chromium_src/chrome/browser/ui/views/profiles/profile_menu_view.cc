a89691ffa7f7bc1f75555daa25642d53bb6042e4
Max Karolinskiy <41635752+mkarolin@users.noreply.github.com>
2025-04-23 00:09:56 -0400

    [cr136 follow up] Fixes sign in button showing up in avatar menu. (#28770)
    The button is shown when `Allow Google login for extensions` toggle is
    turned on in the settings.
    
    Chormium change:
    
    https://source.chromium.org/chromium/chromium/src/+/83106df76363b6a0f301dd60745e711751cf88cd
    
    commit 83106df76363b6a0f301dd60745e711751cf88cd
    Author: David Roger <droger@chromium.org>
    Date:   Thu Mar 13 14:09:39 2025 -0700
    
        Reland "[Uno-D] Enable Phase 0 fast-follow flags by default"
    
        This is a reland of commit cefe36665901c8a494126cbf669708aaefd3d955
    
        Original change's description:
        > [Uno-D] Enable Phase 0 fast-follow flags by default
        >
        > Enables by default:
        > * kOutlineSilhouetteIcon
        > * kImprovedSigninUIOnDesktop
        > * kImprovedSettingsUIOnDesktop
        > * kAutofillRemovePaymentsButterDropdown
        >
        > Bug: 356606258

--- chromium_src/chrome/browser/ui/views/profiles/profile_menu_view.cc
+++ chromium_src/chrome/browser/ui/views/profiles/profile_menu_view.cc
@@ -5,10 +5,30 @@
 
 #include "brave/components/vector_icons/vector_icons.h"
 #include "chrome/app/vector_icons/vector_icons.h"
+#include "chrome/browser/ui/views/profiles/profile_menu_view_base.h"
 #include "ui/gfx/vector_icon_types.h"
+
+namespace {
+
+ProfileMenuViewBase::IdentitySectionParams BraveAdjustIdentitySectionParams(
+    ProfileMenuViewBase::IdentitySectionParams params) {
+  // We never show header, subtitle, or action button.
+  ProfileMenuViewBase::IdentitySectionParams new_params;
+  new_params.profile_image = params.profile_image;
+  new_params.profile_image_padding = params.profile_image_padding;
+  new_params.title = params.title;
+  new_params.has_dotted_ring = params.has_dotted_ring;
+  return new_params;
+}
+
+}  // namespace
 
 #define kGuestMenuArtIcon gfx::VectorIcon::EmptyIcon()
 #define kAccountBoxIcon kLeoUserPictureIcon
+#define SetProfileIdentityWithCallToAction(PARAMS) \
+  SetProfileIdentityWithCallToAction(BraveAdjustIdentitySectionParams(PARAMS))
+
 #include "src/chrome/browser/ui/views/profiles/profile_menu_view.cc"
+#undef SetProfileIdentityWithCallToAction
 #undef kAccountBoxIcon
 #undef kGuestMenuArtIcon
