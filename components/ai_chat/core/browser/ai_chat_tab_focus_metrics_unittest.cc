c71be3566687b37f75c2609d9b0a87aa079a3cba
Darnell Andries <dandries@brave.com>
2025-04-24 12:09:20 -0700

    Update Tab Focus metrics on browser start

--- components/ai_chat/core/browser/ai_chat_tab_focus_metrics_unittest.cc
+++ components/ai_chat/core/browser/ai_chat_tab_focus_metrics_unittest.cc
@@ -9,6 +9,7 @@
 
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/task_environment.h"
+#include "base/time/time.h"
 #include "brave/components/ai_chat/core/common/pref_names.h"
 #include "components/prefs/testing_pref_service.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -23,9 +24,14 @@ class AIChatTabFocusMetricsUnitTest : public testing::Test,
 
   void SetUp() override {
     AIChatTabFocusMetrics::RegisterPrefs(local_state_.registry());
+    profile_prefs_.registry()->RegisterTimePref(prefs::kLastAcceptedDisclaimer,
+                                                {});
     profile_prefs_.registry()->RegisterBooleanPref(
         prefs::kBraveAIChatTabOrganizationEnabled, false);
+    CreateMetrics();
+  }
 
+  void CreateMetrics() {
     metrics_ = std::make_unique<AIChatTabFocusMetrics>(&local_state_,
                                                        &profile_prefs_, this);
   }
@@ -42,6 +48,7 @@ class AIChatTabFocusMetricsUnitTest : public testing::Test,
 };
 
 TEST_F(AIChatTabFocusMetricsUnitTest, TabCountMetrics) {
+  task_environment_.FastForwardBy(base::Minutes(1));
   histogram_tester_.ExpectTotalCount(kTabFocusAvgTabCountHistogramName, 0);
   histogram_tester_.ExpectTotalCount(kTabFocusMaxTabCountHistogramName, 0);
   histogram_tester_.ExpectTotalCount(kTabFocusSessionCountHistogramName, 0);
@@ -59,21 +66,58 @@ TEST_F(AIChatTabFocusMetricsUnitTest, TabCountMetrics) {
 
   metrics_->RecordUsage(30);
   histogram_tester_.ExpectBucketCount(kTabFocusAvgTabCountHistogramName, 2, 1);
-  histogram_tester_.ExpectBucketCount(kTabFocusMaxTabCountHistogramName, 2, 1);
+  histogram_tester_.ExpectBucketCount(kTabFocusMaxTabCountHistogramName, 3, 1);
   histogram_tester_.ExpectBucketCount(kTabFocusSessionCountHistogramName, 1, 2);
+
+  task_environment_.FastForwardBy(base::Days(1));
+  histogram_tester_.ExpectBucketCount(kTabFocusAvgTabCountHistogramName, 2, 2);
+  histogram_tester_.ExpectBucketCount(kTabFocusMaxTabCountHistogramName, 3, 2);
+  histogram_tester_.ExpectBucketCount(kTabFocusSessionCountHistogramName, 1, 3);
 
   // Verify weekly reset
   task_environment_.FastForwardBy(base::Days(7));
 
-  metrics_->ReportAllMetrics();
-  histogram_tester_.ExpectTotalCount(kTabFocusAvgTabCountHistogramName, 3);
-  histogram_tester_.ExpectTotalCount(kTabFocusMaxTabCountHistogramName, 3);
-  histogram_tester_.ExpectTotalCount(kTabFocusSessionCountHistogramName, 3);
+  auto avg_tab_count =
+      histogram_tester_
+          .GetTotalCountsForPrefix(kTabFocusAvgTabCountHistogramName)
+          .begin()
+          ->second;
+  auto max_tab_count =
+      histogram_tester_
+          .GetTotalCountsForPrefix(kTabFocusMaxTabCountHistogramName)
+          .begin()
+          ->second;
+  auto session_count =
+      histogram_tester_
+          .GetTotalCountsForPrefix(kTabFocusSessionCountHistogramName)
+          .begin()
+          ->second;
+
+  task_environment_.FastForwardBy(base::Days(7));
+  EXPECT_EQ(histogram_tester_
+                .GetTotalCountsForPrefix(kTabFocusAvgTabCountHistogramName)
+                .begin()
+                ->second,
+            avg_tab_count);
+  EXPECT_EQ(histogram_tester_
+                .GetTotalCountsForPrefix(kTabFocusMaxTabCountHistogramName)
+                .begin()
+                ->second,
+            max_tab_count);
+  EXPECT_EQ(histogram_tester_
+                .GetTotalCountsForPrefix(kTabFocusSessionCountHistogramName)
+                .begin()
+                ->second,
+            session_count);
 }
 
 TEST_F(AIChatTabFocusMetricsUnitTest, EnabledMetric) {
+  metrics_->RecordEnabled();
+  // Should not report if not opted in to Leo
+  histogram_tester_.ExpectTotalCount(kTabFocusEnabledHistogramName, 0);
+
   // Test disabled state
-  metrics_->ReportAllMetrics();
+  profile_prefs_.SetTime(prefs::kLastAcceptedDisclaimer, base::Time::Now());
   histogram_tester_.ExpectUniqueSample(kTabFocusEnabledHistogramName, 0, 1);
 
   // Test enabled free state
@@ -83,7 +127,7 @@ TEST_F(AIChatTabFocusMetricsUnitTest, EnabledMetric) {
 
   // Test enabled premium state
   is_premium_ = true;
-  metrics_->ReportAllMetrics();
+  metrics_->RecordEnabled();
   histogram_tester_.ExpectBucketCount(kTabFocusEnabledHistogramName, 2, 1);
 }
 
@@ -94,10 +138,15 @@ TEST_F(AIChatTabFocusMetricsUnitTest, LastUsageTime) {
                                        1);
 
   task_environment_.FastForwardBy(base::Days(7));
-  metrics_->ReportAllMetrics();
   histogram_tester_.ExpectBucketCount(kTabFocusLastUsageTimeHistogramName, 2,
                                       1);
-  histogram_tester_.ExpectTotalCount(kTabFocusLastUsageTimeHistogramName, 2);
+  histogram_tester_.ExpectTotalCount(kTabFocusLastUsageTimeHistogramName, 8);
+
+  // Recreate metrics and ensure the same values are reported
+  CreateMetrics();
+  histogram_tester_.ExpectBucketCount(kTabFocusLastUsageTimeHistogramName, 2,
+                                      2);
+  histogram_tester_.ExpectTotalCount(kTabFocusLastUsageTimeHistogramName, 9);
 }
 
 }  // namespace ai_chat
