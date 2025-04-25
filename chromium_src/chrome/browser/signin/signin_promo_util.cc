b12c68e4fe6a8aea0074f2e23f4f5bb27bb05bd1
Max Karolinskiy <max@brave.com>
2025-04-25 18:22:34 -0400

    Fixes sign in promo showing up in bookmark bubble.

--- chromium_src/chrome/browser/signin/signin_promo_util.cc
+++ chromium_src/chrome/browser/signin/signin_promo_util.cc
@@ -4,5 +4,32 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #define SHOULD_SHOW_SIGNIN_PROMO_COMMON return false;
+// Even though this function is used internally in
+// ShouldShowExtensionSignInPromo, that function later calls
+// ShouldShowSignInPromoCommon which is disabled via the above patch.
+#define ShouldShowExtensionSyncPromo ShouldShowExtensionSyncPromo_UnUsed
+// Even though this function is used internally in
+// - ShouldShowSignInPromoCommon, that function is disabled via the above patch,
+// - ShouldShowExtensionSyncPromo, that function is replaced above.
+#define ShouldShowSyncPromo ShouldShowSyncPromo_UnUsed
 #include "src/chrome/browser/signin/signin_promo_util.cc"
+#undef ShouldShowSyncPromo
+#undef ShouldShowExtensionSyncPromo
 #undef SHOULD_SHOW_SIGNIN_PROMO_COMMON
+
+namespace signin {
+
+#if !BUILDFLAG(IS_ANDROID)
+bool ShouldShowSyncPromo(Profile& profile) {
+  return false;
+}
+#endif  // !BUILDFLAG(IS_ANDROID)
+
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+bool ShouldShowExtensionSyncPromo(Profile& profile,
+                                  const extensions::Extension& extension) {
+  return false;
+}
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
+
+}  // namespace signin
