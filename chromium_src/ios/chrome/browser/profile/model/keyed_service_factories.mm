444ffe3bee79c78be087577e7d9e8d63eacfa14e
Kyle Hickinson <git@kylehickinson.com>
2025-04-25 18:23:36 -0400

    [iOS] Support using CWVWebView autofill with Chrome factories
    This change adds the required overrides & subclasses to use the pre-wrapped `CWVAutofillController` & related classes with `BraveWebView`

--- chromium_src/ios/chrome/browser/profile/model/keyed_service_factories.mm
+++ chromium_src/ios/chrome/browser/profile/model/keyed_service_factories.mm
@@ -10,8 +10,11 @@
 #include "ios/chrome/browser/aim/model/ios_chrome_aim_eligibility_service_factory.h"
 #include "ios/chrome/browser/autocomplete/model/autocomplete_classifier_factory.h"
 #include "ios/chrome/browser/autocomplete/model/zero_suggest_cache_service_factory.h"
+#include "ios/chrome/browser/autofill/model/autocomplete_history_manager_factory.h"
+#include "ios/chrome/browser/autofill/model/autofill_image_fetcher_factory.h"
 #include "ios/chrome/browser/autofill/model/autofill_log_router_factory.h"
 #include "ios/chrome/browser/autofill/model/personal_data_manager_factory.h"
+#include "ios/chrome/browser/autofill/model/strike_database_factory.h"
 #include "ios/chrome/browser/bookmarks/model/account_bookmark_sync_service_factory.h"
 #include "ios/chrome/browser/bookmarks/model/bookmark_model_factory.h"
 #include "ios/chrome/browser/bookmarks/model/bookmark_undo_service_factory.h"
@@ -89,6 +92,9 @@
 void EnsureProfileKeyedServiceFactoriesBuilt() {
   autofill::AutofillLogRouterFactory::GetInstance();
   autofill::PersonalDataManagerFactory::GetInstance();
+  autofill::StrikeDatabaseFactory::GetInstance();
+  autofill::AutocompleteHistoryManagerFactory::GetInstance();
+  autofill::AutofillImageFetcherFactory::GetInstance();
   commerce::ShoppingServiceFactory::GetInstance();
   data_sharing::DataSharingServiceFactory::GetInstance();
   ios::AccountBookmarkSyncServiceFactory::GetInstance();
