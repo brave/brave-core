2ad0eeb3dd967f93a0bbaa455a3a8420458fea0b
Arthur Edelstein <aedelstein@brave.com>
2025-04-28 09:06:22 -0700

    features.cc, features.h

---
+++ browser/resources/settings/email_aliases_page/features.cc
@@ -0,0 +1,14 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "brave/browser/resources/settings/email_aliases_page/features.h"
+
+namespace email_aliases {
+
+BASE_FEATURE(kEmailAliases,
+             "EmailAliases",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
+}  // namespace email_aliases 
