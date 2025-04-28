e2e561d423a3b6390a06e5702996d91d7f4cf38f
Mikhail Atuchin <matuchin@brave.com>
2025-04-28 16:24:05 +0400

    Add \n after the license

--- build/commands/lib/start.js
+++ build/commands/lib/start.js
@@ -2,6 +2,7 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
+
 const path = require('path')
 const fs = require('fs-extra')
 const config = require('../lib/config')
