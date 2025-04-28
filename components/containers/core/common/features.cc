417acdd1508ed783e19f0f5af1485018fdc81613
Aleksei Khoroshilov <akhoroshilov@brave.com>
2025-04-28 14:42:02 +0700

    Add containers feature and brave://flag.

---
+++ components/containers/core/common/features.cc
@@ -0,0 +1,16 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "brave/components/containers/core/common/features.h"
+
+namespace containers::features {
+
+// Enable Container Tabs. Allows users to open websites in contained tabs,
+// keeping different identities separate within the same browser profile.
+BASE_FEATURE(kBraveContainers,
+             "BraveContainers",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+}  // namespace containers::features
