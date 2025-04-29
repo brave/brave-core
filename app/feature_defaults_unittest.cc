d318e94a2eb1985001242b300adc44c2f30ef3b0
Max Karolinskiy <max@brave.com>
2025-04-29 19:31:46 -0400

    [cr137] kOutlineSilhouetteIcon feature flag was removed.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/c59ebacc706e16d4bb96587b1993ae2751e61cf0
    
    commit c59ebacc706e16d4bb96587b1993ae2751e61cf0
    Author: David Roger <droger@chromium.org>
    Date:   Mon Apr 28 05:07:44 2025 -0700
    
        [Profiles] Cleanup the outline silhouette flag
    
        switches::kOutlineSilhouetteIcon has been enabled by default and can now
        be cleaned up.
    
        Bug: 370473765

--- app/feature_defaults_unittest.cc
+++ app/feature_defaults_unittest.cc
@@ -80,7 +80,6 @@
 TEST(FeatureDefaultsTest, DisabledFeatures) {
   // Please, keep alphabetized
   const base::Feature* disabled_features[] = {
-      &kOutlineSilhouetteIcon,
       &aggregation_service::kAggregationServiceMultipleCloudProviders,
 #if BUILDFLAG(IS_ANDROID)
       &android_webview::features::kWebViewMediaIntegrityApiBlinkExtension,
--- chromium_src/components/signin/public/base/signin_switches.cc
+++ chromium_src/components/signin/public/base/signin_switches.cc
@@ -16,7 +16,6 @@ OVERRIDE_FEATURE_DEFAULT_STATES({{
 }  // namespace switches
 
 OVERRIDE_FEATURE_DEFAULT_STATES({{
-    {kOutlineSilhouetteIcon, base::FEATURE_DISABLED_BY_DEFAULT},
 #if BUILDFLAG(ENABLE_MIRROR) && !BUILDFLAG(IS_IOS)
     {kVerifyRequestInitiatorForMirrorHeaders,
      base::FEATURE_DISABLED_BY_DEFAULT},
