dfe1eec0c2028f6a71b15d759dd3ced3d41c8ab3
bridiver <34129+bridiver@users.noreply.github.com>
2025-04-29 12:38:31 -0700

    presubmit

--- browser/ui/views/crash_report_permission_ask_dialog_view.cc
+++ browser/ui/views/crash_report_permission_ask_dialog_view.cc
@@ -1,10 +1,11 @@
 /* Copyright (c) 2021 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "brave/browser/ui/views/crash_report_permission_ask_dialog_view.h"
 
+#include <memory>
 #include <utility>
 
 #include "base/functional/bind.h"
