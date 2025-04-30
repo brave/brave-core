6da38d46ae7239ae7194ff1f73a9390a552a076e
Kyle Hickinson <git@kylehickinson.com>
2025-04-30 13:56:00 -0400

    disable translate-internals webui

---
+++ chromium_src/ios/chrome/browser/webui/ui_bundled/chrome_web_ui_ios_controller_factory.mm
@@ -0,0 +1,10 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#import "ios/chrome/browser/webui/ui_bundled/translate_internals/translate_internals_ui.h"
+
+#define TranslateInternalsUI InternalDebugPagesDisabledUI
+#include "src/ios/chrome/browser/webui/ui_bundled/chrome_web_ui_ios_controller_factory.mm"
+#undef TranslateInternalsUI
