988f4b0e25a505828e8f809ad72cbb5ba7b370c0
Szilard Szaloki <szilardszaloki@gmail.com>
2025-05-02 10:57:27 -0600

    Brave Account UI in `brave://hello-world`.

--- browser/ui/webui/hello_world/hello_world_ui.cc
+++ browser/ui/webui/hello_world/hello_world_ui.cc
@@ -6,15 +6,113 @@
 #include "brave/browser/ui/webui/hello_world/hello_world_ui.h"
 
 #include "base/containers/span.h"
-#include "base/version_info/version_info.h"
 #include "brave/components/hello_world/resources/grit/hello_world_resources.h"
 #include "brave/components/hello_world/resources/grit/hello_world_resources_map.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_dialogs.h"
+#include "components/grit/brave_components_strings.h"
+#include "components/grit/brave_components_resources.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/webui/webui_util.h"
+
+namespace {
+constexpr char16_t kBraveAccountSelfCustodyLearnMoreURL[] =
+    u"https://search.brave.com";
+constexpr char16_t kBraveAccountTermsOfServiceURL[] =
+    u"https://brave.com/terms-of-use/";
+constexpr char16_t kBraveAccountPrivacyAgreementURL[] =
+    u"https://brave.com/privacy/browser/";
+
+void AddStringResources(content::WebUIDataSource* source) {
+  static constexpr webui::LocalizedString kStrings[] = {
+      // Row:
+      {"braveAccountRowTitle", IDS_BRAVE_ACCOUNT_ROW_TITLE},
+      {"braveAccountRowDescription", IDS_BRAVE_ACCOUNT_ROW_DESCRIPTION},
+      {"braveAccountGetStartedButtonLabel",
+       IDS_BRAVE_ACCOUNT_GET_STARTED_BUTTON_LABEL},
+      {"braveAccountManageAccountButtonLabel",
+       IDS_BRAVE_ACCOUNT_MANAGE_ACCOUNT_BUTTON_LABEL},
+
+      // 'Entry' dialog:
+      {"braveAccountEntryDialogTitle", IDS_BRAVE_ACCOUNT_ENTRY_DIALOG_TITLE},
+      {"braveAccountEntryDialogDescription",
+       IDS_BRAVE_ACCOUNT_ENTRY_DIALOG_DESCRIPTION},
+      {"braveAccountCreateBraveAccountButtonLabel",
+       IDS_BRAVE_ACCOUNT_ENTRY_DIALOG_CREATE_BRAVE_ACCOUNT_BUTTON_LABEL},
+      {"braveAccountAlreadyHaveAccountSignInButtonLabel",
+       IDS_BRAVE_ACCOUNT_ALREADY_HAVE_ACCOUNT_SIGN_IN_BUTTON_LABEL},
+      {"braveAccountSelfCustodyButtonLabel",
+       IDS_BRAVE_ACCOUNT_SELF_CUSTODY_BUTTON_LABEL},
+
+      // 'Create' dialog:
+      {"braveAccountCreateDialogTitle", IDS_BRAVE_ACCOUNT_CREATE_DIALOG_TITLE},
+      {"braveAccountCreateDialogDescription",
+       IDS_BRAVE_ACCOUNT_CREATE_DIALOG_DESCRIPTION},
+      {"braveAccountEmailInputErrorMessage",
+       IDS_BRAVE_ACCOUNT_EMAIL_INPUT_ERROR_MESSAGE},
+      {"braveAccountCreatePasswordInputLabel",
+       IDS_BRAVE_ACCOUNT_CREATE_PASSWORD_INPUT_LABEL},
+      {"braveAccountPasswordStrengthMeterWeak",
+       IDS_BRAVE_ACCOUNT_PASSWORD_STRENGTH_METER_WEAK},
+      {"braveAccountPasswordStrengthMeterMedium",
+       IDS_BRAVE_ACCOUNT_PASSWORD_STRENGTH_METER_MEDIUM},
+      {"braveAccountPasswordStrengthMeterStrong",
+       IDS_BRAVE_ACCOUNT_PASSWORD_STRENGTH_METER_STRONG},
+      {"braveAccountConfirmPasswordInputLabel",
+       IDS_BRAVE_ACCOUNT_CONFIRM_PASSWORD_INPUT_LABEL},
+      {"braveAccountConfirmPasswordInputPlaceholder",
+       IDS_BRAVE_ACCOUNT_CONFIRM_PASSWORD_INPUT_PLACEHOLDER},
+      {"braveAccountConfirmPasswordInputErrorMessage",
+       IDS_BRAVE_ACCOUNT_CONFIRM_PASSWORD_INPUT_ERROR_MESSAGE},
+      {"braveAccountConfirmPasswordInputSuccessMessage",
+       IDS_BRAVE_ACCOUNT_CONFIRM_PASSWORD_INPUT_SUCCESS_MESSAGE},
+      {"braveAccountCreateAccountButtonLabel",
+       IDS_BRAVE_ACCOUNT_CREATE_ACCOUNT_BUTTON_LABEL},
+
+      // 'Sign In' dialog:
+      {"braveAccountSignInDialogTitle", IDS_BRAVE_ACCOUNT_SIGN_IN_DIALOG_TITLE},
+      {"braveAccountSignInDialogDescription",
+       IDS_BRAVE_ACCOUNT_SIGN_IN_DIALOG_DESCRIPTION},
+      {"braveAccountPasswordInputLabel",
+       IDS_BRAVE_ACCOUNT_PASSWORD_INPUT_LABEL},
+      {"braveAccountForgotPasswordButtonLabel",
+       IDS_BRAVE_ACCOUNT_FORGOT_PASSWORD_BUTTON_LABEL},
+      {"braveAccountSignInButtonLabel", IDS_BRAVE_ACCOUNT_SIGN_IN_BUTTON_LABEL},
+
+      // 'Forgot Password' dialog:
+      {"braveAccountForgotPasswordDialogTitle",
+       IDS_BRAVE_ACCOUNT_FORGOT_PASSWORD_DIALOG_TITLE},
+      {"braveAccountForgotPasswordDialogDescription",
+       IDS_BRAVE_ACCOUNT_FORGOT_PASSWORD_DIALOG_DESCRIPTION},
+      {"braveAccountAlertMessage", IDS_BRAVE_ACCOUNT_ALERT_MESSAGE},
+      {"braveAccountCancelButtonLabel", IDS_BRAVE_ACCOUNT_CANCEL_BUTTON_LABEL},
+      {"braveAccountResetPasswordButtonLabel",
+       IDS_BRAVE_ACCOUNT_RESET_PASSWORD_BUTTON_LABEL},
+
+      // Common:
+      {"braveAccountEmailInputLabel", IDS_BRAVE_ACCOUNT_EMAIL_INPUT_LABEL},
+      {"braveAccountEmailInputPlaceholder",
+       IDS_BRAVE_ACCOUNT_EMAIL_INPUT_PLACEHOLDER},
+      {"braveAccountPasswordInputPlaceholder",
+       IDS_BRAVE_ACCOUNT_PASSWORD_INPUT_PLACEHOLDER},
+  };
+
+  source->AddLocalizedStrings(kStrings);
+
+  source->AddString(
+      "braveAccountSelfCustodyDescription",
+      l10n_util::GetStringFUTF16(IDS_BRAVE_ACCOUNT_SELF_CUSTODY_DESCRIPTION,
+                                 kBraveAccountSelfCustodyLearnMoreURL));
+  source->AddString(
+      "braveAccountConsentCheckboxLabel",
+      l10n_util::GetStringFUTF16(IDS_BRAVE_ACCOUNT_CONSENT_CHECKBOX_LABEL,
+                                 kBraveAccountTermsOfServiceURL,
+                                 kBraveAccountPrivacyAgreementURL));
+}
+}  // namespace
 
 HelloWorldUI::HelloWorldUI(content::WebUI* web_ui)
     : content::WebUIController(web_ui) {
@@ -24,7 +122,11 @@ HelloWorldUI::HelloWorldUI(content::WebUI* web_ui)
   webui::SetupWebUIDataSource(source, kHelloWorldResources,
                               IDR_HELLO_WORLD_HELLO_WORLD_HTML);
 
-  source->AddString("platform", version_info::GetOSType());
+  AddStringResources(source);
+
+  source->AddResourcePath("full_brave_brand.svg", IDR_BRAVE_ACCOUNT_IMAGES_FULL_BRAVE_BRAND_SVG);
+  source->AddResourcePath("full_brave_brand_dark.svg", IDR_BRAVE_ACCOUNT_IMAGES_FULL_BRAVE_BRAND_DARK_SVG);
+  source->UseStringsJs();
 }
 
 HelloWorldUI::~HelloWorldUI() = default;
@@ -37,7 +139,7 @@ void HelloWorldDialog::Show(content::WebUI* web_ui) {
 }
 
 ui::mojom::ModalType HelloWorldDialog::GetDialogModalType() const {
-  return ui::mojom::ModalType::kWindow;
+  return ui::mojom::ModalType::kNone;
 }
 
 std::u16string HelloWorldDialog::GetDialogTitle() const {
@@ -52,8 +154,8 @@ void HelloWorldDialog::GetWebUIMessageHandlers(
     std::vector<content::WebUIMessageHandler*>* handlers) {}
 
 void HelloWorldDialog::GetDialogSize(gfx::Size* size) const {
-  const int kDefaultWidth = 544;
-  const int kDefaultHeight = 628;
+  const int kDefaultWidth = 1024;
+  const int kDefaultHeight = 1024;
   size->SetSize(kDefaultWidth, kDefaultHeight);
 }
 
