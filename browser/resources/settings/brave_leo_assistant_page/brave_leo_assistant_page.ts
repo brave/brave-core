8b0dc7f70c85b8aa32955c487f214283169342da
Emerick Rogul <erogul@brave.com>
2025-05-05 16:38:23 -0400

    [cr138] Fixes webui-eslint/polymer-property-declare errors
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/97f818262f288cf3c833e7497e096b6bf32cb59a
    
    commit 97f818262f288cf3c833e7497e096b6bf32cb59a
    Author: dpapad <dpapad@chromium.org>
    Date:   Tue Mar 18 14:59:32 2025 -0700
    
        WebUI: Add a custom typescript-eslint plugin.
    
        The plugin currently implements two rules:
         - 'lit-property-accessor': Checks for the proper use of the 'accessor'
           keyword in Lit code that uses useDefineForClassFields=true.
         - 'polymer-property-declare': Checks for the proper use of the 'declare'
           keyword in Polymer code that uses useDefineForClassFields=true.
    
        Moreover, it is hooked up
        ui/webui/resources/tools/eslint_ts.config_base.mjs which in turn is used
        by build_webui()'s ":lint" target.
    
        This is done to facilitate the migration off of
        useDefineForClassFields=false, which requires WebUI authors to properly
        use the 'declare' and 'accessor' keywords for Polymer and Lit
        respectively, and to provide an explicit build-time check instead of
        leaving developers debugging why their Lit/Polymer elements don't work
        as expected at runtime.
    
        Bug: 389737066

--- browser/resources/settings/brave_leo_assistant_page/brave_leo_assistant_page.ts
+++ browser/resources/settings/brave_leo_assistant_page/brave_leo_assistant_page.ts
@@ -65,8 +65,8 @@ class BraveLeoAssistantPageElement extends BraveLeoAssistantPageBase {
 
     declare leoAssistantShowOnToolbarPref_: boolean
     declare selectedModelDisplayName_: string
-    declare defaultModelKeyPrefValue_: string
-    declare models_: ModelWithSubtitle[]
+    defaultModelKeyPrefValue_: string
+    models_: ModelWithSubtitle[]
     premiumStatus_: PremiumStatus = PremiumStatus.Unknown
     browserProxy_: BraveLeoAssistantBrowserProxy =
       BraveLeoAssistantBrowserProxyImpl.getInstance()
