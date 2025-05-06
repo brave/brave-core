0cf32123f4411a013d209ef9825b41f50a85c867
Brandon T <JustBrandonT@gmail.com>
2025-05-05 22:40:23 -0400

    Brandon's CSP override fix

--- chromium_src/ios/web/public/webui/url_data_source_ios.h
+++ chromium_src/ios/web/public/webui/url_data_source_ios.h
@@ -6,8 +6,9 @@
 #ifndef BRAVE_CHROMIUM_SRC_IOS_WEB_PUBLIC_WEBUI_URL_DATA_SOURCE_IOS_H_
 #define BRAVE_CHROMIUM_SRC_IOS_WEB_PUBLIC_WEBUI_URL_DATA_SOURCE_IOS_H_
 
-#define GetContentSecurityPolicyObjectSrc    \
-  GetContentSecurityPolicyObjectSrc() const; \
+#define GetContentSecurityPolicyObjectSrc                   \
+  GetContentSecurityPolicyObjectSrc() const;                \
+  virtual std::string GetContentSecurityPolicyBase() const; \
   virtual std::string GetContentSecurityPolicyFrameSrc
 
 #import "src/ios/web/public/webui/url_data_source_ios.h"  // IWYU pragma: export
