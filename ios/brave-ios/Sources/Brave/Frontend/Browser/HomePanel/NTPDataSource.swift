447101ef0fb39337e65306afdfbd314a198216c0
Aleksei Seren <aseren@brave.com>
2025-05-06 14:22:04 -0500

    [ads] Add iOS grace period logging

--- ios/brave-ios/Sources/Brave/Frontend/Browser/HomePanel/NTPDataSource.swift
+++ ios/brave-ios/Sources/Brave/Frontend/Browser/HomePanel/NTPDataSource.swift
@@ -248,7 +248,15 @@ public class NTPDataSource {
       return true
     }
 
-    return Date.now.timeIntervalSince(installDate) > gracePeriod
+    let gracePeriodEndAt = installDate + TimeInterval(gracePeriod)
+    if Date.now >= gracePeriodEndAt {
+      return true
+    }
+
+    Logger.module.info(
+      "Sponsored images not shown: Grace period after installation is still active until \(gracePeriodEndAt)"
+    )
+    return false
   }
 }
 
