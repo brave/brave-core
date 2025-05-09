cad814f2645f84ffad32a10601a00a7e17089912
Claudio DeSouza <cdesouza@brave.com>
2025-05-09 12:51:46 +0100

    [cr138][WIP] `ShouldShowBookmarkBar` now in `Browser` only
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/73a146fefe836a891e3164c37f90f4ccf9ea776d
    
    commit 73a146fefe836a891e3164c37f90f4ccf9ea776d
    Author: Alison Gale <agale@chromium.org>
    Date:   Wed May 7 09:58:55 2025 -0700
    
        [SxS] Refactor logic for checking if bookmark bar should show
    
        For split tabs, we will need this logic to include looking at both
        halves of a split. To do that, it would be helpful if this were a util
        rather than a tab helper since it won't just be looking at one tab. This
        CL does not change any existing functionality, it just moves code.
    
        Bug: 415071206
        Change-Id: Ib5d073f63f485a9d3c26895a7be186cc4c49db09
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6513970
        Commit-Queue: Alison Gale <agale@chromium.org>
        Reviewed-by: Eshwar Stalin <estalin@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1457075}

--- chromium_src/chrome/browser/ui/browser.cc
+++ chromium_src/chrome/browser/ui/browser.cc
@@ -13,18 +13,11 @@
 #include "chrome/browser/ui/browser_command_controller.h"
 #include "chrome/browser/ui/browser_content_setting_bubble_model_delegate.h"
 
-#if !BUILDFLAG(IS_ANDROID)
-#include "brave/browser/ui/bookmark/brave_bookmark_tab_helper.h"
-#endif
-
 #define BRAVE_BROWSER_CREATE return new BraveBrowser(params);
 #define BrowserContentSettingBubbleModelDelegate \
   BraveBrowserContentSettingBubbleModelDelegate
 #define BrowserCommandController BraveBrowserCommandController
 #define BrowserLocationBarModelDelegate BraveLocationBarModelDelegate
-#if !BUILDFLAG(IS_ANDROID)
-#define BookmarkTabHelper BraveBookmarkTabHelper
-#endif
 #define BrowserTabStripModelDelegate BraveTabStripModelDelegate
 #define BrowserActions(...) BraveBrowserActions(__VA_ARGS__)
 
@@ -36,7 +29,3 @@
 #undef BrowserContentSettingBubbleModelDelegate
 #undef BrowserCommandController
 #undef BRAVE_BROWSER_CREATE
-
-#if !BUILDFLAG(IS_ANDROID)
-#undef BookmarkTabHelper
-#endif
