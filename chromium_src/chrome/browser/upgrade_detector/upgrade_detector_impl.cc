9878399035c1b6b243f7d11cd6e6d57f259a4561
Michael Herrmann <michael@herrmann.io>
2025-05-12 17:05:19 +0200

    Fix #include

--- chromium_src/chrome/browser/upgrade_detector/upgrade_detector_impl.cc
+++ chromium_src/chrome/browser/upgrade_detector/upgrade_detector_impl.cc
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "chrome/browser/buildflags.h"
+#include "build/build_config.h"
 
 // When the current build is more than several weeks old, upstream takes this as
 // a sign that automatic updates are broken and shows a prominent "Can't update
