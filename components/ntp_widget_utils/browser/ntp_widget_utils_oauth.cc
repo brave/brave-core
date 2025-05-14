5f9ac3c5f5895a2b0d4e93d68be87ef548c5e271
Claudio DeSouza <cdesouza@brave.com>
2025-05-14 14:40:17 +0100

    [cr138] Unsafe `SHA256HashString` variant deleted
    This change corrects several places to use modernised versions of
    `SHA256Hash*`.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/adc2dcebb5cc1de43f8b4ce958e50a39d9d1f4d1
    
    commit adc2dcebb5cc1de43f8b4ce958e50a39d9d1f4d1
    Author: Elly <ellyjones@chromium.org>
    Date:   Mon May 12 16:29:56 2025 -0700
    
        crypto/sha2: remove truncating sha256
    
        This change removes:
    
          crypto::SHA256HashString(string_view, void*, size_t)
    
        which accepted an output buffer as a raw void*. This function was
        especially pernicious because passing a too-small buffer would result in
        the hash being truncated, and passing a too-big buffer would result in
        the part of the buffer after the hash being left uninitialized. Callers
        that want this behavior now have to explicitly implement it themselves.
    
        Change-Id: I395abeb48aef84fe49f35055a3dfb2ea6f2d1772
        Bug: 372283556
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6534655
        Reviewed-by: Matt Mueller <mattm@chromium.org>
        Commit-Queue: Elly FJ <ellyjones@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1459152}

--- components/ntp_widget_utils/browser/ntp_widget_utils_oauth.cc
+++ components/ntp_widget_utils/browser/ntp_widget_utils_oauth.cc
@@ -31,12 +31,8 @@ std::string GetCryptoRandomString(bool hex_encode) {
 
 std::string GetCodeChallenge(
     const std::string& code_verifier, bool strip_chars) {
-  char raw[crypto::kSHA256Length] = {0};
-  crypto::SHA256HashString(code_verifier,
-                           raw,
-                           crypto::kSHA256Length);
-  std::string code_challenge =
-      base::Base64Encode(std::string_view(raw, crypto::kSHA256Length));
+  auto raw = crypto::SHA256HashString(code_verifier);
+  std::string code_challenge = base::Base64Encode(raw);
 
   if (strip_chars) {
     std::replace(code_challenge.begin(), code_challenge.end(), '+', '-');
