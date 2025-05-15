d9ae60df7b6bf33fcccb1dab11af300774a3abfe
Brian Johnson <34129+bridiver@users.noreply.github.com>
2025-05-15 15:58:21 -0700

    add presubmit checks for banned JS and Profile functions (#29046)

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -352,6 +352,19 @@ chromium_presubmit_overrides.inline_presubmit('//PRESUBMIT.py', globals(),
 
 # pyright: reportUnboundVariable=false, reportUndefinedVariable=false
 
+_BANNED_JAVA_FUNCTIONS += (BanRule(
+    r'/(BraveLeoPrefUtils|Utils)\.getProfile\(\)',
+    ('Prefer passing in the Profile reference instead of relying on the '
+     'static getProfile() call. Only top level entry points '
+     '(e.g. Activities) should call ProfileManager.getLastUsedRegularProfile '
+     'instead. Otherwise, the Profile should either be passed in explicitly '
+     'or retreived from an existing entity with a reference to the Profile '
+     '(e.g. WebContents). This is a warning only for existing usages, new '
+     'usages are strictly banned.', ),
+    False,
+    excluded_paths=(r'.*Test[A-Z]?.*\.java', ),
+), )
+
 _BANNED_CPP_FUNCTIONS += (
     BanRule(
         r'/\b(Basic|W)?StringPiece(16)?\b',
@@ -372,7 +385,15 @@ _BANNED_CPP_FUNCTIONS += (
         treat_as_error=True,
         excluded_paths=[_THIRD_PARTY_EXCEPT_BLINK],
     ),
-)
+    BanRule(
+        r'/\bAllowInjectingJavaScript\(\)',
+        ('ExecuteJavaScript() should not be used outside of chrome:// urls. If '
+         'you must inject into the main world, consider using '
+         'script_injector::ScriptInjector::RequestAsyncExecuteScript(...) '
+         'instead. This is a warning only for existing usages, new usages are '
+         'strictly banned.'),
+        treat_as_error=False,
+    ))
 
 
 # Extend BanRule exclude lists with Brave-specific paths.
