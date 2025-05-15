9eeb5340f4cdbcb063f28815955eb93200d2554e
Vadym Struts <vstruts@brave.com>
2025-05-15 17:59:08 +0200

    crs: https://github.com/brave/brave-core/pull/29044#discussion_r2089799623
    Signed-off-by: Vadym Struts <vstruts@brave.com>

--- components/psst/browser/content/psst_scripts_result_handler.h
+++ components/psst/browser/content/psst_scripts_result_handler.h
@@ -7,17 +7,23 @@
 #define BRAVE_COMPONENTS_PSST_BROWSER_CONTENT_PSST_SCRIPTS_RESULT_HANDLER_H_
 
 #include <memory>
+#include <string>
 
 #include "base/component_export.h"
+#include "brave/components/psst/browser/core/matched_rule.h"
 #include "brave/components/psst/browser/core/psst_dialog_delegate.h"
 #include "brave/components/psst/common/psst_prefs.h"
+#include "brave/components/script_injector/common/mojom/script_injector.mojom.h"
 #include "content/public/browser/global_routing_id.h"
 #include "content/public/browser/web_contents.h"
+#include "mojo/public/cpp/bindings/associated_remote.h"
 
 namespace psst {
 
 class COMPONENT_EXPORT(PSST_BROWSER_CONTENT) PsstScriptsHandler {
  public:
+  using InsertScriptInPageCallback = base::OnceCallback<void(::base::Value)>;
+
   PsstScriptsHandler(const PsstScriptsHandler&) = delete;
   PsstScriptsHandler& operator=(const PsstScriptsHandler&) = delete;
 
@@ -27,6 +33,19 @@ class COMPONENT_EXPORT(PSST_BROWSER_CONTENT) PsstScriptsHandler {
   virtual void Start() = 0;
 
   virtual PsstDialogDelegate* GetPsstDialogDelegate() = 0;
+
+ private:
+  virtual void InsertUserScript(const std::optional<MatchedRule>& rule) = 0;
+
+  virtual void OnUserScriptResult(const MatchedRule& rule,
+                                  base::Value script_result) = 0;
+
+  virtual void InsertScriptInPage(const std::string& script,
+                                  std::optional<base::Value> value,
+                                  InsertScriptInPageCallback cb) = 0;
+
+  virtual mojo::AssociatedRemote<script_injector::mojom::ScriptInjector>&
+  GetRemote(content::RenderFrameHost* rfh) = 0;
 };
 
 class COMPONENT_EXPORT(PSST_BROWSER_CONTENT) PsstScriptsHandlerImpl
@@ -45,12 +64,29 @@ class COMPONENT_EXPORT(PSST_BROWSER_CONTENT) PsstScriptsHandlerImpl
   PsstDialogDelegate* GetPsstDialogDelegate() override;
 
  private:
+  void InsertUserScript(const std::optional<MatchedRule>& rule) override;
+
+  void OnUserScriptResult(const MatchedRule& rule,
+                          base::Value script_result) override;
+
+  void InsertScriptInPage(const std::string& script,
+                          std::optional<base::Value> value,
+                          InsertScriptInPageCallback cb) override;
+
+  //   Get the remote used to send the script to the renderer.
+  mojo::AssociatedRemote<script_injector::mojom::ScriptInjector>& GetRemote(
+      content::RenderFrameHost* rfh) override;
+
   std::unique_ptr<PsstDialogDelegate> delegate_;
   const raw_ptr<PrefService> prefs_;
 
   const content::GlobalRenderFrameHostId render_frame_host_id_;
   raw_ptr<content::WebContents> web_contents_{nullptr};
   const int32_t world_id_;
+
+  mojo::AssociatedRemote<script_injector::mojom::ScriptInjector>
+      script_injector_remote_;
+  base::WeakPtrFactory<PsstScriptsHandlerImpl> weak_factory_{this};
 };
 
 }  // namespace psst
