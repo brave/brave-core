2ca19a395a7fc30d41391af99fe02ea3872a86ce
AlexeyBarabash <alexey@brave.com>
2025-05-16 18:01:09 +0300

    [cr138][Android] Fixed FieldCanBeStatic Error Prone warnings
    This is a semi-automated change, made by set
    `ERRORPRONE_CHECKS_TO_APPLY = ['FieldCanBeStatic']`
    at `src/build/android/gyp/errorprone.py` and run the build with --offline flag
    and some manual adjustments
    
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/4f7d0b80759a7fd39de219f56984ec686d7aa257
    
    	Android: Enable Error Prone checks: FieldCanBeStatic, FieldCanBeFinal, FieldCanBeLocal
    
    	Leaves FieldCanBeStatic off in tests, since it's arguably nicer to use
    	non-statics in test classes for expected values.
    
    	Bug: 40661145
    	Change-Id: I9fdbc6ebed5a6aa98f435dfeccb074715531be16
    	Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6543826

--- android/java/org/chromium/chrome/browser/bookmarks/BraveBookmarkToolbar.java
+++ android/java/org/chromium/chrome/browser/bookmarks/BraveBookmarkToolbar.java
@@ -13,8 +13,8 @@ import org.chromium.base.supplier.OneshotSupplier;
 import org.chromium.chrome.R;
 
 public class BraveBookmarkToolbar extends BookmarkToolbar {
-    private final int MENU_IMPORT_ID = 100;
-    private final int MENU_EXPORT_ID = 101;
+    private static final int MENU_IMPORT_ID = 100;
+    private static final int MENU_EXPORT_ID = 101;
 
     private BraveBookmarkDelegate mBraveBookmarkDelegate;
 
