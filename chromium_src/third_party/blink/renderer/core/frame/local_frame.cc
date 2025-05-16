45827eb440de9cf2232d8585aaed998ce70c0d9a
Claudio DeSouza <cdesouza@brave.com>
2025-05-16 17:25:50 +0100

    [cr138] `Image::CurrentFrameOrientation` renamed
    This member function is now called `Orientation`.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/30a2c1f7c113663303f53a1ee75c875e416b492f
    
    commit 30a2c1f7c113663303f53a1ee75c875e416b492f
    Author: Colin Blundell <blundell@chromium.org>
    Date:   Wed May 14 23:53:12 2025 -0700
    
        [Blink] Rename two Image methods for clarity
    
        - CurrentFrameIsSingleSecurityOrigin() -> IsSingleSecurityOrigin()
        - CurrentFrameOrientation() -> Orientation()
    
        These methods refer to the whole image.
    
        Change-Id: Ibd73c171fce8ee3f52859032b11c9aee39677b25
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6544688
        Reviewed-by: Fredrik Söderquist <fs@opera.com>
        Commit-Queue: Colin Blundell <blundell@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1460523}

--- chromium_src/third_party/blink/renderer/core/frame/local_frame.cc
+++ chromium_src/third_party/blink/renderer/core/frame/local_frame.cc
@@ -92,9 +92,9 @@ SkBitmap LocalFrame::GetImageAtViewportPoint(const gfx::Point& viewport_point) {
   PaintImage paint_image = image->PaintImageForCurrentFrame();
   // Orient the data.
   if (!image->HasDefaultOrientation()) {
-    paint_image = Image::ResizeAndOrientImage(
-        paint_image, image->CurrentFrameOrientation(), gfx::Vector2dF(1, 1), 1,
-        kInterpolationNone);
+    paint_image = Image::ResizeAndOrientImage(paint_image, image->Orientation(),
+                                              gfx::Vector2dF(1, 1), 1,
+                                              kInterpolationNone);
   }
   SkBitmap bitmap;
   if (sk_sp<SkImage> sk_image = paint_image.GetSwSkImage()) {
