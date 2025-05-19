8099a0b0ef6f8a8819022ac213ae64df604de33b
Jay Harris <jay.harris@outlook.co.nz>
2025-05-20 10:16:39 +1200

    [Bookmarks Sidepanel]: Migrate to new patching approach

--- browser/resources/side_panel/bookmarks/brave_overrides/index.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024 The Brave Authors. All rights reserved.
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this file,
-// You can obtain one at https://mozilla.org/MPL/2.0/.
-
-import './cr_toolbar_search_field.js'
-import './power_bookmarks_list.js'
-import './power_bookmarks_edit_dialog.js'
--- chromium_src/chrome/browser/resources/side_panel/bookmarks/power_bookmarks_edit_dialog.ts
+++ chromium_src/chrome/browser/resources/side_panel/bookmarks/power_bookmarks_edit_dialog.ts
@@ -3,8 +3,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import { RegisterStyleOverride } from 'chrome://resources/brave/polymer_overriding.js'
-import { html } from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js'
+import { RegisterStyleOverride } from '//resources/brave/polymer_overriding.js'
+import { html } from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js'
+
+export * from './power_bookmarks_edit_dialog-chromium.js'
 
 RegisterStyleOverride('power-bookmarks-edit-dialog', html`<style>
   :host {
