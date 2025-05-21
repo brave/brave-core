622e5564af246a8897825183df51223f8e635c86
boocmp <boocmp@yandex.ru>
2025-05-21 21:56:07 +0700

    Review issues.

--- browser/ui/webui/settings/brave_extensions_manifest_v2_browsertest.cc
+++ browser/ui/webui/settings/brave_extensions_manifest_v2_browsertest.cc
@@ -17,14 +17,19 @@
 #include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
+#include "components/component_updater/component_updater_switches.h"
 #include "components/crx_file/crx_verifier.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "extensions/browser/crx_file_info.h"
 #include "extensions/browser/disable_reason.h"
+#include "extensions/browser/extension_dialog_auto_confirm.h"
 #include "extensions/browser/extension_system.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/extension_builder.h"
+#include "net/dns/mock_host_resolver.h"
+#include "net/test/embedded_test_server/embedded_test_server.h"
+#include "services/network/public/cpp/network_switches.h"
 
 namespace {
 
@@ -65,6 +70,24 @@ void NonBlockingDelay(base::TimeDelta delay) {
   base::SingleThreadTaskRunner::GetCurrentDefault()->PostDelayedTask(
       FROM_HERE, run_loop.QuitWhenIdleClosure(), delay);
   run_loop.Run();
+}
+
+std::string GupdateResponse() {
+  // For NoScript.
+  return R"json(
+    {
+      "gupdate": {
+        "app": [
+          {
+            "appid": "bgkmgpgeempochogfoddiobpbhdfgkdi",
+            "updatecheck": {
+              "codebase": "https://a.test/manifest_v2/bgkmgpgeempochogfoddiobpbhdfgkdi.crx"
+            }
+          }
+        ]
+      }
+    }
+  )json";
 }
 
 }  // namespace
@@ -115,6 +138,12 @@ class BraveExtensionsManifestV2BrowserTest : public InProcessBrowserTest {
     while (IsExtensionToggled(
                browser()->tab_strip_model()->GetActiveWebContents()) !=
            toggled) {
+      NonBlockingDelay(base::Milliseconds(10));
+    }
+  }
+
+  void WaitExtensionInstalled() {
+    while (!IsExtensionInstalled()) {
       NonBlockingDelay(base::Milliseconds(10));
     }
   }
@@ -184,11 +213,50 @@ IN_PROC_BROWSER_TEST_F(BraveExtensionsManifestV2BrowserTest,
 class BraveExtensionsManifestV2InstallerBrowserTest
     : public BraveExtensionsManifestV2BrowserTest {
  public:
+  BraveExtensionsManifestV2InstallerBrowserTest()
+      : https_server_(net::EmbeddedTestServer::TYPE_HTTPS) {}
+
+  ~BraveExtensionsManifestV2InstallerBrowserTest() override = default;
+
   void SetUp() override {
+    https_server_.SetSSLConfig(net::EmbeddedTestServer::CERT_TEST_NAMES);
+    ASSERT_TRUE(https_server_.InitializeAndListen());
+
     extensions::ChromeContentVerifierDelegate::SetDefaultModeForTesting(
         extensions::ChromeContentVerifierDelegate::VerifyInfo::Mode::
             ENFORCE_STRICT);
     BraveExtensionsManifestV2BrowserTest::SetUp();
+  }
+
+  void SetUpOnMainThread() override {
+    auto get_extension = [](const net::test_server::HttpRequest& request)
+        -> std::unique_ptr<net::test_server::HttpResponse> {
+      if (request.GetURL().path_piece() != "/extensions") {
+        return nullptr;
+      }
+
+      auto http_response =
+          std::make_unique<net::test_server::BasicHttpResponse>();
+      http_response->set_code(net::HTTP_OK);
+      http_response->set_content(GupdateResponse());
+      return http_response;
+    };
+
+    https_server_.RegisterDefaultHandler(base::BindRepeating(get_extension));
+    https_server_.ServeFilesFromDirectory(
+        base::PathService::CheckedGet(brave::DIR_TEST_DATA));
+    https_server_.StartAcceptingConnections();
+    host_resolver()->AddRule("*", "127.0.0.1");
+  }
+
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    BraveExtensionsManifestV2BrowserTest::SetUpCommandLine(command_line);
+    command_line->RemoveSwitch(switches::kComponentUpdater);
+    command_line->AppendSwitchASCII(switches::kComponentUpdater,
+                                    "url-source=https://a.test/extensions");
+    command_line->AppendSwitchASCII(
+        network::switches::kHostResolverRules,
+        "MAP *:443 " + https_server_.host_port_pair().ToString());
   }
 
   void TearDown() override {
@@ -196,10 +264,59 @@ class BraveExtensionsManifestV2InstallerBrowserTest
         std::nullopt);
     BraveExtensionsManifestV2BrowserTest::TearDown();
   }
+
+ protected:
+  net::EmbeddedTestServer https_server_;
 };
 
 IN_PROC_BROWSER_TEST_F(BraveExtensionsManifestV2InstallerBrowserTest,
-                       InstallBravePublishedExtension) {
+                       InstallExtension) {
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(
+      browser(), GURL("brave://settings/extensions/v2")));
+  auto* web_contents = browser()->tab_strip_model()->GetActiveWebContents();
+  EXPECT_FALSE(IsExtensionToggled(web_contents));
+  EXPECT_TRUE(IsExtensionToggleEnabled(web_contents));
+
+  extensions::ScopedTestDialogAutoConfirm confirm(
+      extensions::ScopedTestDialogAutoConfirm::ACCEPT);
+
+  // enable from settings.
+  ClickExtensionToggle(web_contents);
+
+  WaitExtensionToggled(true);
+  WaitExtensionInstalled();
+
+  EXPECT_TRUE(IsExtensionInstalled());
+  EXPECT_TRUE(IsExtensionEnabled());
+}
+
+IN_PROC_BROWSER_TEST_F(BraveExtensionsManifestV2InstallerBrowserTest,
+                       InstallCancelExtension) {
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(
+      browser(), GURL("brave://settings/extensions/v2")));
+  auto* web_contents = browser()->tab_strip_model()->GetActiveWebContents();
+  EXPECT_FALSE(IsExtensionToggled(web_contents));
+  EXPECT_TRUE(IsExtensionToggleEnabled(web_contents));
+
+  extensions::ScopedTestDialogAutoConfirm confirm(
+      extensions::ScopedTestDialogAutoConfirm::CANCEL);
+
+  // enable from settings.
+  ClickExtensionToggle(web_contents);
+
+  WaitExtensionToggled(false);
+  EXPECT_FALSE(IsExtensionInstalled());
+  EXPECT_FALSE(IsExtensionEnabled());
+
+  auto* extension =
+      extensions::ExtensionRegistry::Get(browser()->profile())
+          ->GetExtensionById(kExtensionId,
+                             extensions::ExtensionRegistry::EVERYTHING);
+  EXPECT_FALSE(extension);
+}
+
+IN_PROC_BROWSER_TEST_F(BraveExtensionsManifestV2InstallerBrowserTest,
+                       ExtensionWorks) {
   base::ScopedAllowBlockingForTesting allow_blocking;
 
   constexpr char kTestExtension[] =
@@ -213,9 +330,12 @@ IN_PROC_BROWSER_TEST_F(BraveExtensionsManifestV2InstallerBrowserTest,
   auto installer = extensions::CrxInstaller::CreateSilent(browser()->profile());
   installer->set_allow_silent_install(true);
   installer->set_is_gallery_install(true);
+
+  base::RunLoop run_loop;
   installer->AddInstallerCallback(base::BindLambdaForTesting(
-      [](const std::optional<extensions::CrxInstallError>& result) {
+      [&run_loop](const std::optional<extensions::CrxInstallError>& result) {
         EXPECT_FALSE(result.has_value());
+        run_loop.Quit();
       }));
 
   ui_test_utils::TabAddedWaiter tab_waiter(browser());
@@ -224,6 +344,8 @@ IN_PROC_BROWSER_TEST_F(BraveExtensionsManifestV2InstallerBrowserTest,
   crx.path = test_extension;
   crx.required_format = crx_file::VerifierFormat::CRX3;
   installer->InstallCrxFile(crx);
+
+  run_loop.Run();
 
   content::WebContents* web_contents = tab_waiter.Wait();
   content::WaitForLoadStop(web_contents);
