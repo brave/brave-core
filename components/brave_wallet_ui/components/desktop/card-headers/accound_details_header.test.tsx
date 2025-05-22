327edb17c5b055282e3d1862772bdf6dc7b958b1
Douglas Daniel <douglashdaniel@gmail.com>
2025-05-22 13:13:26 -0500

    fix(wallet): Fix Account Details Header Android

---
+++ components/brave_wallet_ui/components/desktop/card-headers/accound_details_header.test.tsx
@@ -0,0 +1,116 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import * as React from 'react'
+import { render, screen, waitFor } from '@testing-library/react'
+import { Provider } from 'react-redux'
+
+// Utils
+import BraveCoreThemeProvider from '../../../../common/BraveCoreThemeProvider'
+import { createMockStore } from '../../../utils/test-utils'
+import { reduceAddress } from '../../../utils/reduce-address'
+
+// Components
+import { AccountDetailsHeader } from './account-details-header'
+
+// Mocks
+import {
+  mockAccount,
+  mockTokenBalanceRegistry //
+} from '../../../common/constants/mocks'
+
+interface RenderComponentProps {
+  isAndroid: boolean
+  isPanel: boolean
+}
+
+describe('AccountDetailsHeader', () => {
+  const renderComponent = (props: RenderComponentProps) => {
+    const store = createMockStore({})
+    const { container } = render(
+      <Provider store={store}>
+        <BraveCoreThemeProvider>
+          <AccountDetailsHeader
+            account={mockAccount}
+            onClickMenuOption={() => {}}
+            tokenBalancesRegistry={mockTokenBalanceRegistry}
+            isAndroid={props.isAndroid}
+            isPanel={props.isPanel}
+          />
+        </BraveCoreThemeProvider>
+      </Provider>
+    )
+    return { container }
+  }
+
+  it('renders account details header desktop', async () => {
+    const { container } = renderComponent({
+      isAndroid: false,
+      isPanel: false
+    })
+    // Wait for all async operations to complete
+    await waitFor(() => {
+      // Test Mock Data
+      expect(container).toBeInTheDocument()
+      expect(screen.getByText(mockAccount.name)).toBeInTheDocument()
+      expect(screen.getByText(reduceAddress(mockAccount.address)))
+        .toBeInTheDocument()
+
+      // Should be visible on desktop
+      expect(container.querySelector(
+        '[data-key="account-balance-column"]'
+      )).toBeInTheDocument()
+
+      // Test Styles
+      expect(container.querySelector(
+        '[data-key="account-details-header"]'
+      )).toHaveStyle({
+        padding: '24px 0px'
+      })
+    })
+  })
+
+  it('renders account details header panel', async () => {
+    const { container } = renderComponent({
+      isAndroid: false,
+      isPanel: true
+    })
+
+    await waitFor(() => {
+      // Should not be visible on panel
+      expect(container.querySelector(
+        '[data-key="account-balance-column"]'
+      )).not.toBeInTheDocument()
+
+      // Test Styles
+      expect(container.querySelector(
+        '[data-key="account-details-header"]'
+      )).toHaveStyle({
+        padding: '16px'
+      })
+    })
+  })
+
+  it('renders account details header android', async () => {
+    const { container } = renderComponent({
+      isAndroid: true,
+      isPanel: false
+    })
+
+    await waitFor(() => {
+      // Should not be visible on android
+      expect(container.querySelector(
+        '[data-key="account-balance-column"]'
+      )).not.toBeInTheDocument()
+
+      // Test Styles
+      expect(container.querySelector(
+        '[data-key="account-details-header"]'
+      )).toHaveStyle({
+        padding: '16px 0px 0px 0px'
+      })
+    })
+  })
+})
