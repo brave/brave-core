d84fce1826473eac1304ef9f9532a5e8e3cc5da1
Serg <serg.zhukovsky@gmail.com>
2025-05-22 10:06:39 -0400

    Fixes presubmit warnings

--- android/java/org/chromium/chrome/browser/privacy/settings/BravePrivacySettings.java
+++ android/java/org/chromium/chrome/browser/privacy/settings/BravePrivacySettings.java
@@ -7,7 +7,6 @@ package org.chromium.chrome.browser.privacy.settings;
 
 import static org.chromium.build.NullUtil.assumeNonNull;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
@@ -17,7 +16,7 @@ import androidx.preference.PreferenceCategory;
 
 import org.chromium.base.BraveFeatureList;
 import org.chromium.base.BravePreferenceKeys;
-import org.chromium.base.ContextUtils;
+import org.chromium.base.shared_preferences.SharedPreferencesManager;
 import org.chromium.brave_shields.mojom.FilterListAndroidHandler;
 import org.chromium.brave_shields.mojom.FilterListConstants;
 import org.chromium.build.annotations.NullMarked;
@@ -465,8 +464,7 @@ public class BravePrivacySettings extends PrivacySettings implements ConnectionE
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         super.onPreferenceChange(preference, newValue);
         String key = preference.getKey();
-        SharedPreferences.Editor sharedPreferencesEditor =
-                ContextUtils.getAppSharedPreferences().edit();
+        SharedPreferencesManager preferencesManager = ChromeSharedPreferences.getInstance();
         if (PREF_HTTPS_FIRST_MODE.equals(key)) {
             UserPrefs.get(ProfileManager.getLastUsedRegularProfile())
                     .setBoolean(Pref.HTTPS_ONLY_MODE_ENABLED, (boolean) newValue);
@@ -590,7 +588,7 @@ public class BravePrivacySettings extends PrivacySettings implements ConnectionE
         } else if (PREF_FORGET_FIRST_PARTY_STORAGE.equals(key)) {
             BraveShieldsContentSettings.setForgetFirstPartyStoragePref((boolean) newValue);
         } else if (PREF_CLOSE_TABS_ON_EXIT.equals(key)) {
-            sharedPreferencesEditor.putBoolean(
+            preferencesManager.writeBoolean(
                     BravePreferenceKeys.BRAVE_CLOSE_TABS_ON_EXIT, (boolean) newValue);
         } else if (PREF_SEND_P3A.equals(key)) {
             BraveLocalState.get().setBoolean(BravePref.P3A_ENABLED, (boolean) newValue);
@@ -613,12 +611,11 @@ public class BravePrivacySettings extends PrivacySettings implements ConnectionE
             UserPrefs.get(ProfileManager.getLastUsedRegularProfile())
                     .setBoolean(BravePref.LINKED_IN_EMBED_CONTROL_TYPE, (boolean) newValue);
         } else if (PREF_CLEAR_ON_EXIT.equals(key)) {
-            sharedPreferencesEditor.putBoolean(
+            preferencesManager.writeBoolean(
                     BravePreferenceKeys.BRAVE_CLEAR_ON_EXIT, (boolean) newValue);
         } else if (PREF_APP_LINKS.equals(key)) {
-            sharedPreferencesEditor.putBoolean(PREF_APP_LINKS, (boolean) newValue);
-            ChromeSharedPreferences.getInstance()
-                    .writeBoolean(BravePrivacySettings.PREF_APP_LINKS_RESET, false);
+            preferencesManager.writeBoolean(PREF_APP_LINKS, (boolean) newValue);
+            preferencesManager.writeBoolean(BravePrivacySettings.PREF_APP_LINKS_RESET, false);
         } else if (PREF_INCOGNITO_SCREENSHOT.equals(key)) {
             BraveFeatureUtil.enableFeature(
                     BraveFeatureList.BRAVE_INCOGNITO_SCREENSHOT, (boolean) newValue, false);
@@ -649,8 +646,6 @@ public class BravePrivacySettings extends PrivacySettings implements ConnectionE
                 }
             }
         }
-
-        sharedPreferencesEditor.apply();
 
         return true;
     }
@@ -687,10 +682,9 @@ public class BravePrivacySettings extends PrivacySettings implements ConnectionE
 
         updateClearBrowsingFragment();
 
-        SharedPreferences sharedPreferences = ContextUtils.getAppSharedPreferences();
-
+        SharedPreferencesManager preferencesManager = ChromeSharedPreferences.getInstance();
         String blockAdTrackersPref = BraveShieldsContentSettings.getTrackersPref();
-        int cookiesBlockPref = sharedPreferences.getInt(PREF_BLOCK_CROSS_SITE_COOKIES, 1);
+        int cookiesBlockPref = preferencesManager.readInt(PREF_BLOCK_CROSS_SITE_COOKIES, 1);
         String fingerprintingPref = BraveShieldsContentSettings.getFingerprintingPref();
         String httpsUpgradePref = BraveShieldsContentSettings.getHttpsUpgradePref();
 
@@ -803,7 +797,7 @@ public class BravePrivacySettings extends PrivacySettings implements ConnectionE
                 webrtcPolicyToString(BravePrefServiceBridge.getInstance().getWebrtcPolicy()));
 
         mClearBrowsingDataOnExit.setChecked(
-                sharedPreferences.getBoolean(BravePreferenceKeys.BRAVE_CLEAR_ON_EXIT, false));
+                preferencesManager.readBoolean(BravePreferenceKeys.BRAVE_CLEAR_ON_EXIT, false));
 
         mFingerprntLanguagePref.setChecked(
                 UserPrefs.get(ProfileManager.getLastUsedRegularProfile())
