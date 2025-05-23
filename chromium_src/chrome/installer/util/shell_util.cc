99c2ee45d1322fdd068d5e4959e8f74fa7cc53e0
Claudio DeSouza <cdesouza@brave.com>
2025-05-23 01:10:31 +0100

    IWYU fix for `ostream` operators
    This header was being pulled transiently. This caused build failures
    once https://github.com/brave/brave-core/pull/29156 was merged.

--- chromium_src/chrome/installer/util/shell_util.cc
+++ chromium_src/chrome/installer/util/shell_util.cc
@@ -3,6 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include <ostream>
+
 #include "base/notreached.h"
 #include "brave/installer/util/brave_shell_util.h"
 #include "chrome/common/chrome_icon_resources_win.h"
