86b6a1f82e1cbe5011f0393ee23f321dabb7eab4
Stephen Heaps <steaps.4@gmail.com>
2025-05-23 10:58:36 -0400

    Add unit tests for user agent in web view when Brave user agent is masked.

--- ios/brave-ios/Tests/UserAgentTests/UserAgentTests.swift
+++ ios/brave-ios/Tests/UserAgentTests/UserAgentTests.swift
@@ -12,7 +12,9 @@ import XCTest
 
 class UserAgentTests: XCTestCase {
 
-  let desktopUARegex: (String) -> Bool = { ua in
+  // MARK: Brave User Agent
+
+  let desktopBraveUARegex: (String) -> Bool = { ua in
     let range = ua.range(
       of:
         "^Mozilla/5\\.0 \\(Macintosh; Intel Mac OS X [0-9_]+\\) AppleWebKit/[0-9\\.]+ \\(KHTML, like Gecko\\) Brave/[0-9\\.]+ Version/[0-9\\.]+ Safari/[0-9\\.]+$",
@@ -21,7 +23,7 @@ class UserAgentTests: XCTestCase {
     return range != nil
   }
 
-  let mobileUARegex: (String) -> Bool = { ua in
+  let mobileBraveUARegex: (String) -> Bool = { ua in
     let cpuPart =
       UIDevice.isPhone
       ? "\\(iPhone; CPU iPhone OS [0-9_]+ like Mac OS X\\)"
@@ -35,16 +37,63 @@ class UserAgentTests: XCTestCase {
     return range != nil
   }
 
-  // Simple test to make sure the WKWebView UA matches the expected FxiOS pattern.
+  // Simple test to make sure the WKWebView UA matches the expected Brave iOS pattern.
   func testBraveWebViewUserAgentOnPhone() {
     if UIDevice.current.userInterfaceIdiom != .phone { return }
 
-    XCTAssertTrue(mobileUARegex(UserAgent.mobile), "User agent computes correctly.")
+    XCTAssertTrue(mobileBraveUARegex(UserAgent.mobile), "User agent computes correctly.")
 
-    let expectation = self.expectation(description: "Found Firefox user agent")
+    let expectation = self.expectation(description: "Found Brave user agent")
 
     let webView = WKWebView(frame: .zero)
     webView.customUserAgent = UserAgent.mobile
+
+    webView.evaluateJavaScript("navigator.userAgent") { result, error in
+      let userAgent = result as! String
+      if !self.mobileBraveUARegex(userAgent) || self.desktopBraveUARegex(userAgent) {
+        XCTFail("User agent did not match expected pattern! \(userAgent)")
+      }
+      expectation.fulfill()
+    }
+
+    waitForExpectations(timeout: 60, handler: nil)
+  }
+
+  // MARK: Masked User Agent
+
+  let desktopUARegex: (String) -> Bool = { ua in
+    let range = ua.range(
+      of:
+        "^Mozilla/5\\.0 \\(Macintosh; Intel Mac OS X [0-9_]+\\) AppleWebKit/[0-9\\.]+ \\(KHTML, like Gecko\\) Version/[0-9\\.]+ Safari/[0-9\\.]+$",
+      options: .regularExpression
+    )
+    return range != nil
+  }
+
+  let mobileUARegex: (String) -> Bool = { ua in
+    let cpuPart =
+      UIDevice.isPhone
+      ? "\\(iPhone; CPU iPhone OS [0-9_]+ like Mac OS X\\)"
+      : "\\(iPad; CPU OS [0-9_]+ like Mac OS X\\)"
+
+    let range = ua.range(
+      of:
+        "^Mozilla/5\\.0 \(cpuPart) AppleWebKit/[0-9\\.]+ \\(KHTML, like Gecko\\) Version/[0-9\\.]+ Mobile/[A-Za-z0-9]+ Safari/[0-9\\.]+$",
+      options: .regularExpression
+    )
+    return range != nil
+  }
+
+  // Simple test to make sure the WKWebView masked UA matches the expected masked Brave iOS pattern.
+  func testBraveMaskedWebViewUserAgentOnPhone() {
+    if UIDevice.current.userInterfaceIdiom != .phone { return }
+
+    XCTAssertTrue(mobileUARegex(UserAgent.mobileMasked), "Masked user agent computes correctly.")
+
+    let expectation = self.expectation(description: "Found user agent")
+
+    let webView = WKWebView(frame: .zero)
+    webView.customUserAgent = UserAgent.mobileMasked
 
     webView.evaluateJavaScript("navigator.userAgent") { result, error in
       let userAgent = result as! String
