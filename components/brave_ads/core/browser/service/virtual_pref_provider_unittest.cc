7843d11c4b4ad36c8abf471e23f219b8252220b5
aseren <86598290+aseren@users.noreply.github.com>
2025-05-26 11:55:26 -0500

    [ads] Add SmartNTT virtual prefs support on iOS (#29161)
    * [ads] Add SmartNTT virtual prefs support on iOS
    
    * [ads] Add SmartNTT virtual prefs tests
    
    * Fix review comments

---
+++ components/brave_ads/core/browser/service/virtual_pref_provider_unittest.cc
@@ -0,0 +1,229 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "brave/components/brave_ads/core/browser/service/virtual_pref_provider.h"
+
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "base/memory/raw_ptr.h"
+#include "base/test/values_test_util.h"
+#include "base/values.h"
+#include "base/version_info/version_info.h"
+#include "brave/components/brave_ads/core/browser/service/virtual_pref_provider_util.h"
+#include "brave/components/l10n/common/test/scoped_default_locale.h"
+#include "brave/components/skus/browser/pref_names.h"
+#include "components/prefs/pref_registry_simple.h"
+#include "components/prefs/testing_pref_service.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+// npm run test -- brave_unit_tests --filter=BraveAds*
+
+namespace brave_ads {
+
+class VirtualPrefProviderDelegateMock : public VirtualPrefProvider::Delegate {
+ public:
+  MOCK_METHOD(std::string, GetChannel, (), (const));
+
+  MOCK_METHOD(std::string, GetDefaultSearchEngineName, (), (const));
+};
+
+class VirtualPrefProviderTest : public ::testing::Test {
+ public:
+  VirtualPrefProviderTest() {
+    auto delegate = std::make_unique<VirtualPrefProviderDelegateMock>();
+    delegate_ = delegate.get();
+    provider_ = std::make_unique<VirtualPrefProvider>(&pref_service_,
+                                                      std::move(delegate));
+
+    pref_service_.registry()->RegisterDictionaryPref(skus::prefs::kSkusState);
+  }
+
+  ~VirtualPrefProviderTest() override {
+    // Reset delegate to avoid dangling pointer.
+    delegate_ = nullptr;
+  }
+
+  TestingPrefServiceSimple& pref_service() { return pref_service_; }
+
+  VirtualPrefProviderDelegateMock& delegate() const { return *delegate_; }
+
+  VirtualPrefProvider& provider() const { return *provider_; }
+
+ private:
+  TestingPrefServiceSimple pref_service_;
+  raw_ptr<VirtualPrefProviderDelegateMock> delegate_ = nullptr;
+  std::unique_ptr<VirtualPrefProvider> provider_;
+};
+
+TEST_F(VirtualPrefProviderTest, BrowserBuildChannel) {
+  // Arrange
+  EXPECT_CALL(delegate(), GetChannel).WillOnce(testing::Return("release"));
+
+  // Act
+  const base::Value::Dict virtual_prefs = provider().GetPrefs();
+  const std::string* build_channel =
+      virtual_prefs.FindStringByDottedPath("[virtual]:browser.build_channel");
+  ASSERT_TRUE(build_channel);
+
+  // Assert
+  EXPECT_EQ(*build_channel, "release");
+}
+
+TEST_F(VirtualPrefProviderTest, BrowserVersion) {
+  // Act
+  const base::Value::Dict virtual_prefs = provider().GetPrefs();
+  const std::string* version =
+      virtual_prefs.FindStringByDottedPath("[virtual]:browser.version");
+  ASSERT_TRUE(version);
+
+  // Assert
+  EXPECT_EQ(*version, version_info::GetVersionNumber());
+}
+
+TEST_F(VirtualPrefProviderTest, BrowserMajorVersion) {
+  // Act
+  const base::Value::Dict virtual_prefs = provider().GetPrefs();
+  std::optional<int> major_version =
+      virtual_prefs.FindIntByDottedPath("[virtual]:browser.major_version");
+
+  // Assert
+  EXPECT_EQ(major_version, GetMajorVersion());
+}
+
+TEST_F(VirtualPrefProviderTest, BrowserMinorVersion) {
+  // Act
+  const base::Value::Dict virtual_prefs = provider().GetPrefs();
+  std::optional<int> minor_version =
+      virtual_prefs.FindIntByDottedPath("[virtual]:browser.minor_version");
+
+  // Assert
+  EXPECT_EQ(minor_version, GetMinorVersion());
+}
+
+TEST_F(VirtualPrefProviderTest, BrowserBuildVersion) {
+  // Act
+  const base::Value::Dict virtual_prefs = provider().GetPrefs();
+  std::optional<int> build_version =
+      virtual_prefs.FindIntByDottedPath("[virtual]:browser.build_version");
+
+  // Assert
+  EXPECT_EQ(build_version, GetBuildVersion());
+}
+
+TEST_F(VirtualPrefProviderTest, BrowserPatchVersion) {
+  // Act
+  const base::Value::Dict virtual_prefs = provider().GetPrefs();
+  std::optional<int> patch_version =
+      virtual_prefs.FindIntByDottedPath("[virtual]:browser.patch_version");
+
+  // Assert
+  EXPECT_EQ(patch_version, GetPatchVersion());
+}
+
+TEST_F(VirtualPrefProviderTest, OperatingSystemLocaleLanguage) {
+  // Arrange
+  const brave_l10n::test::ScopedDefaultLocale scoped_default_locale("en_KY");
+
+  // Act
+  const base::Value::Dict virtual_prefs = provider().GetPrefs();
+  const std::string* language = virtual_prefs.FindStringByDottedPath(
+      "[virtual]:operating_system.locale.language");
+  ASSERT_TRUE(language);
+
+  // Assert
+  EXPECT_EQ(*language, "en");
+}
+
+TEST_F(VirtualPrefProviderTest, OperatingSystemLocaleRegion) {
+  // Arrange
+  const brave_l10n::test::ScopedDefaultLocale scoped_default_locale("en_KY");
+
+  // Act
+  const base::Value::Dict virtual_prefs = provider().GetPrefs();
+  const std::string* region = virtual_prefs.FindStringByDottedPath(
+      "[virtual]:operating_system.locale.region");
+  ASSERT_TRUE(region);
+
+  // Assert
+  EXPECT_EQ(*region, "KY");
+}
+
+TEST_F(VirtualPrefProviderTest, OperatingSystemIsMobilePlatform) {
+  // Act
+  const base::Value::Dict virtual_prefs = provider().GetPrefs();
+  std::optional<bool> is_mobile_platform = virtual_prefs.FindBoolByDottedPath(
+      "[virtual]:operating_system.is_mobile_platform");
+
+  // Assert
+  EXPECT_EQ(is_mobile_platform, IsMobilePlatform());
+}
+
+TEST_F(VirtualPrefProviderTest, OperatingSystemName) {
+  // Act
+  const base::Value::Dict virtual_prefs = provider().GetPrefs();
+  const std::string* name =
+      virtual_prefs.FindStringByDottedPath("[virtual]:operating_system.name");
+  ASSERT_TRUE(name);
+
+  // Assert
+  EXPECT_EQ(*name, version_info::GetOSType());
+}
+
+TEST_F(VirtualPrefProviderTest, SearchEngineDefaultName) {
+  // Arrange
+  EXPECT_CALL(delegate(), GetDefaultSearchEngineName)
+      .WillOnce(testing::Return("Brave"));
+
+  // Act
+  const base::Value::Dict virtual_prefs = provider().GetPrefs();
+  const std::string* default_search_engine_name =
+      virtual_prefs.FindStringByDottedPath(
+          "[virtual]:search_engine.default_name");
+  ASSERT_TRUE(default_search_engine_name);
+
+  // Assert
+  EXPECT_EQ(*default_search_engine_name, "Brave");
+}
+
+TEST_F(VirtualPrefProviderTest, PrefsWithSkus) {
+  // Arrange
+  pref_service().SetDict(skus::prefs::kSkusState,
+                         base::Value::Dict().Set("skus:development", R"JSON({
+      "orders": {
+        "f24787ab-7bc3-46b9-bc05-65befb360cb8": {
+          "created_at": "2023-10-24T16:00:57.902289",
+          "expires_at": "2023-12-24T17:03:59.030987",
+          "last_paid_at": "2023-11-24T17:03:59.030987",
+          "location": "leo.brave.com",
+          "status": "paid"
+        }
+      }
+    })JSON"));
+
+  // Act
+  const base::Value::Dict virtual_prefs = provider().GetPrefs();
+  const base::Value::Dict* skus = virtual_prefs.FindDict("[virtual]:skus");
+  ASSERT_TRUE(skus);
+
+  // Assert
+  const base::Value::Dict expected_skus = base::test::ParseJsonDict(
+      R"JSON(
+      {
+        "development": {
+          "leo.brave.com": {
+              "created_at": "2023-10-24T16:00:57.902289",
+              "expires_at": "2023-12-24T17:03:59.030987",
+              "last_paid_at": "2023-11-24T17:03:59.030987",
+              "status": "paid"
+          }
+        }
+      })JSON");
+  EXPECT_EQ(expected_skus, *skus);
+}
+
+}  // namespace brave_ads
---
+++ components/brave_ads/core/browser/service/virtual_pref_provider_util.cc
@@ -0,0 +1,50 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "brave/components/brave_ads/core/browser/service/virtual_pref_provider_util.h"
+
+#include <cstddef>
+#include <string_view>
+#include <vector>
+
+#include "base/version.h"
+#include "base/version_info/version_info.h"
+
+namespace brave_ads {
+
+namespace {
+
+int GetVersionComponent(size_t index) {
+  const base::Version& version = version_info::GetVersion();
+  const std::vector<uint32_t>& version_components = version.components();
+  return index < version_components.size()
+             ? static_cast<int>(version_components[index])
+             : 0;
+}
+
+}  // namespace
+
+int GetMajorVersion() {
+  return GetVersionComponent(0);
+}
+
+int GetMinorVersion() {
+  return GetVersionComponent(1);
+}
+
+int GetBuildVersion() {
+  return GetVersionComponent(2);
+}
+
+int GetPatchVersion() {
+  return GetVersionComponent(3);
+}
+
+bool IsMobilePlatform() {
+  std::string_view operating_system_name = version_info::GetOSType();
+  return operating_system_name == "Android" || operating_system_name == "iOS";
+}
+
+}  // namespace brave_ads
