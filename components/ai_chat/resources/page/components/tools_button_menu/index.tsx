0ab334251a1a5cef107802a63674a5d31e310f57
Jay Harris <jay.harris@outlook.co.nz>
2025-05-26 15:22:07 +1200

    begin work on interface

--- components/ai_chat/resources/page/components/tools_button_menu/index.tsx
+++ components/ai_chat/resources/page/components/tools_button_menu/index.tsx
@@ -6,48 +6,59 @@
 import * as React from 'react'
 import ButtonMenu from '@brave/leo/react/buttonMenu'
 import classnames from '$web-common/classnames'
-import * as Mojom from '../../../common/mojom'
 import styles from './style.module.scss'
+import { useMemo } from 'react'
 
-interface Props {
-  inputText: string,
+interface Props<T> {
+  query: string,
   isToolsMenuOpen: boolean,
   setIsToolsMenuOpen: (open: boolean) => void,
-  actionList: Mojom.ActionGroup[],
-  handleActionTypeClick: (action: Mojom.ActionType) => void
+  categories: { category: string, entries: T[] }[],
+
+  handleClick: (entry: T) => void
+  getLabel: (entry: T) => string
+  getSubheading: (entry: T) => string | undefined
+
+  matchesQuery: (query: string, entry: T, category?: string) => boolean
 }
 
-export default function ToolsButtonMenu(props: Props) {
+export default function ToolsButtonMenu<T>(props: Props<T>) {
+  const filtered = useMemo(() => props.categories
+    .map(g => ({
+      ...g,
+      entries: g.entries
+        .filter(entry => props.matchesQuery(props.query, entry, g.category))
+    }))
+    .filter(g => g.entries.length > 0), [props.query, props.categories])
+
   return (
     <ButtonMenu
       className={classnames({
         [styles.buttonMenu]: true,
-        [styles.highlightFirstItem]:
-          props.isToolsMenuOpen && props.inputText.startsWith('/')
+        // If we're filtering the list, highlight the first item
+        [styles.highlightFirstItem]: !!props.query
       })}
       isOpen={props.isToolsMenuOpen}
       onClose={() => props.setIsToolsMenuOpen(false)}
     >
-      {props.actionList.map((actionGroup) => {
+      {filtered.map((category) => {
         return (
-          <React.Fragment key={actionGroup.category}>
+          <React.Fragment key={category.category}>
             <div className={styles.menuSectionTitle}>
-              {actionGroup.category}
+              {category.category}
             </div>
-            {actionGroup.entries.map((entry, i) => {
-              if (entry.subheading) {
-                return <div key={i} className={styles.menuSubtitle}>{entry.subheading}</div>
+            {category.entries.map((entry, i) => {
+              const subheading = props.getSubheading(entry)
+              if (subheading) {
+                return <div key={i} className={styles.menuSubtitle}>{subheading}</div>
               } else {
+                const label = props.getLabel(entry)
                 return (
                   <leo-menu-item
                     key={i}
-                    onClick={() =>
-                      props.handleActionTypeClick(
-                        entry.details?.type ?? Mojom.ActionType.UNSPECIFIED
-                      )
-                    }
+                    onClick={() => props.handleClick(entry)}
                   >
-                    {entry.details?.label}
+                    {label}
                   </leo-menu-item>
                 )
               }
