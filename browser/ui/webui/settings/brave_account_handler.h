deec07fbf3f6ec8c19a85aad93ecc38e14e96b2d
Szilard Szaloki <szilardszaloki@gmail.com>
2025-05-27 16:09:35 -0600

    Moves `brave_account.mojom` from comp to browser.

--- browser/ui/webui/settings/brave_account_handler.h
+++ browser/ui/webui/settings/brave_account_handler.h
@@ -7,7 +7,7 @@
 #define BRAVE_BROWSER_UI_WEBUI_SETTINGS_BRAVE_ACCOUNT_HANDLER_H_
 
 #include "base/memory/raw_ptr.h"
-#include "brave/components/brave_account/core/mojom/brave_account.mojom.h"
+#include "brave/browser/ui/webui/brave_account/brave_account.mojom.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 
