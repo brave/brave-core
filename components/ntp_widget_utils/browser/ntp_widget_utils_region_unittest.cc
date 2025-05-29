83366c0e63fe953f43dc7fc1dba2ac64ae0a9c49
Claudio DeSouza <cdesouza@brave.com>
2025-05-29 16:12:48 +0100

    [cr139][WIP] `kCountryIDAtInstall` prefs moved
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/868d893f72ae7bd012b66f149731c3f128d88c05
    
    commit 868d893f72ae7bd012b66f149731c3f128d88c05
    Author: Oleh Lamzin <lamzin@google.com>
    Date:   Tue May 27 08:10:41 2025 -0700
    
        rcaps: Move kCountryIDAtInstall into prefs
    
        Move country_code::kCountryIDAtInstall into regional_capabilities::prefs
        namespace.
    
        Remove the following functoins in country_code.h:
        - GetCountryIDFromPrefs as this is not used anymore and to avoid
          circular deps between regional_capabilities and country_code,
        - RegisterProfilePrefs also to avoid circular deps between country_code
          and regional_capabilities. Instead register kCountryIDAtInstall
          preference in regional_capabilities::prefs::RegisterProfilePrefs.
    
        Bug: 417690372
        Bug: 328040066
        Change-Id: I231711bbae9f546d73f0ad2a286476b12e42fd6c
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6578124
        Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
        Commit-Queue: Oleh Lamzin <lamzin@google.com>
        Reviewed-by: Gauthier Ambard <gambard@chromium.org>
        Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1465927}

--- components/ntp_widget_utils/browser/ntp_widget_utils_region_unittest.cc
+++ components/ntp_widget_utils/browser/ntp_widget_utils_region_unittest.cc
@@ -3,15 +3,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include <memory>
-
 #include "brave/components/ntp_widget_utils/browser/ntp_widget_utils_region.h"
+
+#include <memory>
 
 #include "brave/components/l10n/common/test/scoped_default_locale.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/country_codes/country_codes.h"
 #include "components/prefs/pref_service.h"
+#include "components/regional_capabilities/regional_capabilities_prefs.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -40,8 +41,8 @@ TEST_F(NTPWidgetUtilsRegionUtilTest, TestRegionAllowedAllowList) {
   auto id = country_codes::CountryId(code);
 
   auto* profile = profile_.get();
-  profile->GetPrefs()->SetInteger(country_codes::kCountryIDAtInstall,
-                                  id.Serialize());
+  profile->GetPrefs()->SetInteger(
+      regional_capabilities::prefs::kCountryIDAtInstall, id.Serialize());
 
   bool is_supported = ::ntp_widget_utils::IsRegionSupported(
       profile->GetPrefs(), supported_regions, true);
@@ -59,8 +60,8 @@ TEST_F(NTPWidgetUtilsRegionUtilTest, TestRegionUnAllowedAllowList) {
   auto id = country_codes::CountryId(code);
 
   auto* profile = profile_.get();
-  profile->GetPrefs()->SetInteger(country_codes::kCountryIDAtInstall,
-                                  id.Serialize());
+  profile->GetPrefs()->SetInteger(
+      regional_capabilities::prefs::kCountryIDAtInstall, id.Serialize());
 
   bool is_supported = ::ntp_widget_utils::IsRegionSupported(
       profile->GetPrefs(), supported_regions, true);
@@ -78,8 +79,8 @@ TEST_F(NTPWidgetUtilsRegionUtilTest, TestRegionAllowedDenyList) {
   auto id = country_codes::CountryId(code);
 
   auto* profile = profile_.get();
-  profile->GetPrefs()->SetInteger(country_codes::kCountryIDAtInstall,
-                                  id.Serialize());
+  profile->GetPrefs()->SetInteger(
+      regional_capabilities::prefs::kCountryIDAtInstall, id.Serialize());
 
   bool is_supported = ::ntp_widget_utils::IsRegionSupported(
       profile->GetPrefs(), unsupported_regions, false);
@@ -97,8 +98,8 @@ TEST_F(NTPWidgetUtilsRegionUtilTest, TestRegionUnAllowedDenyList) {
   auto id = country_codes::CountryId(code);
 
   auto* profile = profile_.get();
-  profile->GetPrefs()->SetInteger(country_codes::kCountryIDAtInstall,
-                                  id.Serialize());
+  profile->GetPrefs()->SetInteger(
+      regional_capabilities::prefs::kCountryIDAtInstall, id.Serialize());
 
   bool is_supported = ::ntp_widget_utils::IsRegionSupported(
       profile->GetPrefs(), unsupported_regions, false);
