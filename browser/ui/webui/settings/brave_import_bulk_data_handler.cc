81a704b5b9c60a8b75b064256005dbe4bd0a34d6
Claudio DeSouza <cdesouza@brave.com>
2025-05-29 16:10:52 +0100

    [cr139] `chrome://welcome` removed upstream
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/47f27e7d0b65de9b02f08d0dcf8dec0008db3845
    
    commit 47f27e7d0b65de9b02f08d0dcf8dec0008db3845
    Author: dpapad <dpapad@chromium.org>
    Date:   Tue May 27 10:54:15 2025 -0700
    
        Cleanup: Remove various chrome://welcome leftovers.
    
        Bug: 40253961
        Change-Id: Ibcd22af7228b10262b94dc3905925d3ef9354650
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6580550
        Reviewed-by: Lei Zhang <thestig@chromium.org>
        Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
        Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
        Reviewed-by: Tim <tjudkins@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1466039}

--- browser/ui/webui/settings/brave_import_bulk_data_handler.cc
+++ browser/ui/webui/settings/brave_import_bulk_data_handler.cc
@@ -19,6 +19,7 @@
 #include "base/functional/bind.h"
 #include "base/json/json_reader.h"
 #include "base/rand_util.h"
+#include "brave/browser/ui/webui/welcome_page/brave_welcome_ui_prefs.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile_attributes_entry.h"
 #include "chrome/browser/profiles/profile_attributes_storage.h"
@@ -97,7 +98,8 @@ void BraveImportBulkDataHandler::PrepareProfile(
                       CHECK(created_profile);
                       // Migrate welcome page flag to new profiles.
                       created_profile->GetPrefs()->SetBoolean(
-                          prefs::kHasSeenWelcomePage, true);
+                          brave::welcome_ui::prefs::kHasSeenBraveWelcomePage,
+                          true);
                       std::move(initialized_callback).Run(created_profile);
                     },
                     std::move(profile_callback)));
