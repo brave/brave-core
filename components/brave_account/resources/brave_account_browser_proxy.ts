17143478e9cb643eb9761000585d24219e07e496
Szilard Szaloki <szilardszaloki@gmail.com>
2025-05-28 18:25:21 -0600

    `brave://account-dialogs`

---
+++ components/brave_account/resources/brave_account_browser_proxy.ts
@@ -0,0 +1,31 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import { PasswordStrengthMeterHandler } from './password_strength_meter.mojom-webui.js'
+import type { PasswordStrengthMeterHandlerInterface } from './password_strength_meter.mojom-webui.js'
+
+export interface BraveAccountBrowserProxy {
+  password_strength_meter_handler: PasswordStrengthMeterHandlerInterface
+  closeDialog: () => void
+}
+
+export class BraveAccountBrowserProxyImpl implements BraveAccountBrowserProxy {
+  password_strength_meter_handler: PasswordStrengthMeterHandlerInterface
+
+  private constructor() {
+    this.password_strength_meter_handler =
+      PasswordStrengthMeterHandler.getRemote()
+  }
+
+  closeDialog() {
+    chrome.send('dialogClose')
+  }
+
+  static getInstance(): BraveAccountBrowserProxy {
+    return instance || (instance = new BraveAccountBrowserProxyImpl())
+  }
+}
+
+let instance: BraveAccountBrowserProxy | null = null
---
+++ components/password_strength_meter/password_strength_meter_handler.h
@@ -0,0 +1,35 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_PASSWORD_STRENGTH_METER_PASSWORD_STRENGTH_METER_HANDLER_H_
+#define BRAVE_COMPONENTS_PASSWORD_STRENGTH_METER_PASSWORD_STRENGTH_METER_HANDLER_H_
+
+#include <string>
+
+#include "brave/components/password_strength_meter/password_strength_meter.mojom.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "mojo/public/cpp/bindings/receiver.h"
+
+namespace password_strength_meter {
+
+class PasswordStrengthMeterHandler
+    : public mojom::PasswordStrengthMeterHandler {
+ public:
+  explicit PasswordStrengthMeterHandler(
+      mojo::PendingReceiver<mojom::PasswordStrengthMeterHandler> handler);
+  ~PasswordStrengthMeterHandler() override;
+
+  void GetPasswordStrength(
+      const std::string& password,
+      mojom::PasswordStrengthMeterHandler::GetPasswordStrengthCallback callback)
+      override;
+
+ private:
+  mojo::Receiver<mojom::PasswordStrengthMeterHandler> handler_;
+};
+
+}  // namespace password_strength_meter
+
+#endif  // BRAVE_COMPONENTS_PASSWORD_STRENGTH_METER_PASSWORD_STRENGTH_METER_HANDLER_H_
