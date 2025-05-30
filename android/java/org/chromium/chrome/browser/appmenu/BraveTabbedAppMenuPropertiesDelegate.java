d2108929fc8de76aa7f29b7bcb1ea9548f15d120
Artem Samoilenko <artem@brave.com>
2025-05-30 09:30:01 -0400

    [cr139][Android] Changes for updateBookmarkMenuItemShortcut
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/3e37737aa5dd9988c545b44c731e5d6e21fa24a3
    
    commit 3e37737aa5dd9988c545b44c731e5d6e21fa24a3
    Author: Ted Choc <tedchoc@chromium.org>
    Date:   Tue May 27 14:32:49 2025 -0700
    
        Migrate Custom Tabs app menu to build property models without XML.
    
        Bug: 40145539
        Change-Id: Idfe73dc32ebfb90c1a06d81eb0ffa4660aca1822
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6583735
        Reviewed-by: Theresa Sullivan <twellington@chromium.org>
        Commit-Queue: Ted Choc <tedchoc@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1466162}

--- android/java/org/chromium/chrome/browser/appmenu/BraveTabbedAppMenuPropertiesDelegate.java
+++ android/java/org/chromium/chrome/browser/appmenu/BraveTabbedAppMenuPropertiesDelegate.java
@@ -322,7 +322,7 @@ public class BraveTabbedAppMenuPropertiesDelegate extends TabbedAppMenuPropertie
         // By this we forcibly initialize BookmarkBridge
         MenuItem bookmarkItem = menu.findItem(R.id.bookmark_this_page_id);
         if (bookmarkItem != null && currentTab != null) {
-            updateBookmarkMenuItemShortcut(bookmarkItem, currentTab, /* fromCct= */ false);
+            updateBookmarkMenuItemShortcut(bookmarkItem, null, currentTab);
         }
 
         // Remove unused dividers. This needs to be done after the visibility of all the items is
