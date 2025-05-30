34ff5ffc3f04121acec43f1673bf8202c7b8c4e7
Brandon T <JustBrandonT@gmail.com>
2025-05-29 23:54:34 -0400

    Fix binding multiple untrusted interfaces to the same URL not working

--- chromium_src/ios/web/public/web_state.h
+++ chromium_src/ios/web/public/web_state.h
@@ -6,6 +6,7 @@
 #ifndef BRAVE_CHROMIUM_SRC_IOS_WEB_PUBLIC_WEB_STATE_H_
 #define BRAVE_CHROMIUM_SRC_IOS_WEB_PUBLIC_WEB_STATE_H_
 
+#include <set>
 #include <string_view>
 
 #include "ios/components/webui/web_ui_url_constants.h"
@@ -22,18 +23,21 @@
           callback) {                                                        \
     DCHECK(!url.is_empty() && url.is_valid() &&                              \
            url.SchemeIs(kChromeUIUntrustedScheme));                          \
-    untrusted_callbacks_.emplace(std::string(url.host()), Interface::Name_); \
+    untrusted_callbacks_[url.host()].insert(Interface::Name_);               \
                                                                              \
     AddInterface(                                                            \
         Interface::Name_,                                                    \
         base::BindRepeating(&WrapCallback<Interface>, std::move(callback))); \
   }                                                                          \
                                                                              \
+  void RemoveUntrustedInterface(const GURL& origin,                          \
+                                std::string_view interface_name);            \
+                                                                             \
   bool IsAllowedForOrigin(const GURL& origin,                                \
                           std::string_view interface_name);                  \
                                                                              \
  private:                                                                    \
-  std::map<std::string, std::string, std::less<>> untrusted_callbacks_
+  std::map<std::string, std::set<std::string>, std::less<>> untrusted_callbacks_
 
 #include "src/ios/web/public/web_state.h"  // IWYU pragma: export
 
