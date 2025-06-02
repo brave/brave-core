476cf2e1e76dd3dfebf45d3ad616a00e3970d157
Simon Hong <shong@brave.com>
2025-06-02 21:10:47 +0900

    Hide split tabs toolbar button (#29305)
    Resolves brave/brave-browser#46441
    
    Hide chromium's split tabs toolbar button always.
    We use menu button on split view separator(resizer).
    
    TEST=BraveToolbarViewTest_SideBySideEnabled.SplitTabsToolbarButtonDisabledTest,
               BraveToolbarViewTest.SplitTabsToolbarButtonDisabledTest

--- browser/ui/views/toolbar/brave_toolbar_view.cc
+++ browser/ui/views/toolbar/brave_toolbar_view.cc
@@ -149,6 +149,12 @@ void BraveToolbarView::Init() {
     return;
   }
 
+  // We don't use chromium's split tabs button in toolbar.
+  if (split_tabs_) {
+    auto split_tabs = container_view->RemoveChildViewT(split_tabs_);
+    split_tabs_ = nullptr;
+  }
+
   Profile* profile = browser()->profile();
 
   // We don't use divider between extensions container and other toolbar
