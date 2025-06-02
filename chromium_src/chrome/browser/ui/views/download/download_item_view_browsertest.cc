d273dffa6909e0a8520647fc7301615abd2c1871
Claudio DeSouza <cdesouza@brave.com>
2025-06-02 20:22:39 +0100

    [cr139] Migrate `DownloadItemViewTest` overrides
    `DownloadItemViewTest` is now a browser test, and this class has
    required brave overrides to build that have to be migrated to the new
    file path.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/01d331fab57f0b8582ea4645e47145db4f8a9c03
    
    commit 01d331fab57f0b8582ea4645e47145db4f8a9c03
    Author: Dominik Klemba <tacet@google.com>
    Date:   Fri May 30 11:58:40 2025 -0700
    
        Migrate DownloadItemViewTest into InProcessBrowserTest
    
        Use of TestWithBrowserView is being deprecated and existing
        tests requiring a Browser environment are being migrated to
        being browser tests.
    
        This CL updates the DownloadItemViewTest fixture to an
        InProcessBrowserTest and removes the TestWithBrowserView
        dependency.
    
        Bug: 417766643
        Change-Id: I4fca510978fded16e42e7d80af383bdff96df455
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6591465
        Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
        Commit-Queue: Dominik Klemba <tacet@google.com>
        Reviewed-by: Dominic Battré <battre@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1467596}

--- chromium_src/chrome/browser/ui/views/download/download_item_view_browsertest.cc
+++ chromium_src/chrome/browser/ui/views/download/download_item_view_browsertest.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
@@ -6,5 +6,5 @@
 #include "brave/browser/ui/views/download/brave_download_item_view.h"
 
 #define DownloadItemView BraveDownloadItemView
-#include "src/chrome/browser/ui/views/download/download_item_view_unittest.cc"
+#include "src/chrome/browser/ui/views/download/download_item_view_browsertest.cc"
 #undef DownloadItemView
