738b8117dfad0a0a0a07fd3c94c3354285013f67
Arthur Edelstein <aedelstein@brave.com>
2025-06-04 16:15:32 -0700

    client_hints_contoller_delegate

---
+++ chromium_src/content/public/browser/client_hints_controller_delegate.h
@@ -0,0 +1,11 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CONTENT_PUBLIC_BROWSER_CLIENT_HINTS_CONTROLLER_DELEGATE_H_
+#define BRAVE_CHROMIUM_SRC_CONTENT_PUBLIC_BROWSER_CLIENT_HINTS_CONTROLLER_DELEGATE_H_
+
+#include "src/content/public/browser/client_hints_controller_delegate.h"  // IWYU pragma: export
+
+#endif  // BRAVE_CHROMIUM_SRC_CONTENT_PUBLIC_BROWSER_CLIENT_HINTS_CONTROLLER_DELEGATE_H_
