f350efa25d36ab277210366d2b230c18d4ecc2fc
Claudio DeSouza <cdesouza@brave.com>
2025-06-04 11:02:47 +0100

    Update from Chromium 138.0.7204.4 to Chromium 139.0.7219.1.

--- chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
+++ chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
@@ -546,9 +546,9 @@ bool ParseCertificatesFile(std::string_view certs_input,
                            Pinsets* pinsets,
                            base::Time* timestamp) {
   constexpr std::string_view brave_certs = R"brave_certs(
-# Last updated: Tue Jun 03 22:43:34 2025
+# Last updated: Wed Jun 04 11:02:47 2025
 PinsListTimestamp
-1748987014
+1749031367
 
 # =====BEGIN BRAVE ROOTS ASC=====
 #From https://www.amazontrust.com/repository/
