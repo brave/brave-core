28eb4d891ecb36e8c30300955d643f6de57967a5
Arthur Edelstein <aedelstein@brave.com>
2025-06-04 18:45:22 -0700

    hide brave brand when showBraveBrand == false

--- chromium_src/components/client_hints/browser/client_hints.cc
+++ chromium_src/components/client_hints/browser/client_hints.cc
@@ -3,15 +3,34 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+#include <iostream>
+
 #include "components/client_hints/browser/client_hints.h"
 
 #include "src/components/client_hints/browser/client_hints.cc"
 
 namespace client_hints {
 
+namespace {
+constexpr char kGoogleChromeBrand[] = "Google Chrome";
+}  // namespace
+
 blink::UserAgentMetadata ClientHints::BraveGetUserAgentMetadata(
     bool showBraveBrand) {
-  return GetUserAgentMetadata();
+  std::cout << "ClientHints::BraveGetUserAgentMetadata"
+            << ", showBraveBrand=" << showBraveBrand << std::endl;
+  auto metadata = GetUserAgentMetadata();
+  if (!showBraveBrand) {
+    for (auto& brand_version : metadata.brand_version_list) {
+      std::cout << "brand=" << brand_version.brand << ", version=" << brand_version.version
+                << std::endl;
+      if (brand_version.brand == "Brave") {
+        brand_version.brand = kGoogleChromeBrand;
+        break;
+      }
+    }
+  }
+  return metadata;
 }
 
 }  // namespace client_hints
