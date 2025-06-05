1856cba94df29328764b437556cab66f9c4eda8d
Sangwoo Ko <sangwoo108@gmail.com>
2025-06-05 18:15:44 +0900

    Attach proper content client creator for unittests

--- test/base/run_all_unittests.cc
+++ test/base/run_all_unittests.cc
@@ -8,6 +8,8 @@
 #include "base/logging.h"
 #include "base/test/launcher/unit_test_launcher.h"
 #include "base/test/test_io_thread.h"
+#include "brave/browser/brave_content_browser_client.h"
+#include "brave/common/brave_content_client.h"
 #include "brave/test/base/brave_unit_test_suite.h"
 #include "build/build_config.h"
 #include "content/public/test/unittest_test_suite.h"
@@ -17,11 +19,32 @@
 #include "chrome/install_static/test/scoped_install_details.h"
 #endif
 
-int main(int argc, char **argv) {
+namespace {
+
+class BraveContentBrowserClientWithoutNetworkServiceInitialization
+    : public BraveContentBrowserClient {
+ public:
+  // content::ContentBrowserClient:
+  // Skip some production Network Service code that doesn't work in unit tests.
+  void OnNetworkServiceCreated(
+      network::mojom::NetworkService* network_service) override {}
+};
+
+std::unique_ptr<content::UnitTestTestSuite::ContentClients>
+CreateContentClients() {
+  auto clients = std::make_unique<content::UnitTestTestSuite::ContentClients>();
+  clients->content_client = std::make_unique<BraveContentClient>();
+  clients->content_browser_client = std::make_unique<
+      BraveContentBrowserClientWithoutNetworkServiceInitialization>();
+  return clients;
+}
+
+}  // namespace
+
+int main(int argc, char** argv) {
   content::UnitTestTestSuite test_suite(
       new BraveUnitTestSuite(argc, argv),
-      base::BindRepeating(
-          content::UnitTestTestSuite::CreateTestContentClients));
+      base::BindRepeating(&CreateContentClients));
 
   base::TestIOThread test_io_thread(base::TestIOThread::kAutoStart);
   mojo::core::ScopedIPCSupport ipc_support(
