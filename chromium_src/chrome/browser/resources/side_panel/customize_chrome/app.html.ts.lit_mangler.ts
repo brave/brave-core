4ee041bb71147d4f467294d9fbf33babfa592854
Sangwoo Ko <sko@brave.com>
2025-06-10 07:04:14 +0900

    [Customize Chrome Panel] Show "Customize toolbar" button (#29446)
    * Replace "Shortcut" section with "Customize toolbar" button
    
    Following our the latest design guidelines, this patch replaces
    the "Shortcut" section with a "Customize toolbar" button
    in the "Theme and Color Section".
    
    <img width="319" alt="image" src="https://github.com/user-attachments/assets/63337d88-b47d-429e-a397-c51573f80f77" />

---
+++ chromium_src/chrome/browser/resources/side_panel/customize_chrome/app.html.ts.lit_mangler.ts
@@ -0,0 +1,44 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import { mangle } from 'lit_mangler'
+
+// Update "Customize toolbar" button's icon
+mangle(
+  (element) => {
+    const el = element.querySelector('#toolbarButton')
+    if (!el) {
+      throw new Error('[Customize Chrome] #toolbarButton is gone.')
+    }
+
+    // Replace existing chevron icon with a new icon and put it at the start
+    // of the button.
+    const icon = el.querySelector('cr-icon[icon="cr:chevron-right"]')
+    if (!icon) {
+      throw new Error(
+        '[Customize Chrome] #toolbarButton does not have a chevron icon.',
+      )
+    }
+    icon.setAttribute('icon', 'window-edit')
+    icon.setAttribute('slot', 'prefix-icon')
+  },
+  (template) => template.text.includes('id="toolbarButton"'),
+)
+
+mangle(
+  (element) => {
+    const el = element.querySelector('#toolbar-customization-inner-heading')
+    if (!el) {
+      throw new Error(
+        '[Customize Chrome] #toolbar-customization-inner-heading is gone.',
+      )
+    }
+
+    // Override text content of the heading with our custom label
+    el.textContent = '$i18n{braveCustomizeMenuToolbarLabel}'
+  },
+  (template) =>
+    template.text.includes('id="toolbar-customization-inner-heading"'),
+)
