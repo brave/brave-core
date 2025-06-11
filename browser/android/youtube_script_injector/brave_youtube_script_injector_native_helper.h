f96288fe4abac17398fc4f408bf4143a594de9fa
Simone Arpe <simon.arpe@gmail.com>
2025-06-11 16:32:49 +0200

    Introduce more browser tests to check JNI method that triggers fullscreen

--- browser/android/youtube_script_injector/brave_youtube_script_injector_native_helper.h
+++ browser/android/youtube_script_injector/brave_youtube_script_injector_native_helper.h
@@ -7,17 +7,29 @@
 #define BRAVE_BROWSER_ANDROID_YOUTUBE_SCRIPT_INJECTOR_BRAVE_YOUTUBE_SCRIPT_INJECTOR_NATIVE_HELPER_H_
 
 #include "base/android/scoped_java_ref.h"
+#include "content/public/browser/web_contents.h"
 #include "url/gurl.h"
 
 namespace youtube_script_injector {
 
+#if defined(BRAVE_JNI_UNUSED_FOR_TESTS)
+#define BRAVE_JNI_MARK_UNUSED_FOR_TESTS __attribute__((unused))
+#else
+#define BRAVE_JNI_MARK_UNUSED_FOR_TESTS
+#endif
+
 static jboolean JNI_BraveYouTubeScriptInjectorNativeHelper_IsYouTubeVideo(
     JNIEnv* env,
-    const base::android::JavaParamRef<jobject>& jweb_contents);
+    const base::android::JavaParamRef<jobject>& jweb_contents)
+    BRAVE_JNI_MARK_UNUSED_FOR_TESTS;
 
 static void JNI_BraveYouTubeScriptInjectorNativeHelper_SetFullscreen(
     JNIEnv* env,
-    const base::android::JavaParamRef<jobject>& jweb_contents);
+    const base::android::JavaParamRef<jobject>& jweb_contents)
+    BRAVE_JNI_MARK_UNUSED_FOR_TESTS;
+
+// Non-static free function that can be easily tested.
+void SetFullscreen(content::WebContents* web_contents);
 
 }  // namespace youtube_script_injector
 
---
+++ browser/android/youtube_script_injector/brave_youtube_script_injector_native_helper_browsertest.cc
@@ -0,0 +1,326 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "brave/browser/android/youtube_script_injector/brave_youtube_script_injector_native_helper.h"
+
+#include <string>
+
+#include "base/functional/callback.h"
+#include "base/logging.h"
+#include "base/path_service.h"
+#include "base/run_loop.h"
+#include "base/time/time.h"
+#include "base/timer/timer.h"
+#include "brave/components/constants/brave_paths.h"
+#include "chrome/test/base/android/android_browser_test.h"
+#include "chrome/test/base/chrome_test_utils.h"
+#include "chrome/test/base/testing_browser_process.h"
+#include "content/public/common/content_client.h"
+#include "content/public/test/browser_test.h"
+#include "content/public/test/browser_test_utils.h"
+#include "content/public/test/content_mock_cert_verifier.h"
+#include "content/public/test/test_navigation_observer.h"
+#include "net/dns/mock_host_resolver.h"
+#include "net/test/embedded_test_server/embedded_test_server.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace {
+const base::TimeDelta kPredicateCheckFrequency = base::Milliseconds(200);
+}  // namespace
+
+class TestPredicateWaiter {
+ public:
+  explicit TestPredicateWaiter(
+      const base::RepeatingCallback<bool(void)>& is_fulfilled)
+      : is_fulfilled_(is_fulfilled) {}
+
+  ~TestPredicateWaiter() = default;
+
+  void Wait() {
+    if (is_fulfilled_.Run()) {
+      return;
+    }
+
+    timer_.Start(FROM_HERE, kPredicateCheckFrequency, this,
+                 &TestPredicateWaiter::CheckPredicate);
+    run_loop_.Run();
+    ASSERT_TRUE(is_fulfilled_.Run());
+  }
+
+ private:
+  void CheckPredicate() {
+    if (is_fulfilled_.Run()) {
+      run_loop_.Quit();
+      timer_.Stop();
+    } else {
+      timer_.Reset();
+    }
+  }
+
+  base::RepeatingCallback<bool(void)> is_fulfilled_;
+  base::RepeatingTimer timer_;
+  base::RunLoop run_loop_;
+};
+
+class BraveYouTubeScriptInjectorNativeHelperBrowserTest
+    : public PlatformBrowserTest {
+ public:
+  BraveYouTubeScriptInjectorNativeHelperBrowserTest()
+      : https_server_(net::EmbeddedTestServer::TYPE_HTTPS) {}
+
+  ~BraveYouTubeScriptInjectorNativeHelperBrowserTest() override = default;
+
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    PlatformBrowserTest::SetUpCommandLine(command_line);
+    mock_cert_verifier_.SetUpCommandLine(command_line);
+  }
+
+  void SetUpInProcessBrowserTestFixture() override {
+    PlatformBrowserTest::SetUpInProcessBrowserTestFixture();
+    mock_cert_verifier_.SetUpInProcessBrowserTestFixture();
+  }
+
+  void SetUpOnMainThread() override {
+    PlatformBrowserTest::SetUpOnMainThread();
+    mock_cert_verifier_.mock_cert_verifier()->set_default_result(net::OK);
+    host_resolver()->AddRule("*", "127.0.0.1");
+
+    base::FilePath test_data_dir = GetTestDataDir();
+
+    https_server_.ServeFilesFromDirectory(test_data_dir);
+    content::SetupCrossSiteRedirector(&https_server_);
+    ASSERT_TRUE(https_server_.Start());
+  }
+
+  base::FilePath GetTestDataDir() {
+    base::ScopedAllowBlockingForTesting allow_blocking;
+    return base::PathService::CheckedGet(brave::DIR_TEST_DATA);
+  }
+
+  void TearDownInProcessBrowserTestFixture() override {
+    mock_cert_verifier_.TearDownInProcessBrowserTestFixture();
+    PlatformBrowserTest::TearDownInProcessBrowserTestFixture();
+  }
+
+  content::WebContents* web_contents() {
+    return chrome_test_utils::GetActiveWebContents(this);
+  }
+
+  void InjectScript(const std::u16string& script) {
+    content::RenderFrameHost* frame = web_contents()->GetPrimaryMainFrame();
+    frame->ExecuteJavaScriptForTests(script, base::NullCallback(),
+                                     content::ISOLATED_WORLD_ID_GLOBAL);
+  }
+
+  bool WaitForJsResult(content::WebContents* web_contents,
+                       const std::string& script) {
+    bool fulfilled = false;
+    TestPredicateWaiter waiter(base::BindRepeating(
+        [](content::WebContents* web_contents, const std::string& script,
+           bool* fulfilled) {
+          bool result = content::EvalJs(web_contents, script).ExtractBool();
+          if (result) {
+            *fulfilled = true;
+          }
+          return result;
+        },
+        web_contents, script, &fulfilled));
+    waiter.Wait();
+    return fulfilled;
+  }
+
+ protected:
+  // Must use HTTPS because `youtube.com` is in Chromium's HSTS preload list.
+  net::EmbeddedTestServer https_server_;
+
+ private:
+  content::ContentMockCertVerifier mock_cert_verifier_;
+};
+
+// TESTS
+
+IN_PROC_BROWSER_TEST_F(BraveYouTubeScriptInjectorNativeHelperBrowserTest,
+                       SetFullscreenFromPausedVideo) {
+  const GURL url = https_server_.GetURL("youtube.com", "/yt_fullscreen.html");
+  content::NavigateToURLBlockUntilNavigationsComplete(web_contents(), url, 1,
+                                                      true);
+
+  // Video is not playing initially.
+  ASSERT_TRUE(
+      content::EvalJs(
+          web_contents(),
+          "document.querySelector('video.html5-main-video').paused === true")
+          .ExtractBool());
+
+  // The document is not in fullscreen mode.
+  ASSERT_TRUE(
+      content::EvalJs(web_contents(), "document.fullscreenElement === null")
+          .ExtractBool());
+
+  youtube_script_injector::SetFullscreen(web_contents());
+  // Wait for the resize to complete triggered by fullscreen change.
+  content::WaitForResizeComplete(web_contents());
+
+  // Check the video is in fullscreen mode.
+  EXPECT_TRUE(
+      WaitForJsResult(web_contents(), "document.fullscreenElement !== null"));
+
+  // Check the video is still playing.
+  EXPECT_TRUE(
+      content::EvalJs(
+          web_contents(),
+          "document.querySelector('video.html5-main-video').paused === false")
+          .ExtractBool());
+}
+
+IN_PROC_BROWSER_TEST_F(BraveYouTubeScriptInjectorNativeHelperBrowserTest,
+                       SetFullscreenFromPlayingVideo) {
+  const GURL url = https_server_.GetURL("youtube.com", "/yt_fullscreen.html");
+  content::NavigateToURLBlockUntilNavigationsComplete(web_contents(), url, 1,
+                                                      true);
+
+  // The document is not in fullscreen mode.
+  ASSERT_TRUE(
+      content::EvalJs(web_contents(), "document.fullscreenElement === null")
+          .ExtractBool());
+
+  // Simulate a playing video before entering fullscreen.
+  ASSERT_TRUE(content::ExecJs(
+      web_contents(),
+      "document.querySelector('video.html5-main-video').play()"));
+  youtube_script_injector::SetFullscreen(web_contents());
+
+  // Check the video is playing.
+  EXPECT_TRUE(
+      content::EvalJs(
+          web_contents(),
+          "document.querySelector('video.html5-main-video').paused === false")
+          .ExtractBool());
+  // Wait for the resize to complete triggered by fullscreen change.
+  content::WaitForResizeComplete(web_contents());
+
+  // Check the video is in fullscreen mode.
+  EXPECT_TRUE(
+      WaitForJsResult(web_contents(), "document.fullscreenElement !== null"));
+
+  // Check the video is still playing.
+  EXPECT_TRUE(
+      content::EvalJs(
+          web_contents(),
+          "document.querySelector('video.html5-main-video').paused === false")
+          .ExtractBool());
+}
+
+IN_PROC_BROWSER_TEST_F(BraveYouTubeScriptInjectorNativeHelperBrowserTest,
+                       VideoIsPlayedFromFullscreenState) {
+  const GURL url = https_server_.GetURL("youtube.com", "/yt_fullscreen.html");
+  content::NavigateToURLBlockUntilNavigationsComplete(web_contents(), url, 1,
+                                                      true);
+
+  // Video is not playing.
+  ASSERT_TRUE(
+      content::EvalJs(
+          web_contents(),
+          "document.querySelector('video.html5-main-video').paused === true")
+          .ExtractBool());
+  // Simulate fullscreen button click to enter fullscreen mode.
+  ASSERT_TRUE(content::ExecJs(
+      web_contents(),
+      "document.querySelector('button.fullscreen-icon').click()"));
+  // Wait for the resize to complete triggered by fullscreen change.
+  content::WaitForResizeComplete(web_contents());
+  // Assert the video is in fullscreen mode.
+  ASSERT_TRUE(
+      WaitForJsResult(web_contents(), "document.fullscreenElement !== null"));
+
+  youtube_script_injector::SetFullscreen(web_contents());
+
+  // Check the video is now playing.
+  EXPECT_TRUE(
+      content::EvalJs(
+          web_contents(),
+          "document.querySelector('video.html5-main-video').paused === false")
+          .ExtractBool());
+}
+
+IN_PROC_BROWSER_TEST_F(BraveYouTubeScriptInjectorNativeHelperBrowserTest,
+                       NoOpIfPlayerIsNotFound) {
+  // Load a page without a video.
+  const GURL url = https_server_.GetURL("youtube.com", "/ytcftg_mock.html");
+  content::NavigateToURLBlockUntilNavigationsComplete(web_contents(), url, 1,
+                                                      true);
+
+  // Assert that movie player is not present.
+  ASSERT_TRUE(
+      content::EvalJs(web_contents(),
+                      "document.getElementById('movie_player') === null")
+          .ExtractBool());
+
+  std::string dom_before =
+      content::EvalJs(web_contents(), "document.body.innerHTML")
+          .ExtractString();
+  // Attempt to set fullscreen, which should not change anything.
+  youtube_script_injector::SetFullscreen(web_contents());
+
+  std::string dom_after =
+      content::EvalJs(web_contents(), "document.body.innerHTML")
+          .ExtractString();
+  // Assert that the DOM remains unchanged.
+  EXPECT_EQ(dom_before, dom_after);
+}
+
+IN_PROC_BROWSER_TEST_F(BraveYouTubeScriptInjectorNativeHelperBrowserTest,
+                       SetFullscreenOnElementsLoadingDelayed) {
+  const GURL url =
+      https_server_.GetURL("youtube.com", "/yt_fullscreen_delayed.html");
+  content::NavigateToURLBlockUntilNavigationsComplete(web_contents(), url, 1,
+                                                      true);
+
+  // Simluate delayed script load by presenting a player ('movie_player') that
+  // is not immediately available. Assert that movie player is present.
+  ASSERT_TRUE(
+      content::EvalJs(web_contents(),
+                      "document.getElementById('movie_player') !== null")
+          .ExtractBool());
+
+  // Assert that the video element (inside the player) is not present initially.
+  ASSERT_TRUE(content::EvalJs(
+                  web_contents(),
+                  "document.querySelector('video.html5-main-video') === null")
+                  .ExtractBool());
+  // Assert that the button (inside the player) to trigger fullscreen is not
+  // present initially.
+  ASSERT_TRUE(content::EvalJs(
+                  web_contents(),
+                  "document.querySelector('button.fullscreen-icon') === null")
+                  .ExtractBool());
+
+  youtube_script_injector::SetFullscreen(web_contents());
+
+  // Inject a script to simulate delayed loading of the video element fullscreen
+  // button.
+  InjectScript(u"window.simulateDelayedScriptLoad();");
+  // Assert that the video element is now present.
+  ASSERT_TRUE(content::EvalJs(
+                  web_contents(),
+                  "document.querySelector('video.html5-main-video') !== null")
+                  .ExtractBool());
+  // Assert that the button to trigger fullscreen is now present.
+  ASSERT_TRUE(content::EvalJs(
+                  web_contents(),
+                  "document.querySelector('button.fullscreen-icon') !== null")
+                  .ExtractBool());
+
+  // Wait for the mutation observer to complete and trigger fullscreen mode.
+  EXPECT_TRUE(
+      WaitForJsResult(web_contents(), "document.fullscreenElement !== null"));
+
+  // Check the video is playing.
+  EXPECT_TRUE(
+      content::EvalJs(
+          web_contents(),
+          "document.querySelector('video.html5-main-video').paused === false")
+          .ExtractBool());
+}
