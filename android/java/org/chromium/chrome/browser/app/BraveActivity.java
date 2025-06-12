2734ef6d97dba372accd27717a9cca19c01270d7
Simone Arpe <simon.arpe@gmail.com>
2025-06-12 15:40:52 +0200

    Add customize menu item to main menu
    # Conflicts:
    #	android/java/org/chromium/chrome/browser/appmenu/BraveTabbedAppMenuPropertiesDelegate.java

--- android/java/org/chromium/chrome/browser/app/BraveActivity.java
+++ android/java/org/chromium/chrome/browser/app/BraveActivity.java
@@ -503,6 +503,8 @@ public abstract class BraveActivity extends ChromeActivity
             enableSpeedreaderMode();
         } else if (id == R.id.brave_leo_id) {
             openBraveLeo();
+        } else if (id == R.id.brave_customize_menu_id) {
+            openCustomizeMenu();
         } else {
             return false;
         }
@@ -2131,6 +2133,11 @@ public abstract class BraveActivity extends ChromeActivity
         if (currentTab != null) {
             BraveLeoUtils.openLeoUrlForTab(currentTab.getWebContents());
         }
+    }
+
+    private void openCustomizeMenu() {
+        // Temp toast - Make sure to remove it.
+        Toast.makeText(this, "Open customize menu", Toast.LENGTH_SHORT).show();
     }
 
     public void showRewardsPage() {
