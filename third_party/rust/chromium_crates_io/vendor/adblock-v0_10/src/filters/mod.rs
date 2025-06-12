ff2b68e50b4621f8dc1a1e7f5a9560829e4a155b
boocmp <boocmp@yandex.ru>
2025-06-12 10:45:58 +0700

    Update Adblock version to 0.10.0

---
+++ third_party/rust/chromium_crates_io/vendor/adblock-v0_10/src/filters/mod.rs
@@ -0,0 +1,10 @@
+//! Contains representations and standalone behaviors of individual filter rules.
+
+mod abstract_network;
+mod network_matchers;
+
+pub mod cosmetic;
+pub(crate) mod fb_network;
+pub(crate) mod flat_filter_map;
+pub mod network;
+pub(crate) mod unsafe_tools;
--- third_party/rust/chromium_crates_io/vendor/adblock-v0_9/src/filters/mod.rs
+++
@@ -1,4 +0,0 @@
-//! Contains representations and standalone behaviors of individual filter rules.
-
-pub mod cosmetic;
-pub mod network;
