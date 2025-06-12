53c42e09241ecf94eafb53ce01e7eaa9edabe62a
Claudio DeSouza <cdesouza@brave.com>
2025-06-12 01:23:42 +0100

    [cr139] Adding `PermissionContextBase` overrides
    `ContentSettingPermissionContextBase` has been broken in two classes
    with `PermissionContextBase` serving as the base class.
    
    This change moves some of the existing override to
    `PermissionContextBase`, to allow the brave version to have the same
    customisation points as before.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/61cf70f0843c9721888ad04c23431d203469bc4d
    
    commit 61cf70f0843c9721888ad04c23431d203469bc4d
    Author: Florian Jacky <fjacky@google.com>
    Date:   Wed Jun 11 04:39:11 2025 -0700
    
        [PermissionOptions] Prepare PCB to support permissions with options
    
        - Support WebsiteSetting values (i.e. base::Value) in PermissionContextBase
        - Create subclass ContentSettingPermissionContextBase of PermissionContextSettingBase to support all existing content setting based permissions
        - Add permission_options value to permssion_request_data, which prompts can use to pass back prompt options
        - Extend PermissionResult with retrieved_permission_data
    
        Full DD: go/multi-state-permissions-dd
    
        Change-Id: I68d16bb1512cd057363f338941d74bd16c3fe430
        Bug: 411025625
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6568536
        Reviewed-by: Andy Paicu <andypaicu@chromium.org>
        Reviewed-by: David Roger <droger@chromium.org>
        Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
        Commit-Queue: Florian Jacky <fjacky@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1472321}

---
+++ chromium_src/components/permissions/permission_context_base.h
@@ -0,0 +1,24 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_PERMISSIONS_PERMISSION_CONTEXT_BASE_H_
+#define BRAVE_CHROMIUM_SRC_COMPONENTS_PERMISSIONS_PERMISSION_CONTEXT_BASE_H_
+
+namespace permissions {
+class ContentSettingPermissionContextBase;
+}  // namespace permissions
+
+#define BRAVE_PERMISSION_CONTEXT_BASE \
+  friend ContentSettingPermissionContextBase;
+#define PermissionDecided virtual PermissionDecided
+#define CleanUpRequest virtual CleanUpRequest
+
+#include "src/components/permissions/permission_context_base.h"  // IWYU pragma: export
+
+#undef BRAVE_PERMISSION_CONTEXT_BASE
+#undef PermissionDecided
+#undef CleanUpRequest
+
+#endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_PERMISSIONS_PERMISSION_CONTEXT_BASE_H_
