0334287aa2ef1971d6c219f9cd4a3e82131a8bc1
Sangwoo Ko <sangwoo108@gmail.com>
2025-06-13 10:59:52 +0900

    [lit mangler]: Try traversing up the directory tree to find mangled files (#29537)
    Sometimes mangled files are not located in a directory corresponding to
    the source file's directory. Instead, they seem to be placed at a
    directory corresponding to BUILD.gn file's directory.
    
    So when failing to find a mangled file in the source file's directory,
    try traversing up the directory tree until a mangled file is found.

--- tools/chromium_src/lit_mangler/mangle.test.ts
+++ tools/chromium_src/lit_mangler/mangle.test.ts
@@ -31,16 +31,67 @@ function* walkManglers(root=chromiumSrc) {
     }
 }
 
+// Sometimes mangled files are not in a directory corresponding to the source
+// file. This function tries to find the mangled file by traversing up the
+// directory structure and inserting '/preprocessed/' at various positions.
+// https://github.com/brave/brave-browser/issues/46817
+function findMangledFileAndOriginalFile(
+  manglerPath: string,
+  genPath: string,
+  root: string,
+): {mangledPath: string, originalPath: string} {
+  const baseName = path.basename(manglerPath)
+  const mangledPathWithoutProcessed = path.relative(
+    root,
+    path.join(genPath, manglerPath),
+  )
+  const pathParts = path
+    .relative(genPath, path.resolve(mangledPathWithoutProcessed))
+    .split(path.sep)
+
+  const assumedMangledPath = path.relative(
+    root,
+    path.join(
+      genPath,
+      manglerPath.replace(baseName, 'preprocessed/' + baseName),
+    ),
+  )
+  let mangledPath = assumedMangledPath
+  const originalPath = path.join('..', manglerPath)
+
+  let attempts = 0
+  while (attempts < pathParts.length - 1 && !fs.existsSync(mangledPath)) {
+    // Try inserting 'preprocessed' at position
+    // e.g.
+    // iter 1) a/b/preprocessed/c/file
+    // iter 2) a/preprocessed/b/c/file
+    const insertPosition = Math.max(0, pathParts.length - 2 - attempts)
+    const newPathParts = [
+      ...pathParts.slice(0, insertPosition),
+      'preprocessed',
+      ...pathParts.slice(insertPosition),
+    ]
+
+    mangledPath = path.join(genPath, ...newPathParts)
+
+    attempts++
+  }
+  if (!fs.existsSync(mangledPath)) {
+    throw new Error(`Mangled file not found: ${assumedMangledPath}`)
+  }
+
+  return {mangledPath, originalPath}
+}
+
 describe('mangled files should have up to date snapshots', () => {
     for (const mangler of walkManglers()) {
         const name = path.relative(root, mangler).replaceAll(path.sep, '/')
         it(`./${name} should match snapshot`, () => {
             const manglerPath = name.replace('.lit_mangler.ts', '')
                 .replace(`chromium_src/`, '')
-            const baseName = path.basename(manglerPath)
 
-            const mangledPath = path.relative(root, path.join(genPath, manglerPath.replace(baseName, 'preprocessed/' + baseName)))
-            const originalPath = path.join('..', manglerPath)
+            const {mangledPath, originalPath}= findMangledFileAndOriginalFile(manglerPath, genPath, root)
+
 
             const mangledText = fs.readFileSync(mangledPath, 'utf8')
                 .replaceAll('\r\n', '\n')
