3b1761a34daaff780730a4752ee441a170f83b98
vadims <118171981+vadimstruts@users.noreply.github.com>
2025-06-13 10:10:59 +0200

    Crash when clicking on shields (#29457)
    Fix for the crash on opening the Shields control panel
    
    The change was made in context of observing lots of crash reports on
    the backtrace. All crashes were related with opening Shields control
    panel and retrieving the size and coordinates of the Sields panel.
    The idea was to get rid of finding active browser window by the
    profile and storing of it, as a member across all Shields panel's
    lifetime. Instead we retrieve the browser window only at the moment
    we need it (with using another approach), and don't have to store it
    as a member.
    
    Resolves: https://github.com/brave/brave-browser/issues/46584
    
    ---------
    
    Signed-off-by: Vadym Struts <vstruts@brave.com>

--- browser/ui/webui/brave_shields/shields_panel_handler.cc
+++ browser/ui/webui/brave_shields/shields_panel_handler.cc
@@ -5,6 +5,7 @@
 
 #include "brave/browser/ui/webui/brave_shields/shields_panel_handler.h"
 
+#include <optional>
 #include <utility>
 
 #include "base/check.h"
@@ -16,17 +17,19 @@
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/browser/ui/webui/top_chrome/top_chrome_web_ui_controller.h"
+#include "chrome/browser/ui/webui/webui_embedding_context.h"
 #include "ui/gfx/geometry/vector2d.h"
 
 ShieldsPanelHandler::ShieldsPanelHandler(
     mojo::PendingReceiver<brave_shields::mojom::PanelHandler> receiver,
     TopChromeWebUIController* webui_controller,
-    BraveBrowserWindow* brave_browser_window,
     Profile* profile)
     : receiver_(this, std::move(receiver)),
       webui_controller_(webui_controller),
-      brave_browser_window_(brave_browser_window),
-      profile_(profile) {}
+      profile_(profile) {
+  CHECK(profile_);
+  CHECK(webui_controller_);
+}
 
 ShieldsPanelHandler::~ShieldsPanelHandler() = default;
 
@@ -48,10 +51,20 @@ void ShieldsPanelHandler::CloseUI() {
 }
 
 void ShieldsPanelHandler::GetPosition(GetPositionCallback callback) {
-  gfx::Vector2d vec =
-      gfx::Vector2d(brave_browser_window_->GetShieldsBubbleRect().x(),
-                    brave_browser_window_->GetShieldsBubbleRect().y());
-  std::move(callback).Run(vec);
+  auto* browser_window_interface = webui::GetBrowserWindowInterface(
+      webui_controller_->web_ui()->GetWebContents());
+  if (!browser_window_interface) {
+    std::move(callback).Run(std::nullopt);
+    return;
+  }
+  auto const* browser = browser_window_interface->GetBrowserForMigrationOnly();
+  if (!browser || !browser->window()) {
+    std::move(callback).Run(std::nullopt);
+    return;
+  }
+  auto* browser_window = BraveBrowserWindow::From(browser->window());
+  auto rect = browser_window->GetShieldsBubbleRect();
+  std::move(callback).Run(gfx::Vector2d(rect.x(), rect.y()));
 }
 
 void ShieldsPanelHandler::SetAdvancedViewEnabled(bool is_enabled) {
