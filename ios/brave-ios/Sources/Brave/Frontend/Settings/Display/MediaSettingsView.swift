b66b754c197ad7244c0fa88bb15a6fe90984f926
StephenHeaps <5314553+StephenHeaps@users.noreply.github.com>
2025-06-17 09:27:02 -0400

    feature(privacy): Add toggles for YouTube filter lists in Settings > Media on iOS (#29520)
    * Add toggles for YouTube filter lists in Settings > Media on iOS.

--- ios/brave-ios/Sources/Brave/Frontend/Settings/Display/MediaSettingsView.swift
+++ ios/brave-ios/Sources/Brave/Frontend/Settings/Display/MediaSettingsView.swift
@@ -3,6 +3,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+import BraveCore
 import BraveStrings
 import BraveUI
 import Preferences
@@ -11,6 +12,11 @@ import SwiftUI
 struct MediaSettingsView: View {
   @ObservedObject var enableBackgroundAudio = Preferences.General.mediaAutoBackgrounding
   @ObservedObject var keepYouTubeInBrave = Preferences.General.keepYouTubeInBrave
+  @ObservedObject var filterListStorage = FilterListStorage.shared
+
+  @State var youtubeRecommendationsBlocking = false
+  @State var youtubeDistractingElementsBlocking = false
+  @State var youtubeShortsBlocking = false
 
   var body: some View {
     Form {
@@ -35,12 +41,89 @@ struct MediaSettingsView: View {
               .foregroundColor(Color(.secondaryBraveLabel))
           }
         }
+        Toggle(isOn: $youtubeRecommendationsBlocking) {
+          VStack(alignment: .leading) {
+            Text(Strings.Settings.blockYoutubeRecommendationsTitle)
+            Text(Strings.Settings.blockYoutubeRecommendationsDesc)
+              .font(.footnote)
+              .foregroundColor(Color(.secondaryBraveLabel))
+          }
+        }
+        .toggleStyle(SwitchToggleStyle(tint: .accentColor))
+        Toggle(isOn: $youtubeDistractingElementsBlocking) {
+          VStack(alignment: .leading) {
+            Text(Strings.Settings.blockYouTubeDistractingElementsTitle)
+            Text(Strings.Settings.blockYouTubeDistractingElementsDesc)
+              .font(.footnote)
+              .foregroundColor(Color(.secondaryBraveLabel))
+          }
+        }
+        .toggleStyle(SwitchToggleStyle(tint: .accentColor))
+        Toggle(isOn: $youtubeShortsBlocking) {
+          VStack(alignment: .leading) {
+            Text(Strings.Settings.blockYouTubeShortsTitle)
+            Text(Strings.Settings.blockYouTubeShortsDesc)
+              .font(.footnote)
+              .foregroundColor(Color(.secondaryBraveLabel))
+          }
+        }
+        .toggleStyle(SwitchToggleStyle(tint: .accentColor))
       }
       .listRowBackground(Color(.secondaryBraveGroupedBackground))
     }
     .navigationBarTitle(Strings.Settings.mediaRootSetting)
     .navigationBarTitleDisplayMode(.inline)
-    .listBackgroundColor(Color(UIColor.braveGroupedBackground))
+    .background(Color(UIColor.braveGroupedBackground))
+    .task {
+      // Update the enabled state filter lists
+      self.youtubeDistractingElementsBlocking = FilterListStorage.shared.isEnabled(
+        for: AdblockFilterListCatalogEntry.youtubeDistractingElementsComponentID
+      )
+      self.youtubeRecommendationsBlocking = FilterListStorage.shared.isEnabled(
+        for: AdblockFilterListCatalogEntry.youtubeMobileRecommendationsComponentID
+      )
+      self.youtubeShortsBlocking = FilterListStorage.shared.isEnabled(
+        for: AdblockFilterListCatalogEntry.youtubeShortsComponentID
+      )
+    }
+    .onChange(of: youtubeRecommendationsBlocking) { _, newValue in
+      FilterListStorage.shared.ensureFilterList(
+        for: AdblockFilterListCatalogEntry.youtubeMobileRecommendationsComponentID,
+        isEnabled: newValue
+      )
+    }
+    .onChange(of: youtubeDistractingElementsBlocking) { _, newValue in
+      FilterListStorage.shared.ensureFilterList(
+        for: AdblockFilterListCatalogEntry.youtubeDistractingElementsComponentID,
+        isEnabled: newValue
+      )
+    }
+    .onChange(of: youtubeShortsBlocking) { _, newValue in
+      FilterListStorage.shared.ensureFilterList(
+        for: AdblockFilterListCatalogEntry.youtubeShortsComponentID,
+        isEnabled: newValue
+      )
+    }
+    .onChange(of: filterListStorage.filterLists) { _, newValue in
+      for filterList in newValue {
+        switch filterList.entry.componentId {
+        case AdblockFilterListCatalogEntry.youtubeDistractingElementsComponentID:
+          if filterList.isEnabled != self.youtubeDistractingElementsBlocking {
+            self.youtubeDistractingElementsBlocking = filterList.isEnabled
+          }
+        case AdblockFilterListCatalogEntry.youtubeMobileRecommendationsComponentID:
+          if filterList.isEnabled != self.youtubeRecommendationsBlocking {
+            self.youtubeRecommendationsBlocking = filterList.isEnabled
+          }
+        case AdblockFilterListCatalogEntry.youtubeShortsComponentID:
+          if filterList.isEnabled != self.youtubeShortsBlocking {
+            self.youtubeShortsBlocking = filterList.isEnabled
+          }
+        default:
+          continue
+        }
+      }
+    }
   }
 }
 
@@ -84,7 +167,7 @@ private struct QualitySettingsView: View {
       .listRowBackground(Color(.secondaryBraveGroupedBackground))
     }
     .navigationBarTitle(Strings.Settings.highestQualityPlayback)
-    .listBackgroundColor(Color(UIColor.braveGroupedBackground))
+    .background(Color(UIColor.braveGroupedBackground))
   }
 
   func qualityOption(preference: YoutubeHighQualityPreference) -> some View {
