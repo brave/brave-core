7ebb624612d2659d6694eb9fba0c365e5f606628
Aleksey Khoroshilov <akhoroshilov@brave.com>
2025-06-17 16:03:36 +0700

    Use -iqoute instead of -I to reference //brave/chromium_src includes.

--- tools/redirect_cc/redirect_cc.cc
+++ tools/redirect_cc/redirect_cc.cc
@@ -27,7 +27,8 @@
 #include "base/environment.h"
 #endif  // defined(REDIRECT_CC_AS_REWRAPPER)
 
-const base::FilePath::StringViewType kIncludeFlag = FILE_PATH_LITERAL("-I");
+const base::FilePath::StringViewType kIncludeQuotedFlag =
+    FILE_PATH_LITERAL("-iquote");
 const base::FilePath::StringViewType kBraveChromiumSrc =
     FILE_PATH_LITERAL("brave/chromium_src");
 const base::FilePath::StringViewType kGen = FILE_PATH_LITERAL("gen");
@@ -93,9 +94,9 @@ class RedirectCC {
     // Find directories to work with first.
     for (const auto* arg : args_.subspan(first_compiler_arg_idx)) {
       base::FilePath::StringViewType arg_piece(arg);
-      if (arg_piece.starts_with(kIncludeFlag) &&
+      if (arg_piece.starts_with(kIncludeQuotedFlag) &&
           arg_piece.ends_with(kBraveChromiumSrc)) {
-        arg_piece.remove_prefix(kIncludeFlag.size());
+        arg_piece.remove_prefix(kIncludeQuotedFlag.size());
         brave_chromium_src_dir = base::FilePath::StringType(arg_piece);
         arg_piece.remove_suffix(kBraveChromiumSrc.size());
         chromium_src_dir_with_slash = base::FilePath::StringType(arg_piece);
