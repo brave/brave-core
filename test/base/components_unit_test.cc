271cb903590847346805e6e3f724f38fbdb9ef0e
bridiver <34129+bridiver@users.noreply.github.com>
2025-06-17 10:10:19 -0700

    cleanup

--- test/base/components_unit_test.cc
+++ test/base/components_unit_test.cc
@@ -1,13 +1,14 @@
-/* Copyright (c) 2025 The Brave Authors. All rights reserved.
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at https://mozilla.org/MPL/2.0/. */
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
 
 #include "brave/components/test/base/components_unit_test.h"
 
 #include "chrome/browser/ui/webui/chrome_web_ui_controller_factory.h"
 #include "content/public/browser/web_ui_controller.h"
 #include "content/public/common/url_utils.h"
+#include "content/public/test/scoped_web_ui_controller_factory_registration.h"
 
 // components unit tests still run in chrome_unit_test_suite which registers
 // ChromeWebUIControllerFactory::GetInstance()
