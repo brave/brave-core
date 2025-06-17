95fe7fe9d19fce585819bd6aeeb823ab756ca83a
AlexeyBarabash <alexey@brave.com>
2025-06-17 23:42:45 +0300

    Fixed icons at CTA menu
    Resolves https://github.com/brave/brave-browser/issues/46811
    
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/fa431a3660de796f7aa18130fd8a6da39f1cc1d7
    
    	[Icon Button] Migrate app menu icon buttons
    
    	Migrated the app menu top row icons from ChromeImageButtons to Material Buttons. The highlight (ripple) is now a perfect circle instead of rectangle.
    
    	Bug: 405189559
    	Change-Id: I0376bf0ecf7f5b734dad2d7e8c69200834c54248
    	Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6477118

--- build/commands/lib/util.js
+++ build/commands/lib/util.js
@@ -1052,6 +1052,11 @@ const util = {
         'res',
       )
 
+      const uiAndroidResSource =
+          path.join(config.braveCoreDir, "ui", "android", "java", "res")
+      const uiAndroidResDest =
+          path.join(config.srcDir, "ui", "android", "java", "res")
+
       // Mapping for copying Brave's Android resource into chromium folder.
       const copyAndroidResourceMapping = {
         [androidTranslateResSource]: [androidTranslateResDest],
@@ -1073,6 +1078,7 @@ const util = {
         [androidBrowserHubInternalResSource]: [
           androidBrowserHubInternalResDest,
         ],
+        [uiAndroidResSource]: [uiAndroidResDest],
       }
 
       console.log('copy Android app icons and app resources')
@@ -1104,6 +1110,32 @@ const util = {
         },
       )
       removeUnlistedAndroidResources(braveOverwrittenFiles)
+
+      console.log('Remove unlisted overridden pngs')
+      const overriddenPngsToRemove = [
+        path.join(androidToolbarResDest,
+            "drawable-hdpi", "btn_toolbar_reload.png"),
+        path.join(androidToolbarResDest,
+            "drawable-mdpi", "btn_toolbar_reload.png"),
+        path.join(androidToolbarResDest,
+            "drawable-xhdpi", "btn_toolbar_reload.png"),
+        path.join(androidToolbarResDest,
+            "drawable-xxhdpi", "btn_toolbar_reload.png"),
+        path.join(androidToolbarResDest,
+            "drawable-xxxhdpi", "btn_toolbar_reload.png"),
+
+        path.join(androidToolbarResDest,
+            "drawable-ldrtl-hdpi", "btn_toolbar_reload.png"),
+        path.join(androidToolbarResDest,
+            "drawable-ldrtl-mdpi", "btn_toolbar_reload.png"),
+        path.join(androidToolbarResDest,
+            "drawable-ldrtl-xhdpi", "btn_toolbar_reload.png"),
+        path.join(androidToolbarResDest,
+            "drawable-ldrtl-xxhdpi", "btn_toolbar_reload.png"),
+        path.join(androidToolbarResDest,
+            "drawable-ldrtl-xxxhdpi", "btn_toolbar_reload.png"),
+      ]
+      overriddenPngsToRemove.forEach((path) => fs.removeSync(path));
     }
     Log.progressFinish('update branding')
   },
