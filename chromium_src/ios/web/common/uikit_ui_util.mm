a9c2150c9d0fda63a3402700e43133c1fd64292f
Kyle Hickinson <kylehickinson@users.noreply.github.com>
2025-06-18 10:48:47 -0400

    [iOS] Replace strict cast in Chromium's GetAnyKeyWindow (#29594)
    This adds additional flexibility to the cast, similar to `GetAllActiveScreens` methods in `screen_ios.mm`, since Brave has the possibility to have a non-window scene connected when using CarPlay

--- chromium_src/ios/web/common/uikit_ui_util.mm
+++ chromium_src/ios/web/common/uikit_ui_util.mm
@@ -1,4 +1,4 @@
-/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
@@ -8,5 +8,5 @@
 // Replaces the strict cast to a standard cast since its possible for Brave to
 // have a non-window scene thanks to the CarPlay support
 #define ObjCCastStrict ObjCCast
-#include "src/ui/display/ios/screen_ios.mm"
+#include "src/ios/web/common/uikit_ui_util.mm"
 #undef ObjCCastStrict
