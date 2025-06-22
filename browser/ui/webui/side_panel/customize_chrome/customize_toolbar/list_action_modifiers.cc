405e9d0367282a9146d1b24b6a01a1e5ac81912b
Sangwoo Ko <sangwoo108@gmail.com>
2025-06-23 08:27:11 +0900

    Use fixed flat set for list up unsupported actions

--- browser/ui/webui/side_panel/customize_chrome/customize_toolbar/list_action_modifiers.cc
+++ browser/ui/webui/side_panel/customize_chrome/customize_toolbar/list_action_modifiers.cc
@@ -7,6 +7,9 @@
 
 #include <vector>
 
+#include "base/containers/contains.h"
+#include "base/containers/fixed_flat_set.h"
+
 namespace customize_chrome {
 
 using side_panel::customize_chrome::mojom::ActionId;
@@ -16,10 +19,11 @@ std::vector<ActionPtr> FilterUnsupportedChromiumActions(
     std::vector<ActionPtr> actions) {
   // Filter out unsupported Chromium actions.
   std::erase_if(actions, [](const ActionPtr& action) -> bool {
-    return action->id == ActionId::kShowPaymentMethods ||
-           action->id == ActionId::kShowTranslate ||
-           action->id == ActionId::kShowReadAnything ||
-           action->id == ActionId::kShowAddresses;
+    static constexpr auto kUnsupportedChromiumActions =
+        base::MakeFixedFlatSet<ActionId>(
+            {ActionId::kShowPaymentMethods, ActionId::kShowTranslate,
+             ActionId::kShowReadAnything, ActionId::kShowAddresses});
+    return base::Contains(kUnsupportedChromiumActions, action->id);
   });
   return actions;
 }
