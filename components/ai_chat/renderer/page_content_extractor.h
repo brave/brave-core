29d8cb29f07af501e4c97ac2376cc93cd53d1577
Anthony Tseng <darkdh@gmail.com>
2025-06-24 14:20:52 -0700

    Fix unresponsive youtube transcript fetching (#29619)
    * Use InnerTube API to fetch caption tracks as ytplayer method now
    gives us empty content when fetching from its content url.
    It also fixes unresponsive content fetching due to callback will never be
    called when xml parsing failed.
    
    * Fix AIChatBrowserTest.YoutubeNavigations
    
    * Add ParseYoutubeTranscriptXml util function along with unit tests
    
    * Remove AIChatResourceSniffer along with old YT <transcript> xml handling
    
    * Add missing PageContentExtractorRenderViewTest
    
    * Enforce browser/ai_chat runtime data files deps
    
    * Add support for separate components unit test exe
    
    * add brave_components_unittests ci feature
    
    * Add missing PageContentFetcherTest
    
    * URLEncoded InnerTube API key
    
    * Make components/ai_chat/core/common/yt_util.h as non-public API
    
    * Add validations for YT video id and innertube API key
    
    * Disable brave_components_unittests becasuse it failed on Android test
    runner. We will re-enable it in a seperate PR.
    
    ---------
    
    Co-authored-by: bridiver <34129+bridiver@users.noreply.github.com>

--- components/ai_chat/renderer/page_content_extractor.h
+++ components/ai_chat/renderer/page_content_extractor.h
@@ -7,19 +7,16 @@
 #define BRAVE_COMPONENTS_AI_CHAT_RENDERER_PAGE_CONTENT_EXTRACTOR_H_
 
 #include <cstdint>
-#include <memory>
 #include <optional>
 #include <string>
 
 #include "base/memory/weak_ptr.h"
 #include "base/values.h"
 #include "brave/components/ai_chat/core/common/mojom/page_content_extractor.mojom.h"
-#include "brave/components/ai_chat/renderer/ai_chat_resource_sniffer_throttle_delegate.h"
 #include "content/public/renderer/render_frame.h"
 #include "content/public/renderer/render_frame_observer.h"
 #include "content/public/renderer/render_frame_observer_tracker.h"
 #include "mojo/public/cpp/bindings/receiver.h"
-#include "mojo/public/cpp/bindings/remote.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
 
 namespace base {
@@ -39,8 +36,7 @@ namespace ai_chat {
 class PageContentExtractor
     : public ai_chat::mojom::PageContentExtractor,
       public content::RenderFrameObserver,
-      public content::RenderFrameObserverTracker<PageContentExtractor>,
-      public AIChatResourceSnifferThrottleDelegate {
+      public content::RenderFrameObserverTracker<PageContentExtractor> {
  public:
   PageContentExtractor(content::RenderFrame* render_frame,
                        service_manager::BinderRegistry* registry,
@@ -50,21 +46,6 @@ class PageContentExtractor
   PageContentExtractor(const PageContentExtractor&) = delete;
   PageContentExtractor& operator=(const PageContentExtractor&) = delete;
   ~PageContentExtractor() override;
-
-  base::WeakPtr<PageContentExtractor> GetWeakPtr();
-
- private:
-  void OnJSTranscriptUrlResult(
-      mojom::PageContentExtractor::ExtractPageContentCallback callback,
-      ai_chat::mojom::PageContentType type,
-      std::optional<base::Value> value,
-      base::TimeTicks start_time);
-  void OnDistillResult(
-      mojom::PageContentExtractor::ExtractPageContentCallback callback,
-      const std::optional<std::string>& content);
-
-  // RenderFrameObserver implementation:
-  void OnDestruct() override;
 
   // PageContentExtractor implementation:
   void ExtractPageContent(
@@ -77,10 +58,25 @@ class PageContentExtractor
       mojom::PageContentExtractor::GetOpenAIChatButtonNonceCallback callback)
       override;
 
-  // AIChatResourceSnifferThrottleDelegate
-  void OnInterceptedPageContentChanged(
-      std::unique_ptr<AIChatResourceSnifferThrottleDelegate::InterceptedContent>
-          content_update) override;
+  base::WeakPtr<PageContentExtractor> GetWeakPtr();
+
+ private:
+  void OnJSTranscriptUrlResult(
+      mojom::PageContentExtractor::ExtractPageContentCallback callback,
+      ai_chat::mojom::PageContentType type,
+      std::optional<base::Value> value,
+      base::TimeTicks start_time);
+  void OnJSYoutubeInnerTubeConfigResult(
+      ai_chat::mojom::PageContentExtractor::ExtractPageContentCallback callback,
+      ai_chat::mojom::PageContentType type,
+      std::optional<base::Value> value,
+      base::TimeTicks start_time);
+  void OnDistillResult(
+      mojom::PageContentExtractor::ExtractPageContentCallback callback,
+      const std::optional<std::string>& content);
+
+  // RenderFrameObserver implementation:
+  void OnDestruct() override;
 
   void BindReceiver(
       mojo::PendingReceiver<mojom::PageContentExtractor> receiver);
@@ -89,9 +85,6 @@ class PageContentExtractor
 
   int32_t global_world_id_;
   int32_t isolated_world_id_;
-
-  std::unique_ptr<AIChatResourceSnifferThrottleDelegate::InterceptedContent>
-      intercepted_content_;
 
   base::WeakPtrFactory<PageContentExtractor> weak_ptr_factory_{this};
 };
