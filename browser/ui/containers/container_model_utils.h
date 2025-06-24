a4f6a49058d8c8d1f3f7c5e8ad11388d06c5959a
Sangwoo Ko <sangwoo108@gmail.com>
2025-06-24 18:01:56 +0900

    Use correct container object to populate icons

--- browser/ui/containers/container_model_utils.h
+++ browser/ui/containers/container_model_utils.h
@@ -17,9 +17,15 @@ namespace containers {
 
 class ContainerModel;
 
+// Returns a vector of `ContainerModel` objects created from preferences. This
+// function retrieves the containers defined in the preferences and converts
+// them into `ContainerModel` instances, which also contains UI specific data
+// such as icons.
 std::vector<ContainerModel> GetContainerModelsFromPrefs(
     const PrefService& prefs);
 
+// Returns an image model for the given container. This function is useful for
+// testability.
 ui::ImageModel GetImageModelForContainer(
     const containers::mojom::ContainerPtr& container);
 
