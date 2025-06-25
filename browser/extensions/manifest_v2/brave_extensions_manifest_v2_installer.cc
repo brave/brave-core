afb9ac8da6f79ab1d928f49eed8b6eff3c50886e
Pavel Beloborodov <73575789+boocmp@users.noreply.github.com>
2025-06-25 07:15:05 +0700

    Bypass Brave hosted extensions verification on install. (#29559)
    * Do not check Brave mv2 extensions in CWS.
    * Updated description test.
    * Do not deprecate Brave-hosted mv2 extensions.

--- browser/extensions/manifest_v2/brave_extensions_manifest_v2_installer.cc
+++ browser/extensions/manifest_v2/brave_extensions_manifest_v2_installer.cc
@@ -35,10 +35,6 @@ namespace extensions_mv2 {
 
 namespace {
 
-bool IsKnownMV2Extension(const extensions::ExtensionId& id) {
-  return kPreconfiguredManifestV2Extensions.contains(id);
-}
-
 GURL GetUpdaterExtensionDownloadUrl(
     const extensions::ExtensionId& extenion_id) {
   const std::string params[] = {base::JoinString({"id", extenion_id}, "="),
@@ -98,6 +94,10 @@ GURL GetCrxDownloadUrl(const base::Value::Dict& update_manifest,
 }
 
 }  // namespace
+
+bool IsKnownMV2Extension(const extensions::ExtensionId& id) {
+  return kPreconfiguredManifestV2Extensions.contains(id);
+}
 
 ExtensionManifestV2Installer::ExtensionManifestV2Installer(
     const std::string& extension_id,
