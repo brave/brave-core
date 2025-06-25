049ccc06022a62b60db196d21673e62d6f1fe629
Claudio DeSouza <cdesouza@brave.com>
2025-06-25 16:04:00 +0100

    [cr140] Update `GetCrxId` to spanified definition
    This only affects the arguments for this override.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/8ff5b0d783a0415593ecf400b2c829463289d25a
    
    commit 8ff5b0d783a0415593ecf400b2c829463289d25a
    Author: Elly <ellyjones@chromium.org>
    Date:   Tue Jun 24 17:52:27 2025 -0700
    
        crx_file: migrate to crypto/keypair and crypto/sign
    
        The new APIs are a bit more ergonomic, have fewer failure cases, and are
        not deprecated.
    
        This change also fixes some unsafe buffer usage (of argv) in
        crx3_build_action.
    
        Bug: 372283556
        Change-Id: I8220ff7499385001f7ba83e8eb638ee7dd987541
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6663109
        Commit-Queue: Elly FJ <ellyjones@chromium.org>
        Auto-Submit: Elly FJ <ellyjones@chromium.org>
        Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1478295}

--- chromium_src/extensions/browser/extension_creator.cc
+++ chromium_src/extensions/browser/extension_creator.cc
@@ -19,21 +19,20 @@ constexpr char kAltPublisherKeySwitch[] = "brave-extension-publisher-key-alt";
 }  // namespace
 
 #define BRAVE_CREATE_CRX(output_path, zip_path, signing_key)              \
-  std::vector<crypto::RSAPrivateKey*> keys{signing_key};                  \
-  std::vector<std::unique_ptr<crypto::RSAPrivateKey>> keep_keys_alive;    \
+  std::vector<crypto::keypair::PrivateKey> keys{signing_key};             \
   const auto* cmd = base::CommandLine::ForCurrentProcess();               \
   const char* switches[] = {kPublisherKeySwitch, kAltPublisherKeySwitch}; \
   for (const char* switch_name : switches) {                              \
     if (cmd->HasSwitch(switch_name)) {                                    \
-      std::unique_ptr<crypto::RSAPrivateKey> key =                        \
+      std::optional<crypto::keypair::PrivateKey> key =                    \
           ReadInputKey(cmd->GetSwitchValuePath(switch_name));             \
       if (!key)                                                           \
         return false; /* error_message_ was set by ReadInputKey() */      \
-      keys.push_back(key.get());                                          \
-      keep_keys_alive.push_back(std::move(key));                          \
+      keys.push_back(std::move(key).value());                             \
     }                                                                     \
   }                                                                       \
-  result = crx_file::CreateWithMultipleKeys(output_path, zip_path, keys);
+  result = crx_file::CreateWithMultipleKeys(output_path, zip_path,        \
+                                            base::span(keys));
 
 #include <extensions/browser/extension_creator.cc>
 
