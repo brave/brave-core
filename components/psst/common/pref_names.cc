b6f6450019a9dfbb71561baee74a5fbd6f6d1887
Vadym Struts <vstruts@brave.com>
2025-06-26 16:20:54 +0200

    fixed copyright year
    Signed-off-by: Vadym Struts <vstruts@brave.com>

--- components/psst/common/pref_names.cc
+++ components/psst/common/pref_names.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2023 The Brave Authors. All rights reserved.
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
@@ -9,8 +9,8 @@
 #include "base/strings/string_util.h"
 #include "brave/components/psst/common/features.h"
 #include "components/prefs/pref_registry_simple.h"
-#include "components/prefs/pref_service.h"
-#include "components/prefs/scoped_user_pref_update.h"
+// #include "components/prefs/pref_service.h"
+// #include "components/prefs/scoped_user_pref_update.h"
 
 namespace psst {
 
