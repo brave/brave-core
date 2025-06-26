8d419a06e179f143d54a8bfd4d0ac3644339311e
Darnell Andries <dandries@brave.com>
2025-06-26 10:16:32 -0700

    Move existing Managed P3A classes to new directory

--- components/p3a/managed/remote_config_manager.cc
+++ components/p3a/managed/remote_config_manager.cc
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "brave/components/p3a/remote_config_manager.h"
+#include "brave/components/p3a/managed/remote_config_manager.h"
 
 #include <string>
 #include <utility>
