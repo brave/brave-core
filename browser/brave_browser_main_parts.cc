a12ddd67d732e4f2137f96583a9c5fa3c50e0952
Michael Herrmann <michael@herrmann.io>
2025-06-26 13:48:00 +0200

    Move mac-only code from brave_browser_main_parts to brave_browser_main_parts_mac

--- browser/brave_browser_main_parts.cc
+++ browser/brave_browser_main_parts.cc
@@ -68,14 +68,6 @@
 #include "brave/components/ipfs/ipfs_component_cleaner.h"
 #endif  // BUILDFLAG(DEPRECATE_IPFS)
 
-#if BUILDFLAG(IS_MAC)
-#include "base/task/task_traits.h"
-#include "brave/browser/mac_features.h"
-#include "brave/updater/updater_p3a.h"
-#include "chrome/common/chrome_constants.h"
-#include "content/public/browser/browser_thread.h"
-#endif
-
 ChromeBrowserMainParts::ChromeBrowserMainParts(bool is_integration_test,
                                                StartupData* startup_data)
     : ChromeBrowserMainParts_ChromiumImpl(is_integration_test, startup_data) {}
@@ -173,16 +165,6 @@ void ChromeBrowserMainParts::PostBrowserStart() {
   ipfs::CleanupIpfsComponent(
       base::PathService::CheckedGet(chrome::DIR_USER_DATA));
 #endif  // BUILDFLAG(DEPRECATE_IPFS)
-
-#if BUILDFLAG(IS_MAC)
-  content::GetUIThreadTaskRunner({base::TaskPriority::BEST_EFFORT})
-      ->PostTask(FROM_HERE, base::BindOnce([]() {
-                   brave_updater::ReportLaunch(
-                       base::Time::Now(), chrome::kChromeVersion,
-                       brave::ShouldUseOmaha4(),
-                       g_browser_process->local_state());
-                 }));
-#endif
 }
 
 void ChromeBrowserMainParts::PreShutdown() {
