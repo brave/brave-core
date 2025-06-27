0b71b37623e1b0971a88fc8e2dfb7a30b8d9e0c7
Simon Hong <shong@brave.com>
2025-06-27 09:15:29 +0900

    Show tab hover state only on hovered split tab with SideBySide (#29775)
    fix brave/brave-browser#47126
    
    Chromium set hovered state when any tab in split tabs is hovered.
    But we only want hover effect only on hovered tab in split tab.
    
    TEST=SideBySideEnabledBrowserTest.SelectTabTest
    
    Manual test:
    
    1. Open Brave with --enable-features=SideBySide
    2. Open split tabs
    3. Hover on tab in split tab
    4. Check only that hovered tab has hover effect

--- browser/ui/views/split_view/split_view_browsertest.cc
+++ browser/ui/views/split_view/split_view_browsertest.cc
@@ -32,6 +32,7 @@
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/frame/browser_non_client_frame_view.h"
 #include "chrome/browser/ui/views/frame/multi_contents_view_mini_toolbar.h"
+#include "chrome/browser/ui/views/tabs/tab.h"
 #include "chrome/browser/ui/views/tabs/tab_strip.h"
 #include "chrome/browser/ui/views/tabs/tab_style_views.h"
 #include "chrome/test/base/chrome_test_utils.h"
@@ -227,6 +228,19 @@ IN_PROC_BROWSER_TEST_F(SideBySideEnabledBrowserTest, SelectTabTest) {
   EXPECT_EQ(0, tab_strip()->GetActiveIndex());
   EXPECT_FALSE(split_view_separator()->GetVisible());
   EXPECT_FALSE(split_view_separator()->menu_button_widget_->IsVisible());
+
+  // Check only hovered split tab has hover animation.
+  auto* hovered_split_tab = tab_strip()->tab_at(2);
+  auto* not_hovered_split_tab = tab_strip()->tab_at(3);
+  EXPECT_TRUE(hovered_split_tab->split().has_value());
+  EXPECT_EQ(hovered_split_tab->split(), not_hovered_split_tab->split());
+  hovered_split_tab->controller()->ShowHover(hovered_split_tab,
+                                             TabStyle::ShowHoverStyle::kSubtle);
+  ASSERT_TRUE(base::test::RunUntil([&]() {
+    return hovered_split_tab->tab_style_views()->GetHoverAnimationValue() != 0;
+  }));
+  EXPECT_EQ(not_hovered_split_tab->tab_style_views()->GetHoverAnimationValue(),
+            0);
 
   // Check selected split tab becomes active tab.
   tab_strip()->SelectTab(tab_strip()->tab_at(2), GetDummyEvent());
