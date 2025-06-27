0287e48ddac7233bc151c994fba9a4504b89cef0
Arthur Edelstein <aedelstein@brave.com>
2025-06-27 10:53:39 -0700

    use jest-fail-on-console to fail on warnings

--- components/test/testSetup.ts
+++ components/test/testSetup.ts
@@ -5,6 +5,9 @@
 
 import '@testing-library/jest-dom'
 import './testPolyfills'
+import failOnConsole from 'jest-fail-on-console'
+
+failOnConsole()
 
 // Mock the locale module so we don't need to provide strings in tests
 jest.mock('$web-common/locale', () => ({
