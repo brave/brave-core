72252f8b7aed3ca5bb3a3623e5103f0f37d8201b
Claudio DeSouza <cdesouza@brave.com>
2025-06-28 02:37:03 +0100

    [cr140] Passing metric arg to `chrome::NewSplitTab`
    This metric argument is being collected by chromium however it seems to
    be irrelevant for brave.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/358549c15a65c5900132b4867897d1360cc5acac
    
    commit 358549c15a65c5900132b4867897d1360cc5acac
    Author: Alison Gale <agale@chromium.org>
    Date:   Wed Jun 25 10:27:16 2025 -0700
    
        Reland "[SxS] Flush out metrics for split tab creation"
    
        This reverts commit 98904ac05447db9b5ab010454b20672de4bacb94.
    
        Correct rollback for the original breakage was:
        https://chromium-review.googlesource.com/c/chromium/src/+/6662662
    
        Reason for revert: Incorrect culprit was reverted
        Bug: 406792705
    
        Original change's description:
        > Revert "[SxS] Flush out metrics for split tab creation"
        >
        > This reverts commit e4eb581c5cd610ad820587942bc2563211e82874.
        >
        > Reason for revert: Step "browser_tests (retry shards) on Ubuntu-22.04" failing on builder "chromium/ci/Linux ChromiumOS MSan Tests"
        > https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/53079/test-results
        >
        > Bug: 406792705
        > Original change's description:
        > > [SxS] Flush out metrics for split tab creation
        > >
        > > With an exhaustive UMA located in the same spot of the TabStripModel,
        > > the previous user action is redundant.
        > >
        > > Bug: 406792705
        > > Change-Id: I148255d63f5f5c3739031a380519fc90fb87c896
        > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651221
        > > Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
        > > Reviewed-by: David Roger <droger@chromium.org>
        > > Commit-Queue: Alison Gale <agale@chromium.org>
        > > Reviewed-by: Eshwar Stalin <estalin@chromium.org>
        > > Cr-Commit-Position: refs/heads/main@{#1477378}
        >
        > Bug: 406792705
        > No-Presubmit: true
        > No-Tree-Checks: true
        > No-Try: true
        > Change-Id: Idec6302c68cc9ec171e21ea1140e86b88a770d26
        > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6665201
        > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
        > Owners-Override: S Ganesh <ganesh@chromium.org>
        > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
        > Auto-Submit: S Ganesh <ganesh@chromium.org>
        > Cr-Commit-Position: refs/heads/main@{#1477607}
    
        Bug: 406792705
        Change-Id: If68eed79f25cb80f14b8aa31c9cbbc3c1cb68a0d
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6667549
        Commit-Queue: Alison Gale <agale@chromium.org>
        Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
        Reviewed-by: David Roger <droger@chromium.org>
        Reviewed-by: S Ganesh <ganesh@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1478639}

--- browser/ui/views/split_view/split_view_browsertest.cc
+++ browser/ui/views/split_view/split_view_browsertest.cc
@@ -139,14 +139,16 @@ IN_PROC_BROWSER_TEST_F(SideBySideEnabledBrowserTest,
   chrome::PinTab(browser());
 
   chrome::AddTabAt(browser(), GURL(), -1, /*foreground*/ true);
-  chrome::NewSplitTab(browser());
+  chrome::NewSplitTab(browser(),
+                      split_tabs::SplitTabCreatedSource::kToolbarButton);
   chrome::PinTab(browser());
 
   chrome::AddTabAt(browser(), GURL(), -1, /*foreground*/ true);
   chrome::PinTab(browser());
 
   chrome::AddTabAt(browser(), GURL(), -1, /*foreground*/ true);
-  chrome::NewSplitTab(browser());
+  chrome::NewSplitTab(browser(),
+                      split_tabs::SplitTabCreatedSource::kToolbarButton);
   chrome::PinTab(browser());
 
   tab_strip()->StopAnimating(/* layout= */ true);
@@ -199,7 +201,8 @@ IN_PROC_BROWSER_TEST_F(SideBySideEnabledBrowserTest,
   EXPECT_NE(gfx::Size(),
             browser_view->contents_separator_for_testing()->GetPreferredSize());
 
-  chrome::NewSplitTab(browser());
+  chrome::NewSplitTab(browser(),
+                      split_tabs::SplitTabCreatedSource::kToolbarButton);
 
   // separator should be empty when split view is opened.
   EXPECT_EQ(gfx::Size(),
@@ -277,7 +280,8 @@ IN_PROC_BROWSER_TEST_F(SideBySideEnabledBrowserTest, SelectTabTest) {
   EXPECT_FALSE(split_view_separator()->menu_button_widget_->IsVisible());
 
   // Created new tab(at 3) for new split view with existing tab(at 2).
-  chrome::NewSplitTab(browser());
+  chrome::NewSplitTab(browser(),
+                      split_tabs::SplitTabCreatedSource::kToolbarButton);
   EXPECT_TRUE(tab_strip()->tab_at(2)->split().has_value());
   EXPECT_FALSE(tab_strip()->tab_at(2)->IsActive());
   EXPECT_TRUE(tab_strip()->tab_at(3)->split().has_value());
@@ -385,8 +389,10 @@ class SplitViewWithTabDialogBrowserTest
   }
 
   void NewSplitTab() {
-    IsSideBySideEnabled() ? chrome::NewSplitTab(browser())
-                          : brave::NewSplitViewForTab(browser());
+    IsSideBySideEnabled()
+        ? chrome::NewSplitTab(browser(),
+                              split_tabs::SplitTabCreatedSource::kToolbarButton)
+        : brave::NewSplitViewForTab(browser());
   }
 
   bool IsSideBySideEnabled() const { return GetParam(); }
