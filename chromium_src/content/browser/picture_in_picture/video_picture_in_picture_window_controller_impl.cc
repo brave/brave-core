d96bffac17f86476a697068322ca9783c70215ca
Claudio DeSouza <cdesouza@brave.com>
2025-06-28 02:39:27 +0100

    [cr140] `MediaSessionImpl::pepper_players_` deleted
    This field has been deleted as part of the breader pppapi content layer
    remove. This change deletes the override that was using this field to
    retrieve the position information for the players, and with that all the
    handling for it.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/dee7e88bd605ece07381b8aa975f1d6a3c7db1a2
    
    commit dee7e88bd605ece07381b8aa975f1d6a3c7db1a2
    Author: Dave Tapuska <dtapuska@chromium.org>
    Date:   Tue Jun 24 19:51:14 2025 -0700
    
        [pepper] More removal of pppai from content layer
    
        - Remove more code
    
        Bug: 40511450
        Change-Id: Id3728a709afe92184b874690e71e31811017689f
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6667722
        Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
        Owners-Override: Dave Tapuska <dtapuska@chromium.org>
        Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
        Reviewed-by: Daniel Cheng <dcheng@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1478323}

--- chromium_src/content/browser/picture_in_picture/video_picture_in_picture_window_controller_impl.cc
+++ chromium_src/content/browser/picture_in_picture/video_picture_in_picture_window_controller_impl.cc
@@ -11,15 +11,9 @@
 // Note that |media_position_| could be incorrect(suspecting timing issue
 // between service process), so we're getting more correct position from media
 // session.
-#define SetPlaybackState(PLAYBACK_STATE)                                  \
-  SetPlaybackState(PLAYBACK_STATE);                                       \
-  if (auto position = MediaSessionImpl::Get(web_contents())               \
-                          ->GetMediaPositionFromNormalPlayerIfPossible(); \
-      position) {                                                         \
-    window_->SetMediaPosition(position);                                  \
-  } else {                                                                \
-    window_->SetMediaPosition(effective_media_position);                  \
-  }
+#define SetPlaybackState(PLAYBACK_STATE) \
+  SetPlaybackState(PLAYBACK_STATE);      \
+  window_->SetMediaPosition(effective_media_position);
 
 // Update seeker enabled state whenever actions are updated.
 // Note that we allow seeking when media session is controllable referring to
