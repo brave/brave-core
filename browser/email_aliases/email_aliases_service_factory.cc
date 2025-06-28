547425e66c79c40a24029cc626d46f489be2786a
Arthur Edelstein <aedelstein@brave.com>
2025-06-27 17:15:07 -0700

    move browser/ui/email_aliases to browser/email_aliases

--- browser/email_aliases/email_aliases_service_factory.cc
+++ browser/email_aliases/email_aliases_service_factory.cc
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "brave/browser/ui/email_aliases/email_aliases_service_factory.h"
+#include "brave/browser/email_aliases/email_aliases_service_factory.h"
 
 #include <utility>
 
