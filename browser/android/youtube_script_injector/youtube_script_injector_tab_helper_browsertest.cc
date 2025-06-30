0b68ca668175298c85a4bd084f82cf428d471e3b
AlexeyBarabash <alexey@brave.com>
2025-06-30 21:20:35 +0300

    [cr140][Android] Fixed missing includes for WebContens

--- browser/android/youtube_script_injector/youtube_script_injector_tab_helper_browsertest.cc
+++ browser/android/youtube_script_injector/youtube_script_injector_tab_helper_browsertest.cc
@@ -10,6 +10,7 @@
 #include "chrome/test/base/android/android_browser_test.h"
 #include "chrome/test/base/chrome_test_utils.h"
 #include "chrome/test/base/testing_browser_process.h"
+#include "content/public/browser/web_contents.h"
 #include "content/public/common/content_client.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
