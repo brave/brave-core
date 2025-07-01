f1c2d6a963b246bab9ebbca0557edf52224f3670
Sangwoo Ko <sangwoo108@gmail.com>
2025-07-02 06:15:32 +0900

    [Customize Chrome Panel] Modify Navigation section of toolbar customization UI (#29831)
    This patch apply modification in favor of Brave's toolbar customization UI.
    
    1. Move "Tab Search" button to Navigation section from Tools section.
    2. Modify icon for "Private Window" button
    3. Add additional Brave-specific actions to the toolbar customization UI:
       kShowSidePanel, kShowWallet, kShowAIChat, kShowVPN,
    
    These buttons are handled in customize_toolbar_handler.cc.
    
    Note that this PR doesn't include observation from native UI side change. 
    e.g. When user hide an action via context menu of the action, the web ui
         is still out of sync with the native UI.
    
    <img width="311" alt="image" src="https://github.com/user-attachments/assets/4d6b3ade-16fb-45df-9abc-dc5440d9e470" />
    
    
    <!-- Add brave-browser issue below that this PR will resolve -->
    Resolves https://github.com/brave/brave-browser/issues/47259

--- browser/ui/webui/side_panel/customize_chrome/customize_toolbar/list_action_modifiers.h
+++ browser/ui/webui/side_panel/customize_chrome/customize_toolbar/list_action_modifiers.h
@@ -10,10 +10,31 @@
 
 #include "chrome/browser/ui/webui/side_panel/customize_chrome/customize_toolbar/customize_toolbar.mojom.h"
 
+namespace content {
+class WebContents;
+}  // namespace content
+
 namespace customize_chrome {
 
+// Removes unsupported Chromium actions from the list of actions.
 std::vector<side_panel::customize_chrome::mojom::ActionPtr>
 FilterUnsupportedChromiumActions(
+    std::vector<side_panel::customize_chrome::mojom::ActionPtr> actions);
+
+// Applies Brave-specific modifications to the list of actions.
+// 1. Moves existing Chromium actions to the desired positions.
+//   e.g. Tab search action is moved to 'Navigation' category after 'New
+//        Incognito Window'.
+// 2. Updates icons for existing actions.
+//   e.g. 'New Incognito Window' action icon is updated to use the
+//        `kLeoProductPrivateWindowIcon`. This icon is different from what
+//        we use for App menu.
+// 3. Adds Brave-specific actions.
+//   e.g. In 'Navigation' category:
+//        `kShowSidePanel`, `kShowWallet`, `kShowAIChat`, `kShowVPN`.
+std::vector<side_panel::customize_chrome::mojom::ActionPtr>
+ApplyBraveSpecificModifications(
+    content::WebContents& web_contents,
     std::vector<side_panel::customize_chrome::mojom::ActionPtr> actions);
 
 }  // namespace customize_chrome
