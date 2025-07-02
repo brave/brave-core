139b01ca87388b1c10ec175cbb779f18e3fe099d
Brian R. Bondy <netzen@gmail.com>
2025-07-02 16:14:59 -0400

    Brave Origin State and disabling of base features

--- components/brave_wallet/common/common_utils.cc
+++ components/brave_wallet/common/common_utils.cc
@@ -14,6 +14,7 @@
 #include "base/notreached.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
+#include "brave/components/brave_origin/brave_origin_state.h"
 #include "brave/components/brave_wallet/common/brave_wallet_types.h"
 #include "brave/components/brave_wallet/common/buildflags.h"
 #include "brave/components/brave_wallet/common/features.h"
@@ -32,7 +33,8 @@ bool IsDisabledByPolicy(PrefService* prefs) {
   return false;
 #else
   DCHECK(prefs);
-  return prefs->IsManagedPreference(prefs::kDisabledByPolicy) &&
+  return (BraveOriginState::GetInstance()->IsBraveOriginUser() ||
+          prefs->IsManagedPreference(prefs::kDisabledByPolicy)) &&
          prefs->GetBoolean(prefs::kDisabledByPolicy);
 #endif
 }
