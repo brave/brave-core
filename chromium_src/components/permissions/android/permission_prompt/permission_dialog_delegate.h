14f26193d0b65eca4a5bba07b488197d564d4ca6
AlexeyBarabash <alexey@brave.com>
2025-07-04 14:06:35 +0300

    [cr140][Android] JNI: Remove unused "jcaller" parameters in //components (part 1)
    In addition moved methods into PermissionDialogJavaDelegate
    
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/30bb892875831fdbdce524d7d3b431976c92a109
    
    	JNI: Remove unused "jcaller" parameters in //components (part 1)
    
    	This was semi-automated using a script to remove all caller params,
    	and then Gemini CLI to fix all resulting build errors
    
    	Bug: 428193049
    	Change-Id: Ida24035fa6bcc287dcd9100facea4ecc512d75ed
    	Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6700248

--- chromium_src/components/permissions/android/permission_prompt/permission_dialog_delegate.h
+++ chromium_src/components/permissions/android/permission_prompt/permission_dialog_delegate.h
@@ -14,6 +14,13 @@ namespace permissions {
 class PermissionPromptAndroid_ChromiumImpl;
 }  // namespace permissions
 
+#define OnRequestingOriginFaviconLoaded                                     \
+  ApplyLifetimeToPermissionRequests(                                        \
+      JNIEnv* env, PermissionPromptAndroid* permission_prompt);             \
+  void ApplyDontAskAgainOption(JNIEnv* env,                                 \
+                               PermissionPromptAndroid* permission_prompt); \
+  void OnRequestingOriginFaviconLoaded
+
 // Additional PermissionDialogDelegate::Create stub method to be in accordance
 // with the overridden method at permission_prompt_android.h
 //   void CreatePermissionDialogDelegate() {
@@ -31,5 +38,7 @@ class PermissionPromptAndroid_ChromiumImpl;
 #include "src/components/permissions/android/permission_prompt/permission_dialog_delegate.h"  // IWYU pragma: export
 
 #undef Create
+
+#undef OnRequestingOriginFaviconLoaded
 
 #endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_PERMISSIONS_ANDROID_PERMISSION_PROMPT_PERMISSION_DIALOG_DELEGATE_H_
