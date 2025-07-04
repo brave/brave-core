8f281cd5051199b0395414902ebd02cff3a41ca8
AlexeyBarabash <alexey@brave.com>
2025-07-04 14:06:35 +0300

    [cr140][Android] JNI: Remove unused "jcaller" parameters in //components (part 1)
    In addition moved methods into PermissionDialogJavaDelegate
    
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/30bb892875831fdbdce524d7d3b431976c92a109
    
    	JNI: Remove unused "jcaller" parameters in //components (part 1)
    
    	This was semi-automated using a script to remove all caller params,
    	and then Gemini CLI to fix all resulting build errors
    
    	Bug: 428193049
    	Change-Id: Ida24035fa6bcc287dcd9100facea4ecc512d75ed
    	Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6700248

--- chromium_src/components/search_engines/android/template_url_service_android.h
+++ chromium_src/components/search_engines/android/template_url_service_android.h
@@ -6,9 +6,8 @@
 #ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_SEARCH_ENGINES_ANDROID_TEMPLATE_URL_SERVICE_ANDROID_H_
 #define BRAVE_CHROMIUM_SRC_COMPONENTS_SEARCH_ENGINES_ANDROID_TEMPLATE_URL_SERVICE_ANDROID_H_
 
-#define DoesDefaultSearchEngineHaveLogo                              \
-  DoesDefaultSearchEngineHaveLogo_ChromiumImpl(                      \
-      JNIEnv* env, const base::android::JavaParamRef<jobject>& obj); \
+#define DoesDefaultSearchEngineHaveLogo                      \
+  DoesDefaultSearchEngineHaveLogo_ChromiumImpl(JNIEnv* env); \
   jboolean DoesDefaultSearchEngineHaveLogo
 
 #include "src/components/search_engines/android/template_url_service_android.h"  // IWYU pragma: export
