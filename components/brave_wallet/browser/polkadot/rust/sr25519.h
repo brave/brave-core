bb7c1f22880fcef36ed1f141544931f91907fd52
Christian Mazakas <christian.mazakas@gmail.com>
2025-07-07 15:36:14 -0700

    add rust stubs

---
+++ components/brave_wallet/browser/polkadot/rust/sr25519.h
@@ -0,0 +1,6 @@
+#include "brave/components/brave_wallet/browser/polkadot/rust/lib.rs.h"
+#include "third_party/rust/cxx/v1/cxx.h"
+
+namespace brave_wallet {
+int test_fn(brave_wallet::polkadot::CxxPolkadotTest);
+}
