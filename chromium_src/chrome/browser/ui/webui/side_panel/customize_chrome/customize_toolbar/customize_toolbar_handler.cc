ab42e74e45eaeeaedf6892ee3a7f6da745c57cc3
Sangwoo Ko <sangwoo108@gmail.com>
2025-07-07 23:07:01 +0900

    [Customize Chrome Panel] Change "Your Chrome(Brave)" label to "Brave Menu" (#29911)
    By replacing resource ID used by customize_toolbar_handler.cc,
    overrides the label saying "Your Chrome(Brave)" to "Brave Menu".
    
    This label is a section title for "Your Chrome" category in toolbar customize_chrome page. 
    This page includes toggle buttons, such as password manager, bookmarks, reading list, etc.
    
    <img width="316" alt="스크린샷 2025-07-04 오후 1 38 23" src="https://github.com/user-attachments/assets/87ca7695-b8f1-4af0-80de-db66ca1fa076" />
    
    <!-- Add brave-browser issue below that this PR will resolve -->
    Resolves https://github.com/brave/brave-browser/issues/47388

--- chromium_src/chrome/browser/ui/webui/side_panel/customize_chrome/customize_toolbar/customize_toolbar_handler.cc
+++ chromium_src/chrome/browser/ui/webui/side_panel/customize_chrome/customize_toolbar/customize_toolbar_handler.cc
@@ -11,6 +11,8 @@
 #include "brave/browser/ui/webui/side_panel/customize_chrome/customize_toolbar/brave_action.h"
 #include "brave/browser/ui/webui/side_panel/customize_chrome/customize_toolbar/list_action_modifiers.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/grit/generated_resources.h"
+#include "components/grit/brave_components_strings.h"
 
 #define ListActions ListActionsChromium
 #define PinAction PinActionChromium
@@ -20,7 +22,15 @@
   Init(__VA_ARGS__); \
   ObserveBraveActions()
 
+// Replace the resource ID for the "Your Chrome" category with "Brave Menu"
+// resource ID.
+#undef IDS_NTP_CUSTOMIZE_TOOLBAR_CATEGORY_YOUR_CHROME
+#define IDS_NTP_CUSTOMIZE_TOOLBAR_CATEGORY_YOUR_CHROME \
+  IDS_CUSTOMIZE_TOOLBAR_CATEGORY_BRAVE_MENU
+
 #include "src/chrome/browser/ui/webui/side_panel/customize_chrome/customize_toolbar/customize_toolbar_handler.cc"
+
+#undef IDS_NTP_CUSTOMIZE_TOOLBAR_CATEGORY_YOUR_CHROME
 
 #undef Init
 #undef PinAction
