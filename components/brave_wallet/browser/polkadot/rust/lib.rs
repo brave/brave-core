72a6099c75354ade2a062c8829625eb852269045
Christian Mazakas <christian.mazakas@gmail.com>
2025-07-08 14:32:38 -0700

    add GenerateFromSeed method

--- components/brave_wallet/browser/polkadot/rust/lib.rs
+++ components/brave_wallet/browser/polkadot/rust/lib.rs
@@ -5,13 +5,55 @@ extern crate cxx;
 #[allow(unsafe_op_in_unsafe_fn)]
 #[cxx::bridge(namespace = brave_wallet::polkadot)]
 mod ffi {
-    struct CxxPolkadotTest {
-        value: u32,
-    }
-
     extern "Rust" {
-        fn rawr(_: CxxPolkadotTest) -> i32;
+        type CxxSr25519KeyPair;
+        type CxxSr25519KeyPairResult;
+
+        fn generate_sr25519_keypair_from_seed(bytes: &[u8],) -> Box<CxxSr25519KeyPairResult>;
+        fn use_keypair(p: &Box<CxxSr25519KeyPairResult>);
+        fn is_ok(self: &CxxSr25519KeyPairResult) -> bool;
+        fn unwrap(self: &mut CxxSr25519KeyPairResult) -> Box<CxxSr25519KeyPair>;
     }
 }
 
-fn rawr(_: ffi::CxxPolkadotTest) -> i32 { 1234 }
+#[derive(Clone, Debug)]
+pub enum Error {
+    Schnorrkel(schnorrkel::SignatureError)
+}
+
+#[derive(Clone)]
+struct CxxSr25519KeyPair(Sr25519KeyPair);
+
+#[derive(Clone)]
+struct CxxSr25519KeyPairResult(Result<Option<CxxSr25519KeyPair>, Error>);
+
+#[derive(Clone)]
+struct Sr25519KeyPair {
+    kp: schnorrkel::Keypair,
+}
+
+fn generate_sr25519_keypair_from_seed(bytes: &[u8],) -> Box<CxxSr25519KeyPairResult> {
+    let kp = schnorrkel::MiniSecretKey::from_bytes(bytes)
+        .map(|kp| Some(CxxSr25519KeyPair(Sr25519KeyPair {kp: kp.expand_to_keypair(schnorrkel::ExpansionMode::Ed25519)})))
+        .map_err(|e| Error::Schnorrkel(e));
+
+    Box::new(CxxSr25519KeyPairResult(kp))
+}
+
+fn use_keypair(p: &Box<CxxSr25519KeyPairResult>) {
+    assert!(<std::result::Result<Option<CxxSr25519KeyPair>, Error> as Clone>::clone(&(&p.0)).unwrap().unwrap().0.kp.secret.to_bytes().len() > 0);
+}
+
+impl CxxSr25519KeyPairResult {
+    fn is_ok(self: &CxxSr25519KeyPairResult) -> bool {
+        match &self.0 {
+            Ok(_) => true,
+            _ => false,
+        }
+    }
+
+    fn unwrap(self: &mut CxxSr25519KeyPairResult) -> Box<CxxSr25519KeyPair>
+    {
+        Box::new(self.0.as_mut().unwrap().take().unwrap())
+    }
+}
