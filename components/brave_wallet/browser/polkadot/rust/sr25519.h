a135c4297ac9b3bba5a28003c10d00e6e9f847ec
Christian Mazakas <christian.mazakas@gmail.com>
2025-07-08 14:32:38 -0700

    add GenerateFromSeed method

--- components/brave_wallet/browser/polkadot/rust/sr25519.h
+++ components/brave_wallet/browser/polkadot/rust/sr25519.h
@@ -1,6 +1,27 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_COMPONENTS_BRAVE_WALLER_BROWSER_POLKADOT_RUST_SR25519_H_
+#define BRAVE_COMPONENTS_BRAVE_WALLER_BROWSER_POLKADOT_RUST_SR25519_H_
+
+#include <memory>
+
+#include "base/containers/span.h"
 #include "brave/components/brave_wallet/browser/polkadot/rust/lib.rs.h"
 #include "third_party/rust/cxx/v1/cxx.h"
 
 namespace brave_wallet {
-int test_fn(brave_wallet::polkadot::CxxPolkadotTest);
-}
+
+namespace polkadot {}  // namespace polkadot
+
+class HDKeySr25519 {
+ public:
+  static std::unique_ptr<HDKeySr25519> GenerateFromSeed(
+      base::span<const uint8_t> seed);
+};
+
+}  // namespace brave_wallet
+
+#endif  // BRAVE_COMPONENTS_BRAVE_WALLER_BROWSER_POLKADOT_RUST_SR25519_H_
--- components/brave_wallet/browser/polkadot/sr25519_unittest.cc
+++ components/brave_wallet/browser/polkadot/sr25519_unittest.cc
@@ -4,9 +4,19 @@
 
 namespace brave_wallet {
 
-TEST(PolkdadotSR25519, HelloWorld) {
-  polkadot::CxxPolkadotTest x;
-  EXPECT_EQ(test_fn(x), 1234);
+TEST(PolkdadotSR25519, GenerateFromSeed) {
+  auto kpresult = HDKeySr25519::GenerateFromSeed({});
+  EXPECT_FALSE(kpresult);
+
+  kpresult = HDKeySr25519::GenerateFromSeed({1, 2, 3, 4});
+  EXPECT_FALSE(kpresult);
+
+  kpresult = HDKeySr25519::GenerateFromSeed({
+      157, 97,  177, 157, 239, 253, 90,  96,  186, 132, 074,
+      244, 146, 236, 044, 196, 68,  073, 197, 105, 123, 050,
+      105, 025, 112, 59,  172, 003, 28,  174, 127, 96,
+  });
+  EXPECT_TRUE(kpresult);
 }
 
 }  // namespace brave_wallet
