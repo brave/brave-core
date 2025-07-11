0c6c2a35367d703c8ca9892ecf4d87d80cec8d9c
Claudio DeSouza <cdesouza@brave.com>
2025-07-11 17:45:54 +0100

    [cr140] `Browser::ShouldShowBookmarkBar` now under `BookmarkBarController`
    With this function moved into `BookmarkBarController`, it is necessary
    to move the override for it to that class too.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/c01aec7ec831e7375c572bea91d9c1b556ac1458
    
    commit c01aec7ec831e7375c572bea91d9c1b556ac1458
    Author: Ho Cheung <hocheung@chromium.org>
    Date:   Tue Jul 8 16:14:20 2025 -0700
    
        Refactor bookmark bar logic into separate BookmarkBarController
    
        This CL extracts bookmark bar responsibilities from Browser class into a
        dedicated BookmarkBarController, which is then composed as a
        BrowserWindowFeature.
    
        The following bookmark bar related members and methods are moved from
        Browser to BookmarkBarController:
        - force_show_bookmark_bar_flags_
        - bookmark_bar_state_
        - BookmarkBarStateChangeReason enum
        - ForceShowBookmarkBarFlag enum
        - ShouldShowBookmarkBar()
        - UpdateBookmarkBarState()
        - SetForceShowBookmarkBarFlag()
        - ClearForceShowBookmarkBarFlag()
        - OnSplitTabChanged()
        - DidFinishNavigation()
    
        This encapsulation improves separation of concerns and allows features
        that only need bookmark bar state to interact with BookmarkBarController
        directly instead of going through Browser.
    
        Bug: 418896419
        Change-Id: I0c99690a1b04eabca69e784b34bc40618473ccfb
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6703933
        Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
        Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
        Auto-Submit: Ho Cheung <hocheung@chromium.org>
        Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1484059}

--- chromium_src/chrome/browser/ui/browser.cc
+++ chromium_src/chrome/browser/ui/browser.cc
@@ -8,22 +8,9 @@
 #include "brave/browser/ui/brave_browser_command_controller.h"
 #include "brave/browser/ui/brave_tab_strip_model_delegate.h"
 #include "brave/browser/ui/tabs/brave_tab_strip_model.h"
-#include "brave/components/constants/pref_names.h"
 #include "chrome/browser/ui/browser_command_controller.h"
 
 #define BRAVE_BROWSER_CREATE return new BraveBrowser(params);
-
-// We have one more option for bookmarks bar visibility.
-// It's "only show bookmarks bar on NTP". And it's patched to prevent
-// checking split view. Chromium shows bookmarks bar if active tab is
-// in split tabs and split tabs includes NTP. But, we don't want to show
-// bookmarks bar if that active tab is not NTP.
-// It's inevitable to copy some upstream code when overriding this method.
-// Also added this before checking bookmarks/tab groups existence.
-// We show bookmarks bar on NTP if there is no bookmarks/tab groups.
-#define BRAVE_BROWSER_SHOULD_SHOW_BOOKMARK_BAR      \
-  return IsShowingNTP(active_tab->GetContents()) && \
-         prefs->GetBoolean(kAlwaysShowBookmarkBarOnNTP);
 
 #define BrowserCommandController BraveBrowserCommandController
 #define BrowserTabStripModelDelegate BraveTabStripModelDelegate
@@ -36,13 +23,8 @@
 #undef BrowserActions
 #undef BrowserTabStripModelDelegate
 #undef BrowserCommandController
-#undef BRAVE_BROWSER_SHOULD_SHOW_BOOKMARK_BAR
 #undef BRAVE_BROWSER_DEPRECATED_CREATE_OWNED_FOR_TESTING
 #undef BRAVE_BROWSER_CREATE
-
-bool IsShowingNTP_ChromiumImpl(content::WebContents* web_contents) {
-  return IsShowingNTP(web_contents);
-}
 
 // static
 std::unique_ptr<Browser> Browser::DeprecatedCreateOwnedForTesting(
