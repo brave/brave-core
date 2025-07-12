383847a26ee0ae1740b6513cfa12479893267f17
Claudio DeSouza <cdesouza@brave.com>
2025-07-12 05:40:55 +0100

    [cr140] `TabInterface` abandoned inheritance
    Upstream has simplified this class to not rely on inheritance. This
    affects the way brave has been overriding this class in several ways.
    This change necessarily marks this class as having a virtual destructor
    again, so we may be able to add to it our own changes.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/8ce7d6b31882aa35172e8ce6b3b4631dff5c4d76
    
    commit 8ce7d6b31882aa35172e8ce6b3b4631dff5c4d76
    Author: Dana Fried <dfried@chromium.org>
    Date:   Tue Jul 8 13:05:46 2025 -0700
    
        [Unowned Data] Switch TabInterface to using UnownedUserDataHost
    
        This switches TabInterface to using an UnownedUserDataHost. TabModel and
        TabFeatures no longer use inheritance to override behavior for testing.
        Features which required substitution via overrides now use injection.
    
        Follow-up: Move unowned_user_data down into ui/base.
    
        Bug: 427458853
        Change-Id: I32468e198b3e77de656e2c00cd0ef4e9a83180ba
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6680232
        Commit-Queue: Dana Fried <dfried@chromium.org>
        Auto-Submit: Dana Fried <dfried@chromium.org>
        Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1483950}

--- chromium_src/chrome/browser/ui/tabs/public/tab_features.h
+++ chromium_src/chrome/browser/ui/tabs/public/tab_features.h
@@ -8,16 +8,10 @@
 
 #include "base/callback_list.h"
 
-#define CreateTabFeatures     \
-  CreateTabFeatures_Unused(); \
-  static std::unique_ptr<TabFeatures> CreateTabFeatures
-#define ReplaceTabFeaturesForTesting                               \
-  ReplaceTabFeaturesForTesting_Unused(TabFeaturesFactory factory); \
-  static void ReplaceTabFeaturesForTesting
+#define Init virtual Init
 
 #include <chrome/browser/ui/tabs/public/tab_features.h>  // IWYU pragma: export
 
-#undef CreateTabFeatures
-#undef ReplaceTabFeaturesForTesting
+#undef Init
 
 #endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_TABS_PUBLIC_TAB_FEATURES_H_
--- chromium_src/chrome/browser/ui/tabs/tab_model.cc
+++ chromium_src/chrome/browser/ui/tabs/tab_model.cc
@@ -1,8 +1,11 @@
-// Copyright (c) 2025 The Brave Authors. All rights reserved.
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this file,
-// You can obtain one at https://mozilla.org/MPL/2.0/.
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+#include "chrome/browser/ui/tabs/tab_model.h"
+
+#include "brave/browser/ui/tabs/public/brave_tab_features.h"
 #include "components/tabs/public/tab_collection.h"
 
 // Add switch case handling for TREE_NODE type in TabModel::UpdateProperties().
@@ -10,6 +13,6 @@
   UNPINNED:      \
   case TabCollection::Type::TREE_NODE
 
-#include <chrome/browser/ui/tabs/tab_model.cc>
+#include <chrome/browser/ui/tabs/tab_model.cc>  // IWYU pragma: export
 
 #undef UNPINNED
