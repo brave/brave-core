3fe32cecf6f0191cf2ace8aa5f92412f52a7606f
Claudio DeSouza <cdesouza@brave.com>
2025-07-13 00:53:57 +0100

    [cr140][WIP] `kDownloadsPageReferrerUrl` deleted
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/ae2cc7a9a65c4b3e0b113399492cd5d120a074f6
    
    commit ae2cc7a9a65c4b3e0b113399492cd5d120a074f6
    Author: Lily Chen <chlily@chromium.org>
    Date:   Thu Jul 10 14:41:15 2025 -0700
    
        Replace referrer URL with initiator origin in chrome://downloads UI
    
        Before this CL, the chrome://downloads UI on desktop platforms shows the
        referrer URL on the item card for most downloads, displayed as a string
        such as "From https://example.site", where the site name may be a
        clickable link to the download's referrer URL.
    
        This CL replaces the referrer URL with the initiator origin of the
        download request in this security-relevant UI display string.
    
        This is implemented behind a base::Feature "killswitch" (which is
        enabled by default), kDownloadsDisplayInitiatorOrigin, which replaces
        the existing killswitch for the referrer URL behavior. No change is made
        to the non-default behavior of showing the download URL when the
        killswitch is disabled.
    
        This CL preserves the existing behavior which omits the display of this
        text for downloads not initiated by a user gesture.
    
        The initiator origin display is also omitted when the download request
        was browser-initiated (not initiated by an origin). This may include
        downloads triggered directly from the omnibox, or from the right-click
        context menu.
    
        Because the origin is not a full URL, after this CL, there is no longer
        an embedded link in this display string.
    
        The overall goal is to accurately represent "where the download came
        from" in a way that is understandable to the user and corresponds to an
        origin that the user interacted with in the course of getting the
        download. In general, we prefer to omit the display string rather than
        show something that may be incorrect or misleading.
    
        Screenshots:
        https://drive.google.com/drive/folders/1uN2IQx379tPMWcASEz0utfOWwpBkSrpF
    
        Bug: 424764882
        Change-Id: I6ffbb33d9c2a5c22d493f6ac1ee21512207877b5
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6719170
        Reviewed-by: Daniel Rubery <drubery@chromium.org>
        Auto-Submit: Lily Chen <chlily@chromium.org>
        Reviewed-by: Joe Mason <joenotcharles@google.com>
        Commit-Queue: Lily Chen <chlily@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1485220}

--- app/feature_defaults_unittest.cc
+++ app/feature_defaults_unittest.cc
@@ -230,7 +230,6 @@ TEST(FeatureDefaultsTest, DisabledFeatures) {
       &privacy_sandbox::kFingerprintingProtectionUx,
       &privacy_sandbox::kOverridePrivacySandboxSettingsLocalTesting,
       &privacy_sandbox::kPrivacySandboxSettings4,
-      &safe_browsing::kDownloadsPageReferrerUrl,
       &safe_browsing::kGooglePlayProtectInApkTelemetry,
       &segmentation_platform::features::kSegmentationPlatformDeviceTier,
       &segmentation_platform::features::kSegmentationPlatformFeature,
