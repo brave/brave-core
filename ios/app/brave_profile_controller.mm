b48cff1adcdc54e22604a4e88b51f4d361bdb168
Emerick Rogul <erogul@brave.com>
2025-07-12 23:06:01 -0400

    [cr140][ios] `WebStateList::ClosingReason` changed to an enum
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/6325d5e05c3c94a05c82e938ee2eb89d475bf905
    
    commit 6325d5e05c3c94a05c82e938ee2eb89d475bf905
    Author: Sylvain Defresne <sdefresne@chromium.org>
    Date:   Fri Jul 11 03:02:38 2025 -0700
    
        [ios] Use an enum class for WebStateList ClosingReason
    
        All usage of CloseWebStateAt(...) and related method use a single
        reason (CLOSE_NO_REASON, CLOSE_USER_ACTION or CLOSE_TABS_CLEANUP)
        without combining them.
    
        Change the parameter from an int interpreted as a bit set to an
        enum class.
    
        Bug: 365701685
        Change-Id: I9ac275c18fd0bbb9c316056da30999e5d744521b
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6722237
        Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
        Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1485452}

--- ios/app/brave_profile_controller.mm
+++ ios/app/brave_profile_controller.mm
@@ -160,13 +160,14 @@
   [_otr_browser->GetCommandDispatcher() prepareForShutdown];
   _otr_browserList->RemoveBrowser(_otr_browser.get());
   CloseAllWebStates(*_otr_browser->GetWebStateList(),
-                    WebStateList::CLOSE_NO_FLAGS);
+                    WebStateList::ClosingReason::kDefault);
   _otr_browser.reset();
 
   _browserList = BrowserListFactory::GetForProfile(_browser->GetProfile());
   [_browser->GetCommandDispatcher() prepareForShutdown];
   _browserList->RemoveBrowser(_browser.get());
-  CloseAllWebStates(*_browser->GetWebStateList(), WebStateList::CLOSE_NO_FLAGS);
+  CloseAllWebStates(*_browser->GetWebStateList(),
+                    WebStateList::ClosingReason::kDefault);
   _browser.reset();
 
   _profile = nil;
@@ -393,7 +394,7 @@
   // in their -dealloc method, ensure the -autorelease introduced by ARC are
   // processed before the WebStateList destructor is called.
   @autoreleasepool {
-    CloseAllWebStates(*webStateList, WebStateList::CLOSE_NO_FLAGS);
+    CloseAllWebStates(*webStateList, WebStateList::ClosingReason::kDefault);
   }
 }
 
