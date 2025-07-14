54c3177481192fd75668f9878846afe7690f49c6
Gaetano Checinski <gchecinski@brave.com>
2025-07-14 16:39:50 +0100

    wip

--- build/commands/lib/test.js
+++ build/commands/lib/test.js
@@ -10,6 +10,61 @@ const config = require('../lib/config')
 const Log = require('../lib/logging')
 const util = require('../lib/util')
 const assert = require('assert')
+const { pipeline } = require("stream");
+const { promisify } = require("util");
+const pipe = promisify(pipeline);
+
+
+
+const cacheClient = () => {
+  const {AWS_REGION: region, BRAVE_TEST_CACHE_S3_BUCKET: bucket} = process.env;
+  if (!region || !bucket) 
+    return null;
+
+  const { 
+    S3Client, 
+    HeadObjectCommand, 
+    GetObjectCommand
+  } = require( "@aws-sdk/client-s3");
+  const s3 = new S3Client({region: AWS_REGION });
+
+  return {
+    check(key) {
+      return s3.send(new HeadObjectCommand({ 
+        Bucket: bucket, 
+        Key: key 
+        })).then(()=>true).catch(()=>false)
+    },
+    
+    async download(key, dest) {
+
+      const response = await s3.send(new GetObjectCommand({
+        Bucket: bucket,
+        Key: key
+      }));
+
+      await pipe(response.Body, createWriteStream(destinationPath));
+
+      console.log(`Downloaded s3://${bucket}/${key} to ${destinationPath}`);
+      return JSON.stringify(fs.readFile(dest, 'utf-8'));
+    },
+
+    async upload(key, file) {
+      const fileStream = fs.createReadStream(file);
+
+      const uploadParams = {
+        Bucket: bucket,
+        Key: key,
+        Body: fileStream,
+        ContentType: "text/plain", // adjust based on your file type
+      };
+
+      await s3.send(new PutObjectCommand(uploadParams));
+
+      console.log(`Uploaded ${file} to s3://${bucket}/${key}`);
+    }
+  }
+}
 
 
 const getTestBinary = (suite) => {
@@ -134,6 +189,7 @@ const deleteFile = (filePath) => {
 }
 
 const runTests = async (passthroughArgs, suite, buildConfig, options) => {
+
   config.buildConfig = buildConfig
   config.update(options)
 
