7d17ea511d3b07a254ca83279d40a27a3bc9301b
Brandon T <JustBrandonT@gmail.com>
2025-07-14 18:31:03 -0400

    Remove unnecessary code and checks. It's impossible to have WebUIs without a profile which has valid WebStates.
    Signed-off-by: Brandon T <JustBrandonT@gmail.com>

--- chromium_src/ios/web/web_state/web_state_impl.h
+++ chromium_src/ios/web/web_state/web_state_impl.h
@@ -1,12 +1,26 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_CHROMIUM_SRC_IOS_WEB_WEB_STATE_WEB_STATE_IMPL_H_
+#define BRAVE_CHROMIUM_SRC_IOS_WEB_WEB_STATE_WEB_STATE_IMPL_H_
+
 #include "base/observer_list.h"
 
 namespace web {
 class WebUIIOS;
 }  // namespace web
 
+// Override observers variable so we can add our own list of WebUIs
+
 #define observers_ \
   observers_;      \
   std::vector<std::unique_ptr<web::WebUIIOS>> brave_web_uis_
+
+// Override CancelDialogs function so we can add our own cleanup code
+// as well as functions for handling WebUI creation and messages.
+// We also add GetMainWebUI to return the WebUI belonging to the main-frame.
 
 #define CancelDialogs                                          \
   CancelDialogs();                                             \
@@ -19,8 +33,10 @@ class WebUIIOS;
   web::WebUIIOS* GetMainWebUI() const;                         \
   void ClearBraveWebUI
 
-#include "src/ios/web/web_state/web_state_impl.h"
+#include "src/ios/web/web_state/web_state_impl.h"  // IWYU pragma: export
 
 #undef CancelDialogs
 
 #undef observers_
+
+#endif  // BRAVE_CHROMIUM_SRC_IOS_WEB_WEB_STATE_WEB_STATE_IMPL_H_
