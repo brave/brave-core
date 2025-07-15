d65729909bae57ad433d16a8fddd21a152ef5f10
Gaetano Checinski <gchecinski@brave.com>
2025-07-15 09:59:31 +0100

    move client to seperate file

--- build/commands/lib/test.js
+++ build/commands/lib/test.js
@@ -10,66 +10,7 @@ const config = require('../lib/config')
 const Log = require('../lib/logging')
 const util = require('../lib/util')
 const assert = require('assert')
-const { pipeline } = require("stream");
-const { promisify } = require("util");
-const pipe = promisify(pipeline);
-
-
-
-const cacheClient = () => {
-  const {AWS_REGION: region, BRAVE_TEST_CACHE_S3_BUCKET: bucket} = process.env;
-  if (!region || !bucket) 
-    return null;
-
-  const { 
-    S3Client, 
-    HeadObjectCommand, 
-    GetObjectCommand
-  } = require( "@aws-sdk/client-s3");
-  const s3 = new S3Client({region: AWS_REGION });
-
-  return {
-    check(key) {
-      return s3.send(new HeadObjectCommand({ 
-        Bucket: bucket, 
-        Key: key 
-        })).then(()=>true).catch(()=>false)
-    },
-    
-    async download(key, dest) {
-
-      const response = await s3.send(new GetObjectCommand({
-        Bucket: bucket,
-        Key: key
-      }));
-
-      try {
-        await pipe(response.Body, createWriteStream(dest));
-
-        console.log(`Downloaded s3://${bucket}/${key} to ${destinationPath}`);
-        return true;
-      } catch (e) {
-        console.error(e);
-        return false;
-      }
-    },
-
-    async upload(key, file) {
-      const fileStream = fs.createReadStream(file);
-
-      const uploadParams = {
-        Bucket: bucket,
-        Key: key,
-        Body: fileStream,
-        ContentType: "text/plain", // adjust based on your file type
-      };
-
-      await s3.send(new PutObjectCommand(uploadParams));
-
-      console.log(`Uploaded ${file} to s3://${bucket}/${key}`);
-    }
-  }
-}
+const cacheClient = require('./cache-client');
 
 
 const getTestBinary = (suite) => {
