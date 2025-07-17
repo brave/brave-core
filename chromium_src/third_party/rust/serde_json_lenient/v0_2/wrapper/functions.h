ee6a9dabed22bae3d790f6d88ad3971c221bd99d
Claudio DeSouza <cdesouza@brave.com>
2025-07-17 14:37:57 +0100

    [json64] Fix broken header guard
    This header guard started failing recently. Maybe something changed
    since M139, but it is not very clear what happened. This change corrects
    the presubmit warning with the expected value.

--- chromium_src/third_party/rust/serde_json_lenient/v0_2/wrapper/functions.h
+++ chromium_src/third_party/rust/serde_json_lenient/v0_2/wrapper/functions.h
@@ -3,8 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#ifndef BRAVE_CHROMIUM_SRC_THIRD_PARTY_RUST_SERDE_JSON_LENIENT_V0_2_WRAPPER_SERDE_JSON_LENIENT_H_
-#define BRAVE_CHROMIUM_SRC_THIRD_PARTY_RUST_SERDE_JSON_LENIENT_V0_2_WRAPPER_SERDE_JSON_LENIENT_H_
+#ifndef BRAVE_CHROMIUM_SRC_THIRD_PARTY_RUST_SERDE_JSON_LENIENT_V0_2_WRAPPER_FUNCTIONS_H_
+#define BRAVE_CHROMIUM_SRC_THIRD_PARTY_RUST_SERDE_JSON_LENIENT_V0_2_WRAPPER_FUNCTIONS_H_
 
 #include <stdint.h>
 
@@ -34,4 +34,4 @@ void dict_set_i64(Dict& ctx, rust::Str key, int64_t val);
 void dict_set_u64(Dict& ctx, rust::Str key, uint64_t val);
 }  // namespace serde_json_lenient
 
-#endif  // BRAVE_CHROMIUM_SRC_THIRD_PARTY_RUST_SERDE_JSON_LENIENT_V0_2_WRAPPER_SERDE_JSON_LENIENT_H_
+#endif  // BRAVE_CHROMIUM_SRC_THIRD_PARTY_RUST_SERDE_JSON_LENIENT_V0_2_WRAPPER_FUNCTIONS_H_
