0931b67f018f2ff28313b656191ea2b9f0b804f8
Artem Samoilenko <artem@brave.com>
2025-07-17 20:42:20 -0400

    [Android] Remove `Copy link` menu item from long press menu

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -1952,6 +1952,18 @@ public class BytecodeTest {
                         BrowserControlsStateProvider.class,
                         ObservableSupplier.class,
                         boolean.class));
+        Assert.assertTrue(
+                constructorsMatch(
+                        "org/chromium/chrome/browser/toolbar/ToolbarLongPressMenuHandler",
+                        "org/chromium/chrome/browser/toolbar/BraveToolbarLongPressMenuHandler",
+                        Context.class,
+                        ObservableSupplier.class,
+                        boolean.class,
+                        BooleanSupplier.class,
+                        ActivityLifecycleDispatcher.class,
+                        WindowAndroid.class,
+                        java.util.function.Supplier.class,
+                        java.util.function.Supplier.class));
     }
 
     @Test
