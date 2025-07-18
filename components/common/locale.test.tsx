3daf46b54e30352ca03b4f044949b22e38419b84
Jay Harris <jay.harris@outlook.co.nz>
2025-07-18 15:55:48 +1200

    [locale]: Add different end tags to formatLocale (#30067)

--- components/common/locale.test.tsx
+++ components/common/locale.test.tsx
@@ -32,7 +32,7 @@ describe('formatString', () => {
   })
 
   it('should allow replacing a range', () => {
-    assert.equal(formatString('$1 in $2nz$2 say $3', {
+    assert.equal(formatString('$1 in $2nz/$2 say $3', {
       $1: 'People',
       $2: 'NZ',
       $3: 'Kiora'
@@ -40,7 +40,7 @@ describe('formatString', () => {
   })
 
   it('should allow replacing with a range and a function', () => {
-    assert.equal(formatString('$1 in $2nz$2 say $3', {
+    assert.equal(formatString('$1 in $2nz/$2 say $3', {
       $1: 'People',
       $2: (content) => content.toUpperCase(),
       $3: 'Kiora'
@@ -48,7 +48,7 @@ describe('formatString', () => {
   })
 
   it('should fail if a replacement does not exist', () => {
-    assert.throws(() => formatString('$1 in $2nz$2 say $3', {
+    assert.throws(() => formatString('$1 in $2nz/$2 say $3', {
       $1: 'People',
       $2: (content) => content.toUpperCase(),
       $3: 'Kiora',
@@ -57,7 +57,7 @@ describe('formatString', () => {
   })
 
   it('should not fail if a replacement does not exist if noErrorOnMissingReplacement is true', () => {
-    expect(formatString('$1 in $2nz$2 say $3', {
+    expect(formatString('$1 in $2nz/$2 say $3', {
       $1: 'People',
       $2: (content) => content.toUpperCase(),
       $3: 'Kiora',
@@ -78,32 +78,32 @@ describe('formatString', () => {
   })
 
   it('should be able to return React nodes and take content as an argument', () => {
-    assert.deepEqual(formatString('Hello $1world$1', {
+    assert.deepEqual(formatString('Hello $1world/$1', {
       $1: (content) => <b>{content}</b>
     }), <>Hello <b>world</b></>)
   })
 
   it('should be possible to mix text and React nodes', () => {
-    assert.deepEqual(formatString('$1Shields are UP$1 for $2', {
+    assert.deepEqual(formatString('$1Shields are UP/$1 for $2', {
       $1: (content) => <span>{content}</span>,
       $2: 'brave.com'
     }), <><span>Shields are UP</span> for {"brave.com"}</>)
   })
 
   it('should be possible to only replace some keys', () => {
-    assert.deepEqual(formatString('$1Shields are UP$1 for $2', {
+    assert.deepEqual(formatString('$1Shields are UP/$1 for $2', {
       $1: (content) => <span>{content}</span>,
     }), <><span>Shields are UP</span> for $2</>)
   })
 
   it('should be possible to only replace some keys out of order', () => {
-    assert.deepEqual(formatString('$1Shields are UP$1 for $2', {
+    assert.deepEqual(formatString('$1Shields are UP/$1 for $2', {
       $2: "brave.com",
-    }), "$1Shields are UP$1 for brave.com")
+    }), "$1Shields are UP/$1 for brave.com")
   })
 
   it('one of everything', () => {
-    assert.deepEqual(formatString('$1Shields are UP$1 for $2 (count: $3) $4nz$4', {
+    assert.deepEqual(formatString('$1Shields are UP/$1 for $2 (count: $3) $4nz/$4', {
       $1: (content) => <span>{content}</span>,
       $2: "brave.com",
       $3: <span>over 9000</span>,
@@ -114,30 +114,43 @@ describe('formatString', () => {
   })
 
   it('should be possible to have a placeholder in a tag', () => {
-    assert.deepEqual(formatString('Hello $1and goodbye $2$1', {
+    assert.deepEqual(formatString('Hello $1and goodbye $2/$1', {
       $1: (content) => <span>{content}</span>,
       $2: <b>world</b>
     }), <>Hello <span><>and goodbye <b>world</b></></span></>)
   })
 
   it('should not break for Czech shields', () => {
-    assert.deepEqual(formatString('$1Štíty pro $2 jsou zapnuty$1', {
+    assert.deepEqual(formatString('$1Štíty pro $2 jsou zapnuty/$1', {
       $1: content => <b>{content}</b>,
       $2: 'brave.com'
     }), <><b>Štíty pro brave.com jsou zapnuty</b></>)
 
-    assert.deepEqual(formatString('$1Štíty pro $2 jsou vypnuty$1', {
+    assert.deepEqual(formatString('$1Štíty pro $2 jsou vypnuty/$1', {
       $1: content => <b>{content}</b>,
       $2: 'brave.com'
     }), <><b>Štíty pro brave.com jsou vypnuty</b></>)
   })
 
   it('should be possible to nest tags', () => {
-    assert.deepEqual(formatString('Hello $1and $3goodbye $2$3$1', {
+    assert.deepEqual(formatString('Hello $1and $3goodbye $2/$3/$1', {
       $1: (content) => <span>{content}</span>,
       $3: content => <i>{content}</i>,
       $2: <b>world</b>
     }), <>Hello <span><>and <i><>goodbye <b>world</b></></i></></span></>)
+  })
+
+  it('should be possible to replace multiple instances of the same key', () => {
+    assert.deepEqual(formatString('Hello from $1 to $1', {
+      $1: 'me'
+    }), 'Hello from me to me')
+  })
+
+  it('should be possible to replace multiple instances of the same key with tags and nesting', () => {
+    assert.deepEqual(formatString('Hello from $2$1/$2 to $1. $2bob/$2 says hi too', {
+      $1: 'me',
+      $2: (content) => <span>{content}</span>
+    }), <>Hello from <span>me</span>{" to "}{"me"}. <span>bob</span> says hi too</>)
   })
 })
 
