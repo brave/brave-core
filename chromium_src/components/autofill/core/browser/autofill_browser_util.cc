1d11e3a26b9d184ebeceffbfd07ee3adeef23f15
Francois Marier <francois@brave.com>
2025-07-18 21:45:17 -0700

    Treat .onion as a secure context for insecure form warnings and autofill
    Fixes brave/brave-browser#44802

---
+++ chromium_src/components/autofill/core/browser/autofill_browser_util.cc
@@ -0,0 +1,26 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#define IsFormMixedContent IsFormMixedContent_ChromiumImpl
+#include "src/components/autofill/core/browser/autofill_browser_util.cc"
+#undef IsFormMixedContent
+
+#include "net/base/url_util.h"
+
+namespace autofill {
+
+bool IsFormMixedContent(const AutofillClient& client, const FormData& form) {
+  if (IsFormMixedContent_ChromiumImpl(client, form)) {
+    return true;
+  }
+
+  // We only need to handle top-level .onion pages since nested cases
+  // are already handled correctly in Chromium.
+  return net::IsOnion(client.GetLastCommittedPrimaryMainFrameOrigin()) &&
+         (form.action().is_valid() &&
+          security_interstitials::IsInsecureFormAction(form.action()));
+}
+
+}  // namespace autofill
