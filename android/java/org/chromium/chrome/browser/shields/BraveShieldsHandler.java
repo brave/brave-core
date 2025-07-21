6fb950827cd5ccb7dfcfdb3251eb8282f96ba4a7
Artem Samoilenko <artem@brave.com>
2025-07-21 13:20:30 -0400

    Presubmit fixes

--- android/java/org/chromium/chrome/browser/shields/BraveShieldsHandler.java
+++ android/java/org/chromium/chrome/browser/shields/BraveShieldsHandler.java
@@ -21,7 +21,6 @@ import android.text.SpannableString;
 import android.text.method.LinkMovementMethod;
 import android.text.method.ScrollingMovementMethod;
 import android.text.style.StyleSpan;
-import android.view.ContextThemeWrapper;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Surface;
@@ -259,7 +258,6 @@ public class BraveShieldsHandler implements BraveRewardsHelper.LargeIconReadyCal
             anchorView = mHardwareButtonMenuAnchor;
         }
 
-        ContextThemeWrapper wrapper = new ContextThemeWrapper(mContext, R.style.OverflowMenuThemeOverlay);
         Point pt = new Point();
         ((Activity)mContext).getWindowManager().getDefaultDisplay().getSize(pt);
         // Get the height and width of the display.
