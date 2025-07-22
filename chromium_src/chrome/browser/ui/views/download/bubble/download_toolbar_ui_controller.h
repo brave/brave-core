1691a315d9f31a60823bb2fcc1c7b4a38c4e7713
Claudio DeSouza <cdesouza@brave.com>
2025-07-22 14:31:35 +0100

    [cr140] `GetIconColor` not useable anymore
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/613ff65167babf477e28bada5aea5ce640808f9b
    
    commit 613ff65167babf477e28bada5aea5ce640808f9b
    Author: Lily Chen <chlily@chromium.org>
    Date:   Mon Jul 21 10:48:01 2025 -0700
    
        Download toolbar button: Use tooltip text to indicate active status
    
        When a download finishes, the download toolbar button changes color and
        adds an underline beneath the symbol to indicate that it has a recently
        finished download for the user to inspect or act on. These are only
        visual indications and this state was not properly represented through
        accessible labels.
    
        This CL adds a tooltip string which is used when there are 0 in-progress
        downloads but the icon is still active, which matches the conditions
        under which the icon is colored differently and has an underline in the
        absence of an in-progress download.
    
        Screenshots:
        https://drive.google.com/drive/folders/1zwhK2kOcTiXSaQFxWc2TXPhmsIY6B7OJ
    
        Fixed: 394374568
        Change-Id: I156e1372912029193895a3bbd88aaa78f4d9cf9e
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6771347
        Reviewed-by: Daniel Rubery <drubery@chromium.org>
        Commit-Queue: Lily Chen <chlily@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1489643}

--- chromium_src/chrome/browser/ui/views/download/bubble/download_toolbar_ui_controller.h
+++ chromium_src/chrome/browser/ui/views/download/bubble/download_toolbar_ui_controller.h
@@ -6,18 +6,15 @@
 #ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_DOWNLOAD_BUBBLE_DOWNLOAD_TOOLBAR_UI_CONTROLLER_H_
 #define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_DOWNLOAD_BUBBLE_DOWNLOAD_TOOLBAR_UI_CONTROLLER_H_
 
+class DownloadToolbarUIController;
+using DownloadToolbarUIController_BraveImpl = DownloadToolbarUIController;
+
 #define DownloadToolbarUIController DownloadToolbarUIController_ChromiumImpl
 
-// Apply active color only when download is completed and user doesn't
-// interact with this button.
-#define UpdateIconDormant                                                      \
-  UpdateIconDormant();                                                         \
-                                                                               \
- protected:                                                                    \
-  virtual SkColor GetIconColor(bool is_dormant,                                \
-                               DownloadDisplay::IconActive active,             \
-                               const ui::ColorProvider* color_provider) const; \
-  void UnUsed
+#define CreateBubbleDialogDelegate              \
+  Unused();                                     \
+  friend DownloadToolbarUIController_BraveImpl; \
+  void CreateBubbleDialogDelegate
 
 // Override to show warning icon in the toolbar when there's an insecure
 // download in progress.
@@ -25,18 +22,14 @@
 
 #include <chrome/browser/ui/views/download/bubble/download_toolbar_ui_controller.h>  // IWYU pragma: export
 #undef UpdateIcon
-#undef UpdateIconDormant
 #undef DownloadToolbarUIController
+#undef CreateBubbleDialogDelegate
 
 class DownloadToolbarUIController
     : public DownloadToolbarUIController_ChromiumImpl {
  public:
   using DownloadToolbarUIController_ChromiumImpl::
       DownloadToolbarUIController_ChromiumImpl;
-
-  SkColor GetIconColor(bool is_dormant,
-                       DownloadDisplay::IconActive active,
-                       const ui::ColorProvider* color_provider) const override;
 
   void UpdateIcon() override;
 
