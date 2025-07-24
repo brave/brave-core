f4280d47d903f3d1051b17780d0faea48f6f1772
bridiver <34129+bridiver@users.noreply.github.com>
2025-07-24 12:50:45 -0700

    fix

--- components/constants/brave_services_key_helper.cc
+++ components/constants/brave_services_key_helper.cc
@@ -15,9 +15,9 @@ inline constexpr char kBraveProxyPattern[] = "*.brave.com";
 inline constexpr char kBraveSoftwareProxyPattern[] = "*.bravesoftware.com";
 
 bool ShouldAddBraveServicesKeyHeader(const GURL& url) {
-  return url.SchemeIs(url::kHttpScheme) &&
-         (base::MatchPattern(url.host(), kBraveProxyPattern) ||
-          base::MatchPattern(url.host(), kBraveSoftwareProxyPattern));
+  return url.SchemeIs(url::kHttpsScheme) &&
+      (base::MatchPattern(url.host(), kBraveProxyPattern) ||
+       base::MatchPattern(url.host(), kBraveSoftwareProxyPattern));
 }
 
 }  // namespace brave
