4af22d8b800e379e84ae0c0034235ee654812043
Claudio DeSouza <cdesouza@brave.com>
2025-07-24 23:20:55 +0100

    [cr140] `IsMorePermissive` moved
    `IsMorePermissive` is now under `content_settings_info.cc`. This has
    required some tweak on how the content type is passed in.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/78e8d46dc026b41b943cf54590c28970d111e1c5
    
    commit 78e8d46dc026b41b943cf54590c28970d111e1c5
    Author: Christian Dullweber <dullweber@chromium.org>
    Date:   Wed Jul 23 03:14:03 2025 -0700
    
        [geo] Register ContentSettings as PermissionSettings
    
        Register ContentSettings in PermissionSettingsRegistry to reuse the
        more generic PermissionSettings logic.
    
        Bug: 425642101
        Change-Id: I852abc0d0c6a15972d4643df4e308d021201dd58
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6725122
        Auto-Submit: Christian Dullweber <dullweber@chromium.org>
        Reviewed-by: Kamila Hasanbega <hkamila@google.com>
        Reviewed-by: Ravjit Uppal <ravjit@chromium.org>
        Reviewed-by: Martin Šrámek <msramek@chromium.org>
        Commit-Queue: Ravjit Uppal <ravjit@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1490705}

--- chromium_src/components/content_settings/core/browser/host_content_settings_map.cc
+++ chromium_src/components/content_settings/core/browser/host_content_settings_map.cc
@@ -5,58 +5,15 @@
 
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 
-#include "base/containers/contains.h"
-#include "base/containers/fixed_flat_set.h"
 #include "brave/components/content_settings/core/browser/remote_list_provider.h"
 #include "build/build_config.h"
-#include "components/content_settings/core/browser/content_settings_utils.h"
-#include "components/content_settings/core/common/content_settings_types.h"
-#include "components/content_settings/core/common/features.h"
 
 #if !BUILDFLAG(IS_IOS)
 #include "brave/components/content_settings/core/browser/brave_content_settings_pref_provider.h"
 #define PrefProvider BravePrefProvider
 #endif
 
-namespace content_settings {
-namespace {
-
-bool IsMorePermissive_BraveImpl(ContentSettingsType content_type,
-                                ContentSetting setting,
-                                ContentSetting initial_setting) {
-  // These types have additional logic for OffTheRecord profiles to always
-  // return BLOCK (with a random timeout) instead of inheriting the setting.
-  //
-  // We must be careful to not break this, otherwise
-  // ProcessIncognitoInheritanceBehavior() will return `initial_setting` which
-  // is usually ASK and incorrect for OffTheRecord profiles.
-  static constexpr auto kOffTheRecordAwareTypes =
-      base::MakeFixedFlatSet<ContentSettingsType>({
-          ContentSettingsType::NOTIFICATIONS,
-          ContentSettingsType::PROTECTED_MEDIA_IDENTIFIER,
-          ContentSettingsType::IDLE_DETECTION,
-          ContentSettingsType::BRAVE_HTTPS_UPGRADE,
-      });
-
-  const bool is_more_permissive = IsMorePermissive(setting, initial_setting);
-  if (is_more_permissive || kOffTheRecordAwareTypes.contains(content_type) ||
-      base::FeatureList::IsEnabled(kAllowIncognitoPermissionInheritance)) {
-    return is_more_permissive;
-  }
-
-  // If the type doesn't have special OffTheRecord handling, force
-  // ProcessIncognitoInheritanceBehavior() to always return `initial_setting`.
-  return true;
-}
-
-}  // namespace
-}  // namespace content_settings
-
-#define IsMorePermissive(a, b) IsMorePermissive_BraveImpl(content_type, a, b)
-
 #include <components/content_settings/core/browser/host_content_settings_map.cc>
-
-#undef IsMorePermissive
 
 #if !BUILDFLAG(IS_IOS)
 #undef PrefProvider
