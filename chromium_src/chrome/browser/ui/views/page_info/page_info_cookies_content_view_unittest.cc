06bd8fde1a1a0259e0c882429dc41766968d7347
Claudio DeSouza <cdesouza@brave.com>
2025-07-27 22:26:30 +0100

    [Reapply][msan] Fix uninitialised read caused by `SetCookieInfo`
    - Revert notes
    
    The original change was having no effect because we were subclassing
    `PageInfoCookiesContentView`, in order to avoid patching, however
    `PageInfoCookiesContentView` does virtual dispatch calls in its
    constructor, and in such cases the derived implementation is entirely
    ignored.
    
    This change corrects this issue with a combination of a patch and
    overrides now. Additionally, tests are being added to confirm that the
    changes work correctly.
    
    Finally, a dangling pointer case was detected and also corrected.
    
    Revert: https://github.com/brave/brave-core/pull/30276
    
    Original desc:
    
    This override was doing a copy of the value passed in, `cookie_info`,
    and then changing the value of one of the fields to `kHide`, in order to
    make things work properly for the rest of the things done by the
    override, and avoid a `CHECK`. The issue here is that `cookie_info` is a
    `const` reference, and `cookie_info` have a copy constructor, so the
    fields were being copied manually. This worked for a short time, until
    other fields were added to this struct, with some being uninitialised.
    
    This change removes the whole copying of this type, as that is
    bug-prone, with new fields being added, and rather overrides
    `SetThirdPartyCookiesInfo` to always bail out, as this is the actual
    effect we are looking for by mutating the value of `cookie_info`.
    `SetThirdPartyCookiesInfo` is only used by `SetCookieInfo` in fact.
    
    Resolves https://github.com/brave/brave-browser/issues/47875

---
+++ chromium_src/chrome/browser/ui/views/page_info/page_info_cookies_content_view_unittest.cc
@@ -0,0 +1,31 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include <chrome/browser/ui/views/page_info/page_info_cookies_content_view_unittest.cc>
+
+using PageInfoCookiesContentViewBaseTestClassBraveOverrides =
+    PageInfoCookiesContentViewBaseTestClass;
+
+TEST_F(PageInfoCookiesContentViewBaseTestClassBraveOverrides,
+       ThirdPartyCoookiesInfoIsHidden) {
+  PageInfoCookiesContentView::CookiesNewInfo cookie_info =
+      DefaultCookieInfoForTests();
+  content_view()->SetCookieInfo(cookie_info);
+
+  EXPECT_FALSE(third_party_cookies_container()->GetVisible());
+  EXPECT_FALSE(third_party_cookies_description_label()->GetVisible());
+}
+
+TEST_F(PageInfoCookiesContentViewBaseTestClassBraveOverrides,
+       ThirdPartyCoookiesInfoIsHiddenInIncognitoMode) {
+  PageInfoCookiesContentView::CookiesNewInfo cookie_info =
+      DefaultCookieInfoForTests();
+  cookie_info.controls_state = CookieControlsState::kAllowed3pc;
+  cookie_info.blocking_status = CookieBlocking3pcdStatus::kAll;
+  cookie_info.is_incognito = true;
+  content_view()->SetCookieInfo(cookie_info);
+  EXPECT_FALSE(third_party_cookies_container()->GetVisible());
+  EXPECT_FALSE(third_party_cookies_description_label()->GetVisible());
+}
