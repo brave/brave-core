ab5311df5a7a8d2afa9c773de75e914486544413
AlexeyBarabash <alexey@brave.com>
2025-07-28 12:21:51 +0300

    [cr140][Android] kPasswordsUseUPMLocalAndSeparateStores moved to anonymous namespace
    Updated BravePasswordManagerAndroidUtilTest.
    Replaced MaybeDeleteLoginDatabases with an empty implementation.
    
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/5794e9a0461497b7b810e9366c1c53eab711b6f2
    
    	[passwords/android] Limit kPasswordsUseUPMLocalAndSeparateStores access
    
    	No behavior change.
    	Instead of allowing arbitrary reads/writes to this three-state pref
    	(kOff/kOffAndMigrationPending/kOn), this CL exposes a minimal API to
    	satisfy production needs. This should prevent bugs like
    	crbug.com/428667627 and make it easier to get rid of the pref in the
    	future. The API consists of:
    	1. (existing) A bool getter which treats kOff and
    	   kOffAndMigrationPending alike. It is renamed to include "Legacy"
    	   and thus discourage new usages.
    	2. (new) A test-only bool setter. The pref is not updated in production
    	   ever since LoginDbDeprecationAndroid launched.
    	3. (new) A function for pref registration.
    
    	Besides that:
    	* Rename SetUsesSplitStoresAndUPMForLocal() to
    	  MaybeDeleteLoginDatabases(), since it doesn't set the pref at all
    	  anymore.
    	* Stop unnecessarily setting the pref in tests where
    	  UsesSplitStoresAndUPMForLocal() is never called (verified by
    	  temporarily replacing the implementation with NOTREACHED and observing
    	  whether the test crashes).
    	  This definition of "unnecessary" is rather conservative, but handling
    	  more tests is left for future CLs.
    
    	After this CL the legacy pref is only read in these places (only public
    	APIs are listed, not internal helpers):
    	 * MaybeDeleteLoginDatabases()
    	 * IsPasswordManagerAvailable()
    	 * LoginDbDeprecationReady()
    	 * LoginDbDeprecationRunnerFactory::GetForProfile()
    
    	Bug: 433682805
    	Change-Id: I987c653cdae9d9c862b80686950902dac9d27e9a
    	Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6691158

--- chromium_src/chrome/browser/password_manager/android/password_manager_android_util.cc
+++ chromium_src/chrome/browser/password_manager/android/password_manager_android_util.cc
@@ -3,56 +3,24 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+#include <memory>
+
+#include "base/files/file_path.h"
 #include "chrome/browser/password_manager/android/password_manager_util_bridge_interface.h"
-#include "components/password_manager/core/common/password_manager_pref_names.h"
 #include "components/prefs/pref_service.h"
 
-#define SetUsesSplitStoresAndUPMForLocal \
-  SetUsesSplitStoresAndUPMForLocal_ChromiumImpl
+#define MaybeDeleteLoginDatabases MaybeDeleteLoginDatabases_ChromiumImpl
 
 #include <chrome/browser/password_manager/android/password_manager_android_util.cc>
 
-#undef SetUsesSplitStoresAndUPMForLocal
+#undef MaybeDeleteLoginDatabases
 
 namespace password_manager_android_util {
 
-namespace {
-
-// On Android passwords can be saved into two stores: kAccountStore and
-// kProfileStore. Account store is not synced through Chromium sync and supposed
-// to store passwords at Google Account. Profile store is the store which saves
-// passwords at profile and passwords are synced as before. Decision which store
-// to use is made at PasswordSaveManagerImpl::GetPasswordStoreForSavingImpl.
-// Finally, `kPasswordsUseUPMLocalAndSeparateStores` pref is checked.
-// The stack:
-//        features_util::CanCreateAccountStore
-//        features_util::internal::CanAccountStorageBeEnabled
-//        features_util::internal::IsUserEligibleForAccountStorage
-//        features_util::GetDefaultPasswordStore
-//        PasswordFeatureManagerImpl::GetDefaultPasswordStore
-//        PasswordSaveManagerImpl::AccountStoreIsDefault
-//        PasswordSaveManagerImpl::GetPasswordStoreForSavingImpl
-// There are two ways to make passwords be saved at the profile store and be
-// synced:
-//   1) override PasswordSaveManagerImpl::AccountStoreIsDefault
-//   2) set `kPasswordsUseUPMLocalAndSeparateStores` pref to `kOff`
-// Choosing option 2.
-void ForcePasswordsProfileStore(PrefService* pref_service) {
-  pref_service->SetInteger(
-      password_manager::prefs::kPasswordsUseUPMLocalAndSeparateStores,
-      static_cast<int>(UseUpmLocalAndSeparateStoresState::kOff));
-}
-
-}  // namespace
-
-// Prevent deleting passwords local DB during migration.
-// Happens at SetUsesSplitStoresAndUPMForLocal =>
-//      MaybeDeactivateSplitStoresAndLocalUpm =>
-//      MaybeDeleteLoginDataFiles
-void SetUsesSplitStoresAndUPMForLocal(
+// Prevent any ability to delete passwords db on Android
+void MaybeDeleteLoginDatabases(
     PrefService* pref_service,
     const base::FilePath& login_db_directory,
-    std::unique_ptr<PasswordManagerUtilBridgeInterface> util_bridge) {
-  ForcePasswordsProfileStore(pref_service);
-}
+    std::unique_ptr<PasswordManagerUtilBridgeInterface> util_bridge) {}
+
 }  // namespace password_manager_android_util
