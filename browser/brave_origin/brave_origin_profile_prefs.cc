872957e40e53a5e6030204a853204e864db85566
Brian Clifton <brian@clifton.me>
2025-07-29 16:18:27 -0700

    Set defaults for the rest of the profile preferences

--- browser/brave_origin/brave_origin_profile_prefs.cc
+++ browser/brave_origin/brave_origin_profile_prefs.cc
@@ -1,7 +1,7 @@
 /* Copyright (c) 2025 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "brave/browser/brave_origin/brave_origin_profile_prefs.h"
 
@@ -9,15 +9,35 @@
 #include "brave/components/brave_news/common/pref_names.h"
 #include "brave/components/brave_origin/brave_origin_state.h"
 #include "brave/components/brave_rewards/core/pref_names.h"
-#include "brave/components/brave_vpn/common/pref_names.h"
+#include "brave/components/brave_vpn/common/buildflags/buildflags.h"
 #include "brave/components/brave_wallet/common/pref_names.h"
+#include "brave/components/brave_wayback_machine/buildflags/buildflags.h"
 #include "brave/components/constants/pref_names.h"
+#include "brave/components/p3a/pref_names.h"
 #include "brave/components/sidebar/browser/pref_names.h"
 #include "brave/components/sidebar/browser/sidebar_item.h"
-#include "brave/components/tor/pref_names.h"
+#include "brave/components/speedreader/common/buildflags/buildflags.h"
+#include "brave/components/tor/buildflags/buildflags.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
+#include "components/metrics/metrics_pref_names.h"
 #include "components/prefs/pref_service.h"
+
+#if BUILDFLAG(ENABLE_BRAVE_WAYBACK_MACHINE)
+#include "brave/components/brave_wayback_machine/pref_names.h"
+#endif
+
+#if BUILDFLAG(ENABLE_SPEEDREADER)
+#include "brave/components/speedreader/speedreader_pref_names.h"
+#endif
+
+#if BUILDFLAG(ENABLE_BRAVE_VPN)
+#include "brave/components/brave_vpn/common/pref_names.h"
+#endif
+
+#if BUILDFLAG(ENABLE_TOR)
+#include "brave/components/tor/pref_names.h"
+#endif
 
 namespace brave_origin {
 
@@ -28,45 +48,41 @@ void SetupBraveOriginProfilePrefs(Profile* profile) {
 
   if (BraveOriginState::GetInstance()->IsBraveOriginUser()) {
     PrefService* prefs = profile->GetPrefs();
-    // prefs->SetDefaultPrefValue(brave_rewards::prefs::kShowLocationBarButton,
-    // base::Value(false));
-    // prefs->SetDefaultPrefValue(brave_vpn::prefs::kBraveVPNShowButton,
-    // base::Value(false)); prefs->SetDefaultPrefValue(kShowWalletIconOnToolbar,
-    // base::Value(false));
-    // prefs->SetDefaultPrefValue(ai_chat::prefs::kBraveAIChatShowToolbarButton,
-    // base::Value(false));
-    // prefs->SetDefaultPrefValue(ai_chat::prefs::kBraveAIChatContextMenuEnabled,
-    // base::Value(false));
-    // prefs->SetDefaultPrefValue(ai_chat::prefs::kBraveChatAutocompleteProviderEnabled,
-    // base::Value(false));
 
-    // Disable BraveVPNDisabled admin policy for Brave Origin profiles
-    // Managed preferences
-    prefs->SetDefaultPrefValue(brave_vpn::prefs::kManagedBraveVPNDisabled,
+    // Profile prefs
+    prefs->SetDefaultPrefValue(brave_rewards::prefs::kDisabledByPolicy,
                                base::Value(true));
     prefs->SetDefaultPrefValue(ai_chat::prefs::kEnabledByPolicy,
                                base::Value(false));
-    prefs->SetDefaultPrefValue(brave_wallet::prefs::kDisabledByPolicy,
-                               base::Value(true));
-    prefs->SetDefaultPrefValue(brave_rewards::prefs::kDisabledByPolicy,
-                               base::Value(true));
     prefs->SetDefaultPrefValue(brave_news::prefs::kBraveNewsDisabledByPolicy,
                                base::Value(true));
+#if BUILDFLAG(ENABLE_BRAVE_VPN)
+    prefs->SetDefaultPrefValue(brave_vpn::prefs::kManagedBraveVPNDisabled,
+                               base::Value(true));
+#endif
+    prefs->SetDefaultPrefValue(brave_wallet::prefs::kDisabledByPolicy,
+                               base::Value(true));
     prefs->SetDefaultPrefValue(kBraveTalkDisabledByPolicy, base::Value(true));
-    g_browser_process->local_state()->SetBoolean(tor::prefs::kTorDisabled,
-                                                 true);
+#if BUILDFLAG(ENABLE_SPEEDREADER)
+    prefs->SetDefaultPrefValue(speedreader::kSpeedreaderDisabledByPolicy,
+                               base::Value(true));
+#endif
+#if BUILDFLAG(ENABLE_BRAVE_WAYBACK_MACHINE)
+    prefs->SetDefaultPrefValue(kBraveWaybackMachineDisabledByPolicy,
+                               base::Value(true));
+#endif
 
-    // TODO search ads!
-    // TODO Email alias!
-    // TODO Playlist
-    // Tor Windows
-
-    /*
-    prefs->SetDefaultPrefValue(brave_news::prefs::kNewTabPageShowToday,
-                               base::Value(false));
-    prefs->SetDefaultPrefValue(brave_news::prefs::kShouldShowToolbarButton,
-                               base::Value(false));
-    */
+    // Local state (spans all profiles)
+    g_browser_process->local_state()->SetDefaultPrefValue(
+        p3a::kP3ADisabledByPolicy, base::Value(true));
+    g_browser_process->local_state()->SetDefaultPrefValue(
+        kStatsReportingDisabledByPolicy, base::Value(true));
+    g_browser_process->local_state()->SetDefaultPrefValue(
+        metrics::prefs::kMetricsReportingEnabled, base::Value(false));
+#if BUILDFLAG(ENABLE_TOR)
+    g_browser_process->local_state()->SetDefaultPrefValue(
+        tor::prefs::kTorDisabled, base::Value(true));
+#endif
   }
 }
 
