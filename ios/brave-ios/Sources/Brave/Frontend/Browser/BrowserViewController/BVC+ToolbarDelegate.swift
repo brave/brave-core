3a78895960562e3d54c5d3bd911bef92993f15f4
Kyle Hickinson <git@kylehickinson.com>
2025-07-30 13:38:51 -0400

    [iOS] Shift url sanitizer Obj-C wrappers into url_sanitizer component
    This also converts `url_sanitizer` into a layered component

--- ios/brave-ios/Sources/Brave/Frontend/Browser/BrowserViewController/BVC+ToolbarDelegate.swift
+++ ios/brave-ios/Sources/Brave/Frontend/Browser/BrowserViewController/BVC+ToolbarDelegate.swift
@@ -1205,7 +1205,7 @@ extension BrowserViewController: UIContextMenuInteractionDelegate {
         image: UIImage(braveSystemNamed: "leo.copy.clean"),
         handler: UIAction.deferredActionHandler { _ in
           let service = URLSanitizerServiceFactory.get(privateMode: tab?.isPrivate ?? true)
-          let cleanedURL = service?.sanitizeURL(url) ?? url
+          let cleanedURL = service?.sanitize(url: url) ?? url
           UIPasteboard.general.url = cleanedURL
         }
       ),
