01a2edadbad5ec9caabbf142f679e8dd725d8f81
Gaetano Checinski <gchecinski@brave.com>
2025-07-30 10:28:54 +0100

    refactor to testuils

--- build/commands/lib/test.js
+++ build/commands/lib/test.js
@@ -10,83 +10,13 @@ const Config = require('../lib/config')
 const Log = require('../lib/logging')
 const util = require('../lib/util')
 const assert = require('assert')
+const {
+  getTestBinary,
+  getChromiumTestsSuites,
+  getTestsToRun,
+  getApplicableFilters
+} = require('./testUtils')
 
-const getTestBinary = (config, suite) => {
-  let testBinary = suite
-  if (testBinary === 'brave_java_unit_tests') {
-    testBinary = path.join('bin', 'run_brave_java_unit_tests')
-  } else if (testBinary === 'brave_junit_tests') {
-    testBinary = path.join('bin', 'run_brave_junit_tests')
-  }
-
-  if (config.isIOS()) {
-    testBinary = path.join(`${testBinary}.app`)
-  }
-
-  if (process.platform === 'win32') {
-    testBinary = path.join(`${testBinary}.exe`)
-  }
-  return path.join(config.outputDir, testBinary)
-}
-
-const getChromiumTestsSuites = (config) => {
-  return getTestsToRun(config, 'chromium_unit_tests').concat(['browser_tests'])
-}
-
-const getTestsToRun = (config, suite) => {
-  const testDepFile = path.join(config.outputDir, `${suite}.json`)
-  if (fs.existsSync(testDepFile)) {
-    suiteDepNames = JSON.parse(
-      fs.readFileSync(testDepFile, { encoding: 'utf-8' }),
-    )
-    return suiteDepNames.map((x) => x.split(':').at(-1))
-  }
-
-  return [suite]
-}
-
-// Returns a list of paths to files containing all the filters that would apply
-// to the current test suite, as long as such files exist in the filesystem.
-//
-// For instance, for Windows 64-bit and assuming all the filters files exist
-// in the filesystem, this method would return paths to the following files:
-//   - unit-tests.filter              -> Base filters
-//   - unit_tests-windows.filters:    -> Platform specific
-//   - unit_tests-windows-x86.filters -> Platform & Architecture specific
-const getApplicableFilters = (config, suite) => {
-  let filterFilePaths = []
-
-  let targetPlatform = process.platform
-  if (targetPlatform === 'win32') {
-    targetPlatform = 'windows'
-  } else if (targetPlatform === 'darwin') {
-    targetPlatform = 'macos'
-  }
-
-  let possibleFilters = [
-    suite,
-    [suite, targetPlatform].join('-'),
-    [suite, targetPlatform, config.targetArch].join('-'),
-  ]
-
-  if (config.is_ubsan) {
-    possibleFilters.push([suite, targetPlatform, 'ubsan'].join('-'))
-  }
-
-  possibleFilters.forEach((filterName) => {
-    let filterFilePath = path.join(
-      config.braveCoreDir,
-      'test',
-      'filters',
-      `${filterName}.filter`,
-    )
-    if (fs.existsSync(filterFilePath)) {
-      filterFilePaths.push(filterFilePath)
-    }
-  })
-
-  return filterFilePaths
-}
 
 const test = async (
   passthroughArgs,
