943704205c60190e4e3bbbbc10b0e126bf9fa43a
Christian Mazakas <christian.mazakas@gmail.com>
2025-07-30 12:49:23 -0700

    remove extraneous SchnorrkelKeyPair class

--- components/brave_wallet/browser/internal/hd_key_sr25519.cc
+++ components/brave_wallet/browser/internal/hd_key_sr25519.cc
@@ -7,43 +7,67 @@
 
 #include <utility>
 
-#include "brave/components/brave_wallet/browser/internal/polkadot/rust/sr25519.h"
+#include "base/containers/span_rust.h"
+#include "brave/components/brave_wallet/browser/internal/polkadot/rust/lib.rs.h"
+#include "third_party/rust/cxx/v1/cxx.h"
 
 namespace brave_wallet {
 
-HDKeySr25519::HDKeySr25519(base::PassKey<HDKeySr25519>,
-                           polkadot::SchnorrkelKeyPair skp)
-    : schnorrkel_key_pair_(std::move(skp)) {}
+namespace {
+bool IsBoxNonNull(rust::Box<polkadot::CxxSchnorrkelKeyPair> const& keypair) {
+  return keypair.operator->();
+}
+}  // namespace
+
+HDKeySr25519::HDKeySr25519(rust::Box<polkadot::CxxSchnorrkelKeyPair> keypair)
+    : keypair_(std::move(keypair)) {}
+
+HDKeySr25519::HDKeySr25519(HDKeySr25519&&) noexcept = default;
+HDKeySr25519& HDKeySr25519::operator=(HDKeySr25519&& rhs) noexcept {
+  if (this != &rhs) {
+    keypair_ = std::move(rhs.keypair_);
+  }
+  return *this;
+}
 
 HDKeySr25519::~HDKeySr25519() = default;
 
 std::optional<HDKeySr25519> HDKeySr25519::GenerateFromSeed(
     base::span<const uint8_t> seed) {
-  auto skp = polkadot::SchnorrkelKeyPair::GenerateFromSeed(seed);
-  if (!skp) {
+  auto bytes = base::SpanToRustSlice(seed);
+  auto mk = polkadot::generate_sr25519_keypair_from_seed(bytes);
+  if (!mk->is_ok()) {
     return std::nullopt;
   }
-  return HDKeySr25519(base::PassKey<HDKeySr25519>{}, std::move(*skp));
+  return HDKeySr25519(mk->unwrap());
 }
 
-polkadot::SR25519PublicKey HDKeySr25519::GetPublicKey() {
-  return schnorrkel_key_pair_.GetPublicKey();
+std::array<uint8_t, kSr25519PublicKeySize> HDKeySr25519::GetPublicKey() {
+  CHECK(IsBoxNonNull(keypair_));
+  return keypair_->get_public_key();
 }
 
-polkadot::SR25519Signature HDKeySr25519::SignMessage(
+std::array<uint8_t, kSr25519SignatureSize> HDKeySr25519::SignMessage(
     base::span<const uint8_t> msg) {
-  return schnorrkel_key_pair_.SignMessage(msg);
+  CHECK(IsBoxNonNull(keypair_));
+  auto bytes = base::SpanToRustSlice(msg);
+  return keypair_->sign_message(bytes);
 }
 
-bool HDKeySr25519::VerifyMessage(polkadot::SR25519Signature const& sig,
-                                 base::span<const uint8_t> msg) {
-  return schnorrkel_key_pair_.VerifyMessage(sig, msg);
+bool HDKeySr25519::VerifyMessage(
+    std::array<uint8_t, kSr25519SignatureSize> const& sig,
+    base::span<const uint8_t> msg) {
+  CHECK(IsBoxNonNull(keypair_));
+  auto sig_bytes = base::SpanToRustSlice(sig);
+  auto bytes = base::SpanToRustSlice(msg);
+  return keypair_->verify_message(sig_bytes, bytes);
 }
 
 HDKeySr25519 HDKeySr25519::DeriveHard(
     base::span<const uint8_t> derive_junction) {
-  return HDKeySr25519(base::PassKey<HDKeySr25519>{},
-                      schnorrkel_key_pair_.DeriveHard(derive_junction));
+  CHECK(IsBoxNonNull(keypair_));
+  return HDKeySr25519(
+      keypair_->derive_hard(base::SpanToRustSlice(derive_junction)));
 }
 
 }  // namespace brave_wallet
