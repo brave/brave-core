62a025cde0fa127374343758248e23bb9cd74a46
AlexeyBarabash <alexey@brave.com>
2025-07-31 00:57:23 +0300

    [cr140][Android] Modified BasicListMenu.ctor
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/9d9b0aabaf648e9e829f80396801a9b125e06a72
    
    	Add rendering for fixed header and scrollable content
    
    	Demo: https://screencast.googleplex.com/cast/NTY5NzQxMTM2MzExMDkxMnxhZjU0YjQ5OC01Zg
    
    	Bug: 432981212
    	Change-Id: I6c9438bed6881ee5da7627cec62f03bac2fd3b2e
    	Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6788783

--- android/java/org/chromium/chrome/browser/settings/NetworkPreferenceAdapter.java
+++ android/java/org/chromium/chrome/browser/settings/NetworkPreferenceAdapter.java
@@ -10,13 +10,12 @@ import static androidx.recyclerview.widget.RecyclerView.NO_POSITION;
 import static org.chromium.components.browser_ui.widget.ListItemBuilder.buildSimpleMenuItem;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -313,15 +312,13 @@ public class NetworkPreferenceAdapter extends RecyclerView.Adapter<ViewHolder> {
                     };
             networkViewHolder.setMenuButtonDelegate(
                     () -> {
-                        View contentView =
-                                LayoutInflater.from(mContext)
-                                        .inflate(
-                                                R.layout.app_menu_layout,
-                                                networkViewHolder.mItem,
-                                                false);
-                        ListView listView = contentView.findViewById(R.id.app_menu_list);
                         return new BasicListMenu(
-                                mContext, menuItems, contentView, listView, delegate, 0);
+                                mContext,
+                                menuItems,
+                                delegate,
+                                R.drawable.default_popup_menu_bg,
+                                /* backgroundTintColor= */ Resources.ID_NULL,
+                                /* bottomHairlineColor= */ null);
                     });
         } else {
             final LabelViewHolder labelViewHolder = (LabelViewHolder) viewHolder;
@@ -443,7 +440,6 @@ public class NetworkPreferenceAdapter extends RecyclerView.Adapter<ViewHolder> {
     static class NetworkViewHolder extends ViewHolder {
         private final TextView mTitle;
         private final TextView mDescription;
-        private final LinearLayout mItem;
         private final ImageView mShowHideNetwork;
 
         private final ListMenuButton mMoreButton;
@@ -455,7 +451,6 @@ public class NetworkPreferenceAdapter extends RecyclerView.Adapter<ViewHolder> {
             mDescription = view.findViewById(R.id.description);
             mShowHideNetwork = view.findViewById(R.id.show_hide_network);
             mMoreButton = view.findViewById(R.id.more);
-            mItem = view.findViewById(R.id.network_item);
         }
 
         /**
