7c9947299eb8b5e3335e0f538218c0f8078b7b25
Gaetano Checinski <gchecinski@brave.com>
2025-07-30 12:07:43 +0100

    chore: add missing dep

--- build/commands/lib/testUtils.js
+++ build/commands/lib/testUtils.js
@@ -3,6 +3,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
+const fs = require('fs-extra')
 const path = require('path')
 
 const getTestBinary = (config, suite) => {
