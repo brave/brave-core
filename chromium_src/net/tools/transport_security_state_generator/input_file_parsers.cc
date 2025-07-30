c4a54929bf42c7b927633acc9baf137c8434ba6f
Max Karolinskiy <41635752+mkarolin@users.noreply.github.com>
2025-07-30 01:29:23 -0400

    Upgrade from Chromium 138.0.7204.168 to Chromium 138.0.7204.184 (1.80.x). (#30345)
    * Update from Chromium 138.0.7204.168 to Chromium 138.0.7204.184.
    
    * Add support for tab group shortcuts
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/4e3df1fa8c65d2dc52990a96eb977670327f982c
    
    commit 4e3df1fa8c65d2dc52990a96eb977670327f982c
    Author: dljames <dljames@chromium.org>
    Date:   Fri Jul 11 17:36:48 2025 -0700
    
        [TabGroups] Default Enable TabGroupShortcuts Feature
    
        Default enables the TabGroupShortcuts feature on ToT in preparation for
        launch.
    
        Mac: The accelerators are omitted in accelerator_table.cc since they
        are already added in global_shortcuts_mac.mm
    
        ChromeOS: The accelerators are omitted since they caused major
        conflicts with the ChromeOS accelerator system. I spent a few days
        trying to fix this (Ex: updating tests, updating shortcuts, adding
        duplicate shortcuts similar to mac) but didn't find success.
    
        Change-Id: Ibdf8544ce844bc8fb4fc7d1192df786793df25c4
        Bug: 403622318
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6711452
        Commit-Queue: Darryl James <dljames@chromium.org>
        Reviewed-by: Steven Luong <stluong@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1485901}
    
    ---------
    
    Co-authored-by: Emerick Rogul <erogul@brave.com>

--- chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
+++ chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
@@ -546,9 +546,9 @@ bool ParseCertificatesFile(std::string_view certs_input,
                            Pinsets* pinsets,
                            base::Time* timestamp) {
   constexpr std::string_view brave_certs = R"brave_certs(
-# Last updated: Tue Jul 22 16:38:26 2025
+# Last updated: Tue Jul 29 16:49:46 2025
 PinsListTimestamp
-1753216706
+1753822186
 
 # =====BEGIN BRAVE ROOTS ASC=====
 #From https://www.amazontrust.com/repository/
