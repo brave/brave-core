02b861f11fdd4b50b3fcdd6449bf42081675a474
Claudio DeSouza <cdesouza@brave.com>
2025-07-31 11:58:52 +0100

    [cr140] `omnibox.mojom` moved into `//components/omnibox`
    Patching in this file was being done to `searchbox.mojom`, adding
    `generate_legacy_js_bindings` legacy to the target. This file has been
    moved though to `//components/omnibox/browser`, and that target is
    already being patched for the legacy js bindings.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/009d2470cf96af5a9d7b880c9f22cd2382a5b63a
    
    commit 009d2470cf96af5a9d7b880c9f22cd2382a5b63a
    Author: Nihar Majmudar <niharm@google.com>
    Date:   Wed Jul 30 14:00:31 2025 -0700
    
        [composebox] Make omnibox and searchbox mojom files shared.
    
        Since mojom files in cr_components/ can't be imported by other .mojom
        files, this is needed in order to import searchbox.mojom into
        composebox.mojom. This is a precursor in order to allow us to eventually
        reuse the SearchboxHandler for Composebox ZPS.
    
        Moves searchbox.mojom to //components/omnibox/browser and serves both
        mojom files from chrome://resources/mojo/components/omnibox/browser.
    
        Change-Id: I019beea4d56111cb36ced906d1e94b5ff8169f82
        Bug: 433980798
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6783770
        Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
        Commit-Queue: Nihar Majmudar <niharm@google.com>
        Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
        Reviewed-by: Ken Buchanan <kenrb@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1494450}

--- browser/resources/brave_new_tab_page_refresh/state/search_state.ts
+++ browser/resources/brave_new_tab_page_refresh/state/search_state.ts
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-import * as mojom from 'gen/ui/webui/resources/cr_components/searchbox/searchbox.mojom.m'
+import * as mojom from 'gen/components/omnibox/browser/searchbox.mojom.m'
 
 export const braveSearchHost = 'search.brave.com'
 
