df2927947ebf29ee50554afc85b61c4a85587935
Claudio DeSouza <cdesouza@brave.com>
2025-08-01 12:20:04 +0100

    [cr140] Migrate `JSCardanoProvider` to `gin::Wrappable`
    This change migrates `JSCardanoProvider` to have its lifetime managed by
    Oilpan.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/61e491610dac67129273aa96549603483c59759f
    
    commit 61e491610dac67129273aa96549603483c59759f
    Author: Andreas Haas <ahaas@chromium.org>
    Date:   Wed Jul 30 03:35:32 2025 -0700
    
        [gin] Delete DeprecatedWrappable
    
        This CL deletes gin::DeprecatedWrappable, and classes and data
        structures related to gin::DeprecatedWrappable. Additionally it removes
        internal fields of JS wrapper objects that were only used by
        gin::DeprecatedWrappable, but are not needed anymore with the new
        gin::Wrappable.
    
        Bug: 345640553
        Change-Id: Ideb4424048e62fec6c6a7ee5fd60a4635f6ef1e8
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6799157
        Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
        Reviewed-by: Camille Lamy <clamy@chromium.org>
        Commit-Queue: Andreas Haas <ahaas@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1494071}

---
+++ chromium_src/gin/public/wrappable_pointer_tags.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_CHROMIUM_SRC_GIN_PUBLIC_WRAPPABLE_POINTER_TAGS_H_
+#define BRAVE_CHROMIUM_SRC_GIN_PUBLIC_WRAPPABLE_POINTER_TAGS_H_
+
+#define kTextInputControllerBindings \
+  kTextInputControllerBindings, kCardanoProvider
+
+#include <gin/public/wrappable_pointer_tags.h>  // IWYU pragma: export
+
+#undef kTextInputControllerBindings
+
+#endif  // BRAVE_CHROMIUM_SRC_GIN_PUBLIC_WRAPPABLE_POINTER_TAGS_H_
