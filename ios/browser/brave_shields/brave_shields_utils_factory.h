76de68237a98315d07bcf5ae6b49cdf81722beea
Stephen Heaps <steaps.4@gmail.com>
2025-08-01 18:44:11 -0400

    Add factory to provide BraveShieldsUtils to iOS. - Factory takes in a `ProfileBridge` which is exposed on   `BraveProfileController` and provides the `BraveShieldsUtils`   implementation from `brave/components/brave_shields/ios/`.

---
+++ ios/browser/brave_shields/brave_shields_utils_factory.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_IOS_BROWSER_BRAVE_SHIELDS_BRAVE_SHIELDS_UTILS_FACTORY_H_
+#define BRAVE_IOS_BROWSER_BRAVE_SHIELDS_BRAVE_SHIELDS_UTILS_FACTORY_H_
+
+#import <Foundation/Foundation.h>
+
+@protocol BraveShieldsUtils;
+@protocol ProfileBridge;
+
+NS_ASSUME_NONNULL_BEGIN
+
+OBJC_EXPORT
+@interface BraveShieldsUtilsFactory : NSObject
++ (id<BraveShieldsUtils>)getForProfile:(id<ProfileBridge>)profile
+    NS_SWIFT_NAME(get(profile:));
+@end
+
+NS_ASSUME_NONNULL_END
+
+#endif  // BRAVE_IOS_BROWSER_BRAVE_SHIELDS_BRAVE_SHIELDS_UTILS_FACTORY_H_
