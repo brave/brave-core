3e496e345ba1d4b221de29d04e195d7275750f3b
Stephen Heaps <steaps.4@gmail.com>
2025-08-01 18:44:11 -0400

    Add factory to provide BraveShieldsUtils to iOS. - Factory takes in a `ProfileBridge` which is exposed on   `BraveProfileController` and provides the `BraveShieldsUtils`   implementation from `brave/components/brave_shields/ios/`.

---
+++ ios/browser/brave_shields/brave_shields_utils_factory.mm
@@ -0,0 +1,31 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "brave/ios/browser/brave_shields/brave_shields_utils_factory.h"
+
+#include "base/apple/foundation_util.h"
+#include "brave/components/brave_shields/ios/browser/brave_shields_utils.h"
+#include "brave/components/brave_shields/ios/browser/brave_shields_utils_impl.h"
+#include "brave/ios/browser/api/profile/profile_bridge_impl.h"
+#include "ios/chrome/browser/content_settings/model/host_content_settings_map_factory.h"
+#include "ios/chrome/browser/shared/model/application_context/application_context.h"
+#include "ios/chrome/browser/shared/model/profile/profile_ios.h"
+
+@implementation BraveShieldsUtilsFactory
+
++ (id<BraveShieldsUtils>)getForProfile:(id<ProfileBridge>)profile {
+  ProfileBridgeImpl* holder =
+      base::apple::ObjCCastStrict<ProfileBridgeImpl>(profile);
+  ProfileIOS* _profile = holder.profile;
+  auto* map = ios::HostContentSettingsMapFactory::GetForProfile(_profile);
+  auto* localState = GetApplicationContext()->GetLocalState();
+  auto* profilePrefs = _profile->GetPrefs();
+  return [[BraveShieldsUtilsImpl alloc]
+      initWithHostContentSettingsMap:map
+                          localState:localState
+                        profilePrefs:profilePrefs];
+}
+
+@end
