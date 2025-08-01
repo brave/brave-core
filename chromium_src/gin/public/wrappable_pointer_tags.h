2525614fa7af0f0421a9091da2fce961d3bf81ad
Claudio DeSouza <cdesouza@brave.com>
2025-08-01 14:25:13 +0100

    [cr140] Migrate `SpeedreaderJSHandler` to `gin::Wrappable`
    This class was still using the deprecated version that is set to be
    deleted. This change migrates this class to use Oilpan GC for memory
    management.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/61e491610dac67129273aa96549603483c59759f
    
    commit 61e491610dac67129273aa96549603483c59759f
    Author: Andreas Haas <ahaas@chromium.org>
    Date:   Wed Jul 30 03:35:32 2025 -0700
    
        [gin] Delete DeprecatedWrappable
    
        This CL deletes gin::DeprecatedWrappable, and classes and data
        structures related to gin::DeprecatedWrappable. Additionally it removes
        internal fields of JS wrapper objects that were only used by
        gin::DeprecatedWrappable, but are not needed anymore with the new
        gin::Wrappable.
    
        Bug: 345640553
        Change-Id: Ideb4424048e62fec6c6a7ee5fd60a4635f6ef1e8
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6799157
        Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
        Reviewed-by: Camille Lamy <clamy@chromium.org>
        Commit-Queue: Andreas Haas <ahaas@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1494071}

--- chromium_src/gin/public/wrappable_pointer_tags.h
+++ chromium_src/gin/public/wrappable_pointer_tags.h
@@ -14,6 +14,7 @@
   kMetaMask,                          \
   kSolanaProvider,                    \
   kSkusBindings,                      \
+  SpeedreaderBindings,                \
   kCardanoProvider
 // clang-format on
 
