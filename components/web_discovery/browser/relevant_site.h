d3efe97581f7618548405b970fc825457784cf88
Darnell Andries <dandries@brave.com>
2025-07-31 22:44:10 -0700

    Add parsing of new Web Discovery DSL, add new URL extractor util, add support for new transform functions

---
+++ components/web_discovery/browser/relevant_site.h
@@ -0,0 +1,32 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_WEB_DISCOVERY_BROWSER_RELEVANT_SITE_H_
+#define BRAVE_COMPONENTS_WEB_DISCOVERY_BROWSER_RELEVANT_SITE_H_
+
+#include <optional>
+#include <string_view>
+
+namespace web_discovery {
+
+enum class RelevantSite {
+  kGoogleImages,   // search-goi
+  kGoogleVideos,   // search-gov
+  kGoogle,         // search-go
+  kYahoo,          // search-ya
+  kBingImages,     // search-bii
+  kBing,           // search-bi
+  kAmazonSearch,   // search-am
+  kAmazonProduct,  // amp
+  kDuckDuckGo,     // search-dd
+  kLinkedIn,       // li
+};
+
+// Converts an ID to the corresponding RelevantSite enum value
+std::optional<RelevantSite> RelevantSiteFromID(std::string_view id);
+
+}  // namespace web_discovery
+
+#endif  // BRAVE_COMPONENTS_WEB_DISCOVERY_BROWSER_RELEVANT_SITE_H_
