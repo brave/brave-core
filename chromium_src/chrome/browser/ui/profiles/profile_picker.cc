7e871741f2c6375af6409bd75280b9576ab9f7fb
Pete Miller <miller.pete@gmail.com>
2025-08-01 17:34:49 -0700

    extra test and comments for chromium_src overrides

--- chromium_src/chrome/browser/ui/profiles/profile_picker.cc
+++ chromium_src/chrome/browser/ui/profiles/profile_picker.cc
@@ -5,6 +5,11 @@
 
 #include "brave/browser/ai_chat/ai_chat_profile.h"
 
+// Customize which profiles are considered "active" and therefore in contention
+// for whether they should be the default profile on startup. If more than
+// one is found then the profile picker will be shown.
+// Make sure the AI Chat profile is not allowed to be the startup profile
+// and does not cause the profile picker to be shown when first created.
 #define BRAVE_PROFILE_PICKER_GET_STARTUP_MODE_REASON_IS_ACTIVE_PROFILE \
   if (ai_chat::IsAIChatContentAgentProfile(entry->GetPath())) {        \
     return false;                                                      \
