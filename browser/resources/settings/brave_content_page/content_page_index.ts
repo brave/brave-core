c838a42ae11fea096dee911a9f85717ed22bba3e
Jay Harris <jay.harris@outlook.co.nz>
2025-08-05 10:01:40 +1200

    [Settings]: Migrate Brave Content to new plugin architecture

---
+++ browser/resources/settings/brave_content_page/content_page_index.ts
@@ -0,0 +1,87 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import 'chrome://resources/cr_elements/cr_view_manager/cr_view_manager.js';
+import '/shared/settings/prefs/prefs.js';
+
+import '../settings_shared.css.js';
+
+import type {CrViewManagerElement} from 'chrome://resources/cr_elements/cr_view_manager/cr_view_manager.js';
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {RouteObserverMixin} from '../router.js';
+import type {Route} from '../router.js';
+import {routes} from '../route.js';
+
+import type {SettingsPlugin} from '../settings_main/settings_plugin.js';
+import {SearchableViewContainerMixin} from '../settings_page/searchable_view_container_mixin.js';
+
+import {getTemplate} from './content_page_index.html.js';
+
+export interface SettingsBraveContentPageIndexElement {
+  $: {
+    viewManager: CrViewManagerElement,
+  };
+}
+
+const SettingsBraveContentPageIndexElementBase =
+    SearchableViewContainerMixin(RouteObserverMixin(PolymerElement));
+
+export class SettingsBraveContentPageIndexElement extends
+    SettingsBraveContentPageIndexElementBase implements SettingsPlugin {
+  static get is() {
+    return 'settings-brave-content-page-index';
+  }
+
+  static get template() {
+    return getTemplate();
+  }
+
+  static get properties() {
+    return {
+      prefs: Object,
+    };
+  }
+
+  declare prefs: {[key: string]: any};
+
+  override currentRouteChanged(newRoute: Route, oldRoute?: Route) {
+    super.currentRouteChanged(newRoute, oldRoute);
+
+    // Need to wait for currentRouteChanged observers on child views to run
+    // first, before switching views.
+    queueMicrotask(() => {
+      switch (newRoute) {
+        case (routes as any).BRAVE_CONTENT:
+          this.$.viewManager.switchView(
+              'parent', 'no-animation', 'no-animation');
+          break;
+        case routes.FONTS:
+          this.$.viewManager.switchView(
+              'fonts', 'no-animation', 'no-animation');
+          break;
+        case routes.BASIC:
+          // Switch back to the default view in case they are part of search
+          // results.
+          this.$.viewManager.switchView(
+              'parent', 'no-animation', 'no-animation');
+          break;
+        default:
+          // Nothing to do. Other parent elements are responsible for updating
+          // the displayed contents.
+          break;
+      }
+    });
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'settings-brave-content-page-index': SettingsBraveContentPageIndexElement;
+  }
+}
+
+customElements.define(
+    SettingsBraveContentPageIndexElement.is, SettingsBraveContentPageIndexElement);
