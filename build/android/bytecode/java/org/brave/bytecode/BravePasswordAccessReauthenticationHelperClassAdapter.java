b13e342df09a6b652c7a85c761049d2d5c9cb1b3
AlexeyBarabash <alexey@brave.com>
2025-08-05 20:59:38 +0300

    [cr140][Android] WIP PasswordAccessReauthenticationHelper removed
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/6875d2ce472e7f2097617525feec45313602e225
    
    	[passwords/android] Remove unused password checkup UI
    
    	The last caller of PasswordCheck.showUi() was removed in
    	https://crrev.com/c/6787824.
    	https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/password_check/android/java/src/org/chromium/chrome/browser/password_check/PasswordCheck.java;l=53;drc=f49f3252d477a894bc8af322f7ae735ca8eb2d07
    
    	Note: PasswordCheckupIntegrationTest is obsolete because it predates
    	the launch of the SafetyHub flag.
    
    	Bug: 433683573
    	Change-Id: I3f7eb4fb9d5c7fd774f09b0ef887908aea63d050
    	Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6793022

--- build/android/bytecode/java/org/brave/bytecode/BravePasswordAccessReauthenticationHelperClassAdapter.java
+++ build/android/bytecode/java/org/brave/bytecode/BravePasswordAccessReauthenticationHelperClassAdapter.java
@@ -11,18 +11,23 @@ import org.objectweb.asm.ClassVisitor;
  * Adapter, used to make some fields from upstream's PasswordAccessReauthenticationHelper be public
  */
 public class BravePasswordAccessReauthenticationHelperClassAdapter extends BraveClassVisitor {
-    static String sHelperClassName =
-            "org/chromium/chrome/browser/password_manager/settings/PasswordAccessReauthenticationHelper"; // presubmit: ignore-long-line
-    static String sBraveHelperClassName =
-            "org/chromium/chrome/browser/sync/settings/BravePasswordAccessReauthenticationHelper";
+    // TODO(alexeybarabash): backport PasswordAccessReauthenticationHelper
+    // Upstream change: 6875d2ce472e7f2097617525feec45313602e225
+    // static String sHelperClassName =
+    //
+    // "org/chromium/chrome/browser/password_manager/settings/PasswordAccessReauthenticationHelper";
+    // // presubmit: ignore-long-line
+    // static String sBraveHelperClassName =
+    //
+    // "org/chromium/chrome/browser/sync/settings/BravePasswordAccessReauthenticationHelper";
 
     BravePasswordAccessReauthenticationHelperClassAdapter(ClassVisitor visitor) {
         super(visitor);
 
-        deleteField(sBraveHelperClassName, "mCallback");
-        makeProtectedField(sHelperClassName, "mCallback");
+        // deleteField(sBraveHelperClassName, "mCallback");
+        // makeProtectedField(sHelperClassName, "mCallback");
 
-        deleteField(sBraveHelperClassName, "mFragmentManager");
-        makeProtectedField(sHelperClassName, "mFragmentManager");
+        // deleteField(sBraveHelperClassName, "mFragmentManager");
+        // makeProtectedField(sHelperClassName, "mFragmentManager");
     }
 }
