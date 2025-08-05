1bb8372000a9b5d25b24f20f21ccff140d822d4a
Shivan Kaul Sahib <shivankaulsahib@gmail.com>
2025-08-05 12:47:24 -0700

    Remove 7 day cookie expiry capping
    Fall back to HTTP cookie lifetime capping i.e. 180 days in Brave

--- chromium_src/services/network/restricted_cookie_manager.cc
+++ chromium_src/services/network/restricted_cookie_manager.cc
@@ -24,37 +24,13 @@
 // for components/content_settings/core/common/cookie_settings_base.{h,cc}.
 #define IsFullCookieAccessAllowed IsEphemeralCookieAccessAllowed
 
-#define FromStorage(NAME, VALUE, DOMAIN, PATH, CREATION, EXPIRY, LAST_ACCESS, \
-                    LAST_UPDATE, SECURE, HTTP_ONLY, SAME_SITE, PRIORITY,      \
-                    PARTITION, SOURCE_SCHEME, PORT, COOKIE_SOURCE)            \
-  FromStorage(NAME, VALUE, DOMAIN, PATH, CREATION,                            \
-              ModifyExpiration(EXPIRY, CREATION), LAST_ACCESS, LAST_UPDATE,   \
-              SECURE, HTTP_ONLY, SAME_SITE, PRIORITY, PARTITION,              \
-              SOURCE_SCHEME, PORT, COOKIE_SOURCE)
-
 #include "src/services/network/restricted_cookie_manager.cc"
 
-#undef FromStorage
 #undef IsFullCookieAccessAllowed
 #undef AnnotateAndMoveUserBlockedCookies
 #undef IsCookieAccessible
 
-namespace {
-
-constexpr base::TimeDelta kMaxCookieExpiration =
-    base::Days(7);  // For JS cookies: CookieStore and document.cookie
-
-}  // namespace
-
 namespace network {
-
-base::Time RestrictedCookieManager::ModifyExpiration(
-    const base::Time& expiry_date,
-    const base::Time& creation_date) const {
-  const base::Time max_expiration = creation_date + kMaxCookieExpiration;
-
-  return std::min(expiry_date, max_expiration);
-}
 
 net::CookieOptions RestrictedCookieManager::MakeOptionsForSet(
     mojom::RestrictedCookieManagerRole role,
