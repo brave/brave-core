985a38e1bdf4ebb814451dfe472ff3f760b0c655
Brian Clifton <brian@clifton.me>
2025-08-05 17:40:14 -0700

    Initialize Brave Origin component

---
+++ components/brave_origin/brave_origin_state.cc
@@ -0,0 +1,35 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "brave/components/brave_origin/brave_origin_state.h"
+
+#include "base/logging.h"
+#include "base/no_destructor.h"
+#include "brave/components/brave_origin/features.h"
+
+BraveOriginState::BraveOriginState()
+    : is_brave_origin_user_(false), initialized_(false) {}
+
+BraveOriginState::~BraveOriginState() = default;
+
+BraveOriginState* BraveOriginState::GetInstance() {
+  static base::NoDestructor<BraveOriginState> instance;
+  return instance.get();
+}
+
+void BraveOriginState::Initialize() {
+  // TODO(https://github.com/brave/brave-browser/issues/47463)
+  // Get the actual purchase state from SKU service.
+  is_brave_origin_user_ =
+      base::FeatureList::IsEnabled(brave_origin::features::kBraveOrigin);
+  initialized_ = true;
+}
+
+bool BraveOriginState::IsBraveOriginUser() const {
+  if (!initialized_) {
+    return false;
+  }
+  return is_brave_origin_user_;
+}
---
+++ components/brave_origin/features.h
@@ -0,0 +1,18 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_BRAVE_ORIGIN_FEATURES_H_
+#define BRAVE_COMPONENTS_BRAVE_ORIGIN_FEATURES_H_
+
+#include "base/feature_list.h"
+#include "build/build_config.h"
+
+namespace brave_origin::features {
+
+BASE_DECLARE_FEATURE(kBraveOrigin);
+
+}  // namespace brave_origin::features
+
+#endif  // BRAVE_COMPONENTS_BRAVE_ORIGIN_FEATURES_H_
