6231ae60f0d141571e3d0233f0f3e16bd37f57d2
Artem Samoilenko <artem@brave.com>
2025-08-07 14:43:00 -0400

    [cr141][Android] Remove duplicated code in SystemAccountManagerDelegate
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/4ba632db1601435a435daa1578887efc26061e39
    
    commit 4ba632db1601435a435daa1578887efc26061e39
    Author: Liza Bipin <mlbipin@google.com>
    Date:   Wed Aug 6 08:53:51 2025 -0700
    
        [Signin][Android]Remove duplicated code in SystemAccountManagerDelegate
    
        Rename the class to be more reflective of current implementation. See
        crbug.com/429143376#comment3 for more context.
    
        Bug: 429143376
        Change-Id: I7883533dd7edc61516f24064bb457eb23752e10e
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6818502
        Commit-Queue: Liza Bipin <mlbipin@google.com>
        Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
        Reviewed-by: Liza Bipin <mlbipin@google.com>
        Cr-Commit-Position: refs/heads/main@{#1497626}

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -1899,8 +1899,8 @@ public class BytecodeTest {
                         WebContents.class));
         Assert.assertTrue(
                 constructorsMatch(
-                        "org/chromium/components/signin/SystemAccountManagerDelegate",
-                        "org/chromium/components/signin/BraveSystemAccountManagerDelegate"));
+                        "org/chromium/components/signin/NullAccountManagerDelegate",
+                        "org/chromium/components/signin/BraveNullAccountManagerDelegate"));
         Assert.assertTrue(
                 constructorsMatch(
                         "org/chromium/chrome/browser/omnibox/AutocompleteEditText",
