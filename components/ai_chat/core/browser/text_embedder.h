eef92b7dd5ab8c24286319541f84f1ee119bc55f
Vandana Malayil <vandanamalayil@Vandanas-MacBook-Pro.local>
2025-08-07 14:46:06 +0100

    adding core files

---
+++ components/ai_chat/core/browser/text_embedder.h
@@ -0,0 +1,110 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+ #ifndef BRAVE_COMPONENTS_AI_CHAT_CORE_BROWSER_TEXT_EMBEDDER_H_
+ #define BRAVE_COMPONENTS_AI_CHAT_CORE_BROWSER_TEXT_EMBEDDER_H_
+ 
+ #include <stddef.h>
+ #include <cstdint>
+ #include <memory>
+ #include <string>
+ #include <utility>
+ #include <vector>
+ 
+ /* "base/*" refers to the base folder inside chromium - https://github.com/chromium/chromium/tree/main/base */
+ #include "base/files/file_path.h"
+ #include "base/functional/callback.h"
+ #include "base/memory/scoped_refptr.h"
+ #include "base/memory/weak_ptr.h"
+ #include "base/synchronization/lock.h"
+ #include "base/task/sequenced_task_runner.h"
+ #include "base/types/expected.h"
+ 
+ /* During build, the .pb.h is generated from the .proto at
+ https://github.com/tensorflow/tflite-support/blob/master/tensorflow_lite_support/cc/task/processor/proto/embedding.proto */
+ #include "tensorflow_lite_support/cc/task/processor/proto/embedding.pb.h"
+ 
+ /* from https://source.chromium.org/chromium/chromium/src/+/main:third_party/ */
+ #include "third_party/abseil-cpp/absl/status/status.h"
+ #include "third_party/tflite_support/src/tensorflow_lite_support/cc/task/text/text_embedder.h"
+ 
+ namespace tflite {
+ namespace task {
+ namespace text {
+ class TextEmbedder;
+ }  // namespace text
+ }  // namespace task
+ }  // namespace tflite
+ 
+ namespace base {
+ class SequencedTaskRunner;
+ }  // namespace base
+ 
+ namespace ai_chat {
+ // TextEmbedder is a wrapper class for tflite::task::text::TextEmbedder.and
+ // runs all the operations on a separate sequence task runner to avoid blocking
+ // owner sequence runner ex. brwoser UI thread.
+ class TextEmbedder {
+     public:
+         static std::unique_ptr<TextEmbedder, base::OnTaskRunnerDeleter> Create(
+             const base::FilePath& model_path,
+             scoped_refptr<base::SequencedTaskRunner> embedder_task_runner);
+ 
+         virtual ~TextEmbedder();
+         TextEmbedder(const TextEmbedder&) = delete;
+         TextEmbedder& operator=(const TextEmbedder&) = delete;
+ 
+         virtual bool IsInitialized() const;
+ 
+         // Initialize tflite::task::text::TextEmbedder with the model file.
+         // Since tflite on Windows doesn't support file path loading so we read the
+         // model file and pass it through file content.
+         using InitializeCallback = base::OnceCallback<void(bool)>;
+         virtual void Initialize(InitializeCallback callback);
+ 
+         '''
+         // Declare the functions for 'suggest tabs for group' feature here //
+         '''
+ 
+         // Cancel all the pending tflite tasks on the embedder task runner.
+         // Should be used right before the TextEmbedder is destroyed to avoid long
+         // running tflite tasks blocking shutdown.
+         void CancelAllTasks();
+ 
+     protected:
+         explicit TextEmbedder(
+             const base::FilePath& model_path,
+             scoped_refptr<base::SequencedTaskRunner> embedder_task_runner);
+             
+     private:
+         friend class TextEmbedderUnitTest;
+ 
+         void InitializeEmbedder(base::OnceCallback<void(bool)> callback);
+ 
+         absl::Status EmbedText(const std::string& text,
+             tflite::task::processor::EmbeddingResult& embedding);
+         
+         '''
+         // Declare the functions or variables for 'suggest tabs for group' feature here //
+         '''
+         // an array containing strings (tab-title + origin)
+         // should we have a limit on the length of this array?
+         std::vector<std::string> tabs_; 
+ 
+         std::vector<tflite::task::processor::EmbeddingResult> embeddings_;
+ 
+         // Lock used to ensure mutual exclusive access to |tflite_text_embedder_|
+         // when setting unique_ptr and accessing it from |owner_task_runner_|.
+         mutable base::Lock lock_;
+         std::unique_ptr<tflite::task::text::TextEmbedder> tflite_text_embedder_;
+ 
+         const base::FilePath model_path_;
+         scoped_refptr<base::SequencedTaskRunner> owner_task_runner_;
+         scoped_refptr<base::SequencedTaskRunner> embedder_task_runner_;
+ 
+         base::WeakPtrFactory<TextEmbedder> weak_ptr_factory_{this};
+ };
+ }  // namespace ai_chat
+ #endif  // BRAVE_COMPONENTS_AI_CHAT_CORE_BROWSER_TEXT_EMBEDDER_H_
---
+++ components/ai_chat/core/browser/text_embedder_unittest.cc
@@ -0,0 +1,87 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this file,
+* You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "brave/components/ai_chat/core/browser/text_embedder.h"
+
+#include <string.h>
+#include <memory>
+#include <string_view>
+#include <utility>
+
+#include "base/compiler_specific.h"
+#include "base/files/file_path.h"
+#include "base/location.h"
+#include "base/path_service.h"
+#include "base/run_loop.h"
+#include "base/strings/string_number_conversions.h"
+#include "base/task/sequenced_task_runner.h"
+#include "base/task/task_traits.h"
+#include "base/task/thread_pool.h"
+#include "base/test/bind.h"
+#include "base/test/task_environment.h"
+#include "brave/components/ai_chat/core/browser/local_models_updater.h"
+#include "brave/components/constants/brave_paths.h"
+#include "build/build_config.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+TEMP_MODEL_PATH = '/Users/vandanamalayil/Downloads/universal_sentence_encoder_qa_with_metadata.tflite'
+
+namespace ai_chat {
+
+class TextEmbedderUnitTest : public testing::Test {
+    public:
+        TextEmbedderUnitTest()
+            : embedder_(nullptr, base::OnTaskRunnerDeleter(nullptr)) {}
+
+        ~TextEmbedderUnitTest() override = default;
+
+        // prepare test env before each test run involving TextEmbedderUnitTest
+        void SetUp() override {
+            // task runner where TFLite embedding work will run
+            embedder_task_runner_ = base::ThreadPool::CreateSequencedTaskRunner(
+                {base::MayBlock(), base::TaskPriority::BEST_EFFORT});
+            
+            // fetch path to test data dir.
+            base::FilePath test_dir =
+                base::PathService::CheckedGet(brave::DIR_TEST_DATA);
+
+            // constructs sub path like ../leo/local-models-updater
+            model_dir_ =
+                test_dir.AppendASCII("leo").AppendASCII("local-models-updater");
+
+            // create text embedder using the given model file. also passes test runner so it can work off thread.
+            // kUniversalQAModelName is defined in local_models_updater.h file. 
+            embedder_ = TextEmbedder::Create(
+                base::FilePath(TEMP_MODEL_PATH),
+                // base::FilePath(model_dir_.AppendASCII(kUniversalQAModelName)),
+                embedder_task_runner_);
+            
+            // if embedder creation fails, test should fail
+            ASSERT_TRUE(embedder_);
+
+            base::RunLoop run_loop;
+            embedder_->Initialize(
+                base::BindLambdaForTesting([&run_loop](bool initialized) {
+                ASSERT_TRUE(initialized);
+                run_loop.Quit();
+            }));
+
+            run_loop.Run();
+            ASSERT_TRUE(embedder_->IsInitialized());
+        }
+    };
+
+
+    TEST_F(TextEmbedderUnitTest, Create) {
+        EXPECT_FALSE(TextEmbedder::Create(base::FilePath(), embedder_task_runner_));
+        // Invalid model path is tested in TextEmbedderUnitTest.Initialize.
+        EXPECT_TRUE(TextEmbedder::Create(
+            base::FilePath(TEMP_MODEL_PATH),
+            // base::FilePath(model_dir_.AppendASCII("model.tflite")),
+            embedder_task_runner_));
+      }
+
+
+
