9d8c8d2a93050e2fe1f8b8d4af93bcd26a7f275d
Claudio DeSouza <cdesouza@brave.com>
2025-08-08 22:29:10 +0100

    [cr141] IWYU fixes

--- components/brave_wallet/browser/keyring_service_unittest.cc
+++ components/brave_wallet/browser/keyring_service_unittest.cc
@@ -13,6 +13,7 @@
 
 #include "base/base64.h"
 #include "base/check.h"
+#include "base/command_line.h"
 #include "base/containers/span.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
