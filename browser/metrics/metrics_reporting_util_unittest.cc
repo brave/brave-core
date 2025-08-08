5b5054c9fac4c5fc44ba76e00f94cb727043653e
Claudio DeSouza <cdesouza@brave.com>
2025-08-08 22:23:47 +0100

    [cr141] `ScopedTestingLocalState` deleted
    The local state is always valid, and should be retrieved from
    `TestingBrowserProcess::GetGlobal()->GetTestingLocalState()`.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/68994f1d5aaecfdb064cdb62384538c16a5c7472
    
    commit 68994f1d5aaecfdb064cdb62384538c16a5c7472
    Author: Jun Ishiguro <junis@google.com>
    Date:   Wed Aug 6 22:16:21 2025 -0700
    
        Remove ScopedTestingLocalState usage (part 6)
    
        This CL removes usages of ScopedTestingLocalState, which has been
        deprecated.
    
        This CL was made by Gemini CLI.
    
        Bug: 422039036
        Change-Id: I143e5bff8b52c15fb530e1c9719a05eb08758df2
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6805174
        Commit-Queue: Jun Ishiguro <junis@google.com>
        Reviewed-by: Georg Neis <neis@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1498019}

--- browser/metrics/metrics_reporting_util_unittest.cc
+++ browser/metrics/metrics_reporting_util_unittest.cc
@@ -7,7 +7,6 @@
 
 #include "brave/browser/metrics/buildflags/buildflags.h"
 #include "brave/browser/metrics/metrics_reporting_util.h"
-#include "chrome/test/base/scoped_testing_local_state.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "components/metrics/metrics_pref_names.h"
 #include "components/prefs/pref_service.h"
@@ -15,10 +14,6 @@
 
 TEST(MetricsUtilTest, CrashReportPermissionAskDialogTest) {
 #if BUILDFLAG(ENABLE_CRASH_DIALOG)
-  std::unique_ptr<ScopedTestingLocalState> local_state =
-      std::make_unique<ScopedTestingLocalState>(
-          TestingBrowserProcess::GetGlobal());
-
   g_browser_process->local_state()->SetBoolean(
       metrics::prefs::kMetricsReportingEnabled, false);
   EXPECT_TRUE(ShouldShowCrashReportPermissionAskDialog());
