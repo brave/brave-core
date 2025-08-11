d991994aba7190465132930cce4f12524b3abced
zenparsing <zenparsing@gmail.com>
2025-08-11 13:53:11 -0400

    [Rewards] Remove unused Rewards UI on Android

--- android/java/org/chromium/chrome/browser/app/BraveActivity.java
+++ android/java/org/chromium/chrome/browser/app/BraveActivity.java
@@ -2092,11 +2092,7 @@ public abstract class BraveActivity extends ChromeActivity
     }
 
     public void showRewardsPage() {
-        if (BraveRewardsHelper.shouldShowNewRewardsUI()) {
-            getBraveToolbarLayout().showRewardsPage();
-        } else {
-            openNewOrSelectExistingTab(BRAVE_REWARDS_SETTINGS_URL);
-        }
+        getBraveToolbarLayout().showRewardsPage();
     }
 
     public static ChromeTabbedActivity getChromeTabbedActivity() {
@@ -2152,17 +2148,12 @@ public abstract class BraveActivity extends ChromeActivity
                 && (requestCode == BraveConstants.VERIFY_WALLET_ACTIVITY_REQUEST_CODE
                         || requestCode == BraveConstants.USER_WALLET_ACTIVITY_REQUEST_CODE
                         || requestCode == BraveConstants.SITE_BANNER_REQUEST_CODE)) {
-            dismissRewardsPanel();
             if (data != null) {
                 String open_url = data.getStringExtra(BraveActivity.OPEN_URL);
                 if (!TextUtils.isEmpty(open_url)) {
                     openNewOrSelectExistingTab(open_url);
                 }
             }
-        } else if (resultCode == RESULT_OK
-                && requestCode == BraveConstants.MONTHLY_CONTRIBUTION_REQUEST_CODE) {
-            dismissRewardsPanel();
-
         } else if (resultCode == RESULT_OK
                 && requestCode == BraveConstants.DEFAULT_BROWSER_ROLE_REQUEST_CODE) {
             // We don't need to anything with the result here.
