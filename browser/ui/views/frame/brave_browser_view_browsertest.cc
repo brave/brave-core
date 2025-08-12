0e19a2c07d762788a702fcdb88adac032a019c30
Simon Hong <shong@brave.com>
2025-08-12 14:49:04 +0900

    [cr140] Fixed BraveBrowserViewLayout regressions
    Addressed upstream's BrowserViewLayout changes.
    Set correct vertical tab insets for bookmark/infobar/contents views
    based on bookmarks bar show state.
    
    TEST=BraveBrowserViewTest.LayoutWithVerticalTabTest
    
    Chromium changes:
        https://chromium.googlesource.com/chromium/src/+/71c4bc6605ccca8cdfa384db489bdc4df23ca84d
    
        commit 71c4bc6605ccca8cdfa384db489bdc4df23ca84d
        Author: dljames <dljames@chromium.org>
        Date:   Mon Jul 21 10:06:20 2025 -0700
    
            Replace int with gfx::Rect to cache view placement in BrowserViewLayout
    
            This change replaces the int parameter and return value that was used by
            various layout functions to keep track of where the next view should be
            placed with a gfx::Rect which gives us the available bounds the next
            view can utilize to position itself.
    
            This is intended to be a pure refactor with no changes to visuals or
            behavior.
    
            Change-Id: Icbeef6b07012194dfa16153c6839d973b7760ebe
            Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6767093
            Commit-Queue: Darryl James <dljames@chromium.org>
            Reviewed-by: David Pennington <dpenning@chromium.org>
            Cr-Commit-Position: refs/heads/main@{#1489610}

--- browser/ui/views/frame/brave_browser_view_browsertest.cc
+++ browser/ui/views/frame/brave_browser_view_browsertest.cc
@@ -7,13 +7,23 @@
 
 #include <string>
 
+#include "base/test/run_until.h"
+#include "brave/browser/ui/bookmark/bookmark_helper.h"
+#include "brave/browser/ui/browser_commands.h"
+#include "brave/browser/ui/views/frame/brave_browser_view.h"
 #include "build/build_config.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser_commands.h"
+#include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/browser/ui/tab_modal_confirm_dialog.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/tabs/tab_strip_user_gesture_details.h"
 #include "chrome/browser/ui/test/test_browser_ui.h"
+#include "chrome/browser/ui/views/bookmarks/bookmark_bar_view.h"
+#include "chrome/browser/ui/views/frame/browser_non_client_frame_view.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/scrim_view.h"
+#include "chrome/browser/ui/views/infobars/infobar_container_view.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
@@ -46,10 +56,115 @@ class BraveBrowserViewTest : public InProcessBrowserTest {
   BraveBrowserViewTest& operator=(const BraveBrowserViewTest&) = delete;
 
  protected:
+  void ToggleVerticalTabStrip() {
+    brave::ToggleVerticalTabStrip(browser());
+    browser_non_client_frame_view()->DeprecatedLayoutImmediately();
+  }
+
+  BrowserNonClientFrameView* browser_non_client_frame_view() {
+    return browser_view()->frame()->GetFrameView();
+  }
+
+  BraveBrowserView* brave_browser_view() {
+    return BraveBrowserView::From(browser_view());
+  }
+
   BrowserView* browser_view() {
     return BrowserView::GetBrowserViewForBrowser(browser());
   }
+
+  views::View* vertical_tab_strip_host_view() {
+    return brave_browser_view()->vertical_tab_strip_host_view_;
+  }
+
+  views::View* contents_container() {
+    return browser_view()->GetContentsContainerForLayoutManager();
+  }
+
+  views::View* infobar_container() {
+    return browser_view()->infobar_container();
+  }
+
+  BookmarkBarView* bookmark_bar() { return browser_view()->bookmark_bar(); }
 };
+
+// Tests bookmark/infobar/contents container layout with vertical tab.
+IN_PROC_BROWSER_TEST_F(BraveBrowserViewTest, LayoutWithVerticalTabTest) {
+  ToggleVerticalTabStrip();
+
+  auto* prefs = browser()->profile()->GetPrefs();
+
+  // Check bookmark only on the NTP is default.
+  EXPECT_EQ(brave::BookmarkBarState::kNtp, brave::GetBookmarkBarState(prefs));
+
+  // BookmarkBar not visible as current active tab is not NTP.
+  EXPECT_FALSE(bookmark_bar()->GetVisible());
+
+  // Infobar is visible at first run.
+  // Update this test if it's not visible at first run.
+  EXPECT_TRUE(infobar_container()->GetVisible());
+  EXPECT_EQ(infobar_container()->bounds().bottom_left(),
+            contents_container()->bounds().origin());
+
+  // Bookmark bar should be visible with NTP.
+  chrome::AddTabAt(browser(), GURL(), -1, true);
+  EXPECT_TRUE(bookmark_bar()->GetVisible());
+  EXPECT_FALSE(infobar_container()->GetVisible());
+  ASSERT_TRUE(base::test::RunUntil(
+      [&]() { return !browser()->window()->IsBookmarkBarAnimating(); }));
+
+  // Check bookmark bar/contents container position.
+  EXPECT_EQ(vertical_tab_strip_host_view()->bounds().top_right(),
+            bookmark_bar()->bounds().origin());
+  EXPECT_EQ(bookmark_bar()->bounds().bottom_left() +
+                gfx::Vector2d(0, /*contents separator*/ 1),
+            contents_container()->bounds().origin());
+
+  // Hide bookmark bar always.
+  // Check contents container is positioned right after the vertical tab.
+  brave::SetBookmarkState(brave::BookmarkBarState::kNever, prefs);
+  EXPECT_EQ(brave::BookmarkBarState::kNever, brave::GetBookmarkBarState(prefs));
+  ASSERT_TRUE(base::test::RunUntil(
+      [&]() { return !browser()->window()->IsBookmarkBarAnimating(); }));
+  EXPECT_FALSE(bookmark_bar()->GetVisible());
+  EXPECT_EQ(vertical_tab_strip_host_view()->bounds().top_right(),
+            contents_container()->bounds().origin());
+
+  // Activate non-NTP tab and check contents container is positioned below the
+  // infobar.
+  browser()->tab_strip_model()->ActivateTabAt(0);
+  EXPECT_TRUE(infobar_container()->GetVisible());
+  EXPECT_EQ(infobar_container()->bounds().bottom_left(),
+            contents_container()->bounds().origin());
+
+  // Show bookmark bar always.
+  // Check vertical tab is positioned below the bookmark bar.
+  // Check contents container is positioned below the info bar.
+  brave::SetBookmarkState(brave::BookmarkBarState::kAlways, prefs);
+  EXPECT_EQ(brave::BookmarkBarState::kAlways,
+            brave::GetBookmarkBarState(prefs));
+  EXPECT_TRUE(infobar_container()->GetVisible());
+  ASSERT_TRUE(base::test::RunUntil(
+      [&]() { return !browser()->window()->IsBookmarkBarAnimating(); }));
+  EXPECT_TRUE(bookmark_bar()->GetVisible());
+  EXPECT_EQ(vertical_tab_strip_host_view()->bounds().origin(),
+            bookmark_bar()->bounds().bottom_left() +
+                gfx::Vector2d(0, /*contents separator*/ 1));
+  EXPECT_EQ(infobar_container()->bounds().bottom_left(),
+            contents_container()->bounds().origin());
+
+  // Activate NTP tab.
+  // Check vertical tab is positioned below the bookmark bar.
+  // Check contents container is positioned right after the vertical tab.
+  browser()->tab_strip_model()->ActivateTabAt(1);
+  EXPECT_FALSE(infobar_container()->GetVisible());
+  EXPECT_TRUE(bookmark_bar()->GetVisible());
+  EXPECT_EQ(vertical_tab_strip_host_view()->bounds().origin(),
+            bookmark_bar()->bounds().bottom_left() +
+                gfx::Vector2d(0, /*contents separator*/ 1));
+  EXPECT_EQ(vertical_tab_strip_host_view()->bounds().top_right(),
+            contents_container()->bounds().origin());
+}
 
 // Tests that a content area scrim is still disabled when a tab modal dialog is
 // active.
