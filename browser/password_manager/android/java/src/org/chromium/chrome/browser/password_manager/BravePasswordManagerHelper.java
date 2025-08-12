a39aabc516c521e7f74efa47cc703f75aee21842
AlexeyBarabash <AlexeyBarabash@users.noreply.github.com>
2025-08-12 19:41:16 +0300

    [cr140][Android] Backport built-in Password Manager (#30433)
    Starting from cr140 Password manager was removed from Android.
    
    This commit switches Android to use the same PasswordManagerSettingsService
    as Desktop does.
    Also backports upstream's Password Manager classes and resources used at cr139.
    
    Chromium changes:
    
    https://source.chromium.org/chromium/chromium/src/+/7511b67d9208bd10463c1e5752976346ab6bd207
    	[LoginDbDeprecationAndroid] Remove last usages and delete the flag
    
    https://source.chromium.org/chromium/chromium/src/+/5794e9a0461497b7b810e9366c1c53eab711b6f2
    	[passwords/android] Limit kPasswordsUseUPMLocalAndSeparateStores access
    
    
    https://source.chromium.org/chromium/chromium/src/+/6875d2ce472e7f2097617525feec45313602e225
    	[passwords/android] Remove unused password checkup UI
    
    https://source.chromium.org/chromium/chromium/src/+/3662471ee9fabd6d1777b1d5316f0b9eede0f115
    	[passwords/android] Remove unused PasswordSettings.java
    	
    Or see the log for `chrome/browser/password_manager/android/` directory
    https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/password_manager/android/;bpv=1

---
+++ browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/BravePasswordManagerHelper.java
@@ -0,0 +1,66 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package org.chromium.chrome.browser.password_manager;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import org.chromium.base.Log;
+import org.chromium.base.supplier.Supplier;
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
+import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.profiles.ProfileKeyedMap;
+import org.chromium.chrome.browser.settings.SettingsNavigationFactory;
+import org.chromium.components.browser_ui.settings.SettingsCustomTabLauncher;
+import org.chromium.ui.modaldialog.ModalDialogManager;
+
+@NullMarked
+public class BravePasswordManagerHelper extends PasswordManagerHelper {
+    private static final String TAG = "BravePasswords";
+    protected static @Nullable ProfileKeyedMap<PasswordManagerHelper> sProfileMap;
+
+    BravePasswordManagerHelper(Profile profile) {
+        super(profile);
+    }
+
+    @Override
+    public void showPasswordSettings(
+            Context context,
+            @ManagePasswordsReferrer int referrer,
+            Supplier<ModalDialogManager> modalDialogManagerSupplier,
+            boolean managePasskeys,
+            @Nullable String account,
+            SettingsCustomTabLauncher settingsCustomTabLauncher) {
+        Bundle fragmentArgs = new Bundle();
+        fragmentArgs.putInt(MANAGE_PASSWORDS_REFERRER, referrer);
+
+        // PasswordSettings is nota available at current package.
+        // Upstream used to resolve it through SettingsNavigation.SettingsFragment.PASSWORDS
+        // enum. To avoid use patching, just use reflection, luckily we have
+        // settingsNavigation.createSettingsIntent which can accept Class argument.
+        try {
+            Class passwordSettingsClass =
+                    Class.forName(
+                            "org.chromium.chrome.browser.password_manager.settings.PasswordSettings"); // presubmit: ignore-long-line
+            context.startActivity(
+                    SettingsNavigationFactory.createSettingsNavigation()
+                            .createSettingsIntent(context, passwordSettingsClass, fragmentArgs));
+        } catch (Exception e) {
+            Log.e(TAG, "showPasswordSettings: ", e);
+        }
+    }
+
+    public static PasswordManagerHelper getForProfile(Profile profile) {
+        if (sProfileMap == null) {
+            sProfileMap =
+                    new ProfileKeyedMap<>(
+                            ProfileKeyedMap.ProfileSelection.REDIRECTED_TO_ORIGINAL,
+                            ProfileKeyedMap.NO_REQUIRED_CLEANUP_ACTION);
+        }
+        return sProfileMap.getForProfile(profile, BravePasswordManagerHelper::new);
+    }
+}
