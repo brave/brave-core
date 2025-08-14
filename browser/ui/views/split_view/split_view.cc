1d78a9b9c62ca862295d076ae4ec9054cb29c968
Claudio DeSouza <cdesouza@brave.com>
2025-08-14 18:22:55 +0100

    [cr141] `ContentsLayoutManager` dropped `watermark_view`
    This affects our override and instantiations for it.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/4c3190b8910c2ce646f11b07f1e931f28d0be012
    
    commit 4c3190b8910c2ce646f11b07f1e931f28d0be012
    Author: Eshwar Stalin <estalin@chromium.org>
    Date:   Tue Aug 12 20:08:07 2025 -0700
    
        [SxS] Move watermark view to contents container view.
    
        This change allows watermarks to be applied to both contents in a split
        view. The change introduces a browser window feature which applies
        watermark based to individual watermark views based on their associated
        web contents which is tracked by the tab feature.
    
        BYPASS_LARGE_CHANGE_WARNING
    
        Bug: 406793514
        Change-Id: Ic33467dce91d52f66a23a355e81f6874887950a0
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6788777
        Reviewed-by: Caroline Rising <corising@chromium.org>
        Commit-Queue: Eshwar Stalin <estalin@chromium.org>
        Reviewed-by: David Pennington <dpenning@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1500571}

--- browser/ui/views/split_view/split_view.cc
+++ browser/ui/views/split_view/split_view.cc
@@ -125,8 +125,7 @@ SplitView::SplitView(Browser& browser,
   secondary_contents_container_->SetLayoutManager(
       std::make_unique<BraveContentsLayoutManager>(
           secondary_contents_web_view_, secondary_lens_overlay_view_,
-          /*watermark_view=*/nullptr, secondary_reader_mode_toolbar_,
-          secondary_contents_scrim_view_));
+          secondary_reader_mode_toolbar_, secondary_contents_scrim_view_));
 #endif
 
   SetLayoutManager(std::make_unique<SplitViewLayoutManager>(
