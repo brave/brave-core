c6bbcbd31d2b2578695ac2e27462a44d87e101bc
Artem Samoilenko <artem@brave.com>
2025-08-18 16:47:24 -0400

    [cr141][Android] Update buttons on hub toolbar pane switching
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/372d5712f98aea3477c209ccef629e33358c3a35
    
    commit 372d5712f98aea3477c209ccef629e33358c3a35
    Author: Jingping Sun <jings@microsoft.com>
    Date:   Mon Jul 14 20:07:00 2025 -0700
    
        Reland "[Hub Bottombar]Separate the Hub action button to its own view component"
    
        This is a reland of commit c7df3539770ff84f4b14e939b52045b63eaeecc5
    
        Original change's description:
        > [Hub Bottombar]Separate the Hub action button to its own view component
        >
        > Separate the action button from HubToolbarView so that it can be used
        > more flexibly.
        >
        > GTSU enabled action button:
        > https://drive.google.com/file/d/1N3qom-CJLKXZ0lTeqEzm__21EjeNcPJb/view?usp=sharing
        > GTUS disabled action button:
        > https://drive.google.com/file/d/1prSIG0RjZWXMO52GTdaStbRAwznU5KEh/view?usp=sharing
        >
        > Bug: 430332944
        > Change-Id: I3e4081cb595e1c45f2ece4a1aac69a664d169c11
        > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6717062
        > Reviewed-by: Sky Malice <skym@chromium.org>
        > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
        > Reviewed-by: Madhav Pruthi <madhavpruthi@google.com>
        > Commit-Queue: Jingping Sun <jings@microsoft.com>
        > Cr-Commit-Position: refs/heads/main@{#1484424}
    
        Bug: 430332944
        Change-Id: Ic519071af04318d8bfe4420fcac7384662bf7978
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6719110
        Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
        Reviewed-by: Brandon Fong <bjfong@google.com>
        Commit-Queue: Jingping Sun <jings@microsoft.com>
        Cr-Commit-Position: refs/heads/main@{#1486712}

--- browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/BraveHubToolbarView.java
+++ browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/BraveHubToolbarView.java
@@ -38,6 +38,14 @@ public class BraveHubToolbarView extends HubToolbarView {
     }
 
     @Override
+    void setPaneSwitcherIndex(int index) {
+        super.setPaneSwitcherIndex(index);
+
+        // Update visibility of action and menu buttons based on the switching panes.
+        updateButtonsVisibility();
+    }
+
+    @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         Context context = getContext();
