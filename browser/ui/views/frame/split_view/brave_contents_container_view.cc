b77b1562eca1518faca20206a918076c553b4387
Claudio DeSouza <cdesouza@brave.com>
2025-08-18 16:07:58 +0100

    [cr141] Accessors for `ContentsContainerView` names corrected
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/8f9beac773d685eb957b36436a75297c3c652367
    
    commit 8f9beac773d685eb957b36436a75297c3c652367
    Author: Eshwar Stalin <estalin@chromium.org>
    Date:   Sun Aug 17 08:52:26 2025 -0700
    
        [SxS] Renaming accessors to conform to naming convention
    
        Change-Id: I784fd963d106646b4f39fc8539515c3febc9d8d4
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6855254
        Commit-Queue: Eshwar Stalin <estalin@chromium.org>
        Reviewed-by: Avi Drissman <avi@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1502447}

--- browser/ui/views/frame/split_view/brave_contents_container_view.cc
+++ browser/ui/views/frame/split_view/brave_contents_container_view.cc
@@ -67,10 +67,10 @@ void BraveContentsContainerView::UpdateBorderAndOverlay(bool is_in_split,
   ContentsContainerView::UpdateBorderAndOverlay(is_in_split, is_active,
                                                 show_scrim);
   // We don't show scrim view always.
-  GetInactiveSplitScrimView()->SetVisible(false);
+  inactive_split_scrim_view()->SetVisible(false);
 
   gfx::RoundedCornersF contents_corner_radius(GetCornerRadius(false));
-  auto* contents_web_view = GetContentsView();
+  auto* contents_web_view = contents_view();
   contents_web_view->layer()->SetRoundedCornerRadius(contents_corner_radius);
   if (contents_web_view->holder()->native_view()) {
     contents_web_view->holder()->SetCornerRadii(contents_corner_radius);
