d062603e9fb496745ab447d43529402be1fdbb67
Arthur Edelstein <aedelstein@brave.com>
2025-08-19 10:43:32 -0700

    remove unused args

--- build/commands/lib/config.js
+++ build/commands/lib/config.js
@@ -265,7 +265,6 @@ const Config = function () {
     'bitflyer_sandbox_client_secret',
     'bitflyer_sandbox_fee_address',
     'bitflyer_sandbox_url',
-    'brave_account_api_endpoint',
     'brave_android_developer_options_code',
     'brave_google_api_endpoint',
     'brave_google_api_key',
@@ -741,7 +740,6 @@ Config.prototype.buildArgs = function () {
     delete args.safebrowsing_api_endpoint
     delete args.safe_browsing_mode
     delete args.enable_hangout_services_extension
-    delete args.brave_account_api_endpoint
     delete args.brave_google_api_endpoint
     delete args.brave_google_api_key
     delete args.brave_stats_updater_url
