dff110a652c84656f6f61e0020868da2e255997d
samartnik <30602739+samartnik@users.noreply.github.com>
2025-08-19 15:41:52 -0400

    [Android] Adds flag to enable dynamic colors (#30666)
    * [Android] Adds flag to enable dynamic colors
    
    * Fixes for browser tests

--- android/java/org/chromium/chrome/browser/util/BraveDynamicColors.java
+++ android/java/org/chromium/chrome/browser/util/BraveDynamicColors.java
@@ -7,15 +7,32 @@ package org.chromium.chrome.browser.util;
 
 import android.app.Activity;
 
+import com.google.android.material.color.DynamicColors;
 import com.google.android.material.color.DynamicColorsOptions;
+
+import org.chromium.base.BraveFeatureList;
+import org.chromium.base.FeatureList;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 
 public class BraveDynamicColors {
     public static void applyToActivityIfAvailable(Activity activity) {
-        // We disable dynamic colors as it causes styling issues with Brave theme.
+        if (!FeatureList.isNativeInitialized()
+                || !ChromeFeatureList.isEnabled(BraveFeatureList.BRAVE_ANDROID_DYNAMIC_COLORS)) {
+            // We disable dynamic colors as it causes styling issues with Brave theme.
+            return;
+        }
+
+        DynamicColors.applyToActivityIfAvailable(activity);
     }
 
     public static void applyToActivityIfAvailable(
             Activity activity, DynamicColorsOptions dynamicColorsOptions) {
-        // We disable dynamic colors as it causes styling issues with Brave theme.
+        if (!FeatureList.isNativeInitialized()
+                || !ChromeFeatureList.isEnabled(BraveFeatureList.BRAVE_ANDROID_DYNAMIC_COLORS)) {
+            // We disable dynamic colors as it causes styling issues with Brave theme.
+            return;
+        }
+
+        DynamicColors.applyToActivityIfAvailable(activity, dynamicColorsOptions);
     }
 }
