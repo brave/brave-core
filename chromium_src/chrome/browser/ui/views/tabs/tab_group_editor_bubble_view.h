792b6ab518ce4e4d229d445d7deee8a06efd418a
Anthony Tseng <darkdh@gmail.com>
2025-08-19 12:59:45 -0700

    Implement SuggestedTabsPressed

--- chromium_src/chrome/browser/ui/views/tabs/tab_group_editor_bubble_view.h
+++ chromium_src/chrome/browser/ui/views/tabs/tab_group_editor_bubble_view.h
@@ -6,6 +6,10 @@
 #ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_TABS_TAB_GROUP_EDITOR_BUBBLE_VIEW_H_
 #define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_TABS_TAB_GROUP_EDITOR_BUBBLE_VIEW_H_
 
+// Forward declarations for TextEmbedder functionality
+#include "base/task/sequenced_task_runner.h"
+#include "brave/components/local_ai/browser/text_embedder.h"
+
 // Inject our new member methods into the upstream class
 #define RebuildMenuContents                                             \
   RebuildMenuContentsUnsed();                                           \
@@ -16,6 +20,19 @@
   void MaybeAddSuggestedTabsButton();                                   \
                                                                         \
  private:                                                               \
+  /* AI-powered tab suggestion functionality */                         \
+  void OnTextEmbedderInitialized(bool initialized);                     \
+  void ProcessTabSuggestion();                                          \
+  void OnTabSuggestionResult(absl::StatusOr<std::vector<int>> result);  \
+  void CleanupTextEmbedder();                                           \
+  void CleanupAndClose();                                               \
+                                                                        \
+  /* Member variables for TextEmbedder functionality */                 \
+  std::unique_ptr<local_ai::TextEmbedder, base::OnTaskRunnerDeleter>    \
+      text_embedder_{nullptr, base::OnTaskRunnerDeleter(nullptr)};      \
+  scoped_refptr<base::SequencedTaskRunner> embedder_task_runner_;       \
+  bool suggestion_in_progress_ = false;                                 \
+                                                                        \
   void RebuildMenuContents
 
 #define TAB_GROUP_HEADER_CXMENU_RECENT_ACTIVITY    \
