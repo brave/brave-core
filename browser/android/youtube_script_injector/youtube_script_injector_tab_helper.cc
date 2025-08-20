3cf38a3ca3ce6502a57626f17de4daea0ecac391
Simone Arpe <simon.arpe@gmail.com>
2025-08-20 21:12:11 +0200

    Update youtube_script_injector_tab_helper.cc

--- browser/android/youtube_script_injector/youtube_script_injector_tab_helper.cc
+++ browser/android/youtube_script_injector/youtube_script_injector_tab_helper.cc
@@ -10,7 +10,6 @@
 
 #include "base/feature_list.h"
 #include "base/supports_user_data.h"
-#include "brave/browser/android/youtube_script_injector/brave_youtube_script_injector_native_helper.h"
 #include "brave/browser/android/youtube_script_injector/features.h"
 #include "brave/components/brave_shields/content/browser/brave_shields_util.h"
 #include "brave/components/constants/pref_names.h"
