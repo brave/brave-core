42c47b3c0290bf503823e3ad198972f250c22abb
bridiver <34129+bridiver@users.noreply.github.com>
2025-08-20 08:44:16 -0700

    don't fail if time is 0

--- script/json2xunit.py
+++ script/json2xunit.py
@@ -64,9 +64,10 @@ def transform(input_json):
         iteration = pick_iteration(test_case, iterations)
 
         output[test_suite]['test_count'] += 1
+        elapsed_time_ms = int(iteration["elapsed_time_ms"])
         output[test_suite]['xml'] += (
             f'<testcase name="{test_case}" '
-            f'time="{int(iteration["elapsed_time_ms"])/100.0}">')
+            f'time="{0 if elapsed_time_ms == 0 else elapsed_time_ms/100.0}">')
         if iteration['status'] == 'SUCCESS':
             if iteration['output_snippet']:
                 sanitized_output = ''.join(
