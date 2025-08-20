b502b7a5e9a27fda6199e7002dfe9623e27be77c
Artem Samoilenko <artem@brave.com>
2025-08-20 09:10:31 -0400

    [cr141][Android] New param for StatusBarColorController c-tor
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/12719a7f7cc937c06c038d49d9a1b53fd9cd1353
    
    commit 12719a7f7cc937c06c038d49d9a1b53fd9cd1353
    Author: Xi Han <hanxi@google.com>
    Date:   Mon Aug 18 15:18:15 2025 -0700
    
        [Theme] Update status bar and toolbar color with NTP's background color.
    
        In this CL, Status bar will get NTP's background color via function
        NtpCustomizationConfigManager#getBackgroundColor(). Status bar will
        listen to the background change, and update itself when the color
        changes.
    
        Demo: http://shortn/_LdsF9GhqbP.
    
        Bug: 432527690
        Change-Id: Ia470fcef8c4a771f08cc35bc6921c80c9ae45889
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6858391
        Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
        Commit-Queue: Xi Han <hanxi@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1502992}

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -1894,8 +1894,8 @@ public class BytecodeTest {
 
         Assert.assertTrue(
                 constructorsMatch(
-                        "org/chromium/chrome/browser/ui/system/StatusBarColorController",
-                        "org/chromium/chrome/browser/ui/system/BraveStatusBarColorController",
+                        "org/chromium/chrome/browser/ui/system/StatusBarColorController", // presubmit: ignore-long-line
+                        "org/chromium/chrome/browser/ui/system/BraveStatusBarColorController", // presubmit: ignore-long-line
                         Window.class,
                         boolean.class,
                         Context.class,
@@ -1906,7 +1906,8 @@ public class BytecodeTest {
                         TopUiThemeColorProvider.class,
                         EdgeToEdgeSystemBarColorHelper.class,
                         OneshotSupplier.class,
-                        ObservableSupplier.class));
+                        ObservableSupplier.class,
+                        boolean.class));
         Assert.assertTrue(
                 constructorsMatch(
                         "org/chromium/chrome/browser/browsing_data/ClearBrowsingDataFragment", // presubmit: ignore-long-line
