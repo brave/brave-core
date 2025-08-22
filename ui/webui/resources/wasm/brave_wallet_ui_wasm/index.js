b54a729e00358f3a1a59e6b0604563b12d0100e8
Christian Mazakas <christian.mazakas@gmail.com>
2025-08-22 11:00:07 -0700

    add get_pubkey demo

--- ui/webui/resources/wasm/brave_wallet_ui_wasm/index.js
+++ ui/webui/resources/wasm/brave_wallet_ui_wasm/index.js
@@ -1,1 +1,1 @@
-export { rawr } from 'gen/brave/ui/webui/resources/wasm/brave_wallet_ui_wasm/bundler/pkg'
+export { rawr, get_pubkey } from 'gen/brave/ui/webui/resources/wasm/brave_wallet_ui_wasm/bundler/pkg'
--- ui/webui/resources/wasm/brave_wallet_ui_wasm/src/lib.rs
+++ ui/webui/resources/wasm/brave_wallet_ui_wasm/src/lib.rs
@@ -1,6 +1,22 @@
+extern crate schnorrkel;
+
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn rawr() -> String {
     String::from("rawr!!!!!! and also, hello world!")
+}
+
+#[wasm_bindgen]
+pub fn get_pubkey() -> String {
+    let seed = [
+        157, 97, 177, 157, 239, 253, 90, 96, 186, 132, 74, 244, 146, 236, 44, 196, 68, 73, 197,
+        105, 123, 50, 105, 25, 112, 59, 172, 3, 28, 174, 127, 96,
+    ];
+
+    let mini_key = schnorrkel::MiniSecretKey::from_bytes(&seed).unwrap();
+    let keypair = mini_key.expand_to_keypair(schnorrkel::ExpansionMode::Ed25519);
+    let bytes = keypair.public.to_bytes();
+
+    bytes.iter().map(|byte| format!("{byte:02X}")).collect::<String>()
 }
