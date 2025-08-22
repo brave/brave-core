1618172d23593c48bd357020764a7d4841d61c78
Simon Hong <shong@brave.com>
2025-08-22 11:07:18 +0900

    Added test

--- browser/ui/views/frame/brave_browser_view.h
+++ browser/ui/views/frame/brave_browser_view.h
@@ -167,6 +167,8 @@ class BraveBrowserView : public BrowserView,
   FRIEND_TEST_ALL_PREFIXES(VerticalTabStripHideCompletelyTest, GetMinimumWidth);
   FRIEND_TEST_ALL_PREFIXES(SideBySideWithRoundedCornersTest,
                            TabFullscreenStateTest);
+  FRIEND_TEST_ALL_PREFIXES(BraveBrowserViewWithRoundedCordersTest,
+                           ContentsBackgroundEventHandleTest);
 
   static void SetDownloadConfirmReturnForTesting(bool allow);
 
