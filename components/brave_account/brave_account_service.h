66a04bf2ed2d767551e83e80a236626a417a03d9
Szilard Szaloki <szilardszaloki@gmail.com>
2025-08-25 23:39:59 -0600

    Brave Account registration flow (desktop+Android)

--- components/brave_account/brave_account_service.h
+++ components/brave_account/brave_account_service.h
@@ -6,8 +6,17 @@
 #ifndef BRAVE_COMPONENTS_BRAVE_ACCOUNT_BRAVE_ACCOUNT_SERVICE_H_
 #define BRAVE_COMPONENTS_BRAVE_ACCOUNT_BRAVE_ACCOUNT_SERVICE_H_
 
+#include <optional>
+#include <string>
+
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/memory/weak_ptr.h"
+#include "base/types/expected.h"
+#include "brave/components/api_request_helper/api_request_helper.h"
+#include "brave/components/brave_account/endpoints/password_finalize.h"
+#include "brave/components/brave_account/endpoints/password_init.h"
+#include "brave/components/brave_account/mojom/brave_account.mojom.h"
 #include "components/keyed_service/core/keyed_service.h"
 
 class PrefService;
@@ -29,9 +38,33 @@ class BraveAccountService : public KeyedService {
 
   ~BraveAccountService() override;
 
+  void RegisterInitialize(
+      const std::string& email,
+      const std::string& blinded_message,
+      mojom::PageHandler::RegisterInitializeCallback callback);
+
+  void RegisterFinalize(const std::string& encrypted_verification_token,
+                        const std::string& serialized_record,
+                        mojom::PageHandler::RegisterFinalizeCallback callback);
+
  private:
+  void OnRegisterInitialize(
+      mojom::PageHandler::RegisterInitializeCallback callback,
+      int response_code,
+      base::expected<std::optional<endpoints::PasswordInit::Response>,
+                     std::optional<endpoints::PasswordInit::Error>> reply);
+
+  void OnRegisterFinalize(
+      mojom::PageHandler::RegisterFinalizeCallback callback,
+      const std::string& encrypted_verification_token,
+      int response_code,
+      base::expected<std::optional<endpoints::PasswordFinalize::Response>,
+                     std::optional<endpoints::PasswordFinalize::Error>> reply);
+
   const raw_ptr<PrefService> pref_service_;
   scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
+  api_request_helper::APIRequestHelper api_request_helper_;
+  base::WeakPtrFactory<BraveAccountService> weak_factory_{this};
 };
 
 }  // namespace brave_account
