1604b3e233e8bc891223364666f1310325623e0c
Kyle Hickinson <git@kylehickinson.com>
2025-08-27 13:27:35 -0400

    fix cr140 change

--- ios/browser/api/web_view/brave_web_view_configuration.mm
+++ ios/browser/api/web_view/brave_web_view_configuration.mm
@@ -9,6 +9,7 @@
 #include "components/autofill/core/browser/data_manager/personal_data_manager.h"
 #include "components/keyed_service/core/service_access_type.h"
 #include "components/password_manager/core/browser/password_store/password_store_interface.h"
+#include "ios/chrome/browser/affiliations/model/ios_chrome_affiliation_service_factory.h"
 #include "ios/chrome/browser/autofill/model/personal_data_manager_factory.h"
 #include "ios/chrome/browser/passwords/model/ios_chrome_account_password_store_factory.h"
 #include "ios/chrome/browser/shared/model/profile/profile_ios.h"
@@ -31,9 +32,13 @@
     scoped_refptr<password_manager::PasswordStoreInterface> passwordStore =
         IOSChromeAccountPasswordStoreFactory::GetForProfile(
             profile, ServiceAccessType::EXPLICIT_ACCESS);
+    affiliations::AffiliationService* affiliation_service =
+        IOSChromeAffiliationServiceFactory::GetForProfile(profile);
     _autofillDataManager = [[CWVAutofillDataManager alloc]
-        initWithPersonalDataManager:personalDataManager
-                      passwordStore:passwordStore.get()];
+         initWithPersonalDataManager:personalDataManager
+                       passwordStore:passwordStore.get()
+                 affiliationsService:affiliation_service
+        isPasswordAffiliationEnabled:NO];
   }
   return _autofillDataManager;
 }
