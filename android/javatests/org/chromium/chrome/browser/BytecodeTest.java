ac454132ef8a274d11054cb77160684e2192a75a
Artem Samoilenko <artem@brave.com>
2025-08-28 13:35:43 -0400

    [cr141][Android] New params for MenuButtonCoordinator c-tor
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/a897a4e519ebb2a6480cda54f2e42f70dd837ede
    
    commit a897a4e519ebb2a6480cda54f2e42f70dd837ede
    Author: Charles Hager <clhager@google.com>
    Date:   Wed Aug 27 10:31:50 2025 -0700
    
        Have MenuButtonCoordinator extend ToolbarChildButton
    
        Bug: 427260734
        Change-Id: I27c0e074fa78f4f0e91a7300700c3ec3c0ca2282
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6886473
        Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
        Commit-Queue: Charles Hager <clhager@google.com>
        Cr-Commit-Position: refs/heads/main@{#1507190}

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -1440,6 +1440,7 @@ public class BytecodeTest {
                 constructorsMatch(
                         "org/chromium/chrome/browser/toolbar/menu_button/MenuButtonCoordinator", // presubmit: ignore-long-line
                         "org/chromium/chrome/browser/toolbar/menu_button/BraveMenuButtonCoordinator", // presubmit: ignore-long-line
+                        Activity.class,
                         OneshotSupplier.class,
                         BrowserStateBrowserControlsVisibilityDelegate.class,
                         WindowAndroid.class,
@@ -1448,6 +1449,7 @@ public class BytecodeTest {
                         boolean.class,
                         Supplier.class,
                         ThemeColorProvider.class,
+                        IncognitoStateProvider.class,
                         Supplier.class,
                         Runnable.class,
                         int.class,
