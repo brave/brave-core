cabca29947bcb040233655f8a0f6d164080686e3
Gaetano Checinski <gchecinski@brave.com>
2025-08-28 15:19:48 +0100

    improve args handling; add clean command and comments

--- build/commands/scripts/generateCoverageReport.js
+++ build/commands/scripts/generateCoverageReport.js
@@ -1,38 +1,72 @@
-const {glob} =require('fs/promises');
-const {writeJSON} = require('fs-extra')
+const {glob, rm} =require('fs/promises');
+const {writeJSON, mkdirp} = require('fs-extra')
 const utils = require('../lib/util')
 const config = require('../lib/config')
+
 
 module.exports = (program) =>
   program
     .command('coverage_report')
+    .description([
+      'generates a coverage report.',
+      'Requires to build with --is_coverage and run test.', 
+      'HTML report will be in out/$BUILD_DIR/coverage/report/$NAME/dist'
+    ].join('\n'))
     .option('-C [build_dir]', 'build config (out/Debug, out/Release)')
     .option('--build_type [build_type]', 'build with coverage information ')
     .option('--target_arch [target_arch]', 'target architecture')
     .option('--target_os <target_os>', 'target OS')
-    .option('--tests [testSuites...]', 'testsuites to consider')
-    .description('generates a coverage report')
-    .action(async (args) => {
-      if (args.buildConfig) {
-        config.buildConfig = args.buildConfig
-      }
+    .option('--name [name]', 'name of the test report [defaults to coverage]')
+    .option('--clean', 'delete recordins and reports')
+    .option('--tests [testSuites]', 'comma seperated list of testsuites to consider. By default it parses all recordings')
+    .arguments('[build_config]')
+    .action(async (buildConfig, args) => {
+      config.buildConfig = buildConfig || config.buildConfig
       config.update(args)
+      const dirsGlob =  args.tests ? `{${args.tests}}/**` : '**'
 
+      const name = args.name||'coverage';
       const out = `${config.outputDir}/coverage`;
-      const recordings = await Array.fromAsync(glob(`${out}/**/*.profraw`))
+      const reportPath = `${out}/report/${name}`;
+      const profDataPath = `${reportPath}/coverage.profdata`
+      const distPath = `${reportPath}/dist`
 
-      const testSuites = [...new Set(recordings
+      const recordings = await Array.fromAsync(glob(`${out}/${dirsGlob}/*.profraw`))
+
+      if(args.clean) {
+        await Promise.all(
+          recordings.concat([
+          reportPath
+        ]).map(path => rm(path, {recursive: true, force: true})))
+       
+        return;
+      }
+
+      const allTestSuites = [...new Set(recordings
         .map(x =>x.replace(out+'/',''))
         .map(x => x.split('/'))
         .filter(x => x.length > 1)
         .map(x => x[0]))
-      ] // seems like some binaries cause the html report to fail but not the json export?!
-      .filter( x => x.includes('brave_browser_tests') )
-      .map(x => `${config.outputDir}/${x}`)
+      ].map(x => `${config.outputDir}/${x}`)
       
 
+      // llvm-cov needs a .profdata (llvms .lcov equivalent) and 
+      // binaries that include the symbols that are instrumented to generate a report
+      // There seems to be some quirks with llvm-cov where merging multiple test suites fails
+      // silently yielding an empty html report.
+      // Wierdly it appears that the json export is unaffected.
+      // We probably want to use gcov / genhtml instead of llvm-cov
+      // For now - if we can - only use brave_browser_tests binary
+      // since it will have the most amount of symbols to generate the report.
+      // It will still include coverage information of code paths of other test suites
+      // as long as their symbols are present in the brave_browser_test binary
+      const hasBBT = allTestSuites.find(x => x.includes('brave_browser_tests'));
+      const testSuites = hasBBT 
+        ? ['brave_browser_tests']
+        : allTestSuites
+
       if (!recordings.length) {
-        console.error(`glob ${out}/**/*.profraw yield any recordings!`)
+        console.error(`glob ${out}/${dirsGlob}/*.profraw yield any recordings!\n make sure you've built with --is_coverage and ran the appropiate tests`)
         return;
       }
 
@@ -40,28 +74,29 @@ module.exports = (program) =>
       process.env.CWD = config.outputDir
       process.env.PATH = `${process.env.PATH}:${config.srcDir}/third_party/llvm-build/Release+Asserts/bin`
       // fetch coverage tools if not available
+      await mkdirp(distPath)
       await utils.runAsync("python3", [`${config.srcDir}/tools/clang/scripts/update.py`, "--package=coverage_tools"])
-      await utils.runAsync("llvm-profdata", ["merge", "-sparse", "-o", `${out}/coverage.profdata`, ...recordings])
+      await utils.runAsync("llvm-profdata", ["merge", "-sparse", "-o", profDataPath, ...recordings])
       await utils.runAsync("llvm-cov", [
         "show", 
         `-compilation-dir=${config.outputDir}`,
-        `-instr-profile=${out}/coverage.profdata`,
+        `-instr-profile=${profDataPath}`,
         "-format=html",
-        `-output-dir=${out}/report`,
+        `-output-dir=${distPath}`,
         ...testSuites
       ]);
 
       const output = await utils.runAsync("llvm-cov", [
         "export", 
         `-compilation-dir=${config.outputDir}`,
-        `-instr-profile=${out}/coverage.profdata`,
+        `-instr-profile=${profDataPath}`,
         "--summary-only",
         ...testSuites
       ], {stdio: 'pipe'});
 
       try {
         const summary = JSON.parse(output);
-        await writeJSON(`${out}/report/coverage.json`, summary);
+        await writeJSON(`${distPath}/coverage.json`, summary);
         console.log(summary.data[0].totals);
       } catch (e) {
         console.error(e)
