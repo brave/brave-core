d3e3d1825bc3f4ebddebeffcec05e2d2fad73e58
Claudio DeSouza <cdesouza@brave.com>
2025-08-31 19:58:22 +0100

    [cr141] `MemoryPressureListener` requires a tag value
    This argument has been added to the constructor of
    `MemoryPressureListener`, and this change provides an override to insert
    values just befor `kMax` for this enum class.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/5cb31fdf68faec8a8edc048929deb98b85de76f5
    
    commit 5cb31fdf68faec8a8edc048929deb98b85de76f5
    Author: Anthony Vallee-Dubois <anthonyvd@google.com>
    Date:   Fri Aug 29 15:28:15 2025 -0700
    
        Reland "Add tags to MemoryPressureListeners"
    
        This reverts commit 1f46c281fcf29e14b87f36e7d4b79d906eea3425.
    
        Bug: None
        Original change's description:
        > Revert "Add tags to MemoryPressureListeners"
        >
        > This reverts commit 0eeecc8e0409ab00e8fdd4c1f7ebb8211e0e3e4e.
        >
        > Reason for revert: Tree closure
        >
        > Bug: None
        > Original change's description:
        > > Add tags to MemoryPressureListeners
        > >
        > > This will allow experimenting with suppressing ranges of memory pressure
        > > listeners, to measure performance impact.
        > >
        > > Bug: None
        > > Change-Id: I89f036c75599abf07e43e6af94c9129931fab0fb
        > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6898335
        > > Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
        > > Owners-Override: Francois Pierre Doray <fdoray@chromium.org>
        > > Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
        > > Cr-Commit-Position: refs/heads/main@{#1508488}
        >
        > Bug: None
        > No-Presubmit: true
        > No-Tree-Checks: true
        > No-Try: true
        > Change-Id: I2695c4b5cde0a6ca5da1d4ef53afbce22ebf42cb
        > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6900496
        > Owners-Override: Marlon Facey <mfacey@chromium.org>
        > Commit-Queue: Marlon Facey <mfacey@chromium.org>
        > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
        > Auto-Submit: Marlon Facey <mfacey@chromium.org>
        > Cr-Commit-Position: refs/heads/main@{#1508500}
    
        Bug: None
        Change-Id: I72f5530e91f8356b5967cad02b6fe23b43791e5d
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6900437
        Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
        Auto-Submit: Anthony Vallée-Dubois <anthonyvd@chromium.org>
        Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
        Owners-Override: Francois Pierre Doray <fdoray@chromium.org>
        Reviewed-by: Anthony Cui <cuianthony@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1508651}

---
+++ chromium_src/base/memory/memory_pressure_listener.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_CHROMIUM_SRC_BASE_MEMORY_MEMORY_PRESSURE_LISTENER_H_
+#define BRAVE_CHROMIUM_SRC_BASE_MEMORY_MEMORY_PRESSURE_LISTENER_H_
+
+#define kMax kRewardsDatabase, kAdsDatabase, kMax
+
+#include <base/memory/memory_pressure_listener.h>  // IWYU pragma: export
+
+#undef kMax
+
+#endif  // BRAVE_CHROMIUM_SRC_BASE_MEMORY_MEMORY_PRESSURE_LISTENER_H_
