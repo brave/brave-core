557c27f2874ee77ad78f54c7ee1fba3c4ca888e2
Claudio DeSouza <cdesouza@brave.com>
2025-08-31 19:50:55 +0100

    [cr141] `TabStrip` removed from `BrowserViewLayout`'s ctor
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/8471c930952ad44125b1c749deb37c595cefa594
    
    commit 8471c930952ad44125b1c749deb37c595cefa594
    Author: dljames <dljames@chromium.org>
    Date:   Thu Aug 28 14:25:45 2025 -0700
    
        [Vertical Tabs] Move TabStrip construction into TabStripRegionView
    
        This CL moves the construction of the TabStrip into TabStripRegionView
        alleviating some responsibility from BrowserView in the process.
    
        TabStripRegionView was already taking ownership of TabStrip so this
        change enforces that.
    
        BrowserView::tabstrip() will be removed in a followup CL in favor of
        interacting with TabStripRegionView::tab_strip until that accessor is
        converted to TabStripViewInterface.
    
        To accommodate tests manually constructing the TabStripRegionView 2
        different strategies were used:
    
        1) Upgrade the test to a browser test since we want to test browser
        specific interactions that are no longer possible without a BrowserView
        pointer.
    
        2) Create a test only constructor that allows us to dependency inject a
        custom TabStrip object.
    
        Change-Id: Ifb06d6821195d3086f1da7a7fb2b7f7fd46895c0
        Bypass-Check-License: Renamed tab_strip_region_view_unittest to tab_strip_region_view_browser_test
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6882616
        Reviewed-by: David Pennington <dpenning@chromium.org>
        Reviewed-by: Eshwar Stalin <estalin@chromium.org>
        Commit-Queue: Darryl James <dljames@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1508012}

--- browser/ui/views/frame/brave_browser_view_layout.h
+++ browser/ui/views/frame/brave_browser_view_layout.h
@@ -21,7 +21,6 @@ class BraveBrowserViewLayout : public BrowserViewLayout {
                          WebAppFrameToolbarView* web_app_frame_toolbar,
                          views::Label* web_app_window_title,
                          TabStripRegionView* tab_strip_region_view,
-                         TabStrip* tab_strip,
                          views::View* vertical_tab_strip_container,
                          views::View* toolbar,
                          InfoBarContainerView* infobar_container,
