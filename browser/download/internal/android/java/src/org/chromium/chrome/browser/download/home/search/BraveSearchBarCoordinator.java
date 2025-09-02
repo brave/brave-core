e76ee81d6a57d092bc9617e108a4d951a6be07a9
AlexeyBarabash <alexey@brave.com>
2025-09-02 12:43:39 +0300

    Fixed build error
    BRAVE_ANDROID_DYNAMIC_COLORS flag is not available at 1.82.x as it
    was introduced at PR#30666

--- browser/download/internal/android/java/src/org/chromium/chrome/browser/download/home/search/BraveSearchBarCoordinator.java
+++ browser/download/internal/android/java/src/org/chromium/chrome/browser/download/home/search/BraveSearchBarCoordinator.java
@@ -7,11 +7,9 @@ package org.chromium.chrome.browser.download.home.search;
 
 import android.content.Context;
 
-import org.chromium.base.BraveFeatureList;
 import org.chromium.base.Callback;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.chrome.browser.download.internal.R;
-import org.chromium.chrome.browser.flags.ChromeFeatureList;
 
 @NullMarked
 public class BraveSearchBarCoordinator extends SearchBarCoordinator {
@@ -19,10 +17,9 @@ public class BraveSearchBarCoordinator extends SearchBarCoordinator {
             Context context, Callback<String> queryCallback, boolean autoFocusSearchBox) {
         super(context, queryCallback, autoFocusSearchBox);
 
-        // When Dynamic colors flag is disabled (default) we replace here icon with one
-        // without color_primary reference toi avoid orange tint on light theme
-        if (!ChromeFeatureList.isEnabled(BraveFeatureList.BRAVE_ANDROID_DYNAMIC_COLORS)) {
-            getView().setBackgroundResource(R.drawable.no_dynamic_download_search_bar_background);
-        }
+        // BRAVE_ANDROID_DYNAMIC_COLORS flag is not available at 1.82.x as it
+        // was introduced at PR#30666.
+        // At 1.82.x use no-dynamic drawable
+        getView().setBackgroundResource(R.drawable.no_dynamic_download_search_bar_background);
     }
 }
