90c6f3ba7f9f757129199b548cfa0a0fca579d1d
boocmp <boocmp@yandex.ru>
2025-09-02 15:08:53 +0700

    Exposed brave_content_settings_utils.h to iOS.

--- components/brave_shields/core/browser/brave_shields_settings.h
+++ components/brave_shields/core/browser/brave_shields_settings.h
@@ -6,7 +6,6 @@
 #ifndef BRAVE_COMPONENTS_BRAVE_SHIELDS_CORE_BROWSER_BRAVE_SHIELDS_SETTINGS_H_
 #define BRAVE_COMPONENTS_BRAVE_SHIELDS_CORE_BROWSER_BRAVE_SHIELDS_SETTINGS_H_
 
-#include "brave/components/brave_shields/core/common/brave_shields_panel.mojom.h"
 #include "brave/components/brave_shields/core/common/shields_settings.mojom.h"
 #include "components/content_settings/core/browser/cookie_settings.h"
 
