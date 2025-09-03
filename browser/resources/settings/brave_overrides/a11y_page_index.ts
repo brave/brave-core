2fddb602d1fd6e57f81311f71366845607ac3559
Jay Harris <jay.harris@outlook.co.nz>
2025-09-03 12:24:23 +1200

    [Settings]: Fix web3 alignment

--- browser/resources/settings/brave_overrides/a11y_page_index.ts
+++ browser/resources/settings/brave_overrides/a11y_page_index.ts
@@ -3,17 +3,17 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
+// <if expr="is_linux">
 import { RegisterPolymerTemplateModifications } from 'chrome://resources/brave/polymer_overriding.js';
 
 RegisterPolymerTemplateModifications({
   'settings-a11y-page-index': (templateContent) => {
-    // <if expr="is_linux">
     const captionsRoute = templateContent.
       querySelector('#captions')
     if (!captionsRoute) {
       throw new Error('Could not find #captions')
     }
     captionsRoute.remove()
-    // </if>
   },
 });
+// </if>
