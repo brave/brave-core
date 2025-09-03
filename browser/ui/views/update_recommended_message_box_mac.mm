224fc3086e38b972261254b2257262234d561cbe
Michael Herrmann <michael@herrmann.io>
2025-09-03 02:41:58 +0200

    Fix "Relaunch to update" button on macOS when Omaha 4 is active (#30969)

--- browser/ui/views/update_recommended_message_box_mac.mm
+++ browser/ui/views/update_recommended_message_box_mac.mm
@@ -1,10 +1,11 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright (c) 2018 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "brave/browser/ui/views/update_recommended_message_box_mac.h"
 
-#import "brave/browser/mac/sparkle_glue.h"
+#include "brave/browser/ui/webui/settings/brave_relaunch_handler_mac.h"
 #include "components/constrained_window/constrained_window_views.h"
 
 // static
@@ -15,6 +16,6 @@ void UpdateRecommendedMessageBoxMac::Show(gfx::NativeWindow parent_window) {
 }
 
 bool UpdateRecommendedMessageBoxMac::Accept() {
-  [[SparkleGlue sharedSparkleGlue] relaunch];
-  return true;
+  return brave_relaunch_handler::RelaunchOnMac() ||
+         UpdateRecommendedMessageBox::Accept();
 }
