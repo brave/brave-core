c6f6840f3e7145bb4339212476e80448c53794f5
Emerick Rogul <erogul@brave.com>
2025-09-04 14:55:54 -0400

    Block Lottie themed images for credit card / password / address save dialogs
    We want to block the various header images show on the credit card, password,
    and address save dialogs. Since those functions are spread across various
    classes, we block the images via an override for the upstream helper function
    that retrieves them.
    
    The set of blocked images are initialized at startup, so we can avoid the need
    to explicitly reference the blocked resource IDs by name in the helper which
    would cause a layering violation (they're defined in
    "chrome/grit/browser_resources.h")/

--- common/resource_bundle_helper.h
+++ common/resource_bundle_helper.h
@@ -1,16 +1,15 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright (c) 2017 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #ifndef BRAVE_COMMON_RESOURCE_BUNDLE_HELPER_H_
 #define BRAVE_COMMON_RESOURCE_BUNDLE_HELPER_H_
 
 namespace brave {
-
 void InitializeResourceBundle();
 bool SubprocessNeedsResourceBundle();
-
+void InitializeBlockedThemedLottieImages();
 }
 
 #endif  // BRAVE_COMMON_RESOURCE_BUNDLE_HELPER_H_
-
