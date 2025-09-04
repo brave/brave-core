58a855fd78237532fc6439a999cbc1ffa68736cd
Claudio DeSouza <cdesouza@brave.com>
2025-09-04 15:00:13 +0100

    [cr142] `IsURLAllowedInIncognito` override moved
    This function has been moved under a new path, and this change corrects
    our overrides for it accordingly.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/02c9b739714b3b7eee101c6e76b573af6061e473
    
    commit 02c9b739714b3b7eee101c6e76b573af6061e473
    Author: Jack Thiesen <jthiesen@chromium.org>
    Date:   Wed Sep 3 17:25:01 2025 -0700
    
        browser_navigator_android.cc stubs
    
        This change updates the implementation of Navigate() for Android Desktop to utilize a separate implementation than what exists already for WML. The IsURLAllowedInIncognito() function is moved to a separate file since it is cross-platform. Ifdefs are used inside the BUILD.gn files to compile the appropriate implementation of Navigate depending on the OS.
    
        Bug: 441594986
        Change-Id: I7756048d3ee52102421d17d6055ab9aaf973e9ac
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6909845
        Reviewed-by: Theresa Sullivan <twellington@chromium.org>
        Reviewed-by: Linyu He <linyuh@google.com>
        Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1510618}

---
+++ chromium_src/chrome/browser/ui/incognito_allowed_url.cc
@@ -0,0 +1,42 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "chrome/browser/ui/incognito_allowed_url.h"
+
+#include <string>
+#include <string_view>
+
+#include "brave/components/constants/webui_url_constants.h"
+#include "chrome/common/webui_url_constants.h"
+#include "content/public/common/url_constants.h"
+#include "url/gurl.h"
+
+namespace {
+
+bool IsURLAllowedInIncognitoBraveImpl(const GURL& url) {
+  std::string scheme = url.scheme();
+  std::string_view host = url.host_piece();
+  if (scheme != content::kChromeUIScheme) {
+    return true;
+  }
+
+  if (host == kRewardsPageHost || host == chrome::kChromeUISyncInternalsHost ||
+      host == chrome::kBraveUISyncHost || host == kAdblockHost ||
+      host == kWelcomeHost || host == kBraveGettingStartedHost) {
+    return false;
+  }
+
+  return true;
+}
+
+}  // namespace
+
+#define BRAVE_IS_URL_ALLOWED_IN_INCOGNITO     \
+  if (!IsURLAllowedInIncognitoBraveImpl(url)) \
+    return false;
+
+#include <chrome/browser/ui/incognito_allowed_url.cc>
+
+#undef BRAVE_IS_URL_ALLOWED_IN_INCOGNITO
