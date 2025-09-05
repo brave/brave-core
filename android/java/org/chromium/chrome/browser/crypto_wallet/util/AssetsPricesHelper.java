e72bce309d184433b5078ed31dc4cc071bd844a8
Anirudha Bose <anirudha.bose@alumni.cern>
2025-09-05 19:07:22 +0530

    Fix android build

--- android/java/org/chromium/chrome/browser/crypto_wallet/util/AssetsPricesHelper.java
+++ android/java/org/chromium/chrome/browser/crypto_wallet/util/AssetsPricesHelper.java
@@ -6,15 +6,10 @@
 package org.chromium.chrome.browser.crypto_wallet.util;
 
 import org.chromium.base.Callbacks;
-import org.chromium.base.Log;
-import org.chromium.brave_wallet.mojom.AssetPrice;
-import org.chromium.brave_wallet.mojom.AssetPriceTimeframe;
 import org.chromium.brave_wallet.mojom.AssetRatioService;
 import org.chromium.brave_wallet.mojom.BlockchainToken;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Locale;
 
 public class AssetsPricesHelper {
     private static final String TAG = "AssetsPricesHelper";
@@ -23,56 +18,8 @@ public class AssetsPricesHelper {
             AssetRatioService assetRatioService,
             BlockchainToken[] assets,
             Callbacks.Callback1<HashMap<String, Double>> callback) {
+        // Method is not used - return empty HashMap to maintain compatibility
         HashMap<String, Double> assetsPrices = new HashMap<String, Double>();
-        AsyncUtils.MultiResponseHandler pricesMultiResponse =
-                new AsyncUtils.MultiResponseHandler(assets.length);
-        ArrayList<AsyncUtils.GetPriceResponseContext> pricesContexts =
-                new ArrayList<AsyncUtils.GetPriceResponseContext>();
-
-        for (BlockchainToken asset : assets) {
-            if (asset == null || asset.symbol == null) {
-                continue;
-            }
-            String assetSymbolLower = asset.symbol.toLowerCase(Locale.ENGLISH);
-            String[] fromAssets = new String[] {assetSymbolLower};
-            String[] toAssets = new String[] {"usd"};
-
-            AsyncUtils.GetPriceResponseContext priceContext =
-                    new AsyncUtils.GetPriceResponseContext(
-                            pricesMultiResponse.singleResponseComplete);
-            pricesContexts.add(priceContext);
-
-            assetRatioService.getPrice(
-                    fromAssets, toAssets, AssetPriceTimeframe.LIVE, priceContext);
-        }
-
-        pricesMultiResponse.setWhenAllCompletedAction(
-                () -> {
-                    for (AsyncUtils.GetPriceResponseContext priceContext : pricesContexts) {
-                        if (!priceContext.success || priceContext.prices.length != 1) {
-                            continue;
-                        }
-
-                        Double usdPerToken = 0.0d;
-                        final AssetPrice thisPrice = priceContext.prices[0];
-                        final String toConvert = thisPrice.price != null ? thisPrice.price : "0.0";
-                        try {
-                            usdPerToken = Double.parseDouble(toConvert);
-                        } catch (NumberFormatException ex) {
-                            Log.e(
-                                    TAG,
-                                    "Cannot parse "
-                                            + toConvert
-                                            + ", Token: "
-                                            + String.valueOf(thisPrice.fromAsset)
-                                            + ", "
-                                            + ex);
-                            continue;
-                        }
-                        assetsPrices.put(
-                                thisPrice.fromAsset.toLowerCase(Locale.getDefault()), usdPerToken);
-                    }
-                    callback.call(assetsPrices);
-                });
+        callback.call(assetsPrices);
     }
 }
