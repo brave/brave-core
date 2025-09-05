cc4d2c5b9b1ad38baf0cefdae38c235a164c180c
Szilard Szaloki <szilardszaloki@gmail.com>
2025-09-04 18:27:52 -0600

    Endpoint client v1 (WIP)

---
+++ components/endpoint_client/client.h
@@ -0,0 +1,91 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_ENDPOINT_CLIENT_CLIENT_H_
+#define BRAVE_COMPONENTS_ENDPOINT_CLIENT_CLIENT_H_
+
+#include <optional>
+#include <string>
+#include <utility>
+
+#include "base/check.h"
+#include "base/containers/to_vector.h"
+#include "base/functional/bind.h"
+#include "base/functional/callback_forward.h"
+#include "base/json/json_writer.h"
+#include "base/types/expected.h"
+#include "base/values.h"
+#include "brave/components/api_request_helper/api_request_helper.h"
+#include "brave/components/endpoint_client/endpoint_builder.h"
+#include "net/http/http_request_headers.h"
+
+// See //brave/components/endpoint_client/README.md
+// for design, motivation, usage, and examples.
+
+namespace endpoint_client {
+
+template <endpoints::concepts::Endpoint E>
+class Client {
+  template <typename T>
+  struct Parse {
+    static auto FromValue(const base::Value& value) {
+      return T::FromValue(value);
+    }
+  };
+
+  template <typename... Ts>
+  struct Parse<std::variant<Ts...>> {
+    static auto FromValue(const base::Value& value) {
+      std::optional<std::variant<Ts...>> result;
+      (
+          [&] {
+            if (result) {
+              return;
+            }
+
+            if (auto t = Ts::FromValue(value)) {
+              result = std::move(*t);
+            }
+          }(),
+          ...);
+      return result;
+    }
+  };
+
+ public:
+  template <endpoints::concepts::Accepts<E> Request,
+            typename Response = E::template ResponseFor<Request>,
+            typename Error = E::template ErrorFor<Request>,
+            typename Expected =
+                base::expected<std::optional<Response>, std::optional<Error>>,
+            typename Callback = base::OnceCallback<void(int, Expected)>>
+  static void Send(api_request_helper::APIRequestHelper& api_request_helper,
+                       Request request,
+                       Callback callback) {
+    auto on_response = [](Callback callback,
+                          api_request_helper::APIRequestResult result) {
+      std::move(callback).Run(
+          result.response_code(),
+          result.Is2XXResponseCode()
+              ? Expected(Parse<Response>::FromValue(result.value_body()))
+              : base::unexpected(Parse<Error>::FromValue(result.value_body())));
+    };
+
+    const auto json = base::WriteJson(request.ToValue());
+    CHECK(json) << "Failed to serialize request to JSON!";
+
+    api_request_helper.Request(
+        std::string(request.Method()), E::URL(), *json, "application/json",
+        base::BindOnce(std::move(on_response), std::move(callback)),
+        base::ToVector(request.headers.GetHeaderVector(),
+                       [](const auto& header) {
+                         return std::pair(header.key, header.value);
+                       }));
+  }
+};
+
+}  // namespace endpoint_client
+
+#endif  // BRAVE_COMPONENTS_ENDPOINT_CLIENT_CLIENT_H_
