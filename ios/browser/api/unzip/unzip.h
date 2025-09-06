4588940a544450a0f4fbf912891faf860388c5e7
brave-builds <45370463+brave-builds@users.noreply.github.com>
2025-09-06 10:09:34 +0900

    [iOS] Remove usage of JS unzipper (uplift to 1.82.x) (#31038)
    Uplift of #30997 (squashed) to release

--- ios/browser/api/unzip/unzip.h
+++ ios/browser/api/unzip/unzip.h
@@ -13,11 +13,20 @@ NS_ASSUME_NONNULL_BEGIN
 OBJC_EXPORT
 NS_SWIFT_NAME(Unzip)
 @interface BraveUnzip : NSObject
-@property(nonatomic, readonly, class, nullable) NSString* jszipScriptPath;
 
+/// Unzips a zip file located on disc asynchronously to a given folder using
+/// the standard in-process unzipper
 + (void)unzip:(NSString*)zipFile
     toDirectory:(NSString*)directory
      completion:(void (^)(bool))completion;
+
+/// Unzips data asynchronously using Chromium's rust unzipper returning an array
+/// containing the data for each unzipped file in the archive or an error if
+/// the zip couldn't be expanded
++ (void)unzipData:(NSData*)data
+       completion:(void (^)(NSArray<NSData*>* _Nullable unzippedFiles,
+                            NSError* _Nullable error))completion
+    NS_SWIFT_NAME(unzip(data:completion:));
 @end
 
 NS_ASSUME_NONNULL_END
