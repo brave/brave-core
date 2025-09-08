03f72187ffecd6ba5e63934fa399ee4a1804cfd4
Claudio DeSouza <cdesouza@brave.com>
2025-09-08 15:58:45 +0100

    [cr142] `gnrt` run for Chromium 142.0.7401.1

--- third_party/rust/chromium_crates_io/vendor/find-msvc-tools-v0_1/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/find-msvc-tools-v0_1/src/lib.rs
@@ -6,6 +6,6 @@
 // by `tools/crates/run_gnrt.py vendor`. Do not edit!
 //
 // This is an empty placeholder that has replaced the
-// `cc-1.2.36` crate.
+// `find-msvc-tools-0.1.1` crate.
 //
 // See `//tools/crates/gnrt/removed_crate.md` to learn more.
