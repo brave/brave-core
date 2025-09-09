0432b288e39b34279d7c124a421a06fc272b2bc6
Szilard Szaloki <szilardszaloki@gmail.com>
2025-09-09 09:10:17 -0600

    Endpoint client v3 (WIP) - SimpleURLLoader

--- components/endpoint_client/client.h
+++ components/endpoint_client/client.h
@@ -14,12 +14,16 @@
 #include "base/containers/to_vector.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_forward.h"
+#include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
 #include "base/types/expected.h"
 #include "base/values.h"
-#include "brave/components/api_request_helper/api_request_helper.h"
 #include "brave/components/endpoint_client/endpoint_builder.h"
 #include "net/http/http_request_headers.h"
+#include "net/http/http_status_code.h"
+#include "services/network/public/cpp/resource_request.h"
+#include "services/network/public/cpp/simple_url_loader.h"
+#include "services/network/public/mojom/url_response_head.mojom.h"
 
 // See //brave/components/endpoint_client/README.md
 // for design, motivation, usage, and examples.
@@ -56,33 +60,65 @@ class Client {
 
  public:
   template <endpoints::concepts::SupportedBy<Endpoint> Request>
-  static void Send(api_request_helper::APIRequestHelper& api_request_helper,
-                   Request request,
-                   Endpoint::template CallbackFor<Request> callback) {
-    auto on_response = [](decltype(callback) callback,
-                          api_request_helper::APIRequestResult result) {
-      using Response = Endpoint::template ResponseFor<Request>;
-      using Error = Endpoint::template ErrorFor<Request>;
-      using Expected = Endpoint::template ExpectedFor<Request>;
+  static void Send(
+      const scoped_refptr<network::SharedURLLoaderFactory>& url_loader_factory,
+      Request request,
+      Endpoint::template CallbackFor<Request> callback) {
+    auto on_response =
+        [](decltype(callback) callback,
+           std::unique_ptr<network::SimpleURLLoader> simple_url_loader,
+           std::optional<std::string> response_body) {
+          using Response = Endpoint::template ResponseFor<Request>;
+          using Error = Endpoint::template ErrorFor<Request>;
+          using Expected = Endpoint::template ExpectedFor<Request>;
 
-      std::move(callback).Run(
-          result.response_code(),
-          result.Is2XXResponseCode()
-              ? Expected(Parse<Response>::FromValue(result.value_body()))
-              : base::unexpected(Parse<Error>::FromValue(result.value_body())));
-    };
+          CHECK(simple_url_loader);
+
+          const auto* response_info = simple_url_loader->ResponseInfo();
+          if (!response_info) {
+            return std::move(callback).Run(simple_url_loader->NetError(),
+                                           base::unexpected(std::nullopt));
+          }
+
+          const auto& headers = response_info->headers;
+          if (!headers) {
+            return std::move(callback).Run(simple_url_loader->NetError(),
+                                           base::unexpected(std::nullopt));
+          }
+
+          const auto status_code = headers->response_code();
+
+          const auto reply = base::JSONReader::Read(response_body.value_or(""));
+          std::move(callback).Run(
+              status_code,
+              status_code >= 200 && status_code < 300
+                  ? Expected(reply ? Parse<Response>::FromValue(*reply)
+                                   : std::nullopt)
+                  : base::unexpected(reply ? Parse<Error>::FromValue(*reply)
+                                           : std::nullopt));
+        };
 
     const auto json = base::WriteJson(request.ToValue());
     CHECK(json) << "Failed to serialize request to JSON!";
 
-    api_request_helper.Request(
-        std::string(request.Method()), Endpoint::URL(), *json,
-        "application/json",
-        base::BindOnce(std::move(on_response), std::move(callback)),
-        base::ToVector(request.headers.GetHeaderVector(),
-                       [](const auto& header) {
-                         return std::pair(header.key, header.value);
-                       }));
+    auto resource_request = std::make_unique<network::ResourceRequest>();
+    resource_request->url = Endpoint::URL();
+    resource_request->method = request.Method();
+    resource_request->headers = std::move(request.headers);
+
+    auto simple_url_loader = network::SimpleURLLoader::Create(
+        std::move(resource_request),
+        net::DefineNetworkTrafficAnnotation("test", "test"));
+    simple_url_loader->SetAllowHttpErrorResults(true);
+    simple_url_loader->AttachStringForUpload(std::move(*json),
+                                             "application/json");
+
+    auto* simple_url_loader_ptr = simple_url_loader.get();
+    simple_url_loader_ptr->DownloadToString(
+        url_loader_factory.get(),
+        base::BindOnce(std::move(on_response), std::move(callback),
+                       std::move(simple_url_loader)),
+        network::SimpleURLLoader::kMaxBoundedStringDownloadSize);
   }
 };
 
