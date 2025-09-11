8a3a8d3e6890c9dc89f242af080ed8c5d1eea417
Claudio DeSouza <cdesouza@brave.com>
2025-09-11 20:51:18 +0100

    [cr142][ios] `BuildServiceInstanceFor` passing a `ProfileIOS*`
    This change corrects all overrides for this function.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/320d2eb7ee6325c6a21375c3c933baef309248b8
    https://chromium.googlesource.com/chromium/src/+/ca9116c2fdb769ef997fc1e454626bf71d050917
    
    commit 320d2eb7ee6325c6a21375c3c933baef309248b8
    Author: Sylvain Defresne <sdefresne@chromium.org>
    Date:   Tue Sep 9 08:43:36 2025 -0700
    
        [ios] Make {Refcounted,}ProfileKeyedServiceFactoryIOS abstract
    
        As all sub-classes override BuildServiceInstanceFor(ProfileIOS*),
        mark the method as abstract and remove the deprecated overload
        BuildServiceInstanceFor(web::BrowserState*).
    
        Bug: 436484783
        Change-Id: Icf6c12c3676296617fccb8d33caa3f2b41e6a38f
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6915535
        Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
        Reviewed-by: Asami Doi <asamidoi@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1513100}
    
    commit ca9116c2fdb769ef997fc1e454626bf71d050917
    Author: Sylvain Defresne <sdefresne@chromium.org>
    Date:   Wed Sep 10 01:59:15 2025 -0700
    
        [ios] Remove ProfileTestingFactory alias
    
        All the factories have been converted to the new name, so remove
        the alias used for the incremental migration.
    
        Bug: 436484783
        Change-Id: I579c0f344b95a6b28bd12d996f89255810361130
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6930234
        Reviewed-by: Federica Germinario <fedegermi@google.com>
        Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1513566}

--- ios/browser/brave_wallet/brave_wallet_service_factory.h
+++ ios/browser/brave_wallet/brave_wallet_service_factory.h
@@ -20,10 +20,6 @@ template <typename T>
 class NoDestructor;
 }  // namespace base
 
-namespace web {
-class BrowserState;
-}  // namespace web
-
 namespace brave_wallet {
 
 class BraveWalletService;
@@ -42,7 +38,7 @@ class BraveWalletServiceFactory : public ProfileKeyedServiceFactoryIOS {
 
   // ProfileKeyedServiceFactoryIOS implementation.
   std::unique_ptr<KeyedService> BuildServiceInstanceFor(
-      web::BrowserState* context) const override;
+      ProfileIOS* profile) const override;
 
   BraveWalletServiceFactory(const BraveWalletServiceFactory&) = delete;
   BraveWalletServiceFactory& operator=(const BraveWalletServiceFactory&) =
