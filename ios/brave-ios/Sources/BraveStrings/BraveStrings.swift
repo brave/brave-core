7de2a91c17295332899cddc7a83bd20e72043088
Kyle Hickinson <kylehickinson@users.noreply.github.com>
2025-09-11 11:56:09 -0400

    [iOS] Add context menu support to new tab tray UI (#30782)
    Adds the ability to long press one or more tabs if in multi-select mode to show a list of actions associated with them

--- ios/brave-ios/Sources/BraveStrings/BraveStrings.swift
+++ ios/brave-ios/Sources/BraveStrings/BraveStrings.swift
@@ -1124,6 +1124,13 @@ extension Strings {
       value: "Select Tabs",
       comment: "A button title which when tapped will allow the user to select multiple tabs in the tab grid"
     )
+    public static let deselectTabsButtonTitle = NSLocalizedString(
+      "tabGrid.deselectTabsButtonTitle",
+      tableName: "BraveShared",
+      bundle: .module,
+      value: "Deselect Tabs",
+      comment: "A button title which when tapped will allow the user to deselect multiple tabs in the tab grid"
+    )
     public static let moreMenuButtonTitle = NSLocalizedString(
       "tabGrid.moreMenuButtonTitle",
       tableName: "BraveShared",
@@ -1138,12 +1145,19 @@ extension Strings {
       value: "New Tab",
       comment: "Accessibility label for a close button that when tapped opens a new tab"
     )
-    public static let closeTabAccessibilityLabel = NSLocalizedString(
+    public static let closeTab = NSLocalizedString(
       "tabGrid.closeTab",
       tableName: "BraveShared",
       bundle: .module,
       value: "Close Tab",
-      comment: "Accessibility label for a close button that when tapped closes the associated tab"
+      comment: "Label for a close button that when tapped closes the associated tab"
+    )
+    public static let closeTabs = NSLocalizedString(
+      "tabGrid.closeTabs",
+      tableName: "BraveShared",
+      bundle: .module,
+      value: "Close Tabs",
+      comment: "Label for a close button that when tapped closes the associated tabs"
     )
     public static let shredTabsAccessibilityLabel = NSLocalizedString(
       "tabGrid.shredTabsAccessibilityLabel",
