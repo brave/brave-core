f797d4686acecb074cf68e3b67845b7c29b12923
sangwoo ko <sangwoo108@gmail.com>
2025-09-11 12:23:17 +0900

    Refactor to use RAII style observer

--- browser/ui/views/permissions/permission_prompt_bubble_base_view_browsertest.cc
+++ browser/ui/views/permissions/permission_prompt_bubble_base_view_browsertest.cc
@@ -5,6 +5,7 @@
 
 #include "chrome/browser/ui/views/permissions/permission_prompt_bubble_base_view.h"
 
+#include "base/test/run_until.h"
 #include "chrome/browser/ui/views/permissions/permission_prompt_style.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "components/content_settings/core/common/content_settings_types.mojom.h"
@@ -104,19 +105,53 @@ IN_PROC_BROWSER_TEST_F(PermissionPromptBubbleBaseViewBrowserTest,
   // This test checks that the permission prompt bubble is created with the
   // correct z-order level, which should be kSecuritySurface.
   MockPermissionPromptDelegate mock_delegate;
-  auto* permission_prompt = new MockPermissionPromptBubbleBaseView(
-      browser(), mock_delegate.GetWeakPtr());
-  EXPECT_EQ(permission_prompt->GetWidget()->GetZOrderLevel(),
-            ui::ZOrderLevel::kSecuritySurface);
+  auto create_permission_prompt = [&]() {
+    auto* permission_prompt = new MockPermissionPromptBubbleBaseView(
+        browser(), mock_delegate.GetWeakPtr());
+    EXPECT_EQ(permission_prompt->GetWidget()->GetZOrderLevel(),
+              ui::ZOrderLevel::kSecuritySurface);
 
-  // Also parent widget should have the same z-order level.
-  auto* parent_widget = permission_prompt->GetWidget()->parent();
-  ASSERT_TRUE(parent_widget);
-  EXPECT_EQ(parent_widget->GetZOrderLevel(), ui::ZOrderLevel::kSecuritySurface);
-
-  permission_prompt->Close();
+    // Also parent widget should have the same z-order level.
+    auto* parent_widget = permission_prompt->GetWidget()->parent();
+    EXPECT_TRUE(parent_widget);
+    EXPECT_EQ(parent_widget->GetZOrderLevel(),
+              ui::ZOrderLevel::kSecuritySurface);
+    return permission_prompt;
+  };
 
   // After closing the prompt widget, the parent widget should have the original
   // z-order level.
+  auto* permission_prompt = create_permission_prompt();
+  auto widget_weak_ptr = permission_prompt->GetWidget()->GetWeakPtr();
+  ASSERT_TRUE(widget_weak_ptr);
+  auto* parent_widget = widget_weak_ptr->parent();
+
+  permission_prompt->GetWidget()->Close();
+
+  ASSERT_TRUE(base::test::RunUntil([&]() { return !widget_weak_ptr; }));
+  EXPECT_NE(parent_widget->GetZOrderLevel(), ui::ZOrderLevel::kSecuritySurface);
+
+  // After Accepting the prompt, the parent widget should have the original
+  // z-order level.
+  permission_prompt = create_permission_prompt();
+  widget_weak_ptr = permission_prompt->GetWidget()->GetWeakPtr();
+  ASSERT_TRUE(widget_weak_ptr);
+  parent_widget = widget_weak_ptr->parent();
+
+  permission_prompt->AcceptDialog();
+
+  ASSERT_TRUE(base::test::RunUntil([&]() { return !widget_weak_ptr; }));
+  EXPECT_NE(parent_widget->GetZOrderLevel(), ui::ZOrderLevel::kSecuritySurface);
+
+  // After Canceling the prompt, the parent widget should have the original
+  // z-order level.
+  permission_prompt = create_permission_prompt();
+  widget_weak_ptr = permission_prompt->GetWidget()->GetWeakPtr();
+  ASSERT_TRUE(widget_weak_ptr);
+  parent_widget = widget_weak_ptr->parent();
+
+  permission_prompt->CancelDialog();
+
+  ASSERT_TRUE(base::test::RunUntil([&]() { return !widget_weak_ptr; }));
   EXPECT_NE(parent_widget->GetZOrderLevel(), ui::ZOrderLevel::kSecuritySurface);
 }
--- chromium_src/chrome/browser/ui/views/permissions/permission_prompt_bubble_base_view.h
+++ chromium_src/chrome/browser/ui/views/permissions/permission_prompt_bubble_base_view.h
@@ -6,17 +6,57 @@
 #ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_PERMISSIONS_PERMISSION_PROMPT_BUBBLE_BASE_VIEW_H_
 #define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_PERMISSIONS_PERMISSION_PROMPT_BUBBLE_BASE_VIEW_H_
 
+#include "base/memory/raw_ref.h"
+#include "base/scoped_observation.h"
 #include "ui/base/ui_base_types.h"
+#include "ui/views/widget/widget_observer.h"
 
-// Add RestoreParentWidgetZOrderLevel() and a member variable in order to reset
-// parent widget's z-order level.
-#define ShowWidget                              \
-  RestoreParentWidgetZOrderLevel();             \
-                                                \
- private:                                       \
-  ui::ZOrderLevel parent_widget_z_order_level_; \
-                                                \
- public:                                        \
+class PermissionPromptBubbleBaseView;
+
+// This class manages z-order of permission prompt bubble and its parent
+class PermissionPromptBubbleZOrderManager : public views::WidgetObserver {
+ public:
+  explicit PermissionPromptBubbleZOrderManager(
+      PermissionPromptBubbleBaseView& permission_prompt_bubble);
+  PermissionPromptBubbleZOrderManager(
+      const PermissionPromptBubbleZOrderManager&) = delete;
+  PermissionPromptBubbleZOrderManager& operator=(
+      const PermissionPromptBubbleZOrderManager&) = delete;
+  ~PermissionPromptBubbleZOrderManager() override;
+
+  // views::WidgetObserver:
+  void OnWidgetDestroying(views::Widget* widget) override;
+
+ private:
+  // Sets the zorder for permission prompt bubble to kSecuritySurface, so that
+  // it appears above other UI elements even they are floating on top. For
+  // example, Picture-in-Picture window is on top of other widgets, but
+  // permission prompt bubble should still be on top of it.
+  void ElevateZOrder();
+
+  // Restores z-order of widget and its parent widget to the level before
+  // elevation.
+  void RestoreZOrder();
+
+  raw_ref<PermissionPromptBubbleBaseView> permission_prompt_bubble_;  // owner
+
+  bool z_order_elevated_ = false;
+
+  ui::ZOrderLevel widget_z_order_level_;
+  ui::ZOrderLevel parent_widget_z_order_level_;
+  base::ScopedObservation<views::Widget, views::WidgetObserver>
+      prompt_widget_observation_{this};
+};
+
+// Add PermissionPromptBubbleZOrderManager as member variable in order to manage
+// widget's z-order level.
+#define ShowWidget                                                       \
+  ShowWidget_Unused();                                                   \
+                                                                         \
+ private:                                                                \
+  std::unique_ptr<PermissionPromptBubbleZOrderManager> z_order_manager_; \
+                                                                         \
+ public:                                                                 \
   void ShowWidget
 
 #include <chrome/browser/ui/views/permissions/permission_prompt_bubble_base_view.h>  // IWYU pragma: export
