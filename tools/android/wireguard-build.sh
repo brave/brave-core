fb483b86849f5cc13daf574a2b64f3d2db8c2170
Brian Clifton <brian@clifton.me>
2025-09-11 10:40:17 -0700

    Fix presubmit issues

--- tools/android/wireguard-build.sh
+++ tools/android/wireguard-build.sh
@@ -1,11 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# Copyright (c) 2025 The Brave Authors. All rights reserved.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at https://mozilla.org/MPL/2.0/.
 
-# Script used to build the WireGuard tunnel library.
+# This script is used to build the WireGuard tunnel library.
 # This should be ran from the Brave root. aka `root/src/brave/`.
 pushd .
 
 # uses JAVA and SDK version from Chromium checkout.
-# this syntax w/ pwd is expanding relative path to absolute path.
+# this syntax w/ realpath is expanding relative path to absolute path.
 export JAVA_HOME=`realpath ../third_party/jdk/current`
 export PATH=$PATH:$JAVA_HOME/bin
 export ANDROID_HOME=`realpath ../third_party/android_sdk/public`
@@ -13,17 +17,18 @@ export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin/"
 echo "JAVA_HOME=$JAVA_HOME"
 echo "ANDROID_HOME=$ANDROID_HOME"
 
-BRAVE_ANDROID_DEPS_DIR=`realpath ./third_party/android_deps/libs/com_wireguard_android`
-echo "Changing directory to $BRAVE_ANDROID_DEPS_DIR"
-cd "$BRAVE_ANDROID_DEPS_DIR"
+BRAVE_DEPS_DIR=`realpath ./third_party/android_deps/libs/com_wireguard_android`
+echo "Changing directory to $BRAVE_DEPS_DIR"
+cd "$BRAVE_DEPS_DIR"
 pushd .
 
-echo "Accepting licenses. If this is a first time, you will need to type Y/N and hit enter for each"
+echo "If you haven't accepted licenses yet this is interactive."
 sdkmanager --licenses
 
 WIREGUARD_TAG="1.0.20250531"
+REPO_URL="https://git.zx2c4.com/wireguard-android"
 echo "Cloning wireguard-android tag: $WIREGUARD_TAG"
-git clone --recurse-submodules --branch "$WIREGUARD_TAG" https://git.zx2c4.com/wireguard-android
+git clone --recurse-submodules --branch "$WIREGUARD_TAG" "$REPO_URL"
 cd wireguard-android/
 
 echo "Building wireguard-around tag: $WIREGUARD_TAG"
@@ -31,7 +36,9 @@ echo "Building wireguard-around tag: $WIREGUARD_TAG"
 popd
 
 echo "Copying binary from tunnel/build/outputs/aar to expected Brave location"
-mv wireguard-android/tunnel/build/outputs/aar/tunnel-release.aar "$BRAVE_ANDROID_DEPS_DIR/tunnel-$WIREGUARD_TAG.aar"
+COPY_SRC="wireguard-android/tunnel/build/outputs/aar/tunnel-release.aar"
+COPY_DST="$BRAVE_DEPS_DIR/tunnel-$WIREGUARD_TAG.aar"
+mv "$COPY_SRC" "$COPY_DST"
 
 echo "Removing code that was cloned/built"
 rm -rf wireguard-android/
