f95ec91a1211ca04120fb7348ad9f11735e7e75f
Stephen Heaps <steaps.4@gmail.com>
2025-09-12 13:45:05 -0400

    Add feature flag (default off) to enable WebKit Advanced Privacy Protections.

--- components/brave_shields/core/common/features.cc
+++ components/brave_shields/core/common/features.cc
@@ -162,6 +162,10 @@ BASE_FEATURE(kCosmeticFilteringCustomScriptlets,
              "CosmeticFilteringCustomScriptlets",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+BASE_FEATURE(kWebKitAdvancedPrivacyProtections,
+             "kWebKitAdvancedPrivacyProtections",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 constexpr base::FeatureParam<int> kComponentUpdateCheckIntervalMins{
     &kAdBlockDefaultResourceUpdateInterval, "update_interval_mins", 100};
 
