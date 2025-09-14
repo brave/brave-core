ec3d8daa1d5ef24d73cbbce4617c7553a2b139e8
Claudio DeSouza <cdesouza@brave.com>
2025-09-14 20:53:00 +0100

    [cr142] `GetEffectiveURL` return value updated
    This was changed in upstream and this change only corrects the brave
    override.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/eb09474c1526ae12085eea342fa2c75b353eb5ef
    
    commit eb09474c1526ae12085eea342fa2c75b353eb5ef
    Author: Camille Lamy <clamy@chromium.org>
    Date:   Thu Sep 11 03:47:22 2025 -0700
    
        Refactor SiteInfo::GetSiteForURLInternal
    
        This CL refactors GetSiteForURLInternal into a
        GetAgentClusterKeyForURL function that will make the decision to use
        an origin-keyed or site-keyed AgentClusterKey, and give it the correct
        origin or site URL. This fixes a bug where AgentClusterKeys for
        DocumentIsolationPolicy were given the origin of their site instead of
        their own origin.
    
        For site URLs computed based on effective URLs, the ContentBrowserClient
        method is updated to return std::nullopt when there is no effective URL.
        This allows us to simply use agent_cluster_key_::GetURL() as a site URL
        when there is no effective URL. When there is an effective URL, we
        compute what the AgentClusterKey would be based on the effective URL,
        and use this computed AgentClusterKey's URL as site URL.
    
        Bug: crbug.com/433443082
        Change-Id: Ife5c7ecc4b693776b81038e9ddafd202f4af47c8
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6818812
        Reviewed-by: Colin Blundell <blundell@chromium.org>
        Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
        Commit-Queue: Camille Lamy <clamy@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1514186}

--- browser/brave_content_browser_client.cc
+++ browser/brave_content_browser_client.cc
@@ -1150,7 +1150,7 @@ void BraveContentBrowserClient::MaybeHideReferrer(
   }
 }
 
-GURL BraveContentBrowserClient::GetEffectiveURL(
+std::optional<GURL> BraveContentBrowserClient::GetEffectiveURL(
     content::BrowserContext* browser_context,
     const GURL& url) {
   Profile* profile = Profile::FromBrowserContext(browser_context);
