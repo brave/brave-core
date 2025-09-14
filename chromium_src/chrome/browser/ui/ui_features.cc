d99128505a5ad2b1b26d410fd629c83584923a06
Claudio DeSouza <cdesouza@brave.com>
2025-09-14 20:52:08 +0100

    [cr142] `kScrimForBrowserWindowModal` deleted
    We don't yet fully support scrim views, so we must keep the browser window modal
    scrim view hidden. The issue for fully enabling scrim views is
    https://github.com/brave/brave-browser/issues/46337.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/1ff95c368cfbf15b2972bcb4d239f168f6547127
    
    commit 1ff95c368cfbf15b2972bcb4d239f168f6547127
    Author: Keren Zhu <kerenzhu@chromium.org>
    Date:   Thu Sep 11 13:17:03 2025 -0700
    
        Cleanup kScrimForBrowserWindowModal and kScrimForTabModal
    
        These features were enabled by default.
    
        Bug: 391903216
        Change-Id: Ib78823de3ee9cdacdfc18bda194c9bb9772af200
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6940398
        Reviewed-by: Dana Fried <dfried@chromium.org>
        Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1514483}

--- chromium_src/chrome/browser/ui/ui_features.cc
+++ chromium_src/chrome/browser/ui/ui_features.cc
@@ -19,7 +19,6 @@ OVERRIDE_FEATURE_DEFAULT_STATES({{
 #endif
     // TODO(https://github.com/brave/brave-browser/issues/46337): Re-enable
     // scrim views if needed.
-    {kScrimForBrowserWindowModal, base::FEATURE_DISABLED_BY_DEFAULT},
     {kSideBySide, base::FEATURE_ENABLED_BY_DEFAULT},
     {kTabHoverCardImages, base::FEATURE_DISABLED_BY_DEFAULT},
     {kTabstripComboButton, base::FEATURE_ENABLED_BY_DEFAULT},
