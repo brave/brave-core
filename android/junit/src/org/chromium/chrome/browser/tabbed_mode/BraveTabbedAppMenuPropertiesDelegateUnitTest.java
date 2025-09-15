dc6600468190d422cb39ade098e8fc31f8a65ecb
AlexeyBarabash <alexey@brave.com>
2025-09-15 17:57:50 +0300

    [cr142][Android] PausedExecutorTestRule was removed
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/468a130e04e7c9f15bbc597271b404045c4179d9
    
    	[Android] Replace usages of PausedExecutorTestRule
    
    	BaseRobolectricTestRule has the same functionality.
    
    	Bug: 40934211
    	Change-Id: Ib0f4df3c59b13b353756b74d19c8d6814b272cdd
    	Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6857364

--- android/junit/src/org/chromium/chrome/browser/tabbed_mode/BraveTabbedAppMenuPropertiesDelegateUnitTest.java
+++ android/junit/src/org/chromium/chrome/browser/tabbed_mode/BraveTabbedAppMenuPropertiesDelegateUnitTest.java
@@ -37,7 +37,7 @@ import org.chromium.base.BraveFeatureList;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.supplier.OneshotSupplierImpl;
-import org.chromium.base.task.test.PausedExecutorTestRule;
+import org.chromium.base.test.BaseRobolectricTestRule;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
@@ -128,7 +128,6 @@ import java.util.List;
 })
 public class BraveTabbedAppMenuPropertiesDelegateUnitTest {
     @Rule public final MockitoRule mMockitoRule = MockitoJUnit.rule();
-    @Rule public PausedExecutorTestRule mExecutorRule = new PausedExecutorTestRule();
 
     @Mock private ActivityTabProvider mActivityTabProvider;
     @Mock private Tab mTab;
@@ -272,7 +271,7 @@ public class BraveTabbedAppMenuPropertiesDelegateUnitTest {
                         mIncognitoReauthControllerSupplier,
                         mReadAloudControllerSupplier);
         delegate.setIsJunitTesting(true);
-        mExecutorRule.runAllBackgroundAndUi();
+        BaseRobolectricTestRule.runAllBackgroundAndUi();
         mTabbedAppMenuPropertiesDelegate = Mockito.spy(delegate);
 
         ChromeSharedPreferences.getInstance()
