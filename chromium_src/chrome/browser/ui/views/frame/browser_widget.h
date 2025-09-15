f9a30e275e13db6a406f522287c46bd5a21dbc5e
Claudio DeSouza <cdesouza@brave.com>
2025-09-15 03:33:15 +0100

    [cr142] `BrowserFrame` renamed to `BrowserWidget`
    This change corrects the places where the type name was occurring as
    well as the override files.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/b1925dd16ef2d151d03af99be8862ea13e965c2b
    
    commit b1925dd16ef2d151d03af99be8862ea13e965c2b
    Author: Dana Fried <dfried@chromium.org>
    Date:   Fri Sep 12 11:04:41 2025 -0700
    
        Reland "[Browser Frame] Rename BrowserFrame to BrowserWidget"
    
        GARDENERS: *IMPORTANT* DO NOT REVERT DUE TO TAST TESTS
        ------------------------------------------------------
        This CL may break tast tests due to renaming a class.
        It is not Chrome policy to fix tast tests due to changes in Chrome.
        Please assign test failure bugs to the tast test owners (hdchuong,
        diwux, etc.) If you have any questions please contact Chrome Desktop TL
        erikchen@google.com.
        ------------------------------------------------------
    
        This reverts commit 8228e0bdf31c5a72bf85069f0dce6a68b35ac53d.
    
        Reason for revert: Tast tests must be fixed separately by ChromeOS
        team; no changes can be made on this CL to avert these failures.
    
        Original change's description:
        > Revert "[Browser Frame] Rename BrowserFrame to BrowserWidget"
        >
        > This reverts commit 3146d563293ccd8d0ee02e5f15df8e2085708ab0.
        >
        > Reason for revert: Culprit/need-to-revert-change of huge failures of tast.* b/444070886
        >
        > Original change's description:
        > > [Browser Frame] Rename BrowserFrame to BrowserWidget
        > >
        > > This clarifies the function of BrowserFrame, which is that it is a
        > > specialization of Widget for Browser.
        > >
        > > All references are updated except a few comments in ash/chromeos code,
        > > which will be fixed in a follow-up.
        > >
        > > browser_frame.h has been left in as a stub/alias only. The second bug
        > > linked below is the cleanup step where this stub will be removed.
        > >
        > > BYPASS_RECITATION_REASON=Moved existing file and left stub.
        > >
        > > Bypass-Check-License: File rename only.
        > > Bug: 443271862, 443281722
        > > Change-Id: I4a989837b23937b1ba5292cfb258671f5d16647c
        > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6920067
        > > Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
        > > Commit-Queue: Dana Fried <dfried@chromium.org>
        > > Auto-Submit: Dana Fried <dfried@chromium.org>
        > > Cr-Commit-Position: refs/heads/main@{#1513018}
        >
        > Bug: 443271862, 443281722, 444070886
        > No-Presubmit: true
        > No-Tree-Checks: true
        > No-Try: true
        > Change-Id: I38cc834c9ef6ff8debb270369a1aab0f6a9b9146
        > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6934198
        > Owners-Override: Ken Okada <kenoss@chromium.org>
        > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
        > Commit-Queue: Ken Okada <kenoss@chromium.org>
        > Cr-Commit-Position: refs/heads/main@{#1513411}
    
        BYPASS_RECITATION_REASON=Moved existing file and left stub.
    
        Bypass-Check-License: File rename only.
        Bug: 443271862, 443281722, 444070886
        Change-Id: Iaa35e7434e5b28ef01163c7d9ccce3ef94fbaf8f
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6936181
        Reviewed-by: Erik Chen <erikchen@chromium.org>
        Commit-Queue: Dana Fried <dfried@chromium.org>
        Reviewed-by: Ken Okada <kenoss@chromium.org>
        Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1514967}

--- chromium_src/chrome/browser/ui/views/frame/browser_widget.h
+++ chromium_src/chrome/browser/ui/views/frame/browser_widget.h
@@ -3,8 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_FRAME_BROWSER_FRAME_H_
-#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_FRAME_BROWSER_FRAME_H_
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_FRAME_BROWSER_WIDGET_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_FRAME_BROWSER_WIDGET_H_
 
 #define OnMenuClosed              \
   OnMenuClosed_Unused() {}        \
@@ -13,10 +13,10 @@
 
 #define SetTabDragKind virtual SetTabDragKind
 
-#include <chrome/browser/ui/views/frame/browser_frame.h>  // IWYU pragma: export
+#include <chrome/browser/ui/views/frame/browser_widget.h>  // IWYU pragma: export
 
 #undef SetTabDragKind
 
 #undef OnMenuClosed
 
-#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_FRAME_BROWSER_FRAME_H_
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_FRAME_BROWSER_WIDGET_H_
