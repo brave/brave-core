36de7add7bf64ba9f817ab126490b0d12f7685fa
Max Karolinskiy <max@brave.com>
2025-09-15 09:50:29 -0400

    [cr141] Disables GDP service requests.
    Per security/privacy teams.
    
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/2ef1f7013a02decc3a67723397b389054def51d7
    
    commit 2ef1f7013a02decc3a67723397b389054def51d7
    Author: Ergün Erdoğmuş <ergunsh@chromium.org>
    Date:   Wed Aug 27 01:45:37 2025 -0700
    
        [DevToolsUIBindings] Add GDP service handler http requests to GDP service
    
        Design doc is here: go/chrome-devtools:gdp-in-devtools-design
    
        Change-Id: I6764f19addb149b294ea91f97be73caba5c86928
        Fixed: 441036948

---
+++ chromium_src/chrome/browser/devtools/gdp_service_handler.h
@@ -0,0 +1,20 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_DEVTOOLS_GDP_SERVICE_HANDLER_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_DEVTOOLS_GDP_SERVICE_HANDLER_H_
+
+#include "chrome/browser/devtools/devtools_http_service_handler.h"
+
+// Override to disable GDP service handler requests as it sends them to
+// googleapis.com.
+#define CanMakeRequest(...)           \
+  CanMakeRequest_Unused(__VA_ARGS__); \
+  void CanMakeRequest(__VA_ARGS__)
+
+#include <chrome/browser/devtools/gdp_service_handler.h>  // IWYU pragma: export
+#undef CanMakeRequest
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_DEVTOOLS_GDP_SERVICE_HANDLER_H_
