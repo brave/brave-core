67a62dca5d6c8a7f288181b04e3c49ac4763a987
Emerick Rogul <erogul@brave.com>
2025-09-15 09:43:48 -0400

    [cr142] `BrowserFrame` -> `BrowserWidget`
    `BrowserFrame` is renamed to `BrowserWidget` upstream.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/b1925dd16ef2d151d03af99be8862ea13e965c2b
    
    commit b1925dd16ef2d151d03af99be8862ea13e965c2b
    Author: Dana Fried <dfried@chromium.org>
    Date:   Fri Sep 12 11:04:41 2025 -0700
    
        Reland "[Browser Frame] Rename BrowserFrame to BrowserWidget"
    
        GARDENERS: *IMPORTANT* DO NOT REVERT DUE TO TAST TESTS
        ------------------------------------------------------
        This CL may break tast tests due to renaming a class.
        It is not Chrome policy to fix tast tests due to changes in Chrome.
        Please assign test failure bugs to the tast test owners (hdchuong,
        diwux, etc.) If you have any questions please contact Chrome Desktop TL
        erikchen@google.com.
        ------------------------------------------------------
    
        This reverts commit 8228e0bdf31c5a72bf85069f0dce6a68b35ac53d.
    
        Reason for revert: Tast tests must be fixed separately by ChromeOS
        team; no changes can be made on this CL to avert these failures.
    
        Original change's description:
        > Revert "[Browser Frame] Rename BrowserFrame to BrowserWidget"
        >
        > This reverts commit 3146d563293ccd8d0ee02e5f15df8e2085708ab0.
        >
        > Reason for revert: Culprit/need-to-revert-change of huge failures of tast.* b/444070886
        >
        > Original change's description:
        > > [Browser Frame] Rename BrowserFrame to BrowserWidget
        > >
        > > This clarifies the function of BrowserFrame, which is that it is a
        > > specialization of Widget for Browser.
        > >
        > > All references are updated except a few comments in ash/chromeos code,
        > > which will be fixed in a follow-up.
        > >
        > > browser_frame.h has been left in as a stub/alias only. The second bug
        > > linked below is the cleanup step where this stub will be removed.
        > >
        > > BYPASS_RECITATION_REASON=Moved existing file and left stub.
        > >
        > > Bypass-Check-License: File rename only.
        > > Bug: 443271862, 443281722
        > > Change-Id: I4a989837b23937b1ba5292cfb258671f5d16647c
        > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6920067
        > > Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
        > > Commit-Queue: Dana Fried <dfried@chromium.org>
        > > Auto-Submit: Dana Fried <dfried@chromium.org>
        > > Cr-Commit-Position: refs/heads/main@{#1513018}
        >
        > Bug: 443271862, 443281722, 444070886
        > No-Presubmit: true
        > No-Tree-Checks: true
        > No-Try: true
        > Change-Id: I38cc834c9ef6ff8debb270369a1aab0f6a9b9146
        > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6934198
        > Owners-Override: Ken Okada <kenoss@chromium.org>
        > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
        > Commit-Queue: Ken Okada <kenoss@chromium.org>
        > Cr-Commit-Position: refs/heads/main@{#1513411}
    
        BYPASS_RECITATION_REASON=Moved existing file and left stub.
    
        Bypass-Check-License: File rename only.
        Bug: 443271862, 443281722, 444070886
        Change-Id: Iaa35e7434e5b28ef01163c7d9ccce3ef94fbaf8f
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6936181
        Reviewed-by: Erik Chen <erikchen@chromium.org>
        Commit-Queue: Dana Fried <dfried@chromium.org>
        Reviewed-by: Ken Okada <kenoss@chromium.org>
        Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1514967}

--- browser/ui/views/frame/brave_browser_widget.cc
+++ browser/ui/views/frame/brave_browser_widget.cc
@@ -1,9 +1,9 @@
 /* Copyright (c) 2019 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "brave/browser/ui/views/frame/brave_browser_frame.h"
+#include "brave/browser/ui/views/frame/brave_browser_widget.h"
 
 #include "brave/browser/themes/brave_private_window_theme_supplier.h"
 #include "brave/browser/ui/darker_theme/features.h"
@@ -16,13 +16,13 @@
 #include "chrome/browser/themes/theme_service.h"
 #include "chrome/browser/themes/theme_service_factory.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/views/frame/browser_frame.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/browser/ui/views/frame/browser_widget.h"
 #include "components/prefs/pref_service.h"
 #include "ui/color/color_provider_key.h"
 
-BraveBrowserFrame::BraveBrowserFrame(BrowserView* browser_view)
-    : BrowserFrame(browser_view), view_(browser_view) {
+BraveBrowserWidget::BraveBrowserWidget(BrowserView* browser_view)
+    : BrowserWidget(browser_view), view_(browser_view) {
   if (view_->browser()->profile()->IsIncognitoProfile() ||
       view_->browser()->profile()->IsTor() ||
       view_->browser()->profile()->IsGuestSession()) {
@@ -31,12 +31,12 @@ BraveBrowserFrame::BraveBrowserFrame(BrowserView* browser_view)
   }
 }
 
-BraveBrowserFrame::~BraveBrowserFrame() = default;
+BraveBrowserWidget::~BraveBrowserWidget() = default;
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
 // Tor/Guest profile should use DarkAura. If not, their native ui is affected by
 // normal windows theme change.
-const ui::NativeTheme* BraveBrowserFrame::GetNativeTheme() const {
+const ui::NativeTheme* BraveBrowserWidget::GetNativeTheme() const {
   if ((view_->browser()->profile()->IsIncognitoProfile() ||
        view_->browser()->profile()->IsTor() ||
        view_->browser()->profile()->IsGuestSession()) &&
@@ -49,7 +49,7 @@ const ui::NativeTheme* BraveBrowserFrame::GetNativeTheme() const {
 #endif
 
 ui::ColorProviderKey::ThemeInitializerSupplier*
-BraveBrowserFrame::GetCustomTheme() const {
+BraveBrowserWidget::GetCustomTheme() const {
   // To provider private(tor) windows's theme color via color provider,
   // we use |theme_supplier_| for both as upstream doesn't use separated
   // mix for private window.
@@ -57,20 +57,20 @@ BraveBrowserFrame::GetCustomTheme() const {
     return theme_supplier_.get();
   }
 
-  return BrowserFrame::GetCustomTheme();
+  return BrowserWidget::GetCustomTheme();
 }
 
-views::internal::RootView* BraveBrowserFrame::CreateRootView() {
+views::internal::RootView* BraveBrowserWidget::CreateRootView() {
   root_view_ = new BraveBrowserRootView(browser_view_, this);
   return root_view_;
 }
 
-void BraveBrowserFrame::SetTabDragKind(TabDragKind kind) {
+void BraveBrowserWidget::SetTabDragKind(TabDragKind kind) {
   const bool should_sync_shared_pinned_tab =
       base::FeatureList::IsEnabled(tabs::features::kBraveSharedPinnedTabs) &&
       tab_drag_kind_ == TabDragKind::kAllTabs && kind == TabDragKind::kNone;
 
-  BrowserFrame::SetTabDragKind(kind);
+  BrowserWidget::SetTabDragKind(kind);
 
   if (should_sync_shared_pinned_tab) {
     SharedPinnedTabServiceFactory::GetForProfile(browser_view_->GetProfile())
@@ -78,8 +78,8 @@ void BraveBrowserFrame::SetTabDragKind(TabDragKind kind) {
   }
 }
 
-ui::ColorProviderKey BraveBrowserFrame::GetColorProviderKey() const {
-  auto key = BrowserFrame::GetColorProviderKey();
+ui::ColorProviderKey BraveBrowserWidget::GetColorProviderKey() const {
+  auto key = BrowserWidget::GetColorProviderKey();
 
   if (base::FeatureList::IsEnabled(darker_theme::features::kBraveDarkerTheme)) {
     // Note that we don't change set SchemeVariant to kDarker if
