4ed2840119216f6fb24dac50492fb95628bd999b
Brian R. Bondy <netzen@gmail.com>
2025-09-15 22:00:48 -0400

    Add Brave Origin Local State pref (#31209)
    Brave Origin will use a local state key to set the Brave Origin policy
    values the user wants to use so that policies can be setup that way on
    startup.

---
+++ components/brave_origin/pref_names.h
@@ -0,0 +1,16 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_BRAVE_ORIGIN_PREF_NAMES_H_
+#define BRAVE_COMPONENTS_BRAVE_ORIGIN_PREF_NAMES_H_
+
+namespace brave_origin {
+
+// Base key for all profile-scoped BraveOrigin policy preferences in local state
+inline constexpr char kBraveOriginPolicies[] = "brave.brave_origin.policies";
+
+}  // namespace brave_origin
+
+#endif  // BRAVE_COMPONENTS_BRAVE_ORIGIN_PREF_NAMES_H_
