c2bf3ca7be7fe682a3a00a8b15920b450660b1be
Terry Mancey <terry.mancey@icloud.com>
2025-09-16 13:39:36 -0500

    [ads] OHTTP POC
    This PR implements client-side support for Oblivious HTTP (OHTTP) in
    Brave Ads. OHTTP enables the Brave browser to send ad confirmations
    through a relay in a way that preserves privacy by preventing the relay
    from learning both the request destination and content. This
    implementation allows the browser to encrypt and forward ad confirmation
    requests via the OHTTP relay, ensuring user privacy when communicating
    with the Brave Ads Server.

---
+++ components/brave_ads/core/browser/service/ads_service_feature.h
@@ -0,0 +1,21 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_BRAVE_ADS_CORE_BROWSER_SERVICE_ADS_SERVICE_FEATURE_H_
+#define BRAVE_COMPONENTS_BRAVE_ADS_CORE_BROWSER_SERVICE_ADS_SERVICE_FEATURE_H_
+
+#include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
+
+namespace brave_ads {
+
+BASE_DECLARE_FEATURE(kAdsServiceFeature);
+
+inline constexpr base::FeatureParam<bool> kShouldSupportOhttp{
+    &kAdsServiceFeature, "should_support_ohttp", false};
+
+}  // namespace brave_ads
+
+#endif  // BRAVE_COMPONENTS_BRAVE_ADS_CORE_BROWSER_SERVICE_ADS_SERVICE_FEATURE_H_
