ef8ebc4b5591e01f70e921ecf424f79859a1381b
brave-builds <brave-builds+devops@brave.com>
2025-09-16 10:55:12 +0000

    Uplift of #30833 (squashed) to beta

--- browser/ui/webui/brave_wallet/ledger/ledger_ui.cc
+++ browser/ui/webui/brave_wallet/ledger/ledger_ui.cc
@@ -25,6 +25,8 @@ UntrustedLedgerUI::UntrustedLedgerUI(content::WebUI* web_ui)
   untrusted_source->AddFrameAncestor(GURL(kBraveUIWalletPageURL));
   untrusted_source->AddFrameAncestor(GURL(kBraveUIWalletPanelURL));
   untrusted_source->OverrideContentSecurityPolicy(
+      network::mojom::CSPDirectiveName::ConnectSrc, "connect-src 'self';");
+  untrusted_source->OverrideContentSecurityPolicy(
       network::mojom::CSPDirectiveName::StyleSrc,
       std::string("style-src 'unsafe-inline';"));
   untrusted_source->AddResourcePath("load_time_data_deprecated.js",
