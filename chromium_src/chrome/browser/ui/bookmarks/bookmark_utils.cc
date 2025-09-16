830e51aaa16b96616047f01a83a47ffe90016847
Agustín Ruiz <aguscr182@gmail.com>
2025-09-16 10:51:18 -0300

    replace chromium folder icon with nala (#31186)
    * replace chromium folder icon with nala
    
    * Run formatter
    
    * Add buildguard
    
    ---------
    
    Co-authored-by: sangwoo ko <sangwoo108@gmail.com>

--- chromium_src/chrome/browser/ui/bookmarks/bookmark_utils.cc
+++ chromium_src/chrome/browser/ui/bookmarks/bookmark_utils.cc
@@ -4,14 +4,18 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "chrome/browser/ui/bookmarks/bookmark_utils.h"
+
 #include "brave/browser/ui/bookmark/bookmark_helper.h"
 #include "brave/components/constants/pref_names.h"
 #include "build/build_config.h"
+#include "chrome/browser/ui/color/chrome_color_id.h"
 #include "chrome/grit/theme_resources.h"
 #include "components/bookmarks/common/bookmark_pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "components/user_prefs/user_prefs.h"
+#include "components/vector_icons/vector_icons.h"
 #include "content/public/browser/browser_context.h"
+#include "ui/base/models/image_model.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/color/color_id.h"
 #include "ui/color/color_provider.h"
@@ -46,7 +50,17 @@ void BraveToggleBookmarkBarState(content::BrowserContext* browser_context) {
   }                                                                        \
   void ToggleBookmarkBarWhenVisible_ChromiumImpl
 
+#if defined(TOOLKIT_VIEWS)
+// Override folder icon to use a fixed 20px size similar to saved groups.
+#define GetBookmarkFolderIcon GetBookmarkFolderIcon_ChromiumImpl
+#endif  // defined(TOOLKIT_VIEWS)
+
 #include <chrome/browser/ui/bookmarks/bookmark_utils.cc>
+
+#if defined(TOOLKIT_VIEWS)
+#undef GetBookmarkFolderIcon
+#endif  // defined(TOOLKIT_VIEWS)
+
 #undef ToggleBookmarkBarWhenVisible
 #undef IsAppsShortcutEnabled
 #undef ShouldShowAppsShortcutInBookmarkBar
@@ -60,5 +74,17 @@ bool IsAppsShortcutEnabled(Profile* profile) {
 bool ShouldShowAppsShortcutInBookmarkBar(Profile* profile) {
   return false;
 }
+
+#if defined(TOOLKIT_VIEWS)
+// Brave override: return folder icons at 20px like the saved groups icon.
+ui::ImageModel GetBookmarkFolderIcon(BookmarkFolderIconType icon_type,
+                                     ui::ColorVariant color) {
+  const gfx::VectorIcon* id = icon_type == BookmarkFolderIconType::kManaged
+                                  ? &vector_icons::kFolderManagedRefreshIcon
+                                  : &vector_icons::kFolderChromeRefreshIcon;
+  // Use toolbar icon color for visual consistency with other toolbar icons.
+  return ui::ImageModel::FromVectorIcon(*id, kColorToolbarButtonIcon, 20);
+}
+#endif  // defined(TOOLKIT_VIEWS)
 
 }  // namespace chrome
