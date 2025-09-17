3e430aef3973f5ab316a4d4d790ebd75652b9f13
zenparsing <zenparsing@gmail.com>
2025-09-17 16:34:14 -0400

    [NTP Next] Use "hide news" text when user has not enabled News

--- browser/resources/brave_new_tab_page_refresh/lib/strings.ts
+++ browser/resources/brave_new_tab_page_refresh/lib/strings.ts
@@ -33,6 +33,7 @@ export type StringKey =
   'newsDisableButtonLabel' |
   'newsEnableButtonLabel' |
   'newsEnableText' |
+  'newsHideButtonLabel' |
   'newsSettingsTitle' |
   'newsWidgetTitle' |
   'photoCreditsText' |
