235f3657dfb8ceb153aec8440396a47a30ade77a
Kyle Hickinson <git@kylehickinson.com>
2025-09-17 16:04:38 -0400

    Move AI Chat url util logic into ai chat component
    These methods will be used in the future by iOS implementing Leo AI Chat WebUI

--- components/ai_chat/core/common/ai_chat_urls.cc
+++ components/ai_chat/core/common/ai_chat_urls.cc
@@ -3,7 +3,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#include "brave/browser/ai_chat/ai_chat_urls.h"
+#include "brave/components/ai_chat/core/common/ai_chat_urls.h"
 
 #include <string_view>
 
