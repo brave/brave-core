3c23aae65e222763c65039894fae72884d68125e
Szilard Szaloki <szilardszaloki@gmail.com>
2025-09-17 14:09:43 -0600

    Add a new pref `brave.account.verification_token`. (#31264)
    This preference will store (`OSCrypt`-encrypted) the `verificationToken` returned by the `POST /v2/accounts/password/init` endpoint. It is then used for:
    - calling `POST /v2/accounts/password/finalize` to complete the registration flow
    - polling `POST /v2/verify/result` to exchange the `verificationToken` for the `authToken` after the user has verified their email

---
+++ components/brave_account/pref_names.h
@@ -0,0 +1,23 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_BRAVE_ACCOUNT_PREF_NAMES_H_
+#define BRAVE_COMPONENTS_BRAVE_ACCOUNT_PREF_NAMES_H_
+
+namespace brave_account::prefs {
+
+// This preference will store (`OSCrypt`-encrypted) the `verificationToken`
+// returned by the POST /v2/accounts/password/init endpoint.
+// It is then used for:
+// - calling POST /v2/accounts/password/finalize
+//   to complete the registration flow
+// - polling POST /v2/verify/result
+//   to exchange the `verificationToken` for the `authToken`
+//   after the user has verified their email
+inline constexpr char kVerificationToken[] = "brave.account.verification_token";
+
+}  // namespace brave_account::prefs
+
+#endif  // BRAVE_COMPONENTS_BRAVE_ACCOUNT_PREF_NAMES_H_
