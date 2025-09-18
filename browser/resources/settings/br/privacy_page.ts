32cd4a76b5a8fe71fd3c38f4360b2050a65dea3a
Emerick Rogul <erogul@brave.com>
2025-09-18 10:45:58 -0400

    [cr142] `storageAccess` is no longer on the main settings privacy page
    Since the `storageAccess` page is no longer shown on a page we include, we can
    stop removing its template.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/1b1c2051c16243851c16d9bcf07e91eb9614e5bf
    
    commit 1b1c2051c16243851c16d9bcf07e91eb9614e5bf
    Author: dpapad <dpapad@chromium.org>
    Date:   Mon Sep 15 09:16:52 2025 -0700
    
        Settings re-architecture: Migrate Privacy section, part 14.
    
        Promote several site settings subpages to standalone views in
        <settings-privacy-page-index>.
    
        As part of the transition to the new architecture also making these
        views searchable.
    
        Bug: 424223101
        Change-Id: I3dff452d4f3ca2f40e6875f29b79ddf4cda4f594
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6939833
        Reviewed-by: Rainhard Findling <rainhard@chromium.org>
        Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1515544}

--- browser/resources/settings/br/privacy_page.ts
+++ browser/resources/settings/br/privacy_page.ts
@@ -364,15 +364,5 @@ RegisterPolymerTemplateModifications({
         privacyGuideLinkRow.setAttribute('hidden', 'true')
       }
     }
-
-    const storageAccessTemplate = templateContent.querySelector(
-      `template[is=dom-if][route-path='/content/storageAccess'`)
-    if (!storageAccessTemplate) {
-      console.error(
-        '[Brave Settings Overrides] Could not find template with' +
-        ' route-path=/content/storageAccess on privacy page.')
-    } else {
-      storageAccessTemplate.remove()
-    }
   },
 })
