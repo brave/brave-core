7aea796b119b55c4703c3ae2253298d1d15f2f0f
Michael Herrmann <michael@herrmann.io>
2025-09-18 15:43:38 +0200

    Strip privacy-sensitive data from update requests

---
+++ components/update_client/brave_protocol_handler.h
@@ -0,0 +1,26 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_UPDATE_CLIENT_BRAVE_PROTOCOL_HANDLER_H_
+#define BRAVE_COMPONENTS_UPDATE_CLIENT_BRAVE_PROTOCOL_HANDLER_H_
+
+#include <memory>
+
+#include "components/update_client/protocol_handler.h"
+#include "components/update_client/protocol_parser.h"
+#include "components/update_client/protocol_serializer.h"
+
+namespace update_client {
+
+class BraveProtocolHandlerFactoryJSON : public ProtocolHandlerFactory {
+ public:
+  // Overrides for ProtocolHandlerFactory.
+  std::unique_ptr<ProtocolParser> CreateParser() const override;
+  std::unique_ptr<ProtocolSerializer> CreateSerializer() const override;
+};
+
+}  // namespace update_client
+
+#endif  // BRAVE_COMPONENTS_UPDATE_CLIENT_BRAVE_PROTOCOL_HANDLER_H_
---
+++ components/update_client/brave_protocol_serializer_json.h
@@ -0,0 +1,31 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_UPDATE_CLIENT_BRAVE_PROTOCOL_SERIALIZER_JSON_H_
+#define BRAVE_COMPONENTS_UPDATE_CLIENT_BRAVE_PROTOCOL_SERIALIZER_JSON_H_
+
+#include <string>
+
+#include "components/update_client/protocol_definition.h"
+#include "components/update_client/protocol_serializer.h"
+
+namespace update_client {
+
+class BraveProtocolSerializerJSON : public ProtocolSerializer {
+ public:
+  BraveProtocolSerializerJSON() = default;
+
+  BraveProtocolSerializerJSON(const BraveProtocolSerializerJSON&) = delete;
+  BraveProtocolSerializerJSON& operator=(const BraveProtocolSerializerJSON&) =
+      delete;
+
+  // Overrides for ProtocolSerializer.
+  std::string Serialize(
+      const protocol_request::Request& request) const override;
+};
+
+}  // namespace update_client
+
+#endif  // BRAVE_COMPONENTS_UPDATE_CLIENT_BRAVE_PROTOCOL_SERIALIZER_JSON_H_
