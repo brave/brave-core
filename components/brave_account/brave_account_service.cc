149d5efc487b888e8adee05d87b94c99d12b30f7
Szilard Szaloki <szilardszaloki@gmail.com>
2025-09-19 09:11:09 -0600

    Brave Account registration flow

--- components/brave_account/brave_account_service.cc
+++ components/brave_account/brave_account_service.cc
@@ -7,16 +7,98 @@
 
 #include <utility>
 
-#include "base/notimplemented.h"
+#include "base/base64.h"
+#include "base/check_deref.h"
+#include "base/functional/bind.h"
+#include "base/functional/callback.h"
+#include "base/strings/strcat.h"
+#include "brave/components/brave_account/endpoint_client/client.h"
+#include "brave/components/brave_account/pref_names.h"
+#include "components/os_crypt/sync/os_crypt.h"
 #include "components/prefs/pref_service.h"
+#include "net/http/http_request_headers.h"
+#include "net/http/http_status_code.h"
+#include "net/traffic_annotation/network_traffic_annotation.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
+namespace {
+inline constexpr net::NetworkTrafficAnnotationTag kTrafficAnnotation =
+    net::DefineNetworkTrafficAnnotation("brave_account_endpoints",
+                                        R"(
+  semantics {
+    sender: "Brave Account client"
+    description:
+      "Implements the creation or sign-in process for a Brave Account."
+    trigger:
+      "User attempts to create or sign in to a Brave Account from settings."
+    user_data: {
+      type: EMAIL
+    }
+    data:
+      "Blinded cryptographic message for secure password setup "
+      "and account email address."
+      "Verification token for account activation and "
+      "serialized cryptographic record for account finalization."
+    destination: OTHER
+    destination_other: "Brave Account service"
+  }
+  policy {
+    cookies_allowed: NO
+    policy_exception_justification:
+      "These requests are essential for Brave Account creation and sign-in "
+      "and cannot be disabled by policy."
+  }
+)");
+
+std::string Encrypt(const std::string& plain_text) {
+  if (plain_text.empty()) {
+    return std::string();
+  }
+
+  std::string encrypted;
+  if (!OSCrypt::EncryptString(plain_text, &encrypted)) {
+    return std::string();
+  }
+
+  return base::Base64Encode(encrypted);
+}
+
+std::string Decrypt(const std::string& base64) {
+  if (base64.empty()) {
+    return std::string();
+  }
+
+  std::string encrypted;
+  if (!base::Base64Decode(base64, &encrypted)) {
+    return std::string();
+  }
+
+  std::string plain_text;
+  if (!OSCrypt::DecryptString(encrypted, &plain_text)) {
+    return std::string();
+  }
+
+  return plain_text;
+}
+}  // namespace
+
 namespace brave_account {
+
+using endpoint_client::Client;
+using endpoint_client::WithHeaders;
+using endpoints::PasswordFinalize;
+using endpoints::PasswordInit;
 
 BraveAccountService::BraveAccountService(
     PrefService* pref_service,
     scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory)
-    : pref_service_(pref_service), url_loader_factory_(url_loader_factory) {}
+    : BraveAccountService(
+          pref_service,
+          std::make_unique<api_request_helper::APIRequestHelper>(
+              kTrafficAnnotation,
+              std::move(url_loader_factory)),
+          base::BindRepeating(&Encrypt),
+          base::BindRepeating(&Decrypt)) {}
 
 BraveAccountService::~BraveAccountService() = default;
 
@@ -25,24 +107,167 @@ void BraveAccountService::BindInterface(
   authentication_receivers_.Add(this, std::move(pending_receiver));
 }
 
+BraveAccountService::BraveAccountService(
+    PrefService* pref_service,
+    std::unique_ptr<api_request_helper::APIRequestHelper> api_request_helper,
+    CryptoCallback encrypt_callback,
+    CryptoCallback decrypt_callback)
+    : pref_service_(pref_service),
+      api_request_helper_(std::move(api_request_helper)),
+      encrypt_callback_(std::move(encrypt_callback)),
+      decrypt_callback_(std::move(decrypt_callback)) {}
+
 void BraveAccountService::RegisterInitialize(
     const std::string& email,
     const std::string& blinded_message,
-    mojom::Authentication::RegisterInitializeCallback callback) {
-  // TODO(https://github.com/brave/brave-browser/issues/47400)
-  // TODO(https://github.com/brave/brave-browser/issues/48488)
-  NOTIMPLEMENTED();
-  std::move(callback).Run(mojom::RegisterInitializeResult::New());
+    RegisterInitializeCallback callback) {
+  DCHECK(!email.empty());
+  DCHECK(!blinded_message.empty());
+
+  if (email.empty() || blinded_message.empty()) {
+    return std::move(callback).Run(
+        base::unexpected(mojom::RegisterFailureReason::kInitializeUnexpected));
+  }
+
+  PasswordInit::Request request;
+  request.blinded_message = blinded_message;
+  request.new_account_email = email;
+  request.serialize_response = true;
+  Client<PasswordInit>::Send(
+      CHECK_DEREF(api_request_helper_.get()), std::move(request),
+      base::BindOnce(&BraveAccountService::OnRegisterInitialize,
+                     weak_factory_.GetWeakPtr(), std::move(callback)));
 }
 
 void BraveAccountService::RegisterFinalize(
     const std::string& encrypted_verification_token,
     const std::string& serialized_record,
-    mojom::Authentication::RegisterFinalizeCallback callback) {
-  // TODO(https://github.com/brave/brave-browser/issues/47400)
-  // TODO(https://github.com/brave/brave-browser/issues/48488)
-  NOTIMPLEMENTED();
-  std::move(callback).Run(mojom::RegisterFinalizeResult::New());
+    RegisterFinalizeCallback callback) {
+  DCHECK(!encrypted_verification_token.empty());
+  DCHECK(!serialized_record.empty());
+
+  if (encrypted_verification_token.empty() || serialized_record.empty()) {
+    return std::move(callback).Run(
+        base::unexpected(mojom::RegisterFailureReason::kFinalizeUnexpected));
+  }
+
+  const std::string verification_token =
+      decrypt_callback_.Run(encrypted_verification_token);
+  if (verification_token.empty()) {
+    return std::move(callback).Run(
+        base::unexpected(mojom::RegisterFailureReason::kFinalizeUnexpected));
+  }
+
+  WithHeaders<PasswordFinalize::Request> request;
+  request.serialized_record = serialized_record;
+  request.headers.SetHeader("Authorization",
+                            base::StrCat({"Bearer ", verification_token}));
+  Client<PasswordFinalize>::Send(
+      CHECK_DEREF(api_request_helper_.get()), std::move(request),
+      base::BindOnce(&BraveAccountService::OnRegisterFinalize,
+                     weak_factory_.GetWeakPtr(), std::move(callback),
+                     encrypted_verification_token));
+}
+
+void BraveAccountService::OnRegisterInitialize(
+    RegisterInitializeCallback callback,
+    int response_code,
+    base::expected<std::optional<PasswordInit::Response>,
+                   std::optional<PasswordInit::Error>> reply) {
+  auto result =
+      std::move(reply)
+          // expected<optional<Response>, [optional<Error>      ]> ==>
+          // expected<optional<Response>, [RegisterFailureReason]>
+          .transform_error([](auto error) {
+            if (!error) {
+              return mojom::RegisterFailureReason::kInitializeUnexpected;
+            }
+
+            switch (error->status) {
+              case net::HTTP_BAD_REQUEST:  // 400
+                return mojom::RegisterFailureReason::kInitializeBadRequest;
+              case net::HTTP_UNAUTHORIZED:  // 401
+                return mojom::RegisterFailureReason::kInitializeUnauthorized;
+              default:
+                return error->status / 100 == 5  // 5xx
+                           ? mojom::RegisterFailureReason::
+                                 kInitializeServerError
+                           : mojom::RegisterFailureReason::kInitializeUnknown;
+            }
+          })
+          // expected<[optional<Response>         ], RegisterFailureReason> ==>
+          // expected<[RegisterInitializeResultPtr], RegisterFailureReason>
+          .and_then([&](auto response)
+                        -> base::expected<mojom::RegisterInitializeResultPtr,
+                                          mojom::RegisterFailureReason> {
+            if (!response) {
+              return base::unexpected(
+                  mojom::RegisterFailureReason::kInitializeUnexpected);
+            }
+
+            if (response->verification_token.empty() ||
+                response->serialized_response.empty()) {
+              return base::unexpected(
+                  mojom::RegisterFailureReason::kInitializeUnexpected);
+            }
+
+            std::string encrypted_verification_token =
+                encrypt_callback_.Run(response->verification_token);
+            if (encrypted_verification_token.empty()) {
+              return base::unexpected(
+                  mojom::RegisterFailureReason::kInitializeUnexpected);
+            }
+
+            return mojom::RegisterInitializeResult::New(
+                std::move(encrypted_verification_token),
+                std::move(response->serialized_response));
+          });
+
+  std::move(callback).Run(std::move(result));
+}
+
+void BraveAccountService::OnRegisterFinalize(
+    RegisterFinalizeCallback callback,
+    const std::string& encrypted_verification_token,
+    int response_code,
+    base::expected<std::optional<PasswordFinalize::Response>,
+                   std::optional<PasswordFinalize::Error>> reply) {
+  auto result =
+      std::move(reply)
+          // expected<optional<Response>, [optional<Error>      ]> ==>
+          // expected<optional<Response>, [RegisterFailureReason]>
+          .transform_error([](auto error) {
+            if (!error) {
+              return mojom::RegisterFailureReason::kFinalizeUnexpected;
+            }
+
+            switch (error->status) {
+              case net::HTTP_BAD_REQUEST:  // 400
+                return mojom::RegisterFailureReason::kFinalizeBadRequest;
+              case net::HTTP_UNAUTHORIZED:  // 401
+                return mojom::RegisterFailureReason::kFinalizeUnauthorized;
+              case net::HTTP_FORBIDDEN:  // 403
+                return mojom::RegisterFailureReason::kFinalizeForbidden;
+              case net::HTTP_NOT_FOUND:  // 404
+                return mojom::RegisterFailureReason::kFinalizeNotFound;
+              default:
+                return error->status / 100 == 5  // 5xx
+                           ? mojom::RegisterFailureReason::kFinalizeServerError
+                           : mojom::RegisterFailureReason::kFinalizeUnknown;
+            }
+          })
+          // expected<[optional<Response>       ], RegisterFailureReason> ==>
+          // expected<[RegisterFinalizeResultPtr], RegisterFailureReason>
+          .and_then([&](auto response)
+                        -> base::expected<mojom::RegisterFinalizeResultPtr,
+                                          mojom::RegisterFailureReason> {
+            pref_service_->SetString(prefs::kVerificationToken,
+                                     encrypted_verification_token);
+
+            return mojom::RegisterFinalizeResult::New();
+          });
+
+  std::move(callback).Run(std::move(result));
 }
 
 }  // namespace brave_account
