e9758c2fade07d71940cdbc597e5620312842a1c
Szilard Szaloki <szilardszaloki@gmail.com>
2025-09-20 19:04:42 -0600

    response.h simplified + comments

--- components/endpoint_client/response.h
+++ components/endpoint_client/response.h
@@ -9,33 +9,41 @@
 #include <concepts>
 #include <optional>
 #include <type_traits>
+#include <variant>
 
 #include "base/values.h"
 
-namespace endpoints {
+namespace endpoints::detail {
 
-template <typename>
-struct WithHeaders;
-
-namespace detail {
-
+// Concept: a type T is a ResponseBody if
+// it defines static std::optional<T> FromValue(const base::Value&).
 template <typename T>
 concept ResponseBody = requires(const base::Value& value) {
   { T::FromValue(value) } -> std::same_as<std::optional<T>>;
 };
 
-template <typename T>
-struct ResponseImpl : std::bool_constant<ResponseBody<T>> {};
+// Primary template: a type is not a Response unless
+// matched by a partial specialization below.
+template <typename>
+struct ResponseImpl : std::false_type {};
 
-template <typename... Ts>
-struct ResponseImpl<std::variant<Ts...>>
-    : std::bool_constant<sizeof...(Ts) >= 2 &&
-                         (ResponseImpl<Ts>::value && ...)> {};
+// Partial specialization: a type is a Response if
+// it satisfies ResponseBody.
+template <ResponseBody Body>
+struct ResponseImpl<Body> : std::true_type {};
 
+// Partial specialization: a std::variant<> is a Response if
+// - it has at least two alternatives, and
+// - every alternative satisfies ResponseBody.
+template <ResponseBody... Bodies>
+  requires(sizeof...(Bodies) >= 2)
+struct ResponseImpl<std::variant<Bodies...>> : std::true_type {};
+
+// Concept: a type is a Response if
+// its ResponseImpl specialization evaluates to true.
 template <typename T>
 concept Response = ResponseImpl<T>::value;
 
-}  // namespace detail
-}  // namespace endpoints
+}  // namespace endpoints::detail
 
 #endif  // BRAVE_COMPONENTS_ENDPOINT_CLIENT_RESPONSE_H_
