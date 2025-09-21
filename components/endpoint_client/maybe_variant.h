640d4d0c2ffc00dd240329ef85c3a837d5401c02
Szilard Szaloki <szilardszaloki@gmail.com>
2025-09-21 12:26:50 -0600

    maybe_variant.h comments

--- components/endpoint_client/maybe_variant.h
+++ components/endpoint_client/maybe_variant.h
@@ -11,16 +11,24 @@
 
 namespace endpoints::detail {
 
+// Primary template: would only match the empty parameter pack,
+// which is excluded by the alias' requires-clause below.
 template <typename...>
 struct MaybeVariantImpl;
 
+// Partial specialization: a single type maps to itself.
 template <typename T>
 struct MaybeVariantImpl<T> : std::type_identity<T> {};
 
+// Partial specialization: two or more types map to std::variant<>.
 template <typename... Ts>
   requires(sizeof...(Ts) > 1)
 struct MaybeVariantImpl<Ts...> : std::type_identity<std::variant<Ts...>> {};
 
+// Alias: maps
+// - sizeof...(Ts) == 1 to the single type,
+// - sizeof...(Ts) > 1 to std::variant<Ts...>,
+// Instantiation with an empty pack is blocked by the requires-clause.
 template <typename... Ts>
   requires(sizeof...(Ts) > 0)
 using MaybeVariant = typename MaybeVariantImpl<Ts...>::type;
