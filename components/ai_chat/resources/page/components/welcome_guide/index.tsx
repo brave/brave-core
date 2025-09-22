226ebd6f2b31067a712ca81d57f34167e178fa83
Douglas Daniel <douglashdaniel@gmail.com>
2025-09-22 13:57:39 -0500

    [AI Chat]: Leo Onboarding UI Update

--- components/ai_chat/resources/page/components/welcome_guide/index.tsx
+++ components/ai_chat/resources/page/components/welcome_guide/index.tsx
@@ -5,55 +5,122 @@
 
 import * as React from 'react'
 import Button from '@brave/leo/react/button'
-import { getLocale } from '$web-common/locale'
-import { useConversation } from '../../state/conversation_context'
+import Icon from '@brave/leo/react/icon'
+import classnames from '$web-common/classnames'
+import { getLocale, formatLocale } from '$web-common/locale'
+import { Url } from 'gen/url/mojom/url.mojom.m.js'
+import { useAIChat } from '../../state/ai_chat_context'
+import TrulyPrivateIllustration from './truly_private_illustration.svg'
+import ChatWithIllustration from './chat_with_illustration.svg'
+import AutomateIllustration from './automate_illustration.svg'
 import styles from './style.module.scss'
 
-function WelcomeGuide() {
-  const conversationContext = useConversation()
+const welcomeCards = [
+  {
+    title: getLocale(S.CHAT_UI_WELCOME_CARD_TRULY_PRIVATE_TITLE),
+    description: getLocale(S.CHAT_UI_WELCOME_CARD_TRULY_PRIVATE_SUBTITLE),
+    illustration: TrulyPrivateIllustration,
+  },
+  {
+    title: getLocale(S.CHAT_UI_WELCOME_CARD_TABS_AND_FILES_TITLE),
+    description: getLocale(S.CHAT_UI_WELCOME_CARD_TABS_AND_FILES_SUBTITLE),
+    illustration: ChatWithIllustration,
+  },
+  {
+    title: getLocale(S.CHAT_UI_WELCOME_CARD_AUTOMATE_WORK_TITLE),
+    description: getLocale(S.CHAT_UI_WELCOME_CARD_AUTOMATE_WORK_SUBTITLE),
+    illustration: AutomateIllustration,
+  },
+]
 
-  const summarizeNow = () => {
-    conversationContext.conversationHandler?.submitSummarizationRequest()
-  }
+const subtitle = formatLocale(S.CHAT_UI_WELCOME_GUIDE_SUBTITLE, {
+  $1: (content) => <span className={styles.subtitlePurple}>{content}</span>,
+})
+
+function WelcomeGuide() {
+  const aiChatContext = useAIChat()
+
+  // State
+  const [visibleCard, setVisibleCard] = React.useState(0)
+  const [userSelectedCard, setUserSelectedCard] = React.useState<
+    number | undefined
+  >(undefined)
+
+  // Methods
+  const handleLearnMoreClicked = React.useCallback(() => {
+    const mojomUrl = new Url()
+    mojomUrl.url = 'https://brave.com/leo/'
+
+    aiChatContext.uiHandler?.openURL(mojomUrl)
+  }, [aiChatContext.uiHandler])
+
+  // Effects
+  React.useEffect(() => {
+    // Only start auto-cycling if user hasn't selected a card
+    if (userSelectedCard !== undefined) {
+      return
+    }
+
+    const interval = setInterval(() => {
+      setVisibleCard((prevIndex) => (prevIndex + 1) % welcomeCards.length)
+    }, 10000) // 10 seconds
+
+    return () => clearInterval(interval)
+  }, [userSelectedCard])
 
   return (
-    <div className={styles.box}>
+    <div className={styles.wrapper}>
       <div className={styles.header}>
-        <h1 className={styles.title}>
+        <span className={styles.title}>
           {getLocale(S.CHAT_UI_WELCOME_GUIDE_TITLE)}
-        </h1>
-        <h2 className={styles.subtitle}>
-          {getLocale(S.CHAT_UI_WELCOME_GUIDE_SUBTITLE)}
-        </h2>
+        </span>
+        <span className={styles.subtitle}>{subtitle}</span>
       </div>
-      <div className={`${styles.card} ${styles.siteHelpCard}`}>
-        <h4 className={styles.cardTitle}>
-          {getLocale(S.CHAT_UI_WELCOME_GUIDE_SITE_HELP_CARD_TITLE)}
-        </h4>
-        {conversationContext.associatedContentInfo.length > 0 ? (
-          <>
-            <p>
-              {getLocale(S.CHAT_UI_WELCOME_GUIDE_SITE_HELP_CARD_WITH_ACTION)}
-            </p>
-            <div className={styles.actions}>
-              <Button
-                kind='outline'
-                onClick={summarizeNow}
-              >
-                {getLocale(S.CHAT_UI_SUMMARIZE_BUTTON_LABEL)}
-              </Button>
+      <div className={styles.cardWrapper}>
+        {welcomeCards.map((card, i) => {
+          const isSelected =
+            userSelectedCard !== undefined
+              ? userSelectedCard === i
+              : visibleCard === i
+          return (
+            <div
+              className={classnames({
+                [styles.card]: true,
+                [styles.cardCollapsed]: !isSelected,
+              })}
+              onClick={() => setUserSelectedCard(i)}
+              key={card.title}
+            >
+              <img
+                className={styles.cardGraphic}
+                src={card.illustration}
+              />
+              <div className={styles.cardInfo}>
+                <span className={styles.cardTitle}>{card.title}</span>
+                <span
+                  className={classnames({
+                    [styles.cardDescription]: true,
+                    [styles.contentHidden]: !isSelected,
+                  })}
+                >
+                  {card.description}
+                </span>
+              </div>
             </div>
-          </>
-        ) : (
-          <p>{getLocale(S.CHAT_UI_WELCOME_GUIDE_SITE_HELP_CARD_DESC)}</p>
-        )}
+          )
+        })}
       </div>
-      <div className={`${styles.card} ${styles.chatCard}`}>
-        <h4 className={styles.cardTitle}>
-          {getLocale(S.CHAT_UI_WELCOME_GUIDE_CHAT_CARD_TITLE)}
-        </h4>
-        <p>{getLocale(S.CHAT_UI_WELCOME_GUIDE_CHAT_CARD_DESC)}</p>
-      </div>
+      <Button
+        kind='plain-faint'
+        size='medium'
+        onClick={handleLearnMoreClicked}
+      >
+        {getLocale(S.CHAT_UI_LEARN_MORE_ABOUT_LEO_AI)}
+        <Icon
+          slot='icon-after'
+          name='launch'
+        />
+      </Button>
     </div>
   )
 }
