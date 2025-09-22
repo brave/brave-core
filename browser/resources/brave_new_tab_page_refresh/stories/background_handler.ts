53fec81bc8c3a05619f206a9f962bf042657212d
zenparsing <zenparsing@gmail.com>
2025-09-22 11:21:39 -0400

    [NTP] Isolate autocomplete results for separate search inputs

--- browser/resources/brave_new_tab_page_refresh/stories/background_handler.ts
+++ browser/resources/brave_new_tab_page_refresh/stories/background_handler.ts
@@ -8,7 +8,6 @@ import { Store } from '../lib/store'
 import {
   BackgroundState,
   BackgroundActions,
-  SponsoredImageBackground,
   SelectedBackgroundType } from '../state/background_state'
 
 function delay(ms: number) {
@@ -20,7 +19,7 @@ function delay(ms: number) {
 const sampleBackground =
     'https://brave.com/static-assets/images/coding-background-texture.jpg'
 
-const sponsoredBackgrounds: Record<string, SponsoredImageBackground | null> = {
+const sponsoredBackgrounds = {
   image: {
     wallpaperType: '',
     imageUrl: sampleBackground,
@@ -69,7 +68,8 @@ export function createBackgroundHandler(
   })
 
   store.update({
-    sponsoredRichMediaBaseUrl: 'https://brave.com'
+    sponsoredRichMediaBaseUrl:
+      new URL(sponsoredBackgrounds.richMedia.imageUrl).origin
   })
 
   return {
@@ -114,6 +114,8 @@ export function createBackgroundHandler(
 
     notifySponsoredImageLogoClicked() {},
 
-    notifySponsoredRichMediaEvent(type) {}
+    notifySponsoredRichMediaEvent(type) {
+      console.log('richMediaEvent', type)
+    }
   }
 }
