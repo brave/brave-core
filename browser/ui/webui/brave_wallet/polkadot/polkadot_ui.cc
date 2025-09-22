01936d464da0c9df8d09cc0b5453d64a5a78679c
Christian Mazakas <christian.mazakas@gmail.com>
2025-09-22 12:55:28 -0700

    add mojo support to demo

--- browser/ui/webui/brave_wallet/polkadot/polkadot_ui.cc
+++ browser/ui/webui/brave_wallet/polkadot/polkadot_ui.cc
@@ -7,8 +7,10 @@
 
 #include <string>
 
+#include "brave/browser/brave_wallet/brave_wallet_service_factory.h"
 #include "brave/browser/ui/webui/untrusted_sanitized_image_source.h"
 #include "brave/components/brave_wallet/browser/brave_wallet_constants.h"
+#include "brave/components/brave_wallet/browser/brave_wallet_service.h"
 #include "brave/components/constants/webui_url_constants.h"
 #include "brave/components/polkadot_bridge/resources/grit/polkadot_bridge_generated_map.h"
 #include "chrome/browser/profiles/profile.h"
@@ -26,7 +28,7 @@
 namespace brave_wallet {
 
 UntrustedPolkadotUI::UntrustedPolkadotUI(content::WebUI* web_ui)
-    : ui::UntrustedWebUIController(web_ui) {
+    : ui::MojoWebUIController(web_ui) {
   auto* untrusted_source = content::WebUIDataSource::CreateAndAdd(
       web_ui->GetWebContents()->GetBrowserContext(), kUntrustedPolkadotURL);
 
@@ -78,6 +80,77 @@ UntrustedPolkadotUI::UntrustedPolkadotUI(content::WebUI* web_ui)
 }
 
 UntrustedPolkadotUI::~UntrustedPolkadotUI() = default;
+
+WEB_UI_CONTROLLER_TYPE_IMPL(UntrustedPolkadotUI)
+
+void UntrustedPolkadotUI::BindInterface(
+    mojo::PendingReceiver<brave_wallet::mojom::PageHandlerFactory> receiver) {
+  page_factory_receiver_.reset();
+  page_factory_receiver_.Bind(std::move(receiver));
+}
+
+void UntrustedPolkadotUI::CreatePageHandler(
+    mojo::PendingReceiver<brave_wallet::mojom::PageHandler> page_receiver,
+    mojo::PendingReceiver<brave_wallet::mojom::WalletHandler> wallet_receiver,
+    mojo::PendingReceiver<brave_wallet::mojom::JsonRpcService>
+        json_rpc_service_receiver,
+    mojo::PendingReceiver<brave_wallet::mojom::BitcoinWalletService>
+        bitcoin_wallet_service_receiver,
+    mojo::PendingReceiver<brave_wallet::mojom::PolkadotWalletService>
+        polkadot_wallet_service_receiver,
+    mojo::PendingReceiver<brave_wallet::mojom::ZCashWalletService>
+        zcash_wallet_service_receiver,
+    mojo::PendingReceiver<brave_wallet::mojom::CardanoWalletService>
+        cardano_wallet_service_receiver,
+    mojo::PendingReceiver<brave_wallet::mojom::SwapService>
+        swap_service_receiver,
+    mojo::PendingReceiver<brave_wallet::mojom::AssetRatioService>
+        asset_ratio_service_receiver,
+    mojo::PendingReceiver<brave_wallet::mojom::KeyringService>
+        keyring_service_receiver,
+    mojo::PendingReceiver<brave_wallet::mojom::BlockchainRegistry>
+        blockchain_registry_receiver,
+    mojo::PendingReceiver<brave_wallet::mojom::TxService> tx_service_receiver,
+    mojo::PendingReceiver<brave_wallet::mojom::EthTxManagerProxy>
+        eth_tx_manager_proxy_receiver,
+    mojo::PendingReceiver<brave_wallet::mojom::SolanaTxManagerProxy>
+        solana_tx_manager_proxy_receiver,
+    mojo::PendingReceiver<brave_wallet::mojom::FilTxManagerProxy>
+        filecoin_tx_manager_proxy_receiver,
+    mojo::PendingReceiver<brave_wallet::mojom::BtcTxManagerProxy>
+        bitcoin_tx_manager_proxy_receiver,
+    mojo::PendingReceiver<brave_wallet::mojom::BraveWalletService>
+        brave_wallet_service_receiver,
+    mojo::PendingReceiver<brave_wallet::mojom::BraveWalletP3A>
+        brave_wallet_p3a_receiver,
+    mojo::PendingReceiver<brave_wallet::mojom::IpfsService>
+        ipfs_service_receiver,
+    mojo::PendingReceiver<brave_wallet::mojom::MeldIntegrationService>
+        meld_integration_service) {
+  auto* profile = Profile::FromWebUI(web_ui());
+  CHECK(profile);
+
+  page_handler_ =
+      std::make_unique<WalletPageHandler>(std::move(page_receiver), profile);
+
+  if (auto* wallet_service =
+          brave_wallet::BraveWalletServiceFactory::GetServiceForContext(
+              profile)) {
+    wallet_service->Bind(std::move(brave_wallet_service_receiver));
+    wallet_service->Bind(std::move(json_rpc_service_receiver));
+    wallet_service->Bind(std::move(bitcoin_wallet_service_receiver));
+    wallet_service->Bind(std::move(polkadot_wallet_service_receiver));
+    wallet_service->Bind(std::move(zcash_wallet_service_receiver));
+    wallet_service->Bind(std::move(cardano_wallet_service_receiver));
+    wallet_service->Bind(std::move(keyring_service_receiver));
+    wallet_service->Bind(std::move(tx_service_receiver));
+    wallet_service->Bind(std::move(eth_tx_manager_proxy_receiver));
+    wallet_service->Bind(std::move(solana_tx_manager_proxy_receiver));
+    wallet_service->Bind(std::move(filecoin_tx_manager_proxy_receiver));
+    wallet_service->Bind(std::move(bitcoin_tx_manager_proxy_receiver));
+    wallet_service->Bind(std::move(brave_wallet_p3a_receiver));
+  }
+}
 
 std::unique_ptr<content::WebUIController>
 UntrustedPolkadotUIConfig::CreateWebUIController(content::WebUI* web_ui,
--- components/brave_wallet_ui/polkadot/polkadot.ts
+++ components/brave_wallet_ui/polkadot/polkadot.ts
@@ -1,12 +1,34 @@
+import 'gen/brave/components/brave_wallet/common/brave_wallet.mojom.m.js'
+import { getAPIProxy } from '../common/async/bridge'
+// import getWalletPageApiProxy from '../page/wallet_page_api_proxy'
+
 // @ts-expect-error
 import { get_signature } from 'chrome-untrusted://resources/brave/polkadot_bridge_wasm.bundle.js'
+
+// let walletPageApiProxyInstance: WalletPageApiProxy
+
+// function getWalletPageApiProxy() {
+//   if (!walletPageApiProxyInstance) {
+//     walletPageApiProxyInstance = new WalletPageApiProxy()
+//   }
+//   return walletPageApiProxyInstance
+// }
+
+const apiProxy = getAPIProxy();
 
 window.addEventListener('message', (msgEvent) => {
   try {
     if (!msgEvent.source) { return; }
 
     msgEvent.source.postMessage(get_signature(), { targetOrigin: msgEvent.origin });
-    window.parent.postMessage(get_signature());
+
+    (async () => {
+      const polkadotWalletService = apiProxy.polkadotWalletService;
+      const network = await polkadotWalletService.getNetworkName();
+      console.log('retrieved the network!');
+      console.log(network);
+    })();
+
   } catch (err) {
     console.error(err);
   }
