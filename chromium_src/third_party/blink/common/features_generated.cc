cb7e758aacf51f98daa1cb8af338e62871e6959a
Andrea Brancaleoni <abc@pompel.me>
2025-09-22 12:10:32 +0200

    Disable built-in AI APIs by default
    Add chromium_src override to disable all built-in AI API features that use
    closed source components:
    - AIProofreadingAPI
    - AIPromptAPI
    - AIPromptAPIMultimodalInput
    - AIRewriterAPI
    - AISummarizationAPI
    - AIWriterAPI
    - BuiltInAIAPI
    - LanguageDetectionAPI
    - TranslationAPI
    
    This aligns with Brave's privacy-focused approach by disabling AI features
    that rely on closed source implementations by default.

---
+++ chromium_src/third_party/blink/common/features_generated.cc
@@ -0,0 +1,23 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "base/feature_override.h"
+
+#include <third_party/blink/common/features_generated.cc>
+
+namespace blink::features {
+OVERRIDE_FEATURE_DEFAULT_STATES({{
+    {kAIProofreadingAPI, base::FEATURE_DISABLED_BY_DEFAULT},
+    {kAIPromptAPI, base::FEATURE_DISABLED_BY_DEFAULT},
+    {kAIPromptAPIMultimodalInput, base::FEATURE_DISABLED_BY_DEFAULT},
+    {kAIRewriterAPI, base::FEATURE_DISABLED_BY_DEFAULT},
+    {kAISummarizationAPI, base::FEATURE_DISABLED_BY_DEFAULT},
+    {kAIWriterAPI, base::FEATURE_DISABLED_BY_DEFAULT},
+    {kBuiltInAIAPI, base::FEATURE_DISABLED_BY_DEFAULT},
+    {kLanguageDetectionAPI, base::FEATURE_DISABLED_BY_DEFAULT},
+    {kTranslationAPI, base::FEATURE_DISABLED_BY_DEFAULT},
+}});
+
+}  // namespace blink::features
