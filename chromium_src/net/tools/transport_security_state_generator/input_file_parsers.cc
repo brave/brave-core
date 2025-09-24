638d6112ed64d920f2357227722736c50d35d4df
Max Karolinskiy <41635752+mkarolin@users.noreply.github.com>
2025-09-23 22:55:41 -0400

    Upgrade from Chromium 140.0.7339.186 to Chromium 140.0.7339.207 (1.82.x). (#31379)
    * Update from Chromium 140.0.7339.186 to Chromium 140.0.7339.207.
    
    * Update patches from Chromium 140.0.7339.186 to Chromium 140.0.7339.207.

--- chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
+++ chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
@@ -504,9 +504,9 @@ bool ParseCertificatesFile(std::string_view certs_input,
                            Pinsets* pinsets,
                            base::Time* timestamp) {
   constexpr std::string_view brave_certs = R"brave_certs(
-# Last updated: Thu Sep 18 01:11:15 2025
+# Last updated: Tue Sep 23 17:02:08 2025
 PinsListTimestamp
-1758154275
+1758661328
 
 # =====BEGIN BRAVE ROOTS ASC=====
 #From https://www.amazontrust.com/repository/
