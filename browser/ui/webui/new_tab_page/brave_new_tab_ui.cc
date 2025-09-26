13970e6dcbdaec60f6e6063e47320ebe2c338cad
Claudio DeSouza <cdesouza@brave.com>
2025-09-26 15:23:36 +0100

    [cr142] Fixing `RealboxHandler` ctor call
    There has been some split in this type, with a new subclass introduced,
    namely, `WebUiOmniboxHandler`. This has slgihtly changed the arguments
    passed during construction. This change corrects our constructor calls.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/ef40fb8a5cda8a1ba27e316b17370427d7985a00
    
    commit ef40fb8a5cda8a1ba27e316b17370427d7985a00
    Author: manukh <manukh@chromium.org>
    Date:   Tue Sep 23 07:48:12 2025 -0700
    
        [omnibox][webui] Split `RealboxHandler` into `WebUiOmniboxHandler`
    
        `RealboxHandler` had some code that was only needed for realbox, and
        some code that was only needed for WebUi Omnibox. Plus, as we bring the
        WebUi Omnibox up to speed, we're going to be adding bunch of code that's
        not relevant to realbox. We already have separate searchbox subclasses
        for each UI, so it seemed cleaner to add another subclass for this new
        UI instead of reusing the realbox subclass.
    
        Removes some Webui Omnibox-only code from the realbox handler. Likewise,
        removes some realbox-only code from the cloned Webui Omnibox handler.
    
        Also cleans up some includes.
    
        Bug: 445510209
        Change-Id: I72f6bf1313045a1fda70ae7f28cc1411e2a5be0e
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6967692
        Auto-Submit: manuk hovanesian <manukh@chromium.org>
        Reviewed-by: Nihar Majmudar <niharm@google.com>
        Commit-Queue: manuk hovanesian <manukh@chromium.org>
        Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1519329}

--- browser/ui/webui/new_tab_page/brave_new_tab_ui.cc
+++ browser/ui/webui/new_tab_page/brave_new_tab_ui.cc
@@ -207,8 +207,7 @@ void BraveNewTabUI::BindInterface(
 
   realbox_handler_ = std::make_unique<RealboxHandler>(
       std::move(pending_page_handler), profile, web_ui()->GetWebContents(),
-      /*metrics_reporter=*/nullptr,
-      /*omnibox_controller=*/nullptr);
+      /*metrics_reporter=*/nullptr);
 }
 
 #if BUILDFLAG(ENABLE_BRAVE_VPN)
