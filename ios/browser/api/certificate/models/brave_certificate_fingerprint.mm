abe54d79bcfd3ebd21dccfa8f99324f732ec1674
Emerick Rogul <erogul@brave.com>
2025-09-27 07:21:17 -0400

    [cr142][ios] `SecureHash` removed non-span APIs
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/3e16d042a48978d92fdbdac4ef1e197944db8cc6
    
    commit 3e16d042a48978d92fdbdac4ef1e197944db8cc6
    Author: Elly <ellyjones@chromium.org>
    Date:   Wed Sep 24 07:50:18 2025 -0700
    
        crypto/secure_hash: remove non-span APIs
    
        These now have no remaining clients except their own tests.
    
        Fixed: 364687923
        Change-Id: Ib107308b39f76a572f803972dfc53294fec0b81e
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6866787
        Reviewed-by: Matt Mueller <mattm@chromium.org>
        Commit-Queue: Elly FJ <ellyjones@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1519980}

--- ios/browser/api/certificate/models/brave_certificate_fingerprint.mm
+++ ios/browser/api/certificate/models/brave_certificate_fingerprint.mm
@@ -4,9 +4,12 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/ios/browser/api/certificate/models/brave_certificate_fingerprint.h"
+
+#include "base/containers/span.h"
 #include "base/hash/sha1.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/sys_string_conversions.h"
+#include "base/types/fixed_array.h"
 #include "brave/ios/browser/api/certificate/models/brave_certificate_enums.h"
 #include "brave/ios/browser/api/certificate/utils/brave_certificate_utils.h"
 #include "brave/ios/browser/api/certificate/utils/brave_certificate_x509_utils.h"
@@ -30,13 +33,15 @@
       } break;
 
       case BraveFingerprintType_SHA256: {
-        std::string data(crypto::kSHA256Length, 0);
+        base::FixedArray<uint8_t> data(crypto::kSHA256Length);
+        base::FixedArray<uint8_t> cert_bytes(CFDataGetLength(cert_data));
+        std::copy_n(CFDataGetBytePtr(cert_data), CFDataGetLength(cert_data),
+                    cert_bytes.data());
         std::unique_ptr<crypto::SecureHash> secure_hash(
             crypto::SecureHash::Create(crypto::SecureHash::SHA256));
 
-        secure_hash->Update(CFDataGetBytePtr(cert_data),
-                            CFDataGetLength(cert_data));
-        secure_hash->Finish(std::data(data), data.size());
+        secure_hash->Update(base::span<const uint8_t>(cert_bytes));
+        secure_hash->Finish(data);
 
         _fingerprintHexEncoded =
             base::SysUTF8ToNSString(base::HexEncode(data.data(), data.size()));
