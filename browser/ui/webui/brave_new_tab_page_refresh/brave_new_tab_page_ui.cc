82a00c6b27520ffee89ee78ae451f513750a42c9
Claudio DeSouza <cdesouza@brave.com>
2025-09-30 00:29:41 +0100

    [cr142] `RealboxHandler` ctor taking two new args
    This class now can take in its ctor a `ComposeboxQueryController`, and
    `ComposeboxMetricsRecorder`. These values are optional, and the current
    instances of this class in Brave are passing `nullptr` to keep the
    behaviour as is.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/1a92fdbad56f0e686c9f0c8162f8aaccc4362df3
    
    commit 1a92fdbad56f0e686c9f0c8162f8aaccc4362df3
    Author: Hannah Collins <hannahrogers@google.com>
    Date:   Sun Sep 28 19:36:12 2025 -0700
    
        [ntp-next] Have the RealboxHandler extend the ContextualSearchboxHandler
    
        This ensures the realbox inherits all contextual search methods. This
        will be needed for NTP next, where contextual search will be supported
        from the realbox.
    
        Also moved the contextual search handler tests to a separate
        contextual_searchbox_handler test file.
    
        Fixed: 445224889
        Change-Id: I35790eacc8c7cc4554c06c32e97123463b8bbbc5
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6986827
        Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
        Reviewed-by: Nihar Majmudar <niharm@google.com>
        Commit-Queue: Hannah Collins <hannahrogers@google.com>
        Cr-Commit-Position: refs/heads/main@{#1521957}

--- browser/ui/webui/brave_new_tab_page_refresh/brave_new_tab_page_ui.cc
+++ browser/ui/webui/brave_new_tab_page_refresh/brave_new_tab_page_ui.cc
@@ -105,7 +105,8 @@ void BraveNewTabPageUI::BindInterface(
 void BraveNewTabPageUI::BindInterface(
     mojo::PendingReceiver<searchbox::mojom::PageHandler> receiver) {
   realbox_handler_ = std::make_unique<RealboxHandler>(
-      std::move(receiver), Profile::FromWebUI(web_ui()),
+      std::move(receiver), /*query_controller=*/nullptr,
+      /*composebox_metrics_recorder=*/nullptr, Profile::FromWebUI(web_ui()),
       web_ui()->GetWebContents(), /*metrics_reporter=*/nullptr);
 }
 
