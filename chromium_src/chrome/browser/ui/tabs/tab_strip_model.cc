ca2b79357640df7866b6e20edbe272e8f8cee640
sangwoo ko <sangwoo108@gmail.com>
2025-10-01 08:34:39 +0900

    Remove unncessary change

--- chromium_src/chrome/browser/ui/tabs/tab_strip_model.cc
+++ chromium_src/chrome/browser/ui/tabs/tab_strip_model.cc
@@ -4,12 +4,9 @@
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "chrome/browser/ui/views/tabs/dragging/tab_drag_controller.h"
-#include "components/tabs/public/tab_strip_collection.h"
 
 #define DraggingTabsSession DraggingTabsSessionChromium
-
 #include <chrome/browser/ui/tabs/tab_strip_model.cc>  // IWYU pragma: export
-
 #undef DraggingTabsSession
 
 void TabStripModel::CloseSelectedTabsWithSplitView() {
