46fb517256f1d3f0c057bbbe0e3aa6278e5e0ee9
Emerick Rogul <erogul@brave.com>
2025-09-30 12:35:41 -0400

    [cr142] `kPolicySources` moved to header, fix `chromium_src` override
    `kPolicySources` used to be defined in the associated source file, but it was
    moved to the header file. Update the `chromium_src` override to reflect that
    change.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/441f253b5c97b5caf118cebbea9b29a9756452f9
    
    commit 441f253b5c97b5caf118cebbea9b29a9756452f9
    Author: Jan Keitel <jkeitel@google.com>
    Date:   Fri Sep 26 10:03:24 2025 -0700
    
        Avoid unsafe buffer usage in policy_conversions_client.cc
    
        As an aside, the CL also modernizes some constant usage. No
        functionality changes.
    
        Bug: 435317390
        Change-Id: I9b3ec28e21624867d4f7e4315a0f1076e0623a55
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6903280
        Commit-Queue: Jan Keitel <jkeitel@google.com>
        Reviewed-by: Artem Sumaneev <asumaneev@google.com>
        Auto-Submit: Jan Keitel <jkeitel@google.com>
        Cr-Commit-Position: refs/heads/main@{#1521372}

--- chromium_src/components/policy/core/browser/policy_conversions.h
+++ chromium_src/components/policy/core/browser/policy_conversions.h
@@ -3,6 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+#ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_POLICY_CORE_BROWSER_POLICY_CONVERSIONS_H_
+#define BRAVE_CHROMIUM_SRC_COMPONENTS_POLICY_CORE_BROWSER_POLICY_CONVERSIONS_H_
+
 #include "components/grit/brave_components_strings.h"
 #include "components/policy/core/common/policy_types.h"
 #include "components/strings/grit/components_strings.h"
@@ -19,10 +22,11 @@ constexpr int kBravePolicySourceRestrictedManagedGuest =
   , {                                                               \
     "policySourceBrave", IDS_POLICY_SOURCE_BRAVE
 
-#include <components/policy/core/browser/policy_conversions.cc>  // IWYU pragma: export
+#include <components/policy/core/browser/policy_conversions.h>  // IWYU pragma: export
 
 #undef IDS_POLICY_SOURCE_RESTRICTED_MANAGED_GUEST_SESSION_OVERRIDE
-#undef IDS_POLICY_SOURCE_ENTERPRISE_DEFAULT
 
 #define IDS_POLICY_SOURCE_RESTRICTED_MANAGED_GUEST_SESSION_OVERRIDE \
   kBravePolicySourceRestrictedManagedGuest
+
+#endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_POLICY_CORE_BROWSER_POLICY_CONVERSIONS_H_
