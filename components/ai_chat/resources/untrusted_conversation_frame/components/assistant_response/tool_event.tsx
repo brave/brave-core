d84f38029d97b0648ead78c18f1404213bb4702c
Pete Miller <miller.pete@gmail.com>
2025-09-30 12:31:05 -0700

    [AI Chat] [Content Agent] TODO Tool

--- components/ai_chat/resources/untrusted_conversation_frame/components/assistant_response/tool_event.tsx
+++ components/ai_chat/resources/untrusted_conversation_frame/components/assistant_response/tool_event.tsx
@@ -8,6 +8,7 @@ import classnames from '$web-common/classnames'
 import * as Mojom from '../../../common/mojom'
 import { getToolLabel } from './get_tool_label'
 import ToolEventContentUserChoice from './tool_event_content_user_choice'
+import ToolEventTodos from './tool_event_todos'
 import styles from './tool_event.module.scss'
 
 interface Props {
@@ -105,6 +106,10 @@ function ToolEventContent(
 
   if (toolUseEvent.toolName === Mojom.USER_CHOICE_TOOL_NAME) {
     component = ToolEventContentUserChoice
+  }
+
+  if (toolUseEvent.toolName === Mojom.TODO_TOOL_NAME) {
+    component = ToolEventTodos
   }
 
   if (component) {
