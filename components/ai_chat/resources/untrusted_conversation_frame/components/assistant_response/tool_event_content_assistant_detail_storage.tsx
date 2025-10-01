15b7efac2427dae15d72271921e03ece6a5871a6
Pete Miller <miller.pete@gmail.com>
2025-10-01 02:18:52 -0700

    [AI Chat] [Content Agent] Assistant Detail Tool
    A simple tool for the LLM to save information it deems important before the context is truncated due to the size of page content results for previous content agent tool use requests. The tool itself doesn't do anything since the content itself is stored in the conversation history and context provided back to the LLM simply by the LLM submitting the data as tool input.
    For the UI we simply show the label that the AI is storing information. When clicked, the information is shown and is streamed in if the tool use request is still being received in chunks.

---
+++ components/ai_chat/resources/untrusted_conversation_frame/components/assistant_response/tool_event_content_assistant_detail_storage.tsx
@@ -0,0 +1,35 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import * as React from 'react'
+import { getLocale } from '$web-common/locale'
+import type { ToolComponent, ToolUseContent } from './tool_event'
+
+const ToolEventContentAssistantDetailStorage: ToolComponent = (props) => {
+  const content: ToolUseContent = {
+    // TODO: translation
+    toolLabel: getLocale(S.CHAT_UI_TOOL_LABEL_ASSISTANT_DETAIL_STORAGE),
+    expandedContent: null,
+  }
+
+  const detailText = React.useMemo(() => {
+    // If we have parsed input, use that
+    if (props.toolInput?.information) {
+      return props.toolInput.information
+    }
+    // Otherwise use the raw input string and strip the "{"information": "" part
+    // from the start of the string
+    if (props.toolUseEvent.argumentsJson) {
+      return props.toolUseEvent.argumentsJson.substring(16)
+    }
+    return ''
+  }, [props.toolInput?.information, props.toolUseEvent.argumentsJson])
+
+  content.expandedContent = detailText
+
+  return props.children(content)
+}
+
+export default ToolEventContentAssistantDetailStorage
