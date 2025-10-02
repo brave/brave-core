6788d8a3326f67e01d685cb14f7880839ec0cff4
Jay Harris <jay.harris@outlook.co.nz>
2025-10-03 08:32:23 +1300

    [AI Chat]: Don't load the ColorChangeUpdater on Android

--- ui/webui/resources/leo/web_components.ts
+++ ui/webui/resources/leo/web_components.ts
@@ -50,7 +50,9 @@ declare global {
 
 window.leoIcons = new Set(Object.keys(iconsMeta.icons))
 
-import('//resources/cr_components/color_change_listener/colors_css_updater.js' as any).then(({ ColorChangeUpdater }) => {
-  ColorChangeUpdater.forDocument().start();
-});
-
+// <if expr="not is_android and not is_ios">
+import('//resources/cr_components/color_change_listener/colors_css_updater.js' as any)
+  .then(({ ColorChangeUpdater }) => {
+    ColorChangeUpdater.forDocument().start();
+  });
+// </if>
