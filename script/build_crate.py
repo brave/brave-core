7fcdcdb2016e2c21d9d1fe86be808f34f8918d5e
Brian Johnson <34129+bridiver@users.noreply.github.com>
2025-10-05 16:27:13 -0700

    make sure cargo uses the correct rustc

--- script/build_crate.py
+++ script/build_crate.py
@@ -12,11 +12,13 @@ import subprocess
 parser = argparse.ArgumentParser(
     description='Build crates in //brave/tools/crates/config.gni')
 parser.add_argument('--temp_dir_path', required=True)
+parser.add_argument('--rust_path', required=True)
 args, cargo_args = parser.parse_known_args()
 env = os.environ.copy()
 if platform.system() == "Windows":
     env['TEMP'] = env['TMP'] = args.temp_dir_path
 else:
     env["TMPDIR"] = args.temp_dir_path
+env["PATH"] = args.rust_path + os.pathsep + env["PATH"]
 subprocess.check_call(cargo_args, env=env)
 Path(f'{args.temp_dir_path}/.stamp').touch()
