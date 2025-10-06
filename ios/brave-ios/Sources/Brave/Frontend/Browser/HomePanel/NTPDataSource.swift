4e55d4a3bc1cda1baab695b2ee0ae08b776e135c
Aleksei Seren <aseren@brave.com>
2025-10-06 15:59:56 -0500

    WIP: RichNTT iOS

--- ios/brave-ios/Sources/Brave/Frontend/Browser/HomePanel/NTPDataSource.swift
+++ ios/brave-ios/Sources/Brave/Frontend/Browser/HomePanel/NTPDataSource.swift
@@ -20,6 +20,19 @@ enum NTPWallpaper {
     return nil
   }
 
+  var richNewTabTakeoverURL: URL? {
+    // TODO(aseren): Construct this URL from parsed creative parameters.
+    return URL(
+      string:
+        "chrome://new-tab-takeover-page?creativeInstanceId=00000000-0000-4000-8000-000000000004&placementId=1234567890"
+    )
+
+    // if case .sponsoredMedia(let background) = self {
+    //   return background.richNewTabTakeoverURL
+    // }
+    // return nil
+  }
+
   var backgroundImage: UIImage? {
     let imagePath: URL
     switch self {
@@ -299,5 +312,13 @@ extension NTPBackgroundImage {
 extension NTPSponsoredImageBackground {
   var isVideoFile: Bool {
     imagePath.pathExtension == "mp4"
+  }
+
+  var richNewTabTakeoverURL: URL? {
+    // TODO(aseren): Construct this URL from parsed creative parameters.
+    return URL(
+      string:
+        "chrome://new-tab-takeover-page?creativeInstanceId=00000000-0000-4000-8000-000000000004&placementId=1234567890"
+    )
   }
 }
