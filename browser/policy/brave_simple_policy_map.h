1aa499b9766a1e0d3620bcb1a424414688d7e5f7
Kyle Hickinson <git@kylehickinson.com>
2025-10-08 14:23:54 -0400

    [CodeHealth] Remove `enable_playlist` gn arg
    This arg was now always true so there is no need for it anymore

--- browser/policy/brave_simple_policy_map.h
+++ browser/policy/brave_simple_policy_map.h
@@ -16,7 +16,7 @@
 #include "brave/components/debounce/core/common/pref_names.h"
 #include "brave/components/ipfs/buildflags/buildflags.h"
 #include "brave/components/p3a/pref_names.h"
-#include "brave/components/playlist/core/common/buildflags/buildflags.h"
+#include "brave/components/playlist/core/common/pref_names.h"
 #include "brave/components/speedreader/common/buildflags/buildflags.h"
 #include "brave/components/tor/buildflags/buildflags.h"
 #include "build/build_config.h"
@@ -43,10 +43,6 @@
 
 #if BUILDFLAG(ENABLE_SPEEDREADER)
 #include "brave/components/speedreader/speedreader_pref_names.h"
-#endif
-
-#if BUILDFLAG(ENABLE_PLAYLIST)
-#include "brave/components/playlist/core/common/pref_names.h"
 #endif
 
 #if BUILDFLAG(ENABLE_BRAVE_WAYBACK_MACHINE)
@@ -83,10 +79,8 @@ inline constexpr PolicyToPreferenceMapEntry kBraveSimplePolicyMap[] = {
      base::Value::Type::BOOLEAN},
     {policy::key::kBraveStatsPingEnabled, kStatsReportingEnabled,
      base::Value::Type::BOOLEAN},
-#if BUILDFLAG(ENABLE_PLAYLIST)
     {policy::key::kBravePlaylistEnabled, playlist::kPlaylistEnabledPref,
      base::Value::Type::BOOLEAN},
-#endif
     {policy::key::kBraveWebDiscoveryEnabled, kWebDiscoveryEnabled,
      base::Value::Type::BOOLEAN},
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
--- renderer/brave_content_renderer_client.cc
+++ renderer/brave_content_renderer_client.cc
@@ -18,7 +18,8 @@
 #include "brave/components/brave_vpn/common/buildflags/buildflags.h"
 #include "brave/components/brave_wallet/common/features.h"
 #include "brave/components/cosmetic_filters/renderer/cosmetic_filters_js_render_frame_observer.h"
-#include "brave/components/playlist/core/common/buildflags/buildflags.h"
+#include "brave/components/playlist/content/renderer/playlist_render_frame_observer.h"
+#include "brave/components/playlist/core/common/features.h"
 #include "brave/components/safe_builtins/renderer/safe_builtins.h"
 #include "brave/components/script_injector/renderer/script_injector_render_frame_observer.h"
 #include "brave/components/skus/common/features.h"
@@ -58,11 +59,6 @@
 #include "brave/components/brave_mobile_subscription/renderer/android/subscription_render_frame_observer.h"
 #endif  // BUILDFLAG(IS_ANDROID)
 #endif  // BUILDFLAG(ENABLE_BRAVE_VPN)
-
-#if BUILDFLAG(ENABLE_PLAYLIST)
-#include "brave/components/playlist/content/renderer/playlist_render_frame_observer.h"
-#include "brave/components/playlist/core/common/features.h"
-#endif
 
 #if BUILDFLAG(ENABLE_WIDEVINE)
 #include "media/base/key_system_info.h"
@@ -198,7 +194,6 @@ void BraveContentRendererClient::RenderFrameCreated(
   }
 #endif
 
-#if BUILDFLAG(ENABLE_PLAYLIST)
   if (base::FeatureList::IsEnabled(playlist::features::kPlaylist) &&
       !process_state::IsIncognitoProcess()) {
     new playlist::PlaylistRenderFrameObserver(
@@ -207,7 +202,6 @@ void BraveContentRendererClient::RenderFrameCreated(
         }),
         ISOLATED_WORLD_ID_BRAVE_INTERNAL);
   }
-#endif
 
   if (ai_chat::features::IsAIChatEnabled() &&
       !process_state::IsIncognitoProcess()) {
@@ -248,28 +242,24 @@ void BraveContentRendererClient::RunScriptsAtDocumentStart(
     observer->RunScriptsAtDocumentStart();
   }
 
-#if BUILDFLAG(ENABLE_PLAYLIST)
   if (base::FeatureList::IsEnabled(playlist::features::kPlaylist)) {
     if (auto* playlist_observer =
             playlist::PlaylistRenderFrameObserver::Get(render_frame)) {
       playlist_observer->RunScriptsAtDocumentStart();
     }
   }
-#endif
 
   ChromeContentRendererClient::RunScriptsAtDocumentStart(render_frame);
 }
 
 void BraveContentRendererClient::RunScriptsAtDocumentEnd(
     content::RenderFrame* render_frame) {
-#if BUILDFLAG(ENABLE_PLAYLIST)
   if (base::FeatureList::IsEnabled(playlist::features::kPlaylist)) {
     if (auto* playlist_observer =
             playlist::PlaylistRenderFrameObserver::Get(render_frame)) {
       playlist_observer->RunScriptsAtDocumentEnd();
     }
   }
-#endif
 
   ChromeContentRendererClient::RunScriptsAtDocumentEnd(render_frame);
 }
