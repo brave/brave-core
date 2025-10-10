9a44e88ce904aad1bbc66d9c4d8cb5d80e0d98e3
zenparsing <zenparsing@gmail.com>
2025-10-10 10:54:59 -0400

    [NTP] Import modern searchbox mojo module

--- browser/resources/brave_new_tab_page_refresh/state/search_state.ts
+++ browser/resources/brave_new_tab_page_refresh/state/search_state.ts
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-import * as mojom from 'gen/components/omnibox/browser/searchbox.mojom.m'
+import * as mojom from 'chrome://resources/mojo/components/omnibox/browser/searchbox.mojom-webui.js'
 
 export const braveSearchHost = 'search.brave.com'
 
