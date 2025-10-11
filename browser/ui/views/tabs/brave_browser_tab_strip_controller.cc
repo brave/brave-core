42d5d3d66416856d167ec1ce05e3784d72f6502c
Simon Hong <shong@brave.com>
2025-10-11 12:13:13 +0900

    Handle close split tab from tab context menu

--- browser/ui/views/tabs/brave_browser_tab_strip_controller.cc
+++ browser/ui/views/tabs/brave_browser_tab_strip_controller.cc
@@ -13,6 +13,7 @@
 #include "brave/browser/ui/views/tabs/brave_tab_strip.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/tabs/tab_strip.h"
+#include "components/tabs/public/tab_interface.h"
 
 BraveBrowserTabStripController::BraveBrowserTabStripController(
     TabStripModel* model,
@@ -62,6 +63,21 @@ void BraveBrowserTabStripController::ShowContextMenuForTab(
 void BraveBrowserTabStripController::ExecuteCommandForTab(
     TabStripModel::ContextMenuCommand command_id,
     const Tab* tab) {
-  // TODO: Handle close tab command for closing only |tab| in split tab.
+  const std::optional<int> model_index = tabstrip_->GetModelIndexOf(tab);
+  if (!model_index.has_value()) {
+    return BrowserTabStripController::ExecuteCommandForTab(command_id, tab);
+  }
+
+  auto* tab_interface = model_->GetTabAtIndex(*model_index);
+  if (command_id == TabStripModel::CommandCloseTab &&
+      tab_interface->IsSplit()) {
+    // If |tab| is in split tab and selection model size is 2,
+    // it means there is no selected tab.
+    if (model_->selection_model().size() == 2) {
+      tab_interface->Close();
+      return;
+    }
+  }
+
   BrowserTabStripController::ExecuteCommandForTab(command_id, tab);
 }
