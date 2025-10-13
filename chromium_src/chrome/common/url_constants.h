ce7f20cf64da4f14f8864a989fdbe57724fb9fc0
Claudio DeSouza <cdesouza@brave.com>
2025-10-13 22:23:52 +0100

    [cr143] `kWalletPassesPageURL` stub
    This is a Google Wallet url, which brave does not support.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/d06de0e3a20ce3de8f710b75beff2916af6427d7
    
    commit d06de0e3a20ce3de8f710b75beff2916af6427d7
    Author: Bruno Braga <brunobraga@google.com>
    Date:   Wed Oct 8 10:14:26 2025 -0700
    
        Reland "[AutofillAi] Walletable Entities have icon and link in management page"
    
        This reverts commit 8a098e225d05ed2977b03214bd5b798bd81230c0.
    
        Reason for revert: The issue with the original CL was likely that
        the test was not taking into account the difference between
        branded an non-branded builds. The branded builds uses an internal
        Google Wallet icon, while the non-branded one uses a placeholder
        string. The test is now fixed to check for a shared element
        (icon button) that exists in both branded and non-branded builds.
    
        Original change's description:
        > Revert "[AutofillAi] Walletable Entities have icon and link in management page"
        >
        > This reverts commit dddb1559416d4e4a4edf9f5b65af62532d4f92a6.
        >
        > Reason for revert: Suspected this broke SettingsTest.AutofillAiSection__AutofillAiSectionUiTest_AutofillAiWalletEntitiesDisplayWalletText - first failing build at https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/53891/overview
        >
        > Original change's description:
        > > [AutofillAi] Walletable Entities have icon and link in management page
        > >
        > > screenshot (non branded): https://screenshot.googleplex.com/C2akg5qAvV2Ckoc
        > > screenshot (branded):
        > > https://screenshot.googleplex.com/3aiVkUxRmfpf8Hp
        > >
        > > Bug: 445677971
        > > Change-Id: I6ab9dc5bec5d38edc7b7acb78f1808e4c3f52fff
        > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6968624
        > > Auto-Submit: Bruno Braga <brunobraga@google.com>
        > > Reviewed-by: Jan Keitel <jkeitel@google.com>
        > > Commit-Queue: Jan Keitel <jkeitel@google.com>
        > > Cr-Commit-Position: refs/heads/main@{#1522739}
        >
        > Bug: 445677971
        > No-Presubmit: true
        > No-Tree-Checks: true
        > No-Try: true
        > Change-Id: Id49860270fc4a6da0429d6a79c487dcfba3eea3b
        > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6999452
        > Reviewed-by: Ravjit Uppal <ravjit@chromium.org>
        > Commit-Queue: Robert Flack <flackr@chromium.org>
        > Owners-Override: Robert Flack <flackr@chromium.org>
        > Auto-Submit: Robert Flack <flackr@chromium.org>
        > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
        > Cr-Commit-Position: refs/heads/main@{#1522823}
    
        Bug: 445677971
        Change-Id: I013c3d16ff568028bad92e1074a04ea466291c2d
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7002760
        Reviewed-by: Tim <tjudkins@chromium.org>
        Commit-Queue: Bruno Braga <brunobraga@google.com>
        Reviewed-by: Norge Vizcay <vizcay@google.com>
        Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1526998}

--- chromium_src/chrome/common/url_constants.h
+++ chromium_src/chrome/common/url_constants.h
@@ -508,6 +508,9 @@ inline constexpr char16_t kHistorySearchV2SettingURL[] =
 inline constexpr char kWallpaperSearchLearnMorePageURL[] =
     "https://support.brave.app/";
 
+// The URL for the passed in Google Wallet.
+inline constexpr char kWalletPassesPageURL[] = "https://support.brave.app/";
+
 // The URL for the "Learn more" page for Wallpaper Search for managed users.
 inline constexpr char kWallpaperSearchLearnMorePageManagedURL[] =
     "https://support.brave.app/";
