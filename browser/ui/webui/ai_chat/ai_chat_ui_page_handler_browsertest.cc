5af3ea4baff00eef6669523581bbb663c058cba3
Jay Harris <jay.harris@outlook.co.nz>
2025-10-13 20:07:06 +1300

    [AI Chat]: Destroy conversations at Shutdown

--- browser/ui/webui/ai_chat/ai_chat_ui_page_handler_browsertest.cc
+++ browser/ui/webui/ai_chat/ai_chat_ui_page_handler_browsertest.cc
@@ -19,6 +19,8 @@
 #include "brave/components/ai_chat/core/browser/tab_tracker_service.h"
 #include "brave/components/ai_chat/core/common/mojom/tab_tracker.mojom.h"
 #include "brave/components/constants/brave_paths.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/chrome_test_utils.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/platform_browser_test.h"
@@ -250,6 +252,33 @@ IN_PROC_BROWSER_TEST_F(AIChatUIPageHandlerBrowserTest, ProcessImageFile) {
   EXPECT_EQ(valid_result->type, ai_chat::mojom::UploadedFileType::kImage);
   EXPECT_GT(valid_result->data.size(), 0u);
   EXPECT_EQ(valid_result->filesize, valid_result->data.size());
+}
+
+IN_PROC_BROWSER_TEST_F(AIChatUIPageHandlerBrowserTest,
+                       AssociateURLDoesNotCrashShutdown) {
+  auto* ai_chat_contents = web_contents();
+  ASSERT_TRUE(ai_chat_contents);
+  EXPECT_EQ(ai_chat_contents->GetLastCommittedURL().scheme(),
+            content::kChromeUIScheme);
+
+  OpenNewTab();
+
+  auto* contents_to_associate = web_contents();
+  ASSERT_TRUE(contents_to_associate);
+
+  // Set the NeedsReload flag so the Association doesn't happen immediately
+  contents_to_associate->GetController().SetNeedsReload();
+
+  auto* page_handler = GetPageHandler(ai_chat_contents);
+  auto* service =
+      AIChatServiceFactory::GetForBrowserContext(browser()->profile());
+  auto* conversation = service->CreateConversation();
+
+  page_handler->AssociateUrlContent(GURL("https://example.com"), "Example",
+                                    conversation->get_conversation_uuid());
+  EXPECT_EQ(
+      conversation->associated_content_manager()->GetAssociatedContent().size(),
+      1u);
 }
 
 }  // namespace ai_chat
