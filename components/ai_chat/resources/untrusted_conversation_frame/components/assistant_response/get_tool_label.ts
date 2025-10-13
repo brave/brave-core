e2d925a84895dfd89f0b80aeb4ec17079664109e
Pete Miller <miller.pete@gmail.com>
2025-10-13 12:24:32 -0700

    [AI Chat] Tool Event UI simplification (#31774)
    * [AI Chat] Tool Event UI simplification
    
    This removes the "timeline" icons and line from tool events. They will be re-introduced with the Task component which groups tool events _and_ all other events by task item.
    Instead, Tool events will be rendered more simply. We still have a wrapper element to perform common functionality for a tool event:
    - Parse and memoize the input, handling bad or incomplete states
    - Provide default content (at the moment a tool label, and a style for active state)

---
+++ components/ai_chat/resources/untrusted_conversation_frame/components/assistant_response/get_tool_label.ts
@@ -0,0 +1,38 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import { formatLocale, getLocale } from '$web-common/locale'
+import * as Mojom from '../../../common/mojom'
+
+/**
+ * See navigation_tool.cc
+ * @param toolInput Expects { "website_url": string } but is not guaranteed.
+ * @returns
+ */
+function getNavigateToolNameLabel(toolInput: any) {
+  if (!toolInput?.website_url) {
+    return getLocale(S.CHAT_UI_TOOL_LABEL_NAVIGATE_WEB_PAGE)
+  }
+  if (typeof toolInput.website_url === 'string') {
+    return formatLocale(S.CHAT_UI_TOOL_LABEL_NAVIGATE_WEB_PAGE_WITH_INPUT, {
+      $1: toolInput.website_url as string,
+    })
+  }
+  return null
+}
+
+/**
+ * Get a display label for the specified tool given its name and input properties
+ */
+export function getToolLabel(toolName: string, toolInput: any) {
+  // TODO(https://github.com/brave/brave-browser/issues/50097): add all other
+  // known tool names as mojom constants and strings.
+  switch (toolName) {
+    case Mojom.NAVIGATE_TOOL_NAME:
+      return getNavigateToolNameLabel(toolInput)
+    default:
+      return toolName
+  }
+}
