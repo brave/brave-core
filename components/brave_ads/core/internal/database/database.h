9ace4714e35539a6d3da9f66442258dbcd24d04e
Claudio DeSouza <cdesouza@brave.com>
2025-10-14 19:38:12 +0100

    [cr143] Use `MemoryPressureListenerRegistration`
    `MemoryPressureListener` is now an abstract class. This change for now
    migrates to use `MemoryPressureListenerRegistration`, but the
    constructor chosen is already deprecated. Another mirgation will take
    place once the path for it is a bit clearer.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/220ae86e5c28f0c21a64990cae0a28d1f234b0fc
    
    commit 220ae86e5c28f0c21a64990cae0a28d1f234b0fc
    Author: Patrick Monette <pmonette@chromium.org>
    Date:   Thu Oct 9 20:29:28 2025 -0700
    
        Add MemoryPressureListener interface
    
        This change adds an new constructor overload to
        MemoryPressureListenerRegistration that accepts a pointer to a
        MemoryPressureListener implementation.
    
        Bug: 436324601
        Change-Id: I0440405e56d986417d8245c73f4263b34cb5f80e
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7016598
        Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
        Commit-Queue: Patrick Monette <pmonette@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1527903}

--- components/brave_ads/core/internal/database/database.h
+++ components/brave_ads/core/internal/database/database.h
@@ -75,7 +75,8 @@ class Database final {
 
   bool is_initialized_ = false;
 
-  std::unique_ptr<base::MemoryPressureListener> memory_pressure_listener_;
+  std::unique_ptr<base::MemoryPressureListenerRegistration>
+      memory_pressure_listener_registration_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
--- components/brave_rewards/core/engine/rewards_database.h
+++ components/brave_rewards/core/engine/rewards_database.h
@@ -64,7 +64,8 @@ class RewardsDatabase : public mojom::RewardsDatabase {
   sql::MetaTable meta_table_;
   bool initialized_ = false;
 
-  std::unique_ptr<base::MemoryPressureListener> memory_pressure_listener_;
+  std::unique_ptr<base::MemoryPressureListenerRegistration>
+      memory_pressure_listener_registration_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 };
