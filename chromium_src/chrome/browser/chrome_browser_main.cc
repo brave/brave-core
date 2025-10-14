90264eabde5c100cf6c210b134cc26e0671a018e
Claudio DeSouza <cdesouza@brave.com>
2025-10-14 19:32:39 +0100

    [cr143] Fixing `ChromeBrowserMainParts` replacements
    Unfortunately with the current way how `ChromeBrowserMainParts` is
    replaced with a `ChromeBrowserMainParts_ChromiumImpl` there a certain
    viral aspect to it, where the external functions using the same class
    point end up pointing to a derived class, rather than the Chromium
    implementation one.
    
    This change applies this substitution in other places where
    `ChromeBrowserMainParts_ChromiumImpl` is expected because the function
    in question is called in the body of `ChromeBrowserMainParts`.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/d71ed037ed0acb08f0c4416e5673d31b047df708
    
    commit d71ed037ed0acb08f0c4416e5673d31b047df708
    Author: Lei Zhang <thestig@chromium.org>
    Date:   Thu Oct 9 12:59:50 2025 -0700
    
        Move ChromeBrowserMainParts code out of chrome_content_browser_client.cc
    
        Currently, ChromeContentBrowserClient::CreateBrowserMainParts() does all
        the work of selecting the right parts to instantiate. Move this code
        into a new ChromeBrowserMainParts::Create() instead. Then
        chrome_content_browser_client.cc, which is the one of the largest and
        slowest to compile files in the source tree, builds about 3 seconds
        faster in build time benchmarks on a physical workstation. While
        chrome_browser_main.cc, which compiles in about half the time, builds
        about 2.5 seconds slower. Given modern CPUs with many cores, this better
        balances parallel work. Since compiling chrome_content_browser_client.cc
        blocks the critical path to linking the chrome binary, this speeds up
        the overall build by about 3 seconds.
    
        Change-Id: I4223aa5fc88f615b5157397fffdc47a8b88a7d2c
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7022412
        Reviewed-by: Erik Chen <erikchen@chromium.org>
        Commit-Queue: Lei Zhang <thestig@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1527728}

--- chromium_src/chrome/browser/chrome_browser_main.cc
+++ chromium_src/chrome/browser/chrome_browser_main.cc
@@ -4,7 +4,10 @@
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "chrome/browser/chrome_browser_main.h"
+
 #include "brave/browser/brave_browser_process_impl.h"
+#include "chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.h"
+#include "chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.h"
 
 #define BrowserProcessImpl BraveBrowserProcessImpl
 #define ChromeBrowserMainParts ChromeBrowserMainParts_ChromiumImpl
