692a62fd92f5883891e784683b5b9ae6c1027b23
Claudio DeSouza <cdesouza@brave.com>
2025-10-14 19:38:12 +0100

    [cr143] Use `MemoryPressureListenerRegistration`
    `MemoryPressureListener` is now an abstract class. This change for now
    migrates to use `MemoryPressureListenerRegistration`, but the
    constructor chosen is already deprecated. Another mirgation will take
    place once the path for it is a bit clearer.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/220ae86e5c28f0c21a64990cae0a28d1f234b0fc
    
    commit 220ae86e5c28f0c21a64990cae0a28d1f234b0fc
    Author: Patrick Monette <pmonette@chromium.org>
    Date:   Thu Oct 9 20:29:28 2025 -0700
    
        Add MemoryPressureListener interface
    
        This change adds an new constructor overload to
        MemoryPressureListenerRegistration that accepts a pointer to a
        MemoryPressureListener implementation.
    
        Bug: 436324601
        Change-Id: I0440405e56d986417d8245c73f4263b34cb5f80e
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7016598
        Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
        Commit-Queue: Patrick Monette <pmonette@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1527903}

--- components/brave_ads/core/internal/database/database.cc
+++ components/brave_ads/core/internal/database/database.cc
@@ -232,10 +232,11 @@ mojom::DBTransactionResultInfo::StatusCode Database::Initialize(
           kFailedToInitializeMetaTable;
     }
 
-    memory_pressure_listener_ = std::make_unique<base::MemoryPressureListener>(
-        FROM_HERE, base::MemoryPressureListenerTag::kAdsDatabase,
-        base::BindRepeating(&Database::MemoryPressureListenerCallback,
-                            weak_factory_.GetWeakPtr()));
+    memory_pressure_listener_registration_ =
+        std::make_unique<base::MemoryPressureListenerRegistration>(
+            FROM_HERE, base::MemoryPressureListenerTag::kAdsDatabase,
+            base::BindRepeating(&Database::MemoryPressureListenerCallback,
+                                weak_factory_.GetWeakPtr()));
 
     is_initialized_ = true;
   }
