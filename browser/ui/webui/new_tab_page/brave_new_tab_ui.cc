b411b0760250db352aea193690d0d938f16db2f8
Claudio DeSouza <cdesouza@brave.com>
2025-10-14 19:42:43 +0100

    [cr143] `RealboxHandler` with `contextual_session_handle`
    This arg is now replacing the previous `query_controller`.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/f4c78c21e386e9f24b4db7b60354ae6e5e3d57c2
    
    commit f4c78c21e386e9f24b4db7b60354ae6e5e3d57c2
    Author: Moe Ahmadi <mahmadi@chromium.org>
    Date:   Wed Oct 8 14:37:28 2025 -0700
    
        [composebox] Starts using ContextualSessionService
    
        Migrates the existing UI components that currently create and hold
    
        std::unique_ptr<ComposeboxQueryController> references to obtain and hold
        a SessionHandle from ContextualSessionService instead.
    
        Bug: b:449809655
        Change-Id: Id6b28a763a82ee26647f2f816029cb59255ffaa4
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7016708
        Commit-Queue: Jason Hu <hujasonx@google.com>
        Reviewed-by: Erik Chen <erikchen@chromium.org>
        Reviewed-by: Jason Hu <hujasonx@google.com>
        Cr-Commit-Position: refs/heads/main@{#1527166}

--- browser/ui/webui/new_tab_page/brave_new_tab_ui.cc
+++ browser/ui/webui/new_tab_page/brave_new_tab_ui.cc
@@ -206,7 +206,7 @@ void BraveNewTabUI::BindInterface(
   CHECK(profile);
 
   realbox_handler_ = std::make_unique<RealboxHandler>(
-      std::move(pending_page_handler), /*query_controller=*/nullptr,
+      std::move(pending_page_handler), /*contextual_session_handle=*/nullptr,
       /*composebox_metrics_recorder=*/nullptr, profile,
       web_ui()->GetWebContents(),
       /*metrics_reporter=*/nullptr);
