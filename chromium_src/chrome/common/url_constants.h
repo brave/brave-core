f319810a6ed86c30e9b064f1e90b118149bd4c7e
Claudio DeSouza <cdesouza@brave.com>
2025-10-15 11:23:44 +0100

    [cr143][WIP] Adding `kGoogleChromeURLScheme` definition
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/fa27381442a1af70f80d69a9d7e17f2436e955da
    
    commit fa27381442a1af70f80d69a9d7e17f2436e955da
    Author: Deepak Ravichandran <deepakr@google.com>
    Date:   Tue Oct 14 05:57:29 2025 -0700
    
        Add google-chrome:// URI scheme support.
    
        Change is feature flag guarded.
    
        This change introduces support for the `google-chrome://` URI scheme
        allowing Chrome to be opened from external applications using
        links with this scheme from command line.
    
        The implementation includes two main parts:
        - Validating the `google-chrome://` scheme during command-line
          parsing.
        - If valid, transforming `google-chrome://xxx` URLs to the
          corresponding `xxx`.
          `google-chrome://https://www.google.com` ==>
          `https://www.google.com`
    
        Bug: b:446672134
        Change-Id: Ia425e6817aa5d255d39d0dd8ce7af0ebb6afd5a5
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6837937
        Reviewed-by: Greg Thompson <grt@chromium.org>
        Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
        Commit-Queue: Deepak Ravichandran <deepakr@google.com>
        Cr-Commit-Position: refs/heads/main@{#1529463}

--- chromium_src/chrome/common/url_constants.h
+++ chromium_src/chrome/common/url_constants.h
@@ -259,6 +259,12 @@ inline constexpr char kGoogleAccountNameEmailAddressEditURL[] =
 inline constexpr char kGoogleTwoFactorIntersitialURL[] =
     "https://support.brave.app/";
 
+// The scheme used for google-chrome:// URLs.
+// TODO(b/446672134): Use a distinct value for each brand (e.g.,
+//  Google Chrome vs. Chromium) and install mode (e.g., stable vs.
+//  side-by-side beta).
+inline constexpr char kGoogleChromeURLScheme[] = "google-chrome";
+
 // URL of the Google Password Manager.
 inline constexpr char kGooglePasswordManagerURL[] = "https://support.brave.app";
 
