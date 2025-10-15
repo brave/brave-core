ac3ff68f0e3a1f6ea059b7f01db66973ac903d46
Pete Miller <miller.pete@gmail.com>
2025-10-14 23:59:41 -0700

    [AI Chat] [Content Agent] UI labels for content agent tools

--- components/ai_chat/resources/untrusted_conversation_frame/components/assistant_response/get_tool_label.ts
+++ components/ai_chat/resources/untrusted_conversation_frame/components/assistant_response/get_tool_label.ts
@@ -12,26 +12,52 @@ import * as Mojom from '../../../common/mojom'
  * @returns
  */
 function getNavigateToolNameLabel(toolInput: any) {
-  if (!toolInput?.website_url) {
-    return getLocale(S.CHAT_UI_TOOL_LABEL_NAVIGATE_WEB_PAGE)
-  }
   if (typeof toolInput.website_url === 'string') {
     return formatLocale(S.CHAT_UI_TOOL_LABEL_NAVIGATE_WEB_PAGE_WITH_INPUT, {
       $1: toolInput.website_url as string,
     })
   }
-  return null
+  return getLocale(S.CHAT_UI_TOOL_LABEL_NAVIGATE_WEB_PAGE)
+}
+
+/**
+ * See history_tool.cc
+ * @param toolInput Expects { "direction": "back" | "forward" } but is not guaranteed.
+ * @returns
+ */
+function getHistoryToolNameLabel(toolInput: any) {
+  // Tool input might not be complete, so check values explicitly.
+  if (toolInput?.direction == 'back') {
+    return getLocale(S.CHAT_UI_TOOL_LABEL_NAVIGATE_HISTORY_BACK)
+  } else if (toolInput?.direction == 'forward') {
+    return getLocale(S.CHAT_UI_TOOL_LABEL_NAVIGATE_HISTORY_FORWARD)
+  }
+  return getLocale(S.CHAT_UI_TOOL_LABEL_NAVIGATE_WEB_PAGE)
 }
 
 /**
  * Get a display label for the specified tool given its name and input properties
  */
 export function getToolLabel(toolName: string, toolInput: any) {
-  // TODO(https://github.com/brave/brave-browser/issues/50097): add all other
-  // known tool names as mojom constants and strings.
   switch (toolName) {
     case Mojom.NAVIGATE_TOOL_NAME:
       return getNavigateToolNameLabel(toolInput)
+    case Mojom.NAVIGATE_HISTORY_TOOL_NAME:
+      return getHistoryToolNameLabel(toolInput)
+    case Mojom.CLICK_TOOL_NAME:
+      return getLocale(S.CHAT_UI_TOOL_LABEL_CLICK_ELEMENT)
+    case Mojom.DRAG_AND_RELEASE_TOOL_NAME:
+      return getLocale(S.CHAT_UI_TOOL_LABEL_DRAG_AND_RELEASE)
+    case Mojom.MOVE_MOUSE_TOOL_NAME:
+      return getLocale(S.CHAT_UI_TOOL_LABEL_MOVE_MOUSE)
+    case Mojom.SCROLL_ELEMENT_TOOL_NAME:
+      return getLocale(S.CHAT_UI_TOOL_LABEL_SCROLL_ELEMENT)
+    case Mojom.SELECT_DROPDOWN_TOOL_NAME:
+      return getLocale(S.CHAT_UI_TOOL_LABEL_SELECT_DROPDOWN)
+    case Mojom.TYPE_TEXT_TOOL_NAME:
+      return getLocale(S.CHAT_UI_TOOL_LABEL_TYPE_TEXT)
+    case Mojom.WAIT_TOOL_NAME:
+      return getLocale(S.CHAT_UI_TOOL_LABEL_WAIT)
     default:
       return toolName
   }
