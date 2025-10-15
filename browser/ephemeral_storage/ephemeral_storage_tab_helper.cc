dac9f8c53d3746fc4205293895758721b4004660
Claudio DeSouza <cdesouza@brave.com>
2025-10-15 11:22:41 +0100

    [cr143] IWYU fixes

--- browser/ephemeral_storage/ephemeral_storage_tab_helper.cc
+++ browser/ephemeral_storage/ephemeral_storage_tab_helper.cc
@@ -14,6 +14,7 @@
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "content/public/browser/navigation_handle.h"
+#include "content/public/browser/site_instance.h"
 #include "content/public/browser/storage_partition.h"
 #include "content/public/browser/web_contents.h"
 #include "net/base/features.h"
