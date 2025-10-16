dc467eb0e2385b8447e479a6ad48e1d455d9174d
sangwoo ko <sangwoo108@gmail.com>
2025-10-16 15:16:54 +0900

    Polish bookmarks icons in darker theme
    Update color values

--- browser/ui/color/material_brave_color_mixer.cc
+++ browser/ui/color/material_brave_color_mixer.cc
@@ -6,10 +6,16 @@
 #include "brave/browser/ui/color/material_brave_color_mixer.h"
 
 #include "base/check.h"
+#include "base/feature_list.h"
+#include "brave/ui/color/nala/nala_color_id.h"
 #include "chrome/browser/ui/color/chrome_color_id.h"
 #include "ui/color/color_provider.h"
 #include "ui/color/color_recipe.h"
 #include "ui/gfx/color_palette.h"
+
+#if defined(TOOLKIT_VIEWS)
+#include "brave/browser/ui/darker_theme/features.h"
+#endif  // defined(TOOLKIT_VIEWS)
 
 void AddMaterialBraveColorMixer(ui::ColorProvider* provider,
                                 const ui::ColorProviderKey& key) {
@@ -21,4 +27,18 @@ void AddMaterialBraveColorMixer(ui::ColorProvider* provider,
   mixer[kColorTabSearchScrollbarThumb] = {
       is_dark ? SkColorSetRGB(0x58, 0x58, 0x58)
               : SkColorSetRGB(0xB4, 0xB4, 0xB4)};
+
+#if defined(TOOLKIT_VIEWS)
+  if (!key.custom_theme &&
+      (!base::FeatureList::IsEnabled(
+           darker_theme::features::kBraveDarkerTheme) ||
+       key.scheme_variant != ui::ColorProviderKey::SchemeVariant::kDarker)) {
+    return;
+  }
+
+  // Override saved tab group button / bookmark folder icon color in darker
+  // theme.
+  mixer[kColorBookmarkFolderIcon] = {nala::kColorPrimitiveNeutral40};
+  mixer[kColorBookmarkButtonIcon] = {nala::kColorPrimitiveNeutral40};
+#endif  // defined(TOOLKIT_VIEWS)
 }
