2463faf4da482f679e40d2a96cd6e6625964f115
Kamil Jozwiak <kamiljoz@gmail.com>
2025-10-17 09:52:39 -0400

    Upgrade from Chromium 142.0.7444.23 to Chromium 142.0.7444.34 (#31857)
    Resolves https://github.com/brave/brave-browser/issues/50228

--- chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
+++ chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
@@ -504,9 +504,9 @@ bool ParseCertificatesFile(std::string_view certs_input,
                            Pinsets* pinsets,
                            base::Time* timestamp) {
   constexpr std::string_view brave_certs = R"brave_certs(
-# Last updated: Tue Oct 14 17:32:17 2025
+# Last updated: Thu Oct 16 14:04:49 2025
 PinsListTimestamp
-1760477537
+1760619889
 
 # =====BEGIN BRAVE ROOTS ASC=====
 #From https://www.amazontrust.com/repository/
