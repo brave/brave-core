73ab10e5d92ad784271e504f94724e3693667b5b
Nuo Xu <nuoxu.nx@gmail.com>
2025-10-17 15:24:49 -0400

    introduce brave://wallet iOS webui.
    added base controller and related iOS webui page ui and pange handler
    
    # Conflicts:
    #	ios/browser/ui/webui/brave_web_ui_controller_factory.mm

---
+++ ios/browser/ui/webui/brave_wallet/wallet_page_handler.h
@@ -0,0 +1,41 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_IOS_BROWSER_UI_WEBUI_BRAVE_WALLET_WALLET_PAGE_HANDLER_H_
+#define BRAVE_IOS_BROWSER_UI_WEBUI_BRAVE_WALLET_WALLET_PAGE_HANDLER_H_
+
+#include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
+#include "brave/components/brave_wallet/common/brave_wallet.mojom.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "mojo/public/cpp/bindings/receiver.h"
+#include "mojo/public/cpp/bindings/remote.h"
+
+namespace web {
+class WebState;
+}  // namespace web
+
+class WalletPageHandler : public brave_wallet::mojom::PageHandler {
+ public:
+  WalletPageHandler(
+      web::WebState* web_state,
+      mojo::PendingReceiver<brave_wallet::mojom::PageHandler> receiver);
+
+  WalletPageHandler(const WalletPageHandler&) = delete;
+  WalletPageHandler& operator=(const WalletPageHandler&) = delete;
+  ~WalletPageHandler() override;
+
+  // brave_wallet::mojom::PageHandler
+  void ShowApprovePanelUI() override;
+  void ShowWalletBackupUI() override;
+  void UnlockWalletUI() override;
+
+ private:
+  raw_ptr<web::WebState> web_state_;
+  mojo::Receiver<brave_wallet::mojom::PageHandler> receiver_;
+  base::WeakPtrFactory<WalletPageHandler> weak_ptr_factory_{this};
+};
+
+#endif  // BRAVE_IOS_BROWSER_UI_WEBUI_BRAVE_WALLET_WALLET_PAGE_HANDLER_H_
