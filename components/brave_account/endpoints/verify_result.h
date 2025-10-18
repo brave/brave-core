9049535a873943792808069d1101ec22e4d4c2a6
Szilard Szaloki <szilardszaloki@gmail.com>
2025-10-17 19:42:30 -0600

    Implements the `/v2/verify/result` endpoint. (#31895)

---
+++ components/brave_account/endpoints/verify_result.h
@@ -0,0 +1,31 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_BRAVE_ACCOUNT_ENDPOINTS_VERIFY_RESULT_H_
+#define BRAVE_COMPONENTS_BRAVE_ACCOUNT_ENDPOINTS_VERIFY_RESULT_H_
+
+#include <string_view>
+
+#include "brave/components/brave_account/endpoint_client/concepts.h"
+#include "brave/components/brave_account/endpoints/error.h"
+#include "brave/components/brave_account/endpoints/verify_result_request.h"
+#include "brave/components/brave_account/endpoints/verify_result_response.h"
+#include "url/gurl.h"
+
+namespace brave_account::endpoints {
+
+struct VerifyResult {
+  using Request = VerifyResultRequest;
+  using Response = VerifyResultResponse;
+  using Error = Error;
+  static GURL URL();
+  static std::string_view Method();
+};
+
+static_assert(endpoint_client::concepts::Endpoint<VerifyResult>);
+
+}  // namespace brave_account::endpoints
+
+#endif  // BRAVE_COMPONENTS_BRAVE_ACCOUNT_ENDPOINTS_VERIFY_RESULT_H_
