a60383a2d82c29ef8b01c306586711c74d4f3e25
Aleksei Seren <aseren@brave.com>
2025-10-18 13:01:29 -0500

    Add BRAVE_REFERRERS content setting policy
    This change allows to set BRAVE_REFERRERS
    content setting via managed preference
    basing on policy.

--- chromium_src/chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chromium_src/chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -7,6 +7,7 @@
 
 #include "brave/browser/policy/brave_simple_policy_map.h"
 #include "brave/browser/policy/handlers/brave_https_upgrade_policy_handler.h"
+#include "brave/browser/policy/handlers/brave_referrers_policy_handler.h"
 #include "components/policy/core/browser/configuration_policy_handler.h"
 
 #define BuildHandlerList BuildHandlerList_ChromiumImpl
@@ -26,6 +27,7 @@ std::unique_ptr<ConfigurationPolicyHandlerList> BuildHandlerList(
   }
 
   handlers->AddHandler(std::make_unique<BraveHttpsUpgradePolicyHandler>());
+  handlers->AddHandler(std::make_unique<BraveReferrersPolicyHandler>());
 
   return handlers;
 }
