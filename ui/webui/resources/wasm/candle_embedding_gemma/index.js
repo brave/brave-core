cccd670a7586cc05a622120fd82b096871333edd
Anthony Tseng <darkdh@gmail.com>
2025-10-20 10:17:34 -0700

    Add candle service to run embedding gemma

---
+++ ui/webui/resources/wasm/candle_embedding_gemma/index.js
@@ -0,0 +1,7 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+export { init, Gemma3Embedder } from
+  'gen/brave/ui/webui/resources/wasm/candle_embedding_gemma/bundler/pkg'
