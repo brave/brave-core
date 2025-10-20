343c06a902b5f771de9ec6683e209b5a4761e7ad
Anthony Tseng <darkdh@gmail.com>
2025-10-20 10:17:34 -0700

    Add candle service to run embedding gemma

---
+++ components/local_ai/browser/candle_service.h
@@ -0,0 +1,46 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_COMPONENTS_LOCAL_AI_BROWSER_CANDLE_SERVICE_H_
+#define BRAVE_COMPONENTS_LOCAL_AI_BROWSER_CANDLE_SERVICE_H_
+
+#include "base/memory/weak_ptr.h"
+#include "base/no_destructor.h"
+#include "brave/components/local_ai/common/candle.mojom.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
+#include "mojo/public/cpp/bindings/receiver_set.h"
+#include "mojo/public/cpp/bindings/remote.h"
+
+namespace local_ai {
+
+class CandleService : public mojom::CandleService {
+ public:
+  static CandleService* GetInstance();
+
+  CandleService(const CandleService&) = delete;
+  CandleService& operator=(const CandleService&) = delete;
+
+  void BindReceiver(mojo::PendingReceiver<mojom::CandleService> receiver);
+
+  void BindEmbeddingGemma(
+      mojo::PendingRemote<mojom::EmbeddingGemmaInterface>) override;
+
+ private:
+  friend class base::NoDestructor<CandleService>;
+
+  CandleService();
+  ~CandleService() override;
+
+  mojo::ReceiverSet<mojom::CandleService> receivers_;
+
+  mojo::Remote<mojom::EmbeddingGemmaInterface> embedding_gemma_remote_;
+
+  base::WeakPtrFactory<CandleService> weak_ptr_factory_{this};
+};
+
+}  // namespace local_ai
+
+#endif  // BRAVE_COMPONENTS_LOCAL_AI_BROWSER_CANDLE_SERVICE_H_
