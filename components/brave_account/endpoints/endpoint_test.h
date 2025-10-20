bcaff20ac522cb1af7c45c4f68d2e807869e60de
boocmp <boocmp@yandex.ru>
2025-10-20 22:12:15 +0700

    Fixed tests.

--- components/brave_account/endpoints/endpoint_test.h
+++ components/brave_account/endpoints/endpoint_test.h
@@ -25,6 +25,27 @@
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 
+namespace {
+
+template <typename Reply>
+std::string GetErrorMessage(const Reply& r) {
+  if (r.has_value()) {
+    return {};
+  }
+  if (const auto* network =
+          std::get_if<brave_account::endpoint_client::NetworkError>(
+              &r.error())) {
+    return network->error_message;
+  } else if (const auto* parse =
+                 std::get_if<brave_account::endpoint_client::ParseError>(
+                     &r.error())) {
+    return parse->error_message;
+  }
+  return "Invalid endpoint error";
+}
+
+}  // namespace
+
 namespace brave_account::endpoints {
 
 inline bool operator==(const Error& lhs, const Error& rhs) {
@@ -33,8 +54,7 @@ inline bool operator==(const Error& lhs, const Error& rhs) {
 
 template <endpoint_client::IsEndpoint T>
 struct EndpointTestCase {
-  using Expected = base::expected<std::optional<typename T::Response>,
-                                  std::optional<typename T::Error>>;
+  using Expected = brave_account::endpoint_client::Reply<T>;
 
   std::string test_name;
   net::HttpStatusCode http_status_code;
@@ -64,8 +84,9 @@ class EndpointTest : public testing::TestWithParam<const EndpointTestCase<T>*> {
     endpoint_client::Client<T>::Send(
         test_url_loader_factory_.GetSafeWeakWrapper(), typename T::Request(),
         future.GetCallback());
-    EXPECT_EQ(future.Take(),
-              std::tie(test_case.http_status_code, test_case.reply));
+    EXPECT_EQ(future.Get(),
+              std::tie(test_case.http_status_code, test_case.reply))
+        << GetErrorMessage(std::get<1>(future.Take()));
   }
 
   base::test::TaskEnvironment task_environment_;
