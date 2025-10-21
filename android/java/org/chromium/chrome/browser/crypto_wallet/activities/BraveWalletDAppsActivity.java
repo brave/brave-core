171072dce0f36c8e81bdef2c52223098833b0ca0
Simone Arpe <simon.arpe@gmail.com>
2025-10-21 11:30:06 +0200

    Fix presubmit warnings

--- android/java/org/chromium/chrome/browser/crypto_wallet/activities/BraveWalletDAppsActivity.java
+++ android/java/org/chromium/chrome/browser/crypto_wallet/activities/BraveWalletDAppsActivity.java
@@ -11,7 +11,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.view.Window;
-import android.view.WindowManager;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
