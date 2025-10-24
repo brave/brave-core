6c2b4d95f4372fa6bc0d601bf8a8a3d6c3485710
Aleksei Seren <86598290+aseren@users.noreply.github.com>
2025-10-24 11:19:54 -0400

    Common shields utils code health (#31996)
    The PR separates locale related util to
    a components/brave_shields/core/browser

--- components/brave_shields/core/common/brave_shield_utils.cc
+++ components/brave_shields/core/common/brave_shield_utils.cc
@@ -119,47 +119,4 @@ void SetBraveShieldsAdBlockOnlyModeEnabled(PrefService* local_state,
   local_state->SetBoolean(prefs::kAdBlockOnlyModeEnabled, enabled);
 }
 
-std::string GetLanguageCodeFromLocale(const std::string& locale) {
-  const std::string::size_type loc = locale.find("-");
-  if (loc == std::string::npos) {
-    return base::ToLowerASCII(locale);
-  }
-  return base::ToLowerASCII(locale.substr(0, loc));
-}
-
-bool IsAdblockOnlyModeSupportedForLocale(const std::string& locale) {
-  return kAdblockOnlyModeSupportedLanguageCodes.contains(
-      GetLanguageCodeFromLocale(locale));
-}
-
-void ManageAdBlockOnlyModeByLocale(PrefService* local_state,
-                                   const std::string& locale) {
-  if (!base::FeatureList::IsEnabled(
-          brave_shields::features::kAdblockOnlyMode)) {
-    return;
-  }
-
-  if (!IsAdblockOnlyModeSupportedForLocale(locale)) {
-    // If the current locale is not supported and Ad Block Only mode is enabled,
-    // then disable Ad Block Only mode and set
-    // `brave.shields.adblock_only_mode_was_enabled_for_supported_locale` pref
-    // value to true.
-    if (local_state->GetBoolean(prefs::kAdBlockOnlyModeEnabled)) {
-      local_state->SetBoolean(prefs::kAdBlockOnlyModeEnabled, false);
-      local_state->SetBoolean(
-          prefs::kAdBlockOnlyModeWasEnabledForSupportedLocale, true);
-    }
-  } else {
-    // If the current locale is supported, Ad Block Only mode is not enabled and
-    // was enabled previously for a supported locale, enable it.
-    if (!local_state->GetBoolean(prefs::kAdBlockOnlyModeEnabled) &&
-        local_state->GetBoolean(
-            prefs::kAdBlockOnlyModeWasEnabledForSupportedLocale)) {
-      local_state->SetBoolean(prefs::kAdBlockOnlyModeEnabled, true);
-      local_state->SetBoolean(
-          prefs::kAdBlockOnlyModeWasEnabledForSupportedLocale, false);
-    }
-  }
-}
-
 }  // namespace brave_shields
