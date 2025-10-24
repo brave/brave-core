5d573c86fd9bba025359700b2a8fcfdb257a94f2
Michael Herrmann <michael@herrmann.io>
2025-10-24 07:26:15 +0200

    Fix chrome/installer/util:installer_util_unittests
    The following test fails:
    
     * InitialPreferencesTest.GoogleUpdateIsMachine
    
    Can run the remaining ones with:
    
        installer_util_unittests --gtest_filter=-InitialPreferencesTest.GoogleUpdateIsMachine

---
+++ chromium_src/chrome/installer/chromium_index_helper_begin.h
@@ -0,0 +1,34 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+// This helper defines CHROMIUM_INDEX for Brave builds.
+// Brave uses different install mode indices than Chromium.
+// CHROMIUM_INDEX doesn't exist in Brave's chromium_install_modes.h.
+// For official Brave builds, the primary mode is STABLE_INDEX.
+// For development builds, the primary mode is DEVELOPER_INDEX.
+//
+// Usage:
+//   #include "brave/chromium_src/chrome/installer/chromium_index_helper_begin.h"
+//   #include <chrome/installer/.../original_file.cc>
+//   #include "brave/chromium_src/chrome/installer/chromium_index_helper_end.h"
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_INSTALLER_CHROMIUM_INDEX_HELPER_BEGIN_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_INSTALLER_CHROMIUM_INDEX_HELPER_BEGIN_H_
+
+#include "build/branding_buildflags.h"
+
+#if !BUILDFLAG(GOOGLE_CHROME_BRANDING) && \
+    !BUILDFLAG(GOOGLE_CHROME_FOR_TESTING_BRANDING)
+
+#if defined(OFFICIAL_BUILD)
+#define CHROMIUM_INDEX STABLE_INDEX
+#else
+#define CHROMIUM_INDEX DEVELOPER_INDEX
+#endif
+
+#endif  // !BUILDFLAG(GOOGLE_CHROME_BRANDING) &&
+        // !BUILDFLAG(GOOGLE_CHROME_FOR_TESTING_BRANDING)
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_INSTALLER_CHROMIUM_INDEX_HELPER_BEGIN_H_
