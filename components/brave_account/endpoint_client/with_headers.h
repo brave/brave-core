abd1f9f6274fc7368907904d30babab56707e292
Szilard Szaloki <szilardszaloki@gmail.com>
2025-10-24 22:18:14 -0600

    Add MaybeStripWithHeaders and unify Client::Send()
    Endpoint client: introduce `MaybeStripWithHeaders<>` to normalize request types by removing `WithHeaders<>` when present. This allows `Client<>::Send<>()` to handle both wrapped and plain requests through a single constrained template, eliminating redundant overloads.

---
+++ components/brave_account/endpoint_client/with_headers.h
@@ -0,0 +1,23 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_BRAVE_ACCOUNT_ENDPOINT_CLIENT_WITH_HEADERS_H_
+#define BRAVE_COMPONENTS_BRAVE_ACCOUNT_ENDPOINT_CLIENT_WITH_HEADERS_H_
+
+#include "brave/components/brave_account/endpoint_client/concepts.h"
+#include "net/http/http_request_headers.h"
+
+namespace brave_account::endpoint_client {
+
+// Wrapper that extends a Request type T
+// with an additional net::HttpRequestHeaders member.
+template <concepts::Request T>
+struct WithHeaders : T {
+  net::HttpRequestHeaders headers;
+};
+
+}  // namespace brave_account::endpoint_client
+
+#endif  // BRAVE_COMPONENTS_BRAVE_ACCOUNT_ENDPOINT_CLIENT_WITH_HEADERS_H_
