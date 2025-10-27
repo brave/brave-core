78985748421beffd3ebc491a3c7ee7f395ee8675
sangwoo ko <sangwoo108@gmail.com>
2025-10-27 15:18:10 +0900

    Introduce TabStripModelObserver::TreeTabChanged()

--- browser/ui/tabs/tree_tab_model.cc
+++ browser/ui/tabs/tree_tab_model.cc
@@ -5,12 +5,15 @@
 
 #include "brave/browser/ui/tabs/tree_tab_model.h"
 
+#include "base/logging.h"
+#include "base/task/sequenced_task_runner.h"
+
 TreeTabModel::TreeTabModel() = default;
 TreeTabModel::~TreeTabModel() = default;
 
 const tabs::TreeTabNode* TreeTabModel::GetNode(
     const tree_tab::TreeTabNodeId& id) const {
-  return tree_tab_nodes_.contains(id) ? nullptr : tree_tab_nodes_.at(id);
+  return tree_tab_nodes_.contains(id) ? tree_tab_nodes_.at(id) : nullptr;
 }
 
 int TreeTabModel::GetTreeHeight(const tree_tab::TreeTabNodeId& id) const {
@@ -28,6 +31,20 @@ void TreeTabModel::AddTreeTabNode(const tabs::TreeTabNode& node) {
   }
 
   tree_tab_nodes_[node.id()] = &node;
+
+  auto notification = [](base::WeakPtr<TreeTabModel> model,
+                         const tree_tab::TreeTabNodeId& id) {
+    if (model) {
+      model->add_tree_tab_node_callback_list_.Notify(
+          *model->tree_tab_nodes_.at(id));
+    }
+  };
+
+  // Defer notification to make sure the tab creation operation is completed
+  // before wrapping it up with TreeTabNode created here.
+  base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
+      FROM_HERE,
+      base::BindOnce(std::move(notification), GetWeakPtr(), node.id()));
 }
 
 void TreeTabModel::RemoveTreeTabNode(const tree_tab::TreeTabNodeId& id) {
@@ -35,9 +52,21 @@ void TreeTabModel::RemoveTreeTabNode(const tree_tab::TreeTabNodeId& id) {
     return;
   }
 
+  remove_tree_tab_node_callback_list_.Notify(id);
+
   tree_tab_nodes_.erase(id);
 }
 
 base::WeakPtr<TreeTabModel> TreeTabModel::GetWeakPtr() {
   return weak_ptr_factory_.GetWeakPtr();
+}
+
+base::CallbackListSubscription TreeTabModel::RegisterAddTreeTabNodeCallback(
+    base::RepeatingCallback<void(const tabs::TreeTabNode&)> callback) {
+  return add_tree_tab_node_callback_list_.Add(std::move(callback));
+}
+
+base::CallbackListSubscription TreeTabModel::RegisterRemoveTreeTabNodeCallback(
+    base::RepeatingCallback<void(const tree_tab::TreeTabNodeId&)> callback) {
+  return remove_tree_tab_node_callback_list_.Add(std::move(callback));
 }
--- browser/ui/tabs/tree_tab_model.h
+++ browser/ui/tabs/tree_tab_model.h
@@ -8,6 +8,7 @@
 
 #include <map>
 
+#include "base/callback_list.h"
 #include "base/memory/weak_ptr.h"
 #include "brave/components/tabs/public/tree_tab_node.h"
 
@@ -28,9 +29,20 @@ class TreeTabModel {
 
   base::WeakPtr<TreeTabModel> GetWeakPtr();
 
+  // Callback registration methods.
+  base::CallbackListSubscription RegisterAddTreeTabNodeCallback(
+      base::RepeatingCallback<void(const tabs::TreeTabNode&)> callback);
+  base::CallbackListSubscription RegisterRemoveTreeTabNodeCallback(
+      base::RepeatingCallback<void(const tree_tab::TreeTabNodeId&)> callback);
+
  private:
   std::map<tree_tab::TreeTabNodeId, raw_ptr<const tabs::TreeTabNode>>
       tree_tab_nodes_;
+
+  base::RepeatingCallbackList<void(const tabs::TreeTabNode&)>
+      add_tree_tab_node_callback_list_;
+  base::RepeatingCallbackList<void(const tree_tab::TreeTabNodeId&)>
+      remove_tree_tab_node_callback_list_;
 
   base::WeakPtrFactory<TreeTabModel> weak_ptr_factory_{this};
 };
