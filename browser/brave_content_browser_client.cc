24021ffc10f68c58c845e1919e499e801001d52f
Jay Harris <jay.harris@outlook.co.nz>
2025-10-28 08:38:52 +1300

    [webui]: Make it possible to expose an interface to all WebUIs in a `WebUIBrowserInterfaceBrokerRegistry` (#31911)

--- browser/brave_content_browser_client.cc
+++ browser/brave_content_browser_client.cc
@@ -594,6 +594,11 @@ void BraveContentBrowserClient::
 void BraveContentBrowserClient::RegisterWebUIInterfaceBrokers(
     content::WebUIBrowserInterfaceBrokerRegistry& registry) {
   ChromeContentBrowserClient::RegisterWebUIInterfaceBrokers(registry);
+#if !BUILDFLAG(IS_ANDROID)
+  registry.AddGlobal<color_change_listener::mojom::PageHandler>(
+      base::BindRepeating(&MaybeBindColorChangeHandler));
+#endif
+
 #if BUILDFLAG(ENABLE_BRAVE_VPN) && !BUILDFLAG(IS_ANDROID)
   if (brave_vpn::IsBraveVPNFeatureEnabled()) {
     registry.ForWebUI<VPNPanelUI>()
@@ -679,7 +684,6 @@ void BraveContentBrowserClient::RegisterWebUIInterfaceBrokers(
   if (brave_account::features::IsBraveAccountEnabled()) {
     registry.ForWebUI<BraveAccountUIDesktop>()
         .Add<brave_account::mojom::Authentication>()
-        .Add<color_change_listener::mojom::PageHandler>()
         .Add<password_strength_meter::mojom::PasswordStrengthMeter>();
   }
 #else   // !BUILDFLAG(IS_ANDROID)
