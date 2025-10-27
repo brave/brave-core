c6da9d34f0d0c5dbb8f55a9b0765716bc7258ff9
Claudio DeSouza <cdesouza@brave.com>
2025-10-27 18:10:12 +0000

    [cr143] Stubbing off `PageInterface::addFileContext`
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/9c1c4aee59644bac9e6caf68c9615a44f2544a1a
    
    commit 9c1c4aee59644bac9e6caf68c9615a44f2544a1a
    Author: Juan Mojica <juanmojica@google.com>
    Date:   Fri Oct 24 18:23:40 2025 -0700
    
        [AIM] Add visual selection thumbnail to Lens composebox
    
        This change adds a thumbnail chip to the Lens composebox, which appears
        at the top of the input area when a visual selection is active.
    
        Key changes include:
        - A new `AddFileContext` Mojo API is now used to send the thumbnail data URI and a deletability flag via an updated `SelectedFileInfo` struct to the composebox WebUI.
        - `LensComposeboxController` is updated to store this visual selection context and call the Mojo API via the handler upon binding.
        - WebUI components (`side_panel_app`, `composebox`) are modified to render the thumbnail in a new top-anchored carousel, adjusting the text input area accordingly.
        - Existing thumbnail logic in `LensOverlayController` and `LensSearchContextualizationController` is refactored to utilize the new centralized methods in `LensSearchController`.
    
        See bug for demo.
    
        Bug: 450599476
        Change-Id: I25e8f07858cbf018795b0274ba9d8f5dab67496d
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7048844
        Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
        Reviewed-by: Nasko Oskov <nasko@chromium.org>
        Reviewed-by: Marlon Facey <mfacey@chromium.org>
        Reviewed-by: Duncan Mercer <mercerd@google.com>
        Commit-Queue: Juan Mojica <juanmojica@google.com>
        Cr-Commit-Position: refs/heads/main@{#1535431}

--- components/brave_new_tab_ui/components/search/SearchContext.tsx
+++ components/brave_new_tab_ui/components/search/SearchContext.tsx
@@ -4,7 +4,7 @@
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
 import usePromise from '$web-common/usePromise';
-import { AutocompleteResult, OmniboxPopupSelection, PageHandler, PageHandlerRemote, PageInterface, PageReceiver } from 'chrome://resources/mojo/components/omnibox/browser/searchbox.mojom-webui.js';
+import { AutocompleteResult, OmniboxPopupSelection, PageHandler, PageHandlerRemote, PageInterface, PageReceiver, SelectedFileInfo } from 'chrome://resources/mojo/components/omnibox/browser/searchbox.mojom-webui.js';
 import * as React from 'react';
 import getNTPBrowserAPI, { SearchEngineInfo } from '../../api/background';
 import { useEngineContext } from './EngineContext';
@@ -77,6 +77,7 @@ class SearchPage implements PageInterface {
   setThumbnail(thumbnailUrl: string) { }
   onContextualInputStatusChanged(token: string, status: FileUploadStatus, errorType: FileUploadErrorType | null) { }
   onTabStripChanged() { }
+  addFileContext(token: string, fileInfo: SelectedFileInfo) { }
 }
 
 export const search = new SearchPage()
