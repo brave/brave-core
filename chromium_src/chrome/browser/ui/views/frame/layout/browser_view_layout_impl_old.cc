cfa359479e9c3eb92ae726c23654fb723ba721ab
Claudio DeSouza <cdesouza@brave.com>
2025-10-27 13:15:02 +0000

    [cr143] `BrowserViewLayout` derived classes under `layout/`
    These derived classes are now living on their respective headers under
    `layout/`, which requires quite a few of the overrides to be shuffled
    around under new shadow files.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/7905b0f981d5e50c6b365696377fec7498a2b2de
    
    commit 7905b0f981d5e50c6b365696377fec7498a2b2de
    Author: Dana Fried <dfried@chromium.org>
    Date:   Thu Oct 23 08:24:16 2025 -0700
    
        [Chrome Next] Split out new layout class for toolbar height side panel
    
        This moves all browser view layout classes into a new layout folder, and
        splits out the old and new layout implementations into new files that
        will only be used by the factory method in BrowserViewLayout and in
        tests.
    
        The new layout that supports the required UI is not fleshed out and
        currently not functional. It will be implemented in follow-up CLs.
    
        Bypass-Check-License: Moved files
        Bug: 453717426
        Change-Id: Ibe7f22ea88be5a56ace5586e0bc6d39fb6776930
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7076757
        Commit-Queue: Dana Fried <dfried@chromium.org>
        Reviewed-by: Eshwar Stalin <estalin@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1534352}

---
+++ chromium_src/chrome/browser/ui/views/frame/layout/browser_view_layout_impl_old.cc
@@ -0,0 +1,21 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "chrome/browser/ui/views/frame/layout/browser_view_layout_impl_old.h"
+
+#include "brave/browser/ui/views/side_panel/brave_side_panel.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/browser/ui/views/frame/layout/browser_view_layout_delegate.h"
+#include "chrome/browser/ui/views/side_panel/side_panel.h"
+
+// Double check if the tab strip is actually visible to calculate constraints.
+#define SupportsWindowFeature(FEATURE)                                       \
+  SupportsWindowFeature(FEATURE) && (FEATURE != Browser::FEATURE_TABSTRIP || \
+                                     delegate().ShouldDrawTabStrip());
+
+#include <chrome/browser/ui/views/frame/layout/browser_view_layout_impl_old.cc>
+
+#undef SupportsWindowFeature
