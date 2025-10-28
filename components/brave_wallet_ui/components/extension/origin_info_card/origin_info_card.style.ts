28dea1d1adba4ccf10d8792f0b1d80e2bbd54693
brave-builds <45370463+brave-builds@users.noreply.github.com>
2025-10-28 04:13:18 +0000

    [Wallet]: Display eTLD+1 in New Confirm Send Panel (uplift to 1.84.x) (#32050)
    Uplift of #32044 (squashed) to release

--- components/brave_wallet_ui/components/extension/origin_info_card/origin_info_card.style.ts
+++ components/brave_wallet_ui/components/extension/origin_info_card/origin_info_card.style.ts
@@ -9,25 +9,33 @@ import * as leo from '@brave/leo/tokens/css/variables'
 // Shared Styles
 import { Row, Text } from '../../shared/style'
 
-export const StyledWrapper = styled(Row)<{ noBackground?: boolean }>`
+export const StyledWrapper = styled(Row)<{
+  noBackground?: boolean
+  orientation?: 'horizontal' | 'vertical'
+}>`
   background-color: ${({ noBackground }) =>
     noBackground ? 'transparent' : leo.color.container.highlight};
+  flex-direction: ${({ orientation }) =>
+    orientation === 'vertical' ? 'column' : 'row'};
 `
 
-export const FavIcon = styled.img`
-  width: 48px;
-  height: 48px;
+export const FavIcon = styled.img<{ bigger?: boolean }>`
+  width: ${({ bigger }) => (bigger ? '56px' : '48px')};
+  height: ${({ bigger }) => (bigger ? '56px' : '48px')};
   border-radius: 8px;
   background-color: ${leo.color.container.background};
   border: 1px solid ${leo.color.divider.subtle};
+  padding: ${({ bigger }) => (bigger ? leo.spacing.m : leo.spacing.s)};
 `
 
 export const OriginName = styled(Text)`
   font: ${leo.font.default.semibold};
   letter-spacing: ${leo.typography.letterSpacing.default};
+  word-break: break-word;
 `
 
 export const OriginUrl = styled(Text)`
   font: ${leo.font.small.regular};
   letter-spacing: ${leo.typography.letterSpacing.small};
+  word-break: break-word;
 `
