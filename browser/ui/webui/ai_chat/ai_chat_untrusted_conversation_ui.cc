6ef7d68ae1099c19ff4792af81f685a96f3ff869
Pete Miller <miller.pete@gmail.com>
2025-10-29 00:32:30 -0700

    Move ui/webui/ai_chat to its own target (#32054)
    * Move ui/webui/ai_chat to its own target

--- browser/ui/webui/ai_chat/ai_chat_untrusted_conversation_ui.cc
+++ browser/ui/webui/ai_chat/ai_chat_untrusted_conversation_ui.cc
@@ -30,7 +30,6 @@
 #include "brave/components/constants/webui_url_constants.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/favicon_source.h"
-#include "chrome/browser/ui/webui/util/image_util.h"
 #include "components/favicon_base/favicon_url_parser.h"
 #include "components/grit/brave_components_resources.h"
 #include "components/grit/brave_components_webui_strings.h"
@@ -57,6 +56,7 @@
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/thumbnails/thumbnail_tracker.h"
 #include "chrome/browser/ui/webui/theme_source.h"
+#include "chrome/browser/ui/webui/util/image_util.h"
 #endif
 
 namespace {
