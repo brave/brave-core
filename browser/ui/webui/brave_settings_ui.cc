8741950b9fb11f3a3e5dc44df4b1fa46ee146db8
Brian Johnson <34129+bridiver@users.noreply.github.com>
2025-10-31 12:53:50 -0700

    presubmit

--- browser/ui/webui/brave_settings_ui.cc
+++ browser/ui/webui/brave_settings_ui.cc
@@ -129,8 +129,7 @@ BraveSettingsUI::BraveSettingsUI(content::WebUI* web_ui) : SettingsUI(web_ui) {
   web_ui->AddMessageHandler(
       std::make_unique<settings::MetricsReportingHandler>());
   web_ui->AddMessageHandler(std::make_unique<BravePrivacyHandler>());
-  web_ui->AddMessageHandler(
-      std::make_unique<DefaultBraveShieldsHandler>());
+  web_ui->AddMessageHandler(std::make_unique<DefaultBraveShieldsHandler>());
   web_ui->AddMessageHandler(std::make_unique<BraveDefaultExtensionsHandler>());
   web_ui->AddMessageHandler(std::make_unique<BraveAppearanceHandler>());
   web_ui->AddMessageHandler(std::make_unique<BraveSyncHandler>());
