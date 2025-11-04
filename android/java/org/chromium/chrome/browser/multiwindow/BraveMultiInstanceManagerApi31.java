256de136dd79c2ff2b32330fb0d4e87b51be952f
AlexeyBarabash <alexey@brave.com>
2025-11-04 12:21:53 +0200

    [cr144][Android] MultiInstanceManagerApi31.closeInstance argument changed
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/a478dadc0c75f6d54165b75faa5d7c1a5bc0abcb
    
    	[RWM] Add metric Android.MultiWindowMode.CloseWindow.AppSource
    
    	The CL adds an enumerated histogram to track app-triggered hard
    	closures:
    	1) From the window manager
    	2) From instance retention expiration
    	3) From last tab closure
    
    	Bug: 454636721
    	Change-Id: Icf06c2699004a7e802ff8f91e9c78194a5777c42
    	Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7101067

--- android/java/org/chromium/chrome/browser/multiwindow/BraveMultiInstanceManagerApi31.java
+++ android/java/org/chromium/chrome/browser/multiwindow/BraveMultiInstanceManagerApi31.java
@@ -72,7 +72,7 @@ class BraveMultiInstanceManagerApi31 extends MultiInstanceManagerApi31 {
             TabModelSelector selector =
                     TabWindowManagerSingleton.getInstance().getTabModelSelectorById(mInstanceId);
             if (selector != null && selector.getTotalTabCount() == 0) {
-                closeInstance(mInstanceId, INVALID_TASK_ID);
+                closeInstance(mInstanceId, CloseWindowAppSource.NO_TABS_IN_WINDOW);
             }
             if (MultiWindowUtils.getInstanceCount() == 1) {
                 BraveMultiWindowUtils.updateEnableMultiWindows(false);
