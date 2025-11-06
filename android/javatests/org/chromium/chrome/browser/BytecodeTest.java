b38450495db310085a726f1efa20b3aa35e488fd
AlexeyBarabash <alexey@brave.com>
2025-11-06 14:20:32 +0200

    [cr144][Android] New  arg at BraveTabbedRootUiCoordinator.ctor
    https://source.chromium.org/chromium/chromium/src/+/61b7d0e50d82d35958f834607e512c7b7ff9910c
    
    	[tips] Add a pref for second run experience for tips opt in promo
    
    	Add a pref for second run experience for the tips opt in promo and
    	trigger the bottom sheet promo.
    
    	Bug: 454718966
    	Change-Id: I6677b56cfd46379ab417fc37a53ab2758367a4b3
    	Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7080471

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -1816,7 +1816,8 @@ public class BytecodeTest {
                         ManualFillingComponentSupplier.class,
                         EdgeToEdgeManager.class,
                         ObservableSupplier.class,
-                        ObservableSupplier.class));
+                        ObservableSupplier.class,
+                        OneshotSupplier.class));
         Assert.assertTrue(
                 constructorsMatch(
                         "org/chromium/chrome/browser/bookmarks/BookmarkToolbar",
