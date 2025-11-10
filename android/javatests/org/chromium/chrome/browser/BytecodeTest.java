5f0133bec3338d8fbfd71e6ac755e5785f03129a
AlexeyBarabash <alexey@brave.com>
2025-11-10 14:42:28 +0200

    [cr144][Android] Make AI Mode button focus omnibox when multimodal flag is on
    NewTabPageManager.focusSearchBox and LocationBarMediator.ctor
    were changed.
    
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/83abbb6092b28fcb3d55e918a0391d2ac210b445
    
    	Make AI Mode button focus omnibox when multimodal flag is on
    
    	Demo: https://screenshot.googleplex.com/9X2QKig3fxSMhcn.png
    
    	Bug: 457815209
    	Change-Id: I4f8759be316901672ae38f02cbfee0361e45d30d
    	Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7119868

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -1706,7 +1706,7 @@ public class BytecodeTest {
                         ObservableSupplier.class,
                         BrowserControlsStateProvider.class,
                         Supplier.class,
-                        ObservableSupplier.class,
+                        ObservableSupplierImpl.class,
                         PageZoomIndicatorCoordinator.class));
         Assert.assertTrue(
                 constructorsMatch(
