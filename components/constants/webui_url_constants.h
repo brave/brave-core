28dd7b72ac5fbf697172f7fcd98a7999f44257d9
Anthony Tseng <darkdh@gmail.com>
2025-11-11 15:18:34 -0800

    Host embedding gemma iframe in local_ai_internals and run some examples Make sure you have model.safetensors, tokenizer.json and config.json in ~/Downloads/embeddinggemma-300m which can be downloaded from https://huggingface.co/google/embeddinggemma-300m/tree/main

--- components/constants/webui_url_constants.h
+++ components/constants/webui_url_constants.h
@@ -65,6 +65,12 @@ inline constexpr char kUntrustedMarketURL[] =
 inline constexpr char kUntrustedTrezorHost[] = "trezor-bridge";
 inline constexpr char kUntrustedTrezorURL[] =
     "chrome-untrusted://trezor-bridge/";
+inline constexpr char kLocalAIInternalsHost[] = "local-ai-internals";
+inline constexpr char kLocalAIInternalsURL[] = "chrome://local-ai-internals/";
+inline constexpr char kUntrustedCandleEmbeddingGemmaWasmHost[] =
+    "candle-embedding-gemma-wasm";
+inline constexpr char kUntrustedCandleEmbeddingGemmaWasmURL[] =
+    "chrome-untrusted://candle-embedding-gemma-wasm/";
 inline constexpr char kShieldsPanelURL[] = "chrome://brave-shields.top-chrome";
 inline constexpr char kShieldsPanelHost[] = "brave-shields.top-chrome";
 inline constexpr char kCookieListOptInHost[] = "cookie-list-opt-in.top-chrome";
