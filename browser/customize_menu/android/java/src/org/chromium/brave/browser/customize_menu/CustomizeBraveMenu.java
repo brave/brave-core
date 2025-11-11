382d7d9a44a501b7399015b4539e4a781e8eb478
Simone Arpe <simon.arpe@gmail.com>
2025-11-11 15:42:27 +0100

    Apply correct tint to custom menu item icons (#32244)
    Apply tinting to correct drawable

--- browser/customize_menu/android/java/src/org/chromium/brave/browser/customize_menu/CustomizeBraveMenu.java
+++ browser/customize_menu/android/java/src/org/chromium/brave/browser/customize_menu/CustomizeBraveMenu.java
@@ -387,7 +387,7 @@ public class CustomizeBraveMenu {
                                 new BitmapDrawable(context.getResources(), bitmap);
 
                         // Apply tinting.
-                        maybeApplyTinting(context, drawable);
+                        maybeApplyTinting(context, resizedDrawable);
 
                         // Return to UI thread with result.
                         PostTask.postTask(
