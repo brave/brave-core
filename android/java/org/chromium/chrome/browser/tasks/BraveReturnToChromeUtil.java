182beef0bab45ba865ec12a5a35c807554a7452a
Serg <serg.zhukovsky@gmail.com>
2025-11-11 13:31:29 -0500

    Adds an Opening Screen preference under New Tab Page preference

--- android/java/org/chromium/chrome/browser/tasks/BraveReturnToChromeUtil.java
+++ android/java/org/chromium/chrome/browser/tasks/BraveReturnToChromeUtil.java
@@ -20,18 +20,16 @@ public final class BraveReturnToChromeUtil {
             return false;
         }
 
-        // Uncomment this when the experiment is ready
-        // When feature is enabled, check the variant
-        // String variant = BraveFreshNtpHelper.getVariant();
-        // switch (variant) {
-        //     case "A":
-        //         // Variant A: Brave's default behavior (no NTP at startup)
-        //         return false;
-        //     default:
-        //         // All other variants: fallback to upstream behavior
-        //         //return ReturnToChromeUtil.shouldShowNtpAsHomeSurfaceAtStartup(
-        //         //        intent, bundle, inactivityTracker);
-        // }
+        String variant = BraveFreshNtpHelper.getVariant();
+        switch (variant) {
+            case "A":
+                // Variant A: Brave's default behavior (no NTP at startup)
+                return false;
+            default:
+                // All other variants: fallback to upstream behavior
+                // return ReturnToChromeUtil.shouldShowNtpAsHomeSurfaceAtStartup(
+                //        intent, bundle, inactivityTracker);
+        }
         return false;
     }
 }
