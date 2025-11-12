d250e5ebef223254b4aa745a596eeee8ffde0b92
Michael Herrmann <michael@herrmann.io>
2025-11-12 16:57:50 +0100

    Move FindArchiveToPatch to brave_setup_util.[cc,h]

---
+++ installer/setup/brave_setup_util.h
@@ -0,0 +1,23 @@
+#ifndef BRAVE_INSTALLER_SETUP_SETUP_UTIL_H_
+#define BRAVE_INSTALLER_SETUP_SETUP_UTIL_H_
+
+namespace base {
+class FilePath;
+class Version;
+}  // namespace base
+
+namespace installer {
+
+class InstallationState;
+class InstallerState;
+
+// Returns the uncompressed archive of the installed version that serves as the
+// source for patching.  If |desired_version| is valid, only the path to that
+// version will be returned, or empty if it doesn't exist.
+base::FilePath FindArchiveToPatch(const InstallationState& original_state,
+                                  const InstallerState& installer_state,
+                                  const base::Version& desired_version);
+
+}  // namespace installer
+
+#endif  // BRAVE_INSTALLER_SETUP_SETUP_UTIL_H_
