8b426fd538c74f105466d54359b3f8de2d09a3c2
Max Karolinskiy <41635752+mkarolin@users.noreply.github.com>
2025-11-11 21:25:09 -0500

    Upgrade from Chromium 142.0.7444.135 to Chromium 142.0.7444.163 (1.84.x). (#32259)
    * Update from Chromium 142.0.7444.135 to Chromium 142.0.7444.163.
    
    * Update patches from Chromium 142.0.7444.135 to Chromium 142.0.7444.163.
    
    * Updated strings for Chromium 142.0.7444.163.
    
    * [cr142] `RealboxHandler` dropped metrics arg
    
    This argument was always being passed as `nullptr` in brave.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/e5be126ebbc0915cf16f1c588f503bb1235dc3bc
    
    commit e5be126ebbc0915cf16f1c588f503bb1235dc3bc
    Author: James Leung <jamesleung@google.com>
    Date:   Mon Oct 27 15:31:16 2025 -0700
    
        [composebox] Align Metrics Recorder Lifespan w/Query Controller's
    
        Problem: The composebox metrics recorder is being initialized separately
        from the composebox query controller, even though composebox query
        controller is being moved in order to be used outside of composebox
        handler.
    
        Solution: From separate changes
        (https://chromium-review.git.corp.google.com/c/chromium/src/+/7015147
        https://chromium-review.googlesource.com/c/chromium/src/+/7016708), the
        controller is owned by the ContextualSessionService. That service's
        session handler or its helper class should own the metrics recorder now
        to maintain the same lifespan. Therefore, metrics recorder has been
        changed to be initialized by the ContextualSessionService. Just like
        how it is for the query controller, per session entry that the service
        creates, there will be one metric recorder. This allows one metric
        recorder to exist per query controller.
    
        Other changes:
        * Removed metrics recorder as constructor parameter
        from children of composebox handler since they are not needed
        * Updated tests to reflect these new constructors
        * Updated outdated related comments
        * Updated contextual session test to verify metric recorder is being
        initialized/handled properly within the session
        * Fixed merge conflicts and formatting/linting errors
    
        Note: View bug page to see which tests are important to re-run
    
        Bug: b:447132062
    
        Change-Id: Iec018be610aa98fe7e3bb616fd92f6bda3b682cd
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7046607
        Reviewed-by: Ananya Seelam <ananyaseelam@google.com>
        Reviewed-by: Nihar Majmudar <niharm@google.com>
        Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
        Commit-Queue: Nihar Majmudar <niharm@google.com>
        Auto-Submit: James Leung <jamesleung@google.com>
        Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
        Cr-Commit-Position: refs/heads/main@{#1536281}
    
    ---------
    
    Co-authored-by: Claudio DeSouza <cdesouza@brave.com>

--- chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
+++ chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
@@ -504,9 +504,9 @@ bool ParseCertificatesFile(std::string_view certs_input,
                            Pinsets* pinsets,
                            base::Time* timestamp) {
   constexpr std::string_view brave_certs = R"brave_certs(
-# Last updated: Wed Nov 05 17:04:26 2025
+# Last updated: Tue Nov 11 14:20:51 2025
 PinsListTimestamp
-1762380266
+1762888851
 
 # =====BEGIN BRAVE ROOTS ASC=====
 #From https://www.amazontrust.com/repository/
