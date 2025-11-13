0ae8c24db5464bad56e348c29ec2bb69ef3f2dd1
Michael Herrmann <michael@herrmann.io>
2025-11-13 15:37:22 +0100

    Move remaining code from util_constants.h.patch into chromium_src

--- chromium_src/chrome/installer/util/util_constants.h
+++ chromium_src/chrome/installer/util/util_constants.h
@@ -6,11 +6,41 @@
 #ifndef BRAVE_CHROMIUM_SRC_CHROME_INSTALLER_UTIL_UTIL_CONSTANTS_H_
 #define BRAVE_CHROMIUM_SRC_CHROME_INSTALLER_UTIL_UTIL_CONSTANTS_H_
 
+#define INSTALL_FAILED \
+  SETUP_PATCH_FAILED = 8, \
+  APPLY_DIFF_PATCH_FAILED = 42, \
+  PATCH_INVALID_ARGUMENTS = 49, \
+  DIFF_PATCH_SOURCE_MISSING = 50, \
+  INSTALL_FAILED
+
+#define NO_STAGE \
+  NO_STAGE, \
+  UPDATING_SETUP // Patching setup.exe with differential update.
+
+#define UNCOMPRESSING \
+  UNCOMPRESSING, \
+  PATCHING // Patching chrome.7z with differential update.
+
 #define kGoogleUpdateIsMachineEnvVar                               \
   kGoogleUpdateIsMachineEnvVar[] = "BraveSoftwareUpdateIsMachine"; \
   inline constexpr char kGoogleUpdateIsMachineEnvVar_UnUsed
 
 #include <chrome/installer/util/util_constants.h>  // IWYU pragma: export
+
+#undef UNCOMPRESSING
+#undef NO_STAGE
+#undef INSTALL_FAILED
 #undef kGoogleUpdateIsMachineEnvVar
+
+namespace installer {
+
+  // The type of an update archive.
+enum ArchiveType {
+  UNKNOWN_ARCHIVE_TYPE,     // Unknown or uninitialized.
+  FULL_ARCHIVE_TYPE,        // Full chrome.7z archive.
+  INCREMENTAL_ARCHIVE_TYPE  // Incremental or differential archive.
+};
+
+}  // namespace installer
 
 #endif  // BRAVE_CHROMIUM_SRC_CHROME_INSTALLER_UTIL_UTIL_CONSTANTS_H_
