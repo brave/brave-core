e98bf4d95df843ff268afdbba7c93698ec9f88d6
boocmp <boocmp@yandex.ru>
2025-11-14 22:03:43 +0700

    BrowserWindowInterface instead of Browser.

--- chromium_src/chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chromium_src/chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -353,7 +353,7 @@ void OnRewriteSuggestionCompleted(
 #endif  // BUILDFLAG(ENABLE_AI_CHAT)
 
 email_aliases::EmailAliasesController* GetEmailAliasesController(
-    Browser* browser) {
+    BrowserWindowInterface* browser) {
   if (!browser) {
     return nullptr;
   }
