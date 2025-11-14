2b0ab631b43fc6236673f6a5778acdcc989ea269
Michael Herrmann <michael@herrmann.io>
2025-11-14 08:07:29 +0100

    Move archive_patch_helper* files out of chromium_src

--- installer/setup/archive_patch_helper.cc
+++ installer/setup/archive_patch_helper.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/installer/setup/archive_patch_helper.h"
+#include "brave/installer/setup/archive_patch_helper.h"
 
 #include <stdint.h>
 
