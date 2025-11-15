d75d8b8da8f01ccfdc8b744622b21507008b9bf5
AlexeyBarabash <alexey@brave.com>
2025-11-15 18:10:10 +0200

    [cr144][Android] New incognitoWindowCountSupplier arg
    Applied to:
    - ToolbarLayout.initialize;
    - TopToolbarCoordinator.ctor.
    
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/42bcda88453ce3583443c1bb1f73fdb9baf613e4
    
    	Get proper count of Incognito windows in Incognito Indicator Menu UI
    
    	This is a follow-up to
    	https://chromium-review.googlesource.com/c/chromium/src/+/7023602 which
    	creates a menu to close all Incognito windows from the Incognito
    	indicator.
    
    	Bug: 435491652
    	Change-Id: Iec6e0ce31bfd8e4f2f4700c6d0858c46566358e5
    	Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6977604

--- android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
+++ android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
@@ -137,6 +137,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
+import java.util.function.Supplier;
 
 public abstract class BraveToolbarLayoutImpl extends ToolbarLayout
         implements BraveToolbarLayout,
@@ -1589,7 +1590,7 @@ public abstract class BraveToolbarLayoutImpl extends ToolbarLayout
             ToolbarDataProvider toolbarDataProvider,
             ToolbarTabController tabController,
             MenuButtonCoordinator menuButtonCoordinator,
-            ToggleTabStackButtonCoordinator tabSwitcherButtonCoordinator,
+            @Nullable ToggleTabStackButtonCoordinator tabSwitcherButtonCoordinator,
             HistoryDelegate historyDelegate,
             UserEducationHelper userEducationHelper,
             ObservableSupplier<Tracker> trackerSupplier,
@@ -1600,7 +1601,8 @@ public abstract class BraveToolbarLayoutImpl extends ToolbarLayout
             @Nullable HomeButtonDisplay homeButtonDisplay,
             @Nullable ExtensionToolbarCoordinator extensionToolbarCoordinator,
             ThemeColorProvider themeColorProvider,
-            IncognitoStateProvider incognitoStateProvider) {
+            IncognitoStateProvider incognitoStateProvider,
+            @Nullable Supplier<Integer> incognitoWindowCountSupplier) {
         super.initialize(
                 toolbarDataProvider,
                 tabController,
@@ -1616,7 +1618,8 @@ public abstract class BraveToolbarLayoutImpl extends ToolbarLayout
                 homeButtonDisplay,
                 extensionToolbarCoordinator,
                 themeColorProvider,
-                incognitoStateProvider);
+                incognitoStateProvider,
+                incognitoWindowCountSupplier);
 
         BraveMenuButtonCoordinator.setMenuFromBottom(
                 isMenuButtonOnBottomControls() || isMenuOnBottomWithBottomAddressBar());
