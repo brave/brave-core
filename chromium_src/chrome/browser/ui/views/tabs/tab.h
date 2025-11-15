445994674e414f64c7744f645d5c226ebc2653b9
Sangwoo Ko <sangwoo108@gmail.com>
2025-11-15 21:21:13 +0900

    Introduce ControllableCloseButtonState
    This is a sort of decorator class that wraps bool showing_close_button_
    so that we can consider additional factors when determining whether to show
    the close button, such as whether the mouse is hovering over the tab,
    or whether the controller wants to always hide the close button.

--- chromium_src/chrome/browser/ui/views/tabs/tab.h
+++ chromium_src/chrome/browser/ui/views/tabs/tab.h
@@ -6,8 +6,34 @@
 #ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_TABS_TAB_H_
 #define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_TABS_TAB_H_
 
+#include "base/memory/raw_ref.h"
+#include "chrome/browser/ui/views/tabs/tab_slot_controller.h"
+
 class BraveTabTest;
 class BraveTab;
+class Tab;
+
+// A struct to wrap the state of whether the close button is shown on a tab,
+// taking into account whether the controller wants to always hide close
+// buttons.
+struct ControllableCloseButtonState final {
+  ControllableCloseButtonState(TabSlotController& controller, Tab& tab);
+  ~ControllableCloseButtonState();
+
+  bool operator=(bool show);
+
+  // Disable "explicit" enforcement to allow implicit conversion to bool.
+  // NOLINTNEXTLINE(google-explicit-constructor)
+  operator bool() const;
+
+  // Controller of the tab that owns this state.
+  raw_ref<TabSlotController> controller;
+
+  // Owner of this state.
+  raw_ref<Tab> tab;
+
+  bool showing_close_button = false;
+};
 
 #define kMinimumContentsWidthForCloseButtons \
   kMinimumContentsWidthForCloseButtons = 55; \
