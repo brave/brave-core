c7bceaad10271318998ab86042622b14f3560b95
Nuo Xu <nuoxu.nx@gmail.com>
2025-11-17 13:39:27 -0500

    Add bridge protocol to handle WalletPageHandler methods in iOS

---
+++ components/brave_wallet/ios/browser/wallet_page_handler_bridge.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_COMPONENTS_BRAVE_WALLET_IOS_BROWSER_WALLET_PAGE_HANDLER_BRIDGE_H_
+#define BRAVE_COMPONENTS_BRAVE_WALLET_IOS_BROWSER_WALLET_PAGE_HANDLER_BRIDGE_H_
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// Browser-side handler for general Brave Wallet UI functions
+///
+/// This is an Obj-C bridge for the PageHandler mojom
+/// interface (see brave_wallet.mojom) and only bridges methods that will be
+/// called in iOS/mobile
+NS_SWIFT_NAME(WalletPageHandler)
+@protocol WalletPageHandlerBridge
+@required
+
+/// Handle when the user taps "Back up now" to back up wallet
+- (void)showWalletBackupUI;
+/// Handle when the user double taps the lock screen to unlock wallet
+- (void)unlockWalletUI;
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#endif  // BRAVE_COMPONENTS_BRAVE_WALLET_IOS_BROWSER_WALLET_PAGE_HANDLER_BRIDGE_H_
