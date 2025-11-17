adbd7dabb85be2c91a9194cbf5396f51e843ee2b
Michael Herrmann <michael@herrmann.io>
2025-11-17 17:41:20 +0100

    Replace mini_string.cc.patch by chromium_src override

---
+++ chromium_src/chrome/installer/mini_installer/mini_string.cc
@@ -0,0 +1,48 @@
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
+#include <chrome/installer/mini_installer/mini_string.cc>
+
+namespace mini_installer {
+
+const wchar_t* SearchStringI(const wchar_t* source, const wchar_t* find) {
+  if (!find || find[0] == L'\0')
+    return source;
+
+  const wchar_t* scan = source;
+  while (*scan) {
+    const wchar_t* s = scan;
+    const wchar_t* f = find;
+
+    while (*s && *f && EqualASCIICharI(*s, *f))
+      ++s, ++f;
+
+    if (!*f)
+      return scan;
+
+    ++scan;
+  }
+
+  return nullptr;
+}
+
+bool FindTagInStr(const wchar_t* str,
+                  const wchar_t* tag,
+                  const wchar_t** position) {
+  int tag_length = ::lstrlen(tag);
+  const wchar_t* scan = str;
+  for (const wchar_t* tag_start = SearchStringI(scan, tag);
+       tag_start != nullptr; tag_start = SearchStringI(scan, tag)) {
+    scan = tag_start + tag_length;
+    if (*scan == L'-' || *scan == L'\0') {
+      if (position != nullptr)
+        *position = tag_start;
+      return true;
+    }
+  }
+  return false;
+}
+
+}  // namespace mini_installer
