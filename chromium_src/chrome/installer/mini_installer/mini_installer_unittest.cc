6406265abe3e8bc137462937abcd487738bcb7f6
Michael Herrmann <michael@herrmann.io>
2025-11-17 17:16:24 +0100

    Replace mini_installer_unittest.cc.patch by chromium_src override

---
+++ chromium_src/chrome/installer/mini_installer/mini_installer_unittest.cc
@@ -0,0 +1,98 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "chrome/installer/mini_installer/mini_installer.h"
+
+#define UnpackBinaryResources(...) UnpackBinaryResources(__VA_ARGS__); \
+  const DWORD expected_windows_error =                               \
+      wcscmp(GetParam().expected_setup_resource_type,                \
+             kLZMAResourceType) == 0 ? kNotAnErrorIsPatchUpdate : 0; \
+      EXPECT_EQ(exit_code.windows_error, expected_windows_error)
+
+#include <chrome/installer/mini_installer/mini_installer_unittest.cc>
+
+#undef UnpackBinaryResources
+
+namespace mini_installer {
+
+namespace {
+
+#define PREVIOUS_VERSION L"62.0.1234.0"
+constexpr wchar_t kPreviousVersion[] = PREVIOUS_VERSION;
+
+class FakeConfiguration : public Configuration {
+  public:
+  FakeConfiguration() { previous_version_ = kPreviousVersion; }
+};
+
+}  // namespace
+
+// A test harness for GetPreviousSetupExePath.
+class GetPreviousSetupExePathTest : public ::testing::Test {
+ public:
+  GetPreviousSetupExePathTest(const GetPreviousSetupExePathTest&) = delete;
+  GetPreviousSetupExePathTest& operator=(const GetPreviousSetupExePathTest&) =
+      delete;
+
+ protected:
+  GetPreviousSetupExePathTest() = default;
+  ~GetPreviousSetupExePathTest() override = default;
+
+  void SetUp() override {
+    ASSERT_NO_FATAL_FAILURE(
+        registry_override_manager_.OverrideRegistry(HKEY_CURRENT_USER));
+  }
+
+  const Configuration& configuration() const { return configuration_; }
+
+  // Writes |path| to the registry in Chrome's ClientState...UninstallString
+  // value.
+  void SetPreviousSetup(const wchar_t* path) {
+    base::win::RegKey key;
+    const install_static::InstallDetails& details =
+        install_static::InstallDetails::Get();
+    ASSERT_EQ(
+        key.Create(HKEY_CURRENT_USER, details.GetClientStateKeyPath().c_str(),
+                   KEY_SET_VALUE | KEY_WOW64_32KEY),
+        ERROR_SUCCESS);
+    ASSERT_EQ(key.WriteValue(installer::kUninstallStringField, path),
+              ERROR_SUCCESS);
+  }
+
+ private:
+  registry_util::RegistryOverrideManager registry_override_manager_;
+  FakeConfiguration configuration_;
+};
+
+// Tests that the path is returned.
+TEST_F(GetPreviousSetupExePathTest, SimpleTest) {
+  static constexpr wchar_t kSetupExePath[] =
+      L"C:\\SomePath\\To\\" PREVIOUS_VERSION L"\\setup.exe";
+  ASSERT_NO_FATAL_FAILURE(SetPreviousSetup(kSetupExePath));
+
+  StackString<MAX_PATH> path;
+  ProcessExitResult result =
+      GetPreviousSetupExePath(configuration(), path.get(), path.capacity());
+  ASSERT_TRUE(result.IsSuccess());
+  EXPECT_STREQ(path.get(), kSetupExePath);
+}
+
+// Tests that quotes are removed, if present.
+TEST_F(GetPreviousSetupExePathTest, QuoteStripping) {
+  static constexpr wchar_t kSetupExePath[] =
+      L"C:\\SomePath\\To\\" PREVIOUS_VERSION L"\\setup.exe";
+  std::wstring quoted_path(L"\"");
+  quoted_path += kSetupExePath;
+  quoted_path += L"\"";
+  ASSERT_NO_FATAL_FAILURE(SetPreviousSetup(quoted_path.c_str()));
+
+  StackString<MAX_PATH> path;
+  ProcessExitResult result =
+      GetPreviousSetupExePath(configuration(), path.get(), path.capacity());
+  ASSERT_TRUE(result.IsSuccess());
+  EXPECT_STREQ(path.get(), kSetupExePath);
+}
+
+}  // namespace mini_installer
