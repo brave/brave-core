4da3405b020e25767e74b7b25f156256a1516387
Andrea <581115+thypon@users.noreply.github.com>
2025-11-17 17:37:02 +0100

    [drumbrake] disable for cr143 breakage (#32337)
    Reference: https://chromium-review.googlesource.com/c/v8/v8/+/7138939

--- build/commands/lib/config.js
+++ build/commands/lib/config.js
@@ -432,11 +432,6 @@ Config.prototype.buildArgs = function () {
     use_siso: this.useSiso,
     use_libfuzzer: this.use_libfuzzer,
     enable_update_notifications: this.isOfficialBuild(),
-    v8_enable_drumbrake:
-      (this.targetArch === 'x64' || this.targetArch === 'arm64')
-      && (this.targetOS === 'win'
-        || this.targetOS === 'linux'
-        || this.targetOS === 'mac'),
   }
 
   if (this.targetOS !== 'ios') {
