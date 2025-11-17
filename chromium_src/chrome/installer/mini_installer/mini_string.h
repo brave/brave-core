fe0b7e9afdcbafa38ec788746d789e75f58c3150
Michael Herrmann <michael@herrmann.io>
2025-11-17 17:37:55 +0100

    Replace mini_string.h.patch by chromium_src override

---
+++ chromium_src/chrome/installer/mini_installer/mini_string.h
@@ -0,0 +1,21 @@
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_INSTALLER_MINI_INSTALLER_MINI_STRING_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_INSTALLER_MINI_INSTALLER_MINI_STRING_H_
+
+#include <chrome/installer/mini_installer/mini_string.h>
+
+namespace mini_installer {
+
+// Case insensitive search of the first occurrence of |find| in |source|.
+const wchar_t* SearchStringI(const wchar_t* source, const wchar_t* find);
+
+// Searches for |tag| within |str|.  Returns true if |tag| is found and is
+// immediately followed by '-' or is at the end of the string.  If |position|
+// is non-nullptr, the location of the tag is returned in |*position| on
+// success.
+bool FindTagInStr(const wchar_t* str,
+                  const wchar_t* tag,
+                  const wchar_t** position);
+
+}
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_INSTALLER_MINI_INSTALLER_MINI_STRING_H_
