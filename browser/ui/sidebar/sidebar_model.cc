756cec306af0f51bf1bf3e226625ea2a7bd8279b
Claudio DeSouza <cdesouza@brave.com>
2025-11-18 11:07:36 +0000

    [cr144] `HistoryServiceObserver::OnURLVisited` updated
    This function now receives a single argument of type `VisitedURLInfo`.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/cb3309d8abc8aceb10bd606ffe66be4edc298645
    
    commit cb3309d8abc8aceb10bd606ffe66be4edc298645
    Author: Tiffany Song <tiffanysong@google.com>
    Date:   Mon Nov 17 08:22:29 2025 -0800
    
        [VL] Add response_code_category to NotifyURLVisited to pass to observers
    
        There are several overrides of HistoryServiceObserver that do not
        include 404 navigations, as 404 visits are not currently inserted into
        History. 404 visits will soon be inserted when
        history::kVisitedLinksOn404 is enabled, and not all of these overrides
        will want to include 404 navigations. In order to know if a visit has a
        404 result, we want to pass response_code_category through OnURLVisited
        and OnURLVisitedWithNavigationId.
    
        This CL also adds a param struct, `VisitedURLInfo`, to store the
        parameters for `NotifyURLVisited` and `OnURLVisited` in order to avoid
        large CLs like this one if more parameters need to be added.
    
        This CL is part of a larger project to make links `:visited` after a 404
        visit, while minimizing disruption to History consumers. Project links:
        - Google-internal project doc: go/stop-filtering-history-by-status-code
        - Issue tree / work status: https://crbug.com/427225439/dependencies
    
        Fixed: 458096507
        Change-Id: I85e201ec046939d987dad7af6683351283e7d67a
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7122997
        Reviewed-by: Nico Weber <thakis@chromium.org>
        Owners-Override: Nico Weber <thakis@chromium.org>
        Commit-Queue: Tiffany Song <tiffanysong@google.com>
        Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
        Reviewed-by: manuk hovanesian <manukh@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1545935}

--- browser/ui/sidebar/sidebar_model.cc
+++ browser/ui/sidebar/sidebar_model.cc
@@ -151,9 +151,9 @@ void SidebarModel::OnItemRemoved(const SidebarItem& item, size_t index) {
   RemoveItemAt(index);
 }
 
-void SidebarModel::OnURLVisited(history::HistoryService* history_service,
-                                const history::URLRow& url_row,
-                                const history::VisitRow& new_visit) {
+void SidebarModel::OnURLVisited(
+    history::HistoryService* history_service,
+    const history::VisitedURLInfo& visited_url_info) {
   for (const auto& item : GetAllSidebarItems()) {
     // Don't try to update builtin items image. It uses bundled one.
     if (item.is_built_in_type()) {
@@ -162,7 +162,7 @@ void SidebarModel::OnURLVisited(history::HistoryService* history_service,
 
     // If same url is added to history service, try to fetch favicon to update
     // for item.
-    if (item.url.host() == url_row.url().host()) {
+    if (item.url.host() == visited_url_info.url_row.url().host()) {
       // Favicon seems cached after this callback.
       // TODO(simonhong): Find more deterministic method instead of using
       // delayed task.
