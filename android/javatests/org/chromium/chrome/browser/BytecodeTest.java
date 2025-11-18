2f851d437c79b8b42798f8ca2ec148e7492659a7
AlexeyBarabash <alexey@brave.com>
2025-11-18 18:03:11 +0200

    [cr144][Android] Changes at LocationBarMediator.ctor
    - new arg navigationAttachmentsCoordinator;
    - autocompleteRequestTypeSupplier is now ObservableSupplier.
    
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/b33b95f74c8ecf2c810888160bcd02ef14d6db30
    
    	Add fusebox metrics
    
    	BYPASS_LARGE_CHANGE_WARNING: boilerplate
    
    	Bug: 457835297
    	Change-Id: I98342570f86aed299ce5ea5d17250179619984d1
    	Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7142169

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -1719,8 +1719,9 @@ public class BytecodeTest {
                         ObservableSupplier.class,
                         BrowserControlsStateProvider.class,
                         Supplier.class,
-                        ObservableSupplierImpl.class,
+                        ObservableSupplier.class,
                         PageZoomIndicatorCoordinator.class,
+                        NavigationAttachmentsCoordinator.class,
                         MultiInstanceManager.class));
         Assert.assertTrue(
                 constructorsMatch(
