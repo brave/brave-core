c6e4b3afa3d3f3ebdb345e276aa96714b4c3dae1
Claudio DeSouza <cdesouza@brave.com>
2025-11-19 11:58:23 +0000

    [cr144] `NavigatorBase` not Supplementable
    This requires our `NavigatorBase` sumplementable classes to be forward
    declared, as a member.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/d99d033c31e934207e45cf88fc01a721e70b56e1
    
    commit d99d033c31e934207e45cf88fc01a721e70b56e1
    Author: Steinar H. Gunderson <sesse@chromium.org>
    Date:   Tue Nov 18 04:56:31 2025 -0800
    
        Make NavigatorBase not Supplementable.
    
        Part of Supplementable cleanup. Done with a script followed by
        human cleanup/review and git cl format.
    
        Bug: 460165754
        Change-Id: If83aa5d85bf53d3c45eb5a02a01edfce2e55dab4
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7157265
        Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
        Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1546446}

--- third_party/blink/renderer/modules/brave/brave.cc
+++ third_party/blink/renderer/modules/brave/brave.cc
@@ -10,13 +10,13 @@
 
 namespace blink {
 
-Brave::Brave(NavigatorBase& navigator) : Supplement<NavigatorBase>(navigator) {}
+Brave::Brave(NavigatorBase& navigator) : navigator_base_(navigator) {}
 
 Brave* Brave::brave(NavigatorBase& navigator) {
-  auto* supplement = Supplement<NavigatorBase>::From<Brave>(navigator);
+  auto supplement = navigator.GetBraveNavigator();
   if (!supplement) {
     supplement = MakeGarbageCollected<Brave>(navigator);
-    Supplement<NavigatorBase>::ProvideTo(navigator, supplement);
+    navigator.SetBraveNavigator(supplement);
   }
   return supplement;
 }
@@ -31,7 +31,7 @@ ScriptPromise<IDLBoolean> Brave::isBrave(ScriptState* script_state) {
 
 void Brave::Trace(Visitor* visitor) const {
   ScriptWrappable::Trace(visitor);
-  Supplement<NavigatorBase>::Trace(visitor);
+  visitor->Trace(navigator_base_);
 }
 
 }  // namespace blink
