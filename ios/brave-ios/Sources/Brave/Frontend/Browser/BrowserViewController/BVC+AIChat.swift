4499418668d945c77bcd99f219bbd400911cba48
Kyle Hickinson <kylehickinson@users.noreply.github.com>
2025-11-19 12:42:21 -0500

    [iOS] Add speech-to-text support for AI Chat Web UI (#32397)

--- ios/brave-ios/Sources/Brave/Frontend/Browser/BrowserViewController/BVC+AIChat.swift
+++ ios/brave-ios/Sources/Brave/Frontend/Browser/BrowserViewController/BVC+AIChat.swift
@@ -5,8 +5,10 @@
 
 import AIChat
 import BraveCore
+import BraveUI
 import Foundation
 import PhotosUI
+import SpeechRecognition
 import UIKit
 import Web
 
@@ -14,7 +16,7 @@ extension BrowserViewController {
   func handleAIChatWebUIPageAction(_ tab: any TabState, action: AIChatWebUIPageAction) {
     switch action {
     case .handleVoiceRecognitionRequest(let completion):
-      completion(nil)
+      handleVoiceRecognitionRequest(completion)
     case .handleFileUploadRequest(_, let completion):
       completion(nil)
     case .presentSettings:
@@ -27,6 +29,83 @@ extension BrowserViewController {
       break
     case .openURL:
       break
+    }
+  }
+
+  private func handleVoiceRecognitionRequest(_ completion: @escaping (String?) -> Void) {
+    if !speechRecognizer.isVoiceSearchAvailable {
+      completion(nil)
+      return
+    }
+
+    // These are the same properties used for the standard search by voice feature on the toolbar
+    onPendingRequestUpdatedCancellable = speechRecognizer.$finalizedRecognition.sink {
+      [weak self] finalizedRecognition in
+      guard let self else {
+        completion(finalizedRecognition)
+        return
+      }
+
+      if let finalizedRecognition {
+        // Feedback indicating recognition is finalized
+        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+        UIImpactFeedbackGenerator(style: .medium).vibrate()
+
+        voiceSearchViewController?.dismiss(animated: true) {
+          self.speechRecognizer.clearSearch()
+          completion(finalizedRecognition)
+        }
+      }
+    }
+
+    Task { @MainActor in
+      if await SpeechRecognizer.requestPermission() {
+        // Pause active playing in PiP when Audio Search is enabled
+        if PlaylistCoordinator.shared.isPictureInPictureActive {
+          PlaylistCoordinator.shared.pauseAllPlayback()
+        }
+
+        voiceSearchViewController = PopupViewController(
+          rootView: SpeechToTextInputView(
+            speechModel: speechRecognizer,
+            disclaimer: Strings.VoiceSearch.screenDisclaimer
+          )
+        )
+
+        if let voiceSearchController = voiceSearchViewController {
+          voiceSearchController.modalTransitionStyle = .crossDissolve
+          voiceSearchController.modalPresentationStyle = .overFullScreen
+          present(voiceSearchController, animated: true)
+        }
+      } else {
+        let alertController = UIAlertController(
+          title: Strings.VoiceSearch.microphoneAccessRequiredWarningTitle,
+          message: Strings.VoiceSearch.microphoneAccessRequiredWarningDescription,
+          preferredStyle: .alert
+        )
+
+        let settingsAction = UIAlertAction(
+          title: Strings.settings,
+          style: .default
+        ) { _ in
+          completion(nil)
+          let url = URL(string: UIApplication.openSettingsURLString)!
+          UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+
+        let cancelAction = UIAlertAction(
+          title: Strings.CancelString,
+          style: .cancel,
+          handler: { _ in
+            completion(nil)
+          }
+        )
+
+        alertController.addAction(settingsAction)
+        alertController.addAction(cancelAction)
+
+        present(alertController, animated: true)
+      }
     }
   }
 }
