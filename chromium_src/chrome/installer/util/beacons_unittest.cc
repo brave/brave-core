478da39ec6ab6848b291ded37a2103b8a2006f17
Michael Herrmann <michael@herrmann.io>
2025-11-20 14:19:27 +0100

    Lint more

--- chromium_src/chrome/installer/util/beacons_unittest.cc
+++ chromium_src/chrome/installer/util/beacons_unittest.cc
@@ -3,6 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+// clang-format off
 #include "brave/chromium_src/chrome/installer/chromium_index_helper_begin.h"
 #include <chrome/installer/util/beacons_unittest.cc>
 #include "brave/chromium_src/chrome/installer/chromium_index_helper_end.h"
+// clang-format on
