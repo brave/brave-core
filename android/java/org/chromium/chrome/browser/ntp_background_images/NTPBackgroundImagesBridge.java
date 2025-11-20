brave/brave-core

commit 75a2652d650c5a7ab91dbb1aa0cf163b20952092

[ads] Remove Android super referrals (#32158)
Remove Android Super Referrals. The feature is no longer supported or
used, and this cleanup helps reduce unused code and simplify the overall
system as part of https://github.com/brave/brave-browser/issues/44403.

android/java/org/chromium/chrome/browser/ntp_background_images/NTPBackgroundImagesBridge.java
@@ -16,20 +16,13 @@ import org.chromium.chrome.browser.BraveRewardsNativeWorker;
 import org.chromium.chrome.browser.ntp_background_images.model.BackgroundImage;
 import org.chromium.chrome.browser.ntp_background_images.model.ImageCredit;
 import org.chromium.chrome.browser.ntp_background_images.model.NTPImage;
-import org.chromium.chrome.browser.ntp_background_images.model.TopSite;
 import org.chromium.chrome.browser.ntp_background_images.model.Wallpaper;
-import org.chromium.chrome.browser.ntp_background_images.util.NewTabPageListener;
 import org.chromium.chrome.browser.profiles.Profile;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class NTPBackgroundImagesBridge {
     private long mNativeNTPBackgroundImagesBridge;
     private final ObserverList<NTPBackgroundImageServiceObserver> mObservers =
             new ObserverList<NTPBackgroundImageServiceObserver>();
-    private static final List<TopSite> sTopSites = new ArrayList<>();
-    private static NewTabPageListener sNewTabPageListener;
 
     public abstract static class NTPBackgroundImageServiceObserver {
         public abstract void onUpdated();
@@ -99,47 +92,6 @@ public class NTPBackgroundImagesBridge {
                         wallpaper.metricType());
     }
 
-    public boolean isSuperReferral() {
-        return NTPBackgroundImagesBridgeJni.get().isSuperReferral(
-                mNativeNTPBackgroundImagesBridge, NTPBackgroundImagesBridge.this);
-    }
-
-    public String getSuperReferralThemeName() {
-        return NTPBackgroundImagesBridgeJni.get().getSuperReferralThemeName(
-                mNativeNTPBackgroundImagesBridge, NTPBackgroundImagesBridge.this);
-    }
-
-    public String getSuperReferralCode() {
-        return NTPBackgroundImagesBridgeJni.get().getSuperReferralCode(
-                mNativeNTPBackgroundImagesBridge, NTPBackgroundImagesBridge.this);
-    }
-
-    public void getTopSites() {
-        sTopSites.clear();
-        NTPBackgroundImagesBridgeJni.get()
-                .getTopSites(mNativeNTPBackgroundImagesBridge, NTPBackgroundImagesBridge.this);
-    }
-
-    public String getReferralApiKey() {
-        return NTPBackgroundImagesBridgeJni.get().getReferralApiKey(
-                mNativeNTPBackgroundImagesBridge, NTPBackgroundImagesBridge.this);
-    }
-
-    public void setNewTabPageListener(NewTabPageListener newTabPageListener) {
-        sNewTabPageListener = newTabPageListener;
-    }
-
-    @CalledByNative
-    public static void loadTopSitesData(
-            String name, String destinationUrl, String backgroundColor, String imagePath) {
-        sTopSites.add(new TopSite(name, destinationUrl, backgroundColor, imagePath));
-    }
-
-    @CalledByNative
-    public static void topSitesLoaded() {
-        sNewTabPageListener.updateTopSites(sTopSites);
-    }
-
     @CalledByNative
     public static BackgroundImage createWallpaper(String imagePath, String author, String link) {
         return new BackgroundImage(imagePath, 0, 0, new ImageCredit(author, link));
@@ -194,19 +146,5 @@ public class NTPBackgroundImagesBridge {
                 String creativeInstanceId,
                 String destinationUrl,
                 int metricType);
-
-        void getTopSites(long nativeNTPBackgroundImagesBridge, NTPBackgroundImagesBridge caller);
-
-        boolean isSuperReferral(
-                long nativeNTPBackgroundImagesBridge, NTPBackgroundImagesBridge caller);
-
-        String getSuperReferralThemeName(
-                long nativeNTPBackgroundImagesBridge, NTPBackgroundImagesBridge caller);
-
-        String getSuperReferralCode(
-                long nativeNTPBackgroundImagesBridge, NTPBackgroundImagesBridge caller);
-
-        String getReferralApiKey(
-                long nativeNTPBackgroundImagesBridge, NTPBackgroundImagesBridge caller);
     }
 }

browser/ntp_background/android/ntp_background_images_bridge.cc
@@ -202,57 +202,6 @@ NTPBackgroundImagesBridge::CreateBrandedWallpaper(
       is_rich_media, static_cast<int>(metric_type));
 }
 
-void NTPBackgroundImagesBridge::GetTopSites(JNIEnv* env,
-                                            const JavaParamRef<jobject>& obj) {
-  std::vector<ntp_background_images::TopSite> top_sites =
-      view_counter_service_ ? view_counter_service_->GetTopSitesData()
-                            : std::vector<ntp_background_images::TopSite>{};
-
-  for (const auto& top_site : top_sites) {
-    Java_NTPBackgroundImagesBridge_loadTopSitesData(
-        env, ConvertUTF8ToJavaString(env, top_site.name),
-        ConvertUTF8ToJavaString(env, top_site.destination_url),
-        ConvertUTF8ToJavaString(env, top_site.background_color),
-        ConvertUTF8ToJavaString(env, top_site.image_file.AsUTF8Unsafe()));
-  }
-
-  Java_NTPBackgroundImagesBridge_topSitesLoaded(env);
-}
-
-bool NTPBackgroundImagesBridge::IsSuperReferral(
-    JNIEnv* env,
-    const JavaParamRef<jobject>& obj) {
-  if (view_counter_service_)
-    return view_counter_service_->IsSuperReferral();
-  return false;
-}
-
-base::android::ScopedJavaLocalRef<jstring>
-NTPBackgroundImagesBridge::GetSuperReferralThemeName(
-    JNIEnv* env,
-    const JavaParamRef<jobject>& obj) {
-  if (view_counter_service_)
-    return ConvertUTF8ToJavaString(
-        env, view_counter_service_->GetSuperReferralThemeName());
-  return ConvertUTF8ToJavaString(env, "");
-}
-
-base::android::ScopedJavaLocalRef<jstring>
-NTPBackgroundImagesBridge::GetSuperReferralCode(
-    JNIEnv* env,
-    const JavaParamRef<jobject>& obj) {
-  if (view_counter_service_)
-    return ConvertUTF8ToJavaString(
-        env, view_counter_service_->GetSuperReferralCode());
-  return ConvertUTF8ToJavaString(env, "");
-}
-
-base::android::ScopedJavaLocalRef<jstring>
-NTPBackgroundImagesBridge::GetReferralApiKey(JNIEnv* env,
-                                             const JavaParamRef<jobject>& obj) {
-  return ConvertUTF8ToJavaString(env, brave_stats::GetAPIKey());
-}
-
 base::android::ScopedJavaLocalRef<jobject>
 NTPBackgroundImagesBridge::GetCurrentWallpaper(
     JNIEnv* env,

android/java/org/chromium/chrome/browser/night_mode/settings/BraveThemePreferences.java
@@ -11,7 +11,6 @@ import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
-import androidx.preference.Preference;
 
 import org.chromium.base.shared_preferences.SharedPreferencesManager;
 import org.chromium.chrome.R;
@@ -19,15 +18,11 @@ import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.night_mode.GlobalNightModeStateProviderHolder;
 import org.chromium.chrome.browser.night_mode.ThemeType;
 import org.chromium.chrome.browser.night_mode.WebContentsDarkModeController;
-import org.chromium.chrome.browser.ntp_background_images.NTPBackgroundImagesBridge;
 import org.chromium.chrome.browser.preferences.ChromeSharedPreferences;
-import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.profiles.ProfileManager;
 import org.chromium.components.browser_ui.settings.SettingsUtils;
 
 public class BraveThemePreferences extends ThemeSettingsFragment {
-
-    private static final String SUPER_REFERRAL = "super_referral";
 
     private boolean mWebContentsDarkModeEnabled;
 
@@ -35,20 +30,6 @@ public class BraveThemePreferences extends ThemeSettingsFragment {
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, String rootKey) {
         SettingsUtils.addPreferencesFromResource(this, R.xml.brave_theme_preferences);
         getActivity().setTitle(getResources().getString(R.string.theme_settings));
-
-        Profile mProfile = ProfileManager.getLastUsedRegularProfile();
-        NTPBackgroundImagesBridge mNTPBackgroundImagesBridge =
-                NTPBackgroundImagesBridge.getInstance(mProfile);
-        if (!NTPBackgroundImagesBridge.enableSponsoredImages()
-                || (mNTPBackgroundImagesBridge != null
-                        && !mNTPBackgroundImagesBridge.isSuperReferral())
-                || Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP) {
-            Preference superReferralPreference =
-                    getPreferenceScreen().findPreference(SUPER_REFERRAL);
-            if (superReferralPreference != null) {
-                getPreferenceScreen().removePreference(superReferralPreference);
-            }
-        }
 
         SharedPreferencesManager sharedPreferencesManager = ChromeSharedPreferences.getInstance();
         BraveRadioButtonGroupThemePreference radioButtonGroupThemePreference =

commit b98a91d8f512998297ce687097e5e8e76b9503a4

[ads] Fixes NTPBackgroundImagesBridge::CreateBrandedWallpaper crash when opening SNTP
Caused by https://github.com/brave/brave-core/pull/32158: I missed some
legacy Android code that retrieves a string from a base::Value::Dict
when parsing super-referrers. Since that string no longer exists, the
code dereferences a null pointer due to missing null checks.

android/java/org/chromium/chrome/browser/ntp_background_images/NTPBackgroundImagesBridge.java
@@ -104,7 +104,6 @@ public class NTPBackgroundImagesBridge {
             int focalPointY,
             String logoPath,
             String logoDestinationUrl,
-            String themeName,
             boolean isSponsored,
             String creativeInstanceId,
             String wallpaperId,
@@ -117,7 +116,6 @@ public class NTPBackgroundImagesBridge {
                 focalPointY,
                 logoPath,
                 logoDestinationUrl,
-                themeName,
                 isSponsored,
                 creativeInstanceId,
                 wallpaperId,

android/java/org/chromium/chrome/browser/ntp_background_images/model/Wallpaper.java
@@ -13,7 +13,6 @@ public class Wallpaper extends NTPImage {
     private final int mFocalPointY;
     private final String mLogoPath;
     private final String mLogoDestinationUrl;
-    private final String mThemeName;
     private final boolean mIsSponsored;
     private final String mCreativeInstanceId;
     private final String mWallpaperId;
@@ -26,7 +25,6 @@ public class Wallpaper extends NTPImage {
             int focalPointY,
             String logoPath,
             String logoDestinationUrl,
-            String themeName,
             boolean isSponsored,
             String creativeInstanceId,
             String wallpaperId,
@@ -37,7 +35,6 @@ public class Wallpaper extends NTPImage {
         mFocalPointY = focalPointY;
         mLogoPath = logoPath;
         mLogoDestinationUrl = logoDestinationUrl;
-        mThemeName = themeName;
         mIsSponsored = isSponsored;
         mCreativeInstanceId = creativeInstanceId;
         mWallpaperId = wallpaperId;
@@ -63,10 +60,6 @@ public class Wallpaper extends NTPImage {
 
     public String getLogoDestinationUrl() {
         return mLogoDestinationUrl;
-    }
-
-    public String getThemeName() {
-        return mThemeName;
     }
 
     public boolean isSponsored() {

browser/ntp_background/android/ntp_background_images_bridge.cc
@@ -154,8 +154,9 @@ NTPBackgroundImagesBridge::CreateBrandedWallpaper(
       data.FindString(ntp_background_images::kWallpaperFilePathKey);
   auto* logo_image_path =
       data.FindStringByDottedPath(ntp_background_images::kLogoImagePath);
-  if (!image_path || !logo_image_path)
+  if (!image_path || !logo_image_path) {
     return base::android::ScopedJavaLocalRef<jobject>();
+  }
 
   auto focal_point_x =
       data.FindInt(ntp_background_images::kWallpaperFocalPointXKey).value_or(0);
@@ -163,13 +164,21 @@ NTPBackgroundImagesBridge::CreateBrandedWallpaper(
       data.FindInt(ntp_background_images::kWallpaperFocalPointYKey).value_or(0);
   auto* logo_destination_url = data.FindStringByDottedPath(
       ntp_background_images::kLogoDestinationURLPath);
-  auto* theme_name = data.FindString(ntp_background_images::kThemeNameKey);
+  if (!logo_destination_url) {
+    return base::android::ScopedJavaLocalRef<jobject>();
+  }
   bool is_sponsored =
       data.FindBool(ntp_background_images::kIsSponsoredKey).value_or(false);
   auto* creative_instance_id =
       data.FindString(ntp_background_images::kCreativeInstanceIDKey);
+  if (!creative_instance_id) {
+    return base::android::ScopedJavaLocalRef<jobject>();
+  }
   const std::string* wallpaper_id =
       data.FindString(ntp_background_images::kWallpaperIDKey);
+  if (!wallpaper_id) {
+    return base::android::ScopedJavaLocalRef<jobject>();
+  }
 
   bool is_rich_media = false;
   if (const std::string* sponsored_rich_media_type =
@@ -186,19 +195,15 @@ NTPBackgroundImagesBridge::CreateBrandedWallpaper(
   }
 
   view_counter_service_->RecordViewedAdEvent(
-      wallpaper_id ? *wallpaper_id : "",
-      creative_instance_id ? *creative_instance_id : "", metric_type);
+      *wallpaper_id, *creative_instance_id, metric_type);
 
   return Java_NTPBackgroundImagesBridge_createBrandedWallpaper(
       env, ConvertUTF8ToJavaString(env, *image_path), focal_point_x,
       focal_point_y, ConvertUTF8ToJavaString(env, *logo_image_path),
-      ConvertUTF8ToJavaString(
-          env, logo_destination_url ? *logo_destination_url : ""),
-      ConvertUTF8ToJavaString(env, *theme_name), is_sponsored,
-      ConvertUTF8ToJavaString(
-          env, creative_instance_id ? *creative_instance_id : ""),
-      ConvertUTF8ToJavaString(env, wallpaper_id ? *wallpaper_id : ""),
-      is_rich_media, static_cast<int>(metric_type));
+      ConvertUTF8ToJavaString(env, *logo_destination_url), is_sponsored,
+      ConvertUTF8ToJavaString(env, *creative_instance_id),
+      ConvertUTF8ToJavaString(env, *wallpaper_id), is_rich_media,
+      static_cast<int>(metric_type));
 }
 
 base::android::ScopedJavaLocalRef<jobject>

components/ntp_background_images/browser/url_constants.h
@@ -17,8 +17,6 @@ inline constexpr char kCustomWallpaperURL[] = "chrome://custom-wallpaper/";
 
 inline constexpr char kCampaignsKey[] = "campaigns";
 
-inline constexpr char kThemeNameKey[] = "themeName";
-
 inline constexpr char kLogoKey[] = "logo";
 
 inline constexpr char kCampaignIdKey[] = "campaignId";