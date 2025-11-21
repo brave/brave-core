4072f416535d28bf104e419e2c7dec54649c170b
Artem Samoilenko <artem@brave.com>
2025-11-21 13:15:21 -0500

    Move BraveReferrer to the referrer folder

--- android/java/org/chromium/chrome/browser/referrer/BraveReferrer.java
+++ android/java/org/chromium/chrome/browser/referrer/BraveReferrer.java
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-package org.chromium.chrome.browser.util;
+package org.chromium.chrome.browser.referrer;
 
 import android.content.Context;
 import android.net.Uri;
@@ -25,6 +25,7 @@ import org.chromium.base.Log;
 import org.chromium.base.task.PostTask;
 import org.chromium.base.task.TaskTraits;
 import org.chromium.chrome.browser.preferences.ChromeSharedPreferences;
+import org.chromium.chrome.browser.util.PackageUtils;
 
 import java.io.File;
 import java.io.FileOutputStream;
--- android/junit/src/org/chromium/chrome/browser/referrer/BraveReferrerTest.java
+++ android/junit/src/org/chromium/chrome/browser/referrer/BraveReferrerTest.java
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-package org.chromium.chrome.browser.util;
+package org.chromium.chrome.browser.referrer;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
