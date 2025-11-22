be34ec499075169d8892ec74158a4e7991801728
Darnell Andries <dandries@brave.com>
2025-11-21 16:51:00 -0800

    Polish policy pref interceptor, add all policies with dynamic_refresh = false

--- chromium_src/components/policy/core/browser/configuration_policy_pref_store.cc
+++ chromium_src/components/policy/core/browser/configuration_policy_pref_store.cc
@@ -3,9 +3,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#define BRAVE_CREATE_PREFERENCES_FROM_POLICIES \
-  pref_interceptor_.InterceptPrefValues(prefs.get());
+#include "components/policy/core/browser/configuration_policy_handler_list.h"
+
+#define ApplyPolicySettings(...)    \
+  ApplyPolicySettings(__VA_ARGS__); \
+  pref_interceptor_.InterceptPrefValues(prefs.get())
 
 #include <components/policy/core/browser/configuration_policy_pref_store.cc>
 
-#undef BRAVE_CREATE_PREFERENCES_FROM_POLICIES
+#undef ApplyPolicySettings
