d4003761dda1700b39573b24bb0c840c352ca114
Szilard Szaloki <szilardszaloki@gmail.com>
2025-11-23 11:09:50 -0700

    Account: add `BraveEndpoint` to endpoint client.

--- components/brave_account/endpoints/login_init.h
+++ components/brave_account/endpoints/login_init.h
@@ -6,23 +6,21 @@
 #ifndef BRAVE_COMPONENTS_BRAVE_ACCOUNT_ENDPOINTS_LOGIN_INIT_H_
 #define BRAVE_COMPONENTS_BRAVE_ACCOUNT_ENDPOINTS_LOGIN_INIT_H_
 
+#include "brave/components/brave_account/endpoint_client/brave_endpoint.h"
 #include "brave/components/brave_account/endpoint_client/is_endpoint.h"
 #include "brave/components/brave_account/endpoint_client/request_types.h"
 #include "brave/components/brave_account/endpoint_client/response.h"
 #include "brave/components/brave_account/endpoints/error.h"
-#include "brave/components/brave_account/endpoints/host.h"
 #include "brave/components/brave_account/endpoints/login_init_request.h"
 #include "brave/components/brave_account/endpoints/login_init_response.h"
-#include "url/gurl.h"
 
 namespace brave_account::endpoints {
 
-struct LoginInit {
-  using Request = endpoint_client::POST<LoginInitRequest>;
-  using Response = endpoint_client::Response<LoginInitResponse, Error>;
-
-  static GURL URL() { return Host().Resolve("/v2/auth/login/init"); }
-};
+using LoginInit = endpoint_client::BraveEndpoint<
+    "accounts.bsg",
+    "/v2/auth/login/init",
+    endpoint_client::POST<LoginInitRequest>,
+    endpoint_client::Response<LoginInitResponse, Error>>;
 
 static_assert(endpoint_client::IsEndpoint<LoginInit>);
 
