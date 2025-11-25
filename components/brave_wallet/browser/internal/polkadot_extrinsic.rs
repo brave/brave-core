12d1fa429e748cf60e3127a1848ed9949343d3ef
Christian Mazakas <christian.mazakas@gmail.com>
2025-11-25 14:56:04 -0800

    remove extraneous extern crate

--- components/brave_wallet/browser/internal/polkadot_extrinsic.rs
+++ components/brave_wallet/browser/internal/polkadot_extrinsic.rs
@@ -3,9 +3,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-extern crate cxx;
-extern crate parity_scale_codec;
-
 use ffi::CxxPolkadotDecodeUnsignedTransfer;
 use parity_scale_codec::{Compact, Decode, Encode};
 use std::fmt;
