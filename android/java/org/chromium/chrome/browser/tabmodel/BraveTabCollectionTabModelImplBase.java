bd5c50dbb271db4e15eebebe31ef74e2bde33e0a
Artem Samoilenko <artem@brave.com>
2025-11-26 07:09:18 -0500

    [cr143][Android] Deprecate TabGroupModelFilterImpl
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/74591e3cce051612058a135c4b7fd55aab1d6320
    
    commit 74591e3cce051612058a135c4b7fd55aab1d6320
    Author: Calder Kitagawa <ckitagawa@chromium.org>
    Date:   Thu Oct 23 07:08:09 2025 -0700
    
        [Tab Collection] Publicize deprecation more loudly
    
        Bug: 454344854
        Change-Id: I7fd5f9af32a76f0c1b8a7e131879d75cbb87dd3e
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7079335
        Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
        Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
        Reviewed-by: Dan Polanco <polardz@google.com>
        Commit-Queue: Dan Polanco <polardz@google.com>
        Cr-Commit-Position: refs/heads/main@{#1534290}

--- android/java/org/chromium/chrome/browser/tabmodel/BraveTabCollectionTabModelImplBase.java
+++ android/java/org/chromium/chrome/browser/tabmodel/BraveTabCollectionTabModelImplBase.java
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 package org.chromium.chrome.browser.tabmodel;
-
+import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.base.BravePreferenceKeys;
 import org.chromium.base.BraveReflectionUtil;
 import org.chromium.build.annotations.Nullable;
@@ -11,15 +11,13 @@ import org.chromium.chrome.browser.preferences.ChromeSharedPreferences;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabLaunchType;
 
-/** Brave's super class for {@link TabGroupModelFilter} */
-public class BraveTabGroupModelFilter {
-    /**
-     * This variable will be used instead of {@link TabGroupModelFilterImpl}'s variable, that will
-     * be deleted in bytecode.
-     */
-    protected boolean mIsResetting;
+/** Brave's super class for {@link TabCollectionTabModelImpl} */
+public abstract class BraveTabCollectionTabModelImplBase extends TabModelJniBridge {
+    public BraveTabCollectionTabModelImplBase(Profile profile) {
+            super(profile);
+    }
 
-    /** Call from {@link TabGroupModelFilterImpl} will be redirected here via bytecode. */
+    /** Call from {@link TabCollectionTabModelImpl} will be redirected here via bytecode. */
     @SuppressWarnings("UnusedMethod")
     protected boolean shouldGroupWithParent(Tab tab, @Nullable Tab parentTab) {
         if (linkClicked(tab.getLaunchType())
@@ -31,14 +29,13 @@ public class BraveTabGroupModelFilter {
                                                 BravePreferenceKeys
                                                         .BRAVE_TAB_GROUPS_ENABLED_DEFAULT_VALUE,
                                                 true))
-                && isTabModelRestored()
-                && !mIsResetting) {
+                && isTabModelRestored()) {
             return true;
         }
         // Otherwise just call parent.
         return (boolean)
                 BraveReflectionUtil.invokeMethod(
-                        TabGroupModelFilterImpl.class,
+                        TabCollectionTabModelImpl.class,
                         this,
                         "shouldGroupWithParent",
                         Tab.class,
@@ -55,6 +52,6 @@ public class BraveTabGroupModelFilter {
     private boolean isTabModelRestored() {
         return (boolean)
                 BraveReflectionUtil.invokeMethod(
-                        TabGroupModelFilterImpl.class, this, "isTabModelRestored");
+                        TabCollectionTabModelImpl.class, this, "isTabModelRestored");
     }
 }
---
+++ build/android/bytecode/java/org/brave/bytecode/BraveTabCollectionTabModelImplClassAdapter.java
@@ -0,0 +1,26 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+package org.brave.bytecode;
+
+import org.objectweb.asm.ClassVisitor;
+
+public class BraveTabCollectionTabModelImplClassAdapter extends BraveClassVisitor {
+    static String sTabCollectionTabModelImplClassName =
+            "org/chromium/chrome/browser/tabmodel/TabCollectionTabModelImpl";
+    static String sBraveTabCollectionTabModelImplBaseClassName =
+            "org/chromium/chrome/browser/tabmodel/BraveTabCollectionTabModelImplBase";
+
+    public BraveTabCollectionTabModelImplClassAdapter(ClassVisitor visitor) {
+        super(visitor);
+
+        changeSuperName(sTabCollectionTabModelImplClassName, sBraveTabCollectionTabModelImplBaseClassName);
+
+        changeMethodOwner(
+                sTabCollectionTabModelImplClassName,
+                "shouldGroupWithParent",
+                sBraveTabCollectionTabModelImplBaseClassName);
+    }
+}
