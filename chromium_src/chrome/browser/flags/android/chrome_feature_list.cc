c200cc4ee3a89a7ba7845e7327588b15471fe090
AlexeyBarabash <alexey@brave.com>
2025-12-03 00:30:18 +0200

    [cr144][Android] Removed kClearBrowsingDataAndroidSurvey flag
    It was disabled by default, so safe to remove
    
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/a0f24061befa70f63a562d2fb2a461731165aa5e
    
    	[QuickDelete] Clean up QD and CBD HaTs surveys
    
    	This cleans up the HaTs survey related code in QD/CBD since the
    	experiment is complete and code is no longer in use.
    
    	Fixed: 394329573
    	Change-Id: I137afbf0bf842a371537aeae68321ee4716d0f9c
    	Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7151302

--- chromium_src/chrome/browser/flags/android/chrome_feature_list.cc
+++ chromium_src/chrome/browser/flags/android/chrome_feature_list.cc
@@ -103,7 +103,6 @@ namespace android {
 
 OVERRIDE_FEATURE_DEFAULT_STATES({{
     {kMagicStackAndroid, base::FEATURE_DISABLED_BY_DEFAULT},
-    {kClearBrowsingDataAndroidSurvey, base::FEATURE_DISABLED_BY_DEFAULT},
 }});
 
 }  // namespace android
