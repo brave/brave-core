ad6b43a4a84bcc5dde4efee2bb6ffb92245d22d5
Brian Clifton <brian@clifton.me>
2025-12-02 12:09:32 -0700

    Fix the path used when looking at registry for some policies (#32628)
    * Fix the path used when looking at registry for UserDataDir policy
    
    This only overrides a specific case for UserDataDir when suffix is not appended.
    
    Fixes https://github.com/brave/brave-browser/issues/41854
    
    Co-authored-by: Simon Hong <shong@brave.com>

--- chromium_src/chrome/install_static/brave_user_data_dir_win_unittest.cc
+++ chromium_src/chrome/install_static/brave_user_data_dir_win_unittest.cc
@@ -20,14 +20,12 @@ inline bool EndsWith(const std::wstring& value, const std::wstring& ending) {
   return std::equal(ending.rbegin(), ending.rend(), value.rbegin());
 }
 
-#if defined(OFFICIAL_BUILD)
 const wchar_t kPolicyRegistryKey[] =
-    L"SOFTWARE\\Policies\\BraveSoftware\\Brave-Browser";
+    L"SOFTWARE\\Policies\\BraveSoftware\\Brave";
+#if defined(OFFICIAL_BUILD)
 const wchar_t kUserDataDirNameSuffix[] =
     L"\\BraveSoftware\\Brave-Browser\\User Data";
 #else
-const wchar_t kPolicyRegistryKey[] =
-    L"SOFTWARE\\Policies\\BraveSoftware\\Brave-Browser-Development";
 const wchar_t kUserDataDirNameSuffix[] =
     L"\\BraveSoftware\\Brave-Browser-Development\\User Data";
 #endif
