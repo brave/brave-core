ec59af774b78b64cc624bfc2ed11ac9e2fafe621
zenparsing <zenparsing@gmail.com>
2025-12-05 14:56:21 -0500

    [Shields UI] Add updated Shields panel front-end

---
+++ components/brave_shields/resources/panel_new/shields_panel.tsx
@@ -0,0 +1,19 @@
+/* Copyright (c) 2026 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import * as React from 'react'
+import { createRoot } from 'react-dom/client'
+import { setIconBasePath } from '@brave/leo/react/icon'
+
+import { AppProvider } from './components/app_context'
+import { App } from './components/app'
+
+setIconBasePath('//resources/brave-icons')
+
+createRoot(document.getElementById('root')!).render(
+  <AppProvider name='shields'>
+    <App />
+  </AppProvider>,
+)
