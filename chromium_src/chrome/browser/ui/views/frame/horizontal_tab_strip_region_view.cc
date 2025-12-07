e2c4e11167d401a9a5e38e8553e5dc7a30dfdbe1
Claudio DeSouza <cdesouza@brave.com>
2025-12-07 22:24:09 +0000

    [cr145] `TabStripRegionView` renamed to `HorizontalTabStripRegionView`
    This is part of the vertical tabs changes. This commit corrects some of
    the overrides and patches we have to target the new file.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/e9874ee393b4da285b89fea40f7fbca07adcc6af
    
    commit e9874ee393b4da285b89fea40f7fbca07adcc6af
    Author: Steven Luong <stluong@chromium.org>
    Date:   Thu Dec 4 11:13:29 2025 -0800
    
        Rename TabStripRegionView to HorizontalTabStripRegionView
    
        NO_IFTTT=Renaming class name
    
        Bypass-Check-License: renaming files
        Bug: 465834207
        Change-Id: Iab1ef113be7ecdae9648c0498e50772bb7d8c9ce
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7226143
        Commit-Queue: Steven Luong <stluong@chromium.org>
        Reviewed-by: Darryl James <dljames@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1554170}

--- chromium_src/chrome/browser/ui/views/frame/horizontal_tab_strip_region_view.cc
+++ chromium_src/chrome/browser/ui/views/frame/horizontal_tab_strip_region_view.cc
@@ -1,13 +1,12 @@
 /* Copyright (c) 2020 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-#include "chrome/browser/ui/views/frame/tab_strip_region_view.h"
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "brave/browser/ui/views/tabs/brave_browser_tab_strip_controller.h"
 #include "brave/browser/ui/views/tabs/brave_new_tab_button.h"
 #include "brave/browser/ui/views/tabs/brave_tab_strip.h"
+#include "chrome/browser/ui/views/frame/tab_strip_region_view.h"
 
 // Currently, `NewTabButton::kButtonSize` is used to calculate tab strip button
 // borders. Since the size of buttons varies depending upon the tabs update
@@ -22,7 +21,7 @@
   auto tab_strip =                                   \
       std::make_unique<BraveTabStrip>(std::move(tabstrip_controller));
 
-#include <chrome/browser/ui/views/frame/tab_strip_region_view.cc>
+#include <chrome/browser/ui/views/frame/horizontal_tab_strip_region_view.cc>
 #undef BRAVE_TAB_STRIP_REGION_VIEW_CREATE_TAB_STRIP
 #undef NewTabButton
 #undef BrowserTabStripController
