fe65eaded1cd4d4861e12ba09dee9da9ef7447ed
Sangwoo Ko <sangwoo108@gmail.com>
2025-12-10 08:32:32 +0900

    Add nullity check for browser_window_features()
    When context menu for vertical tab strip is opened, the
    browser_window_features() can be null, which causes crash.
    This is because our vertical tab strip feature isn't using
    the browser_window_feature() at the time of writing.

--- chromium_src/chrome/browser/ui/views/frame/system_menu_model_delegate.cc
+++ chromium_src/chrome/browser/ui/views/frame/system_menu_model_delegate.cc
@@ -5,11 +5,18 @@
 
 #include "chrome/browser/ui/views/frame/system_menu_model_delegate.h"
 
+#include <string>
+
 #include "brave/app/brave_command_ids.h"
 #include "brave/browser/ui/views/tabs/vertical_tab_utils.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_features.h"
 
 #define IsCommandIdChecked IsCommandIdChecked_ChromiumImpl
+#define GetLabelForCommandId GetLabelForCommandId_ChromiumImpl
+
 #include <chrome/browser/ui/views/frame/system_menu_model_delegate.cc>
+
+#undef GetLabelForCommandId
 #undef IsCommandIdChecked
 
 bool SystemMenuModelDelegate::IsCommandIdChecked(int command_id) const {
@@ -17,4 +24,19 @@ bool SystemMenuModelDelegate::IsCommandIdChecked(int command_id) const {
     return tabs::utils::ShouldShowVerticalTabs(browser_);
   }
   return IsCommandIdChecked_ChromiumImpl(command_id);
+}
+
+std::u16string SystemMenuModelDelegate::GetLabelForCommandId(
+    int command_id) const {
+  if (command_id == IDC_TOGGLE_VERTICAL_TABS) {
+    // Add nullity check for browser_window_features() to avoid crash. At the
+    // time of writing, our vertical tab strip feature isn't using the
+    // browser_window_feature(), so this can be null. In this case, use
+    // our utility function to determine whether vertical tab strip is shown.
+    if (!browser_->browser_window_features()
+             ->vertical_tab_strip_state_controller()) {
+      return l10n_util::GetStringUTF16(IDS_TAB_CXMENU_SHOW_VERTICAL_TABS);
+    }
+  }
+  return GetLabelForCommandId_ChromiumImpl(command_id);
 }
