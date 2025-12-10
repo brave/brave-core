a3fc4af24cc9437a444ac9f7a1454489b871a84a
Claudio DeSouza <cdesouza@brave.com>
2025-12-10 17:20:09 +0000

    [cr145] `TabContextMenuController` taking a delagate
    This class' ctor was taking several thunks, and this is a natural
    simplification.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/b7cbb1ee8aec5f74cc182f28af4eb078942dc038
    
    commit b7cbb1ee8aec5f74cc182f28af4eb078942dc038
    Author: Eshwar Stalin <estalin@chromium.org>
    Date:   Mon Dec 8 11:25:53 2025 -0800
    
        Code cleanup for TabContextMenuController
    
        Introducing a delegate interface instead of using repeating callbacks
        and removing unused GetIndexFromMojomTab function.
    
        Change-Id: I08798c5bec0db474114e4c28df5b4793c0bc635e
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7233448
        Commit-Queue: Eshwar Stalin <estalin@chromium.org>
        Reviewed-by: Charles Meng <charlesmeng@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1555620}

--- browser/ui/tabs/brave_tab_menu_browsertest.cc
+++ browser/ui/tabs/brave_tab_menu_browsertest.cc
@@ -44,22 +44,8 @@ class BraveTabMenuBrowserTest : public InProcessBrowserTest {
             ->tabstrip()
             ->controller());
 
-    auto context_menu_controller = std::make_unique<TabContextMenuController>(
-        base::BindRepeating(
-            &BraveBrowserTabStripController::IsContextMenuCommandChecked,
-            base::Unretained(controller)),
-        base::BindRepeating(
-            &BraveBrowserTabStripController::IsContextMenuCommandEnabled,
-            base::Unretained(controller), tab_index),
-        base::BindRepeating(
-            &BraveBrowserTabStripController::IsContextMenuCommandAlerted,
-            base::Unretained(controller)),
-        base::BindRepeating(
-            &BraveBrowserTabStripController::ExecuteContextMenuCommand,
-            base::Unretained(controller), tab_index),
-        base::BindRepeating(
-            &BraveBrowserTabStripController::GetContextMenuAccelerator,
-            base::Unretained(controller)));
+    auto context_menu_controller =
+        std::make_unique<TabContextMenuController>(tab_index, controller);
 
     return context_menu_controller;
   }
