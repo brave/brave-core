aced9d8307e59525632e833a7290a287cf41e4a5
Christian Mazakas <christian.mazakas@gmail.com>
2025-12-11 11:02:11 -0800

    add missing license to test file

--- components/brave_wallet/browser/polkadot/polkadot_tx_meta_unittest.cc
+++ components/brave_wallet/browser/polkadot/polkadot_tx_meta_unittest.cc
@@ -1,3 +1,8 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
 #include "brave/components/brave_wallet/browser/polkadot/polkadot_tx_meta.h"
 
 #include "base/strings/string_number_conversions.h"
