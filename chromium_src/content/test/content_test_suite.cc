be9d78bef58f173a5af877669bd599cd0096641a
Emerick Rogul <erogul@brave.com>
2025-12-11 11:14:55 -0500

    Disable field trial CHECK for field trial flags we always disable
    Code was recently added upstream to `ContentTestSuite::ContentTestSuite` to
    disable field trial flags that cause test-related failures. As part of disabling
    these flags, there is an initial `CHECK` to confirm that the flags are currently
    enabled. However, one of the flags being checked is one that we always disabled,
    so this `CHECK` will always fail for us as written. To work around this, we
    create an override to exclude flags that we know are already disabled (currently
    only one flag, but it may grow in the future).
    
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/e272b2553614e4d44452ea2d3864e24b071993a0
    
    commit e272b2553614e4d44452ea2d3864e24b071993a0
    Author: Arthur Sonzogni <arthursonzogni@chromium.org>
    Date:   Tue Sep 30 12:26:36 2025 -0700
    
        Reland "Enable field trial testing config in content_unittests"
    
        This is a reland of commit 4ac59628bf3b500cf5822b331d139d2ae7bce20f
    
        Diff:
        - Disable when using sanitizers, due to memory safety issues detected.
          Mostly invalid downcasts. I will fix them in a follow-up.
        - Enable `AllowSwiftShaderFallback` to avoid a test to fail on mac.
        - Revert line about https://crbug.com/447306958, because it has been
          fixed in between now and the revert.
    
        --- Heads-up for the CQ sheriff: this patch is likely not to stick on
        its very second attempt. If you see tests failing content_unittests,
        please don't hesitate to revert this one in priority
        ---
    
        Enable field trial testing config in content_unittests
    
        When rolling out a new feature using field trials, Chrome engineers have
        to edit the file testing/variations/fieldtrial_testing_config.json so
        that their feature is somewhat properly tested before reaching
        Dev/Beta/Stable users.
    
        Currently, the field trial testing config is properly wired into
        content_shell based tests like web tests, content_browsertest,
        components_browsertests, etc...
    
        However, coverage is missing in unittests.
    
        This patch call the previously extracted function to do the proper
        setup. We had to exclude some failing features.
    
        Cq-Include-Trybots: luci.chromium.try:linux_chromium_asan_rel_ng
        Cq-Include-Trybots: luci.chromium.try:linux_chromium_msan_rel_ng
        Cq-Include-Trybots: luci.chromium.try:linux_chromium_tsan_rel_ng
        Cq-Include-Trybots: luci.chromium.try:linux_chromium_ubsan_rel_ng
        Cq-Include-Trybots: luci.chromium.try:mac13-arm64-rel
        Cq-Include-Trybots: luci.chromium.try:mac14-arm64-rel
        Cq-Include-Trybots: luci.chromium.try:mac15-arm64-rel
        Bug: 40105939
        Change-Id: Ieded8349c16031996839adb746c3bef9c2720da0
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6989214
        Reviewed-by: Nasko Oskov <nasko@chromium.org>
        Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
        Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1523064}

---
+++ chromium_src/content/test/content_test_suite.cc
@@ -0,0 +1,34 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "base/sanitizer_buildflags.h"
+#include "build/build_config.h"
+
+#if !BUILDFLAG(IS_WIN) && !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(USING_SANITIZER)
+#include <array>
+#include <string_view>
+
+#include "base/containers/contains.h"
+#include "base/feature_list.h"
+
+namespace {
+constexpr std::array<std::string_view, 1> kFieldTrialExceptions = {
+    "FledgeEnforceKAnonymity",
+};
+}
+
+// `ContentTestSuite` has a CHECK to ensure that the field trial flags it
+// disables are currently enabled. However, there are certain field trial flags
+// that we always disable, so we must skip this CHECK for those flags.
+#define GetEnabledFieldTrialByFeatureName(FEATURE) \
+  GetEnabledFieldTrialByFeatureName(FEATURE) ||    \
+      base::Contains(kFieldTrialExceptions, FEATURE)
+#endif
+
+#include <content/test/content_test_suite.cc>
+
+#if !BUILDFLAG(IS_WIN) && !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(USING_SANITIZER)
+#undef GetEnabledFieldTrialByFeatureName
+#endif
