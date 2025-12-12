f6e6d5b3bcea929ae32391aef571c401b5e0b40e
Artem Samoilenko <artem@brave.com>
2025-12-12 08:32:30 -0500

    [cr145][Android] Convert ToolbarManager.ConstraintsSupplier to a TransitiveObservableSupplier
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/86102b4bc68e75d3d392be417f8208697701184e
    
    commit 86102b4bc68e75d3d392be417f8208697701184e
    Author: Andrew Grieve <agrieve@chromium.org>
    Date:   Wed Dec 10 14:29:26 2025 -0800
    
        Convert ToolbarManager.ConstraintsSupplier to a TransitiveObservableSupplier
    
        Bug: 467389696
        Change-Id: Iccda1bc4dd1f13c7d5444364f44f8ce5de417d5f
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7242146
        Commit-Queue: Andrew Grieve <agrieve@chromium.org>
        Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1556976}

---
+++ android/java/org/chromium/chrome/browser/tab/BraveTabBrowserControlsConstraintsHelper.java
@@ -0,0 +1,19 @@
+/* Copyright (c) 2026 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+package org.chromium.chrome.browser.tab;
+
+import org.chromium.base.supplier.ObservableSupplier;
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
+
+@NullMarked
+public class BraveTabBrowserControlsConstraintsHelper {
+    /** Returns the constraints delegate for a particular tab with destroy guard. */
+    public static @Nullable ObservableSupplier<Integer> getObservableConstraints(Tab tab) {
+        if (tab == null || tab.isDestroyed()) return null;
+        return TabBrowserControlsConstraintsHelper.getObservableConstraints(tab);
+    }
+}
