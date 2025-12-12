7073453042f4ba1643ec34db0c63502008254e6f
Artem Samoilenko <artem@brave.com>
2025-12-12 08:32:30 -0500

    [cr145][Android] Convert ToolbarManager.ConstraintsSupplier to a TransitiveObservableSupplier
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/86102b4bc68e75d3d392be417f8208697701184e
    
    commit 86102b4bc68e75d3d392be417f8208697701184e
    Author: Andrew Grieve <agrieve@chromium.org>
    Date:   Wed Dec 10 14:29:26 2025 -0800
    
        Convert ToolbarManager.ConstraintsSupplier to a TransitiveObservableSupplier
    
        Bug: 467389696
        Change-Id: Iccda1bc4dd1f13c7d5444364f44f8ce5de417d5f
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7242146
        Commit-Queue: Andrew Grieve <agrieve@chromium.org>
        Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1556976}

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -361,6 +361,8 @@ public class BytecodeTest {
         Assert.assertTrue(
                 classExists("org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator"));
         Assert.assertTrue(
+                classExists("org/chromium/chrome/browser/tab/TabBrowserControlsConstraintsHelper"));
+        Assert.assertTrue(
                 classExists(
                         "org/chromium/chrome/browser/incognito/reauth/TabSwitcherIncognitoReauthCoordinator")); // presubmit: ignore-long-line
         Assert.assertTrue(
@@ -1315,6 +1317,13 @@ public class BytecodeTest {
                         MethodModifier.STATIC,
                         int.class,
                         int.class));
+        Assert.assertTrue(
+                methodExists(
+                        "org/chromium/chrome/browser/tab/TabBrowserControlsConstraintsHelper",
+                        "getObservableConstraints",
+                        MethodModifier.STATIC,
+                        ObservableSupplier.class,
+                        Tab.class));
         // NOTE: Add new checks above. For each new check in this method add proguard exception in
         // `brave/android/java/proguard.flags` file under `Add methods for invocation below`
         // section. Both test and regular apks should have the same exceptions.
@@ -1530,7 +1539,7 @@ public class BytecodeTest {
                         Supplier.class,
                         HistoryDelegate.class,
                         boolean.class,
-                        ObservableSupplier.class,
+                        NullableObservableSupplier.class,
                         ObservableSupplier.class,
                         BrowserStateBrowserControlsVisibilityDelegate.class,
                         FullscreenManager.class,
