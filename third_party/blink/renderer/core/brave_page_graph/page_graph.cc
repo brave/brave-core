d297b33a2a69b9b4fdbe65334b41487bcea33cbe
Claudio DeSouza <cdesouza@brave.com>
2025-12-13 02:01:14 +0000

    [cr145] Revert "[WIP][cr144] LocalFrame not Supplementable"
    This reverts commit 20c28a21ae89a53339e5e6882559b62adcbd8a8a.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/0b6fd2e4ba354c8b550caf51e725e5b7c6c4338e
    
    commit 0b6fd2e4ba354c8b550caf51e725e5b7c6c4338e
    Author: Steinar H. Gunderson <sesse@chromium.org>
    Date:   Thu Dec 11 10:20:27 2025 -0800
    
        Revert "Make LocalFrame not Supplementable."
    
        This reverts commit 209a1e822a43055ed8beff03a552a492ea024d2f.
        (See blink-dev for background.)
    
        Bug: 460165754
        Change-Id: I88925508a4f11a989747c2d7d61e165dd51db159
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7245785
        Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
        Reviewed-by: Daniel Cheng <dcheng@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1557521}

--- third_party/blink/renderer/core/brave_page_graph/page_graph.cc
+++ third_party/blink/renderer/core/brave_page_graph/page_graph.cc
@@ -240,9 +240,9 @@ PageGraph* GetPageGraphFromIsolate(v8::Isolate* isolate) {
 
   if (auto* top_local_frame =
           blink::DynamicTo<blink::LocalFrame>(&frame->Tree().Top())) {
-    return top_local_frame->GetPageGraph();
+    return blink::PageGraph::From(*top_local_frame);
   } else {
-    return frame->GetPageGraph();
+    return blink::PageGraph::From(*frame);
   }
 }
 
@@ -341,11 +341,16 @@ static void AssignSecurityOriginToNodeDOMRoot(
 }  // namespace
 
 // static
+PageGraph* PageGraph::From(LocalFrame& frame) {
+  return Supplement<LocalFrame>::From<PageGraph>(frame);
+}
+
+// static
 void PageGraph::ProvideTo(LocalFrame& frame) {
   if (!base::FeatureList::IsEnabled(brave_page_graph::features::kPageGraph)) {
     return;
   }
-  CHECK(!frame.GetPageGraph());
+  CHECK(!PageGraph::From(frame));
   CHECK(frame.IsLocalRoot());
   Page* page = frame.GetPage();
   CHECK(page);
@@ -357,8 +362,8 @@ void PageGraph::ProvideTo(LocalFrame& frame) {
     blink::Node* initiator_node =
         blink::DOMNodeIds::NodeForId(initiator_dom_node_id);
     if (initiator_node) {
-      auto initiator_tree_page_graph =
-          initiator_node->TreeRoot().GetDocument().GetFrame()->GetPageGraph();
+      auto* initiator_tree_page_graph = Supplement<LocalFrame>::From<PageGraph>(
+          *initiator_node->TreeRoot().GetDocument().GetFrame());
       if (initiator_tree_page_graph) {
         frame.GetProbeSink()->AddPageGraph(initiator_tree_page_graph);
         return;
@@ -372,7 +377,8 @@ void PageGraph::ProvideTo(LocalFrame& frame) {
     if (!main_local_frame) {
       continue;
     }
-    auto related_page_graph = main_local_frame->GetPageGraph();
+    auto* related_page_graph =
+        Supplement<LocalFrame>::From<PageGraph>(main_local_frame);
     if (related_page_graph) {
       frame.GetProbeSink()->AddPageGraph(related_page_graph);
       return;
@@ -385,7 +391,8 @@ void PageGraph::ProvideTo(LocalFrame& frame) {
     if (!main_local_frame) {
       continue;
     }
-    auto ordinary_page_graph = main_local_frame->GetPageGraph();
+    auto* ordinary_page_graph =
+        Supplement<LocalFrame>::From<PageGraph>(main_local_frame);
     if (ordinary_page_graph) {
       frame.GetProbeSink()->AddPageGraph(ordinary_page_graph);
       return;
@@ -395,11 +402,11 @@ void PageGraph::ProvideTo(LocalFrame& frame) {
   // Create a new PageGraph for the current frame.
   PageGraph* page_graph = MakeGarbageCollected<PageGraph>(frame);
   frame.GetProbeSink()->AddPageGraph(page_graph);
-  frame.SetPageGraph(page_graph);
+  Supplement<LocalFrame>::ProvideTo(frame, page_graph);
 }
 
 PageGraph::PageGraph(LocalFrame& local_frame)
-    : local_frame_(local_frame),
+    : Supplement<LocalFrame>(local_frame),
       frame_id_(GetFrameId(local_frame)),
       script_tracker_(this),
       request_tracker_(this) {
@@ -437,7 +444,7 @@ PageGraph::PageGraph(LocalFrame& local_frame)
 PageGraph::~PageGraph() = default;
 
 void PageGraph::Trace(blink::Visitor* visitor) const {
-  visitor->Trace(local_frame_);
+  Supplement<LocalFrame>::Trace(visitor);
   visitor->Trace(execution_context_nodes_);
   visitor->Trace(processed_js_urls_);
 }
@@ -961,7 +968,8 @@ void PageGraph::ConsoleMessageAdded(blink::ConsoleMessage* console_message) {
     blink::LocalFrame* frame = console_message->Frame();
     blink::Document* document = frame ? frame->GetDocument() : nullptr;
     if (!document) {
-      document = local_frame_->GetDocument();
+      frame = GetSupplementable();
+      document = frame->GetDocument();
       if (!document) {
         return nullptr;
       }
@@ -2163,7 +2171,7 @@ NodeBinding* PageGraph::GetBindingNode(const Binding binding,
 }
 
 bool PageGraph::IsRootFrame() const {
-  return local_frame_->IsLocalRoot();
+  return GetSupplementable()->IsLocalRoot();
 }
 
 }  // namespace blink
