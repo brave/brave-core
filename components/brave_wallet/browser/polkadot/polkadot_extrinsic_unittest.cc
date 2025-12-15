0658d742ed698fb42bf7857701cb901118e756a3
Christian Mazakas <5543573+cmazakas@users.noreply.github.com>
2025-12-15 15:20:37 -0800

    [Polkadot] add extrinsic signing (#32618)
    Resolves: brave/brave-browser#51480
    
    Add routines that generate the signature payload and add a routine that
    assembles the final signed extrinsic for sending to the validators for inclusion
    into the blockchain.
    
    See: https://spec.polkadot.network/id-extrinsics#defn-extrinsic-signature

--- components/brave_wallet/browser/polkadot/polkadot_extrinsic_unittest.cc
+++ components/brave_wallet/browser/polkadot/polkadot_extrinsic_unittest.cc
@@ -6,6 +6,8 @@
 #include "brave/components/brave_wallet/browser/polkadot/polkadot_extrinsic.h"
 
 #include "base/strings/string_number_conversions.h"
+#include "brave/components/brave_wallet/browser/internal/hd_key_sr25519.h"
+#include "brave/components/brave_wallet/common/hex_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace brave_wallet {
@@ -28,6 +30,14 @@ inline constexpr const char kWestendChainType[] = "Westend";
 inline constexpr const char kPolkadotChainType[] = "Polkadot";
 inline constexpr const char kWestendAssetHubChainType[] = "Westend Asset Hub";
 inline constexpr const char kPolkadotAssetHubChainType[] = "Polkadot Asset Hub";
+
+// Taken from:
+// https://docs.rs/schnorrkel/0.11.4/schnorrkel/keys/struct.MiniSecretKey.html#method.from_bytes
+constexpr uint8_t kSchnorrkelSeed[] = {
+    157, 97,  177, 157, 239, 253, 90, 96,  186, 132, 74,
+    244, 146, 236, 44,  196, 68,  73, 197, 105, 123, 50,
+    105, 25,  112, 59,  172, 3,   28, 174, 127, 96,
+};
 
 }  // namespace
 
@@ -401,6 +411,220 @@ TEST(PolkadotExtrinsics, InvalidDecodeFromIncompatibleParachain) {
                                                 testnet_extrinsic));
   EXPECT_FALSE(PolkadotUnsignedTransfer::Decode(mainnet_assethub_metadata,
                                                 testnet_extrinsic));
+}
+
+TEST(PolkadotExtrinsics, MortalityEncoding) {
+  // clang-format off
+  /*
+    Test vectors were generated using the polkadot-sdk:
+
+    use parity_scale_codec::Encode;
+    use polkadot_sdk::frame_support::sp_runtime::generic::Era;
+
+    fn to_hex_string(xs: &[u8]) -> String {
+        xs.iter().map(|b| format!("{b:02x}")).collect()
+    }
+
+    let era = Era::mortal(64, 0);
+    let x = era.encode();
+    assert_eq!(to_hex_string(&x), "0500");
+
+    ...
+  */
+  // clang-format on
+
+  // Test numeric limits for the block number, from 0 to u32::MAX.
+  auto x = scale_encode_mortality(0, 64);
+  EXPECT_EQ(base::HexEncodeLower(x), "0500");
+
+  x = scale_encode_mortality(std::numeric_limits<uint32_t>::max(), 64);
+  EXPECT_EQ(base::HexEncodeLower(x), "f503");
+
+  // Test numeric limits for the period, which is defined to be 4 to 1 << 16.
+  x = scale_encode_mortality(1234, 4);
+  EXPECT_EQ(base::HexEncodeLower(x), "2100");
+
+  x = scale_encode_mortality(1234, 3);
+  EXPECT_EQ(base::HexEncodeLower(x), "2100");
+
+  x = scale_encode_mortality(1234, 0);
+  EXPECT_EQ(base::HexEncodeLower(x), "2100");
+
+  x = scale_encode_mortality(1234, 1 << 16);
+  EXPECT_EQ(base::HexEncodeLower(x), "df04");
+
+  x = scale_encode_mortality(1234, (1 << 16) - 1);
+  EXPECT_EQ(base::HexEncodeLower(x), "df04");
+
+  x = scale_encode_mortality(1234, 1 << 17);
+  EXPECT_EQ(base::HexEncodeLower(x), "df04");
+}
+
+TEST(PolkadotExtrinsics, SignaturePayload) {
+  auto testnet_metadata =
+      PolkadotChainMetadata::FromChainName(kWestendChainType).value();
+
+  auto mainnet_metadata =
+      PolkadotChainMetadata::FromChainName(kPolkadotChainType).value();
+
+  uint32_t sender_nonce = 2;
+
+  uint128_t send_amount = 1234;
+  std::array<uint8_t, 16> send_amount_bytes = {};
+  base::span(send_amount_bytes)
+      .copy_from(base::byte_span_from_ref(send_amount));
+
+  std::array<uint8_t, 32> recipient = {};
+  base::HexStringToSpan(kBob, recipient);
+
+  uint32_t spec_version = 1020001;
+  uint32_t transaction_version = 27;
+
+  uint32_t block_number = 0x1b41217;
+
+  std::array<uint8_t, 32> genesis_hash = {};
+  EXPECT_TRUE(PrefixedHexStringToFixed(
+      "0xe143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e",
+      genesis_hash));
+
+  std::array<uint8_t, 32> block_hash = {};
+  EXPECT_TRUE(PrefixedHexStringToFixed(
+      "0xbdcb3205ee391126e758556ffef5bb0d5a5fd1bbd996c671a079d5b02a671913",
+      block_hash));
+
+  auto encoded = generate_extrinsic_signature_payload(
+      *testnet_metadata, sender_nonce, send_amount_bytes, recipient,
+      spec_version, transaction_version, block_number, genesis_hash,
+      block_hash);
+
+  constexpr const char kExpected[] =
+      R"(0400008eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a484913750108000061900f001b000000e143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423ebdcb3205ee391126e758556ffef5bb0d5a5fd1bbd996c671a079d5b02a67191300)";
+
+  EXPECT_EQ(base::HexEncodeLower(encoded), kExpected);
+}
+
+TEST(PolkadotExtrinsics, SignedExtrinsic) {
+  auto testnet_metadata =
+      PolkadotChainMetadata::FromChainName(kWestendChainType).value();
+
+  std::array<uint8_t, 32> recipient = {};
+  base::HexStringToSpan(kBob, recipient);
+
+  uint128_t send_amount = 1234;
+  uint32_t spec_version = 1020001;
+  uint32_t transaction_version = 27;
+
+  uint32_t sender_nonce = 1;
+  uint32_t block_number = 28794326;
+  const char block_hash_encoded[] =
+      R"(0x4b12cf2089483b06ea4fab577067bebe0e936dbb5317232d65617ab3af7fa425)";
+
+  const char genesis_hash_encoded[] =
+      R"(0xe143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e)";
+
+  PolkadotUnsignedTransfer transfer(recipient, send_amount);
+
+  uint32_t account_index = 0;
+
+  auto keypair = HDKeySr25519::GenerateFromSeed(kSchnorrkelSeed);
+  keypair = keypair.DeriveHard(base::byte_span_from_cstring("\x1cwestend"));
+  keypair = keypair.DeriveHard(base::byte_span_from_ref(account_index));
+  EXPECT_EQ(base::HexEncodeLower(keypair.GetPublicKey()),
+            "d4f9c4dfa3e6ff57b4e1fdea8699e57b0210cf04afe0281acba187d7d1b49274");
+
+  keypair.UseMockRngForTesting();
+
+  std::array<uint8_t, 16> send_amount_bytes = {};
+  base::span(send_amount_bytes)
+      .copy_from(base::byte_span_from_ref(send_amount));
+
+  std::array<uint8_t, 32> genesis_hash = {};
+  EXPECT_TRUE(PrefixedHexStringToFixed(genesis_hash_encoded, genesis_hash));
+
+  std::array<uint8_t, 32> block_hash = {};
+  EXPECT_TRUE(PrefixedHexStringToFixed(block_hash_encoded, block_hash));
+
+  auto signature_payload = generate_extrinsic_signature_payload(
+      *testnet_metadata, sender_nonce, send_amount_bytes, recipient,
+      spec_version, transaction_version, block_number, genesis_hash,
+      block_hash);
+
+  auto signature = keypair.SignMessage(signature_payload);
+
+  EXPECT_TRUE(keypair.VerifyMessage(signature, signature_payload));
+
+  const char expected_signatured[] =
+      R"(441018831cb0c3977e5e15c1fe632cfb2eeb6147edef9c5d83005df0686fcb64358416735e42f72c0666f8b37fc53d55d4def2b321ef3e143480423ba70d9381)";
+  EXPECT_EQ(base::HexEncodeLower(signature), expected_signatured);
+
+  auto signed_extrinsic = make_signed_extrinsic(
+      *testnet_metadata, keypair.GetPublicKey(), recipient, send_amount_bytes,
+      signature, block_number, sender_nonce);
+
+  auto extrinsic = base::HexEncodeLower(signed_extrinsic);
+
+  // clang-format off
+  /*
+    This extrinsic lives at the testnet here:
+    https://westend.subscan.io/extrinsic/28794338-2
+
+    This can be verified using:
+
+      curl \
+        -H "Content-Type: application/json" \
+        -d "{\"id\":1, \"jsonrpc\":\"2.0\", \"method\": \"chain_getBlock\", \"params\": [\"0x2a569559aa25d5f70760d412af400fb6c05e9c7fed2b161a159b2873662f4f0a\"]}" \
+      https://westend-rpc.polkadot.io
+
+    The response should contain an array of committed transactions, including the one we have below:
+    {
+      "jsonrpc": "2.0",
+      "id": 1,
+      "result": {
+        "block": {
+          "header": {
+            "parentHash": "0x8dcce8c540f1bf9a976f2aa060979156795b9d8c0b2db7c39060bc127c183685",
+            "number": "0x1b75de2", // 28794338 in decimal.
+            "stateRoot": "0x07c8cdac7003abc2487eca5c45b76f44a73a05d61ff4f6942ad004404178cfe6",
+            "extrinsicsRoot": "0x192e5a4077f3a9c7c2bc8779188046fedea352e16622488488aa8cd015e94c56",
+            "digest": {
+              "logs": [
+                "0x0642414245b5010313000000a18c8811000000000e42c1210f073986729415c04138fc8210574d116d45039f09a39755c6ac9f50b15416c75cb23c5cee9a9d85eca1c1e680693b26c53a29c2f4cf42c690c5b501fe302e8df120917052b1b1aa014441d2c02d94ec5c522afd1e5eb291dc8ad407",
+                "0x04424545468403b9409da31bff3549cd873c7c9f283ba4a8ef2e87cfdaf0e8fc406c05bde8e583",
+                "0x05424142450101c89d69dc34a07575db519d335e5f017ec5e770eee528a39c7c64b3dde8d5f618fb0808a143f082627d7176172c0f1c64b3ed122cab86f87215ce32402fc16380"
+              ]
+            }
+          },
+          "extrinsics": [
+            "0x280502000b70fd5ff09a01",
+            ...,
+            "0x35028400d4f9c4dfa3e6ff57b4e1fdea8699e57b0210cf04afe0281acba187d7d1b4927401441018831cb0c3977e5e15c1fe632cfb2eeb6147edef9c5d83005df0686fcb64358416735e42f72c0666f8b37fc53d55d4def2b321ef3e143480423ba70d938165010400000400008eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a484913"
+          ]
+        },
+        "justifications": null
+      }
+    }
+  */
+  // clang-format on
+
+  std::string_view expected_extrinsic =
+      "3502"  // SCALE-encoded length.
+      "84"    // Sign bit set (0x80), extrinsic version (0x04).
+      "00"    // MultiAddress type.
+      "d4f9c4dfa3e6ff57b4e1fdea8699e57b0210cf04afe0281acba187d7d1b49274"
+      "01"  // Signature type (sr25519).
+      "441018831cb0c3977e5e15c1fe632cfb2eeb6147edef9c5d83005df0686fcb64"
+      "358416735e42f72c0666f8b37fc53d55d4def2b321ef3e143480423ba70d9381"
+      "6501"  // MortalEra
+      "04"    // SCALE-encoded nonce.
+      "00"    // Tip.
+      "00"    // Mode (disable metadata hash checking).
+      "0400"  // Pallet index, call index.
+      "00"    // MultiAddress type.
+      "8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48"
+      "4913"  // SCALE-encoded send amount.
+      ;
+
+  EXPECT_EQ(extrinsic, expected_extrinsic);
 }
 
 }  // namespace brave_wallet
