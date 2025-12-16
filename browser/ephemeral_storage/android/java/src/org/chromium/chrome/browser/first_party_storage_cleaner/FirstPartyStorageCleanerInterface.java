3560119e56f9b3debad26341cf07a6a3249f9695
Vadym Struts <vstruts@brave.com>
2025-12-16 19:13:46 +0100

    review renamed package to first_party_storage_cleaner
    Signed-off-by: Vadym Struts <vstruts@brave.com>

--- browser/ephemeral_storage/android/java/src/org/chromium/chrome/browser/first_party_storage_cleaner/FirstPartyStorageCleanerInterface.java
+++ browser/ephemeral_storage/android/java/src/org/chromium/chrome/browser/first_party_storage_cleaner/FirstPartyStorageCleanerInterface.java
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-package org.chromium.chrome.browser.ephemeral_storage;
+package org.chromium.chrome.browser.first_party_storage_cleaner;
 
 import org.chromium.build.annotations.NullMarked;
 
