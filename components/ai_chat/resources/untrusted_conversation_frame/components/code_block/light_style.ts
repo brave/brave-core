b5726bdc11192491ed176d84641012740bbf85fa
Darnell Andries <dandries@brave.com>
2025-12-16 19:20:38 -0800

    [AI Chat] Code execution improvements (#32827)
    * [AI Chat] Code execution improvements
    
    * Use github themes for aichat code block
    
    * Address PR feedback

---
+++ components/ai_chat/resources/untrusted_conversation_frame/components/code_block/light_style.ts
@@ -0,0 +1,118 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+// Properties sourced from https://github.com/highlightjs/highlight.js/blob/main/src/styles/github.css
+
+export default {
+  'hljs': {
+    'display': 'block',
+    'overflowX': 'auto',
+    'padding': '0.5em',
+    'color': '#24292e',
+    'background': '#ffffff',
+  },
+  'hljs-comment': {
+    'color': '#6a737d',
+  },
+  'hljs-quote': {
+    'color': '#22863a',
+  },
+  'hljs-keyword': {
+    'color': '#d73a49',
+  },
+  'hljs-selector-tag': {
+    'color': '#22863a',
+  },
+  'hljs-subst': {
+    'color': '#24292e',
+    'fontWeight': 'normal',
+  },
+  'hljs-number': {
+    'color': '#005cc5',
+  },
+  'hljs-literal': {
+    'color': '#005cc5',
+  },
+  'hljs-variable': {
+    'color': '#005cc5',
+  },
+  'hljs-template-variable': {
+    'color': '#d73a49',
+  },
+  'hljs-tag .hljs-attr': {
+    'color': '#005cc5',
+  },
+  'hljs-string': {
+    'color': '#032f62',
+  },
+  'hljs-doctag': {
+    'color': '#d73a49',
+  },
+  'hljs-title': {
+    'color': '#6f42c1',
+  },
+  'hljs-section': {
+    'color': '#005cc5',
+    'fontWeight': 'bold',
+  },
+  'hljs-selector-id': {
+    'color': '#005cc5',
+  },
+  'hljs-type': {
+    'color': '#d73a49',
+  },
+  'hljs-class .hljs-title': {
+    'color': '#6f42c1',
+  },
+  'hljs-tag': {
+    'color': '#22863a',
+    'fontWeight': 'normal',
+  },
+  'hljs-name': {
+    'color': '#22863a',
+    'fontWeight': 'normal',
+  },
+  'hljs-attribute': {
+    'color': '#005cc5',
+    'fontWeight': 'normal',
+  },
+  'hljs-regexp': {
+    'color': '#032f62',
+  },
+  'hljs-link': {
+    'color': '#032f62',
+  },
+  'hljs-symbol': {
+    'color': '#e36209',
+  },
+  'hljs-bullet': {
+    'color': '#735c0f',
+  },
+  'hljs-built_in': {
+    'color': '#e36209',
+  },
+  'hljs-builtin-name': {
+    'color': '#e36209',
+  },
+  'hljs-meta': {
+    'color': '#005cc5',
+  },
+  'hljs-deletion': {
+    'color': '#b31d28',
+    'background': '#ffeef0',
+  },
+  'hljs-addition': {
+    'color': '#22863a',
+    'background': '#f0fff4',
+  },
+  'hljs-emphasis': {
+    'color': '#24292e',
+    'fontStyle': 'italic',
+  },
+  'hljs-strong': {
+    'color': '#24292e',
+    'fontWeight': 'bold',
+  },
+} as { [key: string]: React.CSSProperties }
