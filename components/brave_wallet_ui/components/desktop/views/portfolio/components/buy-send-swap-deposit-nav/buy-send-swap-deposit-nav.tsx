e20a6ec424be8a9deed9dbe45d3094a230f8ed39
Nuo Xu <nuoxu.nx@gmail.com>
2025-12-16 20:11:52 -0500

    use different options based on isIOS

--- components/brave_wallet_ui/components/desktop/views/portfolio/components/buy-send-swap-deposit-nav/buy-send-swap-deposit-nav.tsx
+++ components/brave_wallet_ui/components/desktop/views/portfolio/components/buy-send-swap-deposit-nav/buy-send-swap-deposit-nav.tsx
@@ -16,11 +16,16 @@ import { useRoute } from '../../../../../../common/hooks/use_route'
 
 // Options
 import {
-  BuySendSwapDepositOptions, //
+  BuySendSwapDepositOptions,
+  BuySendSwapDepositIOSOptions,
 } from '../../../../../../options/nav-options'
 
 // Utils
 import { getLocale } from '../../../../../../../common/locale'
+
+// Selectors
+import { useSafeUISelector } from '../../../../../../common/hooks/use-safe-selector'
+import { UISelectors } from '../../../../../../common/selectors'
 
 // Components
 import {
@@ -44,6 +49,9 @@ export const BuySendSwapDepositNav = () => {
   // refs
   const moreMenuRef = React.useRef<HTMLDivElement>(null)
 
+  // selectors
+  const isIOS = useSafeUISelector(UISelectors.isIOS)
+
   // hooks
   useOnClickOutside(moreMenuRef, () => setShowMoreMenu(false), showMoreMenu)
   const { openOrPushRoute } = useRoute()
@@ -56,9 +64,13 @@ export const BuySendSwapDepositNav = () => {
     [openOrPushRoute],
   )
 
+  const options = isIOS
+    ? BuySendSwapDepositIOSOptions
+    : BuySendSwapDepositOptions
+
   return (
     <Row width='unset'>
-      {BuySendSwapDepositOptions.slice(0, 3).map((option) => (
+      {options.slice(0, 3).map((option) => (
         <ButtonWrapper key={option.id}>
           <Button onClick={() => onClick(option)}>
             <ButtonIcon name={option.icon} />
