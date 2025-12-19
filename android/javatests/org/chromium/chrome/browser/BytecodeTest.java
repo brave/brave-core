f3d24580121b6c923a7f6fe9ba6a55550f5bad96
Artem Samoilenko <artem@brave.com>
2025-12-19 10:55:10 -0500

    [cr145][Android] Changes for the user modified navigation
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/8a572f986d8d90fe1befaa2792dabff71410b4db
    
    commit 8a572f986d8d90fe1befaa2792dabff71410b4db
    Author: Abdelrahman Eed <abdoeed@google.com>
    Date:   Tue Dec 16 11:20:41 2025 -0800
    
        [Clank Navigation] Add support for user-modified (Shift-Click)
    
        This CL adds support for the user modified navigation (Shift + Left
        click) to launch a new browser window or an app if one is present
        depending on the navigation source, for desktop devices.
    
        Changes (only affecting desktop Android devices, or if we are on desktop
        windowing):
        - Launch a new chrome window if navigating from the chrome browser
        - Launch an app to handle the navigation if navigating from a CCT
    
        NO_IFTTT=the check is not working properly, I added the changes needed
    
        Change-Id: I480eb5b648c7da281a83e592fec2b45d52a89631
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6662663
        Commit-Queue: Abdelrahman Eed <abdoeed@google.com>
        Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
        Auto-Submit: Abdelrahman Eed <abdoeed@google.com>
        Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1559473}

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -1375,8 +1375,7 @@ public class BytecodeTest {
                         boolean.class,
                         AsyncTabParamsManager.class,
                         Supplier.class,
-                        Supplier.class,
-                        MultiInstanceManager.class));
+                        Supplier.class));
         Assert.assertTrue(
                 constructorsMatch(
                         "org/chromium/chrome/browser/toolbar/ToolbarManager",
