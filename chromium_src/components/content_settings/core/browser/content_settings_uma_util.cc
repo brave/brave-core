12a8c896199abe90e35ae8607c4cc96183c5164a
boocmp <boocmp@yandex.ru>
2025-12-19 19:57:48 +0700

    Deperecated LNA content setting.

--- chromium_src/components/content_settings/core/browser/content_settings_uma_util.cc
+++ chromium_src/components/content_settings/core/browser/content_settings_uma_util.cc
@@ -44,6 +44,7 @@ static_assert(static_cast<int>(ContentSettingsType::kMaxValue) <
   {ContentSettingsType::BRAVE_GOOGLE_SIGN_IN, brave_value(11)},           \
   {ContentSettingsType::BRAVE_HTTPS_UPGRADE, brave_value(12)},            \
   {ContentSettingsType::BRAVE_REMEMBER_1P_STORAGE, brave_value(13)},      \
+  {ContentSettingsType::DELETED_BRAVE_LOCALHOST_ACCESS, brave_value(14)}, \
   {ContentSettingsType::BRAVE_OPEN_AI_CHAT, brave_value(15)},             \
   {ContentSettingsType::BRAVE_AUTO_SHRED, brave_value(16)},               \
   /* Begin webcompat items */                                                \
