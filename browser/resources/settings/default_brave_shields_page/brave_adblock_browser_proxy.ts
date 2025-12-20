78cfd92dc6da4f07b6031c77a0649562552e99f5
Gordan Nekić <gordan@neki.ch>
2025-12-20 17:11:50 +0100

    docs: :memo: update comment

--- browser/resources/settings/default_brave_shields_page/brave_adblock_browser_proxy.ts
+++ browser/resources/settings/default_brave_shields_page/brave_adblock_browser_proxy.ts
@@ -153,7 +153,8 @@ export class BraveAdblockBrowserProxyImpl implements BraveAdblockBrowserProxy {
       binaryString += String.fromCharCode(...chunk)
     }
     const base64String = btoa(binaryString)
-    // return uint8Array.toBase64() // Use this when supported
+    // Replace with uint8Array.toBase64() when TypeScript adds support for 
+    // toBase64 method.
     return base64String
   }
 
