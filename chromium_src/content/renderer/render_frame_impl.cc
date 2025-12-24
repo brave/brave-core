64227097f213089ff4647907a8c012bdbd6723dc
Pavel Beloborodov <73575789+boocmp@users.noreply.github.com>
2025-12-24 21:25:23 +0700

    Fixed window.name clearing. (#32889)
    * Fixed window.name clearing.

--- chromium_src/content/renderer/render_frame_impl.cc
+++ chromium_src/content/renderer/render_frame_impl.cc
@@ -4,6 +4,7 @@
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "build/build_config.h"
+#include "third_party/blink/public/web/web_local_frame.h"
 
 #if BUILDFLAG(IS_ANDROID)
 #define BRAVE_RENDER_FRAME_IMPL_SHOULD_USE_USER_AGENT_OVERRIDE
@@ -15,6 +16,11 @@
 // clang-format on
 #endif
 
+#define CreateProvisional(...)    \
+  CreateProvisional(__VA_ARGS__); \
+  web_frame->SetOriginForClearWindowNameCheck(replicated_state->origin)
+
 #include <content/renderer/render_frame_impl.cc>
 #undef BRAVE_RENDER_FRAME_IMPL_SHOULD_USE_USER_AGENT_OVERRIDE
 
+#undef CreateProvisional
