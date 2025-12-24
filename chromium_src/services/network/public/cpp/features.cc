f71bb63bd437f0cc9deb88815d954e611f3db078
boocmp <boocmp@yandex.ru>
2025-12-24 16:10:04 +0700

    Enabled upstream features.

--- chromium_src/services/network/public/cpp/features.cc
+++ chromium_src/services/network/public/cpp/features.cc
@@ -12,7 +12,7 @@ namespace network::features {
 OVERRIDE_FEATURE_DEFAULT_STATES({{
     {kBrowsingTopics, base::FEATURE_DISABLED_BY_DEFAULT},
     {kInterestGroupStorage, base::FEATURE_DISABLED_BY_DEFAULT},
-    {kLocalNetworkAccessChecks, base::FEATURE_DISABLED_BY_DEFAULT},
+    {kLocalNetworkAccessChecksWebSockets, base::FEATURE_ENABLED_BY_DEFAULT},
     {kSharedStorageAPI, base::FEATURE_DISABLED_BY_DEFAULT},
 }});
 
