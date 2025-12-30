411a33b700d14db148da81da7e9abd47d277b6df
Simon Hong <shong@brave.com>
2025-12-31 08:53:13 +0900

    [Cleanup] Split view feature flags (#32961)
    Resolves brave/brave-browser#51690
    
    We don't need to explicitely enable kCloseActiveTabInSplitViewViaHotkey/kSideBySide
    as upstream enabled it by default in cr144.
    And don't need to handle reload only active tab from split tabs from Brave(MakeActiveTabReloadOnlyForSplitTab())
    as upstream enabled kReloadSelectionModel features by default for the same purpose.
    
    TEST=SplitViewCommonBrowserTest.SplitViewReloadTest

--- chromium_src/chrome/browser/ui/browser_commands.cc
+++ chromium_src/chrome/browser/ui/browser_commands.cc
@@ -5,60 +5,16 @@
 
 #include "brave/browser/ui/browser_commands.h"
 
-#include <vector>
-
 #include "base/check.h"
 #include "brave/components/commander/common/buildflags/buildflags.h"
 #include "brave/components/tor/buildflags/buildflags.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_commands.h"
-#include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/common/webui_url_constants.h"
-#include "components/tabs/public/tab_interface.h"
-#include "content/public/browser/web_contents.h"
-
-namespace {
-
-// If |selected_tabs| only has two tabs from same split tab,
-// make |selected_tabs| includes only active split tab to reload
-// active tab not the both tab.
-void MakeActiveTabReloadOnlyForSplitTab(
-    TabStripModel* tab_strip_model,
-    std::vector<content::WebContents*>& selected_tabs) {
-  if (selected_tabs.size() != 2) {
-    return;
-  }
-
-  // Only have interests when two tabs are in the same split tab.
-  auto* first_tab = tabs::TabInterface::GetFromContents(selected_tabs[0]);
-  if (!first_tab->GetSplit()) {
-    return;
-  }
-
-  // Only give active tab if two tabs are in the same split tab.
-  auto* second_tab = tabs::TabInterface::GetFromContents(selected_tabs[1]);
-  if (first_tab->GetSplit() == second_tab->GetSplit()) {
-    auto* active_web_contents = tab_strip_model->GetActiveWebContents();
-    if (selected_tabs[0] != active_web_contents &&
-        selected_tabs[1] != active_web_contents) {
-      return;
-    }
-    auto* reload_target =
-        first_tab->IsActivated() ? selected_tabs[0] : selected_tabs[1];
-    selected_tabs.clear();
-    selected_tabs.push_back(reload_target);
-  }
-}
-
-}  // namespace
 
 #define ReloadBypassingCache ReloadBypassingCache_ChromiumImpl
 #define GetReadingListModel GetReadingListModel_ChromiumImpl
 #define kChromeUISplitViewNewTabPageURL kChromeUINewTabURL
-
-// Need to patch to adjust |selected_tabs| in the middle of ReloadInternal().
-#define BRAVE_RELOAD_INTERNAL \
-  MakeActiveTabReloadOnlyForSplitTab(tab_strip_model, tabs_to_reload);
 
 // Don't show toast when user tries to close a pinned tab via the keyboard
 // accelerator.
@@ -67,7 +23,6 @@ void MakeActiveTabReloadOnlyForSplitTab(
 #include <chrome/browser/ui/browser_commands.cc>
 
 #undef BRAVE_CLOSE_TAB
-#undef BRAVE_RELOAD_INTERNAL
 #undef kChromeUISplitViewNewTabPageURL
 #undef ReloadBypassingCache
 #undef GetReadingListModel
