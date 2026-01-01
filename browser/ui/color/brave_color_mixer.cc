brave/brave-core

commit 3e85f9e8e0ebc74efb11d2e30354cebc5addbec0

[Shields UI] Use shields icon for location button icon (#32278)

browser/ui/color/brave_color_mixer.cc
@@ -728,6 +728,7 @@ void AddBraveOmniboxPrivateThemeColorMixer(ui::ColorProvider* provider,
   AddOmniboxHoverSelect(mixer);
 
   mixer[kColorOmniboxIconBackground] = {SK_ColorTRANSPARENT};
+  mixer[kColorOmniboxSecurityChipDangerousBackground] = {SK_ColorTRANSPARENT};
 }
 
 void AddBraveOmniboxColorMixer(ui::ColorProvider* provider,
@@ -747,6 +748,7 @@ void AddBraveOmniboxColorMixer(ui::ColorProvider* provider,
 
   // We don't use bg color for location icon view.
   mixer[kColorOmniboxIconBackground] = {SK_ColorTRANSPARENT};
+  mixer[kColorOmniboxSecurityChipDangerousBackground] = {SK_ColorTRANSPARENT};
 
 #if defined(TOOLKIT_VIEWS)
   if (!base::FeatureList::IsEnabled(

browser/ui/toolbar/brave_location_bar_model_delegate.cc
@@ -7,9 +7,12 @@
 
 #include "base/check.h"
 #include "base/feature_list.h"
+#include "brave/browser/brave_shields/brave_shields_tab_helper.h"
 #include "brave/browser/ui/brave_scheme_utils.h"
+#include "brave/browser/ui/page_info/features.h"
 #include "brave/components/constants/url_constants.h"
 #include "brave/components/constants/webui_url_constants.h"
+#include "brave/components/vector_icons/vector_icons.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/tabs/features.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -79,4 +82,43 @@ bool BraveLocationBarModelDelegate::GetURL(GURL* url) const {
 #endif  // !BUILDFLAG(IS_ANDROID)
 
   return ChromeLocationBarModelDelegate::GetURL(url);
+}
+
+const gfx::VectorIcon* BraveLocationBarModelDelegate::GetVectorIconOverride()
+    const {
+  // Defer to the Chromium implementation first.
+  const gfx::VectorIcon* parent_icon =
+      BrowserLocationBarModelDelegate::GetVectorIconOverride();
+  if (parent_icon) {
+    return parent_icon;
+  }
+
+  // Only override the icon if we are on a secure page.
+  if (GetSecurityLevel() != security_state::SECURE) {
+    return nullptr;
+  }
+
+  auto* fallback_icon = &kLeoTuneSmallIcon;
+
+  // Return a fallback icon if the Shields/Page Info integration feature is
+  // disabled.
+  if (!page_info::features::IsShowBraveShieldsInPageInfoEnabled()) {
+    return fallback_icon;
+  }
+
+  // Return a fallback icon if we can't determine the shields status.
+  content::WebContents* web_contents = GetActiveWebContents();
+  if (!web_contents) {
+    return fallback_icon;
+  }
+  auto* shields_helper =
+      brave_shields::BraveShieldsTabHelper::FromWebContents(web_contents);
+  if (!shields_helper) {
+    return fallback_icon;
+  }
+
+  // Return the appropriate shields icon based on the Shields status.
+  return shields_helper->GetBraveShieldsEnabled()
+             ? &kLeoShieldDoneIcon
+             : &kLeoShieldDisableFilledIcon;
 }

browser/ui/toolbar/brave_location_bar_model_delegate.h
@@ -21,6 +21,9 @@ class BraveLocationBarModelDelegate : public BrowserLocationBarModelDelegate {
   static void FormattedStringFromURL(const GURL& url,
                                      std::u16string* new_formatted_url);
 
+  // LocationBarModelDelegate:
+  const gfx::VectorIcon* GetVectorIconOverride() const override;
+
  private:
   std::u16string FormattedStringWithEquivalentMeaning(
       const GURL& url,

browser/ui/toolbar/brave_location_bar_model_delegate_browsertest.cc
@@ -1,0 +1,116 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "brave/browser/ui/toolbar/brave_location_bar_model_delegate.h"
+
+#include "base/test/scoped_feature_list.h"
+#include "brave/browser/brave_shields/brave_shields_tab_helper.h"
+#include "brave/browser/ui/page_info/features.h"
+#include "brave/components/vector_icons/vector_icons.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/ui_test_utils.h"
+#include "content/public/test/browser_test.h"
+#include "net/dns/mock_host_resolver.h"
+#include "net/test/embedded_test_server/embedded_test_server.h"
+
+class BraveLocationBarModelDelegateBrowserTest : public InProcessBrowserTest {
+ public:
+  BraveLocationBarModelDelegateBrowserTest() = default;
+
+  void SetUpOnMainThread() override {
+    InProcessBrowserTest::SetUpOnMainThread();
+
+    host_resolver()->AddRule("*", "127.0.0.1");
+    ASSERT_TRUE(embedded_https_test_server().Start());
+    ASSERT_TRUE(embedded_test_server()->Start());
+
+    delegate_ = std::make_unique<BraveLocationBarModelDelegate>(
+        browser()->tab_strip_model());
+  }
+
+  void TearDownOnMainThread() override {
+    delegate_.reset();
+    InProcessBrowserTest::TearDownOnMainThread();
+  }
+
+  void NavigateToHTTPS() {
+    GURL https_url =
+        embedded_https_test_server().GetURL("example.com", "/simple.html");
+    ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), https_url));
+  }
+
+  void NavigateToHTTP() {
+    GURL http_url =
+        embedded_test_server()->GetURL("example.com", "/simple.html");
+    ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), http_url));
+  }
+
+  content::WebContents* GetActiveWebContents() {
+    return browser()->tab_strip_model()->GetActiveWebContents();
+  }
+
+  brave_shields::BraveShieldsTabHelper* GetShieldsHelper() {
+    return brave_shields::BraveShieldsTabHelper::FromWebContents(
+        GetActiveWebContents());
+  }
+
+ protected:
+  std::unique_ptr<BraveLocationBarModelDelegate> delegate_;
+};
+
+IN_PROC_BROWSER_TEST_F(BraveLocationBarModelDelegateBrowserTest,
+                       ReturnsNullForNonSecurePage) {
+  NavigateToHTTP();
+  const gfx::VectorIcon* icon = delegate_->GetVectorIconOverride();
+  EXPECT_EQ(icon, nullptr);
+}
+
+IN_PROC_BROWSER_TEST_F(BraveLocationBarModelDelegateBrowserTest,
+                       ReturnsTuneIconForSecurePage) {
+  NavigateToHTTPS();
+  const gfx::VectorIcon* icon = delegate_->GetVectorIconOverride();
+  EXPECT_EQ(icon, &kLeoTuneSmallIcon);
+}
+
+class BraveLocationBarModelDelegateShieldsBrowserTest
+    : public BraveLocationBarModelDelegateBrowserTest {
+ public:
+  BraveLocationBarModelDelegateShieldsBrowserTest() {
+    feature_list.InitAndEnableFeature(
+        page_info::features::kShowBraveShieldsInPageInfo);
+  }
+
+  ~BraveLocationBarModelDelegateShieldsBrowserTest() override = default;
+
+ protected:
+  base::test::ScopedFeatureList feature_list;
+};
+
+IN_PROC_BROWSER_TEST_F(BraveLocationBarModelDelegateShieldsBrowserTest,
+                       ReturnsShieldDoneIconWhenShieldsEnabled) {
+  NavigateToHTTPS();
+
+  auto* shields_helper = GetShieldsHelper();
+  ASSERT_TRUE(shields_helper);
+  EXPECT_TRUE(shields_helper->GetBraveShieldsEnabled());
+
+  const gfx::VectorIcon* icon = delegate_->GetVectorIconOverride();
+  EXPECT_EQ(icon, &kLeoShieldDoneIcon);
+}
+
+IN_PROC_BROWSER_TEST_F(BraveLocationBarModelDelegateShieldsBrowserTest,
+                       ReturnsShieldDisabledIconWhenShieldsDisabled) {
+  NavigateToHTTPS();
+
+  auto* shields_helper = GetShieldsHelper();
+  ASSERT_TRUE(shields_helper);
+
+  shields_helper->SetBraveShieldsEnabled(false);
+  EXPECT_FALSE(shields_helper->GetBraveShieldsEnabled());
+
+  const gfx::VectorIcon* icon = delegate_->GetVectorIconOverride();
+  EXPECT_EQ(icon, &kLeoShieldDisableFilledIcon);
+}

browser/ui/views/brave_actions/brave_actions_container.cc
@@ -10,6 +10,7 @@
 
 #include "base/feature_list.h"
 #include "brave/browser/brave_rewards/rewards_util.h"
+#include "brave/browser/ui/page_info/features.h"
 #include "brave/browser/ui/views/brave_actions/brave_rewards_action_view.h"
 #include "brave/browser/ui/views/brave_actions/brave_shields_action_view.h"
 #include "brave/browser/ui/views/rounded_separator.h"
@@ -53,10 +54,10 @@ void BraveActionsContainer::Init() {
   brave_button_separator_->SetBorder(views::CreateEmptyBorder(
       gfx::Insets::TLBR(0, kSeparatorMargin, 0, kSeparatorMargin)));
   // Just in case the extensions load before this function does (not likely!)
-  // make sure separator is at index 0
-  AddChildViewAt(brave_button_separator_, 0);
-  AddActionViewForShields();
+  // add children to the front in reverse order.
   AddActionViewForRewards();
+  AddActionViewForShields();
+  AddChildViewAt(brave_button_separator_, 0);
 
   // React to Brave Rewards preferences changes.
   show_brave_rewards_button_.Init(
@@ -78,8 +79,13 @@ bool BraveActionsContainer::ShouldShowBraveRewardsAction() const {
 }
 
 void BraveActionsContainer::AddActionViewForShields() {
+  // Do not create the shields button if the shields UI is displayed in the Page
+  // Info bubble.
+  if (page_info::features::IsShowBraveShieldsInPageInfoEnabled()) {
+    return;
+  }
   shields_action_btn_ = AddChildViewAt(
-      std::make_unique<BraveShieldsActionView>(browser_window_interface_), 1);
+      std::make_unique<BraveShieldsActionView>(browser_window_interface_), 0);
   shields_action_btn_->SetPreferredSize(GetActionSize());
   shields_action_btn_->Init();
 }
@@ -87,7 +93,7 @@ void BraveActionsContainer::AddActionViewForShields() {
 void BraveActionsContainer::AddActionViewForRewards() {
   auto button =
       std::make_unique<BraveRewardsActionView>(browser_window_interface_);
-  rewards_action_btn_ = AddChildViewAt(std::move(button), 2);
+  rewards_action_btn_ = AddChildViewAt(std::move(button), 0);
   rewards_action_btn_->SetPreferredSize(GetActionSize());
   rewards_action_btn_->SetVisible(ShouldShowBraveRewardsAction());
   rewards_action_btn_->Update();
@@ -140,6 +146,7 @@ void BraveActionsContainer::ChildPreferredSizeChanged(views::View* child) {
 void BraveActionsContainer::OnBraveRewardsPreferencesChanged() {
   if (rewards_action_btn_) {
     rewards_action_btn_->SetVisible(ShouldShowBraveRewardsAction());
+    UpdateVisibility();
   }
 }
 

chromium_src/chrome/browser/ui/views/location_bar/location_icon_view.cc
@@ -1,22 +1,18 @@
 /* Copyright (c) 2021 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "components/strings/grit/components_strings.h"
-
-namespace {
-constexpr int kDangerousVerboseState = IDS_DANGEROUS_VERBOSE_STATE;
-}  // namespace
-
-// To make |is_text_dangerous| false always.
-// It prevents to get color from LocationIconView. We want to get color
-// from its delegate.
-#undef IDS_DANGEROUS_VERBOSE_STATE
-#define IDS_DANGEROUS_VERBOSE_STATE kDangerousVerboseState) && (false
+// Early return from `UpdateBackground` in order to avoid resetting the ink drop
+// and clearing the current ink drop state. Since we always use the same
+// background color, there is no need to reset the ink drop.
+//
+// Additionally, when the user toggles the Shields status while the page info
+// bubble is open, we intentionally keep the bubble open. Since the Shields
+// status update will trigger a call to this method, an early return prevents
+// the ink drop from disappearing while the bubble is open.
+#define BRAVE_LOCATION_ICON_VIEW_UPDATE_BACKGROUND return;
 
 #include <chrome/browser/ui/views/location_bar/location_icon_view.cc>
 
-#undef IDS_DANGEROUS_VERBOSE_STATE
-
-#define IDS_DANGEROUS_VERBOSE_STATE kDangerousVerboseState
+#undef BRAVE_LOCATION_ICON_VIEW_UPDATE_BACKGROUND

chromium_src/components/omnibox/browser/location_bar_model_util.cc
@@ -1,32 +1,0 @@
-// Copyright (c) 2023 The Brave Authors. All rights reserved.
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this file,
-// You can obtain one at https://mozilla.org/MPL/2.0/.
-
-#include "build/build_config.h"
-#include "components/omnibox/browser/buildflags.h"
-
-#if (!BUILDFLAG(IS_ANDROID) || BUILDFLAG(ENABLE_VR)) && !BUILDFLAG(IS_IOS)
-#include "brave/components/vector_icons/vector_icons.h"
-#endif
-
-#define GetSecurityVectorIcon GetSecurityVectorIcon_Chromium
-
-#include <components/omnibox/browser/location_bar_model_util.cc>
-
-#undef GetSecurityVectorIcon
-
-namespace location_bar_model {
-
-const gfx::VectorIcon& GetSecurityVectorIcon(
-    security_state::SecurityLevel security_level,
-    security_state::VisibleSecurityState* visible_security_state) {
-#if (!BUILDFLAG(IS_ANDROID) || BUILDFLAG(ENABLE_VR)) && !BUILDFLAG(IS_IOS)
-  if (security_level == security_state::SECURE) {
-    return kLeoTuneSmallIcon;
-  }
-#endif
-  return GetSecurityVectorIcon_Chromium(security_level, visible_security_state);
-}
-
-}  // namespace location_bar_model

patches/chrome-browser-ui-views-location_bar-location_icon_view.cc.patch
@@ -1,0 +1,12 @@
+diff --git a/chrome/browser/ui/views/location_bar/location_icon_view.cc b/chrome/browser/ui/views/location_bar/location_icon_view.cc
+index a500b13d933dc7906c039a991fbdf47ec025a867..5d5628c39945bad62d6b07513390aa95323e800d 100644
+--- a/chrome/browser/ui/views/location_bar/location_icon_view.cc
++++ b/chrome/browser/ui/views/location_bar/location_icon_view.cc
+@@ -345,6 +345,7 @@ void LocationIconView::UpdateBackground() {
+ 
+   SetBackgroundColor(GetColorProvider()->GetColor(id));
+ 
++  BRAVE_LOCATION_ICON_VIEW_UPDATE_BACKGROUND
+   if (is_text_dangerous) {
+     ConfigureInkDropForRefresh2023(this, kColorOmniboxSecurityChipInkDropHover,
+                                    kColorOmniboxSecurityChipInkDropRipple);

commit 0e2de563d2134de3a53c7b6e71c2a984994b04f5

Fixed location icon color regression for dangerous site (#32985)
Resolves brave/brave-browser#51654

This regression comes from https://github.com/brave/brave-core/pull/32278.
In that PR, we cleaned up location_icon_view.cc by overriding color via color mixer.
Originally, that override file made |is_text_dangerous| false always in upstream file to change
location icon's bg & fg color two places.
But we only handled bg color. Missed fg color handling.
So, bg color is set by ours and fg color is set by upstream's. See the screenshot in the linked issue.
If |is_text_dangerous| is true, fg color id is |kColorOmniboxSecurityChipText| but we want another
color that delegate gives. It's |kColorOmniboxSecurityChipDangerous|.
Fixed by overriding fg color id(kColorOmniboxSecurityChipText) in color mixer.

TEST=BraveColorMixersTest.ColorOverrideTest

browser/ui/color/brave_color_mixer.cc
@@ -729,6 +729,7 @@ void AddBraveOmniboxPrivateThemeColorMixer(ui::ColorProvider* provider,
 
   mixer[kColorOmniboxIconBackground] = {SK_ColorTRANSPARENT};
   mixer[kColorOmniboxSecurityChipDangerousBackground] = {SK_ColorTRANSPARENT};
+  mixer[kColorOmniboxSecurityChipText] = {kColorOmniboxSecurityChipDangerous};
 }
 
 void AddBraveOmniboxColorMixer(ui::ColorProvider* provider,
@@ -749,6 +750,7 @@ void AddBraveOmniboxColorMixer(ui::ColorProvider* provider,
   // We don't use bg color for location icon view.
   mixer[kColorOmniboxIconBackground] = {SK_ColorTRANSPARENT};
   mixer[kColorOmniboxSecurityChipDangerousBackground] = {SK_ColorTRANSPARENT};
+  mixer[kColorOmniboxSecurityChipText] = {kColorOmniboxSecurityChipDangerous};
 
 #if defined(TOOLKIT_VIEWS)
   if (!base::FeatureList::IsEnabled(

browser/ui/color/brave_color_mixers_unittest.cc
@@ -35,4 +35,6 @@ TEST_F(BraveColorMixersTest, ColorOverrideTest) {
   EXPECT_EQ(color_provider().GetColor(kColorOmniboxIconHover),
             SkColorSetA(color_provider().GetColor(kColorOmniboxText),
                         std::ceil(0.10f * 255.0f)));
+  EXPECT_EQ(color_provider().GetColor(kColorOmniboxSecurityChipText),
+            color_provider().GetColor(kColorOmniboxSecurityChipDangerous));
 }