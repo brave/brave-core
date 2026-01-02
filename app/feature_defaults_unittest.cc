552f4779b3f1ac5b3b4244c3a2cd456a5e4cf1d2
Claudio DeSouza <cdesouza@brave.com>
2026-01-02 21:06:00 +0000

    [cr145] Several feature flags now part of `kSideBySide`
    These flags are now all gated by `kSideBySide`, and with that we don't
    need to test the value of these as this is the desired behaviour.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/290f338f8cafdbe7f41dc1c86640aa4c1e4f78d4
    
    commit 290f338f8cafdbe7f41dc1c86640aa4c1e4f78d4
    Author: Alison Gale <agale@chromium.org>
    Date:   Thu Dec 11 06:05:03 2025 -0800
    
        [SxS] Clean up sub-flags within the split view experiment
    
        These will mostly be gated behind the top level kSideBySide flag till I
        clean that up. Getting these out of the way to simplify things.
    
        Bug: 454347097
        Change-Id: If73131d804bf85fea516952a0b4ef3a5a4b215ea
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7248788
        Commit-Queue: Alison Gale <agale@chromium.org>
        Reviewed-by: David Pennington <dpenning@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1557353}

--- app/feature_defaults_unittest.cc
+++ app/feature_defaults_unittest.cc
@@ -281,8 +281,6 @@ TEST(FeatureDefaultsTest, EnabledFeatures) {
       &media::kEnableTabMuting,
       &net::features::kPartitionConnectionsByNetworkIsolationKey,
 #if !BUILDFLAG(IS_ANDROID)
-      &features::kCloseActiveTabInSplitViewViaHotkey,
-      &features::kReloadSelectionModel,
       &features::kSideBySide,
       &sharing_hub::kDesktopScreenshots,
 #endif
