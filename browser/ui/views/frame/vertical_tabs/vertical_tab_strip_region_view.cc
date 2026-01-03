brave/brave-core

commit fd5528451e4df01dd0e346ba9d30c5bc5115a8da

Remove VerticalTabStripScrollContentsView (#32955)
VerticalTabStripScrollContentsView was using views::FillLayout
And it caused tab_container to be resized twice and infinitely.

browser/ui/views/frame/vertical_tabs/vertical_tab_strip_region_view.cc
@@ -62,9 +62,7 @@
 #include "ui/views/controls/highlight_path_generator.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/resize_area.h"
-#include "ui/views/controls/scroll_view.h"
 #include "ui/views/layout/box_layout.h"
-#include "ui/views/layout/fill_layout.h"
 #include "ui/views/layout/flex_layout.h"
 #include "ui/views/layout/layout_types.h"
 #include "ui/views/view_utils.h"
@@ -322,27 +320,6 @@ END_METADATA
 
 }  // namespace
 
-class VerticalTabStripScrollContentsView : public views::View {
-  METADATA_HEADER(VerticalTabStripScrollContentsView, views::View)
- public:
-  VerticalTabStripScrollContentsView() {
-    SetLayoutManager(std::make_unique<views::FillLayout>());
-  }
-  ~VerticalTabStripScrollContentsView() override = default;
-
-  // views::View:
-  void ChildPreferredSizeChanged(views::View* child) override {
-    PreferredSizeChanged();
-  }
-
-  void OnPaintBackground(gfx::Canvas* canvas) override {
-    canvas->DrawColor(GetColorProvider()->GetColor(kColorToolbar));
-  }
-};
-
-BEGIN_METADATA(VerticalTabStripScrollContentsView)
-END_METADATA
-
 class BraveVerticalTabStripRegionView::HeaderView : public views::View {
   METADATA_HEADER(HeaderView, views::View)
  public:
@@ -437,8 +414,6 @@ BraveVerticalTabStripRegionView::BraveVerticalTabStripRegionView(
       base::BindRepeating(&BraveVerticalTabStripRegionView::ToggleState,
                           base::Unretained(this)),
       this, browser_));
-  contents_view_ =
-      AddChildView(std::make_unique<VerticalTabStripScrollContentsView>());
   header_view_->toggle_button()->SetHighlighted(state_ == State::kExpanded);
   separator_ = AddChildView(std::make_unique<views::View>());
   separator_->SetBackground(
@@ -775,8 +750,12 @@ gfx::Size BraveVerticalTabStripRegionView::GetMinimumSize() const {
 }
 
 void BraveVerticalTabStripRegionView::Layout(PassKey) {
-  // As we have to update ScrollView's viewport size and its contents size,
-  // laying out children manually will be more handy.
+  if (original_region_view_->parent() != this) {
+    // When original_region_view_ is not a child of this view, it means that
+    // the vertical tab strip is not visible.
+    return;
+  }
+
   const auto contents_bounds = GetContentsBounds();
 
   const gfx::Size header_size{contents_bounds.width(),
@@ -790,14 +769,14 @@ void BraveVerticalTabStripRegionView::Layout(PassKey) {
       kSeparatorHeight - header_view_->height();
   const int contents_view_preferred_height =
       tab_strip()->GetPreferredSize().height();
-  contents_view_->SetBoundsRect(gfx::Rect(
+  original_region_view_->SetBoundsRect(gfx::Rect(
       header_view_->bounds().bottom_left(),
       gfx::Size(
           contents_bounds.width(),
           std::min(contents_view_max_height, contents_view_preferred_height))));
 
   gfx::Rect separator_bounds(
-      contents_view_->bounds().bottom_left(),
+      original_region_view_->bounds().bottom_left(),
       gfx::Size(contents_bounds.width(), kSeparatorHeight));
   separator_bounds.Inset(
       gfx::Insets::VH(0, tabs::kMarginForVerticalTabContainers));
@@ -844,14 +823,14 @@ void BraveVerticalTabStripRegionView::UpdateLayout(bool in_destruction) {
       tab_strip_region_view_original_index_ =
           original_parent_of_region_view_->GetIndexOf(original_region_view_);
       original_parent_of_region_view_->RemoveChildView(original_region_view_);
-      contents_view_->AddChildView(original_region_view_.get());
+      AddChildView(original_region_view_.get());
     }
 
     static_cast<views::FlexLayout*>(original_region_view_->GetLayoutManager())
         ->SetOrientation(views::LayoutOrientation::kVertical);
   } else {
     if (Contains(original_region_view_)) {
-      contents_view_->RemoveChildView(original_region_view_);
+      RemoveChildView(original_region_view_);
       CHECK(tab_strip_region_view_original_index_.has_value());
       original_parent_of_region_view_->AddChildViewAt(
           original_region_view_.get(), *tab_strip_region_view_original_index_);

browser/ui/views/frame/vertical_tabs/vertical_tab_strip_region_view.h
@@ -191,7 +191,6 @@ class BraveVerticalTabStripRegionView : public views::View,
   raw_ptr<TabStripRegionView> original_region_view_ = nullptr;
 
   raw_ptr<HeaderView> header_view_ = nullptr;
-  raw_ptr<views::View> contents_view_ = nullptr;
 
   // Separator between tabs and new tab button.
   raw_ptr<views::View> separator_ = nullptr;

browser/ui/views/frame/brave_tab_strip_region_view.cc
@@ -20,21 +20,28 @@
 BraveTabStripRegionView::~BraveTabStripRegionView() = default;
 
 void BraveTabStripRegionView::Layout(PassKey) {
+  auto* widget = GetWidget();
+  if (!widget || widget->IsClosed()) {
+    return;
+  }
+
   UpdateTabStripMargin();
-  LayoutSuperclass<TabStripRegionView>(this);
 
-  if (tabs::utils::ShouldShowVerticalTabs(tab_strip_->GetBrowser())) {
-    // in vertical tabs mode, we make tab strip's height is the same with this
-    // view's height to avoid extra gaps.
-    tab_strip_->SetSize({tab_strip_->size().width(), height()});
+  if (!tabs::utils::ShouldShowVerticalTabs(tab_strip_->GetBrowser())) {
+    LayoutSuperclass<TabStripRegionView>(this);
+
+    // Ensure that the new tab button is positioned after the last tab, with the
+    // correct amount of padding.
+    if (new_tab_button_) {
+      new_tab_button_->SetX(tab_strip_container_->bounds().right() +
+                            GetLayoutConstant(TAB_STRIP_PADDING));
+    }
+    return;
   }
 
-  // Ensure that the new tab button is positioned after the last tab, with the
-  // correct amount of padding.
-  if (new_tab_button_) {
-    new_tab_button_->SetX(tab_strip_container_->bounds().right() +
-                          GetLayoutConstant(TAB_STRIP_PADDING));
-  }
+  // in vertical tabs mode, we make tab strip's height is the same with this
+  // view's height to avoid extra gaps.
+  tab_strip_container_->SetBoundsRect(gfx::Rect(0, 0, width(), height()));
 }
 
 void BraveTabStripRegionView::UpdateTabStripMargin() {

browser/ui/views/frame/vertical_tabs/vertical_tab_strip_browsertest.cc
@@ -535,10 +535,14 @@ IN_PROC_BROWSER_TEST_F(VerticalTabStripBrowserTest, LayoutSanity) {
   // Test if every tabs are laid out inside tab strip region -------------------
   // This is a regression test for
   // https://github.com/brave/brave-browser/issues/28084
+  const auto region_view_bounds =
+      GetBoundsInScreen(region_view, region_view->GetLocalBounds());
   for (int i = 0; i < model->count(); i++) {
     auto* tab = GetTabAt(browser(), i);
-    EXPECT_TRUE(GetBoundsInScreen(region_view, region_view->GetLocalBounds())
-                    .Contains(GetBoundsInScreen(tab, tab->GetLocalBounds())));
+    const auto tab_bounds = GetBoundsInScreen(tab, tab->GetLocalBounds());
+    EXPECT_TRUE(region_view_bounds.Contains(tab_bounds))
+        << "Region view bounds: " << region_view_bounds.ToString()
+        << " vs. Tab bounds: " << tab_bounds.ToString();
   }
 }
 
@@ -561,7 +565,7 @@ IN_PROC_BROWSER_TEST_F(VerticalTabStripBrowserTest,
 
   browser_view()->tabstrip()->StopAnimating();
 
-  int contents_view_height = region_view->contents_view_->height();
+  int contents_view_height = region_view->original_region_view_->height();
   AppendTab(browser());
   browser_view()->tabstrip()->StopAnimating();
 
@@ -570,7 +574,7 @@ IN_PROC_BROWSER_TEST_F(VerticalTabStripBrowserTest,
   contents_view_height +=
       (tabs::kVerticalTabHeight + tabs::kMarginForVerticalTabContainers * 2);
   ASSERT_TRUE(base::test::RunUntil([&]() {
-    return region_view->contents_view_->height() == contents_view_height;
+    return region_view->original_region_view_->height() == contents_view_height;
   }));
 
   AppendTab(browser());
@@ -581,7 +585,7 @@ IN_PROC_BROWSER_TEST_F(VerticalTabStripBrowserTest,
   contents_view_height +=
       (tabs::kVerticalTabHeight + tabs::kVerticalTabsSpacing);
   ASSERT_TRUE(base::test::RunUntil([&]() {
-    return region_view->contents_view_->height() == contents_view_height;
+    return region_view->original_region_view_->height() == contents_view_height;
   }));
 }
 

browser/ui/views/frame/vertical_tabs/vertical_tab_strip_root_view_browsertest.cc
@@ -93,8 +93,12 @@ class VerticalTabStripRootViewBrowserTest : public InProcessBrowserTest {
   }
 };
 
+// Before we have our own interactive ui tests, we need to disable this test as
+// it's flaky when running test suits.
+#define MAYBE_DragAfterCurrentTab DISABLED_DragAfterCurrentTab
+
 IN_PROC_BROWSER_TEST_F(VerticalTabStripRootViewBrowserTest,
-                       DragAfterCurrentTab) {
+                       MAYBE_DragAfterCurrentTab) {
   ToggleVerticalTabStrip();
 
   ASSERT_TRUE(tabs::utils::ShouldShowVerticalTabs(browser()));

commit debadeb81b500c08a411d4c6eb707e3d426dd801

Fix vertical tab regression (uplift to 1.86.x) (#33009)
* Wrap region view with a container view (#32994)

* Wrap region view with a container view

Reportedly, when we add the TabStripRegionView to VerticalTabStripRegionView,
directly, context menu on Omnibox is not working specifically on Windows.

This was regressed from #32955 and what was mainly done was removing the
ScrollView from VerticalTabStripRegionView.

In order to fix this, we wrap the TabStripRegionView with a container view.
We're not sure why this is the case, but this seems to fix the issue.

* Don't allow throbber to be painted to layer for vertical tabs (#32981)

We don't want to allow throbber to be painted to layer for vertical tabs
because vertical tabs are scrollable, and a tab could be out of the viewport.
Otherwise, throbber would be painted even when the tab is not in the viewport.

browser/ui/views/frame/vertical_tabs/vertical_tab_strip_region_view.cc
@@ -64,6 +64,7 @@
 #include "ui/views/controls/resize_area.h"
 #include "ui/views/event_monitor.h"
 #include "ui/views/layout/box_layout.h"
+#include "ui/views/layout/fill_layout.h"
 #include "ui/views/layout/flex_layout.h"
 #include "ui/views/layout/layout_types.h"
 #include "ui/views/view_utils.h"
@@ -446,6 +447,10 @@ BraveVerticalTabStripRegionView::BraveVerticalTabStripRegionView(
   // The default state is kExpanded, so reset animation state to 1.0.
   width_animation_.Reset(1.0);
 
+  region_view_container_ = AddChildView(std::make_unique<views::View>());
+  region_view_container_->SetLayoutManager(
+      std::make_unique<views::FillLayout>());
+
   header_view_ = AddChildView(std::make_unique<HeaderView>(
       base::BindRepeating(&BraveVerticalTabStripRegionView::ToggleState,
                           base::Unretained(this)),
@@ -781,7 +786,7 @@ gfx::Size BraveVerticalTabStripRegionView::GetMinimumSize() const {
 }
 
 void BraveVerticalTabStripRegionView::Layout(PassKey) {
-  if (original_region_view_->parent() != this) {
+  if (original_region_view_->parent() != region_view_container_) {
     // When original_region_view_ is not a child of this view, it means that
     // the vertical tab strip is not visible.
     return;
@@ -800,14 +805,14 @@ void BraveVerticalTabStripRegionView::Layout(PassKey) {
       kSeparatorHeight - header_view_->height();
   const int contents_view_preferred_height =
       tab_strip()->GetPreferredSize().height();
-  original_region_view_->SetBoundsRect(gfx::Rect(
+  region_view_container_->SetBoundsRect(gfx::Rect(
       header_view_->bounds().bottom_left(),
       gfx::Size(
           contents_bounds.width(),
           std::min(contents_view_max_height, contents_view_preferred_height))));
 
   gfx::Rect separator_bounds(
-      original_region_view_->bounds().bottom_left(),
+      region_view_container_->bounds().bottom_left(),
       gfx::Size(contents_bounds.width(), kSeparatorHeight));
   separator_bounds.Inset(
       gfx::Insets::VH(0, tabs::kMarginForVerticalTabContainers));
@@ -854,14 +859,18 @@ void BraveVerticalTabStripRegionView::UpdateLayout(bool in_destruction) {
       tab_strip_region_view_original_index_ =
           original_parent_of_region_view_->GetIndexOf(original_region_view_);
       original_parent_of_region_view_->RemoveChildView(original_region_view_);
-      AddChildView(original_region_view_.get());
+      region_view_container_->AddChildView(original_region_view_.get());
+
+      // Resize area can be overlapped with tabs.
+      // To make it grabbable, it should be top-most view.
+      ReorderChildView(resize_area_, children().size() - 1);
     }
 
     static_cast<views::FlexLayout*>(original_region_view_->GetLayoutManager())
         ->SetOrientation(views::LayoutOrientation::kVertical);
   } else {
     if (Contains(original_region_view_)) {
-      RemoveChildView(original_region_view_);
+      region_view_container_->RemoveChildView(original_region_view_.get());
       CHECK(tab_strip_region_view_original_index_.has_value());
       original_parent_of_region_view_->AddChildViewAt(
           original_region_view_.get(), *tab_strip_region_view_original_index_);

browser/ui/views/frame/vertical_tabs/vertical_tab_strip_region_view.h
@@ -191,6 +191,14 @@ class BraveVerticalTabStripRegionView : public views::View,
 
   raw_ptr<HeaderView> header_view_ = nullptr;
 
+  // Reportedly, when we add the TabStripRegionView to
+  // VerticalTabStripRegionView directly, context menu on Omnibox is not working
+  // specifically on Windows. In order to fix this, we wrap the
+  // TabStripRegionView with a container view. We're not sure why this is the
+  // case, but this seems to fix the issue.
+  // https://github.com/brave/brave-browser/issues/51719
+  raw_ptr<views::View> region_view_container_ = nullptr;
+
   // Separator between tabs and new tab button.
   raw_ptr<views::View> separator_ = nullptr;
 

browser/ui/views/tabs/brave_tab_strip.cc
@@ -90,6 +90,17 @@ bool BraveTabStrip::IsVerticalTabsFloating() const {
               BraveVerticalTabStripRegionView::State::kCollapsed);
 }
 
+bool BraveTabStrip::CanPaintThrobberToLayer() const {
+  if (!ShouldShowVerticalTabs()) {
+    return TabStrip::CanPaintThrobberToLayer();
+  }
+
+  // Don't allow throbber to be painted to layer. Vertical tabs are scrollable,
+  // and a tab could be out of the viewport. Otherwise, throbber would be
+  // painted even when the tab is not in the viewport.
+  return false;
+}
+
 bool BraveTabStrip::ShouldDrawStrokes() const {
   if (ShouldShowVerticalTabs()) {
     // Prevent root view from drawing lines. For vertical tabs stroke , we

browser/ui/views/tabs/brave_tab_strip.h
@@ -41,6 +41,7 @@ class BraveTabStrip : public TabStrip {
       Tab* tab,
       const std::optional<std::u16string>& title) override;
   bool ShouldAlwaysHideCloseButton() const override;
+  bool CanPaintThrobberToLayer() const override;
 
  private:
   FRIEND_TEST_ALL_PREFIXES(VerticalTabStripBrowserTest, ScrollBarVisibility);