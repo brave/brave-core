bc5a90a64a2230a575462d6bbe3d70648cc6f30e
brave-builds <45370463+brave-builds@users.noreply.github.com>
2026-01-05 03:24:07 +0000

    Consider group views when calculating max scroll offset (uplift to 1.86.x) (#33008)
    Uplift of #32993 (squashed) to beta

--- browser/ui/views/tabs/brave_tab_container.h
+++ browser/ui/views/tabs/brave_tab_container.h
@@ -8,6 +8,7 @@
 
 #include <memory>
 #include <optional>
+#include <utility>
 #include <vector>
 
 #include "chrome/browser/ui/tabs/tab_style.h"
@@ -145,12 +146,24 @@ class BraveTabContainer : public TabContainerImpl {
   // Returns the maximum scroll offset for unpinned tabs.
   int GetMaxScrollOffset() const;
 
+  // Returns the first and last visible unpinned slot views (tabs or group
+  // headers). Returns {nullptr, nullptr} when no visible unpinned slot views
+  // exist.
+  std::pair<TabSlotView*, TabSlotView*> FindVisibleUnpinnedSlotViews() const;
+
+  // Returns the ideal bounds for the given slot view (tab or group header).
+  gfx::Rect GetIdealBoundsOf(TabSlotView* slot_view) const;
+
   // Clamp the current scroll_offset_ within valid range.
   void ClampScrollOffset();
 
   // Used to sets the clip path for child views (tabs, group views and etc).
   void UpdateClipPathForChildren(views::View* view,
                                  int pinned_tabs_area_bottom);
+
+  // Updates clip path for all slot views (tabs and group views) based on
+  // scroll_offset.
+  void UpdateClipPathForSlotViews();
 
   // Update scroll offset to make the given tab visible.
   void ScrollTabToBeVisible(Tab* tab);
