fb4b33e39e03bcce1a5d91db212175a24e9f39a0
Max Karolinskiy <max@brave.com>
2026-01-06 17:18:54 -0500

    [cr145] Moves DefaultBrowserWorker override.
    Chromium changes:
    
    https://source.chromium.org/chromium/chromium/src/+/31394abe9a00af4fd76e901e72199bedb4dd04d6
    
    commit 31394abe9a00af4fd76e901e72199bedb4dd04d6
    Author: Atharv Maan <atharvmaan@chromium.org>
    Date:   Fri Dec 12 12:40:52 2025 -0800
    
        Move metric recording logic out of DefaultBrowserInfoBarDelegate
    
        This CL has the following changes:
        - Moves the metric recording logic from DefaultBrowserInfoBarDelegate to
          DefaultBrowserInfoBarManager.
        - Deprecates IGNORE_INFO_BAR metric which was being recorded per-tab.
        - Adds IGNORE_INFO_BAR_V2 that recordes once per browser session, based
          on the last tabbed browser window closing.
        - Moves user interaction logic from the delegate to the manager.
    
        Bug: None
    
    https://source.chromium.org/chromium/chromium/src/+/a0449084d836c7af9c16d05a44cd0269d10ab79f
    
    commit a0449084d836c7af9c16d05a44cd0269d10ab79f
    Author: Atharv Maan <atharvmaan@chromium.org>
    Date:   Tue Dec 16 08:33:14 2025 -0800
    
        [Default Browser] Migrate Default Browser startup infobar
    
        This CL migrates the Default Browser startup infobar to rely on the
        Default Browser framework, conditional on the `kDefaultBrowserFramework`
        feature flag.
    
        Bug: 454597770
    
    https://source.chromium.org/chromium/chromium/src/+/29f46678c4315c0cb0e30b76cc4b1d8941b6c354
    
    commit 29f46678c4315c0cb0e30b76cc4b1d8941b6c354
    Author: Atharv Maan <atharvmaan@chromium.org>
    Date:   Thu Jan 8 06:27:07 2026 -0800
    
        [Default Browser] Migrate Default Browser settings toggle
    
        This CL migrates the Default Browser settings toggle to rely on the
        Default Browser framework, for querying and setting default browser
        state.
    
        Bug: b:454597770

--- chromium_src/chrome/browser/default_browser/setters/shell_integration_default_browser_setter.cc
+++ chromium_src/chrome/browser/default_browser/setters/shell_integration_default_browser_setter.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+/* Copyright (c) 2026 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
@@ -6,5 +6,5 @@
 #include "brave/browser/brave_shell_integration.h"
 
 #define DefaultBrowserWorker BraveDefaultBrowserWorker
-#include <chrome/browser/ui/startup/default_browser_prompt/default_browser_infobar_delegate.cc>
+#include <chrome/browser/default_browser/setters/shell_integration_default_browser_setter.cc>
 #undef DefaultBrowserWorker
