f0a6663da7b53edf5566d33fd27eb15b9e068720
Steeeephen <18271292+Steeeephen@users.noreply.github.com>
2026-01-06 11:32:49 +0000

    [LEO] remove gemma & deepseek-r1, update qwen labels

--- ios/brave-ios/Sources/AIChat/AIChatStrings.swift
+++ ios/brave-ios/Sources/AIChat/AIChatStrings.swift
@@ -284,22 +284,6 @@ extension Strings {
       comment:
         "The model's purpose - Describes what it can do best"
     )
-    public static let introMessageDeepSeekR1ModelPurposeDescription = NSLocalizedString(
-      "aichat.introMessageDeepSeekR1ModelPurposeDescription",
-      tableName: "BraveLeo",
-      bundle: .module,
-      value: "Reasoning model",
-      comment:
-        "The model's purpose - Describes what it can do best"
-    )
-    public static let introMessageGemmaPurposeDescription = NSLocalizedString(
-      "aichat.introMessageGemmaPurposeDescription",
-      tableName: "BraveLeo",
-      bundle: .module,
-      value: "General purpose text and image understanding",
-      comment:
-        "The model's purpose - Describes what it can do best"
-    )
     public static let introMessageLlamaMessageDescription = NSLocalizedString(
       "aichat.introMessageLlamaMessageDescription",
       tableName: "BraveLeo",
@@ -338,22 +322,6 @@ extension Strings {
       bundle: .module,
       value:
         "Hi, I'm Leo. I'm an AI assistant by Brave. I'm powered by Claude Sonnet, a model created by Anthropic to power conversational and text processing tasks. Claude Sonnet is Brave-hosted through our own secure infrastructure.",
-      comment: "The model intro message when you first enter the chat assistant"
-    )
-    public static let introMessageDeepSeekR1MessageDescription = NSLocalizedString(
-      "aichat.introMessageDeepSeekR1MessageDescription",
-      tableName: "BraveLeo",
-      bundle: .module,
-      value:
-        "Hi, I'm Leo. I'm an AI assistant by Brave. I'm powered by DeepSeek R1, a model created by DeepSeek to perform deep reasoning tasks. DeepSeek R1 is Brave-hosted through our own secure infrastructure.",
-      comment: "The model intro message when you first enter the chat assistant"
-    )
-    public static let introMessageGemmaMessageDescription = NSLocalizedString(
-      "aichat.introMessageGemmaMessageDescription",
-      tableName: "BraveLeo",
-      bundle: .module,
-      value:
-        "Hi, I'm Leo. I'm an AI assistant by Brave. I'm powered by Gemma 3 12B, a model created by Google DeepMind to be performant and applicable to many use cases. Gemma is Brave-hosted through our own secure infrastructure.",
       comment: "The model intro message when you first enter the chat assistant"
     )
     public static let introMessageGenericMessageDescription = NSLocalizedString(
