8be3caddad733c656197f052891005e2b256dde9
Claudio DeSouza <cdesouza@brave.com>
2026-01-12 19:34:12 +0000

    [cr145] `BrowserView::ShouldDrawStrokes` renamed
    This method is now called `ShouldDrawTabStrokes`.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/b26a5e135a2162191266b69cf2ace7e27c6f3ae2
    
    commit b26a5e135a2162191266b69cf2ace7e27c6f3ae2
    Author: dljames <dljames@chromium.org>
    Date:   Fri Jan 9 10:47:45 2026 -0800
    
        Rename BrowserView::ShouldDrawStrokes to ShouldDrawTabStrokes
    
        ShouldDrawStrokes was renamed to ShouldDrawTabStrokes as that name more
        explicitly captures what the function is verifying.
    
        Change-Id: I382f7f453eb9da8aefe845295831a1acded7c4c6
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7411034
        Reviewed-by: Muhammad Salmaan <musalmaan@chromium.org>
        Commit-Queue: Darryl James <dljames@chromium.org>
        Reviewed-by: Eshwar Stalin <estalin@chromium.org>
        Reviewed-by: Steven Luong <stluong@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1567053}

--- browser/ui/views/frame/brave_browser_view.h
+++ browser/ui/views/frame/brave_browser_view.h
@@ -222,7 +222,7 @@ class BraveBrowserView : public BrowserView,
   void ShowSplitView(bool focus_active_view) override;
   void HideSplitView() override;
   void ReparentTopContainerForEndOfImmersive() override;
-  bool ShouldDrawStrokes() const override;
+  bool ShouldDrawTabStrokes() const override;
 
   void HandleBrowserWindowMouseEvent(const ui::MouseEvent& event);
   bool IsBraveWebViewRoundedCornersEnabled();
