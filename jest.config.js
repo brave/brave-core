0e25bcd831df50a1526ab38b200cc3087a530ba9
Andrea Brancaleoni <abc@pompel.me>
2026-01-12 17:28:51 +0100

    jest.config.js: simplify transformIgnorePatterns for pnpm
    Previous attempt at fixing transformIgnorePatterns tried to list specific
    packages, but pnpm's node_modules structure with ES modules requires
    transforming most packages. Simplified by only excluding packages that
    cause issues (jest, babel, lodash).
    
    Fixes Jest test failures with:
    - react-markdown and react-syntax-highlighter ES modules
    - bs58/base-x module loading
    - All unified ecosystem packages (remark, rehype, hast, mdast, etc.)

--- jest.config.js
+++ jest.config.js
@@ -113,15 +113,12 @@ module.exports = {
   ],
   testTimeout: 30000,
   transformIgnorePatterns: [
-    '<rootDir>/node_modules/(?!(bs58|base-x|@brave\\/brave-ui|@brave\\/leo)/)',
     // prevent jest from transforming itself
     // https://github.com/jestjs/jest/issues/9503#issuecomment-709041807
     '<rootDir>/node_modules/@babel',
     '<rootDir>/node_modules/@jest',
     '<rootDir>/node_modules/jest-runtime',
-    '<rootDir>/node_modules/lodash',
-    'signal-exit',
-    'is-typedarray'
+    '<rootDir>/node_modules/lodash'
   ],
   setupFilesAfterEnv: ['<rootDir>/components/test/testSetup.ts'],
   moduleNameMapper: {
