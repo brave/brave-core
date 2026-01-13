brave/brave-core

commit 042e9ef7b419aa94a40613b5254161b7e066b51f

Remove unused component resources (#32890)
* Remove unused component resources

* Remove unused ipfs_logo.png

* Remove README that seemed wrongly copied

build/commands/lib/branding.js
@@ -128,30 +128,70 @@ exports.update = () => {
     path.join(braveAppDir, 'theme', 'default_200_percent', 'common'),
     path.join(chromeAppDir, 'theme', 'default_200_percent', 'common'),
   ])
+  // Copy product_logo for version UI (about:version page).
+  // product_logo_name_48.png is renamed to product_logo.png during copy.
   fileMap.add([
-    path.join(braveComponentsDir, 'resources', 'default_100_percent'),
-    path.join(chromeComponentsDir, 'resources', 'default_100_percent'),
-  ])
-  fileMap.add([
-    path.join(braveComponentsDir, 'resources', 'default_100_percent', 'brave'),
+    path.join(
+      braveAppDir,
+      'theme',
+      'default_100_percent',
+      'brave',
+      'product_logo_name_48.png',
+    ),
     path.join(
       chromeComponentsDir,
       'resources',
       'default_100_percent',
       'chromium',
+      'product_logo.png',
     ),
   ])
   fileMap.add([
-    path.join(braveComponentsDir, 'resources', 'default_200_percent'),
-    path.join(chromeComponentsDir, 'resources', 'default_200_percent'),
+    path.join(
+      braveAppDir,
+      'theme',
+      'default_100_percent',
+      'brave',
+      'product_logo_white.png',
+    ),
+    path.join(
+      chromeComponentsDir,
+      'resources',
+      'default_100_percent',
+      'chromium',
+      'product_logo_white.png',
+    ),
   ])
   fileMap.add([
-    path.join(braveComponentsDir, 'resources', 'default_200_percent', 'brave'),
+    path.join(
+      braveAppDir,
+      'theme',
+      'default_200_percent',
+      'brave',
+      'product_logo_name_48.png',
+    ),
     path.join(
       chromeComponentsDir,
       'resources',
       'default_200_percent',
       'chromium',
+      'product_logo.png',
+    ),
+  ])
+  fileMap.add([
+    path.join(
+      braveAppDir,
+      'theme',
+      'default_200_percent',
+      'brave',
+      'product_logo_white.png',
+    ),
+    path.join(
+      chromeComponentsDir,
+      'resources',
+      'default_200_percent',
+      'chromium',
+      'product_logo_white.png',
     ),
   ])
   fileMap.add([

app/theme/brave/win/README
@@ -1,4 +1,0 @@
-Whenever chromium.ico changes, the profile shortcut icons (generated dynamically
-at runtime) need to be migrated to the new icon. Please increment the constant
-kCurrentProfileIconVersion in
-chrome/browser/profiles/profile_shortcut_manager_win.cc.

commit 3347e9332a5738d6a891ca8fb9ff52120b6d9399

Replaced history page favicon with ours (#33084)
Resolves https://github.com/brave/brave-browser/issues/51914

f/u PR for https://github.com/brave/brave-core/pull/32890
It's regression from https://github.com/brave/brave-core/pull/32890.
In that PR, we removed whole `//brave/components/resources/` copy but missed this favicon file.

build/commands/lib/branding.js
@@ -171,6 +171,35 @@ exports.update = () => {
       ),
     ])
   }
+  // Replace history page's favicon.
+  fileMap.add([
+    path.join(
+      braveComponentsDir,
+      'resources',
+      'default_100_percent',
+      'favicon_history.png',
+    ),
+    path.join(
+      chromeComponentsDir,
+      'resources',
+      'default_100_percent',
+      'favicon_history.png',
+    ),
+  ])
+  fileMap.add([
+    path.join(
+      braveComponentsDir,
+      'resources',
+      'default_200_percent',
+      'favicon_history.png',
+    ),
+    path.join(
+      chromeComponentsDir,
+      'resources',
+      'default_200_percent',
+      'favicon_history.png',
+    ),
+  ])
   for (const branding of ['brave', 'brave_origin']) {
     fileMap.add([
       path.join(braveAppVectorIconsDir, 'vector_icons', branding),