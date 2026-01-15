d5e2355caf221ae81d8a61db45d13ce1dde56a67
Kevin Smith <zenparsing@gmail.com>
2026-01-15 09:57:57 -0500

    [Shields UI] Add IPH for new shields panel location (#33094)

--- chromium_src/components/feature_engagement/public/feature_constants.cc
+++ chromium_src/components/feature_engagement/public/feature_constants.cc
@@ -5,9 +5,18 @@
 
 #include "base/feature_override.h"
 
+// This override allows Brave to define its own IPH features, and to modify the
+// IPH features defined by Chromium.
+
 #include <components/feature_engagement/public/feature_constants.cc>
 
 namespace feature_engagement {
+
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+BASE_FEATURE(kIPHBraveShieldsInPageInfoFeature,
+             "IPH_BraveShieldsInPageInfo",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+#endif
 
 OVERRIDE_FEATURE_DEFAULT_STATES({{
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_APPLE) || BUILDFLAG(IS_LINUX)
