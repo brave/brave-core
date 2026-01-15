993ad1451d90660f03b36a2b6555baf6b6dc8ce4
Claudio DeSouza <cdesouza@brave.com>
2026-01-15 15:30:32 +0000

    [WIP][cr146] Old layout deleted with `BrowserViewLayoutImplOld`
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/6d6528f4057921978755dd965d1454152650341a
    
    commit 6d6528f4057921978755dd965d1454152650341a
    Author: Dana Fried <dfried@chromium.org>
    Date:   Tue Jan 13 12:39:44 2026 -0800
    
        [Chrome Next] Remove old layout
    
        Since the new layout is shipped, this simply removes the killswitches
        and the old code.
    
        Bug: 424236535
        Change-Id: I3ff8b7f00a8327d4e130f73205b5ed350412cbef
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7277788
        Reviewed-by: Darryl James <dljames@chromium.org>
        Commit-Queue: Dana Fried <dfried@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1568638}

--- app/feature_defaults_unittest.cc
+++ app/feature_defaults_unittest.cc
@@ -144,7 +144,6 @@ TEST(FeatureDefaultsTest, DisabledFeatures) {
       &feature_engagement::kIPHPasswordsManagementBubbleAfterSaveFeature,
       &feature_engagement::kIPHTabSearchToolbarButtonFeature,
 #endif
-      &features::kAppBrowserUseNewLayout,
       &features::kBookmarkTriggerForPrefetch,
       &features::kChromeStructuredMetrics,
       &features::kDevToolsAiCodeCompletion,
@@ -166,7 +165,6 @@ TEST(FeatureDefaultsTest, DisabledFeatures) {
       &features::kKAnonymityService,
       &features::kKAnonymityServiceOHTTPRequests,
       &features::kNewTabPageTriggerForPrerender2,
-      &features::kPopupBrowserUseNewLayout,
       &features::kPrivacySandboxAdsAPIsOverride,
       &features::kPrivacySandboxAdsAPIsM1Override,
 #if !BUILDFLAG(IS_ANDROID)
@@ -176,7 +174,6 @@ TEST(FeatureDefaultsTest, DisabledFeatures) {
       &features::kSCTAuditing,
       &features::kServiceWorkerAutoPreload,
       &features::kTabHoverCardImages,
-      &features::kTabbedBrowserUseNewLayout,
 #if !BUILDFLAG(IS_ANDROID)
       &features::kTrustSafetySentimentSurvey,
       &features::kTrustSafetySentimentSurveyV2,
