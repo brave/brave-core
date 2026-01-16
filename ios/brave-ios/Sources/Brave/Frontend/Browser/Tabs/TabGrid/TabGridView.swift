0a164126d4fd31196290062a0618d948a4c89ba2
Eli Hini <8249954+EliHini@users.noreply.github.com>
2026-01-16 03:24:19 -0800

    [iOS] Fix to ensure that More or Done button do not offset New tab button in different localization (#33087)
    Fixed the frame settings for both leading and trailing components to use a maxWidth of infinity for their respective edges.

--- ios/brave-ios/Sources/Brave/Frontend/Browser/Tabs/TabGrid/TabGridView.swift
+++ ios/brave-ios/Sources/Brave/Frontend/Browser/Tabs/TabGrid/TabGridView.swift
@@ -459,8 +459,11 @@ struct TabGridView: View {
       } label: {
         Text(Strings.TabGrid.moreMenuButtonTitle)
           .padding(4)
+          .lineLimit(1)
       }
       .menuOrder(.fixed)
+      .frame(maxWidth: .infinity, alignment: .leading)
+
       Spacer()
       Button {
         viewModel.addTab()
@@ -493,8 +496,10 @@ struct TabGridView: View {
       } label: {
         Text(Strings.done)
           .padding(4)
+          .lineLimit(1)
       }
       .keyboardShortcut(.defaultAction)
+      .frame(maxWidth: .infinity, alignment: .trailing)
     }
     .foregroundStyle(Color(braveSystemName: .textSecondary))
     .fontWeight(.medium)
