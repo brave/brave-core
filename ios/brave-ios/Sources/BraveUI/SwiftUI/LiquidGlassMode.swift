709d4fd33367207f32f582561f237f09e95a8e65
Eli Hini <ehini@brave.com>
2026-01-21 15:29:18 -0800

    Revert to using containerCornerOffset
    Using .containerCornerOffset(.leading, sizeToFit: true) instead of tracking window size changes.
    
    Providing true for sizeToFit to alleviate occasional jankiness where the components don’t snap back to occupy where the traffic lights were after they’ve been dismissed
    
    Signed-off-by: Eli Hini <ehini@brave.com>

--- ios/brave-ios/Sources/BraveUI/SwiftUI/LiquidGlassMode.swift
+++ ios/brave-ios/Sources/BraveUI/SwiftUI/LiquidGlassMode.swift
@@ -35,28 +35,12 @@ extension UIView {
 }
 
 struct LiquidGlassLayoutTrafficLightPadding: ViewModifier {
-  @State private var trafficLightsVisible = false
-  @State private var layoutGuideInset: CGFloat = 0
-  let padding: CGFloat = 8
-
-  var explicitInset: CGFloat? = nil
-  var leadingInset: CGFloat { explicitInset ?? layoutGuideInset + padding }
-
-  init(_ explicitInset: CGFloat? = nil) {
-    self.explicitInset = explicitInset
-  }
+  let leadingInset: CGFloat = 8
   func body(content: Content) -> some View {
     if #available(iOS 26.0, *) {
       content
-        .padding(.leading, trafficLightsVisible ? leadingInset : 0)
-        .background(
-          TrafficLightDetector { visible, inset in
-            withAnimation(.easeIn(duration: 0.1)) {
-              trafficLightsVisible = visible
-              layoutGuideInset = inset
-            }
-          }
-        )
+        .padding(.leading, leadingInset)
+        .containerCornerOffset(.leading, sizeToFit: true)
     } else {
       content
     }
@@ -64,136 +48,7 @@ struct LiquidGlassLayoutTrafficLightPadding: ViewModifier {
 }
 
 extension View {
-  public func liquidGlassLayoutTrafficLightPadding(_ inset: CGFloat? = nil) -> some View {
-    modifier(LiquidGlassLayoutTrafficLightPadding(inset))
-  }
-}
-
-struct TrafficLightDetector: UIViewControllerRepresentable {
-  let onVisibilityChange: (Bool, CGFloat) -> Void
-
-  func makeUIViewController(context: Context) -> TrafficLightDetectorViewController {
-    TrafficLightDetectorViewController(onVisibilityChange: onVisibilityChange)
-  }
-
-  func updateUIViewController(
-    _ uiViewController: TrafficLightDetectorViewController,
-    context: Context
-  ) {}
-}
-
-class TrafficLightDetectorViewController: UIViewController {
-  var onVisibilityChange: ((Bool, CGFloat) -> Void)?
-  private var lastVisibilityState: Bool?
-  private var displayLink: CADisplayLink?
-
-  init(onVisibilityChange: ((Bool, CGFloat) -> Void)?) {
-    self.onVisibilityChange = onVisibilityChange
-    super.init(nibName: nil, bundle: nil)
-  }
-
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-    startMonitoring()
-  }
-
-  override func viewWillDisappear(_ animated: Bool) {
-    super.viewWillDisappear(animated)
-    stopMonitoring()
-  }
-
-  private func startMonitoring() {
-    displayLink?.invalidate()
-    displayLink = CADisplayLink(target: self, selector: #selector(checkTrafficLights))
-    displayLink?.preferredFramesPerSecond = 2
-    displayLink?.add(to: .main, forMode: .common)
-
-    checkTrafficLights()
-  }
-
-  private func stopMonitoring() {
-    displayLink?.invalidate()
-    displayLink = nil
-  }
-
-  @objc private func checkTrafficLights() {
-    guard let windowScene = view.window?.windowScene else {
-      updateVisibility(false)
-      return
-    }
-
-    guard #available(iOS 26.0, *) else {
-      updateVisibility(false)
-      return
-    }
-
-    let windowBounds = windowScene.coordinateSpace.bounds
-    let screenBounds = windowScene.screen.bounds
-
-    // Traffic lights are visible when window is NOT maximized. That is, app is in windowing mode
-    let threshold: CGFloat = 1.0
-    let isMaximized =
-      abs(windowBounds.width - screenBounds.width) < threshold
-      && abs(windowBounds.height - screenBounds.height) < threshold
-    let trafficLightsVisible = !isMaximized
-    updateVisibility(trafficLightsVisible)
-  }
-
-  private func updateVisibility(_ visible: Bool) {
-    guard let onVisibilityChange, lastVisibilityState != visible else { return }
-    lastVisibilityState = visible
-
-    DispatchQueue.main.async { [weak self] in
-      guard let self = self else { return }
-      let inset =
-        self.view.bounds.width > 0
-        ? self.view.liquidGlassHorizontalSafeAreaLayoutGuide.layoutFrame.minX
-        : 0
-      onVisibilityChange(visible, inset)
-    }
-  }
-
-  deinit {
-    stopMonitoring()
-  }
-}
-
-private struct LiquidGlassHorizontalInsetMeasurer: UIViewRepresentable {
-  let onInsetsMeasured: (CGFloat, CGFloat) -> Void
-
-  func makeUIView(context: Context) -> LiquidGlassHorizontalInsetMeasuringView {
-    LiquidGlassHorizontalInsetMeasuringView(onInsetsMeasured: onInsetsMeasured)
-  }
-
-  func updateUIView(_ uiView: LiquidGlassHorizontalInsetMeasuringView, context: Context) {
-    uiView.onInsetsMeasured = onInsetsMeasured
-    uiView.setNeedsLayout()
-  }
-}
-
-private class LiquidGlassHorizontalInsetMeasuringView: UIView {
-  var onInsetsMeasured: (CGFloat, CGFloat) -> Void
-
-  init(onInsetsMeasured: @escaping (CGFloat, CGFloat) -> Void) {
-    self.onInsetsMeasured = onInsetsMeasured
-    super.init(frame: .zero)
-    backgroundColor = .clear
-    isUserInteractionEnabled = false
-  }
-
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
-  override func layoutSubviews() {
-    super.layoutSubviews()
-    let layoutGuide = liquidGlassHorizontalSafeAreaLayoutGuide
-    let leading = layoutGuide.layoutFrame.minX
-    let trailing = bounds.width - layoutGuide.layoutFrame.maxX
-    onInsetsMeasured(leading, trailing)
+  public func liquidGlassLayoutTrafficLightPadding() -> some View {
+    modifier(LiquidGlassLayoutTrafficLightPadding())
   }
 }
