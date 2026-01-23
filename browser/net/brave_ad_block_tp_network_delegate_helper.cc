b3bc3f62b32e64126f9124916256a4378fef7f9d
Anton Lazarev <22821309+antonok-edm@users.noreply.github.com>
2026-01-22 18:25:55 -0800

    Update browser/net/brave_ad_block_tp_network_delegate_helper.cc

--- browser/net/brave_ad_block_tp_network_delegate_helper.cc
+++ browser/net/brave_ad_block_tp_network_delegate_helper.cc
@@ -337,7 +337,7 @@ bool ProxySettingsAllowUncloaking(content::BrowserContext* browser_context) {
         !config.value().proxy_rules().proxies_for_https.IsEmpty()) {
       can_uncloak = false;
     }
-    // Check if PAC script is configured|
+    // Check if PAC script is configured
     if (config.value().has_pac_url()) {
       can_uncloak = false;
     }
