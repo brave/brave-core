aa093e08aca1a981e71b25a03b48dfa015571acc
Claudio DeSouza <cdesouza@brave.com>
2026-01-26 13:45:22 +0000

    [cr146] Feature flags for k-anonymity deleted
    These flags were disabling the feature, but the feature is now gone too.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/d46f063ab9d90732f50142909afe5695d57c4ade
    
    commit d46f063ab9d90732f50142909afe5695d57c4ade
    Author: Russ Hamilton <behamilton@google.com>
    Date:   Thu Jan 22 07:37:25 2026 -0800
    
        Remove k-anonymity service client
    
        The k-anonymity service has been turned down so this code is no longer
        needed.
    
        Bug: 477241297
        Change-Id: I7fb80b03794b1b6ba45ac863ea6ac5e4e801a6e4
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7493789
        Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
        Reviewed-by: Tarun Bansal <tbansal@chromium.org>
        Reviewed-by: mmenke <mmenke@chromium.org>
        Commit-Queue: Russ Hamilton <behamilton@google.com>
        Cr-Commit-Position: refs/heads/main@{#1573003}

--- app/feature_defaults_unittest.cc
+++ app/feature_defaults_unittest.cc
@@ -162,8 +162,6 @@ TEST(FeatureDefaultsTest, DisabledFeatures) {
 #endif
       &features::kHttpsFirstBalancedMode,
       &features::kIdleDetection,
-      &features::kKAnonymityService,
-      &features::kKAnonymityServiceOHTTPRequests,
       &features::kNewTabPageTriggerForPrerender2,
       &features::kPrivacySandboxAdsAPIsOverride,
       &features::kPrivacySandboxAdsAPIsM1Override,
