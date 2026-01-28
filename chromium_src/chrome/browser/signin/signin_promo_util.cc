61629a93686b4e19eee739af4dedc59da2b3dc93
Emerick Rogul <erogul@brave.com>
2026-01-28 11:13:48 -0500

    [cr146] ShouldShowExtensionSyncPromo -> ShouldShowExtensionSignInPromo
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/63366519b2bb7e4d77550e9685fa22b075999793
    
    commit 63366519b2bb7e4d77550e9685fa22b075999793
    Author: Monica Salama <msalama@chromium.org>
    Date:   Mon Jan 26 06:43:05 2026 -0800
    
        [Uno-d clean-up] Deprecate kExplicitBrowserSignin
    
        Dice migration is fully launched. All signed in users on Dice platforms
        are explicitly signed in.
    
        Bug: 475822503
        Change-Id: I99e2288e1729bf7cf8a2cbfb0411495dd1fbebf4
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7510806
        Reviewed-by: Amelie Schneider <amelies@google.com>
        Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
        Commit-Queue: Monica Salama <msalama@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1574504}

--- chromium_src/chrome/browser/signin/signin_promo_util.cc
+++ chromium_src/chrome/browser/signin/signin_promo_util.cc
@@ -4,17 +4,14 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #define SHOULD_SHOW_SIGNIN_PROMO_COMMON return false;
-// Even though this function is used internally in
-// ShouldShowExtensionSignInPromo, that function later calls
-// ShouldShowSignInPromoCommon which is disabled via the above patch.
-#define ShouldShowExtensionSyncPromo ShouldShowExtensionSyncPromo_UnUsed
+#define ShouldShowExtensionSignInPromo ShouldShowExtensionSigninPromo_UnUsed
 // Even though this function is used internally in
 // - ShouldShowSignInPromoCommon, that function is disabled via the above patch,
-// - ShouldShowExtensionSyncPromo, that function is replaced above.
+// - ShouldShowExtensionSignInPromo, that function is replaced above.
 #define ShouldShowSyncPromo ShouldShowSyncPromo_UnUsed
 #include <chrome/browser/signin/signin_promo_util.cc>
 #undef ShouldShowSyncPromo
-#undef ShouldShowExtensionSyncPromo
+#undef ShouldShowExtensionSignInPromo
 #undef SHOULD_SHOW_SIGNIN_PROMO_COMMON
 
 namespace signin {
@@ -26,8 +23,8 @@ bool ShouldShowSyncPromo(Profile& profile) {
 #endif  // !BUILDFLAG(IS_ANDROID)
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
-bool ShouldShowExtensionSyncPromo(Profile& profile,
-                                  const extensions::Extension& extension) {
+bool ShouldShowExtensionSignInPromo(Profile& profile,
+                                    const extensions::Extension& extension) {
   return false;
 }
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
