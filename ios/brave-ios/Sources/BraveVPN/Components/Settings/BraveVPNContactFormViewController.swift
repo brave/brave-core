f1c450c8580b5456f9fa1e998f1f2db41201f7b2
Kyle Hickinson <git@kylehickinson.com>
2026-01-28 18:00:08 -0500

    [iOS] Remove Static usage from VPN support contact settings page
    This converts the VPN support form settings page to a SwiftUI View and removes the Static dependency

--- ios/brave-ios/Sources/BraveVPN/Components/Settings/BraveVPNContactFormViewController.swift
+++ ios/brave-ios/Sources/BraveVPN/Components/Settings/BraveVPNContactFormViewController.swift
@@ -11,34 +11,30 @@ import CoreTelephony
 import Foundation
 import GuardianConnect
 import MessageUI
+import OSLog
 import Shared
-import Static
-import UIKit
-import os.log
+import SwiftUI
 
-class BraveVPNContactFormViewController: TableViewController {
+struct VPNContactFormView: View {
+  @State private var includes: DataIncludes = .init()
+  @State private var issueType: IssueType?
+  @State private var isEmailPresented: Bool = false
+  @State private var isEmailUnavailableAlertPresented: Bool = false
 
-  private let supportEmail = "braveios@guardianapp.com"
-
-  init() {
-    super.init(style: .grouped)
-  }
-
-  private struct ContactForm {
-    var hostname: String?
-    var tunnelProtocol: String?
-    var subscriptionType: String?
-    var receipt: String?
-    var appVersion: String?
-    var timezone: String?
-    var networkType: String?
-    var cellularCarrier: String?
-    var issue: String?
-    var logs: [(date: Date, message: String)]?
+  private struct DataIncludes {
+    var hostname: Bool = false
+    var tunnelProtocol: Bool = false
+    var subscriptionType: Bool = false
+    var receipt: Bool = false
+    var appVersion: Bool = false
+    var timezone: Bool = false
+    var networkType: Bool = false
+    var cellularCarrier: Bool = false
+    var logs: Bool = false
   }
 
   /// User can choose an issue from predefined list or write their own in email body.
-  private enum IssueType: String, RepresentableOptionType, CaseIterable {
+  private enum IssueType: String, CaseIterable {
     case otherConnectionProblems
     case noInternet
     case slowConnection
@@ -56,287 +52,81 @@ class BraveVPNContactFormViewController: TableViewController {
     }
   }
 
-  @available(*, unavailable)
-  required init(coder: NSCoder) { fatalError() }
-
-  private var contactForm = ContactForm()
-
-  override func viewDidLoad() {
-    // MARK: Hostname
-    let hostname = BraveVPN.hostname
-    let hostnameRow = Row(
-      text: Strings.VPN.contactFormHostname,
-      accessory:
-        .view(
-          SwitchAccessoryView(
-            initialValue: false,
-            valueChange: { [weak self] isOn in
-              if isOn {
-                self?.contactForm.hostname = hostname
-              } else {
-                self?.contactForm.hostname = nil
-              }
-            }
-          )
-        )
-    )
-
-    // MARK: TunnelProtocol
-    let userPreferredTunnelProtocol = GRDTransportProtocol.getUserPreferredTransportProtocol()
-    let transportProtocol = GRDTransportProtocol.prettyTransportProtocolString(
-      for: userPreferredTunnelProtocol
-    )
-    let tunnelProtocolRow =
-      Row(
-        text: Strings.VPN.protocolPickerTitle,
-        detailText: transportProtocol,
-        accessory: .view(
-          SwitchAccessoryView(
-            initialValue: false,
-            valueChange: { [weak self] isOn in
-              if isOn {
-                self?.contactForm.tunnelProtocol = transportProtocol
-              } else {
-                self?.contactForm.tunnelProtocol = nil
-              }
-            }
-          )
-        ),
-        cellClass: MultilineSubtitleCell.self
-      )
-
-    // MARK: SubscriptionType
-    let subscriptionType = BraveVPN.subscriptionName
-    let subscriptionTypeRow =
-      Row(
-        text: Strings.VPN.contactFormSubscriptionType,
-        detailText: subscriptionType,
-        accessory: .view(
-          SwitchAccessoryView(
-            initialValue: false,
-            valueChange: { [weak self] isOn in
-              if isOn {
-                self?.contactForm.subscriptionType = subscriptionType
-              } else {
-                self?.contactForm.subscriptionType = nil
-              }
-            }
-          )
-        ),
-        cellClass: MultilineSubtitleCell.self
-      )
-
-    // MARK: App Store receipt
-    let receiptRow = Row(
-      text: Strings.VPN.contactFormAppStoreReceipt,
-      accessory:
-        .view(
-          SwitchAccessoryView(
-            initialValue: false,
-            valueChange: { [weak self] isOn in
-              guard let self else { return }
-              if isOn {
-                Task {
-                  self.contactForm.receipt = await self.fetchReceipt()
-                }
-              } else {
-                contactForm.receipt = nil
-              }
-            }
-          )
-        )
-    )
-
-    // MARK: App Version
-    let appVersion = AppInfo.appVersion
-    let appVersionRow =
-      Row(
-        text: Strings.VPN.contactFormAppVersion,
-        detailText: appVersion,
-        accessory: .view(
-          SwitchAccessoryView(
-            initialValue: false,
-            valueChange: { [weak self] isOn in
-              if isOn {
-                self?.contactForm.appVersion = appVersion
-              } else {
-                self?.contactForm.appVersion = nil
-              }
-            }
-          )
-        ),
-        cellClass: MultilineSubtitleCell.self
-      )
-
-    // MARK: Timezone
-    let timezone = TimeZone.current.description
-    let timezoneRow =
-      Row(
-        text: Strings.VPN.contactFormTimezone,
-        detailText: timezone,
-        accessory: .view(
-          SwitchAccessoryView(
-            initialValue: false,
-            valueChange: { [weak self] isOn in
-              if isOn {
-                self?.contactForm.timezone = timezone
-              } else {
-                self?.contactForm.timezone = nil
-              }
-            }
-          )
-        ),
-        cellClass: MultilineSubtitleCell.self
-      )
-
-    // MARK: Network Type
-    let networkType = getNetworkType
-    let networkTypeRow =
-      Row(
-        text: Strings.VPN.contactFormNetworkType,
-        detailText: networkType,
-        accessory: .view(
-          SwitchAccessoryView(
-            initialValue: false,
-            valueChange: { [weak self] isOn in
-              if isOn {
-                self?.contactForm.networkType = networkType
-              } else {
-                self?.contactForm.networkType = nil
-              }
-            }
-          )
-        ),
-        cellClass: MultilineSubtitleCell.self
-      )
-
-    // MARK: Cellular Carrier
-    let carrierName =
-      CTTelephonyNetworkInfo().serviceSubscriberCellularProviders?
-      .first?.value.carrierName ?? "-"
-    let carrierRow =
-      Row(
-        text: Strings.VPN.contactFormCarrier,
-        detailText: carrierName,
-        accessory: .view(
-          SwitchAccessoryView(
-            initialValue: false,
-            valueChange: { [weak self] isOn in
-              if isOn {
-                self?.contactForm.cellularCarrier = carrierName
-              } else {
-                self?.contactForm.cellularCarrier = nil
-              }
-            }
-          )
-        ),
-        cellClass: MultilineSubtitleCell.self
-      )
-
-    // MARK: Error logs
-    let errorLogs =
-      Row(
-        text: Strings.VPN.contactFormLogs,
-        accessory: .view(
-          SwitchAccessoryView(
-            initialValue: false,
-            valueChange: { [weak self] isOn in
-              if isOn {
-                self?.contactForm.logs = BraveVPN.errorLog
-              } else {
-                self?.contactForm.logs = nil
-              }
-            }
-          )
-        ),
-        cellClass: MultilineSubtitleCell.self
-      )
-
-    let section = Section(rows: [
-      hostnameRow, tunnelProtocolRow, subscriptionTypeRow, receiptRow,
-      appVersionRow, timezoneRow, networkTypeRow, carrierRow, errorLogs,
-    ])
-
-    let sendButton = Row(
-      text: Strings.VPN.contactFormSendButton,
-      selection: { [weak self] in
-        guard let self = self else { return }
-        let optionChanged = {
-          [weak self] (vc: OptionSelectionViewController<IssueType>, option: IssueType) -> Void in
-          self?.contactForm.issue = option.displayString
-          self?.createEmailOutline()
+  var body: some View {
+    Form {
+      Section {
+        Toggle(Strings.VPN.contactFormHostname, isOn: $includes.hostname)
+          .listRowBackground(Color(.secondaryBraveGroupedBackground))
+        Toggle(isOn: $includes.tunnelProtocol) {
+          LabeledContent(Strings.VPN.protocolPickerTitle, value: tunnelProtocol)
         }
-
-        let optionsVC =
-          OptionSelectionViewController<IssueType>(
-            headerText: Strings.VPN.contactFormIssue,
-            footerText: Strings.VPN.contactFormIssueDescription,
-            options: IssueType.allCases,
-            optionChanged: optionChanged
+        .listRowBackground(Color(.secondaryBraveGroupedBackground))
+        Toggle(isOn: $includes.subscriptionType) {
+          LabeledContent(Strings.VPN.contactFormSubscriptionType, value: BraveVPN.subscriptionName)
+        }
+        .listRowBackground(Color(.secondaryBraveGroupedBackground))
+        Toggle(Strings.VPN.contactFormAppStoreReceipt, isOn: $includes.receipt)
+          .listRowBackground(Color(.secondaryBraveGroupedBackground))
+        Toggle(isOn: $includes.appVersion) {
+          LabeledContent(Strings.VPN.contactFormAppVersion, value: AppInfo.appVersion)
+        }
+        .listRowBackground(Color(.secondaryBraveGroupedBackground))
+        Toggle(isOn: $includes.timezone) {
+          LabeledContent(Strings.VPN.contactFormTimezone, value: TimeZone.current.description)
+        }
+        .listRowBackground(Color(.secondaryBraveGroupedBackground))
+        Toggle(isOn: $includes.networkType) {
+          LabeledContent(Strings.VPN.contactFormNetworkType, value: networkType)
+        }
+        .listRowBackground(Color(.secondaryBraveGroupedBackground))
+        Toggle(isOn: $includes.cellularCarrier) {
+          LabeledContent(
+            Strings.VPN.contactFormCarrier,
+            value: CTTelephonyNetworkInfo().serviceSubscriberCellularProviders?
+              .first?.value.carrierName ?? "-"
           )
-
-        self.navigationController?.pushViewController(optionsVC, animated: true)
-
-      },
-      cellClass: CenteredButtonCell.self
-    )
-
-    let footerText =
-      "\(Strings.VPN.contactFormFooterSharedWithGuardian)\n\n\(Strings.VPN.contactFormFooter)"
-    let buttonSection = Section(rows: [sendButton], footer: .title(footerText))
-
-    dataSource.sections = [section, buttonSection]
-  }
-
-  private func createEmailOutline() {
-    if !MFMailComposeViewController.canSendMail() {
-      Logger.module.error("Can't send email on this device")
-      let alert = UIAlertController(
-        title: Strings.genericErrorTitle,
-        message: Strings.VPN.contactFormEmailNotConfiguredBody,
-        preferredStyle: .alert
+        }
+        .listRowBackground(Color(.secondaryBraveGroupedBackground))
+        Toggle(Strings.VPN.contactFormLogs, isOn: $includes.logs)
+          .listRowBackground(Color(.secondaryBraveGroupedBackground))
+      }
+      Section {
+        NavigationLink(Strings.VPN.contactFormSendButton) {
+          IssuePicker(issueType: $issueType)
+        }
+        .listRowBackground(Color(.secondaryBraveGroupedBackground))
+      } footer: {
+        Text(
+          "\(Strings.VPN.contactFormFooterSharedWithGuardian)\n\n\(Strings.VPN.contactFormFooter)"
+        )
+      }
+    }
+    .tint(Color(braveSystemName: .primary40))
+    .onChange(of: issueType, initial: false) {
+      if issueType == nil { return }
+      if MFMailComposeViewController.canSendMail() {
+        isEmailPresented = true
+      } else {
+        isEmailUnavailableAlertPresented = true
+        issueType = nil
+      }
+    }
+    .sheet(isPresented: $isEmailPresented) {
+      MailComposeRepresentable(
+        subject:
+          "\(Strings.VPN.contactFormTitle) + \(issueType?.displayString ?? IssueType.other.displayString)",
+        recipients: ["braveios@guardianapp.com"],
+        messageBody: emailMessageBody
       )
-      let okAction = UIAlertAction(title: Strings.OKString, style: .default)
-      alert.addAction(okAction)
-      present(alert, animated: true)
-      return
     }
-
-    let mail = MFMailComposeViewController().then {
-      $0.mailComposeDelegate = self
-      $0.setToRecipients([self.supportEmail])
-    }
-
-    var formTitle = Strings.VPN.contactFormTitle
-    if let issue = contactForm.issue {
-      formTitle += " + \(issue)"
-    }
-
-    mail.setSubject(formTitle)
-    mail.setMessageBody(self.composeEmailBody(with: self.contactForm), isHTML: false)
-    present(mail, animated: true)
-  }
-
-  private var getNetworkType: String {
-    let type = Reachability.shared.status.connectionType
-    if type == .offline {
-      return "-"
-    }
-
-    return type.description
-  }
-
-  nonisolated private func fetchReceipt() async -> String? {
-    guard let receiptUrl = Bundle.main.appStoreReceiptURL else { return nil }
-    do {
-      return try Data(contentsOf: receiptUrl).base64EncodedString()
-    } catch {
-      Logger.module.error("\(error.localizedDescription)")
-      return nil
+    .alert(Strings.genericErrorTitle, isPresented: $isEmailUnavailableAlertPresented) {
+      Button(Strings.OKString) {}
+    } message: {
+      Text(Strings.VPN.contactFormEmailNotConfiguredBody)
     }
   }
 
-  private func composeEmailBody(with contactForm: ContactForm) -> String {
+  private var emailMessageBody: String {
     var body = "\n"
 
     body.append(contentsOf: "#### \(Strings.VPN.contactFormDoNotEditText) ####\n\n")
@@ -344,75 +134,162 @@ class BraveVPNContactFormViewController: TableViewController {
     body.append(Strings.VPN.contactFormPlatform)
     body.append("\n\(UIDevice.current.systemName)\n\n")
 
-    if let issue = contactForm.issue {
+    if let issue = issueType?.displayString {
       body.append(Strings.VPN.contactFormIssue)
       body.append("\n\(issue)\n\n")
     }
 
-    if let hostname = contactForm.hostname {
+    if includes.hostname, let hostname = BraveVPN.hostname {
       body.append(Strings.VPN.contactFormHostname)
       body.append("\n\(hostname)\n\n")
     }
 
-    if let tunnelProtocol = contactForm.tunnelProtocol {
+    if includes.tunnelProtocol {
       body.append(Strings.VPN.protocolPickerTitle)
       body.append("\n\(tunnelProtocol)\n\n")
     }
 
-    if let subcriptionType = contactForm.subscriptionType {
+    if includes.subscriptionType {
       body.append(Strings.VPN.contactFormSubscriptionType)
-      body.append("\n\(subcriptionType)\n\n")
+      body.append("\n\(BraveVPN.subscriptionName)\n\n")
     }
 
-    if let appVersion = contactForm.appVersion {
+    if includes.appVersion {
       body.append(Strings.VPN.contactFormAppVersion)
-      body.append("\n\(appVersion)\n\n")
+      body.append("\n\(AppInfo.appVersion)\n\n")
     }
 
-    if let timezone = contactForm.timezone {
+    if includes.timezone {
       body.append(Strings.VPN.contactFormTimezone)
-      body.append("\n\(timezone)\n\n")
+      body.append("\n\(TimeZone.current.description)\n\n")
     }
 
-    if let networkType = contactForm.networkType {
+    if includes.networkType {
       body.append(Strings.VPN.contactFormNetworkType)
       body.append("\n\(networkType)\n\n")
     }
 
-    if let carrier = contactForm.cellularCarrier {
+    if includes.cellularCarrier {
+      let carrierName =
+        CTTelephonyNetworkInfo().serviceSubscriberCellularProviders?
+        .first?.value.carrierName ?? "-"
       body.append(Strings.VPN.contactFormCarrier)
-      body.append("\n\(carrier)\n\n")
+      body.append("\n\(carrierName)\n\n")
     }
 
-    if let logs = contactForm.logs {
+    if includes.logs {
+      let logs = BraveVPN.errorLog
       body.append("\(Strings.VPN.contactFormLogs)\n")
       let formatter = DateFormatter()
       formatter.dateStyle = .short
       formatter.timeStyle = .long
 
-      logs.forEach {
-        body.append("\(formatter.string(from: $0.date)): \($0.message)\n")
+      for log in logs {
+        body.append("\(formatter.string(from: log.date)): \(log.message)\n")
       }
 
       body.append("\n")
     }
 
-    if let receipt = contactForm.receipt {
+    if includes.receipt,
+      let receiptUrl = Bundle.main.appStoreReceiptURL,
+      let receiptData = try? Data(contentsOf: receiptUrl)
+    {
       body.append(Strings.VPN.contactFormAppStoreReceipt)
-      body.append("\n\(receipt)\n\n")
+      body.append("\n\(receiptData.base64EncodedString())\n\n")
     }
 
     return body
   }
-}
 
-// MARK: - MFMailComposeViewControllerDelegate
-extension BraveVPNContactFormViewController: MFMailComposeViewControllerDelegate {
-  func mailComposeController(
-    _ controller: MFMailComposeViewController,
-    didFinishWith result: MFMailComposeResult,
-    error: Error?
-  ) {
-    controller.dismiss(animated: true)
+  private var tunnelProtocol: String {
+    let userPreferredTunnelProtocol = GRDTransportProtocol.getUserPreferredTransportProtocol()
+    return GRDTransportProtocol.prettyTransportProtocolString(for: userPreferredTunnelProtocol)
+  }
+
+  private var networkType: String {
+    let type = Reachability.shared.status.connectionType
+    if type == .offline {
+      return "-"
+    }
+    return type.description
+  }
+
+  private struct IssuePicker: View {
+    @Binding var issueType: IssueType?
+    @Environment(\.dismiss) private var dismiss
+
+    var body: some View {
+      Form {
+        Section {
+          ForEach(IssueType.allCases, id: \.self) { issue in
+            Button {
+              issueType = issue
+              dismiss()
+            } label: {
+              Text(issue.displayString)
+                .foregroundStyle(Color(braveSystemName: .textPrimary))
+            }
+            .listRowBackground(Color(.secondaryBraveGroupedBackground))
+          }
+        } header: {
+          Text(Strings.VPN.contactFormIssue)
+        } footer: {
+          Text(Strings.VPN.contactFormIssueDescription)
+        }
+      }
+      .scrollContentBackground(.hidden)
+      .background(Color(.braveGroupedBackground))
+    }
   }
 }
+
+private struct MailComposeRepresentable: UIViewControllerRepresentable {
+  var subject: String
+  var recipients: [String]
+  var messageBody: String
+
+  func makeUIViewController(context: Context) -> MFMailComposeViewController {
+    let composer = MFMailComposeViewController()
+    composer.mailComposeDelegate = context.coordinator
+    return composer
+  }
+  func updateUIViewController(_ uiViewController: MFMailComposeViewController, context: Context) {
+    uiViewController.setToRecipients(recipients)
+    uiViewController.setSubject(subject)
+    uiViewController.setMessageBody(messageBody, isHTML: false)
+  }
+
+  func makeCoordinator() -> Coordinator {
+    Coordinator()
+  }
+
+  class Coordinator: NSObject, MFMailComposeViewControllerDelegate {
+    func mailComposeController(
+      _ controller: MFMailComposeViewController,
+      didFinishWith result: MFMailComposeResult,
+      error: Error?
+    ) {
+      controller.dismiss(animated: true)
+    }
+  }
+}
+
+class BraveVPNContactFormViewController: UIHostingController<VPNContactFormView> {
+  init() {
+    super.init(rootView: VPNContactFormView())
+  }
+
+  @available(*, unavailable)
+  required init(coder: NSCoder) {
+    fatalError()
+  }
+}
+
+#if DEBUG
+#Preview {
+  NavigationStack {
+    VPNContactFormView()
+  }
+}
+#endif
