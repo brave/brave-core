9bad9cada06b012ecf6fa45e4a6d985f0c98f327
Emerick Rogul <erogul@brave.com>
2026-02-06 08:53:43 -0500

    [cr146] Add sandbox_policy overrides to unit test
    The `RendererFeatureSandboxWinTest` test was updated to subclass
    `::sandbox::TargetConfig`. Since we previously added overrides to that class, we
    must add them here as well.

---
+++ chromium_src/content/browser/renderer_host/renderer_sandboxed_process_launcher_delegate_unittest.cc
@@ -0,0 +1,18 @@
+/* Copyright (c) 2026 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "sandbox/win/src/sandbox_policy.h"
+#include "sandbox/win/src/sandbox_policy_base.h"
+
+#define IsConfigured                         \
+  IsConfigured() const override {            \
+    return false;                            \
+  }                                          \
+  void SetShouldPatchModuleFileName(bool) {} \
+  bool ShouldPatchModuleFileName
+
+#include <content/browser/renderer_host/renderer_sandboxed_process_launcher_delegate_unittest.cc>  // IWYU pragma: export
+
+#undef IsConfigured
