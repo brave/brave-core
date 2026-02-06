ce1cd8327e6f6e6c73ab5bdb343000d05812e200
Szilard Szaloki <szilardszaloki@gmail.com>
2026-02-06 16:05:03 -0700

    Account: fix Esc key not closing Brave Account dialog

--- components/brave_account/resources/brave_account_dialogs.ts
+++ components/brave_account/resources/brave_account_dialogs.ts
@@ -4,6 +4,10 @@
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 import { CrLitElement } from '//resources/lit/v3_0/lit.rollup.js'
+// <if expr="not is_android and not is_ios">
+import { EventTracker } from '//resources/js/event_tracker.js'
+import { hasKeyModifiers } from '//resources/js/util.js'
+// </if>
 
 import {
   BraveAccountBrowserProxy,
@@ -46,6 +50,27 @@ export class BraveAccountDialogs extends CrLitElement {
     BraveAccountBrowserProxyImpl.getInstance()
 
   protected accessor dialog: Dialog = { type: 'ENTRY' }
+
+  // <if expr="not is_android and not is_ios">
+  override connectedCallback() {
+    super.connectedCallback()
+    this.eventTracker.add(document, 'keydown', this.onKeyDown)
+  }
+
+  override disconnectedCallback() {
+    super.disconnectedCallback()
+    this.eventTracker.removeAll()
+  }
+
+  private onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === 'Escape' && !hasKeyModifiers(e)) {
+      this.onCloseDialog()
+      e.preventDefault()
+    }
+  }
+
+  private eventTracker = new EventTracker()
+  // </if>
 }
 
 declare global {
