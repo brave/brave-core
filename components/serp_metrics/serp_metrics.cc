7397a0fbf283f2ea32f5a441266c586249332740
Terry Mancey <terry.mancey@icloud.com>
2026-02-09 14:49:55 -0500

    Follow up to #52104: Deleted SERP metrics can be recovered (#33770)
    This occurred because TimePeriodStorage keeps internal state in memory
    that can be rewritten to disk; as a result, SERP metrics reappear if the
    user visits SERP again, but they do not reappear if the browser is
    restarted, since the in-memory state is cleared.

--- components/serp_metrics/serp_metrics.cc
+++ components/serp_metrics/serp_metrics.cc
@@ -135,6 +135,12 @@ size_t SerpMetrics::GetSearchCountForStalePeriod() const {
          GetOtherSearchCountForStalePeriod();
 }
 
+void SerpMetrics::ClearHistory() {
+  brave_search_engine_time_period_storage_.Clear();
+  google_search_engine_time_period_storage_.Clear();
+  other_search_engine_time_period_storage_.Clear();
+}
+
 ///////////////////////////////////////////////////////////////////////////////
 
 base::Time SerpMetrics::GetStartOfStalePeriod() const {
