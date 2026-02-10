fb2ebcdc96fd513511eee83a532fca9d4f88b23e
Claudio DeSouza <cdesouza@brave.com>
2026-02-10 15:54:17 +0000

    [cr146] ESLint fixes for extra logic in HTML templates
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/57f40ebb63bb78d0e8a4b63384b18f4fc147b5b5
    
    commit 57f40ebb63bb78d0e8a4b63384b18f4fc147b5b5
    Author: rbpotter <rbpotter@chromium.org>
    Date:   Mon Feb 9 15:11:47 2026 -0800
    
        WebUI: Add a custom eslint rule to prevent extra logic in HTML templates
    
        Initially, automatically detecting:
        * For/for of statements
        * Extra function declarations
    
        Additional checks will be added in followup CLs, after addressing
        existing violations.
    
        Bug: 481519338
        Change-Id: I900492d1d7777f6065f17758e319d361b5a10641
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7541564
        Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
        Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1582082}

--- chromium_src/chrome/browser/resources/tab_search/auto_tab_groups/auto_tab_groups_page.html.ts
+++ chromium_src/chrome/browser/resources/tab_search/auto_tab_groups/auto_tab_groups_page.html.ts
@@ -6,121 +6,9 @@
 import { html } from '//resources/lit/v3_0/lit.rollup.js'
 import type { AutoTabGroupsPageElement } from './auto_tab_groups_page.js'
 
-function getUndoFocusTabsHtml(this:  AutoTabGroupsPageElement) {
-  return this.undoTopic_ !== '' ? html`
-    <leo-alert
-      id="undo"
-      type="success"
-      hideIcon={true}
-    >
-      ${this.getWindowCreatedMessage_()}
-      <leo-button
-        id="undoButton"
-        kind="plain-faint"
-        size="tiny"
-        @click="${this.onUndoClicked_}"
-      >
-        ${this.getUndoButtonLabel_()}
-      </leo-button>
-    </leo-alert>
-  ` : ''
-}
-
-function getTopicsHtml(this:  AutoTabGroupsPageElement) {
-  return this.isLoadingTopics
-  ? [1, 2, 3, 4, 5].map((key) => html`
-    <leo-button
-      key=${key}
-      class="topics-button"
-      size="small"
-      kind="outline"
-      isDisabled={true}
-    >
-      <div class="topic-description">
-        <div class="emoji-wrapper">
-          <leo-progressring class="loading-ring"></leo-progressring>
-        </div>
-        <div class="empty-state"></div>
-      </div>
-    </leo-button>
-  `)
-  : this.topics_.map((entry, index) => html`
-      <leo-button
-        id="${this.getTopicId_(index)}"
-        class="topics-button"
-        size="small"
-        kind="outline"
-        @click="${() => this.onTopicClicked_(index)}"
-      >
-        <div class="topic-description">
-          <div class="emoji-wrapper">
-            ${this.getTopicEmoji_(entry)}
-          </div>
-          ${this.getTopicWithoutEmoji_(entry)}
-        </div>
-      </leo-button>
-  `)
-}
-
-function getHeaderHtml(this:  AutoTabGroupsPageElement) {
-  return html`
-    <div class="title-row">
-      ${!this.showBackButton ? html`
-        <div>
-          <leo-button
-            size="medium"
-            kind="plain-faint"
-            fab
-            @click=${this.onBackClick_}
-          >
-            <leo-icon name="arrow-left"></leo-icon>
-          </leo-button>
-        </div>
-      ` : ''}
-      <div class='title-column'>
-        <div class="title">${this.getTitle_()}</div>
-        ${!this.showFRE_ ? html`
-          <div class="subtitle">${this.getSubtitle_()}</div>
-        ` : ''}
-      </div>
-    </div>
-  `
-}
-
-function getEnableTabFocusHtml(this:  AutoTabGroupsPageElement) {
-  return html`
-    <div class="enable-tab-focus-wrapper">
-      ${getHeaderHtml.bind(this)()}
-      <div class="enable-tab-focus-content-wrapper">
-        <div class="enable-tab-focus-illustration"></div>
-        <div class="enable-tab-focus-info-wrapper">
-          <span class="enable-tab-focus-info-text">
-            ${this.getPrivacyDisclaimerMessage_()}
-          </span>
-          <div class="enable-tab-focus-button-row">
-            <span class="learn-more-link" @click=${this.onLearnMoreClicked_}>
-              ${this.getLearnMoreLabel_()}
-            </span>
-            <div>
-            <leo-button
-              id="enableButton"
-              kind="filled"
-              size="small"
-              @click="${this.onEnableTabFocusClicked_}"
-            >
-              ${this.getEnableButtonLabel_()}
-            </leo-button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  `
-}
-
 export function getHtml(this:  AutoTabGroupsPageElement) {
   if (this.showFRE_) {
-    return getEnableTabFocusHtml.bind(this)()
+    return this.getEnableTabFocusHtml_()
   }
   return html`<!--_html_template_start_-->
     <div id="brave-tab-focus" class="brave-tab-focus">
@@ -130,7 +18,7 @@ export function getHtml(this:  AutoTabGroupsPageElement) {
         aria-live="polite"
         aria-relevant="all"
       >
-        ${getHeaderHtml.bind(this)()}
+        ${this.getHeaderHtml_()}
         <div class="input-row">
           <leo-input
             id="topic-input"
@@ -151,12 +39,12 @@ export function getHtml(this:  AutoTabGroupsPageElement) {
             ${this.getSubmitButtonLabel_()}
           </leo-button>
         </div>
-        ${getUndoFocusTabsHtml.bind(this)()}
+        ${this.getUndoFocusTabsHtml_()}
       </div>
       ${this.errorMessage === '' ? html`
         <div class="topics-wrapper">
           <div class="topics-title">${this.getSuggestedTopicsSubtitle_()}</div>
-          ${getTopicsHtml.bind(this)()}
+          ${this.getTopicsHtml_()}
         </div>
       ` : html`
           <div class="error-wrapper">
