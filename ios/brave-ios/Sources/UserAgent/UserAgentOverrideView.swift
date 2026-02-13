e6f5aa60c05c23dd8e761751ab52c5b8079ede53
StephenHeaps <5314553+StephenHeaps@users.noreply.github.com>
2026-02-13 10:28:09 -0500

    [iOS] Add some additional User Agents to debug view (#33918)
    * Add Safari mobile/desktop with Brave comment to User Agent Override
    
    * Add Safari mobile/desktop w/ Brave suffix

--- ios/brave-ios/Sources/UserAgent/UserAgentOverrideView.swift
+++ ios/brave-ios/Sources/UserAgent/UserAgentOverrideView.swift
@@ -15,6 +15,10 @@ public enum UserAgentOverride: Equatable, Identifiable, Hashable, CaseIterable {
   case chromeDesktop
   case chromeMobileBraveComment
   case chromeDesktopBraveComment
+  case safariMobileBraveComment
+  case safariDesktopBraveComment
+  case safariMobileBraveSuffix
+  case safariDesktopBraveSuffix
   case custom
 
   /// Name to display in the Picker / drop down
@@ -38,6 +42,14 @@ public enum UserAgentOverride: Equatable, Identifiable, Hashable, CaseIterable {
       return "Chrome Mobile w/ Brave comment"
     case .chromeDesktopBraveComment:
       return "Chrome Desktop w/ Brave comment"
+    case .safariMobileBraveComment:
+      return "Safari Mobile w/ Brave comment"
+    case .safariDesktopBraveComment:
+      return "Safari Desktop w/ Brave comment"
+    case .safariMobileBraveSuffix:
+      return "Safari Mobile w/ Brave suffix"
+    case .safariDesktopBraveSuffix:
+      return "Safari Desktop w/ Brave suffix"
     case .custom:
       return "Custom"
     }
@@ -80,6 +92,24 @@ public enum UserAgentOverride: Equatable, Identifiable, Hashable, CaseIterable {
         useSafariUA: false,
         useChromiumVersion: true
       )
+    case .safariMobileBraveComment:
+      let safariUA = UserAgent.mobileMasked
+      // add ` (Brave)` after last occurance of `Safari`
+      if let range = safariUA.range(of: "Safari", options: [.backwards]) {
+        return safariUA.replacingCharacters(in: range, with: "Safari (Brave)")
+      }
+      return safariUA
+    case .safariDesktopBraveComment:
+      let safariUA = UserAgent.desktopMasked
+      // add ` (Brave)` after last occurance of `Safari`
+      if let range = safariUA.range(of: "Safari", options: [.backwards]) {
+        return safariUA.replacingCharacters(in: range, with: "Safari (Brave)")
+      }
+      return safariUA
+    case .safariMobileBraveSuffix:
+      return UserAgent.mobileMasked.appending(" Brave")
+    case .safariDesktopBraveSuffix:
+      return UserAgent.desktopMasked.appending(" Brave")
     case .custom:
       return nil
     }
