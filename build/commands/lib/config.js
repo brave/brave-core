265937c16050deda6ee8221cc71e42e39ce28c86
Mikhail <matuchin@brave.com>
2026-02-14 22:10:28 +0400

    Fix some eslint js issues (#33856)
    The PR fixes eslint issues in *.js files and removes the related rules from the backlist.

--- build/commands/lib/config.js
+++ build/commands/lib/config.js
@@ -1244,7 +1244,7 @@ Object.defineProperty(Config.prototype, 'outputDir', {
     return path.join(baseDir, buildConfigDir)
   },
   set: function (outputDir) {
-    return (this.__outputDir = outputDir)
+    this.__outputDir = outputDir
   },
 })
 
