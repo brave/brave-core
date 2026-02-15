4d6765a34ce0a4093157500bad2274843841173e
Claudio DeSouza <cdesouza@brave.com>
2026-02-15 03:26:30 +0000

    [cr147] Fix lint warnings about if statements in templates
    This change removes `if` statements from source files used for templates
    where the linter was warning about `if` statements. In one case the
    logic is moved into a typescript file, while in another a ternary
    expression is used.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/8632a400263a6ce988712b25b14d1152863ef93e
    
    commit 8632a400263a6ce988712b25b14d1152863ef93e
    Author: rbpotter <rbpotter@chromium.org>
    Date:   Thu Feb 12 14:26:48 2026 -0800
    
        WebUI: Add check for if statements in Lit element template files
    
        Per the styleguide, such statements should not be used. Ternary
        expressions for conditional rendering should be used in most cases
        instead, and cases with complex logic should be delegated to the
        class definition file.
    
        Bug: 481519338
        Change-Id: I04ae0f1e0722b068282f4ec02f6660a55b730b06
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7571091
        Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
        Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1584218}

--- components/brave_account/resources/brave_account_error_dialog.html.ts
+++ components/brave_account/resources/brave_account_error_dialog.html.ts
@@ -6,73 +6,12 @@
 import './brave_account_dialog.js'
 
 import { html } from '//resources/lit/v3_0/lit.rollup.js'
-import { loadTimeData } from '//resources/js/load_time_data.js'
-
 import { BraveAccountErrorDialogElement } from './brave_account_error_dialog.js'
-import { BraveAccountStrings } from './brave_components_webui_strings.js'
-import {
-  LoginErrorCode,
-  RegisterErrorCode,
-} from './brave_account.mojom-webui.js'
 
 export function getHtml(this: BraveAccountErrorDialogElement) {
   return html`<!--_html_template_start_-->
     <brave-account-dialog
-      alert-message=${(() => {
-        const LOGIN_ERROR_STRINGS: Partial<Record<LoginErrorCode, string>> = {
-          [LoginErrorCode.kIncorrectEmail]:
-            '$i18n{BRAVE_ACCOUNT_ERROR_DIALOG_INCORRECT_EMAIL}',
-          [LoginErrorCode.kIncorrectPassword]:
-            '$i18n{BRAVE_ACCOUNT_ERROR_DIALOG_INCORRECT_PASSWORD}',
-        }
-
-        const REGISTER_ERROR_STRINGS: Partial<
-          Record<RegisterErrorCode, string>
-        > = {
-          [RegisterErrorCode.kAccountExists]:
-            '$i18n{BRAVE_ACCOUNT_ERROR_DIALOG_ACCOUNT_EXISTS}',
-          [RegisterErrorCode.kEmailDomainNotSupported]:
-            '$i18n{BRAVE_ACCOUNT_ERROR_DIALOG_EMAIL_DOMAIN_NOT_SUPPORTED}',
-          [RegisterErrorCode.kTooManyVerifications]:
-            '$i18n{BRAVE_ACCOUNT_ERROR_DIALOG_TOO_MANY_VERIFICATIONS}',
-        }
-
-        const getErrorMessage = <T extends LoginErrorCode | RegisterErrorCode>(
-          errorStrings: Partial<Record<T, string>>,
-          details: { statusCode: number | null; errorCode: T | null },
-        ): string => {
-          const { statusCode, errorCode } = details
-
-          if (statusCode == null) {
-            // client-side error
-            return loadTimeData.getStringF(
-              BraveAccountStrings.BRAVE_ACCOUNT_CLIENT_ERROR,
-              errorCode != null
-                ? ` ($i18n{BRAVE_ACCOUNT_ERROR}=${errorCode})`
-                : '',
-            )
-          }
-
-          // server-side error
-          return (
-            (errorCode != null ? errorStrings[errorCode] : null)
-            ?? loadTimeData.getStringF(
-              BraveAccountStrings.BRAVE_ACCOUNT_SERVER_ERROR,
-              statusCode,
-              errorCode != null
-                ? `, $i18n{BRAVE_ACCOUNT_ERROR}=${errorCode}`
-                : '',
-            )
-          )
-        }
-
-        switch (this.error.flow) {
-          case 'login':
-            return getErrorMessage(LOGIN_ERROR_STRINGS, this.error.details)
-          case 'register':
-            return getErrorMessage(REGISTER_ERROR_STRINGS, this.error.details)
-        }
-      })()}
+      alert-message=${this.getAlertMessage()}
       dialog-description="$i18n{BRAVE_ACCOUNT_ERROR_DIALOG_DESCRIPTION}"
       dialog-title="$i18n{BRAVE_ACCOUNT_ERROR_DIALOG_TITLE}"
     >
