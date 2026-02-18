d54851af744a573eb67aef2da43fc5386038ed42
Anthony Tseng <darkdh@gmail.com>
2026-02-18 15:13:25 -0800

    Add unit tests for BackgroundWebContents

---
+++ components/local_ai/content/background_web_contents_unittest.cc
@@ -0,0 +1,98 @@
+// Copyright (c) 2026 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "brave/components/local_ai/content/background_web_contents.h"
+
+#include <memory>
+
+#include "content/public/test/navigation_simulator.h"
+#include "content/public/test/test_renderer_host.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
+
+namespace local_ai {
+
+class MockDelegate : public BackgroundWebContents::Delegate {
+ public:
+  MOCK_METHOD(void, OnBackgroundContentsReady, (), (override));
+  MOCK_METHOD(void,
+              OnBackgroundContentsDestroyed,
+              (BackgroundWebContents::DestroyReason reason),
+              (override));
+};
+
+class BackgroundWebContentsTest : public content::RenderViewHostTestHarness {
+ protected:
+  void TearDown() override {
+    background_contents_.reset();
+    content::RenderViewHostTestHarness::TearDown();
+  }
+
+  void CreateContents(const base::Location& location = FROM_HERE) {
+    SCOPED_TRACE(testing::Message() << location.ToString());
+    background_contents_ = std::make_unique<BackgroundWebContents>(
+        browser_context(), GURL("chrome-untrusted://test/"), &delegate_);
+  }
+
+  // Navigate the background WebContents to trigger DidFinishLoad.
+  void SimulatePageLoad(const base::Location& location = FROM_HERE) {
+    SCOPED_TRACE(testing::Message() << location.ToString());
+    auto* wc = background_contents_->web_contents();
+    ASSERT_NE(nullptr, wc);
+    content::NavigationSimulator::NavigateAndCommitFromBrowser(
+        wc, GURL("chrome-untrusted://test/"));
+  }
+
+  testing::NiceMock<MockDelegate> delegate_;
+  std::unique_ptr<BackgroundWebContents> background_contents_;
+};
+
+TEST_F(BackgroundWebContentsTest, ConstructionCreatesWebContents) {
+  CreateContents();
+  EXPECT_NE(nullptr, background_contents_->web_contents());
+}
+
+TEST_F(BackgroundWebContentsTest, DidFinishLoadCallsReady) {
+  CreateContents();
+
+  EXPECT_CALL(delegate_, OnBackgroundContentsReady());
+
+  SimulatePageLoad();
+}
+
+TEST_F(BackgroundWebContentsTest,
+       CloseContentsCallsOnBackgroundContentsDestroyed) {
+  CreateContents();
+
+  EXPECT_CALL(delegate_, OnBackgroundContentsDestroyed(
+                             BackgroundWebContents::DestroyReason::kClose));
+
+  // CloseContents is called when window.close() fires.
+  static_cast<content::WebContentsDelegate*>(background_contents_.get())
+      ->CloseContents(background_contents_->web_contents());
+}
+
+TEST_F(BackgroundWebContentsTest, UnexpectedUrlCallsDestroyed) {
+  CreateContents();
+
+  EXPECT_CALL(delegate_, OnBackgroundContentsReady()).Times(0);
+  EXPECT_CALL(delegate_,
+              OnBackgroundContentsDestroyed(
+                  BackgroundWebContents::DestroyReason::kInvalidUrl));
+
+  // Navigate to a different URL than expected.
+  auto* wc = background_contents_->web_contents();
+  content::NavigationSimulator::NavigateAndCommitFromBrowser(
+      wc, GURL("chrome-untrusted://wrong/"));
+}
+
+TEST_F(BackgroundWebContentsTest, DestructorDoesNotFireDelegateCallbacks) {
+  CreateContents();
+  // Destroying should not crash or fire delegate callbacks.
+  background_contents_.reset();
+}
+
+}  // namespace local_ai
