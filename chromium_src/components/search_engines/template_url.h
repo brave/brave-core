dcb0e9706adabb3b80ec3cbf39d2a48e5c5cb05e
Emerick Rogul <erogul@brave.com>
2026-02-18 15:42:27 -0500

    [cr146] is_ask_starter_pack -> is_ask_type
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/7237e3a37c200ca815a77ad3d27b53874967f8eb
    
    commit 7237e3a37c200ca815a77ad3d27b53874967f8eb
    Author: Alex Chen <alexwchen@chromium.org>
    Date:   Sun Feb 15 13:59:39 2026 -0800
    
        [146] [search aggregator] Use ask language for engine descriptions
    
        Original change's description:
        > [search aggregator] Use ask language for engine descriptions
        >
        > Use "ask" language for enterprise search aggregators instead of
        > "search".
        >
        > Before: https://screenshot.googleplex.com/6Ejv9TEqwYyTDqm
        > After: https://screenshot.googleplex.com/3oLuxCrDYJogFjM
        >
        > Bug: 483052039
        > Change-Id: Iccb33dc8572699280df3ecf19cd5c89ef5958a93
        > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7558899
        > Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
        > Commit-Queue: Alex Chen <alexwchen@chromium.org>
        > Cr-Commit-Position: refs/heads/main@{#1582789}
    
        Bug: 483795221
        Change-Id: Iccb33dc8572699280df3ecf19cd5c89ef5958a93
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7571685
        Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
        Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
        Cr-Commit-Position: refs/branch-heads/7680@{#358}
        Cr-Branched-From: 76b7d80e5cda23fe6537eed26d68c92e995c7f39-refs/heads/main@{#1582197}

--- chromium_src/components/search_engines/template_url.h
+++ chromium_src/components/search_engines/template_url.h
@@ -8,18 +8,18 @@
 
 // Return true for any Brave-specific starter packs that are "ask" oriented,
 // rather than search oriented.
-#define is_ask_starter_pack()                                             \
-  is_ask_starter_pack() const {                                           \
+#define is_ask_type()                                                     \
+  is_ask_type() const {                                                   \
     if (starter_pack_id() ==                                              \
         template_url_starter_pack_data::StarterPackId::kAskBraveSearch) { \
       return true;                                                        \
     }                                                                     \
-    return is_ask_starter_pack_chromium();                                \
+    return is_ask_type_chromium();                                        \
   }                                                                       \
-  bool is_ask_starter_pack_chromium()
+  bool is_ask_type_chromium()
 
 #include <components/search_engines/template_url.h>  // IWYU pragma: export
 
-#undef is_ask_starter_pack
+#undef is_ask_type
 
 #endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_SEARCH_ENGINES_TEMPLATE_URL_H_
