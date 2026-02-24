4ccbf258c8c467b57efd86260e706fd090e46eb1
Kevin Smith <zenparsing@gmail.com>
2026-02-24 14:12:35 -0500

    [Shields UI] Add main panel app components (#34117)

---
+++ components/brave_shields/resources/panel_new/components/use_initialized_status.ts
@@ -0,0 +1,26 @@
+/* Copyright (c) 2026 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import * as React from 'react'
+import { useShieldsApi } from '../api/shields_api_context'
+
+export function useInitializedStatus() {
+  const api = useShieldsApi()
+  const { data: siteBlockInfo } = api.useGetSiteBlockInfo()
+  const { data: siteSettings } = api.useGetSiteSettings()
+  const getAdvancedViewEnabled = api.useGetAdvancedViewEnabled()
+
+  const initialized = Boolean(
+    siteBlockInfo && siteSettings && !getAdvancedViewEnabled.isPlaceholderData,
+  )
+
+  React.useEffect(() => {
+    if (initialized) {
+      api.showUI()
+    }
+  }, [api, initialized])
+
+  return initialized
+}
