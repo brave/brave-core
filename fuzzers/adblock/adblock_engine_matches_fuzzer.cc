1e5f60a533a6f35852de34bb6e1e5aefefc9aad2
Anton Lazarev <22821309+antonok-edm@users.noreply.github.com>
2026-02-23 16:15:33 -0800

    Move adblock-rust FFI from `core/browser` to `core/common` (#34099)
    * move adblock-rust FFI from core/browser to core/common
    
    * fix existing formatting errors in adblock-rust FFI

--- fuzzers/adblock/adblock_engine_matches_fuzzer.cc
+++ fuzzers/adblock/adblock_engine_matches_fuzzer.cc
@@ -12,7 +12,7 @@
 #include "base/files/file_path.h"
 #include "base/i18n/icu_util.h"
 #include "brave/components/brave_component_updater/browser/dat_file_util.h"
-#include "brave/components/brave_shields/core/browser/adblock/rs/src/lib.rs.h"
+#include "brave/components/brave_shields/core/common/adblock/rs/src/lib.rs.h"
 #include "brave/fuzzers/adblock/adblock_fuzzer.pb.h"
 #include "testing/libfuzzer/proto/lpm_interface.h"
 #include "testing/libfuzzer/proto/url_proto_converter.h"
