07ad8b9eed624b1fb35060647e2de6c832e1a036
Aleksei Khoroshilov <5928869+goodov@users.noreply.github.com>
2026-02-24 20:55:29 +0700

    Revert "Make Origin upgrade case free for Linux users" (#34150)
    Revert "Make Origin upgrade case free for Linux users (#34115)"
    
    This reverts commit f086800037a078301ad29cc51d639c8c1ba6cb9c.

--- components/brave_origin/brave_origin_settings_handler_impl_unittest.cc
+++ components/brave_origin/brave_origin_settings_handler_impl_unittest.cc
@@ -21,7 +21,6 @@
 #include "brave/components/brave_origin/features.h"
 #include "brave/components/brave_origin/pref_names.h"
 #include "brave/components/skus/browser/test/fake_skus_service.h"
-#include "build/build_config.h"
 #include "components/policy/core/common/mock_policy_service.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/testing_pref_service.h"
@@ -408,8 +407,7 @@ TEST_F(BraveOriginHandlerWithSkusTest,
 }
 
 // In branded builds, RefreshPurchaseState unconditionally returns true.
-// On Linux, CheckPurchaseState always returns true (no SKU store).
-#if !BUILDFLAG(IS_BRAVE_ORIGIN_BRANDED) && !BUILDFLAG(IS_LINUX)
+#if !BUILDFLAG(IS_BRAVE_ORIGIN_BRANDED)
 TEST_F(BraveOriginHandlerWithSkusTest,
        RefreshPurchaseState_NoCredentials_ReturnsFalse) {
   fake_skus_service_->SetCredentialSummaryResponse(
