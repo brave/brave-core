ae4916524426fbbae120782e8f391af1b7e12a98
Max Karolinskiy <max@brave.com>
2026-02-25 14:28:25 -0500

    [cr147][iOS] LazyWebStateUserData<T> has been removed.
    Chromium change:
    https://chromium.googlesource.com/chromium/src.git/+/a5a6bc4b29a9a
    
    commit a5a6bc4b29a9a1c2dd75775f30c0937424218c5c
    Author: Sylvain Defresne <sdefresne@chromium.org>
    Date:   Mon Feb 23 09:14:05 2026 -0800
    
        [ios] Remove unused and deprecated template LazyWebStateUserData<T>
    
        This class is no longer useful since the WebStateUserData<T> are
        only created when the WebState becomes realized.
    
        Bug: 409299519

--- ios/browser/api/web_view/brave_web_view.mm
+++ ios/browser/api/web_view/brave_web_view.mm
@@ -254,12 +254,12 @@ class BraveWebViewHolder : public web::WebStateUserData<BraveWebViewHolder> {
   AttachTabHelpers(self.webState);
 
   if (ai_chat::features::IsAIChatWebUIEnabled()) {
-    ai_chat::UIHandlerBridgeHolder::GetOrCreateForWebState(self.webState)
+    ai_chat::UIHandlerBridgeHolder::FromWebState(self.webState)
         ->SetBridge(self.aiChatUIHandler);
-    ai_chat::AIChatTabHelper::GetOrCreateForWebState(self.webState)
+    ai_chat::AIChatTabHelper::FromWebState(self.webState)
         ->SetPageFetcher(self.aiChatUIHandler);
   }
-  brave_wallet::PageHandlerBridgeHolder::GetOrCreateForWebState(self.webState)
+  brave_wallet::PageHandlerBridgeHolder::FromWebState(self.webState)
       ->SetBridge(self.walletPageHandler);
 
   ProfileIOS* profile =
@@ -403,9 +403,9 @@ class BraveWebViewHolder : public web::WebStateUserData<BraveWebViewHolder> {
     (id<AIChatUIHandlerBridge, AIChatAssociatedContentPageFetcher>)bridge {
   _aiChatUIHandler = bridge;
   if (ai_chat::features::IsAIChatWebUIEnabled()) {
-    ai_chat::UIHandlerBridgeHolder::GetOrCreateForWebState(self.webState)
+    ai_chat::UIHandlerBridgeHolder::FromWebState(self.webState)
         ->SetBridge(bridge);
-    ai_chat::AIChatTabHelper::GetOrCreateForWebState(self.webState)
+    ai_chat::AIChatTabHelper::FromWebState(self.webState)
         ->SetPageFetcher(self.aiChatUIHandler);
   }
 }
@@ -416,7 +416,7 @@ class BraveWebViewHolder : public web::WebStateUserData<BraveWebViewHolder> {
 
 - (void)setWalletPageHandler:(id<WalletPageHandlerBridge>)bridge {
   _walletPageHandler = bridge;
-  brave_wallet::PageHandlerBridgeHolder::GetOrCreateForWebState(self.webState)
+  brave_wallet::PageHandlerBridgeHolder::FromWebState(self.webState)
       ->SetBridge(bridge);
 }
 
