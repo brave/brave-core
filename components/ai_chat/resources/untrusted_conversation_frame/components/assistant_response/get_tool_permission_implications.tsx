cbb12869c11919eab29c7c7cf4863bfa4ccb4be4
Pete Miller <miller.pete@gmail.com>
2026-02-24 21:58:31 -0800

    Don't include tab management tool strings on non-enabled builds

--- components/ai_chat/resources/untrusted_conversation_frame/components/assistant_response/get_tool_permission_implications.tsx
+++ components/ai_chat/resources/untrusted_conversation_frame/components/assistant_response/get_tool_permission_implications.tsx
@@ -3,12 +3,15 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
+// <if expr="enable_ai_chat_tab_management_tool">
 import * as React from 'react'
 import { formatLocale } from '$web-common/locale'
 import * as Mojom from '../../../common/mojom'
+// </if>
 
 export function getToolPermissionImplications(toolName: string) {
   switch (toolName) {
+    // <if expr="enable_ai_chat_tab_management_tool">
     case Mojom.TAB_MANAGEMENT_TOOL_NAME:
       return formatLocale(
         S.CHAT_UI_TOOL_TAB_MANAGEMENT_PERMISSION_IMPLICATIONS,
@@ -17,6 +20,7 @@ export function getToolPermissionImplications(toolName: string) {
           $2: (titleString) => <b>{titleString}</b>,
         },
       )
+    // </if>
     default:
       return null
   }
