0ff35622790da5cc58e78be7057407508b7769d3
Aleksei Khoroshilov <akhoroshilov@brave.com>
2026-02-27 19:32:08 +0700

    Use spawnSync to run tests.

--- build/commands/scripts/testScripts.ts
+++ build/commands/scripts/testScripts.ts
@@ -3,25 +3,27 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import { execSync } from 'child_process'
+import { spawnSync } from 'child_process'
 
 // Run the TypeScript compiler.
-run('tsc --project build/commands')
+run('tsc', ['--project', 'build/commands'])
 
 // Run the ESLint linter. This is required to catch possible runtime errors
 // that might occur because of broken imports.
-run('eslint --quiet build/commands')
+run('eslint', ['--quiet', 'build/commands'])
 
 // Run the Jest test runner. Pass through any extra arguments to it.
-const extraArgs = process.argv.slice(2).join(' ')
-run(`jest build/commands${extraArgs ? ` ${extraArgs}` : ''}`)
+run('jest', ['build/commands', ...process.argv.slice(2)])
 
-// Run a command and exit cleanly without exception noise.
-function run(cmd: string): void {
-  console.log(`> ${cmd}`)
-  try {
-    execSync(cmd, { stdio: 'inherit' })
-  } catch (e: any) {
-    process.exit(e.status ?? 1)
+// Run a command and exit on error.
+function run(cmd: string, args: string[]): void {
+  console.log(`> ${cmd} ${args.join(' ')}`)
+  if (process.platform === 'win32') {
+    args = ['/c', cmd, ...args]
+    cmd = 'cmd'
+  }
+  const result = spawnSync(cmd, args, { stdio: 'inherit' })
+  if (result.status !== 0) {
+    process.exit(result.status ?? 1)
   }
 }
