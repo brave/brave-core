aadedea2091fb304fdef8b0a1f75f361901982e9
Dev Doshi <dev.doshi@campus.tu-berlin.de>
2026-02-27 19:15:50 +0100

    refactor: use = default for operator== to cover all fields (C++20)

--- components/brave_wallet/browser/zcash/zcash_transaction.cc
+++ components/brave_wallet/browser/zcash/zcash_transaction.cc
@@ -32,12 +32,6 @@ ZCashTransaction::ZCashTransaction(const ZCashTransaction& other) = default;
 ZCashTransaction::ZCashTransaction(ZCashTransaction&& other) = default;
 ZCashTransaction& ZCashTransaction::operator=(ZCashTransaction&& other) =
     default;
-bool ZCashTransaction::operator==(const ZCashTransaction& other) const {
-  return std::tie(this->transparent_part_, this->orchard_part_, this->locktime_,
-                  this->to_, this->amount_, this->fee_, this->memo_) ==
-         std::tie(other.transparent_part_, other.orchard_part_, other.locktime_,
-                  other.to_, other.amount_, other.fee_, other.memo_);
-}
 
 ZCashTransaction::Outpoint::Outpoint() = default;
 ZCashTransaction::Outpoint::~Outpoint() = default;
