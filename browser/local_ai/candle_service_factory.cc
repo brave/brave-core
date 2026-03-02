left-TTC/kilo-core

Add CandleService and CandleServiceFactory
Add the CandleService KeyedService with background WebContents
management for running WASM in a hidden renderer. Includes the
CandleServiceFactory, idle timer for cleanup, and pending request
queue.

browser/local_ai/candle_service_factory.cc
@@ -1,0 +1,42 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "brave/browser/local_ai/candle_service_factory.h"
+
+#include <memory>
+
+#include "base/no_destructor.h"
+#include "brave/components/local_ai/browser/candle_service.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_selections.h"
+
+namespace local_ai {
+
+// static
+CandleServiceFactory* CandleServiceFactory::GetInstance() {
+  static base::NoDestructor<CandleServiceFactory> instance;
+  return instance.get();
+}
+
+// static
+CandleService* CandleServiceFactory::GetForProfile(Profile* profile) {
+  return static_cast<CandleService*>(
+      GetInstance()->GetServiceForBrowserContext(profile, true));
+}
+
+CandleServiceFactory::CandleServiceFactory()
+    : ProfileKeyedServiceFactory(
+          "CandleService",
+          ProfileSelections::BuildRedirectedInIncognito()) {}
+
+CandleServiceFactory::~CandleServiceFactory() = default;
+
+std::unique_ptr<KeyedService>
+CandleServiceFactory::BuildServiceInstanceForBrowserContext(
+    content::BrowserContext* context) const {
+  return std::make_unique<CandleService>(context);
+}
+
+}  // namespace local_ai

components/local_ai/browser/candle_service.cc
@@ -1,0 +1,211 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "brave/components/local_ai/browser/candle_service.h"
+
+#include "base/logging.h"
+#include "brave/components/constants/webui_url_constants.h"
+#include "content/public/browser/navigation_controller.h"
+#include "content/public/browser/web_contents.h"
+#include "ui/base/page_transition_types.h"
+
+namespace local_ai {
+
+CandleService::PendingEmbedRequest::PendingEmbedRequest() = default;
+CandleService::PendingEmbedRequest::PendingEmbedRequest(std::string text,
+                                                        EmbedCallback callback)
+    : text(std::move(text)), callback(std::move(callback)) {}
+CandleService::PendingEmbedRequest::~PendingEmbedRequest() = default;
+CandleService::PendingEmbedRequest::PendingEmbedRequest(PendingEmbedRequest&&) =
+    default;
+CandleService::PendingEmbedRequest&
+CandleService::PendingEmbedRequest::operator=(PendingEmbedRequest&&) = default;
+
+// WasmWebContentsObserver implementation
+WasmWebContentsObserver::WasmWebContentsObserver(
+    CandleService* service,
+    content::WebContents* web_contents)
+    : content::WebContentsObserver(web_contents), service_(service) {}
+
+WasmWebContentsObserver::~WasmWebContentsObserver() = default;
+
+void WasmWebContentsObserver::DidFinishLoad(
+    content::RenderFrameHost* render_frame_host,
+    const GURL& validated_url) {
+  DVLOG(3) << "WasmWebContentsObserver: WASM page loaded: " << validated_url;
+  if (service_) {
+    service_->OnWasmPageLoaded();
+  }
+}
+
+namespace {
+// Idle timeout before closing the WASM WebContents to free memory
+constexpr base::TimeDelta kIdleTimeout = base::Minutes(1);
+}  // namespace
+
+// CandleService implementation
+CandleService::CandleService(content::BrowserContext* browser_context)
+    : browser_context_(browser_context) {
+  DVLOG(3) << "CandleService created for browser context";
+
+  if (!browser_context_) {
+    DVLOG(0) << "CandleService: No browser context available";
+    return;
+  }
+}
+
+CandleService::~CandleService() {
+  CloseWasmWebContents();
+}
+
+void CandleService::BindReceiver(
+    mojo::PendingReceiver<mojom::CandleService> receiver) {
+  receivers_.Add(this, std::move(receiver));
+  DVLOG(3) << "BindReceiver called";
+}
+
+void CandleService::BindEmbeddingGemma(
+    mojo::PendingRemote<mojom::EmbeddingGemmaInterface> pending_remote) {
+  // Bind the single embedder remote from our WASM WebContents
+  if (embedding_gemma_remote_.is_bound()) {
+    DVLOG(1) << "EmbeddingGemma already bound, resetting";
+    embedding_gemma_remote_.reset();
+    embedding_ready_ = false;
+  }
+  embedding_gemma_remote_.Bind(std::move(pending_remote));
+
+  // Set up disconnect handler - this handles renderer crashes,
+  // manual kills, etc.
+  embedding_gemma_remote_.set_disconnect_handler(base::BindOnce(
+      [](CandleService* service) {
+        DVLOG(1) << "EmbeddingGemma remote disconnected "
+                    "(renderer crash or manual kill)";
+        // Clear pending requests on disconnect
+        for (auto& request : service->pending_embed_requests_) {
+          std::move(request.callback).Run({});
+        }
+        service->pending_embed_requests_.clear();
+        // Close WebContents and reset state so next Embed() will
+        // reinitialize
+        service->CloseWasmWebContents();
+      },
+      base::Unretained(this)));
+
+  DVLOG(3) << "BindEmbeddingGemma: Bound embedder remote";
+
+  // Mark as ready and process pending requests
+  embedding_ready_ = true;
+  ProcessPendingEmbedRequests();
+}
+
+void CandleService::Embed(const std::string& text, EmbedCallback callback) {
+  // Stop idle timer since we have activity
+  StopIdleTimer();
+
+  // Ensure WebContents exists (may have been closed due to idle)
+  EnsureWasmWebContents();
+
+  // If remote is not ready yet, queue the request
+  if (!embedding_ready_) {
+    DVLOG(3) << "Embedding not ready yet, queuing embed request";
+    pending_embed_requests_.emplace_back(text, std::move(callback));
+    return;
+  }
+
+  embedding_gemma_remote_->Embed(text, std::move(callback));
+
+  // Start idle timer after processing the request
+  StartIdleTimer();
+}
+
+void CandleService::OnWasmPageLoaded() {
+  DVLOG(3) << "CandleService: WASM page loaded";
+  wasm_page_loaded_ = true;
+}
+
+void CandleService::Shutdown() {
+  DVLOG(3) << "CandleService: Shutting down";
+
+  // Clear any pending requests
+  for (auto& request : pending_embed_requests_) {
+    std::move(request.callback).Run({});
+  }
+  pending_embed_requests_.clear();
+
+  CloseWasmWebContents();
+}
+
+void CandleService::ProcessPendingEmbedRequests() {
+  if (!embedding_ready_ || !embedding_gemma_remote_) {
+    return;
+  }
+
+  DVLOG(3) << "Processing " << pending_embed_requests_.size()
+           << " pending embed requests";
+
+  // Process all queued requests
+  for (auto& request : pending_embed_requests_) {
+    embedding_gemma_remote_->Embed(request.text, std::move(request.callback));
+  }
+  pending_embed_requests_.clear();
+
+  // Start idle timer after processing requests
+  StartIdleTimer();
+}
+
+void CandleService::EnsureWasmWebContents() {
+  if (wasm_web_contents_) {
+    return;  // Already created
+  }
+
+  DVLOG(3) << "CandleService: Creating WASM WebContents";
+
+  // Create a hidden WebContents to load the WASM
+  content::WebContents::CreateParams create_params(browser_context_);
+  create_params.is_never_composited = true;
+  wasm_web_contents_ = content::WebContents::Create(create_params);
+
+  // Create observer for the WebContents to track page load
+  wasm_web_contents_observer_ =
+      std::make_unique<WasmWebContentsObserver>(this, wasm_web_contents_.get());
+
+  // Navigate to the WASM page - this will trigger
+  // BindEmbeddingGemma automatically
+  GURL wasm_url(kUntrustedCandleEmbeddingGemmaWasmURL);
+  DVLOG(3) << "CandleService: Loading WASM from " << wasm_url;
+  wasm_web_contents_->GetController().LoadURL(wasm_url, content::Referrer(),
+                                              ui::PAGE_TRANSITION_AUTO_TOPLEVEL,
+                                              std::string());
+}
+
+void CandleService::CloseWasmWebContents() {
+  DVLOG(3) << "CandleService: Closing WASM WebContents "
+              "to free memory";
+
+  idle_timer_.Stop();
+  wasm_web_contents_observer_.reset();
+  embedding_gemma_remote_.reset();
+
+  if (wasm_web_contents_) {
+    wasm_web_contents_->Close();
+    wasm_web_contents_.reset();
+  }
+
+  // Reset state so we can reinitialize later
+  wasm_page_loaded_ = false;
+  embedding_ready_ = false;
+}
+
+void CandleService::StartIdleTimer() {
+  idle_timer_.Start(FROM_HERE, kIdleTimeout,
+                    base::BindOnce(&CandleService::CloseWasmWebContents,
+                                   weak_ptr_factory_.GetWeakPtr()));
+}
+
+void CandleService::StopIdleTimer() {
+  idle_timer_.Stop();
+}
+
+}  // namespace local_ai

components/local_ai/browser/candle_service.h
@@ -1,0 +1,123 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_COMPONENTS_LOCAL_AI_BROWSER_CANDLE_SERVICE_H_
+#define BRAVE_COMPONENTS_LOCAL_AI_BROWSER_CANDLE_SERVICE_H_
+
+#include <memory>
+#include <vector>
+
+#include "base/functional/callback.h"
+#include "base/memory/weak_ptr.h"
+#include "base/timer/timer.h"
+#include "brave/components/local_ai/common/candle.mojom.h"
+#include "components/keyed_service/core/keyed_service.h"
+#include "content/public/browser/web_contents_observer.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
+#include "mojo/public/cpp/bindings/receiver_set.h"
+#include "mojo/public/cpp/bindings/remote.h"
+
+namespace content {
+class BrowserContext;
+class WebContents;
+}  // namespace content
+
+namespace local_ai {
+
+class CandleService;
+
+// Helper class to observe WebContents for WASM page load events.
+// This is separated from CandleService so the service can be shared
+// with platforms that don't have content::WebContentsObserver.
+class WasmWebContentsObserver : public content::WebContentsObserver {
+ public:
+  explicit WasmWebContentsObserver(CandleService* service,
+                                   content::WebContents* web_contents);
+  ~WasmWebContentsObserver() override;
+
+  WasmWebContentsObserver(const WasmWebContentsObserver&) = delete;
+  WasmWebContentsObserver& operator=(const WasmWebContentsObserver&) = delete;
+
+ private:
+  // content::WebContentsObserver:
+  void DidFinishLoad(content::RenderFrameHost* render_frame_host,
+                     const GURL& validated_url) override;
+
+  raw_ptr<CandleService> service_;
+};
+
+class CandleService : public KeyedService, public mojom::CandleService {
+ public:
+  explicit CandleService(content::BrowserContext* browser_context);
+  ~CandleService() override;
+
+  CandleService(const CandleService&) = delete;
+  CandleService& operator=(const CandleService&) = delete;
+
+  void BindReceiver(mojo::PendingReceiver<mojom::CandleService> receiver);
+
+  void BindEmbeddingGemma(
+      mojo::PendingRemote<mojom::EmbeddingGemmaInterface>) override;
+
+  void Embed(const std::string& text, EmbedCallback callback) override;
+
+  // Called by WasmWebContentsObserver when the WASM page finishes
+  // loading
+  void OnWasmPageLoaded();
+
+ private:
+  friend class WasmWebContentsObserver;
+
+  // KeyedService:
+  void Shutdown() override;
+
+  void EnsureWasmWebContents();
+  void CloseWasmWebContents();
+  void StartIdleTimer();
+  void StopIdleTimer();
+
+  raw_ptr<content::BrowserContext> browser_context_ = nullptr;
+
+  // The single WebContents that loads the WASM and maintains the
+  // model
+  std::unique_ptr<content::WebContents> wasm_web_contents_;
+
+  // Observer for WASM WebContents load events
+  std::unique_ptr<WasmWebContentsObserver> wasm_web_contents_observer_;
+
+  mojo::ReceiverSet<mojom::CandleService> receivers_;
+
+  // Single embedder remote (shared by all callers)
+  mojo::Remote<mojom::EmbeddingGemmaInterface> embedding_gemma_remote_;
+
+  // Track readiness conditions
+  bool wasm_page_loaded_ = false;
+  bool embedding_ready_ = false;
+
+  // Queue for pending Embed requests while model is initializing
+  struct PendingEmbedRequest {
+    PendingEmbedRequest();
+    PendingEmbedRequest(std::string text, EmbedCallback callback);
+    ~PendingEmbedRequest();
+    PendingEmbedRequest(PendingEmbedRequest&&);
+    PendingEmbedRequest& operator=(PendingEmbedRequest&&);
+
+    std::string text;
+    EmbedCallback callback;
+  };
+  std::vector<PendingEmbedRequest> pending_embed_requests_;
+
+  void ProcessPendingEmbedRequests();
+
+  // Idle timer to close WebContents when not in use
+  base::OneShotTimer idle_timer_;
+
+  base::WeakPtrFactory<CandleService> weak_ptr_factory_{this};
+};
+
+}  // namespace local_ai
+
+#endif  // BRAVE_COMPONENTS_LOCAL_AI_BROWSER_CANDLE_SERVICE_H_

brave/brave-core

browser/local_ai/candle_service_factory.cc
@@ -1,0 +1,42 @@
+// Copyright (c) 2026 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "brave/browser/local_ai/candle_service_factory.h"
+
+#include <memory>
+
+#include "base/no_destructor.h"
+#include "brave/components/local_ai/content/candle_service.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_selections.h"
+
+namespace local_ai {
+
+// static
+CandleServiceFactory* CandleServiceFactory::GetInstance() {
+  static base::NoDestructor<CandleServiceFactory> instance;
+  return instance.get();
+}
+
+// static
+CandleService* CandleServiceFactory::GetForProfile(Profile* profile) {
+  return static_cast<CandleService*>(
+      GetInstance()->GetServiceForBrowserContext(profile, true));
+}
+
+CandleServiceFactory::CandleServiceFactory()
+    : ProfileKeyedServiceFactory(
+          "CandleService",
+          ProfileSelections::BuildRedirectedInIncognito()) {}
+
+CandleServiceFactory::~CandleServiceFactory() = default;
+
+std::unique_ptr<KeyedService>
+CandleServiceFactory::BuildServiceInstanceForBrowserContext(
+    content::BrowserContext* context) const {
+  return std::make_unique<CandleService>(context);
+}
+
+}  // namespace local_ai

components/local_ai/content/candle_service.cc
@@ -1,0 +1,211 @@
+// Copyright (c) 2026 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "brave/components/local_ai/content/candle_service.h"
+
+#include "base/logging.h"
+#include "brave/components/constants/webui_url_constants.h"
+#include "content/public/browser/navigation_controller.h"
+#include "content/public/browser/web_contents.h"
+#include "ui/base/page_transition_types.h"
+
+namespace local_ai {
+
+CandleService::PendingEmbedRequest::PendingEmbedRequest() = default;
+CandleService::PendingEmbedRequest::PendingEmbedRequest(std::string text,
+                                                        EmbedCallback callback)
+    : text(std::move(text)), callback(std::move(callback)) {}
+CandleService::PendingEmbedRequest::~PendingEmbedRequest() = default;
+CandleService::PendingEmbedRequest::PendingEmbedRequest(PendingEmbedRequest&&) =
+    default;
+CandleService::PendingEmbedRequest&
+CandleService::PendingEmbedRequest::operator=(PendingEmbedRequest&&) = default;
+
+// WasmWebContentsObserver implementation
+WasmWebContentsObserver::WasmWebContentsObserver(
+    CandleService* service,
+    content::WebContents* web_contents)
+    : content::WebContentsObserver(web_contents), service_(service) {}
+
+WasmWebContentsObserver::~WasmWebContentsObserver() = default;
+
+void WasmWebContentsObserver::DidFinishLoad(
+    content::RenderFrameHost* render_frame_host,
+    const GURL& validated_url) {
+  DVLOG(3) << "WasmWebContentsObserver: WASM page loaded: " << validated_url;
+  if (service_) {
+    service_->OnWasmPageLoaded();
+  }
+}
+
+namespace {
+// Idle timeout before closing the WASM WebContents to free memory
+constexpr base::TimeDelta kIdleTimeout = base::Minutes(1);
+}  // namespace
+
+// CandleService implementation
+CandleService::CandleService(content::BrowserContext* browser_context)
+    : browser_context_(browser_context) {
+  DVLOG(3) << "CandleService created for browser context";
+
+  if (!browser_context_) {
+    DVLOG(0) << "CandleService: No browser context available";
+    return;
+  }
+}
+
+CandleService::~CandleService() {
+  CloseWasmWebContents();
+}
+
+void CandleService::BindReceiver(
+    mojo::PendingReceiver<mojom::CandleService> receiver) {
+  receivers_.Add(this, std::move(receiver));
+  DVLOG(3) << "BindReceiver called";
+}
+
+void CandleService::BindEmbeddingGemma(
+    mojo::PendingRemote<mojom::EmbeddingGemmaInterface> pending_remote) {
+  // Bind the single embedder remote from our WASM WebContents
+  if (embedding_gemma_remote_.is_bound()) {
+    DVLOG(1) << "EmbeddingGemma already bound, resetting";
+    embedding_gemma_remote_.reset();
+    embedding_ready_ = false;
+  }
+  embedding_gemma_remote_.Bind(std::move(pending_remote));
+
+  // Set up disconnect handler - this handles renderer crashes,
+  // manual kills, etc.
+  embedding_gemma_remote_.set_disconnect_handler(base::BindOnce(
+      [](CandleService* service) {
+        DVLOG(1) << "EmbeddingGemma remote disconnected "
+                    "(renderer crash or manual kill)";
+        // Clear pending requests on disconnect
+        for (auto& request : service->pending_embed_requests_) {
+          std::move(request.callback).Run({});
+        }
+        service->pending_embed_requests_.clear();
+        // Close WebContents and reset state so next Embed() will
+        // reinitialize
+        service->CloseWasmWebContents();
+      },
+      base::Unretained(this)));
+
+  DVLOG(3) << "BindEmbeddingGemma: Bound embedder remote";
+
+  // Mark as ready and process pending requests
+  embedding_ready_ = true;
+  ProcessPendingEmbedRequests();
+}
+
+void CandleService::Embed(const std::string& text, EmbedCallback callback) {
+  // Stop idle timer since we have activity
+  StopIdleTimer();
+
+  // Ensure WebContents exists (may have been closed due to idle)
+  EnsureWasmWebContents();
+
+  // If remote is not ready yet, queue the request
+  if (!embedding_ready_) {
+    DVLOG(3) << "Embedding not ready yet, queuing embed request";
+    pending_embed_requests_.emplace_back(text, std::move(callback));
+    return;
+  }
+
+  embedding_gemma_remote_->Embed(text, std::move(callback));
+
+  // Start idle timer after processing the request
+  StartIdleTimer();
+}
+
+void CandleService::OnWasmPageLoaded() {
+  DVLOG(3) << "CandleService: WASM page loaded";
+  wasm_page_loaded_ = true;
+}
+
+void CandleService::Shutdown() {
+  DVLOG(3) << "CandleService: Shutting down";
+
+  // Clear any pending requests
+  for (auto& request : pending_embed_requests_) {
+    std::move(request.callback).Run({});
+  }
+  pending_embed_requests_.clear();
+
+  CloseWasmWebContents();
+}
+
+void CandleService::ProcessPendingEmbedRequests() {
+  if (!embedding_ready_ || !embedding_gemma_remote_) {
+    return;
+  }
+
+  DVLOG(3) << "Processing " << pending_embed_requests_.size()
+           << " pending embed requests";
+
+  // Process all queued requests
+  for (auto& request : pending_embed_requests_) {
+    embedding_gemma_remote_->Embed(request.text, std::move(request.callback));
+  }
+  pending_embed_requests_.clear();
+
+  // Start idle timer after processing requests
+  StartIdleTimer();
+}
+
+void CandleService::EnsureWasmWebContents() {
+  if (wasm_web_contents_) {
+    return;  // Already created
+  }
+
+  DVLOG(3) << "CandleService: Creating WASM WebContents";
+
+  // Create a hidden WebContents to load the WASM
+  content::WebContents::CreateParams create_params(browser_context_);
+  create_params.is_never_composited = true;
+  wasm_web_contents_ = content::WebContents::Create(create_params);
+
+  // Create observer for the WebContents to track page load
+  wasm_web_contents_observer_ =
+      std::make_unique<WasmWebContentsObserver>(this, wasm_web_contents_.get());
+
+  // Navigate to the WASM page - this will trigger
+  // BindEmbeddingGemma automatically
+  GURL wasm_url(kUntrustedCandleEmbeddingGemmaWasmURL);
+  DVLOG(3) << "CandleService: Loading WASM from " << wasm_url;
+  wasm_web_contents_->GetController().LoadURL(wasm_url, content::Referrer(),
+                                              ui::PAGE_TRANSITION_AUTO_TOPLEVEL,
+                                              std::string());
+}
+
+void CandleService::CloseWasmWebContents() {
+  DVLOG(3) << "CandleService: Closing WASM WebContents "
+              "to free memory";
+
+  idle_timer_.Stop();
+  wasm_web_contents_observer_.reset();
+  embedding_gemma_remote_.reset();
+
+  if (wasm_web_contents_) {
+    wasm_web_contents_->Close();
+    wasm_web_contents_.reset();
+  }
+
+  // Reset state so we can reinitialize later
+  wasm_page_loaded_ = false;
+  embedding_ready_ = false;
+}
+
+void CandleService::StartIdleTimer() {
+  idle_timer_.Start(FROM_HERE, kIdleTimeout,
+                    base::BindOnce(&CandleService::CloseWasmWebContents,
+                                   weak_ptr_factory_.GetWeakPtr()));
+}
+
+void CandleService::StopIdleTimer() {
+  idle_timer_.Stop();
+}
+
+}  // namespace local_ai

components/local_ai/content/candle_service.h
@@ -1,0 +1,123 @@
+// Copyright (c) 2026 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_COMPONENTS_LOCAL_AI_CONTENT_CANDLE_SERVICE_H_
+#define BRAVE_COMPONENTS_LOCAL_AI_CONTENT_CANDLE_SERVICE_H_
+
+#include <memory>
+#include <vector>
+
+#include "base/functional/callback.h"
+#include "base/memory/weak_ptr.h"
+#include "base/timer/timer.h"
+#include "brave/components/local_ai/core/candle.mojom.h"
+#include "components/keyed_service/core/keyed_service.h"
+#include "content/public/browser/web_contents_observer.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
+#include "mojo/public/cpp/bindings/receiver_set.h"
+#include "mojo/public/cpp/bindings/remote.h"
+
+namespace content {
+class BrowserContext;
+class WebContents;
+}  // namespace content
+
+namespace local_ai {
+
+class CandleService;
+
+// Helper class to observe WebContents for WASM page load events.
+// This is separated from CandleService so the service can be shared
+// with platforms that don't have content::WebContentsObserver.
+class WasmWebContentsObserver : public content::WebContentsObserver {
+ public:
+  explicit WasmWebContentsObserver(CandleService* service,
+                                   content::WebContents* web_contents);
+  ~WasmWebContentsObserver() override;
+
+  WasmWebContentsObserver(const WasmWebContentsObserver&) = delete;
+  WasmWebContentsObserver& operator=(const WasmWebContentsObserver&) = delete;
+
+ private:
+  // content::WebContentsObserver:
+  void DidFinishLoad(content::RenderFrameHost* render_frame_host,
+                     const GURL& validated_url) override;
+
+  raw_ptr<CandleService> service_;
+};
+
+class CandleService : public KeyedService, public mojom::CandleService {
+ public:
+  explicit CandleService(content::BrowserContext* browser_context);
+  ~CandleService() override;
+
+  CandleService(const CandleService&) = delete;
+  CandleService& operator=(const CandleService&) = delete;
+
+  void BindReceiver(mojo::PendingReceiver<mojom::CandleService> receiver);
+
+  void BindEmbeddingGemma(
+      mojo::PendingRemote<mojom::EmbeddingGemmaInterface>) override;
+
+  void Embed(const std::string& text, EmbedCallback callback) override;
+
+  // Called by WasmWebContentsObserver when the WASM page finishes
+  // loading
+  void OnWasmPageLoaded();
+
+ private:
+  friend class WasmWebContentsObserver;
+
+  // KeyedService:
+  void Shutdown() override;
+
+  void EnsureWasmWebContents();
+  void CloseWasmWebContents();
+  void StartIdleTimer();
+  void StopIdleTimer();
+
+  raw_ptr<content::BrowserContext> browser_context_ = nullptr;
+
+  // The single WebContents that loads the WASM and maintains the
+  // model
+  std::unique_ptr<content::WebContents> wasm_web_contents_;
+
+  // Observer for WASM WebContents load events
+  std::unique_ptr<WasmWebContentsObserver> wasm_web_contents_observer_;
+
+  mojo::ReceiverSet<mojom::CandleService> receivers_;
+
+  // Single embedder remote (shared by all callers)
+  mojo::Remote<mojom::EmbeddingGemmaInterface> embedding_gemma_remote_;
+
+  // Track readiness conditions
+  bool wasm_page_loaded_ = false;
+  bool embedding_ready_ = false;
+
+  // Queue for pending Embed requests while model is initializing
+  struct PendingEmbedRequest {
+    PendingEmbedRequest();
+    PendingEmbedRequest(std::string text, EmbedCallback callback);
+    ~PendingEmbedRequest();
+    PendingEmbedRequest(PendingEmbedRequest&&);
+    PendingEmbedRequest& operator=(PendingEmbedRequest&&);
+
+    std::string text;
+    EmbedCallback callback;
+  };
+  std::vector<PendingEmbedRequest> pending_embed_requests_;
+
+  void ProcessPendingEmbedRequests();
+
+  // Idle timer to close WebContents when not in use
+  base::OneShotTimer idle_timer_;
+
+  base::WeakPtrFactory<CandleService> weak_ptr_factory_{this};
+};
+
+}  // namespace local_ai
+
+#endif  // BRAVE_COMPONENTS_LOCAL_AI_CONTENT_CANDLE_SERVICE_H_