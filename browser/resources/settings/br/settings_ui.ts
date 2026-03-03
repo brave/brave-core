f7629d9a1312a5d20eba6db6fcf7e92a79ffbcd6
Agustín Ruiz <aguscr182@gmail.com>
2026-03-03 08:52:40 -0300

    Remove inner shadow from Settings header when inner page scroll is used (#34359)
    remove inner shadow

--- browser/resources/settings/br/settings_ui.ts
+++ browser/resources/settings/br/settings_ui.ts
@@ -71,9 +71,9 @@ RegisterStyleOverride(
       cr-drawer settings-menu::part(header) {
         display: none;
       }
-      .cr-container-shadow {
+      .cr-scrollable-top-shadow {
         display: none !important;
-     }
+      }
       #container {
         /* menu and content next to each other in the horizontal center */
       }
