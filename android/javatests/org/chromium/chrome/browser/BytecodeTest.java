d2cd032f54b77a29847d1a25b0d38d75cead564c
Artem Samoilenko <artem@brave.com>
2026-03-03 09:30:54 -0500

    [cr147][Android] Changes for DefaultBrowserPromoUtils.prepareLaunchPromoIfNeeded
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/91f9c167fbd8b6cbba25a3a455232eec0ae8b21e
    
    commit 91f9c167fbd8b6cbba25a3a455232eec0ae8b21e
    Author: Gazal <agazal@google.com>
    Date:   Thu Feb 26 08:21:11 2026 -0800
    
        [SetupList] Integrate Role Manager for Default Browser promo
    
        - Integrate Android Role Manager for the Setup List default browser promo with a bottom sheet fallback.
        - Refresh home modules on NTP resume to trigger animations when returning from system dialogs.
    
        Preview: https://screencast.googleplex.com/cast/NTQ4ODI4MDQ0NjQzNTMyOHwzNWMwOTVjZi01ZQ
    
        Bug: 469425754, 469430107
        Change-Id: I3f380e49fce81179fe2d9347733a1cc5651318c4
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7594638
        Commit-Queue: Gazal Agarwal <agazal@google.com>
        Auto-Submit: Gazal Agarwal <agazal@google.com>
        Reviewed-by: Eleanor Lee <eleanorlee@google.com>
        Reviewed-by: Theresa Sullivan <twellington@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1590847}

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -560,7 +560,8 @@ public class BytecodeTest {
                         boolean.class,
                         Activity.class,
                         WindowAndroid.class,
-                        Tracker.class));
+                        Tracker.class,
+                        int.class));
         Assert.assertTrue(
                 methodExists(
                         "org/chromium/chrome/browser/toolbar/ToolbarManager",
