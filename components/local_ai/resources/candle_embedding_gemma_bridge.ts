left-TTC/kilo-core

Add model loading to CandleService and WASM bridge
Add ModelFiles struct with BigBuffer fields and Init() method to
the mojom interface. CandleService now observes LocalModelsUpdater
for component readiness, loads model files from disk on a background
thread, and sends them to the WASM renderer via BigBuffer. The
TypeScript bridge extracts BigBuffer data and initializes the
Gemma3Embedder.

components/local_ai/resources/candle_embedding_gemma_bridge.ts
@@ -6,6 +6,7 @@
 import {
   CandleService,
   EmbeddingGemmaInterfaceReceiver,
+  ModelFiles,
 } from 'gen/brave/components/local_ai/common/candle.mojom.m.js'
 
 console.log('[Candle WASM] Embedding Gemma Script loaded')
@@ -20,16 +21,130 @@ console.log('[Candle WASM] CandleService remote obtained:', candleService)
 // Implement the EmbeddingGemmaInterface Mojo observer
 class EmbeddingGemmaInterfaceImpl {
   receiver: EmbeddingGemmaInterfaceReceiver
+  embedder: any | null
+  isInitialized: boolean
+  initTime: number
 
   constructor() {
     this.receiver = new EmbeddingGemmaInterfaceReceiver(this)
+    this.embedder = null
+    this.isInitialized = false
+    this.initTime = 0
+  }
+
+  // Implementation of EmbeddingGemmaInterface::Init
+  async init(modelFiles: ModelFiles): Promise<{ success: boolean }> {
+    if (this.isInitialized) {
+      console.log('Model already initialized')
+      return { success: true }
+    }
+
+    const startTime = performance.now()
+    console.log('Loading Embedding Gemma model from provided files...')
+
+    try {
+      // Helper function to extract data from BigBuffer
+      const extractBigBuffer = (
+        buffer: any,
+        name: string,
+      ): Uint8Array | null => {
+        if (buffer.bytes) {
+          const data = new Uint8Array(buffer.bytes)
+          console.log(`${name} using inline bytes, size:`, data.byteLength)
+          return data
+        } else if (buffer.sharedMemory) {
+          const sharedMem = buffer.sharedMemory
+          console.log(`${name} shared memory size:`, sharedMem.size)
+          const mapResult = sharedMem.bufferHandle.mapBuffer(0, sharedMem.size)
+          if (!mapResult.buffer) {
+            console.error(
+              `Failed to map ${name} shared buffer, ` + `result:`,
+              mapResult.result,
+            )
+            return null
+          }
+          const data = new Uint8Array(mapResult.buffer)
+          console.log(`${name} using shared memory, size:`, data.byteLength)
+          return data
+        } else {
+          console.error(
+            `Invalid ${name} BigBuffer: ` + `no bytes or shared memory`,
+          )
+          return null
+        }
+      }
+
+      // Extract all model files from BigBuffer
+      console.log('Extracting model files from BigBuffer...')
+      const weightsData = extractBigBuffer(modelFiles.weights, 'Weights')
+      const weightsDense1Data = extractBigBuffer(
+        modelFiles.weightsDense1,
+        'WeightsDense1',
+      )
+      const weightsDense2Data = extractBigBuffer(
+        modelFiles.weightsDense2,
+        'WeightsDense2',
+      )
+      const tokenizerData = extractBigBuffer(modelFiles.tokenizer, 'Tokenizer')
+      const configData = extractBigBuffer(modelFiles.config, 'Config')
+
+      if (
+        !weightsData
+        || !weightsDense1Data
+        || !weightsDense2Data
+        || !tokenizerData
+        || !configData
+      ) {
+        console.error('Failed to extract model files')
+        return { success: false }
+      }
+
+      // Dynamically import the WASM Gemma3Embedder class
+      console.log('Importing Gemma3Embedder module...')
+      const module = await import(
+        // @ts-ignore - Module path resolved at runtime via resources
+        'chrome-untrusted://resources/brave/candle_embedding_gemma.bundle.js'
+      )
+      const { Gemma3Embedder } = module
+
+      console.log('Creating Gemma3Embedder instance...')
+      this.embedder = new Gemma3Embedder(
+        weightsData,
+        weightsDense1Data,
+        weightsDense2Data,
+        tokenizerData,
+        configData,
+      )
+      this.isInitialized = true
+      this.initTime = performance.now() - startTime
+      console.log(
+        `Embedding Gemma model loaded successfully in `
+          + `${this.initTime.toFixed(2)}ms`,
+      )
+      return { success: true }
+    } catch (error) {
+      console.error('Failed to load model:', error)
+      return { success: false }
+    }
   }
 
   // Implementation of EmbeddingGemmaInterface::Embed
   async embed(input: string): Promise<{ output: number[] }> {
-    console.log('[Candle WASM] Embed called (model not loaded yet):', input)
-    // Model loading will be added in branch 3
-    return { output: [] }
+    if (!this.isInitialized || !this.embedder) {
+      console.error('Model not initialized')
+      return { output: [] }
+    }
+
+    try {
+      console.log('Running embedding inference on input:', input)
+      const embedding = this.embedder.embed(input)
+      console.log('Embedding generated, length:', embedding.length)
+      // Convert Float32Array to number[] (array<double> in mojo)
+      return { output: Array.from(embedding) }
+    } catch (error) {
+      console.error('Error running embedding:', error)
+      return { output: [] }
+    }
   }
 
   getPendingRemote() {

brave/brave-core

components/local_ai/resources/candle_embedding_gemma_bridge.ts
@@ -7,33 +7,144 @@ import {
   LocalAIService,
   PassageEmbedderFactoryReceiver,
   PassageEmbedderReceiver,
+  ModelFiles,
 } from 'gen/brave/components/local_ai/core/local_ai.mojom.m.js'
+
+// Minimal type for the WASM Gemma3Embedder class
+interface Gemma3EmbedderInstance {
+  embed(input: string): Float32Array
+}
 
 // Initialize connection to the browser-side LocalAIService
 const localAIService = LocalAIService.getRemote()
 
-// Implement the PassageEmbedder Mojo interface
+// Implement the PassageEmbedder Mojo interface (GenerateEmbeddings only).
+// Each instance is created by the factory for a specific consumer.
 class PassageEmbedderImpl {
+  embedder: Gemma3EmbedderInstance
+
+  constructor(embedder: Gemma3EmbedderInstance) {
+    this.embedder = embedder
+  }
+
   // Implementation of PassageEmbedder::GenerateEmbeddings
-  async generateEmbeddings(_text: string): Promise<{ embedding: number[] }> {
-    // TODO(https://github.com/brave/brave-browser/issues/52722):
-    // stub until model loading is wired up
-    return { embedding: [] }
+  async generateEmbeddings(text: string): Promise<{ embedding: number[] }> {
+    try {
+      const embedding = this.embedder.embed(text)
+      // Convert Float32Array to number[] (array<double> in mojo)
+      return { embedding: Array.from(embedding) }
+    } catch (error) {
+      console.error('Error running embedding:', error)
+      return { embedding: [] }
+    }
   }
 }
 
-// Implement the PassageEmbedderFactory Mojo interface (stub).
+// Implement the PassageEmbedderFactory Mojo interface.
+// Manages model lifecycle and creates PassageEmbedder instances.
 class PassageEmbedderFactoryImpl {
   receiver: PassageEmbedderFactoryReceiver
+  embedder: Gemma3EmbedderInstance | null
+  isInitialized: boolean
+  initTime: number
+  // Keep references to prevent GC of bound receivers
   boundEmbedders: PassageEmbedderReceiver[]
 
   constructor() {
     this.receiver = new PassageEmbedderFactoryReceiver(this)
+    this.embedder = null
+    this.isInitialized = false
+    this.initTime = 0
     this.boundEmbedders = []
   }
 
+  // Implementation of PassageEmbedderFactory::Init
+  async init(modelFiles: ModelFiles): Promise<{ success: boolean }> {
+    if (this.isInitialized) {
+      return { success: true }
+    }
+
+    const startTime = performance.now()
+
+    try {
+      // Helper function to extract data from BigBuffer
+      const extractBigBuffer = (
+        buffer: any,
+        name: string,
+      ): Uint8Array | null => {
+        if (buffer.bytes) {
+          return new Uint8Array(buffer.bytes)
+        } else if (buffer.sharedMemory) {
+          const sharedMem = buffer.sharedMemory
+          const mapResult = sharedMem.bufferHandle.mapBuffer(0, sharedMem.size)
+          if (!mapResult.buffer) {
+            console.error(`Failed to map ${name} shared buffer`)
+            return null
+          }
+          return new Uint8Array(mapResult.buffer)
+        } else {
+          console.error(`Invalid ${name} BigBuffer`)
+          return null
+        }
+      }
+
+      const weightsData = extractBigBuffer(modelFiles.weights, 'Weights')
+      const weightsDense1Data = extractBigBuffer(
+        modelFiles.weightsDense1,
+        'WeightsDense1',
+      )
+      const weightsDense2Data = extractBigBuffer(
+        modelFiles.weightsDense2,
+        'WeightsDense2',
+      )
+      const tokenizerData = extractBigBuffer(modelFiles.tokenizer, 'Tokenizer')
+      const configData = extractBigBuffer(modelFiles.config, 'Config')
+
+      if (
+        !weightsData
+        || !weightsDense1Data
+        || !weightsDense2Data
+        || !tokenizerData
+        || !configData
+      ) {
+        console.error('Failed to extract model files')
+        return { success: false }
+      }
+
+      // Dynamically import the WASM Gemma3Embedder class
+      const module = await import(
+        // @ts-ignore - Served by the same WebUI data source
+        'chrome-untrusted://on-device-model-worker/'
+          + 'candle_embedding_gemma.bundle.js'
+      )
+      const { Gemma3Embedder } = module
+
+      this.embedder = new Gemma3Embedder(
+        weightsData,
+        weightsDense1Data,
+        weightsDense2Data,
+        tokenizerData,
+        configData,
+      )
+      this.isInitialized = true
+      this.initTime = performance.now() - startTime
+      console.log(
+        `Model loaded successfully in ` + `${this.initTime.toFixed(2)}ms`,
+      )
+      return { success: true }
+    } catch (error) {
+      console.error('Failed to load model:', error)
+      return { success: false }
+    }
+  }
+
+  // Implementation of PassageEmbedderFactory::Bind
   bind(receiver: any): void {
-    const impl = new PassageEmbedderImpl()
+    if (!this.embedder) {
+      console.error('Cannot create embedder: model not initialized')
+      return
+    }
+    const impl = new PassageEmbedderImpl(this.embedder)
     const bound = new PassageEmbedderReceiver(impl)
     bound.$.bindHandle(receiver.handle)
     this.boundEmbedders.push(bound)