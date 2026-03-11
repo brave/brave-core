a0f5f7ff008f24287786c97cb3c73f260135a9b5
Claudio DeSouza <cdesouza@brave.com>
2026-03-11 18:28:39 +0000

    [cr148] Fixing `ad_tracker` inclusion
    This header has been moved and this has to be corrected on our use of
    it.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/1eb011788b5d7286578552bd807a588de4f5218a
    
    commit 1eb011788b5d7286578552bd807a588de4f5218a
    Author: Josh Karlin <jkarlin@chromium.org>
    Date:   Mon Mar 9 14:44:08 2026 -0700
    
        Move the AdTracker into its own directory
    
        Bug: 435683890
        Change-Id: Ic243ac8c7a114d613777a2b788b631251696c84f
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7639470
        Reviewed-by: Daniel Cheng <dcheng@chromium.org>
        Reviewed-by: Nate Chapin <japhet@chromium.org>
        Commit-Queue: Josh Karlin <jkarlin@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1596619}

--- chromium_src/third_party/blink/renderer/core/loader/image_loader.cc
+++ chromium_src/third_party/blink/renderer/core/loader/image_loader.cc
@@ -6,8 +6,8 @@
 #include "third_party/blink/renderer/core/loader/image_loader.h"
 
 #include "brave/components/brave_page_graph/common/buildflags.h"
+#include "third_party/blink/renderer/core/ad_tracker/ad_tracker.h"
 #include "third_party/blink/renderer/core/dom/dom_node_ids.h"
-#include "third_party/blink/renderer/core/frame/ad_tracker.h"
 #include "third_party/blink/renderer/core/probe/core_probes.h"
 #include "third_party/blink/renderer/platform/loader/fetch/fetch_parameters.h"
 
