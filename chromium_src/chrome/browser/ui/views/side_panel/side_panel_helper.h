39eb3cbabe6955323b4a084fb4131494a0501878
Claudio DeSouza <cdesouza@brave.com>
2026-03-11 18:24:03 +0000

    [cr148] `SidePanelUtil` renamed to `SidePanelHelper`
    This change corrects all uses and override for this class to use the new
    path and refer to the new name. It is important to notice though that
    upstream has introduced a new `SidePanelUtil`, and
    `GetSidePanelContentProxy` was subsequently moved out of
    `SidePanelHelper` and into the new `SidePanelUtil`.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/f1041bd627b995b8c64f3738472a4c16768e8488
    https://chromium.googlesource.com/chromium/src/+/2af163009adf96c9bfd7b7918dd6ed32dc08f1b8
    
    commit f1041bd627b995b8c64f3738472a4c16768e8488
    Author: Mark Schillaci <mschillaci@google.com>
    Date:   Mon Mar 9 17:53:26 2026 -0700
    
        [Side Panel Infra] Make the SidePanelContentProxy platform agnostic
    
        This CL continues the work to prepare the Side Panel to be built on
        Android. We make the SidePanelContentProxy generalized so that we can
        include the SidePanelEntryWaiter on Android without having to add
        many special-case #ifdef's for it. We move the remaining utility method
        out of the views package to be in the platform agnostic package, and
        move the content proxy files and update BUILD files as needed.
    
        Bypass-Check-License: This is a moved file, not a new file or content.
        Bug: 489780669
        Change-Id: Ib69ffd474c62201276916de031d7741661cfccb2
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7644871
        Reviewed-by: Caroline Rising <corising@chromium.org>
        Commit-Queue: Mark Schillaci <mschillaci@google.com>
        Cr-Commit-Position: refs/heads/main@{#1596748}
    
    commit 2af163009adf96c9bfd7b7918dd6ed32dc08f1b8
    Author: Mark Schillaci <mschillaci@google.com>
    Date:   Mon Mar 9 17:52:12 2026 -0700
    
        [Side Panel Intra] Rename SidePanelUtil to SidePanelHelper
    
        This CL is a simple rename of an existing class. We rename the utils to
        be clear that it is specifically for views::View related methods. In
        upcoming CLs we will split the non-views related code out of this file
        so that we can generalize the SidePanelContentProxy class to be
        platform agnostic.
    
        Bypass-Check-License: We are renaming files, not creating new ones.
        Bug: 489780669
        Change-Id: Icaed502e09e06f394fd4ad5f8cb960bb0395a490
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7644629
        Reviewed-by: Caroline Rising <corising@chromium.org>
        Reviewed-by: Eshwar Stalin <estalin@chromium.org>
        Commit-Queue: Mark Schillaci <mschillaci@google.com>
        Cr-Commit-Position: refs/heads/main@{#1596746}

---
+++ chromium_src/chrome/browser/ui/views/side_panel/side_panel_helper.h
@@ -0,0 +1,18 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_SIDE_PANEL_SIDE_PANEL_HELPER_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_SIDE_PANEL_SIDE_PANEL_HELPER_H_
+
+#define PopulateGlobalEntries                                             \
+  PopulateGlobalEntries_ChromiumImpl(Browser* browser,                    \
+                                     SidePanelRegistry* global_registry); \
+  static void PopulateGlobalEntries
+
+#include <chrome/browser/ui/views/side_panel/side_panel_helper.h>  // IWYU pragma: export
+
+#undef PopulateGlobalEntries
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_SIDE_PANEL_SIDE_PANEL_HELPER_H_
