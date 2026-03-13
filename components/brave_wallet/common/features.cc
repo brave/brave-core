a5d3783a611d4c895afc02d78b51df1506cfab3c
Oleg Isupov <oisupov@brave.com>
2026-03-13 16:04:02 +0700

    Review fix

--- components/brave_wallet/common/features.cc
+++ components/brave_wallet/common/features.cc
@@ -37,7 +37,7 @@ BASE_FEATURE(kBraveWalletZCashFeature,
 
 BASE_FEATURE(kBraveWalletPolkadotFeature,
              "BraveWalletPolkadot",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 #if !defined(OFFICIAL_BUILD)
 BASE_FEATURE(kBraveWalletDebugFeature,
