682141c5d02966fc026c5a917d2831c2d96fe0ae
Nuo Xu <nuoxu.nx@gmail.com>
2026-03-13 10:27:28 -0400

    [Wallet] Fix Cardano dApp connection failing on first enable() call on iOS (#34499)

--- components/brave_wallet/browser/cardano/cardano_dapp_utils.cc
+++ components/brave_wallet/browser/cardano/cardano_dapp_utils.cc
@@ -67,4 +67,41 @@ mojom::AccountIdPtr GetCardanoPreferredDappAccount(
   return nullptr;
 }
 
+mojom::AccountIdPtr GetCardanoPreferredDappAccount(
+    KeyringService* keyring_service,
+    const std::optional<std::vector<std::string>>& allowed_accounts) {
+  if (!allowed_accounts || allowed_accounts->empty()) {
+    return nullptr;
+  }
+
+  auto cardano_account_ids =
+      GetCardanoAccountPermissionIdentifiers(keyring_service);
+
+  if (cardano_account_ids.empty()) {
+    return nullptr;
+  }
+
+  auto selected_account = keyring_service->GetSelectedCardanoDappAccount();
+  bool is_selected_account_allowed =
+      selected_account &&
+      std::ranges::contains(
+          *allowed_accounts,
+          GetAccountPermissionIdentifier(selected_account->account_id));
+  if (is_selected_account_allowed) {
+    return selected_account->account_id.Clone();
+  }
+
+  // Since there is no account selection when permissions are granted,
+  // we use first allowed account.
+  // Similar behavior implemented in EthereumProviderImpl.
+  for (const auto& account : keyring_service->GetAllAccountInfos()) {
+    bool is_account_allowed = std::ranges::contains(
+        *allowed_accounts, GetAccountPermissionIdentifier(account->account_id));
+    if (is_account_allowed) {
+      return account->account_id.Clone();
+    }
+  }
+  return nullptr;
+}
+
 }  // namespace brave_wallet
