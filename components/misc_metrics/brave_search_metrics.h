5a3bd738486bc0a25b77115f02edfd884c4002bc
Darnell Andries <dandries@brave.com>
2026-03-16 10:54:04 -0700

    Add Brave Search metrics (uplift to 1.89.x) (#34625)
    * Add metrics for Brave queries per day (#33884)
    
    * Add metrics for Brave queries per day
    
    * Move QPD metrics to separate class
    
    * Add search metrics test
    
    * Refactor search metrics to not use daily storage, report after 24 hours has elapsed
    
    * Add Yahoo/DDG default histogram names
    
    * Remove check deref include
    
    * Move unit test to components
    
    * Clear query counts on history clear
    
    * Add check for registry
    
    * Fix rebase error
    
    * Add metrics for Brave Search entry percentages (#33951)
    
    * Add omnibox search percentage metrics
    
    * Add NTP search percentage metric
    
    * Include private queries in search metrics
    
    * Update metric_names.h
    
    * Fix
    
    * Trigger `ReportDailyMetrics` from `PageMetrics::ReportAllMetrics`
    
    * Address PR feedback
    
    * Fix after Chromium upgrade, misc fixes after rebase
    
    * Add CHECK_GT
    
    * Add missing metric names

---
+++ components/misc_metrics/brave_search_metrics.h
@@ -0,0 +1,63 @@
+/* Copyright (c) 2026 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_MISC_METRICS_BRAVE_SEARCH_METRICS_H_
+#define BRAVE_COMPONENTS_MISC_METRICS_BRAVE_SEARCH_METRICS_H_
+
+#include <string_view>
+
+#include "base/memory/raw_ptr.h"
+#include "base/time/time.h"
+#include "url/gurl.h"
+
+class PrefRegistrySimple;
+class PrefService;
+class TemplateURLService;
+
+namespace misc_metrics {
+
+inline constexpr char kSearchDailyQueriesBraveDefaultHistogramName[] =
+    "Brave.Search.DailyQueries.BraveDefault";
+inline constexpr char kSearchDailyQueriesGoogleDefaultHistogramName[] =
+    "Brave.Search.DailyQueries.GoogleDefault";
+inline constexpr char kSearchDailyQueriesDDGDefaultHistogramName[] =
+    "Brave.Search.DailyQueries.DDGDefault";
+inline constexpr char kSearchDailyQueriesYahooDefaultHistogramName[] =
+    "Brave.Search.DailyQueries.YahooDefault";
+inline constexpr char kSearchDailyQueriesOtherDefaultHistogramName[] =
+    "Brave.Search.DailyQueries.OtherDefault";
+inline constexpr char kSearchOmniboxTypedPercentHistogramName[] =
+    "Brave.Search.OmniboxTypedPercent";
+inline constexpr char kSearchOmniboxSuggestionPercentHistogramName[] =
+    "Brave.Search.OmniboxSuggestionPercent";
+inline constexpr char kSearchNTPSearchPercentHistogramName[] =
+    "Brave.Search.NTPSearchPercent";
+
+class BraveSearchMetrics {
+ public:
+  BraveSearchMetrics(PrefService* local_state,
+                     TemplateURLService* template_url_service);
+  ~BraveSearchMetrics();
+
+  static void RegisterPrefs(PrefRegistrySimple* registry);
+
+  void MaybeRecordBraveQuery(const GURL& previous_url, const GURL& current_url);
+  void MaybeRecordOmniboxQuery(const GURL& destination_url, bool is_suggestion);
+  void MaybeRecordNTPSearch(int64_t engine_prepopulate_id);
+
+  void ClearQueryCounts();
+
+  void ReportAllMetrics();
+
+ private:
+  void IncrementDictCount(std::string_view key);
+
+  raw_ptr<PrefService> local_state_ = nullptr;
+  raw_ptr<TemplateURLService> template_url_service_ = nullptr;
+};
+
+}  // namespace misc_metrics
+
+#endif  // BRAVE_COMPONENTS_MISC_METRICS_BRAVE_SEARCH_METRICS_H_
