616aa09581676799aa9a7075756e056aa2f38b56
Anirudha Bose <anirudha.bose@alumni.cern>
2026-03-18 21:25:45 +0100

    Fix broken tests

--- components/brave_rewards/core/engine/util/environment_config.cc
+++ components/brave_rewards/core/engine/util/environment_config.cc
@@ -154,9 +154,11 @@ GURL EnvironmentConfig::BuildGate3OAuthURL(std::string_view provider) const {
       current_environment() == mojom::Environment::kProduction ? "production"
                                                                : "sandbox";
   const auto& gate3_url = engine().options().gate3_url;
-  CHECK(!gate3_url.empty());
-  return URLValue(base::StrCat(
-      {gate3_url, "/api/oauth/", provider, "/", environment, "/"}));
+  CHECK(!gate3_url.empty() || allow_default_values_for_testing_);
+  return URLValue(gate3_url.empty()
+                      ? std::string()
+                      : base::StrCat({gate3_url, "/api/oauth/", provider, "/",
+                                      environment, "/"}));
 }
 
 GURL EnvironmentConfig::URLValue(std::string value) const {
