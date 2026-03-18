cab53b99d8eacc9ceec13705c442fdfc3cb88968
AlexeyBarabash <alexey@brave.com>
2026-03-18 13:26:33 +0200

    [cr148][Android] New arg contentDelegateSupplier at BottomControlsMediator.ctor
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/5214142978dc8e29bf94564a9949636c987a1d4c
    
    commit 5214142978dc8e29bf94564a9949636c987a1d4c
    Author: Calder Kitagawa <ckitagawa@chromium.org>
    Date:   Mon Mar 16 12:17:37 2026 -0700
    
        [BottomBar] Expose capability to control the color and scroll behavior
    
        Per comments on BottomControlsLayer interface it is already possible to dynamically update the color and scroll behavior, then make a call to request an update.
    
        This CL exposes mechanisms to control the BottomControlsContentDelegate implementation and provides a callback to BottomBarContainerCoordinator to update the render state.
    
        Bug: 492262214
        Change-Id: I31d265c95b5771c121e120d2bdf1c4cd096c8a1e
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7664742
        Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
        Reviewed-by: Madhav Pruthi <madhavpruthi@google.com>
        Reviewed-by: Dan Polanco <polardz@google.com>
        Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
        Commit-Queue: Madhav Pruthi <madhavpruthi@google.com>
        Cr-Commit-Position: refs/heads/main@{#1600048}

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -1503,6 +1503,7 @@ public class BytecodeTest {
                         BrowserStateBrowserControlsVisibilityDelegate.class,
                         FullscreenManager.class,
                         int.class,
+                        OneshotSupplier.class,
                         TabObscuringHandler.class,
                         int.class,
                         int.class,
