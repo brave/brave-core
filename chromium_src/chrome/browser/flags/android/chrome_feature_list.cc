cb49c717c18502f6ea0858c5b9f069391289aee5
Artem Samoilenko <artem@brave.com>
2026-03-24 14:54:07 -0400

    [cr147][Android] Disable CCTToolbarRefactor feature
    Disable feature for now, it will be properly enabled in the context of the follow up issue https://github.com/brave/brave-browser/issues/53917

--- chromium_src/chrome/browser/flags/android/chrome_feature_list.cc
+++ chromium_src/chrome/browser/flags/android/chrome_feature_list.cc
@@ -122,6 +122,7 @@ namespace chrome {
 namespace android {
 
 OVERRIDE_FEATURE_DEFAULT_STATES({{
+    {kCCTToolbarRefactor, base::FEATURE_DISABLED_BY_DEFAULT},
     {kMagicStackAndroid, base::FEATURE_DISABLED_BY_DEFAULT},
 }});
 
--- components/cached_flags/android/java/src/org/chromium/components/cached_flags/BraveCachedFlag.java
+++ components/cached_flags/android/java/src/org/chromium/components/cached_flags/BraveCachedFlag.java
@@ -21,6 +21,7 @@ public class BraveCachedFlag extends CachedFlag {
     private static final String SURFACE_POLISH = "SurfacePolish";
     private static final String AUTOFILL_ENABLE_LOYALTY_CARDS_FILLING =
             "AutofillEnableLoyaltyCardsFilling";
+    private static final String CCT_TOOLBAR_REFACTOR = "CCTToolbarRefactor";
 
     // Set of flags to override.
     private static final HashMap<String, Boolean> sFlags;
@@ -28,6 +29,7 @@ public class BraveCachedFlag extends CachedFlag {
     static {
         sFlags = new HashMap<String, Boolean>();
         sFlags.put(AUTOFILL_ENABLE_LOYALTY_CARDS_FILLING, false);
+        sFlags.put(CCT_TOOLBAR_REFACTOR, false);
         sFlags.put(CLEAR_BROWSING_DATA_ANDROID_SURVEY, false);
         sFlags.put(FEED_CONTAINMENT, false);
         sFlags.put(INCOGNITO_REAUTHENTICATION_FOR_ANDROID, true);
