14179b7221c7c1c1abdee5874f23be689a171f04
Pete Miller <miller.pete@gmail.com>
2026-03-24 21:25:12 -0700

    [AI Chat] Fix new conversation navigation
    - When creating a new conversation, change URL to "/" so that we don't keep the URL of an old conversation and can still visit it.
    - Fix flicker of conversation title and "new conversation" button when switching conversations via setting conversationUuid in state immediately and waiting for conversation history to load before deciding to remove it.

--- components/ai_chat/resources/page/state/active_chat_context.tsx
+++ components/ai_chat/resources/page/state/active_chat_context.tsx
@@ -6,6 +6,7 @@
 import * as React from 'react'
 import { useRoute } from '$web-common/useRoute'
 import * as BindConversation from '../api/bind_conversation'
+import useHasConversationStarted from '../hooks/useHasConversationStarted'
 import { useAIChat } from './ai_chat_context'
 
 export const tabAssociatedChatId = 'tab'
@@ -20,15 +21,26 @@ export interface SelectedChatDetails
   isTabAssociated: boolean
 }
 
+/**
+ * The purpose of ActiveChatContext is to decide what the current conversation
+ * being displayed is and provide the bindings to that conversation.
+ * One implementation of it is the ActiveChatProviderFromUrl which listens
+ * to the Url and decides which conversation based on that.
+ * Tests can provide their own version directly.
+ */
 export const ActiveChatContext = React.createContext<SelectedChatDetails>({
   selectedConversationId: undefined,
   updateSelectedConversationId: () => {},
   createNewConversation: () => {},
   isTabAssociated: false,
+
+  // It's ok to stub undefined for now because we don't render children unless
+  // we have a value. For convenience we don't need to have this as an optional
+  // value.
   api: undefined!,
 })
 
-export const updateSelectedConversation = (selectedId: string | undefined) => {
+const updateSelectedConversation = (selectedId: string | undefined) => {
   window.history.pushState(null, '', `/${selectedId ?? ''}`)
 }
 
@@ -69,7 +81,16 @@ function ActiveChatProvider({
       selectedConversationId,
       updateSelectedConversationId,
       createNewConversation: () => {
-        setConversationAPI(BindConversation.newConversation(aiChat.api))
+        // Special case for the tab-associated conversation mode
+        if (selectedConversationId === tabAssociatedChatId) {
+          // Simply bind a new conversation, this will make it associated
+          // with the current tab, if applicable, via the UIHandler.
+          setConversationAPI(BindConversation.newConversation(aiChat.api))
+          return
+        }
+
+        // Otherwise, navigate to "/"
+        updateSelectedConversation('')
       },
       isTabAssociated: selectedConversationId === tabAssociatedChatId,
     }),
@@ -78,7 +99,7 @@ function ActiveChatProvider({
 
   // Handle child frame requests we switch to new conversation
   aiChat.api.useRequestNewConversation(() => {
-    setConversationAPI(BindConversation.newConversation(aiChat.api))
+    details.createNewConversation()
   }, [aiChat.api])
 
   // Only update conversation if we're on the tab associated conversation
@@ -102,15 +123,27 @@ function ActiveChatProvider({
       return
     }
 
-    // Select a specific conversation
-    setConversationAPI(
-      BindConversation.bindConversation(
-        aiChat.api,
-        selectedConversationId === tabAssociatedChatId
-          ? undefined
-          : selectedConversationId,
-      ),
-    )
+    // getState is async fetched, so it's possible that it won't be populated
+    // when we're checking for whether we need to re-bind to the conversation.
+    // This is ok since this is an optimization to prevent re-binding to the
+    // same conversation. Usually, if selectedConversationId changes, there's
+    // been enough time to fetch getState.
+    const currentlyBoundConversationUuid =
+      conversationAPI?.api?.getState.current().conversationUuid
+    if (currentlyBoundConversationUuid !== selectedConversationId) {
+      // Select a specific conversation
+      setConversationAPI(
+        BindConversation.bindConversation(
+          aiChat.api,
+          selectedConversationId === tabAssociatedChatId
+            ? undefined
+            : selectedConversationId,
+        ),
+      )
+    }
+    // We only want the hook to re-fire when the selectedConversationId changes,
+    // not again when the current conversationUuid does.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [aiChat.api, selectedConversationId])
 
   // Clean up bindings when not used anymore
@@ -152,7 +185,44 @@ function ActiveChatProvider({
 
   return (
     <ActiveChatContext.Provider value={details}>
-      {conversationAPI && children}
+      {conversationAPI && (
+        <>
+          <URLUpdater {...details} />
+          {children}
+        </>
+      )}
     </ActiveChatContext.Provider>
   )
+}
+
+function URLUpdater(props: SelectedChatDetails) {
+  const conversationState = props.api.useGetStateData()
+
+  // Update the location when the conversation has been started and we aren't
+  // on a specific conversation URL yet, or we're not on a "special behavior"
+  // conversation, i.e. the tab-associated conversation.
+  const hasConversationStarted = useHasConversationStarted(
+    conversationState.conversationUuid,
+  )
+
+  React.useEffect(() => {
+    // Only change when we have an actual conversation worth re-visiting
+    if (!hasConversationStarted) return
+
+    // Stay on the tab-associated conversation so that we know to change
+    // conversation automatically when the target tab navigates.
+    if (props.selectedConversationId === tabAssociatedChatId) return
+
+    // Don't need to do anything, the URL already represents the conversation
+    if (conversationState.conversationUuid === props.selectedConversationId) {
+      return
+    }
+
+    props.updateSelectedConversationId(conversationState.conversationUuid)
+    // We don't want to re-run this effect on selectedConversationId change as
+    // that would cause an infinite loop.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [hasConversationStarted, props.updateSelectedConversationId])
+
+  return null
 }
