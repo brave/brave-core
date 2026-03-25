c4d3caafade2142140bdd3c5c9e921beffb978a8
Netzenbot <netzenbot@agentmail.to>
2026-03-25 09:31:27 -0400

    Address review: use #define include pattern instead of full file replacement
    Replace full chromium_src file overrides with the minimal #define +
    #include pattern. Only BuildServiceInstanceForBrowserContext is
    overridden to return nullptr; all other factory methods use the
    upstream implementation.

--- chromium_src/chrome/browser/browsing_topics/browsing_topics_service_factory.cc
+++ chromium_src/chrome/browser/browsing_topics/browsing_topics_service_factory.cc
@@ -3,45 +3,29 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "chrome/browser/browsing_topics/browsing_topics_service_factory.h"
-
-#include "base/no_destructor.h"
-#include "chrome/browser/profiles/profile.h"
-#include "components/keyed_service/core/keyed_service.h"
-
 // Brave disables BrowsingTopics by stubbing out the service factory.
 // This ensures BrowsingTopicsService is never created regardless of
 // feature flag state.
 
+#include "chrome/browser/browsing_topics/browsing_topics_service_factory.h"
+
+// Pre-include headers that also declare BuildServiceInstanceForBrowserContext
+// to prevent the #define below from corrupting their declarations.
+#include "chrome/browser/history/history_service_factory.h"
+#include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
+#include "chrome/browser/privacy_sandbox/privacy_sandbox_settings_factory.h"
+
+#define BuildServiceInstanceForBrowserContext \
+  BuildServiceInstanceForBrowserContext_ChromiumImpl
+#include <chrome/browser/browsing_topics/browsing_topics_service_factory.cc>
+#undef BuildServiceInstanceForBrowserContext
+
 namespace browsing_topics {
-
-// static
-BrowsingTopicsService* BrowsingTopicsServiceFactory::GetForProfile(
-    Profile* profile) {
-  return nullptr;
-}
-
-// static
-BrowsingTopicsServiceFactory* BrowsingTopicsServiceFactory::GetInstance() {
-  static base::NoDestructor<BrowsingTopicsServiceFactory> instance;
-  return instance.get();
-}
-
-BrowsingTopicsServiceFactory::BrowsingTopicsServiceFactory()
-    : ProfileKeyedServiceFactory("BrowsingTopicsService",
-                                 ProfileSelections::BuildNoProfilesSelected()) {
-}
-
-BrowsingTopicsServiceFactory::~BrowsingTopicsServiceFactory() = default;
 
 std::unique_ptr<KeyedService>
 BrowsingTopicsServiceFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
   return nullptr;
-}
-
-bool BrowsingTopicsServiceFactory::ServiceIsCreatedWithBrowserContext() const {
-  return false;
 }
 
 }  // namespace browsing_topics
--- chromium_src/chrome/browser/browsing_topics/browsing_topics_service_factory.h
+++ chromium_src/chrome/browser/browsing_topics/browsing_topics_service_factory.h
@@ -6,39 +6,14 @@
 #ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_BROWSING_TOPICS_BROWSING_TOPICS_SERVICE_FACTORY_H_
 #define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_BROWSING_TOPICS_BROWSING_TOPICS_SERVICE_FACTORY_H_
 
-#include "base/no_destructor.h"
-#include "chrome/browser/profiles/profile_keyed_service_factory.h"
+#include "components/keyed_service/content/browser_context_keyed_service_factory.h"
 
-class Profile;
+#define BuildServiceInstanceForBrowserContext         \
+  BuildServiceInstanceForBrowserContext_ChromiumImpl( \
+      content::BrowserContext* context) const;        \
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext
 
-namespace browsing_topics {
-
-class BrowsingTopicsService;
-
-class BrowsingTopicsServiceFactory : public ProfileKeyedServiceFactory {
- public:
-  static BrowsingTopicsService* GetForProfile(Profile* profile);
-  static BrowsingTopicsServiceFactory* GetInstance();
-
-  BrowsingTopicsServiceFactory(const BrowsingTopicsServiceFactory&) = delete;
-  BrowsingTopicsServiceFactory& operator=(const BrowsingTopicsServiceFactory&) =
-      delete;
-  BrowsingTopicsServiceFactory(BrowsingTopicsServiceFactory&&) = delete;
-  BrowsingTopicsServiceFactory& operator=(BrowsingTopicsServiceFactory&&) =
-      delete;
-
- private:
-  friend class base::NoDestructor<BrowsingTopicsServiceFactory>;
-
-  BrowsingTopicsServiceFactory();
-  ~BrowsingTopicsServiceFactory() override;
-
-  // BrowserContextKeyedServiceFactory:
-  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
-      content::BrowserContext* context) const override;
-  bool ServiceIsCreatedWithBrowserContext() const override;
-};
-
-}  // namespace browsing_topics
+#include <chrome/browser/browsing_topics/browsing_topics_service_factory.h>  // IWYU pragma: export
+#undef BuildServiceInstanceForBrowserContext
 
 #endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_BROWSING_TOPICS_BROWSING_TOPICS_SERVICE_FACTORY_H_
