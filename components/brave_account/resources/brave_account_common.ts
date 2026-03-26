ef78459db2546e5bb57cd216a226dc7fb77ef4a7
Szilard Szaloki <szilardszaloki@gmail.com>
2026-03-26 16:54:15 -0600

    Account: extract `<brave-account-password-input>` component (#35021)

--- components/brave_account/resources/brave_account_common.ts
+++ components/brave_account/resources/brave_account_common.ts
@@ -8,16 +8,3 @@ import { LoginError, RegisterError } from './brave_account.mojom-webui.js'
 export type Error =
   | { flow: 'login'; details: LoginError }
   | { flow: 'register'; details: RegisterError }
-
-export function makeFocusHandler(setFocused: (focused: boolean) => void) {
-  return (detail: { innerEvent: Event }) => {
-    setFocused(detail.innerEvent.type === 'focus')
-  }
-}
-
-export function onToggleVisibility(e: CustomEvent) {
-  ;(e.currentTarget as Element).setAttribute(
-    'type',
-    e.detail.show ? 'text' : 'password',
-  )
-}
