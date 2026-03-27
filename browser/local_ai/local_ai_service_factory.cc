bb0f3380c1146aef6224501f6e521c7a3b649b98
Jocelyn Liu <yrliou@gmail.com>
2026-03-27 12:41:47 -0700

    Extract MakeBWCFactory helper to deduplicate factory code
    Extract BackgroundWebContents factory creation into a
    shared helper so OnDeviceSpeechRecognitionServiceFactory
    can reuse it.

--- browser/local_ai/local_ai_service_factory.cc
+++ browser/local_ai/local_ai_service_factory.cc
@@ -8,17 +8,13 @@
 #include <memory>
 #include <utility>
 
-#include "base/functional/bind.h"
 #include "base/no_destructor.h"
-#include "brave/components/local_ai/content/background_web_contents_impl.h"
-#include "brave/components/local_ai/core/background_web_contents.h"
+#include "brave/browser/local_ai/bwc_factory_util.h"
 #include "brave/components/local_ai/core/local_ai_service.h"
 #include "brave/components/local_ai/core/url_constants.h"
 #include "brave/grit/brave_generated_resources.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_selections.h"
-#include "chrome/browser/task_manager/web_contents_tags.h"
-#include "url/gurl.h"
 
 namespace local_ai {
 
@@ -66,18 +62,8 @@ LocalAIServiceFactory::~LocalAIServiceFactory() = default;
 std::unique_ptr<KeyedService>
 LocalAIServiceFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
-  auto factory = base::BindRepeating(
-      [](content::BrowserContext* browser_context,
-         BackgroundWebContents::Delegate* delegate)
-          -> std::unique_ptr<BackgroundWebContents> {
-        return std::make_unique<BackgroundWebContentsImpl>(
-            browser_context, GURL(kUntrustedLocalAIURL), delegate,
-            base::BindOnce([](content::WebContents* web_contents) {
-              task_manager::WebContentsTags::CreateForToolContents(
-                  web_contents, IDS_LOCAL_AI_TASK_MANAGER_TITLE);
-            }));
-      },
-      context);
+  auto factory = MakeBWCFactory(context, kUntrustedLocalAIURL,
+                                IDS_LOCAL_AI_TASK_MANAGER_TITLE);
 
   return std::make_unique<LocalAIService>(
       std::move(factory), LocalModelsUpdaterState::GetInstance());
