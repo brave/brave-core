7abcd2e28acdf608cb8e296effda061fe348fec5
Claudio DeSouza <cdesouza@brave.com>
2026-03-29 18:41:02 +0100

    [cr148] `kDocumentProviderNoSyncRequirement` deleted
    This deletion was approved by Brave's security team, as it is gated on Google
    Account sign-in at the browser level.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/f8a90a556ba3648b75ac49c14715540692a03c49
    
    commit f8a90a556ba3648b75ac49c14715540692a03c49
    Author: Jonathan Lee <jonathanjlee@google.com>
    Date:   Thu Mar 26 14:49:19 2026 -0700
    
        [code-health] Clean up `omnibox-drive-suggestions-no-sync-requirement`
    
        * This flag expired in M132.
        * The `base::Feature` isn't listed in
          go/stale-base-features-exceptions.
        * The Finch configs associated with the `base::Feature` appear fully
          launched.
    
        Fixed: 335232635
        Change-Id: I86a027a6869a66ec5906e5cfee9c24ea4575d582
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7701378
        Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
        Reviewed-by: Robbie Gibson <rkgibson@google.com>
        Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
        Commit-Queue: Robbie Gibson <rkgibson@google.com>
        Cr-Commit-Position: refs/heads/main@{#1605795}

--- app/feature_defaults_unittest.cc
+++ app/feature_defaults_unittest.cc
@@ -215,7 +215,6 @@ TEST(FeatureDefaultsTest, DisabledFeatures) {
       &ntp_features::kNtpAlphaBackgroundCollections,
       &ntp_features::kNtpBackgroundImageErrorDetection,
       &ntp_features::kNtpChromeCartModule,
-      &omnibox::kDocumentProviderNoSyncRequirement,
       &omnibox::kMlUrlScoring,
 #if BUILDFLAG(IS_ANDROID)
       &omnibox::kOmniboxMobileParityUpdateV2,
