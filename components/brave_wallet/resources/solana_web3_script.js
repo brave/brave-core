83a04bb2631b624a9d113e50fe781d02d186ecda
Christian Mazakas <christian.mazakas@gmail.com>
2026-04-01 13:25:50 -0700

    revert unrelated change

--- components/brave_wallet/resources/solana_web3_script.js
+++ components/brave_wallet/resources/solana_web3_script.js
@@ -3,6 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+
 /* Auto generated from @solana/web3.js 1.98.2 index.iife.min.js */
 var solanaWeb3 = $(function (exports) {
   "use strict";
