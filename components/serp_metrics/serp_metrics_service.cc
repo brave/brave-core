0deca93a6c3a9643911c95878a88df15346158e4
Aleksei Seren <86598290+aseren@users.noreply.github.com>
2026-04-02 08:41:19 -0500

    Make SerpMetricsService reusable on iOS (#35185)
    The PR makes `SerpMetricsService` reusable on iOS
    by injecting time period store factory and removing
    its dependency on browser layer types.

--- components/serp_metrics/serp_metrics_service.cc
+++ components/serp_metrics/serp_metrics_service.cc
@@ -3,26 +3,22 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "brave/browser/serp_metrics/serp_metrics_service.h"
+#include "brave/components/serp_metrics/serp_metrics_service.h"
 
 #include "base/feature_list.h"
-#include "base/files/file_path.h"
-#include "brave/browser/serp_metrics/serp_metrics_time_period_store_factory.h"
 #include "brave/components/serp_metrics/serp_metrics.h"
 #include "brave/components/serp_metrics/serp_metrics_feature.h"
-#include "chrome/browser/profiles/profile_attributes_storage.h"
+#include "brave/components/time_period_storage/time_period_store_factory.h"
 #include "components/prefs/pref_service.h"
 
 namespace serp_metrics {
 
 SerpMetricsService::SerpMetricsService(
     PrefService& local_state,
-    base::FilePath profile_path,
-    ProfileAttributesStorage& profile_attributes_storage) {
+    const TimePeriodStoreFactory& time_period_store_factory) {
   if (base::FeatureList::IsEnabled(serp_metrics::kSerpMetricsFeature)) {
-    serp_metrics_ = std::make_unique<SerpMetrics>(
-        &local_state, SerpMetricsTimePeriodStoreFactory(
-                          profile_path, profile_attributes_storage));
+    serp_metrics_ =
+        std::make_unique<SerpMetrics>(&local_state, time_period_store_factory);
   }
 }
 
