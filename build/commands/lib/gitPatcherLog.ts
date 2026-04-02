0b13e7aa0ede0b3fc60f99acef49d531668077a7
Aleksei Khoroshilov <akhoroshilov@brave.com>
2026-04-02 13:58:48 +0700

    Add type PatchStatus.

--- build/commands/lib/gitPatcherLog.ts
+++ build/commands/lib/gitPatcherLog.ts
@@ -7,13 +7,16 @@ import chalk from 'chalk'
 import { divider as LogDivider } from './logging.js'
 import { patchApplyReasonMessages, getReasonName } from './gitPatcher.js'
 
+type PatchStatus = {
+  path: string
+  patchPath: string
+  reason: string
+  error?: { message: string; stdout?: string; stderr?: string }
+  warning?: string
+}
+
 export function printFailedPatchesInJsonFormat(
-  allPatchStatus: {
-    path: string
-    patchPath: string
-    reason: string
-    error?: { message: string; stdout?: string; stderr?: string }
-  }[],
+  allPatchStatus: PatchStatus[],
   bravePath: string,
 ) {
   const failedPatches = allPatchStatus.filter((patch) => patch.error)
@@ -21,18 +24,16 @@ export function printFailedPatchesInJsonFormat(
     return
   }
 
-  const patchFailuresOutput = failedPatches.map(
-    ({ path, patchPath, reason }) => {
-      return {
-        // Trimming the patch path to be relative to the brave core repo. We skip
-        // the first character to avoid the trailing slash from the absolute
-        // path.
-        patchPath: patchPath.replace(bravePath, '').substring(1),
-        reason: getReasonName(reason),
-        path,
-      }
-    },
-  )
+  const patchFailuresOutput = failedPatches.map((patchStatus: PatchStatus) => {
+    return {
+      // Trimming the patch path to be relative to the brave core repo. We skip
+      // the first character to avoid the trailing slash from the absolute
+      // path.
+      patchPath: patchStatus.patchPath.replace(bravePath, '').substring(1),
+      reason: getReasonName(patchStatus.reason),
+      path: patchStatus.path,
+    }
+  })
 
   console.log(
     chalk.red(`${failedPatches.length} Failed patches json breakdown:`),
@@ -42,12 +43,7 @@ export function printFailedPatchesInJsonFormat(
 }
 
 export function allPatchStatus(
-  allPatchStatus: {
-    path: string
-    patchPath: string
-    reason: string
-    error?: { message: string; stdout?: string; stderr?: string }
-  }[],
+  allPatchStatus: PatchStatus[],
   patchGroupName: string,
 ) {
   if (!allPatchStatus.length) {
@@ -57,8 +53,8 @@ export function allPatchStatus(
       ),
     )
   } else {
-    const successfulPatches: any[] = []
-    const failedPatches: any[] = []
+    const successfulPatches: PatchStatus[] = []
+    const failedPatches: PatchStatus[] = []
     for (const patchStatus of allPatchStatus) {
       if (!patchStatus.error) {
         successfulPatches.push(patchStatus)
@@ -84,23 +80,27 @@ export function allPatchStatus(
   }
 }
 
-function logPatchStatus({ reason, path, patchPath, error, warning }) {
-  const success = !error
+function logPatchStatus(patchStatus: PatchStatus) {
+  const success = !patchStatus.error
   const statusColor = success ? chalk.green : chalk.red
-  console.log(statusColor.bold.underline(path || patchPath))
-  console.log(`  - Patch applied because: ${patchApplyReasonMessages[reason]}`)
-  if (error) {
-    console.log(chalk.red(`  - Error - ${error.message}`))
+  console.log(
+    statusColor.bold.underline(patchStatus.path || patchStatus.patchPath),
+  )
+  console.log(
+    `  - Patch applied because: ${patchApplyReasonMessages[patchStatus.reason]}`,
+  )
+  if (patchStatus.error) {
+    console.log(chalk.red(`  - Error - ${patchStatus.error.message}`))
   }
-  if (warning) {
-    console.warn(chalk.yellow(`  - Warning - ${warning}`))
+  if (patchStatus.warning) {
+    console.warn(chalk.yellow(`  - Warning - ${patchStatus.warning}`))
   }
-  if (error) {
-    if (error.stdout) {
-      console.log(chalk.blue(error.stdout))
+  if (patchStatus.error) {
+    if (patchStatus.error.stdout) {
+      console.log(chalk.blue(patchStatus.error.stdout))
     }
-    if (error.stderr) {
-      console.error(chalk.red(error.stderr))
+    if (patchStatus.error.stderr) {
+      console.error(chalk.red(patchStatus.error.stderr))
     }
   }
   console.log(LogDivider)
