f1a060067ab5034c3d29f6563e6eadece4bfd86c
Claudio DeSouza <cdesouza@brave.com>
2026-04-09 22:56:44 +0100

    [cr149] `kTpcdMetadataGrants` deleted
    The feature was being disabled in Brave, and now it is gone.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/b7f05fafa03fe5ae3a7039fbad7dcf16198beda4
    
    commit b7f05fafa03fe5ae3a7039fbad7dcf16198beda4
    Author: Fiona Macintosh <fmacintosh@google.com>
    Date:   Tue Apr 7 12:00:10 2026 -0700
    
        Delete TPCD metadata manager and associated 3pcd cookies logic
    
        Change-Id: I258499866c8714129644e219a76e4816e24f4783
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7726996
        Reviewed-by: Josh Karlin <jkarlin@chromium.org>
        Reviewed-by: Christian Dullweber <dullweber@chromium.org>
        Reviewed-by: Alex Ilin <alexilin@chromium.org>
        Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
        Reviewed-by: Maks Orlovich <morlovich@chromium.org>
        Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
        Reviewed-by: Joshua Hood <jdh@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1610923}

--- chromium_src/components/privacy_sandbox/privacy_sandbox_settings_unittest.cc
+++ chromium_src/components/privacy_sandbox/privacy_sandbox_settings_unittest.cc
@@ -122,7 +122,7 @@ class PrivacySandboxSettingsTest : public testing::Test {
     cookie_settings_ = new content_settings::CookieSettings(
         host_content_settings_map_.get(), &prefs_, false,
         content_settings::CookieSettings::NoFedCmSharingPermissionsCallback(),
-        /*tpcd_metadata_manager=*/nullptr, "chrome-extension");
+        "chrome-extension");
   }
   ~PrivacySandboxSettingsTest() override {
     host_content_settings_map()->ShutdownOnUIThread();
