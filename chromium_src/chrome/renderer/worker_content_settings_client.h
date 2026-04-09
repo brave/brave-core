bd475e078035ad169885362d6efd2011bd51d804
Claudio DeSouza <cdesouza@brave.com>
2026-04-09 23:13:31 +0100

    [cr149][WIP] Restoring deleted fields to `WorkerContentSettingsClient`
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/8c04824cab07837e20185e363ad86289b1aace87
    
    commit 8c04824cab07837e20185e363ad86289b1aace87
    Author: Antonio Sartori <antoniosartori@chromium.org>
    Date:   Wed Apr 8 04:04:19 2026 -0700
    
        Remove redundant params from ContentSettingsManager mojo method
    
        This CL removes the parameters origin, site_for_cookies and
        top_frame_origin from ContentSettingsManager::AllowStorageAccess,
        since they can be retrieved from the RenderFrameHost on the browser
        side.
    
        In order to do that, we need to move the mojo implementation to the UI
        thread, where the RenderFrameHost lives. Since the methods post on the
        UI thread anyway, there shouldn't be any performance implications.
        Note that https://crrev.com/c/3062003 moved the
        ContentSettingsManagerImpl off the UI thread in the first place for
        optimization reasons.
    
        It would make sense to further remove the frame_token parameter and
        make the browser implementation associated with the DocumentData, but
        that requires a deeper refactoring since this interface should then be
        extended to workers, too.
    
        Bug: 492968151
        Change-Id: I42015a053fd15794164a84de7abd9ae58b4b686c
        Fixed: 492968151
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7726063
        Reviewed-by: Christian Dullweber <dullweber@chromium.org>
        Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
        Reviewed-by: Marc Treib <treib@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1611377}

--- chromium_src/chrome/renderer/worker_content_settings_client.h
+++ chromium_src/chrome/renderer/worker_content_settings_client.h
@@ -6,7 +6,9 @@
 #ifndef BRAVE_CHROMIUM_SRC_CHROME_RENDERER_WORKER_CONTENT_SETTINGS_CLIENT_H_
 #define BRAVE_CHROMIUM_SRC_CHROME_RENDERER_WORKER_CONTENT_SETTINGS_CLIENT_H_
 
+#include "net/cookies/site_for_cookies.h"
 #include "third_party/blink/public/platform/web_content_settings_client.h"
+#include "url/origin.h"
 
 class WorkerContentSettingsClient_BraveImpl;
 
@@ -43,6 +45,12 @@ class WorkerContentSettingsClient_BraveImpl
   void EnsureShieldsSettings();
 
   brave_shields::mojom::ShieldsSettingsPtr shields_settings_;
+
+  // These members are stored are reinstated in our impl since they were removed
+  // in https://crrev.com/c/7726063.
+  url::Origin document_origin_;
+  net::SiteForCookies site_for_cookies_;
+  url::Origin top_frame_origin_;
 };
 
 using WorkerContentSettingsClient = WorkerContentSettingsClient_BraveImpl;
