3b023094c191893a646cb4b925f04c5b0f5d30e9
Claudio DeSouza <cdesouza@brave.com>
2026-04-09 14:10:31 +0100

    [cr148] Fix `ParseJSON` to support `PinEntries` arg
    Adapt our `ParseJSON` override to upstream's API change that split PKP and HSTS
    data into separate structures. Confirmed fix with security folks and verified
    that tests on https://github.com/brave/brave-browser/wiki/TLS-Policy continue to
    pass.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/8119b584658ad61e21550dcf2762e22be182a4b2
    
    commit 8119b584658ad61e21550dcf2762e22be182a4b2
    Author: Matt Mueller <mattm@chromium.org>
    Date:   Thu Apr 2 13:09:05 2026 -0700
    
        Split static compiled-in PKP and HSTS data into separate data structures
    
        This is the first step towards moving PKP out of TransportSecurityState
        and into the cert verifier. It also removes some confusing edge cases
        due to interactions between the PKP and HSTS lists in the old
        implementation.
    
        Bug: 497882860, 498560798
        Change-Id: I2ebc99faea99248d24d2189879c33eb49fb25e3a
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7714518
        Reviewed-by: Carlos IL <carlosil@chromium.org>
        Commit-Queue: Matt Mueller <mattm@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1609455}

--- chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
+++ chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
@@ -1267,18 +1267,19 @@ csOPNIXeJm1YsnJyxUGvKRWjOn+vC9k1SXilnnwhcIs9Pp9e5bckYAnB79VJXN/L
 bool ParseJSON(std::string_view hsts_json,
                std::string_view pins_json,
                TransportSecurityStateEntries* entries,
+               PinEntries* pin_entries,
                Pinsets* pinsets) {
   Pinsets chromium_pinsets;
+  PinEntries chromium_pin_entries;
   TransportSecurityStateEntries chromium_entries;
   if (!ParseJSON_ChromiumImpl(hsts_json, pins_json, &chromium_entries,
-                              &chromium_pinsets)) {
+                              &chromium_pin_entries, &chromium_pinsets)) {
     return false;
   }
 
   for (auto& entry : chromium_entries) {
     // Google has asked us not to include the pins that ship with Chrome,
     // but we do want the preloaded HSTS entries.
-    entry->pinset = "";
     if (!entry->force_https) {
       continue;
     }
@@ -1287,7 +1288,7 @@ bool ParseJSON(std::string_view hsts_json,
   }
 
   return ParseJSON_ChromiumImpl(kBraveHstsJson, kBravePinsJson, entries,
-                                pinsets);
+                                pin_entries, pinsets);
 }
 
 }  // namespace net::transport_security_state
