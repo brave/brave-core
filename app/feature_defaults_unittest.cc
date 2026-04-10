002f121b7825a36d087f1e3ce03d1afffdbbb80c
Claudio DeSouza <cdesouza@brave.com>
2026-04-10 12:33:52 +0100

    [cr149] `kTrackingProtection3pcd` deleted
    This feature has been deleted with the flag, which we were disabling.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/6cecf5c9cf156340d248173d59672e41981467af
    
    commit 6cecf5c9cf156340d248173d59672e41981467af
    Author: Fiona Macintosh <fmacintosh@google.com>
    Date:   Thu Apr 9 01:59:36 2026 -0700
    
        Delete kTrackingProtection3pcd feature
    
        Fixed: b:482352097
        Change-Id: I856bdd18a750581b6bc717652e4a531ee5cda00f
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7741961
        Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
        Reviewed-by: Christian Dullweber <dullweber@chromium.org>
        Commit-Queue: Christian Dullweber <dullweber@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1612047}

--- app/feature_defaults_unittest.cc
+++ app/feature_defaults_unittest.cc
@@ -128,7 +128,6 @@ TEST(FeatureDefaultsTest, DisabledFeatures) {
       &commerce::kShoppingPDPMetrics,
       &commerce::kRetailCoupons,
       &compose::features::kEnableCompose,
-      &content_settings::features::kTrackingProtection3pcd,
       &content_settings::features::kUserBypassUI,
       &contextual_tasks::kContextualTasks,
 #if !BUILDFLAG(IS_ANDROID)
