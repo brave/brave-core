5f0864e780b5252323e0e893cc4e8f6c9b1280d3
Max Karolinskiy <max@brave.com>
2026-04-13 12:50:21 -0400

    [cr149] Reanchor override in HandleClearBrowsingData.
    The symbol we used to anchor the override is now used elsewhere in the
    file.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src.git/+/2dd959bfa7b3d
    
    commit 2dd959bfa7b3d307941fb29e3a479f067262762d
    Author: Steven Luong <stluong@chromium.org>
    Date:   Thu Apr 9 15:08:16 2026 -0700
    
        [CUJService] Add journey tracking clear browser history
    
        Adds the user journey to track when the browser history is cleared via
        the app menu button or through the keyboard accelerator.
    
        Fixed: 495493456

--- chromium_src/chrome/browser/ui/webui/settings/settings_clear_browsing_data_handler.cc
+++ chromium_src/chrome/browser/ui/webui/settings/settings_clear_browsing_data_handler.cc
@@ -4,29 +4,39 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/components/ai_chat/core/common/buildflags/buildflags.h"
-#include "chrome/browser/browsing_data/browsing_data_important_sites_util.h"
+#include "chrome/browser/browsing_data/chrome_browsing_data_remover_constants.h"
 #include "chrome/browser/profiles/profile.h"
+#include "components/browsing_data/core/browsing_data_utils.h"
 
 #if BUILDFLAG(IS_WIN)
 #include "chrome/browser/shell_integration_win.h"
 #include "chrome/browser/win/jumplist.h"
 #include "chrome/browser/win/jumplist_updater.h"
 
-void BraveRemoveJumplist(Profile* profile) {
-  if (!JumpList::Enabled() || !profile) {
+// Put this function into `browsing_data` namespace so that we can anchor our
+// override at browsing_data::RecordDeleteBrowsingDataAction.
+namespace browsing_data {
+void BraveRemoveJumplist(uint64_t remove_mask, Profile* profile) {
+  if (!(remove_mask & chrome_browsing_data_remover::DATA_TYPE_HISTORY) ||
+      !profile || !JumpList::Enabled()) {
     return;
   }
   auto app_id =
       shell_integration::win::GetAppUserModelIdForBrowser(profile->GetPath());
   JumpListUpdater::DeleteJumpList(app_id);
 }
+}  // namespace browsing_data
 
 #define GetBrowsingDataRemover()                                       \
   GetBrowsingDataRemover();                                            \
   if (remove_mask & chrome_browsing_data_remover::DATA_TYPE_HISTORY) { \
     BraveRemoveJumplist(profile_);                                     \
   }
-#endif
+
+#define RecordDeleteBrowsingDataAction        \
+  BraveRemoveJumplist(remove_mask, profile_); \
+  RecordDeleteBrowsingDataAction
+#endif  // #if BUILDFLAG(IS_WIN)
 
 #if BUILDFLAG(ENABLE_AI_CHAT)
 #define HOSTED_APPS_DATA                                                    \
@@ -44,4 +54,5 @@ void BraveRemoveJumplist(Profile* profile) {
 
 #if BUILDFLAG(IS_WIN)
 #undef GetBrowsingDataRemover
+#undef RecordDeleteBrowsingDataAction
 #endif
