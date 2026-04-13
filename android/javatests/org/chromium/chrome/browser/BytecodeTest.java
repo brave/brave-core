72b6a6f0c3eade2fa00fe00e220bcacf7644680e
Artem Samoilenko <artem@brave.com>
2026-04-13 11:35:35 -0400

    [cr149][Android] Changes for null-annotation of RootUiCoordinator
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/be6d55a5fb2031ad63aad40de4bd47244f98ba77
    
    commit be6d55a5fb2031ad63aad40de4bd47244f98ba77
    Author: Henrique Nakashima <hnakashima@chromium.org>
    Date:   Fri Apr 10 08:56:51 2026 -0700
    
        [Android] Null-annotate RootUiCoordinator (part 18)
    
        Fix 1 NullAway error due to:
        - mMerchantTrustSignalsCoordinatorSupplier
    
        Down to 0 errors!
    
        Bug: 485252552
        Change-Id: I9eae5b7906ea55e036290557fd877022af78eebb
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7736236
        Owners-Override: Andrew Grieve <agrieve@chromium.org>
        Reviewed-by: Andrew Grieve <agrieve@chromium.org>
        Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1612901}

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -1512,7 +1512,7 @@ public class BytecodeTest {
                         DataSharingTabManager.class,
                         TabContentManager.class,
                         TabCreatorManager.class,
-                        Supplier.class,
+                        MonotonicObservableSupplier.class,
                         OmniboxActionDelegateImpl.class,
                         MonotonicObservableSupplier.class,
                         boolean.class,
