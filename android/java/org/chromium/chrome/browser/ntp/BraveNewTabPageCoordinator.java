e2af22bd75b20cf86f2e27a5afe5bff217c96068
Artem Samoilenko <artem@brave.com>
2026-04-13 10:58:57 -0400

    [cr149][Android] Changes for removal of unused add|removeMostVisitedTileClickObserver
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/af52507108c6fe0f7cfda495aee2d256194266f2
    
    commit af52507108c6fe0f7cfda495aee2d256194266f2
    Author: Xi Han <hanxi@google.com>
    Date:   Fri Apr 10 06:44:17 2026 -0700
    
        [NtpRefacotr] Remove unused add|removeMostVisitedTileClickObserver.
    
        Bug: 487641528
        Change-Id: Ia160a61ed693ddcd779b6936dfd28be057df2832
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7746025
        Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
        Auto-Submit: Xi Han <hanxi@chromium.org>
        Commit-Queue: Xi Han <hanxi@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1612819}

--- android/java/org/chromium/chrome/browser/ntp/BraveNewTabPageCoordinator.java
+++ android/java/org/chromium/chrome/browser/ntp/BraveNewTabPageCoordinator.java
@@ -15,8 +15,13 @@ import org.chromium.chrome.browser.app.BraveActivity;
 import org.chromium.chrome.browser.feed.FeedSurfaceScrollDelegate;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
 import org.chromium.chrome.browser.magic_stack.ModuleRegistry;
+import org.chromium.chrome.browser.preloading.AndroidPrerenderManager;
 import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.suggestions.mostvisited.MostVisitedSites;
+import org.chromium.chrome.browser.suggestions.tile.Tile;
 import org.chromium.chrome.browser.suggestions.tile.TileGroup;
+import org.chromium.chrome.browser.suggestions.tile.TileSource;
+import org.chromium.chrome.browser.suggestions.tile.tile_edit_dialog.CustomTileEditCoordinator;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.tasks.HomeSurfaceTracker;
@@ -24,11 +29,13 @@ import org.chromium.chrome.browser.ui.messages.snackbar.SnackbarManager;
 import org.chromium.chrome.browser.ui.native_page.TouchEnabledDelegate;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 import org.chromium.components.browser_ui.widget.displaystyle.UiConfig;
+import org.chromium.misc_metrics.mojom.MiscAndroidMetrics;
 import org.chromium.ui.base.ActivityResultTracker;
 import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.modaldialog.ModalDialogManager;
 import org.chromium.url.GURL;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 @NullMarked
@@ -100,7 +107,7 @@ public class BraveNewTabPageCoordinator extends NewTabPageCoordinator {
             ActivityLifecycleDispatcher lifecycleDispatcher,
             Supplier<GURL> composeplateUrlSupplier) {
         super.initialize(
-                tileGroupDelegate,
+                new BraveTileGroupDelegate(tileGroupDelegate, mActivity),
                 searchProviderHasLogo,
                 searchProviderIsGoogle,
                 scrollDelegate,
@@ -110,5 +117,98 @@ public class BraveNewTabPageCoordinator extends NewTabPageCoordinator {
                 composeplateUrlSupplier);
 
         mBraveNewTabPageLayout.initialize(mNewTabPageManager, mActivity, mProfile, mWindowAndroid);
+    }
+
+    private static class BraveTileGroupDelegate implements TileGroup.Delegate {
+        private final TileGroup.Delegate mDelegate;
+        private final Activity mActivity;
+
+        BraveTileGroupDelegate(TileGroup.Delegate delegate, Activity activity) {
+            mDelegate = delegate;
+            mActivity = activity;
+        }
+
+        @Override
+        public void openMostVisitedItem(int windowDisposition, Tile tile) {
+            if (mActivity instanceof BraveActivity braveActivity) {
+                MiscAndroidMetrics metrics = braveActivity.getMiscAndroidMetrics();
+                if (metrics != null) {
+                    metrics.recordTopSiteNavigation(tile.getSource() == TileSource.CUSTOM_LINKS);
+                }
+            }
+            mDelegate.openMostVisitedItem(windowDisposition, tile);
+        }
+
+        @Override
+        public void openMostVisitedItemInGroup(int windowDisposition, Tile tile) {
+            mDelegate.openMostVisitedItemInGroup(windowDisposition, tile);
+        }
+
+        @Override
+        public void removeMostVisitedItem(Tile tile) {
+            mDelegate.removeMostVisitedItem(tile);
+        }
+
+        @Override
+        public void setMostVisitedSitesObserver(
+                MostVisitedSites.Observer observer, int maxResults) {
+            mDelegate.setMostVisitedSitesObserver(observer, maxResults);
+        }
+
+        @Override
+        public void onLoadingComplete(List<Tile> tiles) {
+            mDelegate.onLoadingComplete(tiles);
+        }
+
+        @Override
+        public void initAndroidPrerenderManager(AndroidPrerenderManager androidPrerenderManager) {
+            mDelegate.initAndroidPrerenderManager(androidPrerenderManager);
+        }
+
+        @Override
+        public CustomTileEditCoordinator createCustomTileEditCoordinator(
+                @Nullable Tile originalTile) {
+            return mDelegate.createCustomTileEditCoordinator(originalTile);
+        }
+
+        @Override
+        public void showTileUnpinSnackbar(Runnable undoHandler) {
+            mDelegate.showTileUnpinSnackbar(undoHandler);
+        }
+
+        @Override
+        public double getSuggestionScore(GURL url) {
+            return mDelegate.getSuggestionScore(url);
+        }
+
+        @Override
+        public void destroy() {
+            mDelegate.destroy();
+        }
+
+        @Override
+        public boolean addCustomLink(String name, @Nullable GURL url, @Nullable Integer pos) {
+            return mDelegate.addCustomLink(name, url, pos);
+        }
+
+        @Override
+        public boolean assignCustomLink(GURL keyUrl, String name, @Nullable GURL url) {
+            return mDelegate.assignCustomLink(keyUrl, name, url);
+        }
+
+        @Override
+        public boolean deleteCustomLink(GURL keyUrl) {
+            return mDelegate.deleteCustomLink(keyUrl);
+        }
+
+        @Override
+        public boolean hasCustomLink(GURL keyUrl) {
+            return mDelegate.hasCustomLink(keyUrl);
+        }
+
+        @Override
+        public boolean reorderCustomLink(GURL keyUrl, int newPos) {
+            return mDelegate.reorderCustomLink(keyUrl, newPos);
+        }
     }
 }
