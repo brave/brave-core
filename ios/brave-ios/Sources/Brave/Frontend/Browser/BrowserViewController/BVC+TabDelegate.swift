792b183b92d3292fc44c7448b64b2144fba5a83e
brave-builds <45370463+brave-builds@users.noreply.github.com>
2026-04-16 22:29:22 +0100

    [iOS] Brave hiding logic not applying on headers for redirect requests (uplift to 1.89.x) (#35591)
    Uplift of #35459 (squashed) to release

--- ios/brave-ios/Sources/Brave/Frontend/Browser/BrowserViewController/BVC+TabDelegate.swift
+++ ios/brave-ios/Sources/Brave/Frontend/Browser/BrowserViewController/BVC+TabDelegate.swift
@@ -671,52 +671,11 @@ extension BrowserViewController {
     userAgentForType type: UserAgentType,
     request: URLRequest
   ) -> String? {
-    if !Preferences.Debug.userAgentOverride.value.isEmpty {
-      return Preferences.Debug.userAgentOverride.value
-    }
-    let isBraveAllowedInUA =
-      request.mainDocumentURL.flatMap {
-        tab.braveUserAgentExceptions?.canShowBrave($0)
-      } ?? true
-
-    let mobile: String
-    let desktop: String
-    if isBraveAllowedInUA {
-      let userAgentType = GetDefaultBraveIOSUserAgentType()
-      switch userAgentType {
-      case .masked:
-        mobile = UserAgent.mobileMasked
-        desktop = UserAgent.desktopMasked
-      case .version:
-        mobile = UserAgent.mobile
-        desktop = UserAgent.desktop
-      case .suffix:
-        mobile = UserAgent.safariMobileBraveSuffix
-        desktop = UserAgent.safariDesktopBraveSuffix
-      case .suffixComment:
-        mobile = UserAgent.safariMobileBraveSuffixComment
-        desktop = UserAgent.safariDesktopBraveSuffixComment
-      @unknown default:
-        mobile = UserAgent.safariMobileBraveSuffix
-        desktop = UserAgent.safariDesktopBraveSuffix
-      }
-    } else {
-      mobile = UserAgent.mobileMasked
-      desktop = UserAgent.desktopMasked
-    }
-
-    switch type {
-    case .none, .automatic:
-      let screenWidth = UIScreen.main.bounds.width
-      if traitCollection.horizontalSizeClass == .compact && view.bounds.width < screenWidth / 2 {
-        return mobile
-      }
-      return traitCollection.userInterfaceIdiom == .pad
-        && profileController.defaultHostContentSettings.defaultPageMode == .desktop
-        ? desktop : mobile
-    case .desktop: return desktop
-    case .mobile: return mobile
-    }
+    userAgent(
+      for: request,
+      userAgentForType: type,
+      braveUserAgentExceptions: tab.braveUserAgentExceptions
+    )
   }
 
   public func tab(_ tab: some TabState, defaultUserAgentTypeForURL url: URL) -> UserAgentType {
--- ios/browser/api/features/features.h
+++ ios/browser/api/features/features.h
@@ -79,6 +79,8 @@ OBJC_EXPORT
 @property(class, nonatomic, readonly) Feature* kBraveTranslateEnabled;
 @property(class, nonatomic, readonly) Feature* kBraveAppleTranslateEnabled;
 @property(class, nonatomic, readonly) Feature* kUseBraveUserAgent;
+@property(class, nonatomic, readonly)
+    Feature* kShouldCancelRequestsForUserAgentChange;
 @property(class, nonatomic, readonly) Feature* kUseProfileWebViewConfiguration;
 @property(class, nonatomic, readonly) Feature* kBraveAllowExternalPurchaseLinks;
 @property(class, nonatomic, readonly) Feature* kModernTabTrayEnabled;
