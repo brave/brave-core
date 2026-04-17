e36a4261cf99674112e4380be2f834a60732a3d0
Jay Harris <jay.harris@outlook.co.nz>
2026-04-17 15:02:37 +1200

    Add test

--- components/ai_chat/resources/untrusted_conversation_frame/components/conversation_entries/index.tsx
+++ components/ai_chat/resources/untrusted_conversation_frame/components/conversation_entries/index.tsx
@@ -168,7 +168,10 @@ function ConversationEntries() {
 
     const showEditInput = editInputId === entryNumber
     const showEditIndicator = !showEditInput && !!group[0].edits?.length
-    const turnIds = new Set([firstEntry.uuid, ...firstEntry.edits?.map((edit) => edit.uuid) ?? []])
+    const turnIds = new Set([
+      firstEntry.uuid,
+      ...(firstEntry.edits?.map((edit) => edit.uuid) ?? []),
+    ])
 
     // Can't edit complicated structured content (for now)
     // Not allowed to edit agent conversations until the edit submission
@@ -195,8 +198,8 @@ function ConversationEntries() {
       useConversationEventClipboardCopyHandler(firstEntryEdit)
 
     const tabAttachments =
-      conversationContext.associatedContent?.filter(
-        (c) => turnIds.has(c.conversationTurnUuid),
+      conversationContext.associatedContent?.filter((c) =>
+        turnIds.has(c.conversationTurnUuid),
       ) ?? []
     const hasAttachments =
       !!firstEntryEdit.uploadedFiles?.length || tabAttachments.length > 0
