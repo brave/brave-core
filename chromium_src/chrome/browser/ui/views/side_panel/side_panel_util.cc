2d471e2f50a0cec6d2cd1785eaf6e99d19358a7c
Simon Hong <shong@brave.com>
2026-04-20 18:36:52 +0900

    Hide upstream's side panel header in V2 (#35616)
    Resolves https://github.com/brave/brave-browser/issues/54648
    
    Disabled upstream's panel header by calling `SidePanelEntry::set_should_show_header(false);`.
    
    
    TEST=SidebarBrowserTest.SidebarV2NoUpstreamHeaderTest

--- chromium_src/chrome/browser/ui/views/side_panel/side_panel_util.cc
+++ chromium_src/chrome/browser/ui/views/side_panel/side_panel_util.cc
@@ -45,4 +45,9 @@ void SidePanelUtil::PopulateGlobalEntries(Browser* browser,
         base::NullCallback()));
   }
 #endif
+
+  // Brave has its own side panel header, so hide the built-in entry headers.
+  for (auto& entry : global_registry->entries()) {
+    entry->set_should_show_header(false);
+  }
 }
