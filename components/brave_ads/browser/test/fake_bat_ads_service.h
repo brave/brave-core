6be58fc1e7ab83767d3b654302c5a7391425a9fe
Terry Mancey <terry.mancey@icloud.com>
2026-04-20 12:24:24 -0500

    [ads][CodeHealth] Remove mojom_ prefix from Mojo binding variable names (#35647)
    The mojo:: type qualifier at the declaration site already makes the
    Mojo boundary obvious, so the mojom_ prefix is redundant noise.
    
    No behavioral changes.

--- components/brave_ads/browser/test/fake_bat_ads_service.h
+++ components/brave_ads/browser/test/fake_bat_ads_service.h
@@ -31,23 +31,23 @@ class FakeBatAdsService : public bat_ads::mojom::BatAdsService {
   ~FakeBatAdsService() override;
 
   void BindReceiver(mojo::PendingReceiver<bat_ads::mojom::BatAdsService>
-                        bat_ads_service_mojom_pending_receiver);
+                        bat_ads_service_pending_receiver);
 
   // bat_ads::mojom::BatAdsService:
   void Create(const base::FilePath& /*service_path*/,
               mojo::PendingAssociatedRemote<bat_ads::mojom::BatAdsClient>
-              /*bat_ads_client_mojom_pending_associated_remote*/,
+              /*bat_ads_client_pending_associated_remote*/,
               mojo::PendingAssociatedReceiver<bat_ads::mojom::BatAds>
-                  bat_ads_mojom_pending_associated_receiver,
+                  bat_ads_pending_associated_receiver,
               mojo::PendingReceiver<bat_ads::mojom::BatAdsClientNotifier>
-              /*bat_ads_client_notifier_mojom_pending_receiver*/,
+              /*bat_ads_client_notifier_pending_receiver*/,
               CreateCallback callback) override;
 
  private:
   base::RepeatingClosure shutdown_callback_;
 
   FakeBatAds bat_ads_;
-  mojo::Receiver<bat_ads::mojom::BatAdsService> receiver_{this};
+  mojo::Receiver<bat_ads::mojom::BatAdsService> bat_ads_service_receiver_{this};
 };
 
 }  // namespace brave_ads::test
