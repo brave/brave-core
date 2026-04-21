a2775c38946f9ae34594d36c625f2b8c706a7cfe
Aleksei Khoroshilov <akhoroshilov@brave.com>
2026-04-21 14:43:42 +0700

    Use uuids as default container ids.

--- components/containers/core/browser/default_containers_list_unittest.cc
+++ components/containers/core/browser/default_containers_list_unittest.cc
@@ -22,23 +22,24 @@ class DefaultContainersListTest : public testing::Test {
 TEST_F(DefaultContainersListTest, CreatesDefaultContainersList) {
   std::vector<mojom::ContainerPtr> list = CreateDefaultContainersList();
   ASSERT_EQ(list.size(), 4u);
+  ASSERT_EQ(kDefaultContainerIds.size(), list.size());
 
-  EXPECT_EQ(list[0]->id, "personal");
+  EXPECT_EQ(list[0]->id, "0053d88e-7b26-4bfa-9175-783e1bfcba97");
   EXPECT_EQ(list[0]->icon, mojom::Icon::kPersonal);
   EXPECT_FALSE(list[0]->name.empty());
   EXPECT_NE(list[0]->background_color, SK_ColorTRANSPARENT);
 
-  EXPECT_EQ(list[1]->id, "work");
+  EXPECT_EQ(list[1]->id, "2c2777a1-80b3-4b26-b629-ae1aefd9f272");
   EXPECT_EQ(list[1]->icon, mojom::Icon::kWork);
   EXPECT_FALSE(list[1]->name.empty());
   EXPECT_NE(list[1]->background_color, SK_ColorTRANSPARENT);
 
-  EXPECT_EQ(list[2]->id, "social");
+  EXPECT_EQ(list[2]->id, "cfc3ee90-a163-4bd0-99ff-f0a472dda804");
   EXPECT_EQ(list[2]->icon, mojom::Icon::kSocial);
   EXPECT_FALSE(list[2]->name.empty());
   EXPECT_NE(list[2]->background_color, SK_ColorTRANSPARENT);
 
-  EXPECT_EQ(list[3]->id, "school");
+  EXPECT_EQ(list[3]->id, "f5d49086-1a5f-42b0-83fa-58682e5b8ba5");
   EXPECT_EQ(list[3]->icon, mojom::Icon::kSchool);
   EXPECT_FALSE(list[3]->name.empty());
   EXPECT_NE(list[3]->background_color, SK_ColorTRANSPARENT);
