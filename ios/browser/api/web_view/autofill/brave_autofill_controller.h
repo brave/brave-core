2a8c124fbc534349ef0ec5af44b8b5a7b9a5b1f7
Kyle Hickinson <git@kylehickinson.com>
2026-04-22 14:07:43 -0400

    [iOS] Fix access of autofill controller in BraveWebView
    This updates the chromium_src overrides that enable the usage of `CWVAutofillController`, namely removing logic in the `WebView*` types that use `WebViewBrowserState` rather than a dependency injected into the client. This means creating a few new overrides for `IOSWebViewPaymentsAutofillClient` and supplying a different autofill client bridge/driver for the `CWVAutofillController` (via subclass)

---
+++ ios/browser/api/web_view/autofill/brave_autofill_controller.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2026 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_IOS_BROWSER_API_WEB_VIEW_AUTOFILL_BRAVE_AUTOFILL_CONTROLLER_H_
+#define BRAVE_IOS_BROWSER_API_WEB_VIEW_AUTOFILL_BRAVE_AUTOFILL_CONTROLLER_H_
+
+#include "ios/web_view/public/cwv_autofill_controller.h"
+
+@interface BraveAutofillController : CWVAutofillController
+@end
+
+#endif  // BRAVE_IOS_BROWSER_API_WEB_VIEW_AUTOFILL_BRAVE_AUTOFILL_CONTROLLER_H_
