1c2e909f746390da0e2af1f2a7f81803727c4f35
Claudio DeSouza <cdesouza@brave.com>
2026-04-23 16:53:03 +0100

    [cr149] `//chrome/browser/lifetime` modularised
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/94c8534414d7e52827cc07353185709210aa98e8
    
    commit 94c8534414d7e52827cc07353185709210aa98e8
    Author: Erik Chen <erikchen@chromium.org>
    Date:   Wed Apr 22 11:38:16 2026 -0700
    
        Modularize //chrome/browser/lifetime
    
        Extracts the `lifetime` sources (application_lifetime, browser_shutdown,
        browser_close_manager, restartability_monitor, etc.) out of the
        monolithic `//chrome/browser` target into dedicated
        `//chrome/browser/lifetime:lifetime` (public headers) and
        `//chrome/browser/lifetime:impl` targets. Platform-specific sources
        (android, chromeos, mac, aura) are conditionally included via the new
        BUILD.gn rules. All downstream BUILD.gn files that previously relied on
        the bundled target now declare an explicit dep on
        `//chrome/browser/lifetime`. A circular-dep note is preserved explaining
        why `:impl` still has to live under `//chrome/browser` and
        `//chrome/browser/ui`.
    
        AGENT_GENERATED_CL
    
        Bug: 353332589
        Change-Id: I9235a54772bc0941e313dba3e0f7eec495ab1ae0
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7786609
        Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
        Commit-Queue: Erik Chen <erikchen@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1618994}

--- browser/ui/window_closing_confirm_browsertest.cc
+++ browser/ui/window_closing_confirm_browsertest.cc
@@ -13,7 +13,6 @@
 #include "brave/components/constants/pref_names.h"
 #include "chrome/browser/download/download_manager_utils.h"
 #include "chrome/browser/download/download_prefs.h"
-#include "chrome/browser/lifetime/application_lifetime.h"
 #include "chrome/browser/lifetime/application_lifetime_desktop.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
