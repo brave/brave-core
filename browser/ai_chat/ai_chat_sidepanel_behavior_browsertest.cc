c74b5da1ecd77c1a19fb1340ea7497073ee06a7a
Claudio DeSouza <cdesouza@brave.com>
2026-04-23 16:17:18 +0100

    [cr149] `BrowserList` was deleted
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/499cfaea23ab8386461cbfeec1c4a2dcbd8ca8f6
    
    commit 499cfaea23ab8386461cbfeec1c4a2dcbd8ca8f6
    Author: Thomas Lukaszewicz <tluk@chromium.org>
    Date:   Wed Apr 22 11:19:07 2026 -0700
    
        [bedrock] Remove BrowserList
    
        There are no remaining clients of BrowserList and no behavior changes
        in this CL.
    
        The changes involve removing the files and cleaning up any remaining
        includes / comments.
    
        Bug: 431671320
        Change-Id: Ie61bbd3f8371cd5777d179d08ac4f47b9e29dd01
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7744394
        Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
        Reviewed-by: Qikai Zhong <qikaizhong@microsoft.com>
        Cr-Commit-Position: refs/heads/main@{#1618978}

--- browser/ai_chat/ai_chat_sidepanel_behavior_browsertest.cc
+++ browser/ai_chat/ai_chat_sidepanel_behavior_browsertest.cc
@@ -11,7 +11,6 @@
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
-#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
