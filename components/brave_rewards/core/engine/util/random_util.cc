Web3HQ/unbraved-chromium

[cr148] `base::HexEncode` to use span
The deprecated variant has been deleted/

Chromium changes:
https://chromium.googlesource.com/chromium/src/+/da12fb560afc4519dc2ae1f831714008fda90bac

commit da12fb560afc4519dc2ae1f831714008fda90bac
Author: raorui <ruirao565@gmail.com>
Date:   Fri Mar 27 03:27:18 2026 -0700

    Code Health: Use span in base::HexEncode

    - Migrate HexEncode callers from raw pointer+length to controlled views:
      - strings: std::string_view or base::as_byte_span
      - IOBuffer: io_buffer()->span()
      - struct bytes: reinterpret_cast + base::span<const uint8_t>
    - Clamp adapter MAC length before slicing to avoid OOB when encoding
    - Remove pointer-based HexEncode overload and associated TODO
    - Update tests and fuzzers to new interfaces

    Bug: 40284755
    Change-Id: I3352bb1a795b2f10cf5a8bedb432fe7d4bdf7290
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7607319
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Owners-Override: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1606068}

components/brave_rewards/core/engine/util/random_util.cc
@@ -34,7 +34,7 @@ namespace brave_rewards::internal::util {
 std::string GenerateRandomHexString() {
   uint8_t bytes[kLengthHexString];
   crypto::RandBytes(bytes);
-  return base::HexEncode(bytes, sizeof(bytes));
+  return base::HexEncode(base::byte_span_from_ref(bytes));
 }
 
 std::string GeneratePKCECodeVerifier() {

brave/brave-core

components/brave_rewards/core/engine/util/random_util.cc
@@ -34,7 +34,7 @@ namespace brave_rewards::internal::util {
 std::string GenerateRandomHexString() {
   uint8_t bytes[kLengthHexString];
   crypto::RandBytes(bytes);
-  return base::HexEncode(bytes, sizeof(bytes));
+  return base::HexEncode(base::as_byte_span(bytes));
 }
 
 std::string GeneratePKCECodeVerifier() {