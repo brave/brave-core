db1faf21c802266789c106d4390f6561e4a64d4c
Simone Arpe <simon.arpe@gmail.com>
2026-04-28 13:17:07 +0200

    Unify manual YouTube PiP with Brave PiP flow

--- android/java/org/chromium/chrome/browser/app/BraveActivity.java
+++ android/java/org/chromium/chrome/browser/app/BraveActivity.java
@@ -647,6 +647,7 @@ public abstract class BraveActivity extends ChromeActivity
         if (inPicture) {
             mIsYouTubePictureInPictureExiting = false;
             mIsYouTubePictureInPictureInterruptedByScreenLock = false;
+            maybeAdoptYouTubePictureInPictureSession();
         }
         if (inPicture && mResumeMediaSession) {
             mResumeMediaSession = false;
@@ -676,6 +677,22 @@ public abstract class BraveActivity extends ChromeActivity
             }
             handleYouTubePictureInPictureModeExited();
         }
+    }
+
+    private void maybeAdoptYouTubePictureInPictureSession() {
+        if (mIsYouTubePictureInPictureActive) {
+            return;
+        }
+
+        final WebContents currentWebContents = getCurrentWebContents();
+        if (currentWebContents == null
+                || currentWebContents.isDestroyed()
+                || !BraveYouTubeScriptInjectorNativeHelper.isPictureInPictureAvailable(
+                        currentWebContents)) {
+            return;
+        }
+
+        onYouTubePictureInPictureRequested(currentWebContents);
     }
 
     /**
