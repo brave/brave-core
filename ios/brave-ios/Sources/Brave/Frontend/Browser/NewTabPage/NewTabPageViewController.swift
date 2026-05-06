brave/brave-core

commit cbb092dc2f2759a769607c1bb4cb0653f5deadfb

[iOS] Disable privacy reports & other notifications with Origin (#35937)
This avoids scheduling the privacy reports & default browser notifications when the user has Brave Origin purchased

ios/brave-ios/Sources/Brave/Frontend/Browser/NewTabPage/NewTabPageViewController.swift
@@ -204,9 +204,12 @@ class NewTabPageViewController: UIViewController {
             return
           }
 
+          let isOriginPurchased =
+            BraveOriginServiceFactory.get(profile: tab.profile)?.isPurchased() == true
           let host = UIHostingController(
             rootView: PrivacyReportsManager.prepareView(
-              isPrivateBrowsing: privateBrowsingManager.isPrivateBrowsing
+              isPrivateBrowsing: privateBrowsingManager.isPrivateBrowsing,
+              isOriginPurchased: isOriginPurchased
             )
           )
           host.rootView.onDismiss = { [weak self] in

ios/brave-ios/App/iOS/Delegates/SceneDelegate.swift
@@ -38,8 +38,11 @@ struct ProfileState {
   var migrations: BraveProfileMigrations
   var dau: DAU
   var attributionManager: AttributionManager
+  let profile: any Profile
 
   init(profileController: BraveProfileController) {
+    profile = profileController.profile
+
     // Setup DAU
     dau = DAU(braveCoreStats: profileController.braveStats)
 
@@ -453,7 +456,17 @@ extension SceneDelegate {
       object: nil
     )
 
-    PrivacyReportsManager.scheduleNotification(debugMode: !AppConstants.isOfficialBuild)
+    let originService = BraveOriginServiceFactory.get(profile: profileState.profile)
+    Task {
+      // This is early enough in the app lifetime that we need to explicitly call
+      // `checkPurchaseState` rather than simply read `isPurchased` as it wont yet be cached.
+      let isOriginPurchased = await originService?.checkPurchaseState() ?? false
+      if isOriginPurchased {
+        PrivacyReportsManager.cancelNotification()
+      } else {
+        PrivacyReportsManager.scheduleNotification(debugMode: !AppConstants.isOfficialBuild)
+      }
+    }
     PrivacyReportsManager.consolidateData()
     PrivacyReportsManager.scheduleProcessingBlockedRequests(
       isPrivateBrowsing: browserViewController.privateBrowsingManager.isPrivateBrowsing

ios/brave-ios/Sources/Brave/Frontend/Browser/BrowserViewController/BrowserViewController.swift
@@ -1044,6 +1044,11 @@ public class BrowserViewController: UIViewController {
   public static let defaultBrowserNotificationId = "defaultBrowserNotification"
 
   private func scheduleDefaultBrowserNotification() {
+    if BraveOriginServiceFactory.get(profile: profileController.profile)?.isPurchased() == true {
+      Self.cancelScheduleDefaultBrowserNotification()
+      return
+    }
+
     let center = UNUserNotificationCenter.current()
 
     center.requestAuthorization(options: [.provisional, .alert, .sound, .badge]) { granted, error in
@@ -1096,7 +1101,7 @@ public class BrowserViewController: UIViewController {
     }
   }
 
-  private func cancelScheduleDefaultBrowserNotification() {
+  static func cancelScheduleDefaultBrowserNotification() {
     let center = UNUserNotificationCenter.current()
     center.removePendingNotificationRequests(withIdentifiers: [Self.defaultBrowserNotificationId])
 
@@ -2972,7 +2977,11 @@ extension BrowserViewController: PreferencesObserver {
       PrivacyReportsManager.scheduleProcessingBlockedRequests(
         isPrivateBrowsing: privateBrowsingManager.isPrivateBrowsing
       )
-      PrivacyReportsManager.scheduleNotification(debugMode: !AppConstants.isOfficialBuild)
+      if BraveOriginServiceFactory.get(profile: profileController.profile)?.isPurchased() == true {
+        PrivacyReportsManager.cancelNotification()
+      } else {
+        PrivacyReportsManager.scheduleNotification(debugMode: !AppConstants.isOfficialBuild)
+      }
     case Preferences.PrivacyReports.captureVPNAlerts.key:
       PrivacyReportsManager.scheduleVPNAlertsTask()
     case Preferences.Wallet.defaultEthWallet.key:
@@ -3060,7 +3069,7 @@ extension BrowserViewController {
         // Remove pending notification if default browser is set brave
         // Recognized by external link is open
         if !Preferences.DefaultBrowserIntro.defaultBrowserNotificationIsCanceled.value {
-          cancelScheduleDefaultBrowserNotification()
+          Self.cancelScheduleDefaultBrowserNotification()
         }
       }
     }
@@ -3185,9 +3194,12 @@ extension BrowserViewController {
       return
     }
 
+    let isOriginPurchased =
+      BraveOriginServiceFactory.get(profile: profileController.profile)?.isPurchased() == true
     let host = UIHostingController(
       rootView: PrivacyReportsManager.prepareView(
-        isPrivateBrowsing: privateBrowsingManager.isPrivateBrowsing
+        isPrivateBrowsing: privateBrowsingManager.isPrivateBrowsing,
+        isOriginPurchased: isOriginPurchased
       )
     )
 

ios/brave-ios/Sources/Brave/Frontend/Browser/PrivacyHub/PrivacyReportsManager.swift
@@ -83,9 +83,16 @@ public struct PrivacyReportsManager {
 
   // MARK: - View
   /// Fetches required data to present the privacy reports view and returns the view.
-  static func prepareView(isPrivateBrowsing: Bool) -> PrivacyReportsView {
+  static func prepareView(
+    isPrivateBrowsing: Bool,
+    isOriginPurchased: Bool
+  ) -> PrivacyReportsView {
     let last = BraveVPNAlert.last(3)
-    let view = PrivacyReportsView(lastVPNAlerts: last, isPrivateBrowsing: isPrivateBrowsing)
+    var view = PrivacyReportsView(
+      lastVPNAlerts: last,
+      isPrivateBrowsing: isPrivateBrowsing,
+      isOriginPurchased: isOriginPurchased
+    )
 
     Preferences.PrivacyReports.ntpOnboardingCompleted.value = true
 
@@ -154,7 +161,7 @@ public struct PrivacyReportsManager {
     }
   }
 
-  static func cancelNotification() {
+  public static func cancelNotification() {
     UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [
       notificationID
     ])

ios/brave-ios/Sources/Brave/Frontend/Browser/PrivacyHub/PrivacyReportsView.swift
@@ -14,6 +14,7 @@ struct PrivacyReportsView: View {
   let lastVPNAlerts: [BraveVPNAlert]?
 
   private(set) var isPrivateBrowsing: Bool
+  var isOriginPurchased: Bool
   var onDismiss: (() -> Void)?
   var openPrivacyReportsUrl: (() -> Void)?
 
@@ -82,7 +83,7 @@ struct PrivacyReportsView: View {
       ScrollView(.vertical) {
         VStack(alignment: .leading, spacing: 16) {
 
-          if showNotificationPermissionCallout.value && correctAuthStatus {
+          if showNotificationPermissionCallout.value && correctAuthStatus && !isOriginPurchased {
             NotificationCalloutView()
           }
 
@@ -146,9 +147,11 @@ struct PrivacyReports_Previews: PreviewProvider {
   static var previews: some View {
 
     Group {
-      PrivacyReportsView(lastVPNAlerts: nil, isPrivateBrowsing: false)
+      PrivacyReportsView(lastVPNAlerts: nil, isPrivateBrowsing: false, isOriginPurchased: false)
 
-      PrivacyReportsView(lastVPNAlerts: nil, isPrivateBrowsing: false)
+      PrivacyReportsView(lastVPNAlerts: nil, isPrivateBrowsing: false, isOriginPurchased: true)
+
+      PrivacyReportsView(lastVPNAlerts: nil, isPrivateBrowsing: false, isOriginPurchased: false)
         .preferredColorScheme(.dark)
     }
   }

ios/brave-ios/Sources/Brave/Frontend/Settings/SettingsViewController.swift
@@ -889,6 +889,8 @@ class SettingsViewController: TableViewController {
                 rootView: OriginPaywallView(
                   viewModel: .init(store: .init(skusService: skusService)),
                   didPurchase: { [weak self] in
+                    PrivacyReportsManager.cancelNotification()
+                    BrowserViewController.cancelScheduleDefaultBrowserNotification()
                     self?.navigationController?.pushViewController(
                       originSettingsController(),
                       animated: true

commit 9d6c280e8fefcbca1bf80df0529bfa972caa8ca5

[iOS] Avoid tab retain cycle in NTP section setup
Fixes a regression in #35937 which caused a TabState leak by accessing `tab` inside of the setup closure

ios/brave-ios/Sources/Brave/Frontend/Browser/NewTabPage/NewTabPageViewController.swift
@@ -200,7 +200,8 @@ class NewTabPageViewController: UIViewController {
       StatsSectionProvider(
         isPrivateBrowsing: tab.isPrivate,
         openPrivacyHubPressed: { [weak self] in
-          if self?.privateBrowsingManager.isPrivateBrowsing == true {
+          guard let self, let tab = browserTab else { return }
+          if privateBrowsingManager.isPrivateBrowsing == true {
             return
           }
 
@@ -231,7 +232,7 @@ class NewTabPageViewController: UIViewController {
             )
           }
 
-          self?.present(host, animated: true)
+          present(host, animated: true)
         },
         hidePrivacyHubPressed: { [weak self] in
           self?.hidePrivacyHub()