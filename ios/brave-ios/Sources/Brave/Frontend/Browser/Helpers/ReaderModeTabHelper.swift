brave/brave-core

commit 326cf18de7a84f9df826a9423cc4402d7862962b

[iOS] Implement usage of new Reader Mode JavaScriptFeature (#34545)
This enables the usage of the new `ReaderModeJavaScriptFeature` when the `UseProfileWebViewConfiguration` feature flag is enabled

ios/brave-ios/Sources/Brave/Frontend/Browser/Helpers/ReaderModeTabHelper.swift
@@ -1,0 +1,81 @@
+// Copyright (c) 2026 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import BraveCore
+import Shared
+@_spi(ChromiumWebViewAccess) import Web
+
+extension TabDataValues {
+  private struct ReaderModeTabHelperKey: TabDataKey {
+    static var defaultValue: ReaderModeTabHelper?
+  }
+  var readerMode: ReaderModeTabHelper? {
+    get { self[ReaderModeTabHelperKey.self] }
+    set { self[ReaderModeTabHelperKey.self] = newValue }
+  }
+}
+
+class ReaderModeTabHelper: TabObserver {
+  private let tab: any TabState
+  private(set) var readabilityResult: ReadabilityResult?
+
+  init?(tab: some TabState) {
+    if !tab.isChromiumTab {
+      return nil
+    }
+    self.tab = tab
+    self.tab.addObserver(self)
+  }
+
+  deinit {
+    tab.removeObserver(self)
+  }
+
+  var state: ReaderModeState {
+    if let url = tab.visibleURL.flatMap(InternalURL.init), url.isReaderModePage {
+      return .active
+    }
+    return readabilityResult != nil ? .available : .unavailable
+  }
+
+  /// Checks the web views readability then assigns `readabilityResult`
+  @MainActor
+  func checkReadability() async {
+    guard let url = tab.lastCommittedURL, url.isWebPage(includeDataURIs: false),
+      let webView = BraveWebView.from(tab: tab)
+    else {
+      readabilityResult = nil
+      return
+    }
+    guard let result = await webView.checkReadability() else {
+      // No Reader Mode
+      readabilityResult = nil
+      return
+    }
+    do {
+      let jsonObject = try JSONSerialization.jsonObject(with: Data(result.utf8)) as AnyObject
+      readabilityResult = ReadabilityResult(object: jsonObject)
+    } catch {
+      readabilityResult = nil
+    }
+  }
+
+  @MainActor func setStyle(_ style: ReaderModeStyle) {
+    guard state == .active, let webView = BraveWebView.from(tab: tab) else { return }
+    webView.setReaderModeTheme(
+      style.theme.rawValue,
+      fontType: style.fontType.rawValue,
+      fontSize: style.fontSize.rawValue
+    )
+  }
+
+  func tabDidStartNavigation(_ tab: some TabState) {
+    readabilityResult = nil
+  }
+
+  func tabWillBeDestroyed(_ tab: some TabState) {
+    tab.removeObserver(self)
+  }
+}

ios/brave-ios/Sources/Brave/Frontend/Browser/BrowserViewController/BVC+ReaderMode.swift
@@ -3,6 +3,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+import BraveCore
 import BraveShared
 import Preferences
 import Shared
@@ -53,13 +54,17 @@ extension BrowserViewController: ReaderModeStyleViewControllerDelegate {
     // Change the reader mode style on all tabs that have reader mode active
     for tabIndex in 0..<tabManager.count {
       if let tab = tabManager[tabIndex] {
-        if let readerMode = tab.browserData?.getContentScript(
-          name: ReaderModeScriptHandler.scriptName
-        )
-          as? ReaderModeScriptHandler
-        {
-          if readerMode.state == ReaderModeState.active {
-            readerMode.setStyle(style, in: tab)
+        if FeatureList.kUseProfileWebViewConfiguration.enabled {
+          tab.readerMode?.setStyle(style)
+        } else {
+          if let readerMode = tab.browserData?.getContentScript(
+            name: ReaderModeScriptHandler.scriptName
+          )
+            as? ReaderModeScriptHandler
+          {
+            if readerMode.state == ReaderModeState.active {
+              readerMode.setStyle(style, in: tab)
+            }
           }
         }
       }
@@ -71,15 +76,6 @@ extension BrowserViewController: ReaderModeStyleViewControllerDelegate {
 
 extension BrowserViewController: ReaderModeBarViewDelegate {
   func readerModeSettingsTapped(_ view: UIView) {
-    guard
-      let readerMode = tabManager.selectedTab?.browserData?.getContentScript(
-        name: ReaderModeScriptHandler.scriptName
-      ) as? ReaderModeScriptHandler,
-      readerMode.state == ReaderModeState.active
-    else {
-      return
-    }
-
     var readerModeStyle = defaultReaderModeStyle
     if let encodedString = Preferences.ReaderMode.style.value {
       if let style = ReaderModeStyle(encodedString: encodedString) {
@@ -165,19 +161,30 @@ extension BrowserViewController {
       PlaylistScriptHandler.updatePlaylistTab(tab: tab, item: playlistItem)
       self.updateTranslateURLBar(tab: tab, state: translationState)
     } else {
-      // Store the readability result in the cache and load it. This will later move to the ReadabilityHelper.
-      tab.evaluateJavaScript(
-        functionName: "\(readerModeNamespace).readerize",
-        contentWorld: ReaderModeScriptHandler.scriptSandbox
-      ) { (object, error) -> Void in
-        if let readabilityResult = ReadabilityResult(object: object as AnyObject?) {
-          let playlistItem = tab.playlistItem
-          let translationState = tab.translationState ?? .unavailable
+      if FeatureList.kUseProfileWebViewConfiguration.enabled {
+        if let readabilityResult = tab.readerMode?.readabilityResult {
           Task { @MainActor in
             try? await self.readerModeCache.put(currentURL, readabilityResult)
-            if tab.loadRequest(PrivilegedRequest(url: readerModeURL) as URLRequest) != nil {
-              PlaylistScriptHandler.updatePlaylistTab(tab: tab, item: playlistItem)
-              self.updateTranslateURLBar(tab: tab, state: translationState)
+            tab.loadRequest(PrivilegedRequest(url: readerModeURL) as URLRequest)
+            PlaylistScriptHandler.updatePlaylistTab(tab: tab, item: playlistItem)
+            self.updateTranslateURLBar(tab: tab, state: translationState)
+          }
+        }
+      } else {
+        // Store the readability result in the cache and load it. This will later move to the ReadabilityHelper.
+        tab.evaluateJavaScript(
+          functionName: "\(readerModeNamespace).readerize",
+          contentWorld: ReaderModeScriptHandler.scriptSandbox
+        ) { (object, error) -> Void in
+          if let readabilityResult = ReadabilityResult(object: object as AnyObject?) {
+            let playlistItem = tab.playlistItem
+            let translationState = tab.translationState ?? .unavailable
+            Task { @MainActor in
+              try? await self.readerModeCache.put(currentURL, readabilityResult)
+              if tab.loadRequest(PrivilegedRequest(url: readerModeURL) as URLRequest) != nil {
+                PlaylistScriptHandler.updatePlaylistTab(tab: tab, item: playlistItem)
+                self.updateTranslateURLBar(tab: tab, state: translationState)
+              }
             }
           }
         }

ios/brave-ios/Sources/Brave/Frontend/Browser/BrowserViewController/BVC+TabManagerDelegate.swift
@@ -79,6 +79,10 @@ extension BrowserViewController: TabManagerDelegate {
     // we should add it as a policy decider at initialization.
     tab.addPolicyDecider(braveShieldsHelper)
     tab.logins = .init(tab: tab, passwordAPI: profileController.passwordAPI)
+
+    if FeatureList.kUseProfileWebViewConfiguration.enabled {
+      tab.readerMode = .init(tab: tab)
+    }
   }
 
   func tabManager(
@@ -182,17 +186,22 @@ extension BrowserViewController: TabManagerDelegate {
 
     let shouldShowPlaylistURLBarButton = selected?.visibleURL?.isPlaylistSupportedSiteURL == true
 
-    if let readerMode = selected?.browserData?.getContentScript(
-      name: ReaderModeScriptHandler.scriptName
-    )
-      as? ReaderModeScriptHandler,
-      !shouldShowPlaylistURLBarButton
-    {
-      topToolbar.updateReaderModeState(readerMode.state)
-      if readerMode.state == .active {
-        showReaderModeBar(animated: false)
+    if !shouldShowPlaylistURLBarButton {
+      let readerModeState: ReaderModeState?
+      if FeatureList.kUseProfileWebViewConfiguration.enabled {
+        readerModeState = selected?.readerMode?.state
       } else {
-        hideReaderModeBar(animated: false)
+        readerModeState =
+          (selected?.browserData?.getContentScript(name: ReaderModeScriptHandler.scriptName)
+          as? ReaderModeScriptHandler)?.state
+      }
+      if let readerModeState {
+        topToolbar.updateReaderModeState(readerModeState)
+        if readerModeState == .active {
+          showReaderModeBar(animated: false)
+        } else {
+          hideReaderModeBar(animated: false)
+        }
       }
 
       updatePlaylistURLBar(

ios/brave-ios/Sources/Brave/Frontend/Browser/BrowserViewController/BrowserViewController.swift
@@ -2108,10 +2108,21 @@ public class BrowserViewController: UIViewController {
         // because that event will not always fire due to unreliable page caching. This will either let us know that
         // the currently loaded page can be turned into reading mode or if the page already is in reading mode. We
         // ignore the result because we are being called back asynchronous when the readermode status changes.
-        tab.evaluateJavaScript(
-          functionName: "\(readerModeNamespace).checkReadability",
-          contentWorld: ReaderModeScriptHandler.scriptSandbox
-        )
+        if FeatureList.kUseProfileWebViewConfiguration.enabled {
+          if let readerMode = tab.readerMode {
+            Task {
+              await readerMode.checkReadability()
+              if tabManager.selectedTab === tab {
+                topToolbar.updateReaderModeState(readerMode.state)
+              }
+            }
+          }
+        } else {
+          tab.evaluateJavaScript(
+            functionName: "\(readerModeNamespace).checkReadability",
+            contentWorld: ReaderModeScriptHandler.scriptSandbox
+          )
+        }
 
         // Only add history of a url which is not a localhost url
         if !url.isInternalURL(for: .readermode) {
@@ -2175,17 +2186,22 @@ public class BrowserViewController: UIViewController {
 
   func toggleReaderMode() {
     guard let tab = tabManager.selectedTab else { return }
-    if let readerMode = tab.browserData?.getContentScript(name: ReaderModeScriptHandler.scriptName)
-      as? ReaderModeScriptHandler
-    {
-      switch readerMode.state {
-      case .available:
-        enableReaderMode()
-      case .active:
-        disableReaderMode()
-      case .unavailable:
-        break
-      }
+    let readerModeState: ReaderModeState?
+    if FeatureList.kUseProfileWebViewConfiguration.enabled {
+      readerModeState = tab.readerMode?.state
+    } else {
+      readerModeState =
+        (tab.browserData?.getContentScript(name: ReaderModeScriptHandler.scriptName)
+        as? ReaderModeScriptHandler)?.state
+    }
+    guard let readerModeState else { return }
+    switch readerModeState {
+    case .available:
+      enableReaderMode()
+    case .active:
+      disableReaderMode()
+    case .unavailable:
+      break
     }
   }
 

ios/brave-ios/Sources/Brave/Frontend/Browser/Helpers/TabBrowserData.swift
@@ -210,6 +210,9 @@ class TabBrowserData: NSObject, TabObserver {
   lazy var contentBlocker = ContentBlockerHelper(tab: tab)
 
   var readerModeAvailableOrActive: Bool {
+    if FeatureList.kUseProfileWebViewConfiguration.enabled {
+      return tab?.readerMode?.state != .unavailable
+    }
     if let readerMode = getContentScript(name: ReaderModeScriptHandler.scriptName)
       as? ReaderModeScriptHandler
     {

ios/brave-ios/Sources/Web/Chromium/ChromiumWebViewAccess.swift
@@ -20,6 +20,12 @@ extension BraveWebView {
     guard let tab = tab as? ChromiumTabState else { return nil }
     return tab.webView
   }
+
+  @_disfavoredOverload
+  @_spi(ChromiumWebViewAccess) public static func from(tab: any TabState) -> BraveWebView? {
+    guard let tab = tab as? ChromiumTabState else { return nil }
+    return tab.webView
+  }
 }
 
 extension TabState {

commit c7a2674d80c6a24ce4f286ae6361766c1a5de346

[iOS] Fix TabState storage on reader mode tab helper
Fixes a regression in #34545 which caused `TabState` to leak due to storing the `TabState` incorrectly in `ReaderModeTabHelper`

ios/brave-ios/Sources/Brave/Frontend/Browser/Helpers/ReaderModeTabHelper.swift
@@ -18,7 +18,7 @@ extension TabDataValues {
 }
 
 class ReaderModeTabHelper: TabObserver {
-  private let tab: any TabState
+  private weak var tab: (any TabState)?
   private(set) var readabilityResult: ReadabilityResult?
 
   init?(tab: some TabState) {
@@ -26,15 +26,15 @@ class ReaderModeTabHelper: TabObserver {
       return nil
     }
     self.tab = tab
-    self.tab.addObserver(self)
+    tab.addObserver(self)
   }
 
   deinit {
-    tab.removeObserver(self)
+    tab?.removeObserver(self)
   }
 
   var state: ReaderModeState {
-    if let url = tab.visibleURL.flatMap(InternalURL.init), url.isReaderModePage {
+    if let url = tab?.visibleURL.flatMap(InternalURL.init), url.isReaderModePage {
       return .active
     }
     return readabilityResult != nil ? .available : .unavailable
@@ -43,7 +43,7 @@ class ReaderModeTabHelper: TabObserver {
   /// Checks the web views readability then assigns `readabilityResult`
   @MainActor
   func checkReadability() async {
-    guard let url = tab.lastCommittedURL, url.isWebPage(includeDataURIs: false),
+    guard let tab, let url = tab.lastCommittedURL, url.isWebPage(includeDataURIs: false),
       let webView = BraveWebView.from(tab: tab)
     else {
       readabilityResult = nil
@@ -63,7 +63,7 @@ class ReaderModeTabHelper: TabObserver {
   }
 
   @MainActor func setStyle(_ style: ReaderModeStyle) {
-    guard state == .active, let webView = BraveWebView.from(tab: tab) else { return }
+    guard let tab, state == .active, let webView = BraveWebView.from(tab: tab) else { return }
     webView.setReaderModeTheme(
       style.theme.rawValue,
       fontType: style.fontType.rawValue,