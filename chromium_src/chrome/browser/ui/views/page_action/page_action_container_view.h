962b6d68c167d19d004f6b09604e6724d7f0c076
Sangwoo Ko <sangwoo108@gmail.com>
2026-05-12 08:15:05 +0900

    Remove #define macros for PageActionView
    In order to make our patching strategy more robust, we need to remove the
    `#define` macros for PageActionView.

---
+++ chromium_src/chrome/browser/ui/views/page_action/page_action_container_view.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2026 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_PAGE_ACTION_PAGE_ACTION_CONTAINER_VIEW_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_PAGE_ACTION_PAGE_ACTION_CONTAINER_VIEW_H_
+
+namespace page_actions::chromium_impl {
+class PageActionView;
+}  // namespace page_actions::chromium_impl
+
+#include <chrome/browser/ui/views/page_action/page_action_container_view.h>  // IWYU pragma: export
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_PAGE_ACTION_PAGE_ACTION_CONTAINER_VIEW_H_
