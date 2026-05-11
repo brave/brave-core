169b4c993f465d18d8cc58fcb7ccf162d936cf08
Simon Hong <shong@brave.com>
2026-05-11 09:29:41 +0900

    added comment

--- chromium_src/chrome/test/base/interactive_ui_tests_main.cc
+++ chromium_src/chrome/test/base/interactive_ui_tests_main.cc
@@ -5,6 +5,9 @@
 
 #include "brave/test/base/brave_test_launcher_delegate.h"
 
+// Apply Brave-specific behavior change in the test launcher delegate.
+// * Suppress first run dialog (Mac/Linux).
+// * Suppress browser window closing dialog.
 #define ChromeTestLauncherDelegate BraveTestLauncherDelegate
 
 #include <chrome/test/base/interactive_ui_tests_main.cc>
