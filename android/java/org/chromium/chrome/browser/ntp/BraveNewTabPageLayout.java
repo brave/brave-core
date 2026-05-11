d5a648dac36c43d3c2df8905f05dfa8ceda1ed10
Aleksei Seren <aseren@brave.com>
2026-05-10 21:57:43 -0500

    Add reset on NTT load failure + logging

--- android/java/org/chromium/chrome/browser/ntp/BraveNewTabPageLayout.java
+++ android/java/org/chromium/chrome/browser/ntp/BraveNewTabPageLayout.java
@@ -1101,7 +1101,11 @@ public class BraveNewTabPageLayout extends NewTabPageLayout
     private void setupSponsoredBackgroundContent(Wallpaper wallpaper) {
         if (mSponsoredRichMediaWebView == null) {
             mSponsoredRichMediaWebView =
-                    new SponsoredRichMediaWebView(mActivity, mWindowAndroid, mProfile);
+                    new SponsoredRichMediaWebView(
+                            mActivity,
+                            mWindowAndroid,
+                            mProfile,
+                            this::maybeResetSponsoredRichMediaBackground);
 
             mBackgroundSponsoredRichMediaView = findViewById(R.id.bg_sponsored_rich_media_view);
             mBackgroundSponsoredRichMediaView.setVisibility(View.VISIBLE);
@@ -1119,6 +1123,7 @@ public class BraveNewTabPageLayout extends NewTabPageLayout
 
         mBackgroundSponsoredRichMediaView.setVisibility(View.GONE);
         mBackgroundSponsoredRichMediaView.removeAllViews();
+        mSponsoredRichMediaWebView.destroy();
         mSponsoredRichMediaWebView = null;
     }
 
