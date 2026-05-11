51744a571a44581b093628ba19510f0d0ec95a1c
Agustín Ruiz <aguscr182@gmail.com>
2026-05-11 16:08:37 -0300

    patch

--- chromium_src/chrome/browser/ui/webui/settings/settings_manage_profile_handler.cc
+++ chromium_src/chrome/browser/ui/webui/settings/settings_manage_profile_handler.cc
@@ -8,8 +8,8 @@
 #include "chrome/browser/profiles/profile_attributes_entry.h"
 #include "chrome/browser/profiles/profile_manager.h"
 
-// CHROMIUM_SRC_NOLINT: expanded in #included
-// settings_manage_profile_handler.cc.
+// Hook invoked from patched upstream
+// `chrome/browser/ui/webui/settings/settings_manage_profile_handler.cc`.
 #define BRAVE_HANDLE_SET_PROFILE_ICON_TO_GAIA_AVATAR_END(profile) \
   do {                                                            \
     ProfileAttributesEntry* brave_entry =                         \
