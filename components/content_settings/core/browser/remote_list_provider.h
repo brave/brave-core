d1b3cf57ee695b3aa408f03d6a57a3238943857f
Claudio DeSouza <cdesouza@brave.com>
2026-05-12 14:35:22 +0100

    [cr150] `SetWebsiteSetting` passing `Value` by ref
    These interfaces were changed, and it cascaded into our code in quite a
    few places. In one particular case a `Clone()` had to be introduced, as
    the upstream code still takes by value. Trying to fix this in upstream
    has made it clear that things will remain as they are for now.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/41de479be5003693b62fcdf169eccf0af554293a
    
    commit 41de479be5003693b62fcdf169eccf0af554293a
    Author: Antonio Sartori <antoniosartori@chromium.org>
    Date:   Mon May 11 01:30:14 2026 -0700
    
        Convert ContentSettingsProvider::SetWebsiteSetting to const base::Value&
    
        This CL converts ContentSettingsProvider::SetWebsiteSetting to take a
        `const base::Value&` instead of a `base::Value&&`.
    
        This seems to better adhere to the style guide
        (https://google.github.io/styleguide/cppguide.html#Rvalue_references),
        which does not allow using rvalue references excepts in a small number
        of cases, none of which seems to apply in this case.
    
        Note that the C++ linter used to trigger a (non-real) use-after-move
        warning on CLs passing a value by std::move() and then reusing it
        since it was not being consumed by the function. This CL solves that.
    
        Possible alternatives could have been to either pass a `base::Value`
        or a `base::Value&`. The first alternative has been discarded because
        it would need the callers to always pass by value even if in most
        cases SetWebsiteSetting() does not consume the argument. The second
        alternative seems complicated and error-prone (the caller would have
        to remember to check the returned boolean in order to know whether the
        base::Value was consumed or not, which seems like a very likely source
        of bugs), and it would not have supported callers passing a literal
        (Rvalue reference).
    
        Taking a `const base::Value&` seems like the best option, even if it
        sometimes results in the function needing to Clone() the value for
        processing it internally.
    
        Change-Id: I43215b9fc1292470d9be2c3fe1e1241e4b49a98e
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7832363
        Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
        Reviewed-by: James Lee <ljjlee@google.com>
        Reviewed-by: John Lee <johntlee@chromium.org>
        Reviewed-by: Reilly Grant <reillyg@chromium.org>
        Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
        Reviewed-by: Christian Dullweber <dullweber@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1628420}

--- components/content_settings/core/browser/remote_list_provider.h
+++ components/content_settings/core/browser/remote_list_provider.h
@@ -35,7 +35,7 @@ class RemoteListProvider : public ObservableProvider {
   bool SetWebsiteSetting(const ContentSettingsPattern& primary_pattern,
                          const ContentSettingsPattern& secondary_pattern,
                          ContentSettingsType content_type,
-                         base::Value&& value,
+                         const base::Value& value,
                          const ContentSettingConstraints& constraints) override;
 
   void ClearAllContentSettingsRules(ContentSettingsType content_type) override;
