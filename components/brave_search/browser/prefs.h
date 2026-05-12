c8eb915c3447ed98445e6449252fb382e7073cb8
Darnell Andries <dandries@brave.com>
2026-05-11 22:08:00 -0700

    Add feature + params for backup results service

--- components/brave_search/browser/prefs.h
+++ components/brave_search/browser/prefs.h
@@ -22,6 +22,10 @@ inline constexpr char kBackupResultsLastQueryTime[] =
     "brave.search.backup_results.last_query_time";
 inline constexpr char kBackupResultsFailuresStorage[] =
     "brave.search.backup_results.failures_storage";
+inline constexpr char kBackupResultsDailyRequestCount[] =
+    "brave.search.backup_results.daily_request_count";
+inline constexpr char kBackupResultsDailyRequestWindowStart[] =
+    "brave.search.backup_results.daily_request_window_start";
 
 }  // namespace prefs
 }  // namespace brave_search
