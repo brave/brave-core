a79fe404d8a0ce1a54fef3686b4fc830ba19e62e
AlexeyBarabash <alexey@brave.com>
2026-05-14 00:31:46 +0300

    [cr150][Android] PasswordStoreBuiltInBackend needs AffiliatedMatchHelper
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/4fdbebe866ca6a3377b54ef8d832f3300da871bf
    
    commit 4fdbebe866ca6a3377b54ef8d832f3300da871bf
    Author: Ioana Treib <ioanap@chromium.org>
    Date:   Wed May 13 02:16:49 2026 -0700
    
        [Passwords] Remove AffiliatedMatchHelper from the backend init method
    
        It is only used by the built-in backend so it shouldn't stay in the
        common interface anymore.
    
        The FakePasswordStoreBackend shouldn't allow tests on Android to set the
        AffiliatedMatchHelper so that it doesn't diverge from production code.
        That will be done in a follow-up CL.
    
        Bug: 504896739
        Change-Id: Icbd6dfa5a2f6c701228e081cbad6d8e14cd27bac
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7796898
        Reviewed-by: Tommy Martino <tmartino@chromium.org>
        Reviewed-by: Christian Dullweber <dullweber@chromium.org>
        Commit-Queue: Ioana Treib <ioanap@chromium.org>
        Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
        Reviewed-by: Andrii Natiahlyi <natiahlyi@google.com>
        Reviewed-by: Ryan Sultanem <rsult@google.com>
        Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1629846}

---
+++ chromium_src/chrome/browser/password_manager/factories/account_password_store_factory.cc
@@ -0,0 +1,17 @@
+/* Copyright (c) 2026 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "build/build_config.h"
+
+#if BUILDFLAG(IS_ANDROID)
+#define BRAVE_ACCOUNT_PASSWORD_STORE_FACTORY_BUILD_PASSWORD_STORE \
+  affiliation_service = AffiliationServiceFactory::GetForProfile(profile);
+#else
+#define BRAVE_ACCOUNT_PASSWORD_STORE_FACTORY_BUILD_PASSWORD_STORE
+#endif
+
+#include <chrome/browser/password_manager/factories/account_password_store_factory.cc>
+
+#undef BRAVE_ACCOUNT_PASSWORD_STORE_FACTORY_BUILD_PASSWORD_STORE
