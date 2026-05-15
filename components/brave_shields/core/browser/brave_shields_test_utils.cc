08c87ff46aeaf1749f40f620a8589712550b3b05
Aleksei Khoroshilov <akhoroshilov@brave.com>
2026-05-15 20:09:11 +0700

    Fix brave shields test utils location.

--- components/brave_shields/core/browser/brave_shields_test_utils.cc
+++ components/brave_shields/core/browser/brave_shields_test_utils.cc
@@ -3,11 +3,11 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#include "brave/components/brave_shields/content/browser/brave_shields_test_utils.h"
+#include "brave/components/brave_shields/core/browser/brave_shields_test_utils.h"
 
 namespace brave_shields {
 
-// Declared in brave_shields_util.cc.
+// Declared in brave_shields_utils.cc.
 extern uint32_t g_stable_farbling_tokens_seed;
 
 ScopedStableFarblingTokensForTesting::ScopedStableFarblingTokensForTesting(
