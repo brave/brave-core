51d14c7eec2871d21cf800a2d0030a392944eb55
Simon Hong <shong@brave.com>
2026-05-20 21:29:48 +0900

    [Cleanup] Remove deprecated BrowserCaptionButtonContainerWin override
    Also generate BraveBrowserFrameViewWin metadata.

--- chromium_src/chrome/browser/ui/views/frame/browser_caption_button_container_win.h
+++ chromium_src/chrome/browser/ui/views/frame/browser_caption_button_container_win.h
@@ -6,9 +6,6 @@
 #ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_FRAME_BROWSER_CAPTION_BUTTON_CONTAINER_WIN_H_
 #define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_FRAME_BROWSER_CAPTION_BUTTON_CONTAINER_WIN_H_
 
-#include "ui/base/metadata/metadata_header_macros.h"
-
-#define BrowserCaptionButtonContainer BrowserCaptionButtonContainer_ChromiumImpl
 #define OnWindowControlsOverlayEnabledChanged     \
   OnWindowControlsOverlayEnabledChanged_Unused(); \
   friend class BraveBrowserFrameViewWin;          \
@@ -17,18 +14,5 @@
 #include <chrome/browser/ui/views/frame/browser_caption_button_container_win.h>  // IWYU pragma: export
 
 #undef OnWindowControlsOverlayEnabledChanged
-#undef BrowserCaptionButtonContainer
-
-class BrowserCaptionButtonContainer
-    : public BrowserCaptionButtonContainer_ChromiumImpl {
-  METADATA_HEADER(BrowserCaptionButtonContainer,
-                  BrowserCaptionButtonContainer_ChromiumImpl)
- public:
-  explicit BrowserCaptionButtonContainer(BrowserFrameViewWin* frame_view);
-  ~BrowserCaptionButtonContainer() override;
-
- private:
-  const raw_ptr<BrowserFrameViewWin> frame_view_;
-};
 
 #endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_FRAME_BROWSER_CAPTION_BUTTON_CONTAINER_WIN_H_
