f44b63a585abd0c9063372def9a90e246eb221be
Simone Arpe <simon.arpe@gmail.com>
2026-05-20 14:13:17 +0200

    Fix widget promo panel layout (uplift to 1.90.x) (#36568)
    Fix widget promo panel layout

--- android/java/org/chromium/chrome/browser/app/BraveActivity.java
+++ android/java/org/chromium/chrome/browser/app/BraveActivity.java
@@ -2712,7 +2712,10 @@ public abstract class BraveActivity extends ChromeActivity
     private void showWidgetPromoPanel() {
         if (mSearchWidgetPromoPanel != null) {
             final View rootView = requireViewById(android.R.id.content);
-            mSearchWidgetPromoPanel.showIfNeeded(rootView, getBottomOffsetForWidgetPromo(), this);
+            rootView.post(
+                    () ->
+                            mSearchWidgetPromoPanel.showIfNeeded(
+                                    rootView, getBottomOffsetForWidgetPromo(), BraveActivity.this));
         }
     }
 
