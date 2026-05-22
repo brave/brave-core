d35d114c07602d259e68af8f778374aed34e937c
boocmp <boocmp@yandex.ru>
2026-05-22 11:56:10 +0700

    Fixed Manage button's kind.

--- components/email_aliases/resources/content/email_aliases_modal.tsx
+++ components/email_aliases/resources/content/email_aliases_modal.tsx
@@ -400,7 +400,7 @@ export const EmailAliasModal = ({
                 type: EmailAliasModalResultType.ShouldManageAliases,
               })
             }}
-            kind={limitReached ? 'filled' : 'plain'}
+            kind={limitReached ? 'filled' : 'plain-faint'}
           >
             {getLocale(S.SETTINGS_EMAIL_ALIASES_MANAGE_BUTTON)}
           </ManageButton>
