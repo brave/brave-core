bcaae61065aa44c6359603a64debf9bad85de796
Oliver <oddin@brave.com>
2026-05-28 20:17:07 +0200

    [android]: Implement new Browser lock UI.
    This commit implements the new UI components for Brave's browser locking
    capabilities. Both browser-wide and private-tab locking is moved into
    its own dialog.
    
    Additionally, the lock screens themselves have been updated with our new
    UX which is achieved through a resource override. The old Chromium patch
    has been deleted since it's no longer needed.

--- android/javatests/org/chromium/chrome/browser/privacy/settings/BravePrivacySettingsTest.java
+++ android/javatests/org/chromium/chrome/browser/privacy/settings/BravePrivacySettingsTest.java
@@ -44,7 +44,6 @@ public class BravePrivacySettingsTest {
     private static final String PREF_HTTPS_FIRST_MODE = "https_first_mode";
     private static final String PREF_HTTPS_UPGRADE = "https_upgrade";
     private static final String PREF_FORGET_FIRST_PARTY_STORAGE = "forget_first_party_storage";
-    private static final String PREF_INCOGNITO_LOCK = "incognito_lock";
     private static final String PREF_SPONSORED_ADS_ENABLED = "sponsored_ads_enabled";
     private static final String PREF_SPONSORED_ADS_LEARN_MORE = "sponsored_ads_learn_more";
     private static final String PREF_SURVEY_PANELIST = "survey_panelist";
@@ -57,7 +56,7 @@ public class BravePrivacySettingsTest {
     private static final String PREF_ALLOW_ELEMENTS_BLOCKING_ON_PRIVATE_TABS =
             "allow_elements_blocking_on_private_tabs";
 
-    private static final int BRAVE_PRIVACY_SETTINGS_NUMBER_OF_ITEMS = 29;
+    private static final int BRAVE_PRIVACY_SETTINGS_NUMBER_OF_ITEMS = 28;
 
     private int mItemsLeft;
 
@@ -88,7 +87,6 @@ public class BravePrivacySettingsTest {
         checkPreferenceExists(PREF_FORGET_FIRST_PARTY_STORAGE);
         checkPreferenceExists(PREF_SAFE_BROWSING);
         checkPreferenceExists(PREF_SECURE_DNS);
-        checkPreferenceExists(PREF_INCOGNITO_LOCK);
         checkPreferenceExists(PREF_SPONSORED_ADS_ENABLED);
         checkPreferenceExists(PREF_SPONSORED_ADS_LEARN_MORE);
         checkPreferenceExists(PREF_SURVEY_PANELIST);
--- android/javatests/org/chromium/chrome/browser/settings/BraveMainSettingsFragmentTest.java
+++ android/javatests/org/chromium/chrome/browser/settings/BraveMainSettingsFragmentTest.java
@@ -98,6 +98,7 @@ public class BraveMainSettingsFragmentTest {
         "brave_stats",
         "content_settings",
         "brave_downloads",
+        "brave_browser_lock",
         "closing_all_tabs_closes_brave",
         "default_browser",
         "display_section",
@@ -260,8 +261,8 @@ public class BraveMainSettingsFragmentTest {
         // settings screen in that case.
         int expectedCount =
                 ChromeFeatureList.isEnabled(BraveFeatureList.BRAVE_ANDROID_TAB_GROUPS_SETTINGS)
-                        ? 33
-                        : 34;
+                        ? 34
+                        : 35;
 
         assertEquals(
                 "Number of preferences has changed, please check and update preferenceCount"
