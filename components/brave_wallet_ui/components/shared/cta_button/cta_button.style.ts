ed3ad8b0cd8a2a716fc8b01e341c57058966f9a7
Douglas Daniel <douglashdaniel@gmail.com>
2026-06-01 12:29:37 -0500

    [Wallet]: Update Bug Icon and CTA Button

--- components/brave_wallet_ui/components/shared/cta_button/cta_button.style.ts
+++ components/brave_wallet_ui/components/shared/cta_button/cta_button.style.ts
@@ -4,42 +4,13 @@
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
 import styled from 'styled-components'
-import * as leo from '@brave/leo/tokens/css/variables'
-
-// Assets
-import IdeaIcon from '../../../assets/svg-icons/idea.svg'
 
 // Shared Styles
-import { Text, WalletButton } from '../style'
+import { Row } from '../style'
 
-export const Button = styled(WalletButton)`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  padding: 8px 14px;
+export const ButtonWrapper = styled(Row)`
   position: fixed;
   bottom: 32px;
   right: 32px;
-  background-color: ${leo.color.blurple[50]};
-  border: none;
-  outline: none;
-  border-radius: 40px;
-  cursor: pointer;
   z-index: 10;
-`
-
-export const ButtonText = styled(Text)`
-  color: ${leo.color.white};
-`
-
-export const IdeaButtonIcon = styled.span`
-  width: 24px;
-  height: 24px;
-  background-color: ${leo.color.white};
-  -webkit-mask-image: url(${IdeaIcon});
-  mask-image: url(${IdeaIcon});
-  mask-size: cover;
-  margin-right: 8px;
-  rotate: 180deg;
 `
--- components/brave_wallet_ui/components/shared/cta_button/cta_button.tsx
+++ components/brave_wallet_ui/components/shared/cta_button/cta_button.tsx
@@ -4,27 +4,37 @@
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
 import * as React from 'react'
+import Button from '@brave/leo/react/button'
+import Icon from '@brave/leo/react/icon'
 
 // Utils
 import { openTab } from '../../../utils/routes-utils'
 
 // Styled Components
-import { Button, ButtonText, IdeaButtonIcon } from './cta_button.style'
+import { ButtonWrapper } from './cta_button.style'
 
 interface Props {
   buttonText: string
   url: string
+  iconName?: string
 }
 
-export const CTAButton = ({ buttonText, url }: Props) => {
+export const CTAButton = ({ buttonText, url, iconName }: Props) => {
   const onClick = React.useCallback(() => {
     openTab(url)
   }, [url])
 
   return (
-    <Button onClick={onClick}>
-      <IdeaButtonIcon />
-      <ButtonText variant='default.semibold'>{buttonText}</ButtonText>
-    </Button>
+    <ButtonWrapper width='unset'>
+      <Button onClick={onClick}>
+        {iconName && (
+          <Icon
+            slot='icon-before'
+            name={iconName}
+          />
+        )}
+        {buttonText}
+      </Button>
+    </ButtonWrapper>
   )
 }
