brave/brave-core

commit eb5ca4eaa0f8f836b49b2f49f85e2d83245217f5

[plaster] `DownloadToolbarUIController` migration (#35750)
This class has a few customisations that were awkward with macro
replacements and therefore the derived function. With plaster though, we
can just add a `ChromiumImpl` variant of a function, and then only
replace the declartion in the translation unit, which makes a lot of the
complexity present go away.

This change drops the derived class for `DownloadToolbarUIController`,
and just adds a call to our custom function at the end of the relevant
function updating the icon appearance.

This particular class was running into problems in M149
(https://crrev.com/c/7766617).

Resolves https://github.com/brave/brave-browser/issues/54814

patches/chrome-browser-ui-views-download-bubble-download_toolbar_ui_controller.cc.patch
@@ -1,0 +1,20 @@
+diff --git a/chrome/browser/ui/views/download/bubble/download_toolbar_ui_controller.cc b/chrome/browser/ui/views/download/bubble/download_toolbar_ui_controller.cc
+index c67a71bad966bebb540b74a031f6310999dfc529..3cb0ac987e2bae17edfce9970661add88cf8355a 100644
+--- a/chrome/browser/ui/views/download/bubble/download_toolbar_ui_controller.cc
++++ b/chrome/browser/ui/views/download/bubble/download_toolbar_ui_controller.cc
+@@ -674,6 +674,7 @@ void DownloadToolbarUIController::UpdateIcon() {
+   }
+   action_item_->SetProperty(kActionItemUnderlineIndicatorKey, is_icon_active);
+ 
++  icon_color = GetIconColor(state_, active_, browser_view_->GetColorProvider());
+   action_item_->SetImage(ui::ImageModel::FromVectorIcon(*new_icon, icon_color));
+ 
+   // Update the toolbar button's tooltip.
+@@ -723,6 +724,7 @@ void DownloadToolbarUIController::UpdateIcon() {
+     return;
+   }
+   progress_ring->SetDownloading(progress_info_.progress_percentage);
++  UpdateIcon_BraveImpl(browser_view_, action_item_);
+ }
+ 
+ void DownloadToolbarUIController::OpenPrimaryDialog() {

rewrite/chrome/browser/ui/views/download/bubble/download_toolbar_ui_controller.cc.toml
@@ -1,0 +1,22 @@
+# Copyright (c) 2025 The Brave Authors. All rights reserved.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at https://mozilla.org/MPL/2.0/.
+
+[[substitution]]
+description = '''Override to show insecure icon on insecure download link.'
+
+This replacement adds a `UpdateIcon_BraveImpl` call at the end of the function.
+'''
+re_pattern = '(DownloadToolbarUIController::UpdateIcon\(.*?\n)^\}'
+replace = '''\1  UpdateIcon_BraveImpl(browser_view_, action_item_);
+}'''
+re_flags = ['MULTILINE', 'DOTALL']
+count = 1
+
+[[substitution]]
+description = 'Override for the icon colour.'
+re_pattern = '^(\s*)action_item_->SetImage'
+replace = '\1icon_color = GetIconColor(state_, active_, browser_view_->GetColorProvider());\1action_item_->SetImage'
+re_flags = ['MULTILINE']
+count = 1

chromium_src/chrome/browser/ui/views/download/bubble/download_toolbar_ui_controller.cc
@@ -7,15 +7,13 @@
 
 #include "chrome/browser/download/bubble/download_bubble_update_service.h"
 #include "chrome/browser/download/bubble/download_bubble_update_service_factory.h"
-#include "chrome/browser/download/download_ui_model.h"
 #include "chrome/browser/ui/color/chrome_color_id.h"
+#include "chrome/browser/ui/views/frame/browser_view.h"
 #include "components/vector_icons/vector_icons.h"
-#include "ui/base/models/image_model.h"
 
 namespace {
 
-SkColor GetIconColor(SkColor chromium_color,
-                     DownloadDisplay::IconState state,
+SkColor GetIconColor(DownloadDisplay::IconState state,
                      DownloadDisplay::IconActive active,
                      const ui::ColorProvider* color_provider) {
   // Apply active color only when download is completed and user doesn't
@@ -29,51 +27,9 @@ SkColor GetIconColor(SkColor chromium_color,
   return color_provider->GetColor(kColorDownloadToolbarButtonInactive);
 }
 
-}  // namespace
-
-#define DownloadToolbarUIController DownloadToolbarUIController_ChromiumImpl
-
-// Update upstream color with our own. Pass COLOR in so that icon_color variable
-// in the original file isn't flagged as unused by the compiler.
-#define FromVectorIcon(ICON, COLOR)                         \
-  FromVectorIcon(ICON, GetIconColor(COLOR, state_, active_, \
-                                    browser_view_->GetColorProvider()))
-
-#include <chrome/browser/ui/views/download/bubble/download_toolbar_ui_controller.cc>
-#undef FromVectorIcon
-#undef DownloadToolbarUIController
-
-void DownloadToolbarUIController::UpdateIcon() {
-  DownloadToolbarUIController_ChromiumImpl::UpdateIcon();
-
-  if (!action_item_.get()) {
-    return;
-  }
-
-  auto* button = GetDownloadsButton(browser_view_);
-  if (!button) {
-    return;
-  }
-
-  // Use an exclamation point icon while there's an insecure download in the
-  // download models.
-  if (HasInsecureDownloads()) {
-    auto icon_color = browser_view_->GetColorProvider()->GetColor(
-        ui::kColorAlertMediumSeverityIcon);
-    button->SetIconEnabledColorsOverride(icon_color);
-    button->SetVectorIcon(vector_icons::kNotSecureWarningIcon);
-    const gfx::VectorIcon* new_icon = &vector_icons::kNotSecureWarningIcon;
-    const int icon_size = action_item_->GetImage().Size().height();
-    action_item_->SetImage(
-        ui::ImageModel::FromVectorIcon(*new_icon, icon_color, icon_size));
-  } else {
-    button->SetIconEnabledColorsOverride(std::nullopt);
-  }
-}
-
-bool DownloadToolbarUIController::HasInsecureDownloads() {
+bool HasInsecureDownloads(BrowserView* browser_view) {
   auto* update_service = DownloadBubbleUpdateServiceFactory::GetForProfile(
-      browser_view_->GetProfile());
+      browser_view->GetProfile());
   if (!update_service || !update_service->IsInitialized()) {
     return false;
   }
@@ -89,3 +45,44 @@ bool DownloadToolbarUIController::HasInsecureDownloads() {
                 download::DownloadItem::InsecureDownloadStatus::WARN);
   });
 }
+
+// Forward-declaring this customisation point as it depends on
+// `GetDownloadsButton`, which is declared in the unamed namespace of the
+// shadowed source.
+void UpdateIcon_BraveImpl(BrowserView* browser_view,
+                          actions::ActionItem* action_item);
+
+}  // namespace
+
+#include <chrome/browser/ui/views/download/bubble/download_toolbar_ui_controller.cc>
+
+namespace {
+
+void UpdateIcon_BraveImpl(BrowserView* browser_view,
+                          actions::ActionItem* action_item) {
+  if (!action_item) {
+    return;
+  }
+
+  auto* button = GetDownloadsButton(browser_view);
+  if (!button) {
+    return;
+  }
+
+  // Use an exclamation point icon while there's an insecure download in the
+  // download models.
+  if (HasInsecureDownloads(browser_view)) {
+    auto icon_color = browser_view->GetColorProvider()->GetColor(
+        ui::kColorAlertMediumSeverityIcon);
+    button->SetIconEnabledColorsOverride(icon_color);
+    button->SetVectorIcon(vector_icons::kNotSecureWarningIcon);
+    const gfx::VectorIcon* new_icon = &vector_icons::kNotSecureWarningIcon;
+    const int icon_size = action_item->GetImage().Size().height();
+    action_item->SetImage(
+        ui::ImageModel::FromVectorIcon(*new_icon, icon_color, icon_size));
+  } else {
+    button->SetIconEnabledColorsOverride(std::nullopt);
+  }
+}
+
+}  // namespace

chromium_src/chrome/browser/ui/views/download/bubble/download_toolbar_ui_controller.h
@@ -1,40 +1,0 @@
-/* Copyright (c) 2025 The Brave Authors. All rights reserved.
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at https://mozilla.org/MPL/2.0/. */
-
-#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_DOWNLOAD_BUBBLE_DOWNLOAD_TOOLBAR_UI_CONTROLLER_H_
-#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_DOWNLOAD_BUBBLE_DOWNLOAD_TOOLBAR_UI_CONTROLLER_H_
-
-class DownloadToolbarUIController;
-using DownloadToolbarUIController_BraveImpl = DownloadToolbarUIController;
-
-#define DownloadToolbarUIController DownloadToolbarUIController_ChromiumImpl
-
-#define CreateBubbleDialogDelegate              \
-  Unused();                                     \
-  friend DownloadToolbarUIController_BraveImpl; \
-  void CreateBubbleDialogDelegate
-
-// Override to show warning icon in the toolbar when there's an insecure
-// download in progress.
-#define UpdateIcon virtual UpdateIcon
-
-#include <chrome/browser/ui/views/download/bubble/download_toolbar_ui_controller.h>  // IWYU pragma: export
-#undef UpdateIcon
-#undef DownloadToolbarUIController
-#undef CreateBubbleDialogDelegate
-
-class DownloadToolbarUIController
-    : public DownloadToolbarUIController_ChromiumImpl {
- public:
-  using DownloadToolbarUIController_ChromiumImpl::
-      DownloadToolbarUIController_ChromiumImpl;
-
-  void UpdateIcon() override;
-
- private:
-  bool HasInsecureDownloads();
-};
-
-#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_DOWNLOAD_BUBBLE_DOWNLOAD_TOOLBAR_UI_CONTROLLER_H_

commit 1db11f199d724639f9eae17ad10535522c9aea2c

Fixed download toolbar icon regressions with insecure file (#36874)
Resolves brave/brave-browser#55816

This ensures UpdateIcon_BraveImpl is called at the point where the upstream has already updated the
image, rather than at the end of the function where it would be skipped by early returns.

Regressions from #35750
TEST=DownloadToolbarInsecureIconTest.InsecureDownloadIcon

browser/ui/views/download/bubble/BUILD.gn
@@ -3,6 +3,30 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at https://mozilla.org/MPL/2.0/.
 
+source_set("browser_tests") {
+  testonly = true
+  defines = [ "HAS_OUT_OF_PROC_TEST_RUNNER" ]
+
+  sources = [ "download_toolbar_ui_controller_browsertest.cc" ]
+
+  deps = [
+    "//base",
+    "//base/test:test_support",
+    "//chrome/browser",
+    "//chrome/browser/download",
+    "//chrome/browser/ui",
+    "//chrome/browser/ui/download",
+    "//chrome/browser/ui/views/download",
+    "//chrome/test:test_support",
+    "//components/download/public/common:test_support",
+    "//components/keyed_service/content",
+    "//components/vector_icons",
+    "//content/test:test_support",
+    "//testing/gmock",
+    "//testing/gtest",
+  ]
+}
+
 source_set("unit_tests") {
   testonly = true
 

browser/ui/views/download/bubble/download_toolbar_ui_controller_browsertest.cc
@@ -1,0 +1,147 @@
+/* Copyright (c) 2026 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "chrome/browser/ui/views/download/bubble/download_toolbar_ui_controller.h"
+
+#include "base/callback_list.h"
+#include "base/functional/bind.h"
+#include "base/memory/raw_ptr.h"
+#include "chrome/browser/download/bubble/download_bubble_update_service.h"
+#include "chrome/browser/download/bubble/download_bubble_update_service_factory.h"
+#include "chrome/browser/download/download_item_model.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/download/download_display.h"
+#include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/browser/ui/views/frame/toolbar_button_provider.h"
+#include "chrome/browser/ui/views/toolbar/toolbar_button.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "components/download/public/common/download_item.h"
+#include "components/download/public/common/mock_download_item.h"
+#include "components/keyed_service/content/browser_context_dependency_manager.h"
+#include "components/vector_icons/vector_icons.h"
+#include "content/public/browser/browser_context.h"
+#include "content/public/test/browser_test.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+using ::testing::NiceMock;
+using ::testing::Return;
+
+namespace {
+
+// Minimal fake that lets tests control which download items are visible, so
+// HasInsecureDownloads() returns a predictable value without needing real
+// network activity.
+class FakeDownloadBubbleUpdateService : public DownloadBubbleUpdateService {
+ public:
+  explicit FakeDownloadBubbleUpdateService(Profile* profile)
+      : DownloadBubbleUpdateService(profile) {}
+
+  bool IsInitialized() const override { return true; }
+
+  bool GetAllModelsToDisplay(
+      std::vector<DownloadUIModel::DownloadUIModelPtr>& models,
+      const webapps::AppId* /*web_app_id*/,
+      bool /*force_backfill*/) override {
+    models.clear();
+    for (download::DownloadItem* item : items_) {
+      models.push_back(DownloadItemModel::Wrap(item));
+    }
+    return true;
+  }
+
+  void AddItem(download::DownloadItem* item) { items_.push_back(item); }
+
+ private:
+  std::vector<raw_ptr<download::DownloadItem, VectorExperimental>> items_;
+};
+
+std::unique_ptr<KeyedService> BuildFakeService(
+    content::BrowserContext* context) {
+  return std::make_unique<FakeDownloadBubbleUpdateService>(
+      Profile::FromBrowserContext(context));
+}
+
+}  // namespace
+
+class DownloadToolbarInsecureIconTest : public InProcessBrowserTest {
+ public:
+  void SetUpInProcessBrowserTestFixture() override {
+    InProcessBrowserTest::SetUpInProcessBrowserTestFixture();
+    // Register the fake service factory before the browser (and its profile)
+    // is created, so DownloadBubbleUIController picks it up from the start and
+    // no dangling raw_ptr is left to the replaced service.
+    subscription_ =
+        BrowserContextDependencyManager::GetInstance()
+            ->RegisterCreateServicesCallbackForTesting(
+                base::BindRepeating([](content::BrowserContext* context) {
+                  DownloadBubbleUpdateServiceFactory::GetInstance()
+                      ->SetTestingFactory(
+                          context, base::BindRepeating(&BuildFakeService));
+                }));
+  }
+
+  FakeDownloadBubbleUpdateService* fake_service() {
+    return static_cast<FakeDownloadBubbleUpdateService*>(
+        DownloadBubbleUpdateServiceFactory::GetForProfile(
+            browser()->profile()));
+  }
+
+  ToolbarButton* download_button() {
+    return BrowserView::GetBrowserViewForBrowser(browser())
+        ->toolbar_button_provider()
+        ->GetDownloadButton();
+  }
+
+  DownloadToolbarUIController* controller() {
+    return DownloadToolbarUIController::From(browser());
+  }
+
+ private:
+  base::CallbackListSubscription subscription_;
+};
+
+// Verifies that UpdateIcon_BraveImpl sets / clears the toolbar button colour
+// override according to the insecure-download status of the current models.
+// Covers: no insecure download, WARN status, and BLOCK status.
+IN_PROC_BROWSER_TEST_F(DownloadToolbarInsecureIconTest, InsecureDownloadIcon) {
+  auto* ctrl = controller();
+  ASSERT_NE(nullptr, ctrl) << "controller should be available";
+  ctrl->Show();
+
+  auto* btn = download_button();
+  ASSERT_NE(nullptr, btn) << "download button should be present after Show()";
+
+  NiceMock<download::MockDownloadItem> item;
+
+  // No insecure downloads — colour override must be absent.
+  ctrl->UpdateDownloadIcon(
+      {.new_active = DownloadDisplay::IconActive::kActive});
+  EXPECT_FALSE(btn->HasIconEnabledColorsOverride()) << "state: no items";
+  EXPECT_TRUE(!btn->HasVectorIcons() ||
+              &btn->GetVectorIcon() != &vector_icons::kNotSecureWarningIcon)
+      << "state: no items; HasVectorIcons=" << btn->HasVectorIcons();
+
+  // WARN status: warning colour override and warning vector icon must be set.
+  ON_CALL(item, GetInsecureDownloadStatus())
+      .WillByDefault(
+          Return(download::DownloadItem::InsecureDownloadStatus::WARN));
+  fake_service()->AddItem(&item);
+  ctrl->UpdateDownloadIcon(
+      {.new_state = DownloadDisplay::IconState::kProgress});
+  EXPECT_TRUE(btn->HasIconEnabledColorsOverride()) << "state: WARN";
+  EXPECT_EQ(&btn->GetVectorIcon(), &vector_icons::kNotSecureWarningIcon)
+      << "state: WARN";
+
+  // BLOCK status is treated the same way.
+  ON_CALL(item, GetInsecureDownloadStatus())
+      .WillByDefault(
+          Return(download::DownloadItem::InsecureDownloadStatus::BLOCK));
+  ctrl->UpdateDownloadIcon(
+      {.new_state = DownloadDisplay::IconState::kComplete});
+  EXPECT_TRUE(btn->HasIconEnabledColorsOverride()) << "state: BLOCK";
+  EXPECT_EQ(&btn->GetVectorIcon(), &vector_icons::kNotSecureWarningIcon)
+      << "state: BLOCK";
+}

patches/chrome-browser-ui-views-download-bubble-download_toolbar_ui_controller.cc.patch
@@ -1,5 +1,5 @@
 diff --git a/chrome/browser/ui/views/download/bubble/download_toolbar_ui_controller.cc b/chrome/browser/ui/views/download/bubble/download_toolbar_ui_controller.cc
-index 5360031b8a2b29a58572f1caa5531f168c710335..5d9e72d1fe4e398435fa46e620dfa25935da340b 100644
+index 5360031b8a2b29a58572f1caa5531f168c710335..717ed2c360e8350ee97722ef6589d6bd115e5a3c 100644
 --- a/chrome/browser/ui/views/download/bubble/download_toolbar_ui_controller.cc
 +++ b/chrome/browser/ui/views/download/bubble/download_toolbar_ui_controller.cc
 @@ -686,6 +686,7 @@ void DownloadToolbarUIController::UpdateIcon() {
@@ -10,11 +10,11 @@ index 5360031b8a2b29a58572f1caa5531f168c710335..5d9e72d1fe4e398435fa46e620dfa259
    action_item_->SetImage(ui::ImageModel::FromVectorIcon(*new_icon, icon_color));
  
    // Update the toolbar button's tooltip.
-@@ -735,6 +736,7 @@ void DownloadToolbarUIController::UpdateIcon() {
-     return;
-   }
-   progress_ring->SetDownloading(progress_info_.progress_percentage);
+@@ -716,6 +717,7 @@ void DownloadToolbarUIController::UpdateIcon() {
+   GetImageBadge(browser_view_)
+       ->UpdateImage(is_active, progress_download_count, progress_color,
+                     badge_background_color);
 +  UpdateIcon_BraveImpl(browser_view_, action_item_);
- }
  
- void DownloadToolbarUIController::OpenPrimaryDialog() {
+   // Do not show the progress ring when there is no in progress download.
+   if (state_ == IconState::kComplete || progress_info_.download_count == 0) {

rewrite/chrome/browser/ui/views/download/bubble/download_toolbar_ui_controller.cc.toml
@@ -4,14 +4,15 @@
 # You can obtain one at https://mozilla.org/MPL/2.0/.
 
 [[substitution]]
-description = '''Override to show insecure icon on insecure download link.'
+description = '''Override to show insecure icon on insecure download link.
 
-This replacement adds a `UpdateIcon_BraveImpl` call at the end of the function.
+This replacement adds a `UpdateIcon_BraveImpl` call after the upstream image
+update so it runs even when UpdateIcon() returns early afterwards.
 '''
-re_pattern = '(DownloadToolbarUIController::UpdateIcon\(.*?\n)^\}'
-replace = '''\1  UpdateIcon_BraveImpl(browser_view_, action_item_);
-}'''
-re_flags = ['MULTILINE', 'DOTALL']
+re_pattern = '(badge_background_color\);)'
+replace = '''\1
+  UpdateIcon_BraveImpl(browser_view_, action_item_);'''
+re_flags = ['MULTILINE']
 
 [[substitution]]
 description = 'Override for the icon colour.'

test/BUILD.gn
@@ -1398,6 +1398,7 @@ test("brave_browser_tests") {
       "//brave/browser/ui/page_info",
       "//brave/browser/ui/tabs:tab_strip",
       "//brave/browser/ui/toolbar:brave_app_menu_browser_tests",
+      "//brave/browser/ui/views/download/bubble:browser_tests",
       "//brave/browser/ui/webui/brave_new_tab_page_refresh:browser_tests",
       "//brave/browser/ui/webui/side_panel/customize_chrome:browser_tests",
       "//brave/browser/ui/whats_new:browser_test",