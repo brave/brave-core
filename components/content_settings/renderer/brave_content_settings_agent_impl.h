e22f1d4fba8f7cdaa7dd6d270b51aba6d7d7b9b0
Anton Paymyshev <apaymyshev@brave.com>
2026-06-02 19:20:13 +0700

    [Wallet] Don't require user activation for hid api on ledger subframe

--- components/content_settings/renderer/brave_content_settings_agent_impl.h
+++ components/content_settings/renderer/brave_content_settings_agent_impl.h
@@ -71,6 +71,8 @@ class BraveContentSettingsAgentImpl
 
   bool IsReduceLanguageEnabled() override;
 
+  bool RequireTransientActivationForHidRequestDevice() override;
+
  private:
   FRIEND_TEST_ALL_PREFIXES(BraveContentSettingsAgentImplAutoplayBrowserTest,
                            AutoplayBlockedByDefault);
