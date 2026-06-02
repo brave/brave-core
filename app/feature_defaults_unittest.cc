aba5e9adc54c8ea463e83826102481235aadfaf5
boocmp <boocmp@yandex.ru>
2026-06-02 21:31:18 +0700

    Fixed FeatureDefaultsTest.DisabledFeatures.

--- app/feature_defaults_unittest.cc
+++ app/feature_defaults_unittest.cc
@@ -166,7 +166,6 @@ TEST(FeatureDefaultsTest, DisabledFeatures) {
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
       &features::kFewerUpdateConfirmations,
 #endif
-      &features::kHttpsFirstBalancedMode,
       &features::kIdleDetection,
       &features::kIndigo,
       &features::kInfobarRefresh,
