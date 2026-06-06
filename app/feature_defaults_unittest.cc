4cb7f5ae5119da64157e8e8e1810f003686f6f03
Claudio DeSouza <cdesouza@brave.com>
2026-06-06 16:39:57 +0100

    [extensions-mv2][cr151] `kExtensionsManifestV3Only` deleted
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/48e3e540b18dd257beae09cc383fd882a3f1acf0
    
    commit 48e3e540b18dd257beae09cc383fd882a3f1acf0
    Author: Devlin Cronin <rdevlin.cronin@chromium.org>
    Date:   Thu Jun 4 10:36:08 2026 -0700
    
        [Extensions] Remove kExtensionsManifestV3Only feature
    
        The kExtensionsManifestV3Only feature was used to allow admins and
        developers to test the behavior of blocking MV2 extensions by policy.
        Since the associated enterprise policy is no longer available and
        MV2 extensions are blocked by default, we can remove this feature.
    
        Bug: 431097630
        Change-Id: I9d4b01041ef8876808165eb0682f363c775d7aa8
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7891489
        Reviewed-by: Tim <tjudkins@chromium.org>
        Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1641771}

--- app/feature_defaults_unittest.cc
+++ app/feature_defaults_unittest.cc
@@ -146,7 +146,6 @@ TEST(FeatureDefaultsTest, DisabledFeatures) {
 #if !BUILDFLAG(IS_ANDROID)
       &enterprise_data_protection::kEnableForceDownloadToCloud,
       &enterprise_data_protection::kEnableForceDownloadToOneDrive,
-      &extensions_features::kExtensionsManifestV3Only,
 #endif
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_APPLE) || BUILDFLAG(IS_LINUX)
       &feature_engagement::kIPHAutofillAccountNameEmailSuggestionFeature,
