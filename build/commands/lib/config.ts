0fff671198e91057a2587b29f21bc0dd32955fce
Mikhail <matuchin@brave.com>
2026-06-08 19:36:37 +0400

    Temporarily disable LSAN (#37072)
    The PR temporarily disables LSAN until CI issues are resolved.

--- build/commands/lib/config.ts
+++ build/commands/lib/config.ts
@@ -349,7 +349,8 @@ export class Config {
     // LSAN only works with ASAN and has very low overhead.
     // Chromium supports LeakSanitizer is supported on x86_64 Linux only.
     // See https://www.chromium.org/developers/testing/leaksanitizer/
-    return this.isAsan() && this.targetOS === 'linux'
+    // Temporarily disable LSAN: https://github.com/brave/brave-browser/issues/56047
+    return false
   }
 
   isOfficialBuild() {
