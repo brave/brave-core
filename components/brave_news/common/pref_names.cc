0acef009e16c9368a0a8abba621ab3913a2260bc
Jay Harris <jay.harris@outlook.co.nz>
2026-06-10 10:33:09 +1200

    [Brave News]: Add sync support

--- components/brave_news/common/pref_names.cc
+++ components/brave_news/common/pref_names.cc
@@ -8,6 +8,7 @@
 #include "brave/components/brave_news/common/locales_helper.h"
 #include "brave/components/brave_news/common/p3a_pref_names.h"
 #include "brave/components/p3a_utils/feature_usage.h"
+#include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
 
@@ -15,14 +16,25 @@ namespace brave_news {
 namespace prefs {
 
 void RegisterProfilePrefs(PrefRegistrySimple* registry) {
-  registry->RegisterBooleanPref(kShouldShowToolbarButton, true);
-  registry->RegisterBooleanPref(kNewTabPageShowToday,
-                                IsUserInDefaultEnabledLocale());
-  registry->RegisterBooleanPref(kBraveNewsOptedIn, false);
-  registry->RegisterDictionaryPref(kBraveNewsSources);
-  registry->RegisterDictionaryPref(kBraveNewsChannels);
-  registry->RegisterDictionaryPref(kBraveNewsDirectFeeds);
-  registry->RegisterBooleanPref(kBraveNewsOpenArticlesInNewTab, true);
+  // These prefs are synced as part of the "Settings" (PREFERENCES) sync type.
+  registry->RegisterBooleanPref(
+      kShouldShowToolbarButton, true,
+      user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
+  registry->RegisterBooleanPref(
+      kNewTabPageShowToday, IsUserInDefaultEnabledLocale(),
+      user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
+  registry->RegisterBooleanPref(
+      kBraveNewsOptedIn, false,
+      user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
+  registry->RegisterDictionaryPref(
+      kBraveNewsSources, user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
+  registry->RegisterDictionaryPref(
+      kBraveNewsChannels, user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
+  registry->RegisterDictionaryPref(
+      kBraveNewsDirectFeeds, user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
+  registry->RegisterBooleanPref(
+      kBraveNewsOpenArticlesInNewTab, true,
+      user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
   registry->RegisterBooleanPref(kBraveNewsDisabledByPolicy, false);
 
   brave_news::p3a::prefs::RegisterProfileNewsMetricsPrefs(registry);
