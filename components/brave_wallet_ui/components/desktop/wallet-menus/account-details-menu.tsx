2d401f90bff4d5f5bc39fb9c04454a7e352d0832
Douglas Daniel <douglashdaniel@gmail.com>
2026-06-10 10:06:15 -0500

    [Wallet]: Migrate Account Details Menu to use Nala Button Menu (#37136)
    Migrates the Account Details Menu to use the Nala Button Menu component

--- components/brave_wallet_ui/components/desktop/wallet-menus/account-details-menu.tsx
+++ components/brave_wallet_ui/components/desktop/wallet-menus/account-details-menu.tsx
@@ -4,6 +4,7 @@
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
 import * as React from 'react'
+import Icon from '@brave/leo/react/icon'
 
 // Types
 import {
@@ -15,38 +16,29 @@ import {
 import { getLocale } from '../../../../common/locale'
 
 // Styled Components
-import {
-  StyledWrapper,
-  PopupButton,
-  PopupButtonText,
-  ButtonIcon,
-} from './wellet-menus.style'
-import { VerticalDivider, VerticalSpace } from '../../shared/style'
+import { ButtonMenu } from './wellet-menus.style'
 
 interface Props {
+  children: React.ReactNode
   options: AccountButtonOptionsObjectType[]
   onClickMenuOption: (option: AccountModalTypes) => void
 }
 
 export const AccountDetailsMenu = (props: Props) => {
-  const { options, onClickMenuOption } = props
+  const { children, options, onClickMenuOption } = props
 
   return (
-    <StyledWrapper yPosition={42}>
+    <ButtonMenu placement='bottom-end'>
+      {children}
       {options.map((option) => (
         <React.Fragment key={option.id}>
-          {option.id === 'privateKey' && (
-            <>
-              <VerticalDivider />
-              <VerticalSpace space='8px' />
-            </>
-          )}
-          <PopupButton onClick={() => onClickMenuOption(option.id)}>
-            <ButtonIcon name={option.icon} />
-            <PopupButtonText>{getLocale(option.name)}</PopupButtonText>
-          </PopupButton>
+          {option.id === 'privateKey' && <hr />}
+          <leo-menu-item onClick={() => onClickMenuOption(option.id)}>
+            <Icon name={option.icon} />
+            {getLocale(option.name)}
+          </leo-menu-item>
         </React.Fragment>
       ))}
-    </StyledWrapper>
+    </ButtonMenu>
   )
 }
