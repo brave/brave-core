9f2a860b4c496f1078b200c5de4db2ad9d24769d
Artem Samoilenko <artem@brave.com>
2026-06-10 15:48:08 -0400

    [Android] Enable Custom Search Engines flag

--- browser/brave_browser_features.cc
+++ browser/brave_browser_features.cc
@@ -73,7 +73,7 @@ BASE_FEATURE(kBraveFreshNtpAfterIdleExperiment,
 
 // Enable custom search engines on Android, allowing users to add, edit, and
 // remove their own search engine entries from the search engine settings.
-BASE_FEATURE(kBraveCustomSearchEngines, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kBraveCustomSearchEngines, base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_ANDROID)
 
 // The variant of the "day zero" experiment. i.e. A, B, C, D, etc.
