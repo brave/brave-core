ed51ea5a72771dbb4ff26aa05c4a991f605efa34
Aleksei Khoroshilov <5928869+goodov@users.noreply.github.com>
2026-06-10 19:20:28 +0700

    Fix upstream windows build flake by ignoring .rsp in reorder-imports. (#37149)

---
+++ chromium_src/build/win/reorder-imports.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2026 The Brave Authors. All rights reserved.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import glob
+
+import override_utils
+
+
+@override_utils.override_function(glob)
+def iglob(original_function, *args, **kwargs):
+    for fname in original_function(*args, **kwargs):
+        # The .rsp (response file) can still be present while this action runs,
+        # make sure to skip it as it's not a valid input file.
+        if fname.endswith('.rsp'):
+            continue
+        yield fname
