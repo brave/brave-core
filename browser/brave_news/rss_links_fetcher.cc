0c6eae8927985bbc55010a9db7094b7d46e728ae
Emerick Rogul <erogul@brave.com>
2026-06-10 09:22:17 -0400

    fixup! [cr150] Lifting deleted rss code into `brave_news`

--- browser/brave_news/rss_links_fetcher.cc
+++ browser/brave_news/rss_links_fetcher.cc
@@ -3,6 +3,11 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
+// Based on Chromium code subject to the following license:
+// Copyright 2021 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "brave/browser/brave_news/rss_links_fetcher.h"
 
 #include <utility>
