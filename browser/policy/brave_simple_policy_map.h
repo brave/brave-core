f6c7ce63c2b96d4c66f3d33d4496fe9343e1167d
Darnell Andries <dandries@brave.com>
2026-06-11 14:36:32 -0700

    Fix conditional includes/deps for policy pref interceptor

--- browser/policy/brave_simple_policy_map.h
+++ browser/policy/brave_simple_policy_map.h
@@ -83,7 +83,7 @@ namespace policy {
 // Note: If a policy without dynamic refresh support is being added
 //       which requires preservation of the pref value at runtime,
 //       please add it to kNonDynamicPrefs in
-//       components/brave_policy/policy_pref_interceptor.cc.
+//       browser/policy/policy_pref_interceptor_utils.cc.
 //       However, please try to support dynamic refresh if possible.
 inline constexpr PolicyToPreferenceMapEntry kBraveSimplePolicyMap[] = {
 #if BUILDFLAG(ENABLE_BRAVE_REWARDS)
