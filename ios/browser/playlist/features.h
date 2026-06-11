522aab5c6554889942a078828d8d3e6c9b5a3892
brave-builds <45370463+brave-builds@users.noreply.github.com>
2026-06-11 23:03:56 +0200

    [iOS] Add Playlist offline cache feature flag (uplift to 1.92.x) (#37168)
    Uplift of #37140 (squashed) to beta

---
+++ ios/browser/playlist/features.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2026 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_IOS_BROWSER_PLAYLIST_FEATURES_H_
+#define BRAVE_IOS_BROWSER_PLAYLIST_FEATURES_H_
+
+#import "base/feature_list.h"
+
+namespace playlist::features {
+
+BASE_DECLARE_FEATURE(kPlaylistOfflineCacheEnabled);
+
+}  // namespace playlist::features
+
+#endif  // BRAVE_IOS_BROWSER_PLAYLIST_FEATURES_H_
