f0e58d3909d1fb7ae78b22b8d871f3e3c5058f31
Simon Hong <shong@brave.com>
2026-06-12 12:13:04 +0900

    [Sidebar V2] Update panel's default minimum width

---
+++ browser/ui/views/side_panel/side_panel_entry_unittest.cc
@@ -0,0 +1,16 @@
+/* Copyright (c) 2026 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "chrome/browser/ui/side_panel/side_panel_entry.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace brave {
+
+TEST(SidePanelEntryTest, DefaultContentWidthIs400) {
+  EXPECT_EQ(SidePanelEntry::kSidePanelDefaultContentWidth, 400);
+}
+
+}  // namespace brave
