e3a369c1837cbad1ba216d3d6c0a54b8a127a237
Claudio DeSouza <cdesouza@brave.com>
2026-06-15 21:57:56 +0100

    [ts][cr151] Power bookmark migrated to lit
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/99ad0da2fb786a99d7eb0f438f5ae070060babf9
    
    commit 99ad0da2fb786a99d7eb0f438f5ae070060babf9
    Author: rbpotter <rbpotter@chromium.org>
    Date:   Wed Jun 10 17:09:10 2026 -0700
    
        [Side Panel Bookmarks] Clean up Polymer leftovers
    
        Remove Polymer dependency, fix errors revealed by stricter Lit build
        checks, and remove side panel bookmarks from the visibility list.
    
        Bug: 493823435
        Change-Id: Iaf37351a7bb1c795b1b72af030d024ad3436521f
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7903787
        Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
        Reviewed-by: Alison Gale <agale@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1644992}

--- chromium_src/chrome/browser/resources/side_panel/bookmarks/power_bookmarks_edit_dialog.ts
+++ chromium_src/chrome/browser/resources/side_panel/bookmarks/power_bookmarks_edit_dialog.ts
@@ -3,13 +3,15 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import { RegisterStyleOverride } from '//resources/brave/polymer_overriding.js'
-import { html } from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js'
+import { injectStyle } from '//resources/brave/lit_overriding.js'
+import { css } from '//resources/lit/v3_0/lit.rollup.js'
 
-export * from './power_bookmarks_edit_dialog-chromium.js'
+import { PowerBookmarksEditDialogElement } from './power_bookmarks_edit_dialog-chromium.js'
 
-RegisterStyleOverride('power-bookmarks-edit-dialog', html`<style>
+injectStyle(PowerBookmarksEditDialogElement, css`
   :host {
     --leo-button-padding: 4px;
   }
-</style>`)
+`)
+
+export * from './power_bookmarks_edit_dialog-chromium.js'
