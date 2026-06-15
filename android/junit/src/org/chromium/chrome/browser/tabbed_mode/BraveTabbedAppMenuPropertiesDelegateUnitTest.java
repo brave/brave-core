be4dd1c606fdf057c34a632dca8626470aff35ed
Artem Samoilenko <artem@brave.com>
2026-06-15 08:57:41 -0400

    [cr151][Android] Remove expired reader-mode-improvements feature flag
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/508cfeae4c744931dc617d9dd1a01e87fba72d04
    
    commit 508cfeae4c744931dc617d9dd1a01e87fba72d04
    Author: Evan Luo <evanluo@google.com>
    Date:   Mon Jun 8 13:03:35 2026 -0700
    
        [reader mode] Remove expired reader-mode-improvements feature flag
    
        The reader-mode-improvements flag is expired and disabled by default.
        This CL maintains logic tied to the distill-in-app feature flag, which
        will also be subsequently cleaned up.
    
        Bug: 509854470
        Change-Id: I93ec80e1d039786ad6c1316d37fff81ef55424e2
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7906378
        Commit-Queue: Evan Luo <evanluo@google.com>
        Reviewed-by: Brandon Wylie <wylieb@google.com>
        Cr-Commit-Position: refs/heads/main@{#1643419}

--- android/junit/src/org/chromium/chrome/browser/tabbed_mode/BraveTabbedAppMenuPropertiesDelegateUnitTest.java
+++ android/junit/src/org/chromium/chrome/browser/tabbed_mode/BraveTabbedAppMenuPropertiesDelegateUnitTest.java
@@ -126,7 +126,6 @@ import java.util.List;
     ChromeFeatureList.LENS_OVERLAY_ANDROID,
     ChromeFeatureList.SUBMENUS_IN_APP_MENU,
     DomDistillerFeatures.READER_MODE_DISTILL_IN_APP,
-    DomDistillerFeatures.READER_MODE_IMPROVEMENTS,
     BraveFeatureList.BRAVE_SHRED,
 })
 @EnableFeatures({
