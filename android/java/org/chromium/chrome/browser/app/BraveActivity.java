406388a2376a2cd7fc8697f989e63c394d3bb244
brave-builds <brave-builds+devops@brave.com>
2026-06-16 08:27:05 +0000

    Uplift of #37268 (squashed) to beta

--- android/java/org/chromium/chrome/browser/app/BraveActivity.java
+++ android/java/org/chromium/chrome/browser/app/BraveActivity.java
@@ -2739,7 +2739,9 @@ public abstract class BraveActivity extends ChromeActivity
             final View rootView = requireViewById(android.R.id.content);
             rootView.post(
                     () -> {
-                        if (isActivityFinishingOrDestroyed()) {
+                        // Check again that mSearchWidgetPromoPanel is not null otherwise
+                        // it may cause NPE on some edge cases.
+                        if (isActivityFinishingOrDestroyed() || mSearchWidgetPromoPanel == null) {
                             return;
                         }
                         mSearchWidgetPromoPanel.showIfNeeded(
