6edf7e9c03c05dd54a216375bc96de8ecbde3f34
Emerick Rogul <erogul@brave.com>
2026-06-16 11:55:56 -0400

    fixup! [cr150] `c/b/u/page_actions/` -> `c/b/u/page_action/`

--- chromium_src/chrome/browser/ui/page_action/page_action_triggers.h
+++ chromium_src/chrome/browser/ui/page_action/page_action_triggers.h
@@ -3,8 +3,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_PAGE_ACTIONS_PAGE_ACTION_TRIGGERS_H_
-#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_PAGE_ACTIONS_PAGE_ACTION_TRIGGERS_H_
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_PAGE_ACTION_PAGE_ACTION_TRIGGERS_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_PAGE_ACTION_PAGE_ACTION_TRIGGERS_H_
 
 #include <chrome/browser/ui/page_action/page_action_triggers.h>  // IWYU pragma: export
 
@@ -13,4 +13,4 @@ namespace page_actions {
 extern const ui::ClassProperty<int>* const kBravePageActionEventFlagKey;
 }  // namespace page_actions
 
-#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_PAGE_ACTIONS_PAGE_ACTION_TRIGGERS_H_
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_PAGE_ACTION_PAGE_ACTION_TRIGGERS_H_
