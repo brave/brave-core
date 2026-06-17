99c91b038cb805e857a2025117738c3e8483a216
Aleksei Khoroshilov <akhoroshilov@brave.com>
2026-06-17 22:05:50 +0700

    Introduce node_action to run js/ts scripts during the build natively.

--- build/commands/scripts/genTsConfig.js
+++ build/commands/scripts/genTsConfig.js
@@ -3,9 +3,12 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
+import { program } from 'commander'
 import genTsConfig from '../lib/genTsConfig.js'
 
-genTsConfig().catch((err) => {
-  console.error(err)
-  process.exit(1)
-})
+program
+  .requiredOption('--root-gen-dir <path>', 'path to the build gen directory')
+  .action(async (options) => {
+    await genTsConfig(options.rootGenDir)
+  })
+  .parseAsync()
