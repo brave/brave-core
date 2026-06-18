907b4edea7f17b9063040a20a7d15b98f8c75057
Rohit Agarwal <ragarwal@brave.com>
2026-06-18 19:47:54 +0200

    update

--- components/brave_shields/resources/panel_new/components/fingerprinting_details.tsx
+++ components/brave_shields/resources/panel_new/components/fingerprinting_details.tsx
@@ -25,32 +25,30 @@ const webcompatSettingNames = new Map(
       )
     })
     .map(([key, value]) => {
-      if (value == ContentSettingsType.BRAVE_WEBCOMPAT_WEBGL) {
-        return [
-          value,
-          getString('BRAVE_SHIELDS_BLOCK_FINGERPRINTING_WEBGL_LABEL'),
-        ]
+      switch (value) {
+        case ContentSettingsType.BRAVE_WEBCOMPAT_WEBGL:
+          return [
+            value,
+            getString('BRAVE_SHIELDS_BLOCK_FINGERPRINTING_WEBGL_LABEL'),
+          ]
+        case ContentSettingsType.BRAVE_WEBCOMPAT_WEBGL2:
+          return [
+            value,
+            getString('BRAVE_SHIELDS_BLOCK_FINGERPRINTING_WEBGL2_LABEL'),
+          ]
+        case ContentSettingsType.BRAVE_WEBCOMPAT_WEBGPU:
+          return [
+            value,
+            getString('BRAVE_SHIELDS_BLOCK_FINGERPRINTING_WEBGPU_LABEL'),
+          ]
+        default: {
+          const name = key
+            .replace('BRAVE_WEBCOMPAT_', '')
+            .replaceAll('_', ' ')
+            .toLowerCase()
+          return [value, name]
+        }
       }
-
-      if (value == ContentSettingsType.BRAVE_WEBCOMPAT_WEBGL2) {
-        return [
-          value,
-          getString('BRAVE_SHIELDS_BLOCK_FINGERPRINTING_WEBGL2_LABEL'),
-        ]
-      }
-
-      if (value == ContentSettingsType.BRAVE_WEBCOMPAT_WEBGPU) {
-        return [
-          value,
-          getString('BRAVE_SHIELDS_BLOCK_FINGERPRINTING_WEBGPU_LABEL'),
-        ]
-      }
-
-      const name = key
-        .replace('BRAVE_WEBCOMPAT_', '')
-        .replaceAll('_', ' ')
-        .toLowerCase()
-      return [value, name]
     }),
 )
 
