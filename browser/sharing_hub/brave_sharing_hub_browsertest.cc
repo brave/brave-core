68a5087e943e45be77fb9e19cf6b21ae50688b47
Claudio DeSouza <cdesouza@brave.com>
2026-06-23 21:32:24 +0100

    [cr151] `PageActionIconType::kSharingHub` deleted
    The enumaration and the view have been dropped in upstream. This value
    was being used in Brave mostly as a UI anchor.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/35843747a4f23b172ca332ca0d12c0f194df4e99
    
    commit 35843747a4f23b172ca332ca0d12c0f194df4e99
    Author: Marc Treib <treib@chromium.org>
    Date:   Mon Jun 22 02:30:29 2026 -0700
    
        Clean up unused PageActionIconType::kSharingHub and SharingHubIconView
    
        The "Sharing Hub" page action icon has been unused for a long time.
        This CL cleans up some dead code around it. (There's probably more
        Sharing Hub-related code that could be cleaned up in future CLs.)
    
        Bug: none
        Change-Id: Ibfdea2a7d97ad5aff740303fc3e4b6556a6a6964
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7956129
        Reviewed-by: Michael Tatarski <mtatarski@google.com>
        Auto-Submit: Marc Treib <treib@chromium.org>
        Commit-Queue: Marc Treib <treib@chromium.org>
        Reviewed-by: Ian Clelland <iclelland@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1650195}

--- browser/sharing_hub/brave_sharing_hub_browsertest.cc
+++ browser/sharing_hub/brave_sharing_hub_browsertest.cc
@@ -3,13 +3,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sharing_hub/sharing_hub_model.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/page_action/page_action_icon_type.h"
-#include "chrome/browser/ui/views/frame/browser_view.h"
-#include "chrome/browser/ui/views/frame/toolbar_button_provider.h"
-#include "chrome/browser/ui/views/page_action/page_action_icon_view.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/test/browser_test.h"
@@ -18,34 +16,7 @@ using BraveSharingHubTest = InProcessBrowserTest;
 
 namespace sharing_hub {
 
-IN_PROC_BROWSER_TEST_F(BraveSharingHubTest, SharingHubIconVisibility) {
-  views::View* sharing_hub_icon =
-      BrowserView::GetBrowserViewForBrowser(browser())
-          ->toolbar_button_provider()
-          ->GetPageActionIconView(PageActionIconType::kSharingHub);
-  ASSERT_TRUE(sharing_hub_icon);
-
-  // No icon.
-  ASSERT_TRUE(
-      ui_test_utils::NavigateToURL(browser(), GURL("chrome://newtab/")));
-  EXPECT_FALSE(sharing_hub_icon->GetVisible());
-  ASSERT_TRUE(
-      ui_test_utils::NavigateToURL(browser(), GURL("chrome://settings/")));
-  EXPECT_FALSE(sharing_hub_icon->GetVisible());
-
-  // Visible icon.
-  ASSERT_TRUE(
-      ui_test_utils::NavigateToURL(browser(), GURL("https://brave.com/")));
-  EXPECT_TRUE(sharing_hub_icon->GetVisible());
-}
-
 IN_PROC_BROWSER_TEST_F(BraveSharingHubTest, CopyCommandsOrder) {
-  views::View* sharing_hub_icon =
-      BrowserView::GetBrowserViewForBrowser(browser())
-          ->toolbar_button_provider()
-          ->GetPageActionIconView(PageActionIconType::kSharingHub);
-  ASSERT_TRUE(sharing_hub_icon);
-
   {
     ASSERT_TRUE(
         ui_test_utils::NavigateToURL(browser(), GURL("https://brave.com/")));
