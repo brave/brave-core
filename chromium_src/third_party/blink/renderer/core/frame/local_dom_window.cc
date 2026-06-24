55604877785d316eb36bb7362139b02cd7694f17
Rohit Agarwal <ragarwal@brave.com>
2026-06-24 14:06:00 +0200

    move opener logic to brave session cache

--- chromium_src/third_party/blink/renderer/core/frame/local_dom_window.cc
+++ chromium_src/third_party/blink/renderer/core/frame/local_dom_window.cc
@@ -45,26 +45,6 @@ using brave::BlockScreenFingerprinting;
 using brave::FarbleInteger;
 using brave::FarbleKey;
 
-namespace {
-// A helper method to return the execution context of the |opener| if |opener|
-// is not null. Otherwise, returns the |current_context|.
-// Sharing the same execution context as the opener helps to ensure the
-// underlying brave session cache is the same, which helps to keep the same
-// farbling seed for both the opener and the openee ensuring they see the same
-// farbled values.
-ExecutionContext* GetContextFromOpenerIfPossible(
-    const DOMWindow* opener,
-    ExecutionContext* current_context) {
-  if (auto* local_opener = DynamicTo<LocalDOMWindow>(opener)) {
-    if (ExecutionContext* opener_context =
-            local_opener->GetExecutionContext()) {
-      return opener_context;
-    }
-  }
-  return current_context;
-}
-}  // namespace
-
 const SecurityOrigin* GetEphemeralStorageOrigin(LocalDOMWindow* window) {
   auto* frame = window->GetFrame();
   if (!frame) {
@@ -79,11 +59,9 @@ const SecurityOrigin* GetEphemeralStorageOrigin(LocalDOMWindow* window) {
 }
 
 int LocalDOMWindow::outerWidth() const {
-  ExecutionContext* context =
-      GetContextFromOpenerIfPossible(opener(), GetExecutionContext());
-
   // Prevent fingerprinter use of outerWidth by returning a farbled value near
   // innerWidth instead:
+  ExecutionContext* context = GetExecutionContext();
   auto* top_window = DynamicTo<LocalDOMWindow>(top());
   return BlockScreenFingerprinting(context) && top_window
              ? FarbleInteger(context, brave::FarbleKey::kWindowInnerWidth,
@@ -92,11 +70,9 @@ int LocalDOMWindow::outerWidth() const {
 }
 
 int LocalDOMWindow::outerHeight() const {
-  ExecutionContext* context =
-      GetContextFromOpenerIfPossible(opener(), GetExecutionContext());
-
   // Prevent fingerprinter use of outerHeight by returning a farbled value near
   // innerHeight instead:
+  ExecutionContext* context = GetExecutionContext();
   auto* top_window = DynamicTo<LocalDOMWindow>(top());
   return BlockScreenFingerprinting(context) && top_window
              ? FarbleInteger(context, brave::FarbleKey::kWindowInnerHeight,
@@ -105,20 +81,16 @@ int LocalDOMWindow::outerHeight() const {
 }
 
 int LocalDOMWindow::screenX() const {
-  ExecutionContext* context =
-      GetContextFromOpenerIfPossible(opener(), GetExecutionContext());
-
   // Prevent fingerprinter use of screenX, screenLeft by returning value near 0:
+  ExecutionContext* context = GetExecutionContext();
   return BlockScreenFingerprinting(context)
              ? FarbleInteger(context, brave::FarbleKey::kWindowScreenX, 0, 0, 8)
              : screenX_ChromiumImpl();
 }
 
 int LocalDOMWindow::screenY() const {
-  ExecutionContext* context =
-      GetContextFromOpenerIfPossible(opener(), GetExecutionContext());
-
   // Prevent fingerprinter use of screenY, screenTop by returning value near 0:
+  ExecutionContext* context = GetExecutionContext();
   return BlockScreenFingerprinting(context)
              ? FarbleInteger(context, brave::FarbleKey::kWindowScreenY, 0, 0, 8)
              : screenY_ChromiumImpl();
@@ -127,8 +99,7 @@ int LocalDOMWindow::screenY() const {
 void LocalDOMWindow::resizeTo(int width,
                               int height,
                               ExceptionState& exception_state) const {
-  ExecutionContext* context =
-      GetContextFromOpenerIfPossible(opener(), GetExecutionContext());
+  ExecutionContext* context = GetExecutionContext();
   if (BlockScreenFingerprinting(context)) {
     resizeTo_ChromiumImpl(width + outerWidth_ChromiumImpl() - outerWidth(),
                           height + outerHeight_ChromiumImpl() - outerHeight(),
@@ -139,8 +110,7 @@ void LocalDOMWindow::resizeTo(int width,
 }
 
 void LocalDOMWindow::moveTo(int x, int y) const {
-  ExecutionContext* context =
-      GetContextFromOpenerIfPossible(opener(), GetExecutionContext());
+  ExecutionContext* context = GetExecutionContext();
   if (BlockScreenFingerprinting(context)) {
     moveTo_ChromiumImpl(x + screenX_ChromiumImpl() - screenX(),
                         y + screenY_ChromiumImpl() - screenY());
