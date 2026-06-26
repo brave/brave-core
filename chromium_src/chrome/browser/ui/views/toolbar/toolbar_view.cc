e7d30aacd64a4040dcf7be1f01a336fc35053b15
Claudio DeSouza <cdesouza@brave.com>
2026-06-26 16:58:39 +0100

    [cr151][plaster] `ToolbarView` 🩹🩹🩹🩹
    This change migrates `ToolbarView` customisations over onto `plaster` to
    avoid some of the preemptive replacement issues we were seeing.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/3c8c07208139bdbd1b1d945e19d05beea1aa4827
    
    commit 3c8c07208139bdbd1b1d945e19d05beea1aa4827
    Author: Ryan Sultanem <rsult@google.com>
    Date:   Thu Jun 25 08:08:42 2026 -0700
    
        [AvatarButton] Make state_maanger_ a stack variable
    
        Allows to simplify checks and reduce ambiguity for usages.
    
        Change-Id: I3349f4acb2e42c5339126a11042072c0cbcb0199
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8003760
        Commit-Queue: Ryan Sultanem <rsult@google.com>
        Reviewed-by: David Roger <droger@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1652431}

--- chromium_src/chrome/browser/ui/views/toolbar/toolbar_view.cc
+++ chromium_src/chrome/browser/ui/views/toolbar/toolbar_view.cc
@@ -3,26 +3,22 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// BraveAvatarToolbarButton and BraveBrowserAppMenuButton are substituted into
+// ToolbarView::Init() in place of their upstream base classes via plaster, see
+// brave/rewrite/chrome/browser/ui/views/toolbar/toolbar_view.cc.yaml
 #include "brave/browser/ui/views/profiles/brave_avatar_toolbar_button.h"
 #include "brave/browser/ui/views/toolbar/brave_browser_app_menu_button.h"
+#include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "extensions/buildflags/buildflags.h"
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
 #include "brave/browser/ui/views/location_bar/brave_location_bar_view.h"
-#include "chrome/browser/ui/views/location_bar/location_bar_view.h"
-
-#define LocationBarView BraveLocationBarView
 #endif
 
-#define BrowserAppMenuButton BraveBrowserAppMenuButton
-#define BRAVE_TOOLBAR_VIEW_INIT                                                \
-  avatar_ =                                                                    \
-      AddChildView(std::make_unique<BraveAvatarToolbarButton>(browser_view_)); \
-  if (false)
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+using LocationBarViewAlias = BraveLocationBarView;
+#else
+using LocationBarViewAlias = LocationBarView;
+#endif
 
 #include <chrome/browser/ui/views/toolbar/toolbar_view.cc>
-#undef BRAVE_TOOLBAR_VIEW_INIT
-#undef BrowserAppMenuButton
-#if BUILDFLAG(ENABLE_EXTENSIONS)
-#undef LocationBarView
-#endif
