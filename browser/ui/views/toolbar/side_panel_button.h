12ba4beb8619f42b4a05901a2afb21d97bf3a506
Simon Hong <shong@brave.com>
2026-07-01 11:07:55 +0900

    [Refactor] Decouple SidePanelButton and SidebarContainerView (#37631)
    Resolves no issue - refactoring w/o behavior change.
    
    SidebarContainerView doesn't need to know about button.
    Button can update its state by itself. Updated code is covered by existing test.
    
    This change is prerequisite for fixing side panel toolbar highlight is cleared after theme change.
    (brave/brave-browser#51462).
    
    TEST=SidebarBrowserTest.ToolbarButtonPinning

--- browser/ui/views/toolbar/side_panel_button.h
+++ browser/ui/views/toolbar/side_panel_button.h
@@ -7,20 +7,31 @@
 #define BRAVE_BROWSER_UI_VIEWS_TOOLBAR_SIDE_PANEL_BUTTON_H_
 
 #include "base/memory/raw_ref.h"
+#include "brave/components/sidebar/browser/sidebar_service.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_button.h"
 #include "components/prefs/pref_member.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 
-class Browser;
+class PrefService;
 
-class SidePanelButton : public ToolbarButton {
+namespace sidebar {
+class SidebarController;
+}  // namespace sidebar
+
+class SidePanelButton : public ToolbarButton,
+                        public sidebar::SidebarService::Observer {
   METADATA_HEADER(SidePanelButton, ToolbarButton)
 
  public:
-  explicit SidePanelButton(Browser* browser);
+  SidePanelButton(sidebar::SidebarController* sidebar_controller,
+                  PrefService* prefs);
   SidePanelButton(const SidePanelButton&) = delete;
   SidePanelButton& operator=(const SidePanelButton&) = delete;
   ~SidePanelButton() override;
+
+  // SidebarService::Observer:
+  void OnShowSidebarOptionChanged(
+      sidebar::SidebarService::ShowSidebarOption option) override;
 
  private:
   void ButtonPressed();
@@ -30,11 +41,17 @@ class SidePanelButton : public ToolbarButton {
   // the default vector icon is used. When the side panel should open to the
   // left side of the browser the flipped vector icon is used.
   void UpdateToolbarButtonIcon();
+  void UpdateButtonVisibility();
+  void UpdateButtonHighlight();
 
-  const raw_ref<Browser> browser_;
+  const raw_ref<sidebar::SidebarController> sidebar_controller_;
 
   // Observes and listens to side panel alignment changes.
   BooleanPrefMember sidebar_alignment_;
+  BooleanPrefMember show_side_panel_button_;
+  base::ScopedObservation<sidebar::SidebarService,
+                          sidebar::SidebarService::Observer>
+      sidebar_service_observation_{this};
 };
 
 #endif  // BRAVE_BROWSER_UI_VIEWS_TOOLBAR_SIDE_PANEL_BUTTON_H_
