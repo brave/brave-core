1ad0c8df9cc580d3467dba352fd90d28b5790c46
Sangwoo Ko <sangwoo108@gmail.com>
2026-07-07 07:58:42 +0900

    Enable screenshot feature by default (#37717)
    This enables the flag by default but hide the button via pref.
    The button will be shown when the user enables it via customize toolbar
    menu.

--- browser/brave_profile_prefs.cc
+++ browser/brave_profile_prefs.cc
@@ -418,7 +418,7 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   // appearance
   registry->RegisterBooleanPref(kShowBookmarksButton, true);
   registry->RegisterBooleanPref(kShowSidePanelButton, true);
-  registry->RegisterBooleanPref(kShowScreenshotButton, true);
+  registry->RegisterBooleanPref(kShowScreenshotButton, false);
   registry->RegisterBooleanPref(kLocationBarIsWide, false);
   registry->RegisterBooleanPref(kMRUCyclingEnabled, false);
   registry->RegisterBooleanPref(kTabsSearchShow, true);
