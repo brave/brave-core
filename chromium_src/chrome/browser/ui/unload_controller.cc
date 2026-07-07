8ad041dd87c440d78246eca95364c4538b99a919
Claudio DeSouza <cdesouza@brave.com>
2026-07-07 23:23:57 +0100

    [cr152] `Browser` close wrappers now on `UnloadController`
    The overrides have now been replaced with much simpler plasters.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/08cc46b60952b361ccb034e4b12950232bbf4dcf
    
    commit 08cc46b60952b361ccb034e4b12950232bbf4dcf
    Author: Thomas Lukaszewicz <tluk@chromium.org>
    Date:   Sun Jul 5 22:57:58 2026 -0700
    
        Migrate Browser close wrappers to UnloadController
    
        This CL factors out the following Browser window-closing methods
        into UnloadController:
    
         - `TryToCloseWindow`,
         - `ResetTryToCloseWindow`
         - `IsAttemptingToCloseBrowser`
         - `HandleBeforeClose`
         - `OnWindowClosing`
    
        Bug: 496674143
        Change-Id: Ia3f2a57e523455a5299713ed146f7e2fc33ebed4
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8017007
        Reviewed-by: Qikai Zhong <qikaizhong@microsoft.com>
        Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
        Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1656985}

--- chromium_src/chrome/browser/ui/unload_controller.cc
+++ chromium_src/chrome/browser/ui/unload_controller.cc
@@ -5,8 +5,30 @@
 
 #include "chrome/browser/ui/unload_controller.h"
 
+#include "brave/browser/ui/tabs/shared_pinned_tab_service.h"
+#include "brave/browser/ui/tabs/shared_pinned_tab_service_factory.h"
 #include "brave/components/constants/pref_names.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/tabs/features.h"
 #include "components/prefs/pref_service.h"
 
+// SharedPinnedTabService must get a chance to detach/cache shared pinned tabs
+// before CloseAllTabs() runs, otherwise they close like ordinary tabs.
+#define BRAVE_UNLOAD_CONTROLLER_ON_WINDOW_CLOSING                             \
+  if (base::FeatureList::IsEnabled(tabs::kBraveSharedPinnedTabs)) {           \
+    auto* shared_pinned_tab_service =                                         \
+        SharedPinnedTabServiceFactory::GetForProfile(browser_->GetProfile()); \
+    if (shared_pinned_tab_service) {                                          \
+      /* When there are only pinned tabs, OnWindowClosing() will be */        \
+      /* called again, after we detach all pinned tabs from the */            \
+      /* browser from shared_pinned_tab_service->BrowserClosing() */          \
+      if (shared_pinned_tab_service->BrowserClosing(                          \
+              browser_->GetTabStripModel())) {                                \
+        return;                                                               \
+      }                                                                       \
+    }                                                                         \
+  }
+
 #include <chrome/browser/ui/unload_controller.cc>
+
+#undef BRAVE_UNLOAD_CONTROLLER_ON_WINDOW_CLOSING
