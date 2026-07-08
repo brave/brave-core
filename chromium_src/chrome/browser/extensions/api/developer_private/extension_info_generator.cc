f40e4a546b2caae70ac02e65750a3a113cd398e9
brave-builds <45370463+brave-builds@users.noreply.github.com>
2026-07-08 15:26:18 +0900

    Silence 'This extension was turned off message' for our 4 supported MV2 extensions. (uplift to 1.93.x) (#37836)
    Uplift of #37753 (squashed) to beta

--- chromium_src/chrome/browser/extensions/api/developer_private/extension_info_generator.cc
+++ chromium_src/chrome/browser/extensions/api/developer_private/extension_info_generator.cc
@@ -3,7 +3,27 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+#include "chrome/common/extensions/api/developer_private.h"
+#include "extensions/common/constants.h"
+#include "extensions/common/extension_features.h"
 #include "extensions/common/manifest_handlers/incognito_info.h"
+
+namespace {
+
+void ProcessKnownMV2Extensions(
+    extensions::api::developer_private::ExtensionInfo& info) {
+  if (!extensions_mv2::features::IsExtensionReplacementEnabled()) {
+    return;
+  }
+  if (extensions_mv2::IsKnownWebStoreHostedExtension(info.id)) {
+    // Suppress mv2 messages for known extensions (which are being replaced with
+    // brave-hosted versions) on brave://extensions
+    info.is_affected_by_mv2_deprecation = false;
+    info.disable_reasons.unsupported_manifest_version = false;
+  }
+}
+
+}  // namespace
 
 #define BRAVE_CREATE_EXTENSION_INFO_HELPER \
   info.is_split_mode = IncognitoInfo::IsSplitMode(&extension);
