40f5a8f2d9003cd10e15636f9aa48a63f4a8a8ba
Kyle Hickinson <kylehickinson@users.noreply.github.com>
2026-07-08 02:36:35 -0400

    [iOS] Update iOS bootstrap and pre-action scripts to use node directly (#37804)
    This swaps out the `npm run` calls with direct `node` calls which also cleans up `npm` pathing requirements in the Xcode pre-action scripts. This also removes `ios_pack_js` from package.json as theres no reason to ever execute this manually

--- ios/brave-ios/scripts/xcode_scheme_preaction.sh
+++ ios/brave-ios/scripts/xcode_scheme_preaction.sh
@@ -9,29 +9,8 @@
 # $ACTION is empty for Xcode builds unfortunately
 # $RUN_CLANG_STATIC_ANALYZER is NO for builds, YES for cleans
 if [[ $RUN_CLANG_STATIC_ANALYZER = "NO" ]]; then
-  if ! command -v npm &> /dev/null; then
-    # Fixup PATH for users who didn't install node directly or use nvm
-    if [[ -s "$HOME/.nvm/nvm.sh" ]]; then
-      . "$HOME/.nvm/nvm.sh"
-    else
-        BREW='brew'
-        if [[ $(uname -p) == 'arm' ]]; then
-            # On Apple Silicon (M1, M2, M3) Brew is not part
-            # of the PATH.
-            BREW='/opt/homebrew/bin/brew'
-        fi
-      if [[ -x "$(command -v $BREW)" ]]; then
-        if [[ -s "$($BREW --prefix nvm)/nvm.sh" ]]; then
-          . "$($BREW --prefix nvm)/nvm.sh"
-        else
-          # Fixup PATH for brew users
-          eval "$($BREW shellenv)"
-        fi
-      fi
-    fi
-  fi
   # Do not inject Xcode build configs into the GN build
-  env -i PATH="$PATH" python3 \
+  env -i python3 \
     "${PROJECT_DIR}/../scripts/scheme_preaction.py" \
     --platform_name "$PLATFORM_NAME" \
     "$@"
