baab1d06baf021b1ee1149545451f13f95bb7b2b
Rohit Agarwal <ragarwal@brave.com>
2026-07-10 15:07:17 +0200

    update

--- components/brave_shields/core/browser/brave_shields_settings_service.h
+++ components/brave_shields/core/browser/brave_shields_settings_service.h
@@ -9,12 +9,12 @@
 #include "base/containers/span.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/raw_ref.h"
+#include "brave/components/brave_shields/core/common/brave_shield_constants.h"
 #include "brave/components/brave_shields/core/common/brave_shields_panel.mojom.h"
 #include "brave/components/brave_shields/core/common/farbling_prng.h"
 #include "brave/components/brave_shields/core/common/shields_settings.mojom.h"
 #include "components/content_settings/core/browser/cookie_settings.h"
 #include "components/keyed_service/core/keyed_service.h"
-#include "third_party/abseil-cpp/absl/random/random.h"
 
 class GURL;
 class HostContentSettingsMap;
@@ -25,8 +25,6 @@ class PrefRegistrySyncable;
 }
 
 namespace brave_shields {
-
-using FarblingPRNG = absl::random_internal::randen_engine<uint64_t>;
 
 class BraveShieldsSettingsService : public KeyedService {
  public:
