4b71e10b66722815e2d5ed62a6fe46814a88813f
brave-builds <brave-builds+devops@brave.com>
2026-07-10 10:59:43 +0000

    Uplift of #37883 (squashed) to release

--- chromium_src/extensions/browser/mv2_deprecation_impact_checker.cc
+++ chromium_src/extensions/browser/mv2_deprecation_impact_checker.cc
@@ -12,6 +12,7 @@
 #include "extensions/common/extension_features.h"
 #include "extensions/common/extension_id.h"
 #include "extensions/common/hashed_extension_id.h"
+#include "extensions/common/manifest.h"
 
 namespace {
 
@@ -35,6 +36,15 @@ std::string BuildBraveMV2ExceptionList() {
       BuildBraveMV2ExceptionList();                    \
   extensions_features::kExtensionManifestV2ExceptionListParam
 
+// We want to extend support for all MV2 extensions until August 31, 2026.
+// This forces IsExtensionAffected(...) to return false for all extensions
+// excluding those that have a Brave-hosted version.
+// Tracking: https://github.com/brave/brave-browser/issues/57050
+#define IsComponentLocation(...)      \
+  IsComponentLocation(__VA_ARGS__) || \
+      !extensions_mv2::IsKnownWebStoreHostedExtension(extension_id)
+
 #include <extensions/browser/mv2_deprecation_impact_checker.cc>
 
 #undef kExtensionManifestV2ExceptionListParam
+#undef IsComponentLocation
