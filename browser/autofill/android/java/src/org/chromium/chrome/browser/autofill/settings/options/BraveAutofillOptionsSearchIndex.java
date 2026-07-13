115c4591387e85b3060fbd459214cdf34bc7b936
AlexeyBarabash <alexey@brave.com>
2026-07-13 12:13:27 +0300

    [cr152][Android] Moved autofill/options => autofill/settings/options
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/25829d5b45397a4ae3d243ff77c468981493c39a
    
    commit 25829d5b45397a4ae3d243ff77c468981493c39a
    Author: Timofey Chudakov <tchudakov@google.com>
    Date:   Fri Jul 10 01:52:20 2026 -0700
    
        [AutofillAi][HoT] Move options/ and personal_context/ into settings/.
    
        This CL creates a settings/ directory and moves options/ and
        personal_context/ directories into it. This is done to mimic
        the chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/
        directory structure.
    
        Subsequent CLs will move the AutofillHelpMenuProvider into the
        newly created settings/ directory.
    
        Bypass-Check-License: Files were moved.
        Bug: 514254170
        Change-Id: I7e46337515a2b8604d00b6c4192565ba9d6fe8d8
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8063502
        Reviewed-by: Josef Raska <josefraska@google.com>
        Commit-Queue: Timofey Chudakov <tchudakov@google.com>
        Reviewed-by: Jan Keitel <jkeitel@google.com>
        Cr-Commit-Position: refs/heads/main@{#1660131}

--- browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/settings/options/BraveAutofillOptionsSearchIndex.java
+++ browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/settings/options/BraveAutofillOptionsSearchIndex.java
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-package org.chromium.chrome.browser.autofill.options;
+package org.chromium.chrome.browser.autofill.settings.options;
 
 import android.content.Context;
 import android.os.Bundle;
