04d94d46af4040ee8790454e71ced9704d3e80cf
Claudio DeSouza <cdesouza@brave.com>
2026-07-16 10:49:33 +0100

    [cr152] `browser_prefs.mm` 🩹🩹🩹🩹🩹

--- chromium_src/ios/chrome/browser/shared/model/prefs/browser_prefs.mm
+++ chromium_src/ios/chrome/browser/shared/model/prefs/browser_prefs.mm
@@ -13,30 +13,4 @@ void MigrateObsoleteProfilePrefs(PrefService* prefs);
 void MigrateObsoleteLocalStatePrefs(PrefService* prefs);
 }  // namespace brave
 
-#define BRAVE_REGISTER_BROWSER_STATE_PREFS \
-  brave::RegisterBrowserStatePrefs(registry);
-
-#define BRAVE_REGISTER_LOCAL_STATE_PREFS \
-  brave::RegisterLocalStatePrefs(registry);
-
-#define MigrateObsoleteProfilePrefs MigrateObsoleteProfilePrefs_ChromiumImpl
-
-#define MigrateObsoleteLocalStatePrefs \
-  MigrateObsoleteLocalStatePrefs_ChromiumImpl
-
 #include <ios/chrome/browser/shared/model/prefs/browser_prefs.mm>
-
-#undef MigrateObsoleteLocalStatePrefs
-#undef MigrateObsoleteProfilePrefs
-#undef BRAVE_REGISTER_LOCAL_STATE_PREFS
-#undef BRAVE_REGISTER_BROWSER_STATE_PREFS
-
-void MigrateObsoleteProfilePrefs(PrefService* prefs) {
-  MigrateObsoleteProfilePrefs_ChromiumImpl(prefs);
-  brave::MigrateObsoleteProfilePrefs(prefs);
-}
-
-void MigrateObsoleteLocalStatePrefs(PrefService* prefs) {
-  MigrateObsoleteLocalStatePrefs_ChromiumImpl(prefs);
-  brave::MigrateObsoleteLocalStatePrefs(prefs);
-}
