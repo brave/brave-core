c7fbd9464cbfbadbd191f452cdc2fb4f6da7ab8e
Douglas Daniel <douglashdaniel@gmail.com>
2026-07-17 16:15:59 -0500

    [Wallet]: Remove All Verified By DappRadar Labels (uplift to 1.93.x) (#38099)
    [Wallet]: Remove All Verified By DappRadar Labels (#38076)
    
    Removes all use of Verified by DappRadar labels and Icons

--- components/brave_wallet_ui/stories/locale.ts
+++ components/brave_wallet_ui/stories/locale.ts
@@ -1584,7 +1584,6 @@ provideStrings({
   braveWalletClearFilters: 'Clear filters',
   braveWalletShowMore: 'Show more',
   braveWalletDetails: 'Details',
-  braveWalletVerified: 'Verified by DappRadar',
 
   // ZCash
   braveWalletSwitchToShieldedAccount: 'Switch to a shielded account',
