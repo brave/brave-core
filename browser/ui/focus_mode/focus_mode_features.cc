70c2f0cea9753d198b8b3e3262f5d5bcbda25424
zenparsing <zenparsing@gmail.com>
2026-07-21 11:29:43 -0400

    [FocusMode] Enable flag by default

--- browser/ui/focus_mode/focus_mode_features.cc
+++ browser/ui/focus_mode/focus_mode_features.cc
@@ -21,12 +21,12 @@ constexpr auto kFocusModeUrlDisplayOptions =
 
 }  // namespace
 
-BASE_FEATURE(kBraveFocusMode, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kBraveFocusMode, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE_ENUM_PARAM(FocusModeUrlDisplay,
                         kFocusModeUrlDisplay,
                         &kBraveFocusMode,
-                        FocusModeUrlDisplay::kTitleBar,
+                        FocusModeUrlDisplay::kMiniToolbar,
                         kFocusModeUrlDisplayOptions);
 
 FocusModeUrlDisplay GetFocusModeUrlDisplay() {
