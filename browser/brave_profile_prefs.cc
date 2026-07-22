155e0a0dfe887a0686c5fa784e8435481c02f70c
Simon Hong <shong@brave.com>
2026-07-22 18:48:41 +0900

    Migrated brave tab search prefs to tab strip combo button (#38223)
    Resolves #56579 brave-browser#57348
    
    Migrated previous search button prefs to search combo button.
    Search combo button should be hidden when user turned off previous search button.
    F/U PR for #37488
    
    TEST=raveBrowserPrefsTest.TabSearchShow_*

--- browser/brave_profile_prefs.cc
+++ browser/brave_profile_prefs.cc
@@ -405,6 +405,9 @@ void RegisterProfilePrefsForMigration(
 
   // Added 2026-03
   registry->RegisterBooleanPref(kNewTabPageHideAllWidgets, false);
+
+  // Added 2026-06
+  registry->RegisterBooleanPref(kTabsSearchShow, false);
 }
 
 void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
@@ -424,7 +427,6 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterBooleanPref(kShowScreenshotButton, false);
   registry->RegisterBooleanPref(kLocationBarIsWide, false);
   registry->RegisterBooleanPref(kMRUCyclingEnabled, false);
-  registry->RegisterBooleanPref(kTabsSearchShow, false);
   registry->RegisterBooleanPref(kTabMuteIndicatorNotClickable, false);
 
   brave_sync::Prefs::RegisterProfilePrefs(registry);
