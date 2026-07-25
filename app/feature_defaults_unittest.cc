e066491bfd762bb9cf466a71b71cb82a0bfc8a4a
Claudio DeSouza <cdesouza@brave.com>
2026-07-25 08:28:00 +0100

    [cr152] `kPrivateAggregationApi` deleted
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/657f4f5c155c82cafe8e9813dd459ff140e94b61
    
    commit 657f4f5c155c82cafe8e9813dd459ff140e94b61
    Author: Alex Turner <alexmt@chromium.org>
    Date:   Thu Jul 23 05:31:17 2026 -0700
    
        Remove the kPrivateAggregationApi feature flag
    
        Following the removal of the Private Aggregation API, this CL removes
        the blink::features::kPrivateAggregationApi feature flag, which is no
        longer necessary. Also deletes its associated parameters, as well as its
        usage in tests and switch-dependent overrides.
    
        Bug: 531746235
        Change-Id: I844e9e189bb0e3466a2c425b4b1eb01cc435400e
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8128562
        Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
        Auto-Submit: Alex Turner <alexmt@chromium.org>
        Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1667011}

--- app/feature_defaults_unittest.cc
+++ app/feature_defaults_unittest.cc
@@ -122,7 +122,6 @@ TEST(FeatureDefaultsTest, DisabledFeatures) {
       &blink::features::kParakeet,
       &blink::features::kPrerender2,
       &blink::features::kPreloadingEagerViewportHeuristics,
-      &blink::features::kPrivateAggregationApi,
       &blink::features::kTranslationAPI,
       &blink::features::kUserMediaElement,
 #if BUILDFLAG(IS_ANDROID)
