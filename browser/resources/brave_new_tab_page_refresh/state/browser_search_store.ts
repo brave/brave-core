84855eb29712119bd4062020f7b55bb6549cc0f6
Claudio DeSouza <cdesouza@brave.com>
2026-07-25 17:21:27 +0100

    [cr152] Pass `SuggestInventory` to searchbox `queryAutocomplete`
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/c17517a6abf26e35eaa8cc76c2d6af69c8c01b26
    
    commit c17517a6abf26e35eaa8cc76c2d6af69c8c01b26
    Author: Wenhu Cheng <wenhuc@google.com>
    Date:   Thu Jul 23 17:51:00 2026 -0700
    
        [Searchbox] Consolidate 'QueryAutocompleteWithSuggestInventory' into 'QueryAutocomplete'
    
        Previously, the searchbox API exposed two separate methods for
        autocomplete queries: the standard method, and a specialized one that
        accepted a SuggestInventory argument. Keeping two parallel pathways for
        the same core action duplicated logic and made the API harder to
        maintain.
    
        We removed the redundant specialized autocomplete method and
        consolidated its functionality into the primary autocomplete interface.
        The main method has been enhanced to handle contextual information
        directly. As part of this cleanup, all front-end UI components and test
        files that relied on the deprecated method have been successfully
        updated to use the new unified interface.
    
        Fixed: b:532590607
        Change-Id: I378fdc3706a1e659a3f2434b91b70f263b96c688
        Bug: 532590607
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8134484
        Reviewed-by: Nihar Majmudar <niharm@google.com>
        Reviewed-by: Alex Gough <ajgo@chromium.org>
        Commit-Queue: Wenhu Cheng <wenhuc@google.com>
        Reviewed-by: Dhruv Kathpalia <dhruvkathpalia@google.com>
        Cr-Commit-Position: refs/heads/main@{#1667550}

--- browser/resources/brave_new_tab_page_refresh/state/browser_search_store.ts
+++ browser/resources/brave_new_tab_page_refresh/state/browser_search_store.ts
@@ -3,6 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+import { SuggestInventory } from 'chrome://resources/mojo/components/omnibox/browser/searchbox.mojom-webui.js'
+
 import { loadTimeData } from '$web-common/loadTimeData'
 import { SearchBoxProxy } from './search_box_proxy'
 import { NewTabPageProxy } from './new_tab_page_proxy'
@@ -216,6 +218,7 @@ export function createSearchStore() {
         query,
         false,
         query.length,
+        SuggestInventory.kDefault,
         query.length === 0,
       )
     },
