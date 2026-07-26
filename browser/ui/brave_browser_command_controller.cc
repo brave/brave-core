1647c316e22be271053a89f1c9cf73842af6ecbf
slesha aineni <ainenislesha3459@gmail.com>
2026-07-26 22:22:04 +0530

    Add commands for opening new tabs in containers

--- browser/ui/brave_browser_command_controller.cc
+++ browser/ui/brave_browser_command_controller.cc
@@ -370,9 +370,16 @@ void BraveBrowserCommandController::InitBraveCommandState() {
 #endif
 
 #if BUILDFLAG(ENABLE_CONTAINERS)
-  UpdateCommandEnabled(
-      IDC_NEW_TEMPORARY_CONTAINER,
-      ContainersServiceFactory::GetForProfile(browser_->profile()));
+  auto* containers_service = 
+	ContainersServiceFactory::GetForProfile(browser_->profile());
+
+  UpdateCommandEnabled(IDC_NEW_TEMPORARY_CONTAINER,
+		       containers_service != nullptr);
+
+  for (int id = IDC_NEW_TAB_IN_CONTAINER_1;
+      id <= IDC_NEW_TAB_IN_CONTAINER_9; ++id){
+    UpdateCommandEnabled(id, containers_service != nullptr);
+  }
 #endif
 
   if (browser_->is_type_normal()) {
@@ -800,6 +807,40 @@ bool BraveBrowserCommandController::ExecuteBraveCommandWithDisposition(
       break;
 #endif
 #if BUILDFLAG(ENABLE_CONTAINERS)
+    case IDC_NEW_TAB_IN_CONTAINER_1:
+    case IDC_NEW_TAB_IN_CONTAINER_2:
+    case IDC_NEW_TAB_IN_CONTAINER_3:
+    case IDC_NEW_TAB_IN_CONTAINER_4:
+    case IDC_NEW_TAB_IN_CONTAINER_5:
+    case IDC_NEW_TAB_IN_CONTAINER_6:
+    case IDC_NEW_TAB_IN_CONTAINER_7:
+    case IDC_NEW_TAB_IN_CONTAINER_8:
+    case IDC_NEW_TAB_IN_CONTAINER_9: {
+
+       int index = id - IDC_NEW_TAB_IN_CONTAINER_1;
+
+       auto* containers_service =
+	  ContainersServiceFactory::GetForProfile(browser_->profile());
+
+       if (!containers_service) {
+         break;
+       }
+
+       auto containers = containers_service->GetContainers();
+
+       if (index >= static_cast<int>(containers.size())) {
+	 break;
+       }
+
+       brave::OpenUrlInContainer(
+	   base::to_address(browser_),
+	   browser_->GetNewTabURL(),
+	   containers[index],
+	   /*is_link=*/false);
+
+       break;
+      }
+
     case IDC_NEW_TEMPORARY_CONTAINER:
       brave::CreateTemporaryContainerAndOpenUrl(base::to_address(browser_),
                                                 browser_->GetNewTabURL(),
