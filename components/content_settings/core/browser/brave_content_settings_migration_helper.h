84e0b7541e64a1665fd8cde1d380de3550878198
bridiverbot <287305585+bridiverbot@users.noreply.github.com>
2026-07-27 07:53:39 -0700

    Fix migration helper license URLs

--- components/content_settings/core/browser/brave_content_settings_migration_helper.h
+++ components/content_settings/core/browser/brave_content_settings_migration_helper.h
@@ -1,7 +1,7 @@
 /* Copyright (c) 2026 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #ifndef BRAVE_COMPONENTS_CONTENT_SETTINGS_CORE_BROWSER_BRAVE_CONTENT_SETTINGS_MIGRATION_HELPER_H_
 #define BRAVE_COMPONENTS_CONTENT_SETTINGS_CORE_BROWSER_BRAVE_CONTENT_SETTINGS_MIGRATION_HELPER_H_
