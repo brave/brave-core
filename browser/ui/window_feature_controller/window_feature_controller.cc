feed96940c8dd92e5bb66ae87f3c2cb9b84505ad
Simon Hong <shong@brave.com>
2026-07-28 07:58:11 +0900

    Added f/u issues

--- browser/ui/window_feature_controller/window_feature_controller.cc
+++ browser/ui/window_feature_controller/window_feature_controller.cc
@@ -5,6 +5,10 @@
 
 #include "chrome/browser/ui/layout_constants.h"
 
+// TODO(https://github.com/brave/brave-browser/issues/57601): The vertical
+// tabs check should be moved here instead of being done separately in
+// WindowFeatureController to avoid adding external dependency to upstream
+// target.
 bool BraveDisablesImmersiveFullscreenMode() {
   return tabs::UseCompactHorizontalTabs();
 }
