8ed97c8502a130d4990dd3e35b8e847189c9d26a
Claudio DeSouza <cdesouza@brave.com>
2026-07-28 20:52:22 +0100

    [plaster] Cancel `RankerURLFetcher` implementation
    This change stubs off `RankerURLFetcher` entirely, but it also corrects
    the stub to not drop the callback without calling it back.

--- chromium_src/components/assist_ranker/ranker_url_fetcher.cc
+++ chromium_src/components/assist_ranker/ranker_url_fetcher.cc
@@ -1,20 +1,27 @@
 /* Copyright (c) 2020 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * you can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "components/assist_ranker/ranker_url_fetcher.h"
 
-#define Request Request_ChromiumImpl
-#include <components/assist_ranker/ranker_url_fetcher.cc>
-#undef Request
+#include "services/network/public/cpp/simple_url_loader.h"
 
 namespace assist_ranker {
+
+RankerURLFetcher::RankerURLFetcher()
+    : state_(IDLE), retry_count_(0), max_retry_on_5xx_(0) {}
+
+RankerURLFetcher::~RankerURLFetcher() = default;
 
 bool RankerURLFetcher::Request(
     const GURL& url,
     RankerURLFetcher::Callback callback,
     network::mojom::URLLoaderFactory* url_loader_factory) {
+  // A false return means no request was started and `callback` will never
+  // run, which is already part of the documented contract (see the header):
+  // the sole caller, RankerModelLoaderImpl::StartLoadFromURL, checks this
+  // return value and finalizes its own state without waiting on `callback`.
   return false;
 }
 
