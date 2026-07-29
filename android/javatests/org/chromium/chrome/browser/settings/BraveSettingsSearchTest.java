8e4166ba558b4294c7a2c4ff6b97d83cface32eb
Oliver <oddin@brave.com>
2026-07-29 20:05:41 +0200

    [android] Secure directly invoked activities & allow screenshot toggle.
    We now ensure that if our activity handler is invoked before we have a
    profile, we invoke special handling that can cope with not yet having a
    profile by ensuring that we wait until it's ready.
    
    This should resolve the issue discovered by security where directly
    launching an activity via ADB would bypass the lock screen.
    Additionally, the redundant private tabs lock toggle has been removed.
    
    Additionally, we now allow the user to selectively enable/disable
    prevention of screenshotting/videoing the application. The toggle is
    only visible if either of the locking options are enabled. When the
    whole app is secured, no screenshots are permitted. Otherwise, just
    private tabs are protected.
    
    Tests have been added to verify behaviour.

--- android/javatests/org/chromium/chrome/browser/settings/BraveSettingsSearchTest.java
+++ android/javatests/org/chromium/chrome/browser/settings/BraveSettingsSearchTest.java
@@ -1135,9 +1135,6 @@ public class BraveSettingsSearchTest {
         clearAndTypeIntoSearch("Preload pages");
         assertSearchResultEmpty();
 
-        clearAndTypeIntoSearch("Lock Private tabs");
-        assertSearchResultEmpty();
-
         clearAndTypeIntoSearch("password was compromised");
         assertSearchResultEmpty();
 
