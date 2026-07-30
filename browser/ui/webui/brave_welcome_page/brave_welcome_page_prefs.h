e94a64c739318a076bee4ac3ebaf8c82cb1a08fb
zenparsing <zenparsing@gmail.com>
2026-07-30 14:09:59 -0400

    [WelcomeUI] Port kHasSeenBraveWelcomePage pref handling

--- browser/ui/webui/brave_welcome_page/brave_welcome_page_prefs.h
+++ browser/ui/webui/brave_welcome_page/brave_welcome_page_prefs.h
@@ -3,13 +3,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#ifndef BRAVE_BROWSER_UI_WEBUI_WELCOME_PAGE_BRAVE_WELCOME_UI_PREFS_H_
-#define BRAVE_BROWSER_UI_WEBUI_WELCOME_PAGE_BRAVE_WELCOME_UI_PREFS_H_
+#ifndef BRAVE_BROWSER_UI_WEBUI_BRAVE_WELCOME_PAGE_BRAVE_WELCOME_PAGE_PREFS_H_
+#define BRAVE_BROWSER_UI_WEBUI_BRAVE_WELCOME_PAGE_BRAVE_WELCOME_PAGE_PREFS_H_
 
 class PrefRegistrySimple;
 class PrefService;
 
-namespace brave::welcome_ui::prefs {
+namespace brave_welcome_page::prefs {
 
 inline constexpr char kHasSeenBraveWelcomePage[] =
     "brave.has_seen_brave_welcome_page";
@@ -17,6 +17,6 @@ inline constexpr char kHasSeenBraveWelcomePage[] =
 void RegisterProfilePrefs(PrefRegistrySimple* registry);
 void MigratePrefs(PrefService* prefs);
 
-}  // namespace brave::welcome_ui::prefs
+}  // namespace brave_welcome_page::prefs
 
-#endif  // BRAVE_BROWSER_UI_WEBUI_WELCOME_PAGE_BRAVE_WELCOME_UI_PREFS_H_
+#endif  // BRAVE_BROWSER_UI_WEBUI_BRAVE_WELCOME_PAGE_BRAVE_WELCOME_PAGE_PREFS_H_
