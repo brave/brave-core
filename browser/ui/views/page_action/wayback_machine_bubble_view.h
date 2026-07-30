6566e239c8f020fe394271484b1f1108a23d459d
Claudio DeSouza <cdesouza@brave.com>
2026-07-30 10:56:03 +0100

    [cr153] Wayback Machine page action migration
    Similar to the work done for the speedreader icon, this change migrates
    the wayback machine icon off `PageActionIconController` and onto
    `PageActionView/ActionItem`.

--- browser/ui/views/page_action/wayback_machine_bubble_view.h
+++ browser/ui/views/page_action/wayback_machine_bubble_view.h
@@ -3,36 +3,40 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#ifndef BRAVE_BROWSER_UI_VIEWS_WAYBACK_MACHINE_BUBBLE_VIEW_H_
-#define BRAVE_BROWSER_UI_VIEWS_WAYBACK_MACHINE_BUBBLE_VIEW_H_
+#ifndef BRAVE_BROWSER_UI_VIEWS_PAGE_ACTION_WAYBACK_MACHINE_BUBBLE_VIEW_H_
+#define BRAVE_BROWSER_UI_VIEWS_PAGE_ACTION_WAYBACK_MACHINE_BUBBLE_VIEW_H_
 
+#include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
+
+namespace actions {
+class ActionItem;
+}  // namespace actions
 
 namespace content {
 class WebContents;
 }  // namespace content
 
-class Browser;
-
 class WaybackMachineBubbleView : public views::BubbleDialogDelegateView {
   METADATA_HEADER(WaybackMachineBubbleView, views::BubbleDialogDelegateView)
 
  public:
-  static void Show(Browser* browser, views::View* anchor);
+  static void Show(content::WebContents* web_contents,
+                   views::View* anchor,
+                   actions::ActionItem* item);
 
   WaybackMachineBubbleView(base::WeakPtr<content::WebContents> web_contents,
-                           views::View* anchor);
+                           views::View* anchor,
+                           actions::ActionItem* item);
   ~WaybackMachineBubbleView() override;
-
-  // views::BubbleDialogDelegateView override;
-  void OnWidgetVisibilityChanged(views::Widget* widget, bool visible) override;
 
  private:
   void OnAccepted();
 
   base::WeakPtr<content::WebContents> web_contents_;
+  raw_ptr<actions::ActionItem> item_;
 };
 
-#endif  // BRAVE_BROWSER_UI_VIEWS_WAYBACK_MACHINE_BUBBLE_VIEW_H_
+#endif  // BRAVE_BROWSER_UI_VIEWS_PAGE_ACTION_WAYBACK_MACHINE_BUBBLE_VIEW_H_
