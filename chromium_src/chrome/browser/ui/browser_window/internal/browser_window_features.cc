d89d8475504595e6f1ce7ed9a301473851b0b0a6
Claudio DeSouza <cdesouza@brave.com>
2026-07-30 21:20:45 +0100

    [cr153] `WebContentsDelegate` removed from `Browser`
    This change introduces `BraveBrowserWebContentsDelegate`, as upstream
    has dropped `WebContentsDelegate` from `browser`, and introduced
    `BrowserWebContentsDelegate` into `BrowserWindowInterface`. Functionally
    things have just been moved about.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/0c24e8300c81c74132ac40e3dd986904660b6e3b
    
    commit 0c24e8300c81c74132ac40e3dd986904660b6e3b
    Author: Thomas Lukaszewicz <tluk@chromium.org>
    Date:   Tue Jul 28 19:03:20 2026 -0700
    
        Remove WebContentsDelegate inheritance in Browser
    
        All existing methods indirect to BrowserWebContentsDelegate, all
        existing clients already call this directly.
    
        There are no intended behavioral changes.
    
        Bug: 355026937
        Change-Id: Ife2f701ce707aade9dbea2b9fbbe716b7cfcdc0f
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8163031
        Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
        Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1669946}

--- chromium_src/chrome/browser/ui/browser_window/internal/browser_window_features.cc
+++ chromium_src/chrome/browser/ui/browser_window/internal/browser_window_features.cc
@@ -8,6 +8,7 @@
 #include "brave/browser/ui/brave_browser_actions.h"
 #include "brave/browser/ui/brave_browser_command_controller.h"
 #include "brave/browser/ui/brave_browser_content_setting_bubble_model_delegate.h"
+#include "brave/browser/ui/brave_browser_web_contents_delegate.h"
 #include "brave/browser/ui/tabs/public/vertical_tab_controller.h"
 #include "brave/browser/ui/toolbar/brave_location_bar_model_delegate.h"
 #include "brave/browser/ui/views/side_panel/brave_side_panel_coordinator.h"
@@ -28,9 +29,11 @@
 #define SidePanelCoordinator BraveSidePanelCoordinator
 #define BrowserActions BraveBrowserActions
 #define BrowserCommandController BraveBrowserCommandController
+#define BrowserWebContentsDelegate BraveBrowserWebContentsDelegate
 
 #include <chrome/browser/ui/browser_window/internal/browser_window_features.cc>
 
+#undef BrowserWebContentsDelegate
 #undef BrowserCommandController
 #undef BrowserActions
 #undef SidePanelCoordinator
