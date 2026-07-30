3e1ea45732a68eddbe2f8769c166aaa6adb628e9
cdesouza-chromium <cdesouza@brave.com>
2026-07-30 10:57:29 +0100

    [plaster] Various `chrome/browser/extensions/` 🩹🩹🩹🩹 (#38500)
    This change touches on several shadow files under
    `chrome/browser/extensions/`, rolling out the use of plaster files for
    all `#define` uses.
    
    Bug: https://github.com/brave/brave-browser/issues/45050

--- chromium_src/chrome/browser/extensions/extension_install_prompt.cc
+++ chromium_src/chrome/browser/extensions/extension_install_prompt.cc
@@ -6,16 +6,5 @@
 #include "chrome/browser/extensions/extension_install_prompt.h"
 
 #include "brave/grit/brave_generated_resources.h"
-#include "ui/base/l10n/l10n_util.h"
 
-#define GetDialogTitle GetDialogTitle_ChromiumImpl
 #include <chrome/browser/extensions/extension_install_prompt.cc>
-#undef GetDialogTitle
-
-std::u16string ExtensionInstallPrompt::Prompt::GetDialogTitle() const {
-  if (type_ == ExtensionInstallPrompt::INSTALL_PROMPT) {
-    return l10n_util::GetStringUTF16(
-        IDS_UNVETTED_EXTENSION_INSTALL_PROMPT_TITLE);
-  }
-  return GetDialogTitle_ChromiumImpl();
-}
