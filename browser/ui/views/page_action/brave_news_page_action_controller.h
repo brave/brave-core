afa9a54aecc3ff8143e13e7a5508987f8210499a
Claudio DeSouza <cdesouza@brave.com>
2026-07-31 16:02:53 +0100

    [cr153] `BraveNewsActionIconView` now on `PageActionController`
    `PageActionIconView` is now deleted, and this change replaces
    `BraveNewsActionIconView` with `BraveNewsPageActionController` driving
    `PageActionController`.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/2d676e5bdc6283ed1cb7f808bf71fab99512f944
    
    commit 2d676e5bdc6283ed1cb7f808bf71fab99512f944
    Author: Kaan Alsan <alsan@chromium.org>
    Date:   Thu Jul 30 15:02:49 2026 -0700
    
        Remove PageActionIconView and PageActionIconContainerView
    
        All legacy page action icons have been migrated to the new
        PageActionView framework. This CL removes PageActionIconView,
        PageActionIconContainerView, PageActionIconContainer,
        PageActionIconLoadingIndicatorView, PageActionIconParams, and
        PageActionIconViewObserver along with obsolete method declarations in
        ToolbarButtonProvider, BrowserWindow, and LocationBarView.
    
        Additional cleanups of no-op methods will be done in later CLs.
    
        TAG=agy CONV=e7ec7cb8-b5ad-472e-8f69-344a353d2ab2
    
        Bug: 532595265
        Change-Id: I2657dfdf38b0a486ef8ced2053d50a2e3dd28883
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8163642
        Reviewed-by: Darryl James <dljames@chromium.org>
        Commit-Queue: Kaan Alsan <alsan@chromium.org>
        Reviewed-by: Dmytro Yeroshkin <yeroshkin@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1671449}

---
+++ browser/ui/views/page_action/brave_news_page_action_controller.h
@@ -0,0 +1,75 @@
+// Copyright (c) 2026 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_BROWSER_UI_VIEWS_PAGE_ACTION_BRAVE_NEWS_PAGE_ACTION_CONTROLLER_H_
+#define BRAVE_BROWSER_UI_VIEWS_PAGE_ACTION_BRAVE_NEWS_PAGE_ACTION_CONTROLLER_H_
+
+#include <vector>
+
+#include "base/callback_list.h"
+#include "base/memory/raw_ref.h"
+#include "base/scoped_observation.h"
+#include "brave/browser/brave_news/brave_news_tab_helper.h"
+#include "chrome/browser/ui/page_action/page_action_controller.h"
+#include "components/prefs/pref_member.h"
+#include "components/tabs/public/tab_interface.h"
+#include "content/public/browser/web_contents_observer.h"
+
+class ToolbarButtonProvider;
+
+namespace actions {
+class ActionItem;
+}
+
+namespace page_actions {
+
+// Drives the Brave News page action: shows the icon when the current page has
+// an available news feed (colored differently when the user is already
+// subscribed to it), and shows the Brave News bubble when clicked.
+class BraveNewsPageActionController
+    : public BraveNewsTabHelper::PageFeedsObserver,
+      public content::WebContentsObserver {
+ public:
+  BraveNewsPageActionController(
+      tabs::TabInterface& tab,
+      page_actions::PageActionController& page_action_controller);
+  BraveNewsPageActionController(const BraveNewsPageActionController&) = delete;
+  BraveNewsPageActionController& operator=(
+      const BraveNewsPageActionController&) = delete;
+  ~BraveNewsPageActionController() override;
+
+  void Init();
+
+  void ExecuteAction(ToolbarButtonProvider* toolbar_button_provider,
+                     actions::ActionItem* item);
+
+  // BraveNewsTabHelper::PageFeedsObserver:
+  void OnAvailableFeedsChanged(const std::vector<GURL>& feeds) override;
+
+  // content::WebContentsObserver:
+  void WebContentsDestroyed() override;
+
+ private:
+  void AttachToTabHelper();
+  void UpdatePageAction();
+
+  const raw_ref<tabs::TabInterface> tab_;
+  const raw_ref<page_actions::PageActionControllerImpl> page_action_controller_;
+
+  base::CallbackListSubscription did_activate_subscription_;
+  base::CallbackListSubscription will_discard_contents_subscription_;
+
+  base::ScopedObservation<BraveNewsTabHelper,
+                          BraveNewsTabHelper::PageFeedsObserver>
+      page_feeds_observer_{this};
+
+  BooleanPrefMember should_show_;
+  BooleanPrefMember opted_in_;
+  BooleanPrefMember news_enabled_;
+};
+
+}  // namespace page_actions
+
+#endif  // BRAVE_BROWSER_UI_VIEWS_PAGE_ACTION_BRAVE_NEWS_PAGE_ACTION_CONTROLLER_H_
