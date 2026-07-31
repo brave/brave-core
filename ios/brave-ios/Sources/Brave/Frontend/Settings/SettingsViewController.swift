75ca507417914591fb60b2baad5eed6bd6127158
Szilard Szaloki <szilardszaloki@gmail.com>
2026-07-30 18:51:11 -0600

    Account: clean up Mojom error codes (#38640)

--- ios/brave-ios/Sources/Brave/Frontend/Settings/SettingsViewController.swift
+++ ios/brave-ios/Sources/Brave/Frontend/Settings/SettingsViewController.swift
@@ -745,8 +745,6 @@ class SettingsViewController: TableViewController, BraveAccountAuthenticationObs
           .BRAVE_ACCOUNT_REGISTER_TOO_MANY_VERIFICATIONS,
         .dailyVerificationLimitReachedForEmail:
           .BRAVE_ACCOUNT_DAILY_VERIFICATION_LIMIT_REACHED_FOR_EMAIL,
-        .verificationNotFoundOrInvalidIdOrCode:
-          .BRAVE_ACCOUNT_PASSWORD_RESET_VERIFICATION_NOT_FOUND_OR_INVALID_ID_OR_CODE,
         .emailAlreadyVerified:
           .BRAVE_ACCOUNT_PASSWORD_RESET_EMAIL_ALREADY_VERIFIED,
         .maximumCodeVerificationAttemptsExceeded:
