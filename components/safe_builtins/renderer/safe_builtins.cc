52a22e9547799ad9cd41aac98f899766a6e76d9b
Claudio DeSouza <cdesouza@brave.com>
2026-07-31 15:37:15 +0100

    [cr153] `WebScriptController::RegisterExtension` deleted
    This global registration mechanism is now deleted, so this change makes
    `SafeBuiltins` to compile and run its setup script directly.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/c8c5a6a954922aeea94f9f2f97e74b1180087d80
    
    commit c8c5a6a954922aeea94f9f2f97e74b1180087d80
    Author: Jochen Eisinger <jochen@chromium.org>
    Date:   Thu Jul 30 12:28:50 2026 -0700
    
        Delete V8 extensions registration infrastructure
    
        Remove legacy v8::Extension registration and loading pipeline end-to-end.
    
        TAG=agy
        CONV=9fc3b0ac-a815-428d-a2de-1f835441e8d4
    
        Bug: 40347662
        Change-Id: Ic72da940c2bb7410141b52e3410b2763a4b1aa30
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8131017
        Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
        Reviewed-by: Dominic Battré <battre@chromium.org>
        Commit-Queue: Jochen Eisinger <jochen@chromium.org>
        Reviewed-by: Camille Lamy <clamy@chromium.org>
        Reviewed-by: Kentaro Hara <haraken@chromium.org>
        Reviewed-by: Marc Treib <treib@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1671319}

--- components/safe_builtins/renderer/safe_builtins.cc
+++ components/safe_builtins/renderer/safe_builtins.cc
@@ -5,23 +5,23 @@
 
 #include "brave/components/safe_builtins/renderer/safe_builtins.h"
 
+#include <cstddef>
 #include <string>
+#include <tuple>
 
 #include "base/check.h"
 #include "base/containers/heap_array.h"
-#include "base/debug/crash_logging.h"
 #include "base/debug/dump_without_crashing.h"
 #include "third_party/abseil-cpp/absl/strings/str_format.h"
 #include "v8/include/v8-context.h"
 #include "v8/include/v8-exception.h"
-#include "v8/include/v8-extension.h"
 #include "v8/include/v8-function.h"
 #include "v8/include/v8-isolate.h"
 #include "v8/include/v8-microtask-queue.h"
 #include "v8/include/v8-object.h"
 #include "v8/include/v8-primitive-object.h"
 #include "v8/include/v8-primitive.h"
-#include "v8/include/v8-template.h"
+#include "v8/include/v8-script.h"
 
 namespace brave {
 
@@ -29,12 +29,12 @@ namespace {
 
 constexpr char kClassName[] = "brave::SafeBuiltins";
 
-// see //extensions/renderer/safe_builtins.cc for details
+// see //extensions/renderer/safe_builtins.cc for the JS-side algorithm this
+// mirrors. |Apply| and |Save| are passed in as ordinary function arguments and
+// this is compiled and run directly, once per context, by EnsureSetUp() below.
 constexpr char kScript[] =
-    "(function() {\n"
+    "(function(Apply, Save) {\n"
     "'use strict';\n"
-    "native function Apply();\n"
-    "native function Save();\n"
     "\n"
     "// Used in the callback implementation, could potentially be clobbered.\n"
     "function makeCallable(obj, target, isStatic, propertyNames) {\n"
@@ -83,7 +83,7 @@ constexpr char kScript[] =
     "            ['from', 'isArray', 'of']);\n"
     "Save('$', function (value) { return value; })\n"
     "\n"
-    "}());\n";
+    "})";
 
 // Converts |str| to a V8 string.
 // This crashes when strlen(str) > v8::String::kMaxLength.
@@ -142,91 +142,115 @@ v8::Local<v8::Object> Load(const char* name, v8::Local<v8::Context> context) {
   return v8::Local<v8::Object>::Cast(value);
 }
 
-class ExtensionImpl : public v8::Extension {
- public:
-  ExtensionImpl() : v8::Extension(kClassName, kScript) {}
+// The setup script always Save()s '$' last, so its presence indicates setup
+// has already run for |context|.
+bool HasBeenSetUp(v8::Local<v8::Context> context) {
+  v8::Local<v8::Value> value;
+  return context->Global()
+             ->GetPrivate(context, MakeKey("$", v8::Isolate::GetCurrent()))
+             .ToLocal(&value) &&
+         !value->IsUndefined();
+}
 
- private:
-  v8::Local<v8::FunctionTemplate> GetNativeFunctionTemplate(
-      v8::Isolate* isolate,
-      v8::Local<v8::String> name) override {
-    if (name->StringEquals(ToV8StringUnsafe(isolate, "Apply"))) {
-      return v8::FunctionTemplate::New(isolate, Apply);
-    }
-    if (name->StringEquals(ToV8StringUnsafe(isolate, "Save"))) {
-      return v8::FunctionTemplate::New(isolate, Save);
-    }
-    SCOPED_CRASH_KEY_STRING64(
-        "GetNativeFunctionTemplate", "name",
-        std::string(*v8::String::Utf8Value(isolate, name)));
-    base::debug::DumpWithoutCrashing();
-    return v8::Local<v8::FunctionTemplate>();
+void Apply(const v8::FunctionCallbackInfo<v8::Value>& info) {
+  CHECK(info.Length() == 5 &&
+        info[0]->IsFunction() &&  // function
+                                  // info[1] could be an object or a string
+        info[2]->IsObject() &&    // args
+        info[3]->IsInt32() &&     // first_arg_index
+        info[4]->IsInt32());      // args_length
+  v8::Local<v8::Context> context =
+      v8::Isolate::GetCurrent()->GetCurrentContext();
+  v8::MicrotasksScope microtasks(v8::Isolate::GetCurrent(),
+                                 context->GetMicrotaskQueue(),
+                                 v8::MicrotasksScope::kDoNotRunMicrotasks);
+  v8::Local<v8::Function> function = info[0].As<v8::Function>();
+  v8::Local<v8::Object> recv;
+  if (info[1]->IsObject()) {
+    recv = v8::Local<v8::Object>::Cast(info[1]);
+  } else if (info[1]->IsString()) {
+    recv = v8::StringObject::New(v8::Isolate::GetCurrent(),
+                                 v8::Local<v8::String>::Cast(info[1]))
+               .As<v8::Object>();
+  } else {
+    v8::Isolate::GetCurrent()->ThrowException(
+        v8::Exception::TypeError(ToV8StringUnsafe(
+            v8::Isolate::GetCurrent(),
+            "The first argument is the receiver and must be an object")));
+    return;
   }
+  v8::Local<v8::Object> args = v8::Local<v8::Object>::Cast(info[2]);
+  int first_arg_index = info[3].As<v8::Int32>()->Value();
+  int args_length = info[4].As<v8::Int32>()->Value();
 
-  static void Apply(const v8::FunctionCallbackInfo<v8::Value>& info) {
-    CHECK(info.Length() == 5 &&
-          info[0]->IsFunction() &&  // function
-                                    // info[1] could be an object or a string
-          info[2]->IsObject() &&    // args
-          info[3]->IsInt32() &&     // first_arg_index
-          info[4]->IsInt32());      // args_length
-    v8::Local<v8::Context> context =
-        v8::Isolate::GetCurrent()->GetCurrentContext();
-    v8::MicrotasksScope microtasks(v8::Isolate::GetCurrent(),
-                                   context->GetMicrotaskQueue(),
-                                   v8::MicrotasksScope::kDoNotRunMicrotasks);
-    v8::Local<v8::Function> function = info[0].As<v8::Function>();
-    v8::Local<v8::Object> recv;
-    if (info[1]->IsObject()) {
-      recv = v8::Local<v8::Object>::Cast(info[1]);
-    } else if (info[1]->IsString()) {
-      recv = v8::StringObject::New(v8::Isolate::GetCurrent(),
-                                   v8::Local<v8::String>::Cast(info[1]))
-                 .As<v8::Object>();
-    } else {
-      v8::Isolate::GetCurrent()->ThrowException(
-          v8::Exception::TypeError(ToV8StringUnsafe(
-              v8::Isolate::GetCurrent(),
-              "The first argument is the receiver and must be an object")));
+  int argc = args_length - first_arg_index;
+  auto argv = base::HeapArray<v8::Local<v8::Value>>::WithSize(argc);
+  for (int i = 0; i < argc; ++i) {
+    CHECK(IsTrue(args->Has(context, i + first_arg_index)));
+    // Getting a property value could throw an exception.
+    if (!GetProperty(context, args, i + first_arg_index, &argv[i])) {
       return;
     }
-    v8::Local<v8::Object> args = v8::Local<v8::Object>::Cast(info[2]);
-    int first_arg_index = info[3].As<v8::Int32>()->Value();
-    int args_length = info[4].As<v8::Int32>()->Value();
-
-    int argc = args_length - first_arg_index;
-    auto argv = base::HeapArray<v8::Local<v8::Value>>::WithSize(argc);
-    for (int i = 0; i < argc; ++i) {
-      CHECK(IsTrue(args->Has(context, i + first_arg_index)));
-      // Getting a property value could throw an exception.
-      if (!GetProperty(context, args, i + first_arg_index, &argv[i])) {
-        return;
-      }
-    }
-
-    v8::Local<v8::Value> return_value;
-    if (function->Call(context, recv, argc, argv.data())
-            .ToLocal(&return_value)) {
-      info.GetReturnValue().Set(return_value);
-    }
   }
 
-  static void Save(const v8::FunctionCallbackInfo<v8::Value>& info) {
-    CHECK(info.Length() == 2 && info[0]->IsString() && info[1]->IsObject());
-    SaveImpl(*v8::String::Utf8Value(v8::Isolate::GetCurrent(), info[0]),
-             info[1], v8::Isolate::GetCurrent()->GetCurrentContext());
+  v8::Local<v8::Value> return_value;
+  if (function->Call(context, recv, argc, argv.data()).ToLocal(&return_value)) {
+    info.GetReturnValue().Set(return_value);
   }
-};
+}
+
+void Save(const v8::FunctionCallbackInfo<v8::Value>& info) {
+  CHECK(info.Length() == 2 && info[0]->IsString() && info[1]->IsObject());
+  SaveImpl(*v8::String::Utf8Value(v8::Isolate::GetCurrent(), info[0]), info[1],
+           v8::Isolate::GetCurrent()->GetCurrentContext());
+}
+
+// Compiles and runs |kScript| directly in |context|, the first time a
+// SafeBuiltins is created for it, so that Load() below has something to find.
+void EnsureSetUp(v8::Local<v8::Context> context) {
+  if (HasBeenSetUp(context)) {
+    return;
+  }
+
+  v8::Isolate* const isolate = v8::Isolate::GetCurrent();
+  v8::Context::Scope context_scope(context);
+  v8::TryCatch try_catch(isolate);
+
+  v8::ScriptCompiler::Source source(ToV8StringUnsafe(isolate, kScript));
+  v8::Local<v8::Script> script;
+  if (!v8::ScriptCompiler::Compile(
+           context, &source, v8::ScriptCompiler::kNoCompileOptions,
+           v8::ScriptCompiler::NoCacheReason::kNoCacheBecauseInlineScript)
+           .ToLocal(&script)) {
+    base::debug::DumpWithoutCrashing();
+    return;
+  }
+
+  v8::Local<v8::Value> func_as_value;
+  if (!script->Run(context).ToLocal(&func_as_value) ||
+      !func_as_value->IsFunction()) {
+    base::debug::DumpWithoutCrashing();
+    return;
+  }
+
+  v8::Local<v8::Function> apply_fn =
+      v8::Function::New(context, Apply).ToLocalChecked();
+  v8::Local<v8::Function> save_fn =
+      v8::Function::New(context, Save).ToLocalChecked();
+  v8::Local<v8::Value> args[] = {apply_fn, save_fn};
+
+  v8::MicrotasksScope microtasks(isolate, context->GetMicrotaskQueue(),
+                                 v8::MicrotasksScope::kDoNotRunMicrotasks);
+  std::ignore = func_as_value.As<v8::Function>()->Call(
+      context, context->Global(), std::size(args), args);
+}
 
 }  // namespace
 
-// static
-std::unique_ptr<v8::Extension> SafeBuiltins::CreateV8Extension() {
-  return std::make_unique<ExtensionImpl>();
-}
-
 SafeBuiltins::SafeBuiltins(const v8::Local<v8::Context>& context)
-    : context_(context) {}
+    : context_(context) {
+  EnsureSetUp(context_);
+}
 
 SafeBuiltins::~SafeBuiltins() {}
 
