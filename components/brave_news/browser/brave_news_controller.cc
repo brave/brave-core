01f3da305ef7e0e2142eebbae249eaa1af7352e7
Jay Harris <jay.harris@outlook.co.nz>
2026-07-31 13:30:18 +1200

    [Brave News]: Wait for initial subscriptions before building the feed
    GetFollowingFeed, GetChannelFeed and GetPublisherFeed didn't wait for
    |initialization_promise_| the way GetFeed and GetFeedV2 do. When the user
    opts in from the NTP the frontend requests the feed immediately, before
    the async seeding of the "Top Sources" subscription (which needs a
    network round trip to determine the locale) has finished. The feed was
    therefore built from an empty set of subscriptions and reported back as
    FeedV2Error::NoFeeds, so the UI showed "Your Feed is Empty" until the
    page was reloaded.
    
    This became reachable for every new user in 1.94 when the following feed
    became the default feed view.
    
    Also adds the missing IsEnabled() checks to these methods and to
    GetFeedV2 - without them, calling into initialization_promise_ while
    Brave News is disabled hits a CHECK.
    
    Fixes https://github.com/brave/brave-browser/issues/57691

--- components/brave_news/browser/brave_news_controller.cc
+++ components/brave_news/browser/brave_news_controller.cc
@@ -214,18 +214,61 @@ void BraveNewsController::GetFeed(GetFeedCallback callback) {
 
 void BraveNewsController::GetFollowingFeed(GetFollowingFeedCallback callback) {
   DVLOG(1) << __FUNCTION__;
+  if (!pref_manager_.IsEnabled()) {
+    std::move(callback).Run(mojom::FeedV2::New());
+    return;
+  }
+
+  // Note: If we've only recently opted-in but we haven't yet finished adding
+  // the top sources subscription (via the async functions in MaybeInitPrefs),
+  // we need to wait for that to complete. Otherwise we'd build the feed from
+  // an empty set of subscriptions and report it as empty to the UI, which
+  // wouldn't be corrected until the feed was requested again.
+  if (!initialization_promise_.complete()) {
+    initialization_promise_.OnceInitialized(
+        base::BindOnce(&BraveNewsController::GetFollowingFeed,
+                       weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
+    return;
+  }
+
   IN_ENGINE(GetFollowingFeed, std::move(callback));
 }
 
 void BraveNewsController::GetChannelFeed(const std::string& channel,
                                          GetChannelFeedCallback callback) {
   DVLOG(1) << __FUNCTION__;
+  if (!pref_manager_.IsEnabled()) {
+    std::move(callback).Run(mojom::FeedV2::New());
+    return;
+  }
+
+  // See the note in GetFollowingFeed.
+  if (!initialization_promise_.complete()) {
+    initialization_promise_.OnceInitialized(base::BindOnce(
+        &BraveNewsController::GetChannelFeed, weak_ptr_factory_.GetWeakPtr(),
+        channel, std::move(callback)));
+    return;
+  }
+
   IN_ENGINE(GetChannelFeed, std::move(callback), channel);
 }
 
 void BraveNewsController::GetPublisherFeed(const std::string& publisher_id,
                                            GetPublisherFeedCallback callback) {
   DVLOG(1) << __FUNCTION__;
+  if (!pref_manager_.IsEnabled()) {
+    std::move(callback).Run(mojom::FeedV2::New());
+    return;
+  }
+
+  // See the note in GetFollowingFeed.
+  if (!initialization_promise_.complete()) {
+    initialization_promise_.OnceInitialized(base::BindOnce(
+        &BraveNewsController::GetPublisherFeed, weak_ptr_factory_.GetWeakPtr(),
+        publisher_id, std::move(callback)));
+    return;
+  }
+
   IN_ENGINE(GetPublisherFeed, std::move(callback), publisher_id);
 }
 
@@ -259,9 +302,12 @@ void BraveNewsController::EnsureFeedV2IsUpdating() {
 
 void BraveNewsController::GetFeedV2(GetFeedV2Callback callback) {
   DVLOG(1) << __FUNCTION__;
-  // If we're only recently opted-in but we haven't yet finished adding the
-  // top sources subscription (via the async functions in MaybeInitPrefs), we
-  // need to wait for that to complete before we can fetch the feed.
+  if (!pref_manager_.IsEnabled()) {
+    std::move(callback).Run(mojom::FeedV2::New());
+    return;
+  }
+
+  // See the note in GetFollowingFeed.
   if (!initialization_promise_.complete()) {
     initialization_promise_.OnceInitialized(
         base::BindOnce(&BraveNewsController::GetFeedV2,
