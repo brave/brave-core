371b71f6da15d37d470483e900418a20f3acf09d
Claudio DeSouza <cdesouza@brave.com>
2026-08-04 17:31:43 +0100

    [cr153] Browser::browser_window_features()` deleted
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/ded012e952b7ad7db3495ee5764446b13cba3301
    
    commit ded012e952b7ad7db3495ee5764446b13cba3301
    Author: Thomas Lukaszewicz <tluk@chromium.org>
    Date:   Mon Aug 3 10:45:11 2026 -0700
    
        Replace browser_window_features() with GetFeatures()
    
        Removes Browser::browser_window_features() and replaces all existing usage with BrowserWindowInterface::GetFeatures()
        instead, adjusting pointer dereferences to reference member
        accesses.
    
        Calls to GetBrowserForMigrationOnly() for the sole purpose of
        fetching the features pointer have been removed since
        GetFeatures() is directly available on BrowserWindowInterface.
    
        TAG=agy
        CONV=5f705218-157d-46d9-a669-907cb57df1f7
    
        Bug: 496674143
        Change-Id: Idf234713faaa1602515d21f138c14db62ff41b3b
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8184640
        Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
        Reviewed-by: Fred Shih <ffred@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1672764}

--- browser/ui/views/frame/brave_browser_frame_view_mac.mm
+++ browser/ui/views/frame/brave_browser_frame_view_mac.mm
@@ -246,7 +246,7 @@ int BraveBrowserFrameViewMac::NonClientHitTest(const gfx::Point& point) {
   auto* browser = browser_view->browser();
   if (!ImmersiveModeController::From(browser)->IsEnabled()) {
     auto* non_client_hit_test_helper =
-        browser->browser_window_features()->brave_non_client_hit_test_helper();
+        browser->GetFeatures().brave_non_client_hit_test_helper();
     if (auto res =
             non_client_hit_test_helper->NonClientHitTest(browser_view, point);
         res != HTNOWHERE) {
