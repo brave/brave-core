af199931f8233c2e990f5c0f26f1d3b1890126d4
boocmp <boocmp@yandex.ru>
2026-08-05 19:26:11 +0700

    Fixed upstream browser test.

--- chromium_src/chrome/browser/ssl/https_upgrades_browsertest.cc
+++ chromium_src/chrome/browser/ssl/https_upgrades_browsertest.cc
@@ -3,7 +3,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+#include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
+#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/ssl/https_upgrades_navigation_throttle.h"
+#include "chrome/common/chrome_features.h"
 #include "net/dns/dns_http_attempt.h"
 #include "url/gurl.h"
 
@@ -11,6 +15,19 @@ namespace {
 // The same as in chromium_src/.../https_only_mode_blocking_page.cc
 constexpr char kBraveLearnMoreLink[] =
     "https://support.brave.app/hc/en-us/articles/15513090104717";
+
+base::FieldTrialParams ChangeParameters(const base::Feature& feature,
+                                        const base::FieldTrialParams& params) {
+  if (&feature != &features::kHttpsUpgrades) {
+    return params;
+  }
+
+  base::FieldTrialParams result = params;
+  result["fallback-delay"] = "100ms";
+  result["ask-before-http-fallback-delay"] = "100ms";
+  return result;
+}
+
 }  // namespace
 
 // We changed the `Learn more` link on the interstitial page. It checks the link
@@ -23,7 +40,13 @@ constexpr char kBraveLearnMoreLink[] =
 #define set_timeout_for_testing(timeout) \
   set_timeout_for_testing(timeout.is_zero() ? base::Seconds(1) : timeout)
 
+// Also adjusts timeouts as for set_timeout_for_testing.
+#define InitAndEnableFeatureWithParameters(feature, ...) \
+  InitAndEnableFeatureWithParameters(feature,            \
+                                     ChangeParameters(feature, __VA_ARGS__))
+
 #include <chrome/browser/ssl/https_upgrades_browsertest.cc>
 
 #undef GetQuery
 #undef set_timeout_for_testing
+#undef InitAndEnableFeatureWithParameters
