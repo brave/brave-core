9dc41d0ace55c33b589352dad1207190554426ab
Arham Wani <arhamwani765@gmail.com>
2026-08-07 01:50:27 +0530

    Add sharing hub plaster regression fixture

---
+++ tools/cr/test/plasters/sharing_hub_model-expected.cc
@@ -0,0 +1,12 @@
+namespace sharing_hub {
+
+void SharingHubModel::PopulateFirstPartyActions() {
+  MaybeAddCopyCleanLinkItem(context_, &first_party_action_list_);
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  first_party_action_list_.emplace_back(
+      IDC_COPY_URL, l10n_util::GetStringUTF16(IDS_SHARING_HUB_COPY_LINK_LABEL),
+      &kCopyOldIcon, "SharingHubDesktop.CopyURLSelected", IDS_LINK_COPIED);
+}
+
+}  // namespace sharing_hub
