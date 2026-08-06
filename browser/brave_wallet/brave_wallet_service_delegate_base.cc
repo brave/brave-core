49d6e20e4864a6639ba78df04a32b6fca44e7886
Kyle Den Hartog <kdenhartog@users.noreply.github.com>
2026-08-07 09:28:35 +1200

    remove unnecessary header

--- browser/brave_wallet/brave_wallet_service_delegate_base.cc
+++ browser/brave_wallet/brave_wallet_service_delegate_base.cc
@@ -7,7 +7,6 @@
 
 #include "base/auto_reset.h"
 #include "base/command_line.h"
-#include "base/logging.h"
 #include "brave/components/brave_wallet/browser/brave_wallet_constants.h"
 #include "brave/components/brave_wallet/browser/permission_utils.h"
 #include "brave/components/permissions/contexts/brave_wallet_permission_context.h"
