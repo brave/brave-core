9ed7d4bbe372f36951dcf599214fe005c4631efb
Max Karolinskiy <max@brave.com>
2026-08-06 13:34:47 -0400

    [cr153] Browser::GetBrowserView was removed.
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/71becf581ef0957b5110d7d19b7bcc7754acde2e
    
    commit 71becf581ef0957b5110d7d19b7bcc7754acde2e
    Author: Thomas Lukaszewicz <tluk@chromium.org>
    Date:   Wed Aug 5 13:58:36 2026 -0700
    
        Reland "[BrowserView] Remove Browser::GetBrowserView() (CL 6/6)"
    
        This is a reland of commit 5607b1a480460397711a168d2291d6e97530db4b
    
        Reverted due to unrelated failures. No changes from original.
    
        Original change's description:
        > [BrowserView] Remove Browser::GetBrowserView() (CL 6/6)
        >
        > Remove Browser::GetBrowserView() from Browser class now that all callsites have been migrated to use static BrowserView::GetBrowserViewForBrowser().
        >
        > TAG=agy
        > CONV=bf94a0e1-cbc7-42b7-9eb6-f3392e3a906f
        >
        > Bug: 496674143
        > Change-Id: If396d415167cb871aa141b30663499eb45534599
        > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8190574
        > Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
        > Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
        > Cr-Commit-Position: refs/heads/main@{#1673858}
    
        Bug: 496674143
        Change-Id: I55d6398fe15cd229abbf79b4ed0159b5f4d3dc7e
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8206164
        Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
        Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1674480}

--- browser/containers/containers_browsertest.cc
+++ browser/containers/containers_browsertest.cc
@@ -1562,8 +1562,8 @@ IN_PROC_BROWSER_TEST_F(ContainersBrowserTest, ShouldShowTabAccent) {
   auto animation_resetter = gfx::AnimationTestApi::SetRichAnimationRenderMode(
       gfx::Animation::RichAnimationRenderMode::FORCE_DISABLED);
   auto* tab_strip_model = browser()->tab_strip_model();
-  auto* tab_strip =
-      browser()->GetBrowserView().horizontal_tab_strip_for_testing();
+  auto* tab_strip = BrowserView::GetBrowserViewForBrowser(browser())
+                        ->horizontal_tab_strip_for_testing();
   ASSERT_FALSE(tab_strip->ShouldPaintTabAccent(tab_strip->tab_at(0)));
 
   const GURL url("https://a.test/simple.html");
@@ -1608,8 +1608,8 @@ IN_PROC_BROWSER_TEST_F(ContainersBrowserTest, SmallAccentIconViewVisibility) {
   auto animation_resetter = gfx::AnimationTestApi::SetRichAnimationRenderMode(
       gfx::Animation::RichAnimationRenderMode::FORCE_DISABLED);
   auto* tab_strip_model = browser()->tab_strip_model();
-  auto* tab_strip =
-      browser()->GetBrowserView().horizontal_tab_strip_for_testing();
+  auto* tab_strip = BrowserView::GetBrowserViewForBrowser(browser())
+                        ->horizontal_tab_strip_for_testing();
 
   const GURL url("https://a.test/simple.html");
   auto container = containers::mojom::Container::New();
@@ -1662,8 +1662,8 @@ IN_PROC_BROWSER_TEST_F(ContainersBrowserTest,
   auto animation_resetter = gfx::AnimationTestApi::SetRichAnimationRenderMode(
       gfx::Animation::RichAnimationRenderMode::FORCE_DISABLED);
   auto* tab_strip_model = browser()->tab_strip_model();
-  auto* tab_strip =
-      browser()->GetBrowserView().horizontal_tab_strip_for_testing();
+  auto* tab_strip = BrowserView::GetBrowserViewForBrowser(browser())
+                        ->horizontal_tab_strip_for_testing();
 
   const GURL url("https://a.test/simple.html");
   auto container = containers::mojom::Container::New();
@@ -1826,9 +1826,8 @@ IN_PROC_BROWSER_TEST_F(ContainersBrowserTest,
 
   IconLabelBubbleView* partitioned_storage_view =
       page_actions::GetIconLabelBubbleViewForTesting(
-          browser()
-              ->GetBrowserView()
-              .toolbar_button_provider()
+          BrowserView::GetBrowserViewForBrowser(browser())
+              ->toolbar_button_provider()
               ->GetPageActionViewInterface(kActionShowPartitionedStorage),
           kActionShowPartitionedStorage);
   ASSERT_NE(nullptr, partitioned_storage_view);
@@ -1872,9 +1871,8 @@ IN_PROC_BROWSER_TEST_F(
 
   IconLabelBubbleView* const partitioned_storage_view =
       page_actions::GetIconLabelBubbleViewForTesting(
-          browser()
-              ->GetBrowserView()
-              .toolbar_button_provider()
+          BrowserView::GetBrowserViewForBrowser(browser())
+              ->toolbar_button_provider()
               ->GetPageActionViewInterface(kActionShowPartitionedStorage),
           kActionShowPartitionedStorage);
   ASSERT_NE(nullptr, partitioned_storage_view);
