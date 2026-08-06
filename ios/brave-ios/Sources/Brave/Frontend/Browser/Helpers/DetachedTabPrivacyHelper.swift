a1e1b568f0867771f0e85d77969e46d69d518b92
brave-builds <45370463+brave-builds@users.noreply.github.com>
2026-08-06 11:48:01 +0900

    [iOS] Include `CosmeticFilteringTabHelper` in `DetachedTabPrivacyHelper` (uplift to 1.94.x) (#38782)
    Uplift of #38763 (squashed) to beta

--- ios/brave-ios/Sources/Brave/Frontend/Browser/Helpers/DetachedTabPrivacyHelper.swift
+++ ios/brave-ios/Sources/Brave/Frontend/Browser/Helpers/DetachedTabPrivacyHelper.swift
@@ -64,6 +64,7 @@ class DetachedTabPrivacyHelper: TabPolicyDecider {
     let shieldsHelper = BraveShieldsTabHelper(tab: tab, braveShieldsSettings: shieldsSettings)
     tab.braveShieldsHelper = shieldsHelper
     tab.addPolicyDecider(shieldsHelper)
+    tab.cosmeticFilteringTabHelper = .init(tab: tab)
   }
 
   // MARK: - TabPolicyDecider
