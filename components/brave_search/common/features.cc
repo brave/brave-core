a428efcf16e2bed0fd1f165ea2a58a38a15a8d2e
brave-builds <45370463+brave-builds@users.noreply.github.com>
2026-08-06 16:47:51 +0100

    [AI Chat] Prepare new NTP search box for day zero experiment (uplift to 1.93.x) (#38695)
    Uplift of #38638 (squashed) to release

--- components/brave_search/common/features.cc
+++ components/brave_search/common/features.cc
@@ -98,4 +98,10 @@ const base::FeatureParam<std::string> kBackupResultsRendererLanguages{
 const base::FeatureParam<std::string> kBackupResultsLanguagesHeader{
     &kBackupResults, "languages_header", ""};
 
+BASE_FEATURE(kSearchNewTabV1Source, base::FEATURE_DISABLED_BY_DEFAULT);
+
+bool IsSearchNewTabV1SourceEnabled() {
+  return base::FeatureList::IsEnabled(kSearchNewTabV1Source);
+}
+
 }  // namespace brave_search::features
