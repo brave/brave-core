BearNetwork-BRNKC/bnes-brave-core

[FocusMode] Add option to display URL in mini-toolbar

browser/ui/views/frame/split_view/brave_contents_container_view.cc
@@ -114,6 +114,13 @@ bool BraveContentsContainerView::IsActive() const {
 void BraveContentsContainerView::UpdateBorderAndOverlay(bool is_in_split,
                                                         bool is_active,
                                                         bool is_highlighted) {
+  // Configure the mini toolbar before the base class updates its state. The
+  // web panel is not part of a split, but is bordered like one.
+  const bool show_domain = ShouldShowDomain();
+  auto* mini_toolbar = BraveMultiContentsViewMiniToolbar::From(mini_toolbar_);
+  mini_toolbar->SetAlwaysShowDomain(show_domain);
+  mini_toolbar->SetUsesSplitBorder(is_in_split || for_web_panel_);
+
   // We don't use highlighted state as we're always using thicker border
   // for highlighting active split tab.
   ContentsContainerView::UpdateBorderAndOverlay(is_in_split, is_active,
@@ -124,6 +131,14 @@ void BraveContentsContainerView::UpdateBorderAndOverlay(bool is_in_split,
   UpdateBorderRoundedCorners();
 
   if (!is_in_split && !for_web_panel_) {
+    // Outside of a split, the mini toolbar is shown only to display the
+    // domain.
+    if (show_domain) {
+      mini_toolbar_->UpdateState(is_active, /*is_highlighted*/ false);
+    } else {
+      mini_toolbar_->SetVisible(false);
+    }
+
     // Not in split view: draw a subtle 1px outline instead of the
     // active/inactive split borders below. GetCornerRadius() returns all-zero
     // corners whenever rounded corners aren't applicable here (feature
@@ -236,12 +251,26 @@ void BraveContentsContainerView::OnReaderModeToolbarActivate(
 }
 #endif
 
-gfx::RoundedCornersF BraveContentsContainerView::GetCornerRadius(
-    int border_thickness) const {
+bool BraveContentsContainerView::ShouldShowDomain() const {
+  if (for_web_panel_ || !IsActive() || IsTabFullscreen()) {
+    return false;
+  }
+
+  auto* brave_browser_view = BraveBrowserView::From(&browser_view_.get());
+  return brave_browser_view &&
+         brave_browser_view->show_active_contents_domain();
+}
+
+bool BraveContentsContainerView::IsTabFullscreen() const {
   auto* exclusive_access_manager =
       browser_view_->browser()->GetFeatures().exclusive_access_manager();
-  if (exclusive_access_manager &&
-      exclusive_access_manager->fullscreen_controller()->IsTabFullscreen()) {
+  return exclusive_access_manager &&
+         exclusive_access_manager->fullscreen_controller()->IsTabFullscreen();
+}
+
+gfx::RoundedCornersF BraveContentsContainerView::GetCornerRadius(
+    int border_thickness) const {
+  if (IsTabFullscreen()) {
     return {};
   }
 

brave/brave-core

browser/ui/views/frame/split_view/brave_contents_container_view.cc
@@ -114,6 +114,13 @@ bool BraveContentsContainerView::IsActive() const {
 void BraveContentsContainerView::UpdateBorderAndOverlay(bool is_in_split,
                                                         bool is_active,
                                                         bool is_highlighted) {
+  // Configure the mini toolbar before the base class updates its state. The
+  // web panel is not part of a split, but is bordered like one.
+  const bool show_domain = ShouldAlwaysShowDomain();
+  auto* mini_toolbar = BraveMultiContentsViewMiniToolbar::From(mini_toolbar_);
+  mini_toolbar->SetAlwaysShowDomain(show_domain);
+  mini_toolbar->SetUsesSplitBorder(is_in_split || for_web_panel_);
+
   // We don't use highlighted state as we're always using thicker border
   // for highlighting active split tab.
   ContentsContainerView::UpdateBorderAndOverlay(is_in_split, is_active,
@@ -124,6 +131,14 @@ void BraveContentsContainerView::UpdateBorderAndOverlay(bool is_in_split,
   UpdateBorderRoundedCorners();
 
   if (!is_in_split && !for_web_panel_) {
+    // Outside of a split, the mini toolbar is shown only to display the
+    // domain.
+    if (show_domain) {
+      mini_toolbar_->UpdateState(is_active, /*is_highlighted*/ false);
+    } else {
+      mini_toolbar_->SetVisible(false);
+    }
+
     // Not in split view: draw a subtle 1px outline instead of the
     // active/inactive split borders below. GetCornerRadius() returns all-zero
     // corners whenever rounded corners aren't applicable here (feature
@@ -236,12 +251,26 @@ void BraveContentsContainerView::OnReaderModeToolbarActivate(
 }
 #endif
 
-gfx::RoundedCornersF BraveContentsContainerView::GetCornerRadius(
-    int border_thickness) const {
+bool BraveContentsContainerView::ShouldAlwaysShowDomain() const {
+  if (for_web_panel_ || !IsActive() || IsTabFullscreen()) {
+    return false;
+  }
+
+  auto* brave_browser_view = BraveBrowserView::From(&browser_view_.get());
+  return brave_browser_view &&
+         brave_browser_view->show_active_contents_domain_in_mini_toolbar();
+}
+
+bool BraveContentsContainerView::IsTabFullscreen() const {
   auto* exclusive_access_manager =
       browser_view_->browser()->GetFeatures().exclusive_access_manager();
-  if (exclusive_access_manager &&
-      exclusive_access_manager->fullscreen_controller()->IsTabFullscreen()) {
+  return exclusive_access_manager &&
+         exclusive_access_manager->fullscreen_controller()->IsTabFullscreen();
+}
+
+gfx::RoundedCornersF BraveContentsContainerView::GetCornerRadius(
+    int border_thickness) const {
+  if (IsTabFullscreen()) {
     return {};
   }
 