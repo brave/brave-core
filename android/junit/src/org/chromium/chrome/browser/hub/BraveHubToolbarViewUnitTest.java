956d767ee0aa596f3ce6a2605a0ea688fab99354
Artem Samoilenko <artem@brave.com>
2026-08-10 13:13:59 -0400

    [cr153][Android] Changes for HubColorMixerImpl c-tor signature
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/375816021c492e6f233b530378388f51cf2805b0
    
    commit 375816021c492e6f233b530378388f51cf2805b0
    Author: Madhav Pruthi <madhavpruthi@google.com>
    Date:   Tue Aug 4 14:53:05 2026 -0700
    
        [SwipeToSwitch] Smoothly blend theme colors during Hub Tab Switcher swipe transitions
    
        Enable real-time theme color blending when swiping between the Regular
        (light theme) and Incognito (dark theme) Tab Switchers in the Chrome
        Android Hub.
    
        Key Improvements:
        - Real-Time Theme Blending: All top toolbar elements, search bar, action
        buttons, screen background, and bottom bar smoothly transition their
        colors in direct 1:1 synchronization with finger displacement.
        - Continuous Tab Switcher Sliding: The top switcher indicator card
        slides gently alongside finger movement without visual snapping or
        position resets.
    
        Demo: http://screencast/cast/NjAyNTQ1MTQ4NjA1MjM1Mnw1YjM3ZTBhYi0xOA
        Bug: 469833255
        Change-Id: Ib34c730c7a638f03ad28a2a9866765cf62c04173
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8164235
        Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
        Commit-Queue: Madhav Pruthi <madhavpruthi@google.com>
        Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
        Cr-Commit-Position: refs/heads/main@{#1673699}

--- android/junit/src/org/chromium/chrome/browser/hub/BraveHubToolbarViewUnitTest.java
+++ android/junit/src/org/chromium/chrome/browser/hub/BraveHubToolbarViewUnitTest.java
@@ -93,7 +93,10 @@ public class BraveHubToolbarViewUnitTest {
         mColorMixer =
                 spy(
                         new HubColorMixerImpl(
-                                mActivity, ObservableSuppliers.alwaysTrue(), mFocusedPaneSupplier));
+                                mActivity,
+                                ObservableSuppliers.alwaysTrue(),
+                                mFocusedPaneSupplier,
+                                ObservableSuppliers.alwaysNull()));
         mPropertyModel =
                 new PropertyModel.Builder(HubToolbarProperties.ALL_KEYS)
                         .with(COLOR_MIXER, mColorMixer)
