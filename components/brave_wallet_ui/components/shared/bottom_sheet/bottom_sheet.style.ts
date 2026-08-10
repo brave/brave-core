7a786ff24317816b7ed3a883fa951d4066a87bba
Douglas Daniel <douglashdaniel@gmail.com>
2026-08-10 14:23:13 -0500

    [Wallet]: Ensure Bottom Sheet Stays Below the Fold (#38960)
    Ensures that the BottomSheet always stays below the fold

--- components/brave_wallet_ui/components/shared/bottom_sheet/bottom_sheet.style.ts
+++ components/brave_wallet_ui/components/shared/bottom_sheet/bottom_sheet.style.ts
@@ -22,11 +22,13 @@ export const Background = styled(Column)<{ isOpen: boolean }>`
 
 export const BottomCard = styled(Column)<{ isOpen: boolean }>`
   position: fixed;
-  bottom: ${(p) => (p.isOpen ? 0 : -800)}px;
-  transition: all 0.3s ease-in-out;
+  bottom: 0;
+  transform: translateY(${(p) => (p.isOpen ? 0 : 100)}%);
+  transition: transform 0.3s ease-in-out;
   border-radius: 16px 16px 0px 0px;
   background-color: ${leo.color.container.background};
   z-index: 32;
+  pointer-events: ${(p) => (p.isOpen ? 'auto' : 'none')};
 `
 
 export const CloseButton = styled(WalletButton)`
