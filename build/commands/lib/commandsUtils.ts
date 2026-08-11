410bc47f19a831d080b0ec4e27e106267b75888b
goodov <5928869+goodov@users.noreply.github.com>
2026-08-12 01:59:06 +0700

    Move few build/commands utils. (#38994)
    * Move few build/commands utils.
    
    * Remove unused cibuild command.

--- build/commands/lib/commandsUtils.ts
+++ build/commands/lib/commandsUtils.ts
@@ -17,3 +17,43 @@ export function createBuildConfigArgument() {
     },
   )
 }
+
+// Collects values into an array.
+export function collect(
+  value: string,
+  accumulator: string[] | undefined,
+): string[] {
+  if (accumulator === undefined) {
+    accumulator = []
+  }
+  accumulator.push(value)
+  return accumulator
+}
+
+// Use this wrapper function instead of JavaScript's parseInt() with option()
+// when defining integer optional parameters, or the default value might get
+// passed as well into the radix parameter of parseInt(), causing wrong results.
+// https://github.com/brave/brave-browser/issues/13724
+export function parseInteger(string: string): number {
+  // As per the spec [1], not passing the optional radix parameter to parseInt()
+  // will make parsing to interpret the string passed as a decimal number unless
+  // it's prefixed with '0' (octal) or '0x' (hexadecimal). We only need decimal
+  // in this particular case so let's be explicit about that.
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
+  return parseInt(string, 10)
+}
+
+// Parses a boolean value from a string.
+export function parseBoolean(value: string): boolean {
+  try {
+    const parsed = JSON.parse(value)
+    if (typeof parsed !== 'boolean') {
+      console.error(`Value is not a boolean: ${value}`)
+      process.exit(1)
+    }
+    return parsed
+  } catch (error) {
+    console.error(`Value is not a boolean: ${value}`)
+    process.exit(1)
+  }
+}
