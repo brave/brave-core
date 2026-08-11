b8ca40af53e2d231b3e30e22f904d2f7ceb32358
Max Karolinskiy <max@brave.com>
2026-08-11 17:14:31 -0400

    [cr153] Replace extension menu delegate override with plaster.
    Caught by upstream unit test
    ExtensionsSitePermissionsPageViewUnitTest.RemoveLastExtensionWhenSitePermissionsPageIsOpen
    
    Our override was copying the entire body of a method, which got updated
    in cr153, so our override caused the test to fail. Replaced the override
    with a plaster patch.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src.git/+/87c75e2e6dcfa
    
    commit 87c75e2e6dcfa2c555ac76b8a3e75bbbfee45ef1
    Author: Anh Mac <anhmac@google.com>
    Date:   Tue Jul 28 12:55:02 2026 -0700
    
        [Extensions] Close the extensions menu bubble when the last extension is removed.
    
        When the last extension is uninstalled or removed, the extensions menu
        is left empty. This change makes sure the menu closes the bubble when
        it is empty on extensions removal.
    
        Additionally, this updates the unit tests to handle the menu closing on
        extension removal, including adding null checks for the menu widget in
        test helpers since the widget may now be destroyed during the test.
    
        Bug: 426096750
        Change-Id: Ie7e22ad518e12744eedc3fc93d490653d263405b
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8136781
        Commit-Queue: Anh Mac <anhmac@google.com>
        Reviewed-by: Eva Su <evasu@chromium.org>
        Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1669674}

--- chromium_src/chrome/browser/ui/views/extensions/extensions_menu_delegate_desktop.cc
+++ chromium_src/chrome/browser/ui/views/extensions/extensions_menu_delegate_desktop.cc
@@ -3,18 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+// BraveExtensionsMenuMainPageView is substituted into OpenMainPage() in place
+// of its upstream base class via plaster, see
+// brave/rewrite/chrome/browser/ui/views/extensions/extensions_menu_delegate_desktop.cc.yaml
 #include "brave/browser/ui/views/extensions/brave_extensions_menu_main_page_view.h"
 
-#define BRAVE_EXTENSIONS_MENU_VIEW_PLATFORM_DELEGATE_VIEWS_OPEN_MAIN_PAGE  \
-  {                                                                        \
-    auto main_page =                                                       \
-        std::make_unique<BraveExtensionsMenuMainPageView>(browser_, this); \
-    UpdateMainPage(main_page.get());                                       \
-    PopulateMainPage(main_page.get());                                     \
-    SwitchToPage(std::move(main_page));                                    \
-    return;                                                                \
-  }
-
 #include <chrome/browser/ui/views/extensions/extensions_menu_delegate_desktop.cc>
-
-#undef BRAVE_EXTENSIONS_MENU_VIEW_PLATFORM_DELEGATE_VIEWS_OPEN_MAIN_PAGE
