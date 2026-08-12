00aa5cee8807dc8344b1fcbcaff0375beca6a7a9
cdesouza-chromium <cdesouza@brave.com>
2026-08-12 02:32:39 +0100

    [plaster] `OVERRIDE_FEATURE_DEFAULT_STATES` pt.1 (#38972)
    This PR semi-mechanically migrates several uses of
    `OVERRIDE_FEATURE_DEFAULT_STATES` to plaster. The following flags
    overrides were not migrated because the upstream file already has the
    value we were using:
    
     - `kDevToolsUseGcaApi`
     - `kLaunchOnStartup`
     - `kWindows11MicaTitlebar`
     - `kIndigo`
     - `kTrustSafetySentimentSurvey`
     - `kCommerceDeveloper`
     - `kCommerceMerchantViewer`
     - `kPriceAnnotations`
     - `kShoppingList`
     - `kShoppingPDPMetrics`
     - `kContextualTasks`
    
    The values being migrated are the most straightfoward ones.
    
    Bug: https://github.com/brave/brave-browser/issues/45050

--- app/feature_defaults_unittest.cc
+++ app/feature_defaults_unittest.cc
@@ -91,6 +91,7 @@
 
 #if BUILDFLAG(IS_WIN)
 #include "chrome/browser/startup/startup_features.h"
+#include "chrome/browser/win/mica_titlebar.h"
 #endif
 
 #if BUILDFLAG(ENABLE_SCREEN_CAPTURE)
@@ -215,6 +216,9 @@ TEST(FeatureDefaultsTest, DisabledFeatures) {
       &history_embeddings::kHistoryEmbeddings,
       &history_embeddings::kHistoryEmbeddingsAnswers,
       &history_embeddings::kLaunchedHistoryEmbeddings,
+#if BUILDFLAG(IS_WIN)
+      &kWindows11MicaTitlebar,
+#endif
       &lens::features::kLensOverlay,
       &lens::features::kLensStandalone,
       &media::kLiveCaption,
