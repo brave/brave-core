0dde5113adf5fa0f2f9f7a2d76d9c0a0efd9a096
Simon Hong <shong@brave.com>
2026-08-13 10:24:27 +0900

    Restored container submenu from bookmarks bar context menu

--- chromium_src/chrome/browser/ui/views/bookmarks/bookmark_context_menu.cc
+++ chromium_src/chrome/browser/ui/views/bookmarks/bookmark_context_menu.cc
@@ -7,6 +7,22 @@
 
 #include "brave/browser/ui/toolbar/brave_bookmark_context_menu_controller.h"
 
+// Upstream's `BookmarkContextMenu` constructor only descends into the submenu
+// whose command id is `IDC_BOOKMARK_BAR_SUBMENU`. But, we also have the
+// containers submenu `IDC_OPEN_IN_CONTAINER` that needs to be populated, so it
+// seems better to patch here to populate our submenu.
+#define BRAVE_BOOKMARK_CONTEXT_MENU                                            \
+  if (menu_model->GetCommandIdAt(i) == IDC_OPEN_IN_CONTAINER) {                \
+    views::MenuItemView* item = menu_->GetMenuItemByID(IDC_OPEN_IN_CONTAINER); \
+    ui::MenuModel* container_submodel = menu_model->GetSubmenuModelAt(i);      \
+    DCHECK(container_submodel);                                                \
+    for (size_t j = 0; j < container_submodel->GetItemCount(); ++j) {          \
+      views::MenuModelAdapter::AppendMenuItemFromModel(                        \
+          container_submodel, j, item, container_submodel->GetCommandIdAt(j)); \
+    }                                                                          \
+  }
+
 #define BookmarkContextMenuController BraveBookmarkContextMenuController
 #include <chrome/browser/ui/views/bookmarks/bookmark_context_menu.cc>
 #undef BookmarkContextMenuController
+#undef BRAVE_BOOKMARK_CONTEXT_MENU
