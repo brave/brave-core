VexBrowser/vex-core

[plaster][cr152] Fixing UMA insertions
`kHistogramValue` was moved to a new path, and is now called
`kContentSettingsTypeToHistogramValue`. This change introduces a plaster
following the insertion point.

We also drop the `chromium_src` file for `content_settings_uma_util.cc`,
and replace it with a plaster.

Chromium changes:
https://chromium.googlesource.com/chromium/src/+/e26b7e3b541430f6f6cc603c23e29cc5c7134c82

commit e26b7e3b541430f6f6cc603c23e29cc5c7134c82
Author: Judith Hemp <hempjudith@google.com>
Date:   Wed Jul 8 06:52:51 2026 -0700

    Extract content setting uma bucket values to header

    Extract UMA histogram bucket IDs for ContentSettingsType (`ContentType`
    in enums.xml) from content_settings_uma_util.cc into a new file
    permission_uma_constants.h.

    Checked with git diff --no-index -w \
          <(git show origin/main:components/content_settings/core/browser/content_settings_uma_util.cc | sed -n '21,180p') \
          <(sed -n '18,185p' components/content_settings/core/common/content_settings_uma_constants.h)

    and

    git diff -w
    origin/main:components/content_settings/core/browser/content_settings_uma_util.cc
    components/content_settings/core/common/content_settings_uma_constants.h

    NO_IFTTT=moving constant lookup to permission_uma_constants.h without modifying histogram bucket values

    Bug: 519128318
    Change-Id: Ia1924180411975324af06aa2b96c852b9f01b5f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8064268
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Judith Hemp <hempjudith@google.com>
    Cr-Commit-Position: refs/heads/main@{#1658707}

chromium_src/components/content_settings/core/common/content_settings_uma_constants.h
@@ -1,0 +1,36 @@
+/* Copyright (c) 2026 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_CONTENT_SETTINGS_CORE_COMMON_CONTENT_SETTINGS_UMA_CONSTANTS_H_
+#define BRAVE_CHROMIUM_SRC_COMPONENTS_CONTENT_SETTINGS_CORE_COMMON_CONTENT_SETTINGS_UMA_CONSTANTS_H_
+
+#include "components/content_settings/core/common/content_settings_types.h"
+
+namespace content_settings {
+
+// Leave a gap between Chromium values and our values in the
+// kContentSettingsTypeToHistogramValue array so that we don't have to renumber
+// when new content settings types are added upstream.
+//
+// Do not change the value arbitrarily. This is used to validate we have a gap
+// between Chromium's and Brave's histograms. This value must be less than 1000
+// as upstream performs a sanity check that the total number of buckets isn't
+// unreasonably large.
+inline constexpr int kBraveValuesStart = 900;
+
+inline constexpr int brave_value(int incr) {
+  return kBraveValuesStart + incr;
+}
+
+static_assert(static_cast<int>(ContentSettingsType::kMaxValue) <
+                  kBraveValuesStart,
+              "There must a gap between the histograms used by Chromium, and "
+              "the ones used by Brave.");
+
+}  // namespace content_settings
+
+#include <components/content_settings/core/common/content_settings_uma_constants.h>  // IWYU pragma: export
+
+#endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_CONTENT_SETTINGS_CORE_COMMON_CONTENT_SETTINGS_UMA_CONSTANTS_H_

brave/brave-core

chromium_src/components/content_settings/core/common/content_settings_uma_constants.h
@@ -1,0 +1,36 @@
+/* Copyright (c) 2026 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_CONTENT_SETTINGS_CORE_COMMON_CONTENT_SETTINGS_UMA_CONSTANTS_H_
+#define BRAVE_CHROMIUM_SRC_COMPONENTS_CONTENT_SETTINGS_CORE_COMMON_CONTENT_SETTINGS_UMA_CONSTANTS_H_
+
+#include "components/content_settings/core/common/content_settings_types.h"
+
+namespace content_settings {
+
+// Leave a gap between Chromium values and our values in the
+// kContentSettingsTypeToHistogramValue array so that we don't have to renumber
+// when new content settings types are added upstream.
+//
+// Do not change the value arbitrarily. This is used to validate we have a gap
+// between Chromium's and Brave's histograms. This value must be less than 1000
+// as upstream performs a sanity check that the total number of buckets isn't
+// unreasonably large.
+inline constexpr int kBraveHistogramValuesStart = 900;
+
+inline constexpr int GetBraveHistogramValue(int incr) {
+  return kBraveHistogramValuesStart + incr;
+}
+
+static_assert(static_cast<int>(ContentSettingsType::kMaxValue) <
+                  kBraveHistogramValuesStart,
+              "There must a gap between the histograms used by Chromium, and "
+              "the ones used by Brave.");
+
+}  // namespace content_settings
+
+#include <components/content_settings/core/common/content_settings_uma_constants.h>  // IWYU pragma: export
+
+#endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_CONTENT_SETTINGS_CORE_COMMON_CONTENT_SETTINGS_UMA_CONSTANTS_H_