7e0f24556f5ee06859ec517f20ba563c51ec4ef9
Agustín Ruiz <aguscr182@gmail.com>
2026-08-13 11:07:31 -0300

    Use uniform inset for vertical-tab title chip
    Replace a TLBR inset that left the right edge unpadded with a uniform gfx::Insets(kPaddingForGroup) in LayoutTitleChipForVerticalTabs. Previously the right inset was 0 to keep the chip flush with tabs below; this change applies the same padding on all sides, adjusting the title chip layout for vertical tabs.

--- browser/ui/views/tabs/brave_tab_group_header.cc
+++ browser/ui/views/tabs/brave_tab_group_header.cc
@@ -113,10 +113,7 @@ bool BraveTabGroupHeader::ShouldShowVerticalTabs() const {
 
 void BraveTabGroupHeader::LayoutTitleChipForVerticalTabs() {
   auto title_bounds = GetContentsBounds();
-  // Inset the left/top/bottom, but not the right, so the chip's right edge is
-  // flush with the tabs in the group below (which only have a left inset).
-  title_bounds.Inset(gfx::Insets::TLBR(kPaddingForGroup, kPaddingForGroup,
-                                       kPaddingForGroup, 0));
+  title_bounds.Inset(gfx::Insets(kPaddingForGroup));
   title_chip_->SetBoundsRect(title_bounds);
 
   // |title_| is a child view of |title_chip_| and there could be |sync_icon_|
