0c14c11ed03670f464cbfef53defe8f6861286c5
Claudio DeSouza <cdesouza@brave.com>
2026-08-17 09:49:01 +0100

    [cr153][plaster] `browser_window_features.cc` 🩹
    With `BrowserWindowInterface::GetFeatures()` const accessor added, we
    were hitting covariance issues, that were inherent of the macro we had
    in place.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/1e85ecb73a31aa21892aeebeaca97422f9efe769
    
    commit 1e85ecb73a31aa21892aeebeaca97422f9efe769
    Author: Thomas Lukaszewicz <tluk@chromium.org>
    Date:   Tue Jun 17 15:12:14 2025 -0700
    
        [bedrock] Add const BrowserWindowInterface::GetFeatures() accessor
    
        Bug: 413168662
        Change-Id: I8eaeed784ab9b2ff7ffcc0b5c96c7347b04c9d9d
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651108
        Reviewed-by: David Yeung <dayeung@chromium.org>
        Auto-Submit: Tom Lukaszewicz <tluk@chromium.org>
        Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1475257}

--- chromium_src/chrome/browser/ui/browser_window/internal/browser_window_features.cc
+++ chromium_src/chrome/browser/ui/browser_window/internal/browser_window_features.cc
@@ -12,35 +12,8 @@
 #include "brave/browser/ui/tabs/public/vertical_tab_controller.h"
 #include "brave/browser/ui/toolbar/brave_location_bar_model_delegate.h"
 #include "brave/browser/ui/views/side_panel/brave_side_panel_coordinator.h"
-#include "chrome/browser/contextual_tasks/contextual_tasks_side_panel_coordinator.h"
-#include "chrome/browser/sessions/session_service_factory.h"
-#include "chrome/browser/ui/browser_content_setting_bubble_model_delegate.h"
-#include "chrome/browser/ui/browser_web_contents_delegate/browser_web_contents_delegate.h"
-#include "chrome/browser/ui/exclusive_access/exclusive_access_manager.h"
-#include "chrome/browser/ui/find_bar/find_bar_controller.h"
-#include "chrome/browser/ui/views/side_panel/bookmarks/bookmarks_side_panel_coordinator.h"
-#include "chrome/browser/ui/views/side_panel/history_clusters/history_clusters_side_panel_coordinator.h"
-
-#if BUILDFLAG(ENABLE_EXTENSIONS)
-#include "chrome/browser/extensions/extension_browser_window_helper.h"
-#endif
-
-#define BrowserContentSettingBubbleModelDelegate \
-  BraveBrowserContentSettingBubbleModelDelegate
-#define BrowserWindowFeatures BrowserWindowFeatures_ChromiumImpl
-#define SidePanelCoordinator BraveSidePanelCoordinator
-#define BrowserActions BraveBrowserActions
-#define BrowserCommandController BraveBrowserCommandController
-#define BrowserWebContentsDelegate BraveBrowserWebContentsDelegate
 
 #include <chrome/browser/ui/browser_window/internal/browser_window_features.cc>
-
-#undef BrowserWebContentsDelegate
-#undef BrowserCommandController
-#undef BrowserActions
-#undef SidePanelCoordinator
-#undef BrowserWindowFeatures
-#undef BrowserContentSettingBubbleModelDelegate
 
 const SidePanelUI* BrowserWindowFeatures_ChromiumImpl::side_panel_ui() const {
   return const_cast<BrowserWindowFeatures_ChromiumImpl*>(this)->side_panel_ui();
