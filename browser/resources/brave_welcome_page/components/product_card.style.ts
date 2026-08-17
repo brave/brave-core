c6e30207ce2963f84005113666571c595e0667d1
Agustín Ruiz <aguscr182@gmail.com>
2026-08-17 17:09:35 -0300

    Features selection - List of updates (#39157)
    Make product card clickable and add unchecked style
    
    Make ProductCard clickable to toggle selection. Adds onClick and conditional 'unchecked' class in product_card.tsx and stops propagation on the 'Learn more' link so navigation doesn't toggle the card. Styles updated in product_card.style.ts: new --leo-checkbox-size var, adjusted padding, pointer cursor, transitions, and an 'unchecked' state (reduced opacity + grayscale). Also removed h3 opacity override.

--- browser/resources/brave_welcome_page/components/product_card.style.ts
+++ browser/resources/brave_welcome_page/components/product_card.style.ts
@@ -3,27 +3,41 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-import { color, radius, spacing } from '@brave/leo/tokens/css/variables'
+import {
+  color,
+  duration,
+  easing,
+  icon,
+  radius,
+  spacing,
+} from '@brave/leo/tokens/css/variables'
 import { scoped } from '$web-common/scoped_css'
 
 export const style = scoped.css`
   & {
+    --leo-checkbox-size: ${icon.l};
+
     display: flex;
     gap: ${spacing['2Xl']};
     align-items: center;
-    padding: ${spacing.xl};
+    padding: ${spacing.xl} ${spacing['2Xl']} ${spacing.xl} ${spacing.xl} ;
     border-radius: ${radius.xl};
     background: ${color.material.regular};
+    cursor: pointer;
+    transition:
+      opacity ${duration.s} ${easing.out},
+      filter ${duration.s} ${easing.out};
+  }
+
+  &.unchecked {
+    opacity: 0.5;
+    filter: grayscale(1);
   }
 
   img {
     width: 100px;
     height: auto;
     border-radius: ${radius.l};
-  }
-
-  h3 {
-    opacity: 0.9;
   }
 
   p {
