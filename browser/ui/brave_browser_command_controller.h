d749f6fdcebc459b32c36eaf72fbee85612bbe7c
Claudio DeSouza <cdesouza@brave.com>
2026-08-19 01:45:49 +0100

    [cr153] Using `ExecuteCommandWithDispositionAndContext`
    This new override is just a variation of the previous one with an extra
    argument, which affects our code when it comes to the override
    signature, and passing data along.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/66a88ab465ef846fe685e6d4cf433a3100d63778
    
    commit 66a88ab465ef846fe685e6d4cf433a3100d63778
    Author: Eshwar Stalin <estalin@chromium.org>
    Date:   Fri Aug 14 14:01:48 2026 -0700
    
        [BAM] Minor cleanup of CommandUpdater interface
    
        Removing the need for implementing multiple execute command functions
        with the same behavior (i.e. same disposition) and also removing that
        function from the public interface. Also making action context optional
        value as part of existing function instead of introducing multiple new
        functions.
    
        Change-Id: Ic2a96324734a11440507542cf0edc7381e87a660
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8259354
        Reviewed-by: Kunal Daftari <kunaldaftari@google.com>
        Reviewed-by: Darryl James <dljames@chromium.org>
        Commit-Queue: Eshwar Stalin <estalin@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1679887}

--- browser/ui/brave_browser_command_controller.h
+++ browser/ui/brave_browser_command_controller.h
@@ -72,11 +72,11 @@ class BraveBrowserCommandController : public chrome::BrowserCommandController
   // Overriden from CommandUpdater:
   bool SupportsCommand(int id) const override;
   bool IsCommandEnabled(int id) const override;
-  bool ExecuteCommandWithDispositionImpl(
+  bool ExecuteCommandWithDispositionAndContext(
       int id,
       WindowOpenDisposition disposition,
-      base::TimeTicks time_stamp,
-      std::optional<actions::ActionInvocationContext> context) override;
+      std::optional<actions::ActionInvocationContext> context,
+      base::TimeTicks time_stamp) override;
   void AddCommandObserver(int id, CommandObserver* observer) override;
   void RemoveCommandObserver(int id, CommandObserver* observer) override;
   void RemoveCommandObserver(CommandObserver* observer) override;
