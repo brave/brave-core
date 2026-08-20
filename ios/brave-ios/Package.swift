4c4b8178875f4ce3dc106cc465db49012800f685
Stephen Heaps <steaps.4@gmail.com>
2026-08-20 13:42:14 -0400

    Move `ShredSiteSettingsView` to `BraveShields` target

--- ios/brave-ios/Package.swift
+++ ios/brave-ios/Package.swift
@@ -301,7 +301,7 @@ var package = Package(
     .testTarget(name: "BraveUITests", dependencies: ["BraveUI"]),
     .target(
       name: "BraveShields",
-      dependencies: ["Strings", "Preferences", "BraveCore", "Web", "Data"],
+      dependencies: ["Strings", "Preferences", "BraveCore", "BraveUI", "Web", "Data"],
       plugins: ["LoggerPlugin"]
     ),
     .testTarget(
