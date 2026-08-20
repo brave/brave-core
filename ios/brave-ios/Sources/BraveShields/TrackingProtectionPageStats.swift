ebccd3c9ca09b0414dce268b6e8473f3e724b291
Stephen Heaps <steaps.4@gmail.com>
2026-08-20 13:29:24 -0400

    Move more Shields code into BraveShields target - `ShieldsSettingsViewModel` renamed to `ShieldsPanelViewModel` and moved to `BraveShields` target - `TPPageStats` renamed to `TrackingProtectionPageStats` and moved to `BraveShields` target

--- ios/brave-ios/Sources/BraveShields/TrackingProtectionPageStats.swift
+++ ios/brave-ios/Sources/BraveShields/TrackingProtectionPageStats.swift
@@ -1,20 +1,22 @@
-// Copyright 2024 The Brave Authors. All rights reserved.
+// Copyright 2026 The Brave Authors. All rights reserved.
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 import Foundation
 
-struct TPPageStats {
+public struct TrackingProtectionPageStats {
   let adCount: Int
   let trackerCount: Int
   let scriptCount: Int
   let fingerprintingCount: Int
   let httpsCount: Int
 
-  var total: Int { return adCount + trackerCount + scriptCount + fingerprintingCount + httpsCount }
+  public var total: Int {
+    return adCount + trackerCount + scriptCount + fingerprintingCount + httpsCount
+  }
 
-  init(
+  public init(
     adCount: Int = 0,
     trackerCount: Int = 0,
     scriptCount: Int = 0,
@@ -28,14 +30,14 @@ struct TPPageStats {
     self.httpsCount = httpsCount
   }
 
-  func adding(
+  public func adding(
     adCount: Int = 0,
     trackerCount: Int = 0,
     scriptCount: Int = 0,
     fingerprintingCount: Int = 0,
     httpsCount: Int = 0
-  ) -> TPPageStats {
-    TPPageStats(
+  ) -> Self {
+    .init(
       adCount: self.adCount + adCount,
       trackerCount: self.trackerCount + trackerCount,
       scriptCount: self.scriptCount + scriptCount,
