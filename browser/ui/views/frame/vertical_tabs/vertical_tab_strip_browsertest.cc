2a9c8ad18beb216b1ef4d4647388e0fe65d926d2
Claudio DeSouza <cdesouza@brave.com>
2026-08-20 15:40:57 +0100

    [cr154] Including target for `ScopedRTLForTesting`
    This was extracted in upstream.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/fcedb25aeef2b2f314c883db96390c6241d4e320
    
    commit fcedb25aeef2b2f314c883db96390c6241d4e320
    Author: Danilo Tedeschi <danft@google.com>
    Date:   Wed Aug 19 09:23:08 2026 -0700
    
        Remove SetRTLForTesting by using ScopedRTLForTesting
    
        This is part of the LSC to control ICU default locale setting. The first
        step is going to be removing the usages of SetICUDefaultLocale that has
        no control on who can call it.
    
        More info about the project can be found here:
        https://docs.google.com/document/d/1nQkpibd-w1YJRHLaZxFmk1ztlB6XwV9jlEAJht2v4e8/edit?tab=t.0#heading=h.ylysozd1ht7z
    
        Quick note here is that I had to remove g_icu_text_direction which was a
        simple cache to avoid computing rtl when called. Since it is only a
        simple set lookup, it is being removed.
    
        Change-Id: I7a7cb368459822985fe1deb026090c040a8eb189
        Bug: 542711615
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8198687
        Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
        Reviewed-by: David Yeung <dayeung@chromium.org>
        Commit-Queue: Danilo Tedeschi <danft@google.com>
        Cr-Commit-Position: refs/heads/main@{#1682382}

--- browser/ui/views/frame/vertical_tabs/vertical_tab_strip_browsertest.cc
+++ browser/ui/views/frame/vertical_tabs/vertical_tab_strip_browsertest.cc
@@ -6,6 +6,7 @@
 #include <algorithm>
 
 #include "base/i18n/rtl.h"
+#include "base/i18n/test/scoped_rtl_for_testing.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/test/bind.h"
 #include "base/test/run_until.h"
