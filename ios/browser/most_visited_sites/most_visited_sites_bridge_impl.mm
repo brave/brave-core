dc8c08c2933efaf40a713f2f270fbbd06933075d
Nuo Xu <nuoxu.nx@gmail.com>
2026-08-21 13:28:05 -0400

    Allow multiple subscriptions to MostVisitedSites updates

--- ios/browser/most_visited_sites/most_visited_sites_bridge_impl.mm
+++ ios/browser/most_visited_sites/most_visited_sites_bridge_impl.mm
@@ -26,61 +26,45 @@
 
 @end
 
-@interface MostVisitedSitesBridgeImpl () <MostVisitedSitesObserving>
+@interface MostVisitedSitesSubscriptionImpl
+    : NSObject <MostVisitedSitesObserving, MostVisitedSitesSubscription>
+
+- (instancetype)initWithObserver:(id<MostVisitedSitesObserverBridge>)observer
+                mostVisitedSites:(ntp_tiles::MostVisitedSites*)mostVisitedSites
+                     maxNumSites:(NSUInteger)maxNumSites;
+
 @end
 
-@implementation MostVisitedSitesBridgeImpl {
-  std::unique_ptr<ntp_tiles::MostVisitedSites> _mostVisitedSites;
-  std::unique_ptr<ntp_tiles::MostVisitedSitesObserverBridge> _observerBridge;
+@implementation MostVisitedSitesSubscriptionImpl {
   __weak id<MostVisitedSitesObserverBridge> _observer;
+  raw_ptr<ntp_tiles::MostVisitedSites> _mostVisitedSites;
+  std::unique_ptr<ntp_tiles::MostVisitedSitesObserverBridge> _observerBridge;
 }
 
-- (instancetype)initWithMostVisitedSites:
-    (std::unique_ptr<ntp_tiles::MostVisitedSites>)mostVisitedSites {
+- (instancetype)initWithObserver:(id<MostVisitedSitesObserverBridge>)observer
+                mostVisitedSites:(ntp_tiles::MostVisitedSites*)mostVisitedSites
+                     maxNumSites:(NSUInteger)maxNumSites {
   if ((self = [super init])) {
-    _mostVisitedSites = std::move(mostVisitedSites);
+    _observer = observer;
+    _mostVisitedSites = mostVisitedSites;
+    _observerBridge =
+        std::make_unique<ntp_tiles::MostVisitedSitesObserverBridge>(
+            self, mostVisitedSites);
+    _mostVisitedSites->AddMostVisitedURLsObserver(_observerBridge.get(),
+                                                  maxNumSites);
   }
   return self;
 }
 
 - (void)dealloc {
+  [self unsubscribe];
+}
+
+- (void)unsubscribe {
   if (_observerBridge) {
     _mostVisitedSites->RemoveMostVisitedURLsObserver(_observerBridge.get());
     _observerBridge.reset();
   }
-  _mostVisitedSites.reset();
-}
-
-- (void)startTopSitesOnlyWithObserver:
-            (id<MostVisitedSitesObserverBridge>)observer
-                          maxNumSites:(NSUInteger)maxNumSites {
-  if (_observerBridge) {
-    return;
-  }
-  _observer = observer;
-  _observerBridge = std::make_unique<ntp_tiles::MostVisitedSitesObserverBridge>(
-      self, _mostVisitedSites.get());
-  _mostVisitedSites->EnableTileTypes(
-      ntp_tiles::MostVisitedSites::EnableTileTypesOptions().with_top_sites(
-          true));
-  _mostVisitedSites->AddMostVisitedURLsObserver(_observerBridge.get(),
-                                                maxNumSites);
-}
-
-- (void)refresh {
-  _mostVisitedSites->Refresh();
-}
-
-- (void)setBlocked:(BOOL)blocked forURL:(NSURL*)url {
-  GURL gurl = net::GURLWithNSURL(url);
-  if (!gurl.is_valid()) {
-    return;
-  }
-  _mostVisitedSites->AddOrRemoveBlockedUrl(gurl, blocked);
-}
-
-- (void)clearBlockedURLs {
-  _mostVisitedSites->ClearBlockedUrls();
 }
 
 #pragma mark - MostVisitedSitesObserving
@@ -102,13 +86,55 @@
   [_observer mostVisitedSitesDidUpdateTiles:[bridgedTiles copy]];
 }
 
-- (void)mostVisitedSites:(ntp_tiles::MostVisitedSites*)mostVisitedSites
+- (void)mostVisitedSites:(ntp_tiles::MostVisitedSites*)mostVisi
     didUpdateFaviconForURL:(const GURL&)siteURL {
   NSURL* url = net::NSURLWithGURL(siteURL);
   if (!url) {
     return;
   }
   [_observer mostVisitedSitesDidUpdateFaviconForURL:url];
+}
+
+@end
+
+@implementation MostVisitedSitesBridgeImpl {
+  std::unique_ptr<ntp_tiles::MostVisitedSites> _mostVisitedSites;
+}
+
+- (instancetype)initWithMostVisitedSites:
+    (std::unique_ptr<ntp_tiles::MostVisitedSites>)mostVisitedSites {
+  if ((self = [super init])) {
+    _mostVisitedSites = std::move(mostVisitedSites);
+    _mostVisitedSites->EnableTileTypes(
+        ntp_tiles::MostVisitedSites::EnableTileTypesOptions().with_top_sites(
+            true));
+  }
+  return self;
+}
+
+- (id<MostVisitedSitesSubscription>)
+    startTopSitesOnlyWithObserver:(id<MostVisitedSitesObserverBridge>)observer
+                      maxNumSites:(NSUInteger)maxNumSites {
+  return [[MostVisitedSitesSubscriptionImpl alloc]
+      initWithObserver:observer
+      mostVisitedSites:_mostVisitedSites.get()
+           maxNumSites:maxNumSites];
+}
+
+- (void)refresh {
+  _mostVisitedSites->Refresh();
+}
+
+- (void)setBlocked:(BOOL)blocked forURL:(NSURL*)url {
+  GURL gurl = net::GURLWithNSURL(url);
+  if (!gurl.is_valid()) {
+    return;
+  }
+  _mostVisitedSites->AddOrRemoveBlockedUrl(gurl, blocked);
+}
+
+- (void)clearBlockedURLs {
+  _mostVisitedSites->ClearBlockedUrls();
 }
 
 @end
