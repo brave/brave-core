f0ff5b0ba6eb081de39492db9d29cca88e6eaf19
Kyle Hickinson <git@kylehickinson.com>
2026-08-21 12:32:56 -0400

    [iOS] Remove AIChat WebUI enabled feature flag

--- ios/browser/ui/webui/brave_web_ui_controller_factory.mm
+++ ios/browser/ui/webui/brave_web_ui_controller_factory.mm
@@ -34,7 +34,6 @@
 
 #if BUILDFLAG(ENABLE_AI_CHAT)
 #include "brave/components/ai_chat/core/browser/utils.h"
-#include "brave/components/ai_chat/core/common/features.h"
 #include "brave/ios/browser/ui/webui/ai_chat/ai_chat_ui.h"
 #include "brave/ios/browser/ui/webui/ai_chat/ai_chat_untrusted_conversation_ui.h"
 #endif
@@ -120,8 +119,7 @@ WebUIIOSFactoryFunction GetUntrustedWebUIIOSFactoryFunction(const GURL& url) {
   [[maybe_unused]] const std::string_view url_host = url.host();
 
 #if BUILDFLAG(ENABLE_AI_CHAT)
-  if (url_host == kAIChatUntrustedConversationUIHost &&
-      ai_chat::features::IsAIChatWebUIEnabled()) {
+  if (url_host == kAIChatUntrustedConversationUIHost) {
     return &NewRegularProfileOnlyWebUIIOS<AIChatUntrustedConversationUI>;
   }
 #endif
@@ -167,8 +165,7 @@ WebUIIOSFactoryFunction GetWebUIIOSFactoryFunction(const GURL& url) {
   } else if (url_host == kSkusInternalsHost) {
     return &NewWebUIIOS<SkusInternalsUI>;
 #if BUILDFLAG(ENABLE_AI_CHAT)
-  } else if (url_host == kAIChatUIHost &&
-             ai_chat::features::IsAIChatWebUIEnabled()) {
+  } else if (url_host == kAIChatUIHost) {
     return &NewWebUIIOS<AIChatUI>;
 #endif
 #if BUILDFLAG(ENABLE_BRAVE_WALLET)
