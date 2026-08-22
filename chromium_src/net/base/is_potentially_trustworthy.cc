edc70ddfc6d9580cdb4a92e122f8fd6c4c8fb46e
Emerick Rogul <erogul@brave.com>
2026-08-22 12:42:19 -0400

    [cr154][Tor] Restore .onion trustworthy-origin override after upstream move
    Upstream moved `IsOriginPotentiallyTrustworthy` from
    `services/network/public/cpp` to `net/base`, orphaning Brave's chromium_src
    override that treats .onion as a trustworthy origin. Without it, .onion requests
    failed the trustworthy check that gates Sec-Fetch-* headers, breaking
    Sec-Fetch-Site for Tor navigations.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/c9d989b0c8c6d
    
    commit c9d989b0c8c6d88b7b2f4ad2e3aea332a420634b
    Author: Shunya Shishido <sisidovski@chromium.org>
    Date:   Tue Aug 18 05:30:12 2026 -0700
    
        [net] Move IsOriginPotentiallyTrustworthy to //net/base
    
        Move `IsOriginPotentiallyTrustworthy`, `IsUrlPotentiallyTrustworthy`,
        and `SecureOriginAllowlist` from `//services/network/public/cpp` to
        `//net/base`.
    
        This allows lower-level network stack components such as Reporting API
        to check whether an origin or URL is potentially trustworthy according
        to W3C Secure Contexts without violating layer boundaries.
    
        `services/network/public/cpp/is_potentially_trustworthy.h` now forwards
        to `net::` to maintain backward compatibility for existing callers.
    
        TAG=agy
        CONV=6ff22b41-b708-4e40-9d0a-b321d877baf5
    
        Bug: 546285538
        Change-Id: I35668514807a7febe56817c8bcaa1b231aaad859
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8260247
        Reviewed-by: Adam Rice <ricea@chromium.org>
        Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1681295}

--- chromium_src/net/base/is_potentially_trustworthy.cc
+++ chromium_src/net/base/is_potentially_trustworthy.cc
@@ -1,12 +1,12 @@
-/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+/* Copyright (c) 2026 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "services/network/public/cpp/is_potentially_trustworthy.h"
+#include "net/base/is_potentially_trustworthy.h"
 
 #define IsLocalhost(URL) IsLocalhostOrOnion(URL)
 
-#include <services/network/public/cpp/is_potentially_trustworthy.cc>
+#include <net/base/is_potentially_trustworthy.cc>
 
 #undef IsLocalhost
