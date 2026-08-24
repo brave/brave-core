783e7428cbeff7e10cb8067b8f8fd7ae81dfe057
AlexeyBarabash <alexey@brave.com>
2026-08-24 14:46:30 +0300

    [cr154][Android] Clean up AdaptiveButtonInTopToolbarCustomizationV2 flag
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/ea6e0da864178e55ae4a8cffc89ea44454f90e1a
    
    commit ea6e0da864178e55ae4a8cffc89ea44454f90e1a
    Author: Eleanor Lee <eleanorlee@google.com>
    Date:   Fri Aug 21 00:10:57 2026 -0700
    
        [Community Contributions] Clean up AdaptiveButtonInTopToolbarCustomizationV2
    
        This flag has been enabled by default for several years so can be safely cleaned up.
    
        Bug: 40192535
        Change-Id: Id933dd95b067e4ac8032b802a849e4ff30ca8dc2
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8265268
        Reviewed-by: Siddhartha S <ssid@chromium.org>
        Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
        Commit-Queue: Eleanor Lee <eleanorlee@google.com>
        Cr-Commit-Position: refs/heads/main@{#1683722}

--- browser/about_flags.cc
+++ browser/about_flags.cc
@@ -506,15 +506,6 @@ const char* const kBraveSyncImplLink[1] = {"https://github.com/brave/go-sync"};
       kOsAndroid,                                                             \
       FEATURE_VALUE_TYPE(safe_browsing::features::kBraveAndroidSafeBrowsing), \
   })
-#define BRAVE_ADAPTIVE_BUTTON_IN_TOOLBAR_ANDROID                        \
-  EXPAND_FEATURE_ENTRIES({                                              \
-      "adaptive-button-in-toolbar",                                     \
-      "Adaptive Button In Toolbar (quick shortcut)",                    \
-      "Show quick shortcut button in toolbar. ",                        \
-      kOsAndroid,                                                       \
-      FEATURE_VALUE_TYPE(                                               \
-          chrome::android::kAdaptiveButtonInTopToolbarCustomizationV2), \
-  })
 #define BRAVE_CUSTOM_SEARCH_ENGINES                                        \
   EXPAND_FEATURE_ENTRIES({                                                 \
       "brave-custom-search-engines",                                       \
@@ -535,7 +526,6 @@ const char* const kBraveSyncImplLink[1] = {"https://github.com/brave/go-sync"};
 #else
 #define BRAVE_BACKGROUND_VIDEO_PLAYBACK_ANDROID
 #define BRAVE_SAFE_BROWSING_ANDROID
-#define BRAVE_ADAPTIVE_BUTTON_IN_TOOLBAR_ANDROID
 #define BRAVE_CUSTOM_SEARCH_ENGINES
 #define BRAVE_ANDROID_TAB_GROUPS_SETTINGS
 #endif  // BUILDFLAG(IS_ANDROID)
@@ -1621,7 +1611,6 @@ constexpr flags_ui::FeatureEntry::Choice kVerticalTabCollapseDelayChoices[] = {
   TRAFFIC_CONTROL_FEATURE_ENTRIES                                              \
   BRAVE_BACKGROUND_VIDEO_PLAYBACK_ANDROID                                      \
   BRAVE_SAFE_BROWSING_ANDROID                                                  \
-  BRAVE_ADAPTIVE_BUTTON_IN_TOOLBAR_ANDROID                                     \
   BRAVE_ANDROID_TAB_GROUPS_SETTINGS                                            \
   BRAVE_CUSTOM_PROFILE_IMAGE_FEATURE_ENTRY                                     \
   BRAVE_CUSTOM_SEARCH_ENGINES                                                  \
