7384090708fc36c58d3959f853d87a30852aad85
cdesouza-chromium <cdesouza@brave.com>
2026-08-24 23:19:26 +0100

    [plaster] Delete `OVERRIDE_FEATURE_DEFAULT_STATES` - VII (#39346)
    This is the final PR in a series of PRs to get rid of
    `OVERRIDE_FEATURE_DEFAULT_STATES`. This PR deletes all the unused code
    around `OVERRIDE_FEATURE_DEFAULT_STATES`, and the code that has been
    made obsolete around the machinery for it.
    
    Bug: https://github.com/brave/brave-browser/issues/45050

--- chromium_src/components/webui/flags/flags_state.cc
+++ chromium_src/components/webui/flags/flags_state.cc
@@ -42,8 +42,7 @@ void AppendCurrentFeatureStateIfDefault(
                                              : kGenericExperimentChoiceDisabled;
 
   const bool is_feature_enabled_by_default =
-      base::FeatureList::GetCompileTimeFeatureState(feature) ==
-      base::FeatureState::FEATURE_ENABLED_BY_DEFAULT;
+      feature.default_state == base::FeatureState::FEATURE_ENABLED_BY_DEFAULT;
   const std::string_view current_state_flag =
       is_feature_enabled_now != is_feature_enabled_by_default ? "*" : "";
 
