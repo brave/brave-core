ab6ef917e89001fb689fe773f4a33997e22fb889
Claudio DeSouza <cdesouza@brave.com>
2026-08-25 13:18:27 +0100

    [WIP][cr154] `kEnableForceDownloadToOneDrive` flag deleted
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/45e9980e602c85d1d5b3e4e98a8e1d47c30f99f1
    
    commit 45e9980e602c85d1d5b3e4e98a8e1d47c30f99f1
    Author: Elias Homsi <eliashomsi@google.com>
    Date:   Mon Aug 24 12:11:01 2026 -0700
    
        Cleanup kEnableForceDownloadToCloud Finch flag
    
        Fixed: 509846031
        Bug: 509846031
        Change-Id: Ibcfc67d15a21560b713b0757ec1de52173689a80
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8274676
        Auto-Submit: Elias Homsi <eliashomsi@google.com>
        Reviewed-by: Alex Chen <alxchn@google.com>
        Reviewed-by: Lily Chen <chlily@chromium.org>
        Commit-Queue: Elias Homsi <eliashomsi@google.com>
        Cr-Commit-Position: refs/heads/main@{#1684903}

--- app/feature_defaults_unittest.cc
+++ app/feature_defaults_unittest.cc
@@ -156,7 +156,6 @@ TEST(FeatureDefaultsTest, DisabledFeatures) {
       &contextual_tasks::kContextualTasksCookiePrefetch,
 #if !BUILDFLAG(IS_ANDROID)
       &enterprise_data_protection::kEnableForceDownloadToCloud,
-      &enterprise_data_protection::kEnableForceDownloadToOneDrive,
       &extensions_features::kApiGlicPrivate,
 #endif
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_APPLE) || BUILDFLAG(IS_LINUX)
