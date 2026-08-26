b4080faf41ccfc3efbb174f5a5a8c0c5883e6a55
Claudio DeSouza <cdesouza@brave.com>
2026-08-26 12:36:44 +0100

    [cr154] Rework test after mixed-form-warning removal
    The mixed-form-warning suggestion path in `BrowserAutofillManager` was
    removed, and superseded by the full insecure-form interstitial, deleting
    IDS_AUTOFILL_WARNING_MIXED_FORM and `SuggestionType::kMixedFormMessage`.
    
    This change deletes the tests we had for this particular code and
    `.onion` origins, as that code is gone. Smaller direct tests were
    introduced in its place to test onion handling with
    `IsFormMixedContent`.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/ff87896895396f495477513f82542acfde3ebbb0
    
    commit ff87896895396f495477513f82542acfde3ebbb0
    Author: Jihad Hanna <jihadghanna@google.com>
    Date:   Tue Aug 25 09:18:36 2026 -0700
    
        Remove mixed-form warning in the old suggestion generation logic.
    
        This CL drops the requirement of having a secure form action. This has
        already happened in crrev.com/c/7959059 where we agreed to update the
        security model, but this check was not updated there, probably by
        mistake.
    
        Chrome already warns user when submitting over insecure connections
        (See `InsecureFormBlockingPage` in //components/security_interstitials/)
    
        Bug: 409962888
        Change-Id: Id4dad1d6abdec8b7d6ea2bf75281b43fe353875e
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8283379
        Reviewed-by: Rohit Rao <rohitrao@chromium.org>
        Reviewed-by: Jan Keitel <jkeitel@google.com>
        Commit-Queue: Jihad Hanna <jihadghanna@google.com>
        Cr-Commit-Position: refs/heads/main@{#1685636}

---
+++ chromium_src/components/autofill/core/browser/autofill_browser_util_unittest.cc
@@ -0,0 +1,81 @@
+/* Copyright (c) 2026 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "components/autofill/core/browser/autofill_browser_util.h"
+
+#include "components/autofill/core/browser/foundations/test_autofill_client.h"
+#include "base/test/task_environment.h"
+#include "components/autofill/core/common/form_data.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
+
+namespace autofill {
+namespace {
+
+FormData CreateFormWithAction(const GURL& action) {
+  FormData form;
+  form.set_action(action);
+  return form;
+}
+
+class AutofillBrowserUtilTest : public testing::Test {
+ protected:
+  base::test::TaskEnvironment task_environment_;
+  TestAutofillClient client_;
+};
+
+TEST_F(AutofillBrowserUtilTest, HttpsFormWithInsecureAction) {
+  client_.set_last_committed_primary_main_frame_url(GURL("https://myform.com"));
+  EXPECT_TRUE(IsFormMixedContent(
+      client_, CreateFormWithAction(GURL("http://myform.com/submit"))));
+}
+
+TEST_F(AutofillBrowserUtilTest, HttpsFormWithSecureAction) {
+  client_.set_last_committed_primary_main_frame_url(GURL("https://myform.com"));
+  EXPECT_FALSE(IsFormMixedContent(
+      client_, CreateFormWithAction(GURL("https://myform.com/submit"))));
+}
+
+TEST_F(AutofillBrowserUtilTest, HttpFormWithInsecureAction) {
+  // A plain http page isn't a secure context, so it can't have "mixed"
+  // content regardless of the action's scheme.
+  client_.set_last_committed_primary_main_frame_url(GURL("http://myform.com"));
+  EXPECT_FALSE(IsFormMixedContent(
+      client_, CreateFormWithAction(GURL("http://myform.com/submit"))));
+}
+
+// .onion pages are treated as a secure context for mixed-content purposes,
+// even when served over plain http, since Tor already provides the
+// transport security https would otherwise provide.
+TEST_F(AutofillBrowserUtilTest, OnionHttpsFormWithInsecureAction) {
+  client_.set_last_committed_primary_main_frame_url(
+      GURL("https://myform.onion"));
+  EXPECT_TRUE(IsFormMixedContent(
+      client_, CreateFormWithAction(GURL("http://myform.com/submit"))));
+}
+
+TEST_F(AutofillBrowserUtilTest, OnionHttpFormWithInsecureAction) {
+  client_.set_last_committed_primary_main_frame_url(
+      GURL("http://myform.onion"));
+  EXPECT_TRUE(IsFormMixedContent(
+      client_, CreateFormWithAction(GURL("http://myform.com/submit"))));
+}
+
+TEST_F(AutofillBrowserUtilTest, OnionHttpSubdomainFormWithInsecureAction) {
+  client_.set_last_committed_primary_main_frame_url(
+      GURL("http://a.myform.onion"));
+  EXPECT_TRUE(IsFormMixedContent(
+      client_, CreateFormWithAction(GURL("http://myform.com/submit"))));
+}
+
+TEST_F(AutofillBrowserUtilTest, OnionFormWithSecureAction) {
+  client_.set_last_committed_primary_main_frame_url(
+      GURL("http://myform.onion"));
+  EXPECT_FALSE(IsFormMixedContent(
+      client_, CreateFormWithAction(GURL("https://myform.com/submit"))));
+}
+
+}  // namespace
+}  // namespace autofill
