221e04dc184e35f887b4d8af9597bc05b9c72810
Nuo Xu <nuoxu.nx@gmail.com>
2026-08-26 12:18:45 -0400

    add back useful comments

--- ios/brave-ios/Sources/Brave/Frontend/Browser/BrowserViewController/BVC+TabPolicyDecider.swift
+++ ios/brave-ios/Sources/Brave/Frontend/Browser/BrowserViewController/BVC+TabPolicyDecider.swift
@@ -344,6 +344,8 @@ extension BrowserViewController: TabPolicyDecider {
       }
     }
 
+    // This is the normal case, opening a http or https url, which we handle by loading them in this WKWebView. We
+    // always allow this. Additionally, data URIs are also handled just like normal web pages
     if ["http", "https", "data", "blob", "file"].contains(requestURL.scheme) {
       pendingRequests[requestURL.absoluteString] = request
 
