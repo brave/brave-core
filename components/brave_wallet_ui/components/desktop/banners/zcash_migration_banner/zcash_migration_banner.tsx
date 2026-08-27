8b5e74db8647ae70a6115f2c223dddcfa134f6a4
Douglas Daniel <douglashdaniel@gmail.com>
2026-08-27 18:51:01 -0500

    [Wallet]: Zcash Ironwood Migration Banner (#39448)
    - Adds a Zcash Ironwood (NU6.3) migration warning banner that explains limited functionality during the upgrade (unshielding and transparent-to-transparent sends only).
    - Shows the banner on ZEC account details and on the Send screen when the selected asset is ZEC, with a Learn more link to the Zcash community Ironwood update post.

---
+++ components/brave_wallet_ui/components/desktop/banners/zcash_migration_banner/zcash_migration_banner.tsx
@@ -0,0 +1,33 @@
+// Copyright (c) 2026 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import * as React from 'react'
+
+// Utils
+import { getLocale } from '../../../../../common/locale'
+import { openTab } from '../../../../utils/routes-utils'
+
+// Styled Components
+import { Alert, Button } from './zcash_migration_banner.style'
+
+export function ZCashMigrationBanner() {
+  const onClickLearnMore = () => {
+    openTab(
+      'https://forum.zcashcommunity.com/t/ironwood-update-for-users/56721',
+    )
+  }
+  return (
+    <Alert type='warning'>
+      {getLocale('braveWalletZCashMigrationBannerDescription')}
+      <Button
+        kind='plain-faint'
+        size='tiny'
+        onClick={onClickLearnMore}
+      >
+        {getLocale('braveWalletLearnMore')}
+      </Button>
+    </Alert>
+  )
+}
