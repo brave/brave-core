491f55a3ba20f790d206e1de33a3beea5a542bab
yan <yan@brave.com>
2026-08-27 09:14:40 -0700

    address netzenbot comment

--- chromium_src/chrome/browser/component_updater/widevine_cdm_component_installer.cc
+++ chromium_src/chrome/browser/component_updater/widevine_cdm_component_installer.cc
@@ -5,40 +5,8 @@
 
 #include "chrome/browser/component_updater/widevine_cdm_component_installer.h"
 
-#include <memory>
 #include <optional>
-#include <utility>
 
 #include "brave/browser/widevine/widevine_utils.h"
 
-#define RegisterWidevineCdmComponent RegisterWidevineCdmComponent_ChromiumImpl
 #include <chrome/browser/component_updater/widevine_cdm_component_installer.cc>
-#undef RegisterWidevineCdmComponent
-
-namespace component_updater {
-
-namespace {
-
-class BraveWidevineCdmComponentInstallerPolicy
-    : public WidevineCdmComponentInstallerPolicy {
- private:
-  // Upstream returns false because the CRX payload is signature verified.
-  // Require TLS anyway so the request doesn't advertise in plaintext that this
-  // user is installing Widevine.
-  bool RequiresNetworkEncryption() const override { return true; }
-};
-
-}  // namespace
-
-void RegisterWidevineCdmComponent(ComponentUpdateService* cus,
-                                  base::OnceClosure callback) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  if (!IsWidevineEnabled()) {
-    return;
-  }
-  auto installer = base::MakeRefCounted<ComponentInstaller>(
-      std::make_unique<BraveWidevineCdmComponentInstallerPolicy>());
-  installer->Register(cus, std::move(callback));
-}
-
-}  // namespace component_updater
