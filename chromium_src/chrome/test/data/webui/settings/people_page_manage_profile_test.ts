44bb865d8c73c672d05db19b3a9fc3c0cb34502c
Chris Lacy <clacy@brave.com>
2026-09-01 11:39:13 +1000

    Remove unneeded test

--- chromium_src/chrome/test/data/webui/settings/people_page_manage_profile_test.ts
+++ chromium_src/chrome/test/data/webui/settings/people_page_manage_profile_test.ts
@@ -275,21 +275,6 @@ function braveManageProfileFeatureEnabledTests() {
     assertEquals(2, inputClickCount)
   })
 
-  test('PreviewButtonFillsThePreview', async function() {
-    const row = getRequiredCustomProfileImageRow(manageProfile)
-    await row.updateComplete
-
-    const preview = getRequiredElement<HTMLElement>(row.shadowRoot, '#preview')
-    assertTrue(preview.hasAttribute('fab'))
-    const button = getRequiredElement<HTMLButtonElement>(
-      preview.shadowRoot!,
-      'button',
-    )
-    assertEquals('pointer', getComputedStyle(button).cursor)
-    assertEquals(preview.clientWidth, button.offsetWidth)
-    assertEquals(preview.clientHeight, button.offsetHeight)
-  })
-
   test('UploadsValidImage', async function() {
     const row = getRequiredCustomProfileImageRow(manageProfile)
     selectFile(row, 'first.png')
