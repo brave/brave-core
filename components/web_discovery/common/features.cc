2325036fdfe30521d73bf382ccd33b5753351f10
Darnell Andries <dandries@brave.com>
2026-09-02 13:59:59 -0700

    Enable WDP Native by default on Android

--- components/web_discovery/common/features.cc
+++ components/web_discovery/common/features.cc
@@ -5,11 +5,24 @@
 
 #include "brave/components/web_discovery/common/features.h"
 
+#include "build/build_config.h"
+
 namespace web_discovery::features {
 
 BASE_FEATURE(kBraveWebDiscoveryNative,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+#if BUILDFLAG(IS_ANDROID)
+             base::FEATURE_ENABLED_BY_DEFAULT
+#else
+             base::FEATURE_DISABLED_BY_DEFAULT
+#endif
+);
 const base::FeatureParam<std::string> kPatternsPath{&kBraveWebDiscoveryNative,
-                                                    "patterns_path", ""};
+                                                    "patterns_path",
+#if BUILDFLAG(IS_ANDROID)
+                                                    "patterns-android.gz"
+#else
+                                                    ""
+#endif
+};
 
 }  // namespace web_discovery::features
