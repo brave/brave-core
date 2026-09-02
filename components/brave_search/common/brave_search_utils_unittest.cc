e014d6efcad1c61883c40582350b931b3af006a0
Darnell Andries <dandries@brave.com>
2026-09-01 17:10:13 -0700

    Direct NTP Brave search submissions to normal SERP

--- components/brave_search/common/brave_search_utils_unittest.cc
+++ components/brave_search/common/brave_search_utils_unittest.cc
@@ -5,12 +5,35 @@
 
 #include "brave/components/brave_search/common/brave_search_utils.h"
 
+#include "base/test/scoped_feature_list.h"
+#include "brave/components/ai_chat/core/common/buildflags/buildflags.h"
+#include "brave/components/brave_search/common/features.h"
+#include "components/prefs/pref_registry_simple.h"
+#include "components/prefs/testing_pref_service.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 
+#if BUILDFLAG(ENABLE_AI_CHAT)
+#include "brave/components/ai_chat/core/common/features.h"
+#include "brave/components/ai_chat/core/common/pref_names.h"
+#endif
+
 namespace brave_search {
 
-TEST(BraveSearchUtilsUnitTest, IsAllowedHost) {
+class BraveSearchUtilsUnitTest : public testing::Test {
+ protected:
+  void SetUp() override {
+#if BUILDFLAG(ENABLE_AI_CHAT)
+    local_state_.registry()->RegisterBooleanPref(
+        ai_chat::prefs::kNtpInputDayZeroEnabled, false);
+#endif
+  }
+
+  TestingPrefServiceSimple local_state_;
+  base::test::ScopedFeatureList feature_list_;
+};
+
+TEST_F(BraveSearchUtilsUnitTest, IsAllowedHost) {
   EXPECT_TRUE(IsAllowedHost(GURL("https://safesearch.brave.com")));
   EXPECT_TRUE(IsAllowedHost(GURL("https://safesearch.bravesoftware.com")));
   EXPECT_TRUE(IsAllowedHost(GURL("https://search-dev-local.brave.com")));
@@ -26,5 +49,29 @@ TEST(BraveSearchUtilsUnitTest, IsAllowedHost) {
   EXPECT_FALSE(IsAllowedHost(GURL("https://a.search.brave.com")));
   EXPECT_FALSE(IsAllowedHost(GURL("https://search.brave.com.au")));
 }
+
+TEST_F(BraveSearchUtilsUnitTest, AppendsNewTabSource) {
+  const GURL url("https://search.brave.com/search?q=test");
+  EXPECT_EQ("https://search.brave.com/search?q=test&source=newtab",
+            OverrideWithNewTabSource(url, &local_state_, false).spec());
+}
+
+TEST_F(BraveSearchUtilsUnitTest, AppendsNewTabV1Source) {
+  feature_list_.InitAndEnableFeature(features::kSearchNewTabV1Source);
+  const GURL url("https://search.brave.com/search?q=test");
+  EXPECT_EQ("https://search.brave.com/search?q=test&source=newtab_v1",
+            OverrideWithNewTabSource(url, &local_state_, false).spec());
+}
+
+#if BUILDFLAG(ENABLE_AI_CHAT)
+TEST_F(BraveSearchUtilsUnitTest, AppendsNewTabV2Source) {
+  feature_list_.InitWithFeatures(
+      /*enabled_features=*/{ai_chat::features::kShowAIChatInputOnNewTabPage},
+      /*disabled_features=*/{});
+  const GURL url("https://search.brave.com/search?q=test");
+  EXPECT_EQ("https://search.brave.com/search?q=test&source=newtab_v2",
+            OverrideWithNewTabSource(url, &local_state_, false).spec());
+}
+#endif
 
 }  // namespace brave_search
