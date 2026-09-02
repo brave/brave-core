b69c0b7e8aea42aa245ad671e8b846c23911dcaa
Claudio DeSouza <cdesouza@brave.com>
2026-09-02 13:17:28 +0100

    [cr155][plaster] `//chrome/browser/history` 🩹
    This strictly migrates this paths macro replacements to plaster.

--- chromium_src/chrome/browser/history/history_tab_helper.cc
+++ chromium_src/chrome/browser/history/history_tab_helper.cc
@@ -10,7 +10,6 @@
 
 #include "chrome/browser/history/history_tab_helper.h"
 
-// Include these here to avoid overriding "IsOffTheRecord" in them.
 #include "brave/components/request_otr/browser/request_otr_storage_tab_helper.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
@@ -18,30 +17,26 @@
 #include "content/public/browser/browser_context.h"
 
 #if BUILDFLAG(IS_ANDROID)
-// Include these here to avoid overriding "IsOffTheRecord" in them.
 #include "chrome/browser/ui/android/tab_model/tab_model.h"
 #endif
 
 namespace {
 
 bool BraveTabRequestedOffTheRecord(content::WebContents* web_contents) {
+#if BUILDFLAG(ENABLE_REQUEST_OTR)
   if (request_otr::RequestOTRStorageTabHelper* tab_storage =
           request_otr::RequestOTRStorageTabHelper::FromWebContents(
               web_contents)) {
     return tab_storage->has_requested_otr();
   }
   return false;
+#else
+  return false;
+#endif
 }
 
 }  // namespace
 
-#define IsOffTheRecord() \
-  IsOffTheRecord() || BraveTabRequestedOffTheRecord(web_contents())
-
 #endif
 
 #include <chrome/browser/history/history_tab_helper.cc>
-
-#if BUILDFLAG(ENABLE_REQUEST_OTR)
-#undef IsOffTheRecord
-#endif
