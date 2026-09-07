d79b945ddfbb50a38999ae00d58678bcc8bd3e96
Claudio DeSouza <cdesouza@brave.com>
2026-09-07 13:55:14 +0100

    [plaster][cr155] Adjust plaster tests to `gn` output
    There has been some recent subtle change in behaviour for the
    formatter, and this change corrects the failing test we had.

--- tools/cr/plaster_test.py
+++ tools/cr/plaster_test.py
@@ -9279,6 +9279,7 @@ class GnEditDispatchTest(unittest.TestCase):
             'BUILD.gn', 'source_set("a") {\n'
             '  deps = []\n'
             '}\n'
+            '\n'
             'source_set("b") {\n'
             '  deps = []\n'
             '}\n', 'substitutions:\n'
@@ -9291,6 +9292,7 @@ class GnEditDispatchTest(unittest.TestCase):
             result, 'source_set("a") {\n'
             '  deps = [ "//brave/a" ]\n'
             '}\n'
+            '\n'
             'source_set("b") {\n'
             '  deps = []\n'
             '}\n')
