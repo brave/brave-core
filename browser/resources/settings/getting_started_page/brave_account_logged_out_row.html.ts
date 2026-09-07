608fc8ca9f475c5da4d023d46636a89c5c33ecd8
Claudio DeSouza <cdesouza@brave.com>
2026-09-07 11:39:26 +0100

    [cr155] `getHtml()` must return a tagged template literal
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/81671e70304b1775014d2fa91dd87b7b900c297a
    
    commit 81671e70304b1775014d2fa91dd87b7b900c297a
    Author: rbpotter <rbpotter@chromium.org>
    Date:   Thu Sep 3 15:58:05 2026 -0700
    
        WebUI: Enforce that getHtml() returns a tagged template literal
    
        This is useful for HTML formatting to handle a better defined subset
        of cases.
    
        Bug: 512911634
        Change-Id: Ia68c96b8b1f63d3372a0d387712a80f4809db163
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7794396
        Reviewed-by: John Lee <johntlee@chromium.org>
        Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1691998}

--- browser/resources/settings/getting_started_page/brave_account_logged_out_row.html.ts
+++ browser/resources/settings/getting_started_page/brave_account_logged_out_row.html.ts
@@ -10,7 +10,7 @@ import { BraveAccountSettingsStrings } from '../brave_components_webui_strings.j
 import { LoggedOutVerificationIntent } from '../brave_account.mojom-webui.js'
 
 export function getHtml(this: BraveAccountLoggedOutRowElement) {
-  return this.state.verification
+  return html`${this.state.verification
     ? html`
       <div class="first-row">
         <leo-icon name="social-brave-release-favicon-fullheight-color">
@@ -83,5 +83,5 @@ export function getHtml(this: BraveAccountLoggedOutRowElement) {
               BraveAccountSettingsStrings
                    .SETTINGS_BRAVE_ACCOUNT_GET_STARTED_BUTTON_LABEL)}
         </leo-button>
-      </div>`
+      </div>`}`
 }
