2ff23b12736b3904eb2e173eacb7a4340761e459
Artem Samoilenko <artem@brave.com>
2026-09-08 13:57:04 -0400

    [cr155][Android] Grey out toolbar button for unfocused window in split window
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/fc34d05ab5f0368e70bbb19b138fb838adbbf300
    
    commit fc34d05ab5f0368e70bbb19b138fb838adbbf300
    Author: Zhe Li <zheliooo@google.com>
    Date:   Wed Sep 2 18:36:04 2026 -0700
    
        [Toolbar] Grey out toolbar button for unfocused window in split window
    
        - Make AppThemeColorProvider observe MultiWindowModeStateDispatcher.
        - Pass WindowAndroid to determine initial focus state (e.g. when the other window is newly launched in split-screen).
        - Add a DEPS entry for the multi-window dependency.
    
        Demo: https://drive.google.com/file/d/1V_KwLhRZzO9j9XFFG6v1_r5erhwmDYCN/view?usp=sharing
        Fixed: 542671313
        Change-Id: I9b529da1310a41e3f2dbbd0a7431f1d953de6f7f
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8280453
        Commit-Queue: Zhe Li <zheliooo@google.com>
        Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
        Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
        Cr-Commit-Position: refs/heads/main@{#1691265}

--- android/java/org/chromium/chrome/browser/tabbed_mode/BraveTabbedRootUiCoordinator.java
+++ android/java/org/chromium/chrome/browser/tabbed_mode/BraveTabbedRootUiCoordinator.java
@@ -39,6 +39,7 @@ import org.chromium.chrome.browser.keyboard_accessory.ManualFillingComponent;
 import org.chromium.chrome.browser.layouts.LayoutStateProvider;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
 import org.chromium.chrome.browser.multiwindow.MultiInstanceManager;
+import org.chromium.chrome.browser.multiwindow.MultiWindowModeStateDispatcher;
 import org.chromium.chrome.browser.privacy.settings.BravePrivacySettings;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.safe_browsing.AdvancedProtectionCoordinator;
@@ -104,6 +105,7 @@ public class BraveTabbedRootUiCoordinator extends TabbedRootUiCoordinator {
             ActivityResultTracker activityResultTracker,
             OneshotSupplier<ChromeAndroidTask> chromeAndroidTaskSupplier,
             ActivityLifecycleDispatcher activityLifecycleDispatcher,
+            MultiWindowModeStateDispatcher multiWindowModeStateDispatcher,
             MonotonicObservableSupplier<LayoutManagerImpl> layoutManagerSupplier,
             MenuOrKeyboardActionController menuOrKeyboardActionController,
             Supplier<Integer> activityThemeColorSupplier,
@@ -160,6 +162,7 @@ public class BraveTabbedRootUiCoordinator extends TabbedRootUiCoordinator {
                 activityResultTracker,
                 chromeAndroidTaskSupplier,
                 activityLifecycleDispatcher,
+                multiWindowModeStateDispatcher,
                 layoutManagerSupplier,
                 menuOrKeyboardActionController,
                 activityThemeColorSupplier,
