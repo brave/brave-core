7c469e0927585f3e3dc1be6e1e24a249158ebb13
Claudio DeSouza <cdesouza@brave.com>
2026-09-10 11:16:50 +0100

    [cr155] Remove `ExclusiveAccessManager::fullscreen_controller`
    This change removes the `const` override for this function we were
    inserting, as that was breaking upstream code that couldn't disambiguate
    the override when dispatching work.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/dbebb621df847dcef135161100188b095a4197f0
    
    commit dbebb621df847dcef135161100188b095a4197f0
    Author: Kunal Daftari <kunaldaftari@google.com>
    Date:   Tue Sep 8 17:46:30 2026 -0700
    
        [SafeInvoke] Implementing in c/b/ui/views/frame (CL 3)
    
        Modifies various nested if-statement blocks to adopt the SafeInvoke
        pattern. This reduces LOC, enhances readability, and ensures null
        safety.
    
        See bug for description of the SafeInvoke utility.
    
        Bug: 555906346
        Change-Id: I28bb10c17b1b9e2e6667cbdbb2441cf7d3421d9e
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8339756
        Reviewed-by: Darryl James <dljames@chromium.org>
        Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
        Cr-Commit-Position: refs/heads/main@{#1694238}

--- chromium_src/chrome/browser/ui/views/frame/layout/browser_view_layout_delegate_impl.cc
+++ chromium_src/chrome/browser/ui/views/frame/layout/browser_view_layout_delegate_impl.cc
@@ -10,6 +10,7 @@
 #include "brave/browser/ui/views/frame/brave_contents_view_util.h"
 #include "brave/browser/ui/views/tabs/vertical_tab_utils.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/exclusive_access/exclusive_access_manager.h"
 #include "chrome/browser/ui/exclusive_access/fullscreen_controller.h"
 #include "chrome/browser/ui/side_panel/side_panel_ui.h"
@@ -78,8 +79,8 @@ bool BrowserViewLayoutDelegateImpl::IsFullscreenForBrowser() const {
   if (!browser_view().browser()) {
     return false;
   }
-  const ExclusiveAccessManager* exclusive_access_manager =
-      ExclusiveAccessManager::From(browser_view().browser());
+  auto* const exclusive_access_manager = ExclusiveAccessManager::From(
+      const_cast<BrowserWindowInterface*>(browser_view().browser()));
   if (!exclusive_access_manager) {
     return false;
   }
@@ -93,8 +94,8 @@ bool BrowserViewLayoutDelegateImpl::IsFullscreenForTab() const {
   if (!browser_view().browser()) {
     return false;
   }
-  const ExclusiveAccessManager* exclusive_access_manager =
-      ExclusiveAccessManager::From(browser_view().browser());
+  auto* const exclusive_access_manager = ExclusiveAccessManager::From(
+      const_cast<BrowserWindowInterface*>(browser_view().browser()));
   if (!exclusive_access_manager) {
     return false;
   }
