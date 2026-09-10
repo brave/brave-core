517d19759f80a64a2d653bb7a8b9b67e739ef2a1
Pavel Beloborodov <73575789+boocmp@users.noreply.github.com>
2026-09-10 20:41:52 +0700

    Enabled Speedreader TTS feature param by default. (#39506)

--- components/speedreader/common/features.cc
+++ components/speedreader/common/features.cc
@@ -15,7 +15,7 @@ const base::FeatureParam<int> kSpeedreaderMinOutLengthParam{
     &kSpeedreaderFeature, "min_out_length", 1000};
 
 const base::FeatureParam<bool> kSpeedreaderTTS{&kSpeedreaderFeature, "tts",
-                                               false};
+                                               true};
 
 const base::FeatureParam<bool> kSpeedreaderDebugView{&kSpeedreaderFeature,
                                                      "debug_view", false};
