a49972330fc77cd2b3ac95e7a16a4c7deb13cab0
Rohit Agarwal <ragarwal@brave.com>
2026-09-10 17:50:34 +0200

    update

--- chromium_src/content/public/browser/content_browser_client.h
+++ chromium_src/content/public/browser/content_browser_client.h
@@ -9,6 +9,7 @@
 #include "brave/components/brave_shields/core/common/shields_settings.mojom.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "third_party/blink/public/mojom/loader/referrer.mojom.h"
+#include "url/origin.h"
 
 namespace content {
 class StoragePartitionConfig;
