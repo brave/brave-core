e2ea3f4653844c926dd5f06d24858cccafad85fb
Christian Mazakas <christian.mazakas@gmail.com>
2026-09-10 10:45:11 -0700

    add JSPolkadotProvider stub

--- chromium_src/gin/public/wrappable_pointer_tags.h
+++ chromium_src/gin/public/wrappable_pointer_tags.h
@@ -15,7 +15,8 @@
   kSolanaProvider,                    \
   kSkusBindings,                      \
   SpeedreaderBindings,                \
-  kCardanoProvider
+  kCardanoProvider,                   \
+  kPolkadotProvider
 // clang-format on
 
 #include <gin/public/wrappable_pointer_tags.h>  // IWYU pragma: export
