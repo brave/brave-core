eb42cdd9a3b2c235f8ee575af813320044950261
Darnell Andries <dandries@brave.com>
2026-09-10 21:42:25 -0700

    Add wallet usage metrics

--- components/p3a/metric_names.h
+++ components/p3a/metric_names.h
@@ -241,6 +241,7 @@ inline constexpr auto kCollectedTypicalHistograms =
     {"Brave.VerticalTabs.GroupTabs", MetricConfig{.ephemeral = true}},
     {"Brave.VerticalTabs.OpenTabs", MetricConfig{.ephemeral = true}},
     {"Brave.VerticalTabs.PinnedTabs", MetricConfig{.ephemeral = true}},
+    {"Brave.Wallet.UsageWeekly", MetricConfig{.ephemeral = true}},
     {"Brave.Web3.DappVisited", MetricConfig{.ephemeral = true}},
     {"Brave.WebTorrent.UsageWeekly", MetricConfig{.ephemeral = true}},
 });
@@ -270,6 +271,7 @@ inline constexpr auto kCollectedSlowHistograms =
     {"Brave.Today.DirectFeedsTotal.3", MetricConfig{.ephemeral = true}},
     {"Brave.Today.PublisherCount.2", MetricConfig{.ephemeral = true}},
     {"Brave.Today.UsageMonthly", MetricConfig{.ephemeral = true}},
+    {"Brave.Wallet.UsageMonthly", MetricConfig{.ephemeral = true}},
 });
 
 inline constexpr auto kCollectedExpressHistograms =
@@ -404,6 +406,7 @@ inline constexpr auto kCollectedExpressHistograms =
       .record_activation_date = true
     }},
     {"Brave.Uptime.BrowserOpenTime.2", MetricConfig{.ephemeral = true}},
+    {"Brave.Wallet.UsageDaily", MetricConfig{.ephemeral = true}},
     {"Brave.Welcome.InteractionStatus.2", MetricConfig{
       .attributes = MetricAttributes{MetricAttribute::kAnswerIndex, MetricAttribute::kChannel, MetricAttribute::kPlatform, MetricAttribute::kDateOfInstall, MetricAttribute::kSubregion, MetricAttribute::kVersion},
     }},
