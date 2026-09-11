04c23943118e5420a35afaabded2b696b9291e51
Jay Harris <jay.harris@outlook.co.nz>
2026-09-11 13:35:30 +1200

    Add tests

--- chromium_src/content/browser/loader/navigation_url_loader_impl.cc
+++ chromium_src/content/browser/loader/navigation_url_loader_impl.cc
@@ -3,10 +3,15 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+#include <algorithm>
+#include <string>
+
 #include "content/browser/webui/url_data_manager_backend.h"
 #include "content/public/browser/web_ui_url_loader_factory.h"
 #include "content/public/browser/webui_config.h"
 #include "content/public/browser/webui_config_map.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
+#include "services/network/public/mojom/url_loader_factory.mojom.h"
 #include "url/gurl.h"
 
 namespace content {
@@ -19,9 +24,6 @@ namespace {
 // Whether |url| is a WebUI that has asked to be reachable by a service worker,
 // and so must not take the shortcut in NavigationURLLoaderImpl::Start() that
 // loads WebUI without consulting any interceptor.
-//
-// Hosts that have not asked keep the shortcut, so this changes the way a
-// navigation is loaded only where a WebUI opted in.
 bool ShouldInterceptWebUINavigation(BrowserContext* browser_context,
                                     const GURL& url) {
   WebUIConfig* config =
@@ -29,10 +31,6 @@ bool ShouldInterceptWebUINavigation(BrowserContext* browser_context,
   return config && config->ShouldInterceptNavigationsWithServiceWorker();
 }
 
-// The factory the shortcut would have used, for an opted-in host whose service
-// worker did not serve the navigation after all. Reached through
-// CreateTerminalNonNetworkLoaderFactory(), which knows every other non-network
-// scheme but not WebUI, because until now WebUI never got that far.
 mojo::PendingRemote<network::mojom::URLLoaderFactory>
 MaybeCreateWebUILoaderFactory(FrameTreeNode* frame_tree_node, const GURL& url);
 
