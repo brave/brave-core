fcb1d17c2ee71c91da2c54435c0bb377a75b41ce
Chris Lacy <clacy@brave.com>
2026-09-11 13:23:04 +1000

    Add `brave-youtube-fullscreen-settings-workaround` flag

--- browser/android/youtube_script_injector/features.h
+++ browser/android/youtube_script_injector/features.h
@@ -13,6 +13,7 @@ namespace features {
 
 BASE_DECLARE_FEATURE(kBraveBackgroundVideoPlayback);
 BASE_DECLARE_FEATURE(kBravePictureInPictureForYouTubeVideos);
+BASE_DECLARE_FEATURE(kBraveYoutubeFullscreenSettingsWorkaround);
 
 }  // namespace features
 }  // namespace preferences
