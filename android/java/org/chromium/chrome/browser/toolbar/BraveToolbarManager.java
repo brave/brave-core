5a6afdc6a6112fa801e1ee82daa9984abd5ee51e
Artem Samoilenko <artem@brave.com>
2026-09-14 11:51:48 -0400

    [cr155][Android] FindToolbarManager is now lazy
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/68f8c39ac79309ae43a6b5dd56674ed1590911ed
    
    commit 68f8c39ac79309ae43a6b5dd56674ed1590911ed
    Author: Calder Kitagawa <ckitagawa@chromium.org>
    Date:   Fri Sep 11 13:49:35 2026 -0700
    
        [Startup] Make FindToolbarManager lazy
    
        Bug: 525079251
        Change-Id: Ie514a683ddb3065ffa036400d6442b3e0fe3cbcd
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8282775
        Reviewed-by: Sky Malice <skym@chromium.org>
        Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
        Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1696573}

--- android/java/org/chromium/chrome/browser/toolbar/BraveToolbarManager.java
+++ android/java/org/chromium/chrome/browser/toolbar/BraveToolbarManager.java
@@ -19,6 +19,7 @@ import org.chromium.base.BravePreferenceKeys;
 import org.chromium.base.Callback;
 import org.chromium.base.CallbackController;
 import org.chromium.base.ContextUtils;
+import org.chromium.base.supplier.LazyOneshotSupplier;
 import org.chromium.base.supplier.MonotonicObservableSupplier;
 import org.chromium.base.supplier.NonNullObservableSupplier;
 import org.chromium.base.supplier.NullableObservableSupplier;
@@ -231,7 +232,7 @@ public class BraveToolbarManager extends ToolbarManager
             ActivityTabProvider tabProvider,
             ScrimManager scrimManager,
             ToolbarActionModeCallback toolbarActionModeCallback,
-            FindToolbarManager findToolbarManager,
+            LazyOneshotSupplier<FindToolbarManager> findToolbarManagerSupplier,
             MonotonicObservableSupplier<@Nullable Profile> profileSupplier,
             NullableObservableSupplier<BookmarkModel> bookmarkModelSupplier,
             OneshotSupplier<LayoutStateProvider> layoutStateProviderSupplier,
@@ -296,7 +297,7 @@ public class BraveToolbarManager extends ToolbarManager
                 tabProvider,
                 scrimManager,
                 toolbarActionModeCallback,
-                findToolbarManager,
+                findToolbarManagerSupplier,
                 profileSupplier,
                 bookmarkModelSupplier,
                 layoutStateProviderSupplier,
