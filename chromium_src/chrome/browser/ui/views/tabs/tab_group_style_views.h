7d6be24f216918defe1fb8ed3940f39d9197042c
Claudio DeSouza <cdesouza@brave.com>
2026-09-14 10:56:53 +0100

    [cr155] `TabGroupStyle` split with `TabGroupStyleViews added
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/8277d76b10f9eb23a6dba489230f51236cff0aa2
    
    commit 8277d76b10f9eb23a6dba489230f51236cff0aa2
    Author: Vince Lugli <lugli@google.com>
    Date:   Fri Sep 11 12:38:47 2026 -0700
    
        [Unified Tabstrip] Split TabGroupStyle into TabGroupStyle and TabGroupStyleViews.
    
        Split TabGroupStyle into common/TabGroupStyle for static utility
        functions with no views dependency and TabGroupStyleViews,
        views-dependent styling methods holding a reference to TabGroupViews.
    
        Update BUILD.gn files and all call sites in tabs and tabs/common.
    
        Move new_tab_button_menu to shared.
    
        Change-Id: I9cca730cf4aca2703f56cb3d5dbd17d0c0220f3a
        Bug: 558825669
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8372584
        Reviewed-by: Eshwar Stalin <estalin@chromium.org>
        Commit-Queue: Vince Lugli <lugli@google.com>
        Cr-Commit-Position: refs/heads/main@{#1696514}

--- chromium_src/chrome/browser/ui/views/tabs/tab_group_style_views.h
+++ chromium_src/chrome/browser/ui/views/tabs/tab_group_style_views.h
@@ -3,16 +3,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_TABS_TAB_GROUP_STYLE_H_
-#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_TABS_TAB_GROUP_STYLE_H_
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_TABS_TAB_GROUP_STYLE_VIEWS_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_TABS_TAB_GROUP_STYLE_VIEWS_H_
 
-#define TabGroupStyle TabGroupStyle_ChromiumImpl
-#include <chrome/browser/ui/views/tabs/tab_group_style.h>  // IWYU pragma: export
-#undef TabGroupStyle
+#define TabGroupStyleViews TabGroupStyleViews_ChromiumImpl
+#include <chrome/browser/ui/views/tabs/tab_group_style_views.h>  // IWYU pragma: export
+#undef TabGroupStyleViews
 
-class TabGroupStyle : public TabGroupStyle_ChromiumImpl {
+class TabGroupStyleViews : public TabGroupStyleViews_ChromiumImpl {
  public:
-  using TabGroupStyle_ChromiumImpl::TabGroupStyle_ChromiumImpl;
+  using TabGroupStyleViews_ChromiumImpl::TabGroupStyleViews_ChromiumImpl;
 
   static constexpr int kStrokeThicknessForVerticalTabs = 4;
 
@@ -28,4 +28,4 @@ class TabGroupStyle : public TabGroupStyle_ChromiumImpl {
   bool ShouldShowBraveVerticalTabs() const;
 };
 
-#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_TABS_TAB_GROUP_STYLE_H_
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_TABS_TAB_GROUP_STYLE_VIEWS_H_
