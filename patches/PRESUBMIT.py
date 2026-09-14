3a7e58b908ffd8f6549e1a1b48fda5366f5eedbb
Aleksei Khoroshilov <akhoroshilov@brave.com>
2026-09-15 00:56:11 +0700

    Remove hunk boundary patch check (conflicts with plaster usually).

--- patches/PRESUBMIT.py
+++ patches/PRESUBMIT.py
@@ -25,40 +25,6 @@ def CheckTests(input_api, output_api):
         input_api, output_api, '.', files_to_check=TEST_FILE_PATTERN)
 
 
-def CheckPatchFile(input_api, output_api):
-    files_to_check = (r'.+\.patch$', )
-    files_to_skip = ()
-
-    file_filter = lambda f: input_api.FilterSourceFile(
-        f, files_to_check=files_to_check, files_to_skip=files_to_skip)
-
-    items = []
-    for f in input_api.AffectedSourceFiles(file_filter):
-        contents = list(f.NewContents())
-        for lineno, line in enumerate(contents, 1):
-            # Only check empty added/removed lines.
-            if line not in ('+', '-'):
-                continue
-
-            # Check if this empty line is part of a diff hunk.
-            is_in_hunk = False
-            if 1 < lineno < len(contents):
-                is_in_hunk = (contents[lineno - 2].startswith(line)
-                              and contents[lineno].startswith(line))
-
-            if not is_in_hunk:
-                items.append(f'{f.LocalPath()}:{lineno} ({line}empty line)')
-
-    if not items:
-        return []
-
-    return [
-        output_api.PresubmitPromptWarning(
-            'Patch should not add or remove empty lines at hunk boundaries',
-            items)
-    ]
-
-
 def CheckPatchFileSource(input_api, output_api):
     """Checks that the patchfile is patching the expected source."""
     files_to_check = (r'.+\.patch$', )
