578e589526157c40aa9c4afe74b117d22ba142f2
Claudio DeSouza <cdesouza@brave.com>
2026-09-15 12:48:09 +0100

    [cr155] `ScopedChromeExtensionsClient` introduced
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/b236b6c3de56b462d01d800e80fbc716dfd15e2c
    
    commit b236b6c3de56b462d01d800e80fbc716dfd15e2c
    Author: Hidehiko Abe <hidehiko@chromium.org>
    Date:   Sun Sep 13 22:33:07 2026 -0700
    
        Support resetting ExtensionsClient and introduce ScopedChromeExtensionsClient
    
        Update ExtensionsClient::Set(ExtensionsClient* client) so that:
        - If client is null, g_client is reset to nullptr.
        - If client is non-null, CHECK(!g_client) verifies that no client
          has already been set before initializing and setting g_client.
    
        Replace EnsureExtensionsClientInitialized() with an RAII class
        ScopedChromeExtensionsClient for //chrome to manage the lifetime
        of the process-wide ChromeExtensionsClient instance.
        Use ScopedChromeExtensionsClient in BrowserProcessImpl, StartupHelper,
        ChromeContentRendererClient, and ChromeUnitTestSuite.
    
        Also update ChromeExtensionsClientTest in chrome_extensions_client_unittest.cc:
        - Remove SetUp() and TearDown(), as ChromeUnitTestSuite already
          initializes the suite-level ChromeExtensionsClient for unit_tests.
        - Update FeatureDelegatedAvailabilityCheckMap to test against a local
          ChromeExtensionsClient instance so it does not mutate the process-wide
          client.
    
        TAG=agy
        CONV=04fa7772-ef71-470e-ad43-d9d117f5c2c5
    
        Bypass-Check-License: Move files.
        Change-Id: I856622d847806a2614cb3b40c7a4733fbac89108
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8377803
        Reviewed-by: James Cook <jamescook@chromium.org>
        Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1697047}

---
+++ chromium_src/chrome/common/scoped_chrome_extensions_client.cc
@@ -0,0 +1,8 @@
+/* Copyright (c) 2026 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "brave/common/extensions/brave_extensions_client.h"
+
+#include <chrome/common/scoped_chrome_extensions_client.cc>
