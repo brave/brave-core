de574fac374c500acfbaafe81ac4ec4379a4a15d
brave-builds <45370463+brave-builds@users.noreply.github.com>
2026-09-15 19:18:04 +0100

    [Android] Fix VerifyError when opening a bookmark in a new tab (uplift to 1.95.x) (#39847)
    Uplift of #39837 (squashed) to release

--- build/android/bytecode/java/org/brave/bytecode/BraveContentViewClassAdapter.java
+++ build/android/bytecode/java/org/brave/bytecode/BraveContentViewClassAdapter.java
@@ -12,9 +12,15 @@ public class BraveContentViewClassAdapter extends BraveClassVisitor {
             "org/chromium/components/embedder_support/view/ContentView";
     static String sBraveContentViewClassName =
             "org/chromium/components/embedder_support/view/BraveContentView";
+    static String sDeferredContentViewStubClassName =
+            "org/chromium/chrome/browser/tab/TabImpl$DeferredContentViewStub";
 
     public BraveContentViewClassAdapter(ClassVisitor visitor) {
         super(visitor);
+
+        // The constructor redirect below also rewrites this subclass' `super(...)` call, so
+        // its superclass has to be changed to match or it fails dex verification.
+        changeSuperName(sDeferredContentViewStubClassName, sBraveContentViewClassName);
 
         redirectConstructor(sContentViewClassName, sBraveContentViewClassName);
     }
