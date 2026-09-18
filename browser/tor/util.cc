3a60547e49edb95d8387937386c806eee5c67320
Claudio DeSouza <cdesouza@brave.com>
2026-09-18 10:31:22 +0100

    [cr156] `IncognitoModePrefs::GetAvailability` takes `Profile*`
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/29173a9c4a0eaeb88272bf33226140a84810641a
    
    commit 29173a9c4a0eaeb88272bf33226140a84810641a
    Author: Janek Zarzycki <janzarzycki@google.com>
    Date:   Wed Sep 16 02:13:16 2026 -0700
    
        [Isolated mode] Pass Profile to IncognitoModePrefs::GetAvailability
    
        Refactors the GetAvailability to accept a Profile pointer
        instead of a PrefService pointer.
    
        This is a prerequisite for using `enterprise_isolated_mode::IsolatedModeReplacesIncognito(Profile*)` while calculating Incognito availability.
    
        Bug: b:556068487
        Change-Id: Ia70203ebf859a3a90b695043d23180ca46550a02
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8353969
        Commit-Queue: Janek Zarzycki <janzarzycki@google.com>
        Reviewed-by: Jan Keitel <jkeitel@google.com>
        Cr-Commit-Position: refs/heads/main@{#1698770}

--- browser/tor/util.cc
+++ browser/tor/util.cc
@@ -6,15 +6,15 @@
 #include "brave/browser/tor/util.h"
 
 #include "chrome/browser/prefs/incognito_mode_prefs.h"
+#include "chrome/browser/profiles/profile.h"
 #include "components/policy/core/common/policy_pref_names.h"
-#include "components/user_prefs/user_prefs.h"
 #include "content/public/browser/browser_context.h"
 
 namespace tor {
 
 bool IsIncognitoDisabledOrForced(content::BrowserContext* context) {
   const auto availability =
-      IncognitoModePrefs::GetAvailability(user_prefs::UserPrefs::Get(context));
+      IncognitoModePrefs::GetAvailability(Profile::FromBrowserContext(context));
   return availability == policy::IncognitoModeAvailability::kDisabled ||
          availability == policy::IncognitoModeAvailability::kForced;
 }
