80b5de8d81261d2749d1a8c5884b19e0b834bc5e
StephenHeaps <5314553+StephenHeaps@users.noreply.github.com>
2026-09-18 16:27:14 -0400

    Enable Shields panel redesign by default on iOS (#40009)

--- components/brave_shields/core/common/features.cc
+++ components/brave_shields/core/common/features.cc
@@ -157,12 +157,7 @@ constexpr base::FeatureParam<int>
     kAdblockOverrideRegexDiscardPolicyDiscardUnusedSec{
         &kAdblockOverrideRegexDiscardPolicy, "discard_unused_sec", 180};
 
-BASE_FEATURE(kShowUpdatedShieldsPanel,
-#if BUILDFLAG(IS_IOS)
-             base::FEATURE_DISABLED_BY_DEFAULT);
-#else
-             base::FEATURE_ENABLED_BY_DEFAULT);
-#endif
+BASE_FEATURE(kShowUpdatedShieldsPanel, base::FEATURE_ENABLED_BY_DEFAULT);
 
 // When enabled, adblock engines are serialized to DAT files on disk after
 // filter set loading. On subsequent startups, the cached DAT is loaded
