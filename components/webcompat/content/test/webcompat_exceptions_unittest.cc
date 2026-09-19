679304be8240b5ada23bb28ca6cc21a07c95c4a6
Emerick Rogul <erogul@brave.com>
2026-09-19 13:32:28 -0400

    fixup! [cr156] chrome_test_utils.h requires //chrome/test/base:chrome_test_utils

--- components/webcompat/content/test/webcompat_exceptions_unittest.cc
+++ components/webcompat/content/test/webcompat_exceptions_unittest.cc
@@ -6,13 +6,9 @@
 #include <memory>
 #include <string_view>
 
-#include "base/feature_list.h"
 #include "base/test/scoped_feature_list.h"
 #include "brave/components/brave_component_updater/browser/local_data_files_service.h"
 #include "brave/components/brave_shields/core/browser/brave_shields_utils.h"
-#include "brave/components/brave_shields/core/common/brave_shield_constants.h"
-#include "brave/components/brave_shields/core/common/features.h"
-#include "brave/components/constants/brave_paths.h"
 #include "brave/components/webcompat/content/browser/webcompat_exceptions_service.h"
 #include "brave/components/webcompat/core/common/features.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
@@ -23,8 +19,8 @@
 #include "components/content_settings/core/common/content_settings.h"
 #include "components/content_settings/core/common/content_settings_types.h"
 #include "content/public/test/browser_task_environment.h"
-#include "content/public/test/test_utils.h"
 #include "url/origin.h"
+#include "testing/gtest/include/gtest/gtest.h"
 
 using brave_shields::ControlType;
 using content_settings::mojom::ContentSettingsType;
