21f29d3a8bcc74c8f9abaa3b9fbf910f5ff57fdb
Claudio DeSouza <cdesouza@brave.com>
2026-09-24 11:46:47 +0100

    Update from Chromium 154.0.8037.58 to Chromium 155.0.8059.16.

--- chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
+++ chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
@@ -546,9 +546,9 @@ constexpr std::string_view kBraveHstsJson = R"brave_hsts_json({
  ]})brave_hsts_json";
 
 constexpr std::string_view kBraveCerts = R"brave_certs(
-# Last updated: Wed Sep 16 10:38:58 2026
+# Last updated: Thu Sep 24 11:46:46 2026
 PinsListTimestamp
-1789551538
+1790246806
 
 # =====BEGIN BRAVE ROOTS ASC=====
 #From https://www.amazontrust.com/repository/
