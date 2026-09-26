628d2d4c54670f76085034b01836be8b561da2a8
Emerick Rogul <erogul@brave.com>
2026-09-26 08:03:49 -0400

    fixup! [bedrock] Remove unused `browser.h` inclusions

--- chromium_src/chrome/browser/ui/views/frame/browser_frame_view_win.cc
+++ chromium_src/chrome/browser/ui/views/frame/browser_frame_view_win.cc
@@ -4,6 +4,7 @@
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
 #include "brave/browser/ui/tabs/public/vertical_tab_controller.h"
+#include "chrome/browser/ui/window_feature_controller/window_feature_controller.h"
 
 // Override invocation of Browser::SupportsWindowFeature() and
 // WebUITabStripContainerView::SupportsTouchableTabStrip() in the constructor in
