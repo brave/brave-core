723304bb2036cdc1060259eb3b1add0343d979ec
Claudio DeSouza <cdesouza@brave.com>
2026-09-26 03:41:27 +0100

    [cr156] `FirstPartySetMetadata` dropped from `CookieSettings`
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/b749bc4d79b8ce0668165551b54f4a987ad31907
    
    commit b749bc4d79b8ce0668165551b54f4a987ad31907
    Author: Chris Fredrickson <cfredric@chromium.org>
    Date:   Thu Sep 24 05:19:51 2026 -0700
    
        [RWS] Remove dead RWS metadata plumbing from network service
    
        This plumbing supported devtools functionality that was (broken, and
        was) removed in https://crrev.com/c/8420927 and
        https://crrev.com/c/8421383. It also supported clearing cached data
        based on changes in the client's RWSes, but that data clearing was
        removed in https://crrev.com/c/8405168.
    
        Bug: 561405994
        Change-Id: If0c369c708503d3cb419712681cc5aad99699f2b
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8436056
        Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
        Reviewed-by: Rohit Rao <rohitrao@chromium.org>
        Reviewed-by: Avi Drissman <avi@chromium.org>
        Reviewed-by: Maks Orlovich <morlovich@chromium.org>
        Reviewed-by: Dominic Farolino <dom@chromium.org>
        Reviewed-by: Stefano Duo <stefanoduo@google.com>
        Cr-Commit-Position: refs/heads/main@{#1704225}

--- chromium_src/services/network/cookie_settings.h
+++ chromium_src/services/network/cookie_settings.h
@@ -12,7 +12,6 @@
       const net::CanonicalCookie& cookie, const GURL& url,            \
       const net::SiteForCookies& site_for_cookies,                    \
       const std::optional<url::Origin>& top_frame_origin,             \
-      const net::FirstPartySetMetadata& first_party_set_metadata,     \
       net::CookieSettingOverrides overrides,                          \
       net::CookieInclusionStatus* cookie_inclusion_status) const;     \
   net::NetworkDelegate::PrivacySetting IsEphemeralPrivacyModeEnabled( \
@@ -22,7 +21,6 @@
   bool AnnotateAndMoveUserBlockedEphemeralCookies(                    \
       const GURL& url, const net::SiteForCookies& site_for_cookies,   \
       const url::Origin* top_frame_origin,                            \
-      const net::FirstPartySetMetadata& first_party_set_metadata,     \
       net::CookieSettingOverrides overrides,                          \
       net::CookieAccessResultList& maybe_included_cookies,            \
       net::CookieAccessResultList& excluded_cookies) const;           \
